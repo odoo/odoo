62d2538c9dcb0c19efbb6df65c228cfee6cb2dd9
Rodolpho Lima <rcdl@odoo.com>
2024-09-26 20:23:10 +0200

    [IMP] html_editor: rename Image command to Media
    The "/Image" powerbox command opens a dialog that allows the user to
    insert images, documents, icons. When enabled, it also allows the user
    to insert videos.
    
    This commit updates the name and description of the "Image" powerbox
    command in order to reflect its actual capabilities:
    - the new name "Media" is more genereric and reflects better the current
      dialog title ("Select a media").
    - the new description ("Insert image, document, icon or video") matches
      the names of that tabs in the dialog.
    
    This commit also removes the Video powerbox command, as inserting videos
    can be done by Media command, thus making the Video command unnecessary
    and redundant.
    
    This commit makes sure that /image, /document, /icon and /video are all
    aliases to the same Media powerbox command.

--- addons/html_editor/static/tests/powerbox.test.js
+++ addons/html_editor/static/tests/powerbox.test.js
@@ -71,7 +71,7 @@ describe("search", () => {
         const { el, editor } = await setupEditor("<p>ab[]</p>");
         await insertText(editor, "/");
         await animationFrame();
-        expect(commandNames(el).length).toBe(28);
+        expect(commandNames(el).length).toBe(27);
         await insertText(editor, "head");
         await animationFrame();
         expect(commandNames(el)).toEqual(["Heading 1", "Heading 2", "Heading 3"]);
@@ -81,7 +81,7 @@ describe("search", () => {
         const { el, editor } = await setupEditor("<p>ab[]</p>");
         await insertText(editor, "/");
         await animationFrame();
-        expect(commandNames(el).length).toBe(28);
+        expect(commandNames(el).length).toBe(27);
         expect(".o-we-category").toHaveCount(8);
         expect(queryAllTexts(".o-we-category")).toEqual([
             "STRUCTURE",
@@ -104,7 +104,7 @@ describe("search", () => {
         const { el, editor } = await setupEditor("<p>ab[]</p>", { props: { iframe: true } });
         await insertText(editor, "/");
         await animationFrame();
-        expect(commandNames(el).length).toBe(28);
+        expect(commandNames(el).length).toBe(27);
         await insertText(editor, "head");
         await animationFrame();
         expect(commandNames(el)).toEqual(["Heading 1", "Heading 2", "Heading 3"]);
@@ -153,7 +153,7 @@ describe("search", () => {
         await insertText(editor, "/");
         await animationFrame();
         expect(".o-we-powerbox").toHaveCount(1);
-        expect(commandNames(el).length).toBe(28);
+        expect(commandNames(el).length).toBe(27);
 
         await insertText(editor, "headx");
         await animationFrame();
