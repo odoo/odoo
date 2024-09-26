b5813d64719d992c566428c4318ac280fc809b45
shup-odoo <shup@odoo.com>
2024-09-26 17:33:11 +0530

    [FIX] sale_project: set the project manager in project created from SO
    Previously, when SO confirm default project manager remain unset. This commit
    adapts the functionality of setting the project manager if the template is
    included otherwise the manager remains unset.
    
    task-3953729
    
    closes odoo/odoo#181593
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/sale_project/models/sale_order_line.py
+++ addons/sale_project/models/sale_order_line.py
@@ -187,7 +187,7 @@ class SaleOrderLine(models.Model):
             'active': True,
             'company_id': self.company_id.id,
             'allow_billable': True,
-            'user_id': False,
+            'user_id': self.product_id.project_template_id.user_id.id,
         }
 
     def _timesheet_create_project(self):
