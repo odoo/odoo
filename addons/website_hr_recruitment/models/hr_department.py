fc25d7fb9cfa091a08428cf4962c9de3ac4044f6
Krzysztof Magusiak (krma) <krma@odoo.com>
2024-09-26 10:54:46 +0000

    [FIX] odoo: add missing display_name(search)
    Cannot search on display name for some models that define themselves the
    field without specifying the search method.
    
    closes odoo/odoo#181563
    
    Related: odoo/enterprise#70746
    Signed-off-by: Rémy Voet (ryv) <ryv@odoo.com>

--- addons/website_hr_recruitment/models/hr_department.py
+++ addons/website_hr_recruitment/models/hr_department.py
@@ -8,4 +8,4 @@ class Department(models.Model):
     _inherit = 'hr.department'
 
     # Get department name using superuser, because model is not accessible for portal users
-    display_name = fields.Char(compute='_compute_display_name', compute_sudo=True)
+    display_name = fields.Char(compute='_compute_display_name', search='_search_display_name', compute_sudo=True)
