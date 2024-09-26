a2b7fd05c46ca8fe6103d4276ca08b7aaab4fde4
Andrzej(pian) <pian@odoo.com>
2024-09-26 13:48:55 +0000

    [FIX] account: limit attachments discarding to emails only
    When attaching an image in the account module, the `_check_and_decode_attachment`
    method led to the deletion of non-decodable attachments, causing subsequent access
    errors in `IrAttachment._post_add_create` since it was trying to use deleted record
    
    Reproduce
    ---
    - -i account
    - Create new draft invoice (without lines) -> Save
    - Attach some pdf with invoice
    - Try attaching some image to the chatter -> BUG: stuck on uploading
    
    While adapting the tests to test correctly the user endpoints instead of
    the internal functions, we realized that some cases are not possible:
    * plain text and CSV files will be discarded because we can't memorize
      their mimetype when getting the mail, and we can't guess it either.
    * when we are uploading multiple attachments on the journal, we can't
      regroup multiple attachments on the same invoices like we do on the
      mail alias. This could be changed in the future, but the test has only
      been adapted to reflect the current status.
    * the xml preface is not mandatory but we were using it to guess the
      mimetype of xml files. We use `guess_mimetype` instead.
    
    opw-4144868
    
    closes odoo/odoo#191040
    
    X-original-commit: 51d327b7390a68c568e5456d7302c336461a27b4
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>
    Co-authored-by: william-andre <wan@odoo.com>
    Co-authored-by: Andrzej(pian) <pian@odoo.com>

--- addons/account/tests/__init__.py
+++ addons/account/tests/__init__.py
@@ -49,3 +49,4 @@ from . import test_product
 from . import test_unexpected_invoice
 from . import test_mail_tracking_value
 from . import test_res_partner_merge
+from . import test_account_move_attachment
