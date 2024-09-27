aee7045e7afb66c4ef1c45112013e169fc3a98c8
Tanguy Quéguineur <taqu@odoo.com>
2024-09-27 14:21:29 +0000

    [FIX] hr_attendance: show correct company in kiosk mode
    This commit fixes two issues:
    - The logo displayed on the attendance kiosk mode is always the one of the
    first company in DB (id=1), no matter which one the user is actually logged.
    - The list of employees shown when manually indentifying isn't filtered by
    company and shows all employees.
    
    opw-4189915
    
    closes odoo/odoo#182492
    
    X-original-commit: 1fab3e79f9dc2648a5f48d75c4e116b691ff97ce
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- addons/hr_attendance/static/src/components/card_layout/card_layout.js
+++ addons/hr_attendance/static/src/components/card_layout/card_layout.js
@@ -1,7 +1,6 @@
 /** @odoo-module **/
 
 import { Component, useState, onWillUnmount } from "@odoo/owl";
-import { url } from "@web/core/utils/urls";
 
 const { DateTime } = luxon;
 export class CardLayout extends Component {
@@ -20,9 +19,6 @@ export class CardLayout extends Component {
         this.timeInterval = setInterval(() => {
             Object.assign(this.state, this.getDateTime());
         }, 1000);
-        this.companyImageUrl = url("/web/binary/company_logo", {
-            company: this.props.companyId,
-        });
         onWillUnmount(() => {
             clearInterval(this.timeInterval);
         });
