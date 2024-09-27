f465123b43c99accc11202a5afcf8b9b935ffc4d
Mehdi Outagant (MOU) <mou@odoo.com>
2024-09-27 16:34:33 +0200

    [IMP] website: allow highlighted text content in snippets preview
    A highlighted text has the following structure:
    
    ```
    <span class="o_text_highlight o_text_highlight_[highlightId]"
        style="--text-highlight-width: ...; --text-highlight-color: ...;">
        <span class="o_text_highlight_item">
            text content (line 1) ...
            <svg.../>
        </span>
        [<br/>]
        <span class="o_text_highlight_item">
            text content (line 2) ...
            <svg.../>
        </span>
        ...
    </span>
    ```
    
    Which is saved in a minimal format that allows the JS code to rebuild
    the SVGs when it's needed:
    
    ```
    <span class="o_text_highlight o_text_highlight_[highlightId]"
        style="--text-highlight-width: ...; --text-highlight-color: ...;">
        text content ...
    </span>
    ```
    
    The goal of this commit is to use the simplified format for theme
    customizations and adapt the code from the "Snippets Preview" and the
    "New Page Templates Preview" to be able to build the highlights using
    this format when provided in XML.
    
    task-4215788

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -288,6 +288,7 @@
         'web_editor.assets_wysiwyg': [
             'website/static/src/js/editor/editor.js',
             'website/static/src/xml/web_editor.xml',
+            'website/static/src/js/editor/add_snippet_dialog.js'
         ],
         'website.assets_wysiwyg': [
             ('include', 'web._assets_helpers'),
