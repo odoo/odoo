47224028e9bb784412b32a5df477a9468a8a9ed2
sesn-odoo <sesn@odoo.com>
2024-09-27 15:02:56 +0000

    [FIX] l10n_se: correct input VAT tags and tax report
    Steps to reproduce
    ------------------
    1. Install `l10n_se`.
    2. Switch to a Swedish company.
    3. Create and confirm a vendor bill with a price of 200, applying the
       preconfigured 25% purchase tax.
    4. Create and confirm a customer invoice with a price of 200, applying
       the preconfigured 25% sales tax.
    5. Open the tax report.
    
    Expected behavior
    -----------------
    - Fält 48 (Input VAT) = 50
    - Fält 49 (Output VAT - Input VAT) = 50 - 50 = 0
    
    Issue
    -----
    - Input VAT is incorrectly negated, resulting in Fält 48 = -50.
    - Consequently, Fält 49 is incorrectly calculated as 50 - (-50) = 100.
    - Additionally, the total for Block G does not match the value in Fält
      49.
    
    opw-4008171
    
    X-original-commit: ca67a41c53f160b0fb7cf22359162a09d3ffdd57

--- addons/l10n_se/__manifest__.py
+++ addons/l10n_se/__manifest__.py
@@ -4,7 +4,7 @@
     'website': 'https://www.odoo.com/documentation/saas-17.2/applications/finance/fiscal_localizations.html',
     'icon': '/account/static/description/l10n.png',
     'countries': ['se'],
-    'version': '1.0',
+    'version': '1.1',
     'author': 'XCLUDE, Odoo S.A.',
     'category': 'Accounting/Localizations/Account Charts',
     'description': """
