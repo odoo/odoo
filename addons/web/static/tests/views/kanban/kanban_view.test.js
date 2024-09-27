e1433398effca85fbd6951f8753b64a3fc5a51f2
Aaron Bohy <aab@odoo.com>
2024-09-27 12:45:13 +0000

    [FIX] web: kanban: color cards even if field not in arch
    Kanban cards can be colored (their left border) dynamically by
    using the root attribute `highlight_color="color"`, where `color`
    is the name of the integer field to use as color field. However,
    before this commit, this required the given `color` to be also
    defined in the arch, otherwise it wouldn't have been fetched, and
    no card would be colored.
    
    With this commit, we automatically fetch the highlight_color field.
    
    This fixes a bug in planning where colors weren't displayed.
    
    Task~4215979
    
    closes odoo/odoo#181987
    
    X-original-commit: 33570089fcca5cd0f4ccbd05b7049e1a0dfe7c14
    Signed-off-by: Jorge Pinna Puissant (jpp) <jpp@odoo.com>

--- addons/web/static/tests/views/kanban/kanban_view.test.js
+++ addons/web/static/tests/views/kanban/kanban_view.test.js
@@ -8009,7 +8009,6 @@ test("kanban with color attribute", async () => {
         resModel: "category",
         arch: `
             <kanban highlight_color="color">
-                <field name="color"/>
                 <templates>
                     <t t-name="card">
                         <field name="name"/>
