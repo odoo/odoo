9c0b0891cdca2789ffe4738afb8f30254ec15bd5
Brice bib Bartoletti <bib@odoo.com>
2024-09-28 00:28:32 +0200

    [IMP] l10n_eu_oss: OSS on first branch with a TAX ID
    The aim of this commit is to allow to instanciate OSS on the first
    branch having a tax id instead of always instanciating OSS on the root
    company.
    
    Context:
    The end goal is to allow to instanciate OSS for a company outside EU and
    let it use sub branch to manage the tax mapping.
    
    Previous to this commit:
    OSS taxes gets installed automatically on all companies when loading a
    chart template or installing the OSS modules.
    OSS taxes and fiscal position are always setup on the root company.
    
    After this commit:
    OSS taxes never gets installed automatically when loading a chart template
    or installing the OSS module.
    OSS taxes and fiscal position are setup on the first sub branch having a
    TAX ID.
    
    task: no task
    
    Global context:
    TSB, LZZ, THBE and me (BIB) had a very long meeting to find out what
    would be the better the solution to allow a UK company to use the
    OSS tax system with Belgium as its entry point.
    
    The UK company has a wharehouse in Belgium and thus needs to report
    anything coming out of that wharehouse to the on the Belgian VAT report
    and the "Belgian" OSS tax report.
    It also needs to invoice any Belgian with the Belgian taxes and VAT (and
    thus can't use OSS for that one).
    Some of their product might be purely Belgian and thus only have a
    Belgian tax set on them. We thus need to support both the mapping from
    the motherland UK and from Belgium.
    
    The following are required:
    - Creating the Belgian VAT taxes.
    - Creating the OSS taxes as they would be for a Belgian company.
    - Create the fiscal position mapping from Belgian tax to EU taxes.
    - Create the fiscal position mapping from UK to EU taxes.
    
    The main goal of the task is to:
    - be able to instantiate OSS on a specific branch: we go from the
      current one up and we instantiate OSS to the first one with a TAX ID.
    - we should be able to use the classic OSS tag as usually.
    - be able to report those numbers in the OSS tax report.
    
    Part-of: odoo/odoo#181954
    Related: odoo/enterprise#71006
    Signed-off-by: Nicolas Viseur (vin) <vin@odoo.com>

--- addons/l10n_eu_oss/__init__.py
+++ addons/l10n_eu_oss/__init__.py
@@ -3,8 +3,5 @@
 
 from . import models
 
-def l10n_eu_oss_post_init(env):
-    env['res.company']._map_all_eu_companies_taxes()
-
 def l10n_eu_oss_uninstall(env):
     env.cr.execute("DELETE FROM ir_model_data WHERE module = 'l10n_eu_oss' and model in ('account.tax.group', 'account.account');")
