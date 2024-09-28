keyboard-slayer/odoo-eval

[REV] account,point_of_sale: No more factoring of tax amount in taxes computation
This reverts commit 2884d3485b486d075a15334376cc360ad8c108b6.

The previous commit breaks the behavior when dealing with reverse charge taxes in documents
having no accounting entries.
Suppose a reverse charge tax of 21% having a distribution +100 -100.
On a purchase order, the amount will be 21.
On the invoice, it's 0 because the invoice generates two tax lines: +21 & -21.

addons/account/models/account_tax.py
@@ -196,6 +196,7 @@ class AccountTax(models.Model):
     is_used = fields.Boolean(string="Tax used", compute='_compute_is_used')
     repartition_lines_str = fields.Char(string="Repartition Lines", tracking=True, compute='_compute_repartition_lines_str')
     invoice_legal_notes = fields.Html(string="Legal Notes", help="Legal mentions that have to be printed on the invoices.")
+    total_tax_factor = fields.Float(compute='_compute_total_tax_factor')
 
     @api.constrains('company_id', 'name', 'type_tax_use', 'tax_scope', 'country_id')
     def _constrains_name(self):
@@ -447,6 +448,11 @@ class AccountTax(models.Model):
                     Command.create({'document_type': 'refund', 'repartition_type': 'base', 'tag_ids': []}),
                     Command.create({'document_type': 'refund', 'repartition_type': 'tax', 'tag_ids': []}),
                 ]
+
+    @api.depends('invoice_repartition_line_ids.factor', 'invoice_repartition_line_ids.repartition_type')
+    def _compute_total_tax_factor(self):
+        for tax in self:
+            tax.total_tax_factor = sum(tax.invoice_repartition_line_ids.filtered(lambda x: x.repartition_type == 'tax').mapped('factor'))
 
     @staticmethod
     def _parse_name_search(name):
@@ -793,7 +799,7 @@ class AccountTax(models.Model):
                 yield tax_after
 
         def add_extra_base(other_tax, sign):
-            tax_amount = taxes_data[tax.id]['tax_amount']
+            tax_amount = taxes_data[tax.id]['tax_amount_factorized']
             if 'tax_amount' not in taxes_data[other_tax.id]:
                 taxes_data[other_tax.id]['extra_base_for_tax'] += sign * tax_amount
             taxes_data[other_tax.id]['extra_base_for_base'] += sign * tax_amount
@@ -895,7 +901,7 @@ class AccountTax(models.Model):
         """
         self.ensure_one()
         if self.amount_type == 'percent':
-            total_percentage = sum(tax.amount for tax in batch) / 100.0
+            total_percentage = sum(tax.total_tax_factor * tax.amount for tax in batch) / 100.0
             to_price_excluded_factor = 1 / (1 + total_percentage) if total_percentage != -1 else 0.0
             return raw_base * to_price_excluded_factor * self.amount / 100.0
 
@@ -918,7 +924,7 @@ class AccountTax(models.Model):
             return raw_base * self.amount / 100.0
 
         if self.amount_type == 'division':
-            total_percentage = sum(tax.amount for tax in batch) / 100.0
+            total_percentage = sum(tax.total_tax_factor * tax.amount for tax in batch) / 100.0
             incl_base_multiplicator = 1.0 if total_percentage == 1.0 else 1 - total_percentage
             return raw_base * self.amount / 100.0 / incl_base_multiplicator
 
@@ -972,10 +978,11 @@ class AccountTax(models.Model):
         def add_tax_amount_to_results(tax, tax_amount):
             tax_data = taxes_data[tax.id]
             tax_data['tax_amount'] = tax_amount
+            taxes_data[tax.id]['tax_amount_factorized'] = tax_amount * tax.total_tax_factor
             special_mode = evaluation_context['special_mode']
             round_price_include = evaluation_context['round_price_include']
             if rounding_method == 'round_per_line' or (not special_mode and tax_data['price_include'] and round_price_include):
-                taxes_data[tax.id]['tax_amount'] = float_round(taxes_data[tax.id]['tax_amount'], precision_rounding=precision_rounding)
+                taxes_data[tax.id]['tax_amount_factorized'] = float_round(taxes_data[tax.id]['tax_amount'], precision_rounding=precision_rounding)
             sorted_taxes._propagate_extra_taxes_base(tax, taxes_data, special_mode=special_mode)
 
         def eval_tax_amount(tax_amount_function, tax):
@@ -1057,7 +1064,7 @@ class AccountTax(models.Model):
             if 'tax_amount' not in taxes_data[tax.id]:
                 continue
 
-            total_tax_amount = sum(taxes_data[other_tax.id]['tax_amount'] for other_tax in taxes_data[tax.id]['batch'])
+            total_tax_amount = sum(taxes_data[other_tax.id]['tax_amount_factorized'] for other_tax in taxes_data[tax.id]['batch'])
             base = raw_base + taxes_data[tax.id]['extra_base_for_base']
             if taxes_data[tax.id]['price_include'] and special_mode in (False, 'total_included'):
                 base -= total_tax_amount
@@ -1067,7 +1074,7 @@ class AccountTax(models.Model):
 
         if taxes_data_list:
             total_excluded = taxes_data_list[0]['base']
-            tax_amount = sum(tax_data['tax_amount'] for tax_data in taxes_data_list)
+            tax_amount = sum(tax_data['tax_amount_factorized'] for tax_data in taxes_data_list)
             total_included = total_excluded + tax_amount
         else:
             total_included = total_excluded = raw_base
@@ -1080,7 +1087,8 @@ class AccountTax(models.Model):
                     'tax': tax_data['tax'],
                     'group': batching_results['group_per_tax'].get(tax_data['tax'].id) or self.env['account.tax'],
                     'batch': batching_results['batch_per_tax'][tax_data['tax'].id],
-                    'tax_amount': tax_data['tax_amount'],
+                    'tax_amount_unfactorized': tax_data['tax_amount'],
+                    'tax_amount': tax_data['tax_amount_factorized'],
                     'base_amount': tax_data['base'],
                 }
                 for tax_data in taxes_data_list
@@ -1167,17 +1175,13 @@ class AccountTax(models.Model):
 
             # Split naively by repartition line.
             rep_lines = tax[repartition_lines_field].filtered(lambda x: x.repartition_type == 'tax')
-            repartition_line_amounts = [tax_data['tax_amount'] * line.factor for line in rep_lines]
+            repartition_line_amounts = [tax_data['tax_amount_unfactorized'] * line.factor for line in rep_lines]
             if rounding_method == 'round_per_line':
                 repartition_line_amounts = [currency.round(x) for x in repartition_line_amounts]
             total_repartition_line_amount = sum(repartition_line_amounts)
 
             # Fix the rounding error caused by rounding.
-            total_factor = sum(rep_lines.mapped('factor'))
-            total_tax_amount = tax_data['tax_amount'] * total_factor
-            if rounding_method == 'round_per_line':
-                total_tax_amount = currency.round(total_tax_amount)
-            total_error = total_tax_amount - total_repartition_line_amount
+            total_error = tax_data['tax_amount'] - total_repartition_line_amount
             error_sign = -1 if total_error < 0.0 else 1
             total_error *= error_sign
             for index, _amount in enumerate(repartition_line_amounts):
@@ -1200,12 +1204,14 @@ class AccountTax(models.Model):
                 else:
                     rep_line_tags = rep_line.tag_ids
 
-                tax_rep_values_list.append({
+                tax_rep_values = {
                     **tax_data,
                     'tax_amount': line_amount,
                     'tax_repartition_line': rep_line,
                     'tags': tax_data['tags'] | rep_line_tags,
-                })
+                }
+                tax_rep_values.pop('tax_amount_unfactorized')
+                tax_rep_values_list.append(tax_rep_values)
         return tax_rep_values_list
 
     def flatten_taxes_hierarchy(self):

odoo/odoo

X-original-commit: 3145939a40e0bf4214d37fdcea67b05a804798a8

addons/account/models/account_tax.py
@@ -195,6 +195,7 @@ class AccountTax(models.Model):
     is_used = fields.Boolean(string="Tax used", compute='_compute_is_used')
     repartition_lines_str = fields.Char(string="Repartition Lines", tracking=True, compute='_compute_repartition_lines_str')
     invoice_legal_notes = fields.Html(string="Legal Notes", help="Legal mentions that have to be printed on the invoices.")
+    total_tax_factor = fields.Float(compute='_compute_total_tax_factor')
 
     @api.constrains('company_id', 'name', 'type_tax_use', 'tax_scope', 'country_id')
     def _constrains_name(self):
@@ -438,6 +439,11 @@ class AccountTax(models.Model):
                     Command.create({'document_type': 'refund', 'repartition_type': 'base', 'tag_ids': []}),
                     Command.create({'document_type': 'refund', 'repartition_type': 'tax', 'tag_ids': []}),
                 ]
+
+    @api.depends('invoice_repartition_line_ids.factor', 'invoice_repartition_line_ids.repartition_type')
+    def _compute_total_tax_factor(self):
+        for tax in self:
+            tax.total_tax_factor = sum(tax.invoice_repartition_line_ids.filtered(lambda x: x.repartition_type == 'tax').mapped('factor'))
 
     @staticmethod
     def _parse_name_search(name):
@@ -784,7 +790,7 @@ class AccountTax(models.Model):
                 yield tax_after
 
         def add_extra_base(other_tax, sign):
-            tax_amount = taxes_data[tax.id]['tax_amount']
+            tax_amount = taxes_data[tax.id]['tax_amount_factorized']
             if 'tax_amount' not in taxes_data[other_tax.id]:
                 taxes_data[other_tax.id]['extra_base_for_tax'] += sign * tax_amount
             taxes_data[other_tax.id]['extra_base_for_base'] += sign * tax_amount
@@ -886,7 +892,7 @@ class AccountTax(models.Model):
         """
         self.ensure_one()
         if self.amount_type == 'percent':
-            total_percentage = sum(tax.amount for tax in batch) / 100.0
+            total_percentage = sum(tax.total_tax_factor * tax.amount for tax in batch) / 100.0
             to_price_excluded_factor = 1 / (1 + total_percentage) if total_percentage != -1 else 0.0
             return raw_base * to_price_excluded_factor * self.amount / 100.0
 
@@ -909,7 +915,7 @@ class AccountTax(models.Model):
             return raw_base * self.amount / 100.0
 
         if self.amount_type == 'division':
-            total_percentage = sum(tax.amount for tax in batch) / 100.0
+            total_percentage = sum(tax.total_tax_factor * tax.amount for tax in batch) / 100.0
             incl_base_multiplicator = 1.0 if total_percentage == 1.0 else 1 - total_percentage
             return raw_base * self.amount / 100.0 / incl_base_multiplicator
 
@@ -963,10 +969,11 @@ class AccountTax(models.Model):
         def add_tax_amount_to_results(tax, tax_amount):
             tax_data = taxes_data[tax.id]
             tax_data['tax_amount'] = tax_amount
+            taxes_data[tax.id]['tax_amount_factorized'] = tax_amount * tax.total_tax_factor
             special_mode = evaluation_context['special_mode']
             round_price_include = evaluation_context['round_price_include']
             if rounding_method == 'round_per_line' or (not special_mode and tax_data['price_include'] and round_price_include):
-                taxes_data[tax.id]['tax_amount'] = float_round(taxes_data[tax.id]['tax_amount'], precision_rounding=precision_rounding)
+                taxes_data[tax.id]['tax_amount_factorized'] = float_round(taxes_data[tax.id]['tax_amount_factorized'], precision_rounding=precision_rounding)
             sorted_taxes._propagate_extra_taxes_base(tax, taxes_data, special_mode=special_mode)
 
         def eval_tax_amount(tax_amount_function, tax):
@@ -1048,7 +1055,7 @@ class AccountTax(models.Model):
             if 'tax_amount' not in taxes_data[tax.id]:
                 continue
 
-            total_tax_amount = sum(taxes_data[other_tax.id]['tax_amount'] for other_tax in taxes_data[tax.id]['batch'])
+            total_tax_amount = sum(taxes_data[other_tax.id]['tax_amount_factorized'] for other_tax in taxes_data[tax.id]['batch'])
             base = raw_base + taxes_data[tax.id]['extra_base_for_base']
             if taxes_data[tax.id]['price_include'] and special_mode in (False, 'total_included'):
                 base -= total_tax_amount
@@ -1058,7 +1065,7 @@ class AccountTax(models.Model):
 
         if taxes_data_list:
             total_excluded = taxes_data_list[0]['base']
-            tax_amount = sum(tax_data['tax_amount'] for tax_data in taxes_data_list)
+            tax_amount = sum(tax_data['tax_amount_factorized'] for tax_data in taxes_data_list)
             total_included = total_excluded + tax_amount
         else:
             total_included = total_excluded = raw_base
@@ -1071,7 +1078,8 @@ class AccountTax(models.Model):
                     'tax': tax_data['tax'],
                     'group': batching_results['group_per_tax'].get(tax_data['tax'].id) or self.env['account.tax'],
                     'batch': batching_results['batch_per_tax'][tax_data['tax'].id],
-                    'tax_amount': tax_data['tax_amount'],
+                    'tax_amount_unfactorized': tax_data['tax_amount'],
+                    'tax_amount': tax_data['tax_amount_factorized'],
                     'base_amount': tax_data['base'],
                 }
                 for tax_data in taxes_data_list
@@ -1158,17 +1166,13 @@ class AccountTax(models.Model):
 
             # Split naively by repartition line.
             rep_lines = tax[repartition_lines_field].filtered(lambda x: x.repartition_type == 'tax')
-            repartition_line_amounts = [tax_data['tax_amount'] * line.factor for line in rep_lines]
+            repartition_line_amounts = [tax_data['tax_amount_unfactorized'] * line.factor for line in rep_lines]
             if rounding_method == 'round_per_line':
                 repartition_line_amounts = [currency.round(x) for x in repartition_line_amounts]
             total_repartition_line_amount = sum(repartition_line_amounts)
 
             # Fix the rounding error caused by rounding.
-            total_factor = sum(rep_lines.mapped('factor'))
-            total_tax_amount = tax_data['tax_amount'] * total_factor
-            if rounding_method == 'round_per_line':
-                total_tax_amount = currency.round(total_tax_amount)
-            total_error = total_tax_amount - total_repartition_line_amount
+            total_error = tax_data['tax_amount'] - total_repartition_line_amount
             error_sign = -1 if total_error < 0.0 else 1
             total_error *= error_sign
             for index, _amount in enumerate(repartition_line_amounts):
@@ -1191,12 +1195,14 @@ class AccountTax(models.Model):
                 else:
                     rep_line_tags = rep_line.tag_ids
 
-                tax_rep_values_list.append({
+                tax_rep_values = {
                     **tax_data,
                     'tax_amount': line_amount,
                     'tax_repartition_line': rep_line,
                     'tags': tax_data['tags'] | rep_line_tags,
-                })
+                }
+                tax_rep_values.pop('tax_amount_unfactorized')
+                tax_rep_values_list.append(tax_rep_values)
         return tax_rep_values_list
 
     def flatten_taxes_hierarchy(self):