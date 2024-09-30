24221481b590781bb0d66cc7d6f4e9d4a17276e1
mega <mega@odoo.com>
2024-09-30 05:53:59 +0000

    [FIX] account: prevent an error when archiving multiple journals
    This Error occurs when a user tries to archive multiple journals.
    
    Steps to reproduce:
    
    - Install the 'account' module
    - Go to Invoicing / Configuration / Accounting / Journals.
    - Select two or more journals and try to archive them.
    
    ValueError: Expected singleton: account.journal(15, 18)
    
    An error occurs at [1] because the system attempts to retrieve the id from
    'self', which contains multiple records.
    
    link [1]:https://github.com/odoo/odoo/blob/1f7debc24bab4e0a7f2ef0d85e50d9381e1826cd/addons/account/models/account_journal.py#L801
    
    To resolve this issue, Use an 'ids' instead of an id to get a multiple record.
    
    Sentry-5917175119
    
    X-original-commit: d2714fa73e7c70e4e68f94e5ef53acb16c9e9fa9

--- addons/account/tests/test_account_journal.py
+++ addons/account/tests/test_account_journal.py
@@ -167,6 +167,27 @@ class TestAccountJournal(AccountTestInvoicingCommon):
         with self.assertRaises(ValidationError):
             journal.action_archive()
 
+    def test_archive_multiple_journals(self):
+        journals = self.env['account.journal'].create([{
+                'name': 'Test Journal 1',
+                'type': 'sale',
+                'code': 'A1'
+            }, {
+                'name': 'Test Journal 2',
+                'type': 'sale',
+                'code': 'A2'
+            }])
+
+        # Archive the Journals
+        journals.action_archive()
+        self.assertFalse(journals[0].active)
+        self.assertFalse(journals[1].active)
+
+        # Unarchive the Journals
+        journals.action_unarchive()
+        self.assertTrue(journals[0].active)
+        self.assertTrue(journals[1].active)
+
 
 @tagged('post_install', '-at_install', 'mail_alias')
 class TestAccountJournalAlias(AccountTestInvoicingCommon, MailCommon):
