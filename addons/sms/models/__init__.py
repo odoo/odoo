e38db0ad6d0d56df70b79b766cded91360f127ee
Thibault Delavallée <tde@odoo.com>
2024-09-30 15:46:59 +0200

    [MOV] phone_validation: make partner inherit from phone mixin
    Several module use phone_validation and either expect partner model
    to inherit from 'mail.thread.phone', either add the inherit, either
    rely on 'sms' module being automatically installed to work.
    
    It makes sense when installing phone validation tools to activate the
    sanitized number computation on partners. When using phone_validation
    you are probably going to use numbers, hence having this field available
    is handy.
    
    Notably necessary for whatsapp or voip.
    
    Task-4215393
    
    closes odoo/odoo#182159
    
    Related: odoo/enterprise#71099
    Related: odoo/upgrade#6584
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/sms/models/__init__.py
+++ addons/sms/models/__init__.py
@@ -8,7 +8,6 @@ from . import mail_followers
 from . import mail_message
 from . import mail_notification
 from . import mail_thread
-from . import res_partner
 from . import sms_sms
 from . import sms_template
 from . import sms_tracker
