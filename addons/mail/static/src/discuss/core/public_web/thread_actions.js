e27bd8e89b7574d799bec71f6633599fbc7f40b4
tsm-odoo <tsm@odoo.com>
2024-09-30 12:07:14 +0200

    [IMP] mail: navigate to other threads from a thread
    Purpose of this commit:
    Enable navigating and creating a sub thread from another sub thread
    using action buttons.
    
    task-4202826

--- addons/mail/static/src/discuss/core/public_web/thread_actions.js
+++ addons/mail/static/src/discuss/core/public_web/thread_actions.js
@@ -8,7 +8,9 @@ threadActionsRegistry.add("show-threads", {
     close(component, action) {
         action.popover?.close();
     },
-    condition: (component) => component.thread?.hasSubChannelFeature,
+    condition: (component) =>
+        component.thread?.hasSubChannelFeature ||
+        component.thread?.parent_channel_id?.hasSubChannelFeature,
     icon: "fa fa-fw fa-comments-o",
     iconLarge: "fa fa-fw fa-lg fa-comments-o",
     name: _t("Threads"),
@@ -30,10 +32,10 @@ threadActionsRegistry.add("show-threads", {
             },
         });
     },
-    open: (component, action) =>
-        action.popover?.open(component.root.el.querySelector(`[name="${action.id}"]`), {
-            thread: component.thread,
-        }),
+    open: (component, action) => {
+        const thread = component.thread?.parent_channel_id || component.thread;
+        action.popover?.open(component.root.el.querySelector(`[name="${action.id}"]`), { thread });
+    },
     sequence: (comp) => (comp.props.chatWindow ? 40 : 5),
     sequenceGroup: 10,
     toggle: true,
