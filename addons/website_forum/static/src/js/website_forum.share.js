d38c2fb1ecee9c797eef4a0766c6a9cd06e17aed
ksbh-odoo <ksbh@odoo.com>
2024-09-30 12:31:36 +0530

    [IMP] website_forum: remove static xml

--- addons/website_forum/static/src/js/website_forum.share.js
+++ addons/website_forum/static/src/js/website_forum.share.js
@@ -7,6 +7,9 @@ import { renderToElement } from "@web/core/utils/render";
 const ForumShare = publicWidget.Widget.extend({
     selector: '',
     events: {},
+    assetLibs: [
+        "website_forum.assets_widget_xml",
+    ],
 
     /**
      * @override
