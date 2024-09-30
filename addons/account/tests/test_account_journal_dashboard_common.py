17adfed78faa9b29a49fa83adc6edb60d84709f0
guva-odoo <guva@odoo.com>
2024-09-30 15:12:18 +0000

    [IMP] account: add common for journal dashboard tests
    Manual back porting the part of 02f78498972a6fc7d560c0e6c0a5b3bf63eb58d3
    that add common for account journal dashbaord tests.
    
    opw-4182523
    
    X-original-commit: 045317444ab637bcc396dccf2763773be2998cdf

--- addons/account/tests/test_account_journal_dashboard_common.py
+++ addons/account/tests/test_account_journal_dashboard_common.py
@@ -1,4 +1,5 @@
 from odoo.addons.account.tests.common import AccountTestInvoicingCommon
+<<<<<<< 18.0
 
 from odoo import Command
 from odoo.tests import tagged
@@ -70,6 +71,75 @@ class TestAccountJournalDashboardCommon(AccountTestInvoicingCommon):
         # This bill has two amls of 100$. One which is late and due 14 days prior and one which is waiting for payment and due in 15 days.
         # number_waiting += 2, sum_waiting += -400$, number_late += 1, sum_late += -100$
 
+||||||| fa8fae195d3926884285ab7aa21fb86c7ea683ea
+=======
+from odoo import Command
+from odoo.tests import tagged
+from odoo.tools.misc import format_amount
+
+
+@tagged('post_install', '-at_install')
+class TestAccountJournalDashboardCommon(AccountTestInvoicingCommon):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.other_currency = cls.setup_other_currency('EUR')
+
+    def _create_test_vendor_bills(self, journal):
+        # Setup multiple payments term
+        twentyfive_now_term = self.env['account.payment.term'].create({
+            'name': '25% now, rest in 30 days',
+            'note': 'Pay 25% on invoice date and 75% 30 days later',
+            'line_ids': [
+                Command.create({
+                    'value': 'percent',
+                    'value_amount': 25.00,
+                    'delay_type': 'days_after',
+                    'nb_days': 0,
+                }),
+                Command.create({
+                    'value': 'percent',
+                    'value_amount': 75.00,
+                    'delay_type': 'days_after',
+                    'nb_days': 30,
+                }),
+            ],
+        })
+        self.env['account.move'].create({
+            'move_type': 'in_invoice',
+            'journal_id': journal.id,
+            'partner_id': self.partner_a.id,
+            'invoice_date': '2023-04-01',
+            'date': '2023-03-15',
+            'invoice_payment_term_id': twentyfive_now_term.id,
+            'invoice_line_ids': [Command.create({
+                'product_id': self.product_a.id,
+                'quantity': 1,
+                'name': 'product test 1',
+                'price_unit': 4000,
+                'tax_ids': [],
+            })]
+        }).action_post()
+        # This bill has two amls of 10$. Both are waiting for payment and due in 16 and 46 days.
+        # number_waiting += 2, sum_waiting += -4000$, number_late += 0, sum_late += 0$
+        self.env['account.move'].create({
+            'move_type': 'in_invoice',
+            'journal_id': journal.id,
+            'partner_id': self.partner_a.id,
+            'invoice_date': '2023-03-01',
+            'date': '2023-03-15',
+            'invoice_payment_term_id': twentyfive_now_term.id,
+            'invoice_line_ids': [Command.create({
+                'product_id': self.product_a.id,
+                'quantity': 1,
+                'name': 'product test 1',
+                'price_unit': 400,
+                'tax_ids': [],
+            })]
+        }).action_post()
+        # This bill has two amls of 100$. One which is late and due 14 days prior and one which is waiting for payment and due in 15 days.
+        # number_waiting += 2, sum_waiting += -400$, number_late += 1, sum_late += -100$
+>>>>>>> d3fa0a569aa60fdcf27684a4c732a40fd77e3ba6
         self.env['account.move'].create({
             'move_type': 'in_invoice',
             'journal_id': journal.id,
