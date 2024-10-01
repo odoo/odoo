odoo/odoo

commit a707f8345cec8f404077b184f4b0339932c10892

[PERF] spreadsheet_account: optimize fetch_debit_credit SQL query
Issue:
Displaying the data on the dashboard accounting can take up to 1 minute

Analysis:
The spreadsheet_fetch_debit_credit function needs a domain in order to compute the formulas. The domain being set in `_build_spreadsheet_formula_domain` use a like search for account_account.code on account_move_line which is a big table. Thus it does not scale well. There is also a unnecessary complexity on adding an "OR expression" on each one of the account_id code searched for.

Solution:
It can be optimized by searching for the full exhaustive (like in SQL) codes in account.accoun since it has way less records and thus more scalable. And then use an exact condition (=) which is faster than a like search. Also adding more readable while adding the balance_domain and pnl domain only once in the whole query.

Benchmark:

| Before PR | After PR |
|:----------:|:---------:|
| 8s 51 ms | 42 ms code search + 42 ms full query |

Related ticket:
opw-3328144

Version affected:
16.0 and above

closes odoo/odoo#134514

Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

addons/spreadsheet_account/models/account.py
@@ -58,16 +58,17 @@ class AccountMove(models.Model):
             ("date", ">=", start),
             ("date", "<=", end),
         ]
+        # It is more optimized to (like) search for code directly in account.account than in account_move_line
         code_domain = expression.OR(
             [
-                expression.AND([
-                    [("account_id.code", "=like", f"{code}%")],
-                    expression.OR([balance_domain, pnl_domain]),
-                ])
-                for code in codes
+                ("code", "=like", f"{code}%"),
             ]
+            for code in codes
         )
-        domain = expression.AND([code_domain, [("company_id", "=", company_id)]])
+        account_ids = self.env["account.account"].search(code_domain).ids
+        code_domain = [("account_id", "in", account_ids)]
+        period_domain = expression.OR([balance_domain, pnl_domain])
+        domain = expression.AND([code_domain, period_domain, [("company_id", "=", company_id)]])
         if formula_params["include_unposted"]:
             domain = expression.AND(
                 [domain, [("move_id.state", "!=", "cancel")]]

addons/spreadsheet_account/tests/test_debit_credit.py
@@ -919,7 +919,7 @@ class SpreadsheetAccountingFunctionsTest(AccountTestInvoicingCommon):
                     "&",
                     "&",
                     "&",
-                    ("account_id.code", "=like", "sp1234566%"),
+                    ("account_id", "in", [self.account_revenue_c1.id]),
                     "|",
                     "&",
                     ("account_id.include_initial_balance", "=", True),

commit 25056ad22ac8e63be62686578f3aba696cde34b0

[FIX] spreadsheet_account: get balance from other companies
Steps to reproduce:

- use ODOO.BALANCE("1",,1) (make sure it returns some value)
- switch to another company (company 1 should not be active)
=> the balance doesn't appear even though the company_id is provided
   and set to 1

Issue introduced with a707f8345cec8f40407

Task: 4223319

addons/spreadsheet_account/models/account.py
@@ -67,7 +67,7 @@ class AccountMove(models.Model):
             ]
             for code in codes
         )
-        account_ids = self.env["account.account"].search(code_domain).ids
+        account_ids = self.env["account.account"].with_company(company_id).search(code_domain).ids
         code_domain = [("account_id", "in", account_ids)]
         period_domain = expression.OR([balance_domain, pnl_domain])
         domain = expression.AND([code_domain, period_domain, [("company_id", "=", company_id)]])

addons/spreadsheet_account/tests/test_debit_credit.py
@@ -253,6 +253,37 @@ class SpreadsheetAccountingFunctionsTest(AccountTestInvoicingCommon):
             ],
         )
 
+    def test_company_not_in_env(self):
+        Account = self.env["account.account"].with_company(self.company_data["company"].id)
+        self.assertEqual(
+            Account.spreadsheet_fetch_debit_credit(
+                [
+                    {
+                        "date_range": {
+                            "range_type": "year",
+                            "year": 2022,
+                        },
+                        "codes": ["sp1234566"],  # only for company 1
+                        "company_id": self.company_data["company"].id,
+                        "include_unposted": True,
+                    },
+                    {
+                        "date_range": {
+                            "range_type": "year",
+                            "year": 2022,
+                        },
+                        "codes": ["sp99887755"],  # only for company 2
+                        "company_id": self.company_data_2["company"].id,
+                        "include_unposted": True,
+                    },
+                ]
+            ),
+            [
+                {"credit": 0, "debit": 500.0},
+                {"credit": 0, "debit": 1500.0},
+            ],
+        )
+
     def test_do_not_count_future_years(self):
         self.env["account.move"].create(
             {