ba4cb849c1a91a576affa532190933e5900cfa13
Mehdi Outagant (MOU) <mou@odoo.com>
2024-10-01 11:02:31 +0200

    [FIX] web_editor, website: fix highlights in themes customizations
    Starting from `18.0`, text highlight effects are used for snippets
    customizations in themes, which means snippets can be provided with
    highlighted content (in a minimal format) that allows the JS code to
    rebuild the SVGs later when it's needed:
    
    ```
    <span class="o_text_highlight o_text_highlight_[highlightId]"
        style="--text-highlight-width: ...; --text-highlight-color: ...;">
        text content ...
    </span>
    ```
    
    The highlights code provides some tools to adapt them when the content
    is changed: add & remove text, resize… But It doesn't handle the case
    of a dropped snippet with highlights.
    
    The goal of this commit is to be able to build the highlights of a
    snippet once dropped.
    
    Remark: The behavior fixed in this commit wasn't possible until `18.0`
    (with themes snippets using highlights) but we target `17.0` to handle
    every possible customization that uses the effects before themes
    refactoring.
    
    task-4215788

--- addons/website/static/src/js/editor/snippets.editor.js
+++ addons/website/static/src/js/editor/snippets.editor.js
@@ -9,7 +9,7 @@ import wUtils from "@website/js/utils";
 import * as OdooEditorLib from "@web_editor/js/editor/odoo-editor/src/utils/utils";
 import { Component, onMounted, useRef, useState } from "@odoo/owl";
 import { throttleForAnimation } from "@web/core/utils/timing";
-import { switchTextHighlight } from "@website/js/text_processing";
+import { applyTextHighlight, switchTextHighlight } from "@website/js/text_processing";
 
 const getDeepRange = OdooEditorLib.getDeepRange;
 const getTraversedNodes = OdooEditorLib.getTraversedNodes;
@@ -594,6 +594,16 @@ const wSnippetMenu = weSnippetEditor.SnippetsMenu.extend({
      */
     _allowParentsEditors($snippet) {
         return this._super(...arguments) && !$snippet[0].classList.contains("s_popup");
+    },
+    /**
+     * @override
+     */
+    _updateDroppedSnippet($target) {
+        // Build the highlighted text content for the snippets.
+        for (const textEl of $target[0]?.querySelectorAll(".o_text_highlight") || []) {
+            applyTextHighlight(textEl);
+        }
+        return this._super(...arguments);
     },
 
     //--------------------------------------------------------------------------
