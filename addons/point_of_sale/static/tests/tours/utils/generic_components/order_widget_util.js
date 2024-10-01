f6c3bb027e4f74fac516d8695aeef1006ff62a76
roen-odoo <roen@odoo.com>
2024-10-01 14:26:47 +0000

    [FIX] point_of_sale,pos_sale: ignore fixed tax in downpayment
    When you made a downpayment on an order that contained product with
    fixed amount taxes, the tax would be paid multiple times.
    
    Steps to reproduce:
    -------------------
    * Create a tax T1 with a fixed amount of 10€
    * Create a product P1 using the tax T1
    * Make a sale order and add the product P1 to it
    * Open PoS and make a downpayment for the sale order (e.g 50%)
    * You will already pay the 10€ of tax
    * Now if you make a second downpayment (e.g. 10%)
    > Observation: You still have the 10€ tax to pay
    
    Why the fix:
    ------------
    We match the behavior of sales app, and ignore the fixed price taxes
    when creating the downpayment lines.
    
    opw-4163579
    
    X-original-commit: 10b9e1e04d1d6794bc87285aa3694e937a4015c2

--- addons/point_of_sale/static/tests/tours/utils/generic_components/order_widget_util.js
+++ addons/point_of_sale/static/tests/tours/utils/generic_components/order_widget_util.js
@@ -98,3 +98,10 @@ export function hasTax(amount) {
         trigger: `.order-summary .tax:contains("${amount}")`,
     };
 }
+
+export function hasNoTax() {
+    return {
+        content: "order has not tax",
+        trigger: ".order-summary .tax:empty",
+    };
+}
