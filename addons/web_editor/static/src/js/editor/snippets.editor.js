95fa3f7c64b502bbe2bd5966c94ce925acf7eed6
Mehdi Outagant (MOU) <mou@odoo.com>
2024-10-01 22:18:45 +0200

    [FIX] web_editor, website: fix highlights in themes customizations
    Starting from `18.0`, text highlight effects are used for snippets
    customizations in themes, which means snippets can be provided with
    highlighted content (in a minimal format) that allows the JS code to
    rebuild the SVGs later when it's needed:
    
    ```
    <span class="o_text_highlight o_text_highlight_[highlightId]"
        style="--text-highlight-width: ...; --text-highlight-color: ...;">
        text content ...
    </span>
    ```
    
    The highlights code provides some tools to adapt them when the content
    is changed: add & remove text, resize… But It doesn't handle the case
    of a dropped snippet with highlights.
    
    The goal of this commit is to be able to build the highlights of a
    snippet once dropped.
    
    Remark: The behavior fixed in this commit wasn't possible until `18.0`
    (with themes snippets using highlights) but we target `17.0` to handle
    every possible customization that uses the effects before themes
    refactoring.
    
    task-4215788
    
    X-original-commit: e4acec8c5507665c28f760ec368707d05de52333

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -3590,16 +3590,7 @@ var SnippetsMenu = Widget.extend({
                     }
 
                     var $target = $toInsert;
-
-                    if ($target[0].classList.contains("o_snippet_drop_in_only")) {
-                        // If it's a "drop in only" snippet, after dropping
-                        // it, we modify it so that it's no longer a
-                        // draggable snippet but rather simple HTML code, as
-                        // if the element had been created with the editor.
-                        $target[0].classList.remove("o_snippet_drop_in_only");
-                        delete $target[0].dataset.snippet;
-                        delete $target[0].dataset.name;
-                    }
+                    this._updateDroppedSnippet($target);
 
                     this.options.wysiwyg.odooEditor.observerUnactive('dragAndDropCreateSnippet');
                     await this._scrollToSnippet($target, this.$scrollable);
@@ -3849,6 +3840,23 @@ var SnippetsMenu = Widget.extend({
      */
     _allowInTranslationMode($snippet) {
         return globalSelector.is($snippet, { onlyTextOptions: true });
+    },
+    /**
+     * Allows to update the snippets to build & adapt dynamic content right
+     * after adding it to the DOM.
+     *
+     * @private
+     */
+    _updateDroppedSnippet($target) {
+        if ($target[0].classList.contains("o_snippet_drop_in_only")) {
+            // If it's a "drop in only" snippet, after dropping
+            // it, we modify it so that it's no longer a
+            // draggable snippet but rather simple HTML code, as
+            // if the element had been created with the editor.
+            $target[0].classList.remove("o_snippet_drop_in_only");
+            delete $target[0].dataset.snippet;
+            delete $target[0].dataset.name;
+        }
     },
 
     //--------------------------------------------------------------------------
