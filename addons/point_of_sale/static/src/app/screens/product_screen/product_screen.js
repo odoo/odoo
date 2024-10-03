1d2cc3b72ee534fe915dd10888becba3cd8a6fd0
aksi-odoo <aksi@odoo.com>
2024-10-03 11:00:32 +0530

    [FIX] point_of_sale/saas~17.4: fix share trusted pos feature
    The feature of shared trusted PoS is not working in saas~17.4
    This is due to fact, the current order is never added to
    the pendingOrder object. so when '_setOrder' is called,
    no orders are returned and  function exits early

--- addons/point_of_sale/static/src/app/screens/product_screen/product_screen.js
+++ addons/point_of_sale/static/src/app/screens/product_screen/product_screen.js
@@ -61,6 +61,7 @@ export class ProductScreen extends Component {
             if (this.pos.config.iface_start_categ_id) {
                 this.pos.setSelectedCategory(this.pos.config.iface_start_categ_id.id);
             }
+            this.pos.addPendingOrder([this.pos.get_order().id]);
 
             // Call `reset` when the `onMounted` callback in `numberBuffer.use` is done.
             // We don't do this in the `mounted` lifecycle method because it is called before
