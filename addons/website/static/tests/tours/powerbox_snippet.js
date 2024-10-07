keyboard-slayer/odoo-eval

[IMP] web-editor,website: remove banner from website
This commit removes four banner-related commands: Banner Info, Banner
Success, Banner Warning, and Banner Danger. These commands are being
replaced by the existing Alert command, which uses the fully
customizable alert snippet. When users input terms like 'Banner', 'Info'
, 'Success', 'Warning', or 'Danger', the Alert command will be displayed
in the Powerbox instead of the removed banner commands.

task-3572344

addons/website/static/tests/tours/powerbox_snippet.js
@@ -1,0 +1,76 @@
+/** @odoo-module **/
+
+import wTourUtils from "@website/js/tours/tour_utils";
+
+wTourUtils.registerWebsitePreviewTour("website_powerbox_snippet", {
+    edition: true,
+    test: true,
+}, () => [
+    wTourUtils.dragNDrop({
+        id: "s_text_block",
+        name: "Text",
+    }), {
+        content: "Check if s_text_block snippet is inserted",
+        trigger: "iframe .s_text_block",
+    }, {
+        content: "Select the last paragraph",
+        trigger: "iframe .s_text_block p:last-child",
+    }, {
+        content: "Show the powerbox",
+        trigger: "iframe .s_text_block p:last-child",
+        run: function(actions) {
+            actions.text(`/`, this.$anchor[0]);
+            const wrapwrapEl = this.$anchor[0].closest("#wrapwrap");
+            wrapwrapEl.dispatchEvent(
+                new InputEvent("input", {
+                    inputType: "insertText",
+                    data: "/",
+                })
+            );
+        },
+    }, {
+        content: "Initially alert snippet should be present in the powerbox",
+        trigger: ".oe-powerbox-wrapper .oe-powerbox-commandWrapper:contains('Alert')",
+        isCheck: true,
+    }, {
+        content: "Change the content to '/table' so that alert snippet should not be present in the powerbox",
+        trigger: "iframe .s_text_block p:last-child",
+        run: function() {
+            const wrapwrapEl = this.$anchor[0].closest("#wrapwrap");
+            this.$anchor[0].textContent = "/table";
+            wrapwrapEl.ownerDocument.dispatchEvent(
+                new KeyboardEvent('keyup', {
+                    key: 'DummyKey',
+                    code: 'KeyDummy',
+                    cancelable: true,
+                })
+            );
+        },
+    }, {
+        content: "Alert snippet should not be present in the powerbox",
+        trigger: ".oe-powerbox-wrapper .oe-powerbox-commandWrapper:not(:contains('Alert'))",
+        isCheck: true,
+    }, {
+        content: "Change the content to '/banner'",
+        trigger: "iframe .s_text_block p:last-child",
+        run: function() {
+            const wrapwrapEl = this.$anchor[0].closest("#wrapwrap");
+            this.$anchor[0].textContent = "/banner";
+            wrapwrapEl.ownerDocument.dispatchEvent(
+                new KeyboardEvent('keyup', {
+                    key: 'DummyKey',
+                    code: 'KeyDummy',
+                    cancelable: true,
+                })
+            );
+        },
+    }, {
+        content: "Click on the alert snippet",
+        trigger: ".oe-powerbox-wrapper .oe-powerbox-commandWrapper:contains('Alert')",
+        run: "click",
+    }, {
+        content: "Check if s_alert snippet is inserted",
+        trigger: "iframe .s_alert",
+        isCheck: true,
+    }
+]);

odoo/odoo

addons/website/static/tests/tours/powerbox_snippet.js
@@ -1,0 +1,80 @@
+/** @odoo-module **/
+
+import wTourUtils from "@website/js/tours/tour_utils";
+
+wTourUtils.registerWebsitePreviewTour("website_powerbox_snippet", {
+    edition: true,
+    test: true,
+}, () => [
+    wTourUtils.dragNDrop({
+        id: "s_text_block",
+        name: "Text",
+    }), {
+        content: "Check if s_text_block snippet is inserted",
+        trigger: "iframe .s_text_block",
+    }, {
+        content: "Select the last paragraph",
+        trigger: "iframe .s_text_block p:last-child",
+    }, {
+        content: "Show the powerbox",
+        trigger: "iframe .s_text_block p:last-child",
+        run: function(actions) {
+            actions.text(`/`, this.$anchor[0]);
+            const wrapwrapEl = this.$anchor[0].closest("#wrapwrap");
+            wrapwrapEl.dispatchEvent(
+                new InputEvent("input", {
+                    inputType: "insertText",
+                    data: "/",
+                })
+            );
+        },
+    }, {
+        content: "Initially alert snippet should be present in the powerbox",
+        trigger: ".oe-powerbox-wrapper .oe-powerbox-commandName:contains('Alert')",
+        isCheck: true,
+    }, {
+        content: "Change the content to '/table' so that alert snippet should not be present in the powerbox",
+        trigger: "iframe .s_text_block p:last-child",
+        run: function() {
+            const wrapwrapEl = this.$anchor[0].closest("#wrapwrap");
+            this.$anchor[0].textContent = "/table";
+            wrapwrapEl.ownerDocument.dispatchEvent(
+                new KeyboardEvent('keyup', {
+                    key: 'DummyKey',
+                    code: 'KeyDummy',
+                    cancelable: true,
+                })
+            );
+        },
+    }, {
+        content: "Alert snippet should not be present in the powerbox",
+        trigger: ".oe-powerbox-wrapper .oe-powerbox-commandName:not(:contains('Alert'))",
+        isCheck: true,
+    }, {
+        content: "Change the content to '/banner'",
+        trigger: "iframe .s_text_block p:last-child",
+        run: function() {
+            const wrapwrapEl = this.$anchor[0].closest("#wrapwrap");
+            this.$anchor[0].textContent = "/banner";
+            wrapwrapEl.ownerDocument.dispatchEvent(
+                new KeyboardEvent('keyup', {
+                    key: 'DummyKey',
+                    code: 'KeyDummy',
+                    cancelable: true,
+                })
+            );
+        },
+    }, {
+        content: "Alert snippet should be present in the powerbox",
+        trigger: ".oe-powerbox-wrapper .oe-powerbox-commandName:contains('Alert')",
+        isCheck: true,
+    }, {
+        content: "Click on the alert snippet",
+        trigger: ".oe-powerbox-wrapper .oe-powerbox-commandName:contains('Alert')",
+        run: "click",
+    }, {
+        content: "Check if s_alert snippet is inserted",
+        trigger: "iframe .s_alert",
+        isCheck: true,
+    }
+]);