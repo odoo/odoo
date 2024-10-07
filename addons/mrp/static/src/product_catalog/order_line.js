3a79812152a8435a29455a8db1126e123656e172
Mohammad Abdulmoneim <abdu@odoo.com>
2024-10-07 09:17:31 +0000

    [FIX] product: hide price from catalog in case of mrp or repair orders
    This commit hides the unit price of products in the catalog, in case
    that the catalog was opened from a manufacturing or a repair order. This
    is because the catalog in this case is used to add a component so the
    price information is irrelevant.
    
    closes odoo/odoo#183668
    
    X-original-commit: 3e9ce299e64d50bb02eafc1c9fae274c5aaf97cc
    Signed-off-by: Valentin Chevalier <vcr@odoo.com>
    Signed-off-by: Mohammad Abdulmoneim (abdu) <abdu@odoo.com>

---
+++ addons/mrp/static/src/product_catalog/order_line.js
@@ -0,0 +1,8 @@
+import { ProductCatalogOrderLine } from "@product/product_catalog/order_line/order_line";
+import { patch } from "@web/core/utils/patch";
+
+patch(ProductCatalogOrderLine.prototype, {
+    get showPrice() {
+        return super.showPrice && this.env.orderResModel !== "mrp.production";
+    }
+});
