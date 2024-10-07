d66bf1e5bcdda4da37c5f6b736543d14cad96fab
Pedram (pebr) <pebr@odoo.com>
2024-10-07 12:43:22 +0000

    [FIX] point_of_sale: display product template image instead of variant
    Before this commit, the product screen displayed an image of one of the
    product's variants, contrary to the expectation of showing the product
    template image. This commit corrects the behavior to ensure the product
    template image is displayed.
    
    opw-4234351
    
    closes odoo/odoo#182830
    
    X-original-commit: 46597b80a4443fe3cefd0d508de700285556dbd7
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/point_of_sale/static/src/app/screens/product_screen/product_screen.js
+++ addons/point_of_sale/static/src/app/screens/product_screen/product_screen.js
@@ -296,7 +296,7 @@ export class ProductScreen extends Component {
     }
 
     getProductImage(product) {
-        return product.getImageUrl();
+        return product.getTemplateImageUrl();
     }
 
     get searchWord() {
