cd957b57ebdcca32c0cbd3ead9e14ab695940e93
Jinane Maksoud <maji@odoo.com>
2024-10-07 11:07:34 +0000

    [FIX] l10n_*: try loading the coa using parent company first
    In migration and init scripts, when loading the chart of
    accounts or parts of it, we should always start with the
    parrent companies to avoid creating duplicate chart records
    
    X-original-commit: adaaa0a9aef29180f2dcfcaa947a8fdca87b1201

--- addons/l10n_se/migrations/1.1/end-migrate.py
+++ addons/l10n_se/migrations/1.1/end-migrate.py
@@ -3,5 +3,5 @@ from odoo import api, SUPERUSER_ID
 
 def migrate(cr, version):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    for company in env['res.company'].search([('chart_template', '=', 'se')]):
+    for company in env['res.company'].search([('chart_template', '=', 'se')], order="parent_path"):
         env['account.chart.template'].try_loading('se', company)
