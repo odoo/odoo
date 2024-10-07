d7918a7c5e296e454d425a8260824cf3a1bf5f31
Pierre Pulinckx (PIPU) <pipu@odoo.com>
2024-10-07 09:04:11 +0000

    [FIX] website_sale: website_sale_cart_notification tour
    In this commit, we remove unuseful steps and unuseful click actions.
    We take advantages of this commit to decrease checkDelay to make the
    tour faster.
    
    closes odoo/odoo#182631
    
    X-original-commit: 004c91be46a4a454bc11620854c4a1fda0dd5a0f
    Signed-off-by: Bastien Pierre (ipb) <ipb@odoo.com>
    Signed-off-by: Pierre Pulinckx (pipu) <pipu@odoo.com>

--- addons/website_sale/static/tests/tours/website_sale_cart_notification.js
+++ addons/website_sale/static/tests/tours/website_sale_cart_notification.js
@@ -3,42 +3,39 @@
 import { registry } from "@web/core/registry";
 import * as tourUtils from "@website_sale/js/tours/tour_utils";
 
-registry.category("web_tour.tours").add('website_sale_cart_notification', {
+registry.category("web_tour.tours").add("website_sale_cart_notification", {
     test: true,
-    url: '/shop',
+    url: "/shop",
+    checkDelay: 100,
     steps: () => [
-        ...tourUtils.addToCart({productName: 'website_sale_cart_notification_product_1'}),
+        ...tourUtils.addToCart({ productName: "website_sale_cart_notification_product_1" }),
         {
             content: "check that 1 website_sale_cart_notification_product_1 was added",
             trigger: '.toast-body span:contains("1 x website_sale_cart_notification_product_1")',
-            run: "click",
         },
         {
             content: "check the price of 1 website_sale_cart_notification_product_1",
             trigger: '.toast-body div:contains("$ 1,000.00")',
-            run: "click",
         },
         {
             content: "close the notification",
-            trigger: '.toast-header button.btn-close',
+            trigger: ".toast-header button.btn-close",
             run: "click",
         },
         {
             content: "check that the notification is closed",
-            trigger: 'div.position-absolute.w-100.h-100.top-0.pe-none',
-            run: () => {
-                if (
-                    document.querySelectorAll("div.position-absolute.w-100.h-100.top-0.pe-none div")
-                        .length !== 1
-                ) {
-                    console.error('The cart notification is not closed!');
+            trigger: "div.position-absolute.w-100.h-100.top-0.pe-none",
+            run() {
+                if (this.anchor.querySelectorAll("div").length !== 1) {
+                    console.error("The cart notification is not closed!");
                 }
             },
         },
-        ...tourUtils.searchProduct('website_sale_cart_notification_product_2'),
+        ...tourUtils.searchProduct("website_sale_cart_notification_product_2"),
         {
             content: "select website_sale_cart_notification_product_2",
-            trigger: '.oe_product_cart:first a:contains("website_sale_cart_notification_product_2")',
+            trigger:
+                '.oe_product_cart:first a:contains("website_sale_cart_notification_product_2")',
             run: "click",
         },
         {
@@ -50,9 +47,6 @@ registry.category("web_tour.tours").add('website_sale_cart_notification', {
             run: "edit 3",
         },
         {
-            trigger: "#product_detail",
-        },
-        {
             content: "click on add to cart",
             trigger: '#product_detail form[action^="/shop/cart/update"] #add_to_cart',
             run: "click",
@@ -60,17 +54,14 @@ registry.category("web_tour.tours").add('website_sale_cart_notification', {
         {
             content: "check that 3 website_sale_cart_notification_product_2 was added",
             trigger: '.toast-body span:contains("3 x website_sale_cart_notification_product_2")',
-            run: "click",
         },
         {
             content: "check that the novariants/custom attributes are displayed.",
             trigger: '.toast-body span.text-muted.small:contains("Size: S")',
-            run: "click",
         },
         {
             content: "check the price of 1 website_sale_cart_notification_product_2",
             trigger: '.toast-body div:contains("$ 15,000.00")',
-            run: "click",
         },
         {
             content: "Go To Cart",
@@ -78,12 +69,12 @@ registry.category("web_tour.tours").add('website_sale_cart_notification', {
             run: "click",
         },
         tourUtils.assertCartContains({
-            productName: 'website_sale_cart_notification_product_1',
+            productName: "website_sale_cart_notification_product_1",
             backend: false,
         }),
         tourUtils.assertCartContains({
-            productName: 'website_sale_cart_notification_product_2',
+            productName: "website_sale_cart_notification_product_2",
             backend: false,
         }),
-    ]
+    ],
 });
