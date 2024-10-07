849bef351dcefc9cf4a4b99cf948708236093305
Ethan Vincent <etvi@odoo.com>
2024-10-07 12:12:30 +0000

    [FIX] stock: prevent needless write on picking moves
    **Current behavior:**
    Creating a new picking, adding some moves, then adding/changing
    the partner_id field all without saving will remove the added
    moves.
    
    **Expected behavior:**
    Changing the partner shouldn't affect the moves.
    
    **Steps to reproduce:**
    1. Create a new picking, add some moves, then add a partner (all
    without saving)
    
    2. Observe the moves disappear after adding the partner
    
    **Cause of the issue:**
    In the onchange handling partner_id changes, we overwrite the
    new virtual moves with values and mark them as modified. This
    causes them to eventually get invalidated and they aren't saved
    once the update (overwrite) resolves.
    
    **Fix:**
    Do not write on a move's 'picking_type_id' field (which is the
    field that is triggering the eventual invalidation) unless it
    actually necessitates a write.
    
    opw-4120108
    
    closes odoo/odoo#184702
    
    X-original-commit: f5b41d2e69b6544390d26e482616f47d45f1a2ce
    Signed-off-by: Vincent Ethan <etvi@odoo.com>

--- addons/stock/models/stock_picking.py
+++ addons/stock/models/stock_picking.py
@@ -1026,10 +1026,11 @@ class StockPicking(models.Model):
     def _onchange_picking_type(self):
         if self.picking_type_id and self.state == 'draft':
             self = self.with_company(self.company_id)
-            (self.move_ids | self.move_ids_without_package).update({
-                "picking_type_id": self.picking_type_id,  # The compute store doesn't work in case of One2many inverse (move_ids_without_package)
-                "company_id": self.company_id,
-            })
+            # The compute store doesn't work in case of One2many inverse (move_ids_without_package)
+            (self.move_ids | self.move_ids_without_package).filtered(
+                lambda m: m.picking_type_id != self.picking_type_id
+            ).picking_type_id = self.picking_type_id
+            (self.move_ids | self.move_ids_without_package).company_id = self.company_id
             for move in (self.move_ids | self.move_ids_without_package):
                 if not move.product_id:
                     continue
