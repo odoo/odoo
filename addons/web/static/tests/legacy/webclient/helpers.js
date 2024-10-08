ce16292d2f960d14b4341faeaa21a0bf9e43c904
Mathieu Duckerts-Antoine <dam@odoo.com>
2024-10-08 15:59:58 +0200

    [IMP] web: remove target inline
    The model IrActionsActWindow has a target selection field. Its value can be
    "current", "new", "main", "fullscreen" or "inline". The latter value is useless.
    It is only used on settings actions. Historically, it forced the form view mode
    to "edit", which is now always the case. It also ensures that we don't load
    dynamic filters and we don't have a search view, since it is set on actions that
    only have a form view (no multi record views).
    
    In this commit, we remove the value inline from the target selection and remove
    that value in every actions that used to have that target. This means that the
    default "current" will be used. For the model "res.config.settings" on which
    all inline actions were defined, we hard code that loadActionMenus should be
    false, since for the settigs actions we do not have the cog menu anyway.
    
    Task ID: 3245566
    
    closes odoo/odoo#182800
    
    Related: odoo/enterprise#71525
    Related: odoo/upgrade#6593
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/tests/legacy/webclient/helpers.js
+++ addons/web/static/tests/legacy/webclient/helpers.js
@@ -204,7 +204,6 @@ export function getActionManagerServerData() {
             name: "Partner",
             res_id: 2,
             res_model: "partner",
-            target: "inline",
             type: "ir.actions.act_window",
             views: [[false, "form"]],
         },
