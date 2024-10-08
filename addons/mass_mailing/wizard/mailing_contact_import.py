ef60c0227a8b40c22139efd39ed249021d0527e3
Jorge Pinna Puissant <jpp@odoo.com>
2024-10-08 10:21:05 +0000

    [FIX] base_import: active_model as query parameter
    - In any APP (We would use CRM for the example);
    - On a multi-record view (Kanban, List, or other);
    - Click the action menu;
    - Click on “Import records” dropdown;
    - Reload the view (either reload the browser, or activate the debug, or
       change to dark mode on the user menu).
    
    Before this commit, an exception was raised, and the default
    multi-record view was loaded. This occurs because, the client action
    base import required a model (found in the context) that was lost when
    reloading.
    
    Now, the model is put in the query string of the URL (as active_model),
    in that way, when reloading, the client action base import will have the
    needed model. Note that, this is also the behavior of the stock
    TraceabilityReport client action [1].
    
    opw-3959254
    
    [1] : https://github.com/odoo/odoo/commit/8b3deab679bfee844ccd84c7f4f6f831921365d3
    
    closes odoo/odoo#182839
    
    X-original-commit: 9f49fa21fa1efb7d35f0624aa6e567c8c547cfe7
    Signed-off-by: Jorge Pinna Puissant (jpp) <jpp@odoo.com>

--- addons/mass_mailing/wizard/mailing_contact_import.py
+++ addons/mass_mailing/wizard/mailing_contact_import.py
@@ -127,6 +127,6 @@ class MailingContactImport(models.TransientModel):
             'name': _('Import Mailing Contacts'),
             'params': {
                 'context': self.env.context,
-                'model': 'mailing.contact',
+                'active_model': 'mailing.contact',
             }
         }
