bd9c930b2fe5a7dc90bb9134092c49c75bb84a5f
Mahdi Cheikh Rouhou (macr) <macr@odoo.com>
2024-10-08 09:00:24 +0000

    [FIX] web_editor: reset tranformatiom on click twice
    Issue:
    ======
    We can't click twice on the transform button.
    
    Steps to reproduce the issue:
    =============================
    - Create a todo
    - Add an image
    - Select it
    - Click on the transform button in the toolbar
    - The toolbar disappears so we can't click another time
    
    Origin of the issue:
    ====================
    The issue was introduced in [1]. Removing the selection will
    trigger a selectionChange which itself triggers updateToolbar but now we
    don't have any selection so we hide the toolbar.
    
    Solution:
    =========
    Revert the old fix and instead we ignore any `keydown` event when the
    transform container is in the dom (which means we are currently
    transforming an image).
    
    task-4235140
    
    [1]: https://github.com/odoo/odoo/commit/463c248e291a53c80365ae606c7cf39e13cd290b
    
    X-original-commit: 73b6227182bb142c63e36b5fbcfe97df5da772a7

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -1950,8 +1950,6 @@ export class Wysiwyg extends Component {
         });
         $toolbar.find('#image-crop').click(() => this._showImageCrop());
         $toolbar.find('#image-transform').click(e => {
-            const sel = document.getSelection();
-            sel.removeAllRanges();
             if (!this.lastMediaClicked) {
                 return;
             }
@@ -2177,6 +2175,9 @@ export class Wysiwyg extends Component {
         if (!selection) return;
         const anchorNode = selection.anchorNode;
         if (isProtected(anchorNode)) {
+            return;
+        }
+        if (this.odooEditor.document.querySelector(".transfo-container")) {
             return;
         }
 
