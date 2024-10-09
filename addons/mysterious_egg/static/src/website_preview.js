1728c630ff0c0f110eeca020020ffd3d2bef3901
fdardenne <dafl@odoo.com>
2024-10-09 13:18:52 +0200

    [IMP] wip action

---
+++ addons/mysterious_egg/static/src/website_preview.js
@@ -0,0 +1,29 @@
+import { Component, onWillStart } from "@odoo/owl";
+import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
+
+export const unslugHtmlDataObject = (repr) => {
+    const match = repr && repr.match(/(.+)\((\d+),(.*)\)/);
+    if (!match) {
+        return null;
+    }
+    return {
+        model: match[1],
+        id: match[2] | 0,
+    };
+};
+
+class WebsitePreview extends Component {
+    static template = "mysterious_egg.WebsitePreview";
+
+    setup() {
+        this.orm = useService("orm");
+        onWillStart(async () => {
+            const slugCurrentWebsite = await this.orm.call("website", "get_current_website");
+            this.backendWebsiteId = unslugHtmlDataObject(slugCurrentWebsite).id;
+            this.initialUrl = `/website/force/${encodeURIComponent(this.backendWebsiteId)}`;
+        });
+    }
+}
+
+registry.category("actions").add("egg_website_preview", WebsitePreview);
