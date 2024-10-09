02208c9f32819bfb87804e61a800f9ee8baa947a
Jeremy Kersten <jke@odoo.com>
2024-10-09 14:40:23 +0000

    [FIX] http_routing: don't consider path prefixed by _ as a lang during match
    Prior to this commit, when a URL started with an underscore (_) and didn’t
    match any existing controller, Odoo attempted to resolve the first part of the
    URL as a language code.
    
    If the first segment started with an underscore and was not a valid language
    code, Odoo would split at the underscore and attempt to match the empty string
    (''), causing it to default to the first available language.
    
    For example, accessing https://www.odoo.com/__NULL__ on odoo.com would result
    in the language being incorrectly set to Arabic, and a redirection to
    https://www.odoo.com/ar.
    
    X-original-commit: b625488c2d8fe0e9fbb1b8e1c7de5df26d91674e

--- addons/http_routing/models/ir_http.py
+++ addons/http_routing/models/ir_http.py
@@ -399,6 +399,8 @@ class IrHttp(models.AbstractModel):
             return lang_code
 
         short = lang_code.partition('_')[0]
+        if not short:
+            return None
         return next((code for code in frontend_langs if code.startswith(short)), None)
 
     @classmethod
