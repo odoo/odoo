064b4e4433bfd48035a57a01c0ed1d4b427cb76b
fdardenne <dafl@odoo.com>
2024-10-09 17:07:08 +0200

    [IMP] Instantiate editor in website

--- addons/mysterious_egg/static/src/website_preview.js
+++ addons/mysterious_egg/static/src/website_preview.js
@@ -1,7 +1,9 @@
-import { Component, onWillStart } from "@odoo/owl";
+import { Component, onMounted, onWillStart, useRef, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 import { BuilderMenu } from "./builder_menu";
+import { Editor } from "@html_editor/editor";
+import { MAIN_PLUGINS } from "@html_editor/plugin_sets";
 
 export const unslugHtmlDataObject = (repr) => {
     const match = repr && repr.match(/(.+)\((\d+),(.*)\)/);
@@ -20,11 +22,24 @@ class WebsitePreview extends Component {
 
     setup() {
         this.orm = useService("orm");
+        this.websiteContent = useRef("iframe");
         onWillStart(async () => {
             const slugCurrentWebsite = await this.orm.call("website", "get_current_website");
             this.backendWebsiteId = unslugHtmlDataObject(slugCurrentWebsite).id;
             this.initialUrl = `/website/force/${encodeURIComponent(this.backendWebsiteId)}`;
         });
+
+        this.editor = new Editor(
+            {
+                Plugins: [...MAIN_PLUGINS],
+            },
+            this.env.services
+        );
+    }
+
+    onWebsiteLoaded() {
+        debugger;
+        this.editor.attachTo(this.websiteContent.el.contentDocument.body);
     }
 }
 
