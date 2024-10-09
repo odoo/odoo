a3f0a47944a1b890f6dd03041a9cfc6b75c73a3d
bary <bary@odoo.com>
2024-10-09 12:19:31 +0000

    [FIX] sale_timesheet: filter task SO without commercial partner id
    Steps to reproduce:
    - Project > Pick any task > Debug Mode
    - Studio > View tab > Tick 'Show invisible elements'
    - Scroll to 'Sales Order' and click it
    - Untick 'Invisible' then tick and untick readonly
    - Close > you should have a Sales Order field on the task
    - Click to change it's value
    
    An error occurs because the 'commercial_partner_id' field was removed
    from task in 17.0 in 400a93e31f871271b7e7e7812f56e8e4963ebcc7. Because
    of this the domain which filters 'Sales Order' (which contains this
    field) cannot be evaluated.
    
    Since the field is still available from the sale_order model, we can
    simply invert the child_of relation:
    from sale_order.partner_id child_of task.commercial_partner_id
    to sale_order.commercial_partner_id parent_of task.partner_id
    Which should serve essentially the same purpose.
    
    opw-4199947
    
    closes odoo/odoo#183620
    
    X-original-commit: f6853e29a87972794f2208e900b6688b3b43b274
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>
    Signed-off-by: Yoann Baron (bary) <bary@odoo.com>

--- addons/sale_timesheet/models/project_task.py
+++ addons/sale_timesheet/models/project_task.py
@@ -18,7 +18,7 @@ class ProjectTask(models.Model):
                 return related_project.sale_line_employee_ids.sale_line_id.order_partner_id[:1]
         return res
 
-    sale_order_id = fields.Many2one(domain="['|', '|', ('partner_id', '=', partner_id), ('partner_id', 'child_of', commercial_partner_id), ('partner_id', 'parent_of', partner_id)]")
+    sale_order_id = fields.Many2one(domain="['|', '|', ('partner_id', '=', partner_id), ('partner_id.commercial_partner_id.id', 'parent_of', partner_id), ('partner_id', 'parent_of', partner_id)]")
     pricing_type = fields.Selection(related="project_id.pricing_type")
     is_project_map_empty = fields.Boolean("Is Project map empty", compute='_compute_is_project_map_empty')
     has_multi_sol = fields.Boolean(compute='_compute_has_multi_sol', compute_sudo=True)
