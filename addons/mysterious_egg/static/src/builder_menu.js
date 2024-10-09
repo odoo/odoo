9a792df4f84968f51662c48e97b116f2b3ea6268
FrancoisGe <fge@odoo.com>
2024-10-09 16:29:04 +0200

    add notebook

--- addons/mysterious_egg/static/src/builder_menu.js
+++ addons/mysterious_egg/static/src/builder_menu.js
@@ -1,16 +1,15 @@
 import { Component, onWillStart, useState } from "@odoo/owl";
 import { loadBundle } from "@web/core/assets";
+import { Notebook } from "@web/core/notebook/notebook";
 import { blockTab } from "./builder_tab/block_tab";
 import { customizeTab } from "./builder_tab/customize_tab";
 
 export class BuilderMenu extends Component {
     static template = "mysterious_egg.BuilderMenu";
+    static components = { Notebook };
 
     setup() {
-        this.state = useState({
-            currentTab: blockTab,
-        });
-        this.tabs = [blockTab, customizeTab];
+        this.pages = [blockTab, customizeTab];
 
         // TODO we need css
         onWillStart(() => loadBundle("web_editor.wysiwyg_iframe_editor_assets"));
