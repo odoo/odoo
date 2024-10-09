f2d515cb2384eaff02505533dfb608427a654b5e
anusrinps96 <aprakhasam@nps100.com>
2024-10-09 11:39:28 +0200

    [IMP]: Enhanced numpad decimal usage
       Migration to 16.0, missed to include numpad decimal to be
    used for time fields in order to differentiate hours from minutes.
       So, included decimal usage to be used for float_time fields.

--- addons/web/static/src/views/fields/float_time/float_time_field.js
+++ addons/web/static/src/views/fields/float_time/float_time_field.js
@@ -8,7 +8,7 @@ import { useInputField } from "../input_field_hook";
 import { standardFieldProps } from "../standard_field_props";
 import { useNumpadDecimal } from "../numpad_decimal_hook";
 
-import { Component } from "@odoo/owl";
+import { Component,  onMounted, onWillUnmount } from "@odoo/owl";
 
 export class FloatTimeField extends Component {
     setup() {
@@ -18,6 +18,34 @@ export class FloatTimeField extends Component {
             parse: (v) => parseFloatTime(v),
         });
         useNumpadDecimal();
+        onMounted(this.onMounted);
+        onWillUnmount(this.onWillUnmount);
+    }
+
+    // checks for "." in keyboard event and
+    // replaces with ":" in float_time fields
+    async onKeydownListener(ev) {
+        var decimalPoint = ":";
+
+        if (!([".", ","].includes(ev.key))) {
+            return;
+        }
+        ev.preventDefault();
+        ev.originalTarget.setRangeText(
+            decimalPoint,
+            ev.originalTarget.selectionStart,
+            ev.originalTarget.selectionEnd,
+            "end"
+        );
+    }
+
+    onMounted() {
+        this.keydownListenerCallback = this.onKeydownListener.bind(this);
+        this.__owl__.bdom.parentEl.addEventListener('keydown', this.keydownListenerCallback);
+    }
+
+    onWillUnmount() {
+        this.__owl__.bdom.parentEl.removeEventListener('keydown', this.keydownListenerCallback);
     }
 
     get formattedValue() {
