dc525ec88127fb4d9421fd3e1110f4831a74e11d
fdardenne <dafl@odoo.com>
2024-10-09 11:28:04 +0200

    [ADD] create mysterious_egg module

---
+++ addons/mysterious_egg/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "mysterious egg",
+
+    'summary': "Oh? An egg is hatching!",
+
+    'description': """
+    Oh? An egg is hatching!
+    """,
+
+    'author': "Odoo",
+    'website': "https://www.odoo.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'auto_install': True,
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'html_editor'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+}
