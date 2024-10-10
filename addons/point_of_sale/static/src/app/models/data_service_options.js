c63a2e84d215ee3b0288d28cb284c3eca14e5eef
Pedram (pebr) <pebr@odoo.com>
2024-10-10 14:05:00 +0000

    [PERF] point_of_sale: optimize product addition to order
    Before this commit, loading 20,000 products into the PoS and adding a
    product to an order was slow due to multiple writes to the IndexedDb.
    This commit optimizes the process by not saving ongoing order's
    products in the IndexedDb, as they can be loaded as needed. Missing
    products are loaded when loading the orderlines.
    
    opw-4200540
    
    closes odoo/odoo#183404
    
    X-original-commit: 8a6f43fa3ed926e24484444c6776b55cb0f76515
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/point_of_sale/static/src/app/models/data_service_options.js
+++ addons/point_of_sale/static/src/app/models/data_service_options.js
@@ -23,14 +23,6 @@ export class DataServiceOptions {
                     record.pos_order_line_id?.order_id?.finalized &&
                     typeof record.pos_order_line_id.order_id.id === "number",
             },
-            "product.product": {
-                key: "id",
-                condition: (record) => {
-                    return record["<-pos.order.line.product_id"].find(
-                        (l) => !(l.order_id?.finalized && typeof l.order_id.id === "number")
-                    );
-                },
-            },
             "product.attribute.custom.value": {
                 key: "id",
                 condition: (record) => {
