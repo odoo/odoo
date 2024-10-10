99151be717ff03db16576917584144fb77ad1aea
Valeriya(vchu) <vchu@odoo.com>
2024-10-10 16:59:55 +0200

    fixup! [FIX] website_sale_delivery: update carrier_id of SO on shop/payment

--- addons/website_sale_delivery/tests/test_website_sale_controller.py
+++ addons/website_sale_delivery/tests/test_website_sale_controller.py
@@ -12,7 +12,6 @@ from odoo.addons.website.tools import MockRequest
 class TestWebsiteSaleController(TestWebsiteSaleCheckoutAddress):
     def setUp(self):
         super().setUp()
-        self.Controller = WebsiteSaleDelivery()
 
     def test_carrier_update_on_shop_payment(self):
         """ Test that a correct carrier is set on SO on shop/payment when partner_shipping_id is
@@ -21,72 +20,53 @@ class TestWebsiteSaleController(TestWebsiteSaleCheckoutAddress):
         # Unpublish all carriers.
         self.env['delivery.carrier'].search([]).website_published = False
 
-        country_us = self.env.ref('base.us')
+        country_be = self.env.ref('base.be')
         country_fr = self.env.ref('base.fr')
 
         # Create a delivery product.
-        self.product_delivery_poste = self.env['product.product'].create(
-            {
-                'name': 'The Poste',
-                'type': 'service',
-                'categ_id': self.env.ref('delivery.product_category_deliveries').id,
-                'sale_ok': False,
-                'purchase_ok': False,
-            }
-        )
+        self.product_delivery_poste = self.env['product.product'].create({
+            'name': 'The Poste',
+            'type': 'service',
+            'categ_id': self.env.ref('delivery.product_category_deliveries').id,
+            'sale_ok': False,
+            'purchase_ok': False,
+        })
 
-        # Create a carrier which ships only to US.
-        carrier_us = self.env['delivery.carrier'].create(
-            [{
-                'name': 'Fixed 10',
-                'product_id': self.product_delivery_poste.id,
-                'country_ids': [Command.set([country_us.id])],
-                'fixed_price': 10.0,
-                'website_published': True,
-            }]
-        )
+        # Create a carrier which ships only to Belgium.
+        carrier_be = self.env['delivery.carrier'].create([{
+            'name': 'Fixed 10',
+            'product_id': self.product_delivery_poste.id,
+            'country_ids': [Command.set([country_be.id])],
+            'fixed_price': 10.0,
+            'website_published': True,
+        }])
 
         # Create a carrier which ships only to France.
-        carrier_fr = self.env['delivery.carrier'].create(
-            [{
-                'name': 'Fixed 30',
-                'product_id': self.product_delivery_poste.id,
-                'country_ids': [Command.set([country_fr.id])],
-                'fixed_price': 30.0,
-                'website_published': True,
-            }]
-        )
+        carrier_fr = self.env['delivery.carrier'].create([{
+            'name': 'Fixed 30',
+            'product_id': self.product_delivery_poste.id,
+            'country_ids': [Command.set([country_fr.id])],
+            'fixed_price': 30.0,
+            'website_published': True,
+        }])
+
+        # Create partner_shipping_id with BE country.
         p = self.env.user.partner_id
         so = self._create_so(p.id)
-        # Create partner_shipping_id with US country.
-        so.partner_shipping_id = self.env['res.partner'].create(
-            {
-                'name': 'a res.partner address',
-                'email': 'email@email.email',
-                'street': 'ooo',
-                'city': 'ooo',
-                'zip': '1200',
-                'country_id': country_us.id,
-                'parent_id': p.id,
-                'type': 'delivery',
-            }
-        )
-
+        so.partner_shipping_id = self.env['res.partner'].create({
+            'name': 'a res.partner address',
+            'email': 'email@email.email',
+            'street': 'ooo',
+            'city': 'ooo',
+            'zip': '1200',
+            'country_id': country_be.id,
+            'parent_id': p.id,
+            'type': 'delivery',
+        })
+        so.set_delivery_line(carrier_be, carrier_be.fixed_price)
         with MockRequest(self.env, website=self.website, sale_order_id=so.id):
-            self.WebsiteSaleController.confirm_order()
-            self.Controller.shop_payment()
-            self.assertEqual(
-                so.carrier_id,
-                carrier_us,
-                msg="The carrier that ships to US should be set."
-            )
-            self.assertEqual(so.amount_delivery, carrier_us.fixed_price)
             # Change the country of the partner_shipping_id.
             so.partner_shipping_id.country_id = country_fr
-            self.Controller.shop_payment()
-            self.assertEqual(
-                so.carrier_id,
-                carrier_fr,
-                msg="The carrier that ships to France should be set according to shipping address."
-            )
+            WebsiteSaleDelivery().shop_payment()
+            self.assertEqual(so.carrier_id, carrier_fr)
             self.assertEqual(so.amount_delivery, carrier_fr.fixed_price)
