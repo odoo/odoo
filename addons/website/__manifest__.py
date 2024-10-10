9e00c836dc9e5df285aa1b42a60bb170994f0a2b
Jinjiu Liu <jili@odoo.com>
2024-10-10 12:01:20 +0000

    [FIX] website, html_editor: restore fuzzy page search for linkpopover url input
    Steps to reproduce:
    1. install website, add a link into an editing area in Todo app
    2. at the url input, we don't have the internal link suggestion (fuzzy search)
    when typing
    
    The PR target to 18.0 because fuzzy page search is considered as a basic
    function when website is installed. It's a functional fix for the html_editor
    
    In this PR, we've updated the linkpopover component of the html_editor
    to add a fuzzy page search feature to the URL input. Before 18.0, we
    created another OWL app to load the page anchors. Here we do it by using
    autocomplete component, users can now see and select from a dropdown of
    relevant Odoo page anchors as they type in the URL input field.
    
    To achieve this, we modified the popover template to include an
    autocomplete field specifically designed for the link popover. In the
    AutoCompleteInLinkpopover component, two new props were introduced:
    
    1. inputClass: Allows customization of the input field's styling.
    2. updateValue: Enables updating of the link popover's URL state when an
    option is selected from the autocomplete dropdown.
    
    We also remove the redundant linkpreview reload when clicking on apply,
    cause the loadAsyncLinkPreview is already called at onMounted.
    
    A unit test using hoot is added for testing the fuzzy page search in the
    linkpopover.
    
    task-4222657
    
    closes odoo/odoo#213040
    
    X-original-commit: 553a9cabe50164ba3eb78bae7359feb952570bfa
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>
    Signed-off-by: Jinjiu Liu (jili) <jili@odoo.com>

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -11,6 +11,7 @@
         'digest',
         'web',
         'web_editor',
+        'html_editor',
         'http_routing',
         'portal',
         'social_media',
@@ -284,6 +285,9 @@
         'web.qunit_suite_tests': [
             'website/static/tests/redirect_field_tests.js',
         ],
+        'web.assets_unit_tests': [
+            'website/static/tests/website_html_editor.test.js',
+        ],
         'web.tests_assets': [
             'website/static/tests/website_service_mock.js',
         ],
@@ -295,6 +299,10 @@
             'website/static/src/xml/web_editor.xml',
             'website/static/src/js/editor/add_snippet_dialog.js',
             'website/static/src/js/editor/widget_link.js',
+        ],
+        'html_editor.assets_link_popover': [
+            'website/static/src/js/editor/html_editor.js',
+            'website/static/src/xml/html_editor.xml',
         ],
         'website.assets_wysiwyg': [
             ('include', 'web._assets_helpers'),
