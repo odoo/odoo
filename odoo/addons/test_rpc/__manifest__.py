cc131c9c8ef7bfd9bbab7574903fde78f4af572f
Xavier Morel <xmo@odoo.com>
2024-10-10 12:01:29 +0200

    [FIX] test_*: dependencies
    test_http should maybe not need to depend on rpc?
    
    Part-of: odoo/odoo#182971
    Signed-off-by: Raphael Collet <rco@odoo.com>

--- odoo/addons/test_rpc/__manifest__.py
+++ odoo/addons/test_rpc/__manifest__.py
@@ -4,7 +4,7 @@
     "version": "1.0",
     "category": "Hidden/Tests",
     "description": """A module to test the RPC requests.""",
-    "depends": ["base", "web"],
+    "depends": ["web", "rpc"],
     "installable": True,
     "data": ["ir.model.access.csv"],
     'license': 'LGPL-3',
