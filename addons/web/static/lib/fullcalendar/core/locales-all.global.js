f63faa9083da9b678fbf53c0f81f4d96cb172c02
Mattis Megevand <mmeg@odoo.com>
2024-10-10 12:11:45 +0000

    [FIX] web: update fullcalendar from v6.1.10 to v6.1.11
    Steps to reproduce:
    - Install Timesheet
    - Delete all tasks
    - Run server actions to create tasks with the following scripts https://gist.github.com/mattismegevand/5db26ee9d1e7756e7fe4600897244c1e
    - Go to Project -> Tasks -> All tasks
    - Switch to calendar view and go to june 2024
    
    Issues:
    A traceback appears, the cause is an infinite recursion caused by the
    computation of the intersection. Since two events are next to each other
    their intersection is null but fullcalendar try to compute it anyway
    which leads to the intersection span being null which causes a
    traceback.
    
    I suspect that the fix is https://github.com/fullcalendar/fullcalendar/commit/362a37345a4b7cb65d16f345e8555f727f1c8450
    However for good measure and to fix preemptively other bug from
    fullcalendar an update seems like the best decision.
    
    opw-3997810
    
    closes odoo/odoo#184002
    
    X-original-commit: ff2441e426bcd2dd68351ecc5df2630471d60726
    Signed-off-by: Romeo Fragomeli (rfr) <rfr@odoo.com>
    Signed-off-by: Mattis Megevand (mmeg) <mmeg@odoo.com>

--- addons/web/static/lib/fullcalendar/core/locales-all.global.js
+++ addons/web/static/lib/fullcalendar/core/locales-all.global.js
@@ -1,5 +1,5 @@
 /*!
-FullCalendar Core v6.1.10
+FullCalendar Core v6.1.11
 Docs & License: https://fullcalendar.io
 (c) 2023 Adam Shaw
 */
--- addons/web/static/lib/fullcalendar/list/index.global.js
+++ addons/web/static/lib/fullcalendar/list/index.global.js
@@ -1,5 +1,5 @@
 /*!
-FullCalendar List View Plugin v6.1.10
+FullCalendar List View Plugin v6.1.11
 Docs & License: https://fullcalendar.io/docs/list-view
 (c) 2023 Adam Shaw
 */
