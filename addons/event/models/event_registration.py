0e0eae8ace5015051440a41fcda95e57ec814984
yagp-odoo <yagp@odoo.com>
2024-10-10 11:25:36 +0530

    [IMP] event: editable event and ticket type after registration
    Purpose:
    ========
    Enhance the flexibility for event organizers by:
    - Allowing users to change the event after registration.
    - Allowing users to change the ticket type after registration.
    
    After this commit:
    ==================
    Removed the `readonly` attribute from the `event_id`
    and `event_ticket_id` fields.
    Added tracking to allow users to track changes made to these fields.
    
    Task-4243953
    
    closes odoo/odoo#183069
    
    Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>

--- addons/event/models/event_registration.py
+++ addons/event/models/event_registration.py
@@ -32,9 +32,9 @@ class EventRegistration(models.Model):
 
     # event
     event_id = fields.Many2one(
-        'event.event', string='Event', required=True)
+        'event.event', string='Event', required=True, tracking=True)
     event_ticket_id = fields.Many2one(
-        'event.event.ticket', string='Ticket Type', ondelete='restrict')
+        'event.event.ticket', string='Ticket Type', ondelete='restrict', tracking=True)
     active = fields.Boolean(default=True)
     barcode = fields.Char(string='Barcode', default=lambda self: self._get_random_barcode(), readonly=True, copy=False)
     # utm informations
