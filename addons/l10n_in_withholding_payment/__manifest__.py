402f8a9c680f3e68b1efc9a0cd929576def3e7d6
Nishant Jain (NIAI) <niai@odoo.com>
2024-10-11 10:36:33 +0000

    [ADD] l10n_in_withholding_payment: tds entry for payment
    Added new module for creating TDS entry for payment
    
    Task - 4247425
    
    closes odoo/odoo#183622
    
    X-original-commit: e10b08c3f6d1014ceb0f34369748a625c2f268ff
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>

---
+++ addons/l10n_in_withholding_payment/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Indian - TDS For Payment',
+    'version': '1.0',
+    'description': """
+        Support for Indian TDS (Tax Deducted at Source) for Payment.
+    """,
+    'category': 'Accounting/Localizations',
+    'depends': ['l10n_in_withholding'],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
