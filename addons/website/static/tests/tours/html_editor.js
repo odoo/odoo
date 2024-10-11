7690bd68b9cf435282e41d21a5a0f94f3eeeeb25
Chrysanthe (chgo) <chgo@odoo.com>
2024-10-11 10:12:58 +0200

    [FIX] website: fix minor issues within the resource editor
    This commit aims to fix minor design issues within the resource editor
    that require a small amount of changes for a high gain.
    
    1) The `save` and `close` buttons in the top right corner are missing
    a spacing, making them stick together ;
    
    2) When opening the resource editor, a modal is displayed to indicate
    that the changes applied here could be lost during an upgrade but
    the content of that modal was missing a bit of spacing, and there was an
    issue with the layout of the bottom buttons.
    
    This commit takes care of these changes while keeping the same content.
    
    task-4008742
    
    closes odoo/odoo#170560
    
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/website/static/tests/tours/html_editor.js
+++ addons/website/static/tests/tours/html_editor.js
@@ -309,7 +309,7 @@ registerWebsitePreviewTour(
         },
         {
             content: "Bypass warning",
-            trigger: ".o_resource_editor_wrapper div:nth-child(2) button:nth-child(3)",
+            trigger: ".o_resource_editor_wrapper div:nth-child(2) > button",
             run: "click",
         },
         // Test all 3 file type options
