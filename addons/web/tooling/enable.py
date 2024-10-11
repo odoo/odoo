717e2700bff23c62bf7a7bf96d3f9278a537be5f
Lucas Perais <lpe@odoo.com>
2024-10-11 16:03:05 +0200

    [IMP] enable.py works

--- addons/web/tooling/enable.py
+++ addons/web/tooling/enable.py
@@ -16,48 +16,87 @@ def get_odoo_modules(path):
             continue
         yield addon
 
-with open("./_jsconfig.json", "r") as f:
-    jsconfig_file = f.read()
-
-ODOO_MODULES = get_odoo_modules(ODOO_PATH.joinpath("addons"))
-
-def make_js_config_module_paths(addons_path):
+def get_addon_path(addons_path):
     for addon_path in addons_path:
         if not addon_path:
             continue
-        addon_path_path = Path(addon_path)
+        addon_path_path = Path(addon_path).expanduser()
         if addon_path_path.is_absolute():
             modules_path = addon_path_path
-            odoo_from_addon = ODOO_PATH
         else:
             modules_path = ODOO_PATH.joinpath(addon_path)
-            odoo_from_addon = os.path.relpath(ODOO_PATH, start=modules_path.resolve())
+        odoo_from_addon = os.path.relpath(ODOO_PATH, start=modules_path.resolve())
         addon_modules = get_odoo_modules(modules_path)
-        js_paths = {}
-        for path, modules in [(Path(odoo_from_addon).joinpath("addons"), ODOO_MODULES), ("", addon_modules)]:
-            for m in modules:
-                js_paths.update(get_module_paths_aliases(path, m))
-        yield addon_path, addon_path_path, js_paths
+        js_paths = dict()
+        for m in addon_modules:
+            js_paths.update(get_module_paths_aliases("", m))
+        if js_paths:
+            yield {
+                'path_odoo_to_addon': modules_path,
+                "path_addon_to_odoo": odoo_from_addon,
+                "js_paths": js_paths,
+            }
 
 def get_module_paths_aliases(path, module):
     path = str(path)
     path = path + "/" if path else ""
-    result = {}
-    key_src = f"@{module.name}/*"
-    src = f"{path}{module.name}/static/src/*"
-    result[key_src] = [src]
+    if module.joinpath("static/src").is_dir():
+        key_src = f"@{module.name}/*"
+        src = f"{path}{module.name}/static/src/*"
+        yield key_src, [src]
+    if module.joinpath("static/tests").is_dir():
+        key_test = f"@{module.name}/../tests/*"
+        test = f"{path}{module.name}/static/tests/*"
+        yield key_test, [test]
 
-    key_test = f"@{module.name}/../tests/*"
-    test = f"{path}{module.name}/static/tests/*"
-    result[key_test] = [test]
-    return result
+def path_relative_to_odoo(to_odoo, path):
+    if path.startswith("addons/"):
+        return to_odoo + "/" + path
+    return path
 
-print(__name__)
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("--addons-path", required=False, default="../enterprise")
+    arg_parser.add_argument("--addons-path", required=False, default="")
+    arg_parser.add_argument("--update-odoo", required=False, action="store_true")
     args = arg_parser.parse_args()
-    print(args)
 
-    for t in make_js_config_module_paths((args.addons_path or "").split(",")):
-        print(t)
+    odoo_modules = get_odoo_modules(ODOO_PATH.joinpath("addons"))
+    with open("./_jsconfig.json", "r") as f:
+        jsconfig_file = f.read()
+
+    odoo_paths = dict()
+    for module in odoo_modules:
+        odoo_paths.update(get_module_paths_aliases("addons", module))
+
+    for data in get_addon_path((args.addons_path or "").split(",")):
+        js_config = json.loads(jsconfig_file)
+        odoo_paths_local = {}
+        for module, paths in odoo_paths.items():
+            odoo_paths_local[module] = [path_relative_to_odoo(str(data["path_addon_to_odoo"]), p) for p in paths]
+
+        paths = js_config["compilerOptions"].get("paths", {})
+        for js_paths in [odoo_paths_local, data["js_paths"]]:
+            paths.update(js_paths)
+        js_config["compilerOptions"]["paths"] = paths
+
+        include = js_config.get("include", [])
+        for i, path in enumerate(include):
+             include[i] = path_relative_to_odoo(str(data["path_addon_to_odoo"]), path)
+        include.extend(["**/*.js", "**/*.ts"])
+        js_config["include"] = include
+
+        exclude = js_config.get("exclude", [])
+        for i, path in enumerate(exclude):
+             exclude[i] = path_relative_to_odoo(str(data["path_addon_to_odoo"]), path)
+        js_config["exclude"] = exclude
+
+        with open(data["path_odoo_to_addon"].joinpath("jsconfig.json"), "w+") as f:
+            f.write(json.dumps(js_config, indent=2))
+
+    if args.update_odoo:
+        js_config = json.loads(jsconfig_file)
+        paths = js_config["compilerOptions"].get("paths", {})
+        paths.update(odoo_paths)
+        js_config["compilerOptions"]["paths"] = paths
+        with open(ODOO_PATH.joinpath("jsconfig.json"), "w+") as f:
+            f.write(json.dumps(js_config, indent=2))
