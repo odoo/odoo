e111185de51b03b0432da849c4a6142eb0abea0b
Géry Debongnie <ged@odoo.com>
2024-10-11 15:37:02 +0200

    inline tab handling in snippets component

--- addons/mysterious_egg/static/src/snippets_menu_tabs/customize_tab.js
+++ addons/mysterious_egg/static/src/snippets_menu_tabs/customize_tab.js
@@ -3,9 +3,3 @@ import { Component } from "@odoo/owl";
 export class CustomizeTab extends Component {
     static template = "mysterious_egg.CustomizeTab";
 }
-
-export const customizeTab = {
-    Component: CustomizeTab,
-    name: "custom",
-    title: "Customize",
-};
