5183cd51483235f213cb0e27602dbd8817838b08
Géry Debongnie <ged@odoo.com>
2024-10-11 16:08:57 +0200

    add toolbar to customize tab (naive)

--- addons/mysterious_egg/static/src/snippets_menu_tabs/customize_tab.js
+++ addons/mysterious_egg/static/src/snippets_menu_tabs/customize_tab.js
@@ -1,5 +1,8 @@
 import { Component } from "@odoo/owl";
+import { Toolbar } from "@html_editor/main/toolbar/toolbar";
 
 export class CustomizeTab extends Component {
     static template = "mysterious_egg.CustomizeTab";
+    static components = { Toolbar };
+
 }
