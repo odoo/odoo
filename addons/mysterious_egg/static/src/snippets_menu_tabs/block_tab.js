ed3e15f68389c7fafcbcccda663ecedea960e431
Géry Debongnie <ged@odoo.com>
2024-10-11 15:37:02 +0200

    inline tab handling in snippets component

--- addons/mysterious_egg/static/src/snippets_menu_tabs/block_tab.js
+++ addons/mysterious_egg/static/src/snippets_menu_tabs/block_tab.js
@@ -3,9 +3,3 @@ import { Component } from "@odoo/owl";
 export class BlockTab extends Component {
     static template = "mysterious_egg.BlockTab";
 }
-
-export const blockTab = {
-    Component: BlockTab,
-    name: "block",
-    title: "Block",
-};
