f94d668777866531b3811dd804f76828b4190977
Julien Van Roy <juvr@odoo.com>
2024-10-11 14:56:40 +0200

    [FIX] account,l10n_fr[_*]: remove siret field
    The company_registry should be used instead.
    
    In addition, add a dynamic placeholder on the company_registry to
    suggest a SIRET for french partners.
    
    task-4204049

--- odoo/addons/base/models/res_partner.py
+++ odoo/addons/base/models/res_partner.py
@@ -248,6 +248,7 @@ class ResPartner(models.Model):
     same_company_registry_partner_id: ResPartner = fields.Many2one('res.partner', string='Partner with same Company Registry', compute='_compute_same_vat_partner_id', store=False)
     company_registry = fields.Char(string="Company ID", compute='_compute_company_registry', store=True, readonly=False,
        help="The registry number of the company. Use it if it is different from the Tax ID. It must be unique across all partners of a same country")
+    company_registry_placeholder = fields.Char(compute='_compute_company_registry_placeholder')
     bank_ids: ResPartnerBank = fields.One2many('res.partner.bank', 'partner_id', string='Banks')
     website = fields.Char('Website Link')
     comment = fields.Html(string='Notes')
@@ -457,6 +458,9 @@ class ResPartner(models.Model):
         # exists to allow overrides
         for company in self:
             company.company_registry = company.company_registry
+
+    def _compute_company_registry_placeholder(self):
+        self.company_registry_placeholder = False
 
     @api.constrains('parent_id')
     def _check_parent_id(self):
