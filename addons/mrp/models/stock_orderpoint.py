355aee376eeae21a43db3ea09175de6b911072e4
Aurelien van Delft (avd) <avd@odoo.com>
2024-10-11 16:01:55 +0200

    [PERF] mrp, *: add checks to _compute_days_to_order
    In both mrp and purchase_stock, `_compute_days_to_order` does some computation
    when the rule_ids' action is 'manufacture' and 'buy' respectively. This forces
    the computation of rule_ids on all orderpoints in self. This commit adds
    some prechecks and prefiltering on self to reduce the size
    of the recordset that needs the value of rule_ids.

--- addons/mrp/models/stock_orderpoint.py
+++ addons/mrp/models/stock_orderpoint.py
@@ -64,7 +64,12 @@ class StockWarehouseOrderpoint(models.Model):
 
     def _compute_days_to_order(self):
         res = super()._compute_days_to_order()
-        for orderpoint in self:
+        # Avoid computing rule_ids in case no manufacture rules.
+        if not self.env['stock.rule'].search([('action', '=', 'manufacture')]):
+            return res
+        # Compute rule_ids only for orderpoint with boms
+        orderpoints_with_bom = self.filtered(lambda orderpoint: orderpoint.product_id.variant_bom_ids or orderpoint.product_id.bom_ids)
+        for orderpoint in orderpoints_with_bom:
             if 'manufacture' in orderpoint.rule_ids.mapped('action'):
                 boms = (orderpoint.product_id.variant_bom_ids or orderpoint.product_id.bom_ids)
                 orderpoint.days_to_order = boms and boms[0].days_to_prepare_mo or 0
