c81de039d62d2e8d86c2af1576501cc58563adab
Julien Castiaux <juc@odoo.com>
2024-10-14 18:45:25 +0200

    [IMP] core: stop using code 200 for all RPC errors
    The JSONRPC specification specifies what "code" should be used with what
    kind of errors. Odoo never cared to implement those errors correctly but
    frankly 200 is the single worst value to pick. Use 0 instead. It doesn't
    respect the specification either but at least it is not 200.
    
    task-4257153

--- addons/point_of_sale/static/tests/unit/data_service.test.js
+++ addons/point_of_sale/static/tests/unit/data_service.test.js
@@ -120,7 +120,7 @@ class ResPartner extends models.ServerModel {
     create() {
         const error = new RPCError();
         error.exceptionName = "odoo.exceptions.ValidationError";
-        error.code = 200;
+        error.code = 0;
         error.message = "ValidationError";
         error.data = {
             name: "ValidationError",
