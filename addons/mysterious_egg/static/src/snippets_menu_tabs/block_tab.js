47f9d8308fd9aa51a0c148adaf1838603380444a
FrancoisGe <fge@odoo.com>
2024-10-14 10:21:58 +0200

    add basic block + install

--- addons/mysterious_egg/static/src/snippets_menu_tabs/block_tab.js
+++ addons/mysterious_egg/static/src/snippets_menu_tabs/block_tab.js
@@ -1,5 +1,77 @@
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
+import { ConfirmationDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
+import { _t } from "@web/core/l10n/translation";
+import { RPCError } from "@web/core/network/rpc";
+import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
+import { escape } from "@web/core/utils/strings";
+
+function splitArrayBy3(arr) {
+    const result = [];
+    for (let i = 0; i < arr.length; i += 3) {
+        result.push(arr.slice(i, i + 3));
+    }
+    return result;
+}
 
 export class BlockTab extends Component {
     static template = "mysterious_egg.BlockTab";
+
+    setup() {
+        this.dialog = useService("dialog");
+        this.orm = useService("orm");
+    }
+
+    get categoriesBy3() {
+        const categories = registry
+            .category("website.snippets")
+            .category("block.categories")
+            .getEntries()
+            .filter(
+                ([, category]) =>
+                    category.isAvailable === undefined || category.isAvailable(this.env)
+            );
+        return splitArrayBy3(categories);
+    }
+
+    onClickInstall(snippet) {
+        // TODO: Should be the app name, not the snippet name ... Maybe both ?
+        const bodyText = _t("Do you want to install %s App?", snippet.title);
+        const linkText = _t("More info about this app.");
+        const linkUrl = "/odoo/action-base.open_module_tree/" + encodeURIComponent(snippet.install);
+
+        this.dialog.add(ConfirmationDialog, {
+            title: _t("Install %s", snippet.title),
+            body: markup(
+                `${escape(bodyText)}\n<a href="${linkUrl}" target="_blank">${escape(linkText)}</a>`
+            ),
+            confirm: async () => {
+                try {
+                    await this.orm.call("ir.module.module", "button_immediate_install", [
+                        [snippet.install],
+                    ]);
+                    this.invalidateSnippetCache = true;
+
+                    // TODO Need to Reload webclient
+                    // this._onSaveRequest({
+                    //     data: {
+                    //         reloadWebClient: true,
+                    //     },
+                    // });
+                } catch (e) {
+                    if (e instanceof RPCError) {
+                        const message = escape(_t("Could not install module %s", snippet.title));
+                        this.notification.add(message, {
+                            type: "danger",
+                            sticky: true,
+                        });
+                        return;
+                    }
+                    throw e;
+                }
+            },
+            confirmLabel: _t("Save and Install"),
+            cancel: () => {},
+        });
+    }
 }
