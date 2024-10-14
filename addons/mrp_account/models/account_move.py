3d6687392122ef8681f0294d1f5ce30ab5f0cf06
Lulu Grimalkin (lugr) <lugr@odoo.com>
2024-10-14 10:48:43 +0200

    [IMP] mrp_account: add smart button for WIP journal entry to MO view
    Adds a smart button to manufacturing order views to view the
    corresponding WIP journal entries, mirroring the function of the smart
    button in journal entries.
    
    Task ID: 4154879

--- addons/mrp_account/models/account_move.py
+++ addons/mrp_account/models/account_move.py
@@ -10,7 +10,7 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     wip_production_ids = fields.Many2many(
-        'mrp.production', string="Relevant WIP MOs",
+        'mrp.production', 'wip_move_production_rel', 'move_id', 'production_id', string="Relevant WIP MOs",
         help="The MOs that this WIP entry was based on. Expected to be set at time of WIP entry creation.")
     wip_production_count = fields.Integer("Manufacturing Orders Count", compute='_compute_wip_production_count')
 
