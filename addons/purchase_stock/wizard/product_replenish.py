d118359c323c2749fbd3f0277dedc5d4c9c9760d
kdeb@odoo.com <kdeb@odoo.com>
2024-10-14 11:18:55 +0000

    [FIX] purchase_stock: set replenish date based on action buy/manufacture
    Issue:
        - When a database is set to a language other than English,
        the 'Expected Date' for product replenishment does not adapt
        according to the set vendor lead time.
        This issue does not occur when the database is set to English.
    
    Steps To Reproduce:
        - Create a storable product.
        - Define a vendor for this product and add a delivery lead time.
        - Navigate to the product template and click on the "Replenish" button.
        - Observe that in an English language setting, the schedule date is
         calculated correctly considering the vendor's delivery lead time.
        - Change the language of the database to a different language.
        - Repeat the replenish process.
        - Observe that the schedule date is not calculated correctly.
    
    Solution:
        - Replaced route name check with action check in `_get_date_planned`
        for better reliability, as action-based conditions reduce errors
        compared to route name comparisons.
    
    opw-4199660
    
    closes odoo/odoo#187267
    
    X-original-commit: 387cd9a8b0b93ef4b2dad6932326db20f3938412
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Kawtar Drissi El Bouzaidi (kdeb) <kdeb@odoo.com>

--- addons/purchase_stock/wizard/product_replenish.py
+++ addons/purchase_stock/wizard/product_replenish.py
@@ -31,7 +31,7 @@ class ProductReplenish(models.TransientModel):
     def _compute_date_planned(self):
         super()._compute_date_planned()
         for rec in self:
-            if rec.route_id.name == 'Buy':
+            if 'buy' in rec.route_id.rule_ids.mapped('action'):
                 rec.date_planned = rec._get_date_planned(rec.route_id, supplier=rec.supplier_id, show_vendor=rec.show_vendor)
 
     def _prepare_run_values(self):
@@ -71,7 +71,7 @@ class ProductReplenish(models.TransientModel):
 
     def _get_date_planned(self, route_id, **kwargs):
         date = super()._get_date_planned(route_id, **kwargs)
-        if route_id.name != 'Buy':
+        if 'buy' not in route_id.rule_ids.mapped('action'):
             return date
 
         supplier = kwargs.get('supplier')
