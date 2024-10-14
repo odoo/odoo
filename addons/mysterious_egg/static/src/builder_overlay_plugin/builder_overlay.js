892c17bf475b499e0ed34cf562f7c5ecf7351a1a
fdardenne <dafl@odoo.com>
2024-10-14 16:56:21 +0200

    [IMP] add overlay when clicking on selectors

--- addons/mysterious_egg/static/src/builder_overlay_plugin/builder_overlay.js
+++ addons/mysterious_egg/static/src/builder_overlay_plugin/builder_overlay.js
@@ -1,4 +1,5 @@
-import { Component, onWillDestroy, useRef, useState } from "@odoo/owl";
+import { Component, useRef, useState } from "@odoo/owl";
+import { usePosition } from "@web/core/position/position_hook";
 
 export class BuilderOverlay extends Component {
     static template = "mysterious_egg.BuilderOverlay";
@@ -8,29 +9,14 @@ export class BuilderOverlay extends Component {
             height: this.props.target.clientHeight,
             width: this.props.target.clientWidth,
         });
-        // TODO: we can use the editor overlay service instead
-        // But to do so we have to modify computePosition to
-        // add the "full" direction
-        this.position = this.getOffset(this.props.target);
-    }
 
-    getOffset(target) {
-        if (!target.getClientRects().length) {
-            return { top: 0, left: 0 };
-        } else {
-            const rect = target.getBoundingClientRect();
-            const win = target.ownerDocument.defaultView;
-            const frameElement = target.ownerDocument.defaultView.frameElement;
-            const offset = { top: 0, left: 0 };
-            if (frameElement) {
-                const frameRect = frameElement.getBoundingClientRect();
-                offset.left += frameRect.left;
-                offset.top += frameRect.top;
-            }
-            return {
-                top: rect.top + win.pageYOffset + offset.top,
-                left: rect.left + win.pageXOffset + offset.left,
-            };
-        }
+        usePosition("root", () => this.props.target, {
+            position: "center",
+            container: () => this.props.container,
+            onPositioned: () => {
+                this.size.height = this.props.target.clientHeight; 
+                this.size.width = this.props.target.clientWidth;
+            },
+        });
     }
 }
