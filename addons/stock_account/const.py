da872ea6caff515f570d2709d63237f3fbfb8812
kcv-odoo <kcv@odoo.com>
2024-10-15 11:24:46 +0530

    [FIX] Test

--- addons/stock_account/const.py
+++ addons/stock_account/const.py
@@ -1,12 +1,1 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-COST_METHOD = [
-    ('standard', 'Standard Price'),
-    ('fifo', 'First In First Out (FIFO)'),
-    ('average', 'Average Cost (AVCO)'),
-]
-
-VALUATION = [
-    ('manual_periodic', 'Manual'),
-    ('real_time', 'Automated'),
-]
