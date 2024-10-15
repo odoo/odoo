e9bf66a0fcf45d1a37fa8697158f94dfb9bc548f
Megaaaaaa <thbe@odoo.com>
2024-10-15 10:12:34 +0200

    [FIX] l10n_pl: wrong account code prefixes
    Description of the issue this commit addresses:
    
    The code prefix for bank cash and transfer account in the polish company
    template are wrong because the code digits was wrongly set to 8 believing it
    was the amount of digits while it really is the amount of characters. Since
    there are two dots in the codes, the code_digits was wrongly two too low.
    
    Desired behavior after this commit is merged:
    
    The automatic generation of accounts using the bank, cash and transfer
    prefix work as intended since the length of the codes are now 10 instead of 8
    
    Found during task-3950432

--- addons/l10n_pl/models/template_pl.py
+++ addons/l10n_pl/models/template_pl.py
@@ -13,7 +13,7 @@ class AccountChartTemplate(models.AbstractModel):
             'property_account_payable_id': 'chart21000100',
             'property_account_expense_categ_id': 'chart70010100',
             'property_account_income_categ_id': 'chart73000100',
-            'code_digits': '8',
+            'code_digits': '10',
             'use_storno_accounting': True,
         }
 
@@ -22,9 +22,9 @@ class AccountChartTemplate(models.AbstractModel):
         return {
             self.env.company.id: {
                 'account_fiscal_country_id': 'base.pl',
-                'bank_account_code_prefix': '11.000.00',
-                'cash_account_code_prefix': '12.000.00',
-                'transfer_account_code_prefix': '11.090.00',
+                'bank_account_code_prefix': '11.000.',
+                'cash_account_code_prefix': '12.000.',
+                'transfer_account_code_prefix': '11.090.',
                 'account_default_pos_receivable_account_id': 'chart20000200',
                 'income_currency_exchange_account_id': 'chart75000600',
                 'expense_currency_exchange_account_id': 'chart75010400',
