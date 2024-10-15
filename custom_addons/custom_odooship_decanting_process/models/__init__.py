f31e33dc027e136bd23cbff34d59d1d060b20b01
Drishti <drishti@shiperoo.com>
2024-10-16 05:20:11 +1100

    [ADD]Added Site Code and Tenant Code Functionality

--- custom_addons/custom_odooship_decanting_process/models/__init__.py
+++ custom_addons/custom_odooship_decanting_process/models/__init__.py
@@ -5,3 +5,7 @@ from . import stock_move_inherit
 from . import automation_decanting_orders_process
 from . import crate_barcode_configuration
 from . import license_plate_view
+from . import site_code_configuration
+from . import tenant_code_configuration
+from . import stock_picking_inherit
+from . import res_partner_inherit
