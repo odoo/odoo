86a1a83c97b65e570f926eecd4ed68b7ae59d5ff
fdardenne <dafl@odoo.com>
2024-10-16 17:13:35 +0200

    Better snippet selection management

--- addons/mysterious_egg/static/src/builder/builder_overlay_plugin/builder_overlay_plugin.js
+++ addons/mysterious_egg/static/src/builder/builder_overlay_plugin/builder_overlay_plugin.js
@@ -15,24 +15,44 @@ export class BuilderOverlayPlugin extends Plugin {
                 position: "center",
             },
         });
+        this.addDomListener(this.editable, "pointerup", (e) => {
+            if (!this.shared.getEditableSelection().isCollapsed) {
+                return;
+            }
+            this.changeSnippet(e.target);
+        });
     }
 
     onSelectionChange(selection) {
+        if (selection.editableSelection.isCollapsed) {
+            // Some elements are not selectable in the editor but still can be
+            // a snippet. The selection will be put in the closest selectable element.
+            // Therefore if the selection is collapsed, let the pointerup event handle
+            return;
+        }
         let selectionNode = selection.editableSelection.commonAncestorContainer;
         if (selectionNode.nodeType === Node.TEXT_NODE) {
             selectionNode = selectionNode.parentElement;
         }
+        this.changeSnippet(selectionNode);
+    }
 
+    findSnippetElementFromTarget(target) {
         for (const selector of this.selectors) {
-            const snippetElement = selectionNode.closest(selector);
+            const snippetElement = target.closest(selector);
             if (snippetElement) {
-                this.openBuilderOverlay(snippetElement);
-                for (const handler of this.resources.onSnippetChange || []) {
-                    handler(snippetElement);
-                }
-                return;
+                return snippetElement;
             }
         }
+    }
+
+    changeSnippet(selectedElement) {
+        const snippetElement = this.findSnippetElementFromTarget(selectedElement);
+        this.openBuilderOverlay(snippetElement);
+        for (const handler of this.resources.onSnippetChange || []) {
+            handler(snippetElement);
+        }
+        return;
     }
 
     openBuilderOverlay(target) {
