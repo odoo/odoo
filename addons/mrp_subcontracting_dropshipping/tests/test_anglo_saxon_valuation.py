1aa3b4a949ba7399ac81d054e91270d936a4793d
Ethan Vincent <etvi@odoo.com>
2024-10-16 12:28:35 +0000

    [FIX] mrp_{subcontracting_dropshipping,account}: use comp qty in price unit calc
    **Current behavior:**
    Selling a kit bom product will result in inaccurate journal
    entries in the stock output and expense accounts, the
    amount_currency field will only reflect the price unit of the
    kit's components.
    
    **Expected behavior:**
    The price unit should reflect the total cost of the components
    on the BoM: e.g., if a kit product needs 4 of some component1,
    the price unit should be 4 * component1.standard_price.
    
    **Steps to reproduce:**
    1. Create a kit product with 2 components, both with qty > 1
    
    2. Assign dropshipping to the kit
    
    3. Create a sale order and confirm -> confirm the purchase order
    
    4. Validate the dropship transfer, invoice the sale order and
    post it
    
    5. See the 2 inaccurate journal entries, where the debit/credit
    respectively only total to one instance of each component's
    cost added together
    
    **Cause of the issue:**
    The price unit calculation didn't look at the bom line product
    qty, only using the price unit 1 time per component.
    
    **Fix:**
    Multiply the value by the quantity of it required on its line in
    the BoM.
    
    opw-4253827
    
    closes odoo/odoo#185802
    
    X-original-commit: 24555eb2ed5f094a3eabbbbd0c55fbdc1380c1b0
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Vincent Ethan <etvi@odoo.com>

--- addons/mrp_subcontracting_dropshipping/tests/test_anglo_saxon_valuation.py
+++ addons/mrp_subcontracting_dropshipping/tests/test_anglo_saxon_valuation.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo.addons.stock_account.tests.test_anglo_saxon_valuation_reconciliation_common import ValuationReconciliationTestCommon
+from odoo import Command
 from odoo.tests import tagged, Form
 
 
@@ -210,6 +211,17 @@ class TestSubcontractingDropshippingValuation(ValuationReconciliationTestCommon)
         purchase order line has been manually edited.
         """
         kit_final_prod = self.product_a
+        product_c = self.env['product.product'].create({
+            'name': 'product_c',
+            'uom_id': self.env.ref('uom.product_uom_dozen').id,
+            'uom_po_id': self.env.ref('uom.product_uom_dozen').id,
+            'lst_price': 120.0,
+            'standard_price': 100.0,
+            'property_account_income_id': self.copy_account(self.company_data['default_account_revenue']).id,
+            'property_account_expense_id': self.copy_account(self.company_data['default_account_expense']).id,
+            'taxes_id': [Command.set((self.tax_sale_a + self.tax_sale_b).ids)],
+            'supplier_taxes_id': [Command.set((self.tax_purchase_a + self.tax_purchase_b).ids)],
+        })
         kit_bom = self.env['mrp.bom'].create({
             'product_tmpl_id': kit_final_prod.product_tmpl_id.id,
             'product_uom_id': kit_final_prod.uom_id.id,
@@ -218,13 +230,21 @@ class TestSubcontractingDropshippingValuation(ValuationReconciliationTestCommon)
         })
         kit_bom.bom_line_ids = [(0, 0, {
             'product_id': self.product_b.id,
-            'product_qty': 1,
+            'product_qty': 4,
+        }), (0, 0, {
+            'product_id': product_c.id,
+            'product_qty': 2,
         })]
 
         self.env['product.supplierinfo'].create({
             'product_id': self.product_b.id,
             'partner_id': self.partner_a.id,
-            'price': 2000,
+            'price': 160,
+        })
+        self.env['product.supplierinfo'].create({
+            'product_id': product_c.id,
+            'partner_id': self.partner_a.id,
+            'price': 100,
         })
 
         (kit_final_prod + self.product_b).categ_id.write({
@@ -251,14 +271,18 @@ class TestSubcontractingDropshippingValuation(ValuationReconciliationTestCommon)
         account_move = sale_order._create_invoices()
         account_move.action_post()
 
+        # Each product_a should cost:
+        # 4x product_b = 160 * 4 = 640 +
+        # 2x product_c = 100 * 2 = 200
+        #                        = 840
         self.assertRecordValues(
             account_move.line_ids.sorted('balance'),
             [
-                {'name': 'product_a',                       'debit': 0.0,       'credit': 4000.0},
                 {'name': 'product_a',                       'debit': 0.0,       'credit': 1800.0},
+                {'name': 'product_a',                       'debit': 0.0,       'credit': 1680.0},
                 {'name': '15% (Copy)',                      'debit': 0.0,       'credit': 270.0},
                 {'name': 'INV/2024/00001 installment #1',   'debit': 621.0,     'credit': 0.0},
                 {'name': 'INV/2024/00001 installment #2',   'debit': 1449.0,    'credit': 0.0},
-                {'name': 'product_a',                       'debit': 4000.0,    'credit': 0.0},
+                {'name': 'product_a',                       'debit': 1680.0,    'credit': 0.0},
             ]
         )
