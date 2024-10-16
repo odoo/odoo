28ea76b928b5bca5a4365e3de81fe686adac6e15
Thịnh <truongthinhm1@gmail.com>
2024-10-16 15:09:18 +0700

    I newbie try build a module myself

---
+++ addons/My_custom_module/__manifest__.py
@@ -0,0 +1,16 @@
+{
+'name': "My library",
+'summary': "Manage books easily",
+'description': """
+Manage Library
+==============
+Description related to library.
+""",
+'author': "Your name",
+'website': "http://www.example.com",
+'category': 'Uncategorized',
+'version': '13.0.1',
+'depends': ['base'],
+'data': ['views/views.xml'],
+'demo': ['demo.xml'],
+}
