a58b6ce14124f64af2ac82da01f7cd17b5bdb8b5
“Beekan” <mmha@odoo.com>
2024-10-16 09:26:37 +0000

    [PERF] mail: Disable prefetching of unneeded fields
    Modifications are made around read, filtered, list comprehension.
    
    The read function usually does not prefetech fields other than
    stated. But in this case, since the fields are related fields
    from another model, The fields of the other model are prefeteched.
    This includes the body which can be very big in size and cause an
    out of memory error.
    
    Filtered does not need other fields and keeps them unnecessarily
    in cache.
    
    List comprehension where it has a reference to specific field inside
    the model also triggers the prefetecher.
    
    Benchmark:
    |        |Number of queries| SQL time| Python time||
    |-------|----------------------------|----------------|---------------|-|
    |with prefetch| 82| 1.887 | 7.898| Out of memory|
    |Without prefetch| 120| 0.385 | 5.973| |
    
    The benchmark with the prefetch was done locally with
    increasing the memory limit to get the data.
    
    X-original-commit: ca837881bca7c2abae4bce091d8d9feb2614f5e0

--- addons/mail/models/mail_mail.py
+++ addons/mail/models/mail_mail.py
@@ -545,7 +545,7 @@ class MailMail(models.Model):
         Return iterators over
             mail_server_id, email_from, Records<mail.mail>.ids
         """
-        mail_values = self.read(['id', 'email_from', 'mail_server_id', 'record_alias_domain_id'])
+        mail_values = self.with_context(prefetch_fields=False).read(['id', 'email_from', 'mail_server_id', 'record_alias_domain_id'])
 
         # First group the <mail.mail> per mail_server_id, per alias_domain (if no server) and per email_from
         group_per_email_from = defaultdict(list)
@@ -835,6 +835,7 @@ class MailMail(models.Model):
                 if post_send_callback:
                     post_send_callback([mail_id])
                 self._cr.commit()
+            mail.invalidate_recordset(['body_html'])
         if post_send_callback:
             post_send_callback(self.ids)
         return True
