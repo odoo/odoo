305452815601b54ffb055a0c7aca31c2a5363351
Luis González <lgonzalez@vauxoo.com>
2024-10-16 01:12:30 +0000

    [FIX] web: ARIA attributes of field widgets not ported correctly to OWL
    When field widgets were ported to OWL in 48ef812a, a few ARIA attributes
    were not adapted correctly. For instance, in the statusbar widget, the
    title was ported as an `aria-label` attribute ("Current state" and "Not
    active state"), which overrides the actual status name.
    
    This commit fixes the above issue by removing the incorrect `aria-label`
    attributes and fixing logic for other ones like `aria-checked`.
    
    X-original-commit: 355e035e9e156646b63ffb4eb6eb55c5bad8485d

--- addons/web/static/tests/views/fields/statusbar_field.test.js
+++ addons/web/static/tests/views/fields/statusbar_field.test.js
@@ -390,7 +390,7 @@ test("statusbar: choose an item from the folded menu", async () => {
         `,
     });
 
-    expect("[aria-label='Current state is aaa']").toHaveText("aaa", {
+    expect("[aria-checked='true']").toHaveText("aaa", {
         message: "default status is 'aaa'",
     });
 
@@ -403,7 +403,7 @@ test("statusbar: choose an item from the folded menu", async () => {
     await click(".o-dropdown--menu .dropdown-item");
     await animationFrame();
 
-    expect("[aria-label='Current state is second record']").toHaveText("second record", {
+    expect("[aria-checked='true']").toHaveText("second record", {
         message: "status has changed to the selected dropdown item",
     });
 });
