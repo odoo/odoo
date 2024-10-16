6df1fa0875aac29869a161246cf8ec8cf0a069a8
Walid (wasa) <wasa@odoo.com>
2024-10-16 12:55:27 +0000

    [FIX] website_sale: ensure applied filters are preserved when sorting
    Problem:
    When generating sorting URLs, the `post` dictionary only contains
    a single selected attribute. This is due to `request.params` only
    passing the first `attrib` parameter during dispatch, even though
    there can be multiple.
    
    Solution:
    Modify the `post` dictionary to store attributes as a list before
    passing it to the `_shop_get_query_url_kwargs` method. This ensures
    that all selected attributes are preserved and properly passed to the
    `keep` query parameters.
    
    Steps to reproduce:
    - Go to `/shop`.
    - Apply multiple attribute filters.
    - Change the sorting option.
    - Only the first selected attribute is applied in the filter.
    
    opw-4244895
    
    closes odoo/odoo#187681
    
    X-original-commit: 94a982727f200dba34cb9153299decd4e56aa2f7
    Signed-off-by: Valentin Chevalier <vcr@odoo.com>
    Signed-off-by: Walid Sahli (wasa) <wasa@odoo.com>

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -260,6 +260,8 @@ class WebsiteSale(payment_portal.PaymentPortal):
         attrib_values = [[int(x) for x in v.split("-")] for v in attrib_list if v]
         attributes_ids = {v[0] for v in attrib_values}
         attrib_set = {v[1] for v in attrib_values}
+        if attrib_list:
+            post['attrib'] = attrib_list
 
         filter_by_tags_enabled = website.is_view_active('website_sale.filter_products_tags')
         if filter_by_tags_enabled:
@@ -300,8 +302,6 @@ class WebsiteSale(payment_portal.PaymentPortal):
         url = '/shop'
         if search:
             post['search'] = search
-        if attrib_list:
-            post['attrib'] = attrib_list
 
         options = self._get_search_options(
             category=category,
