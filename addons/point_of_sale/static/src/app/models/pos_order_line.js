9fed4235491dd5922156de50bfc295001d4041e3
parp-odoo <parp@odoo.com>
2024-10-16 10:46:20 +0530

    [IMP] point_of_sale: show combo line amount and hide individual line amounts
    In this commit:
    ===============
    - Display the total amount for combo product lines based on selected
      product prices.
    - Hide the amounts for individual order lines that are part of a combo.
    
    Task:4229373

--- addons/point_of_sale/static/src/app/models/pos_order_line.js
+++ addons/point_of_sale/static/src/app/models/pos_order_line.js
@@ -366,11 +366,23 @@ export class PosOrderline extends Base {
     }
 
     get unitDisplayPrice() {
-        if (this.config.iface_tax_included === "total") {
-            return this.allUnitPrices.priceWithTax;
-        } else {
-            return this.allUnitPrices.priceWithoutTax;
-        }
+        const prices =
+            this.combo_line_ids.length > 0
+                ? this.combo_line_ids.reduce(
+                      (acc, cl) => {
+                          const clPrices = cl.getAllPrices(1);
+                          return {
+                              priceWithTax: acc.priceWithTax + clPrices.priceWithTax,
+                              priceWithoutTax: acc.priceWithoutTax + clPrices.priceWithoutTax,
+                          };
+                      },
+                      { priceWithTax: 0, priceWithoutTax: 0 }
+                  )
+                : this.getAllPrices(1);
+
+        return this.config.iface_tax_included === "total"
+            ? prices.priceWithTax
+            : prices.priceWithoutTax;
     }
 
     getUnitDisplayPriceBeforeDiscount() {
@@ -617,7 +629,13 @@ export class PosOrderline extends Base {
             ? // free if the discount is 100
               _t("Free")
             : this.combo_line_ids.length > 0
-            ? // empty string if it is a combo parent line
+            ? // total of all combo lines if it is combo parent
+              formatCurrency(
+                  this.combo_line_ids.reduce((total, cl) => total + cl.getDisplayPrice(), 0),
+                  this.currency
+              )
+            : this.combo_parent_id
+            ? // empty string if it has combo parent
               ""
             : formatCurrency(this.getDisplayPrice(), this.currency);
     }
