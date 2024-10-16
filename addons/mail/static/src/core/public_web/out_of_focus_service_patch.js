8107664407bdeed265a4a58016491045982b8a94
Alexandre Kühn <aku@odoo.com>
2024-10-16 13:09:43 +0000

    [FIX] mail: less distracting out-of-focus tab title message counter
    Follow-up of odoo#177238
    
    PR above improved showing of new messages in Odoo while being out
    of focus by showing it in tab title and blinking the text.
    
    However this felt too obstructive in practice.
    
    This commit removes the blinking part, and instead show the
    out-of-focus amount of unread messages as a counter as prefix
    of the tab title. For example, if the action is "Inbox", it shows
    "(4) Inbox" when there are 4 unread and important messages received
    while odoo is out-of-focus.
    
    Also take new inbox messages into account for the out-of-focus
    counter.
    
    Task-4242524
    
    closes odoo/odoo#184028
    
    X-original-commit: 216234f4f636c938d5b99c702b455d3b6178c3e6
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/core/public_web/out_of_focus_service_patch.js
+++ addons/mail/static/src/core/public_web/out_of_focus_service_patch.js
@@ -1,45 +1,27 @@
 import { OutOfFocusService, outOfFocusService } from "@mail/core/common/out_of_focus_service";
 import { patch } from "@web/core/utils/patch";
-import { _t } from "@web/core/l10n/translation";
-import { sprintf } from "@web/core/utils/strings";
-
-export const UNREAD_MSG_TITLE = 100;
 
 patch(OutOfFocusService.prototype, {
     setup(env, services) {
         super.setup(env, services);
         this.titleService = services.title;
         this.counter = 0;
+        this.contributingMessageLocalIds = new Set();
         env.bus.addEventListener("window_focus", () => this.clearUnreadMessage());
-        this.intervalShowing = true;
     },
     clearUnreadMessage() {
         this.counter = 0;
-        this.titleService.setParts({ [UNREAD_MSG_TITLE]: undefined });
-        this.intervalShowing = true;
-        this.titlePattern = undefined;
-        clearInterval(this.newMessageInterval);
+        this.contributingMessageLocalIds.clear();
+        this.titleService.setCounters({ discuss: undefined });
     },
-    setInterval(func, duration) {
-        return setInterval(func, duration);
-    },
-    updateTitle() {
-        this.titleService.setParts({
-            [UNREAD_MSG_TITLE]: this.intervalShowing
-                ? sprintf(this.titlePattern, this.counter)
-                : undefined,
-        });
-    },
-    notify() {
+    notify(message) {
         super.notify(...arguments);
-        clearInterval(this.newMessageInterval);
+        if (this.contributingMessageLocalIds.has(message.localId)) {
+            return;
+        }
+        this.contributingMessageLocalIds.add(message.localId);
         this.counter++;
-        this.titlePattern = this.counter === 1 ? _t("%s Message") : _t("%s Messages");
-        this.updateTitle();
-        this.newMessageInterval = this.setInterval(() => {
-            this.updateTitle();
-            this.intervalShowing = !this.intervalShowing;
-        }, 1_000);
+        this.titleService.setCounters({ discuss: this.counter });
     },
 });
 outOfFocusService.dependencies = [...outOfFocusService.dependencies, "title"];
