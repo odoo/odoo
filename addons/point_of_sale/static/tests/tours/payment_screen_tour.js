8598fd61d02977e52a9776e4a9a2266361644a16
manv-afk <manv@odoo.com>
2024-10-16 14:17:08 +0000

    [IMP] point_of_sale: numpad behaviour & unselect orderline
    -Select orderline only when I've just added a product to cart
    -Adapt tours tests to match this new behaviour and limit the use of 'inLeftSide' to avoid going uselessly back and forth to the product list in mobile.
    
    closes odoo/odoo#184968
    
    Task-id: 4167563
    X-original-commit: 370f30ac3dc8a1e1689c84a1c93628abc0dd9c6a
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Manu Vaillant (manv) <manv@odoo.com>

--- addons/point_of_sale/static/tests/tours/payment_screen_tour.js
+++ addons/point_of_sale/static/tests/tours/payment_screen_tour.js
@@ -10,7 +10,6 @@ registry.category("web_tour.tours").add("PaymentScreenTour", {
         [
             Chrome.startPoS(),
             ProductScreen.addOrderline("Letter Tray", "10"),
-            ProductScreen.selectedOrderlineHas("Letter Tray", "10.0"),
             ProductScreen.clickPayButton(),
             PaymentScreen.emptyPaymentlines("52.8"),
 
