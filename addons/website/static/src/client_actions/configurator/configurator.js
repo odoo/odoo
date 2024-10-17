e0387acf7392105db067a84aeae129ed16767928
Robin Lejeune (role) <role@odoo.com>
2024-10-17 15:29:30 +0200

    [FIX] website, *: support keyboard navigation on configurator
    *: test_website_modules
    
    Keyboard navigation and validation was not possible on the configurator
    pages. This commit makes sure it now behaves as expected and that
    components behaving as buttons are semantically treated as buttons.
    
    It also adds some quality-of-life improvements. On the 2nd page of the
    configurator, the 1st field (type selection) is automatically focused.
    Subsequently, for each field, once the use confirms their choice, the
    focus is moved to the next field (type -> industry -> purpose).
    
    task-3758859

--- addons/website/static/src/client_actions/configurator/configurator.js
+++ addons/website/static/src/client_actions/configurator/configurator.js
@@ -1,12 +1,13 @@
 import { browser } from "@web/core/browser/browser";
 const sessionStorage = browser.sessionStorage;
 import { AutoComplete } from "@web/core/autocomplete/autocomplete";
+import { getActiveHotkey } from "@web/core/hotkeys/hotkey_service";
 import { delay } from "@web/core/utils/concurrency";
 import { getDataURLFromFile, redirect } from "@web/core/utils/urls";
 import weUtils from '@web_editor/js/common/utils';
 import { _t } from "@web/core/l10n/translation";
 import { svgToPNG, webpToPNG } from "@website/js/utils";
-import { useService } from "@web/core/utils/hooks";
+import { useAutofocus, useService } from "@web/core/utils/hooks";
 import { registry } from "@web/core/registry";
 import { rpc } from "@web/core/network/rpc";
 import { mixCssColors } from '@web/core/utils/colors';
@@ -169,10 +170,22 @@ export class DescriptionScreen extends Component {
     };
     setup() {
         this.industrySelection = useRef('industrySelection');
+        this.purposeSelectionRef = useRef("purposeSelection");
         this.state = useStore();
         this.orm = useService('orm');
+        useAutofocus();
 
         onMounted(() => this.onMounted());
+
+        // Autofocus the next field once the current one is confirmed.
+        useEffect((selectedType, selectedIndustry) => {
+            if (selectedType && !selectedIndustry) {
+                this.industrySelection.el.querySelector("input").focus();
+            }
+            if (selectedIndustry) {
+                this.purposeSelectionRef.el.focus();
+            }
+        }, () => [this.state.selectedType, this.state.selectedIndustry]);
     }
 
     onMounted() {
@@ -243,9 +256,6 @@ export class DescriptionScreen extends Component {
 
     selectWebsiteType(id) {
         this.state.selectWebsiteType(id);
-        setTimeout(() => {
-            this.industrySelection.el.querySelector("input").focus();
-        });
         this.checkDescriptionCompletion();
     }
 
@@ -265,6 +275,16 @@ export class DescriptionScreen extends Component {
                 });
             }
             this.props.navigate(ROUTES.paletteSelectionScreen);
+        }
+    }
+    /**
+     * Hide the dropdown once the focus isn't contained within it anymore.
+     *
+     * @param {FocusEvent} ev
+     */
+    onDropdownFocusout(ev) {
+        if (ev.relatedTarget?.closest(".dropdown") !== ev.currentTarget) {
+            window.Dropdown.getOrCreateInstance(ev.currentTarget).hide();
         }
     }
 }
@@ -467,6 +487,13 @@ export class FeaturesSelectionScreen extends ApplyConfiguratorScreen {
         } else {
             this.state.updateRecommendedThemes(themes);
             this.props.navigate(ROUTES.themeSelectionScreen);
+        }
+    }
+
+    onKeydown(ev) {
+        const hotkey = getActiveHotkey(ev);
+        if (["enter", "space"].includes(hotkey)) {
+            ev.target.click();
         }
     }
 }
