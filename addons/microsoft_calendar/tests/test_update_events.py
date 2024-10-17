dd2ee6dd46babedf3c385aeeb5a7dcd8f0a891e2
kpt-odoo <kpt@odoo.com>
2024-10-17 11:08:08 +0530

    [CLN] microsoft_calendar: split test file and clean up code
    closes odoo/odoo#184031
    
    Signed-off-by: Arnaud Joset (arj) <arj@odoo.com>

--- addons/microsoft_calendar/tests/test_update_events.py
+++ addons/microsoft_calendar/tests/test_update_events.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 from datetime import datetime, timedelta
 from dateutil.parser import parse
 import logging
