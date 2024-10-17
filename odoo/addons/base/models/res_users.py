75a5648635feaee8d287628469ed6492e7dafd79
Nicolas Lempereur <nle@odoo.com>
2024-10-17 18:15:58 +0000

    [FIX] base: give has_group access to internal user
    The code comment says non-internal users should not have access to
    has_group if it's not for themselves. But the code checked the group
    of the targetted user, not the current user.
    
    Without the fix, the added test failed because:
    - AssertError was not raised if non-internal accessed internal user
    - AssertError was raised when internal accessed non-internal user
    
    note: found when reviewing 18.0 forward-port of d0828eecf60f7c8622d6875b
    
    opw-4096073
    
    closes odoo/odoo#184714
    
    X-original-commit: 3cc0044c0129c90f52df78d6b4e23c6a424736ca
    Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>

--- odoo/addons/base/models/res_users.py
+++ odoo/addons/base/models/res_users.py
@@ -1177,7 +1177,7 @@ class ResUsers(models.Model):
         the current request is in debug mode.
         """
         self.ensure_one()
-        if not (self.env.su or self == self.env.user or self._has_group('base.group_user')):
+        if not (self.env.su or self == self.env.user or self.env.user._has_group('base.group_user')):
             # this prevents RPC calls from non-internal users to retrieve
             # information about other users
             raise AccessError(_("You can ony call user.has_group() with your current user."))
