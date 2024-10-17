81b4d53d829bbabde2b75cafb0c97e37998b2eff
Mahdi Cheikh Rouhou (macr) <macr@odoo.com>
2024-10-17 14:06:51 +0200

    [FIX] web_editor: display columns correctly in pdf
    Steps to reproduce the issue:
    =============================
    - Go to any sale order
    - Add 2 columns in terms and conditions
    - Make sure the 2 columns have a long text in them
    - Print it
    - The 2 columns aren't displayed correctly in the pdf
    
    Origin of the issue:
    ====================
    The style of `col-lg-sz` wasn't applied because the media query consider
    the report as mobile view.
    
    Solution:
    =========
    We use `col-sz` instead for colums as used in.
    
    opw-4224039

--- addons/web_editor/static/src/js/editor/odoo-editor/src/commands/commands.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/commands/commands.js
@@ -983,7 +983,7 @@ export const editorCommands = {
             const columns = [];
             for (let i = 0; i < numberOfColumns; i++) {
                 const column = document.createElement('div');
-                column.classList.add(`col-lg-${columnSize}`);
+                column.classList.add(`col-${columnSize}`);
                 row.append(column);
                 columns.push(column);
             }
@@ -1016,7 +1016,7 @@ export const editorCommands = {
                 let lastColumn = columns[columns.length - 1];
                 for (let i = 0; i < diff; i++) {
                     const column = document.createElement('div');
-                    column.classList.add(`col-lg-${columnSize}`);
+                    column.classList.add(`col-${columnSize}`);
                     const p = document.createElement('p');
                     p.append(document.createElement('br'));
                     p.classList.add('oe-hint');
@@ -1030,7 +1030,7 @@ export const editorCommands = {
                 // Remove superfluous columns.
                 const restore = preserveCursor(editor.document);
                 for (const column of columns) {
-                    column.className = column.className.replace(REGEX_BOOTSTRAP_COLUMN, `col$1-${columnSize}`);
+                    column.className = column.className.replace(REGEX_BOOTSTRAP_COLUMN, `col-${columnSize}`);
                 }
                 const contents = [];
                 for (let i = diff; i < 0; i++) {
