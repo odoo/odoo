b8ca4de9449e53d9ff4729662f34db754aae852d
vlst-odoo <vlst@odoo.com>
2024-10-17 17:16:13 +0200

    [IMP] pos_hr: single pdf employee report
    When printing a sales report with the "Add a report per each employee"
    option selected, instead of getting one pdf file with both the session
    report and the per employee reports, we get 2 pdf files, one for the
    session and one for the employee reports.
    
    In this commit we make it such that we get all of the pages inside one
    pdf file.
    
    The changes also significantly simplify the code.
    
    Task: 4194165
    
    Task: 4194165

--- addons/point_of_sale/wizard/pos_daily_sales_reports.py
+++ addons/point_of_sale/wizard/pos_daily_sales_reports.py
@@ -9,6 +9,8 @@ class PosDailySalesReportsWizard(models.TransientModel):
 
     pos_session_id = fields.Many2one('pos.session', required=True)
 
+    def _get_report_data(self):
+        return {'date_start': False, 'date_stop': False, 'config_ids': self.pos_session_id.config_id.ids, 'session_ids': self.pos_session_id.ids}
+
     def generate_report(self):
-        data = {'date_start': False, 'date_stop': False, 'config_ids': self.pos_session_id.config_id.ids, 'session_ids': self.pos_session_id.ids}
-        return self.env.ref('point_of_sale.sale_details_report').report_action([], data=data)
+        return self.env.ref('point_of_sale.sale_details_report').report_action([], data=self._get_report_data())
