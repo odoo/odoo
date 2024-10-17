0c47b45bf7177cf4a1c03cf1199651273a6c4786
Pedram (pebr) <pebr@odoo.com>
2024-10-17 10:47:33 +0000

    [FIX] point_of_sale: synchronize cashier info on order and receipt
    Before this commit, adding a product to an order and then changing the
    cashier would not update the cashier information on the captured order,
    resulting in the previous cashier being recorded. However, the printed
    receipt would display the current cashier, leading to inconsistency.
    
    opw-4257705
    
    closes odoo/odoo#185706
    
    X-original-commit: e6d7af9178b82787c05bb63563d943a1d3c086bd
    Signed-off-by: Vlad Stroia (vlst) <vlst@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

---
+++ addons/pos_hr/static/src/overrides/models/pos_order.js
@@ -0,0 +1,9 @@
+import { PosOrder } from "@point_of_sale/app/models/pos_order";
+import { patch } from "@web/core/utils/patch";
+
+patch(PosOrder.prototype, {
+    // @Override
+    getCashierName() {
+        return this.employee_id?.name || super.getCashierName(...arguments);
+    },
+});
