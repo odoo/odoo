053ad13f05db3e463d8e971a561d8c75e38b51c7
tsm-odoo <tsm@odoo.com>
2024-10-17 07:32:30 +0000

    [FIX] mail: less persistent unread messages banner
    The "mark as read" banner helps users keep track of where they last
    left off in a conversation. Currently, the banner is removed in the
    following situations:
    
    - When returning to the thread twice.
    - When posting a message.
    - When clicking on the "mark as read" button on the banner.
    
    However, this approach is insufficient, as the banner appears too
    frequently. This pull request streamlines the conditions under which
    the banner disappears. The banner will now be removed wheni on top of
    the other conditions:
    
    - The user scrolls to the bottom of the thread.
    - The user opens a thread that does not have a scrollbar.
    
    Both circumstances are sufficient to indicate that the user has
    actually read the messages.
    
    task-4102924
    
    X-original-commit: 473a0948d361b11fee4963d45b573d72e2f0f7ce

--- addons/mail/static/src/core/common/thread_model.js
+++ addons/mail/static/src/core/common/thread_model.js
@@ -505,7 +505,7 @@ export class Thread extends Record {
     }
 
     get showUnreadBanner() {
-        return this.selfMember?.localMessageUnreadCounter > 0;
+        return !this.selfMember?.hideUnreadBanner && this.selfMember?.localMessageUnreadCounter > 0;
     }
 
     get rpcParams() {
