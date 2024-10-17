25fd7f71e82edefde36a1a7201139bb6ea8757ca
vlst-odoo <vlst@odoo.com>
2024-10-17 17:16:13 +0200

    [IMP] pos_hr: single pdf employee report
    When printing a sales report with the "Add a report per each employee"
    option selected, instead of getting one pdf file with both the session
    report and the per employee reports, we get 2 pdf files, one for the
    session and one for the employee reports.
    
    In this commit we make it such that we get all of the pages inside one
    pdf file.
    
    The changes also significantly simplify the code.
    
    closes odoo/odoo#184292
    
    Task: 4194165
    Task: 4194165
    Related: odoo/upgrade#6639
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/pos_hr/wizard/pos_daily_sales_reports.py
+++ addons/pos_hr/wizard/pos_daily_sales_reports.py
@@ -9,18 +9,11 @@ class PosDailySalesReportsWizard(models.TransientModel):
     add_report_per_employee = fields.Boolean(string='Add a report per each employee', default=True)
     employee_ids = fields.Many2many('hr.employee', compute='_compute_employee_ids')
 
-    def _get_report_data(self, pos_session_id):
-        pos_session = self.env['pos.session'].browse(pos_session_id)
-        return {'date_start': False, 'date_stop': False, 'config_ids': pos_session.config_id.ids, 'session_ids': pos_session.ids}
-
-    def get_single_report_print_action(self, pos_session_id):
-        data = self._get_report_data(pos_session_id)
-        return self.env.ref('point_of_sale.sale_details_report').report_action([], data=data)
-
-    def get_multi_report_print_action(self, pos_session_id, employee_ids):
-        data = self._get_report_data(pos_session_id)
-        data['employee_ids'] = employee_ids
-        return self.env.ref('pos_hr.multi_employee_sales_report_action').report_action([], data=data)
+    def _get_report_data(self):
+        return {
+            **super()._get_report_data(),
+            'employee_ids': self.employee_ids.ids if self.add_report_per_employee else [],
+        }
 
     @api.depends('pos_session_id')
     def _compute_employee_ids(self):
