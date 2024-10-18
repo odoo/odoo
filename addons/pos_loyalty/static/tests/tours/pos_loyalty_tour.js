8b26c84bd4ff2c46279c5baf773ce340720bf7ae
roen-odoo <roen@odoo.com>
2024-10-18 13:01:11 +0000

    [FIX] pos_loyalty: correctly detect gift card and eWallet
    When adding a rules in loyalty program that had a product set, if you
    try to refund an order containing this product you would get an error
    
    Steps to reproduce:
    -------------------
    * Create a loyalty program with a rules that has product_ids set to any
      product.
    * Open PoS and make an order with the product set on the loyalty program
    * Validate the order
    * Try to refund the order
    > Observation: You get an error saying you cannot refund giftcards or
      eWallets
    
    Why the fix:
    ------------
    This was happening because `trigger_product_ids` is related to
    `rule_ids.product_ids`. And when checking if a product is a giftcard or
    eWallet we first checked that `trigger_product_ids` was set.
    Now we only check the type of the program linked to a product.
    
    opw-4206226
    
    closes odoo/odoo#187325
    
    X-original-commit: 0b95547bc6e290e3e869e076d45329173a4add12
    Signed-off-by: Vlad Stroia (vlst) <vlst@odoo.com>
    Signed-off-by: Robin Engels (roen) <roen@odoo.com>

--- addons/pos_loyalty/static/tests/tours/pos_loyalty_tour.js
+++ addons/pos_loyalty/static/tests/tours/pos_loyalty_tour.js
@@ -4,6 +4,7 @@ import * as SelectionPopup from "@point_of_sale/../tests/tours/utils/selection_p
 import * as Dialog from "@point_of_sale/../tests/tours/utils/dialog_util";
 import * as Chrome from "@point_of_sale/../tests/tours/utils/chrome_util";
 import * as Notification from "@point_of_sale/../tests/tours/utils/generic_components/notification_util";
+import * as TicketScreen from "@point_of_sale/../tests/tours/utils/ticket_screen_util";
 import { registry } from "@web/core/registry";
 import { scan_barcode } from "@point_of_sale/../tests/tours/utils/common";
 
@@ -529,5 +530,22 @@ registry.category("web_tour.tours").add("PosLoyaltyPromocodePricelist", {
             ProductScreen.addOrderline("Test Product 1", "1"),
             PosLoyalty.enterCode("hellopromo"),
             PosLoyalty.orderTotalIs("25.87"),
+        ].flat(),
+});
+
+registry.category("web_tour.tours").add("RefundRulesProduct", {
+    steps: () =>
+        [
+            Chrome.startPoS(),
+            Dialog.confirm("Open Register"),
+            ProductScreen.clickDisplayedProduct("product_a"),
+            PosLoyalty.finalizeOrder("Cash", "1000"),
+            ProductScreen.isShown(),
+            ...ProductScreen.clickRefund(),
+            TicketScreen.filterIs("Paid"),
+            TicketScreen.selectOrder("-0001"),
+            ProductScreen.clickNumpad("1"),
+            TicketScreen.confirmRefund(),
+            ProductScreen.isShown(),
         ].flat(),
 });
