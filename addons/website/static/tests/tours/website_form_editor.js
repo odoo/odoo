9864196b3a6fb5c64d146369e22c2de75c4f3aa1
divy-odoo <divy@odoo.com>
2024-10-18 05:03:57 +0000

    [FIX] website: fix visibility option value for relational field
    Step to reproduce.
    1. Drag and drop a Contact Us Form
    2. Select the form action as : " Apply for Job"
    3. Select a field Type : "Activities" or "Kanban Stage"
    4. Now on other field set visibility depending on this field
    5. Now set the visibility options.
    -> Visibility value shows ID instead actual value/name.
    
    Prior to this commit, fields with conditional visibility showed the
    record's ID instead of its display name when the dependent field was a
    checkbox or radio button.
    
    This commit fix the issue by displaying the displayName of the record
    instead ID.
    
    task-4267217
    
    closes odoo/odoo#187728
    
    X-original-commit: 82715c76d2a7cc9037aa0f60df66bcc66884dcd5
    Signed-off-by: Serge Bayet (seba) <seba@odoo.com>
    Signed-off-by: Divyesh Vyas (divy) <divy@odoo.com>

--- addons/website/static/tests/tours/website_form_editor.js
+++ addons/website/static/tests/tours/website_form_editor.js
@@ -313,7 +313,25 @@ registerWebsitePreviewTour("website_form_editor_tour", {
                     ":has(.checkbox:has(label:contains('Xperia')):has(input[type='checkbox'][required]))" +
                     ":has(.checkbox:has(label:contains('Wiko Stairway')):has(input[type='checkbox'][required]))",
     },
-
+    // Check conditional visibility for the relational fields
+    ...selectButtonByData("data-set-visibility='conditional'"),
+    ...selectButtonByData("data-set-visibility-dependency='recipient_ids'"),
+    ...selectButtonByText("Is not equal to"),
+    ...selectButtonByText("Mitchell Admin"),
+    ...clickOnSave(),
+    {
+        content: "Check 'products' field is visible.",
+        trigger: `iframe .s_website_form:has(${triggerFieldByLabel("Products")}:visible)`,
+        isCheck: true,
+    }, {
+        content: "choose the option 'Mitchell Admin' of partner.",
+        trigger: "iframe .checkbox:has(label:contains('Mitchell Admin')) input[type='checkbox']",
+    }, {
+        content: "Check 'products' field is not visible.",
+        trigger: "iframe .s_website_form" +`:has(${triggerFieldByLabel("Products")}:not(:visible))`,
+        isCheck: true,
+    },
+    ...clickOnEditAndWaitEditMode(),
     ...addCustomField('selection', 'radio', 'Service', true),
     {
         content: "Change Option 1 label",
