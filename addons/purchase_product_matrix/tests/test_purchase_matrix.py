1320bdbee146d064331a25e1156a8c43b8ec4ee6
William Henrotin <whe@odoo.com>
2024-10-18 11:48:40 +0000

    [FIX] {purchase_,}product_matrix: do not create dynamic variant in matrix
    nodeValue on `input` always return null. To get the previous value of
    the input cell, the query should be
    `matrixInput.attributes.value.nodeValue`. That way unchanged cell won't
    be send to the list of data to be updated.
    
    closes odoo/odoo#185640
    
    Opw: 3987284
    X-original-commit: 19a9fcce6331ff830e6d8fdc2be29b6f2e536ec1
    Signed-off-by: Valentin Chevalier <vcr@odoo.com>
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/purchase_product_matrix/tests/test_purchase_matrix.py
+++ addons/purchase_product_matrix/tests/test_purchase_matrix.py
@@ -14,13 +14,19 @@ class TestPurchaseMatrixUi(TestMatrixCommon):
         # Ensures some dynamic create variants have been created by the matrix
         # Ensures a PO has been created with exactly x lines ...
 
-        self.assertEqual(len(self.matrix_template.product_variant_ids), 8)
+        self.assertEqual(len(self.matrix_template.product_variant_ids), 7)
         self.assertEqual(len(self.matrix_template.product_variant_ids.product_template_attribute_value_ids), 6)
         self.assertEqual(len(self.matrix_template.attribute_line_ids.product_template_value_ids), 8)
-        self.env['purchase.order.line'].search([('product_id', 'in', self.matrix_template.product_variant_ids.ids)]).order_id.button_confirm()
 
+        # check variant (PAV11, PAV21, PAV31) is not created because the two cell on it were 0
+        dyn = self.matrix_template.product_variant_ids.filtered(
+            lambda p: p.product_template_variant_value_ids.mapped('name') == ['PAV11', 'PAV21', 'PAV31']
+        )
+        self.assertFalse(dyn)
+
+        self.env['purchase.order.line'].search([('product_id', 'in', self.matrix_template.product_variant_ids.ids)]).order_id.button_confirm()
         self.matrix_template.flush_recordset()
-        self.assertEqual(round(self.matrix_template.purchased_product_qty, 2), 56.8)
+        self.assertEqual(round(self.matrix_template.purchased_product_qty, 2), 51.8)
         for variant in self.matrix_template.product_variant_ids:
             # 5 and 9.2 because of no variant attributes
             self.assertIn(round(variant.purchased_product_qty, 2), [5, 9.2])
