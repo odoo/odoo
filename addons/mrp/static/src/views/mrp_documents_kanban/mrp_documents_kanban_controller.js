d6267b7bdcffa5ce483826f60cbdbbdca24e47fb
Martin Maes <marm@odoo.com>
2024-10-18 08:56:53 +0000

    [FIX] mrp: document visible on bom
    Since https://github.com/odoo/odoo/pull/176483, the product_document_kanban_controller
    methods have been modified but in MRP. This commit fixes it.
    
    Before
    ------
    When clicking on the document icon of a bom line, and then adding a document, the
    document whill shortly appear and then disappear because the visibility is not set on
    bom https://drive.google.com/file/d/1gXiqtU3zEoFP76ue2pk-0FHnf5MyRT-P/view
    
    After
    -----
    The document visibility is set on 'bom' and the document is visible when added
    
    closes odoo/odoo#184366
    
    X-original-commit: 2b3963d1270e1be4315af1d2dcf041c82ee75d57
    Related: odoo/enterprise#72319
    Signed-off-by: Valentin Chevalier <vcr@odoo.com>

--- addons/mrp/static/src/views/mrp_documents_kanban/mrp_documents_kanban_controller.js
+++ addons/mrp/static/src/views/mrp_documents_kanban/mrp_documents_kanban_controller.js
@@ -4,10 +4,10 @@ import { patch } from "@web/core/utils/patch";
 import { ProductDocumentKanbanController } from "@product/js/product_document_kanban/product_document_kanban_controller";
 
 patch(ProductDocumentKanbanController.prototype, {
-    buildFormData(formData) {
-        super.buildFormData(formData);
+    setup() {
+        super.setup(...arguments);
         if (this.props.context.attached_on_bom) {
-            formData.append("attached_on_bom", this.props.context.bom_id);
+            this.formData.attached_on_bom = this.props.context.bom_id;
         }
     },
 });
