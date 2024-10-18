16f91cfe78020adefc8308e8e5a59153ccc3732c
jayp-odoo <jayp@odoo.com>
2024-10-18 17:36:01 +0530

    [FIX] hr_holidays, mail: IM status fa - plane not consistent
    before commit:
    
    prior to this commit fa-plane icon does not displayed after user comes
    online/offline/away while on leave without any reload.
    
    after commit:
    
    now fa-plane icon is displayed with respective color of im_status without any
    reload
    
    task-4237384
    
    closes odoo/odoo#184471
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

---
+++ addons/hr_holidays/static/tests/im_status_tests.js
@@ -0,0 +1,46 @@
+/* @odoo-module */
+
+import { startServer } from "@bus/../tests/helpers/mock_python_environment";
+
+import { start } from "@mail/../tests/helpers/test_utils";
+import { Persona } from "@mail/core/common/persona_model";
+import { Command } from "@mail/../tests/helpers/command";
+
+import { contains } from "@web/../tests/utils";
+import { patchWithCleanup } from "@web/../tests/helpers/utils";
+
+QUnit.module("im_status");
+
+QUnit.test("change icon on change partner im_status for leave variants", async () => {
+    const pyEnv = await startServer();
+    pyEnv["res.partner"].write([pyEnv.currentPartnerId], {
+        im_status: "online",
+        out_of_office_date_end: "2023-01-01",
+    });
+    const channelId = pyEnv["discuss.channel"].create({
+        channel_member_ids: [Command.create({ partner_id: pyEnv.currentPartnerId })],
+        channel_type: "chat",
+    });
+    patchWithCleanup(Persona, { IM_STATUS_DEBOUNCE_DELAY: 0 });
+    const { openDiscuss } = await start();
+    openDiscuss(channelId);
+    await contains(".o-mail-ImStatus .fa-plane[title='Online']");
+
+    pyEnv["bus.bus"]._sendone("broadcast", "bus.bus/im_status_updated", {
+        partner_id: pyEnv.currentPartnerId,
+        im_status: "offline",
+    });
+    await contains(".o-mail-ImStatus .fa-plane[title='Out of office']");
+
+    pyEnv["bus.bus"]._sendone("broadcast", "bus.bus/im_status_updated", {
+        partner_id: pyEnv.currentPartnerId,
+        im_status: "away",
+    });
+    await contains(".o-mail-ImStatus .fa-plane[title='Idle']");
+
+    pyEnv["bus.bus"]._sendone("broadcast", "bus.bus/im_status_updated", {
+        partner_id: pyEnv.currentPartnerId,
+        im_status: "online",
+    });
+    await contains(".o-mail-ImStatus .fa-plane[title='Online']");
+});
