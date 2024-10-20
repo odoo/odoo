04ae4cc2ce9156065090a89c359e57e0c1724d8b
vega-odoo <vega@odoo.com>
2024-10-20 12:59:44 +0000

    [FIX] web_editor, website_blog: new update translation controller
    In the current implementation, updating translations for a `blog.post`
    via the route
    `/web/dataset/call_kw/blog.post/web_update_field_translations`
    fails to apply changes when the post has already been translated once.
    This issue is caused by how the system fetches `website.default_lang_id`
    from the request.
    
    __Current behavior before commit:__
    When editing a previously translated field for `blog.post` or
    `product.template`, the system retrieves the website from the request to
    determine the `source_lang`. This is used to search for the original
    term in the source language and update its translation. However, due to
    the `website=True` parameter not being set for the controller,the system
    defaults to English (`source_lang = 'en_US'`). As a result:
    
    1. The system searches for the original word in English, even if it
    exists in another language.
    2. The translation update fails because the original word cannot be
    found in English.
    
    __Description of the fix:__
    A new controller was introduced to specifically handle translation
    updates, ensuring the correct language is fetched and the translation
    logic is applied correctly.
    
    __Steps to reproduce the issue:__
    1. Add 2 or more languages to the website.
    2. Set the default language to any language other than English (US).
    3. Create a blog post and write text in the default language.
    4. Switch to another language, translate the text and save.
    5. Edit the translated text again.
    The last edits are not saved.
    
    - bug introduced in: https://github.com/odoo/odoo/commit/06346b049a3a13a9a8b63a4f60151ec24fabb59e
    - opw-4239512
    
    X-original-commit: 2844a2fd97de9ead4dbf2887191999b3b845200b

--- addons/web_editor/models/models.py
+++ addons/web_editor/models/models.py
@@ -14,13 +14,3 @@ class Base(models.AbstractModel):
         keys.append('sanitize')
         keys.append('sanitize_tags')
         return keys
-
-    def web_update_field_translations(self, fname, translations):
-        field = self._fields[fname]
-        source_lang = None
-        if callable(field.translate):
-            for translation in translations.values():
-                for key, value in translation.items():
-                    translation[key] = field.translate.term_converter(value)
-            source_lang = self._get_base_lang()
-        return self._update_field_translations(fname, translations, lambda old_term: sha256(old_term.encode()).hexdigest(), source_lang=source_lang)
