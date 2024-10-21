be34ffb12a77a06ce90d29bae6778bd59fef9940
Krzysztof Magusiak (krma) <krma@odoo.com>
2024-10-21 11:59:25 +0000

    [FIX] orm: import typing.Self conditionally
    Import Self conditionally for python 3.10.
    When TYPE_CHECKING import extensions first so that static tools try to
    use it, otherwise fallback to typing.Any.

--- odoo/orm/types.py
+++ odoo/orm/types.py
@@ -7,10 +7,19 @@ from .fields import Field
 from .identifiers import IdType, NewId
 from .models import BaseModel
 from .registry import Registry
-try:
-    from typing_extensions import Self
-except ImportError:
-    from typing import Self
+
+if typing.TYPE_CHECKING:
+    # for pre python 3.11, import extensions first
+    try:
+        from typing_extensions import Self
+    except ImportError:
+        from typing import Self
+else:
+    try:
+        from typing import Self
+    except ImportError:
+        # fallback for 3.10
+        Self = typing.Any
 
 DomainType = list[str | tuple[str, str, typing.Any]]
 ContextType = Mapping[str, typing.Any]
