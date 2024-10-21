3dec554bff88673b5d1c991b998e452cd7321fad
Louis Wicket (wil) <wil@odoo.com>
2024-10-21 13:39:24 +0000

    [FIX] web: make control panel breadcrumb tooltips translatable
    Control panel breadcrumb tooltips aren't translated. This is because the
    tooltip is defined with a t-att-*, and dynamic attributes are never
    translatable.
    
    This commit redefines the tooltip in a different way that allows
    translation.
    
    opw-4160838
    
    X-original-commit: b5216f53b8aafc5fb2f5732e13ac4a1002a77d63

--- addons/web/static/src/search/control_panel/control_panel.js
+++ addons/web/static/src/search/control_panel/control_panel.js
@@ -118,6 +118,10 @@ export class ControlPanel extends Component {
         });
     }
 
+    getBreadcrumbTooltip({ name }) {
+        return _t("Back to “%s”", name);
+    }
+
     getScrollingElement() {
         return this.root.el.parentElement;
     }
