9974ceba9c91870121fa4235ee70fa22a3a93112
Tanguy Quéguineur <taqu@odoo.com>
2024-10-21 14:04:44 +0200

    [FIX] hr: fix error when accessing formview
    Fix an error introcuded by commit 2773ab7ff5855a8345eabaf52a3bb5f8b1b80235

--- addons/hr/models/hr_employee.py
+++ addons/hr/models/hr_employee.py
@@ -348,12 +348,11 @@ class HrEmployeePrivate(models.Model):
 
     def get_formview_id(self, access_uid=None):
         """ Override this method in order to redirect many2one towards the right model depending on access_uid """
+        user = self.env.user
         if access_uid:
-            self_sudo = self.with_user(access_uid)
-        else:
-            self_sudo = self
+            user = self.env['res.users'].browse(access_uid).sudo()
 
-        if self_sudo.user_has_groups('hr.group_hr_user'):
+        if user.has_group('hr.group_hr_user'):
             return super(HrEmployeePrivate, self).get_formview_id(access_uid=access_uid)
         # Hardcode the form view for public employee
         return self.env.ref('hr.hr_employee_public_view_form').id
@@ -361,12 +360,11 @@ class HrEmployeePrivate(models.Model):
     def get_formview_action(self, access_uid=None):
         """ Override this method in order to redirect many2one towards the right model depending on access_uid """
         res = super(HrEmployeePrivate, self).get_formview_action(access_uid=access_uid)
+        user = self.env.user
         if access_uid:
-            self_sudo = self.with_user(access_uid)
-        else:
-            self_sudo = self
+            user = self.env['res.users'].browse(access_uid).sudo()
 
-        if not self_sudo.user_has_groups('hr.group_hr_user'):
+        if not user.has_group('hr.group_hr_user'):
             res['res_model'] = 'hr.employee.public'
 
         return res
