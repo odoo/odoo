f86ab533f5d2a3f2a3300778822ea56017b6d709
Pedram (pebr) <pebr@odoo.com>
2024-10-22 07:17:32 +0000

    [FIX] pos_sms: use mobile number to send SMS
    Prior to this commit, the phone field was used to send SMS, which could
    be incorrect.
    
    opw-4273085
    
    X-original-commit: be4c07545d1ff9349a846c75861b59480397e8d8

--- addons/pos_sms/static/src/overrides/receipt_screen.js
+++ addons/pos_sms/static/src/overrides/receipt_screen.js
@@ -8,11 +8,61 @@ patch(ReceiptScreen.prototype, {
     showPhoneInput() {
         return super.showPhoneInput() || this.pos.config.module_pos_sms;
     },
+<<<<<<< 18.0
     actionSendReceiptOnSMS() {
         this.sendReceipt.call({
             action: "action_sent_message_on_sms",
             destination: this.state.phone,
             name: "SMS",
         });
+||||||| 7692c830bf453cd31f0520313c64da97e9c1d88a
+    actionSendReceipt() {
+        if (this.state.mode === "phone" && this.isValidPhoneNumber(this.state.input)) {
+            this.sendReceipt.call({ action: "action_sent_message_on_sms", name: "SMS" });
+        } else if (this.state.mode === "phone") {
+            this.notification.add(_t("Please enter a valid phone number"), {
+                type: "danger",
+            });
+        } else {
+            super.actionSendReceipt(...arguments);
+        }
+    },
+    changeMode(mode) {
+        if (mode !== "phone" || !this.pos.config.module_pos_sms) {
+            return super.changeMode(mode);
+        }
+
+        this.state.mode = mode;
+        this.state.input = this.currentOrder.partner_id?.phone || "";
+    },
+    get isValidInput() {
+        return this.state.mode === "phone"
+            ? this.isValidPhoneNumber(this.state.input)
+            : super.isValidInput;
+=======
+    actionSendReceipt() {
+        if (this.state.mode === "phone" && this.isValidPhoneNumber(this.state.input)) {
+            this.sendReceipt.call({ action: "action_sent_message_on_sms", name: "SMS" });
+        } else if (this.state.mode === "phone") {
+            this.notification.add(_t("Please enter a valid phone number"), {
+                type: "danger",
+            });
+        } else {
+            super.actionSendReceipt(...arguments);
+        }
+    },
+    changeMode(mode) {
+        if (mode !== "phone" || !this.pos.config.module_pos_sms) {
+            return super.changeMode(mode);
+        }
+
+        this.state.mode = mode;
+        this.state.input = this.currentOrder.partner_id?.mobile || "";
+    },
+    get isValidInput() {
+        return this.state.mode === "phone"
+            ? this.isValidPhoneNumber(this.state.input)
+            : super.isValidInput;
+>>>>>>> 0658ceeb111cae8fdc9aa1c4053ac0e46d1a6e42
     },
 });
