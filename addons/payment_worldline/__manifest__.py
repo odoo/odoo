87095c96a6e162770bda6df35c4dc69ab155c642
Levi Siuzdak (sile) <sile@odoo.com>
2024-10-22 11:25:33 +0000

    [FIX] payment_ogone: retry tx in redirect flow on 3DS auth request
    Versions
    --------
    - saas-17.4+
    
    Steps
    -----
    Try to pay with saved payment method.
    
    Issue
    -----
    > The payment has been declined: Authentication required.
    > The financial institution insists on 3-D Secure.
    
    Cause
    -----
    Bank may insist on 3-D Secure authentication, failing the payment
    transaction, which isn't handled in the `payment_token` flow.
    
    Solution
    --------
    If 3-D Secure authentication is requested, reset transaction to draft,
    and attempt redirect flow.
    
    > [!Note]
    > Doesn't work for automated payments (subscriptions).
    
    opw-4223022
    
    closes odoo/odoo#187650
    
    X-original-commit: dcb348c58a3b44241f4af08e33550d6b97d8f2fb
    Signed-off-by: Levi Siuzdak <sile@odoo.com>

--- addons/payment_worldline/__manifest__.py
+++ addons/payment_worldline/__manifest__.py
@@ -13,6 +13,11 @@
 
         'data/payment_provider_data.xml',
     ],
+    'assets': {
+        'web.assets_frontend': [
+            'payment_worldline/static/src/js/payment_form.js',
+        ],
+    },
     'post_init_hook': 'post_init_hook',
     'uninstall_hook': 'uninstall_hook',
     'license': 'LGPL-3',
