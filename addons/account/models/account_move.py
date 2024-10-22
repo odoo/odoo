a4ffa7986661af347e9ef2cfa9ceca87b33ee94b
John Laterre (jol) <jol@odoo.com>
2024-10-22 13:31:07 +0000

    [FIX] account: remove extra depreciation moves
    Steps to reproduce:
    - Install `account_audit_trail`.
    - Enable audit trail in the settings.
    - Create an asset with montly depreciation dated 3 months ago.
    - Click on "Modify Depreciation" and dispose it before the end of last month.
    
    Observed behavior:
    You will be met with a `UserError`: "The remaining value on the last depreciation line must be 0".
    
    Explaination:
    This occurs because `_reverse_moves()` tries to change the depreciation amount on the
    first available draft move. There aren't any since the method was being called right after
    removing all the draft depreciation moves.
    
    Solution:
    By adapting the logic inside `_unlink_or_reverse()`. We now distinguish 3 use cases:
    1. The move is in a hashed journal or inisde a locked period --> to be reverted.
    2. The move is protected by the audit trail --> to be cancelled.
    3. If neither of the above applies, the move is unlinked.
    
    Additionally:
    On the cancelled depreciation moves:
    - The `asset_remaining_value` will be set to 0.
    - The `asset_depreciated_value` will be set to `total_depreciable_value`.
    They will also appear in grey in the depreciation board,
    and the move name is now a link to the journal entry.
    
    task-4251586
    
    closes odoo/odoo#186052
    
    X-original-commit: cd4b18a46f8748157e767f265e777b7f4dc53422
    Related: odoo/enterprise#73165
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: John Laterre (jol) <jol@odoo.com>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -4144,18 +4144,25 @@ class AccountMove(models.Model):
         lock_date = self.company_id._get_user_fiscal_lock_date()
         return not self.inalterable_hash and self.date > lock_date
 
+    def _is_protected_by_audit_trail(self):
+        return False
+
     def _unlink_or_reverse(self):
         if not self:
             return
-        to_reverse = self.env['account.move']
         to_unlink = self.env['account.move']
+        to_cancel = self.env['account.move']
+        to_reverse = self.env['account.move']
         for move in self:
-            if move._can_be_unlinked():
-                to_unlink += move
-            else:
+            if not move._can_be_unlinked():
                 to_reverse += move
+            elif move._is_protected_by_audit_trail():
+                to_cancel += move
+            else:
+                to_unlink += move
         to_unlink.filtered(lambda m: m.state in ('posted', 'cancel')).button_draft()
         to_unlink.filtered(lambda m: m.state == 'draft').unlink()
+        to_cancel.button_cancel()
         return to_reverse._reverse_moves(cancel=True)
 
     def _post(self, soft=True):
