19f803078b7c7016fd9be7c4068350f7e83f38b2
Paolo Gatti <pgi@odoo.com>
2024-10-22 16:13:55 +0000

    [IMP] l10n_it_edi: DatiFattureCollegate must include all reconciled moves
    The `move.reversed_move_id` field only represents the credit note that the
    user has manually created to revert a move, entirely or partially, but
    it doesn't take into consideration all moves reconciled through other
    means.
    
    We now use the method `move._get_reconciled_invoices()` which goes through
    all the partials to retrieve them instead.
    
    This enables also the reconciliation with the outstanding credits case to be covered.
    
    task-3281816
    
    X-original-commit: 07e3c2ae2928b7a28ed5ba567956769837b1dac1

--- addons/l10n_it_edi/models/account_move.py
+++ addons/l10n_it_edi/models/account_move.py
@@ -546,6 +546,7 @@ class AccountMove(models.Model):
             'document_type': document_type,
             'payment_method': 'MP05',
             'downpayment_moves': downpayment_moves,
+            'reconciled_moves': self._get_reconciled_invoices(),
             'rc_refund': reverse_charge_refund,
             'conversion_rate': conversion_rate,
             'balance_multiplicator': -1 if self.is_inbound() else 1,
