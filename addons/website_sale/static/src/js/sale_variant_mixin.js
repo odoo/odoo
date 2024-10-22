8106eb78f9aae641f257296116421f38a7090ee2
Victor Feyens <vfe@odoo.com>
2024-10-22 14:01:14 +0200

    [CLN] website_sale: imports
    closes odoo/odoo#184739
    
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/website_sale/static/src/js/sale_variant_mixin.js
+++ addons/website_sale/static/src/js/sale_variant_mixin.js
@@ -1,12 +1,12 @@
 /** @odoo-module **/
 
+import { localization } from "@web/core/l10n/localization";
+import { _t } from "@web/core/l10n/translation";
+import { rpc } from "@web/core/network/rpc";
 import { KeepLast } from "@web/core/utils/concurrency";
 import { memoize, uniqueId } from "@web/core/utils/functions";
-import { throttleForAnimation } from "@web/core/utils/timing";
 import { insertThousandsSep } from "@web/core/utils/numbers";
-import { _t } from "@web/core/l10n/translation";
-import { localization } from "@web/core/l10n/localization";
-import { rpc } from "@web/core/network/rpc";
+import { throttleForAnimation } from "@web/core/utils/timing";
 
 var VariantMixin = {
     events: {
