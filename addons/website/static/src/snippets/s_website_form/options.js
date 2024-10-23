5a8e6bb603efd89633af8f741ee4688c152a4302
Benoit Socias <bso@odoo.com>
2024-10-23 07:31:59 +0000

    [FIX] website: allow empty default value for select form fields
    When model selection field is used in a form, its default value is
    always one of the available values. Because of this, if the field is
    required, it does not force the user to pick a value, but it provides a
    possibly incorrect default value.
    
    This commit makes an empty value available for selection if no default
    value is chosen.
    The behavior is also made available on custom fields, if such an empty
    value had been manually created, it will be replaced by this
    implementation.
    
    Steps to reproduce:
    - Install website_form_project
    - Add a form in a page
    - Select the "Create a Task" action
    - Add the "Analytic Account" field to the form
    - Make the field required
    
    => There was no way to have no default value by default.
    
    opw-4268164
    
    closes odoo/odoo#185495
    
    X-original-commit: a3812be90017a921df3cfeb4af48a04a75caa315
    Signed-off-by: Serge Bayet (seba) <seba@odoo.com>
    Signed-off-by: Benoit Socias (bso) <bso@odoo.com>

--- addons/website/static/src/snippets/s_website_form/options.js
+++ addons/website/static/src/snippets/s_website_form/options.js
@@ -1149,7 +1149,21 @@ options.registry.WebsiteFieldEditor = FieldEditor.extend({
      * Apply the we-list on the target and rebuild the input(s)
      */
     renderListItems: async function (previewMode, value, params) {
-        const valueList = JSON.parse(value);
+        let valueList = JSON.parse(value);
+        if (this._getSelect()) {
+            // Default entry only for fields rendered as select.
+            // Remove previous default.
+            valueList = valueList.filter(value => value.id !== "" || value.display_name !== "");
+            // Add default in first position if no default value is set.
+            const hasDefault = valueList.some(value => value.selected);
+            if (valueList.length && !hasDefault) {
+                valueList.unshift({
+                    id: "",
+                    display_name: "",
+                    selected: true,
+                });
+            }
+        }
 
         // Synchronize the possible values with the fields whose visibility
         // depends on the current field
@@ -1242,7 +1256,13 @@ options.registry.WebsiteFieldEditor = FieldEditor.extend({
             case 'toggleRequired':
                 return this.$target[0].classList.contains(params.activeValue) ? params.activeValue : 'false';
             case 'renderListItems':
-                return JSON.stringify(this._getListItems());
+                // TODO In master use a parameter.
+                this.__getListItems_forWidgetState = true;
+                try {
+                    return JSON.stringify(this._getListItems());
+                } finally {
+                    delete this.__getListItems_forWidgetState;
+                }
             case 'setVisibilityDependency':
                 return this.$target[0].dataset.visibilityDependency || '';
         }
@@ -1561,6 +1581,15 @@ options.registry.WebsiteFieldEditor = FieldEditor.extend({
         let options = [];
         if (select) {
             options = [...select.querySelectorAll('option')];
+            if (
+                this.__getListItems_forWidgetState &&
+                options.length &&
+                options[0].value === "" &&
+                options[0].textContent === "" &&
+                options[0].selected === true
+            ) {
+                options.shift();
+            }
         } else if (multipleInputs) {
             options = [...multipleInputs.querySelectorAll('.checkbox input, .radio input')];
         }
