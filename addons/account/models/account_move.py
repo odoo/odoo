3ef3bc4ae74c1a8fd82314771b729ed1a59a861c
guva-odoo <guva@odoo.com>
2024-10-23 13:08:46 +0000

    [FIX] account: move type on credit note
    When reversing a move of type 'in/out refund', we create a move
    of type 'entry' instead of 'in/out invoice'
    
    opw-4247643
    
    closes odoo/odoo#187278
    
    X-original-commit: 3df6390c14b3e61e6779c51100f5a716413950f4
    Signed-off-by: Ruben Gomes (rugo) <rugo@odoo.com>
    Signed-off-by: Guillaume Vanleynseele (guva) <guva@odoo.com>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -50,9 +50,9 @@ PAYMENT_STATE_SELECTION = [
 TYPE_REVERSE_MAP = {
     'entry': 'entry',
     'out_invoice': 'out_refund',
-    'out_refund': 'entry',
+    'out_refund': 'out_invoice',
     'in_invoice': 'in_refund',
-    'in_refund': 'entry',
+    'in_refund': 'in_invoice',
     'out_receipt': 'out_refund',
     'in_receipt': 'in_refund',
 }
