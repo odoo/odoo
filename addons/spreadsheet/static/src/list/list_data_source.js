bbb2423c1766281901345df9dcd7832bf64c717c
Pierre Rousseau <pro@odoo.com>
2024-10-23 11:37:35 +0200

    [FIX] spreadsheet: do not crash on invalid model/fields
    Before this commit, if an odoo datasource (pivot, list or chart) has an
    invalid model or field (caused for example by renaming, deletion, ...),
    some tracebacks were raised (in the formulas, while opening the side
    panel, ...)
    
    This commit fixes the issue by handling the case where the datasource
    model is invalid by showing a clear error in the cell (this commit) and
    displaying an error in the side panel (see enterprise PR)
    
    Task: 4256642

--- addons/spreadsheet/static/src/list/list_data_source.js
+++ addons/spreadsheet/static/src/list/list_data_source.js
@@ -56,6 +56,10 @@ export class ListDataSource extends OdooViewsDataSource {
         this.maxPosition = Math.max(this.maxPosition, position);
     }
 
+    isModelValid() {
+        return this._isModelValid;
+    }
+
     /**
      * @param {string} fieldName
      */
@@ -149,6 +153,12 @@ export class ListDataSource extends OdooViewsDataSource {
      * @returns {string | EvaluationError}
      */
     getListHeaderValue(fieldName) {
+        if (this.isLoading()) {
+            return LOADING_ERROR;
+        }
+        if (!this._isValid || !this._isModelValid) {
+            return this._loadError;
+        }
         if (!this.isMetaDataLoaded()) {
             this._triggerFetching();
             return LOADING_ERROR;
@@ -167,7 +177,7 @@ export class ListDataSource extends OdooViewsDataSource {
         if (this.isLoading()) {
             return LOADING_ERROR;
         }
-        if (!this._isValid) {
+        if (!this._isValid || !this._isModelValid) {
             return this._loadError;
         }
         if (position >= this.maxPositionFetched) {
