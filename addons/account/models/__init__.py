3ec260a72e1ec998e6bff1c11029f8bc2cf2c72a
Josse Colpaert <jco@odoo.com>
2024-10-23 16:53:37 +0200

    [IMP] account, l10n_es, l10n_*: country group fiscal exceptions for EU
    The important fiscal exceptions are the Canarias/Ceuta/Melilla
    for Spain and the Nederlandse Antillen for NL.
    
    By adding a field exclude_state_ids on the res_country_group we
    exclude those zones in the Intracommunity which makes those
    to be extracommunity instead.
    
    In Spain we change the domestic fiscal position to Mainland Spain,
    where we use also a special country group Mainland Spain VAT
    which has only one country Spain, but with the same exclusion states
    for Spain. We do the same for the Netherlands.
    
    We also updated translations.
    
    Task 4269927
    
    closes odoo/odoo#185571
    
    Signed-off-by: Ruben Gomes (rugo) <rugo@odoo.com>

--- addons/account/models/__init__.py
+++ addons/account/models/__init__.py
@@ -29,6 +29,7 @@ from . import account_journal_dashboard
 from . import product
 from . import company
 from . import res_config_settings
+from . import res_country_group
 from . import account_cash_rounding
 from . import account_incoterms
 from . import decimal_precision
