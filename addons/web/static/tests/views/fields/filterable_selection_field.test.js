ff83496010f00ded50cd955b7b120f5fd6ee250f
Valentin Chevalier <vcr@odoo.com>
2024-10-23 14:00:32 +0200

    [FIX] web: revert fix showing empty option when selection is required
    This reverts commit 21b0872db2ca0483955dd11ff75c5d37606560b1.
    
    This commit introduced an unexpected behaviour where the field seems to
    select by default the first choice but has no value.
    
    closes odoo/odoo#184887
    
    Signed-off-by: Valentin Chevalier <vcr@odoo.com>

--- addons/web/static/tests/views/fields/filterable_selection_field.test.js
+++ addons/web/static/tests/views/fields/filterable_selection_field.test.js
@@ -18,6 +18,8 @@ class Program extends models.Model {
 }
 defineModels([Program]);
 
+// Note: the `toHaveCount` always check for one more as there will be an invisible empty option every time.
+
 test(`FilterableSelectionField test whitelist`, async () => {
     await mountView({
         resModel: "program",
@@ -29,7 +31,7 @@ test(`FilterableSelectionField test whitelist`, async () => {
         `,
         resId: 1,
     });
-    expect(`select option`).toHaveCount(2);
+    expect(`select option`).toHaveCount(3);
     expect(`.o_field_widget[name="type"] select option[value='"coupon"']`).toHaveCount(1);
     expect(`.o_field_widget[name="type"] select option[value='"promotion"']`).toHaveCount(1);
 });
@@ -45,7 +47,7 @@ test(`FilterableSelectionField test blacklist`, async () => {
         `,
         resId: 1,
     });
-    expect(`select option`).toHaveCount(2);
+    expect(`select option`).toHaveCount(3);
     expect(`.o_field_widget[name="type"] select option[value='"coupon"']`).toHaveCount(1);
     expect(`.o_field_widget[name="type"] select option[value='"promotion"']`).toHaveCount(1);
 });
@@ -62,13 +64,13 @@ test(`FilterableSelectionField test with invalid value`, async () => {
         `,
         resId: 2,
     });
-    expect(`select option`).toHaveCount(3);
+    expect(`select option`).toHaveCount(4);
     expect(`.o_field_widget[name="type"] select option[value='"gift_card"']`).toHaveCount(1);
     expect(`.o_field_widget[name="type"] select option[value='"coupon"']`).toHaveCount(1);
     expect(`.o_field_widget[name="type"] select option[value='"promotion"']`).toHaveCount(1);
 
     await contains(`.o_field_widget[name="type"] select`).select(`"coupon"`);
-    expect(`select option`).toHaveCount(2);
+    expect(`select option`).toHaveCount(3);
     expect(`.o_field_widget[name="type"] select option[value='"gift_card"']`).toHaveCount(0);
     expect(`.o_field_widget[name="type"] select option[value='"coupon"']`).toHaveCount(1);
     expect(`.o_field_widget[name="type"] select option[value='"promotion"']`).toHaveCount(1);
