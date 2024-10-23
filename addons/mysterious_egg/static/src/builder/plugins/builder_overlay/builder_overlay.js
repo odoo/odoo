b9b4729e48b6e36b2b4682b299ce19d6e1a0e0a6
fdardenne <dafl@odoo.com>
2024-10-23 16:16:02 +0200

    builder_overlay displays paddings

--- addons/mysterious_egg/static/src/builder/plugins/builder_overlay/builder_overlay.js
+++ addons/mysterious_egg/static/src/builder/plugins/builder_overlay/builder_overlay.js
@@ -16,7 +16,38 @@ export class BuilderOverlay extends Component {
             onPositioned: () => {
                 this.size.height = this.props.target.clientHeight;
                 this.size.width = this.props.target.clientWidth;
+                this.size.paddingBottom = window
+                    .getComputedStyle(this.props.target)
+                    .getPropertyValue("padding-bottom");
+                this.size.paddingTop = window
+                    .getComputedStyle(this.props.target)
+                    .getPropertyValue("padding-top");
             },
         });
     }
+
+    // getSize() {
+    //     var nClass = "pt";
+    //     var nProp = "padding-top";
+    //     var sClass = "pb";
+    //     var sProp = "padding-bottom";
+    //     debugger;
+    //     if (this.$target.is("hr")) {
+    //         nClass = "mt";
+    //         nProp = "margin-top";
+    //         sClass = "mb";
+    //         sProp = "margin-bottom";
+    //     }
+
+    //     var grid = [];
+    //     for (var i = 0; i <= 256 / 8; i++) {
+    //         grid.push(i * 8);
+    //     }
+    //     grid.splice(1, 0, 4);
+    //     this.grid = {
+    //         n: [grid.map((v) => nClass + v), grid, nProp],
+    //         s: [grid.map((v) => sClass + v), grid, sProp],
+    //     };
+    //     return this.grid;
+    // }
 }
