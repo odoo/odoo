be7b2308498bfc57089ebca76582fafbe144dd50
bram1000 <brvg@odoo.com>
2024-10-24 13:22:25 +0200

    [ADD] auth_signup_recaptcha
    Before this commit captcha was not enabled on signup and reset password
    requests. This meant that a lot of emails could be sent by automated
    means, causing issues for clients.
    
    Task-3626220

--- addons/auth_signup/static/src/js/signup.js
+++ addons/auth_signup/static/src/js/signup.js
@@ -17,7 +17,9 @@ publicWidget.registry.SignUpForm = publicWidget.Widget.extend({
      */
     _onSubmit: function () {
         var $btn = this.$('.oe_login_buttons > button[type="submit"]');
-        $btn.attr('disabled', 'disabled');
-        $btn.prepend('<i class="fa fa-refresh fa-spin"/> ');
+        if(!$btn.prop('disabled')) {
+            $btn.attr('disabled', 'disabled');
+            $btn.prepend('<i class="fa fa-refresh fa-spin"/> ');
+        }
     },
 });
