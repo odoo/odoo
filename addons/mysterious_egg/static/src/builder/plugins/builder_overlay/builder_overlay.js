d84997c0e068d7082afcbfb3142652d547fb71ef
fdardenne <dafl@odoo.com>
2024-10-24 13:48:05 +0200

    wip resize

--- addons/mysterious_egg/static/src/builder/plugins/builder_overlay/builder_overlay.js
+++ addons/mysterious_egg/static/src/builder/plugins/builder_overlay/builder_overlay.js
@@ -1,74 +1,140 @@
 import { Component, useRef, useState } from "@odoo/owl";
 import { usePosition } from "@web/core/position/position_hook";
-import { useDraggable } from "@web/core/utils/draggable";
+import { makeDraggableHook } from "@web/core/utils/draggable_hook_builder_owl";
+import { pick } from "@web/core/utils/objects";
+
+const useDraggableWithoutFollow = makeDraggableHook({
+    name: "useDraggable",
+    onComputeParams: ({ ctx }) => {
+        ctx.followCursor = false;
+    },
+    onWillStartDrag: ({ ctx }) => pick(ctx.current, "element"),
+    onDragStart: ({ ctx }) => pick(ctx.current, "element"),
+    onDrag: ({ ctx }) => pick(ctx.current, "element"),
+    onDragEnd: ({ ctx }) => pick(ctx.current, "element"),
+    onDrop: ({ ctx }) => pick(ctx.current, "element"),
+});
 
 export class BuilderOverlay extends Component {
     static template = "mysterious_egg.BuilderOverlay";
     setup() {
         this.overlay = useRef("overlay");
+        this.target = this.props.target;
         this.size = useState({
-            height: this.props.target.clientHeight,
-            width: this.props.target.clientWidth,
+            height: this.target.clientHeight,
+            width: this.target.clientWidth,
         });
-
-        usePosition("root", () => this.props.target, {
+        usePosition("root", () => this.target, {
             position: "center",
             container: () => this.props.container,
             onPositioned: () => {
-                this.size.height = this.props.target.clientHeight;
-                this.size.width = this.props.target.clientWidth;
+                this.size.height = this.target.clientHeight;
+                this.size.width = this.target.clientWidth;
                 this.size.paddingBottom = window
-                    .getComputedStyle(this.props.target)
+                    .getComputedStyle(this.target)
                     .getPropertyValue("padding-bottom");
                 this.size.paddingTop = window
-                    .getComputedStyle(this.props.target)
+                    .getComputedStyle(this.target)
                     .getPropertyValue("padding-top");
             },
         });
         //WIP
-        useDraggable({
+        this.currentDraggable = {
+            initialX: 0,
+            initialY: 0,
+            direction: "",
+        };
+        useDraggableWithoutFollow({
             ref: { el: window.document.body },
             elements: ".o_handle",
-            onWillStartDrag: () => {
-                console.log("onWillStartDrag", arguments);
+            onWillStartDrag: ({ x, y, element }) => {
+                this.currentDraggable.intialX = x;
+                this.currentDraggable.intialY = y;
+                this.currentDraggable.direction = this.getCurrentDirection(element);
+                this.currentDraggable.sizes = this.getSize()[this.currentDraggable.direction];
+
+                // Find the index of the current padding class applied to the target
+                const paddingDirection = this.currentDraggable.sizes[2];
+                for (let i = 0; i < this.currentDraggable.sizes[0].length; i++) {
+                    const paddingClass = this.currentDraggable.sizes[0][i];
+                    const paddingValue = this.currentDraggable.sizes[1][i];
+                    if (this.target.classList.contains(paddingClass)) {
+                        this.currentDraggable.currentSizeIndex = i;
+                    } else if (
+                        window.getComputedStyle(this.target).getPropertyValue(paddingDirection) ===
+                        paddingValue + "px"
+                    ) {
+                        this.currentDraggable.currentSizeIndex = i;
+                    }
+                }
             },
-            onDragStart: ({ element }) => {
-                console.log("onDragStart", arguments);
+            onDragStart: (test) => {
+                console.log("onDragStart", test);
             },
-            onDrag: ({ x, y, element }) => {
-                console.log("onDrag", arguments);
+            onDrag: ({ x, y }) => {
+                console.log("onDrag", x, y);
+                const deltaX = x - this.currentDraggable.initialX;
+                const deltaY = y - this.currentDraggable.initialY;
+                const currentSizeIndex = this.currentDraggable.currentSizeIndex;
+                // const nextSizeIndex = currentSizeIndex + (deltaX > 0 ? 1 : -1);
+                    // const dd = ev['page' + dir.XY] - dir.xy + dir.resize[1][dir.begin];
+                    // const next = dir.current + (dir.current + 1 === dir.resize[1].length ? 0 : 1);
+                    // const prev = dir.current ? (dir.current - 1) : 0;
             },
-            onDrop: ({ element }) => {
-                console.log("onDrop", arguments);
+            onDrop: (test) => {
+                console.log("onDrop", test);
             },
-            onDragEnd: ({ element }) => {
-                console.log("onDragEnd", arguments);
+            onDragEnd: (test) => {
+                console.log("onDragEnd", test);
             },
         });
     }
 
-    // getSize() {
-    //     var nClass = "pt";
-    //     var nProp = "padding-top";
-    //     var sClass = "pb";
-    //     var sProp = "padding-bottom";
-    //     debugger;
-    //     if (this.$target.is("hr")) {
-    //         nClass = "mt";
-    //         nProp = "margin-top";
-    //         sClass = "mb";
-    //         sProp = "margin-bottom";
-    //     }
+    getSize() {
+        var nClass = "pt";
+        var nProp = "padding-top";
+        var sClass = "pb";
+        var sProp = "padding-bottom";
 
-    //     var grid = [];
-    //     for (var i = 0; i <= 256 / 8; i++) {
-    //         grid.push(i * 8);
-    //     }
-    //     grid.splice(1, 0, 4);
-    //     this.grid = {
-    //         n: [grid.map((v) => nClass + v), grid, nProp],
-    //         s: [grid.map((v) => sClass + v), grid, sProp],
-    //     };
-    //     return this.grid;
-    // }
+        if (this.target.tagName === "HR") {
+            nClass = "mt";
+            nProp = "margin-top";
+            sClass = "mb";
+            sProp = "margin-bottom";
+        }
+
+        var grid = [];
+        for (var i = 0; i <= 256 / 8; i++) {
+            grid.push(i * 8);
+        }
+        grid.splice(1, 0, 4);
+        this.grid = {
+            n: [grid.map((v) => nClass + v), grid, nProp],
+            s: [grid.map((v) => sClass + v), grid, sProp],
+        };
+        return this.grid;
+    }
+
+    getCurrentDirection(handleElement) {
+        const handleClasses = handleElement.classList;
+        if (handleClasses.contains("n")) {
+            return "n";
+        } else if (handleClasses.contains("s")) {
+            return "s";
+        } else if (handleClasses.contains("e")) {
+            return "e";
+        } else if (handleClasses.contains("w")) {
+            return "w";
+        } else if (handleClasses.contains("nw")) {
+            return "nw";
+        } else if (handleClasses.contains("ne")) {
+            return "ne";
+        } else if (handleClasses.contains("sw")) {
+            return "sw";
+        } else if (handleClasses.contains("se")) {
+            return "se";
+        } else {
+            return "";
+        }
+    }
 }
