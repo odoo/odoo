2992821009623c4977674a74a76d73d074a06a80
paru-odoo <paru@odoo.com>
2024-10-24 14:50:44 +0530

    [IMP] website: trial code

---
+++ addons/website/static/src/js/website_signup_form.js
@@ -0,0 +1,24 @@
+import options from "@web_editor/js/editor/snippets.options";
+
+options.registry.WebsiteSignupFormEditor = options.Class.extend({
+    async start() {
+    },
+});
+
+options.registry.AddSignupFieldForm = options.Class.extend({
+    async start() {
+    },
+});
+
+options.registry.AddSignupField = options.Class.extend({
+    async start() {
+    },
+});
+options.registry.WebsiteSignupFormFieldRequired = options.Class.extend({
+    async start() {
+    },
+});
+options.registry.WebsiteSignupFieldEditor = options.Class.extend({
+    async start() {
+    },
+});
