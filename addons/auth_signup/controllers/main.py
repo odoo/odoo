4650fff0d3741773fd2c086992652a75223230f3
bram1000 <brvg@odoo.com>
2024-10-24 11:22:25 +0000

    [IMP] captcha: enable on registration and password reset
    Before this commit, Odoo instances were being spammed by requests that
    caused emails to be sent out. Primarily in the signup and forgot
    password feature. This is the backwards compatibile commit for the
    master commit located at https://github.com/odoo/odoo/pull/185648.
    
    Task-3626220
    
    X-original-commit: 7ec74bcc692fc1c0d89fbeb243eef31685996784

--- addons/auth_signup/controllers/main.py
+++ addons/auth_signup/controllers/main.py
@@ -41,6 +41,9 @@ class AuthSignupHome(Home):
 
         if 'error' not in qcontext and request.httprequest.method == 'POST':
             try:
+                if not request.env['ir.http']._verify_request_recaptcha_token('signup'):
+                    raise UserError(_("Suspicious activity detected by Google reCaptcha."))
+
                 self.do_signup(qcontext)
 
                 # Set user to public if they were not signed in by do_signup
@@ -86,6 +89,8 @@ class AuthSignupHome(Home):
 
         if 'error' not in qcontext and request.httprequest.method == 'POST':
             try:
+                if not request.env['ir.http']._verify_request_recaptcha_token('password_reset'):
+                    raise UserError(_("Suspicious activity detected by Google reCaptcha."))
                 if qcontext.get('token'):
                     self.do_signup(qcontext)
                     return self.web_login(*args, **kw)
