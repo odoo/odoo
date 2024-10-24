62423d774ba2edacc06ffc87a3427f4cb37739e7
paru-odoo <paru@odoo.com>
2024-10-24 14:50:44 +0530

    [IMP] auth_signup,website: make signup form editable on website install
    The signup form is now editable, letting users add custom fields easily.
    Existing fields are stored in res_users, while custom fields are logged
    in the chatter of res.partner, enhancing traceability and customization.
    
    Key changes:
    1. Allow adding both custom and existing fields.
    2. Enable field position changes and duplication for custom fields.
    3. Treat Name, Email, and Password as technical fields—these are
       restricted and cannot be deleted.
    4. Introduce a new `signup_form` action (not displayed in other forms).
    5. Log custom fields (not in `res.users`) as comments in the
       `res.partner` model.
    6. Hide the On Success button and URL input from the form.
    
    task-3857565

--- addons/website/static/src/snippets/s_website_form/form.js
+++ addons/website/static/src/snippets/s_website_form/form.js
@@ -358,9 +358,12 @@ export class Form extends Interaction {
         for (const [key, value] of Object.entries(formValues)) {
             formData.append(key, value);
         }
-
+        let actionUrl = this.el.getAttribute("action");
+        if (this.el.dataset.model_name !== "res.users") {
+            actionUrl += (this.el.dataset.force_action || this.el.dataset.model_name);
+        }
         // Post form and handle result
-        post(this.el.getAttribute("action") + (this.el.dataset.force_action || this.el.dataset.model_name), formData)
+        post(actionUrl, formData)
             .then(async (resultData) => {
                 // Restore send button behavior
                 buttonEl.removeAttribute("disabled");
