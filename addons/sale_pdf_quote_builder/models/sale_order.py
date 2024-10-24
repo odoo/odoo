69064c119b823cd9a9539a9de81483bf0b5a7575
Louis (loti) <loti@odoo.com>
2024-10-24 15:34:10 +0200

    [FIX] sale_pdf_quote_builder: fix multi-company behavior of quotation documents
    A quotation document can be linked either to a single company, or to all
    companies. However, this information was ignored, and all quotation documents
    were available in all companies, regardless of their linked companies.
    
    This change enforces consistent multi-company behavior, i.e. quotation documents
    are only available in their linked companies.
    
    opw-4087354

--- addons/sale_pdf_quote_builder/models/sale_order.py
+++ addons/sale_pdf_quote_builder/models/sale_order.py
@@ -20,6 +20,7 @@ class SaleOrder(models.Model):
         string="Headers/Footers",
         comodel_name='quotation.document',
         readonly=False,
+        check_company=True,
     )
     customizable_pdf_form_fields = fields.Json(
         string="Customizable PDF Form Fields",
@@ -32,7 +33,8 @@ class SaleOrder(models.Model):
     def _compute_available_product_document_ids(self):
         for order in self:
             order.available_product_document_ids = self.env['quotation.document'].search(
-                [], order='sequence'
+                self.env['quotation.document']._check_company_domain(self.company_id),
+                order='sequence',
             ).filtered(lambda doc:
                 self.sale_order_template_id in doc.quotation_template_ids
                 or not doc.quotation_template_ids
