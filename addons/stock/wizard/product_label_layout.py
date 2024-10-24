a9f241f75a57cc3cd2041af237eeef5ec58b45d0
Lulu Grimalkin (lugr) <lugr@odoo.com>
2024-10-24 10:54:32 +0200

    [IMP] product, stock: merge unpriced product labels, add base prices
    Priced and unpriced product label formats were merged to avoid
    duplicates. The option to print the price is now a separate option in
    the view. The pricelist field is hidden from the view if the price
    isn't set to be printed. Additionally, it is now possible to print the
    price on every product label format, as well as the base unit price,
    if enabled and set.
    
    Additionally, the ZPL product label templates were made to be generic
    enough to work without `stock` and were moved to `product`.
    
    Task ID: 4277792

--- addons/stock/wizard/product_label_layout.py
+++ addons/stock/wizard/product_label_layout.py
@@ -2,50 +2,19 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from collections import defaultdict
-import requests
-import base64
-from odoo import api, fields, models
+from odoo import fields, models
 from odoo.tools import float_compare, float_is_zero
-from odoo.tools.misc import file_open
-
-# Format table expressed as width x height in inch.
-ZPL_FORMAT_SIZE = {
-    'normal': (2.25, 1.25),
-    'small': (1.25, 1.00),
-    'alternative': (2.00, 1.00),
-    'jewelry': (2.20, 0.50),
-}
 
 class ProductLabelLayout(models.TransientModel):
     _inherit = 'product.label.layout'
-
-    @api.model
-    def _get_zpl_label_placeholder(self):
-        return base64.b64encode(file_open('stock/static/img/zpl_label_placeholder.png', 'rb').read())
 
     move_ids = fields.Many2many('stock.move')
     move_quantity = fields.Selection([
         ('move', 'Operation Quantities'),
         ('custom', 'Custom')], string="Quantity to print", required=True, default='custom')
-    print_format = fields.Selection(selection_add=[
-        ('zpl', 'ZPL Labels'),
-        ('zplxprice', 'ZPL Labels with price')
-    ], ondelete={'zpl': 'set default', 'zplxprice': 'set default'})
-    zpl_template = fields.Selection([
-        ('normal', 'Normal (2.25" x 1.25")'),
-        ('small', 'Small (1.25" x 1.00")'),
-        ('alternative', 'Alternative (2.00" x 1.00")'),
-        ('jewelry', 'Jewelry (2.20" x 0.50")'),
-    ], string="ZPL Template", default='normal', required=True)
-    zpl_preview = fields.Image('ZPL Preview', readonly=True, default=_get_zpl_label_placeholder)
 
     def _prepare_report_data(self):
         xml_id, data = super()._prepare_report_data()
-
-        if 'zpl' in self.print_format:
-            xml_id = 'stock.label_product_product'
-            data['zpl_template'] = self.zpl_template
-
         quantities = defaultdict(int)
         uom_unit = self.env.ref('uom.product_uom_unit', raise_if_not_found=False)
         if self.move_quantity == 'move' and self.move_ids and all(float_is_zero(ml.quantity, precision_rounding=ml.product_uom_id.rounding) for ml in self.move_ids.move_line_ids):
