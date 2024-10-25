faddb5a112887dde92184d16588409d3ffe8c6be
qsm-odoo <qsm@odoo.com>
2024-10-25 18:41:48 +0200

    [FIX] website: properly pause auto-cycling on carousel drag and drop
    Follow-up of [1] which was not enough to fully fix the issue.
    
    Indeed, there was a remaining call to `.carousel("pause")` in an editor
    option `start` that funnily enough actually unpaused the carousel.
    Indeed, it created the carousel bootstrap instance, paused... but meant
    to restart on first mouseleave.
    
    The `slider` public widget is in charge of pausing the carousel in edit
    mode. And it does so by preventing it to cycle for the whole edit mode
    duration. But because of that editor option code, it happened too late.
    
    [1]: https://github.com/odoo/odoo/commit/9eff9ae1904e0583f5dc60c6d925c52b48b208ec
    
    closes odoo/odoo#185355
    
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/website/static/src/js/editor/snippets.options.js
+++ addons/website/static/src/js/editor/snippets.options.js
@@ -2022,7 +2022,6 @@ options.registry.Carousel = options.registry.CarouselHandler.extend({
      * @override
      */
     start: function () {
-        this.$bsTarget.carousel('pause');
         this.$indicators = this.$target.find('.carousel-indicators');
         this.$controls = this.$target.find('.carousel-control-prev, .carousel-control-next, .carousel-indicators');
 
