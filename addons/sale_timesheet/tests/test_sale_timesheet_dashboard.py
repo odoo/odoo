f6bbeec5429936e1d82f023e217e953e7be0351a
K Theja (kthe) <kthe@odoo.com>
2024-10-25 16:12:37 +0530

    [IMP] project, sale_(*): Added back the SOL display in rightside panel to use display_name
    _(*) = project, timesheet
    
    After this commit SOL name in project update revenues will use display_name
    instead of name because of differentiating between the SOL's.Fixup of all
    test cases related.
    
    task-4190761
    
    Part-of: odoo/odoo#185436
    Related: odoo/enterprise#72860
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/sale_timesheet/tests/test_sale_timesheet_dashboard.py
+++ addons/sale_timesheet/tests/test_sale_timesheet_dashboard.py
@@ -28,7 +28,7 @@ class TestSaleTimesheetDashboard(Common):
     def test_get_sale_item_data_various_sol_with_timesheet_installed(self):
         """This test ensures that when the timesheet module is installed, the sols are computed and put into the new profitability sections."""
 
-        sol_service_1, sol_service_2, sol_service_3, sol_service_4, sol_service_5 = self.dashboardSaleOrderLine.create([{
+        sols = self.dashboardSaleOrderLine.create([{
             'product_id': self.product_milestone.id,
             'product_uom_qty': 1,
         }, {
@@ -44,28 +44,32 @@ class TestSaleTimesheetDashboard(Common):
             'product_id': self.dashboard_product_delivery_service.id,
             'product_uom_qty': 1,
         }])
+
         sale_item_data = self.dashboard_project.get_sale_items_data(with_action=False, limit=5, section_id='materials')
+        expected_dict = sols._read_format(
+            ['display_name', 'product_uom_qty', 'qty_delivered', 'qty_invoiced', 'product_uom_id', 'product_id']
+        )
         self.assertEqual(
-            sale_item_data['sol_items'],
-            [{'id': sol_service_3.id, 'name': 'Material', 'product_uom_qty': 1.0, 'qty_delivered': 0.0, 'qty_invoiced': 0.0, 'product_uom_id': (1, 'Units'), 'product_id': (self.material_product.id, 'Material')}]
+            sale_item_data['sol_items'][0],
+            expected_dict[2]
         )
         sale_item_data = self.dashboard_project.get_sale_items_data(with_action=False, limit=5, section_id='billable_fixed')
         self.assertEqual(
-            sale_item_data['sol_items'],
-            [{'id': sol_service_2.id, 'name': '[SERV-ORDERED2] Product prepaid', 'product_uom_qty': 1.0, 'qty_delivered': 0.0, 'qty_invoiced': 0.0, 'product_uom_id': (4, 'Hours'), 'product_id': (self.product_prepaid.id, '[SERV-ORDERED2] Product prepaid')}]
+            sale_item_data['sol_items'][0],
+            expected_dict[1]
         )
         sale_item_data = self.dashboard_project.get_sale_items_data(with_action=False, limit=5, section_id='billable_milestones')
         self.assertEqual(
-            sale_item_data['sol_items'],
-            [{'id': sol_service_1.id, 'name': '[SERV-ORDERED2] Service Milestone', 'product_uom_qty': 1.0, 'qty_delivered': 0.0, 'qty_invoiced': 0.0, 'product_uom_id': (4, 'Hours'), 'product_id': (self.product_milestone.id, '[SERV-ORDERED2] Service Milestone')}]
+            sale_item_data['sol_items'][0],
+            expected_dict[0]
         )
         sale_item_data = self.dashboard_project.get_sale_items_data(with_action=False, limit=5, section_id='billable_time')
         self.assertEqual(
-            sale_item_data['sol_items'],
-            [{'id': sol_service_4.id, 'name': '[SERV-DELI1] Service delivered', 'product_uom_qty': 1.0, 'qty_delivered': 0.0, 'qty_invoiced': 0.0, 'product_uom_id': (4, 'Hours'), 'product_id': (self.dashboard_product_delivery_timesheet.id, '[SERV-DELI1] Service delivered')}]
+            sale_item_data['sol_items'][0],
+            expected_dict[3]
         )
         sale_item_data = self.dashboard_project.get_sale_items_data(with_action=False, limit=5, section_id='billable_manual')
         self.assertEqual(
-            sale_item_data['sol_items'],
-            [{'id': sol_service_5.id, 'name': '[SERV-ORDERED2] Service Delivery', 'product_uom_qty': 1.0, 'qty_delivered': 0.0, 'qty_invoiced': 0.0, 'product_uom_id': (4, 'Hours'), 'product_id': (self.dashboard_product_delivery_service.id, '[SERV-ORDERED2] Service Delivery')}]
+            sale_item_data['sol_items'][0],
+            expected_dict[4]
         )
