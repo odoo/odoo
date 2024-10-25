odoo/odoo

commit 50b28c06c66f2eaebfb30e4cf195a8c79c0ecced

[PERF] spreadsheet_dashboard: handcraft json response body
This commit changes the way dashboard data is loaded server-side.
Some part are overwritten in Enterprise to make it blazing fast
(but the API is the same in both community and enterprise).
We can't apply the same trick here as in enterprise because we
need to update the dashboard locale, for which we need to parse
the dashboard json file anyway.

With the enterprise optimisation:

Server-side load time for a 16.7Mb spreadsheet:

**before: 606ms**  (`join_spreadsheet_session`)
**after: 83ms** (-86%!) (new `/spreadsheet/dashboard/data/<...>` controller)

Previously, `join_spreadsheet_session` returned a combination of:

- The full spreadsheet snapshot (JSON file on the filestore)
- Pending revision data (also serialized JSON, stored in a Char field)
- Additional metadata

These were combined into a Python dict and re-serialized into JSON for the
response. This process is inefficient as it involved unnecessary parsing and
re-serialization of already serialized JSON data.

With this commit, we handcraft the json response body, avoiding the need for
parsing and re-serializing large json objects, resulting in significant
performance improvements in both CPU and memory.

To have control of the response body, we now have to use an http controller
instead of an RPC method.

We could also have sent the raw/serialized data from
`join_spreadsheet_session` but then it would be serialized json over json
which has several drawbacks:

- reduces client-side debuggability
- introduce a lot of additional escape characters (e.g. `\"`)
- the client would need to explicitly parse the nested json

With this solution, the client receives a clean, valid json content type

Alternative idea: we could stream the snapshot file directly from the file
store to be even more efficient. But then we would need to load the
additional metadata and revisions with another http request. As those two

addons/spreadsheet/utils/json.py
@@ -1,0 +1,17 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+def extend_serialized_json(json: str, key_value_pairs: list) -> str:
+    """
+    Add key-value pairs to a serialized JSON object string.
+    value should be already serialized.
+    """
+    # avoid copying strings as much as possible for performance reasons
+    parts = [json.rstrip('}')]
+    if json != '{}':
+        parts.append(',')
+    for i, (key, value) in enumerate(key_value_pairs):
+        parts.extend([f'"{key}":', value])
+        if i != len(key_value_pairs) - 1:
+            parts.append(',')
+    parts.append('}')
+    return ''.join(parts)

addons/spreadsheet/utils/__init__.py
@@ -1,2 +1,3 @@
 from . import formatting
 from . import validate_data
+from . import json

addons/spreadsheet_dashboard/controllers/__init__.py
@@ -1,1 +1,2 @@
 from . import share
+from . import dashboards_controllers

addons/spreadsheet_dashboard/tests/__init__.py
@@ -1,4 +1,5 @@
 from . import test_spreadsheet_dashboard
 from . import test_dashboard_share
+from . import test_dashboard_controllers
 from . import test_share_dashboard_tour
 from . import test_share_controllers

addons/spreadsheet/static/src/helpers/model.js
@@ -19,9 +19,9 @@ import {
  */
 
 export async function fetchSpreadsheetModel(env, resModel, resId) {
-    const { data, revisions } = await env.services.orm.call(resModel, "join_spreadsheet_session", [
-        resId,
-    ]);
+    const { data, revisions } = await env.services.http.get(
+        `/spreadsheet/data/${resModel}/${resId}`
+    );
     return createSpreadsheetModel({ env, data, revisions });
 }
 
@@ -116,13 +116,20 @@ export async function freezeOdooData(model) {
                     for (let row = top; row <= bottom; row++) {
                         for (let col = left; col <= right; col++) {
                             const xc = toXC(col, row);
-                            const evaluatedCell = model.getters.getEvaluatedCell({ sheetId, col, row });
+                            const evaluatedCell = model.getters.getEvaluatedCell({
+                                sheetId,
+                                col,
+                                row,
+                            });
                             sheet.cells[xc] = {
                                 ...sheet.cells[xc],
                                 content: evaluatedCell.value.toString(),
                             };
                             if (evaluatedCell.format) {
-                                sheet.cells[xc].format = getItemId(evaluatedCell.format, data.formats);
+                                sheet.cells[xc].format = getItemId(
+                                    evaluatedCell.format,
+                                    data.formats
+                                );
                             }
                         }
                     }

addons/spreadsheet/static/tests/helpers/data.js
@@ -4,10 +4,12 @@ import {
     defineModels,
     fields,
     models,
+    onRpc,
     serverState,
     webModels,
 } from "@web/../tests/web_test_helpers";
 import { mailModels } from "@mail/../tests/mail_test_helpers";
+import { RPCError } from "@web/core/network/rpc";
 
 /**
  * @typedef {object} ServerData
@@ -119,6 +121,27 @@ export function getBasicListArchs() {
         "partner,false,form": /* xml */ `<form/>`,
     };
 }
+
+function mockSpreadsheetDataController(request) {
+    const parts = request.url.split("/");
+    const resModel = parts.at(-2);
+    const resId = parseInt(parts.at(-1));
+    const record = this.env[resModel].search_read([["id", "=", resId]])[0];
+    if (!record) {
+        const error = new RPCError(`Spreadsheet ${resId} does not exist`);
+        error.data = {};
+        throw error;
+    }
+    return {
+        data: JSON.parse(record.spreadsheet_data),
+        name: record.name,
+        revisions: [],
+        isReadonly: false,
+        writable_rec_name_field: "name",
+    };
+}
+
+onRpc("/spreadsheet/data/*", mockSpreadsheetDataController, { pure: true });
 
 export function defineSpreadsheetModels() {
     defineModels(SpreadsheetModels);

addons/spreadsheet_dashboard/controllers/dashboards_controllers.py
@@ -1,0 +1,28 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import http
+from odoo.http import request
+
+
+class DashboardDataRoute(http.Controller):
+    @http.route(['/spreadsheet/dashboard/data/<model("spreadsheet.dashboard"):dashboard>'], type='http', auth='user')
+    def get_dashboard_data(self, dashboard):
+        dashboard = dashboard.exists()
+        if not dashboard:
+            raise request.not_found()
+        cids_str = request.cookies.get('cids', str(request.env.user.company_id.id))
+        cids = [int(cid) for cid in cids_str.split('-')]
+        dashboard = dashboard.with_context(allowed_company_ids=cids)
+        if dashboard._dashboard_is_empty() and dashboard.sample_dashboard_file_path:
+            sample_data = dashboard._get_sample_dashboard()
+            if sample_data:
+                return request.make_json_response({
+                    'snapshot': sample_data,
+                    'is_sample': True,
+                })
+        body = dashboard._get_serialized_readonly_dashboard()
+        headers = [
+            ('Content-Length', len(body)),
+            ('Content-Type', 'application/json; charset=utf-8'),
+        ]
+        return request.make_response(body, headers)

addons/spreadsheet_dashboard/models/spreadsheet_dashboard.py
@@ -43,24 +43,16 @@ class SpreadsheetDashboard(models.Model):
         else:
             self.sudo().favorite_user_ids = [Command.link(current_user_id)]
 
-    def get_readonly_dashboard(self):
-        self.ensure_one()
+    def _get_serialized_readonly_dashboard(self):
         snapshot = json.loads(self.spreadsheet_data)
-        if self._dashboard_is_empty() and self.sample_dashboard_file_path:
-            sample_data = self._get_sample_dashboard()
-            if sample_data:
-                return {
-                    "snapshot": sample_data,
-                    "is_sample": True,
-                }
         user_locale = self.env['res.lang']._get_user_spreadsheet_locale()
         snapshot.setdefault('settings', {})['locale'] = user_locale
         default_currency = self.env['res.currency'].get_company_currency_for_spreadsheet()
-        return {
+        return json.dumps({
             'snapshot': snapshot,
             'revisions': [],
             'default_currency': default_currency,
-        }
+        })
 
     def _get_sample_dashboard(self):
         try:

commit 91228370b7c55d82ce224a0f6d8c32e2c97047d6

[FIX] spreadsheet: fix handcrafted json
If a spreadsheet json ends with two closing curly brackets `}}`
`extend_serialized_json` would remove both `}` leading to an invalid
serialized json.

Bug introduced by 50b28c06c66f2eaebfb30e4cf195a8c79c0ecced

closes odoo/odoo#185072

Task: 4282874
Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>

addons/spreadsheet/utils/json.py
@@ -6,7 +6,7 @@ def extend_serialized_json(json: str, key_value_pairs: list) -> str:
     value should be already serialized.
     """
     # avoid copying strings as much as possible for performance reasons
-    parts = [json.rstrip('}')]
+    parts = [json.removesuffix('}')]
     if json != '{}':
         parts.append(',')
     for i, (key, value) in enumerate(key_value_pairs):

addons/spreadsheet/tests/__init__.py
@@ -5,3 +5,4 @@ from . import test_currency_rate
 from . import test_display_names
 from . import test_locale
 from . import test_session_info
+from . import test_utils

addons/spreadsheet/tests/test_utils.py
@@ -1,0 +1,39 @@
+from odoo.tests.common import TransactionCase
+
+from odoo.addons.spreadsheet.utils.json import extend_serialized_json
+
+
+class TestSpreadsheetUtils(TransactionCase):
+
+    def test_extend_serialized_json(self):
+        self.assertEqual(extend_serialized_json('{}', []), '{}')
+        self.assertEqual(extend_serialized_json('{}', [('key', '{}')]), '{"key":{}}')
+        self.assertEqual(extend_serialized_json('{}', [('key', '[]')]), '{"key":[]}')
+        self.assertEqual(
+            extend_serialized_json('{}', [('key', '"value"')]),
+            '{"key":"value"}'
+        )
+        self.assertEqual(
+            extend_serialized_json('{"a": 1}', [('key', '"value"')]),
+            '{"a": 1,"key":"value"}'
+        )
+        self.assertEqual(
+            extend_serialized_json('{"a": 1}', [('key', '{"b": 2}')]),
+            '{"a": 1,"key":{"b": 2}}'
+        )
+        self.assertEqual(
+            extend_serialized_json('{"a": {}}', [('key', '{"b": 2}')]),
+            '{"a": {},"key":{"b": 2}}'
+        )
+        self.assertEqual(
+            extend_serialized_json('{"a": 1}', [('key', '[]')]),
+            '{"a": 1,"key":[]}'
+        )
+        self.assertEqual(
+            extend_serialized_json('{"a": []}', [('key', '[]')]),
+            '{"a": [],"key":[]}'
+        )
+        self.assertEqual(
+            extend_serialized_json('{}', [('key1', '1'), ('key2', '2')]),
+            '{"key1":1,"key2":2}'
+        )