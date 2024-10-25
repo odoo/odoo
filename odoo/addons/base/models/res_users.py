53dfa8d6e6c947051a990838f81fd91f63eb0202
Walid (wasa) <wasa@odoo.com>
2024-10-25 13:46:21 +0000

    [FIX] base: prevent selection of invalid home actions
    Problem:
    Certain `ir.actions.act_window` actions contain a context with `active_id`, which causes issues when set as home actions, as `active_id` will not be defined in that context.
    
    Solution:
    Prevent the selection of actions that include `active_id` in their context from being set as home actions.
    
    Steps to reproduce:
    1. Enable debug mode.
    2. Navigate to `Settings / Users & Companies / Users`.
    3. Select the current user from the list.
    4. Under Preferences / Menus Customization set "Home Action" to "Quotations and Sales."
    5. After that: each time you go to backend homepage (/) you get a traceback error instead of the app dashboard
    
    opw-4283156
    
    closes odoo/odoo#185924
    
    X-original-commit: 4b3eb2f6ab95e7d79528bd275665d63829697317
    Signed-off-by: Rémy Voet (ryv) <ryv@odoo.com>
    Signed-off-by: Walid Sahli (wasa) <wasa@odoo.com>

--- odoo/addons/base/models/res_users.py
+++ odoo/addons/base/models/res_users.py
@@ -593,13 +593,23 @@ class Users(models.Model):
         action_open_website = self.env.ref('base.action_open_website', raise_if_not_found=False)
         if action_open_website and any(user.action_id.id == action_open_website.id for user in self):
             raise ValidationError(_('The "App Switcher" action cannot be selected as home action.'))
-        # Prevent using reload actions.
         # We use sudo() because  "Access rights" admins can't read action models
         for user in self.sudo():
             if user.action_id.type == "ir.actions.client":
+                # Prevent using reload actions.
                 action = self.env["ir.actions.client"].browse(user.action_id.id)  # magic
                 if action.tag == "reload":
                     raise ValidationError(_('The "%s" action cannot be selected as home action.', action.name))
+
+            elif user.action_id.type == "ir.actions.act_window":
+                # Restrict actions that include 'active_id' in their context.
+                action = self.env["ir.actions.act_window"].browse(user.action_id.id)  # magic
+                if not action.context:
+                    continue
+                if "active_id" in action.context:
+                    raise ValidationError(
+                        _('The action "%s" cannot be set as the home action because it requires a record to be selected beforehand.', action.name)
+                    )
 
 
     @api.constrains('groups_id')
