fbdf519e0dc8830326a8ac120475966002b8474f
reka-odoo <reka@odoo.com>
2024-10-25 18:13:15 +0530

    [FIX] account: prevent error when removing currency from invoice
    When the user removes the currency from the invoice,
    a traceback will appear.
    
    Steps to reproduce the error:
    - Enable multiple currencies
    - Create a invoice > Add a line > Add any product > Remove currency
    
    Error: A traceback appears:
    "AssertionError: precision_rounding must be positive, got 0.0"
    
    https://github.com/odoo/odoo/blob/0bcc55685fd75f88214b7da1f5df9ec9a7aff784/addons/account/models/account_tax.py#L1041
    When, the user removes the currency, "precision_rounding" will be 0.0
    because at [1], "base_line['currency_id']" will be empty,
    So, the rounding will be 0.0
    So, It will lead to the above traceback.
    
    [1]- https://github.com/odoo/odoo/blob/0bcc55685fd75f88214b7da1f5df9ec9a7aff784/addons/account/models/account_tax.py#L1324
    
    When, the user removes the currency, precision_rounding will be 0.0
    because at [2], currency will be empty,
    [2]- https://github.com/odoo/odoo/blob/a2c9755e3924bc04e524f5ca0e5e17cd98be5b12/addons/account/models/account_tax.py#L985
    
    When, the user removes the currency,
    At [3] "singleton: res.currency()" error will be raised.
    so, fallback value is added for that.
    [3]- https://github.com/odoo/odoo/blob/a2c9755e3924bc04e524f5ca0e5e17cd98be5b12/addons/account/models/account_tax.py#L1433
    
    When the customer and product are added to the invoice, then the user removes
    the currency, At [4] "singleton: res.currency()" error will be raised.
    so, fallback value is added for that at [5]
    [4]- https://github.com/odoo/odoo/blob/a2c9755e3924bc04e524f5ca0e5e17cd98be5b12/addons/account/models/account_tax.py#L1640
    
    [5]- https://github.com/odoo/odoo/blob/a2c9755e3924bc04e524f5ca0e5e17cd98be5b12/addons/account/models/account_tax.py#L1601
    
    When the customer and product are added to the invoice, then the user removes
    the currency, At [6] "singleton: res.currency()" error will be raised because
    k['currency_id'] is False.
    [6]- https://github.com/odoo/odoo/blob/a2c9755e3924bc04e524f5ca0e5e17cd98be5b12/addons/account/models/account_tax.py#L2173
    
    sentry-6018518380
    
    closes odoo/odoo#185304
    
    Signed-off-by: Ruben Gomes (rugo) <rugo@odoo.com>

--- addons/account/tests/test_account_move_out_invoice.py
+++ addons/account/tests/test_account_move_out_invoice.py
@@ -4438,3 +4438,21 @@ class TestAccountMoveOutInvoiceOnchanges(AccountTestInvoicingCommon):
                 'credit': 0.0,
             },
         ])
+
+    def test_invoice_with_empty_currency(self):
+
+        move = self.env['account.move'].create({
+            'move_type': 'out_invoice',
+            'partner_id': self.partner_a.id,
+            'invoice_line_ids': [
+                Command.create({
+                    'name': 'invoice_line',
+                    'quantity': 1.0,
+                    'price_unit': 100.0,
+                    'tax_ids': [Command.set(self.company_data['default_tax_sale'].ids)],
+                }),
+            ],
+        })
+        move_form = Form(move)
+        move_form.currency_id = self.env['res.currency']
+        self.assertTrue(move.currency_id)
