d83f44467cf1f6ee431458913b51a9886ac46447
Julien Alardot (jual) <jual@odoo.com>
2024-10-28 14:37:48 +0100

    [IMP] sale_expense: Split file per models
    This moves the account.move model into its own file, for more clarity
    
    task-3458826

--- addons/sale_expense/models/__init__.py
+++ addons/sale_expense/models/__init__.py
@@ -1,5 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import account_move
 from . import account_move_line
 from . import hr_expense
 from . import hr_expense_sheet
