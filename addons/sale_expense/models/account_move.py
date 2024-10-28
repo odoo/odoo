fa4a06108b8e93a8b1b0a5978c2a039593cf8a08
Julien Alardot (jual) <jual@odoo.com>
2024-10-28 14:37:48 +0100

    [IMP] sale_expense: Split file per models
    This moves the account.move model into its own file, for more clarity
    
    task-3458826
    
    Part-of: odoo/odoo#150372
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>

---
+++ addons/sale_expense/models/account_move.py
@@ -0,0 +1,17 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    def _reverse_moves(self, default_values_list=None, cancel=False):
+        # EXTENDS sale
+        self.expense_sheet_id._sale_expense_reset_sol_quantities()
+        return super()._reverse_moves(default_values_list, cancel)
+
+    def button_draft(self):
+        # EXTENDS sale
+        self.expense_sheet_id._sale_expense_reset_sol_quantities()
+        return super().button_draft()
