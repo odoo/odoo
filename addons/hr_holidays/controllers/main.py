18f3e41272db8c6478cd5501e9f2821734372ef6
jayp-odoo <jayp@odoo.com>
2024-10-28 12:03:59 +0530

    [IMP] hr_holidays: (discuss) out of office for global time off
    before commit:
    
    prior to this commit public holidays are not considered to display out of office
    message in discuss.
    
    after commit:
    
    now public holidays will be considered to display out of office message.
    
    task-2377854

--- addons/hr_holidays/controllers/main.py
+++ addons/hr_holidays/controllers/main.py
@@ -3,7 +3,7 @@
 
 from odoo.addons.mail.controllers.mail import MailController
 from odoo import http
-
+from datetime import date
 
 class HrHolidaysController(http.Controller):
 
@@ -56,3 +56,14 @@ class HrHolidaysController(http.Controller):
             except Exception:
                 return MailController._redirect_to_messaging()
         return redirect
+
+    @http.route('/hr_holidays/get_public_holidays', type='jsonrpc', auth='user')
+    def hr_holidays_get_public_holidays(self):
+        today = date.today()
+        isPublicHoliday = bool(http.request.env['resource.calendar.leaves'].search([
+            ('resource_id', '=', False),
+            ('company_id', 'in', self.env.company.ids),
+            ('date_from', '<=', today),
+            ('date_to', '>=', today),
+        ], limit=1))
+        return isPublicHoliday
