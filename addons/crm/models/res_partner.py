7adead9793bad8f93e248a5f54cf5bc2479db846
nihp-odoo <nihp@odoo.com>
2024-10-28 14:42:21 +0530

    [IMP] crm: open list view by default on opportunity stat button
    Purpose:
    =====================
    In contact, when the user clicks the 'opportunity' stat button no
    stages are displayed, when all stages are assigned to the sales team.
    
    Specifications:
    =====================
    Open the list view by default, it's easier to open as we cannot
    guarantee that the kanban will have stages, and that prevents an easy
    creation of a lead. Also, default search filters are activated to match
    stat button statistics.
    
    And some UI change in lost opportunity wizard.
    
    Task-4283563
    
    closes odoo/odoo#185824
    
    Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>

--- addons/crm/models/res_partner.py
+++ addons/crm/models/res_partner.py
@@ -61,7 +61,13 @@ class ResPartner(models.Model):
         This function returns an action that displays the opportunities from partner.
         '''
         action = self.env['ir.actions.act_window']._for_xml_id('crm.crm_lead_opportunities')
-        action['context'] = {}
+        action['context'] = {
+            'search_default_filter_won': 1,
+            'search_default_filter_ongoing': 1,
+            'search_default_filter_lost': 1
+        }
+        # we want the list view first
+        action['views'] = sorted(action['views'], key=lambda view: view[1] != 'list')
         if self.is_company:
             action['domain'] = [('partner_id.commercial_partner_id', '=', self.id)]
         else:
