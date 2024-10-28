dd38c2b962b031d0323faf3caa54fbf605ca97fb
Louis Baudoux <lba@odoo.com>
2024-10-28 14:56:59 +0000

    [FIX] iap: add log in case of exception during IAP JSONRPC
    When an exception occurred during a request to IAP, there was no way of
    knowing which one it was exactly. This log should help understand what
    went wrong.
    
    Related ticket for which this would have been helpful: #4276907
    
    closes odoo/odoo#186360
    
    X-original-commit: a1477d1d00adb928b149f8e019f65c00f75ef129
    Signed-off-by: Ruben Gomes (rugo) <rugo@odoo.com>
    Signed-off-by: Louis Baudoux (lba) <lba@odoo.com>

--- addons/iap/tools/iap_tools.py
+++ addons/iap/tools/iap_tools.py
@@ -140,7 +140,8 @@ def iap_jsonrpc(url, method='call', params=None, timeout=15):
             e.data = response['error']['data']
             raise e
         return response.get('result')
-    except (ValueError, requests.exceptions.ConnectionError, requests.exceptions.MissingSchema, requests.exceptions.Timeout, requests.exceptions.HTTPError) as e:
+    except (ValueError, requests.exceptions.ConnectionError, requests.exceptions.MissingSchema, requests.exceptions.Timeout, requests.exceptions.HTTPError):
+        _logger.exception("iap jsonrpc %s failed", url)
         raise exceptions.AccessError(
             _("An error occurred while reaching %s. Please contact Odoo support if this error persists.", url)
         )
