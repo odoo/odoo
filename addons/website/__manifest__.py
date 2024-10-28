3a2ad0eee146c4afc7c52221aede6432e317bf34
Brieuc-brd <brd@odoo.com>
2024-10-28 16:38:54 +0100

    [IMP] website: standardises and finetunes `s_tabs` options and styles
    This commit standardises the options and styles of `s_tabs` snippet so
    that they can be reused in other tabs snippets.
    
    It also finetunes the style and options:
    - Reviews the "focus-visible" shadow to make it correctly visible in all
      tab styles
    - Reviews the text wrapping of tab items to ensure correct scrolling
      when the tab overflows the container
    - Reviews the arrows of the Slide Effect buttons to match the correct
      animation.
    - Maintains style consistency by replacing arbitrary values with
      variables.
    - Removes unnecessary commented code.
    
    task-4094407
    Part of task-4077427
    
    Part-of: odoo/odoo#185642
    Related: odoo/upgrade#6691
    Related: odoo/design-themes#1001
    Signed-off-by: Serge Bayet (seba) <seba@odoo.com>

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -359,7 +359,7 @@
             'website/static/src/js/editor/snippets.editor.js',
             'website/static/src/js/editor/snippets.options.js',
             'website/static/src/js/editor/shared_options/pricelist.js',
-            'website/static/src/js/editor/shared_options/options.js',
+            'website/static/src/js/editor/shared_options/tabs.js',
             'website/static/src/snippets/s_alert/options.js',
             'website/static/src/snippets/s_accordion/options.js',
             'website/static/src/snippets/s_facebook_page/options.js',
