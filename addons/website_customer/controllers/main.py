odoo/odoo

commit f11880d38b43e8bca8ff2fbf20c26182eb90809d

[REF] *: remove read_group usage
read_group will be deprecated soon, replace read_group
usage from the the business code with _read_group.

closes odoo/odoo#184153

Related: odoo/enterprise#72217
Signed-off-by: Rémy Voet (ryv) <ryv@odoo.com>

addons/website_customer/controllers/main.py
@@ -79,32 +79,31 @@ class WebsiteCustomer(GoogleMap):
             domain += [('website_tag_ids', 'in', tag_id)]
 
         # group by industry, based on customers found with the search(domain)
-        industries = Partner.sudo().read_group(domain, ["id", "industry_id"], groupby="industry_id", orderby="industry_id")
-        partners_count = Partner.sudo().search_count(domain)
+        industry_groups = Partner.sudo()._read_group(
+            domain, ["industry_id"], ["__count"], order="industry_id")
 
         if industry:
             domain.append(('industry_id', '=', industry.id))
-            if industry.id not in (x['industry_id'][0] for x in industries if x['industry_id']):
-                if industry.exists():
-                    industries.append({
-                        'industry_id_count': 0,
-                        'industry_id': (industry.id, industry.name)
-                    })
+            if industry.id not in (ind.id for ind, __ in industry_groups) and industry.exists():
+                industry_groups.append((industry, 0))
+                industry_groups = sorted(industry_groups, key=lambda i, __: i.name or '')
 
-        industries.sort(key=lambda d: (d.get('industry_id') or (0, ''))[1])
-
-        industries.insert(0, {
-            'industry_id_count': partners_count,
-            'industry_id': (0, _("All Industries"))
-        })
+        industries = [{
+            'industry_id_count': sum(count for __, count, in industry_groups),
+            'industry_id': (0, _("All Industries")),
+        }]
+        for g_industry, count in industry_groups:
+            industries.append({
+                'industry_id_count': count,
+                'industry_id': g_industry and (g_industry.id, g_industry.display_name),
+            })
 
         # group by country, based on customers found with the search(domain)
-        countries = Partner.sudo().read_group(domain, ["id", "country_id"], groupby="country_id", orderby="country_id")
-        country_count = Partner.sudo().search_count(domain)
+        country_groups = Partner.sudo()._read_group(domain, ["country_id"], ["__count"], order="country_id")
 
         fallback_all_countries = False
         if country:
-            if country_count > 0 and country.id not in (x['country_id'][0] for x in countries if x['country_id']):
+            if country_groups and country.id not in (country.id for country, __ in country_groups):
                 # fallback on all countries if no customer found for the country
                 # and there are matching customers for other countries
                 fallback_all_countries = True
@@ -112,10 +111,15 @@ class WebsiteCustomer(GoogleMap):
             else:
                 domain += [('country_id', '=', country.id)]
 
-        countries.insert(0, {
-            'country_id_count': country_count,
-            'country_id': (0, _("All Countries"))
-        })
+        countries = [{
+            'country_id_count': sum(count for __, count in country_groups),
+            'country_id': (0, _("All Countries")),
+        }]
+        for g_country, count in country_groups:
+            countries.append({
+                'country_id_count': count,
+                'country_id': g_country and (g_country.id, g_country.sudo().display_name),
+            })
 
         # search customers to display
         partner_count = Partner.sudo().search_count(domain)

addons/website_membership/controllers/main.py
@@ -62,25 +62,27 @@ class WebsiteMembership(http.Controller):
         if post_name:
             country_domain += ['|', ('name', 'ilike', post_name), ('website_description', 'ilike', post_name)]
 
-        countries = Partner.sudo().read_group(country_domain + [("website_published", "=", True)], ["__count"], groupby="country_id")
-        countries_total = sum(country_dict['country_id_count'] for country_dict in countries)
+        country_groups = Partner.sudo()._read_group(
+            country_domain + [("website_published", "=", True)],
+            ["country_id"], ["__count"], order="country_id")
 
         line_domain = list(base_line_domain)
         if country_id:
             line_domain.append(('partner.country_id', '=', country_id))
-            current_country = Country.browse(country_id).read(['id', 'name'])[0]
-            if not any(x['country_id'][0] == country_id for x in countries if x['country_id']):
-                countries.append({
-                    'country_id_count': 0,
-                    'country_id': (country_id, current_country["name"])
-                })
-                countries = [d for d in countries if d['country_id']]
-                countries.sort(key=lambda d: d['country_id'][1])
+            if not any(country.id == country_id for country, __ in country_groups):
+                country_groups = [(g_country, count) for g_country, count in country_groups if g_country]
+                country_groups.append((Country.browse(country_id).sudo(), 0))
+                country_groups = sorted(country_groups, key=lambda c, __: c.name or '')
 
-        countries.insert(0, {
-            'country_id_count': countries_total,
-            'country_id': (0, _("All Countries"))
-        })
+        countries = [{
+            'country_id_count': sum(count for __, count in country_groups),
+            'country_id': (0, _("All Countries")),
+        }]
+        for g_country, count in country_groups:
+            countries.append({
+                'country_id_count': count,
+                'country_id': g_country and (g_country.id, g_country.display_name),
+            })
 
         # format domain for group_by and memberships
         memberships = Product.search([('membership', '=', True)], order="website_sequence")

addons/website_crm_partner_assign/controllers/main.py
@@ -261,15 +261,14 @@ class WebsiteCrmPartnerAssign(WebsitePartnerPage, GoogleMap):
         country_domain = list(base_partner_domain)
         if grade:
             country_domain += [('grade_id', '=', grade.id)]
-        countries = partner_obj.sudo().read_group(
+
+        country_groups = partner_obj.sudo()._read_group(
             country_domain + [('country_id', '!=', False)],
-            ["id", "country_id"],
-            groupby="country_id", orderby="country_id")
+            ["country_id"], ["__count"], order="country_id")
 
         # Fallback on all countries if no partners found for the country and
         # there are matching partners for other countries.
-        country_ids = [c['country_id'][0] for c in countries]
-        fallback_all_countries = country and country.id not in country_ids
+        fallback_all_countries = country and country.id not in (c.id for c, __ in country_groups)
         if fallback_all_countries:
             country = None
 
@@ -277,28 +276,31 @@ class WebsiteCrmPartnerAssign(WebsitePartnerPage, GoogleMap):
         grade_domain = list(base_partner_domain)
         if country:
             grade_domain += [('country_id', '=', country.id)]
-        grades = partner_obj.sudo().read_group(
-            grade_domain, ["id", "grade_id"],
-            groupby="grade_id")
-        grades_partners = partner_obj.sudo().search_count(grade_domain)
-        # flag active grade
-        for grade_dict in grades:
-            grade_dict['active'] = grade and grade_dict['grade_id'][0] == grade.id
-        grades.insert(0, {
-            'grade_id_count': grades_partners,
+        grade_groups = partner_obj.sudo()._read_group(
+            grade_domain, ["grade_id"], ["__count"], order="grade_id")
+        grades = [{
+            'grade_id_count': sum(count for __, count in grade_groups),
             'grade_id': (0, _("All Categories")),
-            'active': bool(grade is None),
-        })
+            'active': grade is None,
+        }]
+        for g_grade, count in grade_groups:
+            grades.append({
+                'grade_id_count': count,
+                'grade_id': (g_grade.id, g_grade.display_name),
+                'active': grade and grade.id == g_grade.id,
+            })
 
-        countries_partners = partner_obj.sudo().search_count(country_domain)
-        # flag active country
-        for country_dict in countries:
-            country_dict['active'] = country and country_dict['country_id'] and country_dict['country_id'][0] == country.id
-        countries.insert(0, {
-            'country_id_count': countries_partners,
+        countries = [{
+            'country_id_count': sum(count for __, count in country_groups),
             'country_id': (0, _("All Countries")),
-            'active': bool(country is None),
-        })
+            'active': country is None,
+        }]
+        for g_country, count in country_groups:
+            countries.append({
+                'country_id_count': count,
+                'country_id': (g_country.id, g_country.display_name),
+                'active': country and g_country.id == country.id,
+            })
 
         # current search
         if grade:

addons/website_event/controllers/main.py
@@ -87,12 +87,18 @@ class WebsiteEventController(http.Controller):
                 date[3] = Event.search_count(expression.AND(no_date_domain) + domain_search + date[2])
 
         no_country_domain = event_details['no_country_domain']
-        countries = Event.read_group(expression.AND(no_country_domain) + domain_search, ["id", "country_id"],
-            groupby="country_id", orderby="country_id")
-        countries.insert(0, {
-            'country_id_count': sum([int(country['country_id_count']) for country in countries]),
-            'country_id': ("all", _("All Countries"))
-        })
+        country_groups = Event._read_group(
+            expression.AND(no_country_domain) + domain_search,
+            ["country_id"], ["__count"], order="country_id")
+        countries = [{
+            'country_id_count': sum(count for __, count in country_groups),
+            'country_id': (0, _("All Countries")),
+        }]
+        for g_country, count in country_groups:
+            countries.append({
+                'country_id_count': count,
+                'country_id': g_country and (g_country.id, g_country.sudo().display_name),
+            })
 
         search_tags = event_details['search_tags']
         current_date = event_details['current_date']

addons/hr_recruitment/models/hr_applicant.py
@@ -340,16 +340,15 @@ class HrApplicant(models.Model):
                     model_description="Applicant",
                 )
         # Copy CV from candidate to applicant at record creation
-        attachments_result = self.env['ir.attachment'].read_group([
+        attachments_by_candidate = dict(self.env['ir.attachment']._read_group([
             ('res_id', 'in', applicants.candidate_id.ids),
             ('res_model', '=', "hr.candidate")
-        ], ['ids:array_agg(id)'], groupby=['res_id'])
-        attachments_by_candidate = {e['res_id']: e['ids'] for e in attachments_result}
+        ], groupby=['res_id'], aggregates=['id:recordset']))
         for applicant in applicants:
             candidate_id = applicant.candidate_id.id
             if candidate_id not in attachments_by_candidate:
                 continue
-            self.env['ir.attachment'].browse(attachments_by_candidate[candidate_id]).copy({
+            attachments_by_candidate[candidate_id].copy({
                 'res_id': applicant.id,
                 'res_model': 'hr.applicant'
             })

addons/product/models/product_product.py
@@ -212,16 +212,18 @@ class ProductProduct(models.Model):
 
     def _check_duplicated_product_barcodes(self, barcodes_within_company, company_id):
         domain = self._get_barcode_search_domain(barcodes_within_company, company_id)
-        products_by_barcode = self.sudo().read_group(domain, ['barcode', 'id:array_agg'], ['barcode'])
+        products_by_barcode = self.sudo()._read_group(
+            domain, ['barcode'], ['id:recordset'], having=[('__count', '>', 1)],
+        )
 
         duplicates_as_str = "\n".join(
             _(
                 "- Barcode \"%(barcode)s\" already assigned to product(s): %(product_list)s",
-                barcode=record['barcode'], product_list=format_list(self.env, [p.display_name for p in self.search([('id', 'in', record['id'])])]),
+                barcode=barcode, product_list=format_list(self.env, duplicate_products._filtered_access('read').mapped('display_name')),
             )
-            for record in products_by_barcode if len(record['id']) > 1
+            for barcode, duplicate_products in products_by_barcode
         )
-        if duplicates_as_str.strip():
+        if duplicates_as_str:
             duplicates_as_str += _(
                 "\n\nNote: products that you don't have access to will not be shown above."
             )

addons/sale_stock/models/sale_order.py
@@ -103,7 +103,12 @@ class SaleOrder(models.Model):
     def _check_warehouse(self):
         """ Ensure that the warehouse is set in case of storable products """
         orders_without_wh = self.filtered(lambda order: order.state not in ('draft', 'cancel') and not order.warehouse_id)
-        company_ids_with_wh = {group['company_id'][0] for group in self.env['stock.warehouse'].read_group(domain=[('company_id', 'in', orders_without_wh.mapped('company_id').ids)], fields=['id:recordset'], groupby=['company_id'])} if orders_without_wh else {}
+        company_ids_with_wh = {
+            company_id.id for [company_id] in self.env['stock.warehouse']._read_group(
+                domain=[('company_id', 'in', orders_without_wh.company_id.ids)],
+                groupby=['company_id'],
+            )
+        }
         other_company = set()
         for order_line in orders_without_wh.order_line:
             if order_line.product_id.type != 'consu':

addons/stock/models/stock_location.py
@@ -467,30 +467,39 @@ class StockLocation(models.Model):
         """Returns a dictionary with the net and forecasted weight of the location.
         param excluded_sml_ids: set of stock.move.line ids to exclude from the computation
         """
-        result = defaultdict(lambda: defaultdict(float))
         if not excluded_sml_ids:
             excluded_sml_ids = set()
         Product = self.env['product.product']
         StockMoveLine = self.env['stock.move.line']
 
-        quants = self.env['stock.quant'].read_group([('location_id', 'in', self.ids)], ['quantity'], ['location_id', 'product_id'], lazy=False)
+        quants = self.env['stock.quant']._read_group(
+            [('location_id', 'in', self.ids)],
+            groupby=['location_id', 'product_id'], aggregates=['quantity:sum'],
+        )
         base_domain = [('state', 'not in', ['draft', 'done', 'cancel']), ('id', 'not in', tuple(excluded_sml_ids))]
-        outgoing_move_lines = StockMoveLine.read_group(expression.AND([[('location_id', 'in', self.ids)], base_domain]), ['quantity_product_uom'], ['location_id', 'product_id'], lazy=False)
-        incoming_move_lines = StockMoveLine.read_group(expression.AND([[('location_dest_id', 'in', self.ids)], base_domain]), ['quantity_product_uom'], ['location_dest_id', 'product_id'], lazy=False)
+        outgoing_move_lines = StockMoveLine._read_group(
+            expression.AND([[('location_id', 'in', self.ids)], base_domain]),
+            groupby=['location_id', 'product_id'], aggregates=['quantity_product_uom:sum'],
+        )
+        incoming_move_lines = StockMoveLine._read_group(
+            expression.AND([[('location_dest_id', 'in', self.ids)], base_domain]),
+            groupby=['location_dest_id', 'product_id'], aggregates=['quantity_product_uom:sum']
+        )
 
-        product_ids = {record['product_id'][0] for record in quants + outgoing_move_lines + incoming_move_lines}
-        weight_per_product = {weight['id']: weight['weight'] for weight in Product.browse(product_ids).read(['weight'])}
+        products = Product.union(*(product for __, product, __ in quants + outgoing_move_lines + incoming_move_lines))
+        products.fetch(['weight'])
 
-        for quant in quants:
-            weight = quant['quantity'] * weight_per_product[quant['product_id'][0]]
-            result[self.browse(quant['location_id'][0])]['net_weight'] += weight
-            result[self.browse(quant['location_id'][0])]['forecast_weight'] += weight
+        result = defaultdict(lambda: defaultdict(float))
+        for loc, product, quantity_sum in quants:
+            weight = quantity_sum * product.weight
+            result[loc]['net_weight'] += weight
+            result[loc]['forecast_weight'] += weight
 
-        for line in outgoing_move_lines:
-            result[self.browse(line['location_id'][0])]['forecast_weight'] -= line['quantity_product_uom'] * weight_per_product[line['product_id'][0]]
+        for loc, product, quantity_product_uom_sum in outgoing_move_lines:
+            result[loc]['forecast_weight'] -= quantity_product_uom_sum * product.weight
 
-        for line in incoming_move_lines:
-            result[self.browse(line['location_dest_id'][0])]['forecast_weight'] += line['quantity_product_uom'] * weight_per_product[line['product_id'][0]]
+        for dest_loc, product, quantity_product_uom_sum in incoming_move_lines:
+            result[dest_loc]['forecast_weight'] += quantity_product_uom_sum * product.weight
 
         return result
 

commit bfb6bafa72e1abfd526b556e45878961b7453f12

[FIX] website_*: fix bad `key` argument of `sorted`
f11880d38b43e8bca8ff2fbf20c26182eb90809d introduced two similar bugs:
We get a traceback when using links like
"/customers/industry/<name-industry>" where the industry isn't linked
to any contact (same for "/members/association/<membership_id>").

The origin of the traceback is that the key argument of the sorted call
takes only one argument (a tuple of 2 elements) instead of trying to
deconstruct the group directly in the arguments...

addons/website_customer/controllers/main.py
@@ -84,9 +84,9 @@ class WebsiteCustomer(GoogleMap):
 
         if industry:
             domain.append(('industry_id', '=', industry.id))
-            if industry.id not in (ind.id for ind, __ in industry_groups) and industry.exists():
+            if not any(ind.id == industry.id for ind, __ in industry_groups) and industry.exists():
                 industry_groups.append((industry, 0))
-                industry_groups = sorted(industry_groups, key=lambda i, __: i.name or '')
+                industry_groups = sorted(industry_groups, key=lambda group: group[0].name or '')
 
         industries = [{
             'industry_id_count': sum(count for __, count, in industry_groups),

addons/website_membership/controllers/main.py
@@ -72,7 +72,7 @@ class WebsiteMembership(http.Controller):
             if not any(country.id == country_id for country, __ in country_groups):
                 country_groups = [(g_country, count) for g_country, count in country_groups if g_country]
                 country_groups.append((Country.browse(country_id).sudo(), 0))
-                country_groups = sorted(country_groups, key=lambda c, __: c.name or '')
+                country_groups = sorted(country_groups, key=lambda group: group[0].name or '')
 
         countries = [{
             'country_id_count': sum(count for __, count in country_groups),