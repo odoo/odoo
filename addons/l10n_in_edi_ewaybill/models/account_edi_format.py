a7028f4d16f29ffb6ba6ee5f713fd11920d459b0
Harsh Modi <hamo@odoo.com>
2024-10-29 12:13:12 +0000

    [FIX] l10n_in_edi_ewaybill: fix the ewaybill name log
    Issue found at commit: https://github.com/odoo/odoo/commit/0e8f3d5c42b4f219e4e7db6915dd1dd7ec45e4b4
    After the above mentioned commit when using eWaybill with
    e-Invoicing the log note of ewaybill i.e.
    ```
    E-wayBill Sent
    Number -> 0
    Validity -> None
    ```
    where as in the response attachment of eWaybill, the ewaybill number
    does exist
    
    In this commit, we resolve the above issue and the correct ewaybill
    number gets logged
    
    opw-4280093
    
    X-original-commit: b3f73d0917477c94ad9e99087423a38424141029

--- addons/l10n_in_edi_ewaybill/models/account_edi_format.py
+++ addons/l10n_in_edi_ewaybill/models/account_edi_format.py
@@ -266,7 +266,7 @@ class AccountEdiFormat(models.Model):
             """).format(
                 _('E-wayBill Sent'),
                 _('Number'),
-                str(response.get("data", {}).get('ewayBillNo', 0)) or str(response.get("data", {}).get('EwbNo', 0)),
+                str(response.get("data", {}).get('EwbNo')),
                 _('Validity'),
                 str(response.get("data", {}).get('EwbValidTill'))
             )
@@ -366,9 +366,9 @@ class AccountEdiFormat(models.Model):
             """).format(
                 _('E-wayBill Sent'),
                 _('Number'),
-                str(response.get("data", {}).get('ewayBillNo', 0)) or str(response.get("data", {}).get('EwbNo', 0)),
+                str(response.get("data", {}).get('ewayBillNo')),
                 _('Validity'),
-                str(response.get("data", {}).get('EwbValidTill'))
+                str(response.get("data", {}).get('validUpto'))
             )
             invoices.message_post(body=body)
             self._l10n_in_edi_ewaybill_handle_zero_distance_alert_if_present(invoices, response)
