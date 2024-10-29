5c2d0ed9b28a14a30a46682a86d157e9cc6fe3e6
Shrey Mehta <shrm@odoo.com>
2024-10-29 12:00:41 +0530

    [FIX] l10n_pe_edi_website_sale: traceback on submit in address form
    Steps to Reproduce:
    - Set up a website for a Peru company.
    - Go to the shop section.
    - Purchase any product.
    - Click submit on address form.
    
    Issue:
    - A traceback appears upon clicking submit.
    
    Cause:
    - The error is due to an attempt to access an element in the form that is not
      present.
    
    Fix:
    - Updated the view and controller to display the identification type field
      when use_delivery_as_billing is enabled.
    
    opw-4314252
    
    closes odoo/odoo#185644
    
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>

--- addons/l10n_pe_website_sale/controllers/main.py
+++ addons/l10n_pe_website_sale/controllers/main.py
@@ -39,7 +39,7 @@ class L10nPEWebsiteSale(WebsiteSale):
         if request.website.sudo().company_id.country_id.code != 'PE':
             return rendering_values
 
-        if address_type == 'billing':
+        if address_type == 'billing' or rendering_values['use_delivery_as_billing']:
             can_edit_vat = rendering_values['can_edit_vat']
             LatamIdentificationType = request.env['l10n_latam.identification.type'].sudo()
             rendering_values.update({
