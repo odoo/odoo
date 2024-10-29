d97560b5ceab69c3e941d6736178c3536dfaf1fa
Pierre Pulinckx (PIPU) <pipu@odoo.com>
2024-10-29 12:53:40 +0000

    [REF] web_tour: improves trigger search method
    In this commit, we improve the algorithm for finding the trigger.
    In fact, in macro.js, when each time the findTrigger method returns
    "false", then we wait for the next mutation to run findTrigger again.
    So in fact, it is not correct to check if the element found for the
    tour step is disabled AFTER having found it. We must find an element
    that meets specific criteria, namely that it is not disabled.
    We take advantage of this commit to improve the error messages.
    We take advantage of this commit to move the pointer in tour_automatic
    and no longer have to pass it in tour_step_automatic.
    
    closes odoo/odoo#185731
    
    X-original-commit: f475c579cc785a437744c01ccfd595febb19590f
    Signed-off-by: Julien Mougenot (jum) <jum@odoo.com>
    Signed-off-by: Pierre Pulinckx (pipu) <pipu@odoo.com>

--- addons/web_tour/static/src/tour_service/tour_automatic.js
+++ addons/web_tour/static/src/tour_service/tour_automatic.js
@@ -2,6 +2,7 @@ import { tourState } from "./tour_state";
 import { config as transitionConfig } from "@web/core/transition";
 import { TourStepAutomatic } from "./tour_step_automatic";
 import { MacroEngine } from "@web/core/macro";
+import { browser } from "@web/core/browser/browser";
 
 export class TourAutomatic {
     mode = "auto";
@@ -17,16 +18,22 @@ export class TourAutomatic {
         this.stepDelay = tourConfig.stepDelay;
     }
 
+    get currentIndex() {
+        return tourState.getCurrentIndex();
+    }
+
+    get currentStep() {
+        return this.steps[this.currentIndex];
+    }
+
     get debugMode() {
         const tourConfig = tourState.getCurrentConfig() || {};
         return tourConfig.debug !== false;
     }
 
     start(pointer, callback) {
-        this.pointer = pointer;
-        const currentStepIndex = tourState.getCurrentIndex();
         const macroSteps = this.steps
-            .filter((step) => step.index >= currentStepIndex)
+            .filter((step) => step.index >= this.currentIndex)
             .flatMap((step) => {
                 return [
                     {
@@ -34,7 +41,18 @@ export class TourAutomatic {
                     },
                     {
                         trigger: () => step.findTrigger(),
-                        action: (stepEl) => step.doAction(stepEl),
+                        action: async () => {
+                            tourState.setCurrentIndex(step.index + 1);
+                            if (!step.skipped && this.showPointerDuration > 0 && step.element) {
+                                // Useful in watch mode.
+                                pointer.pointTo(step.element, this);
+                                await new Promise((r) =>
+                                    browser.setTimeout(r, this.showPointerDuration)
+                                );
+                                pointer.hide();
+                            }
+                            return step.doAction();
+                        },
                     },
                     {
                         action: () => step.waitForPause(),
