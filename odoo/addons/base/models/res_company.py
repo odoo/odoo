7748e104242204281d821345a58db5faa1987ff4
william-andre <wan@odoo.com>
2024-10-29 13:07:13 +0100

    [IMP] base: cache frozenset instead of tuple
    We don't care about the order of `env.companies` in the context of
    `__accessible_branches`.
    This improvement avoids caching a value for all the permutations of
    `env.companies`, slightly reducing the number of queries and the size of
    the cache[^1]
    
    [^1]: It is slightly more expensive for the size if the set of
    companies is small, but it becomes a lot less expensive the more
    possible permutation are possible.

--- odoo/addons/base/models/res_company.py
+++ odoo/addons/base/models/res_company.py
@@ -460,7 +460,7 @@ class ResCompany(models.CachedModel):
 
         return main_company
 
-    @ormcache('tuple(self.env.companies.ids)', 'self.id', 'self.env.uid')
+    @ormcache('frozenset(self.env.companies.ids)', 'self.id', 'self.env.uid')
     def __accessible_branches(self):
         # Get branches of this company that the current user can use
         self.ensure_one()
