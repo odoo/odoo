6b52313ba7dfae225761d1780c80b07c01d01838
Romain Estievenart <res@odoo.com>
2024-10-29 09:29:33 +0100

    [FIX] web: eslint on hoot .test.js files
    Since the creation of v18, the JavaScript tooling wasn't enabled.
    The tooling was enabled in commit[1]. In this commit, we applied a
    manual eslint pass inside the "web/static/tests" directory to ensure we
    have well-formatted .test.js files.
    
    ```
    eslint '**/web/static/tests/**/*.js' --fix
    ```
    
    [1]: https://github.com/odoo/odoo/commit/f3407c392d4e7595e9a299662115df03bacbd41c
    
    closes odoo/odoo#185638
    
    Signed-off-by: Romeo Fragomeli (rfr) <rfr@odoo.com>

--- addons/web/static/tests/views/fields/stat_info_field.test.js
+++ addons/web/static/tests/views/fields/stat_info_field.test.js
@@ -96,12 +96,14 @@ test.tags("mobile")("StatInfoField in form view on mobile", async () => {
     });
 });
 
-test.tags("desktop")("StatInfoField in form view with specific label_field on desktop", async () => {
-    await mountView({
-        type: "form",
-        resModel: "partner",
-        resId: 1,
-        arch: /* xml */ `
+test.tags("desktop")(
+    "StatInfoField in form view with specific label_field on desktop",
+    async () => {
+        await mountView({
+            type: "form",
+            resModel: "partner",
+            resId: 1,
+            arch: /* xml */ `
             <form>
                 <sheet>
                     <div class="oe_button_box" name="button_box">
@@ -115,18 +117,19 @@ test.tags("desktop")("StatInfoField in form view with specific label_field on de
                 </sheet>
             </form>
         `,
-    });
+        });
 
-    expect("button.oe_stat_button .o_field_widget .o_stat_info").toHaveCount(1, {
-        message: "should have one stat button",
-    });
-    expect("button.oe_stat_button .o_field_widget .o_stat_value").toHaveText("10", {
-        message: "should have 10 as value",
-    });
-    expect("button.oe_stat_button .o_field_widget .o_stat_text").toHaveText("yop", {
-        message: "should have 'yop' as text, since it is the value of field foo",
-    });
-});
+        expect("button.oe_stat_button .o_field_widget .o_stat_info").toHaveCount(1, {
+            message: "should have one stat button",
+        });
+        expect("button.oe_stat_button .o_field_widget .o_stat_value").toHaveText("10", {
+            message: "should have 10 as value",
+        });
+        expect("button.oe_stat_button .o_field_widget .o_stat_text").toHaveText("yop", {
+            message: "should have 'yop' as text, since it is the value of field foo",
+        });
+    }
+);
 
 test.tags("mobile")("StatInfoField in form view with specific label_field on mobile", async () => {
     await mountView({
