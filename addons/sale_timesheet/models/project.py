836edadbae91343a4e5d1f178e2d515f3b11097b
bary <bary@odoo.com>
2024-10-30 14:52:30 +0000

    [FIX] sale_timesheet: copy project allocated hours
    Steps to reproduce:
    - Install Project and sale_timesheet
    - Create a project with Timesheet option enabled
    - Go to that project's setting and allocate hours
    - Gear Icon > Duplicate
    
    The duplicated project has 0 allocated hours, this is odd since
    sale_timesheet forces that field to copy=False despite every other
    module allowing it (Even Timesheet). Additionally, copied tasks
    still have their allocated hours no matter what so it is strange to
    remove them from the project itself.
    
    opw-4284950
    
    closes odoo/odoo#186351
    
    X-original-commit: 5a6fb1729d6a457811ced6bc75235c40083ae265
    Signed-off-by: Yoann Baron (bary) <bary@odoo.com>
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/sale_timesheet/models/project.py
+++ addons/sale_timesheet/models/project.py
@@ -53,7 +53,7 @@ class Project(models.Model):
     warning_employee_rate = fields.Boolean(compute='_compute_warning_employee_rate', compute_sudo=True)
     partner_id = fields.Many2one(
         compute='_compute_partner_id', store=True, readonly=False)
-    allocated_hours = fields.Float(copy=False)
+    allocated_hours = fields.Float()
     billing_type = fields.Selection(
         compute="_compute_billing_type",
         selection=[
