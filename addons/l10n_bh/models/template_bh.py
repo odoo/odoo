7b1077beb78f8e2612d67b62db5dffec07339b8f
Maximilien (malb) <malb@odoo.com>
2024-10-31 15:55:48 +0000

    [ADD] l10n_bh: Add basic package
    This commit will add the basic package of the bahrain localisation.
    - Accounts
    - Taxes
    - Fiscal positions
    - States
    - Tax reports
    
    closes odoo/odoo#191062
    
    Task: 3927915
    X-original-commit: 5b67b8975425c1e22cd160379ee532d1ee1184f9
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>
    Signed-off-by: Maximilien La Barre (malb) <malb@odoo.com>

---
+++ addons/l10n_bh/models/template_bh.py
@@ -0,0 +1,43 @@
+from odoo import models
+from odoo.addons.account.models.chart_template import template
+
+
+class AccountChartTemplate(models.AbstractModel):
+    _inherit = 'account.chart.template'
+
+    @template('bh')
+    def _get_bh_template_data(self):
+        return {
+            'property_account_receivable_id': 'bh_account_100201',
+            'property_account_payable_id': 'bh_account_200101',
+            'property_account_expense_id': 'bh_account_500101',
+            'property_account_income_id': 'bh_account_400101',
+            'property_stock_valuation_account_id': 'bh_account_100502',
+            'property_stock_account_input_categ_id': 'bh_account_100503',
+            'property_stock_account_output_categ_id': 'bh_account_100504',
+            'property_stock_account_production_cost_id': 'bh_account_100505',
+            'code_digits': '6',
+        }
+
+    @template('bh', 'res.company')
+    def _get_bh_res_company(self):
+        return {
+            self.env.company.id: {
+                'account_fiscal_country_id': 'base.bh',
+                'bank_account_code_prefix': '1000',
+                'cash_account_code_prefix': '1009',
+                'transfer_account_code_prefix': '1001',
+                'account_default_pos_receivable_account_id': 'bh_account_100202',
+                'income_currency_exchange_account_id': 'bh_account_400301',
+                'expense_currency_exchange_account_id': 'bh_account_500903',
+                'account_journal_suspense_account_id': 'bh_account_100102',
+                'account_journal_early_pay_discount_loss_account_id': 'bh_account_501107',
+                'account_journal_early_pay_discount_gain_account_id': 'bh_account_400304',
+                'default_cash_difference_income_account_id': 'bh_account_400302',
+                'default_cash_difference_expense_account_id': 'bh_account_500909',
+                'deferred_expense_account_id': 'bh_account_100416',
+                'deferred_revenue_account_id': 'bh_account_200401',
+                'expense_account_id': 'bh_account_500101',
+                'income_account_id': 'bh_account_400101',
+            },
+        }
