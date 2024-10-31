3db974ac8cb81d270b0bcd2ce6cc94e40e16648a
william-andre <wan@odoo.com>
2024-10-31 14:50:10 +0000

    [IMP] account: only install CoA if it matches the country
    When installing a module containing the CoA, we instanciate it
    automatically on the current company only if it matches the country of
    the company. It can still be installed manually in the settings.
    
    This fixes two corner cases:
    * If a module supports multiple countries (i.e. France also supports
      Monaco), then we need to instanciate the right package.
    * If on a database containing multiple companies in different countries,
      we used to try to instanciate every CoA, leading in the last one being
      the winner but independently of which one was the right one.
    
    closes odoo/odoo#186411
    
    X-original-commit: d9b7cb3d9ee583eac4a56ae973ca1d3988bba10c
    Signed-off-by: de Wouters de Bouchout Jean-Benoît (jbw) <jbw@odoo.com>
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: Guillaume Vanleynseele (guva) <guva@odoo.com>

--- addons/account/models/ir_module.py
+++ addons/account/models/ir_module.py
@@ -63,10 +63,20 @@ class IrModule(models.Model):
         was_installed = len(self) == 1 and self.state in ('installed', 'to upgrade', 'to remove')
         res = super().write(vals)
         is_installed = len(self) == 1 and self.state == 'installed'
-        if not was_installed and is_installed and not self.env.company.chart_template and self.account_templates:
+        if (
+            not was_installed and is_installed
+            and not self.env.company.chart_template
+            and self.account_templates
+            and (guessed := next((
+                tname
+                for tname, tvals in self.account_templates.items()
+                if tvals['country_id'] == self.env.company.country_id.id
+                or not tvals['country_id']
+            ), None))
+        ):
             def try_loading(env):
                 env['account.chart.template'].try_loading(
-                    next(iter(self.account_templates)),
+                    guessed,
                     env.company,
                 )
             self.env.registry._auto_install_template = try_loading
