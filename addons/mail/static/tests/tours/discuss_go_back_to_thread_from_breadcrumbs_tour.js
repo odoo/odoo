335ec260a2ba35b04966d33f1e30446e16d3c796
tsm-odoo <tsm@odoo.com>
2024-10-31 14:31:31 +0100

    [FIX] mail: go back to the correct thread from breadcrumbs
    Before this PR, going back to discuss from the breadcrumbs would
    always redirect to the thread discuss first opened.
    
    Steps to reproduce:
    - Go to `http://localhost:8069/odoo/discuss?active_id=mail_box.inbox`.
    - Switch to the starred inbox.
    - Go to the form view of the general channel.
    - Click on the breadcrumb "Starred".
    - You are on the Inbox channel.
    
    This happens because the action service restores the action. Discuss
    only has one action: the one that opens the app. Then upon navigation,
    the current thread is added to the URL. However, the changes in the URL
    are not taken into account by the action service.
    
    This PR fixes the issue by updating the action when the thread changes
    so that it can be restored.
    
    task-4210141

---
+++ addons/mail/static/tests/tours/discuss_go_back_to_thread_from_breadcrumbs_tour.js
@@ -0,0 +1,12 @@
+import { registry } from "@web/core/registry";
+
+registry.category("web_tour.tours").add("discuss_go_back_to_thread_from_breadcrumbs.js", {
+    test: true,
+    steps: () => [
+        { trigger: ".o-mail-Discuss-threadName[title='Inbox']", allowDisabled: true },
+        { trigger: ".o-mail-DiscussSidebar-item:contains(Starred)", run: "click" },
+        { trigger: "button[title='View or join channels']", run: "click" },
+        { trigger: ".breadcrumb-item:contains(Starred)", run: "click" },
+        { trigger: ".o-mail-Discuss-threadName[title='Starred']", allowDisabled: true },
+    ],
+});
