c0f2a0214666439d6b8b5fed474bd012b933ba35
Manav Shah <sham@odoo.com>
2024-11-04 18:51:53 +0530

    [ADD] l10n_in_website_sale: Auto Validate GSTIN on Website
    This commit introduces a new module that activates via enabling
    setting to Auto Check GSTIN status on Website. If this setting
    is enabled, the GSTIN status is fetched from API and
    GST Treatment and Fiscal Position of contact is updated accordingly.
    
    TaskID:3916584

---
+++ addons/l10n_in_website_sale/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Indian eCommerce",
+    'description': "Indian Localization E-Commerce Module",
+    'version': '1.0',
+    'countries': ['in'],
+    'depends': ['l10n_in', 'website_sale'],
+    'installable': True,
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
