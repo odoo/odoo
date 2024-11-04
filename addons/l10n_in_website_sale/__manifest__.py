bc7c2269c85e47a7b7eacdf7589bec0049cad46a
Manav Shah <sham@odoo.com>
2024-11-04 18:51:53 +0530

    [ADD] l10n_in_website_sale: Auto Validate GSTIN on Website
    This commit introduces a new module which is auto installed upon
    installing `l10n_in` and `website_sale`.
    It makes use of the IAP Autocomplete API and updates the GST
    Treatment and Fiscal Position of partner when the partner address
    is updated from website shop checkout address page and profile
    page.
    
    TaskID:3916584

---
+++ addons/l10n_in_website_sale/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Indian eCommerce",
+    'description': "Indian Localization E-Commerce Module",
+    'version': '1.0',
+    'countries': ['in'],
+    'depends': ['l10n_in', 'website_sale'],
+    'auto_install': True,
+    'installable': True,
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
---
+++ addons/l10n_in_website_sale/controllers/portal.py
@@ -0,0 +1,32 @@
+from odoo.addons.portal.controllers.portal import CustomerPortal
+
+
+class L10nINCustomerPortal(CustomerPortal):
+
+    def _create_or_update_address(
+        self,
+        partner_sudo,
+        address_type='billing',
+        use_delivery_as_billing=False,
+        callback='/my/addresses',
+        required_fields=False,
+        **form_data
+    ):
+        old_vat = partner_sudo.vat or False
+        partner_sudo, feedback_dict = super()._create_or_update_address(
+            partner_sudo,
+            address_type=address_type,
+            use_delivery_as_billing=use_delivery_as_billing,
+            callback=callback,
+            required_fields=required_fields,
+            **form_data
+        )
+        if (
+            self.env.company.account_fiscal_country_id.code == 'IN'
+            and partner_sudo == partner_sudo.commercial_partner_id
+            and partner_sudo.country_id.code == 'IN'
+            and partner_sudo.vat
+            and partner_sudo.vat != old_vat
+        ):
+            partner_sudo._update_l10n_in_gst_treatment_and_fp_from_iap_autocomplete()
+        return partner_sudo, feedback_dict
