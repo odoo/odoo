6cd821357ba826f52dc7f75e830d7c32feae8ff9
Xavier Bol (xbo) <xbo@odoo.com>
2024-11-04 17:30:05 +0100

    [REM] project: remove old project sharing chatter
    Part-of: odoo/odoo#186164
    Related: odoo/enterprise#73240
    Related: odoo/upgrade#6719
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/project/static/src/project_sharing/components/chatter/chatter_message_counter.js
+++
@@ -1,8 +0,0 @@
-import { Component } from "@odoo/owl";
-
-export class ChatterMessageCounter extends Component {
-    static template = "project.ChatterMessageCounter";
-    static props = {
-        count: Number,
-    };
-}
