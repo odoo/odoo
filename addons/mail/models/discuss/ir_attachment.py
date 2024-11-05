30c64f9863ee7a3ce6ceee5d36583f449028fb6b
Thibault Delavallée <tde@odoo.com>
2024-11-05 10:57:14 +0000

    [IMP] mail, various: support voice metadata in post attachments
    Support 'voice' kwarg in message_post process when dealing with
    user-given attachments.
    
    TODO in master: somehow cleanup '_post_add_create' method to try
    to have simpler flows.
    
    Task-3839838
    
    closes odoo/odoo#186458
    
    X-original-commit: 50f1becff52d4b17bc120696b3d4df92e1e69b3c
    Related: odoo/enterprise#73387
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mail/models/discuss/ir_attachment.py
+++ addons/mail/models/discuss/ir_attachment.py
@@ -21,10 +21,15 @@ class IrAttachment(models.Model):
     def _to_store(self, store: Store, **kwargs):
         super()._to_store(store, **kwargs)
         for attachment in self:
+            # TODO master: make a real computed / inverse field and stop propagating
+            # kwargs through hook methods
             # sudo: discuss.voice.metadata - checking the existence of voice metadata for accessible attachments is fine
             store.add(attachment, {"voice": bool(attachment.sudo().voice_ids)})
 
     def _post_add_create(self, **kwargs):
         super()._post_add_create(**kwargs)
         if kwargs.get('voice'):
-            self.env["discuss.voice.metadata"].create([{"attachment_id": attachment.id} for attachment in self])
+            self._set_voice_metadata()
+
+    def _set_voice_metadata(self):
+        self.env["discuss.voice.metadata"].create([{"attachment_id": att.id} for att in self])
