38f81dbf6f2305ce1ea919c82065431d3a8f80f0
Nicolas Martinelli <nim@odoo.com>
2024-11-05 16:17:54 +0000

    [IMP] *: read-only controllers
    These controllers / methods are the frequently called and can be set as
    read-only.
    
    closes odoo/odoo#186684
    
    X-original-commit: c042e1baa9ef74c8b12cb8b8a79da81aed6098d9
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- addons/hr_attendance/controllers/main.py
+++ addons/hr_attendance/controllers/main.py
@@ -171,7 +171,7 @@ class HrAttendance(http.Controller):
         employee._attendance_action_change(geo_ip_response)
         return self._get_employee_info_response(employee)
 
-    @http.route('/hr_attendance/attendance_user_data', type="jsonrpc", auth="user")
+    @http.route('/hr_attendance/attendance_user_data', type="jsonrpc", auth="user", readonly=True)
     def user_attendance_data(self):
         employee = request.env.user.employee_id
         return self._get_user_attendance_data(employee)
