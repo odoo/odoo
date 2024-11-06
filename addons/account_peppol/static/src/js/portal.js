c46a0d918557b92ca22bb2cb21078b68877f8181
Mohammed Shekha <msh@odoo.com>
2024-11-06 06:24:34 +0000

    [FIX] account_peppol: Resolve conflict with duplicate PublicWidget name
    When both the portal and account_peppol modules are installed, certain
    features on the "My Account" page fail to work correctly. For example,
    changing the country does not update the State selection. This issue
    occurs because both modules define a PublicWidget with the name
    publicWidget.registry.portalDetails. When account_peppol is loaded, it
    overrides the existing portalDetails widget defined by the portal
    module, causing functionality conflicts.
    
    To resolve this, we have applied the include feature to patch the
    existing portalDetails class from the portal module, avoiding the
    conflict and preserving functionality across both modules.
    
    This issue was introduced in commit: https://github.com/odoo/odoo/pull/182585/commits/857b9a188c77cef2e3cd8a1c6b3035e7cd8d1305#diff-e0a8e9753f5e3ae55848bc264b7546eb7e187fe77c7500f9ebd97b1edfb23f46
    
    X-original-commit: bae371b243bb915eeb9ce2013406524d4df27cea

--- addons/account_peppol/static/src/js/portal.js
+++ addons/account_peppol/static/src/js/portal.js
@@ -1,15 +1,24 @@
+<<<<<<< master
 import publicWidget from "@web/legacy/js/public/public_widget";
+||||||| 371712bd1c245e56960d231df6b7592e95c37784
+/** @odoo-module **/
 
-publicWidget.registry.portalDetails = publicWidget.Widget.extend({
-    selector: '.o_portal_details',
-    events: {
-        'change select[name="invoice_sending_method"]': '_onSendingMethodChange',
-    },
+import publicWidget from "@web/legacy/js/public/public_widget";
+=======
+/** @odoo-module **/
+
+import portalDetails from "@portal/js/portal";
+>>>>>>> 92eab5e754befb2700c5a5c819017a34693b8ca8
+
+portalDetails.include({
+    events: Object.assign({}, portalDetails.prototype.events, {
+        'change select[name="invoice_sending_method"]': "_onSendingMethodChange",
+    }),
 
     start() {
         this._showPeppolConfig();
         this.orm = this.bindService("orm");
-        return this._super.apply(this, arguments);
+        return this._super(...arguments);
     },
 
     _showPeppolConfig() {
@@ -17,9 +26,9 @@ publicWidget.registry.portalDetails = publicWidget.Widget.extend({
         const divToToggle = document.querySelectorAll(".portal_peppol_toggle");
         for (const peppolDiv of divToToggle) {
             if (method === "peppol") {
-                peppolDiv.classList.remove("d-none")
+                peppolDiv.classList.remove("d-none");
             } else {
-                peppolDiv.classList.add("d-none")
+                peppolDiv.classList.add("d-none");
             }
         }
     },
