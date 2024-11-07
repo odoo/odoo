859f7022937adcd05c076244ec42236c1ff88b74
dhka-odoo <dhka@odoo.com>
2024-11-07 18:47:03 +0530

    [IMP] hr_holidays,*: working schedule ux
    * = hr_contract, resource
    changing ux for working schedule
    counts now visible for work resources and contracts
    added new columns in list view
    The public time off smart button now displays only the public time off
    related to the working schedule
    added filter for no running contracts and partial working schedules
    remove action_open_contracts and added action for opening contracts
    
    task-4313468
    
    closes odoo/odoo#187189
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr_contract/models/resource.py
+++ addons/hr_contract/models/resource.py
@@ -1,8 +1,8 @@
-# -*- coding:utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+from collections import defaultdict
 from datetime import datetime
 
-from odoo import fields, models
+from odoo import _, api, fields, models
 from odoo.osv.expression import AND
 
 
@@ -10,6 +10,9 @@ class ResourceCalendar(models.Model):
     _inherit = 'resource.calendar'
 
     contracts_count = fields.Integer("# Contracts using it", compute='_compute_contracts_count', groups="hr_contract.group_hr_contract_manager")
+    running_contracts_count = fields.Integer("Running contracts count", compute='_compute_contracts_count',
+        groups="hr_contract.group_hr_contract_manager", search="_search_running_contracts_count")
+    contract_ids = fields.One2many('hr.contract', 'resource_calendar_id', groups="hr_contract.group_hr_contract_manager")
 
     def transfer_leaves_to(self, other_calendar, resources=None, from_date=None):
         """
@@ -28,14 +31,40 @@ class ResourceCalendar(models.Model):
             'calendar_id': other_calendar.id,
         })
 
+    @api.depends('contract_ids', 'contract_ids.state')
     def _compute_contracts_count(self):
-        count_data = self.env['hr.contract']._read_group(
-            [('resource_calendar_id', 'in', self.ids), ('employee_id', '!=', False)],
-            ['resource_calendar_id'],
-            ['__count'])
-        mapped_counts = {resource_calendar.id: count for resource_calendar, count in count_data}
+        contracts_data = self.env['hr.contract']._read_group(
+            domain=[
+                ('resource_calendar_id', 'in', self.ids),
+                ('company_id', 'in', self.env.companies.ids),
+                ('employee_id', '!=', False)],
+            groupby=['resource_calendar_id', 'state'],
+            aggregates=['__count']
+        )
+        total_contracts_count = defaultdict(int)
+        running_contracts_count = defaultdict(int)
+        for calendar, state, count in contracts_data:
+            if state == 'open':
+                running_contracts_count[calendar.id] = count
+            total_contracts_count[calendar.id] += count
         for calendar in self:
-            calendar.contracts_count = mapped_counts.get(calendar.id, 0)
+            calendar.contracts_count = total_contracts_count[calendar.id]
+            calendar.running_contracts_count = running_contracts_count[calendar.id]
+
+    @api.model
+    def _search_running_contracts_count(self, operator, value):
+        if operator not in ['=', '!=', '<', '>'] or not isinstance(value, int):
+            raise NotImplementedError(_('Operation not supported.'))
+        calendar_ids = self.env['resource.calendar'].search([])
+        if operator == '=':
+            calender = calendar_ids.filtered(lambda m: m.running_contracts_count == value)
+        elif operator == '!=':
+            calender = calendar_ids.filtered(lambda m: m.running_contracts_count != value)
+        elif operator == '<':
+            calender = calendar_ids.filtered(lambda m: m.running_contracts_count < value)
+        elif operator == '>':
+            calender = calendar_ids.filtered(lambda m: m.running_contracts_count > value)
+        return [('id', 'in', calender.ids)]
 
     def action_open_contracts(self):
         self.ensure_one()
