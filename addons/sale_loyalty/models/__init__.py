77bf1de20d877add33b30dda96bc191f1e7af9c5
Demesmaeker <edm@odoo.com>
2024-11-07 15:18:25 +0100

    [CLN] {sale_}loyalty: reorder imports
    Part-of: odoo/odoo#186624
    Signed-off-by: Morgane Demesmaeker <edm@odoo.com>

--- addons/sale_loyalty/models/__init__.py
+++ addons/sale_loyalty/models/__init__.py
@@ -4,6 +4,6 @@ from . import loyalty_card
 from . import loyalty_history
 from . import loyalty_program
 from . import loyalty_reward
+from . import sale_order
 from . import sale_order_coupon_points
 from . import sale_order_line
-from . import sale_order
