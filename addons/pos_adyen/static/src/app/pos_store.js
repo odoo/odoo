c4b68362aaa6433d7f3986f74e29897c92b06bdc
David Monnom (moda) <moda@odoo.com>
2024-11-07 11:40:55 +0000

    [FIX] pos_adyen: fix adyen callback
    Add a check on adyen callback when paying. Verify if the pending payment
    line exist before processing the callback.
    
    opw-4242322
    
    X-original-commit: 88108e285875936e655f54760631abd2636f3749

--- addons/pos_adyen/static/src/app/pos_store.js
+++ addons/pos_adyen/static/src/app/pos_store.js
@@ -7,9 +7,11 @@ patch(PosStore.prototype, {
     async setup() {
         await super.setup(...arguments);
         this.onNotified("ADYEN_LATEST_RESPONSE", () => {
-            this.getPendingPaymentLine(
-                "adyen"
-            ).payment_method.payment_terminal.handleAdyenStatusResponse();
+            const pendingLine = this.pos.getPendingPaymentLine("adyen");
+
+            if (pendingLine) {
+                pendingLine.payment_method.payment_terminal.handleAdyenStatusResponse();
+            }
         });
     },
 });
