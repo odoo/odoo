6229c6778a3108fb53474e35ba79874c8b15fbda
roen-odoo <roen@odoo.com>
2024-11-07 14:31:41 +0000

    [FIX] pos_discount: correct tax group discount
    When applying a global discount on an order that contains lines using
    tax group, the tax amount was always ignored by the discount.
    
    Steps to reproduce:
    -------------------
    * Create a tax group with 2 taxes included in price (e.g 15% and 5%)
    * Create a product with a price of 120€, the price without tax should be
      99.38€
    * Open PoS and add the product
    * Add a global discount of 10%
    > Observation: As all the taxes are included in price the discount
    should be 12€ (10% of 120) but it is not
    
    Why the fix:
    ------------
    We now get the base by getting the total with tax of the line and
    substracting the tax amount that comes from that that are not included
    in price
    
    opw-4289157
    
    closes odoo/odoo#187493
    
    X-original-commit: 48653d1f56c59550e9bb94e9396d5348992b20ff
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Robin Engels (roen) <roen@odoo.com>

---
+++ addons/pos_discount/tests/test_frontend.py
@@ -0,0 +1,72 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo.tests import tagged
+from odoo.addons.point_of_sale.tests.test_frontend import TestPointOfSaleHttpCommon
+from odoo import Command
+
+
+@tagged('post_install', '-at_install')
+class TestUi(TestPointOfSaleHttpCommon):
+    def test_global_discount_tax_group_included(self):
+        tax_10 = self.env['account.tax'].create({
+            'name': "tax_10",
+            'amount_type': 'percent',
+            'amount': 10.0,
+            'type_tax_use': 'none',
+            'price_include_override': 'tax_included'
+        })
+        tax_20 = self.env['account.tax'].create({
+            'name': "tax_20",
+            'amount_type': 'percent',
+            'amount': 20.0,
+            'type_tax_use': 'none',
+            'price_include_override': 'tax_included'
+        })
+        tax_group_10_20 = self.env['account.tax'].create({
+            'name': "tax_group_10_20",
+            'amount_type': 'group',
+            'children_tax_ids': [Command.set((tax_10 + tax_20).ids)],
+            'type_tax_use': 'sale',
+        })
+        self.env['product.product'].create({
+            'name': 'Test Product',
+            'lst_price': 100,
+            'taxes_id': [Command.set(tax_group_10_20.ids)],
+            'available_in_pos': True,
+        })
+
+        self.main_pos_config.module_pos_discount = True
+        self.main_pos_config.discount_product_id = self.env.ref("pos_discount.product_product_consumable", raise_if_not_found=False)
+        self.main_pos_config.with_user(self.pos_user).open_ui()
+        self.start_tour("/pos/ui?config_id=%d" % self.main_pos_config.id, 'pos_global_discount_tax_group', login="pos_user")
+
+    def test_global_discount_tax_group_include_exclude(self):
+        tax_10 = self.env['account.tax'].create({
+            'name': "tax_10",
+            'amount_type': 'percent',
+            'amount': 10.0,
+            'type_tax_use': 'none',
+            'price_include_override': 'tax_included'
+        })
+        tax_20 = self.env['account.tax'].create({
+            'name': "tax_20",
+            'amount_type': 'percent',
+            'amount': 20.0,
+            'type_tax_use': 'none',
+        })
+        tax_group_10_20 = self.env['account.tax'].create({
+            'name': "tax_group_10_20",
+            'amount_type': 'group',
+            'children_tax_ids': [Command.set((tax_10 + tax_20).ids)],
+            'type_tax_use': 'sale',
+        })
+        self.env['product.product'].create({
+            'name': 'Test Product',
+            'lst_price': 100,
+            'taxes_id': [Command.set(tax_group_10_20.ids)],
+            'available_in_pos': True,
+        })
+
+        self.main_pos_config.module_pos_discount = True
+        self.main_pos_config.discount_product_id = self.env.ref("pos_discount.product_product_consumable", raise_if_not_found=False)
+        self.main_pos_config.with_user(self.pos_user).open_ui()
+        self.start_tour("/pos/ui?config_id=%d" % self.main_pos_config.id, 'pos_global_discount_tax_group_2', login="pos_user")
