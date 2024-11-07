70951f0c2ec384c23ea220fe9336b735cc1e2b0b
dhka-odoo <dhka@odoo.com>
2024-11-07 18:47:03 +0530

    [IMP] hr_holidays,*: working schedule ux
    * = hr_contract, resource
    changing ux for working schedule
    counts now visible for work resources and contracts
    added new columns in list view
    The public time off smart button now displays only the public time off
    related to the working schedule
    added filter for no running contracts and partial working schedules
    remove action_open_contracts and added action for opening contracts
    
    task-4313468
    
    closes odoo/odoo#187189
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr_holidays/models/resource.py
+++ addons/hr_holidays/models/resource.py
@@ -165,7 +165,7 @@ class ResourceCalendar(models.Model):
 
     def _compute_associated_leaves_count(self):
         leaves_read_group = self.env['resource.calendar.leaves']._read_group(
-            [('resource_id', '=', False), ('calendar_id', 'in', [False, *self.ids])],
+            [('resource_id', '=', False), ('calendar_id', 'in', self.ids)],
             ['calendar_id'],
             ['__count'],
         )
