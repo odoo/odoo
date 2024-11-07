7e26c7f490d68ef69963e04ac5633b526a9a52e9
Mahdi Cheikh Rouhou (macr) <macr@odoo.com>
2024-11-07 15:26:49 +0000

    [FIX] website, *: properly insert link in mass_mailing after url choice
    *: web_editor
    
    Steps to reproduce the issue:
    - Install website and email marketing
    - Create a new mass mailing
    - Add a button using `/button`
    - Add a label
    - Write `/` in url and choose any option make sure to click on it using
      the mouse
    - Save
    - The link isn't inserted
    
    When we click to select an item, the event `mousedown` is triggered on
    the document which will force a wysiwyg blur. So the selection is no
    more in the editable and we can't insert the link.
    
    To avoid the blur, we add the selector of the autocomplete to the
    selectors that delay the blur.
    
    opw-4283325
    
    closes odoo/odoo#192065
    
    X-original-commit: de47ec932742ef82b315990cbac8787f82aa91d5
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -2919,6 +2919,11 @@ export class Wysiwyg extends Component {
             }
         }
     }
+
+    _getDelayBlurSelectors() {
+        return [".oe-toolbar", ".oe-powerbox-wrapper", ".o_we_crop_widget"];
+    }
+
     _onDocumentMousedown(e) {
         if (!e.target.classList.contains('o_editable_date_field_linked')) {
             this.$editable.find('.o_editable_date_field_linked').removeClass('o_editable_date_field_linked');
@@ -2926,7 +2931,7 @@ export class Wysiwyg extends Component {
         const closestDialog = e.target.closest('.o_dialog, .o_web_editor_dialog');
         if (
             e.target.closest("#oe_snippets") ||
-            e.target.closest('.oe-toolbar,.oe-powerbox-wrapper,.o_we_crop_widget') ||
+            e.target.closest(this._getDelayBlurSelectors().join(",")) ||
             (closestDialog && closestDialog.querySelector('.o_select_media_dialog, .o_link_dialog'))
         ) {
             this._shouldDelayBlur = true;
