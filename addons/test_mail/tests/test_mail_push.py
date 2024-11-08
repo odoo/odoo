7eca068ff4faa8d40788069d60794e1e4113f2cf
ramh <ramh@odoo.com>
2024-11-08 10:00:10 +0000

    [FIX] mail: correct title display in web notifications for group chats
    Before this commit:
    
    When a user posted a message in a group chat, the web notification displayed
    `#False` as the title. This occurred because `record_name` was used as the title
    but it was empty for channels with `channel_type = 'group'`, as the displayName
    name is calculated on the UI side when displaying chat in Discuss.
    
    After this commit:
    
    The title of the web notification now correctly displays the group chat name,
    ensuring recipients see accurate and complete group titles in notifications.
    
    This commit also take guests into account in push notification
    from new messages in channels as author. Group chat default name
    also uses guest names for the conversation name.
    
    X-original-commit: 422cd1c601cdb4d2f910cf02410ad8a2867ba570
    X-original-commit: 77dd53c

--- addons/test_mail/tests/test_mail_push.py
+++ addons/test_mail/tests/test_mail_push.py
@@ -90,38 +90,48 @@ class TestWebPushNotification(SMSCommon):
                     (4, self.user_inbox.partner_id.id),
                 ],
                 'channel_type': channel_type,
-                'name': f'{channel_type} Message',
+                'name': f'{channel_type} Message' if channel_type != 'group' else '',
             } for channel_type in ['chat', 'channel', 'group']
         ])
+        group_channel.add_members(guest_ids=[self.guest.id])
 
-        for channel, has_notification in zip(
-            (chat_channel + channel_channel + group_channel),
-            (True, False, True)
+        for channel, sender, notification_count in zip(
+            (chat_channel + channel_channel + group_channel + group_channel),
+            (self.user_email, self.user_email, self.user_email, self.guest),
+            (1, 0, 1, 2),
         ):
             with self.subTest(channel_type=channel.channel_type):
-                # Test Direct Message
-                channel.with_user(self.user_email).message_post(
-                    body='Test Push',
-                    message_type='comment',
-                    subtype_xmlid='mail.mt_comment',
-                )
-                if has_notification:
-                    push_to_end_point.assert_called_once()
+                if sender == self.guest:
+                    channel_as_sender = channel.with_user(self.env.ref('base.public_user')).with_context(guest=sender)
+                else:
+                    channel_as_sender = channel.with_user(self.user_email)
+                channel_as_sender.message_post(
+                        body='Test Push',
+                        message_type='comment',
+                        subtype_xmlid='mail.mt_comment',
+                    )
+                self.assertEqual(push_to_end_point.call_count, notification_count)
+                if notification_count > 0:
                     payload_value = json.loads(push_to_end_point.call_args.kwargs['payload'])
                     if channel.channel_type == 'chat':
                         self.assertEqual(payload_value['title'], f'{self.user_email.name}')
+                    elif channel.channel_type == 'group':
+                        self.assertIn(self.user_email.name, payload_value['title'])
+                        self.assertIn(self.user_inbox.name, payload_value['title'])
+                        self.assertIn(self.guest.name, payload_value['title'])
+                        self.assertNotIn("False", payload_value['title'])
                     else:
                         self.assertEqual(payload_value['title'], f'#{channel.name}')
-                    self.assertEqual(
-                        payload_value['options']['icon'],
-                        f'/web/image/res.partner/{self.user_email.partner_id.id}/avatar_128'
+                    icon = (
+                        '/web/static/img/odoo-icon-192x192.png'
+                        if sender == self.guest
+                        else f'/web/image/res.partner/{self.user_email.partner_id.id}/avatar_128'
                     )
+                    self.assertEqual(payload_value['options']['icon'], icon)
                     self.assertEqual(payload_value['options']['body'], 'Test Push')
                     self.assertEqual(payload_value['options']['data']['res_id'], channel.id)
                     self.assertEqual(payload_value['options']['data']['model'], channel._name)
                     self.assertEqual(push_to_end_point.call_args.kwargs['device']['endpoint'], 'https://test.odoo.com/webpush/user2')
-                else:
-                    push_to_end_point.assert_not_called()
                 push_to_end_point.reset_mock()
 
         # Test Direct Message with channel muted -> should skip push notif
