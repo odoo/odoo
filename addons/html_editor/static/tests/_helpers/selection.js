8fb4d2fbc66407d109af860861f2e27191ee5b93
FrancoisGe <fge@odoo.com>
2024-11-08 16:22:18 +0100

    fix setContent

--- addons/html_editor/static/tests/_helpers/selection.js
+++ addons/html_editor/static/tests/_helpers/selection.js
@@ -100,7 +100,10 @@ export function setContent(el, content) {
         textNode.textContent = textNode.textContent.replace("[", "").replace("]", "");
     }
     // remove extra empty text nodes
-    el.innerHTML = div.innerHTML;
+    const divInnerHTML = div.innerHTML;
+    if (el.innerHTML !== divInnerHTML) {
+        el.innerHTML = divInnerHTML;
+    }
 
     const configSelection = getSelection(el, content);
     if (configSelection) {
