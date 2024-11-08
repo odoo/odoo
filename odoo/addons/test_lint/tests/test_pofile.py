b2965adb7bd6148ec8cdb9539fe8f45e6aa853a0
william-andre <wan@odoo.com>
2024-11-08 16:22:58 +0100

    [IMP] translation: allow translating from xml data file
    In order to simplify the process of translating localization modules
    that need support for only a few languages, this commit is adding
    support for translated values in the XML data file directly, instead of
    managing the translations in the common `.po`/`.pot` translation files.
    
    Managing those translation files by hand is indeed a lot of error prone
    work, that is often forgotten because it is usually only done *after*
    the review process.
    
    There are a few things to keep in mind:
    * the case that we want to solve is mainly for translating
      `account.report`/`account.report.line`
    * these models are sometimes defined in
      * generic modules (`account`/`account_reports`): in that case we need
        all the translations, which is more fit using the common translation
        files, translated by translators on an external platform.
      * localization modules (`l10n_*`): in that case we only need a few
        languages, and the translations are often official terms released by
        the government/tax agencies/..., it doesn't make sense to be
        translated on an external platform.
    * being able to import the values is not enough because we also don't
      want to add entries in the `.pot` file if it is already translated in
      the XML data file. Having these smaller translation files will allow
      us to use the external translation tool for localization modules as
      well, for the remaining errors/views, where having more languages
      makes more sense if wanted/needed.
    * we should still be able to import/export translations of the records
      outside of the scope of the module, like before.
    
    Part-of: odoo/odoo#187546
    Related: odoo/enterprise#74141
    Signed-off-by: Raphael Collet <rco@odoo.com>

--- odoo/addons/test_lint/tests/test_pofile.py
+++ odoo/addons/test_lint/tests/test_pofile.py
@@ -5,14 +5,14 @@ from collections import Counter
 
 from odoo.modules import get_modules
 from odoo.tests.common import TransactionCase
-from odoo.tools.translate import TranslationFileReader
+from odoo.tools.translate import translation_file_reader
 from odoo.tools.misc import file_path
 
 
 class PotLinter(TransactionCase):
     def test_pot_duplicate_entries(self):
         def format(entry):
-            # TranslationFileReader only returns those three types
+            # translation_file_reader only returns those three types
             if entry['type'] == 'model':
                 return ('model', entry['name'], entry['imd_name'])
             elif entry['type'] == 'model_terms':
@@ -26,6 +26,6 @@ class PotLinter(TransactionCase):
                 filename = file_path(f'{module}/i18n/{module}.pot')
             except FileNotFoundError:
                 continue
-            counts = Counter(map(format, TranslationFileReader(filename)))
+            counts = Counter(map(format, translation_file_reader(filename)))
             duplicates = [key for key, count in counts.items() if count > 1]
             self.assertFalse(duplicates, "Duplicate entries found in %s" % filename)
