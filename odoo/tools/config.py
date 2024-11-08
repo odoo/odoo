odoo/odoo

commit fd4c7f36243ad6996ce1c9e208bc1230672b5db0

[FIX] core: config file mutually exclusive options
There are multiple mutually exclusive options, eg. --syslog vs --logfile
but this was only checked when the options came from the command line
and not from the configuration file.

About `-i` and `-u` incompatible with multiple databases `-d db1,db2`,
the two `-i` and `-u` are only ever taken from the CLI, in case the
options are present inside the configuration file they will be ignored.

Task-3380313

Part-of: odoo/odoo#126099
Related: odoo/enterprise#43612
Signed-off-by: Julien Castiaux (juc) <juc@odoo.com>

odoo/tools/config.py
@@ -432,18 +432,6 @@ class configmanager:
         # Ensures no illegitimate argument is silently discarded (avoids insidious "hyphen to dash" problem)
         die(args, "unrecognized parameters: '%s'" % " ".join(args))
 
-        die(bool(opt.syslog) and bool(opt.logfile),
-            "the syslog and logfile options are exclusive")
-
-        die(opt.translate_in and (not opt.language or not opt.db_name),
-            "the i18n-import option cannot be used without the language (-l) and the database (-d) options")
-
-        die(opt.overwrite_existing_translations and not (opt.translate_in or opt.update),
-            "the i18n-overwrite option cannot be used without the i18n-import option or without the update option")
-
-        die(opt.translate_out and (not opt.db_name),
-            "the i18n-export option cannot be used without the database (-d) option")
-
         # Check if the config file exists (-c used, but not -s)
         die(not opt.save and opt.config and not os.access(opt.config, os.R_OK),
             "The config file '%s' selected with -c/--config doesn't exist or is not readable, "\
@@ -541,8 +529,21 @@ class configmanager:
             elif isinstance(self.options[arg], str) and self.casts[arg].type in optparse.Option.TYPE_CHECKER:
                 self.options[arg] = optparse.Option.TYPE_CHECKER[self.casts[arg].type](self.casts[arg], arg, self.options[arg])
 
-        ismultidb = ',' in (self.options.get('db_name') or '')
-        die(ismultidb and (opt.init or opt.update), "Cannot use -i/--init or -u/--update with multiple databases in the -d/--database/db_name")
+        die(bool(self.options['syslog']) and bool(self.options['logfile']),
+            "the syslog and logfile options are exclusive")
+
+        die(self.options['translate_in'] and (not self.options['language'] or not self.options['db_name']),
+            "the i18n-import option cannot be used without the language (-l) and the database (-d) options")
+
+        die(self.options['overwrite_existing_translations'] and not (self.options['translate_in'] or self.options['update']),
+            "the i18n-overwrite option cannot be used without the i18n-import option or without the update option")
+
+        die(self.options['translate_out'] and (not self.options['db_name']),
+            "the i18n-export option cannot be used without the database (-d) option")
+
+        die(',' in (self.options.get('db_name') or '') and (opt.init or opt.update),
+            "Cannot use -i/--init or -u/--update with multiple databases in the -d/--database/db_name")
+
         self.options['root_path'] = self._normalize(os.path.join(os.path.dirname(__file__), '..'))
         if not self.options['addons_path'] or self.options['addons_path']=='None':
             default_addons = []

odoo/addons/base/tests/test_configmanager.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import call, patch
 
 import odoo
 from odoo.tests import TransactionCase
@@ -21,6 +21,12 @@ class TestConfigManager(TransactionCase):
         patcher.start()
         self.addCleanup(patcher.stop)
         self.config = configmanager()
+
+    def parse_reset(self, args=None):
+        options = dict(self.config.options)
+        with patch.dict(self.config.options, options):
+            cli = self.config._parse_config(args)
+        return cli, options
 
     def test_00_setUp(self):
         self.assertEqual(self.config.rcfile, EMPTY_CONFIG_PATH)
@@ -189,7 +195,7 @@ class TestConfigManager(TransactionCase):
 
             # logging
             'logfile': '/foo/bar/odoo.log',
-            'syslog': True,
+            'syslog': False,
             'log_handler': [':DEBUG'],
             'log_db': True,
             'log_db_level': 'debug',
@@ -394,3 +400,35 @@ class TestConfigManager(TransactionCase):
         config._warn_deprecated_options()
         config._parse_config()
         config._warn_deprecated_options()
+
+    @patch('optparse.OptionParser.error')
+    def test_06_syslog_logfile_exclusive_cli(self, error):
+        self.parse_reset(['--syslog', '--logfile', 'logfile'])
+        self.parse_reset(['-c', file_path('base/tests/config/sysloglogfile.conf')])
+        error.assert_has_calls(2 * [call("the syslog and logfile options are exclusive")])
+
+    @patch('optparse.OptionParser.error')
+    def test_07_translate_in_requires_language_and_db_name(self, error):
+        self.parse_reset(['--i18n-import', '/path/to/file.csv'])
+        self.parse_reset(['--i18n-import', '/path/to/file.csv', '-d', 'dbname'])
+        self.parse_reset(['--i18n-import', '/path/to/file.csv', '-l', 'fr_FR'])
+        error.assert_has_calls(3 * [call("the i18n-import option cannot be used without the language (-l) and the database (-d) options")])
+
+    @patch('optparse.OptionParser.error')
+    def test_08_overwrite_existing_translations_incompatible_with_translate_in_or_update(self, error):
+        self.parse_reset(['--i18n-overwrite', '-l', 'fr_FR'])
+        self.parse_reset(['--i18n-overwrite', '-u', 'base'])
+        error.assert_has_calls(2 * [call("the i18n-overwrite option cannot be used without the i18n-import option or without the update option")])
+
+    @patch('optparse.OptionParser.error')
+    def test_09_translate_out_requires_db_name(self, error):
+        self.parse_reset(['--i18n-export', '/path/to/file.csv'])
+        error.assert_has_calls(1 * [call("the i18n-export option cannot be used without the database (-d) option")])
+
+    @patch('optparse.OptionParser.error')
+    def test_10_init_update_incompatible_with_multidb(self, error):
+        self.parse_reset(['-d', 'db1,db2', '-i', 'base'])
+        self.parse_reset(['-d', 'db1,db2', '-u', 'base'])
+        self.parse_reset(['-c', file_path('base/tests/config/multidb.conf'), '-i', 'base'])
+        self.parse_reset(['-c', file_path('base/tests/config/multidb.conf'), '-u', 'base'])
+        error.assert_has_calls(4 * [call("Cannot use -i/--init or -u/--update with multiple databases in the -d/--database/db_name")])

odoo/addons/base/tests/config/multidb.conf
@@ -1,0 +1,2 @@
+[options]
+db_name = db1,db2

odoo/addons/base/tests/config/non_default.conf
@@ -43,7 +43,7 @@ screenshots = /temp/screenshots
 
 # logging
 logfile = /foo/bar/odoo.log
-syslog = True
+syslog = False
 log_handler = :DEBUG
 log_db = True
 log_db_level = debug

odoo/addons/base/tests/config/sysloglogfile.conf
@@ -1,0 +1,3 @@
+[options]
+syslog = True
+logfile = /var/log/odoo.log

commit cf5b3622065b2b59b2c6d196f979e4af13c76845

[REF] core: ConfigCleaner(8) parse config file
This commit is part of a larger refactor, see associated PR.

Immediately parse the values coming from the configuration file. Don't
leave dangling strings that need to be re-parsed as int/float/... This
also makes that options that are either file-only or cli-only are
correctly parsed (--save and the websocket options).

This fixes the test_08_overwrite_existing... test that was broken in
commit fd4c7f3. The assertions in the test were wrong, the second call
with both `--i18n-overwrite` and `-u` wasn't expected to crash (wrong
assertion). It was crashing because the cli-only options were not
correctly parsed.

odoo/tools/config.py
@@ -479,23 +479,13 @@ class configmanager:
             opt.config or os.environ.get('ODOO_RC') or os.environ.get('OPENERP_SERVER') or rcfilepath)
         self.load()
 
-        # Verify that we want to log or not, if not the output will go to stdout
-        if self.options['logfile'] in ('None', 'False'):
-            self.options['logfile'] = False
-        # the same for the pidfile
-        if self.options['pidfile'] in ('None', 'False'):
-            self.options['pidfile'] = False
-        # the same for the test_tags
-        if self.options['test_tags'] == 'None':
-            self.options['test_tags'] = None
-        # and the server_wide_modules
-        if self.options['server_wide_modules'] in ('', 'None', 'False'):
+        if not self.options['server_wide_modules']:
             self.options['server_wide_modules'] = 'base,web,rpc'
 
         keys = [
             option_name for option_name, option
             in self.options_index.items()
-            if option.cli_loadable and option.file_loadable
+            if option.cli_loadable
             if option.action != 'append'
         ]
 
@@ -504,9 +494,6 @@ class configmanager:
             # action=append requiring a real default, so we cannot use the my_default workaround)
             if getattr(opt, arg, None) is not None:
                 self.options[arg] = getattr(opt, arg)
-            # ... or keep, but cast, the config file value.
-            elif isinstance(self.options[arg], str) and self.casts[arg].type in optparse.Option.TYPE_CHECKER:
-                self.options[arg] = optparse.Option.TYPE_CHECKER[self.casts[arg].type](self.casts[arg], arg, self.options[arg])
 
         if isinstance(self.options['log_handler'], str):
             self.options['log_handler'] = self.options['log_handler'].split(',')
@@ -679,10 +666,14 @@ class configmanager:
                     continue
                 if not option.file_loadable:
                     continue
-                if value=='True' or value=='true':
+                if value == 'None':
+                    value = None
+                elif value == 'True' or value == 'true':
                     value = True
-                if value=='False' or value=='false':
+                elif value == 'False' or value == 'false':
                     value = False
+                elif option.type in optparse.Option.TYPE_CHECKER:
+                    value = optparse.Option.TYPE_CHECKER[option.type](option, name, value)
                 self.options[name] = value
         except IOError:
             pass

odoo/addons/base/tests/test_configmanager.py
@@ -31,7 +31,7 @@ class TestConfigManager(TransactionCase):
         self.assertEqual(self.config.rcfile, EMPTY_CONFIG_PATH)
 
     def test_01_default_config(self):
-        default_values = {
+        self.assertDictEqual(self.config.options, {
             # options not exposed on the command line
             'admin_passwd': 'admin',
             'bin_path': '',
@@ -46,7 +46,7 @@ class TestConfigManager(TransactionCase):
 
             # common
             'config': None,
-            'save': None,
+            'save': False,
             'init': {},
             'update': {},
             'without_demo': False,
@@ -130,28 +130,27 @@ class TestConfigManager(TransactionCase):
             'unaccent': False,
             'geoip_city_db': '/usr/share/GeoIP/GeoLite2-City.mmdb',
             'geoip_country_db': '/usr/share/GeoIP/GeoLite2-Country.mmdb',
-        }
 
-        if IS_POSIX:
             # multiprocessing
-            default_values.update(
-                {
-                    'workers': 0,
-                    'limit_memory_soft': 2048 * 1024 * 1024,
-                    'limit_memory_soft_gevent': False,
-                    'limit_memory_hard': 2560 * 1024 * 1024,
-                    'limit_memory_hard_gevent': False,
-                    'limit_time_cpu': 60,
-                    'limit_time_real': 120,
-                    'limit_time_real_cron': -1,
-                    'limit_request': 2**16,
-                }
-            )
-
-        self.assertEqual(self.config.options, default_values, "Options don't match")
+            **({
+                'workers': 0,
+                'limit_memory_soft': 2048 * 1024 * 1024,
+                'limit_memory_soft_gevent': False,
+                'limit_memory_hard': 2560 * 1024 * 1024,
+                'limit_memory_hard_gevent': False,
+                'limit_time_cpu': 60,
+                'limit_time_real': 120,
+                'limit_time_real_cron': -1,
+                'limit_request': 2**16,
+            } if IS_POSIX else {}),
+        })
 
     def test_02_config_file(self):
-        values = {
+        config_path = file_path('base/tests/config/non_default.conf')
+        self.config._parse_config(['-c', config_path])
+        self.assertEqual(self.config.rcfile, config_path)
+        self.assertEqual(self.config.rcfile, self.config['config'])
+        self.assertDictEqual(self.config.options, {
             # options not exposed on the command line
             'admin_passwd': 'Tigrou007',
             'bin_path': '',
@@ -160,13 +159,13 @@ class TestConfigManager(TransactionCase):
             'proxy_access_token': '',
             'publisher_warranty_url': 'http://example.com',  # blacklist for save, read from the config file
             'reportgz': True,
-            'websocket_rate_limit_burst': '1',
-            'websocket_rate_limit_delay': '2',
-            'websocket_keep_alive_timeout': '600',
+            'websocket_rate_limit_burst': 1,
+            'websocket_rate_limit_delay': 2.0,
+            'websocket_keep_alive_timeout': 600,
 
             # common
-            'config': None,
-            'save': None,
+            'config': config_path,
+            'save': False,
             'init': {},  # blacklist for save, ignored from the config file
             'update': {},  # blacklist for save, ignored from the config file
             'without_demo': True,
@@ -250,29 +249,20 @@ class TestConfigManager(TransactionCase):
             'unaccent': True,
             'geoip_city_db': '/tmp/city.db',
             'geoip_country_db': '/tmp/country.db',
-        }
 
-        if IS_POSIX:
             # multiprocessing
-            values.update(
-                {
-                    'workers': 92,
-                    'limit_memory_soft': 1048576,
-                    'limit_memory_soft_gevent': 1048577,
-                    'limit_memory_hard': 1048578,
-                    'limit_memory_hard_gevent': 1048579,
-                    'limit_time_cpu': 60,
-                    'limit_time_real': 61,
-                    'limit_time_real_cron': 62,
-                    'limit_request': 100,
-                }
-            )
-
-        config_path = file_path('base/tests/config/non_default.conf')
-        self.config._parse_config(['-c', config_path])
-        self.assertEqual(self.config.options, values, "Options don't match")
-        self.assertEqual(self.config.rcfile, config_path)
-        self.assertNotEqual(self.config.rcfile, self.config['config'])  # funny
+            **({
+                'workers': 92,
+                'limit_memory_soft': 1048576,
+                'limit_memory_soft_gevent': 1048577,
+                'limit_memory_hard': 1048578,
+                'limit_memory_hard_gevent': 1048579,
+                'limit_time_cpu': 60,
+                'limit_time_real': 61,
+                'limit_time_real_cron': 62,
+                'limit_request': 100,
+            } if IS_POSIX else {}),
+        })
 
     @unittest.skipIf(not IS_POSIX, 'this test is POSIX only')
     def test_03_save_default_options(self):
@@ -292,9 +282,15 @@ class TestConfigManager(TransactionCase):
     def test_04_odoo16_config_file(self):
         # test that loading the Odoo 16.0 generated default config works
         # with a modern version
-        assert_options = {
+        config_path = file_path('base/tests/config/16.0.conf')
+        with self.assertLogs('odoo.tools.config', 'WARNING') as capture:
+            self.config._parse_config(['--config', config_path])
+        with self.assertNoLogs('py.warnings'):
+            self.config._warn_deprecated_options()
+        self.assertDictEqual(self.config.options, {
             # options taken from the configuration file
             'admin_passwd': 'admin',
+            'config': config_path,
             'csv_internal_sep': ',',
             'db_host': False,
             'db_maxconn': 64,
@@ -347,14 +343,13 @@ class TestConfigManager(TransactionCase):
 
             # options that are not taken from the file (also in 14.0)
             'addons_path': f'{PROJECT_PATH}/odoo/addons,{PROJECT_PATH}/addons',
-            'config': None,
             'data_dir': _get_default_datadir(),
             'dev_mode': [],
             'geoip_database': '/usr/share/GeoIP/GeoLite2-City.mmdb',
             'init': {},
             'language': None,
             'publisher_warranty_url': 'http://services.odoo.com/publisher-warranty/',
-            'save': None,
+            'save': False,
             'shell_interface': None,
             'stop_after_init': False,
             'translate_in': '',
@@ -366,6 +361,19 @@ class TestConfigManager(TransactionCase):
             'osv_memory_age_limit': 'False',
             'proxy_access_token': '',
 
+            # multiprocessing
+            **({
+                'workers': 0,
+                'limit_memory_soft': 2048 * 1024 * 1024,
+                'limit_memory_soft_gevent': False,
+                'limit_memory_hard': 2560 * 1024 * 1024,
+                'limit_memory_hard_gevent': False,
+                'limit_time_cpu': 60,
+                'limit_time_real': 120,
+                'limit_time_real_cron': -1,
+                'limit_request': 1 << 16,
+            } if IS_POSIX else {}),
+
             # new options since 14.0
             'db_maxconn_gevent': False,
             'db_replica_host': False,
@@ -375,33 +383,12 @@ class TestConfigManager(TransactionCase):
             'gevent_port': 8072,
             'smtp_ssl_certificate_filename': False,
             'smtp_ssl_private_key_filename': False,
-            'websocket_keep_alive_timeout': '3600',
-            'websocket_rate_limit_burst': '10',
-            'websocket_rate_limit_delay': '0.2',
+            'websocket_keep_alive_timeout': 3600,
+            'websocket_rate_limit_burst': 10,
+            'websocket_rate_limit_delay': 0.2,
             'x_sendfile': False,
-        }
-        if IS_POSIX:
-            # multiprocessing
-            assert_options.update(
-                {
-                    'workers': 0,
-                    'limit_memory_soft': 2048 * 1024 * 1024,
-                    'limit_memory_soft_gevent': False,
-                    'limit_memory_hard': 2560 * 1024 * 1024,
-                    'limit_memory_hard_gevent': False,
-                    'limit_time_cpu': 60,
-                    'limit_time_real': 120,
-                    'limit_time_real_cron': -1,
-                    'limit_request': 1 << 16,
-                }
-            )
+        })
 
-        config_path = file_path('base/tests/config/16.0.conf')
-        with self.assertLogs('odoo.tools.config', 'WARNING') as capture:
-            self.config._parse_config(['--config', config_path])
-        with self.assertNoLogs('py.warnings'):
-            self.config._warn_deprecated_options()
-        self.assertEqual(self.config.options, assert_options, "Options don't match")
         output = [
             (f"WARNING:odoo.tools.config:unknown option '{option}' in "
              f"the config file at {config_path}, option stored as-is, "
@@ -437,7 +424,7 @@ class TestConfigManager(TransactionCase):
 
             # common
             'config': None,
-            'save': None,
+            'save': False,
             'init': {'hr': 1, 'stock': 1},
             'update': {'account': 1, 'website': 1},
             'without_demo': 'rigolo',
@@ -560,10 +547,12 @@ class TestConfigManager(TransactionCase):
         error.assert_has_calls(3 * [call("the i18n-import option cannot be used without the language (-l) and the database (-d) options")])
 
     @patch('optparse.OptionParser.error')
-    def test_08_overwrite_existing_translations_incompatible_with_translate_in_or_update(self, error):
-        self.parse_reset(['--i18n-overwrite', '-l', 'fr_FR'])
+    def test_08_overwrite_existing_translations_requires_translate_in_or_update(self, error):
+        self.parse_reset(['--i18n-overwrite'])
+        error.assert_has_calls(1 * [call("the i18n-overwrite option cannot be used without the i18n-import option or without the update option")])
+        error.reset_mock()
         self.parse_reset(['--i18n-overwrite', '-u', 'base'])
-        error.assert_has_calls(2 * [call("the i18n-overwrite option cannot be used without the i18n-import option or without the update option")])
+        error.assert_not_called()
 
     @patch('optparse.OptionParser.error')
     def test_09_translate_out_requires_db_name(self, error):