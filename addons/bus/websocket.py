13c64a780d8b42f852cecd27bc06ebb1f6ce1589
tsm-odoo <tsm@odoo.com>
2024-11-12 12:03:28 +0000

    [FIX] bus: log websocket session downgrade
    Debugging websocket can be tough as we don't know
    when a session is downgraded. Some messages can be
    received, giving the impression that everything is
    working fine while the socket is actually linked to
    a public user.
    
    This PR introduces some logs to help debugging this
    scenario.
    
    opw-4218953
    
    closes odoo/odoo#186971
    
    X-original-commit: 21efbc31483fa806e8abbe18053e10d1121cc7f1
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/bus/websocket.py
+++ addons/bus/websocket.py
@@ -961,6 +961,14 @@ class WebsocketConnectionHandler:
         headers = request.httprequest.headers
         origin_url = urlparse(headers.get('origin'))
         if origin_url.netloc != headers.get('host') or origin_url.scheme != request.httprequest.scheme:
+            _logger.warning(
+                'Downgrading websocket session. Host=%(host)s, Origin=%(origin)s, Scheme=%(scheme)s.',
+                {
+                    'host': headers.get('host'),
+                    'origin': headers.get('origin'),
+                    'scheme': request.httprequest.scheme,
+                },
+            )
             session = root.session_store.new()
             session.update(get_default_session(), db=request.session.db)
             root.session_store.save(session)
