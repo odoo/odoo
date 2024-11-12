b78d9c38c3947d80ec09a98920a5b9a410634c46
Ethan Vincent <etvi@odoo.com>
2024-11-12 09:42:02 +0000

    [FIX] core: module state may be incorrect after successful installation
    Given the following module hierarchy:
    <pre>
    ...
        \_ test_install_base
            \_ test_install_auto
            \_ test_install_fail
    </pre>
    Where:
    * The auto-installed module has (for sake of example) some a model
      override of `ResCurrency` that defines a new method for the class.
      This module also has some data that defines an `IrCron` record, which
      executes the method added by the `ResCurrency` override within its own
      model files.
    * The data error module has a view extension which has some bad syntax
      that will trigger a failure in `load_data()`.
    
    Upon running an odoo instance with `-i test_install_fail`, the registry
    loading will proceed until failing at this module.  As expected, the
    module test_install_auto has been installed just before the failing one.
    The CRON data loaded from test_install_auto is in the database.
    However, the module itself is not marked as 'installed', and the cron
    will fail to execute, since the module test_install_auto is not loaded
    by the registry.
    
    The problem occurs because a package's data/schema changes are committed
    prior to actually marking the module record as 'installed'.  Moreover,
    the module's state change is not committed right away, which causes that
    change to be part of the next module's loading transaction.
    
    When the module test_install_fail fails to install, the current
    transaction is rolled back, and the module's state is reset to
    'uninstalled'.  Because the state change of module test_install_auto is
    part of the rolled back transaction, that module remains in state 'to
    install', and is eventually also reset to 'uninstalled'.
    
    Because of this, we are left with a CRON record from test_install_auto,
    but the latter is not loaded by the registry.  This causes the CRON to
    fail to execute, because of a missing method used by the CRON.
    
    closes odoo/odoo#194625
    
    X-original-commit: 0e72fca3617edf1f1eb7ae012bcdccb73c460ff6
    Signed-off-by: Raphael Collet <rco@odoo.com>
    Signed-off-by: Vincent Ethan <etvi@odoo.com>

--- odoo/addons/base/tests/__init__.py
+++ odoo/addons/base/tests/__init__.py
@@ -19,6 +19,7 @@ from . import test_groups
 from . import test_http_case
 from . import test_i18n
 from . import test_image
+from . import test_install
 from . import test_avatar_mixin
 from . import test_ir_actions
 from . import test_ir_attachment
---
+++ odoo/addons/base/tests/test_install_addons/test_install_auto/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'test_install_auto',
+    'version': '1.0',
+    'category': 'Hidden/Tools',
+    'description': "",
+    'depends': ['test_install_base'],
+    'data': [
+        'data/ir_cron.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
