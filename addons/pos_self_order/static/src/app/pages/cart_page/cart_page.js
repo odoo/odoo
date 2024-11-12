b76dc48db0ae37ffca25fc61b70ffb1efd6e2225
Pedram (pebr) <pebr@odoo.com>
2024-11-12 15:36:49 +0000

    [FIX] pos_self_order: prevent missing payment step with slow internet
    Before this commit, with a slow internet connection, if a user added an
    item to the order and pressed the pay button very quickly (before
    completing the price loading), it would bypass the payment step and
    directly give the receipt.
    
    opw-4305214
    
    X-original-commit: 9825c42a4d9a6c9592eb6895b0ddd8885bceb53b

--- addons/pos_self_order/static/src/app/pages/cart_page/cart_page.js
+++ addons/pos_self_order/static/src/app/pages/cart_page/cart_page.js
@@ -49,7 +49,11 @@ export class CartPage extends Component {
         const type = this.selfOrder.config.self_ordering_mode;
         const takeAway = this.selfOrder.currentOrder.takeaway;
 
-        if (this.selfOrder.rpcLoading || !this.selfOrder.verifyCart()) {
+        if (
+            this.selfOrder.rpcLoading ||
+            !this.selfOrder.verifyCart() ||
+            !this.selfOrder.verifyPriceLoading()
+        ) {
             return;
         }
 
--- addons/pos_self_order/static/src/app/self_order_service.js
+++ addons/pos_self_order/static/src/app/self_order_service.js
@@ -832,6 +832,16 @@ export class SelfOrder extends Reactive {
         return result;
     }
 
+    verifyPriceLoading() {
+        if (this.priceLoading) {
+            this.notification.add(_t("Please wait until the price is loaded"), {
+                type: "danger",
+            });
+            return false;
+        }
+        return true;
+    }
+
     getProductDisplayPrice(product) {
         const pricelist = this.config.pricelist_id;
         const price = product.get_price(pricelist, 1, 0, false, product.list_price);
