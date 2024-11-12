67dc3481dbd45e6938d8edf9912704ffd7ec90be
Romaric MOYEUVRE <romo@odoo.com>
2024-11-12 08:57:42 +0100

    [FIX] web_editor: fix custom colors in colorpicker
    Before this commit, the colorpalette for the background color would
    not display custom colors already on the page. The issue appeared in
    17.0 and worked in 16.0 before the conversion of wysiwyg to owl.
    github.com/#118966
    
    The "getEditableCustomColors" method was not forwarded and therefore
    we used the default function, returning nothing.
    
    Steps to reproduce the issue:
    - Add a snippet
    - Set the background / font color of the snippet to a custom color
    - Add another snippet
    - Open the colorpicker for the text color
    (The colorpicker have access to the custom color set above)
    - Open the colorpicker for the background color
    (The colorpicker does not have access to the custom color set above)
    
    task-3806989

--- addons/web_editor/static/src/js/editor/snippets.options.js
+++ addons/web_editor/static/src/js/editor/snippets.options.js
@@ -1818,6 +1818,7 @@ const ColorpickerUserValueWidget = SelectUserValueWidget.extend({
         if (wysiwyg) {
             options.document = this.$target[0].ownerDocument;
             options.getTemplate = wysiwyg.getColorpickerTemplate.bind(wysiwyg);
+            options.getEditableCustomColors = wysiwyg.colorPalettesProps?.background?.getEditableCustomColors;
         }
         this.colorPaletteWrapper?.destroy();
         const sidebarDocument = this.colorPaletteEl.ownerDocument;
