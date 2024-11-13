457b8cdc00807c8b5cf17a5b7995a3b6ecbfc378
Yanbin Li <liya@odoo.com>
2024-11-13 11:37:54 -0500

    [FIX] pos_mrp: Correct total cost when kit products share components
    **Current behavior:**
    When computing the total cost for a kit product in a POS order, extra stock moves are included if another POS order line has a product that shares the same components. This results in an incorrect total cost displayed on the POS order, which does not match the computed price from the BOM shown on the product page.
    
    **Expected behavior:**
    The total cost for the POS order should match the computed BOM price shown on the product page.
    
    **Steps to reproduce:**
    
    1. Create two products: Product 1 and Product 2.
    2. Set both products to be tracked by "quantity" or make them "storable" and assign them to a product category with "average cost" as the costing method (instead of "standard price").
    3. Create a Bill of Materials for each product, ensuring they share at least one component. Make sure BoM Type is 'kit'
       - Tip: For better visibility of the error, assign a quantity of 10 or more to the shared component in Product 2's BOM, and set the shared component’s `standard_price` (cost) to 5 or higher. This will make the discrepancy in the total cost more apparent.
    4. Create the shared component as a new product and assign it a price.
    5. Open a new POS order, add Product 1 and Product 2, and close the order.
    6. Locate the order and observe that the "total cost" is incorrect.
    
    **Cause of the issue:**
    The stock move filter does not consider that stock moves for BOM lines need to belong to the current product. As a result, if two kit products share components, the stock moves for one product are mistakenly included in the calculation for the other, leading to an incorrect total cost.
    
    **Fix:**
    When filtering out the stock moves, ensure they are correctly associated with the current product by verifying that each stock move’s BOM line belongs specifically to the current product and does not include nested BOMs.
    
    opw-4274532

--- addons/pos_mrp/models/pos_order.py
+++ addons/pos_mrp/models/pos_order.py
@@ -8,12 +8,14 @@ class PosOrderLine(models.Model):
 
     def _get_stock_moves_to_consider(self, stock_moves, product):
         self.ensure_one()
-        bom = product.env['mrp.bom']._bom_find(product, company_id=stock_moves.company_id.id, bom_type='phantom')[product]
+        bom = product.env['mrp.bom']._bom_find(product, company_id=stock_moves.company_id.id, bom_type='phantom').get(product)
         if not bom:
             return super()._get_stock_moves_to_consider(stock_moves, product)
-        _dummy, components = bom.explode(product, self.qty)
+        boms, components = bom.explode(product, self.qty)
         ml_product_to_consider = (product.bom_ids and [comp[0].product_id.id for comp in components]) or [product.id]
-        return stock_moves.filtered(lambda ml: ml.product_id.id in ml_product_to_consider and ml.bom_line_id)
+        if len(boms) > 1:
+            return stock_moves.filtered(lambda ml: ml.product_id.id in ml_product_to_consider and ml.bom_line_id)
+        return stock_moves.filtered(lambda ml: ml.product_id.id in ml_product_to_consider and (ml.bom_line_id in bom.bom_line_ids))
 
 class PosOrder(models.Model):
     _inherit = "pos.order"
