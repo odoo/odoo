94a3be37d094b934678b1c5ca575807bc369b9c8
maad-odoo <maad@odoo.com>
2024-11-13 14:49:35 +0530

    [IMP] project: covert tests to hoot
    Purpose for this commit:
    To convert the qunit test cases that depends on the
    'mail/../tests/helpers/test_utils'
    
    Part of task-3818666
    
    Part-of: odoo/odoo#187029
    Related: odoo/enterprise#73789
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/project/static/tests/project_task_kanban_view.test.js
+++ addons/project/static/tests/project_task_kanban_view.test.js
@@ -1,11 +1,30 @@
 import { describe, expect, test } from "@odoo/hoot";
+import { animationFrame } from "@odoo/hoot-dom";
 
-import { mountView } from "@web/../tests/web_test_helpers";
+import { mountView, onRpc } from "@web/../tests/web_test_helpers";
 
 import { defineProjectModels, ProjectTask } from "./project_models";
 
 defineProjectModels();
 describe.current.tags("desktop");
+
+const viewParams = {
+    resModel: "project.task",
+    type: "kanban",
+    arch: `
+        <kanban js_class="project_task_kanban">
+            <templates>
+                <t t-name="card">
+                    <field name="name"/>
+                </t>
+            </templates>
+        </kanban>`,
+    context: {
+        active_model: "project.project",
+        default_project_id: 1,
+    },
+    groupBy: ["stage_id"],
+};
 
 test("shadow stages should be displayed in the project Kanban", async () => {
     ProjectTask._records = [];
@@ -23,11 +42,25 @@ test("shadow stages should be displayed in the project Kanban", async () => {
             </kanban>
         `,
         context: {
-            'active_model': "project.task.type.delete.wizard",
-            'default_project_id': 1,
+            active_model: "project.task.type.delete.wizard",
+            default_project_id: 1,
         },
     });
 
     expect(".o_kanban_header").toHaveCount(1);
     expect(".o_kanban_example_background_container").toHaveCount(1);
+});
+
+test("quick create button is visible when the user has access rights.", async () => {
+    onRpc("has_group", () => true);
+    await mountView(viewParams);
+    await animationFrame();
+    expect(".o_column_quick_create").toHaveCount(1);
+});
+
+test("quick create button is not visible when the user not have access rights", async () => {
+    onRpc("has_group", () => false);
+    await mountView(viewParams);
+    await animationFrame();
+    expect(".o_column_quick_create").toHaveCount(0);
 });
