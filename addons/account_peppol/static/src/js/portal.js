odoo/odoo

commit 857b9a188c77cef2e3cd8a1c6b3035e7cd8d1305

[IMP] account,*: partner invoice sending defaults on portal
*: snailmail_account, account_peppol

With https://github.com/odoo/odoo/pull/180212 we added the possibility
to set invoice sending preferences on the partner.
With this PR, we add the possibility for users to set it themselves
from their portal account view.

task-4070132

X-original-commit: de567b6b41309321e07e4c4ad0c5f226286b3787

addons/account_peppol/static/src/js/portal.js
@@ -1,0 +1,32 @@
+/** @odoo-module **/
+
+import publicWidget from "@web/legacy/js/public/public_widget";
+
+publicWidget.registry.portalDetails = publicWidget.Widget.extend({
+    selector: '.o_portal_details',
+    events: {
+        'change select[name="invoice_sending_method"]': '_onSendingMethodChange',
+    },
+
+    start() {
+        this._showPeppolConfig();
+        this.orm = this.bindService("orm");
+        return this._super.apply(this, arguments);
+    },
+
+    _showPeppolConfig() {
+        const method = document.querySelector("select[name='invoice_sending_method']").value;
+        const divToToggle = document.querySelectorAll(".portal_peppol_toggle");
+        for (const peppolDiv of divToToggle) {
+            if (method === "peppol") {
+                peppolDiv.classList.remove("d-none")
+            } else {
+                peppolDiv.classList.add("d-none")
+            }
+        }
+    },
+
+    _onSendingMethodChange() {
+        this._showPeppolConfig();
+    },
+});

addons/account/controllers/portal.py
@@ -205,3 +205,19 @@ class PortalAccount(CustomerPortal):
                 error[field.name] = 'error'
                 error_message.append(_('The field %s must be filled.', field.field_description.lower()))
         return error, error_message
+
+    def _get_optional_fields(self):
+        # EXTENDS 'portal
+        optional_fields = super()._get_optional_fields()
+        optional_fields.extend(('invoice_sending_method', 'invoice_edi_format'))
+        return optional_fields
+
+    def _prepare_portal_layout_values(self):
+        # EXTENDS 'portal'
+        portal_layout_values = super()._prepare_portal_layout_values()
+        partner = request.env.user.partner_id
+        portal_layout_values.update({
+            'invoice_sending_methods': {'email': _('by Email')},
+            'invoice_edi_formats': dict(partner._fields['invoice_edi_format'].selection),
+        })
+        return portal_layout_values

addons/account/i18n/account.pot
@@ -728,6 +728,14 @@ msgid ""
 msgstr ""
 
 #. module: account
+#: model_terms:ir.ui.view,arch_db:account.portal_my_details_fields
+msgid ""
+"<small class=\"form-text text-muted\">\n"
+"                    You can choose how you want us to send your invoices, and with which electronic format.\n"
+"                </small>"
+msgstr ""
+
+#. module: account
 #: model_terms:ir.ui.view,arch_db:account.portal_my_invoices
 msgid ""
 "<span class=\"badge rounded-pill text-bg-warning\">\n"
@@ -6398,6 +6406,11 @@ msgid "Electronic Data Interchange"
 msgstr ""
 
 #. module: account
+#: model_terms:ir.ui.view,arch_db:account.portal_my_details_fields
+msgid "Electronic format"
+msgstr ""
+
+#. module: account
 #: model:ir.model.fields,field_description:account.field_account_journal__alias_email
 #: model_terms:ir.ui.view,arch_db:account.view_account_journal_form
 msgid "Email Alias"
@@ -12035,20 +12048,6 @@ msgid "RD Expenses"
 msgstr ""
 
 #. module: account
-#: model:ir.model.fields,field_description:account.field_account_account__rating_ids
-#: model:ir.model.fields,field_description:account.field_account_bank_statement_line__rating_ids
-#: model:ir.model.fields,field_description:account.field_account_journal__rating_ids
-#: model:ir.model.fields,field_description:account.field_account_move__rating_ids
-#: model:ir.model.fields,field_description:account.field_account_payment__rating_ids
-#: model:ir.model.fields,field_description:account.field_account_reconcile_model__rating_ids
-#: model:ir.model.fields,field_description:account.field_account_setup_bank_manual_config__rating_ids
-#: model:ir.model.fields,field_description:account.field_account_tax__rating_ids
-#: model:ir.model.fields,field_description:account.field_res_company__rating_ids
-#: model:ir.model.fields,field_description:account.field_res_partner_bank__rating_ids
-msgid "Ratings"
-msgstr ""
-
-#. module: account
 #: model_terms:ir.ui.view,arch_db:account.account_resequence_view
 msgid "Re-Sequence"
 msgstr ""
@@ -12097,6 +12096,11 @@ msgstr ""
 #. module: account
 #: model:ir.model.fields.selection,name:account.selection__account_payment_register__payment_type__inbound
 msgid "Receive Money"
+msgstr ""
+
+#. module: account
+#: model_terms:ir.ui.view,arch_db:account.portal_my_details_fields
+msgid "Receive invoices"
 msgstr ""
 
 #. module: account
@@ -17609,6 +17613,8 @@ msgid "bills for"
 msgstr ""
 
 #. module: account
+#. odoo-python
+#: code:addons/account/controllers/portal.py:0
 #: model:ir.model.fields.selection,name:account.selection__res_partner__invoice_sending_method__email
 msgid "by Email"
 msgstr ""

addons/account/views/account_portal_templates.xml
@@ -249,4 +249,39 @@
             </div>
         </t>
     </template>
+
+    <template id="portal_my_details_fields" inherit_id="portal.portal_my_details_fields">
+        <xpath expr="//select[@name='country_id']/.." position="after">
+            <div class="col-12 d-none d-xl-block">
+                <small class="form-text text-muted">
+                    You can choose how you want us to send your invoices, and with which electronic format.
+                </small>
+            </div>
+            <div class="row m-0 p-0" t-if="len(invoice_sending_methods) > 1">
+                <div class="col-xl-6">
+                    <label class="col-form-label" for="invoice_sending_method">Receive invoices</label>
+                    <select name="invoice_sending_method" class="form-select">
+                        <t t-foreach="invoice_sending_methods" t-as="method">
+                            <option t-att-value="method" t-att-selected="(invoice_sending_method or partner.invoice_sending_method) == method">
+                                <t t-esc="method_value"/>
+                            </option>
+                        </t>
+                    </select>
+                </div>
+            </div>
+            <div class="row m-0 mb-3 p-0">
+                <div class="col-xl-6">
+                    <label class="col-form-label" for="invoice_edi_format">Electronic format</label>
+                    <select name="invoice_edi_format" class="form-select">
+                        <option value=""/>
+                        <t t-foreach="invoice_edi_formats" t-as="format">
+                            <option t-att-value="format" t-att-selected="(invoice_edi_format or partner.invoice_edi_format) == format">
+                                <t t-esc="format_value"/>
+                            </option>
+                        </t>
+                    </select>
+                </div>
+            </div>
+        </xpath>
+    </template>
 </odoo>

addons/account_peppol/__init__.py
@@ -1,5 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import controllers
 from . import models
 from . import wizard
 from . import tools

addons/account_peppol/__manifest__.py
@@ -19,6 +19,7 @@
         'security/ir.model.access.csv',
         'views/account_journal_dashboard_views.xml',
         'views/account_move_views.xml',
+        'views/account_portal_templates.xml',
         'views/res_partner_views.xml',
         'views/res_config_settings_views.xml',
         'wizard/peppol_registration_views.xml',
@@ -32,6 +33,9 @@
     'assets': {
         'web.assets_backend': [
             'account_peppol/static/src/components/**/*',
+        ],
+        'web.assets_frontend': [
+            'account_peppol/static/src/js/*',
         ],
     }
 }

addons/account_peppol/controllers/__init__.py
@@ -1,0 +1,1 @@
+from . import portal

addons/account_peppol/controllers/portal.py
@@ -1,0 +1,62 @@
+from odoo import _
+from odoo.addons.portal.controllers.portal import CustomerPortal
+from odoo.addons.account.models.company import PEPPOL_LIST
+from odoo.http import request
+
+
+class PortalAccount(CustomerPortal):
+
+    # ------------------------------------------------------------
+    # My Account
+    # ------------------------------------------------------------
+
+    def _prepare_portal_layout_values(self):
+        # EXTENDS 'portal'
+        portal_layout_values = super()._prepare_portal_layout_values()
+        can_send = request.env['account_edi_proxy_client.user']._get_can_send_domain()
+        if request.env.company.account_peppol_proxy_state in can_send:
+            partner = request.env.user.partner_id
+            portal_layout_values['invoice_sending_methods'].update({'peppol': _('by Peppol')})
+            portal_layout_values.update({
+                'peppol_eas_list': dict(partner._fields['peppol_eas'].selection),
+            })
+        return portal_layout_values
+
+    def _get_mandatory_fields(self):
+        # EXTENDS 'portal'
+        mandatory_fields = super()._get_mandatory_fields()
+
+        sending_method = request.params.get('invoice_sending_method')
+        if sending_method == 'peppol':
+            mandatory_fields += ['peppol_eas', 'peppol_endpoint', 'invoice_edi_format']
+
+        return mandatory_fields
+
+    def _get_optional_fields(self):
+        # EXTENDS 'portal'
+        optional_fields = super()._get_optional_fields()
+
+        sending_method = request.params.get('invoice_sending_method')
+        if sending_method and sending_method != 'peppol':
+            optional_fields += ['peppol_eas', 'peppol_endpoint']
+        return optional_fields
+
+    def details_form_validate(self, data, partner_creation=False):
+        # EXTENDS 'portal'
+        error, error_message = super().details_form_validate(data, partner_creation=False)
+
+        if data.get('invoice_sending_method') == 'peppol':
+            peppol_eas = data.get('peppol_eas')
+            peppol_endpoint = data.get('peppol_endpoint')
+            edi_format = data.get('invoice_edi_format')
+            if request.env['res.country'].browse(int(data.get('country_id'))).code not in PEPPOL_LIST:
+                error['country_id'] = 'error'
+                error_message.append(_('That country is not available for Peppol.'))
+            if endpoint_error_message := request.env['res.partner']._build_error_peppol_endpoint(peppol_eas, peppol_endpoint):
+                error['invalid_peppol_endpoint'] = 'error'
+                error_message.append(endpoint_error_message)
+            if request.env['res.partner']._get_peppol_verification_state(peppol_endpoint, peppol_eas, edi_format) != 'valid':
+                error['invalid_peppol_config'] = 'error'
+                error_message.append(_('If you want to be invoiced by Peppol, your configuration must be valid.'))
+
+        return error, error_message

commit b3c64be29f142a8829d50621f96c6d8ee375cbfd

[FIX] account_peppol: resolve conflict with duplicate publicwidget name
Steps to reproduce:
1. Go to website > My Account > Edit information
2. Change the country from say India to United States
3. Click on the state drop down, which then appears blank.

The issue occurs because the `account_peppol` module extends the public
widget by using the same name as the existing `portalDetails` public
widget from the portal module, effectively overriding it. This causes
certain functionalities to be lost when `account_peppol` is installed.

This commit modifies the `account_peppol` module to extend the existing
portalDetails widget instead of overriding it.

Issue introduced in commit: https://github.com/odoo/odoo/commit/857b9a188c77cef2e3cd8a1c6b3035e7cd8d1305

task-4310315

addons/account_peppol/static/src/js/portal.js
@@ -2,16 +2,15 @@
 
 import publicWidget from "@web/legacy/js/public/public_widget";
 
-publicWidget.registry.portalDetails = publicWidget.Widget.extend({
-    selector: '.o_portal_details',
-    events: {
-        'change select[name="invoice_sending_method"]': '_onSendingMethodChange',
-    },
+publicWidget.registry.portalDetails.include({
+    events: Object.assign({}, publicWidget.registry.portalDetails.prototype.events, {
+        "change select[name='invoice_sending_method']": "_onSendingMethodChange",
+    }),
 
     start() {
         this._showPeppolConfig();
         this.orm = this.bindService("orm");
-        return this._super.apply(this, arguments);
+        return this._super(...arguments);
     },
 
     _showPeppolConfig() {
@@ -19,9 +18,9 @@ publicWidget.registry.portalDetails = publicWidget.Widget.extend({
         const divToToggle = document.querySelectorAll(".portal_peppol_toggle");
         for (const peppolDiv of divToToggle) {
             if (method === "peppol") {
-                peppolDiv.classList.remove("d-none")
+                peppolDiv.classList.remove("d-none");
             } else {
-                peppolDiv.classList.add("d-none")
+                peppolDiv.classList.add("d-none");
             }
         }
     },