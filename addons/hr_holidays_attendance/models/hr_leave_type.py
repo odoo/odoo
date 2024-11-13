8ab5af7efe8d4bab0702f6d392c11fc0febbce0f
Ahmad Khanalizadeh <khah@odoo.com>
2024-11-13 14:56:55 +0100

    [IMP] hr_holidays(_attendance): extra hours as allocation option
    This commit adds "Based on Extra Hours" as a selection for the
    field `requires_allocation` and removes the corresponding bool
    field. The reason is that the option does not work when the
    time off type requires allocation.
    
    task-4317343

--- addons/hr_holidays_attendance/models/hr_leave_type.py
+++ addons/hr_holidays_attendance/models/hr_leave_type.py
@@ -8,11 +8,10 @@ from odoo import _, api, fields, models
 class HrLeaveType(models.Model):
     _inherit = 'hr.leave.type'
 
-    overtime_deductible = fields.Boolean(
-        "Deduct Extra Hours", default=False,
-        help="Once a time off of this type is approved, extra hours in attendances will be deducted.")
+    requires_allocation = fields.Selection(selection_add=[('extra_hours', 'Based on Extra Hours')], ondelete={'extra_hours': 'set default'})
+    display_extra_hours = fields.Boolean(related='company_id.hr_attendance_display_overtime')
 
-    @api.depends('overtime_deductible', 'requires_allocation')
+    @api.depends('requires_allocation')
     @api.depends_context('request_type', 'leave', 'holiday_status_display_name', 'employee_id')
     def _compute_display_name(self):
         # Exclude hours available in allocation contexts, it might be confusing otherwise
@@ -23,7 +22,7 @@ class HrLeaveType(models.Model):
         if employee.total_overtime <= 0:
             return super()._compute_display_name()
 
-        overtime_leaves = self.filtered(lambda l_type: l_type.overtime_deductible and l_type.requires_allocation == 'no')
+        overtime_leaves = self.filtered(lambda l_type: l_type.requires_allocation == 'extra_hours')
         for leave_type in overtime_leaves:
             leave_type.display_name = "%(name)s (%(count)s)" % {
                 'name': leave_type.name,
@@ -35,8 +34,7 @@ class HrLeaveType(models.Model):
     def get_allocation_data(self, employees, date=None):
         res = super().get_allocation_data(employees, date)
         deductible_time_off_types = self.env['hr.leave.type'].search([
-            ('overtime_deductible', '=', True),
-            ('requires_allocation', '=', 'no')])
+            ('requires_allocation', '=', 'extra_hours')])
         leave_type_names = deductible_time_off_types.mapped('name')
         for employee in res:
             for leave_data in res[employee]:
@@ -49,5 +47,5 @@ class HrLeaveType(models.Model):
 
     def _get_days_request(self, date=None):
         res = super()._get_days_request(date)
-        res[1]['overtime_deductible'] = self.overtime_deductible
+        res[1]['overtime_deductible'] = self.requires_allocation == 'extra_hours'
         return res
