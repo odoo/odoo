7baba7ce5208ee9dac376d355a67eb35421ea3bf
Roy Le <roy.le@viindoo.com>
2024-11-13 13:59:05 +0700

    [FIX] purchase_stock: use currency rate at bill date when invoice before receipt
    In some business case when the invoice is done before the receipt, the
    `stock.valuation.layer` currency rate will be taken on the bill rather
    than the current date.
    
    To reproduce the issue:
    (Need account_accountant)
    1. Create a product category PC
       - Costing method: FIFO
       - Inventory valuation: Automated
    2. Create a product P
       - Type: Storable
       - Category: PC
    3. On 10/11/2024, confirm a PO with 1 x P at Є10
    4. On 11/11/2024, bill it with currency rate of 1.06 at $10.6
    4. On 12/11/2024, receive P with currency rate of 1.07 at $10.7
    
    Errors: The price SVL is different from the bill
    
    Solution: Use currency rate at bill date rather
    than the current date when invoice before receipt

--- addons/purchase_stock/models/stock_move.py
+++ addons/purchase_stock/models/stock_move.py
@@ -86,8 +86,12 @@ class StockMove(models.Model):
             # in assigned state. However, the move date is the scheduled date until move is
             # done, then date of actual move processing. See:
             # https://github.com/odoo/odoo/blob/2f789b6863407e63f90b3a2d4cc3be09815f7002/addons/stock/models/stock_move.py#L36
+            convert_date = fields.Date.context_today(self)
+            # use currency rate at bill date when invoice before receipt
+            if float_compare(line.qty_invoiced, received_qty, precision_rounding=line.product_uom.rounding) > 0:
+                convert_date = max(line.sudo().invoice_lines.move_id.filtered(lambda m: m.state == 'posted').mapped('invoice_date'), default=convert_date)
             price_unit = order.currency_id._convert(
-                price_unit, order.company_id.currency_id, order.company_id, fields.Date.context_today(self), round=False)
+                price_unit, order.company_id.currency_id, order.company_id, convert_date, round=False)
         return price_unit
 
     def _generate_valuation_lines_data(self, partner_id, qty, debit_value, credit_value, debit_account_id, credit_account_id, svl_id, description):
