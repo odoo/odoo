f3593ab62eab8d9f956ebc87e4b75388a1b1df73
David Monnom (moda) <moda@odoo.com>
2024-11-13 13:28:40 +0100

    [IMP] event, event_sale, pos_event: share state between event and pos
    Before the `state` fields was only managed with event_sale. But since we
    are able to sell tickets from the POS, we need to share the state
    between these modules to have a coherent state.
    
    This commit adds the `state` field to the `event.registration` model
    directly in `event` module. This way, the state is shared between
    `event`, `event_sale` and `pos_event`.
    
    This commit also add a `oe_stat_button` to the `event.registration`
    form view to be able to access to its pos_order directly.
    
    closes odoo/odoo#187066
    
    Taskid: 4330905
    Related: odoo/upgrade#6794
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/pos_event/models/event_registration.py
+++ addons/pos_event/models/event_registration.py
@@ -1,5 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from odoo import fields, models, api
+from odoo.tools import float_is_zero
 
 
 class EventRegistration(models.Model):
@@ -8,6 +9,24 @@ class EventRegistration(models.Model):
 
     pos_order_id = fields.Many2one(related='pos_order_line_id.order_id', string='PoS Order')
     pos_order_line_id = fields.Many2one('pos.order.line', string='PoS Order Line', ondelete='cascade', copy=False)
+
+    def _has_order(self):
+        return super()._has_order() or self.pos_order_id
+
+    @api.depends('pos_order_id.state', 'pos_order_id.currency_id', 'pos_order_id.amount_total')
+    def _compute_registration_status(self):
+        if self.pos_order_id:
+            for registration in self:
+                if registration.pos_order_id.state == 'cancel':
+                    registration.state = 'cancel'
+                elif float_is_zero(registration.pos_order_id.amount_total, precision_rounding=registration.pos_order_id.currency_id.rounding):
+                    registration.sale_status = 'free'
+                    registration.state = 'open'
+                else:
+                    registration.sale_status = 'sold'
+                    registration.state = 'open'
+
+        super()._compute_registration_status()
 
     @api.model
     def _load_pos_data_domain(self, data):
@@ -33,3 +52,9 @@ class EventRegistration(models.Model):
         session_ids = self.env['pos.session'].sudo().search([("state", "!=", "closed")])
         if len(session_ids) > 0:
             session_ids.config_id._update_events_seats(self.event_id)
+
+    def action_view_pos_order(self):
+        action = self.env["ir.actions.actions"]._for_xml_id("point_of_sale.action_pos_pos_form")
+        action['views'] = [(False, 'form')]
+        action['res_id'] = self.pos_order_id.id
+        return action
