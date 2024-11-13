b5d80cab249da05c2a5c1ade54e78480dac66465
Andrzej(pian) <pian@odoo.com>
2024-11-13 09:27:34 +0100

    [FIX] Reverted previous fix, now we're writing NULL to db
    but we still get False in the create vals, so we have to detect that and
    set it to default

--- addons/calendar/models/calendar_event.py
+++ addons/calendar/models/calendar_event.py
@@ -435,13 +435,15 @@ class Meeting(models.Model):
         # Prevent sending update notification when _inverse_dates is called
         self = self.with_context(is_calendar_event_new=True)
 
-        for vals in vals_list:
-            if 'user_id' not in vals:  # Else bug with quick_create when we are filter on an other user
-                vals.setdefault('user_id', self.env.user.id)
-            if vals.get('res_id') == 0:
-                vals.pop('res_id')
+        vals_list = [  # Else bug with quick_create when we are filter on an other user
+            dict(vals, user_id=self.env.user.id) if not 'user_id' in vals else vals
+            for vals in vals_list
+        ]
 
         defaults = self.default_get(['activity_ids', 'res_model_id', 'res_id', 'user_id', 'res_model', 'partner_ids'])
+        for vals in vals_list:
+            if defaults.get('res_id') and not vals.get('res_id'):
+                vals.update({'res_id': defaults['res_id']})
         meeting_activity_type = self.env['mail.activity.type'].search([('category', '=', 'meeting')], limit=1)
         # get list of models ids and filter out None values directly
         model_ids = list(filter(None, {values.get('res_model_id', defaults.get('res_model_id')) for values in vals_list}))
