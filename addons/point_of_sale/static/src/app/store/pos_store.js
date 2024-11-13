6ef111f600a65da7c9ee4bb581168eab23c03e62
manv-afk <manv@odoo.com>
2024-11-13 14:31:35 +0000

    [IMP] point_of_sale: Do not display lot number that are not available
    Only display lot/serial numbers if their forecasted quantity is available. Now when using a 2 step routes (inventory) we only show in PoS the serial numbers that are not already reserved for a stock move (like in sales).
    To do this we return the `stock.lot` that have a related `stock.quant` which  is located in the `pos_config` default source location and have a quantity > 0.
    
    closes odoo/odoo#190493
    
    Task-id: 4320066
    X-original-commit: 4b91652f2639c66c962d6830b3614e5325c5835b
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Manu Vaillant (manv) <manv@odoo.com>

--- addons/point_of_sale/static/src/app/store/pos_store.js
+++ addons/point_of_sale/static/src/app/store/pos_store.js
@@ -1865,10 +1865,16 @@ export class PosStore extends Reactive {
 
         let existingLots = [];
         try {
-            existingLots = await this.data.call("pos.order.line", "get_existing_lots", [
-                this.company.id,
-                product.id,
-            ]);
+            existingLots = await this.data.call(
+                "pos.order.line",
+                "get_existing_lots",
+                [this.company.id, product.id],
+                {
+                    context: {
+                        config_id: this.config.id,
+                    },
+                }
+            );
             if (!canCreateLots && (!existingLots || existingLots.length === 0)) {
                 this.dialog.add(AlertDialog, {
                     title: _t("No existing serial/lot number"),
