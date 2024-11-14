449cb8f5e685aaa127dfbe1b4ef17f58399452fe
paru-odoo <paru@odoo.com>
2024-11-14 10:50:57 +0000

    [FIX] web_editor, *: resolve visibility issue at initial stage
    *: website
    
    Steps to reproduce:
    1. Drag and drop a popup snippet.
    2. Add a banner inside the popup snippet.
    3. Set the banner to be invisible on desktop by selecting `no_desktop`
    visibility on the `Block` element.
    
    Issue:
    The popup remains open, but the element is incorrectly marked as hidden
    in the invisibility elements list.
    
    Solution:
    In this commit, we call `trigger_up` to `activate_snippet` mode, which
    disables `snippet options`. Following this, we pass toggle visibility
    accordingly to the parent. The `visibility` variable will be set to
    `True` and passed to `snippetMenu`. Since we have already removed the
    `snippet editor options` by setting `activate_snippet` to `false`, it
    is unnecessary to call that function again.
    
    task-4337481
    
    closes odoo/odoo#205951
    
    X-original-commit: b78c9169f3a433b682fbb9bc75b5514a0ebabf6e
    Signed-off-by: Serge Bayet (seba) <seba@odoo.com>

--- addons/website/tests/test_ui.py
+++ addons/website/tests/test_ui.py
@@ -698,3 +698,6 @@ class TestUi(odoo.tests.HttpCase):
 
     def test_media_iframe_video(self):
         self.start_tour("/", "website_media_iframe_video", login="admin")
+
+    def test_popup_visibility_option(self):
+        self.start_tour("/", "website_popup_visibility_option", login="admin")
