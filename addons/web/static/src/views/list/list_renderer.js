5162f33a0200cc72170d1f524aceead9439c4fd0
Achraf (abz) <abz@odoo.com>
2024-11-14 09:32:19 +0000

    [FIX] web: Prevent traceback when users use shift click on several page
    Steps:
        - Open any list view with more than 1 page
        - apply a limit if you don't have enough records
        - Toggle first record checkbox
        - Click on `o_pager_next`
        - Shift click on another record
        - Traceback
    
    The problem arises when you change page after using range-selection with
    the shift key.
    
    Indeed, range-selection uses `this.lastCheckedRecord`, which is the last
    record clicked on to start the selection, except that if you change page
    (and if you use a filter), this element may no longer be in the current
    page.
    
    Currently, the code just checks that this element exists before attempting
    to create a selection by range.
    
    The fix consists in checking that this element exists in the list of
    records before attempting anything.
    
    opw-4284708
    
    X-original-commit: 3590d2fb29b455d84576eaa11a6bc6f4f1ff8016

--- addons/web/static/src/views/list/list_renderer.js
+++ addons/web/static/src/views/list/list_renderer.js
@@ -1815,7 +1815,8 @@ export class ListRenderer extends Component {
         if (!this.canSelectRecord) {
             return;
         }
-        if (this.shiftKeyMode && this.lastCheckedRecord) {
+        const isRecordPresent = this.props.list.records.includes(this.lastCheckedRecord);
+        if (this.shiftKeyMode && isRecordPresent) {
             this.toggleRecordShiftSelection(record);
         } else {
             record.toggleSelection();
