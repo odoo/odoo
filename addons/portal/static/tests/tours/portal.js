bacdbd6fec4c7b471fa8de7b4387d201f5fb0bdb
panv-odoo <panv@odoo.com>
2024-11-14 16:31:36 +0530

    [IMP] portal, *: allow portal users to update their profile picture *: website_profile Specification:
    Previously, only backend users could modify a contact/portal user's
    profile photo via the Contacts App.
    
    With this improvement, portal users now have the option to update their
    own profile picture directly from the portal. A test tour has also been
    added to validate this functionality.
    
    task-4049054
    
    Co-authored-by: Mohammed Shekha <msh@odoo.com>

--- addons/portal/static/tests/tours/portal.js
+++ addons/portal/static/tests/tours/portal.js
@@ -1,4 +1,7 @@
 import { registry } from "@web/core/registry";
+import {
+    registerWebsitePreviewTour,
+} from "@website/js/tours/tour_utils";
 
 registry.category("web_tour.tours").add('portal_load_homepage', {
     url: '/my',
@@ -29,3 +32,81 @@ registry.category("web_tour.tours").add('portal_load_homepage', {
         }
     ]
 });
+
+let previousProfilePictureSrc = "";
+
+function compareProfilePictures(currentSrc) {
+    if (currentSrc === previousProfilePictureSrc) {
+        throw new Error("Profile Picture is not updated!");
+    }
+}
+
+registerWebsitePreviewTour(
+    "portal_profile_update",
+    {
+        url: "/my/account",
+    },
+    () => [
+        {
+            content: "Ensure that profile picture for portal user is present",
+            trigger: ":iframe .o_wportal_avatar_img",
+        },
+        {
+            content: "Update the profile picture using edit button",
+            trigger: ":iframe .o_portal_picture_card",
+            run: async function () {
+                previousProfilePictureSrc = this.anchor.querySelector(".o_wportal_avatar_img").src;
+                const fileInputEl = this.anchor.querySelector(".o_file_upload");
+                const svgData =
+                    "PHN2ZwogICAgICAgICAgICAgICAgICAgIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIKICAgICAgICAgICAgICAgICAgICB2aWV3Qm94PSIwIDAgNSA1Ij4KICAgICAgICAgICAgICAgICAgICA8Y2lyY2xlIGN4PSIyLjUiIGN5PSIyLjUiIHI9IjIiIGZpbGw9IiNmZGQ4MzUiLz4KICAgICAgICAgICAgICAgICAgICA8Y2lyY2xlIGN4PSIxLjciIGN5PSIyIiByPSIwLjMiIGZpbGw9IiMwMDAiLz4KICAgICAgICAgICAgICAgICAgICA8Y2lyY2xlIGN4PSIzLjMiIGN5PSIyIiByPSIwLjMiIGZpbGw9IiMwMDAiLz4KICAgICAgICAgICAgICAgICAgICA8cGF0aCBkPSJNMS41IDMuMiBRMi41IDQsIDMuNSAzLjIiIHN0cm9rZT0iIzAwMCIgc3Ryb2tlLXdpZHRoPSIwLjIiIGZpbGw9Im5vbmUiIC8+CiAgICAgICAgICAgICAgICA8L3N2Zz4=";
+                const blob = new Blob([atob(svgData)], { type: "image/svg+xml" });
+                const simulatedImageFile = new File([blob], "smiley.svg", {
+                    type: blob.type,
+                });
+                const dataTransfer = new DataTransfer();
+                dataTransfer.items.add(simulatedImageFile);
+                fileInputEl.files = dataTransfer.files;
+
+                fileInputEl.dispatchEvent(new Event("change", { bubbles: true, cancelable: true }));
+            },
+        },
+        {
+            content: "Check if profile picture is updated successfully.",
+            trigger: ":iframe .o_wportal_avatar_img",
+            run: function () {
+                compareProfilePictures(this.anchor.src);
+            },
+        },
+        {
+            content: "Click on save button",
+            trigger: ":iframe button#save_address",
+            run: "click",
+        },
+        {
+            content: "Click on edit information",
+            trigger: ":iframe a[href*='/my/account']:contains('Edit information')",
+            run: "click",
+        },
+        {
+            content: "Click on delete button",
+            trigger: ":iframe .o_portal_profile_pic_clear",
+            run: function () {
+                previousProfilePictureSrc =
+                    this.anchor.parentNode.parentNode.querySelector(".o_wportal_avatar_img").src;
+                this.anchor.click();
+            },
+        },
+        {
+            content: "Click on save button",
+            trigger: ":iframe button#save_address",
+            run: "click",
+        },
+        {
+            content: "Check if profile picture is updated successfully.",
+            trigger: ":iframe img[alt=Contact]",
+            run: function () {
+                compareProfilePictures(this.anchor.src);
+            },
+        },
+    ]
+);
