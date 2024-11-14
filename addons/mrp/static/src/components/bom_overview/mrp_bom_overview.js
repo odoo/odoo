8025293f7cf05327cbbd50bc5a3eae79c8e08fd5
William Henrotin <whe@odoo.com>
2024-11-14 10:08:18 +0100

    [IMP] mrp: extend existing context of getBomData

--- addons/mrp/static/src/components/bom_overview/mrp_bom_overview.js
+++ addons/mrp/static/src/components/bom_overview/mrp_bom_overview.js
@@ -16,6 +16,7 @@ export class BomOverviewComponent extends Component {
     static props = { ...standardActionServiceProps };
     setup() {
         this.orm = useService("orm");
+        this.context = this.props.action.context;
         this.actionService = useService("action");
 
         this.variants = [];
@@ -79,7 +80,10 @@ export class BomOverviewComponent extends Component {
             this.state.bomQuantity,
             this.state.currentVariantId,
         ];
-        const context = this.state.currentWarehouse ? { warehouse: this.state.currentWarehouse.id } : {};
+        const context = { ...this.context};
+        if (this.state.currentWarehouse) {
+            context.warehouse = this.state.currentWarehouse.id;
+        }
         const bomData = await this.orm.call(
             "report.mrp.report_bom_structure",
             "get_html",
