8c9a1c2cad30857d9d48c99a4c13181ea2c96821
Adrien Minne (adrm) <adrm@odoo.com>
2024-11-14 12:08:20 +0100

    [IMP] spreadsheet: add odoo scatter chart
    Add scatter charts for odoo data.
    
    Task: 4266486
    Part-of: odoo/odoo#185487
    Related: odoo/enterprise#72892
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>

---
+++ addons/spreadsheet/static/src/chart/odoo_chart/odoo_scatter_chart.js
@@ -0,0 +1,91 @@
+import { registries, chartHelpers } from "@odoo/o-spreadsheet";
+import { _t } from "@web/core/l10n/translation";
+import { OdooChart } from "./odoo_chart";
+
+const { chartRegistry } = registries;
+
+const {
+    getScatterChartDatasets,
+    CHART_COMMON_OPTIONS,
+    getChartLayout,
+    getScatterChartScales,
+    getLineChartTooltip,
+    getChartTitle,
+    getScatterChartLegend,
+    getChartShowValues,
+    getTrendDatasetForLineChart,
+    truncateLabel,
+} = chartHelpers;
+
+export class OdooScatterChart extends OdooChart {
+    constructor(definition, sheetId, getters) {
+        super(definition, sheetId, getters);
+        this.verticalAxisPosition = definition.verticalAxisPosition;
+        this.axesDesign = definition.axesDesign;
+    }
+
+    getDefinition() {
+        return {
+            ...super.getDefinition(),
+            verticalAxisPosition: this.verticalAxisPosition,
+            axesDesign: this.axesDesign,
+        };
+    }
+}
+
+chartRegistry.add("odoo_scatter", {
+    match: (type) => type === "odoo_scatter",
+    createChart: (definition, sheetId, getters) =>
+        new OdooScatterChart(definition, sheetId, getters),
+    getChartRuntime: createOdooChartRuntime,
+    validateChartDefinition: (validator, definition) =>
+        OdooScatterChart.validateChartDefinition(validator, definition),
+    transformDefinition: (definition) => OdooScatterChart.transformDefinition(definition),
+    getChartDefinitionFromContextCreation: () =>
+        OdooScatterChart.getDefinitionFromContextCreation(),
+    name: _t("Scatter"),
+});
+
+function createOdooChartRuntime(chart, getters) {
+    const background = chart.background || "#FFFFFF";
+    const { datasets, labels } = chart.dataSource.getData();
+
+    const definition = chart.getDefinition();
+    const locale = getters.getLocale();
+
+    const trendDataSetsValues = datasets.map((dataset, index) => {
+        const trend = definition.dataSets[index]?.trend;
+        return !trend?.display
+            ? undefined
+            : getTrendDatasetForLineChart(trend, dataset.data, labels, "category", locale);
+    });
+
+    const chartData = {
+        labels,
+        dataSetsValues: datasets.map((ds) => ({ data: ds.data, label: ds.label })),
+        locale,
+        trendDataSetsValues,
+    };
+
+    const config = {
+        type: "line",
+        data: {
+            labels: chartData.labels.map(truncateLabel),
+            datasets: getScatterChartDatasets(definition, chartData),
+        },
+        options: {
+            ...CHART_COMMON_OPTIONS,
+            layout: getChartLayout(definition),
+            scales: getScatterChartScales(definition, chartData),
+            plugins: {
+                title: getChartTitle(definition),
+                legend: getScatterChartLegend(definition, chartData),
+                tooltip: getLineChartTooltip(definition, chartData),
+                chartShowValuesPlugin: getChartShowValues(definition, chartData),
+            },
+            ...getters.getChartDatasetActionCallbacks(chart),
+        },
+    };
+
+    return { background, chartJsConfig: config };
+}
