3d015e68ae088961d30cd216f769dc50072e37a5
Adnan Saiyed <adsa@odoo.com>
2024-11-14 10:30:39 +0000

    [FIX] web_editor: remove video tab from media dialog in report
    Behavior before commit:
    
    In web_studio video command is not allowed in report. However,
    user can still upload video from media dialog using video tab.
    
    Behavior after commit:
    
    This commit aims to remove videos tab from media dialog.
    
    task-4285231
    
    closes odoo/odoo#191888
    
    X-original-commit: 81d412fb9981c649eaa2efbc1229b9ab1110a532
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -1687,6 +1687,7 @@ export class Wysiwyg extends Component {
             close: () => restoreSelection(),
             ...this.options.mediaModalParams,
             ...params,
+            noVideos: !this.options.allowCommandVideo,
         });
     }
     // todo: test me
