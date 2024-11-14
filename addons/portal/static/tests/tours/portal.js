7e08c2fbdd9a1b965413ea8a3fd465c8527ef5ce
panv-odoo <panv@odoo.com>
2024-11-14 16:31:36 +0530

    [IMP] portal: allow portal users to update their profile picture
    Specification:
    
    Previously, only backend users could modify a contact/portal user's
    profile photo via the Contacts App.
    
    With this improvement, portal users now have the option to update their
    own profile picture directly from the portal. A test tour has also been
    added to validate this functionality.
    
    task-4049054
    
    Co-authored-by: Mohammed Shekha <msh@odoo.com>

--- addons/portal/static/tests/tours/portal.js
+++ addons/portal/static/tests/tours/portal.js
@@ -29,3 +29,86 @@ registry.category("web_tour.tours").add('portal_load_homepage', {
         }
     ]
 });
+
+let profilePictureSrc = "";
+
+registry.category("web_tour.tours").add("portal_profile_update", {
+    url: "/my/account",
+    steps: () => [
+        {
+            content: "Ensure that Profile Picture for Portal user is present",
+            trigger: ".o_portal_details .o_wportal_avatar_img",
+        },
+        {
+            content: "Ensure that Edit Button is present and Update the Profile Picture",
+            trigger: ".o_portal_details .o_portal_profile_pic_edit",
+            run: async function () {
+                profilePictureSrc = document.querySelector(
+                    ".o_portal_details .o_wportal_avatar_img"
+                ).src;
+
+                const fileInputEl = document.querySelector(".o_file_upload");
+                // fetching image for upload
+                const response = await fetch("/portal/static/src/img/portal-addresses.svg");
+                const blob = await response.blob();
+                // simulating or mimicing file upload
+                const simulatedImageFile = new File([blob], "portal-addresses.svg", {
+                    type: blob.type,
+                });
+                const dataTransfer = new DataTransfer();
+
+                dataTransfer.items.add(simulatedImageFile);
+                fileInputEl.files = dataTransfer.files;
+
+                fileInputEl.dispatchEvent(new Event("change", { bubbles: true, cancelable: true }));
+            },
+        },
+        {
+            content: "Comparing previous Profile Picture with new One",
+            trigger: ".o_wportal_avatar_img",
+            run: function () {
+                if (this.anchor.src === profilePictureSrc) {
+                    throw new Error("Profile Picture is not updated!");
+                }
+            },
+        },
+        {
+            content: "Click on save button",
+            trigger: ".o_portal_details button[type=submit]",
+            run: "click",
+        },
+        {
+            content: "Click on edit information",
+            trigger: "a:contains('Edit information')",
+            run: "click",
+        },
+        {
+            content: "Click on delete button",
+            trigger: ".o_portal_details .o_portal_profile_pic_clear",
+            run: function () {
+                profilePictureSrc = document.querySelector(
+                    ".o_portal_details .o_wportal_avatar_img"
+                ).src;
+                this.anchor.click();
+            },
+        },
+        {
+            content: "Check whether delete icon is removed or not",
+            trigger: ".o_portal_details a:not(.o_portal_profile_pic_clear)",
+        },
+        {
+            content: "Click on save button",
+            trigger: ".o_portal_details button[type=submit]",
+            run: "click",
+        },
+        {
+            content: "Comparing previous Profile Picture with new One",
+            trigger: ".o_portal_contact_img",
+            run: function () {
+                if (this.anchor.src == profilePictureSrc) {
+                    throw new Error("Profile Picture is not removed after deletion!");
+                }
+            },
+        },
+    ],
+});
