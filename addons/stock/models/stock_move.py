45551508235cea38691792f2b8ab0840266a9091
Pieter Claeys (clpi) <clpi@odoo.com>
2024-11-15 17:01:02 +0100

    [FIX] select right qty when manually adding lines with hidden smls
    Part-of: odoo/odoo#190467
    Related: odoo/enterprise#75532
    Related: odoo/upgrade#6926
    Signed-off-by: Quentin Wolfs (quwo) <quwo@odoo.com>

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -2462,3 +2462,7 @@ Please change the quantity done or the rounding precision of your unit of measur
             ),
             'readOnly': False,
         }
+
+    def _visible_quantity(self):
+        self.ensure_one()
+        return self.quantity
