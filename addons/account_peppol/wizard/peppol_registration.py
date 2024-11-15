99583f9e287e7c160e7e8706bb371b4c89f802d3
Claire (clbr) <clbr@odoo.com>
2024-11-15 09:24:21 +0000

    [IMP] account,*: Peppol First
    *: account_edi_ubl_cii, account_peppol
    
    Revamping around Peppol to simplify onboarding.
    
    1. In previous commit [1], we added a footer in email when relevant with some Peppol
    related information.
    2. Simplify the res.config.setting view.
    3. Auto-install the Peppol module for following countries:
    AT, BE, CH, CY, CZ, DE, DK, EE, ES, FI, FR, GR, IE, IS, IT, LT,
    LU, LV, MT, NL, NO, PL, PT, RO, SE, SI.
    4. Peppol Registration wizard: **Remove SMS validation** => we are assessing some
    KYC, but the SMS validation is not preventing much, so its only adding frictions.
    Remove the migrations key from the wizard, if the user has registered a key through his
    previous Access Point, we retrieve it and automatically migrates the participant.
    We now propose to register as receiver by default. Small UI improvements.
    5. Print & Send wizard: refine warnings, disable the "by Peppol" button
    if the customer is not on Peppol. Add some modals with explanations about Peppol
    if the customer is on Peppol but the Sender didn't check the option.
    6. Partner form: Hide in debug some too technical field. Small UI improvements.
    7. EAS cleanup: revamp EAS description to be shorter and clearer. Better handling of translations.
    8. Technical cleanup of the demo utils
    
    [1] https://github.com/odoo/odoo/commit/4e984c0ae234f02bd07d96fc555d5b5e9ad144c3
    
    task-4332306
    
    X-original-commit: 171b4ae657bc86bba1b113f0aed7a4ba4a154ed9

--- addons/account_peppol/wizard/peppol_registration.py
+++ addons/account_peppol/wizard/peppol_registration.py
@@ -21,33 +21,30 @@ class PeppolRegistration(models.TransientModel):
         required=True,
         default=lambda self: self.env.company,
     )
-    contact_email = fields.Char(
-        related='company_id.account_peppol_contact_email', readonly=False,
-        required=True,
-    )
     edi_mode = fields.Selection(
         string='EDI mode',
         selection=[('demo', 'Demo'), ('test', 'Test'), ('prod', 'Live')],
         compute='_compute_edi_mode',
-        inverse='_inverse_edi_mode',
-        readonly=False,
     )
     edi_user_id = fields.Many2one(
         comodel_name='account_edi_proxy_client.user',
         string='EDI user',
         compute='_compute_edi_user_id',
     )
-    account_peppol_migration_key = fields.Char(related='company_id.account_peppol_migration_key', readonly=False)
-    phone_number = fields.Char(related='company_id.account_peppol_phone_number', readonly=False)
-    account_peppol_proxy_state = fields.Selection(related='company_id.account_peppol_proxy_state', readonly=False)
-    verification_code = fields.Char(related='edi_user_id.peppol_verification_code', readonly=False)
-    peppol_eas = fields.Selection(related='company_id.peppol_eas', readonly=False, required=True)
-    peppol_endpoint = fields.Char(related='company_id.peppol_endpoint', readonly=False, required=True)
+    account_peppol_proxy_state = fields.Selection(related='company_id.account_peppol_proxy_state')
     peppol_warnings = fields.Json(
         string="Peppol warnings",
         compute="_compute_peppol_warnings",
     )
-    smp_registration = fields.Boolean(string='Register as a receiver')
+    contact_email = fields.Char(
+        related='company_id.account_peppol_contact_email',
+        readonly=False,
+        required=True,
+    )
+    phone_number = fields.Char(related='company_id.account_peppol_phone_number', readonly=False)
+    peppol_eas = fields.Selection(related='company_id.peppol_eas', readonly=False, required=True)
+    peppol_endpoint = fields.Char(related='company_id.peppol_endpoint', readonly=False, required=True)
+    smp_registration = fields.Boolean(string='Register as a receiver', default=True)
 
     # -------------------------------------------------------------------------
     # ONCHANGE METHODS
@@ -103,28 +100,22 @@ class PeppolRegistration(models.TransientModel):
                 }
             wizard.peppol_warnings = peppol_warnings or False
 
-    @api.depends('edi_user_id')
+    @api.depends('company_id', 'edi_user_id')
     def _compute_edi_mode(self):
-        edi_mode = self.env['ir.config_parameter'].sudo().get_param('account_peppol.edi.mode')
         for wizard in self:
-            if wizard.edi_user_id:
-                wizard.edi_mode = wizard.edi_user_id.edi_mode
-            else:
-                wizard.edi_mode = edi_mode or 'prod'
-
-    def _inverse_edi_mode(self):
-        for wizard in self:
-            if not wizard.edi_user_id and wizard.edi_mode:
-                self.env['ir.config_parameter'].sudo().set_param('account_peppol.edi.mode', wizard.edi_mode)
-                return
+            wizard.edi_mode = wizard.company_id._get_peppol_edi_mode()
 
     # -------------------------------------------------------------------------
     # BUSINESS ACTIONS
     # -------------------------------------------------------------------------
 
+    def _ensure_mandatory_fields(self):
+        if not self.contact_email or not self.phone_number:
+            raise ValidationError(_("Contact email and phone number are required."))
+
     def _action_open_peppol_form(self, reopen=True):
         action_dict = {
-            'name': _("Send via Peppol"),
+            'name': _("Activate Electronic Invoicing (via Peppol)"),
             'type': 'ir.actions.act_window',
             'view_mode': 'form',
             'res_model': 'peppol.registration',
@@ -135,7 +126,6 @@ class PeppolRegistration(models.TransientModel):
         if reopen:
             action_dict.update({
                 'res_id': self.id,
-                'context': {**self.env.context, 'disable_sms_verification': True},
             })
         return action_dict
 
@@ -158,35 +148,15 @@ class PeppolRegistration(models.TransientModel):
             }
         }
 
-    @handle_demo
-    def button_peppol_sender_registration(self):
-        """
-        The first step of the Peppol onboarding.
-        - Creates an EDI proxy user on the iap side, then the client side
-        - Calls /activate_participant to mark the EDI user as peppol user
-        - Allows the user to become a sender but not a receiver on the Peppol network.
-        Basically, a Sender does not exist on the peppol network. They use our
-        Access Point to send invoices to Peppol participants without having to register
-        themselves.
-        """
+    def button_register_peppol_participant(self):
         self.ensure_one()
+        self._ensure_mandatory_fields()
 
-        if self.account_peppol_proxy_state != 'not_registered':
+        if self.account_peppol_proxy_state in ('smp_registration', 'receiver', 'rejected'):
             raise UserError(
                 _('Cannot register a user with a %s application', self.account_peppol_proxy_state))
 
-        if not self.phone_number:
-            raise ValidationError(_("Please enter a phone number to verify your application."))
-        if not self.contact_email:
-            raise ValidationError(_("Please enter a primary contact email to verify your application."))
-
-        company = self.company_id
-        if self.smp_registration:
-            self.edi_user_id._check_company_on_peppol(self.company_id, f'{self.peppol_eas}:{self.peppol_endpoint}')
-
-        edi_user = self.edi_user_id.sudo()._register_proxy_user(company, 'peppol', self.edi_mode)
-        if not self.edi_user_id:
-            self.edi_user_id = edi_user
+        edi_user = self.edi_user_id or self.env['account_edi_proxy_client.user']._register_proxy_user(self.company_id, 'peppol', self.edi_mode)
 
         # if there is an error when activating the participant below,
         # the client side is rolled back and the edi user is deleted on the client side
@@ -195,140 +165,24 @@ class PeppolRegistration(models.TransientModel):
         if not modules.module.current_test:
             self.env.cr.commit()
 
-        params = {
-            'company_details': {
-                'peppol_company_name': company.display_name,
-                'peppol_company_vat': company.vat,
-                'peppol_company_street': company.street,
-                'peppol_company_city': company.city,
-                'peppol_company_zip': company.zip,
-                'peppol_country_code': company.country_id.code,
-                'peppol_phone_number': self.phone_number,
-                'peppol_contact_email': self.contact_email,
-            },
-        }
-
-        edi_user._call_peppol_proxy(
-            endpoint='/api/peppol/1/activate_participant',
-            params=params,
-        )
-
-        if edi_user.edi_mode != 'demo':
-            return self.button_send_peppol_verification_code()
-        return self._action_open_peppol_form()
-
-    @handle_demo
-    def button_peppol_smp_registration(self):
-        """
-        The second (optional) step in Peppol registration.
-        The user can choose to become a Receiver and officially register on the Peppol
-        network, i.e. receive documents from other Peppol participants.
-        """
-        self.ensure_one()
-        try:
-            self.edi_user_id._peppol_register_sender_as_receiver()
-        except (UserError, AccountEdiProxyError) as e:
-            self.button_deregister_peppol_participant()
-            registration_form_action = self._action_open_peppol_form()
-            registration_form_action['views'] = [(False, 'form')]
-            return {
-                'type': 'ir.actions.client',
-                'tag': 'display_notification',
-                'params': {
-                    'type': 'danger',
-                    'message': e,
-                    'next': registration_form_action,
-                }
-            }
-
-        if self.company_id.account_peppol_proxy_state == 'smp_registration':
-            return self._action_send_notification(
-                title=_("Registered to receive documents via Peppol."),
-                message=_("Your registration on Peppol network should be activated within a day. The updated status will be visible in Settings."),
-            )
-        return self._action_open_peppol_form()
-
-    @handle_demo
-    def button_update_peppol_user_data(self):
-        """
-        Action for the user to be able to update their contact details any time
-        Calls /update_user on the iap server
-        """
-        self.ensure_one()
-
-        if not self.contact_email or not self.phone_number:
-            raise ValidationError(_("Contact email and phone number are required."))
-
-        edi_identification = f'{self.peppol_eas}:{self.peppol_endpoint}'
-        if self.smp_registration:
-            self.edi_user_id._check_company_on_peppol(self.company_id, edi_identification)
-
-        params = {
-            'update_data': {
-                'edi_identification': edi_identification,
-                'peppol_phone_number': self.phone_number,
-                'peppol_contact_email': self.contact_email,
-            }
-        }
-
-        self.edi_user_id._call_peppol_proxy(
-            endpoint='/api/peppol/1/update_user',
-            params=params,
-        )
-        return True
-
-    def button_send_peppol_verification_code(self):
-        """
-        Request user verification via SMS
-        Calls the /send_verification_code to send the 6-digit verification code
-        """
-        self.ensure_one()
-
-        self.button_update_peppol_user_data()
-        self.edi_user_id._call_peppol_proxy(
-            endpoint='/api/peppol/1/send_verification_code',
-            params={'message': _("Your confirmation code is")},
-        )
-        self.account_peppol_proxy_state = 'in_verification'
-        return self._action_open_peppol_form()
-
-    @handle_demo
-    def button_check_peppol_verification_code(self):
-        """
-        Calls /verify_phone_number to compare user's input and the
-        code generated on the IAP server
-        """
-        self.ensure_one()
-        if self.account_peppol_proxy_state != 'in_verification':
-            raise ValidationError(_("Please first verify your phone number by clicking on 'Send a registration code by SMS'."))
-
-        if not self.verification_code or len(self.verification_code) != 6:
-            raise ValidationError(_("The verification code should contain six digits."))
-
-        # update contact details in case the user made changes
-        self.button_update_peppol_user_data()
-
-        self.edi_user_id._call_peppol_proxy(
-            endpoint='/api/peppol/2/verify_phone_number',
-            params={'verification_code': self.verification_code},
-        )
-        self.account_peppol_proxy_state = 'sender'
-        self.verification_code = False
+        edi_user._peppol_register_sender()
 
         if self.smp_registration:
-            return self.button_peppol_smp_registration()
+            try:
+                edi_user._peppol_register_sender_as_receiver()
+                edi_user._peppol_get_participant_status()
+            except (UserError, AccountEdiProxyError):
+                edi_user._peppol_deregister_participant()
+                raise
+
+            if self.company_id.account_peppol_proxy_state == 'smp_registration':
+                return self._action_send_notification(
+                    title=_("Registered to receive documents via Peppol."),
+                    message=_(
+                        "Your registration on Peppol network should be activated within a day. The updated status will be visible in Settings."),
+                )
+
         return self._action_send_notification(
             title=_("Registered as a sender."),
             message=_("You can now send invoices via Peppol."),
         )
-
-    @handle_demo
-    def button_deregister_peppol_participant(self):
-        """
-        Deregister the edi user from Peppol network
-        """
-        self.ensure_one()
-
-        if self.edi_user_id:
-            self.edi_user_id._peppol_deregister_participant()
-        return True
