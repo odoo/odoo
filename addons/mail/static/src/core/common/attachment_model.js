19a2235a3c1e2bd18f8c91f8d8f452585873b1c9
Didier (did) <did@odoo.com>
2024-11-18 08:23:09 +0000

    [FIX] mail: don't add query strings on non-binary attachment
    Before this PR, query strings were added to the attachment of URL types.
    This is incorrect since those URLs can already have query strings, and the added
    query strings don't make sense for non-binary attachments.
    
    X-original-commit: 85480d53e4c5e512e466b4cabe367d97a4af315c

--- addons/mail/static/src/core/common/attachment_model.js
+++ addons/mail/static/src/core/common/attachment_model.js
@@ -32,7 +32,7 @@ export class Attachment extends FileModelMixin(Record) {
     message = Record.one("Message", { inverse: "attachment_ids" });
     /** @type {luxon.DateTime} */
     create_date = Record.attr(undefined, { type: "datetime" });
-    /** @type {string} */
+    /** @type {'binary'|'url'} */
     type;
     /** @type {string} */
     url;
