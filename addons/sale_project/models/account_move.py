dd663d27d10cb74a6d1759d1ce1bc2a8f2504501
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2024-11-18 15:12:27 +0000

    [FIX] sale_project: analytic assigned to payment term line
    When confirming a BILL via Project's Updates, the generated payment
    terms line will have the analytic account of the project assigned
    Version: 17.0+
    
    Steps to reproduce:
    - Have a project (i.e. Renovations)
    - Create a Purchase Order
    - Add on the line the analytic account of the project
    - Go to Projects Kanban view
    - Open Project 3 dots menu > Reporting > Project Updates
    - Open the created PO, Create Bill
    Issue: Analytic account will be assigned to the payment term line
    
    opw-4193509
    
    X-original-commit: 8ab60b9fce8f55851619b850c3723609d0981dc7

--- addons/sale_project/models/account_move.py
+++ addons/sale_project/models/account_move.py
@@ -6,6 +6,30 @@ from odoo import models
 class AccountMove(models.Model):
     _inherit = "account.move"
 
+<<<<<<< 18.0
     def _get_action_per_item(self):
         action = self.env.ref('account.action_move_out_invoice_type').id
         return {invoice.id: action for invoice in self}
+||||||| 9e3ad1086a2577d13f307cbfbb33020c4081f37d
+    def _compute_analytic_distribution(self):
+        # when a project creates an aml, it adds an analytic account to it. the following filter is to save this
+        # analytic account from being overridden by analytic default rules and lack thereof
+        project_amls = self.filtered(lambda aml: aml.analytic_distribution and any(aml.sale_line_ids.project_id))
+        super(AccountMoveLine, self - project_amls)._compute_analytic_distribution()
+        project_id = self.env.context.get('project_id')
+        if project_id:
+            analytic_account = self.env['project.project'].browse(project_id).analytic_account_id
+            for line in self:
+                line.analytic_distribution = line.analytic_distribution or {analytic_account.id: 100}
+=======
+    def _compute_analytic_distribution(self):
+        # when a project creates an aml, it adds an analytic account to it. the following filter is to save this
+        # analytic account from being overridden by analytic default rules and lack thereof
+        project_amls = self.filtered(lambda aml: aml.analytic_distribution and any(aml.sale_line_ids.project_id))
+        super(AccountMoveLine, self - project_amls)._compute_analytic_distribution()
+        project_id = self.env.context.get('project_id')
+        if project_id:
+            analytic_account = self.env['project.project'].browse(project_id).analytic_account_id
+            for line in project_amls:
+                line.analytic_distribution = line.analytic_distribution or {analytic_account.id: 100}
+>>>>>>> eab95d42e845edc4ea2de8a0c7b7bc9766601f39
