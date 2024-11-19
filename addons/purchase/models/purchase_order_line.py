1ed9116117dff60bc15060ad5d78e5710ee12195
Anton Romanova (roan) <roan@odoo.com>
2024-11-19 17:08:22 +0100

    [IMP] account,sale,purchase,l10n_it_edi: refactor tax details management for rounding
    Before `_add_tax_details_in_base_line` populated tax details like `tax_amount_currency` directly, and `_round_base_lines_tax_details` subsequently moved these values to new keys prefixed with `raw_` while rounding the originals. The problem with that is the meaning of certain dictionary keys like `tax_amount_currency` changed depending on the method that processed them
    
    Now, `_add_tax_details_in_base_line` only adds raw values (e.g., `raw_tax_amount_currency`), and `_round_base_lines_tax_details` separately adds the rounded values
    
    This simplifies state management and makes sure that raw and rounded values are clearly separated.
    
    task: 4277573

--- addons/purchase/models/purchase_order_line.py
+++ addons/purchase/models/purchase_order_line.py
@@ -94,8 +94,8 @@ class PurchaseOrderLine(models.Model):
         for line in self:
             base_line = line._prepare_base_line_for_taxes_computation()
             self.env['account.tax']._add_tax_details_in_base_line(base_line, line.company_id)
-            line.price_subtotal = base_line['tax_details']['total_excluded_currency']
-            line.price_total = base_line['tax_details']['total_included_currency']
+            line.price_subtotal = base_line['tax_details']['raw_total_excluded_currency']
+            line.price_total = base_line['tax_details']['raw_total_included_currency']
             line.price_tax = line.price_total - line.price_subtotal
 
     def _prepare_base_line_for_taxes_computation(self):
