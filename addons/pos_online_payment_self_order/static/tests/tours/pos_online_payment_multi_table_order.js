2dce0f22e92299b17162343e3629dcdab8a697b5
aamo-odoo <aamo@odoo.com>
2024-11-19 17:35:35 +0530

    [FIX] pos_online_payment: prevent error when making another payment
    This error occurs when a product is added to one table, and the payment screen
    is accessed where an online payment method is selected but the payment is not
    completed. Subsequently, switching to another table and attempting to make an
    online payment results in the issue.
    
    Steps to reproduce:
    - Install 'pos_online_payment' and 'pos_restaurant' module
    - Create a new online payment in 'Payment Method(eg: Test)'
    - In Configuration/Settings, add it for a restaurant in 'Payment Method'
    - Now open a session in a restaurant > Select one table > Add products > Order
      > Payment > Click on Test
    - Click on '+' > Add product > Payment
    - Click on Test and Validate
    
    Traceback:
    Expected singleton: pos.order('p', 'o', 's', '.', 'o', 'r', 'd', 'e', 'r', '_', '2')
    
    Previous Behaviour:
    When an online payment is selected for the first table but exited without
    completing the transaction, an RPC error occurs, causing the system to switch
    to offline mode. Consequently, the order encounters an error when attempting
    to place an order and process an online payment at the second table. At that
    time, it creates a new order as ID 'pos.order_2' and updates the previous order.
    Due to what we get ID like 'pos.order_2',  leading to the issue.
    
    After Commit:
    When creating a new order for another table at [1], the
    'online_account_payment_id' is set to false. If this value is false, we will
    assign an empty set to 'online_account_payments_by_pm[pm_id]'.
    
    [1]- https://github.com/odoo/odoo/blob/74d4503a45c9d4ca741d349c3fce673358d2cc42/addons/pos_online_payment/models/pos_payment.py#L23
    
    sentry-5703437687

---
+++ addons/pos_online_payment_self_order/static/tests/tours/pos_online_payment_multi_table_order.js
@@ -0,0 +1,32 @@
+import * as ChromePos from "@point_of_sale/../tests/tours/utils/chrome_util";
+import * as ChromeRestaurant from "@pos_restaurant/../tests/tours/utils/chrome";
+import * as Dialog from "@point_of_sale/../tests/tours/utils/dialog_util";
+import * as FloorScreen from "@pos_restaurant/../tests/tours/utils/floor_screen_util";
+import * as PaymentScreen from "@point_of_sale/../tests/tours/utils/payment_screen_util";
+import * as ProductScreenPos from "@point_of_sale/../tests/tours/utils/product_screen_util";
+import * as ProductScreenResto from "@pos_restaurant/../tests/tours/utils/product_screen_util";
+import { registry } from "@web/core/registry";
+
+const Chrome = { ...ChromePos, ...ChromeRestaurant };
+const ProductScreen = { ...ProductScreenPos, ...ProductScreenResto };
+
+registry.category("web_tour.tours").add("OnlinePaymentWithMultiTables", {
+    steps: () =>
+        [
+            Chrome.startPoS(),
+            Dialog.confirm("Open Register"),
+            FloorScreen.clickTable("1"),
+            ProductScreen.orderBtnIsPresent(),
+            ProductScreen.clickDisplayedProduct("Coca-Cola"),
+            ProductScreen.clickOrderButton(),
+            ProductScreen.orderlinesHaveNoChange(),
+            ProductScreen.clickPayButton(),
+            PaymentScreen.totalIs("2.53"),
+            Chrome.createFloatingOrder(),
+            ProductScreen.clickDisplayedProduct("Coca-Cola"),
+            ProductScreen.clickPayButton(),
+            PaymentScreen.totalIs("2.53"),
+            PaymentScreen.validateButtonIsHighlighted(true),
+            PaymentScreen.clickValidate(),
+        ].flat(),
+});
