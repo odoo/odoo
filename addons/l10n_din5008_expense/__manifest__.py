873cbe37ddeaa105d6b76ad6b0a5e2eb4d3afaab
David Van Droogenbroeck (DROD) <drod@odoo.com>
2024-11-19 16:36:19 +0100

    [ADD] l10n_din5008_expense: add bridge module for din5008 expense reports
    Issue:
    Expense title is duplicated when printing an expense report for localizations using the DIN5008 standard.
    
    Steps to reproduce:
    - Install German localization
    - Create a new expense report
    - Print the expense report PDF
    -> Title is duplicated
    
    Cause:
    DIN5008 reports tries to load a specific field `l10n_din5008_document_title` in their header
    and fallbacks to report's name
    
    With this commit, we add a bridge module to add the field
    `l10n_din5008_document_title` to `hr.expense.sheet` and set it to
    'Expenses Report`, therefore the header will have 'Expenses Report' as
    title, like in the standard expense report.
    
    opw-4314414

---
+++ addons/l10n_din5008_expense/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'DIN 5008 - Expenses',
+    'category': 'Accounting/Localizations',
+    'depends': [
+        'l10n_din5008',
+        'hr_expense',
+    ],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
