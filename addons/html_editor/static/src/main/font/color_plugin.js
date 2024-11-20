0b0e2cca1df8636683c91414fce655cfb8aafadc
Adnan Saiyed <adsa@odoo.com>
2024-11-20 06:17:06 +0000

    [FIX] html_editor: used custom colors appearing twice
    When a text having a text-color is selected, opening
    custom color picker shows that color twice at the top.
    It happens because that specific color of selected text
    is stored in this.usedCustomColors when opening color
    selector. When color selector component is renedered,
    it shows usedCustomColors button with currentCustomColor
    and button appears twice.
    
    task-4341528
    
    closes odoo/odoo#192069
    
    X-original-commit: d36f570d3fdcb1d64862743ee76decabfe612c7d
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/html_editor/static/src/main/font/color_plugin.js
+++ addons/html_editor/static/src/main/font/color_plugin.js
@@ -326,7 +326,7 @@ export class ColorPlugin extends Plugin {
         const usedCustomColors = new Set();
         for (const font of allFont) {
             if (isCSSColor(font.style[mode])) {
-                usedCustomColors.add(font.style[mode]);
+                usedCustomColors.add(rgbToHex(font.style[mode]));
             }
         }
         return usedCustomColors;
