odoo/odoo

commit 7b424cdec4a841f326d1e4935b2aab5144494ad8

[IMP] website_sale, *: move delivery selection to /checkout page
A single step for selecting a delivery method and a payment provider
poses two problems:

1) The interface looks cluttered when multiple pms and delivery methods
are configured.
2) Complexity in a payment and a delivery widget dependencies. As they are
rendered on the same page it requires to dynamically update parts of its
content in js causing bugs and inconsistencies.
For those reasons we decided to move the delivery selection step to
the /checkout.

task-3133415

closes odoo/odoo#153063

Related: odoo/upgrade#6035
Related: odoo/enterprise#56049
Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>

addons/website_sale_loyalty/controllers/delivery.py
@@ -1,46 +1,19 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo.http import request, route
+from odoo.http import request
 
-from odoo.addons.payment import utils as payment_utils
-from odoo.addons.website_sale.controllers.delivery import WebsiteSaleDelivery
+from odoo.addons.website_sale.controllers.delivery import Delivery
 
 
-class WebsiteSaleLoyaltyDelivery(WebsiteSaleDelivery):
+class WebsiteSaleLoyaltyDelivery(Delivery):
 
-    def _update_website_sale_delivery_return(self, order, **post):
-        if order:
-            order._update_programs_and_rewards()
-        result = super()._update_website_sale_delivery_return(order, **post)
-        if order:
-            free_shipping_lines = order._get_free_shipping_lines()
-            if free_shipping_lines:
-                Monetary = request.env['ir.qweb.field.monetary']
-                currency = order.currency_id
-                amount_free_shipping = sum(free_shipping_lines.mapped('price_subtotal'))
-                result.update({
-                    'new_amount_delivery': Monetary.value_to_html(0.0, {'display_currency': currency}),
-                    'new_amount_order_discounted': Monetary.value_to_html(order.reward_amount - amount_free_shipping, {'display_currency': currency}),
-                    'delivery_discount_minor_amount': payment_utils.to_minor_currency_units(
-                        amount_free_shipping, currency
-                    ),
-                })
-        return result
-
-    @route()
-    def cart_carrier_rate_shipment(self, carrier_id, **kw):
+    def _order_summary_values(self, order, **post):
         Monetary = request.env['ir.qweb.field.monetary']
-        order = request.website.sale_get_order(force_create=True)
+        res = super()._order_summary_values(order, **post)
         free_shipping_lines = order._get_free_shipping_lines()
-        # Avoid computing carrier price delivery is free (coupon). It means if
-        # the carrier has error (eg 'delivery only for Belgium') it will show
-        # Free until the user clicks on it.
         if free_shipping_lines:
-            return {
-                'carrier_id': carrier_id,
-                'status': True,
-                'is_free_delivery': True,
-                'new_amount_delivery': Monetary.value_to_html(0.0, {'display_currency': order.currency_id}),
-                'error_message': None,
-            }
-        return super().cart_carrier_rate_shipment(carrier_id, **kw)
+            res['amount_delivery_discounted'] = Monetary.value_to_html(
+                sum(free_shipping_lines.mapped('price_subtotal')),
+                {'display_currency': order.currency_id}
+            )
+        return res

addons/delivery/models/sale_order.py
@@ -9,7 +9,6 @@ class SaleOrder(models.Model):
 
     carrier_id = fields.Many2one('delivery.carrier', string="Delivery Method", check_company=True, help="Fill this field if you plan to invoice the shipping based on picking.")
     delivery_message = fields.Char(readonly=True, copy=False)
-    delivery_rating_success = fields.Boolean(copy=False)
     delivery_set = fields.Boolean(compute='_compute_delivery_state')
     recompute_delivery_price = fields.Boolean('Delivery cost should be recomputed')
     is_all_service = fields.Boolean("Service Product", compute="_compute_is_service_products")

addons/delivery/views/delivery_carrier_views.xml
@@ -171,7 +171,7 @@
     </record>
 
     <record id="action_delivery_carrier_form" model="ir.actions.act_window">
-        <field name="name">Shipping Methods</field>
+        <field name="name">Delivery Methods</field>
         <field name="res_model">delivery.carrier</field>
         <field name="view_mode">tree,form</field>
         <field name="context">{'search_default_group_by_provider': True}</field>

addons/delivery/wizard/choose_delivery_carrier.py
@@ -33,7 +33,7 @@ class ChooseDeliveryCarrier(models.TransientModel):
     def _onchange_carrier_id(self):
         self.delivery_message = False
         if self.delivery_type in ('fixed', 'base_on_rule'):
-            vals = self._get_shipment_rate()
+            vals = self._get_delivery_rate()
             if vals.get('error_message'):
                 return {'error': vals['error_message']}
         else:
@@ -44,7 +44,7 @@ class ChooseDeliveryCarrier(models.TransientModel):
     def _onchange_order_id(self):
         # fixed and base_on_rule delivery price will computed on each carrier change so no need to recompute here
         if self.carrier_id and self.order_id.delivery_set and self.delivery_type not in ('fixed', 'base_on_rule'):
-            vals = self._get_shipment_rate()
+            vals = self._get_delivery_rate()
             if vals.get('error_message'):
                 warning = {
                     'title': _("%(carrier)s Error", carrier=self.carrier_id.name),
@@ -64,7 +64,7 @@ class ChooseDeliveryCarrier(models.TransientModel):
             carriers = self.env['delivery.carrier'].search(self.env['delivery.carrier']._check_company_domain(rec.order_id.company_id))
             rec.available_carrier_ids = carriers.available_carriers(rec.order_id.partner_shipping_id) if rec.partner_id else carriers
 
-    def _get_shipment_rate(self):
+    def _get_delivery_rate(self):
         vals = self.carrier_id.with_context(order_weight=self.total_weight).rate_shipment(self.order_id)
         if vals.get('success'):
             self.delivery_message = vals.get('warning_message', False)
@@ -74,7 +74,7 @@ class ChooseDeliveryCarrier(models.TransientModel):
         return {'error_message': vals['error_message']}
 
     def update_price(self):
-        vals = self._get_shipment_rate()
+        vals = self._get_delivery_rate()
         if vals.get('error_message'):
             raise UserError(vals.get('error_message'))
         return {

addons/website_payment/static/src/js/payment_form.js
@@ -1,7 +1,6 @@
 /** @odoo-module **/
 
 import { _t } from '@web/core/l10n/translation';
-import { Component } from '@odoo/owl';
 
 import PaymentForm from '@payment/js/payment_form';
 
@@ -10,15 +9,6 @@ PaymentForm.include({
         'change input[name="o_donation_amount"]': '_updateAmount',
     }),
 
-    // #=== WIDGET LIFECYCLE ===#
-
-    /**
-     * @override
-     */
-    async start() {
-        Component.env.bus.addEventListener('update_shipping_cost', (ev) => this._updateShippingCost(ev.detail));
-        return await this._super.apply(this, arguments);
-    },
 
     // #=== EVENT HANDLERS ===#
 
@@ -34,18 +24,6 @@ PaymentForm.include({
             this.paymentContext.amount = ev.target.value;
         }
     },
-
-    /**
-     * Update the total amount to be paid.
-     *
-     * Called upon change of shipping method
-     *
-     * @private
-     * @param {float} amount
-     */
-     _updateShippingCost: function (amount) {
-        this.paymentContext.amount = amount;
-     },
 
     // #=== PAYMENT FLOW ===#
 

addons/website_sale/__manifest__.py
@@ -24,6 +24,7 @@
         'views/account_move_views.xml',
         'views/crm_team_views.xml',
         'views/delivery_carrier_views.xml',
+        'views/delivery_form_templates.xml',
         'views/digest_views.xml',
         'views/product_attribute_views.xml',
         'views/product_configurator.xml',
@@ -46,7 +47,6 @@
         'views/product_product_add.xml',
         'views/website_views.xml',
         'views/website_pages_views.xml',
-        'views/website_sale_delivery_templates.xml',
         'views/website_sale_menus.xml',
         'views/variant_templates.xml',
     ],
@@ -93,7 +93,6 @@
             'website_sale/static/src/js/components/website_sale_image_viewer.js',
             'website_sale/static/src/xml/website_sale_reorder_modal.xml',
             'website_sale/static/src/js/website_sale_reorder.js',
-            'website_sale/static/src/js/website_sale_delivery.js',
             'website_sale/static/src/scss/product_configurator.scss',
             'website_sale/static/src/js/notification/add_to_cart_notification/add_to_cart_notification.js',
             'website_sale/static/src/js/notification/add_to_cart_notification/add_to_cart_notification.xml',

addons/website_sale/controllers/main.py
@@ -95,7 +95,7 @@ class TableCompute:
 
 class WebsiteSale(payment_portal.PaymentPortal):
     _express_checkout_route = '/shop/express_checkout'
-    _express_checkout_shipping_route = '/shop/express/shipping_address_change'
+    _express_checkout_delivery_route = '/shop/express/shipping_address_change'
 
     WRITABLE_PARTNER_FIELDS = [
         'name',
@@ -1178,6 +1178,25 @@ class WebsiteSale(payment_portal.PaymentPortal):
                 new_values['parent_id'] = commercial_partner.id
         return new_values, errors, error_msg
 
+    def should_skip_delivery_step(self, delivery_methods):
+        """ Check if the delivery step should be skipped based on the available delivery methods.
+
+        :param delivery.carrier delivery_methods: The available delivery methods.
+        :return: Whether the delivery step should be skipped.
+        :rtype: bool
+        """
+        if not delivery_methods or len(delivery_methods) > 1:
+            # The user must choose the carrier or see a warning if no available ones.
+            return False
+        delivery_method = delivery_methods[0]
+        if hasattr(delivery_method, delivery_method.delivery_type + '_use_locations'):
+            # Check if the user must choose a pickup point.
+            use_location = getattr(
+                delivery_method, delivery_method.delivery_type + '_use_locations'
+            )
+            return not use_location
+        return True
+
     @route(['/shop/address'], type='http', methods=['GET', 'POST'], auth="public", website=True, sitemap=False)
     def address(self, **kw):
         Partner = request.env['res.partner'].with_context(show_address=1).sudo()
@@ -1298,7 +1317,7 @@ class WebsiteSale(payment_portal.PaymentPortal):
                 # -> TDE: you are the guy that did what we should never do in commit e6f038a
                 order.message_partner_ids = [(4, partner_id), (3, request.website.partner_id.id)]
                 if not errors:
-                    return request.redirect(kw.get('callback') or '/shop/confirm_order')
+                    return request.redirect(kw.get('callback') or '/shop/checkout?express=1')
 
         is_public_user = request.website.is_public_user()
         render_values = {
@@ -1394,9 +1413,12 @@ class WebsiteSale(payment_portal.PaymentPortal):
                 order_sudo.partner_shipping_id = child_partner_id or self._create_or_edit_partner(
                     shipping_address, type='delivery', parent_id=order_sudo.partner_id.id
                 )
-            # Process the delivery carrier
+            # Process the delivery method.
             if shipping_option:
-                order_sudo._check_carrier_quotation(force_carrier_id=int(shipping_option['id']))
+                delivery_method_sudo = request.env['delivery.carrier'].sudo().browse(
+                    int(shipping_option['id'])
+                ).exists()
+                order_sudo._set_delivery_method(delivery_method_sudo)
 
         return order_sudo.partner_id.id
 
@@ -1521,10 +1543,23 @@ class WebsiteSale(payment_portal.PaymentPortal):
         if redirection:
             return redirection
 
-        if post.get('express'):
-            return request.redirect('/shop/confirm_order')
-
         values = self.checkout_values(order_sudo, **post)
+
+        should_skip_delivery = True  # Delivery is only needed for deliverable products.
+        if order_sudo._has_deliverable_products():
+            available_dms = values['delivery_methods'] = order_sudo._get_delivery_methods()
+            delivery_method = order_sudo._get_preferred_delivery_method(available_dms)
+            rate = delivery_method.rate_shipment(order_sudo)
+            if (
+                not order_sudo.carrier_id
+                or not rate.get('success')
+                or order_sudo.amount_delivery != rate['price']
+            ):
+                order_sudo._set_delivery_method(delivery_method, rate=rate)
+            should_skip_delivery = self.should_skip_delivery_step(available_dms)
+
+        if post.get('express') and should_skip_delivery:
+            return request.redirect('/shop/confirm_order')
 
         # Avoid useless rendering if called in ajax
         if post.get('xhr'):
@@ -1563,9 +1598,11 @@ class WebsiteSale(payment_portal.PaymentPortal):
             and partner_sudo != order_sudo.partner_shipping_id
         ):
             order_sudo.partner_shipping_id = partner_id
-            if order_sudo.carrier_id:
-                # update carrier rates on shipping address change
-                order_sudo._check_carrier_quotation(force_carrier_id=order_sudo.carrier_id.id)
+            if order_sudo.carrier_id and order_sudo._has_deliverable_products():
+                # Update the delivery method on shipping address change.
+                delivery_methods = order_sudo._get_delivery_methods()
+                delivery_method = order_sudo._get_preferred_delivery_method(delivery_methods)
+                order_sudo._set_delivery_method(delivery_method)
         else:
             # TODO someday we should gracefully handle invalid addresses
             return
@@ -1637,14 +1674,11 @@ class WebsiteSale(payment_portal.PaymentPortal):
             'express_checkout_route': self._express_checkout_route,
             'landing_route': '/shop/payment/validate',
             'payment_method_unknown_id': request.env.ref('payment.payment_method_unknown').id,
+            'shipping_info_required': order._has_deliverable_products(),
+            'shipping_address_update_route': self._express_checkout_delivery_route,
         })
         if request.website.is_public_user():
             payment_form_values['partner_id'] = -1
-        if request.website.enabled_delivery:
-            payment_form_values.update({
-                'shipping_info_required': not order.only_services,
-                'shipping_address_update_route': self._express_checkout_shipping_route,
-            })
         return payment_form_values
 
     def _get_shop_payment_values(self, order, **kwargs):
@@ -1664,23 +1698,7 @@ class WebsiteSale(payment_portal.PaymentPortal):
             'landing_route': '/shop/payment/validate',
             'sale_order_id': order.id,  # Allow Stripe to check if tokenization is required.
         }
-        values = {**checkout_page_values, **payment_form_values}
-        if request.website.enabled_delivery:
-            has_storable_products = any(
-                line.product_id.type in ['consu', 'product'] for line in order.order_line
-            )
-            if has_storable_products:
-                if order.carrier_id and not order.delivery_rating_success:
-                    order._remove_delivery_line()
-                    order._check_carrier_quotation()
-                values['deliveries'] = order._get_delivery_methods().sudo()
-
-            values['delivery_has_storable'] = has_storable_products
-            values['delivery_action_id'] = request.env.ref(
-                'delivery.action_delivery_carrier_form'
-            ).id
-
-        return values
+        return checkout_page_values | payment_form_values
 
     def _get_shop_payment_errors(self, order):
         """ Check that there is no error that should block the payment.
@@ -1691,11 +1709,11 @@ class WebsiteSale(payment_portal.PaymentPortal):
         """
         errors = []
 
-        if not order.only_services and not order._get_delivery_methods():
+        if order._has_deliverable_products() and not order._get_delivery_methods():
             errors.append((
-                _('Sorry, we are unable to ship your order'),
-                _('No shipping method is available for your current order and shipping address. '
-                   'Please contact us for more information.'),
+                _("Sorry, we are unable to ship your order."),
+                _("No shipping method is available for your current order and shipping address."
+                  " Please contact us for more information."),
             ))
         return errors
 
@@ -1711,20 +1729,6 @@ class WebsiteSale(payment_portal.PaymentPortal):
            paying / canceling
         """
         order = request.website.sale_get_order()
-
-        if order and not order.only_services and (request.httprequest.method == 'POST' or not order.carrier_id):
-            # Update order's carrier_id (will be the one of the partner if not defined)
-            # If a carrier_id is (re)defined, redirect to "/shop/payment" (GET method to avoid infinite loop)
-            carrier_id = post.get('carrier_id')
-            keep_carrier = post.get('keep_carrier', False)
-            if keep_carrier:
-                keep_carrier = bool(int(keep_carrier))
-            if carrier_id:
-                carrier_id = int(carrier_id)
-            order._check_carrier_quotation(force_carrier_id=carrier_id, keep_carrier=keep_carrier)
-            if carrier_id:
-                return request.redirect("/shop/payment")
-
         redirection = self.checkout_redirection(order) or self.checkout_check_address(order)
         if redirection:
             return redirection

commit 3550770874116ea43c2792281bd269e907564bcb

[FIX] website_sale_loyalty: consider free ship. disc. in EC
Consider free shipping discount in Express Checkout

Versions
--------
- 17.4+

Steps
-----
1. Create a discount code with a free shipping reward with a max amount;
3. Create one or more shipping methods with a price higher than the max
   amount previously set;
4. in eCommerce, add a product to the cart, and go to checkout;
5. apply coupon;
6. open the express checkout form using Stripe
7. switch between shipping methods.

Issue
-----
The free shipping discount is not considered in the Express Checkout
form.

Cause
-----
Commit 7b424cd accidentally removed `delivery_discount_minor_amount`
from `_order_summary_values` (previously called
`_update_website_sale_delivery_return`) introduced by commit ab9aa22,
preventing the express checkout to get the up to date value.

Solution
--------
Add `delivery_discount_minor_amount` back to `_order_summary_values`.

opw-4204901

closes odoo/odoo#187751

X-original-commit: e5055ab6bdfd701f7333274dba6a08054256675e
Signed-off-by: Valentin Chevalier <vcr@odoo.com>

addons/payment_stripe/static/src/js/express_checkout_form.js
@@ -204,7 +204,7 @@ paymentExpressCheckoutForm.include({
                     status: 'success',
                     ...this._getOrderDetails(
                         ev.shippingOption.amount,
-                        result.delivery_discount_minor_amount || 0,
+                        parseInt(result.delivery_discount_minor_amount) || 0,
                     ),
                 });
             });

addons/website_sale_loyalty/controllers/delivery.py
@@ -4,6 +4,7 @@ from functools import partial
 
 from odoo.http import request
 
+from odoo.addons.payment import utils as payment_utils
 from odoo.addons.website_sale.controllers.delivery import Delivery
 
 
@@ -17,8 +18,10 @@ class WebsiteSaleLoyaltyDelivery(Delivery):
         res = super()._order_summary_values(order, **post)
         free_shipping_lines = order._get_free_shipping_lines()
         if free_shipping_lines:
-            res['amount_delivery_discounted'] = to_html(
-                sum(free_shipping_lines.mapped('price_subtotal')),
+            shipping_discount = sum(free_shipping_lines.mapped('price_subtotal'))
+            res['amount_delivery_discounted'] = to_html(shipping_discount)
+            res['delivery_discount_minor_amount'] = payment_utils.to_minor_currency_units(
+                shipping_discount, order.currency_id
             )
         res['discount_reward_amounts'] = [
             to_html(line.price_subtotal)

addons/website_sale_loyalty/tests/test_website_sale_loyalty_delivery.py
@@ -3,9 +3,20 @@
 from odoo.fields import Command
 from odoo.tests import HttpCase, tagged
 
+from odoo.addons.website.tools import MockRequest
+from odoo.addons.website_sale.tests.common import WebsiteSaleCommon
+from odoo.addons.website_sale_loyalty.controllers.delivery import (
+    WebsiteSaleLoyaltyDelivery,
+)
+
 
 @tagged('post_install', '-at_install')
-class TestWebsiteSaleDelivery(HttpCase):
+class TestWebsiteSaleDelivery(HttpCase, WebsiteSaleCommon):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.Controller = WebsiteSaleLoyaltyDelivery()
 
     def setUp(self):
         super().setUp()
@@ -144,3 +155,35 @@ class TestWebsiteSaleDelivery(HttpCase):
             })],
         })
         self.start_tour("/", 'check_shipping_discount', login="admin")
+
+    def test_express_checkout_shipping_discount(self):
+        """
+        Check display of shipping discount promotion in express checkout form by ensuring is present
+        in the values returned to the form.
+        """
+        # Create a discount code
+        program = self.env['loyalty.program'].sudo().create({
+            'name': 'Free Shipping',
+            'program_type': 'promo_code',
+            'rule_ids': [
+                Command.create({
+                    'code': "FREE",
+                    'minimum_amount': 0,
+                })
+            ],
+            'reward_ids': [
+                Command.create({
+                    'reward_type': 'shipping',
+                    'discount_max_amount': 6.0,
+                })
+            ]
+            }
+        )
+
+        # Apply discount
+        self.cart._try_apply_code("FREE")
+        self.cart._apply_program_reward(program.reward_ids, program.coupon_ids)
+
+        with MockRequest(self.env, sale_order_id=self.cart.id, website=self.website):
+            result = self.Controller.shop_set_delivery_method(self.normal_delivery2.id)
+        self.assertEqual(result['delivery_discount_minor_amount'], -600)