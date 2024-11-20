9b385e58c5ac2fe68af0593332a876f2b2215b78
Raja Prasad <rajp@odoo.com>
2024-11-20 09:50:49 +0000

    [FIX] hr,hr_skills,resource_mail: prevent component destruction
    Steps to Reproduce:
    - Open the Planning app.
    - Click on the avatar of a resource.
    - Refresh the page, then click on the avatar of a resource again.
    
    Issue:
    - A traceback error occurs when clicking on the avatar after a page refresh, likely
      due to component destruction or incomplete loading.
    
    Solution:
    - Consolidated multiple RPC calls into a single method in the parent component
      to avoid issues with component destruction during `super.onWillStart`.
    - Modified `onWillStart` to ensure `hr_access` data is fetched without risking
      lifecycle conflicts.
    
    task-4210513
    
    X-original-commit: eaa21a9dcc65bd4d3d86289b3448f529e7544485

--- addons/hr/models/hr_employee.py
+++ addons/hr/models/hr_employee.py
@@ -1345,6 +1345,8 @@ class HrEmployee(models.Model):
             '|', ('contract_date_end', '>=', date_from), ('contract_date_end', '=', False),
         ])
 
+    def get_avatar_card_data(self, fields):
+        return self.read(fields)
     # ---------------------------------------------------------
     # Messaging
     # ---------------------------------------------------------
