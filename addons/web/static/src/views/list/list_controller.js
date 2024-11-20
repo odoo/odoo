06c614609a341f0d45e92f8a84b97933d0e829b9
Michaël Mattiello <mcm@odoo.com>
2024-11-20 11:47:50 +0000

    [FIX] web: improve a bit the perf of list view
    This commit reduces the number of read to a getter `editedRecord` which
    loops on all the record of the list to find the current one in edition.
    
    closes odoo/odoo#188892
    
    X-original-commit: be38c8a0823fd7a2a2e10c910c433c8d0bd9c2ca
    Related: odoo/enterprise#74735
    Signed-off-by: Julien Mougenot (jum) <jum@odoo.com>
    Signed-off-by: Michaël Mattiello (mcm) <mcm@odoo.com>

--- addons/web/static/src/views/list/list_controller.js
+++ addons/web/static/src/views/list/list_controller.js
@@ -33,6 +33,7 @@ import {
     Component,
     onMounted,
     onWillPatch,
+    onWillRender,
     onWillStart,
     useEffect,
     useRef,
@@ -97,6 +98,11 @@ export class ListController extends Component {
 
         this.optionalActiveFields = {};
 
+        this.editedRecord = null;
+        onWillRender(() => {
+            this.editedRecord = this.model.root.editedRecord;
+        });
+
         onWillStart(async () => {
             this.isExportEnable = await user.hasGroup("base.group_allow_export");
         });
@@ -128,9 +134,8 @@ export class ListController extends Component {
                 return this.model.root.leaveEditMode();
             },
             beforeUnload: async (ev) => {
-                const editedRecord = this.model.root.editedRecord;
-                if (editedRecord) {
-                    const isValid = await editedRecord.urgentSave();
+                if (this.editedRecord) {
+                    const isValid = await this.editedRecord.urgentSave();
                     if (!isValid) {
                         ev.preventDefault();
                         ev.returnValue = "Unsaved changes";
@@ -159,8 +164,8 @@ export class ListController extends Component {
                 limit: limit,
                 total: count,
                 onUpdate: async ({ offset, limit }, hasNavigated) => {
-                    if (this.model.root.editedRecord) {
-                        if (!(await this.model.root.editedRecord.save())) {
+                    if (this.editedRecord) {
+                        if (!(await this.editedRecord.save())) {
                             return;
                         }
                     }
@@ -314,7 +319,7 @@ export class ListController extends Component {
 
     async onClickSave() {
         return executeButtonCallback(this.rootRef.el, async () => {
-            const saved = await this.model.root.editedRecord.save();
+            const saved = await this.editedRecord.save();
             if (saved) {
                 await this.model.root.leaveEditMode();
             }
@@ -610,8 +615,8 @@ export class ListController extends Component {
     }
 
     async beforeExecuteActionButton(clickParams) {
-        if (clickParams.special !== "cancel" && this.model.root.editedRecord) {
-            return this.model.root.editedRecord.save();
+        if (clickParams.special !== "cancel" && this.editedRecord) {
+            return this.editedRecord.save();
         }
     }
 
@@ -628,7 +633,7 @@ export class ListController extends Component {
                 const dialogProps = {
                     confirm: () => resolve(true),
                     cancel: () => {
-                        if (this.model.root.editedRecord) {
+                        if (this.editedRecord) {
                             this.model.root.leaveEditMode({ discard: true });
                         } else {
                             editedRecord.discard();
