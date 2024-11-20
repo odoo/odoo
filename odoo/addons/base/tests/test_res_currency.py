44fbe4d6497e0c440ead93b5f5af7311b5de1459
Leloup Loïc (lole) <lole@odoo.com>
2024-11-20 09:13:44 +0100

    [FIX] core : _name_search on fields with inconsistent types
    Steps to reproduce the issue:
    - go to currencies;
    - search for a rate with something else than a date;
    - it crashes.
    
    This happens because the currency rate model is searched with two
    fields, a date field (name) and a float field (rate).  This crashes when
    converting the value to match against, because the value cannot be
    serialized in SQL both as a date and a float.
    
    What we do in such a case is to explicitly convert the value to the
    field's type.  If the conversion fails, we simply ignore that part of
    the domain.
    
    opw-4278234
    
    closes odoo/odoo#187838
    
    Signed-off-by: Raphael Collet <rco@odoo.com>

--- odoo/addons/base/tests/test_res_currency.py
+++ odoo/addons/base/tests/test_res_currency.py
@@ -21,3 +21,20 @@ class TestResConfig(TransactionCase):
                 node_inverse_company_rate = tree.xpath('//field[@name="inverse_company_rate"]')[0]
                 self.assertEqual(node_company_rate.get('string'), f'Unit per {expected_currency}')
                 self.assertEqual(node_inverse_company_rate.get('string'), f'{expected_currency} per Unit')
+
+    def test_res_currency_name_search(self):
+        currency_A, currency_B = self.env["res.currency"].create([
+            {"name": "cuA", "symbol": "A"},
+            {"name": "cuB", "symbol": "B"},
+        ])
+        self.env["res.currency.rate"].create([
+            {"name": "1971-01-01", "rate": 2.0, "currency_id": currency_A.id},
+            {"name": "1971-01-01", "rate": 1.5, "currency_id": currency_B.id},
+            {"name": "1972-01-01", "rate": 0.69, "currency_id": currency_B.id},
+        ])
+        # should not try to match field 'rate' (float field)
+        self.assertEqual(self.env["res.currency"].search_count([["rate_ids", "=", "1971-01-01"]]), 2)
+        # should not try to match field 'name' (date field)
+        self.assertEqual(self.env["res.currency"].search_count([["rate_ids", "=", "0.69"]]), 1)
+        # should not try to match any of 'name' and 'rate'
+        self.assertEqual(self.env["res.currency"].search_count([["rate_ids", "=", "irrelevant"]]), 0)
