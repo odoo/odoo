1d2e03e691e1df8046e3bbf10f1d7501bd89078a
william-andre <wan@odoo.com>
2024-11-20 13:41:53 +0000

    [FIX] l10n_*: auto install syscohada based on account
    The dependency `l10n_syscohada` doesn't get installed automatically when
    account is installed, therefore these l10n don't get installed either.
    
    Each localization that had `countries` set in the manifest should have
    an `auto_install` containing the main app it is for.
    
    closes odoo/odoo#188095
    
    X-original-commit: a11db3e6009f0788ff26b5309b9d1cb00d29ae36
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>

--- addons/l10n_ne/__manifest__.py
+++ addons/l10n_ne/__manifest__.py
@@ -11,8 +11,9 @@ The Chart of Accounts is from SYSCOHADA.
     """,
     'depends': [
         'l10n_syscohada',
+        'account',
     ],
-    'auto_install': ['l10n_syscohada'],
+    'auto_install': ['account'],
     'data': [
         'data/account_tax_report_data.xml'
     ],
