keyboard-slayer/odoo-eval

[IMP] pos_*: payment terminal based methods in point of sale
pos_*: point_of_sale, pos_adyen, pos_mercado_pago, pos_online_payment,
pos_paytm, pos_razorpay, pos_six, pos_stripe, pos_viva_wallet,
pos_restaurant_adyeny

Before this commit:

- Users had to add common field values for the same provider-based method
  multiple times, which was time-consuming.
- There is a boolean field called `is_online_payment` used to indicate whether
  an online payment method is being created.

After this commit:

- Users only need to add common field values once. After that, common field
  values are automatically populated, saving time when creating payment methods.
- The `is_online_payment` field has been removed from `pos.payment.method`. The
  selection value `Online` has been added to create an online payment method.

Related PR:

Enterprise: odoo/enterprise#67699
Upgrade: odoo/upgrade#6329

task-3506646

addons/pos_online_payment/static/src/overrides/pos_overrides/components/payment_screen/payment_screen.js
@@ -8,7 +8,10 @@ import { ask } from "@point_of_sale/app/store/make_awaitable_dialog";
 
 patch(PaymentScreen.prototype, {
     async addNewPaymentLine(paymentMethod) {
-        if (paymentMethod.is_online_payment && typeof this.currentOrder.id === "string") {
+        if (
+            paymentMethod.payment_method_type === "online" &&
+            typeof this.currentOrder.id === "string"
+        ) {
             this.currentOrder.date_order = luxon.DateTime.now().toFormat("yyyy-MM-dd HH:mm:ss");
             this.pos.addPendingOrder([this.currentOrder.id]);
             await this.pos.syncAllOrders();
@@ -18,7 +21,8 @@ patch(PaymentScreen.prototype, {
     getRemainingOnlinePaymentLines() {
         return this.paymentLines.filter(
             (line) =>
-                line.payment_method_id.is_online_payment && line.get_payment_status() !== "done"
+                line.payment_method_id.payment_method_type === "online" &&
+                line.get_payment_status() !== "done"
         );
     },
     checkRemainingOnlinePaymentLines(unpaidAmount) {
@@ -59,7 +63,7 @@ patch(PaymentScreen.prototype, {
             return false;
         }
 
-        if (!this.payment_methods_from_config.some((pm) => pm.is_online_payment)) {
+        if (!this.payment_methods_from_config.some((pm) => pm.payment_method_type === "online")) {
             return true;
         }
 

odoo/odoo

addons/pos_online_payment/static/src/overrides/pos_overrides/components/payment_screen/payment_screen.js
@@ -8,7 +8,10 @@ import { ask } from "@point_of_sale/app/utils/make_awaitable_dialog";
 
 patch(PaymentScreen.prototype, {
     async addNewPaymentLine(paymentMethod) {
-        if (paymentMethod.is_online_payment && typeof this.currentOrder.id === "string") {
+        if (
+            paymentMethod.payment_method_type === "online" &&
+            typeof this.currentOrder.id === "string"
+        ) {
             this.currentOrder.date_order = luxon.DateTime.now().toFormat("yyyy-MM-dd HH:mm:ss");
             this.pos.addPendingOrder([this.currentOrder.id]);
             await this.pos.syncAllOrders();
@@ -17,7 +20,9 @@ patch(PaymentScreen.prototype, {
     },
     getRemainingOnlinePaymentLines() {
         return this.paymentLines.filter(
-            (line) => line.payment_method_id.is_online_payment && line.getPaymentStatus() !== "done"
+            (line) =>
+                line.payment_method_id.payment_method_type === "online" &&
+                line.getPaymentStatus() !== "done"
         );
     },
     checkRemainingOnlinePaymentLines(unpaidAmount) {
@@ -58,7 +63,7 @@ patch(PaymentScreen.prototype, {
             return false;
         }
 
-        if (!this.payment_methods_from_config.some((pm) => pm.is_online_payment)) {
+        if (!this.payment_methods_from_config.some((pm) => pm.payment_method_type === "online")) {
             return true;
         }
 