24747c3b78cbc80c5663efa8a1fa9fe837ba5f50
Yousef Alnashef (alny) <alny@odoo.com>
2024-11-21 09:44:13 +0000

    Cherry pick of 796b434dbd7b862dde1a69b1f526972853077f60 failed
    stdout:
    Auto-merging addons/l10n_sa/__manifest__.py
    CONFLICT (content): Merge conflict in addons/l10n_sa/__manifest__.py
    Auto-merging addons/l10n_sa_pos/__manifest__.py
    Auto-merging odoo/addons/base/i18n/base.pot
    
    stderr:
    
    X-original-commit: acb5273b9893073c98ea85fe64c33e47bd4a6982

--- addons/l10n_sa/__manifest__.py
+++ addons/l10n_sa/__manifest__.py
@@ -6,10 +6,38 @@
     'version': '2.0',
     'author': 'Odoo S.A., DVIT.ME (http://www.dvit.me)',
     'category': 'Accounting/Localizations/Account Charts',
+<<<<<<< saas-17.2
     'description': """
 Odoo Arabic localization for most Saudi Arabia.
 """,
+<<<<<<< saas-17.4
     'website': 'https://www.odoo.com/documentation/master/applications/finance/fiscal_localizations/saudi_arabia.html',
+||||||| a9ae7f5bb0f978d40a99389c048edb5ddb397339
+    'website': 'https://www.odoo.com/documentation/saas-17.2/applications/finance/fiscal_localizations/saudi_arabia.html',
+=======
+    'website': 'https://www.odoo.com/documentation/saas-17.2/applications/finance/fiscal_localizations/saudi_arabia.html',
+||||||| 994c50671e6dc694d869fe27b385002634432dcd
+    'description': """
+Odoo Arabic localization for most Saudi Arabia.
+""",
+    'website': 'https://www.odoo.com/documentation/17.0/applications/finance/fiscal_localizations/saudi_arabia.html',
+=======
+    'website': 'https://www.odoo.com/documentation/17.0/applications/finance/fiscal_localizations/saudi_arabia.html',
+    'description': """
+Saudi Arabia Accounting Module
+===========================================================
+Saudi Arabia Accounting Basic Charts and Localization
+
+Activates:
+
+- Chart of Accounts
+- Taxes
+- Vat Filling Report
+- Withholding Tax Report
+- Fiscal Positions
+""",
+>>>>>>> a5c4643dc81b7dccdf2bf32bd82233edc664585e
+>>>>>>> acb5273b9893073c98ea85fe64c33e47bd4a6982
     'depends': [
         'l10n_gcc_invoice',
         'account',
