d2601f09e6b9040b9c7870f432abde061251deff
FrancoisGe <fge@odoo.com>
2024-11-21 13:09:05 +0100

    remove toolbar from snippet menu

--- addons/html_builder/static/src/builder/snippets_menu_tabs/customize_tab.js
+++ addons/html_builder/static/src/builder/snippets_menu_tabs/customize_tab.js
@@ -1,10 +1,9 @@
 import { Component } from "@odoo/owl";
-import { Toolbar } from "@html_editor/main/toolbar/toolbar";
 import { WithSubEnv } from "../builder_helpers";
 
 export class CustomizeTab extends Component {
     static template = "html_builder.CustomizeTab";
-    static components = { Toolbar, WithSubEnv };
+    static components = { WithSubEnv };
     static props = {
         selectedToolboxes: { type: Object },
     };
