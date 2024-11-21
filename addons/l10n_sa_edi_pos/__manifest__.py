b9c38f95ede3130342bccbfcb7d41e58c7efb47e
Yousef Alnashef (alny) <alny@odoo.com>
2024-11-21 05:17:43 +0000

    [IMP] l10n_sa, *: Improve module descriptions
    In this commit module descriptions are improved to reflect more accurate information.
    
    The reason for this change is to keep consistency between l10n manifests.
    
    task-4179478
    
    X-original-commit: 88f317753bb8c9a986462c41139f7b2ecf4c144e

--- addons/l10n_sa_edi_pos/__manifest__.py
+++ addons/l10n_sa_edi_pos/__manifest__.py
@@ -8,12 +8,11 @@
         'l10n_sa_pos',
         'l10n_sa_edi',
     ],
-    'author': 'Odoo',
     'summary': """
         ZATCA E-Invoicing, support for PoS
     """,
     'description': """
-        E-invoice implementation for the Kingdom of Saudi Arabia (PoS)
+E-invoice implementation for Saudi Arabia; Integration with ZATCA (POS)
     """,
     'category': 'Accounting/Localizations/EDI',
     'license': 'LGPL-3',
