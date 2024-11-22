78aa3981b62ed73399e6b13ae6dba5b2abdc73bd
Antoine Guenet <age@odoo.com>
2024-11-22 12:56:24 +0100

    [IMP] html_editor: toggle lists over block segments instead of blocks
    Use the new `splitBlockSegments` method before toggling a list, so that
    multiple block segments will create multiple list items:
    
    ```html
    <p>a<br>b<br>[c<br>d<br>e]<br>f<br>g</p>
    ```
    
    -> toggle unordered list
    
    ->
    
    ```html
    <p>a<br>b</p>
    <ul>
        <li>[c</li>
        <li>d</li>
        <li>e]</li>
    </ul>
    <p>f<br>g</p>
    ```
    
    task-4636426

--- addons/html_editor/static/src/main/list/list_plugin.js
+++ addons/html_editor/static/src/main/list/list_plugin.js
@@ -266,6 +266,8 @@ export class ListPlugin extends Plugin {
             throw new Error(`listStyle is not compatible with "CL" list type`);
         }
 
+        this.dependencies.split.splitBlockSegments();
+
         // @todo @phoenix: original implementation removed whitespace-only text nodes from targetedNodes.
         // Check if this is necessary.
 
