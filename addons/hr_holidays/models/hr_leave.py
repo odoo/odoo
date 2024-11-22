1bd1a9c92fcf31af4b86079255bbfe213992a110
ravb-odoo <ravb@odoo.com>
2024-11-22 17:35:10 +0530

    [FIX] hr_holidays: fix activity when time off officer is not set
    Steps:
    - Install the hr_holiday module
    - Configure Time Off Type with validation set to `Approved by Time Off Officer`
      or `Both`
    - Create a Time Off request without setting a Time Off Officer.
    
    Description of the issue/feature this PR addresses:
    When the validation type is set to `hr` or `both` and no Time Off Officer is
    configured, an activity is incorrectly generated upon creating a Time Off
    request.
    
    Cause:
    This issue arises due to unhandled overflow in cases where feedback is
    unpublished.
    
    Fix:
    This PR resolves the issue by ensuring the responsible user is set to empty when
    validation is `hr` or `Both` and no Time Off Officer is configured.
    
    task-4351688

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -1541,12 +1541,14 @@ Attempting to double-book your time off won't magically make your vacation 2x be
     def _get_responsible_for_approval(self):
         self.ensure_one()
 
-        responsible = self.env.user
+        responsible = self.env['res.users']
 
         if self.holiday_type != 'employee':
             return responsible
-
-        if self.validation_type == 'manager' or (self.validation_type == 'both' and self.state == 'confirm'):
+        if self.validation_type == 'both' and not self.holiday_status_id.responsible_ids and self.state == 'confirm':
+            if self.employee_id.parent_id.user_id:
+                responsible = self.employee_id.parent_id.user_id
+        elif self.validation_type == 'manager' or (self.validation_type == 'both' and self.state == 'confirm'):
             if self.employee_id.leave_manager_id:
                 responsible = self.employee_id.leave_manager_id
             elif self.employee_id.parent_id.user_id:
