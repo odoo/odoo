e1d41ec57ebc84612f9d5c5c320569a972dde635
Aaron Bohy <aab@odoo.com>
2024-11-22 13:44:29 +0100

    [FIX] web: RelationalModel: reset offset if no record
    Open a list view with multiple pages. Once opened, simulate some
    records are deleted (e.g. in another tab, delete some records).
    You must delete enough records s.t. there one page less than when
    you loaded the list in the first tab. Then go back to the first
    tab, and try to go to the last page.
    
    Before this commit, it displayed an empty list, and the pager
    was a bit lost (e.g. it could display something like 4-3/3).
    With this commit, we detect that we tried to load records with an
    offset, but the result was empty, so we reset the offset to 0 and
    try again. We thus land on the first page, with a correct pager.
    
    Task~3736927
    
    Part-of: odoo/odoo#188235
    Signed-off-by: Lucas Perais (lpe) <lpe@odoo.com>

--- addons/web/static/src/views/list/list_renderer.js
+++ addons/web/static/src/views/list/list_renderer.js
@@ -1064,7 +1064,7 @@ export class ListRenderer extends Component {
         return {
             offset: list.offset,
             limit: list.limit,
-            total: list.isGrouped ? list.count : group.count,
+            total: list.count,
             onUpdate: async ({ offset, limit }) => {
                 await list.load({ limit, offset });
                 this.render(true);
@@ -1813,10 +1813,7 @@ export class ListRenderer extends Component {
     }
 
     showGroupPager(group) {
-        return (
-            !group.isFolded &&
-            group.list.limit < (group.list.isGrouped ? group.list.count : group.count)
-        );
+        return !group.isFolded && group.list.limit < group.list.count;
     }
 
     /**
