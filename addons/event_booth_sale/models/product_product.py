55594b6c0da6b217afc2b3851ce57609f086a70d
Andrzej(pian) <pian@odoo.com>
2024-11-22 14:24:16 +0100

    [FIX] event_booth_sale: Constrain service_tracking for Event Booths products
    Using "event" service_tracking (attempting to create new event registration
    on booth booking) was leading breaking the flow described below. This commit
    is locking users from setting the event service tracking on products using
    event booths.
    
    Reproduction steps
    ---
    - -i website_event_booth_sale
    - Create Product P
    	- Product Type: Service
    	- Create on Order: Event Registration
    - Create a Booth Category BC with: Product P
    - Have an Published Event E with:
    	- Booth B of the category BC
    	- Website Submenu and Booth Register
    - Go to website-> event-> E -> select B -> "Go to Payment" -> BUG: page frozen
    
    opw-4280158

--- addons/event_booth_sale/models/product_product.py
+++ addons/event_booth_sale/models/product_product.py
@@ -1,4 +1,5 @@
 from odoo import api, models
+from odoo.exceptions import ValidationError
 
 
 class Product(models.Model):
@@ -8,3 +9,14 @@ class Product(models.Model):
     def _onchange_type_event_booth(self):
         if self.service_tracking == 'event_booth':
             self.invoice_policy = 'order'
+
+    @api.constrains('service_tracking')
+    def _check_service_tracking_for_event_booths(self):
+        for product in self:
+            if product.service_tracking != 'event_booth':
+                booth_category = self.env['event.booth.category'].search([('product_id', '=', product.id)], limit=1)
+                if booth_category:
+                    raise ValidationError(
+                        "You cannot change the service_tracking of this product because it is already assigned "
+                        "to an Event Booth Category. The service_tracking must remain 'event_booth'."
+                    )
--- addons/event_booth_sale/models/product_template.py
+++ addons/event_booth_sale/models/product_template.py
@@ -1,4 +1,5 @@
 from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class ProductTemplate(models.Model):
@@ -20,3 +21,19 @@ class ProductTemplate(models.Model):
 
     def _service_tracking_blacklist(self):
         return super()._service_tracking_blacklist() + ['event_booth']
+
+    @api.constrains('service_tracking')
+    def _check_service_tracking_for_event_booths(self):
+        """ Prevent changing the service_tracking field if the product template or any of its variants
+        is linked to an Event Booth Category.
+        """
+        for template in self:
+            linked_booth_category = self.env['event.booth.category'].sudo().search([
+                ('product_id', 'in', template.product_variant_ids.ids)
+            ], limit=1)
+            if linked_booth_category and template.service_tracking != 'event_booth':
+                raise ValidationError(
+                    _('The "service_tracking" for this product template cannot be changed because '
+                      'one of its variants is assigned to an Event Booth Category. '
+                      'The service_tracking must remain "Event Booth".')
+                )
