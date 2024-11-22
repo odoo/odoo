fd7d9c7b4b392fa28c6a52d5075eac597e9a91c7
Dossogne Bertrand <bedo@odoo.com>
2024-11-22 13:22:15 +0100

    [MOV] move hr_work_entry_holidays_contract tests to hr_payroll_holidays

--- addons/hr_work_entry_holidays_contract/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import test_leave
-from . import test_multi_contract
-from . import test_payslip_holidays_computation
-from . import test_performance
-from . import test_work_entry
