e62dce8378846246da0fb887506f8df778242741
Arnold Moyaux <arm@odoo.com>
2024-11-22 09:16:48 +0000

    [FIX] stock_account: modify costing method triple valuation amount
    Following commit 28344743fc578024126538a8998fabc3675f9d90
    Changing the costing method will only debit the stock valuation account
    resulting in a completely wrong value.
    
    Fix the context typo making the patch ineffective + adding it
    to the product template when switching the category on the product.
    
    opw-4315793
    
    X-original-commit: a3a844e824674ea25db3862764188d9c0a1f4ee7
    Part-of: odoo/odoo#188666
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/stock_account/models/product.py
+++ addons/stock_account/models/product.py
@@ -49,7 +49,7 @@ class ProductTemplate(models.Model):
                     ._svl_empty_stock(description, product_template=product_template)
                 out_stock_valuation_layers = SVL.create(out_svl_vals_list)
                 if product_template.valuation == 'real_time':
-                    move_vals_list += Product._svl_empty_stock_am(out_stock_valuation_layers)
+                    move_vals_list += Product.with_context(products_orig_quantity_svl=products_orig_quantity_svl)._svl_empty_stock_am(out_stock_valuation_layers)
                 impacted_templates[product_template] = (products, description, products_orig_quantity_svl)
 
         res = super(ProductTemplate, self).write(vals)
@@ -976,7 +976,8 @@ class ProductCategory(models.Model):
                     ._svl_empty_stock(description, product_category=product_category)
                 out_stock_valuation_layers = SVL.sudo().create(out_svl_vals_list)
                 if product_category.property_valuation == 'real_time':
-                    move_vals_list += Product.with_context(product_orig_quantity_svl=products_orig_quantity_svl)._svl_empty_stock_am(out_stock_valuation_layers)
+
+                    move_vals_list += Product.with_context(products_orig_quantity_svl=products_orig_quantity_svl)._svl_empty_stock_am(out_stock_valuation_layers)
                 impacted_categories[product_category] = (products, description, products_orig_quantity_svl)
 
         res = super(ProductCategory, self).write(vals)
