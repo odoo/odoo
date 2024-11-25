9d209305a8a4210e9a534d11348abc03c27e63a7
Prakash Prajapati <ppr@odoo.com>
2024-11-25 18:15:38 +0530

    [IMP] project, sale_project: hootify tests
    task-4354290
    
    closes odoo/odoo#188943
    
    Related: odoo/enterprise#74759
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

---
+++ addons/sale_project/static/tests/project_task_model.js
@@ -0,0 +1,27 @@
+import { fields, models } from "@web/../tests/web_test_helpers";
+import { projectModels } from "@project/../tests/project_models";
+
+
+export class ProjectTask extends projectModels.ProjectTask {
+    _name = "project.task";
+
+    sale_line_id = fields.Many2one({ string: "Sale Order Line", relation: "sale.order.line" });
+}
+
+export class ProjectMilestone extends projectModels.ProjectMilestone {
+    _name = "project.milestone";
+
+    product_uom_qty = fields.Float({ string: "Quantity" });
+    quantity_percentage = fields.Float({ string: "Percentage" });
+}
+
+export class SaleOrderLine extends models.Model {
+    _name = "sale.order.line";
+
+    name = fields.Char({ string: "name" });
+}
+
+Object.assign(projectModels, {
+    ProjectMilestone,
+    ProjectTask,
+});
