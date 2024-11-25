3ad4d78d58e05014ad36334d5e36e8b32a100ffa
FrancoisGe <fge@odoo.com>
2024-11-25 15:36:35 +0100

    ToolboxComponent => OptionComponent

--- addons/html_builder/static/src/builder/options/layout_option.js
+++ addons/html_builder/static/src/builder/options/layout_option.js
@@ -69,8 +69,8 @@ export class LayoutOption extends Component {
     }
 }
 
-registry.category("sidebar-element-option").add("SectionToolbox", {
-    ToolboxComponent: LayoutOption,
+registry.category("sidebar-element-option").add("LayoutOption", {
+    OptionComponent: LayoutOption,
     selector: "section, section.s_carousel_wrapper .carousel-item, .s_carousel_intro_item",
     // TODO add exclude  data-exclude=".s_dynamic, .s_dynamic_snippet_content, .s_dynamic_snippet_title, .s_masonry_block, .s_framed_intro, .s_features_grid, .s_media_list, .s_table_of_content, .s_process_steps, .s_image_gallery, .s_timeline, .s_pricelist_boxed, .s_quadrant, .s_pricelist_cafe, .s_faq_horizontal, .s_image_frame, .s_card_offset, .s_contact_info"
     // TODO add target (applyTo) data-target="> *:has(> .row), > .s_allow_columns"
