b924ef9fb02d0c4eb5ae10c84e4bd56d878ae18c
Julien Schwanen <scju@odoo.com>
2024-11-25 12:53:16 +0100

    [FIX] hr_holidays: allow search on name
    Steps to reproduce:
    
     - Go in time off app -> overview
     - Search on name
    
    Issue:
    
    When searching on name, the search does not work. This is due to the
    name being computed and not stored. Directly related to the [recent
    changes](odoo/odoo@1239c389b4281ce3663e86aa671b6c8aeda3bb70)
    
    Fix
    
    The field has now a search method on the different component of the name
    separetly.
    
    opw-4351362

--- addons/hr_holidays/tests/test_holidays_calendar.py
+++ addons/hr_holidays/tests/test_holidays_calendar.py
@@ -40,3 +40,25 @@ class TestHolidaysCalendar(HttpCase, TestHrHolidaysCommon):
         self.assertEqual(last_leave.date_from.weekday(), 3, "It should be Thursday")
         self.assertEqual(last_leave.date_from.hour, expected_leave_start, "Wrong start of the day")
         self.assertEqual(last_leave.date_to.hour, expected_leave_end, "Wrong end of the day")
+
+    def test_search_holidays_calendar(self):
+        """
+        Test the search functionality of the holidays calendar.
+
+        Verifies that the search results match expected outcomes for different
+        search terms and user roles, considering user access rights.
+        """
+        search_cases = [
+            ('3 days', [True, True, True]),
+            ('Sick', [False, True, True]),
+            ('Ronnie Hart', [True, True, True]),
+        ]
+        users = [self.user_employee, self.user_hrmanager, self.user_hruser]
+
+        for term, expected_results in search_cases:
+            for user, expected in zip(users, expected_results):
+                records = self.env['hr.leave.report.calendar'].search(
+                    self.env['hr.leave.report.calendar'].with_user(user)._search_name('ilike', term)
+                )
+                self.assertEqual(bool(records), expected, 
+                                f"Failed for term '{term}' with user {user.login}. Expected {expected}, got {bool(records)}.")
