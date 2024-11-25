c5a0484469b6fd3019f8712ecc5ba9947cf39e5a
Nguyễn Đại Dương <daiduongnguyen2709@gmail.com>
2024-11-25 06:44:31 +0000

    [FIX] mail: can't open document when message log on channel
    * STEP TO REPRODUCE: try to _message_log on a channel contain record
    link (an a tag with data-oe-model and data-oe-link) then click on it ->
    nothing happen
    * REASON: when _message_log in mail.channel, it will consider that
    message as a NotificationMessage and we do not have store.handleClickOnLink
    for it unlike Message
    * SOLUTION: add handleClickOnLink for MessageNotification
    
    X-original-commit: 9d4f8b2fda5c75438ad30a08b4f7d95eac4f2f23

--- addons/mail/static/src/core/common/thread.js
+++ addons/mail/static/src/core/common/thread.js
@@ -415,6 +415,8 @@ export class Thread extends Component {
                 }),
                 this.props.thread
             );
+        } else {
+            this.store.handleClickOnLink(ev, this.props.thread);
         }
     }
 
