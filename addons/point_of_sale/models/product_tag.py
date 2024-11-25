0125c16736e82948e49755eabf1b2a8872a21a3f
Pedram (pebr) <pebr@odoo.com>
2024-11-25 10:38:16 +0000

    [PERF] point_of_sale: load needed product tags fields
    Before this commit, all fields of product tags were loaded into PoS,
    which was unnecessary. For example, product templates field and product
    variants field, which contain all products with that tag, were loaded.
    This caused performance issues when there were a high number of
    products using tags. This commit fixes the issue by only loading the
    name field, which is needed in PoS.
    
    opw-4343131
    
    closes odoo/odoo#188849
    
    X-original-commit: 2cad17a53fb62bc87d392d15cf61662e1d2d3668
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/point_of_sale/models/product_tag.py
+++ addons/point_of_sale/models/product_tag.py
@@ -1,7 +1,12 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from odoo import models
+from odoo import api
 
 
 class ProductTag(models.Model):
     _name = 'product.tag'
     _inherit = ['product.tag', 'pos.load.mixin']
+
+    @api.model
+    def _load_pos_data_fields(self, config_id):
+        return ['id', 'name']
