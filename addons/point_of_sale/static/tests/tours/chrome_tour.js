15abe631683141e830c8888c2eb382342d01ecaa
Sarah Bellefroid <sbel@odoo.com>
2024-11-25 12:44:41 +0000

    [FIX] point_of_sale: extend customer search fields
    Currently, when searching the through the customers loaded in pos you can
    search using the zip code but if the customer is not loaded you cannot search
    those in db using the zip code.
    
    Steps to reproduce:
    -------------------
    * Create a customer and set his zip code.
    * Open pos shop
    * Search for customers
    * Enter the zip code, normally you shouldn't see the customer
    * Select "Search more"
    > Observation: No customer found
    
    Note: If you search with the customer name it will find it.
    
    Why the fix:
    ------------
    Adding a few search values when loeading customers in the session.
    
    opw-4334412
    
    X-original-commit: 8c30ebb6b7c90f3e62e4f9a4419939bb326761c2

--- addons/point_of_sale/static/tests/tours/chrome_tour.js
+++ addons/point_of_sale/static/tests/tours/chrome_tour.js
@@ -4,6 +4,7 @@ import * as Dialog from "@point_of_sale/../tests/tours/utils/dialog_util";
 import * as PaymentScreen from "@point_of_sale/../tests/tours/utils/payment_screen_util";
 import * as TicketScreen from "@point_of_sale/../tests/tours/utils/ticket_screen_util";
 import * as Chrome from "@point_of_sale/../tests/tours/utils/chrome_util";
+import * as Utils from "@point_of_sale/../tests/tours/utils/common";
 import { registry } from "@web/core/registry";
 
 registry.category("web_tour.tours").add("ChromeTour", {
@@ -125,5 +126,18 @@ registry.category("web_tour.tours").add("OrderModificationAfterValidationError",
 
             // Allow order changes after the error
             ProductScreen.clickDisplayedProduct("Test Product", true, "2.00"),
+        ].flat(),
+});
+
+registry.category("web_tour.tours").add("SearchMoreCustomer", {
+    test: true,
+    steps: () =>
+        [
+            Dialog.confirm("Open session"),
+            ProductScreen.clickPartnerButton(),
+            ProductScreen.inputCustomerSearchbar("1111"),
+            Utils.selectButton("Search more"),
+            ProductScreen.clickCustomer("BPartner"),
+            ProductScreen.isShown(),
         ].flat(),
 });
