6cb22f3494dc12c40b819ad467920bef8dc63afd
Rodolpho Lima <rcdl@odoo.com>
2024-11-26 13:48:05 +0100

    [IMP] html_editor: upload file in link popover
    This commit adds the possibility to upload a file directly from the link
    popover, allowing the user to create a link to the uploaded file.
    
    Such links, when not styled as buttons, have a small download icon
    before them.
    
    task-3522395

--- addons/html_editor/static/src/main/link/link_plugin.js
+++ addons/html_editor/static/src/main/link/link_plugin.js
@@ -100,6 +100,21 @@ async function fetchInternalMetaData(url) {
     return result;
 }
 
+async function fetchAttachmentMetaData(url, ormService) {
+    try {
+        const urlParsed = new URL(url, window.location.origin);
+        const attachementId = parseInt(urlParsed.pathname.split("/").pop());
+        const [{ name, mimetype }] = await ormService.read(
+            "ir.attachment",
+            [attachementId],
+            ["name", "mimetype"]
+        );
+        return { name, mimetype };
+    } catch {
+        return { name: url, mimetype: undefined };
+    }
+}
+
 /**
  * @typedef { Object } LinkShared
  * @property { LinkPlugin['createLink'] } createLink
@@ -215,6 +230,9 @@ export class LinkPlugin extends Plugin {
 
         this.getExternalMetaData = memoize(fetchExternalMetaData);
         this.getInternalMetaData = memoize(fetchInternalMetaData);
+        this.getAttachmentMetadata = memoize((url) =>
+            fetchAttachmentMetaData(url, this.services.orm)
+        );
     }
 
     destroy() {
@@ -317,6 +335,7 @@ export class LinkPlugin extends Plugin {
             },
             getInternalMetaData: this.getInternalMetaData,
             getExternalMetaData: this.getExternalMetaData,
+            getAttachmentMetadata: this.getAttachmentMetadata,
         };
         if (!selectionData.documentSelectionIsInEditable) {
             // note that data-prevent-closing-overlay also used in color picker but link popover
@@ -404,6 +423,8 @@ export class LinkPlugin extends Plugin {
                     this.removeCurrentLinkIfEmtpy();
                     this.dependencies.history.addStep();
                 },
+                fileUploadEnabled: !this.config.disableFile,
+                uploadFile: this.uploadFile.bind(this),
             };
 
             if (linkEl.isConnected) {
@@ -692,6 +713,19 @@ export class LinkPlugin extends Plugin {
         [targetNode, targetOffset] = edge === "start" ? leftPos(targetNode) : rightPos(targetNode);
         splitOrLineBreakCallback({ ...params, targetNode, targetOffset });
         return true;
+    }
+
+    async uploadFile() {
+        const { upload, getURL } = this.services.uploadLocalFiles;
+        const { resModel, resId } = this.config.getRecordInfo?.() || {};
+        const [attachment] = await upload({ resModel, resId, accessToken: true });
+        if (!attachment) {
+            // No file selected or upload failed
+            return null;
+        }
+        this.config.onAttachmentChange?.(attachment);
+        const url = getURL(attachment, { download: true, unique: true, accessToken: true });
+        return { url, filename: attachment.name };
     }
 }
 
