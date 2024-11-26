3653131d32997392b2170ce542bfc544fd96bef2
masm-odoo <masm@odoo.com>
2024-11-26 11:11:00 +0530

    [FIX] l10n_in: fix fiscal position for vendor bill and refunds
    Before this commit: Fiscal position computation was limited to Invoices and
    Credit Notes. Vendor Bills and Refunds were not included.
    
    After this commit: Fiscal position computation now includes Vendor Bills and
    Refunds as well.
    
    task-4357473
    
    closes odoo/odoo#188564
    
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>

--- addons/l10n_in/models/account_invoice.py
+++ addons/l10n_in/models/account_invoice.py
@@ -71,23 +71,53 @@ class AccountMove(models.Model):
 
     @api.depends('l10n_in_state_id')
     def _compute_fiscal_position_id(self):
-        other_country = None
-        in_moves = self.filtered(lambda move: move.country_code == 'IN' and move.is_sale_document(include_receipts=True))
-        for move in in_moves:
-            state_id = move.l10n_in_state_id.id
-            country_id = move.l10n_in_state_id.country_id.id
-            if move.l10n_in_state_id.l10n_in_tin == '96':  # Foreign Country
-                other_country = other_country or self.env['res.country'].search([('code', '!=', 'IN')], limit=1)
-                country_id = other_country.id
-                state_id = False
-            virtual_partner = self.env['res.partner'].new({
-                'state_id': state_id,
-                'country_id': country_id,
-            })
-            move.fiscal_position_id = self.env['account.fiscal.position'].with_company(
-                move.company_id
-            )._get_fiscal_position(virtual_partner)
-        super(AccountMove, self - in_moves)._compute_fiscal_position_id()
+
+        def _get_fiscal_state(move):
+            """
+            Maps each move to its corresponding fiscal state based on its type,
+            fiscal conditions, and the state of the associated partner or company.
+            """
+
+            if (
+                move.country_code != 'IN'
+                or not move.is_invoice(include_receipts=True)
+                # Partner's FP takes precedence through super
+                or move.partner_shipping_id.property_account_position_id
+                or move.partner_id.property_account_position_id
+            ):
+                return False
+            elif move.l10n_in_gst_treatment == 'special_economic_zone':
+                # Special Economic Zone
+                return self.env.ref('l10n_in.state_in_oc')
+            elif move.is_sale_document(include_receipts=True):
+                # In Sales Documents: Compare place of supply with company state
+                return move.l10n_in_state_id
+            elif move.is_purchase_document(include_receipts=True) and move.partner_id.country_id.code == 'IN':
+                # In Purchases Documents: Compare place of supply with vendor state
+                pos_state_id = move.l10n_in_state_id
+                if pos_state_id.l10n_in_tin == '96':
+                    return pos_state_id
+                elif pos_state_id == move.partner_id.state_id:
+                    # Intra-State: Group by state matching the company's state.
+                    return move.company_id.state_id
+                elif pos_state_id != move.partner_id.state_id:
+                    # Inter-State: Group by state that doesn't match the company's state.
+                    return (
+                        pos_state_id == move.company_id.state_id
+                        and move.partner_id.state_id
+                        or pos_state_id
+                    )
+            return False
+
+        for state_id, moves in self.grouped(_get_fiscal_state).items():
+            if state_id:
+                virtual_partner = self.env['res.partner'].new({
+                    'state_id': state_id.id,
+                    'country_id': state_id.country_id.id,
+                })
+                moves.fiscal_position_id = self.env['account.fiscal.position']._get_fiscal_position(virtual_partner)
+            else:
+                super(AccountMove, moves)._compute_fiscal_position_id()
 
     @api.onchange('name')
     def _onchange_name_warning(self):
