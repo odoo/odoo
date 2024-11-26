9a3001b3d35051074fc8ae7aa133a9e67df8d06d
Jorge Pinna Puissant <jpp@odoo.com>
2024-11-26 08:59:25 +0100

    [IMP] mail: open activity menu record in a new window
    *: calendar, crm, hr_expense, project_todo
    
    This commit adds the possibility to use middle click/Ctrl click to open
    a record in a new window/new tab.
    
    The <a> element has been replaced by a <span>, because we cannot prevent
    the default open in new tab behavior for elements with an href. Since we
    can simply use bootstrap classes to match the visual style, there is no
    difference at all.
    
    Patches have been adapted as well.
    
    task-id 4031939
    
    Part-of: odoo/odoo#185674
    Related: odoo/enterprise#72986
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>
    Co-authored-by: Luca Vitali <luvi@odoo.com>
    Co-authored-by: Jorge Pinna Puissant <jpp@odoo.com>

--- addons/calendar/static/src/activity/activity_menu_patch.js
+++ addons/calendar/static/src/activity/activity_menu_patch.js
@@ -2,10 +2,11 @@ import { ActivityMenu } from "@mail/core/web/activity_menu";
 import { patch } from "@web/core/utils/patch";
 
 patch(ActivityMenu.prototype, {
-    openActivityGroup(group) {
+    openActivityGroup(group, filter, newWindow) {
         if (group.model === "calendar.event") {
             this.dropdown.close();
             this.action.doAction("calendar.action_calendar_event", {
+                newWindow,
                 additionalContext: {
                     default_mode: "day",
                     search_default_mymeetings: 1,
