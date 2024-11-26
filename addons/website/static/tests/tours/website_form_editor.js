2624d5ec9cabc5a99fdc0b80f6799064a4cc5d75
divy-odoo <divy@odoo.com>
2024-11-26 05:09:56 +0000

    [FIX] website: fix website_form_editor tour
    With this PR [1], unfortunately an extra parameter `isCheck` was
    introduced in the tour. This commit removes that unusual parameter.
    
    [1]: https://github.com/odoo/odoo/pull/168474
    
    closes odoo/odoo#189124
    
    X-original-commit: 1f4a85386eb258b08a3a6f43f1f3910598f005c4
    Signed-off-by: Pierre Pulinckx (pipu) <pipu@odoo.com>
    Signed-off-by: Divyesh Vyas (divy) <divy@odoo.com>

--- addons/website/static/tests/tours/website_form_editor.js
+++ addons/website/static/tests/tours/website_form_editor.js
@@ -317,15 +317,14 @@ registerWebsitePreviewTour("website_form_editor_tour", {
     ...clickOnSave(),
     {
         content: "Check 'products' field is visible.",
-        trigger: `iframe .s_website_form:has(${triggerFieldByLabel("Products")}:visible)`,
-        isCheck: true,
+        trigger: `:iframe .s_website_form:has(${triggerFieldByLabel("Products")}:visible)`,
     }, {
         content: "choose the option 'Mitchell Admin' of partner.",
-        trigger: "iframe .checkbox:has(label:contains('Mitchell Admin')) input[type='checkbox']",
+        trigger: ":iframe .checkbox:has(label:contains('Mitchell Admin')) input[type='checkbox']",
+        run: "click",
     }, {
         content: "Check 'products' field is not visible.",
-        trigger: "iframe .s_website_form" +`:has(${triggerFieldByLabel("Products")}:not(:visible))`,
-        isCheck: true,
+        trigger: ":iframe .s_website_form" +`:has(${triggerFieldByLabel("Products")}:not(:visible))`,
     },
     ...clickOnEditAndWaitEditMode(),
     ...addCustomField('selection', 'radio', 'Service', true),
