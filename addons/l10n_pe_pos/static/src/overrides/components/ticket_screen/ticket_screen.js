4f74c3191dcb2fc5177cd6e1a70ca46bc144ee63
Mathieu Coutant <mcou@odoo.com>
2024-11-26 14:58:50 +0100

    [FIX] l10n_pe_pos : autofill the partner on refund
    Steps to reproduce:
    - Install "l10n_pe_pos"
    - Switch to a Peruvian company and open a POS session
    - Sell a product with a partner specified (different from "Consumidor Final")
    - Try to refund the order
    - The partner is back to "Consumidor Final" and can't be changed
    
    Cause:
    The code supposed to put back the right partner is checking a wrong field (`this.pos.consumidorFinalAnonimoId` is undifined) so the partner is not correctly set.
    
    Solution:
    Use the correct field : `this.pos.session._consumidor_final_anonimo_id`
    
    opw-4282195
    
    closes odoo/odoo#188629
    
    Signed-off-by: Andrea Grazioso (agr) <agr@odoo.com>
    Signed-off-by: Mathieu Coutant (mcou) <mcou@odoo.com>

--- addons/l10n_pe_pos/static/src/overrides/components/ticket_screen/ticket_screen.js
+++ addons/l10n_pe_pos/static/src/overrides/components/ticket_screen/ticket_screen.js
@@ -9,7 +9,7 @@ patch(TicketScreen.prototype, {
         if (
             partner &&
             (!destinationOrder.getPartner() ||
-                destinationOrder.getPartner().id === this.pos.consumidorFinalAnonimoId)
+                destinationOrder.getPartner().id === this.pos.session._consumidor_final_anonimo_id)
         ) {
             return destinationOrder.setPartner(partner);
         }
