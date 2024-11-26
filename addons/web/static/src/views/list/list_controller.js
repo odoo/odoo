021f83a8a467967632afac93cdb3aec7b790aa43
Jorge Pinna Puissant <jpp@odoo.com>
2024-11-26 08:36:07 +0100

    [IMP] web: open list view record in a new window
    This commit adds the possibility to use middle click/Ctrl click to open
    a record in a new window/new tab.
    
    task-id 4031939
    
    Co-authored-by: Luca Vitali <luvi@odoo.com>
    Co-authored-by: Jorge Pinna Puissant <jpp@odoo.com>

--- addons/web/static/src/views/list/list_controller.js
+++ addons/web/static/src/views/list/list_controller.js
@@ -274,26 +274,31 @@ export class ListController extends Component {
         }
     }
 
-    async openRecord(record, force = false) {
+    async openRecord(record, { force, newWindow } = { force: false }) {
         const dirty = await record.isDirty();
         if (dirty) {
             await record.save();
         }
         if (this.archInfo.openAction) {
-            this.actionService.doActionButton({
-                name: this.archInfo.openAction.action,
-                type: this.archInfo.openAction.type,
-                resModel: record.resModel,
-                resId: record.resId,
-                resIds: record.resIds,
-                context: record.context,
-                onClose: async () => {
-                    await record.model.root.load();
+            this.actionService.doActionButton(
+                {
+                    name: this.archInfo.openAction.action,
+                    type: this.archInfo.openAction.type,
+                    resModel: record.resModel,
+                    resId: record.resId,
+                    resIds: record.resIds,
+                    context: record.context,
+                    onClose: async () => {
+                        await record.model.root.load();
+                    },
                 },
-            });
+                {
+                    newWindow,
+                }
+            );
         } else {
             const activeIds = this.model.root.records.map((datapoint) => datapoint.resId);
-            this.props.selectRecord(record.resId, { activeIds, force });
+            this.props.selectRecord(record.resId, { activeIds, force, newWindow });
         }
     }
 
