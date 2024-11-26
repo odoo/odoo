d6de920fc1eb68013509b36d6a8ca0863169a8c7
FrancoisGe <fge@odoo.com>
2024-11-26 17:36:44 +0100

    optionContainer title base on editingEl

--- addons/html_builder/static/src/builder/components/OptionsContainer.js
+++ addons/html_builder/static/src/builder/components/OptionsContainer.js
@@ -3,7 +3,10 @@ import { Component } from "@odoo/owl";
 export class OptionsContainer extends Component {
     static template = "html_builder.OptionsContainer";
     static props = {
-        title: String,
         slots: { type: Object, optional: true },
     };
+
+    get title() {
+        return this.env.editingElement.dataset.name;
+    }
 }
