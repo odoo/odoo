db74b61ece529d55ce9376c8c4dd47f4f04d5c6e
Seif (segn) <segn@odoo.com>
2024-11-27 10:36:32 +0100

    [IMP] l10n_{au,nz}: Add custom company registry label
    closes odoo/odoo#187586
    
    closes odoo/odoo#190993
    
    Related: odoo/enterprise#74005
    Related: odoo/enterprise#75824
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: Seif Gneedy (segn) <segn@odoo.com>

---
+++ addons/l10n_nz/models/res_partner.py
@@ -0,0 +1,10 @@
+from odoo import models, _
+
+
+class ResPartner(models.Model):
+    _inherit = ['res.partner']
+
+    def _get_company_registry_labels(self):
+        labels = super()._get_company_registry_labels()
+        labels['NZ'] = _("NZBN")
+        return labels
