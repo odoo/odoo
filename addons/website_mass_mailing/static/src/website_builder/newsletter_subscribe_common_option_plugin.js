350da9c8561276a45d65b3e74418a8cfc28eda16
Antoine (anso) <anso@odoo.com>
2024-11-27 13:20:43 +0100

    [IMP] website(*): add new `s_newsletter_aside` snippet
    *: _mass_mailing
    
    This commit adds the new `s_newsletter_aside` snippet
    
    task-4306896
    
    closes odoo/odoo#188802
    
    Signed-off-by: Benoit Socias (bso) <bso@odoo.com>
    Co-authored-by: Benoit Socias (bso) <bso@odoo.com>

--- addons/website_mass_mailing/static/src/website_builder/newsletter_subscribe_common_option_plugin.js
+++ addons/website_mass_mailing/static/src/website_builder/newsletter_subscribe_common_option_plugin.js
@@ -3,6 +3,7 @@ import { POPUP } from "@website/builder/plugins/options/popup_option_plugin";
 import { Plugin } from "@html_editor/plugin";
 import { withSequence } from "@html_editor/utils/resource";
 import { registry } from "@web/core/registry";
+import { renderToFragment } from "@web/core/utils/render";
 import { NewsletterSubscribeCommonOption } from "./newsletter_subscribe_common_option";
 
 export const NEWSLETTER_SELECT = before(POPUP);
@@ -22,6 +23,7 @@ class NewsletterSubscribeCommonOptionPlugin extends Plugin {
                     ".s_newsletter_box .s_newsletter_list",
                     ".s_newsletter_centered .s_newsletter_list",
                     ".s_newsletter_grid .s_newsletter_list",
+                    ".s_newsletter_aside .s_newsletter_list",
                 ].join(", "),
             }),
             withSequence(NEWSLETTER_SELECT, {
@@ -42,6 +44,7 @@ class NewsletterSubscribeCommonOptionPlugin extends Plugin {
                 dropIn: ".row.o_grid_mode",
             },
         ],
+        on_snippet_dropped_handlers: withSequence(-1, (args) => this.onSnippetDropped(args)),
     };
 
     getProps() {
@@ -49,6 +52,29 @@ class NewsletterSubscribeCommonOptionPlugin extends Plugin {
             fetchMailingLists: this.dependencies.mailingListSubscribeOption.fetchMailingLists,
             hasRecaptcha: this.dependencies.recaptchaSubscribeOption.hasRecaptcha,
         };
+    }
+
+    async onSnippetDropped({ snippetEl }) {
+        if (snippetEl.matches(".s_newsletter_aside")) {
+            const sampleEl = snippetEl.querySelector("#checkbox_sample");
+            if (sampleEl) {
+                const mailingLists = await this.services.orm.searchRead(
+                    "mailing.list",
+                    [["is_public", "=", true]],
+                    ["id", "name"],
+                );
+                if (mailingLists.length) {
+                    const parentEl = sampleEl.parentElement;
+                    sampleEl.remove();
+                    const checkboxesFragment = renderToFragment(
+                        "website_mass_mailing.NewsletterMailingListsCheckboxes", { mailingLists }
+                    );
+                    parentEl.append(...checkboxesFragment.children);
+                } else {
+                    sampleEl.closest(".s_website_form_field").remove();
+                }
+            }
+        }
     }
 }
 
