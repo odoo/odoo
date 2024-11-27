e6d610b4131b6d63dc1b765ddfabd0fdd3713a36
Basioni (basm) <basm@odoo.com>
2024-11-27 09:10:36 +0000

    [FIX] website_sale, delivery: make delivery dialog fully translatable.
    When you try to ship your products to a pick-up point on the website,
    you encounter a dialog to choose the exact location of the pick-up point
    that has the following strings untranslated: "Choose a pick-up point",
    "Choose this location", and "Closed" (if the pick-up location is closed
    on someday). This happens because these strings are defined in
    the `delivery` module, which is not a frontend module. This commit redefines
    those strings in the `website_sale` module, which is a fronend module.
    
    Task-4328208
    OPW-4326840
    
    closes odoo/odoo#189322
    
    X-original-commit: f829fe97f9b430f1289ff873207aac85ca290ce0
    Signed-off-by: Valentin Chevalier <vcr@odoo.com>
    Signed-off-by: Mohammed Basioni (basm) <basm@odoo.com>

--- addons/website_sale/static/src/js/location_selector/location_selector_dialog/location_selector_dialog.js
+++ addons/website_sale/static/src/js/location_selector/location_selector_dialog/location_selector_dialog.js
@@ -4,6 +4,7 @@ import {
     LocationSelectorDialog
 } from '@delivery/js/location_selector/location_selector_dialog/location_selector_dialog';
 import { patch } from '@web/core/utils/patch';
+import { _t } from '@web/core/l10n/translation';
 
 patch(LocationSelectorDialog, {
     props: {
@@ -20,5 +21,20 @@ patch(LocationSelectorDialog.prototype, {
         if (this.props.isFrontend) {
             this.getLocationUrl = '/website_sale/get_pickup_locations';
         }
+    },
+
+    get title() {
+        // The original definition of this getter is in `delivery` module which is not a frontend module. This problem happens in the context of the website. So, it should be repeated here as translations are only fetched in the context of a frontend module, which is `website_sale` in this case.
+        return _t("Choose a pick-up point");
+    },
+
+    get validationButtonLabel() {
+        // The original definition of this getter is in `delivery` module which is not a frontend module. This problem happens in the context of the website. So, it should be repeated here as translations are only fetched in the context of a frontend module, which is `website_sale` in this case.
+        return _t("Choose this location");
+    },
+
+    get postalCodePlaceholder() {
+        // The original definition of this getter is in `delivery` module which is not a frontend module. This problem happens in the context of the website. So, it should be repeated here as translations are only fetched in the context of a frontend module, which is `website_sale` in this case.
+        return _t("Your postal code");
     },
 });
