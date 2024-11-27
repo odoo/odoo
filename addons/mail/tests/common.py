18aadc810d169245504b945bb5b4b2867e6c8d55
Julien Castiaux <juc@odoo.com>
2024-11-27 16:49:58 +0100

    [FIX] mail: multi recipient duplication
    Install mass-mailing and crm. Change the email address of Brandon to
    `a@example.com;b@example.com`. In the list view of crm, add the select
    all leads and change their customer to Brandon (the customer column is
    not displayed by default, just make it visible). Create a new mass-
    mailing with the recipient list as "Lead/Opportunity". Start the
    campaign. Brandon receives as many emails as there are leads but he
    shall only receive one.
    
    The system has a known limitation when it comes to filtering duplicates:
    it skips all records that have multiple recipients. In this case Brandon
    has two: a@example.com and b@example.com. The de-duplication mechanism
    was skipped for every lead he was the customer of and each time a new
    email was sent, spamming him.
    
    In this work we make it possible to also process records with multiple
    recipients. It is a best-effort and will still let some duplicates
    through. Nonetheless it solves the current problem with minimal changes.
    
    Note: `any([])` and `any([''])` are both False while `all([])` is True,
    hence we now check for empty list / empty email first otherwise an empty
    list would be considered to be opt-out instead of empty.
    
    Task-3927361
    
    X-original-commit: 66b9b438394a4b7b6ba2bedf6fa0175ea1a5abb7
    Part-of: odoo/odoo#189071
    Related: odoo/enterprise#75667
    Signed-off-by: Julien Castiaux (juc) <juc@odoo.com>
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mail/tests/common.py
+++ addons/mail/tests/common.py
@@ -243,7 +243,7 @@ class MockEmail(common.BaseCase, MockSmtplibCase):
         find an email sent to them and use its message-ID to simulate a reply.
 
         Some noise is added in References just to test some robustness. """
-        sent_mail = self._find_sent_mail_wemail(email_to)
+        sent_mail = self._find_sent_email_wemail(email_to)
 
         if use_in_reply_to:
             extra = 'In-Reply-To:\r\n\t%s\n' % sent_mail['message_id']
@@ -285,7 +285,37 @@ class MockEmail(common.BaseCase, MockSmtplibCase):
     # GATEWAY GETTERS
     # ------------------------------------------------------------
 
-    def _find_sent_mail_wemail(self, email_to):
+    def _find_sent_email(self, email_from, emails_to, subject=None, body=None, attachment_names=None):
+        """ Find an outgoing email based on from / to and optional subject, body
+        and attachment names when having conflicts.
+
+        :return sent_email: an outgoing email generated during the mock;
+        """
+        sent_emails = [
+            mail for mail in self._mails
+            if set(mail['email_to']) == set(emails_to) and mail['email_from'] == email_from
+        ]
+        if len(sent_emails) > 1:
+            # try to better filter
+            sent_email = next((mail for mail in sent_emails
+                               if (subject is None or mail['subject'] == subject)
+                               and (body is None or mail['body'] == body)
+                               and (attachment_names is None
+                                    or set(attachment_names) == set(attachment[0] for attachment in mail['attachments']))
+                               ), False)
+        else:
+            sent_email = sent_emails[0] if sent_emails else False
+
+        if not sent_email:
+            debug_info = '\n'.join(
+                f"From: {mail['email_from']} - To {mail['email_to']}"
+                for mail in self._mails
+            )
+            raise AssertionError(f'sent mail not found for email_to {emails_to}\n{debug_info}')
+
+        return sent_email
+
+    def _find_sent_email_wemail(self, email_to):
         """ Find a sent email with a given list of recipients. Email should match
         exactly the recipients.
 
@@ -423,28 +453,6 @@ class MockEmail(common.BaseCase, MockSmtplibCase):
                 f'mail.mail not found for message {mail_message} / status {status} / record {record._name}, {record.id} / author {author} ({email_from})\n{debug_info}'
             )
         return mail
-
-    def _find_sent_email(self, email_from, emails_to, subject=None, body=None, attachment_names=None):
-        """ Find an outgoing email based on from / to and optional subject, body
-        and attachment names when having conflicts.
-
-        :return sent_email: an outgoing email generated during the mock;
-        """
-        sent_emails = [
-            mail for mail in self._mails
-            if set(mail['email_to']) == set(emails_to) and mail['email_from'] == email_from
-        ]
-        if len(sent_emails) > 1:
-            # try to better filter
-            sent_email = next((mail for mail in sent_emails
-                               if (subject is None or mail['subject'] == subject)
-                               and (body is None or mail['body'] == body)
-                               and (attachment_names is None
-                                    or set(attachment_names) == set(attachment[0] for attachment in mail['attachments']))
-                               ), False)
-        else:
-            sent_email = sent_emails[0] if sent_emails else False
-        return sent_email
 
     # ------------------------------------------------------------
     # GATEWAY ASSERTS
