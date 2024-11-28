901d66fcf6142631ad49fc252eca8a02206f2604
Nicolas Bayet <nby@odoo.com>
2024-11-28 11:08:33 +0100

    add tooltips to WeRow

--- addons/html_builder/static/src/builder/components/WeRow.js
+++ addons/html_builder/static/src/builder/components/WeRow.js
@@ -4,6 +4,7 @@ export class WeRow extends Component {
     static template = "html_builder.WeRow";
     static props = {
         label: String,
+        tooltip: { type: String, optional: true },
         slots: { type: Object, optional: true },
     };
 }
