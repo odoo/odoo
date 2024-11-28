2251a36bfe519db64d59be1686289bb401bb2d81
Djamel Touati <otd@odoo.com>
2024-11-28 14:46:01 +0000

    [FIX] mrp: do not link BoM attachments to the Product
    Steps to reproduce the issue:
    - Create a storable product “P1” with two BoMs
    - Open one of the BoMs and add an attachment
    - Open the second BoM
    
    Problem:
    The attachment is added to all the BoMs.
    
    opw-4263943
    
    closes odoo/odoo#189386
    
    X-original-commit: af1ec1a3f1faa45d45aeb43c80b94f49258cf709
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Djamel Touati (otd) <otd@odoo.com>

--- addons/mrp/models/__init__.py
+++ addons/mrp/models/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import ir_attachment
 from . import product_document
 from . import res_config_settings
 from . import mrp_bom
