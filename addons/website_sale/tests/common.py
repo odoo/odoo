fe7380e08989f878bf55b8225a9e7bb7ecf402d4
Levi Siuzdak (sile) <sile@odoo.com>
2024-11-28 15:50:14 +0000

    [FIX] website_sale: show desired amount of alternative products
    Versions
    --------
    - saas-17.4+
    
    Steps
    -----
    1. Have a published product 'A' with 16+ variants;
    2. have a published product 'B' with alternative product 'A';
    3. add more alternative products whose names are greater than 'A';
    4. open product 'B' in eCommerce.
    
    Issue
    -----
    Only product 'A' gets displayed.
    
    Cause
    -----
    The dynamic filter for alternative products applies the default limit of
    '16' to the variant count.
    
    This was not an issue in earlier versions, as alternative products
    displayed variants instead if templates.
    
    Solution
    --------
    In the `_prepare_values` method, set the `hide_variants` context value
    even if `self.filter_id` is falsy, making it accessible for dynamic
    filters as well.
    
    In the `_get_products_*` methods, check this context value. If present,
    limit each product template to a single variant before calling `search`.
    
    opw-4318624
    
    closes odoo/odoo#192548
    
    X-original-commit: ed8a0354ed36d5127bb5380bf693d9180a952039
    Signed-off-by: Levi Siuzdak <sile@odoo.com>

--- addons/website_sale/tests/common.py
+++ addons/website_sale/tests/common.py
@@ -5,8 +5,6 @@ from odoo.fields import Command
 from odoo.addons.delivery.tests.common import DeliveryCommon
 from odoo.addons.product.tests.common import ProductCommon
 
-# from odoo.addons.website.tests.common import WebsiteCommon
-
 
 class WebsiteSaleCommon(ProductCommon, DeliveryCommon):
     # Not based on SaleCommon as there is no need for SalesTeamCommon nor standard SaleCommon data
@@ -17,7 +15,8 @@ class WebsiteSaleCommon(ProductCommon, DeliveryCommon):
 
         cls.website = cls.env.company.website_id
         if not cls.website:
-            pass # TODO WebsiteCommon
+            cls.website = cls.env.ref('website.default_website')
+            cls.website.company_id = cls.env.company
 
         cls.public_user = cls.website.user_id
         cls.public_partner = cls.public_user.partner_id
