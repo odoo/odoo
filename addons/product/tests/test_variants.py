040fb3b582694bde1107aebc3a6fe3347fc93e91
Iván Todorovich <ivan.todorovich@gmail.com>
2024-11-28 10:45:47 -0300

    [IMP] product: leverage related fields for single variant products
    Related fields were recently upgraded, and it's now possible to group over them.
    
    With that in mind, this commit refactors the way we handle template fields for
    single variant products, to leverage related fields instead of computed ones.
    
    This reduces the need for boilerplate code.

--- addons/product/tests/test_variants.py
+++ addons/product/tests/test_variants.py
@@ -312,10 +312,8 @@ class TestVariants(ProductVariantsCommon):
         })
         self.assertEqual(len(template.product_variant_ids), 1)
         self.assertEqual(template.barcode, 'test')
-
         template.product_variant_ids.action_archive()
         self.assertFalse(template.active)
-        template.invalidate_model(['barcode'])
         self.assertEqual(template.barcode, 'test')
         template.product_variant_ids.action_unarchive()
         template.action_unarchive()
@@ -338,11 +336,9 @@ class TestVariants(ProductVariantsCommon):
         variant_2.barcode = 'v2_barcode'
 
         variant_1.action_archive()
-        template.invalidate_model(['barcode'])
         self.assertEqual(template.barcode, variant_2.barcode)  # 1 active variant --> barcode on template
 
         variant_1.action_unarchive()
-        template.invalidate_model(['barcode'])
         self.assertFalse(template.barcode)  # 2 active variants --> no barcode on template
 
     @mute_logger('odoo.models.unlink')
