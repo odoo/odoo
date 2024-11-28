e6f77a0d14feca6a4f03d40e459202e926893ed1
odoo <pda@odoo.com>
2024-11-28 17:19:23 +0100

    [IMP] website_sale: add product information as subject in contactus
    Use the contactus url defined in the settings and add a subject
    parameter containing the information of the product such as the
    name and the variants chosen.
    The form in the contact page is then pre-filled with product info
    for the recipient of the email.
    
    task-4331152

--- addons/website_sale/static/src/js/sale_variant_mixin.js
+++ addons/website_sale/static/src/js/sale_variant_mixin.js
@@ -395,6 +395,8 @@ var VariantMixin = {
             contactUsButton.removeClass('d-flex').addClass('d-none');
             product_unavailable.removeClass('d-flex').addClass('d-none');
         }
+        const url = contactUsButton.find('a').attr('data-url');
+        contactUsButton.find('a').attr('href', `${url}?subject=${combination.display_name}`);
 
         const self = this;
         const $price = $parent.find(".oe_price:first .oe_currency_value");
