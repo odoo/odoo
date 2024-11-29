47e90c2e1f110cfb2d06c1564dac37e0df1e1415
damr <damr@odoo.com>
2024-11-29 14:56:55 +0100

    [FIX] project : prevent mismatch with analytic account and their plan
    Currently, when the plan of an analytic account is updated, there is no
    sanity check, nor any computation done on the project accounts. This
    leads to account being linked to the wrong plan in the project setting,
    as well as strange computation result in the project profitability.
    
    step to reproduce :
    - open project setting
    - go to the analytic folder
    - If there are no analytic account on the project plan,  add a new one
    - go to the form view of the analytic account
    - change the plan from project to department (or any other plan)
    - go back to the project setting -> analytic
    
    => the analytic account is still set on the 'project plan' field of the project.
    
    Solution : adds a warning message for the user when he tries to update
    the plan of analytic account from the form or the list view of analytic
    account. If the user confirms his update, the account are removed from their associated project.
    An sql constraint was also added in case some use case update the
    analytic plan without making use of the form view or the list view.
    
    task - 4334520
    affected version : 18.0 - master

--- addons/project/models/account_analytic_account.py
+++ addons/project/models/account_analytic_account.py
@@ -3,6 +3,7 @@
 
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
+from odoo.osv.expression import OR
 
 
 class AccountAnalyticAccount(models.Model):
@@ -40,3 +41,20 @@ class AccountAnalyticAccount(models.Model):
             result['views'] = [(False, "form")]
             result['res_id'] = self.project_ids.id
         return result
+
+    @api.constrains('plan_id')
+    def _ensure_project_plan_consistency(self):
+
+        plan_column_names = self.env['project.project']._get_plan_fnames()
+        # It's cleaner to use the 'auto_account_id' to do this search, but using it causes issues at install with demo data
+        domain = OR([[(fname, 'in', self.ids)] for fname in plan_column_names])
+        projects = self.env['project.project'].search(domain)
+        if not projects:
+            return
+        for project in projects:
+            for plan_column_name in plan_column_names:
+                account = project[plan_column_name]
+                if account and account.id in self.ids and account.root_plan_id._column_name() != plan_column_name:
+                    raise UserError(_("The account '%(account_name)s' is linked to the project '%(project_name)s'. "
+                                            "Unlink it from its project before updating its plan.",
+                                            account_name=account.name, project_name=project.name))
--- addons/project/tests/test_project_profitability.py
+++ addons/project/tests/test_project_profitability.py
@@ -15,13 +15,11 @@ class TestProjectProfitabilityCommon(TransactionCase):
             'name': 'Georges',
             'email': 'georges@project-profitability.com'})
 
-        cls.analytic_plan = cls.env['account.analytic.plan'].create({
-            'name': 'Plan A',
-        })
+        cls.project_plan = cls.env['account.analytic.plan'].browse(int(cls.env['ir.config_parameter'].get_param('analytic.project_plan', 0)))
         cls.analytic_account = cls.env['account.analytic.account'].create({
             'name': 'Project - AA',
             'code': 'AA-1234',
-            'plan_id': cls.analytic_plan.id,
+            'plan_id': cls.project_plan.id,
         })
         cls.project = cls.env['project.project'].with_context({'mail_create_nolog': True}).create({
             'name': 'Project',
