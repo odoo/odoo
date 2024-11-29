d0352ad10391bfbdeb5f37915307acb01ec2a9b0
Sébastien Geelen <sge@odoo.com>
2024-11-29 13:50:45 +0100

    [IMP] html_editor: link popover fixes & improvements
    Fixes :
     * Prevent the Link creation when the selection span multiples Block.
     * Prevent the link extension / link Tool opening when the selection span multiple links.
     * Fix some styles loss during link creation (expect color/bgcolor, see: spec changes).
    
    Specs changes :
     * When creating a new link on existing text, remove the color and background color style.
     * When editing a link with url === label, the label input should be empty.
     * Change the default protocol from "http" to "https" when no protocol was provided in a valid url.
     * Adapt placeholder copywriting for link modal inputs.
    
    Improvements :
     * Change the link creation behavior to avoid inserting link in the DOM before the user validate link popover.
    
    task-4266410
    
    closes odoo/odoo#189594
    
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/html_editor/static/tests/paste.test.js
+++ addons/html_editor/static/tests/paste.test.js
@@ -2546,6 +2546,10 @@ describe("link", () => {
             expect(cleanLinkArtifacts(getContent(el))).toBe(
                 `<p>abc <a href="${url}">${url}</a> def[]</p>`
             );
+            undo(editor);
+            expect(cleanLinkArtifacts(getContent(el))).toBe(
+                `<p placeholder='Type "/" for commands' class="o-we-hint">[]</p>`
+            );
         });
 
         test("should paste and transform image URL among text (collapsed)", async () => {
@@ -2576,6 +2580,10 @@ describe("link", () => {
             expect(cleanLinkArtifacts(getContent(el))).toBe(
                 `<p><a href="${url}">${url}</a> <a href="${videoUrl}">${videoUrl}</a> <a href="${imgUrl}">${imgUrl}</a>[]</p>`
             );
+            undo(editor);
+            expect(cleanLinkArtifacts(getContent(el))).toBe(
+                `<p placeholder='Type "/" for commands' class="o-we-hint">[]</p>`
+            );
         });
 
         test("should paste and transform multiple URLs (collapsed) (2)", async () => {
@@ -2585,6 +2593,10 @@ describe("link", () => {
             expect(".o-we-powerbox").toHaveCount(0);
             expect(cleanLinkArtifacts(getContent(el))).toBe(
                 `<p><a href="${url}">${url}</a> abc <a href="${videoUrl}">${videoUrl}</a> def <a href="${imgUrl}">${imgUrl}</a>[]</p>`
+            );
+            undo(editor);
+            expect(cleanLinkArtifacts(getContent(el))).toBe(
+                `<p placeholder='Type "/" for commands' class="o-we-hint">[]</p>`
             );
         });
 
