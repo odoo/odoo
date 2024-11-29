a55f8d1da9a917a5458bf607596946625f9313df
vlst-odoo <vlst@odoo.com>
2024-11-29 17:08:22 +0100

    [IMP] point_of_sale: add test for renderer
    In this commit we add a unit test for the `render_service`.
    
    closes odoo/odoo#189192
    
    Task: 4370377
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- addons/point_of_sale/__manifest__.py
+++ addons/point_of_sale/__manifest__.py
@@ -94,6 +94,9 @@
             'point_of_sale/static/src/proxy_trap.js',
             'point_of_sale/static/src/lazy_getter.js',
             'point_of_sale/static/src/app/services/data_service.js',
+
+            'point_of_sale/static/src/app/utils/html-to-image.js',
+            'point_of_sale/static/src/app/services/render_service.js',
             'point_of_sale/static/tests/unit/**/*',
         ],
 
