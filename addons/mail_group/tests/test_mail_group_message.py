e5aa5040ad3c9ba32616d01bb63ad9c145ae51ed
Raphael Collet <rco@odoo.com>
2024-11-29 11:24:41 +0100

    [IMP] *: add explicit group implications to make sense of ir.rules
    Some ir.rules defined in those modules have no direct or indirect
    ir.model.access counterpart:
     - base.res_partner_rule_edit_all
     - im_livechat.ir_rule_discuss_channel_group_im_livechat_group_manager
     - im_livechat.ir_rule_discuss_channel_member_group_im_livechat_group_manager
     - mail.ir_rule_mail_canned_response_admin
     - mail_group.mail_group_rule_administrator
     - mail_group.mail_group_message_rule_administrator
     - mail_group.mail_group_member_rule_administrator
     - mail_group.mail_group_moderation_rule_administrator
     - website_blog.website_blog_public
     - website_slides_forum.website_slides_forum_website_slides_officer_post
     - website_slides_forum.website_slides_forum_website_slides_officer_tag
    
    Also, some ir.model.access have no ir.rule for their group:
     - analytic.group_analytic_accounting
    
    It is therefore difficult to determine exactly what permission those
    rules add.  We add group implications so that all the rules' group(s)
    have some corresponding ir.model.access:
     - analytic.group_analytic_accounting -> base.group_user
     - base.group_partner_manager -> base.group_user
     - im_livechat.im_livechat_group_manager -> base.group_user
     - mail.group_mail_canned_response_admin -> base.group_user
     - mail_group.group_mail_group_manager -> base.group_user
     - website.group_website_restricted_editor -> base.group_user
     - website_blog.group_website_blog_manager -> base.group_user
     - website_slides.group_website_slides_officer -> base.group_user
    
    Also add the following implications to make sense of tests:
     - product.group_product_manager -> base.group_user
     - test_website.group_test_website_admin -> base.group_user
     - test_website.group_test_website_tester -> base.group_user
    
    Part-of: odoo/odoo#166359
    Related: odoo/enterprise#120158
    Related: odoo/upgrade#10466
    Related: odoo/upgrade-util#442
    Signed-off-by: Christophe Monniez (moc) <moc@odoo.com>

--- addons/mail_group/tests/test_mail_group_message.py
+++ addons/mail_group/tests/test_mail_group_message.py
@@ -107,6 +107,11 @@ class TestMailGroupMessage(TestMailListCommon):
         self.test_group.access_group_id = user_group
         self.test_group.access_mode = 'groups'
 
+        # As such, the code below crashes because base.group_partner_manager
+        # implies base.group_user, which is disjoint from base.group_portal.
+        # Does it make any sense (a portal user being a partner manager) ?
+        user_group.implied_ids -= self.env.ref('base.group_user')
+
         # Message pending
         with self.assertRaises(AccessError, msg='Portal should not have access to pending messages'):
             self.test_group_msg_1_pending.with_user(self.user_portal).check_access('read')
