ffbb72c08a7ee1e9c67ef213c1b3561d71a06f51
FrancoisGe <fge@odoo.com>
2024-11-29 09:35:06 +0100

    Add template option

--- addons/html_builder/static/src/builder/options/alignment_option.js
+++ addons/html_builder/static/src/builder/options/alignment_option.js
@@ -1,13 +1,6 @@
-import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
-import { defaultOptionComponents } from "../components/defaultComponents";
-
-export class AlignmentOption extends Component {
-    static template = "html_builder.AlignmentOption";
-    static components = { ...defaultOptionComponents };
-}
 
 registry.category("sidebar-element-option").add("AlignmentOption", {
-    OptionComponent: AlignmentOption,
-    selector: ".s_share, .s_text_highlight, .s_social_media",
+    template: "html_builder.AlignmentOption",
+    selector: ".s_share, .s_text_highlight, .s_social_media, section",
 });
--- addons/html_builder/static/src/builder/snippets_menu_tabs/customize_tab.js
+++ addons/html_builder/static/src/builder/snippets_menu_tabs/customize_tab.js
@@ -1,10 +1,9 @@
 import { Component } from "@odoo/owl";
-import { WithSubEnv } from "../builder_helpers";
 import { OptionsContainer } from "../components/OptionsContainer";
 
 export class CustomizeTab extends Component {
     static template = "html_builder.CustomizeTab";
-    static components = { WithSubEnv, OptionsContainer };
+    static components = { OptionsContainer };
     static props = {
         currentOptionsContainers: { type: Object },
     };
