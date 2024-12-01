5411c28e772786007c98a76a899c8193a42bcce4
Elier Ayala Bernal <elierayalawii@gmail.com>
2024-12-01 11:32:56 -0500

    [FIX] Error: An error occured in the owl lifecycle (see this Error's 'cause' property)

--- addons/bus/static/src/views/list/list_renderer.js
+++ addons/bus/static/src/views/list/list_renderer.js
@@ -1,6 +1,6 @@
-import { patch } from "@web/core/utils/patch";
-import { ListRenderer } from "@web/views/list/list_renderer";
-import { onWillUnmount } from "@odoo/owl";
+import {patch} from "@web/core/utils/patch";
+import {ListRenderer} from "@web/views/list/list_renderer";
+import {onWillUnmount} from "@odoo/owl";
 import {useService} from "@web/core/utils/hooks";
 
 patch(ListRenderer.prototype, {
@@ -9,12 +9,19 @@ patch(ListRenderer.prototype, {
         this.bus = useService('bus_service');
         this.mergedView = `realtime_sync_${this.props.list.resModel}_${this.env.config.viewId}`;
 
-        this.bus.addChannel('NOTIFICATION_FROM_NEW_RECORD');
+        this.bus.addChannel(this.mergedView);
         this.bus.subscribe('NOTIFICATION_FROM_NEW_RECORD', this.wsSyncView.bind(this));
 
         onWillUnmount(() => {
-            this.bus.deleteChannel('NOTIFICATION_FROM_NEW_RECORD');
+            if (this.bus && typeof this.bus.deleteChannel === "function") {
+                this.bus.deleteChannel(this.mergedView);
+            }
+            if (this.bus && typeof this.bus.unsubscribe === "function") {
+                this.bus.unsubscribe('NOTIFICATION_FROM_NEW_RECORD');
+            }
+
         });
+
     },
 
     wsSyncView(args) {
