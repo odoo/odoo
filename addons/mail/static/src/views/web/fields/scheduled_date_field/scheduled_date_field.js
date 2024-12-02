468f83a2d6bdf800416cdf3f0ab7851d3cd20907
Adrien Schoffeniels <adsc@odoo.com>
2024-12-02 17:09:03 +0100

    [IMP] mail: rework message scheduling ui
    Purpose:
    -------
    Rework the message scheduling ui in the composer to simplify the process.
    
    Instead of having to open a dropdown to be able to open the scheduled
    date selection dialog, the scheduled_date field has its own button that
    displays the selected scheduled date if any and opens the scheduled date
    selection dialog when clicked. The dialog now contains a "clear" button
    allowing to remove the scheduled date.
    This should also allow to make it easier for the user to see that the
    message will be scheduled when selecting a template with a scheduled date
    set on it in monocomment mode.
    
    This change is also reflected in the mail_scheduled_message form view.
    This also simplifies the `action_schedule_message` method of the composer
    (removes the need of the scheduled_date parameter as the scheduled date
    will now always be obtained from the record).
    
    Task-4373403

---
+++ addons/mail/static/src/views/web/fields/scheduled_date_field/scheduled_date_field.js
@@ -0,0 +1,85 @@
+import { ScheduledDateDialog } from "./scheduled_date_dialog";
+import { deserializeDateTime, serializeDateTime } from "@web/core/l10n/dates";
+import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
+import { standardFieldProps } from "@web/views/fields/standard_field_props";
+
+import { Component } from "@odoo/owl";
+
+/**
+ * Widget used to display and select the scheduled date in the composer (in monocomment mode)
+ * and in the mail_scheduled_message form view.
+ * There are two different widgets because the composer uses a text field to store the
+ * scheduled date whereas the mail_scheduled_message model uses a datetime field.
+ */
+
+class ScheduledDateFieldCommon extends Component {
+    static props = standardFieldProps;
+    static template = "mail.ScheduledDateField";
+
+    setup() {
+        super.setup();
+        this.dialog = useService("dialog");
+        this.dateTimeFormat = {
+            day: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            month: "short",
+        };
+    }
+
+    onClick(ev) {
+        this.dialog.add(ScheduledDateDialog, {
+            save: (scheduledDate) => this.setScheduledDate(scheduledDate),
+            isRemovable: this.isRemovable,
+            scheduledDate: this.scheduledDate,
+        });
+        // prevents the button to look focused (text-info to look darker) when closing the dialog
+        ev.currentTarget.blur();
+    }
+}
+
+class MailComposerScheduledDateField extends ScheduledDateFieldCommon {
+    setup() {
+        super.setup();
+        this.isRemovable = true;
+    }
+
+    get scheduledDate() {
+        const serializedDatetime = this.props.record.data[this.props.name];
+        return Boolean(serializedDatetime) && deserializeDateTime(serializedDatetime);
+    }
+
+    setScheduledDate(scheduledDate) {
+        this.props.record.update({
+            scheduled_date: scheduledDate ? serializeDateTime(scheduledDate) : "",
+        });
+    }
+}
+
+const mailComposerScheduledDateField = {
+    component: MailComposerScheduledDateField,
+};
+registry.category("fields").add("mail_composer_scheduled_date", mailComposerScheduledDateField);
+
+class MailScheduledMessageScheduledDateField extends ScheduledDateFieldCommon {
+    setup() {
+        super.setup();
+        this.isRemovable = false;
+    }
+
+    get scheduledDate() {
+        return this.props.record.data[this.props.name];
+    }
+
+    setScheduledDate(scheduledDate) {
+        this.props.record.update({ scheduled_date: scheduledDate });
+    }
+}
+
+const mailScheduledMessageScheduledDateField = {
+    component: MailScheduledMessageScheduledDateField,
+};
+registry
+    .category("fields")
+    .add("mail_scheduled_message_scheduled_date", mailScheduledMessageScheduledDateField);
