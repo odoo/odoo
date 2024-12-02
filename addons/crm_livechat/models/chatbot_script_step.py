8d9aff259ea44a675dff7cb25f7e64c411124474
ramh <ramh@odoo.com>
2024-12-02 16:57:27 +0530

    [IMP] crm,*: add new chatbot script step to create lead and forward
    * = im_livechat, crm_livechat
    
    This commit introduces a new chatbot script step 'create lead & forward'.
    
    On selection, the chatbot will:
    1. Create a new lead.
    2. Assign the lead and forward the conversation to a salesperson based on the
        assignment rules configured for the selected sales team, provided a salesperson
        is available.
    3. If no salesperson is available, the conversation will be forwarded
        to an livechat operator.

--- addons/crm_livechat/models/chatbot_script_step.py
+++ addons/crm_livechat/models/chatbot_script_step.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from ast import literal_eval
+
 from odoo import _, models, fields
+from odoo.tools import plaintext2html
 
 
 class ChatbotScriptStep(models.Model):
     _inherit = 'chatbot.script.step'
 
     step_type = fields.Selection(
-        selection_add=[('create_lead', 'Create Lead')], ondelete={'create_lead': 'cascade'})
+        selection_add=[
+            ("create_lead", "Create Lead"),
+            ("create_lead_and_forward", "Create Lead & Forward")
+        ], ondelete={"create_lead": "cascade", "create_lead_and_forward": "cascade"})
     crm_team_id = fields.Many2one(
         'crm.team', string='Sales Team', ondelete='set null',
         help="Used in combination with 'create_lead' step type in order to automatically "
@@ -31,6 +37,8 @@ class ChatbotScriptStep(models.Model):
 
         if self.step_type == 'create_lead':
             self._process_step_create_lead(discuss_channel)
+        elif self.step_type == "create_lead_and_forward":
+            return self._process_step_create_lead_and_forward(discuss_channel, posted_message)
 
         return posted_message
 
@@ -62,4 +70,39 @@ class ChatbotScriptStep(models.Model):
         create_values.update(self._chatbot_crm_prepare_lead_values(
             discuss_channel, customer_values['description']))
 
-        self.env['crm.lead'].create(create_values)
+        return self.env['crm.lead'].create(create_values)
+
+    def _process_step_create_lead_and_forward(self, discuss_channel, posted_message):
+        lead = self._process_step_create_lead(discuss_channel)
+        team = self.crm_team_id
+        domain = literal_eval(team.assignment_domain or "[]")
+        lead = lead.filtered_domain(domain)
+        if team and lead:
+            member_to_assign = next(
+                (
+                    member for member in team.crm_team_member_ids
+                    if not member.assignment_optout
+                    and member._get_assignment_quota() > 0
+                ),
+                None
+            )
+            if member_to_assign:
+                lead.user_id = member_to_assign.user_id
+                if member_to_assign.user_id._is_user_available():
+                    user = member_to_assign.user_id
+                    discuss_channel.with_user(user).sudo().add_members(user.partner_id.ids, open_chat_window=True)
+                    # sudo - discuss.channel: let the chat bot proceed to the forward step (change channel operator, add human operator
+                    # as member, remove bot from channel, rename channel and finally broadcast the channel to the new operator).
+                    channel_sudo = discuss_channel.sudo()
+                    channel_sudo.livechat_operator_id = user.partner_id
+                    if bot_member := channel_sudo.channel_member_ids.filtered(
+                        lambda m: m.partner_id == self.chatbot_script_id.operator_partner_id
+                    ):
+                        channel_sudo._action_unfollow(partner=bot_member.partner_id)
+                    channel_sudo.name = user.livechat_username or user.name
+                    channel_sudo._broadcast(user.partner_id.ids)
+                    return posted_message
+        return self._process_step_forward_operator(discuss_channel, posted_message)
+
+    def _get_operator_step_types(self):
+        return super()._get_operator_step_types() + ["create_lead_and_forward"]
--- addons/im_livechat/models/mail_message.py
+++ addons/im_livechat/models/mail_message.py
@@ -51,7 +51,7 @@ class MailMessage(models.Model):
                         "id": (step.id, channel.id),
                         "message": message.id,
                         "scriptStep": step.id,
-                        "operatorFound": step.step_type == "forward_operator"
+                        "operatorFound": step.step_type in step._get_operator_step_types()
                         and channel.livechat_operator_id != chatbot
                     }
                     if answer := chatbot_message.user_script_answer_id:
