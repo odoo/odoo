90ead04f3f9bf5846f119ac582955bef10a20000
tsm-odoo <tsm@odoo.com>
2024-12-02 15:32:11 +0100

    [IMP] bus: convert unit tests to hoot
    closes odoo/odoo#189306
    
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

---
+++ addons/bus/static/tests/websocket_worker.test.js
@@ -0,0 +1,74 @@
+import { patchWebsocketWorkerWithCleanup } from "@bus/../tests/mock_websocket";
+import { WEBSOCKET_CLOSE_CODES } from "@bus/workers/websocket_worker";
+import { describe, expect, test } from "@odoo/hoot";
+import { asyncStep, waitForSteps } from "@web/../tests/web_test_helpers";
+
+describe.current.tags("desktop");
+
+test("connect event is broadcasted after calling start", async () => {
+    const worker = patchWebsocketWorkerWithCleanup({
+        broadcast(type) {
+            if (type != "update_state") {
+                asyncStep(`broadcast ${type}`);
+            }
+        },
+    });
+    worker._start();
+    await waitForSteps(["broadcast connect"]);
+});
+
+test("disconnect event is broadcasted", async () => {
+    const worker = patchWebsocketWorkerWithCleanup({
+        broadcast(type) {
+            if (type != "update_state") {
+                asyncStep(`broadcast ${type}`);
+            }
+        },
+    });
+    worker._start();
+    await waitForSteps(["broadcast connect"]);
+    worker.websocket.close(WEBSOCKET_CLOSE_CODES.CLEAN);
+    await waitForSteps(["broadcast disconnect"]);
+});
+
+test("reconnecting/reconnect event is broadcasted", async () => {
+    const worker = patchWebsocketWorkerWithCleanup({
+        broadcast(type) {
+            if (type != "update_state") {
+                asyncStep(`broadcast ${type}`);
+            }
+        },
+    });
+    worker._start();
+    await waitForSteps(["broadcast connect"]);
+    worker.websocket.close(WEBSOCKET_CLOSE_CODES.ABNORMAL_CLOSURE);
+    await waitForSteps(["broadcast disconnect", "broadcast reconnecting", "broadcast reconnect"]);
+});
+
+test("notification event is broadcasted", async () => {
+    const notifications = [
+        {
+            id: 70,
+            message: {
+                type: "bundle_changed",
+                payload: {
+                    server_version: "15.5alpha1+e",
+                },
+            },
+        },
+    ];
+    const worker = patchWebsocketWorkerWithCleanup({
+        broadcast(type, message) {
+            if (type === "notification") {
+                expect(message).toEqual(notifications);
+            }
+            if (["connect", "notification"].includes(type)) {
+                asyncStep(`broadcast ${type}`);
+            }
+        },
+    });
+    worker._start();
+    await waitForSteps(["broadcast connect"]);
+    worker.websocket.simulateIncomingMessage(JSON.stringify(notifications));
+    await waitForSteps(["broadcast notification"]);
+});
