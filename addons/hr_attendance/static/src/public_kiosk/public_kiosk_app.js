045e8c72100bc889c931f050710b19ab6dd6373c
thcl-odoo <thcl@odoo.com>
2024-12-03 09:06:15 +0000

    [FIX] hr_attendance: correctly declare props
    Before this commit, when we were in debug mode, we couldn't access Kiosk mode (white screen with message in console). This was because the props were not fully declared, and, in debug mode, we validated the props here : https://github.com/odoo/odoo/blob/c3e7ee585a58fc07c11d07677d1d64dbd357d6b7/addons/web/static/lib/owl/owl.js#L5715-L5717
    As the components are only used in one place each, I've updated the props declaration to match the usage.
    
    closes odoo/odoo#197050
    
    X-original-commit: ce5ad5219994626e328798696eb1d275c9e48fab
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>
    Signed-off-by: Thibault Claude (thcl) <thcl@odoo.com>

--- addons/hr_attendance/static/src/public_kiosk/public_kiosk_app.js
+++ addons/hr_attendance/static/src/public_kiosk/public_kiosk_app.js
@@ -18,7 +18,15 @@ import { session } from "@web/session";
 
 class kioskAttendanceApp extends Component{
     static template = "hr_attendance.public_kiosk_app";
-    static props = [];
+    static props = {
+        token: { type: String },
+        companyId: { type: Number },
+        companyName: { type: String },
+        employees: { type: Array },
+        departments: { type: Array },
+        kioskMode: { type: String },
+        barcodeSource: { type: String },
+    };
     static components = {
         KioskBarcodeScanner,
         CardLayout,
