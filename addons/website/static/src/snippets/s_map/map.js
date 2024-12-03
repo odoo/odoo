4bb4aa6f3e3664a949b044b0fc3d1641669072d4
Benoit Socias <bso@odoo.com>
2024-12-03 11:27:48 +0100

    rename 000.js: map

--- addons/website/static/src/snippets/s_map/map.js
+++ addons/website/static/src/snippets/s_map/map.js
@@ -1,13 +1,13 @@
 import { Interaction } from "@website/core/interaction";
 import { registry } from "@web/core/registry";
 
-import { generateGMapLink, generateGMapIframe } from '@website/js/utils';
+import { generateGMapLink, generateGMapIframe } from "@website/js/utils";
 
 export class Map extends Interaction {
     static selector = ".s_map";
 
     start() {
-        if (!this.el.querySelector('.s_map_embedded')) {
+        if (!this.el.querySelector(".s_map_embedded")) {
             // The iframe is not found inside the snippet. This is probably due
             // to the sanitization of a field during the save, like in a product
             // description field.
@@ -15,7 +15,7 @@ export class Map extends Interaction {
             const dataset = this.el.dataset;
             if (dataset.mapAddress) {
                 const iframeEl = generateGMapIframe();
-                this.el.querySelector('.s_map_color_filter').before(iframeEl);
+                this.el.querySelector(".s_map_color_filter").before(iframeEl);
                 this.services.website_cookies.manageIframeSrc(iframeEl, generateGMapLink(dataset));
             }
         }
