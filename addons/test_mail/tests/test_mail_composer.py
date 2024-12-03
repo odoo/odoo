fbad5afff588c852db254dca2a1f87e46aaf4f39
Adrien Schoffeniels <adsc@odoo.com>
2024-12-03 13:58:43 +0100

    [IMP] mail: post scheduled messages with the original context
    Purpose:
    --------
    Some actions might be performed based on context keys when posting a
    message (eg. subscribe recipients with the `mail_post_autofollow` key or
    change a sale order's state with the `mark_so_as_sent` key).
    
    These context keys are currently lost when scheduling a message, and the
    related actions are thus not triggered.
    
    This commit adds a field `send_context` on the `mail_scheduled_message`
    model that will store the context used when scheduling the message.
    This context will then be reused when the scheduled messages are posted,
    resulting in the actions to be triggered. For example in sales, a sale
    order will move to "quotation sent" when a message scheduled using the
    "send by email" button is sent.
    
    Task-4373403
    
    Part-of: odoo/odoo#189355
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/test_mail/tests/test_mail_composer.py
+++ addons/test_mail/tests/test_mail_composer.py
@@ -1260,7 +1260,7 @@ class TestComposerInternals(TestMailComposer):
         # schedule a message in mono-comment mode
         attachment_data = self._generate_attachments_data(1, 'mail.compose.message', 0)[0]
         composer = self.env['mail.compose.message'].with_context(
-            self._get_web_context(self.test_record)
+            self._get_web_context(self.test_record, False, additional_ctx_key=True)
             ).create({
                 'body': '<p>Test Body</p>',
                 'subject': 'Test Subject',
@@ -1289,6 +1289,7 @@ class TestComposerInternals(TestMailComposer):
         self.assertEqual(scheduled_message.author_id, self.partner_employee)
         self.assertEqual(scheduled_message.partner_ids, self.test_record.customer_id)
         self.assertFalse(scheduled_message.is_note)
+        self.assertEqual(scheduled_message.send_context, {'additional_ctx_key': True})
         # attachment transfer
         self.assertEqual(composer_attachment.res_model, scheduled_message._name)
         self.assertEqual(composer_attachment.res_id, scheduled_message.id)
