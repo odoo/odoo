f576435f6b045f5fa3c1ce96b51ff35efbc657bf
parp-odoo <parp@odoo.com>
2024-12-03 12:49:09 +0530

    [IMP] pos*: introduce sale warnings when selecting a customer or product
    pos*: pos, pos_sale
    
    In this commit:
    =========
    Introduce a sale warning feature when adding a customer or product
    to a current order (similar to sales).
    
    The available sale warnings are:-
    - No Message (default): No warning.
    - Warning: A warning popup is shown, but the customer or product is still added.
    - Blocking Message: A warning popup is shown, and the customer or product
      is not added.
    
    Task - 4375272

--- addons/point_of_sale/static/src/app/services/pos_store.js
+++ addons/point_of_sale/static/src/app/services/pos_store.js
@@ -1706,6 +1706,10 @@ export class PosStore extends WithLazyGetterTrap {
             preset.computeAvailabilities();
         }
     }
+    // There for override to do something before adding partner to current order from partner list
+    setPartnerToCurrentOrder(partner) {
+        this.getOrder().setPartner(partner);
+    }
     async selectPartner() {
         // FIXME, find order to refund when we are in the ticketscreen.
         const currentOrder = this.getOrder();
@@ -1727,11 +1731,7 @@ export class PosStore extends WithLazyGetterTrap {
             partner: currentPartner,
         });
 
-        if (payload) {
-            currentOrder.setPartner(payload);
-        } else {
-            currentOrder.setPartner(false);
-        }
+        this.setPartnerToCurrentOrder(payload || false);
 
         return payload;
     }
