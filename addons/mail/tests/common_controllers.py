491e4f50a42cdbb650ebfe53934a47055eccb949
Maryam Kia <maki@odoo.com>
2024-12-04 16:41:53 +0100

    [IMP] mail, im_livechat: updated message params as a dict
    `/mail/message/update_content` will be used in more use cases in the portal
    (popup rating composer). To do this, we need to pass more update params to the
    route (updated `rating_value`). This is a preparation PR to make passing update
    params simpler. This way, the updated data that should be handled in
    `_message_update_content` is sent as `update_data` dict so it's possible to
    manipulate it when it's needed. Non-update params such as security params are
    sent in `kwargs` as before.

--- addons/mail/tests/common_controllers.py
+++ addons/mail/tests/common_controllers.py
@@ -334,8 +334,7 @@ class MailControllerUpdateCommon(MailControllerCommon):
             route="/mail/message/update_content",
             params={
                 "message_id": message_id,
-                "body": body,
-                "attachment_ids": [],
+                "update_data": {"attachment_ids": [], "body": body},
                 **route_kw,
             },
         )
