dae907ba769f403517a14a87b31fbe0a4dfe071e
Nesma Harraz <neha@odoo.com>
2024-12-04 16:37:22 +0200

    [IMP] l10n_mt_pos: Introduce compliance letter module for EXO number requests - Created a new module `l10n_mt_pos` to provide a compliance letter required by Maltese businesses for requesting an EXO number. - Automatically installs for Malta by default. - Added a menu item under "Reporting" > "Compliance Letter" to allow users to download the document.
    Task ID: 4373339

---
+++ addons/l10n_mt_pos/__manifest__.py
@@ -0,0 +1,17 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    "name": "Malta - Point of Sale",
+    "version": "1.0",
+    "category": "Accounting/Localizations/Point of Sale",
+    "description": """Malta Compliance Letter for EXO Number""",
+    "depends": [
+        "point_of_sale",
+    ],
+    "data": [
+        'security/ir.model.access.csv',
+        'wizards/compliance_letter_view.xml',
+        'reports/compliance_letter_report.xml',
+    ],
+    "auto_install": True,
+    "license": "LGPL-3",
+}
