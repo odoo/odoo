3ea08831e9aa3e2c6088d02f159efb28152f3bfe
Pedram (pebr) <pebr@odoo.com>
2024-12-04 15:20:59 +0000

    [FIX] point_of_sale: prevent error when loading PoS in other currency
    Before this commit, it wasn't possible to open a PoS in a different
    currency than the company currency.
    
    opw-4378523
    
    X-original-commit: b957dcd964340aca19ca698acfb8a5f021d98983

--- addons/point_of_sale/models/res_currency.py
+++ addons/point_of_sale/models/res_currency.py
@@ -7,7 +7,16 @@ class ResCurrency(models.Model):
 
     @api.model
     def _load_pos_data_domain(self, data):
+<<<<<<< master
         return [('id', '=', data['pos.config'][0]['currency_id'])]
+||||||| c6857d98be168160c067942e99b63b1634c3ffb7
+        return [('id', '=', data['pos.config']['data'][0]['currency_id'])]
+=======
+        company_currency_id = self.env['res.company'].browse(data['pos.config']['data'][0]['company_id']).currency_id.id
+        if company_currency_id != data['pos.config']['data'][0]['currency_id']:
+            return [('id', 'in', [company_currency_id, data['pos.config']['data'][0]['currency_id']])]
+        return [('id', '=', data['pos.config']['data'][0]['currency_id'])]
+>>>>>>> be349795fe8696886a1c5bdf13153b2a65281e22
 
     @api.model
     def _load_pos_data_fields(self, config_id):
