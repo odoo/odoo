0323fa16a3f7a5d6b0e283d42b9543d5887b6a8a
Julien Alardot (jual) <jual@odoo.com>
2024-12-05 11:10:33 +0100

    [IMP] hr_expense: Bye Bye reports
    As the expense report would add extra steps when
    creating and submitting expenses, a simplification was decided
    and the model hr.expense.sheet is removed, giving all of its remaining
    important logic to the hr.expense
    
    task-id: 4370689

--- addons/hr_expense/static/src/views/list.js
+++ addons/hr_expense/static/src/views/list.js
@@ -18,7 +18,6 @@ export class ExpenseListController extends ExpenseDocumentUpload(ListController)
         super.setup();
         this.orm = useService('orm');
         this.actionService = useService('action');
-        this.isExpenseSheet = this.model.config.resModel === "hr.expense.sheet";
 
         onWillStart(async () => {
             this.userIsExpenseTeamApprover = await user.hasGroup("hr_expense.group_hr_expense_team_approver");
@@ -28,22 +27,22 @@ export class ExpenseListController extends ExpenseDocumentUpload(ListController)
 
     displaySubmit() {
         const records = this.model.root.selection;
-        return records.length && records.every(record => record.data.state === 'draft') && this.isExpenseSheet;
+        return records.length && records.every(record => record.data.state === 'draft');
     }
 
     displayApprove() {
         const records = this.model.root.selection;
-        return this.userIsExpenseTeamApprover && records.length && records.every(record => record.data.state === 'submit') && this.isExpenseSheet;
+        return this.userIsExpenseTeamApprover && records.length && records.every(record => record.data.state === 'submit');
     }
 
     displayPost() {
         const records = this.model.root.selection;
-        return this.userIsAccountInvoicing && records.length && records.every(record => record.data.state === 'approve') && this.isExpenseSheet;
+        return this.userIsAccountInvoicing && records.length && records.every(record => record.data.state === 'approve');
     }
 
     displayPayment() {
         const records = this.model.root.selection;
-        return this.userIsAccountInvoicing && records.length && records.every(record => record.data.state === 'post' && record.data.payment_state === 'not_paid') && this.isExpenseSheet;
+        return this.userIsAccountInvoicing && records.length && records.every(record => record.data.state === 'post' && record.data.payment_state === 'not_paid');
     }
 
     async onClick (action) {
@@ -51,7 +50,7 @@ export class ExpenseListController extends ExpenseDocumentUpload(ListController)
         const recordIds = records.map((a) => a.resId);
         const model = this.model.config.resModel;
         const context = {};
-        if (action === 'action_approve_expense_sheets') {
+        if (action === 'action_approve') {
             context['validate_analytic'] = true;
         }
         const res = await this.orm.call(model, action, [recordIds], {context: context});
--- addons/hr_expense/static/tests/tours/expense_form_in_sheet_tours.js
+++ addons/hr_expense/static/tests/tours/expense_form_in_sheet_tours.js
@@ -1,18 +1,18 @@
 import { registry } from "@web/core/registry";
 import { stepUtils } from "@web_tour/tour_service/tour_utils";
 
-registry.category("web_tour.tours").add("do_not_create_zero_amount_expense_in_sheet", {
+registry.category("web_tour.tours").add("do_not_create_zero_amount_expense", {
     url: "/odoo",
     steps: () => [
         ...stepUtils.goToAppSteps("hr_expense.menu_hr_expense_root", "Go to the Expenses app"),
         {
-            content: "Go to Expense Reports",
-            trigger: '.dropdown-item[data-menu-xmlid="hr_expense.menu_hr_expense_report"]',
+            content: "Go to Expense",
+            trigger: '.dropdown-item[data-menu-xmlid="hr_expense.menu_hr_expense_"]',
             run: "click",
         },
         {
             content: "Go to a report",
-            trigger: '.o_data_row .o_data_cell[data-tooltip="report_for_tour"]',
+            trigger: '.o_data_row .o_data_cell[data-tooltip="expense_for_tour"]',
             run: "click",
         },
         {
