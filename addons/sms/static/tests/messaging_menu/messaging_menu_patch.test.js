e86fa2d220b2fd7c45216d9189c50e887c6f2dc5
Alexandre Kühn <aku@odoo.com>
2024-12-05 18:00:13 +0100

    [IMP] mail: clarity of PWA and push notif items in messaging menu
    - Renamed "OdooBot has a suggestion" to "Install Odoo"
    - Renamed "OdooBot has a request" to "Turn on notifications"
    - Renamed failure name, e.g. "Contacts" to "Failure: Contact"
    - Type of failure is in prefix, e.g. "Email Failure: Contact"
    - When failure is related to a single record, it names the
      record name in notification body.
    - Make chat notification take max 1 line for name (same as before),
      text message preview can take up to 2 lines (up from 1).
    - All other notifications (failures, odoobot suggestions) does not
    truncate in title and body.
    
    Motivation:
    
    Before this commit, the items in messaging menu to suggest to
    install Odoo PWA and Enabling chat push notifications looked too
    similar and had cryptic header name:
    
    - "OdooBot has a suggestion" for PWA install
    - "OdooBot has a request" for chat push notification enabling
    
    This commit improves the wording of title as follow:
    - "Install Odoo"
    - "Turn on notifications"
    
    Also notification items had their title and body truncated on
    a single line each. This is intended to keep chat items short,
    but for other notifications there's not really a reason to truncate
    the content. This commit also adds a feature on `NotificationItem`
    to specify the max amount of lines for title and body of a given
    notification. By default there's none.
    
    Chat notifications had title and body max line set to 1. This commit
    keeps max line 1 for name, but increases to 2 for text. This means
    the items take slightly more space but more text preview of message
    is visible in messaging menu item.
    
    closes odoo/odoo#189805
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/sms/static/tests/messaging_menu/messaging_menu_patch.test.js
+++ addons/sms/static/tests/messaging_menu/messaging_menu_patch.test.js
@@ -3,7 +3,7 @@ import {
     contains,
     start,
     startServer,
-    triggerEvents
+    triggerEvents,
 } from "@mail/../tests/mail_test_helpers";
 import { describe, expect, test } from "@odoo/hoot";
 import { defineSMSModels } from "@sms/../tests/sms_test_helpers";
@@ -30,7 +30,7 @@ test("mark as read", async () => {
     await triggerEvents(".o-mail-NotificationItem", ["mouseenter"], { text: "" });
     await contains(".o-mail-NotificationItem [title='Mark As Read']");
     await contains(".o-mail-NotificationItem-text", {
-        text: "An error occurred when sending an SMS",
+        text: "An error occurred when sending an SMS on “Mitchell Admin”",
     });
     await click(".o-mail-NotificationItem [title='Mark As Read']");
     await contains(".o-mail-NotificationItem", { count: 0 });
@@ -78,14 +78,14 @@ test("notifications grouped by notification_type", async () => {
     await contains(".o-mail-NotificationItem", { count: 2 });
     await contains(":nth-child(1 of .o-mail-NotificationItem)", {
         contains: [
-            [".o-mail-NotificationItem-name", { text: "Contact" }],
+            [".o-mail-NotificationItem-name", { text: "Email Failure: Contact" }],
             [".o-mail-NotificationItem-counter", { text: "2" }],
             [".o-mail-NotificationItem-text", { text: "An error occurred when sending an email" }],
         ],
     });
     await contains(":nth-child(2 of .o-mail-NotificationItem)", {
         contains: [
-            [".o-mail-NotificationItem-name", { text: "Contact" }],
+            [".o-mail-NotificationItem-name", { text: "SMS Failure: Contact" }],
             [".o-mail-NotificationItem-counter", { text: "2" }],
             [".o-mail-NotificationItem-text", { text: "An error occurred when sending an SMS" }],
         ],
@@ -94,7 +94,7 @@ test("notifications grouped by notification_type", async () => {
 
 test("grouped notifications by document model", async () => {
     const pyEnv = await startServer();
-    const [partnerId_1, partnerId_2 ]= pyEnv["res.partner"].create([{}, {}]);
+    const [partnerId_1, partnerId_2] = pyEnv["res.partner"].create([{}, {}]);
     const [messageId_1, messageId_2] = pyEnv["mail.message"].create([
         {
             message_type: "sms",
@@ -125,7 +125,11 @@ test("grouped notifications by document model", async () => {
             expect(action.name).toBe("SMS Failures");
             expect(action.type).toBe("ir.actions.act_window");
             expect(action.view_mode).toBe("kanban,list,form");
-            expect(action.views).toEqual([[false, "kanban"],[false, "list"],[false, "form"]]);
+            expect(action.views).toEqual([
+                [false, "kanban"],
+                [false, "list"],
+                [false, "form"],
+            ]);
             expect(action.target).toBe("current");
             expect(action.res_model).toBe("res.partner");
             expect(action.domain).toEqual([["message_has_sms_error", "=", true]]);
@@ -134,7 +138,7 @@ test("grouped notifications by document model", async () => {
     await start();
     await click(".o_menu_systray i[aria-label='Messages']");
     await click(".o-mail-NotificationItem", {
-        text: "Contact",
+        text: "SMS Failure: Contact",
         contains: [".badge", { text: "2" }],
     });
     await waitForSteps(["do_action"]);
