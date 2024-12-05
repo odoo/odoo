6c99b44e517cb7d86aced646b1ba4a4044727ee6
jorv-odoo <jorv@odoo.com>
2024-12-05 07:22:13 +0000

    [FIX] rating : remove one click rating behavior
    Context:
    In its current form, the HTTP route using `/rate/<string:token>/<int:rate>`,
    that is used in URL links generated by emails asking for customer feedback,
    will have a "one-click write" behavior that commits the rating encoded
    in `<int:rate>`. So the record is already updated with the new value.
    Only then renders the feedback submit Form, where the user
    can change their preselected rating and leave a commentary before submitting
    it via a POST request.
    
    Over the last years email providers and third party services will
    preemptively crawl URL in emails to verify if they are "dangerous" (e.g.
    Microsoft's Safe link and Defender features).
    By doing so, the rating for the records will be set to the last clicked
    URL using the above HTTP route, without any input from a human user.
    
    It will be creating a number of false positive or negative ratings that do not
    represent the real satisfaction of the surveyed customer.
    
    Proposed solution:
    We are removing the implicit "one-click write" behavior of the route and
    favoring the Form submission expecting human input.
    This will increase the likelihood a given rating was the result of the customer
    clicking on the link and taking the time to leave a feedback, versus
    random inputs from URL bots.
    
    Added adapted test to account for new expected scenario
    
    Future outlook:
    With the ever-changing landscape of URL crawler bots, trying to develop
    counter measures to identify and flag bot behavior is possible, but would
    add immense overhead and maintenance cost of the code for an arguably minor
    feature, i.e. the customer can rate the ticket by just clicking the picture
    in the email.
    Other methods to make these routes more resilient to URL crawlers might
    be revisited in the future, but for the short-term this seems like a sufficient
    solution to the unwanted behavior.
    
    FW fixes:
    - updated test to match new sub test approach
    - adapted parsing of Form elements in the HTML response to account for
    more than one Form element present (there might be /website/search ones)
    - adapted test test_controller_rating for new expected behavior
    
    OPW-3411799
    OPW-4292166
    Task-4353243
    
    closes odoo/odoo#190542
    
    X-original-commit: 4d2f3e80e97f07d8d7731872dd6bf4fde3d23761
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>
    Signed-off-by: Joel Rodrigues Vitória (jorv) <jorv@odoo.com>
    Co-authored-by: Julien Castiaux <juc@odoo.com>
    Co-authored-by: Thibault Delavallée <tde-banana-odoo@users.noreply.github.com>

--- addons/test_mail_full/tests/test_rating.py
+++ addons/test_mail_full/tests/test_rating.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import lxml
 from datetime import datetime
 
+from odoo import http
 from odoo.addons.test_mail_full.tests.common import TestMailFullCommon
 from odoo.addons.test_mail_sms.tests.common import TestSMSRecipients
 from odoo.tests import tagged
@@ -214,14 +216,80 @@ class TestRatingRoutes(HttpCase, TestRatingCommon):
         for record_rating, is_rating_mixin_test in ((self.record_rating_thread, False),
                                                     (self.record_rating, True)):
             with self.subTest('With rating mixin' if is_rating_mixin_test else 'Without rating mixin'):
+                """
+                16.0 + expected behavior
+                1) Clicking on the smiley image triggers the /rate/<string:token>/<int:rate>
+                route should not update the rating of the record but simply redirect
+                to the feedback form
+                2) Customer interacts with webpage and submits FORM. Triggers /rate/<string:token>/submit_feedback
+                route. Should update the rating of the record with the data in the POST request
+                """
+                self.authenticate(None, None)  # set up session for public user
                 access_token = record_rating._rating_get_access_token()
-                self.url_open(f"/rate/{access_token}/5")
 
+                # First round of clicking the URL and then submitting FORM data
+                response_click_one = self.url_open(f"/rate/{access_token}/5")
+                response_click_one.raise_for_status()
+
+                # there should be a form to post to validate the feedback and avoid one-click anyway
+                forms = lxml.html.fromstring(response_click_one.content).xpath('//form')
+                matching_rate_form = next((form for form in forms if form.get("action", "").startswith("/rate")), None)
+                self.assertEqual(matching_rate_form.get('method'), 'post')
+                self.assertEqual(matching_rate_form.get('action', ''), f'/rate/{access_token}/submit_feedback')
+
+                # rating should not change, i.e. default values
                 rating = record_rating.rating_ids
-                self.assertTrue(rating.consumed)
-                self.assertEqual(rating.rating, 5)
+                self.assertFalse(rating.consumed)
+                self.assertEqual(rating.rating, 0)
+                self.assertFalse(rating.feedback)
+                if is_rating_mixin_test:
+                    self.assertEqual(record_rating.rating_last_value, 0)
+
+                response_submit_one = self.url_open(
+                    f"/rate/{access_token}/submit_feedback",
+                    data={
+                        "rate": 5,
+                        "csrf_token": http.Request.csrf_token(self),
+                        "feedback": "good",
+                    }
+                )
+                response_submit_one.raise_for_status()
+
+                rating_post_submit_one = record_rating.rating_ids
+                self.assertTrue(rating_post_submit_one.consumed)
+                self.assertEqual(rating_post_submit_one.rating, 5)
+                self.assertEqual(rating_post_submit_one.feedback, "good")
                 if is_rating_mixin_test:
                     self.assertEqual(record_rating.rating_last_value, 5)
+
+                # Second round of clicking the URL and then submitting FORM data
+                response_click_two = self.url_open(f"/rate/{access_token}/1")
+                response_click_two.raise_for_status()
+                if is_rating_mixin_test:
+                    self.assertEqual(record_rating.rating_last_value, 5)  # should not be updated to 1
+
+                # check returned form
+                forms = lxml.html.fromstring(response_click_two.content).xpath('//form')
+                matching_rate_form = next((form for form in forms if form.get("action", "").startswith("/rate")), None)
+                self.assertEqual(matching_rate_form.get('method'), 'post')
+                self.assertEqual(matching_rate_form.get('action', ''), f'/rate/{access_token}/submit_feedback')
+
+                response_submit_two = self.url_open(
+                    f"/rate/{access_token}/submit_feedback",
+                    data={
+                        "rate": 1,
+                        "csrf_token": http.Request.csrf_token(self),
+                        "feedback": "bad job"
+                    }
+                )
+                response_submit_two.raise_for_status()
+
+                rating_post_submit_second = record_rating.rating_ids
+                self.assertTrue(rating_post_submit_second.consumed)
+                self.assertEqual(rating_post_submit_second.rating, 1)
+                self.assertEqual(rating_post_submit_second.feedback, "bad job")
+                if is_rating_mixin_test:
+                    self.assertEqual(record_rating.rating_last_value, 1)
 
     def test_portal_user_can_post_message_with_rating(self):
         """Test portal user can post a message with a rating on a thread with
