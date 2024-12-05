720e965d0ebad9988e324ba5775cdf5ce4922efd
Sarah Bellefroid <sbel@odoo.com>
2024-12-05 15:15:33 +0000

    [FIX] l10n_be_pos_sale: activate default invoice only with intracom taxes:
    Previously, settling a sale.order from the POS of a Belgian company
    would auto check the invoice option before paying.
    
    Steps to reproduce:
    -------------------
    * Install `l10n_be_pos_cert`
    * Switch to the Beligian company
    * Crete a quotation in **Sale** app
    * Add any partner and any product, remove all taxes for the product line
    * Save
    * Open pos shop
    * Settle the order
    * Go to the payment screen
    > Observation: The invoice button is already selected.
    
    Why the fix:
    ------------
    This invoice option was auto selected since this commit: c760fbb
    
    The following commit 3a5e222
    updates the previous one as the invoice enforcement is only needed when we have
    intracom taxes on the SO. Thus when no intracom taxes apply we can still
    decide wether or not to invoice. We fallback on the default behavior which
    is to have the invoice button un-selected.
    
    opw-4334095
    
    X-original-commit: 3aebaa45df6b19cb8ebda1c492306561198747e5

--- addons/l10n_be_pos_sale/static/src/js/models.js
+++ addons/l10n_be_pos_sale/static/src/js/models.js
@@ -6,11 +6,16 @@ import { PosStore } from "@point_of_sale/app/store/pos_store";
 
 patch(Order.prototype, {
     async pay() {
-        const has_origin_order = this.get_orderlines().some((line) => line.sale_order_origin_id);
+        const orderLines = this.get_orderlines();
+        const has_origin_order = orderLines.some((line) => line.sale_order_origin_id);
+        const has_intracom_taxes = orderLines.some((line) =>
+            line.tax_ids?.some((tax) => this.pos.intracom_tax_ids?.includes(tax.id))
+        );
         if (
             this.pos.company.country_id &&
             this.pos.company.country_id.code === "BE" &&
-            has_origin_order
+            has_origin_order &&
+            has_intracom_taxes
         ) {
             this.to_invoice = true;
         }
