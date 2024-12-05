b214c337f2e475c57fded829e989e678656338a7
Mathieu Walravens <wama@odoo.com>
2024-12-05 12:29:31 +0100

    [FIX] models: incorrect string unquote
    Before this commit:
    Calling `_check_company_domain` with a string will always transform it
    to an unquoted string, which is not alwaysintended, and result in syntax
    erorrs when executing SQL queries.
    
    After this commit:
    Do not unquote regular string and keep unquoted string as unquoted.
    
    opw-todo

--- odoo/orm/models.py
+++ odoo/orm/models.py
@@ -4085,7 +4085,7 @@ class BaseModel(metaclass=MetaModel):
         """
         if not companies:
             return [('company_id', '=', False)]
-        if isinstance(companies, str):
+        if isinstance(companies, unquote):
             return [('company_id', 'in', unquote(f'{companies} + [False]'))]
         return [('company_id', 'in', to_record_ids(companies) + [False])]
 
