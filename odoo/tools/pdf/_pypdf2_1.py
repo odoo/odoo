5336c3ec5fb8a405985bd233d50064524fd15bf6
dane@odoo.com <dane@odoo.com>
2024-12-05 08:26:50 +0000

    [FIX] tools: prevent warning override behaviour
    current behaviour:
    when buggy server action is introduced, we don't handle it well, eg:
    ```
    name = record.name
    if name != record.partner_id:
        raise UserError("Datatype")
    ```
    here, comparing `name` and `record.partner_id` doesn't make sense,
    therefore our ORM default behaviour is to return false and rise
    `AttributeError`, but the problem occurs when pypdf2 attempts to format
    the warning message, which rises an error which isn't being handled:
    ```
      File "/home/odoo/.local/lib/python3.10/site-packages/PyPDF2/pdf.py", line 1069, in _showwarning
        file.write(formatWarning(message, category, filename, lineno, line))
      File "/home/odoo/.local/lib/python3.10/site-packages/PyPDF2/utils.py", line 69, in formatWarning
        file = filename.replace("/", "\\").rsplit("\\", 1)[1] # find the file name
    IndexError: list index out of range
    ```
    it is problematic since it is server action.
    
    expected behaviour:
    Even when buggy code is introduced, comparision should return False and
    error should be handled well.
    
    steps to reproduce:
    1) Create new database from scratch and create server action with the
    buggy code above, set model to `sale.orde` and add it to
    `contextual action`
    2) Go to any `sale.order` record and trigger the action.
    3) it outputs the error.
    4) please note that, if you restart the server, error doesn't happen,
    because places where override happens doesn't get invoked.
    
    task-4365106
    
    closes odoo/odoo#189944
    
    X-original-commit: 2f24a6a2a806a7c6be83ddd26e2f00f9a481d31d
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- odoo/tools/pdf/_pypdf2_1.py
+++ odoo/tools/pdf/_pypdf2_1.py
@@ -10,8 +10,13 @@ __all__ = [
     "generic",
 ]
 
-
+# by default PdfFileReader will overwrite warnings.showwarning which is what
+# logging.captureWarnings does, meaning it essentially reverts captureWarnings
+# every time it's called which is undesirable
 class PdfReader(PdfFileReader):
+    def __init__(self, stream, strict=True, warndest=None, overwriteWarnings=True):
+        super().__init__(stream, strict=strict, warndest=warndest, overwriteWarnings=False)
+
     def getFormTextFields(self):
         if self.getFields() is None:
             # Prevent this version of PyPDF2 from trying to iterate over `None`
