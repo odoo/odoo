fb2520c4ac4f7e8e6c2a28ad56afc16837f7efd1
alan-odoo <alan@odoo.com>
2024-12-06 11:33:19 +0100

    Some changed.

--- addons/event/models/event_question_answer.py
+++ addons/event/models/event_question_answer.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from email.policy import default
 
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
