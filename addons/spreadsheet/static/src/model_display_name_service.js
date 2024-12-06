abbbc3d1b7cc7d69bb8d495deb975e97dd2952d0
Pierre Rousseau <pro@odoo.com>
2024-12-06 09:35:14 +0100

    [IMP] spreadsheet: introduce model display name service
    This service is used to get the display name of a model. It takes
    advantage of the Cache object of web.
    
    Before this commit, display names were fetched either from `serverData`
    (with a custom cache management) or from the orm service (components).
    With the introduction of this service, the cache is global to all the
    calls (components and serverData).
    
    This change will allow us to remove the `fetch` method on `serverData`
    
    Task: 4384807
    Part-of: odoo/odoo#189763
    Related: odoo/enterprise#75258
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

---
+++ addons/spreadsheet/static/src/model_display_name_service.js
@@ -0,0 +1,37 @@
+import { Cache } from "@web/core/utils/cache";
+import { registry } from "@web/core/registry";
+
+export const modelDisplayNameService = {
+    dependencies: ["orm"],
+    async: ["getModelDisplayName"],
+    start(env, { orm }) {
+        const cache = new Cache(
+            (model) =>
+                orm
+                    .call("ir.model", "display_name_for", [[model]])
+                    .then((result) => result[0]?.display_name)
+                    .catch((error) => {
+                        cache.clear(model);
+                        return Promise.reject(error);
+                    }),
+            (model) => model
+        );
+
+        env.bus.addEventListener("CLEAR-CACHES", () => cache.invalidate());
+
+        /**
+         * @param {string} model
+         * @returns {Promise<object>}
+         */
+        async function getModelDisplayName(model) {
+            if (typeof model !== "string" || !model) {
+                throw new Error(`Invalid model name: ${model}`);
+            }
+            return cache.read(model);
+        }
+
+        return { getModelDisplayName };
+    },
+};
+
+registry.category("services").add("modelDisplayName", modelDisplayNameService);
