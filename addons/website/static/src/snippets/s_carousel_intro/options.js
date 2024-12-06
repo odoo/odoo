281d0b8418b30d1682bd6ae42cd1f3dc16798df4
Serge Bayet (seba) <seba@odoo.com>
2024-12-06 11:32:17 +0000

    [FIX] website: correct carousel controllers options handling
    Since [1], a method was introduced to prevent the "controllers" options
    from being centered when arrows and indicators are displayed. However,
    it was implemented in the wrong overridden method (computeWidgetState),
    which cannot update the DOM. Additionally, it failed to account for the
    preview mode.
    
    This commit resolves these issues.
    
    Steps to reproduce:
    
    - Website - Edit mode
    - Drag and drop a "carousel intro" snippet onto the page.
    - Select the option "Controllers > Arrows > Hidden."
    - Then select the option "Controllers > Centered."
    - The controllers are now displayed as centered.
    - Hover over the "Controllers > Arrows" options and preview a value
      other than "Hidden."
    - The preview does not update correctly.
    
    [1]: https://github.com/odoo/odoo/commit/454d743ed631c068b7b0bc72e4fd81b225a5ac52
    
    Related to:
    task-4094405
    
    closes odoo/odoo#195869
    
    X-original-commit: 247f28fdec788c7eb7c4288db29b931c73a23757
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>
    Signed-off-by: Serge Bayet (seba) <seba@odoo.com>

--- addons/website/static/src/snippets/s_carousel_intro/options.js
+++ addons/website/static/src/snippets/s_carousel_intro/options.js
@@ -5,22 +5,37 @@ options.registry.CarouselIntro = options.registry.Carousel.extend({
     /**
      * @override
      */
-    async _computeWidgetState(methodName, params) {
+    async _renderCustomXML(uiFragment) {
+        // To remove in MASTER.
+        const arrowsOptionsEl = uiFragment
+            .querySelector(`[data-select-class="s_carousel_default"]`)
+            .closest("we-select");
+        const indicatorsOptionsEl = uiFragment
+            .querySelector(`[data-select-class="s_carousel_indicators_dots"]`)
+            .closest("we-select");
+        arrowsOptionsEl.dataset.name = "arrows_opt";
+        indicatorsOptionsEl.dataset.name = "indicators_opt";
+    },
+    /**
+     * @override
+     */
+    async selectClass(previewMode, widgetValue, params) {
         // Prevent the "Controllers" option from being "centered" when
-        // arrows and indicators are displayed.
-        if (methodName === "selectClass" && params.name === "carousel_controllers_centered_opt") {
-            const controllersEl = this.$target[0];
-            const carouselEl = controllersEl.closest(".carousel");
-            const indicatorsEl = controllersEl.querySelector(".carousel-indicators");
-            if (
-                !carouselEl.classList.contains("s_carousel_arrows_hidden")
-                && !indicatorsEl.classList.contains("s_carousel_indicators_hidden")
-            )
-            {
-                controllersEl.classList.toggle("justify-content-center");
-                controllersEl.classList.toggle("justify-content-between");
-            }
+        // arrows and indicators are displayed
+        await this._super(...arguments);
+        if (["arrows_opt", "indicators_opt"].includes(params.name)) {
+            const carouselEl = this.$target[0].closest(".carousel");
+            const controllersEl = carouselEl.querySelector(".s_carousel_intro_controllers_row");
+            const indicatorsEl = carouselEl.querySelector(".carousel-indicators");
+
+            const hasHiddenArrows = carouselEl.classList.contains("s_carousel_arrows_hidden");
+            const hasHiddenIndicators = indicatorsEl.classList.contains(
+                "s_carousel_indicators_hidden"
+            );
+
+            const contentBetween = !hasHiddenIndicators && !hasHiddenArrows;
+            controllersEl.classList.toggle("justify-content-between", contentBetween);
+            controllersEl.classList.toggle("justify-content-center", !contentBetween);
         }
-        return this._super(...arguments);
     },
 });
