73b76d4d46bbec6fb1410ececfd27c4eed8ac510
Max Whale <mawh@odoo.com>
2024-12-06 09:37:27 +0000

    [IMP] hw_drivers,point_of_sale: certification for scales
    In order to comply with the EU Measuring Instruments
    Directive, the code for weighing products must be
    verified with a checksum to ensure it has not been
    modified. This commit refactors both the IoT driver
    and the POS code, with the goal of removing dead
    code and making the amount of code that needs to
    be certified as small as possible.
    
    task-3929839
    
    closes odoo/odoo#201582
    
    X-original-commit: d8835389b13a557e84a67be38080e21ce6711313
    Related: odoo/enterprise#81352
    Signed-off-by: Yaroslav Soroko (yaso) <yaso@odoo.com>
    Signed-off-by: Max Whale (mawh) <mawh@odoo.com>

--- addons/point_of_sale/static/src/customer_display/customer_display.js
+++ addons/point_of_sale/static/src/customer_display/customer_display.js
@@ -4,7 +4,6 @@ import { MainComponentsContainer } from "@web/core/main_components_container";
 import { session } from "@web/session";
 import { useService } from "@web/core/utils/hooks";
 import { mountComponent } from "@web/env";
-import { roundPrecision as round_pr } from "@web/core/utils/numbers";
 import { TagsList } from "@web/core/tags_list/tags_list";
 
 export class CustomerDisplay extends Component {
@@ -17,13 +16,6 @@ export class CustomerDisplay extends Component {
         this.order = useService("customer_display_data");
     }
 
-    get netWeight() {
-        const weight = round_pr(this.order.weight || 0, this.order.scaleData.uomRounding);
-        const weightRound = weight.toFixed(
-            Math.ceil(Math.log(1.0 / this.order.scaleData.uomRounding) / Math.log(10))
-        );
-        return weightRound - parseFloat(this.order.tare);
-    }
     getInternalNotes() {
         return JSON.parse(this.line.internalNote || "[]");
     }
