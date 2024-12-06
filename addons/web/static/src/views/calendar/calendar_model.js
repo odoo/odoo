b0b0c100f93cbdcf94af8bd43156256fce2839a2
Abderraouf Ghrissi (abgh) <abgh@odoo.com>
2024-12-06 15:53:33 +0000

    [REF] web: move default dates to a function
    - getAllDayDates function was added to be able to inherit it
      in other modules and change default dates if needed.
    
    task-4164485
    
    closes odoo/odoo#206243
    
    X-original-commit: 9c4af7a3ec1356aa8fb8e2e6f6e7591fc9306958
    Related: odoo/enterprise#83502
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/web/static/src/views/calendar/calendar_model.js
+++ addons/web/static/src/views/calendar/calendar_model.js
@@ -299,6 +299,9 @@ export class CalendarModel extends Model {
     }
 
     //--------------------------------------------------------------------------
+    getAllDayDates(start, end) {
+        return [start.set({ hours: 7 }), end.set({ hours: 19 })];
+    }
 
     buildRawRecord(partialRecord, options = {}) {
         const data = {};
@@ -323,8 +326,7 @@ export class CalendarModel extends Model {
         if (partialRecord.isAllDay) {
             if (!this.hasAllDaySlot && !isDateEvent && !partialRecord.id) {
                 // default hours in the user's timezone
-                start = start.set({ hours: 7 });
-                end = end.set({ hours: 19 });
+                [start, end] = this.getAllDayDates(start, end);
             }
         }
 
