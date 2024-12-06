4832510882d07941af0315d89c51fd96f69237d3
Walid (wasa) <wasa@odoo.com>
2024-12-06 18:21:10 +0000

    [FIX] web_editor: adjust selection on column deletion
    **Problem**:
    When deleting columns in the editor, if the start and end points of the selection are inside the columns, the `range.extractContents` method extracts the content but leaves behind `div` elements for the first and last columns (or sometimes only one of them). This occurs because the columns are partially selected (content but not container).
    
    **Solution**:
    To ensure `range.extractContents` removes the columns completely, extend the selection to encompass the entire content of the `o_text_columns` elements when the selection starts at the first leaf and ends at the last leaf within the columns.
    
    **Steps to reproduce**:
    1. Open the editor.
    2. Create columns (any number).
    3. Select all content (e.g., with `Ctrl+A`).
    4. Press `Backspace` to delete the content.
    5. Observe that the DOM still contains `o_text_columns` and `row` elements, even though the content appears deleted.
    
    opw-4350486
    
    X-original-commit: 08f0c6481a190349369ead5ab9328886392ddbeb

--- addons/web_editor/static/src/js/editor/odoo-editor/src/utils/utils.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/src/utils/utils.js
@@ -947,12 +947,14 @@ export function getDeepRange(editable, { range, sel, splitText, select, correctT
     // at the last position of the previous node instead.
     const endLeaf = firstLeaf(end);
     const beforeEnd = endLeaf.previousSibling;
+    const isInsideColumn = closestElement(end, '.o_text_columns')
     if (
         correctTripleClick &&
         !endOffset &&
         (start !== end || startOffset !== endOffset) &&
         (!beforeEnd || (beforeEnd.nodeType === Node.TEXT_NODE && !isVisibleTextNode(beforeEnd) && !isZWS(beforeEnd))) &&
-        !closestElement(endLeaf, 'table')
+        !closestElement(endLeaf, 'table') &&
+        !isInsideColumn
     ) {
         const previous = previousLeaf(endLeaf, editable, true);
         if (previous && closestElement(previous).isContentEditable) {
