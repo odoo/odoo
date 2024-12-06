21058c9cca2e8164e0bca020cc5df23e9787cf14
Pierre Rousseau <pro@odoo.com>
2024-12-06 09:43:18 +0100

    [IMP] spreadsheet: remove fetch method on serverData
    With the introduction of the modelDisplayName service and the use of
    the field service, the fetch method on the serverData service is no
    longer needed. This commit removes the fetch method and its tests.
    
    closes odoo/odoo#189763
    
    Task: 4384807
    Related: odoo/enterprise#75258
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- addons/spreadsheet/static/tests/data_fetching/server_data.test.js
+++ addons/spreadsheet/static/tests/data_fetching/server_data.test.js
@@ -48,60 +48,6 @@ test("synchronous get which returns an error", async () => {
     expect.verifySteps([]);
 });
 
-test("simple async fetch", async () => {
-    const orm = {
-        call: async (model, method, args) => {
-            expect.step(`${model}/${method}`);
-            return args[0];
-        },
-    };
-    const serverData = new ServerData(orm, {
-        whenDataStartLoading: () => expect.step("data-fetching-notification"),
-    });
-    const result = await serverData.fetch("partner", "get_something", [5]);
-    expect(result).toBe(5);
-    expect.verifySteps(["partner/get_something"]);
-    expect(await serverData.fetch("partner", "get_something", [5])).toBe(5);
-    expect.verifySteps([]);
-});
-
-test("async fetch which throws an error", async () => {
-    const orm = {
-        call: async (model, method, args) => {
-            expect.step(`${model}/${method}`);
-            throw new Error("error while fetching data");
-        },
-    };
-    const serverData = new ServerData(orm, {
-        whenDataStartLoading: () => expect.step("data-fetching-notification"),
-    });
-    expect(serverData.fetch("partner", "get_something", [5])).rejects.toThrow();
-    expect.verifySteps(["partner/get_something"]);
-    expect(serverData.fetch("partner", "get_something", [5])).rejects.toThrow();
-    expect.verifySteps([]);
-});
-
-test("two identical concurrent async fetch", async () => {
-    const orm = {
-        call: async (model, method, args) => {
-            expect.step(`${model}/${method}`);
-            return args[0];
-        },
-    };
-    const serverData = new ServerData(orm, {
-        whenDataStartLoading: () => expect.step("data-fetching-notification"),
-    });
-    const [result1, result2] = await Promise.all([
-        serverData.fetch("partner", "get_something", [5]),
-        serverData.fetch("partner", "get_something", [5]),
-    ]);
-    // it should have fetch the data once
-    expect.verifySteps(["partner/get_something"]);
-    expect(result1).toBe(5);
-    expect(result2).toBe(5);
-    expect.verifySteps([]);
-});
-
 test("batch get with a single item", async () => {
     const deferred = new Deferred();
     const orm = {
@@ -190,78 +136,6 @@ test("batch get with one error", async () => {
     expect(serverData.batch.get("partner", "get_something_in_batch", 4)).toBe(4);
     expect(() => serverData.batch.get("partner", "get_something_in_batch", 5)).toThrow(Error);
     expect(serverData.batch.get("partner", "get_something_in_batch", 6)).toBe(6);
-    expect.verifySteps([]);
-});
-
-test("concurrently fetch then get the same request", async () => {
-    const orm = {
-        call: async (model, method, args) => {
-            expect.step(`${model}/${method}`);
-            return args[0];
-        },
-    };
-    const serverData = new ServerData(orm, {
-        whenDataStartLoading: () => expect.step("data-fetching-notification"),
-    });
-    const promise = serverData.fetch("partner", "get_something", [5]);
-    expect(() => serverData.get("partner", "get_something", [5])).toThrow(LoadingDataError);
-    // it loads the data independently
-    expect.verifySteps([
-        "partner/get_something",
-        "partner/get_something",
-        "data-fetching-notification",
-    ]);
-    const result = await promise;
-    await animationFrame();
-    expect(result).toBe(5);
-    expect(serverData.get("partner", "get_something", [5])).toBe(5);
-    expect.verifySteps([]);
-});
-
-test("concurrently get then fetch the same request", async () => {
-    const orm = {
-        call: async (model, method, args) => {
-            expect.step(`${model}/${method}`);
-            return args[0];
-        },
-    };
-    const serverData = new ServerData(orm, {
-        whenDataStartLoading: () => expect.step("data-fetching-notification"),
-    });
-    expect(() => serverData.get("partner", "get_something", [5])).toThrow(LoadingDataError);
-    const result = await serverData.fetch("partner", "get_something", [5]);
-    // it should have fetch the data once
-    expect.verifySteps([
-        "partner/get_something",
-        "data-fetching-notification",
-        "partner/get_something",
-    ]);
-    expect(result).toBe(5);
-    expect(serverData.get("partner", "get_something", [5])).toBe(5);
-    expect.verifySteps([]);
-});
-
-test("concurrently batch get then fetch the same request", async () => {
-    const orm = {
-        call: async (model, method, args) => {
-            expect.step(`${model}/${method}`);
-            return args[0];
-        },
-    };
-    const serverData = new ServerData(orm, {
-        whenDataStartLoading: () => expect.step("data-fetching-notification"),
-    });
-    expect(() => serverData.batch.get("partner", "get_something", 5)).toThrow(LoadingDataError);
-    const result = await serverData.fetch("partner", "get_something", [5]);
-    await animationFrame();
-    // it should have fetch the data once
-    expect.verifySteps([
-        "partner/get_something",
-        "partner/get_something",
-        "data-fetching-notification",
-    ]);
-    expect(result).toBe(5);
-    expect(serverData.batch.get("partner", "get_something", 5)).toBe(5);
     expect.verifySteps([]);
 });
 
