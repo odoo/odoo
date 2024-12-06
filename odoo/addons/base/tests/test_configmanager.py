088d99bf40d792c3e11b28713a35ef88fc0c38f1
Krzysztof Magusiak (krma) <krma@odoo.com>
2024-12-06 16:31:57 +0000

    [IMP] core: add soft time limit for cron jobs
    Adding an option `limit_time_soft_cron` that indicates after how many
    seconds the processing is given per cron batch for a database after
    which it should stop gracefully.
    
    There is already a hard limit on the cron batch execution. The soft
    limit allows ir.cron to avoid processing too many jobs and being killed.
    This allows also to better monitor which databases respect the soft
    limit by stopping after allocated time or are killed. Respectively, the
    first ones will have multiple smaller jobs and the ones being killed
    will have jobs lasting very long (a behaviour we want to avoid).
    
    task-4433571

--- odoo/addons/base/tests/test_configmanager.py
+++ odoo/addons/base/tests/test_configmanager.py
@@ -165,6 +165,7 @@ class TestConfigManager(TransactionCase):
             'limit_time_cpu': 60,
             'limit_time_real': 120,
             'limit_time_real_cron': -1,
+            'limit_time_soft_cron': -1,
             'limit_request': 2**16,
         })
 
@@ -280,6 +281,7 @@ class TestConfigManager(TransactionCase):
             'limit_time_cpu': 60,
             'limit_time_real': 61,
             'limit_time_real_cron': 62,
+            'limit_time_soft_cron': -1,
             'limit_request': 100,
         })
         self.assertEqual(capture.output, [
@@ -394,6 +396,7 @@ class TestConfigManager(TransactionCase):
             'limit_time_cpu': 60,
             'limit_time_real': 120,
             'limit_time_real_cron': -1,
+            'limit_time_soft_cron': -1,
             'limit_request': 1 << 16,
 
             # new options since 14.0
@@ -558,6 +561,7 @@ class TestConfigManager(TransactionCase):
             'limit_time_cpu': 60,
             'limit_time_real': 61,
             'limit_time_real_cron': 62,
+            'limit_time_soft_cron': 55,
             'limit_request': 100,
         })
 
