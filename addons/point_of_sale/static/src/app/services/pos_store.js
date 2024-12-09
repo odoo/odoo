9daaf99124c40e5ce27538421317a6915a5832ad
Joren Van Onder <jov@odoo.com>
2024-12-09 21:18:06 +0000

    [IMP] point_of_sale: add some hooks for l10n_br_edi_pos
    This doesn't change behavior, only adds a way to modify the behavior
    from other modules.
    
    task-3564171
    
    X-original-commit: 5863282e2f2cade805e5847fdf96c5fd4140b2e7

--- addons/point_of_sale/static/src/app/services/pos_store.js
+++ addons/point_of_sale/static/src/app/services/pos_store.js
@@ -1746,6 +1746,9 @@ export class PosStore extends WithLazyGetterTrap {
             );
         });
     }
+    editPartnerContext(partner) {
+        return {};
+    }
     /**
      * @param {import("@point_of_sale/app/models/res_partner").ResPartner?} partner leave undefined to create a new partner
      */
@@ -1755,6 +1758,7 @@ export class PosStore extends WithLazyGetterTrap {
             "point_of_sale.res_partner_action_edit_pos",
             {
                 props: { resId: partner?.id },
+                additionalContext: this.editPartnerContext(),
             }
         );
         const newPartner = await this.data.read("res.partner", record.config.resIds);
@@ -1784,8 +1788,8 @@ export class PosStore extends WithLazyGetterTrap {
     async allowProductCreation() {
         return await user.hasGroup("base.group_system");
     }
-    async orderDetails(order) {
-        this.dialog.add(FormViewDialog, {
+    orderDetailsProps(order) {
+        return {
             resModel: "pos.order",
             resId: order.id,
             onRecordSaved: async (record) => {
@@ -1798,7 +1802,10 @@ export class PosStore extends WithLazyGetterTrap {
                     type: "ir.actions.act_window_close",
                 });
             },
-        });
+        };
+    }
+    async orderDetails(order) {
+        this.dialog.add(FormViewDialog, this.orderDetailsProps(order));
     }
     async closePos() {
         this._resetConnectedCashier();
