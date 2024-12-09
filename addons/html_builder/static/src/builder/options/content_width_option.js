3846bd9b4833635fec69fe6f327a3d967d847a29
FrancoisGe <fge@odoo.com>
2024-12-09 14:39:03 +0100

    use template option

--- addons/html_builder/static/src/builder/options/content_width_option.js
+++ addons/html_builder/static/src/builder/options/content_width_option.js
@@ -1,19 +1,9 @@
-import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
-import { defaultOptionComponents } from "../components/defaultComponents";
 
 // TODO to import in html_builder
 
-export class ContentWidthOption extends Component {
-    static template = "html_builder.ContentWidthOption";
-    static components = {
-        ...defaultOptionComponents,
-    };
-    static props = {};
-}
-
 registry.category("sidebar-element-option").add("ContentWidthOption", {
-    OptionComponent: ContentWidthOption,
+    template: "html_builder.ContentWidthOption",
     selector: "section, .s_carousel .carousel-item, .s_carousel_intro_item",
     // TODO add exclude and target and remove applyTo in the template of ContentWidthOption
     // exclude: "[data-snippet] :not(.oe_structure) > [data-snippet]",
