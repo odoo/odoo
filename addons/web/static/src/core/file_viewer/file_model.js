4e578e4f0bd25ab3c8ab23d146bd38dfe213cb41
zel-odoo <zel@odoo.com>
2024-12-09 19:55:13 +0000

    [FIX] web,*: file viewer to show url attachment
    The file viewer tries to show the document from the url obtained from
    the defaultSource function in file_model.js. This function builds the
    url using urlRoute.
    
    Before the fix, the urlRoute is the url from the attachment. This causes
    the PDF viewer to fail because it has a same-origin policy.
    
    This fix avoids to use the url to the one from the attachment's url
    directly so that the viewer can show the document correctly with the
    computed urlRoute.
    
    The attachment url does not need to be used at all, as the odoo server
    will properly direct.
    
    This fix apply formatter on file_model.js.
    
    Forward-port-of: https://github.com/odoo/odoo/pull/189080
    Forward-port-of: https://github.com/odoo/odoo/pull/187970
    X-original-commit: 321eda23968f243e20fa7f167ba21dde28bbdfb5

--- addons/web/static/src/core/file_viewer/file_model.js
+++ addons/web/static/src/core/file_viewer/file_model.js
@@ -1,5 +1,6 @@
 import { url } from "@web/core/utils/urls";
 
+<<<<<<< 18.0
 export const FileModelMixin = T => class extends T {
     access_token;
     checksum;
@@ -15,28 +16,146 @@ export const FileModelMixin = T => class extends T {
     url;
     /** @type {boolean} */
     uploading;
+||||||| e4874075c478e9d5c36acc1105c7e33f4d738437
+export const FileModelMixin = T => class extends T {
+    accessToken;
+    checksum;
+    extension;
+    filename;
+    id;
+    mimetype;
+    name;
+    type;
+    /** @type {string} */
+    tmpUrl;
+    /** @type {string} */
+    url;
+    /** @type {boolean} */
+    uploading;
+=======
+export const FileModelMixin = (T) =>
+    class extends T {
+        accessToken;
+        checksum;
+        extension;
+        filename;
+        id;
+        mimetype;
+        name;
+        /** @type {"binary"|"url"} */
+        type;
+        /** @type {string} */
+        tmpUrl;
+        /**
+         * This URL should not be used as the URL to serve the file. `urlRoute` should be used
+         * instead. The server will properly redirect to the correct URL when necessary.
+         *
+         * @type {string}
+         */
+        url;
+        /** @type {boolean} */
+        uploading;
+>>>>>>> 96f89670e3c90d74f4459cc8321c909e1d684330
 
-    get defaultSource() {
-        const route = url(this.urlRoute, this.urlQueryParams);
-        const encodedRoute = encodeURIComponent(route);
-        if (this.isPdf) {
-            return `/web/static/lib/pdfjs/web/viewer.html?file=${encodedRoute}#pagemode=none`;
+        get defaultSource() {
+            const route = url(this.urlRoute, this.urlQueryParams);
+            const encodedRoute = encodeURIComponent(route);
+            if (this.isPdf) {
+                return `/web/static/lib/pdfjs/web/viewer.html?file=${encodedRoute}#pagemode=none`;
+            }
+            if (this.isUrlYoutube) {
+                const urlArr = this.url.split("/");
+                let token = urlArr[urlArr.length - 1];
+                if (token.includes("watch")) {
+                    token = token.split("v=")[1];
+                    const amp = token.indexOf("&");
+                    if (amp !== -1) {
+                        token = token.substring(0, amp);
+                    }
+                }
+                return `https://www.youtube.com/embed/${token}`;
+            }
+            return route;
         }
-        if (this.isUrlYoutube) {
-            const urlArr = this.url.split("/");
-            let token = urlArr[urlArr.length - 1];
-            if (token.includes("watch")) {
-                token = token.split("v=")[1];
-                const amp = token.indexOf("&");
-                if (amp !== -1) {
-                    token = token.substring(0, amp);
+
+        get displayName() {
+            return this.name || this.filename;
+        }
+
+        get downloadUrl() {
+            return url(this.urlRoute, { ...this.urlQueryParams, download: true });
+        }
+
+        get isImage() {
+            const imageMimetypes = [
+                "image/bmp",
+                "image/gif",
+                "image/jpeg",
+                "image/png",
+                "image/svg+xml",
+                "image/tiff",
+                "image/x-icon",
+                "image/webp",
+            ];
+            return imageMimetypes.includes(this.mimetype);
+        }
+
+        get isPdf() {
+            return this.mimetype && this.mimetype.startsWith("application/pdf");
+        }
+
+        get isText() {
+            const textMimeType = [
+                "application/javascript",
+                "application/json",
+                "text/css",
+                "text/html",
+                "text/plain",
+            ];
+            return textMimeType.includes(this.mimetype);
+        }
+
+        get isUrl() {
+            return this.type === "url" && this.url;
+        }
+
+        get isUrlYoutube() {
+            return !!this.url && this.url.includes("youtu");
+        }
+
+        get isVideo() {
+            const videoMimeTypes = ["audio/mpeg", "video/x-matroska", "video/mp4", "video/webm"];
+            return videoMimeTypes.includes(this.mimetype);
+        }
+
+        get isViewable() {
+            return (
+                (this.isText || this.isImage || this.isVideo || this.isPdf || this.isUrlYoutube) &&
+                !this.uploading
+            );
+        }
+
+        /**
+         * @returns {Object}
+         */
+        get urlQueryParams() {
+            if (this.uploading && this.tmpUrl) {
+                return {};
+            }
+            const params = {
+                access_token: this.accessToken,
+                filename: this.name,
+                unique: this.checksum,
+            };
+            for (const prop in params) {
+                if (!params[prop]) {
+                    delete params[prop];
                 }
             }
-            return `https://www.youtube.com/embed/${token}`;
+            return params;
         }
-        return route;
-    }
 
+<<<<<<< 18.0
     get displayName() {
         return this.name || this.filename;
     }
@@ -109,23 +228,90 @@ export const FileModelMixin = T => class extends T {
         for (const prop in params) {
             if (!params[prop]) {
                 delete params[prop];
-            }
-        }
-        return params;
+||||||| e4874075c478e9d5c36acc1105c7e33f4d738437
+    get displayName() {
+        return this.name || this.filename;
+    }
+
+    get downloadUrl() {
+        return url(this.urlRoute, { ...this.urlQueryParams, download: true });
+    }
+
+    get isImage() {
+        const imageMimetypes = [
+            "image/bmp",
+            "image/gif",
+            "image/jpeg",
+            "image/png",
+            "image/svg+xml",
+            "image/tiff",
+            "image/x-icon",
+            "image/webp",
+        ];
+        return imageMimetypes.includes(this.mimetype);
+    }
+
+    get isPdf() {
+        return this.mimetype && this.mimetype.startsWith("application/pdf");
+    }
+
+    get isText() {
+        const textMimeType = [
+            "application/javascript",
+            "application/json",
+            "text/css",
+            "text/html",
+            "text/plain",
+        ];
+        return textMimeType.includes(this.mimetype);
+    }
+
+    get isUrl() {
+        return this.type === "url" && this.url;
+    }
+
+    get isUrlYoutube() {
+        return !!this.url && this.url.includes("youtu");
+    }
+
+    get isVideo() {
+        const videoMimeTypes = ["audio/mpeg", "video/x-matroska", "video/mp4", "video/webm"];
+        return videoMimeTypes.includes(this.mimetype);
+    }
+
+    get isViewable() {
+        return (
+            (this.isText || this.isImage || this.isVideo || this.isPdf || this.isUrlYoutube) &&
+            !this.uploading
+        );
     }
 
     /**
-     * @returns {string}
+     * @returns {Object}
      */
-    get urlRoute() {
-        if (this.isUrl) {
-            return this.url;
-        }
+    get urlQueryParams() {
         if (this.uploading && this.tmpUrl) {
-            return this.tmpUrl;
+            return {};
         }
-        return this.isImage ? `/web/image/${this.id}` : `/web/content/${this.id}`;
-    }
-}
+        const params = {
+            access_token: this.accessToken,
+            filename: this.name,
+            unique: this.checksum,
+        };
+        for (const prop in params) {
+            if (!params[prop]) {
+                delete params[prop];
+=======
+        /**
+         * @returns {string}
+         */
+        get urlRoute() {
+            if (this.uploading && this.tmpUrl) {
+                return this.tmpUrl;
+>>>>>>> 96f89670e3c90d74f4459cc8321c909e1d684330
+            }
+            return this.isImage ? `/web/image/${this.id}` : `/web/content/${this.id}`;
+        }
+    };
 
 export class FileModel extends FileModelMixin(Object) {}
