4b8a45b2bc994c38c103a6438823c0d9e21f276a
“Beekan” <mmha@odoo.com>
2024-12-09 17:09:59 +0100

    [IMP] profiler: Add a wizard for profile rendering

--- addons/web/controllers/profiling.py
+++ addons/web/controllers/profiling.py
@@ -3,7 +3,9 @@
 import json
 
 from odoo.exceptions import UserError
-from odoo.http import Controller, request, Response, route
+from odoo.http import Controller, request, Response, route, content_disposition
+import urllib
+import base64
 
 class Profiling(Controller):
 
@@ -25,13 +27,32 @@ class Profiling(Controller):
         '/web/speedscope/<model("ir.profile"):profile>',
     ], type='http', sitemap=False, auth='user', readonly=True)
     def speedscope(self, profile=None):
-        # don't server speedscope index if profiling is not enabled
-        if not request.env['ir.profile']._enabled_until():
-            return request.not_found()
         icp = request.env['ir.config_parameter']
         context = {
             'profile': profile,
             'url_root': request.httprequest.url_root,
             'cdn': icp.sudo().get_param('speedscope_cdn', "https://cdn.jsdelivr.net/npm/speedscope@1.13.0/dist/release/")
         }
+        context['search_params'] = urllib.parse.quote(urllib.parse.urlencode(request.httprequest.args))
         return request.render('web.view_speedscope_index', context)
+    
+    @route([
+        '/web/speedscope/download',
+        '/web/speedscope/download/<model("ir.profile"):profile>',
+    ], type='http', sitemap=False, auth='user')
+    def speedscope_download(self, profile=None):
+        #The request already has the URL parameters which are used when the computed
+        #field speedscope is triggered. The parameters are accessed from there.
+        file_content = base64.b64decode(profile.speedscope)
+        headers = _get_headers("speedscope.json", 'application/json', file_content)
+        response = request.make_response(file_content,headers)
+        return response
+
+def _get_headers(filename, filetype, content):
+    return [
+        ('Content-Type', filetype),
+        ('Content-Length', len(content)),
+        ('Content-Disposition', content_disposition(filename)),
+        ('X-Content-Type-Options', 'nosniff'),
+    ]
+
