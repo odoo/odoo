06aaa32afe57151909d7e7a9288f5916f17c34da
Arnaud Sibille <arsi@odoo.com>
2024-12-09 16:20:01 +0000

    [IMP] account: compute invoice salesperson from partner salesperson
    For `sale.order`, the salesperson (`user_id`) on the order is set to
    the partner's salesperson (`user_id` on `res.partner`) if set.
    
    Currently, `account.move` sets the salesperson (`invoice_user_id`) to
    the user creating the invoice.
    
    With this commit, `invoice_user_id` is set to the partner's salesperson
    if exists.
    
    task-4374265
    
    closes odoo/odoo#200104
    
    X-original-commit: fa67f619bd3245688f45cd187fef3e120c55a9c2
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>
    Signed-off-by: Arnaud Sibille (arsi) <arsi@odoo.com>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -758,13 +758,19 @@ class AccountMove(models.Model):
     # COMPUTE METHODS
     # -------------------------------------------------------------------------
 
-    @api.depends('move_type')
+    @api.depends('move_type', 'partner_id')
     def _compute_invoice_default_sale_person(self):
         # We want to modify the sale person only when we don't have one and if the move type corresponds to this condition
         # If the move doesn't correspond, we remove the sale person
         for move in self:
             if move.is_sale_document(include_receipts=True):
-                move.invoice_user_id = move.invoice_user_id or self.env.user
+                if move.partner_id:
+                    move.invoice_user_id = (
+                        move.invoice_user_id
+                        or move.partner_id.user_id
+                        or move.partner_id.commercial_partner_id.user_id
+                        or self.env.user
+                    )
             else:
                 move.invoice_user_id = False
 
