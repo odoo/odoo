fac6357756141b8804f335fd6b67e5a6653c82d7
Louis Travaux <lotr@odoo.com>
2024-12-09 11:04:35 +0000

    [IMP] hw_drivers: better check for server url
    Lots of methods need to know the associated database url. It is sometimes checked
    in the caller method, and sometimes inside of the called method itself.
    
    We now use a decorator that checks it for us.
    
    Task: 4391509
    X-original-commit: 08fb3ba609754c3335fb179924e5f2b24166460d

--- addons/hw_drivers/tools/helpers.py
+++ addons/hw_drivers/tools/helpers.py
@@ -8,6 +8,7 @@ import datetime
 from enum import Enum
 from functools import cache, wraps
 from importlib import util
+import inspect
 import io
 import logging
 import netifaces
@@ -98,6 +99,29 @@ elif platform.system() == 'Linux':
                 subprocess.run(["sudo", "mount", "-o", "remount,rw", "/root_bypass_ramdisks/etc/cups"], check=False)
 
 
+def require_db(function):
+    """Decorator to check if the IoT Box is connected to the internet
+    and to a database before executing the function.
+    This decorator injects the ``server_url`` parameter if the function has it.
+    """
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        fname = f"<function {function.__module__}.{function.__qualname__}>"
+        server_url = get_odoo_server_url()
+        iot_box_ip = get_ip()
+        if not iot_box_ip or iot_box_ip == "10.11.12.1" or not server_url:
+            _logger.info('Ignoring the function %s without a connected database', fname)
+            return
+
+        arg_name = 'server_url'
+        if arg_name in inspect.signature(function).parameters:
+            _logger.debug('Adding server_url param to %s', fname)
+            kwargs[arg_name] = server_url
+
+        return function(*args, **kwargs)
+    return wrapper
+
+
 def start_nginx_server():
     if platform.system() == 'Windows':
         path_nginx = get_path_nginx()
@@ -152,12 +176,15 @@ def check_certificate():
 
 
 @toggleable
-def check_git_branch():
+@require_db
+def check_git_branch(server_url=None):
     """Check if the local branch is the same as the connected Odoo DB and
     checkout to match it if needed.
+
+    :param server_url: The URL of the connected Odoo database (provided by decorator).
     """
     try:
-        response = requests.post(get_odoo_server_url() + "/web/webclient/version_info", json={}, timeout=5)
+        response = requests.post(server_url + "/web/webclient/version_info", json={}, timeout=5)
         response.raise_for_status()
         data = response.json()
     except requests.exceptions.HTTPError:
@@ -311,14 +338,10 @@ def get_path_nginx():
 @cache
 def get_odoo_server_url():
     """Get the URL of the linked Odoo database.
-    If no internet connection is available, return None to avoid trying
-    to reach the server.
 
     :return: The URL of the linked Odoo database.
     :rtype: str or None
     """
-    if get_ip() == "10.11.12.1":
-        return None
     return get_conf('remote_server')
 
 
@@ -412,41 +435,43 @@ def delete_iot_handlers():
 
 
 @toggleable
-def download_iot_handlers(auto=True):
+@require_db
+def download_iot_handlers(auto=True, server_url=None):
     """Get the drivers from the configured Odoo server.
     If drivers did not change on the server, download
     will be skipped.
+
+    :param auto: If True, the download will depend on the parameter set in the database
+    :param server_url: The URL of the connected Odoo database (provided by decorator).
     """
-    server = get_odoo_server_url()
-    if server:
-        etag = get_conf('iot_handlers_etag')
-        try:
-            response = requests.post(
-                server + '/iot/get_handlers',
-                data={'mac': get_mac_address(), 'auto': auto},
-                timeout=8,
-                headers={'If-None-Match': etag} if etag else None,
-            )
-            response.raise_for_status()
-        except requests.exceptions.RequestException:
-            _logger.exception('Could not reach configured server to download IoT handlers')
-            return
+    etag = get_conf('iot_handlers_etag')
+    try:
+        response = requests.post(
+            server_url + '/iot/get_handlers',
+            data={'mac': get_mac_address(), 'auto': auto},
+            timeout=8,
+            headers={'If-None-Match': etag} if etag else None,
+        )
+        response.raise_for_status()
+    except requests.exceptions.RequestException:
+        _logger.exception('Could not reach configured server to download IoT handlers')
+        return
 
-        data = response.content
-        if response.status_code == 304 or not data:
-            _logger.info('No new IoT handler to download')
-            return
+    data = response.content
+    if response.status_code == 304 or not data:
+        _logger.info('No new IoT handler to download')
+        return
 
-        try:
-            update_conf({'iot_handlers_etag': response.headers['ETag'].strip('"')})
-        except KeyError:
-            _logger.exception('No ETag in the response headers')
+    try:
+        update_conf({'iot_handlers_etag': response.headers['ETag'].strip('"')})
+    except KeyError:
+        _logger.exception('No ETag in the response headers')
 
-        delete_iot_handlers()
-        with writable():
-            path = path_file('odoo', 'addons', 'hw_drivers', 'iot_handlers')
-            zip_file = zipfile.ZipFile(io.BytesIO(data))
-            zip_file.extractall(path)
+    delete_iot_handlers()
+    with writable():
+        path = path_file('odoo', 'addons', 'hw_drivers', 'iot_handlers')
+        zip_file = zipfile.ZipFile(io.BytesIO(data))
+        zip_file.extractall(path)
 
 
 def compute_iot_handlers_addon_name(handler_kind, handler_file_name):
