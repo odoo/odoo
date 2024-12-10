4a60df49875b6d43eb2d1217accc8af642da9ce0
tsm-odoo <tsm@odoo.com>
2024-12-10 12:22:31 +0100

    [IMP] mail: convert remaining tests to hoot
    task-3818666
    
    closes odoo/odoo#189754
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

---
+++ addons/mail/static/tests/web/debug_menu.test.js
@@ -0,0 +1,41 @@
+import {
+    click,
+    contains,
+    defineMailModels,
+    openFormView,
+    start,
+    startServer,
+} from "@mail/../tests/mail_test_helpers";
+import { describe, expect, test } from "@odoo/hoot";
+import { asyncStep, onRpc, serverState, waitForSteps } from "@web/../tests/web_test_helpers";
+import { MailMessage } from "../mock_server/mock_models/mail_message";
+
+defineMailModels();
+describe.current.tags("desktop");
+
+test("Manage messages", async () => {
+    serverState.debug = true;
+    const pyEnv = await startServer();
+    MailMessage._views = {
+        "list,false": "<list/>",
+        "form,false": "<form/>",
+        "search,false": "<search/>",
+    };
+    onRpc("mail.message", "web_search_read", (params) => {
+        expect(params.kwargs.context.default_res_id).toBe(partnerId);
+        expect(params.kwargs.context.default_res_model).toBe("res.partner");
+        expect(params.kwargs.domain).toEqual([
+            "&",
+            ["res_id", "=", partnerId],
+            ["model", "=", "res.partner"],
+        ]);
+        asyncStep("message_read");
+    });
+    await start();
+    const partnerId = pyEnv["res.partner"].create({ name: "Bob" });
+    await openFormView("res.partner", partnerId);
+    await click(".o_debug_manager .dropdown-toggle");
+    await click(".dropdown-item", { text: "Messages" });
+    await waitForSteps(["message_read"]);
+    await contains(".o_breadcrumb .active > span", { text: "Messages" });
+});
