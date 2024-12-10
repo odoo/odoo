75eeaa5581e0fb48454a6a2aa3f3e4c0ae7e050c
Aaron Bohy <aab@odoo.com>
2024-12-10 14:54:10 +0000

    [FIX] web: export all: correct group counts
    Before this commit, exporting all records in a grouped list view
    generated wrong group counts in the sheet, because archived records
    where taken into account (even though they weren't exported). The
    issue comes from [1], which tried to fix a group count issue when
    exporting archived records from a grouped list. However, it didn't
    really work as only the count was correct, but the archived records
    weren't exported anyway. This commit fixes that flow as well.
    
    [1] https://github.com/odoo/odoo/pull/166097
    
    task~4375142
    
    X-original-commit: a546e2e5a87c311bef054ca4a6909e63d70c91fd

--- addons/test_import_export/models/models_export.py
+++ addons/test_import_export/models/models_export.py
@@ -23,5 +23,7 @@ class ExportAggregatorOne2many(models.Model):
     _name = 'export.aggregator.one2many'
     _description = 'Export Aggregator One2Many'
 
+    name = fields.Char()
     parent_id = fields.Many2one('export.aggregator')
     value = fields.Integer()
+    active = fields.Boolean(default=True)
