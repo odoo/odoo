80b2755a8238478a8bdd0253bd98103ca1859ee8
Yash Vaishnav <yava@odoo.com>
2024-12-10 18:23:28 +0530

    [IMP] hr: added filter of activity plans on the employee
    In this commit,
    - The 'Ongoing Activity Plan' group has been added to the employee search view.
    - When you create plans using the 'Launch Plan' button, you will be redirected
    to the employee view with the default group-by setting applied.
    - Activities that are created will be named in the format
    'Activity Plan Name - Summary.'
    
    Task-4327533

--- addons/hr/wizard/mail_activity_schedule.py
+++ addons/hr/wizard/mail_activity_schedule.py
@@ -47,6 +47,19 @@ class MailActivitySchedule(models.TransientModel):
                     } for key, templates in templates_by_responsible_type.items()
                 )}
 
+    def action_schedule_plan(self):
+        result = super().action_schedule_plan()
+        if result.get('res_model') == 'hr.employee':
+            applied_on = self._get_applied_on_records()
+            if len(applied_on) == 1:
+                return None  #don't want to open multiple form view
+            result.pop('domain', None)
+            if 'context' in result:
+                result['context'].update({'search_default_group_activity_plans_ids': 1})
+            else:
+                result['context'] = {'search_default_group_activity_plans_ids': 1}
+        return result
+
     @api.depends('res_model_id', 'res_ids')
     def _compute_department_id(self):
         for wizard in self:
@@ -56,3 +69,14 @@ class MailActivitySchedule(models.TransientModel):
                 wizard.department_id = False if len(all_departments) > 1 else all_departments
             else:
                 wizard.department_id = False
+
+    def _create_activity(self, record, template, responsible, date_deadline):
+        if self.res_model == 'hr.employee' and template.plan_id:
+            record.activity_plans_ids |= self.plan_id
+            kwargs = {
+                'summary': f"{template.plan_id.name} - {template.summary if template.summary else template.activity_type_id.name}",
+            }
+            activity = super()._create_activity(record, template, responsible, date_deadline, **kwargs)
+            activity.plan_id = self.plan_id
+            return activity
+        super()._create_activity(record, template, responsible, date_deadline)
