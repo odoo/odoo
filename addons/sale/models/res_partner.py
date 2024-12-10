3ffd64869129ee145dd5fa25aeb69b28a77c5a0a
sami odoo <sami@odoo.com>
2024-12-10 12:46:59 +0530

    [IMP] sale,stock: retain filtered data after reload
    Before this commit:
    When a user was redirected to a window through a stat button, the window
    displayed only filtered data using a domain set by a Python function. However,
    when the user reloaded the page, the window displayed unfiltered data.
    
    After this commit:
    - After a reload, the window will display the same filtered data by creating a
      new XML action window with a specified domain.
    - For dedicated action windows related to stat buttons, the domain and context
      have been directly added to the action window.
    - Redundant and unused functions have been removed from the codebase to improve
      efficiency.
    
    Task-4229211
    
    closes odoo/odoo#189270
    
    Related: odoo/enterprise#74959
    Signed-off-by: Jorge Pinna Puissant (jpp) <jpp@odoo.com>

--- addons/sale/models/res_partner.py
+++ addons/sale/models/res_partner.py
@@ -72,13 +72,6 @@ class ResPartner(models.Model):
             [('partner_id', 'child_of', self.commercial_partner_id.id)]
         )
 
-    @api.readonly
-    def action_view_sale_order(self):
-        action = self.env['ir.actions.act_window']._for_xml_id('sale.act_res_partner_2_sale_order')
-        all_child = self.with_context(active_test=False).search([('id', 'child_of', self.ids)])
-        action["domain"] = [("partner_id", "in", all_child.ids)]
-        return action
-
     def _compute_credit_to_invoice(self):
         # EXTENDS 'account'
         super()._compute_credit_to_invoice()
