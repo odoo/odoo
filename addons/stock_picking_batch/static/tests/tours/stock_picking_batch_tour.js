0f6e262aa1658fcb740235fab595c4388a0e59fa
Pierre Pulinckx (PIPU) <pipu@odoo.com>
2024-12-10 15:53:14 +0100

    [FIX] stock_picking_batch: test_stock_picking_batch_sm_to_sml_sync tour
    In the test_stock_picking_batch_sm_to_sml_synchronization tour, it is
    imperative to be exhaustive about whether we are in a modal or not.
    The trigger targets an element that is not in a modal when it should be.
    In this commit, we stipulate each modal for each trigger. This not only
    makes the tour more understandable but also avoids any ambiguity.
    
    runbot-error-id~109487
    
    closes odoo/odoo#190203
    
    X-original-commit: b774656f081ae9a9f9cce20388c8d8120b0405db
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>
    Signed-off-by: Pierre Pulinckx (pipu) <pipu@odoo.com>

--- addons/stock_picking_batch/static/tests/tours/stock_picking_batch_tour.js
+++ addons/stock_picking_batch/static/tests/tours/stock_picking_batch_tour.js
@@ -1,6 +1,6 @@
 import { registry } from "@web/core/registry";
 
-registry.category("web_tour.tours").add('test_stock_picking_batch_sm_to_sml_synchronization', {
+registry.category("web_tour.tours").add("test_stock_picking_batch_sm_to_sml_synchronization", {
     steps: () => [
         {
             trigger: ".btn-primary[name=action_confirm]",
@@ -11,101 +11,98 @@ registry.category("web_tour.tours").add('test_stock_picking_batch_sm_to_sml_sync
             run: "click",
         },
         {
-            trigger: "h4:contains('Transfers')",
+            content: "Check the modal 'Open: Transfers' is open",
+            trigger: ".modal h4:contains(open: transfers)",
+        },
+        {
+            content: "Click in cell to start edition",
+            trigger: ".modal:contains(open: transfers) .o_data_row > td:contains('Product A')",
             run: "click",
         },
         {
-            trigger: ".o_data_row > td:contains('Product A')",
+            trigger: ".modal:contains(open: transfers) .o_list_number > div[name=quantity] input",
+            run: "edit 7",
+        },
+        {
+            trigger: ".modal:contains(open: transfers) .fa-list",
             run: "click",
         },
         {
-            trigger: ".o_list_number > div[name=quantity] input",
-            run: 'edit 7',
+            trigger: ".modal:contains(detailed operations)",
         },
         {
-            trigger: ".fa-list",
+            trigger:
+                ".modal:contains(detailed operations) .o_field_pick_from > span:contains('WH/Stock/Shelf A')",
             run: "click",
         },
         {
-            trigger: "h4:contains('Detailed Operations')",
-            run: "click",
+            trigger: ".modal:contains(detailed operations) .o_list_number[name=quantity] input",
+            run: "edit 2 && press Tab",
         },
         {
-            trigger: ".o_field_pick_from > span:contains('WH/Stock/Shelf A')",
-            run: "click",
-        },
-        {
-            trigger: ".modal:not(.o_inactive_modal) .o_list_number[name=quantity] input",
-            run: 'edit 2',
-        },
-        {
-            trigger: ".o_list_footer .o_list_number > span:contains('7')",
-            run: "click",
-        },
-        {
-            trigger: ".o_list_footer .o_list_number > span:contains('8')",
+            trigger:
+                ".modal:contains(detailed operations) .o_list_footer .o_list_number > span:contains('8')",
         },
         {
             content: "Click Save",
-            trigger: ".modal:not(.o_inactive_modal) .o_form_button_save",
+            trigger: ".modal:contains(detailed operations) .o_form_button_save",
             run: "click",
         },
         {
-            trigger: ".o_data_row > td:contains('Product A')",
+            content: "Click in cell to start edition",
+            trigger: ".modal:contains(open: transfers) .o_data_row > td:contains('Product A')",
             run: "click",
         },
         {
-            trigger: ".modal .o_list_number[name=quantity] input",
-            run: 'edit 21',
+            trigger: ".modal:contains(open: transfers) .o_list_number[name=quantity] input",
+            run: "edit 21",
         },
         {
-            trigger: ".fa-list",
+            trigger: ".modal:contains(open: transfers) .fa-list",
             run: "click",
         },
         {
-            trigger: "h4:contains('Detailed Operations')",
+            trigger: "h4:contains(detailed operations)",
+        },
+        {
+            content: "Click in cell to start edition",
+            trigger:
+                ".modal:contains(detailed operations) .o_field_pick_from > span:contains('WH/Stock/Shelf A')",
             run: "click",
         },
         {
-            trigger: ".o_field_pick_from > span:contains('WH/Stock/Shelf A')",
-            run: "click",
-        },
-        {
-            trigger: ".modal:not(.o_inactive_modal) .o_list_number[name=quantity] input",
-            run: 'edit 27',
+            trigger: ".modal:contains(detailed operations) .o_list_number[name=quantity] input",
+            run: "edit 27",
         },
         {
             content: "Click Save",
-            trigger: ".modal:not(.o_inactive_modal) .o_form_button_save",
+            trigger: ".modal:contains(detailed operations) .o_form_button_save:contains(save)",
             run: "click",
         },
         {
-            trigger: ".o_data_row > td:contains('46')",
+            content: "Click in cell to start edition",
+            trigger: ".modal:contains(open: transfers) .o_data_row > td:contains(46)",
             run: "click",
         },
         {
-            trigger: ".o_field_widget[name=quantity] input",
-            run: 'edit 7',
+            trigger: ".modal:contains(open: transfers) .o_field_widget[name=quantity] input",
+            run: "edit 7",
         },
         {
-            trigger: ".fa-list",
+            trigger: ".modal:contains(open: transfers) .fa-list",
             run: "click",
         },
         {
-            trigger: ".o_list_footer .o_list_number > span:contains('7')",
+            trigger: ".modal:contains(detailed operations) .o_data_row > td:contains(7)",
         },
         {
             content: "Click Save",
-            trigger: ".modal:not(.o_inactive_modal) .o_form_button_save",
+            trigger: ".modal:contains(detailed operations) .o_form_button_save",
             run: "click",
         },
         {
-            trigger: ".modal .o_form_button_save",
+            trigger: ".modal:contains(open: transfers) .o_form_button_save",
             run: "click",
         },
-        {
-            content: "wait for save completion",
-            trigger: ".o_form_readonly, .o_form_saved",
-        },
-    ]
+    ],
 });
