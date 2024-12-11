a8e006dc6dbcb147af37a5a6fb86d5ae076e865f
fdardenne <dafl@odoo.com>
2024-12-11 10:47:04 +0100

    [FIX] alert icon is changed

--- addons/html_builder/static/src/builder/options/alert_option.js
+++ addons/html_builder/static/src/builder/options/alert_option.js
@@ -5,3 +5,19 @@ registry.category("sidebar-element-option").add("AlertOption", {
     selector: ".s_alert",
     sequence: 5,
 });
+
+registry.category("website-builder-actions").add("alertIcon", {
+    apply: ({ editingElement, param: className, value }) => {
+        const icon = editingElement.querySelector("i");
+        icon.classList.add(className);
+    },
+    clean: ({ editingElement, param: className, value }) => {
+        const icon = editingElement.querySelector("i");
+        icon.classList.remove(className);
+    },
+
+    isActive: ({ editingElement, param: className }) => {
+        const icon = editingElement.querySelector("i");
+        return icon.classList.contains(className);
+    },
+});
