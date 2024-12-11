3f82cb3b8f6ba887029f7ade39c17cbf070484e1
Victorien (viso) <viso@odoo.com>
2024-12-11 16:01:20 +0000

    [FIX] pos_restaurant: prevent multiple order send
    before this commit and since commit 42b207f, the pos_restaurant
    does not lock the state of the order button anymore.
    The has as an effect the if the order submitting takes a lot of time
    (which e.g. happens when a preparation printer is configured)
    the user could press the order button multiple times.
    
    This situation can happen both intentionally or unintentionally.
    
    The end result is a disturbed flow for users, where orders are
    sent multiple times in preparation and therefore need to be manually
    checked by the staff regularly risking wrong extra order preparation
    otherwise.
    
    This commit reverts the changes adding back the state locking the
    order button while it's being processed.
    
    opw-4242269
    
    X-original-commit: 370de95190cf07f17281f0ed6365b6806663cac2

--- addons/pos_restaurant/static/src/app/screens/product_screen/actionpad_widget/actionpad_widget.js
+++ addons/pos_restaurant/static/src/app/screens/product_screen/actionpad_widget/actionpad_widget.js
@@ -1,5 +1,6 @@
 import { patch } from "@web/core/utils/patch";
 import { ActionpadWidget } from "@point_of_sale/app/screens/product_screen/action_pad/action_pad";
+import { useState } from "@odoo/owl";
 import { TicketScreen } from "@point_of_sale/app/screens/ticket_screen/ticket_screen";
 
 /**
@@ -16,6 +17,9 @@ patch(ActionpadWidget, {
 patch(ActionpadWidget.prototype, {
     setup() {
         super.setup();
+        this.uiState = useState({
+            clicked: false,
+        });
     },
     get swapButton() {
         return (
@@ -30,9 +34,38 @@ patch(ActionpadWidget.prototype, {
                 : hasChange.count || hasChange.generalCustomerNote || hasChange.modeUpdate;
         return hasChange;
     },
+<<<<<<< saas-18.1:addons/pos_restaurant/static/src/app/screens/product_screen/actionpad_widget/actionpad_widget.js
     async submitOrder() {
         await this.pos.sendOrderInPreparationUpdateLastChange(this.currentOrder);
         this.pos.showDefault();
+||||||| c8ebc003f4a68875c3d73b17494898cac9f124e7:addons/pos_restaurant/static/src/overrides/components/product_screen/actionpad_widget/actionpad_widget.js
+    get swapButtonClasses() {
+        return {
+            "highlight btn-primary justify-content-between": this.displayCategoryCount.length,
+            "btn-light pe-none disabled justify-content-center": !this.displayCategoryCount.length,
+            altlight: !this.hasChangesToPrint && this.currentOrder?.hasSkippedChanges(),
+        };
+    },
+    submitOrder() {
+        this.pos.sendOrderInPreparationUpdateLastChange(this.currentOrder);
+=======
+    get swapButtonClasses() {
+        return {
+            "highlight btn-primary justify-content-between": this.displayCategoryCount.length,
+            "btn-light pe-none disabled justify-content-center": !this.displayCategoryCount.length,
+            altlight: !this.hasChangesToPrint && this.currentOrder?.hasSkippedChanges(),
+        };
+    },
+    async submitOrder() {
+        if (!this.uiState.clicked) {
+            this.uiState.clicked = true;
+            try {
+                await this.pos.sendOrderInPreparationUpdateLastChange(this.currentOrder);
+            } finally {
+                this.uiState.clicked = false;
+            }
+        }
+>>>>>>> ddf200a3d53effdaa45457e4f225855a36430fa7:addons/pos_restaurant/static/src/overrides/components/product_screen/actionpad_widget/actionpad_widget.js
     },
     hasQuantity(order) {
         if (!order) {
