1f511cb932b09faaccc1575159b662cabcf235ed
Pierre Lamotte (pila) <pila@odoo.com>
2024-12-11 13:44:00 +0100

    [FIX] account: allow accountants to create products
    **Issue:**
    Accountants cannot create products through Customer Invoice or Vendor Bill product lines.
    
    **Expected:**
    Accountants should be allowed to manage the products database.
    
    **Steps to reproduce:**
    - Activate Invoicing app;
    - Configure a branch to the company;
    - Create a user with an accounting `Accountant` role and set the branch company as only entry in Allowed Companies and as Default Company;
    - Log in as that new user;
    - Try create a new product through a Customer Invoice or a Vendor Bill.
    
    **Cause:**
    The `Accountant` role itself has no right on products.
    
    **Fix:**
    Add a new access right on `product_template` for group `group_account_manager`.
    
    opw-4293151
    
    closes odoo/odoo#190360
    
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/account/tests/test_product.py
+++ addons/account/tests/test_product.py
@@ -16,6 +16,11 @@ class TestProduct(AccountTestInvoicingCommon):
             login="internal_user",
             groups="base.group_user",
         )
+        cls.account_manager_user = new_test_user(
+            cls.env,
+            login="account_manager_user",
+            groups="account.group_account_manager",
+        )
 
     def test_internal_user_can_read_product_with_tax_and_tags(self):
         """Internal users need read access to products, no matter their taxes."""
@@ -52,3 +57,10 @@ class TestProduct(AccountTestInvoicingCommon):
             'taxes_id': self.company_data['company'].account_sale_tax_id.ids,
             'supplier_taxes_id': self.company_data['company'].account_purchase_tax_id.ids,
         }])
+
+    def test_account_manager_user_can_create_product(self):
+        """Test that a user with group_account_manager can create a product."""
+        product = self.env['product.product'].with_user(self.account_manager_user).create({
+            'name': 'Test Accountant', 'type': 'product', 'list_price': 50.0,
+        })
+        self.assertTrue(product)
