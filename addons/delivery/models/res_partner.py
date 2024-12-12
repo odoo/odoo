efac182b80f580e069dea511dc0e66543c02ea01
Valeriya(vchu) <vchu@odoo.com>
2024-12-12 16:31:43 +0100

    [IMP] delivery,website_sale: do not show pickup location on checkout
    Before this commit when a customer selected a pickup location a
    corresponding partner was created with `delivery` type. It was
    displayed on the checkout page along with customer's delivery addresses
    which was confusing as pickup locations are meant to be chosen only with
    the corresponding delivery method.
    This commit aims to distinguish pickup addresses partners by adding new
    field in order to prevent from displaying them on the checkout and
    computing it as a default on SO.
    
    task-4370310

--- addons/delivery/models/res_partner.py
+++ addons/delivery/models/res_partner.py
@@ -7,3 +7,4 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     property_delivery_carrier_id = fields.Many2one('delivery.carrier', company_dependent=True, string="Delivery Method", help="Used in sales orders.")
+    is_pickup_location = fields.Boolean()  # Whether it is a pickup point address.
