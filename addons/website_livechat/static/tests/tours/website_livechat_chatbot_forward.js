238ed2d713fef2a5cbcd7c71192fa600533f6582
tsm-odoo <tsm@odoo.com>
2024-12-12 15:56:11 +0100

    [IMP] im_livechat, *: remove chat bot from channel once operator is found
    *: website_livechat.
    
    Having the chat bot in the channel members feels unnecessary. This
    PR adapts the forward operator step in order to remove the chat bot
    from the channel once an operator is found.
    
    task-4354075

---
+++ addons/website_livechat/static/tests/tours/website_livechat_chatbot_forward.js
@@ -0,0 +1,26 @@
+import { registry } from "@web/core/registry";
+
+const messagesContain = (text) => `.o-livechat-root:shadow .o-mail-Message:contains("${text}")`;
+
+registry.category("web_tour.tours").add("website_livechat.chatbot_forward", {
+    url: "/",
+    steps: () => [
+        {
+            trigger: ".o-livechat-root:shadow .o-livechat-LivechatButton",
+            run: "click",
+        },
+        {
+            trigger: messagesContain("Hello, what can I do for you?"),
+        },
+        {
+            trigger: ".o-livechat-root:shadow li:contains(Forward to operator)",
+            run: "click",
+        },
+        {
+            trigger: messagesContain("I'll forward you to an operator."),
+        },
+        {
+            trigger: ".o-livechat-root:shadow .o-mail-Composer-input:enabled",
+        },
+    ],
+});
