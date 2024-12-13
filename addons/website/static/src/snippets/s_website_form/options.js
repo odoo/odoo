c316027c5343c4bd44bfef8ff8fe86a193ffc8c9
divy-odoo <divy@odoo.com>
2024-12-13 12:57:28 +0530

    [IMP] website: allow multiple values for visibility conditions
    Steps to Reproduce:
    1. Go to Website → Edit Mode.
    2. Drag and drop a "Contact Us" form.
    3. Add two new fields:
        - A selection field (e.g., "Author").
        - A text field.
    4. Attempt to set the visibility of the text field such that it only
    appears when the user selects multiple values. (e.g., "Mitchell Admin"
    and "Marc Demo").
    
    Users cannot select multiple values for visibility conditions when the
    comparator is `Contains/Doesn't Contain`, and its behavior is similar to
    `Is Equal To/Is Not Equal To`.
    
    This commit introduces support for selecting multiple values when the
    visibility comparator is set to `Contains/Doesn't Contain`, provided
    the dependency field type is `select`, `checkbox` or `radio`.
    
    task-4203938

--- addons/website/static/src/snippets/s_website_form/options.js
+++ addons/website/static/src/snippets/s_website_form/options.js
@@ -1289,6 +1289,8 @@ options.registry.WebsiteFieldEditor = FieldEditor.extend({
                 return this.$target[0].classList.contains(params.activeValue) ? params.activeValue : 'false';
             case 'renderListItems':
                 return JSON.stringify(this._getListItems(true));
+            case "renderDependencyListItems":
+                return JSON.stringify(this._getDependencyListItem());
             case 'setVisibilityDependency':
                 return this.$target[0].dataset.visibilityDependency || '';
         }
@@ -1329,6 +1331,12 @@ options.registry.WebsiteFieldEditor = FieldEditor.extend({
                     || dependencyEl.nodeName === 'TEXTAREA') && !['set', '!set'].includes(this.$target[0].dataset.visibilityComparator);
             case 'hidden_condition_no_text_opt':
                 return dependencyEl && (dependencyEl.type === 'checkbox' || dependencyEl.type === 'radio' || dependencyEl.nodeName === 'SELECT');
+            case "hidden_condition_no_text_only_opt":
+                return dependencyEl && (dependencyEl.type === "checkbox" || dependencyEl.type === "radio" || dependencyEl.nodeName === "SELECT") &&
+                    ["selected", "!selected"].includes(this.$target[0].dataset.visibilityComparator);
+            case "hidden_condition_no_text_multi_opt":
+                return dependencyEl && (dependencyEl.type === "checkbox" || dependencyEl.type === "radio" || dependencyEl.nodeName === "SELECT") &&
+                    ["contains", "!contains"].includes(this.$target[0].dataset.visibilityComparator);
             case 'hidden_condition_num_opt':
                 return dependencyEl && dependencyEl.type === 'number';
             case 'hidden_condition_text_opt':
@@ -1432,6 +1440,14 @@ options.registry.WebsiteFieldEditor = FieldEditor.extend({
         return recursiveFindCircular(dependentFieldEl, targetFieldEl);
     },
     /**
+     * Apply the we-list on the target and rebuild the input(s)
+     */
+    async renderDependencyListItems(previewMode, value, params) {
+        const values = JSON.parse(value);
+        const selectedList = values.filter(({ selected }) => selected).map(({ name }) => name);
+        this.$target[0].dataset.visibilityCondition = selectedList;
+    },
+    /**
      * @override
      */
     _renderCustomXML: async function (uiFragment) {
@@ -1484,13 +1500,9 @@ options.registry.WebsiteFieldEditor = FieldEditor.extend({
                 fieldType === "record"
             ) {
                 // Update available visibility options
-                const selectOptName =
-                    fieldType === "record"
-                        ? "hidden_condition_record_opt"
-                        : "hidden_condition_no_text_opt";
-                const selectOptEl = uiFragment.querySelectorAll(
-                    `we-select[data-name="${selectOptName}"]`,
-                )[1];
+                const selectOptEl = fieldType === "record" ?
+                    uiFragment.querySelectorAll(`we-select[data-name="hidden_condition_record_opt"]`)[1] :
+                    uiFragment.querySelector(`we-select[data-name="hidden_condition_no_text_only_opt"]`);
                 const inputContainerEl = this.$target[0];
                 const dependencyEl = this._getDependencyEl();
                 if (dependencyEl.nodeName === 'SELECT') {
@@ -1685,6 +1697,33 @@ options.registry.WebsiteFieldEditor = FieldEditor.extend({
                 selected: select ? opt.selected : opt.checked,
             };
         });
+    },
+    /**
+     *
+     * @private
+     * @returns {Array} List of values of dependency element
+     */
+    _getDependencyListItem() {
+        const dependencyEl = this._getDependencyEl();
+        if (!dependencyEl) {
+            return [];
+        }
+        const select = dependencyEl.nodeName === "SELECT";
+        const multipleInputs = ["radio", "checkbox"].includes(dependencyEl.type);
+        const visibilityCondition = this.$target[0].dataset.visibilityCondition;
+        let optionEls = [];
+        if (select) {
+            optionEls = dependencyEl.querySelectorAll("option");
+        } else if (multipleInputs) {
+            optionEls = dependencyEl.closest(".s_website_form_field")?.querySelectorAll(".s_website_form_input");
+        }
+        return Array.from(optionEls).map(el => ({
+            id: el.value,
+            name: el.value,
+            display_name: select ? el.textContent : el.labels[0]?.textContent,
+            undeletable: true,
+            selected: visibilityCondition?.includes(el.value)
+        }));
     },
     /**
      * Returns the select element if it exist else null
