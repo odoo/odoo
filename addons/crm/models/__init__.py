2b77bd52581caf4fd2f266688267cf2fc3a2be09
Nguyễn Đại Dương <daiduongnguyen2709@gmail.com>
2024-12-13 15:51:26 +0700

    [REM] crm,sale_crm: remove dead fields
    * These fields no longer use anywhere, it belongs to old feature from a
    long time ago, it should be delete in
    https://github.com/odoo/odoo/commit/877711b357c5de9c9c4827e92fdbbc94cae673fe

--- addons/crm/models/__init__.py
+++ addons/crm/models/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import res_users
 from . import calendar
 from . import crm_lead
 from . import crm_lost_reason
