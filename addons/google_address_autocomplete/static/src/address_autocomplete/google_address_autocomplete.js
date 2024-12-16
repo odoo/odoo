366a0ab0cf53540f53760d2bd0c60ccc9a908ece
Lucas Perais <lpe@odoo.com>
2024-12-16 15:38:35 +0100

    [IMP] google_address_autocomplete: field widget supports field mapping
    PR odoo/odoo#176262 implements a field that can autocomplete an address being written.
    
    This commit implements options on that field to map standard address field to actual fields
    of the model being edited.
    
    If no mapping is provided, standard address fields are the default.
    
    Follow up task-3851045

--- addons/google_address_autocomplete/static/src/address_autocomplete/google_address_autocomplete.js
+++ addons/google_address_autocomplete/static/src/address_autocomplete/google_address_autocomplete.js
@@ -4,9 +4,48 @@ import { CharField, charField } from "@web/views/fields/char/char_field";
 import { AutoComplete } from "@web/core/autocomplete/autocomplete";
 import { googlePlacesSession } from "../google_places_session";
 
+const standardAddressFields = {
+    street: {
+        label: _t("Street field"),
+        type: ["char"]
+    },
+    street2: {
+        label: _t("Additional street field"),
+        type: ["char"]
+    },
+    city: {
+        label: _t("City field"),
+        type: ["char"]
+    },
+    state_id: {
+        label: _t("State field"),
+        type: ["char", "many2one"]
+    },
+    zip: {
+        label: _t("Zip field"),
+        type: ["char"]
+    },
+    country_id: {
+        label: _t("Country field"),
+        type: ["char", "many2one"]
+    }
+}
+
 export class AddressAutoComplete extends CharField {
     static template = "google_address_autocomplete.AddressAutoCompleteTemplate";
     static components = { AutoComplete, ...CharField.components };
+
+    static props = {...CharField.props,
+        addressFieldMap: {
+            type: Object,
+            optional: true,
+        }
+    }
+
+    static defaultProps = {
+        ...CharField.defaultProps,
+        addressFieldMap: {},
+    }
 
     get sources() {
         return [
@@ -49,24 +88,30 @@ export class AddressAutoComplete extends CharField {
             country_id: "country",
             state_id: "state",
         };
-        const fieldsToUpdate = ["street", "street2", "city", "state_id", "zip", "country_id"];
+        const fieldsToUpdate = Object.keys(standardAddressFields);
 
         const activeFields = this.props.record.activeFields;
         const fields = this.props.record.fields;
+        const addressFieldMap = this.props.addressFieldMap;
 
         const valuesToUpdate = {};
         const rest = [];
         fieldsToUpdate.forEach((fieldName) => {
             const addressField = fieldToDetail[fieldName] || fieldName;
             let value = address[addressField];
-            if (fieldName in activeFields) {
-                valuesToUpdate[fieldName] = value || false;
-            } else if (!(fieldName in fields)) {
+
+            const recordFieldName = addressFieldMap[fieldName] || fieldName;
+            if (recordFieldName in activeFields) {
+                if (Array.isArray(value) && fields[recordFieldName].type !== "many2one") {
+                    value = value[1];
+                }
+                valuesToUpdate[recordFieldName] = value || false;
+            } else if (!(recordFieldName in fields)) {
                 value = Array.isArray(value) ? value[1] : value;
                 rest.push(value);
             }
         });
-        if (!fieldsToUpdate.includes(this.props.name) && rest.length) {
+        if (!fieldsToUpdate.includes(this.props.name) && !(this.props.name in valuesToUpdate) && rest.length) {
             valuesToUpdate[this.props.name] = rest.join(" ");
         }
         this.props.record.update(valuesToUpdate);
@@ -78,5 +123,29 @@ export const addressAutoComplete = {
     component: AddressAutoComplete,
     displayName: _t("Address AutoComplete"),
     supportedTypes: ["char"],
+    supportedOptions: [
+        ...charField.supportedOptions,
+        ...Object.entries(standardAddressFields).map(([fname, data]) => {
+            return {
+                label: data.label,
+                placeholder: fname,
+                type : "field",
+                name: fname,
+                availableTypes: data.type,
+            }
+        })
+    ],
+    extractProps: ({ attrs, options }) => {
+        const props = charField.extractProps({attrs, options});
+        const addressFieldMap = {};
+        Object.keys(standardAddressFields).forEach((fname) => {
+            const optionValue = options[fname];
+            if (optionValue) {
+                addressFieldMap[fname] = optionValue;
+            }
+        });
+        props.addressFieldMap = addressFieldMap;
+        return props;
+    }
 };
 registry.category("fields").add("google_address_autocomplete", addressAutoComplete);
