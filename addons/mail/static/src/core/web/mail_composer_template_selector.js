8d901a21ad3941d3329f4f9908d0fb23c39307cc
Julien Banken <jbn@odoo.com>
2024-12-16 18:47:02 +0100

    [IMP] mail: simplify mail template selector in full mail composer
    Currently, users can overwrite or delete templates directly from the
    template selector in the full mail composer. However, managing templates
    this way can lead to accidental overwrites or deletions, especially when
    users rely solely on template names to make selection. This is particularly
    problematic when multiple templates share the same name, as there is no
    preview available to confirm their choice.
    
    To address this issue, we will remove the overwrite and delete options
    from the template selector. Instead, we will provide a link to the mail
    template list view. From there, they can preview, edit, or delete
    templates as needed. This approach significantly reduces the risk of
    accidental overwrites or deletions.
    
    task-4365019
    
    closes odoo/odoo#190513
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mail/static/src/core/web/mail_composer_template_selector.js
+++ addons/mail/static/src/core/web/mail_composer_template_selector.js
@@ -1,12 +1,10 @@
 import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
-import { sprintf } from "@web/core/utils/strings";
 import { standardFieldProps } from "@web/views/fields/standard_field_props";
 import { user } from "@web/core/user";
 import { useService } from "@web/core/utils/hooks";
 
 import { Component, useState, onWillStart } from "@odoo/owl";
-import { ConfirmationDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 import { Dropdown } from "@web/core/dropdown/dropdown";
 import { DropdownItem } from "@web/core/dropdown/dropdown_item";
 import { SelectCreateDialog } from "@web/views/view_dialogs/select_create_dialog";
@@ -20,8 +18,12 @@ export class MailComposerTemplateSelector extends Component {
     setup() {
         this.action = useService("action");
         this.orm = useService("orm");
-        this.state = useState({});
-        this.limit = 7;
+        this.limit = 80;
+
+        const { context } = this.props.record.evalContext;
+        this.state = useState({
+            hideMailTemplateManagementOptions: context?.hide_mail_template_management_options,
+        });
 
         onWillStart(() => {
             this.fetchTemplates();
@@ -54,50 +56,6 @@ export class MailComposerTemplateSelector extends Component {
         });
     }
 
-    /**
-     * @param {Object} template
-     * @param {integer} template.id
-     * @param {string} template.display_name
-     */
-    async onDeleteTemplate(template) {
-        this.env.services.dialog.add(ConfirmationDialog, {
-            body: sprintf(_t('Are you sure you want to delete "%(template_name)s" for all users?\nOnce done, there is no going back!'), {
-                template_name: template.display_name,
-            }),
-            confirmLabel: _t("Delete Template"),
-            confirm: async () => {
-                await this.orm.unlink("mail.template", [template.id]);
-                this.state.templates = this.state.templates.filter(current => {
-                    return current.id !== template.id;
-                });
-            },
-            cancelLabel: _t("No, keep it"),
-            cancel: () => {},
-            title: _t("Delete Email Template"),
-        });
-    }
-
-    /**
-     * @param {Object} template
-     * @param {integer} template.id
-     * @param {string} template.display_name
-     */
-    async onOverwriteTemplate(template) {
-        this.env.services.dialog.add(ConfirmationDialog, {
-            body: sprintf(_t('Are your sure you want to update "%(template_name)s"?'), {
-                template_name: template.display_name,
-            }),
-            confirmLabel: _t("Update Template"),
-            confirm: async () => {
-                await this.orm.write("mail.template", [template.id], {
-                    subject: this.props.record.data.subject,
-                    body_html: this.props.record.data.body,
-                });
-            },
-            cancel: () => {},
-        });
-    }
-
     async onSaveTemplate() {
         if (!(await this.props.record.save())) {
             return;
@@ -110,48 +68,27 @@ export class MailComposerTemplateSelector extends Component {
         });
     }
 
+    async onManageTemplateBtnClick() {
+        const action = await this.action.loadAction("mail.action_email_template_tree_all");
+        action.context = {
+            search_default_my_templates: 1,
+            search_default_model: this.props.record.data.model,
+            default_model: this.props.record.data.model,
+            default_user_id: user.userId,
+        };
+        this.action.doAction(action);
+    }
+
     onSelectTemplateSearchMoreBtnClick() {
         this.env.services.dialog.add(SelectCreateDialog, {
             resModel: "mail.template",
-            title: _t("Insert Templates"),
+            title: _t("Select a Template"),
             multiSelect: false,
             noCreate: true,
             domain: [["model", "=", this.props.record.data.render_model]],
             onSelected: async templateIds => {
                 await this.props.record.update({
                     template_id: templateIds
-                });
-            },
-        });
-    }
-
-    onDeleteTemplateSearchMoreBtnClick() {
-        this.env.services.dialog.add(SelectCreateDialog, {
-            resModel: "mail.template",
-            title: _t("Delete Template"),
-            multiSelect: true,
-            noCreate: true,
-            domain: [["model", "=", this.props.record.data.render_model]],
-            onSelected: async templateIds => {
-                await this.orm.unlink("mail.template", templateIds);
-                this.state.templates = this.state.templates.filter(current => {
-                    return !templateIds.includes(current.id);
-                });
-            },
-        });
-    }
-
-    onOverwriteTemplateSearchMoreBtnClick() {
-        this.env.services.dialog.add(SelectCreateDialog, {
-            resModel: "mail.template",
-            title: _t("Overwrite Template"),
-            multiSelect: false,
-            noCreate: true,
-            domain: [["model", "=", this.props.record.data.render_model]],
-            onSelected: async templateIds => {
-                await this.orm.write("mail.template", templateIds, {
-                    subject: this.props.record.data.subject,
-                    body_html: this.props.record.data.body,
                 });
             },
         });
