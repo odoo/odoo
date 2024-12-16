933b1511e89d1e9714bf5fff9d3f8ef1a58c3f3f
Giorgio Tocco (gito) <gito@odoo.com>
2024-12-16 17:47:25 +0100

    [IMP] im_livechat, *: prioritize livechat operators with expertise
    * = test_discuss_full, web
    
    This commit introduces expertise tags for the Live Chat feature.
    Each user can now have specific Live Chat Expertise tags.
    
    In the "Forward to Operator" chatbot step expertise tags can be defined.
    
    While language remains the primary filter, expertise tags allow further
    filtering of operators according to the user's needs.
    
    task-4354297
    
    closes odoo/odoo#189349
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/im_livechat/models/res_users.py
+++ addons/im_livechat/models/res_users.py
@@ -12,15 +12,32 @@ class ResUsers(models.Model):
 
     livechat_username = fields.Char(string='Livechat Username', compute='_compute_livechat_username', inverse='_inverse_livechat_username', store=False)
     livechat_lang_ids = fields.Many2many('res.lang', string='Livechat Languages', compute='_compute_livechat_lang_ids', inverse='_inverse_livechat_lang_ids', store=False)
+    livechat_expertise_ids = fields.Many2many(
+        "im_livechat.expertise",
+        string="Live Chat Expertise",
+        compute="_compute_livechat_expertise_ids",
+        inverse="_inverse_livechat_expertise_ids",
+        store=False,
+        help="When forwarding live chat conversations, the chatbot will prioritize users with matching expertise.",
+    )
     has_access_livechat = fields.Boolean(compute='_compute_has_access_livechat', string='Has access to Livechat', store=False, readonly=True)
 
     @property
     def SELF_READABLE_FIELDS(self):
-        return super().SELF_READABLE_FIELDS + ['livechat_username', 'livechat_lang_ids', 'has_access_livechat']
+        return super().SELF_READABLE_FIELDS + [
+            "has_access_livechat",
+            "livechat_expertise_ids",
+            "livechat_lang_ids",
+            "livechat_username",
+        ]
 
     @property
     def SELF_WRITEABLE_FIELDS(self):
-        return super().SELF_WRITEABLE_FIELDS + ['livechat_username', 'livechat_lang_ids']
+        return super().SELF_WRITEABLE_FIELDS + [
+            "livechat_expertise_ids",
+            "livechat_lang_ids",
+            "livechat_username",
+        ]
 
     @api.depends('res_users_settings_id.livechat_username')
     def _compute_livechat_username(self):
@@ -42,6 +59,17 @@ class ResUsers(models.Model):
             settings = self.env['res.users.settings']._find_or_create_for_user(user)
             settings.livechat_lang_ids = user.livechat_lang_ids
 
+    @api.depends("res_users_settings_id.livechat_expertise_ids")
+    def _compute_livechat_expertise_ids(self):
+        for user in self:
+            user.livechat_expertise_ids = user.res_users_settings_id.livechat_expertise_ids
+
+    def _inverse_livechat_expertise_ids(self):
+        for user in self:
+            settings = self.env["res.users.settings"]._find_or_create_for_user(user)
+            settings.livechat_expertise_ids = user.livechat_expertise_ids
+
+    @api.depends("groups_id")
     def _compute_has_access_livechat(self):
         for user in self.sudo():
             user.has_access_livechat = user.has_group('im_livechat.im_livechat_group_user')
