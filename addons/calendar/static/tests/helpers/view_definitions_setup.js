834c9356b1d0e1b9ce3738699c437c92cd414fab
Rahul Prajapati <rapr@odoo.com>
2024-12-17 18:42:14 +0530

    [IMP] calendar: convert qunit tests to hoot
    This commit aims to convert remaining QUnit tests to hoot.
    
    Part of task-3818666
    
    closes odoo/odoo#190855
    
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/calendar/static/tests/helpers/view_definitions_setup.js
+++
@@ -1,6 +0,0 @@
-import { registry } from '@web/core/registry';
-
-const viewArchsRegistry = registry.category('bus.view.archs');
-const calendarArchsRegistry = viewArchsRegistry.category('calendar');
-
-calendarArchsRegistry.add('default', '<calendar date_start="start"/>');
