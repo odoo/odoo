6859fcd612312d7a768740e53e4cbbf2f77fadc5
tau <tau@odoo.com>
2024-12-17 16:40:58 +0800

    [ADD] l10n_tw_edi_ecpay_website_sale
    Passing values from ecommerce to invoice for Taiwan E-invoice

---
+++ addons/l10n_tw_edi_ecpay_website_sale/__manifest__.py
@@ -0,0 +1,27 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Taiwan - E-invoicing Ecommerce",
+    "version": "1.0",
+    "category": "Website Sale/Localizations/EDI",
+    "summary": """ECpay E-invoice bridge module for Ecommerce""",
+    "description": """
+        This bridge module allows the user to input Ecpay information in ecommerce for sending their invoices to the Ecpay system
+    """,
+    "website": "https://www.odoo.com",
+    "license": "LGPL-3",
+    "depends": [
+        "website_sale",
+        "l10n_tw_edi_ecpay",
+    ],
+    "data": [
+        "views/sale_order_view.xml",
+        "views/templates.xml"
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            "l10n_tw_edi_ecpay_website_sale/static/src/**/*"
+        ]
+    },
+    "auto_install": True,
+}
