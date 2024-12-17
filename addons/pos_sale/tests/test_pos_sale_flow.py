8efba41b8d7f984b309f469a710422fd133493ba
Pierre Lamotte (pila) <pila@odoo.com>
2024-12-17 17:30:13 +0100

    [FIX] sale: remove draft indicator for pos down payment
    ## Versions:
    16.0
    17.0
    No fix needed for saas-17.2 as per https://github.com/odoo/odoo/commit/5542bfd48fad3b89e3e54c9534da2e4963a90d54#diff-9d184722ff87da4215bc50fa2e539c0f6c2f099c831aa398e593220cf1325640
    https://github.com/odoo/odoo/commit/ba954604e5291ef206d58f621a815a515917d988
    
    ## Issue:
    A `(Draft)` text appears on sale orders lines when a down payment is done from the `Point of Sale` app.
    This text keeps appearing during the entire process, even on the invoice.
    
    ## Expected:
    A down payment made from the PoS app should not be a draft as it has been paid. The trailing text should not appear.
    
    ## Steps to reproduce:
    - Activate Sales and Point Of Sale apps;
    - Ensure, in the PoS settings, the shop you will use has a down payment product setup (`Down Payment (POS)`;
    - Create a new `Quotation` from the `Sales` app with a product available in the PoS;
    - Got to the shop view (open a session) and click on the `Quotation/Order` button;
    - Select the lately created order and `Apply a down payment` with any value;
    - Proceed payment with any method and navigate to backend;
    - Go back to the quotation and look at the notebook.
    
    ## Cause:
    `(Draft)` text is added at the end of the down payment product's line as the `dp_state` is equal to `'draft'`.
    
    opw-4380985

--- addons/pos_sale/tests/test_pos_sale_flow.py
+++ addons/pos_sale/tests/test_pos_sale_flow.py
@@ -622,3 +622,42 @@ class TestPoSSale(TestPointOfSaleHttpCommon):
         # Confirm all invoices
         sale_order.invoice_ids.action_post()
         self.assertEqual(sale_order.order_line[1].price_unit, 10)
+
+    def test_downpayment_line_name(self):
+        product_a = self.env['product.product'].create({
+            'name': 'Product A',
+            'available_in_pos': True,
+            'type': 'product',
+            'lst_price': 100.0,
+            'taxes_id': [],
+        })
+        partner_test = self.env['res.partner'].create({'name': 'Test Partner'})
+
+        sale_order = self.env['sale.order'].create({
+            'partner_id': partner_test.id,
+            'order_line': [(0, 0, {
+                'product_id': product_a.id,
+                'name': product_a.name,
+                'product_uom_qty': 1,
+                'product_uom': product_a.uom_id.id,
+                'price_unit': product_a.lst_price,
+            })],
+        })
+        sale_order.action_confirm()
+
+        downpayment_product = self.env['product.product'].create({
+            'name': 'Down Payment',
+            'available_in_pos': True,
+            'type': 'service',
+            'taxes_id': [],
+        })
+        self.main_pos_config.write({
+            'down_payment_product_id': downpayment_product.id,
+        })
+        self.main_pos_config.open_ui()
+        self.start_tour("/pos/ui?config_id=%d" % self.main_pos_config.id, 'PoSDownPaymentAmount', login="accountman")
+
+        downpayment_line_pos = sale_order.order_line.filtered('is_downpayment')
+        self.assertTrue(downpayment_line_pos)
+        self.assertNotIn('(draft)', downpayment_line_pos.name.lower())
+        self.assertNotIn('(canceled)', downpayment_line_pos.name.lower())
