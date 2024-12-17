f23891ab73b3c222d5621f2f88984bf50f96959b
tau <tau@odoo.com>
2024-12-17 08:40:58 +0000

    [ADD] l10n_tw_edi_ecpay_website_sale
    Passing values from ecommerce to invoice for Taiwan E-invoice
    
    closes odoo/odoo#236870
    
    X-original-commit: 9a8350cb476af73d6bf4eea75344963eac1c7831
    Signed-off-by: Nicolas Viseur (vin) <vin@odoo.com>
    Signed-off-by: Tommy Au (tau) <tau@odoo.com>

---
+++ addons/l10n_tw_edi_ecpay_website_sale/models/sale_order.py
@@ -0,0 +1,36 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    l10n_tw_edi_is_print = fields.Boolean(string="Print")
+    l10n_tw_edi_love_code = fields.Char(string="Love Code")
+    l10n_tw_edi_carrier_type = fields.Selection(
+        string="Carrier Type",
+        selection=[
+            ("1", "ECpay e-invoice carrier"),
+            ("2", "Citizen Digital Certificate"),
+            ("3", "Mobile Barcode"),
+            ("4", "EasyCard"),
+            ("5", "iPass"),
+        ],
+    )
+    l10n_tw_edi_carrier_number = fields.Char(string="Carrier Number")
+    l10n_tw_edi_carrier_number_2 = fields.Char(string="Carrier Number 2")
+
+    def _prepare_invoice(self):
+        res = super()._prepare_invoice()
+        if self.company_id.country_id.code == 'TW' and self.company_id._is_ecpay_enabled():
+            res.update(
+                {
+                    "l10n_tw_edi_is_print": self.l10n_tw_edi_is_print,
+                    "l10n_tw_edi_love_code": self.l10n_tw_edi_love_code,
+                    "l10n_tw_edi_carrier_type": self.l10n_tw_edi_carrier_type,
+                    "l10n_tw_edi_carrier_number": self.l10n_tw_edi_carrier_number,
+                    "l10n_tw_edi_carrier_number_2": self.l10n_tw_edi_carrier_number_2,
+                }
+            )
+        return res
