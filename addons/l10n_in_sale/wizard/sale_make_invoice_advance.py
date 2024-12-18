747ec7d622549d5579823aa4670099a6cbb431b8
hatr-odoo <hatr@odoo.com>
2024-12-18 14:49:25 +0530

    [REM] l10n_in_*: remove l10n_in_purchase and gst_treatment
    This commit removes l10n_in_purchase module. Also GST Treatment field and
    unwanted HSN Code on report has been removed from l10n_in_sale module. Along
    with this the logic for sending GST Treatment while preparing invoice from
    l10n_in_pos and l10n_in_sale has also been removed and rather this will be
    handled directly on account_invoice, on l10n_in, using compute method on
    l10n_in_gst_treatment.
    
    Additionally, the logic for compute method for l10n_in_gst_treatment on
    account_invoice in l10n_in has also been fixed.
    
    Before this Commit:
    The current logic assigns 'Registered' treatment to an invoice if GSTIN is
    present on the partner. However, this approach incorrectly assigns 'Registered'
    treatment even when the GSTIN is invalid (e.g., '/'), which is undesirable.
    
    After this Commit:
    GST treatment will be set to 'Registered' only if the GSTIN is validated.
    If the GSTIN is invalid or absent, the treatment will be set by default to
    'Consumer'.
    
    task-4410142

--- addons/l10n_in_sale/wizard/sale_make_invoice_advance.py
+++ addons/l10n_in_sale/wizard/sale_make_invoice_advance.py
@@ -9,8 +9,6 @@ class SaleAdvancePaymentInv(models.TransientModel):
 
     def _prepare_invoice_values(self, order, so_line, accounts):
         res = super()._prepare_invoice_values(order, so_line, accounts)
-        if order.country_code == 'IN':
-            res['l10n_in_gst_treatment'] = order.l10n_in_gst_treatment
         if order.l10n_in_reseller_partner_id:
             res['l10n_in_reseller_partner_id'] = order.l10n_in_reseller_partner_id
         return res
