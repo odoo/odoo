2e113fe33407c939a767700b56bad6b88b67a0f6
David Monnom (moda) <moda@odoo.com>
2024-12-18 10:31:51 +0000

    [FIX] pos_self_order, pos_*: fix when sending order to PDIS from kiosk
    *: pos_online_payment_self_order
    
    When creating an order from the kiosk and pay it at the counter, it
    should be sent to PDIS when creating it.
    
    opw-4364309
    
    closes odoo/odoo#191023
    
    X-original-commit: 6f708d84ae764e039f133c1a03929d728b10c5f8
    Related: odoo/enterprise#75832
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/pos_self_order/static/src/app/pages/confirmation_page/confirmation_page.js
+++ addons/pos_self_order/static/src/app/pages/confirmation_page/confirmation_page.js
@@ -75,9 +75,9 @@ export class ConfirmationPage extends Component {
         );
         this.confirmedOrder = order;
 
-        const paymentMethods = this.selfOrder.models["pos.payment.method"].filter(
-            (p) => p.is_online_payment
-        );
+        const paymentMethods = this.selfOrder.filterPaymentMethods(
+            this.selfOrder.models["pos.payment.method"].getAll()
+        ); // Stripe, Adyen, Online
 
         if (
             !order ||
