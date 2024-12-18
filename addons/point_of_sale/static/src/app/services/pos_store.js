6ecebf0ba144984122524730a828325e082fdcce
Max Whale <mawh@odoo.com>
2024-12-18 09:09:26 +0000

    [FIX] point_of_sale: scale customer display fixes
    Since #176062, the customer display is able
    to mirror the scale measurements. This commit
    fixes some bugs with this feature:
    - A traceback when no order is selected (this can
      happen when returning to the plan view in a
      restaurant)
    - The net weight not updating correctly on the
      customer display
    - The scale values remaining on the customer
      display indefinitely if weighing is cancelled
    
    opw-4343571, opw-4343559
    
    X-original-commit: dd4e90602d7f2a323fa5dfacfe7df6c438c875e0

--- addons/point_of_sale/static/src/app/services/pos_store.js
+++ addons/point_of_sale/static/src/app/services/pos_store.js
@@ -800,10 +800,9 @@ export class PosStore extends WithLazyGetterTrap {
                     ScaleScreen,
                     this.scaleData
                 );
-                if (!weight) {
-                    return;
+                if (weight) {
+                    values.qty = weight;
                 }
-                values.qty = weight;
                 this.isScaleScreenVisible = false;
                 this.scaleWeight = 0;
                 this.scaleTare = 0;
