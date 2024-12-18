a5ad99b6566d48b1e3a8c93940bd45e65d912647
Victor Piryns (pivi) <pivi@odoo.com>
2024-12-18 15:39:39 +0000

    [PERF] iap_crm, website_crm_iap_reveal: missing index on `reveal_id`
    Description:
    ir_cron_crm_reveal_lead (executed by OdooBot)
    ⤷ _process_lead_generation
      ⤷ _perform_reveal_service
        ⤷ _create_lead_from_response
          ⤷ `already_created_lead = self.env['crm.lead'].search([
         ('reveal_id', '=', result['clearbit_id'])])`
             ⤷ `Seq.Scan` on `crm.lead`
    
    Also `search(...)` -> `search_count(..., limit=1)` as it's just an
    existence check, searching for all instances is not necessary.
    
    Benchmark:
    On a database with over 8-digits count of `crm.lead`:
    
    |               | Before | After    |
    |---------------|--------|----------|
    | Timings (hot) | 6.4 s  | 0.041 ms |
    | Buffers usage | 2.6 GB | 32KB     |
    
    Reference:
    task-3977976
    
    closes odoo/odoo#192397
    
    X-original-commit: 4c41f25a2314947c25bca4badb1bb9df9f2c0248
    Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>
    Signed-off-by: Piryns Victor (pivi) <pivi@odoo.com>

--- addons/iap_crm/models/crm_lead.py
+++ addons/iap_crm/models/crm_lead.py
@@ -7,7 +7,7 @@ from odoo import fields, models
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
-    reveal_id = fields.Char(string='Reveal ID') # Technical ID of reveal request done by IAP
+    reveal_id = fields.Char(string='Reveal ID', index='btree_not_null') # Technical ID of reveal request done by IAP
 
     def _merge_get_fields(self):
         return super()._merge_get_fields() + ['reveal_id']
