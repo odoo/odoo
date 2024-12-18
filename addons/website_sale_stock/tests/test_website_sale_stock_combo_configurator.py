16294bb6ce665a745dda5daa861a71139092a342
Louis (loti) <loti@odoo.com>
2024-12-18 11:43:59 +0100

    [IMP] product,*sale*: add tests for combo logic
    This change:
    - Adds tests for the combo models,
    - Adds tests for the combo configurator,
    - Aligns the product configurator tests with the combo configurator ones.
    
    task-4320163

---
+++ addons/website_sale_stock/tests/test_website_sale_stock_combo_configurator.py
@@ -0,0 +1,35 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo.fields import Command
+from odoo.tests import HttpCase, tagged
+
+from odoo.addons.website_sale_stock.tests.common import WebsiteSaleStockCommon
+
+
+@tagged('post_install', '-at_install')
+class TestWebsiteSaleStockComboConfigurator(HttpCase, WebsiteSaleStockCommon):
+
+    def test_website_sale_stock_combo_configurator(self):
+        product = self._create_product(
+            name="Test product",
+            is_storable=True,
+            allow_out_of_stock_order=False,
+        )
+        self.env['stock.quant'].create({
+            'product_id': product.id,
+            'location_id': self.warehouse.lot_stock_id.id,
+            'quantity': 2,
+        })
+        combo = self.env['product.combo'].create({
+            'name': "Test combo",
+            'combo_item_ids': [
+                Command.create({'product_id': product.id}),
+                Command.create({'product_id': self._create_product().id}),
+            ],
+        })
+        self._create_product(
+            name="Combo product",
+            type='combo',
+            combo_ids=[Command.link(combo.id)]
+        )
+        self.start_tour('/', 'website_sale_stock_combo_configurator')
