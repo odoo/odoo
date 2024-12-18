e08c270a773634ae6046d45ce595472f33d20609
Adrien Dieudonné <adr@odoo.com>
2024-12-18 15:39:52 +0100

    [FIX] web: don't show tooltip on button press on small screen
    On small screens:
    Before this commit, the tooltip (often used as hint) was
    appearing when you touch a button on small screens. This was
    a bit annoying when the button opens a dropdown because the tooltip
    was on top of it.
    
    To avoid this, we now open this toolip after a long press (250 ms).
    It will disappear at the next 'touch' event or if you directly press on
    the button itself.
    
    On Desktop:
    The tooltip is now closed if you click on the button.
    
    Steps to reproduce:
    On a small screen :
    - Go to Sales
    - Open any quotation
    - Tap on the cog menu

--- addons/web/static/src/core/tooltip/tooltip_service.js
+++ addons/web/static/src/core/tooltip/tooltip_service.js
@@ -39,17 +39,17 @@ import { whenReady } from "@odoo/owl";
  * with "info" being a stringified object with two keys "x" and "y".
  */
 
-const OPEN_DELAY = 400;
-const CLOSE_DELAY = 200;
+export const OPEN_DELAY = 400;
+export const CLOSE_DELAY = 200;
+export const SHOW_AFTER_DELAY = 250;
 
 export const tooltipService = {
     dependencies: ["popover"],
     start(env, { popover }) {
         let openTooltipTimeout;
         let closeTooltip;
+        let showTimer;
         let target = null;
-        let touchPressed;
-        let mouseEntered;
         const elementsWithTooltips = new WeakMap();
 
         /**
@@ -75,9 +75,6 @@ export const tooltipService = {
             }
             if (!document.body.contains(target)) {
                 return true; // target is no longer in the DOM
-            }
-            if (hasTouch() && !mouseEntered) {
-                return !touchPressed;
             }
             return false;
         }
@@ -133,10 +130,11 @@ export const tooltipService = {
             if (el.nodeType === Node.TEXT_NODE) {
                 return;
             }
+            const element = el.closest("[data-tooltip], [data-tooltip-template]");
             if (elementsWithTooltips.has(el)) {
                 openTooltip(el, elementsWithTooltips.get(el));
-            } else if (el.matches("[data-tooltip], [data-tooltip-template]")) {
-                const dataset = el.dataset;
+            } else if (element) {
+                const dataset = element.dataset;
                 const params = {
                     tooltip: dataset.tooltip,
                     template: dataset.tooltipTemplate,
@@ -148,7 +146,7 @@ export const tooltipService = {
                 if (dataset.tooltipDelay) {
                     params.delay = parseInt(dataset.tooltipDelay, 10);
                 }
-                openTooltip(el, params);
+                openTooltip(element, params);
             }
         }
 
@@ -160,13 +158,11 @@ export const tooltipService = {
          * @param {MouseEvent} ev a "mouseenter" event
          */
         function onMouseenter(ev) {
-            mouseEntered = true;
             openElementsTooltip(ev.target);
         }
 
-        function onMouseleave(ev) {
-            if (target === ev.target) {
-                mouseEntered = false;
+        function cleanupTooltip(ev) {
+            if (target === ev.target.closest("[data-tooltip], [data-tooltip-template]")) {
                 cleanup();
             }
         }
@@ -178,8 +174,11 @@ export const tooltipService = {
          * @param {TouchEvent} ev a "touchstart" event
          */
         function onTouchStart(ev) {
-            touchPressed = true;
-            openElementsTooltip(ev.target);
+            cleanup();
+
+            showTimer = browser.setTimeout(() => {
+                openElementsTooltip(ev.target);
+            }, SHOW_AFTER_DELAY);
         }
 
         whenReady(() => {
@@ -194,17 +193,18 @@ export const tooltipService = {
                 document.body.addEventListener("touchstart", onTouchStart);
 
                 document.body.addEventListener("touchend", (ev) => {
-                    if (ev.target.matches("[data-tooltip], [data-tooltip-template]")) {
+                    if (ev.target.closest("[data-tooltip], [data-tooltip-template]")) {
                         if (!ev.target.dataset.tooltipTouchTapToShow) {
-                            touchPressed = false;
+                            browser.clearTimeout(showTimer);
+                            browser.clearTimeout(openTooltipTimeout);
                         }
                     }
                 });
-
                 document.body.addEventListener("touchcancel", (ev) => {
-                    if (ev.target.matches("[data-tooltip], [data-tooltip-template]")) {
+                    if (ev.target.closest("[data-tooltip], [data-tooltip-template]")) {
                         if (!ev.target.dataset.tooltipTouchTapToShow) {
-                            touchPressed = false;
+                            browser.clearTimeout(showTimer);
+                            browser.clearTimeout(openTooltipTimeout);
                         }
                     }
                 });
@@ -213,7 +213,8 @@ export const tooltipService = {
             // Listen (using event delegation) to "mouseenter" events to open the tooltip if any
             document.body.addEventListener("mouseenter", onMouseenter, { capture: true });
             // Listen (using event delegation) to "mouseleave" events to close the tooltip if any
-            document.body.addEventListener("mouseleave", onMouseleave, { capture: true });
+            document.body.addEventListener("mouseleave", cleanupTooltip, { capture: true });
+            document.body.addEventListener("click", cleanupTooltip, { capture: true });
         });
 
         return {
