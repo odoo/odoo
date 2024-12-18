1b3da9bb34731c2a2dc3e434c7bfa34cc513c5a1
rmun-odoo <rmun@odoo.com>
2024-12-18 05:44:19 +0000

    [FIX] hr_holidays: allow date modification for unapproved time off requests
    Steps to Reproduce:
    • Install the Time Off app.
    • Create a new user and corresponding employee without any group in Time Off.
    • Create a new time off request with a start date < today's date.
    • Attempt to change the date, which results in a validation error.
    
    Issue:
    - Users are unable to modify time off requests, even if they are not yet
      approved.
    
    Fix:
    - Added a check to ensure that modifications are allowed for time off requests
      that are not in an approved state.
    
    task-4236572
    
    X-original-commit: ff07c308c658eb6e46d90f6fa50c925e4dd165b9

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -757,7 +757,8 @@ Attempting to double-book your time off won't magically make your vacation 2x be
     def write(self, values):
         is_officer = self.env.user.has_group('hr_holidays.group_hr_holidays_user') or self.env.is_superuser()
         if not is_officer and values.keys() - {'attachment_ids', 'supported_attachment_ids', 'message_main_attachment_id'}:
-            if any(hol.date_from.date() < fields.Date.today() and hol.employee_id.leave_manager_id != self.env.user for hol in self):
+            if any(hol.date_from.date() < fields.Date.today() and hol.employee_id.leave_manager_id != self.env.user
+                   and hol.state not in ('confirm', 'draft') for hol in self):
                 raise UserError(_('You must have manager rights to modify/validate a time off that already begun'))
             if any(leave.state == 'cancel' for leave in self):
                 raise UserError(_('Only a manager can modify a canceled leave.'))
