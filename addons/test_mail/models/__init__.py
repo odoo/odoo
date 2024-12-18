69969f3924616e20378a73f63a29ee23f3d00b18
Thibault Delavallée <tde@odoo.com>
2024-12-18 09:49:15 +0100

    [IMP] test_mail: add lead-like email flow
    PURPOSE
    
    Add an email discussion like flow, on a lead-like model. This allows to
    test a full discussion with some real-life use cases and see how it
    behaves in current codebase.
    
    NEXT
    
    This is done in 18 in order to assert behavior and also ease future
    changes. Indeed email flow is going to change in between Odoo 18 and
    19 and it is a good practice to have a flow for the "current behavior"
    not only the new one.
    
    Prepares Task-4281157: [mail] Allow to alter email.message To
    Prepares Task-4332797: [mail] Partner from emails 3.0
    Prepares Task-4286232: [mail] Store email{cc/to} on mail.message
    Prepares Task-4273569: [mail] Reply, Quotes, Expand and Forward
    Prepares Task-4273479: [mail] Email-like recipients
    
    Part-of: odoo/odoo#190891
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/test_mail/models/__init__.py
+++ addons/test_mail/models/__init__.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import mail_test_access
+from . import mail_test_lead
 from . import test_mail_corner_case_models
 from . import test_mail_feature_models
 from . import test_mail_models
