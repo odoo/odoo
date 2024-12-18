723f3db6267926b6420484520b37b8a589236dcd
Aaron Bohy <aab@odoo.com>
2024-12-18 15:19:22 +0100

    [IMP] orm,base,web,*: add falsy_value_label attr on fields
    *hr_timesheet,project
    
    This commit allows to specify, on a field definition, the value to
    display when that field is not set. That value is then used by the
    webclient in various views, when grouped, as display name of the
    group representing the `false` value (instead of "None", which is
    the default).
    
    Previously, to achieve this, one had to define custom views
    (js_class) for each view type, and to override the adequate
    function. The "logic" was thus replicated in all views where we
    wanted the customization. Now, it is defined at a single place, and
    automatically applied to all views (kanban, list, pivot, graph +
    map in enterprise).
    
    This commit takes the benefit of the new attribute in project,
    where there were overrides for all these view types for the task
    model, to display "Private" when grouped by `project_id` and
    "Unnassigned" when grouped by `user_ids`. A lot of js_class have
    thus been removed.
    
    This commit also factorizes the group displayName logic in kanban
    and list by moving it to the RelationalModel. The group datapoint
    already had a `displayName` property, but it wasn't computed
    correctly (it was not even always a string), so some additional
    processing was required in both list and kanban. We now apply this
    logic directly in the model, s.t. the displayName of a group is now
    always a string and can be used directly by the views.
    
    Task~4410227
    
    closes odoo/odoo#190719
    
    Related: odoo/enterprise#75678
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/project/static/tests/project_task_groupby.test.js
+++ addons/project/static/tests/project_task_groupby.test.js
@@ -27,60 +27,6 @@ beforeEach(() => {
     ];
 });
 
-test("project.task (tree): check group label for no project", async () => {
-    await mountView({
-        resModel: "project.task",
-        type: "list",
-        arch: `<list js_class="project_task_list"/>`,
-        groupBy: ["project_id"],
-    });
-    expect(".o_group_name").toHaveText("🔒 Private (1)");
-});
-
-test("project.task (tree): check group label for no assignees", async () => {
-    await mountView({
-        resModel: "project.task",
-        type: "list",
-        arch: `<list js_class="project_task_list"/>`,
-        groupBy: ["user_ids"],
-    });
-    expect(".o_group_name").toHaveText("👤 Unassigned (1)");
-});
-
-test("project.task (tree): check group label for no deadline", async () => {
-    await mountView({
-        resModel: "project.task",
-        type: "list",
-        arch: `<list js_class="project_task_list"/>`,
-        groupBy: ["date_deadline"],
-    });
-    expect(".o_group_name").toHaveText("None (1)");
-});
-
-test("project.task (kanban): check group label for no project", async () => {
-    await mountView({
-        ...kanbanViewParams,
-        groupBy: ["project_id"],
-    });
-    expect(".o_column_title").toHaveText("🔒 Private\n(1)");
-});
-
-test("project.task (kanban): check group label for no assignees", async () => {
-    await mountView({
-        ...kanbanViewParams,
-        groupBy: ["user_ids"],
-    });
-    expect(".o_column_title").toHaveText("👤 Unassigned\n(1)");
-});
-
-test("project.task (kanban): check group label for no deadline", async () => {
-    await mountView({
-        ...kanbanViewParams,
-        groupBy: ["date_deadline"],
-    });
-    expect(".o_column_title").toHaveText("None\n(1)");
-});
-
 test("project.task (kanban): Can create stage if we are in tasks of specific project", async () => {
     await mountView({
         ...kanbanViewParams,
@@ -110,43 +56,4 @@ test("project.task (kanban): Cannot create stage if we are not in tasks of speci
     expect(".o_kanban_add_column").toHaveCount(0, {
         message: "Add column button should not be visible",
     });
-});
-
-test("project.task (pivot): check group label for no project", async () => {
-    await mountView({
-        resModel: "project.task",
-        type: "kanban",
-        arch: `
-            <pivot js_class="project_pivot">
-                <field name="project_id" type="row"/>
-            </pivot>
-        `,
-    });
-    expect("tr:nth-of-type(2) .o_pivot_header_cell_closed").toHaveText("Private");
-});
-
-test("project.task (pivot): check group label for no assignees", async () => {
-    await mountView({
-        resModel: "project.task",
-        type: "kanban",
-        arch: `
-            <pivot js_class="project_pivot">
-                <field name="user_ids" type="row"/>
-            </pivot>
-        `,
-    });
-    expect("tr:nth-of-type(2) .o_pivot_header_cell_closed").toHaveText("Unassigned");
-});
-
-test("project.task (pivot): check group label for no deadline", async () => {
-    await mountView({
-        resModel: "project.task",
-        type: "kanban",
-        arch: `
-            <pivot js_class="project_pivot">
-                <field name="date_deadline" type="row"/>
-            </pivot>
-        `,
-    });
-    expect("tr:nth-of-type(2) .o_pivot_header_cell_closed").toHaveText("None");
 });
