8636f8b61ee123a4aa7c588c9202cb3e768ef436
Louis Travaux <lotr@odoo.com>
2024-12-18 10:25:18 +0000

    [FIX] hw_drivers: always use pi as home directory
    To allow compatibility between all db versions and new IoT Box images, we need
    to ensure that `path_file` method returns the path starting from `/home/pi`
    instead of the path of the service user (which is `/home/odoo` in newer images).
    
    closes odoo/odoo#191149
    
    X-original-commit: 3a63c90fff615b70881131e11d7375af2ae082a6
    Signed-off-by: Yaroslav Soroko (yaso) <yaso@odoo.com>

--- addons/hw_drivers/iot_handlers/drivers/KeyboardUSBDriver_L.py
+++ addons/hw_drivers/iot_handlers/drivers/KeyboardUSBDriver_L.py
@@ -181,7 +181,7 @@ class KeyboardUSBDriver(Driver):
                 - variant (str): An optional key to represent the variant of the
                                  selected layout
         """
-        file_path = Path.home() / 'odoo-keyboard-layouts.conf'
+        file_path = helpers.path_file('odoo-keyboard-layouts.conf')
         if file_path.exists():
             data = json.loads(file_path.read_text())
         else:
@@ -193,7 +193,7 @@ class KeyboardUSBDriver(Driver):
         """Read the layout from the saved filed and set it as current layout.
         If no file or no layout is found we use 'us' by default.
         """
-        file_path = Path.home() / 'odoo-keyboard-layouts.conf'
+        file_path = helpers.path_file('odoo-keyboard-layouts.conf')
         if file_path.exists():
             data = json.loads(file_path.read_text())
             layout = data.get(self.device_identifier, {'layout': 'us'})
@@ -213,7 +213,7 @@ class KeyboardUSBDriver(Driver):
         scanner_name = ['barcode', 'scanner', 'reader']
         is_scanner = any(x in device_name for x in scanner_name) or self.dev.interface_protocol == '0'
 
-        file_path = Path.home() / 'odoo-keyboard-is-scanner.conf'
+        file_path = helpers.path_file('odoo-keyboard-is-scanner.conf')
         if file_path.exists():
             data = json.loads(file_path.read_text())
             is_scanner = data.get(self.device_identifier, {}).get('is_scanner', is_scanner)
@@ -255,7 +255,7 @@ class KeyboardUSBDriver(Driver):
         We need that in order to keep the selected type of device after a reboot.
         """
         is_scanner = {'is_scanner': data.get('is_scanner')}
-        file_path = Path.home() / 'odoo-keyboard-is-scanner.conf'
+        file_path = helpers.path_file('odoo-keyboard-is-scanner.conf')
         if file_path.exists():
             data = json.loads(file_path.read_text())
         else:
