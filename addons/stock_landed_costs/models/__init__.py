c19355be3dce8760d64a4b31d0580168bd58a62c
lase@odoo.com <lase@odoo.com>
2024-12-19 14:39:23 +0100

    [FIX] stock_landed_costs: avoid uom user error from valid landed cost lines
    Issue:
    
    Trying to validate a receipt related to a landed cost account line for
    a product using a uom from a different uom category will raise a
    userError that do not allow you to validate the receipt.
    
    Steps to reproduce:
    
    - Create a product invoiced on delivered quantities using an UOM like kg
    - Create a PO and a bill for it, post the bill
    - Receipt half the quantity
    - Add a landed cost product on the PO, create a second bill for this
    landed cost product. Create and validate the landed cost.
    - Try to validate the receipt for the second half quantity.
    > UserError: The unit of measure Units defined on the order line doesn't
    > belong to the same category as the unit of measure kg defined on the
    > product.
    
    Cause of the issue:
    
    Since Commit 32543ce9356f228d087d17a0ead1e0a80449e5de
    The landed_cost account lines related to a given stock move are also
    considered in the `_get_price_unit` call of stock moves:
    https://github.com/odoo/odoo/pull/185889/commits/daa5e91799265f2541ae0befa5b4910559c27736#diff-d601e0945d548cd4d323375bcb53addfbb594bd198424a9b6aac0651064ecaccL60-R60
    However, the uom of related to the landed cost account line might not
    match the uom category of the product related to the move. As such, this
    line will raise a UserError:
    https://github.com/odoo/odoo/blob/da1f44c2296486c0267a7bdbbe3a314ada3aa679/addons/purchase_stock/models/stock_move.py#L70
    https://github.com/odoo/odoo/blob/da1f44c2296486c0267a7bdbbe3a314ada3aa679/addons/uom/models/uom_uom.py#L223-L227
    
    Revert Commit 32543ce9356f228d087d17a0ead1e0a80449e5de
    Authored by @Whenrow
    
    opw-4389366

--- addons/stock_landed_costs/models/__init__.py
+++ addons/stock_landed_costs/models/__init__.py
@@ -6,5 +6,6 @@ from . import purchase
 from . import res_company
 from . import res_config_settings
 from . import stock_landed_cost
+from . import stock_move
 from . import account_move
 from . import stock_valuation_layer
