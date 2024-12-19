4be3c4d1585588ac7d0644bd7226c4a66cef2b74
flotchet <mafl@odoo.com>
2024-12-19 11:11:35 +0100

    s

--- addons/hr_base/models/ir_ui_view.py
+++ addons/hr_base/models/ir_ui_view.py
@@ -7,7 +7,7 @@ class IrUiView(models.Model):
 
     def _postprocess_view(self, node, model_name, editable=True, node_info=None, **options):
         model = self.env[model_name]
-        country_code = 'country_code' if 'country_code' in model._fields else self.env.company.country_id.code
+        country_code = 'country_code' if 'country_code' in model._fields else f"'{self.env.company.country_id.code}'"
         for elem in node.xpath('.//*[@l10n]'):
             l10n_attr_value = elem.get('l10n').upper()
             if not  len(l10n_attr_value) == 2 and l10n_attr_value.isalpha():
@@ -16,6 +16,6 @@ class IrUiView(models.Model):
             existing_condition = ''
             if rule := elem.get(modifier):
                 existing_condition = '(' + rule + ') and '
-            elem.set(modifier, existing_condition + f"'{country_code}' != '{l10n_attr_value}'")
+            elem.set(modifier, existing_condition + f"{country_code} != '{l10n_attr_value}'")
 
         return super()._postprocess_view(node, model_name, editable=editable, node_info=node_info, **options)
