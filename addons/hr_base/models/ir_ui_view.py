10bd66ef796bff02208f05dd04f4252907775879
flotchet <mafl@odoo.com>
2024-12-19 11:49:09 +0100

    e

--- addons/hr_base/models/ir_ui_view.py
+++ addons/hr_base/models/ir_ui_view.py
@@ -12,11 +12,9 @@ class IrUiView(models.Model):
             l10n_attr_value = elem.get('l10n').upper()
             if len(l10n_attr_value) != 2 or not l10n_attr_value.isalpha():
                 raise ValidationError(_('l10n attribute must be a valid 2 letter country code e.g. "BE"'))
-            modifier = 'column_invisible' if elem.tag == 'field' and self.type == 'list' else 'invisible'
-            existing_condition = ''
-            if rule := elem.get(modifier):
-                existing_condition = '(' + rule + ') and '
-            print(existing_condition + f"{country_code} != '{l10n_attr_value}'")
-            elem.set(modifier, existing_condition + f"{country_code} != '{l10n_attr_value}'")
+            invisible_modifier = 'column_invisible' if elem.tag == 'field' and self.type == 'list' else 'invisible'
+            rule = elem.get(invisible_modifier)
+            invisible_condition = f'{country_code} != \'{l10n_attr_value}\'' + (f' and ({rule})' if rule else '')
+            elem.set(invisible_modifier, invisible_condition)
 
         return super()._postprocess_view(node, model_name, editable=editable, node_info=node_info, **options)
