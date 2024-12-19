739ba6b990968f9789d32a634e32d8f47aa030a6
flotchet <mafl@odoo.com>
2024-12-19 11:24:37 +0100

    v

--- addons/hr_base/models/ir_ui_view.py
+++ addons/hr_base/models/ir_ui_view.py
@@ -10,7 +10,7 @@ class IrUiView(models.Model):
         country_code = 'country_code' if 'country_code' in model._fields else f"'{self.env.company.country_id.code}'"
         for elem in node.xpath('.//*[@l10n]'):
             l10n_attr_value = elem.get('l10n').upper()
-            if not  len(l10n_attr_value) == 2 and l10n_attr_value.isalpha():
+            if len(l10n_attr_value) != 2 or not l10n_attr_value.isalpha():
                 raise ValidationError(_('l10n attribute must be a valid 2 letter country code e.g. "BE"'))
             modifier = 'column_invisible' if elem.tag == 'field' and self.type == 'list' else 'invisible'
             existing_condition = ''
