8dd4b8d227ad496f9df8b4175d03dbe5b6c0f8a9
Krzysztof Magusiak (krma) <krma@odoo.com>
2024-12-19 16:45:39 +0000

    [FIX] core: one2many field domains
    The domain for inverses is not always correct and may crash for computed
    fields.
    This commit just takes over the tests from the original fix which is
    already applied on this branch.
    
    task-4380712

--- odoo/addons/test_new_api/models/test_new_api.py
+++ odoo/addons/test_new_api/models/test_new_api.py
@@ -1256,19 +1256,30 @@ class Test_New_ApiModel_Many2one_Reference(models.Model):
 
     res_model = fields.Char('Resource Model')
     res_id = fields.Many2oneReference('Resource ID', model_field='res_model')
+    const = fields.Boolean(default=True)
 
 
 class Test_New_ApiInverse_M2o_Ref(models.Model):
     _name = 'test_new_api.inverse_m2o_ref'
     _description = 'dummy m2oref inverse model'
 
-    model_ids = fields.One2many('test_new_api.model_many2one_reference', 'res_id', string="Models")
+    model_ids = fields.One2many(
+        'test_new_api.model_many2one_reference', 'res_id',
+        string="Models", domain=[('const', '=', True)])
     model_ids_count = fields.Integer("Count", compute='_compute_model_ids_count')
+    model_computed_ids = fields.One2many(
+        'test_new_api.model_many2one_reference',
+        string="Models Computed",
+        compute='_compute_model_computed_ids',
+    )
 
     @api.depends('model_ids')
     def _compute_model_ids_count(self):
         for rec in self:
             rec.model_ids_count = len(rec.model_ids)
+
+    def _compute_model_computed_ids(self):
+        self.model_computed_ids = []
 
 
 class Test_New_ApiModel_Child_M2o(models.Model):
