c2b3965b01b38a558c86dcdd0ac958bca5bac8e2
David Monnom (moda) <moda@odoo.com>
2024-12-19 10:41:47 +0100

    [IMP] point_of_sale, pos_restaurant: Different UX improvements
    Point of sale:
    - Adding margin left on preset time on product screen.
    - Replacing ActionPad button "action" by 3 vertical dots
    - Allow to reprint the last prepration ticket
    - Adding number of orders by pages on ticket screen.
    - Removing seconds on the time of the order.
    - Resizing search bar on ticket screen.
    
    Preparation display:
    - Adding zoom parameter on the navbar
    - Removing waiter name.
    - Adding order floating name.
    - Adding preset name and time on orders.
    
    taskId: 4398826

--- addons/point_of_sale/static/src/app/screens/ticket_screen/ticket_screen.js
+++ addons/point_of_sale/static/src/app/screens/ticket_screen/ticket_screen.js
@@ -23,6 +23,8 @@ import { fuzzyLookup } from "@web/core/utils/search";
 import { parseUTCString } from "@point_of_sale/utils";
 import { useTrackedAsync } from "@point_of_sale/app/hooks/hooks";
 import { OrderDisplay } from "@point_of_sale/app/components/order_display/order_display";
+import { makeAwaitable } from "@point_of_sale/app/utils/make_awaitable_dialog";
+import { NumberPopup } from "@point_of_sale/app/components/popups/number_popup/number_popup";
 
 const NBR_BY_PAGE = 30;
 
@@ -67,6 +69,7 @@ export class TicketScreen extends Component {
         });
 
         this.state = useState({
+            nbrByPage: NBR_BY_PAGE,
             page: 1,
             nbrPage: 1,
             filter: null,
@@ -84,6 +87,20 @@ export class TicketScreen extends Component {
             // Show updated list of synced orders when going back to the screen.
             this.onFilterSelected(this.state.filter);
         });
+    }
+    async onClickPageNbr() {
+        const nbr = await makeAwaitable(this.dialog, NumberPopup, {
+            title: _t("Number by page"),
+            isValid: (value) => value > 0 && value <= 100,
+        });
+
+        if (nbr && !isNaN(nbr)) {
+            this.state.nbrByPage = parseInt(nbr);
+            this.state.page = 1;
+            if (this.state.filter == "SYNCED") {
+                await this._fetchSyncedOrders();
+            }
+        }
     }
     onPresetSelected(preset) {
         if (this.state.selectedPreset === preset) {
@@ -368,19 +385,19 @@ export class TicketScreen extends Component {
 
         if (this.state.filter === "SYNCED") {
             return sortOrders(orders).slice(
-                (this.state.page - 1) * NBR_BY_PAGE,
-                this.state.page * NBR_BY_PAGE
+                (this.state.page - 1) * this.state.nbrByPage,
+                this.state.page * this.state.nbrByPage
             );
         } else {
             this.pos.ticketScreenState.totalCount = orders.length;
             return sortOrders(orders, true).slice(
-                (this.state.page - 1) * NBR_BY_PAGE,
-                this.state.page * NBR_BY_PAGE
+                (this.state.page - 1) * this.state.nbrByPage,
+                this.state.page * this.state.nbrByPage
             );
         }
     }
     getDate(order) {
-        return formatDateTime(parseUTCString(order.date_order));
+        return formatDateTime(parseUTCString(order.date_order), { format: "dd/mm/yyyy HH:mm" });
     }
     getTotal(order) {
         return this.env.utils.formatCurrency(order.getTotalWithTax());
@@ -452,14 +469,14 @@ export class TicketScreen extends Component {
         };
     }
     getNbrPages() {
-        return Math.ceil(this.pos.ticketScreenState.totalCount / NBR_BY_PAGE);
+        return Math.ceil(this.pos.ticketScreenState.totalCount / this.state.nbrByPage);
     }
     getPageNumber() {
         if (!this.pos.ticketScreenState.totalCount) {
             return `0/0`;
         } else {
-            return `${(this.state.page - 1) * NBR_BY_PAGE + 1}-${Math.min(
-                this.state.page * NBR_BY_PAGE,
+            return `${(this.state.page - 1) * this.state.nbrByPage + 1}-${Math.min(
+                this.state.page * this.state.nbrByPage,
                 this.pos.ticketScreenState.totalCount
             )} / ${this.pos.ticketScreenState.totalCount}`;
         }
@@ -716,7 +733,7 @@ export class TicketScreen extends Component {
             {
                 config_id,
                 domain,
-                limit: 30,
+                limit: this.state.nbrByPage,
                 offset,
             }
         );
