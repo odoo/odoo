c063b1f9698bf87cded7a23ccb1ca267197aafb1
flotchet <mafl@odoo.com>
2024-12-19 10:50:18 +0100

    s

--- addons/hr_base/models/ir_ui_view.py
+++ addons/hr_base/models/ir_ui_view.py
@@ -7,7 +7,7 @@ class IrUiView(models.Model):
     def _postprocess_view(self, node, model_name, editable=True, node_info=None, **options):
         model = self.env[model_name]
     
-        for elem in root.xpath('.//*[@l10n]'):
+        for elem in node.xpath('.//*[@l10n]'):
             l10n_attr_value = self._convert_l10n(elem.get('l10n')).upper()
             assert len(l10n_attr_value) == 2 and l10n_attr_value.isalpha(), 'l10n attribute must be a valid 2 letter country code e.g. "BE"'
             existing_condition = ''
