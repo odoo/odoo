95a3905e1094c75897e45abebd68b7312329924d
flotchet <mafl@odoo.com>
2024-12-19 10:40:34 +0100

    [ADD] hr_base: Add l10n to view

---
+++ addons/hr_base/__manifest__.py
@@ -0,0 +1,12 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'HR - L10N',
+    'icon': '/account/static/description/l10n.png',
+    'version': '1.0',
+    'description': """
+    """,
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'license': 'LGPL-3',
+}
---
+++ addons/hr_base/models/ir_ui_view.py
@@ -0,0 +1,18 @@
+from odoo import models
+
+
+class IrUiView(models.Model):
+    _inherit = ['ir.ui.view']
+
+    def _postprocess_view(self, node, model_name, editable=True, node_info=None, **options):
+        model = self.env[model_name]
+    
+        for elem in root.xpath('.//*[@l10n]'):
+            l10n_attr_value = self._convert_l10n(elem.get('l10n')).upper()
+            assert len(l10n_attr_value) == 2 and l10n_attr_value.isalpha(), 'l10n attribute must be a valid 2 letter country code e.g. "BE"'
+            existing_condition = ''
+            if rule := elem.get(modifier):
+                existing_condition = '(' + rule + ') and '
+            elem.set(modifier, existing_condition + f"'{self.env.company.country_id.code}' != '{l10n_attr_value}'")
+
+        return super()._postprocess_view(node, model_name, editable=editable, node_info=node_info, **options)        
