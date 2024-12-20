8d705c5ed605c50a7f891f7cd19a08df486d575a
Krzysztof Magusiak (krma) <krma@odoo.com>
2024-12-20 11:03:14 +0000

    [IMP] orm: Domain optimization transform `=` into `in`
    '=' is no longer a default operator. Transform all '=' into 'in'.
    This allows to simplify handling of operators when generating queries
    and benefit from optimizations of the 'in' operators.
    
    All domains `[(x, '=', value)]` become `[(x, 'in', [value])]`.
    
    The `Field.search` methods may not be immediately compatible with this
    change. These methods will be reviewed in the next commit and a
    temporary compatiblity layer is added for searchable fields.
    
    task-4150178

--- odoo/addons/base/tests/test_expression.py
+++ odoo/addons/base/tests/test_expression.py
@@ -1657,13 +1657,13 @@ class TestQueries(TransactionCase):
                 "res_partner"."active" IS TRUE
                 AND "res_partner"."name" LIKE %s
                 AND (
-                    "res_partner"."country_id" = %s OR (
-                        "res_partner"."ref" != %s OR
+                    "res_partner"."country_id" IN %s OR (
+                        "res_partner"."ref" NOT IN %s OR
                         "res_partner"."ref" IS NULL
                     )
                 )
             )
-            ORDER BY "res_partner"."complete_name"asc,"res_partner"."id"desc
+            ORDER BY "res_partner"."complete_name" ASC, "res_partner"."id" DESC
         ''']):
             Model.search(domain)
 
@@ -1746,7 +1746,7 @@ class TestQueries(TransactionCase):
         with self.assertQueries(['''
             SELECT COUNT(*)
             FROM "res_country"
-            WHERE "res_country"."id" = %s
+            WHERE "res_country"."id" IN %s
         ''']):
             Model.search_count([('id', '=', 1)])
 
@@ -1771,7 +1771,7 @@ class TestQueries(TransactionCase):
             LEFT JOIN "res_partner" AS "res_users__partner_id" ON
                 ("res_users"."partner_id" = "res_users__partner_id"."id")
             WHERE "res_users"."active" IS TRUE
-            AND ("res_users"."id" = %s AND "res_users__partner_id"."id" = %s)
+            AND ("res_users"."id" IN %s AND "res_users__partner_id"."id" IN %s)
             ORDER BY "res_users__partner_id"."name", "res_users"."login"
         ''']):
             Model.search([])
@@ -1844,7 +1844,7 @@ class TestMany2one(TransactionCase):
         with self.assertQueries(['''
             SELECT "res_partner"."id"
             FROM "res_partner"
-            WHERE "res_partner"."company_id" = %s
+            WHERE "res_partner"."company_id" IN %s
             ORDER BY "res_partner"."complete_name"asc,"res_partner"."id"desc
         ''']):
             self.Partner.search([('company_id', '=', self.company.id)])
@@ -2278,7 +2278,7 @@ class TestOne2many(TransactionCase):
                             AND "res_partner_bank"."sanitized_acc_number" LIKE %s
                         )
                     )
-                    AND ("res_partner"."name" != %s OR "res_partner"."name" IS NULL)
+                    AND ("res_partner"."name" NOT IN %s OR "res_partner"."name" IS NULL)
                     AND "res_partner"."parent_id" IS NOT NULL
                 )
             )
--- odoo/orm/fields.py
+++ odoo/orm/fields.py
@@ -1206,12 +1206,6 @@ class Field(typing.Generic[T]):
             return SQL("%s%s%s", sql_field, SQL_OPERATORS[operator], value)
 
         # operator: in (equality)
-        equal_operator = None
-        if operator in ('=', '!='):
-            equal_operator = operator
-            operator = 'in' if operator == '=' else 'not in'
-            value = [value]
-
         if operator in ('in', 'not in'):
             assert isinstance(value, COLLECTION_TYPES), \
                 f"condition_to_sql() 'in' operator expects a collection, not a {value!r}"
@@ -1227,11 +1221,7 @@ class Field(typing.Generic[T]):
 
             sql = None
             if params:
-                if equal_operator:
-                    assert len(params) == 1
-                    sql = SQL("%s%s%s", sql_field, SQL_OPERATORS[equal_operator], params[0])
-                else:
-                    sql = SQL("%s%s%s", sql_field, SQL_OPERATORS[operator], params)
+                sql = SQL("%s%s%s", sql_field, SQL_OPERATORS[operator], params)
 
             if (operator == 'in') == null_in_condition:
                 # field in {val, False} => field IN vals OR field IS NULL
