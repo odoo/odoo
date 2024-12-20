120879e9417f5f7a1e450d3d3f24b12b83e69b3d
tsm-odoo <tsm@odoo.com>
2024-12-20 08:56:00 +0100

    [FIX] im_livechat: assign operator according to lang
    Before this PR, the `forward_operator` chat bot step was not taking the
    user lang into account. It uses the `livechat_visitor_id` field which is
    not available in im_livechat. It should instead use the lang from the
    context.
    
    At the same time, this PR fixes an issue when restarting the chat bot:
    the lang of the first message following a restart is incorrect. It
    occurs because the lang is not properly propagated.
    
    task-4411088
    opw-4341694
    
    closes odoo/odoo#191243
    
    X-original-commit: 9ef1d2e2d691b2a57f8c12e0cfdfd6bf356370ce
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

---
+++ addons/website_livechat/static/tests/tours/website_livechat_chatbot_fw_operator_matching_lang.js
@@ -0,0 +1,19 @@
+/** @odoo-module */
+
+import { registry } from "@web/core/registry";
+
+registry.category("web_tour.tours").add("chatbot_fw_operator_matching_lang", {
+    test: true,
+    steps: () => [
+        {
+            trigger: ".o-livechat-root:shadow .o-mail-Message:contains('Hello! I'm a bot!')",
+        },
+        {
+            trigger: ".o-livechat-root:shadow li:contains(I want to speak with an operator)",
+            run: "click",
+        },
+        {
+            trigger: ".o-livechat-root:shadow .o-mail-Composer-input:enabled",
+        },
+    ],
+});
