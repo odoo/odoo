40dbe03af0fa6925df60e0cdd68468b5fe6d02ea
tsm-odoo <tsm@odoo.com>
2024-12-23 08:46:49 +0000

    [FIX] im_livechat: chatbot rules with same regex url
    Before this PR, chatbot rules with the same target URL could be
    ignored, leading to missing chat bot in some cases.
    
    Steps to reproduce:
    - Setup chatbot A on url "/".
    - Setup chatbot B on url "/". Only enable it if there aren't any
    available operators.
    - Ensure no operator is connected.
    - Go to the "/" URL as a visitor.
    - No chat bot is present.
    
    This occurs because the first rule is returned then discarded since
    there aren't any operators. We should instead ensure that the second
    one is used a fallback. This PR corrects the `match_rule` method to do
    so.
    
    opw-4352046
    
    closes odoo/odoo#191781
    
    X-original-commit: ccb693a6403c553061b5428ec949294b80470498
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/im_livechat/models/im_livechat_channel.py
+++ addons/im_livechat/models/im_livechat_channel.py
@@ -462,8 +462,15 @@ class Im_LivechatChannelRule(models.Model):
             for rule in rules:
                 # url might not be set because it comes from referer, in that
                 # case match the first rule with no regex_url
-                if re.search(rule.regex_url or '', url or ''):
-                    return rule
+                if not re.search(rule.regex_url or "", url or ""):
+                    continue
+                if rule.chatbot_script_id and (
+                    not rule.chatbot_script_id.active or not rule.chatbot_script_id.script_step_ids
+                ):
+                    continue
+                if rule.chatbot_only_if_no_operator and rule.channel_id.available_operator_ids:
+                    continue
+                return rule
             return False
         # first, search the country specific rules (the first match is returned)
         if country_id: # don't include the country in the research if geoIP is not installed
