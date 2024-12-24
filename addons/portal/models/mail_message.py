7a19b9571e258a420f040845f717b02270ffe8fd
Thibault Delavallée <tde@odoo.com>
2024-12-24 09:03:10 +0100

    [REF] mail, portal: simplify attachment / msg controller access check
    Code is currently using a mix of code defined on attachment controller,
    thread controller, and message model. This leads to pong-pong style
    code hard to follow when trying to understand what it does.
    
    Instead, just have two controllers, _can_edit_message and _can_delete_attachment
    that are correctly defined and overridden.
    
    Prepares Task-4430903: [mail] Stop aufollow madness !
    Prepares Task-4273479: [mail] Email-like recipients
    
    Part-of: odoo/odoo#191468
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/portal/models/mail_message.py
+++ addons/portal/models/mail_message.py
@@ -3,7 +3,6 @@
 from odoo import models
 from odoo.http import request
 from odoo.tools import format_datetime, groupby
-from odoo.addons.portal.utils import get_portal_partner
 
 
 class MailMessage(models.Model):
@@ -155,14 +154,3 @@ class MailMessage(models.Model):
             'video' in (attachment_values["mimetype"] or "")
             else attachment_values["mimetype"])
         return attachment_values
-
-    def _is_editable_in_portal(self, **kwargs):
-        self.ensure_one()
-        if self.model and self.res_id and self.env.user._is_public():
-            thread = request.env[self.model].browse(self.res_id)
-            partner = get_portal_partner(
-                thread, kwargs.get("hash"), kwargs.get("pid"), kwargs.get("token")
-            )
-            if partner and self.author_id == partner:
-                return True
-        return False
