3d94972201e83ef48527a720bf981333350e9fda
Lionel Piraux (lipi) <lipi@odoo.com>
2024-12-26 15:44:44 +0000

    [IMP] sale: display unpublished combo's item images
    Similarly to other types of products, combo products must be published
    to make them available in eCommerce. However, combo item products (i.e.
    the different choices in a combo) don't need to be published.
    Unfortunately, if a product is unpublished, public/portal users don't
    have access to its images, so they won't be shown in the combo
    configurator.
    
    This is a bad UX, as public/portal users are expected to make a product
    selection, but we don't show them the corresponding images.
    
    This commits uses the `src` attribute of the `img` tag to show the raw
    combo item's image instead of using the image url, bypassing access
    checks.
    
    This is done only for combo items that are not accessible to users.
    
    task-4337641
    
    closes odoo/odoo#201275
    
    X-original-commit: 6fe373c83615e518e2d870ae858771c713e0c965
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>
    Signed-off-by: Lionel Piraux (lipi) <lipi@odoo.com>

--- addons/website_sale/controllers/combo_configurator.py
+++ addons/website_sale/controllers/combo_configurator.py
@@ -1,6 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo.http import route
+from odoo.tools.image import image_data_uri
 
 from odoo.addons.sale.controllers.combo_configurator import (
     SaleComboConfiguratorController,
@@ -31,3 +32,20 @@ class WebsiteSaleComboConfiguratorController(SaleComboConfiguratorController, We
     def website_sale_combo_configurator_get_price(self, *args, **kwargs):
         self._populate_currency_and_pricelist(kwargs)
         return super().sale_combo_configurator_get_price(*args, **kwargs)
+
+    def _get_combo_item_data(
+        self, combo, combo_item, selected_combo_item, date, currency, pricelist, **kwargs
+    ):
+        data = super()._get_combo_item_data(
+            combo, combo_item, selected_combo_item, date, currency, pricelist, **kwargs
+        )
+        # To sell a product type 'combo', one doesn't need to publish all combo choices. This causes
+        # an issue when public users access the image of each choice via the /web/image route. To
+        # bypass this access check, we send the raw image URL if the product is inaccessible to the
+        # current user.
+        if (
+            not combo_item.product_id.sudo(False).has_access('read')
+            and combo_item.product_id.image_128
+        ):
+            data['product']['image_src'] = image_data_uri(combo_item.product_id.image_128)
+        return data
