beda85d67318025ce492cd0dd3a4d3a71e82d49e
Lionel Piraux (lipi) <lipi@odoo.com>
2024-12-26 15:44:44 +0000

    [IMP] sale: display unpublished combo's item images
    Similarly to other types of products, combo products must be published
    to make them available in eCommerce. However, combo item products (i.e.
    the different choices in a combo) don't need to be published.
    Unfortunately, if a product is unpublished, public/portal users don't
    have access to its images, so they won't be shown in the combo
    configurator.
    
    This is a bad UX, as public/portal users are expected to make a product
    selection, but we don't show them the corresponding images.
    
    This commits uses the `src` attribute of the `img` tag to show the raw
    combo item's image instead of using the image url, bypassing access
    checks.
    
    This is done only for combo items that are not accessible to users.
    
    task-4337641
    
    closes odoo/odoo#199899
    
    X-original-commit: ceae03d50643a062230c4bb3f1b88d30f841d361
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>
    Signed-off-by: Lionel Piraux (lipi) <lipi@odoo.com>

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -20,6 +20,7 @@ from odoo.tools import (
     single_email_re,
     str2bool,
 )
+from odoo.tools.image import image_data_uri
 from odoo.tools.json import scriptsafe as json_scriptsafe
 from odoo.tools.translate import _
 
@@ -969,8 +970,18 @@ class WebsiteSale(payment_portal.PaymentPortal):
 
     def _get_additional_notification_information(self, line):
         # Only set the linked line id for combo items, not for optional products.
-        if line.combo_item_id:
-            return {'linked_line_id': line.linked_line_id.id}
+        if combo_item := line.combo_item_id:
+            infos = {'linked_line_id': line.linked_line_id.id}
+            # To sell a product type 'combo', one doesn't need to publish all combo choices. This
+            # causes an issue when public users access the image of each choice via the /web/image
+            # route. To bypass this access check, we send the raw image URL if the product is
+            # inaccessible to the current user.
+            if (
+                not combo_item.product_id.sudo(False).has_access('read')
+                and combo_item.product_id.image_128
+            ):
+                infos['image_url'] = image_data_uri(combo_item.product_id.image_128)
+            return infos
         return {}
 
     # ------------------------------------------------------
