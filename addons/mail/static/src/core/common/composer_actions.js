e2aa9bde762a7083c8227c424015c2c5f6e2dc38
tsm-odoo <tsm@odoo.com>
2024-12-27 13:07:29 +0000

    [FIX] im_livechat: attach file button not opening file selector
    Before this PR, the attachm file button in the composer was displayed
    even if upload was not allowed. Clicking on this button woudln't do
    anything since upload is forbidden.
    
    This PR hides the upload file action is upload is not allowed.
    
    task-4433082
    
    closes odoo/odoo#192274
    
    X-original-commit: c38f01e334fc6353fe02bb7417b5d132db72bc56
    Related: odoo/enterprise#76424
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/core/common/composer_actions.js
+++ addons/mail/static/src/core/common/composer_actions.js
@@ -100,11 +100,16 @@ composerActionsRegistry
         sequenceQuick: 20,
     })
     .add("upload-files", {
-        condition: (component) =>
-            !(
+        condition: (component) => {
+            const thread = component.thread ?? component.message?.thread;
+            if (!thread?.allow_public_upload && component.store.self.type === "guest") {
+                return false;
+            }
+            return !(
                 component.thread?.channel_type === "whatsapp" &&
                 component.props.composer.attachments.length > 0
-            ),
+            );
+        },
         icon: "fa fa-paperclip",
         name: _t("Attach Files"),
         onClick: (component, action, ev) => {
