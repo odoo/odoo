2970a2f40eea25763694e393099beb02423750ea
Erwan Martin <public@fzwte.net>
2024-12-29 18:17:47 +0100

    [IMP] l10n_fr_{account|facturx_chorus_pro}: Add new French associative chart of accounts
    This commit adds a fiscal localization to add the CoA for association
    in France.
    
    Source: https://www.associations.gouv.fr/IMG/pdf/reglt_2018-06_association.pdf
    
    Thanks to Erwin Martin - ZeWaren for the initial PR.
    
    task-4684053
    original-community-pr: https://github.com/odoo/odoo/pull/191966

--- addons/l10n_fr_account/models/template_fr_comp.py
+++ addons/l10n_fr_account/models/template_fr_comp.py
@@ -6,18 +6,19 @@ from odoo.addons.account.models.chart_template import template
 class AccountChartTemplate(models.AbstractModel):
     _inherit = 'account.chart.template'
 
-    @template('fr')
-    def _get_fr_template_data(self):
+    @template('fr_comp')
+    def _get_fr_comp_template_data(self):
         return {
-            'name': _('General accounting plan'),
-            'visible': False,
+            'name': _('Companies accounting plan'),
+            'parent': 'fr',
+            'sequence': 0,
             'code_digits': 6,
             'property_account_receivable_id': 'fr_pcg_recv',
             'property_account_payable_id': 'fr_pcg_pay',
-            'downpayment_account_id': 'pcg_4191',
+            'property_account_downpayment_categ_id': 'pcg_4191',
         }
 
-    @template('fr', 'res.company')
+    @template('fr_comp', 'res.company')
     def _get_fr_res_company(self):
         return {
             self.env.company.id: {
@@ -33,24 +34,11 @@ class AccountChartTemplate(models.AbstractModel):
                 'account_journal_early_pay_discount_gain_account_id': 'pcg_765',
                 'deferred_expense_account_id': 'pcg_486',
                 'deferred_revenue_account_id': 'pcg_487',
+                'l10n_fr_rounding_difference_loss_account_id': 'pcg_4768',
+                'l10n_fr_rounding_difference_profit_account_id': 'pcg_4778',
                 'account_sale_tax_id': 'tva_normale',
                 'account_purchase_tax_id': 'tva_acq_normale',
+                'expense_account_id': 'pcg_607_account',
+                'income_account_id': 'pcg_707_account',
             },
         }
-
-    @template('fr', 'account.journal')
-    def _get_fr_account_journal(self):
-        return {
-            'sale': {'refund_sequence': True},
-            'purchase': {'refund_sequence': True},
-        }
-
-    def _get_bank_fees_reco_account(self, company):
-        # French account for the bank fees reco model. We need to be as precise
-        # as possible in case it's modified so it's missing and not replaced.
-        fr_account = self.env['account.account'].with_company(company).search([
-            ('code', '=', '627800'),
-            ('account_type', '=', 'expense'),
-            ('name', '=', 'Other expenses and commissions on services supplied'),
-        ], limit=1)
-        return fr_account or super()._get_bank_fees_reco_account(company)
