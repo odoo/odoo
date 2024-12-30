7601116d813e15d1b24f55981458c50aa47af84f
divy-odoo <divy@odoo.com>
2024-12-30 18:38:57 +0530

    [IMP] website: alternative solution after improvement

--- addons/website/static/src/snippets/s_website_form/options.js
+++ addons/website/static/src/snippets/s_website_form/options.js
@@ -325,7 +325,7 @@ const FieldEditor = FormEditor.extend({
      */
     _getFieldName: function (fieldEl = this.$target[0]) {
         const multipleName = fieldEl.querySelector('.s_website_form_multiple');
-        return multipleName ? multipleName.dataset.name : fieldEl.querySelector('.s_website_form_input')?.name;
+        return multipleName ? multipleName.dataset.name : fieldEl.querySelector('.s_website_form_input').name;
     },
     /**
      * Returns the type of the  field, can be used for both custom and existing fields
@@ -1007,7 +1007,7 @@ options.registry.WebsiteFieldEditor = FieldEditor.extend({
      * @override
      */
     onRemove() {
-        const fieldName = this.$target[0].querySelector(".s_website_form_input")?.name;
+        const fieldName = this.$target[0].querySelector('.s_website_form_input').name;
         const isMultipleField = this.formEl.querySelectorAll(`.s_website_form_input[name="${CSS.escape(fieldName)}"]`).length > 1;
         if (isMultipleField) {
             return;
@@ -1185,7 +1185,7 @@ options.registry.WebsiteFieldEditor = FieldEditor.extend({
         // depends on the current field
         const newValuesText = valueList.map(value => value.name);
         const inputEls = this.$target[0].querySelectorAll('.s_website_form_input, option');
-        const inputName = this.$target[0].querySelector(".s_website_form_input")?.name;
+        const inputName = this.$target[0].querySelector('.s_website_form_input').name;
         for (let i = 0; i < inputEls.length; i++) {
             const input = inputEls[i];
             if (newValuesText[i] && input.value && !newValuesText.includes(input.value)) {
@@ -1427,16 +1427,26 @@ options.registry.WebsiteFieldEditor = FieldEditor.extend({
         // Get the authorized existing fields for the form model
         // Do it on each render because of custom property fields which can
         // change depending on the project selected.
-        this.existingFields = await authorizedFieldsCache.get(this.formEl, this.orm).then((fields) => {
+        this.existingFields = await authorizedFieldsCache.get(this.formEl, this.orm).then(async (fields) => {
             this.fields = {};
-            for (const [fieldName, field] of Object.entries(fields)) {
-                field.name = fieldName;
-                const fieldDomain = _getDomain(this.formEl, field.name, field.type, field.relation);
-                field.domain = fieldDomain || field.domain || [];
-                this.fields[fieldName] = field;
-            }
+            const validFields = Object.fromEntries(
+                await Promise.all(
+                    Object.entries(fields).map(async ([fieldName, field]) => {
+                        // Filter selection fields that have no records
+                        if (field.relation && field.relation !== 'ir.attachment') {
+                            const records = await this._fetchFieldRecords(field);
+                            if (records.length === 0) return null; // Exclude fields with no records
+                        }
+                        field.name = fieldName;
+                        const fieldDomain = _getDomain(this.formEl, field.name, field.type, field.relation);
+                        field.domain = fieldDomain || field.domain || [];
+                        this.fields[fieldName] = field;
+                        return [fieldName, field];
+                    })
+                ).then(results => results.filter(Boolean)) // Remove null entries
+            );
             // Create the buttons for the type we-select
-            return Object.keys(fields).map(key => {
+            return Object.keys(validFields).map(key => {
                 const field = fields[key];
                 const button = document.createElement('we-button');
                 button.textContent = field.string;
@@ -1450,7 +1460,7 @@ options.registry.WebsiteFieldEditor = FieldEditor.extend({
         for (const el of this.formEl.querySelectorAll('.s_website_form_field:not(.s_website_form_dnone)')) {
             const inputEl = el.querySelector('.s_website_form_input');
             if (el.querySelector('.s_website_form_label_content') && inputEl && inputEl.name
-                    && inputEl.name !== this.$target[0].querySelector(".s_website_form_input")?.name
+                    && inputEl.name !== this.$target[0].querySelector('.s_website_form_input').name
                     && !existingDependencyNames.includes(inputEl.name) && !this._findCircular(el)) {
                 const button = document.createElement('we-button');
                 button.textContent = el.querySelector('.s_website_form_label_content').textContent;
@@ -1578,8 +1588,8 @@ options.registry.WebsiteFieldEditor = FieldEditor.extend({
         const dataFillWith = inputEl ? inputEl.dataset.fillWith : undefined;
         const hasConditionalVisibility = this.$target[0].classList.contains('s_website_form_field_hidden_if');
         const previousInputEl = this.$target[0].querySelector('.s_website_form_input');
-        const previousName = previousInputEl?.name;
-        const previousType = previousInputEl?.type;
+        const previousName = previousInputEl.name;
+        const previousType = previousInputEl.type;
         [...this.$target[0].childNodes].forEach(node => node.remove());
         [...fieldEl.childNodes].forEach(node => this.$target[0].appendChild(node));
         [...fieldEl.attributes].forEach(el => this.$target[0].removeAttribute(el.nodeName));
@@ -1589,8 +1599,8 @@ options.registry.WebsiteFieldEditor = FieldEditor.extend({
         }
         const dependentFieldEls = this.formEl.querySelectorAll(`.s_website_form_field[data-visibility-dependency="${CSS.escape(previousName)}"]`);
         const newFormInputEl = this.$target[0].querySelector('.s_website_form_input');
-        const newName = newFormInputEl?.name;
-        const newType = newFormInputEl?.type;
+        const newName = newFormInputEl.name;
+        const newType = newFormInputEl.type;
         if ((previousName !== newName || previousType !== newType) && dependentFieldEls) {
             // In order to keep the visibility conditions consistent,
             // when the name has changed, it means that the type has changed so
