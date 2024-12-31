1525626ec1a8e9dd68d8eeb0fd17de0428bebec2
abd-msyukyu-odoo <abd@odoo.com>
2024-12-31 13:33:46 +0100

    [IMP] html_editor: optimize `childNodes` and `children` usages
    Usage of native `Element.children` and `Node.childNodes` is computationally
    expensive because they are `live` collections, and accessing items in such
    collections has a worst case computational complexity of `O(n)`. Therefore in
    cases where multiple elements are accessed and it is known that childNodes won't
    change during the operation, it is best to generate a static list of children by
    manually going through `Node.firstChild` and then its `nextSibling`s.
    
    This commit is a non-exhaustive update to try and remove usages of such
    functions for better overall performances.
    
    task-4294440

--- addons/html_editor/static/src/core/clipboard_plugin.js
+++ addons/html_editor/static/src/core/clipboard_plugin.js
@@ -131,7 +131,7 @@ export class ClipboardPlugin extends Plugin {
         // Repair the copied range.
         if (clonedContents.firstChild.nodeName === "LI") {
             const list = selection.commonAncestorContainer.cloneNode();
-            list.replaceChildren(...clonedContents.childNodes);
+            list.replaceChildren(...childNodes(clonedContents));
             clonedContents = list;
         }
         if (
@@ -198,7 +198,7 @@ export class ClipboardPlugin extends Plugin {
                 // related ones like headings etc.
                 if (!isBlock(ancestor) || paragraphRelatedElements.includes(ancestor.nodeName)) {
                     const clone = ancestor.cloneNode();
-                    clone.append(...clonedContents.childNodes);
+                    clone.append(...childNodes(clonedContents));
                     clonedContents.appendChild(clone);
                 }
             }
@@ -486,14 +486,14 @@ export class ClipboardPlugin extends Plugin {
                 }
             } else if (
                 ["S", "U"].includes(node.nodeName) &&
-                node.childNodes.length === 1 &&
+                childNodes(node).length === 1 &&
                 node.firstChild.nodeName === "FONT"
             ) {
                 // S and U tags sometimes contain FONT tags. We prefer the
                 // strike to adopt the style of the text, so we invert them.
                 const fontNode = node.firstChild;
                 node.before(fontNode);
-                node.replaceChildren(...fontNode.childNodes);
+                node.replaceChildren(...childNodes(fontNode));
                 fontNode.appendChild(node);
             } else if (
                 node.nodeName === "IMG" &&
@@ -534,7 +534,7 @@ export class ClipboardPlugin extends Plugin {
                     node.classList.remove(klass);
                 }
             }
-            for (const child of [...node.childNodes]) {
+            for (const child of childNodes(node)) {
                 this.cleanForPaste(child);
             }
         }
