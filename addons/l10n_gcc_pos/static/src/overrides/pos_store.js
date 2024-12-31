d9cfdea3dfbea0b2b3f78a9ecd9fa10ce2301d33
Kripal K <kripal259@gmail.com>
2024-12-31 15:20:30 +0300

    [FIX] l10n_gcc_pos: Prevent duplicate 'Served by' text in POS receipt for GCC countries.

--- addons/l10n_gcc_pos/static/src/overrides/pos_store.js
+++ addons/l10n_gcc_pos/static/src/overrides/pos_store.js
@@ -3,11 +3,16 @@ import { PosStore } from "@point_of_sale/app/store/pos_store";
 
 patch(PosStore.prototype, {
     getReceiptHeaderData(order) {
+        const result = super.getReceiptHeaderData(...arguments);
+        const is_gcc_country = ["SA", "AE", "BH", "OM", "QA", "KW"].includes(
+            this.company.country_id?.code
+        );
         return {
-            ...super.getReceiptHeaderData(...arguments),
-            is_gcc_country: ["SA", "AE", "BH", "OM", "QA", "KW"].includes(
-                this.company.country_id?.code
-            ),
+            ...result,
+            is_gcc_country: is_gcc_country,
+            cashier: is_gcc_country
+                ? order?.getCashierName() || this.get_cashier()?.name
+                : result.cashier,
         };
     },
 });
