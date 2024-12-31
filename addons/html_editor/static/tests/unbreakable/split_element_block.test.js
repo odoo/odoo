bb9129f28110e98b99c96c92da6f5a0ea8b2697e
abd-msyukyu-odoo <abd@odoo.com>
2024-12-31 13:38:25 +0100

    [FIX] html_editor: refine `contenteditable` and `unsplittable` interactions
    In some cases, `contenteditable='false'` elements could be unwrapped during
    `domInsert` or `split` during `SplitPlugin` operations. This is undesirable as
    the editor should not be able to modify such nodes in normal circumstances.
    
    This commit define how `contenteditable` elements should interact with the
    `unsiplittable` feature of the editor, to avoid such issues:
    
    In general:
    - an element with `isContentEditable === false` should never be mergeable/splittable
    - an element with `isContentEditable === true` should be mergeable/splittable, with
      exceptions:
      - an element with `contenteditable='true'` attribute should NOT be
        mergeable/splittable if it is in a non-editable context (has a
        `contenteditable='false'` ancestor)
    
    Special case of disconnected nodes:
    - in JavaScript, the default behavior is that unmarked disconnected elements
      have `isContentEditable === false` (if they don't have an ancestor explicitly
      setting `contenteditable='true'`). But in an editor context, when we handle
      such nodes (i.e. in `domPlugin.insert`), they will be inserted inside an editable
      context, so, such nodes SHOULD BE considered as mergeable/splittable.
    
    task-4294440

--- addons/html_editor/static/tests/unbreakable/split_element_block.test.js
+++ addons/html_editor/static/tests/unbreakable/split_element_block.test.js
@@ -1,6 +1,7 @@
-import { test } from "@odoo/hoot";
-import { testEditor } from "../_helpers/editor";
+import { expect, test } from "@odoo/hoot";
+import { setupEditor, testEditor } from "../_helpers/editor";
 import { splitBlock } from "../_helpers/user_actions";
+import { getContent } from "../_helpers/selection";
 
 test("should replace splitElementBlock with insertLineBreak (selection start)", async () => {
     await testEditor({
@@ -21,5 +22,25 @@ test("should replace splitElementBlock with insertLineBreak (selection end)", as
         contentBefore: `<div>ab[]</div>`,
         stepFunction: splitBlock,
         contentAfter: `<div>ab<br>[]<br></div>`,
+    });
+});
+test("should not split a contenteditable='false'", async () => {
+    const { editor, el } = await setupEditor(`<p contenteditable="false">ab</p>`);
+    const p = el.querySelector("p");
+    editor.shared.split.splitBlockNode({ targetNode: p, targetOffset: 0 });
+    expect(getContent(el)).toBe(`<p contenteditable="false">ab</p>`);
+});
+test("should split an explicit contenteditable='true' if its ancestor isContentEditable", async () => {
+    await testEditor({
+        contentBefore: `<p contenteditable="true">ab[]</p>`,
+        stepFunction: splitBlock,
+        contentAfter: `<p contenteditable="true">ab</p><p contenteditable="true">[]<br></p>`,
+    });
+});
+test("should insert a newline instead of splitting an explicit contenteditable='true' if its ancestor is not contentEditable", async () => {
+    await testEditor({
+        contentBefore: `<div contenteditable="false"><p contenteditable="true">ab[]</p></div>`,
+        stepFunction: splitBlock,
+        contentAfter: `<div contenteditable="false"><p contenteditable="true">ab<br>[]<br></p></div>`,
     });
 });
