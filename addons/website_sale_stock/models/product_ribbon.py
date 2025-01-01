ee1fb84920a0346bc9e1c8d7100adc37ff4c2d4c
dere-odoo <dere@odoo.com>
2025-01-01 15:28:32 +0530

    [IMP] website_sale: ribbon priority assignment

--- addons/website_sale_stock/models/product_ribbon.py
+++ addons/website_sale_stock/models/product_ribbon.py
@@ -1,7 +1,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields
-from odoo.http import request
+from odoo import _, api, fields, models
+
+from odoo.exceptions import ValidationError
 
 
 class ProductRibbon(models.Model):
@@ -14,7 +15,24 @@ class ProductRibbon(models.Model):
         ondelete={'out_of_stock': 'cascade'}
     )
 
-    def _get_ribbon(self, product, variant, product_prices):
-        if product._is_sold_out():
-            return self.sudo().search([('assign', '=', 'out_of_stock')], limit=1)
-        return super()._get_ribbon(product, variant, product_prices)
+    @api.constrains('assign')
+    def _check_assign(self):
+        super()._check_assign()
+        for record in self:
+            if record.assign in ['out_of_stock']:
+                existing_ribbons = self.search([
+                    ('id', '!=', record.id),
+                    ('assign', '=', record.assign)
+                ], limit=1)
+                if existing_ribbons:
+                    raise ValidationError(
+                        _(
+                            "Only one record with the assign %s is allowed." ,
+                            dict(self._fields['assign'].selection).get(record.assign)
+                        )
+                    )
+
+    def _match_assign(self, product, product_prices):
+        if self.assign == 'out_of_stock':
+            return product._is_sold_out()
+        return super()._match_assign(product, product_prices)
