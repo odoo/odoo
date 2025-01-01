0241d2f3f1027685b1650d13a856c3ccf8f7e581
Shubham Agarwal [SHAG] <shag@odoo.com>
2025-01-01 13:26:44 +0000

    [FIX] point_of_sale: product card not updated according to order
    Steps to reproduce:
    1. Create a new free order and add 2 quantities of any product.
    2. Create another free order and add 2 quantities of any product different from
    1st order.
    3. Switch to orders 1 and 2, and you'll see the same product and quantity
    loading in both orders.
    
    Issue:
    - Both free orders show same product card if quantity is same.
    
    Fix:
    - UseEffect was set to update on order rather than quantity.
    
    task-4438675
    
    closes odoo/odoo#193034
    
    X-original-commit: 2d6f93b624c5c7eba054fa93cbe662093c98a318
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Shubham Agarwal (shag) <shag@odoo.com>

--- addons/point_of_sale/tests/test_frontend.py
+++ addons/point_of_sale/tests/test_frontend.py
@@ -587,6 +587,7 @@ class TestUi(TestPointOfSaleHttpCommon):
         self.start_tour("/pos/ui?config_id=%d" % self.main_pos_config.id, 'pos_basic_order_01_multi_payment_and_change', login="pos_user")
         self.start_tour("/pos/ui?config_id=%d" % self.main_pos_config.id, 'pos_basic_order_02_decimal_order_quantity', login="pos_user")
         self.start_tour("/pos/ui?config_id=%d" % self.main_pos_config.id, 'pos_basic_order_03_tax_position', login="pos_user")
+        self.start_tour("/pos/ui?config_id=%d" % self.main_pos_config.id, 'FloatingOrderTour', login="pos_user")
         self.start_tour("/pos/ui?config_id=%d" % self.main_pos_config.id, 'ProductScreenTour', login="pos_user")
         self.start_tour("/pos/ui?config_id=%d" % self.main_pos_config.id, 'PaymentScreenTour', login="pos_user")
         self.start_tour("/pos/ui?config_id=%d" % self.main_pos_config.id, 'ReceiptScreenTour', login="pos_user")
