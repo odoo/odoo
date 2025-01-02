8f5afee559de35030f2b4e51294d28dde2c77f6e
Maryam Kia <maki@odoo.com>
2025-01-02 17:10:05 +0100

    [FIX] mail: trash icon on attachments
    Portal users don't have access to delete attachments created by others. In this
    case, the trash icon shouldn't be displayed. This PR removes this icon from the
    attachment of a message when the message is not editable by the current
    user.

--- addons/mail/static/src/core/common/attachment_model.js
+++ addons/mail/static/src/core/common/attachment_model.js
@@ -34,6 +34,9 @@ export class Attachment extends FileModelMixin(Record) {
     create_date = Record.attr(undefined, { type: "datetime" });
 
     get isDeletable() {
+        if (this.message && !this.store.self.isInternalUser) {
+            return this.message.editable;
+        }
         return true;
     }
 
