c44914d95667d67ab24041c5f967443c2985d7b2
ksbh-odoo <ksbh@odoo.com>
2025-01-02 19:02:56 +0530

    [IMP] website_blog: blog improvements
    This commit introduces few improvements to website_blog.
    Improvements include :
    
    1. If "New Blog Post" is selected from a specific blog, the pop-up
    should default to that blog. For example, selecting it from "Travel"
    should default to "Travel". This is made possible by passing the
    correct context while calling action for pop-up.
    
    2. Adds functionality to display details like publish date and author
    when the Title Inside Cover layout is selected. Also adjusted
    performance test for the same as it makes extra queries while accessing
    "blog_post" record set.
    
    3. Replaces the static "Follow Us" links in the header with a dynamic
    social media snippet, allowing users or blog owners to edit them.
    
    4. The comments are only displayed when the comments option is enabled.
    
    task-4430868
    
    closes odoo/odoo#192189
    
    Signed-off-by: Serge Bayet (seba) <seba@odoo.com>
    Co-authored-by: Mohammed Shekha <msh@odoo.com>
    Co-authored-by: Sanjay Sharma <shsa@odoo.com>

---
+++ addons/website_blog/static/tests/tours/blog_context.js
@@ -0,0 +1,81 @@
+import {
+    clickOnEditAndWaitEditMode,
+    clickOnSnippet,
+    registerWebsitePreviewTour,
+} from "@website/js/tours/tour_utils";
+
+registerWebsitePreviewTour(
+    "blog_context_and_social_media",
+    {
+        url: "/blog",
+    },
+    () => [
+        {
+            content: "Ensure we are in blog page",
+            trigger: ":iframe html[data-view-xmlid='website_blog.blog_post_short']",
+        },
+        {
+            content: "Click on 'aaa Blog Test' Blog",
+            trigger: ":iframe .website_blog nav .nav-item a:contains('aaa Blog Test')",
+            run: "click",
+        },
+        {
+            content: "Check that 'aaa Blog Test' Blog is open",
+            trigger: ":iframe .website_blog nav .nav-item a.active:contains('aaa Blog Test')",
+        },
+        {
+            content: "Click on New Post",
+            trigger: ".o_menu_systray .o_new_content_container button",
+            run: "click",
+        },
+        {
+            content: "Click on Blog Post",
+            trigger: "#o_new_content_menu_choices button[title='Blog Post']",
+            run: "click",
+        },
+        {
+            content: "Check in dialog current Selected Blog is 'aaa Blog Test'",
+            trigger: ".modal-dialog .o_field_widget[name='blog_id'] .o_input",
+            run: function () {
+                if (this.anchor.value !== "aaa Blog Test") {
+                    console.error("Current selected blog should be 'aaa Blog Test'");
+                }
+            },
+        },
+        {
+            content: "Click on Discard",
+            trigger: ".modal-footer .o_form_button_cancel",
+            run: "click",
+        },
+        {
+            content: "Click away to close the modal",
+            trigger: "#o_new_content_menu_choices",
+            run: "click",
+        },
+        ...clickOnEditAndWaitEditMode(),
+        {
+            content: "Check that SideBar is enabled",
+            trigger: ":iframe #wrap #o_wblog_sidebar",
+            run: "click",
+        },
+        ...clickOnSnippet("#o_wblog_sidebar .s_social_media"),
+        {
+            content: "Check Social Media Options are available",
+            trigger: ".o_customize_tab table.o_social_media_list",
+        },
+        {
+            content: "Click on Discard",
+            trigger: ".o-snippets-top-actions [data-action='cancel']",
+            run: "click",
+        },
+        {
+            content: "Click on the first article",
+            trigger: ":iframe article[name='blog_post'] a",
+            run: "click",
+        },
+        {
+            content: "Check the blog info is available",
+            trigger: ":iframe #o_wblog_post_info",
+        },
+    ]
+);
