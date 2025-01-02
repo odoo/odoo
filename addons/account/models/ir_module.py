0856c0af9d47df66907fd3f907a94298e9d83e65
william-andre <wan@odoo.com>
2025-01-02 17:35:02 +0100

    [FIX] account: translate tax tags
    The tax tags are translatable but never translated automatically because
    they are generated dynamically from the tax report.
    
    This commit aims at improving this by translating automatically the tags
    that match exactly the name of the tax report line, for instance in
    Israel.

--- addons/account/models/ir_module.py
+++ addons/account/models/ir_module.py
@@ -77,6 +77,7 @@ class IrModule(models.Model):
         if 'account' in modules:
             def load_account_translations(env):
                 env['account.chart.template']._load_translations(langs=langs)
+                env['account.account.tag']._translate_tax_tags(langs=langs)
             if self.env.registry.loaded:
                 load_account_translations(self.env)
             else:
