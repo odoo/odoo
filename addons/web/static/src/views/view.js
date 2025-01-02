odoo/odoo

commit f983703dfa3c5102fa818523ae419a70cc4b5230

[IMP] web, base: Add embedded actions
This update introduces the implementation of embedded actions, enabling
the addition of embedded actions in the control panel for seamless
toggling among various actions.

Functional specifications:

This task aims to centralize and access models related to the current
parent model.

We introduce the concept of embedded action. If an action has embedded
actions, then a button with an 'sliders' icon is displayed in the control
panel.

When the user clicks the button, the embedded actions appears.

The implementation consists as various actions chosen as visible
by the user. By default, the embedded actions only displays the parent
action (and will always display it).

There is a dropdown button to the right of the topbar that displays
all the actions available to the user. These actions depend on the model
, but it's also possible that there is a domain on these actions making
them invisible. For example, a project without the 'allow timesheets'
option will not display the 'Timesheets' action.

The default actions cannot be deleted by the user; they can only
decide whether to display them or not. The visibility of actions is
different for each model but also for each model ID, meaning, for
example, that the actions visible in one project will not be the same
for another.

Each user can also save the current view and thus create a new embedded
action. They can decide to share this embedded action so that it is
visible to everyone, or not. The view type and filters are saved when
creating a new embedded action. Those saved filters are saved as favorites
filters that cannot be deleted while the embedded action exists.

If the user has the rights, he can also drag and drop the actions to
reorder them. This order will be specific to the user and to the model
ID in which the reordering was made.

On Mobile, only a dropdown is displayed with the current action. When
the dropdown is unfolded, all available actions are displayed. It is

addons/web/static/src/views/view.js
@@ -22,7 +22,7 @@ import {
 const viewRegistry = registry.category("views");
 
 viewRegistry.addValidation({
-    Controller: { validate: (c) => c.prototype instanceof Component }, 
+    Controller: { validate: (c) => c.prototype instanceof Component },
     "*": true,
 });
 
@@ -49,6 +49,9 @@ export function getDefaultConfig() {
     const config = {
         actionId: false,
         actionType: false,
+        embeddedActions: [],
+        currentEmbeddedActionId: false,
+        parentActionId: false,
         actionFlags: {},
         breadcrumbs: reactive([
             {
@@ -267,7 +270,13 @@ export class View extends Component {
             // a loadViews is done to complete the missing information
             const result = await this.viewService.loadViews(
                 { context, resModel, views },
-                { actionId: this.env.config.actionId, loadActionMenus, loadIrFilters }
+                {
+                    actionId: this.env.config.actionId,
+                    embeddedActionId: this.env.config.currentEmbeddedActionId,
+                    embeddedParentResId: context.active_id,
+                    loadActionMenus,
+                    loadIrFilters,
+                }
             );
             // Note: if props.views is different from views, the cached descriptions
             // will certainly not be reused! (but for the standard flow this will work as

addons/web/controllers/action.py
@@ -41,7 +41,6 @@ class Action(Controller):
         base_action = Actions.browse([action_id]).sudo().read(['type'])
         if not base_action:
             raise MissingActionError(_("The action %r does not exist.", action_id))
-
         action_type = base_action[0]['type']
         if action_type == 'ir.actions.report':
             request.update_context(bin_size=True)
@@ -52,6 +51,9 @@ class Action(Controller):
             if parent_path:
                 result['path'] = parent_path
             return clean_action(result, env=action.env) if result else {'type': 'ir.actions.act_window_close'}
+        if action_type == 'ir.actions.act_window':
+            result = request.env[action_type].sudo().browse([action_id])._get_action_dict()
+            return clean_action(result, env=request.env) if result else False
         result = request.env[action_type].sudo().browse([action_id]).read()
         return clean_action(result[0], env=request.env) if result else False
 

addons/web/static/src/search/control_panel/control_panel.js
@@ -8,10 +8,34 @@ import { Dropdown } from "@web/core/dropdown/dropdown";
 import { useCommand } from "@web/core/commands/command_hook";
 import { DropdownItem } from "@web/core/dropdown/dropdown_item";
 import { useHotkey } from "@web/core/hotkeys/hotkey_hook";
+import { useSortable } from "@web/core/utils/sortable_owl";
+import { user } from "@web/core/user";
+import { AccordionItem } from "@web/core/dropdown/accordion_item";
+import { CheckBox } from "@web/core/checkbox/checkbox";
+import { makeContext } from "@web/core/context";
+import { ConfirmationDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 
 import { Component, useState, onMounted, useExternalListener, useRef, useEffect } from "@odoo/owl";
 
 const STICKY_CLASS = "o_mobile_sticky";
+
+/**
+ * @typedef EmbeddedAction
+ * @property {number} id
+ * @property {[number, string]} parent_action_id
+ * @property {string} name
+ * @property {number} sequence
+ * @property {number} parent_res_id
+ * @property {string} parent_res_model
+ * @property {[number, string]} action_id
+ * @property {string} python_method
+ * @property {number} user_id
+ * @property {boolean} is_deletable
+ * @property {string} default_view_mode
+ * @property {string} filter_ids
+ * @property {string} domain
+ * @property {string} context
+ */
 
 export class ControlPanel extends Component {
     static template = "web.ControlPanel";
@@ -20,6 +44,8 @@ export class ControlPanel extends Component {
         SearchBar,
         Dropdown,
         DropdownItem,
+        AccordionItem,
+        CheckBox,
     };
     static props = {
         display: { type: Object, optional: true },
@@ -31,14 +57,75 @@ export class ControlPanel extends Component {
         this.pagerProps = this.env.config.pagerProps
             ? useState(this.env.config.pagerProps)
             : undefined;
+        this.notificationService = useService("notification");
         this.breadcrumbs = useState(this.env.config.breadcrumbs);
+        this.orm = useService("orm");
+        this.dialogService = useService("dialog");
 
         this.root = useRef("root");
+        this.newActionNameRef = useRef("newActionNameRef");
+        this.isEmbeddedActionsOrderModifiable = false;
+        this.defaultEmbeddedActions = this.env.config.embeddedActions;
+        if (this.env.config.embeddedActions?.length > 0 && !this.env.config.parentActionId) {
+            const { parent_res_model, parent_action_id } = this.env.config.embeddedActions[0];
+            this.defaultEmbeddedActions = [
+                {
+                    id: false,
+                    name: this.env.config?.actionName,
+                    parent_action_id,
+                    parent_res_model,
+                    action_id: parent_action_id,
+                    user_id: false,
+                    context: {},
+                },
+                ...this.env.config.embeddedActions,
+            ];
+            this.env.config.setEmbeddedActions(this.defaultEmbeddedActions);
+        }
+
+        /**
+         * The visible embedded actions are unique to each user and to each res_id. The visible actions chosen by the
+         * user are stored in the local storage in a key corresponding to a combination of the actionId, the activeId
+         * and the currrent userId. Each key contains a dict. The keys of the latter are the id of the visible embedded
+         * actions.
+         */
+        const parentActionId =
+            this.env.config.parentActionId ||
+            this.env.config.embeddedActions?.[0]?.parent_action_id[0] ||
+            this.env.config.embeddedActions?.[0]?.parent_action_id ||
+            "";
+        this.embeddedActionsVisibilityKey = `visibleEmbeddedActions${parentActionId}+${
+            this.env.searchModel?.globalContext.active_id || ""
+        }+${user.userId}`;
+
+        this.embeddedVisibilityKey = `visibleEmbedded${parentActionId}+${
+            this.env.searchModel?.globalContext.active_id || ""
+        }+${user.userId}`;
+
+        this.embeddedOrderKey = `orderEmbedded${parentActionId}+${
+            this.env.searchModel?.globalContext.active_id || ""
+        }+${user.userId}`;
 
         this.state = useState({
             showSearchBar: false,
             showMobileSearch: false,
             showViewSwitcher: false,
+            embeddedInfos: {
+                showEmbedded:
+                    this.env.config.embeddedActions?.length > 0 &&
+                    (!!this.env.config.parentActionId ||
+                        !!JSON.parse(browser.localStorage.getItem(this.embeddedVisibilityKey))),
+                embeddedActions: this.defaultEmbeddedActions || [],
+                newActionIsShared: false,
+                newActionName: this.newActionNameGetter,
+                visibleEmbeddedActions:
+                    (this.env.config.embeddedActions?.length > 0 &&
+                        JSON.parse(
+                            browser.localStorage.getItem(this.embeddedActionsVisibilityKey)
+                        )) ||
+                    {},
+                currentEmbeddedAction: this.currentEmbeddedAction,
+            },
         });
 
         this.onScrollThrottledBound = this.onScrollThrottled.bind(this);
@@ -79,7 +166,22 @@ export class ControlPanel extends Component {
                 scrollingEl.removeEventListener("scroll", this.onScrollThrottledBound);
             };
         });
-        onMounted(() => {
+
+        onMounted(async () => {
+            if (this.state.embeddedInfos.embeddedActions?.length > 0) {
+                // If there is no visible embedded actions, the current action (if it exists) is put by default
+                if (
+                    !Object.keys(this.state.embeddedInfos.visibleEmbeddedActions).includes("false")
+                ) {
+                    this._setVisibility(false);
+                }
+                const embeddedOrderLocalStorageKey = browser.localStorage.getItem(
+                    this.embeddedOrderKey
+                );
+                if (embeddedOrderLocalStorageKey) {
+                    this._sortEmbeddedActions(JSON.parse(embeddedOrderLocalStorageKey));
+                }
+            }
             if (
                 !this.env.isSmall ||
                 ("adaptToScroll" in this.display && !this.display.adaptToScroll)
@@ -116,10 +218,54 @@ export class ControlPanel extends Component {
                 button.classList.add("dropdown-item", "btn", "btn-link");
             }
         });
+
+        useSortable({
+            enable: true,
+            ref: this.root,
+            elements: ".o_draggable",
+            cursor: "move",
+            delay: 200,
+            tolerance: 10,
+            onWillStartDrag: (params) => this._sortEmbeddedActionStart(params),
+            onDrop: (params) => this._sortEmbeddedActionDrop(params),
+        });
+    }
+
+    getDropdownClass(action) {
+        const isSelected =
+            (!this.env.isSmall && this._checkValueLocalStorage(action)) ||
+            (this.env.isSmall && this.state.embeddedInfos.currentEmbeddedAction?.id === action.id)
+                ? "selected"
+                : "";
+        const isClickable =
+            action.id === false && !this.env.isSmall ? "cursor-default text-muted" : "";
+        return `${isSelected} ${isClickable}`;
     }
 
     getScrollingElement() {
         return this.root.el.parentElement;
+    }
+
+    /**
+     * @returns {EmbeddedAction}
+     */
+    get currentEmbeddedAction() {
+        if (!this.env.config) {
+            return {};
+        }
+        const { currentEmbeddedActionId } = this.env.config;
+        return (
+            this.defaultEmbeddedActions?.find(({ id }) => id === currentEmbeddedActionId) ||
+            this.defaultEmbeddedActions?.[0]
+        );
+    }
+
+    get newActionNameGetter() {
+        if (this.currentEmbeddedAction?.name) {
+            return _t("Custom %s", this.currentEmbeddedAction.name);
+        } else {
+            return _t("Custom Embedded Action");
+        }
     }
 
     /**
@@ -150,6 +296,15 @@ export class ControlPanel extends Component {
      */
     onBreadcrumbClicked(jsId) {
         this.actionService.restore(jsId);
+    }
+
+    onClickShowEmbedded() {
+        if (this.state.embeddedInfos.showEmbedded) {
+            browser.localStorage.removeItem(this.embeddedVisibilityKey);
+        } else {
+            browser.localStorage.setItem(this.embeddedVisibilityKey, true);
+        }
+        this.state.embeddedInfos.showEmbedded = !this.state.embeddedInfos.showEmbedded;
     }
 
     /**
@@ -232,5 +387,240 @@ export class ControlPanel extends Component {
             ev.preventDefault();
             ev.stopPropagation();
         }
+    }
+
+    /**
+     * @param {EmbeddedAction} action
+     */
+    _checkValueLocalStorage(action) {
+        const actionIdStr = action.id.toString();
+        return this.state.embeddedInfos.visibleEmbeddedActions[actionIdStr];
+    }
+
+    /**
+     * The selected action is put into (or removed from) the localStorage and its visibility changes.
+     * The state variable visibleEmbeddedActions keeps track of the visible actions to avoid  having to parse
+     * the localStorage values every time we want to access them.
+     * @param {EmbeddedAction} action
+     */
+    _setVisibility(actionId) {
+        const actionIdStr = actionId.toString();
+        if (this.state.embeddedInfos.visibleEmbeddedActions[actionIdStr]) {
+            delete this.state.embeddedInfos.visibleEmbeddedActions[actionIdStr];
+        } else {
+            this.state.embeddedInfos.visibleEmbeddedActions[actionIdStr] = true;
+        }
+        browser.localStorage.setItem(
+            this.embeddedActionsVisibilityKey,
+            JSON.stringify(this.state.embeddedInfos.visibleEmbeddedActions)
+        );
+    }
+
+    _onShareCheckboxChange() {
+        this.state.embeddedInfos.newActionIsShared = !this.state.embeddedInfos.newActionIsShared;
+    }
+
+    /**
+     * @param {Event} ev
+     */
+    async _saveNewAction(ev) {
+        const {
+            newActionName,
+            newActionIsShared,
+            embeddedActions,
+            currentEmbeddedAction,
+            visibleEmbeddedActions,
+        } = this.state.embeddedInfos;
+        if (!newActionName) {
+            this.notificationService.add(_t("A name for your new action is required."), {
+                type: "danger",
+            });
+            ev.stopPropagation();
+            return this.newActionNameRef.el.focus();
+        }
+        const duplicateName = embeddedActions.some(({ name }) => name === newActionName);
+        if (duplicateName) {
+            this.notificationService.add(_t("An action with the same name already exists."), {
+                type: "danger",
+            });
+            ev.stopPropagation();
+            return this.newActionNameRef.el.focus();
+        }
+        const userId = newActionIsShared ? false : user.userId;
+
+        const extractValues = ({ parent_action_id, action_id, parent_res_model }) => ({
+            parent_action_id: parent_action_id[0],
+            action_id: action_id[0] || this.env.config.actionId,
+            parent_res_model,
+            parent_res_id: this.env.searchModel.globalContext.active_id,
+            user_id: userId,
+            is_deletable: true,
+            default_view_mode: this.env.config.viewType,
+        });
+        const { parent_action_id, action_id, python_method, domain, context, groups_ids } =
+            currentEmbeddedAction;
+        const values = {
+            ...extractValues(currentEmbeddedAction),
+            python_method,
+            domain,
+            context,
+            groups_ids,
+            name: newActionName,
+        };
+        const embeddedActionId = await this.orm.create("ir.embedded.actions", [values]);
+        const description = `${newActionName}`;
+        this.env.searchModel.createNewFavorite({
+            description,
+            isDefault: true,
+            isShared: userId,
+            embeddedActionId: embeddedActionId[0],
+        });
+        Object.assign(this.state.embeddedInfos, {
+            newActionName: "",
+            newActionIsShared: false,
+        });
+        const enrichedNewEmbeddedAction = {
+            ...values,
+            parent_action_id,
+            action_id,
+            id: embeddedActionId[0],
+        };
+        this.state.embeddedInfos.embeddedActions.push(enrichedNewEmbeddedAction);
+        const embeddedActionIdStr = embeddedActionId[0].toString();
+        visibleEmbeddedActions[embeddedActionIdStr] = true;
+        const order = this.state.embeddedInfos.embeddedActions.map((el) => el.id);
+        browser.localStorage.setItem(
+            this.embeddedActionsVisibilityKey,
+            JSON.stringify(visibleEmbeddedActions)
+        );
+        browser.localStorage.setItem(this.embeddedOrderKey, JSON.stringify(order));
+        this.env.config.setCurrentEmbeddedAction(embeddedActionId);
+        this.state.embeddedInfos.currentEmbeddedAction = enrichedNewEmbeddedAction;
+        this.state.embeddedInfos.newActionName = `${newActionName} Custom`;
+    }
+
+    /**
+     * @param {EmbeddedAction} action
+     */
+    openConfirmationDialog(action) {
+        const dialogProps = {
+            title: _t("Warning"),
+            body: action.user_id
+                ? _t("Are you sure that you want to remove this embedded action?")
+                : _t("This embedded action is global and will be removed for everyone."),
+            confirmLabel: _t("Delete"),
+            confirm: async () => await this._deleteEmbeddedAction(action),
+            cancel: () => {},
+        };
+        this.dialogService.add(ConfirmationDialog, dialogProps);
+    }
+
+    /**
+     * @param {EmbeddedAction} action
+     */
+    async _deleteEmbeddedAction(action) {
+        const { visibleEmbeddedActions, embeddedActions, currentEmbeddedAction } =
+            this.state.embeddedInfos;
+        const actionIdStr = action.id.toString();
+        if (visibleEmbeddedActions[actionIdStr]) {
+            delete visibleEmbeddedActions[actionIdStr];
+        }
+        browser.localStorage.setItem(
+            this.embeddedActionsVisibilityKey,
+            JSON.stringify(visibleEmbeddedActions)
+        );
+        this.state.embeddedInfos.embeddedActions = embeddedActions.filter(
+            ({ id }) => id !== action.id
+        );
+        await this.orm.unlink("ir.embedded.actions", [action.id]);
+        if (action.id === currentEmbeddedAction?.id) {
+            const { active_id, active_model } = this.env.searchModel.globalContext;
+            const actionContext = action.context ? makeContext([action.context]) : {};
+            const additionalContext = {
+                ...actionContext,
+                active_id,
+                active_model,
+            };
+            this.actionService.doAction(action.parent_action_id[0] || action.parent_action_id, {
+                additionalContext,
+                stackPosition: "replaceCurrentAction",
+            });
+        }
+    }
+
+    /**
+     * @param {EmbeddedAction} action
+     */
+    async onEmbeddedActionClick(action) {
+        this.env.config.setEmbeddedActions(this.state.embeddedInfos.embeddedActions);
+        const { active_id, active_model } = this.env.searchModel.globalContext;
+        const actionContext = action.context ? makeContext([action.context]) : {};
+        const context = {
+            ...actionContext,
+            active_id,
+            active_model,
+            current_embedded_action_id: action.id,
+            parent_action_embedded_actions: this.state.embeddedInfos.embeddedActions,
+            parent_action_id: action.parent_action_id[0] || action.parent_action_id,
+        };
+        this.actionService.doActionButton({
+            type: action.python_method ? "object" : "action",
+            resId: this.env.searchModel?.globalContext.active_id,
+            name: action.python_method || action.action_id[0] || action.action_id,
+            resModel: action.parent_res_model,
+            context,
+            stackPosition: this.env.config.parentActionId ? "replaceCurrentAction" : "",
+            viewType: action.default_view_mode,
+        });
+    }
+
+    /**
+     * @param {number[]} order
+     */
+    _sortEmbeddedActions(order) {
+        this.state.embeddedInfos.embeddedActions = this.state.embeddedInfos.embeddedActions.sort(
+            (a, b) => {
+                const indexA = order.indexOf(a.id);
+                if (!indexA) {
+                    return -1;
+                }
+                const indexB = order.indexOf(b.id);
+                if (!indexB) {
+                    return 1;
+                }
+                return indexA - indexB;
+            }
+        );
+    }
+
+    /**
+     * @param {Object} params
+     * @param {HTMLElement} params.element
+     */
+    _sortEmbeddedActionStart({ element, addClass }) {
+        addClass(element, "o_dragged_embedded_action");
+    }
+
+    /**
+     * @param {Object} params
+     * @param {HTMLElement} params.element
+     * @param {HTMLElement} params.previous
+     */
+    _sortEmbeddedActionDrop({ element, previous }) {
+        const order = this.state.embeddedInfos.embeddedActions
+            .map((el) => el.id)
+            .filter((el) => el !== false);
+        const elementId = Number(element.dataset.id);
+        const elementIndex = order.indexOf(elementId);
+        order.splice(elementIndex, 1);
+        if (previous) {
+            const prevIndex = order.indexOf(Number(previous.dataset.id));
+            order.splice(prevIndex + 1, 0, elementId);
+        } else {
+            order.splice(0, 0, elementId);
+        }
+        order.unshift(false);
+        this._sortEmbeddedActions(order);
+        browser.localStorage.setItem(this.embeddedOrderKey, JSON.stringify(order));
     }
 }

addons/web/static/src/search/control_panel/control_panel.scss
@@ -53,6 +53,39 @@
     }
 }
 
+.o_embedded_line {
+    flex: 1 1 0%;
+    height: 1px;
+    background-color: #ddd;
+}
+
+.o_dragged_embedded_action {
+    transition: transform 0.5s;
+    transform: rotate(2deg);
+    opacity: 0.8;
+}
+
+.o_embedded_actions {
+    opacity: 0;
+    max-height: 0;
+    align-self: center;
+    overflow: hidden;
+    transition: opacity 0.5s, max-height 1.0s ease;
+}
+
+.o_embedded_actions_buttons_wrapper {
+    max-width: 80%;
+}
+
+.o_embedded_actions.visible {
+    opacity: 1;
+    max-height: 500px;
+}
+
+.o_save_current_view {
+    color: $link-color;
+}
+
 @include media-breakpoint-down(md) {
     .o_rtl .o_control_panel .o_back_button:before {
         transform: rotate(180deg);

commit d836eef5f05b5bc876b76d10fffd0a6b9749f107

[PERF] Fix webclient view cache for not embedded action views
Steps to reproduce (a bit technical):

1. Navigate to the "Contacts" kanban view.
2. Open the Network tab in the browser's developer tools.
3. Click on the first contact activities and "Schedule a new activity".
4. Notice a call to `get_views` in the Network tab.
5. Click on a different contact activities, and "Schedule a new activity".
6. Notice a new call to `get_views` in the Network tab.

Expected:

The view is already cached, so the webclient should not attempt to load it.

Explanation:

This is likely a regression since f983703d, when the embedded actions were
introduced. Now, the `embeddedParentResId` is part of the views cache key, and
so we get much fewer cache hits.

For most views, there isn't really an `embeddedActionId` set, and yet the
`embeddedParentResId` was always set. The later is only used in combination with
`embeddedActionId`, so it causes a lot of avoidable cache misses.

The solution is to simply set the `embeddedParentResId` only if there's an
`embeddedActionId`.

addons/web/static/src/views/view.js
@@ -283,7 +283,7 @@ export class View extends Component {
                 {
                     actionId: this.env.config.actionId,
                     embeddedActionId: this.env.config.currentEmbeddedActionId,
-                    embeddedParentResId: context.active_id,
+                    embeddedParentResId: this.env.config.currentEmbeddedActionId && context.active_id,
                     loadActionMenus,
                     loadIrFilters,
                 }