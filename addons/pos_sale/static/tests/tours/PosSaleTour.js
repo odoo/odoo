c31b39dd7597a953e6efa586fa0da9cb81222e68
roen-odoo <roen@odoo.com>
2025-01-02 14:58:12 +0000

    [FIX] pos_sale: separate downpayment for fixed tax
    When a product has a fixed tax, there should be a separate downpayment
    line for the fixed tax amount.
    
    Steps to reproduce:
    -------------------
    * Create a fixed tax of 1€
    * Assign this tax to any product
    * Create a sale order with this product
    * Open PoS and make a downpayment of 10%
    > Observation: There is only one downpayment line with the total amount
      of the product and the fixed tax combined.
    
    opw-4252104
    
    closes odoo/odoo#193269
    
    X-original-commit: eea695867585f4a3f5ca33951509107b43ec2605
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: Robin Engels (roen) <roen@odoo.com>

--- addons/pos_sale/static/tests/tours/PosSaleTour.js
+++ addons/pos_sale/static/tests/tours/PosSaleTour.js
@@ -329,23 +329,6 @@ registry.category("web_tour.tours").add("PosShipLaterNoDefault", {
         ].flat(),
 });
 
-registry.category("web_tour.tours").add("PoSDownPaymentLinesPerFixedTax", {
-    test: true,
-    steps: () =>
-        [
-            Dialog.confirm("Open session"),
-            ProductScreen.controlButton("Quotation/Order"),
-            ProductScreen.downPayment20PercentFirstOrder(),
-            Order.hasLine({
-                productName: "Down Payment",
-                quantity: "1.0",
-                price: "22",
-            }),
-            Order.hasNoTax(),
-            ProductScreen.totalAmountIs(22.0),
-        ].flat(),
-});
-
 registry.category("web_tour.tours").add("PoSDownPaymentAmount", {
     test: true,
     steps: () =>
@@ -397,5 +380,24 @@ registry.category("web_tour.tours").add("PosSettleOrderShipLater", {
             PaymentScreen.remainingIs("0.0"),
             PaymentScreen.clickValidate(),
             ReceiptScreen.isShown(),
+        ].flat(),
+});
+
+registry.category("web_tour.tours").add("PoSDownPaymentFixedTax", {
+    steps: () =>
+        [
+            Dialog.confirm("Open session"),
+            ProductScreen.controlButton("Quotation/Order"),
+            ProductScreen.downPayment20PercentFirstOrder(),
+            Order.hasLine({
+                productName: "Down Payment",
+                quantity: "1.0",
+                price: "1.00",
+            }),
+            Order.hasLine({
+                productName: "Down Payment",
+                quantity: "1.0",
+                price: "22.00",
+            }),
         ].flat(),
 });
