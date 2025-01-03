574816e652a6a94fe75b38ea6f917c58c1d59329
Julien Banken <jbn@odoo.com>
2025-01-03 17:38:31 +0100

    [IMP] mail: move web push configuration to mixin
    This commit will move some of the fields of the `mail.push.device` push
    device into a mixin so that they can be reused for the push notifications
    in the `social` and `event` module.
    
    Task-4316624

--- addons/mail/models/__init__.py
+++ addons/mail/models/__init__.py
@@ -1,5 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import push_device_config
+
 # core models (required for mixins)
 from . import mail_alias
 from . import mail_alias_domain
