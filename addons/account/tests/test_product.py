6c19d0b5cdedd5f0ec032121ab0e91f0858a4852
Emanuele <emi@odoo.com>
2025-01-06 10:20:48 +0000

    [FIX] account: replace type of product in test
    Issue:
    - run the `test_account_manager_user_can_create_product` with `account`and its dependencies as the only modules installed
    - error is given as `product` is not a valid product type (it is added in the `stock` module)
    
    Solution:
    - replace `product` with a product type that is defined in one of the dependencies
    
    X-original-commit: de3c8fc2c725e558ca185a25a21351aaa01b2335

--- addons/account/tests/test_product.py
+++ addons/account/tests/test_product.py
@@ -52,3 +52,10 @@ class TestProduct(AccountTestInvoicingCommon):
             'taxes_id': self.company_data['company'].account_sale_tax_id.ids,
             'supplier_taxes_id': self.company_data['company'].account_purchase_tax_id.ids,
         }])
+
+    def test_account_manager_user_can_create_product(self):
+        """Test that a user with group_account_manager can create a product."""
+        product = self.env['product.product'].with_user(self.account_manager_user).create({
+            'name': 'Test Accountant', 'type': 'consu', 'list_price': 50.0,
+        })
+        self.assertTrue(product)
