189d7f0e6f406c601b4732b031701a227f094762
Djamel Touati <otd@odoo.com>
2025-01-07 18:03:27 +0100

    [FIX] stock_picking_batch: create a new wave for SML with 0 quantity
    Steps to reproduce the bug:
    - Enable the multi-step route.
    - Go to the Delivery Operation Type:
        - Enable "Automatic Batches".
            - Enable "Wave Grouping".
                - Product Category: All
    - Create two storable products: "P1" and "P2".
    - Create a first delivery:
        - One unit of P1.
        - Mark it as "To Do" → a wave transfer is created.
    - Create a second delivery:
        - One unit of P2.
        - Mark it as "To Do" → it is added to the first wave.
    - Go to the wave:
         - Update the quantity of P2 to 0.
    - Validate the wave
    
    Problem:
    The picking of P2 is detached but not linked to a new wave.
    
    opw-4444263

--- addons/stock_picking_batch/models/stock_move_line.py
+++ addons/stock_picking_batch/models/stock_move_line.py
@@ -96,8 +96,7 @@ class StockMoveLine(models.Model):
     def _is_auto_waveable(self):
         self.ensure_one()
         if not self.picking_id \
-           or self.picking_id.state != 'assigned' \
-           or float_is_zero(self.quantity, precision_rounding=self.product_uom_id.rounding) \
+           or (self.picking_id.state != 'assigned' or float_is_zero(self.quantity, precision_rounding=self.product_uom_id.rounding)) and not self.env.context.get('skip_auto_waveable')  \
            or self.batch_id.is_wave \
            or not self.picking_type_id._is_auto_wave_grouped() \
            or (self.picking_type_id.wave_group_by_category and self.product_id.categ_id not in self.picking_type_id.wave_category_ids):  # noqa: SIM103
