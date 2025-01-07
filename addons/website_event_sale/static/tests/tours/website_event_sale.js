0c6f5aecd96017a7a0d8c7f9dada490c96577fd5
Pierre Pulinckx (PIPU) <pipu@odoo.com>
2025-01-07 09:00:28 +0100

    [FIX] *: wait modal is show before continue the tour
    In this commit, we wait the modal is shown (managed by jQuery) before
    to continue the tour. In these modals, if we don't wait for the modal
    is shown, as the tour engine can be too much fast, element in modal
    can be not focusable when we try to target it.
    
    In odoo/addons/website/static/src/js/content/website_root.js
    the class modal_show is added in _onModalShown:
    
    ```javascript
    _onModalShown: function (ev) {
    	$(ev.target).addClass('modal_shown');
    },
    ```
    
    This fix add classes in triggers to ensure modal is shown before
    continuing the tour.

--- addons/website_event_sale/static/tests/tours/website_event_sale.js
+++ addons/website_event_sale/static/tests/tours/website_event_sale.js
@@ -41,9 +41,8 @@ registry.category("web_tour.tours").add("event_buy_tickets", {
             run: "click",
         },
         {
-            content: "Fill attendees details",
-            trigger: '.modal form[id="attendee_registration"] .btn[type=submit]',
-            run: "hover",
+            content: "Wait the modal is shown before continue",
+            trigger: ".modal.modal_shown.show form[id=attendee_registration]",
         },
         {
             trigger: ".modal#modal_attendees_registration input[name*='1-email']",
