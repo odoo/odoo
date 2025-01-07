bb822ced96790b8a2fb17267dc73a05e13a06330
Walid (wasa) <wasa@odoo.com>
2025-01-07 08:59:38 +0100

    [FIX] html_editor: reset selection when editable is empty
    **Problem**:
    When selecting all content (`Ctrl+A`) in an empty editable area, the selection includes only a `<br>` element. This causes issues when interacting with non-selectable content inside the selection, leading to unexpected behavior.
    
    **Solution**:
    If the selection contains only on `br` element, reset the selection on click to avoid inconsistent states.
    
    **Steps to Reproduce**:
    1. Open the editor.
    2. Press `Ctrl+A` to select all.
    3. Click anywhere within the editable area to hide the toolbar.
    4. Observe that the toolbar remains visible, and the selection state does not update.
    
    opw-4438513

--- addons/html_editor/static/tests/mouse/click.test.js
+++ addons/html_editor/static/tests/mouse/click.test.js
@@ -101,6 +101,15 @@ test("should insert a paragraph before the table, then one after it", async () =
     );
 });
 
+test("should reset selection when empty", async () => {
+    const { el } = await setupEditor("<p>[<br>]</p>");
+    const p = el.querySelector("p");
+    await simulateMouseClick(p, true);
+    expect(getContent(el)).toBe(
+        `<p placeholder='Type "/" for commands' class="o-we-hint">[]<br></p>`
+    );
+});
+
 test.tags("desktop");
 test("should have collapsed selection when mouse down on a table cell", async () => {
     const { el } = await setupEditor(
