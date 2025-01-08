de6cdca1240949a3dba43af930b88cab7d84ba24
chha-odoo <chha@odoo.com>
2025-01-08 18:02:33 +0530

    [FIX] product: setting display uom
    Issue Before This Commit:
    -----------------------------------------------
    - In General Settings, the radio button for Weight is displayed horizontally,
      while the radio button for Volume is displayed vertically. Both units of
      measure need to be displayed in the same manner.
    
    With this commit:
    -----------------------------------------------
    - Both Weight and Volume radio buttons are now displayed vertically to ensure a
      consistent and uniform layout.
    - Added symbols as well, e.g., kg, lb (for weight), and m³, ft³ (for volume).
    
    Task-id: 4438804

--- addons/product/models/res_config_settings.py
+++ addons/product/models/res_config_settings.py
@@ -12,12 +12,12 @@ class ResConfigSettings(models.TransientModel):
     group_product_pricelist = fields.Boolean("Pricelists",
         implied_group='product.group_product_pricelist')
     product_weight_in_lbs = fields.Selection([
-        ('0', 'Kilograms'),
-        ('1', 'Pounds'),
+        ('0', 'Kilograms (kg)'),
+        ('1', 'Pounds (lb)'),
     ], 'Weight unit of measure', config_parameter='product.weight_in_lbs', default='0')
     product_volume_volume_in_cubic_feet = fields.Selection([
-        ('0', 'Cubic Meters'),
-        ('1', 'Cubic Feet'),
+        ('0', 'Cubic Meters (m³)'),
+        ('1', 'Cubic Feet (ft³)'),
     ], 'Volume unit of measure', config_parameter='product.volume_in_cubic_feet', default='0')
 
     @api.onchange('group_product_pricelist')
