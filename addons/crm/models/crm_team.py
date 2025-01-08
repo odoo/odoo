69abc024625885f06ef58d3e061792350a07605b
Lopes Marc (loma) <loma@odoo.com>
2025-01-08 16:13:39 +0000

    [FIX] crm: fix team assignation on lead quick create
    How to reproduce:
    ================
    - ​Have Marc Demo work in another sales team that yours.
    - ​Go to a kanban view of leads, grouped by salesperson.
    - ​Quick create a new lead.
    
    Current behavior:
    ================
    ​It gets assigned to your team, probably because
    the default value is not properly updated.
    
    Expected behavior:
    =================
    - ​The lead belongs to Marc and should be linked to his team.
    - ​It works fine if you do it from the form view.
    
    The issue is caused by the default_team_id set by the action associated
    with the CRM menu. Keeping it does not seem necessary, an update of the
    codebase on 16.0 set crm_lead.team_id as a precomputed field.
    
    Aslo, an old fix on multi-company lead creation is removed from the same
    action, as it is no longer necessary (odoo/odoo#45234).
    
    Finally, a slight modification is made to the rainbow man tour to prevent
    the test from breaking on more recent versions of Odoo.
    
    task-4438021
    
    closes odoo/odoo#200210
    
    X-original-commit: 282b2de0a5378a77cef04ff160663f3f429401f3
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>
    Signed-off-by: Marc Lopes (loma) <loma@odoo.com>

--- addons/crm/models/crm_team.py
+++ addons/crm/models/crm_team.py
@@ -600,10 +600,7 @@ class Team(models.Model):
     @api.model
     def _action_update_to_pipeline(self, action):
         user_team_id = self.env.user.sale_team_id.id
-        if user_team_id:
-            # To ensure that the team is readable in multi company
-            user_team_id = self.search([('id', '=', user_team_id)], limit=1).id
-        else:
+        if not user_team_id:
             user_team_id = self.search([], limit=1).id
             action['help'] = "<p class='o_view_nocontent_smiling_face'>%s</p><p>" % _("Create an Opportunity")
             if user_team_id:
@@ -615,9 +612,6 @@ class Team(models.Model):
                     action['help'] += "<p>%s</p>" % _("""As you are a member of no Sales Team, you are showed the Pipeline of the <b>first team by default.</b>
                                         To work with the CRM, you should join a team.""")
         action_context = safe_eval(action['context'], {'uid': self.env.uid})
-        if user_team_id:
-            action_context['default_team_id'] = user_team_id
-
         action['context'] = action_context
         return action
 
