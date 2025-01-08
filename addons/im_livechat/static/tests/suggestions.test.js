ff4175f3df808d6657af2b3bd7649c3c44438960
Alexandre Kühn <aku@odoo.com>
2025-01-08 18:08:50 +0100

    [IMP] mail: canned response with :: instead of :
    Before this commit, delimiter to use canned response
    was ":". This puts some conflicts with emoji substitution,
    which has been minimized a bit by not putting the focus on
    the suggestion list of emojis.
    
    However this is still bothersome, so it's worth changing it
    slightly. This PR minimises the impact of change by replacing
    it to "::", which is relatively close to the former ":".
    
    Since the discovery of the feature was only through typing ":",
    we added a new item in the "+" button of composer to insert a
    new canned response, which simply adds the "::" in the composer
    to help select the canned response. This composer action only
    shows the shortcut to trigger canned response: it let the
    user learns of this feature, and in case the delimiter changes
    like with this PR, the user can relatively easily re-learn the
    new trigger thanks to this composer action.
    
    The "Insert a Canned response" composer action is visible when
    the user can at least find a canned response. Also the canned
    response suggestions are focused by default, now that they do
    not conflict with emoji substitution.
    
    Task-4110505
    
    closes odoo/odoo#192953
    
    Related: odoo/enterprise#77184
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/im_livechat/static/tests/suggestions.test.js
+++ addons/im_livechat/static/tests/suggestions.test.js
@@ -12,7 +12,7 @@ import { defineLivechatModels } from "./livechat_test_helpers";
 describe.current.tags("desktop");
 defineLivechatModels();
 
-test("Suggestions are shown after delimiter was used in text (:)", async () => {
+test("Suggestions are shown after delimiter was used in text (::)", async () => {
     const pyEnv = await startServer();
     pyEnv["mail.canned.response"].create({
         source: "hello",
@@ -29,11 +29,11 @@ test("Suggestions are shown after delimiter was used in text (:)", async () => {
     });
     await start();
     await openDiscuss(channelId);
-    await insertText(".o-mail-Composer-input", ":");
+    await insertText(".o-mail-Composer-input", "::");
     await contains(".o-mail-Composer-suggestion strong", { text: "hello" });
     await insertText(".o-mail-Composer-input", ")");
     await contains(".o-mail-Composer-suggestion strong", { count: 0 });
-    await insertText(".o-mail-Composer-input", " :");
+    await insertText(".o-mail-Composer-input", " ::");
     await contains(".o-mail-Composer-suggestion strong", { text: "hello" });
 });
 
