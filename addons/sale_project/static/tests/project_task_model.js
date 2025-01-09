bbbe0f3c3b5eb4bece417196fa4fd0a983049adc
Maxime de Neuville <mane@odoo.com>
2025-01-09 15:38:22 +0100

    [IMP] sale_{project,timesheet}: add an SO creation button from the SOL field
    Purpose:
    -------------------
    In v17, we introduced the possibility of creating SOLs on the fly from tasks or tickets.
    This feature has been a great source of bugs that are difficult to fix, only to break again later.
    We are also limited in the tests we can have to ensure this feature remains stable.
    Therefore, we are replacing it with a button to create an SO instead.
    
    Details:
    -------------------
    * We replace the creation of SOL on the fly by an SO creation button at multiple places:
        - Task form view
        - Employee mapping list view
        - Ticket form view
    
    * This new button (custom widget 'so_line_create_button) is placed at the right of the SOL field and takes the shape of the "fa-plus" icon. It is displayed if no SOL has been defined yet and appears on hover.
    * It is implemented for the task, ticket, and employee mapping models.
    * It's only visible if the user has "sales > user: own documents" access rights.
    * Clicking on the button opens the SO form view and fills in some information (company, partner, etc) from the context from which it's opened.
    * Saving the SO from that form creates a new confirmed SO and links the SOL containing a service product with the lowest sequence to the task/ticket/employee mapping. If no service product was mentioned in the SO, an error is raised.
    * No projects/tasks are generated in the process (if some products were configured to generate some).
    * We removed tests and tours dedicated to the "SOL on the fly" functionality, and added new tests for the new button and the widget.
    * If empty, the re-invoiced SO of projects is automatically set to the sales order of the project's sales order item.
    
    task-4461919
    
    closes odoo/odoo#193079
    
    Related: odoo/enterprise#77071
    Related: odoo/upgrade#7064
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/sale_project/static/tests/project_task_model.js
+++ addons/sale_project/static/tests/project_task_model.js
@@ -15,10 +15,40 @@ export class ProjectMilestone extends projectModels.ProjectMilestone {
     quantity_percentage = fields.Float({ string: "Percentage" });
 }
 
+export class SaleOrder extends models.Model {
+    _name = "sale.order";
+
+    name = fields.Char({ string: "name" });
+    partner_id = fields.Many2one({ string: "Customer", relation: "res.partner" });
+    project_id = fields.Many2one({ string: "Project", relation: "project.project" });
+    order_line = fields.One2many({ relation: "sale.order.line" });
+
+    _records = [{ id: 1, name: "Sales Order 1" }];
+}
+
 export class SaleOrderLine extends models.Model {
     _name = "sale.order.line";
 
-    name = fields.Char({ string: "name" });
+    name = fields.Char({ related: "product_id.name" });
+    product_id = fields.Many2one({ string: "Product", relation: "product.product" });
+
+    _records = [{ id: 1, product_id: 1 }];
+}
+
+export class ProductProduct extends models.Model {
+    _name = "product.product";
+
+    name = fields.Char();
+    type = fields.Selection({
+        string: "Type",
+        selection: [("consu", "Goods"), ("service", "Service"), ("combo", "Combo")],
+    });
+
+    _records = [
+        { id: 1, name: "Service Product 1", type: "service" },
+        { id: 2, name: "Consumable Product 1", type: "consu" },
+        { id: 3, name: "Service Product 2", type: "service" },
+    ];
 }
 
 Object.assign(projectModels, {
