0b9a0f6a674bda00f3329eb590bb968e7de362d0
Pierre Paridans <app@odoo.com>
2025-01-09 20:38:41 +0000

    [FIX] tests: use devtools emulated devices for Chrome viewport size
    To run tours or unit tests, we instrument Chrome and run it headlessly.
    One of the arguments passed while doing so is the "window size" it needs
    to use to emulate desktop-like or mobile-like screen sizes.
    
    Newer versions of Chrome sometimes introduce slight variation between
    the Chrome's window size and its viewport size (due to changes in the
    interface layout...). While often not a deal-breaker, Chrome 128 forces
    a min-width for the window greater than the breakpoint we use for
    smaller/mobile-like testing (e.g. 375px, which matches a small
    smartphone).
    
    This commit changes the way we instrument Chrome to use a specific size
    by using the DevTools' Emulated Device (aka. "responsive mode"). It
    allows to apply a very specific viewport's size independently of the
    actual Chrome's window size, which makes it way more predictable than
    before.
    
    Note: this change is part of the preparation to support the "new"
    headless mode, which becomes the default starting with Chrome 128.
    
    Reference:
    - https://chromedevtools.github.io/devtools-protocol/tot/Emulation/#method-setDeviceMetricsOverride
    
    closes odoo/odoo#193847
    
    X-original-commit: 8c90529757cd0a2b165c63a1704c13627c703cbc
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>
    Signed-off-by: Pierre Paridans (app) <app@odoo.com>

--- odoo/tests/common.py
+++ odoo/tests/common.py
@@ -946,7 +946,6 @@ class ChromeBrowser:
 
         self.chrome, self.devtools_port = self._chrome_start(
             user_data_dir=self.user_data_dir,
-            window_size=test_case.browser_size,
             touch_enabled=test_case.touch_enabled,
             headless=headless,
             debug=debug,
@@ -976,6 +975,14 @@ class ChromeBrowser:
         self._websocket_send('Runtime.enable')
         self._logger.info('Chrome headless enable page notifications')
         self._websocket_send('Page.enable')
+        emulated_device = {
+            'mobile': False,
+            'width': None,
+            'height': None,
+            'deviceScaleFactor': 1,
+        }
+        emulated_device['width'], emulated_device['height'] = [int(size) for size in test_case.browser_size.split(",")]
+        self._websocket_request('Emulation.setDeviceMetricsOverride', params=emulated_device)
 
     @property
     def screencasts_frames_dir(self):
@@ -1056,7 +1063,7 @@ class ChromeBrowser:
     def _chrome_start(
             self,
             user_data_dir: str,
-            window_size: str, touch_enabled: bool,
+            touch_enabled: bool,
             headless=True,
             debug=False,
     ):
@@ -1085,7 +1092,6 @@ class ChromeBrowser:
             '--remote-debugging-address': HOST,
             '--remote-debugging-port': str(self.remote_debugging_port),
             '--user-data-dir': user_data_dir,
-            '--window-size': window_size,
             '--no-first-run': '',
             # '--enable-precise-memory-info': '',  # uncomment to debug memory leaks in unit tests
             # FIXME: the next flag is temporarily uncommented to allow client
