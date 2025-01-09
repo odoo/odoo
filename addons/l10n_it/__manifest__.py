d832049d43669330e3323967408572f4d64d4cba
Thomas Becquevort <thbe@odoo.com>
2025-01-09 14:02:46 +0100

    [IMP] l10n_it: Split Periodic & Annual Tax Reports
    Description of the issue this commit addresses:
    
    The Italian tax report lacks clarity and can be improved both in readabilty and
    usability by splitting the monthly and the annual reports.
    
    Desired behavior after this commit is merged:
    
    The Italian tax report is now split in a monthly and an annual report.
    The report uses the sections mechanism to be split in the different parts it is
    made from and clean the UI by not having everything on the same sreen.
    
    Task-3479785
    
    X-original-commit: 2ea2fa0f347d9a864d28b8e45d535cda2f09027a

--- addons/l10n_it/__manifest__.py
+++ addons/l10n_it/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': 'Italy - Accounting',
     'countries': ['it'],
-    'version': '0.6',
+    'version': '0.7',
     'depends': [
         'account',
         'base_iban',
@@ -20,6 +20,14 @@ Italian accounting chart and localization.
     'website': 'https://www.odoo.com/documentation/saas-17.2/applications/finance/fiscal_localizations/italy.html',
     'data': [
         'data/account_account_tag.xml',
+        'data/tax_report/account_monthly_tax_report_data.xml',
+        'data/tax_report/annual_report_sections/va.xml',
+        'data/tax_report/annual_report_sections/ve.xml',
+        'data/tax_report/annual_report_sections/vf.xml',
+        'data/tax_report/annual_report_sections/vh.xml',
+        'data/tax_report/annual_report_sections/vj.xml',
+        'data/tax_report/annual_report_sections/vl.xml',
+        'data/tax_report/account_annual_tax_report_data.xml',
         'data/account_tax_report_data.xml',
         'views/account_tax_views.xml'
     ],
