af6d93cccaf1e360381bc9cb7b6057d55b39b9da
Ethan Vincent <etvi@odoo.com>
2025-01-09 10:55:05 +0100

    [FIX] mrp{,_subcontracting_purchase}: make MO date set consistent
    **Current behavior:**
    Having some product with a subcontract BoM and using the
    unarchived 'Subcontract' picking type and creating a purchase
    order to the subcontractor for that product: changing the
    `scheduled_date` of the generated receipt will improperly set
    the start/end date on the subcontracting MO which was generated
    alongside the receipt.
    
    Specifically, the `scheduled_date` of the receipt should
    correspond to the `date_finished` of the MO- however currently
    it is incorrectly modifying the MO `date_start` instead.
    
    **Expected behavior:**
    Modifying `scheduled_date` on the receipt will in turn modify
    the `date_finished` of the MO.
    
    **Steps to reproduce:**
    *Unarchive the Subcontract picking type operation*
    1. Create a final product with a subcontract type BoM, on the
    BoM set some positive value for Manuf. Lead Time (in
    miscellaneous tab)
    
    2. Create a purchase order to the subcontractor for that product
    
    3. Confirm the PO -> observe that on creation, the
    scheduled date of the receipt matches the end date of the MO
    
    4. Change the scheduled date of the receipt (for example, 10
    days in the future) -> observe that after saving, the MO date
    correspondence is inverted (end date = scheduled date + lead
    time) instead of the start date being set with respect to the
    end time (like on creation), the end date gets set with respect
    to the start date
    
    **Cause of the issue:**
    It's an inconsistency with how the fields (start, end) are
    calculated and when.
    
    **Fix:**
    Make this case behave the same as the creation does- that is,
    end date is informed by the receipt's scheduled date and the
    start date of the MO is set by subtracting the lead time from
    there.
    
    opw-4231548

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -867,7 +867,18 @@ class MrpProduction(models.Model):
                 if production.state == 'draft' and picking_type != production.picking_type_id:
                     production.name = picking_type.sequence_id.next_by_id()
 
-        res = super(MrpProduction, self).write(vals)
+        date_start_map = dict()
+        if 'date_start' in vals:
+            date_start_map = {
+                prod: vals['date_start'] - datetime.timedelta(days=prod.bom_id.produce_delay)
+                if prod.bom_id else vals['date_start']
+                for prod in self
+            }
+            res = True
+            for production in self:
+                res &= super(MrpProduction, production).write({**vals, 'date_start': date_start_map[production]})
+        else:
+            res = super().write(vals)
 
         for production in self:
             if 'date_start' in vals and not self.env.context.get('force_date', False):
@@ -892,7 +903,7 @@ class MrpProduction(models.Model):
                 if 'qty_producing' in vals:
                     finished_move.quantity = vals.get('qty_producing')
             if self._has_workorders() and not production.workorder_ids.operation_id and vals.get('date_start') and not vals.get('date_finished'):
-                new_date_start = fields.Datetime.to_datetime(vals.get('date_start'))
+                new_date_start = fields.Datetime.to_datetime(production.date_start)
                 if not production.date_finished or new_date_start >= production.date_finished:
                     production.date_finished = new_date_start + datetime.timedelta(hours=1)
         return res
