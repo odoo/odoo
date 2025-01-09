81e5f78c1d08fb8002694c98d7682ff64f5e6a26
Xavier Bol (xbo) <xbo@odoo.com>
2025-01-09 16:06:44 +0000

    [FIX] project_todo: allow user to edit tag and users in todo on mobile
    Before this commit, when the user is on mobile and try to edit/create a
    new todo, the user cannot edit a tag or users because those fields are
    displayed in actions dropdown and that dropdown is closed each time the
    user clicks on one of those fields.
    
    This commit prevents to close the dropdown in todo form view since that
    dropdown contains fields and not buttons to let the user to edit those
    fields.
    
    X-original-commit: 20cd56a3442e5bf7d579b0be6f5c14d2c92b05a5

--- addons/project_todo/static/src/views/todo_form/todo_form_renderer.js
+++ addons/project_todo/static/src/views/todo_form/todo_form_renderer.js
@@ -1,3 +1,4 @@
+<<<<<<< 18.0
 import { FormRendererWithHtmlExpander } from "@resource/views/form_with_html_expander/form_renderer_with_html_expander";
 import { useBus } from "@web/core/utils/hooks";
 
@@ -15,4 +16,18 @@ export class TodoFormRenderer extends FormRendererWithHtmlExpander {
     _canExpandHTMLField(size) {
         return size >= this.sizeToExpandHTMLField;
     }
+||||||| 1fb35add8090b5fc9e706aad67f6bb38432273e0
+=======
+/* @odoo-module */
+
+import { FormRenderer } from "@web/views/form/form_renderer";
+
+import { TodoFormStatusBarButtons } from "./todo_form_status_bar_button";
+
+export class TodoFormRenderer extends FormRenderer {
+    static components = {
+        ...FormRenderer.components,
+        StatusBarButtons: TodoFormStatusBarButtons,
+    };
+>>>>>>> da59bf1857979ad7dae5008bc3c832ae6619a517
 }
--- addons/project_todo/static/src/views/todo_form/todo_form_view.js
+++ addons/project_todo/static/src/views/todo_form/todo_form_view.js
@@ -1,13 +1,21 @@
 import { registry } from "@web/core/registry";
 import { formView } from "@web/views/form/form_view";
 import { TodoFormController } from "./todo_form_controller";
+<<<<<<< 18.0
 import { TodoFormControlPanel } from "./todo_form_control_panel";
+||||||| 1fb35add8090b5fc9e706aad67f6bb38432273e0
+=======
+>>>>>>> da59bf1857979ad7dae5008bc3c832ae6619a517
 import { TodoFormRenderer } from "./todo_form_renderer";
 
 export const todoFormView = {
     ...formView,
     Controller: TodoFormController,
+<<<<<<< 18.0
     ControlPanel: TodoFormControlPanel,
+||||||| 1fb35add8090b5fc9e706aad67f6bb38432273e0
+=======
+>>>>>>> da59bf1857979ad7dae5008bc3c832ae6619a517
     Renderer: TodoFormRenderer,
 };
 
