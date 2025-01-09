2a86fc98d53ffa7570b17e4bc56fc1149de58ad0
Adrien Widart (awt) <awt@odoo.com>
2025-01-09 15:22:21 +0100

    [FIX] repair,sale_stock,stock_account: repair with auto FIFO component
    When using an auto fifo product as part of the repair, the COGS
    won't be based on the used product
    
    To reproduce the issue:
    1. Setup auto fifo product
    2. Receive 1@10 and 1@20
    3. Process a RO:
       - Invoice method: After repair
       - Parts:
         - Add 1 x fifo product
    4. Create and post the invoice
    5. Open its journal items
    
    Error: Cogs are $20 instead of $10
    
    When posting the invoice, we generate the COGS:
    https://github.com/odoo/odoo/blob/4df156164cf1d2764ba23682beee588777457fd6/addons/stock_account/models/account_move.py#L47-L48
    We therefore compute the "anglo saxon unit price":
    https://github.com/odoo/odoo/blob/4df156164cf1d2764ba23682beee588777457fd6/addons/stock_account/models/account_move.py#L133
    However, there isn't any override to handle the RO case, so it leads
    to the default mechanism, i.e. the standard price of the product:
    https://github.com/odoo/odoo/blob/4df156164cf1d2764ba23682beee588777457fd6/addons/stock_account/models/account_move.py#L294-L295
    https://github.com/odoo/odoo/blob/4df156164cf1d2764ba23682beee588777457fd6/addons/stock_account/models/account_move.py#L294-L295
    https://github.com/odoo/odoo/blob/7cd7563f6708331bb6baf0e06d07a9f9ee329e38/addons/stock_account/models/product.py#L753-L757
    
    And, since the first product is out, its standard price is now based
    on the next candidate: $20
    
    About the `sudo`: an accountman has not any access to `repair`, so
    posting such an invoice would raise an error. Since this diff is
    specific to Odoo 16, the idea is not to impact any security rules
    and rather minimize the changes.
    
    Note: Indeed, `repair` does not depend on `stock_account`, so this
    commit could lead to a traceback if the bridge is removed. I delegate
    this issue to the error of dependencies. Anyway, removing the bridge
    would lead to other bugs. Hopefully, this has been fixed on master [1].
    
    [1] f7dbdec11b74f8c7d969763d8c5cf09542a47f86
    
    OPW-4166570

--- addons/repair/models/account_move.py
+++ addons/repair/models/account_move.py
@@ -20,3 +20,12 @@ class AccountMoveLine(models.Model):
 
     repair_line_ids = fields.One2many('repair.line', 'invoice_line_id', readonly=True, copy=False)
     repair_fee_ids = fields.One2many('repair.fee', 'invoice_line_id', readonly=True, copy=False)
+
+    def _stock_account_get_anglo_saxon_price_unit(self):
+        price_unit = super()._stock_account_get_anglo_saxon_price_unit()
+        ro_line = self.sudo().repair_line_ids
+        if ro_line:
+            am = ro_line.invoice_line_id.move_id.sudo(False)
+            sm = ro_line.move_id.sudo(False)
+            price_unit = self._deduce_anglo_saxon_unit_price(am, sm)
+        return price_unit
