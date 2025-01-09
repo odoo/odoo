b738098c5159e7c1d7c5533cae7354a72cd84a6f
Maximilien (malb) <malb@odoo.com>
2025-01-09 08:41:38 +0100

    [IMP] l10n_tr_nilvera{_einvoice}: unit codes legal requirement
    In the xml we sent, we have to specify the unit code, there is mapping for that
    on the documentation: https://developer.nilvera.com/en/code-lists#birim-kodlari
    
    This commit adds some units of measure to the uom_to_unece_code.
    We will override the function _get_uom_unece_code and if the uom is the default
    one then we try to find the unece code with new UOM.
    The default value is C62.
    
    task-4457115
    
    closes odoo/odoo#193030
    
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>

--- addons/l10n_tr_nilvera/__manifest__.py
+++ addons/l10n_tr_nilvera/__manifest__.py
@@ -10,6 +10,7 @@ Base module containing core functionalities required by other Nilvera modules.
         'security/ir.model.access.csv',
         'views/res_config_settings_views.xml',
         'views/res_partner_views.xml',
+        'data/uom_data.xml',
     ],
     'post_init_hook': '_l10n_tr_nilvera_post_init',
     'license': 'LGPL-3',
