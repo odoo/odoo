cfffa535fdbab6569498b4b230258c1c040c0335
manv-afk <manv@odoo.com>
2025-01-10 13:55:19 +0100

    [FIX] point_of_sale: fix load onboarding scenario from company branch
    - `point_of_sale`: when creating cash payment methods, we now use the default cash account if it exists.
    - `l10n_be_pos_restaurant`: filter taxes to create to avoid recreating same tax in sub company
    
    closes odoo/odoo#193275
    
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- addons/l10n_be_pos_restaurant/models/template_be.py
+++ addons/l10n_be_pos_restaurant/models/template_be.py
@@ -8,6 +8,10 @@ class AccountChartTemplate(models.AbstractModel):
 
     @template('be', 'account.tax')
     def _get_be_pos_restaurant_account_tax(self):
-        pos_taxes = self._parse_csv('be', 'account.tax', module='l10n_be_pos_restaurant')
-        self._deref_account_tags('be_comp', pos_taxes)
-        return pos_taxes
+        be_restaurant_tax = self._parse_csv('be', 'account.tax', module='l10n_be_pos_restaurant')
+        existing_taxes = self.env['account.tax'].search([('company_id', 'child_of', self.env.company.root_id.id)])
+        # Filter out taxes that already exist
+        existing_tax_names = set(existing_taxes.mapped('name'))
+        taxes_to_create = {name: tax for name, tax in be_restaurant_tax.items() if tax['name'] not in existing_tax_names}
+        self._deref_account_tags('be_comp', be_restaurant_tax)
+        return taxes_to_create
