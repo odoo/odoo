d21557992e166152cf0e4414793c58372b87b0af
Shrey Mehta <shrm@odoo.com>
2025-01-10 15:37:21 +0530

    [IMP] sale: onboard user with stripe connect
    Prior this commit:
    Users were not invited to configure online payments after the removal of the
    config bar for onboarding.
    
    Post this commit:
    When online payments are activated for Sales or Invoicing, users are onboarded
    to configure Stripe Connect directly in the settings.
    
    task-4156274

--- addons/sale/wizard/res_config_settings.py
+++ addons/sale/wizard/res_config_settings.py
@@ -57,6 +57,17 @@ class ResConfigSettings(models.TransientModel):
     prepayment_percent = fields.Float(
         related='company_id.prepayment_percent',
         readonly=False)
+    is_stripe_supported_country = fields.Boolean(
+        related='company_id.country_id.is_stripe_supported_country')
+    providers_state = fields.Selection(
+        selection=[
+            ('none', 'None'),
+            ('paypal_only', 'Paypal Only'),
+            ('other_than_paypal', 'Other than Paypal')
+        ],
+        compute='_compute_providers_state')
+    first_provider_label = fields.Char(
+        compute='_compute_providers_state')
 
     # Modules
     module_delivery = fields.Boolean("Delivery Methods")
@@ -124,3 +135,52 @@ class ResConfigSettings(models.TransientModel):
         send_invoice_cron = self.env.ref('sale.send_invoice_cron', raise_if_not_found=False)
         if send_invoice_cron and send_invoice_cron.active != self.automatic_invoice:
             send_invoice_cron.active = self.automatic_invoice
+
+    def _get_activated_providers(self):
+        self.ensure_one()
+        wire_transfer = self.env.ref('payment.payment_provider_transfer', raise_if_not_found=False)
+        return self.env['payment.provider'].search([
+            ('state', '!=', 'disabled'),
+            ('id', '!=', wire_transfer.id if wire_transfer else False),
+        ])
+
+    def _compute_providers_state(self):
+        paypal = self.env.ref('payment.payment_provider_paypal', raise_if_not_found=False)
+        stripe = self.env.ref('payment.payment_provider_stripe', raise_if_not_found=False)
+        for config in self:
+            providers = config._get_activated_providers()
+            first_provider = (stripe if stripe and stripe in providers
+                             else providers[0] if providers
+                             else providers
+                            )
+            config.first_provider_label = _('Configure %s', first_provider.name)
+            if len(providers) == 1 and providers == paypal:
+                config.providers_state = 'paypal_only'
+            elif len(providers) >= 1:
+                config.providers_state = 'other_than_paypal'
+            else:
+                config.providers_state = 'none'
+
+    def action_activate_stripe(self):
+        self.ensure_one()
+        if not self.is_stripe_supported_country:
+            return False
+        menu = self.env.ref('sale.menu_sale_general_settings', raise_if_not_found=False)
+        menu_id = menu and menu.id
+        return self.env.company._run_payment_onboarding_step(menu_id=menu_id)
+
+    def action_configure_first_provider(self):
+        self.ensure_one()
+        stripe = self.env['payment.provider'].search([
+            *self.env['payment.provider']._check_company_domain(self.env.company),
+            ('code', '=', 'stripe')
+        ], limit=1)
+        providers = self._get_activated_providers()
+        return {
+            'name': self.first_provider_label,
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'payment.provider',
+            'views': [[False, 'form']],
+            'res_id': stripe.id if stripe in providers else providers[0].id,
+        }
