b9449358fc0b52c049368a7b5f539eae09c0c2ce
Stephane Vanmeerhaeghe <stva@odoo.com>
2025-01-10 16:10:55 +0100

    [IMP] pos_restaurant: send dishes by course
    This commit introduces the ability to sort order lines by course (e.g., starters, mains, desserts)  and trigger course notifications for the kitchen. The kitchen will know when to prepare each course to ensure that the client do not wait too much.
    
    closes odoo/odoo#194367
    
    Task: 4444501
    Related: odoo/enterprise#77426
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/pos_restaurant/static/src/app/components/order_display/order_display.js
+++ addons/pos_restaurant/static/src/app/components/order_display/order_display.js
@@ -1,7 +1,7 @@
 import { patch } from "@web/core/utils/patch";
 import { _t } from "@web/core/l10n/translation";
 import { OrderDisplay } from "@point_of_sale/app/components/order_display/order_display";
-
+import { OrderCourse } from "@pos_restaurant/app/components/order_course/order_course";
 patch(OrderDisplay.prototype, {
     emptyCartText() {
         let text = super.emptyCartText(...arguments);
@@ -10,4 +10,16 @@ patch(OrderDisplay.prototype, {
         }
         return text;
     },
+
+    get displayCourses() {
+        if (
+            !this.order.config.module_pos_restaurant ||
+            this.props.mode !== "display" ||
+            this.order.finalized
+        ) {
+            return false;
+        }
+        return this.order.course_ids.length > 0;
+    },
 });
+OrderDisplay.components = { ...OrderDisplay.components, OrderCourse };
