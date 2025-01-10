3ad9d352bb145bab57a8e7ea1bd4fe9565a720d3
Shrey Mehta <shrm@odoo.com>
2025-01-10 15:37:21 +0530

    [IMP] payment, _*: onboard user with stripe or razorpay
    _*= sale, website_payment, account_payment
    
    Prior this commit:
    Users were not invited to configure online payments after the removal of the
    config bar for onboarding.
    
    Post this commit:
    When online payments are activated for Sales or Invoicing, users are onboarded
    to configure Stripe Connect or Razorpay directly in the settings.
    
    task-4156274

--- addons/payment/models/res_company.py
+++ addons/payment/models/res_company.py
@@ -1,50 +1,42 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    payment_onboarding_payment_method = fields.Selection(
-        string="Selected onboarding payment method",
-        selection=[
-            ('paypal', "PayPal"),
-            ('stripe', "Stripe"),
-            ('manual', "Manual"),
-            ('other', "Other"),
-        ])
-
-    def _run_payment_onboarding_step(self, menu_id=None):
+    def _run_payment_onboarding_step(self, provider_code, menu_id=None):
         """ Install the suggested payment modules and configure the providers.
 
         It's checked that the current company has a Chart of Account.
 
         :param int menu_id: The menu from which the user started the onboarding step, as an
                             `ir.ui.menu` id
-        :return: The action returned by `action_stripe_connect_account`
+        :param str provider_code: The code of provider for which the onboarding step is started.
+        :return: The action returned by `_run_onboarding_action`
         :rtype: dict
         """
         self.env.company.get_chart_of_accounts_or_fail()
 
-        self._install_modules(['payment_stripe'])
+        self._install_modules([f'payment_{provider_code}'])
 
         # Create a new env including the freshly installed module(s)
         new_env = api.Environment(self.env.cr, self.env.uid, self.env.context)
 
-        # Configure Stripe
-        stripe_provider = new_env['payment.provider'].search([
+        # Configure Provider
+        provider = new_env['payment.provider'].search([
             *self.env['payment.provider']._check_company_domain(self.env.company),
-            ('code', '=', 'stripe')
+            ('code', '=', provider_code),
         ], limit=1)
-        if not stripe_provider:
-            base_provider = self.env.ref('payment.payment_provider_stripe')
+        if not provider:
+            base_provider = self.env.ref(f'payment.payment_provider_{provider_code}')
             # Use sudo to access payment provider record that can be in different company.
-            stripe_provider = base_provider.sudo().with_context(
-                stripe_connect_onboarding=True,
+            provider = base_provider.sudo().with_context(
+                provider_onboarding=True,
             ).copy(default={'company_id': self.env.company.id})
 
-        return stripe_provider.action_stripe_connect_account(menu_id=menu_id)
+        return provider._run_onboarding_action(menu_id=menu_id)
 
     def _install_modules(self, module_names):
         modules_sudo = self.env['ir.module.module'].sudo().search([('name', 'in', module_names)])
