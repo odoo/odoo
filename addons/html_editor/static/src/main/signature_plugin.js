6dde1375fc71d5d261bc6ba60312a44d3bf6602e
abd-msyukyu-odoo <abd@odoo.com>
2025-01-13 18:08:22 +0100

    [IMP] html_editor, mail: adapt signature to basecontainer
    This commit adapts the signature feature of the composer to the new
    `baseContainer` of the editor (usage of `div` nodes).
    
    Key features:
    - The signature will no longer be displayed in the small composer
      (there where cases where it was possible to end up with one prior to this
      commit). This is because the signature is html content which is incompatible
      with the TextArea of the small composer (loss of information in the conversion
      html -> text).
    - Signatures are wrapped in a special editor block so that it is easier to
      add/remove/modify it directly, without relying on a regex.
    - Usage of a bus to communicate between the editor and the composer component to
      request the editable content, instead of using selectors in a DOM regulated by
      OWL lifecycle.
    
    task-4260398
    
    Part-of: odoo/odoo#196481
    Related: odoo/enterprise#78545
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/html_editor/static/src/main/signature_plugin.js
+++ addons/html_editor/static/src/main/signature_plugin.js
@@ -3,10 +3,16 @@ import { _t } from "@web/core/l10n/translation";
 import { parseHTML } from "@html_editor/utils/html";
 import { user } from "@web/core/user";
 import { withSequence } from "@html_editor/utils/resource";
+import { renderToString } from "@web/core/utils/render";
+import { markup } from "@odoo/owl";
+import { isEmptyBlock, paragraphRelatedElementsSelector } from "@html_editor/utils/dom_info";
+
+export const SIGNATURE_CLASS = "o-signature-container";
 
 export class SignaturePlugin extends Plugin {
     static id = "signature";
-    static dependencies = ["dom", "history"];
+    static dependencies = ["dom", "history", "selection"];
+    static shared = ["cleanSignatures"];
     resources = {
         user_commands: [
             {
@@ -24,7 +30,16 @@ export class SignaturePlugin extends Plugin {
                 commandId: "insertSignature",
             },
         ],
+        is_empty_predicates: this.isEmpty.bind(this),
+        unsplittable_node_predicates: (host) =>
+            host.nodeType === Node.ELEMENT_NODE && host.matches(`.${SIGNATURE_CLASS}`),
     };
+
+    cleanSignatures({ rootClone }) {
+        for (const el of rootClone.querySelectorAll(`.${SIGNATURE_CLASS}`)) {
+            el.remove();
+        }
+    }
 
     async insertSignature() {
         const [currentUser] = await this.services.orm.read(
@@ -33,8 +48,37 @@ export class SignaturePlugin extends Plugin {
             ["signature"]
         );
         if (currentUser && currentUser.signature) {
-            this.dependencies.dom.insert(parseHTML(this.document, currentUser.signature));
+            // User signature is sanitized in backend.
+            const signatureFragment = parseHTML(
+                this.document,
+                renderToString("html_editor.Signature", {
+                    signature: markup(currentUser.signature),
+                    signatureClass: SIGNATURE_CLASS,
+                })
+            );
+            const signatureBlock = signatureFragment.firstElementChild;
+            this.dependencies.dom.insert(signatureFragment);
+            if (signatureBlock) {
+                const lastPhrasingElement = [
+                    ...signatureBlock.querySelectorAll(paragraphRelatedElementsSelector),
+                ].at(-1);
+                if (lastPhrasingElement) {
+                    this.dependencies.selection.setCursorEnd(lastPhrasingElement);
+                } else {
+                    this.dependencies.selection.setCursorEnd(signatureBlock);
+                }
+            }
             this.dependencies.history.addStep();
+        }
+    }
+
+    isEmpty(element) {
+        if (
+            element.nodeType === Node.ELEMENT_NODE &&
+            element.matches(`.${SIGNATURE_CLASS}`) &&
+            isEmptyBlock(element)
+        ) {
+            return true;
         }
     }
 }
