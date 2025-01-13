e0a25bf23bd6e15cdb8707ffeeb0e93cdec8056d
Krzysztof Magusiak (krma) <krma@odoo.com>
2025-01-13 08:20:57 +0000

    [IMP] sms, snailmail: lower cron trigger frequency
    - sms: lower frequency to daily and add trigger when creating SMS's.
    - snailmail: lower frequency to daily
    
    task-2774819
    
    closes odoo/odoo#191863
    
    Related: odoo/enterprise#76234
    Signed-off-by: Rémy Voet (ryv) <ryv@odoo.com>

--- addons/test_mail_sms/tests/test_sms_performance.py
+++ addons/test_mail_sms/tests/test_sms_performance.py
@@ -36,7 +36,7 @@ class TestSMSPerformance(BaseMailPerformance, sms_common.SMSCase):
     def test_message_sms_record_1_partner(self):
         record = self.test_record.with_user(self.env.user)
         pids = self.customer.ids
-        with self.subTest("QueryCount"), self.mockSMSGateway(sms_allow_unlink=True), self.assertQueryCount(employee=27):  # tms: 27
+        with self.subTest("QueryCount"), self.mockSMSGateway(sms_allow_unlink=True), self.assertQueryCount(employee=31):  # tms: 31
             messages = record._message_sms(
                 body='Performance Test',
                 partner_ids=pids,
@@ -51,7 +51,7 @@ class TestSMSPerformance(BaseMailPerformance, sms_common.SMSCase):
     def test_message_sms_record_10_partners(self):
         record = self.test_record.with_user(self.env.user)
         pids = self.partners.ids
-        with self.subTest("QueryCount"), self.mockSMSGateway(sms_allow_unlink=True), self.assertQueryCount(employee=27):  # tms: 27
+        with self.subTest("QueryCount"), self.mockSMSGateway(sms_allow_unlink=True), self.assertQueryCount(employee=31):  # tms: 31
             messages = record._message_sms(
                 body='Performance Test',
                 partner_ids=pids,
@@ -65,7 +65,7 @@ class TestSMSPerformance(BaseMailPerformance, sms_common.SMSCase):
     @warmup
     def test_message_sms_record_default(self):
         record = self.test_record.with_user(self.env.user)
-        with self.subTest("QueryCount"), self.mockSMSGateway(sms_allow_unlink=True), self.assertQueryCount(employee=28):  # tms: 28
+        with self.subTest("QueryCount"), self.mockSMSGateway(sms_allow_unlink=True), self.assertQueryCount(employee=32):  # tms: 32
             messages = record._message_sms(
                 body='Performance Test',
             )
@@ -119,7 +119,7 @@ class TestSMSMassPerformance(BaseMailPerformance, sms_common.MockSMS):
             'mass_keep_log': False,
         })
 
-        with self.mockSMSGateway(sms_allow_unlink=True), self.assertQueryCount(employee=55):
+        with self.mockSMSGateway(sms_allow_unlink=True), self.assertQueryCount(employee=59):
             composer.action_send_sms()
 
     @mute_logger('odoo.addons.sms.models.sms_sms')
@@ -135,5 +135,5 @@ class TestSMSMassPerformance(BaseMailPerformance, sms_common.MockSMS):
             'mass_keep_log': True,
         })
 
-        with self.mockSMSGateway(sms_allow_unlink=True), self.assertQueryCount(employee=58):
+        with self.mockSMSGateway(sms_allow_unlink=True), self.assertQueryCount(employee=62):
             composer.action_send_sms()
