03db784b85e42297c757d1a76e6396fb50003e8b
jayp-odoo <jayp@odoo.com>
2025-01-13 14:28:14 +0530

    [FIX] hr_holidays: remove unnecessary code
    Clean up `get outOfOfficeText` method by removing unused code and redundant
    conditions
    
    closes odoo/odoo#193354
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/hr_holidays/static/src/persona_model_patch.js
+++ addons/hr_holidays/static/src/persona_model_patch.js
@@ -1,7 +1,6 @@
 import { Persona } from "@mail/core/common/persona_model";
 import { deserializeDateTime } from "@web/core/l10n/dates";
 import { _t } from "@web/core/l10n/translation";
-import { user } from "@web/core/user";
 import { patch } from "@web/core/utils/patch";
 
 const { DateTime } = luxon;
@@ -23,19 +22,8 @@ patch(Persona.prototype, {
         if (!this.out_of_office_date_end) {
             return "";
         }
-        const currentDate = new Date();
         const date = deserializeDateTime(this.out_of_office_date_end);
-        // const options = { day: "numeric", month: "short" };
-        if (currentDate.getFullYear() !== date.year) {
-            // options.year = "numeric";
-        }
-        let localeCode = user.lang.replace(/_/g, "-");
-        if (localeCode === "sr@latin") {
-            localeCode = "sr-Latn-RS";
-        }
-        // const fdate = date.toLocaleString(DateTime.TIME_SHORT);
         const fdate = date.toLocaleString(DateTime.DATE_MED);
-        // const formattedDate = date.toLocaleDateString(localeCode, options);
         return _t("Out of office until %s", fdate);
     },
 });
