f3f1a552360e10bd0d89e228d5ff0071dbef94a1
Adrien Guilliams (adgu) <adgu@odoo.com>
2025-01-13 10:06:33 +0100

    [FIX] bunch of fix after fp feedback
    This commit consists of:
    
    Preset form
        - realingment
        - add tip in case of return mode without pricelist
        - add placeholder on pricelist and fp
        - Rename timing to time slots
    
    Preset selection popup
        - reduce modal size
    
    Slot selection popup
        - reput css rule to make slot button red if full, green if still
        available and future and secondary if past.
    
    When clicking on a preset which is a return preset, the quantities
    already in the cart becomes negatives
    
    Change bg-color and opacity of the quantity in cart span on the
    product_cards
    
    Add time slot in the ticket screen:
        - white if future preset
        - orange if current preset
        - red if past preset

--- addons/point_of_sale/static/src/app/models/pos_preset.js
+++ addons/point_of_sale/static/src/app/models/pos_preset.js
@@ -12,6 +12,10 @@ export class PosPreset extends Base {
         this.uiState = {
             availabilities: {},
         };
+
+        // This will compute availabilities locally
+        // when selecting a preset it will be updated with the server data
+        this.computeAvailabilities();
     }
 
     get needsSlot() {
@@ -55,7 +59,7 @@ export class PosPreset extends Base {
         );
     }
 
-    computeAvailabilities(usages) {
+    computeAvailabilities(usages = {}) {
         this.generateSlots();
 
         const allSlots = Object.values(this.uiState.availabilities).reduce(
@@ -70,6 +74,18 @@ export class PosPreset extends Base {
         }
 
         return this.uiState.availabilities;
+    }
+
+    get currentSlot() {
+        const now = DateTime.now();
+        const interval = this.interval_time * 60 * 1000;
+        const todayAvailabilities = this.availabilities[now.toFormat("yyyy-MM-dd")];
+        for (const slot of Object.values(todayAvailabilities)) {
+            if (slot.datetime < now && slot.datetime.plus({ minutes: interval }) > now) {
+                return slot;
+            }
+        }
+        return false;
     }
 
     generateSlots() {
