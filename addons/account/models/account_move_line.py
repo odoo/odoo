a86b4e36082ce3680bd786fe1ef06ebe31e76ba9
Mathieu Coutant <mcou@odoo.com>
2025-01-14 16:07:19 +0100

    [FIX] account: fallback on the currency_rate when calculating the rate
    Steps to reproduce:
    - In Accounting create a new tax like this:
    	-Tax computation: percentage of price tax included
    	-Amount: 100%
    	-Included in Price: Ticked
    - Create a new Customer Invoice in another currency
    - Use the 100% tax
    - Go in the tab "Journal Entries"
    - The amounts where not converted
    
    Cause:
    The rate is calculated like this : `rate = line.amount_currency / line.balance if line.balance else 1`
    But with this specific tax `amount_currency` and `balance` are 0 so the rate defaults to 1 thus not converting.
    
    Solution:
    Fallback on the rate of the line.
    
    opw-4443522

--- addons/account/models/account_move_line.py
+++ addons/account/models/account_move_line.py
@@ -963,7 +963,7 @@ class AccountMoveLine(models.Model):
                 include_caba_tags=line.move_id.always_tax_exigible,
                 fixed_multiplicator=sign,
             )
-            rate = line.amount_currency / line.balance if line.balance else 1
+            rate = line.amount_currency / line.balance if line.balance else line.currency_rate
             line.compute_all_tax_dirty = True
             line.compute_all_tax = {
                 frozendict({
