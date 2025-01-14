1617dafa0b1b91938f34755eaa490270683ba131
Juan José Scarafia <jjs@adhoc.com.ar>
2025-01-14 09:52:21 -0300

    [IMP] l10n_ar, l10n_ar_withholding: Tax Computation for Perceptions and Withholdings 1) Set jurisdiction field on 'AR' taxes (csv files) and show it on account.tax form view. 2) When l10n_ar_withholding module is installed:     2.a) Add sequences to withholding taxes.     2.b) Update demo data.     2.c) Set company tax base account. 3) Add sequences to AR withholding taxes during the installation of the chart of accounts for a new Argentine company. 4) Determine automatically partner fiscal position on customer invoices or vendor payments. 5) New page on fiscal positions form view (Perceptions and Withholdings). Set here default withholding and perception taxes that should be computed on customer invoices or vendor payments. 6) New groups on res.partner form view (Sale Perceptions and Purchase Withholding). Here are set perception or withholding taxes manually by the user or automatically by date range (optional). 7)Determine applicable taxes for a partner on customer invoices or vendor payments by filtering and matching tax rules based on the provided tax type ('perception' or 'withholding'), date, and company. Compute perceptions and withholding amounts automatically. Steps:     7.a) Set fiscal position automatically on invoice or payment.     7.b) Perception and withholding taxes are automatically applied to invoices or payments based on the default taxes configured in the fiscal position, as well as the values set in the "Sale Perceptions" and "Purchase Withholding" sections on the partner form. If a profit perception or withholding tax is explicitly defined on the partner form, it will be used; otherwise, the system falls back to the tax defined in the fiscal position. When both the fiscal position and the partner contain gross income perception or withholding taxes with the same tax group, the taxes from the partner will be used—provided they match the invoice or payment’s date and company. If they do not match, the taxes from the fiscal position will be applied. However, if the taxes from the fiscal position have an associated data source, the system will connect to the data source API to retrieve the applicable aliquot percentage for the partner based on the transaction date. A new tax will then be created by duplicating the default tax and updating its amount with the retrieved aliquot percentage. This new tax will be assigned to the partner in the corresponding "Sale Perceptions" or "Purchase Withholding" section of the partner form and also the new tax will be set on the invoice or payment.     7.c) The perception and withholding amounts on the invoice or payment will be automatically calculated. 8) Update id and tax groups on tax csv files. New withholding tax groups are created on addons/l10n_ar_withholding/data/template/account.tax.group-ar_base.csv and addons/l10n_ar_withholding/data/template/account.tax.group-ar_ex.csv . 9) New demo files:     9.a) Add demo fiscal positions and demo perception and withholding (new model account.fiscal.position.l10n_ar_tax) tax ids on fiscal positions.     9.b) Add demo invoices.     9.c) Activate Cordoba taxes.     9.d) Create demo partners. 10) Add translations. 11) Add unit tests.
    Task Adhoc side: 50717
    Task Latam side: 1126

--- addons/l10n_ar_withholding/models/account_move.py
+++ addons/l10n_ar_withholding/models/account_move.py
@@ -15,3 +15,25 @@ class AccountMove(models.Model):
     def _compute_l10n_ar_withholding_ids(self):
         for move in self:
             move.l10n_ar_withholding_ids = move.line_ids.filtered(lambda l: l.tax_line_id.l10n_ar_withholding_payment_type)
+
+    def _get_tax_factor(self):
+        """
+        Calculate and return the tax factor for the current account move.
+
+        The tax factor is determined based on the ratio of the untaxed amount
+        to the total amount. If the total amount is zero, a default tax factor
+        of 1.0 is used. Additionally, if the document type letter is 'B' and
+        the tax factor is 1.0, the tax factor is adjusted to account for a
+        21% VAT exemption.
+
+        Returns:
+            float: The calculated tax factor.
+        """
+        self.ensure_one()
+        tax_factor = self.amount_total and (self.amount_untaxed / self.amount_total) or 1.0
+        doc_letter = self.l10n_latam_document_type_id.l10n_ar_letter
+        # if we receive B invoices, then we take out 21 of vat
+        # this use of case if when company is except on vat for eg.
+        if tax_factor == 1.0 and doc_letter == 'B':
+            tax_factor = 1.0 / 1.21
+        return tax_factor
--- addons/l10n_ar_withholding/models/res_company.py
+++ addons/l10n_ar_withholding/models/res_company.py
@@ -1,5 +1,4 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import fields, models
+from odoo import models, fields
 
 
 class ResCompany(models.Model):
