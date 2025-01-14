4db5c5637ccff22b6fa25f8c0def7d663a52b7df
Robin Lejeune (role) <role@odoo.com>
2025-01-14 11:25:06 +0100

    !fixup website main commit: Instagram page

--- addons/website/static/src/snippets/s_instagram_page/instagram_page.js
+++ addons/website/static/src/snippets/s_instagram_page/instagram_page.js
@@ -8,32 +8,30 @@ import { _t } from "@web/core/l10n/translation";
 
 export class InstagramPage extends Interaction {
     static selector = ".s_instagram_page";
+    dynamicSelectors = {
+        ...this.dynamicSelectors,
+        _iframe: () => this.iframeEl,
+    };
     dynamicContent = {
         // We have to setup the message listener before setting the src, because
         // the iframe can send a message before this JS is fully loaded.
-        _window: {
-            "t-on-message": this.onMessage,
-        },
-    }
+        _window: { "t-on-message": this.onMessage },
+        _iframe: { "t-att-height": () => this.height },
+    };
 
     setup() {
         this.iframeEl = document.createElement("iframe");
         this.iframeEl.setAttribute("scrolling", "no");
         this.iframeEl.setAttribute("aria-label", _t("Instagram"));
         this.iframeEl.classList.add("w-100");
+        this.insert(this.iframeEl, this.el.querySelector(".o_instagram_container"));
 
         // In the meantime Instagram doesn't send us a message with the height,
         // we use a formula to estimate the height of the iframe (the formula
         // has been found with a linear regression).
         const iframeWidth = parseInt(getComputedStyle(this.iframeEl).width);
         // The profile picture is smaller when width < 432px.
-        this.iframeEl.height = 0.659 * iframeWidth + (iframeWidth < 432 ? 156 : 203);
-
-        // TODO : Check if the next lines can be replace by `this.insert(this.iframeEl, this.el.querySelector(".o_instagram_container"));`
-
-        this.el.querySelector(".o_instagram_container").appendChild(this.iframeEl);
-        // TODO : ...observerUnactive() / ...observerActive()
-        this.registerCleanup(() => { this.iframeEl.remove(); });
+        this.height = Math.ceil(0.659 * iframeWidth + (iframeWidth < 432 ? 156 : 203));
     }
 
     start() {
@@ -47,19 +45,6 @@ export class InstagramPage extends Interaction {
      * @param {Event} ev
      */
     onMessage(ev) {
-        // TODO Check if this issue is fixed with the new editor
-        /*
-        if (!this.iframeEl) {
-            // TODO: fix this case. We should never end up here. It happens when
-            // - Drop Instagram snippet
-            // - Undo
-            // - Drop a new Instagram snippet
-            // => The listener of the first one is still active because the
-            // public widget has not been destroyed.
-            window.removeEventListener("message", this.__onMessage);
-            return;
-        }
-        */
         if (ev.origin !== "https://www.instagram.com" || this.iframeEl.contentWindow !== ev.source) {
             return;
         }
@@ -71,9 +56,7 @@ export class InstagramPage extends Interaction {
         // Here we get the exact height of the iframe.
         // Instagram can return a height of 0 before the real height.
         if (height) {
-            // this.options.wysiwyg?.odooEditor.observerUnactive();
-            this.iframeEl.height = height;
-            // this.options.wysiwyg?.odooEditor.observerActive();
+            this.height = height;
         }
     }
 }
