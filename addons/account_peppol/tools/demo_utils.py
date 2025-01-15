84a0b81a258262e3bb9dbaa9c9f37796303a9dad
Claire (clbr) <clbr@odoo.com>
2025-01-15 10:22:43 +0100

    [IMP] account_edi_ubl_cii,account_peppol: Peppol First amendments
    - Improvements of message of Peppol/BIS3 related warnings of the Print & Send wizards.
    - Wording improvements for clarity.
    - Make the "What is Peppol" modal more present. Change its flow: the "activate" buttons
    now leads to the Peppol registration wizard if the company is not registered yet.
    - Even if the partner has no preferred e-invoice format, we should use BIS3 when
    "by Peppol" is checked in the Print & Send, therefore display related warnings.
    
    task-4478365
    
    closes odoo/odoo#193714
    
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>

--- addons/account_peppol/tools/demo_utils.py
+++ addons/account_peppol/tools/demo_utils.py
@@ -91,17 +91,19 @@ def _mock_button_verify_partner_endpoint(func, self, *args, **kwargs):
     old_value = self.peppol_verification_state
     company = kwargs.get('company') or self.env.company
     endpoint, eas, edi_format = self.peppol_endpoint, self.peppol_eas, self.invoice_edi_format
-    if endpoint and eas and edi_format:
-        self.with_company(company).peppol_verification_state = 'valid'
+    state = _mock_get_peppol_verification_state(func, self, endpoint, eas, edi_format)
+    self.with_company(company).peppol_verification_state = state
+    self._log_verification_state_update(company, old_value, state)
+    if state == 'valid':
         self.with_company(company).invoice_sending_method = 'peppol'
-        self._log_verification_state_update(company, old_value, 'valid')
-    else:
-        self.with_company(company).peppol_verification_state = 'not_valid'
 
 
 def _mock_get_peppol_verification_state(func, self, *args, **kwargs):
     (endpoint, eas, format) = args
-    return 'valid' if endpoint and eas and format else False
+    if endpoint and eas:
+        return 'valid' if format in self._get_peppol_formats() else 'not_valid_format'
+    else:
+        return 'not_valid'
 
 
 def _mock_user_creation(func, self, *args, **kwargs):
