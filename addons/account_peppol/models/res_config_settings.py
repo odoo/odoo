6ab21248f79e22828d8799a4c80e8d0ec2a58d49
Anton Romanova (roan) <roan@odoo.com>
2025-01-15 15:05:05 +0000

    [FIX] account_peppol: remove peppol registration migration button
    Peppol migration creates more issues than it solves. A lot of users
    issue migration requests to move to another SMP but later discover that
    the SMP they wanted to migrate to does not support migration keys. This
    creates a state that we need to handle manually. Most SMP do not support
    migrating away and force users to deregister and reregister again on
    another one.
    
    This commit adds a fix in stable for that issue by removing the button
    from the res_config_settings_buttons and adding a deprecated warning
    when the user tries to call the `button_migrate_peppol_registration`
    method.
    
    task-4394408
    
    closes odoo/odoo#194524
    
    X-original-commit: 3c96e205e9fcc1ec75acba5bafdf7fb33e39f192
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

--- addons/account_peppol/models/res_config_settings.py
+++ addons/account_peppol/models/res_config_settings.py
@@ -102,12 +102,7 @@ class ResConfigSettings(models.TransientModel):
         The migration key is then displayed in Peppol settings.
         Currently, reopening after migrating away is not supported.
         """
-        self.ensure_one()
-        if self.account_peppol_proxy_state != 'receiver':
-            raise UserError(_("Can't migrate unless registered to receive documents."))
-
-        self.account_peppol_edi_user._peppol_migrate_registration()
-        return True
+        raise UserError(_("This feature is deprecated. Contact odoo support if you need a migration key."))
 
     @handle_demo
     def button_deregister_peppol_participant(self):
