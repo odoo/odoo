0f737ac542228a3acb32522172d4ad2b90cdbe94
Yosua Nicolaus <yoni@odoo.com>
2025-01-15 16:52:02 +0100

    [IMP] account_peppol: disable peppol checkbox if not_verified
    Previously, we only disable the account move send wizard checkbox for
    peppol if the partner of the move have an invalid peppol address.
    However, this approach misses the case when the partner does not have a
    peppol address at all.
    
    This commit fixes that behavior by also disabling if the partner peppol
    verification state is in both case (invalid/not verified).
    
    closes odoo/odoo#198770
    
    Task-id: 4345477
    Signed-off-by: Laurent Smet (las) <las@odoo.com>
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

--- addons/account_peppol/models/res_partner.py
+++ addons/account_peppol/models/res_partner.py
@@ -199,7 +199,7 @@ class ResPartner(models.Model):
     @handle_demo
     def _get_peppol_verification_state(self, peppol_endpoint, peppol_eas, invoice_edi_format):
         if not (peppol_eas and peppol_endpoint) or invoice_edi_format not in self._get_peppol_formats():
-            return False
+            return 'not_verified'
 
         edi_identification = f"{peppol_eas}:{peppol_endpoint}".lower()
         participant_info = self._get_participant_info(edi_identification)
