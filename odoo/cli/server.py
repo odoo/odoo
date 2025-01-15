3a82260f129d9d71e17fdb928d0ee37a188292c4
Johan Demaret Rivarola <jde@odoo.com>
2025-01-15 11:12:52 +0000

    [FIX] core: falsy evaluation of db_replica_host
    Following the configuration refactor[1], the falsy evaluation of
    db_replica_host was wrong. The default is now an empty string.
    
    This lead to situations where a readonly cursor was wrongfully used.
    
    [1] https://github.com/odoo/odoo/commit/b1f7be518e2075c546bcedc0f4ba6dd62dff3c8f
    
    closes odoo/odoo#194371
    
    X-original-commit: cb54f139fefb1b6c2388746d1e9b572b07e1fa60
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- odoo/cli/server.py
+++ odoo/cli/server.py
@@ -66,7 +66,7 @@ def report_configuration():
     _logger.info('database: %s@%s:%s', user, host, port)
     replica_host = config['db_replica_host']
     replica_port = config['db_replica_port']
-    if replica_host is not False or replica_port:
+    if replica_host or replica_port:
         _logger.info('replica database: %s@%s:%s', user, replica_host or 'default', replica_port or 'default')
     if sys.version_info[:2] > odoo.MAX_PY_VERSION:
         _logger.warning("Python %s is not officially supported, please use Python %s instead",
