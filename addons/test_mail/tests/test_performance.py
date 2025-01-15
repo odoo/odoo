0779feddd30e9dd42ea020052dc4ad174be34c9a
Renaud Thiry <reth@odoo.com>
2025-01-15 16:13:33 +0100

    [IMP] mail: increase query counts
    increase query counts from changes in previous commits adding mass
    notification creation and the ability to resend notifications without
    linked partner
    
    task-2199202

--- addons/test_mail/tests/test_performance.py
+++ addons/test_mail/tests/test_performance.py
@@ -499,7 +499,7 @@ class TestBaseAPIPerformance(BaseMailPerformance):
                 'default_template_id': test_template.id,
             }).create({})
 
-        with self.assertQueryCount(admin=55, employee=55), self.mock_mail_gateway():
+        with self.assertQueryCount(admin=77, employee=77), self.mock_mail_gateway():
             composer._action_send_mail()
 
         self.assertEqual(len(self._new_mails), 10)
@@ -1272,7 +1272,7 @@ class TestMailAPIPerformance(BaseMailPerformance):
         rec1 = rec.with_context(active_test=False)      # to see inactive records
         self.assertEqual(rec1.message_partner_ids, self.partners | self.env.user.partner_id | self.user_portal.partner_id)
 
-        with self.assertQueryCount(admin=31, employee=31):
+        with self.assertQueryCount(admin=34, employee=34):
             rec.write({
                 'name': 'Test2',
                 'customer_id': customer_id,
