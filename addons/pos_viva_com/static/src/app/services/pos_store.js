d1ad773482429708b0fed4843876ef186eb3cdff
Louis Travaux <lotr@odoo.com>
2025-01-15 14:26:39 +0100

    [REF] pos_viva_com: adapt from Viva Wallet to Viva.com
    As requested by Viva.com teams, we adapted Viva Wallet references to Viva.com to comply to their
    new branding.
    
    Task: 4484901

--- addons/pos_viva_com/static/src/app/services/pos_store.js
+++ addons/pos_viva_com/static/src/app/services/pos_store.js
@@ -4,11 +4,11 @@ import { PosStore } from "@point_of_sale/app/services/pos_store";
 patch(PosStore.prototype, {
     async setup() {
         await super.setup(...arguments);
-        this.data.connectWebSocket("VIVA_WALLET_LATEST_RESPONSE", () => {
-            const pendingLine = this.getPendingPaymentLine("viva_wallet");
+        this.data.connectWebSocket("VIVA_COM_LATEST_RESPONSE", () => {
+            const pendingLine = this.getPendingPaymentLine("viva_com");
 
             if (pendingLine) {
-                pendingLine.payment_method_id.payment_terminal.handleVivaWalletStatusResponse();
+                pendingLine.payment_method_id.payment_terminal.handleVivaComStatusResponse();
             }
         });
     },
