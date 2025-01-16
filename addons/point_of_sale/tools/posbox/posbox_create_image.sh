9779205168005c72640a049666373b330cab7c1a
Louis Travaux <lotr@odoo.com>
2025-01-16 11:06:34 +0000

    [IMP] point_of_sale: remove iot patch loop from build
    In odoo/odoo#192449, we removed the only code patch we had on the IoT Box. We can then remove the
    patching logic.
    
    closes odoo/odoo#193988
    
    X-original-commit: af6eb7c484aa7a614f5d58268353dd10ed611e2a
    Signed-off-by: Yaroslav Soroko (yaso) <yaso@odoo.com>
    Signed-off-by: Louis Travaux (lotr) <lotr@odoo.com>

--- addons/point_of_sale/tools/posbox/posbox_create_image.sh
+++ addons/point_of_sale/tools/posbox/posbox_create_image.sh
@@ -158,14 +158,6 @@ rm -rf "${CLONE_DIR}"
 rm -rf "${OVERWRITE_FILES_BEFORE_INIT_DIR}/usr"
 cp -a "${OVERWRITE_FILES_AFTER_INIT_DIR}"/* "${MOUNT_POINT}"
 
-find "${MOUNT_POINT}"/ -type f -name "*.iotpatch"|while read iotpatch; do
-    DIR=$(dirname "${iotpatch}")
-    BASE=$(basename "${iotpatch%.iotpatch}")
-    find "${DIR}" -type f -name "${BASE}" ! -name "*.iotpatch"|while read file; do
-        patch -f --verbose "${file}" < "${iotpatch}"
-    done
-done
-
 # cleanup
 umount -fv "${MOUNT_POINT}"/boot/
 umount -lv "${MOUNT_POINT}"/
