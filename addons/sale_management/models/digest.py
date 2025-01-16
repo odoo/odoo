c438cacf6a95b721cb32f13c57d7e2e2737f1b56
sesn-odoo <sesn@odoo.com>
2025-01-16 15:02:46 +0000

    [FIX] digest, various: fix URLs to new format
    Some URLs in the digest emails are currently broken due to an outdated
    URL format.
    
    Steps to reproduce
    ------------------
    * Install `digest` and `crm` modules
    * Go to Settings > Technical > Digest Emails
    * Select the "Your Odoo Periodic Digest" digest and send it
    * Check the sent email and click on any of the "Open Email" links
    
    You should encounter an error indicating that the action does not exist.
    
    Although the reproduction steps use the `crm` module as an example, this
    issue occurs with a few more modules.
    
    Cause
    -----
    The "Open Report" URLs were not updated to account for the new URL
    format.
    
    opw-4405288
    
    closes odoo/odoo#196349
    
    X-original-commit: 8115594bd80f33ff0e6e58ee0a8a4b14ae7acd62
    Signed-off-by: John Laterre (jol) <jol@odoo.com>
    Signed-off-by: Séna Serge Nshimiyimana (sesn) <sesn@odoo.com>

--- addons/sale_management/models/digest.py
+++ addons/sale_management/models/digest.py
@@ -24,5 +24,5 @@ class DigestDigest(models.Model):
 
     def _compute_kpis_actions(self, company, user):
         res = super()._compute_kpis_actions(company, user)
-        res['kpi_all_sale_total'] = 'sale.report_all_channels_sales_action&menu_id=%s' % self.env.ref('sale.sale_menu_root').id
+        res['kpi_all_sale_total'] = 'sale.report_all_channels_sales_action?menu_id=%s' % self.env.ref('sale.sale_menu_root').id
         return res
