f557e629c79b6c44dfcac5533c94f76c213cd1ae
“Beekan” <mmha@odoo.com>
2025-01-16 11:16:32 +0100

    [IMP] profiler: Add memory profiling
    tracemalloc and memory collectors are a process wide collector.
    There should be no two memory collector running at the same time.
    This is to prevent memory from on request tainting the other and
    another thread closing the memory collection from another thread.
    To prevent this memory profiling was made to be a thread safe event.
    Which required locks on starting the memory profiler. This lock will
    only affect users who are memory profiling, but the rest of the
    requests will work as normal.
    
    closes odoo/odoo#197625
    
    Signed-off-by: Xavier Dollé (xdo) <xdo@odoo.com>

--- addons/web/controllers/profiling.py
+++ addons/web/controllers/profiling.py
@@ -23,22 +23,16 @@ class Profiling(Controller):
             return Response(response='error: %s' % e, status=500, mimetype='text/plain')
 
     @route([
-        '/web/speedscope',
         '/web/speedscope/<profile>',
     ], type='http', sitemap=False, auth='user', readonly=True)
     def speedscope(self, profile=None, action=False, **kwargs):
-        if not profile:
-            raise request.not_found()
+        profiles = request.env['ir.profile'].browse(int(p) for p in profile.split(',')).exists()
         profile_str = profile
-        profiles = request.env['ir.profile'].browse((int(p) for p in profile.split(',')))
-        if not kwargs and not action:
-            context = {
-                'profile_str': profile_str,
-                'profiles': profiles,
-            }
-            return request.render('web.config_speedscope_index', context)
-        speedscope_result = profiles._generate_speedscope(profiles._parse_params(kwargs))
-        if action == 'download_json':
+        if not profiles:
+            raise request.not_found()
+        params = kwargs or profiles._default_profile_params()
+        speedscope_result = profiles._generate_speedscope(profiles._parse_params(params))
+        if action == 'speedscope_download_json':
             headers = [
                 ('Content-Type', 'application/json'),
                 ('X-Content-Type-Options', 'nosniff'),
@@ -53,8 +47,33 @@ class Profiling(Controller):
             'cdn': icp.sudo().get_param('speedscope_cdn', "https://cdn.jsdelivr.net/npm/speedscope@1.13.0/dist/release/")
         }
         response = request.render('web.view_speedscope_index', context)
-        if action == 'download_html':
+        if action == 'speedscope_download_html':
             response.headers['Content-Disposition'] = content_disposition(f'profile_{profile_str}.html')
             response.headers['X-Content-Type-Options'] = 'nosniff'
             response.headers['Content-Type'] = 'text/html'
         return response
+
+    @route([
+      '/web/profile_config/<profile>',
+    ], type='http', sitemap=False, auth='user', readonly=True)
+    def profile_config(self, profile=None, action=False, **kwargs):
+        profile_str = profile
+        profiles = request.env['ir.profile'].browse(int(p) for p in profile_str.split(',')).exists()
+        if not profiles:
+            raise request.not_found()
+
+        if action == 'memory_open':
+            memory_profile = profiles._generate_memory_profile(profiles._parse_params(kwargs))
+            encoded_memory_profile = json.dumps(memory_profile).encode('utf_8')
+            context = {
+                'profile': profiles,
+                'memory_graph': base64.b64encode(encoded_memory_profile).decode('utf-8'),
+                }
+            return request.render('web.view_memory', context)
+
+        context = {
+            'default_params': profiles._default_profile_params(),
+            'profile_str': profile_str,
+            'profiles': profiles,
+        }
+        return request.render('web.config_speedscope_index', context)
