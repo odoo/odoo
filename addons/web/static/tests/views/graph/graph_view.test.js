3b086e66ed85d08cd6ba8bc37fa27e5f55bfdfd5
Michaël Mattiello <mcm@odoo.com>
2025-01-16 07:11:10 +0000

    [FIX] web: graph: prevent rendering too many datasets
    Chart.js can be really slow when rendering a large number of datasets.
    So this commit limits the amount of datasets displayed by default on
    the graph. However a button is present to display all datasets even if
    it can take a moment to render them.
    
    task-4351783
    
    X-original-commit: 2b97c12def5bbfa396bc57910e486dfe1ce12cb5

--- addons/web/static/tests/views/graph/graph_view.test.js
+++ addons/web/static/tests/views/graph/graph_view.test.js
@@ -49,6 +49,7 @@ import { DEFAULT_BG, getBorderWhite, getColors, lightenColor } from "@web/core/c
 import { Domain } from "@web/core/domain";
 import { SampleServer } from "@web/model/sample_server";
 import { GraphArchParser } from "@web/views/graph/graph_arch_parser";
+import { GraphModel } from "@web/views/graph/graph_model";
 import { GraphRenderer } from "@web/views/graph/graph_renderer";
 import { WebClient } from "@web/webclient/webclient";
 
@@ -2825,4 +2826,66 @@ test("display the field's falsy_value_label for false group, if defined", async 
     });
 
     checkLabels(view, ["xphone", "xpad", "I'm the false group"]);
+});
+
+test("limit dataset amount", async () => {
+    class Project extends models.Model {
+        id = fields.Integer();
+        name = fields.Char();
+    }
+    class Stage extends models.Model {
+        id = fields.Integer();
+        name = fields.Char();
+    }
+    class Task extends models.Model {
+        id = fields.Integer();
+        name = fields.Char();
+        project_id = fields.Many2one({ relation: "project" });
+        stage_id = fields.Many2one({ relation: "stage" });
+    }
+    defineModels([Project, Stage, Task]);
+
+    for (let i = 1; i <= 600; i++) {
+        Project._records.push({
+            id: i,
+            name: `Project ${i}`,
+        });
+        Stage._records.push({
+            id: i,
+            name: `Stage ${i}`,
+        });
+        Task._records.push({
+            id: i,
+            project_id: i,
+            stage_id: i,
+            name: `Task ${i}`,
+        });
+    }
+
+    const view = await mountView({
+        type: "graph",
+        resModel: "task",
+        arch: `
+            <graph>
+                <field name="project_id"/>
+                <field name="stage_id"/>
+            </graph>
+        `,
+    });
+    const model = getGraphModel(view);
+    expect(model.data.exceeds).toBe(true);
+    expect(model.data.datasets).toHaveLength(80);
+    expect(model.data.labels).toHaveLength(80);
+    expect(`.o_graph_alert`).toHaveCount(1);
+
+    patchWithCleanup(GraphModel.prototype, {
+        notify() {
+            expect.step("rerender");
+        },
+    });
+    await contains(`.o_graph_load_all_btn`).click();
+    expect.verifySteps(["rerender"]);
+    expect(model.data.exceeds).toBe(false);
+    expect(model.data.datasets).toHaveLength(600);
+    expect(model.data.labels).toHaveLength(600);
 });
