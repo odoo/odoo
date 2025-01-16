dbfe4f8e6de4075d5b4c9f848024f12897706586
Djamel Touati <otd@odoo.com>
2025-01-16 17:04:32 +0000

    [FIX] stock: clean context before scrap validation
    Steps to reproduce the bug:
    - Create a storable product “P1”:
        - UoM: g
    - Go to inventory > operation > Scrap:
        - Create a new one:
            - product: P1
           - UoM: KG
           - Quantity: 1
    
    - Validate the scrap
    - A wizard is triggered indicating insufficient stock quantity.
    - validate it
    
    Problem:
    The quantity in the scrap is updated to 1000 kg, and the related move
    is created with a quantity of 1000 kg.
    
    opw-4485849
    
    closes odoo/odoo#196040
    
    X-original-commit: 9e2f4b825f6d739f6822ac31f3e901a3f4d45554
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Djamel Touati (otd) <otd@odoo.com>

--- addons/stock/wizard/stock_warn_insufficient_qty.py
+++ addons/stock/wizard/stock_warn_insufficient_qty.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
+from odoo.tools.misc import clean_context
 
 
 class StockWarnInsufficientQty(models.AbstractModel):
@@ -42,7 +43,7 @@ class StockWarnInsufficientQtyScrap(models.TransientModel):
         return self.scrap_id.company_id
 
     def action_done(self):
-        return self.scrap_id.do_scrap()
+        return self.with_context(clean_context(self.env.context)).scrap_id.do_scrap()
 
     def action_cancel(self):
         # FIXME in master: we should not have created the scrap in a first place
