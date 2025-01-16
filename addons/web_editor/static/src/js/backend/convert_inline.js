4ed3fe904bc2850c5044e465dfc205a67a315046
Walid (wasa) <wasa@odoo.com>
2025-01-16 13:27:29 +0000

    [FIX] web_editor: adapt `border-style` value based on border widths
    **Problem**:
    Outlook always displays the border of an element if `border-style` is set
    to `solid`, even when all border widths are 0.
    
    **Solution**:
    Change `border-style` to `none` if all border widths are 0.
    
    **Steps to Reproduce**:
    1. Open the "Event: Registration Confirmation" email template in the web editor.
    2. Add a character and save the template.
    3. Check the saved `body_html`.
       -> All table elements will have `border-style: solid;` added as inline styling,
          causing borders to appear in Outlook.
    
    opw-4211794
    
    closes odoo/odoo#195282
    
    X-original-commit: b9137c84078c0450f322bb864dc078c154696b58
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Walid Sahli (wasa) <wasa@odoo.com>

--- addons/web_editor/static/src/js/backend/convert_inline.js
+++ addons/web_editor/static/src/js/backend/convert_inline.js
@@ -457,6 +457,7 @@ function classToStyle($editable, cssRules) {
                 style = `${key}:${value};${style}`;
             }
         };
+        style = correctBorderAttributes(style);
         if (Object.keys(style || {}).length === 0) {
             writes.push(() => { node.removeAttribute('style'); });
         } else {
@@ -1787,6 +1788,56 @@ function _normalizeStyle(style) {
     element.parentElement.insertBefore(wrapper, element);
     wrapper.append(element);
     return wrapper;
+}
+
+/**
+ * Corrects the `border-style` attribute in the provided inline style string.
+ * This is specifically for Outlook, which displays borders even when their widths are set to 0px.
+ * If all border widths are 0, the function updates `border-style` to `none`.
+ *
+ * @param {string} style - The inline style string to correct.
+ * @returns {string} - The corrected inline style string.
+ */
+function correctBorderAttributes(style) {
+    const stylesObject = style
+        .replace(/\s+/g, "")
+        .split(";")
+        .reduce((styles, styleString) => {
+            const [attribute, value] = styleString.split(":").map((str) => str.trim());
+            if (attribute) {
+                styles[attribute] = value;
+            }
+            return styles;
+        }, {});
+
+    const BORDER_WIDTHS_ATTRIBUTES = [
+        "border-bottom-width",
+        "border-left-width",
+        "border-right-width",
+        "border-top-width",
+    ];
+
+    const isBorderStyleApplied = BORDER_WIDTHS_ATTRIBUTES.some(
+        (attribute) => attribute in stylesObject
+    );
+
+    if (!isBorderStyleApplied) {
+        return style;
+    }
+
+    const totalBorderWidth = BORDER_WIDTHS_ATTRIBUTES.reduce((totalWidth, attribute) => {
+        const widthValue = stylesObject[attribute] || "0px";
+        const numericWidth = parseFloat(widthValue.replace("px", "")) || 0;
+        return totalWidth + numericWidth;
+    }, 0);
+
+    if (totalBorderWidth === 0) {
+        stylesObject["border-style"] = "none";
+    }
+
+    return Object.entries(stylesObject)
+        .map(([attribute, value]) => `${attribute}:${value}`)
+        .join(";");
 }
 
 export default {
