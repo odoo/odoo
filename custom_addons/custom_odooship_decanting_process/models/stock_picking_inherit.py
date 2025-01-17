4bcf35dead692a7501e374ed86c20085689f4fee
Drishti <drishti@shiperoo.com>
2025-01-17 15:48:37 +1100

    [MOD]Modified Validation Order method add schdule date and todays date data while sending thru api

--- custom_addons/custom_odooship_decanting_process/models/stock_picking_inherit.py
+++ custom_addons/custom_odooship_decanting_process/models/stock_picking_inherit.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields, api
+from odoo import models, fields, api,_
 from odoo.exceptions import UserError, ValidationError
 import json
 import requests
 import logging
+from datetime import date
 
 
 _logger = logging.getLogger(__name__)
@@ -25,90 +26,90 @@ class StockPicking(models.Model):
     site_code = fields.Char(related='site_code_id.name',string='Site Code', store=True)
     is_automation = fields.Boolean(string='Is Automation', default=False)
 
-    def button_manual_pack(self):
-        """
-        This method manually packs line items marked as 'Packed'
-        and sends them to the external Flask API in a consolidated format.
-        """
-
-        for picking in self:
-            packed_lines = picking.move_ids_without_package.filtered(lambda line: line.packed and not line.released_manual)
-            if not packed_lines:
-                raise UserError("No packed items found in this picking.")
-
-            sku_list = []
-            total_sku_amount = 0  # To calculate the total quantity
-            sku_type_amount = 0  # To count unique SKUs
-
-            # Loop through each packed line to build the SKU list
-            for line in packed_lines:
-                total_sku_amount += line.quantity  # Accumulate total quantity
-                sku_list.append({
-                    "sku_code": line.product_id.default_code,  # Product code (SKU)
-                    "sku_amount": line.quantity,  # Quantity
-                    "amount": line.quantity,  # Quantity
-                    "out_batch_code": "ECOM",  # Batch code
-                    "owner_code": picking.tenant_code_id.name,  # Tenant code
-                    "out_order_code": picking.name,  # Picking name as the order code
-                })
-
-            sku_type_amount = len(sku_list)  # Count unique SKUs
-
-            # Prepare payload with consolidated SKU list
-            data = {
-                "header": {
-                    "user_id": "admin",  # Replace as needed
-                    "user_key": "admin",
-                    "warehouse_code": picking.site_code_id.name,  # Site Code
-                    # "interface_code": "feedback_outbound_container"  # Interface Code
-                },
-                "body": {
-                    "container_list": [
-                        {
-                            "container_code": picking.name,  # Receipt number
-                            "sku_type_amount": sku_type_amount,  # Unique SKU count
-                            "sku_amount": total_sku_amount,  # Total quantities of all SKUs
-                            "sku_list": sku_list,  # Consolidated SKU list
-                        }
-                    ],
-                    "warehouse_code": picking.site_code_id.name,  # Warehouse code
-                }
-            }
-
-            # Convert data to JSON format
-            json_data = json.dumps(data, indent=4)
-            _logger.info(f"Generated data Pack: {json_data}")
-
-            # Determine environment-specific URL
-            is_production = self.env['ir.config_parameter'].sudo().get_param('is_production_env')
-            url_manual_packing = (
-                "https://shiperooconnect-prod.automation.shiperoo.com/receive_payload"
-                if is_production == 'True'
-                else "https://shiperooconnect.automation.shiperoo.com/receive_payload"
-            )
-
-            headers = {
-                'Content-Type': 'application/json'
-            }
-
-            try:
-                # Send POST request to the external Flask API
-                response = requests.post(url_manual_packing, data=json_data, headers=headers)
-                if response.status_code == 200:
-                    api_response = response.json()
-                    if api_response.get('success'):
-                        for line in packed_lines:
-                            line.released_manual = True  # Mark the line as released
-                        self.env.cr.commit()  # Commit changes immediately to avoid conflicts
-                    else:
-                        raise UserError(
-                            f"Failed to send data to Pack App {picking.name}: {api_response.get('message')}")
-                else:
-                    raise UserError(f"Flask API error ({response.status_code}): {response.text}")
-            except requests.RequestException as e:
-                raise UserError(f"Error communicating with Flask API: {str(e)}")
-
-        return True
+    # def button_manual_pack(self):
+    #     """
+    #     This method manually packs line items marked as 'Packed'
+    #     and sends them to the external Flask API in a consolidated format.
+    #     """
+    #
+    #     for picking in self:
+    #         packed_lines = picking.move_ids_without_package.filtered(lambda line: line.packed and not line.released_manual)
+    #         if not packed_lines:
+    #             raise UserError("No packed items found in this picking.")
+    #
+    #         sku_list = []
+    #         total_sku_amount = 0  # To calculate the total quantity
+    #         sku_type_amount = 0  # To count unique SKUs
+    #
+    #         # Loop through each packed line to build the SKU list
+    #         for line in packed_lines:
+    #             total_sku_amount += line.quantity  # Accumulate total quantity
+    #             sku_list.append({
+    #                 "sku_code": line.product_id.default_code,  # Product code (SKU)
+    #                 "sku_amount": line.quantity,  # Quantity
+    #                 "amount": line.quantity,  # Quantity
+    #                 "out_batch_code": "ECOM",  # Batch code
+    #                 "owner_code": picking.tenant_code_id.name,  # Tenant code
+    #                 "out_order_code": picking.name,  # Picking name as the order code
+    #             })
+    #
+    #         sku_type_amount = len(sku_list)  # Count unique SKUs
+    #
+    #         # Prepare payload with consolidated SKU list
+    #         data = {
+    #             "header": {
+    #                 "user_id": "admin",  # Replace as needed
+    #                 "user_key": "admin",
+    #                 "warehouse_code": picking.site_code_id.name,  # Site Code
+    #                 # "interface_code": "feedback_outbound_container"  # Interface Code
+    #             },
+    #             "body": {
+    #                 "container_list": [
+    #                     {
+    #                         "container_code": picking.name,  # Receipt number
+    #                         "sku_type_amount": sku_type_amount,  # Unique SKU count
+    #                         "sku_amount": total_sku_amount,  # Total quantities of all SKUs
+    #                         "sku_list": sku_list,  # Consolidated SKU list
+    #                     }
+    #                 ],
+    #                 "warehouse_code": picking.site_code_id.name,  # Warehouse code
+    #             }
+    #         }
+    #
+    #         # Convert data to JSON format
+    #         json_data = json.dumps(data, indent=4)
+    #         _logger.info(f"Generated data Pack: {json_data}")
+    #
+    #         # Determine environment-specific URL
+    #         is_production = self.env['ir.config_parameter'].sudo().get_param('is_production_env')
+    #         url_manual_packing = (
+    #             "https://shiperooconnect-prod.automation.shiperoo.com/receive_payload"
+    #             if is_production == 'True'
+    #             else "https://shiperooconnect.automation.shiperoo.com/receive_payload"
+    #         )
+    #
+    #         headers = {
+    #             'Content-Type': 'application/json'
+    #         }
+    #
+    #         try:
+    #             # Send POST request to the external Flask API
+    #             response = requests.post(url_manual_packing, data=json_data, headers=headers)
+    #             if response.status_code == 200:
+    #                 api_response = response.json()
+    #                 if api_response.get('success'):
+    #                     for line in packed_lines:
+    #                         line.released_manual = True  # Mark the line as released
+    #                     self.env.cr.commit()  # Commit changes immediately to avoid conflicts
+    #                 else:
+    #                     raise UserError(
+    #                         f"Failed to send data to Pack App {picking.name}: {api_response.get('message')}")
+    #             else:
+    #                 raise UserError(f"Flask API error ({response.status_code}): {response.text}")
+    #         except requests.RequestException as e:
+    #             raise UserError(f"Error communicating with Flask API: {str(e)}")
+    #
+    #     return True
 
     def button_validate_picking(self):
         """
@@ -129,7 +130,10 @@ class StockPicking(models.Model):
 
             if not picking.move_ids_without_package:
                 raise ValidationError(_("No product lines found to send."))
+            schedule_date = picking.scheduled_date.strftime("%d-%m-%Y") if picking.scheduled_date else "N/A"
 
+            # Get today's date in "YYYY-MM-DD" format
+            current_date = date.today().strftime("%d-%m-%Y")
             # Build the payload
             product_lines = []
             for line in picking.move_ids_without_package:
@@ -151,6 +155,8 @@ class StockPicking(models.Model):
                 "site_code": picking.site_code_id.name,
                 "origin": picking.origin or "N/A",
                 "product_lines": product_lines,
+                "schedule_date":schedule_date,
+                "current_date":current_date,
             }
             # Define the URLs for Shiperoo Connect
             is_production = self.env['ir.config_parameter'].sudo().get_param('is_production_env')
