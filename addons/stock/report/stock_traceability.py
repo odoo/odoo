8f1224e02b242773d4c81d719dad8f08073c5c90
Lulu Grimalkin (lugr) <lugr@odoo.com>
2025-01-17 10:41:28 +0100

    [IMP] stock: display src and dest warehouses in traceability reports
    Warehouse codes are prefixed to the source and destination locations
    of lots in traceability reports for clarity if multiwarehouse is
    toggled on.
    
    Because the desired names are slightly different from the existing
    display names created through `name_create()`, the overridden
    `_compute_display_name()` relies on a context parameter.
    
    Task ID: 4430229

--- addons/stock/report/stock_traceability.py
+++ addons/stock/report/stock_traceability.py
@@ -131,8 +131,8 @@ class StockTraceabilityReport(models.TransientModel):
             'product_qty_uom': "%s %s" % (self._quantity_to_str(move_line.product_uom_id, move_line.product_id.uom_id, move_line.quantity), move_line.product_id.uom_id.name),
             'lot_name': move_line.lot_id.name,
             'lot_id': move_line.lot_id.id,
-            'location_source': move_line.location_id.name,
-            'location_destination': move_line.location_dest_id.name,
+            'location_source': move_line.location_id.with_context(include_wh_code=True).display_name,
+            'location_destination': move_line.location_dest_id.with_context(include_wh_code=True).display_name,
             'reference_id': ref,
             'res_id': res_id,
             'res_model': res_model}]
