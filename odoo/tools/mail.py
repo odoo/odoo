66e1d9d2a50cfb043741302aa5d100eac674cb69
Renaud Thiry <reth@odoo.com>
2025-01-17 06:57:01 +0000

    [IMP] mail: quote blank spaces
    Blank spaces following a quoted node should also be quoted.
    
    This avoids having long trailing spaces below a signature
    and having unquoted blank spaces between two quoted blocks
    
    task-4381505
    
    X-original-commit: 118b1a26eee9e2187adfdb324196ec1e08da8709

--- odoo/tools/mail.py
+++ odoo/tools/mail.py
@@ -245,6 +245,8 @@ def tag_quote(el):
         el.set('data-o-mail-quote', '1')
     if el.getparent() is not None and (el.getparent().get('data-o-mail-quote') or el.getparent().get('data-o-mail-quote-container')) and not el.getparent().get('data-o-mail-quote-node'):
         el.set('data-o-mail-quote', '1')
+    if el.getprevious() is not None and el.getprevious().get('data-o-mail-quote') and not el.text_content().strip():
+        el.set('data-o-mail-quote', '1')
 
 
 def html_normalize(src, filter_callback=None, output_method="html"):
