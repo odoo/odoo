d0d7ba62ae218e069b8250e24c9d1211f9d683c8
Pedram (PEBR) <pebr@odoo.com>
2025-01-20 16:13:46 +0000

    [PERF] pos_self_order: prevent loading product images in load
    Before this commit, product images were unnecessarily loaded along with
    the POS data. This was redundant since the images are loaded
    separately. Additionally, loading the images caused excessive memory
    usage, leading to memory errors in certain scenarios.
    
    opw-4494712
    
    closes odoo/odoo#194920
    
    X-original-commit: 18c16317b44fe9472d45d1b6faf320d4b8afe6c9
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/pos_self_order/models/product_product.py
+++ addons/pos_self_order/models/product_product.py
@@ -48,6 +48,7 @@ class ProductTemplate(models.Model):
             product['_archived_combinations'] = []
             for product_product in self.env['product.product'].with_context(active_test=False).search([('product_tmpl_id', '=', product['id']), ('active', '=', False)]):
                 product['_archived_combinations'].append(product_product.product_template_attribute_value_ids.ids)
+            product['image_128'] = bool(product['image_128'])
 
     @api.model
     def _load_pos_data_fields(self, config_id):
