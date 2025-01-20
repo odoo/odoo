dcffda0f659a8b2b32fbdce6ecc23fdd490abdf1
tsm-odoo <tsm@odoo.com>
2025-01-20 13:19:54 +0100

    [FIX] im_livechat: fix unknown models warning
    The chatbot relies on several models to work on embed pages
    such as: `chatbot.script` or `chatbot.script.step`. Data related
    to those models are sent to the store when a chatbot is active on
    a thread. However, those JS models are only preset in the live
    chat embed bundle. As a result, warning are emitted on the odoo
    backend when data related to those models are received, and ignore.
    This PR moves the models to the common bundle. While not ideal, this
    approach will allow the live chat tours to be executed as warning are
    making them fail.

--- addons/im_livechat/static/src/core/common/message_model_patch.js
+++ addons/im_livechat/static/src/core/common/message_model_patch.js
@@ -1,9 +1,14 @@
 import { Message } from "@mail/core/common/message_model";
+import { Record } from "@mail/core/common/record";
 
 import { patch } from "@web/core/utils/patch";
 
 /** @type {import("models").Message} */
 const messagePatch = {
+    setup() {
+        super.setup(...arguments);
+        this.chatbotStep = Record.one("ChatbotStep", { inverse: "message" });
+    },
     isTranslatable(thread) {
         return (
             super.isTranslatable(thread) ||
