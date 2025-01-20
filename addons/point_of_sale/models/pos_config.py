e538d6f0f1519b015c5f86725d8c94f66739b4e6
Dylan Kiss (dyki) <dyki@odoo.com>
2025-01-20 10:02:48 +0000

    [FIX] point_of_sale: translate created journals
    In the onboarding wizard, we create a cash journal for different
    scenarios. However the names of these journals were not translatable.
    
    We make them translatable in this commit.
    
    X-original-commit: 580cec8b5ba388ca6b74f534c5c40688832fc0ee

--- addons/point_of_sale/models/pos_config.py
+++ addons/point_of_sale/models/pos_config.py
@@ -906,7 +906,7 @@ class PosConfig(models.Model):
             'point_of_sale.pos_category_lower',
             'point_of_sale.pos_category_others'
         ])
-        journal, payment_methods_ids = self._create_journal_and_payment_methods(cash_journal_vals={'name': 'Cash Clothes Shop', 'show_on_dashboard': False})
+        journal, payment_methods_ids = self._create_journal_and_payment_methods(cash_journal_vals={'name': _("Cash Clothes Shop"), 'show_on_dashboard': False})
         config = self.env['pos.config'].create([{
             'name': _('Clothes Shop'),
             'company_id': self.env.company.id,
@@ -927,7 +927,7 @@ class PosConfig(models.Model):
         if not self.env.ref(ref_name, raise_if_not_found=False):
             convert.convert_file(self.env, 'point_of_sale', 'data/scenarios/bakery_data.xml', None, mode='init', noupdate=True, kind='data')
 
-        journal, payment_methods_ids = self._create_journal_and_payment_methods(cash_journal_vals={'name': 'Cash Bakery', 'show_on_dashboard': False})
+        journal, payment_methods_ids = self._create_journal_and_payment_methods(cash_journal_vals={'name': _("Cash Bakery"), 'show_on_dashboard': False})
         bakery_categories = self.get_record_by_ref([
             'point_of_sale.pos_category_breads',
             'point_of_sale.pos_category_pastries',
@@ -954,7 +954,7 @@ class PosConfig(models.Model):
 
         journal, payment_methods_ids = self._create_journal_and_payment_methods(
             cash_ref='point_of_sale.cash_payment_method_furniture',
-            cash_journal_vals={'name': 'Cash Furn. Shop', 'show_on_dashboard': False},
+            cash_journal_vals={'name': _("Cash Furn. Shop"), 'show_on_dashboard': False},
         )
         furniture_categories = self.get_record_by_ref([
             'point_of_sale.pos_category_miscellaneous',
@@ -1006,7 +1006,7 @@ class PosConfig(models.Model):
         pos_restaurant_module = self.env['ir.module.module'].search([('name', '=', 'pos_restaurant')])
         pos_restaurant_module.button_immediate_install()
         return {'installed_with_demo': pos_restaurant_module.demo}
-    
+
     def _get_available_pricelists(self):
         self.ensure_one()
         return self.available_pricelist_ids if self.use_pricelist else self.pricelist_id
