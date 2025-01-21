3854aaa708ebfd01ac2ce3e7f749485de71cf01b
Mohammad Abdulmoneim (abdu) <abdu@odoo.com>
2025-01-21 14:38:26 +0100

    [FIX] product: packaging barcodes adjustments
    The commit prevents copying barcode field when duplicating a packaging
    barcode. The "Packaging Barcodes" smart button is also now always shown
    even if no barcodes were created for this unit to allow users to create
    barcodes for any unit. The commit also fixes creating a barcode record
    for one record and adds a new ZPL report for packaging barcodes.
    
    Task-4471379
    
    Part-of: odoo/odoo#196938
    Related: odoo/enterprise#78809
    Related: odoo/upgrade#7188
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/product/models/uom_uom.py
+++ addons/product/models/uom_uom.py
@@ -1,22 +1,13 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models, fields, _
+from odoo import models, fields, _
 
 
 class UomUom(models.Model):
     _inherit = 'uom.uom'
 
     product_uom_ids = fields.One2many('product.uom', 'uom_id', string='Barcodes', domain=lambda self: ['|', ('product_id', '=', self.env.context.get('product_id')), ('product_id', 'in', self.env.context.get('product_ids', []))])
-    packaging_barcodes_count = fields.Integer('Packaging Barcodes', compute='_compute_packaging_barcodes_count')
-
-    @api.depends('product_uom_ids')
-    def _compute_packaging_barcodes_count(self):
-        uom_to_barcode_count = dict(self.env['product.uom']._read_group(
-            [('uom_id', 'in', self.ids)], ['uom_id'], ['barcode:count'],
-        ))
-        for uom in self:
-            uom.packaging_barcodes_count = uom_to_barcode_count.get(uom, 0)
 
     def action_open_packaging_barcodes(self):
         self.ensure_one()
