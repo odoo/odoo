f1dac237ba86fe3104003d9d4b96eb12204bb16e
Yosua Nicolaus <yoni@odoo.com>
2025-01-21 08:20:42 +0000

    [FIX] l10n_ec_sale: missing sudo on geting default EC payment methods
    This commit fixes an AccessError on a failing test in runbot, by
    granting a `sudo` on the default record search for EC Payment Method
    field.
    
    runbot-102689
    
    closes odoo/odoo#194576
    
    X-original-commit: e3a2f641dfaf1f55c1bd95870089891265245073
    Signed-off-by: Laurent Smet (las) <las@odoo.com>
    Signed-off-by: Yosua Nicolaus (yoni) <yoni@odoo.com>

--- addons/l10n_ec_sale/models/sale_order.py
+++ addons/l10n_ec_sale/models/sale_order.py
@@ -8,7 +8,7 @@ class SaleOrder(models.Model):
         comodel_name="l10n_ec.sri.payment",
         string="Payment Method (SRI)",
         help="Ecuador: Payment Methods Defined by the SRI.",
-        default=lambda self: self.env['l10n_ec.sri.payment'].search([], limit=1),
+        default=lambda self: self.env['l10n_ec.sri.payment'].sudo().search([], limit=1),
     )
 
     def _prepare_invoice(self):
