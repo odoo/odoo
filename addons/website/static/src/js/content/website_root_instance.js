484d5ce7e8e0379f8e2b114651397f0667283cbb
Géry Debongnie <ged@odoo.com>
2025-01-21 10:34:55 +0100

    [IMP] website: parallelize assets loading
    This should make the website faster to load, in edit mode

--- addons/website/static/src/js/content/website_root_instance.js
+++ addons/website/static/src/js/content/website_root_instance.js
@@ -9,8 +9,11 @@ const prom = createPublicRoot(WebsiteRoot).then(async rootInstance => {
     // This data attribute is set by the WebsitePreview client action for a
     // restricted editor user.
     if (window.frameElement && window.frameElement.dataset.loadWysiwyg === 'true') {
-        await loadBundle("website.assets_all_wysiwyg");
-        await loadBundle("website.assets_edit_frontend");
+
+        await Promise.all([
+            loadBundle("website.assets_all_wysiwyg"),
+            loadBundle("website.assets_edit_frontend")
+        ]);
         window.dispatchEvent(new CustomEvent('PUBLIC-ROOT-READY', {detail: {rootInstance}}));
     }
     return rootInstance;
