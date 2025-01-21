0454bf0bbd453ea358ce6c44b47872971b15a2d6
Dylan Kiss (dyki) <dyki@odoo.com>
2025-01-21 16:38:56 +0100

    [FIX] account: validate entire structured reference
    In SEPA payment files, when a structured reference is used, it is put
    in the section `RmtInf/Strd/CdtrRefInf/Ref`. This section has a
    maximum length of 35 characters. Since we only use structured references
    that are always less that 35 characters, this should not be a problem.
    
    However, the way we validated the structured reference was by performing
    a `re.match()`, which only matches from the beginning of the string. So
    if more characters would be after the structured reference, it would
    still match and it would be possible to go beyond 35 characters.
    
    In this commit, we fix this by using a full match instead of a match
    from the beginning.
    
    opw-4357554
    
    closes odoo/odoo#194583
    
    Signed-off-by: Hugo Poncelet (hupo) <hupo@odoo.com>

--- addons/account/tests/test_structured_reference.py
+++ addons/account/tests/test_structured_reference.py
@@ -25,6 +25,8 @@ class StructuredReferenceTest(TransactionCase):
         self.assertFalse(is_valid_structured_reference_iso('18539007547034RF'))
         # Does not validate invalid checksum
         self.assertFalse(is_valid_structured_reference_iso('RF17539007547034'))
+        # Validates the entire string
+        self.assertFalse(is_valid_structured_reference_be('RF18539007547034-OTHER-RANDOM-STUFF'))
 
     def test_structured_reference_be(self):
         # Accepts references in both structured formats
@@ -39,6 +41,8 @@ class StructuredReferenceTest(TransactionCase):
         self.assertFalse(is_valid_structured_reference_be('***02/03430/57642***'))
         # Does not validate invalid checksum
         self.assertFalse(is_valid_structured_reference_be('020343057641'))
+        # Validates the entire string
+        self.assertFalse(is_valid_structured_reference_be('020343053497-OTHER-RANDOM-STUFF'))
 
     def test_structured_reference_fi(self):
         # Accepts references in structured format
@@ -54,6 +58,8 @@ class StructuredReferenceTest(TransactionCase):
         self.assertFalse(is_valid_structured_reference_fi('000000000002023000098'))
         # Does not validate invalid checksum
         self.assertFalse(is_valid_structured_reference_fi('2023000095'))
+        # Validates the entire string
+        self.assertFalse(is_valid_structured_reference_fi('2023000098-OTHER-RANDOM-STUFF'))
 
     def test_structured_reference_no_se(self):
         # Accepts references in structured format
@@ -67,6 +73,8 @@ class StructuredReferenceTest(TransactionCase):
         self.assertFalse(is_valid_structured_reference_no_se('1234/5678/97'))
         # Does not validate invalid checksum
         self.assertFalse(is_valid_structured_reference_no_se('1234567898'))
+        # Validates the entire string
+        self.assertFalse(is_valid_structured_reference_no_se('1234567897-OTHER-RANDOM-STUFF'))
 
     def test_structured_reference(self):
         # Accepts references in structured format
