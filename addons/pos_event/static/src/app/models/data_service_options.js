07a391b249348bcaa29a9699efbf10dd07b690c6
David Monnom <moda@odoo.com>
2025-01-22 16:05:29 +0100

    m

--- addons/pos_event/static/src/app/models/data_service_options.js
+++ addons/pos_event/static/src/app/models/data_service_options.js
@@ -2,6 +2,14 @@ import { DataServiceOptions } from "@point_of_sale/app/models/data_service_optio
 import { patch } from "@web/core/utils/patch";
 
 patch(DataServiceOptions.prototype, {
+    get serializableModels() {
+        const models = super.serializableModels;
+        return new Set([
+            "event.registration",
+            "event.registration.answer",
+            ...models,
+        ]);
+    },
     get databaseTable() {
         return {
             ...super.databaseTable,
