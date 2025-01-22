652d8bf9a2d9e1f88d3aa3e7d3936a5d668a41f3
Antoine Boonen <aboo@odoo.com>
2025-01-22 10:39:46 +0100

    [IMP] spreadsheet_account: Add formula ODOO.RESIDUAL and ODOO.PARTNER.BALANCE
    Problem
    ---------
    There is currently no formula to get the residual amount of accounts nor
    to get the balance of a partner in a given period of time in
    spreadsheets.
    
    Objective
    ---------
    Add the formula ODOO.RESIDUAL and ODOO.PARTNER.BALANCE that do just that
    
    ODOO.RESIDUAL should take the following parameters:
    - account_codes (mandatory)(can be more than one separate by ,): could
    be optional, in that case it could set the domain to all
    receivables/payables accounts
    - due_date_range or expected_date (optional): date range referred to due
    dates (not AML date)
    - offset (optional): adding the possibility of the offset to be not only
    “Year” but also, a specific date or other periods like Month, Quarter.
    - company_id (optional)
    - include_unposted (optional) (default: false)
    
    ODOO.PARTNER.BALANCE should take the following paramters:
    - partner_ids (mandatory) (can be more than one separate by ,)
    - accound_codes (optional) (the prefix of the account(s))
    - date_range (optional) (21/12/2022,Q1 2022, 12/2022, 2022)
    - offset (optional) (default: 0)
    - company_id (optional)
    - include_unposted (optional) (default: false)
    
    Solution
    ---------
    1. Define the spreadsheet function with the correct parameters in
    `accounting_function.js`
    2. Hook it to the data source so that it can perform server calls
    3. Craft that server python function to returns the list a residual
    amounts for the given list of parameters.
    4. Adds some tests.
    
    Task-3679808

--- addons/spreadsheet_account/models/account.py
+++ addons/spreadsheet_account/models/account.py
@@ -42,10 +42,15 @@ class AccountMove(models.Model):
             start, _ = date_utils.get_fiscal_year(end, fiscal_day, fiscal_month)
         return start, end
 
-    def _build_spreadsheet_formula_domain(self, formula_params):
+    def _build_spreadsheet_formula_domain(self, formula_params, default_accounts=False):
         codes = [code for code in formula_params["codes"] if code]
+
+        default_domain = expression.FALSE_DOMAIN
         if not codes:
-            return expression.FALSE_DOMAIN
+            if not default_accounts:
+                return default_domain
+            default_domain = [('account_type', 'in', ['liability_payable', 'asset_receivable'])]
+
         company_id = formula_params["company_id"] or self.env.company.id
         company = self.env["res.company"].browse(company_id)
         start, end = self._get_date_period_boundaries(
@@ -67,7 +72,8 @@ class AccountMove(models.Model):
             ]
             for code in codes
         )
-        account_ids = self.env["account.account"].with_company(company_id).search(code_domain).ids
+        account_domain = expression.OR([code_domain, default_domain])
+        account_ids = self.env["account.account"].with_company(company_id).search(account_domain).ids
         code_domain = [("account_id", "in", account_ids)]
         period_domain = expression.OR([balance_domain, pnl_domain])
         domain = expression.AND([code_domain, period_domain, [("company_id", "=", company_id)]])
@@ -79,11 +85,16 @@ class AccountMove(models.Model):
             domain = expression.AND(
                 [domain, [("move_id.state", "=", "posted")]]
             )
+        partner_ids = [int(partner_id) for partner_id in formula_params.get('partner_ids', []) if partner_id]
+        if partner_ids:
+            domain = expression.AND(
+                [domain, [("partner_id", "in", partner_ids)]]
+            )
         return domain
 
     @api.model
     def spreadsheet_move_line_action(self, args):
-        domain = self._build_spreadsheet_formula_domain(args)
+        domain = self._build_spreadsheet_formula_domain(args, default_accounts=True)
         return {
             "type": "ir.actions.act_window",
             "res_model": "account.move.line",
@@ -91,7 +102,7 @@ class AccountMove(models.Model):
             "views": [[False, "list"]],
             "target": "current",
             "domain": domain,
-            "name": _("Journal items for account prefix %s", ", ".join(args["codes"])),
+            "name": _("Cell Audit"),
         }
 
     @api.model
@@ -115,6 +126,60 @@ class AccountMove(models.Model):
             MoveLines = self.env["account.move.line"].with_company(company_id)
             [(debit, credit)] = MoveLines._read_group(domain, aggregates=['debit:sum', 'credit:sum'])
             results.append({'debit': debit or 0, 'credit': credit or 0})
+
+        return results
+
+    @api.model
+    def spreadsheet_fetch_residual_amount(self, args_list):
+        """Fetch data for ODOO.RESUDUAL formulas
+        The input list looks like this:
+        [{
+            date_range: {
+                range_type: "year"
+                year: int
+            },
+            company_id: int
+            codes: str[]
+            include_unposted: bool
+        }]
+        """
+        results = []
+        for args in args_list:
+            company_id = args["company_id"] or self.env.company.id
+            domain = self._build_spreadsheet_formula_domain(args, default_accounts=True)
+            MoveLines = self.env["account.move.line"].with_company(company_id)
+            [(amount_residual,)] = MoveLines._read_group(domain, aggregates=['amount_residual:sum'])
+            results.append({'amount_residual': amount_residual or 0})
+
+        return results
+
+    @api.model
+    def spreadsheet_fetch_partner_balance(self, args_list):
+        """Fetch data for ODOO.PARTNER.BALANCE formulas
+        The input list looks like this:
+        [{
+            date_range: {
+                range_type: "year"
+                year: int
+            },
+            company_id: int
+            codes: str[]
+            include_unposted: bool
+            partner_ids: int[]
+        }]
+        """
+        results = []
+        for args in args_list:
+            partner_ids = [partner_id for partner_id in args.get('partner_ids', []) if partner_id]
+            if not partner_ids:
+                results.append({'balance': 0})
+                continue
+
+            company_id = args["company_id"] or self.env.company.id
+            domain = self._build_spreadsheet_formula_domain(args, default_accounts=True)
+            MoveLines = self.env["account.move.line"].with_company(company_id)
+            [(balance,)] = MoveLines._read_group(domain, aggregates=['balance:sum'])
+            results.append({'balance': balance or 0})
 
         return results
 
