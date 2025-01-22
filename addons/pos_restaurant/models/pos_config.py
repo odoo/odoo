35a3a2c637e2693634457b7b9109a0546dbe6751
dhruv <djip@odoo.com>
2025-01-22 05:18:04 +0000

    [IMP] pos_self_order: configure preset in config
    Before this commit:
    
    - Preset data was not updated for self order (without demo data)
    
    After this commit:
    
    - Preset data updated in pos self order (without demo data)
    - If there are no presets found, set 'use_presets' as False
    - if there is any manually created preset, add it to the default preset
    
    Related PR: https://github.com/odoo/odoo/pull/194777
    Related PR: https://github.com/odoo/odoo/pull/199161
    
    Task: 4501570, 4612542
    X-original-commit: 0823a936fa07c3fc512210126e4e5133e944a708

--- addons/pos_restaurant/models/pos_config.py
+++ addons/pos_restaurant/models/pos_config.py
@@ -102,12 +102,36 @@ class PosConfig(models.Model):
             self.iface_available_categ_ids = bar_categories
 
     @api.model
+<<<<<<< saas-18.2
     def load_onboarding_restaurant_scenario(self, with_demo_data=True):
         if not self.env.ref('pos_restaurant.pos_resource_preset', raise_if_not_found=False):
             convert.convert_file(self._env_with_clean_context(), 'pos_restaurant', 'data/scenarios/restaurant_preset.xml', idref=None, mode='init', noupdate=True)
         journal, payment_methods_ids = self._create_journal_and_payment_methods(cash_journal_vals={'name': _('Cash Restaurant'), 'show_on_dashboard': False})
+||||||| 93e5eba827d5a65a27079dd1293bc40b3917c553
+    def load_onboarding_restaurant_scenario(self):
+        ref_name = 'pos_restaurant.pos_config_main_restaurant'
+        if not self.env.ref(ref_name, raise_if_not_found=False):
+            self._load_restaurant_data()
+
+        journal, payment_methods_ids = self._create_journal_and_payment_methods(cash_journal_vals={'name': 'Cash Restaurant', 'show_on_dashboard': False})
+        restaurant_categories = self.get_record_by_ref([
+            'pos_restaurant.food',
+            'pos_restaurant.drinks',
+        ])
+=======
+    def load_onboarding_restaurant_scenario(self):
+        ref_name = 'pos_restaurant.pos_config_main_restaurant'
+        if not self.env.ref(ref_name, raise_if_not_found=False):
+            self._load_restaurant_data()
+
+        journal, payment_methods_ids = self._create_journal_and_payment_methods(cash_journal_vals={'name': 'Cash Restaurant', 'show_on_dashboard': False})
+        restaurant_categories = self.get_record_by_ref([
+            'pos_restaurant.food',
+            'pos_restaurant.drinks',
+        ])
+        default_preset = self.env.ref('pos_restaurant.pos_takein_preset', False) or self.env['pos.preset'].search([('identification', '=', 'none')], limit=1)
+>>>>>>> bdc244bc2fb01e75fd6b96d6e9e404f580e4148b
         presets = self.get_record_by_ref([
-            'pos_restaurant.pos_takein_preset',
             'pos_restaurant.pos_takeout_preset',
             'pos_restaurant.pos_delivery_preset',
         ])
@@ -118,9 +142,9 @@ class PosConfig(models.Model):
             'payment_method_ids': payment_methods_ids,
             'iface_splitbill': True,
             'module_pos_restaurant': True,
-            'use_presets': True,
-            'default_preset_id': presets[0] if presets else False,
-            'available_preset_ids': [(6, 0, presets[1:])],
+            'use_presets': True if default_preset else False,
+            'default_preset_id': default_preset.id if default_preset else False,
+            'available_preset_ids': [(6, 0, ([default_preset.id] + presets) if default_preset else presets)],
         })
         self.env['ir.model.data']._update_xmlids([{
             'xml_id': self._get_suffixed_ref_name('pos_restaurant.pos_config_main_restaurant'),
