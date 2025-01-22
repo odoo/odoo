060f763ec13dcd2b2bfa6fd966b631d761c9363f
Vincent Larcin <vila@odoo.com>
2025-01-22 13:33:27 +0100

    [IMP] project,website_project: move "email_from" field to project
    This commit moves the field "email_from" on tasks from website_project to project
    in preparation of further features preventing infinite email loops.
    
    Task-4461572
    
    closes odoo/odoo#194699
    
    Related: odoo/upgrade#7106
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/website_project/models/project_task.py
+++ addons/website_project/models/project_task.py
@@ -1,13 +1,9 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import api, models, fields
+from odoo import fields, models
 
 
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
-    # Need this field to check there is no email loops when Odoo reply automatically
-    email_from = fields.Char('Email From')
     # Used to submit tasks from a contact form
     partner_name = fields.Char(string='Customer Name', related="partner_id.name", store=True, readonly=False, tracking=False)
     partner_company_name = fields.Char(string='Company Name', related="partner_id.company_name", store=True, readonly=False, tracking=False)
