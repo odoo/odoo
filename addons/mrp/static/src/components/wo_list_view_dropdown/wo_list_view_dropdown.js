5767cf0c0cbf2a4130430cd92a3cba8891b84510
Mathias Mathy (MAMA) <mama@odoo.com>
2025-01-22 13:25:00 +0100

    [IMP] mrp: workorder revamp
    - Allow manual editing of work order states to provide greater flexibility.
    - Simplify work order status computation, reducing rigid state dependencies.
    - Minimize state recalculations, ensuring a more straightforward and maintainable codebase.
    - Address pain points where state transitions were irreversible and difficult to modify.
    - Facilitate easier application of future changes and enhancements.
    
    Task: [4289826](https://www.odoo.com/odoo/966/tasks/4289826)
    odoo/enterprise#77691
    odoo/upgrade#7405
    
    closes odoo/odoo#194841
    
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/mrp/static/src/components/wo_list_view_dropdown/wo_list_view_dropdown.js
+++ addons/mrp/static/src/components/wo_list_view_dropdown/wo_list_view_dropdown.js
@@ -1,15 +1,10 @@
 import { Component, useState } from "@odoo/owl";
-import { downloadReport } from "@web/webclient/actions/reports/utils";
 import { Dropdown } from "@web/core/dropdown/dropdown";
 import { DropdownItem } from "@web/core/dropdown/dropdown_item";
 import { registry } from "@web/core/registry";
-import { rpc } from "@web/core/network/rpc";
 import { standardWidgetProps } from "@web/views/widgets/standard_widget_props";
-import { _t } from "@web/core/l10n/translation";
-import { user } from "@web/core/user";
 import { useService } from "@web/core/utils/hooks";
 
-const workcenterStatusBlocked = "blocked";
 
 export class MOListViewDropdown extends Component {
     static template = "mrp.MOViewListDropdown";
@@ -26,8 +21,11 @@ export class MOListViewDropdown extends Component {
             state: this.props.record.data.state,
         });
         this.colorIcons = {
-            "done": "o_status_success",
-            [workcenterStatusBlocked]: "o_status_danger",
+            "blocked": "bg-warning",
+            "ready": "bg-muted",
+            "progress": "bg-info",
+            "cancel": "bg-danger",
+            "done": "bg-success",
         };
     }
 
@@ -37,58 +35,35 @@ export class MOListViewDropdown extends Component {
     }
 
     get statusColor() {
-        const state = this.isBlocked ? workcenterStatusBlocked : this.workorderState.state;
+        const state = this.workorderState.state;
         return this.colorIcons[state] || "";
     }
 
-    get isBlocked() {
-        return this.props.record.data.working_state == workcenterStatusBlocked;
-    }
-
-    get blockTitle(){
-        if (this.isBlocked) {
-            return _t("Unblock");
+    async setState(state) {
+        let selectedWorkorders = this.props.record.model.root.selection;
+        if (!selectedWorkorders || selectedWorkorders.length == 0) {
+            selectedWorkorders = [this.props.record];
         }
-        return _t("Block");
-    }
-
-    async toggleBlock() {
-        if (this.isBlocked) {
-            await this.callOrm("button_unblock");
-            return;
+        let ids = selectedWorkorders.filter((wo) => !([state, 'done'].includes(wo.data.state) || wo.data.production_state == 'done')).map((wo) => wo.resId)  
+        if (ids && ids.length > 0) {
+            await this.callOrm("set_state", [state], ids);
         }
-        const options = {
-            additionalContext: { default_workcenter_id: this.props.record.data.workcenter_id[0] },
-            onClose: async () => {
-                await this.reload();
-            },
-        };
-        this.action.doAction("mrp.act_mrp_block_workcenter_wo", options);
     }
 
-    async markAsDone() {
-        await this.callOrm("action_mark_as_done");
-    }
 
-    async printWO() {
-        await downloadReport(
-            rpc,
-            {
-                ...{ report_name: "mrp.report_mrp_workorder", report_type: "qweb-pdf" },
-                context: { active_ids: [this.props.record.resId] },
-            },
-            "pdf",
-            user.context
-        );
-    }
-
-    async callOrm(functionName){
-        let ids = this.props.record.model.root.selection?.map((element) => element.evalContext.id);
+    async callOrm(functionName, args, ids = undefined) {
+        if (!ids){
+            ids = this.props.record.model.root.selection?.map((element) => element.evalContext.id);
+        }
         // if no records selected, take the current clicked one
         if (!ids || ids.length == 0) {
             ids = [this.props.record.resId];
         }
-        await this.orm.call("mrp.workorder", functionName, [ids]);
+        if (args !== undefined) {
+            await this.orm.call("mrp.workorder", functionName, [ids, ...args]);
+        } else {
+            await this.orm.call("mrp.workorder", functionName, [ids]);
+        }
         await this.reload();
     }
 }
