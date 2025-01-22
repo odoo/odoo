05a1d6ef542711ec8671e2a1816c62fca2ed4d90
JF Aubert <ajf@odoo.com>
2025-01-22 13:19:09 +0100

    [IMP] mrp,*: adapt operation duration calculation
    Adapt the way capacities are used
    Remove _get_expected_duration on workcenter
    Adapt and only use _get_duration_expected on operation
    Add new fields cycle_number and time_total
    
    task: 4300594
    
    See odoo/enterprise#78517
    See odoo/upgrade#7157

--- addons/mrp_account/models/__init__.py
+++ addons/mrp_account/models/__init__.py
@@ -3,7 +3,6 @@ from . import analytic_account
 from . import mrp_workcenter
 from . import mrp_workorder
 from . import mrp_production
-from . import mrp_routing
 from . import product
 from . import stock_move
 from . import account_move
