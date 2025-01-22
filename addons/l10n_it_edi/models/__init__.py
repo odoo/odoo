1cbff662548ab4b785207f328fcb5a3f0dab94b2
Maximilien (malb) <malb@odoo.com>
2025-01-22 15:17:57 +0100

    [IMP] l10n_it_edi, l10n_it_edi_ndd: merge module
    This commit will merge the two modules.
    
    task-4497665
    
    closes odoo/odoo#194729
    
    Related: odoo/upgrade#7108
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/l10n_it_edi/models/__init__.py
+++ addons/l10n_it_edi/models/__init__.py
@@ -2,8 +2,10 @@
 from . import res_partner
 from . import res_company
 from . import res_config_settings
+from . import account_payment_method_line
 from . import account_move
 from . import account_move_send
 from . import ddt
 from . import ir_attachment
 from . import account_edi_proxy_user
+from. import l10n_it_document_type
