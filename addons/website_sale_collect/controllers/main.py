8f55a11c9e63e4567e7e5bf3cd0a83e3f857f16f
Valentin Chevalier <vcr@odoo.com>
2025-01-22 19:23:13 +0100

    fixup! [IMP] website_sale_collect: hide location selector if only one location

--- addons/website_sale_collect/controllers/main.py
+++ addons/website_sale_collect/controllers/main.py
@@ -30,25 +30,21 @@ class WebsiteSaleCollect(WebsiteSale):
         location and set the pickup location when there is only one warehouse available. """
         res = super()._prepare_checkout_page_values(order_sudo, **query_params)
 
-        res['pickup_locations'] = [
-            {
-                'dm_id': dm.id,
-                'pickup_location': dm._in_store_get_close_locations(
-                    partner_address=order_sudo.partner_shipping_id,
-                    product_id=None
-                )[0]
+        res['default_pickup_locations'] = {
+            in_store_dm.id: {
+                'pickup_location_data': (
+                    pickup_location_data := in_store_dm._in_store_get_close_locations(
+                        partner_address=order_sudo.partner_shipping_id,
+                    )[0]
+                ),
+                'unavailable_order_lines': order_sudo._get_unavailable_order_lines(
+                    pickup_location_data['id']
+                ),
             }
-            for dm in order_sudo._get_delivery_methods()
-            if dm.delivery_type == 'in_store' and len(dm.warehouse_ids) == 1
-                and dm._in_store_get_close_locations(order_sudo.partner_shipping_id, None)
-        ]
-
-        if res.get('pickup_locations'):
-            res['unavailable_order_lines'] = [
-                line
-                for location in res['pickup_locations']
-                for line in order_sudo._get_unavailable_order_lines(location['pickup_location'].get('id'))
-            ]
+            for in_store_dm in order_sudo._get_delivery_methods().filtered(
+                lambda dm: dm.delivery_type == 'in_store' and len(dm.warehouse_ids) == 1
+            ) - order_sudo.carrier_id  # If Pickup is selected, assume location data is included.
+        }
 
         if order_sudo.carrier_id.delivery_type == 'in_store' and order_sudo.pickup_location_data:
             res['unavailable_order_lines'] = order_sudo._get_unavailable_order_lines(
