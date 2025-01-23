4aa131dfef20daec1324d60aafc031f2930a3b9c
Tiffany Chang (tic) <tic@odoo.com>
2025-01-23 09:20:01 +0000

    [I18N] point_of_sale: make receipt header fully translated
    Because 'Tax ID' was within the `t-esc` the term was never translated.
    This goes against some country's compliance requirements for receipts,
    so we make it translatable now.
    
    We also make the full line translatable together since sometimes there
    are spacing + ordering differences between languages
    
    closes odoo/odoo#195487
    
    X-original-commit: 5e5347b4eca58895b03a78e4fae4caf266dd793c
    Signed-off-by: Louis Wicket (wil) <wil@odoo.com>
    Signed-off-by: Tiffany Chang (tic) <tic@odoo.com>

--- addons/point_of_sale/static/src/app/screens/receipt_screen/receipt/receipt_header/receipt_header.js
+++ addons/point_of_sale/static/src/app/screens/receipt_screen/receipt/receipt_header/receipt_header.js
@@ -1,3 +1,4 @@
+import { _t } from "@web/core/l10n/translation";
 import { Component } from "@odoo/owl";
 
 export class ReceiptHeader extends Component {
@@ -12,5 +13,15 @@ export class ReceiptHeader extends Component {
 
     get partnerAddress() {
         return this.order.partner_id.pos_contact_address.split("\n");
+    }
+
+    get vatText() {
+        if (this.order.company.country_id?.vat_label) {
+            return _t("%(vatLabel)s: %(vatId)s", {
+                vatLabel: this.order.company.country_id.vat_label,
+                vatId: this.order.company.vat,
+            });
+        }
+        return _t("Tax ID: %(vatId)s", { vatId: this.order.company.vat });
     }
 }
