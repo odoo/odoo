8903ec9b0528e01a5167c46d082560e5a8e461c1
Benjamin Vray <bvr@odoo.com>
2025-01-23 16:18:21 +0100

    [FIX] website: fix required fields conditionally hidden
    Steps to Reproduce:
    
    - Go to the website editor.
    - Add a form to the page.
    - Select the form and change the default action from "Send an email" to
    "Create an opportunity."
    - Select the "Subject" field.
    - Set the visibility condition of the "Subject" field to "visible only
    if the phone number is set."
    - Since this is a mandatory field, define a default value for the
    "Subject" field.
    - Save the changes.
    - Fill out the form but leave the phone number field empty so that the
    "Subject" field does not appear.
    - Attempt to submit the form.
    - Bug: the form is not sent and there is an error message.
    
    The error occurs preventing the record from being created because all
    required fields are not present, we should use the default value as it
    has been provided.
    
    This commit prevents adding the "disabled" attribute to inputs that are
    required for models.
    
    This commit also adds tests to ensure that a field required for a model
    is not disabled when conditionally hidden, and that a required field
    (but not required for a model) is disabled when conditionally hidden.
    
    opw-4447039

--- addons/website/static/src/snippets/s_website_form/000.js
+++ addons/website/static/src/snippets/s_website_form/000.js
@@ -714,10 +714,13 @@ odoo.define('website.s_website_form', function (require) {
         _updateFieldVisibility(fieldEl, haveToBeVisible) {
             const fieldContainerEl = fieldEl.closest('.s_website_form_field');
             fieldContainerEl.classList.toggle('d-none', !haveToBeVisible);
-            for (const inputEl of fieldContainerEl.querySelectorAll('.s_website_form_input')) {
-                // Hidden inputs should also be disabled so that their data are
-                // not sent on form submit.
-                inputEl.disabled = !haveToBeVisible;
+            // Do not disable inputs that are required for the model.
+            if (!fieldContainerEl.matches(".s_website_form_model_required")) {
+                for (const inputEl of fieldContainerEl.querySelectorAll(".s_website_form_input")) {
+                    // Hidden inputs should also be disabled so that their data are
+                    // not sent on form submit.
+                    inputEl.disabled = !haveToBeVisible;
+                }
             }
         },
 
