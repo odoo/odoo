bfd4d12d81663ee308756d19a6b3593aa684ad94
Xavier ALT <xal@odoo.com>
2025-01-24 08:08:57 +0000

    [FIX] l10n_in_gstin_status: restrict GSTIN update to IN companies
    This commit fixes a few issues in multi-company context:
    
    - only show the GSTIN status/update button when user has at least one
      IN companies selected
    
    - only allow the update when user's active company is an IN company
      (to ensure we correctly determine the EDI test/production status)
    
    opw-4367302
    
    closes odoo/odoo#196860
    
    X-original-commit: 77fdc51855977b39b54fa1008c6e44514ec4f5e6
    Signed-off-by: Ruben Gomes (rugo) <rugo@odoo.com>
    Signed-off-by: Xavier Alt (xal) <xal@odoo.com>

--- addons/l10n_in/models/res_partner.py
+++ addons/l10n_in/models/res_partner.py
@@ -90,6 +90,9 @@ class ResPartner(models.Model):
 
     def action_l10n_in_verify_gstin_status(self):
         self.ensure_one()
+        self.check_access('write')
+        if self.env.company.sudo().account_fiscal_country_id.code != 'IN':
+            raise UserError(_('You must be logged in an Indian company to use this feature'))
         if not self.vat:
             raise ValidationError(_("Please enter the GSTIN"))
         if not self.env.company.l10n_in_gstin_status_feature:
