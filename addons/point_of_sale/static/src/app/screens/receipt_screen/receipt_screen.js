c03b2aed4f6fe272dbb5a4ddc67717cfadbebbb4
Parth Solanki[PASO] <paso@odoo.com>
2025-01-24 10:17:35 +0000

    [IMP] pos_hr: make edit payment button invisible on receipt print
    Before this commit:
    ===============
    - The 'Edit Payment' button appears even after printing the receipt.
    (only when pos_hr installed)
    
    After this commit:
    ===============
    - The 'Edit Payment' button will disappear after printing the receipt.
    
    task-4507326
    
    closes odoo/odoo#196379
    
    X-original-commit: eef2ffcfccf2e123e40a0bf42edc4fa760da1980
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Parth Solanki (paso) <paso@odoo.com>

--- addons/point_of_sale/static/src/app/screens/receipt_screen/receipt_screen.js
+++ addons/point_of_sale/static/src/app/screens/receipt_screen/receipt_screen.js
@@ -20,7 +20,6 @@ export class ReceiptScreen extends Component {
         this.renderer = useService("renderer");
         this.notification = useService("notification");
         this.dialog = useService("dialog");
-        this.currentOrder = this.pos.getOrder();
         const partner = this.currentOrder.getPartner();
         const email = partner?.invoice_emails || partner?.email || "";
         this.state = useState({
@@ -45,6 +44,9 @@ export class ReceiptScreen extends Component {
             destination: this.state.email,
             name: "Email",
         });
+    }
+    get currentOrder() {
+        return this.pos.getOrder();
     }
     get orderAmountPlusTip() {
         const order = this.currentOrder;
