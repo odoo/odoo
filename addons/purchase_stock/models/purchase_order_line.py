0cd14c0a96c2d77415148515f8841bb9ec746149
Arnold Moyaux <arm@odoo.com>
2025-01-24 14:43:38 +0100

    [FIX] uom: be able to modify the UoM on product template
    The constraint to change the main UoM of a product has been removed.
    Globally on advanced case, it will result in a data corruption.
    
    There is two different use case when modifying the UoM of product.
    First, the user wants to manage the product flow to handle a different packaging
    by default (e.g. unit to pack of 6). In this case, the previous
    documents (SO/PO/...) can be trust and we need to update the current inventory
    in consequence (30 units -> 5 pack of 6).
    Second, he made a mistake or install the UoM setting and wants to update
    the everything at once. In this case we set the new UoM everywhere and
    we don't do any recomputation.
    
    Both flows are valid use cases but we choose to handle the second for a
    better onboarding of new users.
    
    In order to do it, we check if all the documents are in the base UoM and
    if it's possible to modify them everywhere without any computation. If
    not, we block as before. If yes, we update all the stored many2one for
    UoM accordingly.
    
    closes odoo/odoo#195052
    
    Related: odoo/enterprise#78282
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/purchase_stock/models/purchase_order_line.py
+++ addons/purchase_stock/models/purchase_order_line.py
@@ -101,11 +101,6 @@ class PurchaseOrderLine(models.Model):
         lines = self.filtered(lambda l: l.order_id.state == 'purchase'
                                         and not l.display_type)
 
-        if 'product_uom_id' in values and values['product_uom_id'] != self.product_id.uom_id.id:
-            self.move_ids.filtered(
-                lambda m: m.state not in ['cancel', 'done']
-            ).product_uom = values['product_uom_id']
-
         previous_product_uom_qty = {line.id: line.product_uom_qty for line in lines}
         previous_product_qty = {line.id: line.product_qty for line in lines}
         result = super(PurchaseOrderLine, self).write(values)
