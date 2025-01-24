66c362766da7339cd87ccb24767ee03ab662811e
Walid (wasa) <wasa@odoo.com>
2025-01-24 09:13:14 +0000

    [FIX] html_editor: programmatically add space on automatic link creation
    **Problem**:
    In Safari, relying on the browser to add a space after automatic link creation
    causes the cursor to be incorrectly positioned. This issue arises due to DOM
    manipulations (adding and removing nodes) during the process.
    
    **Solution**:
    To ensure proper link insertion and cursor positioning, explicitly add the space
    after the link and set the selection programmatically.
    
    **Steps to Reproduce**:
    1. Open the Editor on Safari.
    2. Add the text "test.com".
    3. Press the space key.
    4. Observe that the link is created, but the cursor moves to the start of the paragraph.
    
    opw-4441817
    
    X-original-commit: 996e7e7c5d53be1e9f851b50f3f16b09f2738e4a

--- addons/html_editor/static/tests/link/transform.test.js
+++ addons/html_editor/static/tests/link/transform.test.js
@@ -7,12 +7,24 @@ import { getContent, setSelection } from "../_helpers/selection";
 import { insertText, undo } from "../_helpers/user_actions";
 
 async function insertSpace(editor) {
-    await manuallyDispatchProgrammaticEvent(editor.editable, "keydown", { key: " " });
-    // InputEvent is required to simulate the insert text.
-    await manuallyDispatchProgrammaticEvent(editor.editable, "beforeinput", {
-        inputType: "insertText",
-        data: " ",
+    const keydownEvent = await manuallyDispatchProgrammaticEvent(editor.editable, "keydown", {
+        key: " ",
     });
+    if (keydownEvent.defaultPrevented) {
+        return;
+    }
+    // InputEvent is required to simulate the insert text.
+    const beforeinputEvent = await manuallyDispatchProgrammaticEvent(
+        editor.editable,
+        "beforeinput",
+        {
+            inputType: "insertText",
+            data: " ",
+        }
+    );
+    if (beforeinputEvent.defaultPrevented) {
+        return;
+    }
     const range = editor.document.getSelection().getRangeAt(0);
     if (!range.collapsed) {
         throw new Error("need to implement something... maybe");
@@ -41,11 +53,13 @@ async function insertSpace(editor) {
         anchorOffset: offset,
     });
 
-    await manuallyDispatchProgrammaticEvent(editor.editable, "input", {
+    const inputEvent = await manuallyDispatchProgrammaticEvent(editor.editable, "input", {
         inputType: "insertText",
         data: " ",
     });
-
+    if (inputEvent.defaultPrevented) {
+        return;
+    }
     // KeyUpEvent is not required but is triggered like the browser would.
     await manuallyDispatchProgrammaticEvent(editor.editable, "keyup", { key: " " });
 }
@@ -60,7 +74,7 @@ test("should transform url after space", async () => {
             await insertSpace(editor);
         },
         contentAfter:
-            '<p>a http://test.com b <a href="http://test.com">http://test.com</a> []&nbsp;c http://test.com d</p>',
+            '<p>a http://test.com b <a href="http://test.com">http://test.com</a>&nbsp;[] c http://test.com d</p>',
     });
     await testEditor({
         contentBefore: "<p>http://test.com[]</p>",
@@ -74,7 +88,7 @@ test("should transform url after space", async () => {
             // Action: insert space
             await insertSpace(editor);
         },
-        contentAfter: '<p><a href="http://test.com">http://test.com</a> []</p>',
+        contentAfter: '<p><a href="http://test.com">http://test.com</a>&nbsp;[]</p>',
     });
 });
 
@@ -84,22 +98,22 @@ test("should transform url followed by punctuation characters after space", asyn
         stepFunction: async (editor) => {
             await insertSpace(editor);
         },
-        contentAfter: '<p><a href="http://test.com">http://test.com</a>. []</p>',
+        contentAfter: '<p><a href="http://test.com">http://test.com</a>.&nbsp;[]</p>',
     });
     await testEditor({
         contentBefore: "<p>test.com...[]</p>",
         stepFunction: (editor) => insertSpace(editor),
-        contentAfter: '<p><a href="https://test.com">test.com</a>... []</p>',
+        contentAfter: '<p><a href="https://test.com">test.com</a>...&nbsp;[]</p>',
     });
     await testEditor({
         contentBefore: "<p>test.com,[]</p>",
         stepFunction: (editor) => insertSpace(editor),
-        contentAfter: '<p><a href="https://test.com">test.com</a>, []</p>',
+        contentAfter: '<p><a href="https://test.com">test.com</a>,&nbsp;[]</p>',
     });
     await testEditor({
         contentBefore: "<p>test.com,hello[]</p>",
         stepFunction: (editor) => insertSpace(editor),
-        contentAfter: '<p><a href="https://test.com">test.com</a>,hello []</p>',
+        contentAfter: '<p><a href="https://test.com">test.com</a>,hello&nbsp;[]</p>',
     });
     await testEditor({
         contentBefore: "<p>http://test.com[]</p>",
@@ -113,7 +127,7 @@ test("should transform url followed by punctuation characters after space", asyn
             // Action: insert space
             await insertSpace(editor);
         },
-        contentAfter: '<p><a href="http://test.com">http://test.com</a> []</p>',
+        contentAfter: '<p><a href="http://test.com">http://test.com</a>&nbsp;[]</p>',
     });
 });
 
@@ -166,7 +180,7 @@ test("transform text url into link and undo it", async () => {
     const { el, editor } = await setupEditor(`<p>[]</p>`);
     await insertText(editor, "www.abc.jpg ");
     expect(cleanLinkArtifacts(getContent(el))).toBe(
-        '<p><a href="https://www.abc.jpg">www.abc.jpg</a> []</p>'
+        '<p><a href="https://www.abc.jpg">www.abc.jpg</a>&nbsp;[]</p>'
     );
 
     undo(editor);
