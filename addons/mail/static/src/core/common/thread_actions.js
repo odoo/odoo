4045972f059115ff2cf6bafc3b9284e03f59f81f
Alexandre Kühn <aku@odoo.com>
2025-01-24 12:38:58 +0100

    [FIX] mail: livechat from visitor stays open in mobile
    Before this commit, when using livechat by visitor in mobile,
    any page reload would remove access to the livechat.
    
    Steps to reproduce:
    - access to website with livechat installed on mobile
    - click on livechat button (bubble icon in bottom right)
    - send a message
    - reload the page
    
    => the livechat is not open, not even as a chat bubble.
    
    This prevents visitor from access the livechat again, except if
    user finds a workaround to open the same page in non-mobile mode
    (e.g. landscape mode or disabling high-dpi).
    
    This happens because livechat relies on discuss chat hub, and
    there's some code to prevent server-side synchronisation of chat
    windows and bubbles in mobile. As a reminder, conversation in
    mobile are shown as fullscreen chat windows.
    This code was preventing the opening of chat window on livechat
    for visitor, leading to prevention of continuing to live chat
    on the device.
    
    This commit fixes by adding fold action to chat windows in mobile,
    thus allowing to have chat bubbles in mobile. This fixes allow to
    show chat windows as bubbles to livechat visitors, which is the
    primary fix of these changes.
    
    opw-4423556
    opw-4488830

--- addons/mail/static/src/core/common/thread_actions.js
+++ addons/mail/static/src/core/common/thread_actions.js
@@ -9,7 +9,7 @@ export const threadActionsRegistry = registry.category("mail.thread/actions");
 threadActionsRegistry
     .add("fold-chat-window", {
         condition(component) {
-            return !component.ui.isSmall && component.props.chatWindow;
+            return component.props.chatWindow;
         },
         icon: "fa fa-fw fa-minus",
         name(component) {
@@ -103,6 +103,9 @@ function transformAction(component, id, action) {
         },
         /** Condition to display this action. */
         get condition() {
+            if (action.condition === undefined) {
+                return true;
+            }
             return action.condition(component);
         },
         /** Condition to disable the button of this action (but still display it). */
