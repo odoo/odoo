74f62c4e4aee635896c99f83cff68be67a8b4b4a
Serge Bayet (seba) <seba@odoo.com>
2025-01-24 07:37:12 +0000

    [FIX] website: clean keywords in SEO dialog
    Steps to reproduce:
    
    - Edit a page
    - open "Optimize SEO" dialog
    - Add a tag with a parenthesis. eg. "Webinar Tools (OBS, YouTube)"
    - Save
    - Reload the page
    - Reopen the "Optimize SEO" dialog
    - ... crash
    
    Since [1], the application crashes because keywords are dynamically used
    in a regular expression, and special characters (e.g., parentheses) are
    not properly escaped.
    
    We address the keywords issues by escaping control characters before
    using them in the regular expression to prevent crashes.
    
    [1]: https://github.com/odoo/odoo/commit/ac55f2bb113ecf7c774fe6e96d28e716184a97d1#diff-b51336c1ad97255bfb3646f013327eca1904ca194078f4ce671e517096233c58
    
    task-4420262
    
    closes odoo/odoo#196118
    
    X-original-commit: 6145bcfd54a1e7725724a04b3acc955649301080
    Signed-off-by: Benjamin Vray (bvr) <bvr@odoo.com>
    Signed-off-by: Serge Bayet (seba) <seba@odoo.com>

--- addons/website/static/src/components/dialog/seo.js
+++ addons/website/static/src/components/dialog/seo.js
@@ -1,6 +1,7 @@
 import { _t } from "@web/core/l10n/translation";
 import { pyToJsLocale, jsToPyLocale } from "@web/core/l10n/utils";
 import { rpc } from "@web/core/network/rpc";
+import { escapeRegExp } from "@web/core/utils/strings";
 import { useService, useAutofocus } from '@web/core/utils/hooks';
 import { MediaDialog } from '@web_editor/components/media_dialog/media_dialog';
 import { WebsiteDialog } from './dialog';
@@ -157,13 +158,19 @@ class Keyword extends Component {
                 lang: jsToPyLocale(this.props.language),
                 keywords: this.props.keyword,
             });
-            const regex = new RegExp(WORD_SEPARATORS_REGEX + this.props.keyword + WORD_SEPARATORS_REGEX, 'gi');
+            const regex = new RegExp(
+                WORD_SEPARATORS_REGEX + escapeRegExp(this.props.keyword) + WORD_SEPARATORS_REGEX,
+                "gi"
+            );
             this.state.suggestions = [...new Set(JSON.parse(suggestions).map(word => word.replace(regex, '').trim()))];
         });
     }
 
     isKeywordIn(string) {
-        return new RegExp(WORD_SEPARATORS_REGEX + this.props.keyword + WORD_SEPARATORS_REGEX, 'gi').test(string);
+        return new RegExp(
+            WORD_SEPARATORS_REGEX + escapeRegExp(this.props.keyword) + WORD_SEPARATORS_REGEX,
+            "gi"
+        ).test(string);
     }
 
     getHeaders(tag) {
@@ -238,7 +245,7 @@ class MetaKeywords extends Component {
     }
 
     addKeyword(keyword) {
-        keyword = keyword.trim();
+        keyword = keyword.replaceAll(/,\s*/gi, " ").trim();
         if (keyword && !this.isFull && !this.seoContext.keywords.includes(keyword)) {
             this.seoContext.keywords.push(keyword);
             this.state.keyword = '';
