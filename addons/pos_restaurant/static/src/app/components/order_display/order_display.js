7b0b6f117a44cc0618d8bad70982f6cc3b17a705
Loukas Wets (lowe) <lowe@odoo.com>
2025-01-24 08:48:03 +0000

    [IMP] point_of_sale, *: revamp UI
    *: pos_restaurant
    
    This commit introduces some UI cleaning for the POS.
    
    task-4496287
    
    Revert "[IMP] point_of_sale,pos_restaurant: change table name system"
    
    This reverts commit 545a32c6a7398d203925ee6aad2060e571be72b7.
    
    X-original-commit: 5e77c14912324bf967a55a1a40bb01071eca5c8b

--- addons/pos_restaurant/static/src/app/components/order_display/order_display.js
+++ addons/pos_restaurant/static/src/app/components/order_display/order_display.js
@@ -1,16 +1,7 @@
 import { patch } from "@web/core/utils/patch";
-import { _t } from "@web/core/l10n/translation";
 import { OrderDisplay } from "@point_of_sale/app/components/order_display/order_display";
 import { OrderCourse } from "@pos_restaurant/app/components/order_course/order_course";
 patch(OrderDisplay.prototype, {
-    emptyCartText() {
-        let text = super.emptyCartText(...arguments);
-        if (this.order.config.module_pos_restaurant && !this.order.isBooked) {
-            text += " " + _t("or book the table for later");
-        }
-        return text;
-    },
-
     get displayCourses() {
         if (
             !this.order.config.module_pos_restaurant ||
