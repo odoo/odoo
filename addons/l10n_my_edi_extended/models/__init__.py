0d60ee629128dd6c3f8ebd4814dc833795ac9433
Nicolas Viseur (vin) <vin@odoo.com>
2025-01-27 09:54:07 +0800

    [IMP] l10n_my_*: merge modules
    In 4424a0f, we added a new module for the Malaysian
    EDI in order to bring some missing features to the
    EDI.
    This commit will merge that new module with the
    original one, keeping all the features in a single
    place.
    
    task-4516544

--- addons/l10n_my_edi_extended/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-from . import account_edi_xml_ubl_my
-from . import account_move
-from . import account_move_line
-from . import account_move_send
-from . import res_partner
