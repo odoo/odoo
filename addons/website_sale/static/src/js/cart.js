5dea65a41cd9842342d3b252e0ea1f750fe074c9
Lina (liew) <liew@odoo.com>
2025-01-27 15:26:06 +0000

    [FIX] website_sale: fix overlapping divs on mobile
    The navigation buttons on the cart were repositioned on the screen
    for mobile view by setting position to absolute.
    However this causes an issue if a user adds blocks from the editor
    below the checkout screen, since  disregards
    the height of the element.
    To fix this the height of the parent container is updated to include
    the size of navigation buttons.
    
    opw-4498660
    
    X-original-commit: bb9573cfaabb238bd771d206d21d65dd399f6140

--- addons/website_sale/static/src/js/cart.js
+++ addons/website_sale/static/src/js/cart.js
@@ -1,6 +1,7 @@
 import { Component } from "@odoo/owl";
 import { rpc } from "@web/core/network/rpc";
 import { debounce } from "@web/core/utils/timing";
+import { utils as uiUtils } from "@web/core/ui/ui_service";
 import publicWidget from "@web/legacy/js/public/public_widget";
 
 import wSaleUtils from "@website_sale/js/website_sale_utils";
@@ -97,6 +98,45 @@ publicWidget.registry.websiteSaleCart = publicWidget.Widget.extend({
             // Propagating the change to the express checkout forms
             Component.env.bus.trigger('cart_amount_changed', [data.amount, data.minor_amount]);
         });
+    },
+});
+
+publicWidget.registry.websiteSaleCartNavigation = publicWidget.Widget.extend({
+    selector: '.o_website_sale_checkout',
+
+    /**
+     * For mobile screens, `.o_cta_navigation_container` has an absolute position causing
+     * overlapping issues with nearby divs, therefore the height of `.o_website_sale_checkout` needs
+     * to include the height of the absolute div and needs to be updated every time an element on
+     * the checkout is expanded (i.e. payment methods, cart summary)
+     *
+     * @override
+     */
+    start() {
+        const ctaNavigation = document.querySelector('.o_cta_navigation_container')
+        if (uiUtils.isSmall() && ctaNavigation) {
+            const updateCheckoutHeight = () => {
+                const updatedHeight = ctaNavigation.offsetTop + ctaNavigation.offsetHeight
+                this.el.style.height = `${updatedHeight}px`;
+            }
+            this.resizeObserver = new ResizeObserver(updateCheckoutHeight);
+            const paymentForm = document.getElementById('o_payment_form');
+            const cartSummary = document.getElementById('o_wsale_accordion_item');
+            if (paymentForm) {
+                this.resizeObserver.observe(paymentForm);
+            }
+            if (cartSummary) {
+                this.resizeObserver.observe(cartSummary);
+            }
+        }
+    },
+
+    /**
+     * @override
+     */
+    destroy() {
+        this.resizeObserver.disconnect();
+        super.destroy();
     },
 });
 
