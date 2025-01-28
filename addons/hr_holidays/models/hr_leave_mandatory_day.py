25007e10c59a7dfe0fed6327a9bcb49b44f2e0fe
ravb-odoo <ravb@odoo.com>
2025-01-28 17:36:01 +0530

    [IMP] hr_holidays: improve mandatory day functionality with job position
    This PR improves the mandatory day functionality by introducing a job position
    field. Mandatory days can now be defined based on specific job positions within
    a department.
    
    task-4018949

--- addons/hr_holidays/models/hr_leave_mandatory_day.py
+++ addons/hr_holidays/models/hr_leave_mandatory_day.py
@@ -1,7 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from random import randint
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class HrLeaveMandatoryDay(models.Model):
@@ -17,9 +17,17 @@ class HrLeaveMandatoryDay(models.Model):
     resource_calendar_id = fields.Many2one(
         'resource.calendar', 'Working Hours',
         domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
+    departments_for_job_domain = fields.Many2many('hr.department', compute='_compute_department_domain')
     department_ids = fields.Many2many('hr.department', string="Departments")
+    job_ids = fields.Many2many('hr.job', string="Job Position", domain="[('department_id', 'in', departments_for_job_domain)]")
 
     _date_from_after_day_to = models.Constraint(
         'CHECK(start_date <= end_date)',
         'The start date must be anterior than the end date.',
     )
+
+    @api.depends('job_ids', 'department_ids')
+    def _compute_department_domain(self):
+        all_departments = self.env['hr.department'].search([])
+        for record in self:
+            record.departments_for_job_domain = [(6, 0, record.department_ids.ids if record.department_ids else all_departments.ids)]
