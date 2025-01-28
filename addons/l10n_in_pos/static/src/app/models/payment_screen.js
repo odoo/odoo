9a3cf6f2b83323563786db9efdedd1b96450dd10
Meet Jivani [MEJI] <meji@odoo.com>
2025-01-28 11:57:23 +0530

    [FIX] l10n_in_pos: correctly fetch company from backend
    Before this commit:
    - If the company had no state set, attempting to close the POS session would
    trigger a company state dialog, redirecting to the company settings page.
    - After adding the state and reopening the session, the same dialog would appear
    again, despite the state being present.
    
    After this commit:
    - The system correctly fetches the company state from the backend, preventing
    unnecessary redirections.
    
    task-4517944
    
    closes odoo/odoo#195378
    
    Related: odoo/enterprise#78507
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/l10n_in_pos/static/src/app/models/payment_screen.js
+++ addons/l10n_in_pos/static/src/app/models/payment_screen.js
@@ -4,11 +4,12 @@ import { patch } from "@web/core/utils/patch";
 import { companyStateDialog } from "@l10n_in_pos/app/components/popups/company_state_dialog/company_state_dialog";
 
 patch(PaymentScreen.prototype, {
-    toggleIsToInvoice() {
+    async toggleIsToInvoice() {
+        await this.pos.data.read("res.company", [this.pos.company.id]);
         if (this.pos.company.country_id?.code === "IN" && !this.pos.company.state_id) {
             this.dialog.add(companyStateDialog);
             return;
         }
-        return super.toggleIsToInvoice();
+        return await super.toggleIsToInvoice();
     },
 });
