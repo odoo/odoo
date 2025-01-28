cd2b3425235fe433c94fae6f3c4678d146f11cbd
Claire (clbr) <clbr@odoo.com>
2025-01-28 09:33:25 +0000

    [IMP] account{,_edi_ubl_cii,_peppol}: Misc improvements
    Miscellanous improvements to the Print & Send and to Peppol onboarding:
    
    - Nicer translation, warning messages.
    - Let the bank account be editable when the invoice is posted.
    Only make it readonly when the invoice is sent.
    - Some UX change of the EAS fields.
    - Print & Send: check "email" by default, even when no email is set.
    Remove all warnings when no email is set.
    This will be improved in the generic feature in the future.
    - Print & Send: Rename "Recipients" to "To". The field is now required.
    - Add a "Print" button to invoice form so that it prints the PDF.
    - Update the EAS mapping per country.
    - EAS renaming to be more user-friendly.
    - Add a Print button to quickly preview the PDF.
    !this does not trigger a Print & Send wizard => no declaration to government.
    - Rename Print & Send to "Send" as we did in 18.1+ already.
    
    task-4478365
    
    closes odoo/odoo#196197
    
    X-original-commit: 06d4d0afe0bb5db3900c4ee5afcd6ad9c7a053ce
    Related: odoo/enterprise#78390
    Signed-off-by: Laurent Smet (las) <las@odoo.com>
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

--- addons/account/models/account_move_send.py
+++ addons/account/models/account_move_send.py
@@ -100,9 +100,11 @@ class AccountMoveSend(models.AbstractModel):
         - action the action to run when the link is clicked
         """
         alerts = {}
-        if partners_without_mail := moves.filtered(lambda m: 'email' in moves_data[m]['sending_methods'] and not m.partner_id.email).partner_id:
+        if len(moves) > 1 and (partners_without_mail := moves.filtered(
+                lambda m: 'email' in moves_data[m]['sending_methods'] and not m.partner_id.email).partner_id
+        ):
             alerts['account_missing_email'] = {
-                'level': 'danger' if len(moves) == 1 else 'warning',
+                'level': 'warning',
                 'message': _("Partner(s) should have an email address."),
                 'action_text': _("View Partner(s)"),
                 'action': partners_without_mail._get_records_action(name=_("Check Partner(s) Email(s)")),
@@ -177,7 +179,7 @@ class AccountMoveSend(models.AbstractModel):
             partner_to = self._get_mail_default_field_value_from_template(mail_template, mail_lang, move, 'partner_to')
             partner_ids = mail_template._parse_partner_to(partner_to)
             partners |= self.env['res.partner'].sudo().browse(partner_ids).exists()
-        return partners
+        return partners.filtered('email')
 
     # -------------------------------------------------------------------------
     # ATTACHMENTS
@@ -272,9 +274,9 @@ class AccountMoveSend(models.AbstractModel):
     @api.model
     def _check_move_constrains(self, moves):
         if any(move.state != 'posted' for move in moves):
-            raise UserError(_("You can't Print & Send invoices that are not posted."))
+            raise UserError(_("You can't generate invoices that are not posted."))
         if any(not move.is_sale_document(include_receipts=True) for move in moves):
-            raise UserError(_("You can only Print & Send sales documents."))
+            raise UserError(_("You can only generate sales documents."))
 
     @api.model
     def _check_invoice_report(self, moves, **custom_settings):
@@ -326,9 +328,6 @@ class AccountMoveSend(models.AbstractModel):
     @api.model
     def _is_applicable_to_move(self, method, move):
         """ TO OVERRIDE - """
-        if method == 'email':
-            return bool(move.partner_id.email)
-
         return True
 
     @api.model
--- addons/account_peppol/models/account_move_send.py
+++ addons/account_peppol/models/account_move_send.py
@@ -67,6 +67,7 @@ class AccountMoveSend(models.AbstractModel):
                         'target': 'new',
                         'context': {
                             'footer': False,
+                            'dialog_size': 'medium',
                             'action_on_activate': self.action_what_is_peppol_activate(moves),
                         },
                     },
