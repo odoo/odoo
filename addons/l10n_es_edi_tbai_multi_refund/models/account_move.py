4d29b6eaf348db955175c072b68aa5e810298a79
Jairo Llopis <yajo.sk8@gmail.com>
2025-01-28 11:02:51 +0000

    [FIX] l10n_es_edi_tbai_multi_refund: enable selecting refunded vendor bills
    When trying to select refunded vendor bills with this module, only customer invoices were searchable.
    
    It turns out that the web client has a bug. When there's a field defined twice in the same form, to make it conditionally visible with different properties, and each of the field instances has a different domain, only the last one will be used always.
    
    I couldn't find the fix for that bug, but with this fix I can avoid defining the field twice:
    - The field name and help doesn't involve anything specific to customer or vendor invoices.
    - The domain itself contains the condition.
    
    Now both customer and vendor refunds allow selecting their corresponding invoices before posting. This makes the feature from https://github.com/odoo/odoo/pull/194130 actually usable.
    
    @moduon MT-8778 OPW-4509823
    
    closes odoo/odoo#195418
    
    Signed-off-by: Ricardo Gomes Rodrigues (rigr) <rigr@odoo.com>

--- addons/l10n_es_edi_tbai_multi_refund/models/account_move.py
+++ addons/l10n_es_edi_tbai_multi_refund/models/account_move.py
@@ -6,7 +6,7 @@ class AccountMove(models.Model):
 
     l10n_es_tbai_reversed_ids = fields.Many2many(
         'account.move', 'account_move_tbai_reversed_moves', 'refund_id', 'reversed_move_id',
-        string="Refunded Vendor Bills",
-        domain="[('move_type', '=', 'in_invoice'), ('commercial_partner_id', '=', commercial_partner_id)]",
-        help="In the case where a vendor refund has multiple original invoices, you can set them here. ",
+        string="Refunded Invoices",
+        domain="[('move_type', '=', 'in_invoice' if move_type == 'in_refund' else 'out_invoice'), ('commercial_partner_id', '=', commercial_partner_id)]",
+        help="In the case where a refund has multiple original invoices, you can set them here. ",
     )
