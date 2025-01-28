9520bc5845cd2de6082bb9b66082e92599d3d7e5
Stanislas Sobieski <sts@odoo.com>
2025-01-28 11:06:48 +0000

    [IMP] core: allow to fail fast test suite
    Allow to limit to max failures or errors in odoo test suite
    when environment variable ODOO_TEST_MAX_FAILED_TESTS is set.
    Above that limit, skip all following tests
    
    ODOO_TEST_MAX_FAILED_TESTS must be an int
    
    closes odoo/odoo#201956
    
    X-original-commit: 34ece0111b17deb662fbaf3f35a8092d498e20bb
    Signed-off-by: Xavier Dollé (xdo) <xdo@odoo.com>
    Signed-off-by: Stanislas Sobieski (sts) <sts@odoo.com>

--- odoo/modules/loading.py
+++ odoo/modules/loading.py
@@ -287,7 +287,7 @@ def load_module_graph(
                 registry.check_null_constraints(env.cr)
                 # Python tests
                 tests_t0, tests_q0 = time.time(), odoo.sql_db.sql_counter
-                test_results = loader.run_suite(suite)
+                test_results = loader.run_suite(suite, global_report=report)
                 assert report is not None, "Missing report during tests"
                 report.update(test_results)
                 test_time = time.time() - tests_t0
