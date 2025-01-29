421f8be5505d19d25f5d0efddbd771009be72d6f
Lucas Lefèvre (lul) <lul@odoo.com>
2025-01-29 10:19:40 +0100

    [IMP] spreadsheet_dashboard: add tooltip to clickable cells
    In dashboard mode, you can click on some cells (list formulas, pivot formulas)
    and they have different effects: open the form view, or open the list view of
    records, or use the value as filter. But it's virtually impossible to know
    what it's gonna do before clicking.
    
    This task adds the possibility to add tooltips when hovering a clickable cell
    to hint what it's gonna do
    
    Task: 4526010

--- addons/spreadsheet_dashboard/static/src/bundle/list/clickable_cell.js
+++ addons/spreadsheet_dashboard/static/src/bundle/list/clickable_cell.js
@@ -1,5 +1,6 @@
 import { SEE_RECORD_LIST, SEE_RECORD_LIST_VISIBLE } from "@spreadsheet/list/list_actions";
 import * as spreadsheet from "@odoo/o-spreadsheet";
+import { _t } from "@web/core/l10n/translation";
 
 const { clickableCellRegistry } = spreadsheet.registries;
 
@@ -7,4 +8,5 @@ clickableCellRegistry.add("list", {
     condition: SEE_RECORD_LIST_VISIBLE,
     execute: SEE_RECORD_LIST,
     sequence: 10,
+    title: _t("Open record"),
 });
