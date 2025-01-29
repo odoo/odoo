b37ea5b31f03099fd7c72aa8eed5cc087c7c604c
Abdelrahman Mahmoud (amah) <amah@odoo.com>
2025-01-29 10:17:32 +0100

    [IMP] hr_hourly_cost: change hourly_cost precision
    - As LU law state a 4 digit hourly wage, the
    `hourly_cost` must be modified into float and
    have a decimal accurecy to able to adapt
    to anyother regulations
    
    Task: 3382221

--- addons/hr_hourly_cost/models/hr_employee.py
+++ addons/hr_hourly_cost/models/hr_employee.py
@@ -6,5 +6,5 @@ from odoo import fields, models
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    hourly_cost = fields.Monetary('Hourly Cost', currency_field='currency_id',
+    hourly_cost = fields.Float(string='Hourly Cost', digits='Hourly Cost',
         groups="hr.group_hr_user", default=0.0, tracking=True)
