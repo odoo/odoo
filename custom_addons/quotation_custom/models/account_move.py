3708e8da63f76faa4adb01ec59eb23b35a7ca4bb
nomadmotion <lukmanhajiarofah@gmail.com>
2025-01-30 02:25:22 +0700

    [ADD] quotation_custom : function  in account_move.py to give exception to action_confirm in sale_order

--- custom_addons/quotation_custom/models/account_move.py
+++ custom_addons/quotation_custom/models/account_move.py
@@ -40,3 +40,23 @@ _logger = logging.getLogger(__name__)
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
+
+    def _build_credit_exception_notification(self, record, current_amount=0.0, exclude_current=False, exclude_amount=0.0):
+        """ Build the UserError message that will be displayed in a pop up on the Quotation form.
+            if the partner exceeds a credit limit (set on the company or the partner itself).
+            :param record:                  The record where the warning will appear (Invoice, Sales Order...).
+            :param current_amount (float):  The partner's outstanding credit amount from the current document.
+            :param exclude_current (bool):  DEPRECATED in favor of parameter `exclude_amount`:
+                                            Whether to exclude `current_amount` from the credit to invoice.
+            :param exclude_amount (float):  The amount to subtract from the partner's `credit_to_invoice`.
+                                            Consider the warning on a draft invoice created from a sales order.
+                                            After confirming the invoice the (partial) amount (on the invoice)
+                                            stemming from sales orders will be substracted from the `credit_to_invoice`.
+                                            This will reduce the total credit of the partner.
+                                            This parameter is used to reflect this amount.
+            :return (str):                  The UserError message to be showed.
+        """
+        warning_message = super()._build_credit_warning_message(record, current_amount, exclude_current, exclude_amount)
+
+        if warning_message:
+            raise UserError(warning_message + '\n' + _('You can not confirm this quotation.'))
