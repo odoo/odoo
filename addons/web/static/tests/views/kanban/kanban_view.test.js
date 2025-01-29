c8f74852e6858366a3b15dbbcec6be5d52ca7f6b
Julien Carion (juca) <juca@odoo.com>
2025-01-29 09:22:27 +0100

    [IMP] web: hide default_group_by facet in kanban
    Since https://github.com/odoo/odoo/pull/179887, the default_group_by
    is displayed inside the facets of the search bar. This feature has been
    deemed useless in the case of the kanban view since removing groupbys
    in this view doesn't make much sense functionally.
    
    task-4489226
    
    closes odoo/odoo#195545
    
    Signed-off-by: Mathieu Duckerts-Antoine (dam) <dam@odoo.com>

--- addons/web/static/tests/views/kanban/kanban_view.test.js
+++ addons/web/static/tests/views/kanban/kanban_view.test.js
@@ -5122,7 +5122,7 @@ test("prevent drag and drop of record if save fails", async () => {
 });
 
 test("kanban view with default_group_by", async () => {
-    expect.assertions(13);
+    expect.assertions(11);
 
     Partner._records[0].product_id = 1;
     Product._records.push({ id: 1, display_name: "third product" });
@@ -5162,8 +5162,7 @@ test("kanban view with default_group_by", async () => {
     if (queryAll(".o_control_panel_navigation > button").length) {
         await contains(".o_control_panel_navigation > button").click();
     }
-    expect(`.o_searchview_facet`).toHaveCount(1);
-    expect(`.o_searchview_facet`).toHaveText("Bar");
+    expect(`.o_searchview_facet`).toHaveCount(0);
 
     // simulate an update coming from the searchview, with another groupby given
     await toggleSearchBarMenu();
@@ -5175,8 +5174,7 @@ test("kanban view with default_group_by", async () => {
     // simulate an update coming from the searchview, removing the previously set groupby
     await contains(".o_searchview_facet .o_facet_remove").click();
     expect(".o_kanban_group").toHaveCount(2);
-    expect(`.o_searchview_facet`).toHaveCount(1);
-    expect(`.o_searchview_facet`).toHaveText("Bar");
+    expect(`.o_searchview_facet`).toHaveCount(0);
 });
 
 test.tags("desktop");
