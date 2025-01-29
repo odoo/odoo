219de7a8859fbbf3a73da5f2d06c78090049809b
FrancoisGe <fge@odoo.com>
2025-01-29 13:40:31 +0100

    add shadowOption (Box before)

--- addons/html_builder/static/src/builder/builder_components/global_builder_options.js
+++ addons/html_builder/static/src/builder/builder_components/global_builder_options.js
@@ -1,5 +1,7 @@
 import { BorderConfigurator } from "@html_builder/builder/options/border_configurator";
+import { ShadowOption } from "../options/shadow_option";
 
 export const globalBuilderOptions = {
     BorderConfigurator,
+    ShadowOption,
 };
