f2eac95e27bfe77948d2e9e8ff3f194ac04b0411
aamo-odoo <aamo@odoo.com>
2025-01-30 07:13:28 +0000

    [FIX] hr_attendance: new user can load demo data
    Currently, an error occurs when a new user tries to load demo data in the
    'Attendance' module.
    
    Steps to reproduce:
    - Install 'hr_attendance' module (without demo data)
    - Create NEW user > Login with new user
    - Open 'Attendance' and Click 'Load Demo Data'
    
    Traceback:
    AccessError
    You are not allowed to create 'Resource Working Time' (resource.calendar) records.
    
    This operation is allowed for the following groups:
    	- Administration/Settings
    
    Contact your administrator to request access if necessary.
    
    ParseError
    while parsing /home/odoo/src/odoo/saas-18.1/addons/hr_attendance/data/scenarios/hr_attendance_scenario.xml:5, somewhere inside
    <record id="resource_calendar_std_38h" model="resource.calendar" forcecreate="1">
                <field name="name">Standard 32 hours/week (4 work days, friday free)</field>
                <field name="company_id" eval="False"/>
                <field name="hours_per_day">8</field>
                <field name="attendance_ids" eval="[(5, 0, 0),                     (0, 0, {'name': 'Monday Morning', 'dayofweek': '0', 'hour_from': 8, 'hour_to': 12, 'day_period': 'morning'}),                     (0, 0, {'name': 'Monday Lunch', 'dayofweek': '0', 'hour_from': 12, 'hour_to': 13, 'day_period': 'lunch'}),                     (0, 0, {'name': 'Monday Afternoon', 'dayofweek': '0', 'hour_from': 13, 'hour_to': 17, 'day_period': 'afternoon'}),                     (0, 0, {'name': 'Tuesday Morning', 'dayofweek': '1', 'hour_from': 8, 'hour_to': 12, 'day_period': 'morning'}),                     (0, 0, {'name': 'Tu...
    
    ValueError
    ParseError('while parsing /home/odoo/src/odoo/saas-18.1/addons/hr_attendance/data/scenarios/hr_attendance_scenario.xml:5, somewhere inside\n<record id="resource_calendar_std_38h" model="resource.calendar" forcecreate="1">\n            <field name="name">Standard 32 hours/week (4 work days, friday free)</field>\n            <field name="company_id" eval="False"/>\n            <field name="hours_per_day">8</field>\n            <field name="attendance_ids" eval="[(5, 0, 0),                     (0, 0, {\'name\': \'Monday Morning\', \'dayofweek\': \'0\', \'hour_from\': 8, \'hour_to\': 12, \'day_period\': \'morning\'}),                     (0, 0, {\'name\': \'Monday Lunch\', \'dayofweek\': \'0\', \'hour_from\': 12, \'hour_to\': 13, \'day_period\': \'lunch\'}),                     (0, 0, {\'name\': \'Monday Afternoon\', \'dayofweek\': \'0\', \'hour_from\': 13, \'hour_to\': 17, \'day_period\': \'afternoon\'}),                     (0, 0, {\'name\': \'Tuesday Morning\', \'dayofweek\': \'1\', \'hour_from\': 8, \'hour...
    
    This error occurs because the new user has not been granted administrative
    rights.
    
    This commit resolves the issue by granting the user superuser rights.
    
    sentry-6110523247
    
    closes odoo/odoo#228790
    
    X-original-commit: be767b8ecc87884cab43072abe3211ce9382c988
    Related: odoo/enterprise#95622
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>
    Signed-off-by: Aayush Modi (aamo) <aamo@odoo.com>

--- addons/hr_attendance/models/hr_attendance.py
+++ addons/hr_attendance/models/hr_attendance.py
@@ -413,9 +413,10 @@ class HrAttendance(models.Model):
     def _load_demo_data(self):
         if self.has_demo_data():
             return
-        self.env['hr.employee']._load_scenario()
+        env_sudo = self.sudo().with_context({}).env
+        env_sudo['hr.employee']._load_scenario()
         # Load employees, schedules, departments and partners
-        convert.convert_file(self.env, 'hr_attendance', 'data/scenarios/hr_attendance_scenario.xml', None, mode='init')
+        convert.convert_file(env_sudo, 'hr_attendance', 'data/scenarios/hr_attendance_scenario.xml', None, mode='init')
 
         employee_sj = self.env.ref('hr.employee_sj')
         employee_mw = self.env.ref('hr.employee_mw')
