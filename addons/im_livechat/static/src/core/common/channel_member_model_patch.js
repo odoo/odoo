986f5f4006ebe7bfbbde8fe8d721f1af65699e76
Didier (did) <did@odoo.com>
2025-01-30 11:08:52 +0000

    [FIX] im_livechat: avoid traceback when deleting a livechat channel
    Task-4504442
    
    closes odoo/odoo#195879
    
    X-original-commit: 19ac690b96117982b91a1245e69133b555e6c8da
    Signed-off-by: Didier Debondt (did) <did@odoo.com>

--- addons/im_livechat/static/src/core/common/channel_member_model_patch.js
+++ addons/im_livechat/static/src/core/common/channel_member_model_patch.js
@@ -4,10 +4,10 @@ import { patch } from "@web/core/utils/patch";
 /** @type {import("models").ChannelMember} */
 const channelMemberPatch = {
     get name() {
-        if (this.thread.channel_type === "livechat" && this.persona.user_livechat_username) {
+        if (this.thread?.channel_type === "livechat" && this.persona.user_livechat_username) {
             return this.persona.user_livechat_username;
         }
-        if (this.persona.is_public) {
+        if (this.thread && this.persona.is_public) {
             return this.thread.anonymous_name;
         }
         return super.name;
