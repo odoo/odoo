e0bc2472acea2f18a618102dfdc7e9e84d94dc96
Alexandre Kühn <aku@odoo.com>
2025-01-30 14:38:28 +0100

    [IMP] mail: quick reaction opens full picker on char keypress
    "Add reaction" message action shows the quick reaction menu, which
    shows a nice listing of the most frequently used emoji.
    
    Sometimes the desired emoji is not there, and it's clunky to click on
    "+" and then search.
    
    This commit intercepts keydown that are not used for naviation in
    the quick reaction menu, so that it automatically opens the full
    emoji picker. That way, we can quickly search for the desired emoji
    for the message reaction

--- addons/web/static/src/core/emoji_picker/emoji_picker.js
+++ addons/web/static/src/core/emoji_picker/emoji_picker.js
@@ -92,7 +92,7 @@ export const PICKER_PROPS = [
 ];
 
 export class EmojiPicker extends Component {
-    static props = [...PICKER_PROPS, "class?"];
+    static props = [...PICKER_PROPS, "class?", "initialSearchTerm?"];
     static template = "web.EmojiPicker";
 
     categories = null;
@@ -110,7 +110,7 @@ export class EmojiPicker extends Component {
         this.state = useState({
             activeEmojiIndex: 0,
             categoryId: null,
-            searchTerm: "",
+            searchTerm: this.props.initialSearchTerm ?? "",
             /** @type {Emoji|undefined} */
             hoveredEmoji: undefined,
         });
@@ -580,7 +580,7 @@ export function usePicker(PickerComponent, ref, props, options = {}) {
             }
             return def;
         }
-        return popover.open(ref.el, props);
+        return popover.open(ref.el, { ...props, ...openProps });
     }
 
     function close() {
