a23704e2f10c96d48d10af02d0cf902290f32876
FrancoisGe <fge@odoo.com>
2025-01-30 16:08:14 +0100

    small rem

--- addons/html_builder/static/src/options/border_option.js
+++ addons/html_builder/static/src/options/border_option.js
@@ -12,7 +12,6 @@ class BorderOptionPlugin extends Plugin {
                 template: "html_builder.BorderOption",
                 selector: "section .row > div",
                 exclude: `.s_col_no_bgcolor, .s_col_no_bgcolor.row > div, .s_image_gallery .row > div, .s_masonry_block .s_col_no_resize, .s_text_cover .row > .o_not_editable, ${card_parent_handlers}`,
-                // TODO add border shadow.
             },
         ],
     };
