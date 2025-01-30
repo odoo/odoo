63a294775a6aab6ebf08c36c17b5a3c910603408
Achraf (abz) <abz@odoo.com>
2025-01-30 10:14:20 +0000

    [FIX] web: Prevent users from using json field in the field_selector
    Steps:
        - Install `purchase` or any other model with analytic_mixin
        - Install `analytic`
        - Install `web_studio`
        - Enable analytic in configuration
        - Add a new approval on 'button_confirm' in `purchase.order`
        - Set a domain with analytic_distribution e.g
        -   - e.g. ("order_line.analytic_distribution", "=", "Administrative")
        - Click on Refresh button
        - The refresh button preview shows x records
        - Create a new purchase.order
        - Add analytic on optional column
        - Add a new product with analytic_distribution Administrative
        - Try to use `button_confirm`
        - Approval is not working while the refresh shows record is available
    
    The json fields, in particular the `analytic_distribution` fields,
    are not designed to be searched via a `filtered_domain`
    (which is what studio approval uses to check whether or not a button can
    be used by the current user), this is currently a technical limitation.
    
    To avoid any future problems, this commit prevents users from using a
    json field to search on them, since this is not supported.
    
    opw-4416646
    
    closes odoo/odoo#195916
    
    X-original-commit: 60f1269f3609294e6bc4ecef374378554de60c6e
    Signed-off-by: Mathieu Duckerts-Antoine (dam) <dam@odoo.com>
    Signed-off-by: Achraf Ben Azzouz (abz) <abz@odoo.com>

--- addons/web/static/src/core/model_field_selector/model_field_selector_popover.js
+++ addons/web/static/src/core/model_field_selector/model_field_selector_popover.js
@@ -38,7 +38,7 @@ class Page {
 
     get title() {
         const prefix = this.previousPage?.previousPage ? "... > " : "";
-        const title = this.previousPage?.selectedField.string || "";
+        const title = this.previousPage?.selectedField?.string || "";
         return `${prefix}${title}`;
     }
 
@@ -100,7 +100,7 @@ export class ModelFieldSelectorPopover extends Component {
         update: Function,
     };
     static defaultProps = {
-        filter: (fieldDef) => fieldDef.searchable,
+        filter: (value) => value.searchable && value.type != "json",
         isDebugMode: false,
         followRelations: true,
     };
