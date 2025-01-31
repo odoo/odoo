7c67ce75d9091b3a631a1bd42d38d88851d0d67e
paru-odoo <paru@odoo.com>
2025-01-31 15:09:21 +0530

    [FIX] website: retain image height when reordering in image gallery
    Steps to reproduce:
    1. Go to the website and enter edit mode.
    2. Drag and drop the image gallery.
    3. Select an image and change its position through reordering.
    
    Issue:
    You will notice that the image size becomes larger than its original
    height.
    
    Expected behavior:
    Images should retain their original height after reordering.
    
    Reason:
    During reordering, the image's original height is removed and replaced
    with a calculated height of 70% of window.innerHeight, causing a loss of
    its original dimensions.
    
    Solution:
    This commit resolves the issue by ensuring that images maintain their
    original height during and after reordering.
    
    task-4487638

--- addons/website/static/src/snippets/s_image_gallery/options.js
+++ addons/website/static/src/snippets/s_image_gallery/options.js
@@ -287,7 +287,6 @@ options.registry.gallery = options.Class.extend({
      */
     mode: function (previewMode, widgetValue, params) {
         widgetValue = widgetValue || 'slideshow'; // FIXME should not be needed
-        this.$target.css('height', '');
         this.$target
             .removeClass('o_nomode o_masonry o_grid o_slideshow')
             .addClass('o_' + widgetValue);
@@ -425,7 +424,6 @@ options.registry.gallery = options.Class.extend({
             }
             el.setAttribute("data-index", index);
         });
-        this.$target.css('height', Math.round(window.innerHeight * 0.7));
 
         // Apply layout animation
         this.$target.off('slide.bs.carousel').off('slid.bs.carousel');
