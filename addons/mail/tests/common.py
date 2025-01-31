2618b2c3c2e17b0c1adb9825ce50dda86bc90412
Thibault Delavallée <tde@odoo.com>
2025-01-31 08:18:55 +0000

    [IMP] test_mail: add tests for notification layout header/footer
    Tests related to odoo/odoo#190390 where we lessen display of header
    and footer in notification email layout but introduce context keys
    to enable backward compatible behavior.
    
    Task-4529263
    
    closes odoo/odoo#196038
    
    X-original-commit: 235d7bb4e67ef395f3844dae4971bf1aae4a0da3
    Signed-off-by: Olivier Dony (odo) <odo@odoo.com>
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mail/tests/common.py
+++ addons/mail/tests/common.py
@@ -1810,18 +1810,22 @@ class MailCommon(common.TransactionCase, MailCase):
         if not layout_arch_db:
             layout_arch_db = """
 <body>
+    <t t-set="show_header" t-value="email_notification_force_header or (
+        email_notification_allow_header and has_button_access)"/>
+    <t t-set="show_footer" t-value="email_notification_force_footer or (
+        email_notification_allow_footer and show_header and author_user and author_user._is_internal())"/>
     <p>English Layout for <t t-esc="model_description"/></p>
     <img t-att-src="'/logo.png?company=%s' % (company.id or 0)" t-att-alt="'%s' % company.name"/>
-    <a t-if="has_button_access" t-att-href="button_access['url']">
-        <t t-esc="button_access['title']"/>
-    </a>
-    <t t-if="actions">
-        <t t-foreach="actions" t-as="action">
+    <div t-if="show_header">HEADER
+        <a t-if="has_button_access" t-att-href="button_access['url']">
+            <t t-esc="button_access['title']"/>
+        </a>
+        <t t-if="actions" t-foreach="actions" t-as="action">
             <a t-att-href="action['url']">
                 <t t-esc="action['title']"/>
             </a>
         </t>
-    </t>
+    </div>
     <t t-out="message.body"/>
     <ul t-if="tracking_values">
         <li t-foreach="tracking_values" t-as="tracking">
@@ -1829,7 +1833,12 @@ class MailCommon(common.TransactionCase, MailCase):
         </li>
     </ul>
     <div t-if="signature" t-out="signature"/>
-    <p>Sent by <t t-esc="company.name"/></p>
+    <div t-if="show_footer">
+        <p>Sent by <t t-esc="company.name"/></p>
+        <span t-if="show_unfollow" id="mail_unfollow">
+            | <a href="/mail/unfollow" style="text-decoration:none; color:#555555;">Unfollow</a>
+        </span>
+    </div>
 </body>"""
         view = cls.env['ir.ui.view'].create({
             'arch_db': layout_arch_db,
