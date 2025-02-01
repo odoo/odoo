cdac25496ac5d265be8ab2bf72fb3a238c3f0954
Elier Ayala Bernal <elier@wclik.com>
2025-02-01 12:57:07 -0500

    [IMP] phone_validation: enhance phone number formatting in ResPartner
    These changes ensure consistent phone number formatting across the system, particularly beneficial for modules like website_sale where properly formatted phone numbers are crucial for order processing and communication.

--- addons/phone_validation/models/res_partner.py
+++ addons/phone_validation/models/res_partner.py
@@ -16,3 +16,27 @@ class ResPartner(models.Model):
     def _onchange_mobile_validation(self):
         if self.mobile:
             self.mobile = self._phone_format(fname='mobile', force_format='INTERNATIONAL') or self.mobile
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        partners = super(ResPartner, self).create(vals_list)
+        partners.mapped(lambda partner: partner._format_phone_numbers())
+        return partners
+
+    def _format_phone_numbers(self):
+        """
+         Format the mobile and phone numbers of the record to international format.
+
+         This method is used to ensure consistent phone number formatting across the system.
+         It's particularly useful in modules like `website_sale`, where properly formatted
+
+         The method attempts to format both mobile and phone numbers if they exist.
+         If formatting fails, it falls back to the original number.
+
+         Returns:
+         None. The method updates the mobile and phone fields in place.
+         """
+        if self.mobile:
+            self.mobile = self._phone_format(fname='mobile', country=self.country_id, force_format='INTERNATIONAL') or self.mobile
+        if self.phone:
+            self.phone = self._phone_format(fname='phone', country=self.country_id, force_format='INTERNATIONAL') or self.phone
