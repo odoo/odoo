fe9a830e365d397b33032ff62f87758b491f439f
Mélanie <mepe@odoo.com>
2025-02-03 15:07:14 +0100

    [IMP] hr_skills: avoid skill type without skill and level
    With this commit a skill type need to have at least one skill and one level.
    
    With this changes, the widget boolean_toggle_load needs to be changed
    because it was not possible anymore to save the record to retrieve
    the right default level's value.
    To solve this issue, a new field computed, no-readonly, no-stored is introduced.
    With this compute the frontend send to the backend the default level.
    
    task-4467338
    
    closes odoo/odoo#196236
    
    Related: odoo/enterprise#78491
    Related: odoo/upgrade#7165
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>
    Co-authored-by: FrancoisGe <fge@odoo.com>
    Co-authored-by: aab-odoo <aab@odoo.com>

--- addons/hr_skills/models/hr_skill_type.py
+++ addons/hr_skills/models/hr_skill_type.py
@@ -1,7 +1,9 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from random import randint
-from odoo import fields, models
+
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class HrSkillType(models.Model):
@@ -17,6 +19,26 @@ class HrSkillType(models.Model):
     skill_ids = fields.One2many('hr.skill', 'skill_type_id', string="Skills")
     skill_level_ids = fields.One2many('hr.skill.level', 'skill_type_id', string="Levels", copy=True)
     color = fields.Integer('Color', default=_get_default_color)
+
+    @api.constrains('skill_ids', 'skill_level_ids')
+    def _check_no_null_skill_or_skill_level(self):
+        incorrect_skill_type = self.env['hr.skill.type']
+        for skill_type in self:
+            if not skill_type.skill_ids or not skill_type.skill_level_ids:
+                incorrect_skill_type |= skill_type
+        if incorrect_skill_type:
+            raise ValidationError(
+                _("The following skills type must contain at least one skill and one level: %s",
+                  "\n".join(skill_type.name for skill_type in incorrect_skill_type)))
+
+    @api.onchange('skill_level_ids')
+    def _onchange_skill_level_ids(self):
+        for level in self.skill_level_ids:
+            if level.technical_is_new_default:
+                (self.skill_level_ids - level).write({'default_level': False})
+                # This value need to be set to False, to reset it for the frontend.
+                level.technical_is_new_default = False
+                break
 
     def copy_data(self, default=None):
         vals_list = super().copy_data(default=default)
--- addons/hr_skills/tests/test_ui.py
+++ addons/hr_skills/tests/test_ui.py
@@ -1,24 +1,30 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import odoo.tests
+from odoo.tests import tagged, Form, HttpCase
 
-@odoo.tests.tagged('-at_install', 'post_install')
-class SkillsTestUI(odoo.tests.HttpCase):
+@tagged('-at_install', 'post_install')
+class SkillsTestUI(HttpCase):
     def test_ui(self):
-        levels = self.env['hr.skill.level'].create([{
-            'name': f'Level {x}',
-            'level_progress': x * 10,
-        } for x in range(10)])
-        skill_type = self.env['hr.skill.type'].create({
-            'name': 'Best Music',
-            'skill_level_ids': levels.ids,
-        })
-        self.env['hr.skill'].create([{
-            'name': 'Fortunate Son',
-            'skill_type_id': skill_type.id,
-        }, {
-            'name': 'Oh Mary',
-            'skill_type_id': skill_type.id,
-        }])
+        with Form(self.env['hr.skill.type']) as skill_type:
+            skill_type.name = 'Best Music'
+            with skill_type.skill_ids.new() as skill:
+                skill.name = f'Fortunate Son'
+            with skill_type.skill_ids.new() as skill:
+                skill.name = f'Oh Mary'
+            for x in range(10):
+                with skill_type.skill_level_ids.new() as level:
+                    level.name = f"level {x}"
+                    level.level_progress = x * 10
+                    level.default_level = x % 2
+        skill_type = skill_type.save()
 
         self.start_tour("/odoo", 'hr_skills_tour', login='admin')
+
+    def test_ui2(self):
+
+        self.start_tour("/odoo", 'hr_skills_type_tour', login='admin')
+        skill_type_id = self.env['hr.skill.type'].search([('name', '=', 'Cooking Skill')]).id
+        self.assertTrue(self.env['hr.skill.level'].search([
+            ('default_level', '=', True),
+            ('skill_type_id', '=', skill_type_id)
+        ]).name, "Intermediate")
