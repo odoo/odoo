69fa253e8a2783112494e8dd391ed829a0d5f9e5
kcv-odoo <kcv@odoo.com>
2025-02-03 17:42:18 +0530

    [REV] l10n_br_website_sale: revert commit https://github.com/odoo/odoo/commit/12252c079fa2d65faebc77b2f4d961fb4209eb2d
    X-original-commit: https://github.com/odoo/odoo/commit/de9008cc64fff41ee99311f756f5b6e61728b207

--- addons/l10n_br_website_sale/controllers/main.py
+++ addons/l10n_br_website_sale/controllers/main.py
@@ -17,7 +17,7 @@ class L10nBRWebsiteSale(WebsiteSale):
         ):
             mandatory_fields.add('vat')
 
-        if 'vat' in mandatory_fields and not request.website._display_partner_b2b_fields():
+        if 'vat' in mandatory_fields:
             mandatory_fields.remove('vat')
 
         return mandatory_fields
