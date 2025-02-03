6a6bc2de9e862fd204420282fdcb6e1dcb1b2b30
Julien Banken <jbn@odoo.com>
2025-02-03 11:25:16 +0100

    [IMP] mail: restore recipient lists from mail composer wizard
    This commit ensures that the recipient list in the chatter is updated
    whenever the user modifies the recipients in the full mail composer
    and closes the wizard.
    
    Task-4496350

--- addons/mail/static/src/chatter/web/mail_composer_form.js
+++ addons/mail/static/src/chatter/web/mail_composer_form.js
@@ -16,6 +16,7 @@ export class MailComposerFormController extends formView.Controller {
 export class MailComposerFormRenderer extends formView.Renderer {
     setup() {
         super.setup();
+        this.orm = useService("orm");
         // Autofocus the visible editor in edition mode.
         this.root = useRef("compiled_view_root");
         useEffect((isInEdition, root) => {
@@ -32,6 +33,15 @@ export class MailComposerFormRenderer extends formView.Renderer {
             this.props.record.resId
         ]);
 
+        const getActiveMailThread = () => {
+            const resIds = JSON.parse(this.props.record.data.res_ids);
+            const thread = this.mailStore.Thread.insert({
+                model: this.props.record.data.model,
+                id: resIds[0],
+            });
+            return thread;
+        };
+
         // Add file dropzone on full mail composer:
         this.attachmentUploadService = useService("mail.attachment_upload");
         this.operations = useX2ManyCrud(() => {
@@ -41,14 +51,91 @@ export class MailComposerFormRenderer extends formView.Renderer {
         useCustomDropzone(this.root, MailAttachmentDropzone, {
             /** @param {Event} event */
             onDrop: async event => {
-                const resIds = JSON.parse(this.props.record.data.res_ids);
-                const thread = this.mailStore.Thread.insert({
-                    model: this.props.record.data.model,
-                    id: resIds[0],
-                });
+                const thread = getActiveMailThread();
                 for (const file of event.dataTransfer.files) {
                     const attachment = await this.attachmentUploadService.upload(thread, thread.composer, file);
                     await this.operations.saveRecord([attachment.id]);
+                }
+            }
+        });
+
+        /** @param {function} */
+        const onCloseWizardModal = (callback) => {
+            this.env.dialogData.dismiss = callback;
+        };
+
+        // Update the thread recipient lists:
+        onCloseWizardModal(async () => {
+            const thread = getActiveMailThread();
+            const selectedPartnerIds = this.props.record.data.partner_ids.records.map(
+                (partner) => partner.resId
+            );
+            const partners = await this.orm.searchRead(
+                "res.partner",
+                [["id", "in", selectedPartnerIds]],
+                ["email", "id", "lang", "name"]
+            );
+
+            // Update the recipient lists:
+            thread.suggestedRecipients = thread.suggestedRecipients.map((suggestedRecipient) => {
+                const partner = partners.find(
+                    (partner) =>
+                        partner.id === suggestedRecipient.id ||
+                        partner.email === suggestedRecipient.email
+                );
+                if (partner) {
+                    return {
+                        ...suggestedRecipient,
+                        email: partner.email,
+                        lang: partner.lang,
+                        name: partner.name,
+                        partner_id: partner.id,
+                        persona: { type: "partner", id: partner.id },
+                    };
+                }
+                return suggestedRecipient;
+            });
+            thread.additionalRecipients = thread.additionalRecipients.map((additionalRecipient) => {
+                const partner = partners.find(
+                    (partner) =>
+                        partner.id === additionalRecipient.id ||
+                        partner.email === additionalRecipient.email
+                );
+                if (partner) {
+                    return {
+                        ...additionalRecipient,
+                        email: partner.email,
+                        lang: partner.lang,
+                        name: partner.name,
+                        partner_id: partner.id,
+                        persona: { type: "partner", id: partner.id },
+                    };
+                }
+                return additionalRecipient;
+            });
+
+            // Remove the recipients that got removed from the composer:
+            thread.suggestedRecipients = thread.suggestedRecipients.filter((suggestedRecipient) =>
+                selectedPartnerIds.includes(suggestedRecipient.partner_id)
+            );
+            thread.additionalRecipients = thread.additionalRecipients.filter(
+                (additionalRecipient) => selectedPartnerIds.includes(additionalRecipient.partner_id)
+            );
+
+            // Add the recipients that got added to the composer:
+            for (const partner of partners) {
+                const allRecipients = [
+                    ...thread.suggestedRecipients,
+                    ...thread.additionalRecipients,
+                ];
+                if (allRecipients.every((recipient) => recipient.partner_id !== partner.id)) {
+                    thread.additionalRecipients.push({
+                        email: partner.email,
+                        lang: partner.lang,
+                        name: partner.name,
+                        partner_id: partner.id,
+                        persona: { type: "partner", id: partner.id },
+                    });
                 }
             }
         });
