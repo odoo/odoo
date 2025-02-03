b1ce16927e5296257b9d979a6e49799690b67dd7
Krzysztof Magusiak (krma) <krma@odoo.com>
2025-02-03 09:46:47 +0000

    [IMP] orm: check decorators in overrides
    When overwriting methods, most decorators are not automatically
    propagated. Most of the time this is on purpose, but sometimes we should
    have the same attribute set.
    
    The main example is @api.model: it is a kind of classmethod and an
    override should be as well.
    This prevents forgetting to add @api.model and break RPC calls.
    
    closes odoo/odoo#196923
    
    Related: odoo/enterprise#78798
    Signed-off-by: Rémy Voet (ryv) <ryv@odoo.com>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -3084,6 +3084,7 @@ class AccountMove(models.Model):
     # LOW-LEVEL METHODS
     # -------------------------------------------------------------------------
 
+    @api.model
     def check_field_access_rights(self, operation, field_names):
         result = super().check_field_access_rights(operation, field_names)
         if not field_names:
