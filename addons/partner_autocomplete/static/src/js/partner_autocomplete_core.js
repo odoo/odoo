2c3b8a557e7d37fb1f236570c029f2e286fd4ae2
Louis Baudoux <lba@odoo.com>
2025-02-04 16:34:00 +0000

    [FIX] partner_autocomplete: fix unreliability with Clearbit Autocomplete
    The Partner Autocomplete service was unreliable when used on a database
    hosted under a subdomain of odoo.com (i.e. from *.odoo.com).
    
    This was caused by a combination of the CORS policy of the Autocomplete
    API of Clearbit and the caching of the browser.
    
    If the user performed a first request with search query "Odoo" from
    https://a.odoo.com, the browser would cache the results, including the
    `Access-Control-Allow-Origin` header set to `https://a.odoo.com`.
    
    The same request performed from https://b.odoo.com would lead to a CORS
    policy error as the browser would use the cached response from earlier
    request.
    
    This commit addresses the issue by disabling the caching of the requests
    made to the Clearbit Autocomplete API.
    
    X-original-commit: a08b323134be951819f8e1fecccad5b14c5dea5f

--- addons/partner_autocomplete/static/src/js/partner_autocomplete_core.js
+++ addons/partner_autocomplete/static/src/js/partner_autocomplete_core.js
@@ -2,6 +2,7 @@
 
 import { loadJS } from "@web/core/assets";
 import { _t } from "@web/core/l10n/translation";
+import { browser } from "@web/core/browser/browser";
 import { KeepLast } from "@web/core/utils/concurrency";
 import { useService } from "@web/core/utils/hooks";
 import { renderToMarkup } from "@web/core/utils/render";
@@ -18,7 +19,6 @@ export function usePartnerAutocomplete() {
     const keepLastOdoo = new KeepLast();
     const keepLastClearbit = new KeepLast();
 
-    const http = useService("http");
     const notification = useService("notification");
     const orm = useService("orm");
 
@@ -250,7 +250,13 @@ export function usePartnerAutocomplete() {
      */
     async function getClearbitSuggestions(value) {
         const url = `https://autocomplete.clearbit.com/v1/companies/suggest?query=${value}`;
-        const prom = http.get(url);
+        const prom = browser.fetch(
+            url,
+            {
+                method: 'GET',
+                cache: 'no-cache',
+            }
+        ).then((response) => (response['json']()));
         return keepLastClearbit.add(prom);
     }
 
