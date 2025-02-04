odoo/odoo

commit 04f75728fd3e8516842c49f0fe49a8992b9c4ab4

[FIX] *: restore gettext and unlink lints
Unwittingly broken by the removal of `__implements__` in
a6d601dc4e1aea36a5a28914799c0309765ba13d, these lints don't run
correctly with the old pylint, allowing new errors to creep in since.

closes odoo/odoo#139605

X-original-commit: 99cec73f585c8759142a707b06934d9c23aa5478
Related: odoo/enterprise#49473
Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

addons/payment_adyen/models/payment_transaction.py
@@ -429,22 +429,17 @@ class PaymentTransaction(models.Model):
                     _("An error occurred during the processing of your payment. Please try again.")
                 )
             elif event_code == 'CANCELLATION':
-                failed_void_msg = "The void of the transaction with reference %s failed."
-                logger_msg = failed_void_msg + " reason: %s"
-                _logger.warning(logger_msg, self.reference, refusal_reason)
+                _logger.warning("The void of the transaction with reference %s failed. reason: %s", self.reference, refusal_reason)
                 if self.source_transaction_id:  # child tx => The event can't be retried.
-                    self._set_error(_(failed_void_msg, self.reference))
+                    self._set_error(_("The void of the transaction with reference %s failed.", self.reference))
                 else:  # source tx with failed void stays in its state, could be voided again
-                    self._log_message_on_linked_documents(_(failed_void_msg, self.reference))
+                    self._log_message_on_linked_documents(_("The void of the transaction with reference %s failed.", self.reference))
             else:  # 'CAPTURE', 'CAPTURE_FAILED'
-                failed_capture_msg = "The capture of the transaction with reference %s failed."
-                logger_msg = failed_capture_msg + " reason: %s"
-                _logger.warning(logger_msg, self.reference, refusal_reason)
+                _logger.warning("The capture of the transaction with reference %s failed. reason: %s", self.reference, refusal_reason)
                 if self.source_transaction_id:  # child_tx => The event can't be retried.
-                    self._set_error(_(failed_capture_msg, self.reference))
+                    self._set_error(_("The capture of the transaction with reference %s failed.", self.reference))
                 else:  # source tx with failed capture stays in its state, could be captured again
-                    self._log_message_on_linked_documents(_(failed_capture_msg, self.reference))
-        elif payment_state in const.RESULT_CODES_MAPPING['refused']:
+                    self._log_message_on_linked_documents(_("The capture of the transaction with reference %s failed.", self.reference))
             _logger.warning(
                 "the transaction with reference %s was refused. reason: %s",
                 self.reference, refusal_reason

addons/account/models/account_tax.py
@@ -1497,11 +1497,11 @@ class AccountTaxRepartitionLine(models.Model):
             raise ValidationError(_("The tax named %s has already been used, you cannot add nor delete its tax repartition lines.", tax.name))
         return super().create(vals)
 
-    def unlink(self):
+    @api.ondelete(at_uninstall=False)
+    def _check_tax_use(self):
         for repartition_line in self:
             if repartition_line.tax_id.is_used:
                 raise ValidationError(_("The tax named %s has already been used, you cannot add nor delete its tax repartition lines.", repartition_line.tax_id.name))
-        return super().unlink()
 
     @api.depends('company_id.multi_vat_foreign_country_ids', 'company_id.account_fiscal_country_id')
     def _compute_tag_ids_domain(self):

addons/account_qr_code_sepa/models/res_bank.py
@@ -47,7 +47,7 @@ class ResPartnerBank(models.Model):
             sepa_iban_codes = {code for code in sepa_country_codes if code not in non_iban_codes}
             error_messages = []
             if currency.name != 'EUR':
-                error_messages.append(_("Can't generate a SEPA QR Code with the %s currency." % currency.name))
+                error_messages.append(_("Can't generate a SEPA QR Code with the %s currency.", currency.name))
             if self.acc_type != 'iban':
                 error_messages.append(_("Can't generate a SEPA QR code if the account type isn't IBAN."))
             if not (self.sanitized_acc_number and self.sanitized_acc_number[:2] in sepa_iban_codes):

addons/event_booth_sale/models/sale_order.py
@@ -28,8 +28,8 @@ class SaleOrder(models.Model):
                 continue
             so_lines_missing_booth = so.order_line.filtered(lambda line: line.product_type == 'event_booth' and not line.event_booth_pending_ids)
             if so_lines_missing_booth:
-                so_lines_descriptions = "".join([f"- {so_line_description.name} \n" for so_line_description in so_lines_missing_booth])
-                raise ValidationError(_("Please make sure all your event-booth related lines are configured before confirming this order: \n%s" % so_lines_descriptions))
+                so_lines_descriptions = "".join(f"\n- {so_line_description.name}" for so_line_description in so_lines_missing_booth)
+                raise ValidationError(_("Please make sure all your event-booth related lines are configured before confirming this order:%s", so_lines_descriptions))
             so.order_line._update_event_booths()
         return res
 

addons/event_sale/models/sale_order.py
@@ -27,8 +27,8 @@ class SaleOrder(models.Model):
                 continue
             so_lines_missing_events = so.order_line.filtered(lambda line: line.product_type == 'event' and not line.event_id)
             if so_lines_missing_events:
-                so_lines_descriptions = "".join([f"- {so_line_description.name} \n" for so_line_description in so_lines_missing_events])
-                raise ValidationError(_("Please make sure all your event related lines are configured before confirming this order: \n%s" % so_lines_descriptions))
+                so_lines_descriptions = "".join(f"\n- {so_line_description.name}" for so_line_description in so_lines_missing_events)
+                raise ValidationError(_("Please make sure all your event related lines are configured before confirming this order:%s", so_lines_descriptions))
             # Initialize registrations
             so.order_line._init_registrations()
             if len(self) == 1:

addons/hr_expense/models/account_move.py
@@ -1,6 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import models, fields, api, _
+from odoo.api import ondelete
 from odoo.exceptions import UserError
 from odoo.tools.misc import frozendict
 
@@ -58,8 +59,7 @@ class AccountMove(models.Model):
         # else, when restarting the expense flow we get duplicate issue on vendor.bill
         return super()._reverse_moves(default_values_list=default_values_list, cancel=cancel)
 
-    def unlink(self):
-        # EXTENDS account
+    @ondelete(at_uninstall=True)
+    def _must_delete_all_expense_entries(self):
         if self.expense_sheet_id and self.expense_sheet_id.account_move_ids - self:  # If not all the payments are to be deleted
             raise UserError(_("You cannot delete only some entries linked to an expense report. All entries must be deleted at the same time."))
-        return super().unlink()

addons/hr_expense/models/account_payment.py
@@ -1,6 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import models, _
+from odoo.api import ondelete
 from odoo.exceptions import UserError
 
 
@@ -41,8 +42,7 @@ class AccountPayment(models.Model):
             return _("Payment created for: %s", self.move_id.expense_sheet_id._get_html_link())
         return super()._creation_message()
 
-    def unlink(self):
-        # EXTENDS account
+    @ondelete(at_uninstall=True)
+    def _must_delete_all_expense_payments(self):
         if self.expense_sheet_id and self.expense_sheet_id.account_move_ids.payment_ids - self:  # If not all the payments are to be deleted
             raise UserError(_("You cannot delete only some payments linked to an expense report. All payments must be deleted at the same time."))
-        return super().unlink()

addons/hr_holidays/models/hr_leave.py
@@ -710,24 +710,27 @@ class HolidaysRequest(models.Model):
                 conflicting_holidays_strings = []
                 if holidays_only_have_uid:
                     for conflicting_holiday_data in conflicting_holidays_list:
-                        conflicting_holidays_string = _('From %(date_from)s To %(date_to)s - %(state)s',
+                        conflicting_holidays_string = _('from %(date_from)s to %(date_to)s - %(state)s',
                                                         date_from=conflicting_holiday_data['date_from'],
                                                         date_to=conflicting_holiday_data['date_to'],
                                                         state=conflicting_holiday_data['state'])
                         conflicting_holidays_strings.append(conflicting_holidays_string)
-                    raise ValidationError(_("You’ve already booked time off which overlaps with this period:" +
-                        "\n".join(conflicting_holidays_strings) +
-                        "Attempting to double-book your time off won't magically make your vacation 2x better!"))
+                    raise ValidationError(_("""\
+You've already booked time off which overlaps with this period:
+%s
+Attempting to double-book your time off won't magically make your vacation 2x better!
+""",
+                        "\n".join(conflicting_holidays_strings)))
                 for conflicting_holiday_data in conflicting_holidays_list:
-                    conflicting_holidays_string = _('%(employee_name)s - From %(date_from)s To %(date_to)s - %(state)s',
+                    conflicting_holidays_string = "\n" + _('%(employee_name)s - from %(date_from)s to %(date_to)s - %(state)s',
                                                     employee_name=conflicting_holiday_data['employee_name'],
                                                     date_from=conflicting_holiday_data['date_from'],
                                                     date_to=conflicting_holiday_data['date_to'],
                                                     state=conflicting_holiday_data['state'])
                     conflicting_holidays_strings.append(conflicting_holidays_string)
-                conflicting_employees = set(employee_ids) - set(conflicting_holidays.employee_id.ids)
-                raise ValidationError(_("An employee already booked time off which overlaps with this period:" +
-                    "\n".join(conflicting_holidays_strings)))
+                raise ValidationError(_(
+                    "An employee already booked time off which overlaps with this period:%s",
+                    "".join(conflicting_holidays_strings)))
 
     @api.constrains('date_from', 'date_to', 'employee_id')
     def _check_date_state(self):

commit 3204b419169ed098f3412b95b1fa54543226c2d3

[FIX] payment_adyen: log refusal reason for refused payments
Versions
--------
- 17.0+

Steps
-----
1. Enable Adyen as payment provider in test mode;
2. do an eCommerce checkout;
3. select Adyen as payment option;
4. use the following card details:
  - Number: 4111111111111111
  - expiry: 03/30
  - cvc: 737
  - name on card: CARD_EXPIRED
5. click Pay Now;
6. check order chatter on the backend.

Issue
-----
> Error: Adyen: Received data with invalid payment state: Refused

Refusal shouldn't be an invalid payment state.

Cause
-----
Commit 04f75728fd3e accidentally removed an `elif` branch that was added
by commit 4d340a330462e specifically to handle refused payments.

Solution
--------
Re-add the `elif` branch.

opw-4481602

addons/payment_adyen/models/payment_transaction.py
@@ -460,6 +460,7 @@ class PaymentTransaction(models.Model):
                     self._log_message_on_linked_documents(_(
                         "The capture of the transaction with reference %s failed.", self.reference
                     ))
+        elif payment_state in const.RESULT_CODES_MAPPING['refused']:
             _logger.warning(
                 "the transaction with reference %s was refused. reason: %s",
                 self.reference, refusal_reason