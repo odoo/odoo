0507623c5d746c2a160edb595e8eb1e1417ff0d2
abd-msyukyu-odoo <abd@odoo.com>
2025-02-04 14:38:02 +0100

    [FIX] html_editor: convert xml self-closing elements to html
    HTML content is often saved as XML (i.e. through templates) and saved as such in
    the database. This introduces issues when elements are written under their
    `self-closing` format (i.e. `<a/>` or `<br/>`) as browsers will incorrectly
    parse these values as HTML by adding a closing tag at an arbitrary position
    which may modify the initial nodes configuration. This can be prevented by
    post-processing untrusted content before the html parsing.
    
    This commit is an overhaul of:
    https://github.com/odoo/odoo/commit/26b922ef5cad42da7e188195e919e54878d472fc
    https://github.com/odoo/odoo/commit/1a8a943d8373864165cb513b3767b866d626fad8
    https://github.com/odoo/odoo/commit/4e547b24a323f8e59a11c5b78cbea38effc37938
    
    in order to apply the conversion at critical entry points for editor assets:
    - HtmlField is an entry point for data coming from the server, stored on the
      record. Every access of the record data should not be trusted and go through
      the post-processing.
    
    - HtmlViewer is an entry point as it can be used as a standalone (see
      `website_knowledge`, or the HistoryDialog) for data coming from the server.
    
    - Editor is an entry point as it was developed to be useable as a standalone.
    
    task-4547973

--- addons/html_editor/static/src/utils/html.js
+++ addons/html_editor/static/src/utils/html.js
@@ -1,5 +1,3 @@
-import { fixInvalidHTML } from "./sanitize";
-
 /**
  * @param { Document } document
  * @param { string } html
@@ -29,7 +27,7 @@ export function parseHTML(document, html) {
  */
 export function normalizeHTML(content, cleanup = () => {}) {
     const parser = new document.defaultView.DOMParser();
-    const body = parser.parseFromString(fixInvalidHTML(content), "text/html").body;
+    const body = parser.parseFromString(content, "text/html").body;
     cleanup(body);
     return body.innerHTML;
 }
