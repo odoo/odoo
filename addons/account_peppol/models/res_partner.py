f8266821dc7613714d988ef4668b180c9e6873f8
Claire (clbr) <clbr@odoo.com>
2025-02-04 16:30:07 +0100

    [IMP] account_{edi_ubl_cii,peppol}: Stop setting automatically an invoice edi format
    In previous refactor[1], we set automatically an invoice EDI format.
    This decision was too zealous considering the overall state of e-invoicing
    and now cause more frictions than necessary.
    We therefore decided to remove the automatic computation of EDI format on
    partners.
    The only EDI format for now that will stay auto-computed is the Italian EDI XML.
    
    [1]: https://github.com/odoo/odoo/commit/9e769e1b11f22890e5245859053bc8dd31e42634
    
    task-4478365
    
    Part-of: odoo/odoo#196657
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- addons/account_peppol/models/res_partner.py
+++ addons/account_peppol/models/res_partner.py
@@ -203,7 +203,7 @@ class ResPartner(models.Model):
         self_partner.peppol_verification_state = self._get_peppol_verification_state(
             self.peppol_endpoint,
             self.peppol_eas,
-            self_partner.invoice_edi_format
+            self_partner._get_peppol_edi_format(),
         )
         if self_partner.peppol_verification_state == 'valid':
             self_partner.invoice_sending_method = 'peppol'
