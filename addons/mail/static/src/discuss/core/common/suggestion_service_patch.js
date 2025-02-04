f6e8c4bfd1bb16ecff4e2d7b81553e0f738705cd
jayp-odoo <jayp@odoo.com>
2025-02-04 23:44:32 +0530

    [FIX] mail,im_livechat: Hide live chat commands to visitors
    Before Commit:
    
    channel commands like `/help ` or `/leave` are visible to visitors even it is
    not functional for them.
    
    After Commit:
    
    commands are now hidden from visitors.
    
    task-4548666

--- addons/mail/static/src/discuss/core/common/suggestion_service_patch.js
+++ addons/mail/static/src/discuss/core/common/suggestion_service_patch.js
@@ -32,19 +32,24 @@ const suggestionServicePatch = {
                 if (!cleanTerm(name).includes(cleanedSearchTerm)) {
                     return false;
                 }
+                if (this.store.self.type !== "partner") {
+                    // or we can do it with isInternalUser
+                    return false;
+                }
+                // if (command.isAvailable) {
+                //     return command.isAvailable(this.store, thread);
+                // }
                 if (command.channel_types) {
                     return command.channel_types.includes(thread.channel_type);
                 }
                 return true;
             })
-            .map(([name, command]) => {
-                return {
-                    channel_types: command.channel_types,
-                    help: command.help,
-                    id: command.id,
-                    name,
-                };
-            });
+            .map(([name, command]) => ({
+                channel_types: command.channel_types,
+                help: command.help,
+                id: command.id,
+                name,
+            }));
         const sortFunc = (c1, c2) => {
             if (c1.channel_types && !c2.channel_types) {
                 return -1;
