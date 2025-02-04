a2a7c1f1caeb30f8e9de3c3a253a09bda6e8929c
jayp-odoo <jayp@odoo.com>
2025-02-04 18:14:32 +0000

    [FIX] mail,*: Hide live chat commands to visitors/guest
    *= im_livechat, crm_livechat
    
    Before Commit:
    
    channel commands like `/help ` or `/leave` are visible to visitors
    or guest even it is not functional for them.
    
    After Commit:
    
    all commands are now hidden from visitors/guests.
    
    task-4548666
    
    X-original-commit: acd51f03cf4c395e62c10ac39a488a880151af84

--- addons/crm_livechat/static/src/core/channel_commands.js
+++ addons/crm_livechat/static/src/core/channel_commands.js
@@ -2,6 +2,7 @@ import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
 
 registry.category("discuss.channel_commands").add("lead", {
+    condition: ({ store }) => store.has_access_livechat,
     help: _t("Create a new lead (/lead lead title)"),
     methodName: "execute_command_lead",
 });
