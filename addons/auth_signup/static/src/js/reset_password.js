618bc5886561685f579fe62f3ed7deb5730e387b
bram1000 <brvg@odoo.com>
2025-02-04 10:21:21 +0000

    [FIX] auth_signup: reset password spinner
    Currently signup.js also defines SignUpForm, which overrides the reset
    password behavior. This bug is not present in 16.0, so I used the same
    name for the registry that is being used in 16.0.
    
    X-original-commit: 731819e283f0c5cbab5e5183070af9ddda533561

---
+++ addons/auth_signup/static/src/js/reset_password.js
@@ -0,0 +1,28 @@
+<<<<<<< HEAD
+||||||| MERGE BASE
+=======
+/** @odoo-module **/
+
+import publicWidget from "@web/legacy/js/public/public_widget";
+
+publicWidget.registry.ResetPasswordForm = publicWidget.Widget.extend({
+    selector: '.oe_reset_password_form',
+    events: {
+        'submit': '_onSubmit',
+    },
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * @private
+     */
+    _onSubmit: function () {
+        var $btn = this.$('.oe_login_buttons > button[type="submit"]');
+        $btn.attr('disabled', 'disabled');
+        $btn.prepend('<i class="fa fa-refresh fa-spin"/> ');
+    },
+});
+
+>>>>>>> FORWARD PORTED
