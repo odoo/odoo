83c00efb0f6b14aa91f12ab3e8b113fbb0af9b80
Xavier-Do <xdo@odoo.com>
2025-02-05 12:20:32 +0100

    [IMP] profiler: standalone speedscope page
    Before this commit, the speedscope page will change is own hash to the
    url of the speedscope json profile.
    
    An aternative solution was found allowing to embed the speedscope
    output inside the page, leading to a self contained html result.
    
    This page can be easily downloaded and shared.
    
    A small change is also made to stop using the web/content page since for
    a cleaner result and preparation for the next commit.
    
    Part-of: odoo/odoo#189370
    Signed-off-by: Xavier Dollé (xdo) <xdo@odoo.com>

--- addons/web/controllers/profiling.py
+++ addons/web/controllers/profiling.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+import base64
 import json
 
 from odoo.exceptions import UserError
-from odoo.http import Controller, request, Response, route
-import urllib.parse
+from odoo.http import Controller, request, Response, route, content_disposition
+
 
 class Profiling(Controller):
 
@@ -23,24 +24,36 @@ class Profiling(Controller):
 
     @route([
         '/web/speedscope',
-        '/web/speedscope/<model("ir.profile"):profile>',
+        '/web/speedscope/<profile>',
     ], type='http', sitemap=False, auth='user', readonly=True)
     def speedscope(self, profile=None, action=False, **kwargs):
-        if profile and isinstance(profile, str):
-            profile = request.env['ir.profile'].browse(int(profile))
+        if not profile:
+            return
+        profile_str = profile
+        profile = request.env['ir.profile'].browse(int(profile_str))
         if not kwargs and not action:
             context = {
                 'profile': profile,
             }
             return request.render('web.config_speedscope_index', context)
+        speedscope_result = profile._generate_speedscope(profile._parse_params(kwargs))
+        if action == 'download_json':
+            headers = [
+                ('Content-Type', 'application/json'),
+                ('X-Content-Type-Options', 'nosniff'),
+                ('Content-Disposition', content_disposition(f'profile_{profile_str}.json')),
+            ]
+            return request.make_response(speedscope_result, headers)
         icp = request.env['ir.config_parameter']
         context = {
             'profile': profile,
+            'speedscope_base64': base64.b64encode(speedscope_result).decode('utf-8'),
             'url_root': request.httprequest.url_root,
             'cdn': icp.sudo().get_param('speedscope_cdn', "https://cdn.jsdelivr.net/npm/speedscope@1.13.0/dist/release/")
         }
-        if profile and action == 'download_json':
-            url_params = urllib.parse.urlencode(kwargs)
-            return request.redirect(f'/web/content/ir.profile/{profile.id}/speedscope?download=true&filename=profile_{profile.id}.json{"&" + url_params if url_params else ""}')
         response = request.render('web.view_speedscope_index', context)
+        if action == 'download_html':
+            response.headers['Content-Disposition'] = content_disposition(f'profile_{profile_str}.html')
+            response.headers['X-Content-Type-Options'] = 'nosniff'
+            response.headers['Content-Type'] = 'text/html'
         return response
