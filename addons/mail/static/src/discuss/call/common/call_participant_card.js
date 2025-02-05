333dbc5b4d9d58459d2714535a3c3da1f142744d
Rahul Prajapati <rapr@odoo.com>
2025-02-05 07:32:05 +0000

    [FIX] mail: call participant card drag-drop on small screen
    Before this commit:
    
    - Users could not drag and drop the call participant card in the chat window
      due to incorrect position computations.
    - Participant card was positioned at bottom right,
      causing the "End Call" button to be hidden behind it.
    
    This commit:
    
    - Fixes the drag-and-drop issue by correcting the position calculations.
    - Adds 5vh bottom offset for card so it doesn't overlap "End Call" button.
    
    closes odoo/odoo#199725
    
    X-original-commit: df7f06b8e98ff75b93d4ef9ec0102bf491c0d0ab
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/discuss/call/common/call_participant_card.js
+++ addons/mail/static/src/discuss/call/common/call_participant_card.js
@@ -6,14 +6,7 @@ import { isEventHandled, markEventHandled } from "@web/core/utils/misc";
 import { browser } from "@web/core/browser/browser";
 import { isMobileOS } from "@web/core/browser/feature_detection";
 
-import {
-    Component,
-    onMounted,
-    onWillUnmount,
-    useRef,
-    useState,
-    useExternalListener,
-} from "@odoo/owl";
+import { Component, onMounted, onWillUnmount, useRef, useExternalListener } from "@odoo/owl";
 import { usePopover } from "@web/core/popover/popover_hook";
 import { useService } from "@web/core/utils/hooks";
 import { rpc } from "@web/core/network/rpc";
@@ -38,7 +31,9 @@ export class CallParticipantCard extends Component {
         this.ui = useService("ui");
         this.rootHover = useHover("root");
         this.isMobileOS = isMobileOS();
-        this.state = useState({ drag: false, dragPos: undefined });
+        this.dragPos = undefined;
+        this.isDrag = false;
+        this.parentBoundingRect = undefined;
         onMounted(() => {
             if (!this.rtcSession) {
                 return;
@@ -164,8 +159,8 @@ export class CallParticipantCard extends Component {
         if (isEventHandled(ev, "CallParticipantCard.clickVolumeAnchor")) {
             return;
         }
-        if (this.state.drag) {
-            this.state.drag = false;
+        if (this.isDrag) {
+            this.isDrag = false;
             return;
         }
         if (this.rtcSession) {
@@ -218,6 +213,7 @@ export class CallParticipantCard extends Component {
         const onMousemove = (ev) => this.drag(ev);
         const onMouseup = () => {
             const insetEl = this.root.el;
+            const bottomOffset = this.env.inChatWindow ? window.innerHeight * 0.05 : 0; // 5vh in pixels
             if (parseInt(insetEl.style.left) < insetEl.parentNode.offsetWidth / 2) {
                 insetEl.style.left = "1vh";
                 insetEl.style.right = "";
@@ -225,13 +221,18 @@ export class CallParticipantCard extends Component {
                 insetEl.style.left = "";
                 insetEl.style.right = "1vh";
             }
-            if (parseInt(insetEl.style.top) < insetEl.parentNode.offsetHeight / 2) {
+            if (
+                parseInt(insetEl.style.top) <
+                (insetEl.parentNode.offsetHeight - bottomOffset) / 2
+            ) {
                 insetEl.style.top = "1vh";
                 insetEl.style.bottom = "";
             } else {
-                insetEl.style.bottom = "1vh";
-                insetEl.style.top = "";
+                insetEl.style.bottom = this.env.inChatWindow ? "5vh" : "1vh";
+                insetEl.style.top = "unset";
             }
+            this.dragPos = undefined;
+            this.parentBoundingRect = undefined;
             document.removeEventListener("mouseup", onMouseup);
             document.removeEventListener("mousemove", onMousemove);
         };
@@ -247,34 +248,25 @@ export class CallParticipantCard extends Component {
     }
 
     drag(ev) {
-        this.state.drag = true;
+        this.isDrag = true;
         const insetEl = this.root.el;
         const parent = insetEl.parentNode;
-        const clientX = ev.clientX ?? ev.touches[0].clientX;
-        const clientY = ev.clientY ?? ev.touches[0].clientY;
-        if (!this.state.dragPos) {
-            this.state.dragPos = { posX: clientX, posY: clientY };
+        const boundingRect =
+            this.parentBoundingRect || (this.parentBoundingRect = parent.getBoundingClientRect());
+        const bottomOffset = this.env.inChatWindow ? window.innerHeight * 0.05 : 0; // 5vh in pixels
+        const clientX = Math.max((ev.clientX ?? ev.touches[0].clientX) - boundingRect.left, 0);
+        const clientY = Math.max((ev.clientY ?? ev.touches[0].clientY) - boundingRect.top, 0);
+        if (!this.dragPos) {
+            this.dragPos = { posX: clientX, posY: clientY };
         }
-        const dX = this.state.dragPos.posX - clientX;
-        const dY = this.state.dragPos.posY - clientY;
-        this.state.dragPos.posX = Math.min(
-            Math.max(clientX, parent.offsetLeft),
-            parent.offsetLeft + parent.offsetWidth - insetEl.clientWidth
-        );
-        this.state.dragPos.posY = Math.min(
-            Math.max(clientY, parent.offsetTop),
-            parent.offsetTop + parent.offsetHeight - insetEl.clientHeight
-        );
-        insetEl.style.left =
-            Math.min(
-                Math.max(insetEl.offsetLeft - dX, 0),
-                parent.offsetWidth - insetEl.clientWidth
-            ) + "px";
-        insetEl.style.top =
-            Math.min(
-                Math.max(insetEl.offsetTop - dY, 0),
-                parent.offsetHeight - insetEl.clientHeight
-            ) + "px";
+        const dX = this.dragPos.posX - clientX;
+        const dY = this.dragPos.posY - clientY;
+        const widthOffset = parent.offsetWidth - insetEl.clientWidth;
+        const heightOffset = parent.offsetHeight - insetEl.clientHeight - bottomOffset;
+        this.dragPos.posX = Math.min(clientX, widthOffset);
+        this.dragPos.posY = Math.min(clientY, heightOffset);
+        insetEl.style.left = Math.min(Math.max(insetEl.offsetLeft - dX, 0), widthOffset) + "px";
+        insetEl.style.top = Math.min(Math.max(insetEl.offsetTop - dY, 0), heightOffset) + "px";
     }
 
     onFullScreenChange() {
