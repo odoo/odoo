04206b8d3b401a6fd2e73ca9be0e28510f93b53b
Patricia Daloze (pda) <pda@odoo.com>
2025-02-05 17:01:43 +0100

    [IMP] multi-modules: product redesign
    Minor changes on the UI by renaming labels, adapting orders
    or using built-in tools to improve the user experience.
    
    task-4417236

--- addons/product/models/product_combo_item.py
+++ addons/product/models/product_combo_item.py
@@ -12,7 +12,7 @@ class ProductComboItem(models.Model):
     company_id = fields.Many2one(related='combo_id.company_id', precompute=True, store=True)
     combo_id = fields.Many2one(comodel_name='product.combo', ondelete='cascade', required=True, index=True)
     product_id = fields.Many2one(
-        string="Product",
+        string="Options",
         comodel_name='product.product',
         ondelete='restrict',
         domain=[('type', '!=', 'combo')],
