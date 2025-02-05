5aa3606e441489c55588e87102a4e3e1871d4e00
David Monnom <moda@odoo.com>
2025-02-05 15:16:44 +0100

    [FIX] point_of_sale, pos_self_order: undefined channel name
    Prevent subscription to undefined channel name in Self Order.

--- addons/point_of_sale/static/src/app/models/data_service.js
+++ addons/point_of_sale/static/src/app/models/data_service.js
@@ -33,7 +33,6 @@ export class PosData extends Reactive {
         this.records = {};
         this.opts = new DataServiceOptions();
         this.channels = [];
-        this.onNotified = getOnNotified(this.bus, odoo.access_token);
 
         this.network = {
             warningTriggered: false,
@@ -42,6 +41,7 @@ export class PosData extends Reactive {
             unsyncData: [],
         };
 
+        this.intializeWebsocket();
         this.initIndexedDB();
         await this.initData();
 
@@ -68,8 +68,12 @@ export class PosData extends Reactive {
         this.bus.addEventListener("connect", this.reconnectWebSocket.bind(this));
     }
 
-    reconnectWebSocket() {
+    intializeWebsocket() {
         this.onNotified = getOnNotified(this.bus, odoo.access_token);
+    }
+
+    reconnectWebSocket() {
+        this.intializeWebsocket();
 
         const channels = [...this.channels];
         this.channels = [];
