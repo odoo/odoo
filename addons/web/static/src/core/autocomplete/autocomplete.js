odoo/odoo

commit 9c0f5612e9fab514116e2385338d8fc1ce42b821

[FIX] web/core: Autocomplete: disable browser's autocomplete
Before this commit, the input had its autocomplete attribute set to "off".
It is widely known that this attribute's value is not respected by browsers.

This commit forces to disable it with the "new-password" value.

Part-of: odoo/odoo#176262
Related: odoo/enterprise#68201
Signed-off-by: Lucas Perais (lpe) <lpe@odoo.com>

addons/web/static/src/core/autocomplete/autocomplete.xml
@@ -7,7 +7,7 @@
                 type="text"
                 t-att-id="props.id"
                 class="o-autocomplete--input o_input pe-3"
-                autocomplete="off"
+                autocomplete="new-password"
                 t-att-placeholder="props.placeholder"
                 role="combobox"
                 t-att-aria-activedescendant="activeSourceOptionId"

commit 6664ed0034f75bcb706eaddbae01949ea5c35cbe

[FIX] web: disable autocomplete for many2x field input
Steps:
- Open a task
- Click on "Tags" field input

Actual result:
- Autocomplete is there and hide dropdown

Expected result:
- Autocomplete is really off by default for many2x field
- No autocompletion like before 18.1, we already have one as it's a dropdown

opw-4551051

Caused by: https://github.com/odoo/odoo/commit/9c0f5612e9fab514116e2385338d8fc1ce42b821

addons/web/static/src/core/autocomplete/autocomplete.js
@@ -24,6 +24,7 @@ export class AutoComplete extends Component {
             },
         },
         placeholder: { type: String, optional: true },
+        autocomplete: { type: String, optional: true},
         autoSelect: { type: Boolean, optional: true },
         resetOnSelect: { type: Boolean, optional: true },
         onInput: { type: Function, optional: true },
@@ -41,6 +42,7 @@ export class AutoComplete extends Component {
     static defaultProps = {
         value: "",
         placeholder: "",
+        autocomplete: "new-password",
         autoSelect: false,
         dropdown: true,
         onInput: () => {},

addons/web/static/src/core/autocomplete/autocomplete.xml
@@ -7,7 +7,7 @@
                 type="text"
                 t-att-id="props.id"
                 class="o-autocomplete--input o_input pe-3"
-                autocomplete="new-password"
+                t-att-autocomplete="props.autocomplete"
                 t-att-placeholder="props.placeholder"
                 role="combobox"
                 t-att-aria-activedescendant="activeSourceOptionId"

addons/web/static/src/views/fields/relational_utils.xml
@@ -36,6 +36,7 @@
             value="props.value"
             id="props.id"
             placeholder="props.placeholder"
+            autocomplete="'off'"
             sources="sources"
             autoSelect="props.autoSelect"
             onSelect.bind="onSelect"