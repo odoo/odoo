ff935ce8958602109f8fda17384db36aa462cdf5
visp-odoo <visp@odoo.com>
2025-02-05 18:53:30 +0530

    [IMP] website_links: add link tracker dialog

--- addons/website_links/static/src/services/website_custom_menus.js
+++ addons/website_links/static/src/services/website_custom_menus.js
@@ -1,6 +1,19 @@
-import { registry } from '@web/core/registry';
+import { registry } from "@web/core/registry";
+import { LinkTrackerDialog } from "../components/dialog/link_tracker_dialog";
 
-registry.category('website_custom_menus').add('website_links.menu_link_tracker', {
-    openWidget: (services) => services.website.goToWebsite({ path: `/r?u=${encodeURIComponent(services.website.contentWindow.location.href)}` }),
+registry.category("website_custom_menus").add("website_links.menu_link_tracker", {
+    Component: LinkTrackerDialog,
     isDisplayed: (env) => env.services.website.currentWebsite && env.services.website.contentWindow,
+    getProps: async ({ orm, website }) => {
+        debugger;
+        const mainObject = website.currentWebsite.metadata.mainObject;
+        const isPage = mainObject.model === "website.page";
+        const model = "link.tracker.dialog";
+        return {
+            resModel: model,
+            onRecordSaved: async (record) => {
+             debugger;
+            },
+        };
+    },
 });
