35b9c8ff5a3f5e2ad8cfd5ddf430db082f9b53d1
Mohammad Abdulmoneim (abdu) <abdu@odoo.com>
2025-02-06 14:27:17 +0000

    Cherry pick of 37e185720f9d2e69e31174fd595feddf9c1ebf7c failed
    stdout:
    Auto-merging addons/mrp/views/mrp_bom_views.xml
    Auto-merging addons/mrp/views/mrp_production_views.xml
    Auto-merging addons/product/models/product_template.py
    Auto-merging addons/purchase/views/purchase_views.xml
    Auto-merging addons/uom/static/src/components/many2x_uom_tags/many2x_uom_tags.js
    CONFLICT (content): Merge conflict in addons/uom/static/src/components/many2x_uom_tags/many2x_uom_tags.js
    
    stderr:

--- addons/product/models/uom_uom.py
+++ addons/product/models/uom_uom.py
@@ -8,6 +8,28 @@ class UomUom(models.Model):
     _inherit = 'uom.uom'
 
     product_uom_ids = fields.One2many('product.uom', 'uom_id', string='Barcodes', domain=lambda self: ['|', ('product_id', '=', self.env.context.get('product_id')), ('product_id', 'in', self.env.context.get('product_ids', []))])
+<<<<<<< master
+||||||| 6e4ca1e184db037b191d707ae7a06a2f630ee4ab
+    packaging_barcodes_count = fields.Integer('Packaging Barcodes', compute='_compute_packaging_barcodes_count')
+
+    @api.depends('product_uom_ids')
+    def _compute_packaging_barcodes_count(self):
+        uom_to_barcode_count = dict(self.env['product.uom']._read_group(
+            [('uom_id', 'in', self.ids)], ['uom_id'], ['barcode:count'],
+        ))
+        for uom in self:
+            uom.packaging_barcodes_count = uom_to_barcode_count.get(uom, 0)
+=======
+    packaging_barcodes_count = fields.Integer('Packaging Barcodes', compute='_compute_packaging_barcodes_count')
+
+    @api.depends('product_uom_ids')
+    def _compute_packaging_barcodes_count(self):
+        uom_to_barcode_count = dict(self.env['product.uom']._read_group(
+            [('uom_id', 'in', self.ids)], ['uom_id'], ['barcode:count'],
+        ))
+        for uom in self:
+            uom.packaging_barcodes_count = uom_to_barcode_count.get(uom, 1)  # We always want to show the barcodes smart button
+>>>>>>> 20968dd4194fc95ca52509b813a3297220385496
 
     def action_open_packaging_barcodes(self):
         self.ensure_one()
