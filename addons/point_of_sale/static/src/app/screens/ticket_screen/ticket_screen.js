c3814883ae6dca01d0410bfd89874f3ab4f7452e
manv-afk <manv@odoo.com>
2025-02-06 14:34:09 +0000

    [FIX] point_of_sale: loading ticket screen
    This fix prevent blank screen (when offline) & inifinte loop (when in 3G or other low connection) on order screen.
    
    - **Offline Mode:** When accessing the order screen offline, now the screen loads previously fetched orders and displays a "Limited Functionality" popup.
    - **Slow Connection (3G):** Infinite loop (caused by repeated call to  `await this.pos.getServerOrders()`)  is fixed by ensuring orders load only once, even on slow connections using the flag `loadingOrderState`
    
    closes odoo/odoo#197026
    
    Task-id: 4550978
    X-original-commit: fecaba3ccbd1028c905ec4b9ac87655d2659a595
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: Manu Vaillant (manv) <manv@odoo.com>

--- addons/point_of_sale/static/src/app/screens/ticket_screen/ticket_screen.js
+++ addons/point_of_sale/static/src/app/screens/ticket_screen/ticket_screen.js
@@ -26,6 +26,7 @@ import { OrderDisplay } from "@point_of_sale/app/components/order_display/order_
 import { BarcodeVideoScanner } from "@web/core/barcode/barcode_video_scanner";
 import { makeAwaitable } from "@point_of_sale/app/utils/make_awaitable_dialog";
 import { NumberPopup } from "@point_of_sale/app/components/popups/number_popup/number_popup";
+import { ConnectionLostError } from "@web/core/network/rpc";
 
 const NBR_BY_PAGE = 30;
 const { DateTime } = luxon;
@@ -85,7 +86,20 @@ export class TicketScreen extends Component {
 
         onMounted(this.onMounted);
         onWillStart(async () => {
-            await this.pos.getServerOrders();
+            if (!this.pos.loadingOrderState) {
+                try {
+                    this.pos.loadingOrderState = true;
+                    await this.pos.getServerOrders();
+                } catch (error) {
+                    if (error instanceof ConnectionLostError) {
+                        Promise.reject(error);
+                        return error;
+                    }
+                    throw error;
+                } finally {
+                    this.pos.loadingOrderState = false;
+                }
+            }
         });
     }
     onMounted() {
