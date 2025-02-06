fc3332103888c7cb5ab098d8cdcc2faf5c54c01f
gteboul <gute@odoo.com>
2025-02-06 13:03:55 +0000

    [FIX] stock: display correct description 3 step incoming
    There is an inconsistency in the product description during 3-step incoming shipments.
    
    How to reproduce the issue:
    
    - Enable three-step incoming shipments.
    
    - Create a product with a description for receipts (e.g., 'receipt') and a different description for internal transfers (e.g., 'transfer').
    
    - Create a receipt with the product, validate it, and open the corresponding picking for the internal transfer.
    
    The move_line description_picking is taken from the receipt description instead of the internal transfer description (it shows 'receipt' instead of 'transfer').
    
    enterprise: https://github.com/odoo/enterprise/pull/79349
    
    opw-4406582
    
    X-original-commit: 236ea3d1d13306deb6081ddc37b86de373f848ba

--- addons/stock/models/stock_move_line.py
+++ addons/stock/models/stock_move_line.py
@@ -956,7 +956,7 @@ class StockMoveLine(models.Model):
             'product_id': self.product_id.id,
             'product_uom_qty': 0 if self.picking_id and self.picking_id.state != 'done' else self.quantity,
             'product_uom': self.product_uom_id.id,
-            'description_picking': self.description_picking,
+            'description_picking': self.description_picking or self.product_id.with_context(lang=self.env.context.get('lang'))._get_description(self.picking_type_id),
             'location_id': self.picking_id.location_id.id,
             'location_dest_id': self.picking_id.location_dest_id.id,
             'picked': self.picked,
