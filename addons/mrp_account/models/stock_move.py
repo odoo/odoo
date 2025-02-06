49565cdd9007ac66a3b835dc073777e2e6c48f2c
Ethan Vincent <etvi@odoo.com>
2025-02-06 15:48:37 +0100

    [FIX] mrp_account: constrain fifo candidates for unbuild SVL
    **Current behavior:**
    It's possible to end up with a negative valuation for a product
    with zero product qty.
    
    **Expected behavior:**
    No negative valuation for 0 product qty.
    
    **Steps to reproduce:**
    1. Create a FIFO, real-time product; create a BOM for
    fifo-product with some component (tracked by qty, costing method
    doesn't matter, `standard_price=1`)
    
    2. Purchase 2 units of component @ $1
    
    3. Create and fully process an MO for fifo-prod
    
    4. Change the standard price of component from $1 -> $2
    
    5. Create and fully process a second MO for fifo-prod
    
    6. Unbuild the second MO (from step 5)
    
    7. Sell 1 unit of fifo-prod
    
    8. Check the valuation, see that both the unbuild and out move
    (sale) use a `unit_cost=$2` despite there having been only 1 qty
    valued at $2 in the valuation history
    
    **Cause of the issue:**
    SVL creation for fifo costing in an unbuild context does not
    limit the candidate layer search to moves linked to the actual
    manufacturing order specified on the unbuild form.
    
    So despite us unbuilding the second MO in step 6, the valuation
    from the first MO's SVL is used.
    
    The reason we get the correct `unit_cost` despite using the
    wrong layer is due to commit: 84dda96
    Prior to which, the valuation would have correctly zeroed out
    but there would have still been this valuation mis-match behind
    the scenes.
    
    **Fix:**
    Constrain the fifo candidate layer search to finished moves from
    the unbuilt production with an additional domain leaf.
    
    opw-4416350
    
    closes odoo/odoo#197190
    
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/mrp_account/models/stock_move.py
+++ addons/mrp_account/models/stock_move.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from collections import defaultdict
+
 from odoo import models
 
 
@@ -44,6 +46,13 @@ class StockMove(models.Model):
     def _is_production_consumed(self):
         self.ensure_one()
         return self.location_dest_id.usage == 'production' and self.location_id._should_be_valued()
+
+    def _create_out_svl(self, forced_quantity=None):
+        product_unbuild_map = defaultdict(self.env['mrp.unbuild'].browse)
+        for move in self:
+            if move.unbuild_id:
+                product_unbuild_map[move.product_id] |= move.unbuild_id
+        return super(StockMove, self.with_context(product_unbuild_map=product_unbuild_map))._create_out_svl(forced_quantity)
 
     def _get_out_svl_vals(self, forced_quantity):
         unbuild_moves = self.filtered('unbuild_id')
