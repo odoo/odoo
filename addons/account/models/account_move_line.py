d570b153402d50470c9bfc8ac2142ed56c8d986b
Ruben Gomes <rugo@odoo.com>
2025-02-06 13:24:30 +0000

    [IMP] account - imported line shouldn't change tax
    Forward-port https://github.com/odoo/odoo/pull/193483 messed up and
    removed important code of https://github.com/odoo/odoo/pull/172787.
    
    This commit adds it back.
    
    task-4547597
    
    closes odoo/odoo#197039
    
    X-original-commit: ac32c2d5efcef243565193e4e227e13246a94236
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>

--- addons/account/models/account_move_line.py
+++ addons/account/models/account_move_line.py
@@ -849,7 +849,7 @@ class AccountMoveLine(models.Model):
     @api.depends('product_id', 'product_uom_id')
     def _compute_tax_ids(self):
         for line in self:
-            if line.display_type in ('line_section', 'line_note', 'payment_term'):
+            if line.display_type in ('line_section', 'line_note', 'payment_term') or line.is_imported:
                 continue
             # /!\ Don't remove existing taxes if there is no explicit taxes set on the account.
             if line.product_id or (line.display_type != 'discount' and (line.account_id.tax_ids or not line.tax_ids)):
