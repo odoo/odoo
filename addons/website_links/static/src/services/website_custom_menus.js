1548d198eaec53300e14df26900f6da83eee530e
visp-odoo <visp@odoo.com>
2025-02-06 15:23:30 +0530

    [IMP] website_links: add link tracker dialog3

--- addons/website_links/static/src/services/website_custom_menus.js
+++ addons/website_links/static/src/services/website_custom_menus.js
@@ -1,18 +1,32 @@
 import { registry } from "@web/core/registry";
 import { LinkTrackerDialog } from "../components/dialog/link_tracker_dialog";
+import { browser } from "@web/core/browser/browser";
 
 registry.category("website_custom_menus").add("website_links.menu_link_tracker", {
+    // openWidget: (services) => services.website.goToWebsite({ path: `/r?u=${encodeURIComponent(services.website.contentWindow.location.href)}` }),
     Component: LinkTrackerDialog,
     isDisplayed: (env) => env.services.website.currentWebsite && env.services.website.contentWindow,
     getProps: async ({ orm, website }) => {
-        debugger;
-        const mainObject = website.currentWebsite.metadata.mainObject;
-        const isPage = mainObject.model === "website.page";
         const model = "link.tracker";
         return {
             resModel: model,
             onRecordSaved: async (record) => {
-             debugger;
+                const params = {
+                    url: record.data.url,
+                    campaign_id: record.data.campaign_id[0],
+                    medium_id: record.data.medium_id[0],
+                    source_id: record.data.source_id[0],
+                    label: record.data.label,
+                };
+                const isRecordCreated = await orm.call(model, "search_or_create", [params]);
+
+                if (isRecordCreated) {
+                    browser.navigator.clipboard.writeText(record.data.url);
+                    website.websiteRootInstance.options.services.notification.add(
+                        "Your tracked link is now created & copied !",
+                        { type: "success" }
+                    );
+                }
             },
         };
     },
