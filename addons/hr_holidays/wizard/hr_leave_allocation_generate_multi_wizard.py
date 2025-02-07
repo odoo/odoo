5e445c946ad2adbba0941ea34d84337e69d27a8a
Mayurrajsinh Rathod <mrsr@odoo.com>
2025-02-07 17:02:15 +0530

    [MOV] core, *: move intervals to tools
    The intervals are the generalized concept that can be applied to any model with
    start and stop date.
    
    This commit moves the `Interval` class and a few utils methods from resource and
    appointment to odoo/tools. It improve flexibility and reusability across different models without
    any additional dependencies.
    
    Task-3997545
    
    Part-of: odoo/odoo#182082
    Related: odoo/enterprise#66386
    Related: odoo/upgrade#6446
    Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>

--- addons/hr_holidays/wizard/hr_leave_allocation_generate_multi_wizard.py
+++ addons/hr_holidays/wizard/hr_leave_allocation_generate_multi_wizard.py
@@ -1,7 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import _, api, fields, models
-from odoo.addons.resource.models.utils import HOURS_PER_DAY
+from odoo.tools.date_intervals import HOURS_PER_DAY
 
 
 class HrLeaveAllocationGenerateMultiWizard(models.TransientModel):
--- addons/website_event_exhibitor/models/event_sponsor.py
+++ addons/website_event_exhibitor/models/event_sponsor.py
@@ -5,8 +5,8 @@ from datetime import datetime, timedelta
 from pytz import timezone, utc
 
 from odoo import api, fields, models, _
-from odoo.addons.resource.models.utils import float_to_time
 from odoo.tools import is_html_empty
+from odoo.tools.date_intervals import float_to_time
 from odoo.tools.translate import html_translate
 
 
