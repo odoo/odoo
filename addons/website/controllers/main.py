3489523d049e187b9a1a76ee02b760c2a082db18
chdh-odoo <chdh@odoo.com>
2025-02-07 18:46:14 +0530

    [IMP] website: added configurator pages inside new page templates
    This commit aims to add user-selected configurator pages to the new page
    template list under the Landing Pages category, ensuring they are
    displayed in their original, unmodified state.
    
    Key changes:
    1. Updated configurator_apply method to store selected pages in
    ir.config_parameter, ensuring they can be accessed later.
    2. Modified get_new_page_templates route to:
      - Fetch stored pages from ir.config_parameter and retrieve their
      relevant view XML.
      - Display these pages in the "New Page" template selection, allowing
      users to reuse them.
    3. Renamed configurator_translation.js to configurator.js for better
    clarity.
    4. Added new steps to test if configurator pages appear in the landing
    pages category.
    
    task-4252670

--- addons/website/controllers/main.py
+++ addons/website/controllers/main.py
@@ -677,12 +677,41 @@ class Website(Home):
             return json.dumps({'view_id': page.get('view_id')})
         return json.dumps({'url': url})
 
+    def get_filtered_page_template(self, page):
+        """
+        Retrieves and extracts the relevant arch from an ir.ui.view associated
+        with a page record.
+
+        :param page: Record of website.page to fetch and filter its view's XML.
+        :return: Processed arch or False if not found.
+        """
+        if not page.exists() or not page.view_id or (page.view_id and not page.view_id.arch):
+            return False
+        page_view = page.view_id.arch
+        try:
+            tree = html.fromstring(page_view)
+            wrap_el = tree.xpath("//div[@id='wrap']")
+            return html.tostring(wrap_el[0], encoding="unicode") if wrap_el else page_view
+        except Exception as e:
+            logger.warning(f"Error parsing XML content for page ID {page.id}: {e}")
+            return page_view
+
     @http.route('/website/get_new_page_templates', type='jsonrpc', auth='user', website=True, readonly=True)
     def get_new_page_templates(self, **kw):
         View = request.env['ir.ui.view']
         result = []
         groups_html = View._render_template("website.new_page_template_groups")
         groups_el = etree.fromstring(f'<data>{groups_html}</data>')
+
+        website = self.env['website'].get_current_website()
+        param_key = f'website.untouched_configurator_pages.{website.id}'
+        page_ids = json.loads(self.env['ir.config_parameter'].sudo().get_param(param_key, '[]'))
+        current_website_pages = self.env['website.page'].search([
+            ('id', 'in', page_ids),
+            ('website_id', '=', website.id)
+        ])
+        filtered_configurator_templates = [self.get_filtered_page_template(page) for page in current_website_pages]
+
         for group_el in groups_el.getchildren():
             group = {
                 'id': group_el.attrib['id'],
@@ -732,6 +761,14 @@ class Website(Home):
                     logger.warning("Theme not compatible with template %r: %s", template.key, qe)
             if group['templates']:
                 result.append(group)
+
+        # Insert configurator pages at the beginning of the landing group
+        for group in result:
+            if group['id'] == 'landing':
+                landing_templates = group['templates']
+                for index, template in enumerate(reversed(filtered_configurator_templates), start=1):
+                    landing_templates.insert(0, {"key": f"configurator_pages_{index}", "template": template})
+                break
         return result
 
     @http.route('/website/save_xml', type='jsonrpc', auth='user', website=True)
