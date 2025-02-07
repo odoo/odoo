ac8f0edc17a19df685a0fbb201facf0b2312304b
Mylyna Hy <myhy@odoo.com>
2025-02-07 22:24:44 +0000

    [FIX] hr_holidays: accrual plans access rights
    Accrual plans are not readable for regular users, so once a user has an
    accrual plan set on one of their allocations, they cannot open their
    time off dashboard. Access rights error gets thrown.
    
    The bug was supposedly fixed in commit: https://github.com/odoo/odoo/commit/30f7fe62ef2579ed0a0c3ad9dbec5edf2ca60b18
    but it did not consider adding the sudo() for the condition
    based on the carryover_policy.
    
    Steps to Reproduce on Runbot:
    As Mitchell Admin:
    1. Install Time Off app
    2. Create a time off type (show in dashboard,requires allocation)
    3. Create an accrual plan (accrued_gain_time: end, carryover_date: year_start)
    4. Create a rule for the accrual plan
    (Accumulate 0.5 days, month at the 1st of the month, carryover policy is either none or maximum)
    5. Create an allocation for Marc Demo,who has no time off rights, for the time off type.
    Set to accrual allocation and link to the accrual plan.Then, set the start date to the
    1st of the month and run until some future date.
    
    As Marc Demo:
    1. Attempt to click into the Time Off app
    2. Observe Access Right error
    
    opw-4551392
    
    X-original-commit: 40ebf4b9d5ac88ac7306a327a6cf44a3387d7b3a

--- addons/hr_holidays/models/hr_leave_type.py
+++ addons/hr_holidays/models/hr_leave_type.py
@@ -642,7 +642,7 @@ class HrLeaveType(models.Model):
             carryover_policy = accrual_plan_level.action_with_unused_accruals if accrual_plan_level else False
             carryover_date = False
             if carryover_policy in ['maximum', 'lost']:
-                carryover_date = allocation._get_carryover_date(target_date)
+                carryover_date = allocation.sudo()._get_carryover_date(target_date)
                 # If carry over date == target date, then add 1 year to carry over date.
                 # Rational: for example if carry over date = 01/01 this year and target date = 01/01 this year,
                 # then any accrued days on 01/01 this year will have their carry over date 01/01 next year
