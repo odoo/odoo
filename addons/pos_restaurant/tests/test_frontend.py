6e93db7224ed27506f381943b0bc97964a383f91
aamo-odoo <aamo@odoo.com>
2025-02-07 10:53:08 +0530

    [FIX] pos_restaurant: prevent error when archiving floor plans
    This error occurs when we attempt to archive the floor plans.
    
    Steps to reproduce:
    - Install the 'pos_restaurant' module
    - POS > Configuration > Floor Plans
    - Select two plans > Archive
    
    Traceback : ValueError
    Expected singleton: restaurant.floor(2, 3, 4, 5)
    
    At [1], this error occurs because we are retrieving the 'floor_prefix' data
    from self, which returns all records instead of a single record.
    
    This commit will resolve the above error by using 'floor' in place of self in
    the for loop.
    
    [1]- https://github.com/odoo/odoo/blob/4d9984d0b5f5a856104d11261fecd27ddc2e5466/addons/pos_restaurant/models/pos_restaurant.py#L58-L60
    
    sentry-6275463514

--- addons/pos_restaurant/tests/test_frontend.py
+++ addons/pos_restaurant/tests/test_frontend.py
@@ -62,12 +62,12 @@ class TestFrontendCommon(TestPointOfSaleHttpCommon):
 
         cls.pos_config.floor_ids.unlink()
 
-        main_floor = cls.env['restaurant.floor'].create({
+        cls.main_floor = cls.env['restaurant.floor'].create({
             'name': 'Main Floor',
             'pos_config_ids': [(4, cls.pos_config.id)],
             'floor_prefix': 1,
         })
-        second_floor = cls.env['restaurant.floor'].create({
+        cls.second_floor = cls.env['restaurant.floor'].create({
             'name': 'Second Floor',
             'pos_config_ids': [(4, cls.pos_config.id)],
             'floor_prefix': 2,
@@ -75,14 +75,14 @@ class TestFrontendCommon(TestPointOfSaleHttpCommon):
 
         cls.main_floor_table_5 = cls.env['restaurant.table'].create([{
             'table_number': 105,
-            'floor_id': main_floor.id,
+            'floor_id': cls.main_floor.id,
             'seats': 4,
             'position_h': 100,
             'position_v': 100,
         }])
         cls.env['restaurant.table'].create([{
             'table_number': 104,
-            'floor_id': main_floor.id,
+            'floor_id': cls.main_floor.id,
             'seats': 4,
             'shape': 'square',
             'position_h': 350,
@@ -90,7 +90,7 @@ class TestFrontendCommon(TestPointOfSaleHttpCommon):
         },
         {
             'table_number': 102,
-            'floor_id': main_floor.id,
+            'floor_id': cls.main_floor.id,
             'seats': 4,
             'position_h': 250,
             'position_v': 100,
@@ -98,7 +98,7 @@ class TestFrontendCommon(TestPointOfSaleHttpCommon):
         {
 
             'table_number': 201,
-            'floor_id': second_floor.id,
+            'floor_id': cls.second_floor.id,
             'seats': 4,
             'shape': 'square',
             'position_h': 100,
@@ -106,7 +106,7 @@ class TestFrontendCommon(TestPointOfSaleHttpCommon):
         },
         {
             'table_number': 203,
-            'floor_id': second_floor.id,
+            'floor_id': cls.second_floor.id,
             'seats': 4,
             'position_h': 100,
             'position_v': 250,
