72bc6550aca16c56549e4080c124c7ea710eeafb
Krzysztof Magusiak (krma) <krma@odoo.com>
2025-02-08 11:47:04 +0000

    [IMP] website: cron unlink visitors
    Use the new progress API and remove auto_commit.
    
    closes odoo/odoo#197781
    
    Related: odoo/documentation#12077
    Signed-off-by: Julien Castiaux (juc) <juc@odoo.com>

--- addons/website/models/website_visitor.py
+++ addons/website/models/website_visitor.py
@@ -4,12 +4,11 @@ from datetime import datetime, timedelta
 
 import hashlib
 import pytz
-import threading
 
-from odoo import fields, models, api, _
+from odoo import api, fields, models
 from odoo.addons.base.models.res_partner import _tz_get
 from odoo.exceptions import UserError
-from odoo.tools import split_every, SQL
+from odoo.tools import _, SQL
 from odoo.tools.misc import _format_time_ago
 from odoo.http import request
 from odoo.osv import expression
@@ -336,21 +335,13 @@ class WebsiteVisitor(models.Model):
         Visitors were previously archived but we came to the conclusion that
         archived visitors have very little value and bloat the database for no
         reason. """
-        auto_commit = not getattr(threading.current_thread(), 'testing', False)
-        visitor_model = self.env['website.visitor']
-        visitor_ids = visitor_model.sudo().search(self._inactive_visitors_domain(), limit=limit).ids
-        visitor_done = 0
-        for inactive_visitors_batch in split_every(
-            batch_size,
-            visitor_ids,
-            visitor_model.browse,
-        ):
-            inactive_visitors_batch.unlink()
-            visitor_done += len(inactive_visitors_batch)
-            if auto_commit:
-                self.env['ir.cron']._notify_progress(done=visitor_done, remaining=len(visitor_ids) - visitor_done)
-                self.env.cr.commit()
-        self.env['ir.cron']._notify_progress(done=visitor_done, remaining=len(visitor_ids) - visitor_done)
+        domain = self._inactive_visitors_domain()
+        visitors = self.env['website.visitor'].sudo().search(domain, limit=batch_size)
+        visitors.unlink()
+        self.env['ir.cron']._notify_progress(
+            done=len(visitors),
+            remaining=0 if len(visitors) < batch_size else visitors.search_count(domain),
+        )
 
     def _inactive_visitors_domain(self):
         """ This method defines the domain of visitors that can be cleaned. By
