3517633f4c5884bc14ef6d6c54534c4e345fb0c7
tsm-odoo <tsm@odoo.com>
2025-02-10 18:37:33 +0100

    [FIX] crm_livechat: simplify model
    Remove useless field on crm.lead which is a huge model. No need to
    add useless noise.
    
    Remove useless groups. ACLs should be correctly implemented.
    
    Remove useless code in tests.
    
    follow up of task-4432728

--- addons/crm_livechat/tests/test_discuss_channel_access.py
+++ addons/crm_livechat/tests/test_discuss_channel_access.py
@@ -1,3 +1,6 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo.exceptions import AccessError
 from odoo.tests.common import HttpCase, new_test_user, tagged
 
 
@@ -35,3 +38,31 @@ class TestDiscussChannelAccess(HttpCase):
                 f"user_grp={user_grp}, has_lead={has_lead}, expected_result={expected_result}",
             )
 
+    def test_cannot_link_lead_to_restricted_channel(self):
+        user = new_test_user(self.env, login="bob_user", groups="sales_team.group_sale_salesman")
+        channel = (
+            self.env["discuss.channel"]
+            .create(
+                {
+                    "name": f"Visitor #11",
+                    "livechat_operator_id": self.env.user.partner_id.id,
+                    "channel_type": "livechat",
+                }
+            )
+            .with_user(user)
+        )
+        self.assertFalse(channel.has_access("read"))
+        with self.assertRaises(
+            AccessError,
+            msg="You cannot create a lead linked to a channel you don't have access to.",
+        ):
+            self.env["crm.lead"].with_user(user).create(
+                {"name": "lead", "origin_channel_id": channel.id}
+            )
+
+        lead = self.env["crm.lead"].with_user(user).create({"name": "lead"})
+        with self.assertRaises(
+            AccessError,
+            msg="You cannot update a lead and link it to a channel you don't have access to.",
+        ):
+            lead.write({"origin_channel_id": channel.id})
