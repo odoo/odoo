6a97c79f76d8839aeb1a756bc2e4efcbbe594dfe
Arnold Moyaux <arm@odoo.com>
2025-02-10 14:50:43 +0100

    [IMP] purchase_stock: add OTD to dashboard
    Part-of: odoo/odoo#197211
    Related: odoo/upgrade#7213
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/purchase_stock/models/purchase_order.py
+++ addons/purchase_stock/models/purchase_order.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+from dateutil.relativedelta import relativedelta
 from markupsafe import Markup
 
 from odoo import api, Command, fields, models, SUPERUSER_ID, _
-from odoo.tools.float_utils import float_compare
+from odoo.tools.float_utils import float_compare, float_repr
 from odoo.exceptions import UserError
 from odoo.tools import format_list
 from odoo.tools.misc import OrderedSet
@@ -178,6 +179,32 @@ class PurchaseOrder(models.Model):
 
     def action_view_picking(self):
         return self._get_action_view_picking(self.picking_ids)
+
+    @api.model
+    def retrieve_dashboard(self):
+        result = super().retrieve_dashboard()
+        three_months_ago = fields.Datetime.to_string(fields.Datetime.now() - relativedelta(months=3))
+
+        purchases = self.env['purchase.order'].search_fetch(
+            [('state', 'in', ['purchase', 'done']), ('date_planned', '>=', three_months_ago)],
+            ['date_planned', 'effective_date', 'user_id'])
+
+        otd_purchase_count = 0
+        my_purchase_count = 0
+        my_otd_purchase_count = 0
+        for po in purchases:
+            if po.user_id == self.env.user:
+                my_purchase_count += 1
+            if not po.effective_date or po.effective_date > po.date_planned:
+                continue
+            otd_purchase_count += 1
+            if po.user_id == self.env.user:
+                my_otd_purchase_count += 1
+
+        result['global']['otd'] = _("%(otd)s %%", otd=float_repr(otd_purchase_count / len(purchases) * 100 if purchases else 100, precision_digits=0))
+        result['my']['otd'] = _("%(otd)s %%", otd=float_repr(my_otd_purchase_count / my_purchase_count * 100 if my_purchase_count else 100, precision_digits=0))
+        result['days_to_purchase'] = self.env.company.days_to_purchase
+        return result
 
     def _get_action_view_picking(self, pickings):
         """ This function returns an action that display existing picking orders of given purchase order ids. When only one found, show the picking immediately.
