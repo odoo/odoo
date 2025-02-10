f8c04e7c1028a018f710a682e663ffaf38a29aaf
Antoine Guenet <age@odoo.com>
2025-02-10 11:36:21 +0000

    [FIX] html_editor: prevent non-deterministic fail due to link popover
    Some tests end with the selection in a link, leading the link popover to
    open, which blurs the editable element. To prevent that, we would focus
    the editable by force, then wait a tick. But when the CPU is slow,
    sometimes the focus into the link popover happens too late to redirect
    it. This waits for the popover to open before focusing the editable.
    
    runbot-112609
    
    closes odoo/odoo#197956
    
    X-original-commit: 2e20d83e3de676ea7bc75159138309de65f6250b
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Antoine Guenet (age) <age@odoo.com>

--- addons/html_editor/static/tests/insert/paragraph_break.test.js
+++ addons/html_editor/static/tests/insert/paragraph_break.test.js
@@ -1,10 +1,11 @@
 import { describe, test } from "@odoo/hoot";
-import { tick } from "@odoo/hoot-mock";
+import { waitFor } from "@odoo/hoot-dom";
 import { testEditor } from "../_helpers/editor";
 import { insertText, splitBlock } from "../_helpers/user_actions";
 import { unformat } from "../_helpers/format";
 import { MAIN_PLUGINS } from "@html_editor/plugin_sets";
 import { QWebPlugin } from "@html_editor/others/qweb_plugin";
+import { findInSelection } from "@html_editor/utils/selection";
 
 describe("Selection collapsed", () => {
     describe("Basic", () => {
@@ -449,10 +450,13 @@ describe("Selection collapsed", () => {
 
         async function splitBlockA(editor) {
             // splitBlock in an <a> tag will open the linkPopover which will take the focus.
-            // So we need to put the selection back into the editor
+            // So we need to wait for it to open and put the selection back into the editor.
             splitBlock(editor);
+            const editableSelection = editor.shared.selection.getSelectionData().editableSelection;
+            if (findInSelection(editableSelection, "a:not([href])")) {
+                await waitFor(".o-we-linkpopover");
+            }
             editor.shared.selection.focusEditable();
-            await tick();
         }
 
         // @todo: re-evaluate this possibly outdated comment:
@@ -487,7 +491,7 @@ describe("Selection collapsed", () => {
             });
         });
 
-        test("should insert a paragraph break outside the starting edge of an anchor", async () => {
+        test("should insert a paragraph break outside the starting edge of an anchor at start of block", async () => {
             await testEditor({
                 contentBefore: "<p><a>[]ab</a></p>",
                 stepFunction: splitBlockA,
@@ -495,6 +499,8 @@ describe("Selection collapsed", () => {
                     '<p><br></p><p>\ufeff<a class="o_link_in_selection">\ufeff[]ab\ufeff</a>\ufeff</p>',
                 contentAfter: "<p><br></p><p><a>[]ab</a></p>",
             });
+        });
+        test("should insert a paragraph break outside the starting edge of an anchor after some text", async () => {
             await testEditor({
                 contentBefore: "<p>ab<a>[]cd</a></p>",
                 stepFunction: splitBlockA,
@@ -516,7 +522,7 @@ describe("Selection collapsed", () => {
             await testEditor({
                 contentBefore: "<p><a>ab[]</a></p>",
                 stepFunction: splitBlockA,
-                contentAfterEdit: `<p>\ufeff<a>\ufeffab\ufeff</a>\ufeff</p><p placeholder='Type "/" for commands' class="o-we-hint">[]<br></p>`,
+                contentAfterEdit: `<p>\ufeff<a>\ufeffab\ufeff</a>\ufeff</p><p>[]<br></p>`,
                 contentAfter: `<p><a>ab</a></p><p>[]<br></p>`,
             });
         });
