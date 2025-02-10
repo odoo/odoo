31ff47d2fabc2b1ffff60e4f0a64c844f25ced6c
Anton Romanova (roan) <roan@odoo.com>
2025-02-10 17:00:03 +0100

    [IMP] l10n_tr_nilvera_einvoice: generic einvoice fetch/notification
    We start accumulating buttons for fetching electronic invoices and their
    statuses with different localisations. In addition to peppol, there
    l10n_tk and soon l10n_dk (with Nemhandel:
    https://github.com/odoo/odoo/pull/190474) which also needs such a button
    
    In addition to these buttons, we also need a standard way of notifying
    users when receiving einvoices for these different modules
    
    This commit adds overrides the generic einvoices (status) fetch button
    methods for Türkiye localisation as well as hooking into the generic
    function to send email upon einvoices reception.
    
    task-4457146

--- addons/l10n_tr_nilvera_einvoice/__manifest__.py
+++ addons/l10n_tr_nilvera_einvoice/__manifest__.py
@@ -8,7 +8,6 @@ For sending and receiving electronic invoices to Nilvera.
     'depends': ['l10n_tr_nilvera', 'account_edi_ubl_cii'],
     'data': [
         'data/cron.xml',
-        'views/account_journal_dashboard_views.xml',
         'views/account_move_views.xml',
     ],
     'auto_install': ['l10n_tr_nilvera'],
