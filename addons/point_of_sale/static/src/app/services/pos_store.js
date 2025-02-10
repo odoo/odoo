cd3b9631f7af3aad2752cc01608713039ab120e3
David Monnom <moda@odoo.com>
2025-02-10 14:20:24 +0000

    [FIX] point_of_sale, pos_restaurant: fix random error in test
    The `PreparationPrinterContent` test was checking if the preparation
    receipt was containing a specific string. But the check was performed
    on the `.render-container` which is generated for printing and erased
    just after. That was causing a random error in the test.
    
    Now we call internal `pos_store` method that generate the receipt
    content and we check if the string is present in the content.
    
    runbot error id: 114884
    
    X-original-commit: e83cb544e6424cffba29811b33f705a5bd2cad0c

--- addons/point_of_sale/static/src/app/services/pos_store.js
+++ addons/point_of_sale/static/src/app/services/pos_store.js
@@ -1515,6 +1515,9 @@ export class PosStore extends WithLazyGetterTrap {
     getOrderChanges(order = this.getOrder()) {
         return getOrderChanges(order, this.config.preparationCategories);
     }
+    changesToOrder(order, skipped = false, orderPreparationCategories, cancelled = false) {
+        return changesToOrder(order, skipped, orderPreparationCategories, cancelled);
+    }
     // Now the printer should work in PoS without restaurant
     async sendOrderInPreparation(order, opts = {}) {
         if (this.config.printerCategories.size && !opts.byPassPrint) {
@@ -1559,8 +1562,7 @@ export class PosStore extends WithLazyGetterTrap {
         await this.sendOrderInPreparation(o, { cancelled });
     }
 
-    async printChanges(order, orderChange, reprint = false) {
-        const unsuccedPrints = [];
+    generateOrderChange(order, orderChange, categories, reprint = false) {
         orderChange.new.sort((a, b) => {
             const sequenceA = a.pos_categ_sequence;
             const sequenceB = b.pos_categ_sequence;
@@ -1570,7 +1572,6 @@ export class PosStore extends WithLazyGetterTrap {
 
             return sequenceA - sequenceB;
         });
-
         const orderData = {
             reprint: reprint,
             pos_reference: order.getName(),
@@ -1587,10 +1588,19 @@ export class PosStore extends WithLazyGetterTrap {
             },
         };
 
+        const changes = this.filterChangeByCategories(categories, orderChange);
+        return { orderData, changes };
+    }
+
+    async printChanges(order, orderChange, reprint = false) {
+        const unsuccedPrints = [];
+
         for (const printer of this.unwatched.printers) {
-            const changes = this.filterChangeByCategories(
+            const { orderData, changes } = this.generateOrderChange(
+                order,
+                orderChange,
                 printer.config.product_categories_ids,
-                orderChange
+                reprint
             );
 
             if (changes.new.length) {
