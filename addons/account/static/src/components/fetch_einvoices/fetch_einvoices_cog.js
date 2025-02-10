97690f0cb0b8546f96a0b4a435ac5eced1cb413b
Anton Romanova (roan) <roan@odoo.com>
2025-02-10 16:37:03 +0100

    [IMP] account: generic einvoice fetch/notification
    We start accumulating buttons for fetching electronic invoices and their
    statuses with different localisations. In addition to peppol, there
    l10n_tk and soon l10n_dk (with Nemhandel:
    https://github.com/odoo/odoo/pull/190474) which also needs such a button
    
    In addition to these buttons, we also need a standard way of notifying
    users when receiving einvoices for these different modules
    
    This commit adds the base functionnality for having one unique button on
    purchase/sale journal to fetch new invoices/sent invoice statuses and
    sending email for received einvoices.
    
    related prs:
    - odoo/upgrade#7205
    - odoo/enterprise#80111
    
    task-4457146
    
    Part-of: odoo/odoo#197233
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

---
+++ addons/account/static/src/components/fetch_einvoices/fetch_einvoices_cog.js
@@ -0,0 +1,53 @@
+import { Component } from "@odoo/owl";
+import { DropdownItem } from "@web/core/dropdown/dropdown_item";
+import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
+import { _t } from "@web/core/l10n/translation";
+import { ACTIONS_GROUP_NUMBER } from "@web/search/action_menus/action_menus";
+
+const cogMenuRegistry = registry.category("cogMenu");
+
+export class FetchEInvoices extends Component {
+    static template = "account.FetchEInvoices";
+    static props = {};
+    static components = { DropdownItem };
+
+    setup() {
+        super.setup();
+        this.action = useService("action");
+    }
+
+    get buttonLabel() {
+        return this.env.searchModel.globalContext.show_fetch_in_einvoices_button
+            ? _t("Fetch e-Invoices")
+            : _t("Refresh e-Invoices Status");
+    }
+
+    fetchEInvoices() {
+        const journalId = this.env.searchModel.globalContext.default_journal_id;
+        if (!journalId) {
+            return;
+        }
+
+        this.action.doActionButton({
+            type: "object",
+            resId: journalId,
+            name: "button_fetch_in_einvoices",
+            resModel: "account.journal",
+            onClose: () => window.location.reload(),
+        });
+    }
+}
+
+export const fetchEInvoicesActionMenu = {
+    Component: FetchEInvoices,
+    groupNumber: ACTIONS_GROUP_NUMBER,
+    isDisplayed: ({ config, searchModel }) =>
+        searchModel.resModel === "account.move" &&
+        (searchModel.globalContext.default_journal_id || false) &&
+        (searchModel.globalContext.show_fetch_in_einvoices_button ||
+            searchModel.globalContext.show_refresh_out_einvoices_status_button ||
+            false),
+};
+
+cogMenuRegistry.add("account-fetch-e-invoices", fetchEInvoicesActionMenu, { sequence: 11 });
