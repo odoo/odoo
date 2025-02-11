9ab1d1cc034b000fbab8b61a48a1bfd4ba625b5b
Julien Banken <jbn@odoo.com>
2025-02-11 11:32:42 +0100

    [IMP] mail: restore recipient lists from mail composer wizard
    This commit ensures that the recipient list in the chatter is updated
    whenever the user modifies the recipients in the full mail composer
    and closes the wizard.
    
    Task-4496350
    
    Part-of: odoo/odoo#188642
    Related: odoo/enterprise#74599
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mail/static/src/chatter/web/mail_composer_form.js
+++ addons/mail/static/src/chatter/web/mail_composer_form.js
@@ -25,6 +25,7 @@ export class MailComposerFormController extends formView.Controller {
 export class MailComposerFormRenderer extends formView.Renderer {
     setup() {
         super.setup();
+        this.orm = useService("orm");
         // Autofocus the visible editor in edition mode.
         this.root = useRef("compiled_view_root");
         useEffect(
@@ -44,23 +45,110 @@ export class MailComposerFormRenderer extends formView.Renderer {
             () => [this.props.record.isInEdition, this.root.el, this.props.record.resId]
         );
 
+        const getActiveMailThreads = () =>
+            JSON.parse(this.props.record.data.res_ids).map((resId) => {
+                const thread = this.mailStore.Thread.insert({
+                    model: this.props.record.data.model,
+                    id: resId,
+                });
+                return thread;
+            });
+
         // Add file dropzone on full mail composer:
         this.attachmentUploadService = useService("mail.attachment_upload");
-        this.operations = useX2ManyCrud(() => {
-            return this.props.record.data["attachment_ids"];
-        }, true);
+        this.operations = useX2ManyCrud(() => this.props.record.data["attachment_ids"], true);
 
         useCustomDropzone(this.root, MailAttachmentDropzone, {
             /** @param {Event} event */
-            onDrop: async event => {
-                const resIds = JSON.parse(this.props.record.data.res_ids);
-                const thread = this.mailStore.Thread.insert({
-                    model: this.props.record.data.model,
-                    id: resIds[0],
-                });
-                for (const file of event.dataTransfer.files) {
-                    const attachment = await this.attachmentUploadService.upload(thread, thread.composer, file);
-                    await this.operations.saveRecord([attachment.id]);
+            onDrop: async (event) => {
+                for (const thread of getActiveMailThreads()) {
+                    for (const file of event.dataTransfer.files) {
+                        const attachment = await this.attachmentUploadService.upload(
+                            thread,
+                            thread.composer,
+                            file
+                        );
+                        await this.operations.saveRecord([attachment.id]);
+                    }
+                }
+            },
+        });
+
+        /** @param {function} */
+        const onCloseWizardModal = (callback) => {
+            this.env.dialogData.dismiss = callback;
+        };
+
+        onCloseWizardModal(async () => {
+            const selectedPartnerIds = this.props.record.data.partner_ids.records.map(
+                (partner) => partner.resId
+            );
+            const selectedPartners = await this.orm.searchRead(
+                "res.partner",
+                [["id", "in", selectedPartnerIds]],
+                ["email", "id", "lang", "name"]
+            );
+
+            /**
+             * @param {SuggestedRecipient} recipient
+             * @returns {SuggestedRecipient}
+             */
+            const updateRecipientWithCorrespondingPartner = (recipient) => {
+                const partner = selectedPartners.find(
+                    (partner) => partner.id === recipient.id || partner.email === recipient.email
+                );
+                if (partner) {
+                    return {
+                        ...recipient,
+                        email: partner.email,
+                        lang: partner.lang,
+                        name: partner.name,
+                        partner_id: partner.id,
+                        persona: { type: "partner", id: partner.id },
+                    };
+                }
+                return recipient;
+            };
+
+            /**
+             * @param {SuggestedRecipient} recipient
+             * @returns {boolean}
+             */
+            const isRecipientSelectedFromFullMailComposer = (recipient) =>
+                selectedPartnerIds.includes(recipient.partner_id);
+
+            for (const thread of getActiveMailThreads()) {
+                // Update the recipient lists:
+                thread.suggestedRecipients = thread.suggestedRecipients.map(
+                    updateRecipientWithCorrespondingPartner
+                );
+                thread.additionalRecipients = thread.additionalRecipients.map(
+                    updateRecipientWithCorrespondingPartner
+                );
+
+                // Remove the recipients that got removed from the composer:
+                thread.suggestedRecipients = thread.suggestedRecipients.filter(
+                    isRecipientSelectedFromFullMailComposer
+                );
+                thread.additionalRecipients = thread.additionalRecipients.filter(
+                    isRecipientSelectedFromFullMailComposer
+                );
+
+                // Add the recipients that got added to the composer:
+                for (const partner of selectedPartners) {
+                    const allRecipients = [
+                        ...thread.suggestedRecipients,
+                        ...thread.additionalRecipients,
+                    ];
+                    if (!allRecipients.some((recipient) => recipient.partner_id === partner.id)) {
+                        thread.additionalRecipients.push({
+                            email: partner.email,
+                            lang: partner.lang,
+                            name: partner.name,
+                            partner_id: partner.id,
+                            persona: { type: "partner", id: partner.id },
+                        });
+                    }
                 }
             }
         });
