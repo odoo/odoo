270c7d8ae6176e0f4112dfa226e211c079e53157
Logan Staelens <lost@odoo.com>
2025-02-11 16:32:12 +0000

    [FIX] project_sale_expense: Keep Analytic distribution when reinvoicing expenses
    Steps to reproduce the flow:
    - Create an expensable product “Hotel” that can be reinvoiced to a customer
    - Create an analytic distribution model that automatically applies the analytic account “Commercial &Marketing” to the previous created product “Hotel”
    - Create an SO for a customer “Gilles”. The SO is linked to the analytic account “Home Construction”
    - Create an expense which category is “Hotel”. The analytic distribution is automatically set to “Commercial & Marketing”
    - Reinvoice this expense to “Gilles”. The analytic distribution automatically changes to “Home Construction”. -> Wrong. We lose part of the information
    - When posting the expense report, the expense is added in the SO through a dedicated SO line. The analytic account previously set as “Home Construction” is now transformed, for an unknown reason, into “Commercial & Marketing”. -> Wrong.
    - On the vendor bill, the analytic distribution is “Home Construction”
    
    Now:
    1. The analytic plans from the expense and from the SO are different. Simply add the analytic account from the SO
    2. The analytic plans are the same. There are conflicts. In this case, the analytic distribution set on the SO is prioritary. We ensure that costs incurred or sales generated from the expense are reflected in the SO's analytic plan rather than the generic one. In the previous example, Home Construction is the only analytic account that should be considered on the expense, bill and SO.
    
    task-4564463
    
    closes odoo/odoo#240762
    
    X-original-commit: 63d3d526ee32da01f8ed4522d7952ebb16871386
    Signed-off-by: Julien Alardot (jual) <jual@odoo.com>
    Signed-off-by: Logan Staelens (lost) <lost@odoo.com>

--- addons/analytic/models/analytic_mixin.py
+++ addons/analytic/models/analytic_mixin.py
@@ -49,6 +49,16 @@ class AnalyticMixin(models.AbstractModel):
             self._field_to_sql(table or self._table, 'analytic_distribution'),
         )
 
+    @api.model
+    def _get_analytic_account_ids_from_distributions(self, distributions):
+        if not distributions:
+            return []
+
+        if isinstance(distributions, (list, tuple, set)):
+            return {int(_id) for distribution in distributions for key in (distribution or {}) for _id in key.split(',')}
+        else:
+            return {int(_id) for key in (distributions or {}) for _id in key.split(',')}
+
     @api.depends('analytic_distribution')
     def _compute_distribution_analytic_account_ids(self):
         all_ids = {int(_id) for rec in self for key in (rec.analytic_distribution or {}) for _id in key.split(',')}
