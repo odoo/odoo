c0ad6e8fc4f19148bdbc20055fe123f1e450aca3
adip-odoo <adip@odoo.com>
2025-02-11 09:19:55 +0000

    [FIX] pos_sale: prevent error when canceling or confirming invoice
    Currently, multiple errors occur when the user tries to edit the invoice
    created by the POS application.
    
    Steps to produce:
    - Install the `pos_sale` module.
    - Create a new order using the 'POS' application (with invoice).
    - Open the corresponding invoice from `Invoicing > Customers > Invoices (list)`.
    - Click on the `Reset to Draft` button.
    - Click the `Cancel` button → encounter **Error-1**.
    - Click the `Confirm` button → encounter **Error-2**.
    
    **Error-1**: `TypeError - argument of type 'bool' is not iterable`
    **Error-2**: `AttributeError - 'bool' object has no attribute 'replace'`
    
    The issue occurs because `pos_order_line.sale_order_line_id.name` is accessed
    at [1], without verifying whether `sale_order_line_id` exists.
    
    [1] - https://github.com/odoo/odoo/blob/add64f4c8a8d7e3aeb324621da14fd9b0a3660c5/addons/pos_sale/models/account_move.py#L11-L15
    
    This commit resolves the issue by ensuring the `sale_order_line_id` exists
    before accessing its attributes. If no sale order is defined, the method skips
    further computation to prevent errors.
    
    sentry-6272400381, 6272399674
    
    X-original-commit: d07d1614dd110fe607c923fa70af699c20024340

--- addons/pos_sale/models/account_move.py
+++ addons/pos_sale/models/account_move.py
@@ -8,12 +8,28 @@ class AccountMove(models.Model):
         if self.env.user.has_group('point_of_sale.group_pos_user'):
             for invoice in self:
                 for pos_order_line in invoice.pos_order_ids.mapped('lines'):
+<<<<<<< saas-18.2
                     if pos_order_line.sale_order_line_id:
                         if isCancelled and "(Cancelled)" not in pos_order_line.sale_order_line_id.name:
                             name = _("%(old_name)s (Cancelled)", old_name=pos_order_line.sale_order_line_id.name)
                             pos_order_line.sale_order_line_id.name = name
                         elif not isCancelled and "(Cancelled)" in pos_order_line.sale_order_line_id.name:
                             pos_order_line.sale_order_line_id.name = pos_order_line.sale_order_line_id.name.replace(" (Cancelled)", "")
+||||||| 21f8121fb102cdb49238668a334a133ece3808ae
+                    if isCancelled and "(Cancelled)" not in pos_order_line.sale_order_line_id.name:
+                        name = _("%(old_name)s (Cancelled)", old_name=pos_order_line.sale_order_line_id.name)
+                        pos_order_line.sale_order_line_id.name = name
+                    elif not isCancelled:
+                        pos_order_line.sale_order_line_id.name = pos_order_line.sale_order_line_id.name.replace(" (Cancelled)", "")
+=======
+                    if not pos_order_line.sale_order_line_id:
+                        continue
+                    if isCancelled and "(Cancelled)" not in pos_order_line.sale_order_line_id.name:
+                        name = _("%(old_name)s (Cancelled)", old_name=pos_order_line.sale_order_line_id.name)
+                        pos_order_line.sale_order_line_id.name = name
+                    elif not isCancelled:
+                        pos_order_line.sale_order_line_id.name = pos_order_line.sale_order_line_id.name.replace(" (Cancelled)", "")
+>>>>>>> 2dcd8d3dc4807f3f7f4e7c762f388f488d533a1f
 
     def button_cancel(self):
         res = super().button_cancel()
