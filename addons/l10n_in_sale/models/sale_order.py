2f2e7e126c67c983d7ef40b6b6631c2fd3b3401e
masm-odoo <masm@odoo.com>
2025-02-11 18:02:25 +0530

    [FIX] l10n_in_sale: compute FPOS based on POS for Indian Compliance
    *FPOS - Fiscal Position
    *POS - Place of Supply
    
    - Before this commit: The fiscal position was determined based on the partner's
    shipping address (`partner_shipping_id`) in sales orders, which did not align
    with Indian tax regulations.
    
    - After this commit: The fiscal position is now computed based on the
    place of supply, ensuring compliance with Indian tax requirements.

--- addons/l10n_in_sale/models/sale_order.py
+++ addons/l10n_in_sale/models/sale_order.py
@@ -19,6 +19,58 @@ class SaleOrder(models.Model):
             ('deemed_export', 'Deemed Export'),
             ('uin_holders', 'UIN Holders'),
         ], string="GST Treatment", readonly=False, compute="_compute_l10n_in_gst_treatment", store=True)
+    l10n_in_sale_state_id = fields.Many2one('res.country.state', string="Place of supply",
+        compute="_compute_l10n_in_sale_state_id", store=True, copy=True, precompute=True)
+    l10n_in_sale_is_gst_registered_enabled = fields.Boolean(related='company_id.l10n_in_is_gst_registered')
+
+    @api.depends('partner_id', 'partner_shipping_id', 'company_id')
+    def _compute_l10n_in_sale_state_id(self):
+        for order in self:
+            if order.country_code == 'IN':
+                partner_state = (
+                    order.partner_id.commercial_partner_id == order.partner_shipping_id.commercial_partner_id
+                    and order.partner_shipping_id.state_id
+                    or order.partner_id.state_id
+                )
+                if not partner_state:
+                    partner_state = order.partner_id.commercial_partner_id.state_id or order.company_id.state_id
+                if partner_state.country_id.code == 'IN':
+                    order.l10n_in_sale_state_id = partner_state
+                else:
+                    order.l10n_in_sale_state_id = self.env.ref('l10n_in.state_in_oc', raise_if_not_found=False)
+            else:
+                order.l10n_in_sale_state_id = False
+
+    @api.depends('l10n_in_sale_state_id')
+    def _compute_fiscal_position_id(self):
+
+        def _get_fiscal_state(order):
+            """
+            Maps each order to its corresponding fiscal state based on its type,
+            fiscal conditions, and the state of the associated partner or company.
+            """
+
+            if (
+                order.country_code != 'IN'
+                # Partner's FP takes precedence through super
+                or order.partner_shipping_id.property_account_position_id
+                or order.partner_id.property_account_position_id
+            ):
+                return False
+            elif order.l10n_in_gst_treatment == 'special_economic_zone':
+                # Special Economic Zone
+                return self.env.ref('l10n_in.state_in_oc')
+            return order.l10n_in_sale_state_id
+
+        for state_id, orders in self.grouped(_get_fiscal_state).items():
+            if state_id:
+                virtual_partner = self.env['res.partner'].new({
+                    'state_id': state_id.id,
+                    'country_id': state_id.country_id.id,
+                })
+                orders.fiscal_position_id = self.env['account.fiscal.position']._get_fiscal_position(virtual_partner)
+            else:
+                super(SaleOrder, orders)._compute_fiscal_position_id()
 
     @api.depends('partner_id')
     def _compute_l10n_in_gst_treatment(self):
