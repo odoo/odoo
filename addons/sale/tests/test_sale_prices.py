483b644c43d5a333c4f30d5907b846dd37c88294
Pieter Claeys (clpi) <clpi@odoo.com>
2025-02-11 15:49:22 +0100

    [IMP] product: product administrator group
    Remove all write/create/delete permission on product related models from
    the different 'manager' roles in different modules.
    Instead, we expect people to be assigned the product administrator group
    to have these permissions.
    
    Goal is to allow for more granular control over who has access over
    product models and decouple app specific managers from product managers.
    
    ENT PR: https://github.com/odoo/enterprise/pull/79091
    Upgrade PR: https://github.com/odoo/upgrade/pull/7346
    task-4578961

--- addons/sale/tests/test_sale_prices.py
+++ addons/sale/tests/test_sale_prices.py
@@ -370,6 +370,7 @@ class TestSalePrices(SaleCommon):
                 self.env.ref('product.group_product_manager')
             ),
         })
+        user_in_other_company.group_ids += self.env.ref("product.group_product_manager")
         with mute_logger('odoo.models.unlink'):
             self.env['res.currency.rate'].search([]).unlink()
         self.env['res.currency.rate'].create({
