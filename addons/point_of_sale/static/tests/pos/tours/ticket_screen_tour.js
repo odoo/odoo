15a8b9ba9489bb4587fa16f5abb503288f1c1e38
manv-afk <manv@odoo.com>
2025-02-12 14:18:51 +0000

    [IMP] point_of_sale: test load ticket screen offline
    - Since the fix done in thix PR (https://github.com/odoo/odoo/pull/196810) we can access the ticket screen when offline. This PR is just adding steps in existing tour to test this feature.
    
    closes odoo/odoo#197611
    
    Task-id: 4550978
    X-original-commit: d4444c7043c5b387691645a74d7ad3a8e1b70f36
    Signed-off-by: Manu Vaillant (manv) <manv@odoo.com>

--- addons/point_of_sale/static/tests/pos/tours/ticket_screen_tour.js
+++ addons/point_of_sale/static/tests/pos/tours/ticket_screen_tour.js
@@ -9,6 +9,7 @@ import * as Chrome from "@point_of_sale/../tests/pos/tours/utils/chrome_util";
 import * as Dialog from "@point_of_sale/../tests/generic_helpers/dialog_util";
 import { inLeftSide } from "@point_of_sale/../tests/pos/tours/utils/common";
 import { registry } from "@web/core/registry";
+import * as OfflineUtil from "@point_of_sale/../tests/generic_helpers/offline_util";
 
 registry.category("web_tour.tours").add("TicketScreenTour", {
     checkDelay: 50,
@@ -16,7 +17,10 @@ registry.category("web_tour.tours").add("TicketScreenTour", {
         [
             Chrome.startPoS(),
             Dialog.confirm("Open Register"),
+            OfflineUtil.setOfflineMode(),
             Chrome.clickOrders(),
+            Dialog.confirm("Continue with limited functionality"),
+            OfflineUtil.setOnlineMode(),
             Chrome.createFloatingOrder(),
             ProductScreen.addOrderline("Desk Pad", "1", "3"),
             Chrome.clickOrders(),
