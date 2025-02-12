52f6d7e446984fae053d718b6f139a744ea4866d
Arnold Moyaux <arm@odoo.com>
2025-02-12 15:41:27 +0100

    [IMP] purchase: Better purchase comparison
    The purchase history button is on each `purchase.order.line`.
    But it overcharge the view and add a default search on the
    current vendor.
    
    We removed it to a stat button group by product.
    It also add a decoration-info on the line in the current
    purchase.order to easily find it.
    
    Part-of: odoo/odoo#197211
    Related: odoo/upgrade#7213
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/purchase/models/purchase_order_line.py
+++ addons/purchase/models/purchase_order_line.py
@@ -405,20 +405,8 @@ class PurchaseOrderLine(models.Model):
         order = self.env['purchase.order'].browse(self.env.context.get('order_id'))
         return order.with_context(child_field='order_line').action_add_from_catalog()
 
-    def action_purchase_history(self):
-        self.ensure_one()
-        action = self.env["ir.actions.actions"]._for_xml_id("purchase.action_purchase_history")
-        action['domain'] = [('state', 'in', ['purchase', 'done']), ('product_id', '=', self.product_id.id)]
-        action['display_name'] = _("Purchase History for %s", self.product_id.display_name)
-        action['context'] = {
-            'search_default_partner_id': self.partner_id.id
-        }
-
-        return action
-
     def _suggest_quantity(self):
-        '''
-        Suggest a minimal quantity based on the seller
+        ''' Suggest a minimal quantity based on the seller
         '''
         if not self.product_id:
             return
