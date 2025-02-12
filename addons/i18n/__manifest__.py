d4f5146eb4bd91d3b7ba6d9e6819effc52978a04
Paolo Gatti <pgi@odoo.com>
2025-02-12 15:23:45 +0100

    [WIP]

--- addons/i18n/__manifest__.py
+++ addons/i18n/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'I18n',
     'icon': '/account/static/description/l10n.png',
-    'version': '2.1',
+    'version': '0.1',
     'category': 'Technical/Localizations',
     'description': """Import, export, setup internationalization files""",
     'depends': [
