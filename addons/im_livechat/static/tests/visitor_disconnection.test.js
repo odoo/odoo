ccc17b7aadf09e92129b2c9784eefcc1a3d142bd
Giorgio Tocco (gito) <gito@odoo.com>
2025-02-12 11:26:04 +0000

    [IMP] im_livechat, mail, *: notify visitor left the conversation
    * = test_discuss_full
    
    This commit introduces a banner, shown when the visitor status is no
    more `online`, alerting the operator that the visitor might have
    abandoned the conversation.
    
    task-4452913
    
    closes odoo/odoo#197655
    
    X-original-commit: 0f6c64b787775e8d92c0ce0d7e72e54a1be9f539
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

---
+++ addons/im_livechat/static/tests/visitor_disconnection.test.js
@@ -0,0 +1,75 @@
+import { Command, patchWithCleanup, serverState } from "@web/../tests/web_test_helpers";
+import { defineLivechatModels } from "@im_livechat/../tests/livechat_test_helpers";
+import {
+    contains,
+    setupChatHub,
+    start,
+    startServer,
+    click,
+} from "@mail/../tests/mail_test_helpers";
+import { describe, test } from "@odoo/hoot";
+import { mockDate } from "@odoo/hoot-mock";
+import { Persona } from "@mail/core/common/persona_model";
+import { Thread } from "@mail/core/common/thread";
+
+describe.current.tags("desktop");
+defineLivechatModels();
+
+test("Visitor going offline shows disconnection banner to operator", async () => {
+    patchWithCleanup(Persona, { IM_STATUS_DEBOUNCE_DELAY: 0 });
+    patchWithCleanup(Thread.prototype, {
+        setup() {
+            super.setup();
+            this.IM_STATUS_DELAY = 0;
+        },
+    });
+    const pyEnv = await startServer();
+    pyEnv["res.users"].write([serverState.userId], {
+        groups_id: pyEnv["res.groups"]
+            .search_read([["id", "=", serverState.groupLivechatId]])
+            .map(({ id }) => id),
+    });
+    const guestId = pyEnv["mail.guest"].create({ name: "Visitor", im_status: "online" });
+    const livechatChannelId = pyEnv["im_livechat.channel"].create({
+        name: "HR",
+        user_ids: [serverState.userId],
+    });
+    const channel_id = pyEnv["discuss.channel"].create({
+        channel_type: "livechat",
+        channel_member_ids: [
+            Command.create({ partner_id: serverState.partnerId }),
+            Command.create({ guest_id: guestId }),
+        ],
+        livechat_active: true,
+        livechat_channel_id: livechatChannelId,
+        livechat_operator_id: serverState.partnerId,
+        create_uid: serverState.publicUserId,
+    });
+    setupChatHub({ opened: [channel_id] });
+    await start();
+    await contains(".o-mail-ChatWindow");
+    mockDate("2025-01-01 12:00:00", +1);
+    pyEnv["mail.guest"].write(guestId, { im_status: "offline" });
+    pyEnv["bus.bus"]._sendone(guestId, "bus.bus/im_status_updated", {
+        guest_id: guestId,
+        im_status: "offline",
+    });
+    await contains(".o-livechat-VisitorDisconnected", {
+        text: "Visitor is disconnected since 1:00 PM",
+    });
+    mockDate("2025-01-02 12:00:00", +1);
+    await click("button[title*='Fold']");
+    await click(".o-mail-ChatBubble");
+    await contains(".o-livechat-VisitorDisconnected", {
+        text: "Visitor is disconnected since yesterday at 1:00 PM",
+    });
+    mockDate("2025-01-05 12:00:00", +1);
+    await click("button[title*='Fold']");
+    await click(".o-mail-ChatBubble");
+    await contains(".o-livechat-VisitorDisconnected", { text: `Visitor is disconnected` });
+    pyEnv["bus.bus"]._sendone(guestId, "bus.bus/im_status_updated", {
+        guest_id: guestId,
+        im_status: "online",
+    });
+    await contains(".o-livechat-VisitorDisconnected", { count: 0 });
+});
