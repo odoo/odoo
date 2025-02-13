bf3c21092a534c4f9ebbf547a3d51a659ccdeefa
yaso <yaso@odoo.com>
2025-02-13 17:32:34 +0000

    [IMP] hw_drivers: add logs for printing
    Following the ticket opw-4523417 there is a lack of logging related to
    printing in hw_drivers module which makes it difficult to analyse the
    issued on the client's side, especially on Windows IoT
    
    This PR adds
    1) some logs upon receiving of a printing request via longpolling route
    2) some logs when the methods of printing are being called
    3) some logs when these methods have finished their job
    
    Since for printing the Odoo database isn't notified of a succeful
    printing this is very much needed
    
    X-original-commit: 113feaf94566f649acf36a220d34af2f1a29c5c1

--- addons/hw_drivers/iot_handlers/drivers/PrinterDriver_W.py
+++ addons/hw_drivers/iot_handlers/drivers/PrinterDriver_W.py
@@ -129,6 +129,8 @@ class PrinterDriver(Driver):
         ghostscript.Ghostscript(*args)
 
     def print_receipt(self, data):
+        _logger.debug("print_receipt called for printer %s", self.device_name)
+
         receipt = b64decode(data['receipt'])
         im = Image.open(io.BytesIO(receipt))
 
@@ -158,17 +160,27 @@ class PrinterDriver(Driver):
 
     def open_cashbox(self, data):
         """Sends a signal to the current printer to open the connected cashbox."""
+        _logger.debug("open_cashbox called for printer %s", self.device_name)
+        
         commands = RECEIPT_PRINTER_COMMANDS[self.receipt_protocol]
         for drawer in commands['drawers']:
             self.print_raw(drawer)
 
     def _action_default(self, data):
+        _logger.debug("_action_default called for printer %s", self.device_name)
+
         document = b64decode(data['document'])
         mimetype = guess_mimetype(document)
         if mimetype == 'application/pdf':
             self.print_report(document)
         else:
             self.print_raw(document)
+<<<<<<< saas-17.4
         send_to_controller(self.connection_type, {'print_id': data['print_id'], 'device_identifier': self.device_identifier})
+||||||| de68bd5321a74974cddb66508ac7aae4a4079a5f
+=======
+        _logger.debug("_action_default finished with mimetype %s for printer %s", mimetype, self.device_name)
+
+>>>>>>> f68c7b16c94f2335517b8bdf25a14c17a1cff57a
 
 proxy_drivers['printer'] = PrinterDriver
