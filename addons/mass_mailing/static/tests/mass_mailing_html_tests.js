907278a4a32b4a32aa352da058db1317ad727456
Panagiotis Kyriakou <paky@odoo.com>
2025-02-13 14:33:46 +0100

    [IMP] *: prepare different modules for the new ChatGPT integration
    * = mail,html_editor,mass_mailing,account
    
    In this commit we remove or change some code modules in order to facilitate
    the new ChatGPT integration.
    
    In the mail module, we create methods for styling and action conditions
    so we can extend them in the new ai module and change their functionality
    for the AI demands. We also completely remove the mail_composer_chatgpt
    component, which will now be part of the AI module and not something
    standard in mail. We also remove mentions of the component from the scheduled
    message form view and the compose message wizard. We add them both in the
    AI module. The same is done in the account module, the component is used in
    a wizard - we remove it and add it back in a bridge module between AI and
    account.
    
    In the html_editor module we remove the chatgpt plugin so we can add it
    back in the AI module, in which it will now belong. We edit some tests that
    rely on the fact that it was there as well as edit the html_field class to
    return some extra information needed for the AI functions.
    
    Finally we edit a legacy test in mass mailing, mocking the store service
    because it was failing - the store service is required by the HTML editor now.
    
    task-4526290

--- addons/mass_mailing/static/tests/mass_mailing_html_tests.js
+++ addons/mass_mailing/static/tests/mass_mailing_html_tests.js
@@ -1,5 +1,6 @@
 import weTestUtils from "@web_editor/../tests/test_utils";
 import { makeView, setupViewRegistries } from "@web/../tests/views/helpers";
+import { registry } from "@web/core/registry";
 import {
     editInput,
     getFixture,
@@ -13,10 +14,21 @@ import { Wysiwyg } from '@web_editor/js/wysiwyg/wysiwyg';
 let serverData;
 let fixture;
 
+function makeFakeMailStoreService() {
+    return {
+        start: (env) => ({}),
+    };
+}
+
+function loadServices() {
+    registry.category("services").add("mail.store", makeFakeMailStoreService());
+}
+
 QUnit.module('mass_mailing', {}, function () {
 QUnit.module('field html', (hooks) => {
     hooks.beforeEach(() => {
         fixture = getFixture();
+        loadServices();
         const models = weTestUtils.wysiwygData({
             'mailing.mailing': {
                 fields: {
