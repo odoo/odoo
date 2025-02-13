38282b43c5eaacc57f5f38d6a2159156ace2e31f
Valeriya(vchu) <vchu@odoo.com>
2025-02-13 07:49:38 +0000

    [FIX] website_sale_collect: fix test import
    Commit 2d4be3a added tests but import was missed.
    
    X-original-commit: 6d30ba4a387de42101c50e04ac4d04b8b2951624

--- addons/website_sale_collect/tests/__init__.py
+++ addons/website_sale_collect/tests/__init__.py
@@ -5,4 +5,9 @@ from . import test_in_store_delivery
 from . import test_payment_provider
 from . import test_payment_transaction
 from . import test_sale_order
+<<<<<<< master
 from . import test_website
+||||||| adbc1f5fe6e2d90b6224125c25f9154463cdbc2c
+=======
+from . import test_website
+>>>>>>> 53ca584d298c0fb66d2064086d54a7eb093d5d39
