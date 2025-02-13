0c3690597464ca189223f0121a6676f315ed8541
Jérémy Hennecart (jeh) <jeh@odoo.com>
2025-02-13 16:07:35 +0000

    [IMP] website_event: duplicate event page
    Duplicate the content of the event introduction and location menu
    when duplicating an event record.
    
    When copying the views, we get the last one modified that matches the template key
    and the website_id as it's possible to have multiple views with the same key on
    different website. This view is then used and assiociated to the new website event menu.
    This is done to have the same behavior of event page renderer which is based
    on the key and the website.
    
    Also, the key of the view linked to the website_event_menu use a '-t{timestamp}'
    at the end. This is done to have an unique key after the copy and to make sure the
    _is_active() is computed correctly.
    Indeed, if we were to rely on the get_unique_key() for the view linked to an url,
    we could end up with multiple active menus at the same time because of the unslug_url.
    For example:
    /event-1/page/intro-1 => /1/page/1 and /event-1/page/location-1 => /1/page/1
    with the 't{timestamp}}' the last part is no longer misstaken as a record.
    
    task-4526153
    
    X-original-commit: fcf651f1663778c73fee808b7ac1dc4b2f9efb5d

--- addons/website_event/models/website_event_menu.py
+++ addons/website_event/models/website_event_menu.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from datetime import datetime
+
+from odoo import api, fields, models
 
 
 class WebsiteEventMenu(models.Model):
@@ -19,6 +21,49 @@ class WebsiteEventMenu(models.Model):
          ('location', 'Location'),
          ('register', 'Info'),
         ], string="Menu Type", required=True)
+
+    def copy(self, default=None):
+        new_menus = super().copy(default=default)
+        for new_menu, old_menu in zip(new_menus, self):
+            if not old_menu.view_id:
+                continue
+            # Get the last view modified based on the key and the website of the event
+            # as multiple views with the same key can exist with different website.
+            view = self.env['ir.ui.view'].sudo().search([
+                ('key', '=', old_menu.view_id.key),
+                ('website_id', '=?', old_menu.event_id.website_id.id)
+            ], order='write_date DESC', limit=1)
+            # The "-t{timestamp}" at the end of the key is needed when _is_active() is called on the menu.
+            # Without it, the unslug url can think of the copied view key as a record and keep only the number
+            # which can lead to issue where we get multiple menus active at the same time. Also that way, we ensure
+            # that the key is unique.
+            new_menu.view_id = view.copy({
+                'key': f"{old_menu.view_id.key}-t{int(datetime.now().timestamp())}",
+                'website_id': view.website_id.id,
+            })
+            self._copy_children_views(new_menu.view_id, view.inherit_children_ids, old_menu.event_id.website_id.id)
+            new_menu.menu_id = old_menu.menu_id.copy({
+                'url': f"/event/{self.env['ir.http']._slug(new_menu.event_id)}/page/{new_menu.view_id.key.split('.')[-1]}"
+            })
+        return new_menus
+
+    @api.model
+    def _copy_children_views(self, new_view, children_views, website_id):
+        """ Duplicate the children associated in the new view """
+        new_view.ensure_one()
+        for child_view in children_views:
+            view_info = child_view.key.split('.')
+            # Get the last view modified based on the key and the website of the event
+            view = self.env['ir.ui.view'].sudo().search([
+                ('key', '=', child_view.key),
+                ('website_id', '=?', website_id)
+            ], order='write_date DESC', limit=1)
+            new_child_view = view.copy({
+                'key': self.env['website'].get_unique_key(view_info[-1], view_info[0]),
+                'inherit_id': new_view.id,
+                'website_id': view.website_id.id,
+            })
+            self._copy_children_views(new_child_view, view.inherit_children_ids, website_id)
 
     def unlink(self):
         self.view_id.sudo().unlink()
