06f67363e2ef5e92ff24cd54859f45d989ac46c6
Julien Castiaux <juc@odoo.com>
2025-02-14 16:16:33 +0100

    [IMP] web: /json/2/<model>/<method>
    A RPC endpoint that supports API Key.
    
    Request:
    
    	POST /json/2/res.partner/read HTTP/1.1
    	Host: <yourcompany>.odoo.com
    	Authorization: bearer <api-key>
    	Content-Type: application/json
    
    	{
    	    "ids": [3, 4],
    	    "kwargs": {
    	        "fields": ["name"]
    	    },
    	    "context": {}
    	}
    
    Response:
    
    	HTTP/1.1 200 OK
    	Content-Type: application/json; charset=utf-8
    
    	[
    	    {
    		"id": 3,
    		"name": "Mitchell Admin"
    	    },
    	    {
    		"id": 4,
    		"name": "Public user"
    	    }
    	]
    
    Error responses use proper HTTP verb!
    
    	HTTP/1.1 422 UNPROCESSABLE ENTITY
    	Content-Type: application/json; charset=utf-8
    
    	"got an unexpected keyword argument 'badparameter'"
    
    task-4572591
    
    Part-of: odoo/odoo#197801
    Related: odoo/enterprise#80365
    Signed-off-by: Julien Castiaux (juc) <juc@odoo.com>

---
+++ addons/web/controllers/json2.py
@@ -0,0 +1,151 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import inspect
+import logging
+from collections.abc import Callable, Mapping, Sequence
+from http import HTTPStatus
+from typing import Any
+
+from werkzeug.exceptions import (
+    BadRequest,
+    HTTPException,
+    MethodNotAllowed,
+    NotFound,
+    UnprocessableEntity,
+)
+
+from odoo import http
+from odoo.exceptions import UserError
+from odoo.http import request
+from odoo.models import BaseModel
+from odoo.service.model import get_public_method
+from odoo.tools import frozendict
+
+_logger = logging.getLogger(__name__)
+
+SESSION_EXPIRED_MSG = """\
+Session Expired
+
+Surely this is because you are using a session_id cookie but that\
+ session does no longer exist.
+
+You can get a renewed cookie via the html form at /web/login?db= or the\
+ JSONRPC endpoint at /web/session/authenticate with method "call"\
+ and params "login", "password" and "db".
+"""
+
+
+class Json2RpcDispatcher(http.Dispatcher):
+    routing_type = '/json/2/rpc'
+
+    @classmethod
+    def is_compatible_with(cls, request):
+        return request.httprequest.mimetype == 'application/json'
+
+    def dispatch(self, endpoint, args):
+        self.request.params = dict(args)
+        if self.request.httprequest.content_length:
+            try:
+                self.request.params |= self.request.get_json_data()
+            except ValueError as exc:
+                e = f"could not parse the body as json: {exc.args[0]}"
+                raise BadRequest(e) from exc
+            except TypeError as exc:
+                e = "could not parse the body, expecting a json object"
+                raise BadRequest(e) from exc
+        if self.request.db:
+            result = self.request.registry['ir.http']._dispatch(endpoint)
+        else:
+            result = endpoint(**self.request.params)
+        if isinstance(result, http.Response):
+            return result
+        return self.request.make_json_response(result)
+
+    def handle_error(self, exc: Exception) -> Callable:
+        headers = None
+        cookies = None
+        if isinstance(exc, UserError):
+            msg = exc.args[0]
+            status = exc.http_code
+        elif isinstance(exc, HTTPException):
+            if res := exc.response:
+                ct = res.headers.get('Content-Type', 'application/octet-stream')
+                if 'json' in ct or not ct.startswith('text/'):
+                    return exc
+                msg = res.get_data(as_text=True)
+                status = exc.code
+                headers = res.headers
+            else:
+                msg = exc.description
+                status = exc.code
+        elif isinstance(exc, http.SessionExpiredException):
+            # needed by auth='user' but not by auth='bearer'
+            msg = SESSION_EXPIRED_MSG
+            status = HTTPStatus.FORBIDDEN
+        else:
+            msg = "internal server error"
+            status = HTTPStatus.INTERNAL_SERVER_ERROR
+        return self.request.make_json_response(msg, headers, cookies, status)
+
+
+class WebJson2Controller(http.Controller):
+    def _web_json_2_rpc_readonly(self, rule, args):
+        try:
+            model_name = args['model']
+            method_name = args['method']
+            Model = request.registry[model_name]
+        except KeyError:
+            # no need of a read/write cursor to send a 404 http error
+            return True
+        for cls in Model.mro():
+            method = getattr(cls, method_name, None)
+            if method is not None and hasattr(method, '_readonly'):
+                return method._readonly
+        return False
+
+    @http.route(
+        '/json/2/<model>/<method>',
+        # methods=['POST'], # must accept all methods, otherwise /json/<path:subpath> takes over
+        auth='bearer',
+        type='/json/2/rpc',
+        readonly=_web_json_2_rpc_readonly,
+        save_session=False,
+    )
+    def web_json_2_rpc(
+        self,
+        model: str,
+        method: str,
+        ids: Sequence[int] = (),
+        args: Sequence[Any] = (),
+        kwargs: Mapping[str, Any] = frozendict(),
+        context: Mapping[str, Any] = frozendict(),
+    ):
+        if request.httprequest.method != 'POST':
+            raise MethodNotAllowed()
+
+        try:
+            Model = request.env[model].with_context(context)
+        except KeyError as exc:
+            e = f"the model {model!r} does not exist"
+            raise NotFound(e) from exc
+
+        try:
+            func = get_public_method(Model, method)
+        except AttributeError as exc:
+            raise NotFound(exc.args[0]) from exc
+        if hasattr(func, '_api_model') and ids:
+            e = f"cannot call {model}.{method} with ids"
+            raise UnprocessableEntity(e)
+
+        records = Model.browse(ids)
+        signature = inspect.signature(func)
+        try:
+            signature.bind(records, *args, **kwargs)
+        except TypeError as exc:
+            raise UnprocessableEntity(exc.args[0])
+
+        result = func(records, *args, **kwargs)
+        if isinstance(result, BaseModel):
+            result = result.ids
+
+        return result
---
+++ odoo/addons/test_http/tests/test_webjson2.py
@@ -0,0 +1,110 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from datetime import datetime, timedelta
+from http import HTTPStatus
+
+from odoo.tests import Like, get_db_name, new_test_user, tagged
+
+from .test_common import TestHttpBase
+from odoo.addons.test_http.controllers import CT_JSON
+
+SEC_FETCH_HEADERS = {
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
+}
+
+
+@tagged('-at_install', 'post_install')
+class TestHttpWebJson_2(TestHttpBase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.jackoneill = new_test_user(cls.env, 'jackoneill', context={'lang': 'en_US'})
+        cls.jackoneill = cls.jackoneill.with_user(cls.jackoneill)
+        key = cls.jackoneill.env['res.users.apikeys']._generate(
+            scope='rpc', name='test', expiration_date=datetime.now() + timedelta(days=0.5))
+        cls.bearer_header = {"Authorization": f"Bearer {key}"}
+
+    def test_webjson2_multi_db(self):
+        res = self.multidb_url_open(
+            '/json/2/res.partner/search',
+            data=r'{"kwargs": {"domain": []}}',
+            headers=CT_JSON | self.bearer_header,
+            dblist=(get_db_name(), 'another-database'),
+        )
+        self.assertIn("The requested URL was not found on the server.", res.text)
+        self.assertEqual(res.status_code, HTTPStatus.NOT_FOUND)
+        self.assertEqual(res.headers.get('Content-Type'), 'text/html; charset=utf-8')
+
+    def test_webjson2_bad_content_type(self):
+        res = self.db_url_open(
+            # application/x-www-form-urlencoded
+            '/json/2/res.users/search',
+            data={"kwargs": {"domain": []}},
+            headers=self.bearer_header,
+        )
+        self.assertEqual(res.text, Like("""
+            ...Request inferred type is compatible with...http...but...
+            /json/2...is type=...json...
+        """))
+        self.assertEqual(res.status_code, HTTPStatus.UNSUPPORTED_MEDIA_TYPE)
+        self.assertEqual(res.headers.get('Content-Type'), 'text/html; charset=utf-8')
+
+    def test_webjson2_bad_data(self):
+        res = self.db_url_open(
+            '/json/2/res.users/search',
+            data=r"not json",
+            headers=CT_JSON | self.bearer_header,
+        )
+        self.assertEqual(res.text, '"could not parse the body as json: Expecting value: line 1 column 1 (char 0)"')
+        self.assertEqual(res.status_code, HTTPStatus.BAD_REQUEST)
+        self.assertEqual(res.headers.get('Content-Type'), 'application/json; charset=utf-8')
+
+    def test_webjson2_not_json_object(self):
+        res = self.db_url_open(
+            '/json/2/res.users/search',
+            data=r'null',
+            headers=CT_JSON | self.bearer_header,
+        )
+        self.assertEqual(res.text, '''"could not parse the body, expecting a json object"''')
+        self.assertEqual(res.status_code, HTTPStatus.BAD_REQUEST)
+        self.assertEqual(res.headers.get('Content-Type'), 'application/json; charset=utf-8')
+
+    def test_webjson2_missing_auth(self):
+        res = self.db_url_open(
+            '/json/2/res.users/search',
+            data=r'{"kwargs": {"domain": []}}',
+            headers=CT_JSON,
+        )
+        self.assertEqual(res.status_code, HTTPStatus.UNAUTHORIZED)
+
+    def test_webjson2_missing_argument(self):
+        res = self.db_url_open(
+            '/json/2/res.users/search',
+            data=r'{}',
+            headers=CT_JSON | self.bearer_header,
+        )
+        self.assertEqual(res.text, '''"missing a required argument: 'domain'"''')
+        self.assertEqual(res.status_code, HTTPStatus.UNPROCESSABLE_ENTITY)
+        self.assertEqual(res.headers.get('Content-Type'), 'application/json; charset=utf-8')
+
+    def test_webjson2_good(self):
+        res = self.db_url_open(
+            '/json/2/res.users/search',
+            data=r'{"kwargs": {"domain": [["id","=",%d]]}}' % self.jackoneill.id,
+            headers=CT_JSON | self.bearer_header,
+        )
+        self.assertEqual(res.text, f"[{self.jackoneill.id}]")
+        self.assertEqual(res.status_code, HTTPStatus.OK)
+        self.assertEqual(res.headers.get('Content-Type'), 'application/json; charset=utf-8')
+
+    def test_webjson2_api_model(self):
+        res = self.db_url_open(
+            '/json/2/res.users/create',
+            data=r'{"ids": [0], "args": [{}]}',
+            headers=CT_JSON | self.bearer_header,
+        )
+        self.assertEqual(res.text, '''"cannot call res.users.create with ids"''')
+        self.assertEqual(res.status_code, HTTPStatus.UNPROCESSABLE_ENTITY)
+        self.assertEqual(res.headers.get('Content-Type'), 'application/json; charset=utf-8')
