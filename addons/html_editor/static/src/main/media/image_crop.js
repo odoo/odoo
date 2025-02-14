c837886681f67e2568409e06ea10a9b612a65dc8
Antoine Guenet <age@odoo.com>
2025-02-14 13:07:35 +0100

    [FIX] html_editor: prevent traceback on crop image at bottom of viewport
    When trying to crop an image at the bottom of the viewport, an async
    call was waiting forever, causing the instance of cropper not to be
    defined. This is because we forced scrolling down even though it wasn't
    necessary, then waiting for scrolling to end (which never happened since
    it never even started).
    
    task-4493009

--- addons/html_editor/static/src/main/media/image_crop.js
+++ addons/html_editor/static/src/main/media/image_crop.js
@@ -256,7 +256,6 @@ export class ImageCrop extends Component {
         if (rect.top < viewportTop || viewportBottom - rect.bottom < 100) {
             await scrollTo(this.media, {
                 behavior: "smooth",
-                isAnchor: true,
                 ...(scrollable && { scrollable }),
             });
         }
