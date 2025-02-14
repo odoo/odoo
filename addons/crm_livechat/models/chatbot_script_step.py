c5bf56929409380745dfaefda791a0915293e643
aamo-odoo <aamo@odoo.com>
2025-02-14 16:06:41 +0530

    [FIX] crm_livechat: saving operator_partner value before processing step
    This error occurs when attempting to create a lead and forward it from the live
    chat.
    
    Traceback : StopIteration: null
      File "odoo/http.py", line 2420, in __call__
        response = request._serve_db()
      File "odoo/http.py", line 1946, in _serve_db
        return self._transactioning(
      File "odoo/http.py", line 2010, in _transactioning
        return service_model.retrying(func, env=self.env)
      File "odoo/service/model.py", line 137, in retrying
        result = func()
      File "odoo/http.py", line 1977, in _serve_ir_http
        response = self.dispatcher.dispatch(rule.endpoint, args)
      File "odoo/http.py", line 2228, in dispatch
        result = self.request.registry['ir.http']._dispatch(endpoint)
      File "odoo/addons/base/models/ir_http.py", line 335, in _dispatch
        result = endpoint(**request.params)
      File "odoo/http.py", line 741, in route_wrapper
        result = endpoint(self, *args, **params_ok)
      File "addons/mail/models/discuss/mail_guest.py", line 38, in wrapper
        return func(self, *args, **kwargs)
      File "addons/im_livechat/controllers/chatbot.py", line 69, in chatbot_trigger_step
        posted_message = next_step._process_step(discuss_channel)
      File "addons/crm_livechat/models/chatbot_script_step.py", line 42, in _process_step
        return self._process_step_create_lead_and_forward(discuss_channel)
      File "addons/crm_livechat/models/chatbot_script_step.py", line 107, in _process_step_create_lead_and_forward
        user = next(user for user in users if user.partner_id == operator_partner)
    
    The error occurs at [1] because both 'users' and 'teams' are empty. As a result,
    'next()' attempts to retrieve an item from an empty iterator, leading to a
    'StopIteration' exception.
    
    This issue arises when the bot assigned in the script is changed during an
    active session. Consequently, 'operator_partner' refers to the old bot
    (the one configured at the start of the session) rather than the new one.
    This discrepancy causes the condition to be evaluated as True, leading to the
    error.
    
    This commit resolves the error by saving the value of 'operator_partner'
    before calling '_process_step_forward_operator'. This ensures that
    'operator_partner' remains the same until the end of the session.
    
    [1]- https://github.com/odoo/odoo/blob/db62ec3b72b30b01ff04b067cf2c235bd9a8411e/addons/crm_livechat/models/chatbot_script_step.py#L101-L109
    
    sentry-6296911936

--- addons/crm_livechat/models/chatbot_script_step.py
+++ addons/crm_livechat/models/chatbot_script_step.py
@@ -101,8 +101,9 @@ class ChatbotScriptStep(models.Model):
         users = discuss_channel.livechat_channel_id.sudo()._get_available_operators_by_livechat_channel(
             self.env["res.users"].browse(assignable_user_ids)
         )[discuss_channel.livechat_channel_id]
-        message = self._process_step_forward_operator(discuss_channel, users=users)
         operator_partner = discuss_channel.livechat_operator_id
+        message = self._process_step_forward_operator(discuss_channel, users=users)
+
         if operator_partner != self.chatbot_script_id.operator_partner_id:
             user = next(user for user in users if user.partner_id == operator_partner)
             lead.user_id = user
