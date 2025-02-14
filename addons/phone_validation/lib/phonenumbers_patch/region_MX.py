8287489a3d7e2087c29a019fcad05b3d3c7c3db0
Antoine (ande) <ande@odoo.com>
2025-02-14 20:22:16 +0100

    [FIX] phone_validation: phone numbers in mexico
    Current behaviour:
    Mexican phone numbers are not managed correctly
    following the 2019 changes in Mexico.
    (Removing a 1 in phone numbers)
    
    Cause of the issue:
    The phonenumbers library removing support of mexican phone numbers
    starting with 1 in 8.13.38
    The Whatsapp API still using the mexican phone numbers starting with 1
    
    Fixes:
    Patched the phonenumbers library, removing 1
    at the right place for mexican phone numbers.
    Similar to: https://github.com/odoo/odoo/commit/53885e41867653ad45ca3aef55886c280240b76a
    
    Changed the forcing to international format when parsing, to make
    sure the patches are applied before detecting an issue.
    Similar to: https://github.com/odoo/odoo/commit/29a4de8e29a2e330b8e5993c9fde23c69a2eff5e
    
    opw-4473528

---
+++ addons/phone_validation/lib/phonenumbers_patch/region_MX.py
@@ -0,0 +1,18 @@
+"""Auto-generated file, do not edit by hand. MX metadata"""
+from ..phonemetadata import NumberFormat, PhoneNumberDesc, PhoneMetadata
+
+PHONE_METADATA_MX = PhoneMetadata(id='MX', country_code=52, international_prefix='0[09]',
+    general_desc=PhoneNumberDesc(national_number_pattern='[2-9]\\d{9}', possible_length=(10,), possible_length_local_only=(7, 8)),
+    fixed_line=PhoneNumberDesc(national_number_pattern='657[12]\\d{6}|(?:2(?:0[01]|2\\d|3[1-35-8]|4[13-9]|7[1-689]|8[1-578]|9[467])|3(?:1[1-79]|[2458][1-9]|3\\d|7[1-8]|9[1-5])|4(?:1[1-57-9]|[267][1-9]|3[1-8]|[45]\\d|8[1-35-9]|9[2-689])|5(?:[56]\\d|88|9[1-79])|6(?:1[2-68]|[2-4][1-9]|5[1-3689]|6[0-57-9]|7[1-7]|8[67]|9[4-8])|7(?:[1346][1-9]|[27]\\d|5[13-9]|8[1-69]|9[17])|8(?:1\\d|2[13-689]|3[1-6]|4[124-6]|6[1246-9]|7[0-378]|9[12479])|9(?:1[346-9]|2[1-4]|3[2-46-8]|5[1348]|[69]\\d|7[12]|8[1-8]))\\d{7}', example_number='2001234567', possible_length=(10,), possible_length_local_only=(7, 8)),
+    mobile=PhoneNumberDesc(national_number_pattern='657[12]\\d{6}|(?:2(?:2\\d|3[1-35-8]|4[13-9]|7[1-689]|8[1-578]|9[467])|3(?:1[1-79]|[2458][1-9]|3\\d|7[1-8]|9[1-5])|4(?:1[1-57-9]|[267][1-9]|3[1-8]|[45]\\d|8[1-35-9]|9[2-689])|5(?:[56]\\d|88|9[1-79])|6(?:1[2-68]|[2-4][1-9]|5[1-3689]|6[0-57-9]|7[1-7]|8[67]|9[4-8])|7(?:[1346][1-9]|[27]\\d|5[13-9]|8[1-69]|9[17])|8(?:1\\d|2[13-689]|3[1-6]|4[124-6]|6[1246-9]|7[0-378]|9[12479])|9(?:1[346-9]|2[1-4]|3[2-46-8]|5[1348]|[69]\\d|7[12]|8[1-8]))\\d{7}', example_number='2221234567', possible_length=(10,), possible_length_local_only=(7, 8)),
+    toll_free=PhoneNumberDesc(national_number_pattern='8(?:00|88)\\d{7}', example_number='8001234567', possible_length=(10,)),
+    premium_rate=PhoneNumberDesc(national_number_pattern='900\\d{7}', example_number='9001234567', possible_length=(10,)),
+    shared_cost=PhoneNumberDesc(national_number_pattern='300\\d{7}', example_number='3001234567', possible_length=(10,)),
+    personal_number=PhoneNumberDesc(national_number_pattern='500\\d{7}', example_number='5001234567', possible_length=(10,)),
+    preferred_international_prefix='00',
+    number_format=[NumberFormat(pattern='(\\d{5})', format='\\1', leading_digits_pattern=['53']),
+        NumberFormat(pattern='(\\d{2})(\\d{4})(\\d{4})', format='\\1 \\2 \\3', leading_digits_pattern=['33|5[56]|81']),
+        NumberFormat(pattern='(\\d{3})(\\d{3})(\\d{4})', format='\\1 \\2 \\3', leading_digits_pattern=['[2-9]'])],
+    intl_number_format=[NumberFormat(pattern='(\\d{2})(\\d{4})(\\d{4})', format='\\1 \\2 \\3', leading_digits_pattern=['33|5[56]|81']),
+        NumberFormat(pattern='(\\d{3})(\\d{3})(\\d{4})', format='\\1 \\2 \\3', leading_digits_pattern=['[2-9]'])],
+    mobile_number_portable_region=True)
