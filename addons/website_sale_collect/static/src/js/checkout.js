7c58a7446022eca80d78c315c130c59a60818c79
Valeriya(vchu) <vchu@odoo.com>
2025-02-14 07:13:59 +0000

    [FIX] website_sale_collect: skip if radio null
    Steps to reproduce:
    1) Install Click & Collect
    2) Configure a standard delivery to deliver only to Belgium and publish
    3) Unpublish the others
    4) Go to /shop page add a storable product
    5) Proceed to /checkout add two addresses one with US country, the other
      with Belgium
    6) Try to click on US partner then on Belgium then again on US
    7) Observe traceback
    
    After this commit we skip the check in _canEnableMainButton if a radio
    element is not found.
    
    X-original-commit: 282cecbe471d1b6391996fb0b04820cf3c03c20d

--- addons/website_sale_collect/static/src/js/checkout.js
+++ addons/website_sale_collect/static/src/js/checkout.js
@@ -50,11 +50,14 @@ publicWidget.registry.WebsiteSaleCheckout.include({
             return this._super.apply(this, arguments);  // Skip override.
         }
         const checkedRadio = this.el.querySelector('input[name="o_delivery_radio"]:checked');
-        const deliveryContainer = this._getDeliveryMethodContainer(checkedRadio);
-        const hasWarning = (
-            checkedRadio.dataset.deliveryType === 'in_store'
-            && deliveryContainer.querySelector('[name="unavailable_products_warning"]')
-        );
+        let hasWarning = false;
+        if (checkedRadio) {
+            const deliveryContainer = this._getDeliveryMethodContainer(checkedRadio);
+            hasWarning = (
+                checkedRadio.dataset.deliveryType === 'in_store'
+                && deliveryContainer.querySelector('[name="unavailable_products_warning"]')
+            );
+        }
         return this._super.apply(this, arguments) && !hasWarning;
     },
 
