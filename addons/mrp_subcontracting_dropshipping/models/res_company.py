9e314629bcdf14d71dcde606a7d60bee68647532
dhha-odoo <dhha@odoo.com>
2025-02-16 19:29:33 +0530

    [IMP] stock*: product routes improvements
    *: mrp, mrp_subcontracting, mrp_subcontracting_dropshipping, purchase_stock
    
    With this commit
    =======================
    
    - The operations section is hidden if no routes on the product.
    - No need to select routes in the following cases:
    
         - If purchase_ok is enabled and the product has a vendor,
           the 'Buy' route is automatically selected.
         - If the product has a BOM, the 'mrp' route is automatically selected.
         - If a product is a component of a subcontracting BOM and has sufficient
           available quantity, the 'Resupply Subcontractor on Order'
           route is automatically selected.
    
    - Now, the 'Dropship' route can also work for 'subcontracting BOMs',
      similar to the 'Dropship Subcontractor on Order' route.
    
    Task - [4385221](https://www.odoo.com/odoo/my-tasks/4385221)

--- addons/mrp_subcontracting_dropshipping/models/res_company.py
+++ addons/mrp_subcontracting_dropshipping/models/res_company.py
@@ -45,7 +45,7 @@ class ResCompany(models.Model):
                 pick_type.company_id.dropship_subcontractor_pick_type_id = pick_type.id
 
     def _create_subcontracting_dropshipping_rules(self):
-        route = self.env.ref('mrp_subcontracting_dropshipping.route_subcontracting_dropshipping')
+        dropship_route = self.env.ref('stock_dropshipping.route_drop_shipping')
         supplier_location = self.env.ref('stock.stock_location_suppliers')
         vals = []
         for company in self:
@@ -62,7 +62,7 @@ class ResCompany(models.Model):
                     'location_dest_id': subcontracting_location.id,
                     'location_src_id': supplier_location.id,
                     'procure_method': 'make_to_stock',
-                    'route_id': route.id,
+                    'route_id': dropship_route.id,
                     'picking_type_id': dropship_picking_type.id,
                     'company_id': company.id,
                 })
