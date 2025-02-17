a67fbbb90aa9cc093d9407f4a679d494b87068d7
Jay Savaliya <jasa@odoo.com>
2025-02-17 14:33:49 +0530

    [IMP] l10n_in: gstr mapping field added in account_move_line
    With this commit, gstr section mapping field will be added in
    `account.move.line`, enabling efficient cross-verification of user-provided data
    with GSTR Reporting.
    
    task-3360018

--- addons/l10n_in/models/account_move_line.py
+++ addons/l10n_in/models/account_move_line.py
@@ -7,6 +7,31 @@ class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
     l10n_in_hsn_code = fields.Char(string="HSN/SAC Code", compute="_compute_l10n_in_hsn_code", store=True, readonly=False, copy=False)
+    l10n_in_gstr_section = fields.Selection(
+        selection=[
+            ("b2b_rcm", "B2B RCM"),
+            ("b2b_regular", "B2B Regular"),
+            ("b2cl", "B2CL"),
+            ("b2cs", "B2CS"),
+            ("exp_wp", "EXP(WP)"),
+            ("exp_wop", "EXP(WOP)"),
+            ("sez_wp", "SEZ(WP)"),
+            ("sez_wop", "SEZ(WOP)"),
+            ("deemed_export", "Deemed Export"),
+            ("cdnr_rcm", "CDNR RCM"),
+            ("cdnr_regular", "CDNR Regular"),
+            ("cdnr_deemed_export", "CDNR(Deemed Export)"),
+            ("cdnr_sez", "CDNR(SEZ)"),
+            ("cdnur_b2cl", "CDNUR(B2CL)"),
+            ("cdnur_exp_wp", "CDNUR(EXP-WP)"),
+            ("cdnur_exp_wop", "CDNUR(EXP-WOP)"),
+            ("nil_rated", "Nil Rated"),
+            ("out_of_scope", "Out of Scope")
+            ],
+        string="GSTR Section",
+        compute="_compute_l10n_in_gstr_section",
+        store=True,
+    )
 
     # withholding related fields
     l10n_in_withhold_tax_amount = fields.Monetary(string="TDS Tax Amount", compute='_compute_l10n_in_withhold_tax_amount')
@@ -38,3 +63,210 @@ class AccountMoveLine(models.Model):
                 product_line=self.product_id.name or self.name
             )
         return False
+
+    def get_l10n_in_tax_tag_ids(self):
+
+        def get_tag_ids(*refs):
+            return [self.env.ref(ref).id for ref in refs]
+
+        return {
+            'gst_rc': get_tag_ids(
+                'l10n_in.tax_tag_base_sgst_rc', 'l10n_in.tax_tag_sgst_rc',
+                'l10n_in.tax_tag_base_cgst_rc', 'l10n_in.tax_tag_cgst_rc',
+                'l10n_in.tax_tag_base_igst_rc', 'l10n_in.tax_tag_igst_rc',
+                'l10n_in.tax_tag_base_cess_rc', 'l10n_in.tax_tag_cess_rc'
+            ),
+            'gst': get_tag_ids(
+                'l10n_in.tax_tag_base_sgst', 'l10n_in.tax_tag_sgst',
+                'l10n_in.tax_tag_base_cgst', 'l10n_in.tax_tag_cgst',
+                'l10n_in.tax_tag_base_igst', 'l10n_in.tax_tag_igst',
+                'l10n_in.tax_tag_base_cess', 'l10n_in.tax_tag_cess'
+            ),
+            'nil': get_tag_ids(
+                'l10n_in.tax_tag_exempt', 'l10n_in.tax_tag_nil_rated', 'l10n_in.tax_tag_non_gst_supplies'
+            ),
+            'export': get_tag_ids(
+                'l10n_in.tax_tag_zero_rated', 'l10n_in.tax_tag_base_igst', 'l10n_in.tax_tag_igst',
+                'l10n_in.tax_tag_base_cess', 'l10n_in.tax_tag_cess'
+            ),
+            'igst_lut': get_tag_ids('l10n_in.tax_tag_igst_lut')
+        }
+
+    @api.depends('move_id.l10n_in_gst_treatment', 'move_id.amount_total', 'move_id.l10n_in_state_id', 'company_id', 'tax_tag_ids')
+    def _compute_l10n_in_gstr_section(self):
+
+        def has_tags(category):
+            return any(tag in tax_tags for tag in tax_tags_ids[category])
+
+        def is_invoice_or_bill(move):
+            return (
+                (move.move_type in ('out_invoice', 'out_receipt', 'in_invoice', 'in_receipt'))
+                and not move.debit_origin_id
+            )
+
+        def get_transaction_type(move):
+            return 'intra_state' if move.l10n_in_state_id == move.company_id.state_id else 'inter_state'
+
+        def is_b2b_rcm(line):
+            return (
+                is_invoice_or_bill(line.move_id) and
+                line.move_id.l10n_in_gst_treatment in ('regular', 'composition', 'uin_holders') and
+                has_tags('gst_rc')
+            )
+
+        def is_b2b_regular(line):
+            return (
+                is_invoice_or_bill(line.move_id) and
+                line.move_id.l10n_in_gst_treatment in ('regular', 'composition', 'uin_holders') and
+                has_tags('gst')
+            )
+
+        def is_b2cl(line):
+            return (
+                is_invoice_or_bill(line.move_id) and
+                line.move_id.l10n_in_gst_treatment in ('unregistered', 'consumer') and
+                has_tags('gst') and
+                get_transaction_type(line.move_id) == 'inter_state' and
+                line.move_id.amount_total > 100000
+            )
+
+        def is_b2cs(line):
+            return (
+                line.move_id.l10n_in_gst_treatment in ('unregistered', 'consumer') and
+                has_tags('gst') and
+                (get_transaction_type(line.move_id) == 'intra_state' or
+                (get_transaction_type(line.move_id) == 'inter_state' and
+                    (is_invoice_or_bill(line.move_id) and line.move_id.amount_total <= 100000) or
+                    (line.move_id.debit_origin_id and line.move_id.debit_origin_id.amount_total <= 100000) or
+                    (line.move_id.reversed_entry_id and line.move_id.reversed_entry_id.amount_total <= 100000)
+                ))
+            )
+
+        def is_exp_wp(line):
+            return (
+                is_invoice_or_bill(line.move_id) and
+                line.move_id.l10n_in_gst_treatment == 'overseas' and
+                has_tags('export')
+            )
+
+        def is_exp_wop(line):
+            return (
+                is_invoice_or_bill(line.move_id) and
+                line.move_id.l10n_in_gst_treatment == 'overseas' and
+                has_tags('igst_lut')
+            )
+
+        def is_sez_wp(line):
+            return (
+                is_invoice_or_bill(line.move_id) and
+                line.move_id.l10n_in_gst_treatment == 'special_economic_zone' and
+                has_tags('export')
+            )
+
+        def is_sez_wop(line):
+            return (
+                is_invoice_or_bill(line.move_id) and
+                line.move_id.l10n_in_gst_treatment == 'special_economic_zone' and
+                has_tags('igst_lut')
+            )
+
+        def is_deemed_export(line):
+            return (
+                is_invoice_or_bill(line.move_id) and
+                line.move_id.l10n_in_gst_treatment == 'deemed_export' and
+                has_tags('gst')
+            )
+
+        def is_nil_rated(line):
+            return (
+                line.move_id.l10n_in_gst_treatment not in ('overseas', 'special_economic_zone') and
+                has_tags('nil')
+            )
+
+        def is_cdnr_rcm(line):
+            return (
+                not is_invoice_or_bill(line.move_id) and
+                line.move_id.l10n_in_gst_treatment in ('regular', 'composition', 'uin_holders') and
+                has_tags('gst_rc')
+            )
+
+        def is_cdnr_regular(line):
+            return (
+                not is_invoice_or_bill(line.move_id) and
+                line.move_id.l10n_in_gst_treatment in ('regular', 'composition', 'uin_holders') and
+                has_tags('gst')
+            )
+
+        def is_cdnr_sez(line):
+            return (
+                not is_invoice_or_bill(line.move_id) and
+                line.move_id.l10n_in_gst_treatment == 'special_economic_zone' and
+                has_tags('export')
+            )
+
+        def is_cdnr_deemed_export(line):
+            return (
+                not is_invoice_or_bill(line.move_id) and
+                line.move_id.l10n_in_gst_treatment == 'deemed_export' and
+                has_tags('gst')
+            )
+
+        def is_cdnur_b2cl(line):
+            return (
+                not is_invoice_or_bill(line.move_id) and
+                line.move_id.l10n_in_gst_treatment in ('unregistered', 'consumer') and
+                has_tags('gst') and
+                get_transaction_type(line.move_id) == 'inter_state' and
+                (
+                    (line.move_id.debit_origin_id and line.move_id.debit_origin_id.amount_total > 100000) or
+                    (line.move_id.reversed_entry_id and line.move_id.reversed_entry_id.amount_total > 100000) or
+                    (not line.move_id.reversed_entry_id and not line.move_id.is_inbound())
+                )
+            )
+
+        def is_cdnur_exp_wp(line):
+            return (
+                not is_invoice_or_bill(line.move_id) and
+                line.move_id.l10n_in_gst_treatment == 'overseas' and
+                has_tags('export')
+            )
+
+        def is_cdnur_exp_wop(line):
+            return (
+                not is_invoice_or_bill(line.move_id) and
+                line.move_id.l10n_in_gst_treatment == 'overseas' and
+                has_tags('igst_lut')
+            )
+
+        indian_moves_lines = self.filtered(lambda l: l.move_id.country_code == 'IN' and l.move_id.move_type != 'entry' and l.parent_state == 'posted')
+        (self - indian_moves_lines).l10n_in_gstr_section = False
+        if not indian_moves_lines:
+            return
+
+        tax_tags_ids = self.get_l10n_in_tax_tag_ids()
+
+        gstr_mapping_fun = {
+            "b2b_rcm": is_b2b_rcm,
+            "b2b_regular": is_b2b_regular,
+            "b2cl": is_b2cl,
+            "b2cs": is_b2cs,
+            "exp_wp": is_exp_wp,
+            "exp_wop": is_exp_wop,
+            "sez_wp": is_sez_wp,
+            "sez_wop": is_sez_wop,
+            "deemed_export": is_deemed_export,
+            "cdnr_rcm": is_cdnr_rcm,
+            "cdnr_regular": is_cdnr_regular,
+            "cdnr_sez": is_cdnr_sez,
+            "cdnr_deemed_export": is_cdnr_deemed_export,
+            "cdnur_b2cl": is_cdnur_b2cl,
+            "cdnur_exp_wp": is_cdnur_exp_wp,
+            "cdnur_exp_wop": is_cdnur_exp_wop,
+            "nil_rated": is_nil_rated
+        }
+        for line in indian_moves_lines:
+            tax_tags = line.tax_tag_ids.ids
+            line.l10n_in_gstr_section = next(
+                (section for section, function in gstr_mapping_fun.items() if function(line)),
+                'out_of_scope'
+            )
--- addons/l10n_in_pos/models/__init__.py
+++ addons/l10n_in_pos/models/__init__.py
@@ -5,5 +5,6 @@ from . import pos_order
 from . import pos_order_line
 from . import product_template
 from . import account_move
+from . import account_move_line
 from . import account_tax
 from . import pos_bill
