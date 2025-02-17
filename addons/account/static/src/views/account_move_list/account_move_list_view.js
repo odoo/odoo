718684dc370f9544abaa1aea4849d04c823c43b7
Ricardo Gomes Rodrigues <rigr@odoo.com>
2025-02-17 10:55:07 +0100

    [IMP] account: bills duplicate detection
    Purpose
    =======
    
    Recent changes have made bill encoding easier than ever
    (most notably, a bill date is set by default if left empty),
    but offering a robust duplicate bill detection is more important
    than ever in the modern electronic invoicing world, where bills
    are more and more handled automatically.
    
    We want to make sure that:
    - Potential duplicates remain visible at all time, including in list view
    - Automated actions like auto posting are not done when a potential duplicate is detected.
    
    Specs
    =====
    
    1.  Do not hide the warning on potential bill duplicate for posted invoices.
    The information remains pertinent.
    
    2. When a move should be posted automatically (by the Auto-post feature on
    account.move or by the OCR auto-post), but a potential duplicate is found,
    prevent the auto-post from happening. Log the information in the chatter:
    "Auto-post was disabled on this invoice because a potential duplicate was detected."
    
    3. In list view put the reference in red background when a
    potential duplicate on that invoice is detected.
    
    task-4578751
    
    Part-of: odoo/odoo#197891
    Related: odoo/enterprise#79471
    Signed-off-by: Ruben Gomes (rugo) <rugo@odoo.com>

--- addons/account/static/src/views/account_move_list/account_move_list_view.js
+++ addons/account/static/src/views/account_move_list/account_move_list_view.js
@@ -1,12 +1,12 @@
 import { registry } from "@web/core/registry";
 import { fileUploadListView } from "../file_upload_list/file_upload_list_view";
 import { AccountMoveListController } from "./account_move_list_controller";
-import { AccountMoveUploadListRenderer } from "./account_move_list_renderer";
+import { AccountMoveListRenderer } from "./account_move_list_renderer";
 
 export const accountMoveUploadListView = {
     ...fileUploadListView,
     Controller: AccountMoveListController,
-    Renderer: AccountMoveUploadListRenderer,
+    Renderer: AccountMoveListRenderer,
     buttonTemplate: "account.AccountMoveListView.Buttons",
 };
 
