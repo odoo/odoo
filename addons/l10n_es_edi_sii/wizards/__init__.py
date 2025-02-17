dd2f860cb05708518cd41118bd22a97dce926438
Jairo Llopis <yajo.sk8@gmail.com>
2025-02-17 12:35:22 +0000

    [FIX] l10n_es_edi_sii, l10n_es_edi_tbai: allow resequencing vendor bills
    Before this patch, it was impossible to resequence vendor bills if already sent to SII/tbai.
    
    However, it is required to be able to resequence them, and the move name isn't sent anymore since https://github.com/odoo/odoo/pull/195113 was merged.
    
    Thus, here I'm lifting that constraint to allow accountants do the resequencing.
    
    @moduon MT-8728 OPW-4567144
    
    closes odoo/odoo#209721
    
    X-original-commit: 91056d83f3886dc3ef7d91097e24684d26baafb1
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>

--- addons/l10n_es_edi_sii/wizards/__init__.py
+++ addons/l10n_es_edi_sii/wizards/__init__.py
@@ -1,4 +1,2 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from . import models
-from . import wizards
+from . import account_resequence_wizard
