401a966233d2e83745178387f4ee4416c8763e37
TSO <tso@odoo.com>
2025-02-17 09:29:57 +0100

    [IMP] discuss: upgrade to SFU as fallback for p2p failures
    This commit makes it so that in the case of unsuccessful peer-to-peer
    connections, the client will request to the Odoo server to upgrade
    the channel with a SFU.
    
    backport of: odoo/odoo#200122 and odoo/odoo#206411
    
    closes odoo/odoo#228563
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/discuss/call/common/peer_to_peer.js
+++ addons/mail/static/src/discuss/call/common/peer_to_peer.js
@@ -12,6 +12,7 @@ export const UPDATE_EVENT = Object.freeze({
     CONNECTION_CHANGE: "connection_change",
     DISCONNECT: "disconnect",
     INFO_CHANGE: "info_change",
+    RECOVERY: "recovery",
     TRACK: "track",
 });
 const LOG_LEVEL = Object.freeze({
@@ -688,6 +689,7 @@ export class PeerToPeer extends EventTarget {
                 if (!peer?.connection || !this.channelId || (connectionSuccess && iceSuccess)) {
                     return;
                 }
+                this._emitUpdate({ name: UPDATE_EVENT.RECOVERY, payload: { id } });
                 this._emitLog(id, `attempting to recover connection: ${reason}`, LOG_LEVEL.ERROR);
                 this._busNotify(INTERNAL_EVENT.DISCONNECT, { targets: [peer.id] });
                 this.removePeer(peer.id);
