bd7d460bbdb60879325c2ed27eb86d2e565c3b4b
Louis Travaux <lotr@odoo.com>
2025-02-17 16:04:38 +0100

    [FIX] hw_{drivers,posbox_homepage}: Windows IoT Box issues after recent changes
    - logs path was incorrect (`server/odoo.log` instead of `odoo.log`),
    - some imports were not supported for windows: a condition to import only on linux has been added,
    - qr code generation methods to connect to Wi-Fi (from status display) were called without checking the
    system,
    - switched back from `wine-devel` to `wine-stable` as the fix used is now available in stable.
    
    closes odoo/odoo#199380
    
    Signed-off-by: Yaroslav Soroko (yaso) <yaso@odoo.com>

--- addons/hw_posbox_homepage/static/src/app/components/FooterButtons.js
+++ addons/hw_posbox_homepage/static/src/app/components/FooterButtons.js
@@ -19,9 +19,15 @@ export class FooterButtons extends Component {
         this.store = useStore();
     }
 
+    get url() {
+        const url = new URL(window.location.href);
+        const port = url.protocol === "https:" ? "" : ":8069";
+        return `${url.protocol}//${this.store.base.ip}${port}`;
+    }
+
     static template = xml`
     <div class="w-100 d-flex flex-wrap align-items-cente gap-2 justify-content-center">
-        <a t-if="store.isLinux and !store.base.is_access_point_up" class="btn btn-primary btn-sm" t-att-href="'http://' + this.store.base.ip + '/status'" target="_blank">
+        <a t-if="store.isLinux and !store.base.is_access_point_up" class="btn btn-primary btn-sm" t-att-href="this.url + '/status'" target="_blank">
             Status Display
         </a>
         <a t-if="store.isLinux and !store.base.is_access_point_up" class="btn btn-primary btn-sm" t-att-href="'http://' + this.store.base.ip + ':631'" target="_blank">
@@ -30,7 +36,7 @@ export class FooterButtons extends Component {
         <RemoteDebugDialog t-if="this.store.advanced and this.store.isLinux" />
         <CredentialDialog t-if="this.store.advanced" />
         <HandlerDialog t-if="this.store.advanced" />
-        <a t-if="this.store.advanced" class="btn btn-primary btn-sm" t-att-href="'http://' + this.store.base.ip + '/logs'" target="_blank">View Logs</a>
+        <a t-if="this.store.advanced" class="btn btn-primary btn-sm" t-att-href="this.url + '/logs'" target="_blank">View Logs</a>
     </div>
   `;
 }
