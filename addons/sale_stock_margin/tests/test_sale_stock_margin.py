0cc3bfb34f3c1748ca2dc12a826189c3accba943
Ethan Vincent <etvi@odoo.com>
2025-02-17 08:27:39 +0000

    [FIX] sale_{margin,stock_margin}: find margin when line added from delivery
    **Current behavior:**
    Adding a product which is invoiced based on delivered
    quantities to the delivery of a sale which has already been
    confirmed, then validating the delivery, does not reflect the
    change in price (which will be invoiced) on the sale order.
    
    **Expected behavior:**
    The sale order should show the actual amount of money to be
    charged to the orderer.
    
    **Steps to reproduce:**
    1. Create a stored product with some price and change its
    invoicing policy to 'delivery'
    
    2. Create a new sale order, add 1 of the created product,
    then confirm it
    
    3. Open the delivery, add another line to it for the same
    created product, fill in the quantities for each move, then
    validate the picking
    
    4. Check the sale order line to see that, while the price unit
    is populated with the product's price, the subtotal/total fields
    are not
    
    **Cause of the issue:**
    Order lines added in this manner have `product_uom_qty == 0`
    which will zero-out the price computation.
    
    **Fix:**
    First, `_compute_purchase_price` is modified such that lines for
    standard cost product will have a value computed (where
    previously they were skipped).
    
    Then for `_compute_margin`:
    use `qty_delivered` and an on-the-fly calculated
    `price_subtotal` to find a margin value for a SOL added from a
    delivery (which is determined when `product_uom_qty == 0` and
    `qty_delivered != 0`
    
    opw-4210056
    
    closes odoo/odoo#200653
    
    X-original-commit: e24d922afa914a7f51b260863652608554a18177
    Signed-off-by: Quentin Wolfs (quwo) <quwo@odoo.com>

--- addons/sale_stock_margin/tests/test_sale_stock_margin.py
+++ addons/sale_stock_margin/tests/test_sale_stock_margin.py
@@ -318,3 +318,35 @@ class TestSaleStockMargin(TestStockValuationCommon):
 
         self.assertEqual(so.state, 'sent')
         self.assertEqual(so.order_line[0].purchase_price, 15)
+
+    def test_add_product_on_delivery_price_unit_on_sale(self):
+        """ Adding a product directly on a sale order's delivery should result in the new SOL
+        having its `purchase_price` and `margin` + `margin_percent` fields correctly calculated.
+        """
+        products = [self._create_product() for _ in range(2)]
+        for product, cost, price in zip(products, [20, 10], [25, 20]):
+            product.categ_id.property_cost_method = 'standard'
+            product.write({
+                'standard_price': cost,
+                'list_price': price,
+                'invoice_policy': 'delivery',
+            })
+        sale_order = self._create_sale_order()
+        self._create_sale_order_line(sale_order, products[0], 10, products[0].list_price)
+        sale_order.action_confirm()
+        delivery = sale_order.picking_ids[0]
+        with Form(delivery) as delivery_form:
+            with delivery_form.move_ids_without_package.new() as move:
+                move.product_id = products[1]
+                move.product_uom_qty = 10
+        delivery.move_ids.quantity = 10
+        delivery.button_validate()
+        self.assertRecordValues(
+            sale_order.order_line.filtered(lambda sol: sol.product_id == products[1]),
+            [{
+                'price_unit': products[1].list_price,
+                'purchase_price': products[1].standard_price,
+                'margin': 100,
+                'margin_percent': 0.5,
+            }]
+        )
