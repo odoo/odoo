04f8ccdc084a67770a6075dd2043b3a10fdccc2a
Jairo Llopis <yajo.sk8@gmail.com>
2025-02-17 12:35:22 +0000

    [FIX] l10n_es_edi_sii, l10n_es_edi_tbai: allow resequencing vendor bills
    Before this patch, it was impossible to resequence vendor bills if already sent to SII/tbai.
    
    However, it is required to be able to resequence them, and the move name isn't sent anymore since https://github.com/odoo/odoo/pull/195113 was merged.
    
    Thus, here I'm lifting that constraint to allow accountants do the resequencing.
    
    @moduon MT-8728 OPW-4567144
    
    closes odoo/odoo#198189
    
    X-original-commit: 6256e8e0b4843340ff942f5caf25d541943da9c3
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>

--- addons/account_edi/wizard/account_resequence.py
+++ addons/account_edi/wizard/account_resequence.py
@@ -5,8 +5,17 @@ from odoo.exceptions import UserError
 class ReSequenceWizard(models.TransientModel):
     _inherit = 'account.resequence.wizard'
 
+    def _frozen_edi_documents(self):
+        """Get EDI documents that can't change.
+
+        Their moves are restricted and cannot be resequenced.
+        """
+        return self.move_ids.edi_document_ids.filtered(
+            lambda d: d.edi_format_id._needs_web_services() and d.state == "sent"
+        )
+
     def resequence(self):
-        edi_sent_moves = self.move_ids.edi_document_ids.filtered(lambda d: d.edi_format_id._needs_web_services() and d.state == 'sent')
+        edi_sent_moves = self._frozen_edi_documents()
         if edi_sent_moves:
             raise UserError(_("The following documents have already been sent and cannot be resequenced: %s")
                 % ", ".join(set(edi_sent_moves.move_id.mapped('name')))
