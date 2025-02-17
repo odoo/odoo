615a79d4b0a89863ae390e141f45d5f241cbcdba
roen-odoo <roen@odoo.com>
2025-02-17 17:55:45 +0000

    [FIX] pos_self_order: fix stripe kiosk payment
    When you have multiple payment methods that can work on the kiosk, they
    would all be shown on the kiosk screen.
    
    Steps to reproduce:
    -------------------
    * Setup 2 Stripe payment methods
    * Create a PoS kiosk
    * Assign one of the methods to the kiosk
    * Open the kiosk
    * Create an order and go to the payment screen
    > Observation: Both payment methods are shown
    
    Why the fix:
    ------------
    The payment methods are now filtered based on the payment methods
    assigned to the kiosk.
    
    opw-4574644
    
    closes odoo/odoo#198706
    
    X-original-commit: 62dc949e2286e3a2086db5605c8a787c64cbe0de
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Robin Engels (roen) <roen@odoo.com>

---
+++ addons/pos_self_order_stripe/tests/test_self_order_kiosk_stripe.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import odoo.tests
+from odoo.addons.pos_self_order.tests.self_order_common_test import SelfOrderCommonTest
+from odoo.tests import Command
+
+@odoo.tests.tagged("post_install", "-at_install")
+class TestSelfOrderKioskStripe(SelfOrderCommonTest):
+    def test_self_order_kiosk_stripe(self):
+        self.pos_config.write({
+            'self_ordering_mode': 'kiosk',
+            'self_ordering_pay_after': 'each',
+            'self_ordering_service_mode': 'table',
+        })
+        stripe = self.env['pos.payment.method'].create({
+            'name': 'Stripe',
+            'use_payment_terminal': 'stripe',
+        })
+
+        self.env['pos.payment.method'].create({
+            'name': 'Stripe 2',
+            'use_payment_terminal': 'stripe',
+        })
+
+        self.pos_config.write({
+            'payment_method_ids': [Command.set([stripe.id])]
+        })
+
+        res = self.pos_config.load_self_data()
+        pm = res.get('pos.payment.method', [])
+        self.assertEqual(len(pm), 1, 'Only one payment method should be loaded')
+        self.assertEqual(pm[0]['name'], 'Stripe', 'The loaded payment method should be Stripe')
