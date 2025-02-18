997b104571bdb802e719d952257639e3b9ba3743
Aaron Bohy <aab@odoo.com>
2025-02-18 09:24:03 +0000

    [FIX] web: pivot: fix tests failing randomly
    This commit fixes two pivot tests failing randomly by ensuring that
    we wait for the download to be completed before ending the test or
    asserting the steps.
    
    Runbot error-134572
    
    X-original-commit: 97b68c7196c089fb2fb4d7bcf058a781f6e7e078

--- addons/web/static/tests/views/pivot_view_tests.js
+++ addons/web/static/tests/views/pivot_view_tests.js
@@ -1929,6 +1929,7 @@ QUnit.module("Views", (hooks) => {
         async function (assert) {
             assert.expect(2);
 
+            const downloadDef = makeDeferred();
             mockDownload(async ({ url, data }) => {
                 data = JSON.parse(await data.data.text());
                 assert.strictEqual(url, "/web/pivot/export_xlsx");
@@ -1937,6 +1938,7 @@ QUnit.module("Views", (hooks) => {
                     4,
                     "should have measure_headers in data"
                 );
+                downloadDef.resolve();
                 return Promise.resolve();
             });
 
@@ -1952,6 +1954,7 @@ QUnit.module("Views", (hooks) => {
             });
 
             await click(target.querySelector(".o_pivot_download"));
+            await downloadDef;
         }
     );
 
@@ -3612,6 +3615,7 @@ QUnit.module("Views", (hooks) => {
 
         patchDate(2016, 11, 20, 1, 0, 0);
 
+        const downloadDef = makeDeferred();
         mockDownload(async ({ url, data }) => {
             data = JSON.parse(await data.data.text());
             for (const l of data.col_group_headers) {
@@ -3631,6 +3635,7 @@ QUnit.module("Views", (hooks) => {
                 "/web/pivot/export_xlsx",
                 "should call get_file with correct parameters"
             );
+            downloadDef.resolve();
             return Promise.resolve();
         });
 
@@ -3669,6 +3674,7 @@ QUnit.module("Views", (hooks) => {
         // With the data above, the time ranges contain some records.
         // export data. Should execute 'get_file'
         await click(target.querySelector(".o_pivot_buttons button.o_pivot_download"));
+        await downloadDef;
 
         assert.verifySteps([
             // col group headers
