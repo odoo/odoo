292113b9566e8afd76cfadf4581df2a9f6731d05
Ricardo Gomes Rodrigues <rigr@odoo.com>
2025-02-18 14:13:23 +0100

    [MOV] account,purchase: Vendor Bills smart button to account
    Move the Vendor Bills smart button from the `purchase` module
    to the `account` module.
    
    The views records in `purchase` module are kept as but made
    empty so that if the user updates the `account` module, it
    will also update the `purchase` module and hide the smart
    button from the `purchase` to only keep the new one from
    the `account` module.
    
    task-4584035

--- addons/purchase/models/res_partner.py
+++ addons/purchase/models/res_partner.py
@@ -28,26 +28,6 @@ class res_partner(models.Model):
                 partner = partner.parent_id
         (self - partners).purchase_order_count = 0
 
-    def _compute_supplier_invoice_count(self):
-        # retrieve all children partners and prefetch 'parent_id' on them
-        all_partners = self.with_context(active_test=False).search([('id', 'child_of', self.ids)])
-        all_partners.read(['parent_id'])
-
-        supplier_invoice_groups = self.env['account.move']._read_group(
-            domain=[('partner_id', 'in', all_partners.ids),
-                    ('move_type', 'in', ('in_invoice', 'in_refund'))],
-            fields=['partner_id'], groupby=['partner_id']
-        )
-        partners = self.browse()
-        for group in supplier_invoice_groups:
-            partner = self.browse(group['partner_id'][0])
-            while partner:
-                if partner in self:
-                    partner.supplier_invoice_count += group['partner_id_count']
-                    partners |= partner
-                partner = partner.parent_id
-        (self - partners).supplier_invoice_count = 0
-
     @api.model
     def _commercial_fields(self):
         return super(res_partner, self)._commercial_fields()
@@ -56,7 +36,6 @@ class res_partner(models.Model):
         'res.currency', string="Supplier Currency", company_dependent=True,
         help="This currency will be used, instead of the default one, for purchases from the current partner")
     purchase_order_count = fields.Integer(compute='_compute_purchase_order_count', string='Purchase Order Count')
-    supplier_invoice_count = fields.Integer(compute='_compute_supplier_invoice_count', string='# Vendor Bills')
     purchase_warn = fields.Selection(WARNING_MESSAGE, 'Purchase Order Warning', help=WARNING_HELP, default="no-message")
     purchase_warn_msg = fields.Text('Message for Purchase Order')
 
