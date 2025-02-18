bd7b63a3bb81a145d8277deb9c2d846fac4673ad
Max Whale <mawh@odoo.com>
2025-02-18 14:31:15 +0000

    [IMP] hw_drivers: refresh pairing code
    Before this commit, a pairing code is fetched once
    upon startup, displayed for 5 minutes, and then
    cleared.
    
    After this commit, the code timeout is extended to
    10 minutes, and will be refreshed 2 times
    automatically.
    
    task-4586979
    
    closes odoo/odoo#198791
    
    X-original-commit: 993b2b8e56ff23b015053a124a320291a15987fa
    Signed-off-by: Yaroslav Soroko (yaso) <yaso@odoo.com>
    Signed-off-by: Max Whale (mawh) <mawh@odoo.com>

--- addons/hw_drivers/connection_manager.py
+++ addons/hw_drivers/connection_manager.py
@@ -12,7 +12,11 @@ from odoo.addons.hw_drivers.tools import helpers, wifi
 
 _logger = logging.getLogger(__name__)
 
-PAIRING_CODE_TIMEOUT_SECONDS = 300
+# We use a timeout slightly less than the IoT proxy so
+# that there is a grace period between codes
+PAIRING_CODE_TIMEOUT_SECONDS = 580
+
+MAXIMUM_NUMBER_OF_CODES = 3
 
 
 class ConnectionManager(Thread):
@@ -20,44 +24,66 @@ class ConnectionManager(Thread):
         super(ConnectionManager, self).__init__()
         self.pairing_code = False
         self.pairing_uuid = False
+        self.pairing_code_expires = 0
+        self.pairing_code_count = 0
+        self.running = False
+        requests.packages.urllib3.disable_warnings()
 
     def run(self):
-        if platform.system() == 'Linux' and wifi.is_access_point():
-            return
+        self.running = True
+        while self.running:
+            if self._should_fetch_pairing_code():
+                if time.monotonic() > self.pairing_code_expires:
+                    self._refresh_pairing_code()
+                else:
+                    self._poll_pairing_result()
+            else:
+                self.running = False
+                self.pairing_code = False
+                self.pairing_uuid = False
+                self.pairing_code_expires = 0
+            time.sleep(10)
 
-        if not helpers.get_odoo_server_url():
-            end_time = time.monotonic() + PAIRING_CODE_TIMEOUT_SECONDS
-            while time.monotonic() < end_time:
-                self._connect_box()
-                time.sleep(10)
-            self.pairing_code = False
-            self.pairing_uuid = False
+    def _should_fetch_pairing_code(self):
+        return (
+            not helpers.get_odoo_server_url() and
+            helpers.get_ip() and
+            not (platform.system() == 'Linux' and wifi.is_access_point()) and
+            self.pairing_code_count <= MAXIMUM_NUMBER_OF_CODES
+        )
 
-    def _connect_box(self):
-        if not helpers.get_ip() or (platform.system() == 'Linux' and wifi.is_access_point()):
-            return
-
+    def _call_iot_proxy(self, pairing_code, pairing_uuid):
         data = {
             'jsonrpc': 2.0,
             'params': {
-                'pairing_code': self.pairing_code,
-                'pairing_uuid': self.pairing_uuid,
+                'pairing_code': pairing_code,
+                'pairing_uuid': pairing_uuid,
             }
         }
 
         try:
-            requests.packages.urllib3.disable_warnings()
             req = requests.post(
                 'https://iot-proxy.odoo.com/odoo-enterprise/iot/connect-box', json=data, verify=False, timeout=5
             )
-            result = req.json().get('result', {})
-            if all(key in result for key in ['pairing_code', 'pairing_uuid']):
-                self.pairing_code = result['pairing_code']
-                self.pairing_uuid = result['pairing_uuid']
-            elif all(key in result for key in ['url', 'token', 'db_uuid', 'enterprise_code']):
-                self._connect_to_server(result['url'], result['token'], result['db_uuid'], result['enterprise_code'])
+            return req.json().get('result', {})
         except Exception:
             _logger.exception('Could not reach iot-proxy.odoo.com')
+            return {}
+
+    def _poll_pairing_result(self):
+        result = self._call_iot_proxy(self.pairing_code, self.pairing_uuid)
+        
+        if all(key in result for key in ['url', 'token', 'db_uuid', 'enterprise_code']):
+            self._connect_to_server(result['url'], result['token'], result['db_uuid'], result['enterprise_code'])
+
+    def _refresh_pairing_code(self):
+        result = self._call_iot_proxy(pairing_code=None, pairing_uuid=None)
+
+        if all(key in result for key in ['pairing_code', 'pairing_uuid']):
+            self.pairing_code = result['pairing_code']
+            self.pairing_uuid = result['pairing_uuid']
+            self.pairing_code_expires = time.monotonic() + PAIRING_CODE_TIMEOUT_SECONDS
+            self.pairing_code_count += 1
 
     def _connect_to_server(self, url, token, db_uuid, enterprise_code):
         # Save DB URL and token
