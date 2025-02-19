15ef66dcdaaf9de1cd43bdd2df8510ae6d106198
dhba <dhba@odoo.com>
2025-02-19 05:44:07 +0000

    [FIX] html_editor: infinite loop in removeFormat
    Before this commit:
    
    - When color was applied to a block-level element, attempting to remove it
      caused the removeFormat method to enter an infinite loop.
    
    After this commit:
    
    - Now, the color is correctly removed from the selected text, even when it is
      applied to a block-level element.
    
    closes odoo/odoo#223939
    
    Task: 4575459
    X-original-commit: c6a6327c19584ecc21df747a7bd3aab40c378a78
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: DHAVAL BARAIYA (dhba) <dhba@odoo.com>

--- addons/html_editor/static/src/main/font/color_plugin.js
+++ addons/html_editor/static/src/main/font/color_plugin.js
@@ -19,6 +19,7 @@ import { reactive } from "@odoo/owl";
 import { _t } from "@web/core/l10n/translation";
 import { isColorGradient, isCSSColor, RGBA_REGEX, rgbaToHex } from "@web/core/utils/colors";
 import { ColorSelector } from "./color_selector";
+import { isBlock } from "@html_editor/utils/blocks";
 
 const RGBA_OPACITY = 0.6;
 const HEX_OPACITY = "99";
@@ -274,7 +275,13 @@ export class ColorPlugin extends Plugin {
 
         const getFonts = (selectedNodes) =>
             selectedNodes.flatMap((node) => {
-                let font = closestElement(node, "font") || closestElement(node, "span");
+                let font =
+                    closestElement(node, "font") ||
+                    closestElement(
+                        node,
+                        '[style*="color"]:not(li), [style*="background-color"]:not(li), [style*="background-image"]:not(li)'
+                    ) ||
+                    closestElement(node, "span");
                 const children = font && descendants(font);
                 const hasInlineGradient = font && isColorGradient(font.style["background-image"]);
                 const isFullySelected =
@@ -287,17 +294,33 @@ export class ColorPlugin extends Plugin {
                         (mode === "backgroundColor" && !isTextGradient));
                 if (
                     font &&
-                    (font.nodeName === "FONT" || (font.nodeName === "SPAN" && font.style[mode])) &&
+                    font.nodeName !== "T" &&
+                    (font.nodeName !== "SPAN" || font.style[mode] || font.style.backgroundImage) &&
                     (isColorGradient(color) ||
                         color === "" ||
                         !hasInlineGradient ||
-                        shouldReplaceExistingGradient)
+                        shouldReplaceExistingGradient) &&
+                    !this.dependencies.split.isUnsplittable(font)
                 ) {
                     // Partially selected <font>: split it.
                     const selectedChildren = children.filter((child) =>
                         selectedNodes.includes(child)
                     );
                     if (selectedChildren.length) {
+                        if (isBlock(font)) {
+                            const colorStyles = ["color", "background-color", "background-image"];
+                            const newFont = this.document.createElement("font");
+                            for (const style of colorStyles) {
+                                const styleValue = font.style[style];
+                                if (styleValue) {
+                                    this.colorElement(newFont, styleValue, style);
+                                    font.style.removeProperty(style);
+                                }
+                            }
+                            newFont.append(...font.childNodes);
+                            font.append(newFont);
+                            font = newFont;
+                        }
                         const closestGradientEl = closestElement(
                             node,
                             'font[style*="background-image"], span[style*="background-image"]'
@@ -406,6 +429,7 @@ export class ColorPlugin extends Plugin {
             if (
                 !hasColor(font, "color") &&
                 !hasColor(font, "backgroundColor") &&
+                ["FONT", "SPAN"].includes(font.nodeName) &&
                 (!font.hasAttribute("style") || !color)
             ) {
                 for (const child of [...font.childNodes]) {
