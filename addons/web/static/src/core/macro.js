46bfd1901405498b9c997940c8d829b944bbefd0
Pierre Pulinckx <pipu@odoo.com>
2025-02-19 13:26:15 +0000

    [REM] web: accept only function for action macro
    In order to simplify the macro.js API, the action of a step must be
    a function. The action can then call (Hoot or others) events to interact
    with the trigger. In any case, it is not useful to have ACTION_HELPERS
    in macro.js which are only used in very rare cases in the codebase.
    
    closes odoo/odoo#198559
    
    X-original-commit: bae79ce29e61b64038a6587519102229dfccde62
    Signed-off-by: Bastien Pierre (ipb) <ipb@odoo.com>
    Signed-off-by: Pierre Pulinckx (pipu) <pipu@odoo.com>

--- addons/web/static/src/core/macro.js
+++ addons/web/static/src/core/macro.js
@@ -29,25 +29,6 @@ const macroSchema = {
     onError: { type: Function, optional: true },
 };
 
-export const ACTION_HELPERS = {
-    click(el, _step) {
-        el.dispatchEvent(new MouseEvent("mouseover"));
-        el.dispatchEvent(new MouseEvent("mouseenter"));
-        el.dispatchEvent(new MouseEvent("mousedown"));
-        el.dispatchEvent(new MouseEvent("mouseup"));
-        el.click();
-        el.dispatchEvent(new MouseEvent("mouseout"));
-        el.dispatchEvent(new MouseEvent("mouseleave"));
-    },
-    text(el, step) {
-        // simulate an input (probably need to add keydown/keyup events)
-        this.click(el, step);
-        el.value = step.value;
-        el.dispatchEvent(new InputEvent("input", { bubbles: true }));
-        el.dispatchEvent(new InputEvent("change", { bubbles: true }));
-    },
-};
-
 const mutex = new Mutex();
 
 class MacroError extends Error {
@@ -116,7 +97,7 @@ export class Macro {
         if (proceedToAction) {
             this.onStep(this.currentElement, this.currentStep, this.currentIndex);
             this.clearTimer();
-            const actionResult = await this.performAction();
+            const actionResult = await this.stepAction(this.currentElement);
             if (!actionResult) {
                 // If falsy action result, it means the action worked properly.
                 // So we can proceed to the next step.
@@ -158,17 +139,15 @@ export class Macro {
     }
 
     /**
-     * Calls the `step.action` expecting no return to be successful.
+     * Must not return anything for macro to continue.
      */
-    async performAction() {
-        let actionResult;
+    async stepAction(element) {
+        const { action } = this.currentStep;
+        if (this.isComplete || !action) {
+            return;
+        }
         try {
-            const action = this.currentStep.action;
-            if (action in ACTION_HELPERS) {
-                actionResult = ACTION_HELPERS[action](this.currentElement, this.currentStep);
-            } else if (typeof action === "function") {
-                actionResult = await action(this.currentElement);
-            }
+            return await action(element);
         } catch (error) {
             this.stop(
                 new MacroError("Action", `ERROR during perform action:\n${error.message}`, {
@@ -176,7 +155,6 @@ export class Macro {
                 })
             );
         }
-        return actionResult;
     }
 
     get currentStep() {
