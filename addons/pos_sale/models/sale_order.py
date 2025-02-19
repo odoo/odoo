2b73a92d8f51cfa54c4605f5156ff186a6b8eb7d
Sarah Bellefroid <sbel@odoo.com>
2025-02-19 10:39:48 +0000

    [FIX] pos_sale: consider POS downpayments in amount_invoiced
    Currently when invoicing a SO which has a downpayment made in Pos
    this downpayment is not reflected in the wizard.
    
    Steps to reproduce:
    -------------------
    * Create an SO
    * Open pos session and make a downpayment for the SO
    * Close register
    * Go back to the SO
    * Deliver the items
    * Select the invoice
    > Observation: Amount already invoiced is at 0
    
    Why the fix:
    ------------
    There was a previous fix at the same place previously:
    https://github.com/odoo/odoo/commit/f613b87c38d208730ba2470e0a26a110c2089b66
    
    However the wizard had changed and instead of showing `amount_to_invoice`
    we now show `amount_invoiced`
    
    opw-4585413
    
    X-original-commit: ee925ec49a4639b9a9955233358502c9fce60e13

--- addons/pos_sale/models/sale_order.py
+++ addons/pos_sale/models/sale_order.py
@@ -59,6 +59,16 @@ class SaleOrder(models.Model):
             order_amount = sum(order.sudo().pos_order_line_ids.filtered(lambda pol: pol.sale_order_line_id.is_downpayment).mapped('price_subtotal_incl'))
             order.amount_to_invoice -= order_amount
 
+    @api.depends('order_line.pos_order_line_ids')
+    def _compute_amount_invoiced(self):
+        super()._compute_amount_invoiced()
+        for order in self:
+            if order.invoice_status == 'invoiced':
+                continue
+            # We need to account for the downpayment paid in POS with and without invoice
+            order_amount = sum(order.sudo().pos_order_line_ids.filtered(lambda pol: pol.order_id.state in ['paid', 'done', 'invoiced'] and pol.sale_order_line_id.is_downpayment).mapped('price_subtotal_incl'))
+            order.amount_invoiced += order_amount
+
 
 class SaleOrderLine(models.Model):
     _name = 'sale.order.line'
