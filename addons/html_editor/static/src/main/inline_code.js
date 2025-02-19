ec2fd2a7de379d9e5d269ab9fc6dcc015b3cdfb9
dhba <dhba@odoo.com>
2025-02-19 16:04:12 +0530

    [IMP] html_editor: wrap selected text in inline code
    Purpose:
    
    - Wrap the selected text in a inline code element when the '`' key is pressed.
    
    task: 4585706

--- addons/html_editor/static/src/main/inline_code.js
+++ addons/html_editor/static/src/main/inline_code.js
@@ -1,20 +1,52 @@
 import { Plugin } from "@html_editor/plugin";
-import { splitTextNode } from "@html_editor/utils/dom";
+import { isBlock } from "@html_editor/utils/blocks";
+import { splitTextNode, unwrapContents } from "@html_editor/utils/dom";
+import { isTextNode } from "@html_editor/utils/dom_info";
 import { closestElement } from "@html_editor/utils/dom_traversal";
-import { DIRECTIONS } from "@html_editor/utils/position";
+import { DIRECTIONS, nodeSize } from "@html_editor/utils/position";
+import { withSequence } from "@html_editor/utils/resource";
 
 export class InlineCodePlugin extends Plugin {
     static id = "inlineCode";
-    static dependencies = ["selection", "history", "input"];
+    static dependencies = ["selection", "history", "input", "split"];
     resources = {
         input_handlers: this.onInput.bind(this),
+        beforeinput_handlers: withSequence(1, this.onBeforeInput.bind(this)),
     };
+
+    onBeforeInput() {
+        const selection = this.dependencies.selection.getEditableSelection();
+        const traversedBlocks = this.dependencies.selection.getTraversedBlocks();
+        const hasTextNode = this.dependencies.selection.getTraversedNodes().some(isTextNode);
+        if (!selection.isCollapsed && traversedBlocks.size === 1 && hasTextNode) {
+            this.historySavePointRestore = this.dependencies.history.makeSavePoint();
+        }
+    }
 
     onInput(ev) {
         const selection = this.dependencies.selection.getEditableSelection();
         if (ev.data !== "`" || closestElement(selection.anchorNode, "code")) {
             return;
         }
+        if (this.historySavePointRestore) {
+            this.historySavePointRestore();
+            this.dependencies.split.splitSelection();
+            const traversedNodes = this.dependencies.selection
+                .getSelectedNodes()
+                .filter((node) => isTextNode(node) || node.nodeName === "IMG");
+            if (traversedNodes.length) {
+                const codeElement = this.insertInlineCode(traversedNodes);
+                codeElement.after(document.createTextNode("\u200B"));
+                this.dependencies.selection.setSelection({
+                    anchorNode: codeElement,
+                    anchorOffset: nodeSize(codeElement),
+                });
+                this.dependencies.history.addStep();
+            }
+            delete this.historySavePointRestore;
+            return;
+        }
+
         // We just inserted a backtick, check if there was another
         // one in the text.
         let textNode = selection.startContainer;
@@ -33,7 +65,10 @@ export class InlineCodePlugin extends Plugin {
             sibling.remove();
             sibling = textNode.nextSibling;
         }
-        this.dependencies.selection.setSelection({ anchorNode: textNode, anchorOffset: offset });
+        this.dependencies.selection.setSelection({
+            anchorNode: textNode,
+            anchorOffset: offset,
+        });
         const textHasTwoTicks = /`.*`/.test(textNode.textContent);
         // We don't apply the code tag if there is no content between the two `
         if (textHasTwoTicks && textNode.textContent.replace(/`/g, "").length) {
@@ -69,16 +104,7 @@ export class InlineCodePlugin extends Plugin {
                 textNode.textContent.length - 1
             );
             // Insert code element.
-            const codeElement = this.document.createElement("code");
-            codeElement.classList.add("o_inline_code");
-            textNode.before(codeElement);
-            codeElement.append(textNode);
-            if (
-                !codeElement.previousSibling ||
-                codeElement.previousSibling.nodeType !== Node.TEXT_NODE
-            ) {
-                codeElement.before(document.createTextNode("\u200B"));
-            }
+            const codeElement = this.insertInlineCode([textNode]);
             if (isClosingForward) {
                 // Move selection out of code element.
                 codeElement.after(document.createTextNode("\u200B"));
@@ -92,7 +118,50 @@ export class InlineCodePlugin extends Plugin {
                     anchorOffset: 0,
                 });
             }
+            this.dependencies.history.addStep();
         }
-        this.dependencies.history.addStep();
+    }
+
+    insertInlineCode(textNode) {
+        let codeElement = document.createElement("code");
+        codeElement.classList.add("o_inline_code");
+        let shouldBreakCodeEl = false;
+        // Wrap each selected text node inside a code element
+        for (let node of textNode) {
+            if (node.nodeName === "IMG" || closestElement(node, "a")) {
+                shouldBreakCodeEl = true;
+                continue;
+            }
+            if (shouldBreakCodeEl) {
+                codeElement = document.createElement("code");
+                codeElement.classList.add("o_inline_code");
+                shouldBreakCodeEl = false;
+            }
+            let parentElement = node.parentElement;
+            while (parentElement) {
+                // If node is inline code then unwrap it to avoid nested inline code block
+                if (node.nodeName === "CODE") {
+                    parentElement = node.parentElement;
+                    [node] = unwrapContents(node);
+                } else if (isBlock(parentElement)) {
+                    break;
+                } else {
+                    // split Element until parent Element
+                    node = this.dependencies.split.splitAroundUntil(node, parentElement);
+                    parentElement = node.parentElement;
+                }
+            }
+            if (!codeElement.isConnected) {
+                node.before(codeElement);
+            }
+            codeElement.append(node);
+        }
+        if (
+            !codeElement.previousSibling ||
+            codeElement.previousSibling.nodeType !== Node.TEXT_NODE
+        ) {
+            codeElement.before(document.createTextNode("\u200B"));
+        }
+        return codeElement;
     }
 }
