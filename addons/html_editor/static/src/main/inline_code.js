93515de8beed17eec3c5a2453b8a079be9aad741
dhba <dhba@odoo.com>
2025-02-19 16:04:12 +0530

    [IMP] html_editor: wrap selected text in inline code
    Purpose:
    
    - Wrap the selected text in a inline code element when the '`' key is pressed.
    
    task: 4585706

--- addons/html_editor/static/src/main/inline_code.js
+++ addons/html_editor/static/src/main/inline_code.js
@@ -1,98 +1,163 @@
 import { Plugin } from "@html_editor/plugin";
-import { splitTextNode } from "@html_editor/utils/dom";
+import { isBlock } from "@html_editor/utils/blocks";
+import { splitTextNode, unwrapContents } from "@html_editor/utils/dom";
+import { isTextNode } from "@html_editor/utils/dom_info";
 import { closestElement } from "@html_editor/utils/dom_traversal";
-import { DIRECTIONS } from "@html_editor/utils/position";
+import { DIRECTIONS, nodeSize } from "@html_editor/utils/position";
+import { withSequence } from "@html_editor/utils/resource";
 
 export class InlineCodePlugin extends Plugin {
     static id = "inlineCode";
-    static dependencies = ["selection", "history", "input"];
+    static dependencies = ["selection", "history", "input", "split"];
     resources = {
         input_handlers: this.onInput.bind(this),
+        beforeinput_handlers: withSequence(1, this.onBeforeInput.bind(this)),
     };
+
+    onBeforeInput() {
+        const selection = this.dependencies.selection.getEditableSelection();
+        const traversedBlocks = this.dependencies.selection.getTraversedBlocks();
+        const traversedNodes = this.dependencies.selection
+            .getTraversedNodes()
+            .filter((node) => isTextNode(node));
+        if (!selection.isCollapsed && traversedBlocks.size < 2 && traversedNodes.length) {
+            this.historySavePointRestore = this.dependencies.history.makeSavePoint();
+        }
+    }
 
     onInput(ev) {
         const selection = this.dependencies.selection.getEditableSelection();
         if (ev.data !== "`" || closestElement(selection.anchorNode, "code")) {
             return;
         }
-        // We just inserted a backtick, check if there was another
-        // one in the text.
-        let textNode = selection.startContainer;
-        let offset = selection.startOffset;
-        let sibling = textNode.previousSibling;
-        while (sibling && sibling.nodeType === Node.TEXT_NODE) {
-            offset += sibling.textContent.length;
-            sibling.textContent += textNode.textContent;
-            textNode.remove();
-            textNode = sibling;
-            sibling = textNode.previousSibling;
-        }
-        sibling = textNode.nextSibling;
-        while (sibling && sibling.nodeType === Node.TEXT_NODE) {
-            textNode.textContent += sibling.textContent;
-            sibling.remove();
-            sibling = textNode.nextSibling;
-        }
-        this.dependencies.selection.setSelection({ anchorNode: textNode, anchorOffset: offset });
-        const textHasTwoTicks = /`.*`/.test(textNode.textContent);
-        // We don't apply the code tag if there is no content between the two `
-        if (textHasTwoTicks && textNode.textContent.replace(/`/g, "").length) {
-            this.dependencies.history.addStep();
-            const insertedBacktickIndex = offset - 1;
-            const textBeforeInsertedBacktick = textNode.textContent.substring(
-                0,
-                insertedBacktickIndex - 1
-            );
-            let startOffset, endOffset;
-            const isClosingForward = textBeforeInsertedBacktick.includes("`");
-            if (isClosingForward) {
-                // There is a backtick before the new backtick.
-                startOffset = textBeforeInsertedBacktick.lastIndexOf("`");
-                endOffset = insertedBacktickIndex;
-            } else {
-                // There is a backtick after the new backtick.
-                const textAfterInsertedBacktick = textNode.textContent.substring(offset);
-                startOffset = insertedBacktickIndex;
-                endOffset = offset + textAfterInsertedBacktick.indexOf("`");
-            }
-            // Split around the backticks if needed so text starts
-            // and ends with a backtick.
-            if (endOffset && endOffset < textNode.textContent.length) {
-                splitTextNode(textNode, endOffset + 1, DIRECTIONS.LEFT);
-            }
-            if (startOffset) {
-                splitTextNode(textNode, startOffset);
-            }
-            // Remove ticks.
-            textNode.textContent = textNode.textContent.substring(
-                1,
-                textNode.textContent.length - 1
-            );
-            // Insert code element.
-            const codeElement = this.document.createElement("code");
-            codeElement.classList.add("o_inline_code");
-            textNode.before(codeElement);
-            codeElement.append(textNode);
-            if (
-                !codeElement.previousSibling ||
-                codeElement.previousSibling.nodeType !== Node.TEXT_NODE
-            ) {
-                codeElement.before(document.createTextNode("\u200B"));
-            }
-            if (isClosingForward) {
-                // Move selection out of code element.
+        if (this.historySavePointRestore) {
+            this.historySavePointRestore();
+            this.dependencies.split.splitSelection();
+            const traversedNodes = this.dependencies.selection
+                .getSelectedNodes()
+                .filter((node) => isTextNode(node) || node.nodeName === "IMG");
+            if (traversedNodes.length > 0) {
+                const codeElement = this.insertInlineCode(traversedNodes);
                 codeElement.after(document.createTextNode("\u200B"));
                 this.dependencies.selection.setSelection({
-                    anchorNode: codeElement.nextSibling,
-                    anchorOffset: 1,
+                    anchorNode: codeElement,
+                    anchorOffset: nodeSize(codeElement),
                 });
-            } else {
-                this.dependencies.selection.setSelection({
-                    anchorNode: codeElement.firstChild,
-                    anchorOffset: 0,
-                });
+                this.dependencies.history.addStep();
+            }
+            delete this.historySavePointRestore;
+        } else {
+            // We just inserted a backtick, check if there was another
+            // one in the text.
+            let textNode = selection.startContainer;
+            let offset = selection.startOffset;
+            let sibling = textNode.previousSibling;
+            while (sibling && sibling.nodeType === Node.TEXT_NODE) {
+                offset += sibling.textContent.length;
+                sibling.textContent += textNode.textContent;
+                textNode.remove();
+                textNode = sibling;
+                sibling = textNode.previousSibling;
+            }
+            sibling = textNode.nextSibling;
+            while (sibling && sibling.nodeType === Node.TEXT_NODE) {
+                textNode.textContent += sibling.textContent;
+                sibling.remove();
+                sibling = textNode.nextSibling;
+            }
+            this.dependencies.selection.setSelection({
+                anchorNode: textNode,
+                anchorOffset: offset,
+            });
+            const textHasTwoTicks = /`.*`/.test(textNode.textContent);
+            // We don't apply the code tag if there is no content between the two `
+            if (textHasTwoTicks && textNode.textContent.replace(/`/g, "").length) {
+                this.dependencies.history.addStep();
+                const insertedBacktickIndex = offset - 1;
+                const textBeforeInsertedBacktick = textNode.textContent.substring(
+                    0,
+                    insertedBacktickIndex - 1
+                );
+                let startOffset, endOffset;
+                const isClosingForward = textBeforeInsertedBacktick.includes("`");
+                if (isClosingForward) {
+                    // There is a backtick before the new backtick.
+                    startOffset = textBeforeInsertedBacktick.lastIndexOf("`");
+                    endOffset = insertedBacktickIndex;
+                } else {
+                    // There is a backtick after the new backtick.
+                    const textAfterInsertedBacktick = textNode.textContent.substring(offset);
+                    startOffset = insertedBacktickIndex;
+                    endOffset = offset + textAfterInsertedBacktick.indexOf("`");
+                }
+                // Split around the backticks if needed so text starts
+                // and ends with a backtick.
+                if (endOffset && endOffset < textNode.textContent.length) {
+                    splitTextNode(textNode, endOffset + 1, DIRECTIONS.LEFT);
+                }
+                if (startOffset) {
+                    splitTextNode(textNode, startOffset);
+                }
+                // Remove ticks.
+                textNode.textContent = textNode.textContent.substring(
+                    1,
+                    textNode.textContent.length - 1
+                );
+                // Insert code element.
+                const codeElement = this.insertInlineCode([textNode]);
+                if (isClosingForward) {
+                    // Move selection out of code element.
+                    codeElement.after(document.createTextNode("\u200B"));
+                    this.dependencies.selection.setSelection({
+                        anchorNode: codeElement.nextSibling,
+                        anchorOffset: 1,
+                    });
+                } else {
+                    this.dependencies.selection.setSelection({
+                        anchorNode: codeElement.firstChild,
+                        anchorOffset: 0,
+                    });
+                }
+                this.dependencies.history.addStep();
             }
         }
-        this.dependencies.history.addStep();
+    }
+
+    insertInlineCode(textNode) {
+        let codeElement = document.createElement("code");
+        codeElement.classList.add("o_inline_code");
+        // Wrap each selected text node inside a code element
+        for (let node of textNode) {
+            if (node.nodeName === "IMG") {
+                codeElement = document.createElement("code");
+                codeElement.classList.add("o_inline_code");
+                continue;
+            }
+            let parentElement = node.parentElement;
+            while (parentElement) {
+                // If node is inline code then unwrap it to avoid nested inline code block
+                if (node.nodeName === "CODE") {
+                    parentElement = node.parentElement;
+                    [node] = unwrapContents(node);
+                } else if (isBlock(parentElement)) {
+                    break;
+                } else {
+                    // split Element until parent Element
+                    node = this.dependencies.split.splitAroundUntil(node, parentElement);
+                    parentElement = node.parentElement;
+                }
+            }
+            if (!codeElement.isConnected) {
+                node.before(codeElement);
+            }
+            codeElement.append(node);
+        }
+        if (
+            !codeElement.previousSibling ||
+            codeElement.previousSibling.nodeType !== Node.TEXT_NODE
+        ) {
+            codeElement.before(document.createTextNode("\u200B"));
+        }
+        return codeElement;
     }
 }
