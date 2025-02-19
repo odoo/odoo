61720bea47350775b6b77c54938bd33ba2c36819
Anita (anko) <anko@odoo.com>
2025-02-19 11:37:28 +0100

    [IMP] sale_gelato : visual improvements
    Ecommerce description is moved before quotation description and
    gelato group is moved after quotation description. Gelato setting is
    visible from sales settings.
    
    closes odoo/odoo#198360
    
    Related: odoo/upgrade#7271
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>

--- addons/sale_gelato/wizards/res_config_settings.py
+++ addons/sale_gelato/wizards/res_config_settings.py
@@ -6,7 +6,5 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    module_sale_gelato = fields.Boolean("Gelato")
-
     gelato_api_key = fields.Char(related='company_id.gelato_api_key', readonly=False)
     gelato_webhook_secret = fields.Char(related='company_id.gelato_webhook_secret', readonly=False)
