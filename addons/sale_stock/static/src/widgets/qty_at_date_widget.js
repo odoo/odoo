67e6ef9ced81281326dffae43db393ac60c6a2fa
Mohammad Abdulmoneim (abdu) <abdu@odoo.com>
2025-02-20 14:22:55 +0100

    [IMP] sale_stock: show quantity in product unit in availability widget
    This commit modifies the availability forecast widget used on sale order
    lines so that it shows the forecast in the product unit in addition to
    the packaging unit (unit of sale order line) in case it's different
    than the product base unit.
    
    Task-4471379
    
    Part-of: odoo/odoo#207785
    Related: odoo/enterprise#84325
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>
    Signed-off-by: Mohammad Abdulmoneim (abdu) <abdu@odoo.com>

--- addons/sale_stock/static/src/widgets/qty_at_date_widget.js
+++ addons/sale_stock/static/src/widgets/qty_at_date_widget.js
@@ -5,6 +5,7 @@ import { useService } from "@web/core/utils/hooks";
 import { usePopover } from "@web/core/popover/popover_hook";
 import { Component, onWillRender } from "@odoo/owl";
 import { standardWidgetProps } from "@web/views/widgets/standard_widget_props";
+import { roundPrecision } from "@web/core/utils/numbers";
 
 export class QtyAtDatePopover extends Component {
     static template = "sale_stock.QtyAtDatePopover";
@@ -37,15 +38,28 @@ export class QtyAtDateWidget extends Component {
     static props = {...standardWidgetProps};
     setup() {
         this.popover = usePopover(this.constructor.components.Popover, { position: "top" });
+        this.orm = useService("orm");
         this.calcData = {};
-        onWillRender(() => {
-            this.initCalcData();
+        onWillRender(async () => {
+            await this.initCalcData();
         })
     }
 
-    initCalcData() {
+    async initCalcData() {
         // calculate data not in record
         const { data } = this.props.record;
+        let lineUom;
+        if (data.product_uom_id?.[0]) {
+            lineUom = (await this.orm.read("uom.uom", [data.product_uom_id[0]], ["factor", "rounding"]))[0];
+        }
+        let lineProduct;
+        if (data.product_id?.[0]) {
+            lineProduct = await this.orm.searchRead("product.product", [["id", "=", data.product_id[0]]], ["uom_id"]);
+        }
+        let productUom;
+        if (lineProduct?.[0]?.uom_id?.[0]) {
+            productUom = (await this.orm.searchRead("uom.uom", [["id", "=", lineProduct[0].uom_id[0]]], ["factor", "name"]))[0];
+        }
         if (data.scheduled_date) {
             // TODO: might need some round_decimals to avoid errors
             if (data.state === 'sale') {
@@ -61,6 +75,11 @@ export class QtyAtDateWidget extends Component {
                 // Moves are created, using the forecasted data of related moves
                 this.calcData.forecasted_issue = !this.calcData.will_be_fulfilled || this.calcData.will_be_late;
             }
+        }
+        if (lineUom && productUom) {
+            this.calcData.product_uom_virtual_available_at_date = roundPrecision(data.virtual_available_at_date * lineUom.factor / productUom.factor, 1);
+            this.calcData.product_uom_free_qty_today = roundPrecision(data.free_qty_today * lineUom.factor / productUom.factor, 1);
+            this.calcData.product_uom_name = productUom.name;
         }
     }
 
