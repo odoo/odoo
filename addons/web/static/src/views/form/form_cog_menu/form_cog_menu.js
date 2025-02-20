1ec6f2d6e308c503a6da77bee5d880a177666bd9
Pierre Paridans <app@odoo.com>
2025-02-20 16:49:14 +0100

    [IMP] web: statusbar buttons display first, dropdown rest on mobile
    This commit adapts the statusbar buttons layout on smaller screen to
    make them more discoverable than moving them all to the CogMenu.
    
    In a nutshell, the first button is displayed like on bigger screens, but
    the other ones (if any) are folded into a dropdown.
    
    task-4586651

--- addons/web/static/src/views/form/form_cog_menu/form_cog_menu.js
+++ addons/web/static/src/views/form/form_cog_menu/form_cog_menu.js
@@ -1,10 +1,5 @@
 import { CogMenu } from "@web/search/cog_menu/cog_menu";
-import { StatusBarDropdownItems } from "../status_bar_dropdown_items/status_bar_dropdown_items";
 
 export class FormCogMenu extends CogMenu {
     static template = "web.FormCogMenu";
-    static components = {
-        ...CogMenu.components,
-        StatusBarDropdownItems,
-    };
 }
