651d9d89499e09c9c2340a3ab408457ea25a434f
Lucas Lefèvre (lul) <lul@odoo.com>
2025-02-20 14:06:21 +0000

    [FIX] spreadsheet: fix inconsistent day of week
    Steps to reproduce:
    
    - change your language to French (or any language which doesn't the week on Sundays)
    - insert a pivot into a spreadsheet
    - insert the dynamic version with =PIVOT(1)
    - change the row groups to "Day of week"
    - right click on any pivot value and "See records"
    
    => the day of week displayed in spreadsheet doesn't match the records.
    
    closes odoo/odoo#199181
    
    Task: 4591993
    X-original-commit: 69b5acc0ff9ec29e0b61a6b8d089a784dbe5cee6
    Related: odoo/enterprise#80113
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>

--- addons/spreadsheet/static/src/pivot/odoo_pivot.js
+++ addons/spreadsheet/static/src/pivot/odoo_pivot.js
@@ -195,6 +195,7 @@ export class OdooPivot {
             {
                 orm: this.odooDataProvider.orm,
                 serverData: this.odooDataProvider.serverData,
+                getters: this.getters,
             }
         );
         return { model, definition };
