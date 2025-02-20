50ceb96421d1cddc35d9676aa7f53a99565f9e76
adch-odoo <adch@odoo.com>
2025-02-20 09:13:34 +0000

    [FIX] html_editor: preserve bullet for image only list item
    Steps to reproduce:
    
    - Open the To-Do app.
    - Create a list (e.g., `/list`).
    - Insert an image into a list item and press `Enter`.
    - Notice that the list item containing the image is outdented.
    
    Solution:
    
    - In `handleSplitBlock`, the `!closestLI.textContent` check did not handle list
    items containing only an image. Adding an `isEmptyBlock` check ensures
    the bullet point is preserved.
    
    Before this commit:
    
    - Pressing Enter in list item containing only an image removed the bullet point.
    
    After this commit:
    
    - Pressing Enter in a list item with only an image preserves the bullet point
    and creates a new empty list item.
    
    task-4586704
    
    closes odoo/odoo#200300
    
    X-original-commit: ee59572e2d4775b4b0779c131fa945d4d3af40a7
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/html_editor/static/src/main/list/list_plugin.js
+++ addons/html_editor/static/src/main/list/list_plugin.js
@@ -800,7 +800,7 @@ export class ListPlugin extends Plugin {
         if (!closestLI || isBlockUnsplittable) {
             return;
         }
-        if (!closestLI.textContent) {
+        if (isEmptyBlock(closestLI)) {
             this.outdentLI(closestLI);
             return true;
         }
