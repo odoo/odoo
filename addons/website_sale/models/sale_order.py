4e4ef97ff3bf5aa62c00ab9388bfb892240ad90d
Valentin Chevalier <vcr@odoo.com>
2025-02-20 17:55:42 +0000

    [FIX] website_sale: don't aggregate combos in the cart
    Since 7641ac6, combos are wrongly aggregated in the cart.
    
    This commit address this issue.
    
    closes odoo/odoo#199351
    
    X-original-commit: 61e3f5f38f804308e421792c88001abae2334135
    Signed-off-by: Valentin Chevalier <vcr@odoo.com>

--- addons/website_sale/models/sale_order.py
+++ addons/website_sale/models/sale_order.py
@@ -335,6 +335,10 @@ class SaleOrder(models.Model):
         if not self.order_line:
             return self.env['sale.order.line']
 
+        product = self.env['product.product'].browse(product_id)
+        if product.type == 'combo':
+            return self.env['sale.order.line']
+
         domain = [
             ('product_id', '=', product_id),
             ('product_custom_attribute_value_ids', '=', False),
@@ -345,7 +349,6 @@ class SaleOrder(models.Model):
         if not filtered_sol:
             return self.env['sale.order.line']
 
-        product = self.env['product.product'].browse(product_id)
         if product.product_tmpl_id._has_no_variant_attributes():
             filtered_sol = filtered_sol.filtered(
                 lambda sol:
