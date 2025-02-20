bb6c715baba9db95689c17d05dd05bed92e22278
paru-odoo <paru@odoo.com>
2025-02-20 06:49:00 +0000

    [FIX] web, website: resolve traceback when leaving edit mode via browser
    Steps to reproduce:
    1. Go to Website > Edit.
    2. Open the snippet modal and select any snippet.
    3. Press the 'Back' button in your browser.
    4. A dialog will appear asking to discard changes; click 'OK'.
    5. A traceback error occurs, and an empty space appears in the editor.
    
    Issue:
    - Previously, a commit addressed a similar scenario, but that time the
      browser had an event listener bind on hashchange.
    - Now, that `hashchange` event of browser has been replaced with
      `popstate`, which triggers before the 'window' event listener.
    - As a result, the editor is left in an unstable state, causing a
      traceback error.
    
    Solution:
    - This commit ensures the 'window' event executes before the browser
      event.
    - It verifies if the editor is open and forces a `skipLoad`, preventing
      the `route_change` call in the browser.
    
    task-4570164
    
    closes odoo/odoo#235709
    
    X-original-commit: 26571941dd63949ee9373f4a31463228c98ccb87
    Signed-off-by: Benoit Socias (bso) <bso@odoo.com>
    Signed-off-by: Rudra Patel (paru) <paru@odoo.com>

--- addons/website/tests/test_ui.py
+++ addons/website/tests/test_ui.py
@@ -12,6 +12,7 @@ from odoo.addons.base.tests.common import HttpCaseWithUserDemo
 from odoo.addons.web_editor.controllers.main import Web_Editor
 from odoo.addons.website.tests.common import HttpCaseWithWebsiteUser
 from odoo.fields import Command
+from odoo.tools import mute_logger
 
 
 @odoo.tests.tagged('-at_install', 'post_install')
@@ -756,3 +757,7 @@ class TestUi(HttpCaseWithWebsiteUser):
 
     def test_website_edit_megamenu_visibility(self):
         self.start_tour("/", 'edit_megamenu_visibility', login='admin')
+
+    @mute_logger("odoo.http")
+    def test_website_replace_remove_image(self):
+        self.start_tour("/", "website_replace_remove_image", login="admin")
