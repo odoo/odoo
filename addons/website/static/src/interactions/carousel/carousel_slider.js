cdd02d65ceeb5470d9152b366138fa4bbc62b072
cove <cove@odoo.com>
2025-02-20 14:14:16 +0100

    [IMP] website: implement single-scroll mode on dynamic carousel
    This commit introduces a new single-scroll carousel mode, which allows
    the carousel to display multiple items at once (e.g. each carousel item
    containing a single column of products) and enables the user to scroll
    through items (or columns) one by one, as opposed to the "normal" mode
    where only one item is displayed (potentially containing multiple
    columns).
    
    Key Changes:
    - Editor option: A new option has been added in the editor to enable
      single-scroll mode for the carousel. The no-preview flag is included to
      avoid content re-renders on hover.
    
    - Single-scroll template: The carousel template is adapted to display
      multiple carousel-items simultaneously (in the products snippet case:
      each containing a single column of product).
    
    - Custom CSS class: A custom class `.o_carousel_multi_items` is
      introduced to modify the default Bootstrap carousel behavior. This class
      enables a single-item scroll, while multiple are displayed at the same
      time, compared to the default multi-item carousel setup. By default,
      4 items are displayed but it can be changed by changing the item sizes
      with the CSS variable `--o-carousel-item-width-percentage` (which
      is thus 25% by default).
    
    - Slide preloading: The carousel experienced a brief animation delay on
      the first slide due to images in loading=lazy. Now it preloads slides
      that are likely to appear, reducing animation delay or prevent the
      carousel to slide if image are not loaded.
    
    This approach allows the carousel to maintain compatibility with the
    default Bootstrap carousel (main reason of this choice).
    The templates for dynamic_snippet_carousel were also updated to
    accommodate the new single-item-per-slide design.
    
    task-4309599
    
    Co-authored-by: qsm-odoo <qsm@odoo.com>

--- addons/website/static/src/interactions/carousel/carousel_slider.js
+++ addons/website/static/src/interactions/carousel/carousel_slider.js
@@ -4,6 +4,10 @@ import { registry } from "@web/core/registry";
 export class CarouselSlider extends Interaction {
     static selector = ".carousel";
     dynamicContent = {
+        _root: {
+            "t-on-slide.bs.carousel": this.onSlideCarousel,
+            "t-on-slid.bs.carousel": this.onSlidCarousel,
+        },
         "img": {
             "t-on-load": this.computeMaxHeight,
         },
@@ -27,6 +31,26 @@ export class CarouselSlider extends Interaction {
         this.updateContent();
         const carouselBS = window.Carousel.getOrCreateInstance(this.el, this.carouselOptions);
         this.registerCleanup(() => carouselBS.dispose());
+
+        this.carouselInnerEl = this.el.querySelector(".carousel-inner");
+
+        const itemWidth = getComputedStyle(this.el).getPropertyValue("--o-carousel-item-width-percentage");
+        const itemsPerSlide = itemWidth ? Math.round(100 / parseFloat(itemWidth)) : 1;
+        this.options = {
+            scrollMode: this.el.classList.contains('o_carousel_multi_items') ? 'single' : 'all',
+            itemsPerSlide: itemsPerSlide,
+        };
+
+        // Preload first items only when carousel is on screen
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    this.loadItemsToAppear();
+                    observer.unobserve(this.el);
+                }
+            });
+        });
+        observer.observe(this.el);
     }
 
     computeMaxHeight() {
@@ -39,6 +63,113 @@ export class CarouselSlider extends Interaction {
                 this.maxHeight = height;
             }
             itemEl.classList.toggle("active", isActive);
+        }
+    }
+
+    /**
+     * Handles the 'slide' event of the carousel. Called *before* a slide
+     * transition.
+     *
+     * @param {Event} ev The Bootstrap Carousel slide event.
+     */
+    onSlideCarousel(ev) {
+        const imageEls = [...this.carouselInnerEl.querySelectorAll("img")];
+        const isLoading = imageEls.some(el => el.loading !== "lazy" && !el.complete);
+        if (isLoading) {
+            // If images are loading, prevent the slide transition. It will
+            // slide once the next images are loaded.
+            ev.preventDefault();
+            return;
+        }
+        if (this.options.scrollMode === "single") {
+            this.onSlideSingleScroll(ev);
+        }
+    }
+
+    /**
+     * Handles the 'slid' event of the carousel. Called *after* a slide
+     * transition.
+     *
+     * @param {Event} ev The Bootstrap Carousel slid event.
+     */
+    onSlidCarousel(ev) {
+        if (this.options.scrollMode === "single") {
+            this.onSlidSingleScroll(ev);
+        }
+        this.loadItemsToAppear(); // Preload future items after a slide
+    }
+
+    /**
+     * Manages multi-items single-scroll behavior during the 'slide' event.
+     * Prepares the DOM for smooth transitions by moving elements.
+     *
+     * @param {Event} ev The Bootstrap Carousel slide event.
+     */
+    onSlideSingleScroll(ev) {
+        // We need to keep the active element at the beginning of the carousel-items elements
+        // This allows to have a smooth transition when the carousel is sliding
+        if (ev.direction === "right") {
+            const carouselItemsEls = Array.from(this.carouselInnerEl.querySelectorAll(".carousel-item"));
+            this.carouselInnerEl.prepend(carouselItemsEls.pop());
+        }
+    }
+
+    /**
+     * Manages single-item scroll behavior during the 'slid' event.
+     * Completes the DOM manipulation started in `onSlideSingleScroll`.
+     *
+     * @param {Event} ev The Bootstrap Carousel slid event.
+     */
+    onSlidSingleScroll(ev) {
+        // As for the onSlideSingleScroll method, we need to keep the active
+        // element at the beginning of the carousel-items list in the DOM. So
+        // when animation is done, we move the first item (which is not active
+        // anymore) to the end.
+        if (ev.direction === "left") {
+            const carouselItemsEls = this.carouselInnerEl.querySelectorAll(".carousel-item");
+            this.carouselInnerEl.appendChild(carouselItemsEls[0]);
+        }
+    }
+
+    /**
+     * Loads images of the carousel-item necessary for both 'prev' and 'next'
+     * animations. Loads images for items that are about to become visible.
+     *
+     * @param {number} [nbItemsToLoad=1] The number of items to preload on each side.
+     */
+    loadItemsToAppear(nbItemsToLoad = 1) {
+        const itemEls = Array.from(this.carouselInnerEl.children);
+        const index = itemEls.findIndex(el => el.classList.contains("active"));
+        const activeItemIndex = index >= 0 ? index : 0;
+
+        // Load "Next" items: nbItemsToLoad items after the active element
+        const nbItemElsOnScreen = this.options.scrollMode === "single" ? this.options.itemsPerSlide + 1 : 1;
+        const nextEndIndex = Math.min(activeItemIndex + nbItemElsOnScreen + nbItemsToLoad + 1, itemEls.length);
+        const nextItemElsToLoad = itemEls.slice(activeItemIndex, nextEndIndex);
+
+        // load "Prev" items : nbItemsToLoad items before the active element (circular wrapping)
+        // if currentIndex is 0, then the nbItemsToLoad items are the last elements of the carousel
+        let prevItemElsToLoad = [];
+        if (activeItemIndex - nbItemsToLoad < 0) {
+            const wrapAmount = Math.abs(activeItemIndex - nbItemsToLoad);
+            prevItemElsToLoad = itemEls.slice(itemEls.length - wrapAmount, itemEls.length).reverse().concat(itemEls.slice(0, activeItemIndex).reverse());
+        } else {
+            prevItemElsToLoad = itemEls.slice(Math.max(0, activeItemIndex - nbItemsToLoad), activeItemIndex).reverse();
+        }
+
+        // Set the `loading` attribute of lazy-loaded images to `eager` for the
+        // given carousel items. This forces the browser to load the images
+        // immediately.
+        const itemsToLoad = nextItemElsToLoad.concat(prevItemElsToLoad);
+        for (let carouselItemEl of itemsToLoad) {
+            const imageEls = carouselItemEl.querySelectorAll("img[loading='lazy']");
+            for (const imageEl of imageEls) {
+                // Note that we remove the attribute with the goal of forcing it
+                // to the "eager" value. Removing the attribute is better so
+                // that the attribute is not saved as eager in edit mode (the
+                // lazy value is auto added on page rendering).
+                imageEl.removeAttribute("loading");
+            }
         }
     }
 }
