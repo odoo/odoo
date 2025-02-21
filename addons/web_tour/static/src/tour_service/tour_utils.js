2568f395cb66523fa32a2fa378cd58f3ed8ac38c
Pierre Pulinckx <pipu@odoo.com>
2025-02-21 11:57:29 +0100

    [REF] web_tour: improve check for non determinstic errors
    in this commit, we improve the method to look for indeterminisms in
    the tours. The method waits a certain amount of time once the trigger
    has been found and then checks if the element is still visible, if it
    has changed or if it has undergone mutations.
    We take advantage of this commit to also improve the method to find
    the trigger in the DOM (we check that body does not have the class
    o_is_blocked and that the frame is is-ready=true if the element is in
    an iframe)
    
    closes odoo/odoo#198596
    
    Signed-off-by: Julien Mougenot (jum) <jum@odoo.com>

--- addons/web_tour/static/src/tour_service/tour_utils.js
+++ addons/web_tour/static/src/tour_service/tour_utils.js
@@ -25,6 +25,69 @@ export function callWithUnloadCheck(func, ...args) {
     }
 }
 
+function formatValue(key, value, maxLength = 200) {
+    if (!value) {
+        return "(empty)";
+    }
+    return value.length > maxLength ? value.slice(0, maxLength) + "..." : value;
+}
+
+function serializeNode(node) {
+    if (node.nodeType === Node.TEXT_NODE) {
+        return `"${node.nodeValue.trim()}"`;
+    }
+    return node.outerHTML ? formatValue("node", node.outerHTML, 500) : "[Unknown Node]";
+}
+
+export function serializeChanges(snapshot, current) {
+    const changes = {
+        node: serializeNode(current),
+    };
+    function pushChanges(key, obj) {
+        changes[key] = changes[key] || [];
+        changes[key].push(obj);
+    }
+
+    if (snapshot.textContent !== current.textContent) {
+        pushChanges("modifiedText", { before: snapshot.textContent, after: current.textContent });
+    }
+
+    const oldChildren = [...snapshot.childNodes].filter((node) => node.nodeType !== Node.TEXT_NODE);
+    const newChildren = [...current.childNodes].filter((node) => node.nodeType !== Node.TEXT_NODE);
+    oldChildren.forEach((oldNode, index) => {
+        if (!newChildren[index] || !oldNode.isEqualNode(newChildren[index])) {
+            pushChanges("removedNodes", { oldNode: serializeNode(oldNode) });
+        }
+    });
+    newChildren.forEach((newNode, index) => {
+        if (!oldChildren[index] || !newNode.isEqualNode(oldChildren[index])) {
+            pushChanges("addedNodes", { newNode: serializeNode(newNode) });
+        }
+    });
+
+    const oldAttrNames = new Set([...snapshot.attributes].map((attr) => attr.name));
+    const newAttrNames = new Set([...current.attributes].map((attr) => attr.name));
+    new Set([...oldAttrNames, ...newAttrNames]).forEach((attributeName) => {
+        const oldValue = snapshot.getAttribute(attributeName);
+        const newValue = current.getAttribute(attributeName);
+        const before = oldValue !== newValue || !newAttrNames.has(attributeName) ? oldValue : null;
+        const after = oldValue !== newValue || !oldAttrNames.has(attributeName) ? newValue : null;
+        if (before || after) {
+            pushChanges("modifiedAttributes", { attributeName, before, after });
+        }
+    });
+    return changes;
+}
+
+export function serializeMutation(mutation) {
+    const { type, attributeName } = mutation;
+    if (type === "attributes" && attributeName) {
+        return `attribute: ${attributeName}`;
+    } else {
+        return type;
+    }
+}
+
 /**
  * @param {HTMLElement} element
  * @returns {HTMLElement | null}
