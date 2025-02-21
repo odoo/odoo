3a0c75bc356975e6c520fa0e8dfb01bf74f0e775
Renaud Thiry <reth@odoo.com>
2025-02-21 11:16:52 +0100

    [IMP] marketing_card: allow picking a render language
    Currently cards are rendered in the language of the current user.
    Additionally translations are only performed for fields and not for static content.
    
    We now allow each record to have 1 card per language for any given card campaign.
    Users are now able to pick a language for their card mailing to be used
    when rendering the cards. If the mailing is for a card campaign
    it MUST have a language set to select which set of cards should
    be linked to the recipients.
    
    This allows card recipients to better promote their talks locally.
    
    Statistics will count 1 card per recipient even when they have multiple
    cards in different languages. Only the card with the highest status is
    counted towards statistics ("exists" < "opened" < "shared").
    
    task-4247003

--- addons/marketing_card/models/mailing_mailing.py
+++ addons/marketing_card/models/mailing_mailing.py
@@ -5,6 +5,11 @@ from odoo.fields import Domain
 class MailingMailing(models.Model):
     _inherit = 'mailing.mailing'
 
+    @api.model
+    def _lang_get(self):
+        return self.env['res.lang'].get_installed()
+
+    lang = fields.Selection(string='Language', selection=_lang_get, default=lambda self: self.env.lang)
     card_requires_sync_count = fields.Integer(compute="_compute_card_requires_sync_count")
     card_campaign_id = fields.Many2one('card.campaign', index='btree_not_null')
 
@@ -18,13 +23,19 @@ class MailingMailing(models.Model):
                         model_name=self.env['ir.model']._get(mailing.card_campaign_id.res_model).display_name
                     ))
 
+    @api.constrains('card_campaign_id', 'lang')
+    def _check_mailing_lang(self):
+        for mailing in self:
+            if mailing.card_campaign_id and not mailing.lang:
+                raise exceptions.ValidationError(_('Card Campaign Mailing should have a target language.'))
+
     @api.depends('card_campaign_id')
     def _compute_mailing_model_id(self):
         super()._compute_mailing_model_id()
         for mailing in self.filtered('card_campaign_id'):
             mailing.mailing_model_id = self.env['ir.model']._get_id(mailing.card_campaign_id.res_model)
 
-    @api.depends('card_campaign_id')
+    @api.depends('card_campaign_id', 'lang')
     def _compute_card_requires_sync_count(self):
         """Check if there's any missing or outdated card."""
         self.card_requires_sync_count = 0
@@ -35,7 +46,8 @@ class MailingMailing(models.Model):
             out_of_date_count = self.env['card.card'].search_count([
                 ('campaign_id', '=', mailing.card_campaign_id.id),
                 ('res_id', 'in', recipients.ids),
-                ('requires_sync', '=', False)
+                ('lang', '=', mailing.lang),
+                ('requires_sync', '=', False),
             ])
             mailing.card_requires_sync_count = len(recipients) - out_of_date_count
 
@@ -61,7 +73,7 @@ class MailingMailing(models.Model):
     def action_update_cards(self):
         """Update the cards in batches, commiting after each batch."""
         for campaign in self.filtered(lambda mailing: mailing.state == 'draft').card_campaign_id:
-            campaign._update_cards(self._parse_mailing_domain(), auto_commit=True)
+            campaign._update_cards(self._parse_mailing_domain(), self.lang, auto_commit=True)
         return {
             'type': 'ir.actions.act_window',
             'res_model': 'mailing.mailing',
@@ -74,6 +86,9 @@ class MailingMailing(models.Model):
         """Domain with an additional condition that the card must exist for the records."""
         domain = Domain(super()._get_recipients_domain())
         if self.card_campaign_id:
-            res_ids = self.env['card.card'].search_fetch([('campaign_id', '=', self.card_campaign_id.id)], ['res_id']).mapped('res_id')
+            res_ids = self.env['card.card'].search_fetch([
+                ('campaign_id', '=', self.card_campaign_id.id),
+                ('lang', '=', self.lang)
+            ], ['res_id']).mapped('res_id')
             domain &= Domain('id', 'in', res_ids)
         return domain
