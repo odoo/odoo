0bc83f37709a167d1dca8caaf82c62ad45db6a21
Julien Mougenot <jum@odoo.com>
2025-02-24 16:52:42 +0100

    [FIX] web: tests - mock server fixes and improvements
    This commit introduces several changes to the mock server:
    
    - it allows mock models to act as an extension to one another by sharing
    the same name (like on the actual server);
    
    - it prevents the use of `_records` after the server has been
    initialized: this was needed because the `_records` property would not
    be properly updated to reflect the actual list of records kept on each
    model (this worked *sometimes* as a side-effect, as records are
    shallowly copied);
    
    - all "mock server params" helpers (like `defineModels`,
    `defineActions`, etc.) now perform minimal processing outside of their
    inner `before` call, ensuring that dry test runs do not process too
    much unnecessary code (like spawning thousands of mock model instances)
    before it's actually needed;
    
    - it also slightly simplifies the mock server: cleaning up the overall
    API, removing dead code, and making some error messages more explicit.

--- addons/spreadsheet/static/tests/lists/list_plugin.test.js
+++ addons/spreadsheet/static/tests/lists/list_plugin.test.js
@@ -86,7 +86,7 @@ test("Numeric/monetary fields are correctly loaded and displayed", async () => {
         field_with_array_agg: 0,
         currency_id: 2,
         pognon: 0,
-    })
+    });
     const { model } = await createSpreadsheetWithList({
         columns: ["pognon", "probability", "field_with_array_agg"],
     });
@@ -987,7 +987,7 @@ test("can import (export) action xml id", async function () {
                 domain: [],
                 model: "partner",
                 orderBy: [],
-                actionXmlId: "spreadsheet.test_action"
+                actionXmlId: "spreadsheet.test_action",
             },
         },
     };
