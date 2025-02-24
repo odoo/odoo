odoo/odoo

commit b97e78c937723cedfab575115ea46b19f859df02

[IMP] sale, (*_)loyalty: taxes on discounted amounts
Before this commit:
Taxes are applied to all reward lines regardless of discount mode or discount
applicability .

After this commit:
Reward lines that reflect discounts on the order total will have a unit price
matching the discount amount, and taxes will not be applied unless they are set
manually for that reward or discount. This change applies when the discount mode
is either per point or per order, and the discount applicability is on the order
and also for fixed amount discounts.

closes odoo/odoo#167347

Task: 3527759
Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

addons/loyalty/views/loyalty_reward_views.xml
@@ -37,6 +37,14 @@
 
                         <group string="Discount" invisible="reward_type != 'discount' or program_type in ('gift_card','ewallet')">
                             <field name="discount_max_amount"/>
+                            <field
+                                name="tax_ids"
+                                placeholder="Untaxed discount"
+                                widget="many2many_tags"
+                                options="{'no_create': True}"
+                                invisible="discount_applicability != 'order' or
+                                    discount_mode not in ('per_order','per_point')"
+                            />
                             <field name="discount_product_domain" groups="base.group_no_one" widget="domain" options="{'model': 'product.product', 'in_dialog': true}" invisible="discount_applicability != 'specific'"/>
                             <field name="discount_product_ids" widget="many2many_tags" invisible="discount_applicability != 'specific'"/>
                             <field name="discount_product_category_id" invisible="discount_applicability != 'specific'"/>
@@ -75,6 +83,7 @@
         <field name="model">loyalty.reward</field>
         <field name="arch" type="xml">
             <kanban>
+                <field name="company_id"/>
                 <field name="currency_id"/>
                 <field name="reward_type"/>
                 <field name="discount_applicability"/>

addons/pos_loyalty/models/loyalty_reward.py
@@ -24,7 +24,7 @@ class LoyaltyReward(models.Model):
     def _load_pos_data_fields(self, config_id):
         return ['description', 'program_id', 'reward_type', 'required_points', 'clear_wallet', 'currency_id',
                 'discount', 'discount_mode', 'discount_applicability', 'all_discount_product_ids', 'is_global_discount',
-                'discount_max_amount', 'discount_line_product_id', 'reward_product_id',
+                'discount_max_amount', 'discount_line_product_id', 'reward_product_id', 'tax_ids',
                 'multi_product', 'reward_product_ids', 'reward_product_qty', 'reward_product_uom_id', 'reward_product_domain']
 
     def _load_pos_data(self, data):

addons/sale/wizard/sale_order_discount.py
@@ -24,6 +24,12 @@ class SaleOrderDiscount(models.TransientModel):
         ],
         default='sol_discount',
     )
+    tax_ids = fields.Many2many(
+        string="Taxes",
+        help="Taxes to add on the discount line.",
+        comodel_name='account.tax',
+        domain="[('type_tax_use', '=', 'sale'), ('company_id', '=', company_id)]",
+    )
 
     # CONSTRAINT METHODS #
 
@@ -84,7 +90,7 @@ class SaleOrderDiscount(models.TransientModel):
                 self._prepare_discount_line_values(
                     product=discount_product,
                     amount=self.discount_amount,
-                    taxes=self.env['account.tax'],
+                    taxes=self.tax_ids,
                 )
             ]
         else: # so_discount

addons/sale/wizard/sale_order_discount_views.xml
@@ -21,6 +21,13 @@
                                 <field name="discount_percentage"
                                        invisible="discount_type not in ('so_discount', 'sol_discount')"
                                        widget="percentage" nolabel="1"/>
+                                <field
+                                    name="tax_ids"
+                                    placeholder="Untaxed discount"
+                                    widget="many2many_tags"
+                                    options="{'no_create': True}"
+                                    invisible="discount_type != 'amount'"
+                                />
                             </group>
                         </div>
                         <div class="col-sm-7 col-md-8 col-lg-8 col-8">

commit a5c504298e8e572e4cc24f10569b73f38fd3ed8d

[REV] {*_}loyalty,sale: fix amount per tax
Commit b97e78c937723cedfab575115ea46b19f859df02 introduced a wrong
behaviour for the discounts applied to an order when they are fixed or
depending on the number of points. With this commit, the taxes applied
would be the ones set on the reward product if at least one line in the
sale order had these taxes. Disregarding any other taxes applied, or not
setting taxes at all. This was done to facilitate the reading of a sale
order.

Accounting wise, this is wrong. A product is paid less, so less taxes
needs to be recorded. It's not optimization, it's regulation.

Reverting part of b97e78c to evaluate
how to ease the user life without having them getting on the warpath of
their accountant.

opw-4486030

Forward-port-of: #197648
Forward-port-of: #196252
X-original-commit: a1a8d2bc1e56031e1ec71cabf634c38d0b5fdb94
Part-of: odoo/odoo#198198
Signed-off-by: Morgane Demesmaeker <edm@odoo.com>

addons/loyalty/views/loyalty_reward_views.xml
@@ -37,14 +37,7 @@
 
                         <group string="Discount" invisible="reward_type != 'discount' or program_type in ('gift_card','ewallet')">
                             <field name="discount_max_amount"/>
-                            <field
-                                name="tax_ids"
-                                placeholder="Untaxed discount"
-                                widget="many2many_tax_tags"
-                                options="{'no_create': True}"
-                                invisible="discount_applicability != 'order' or
-                                    discount_mode not in ('per_order','per_point')"
-                            />
+                            <field name="tax_ids" invisible="1"/>
                             <field name="discount_product_domain" groups="base.group_no_one" widget="domain" options="{'model': 'product.product', 'in_dialog': true}" invisible="discount_applicability == 'order'"/>
                             <field name="discount_product_ids" widget="many2many_tags" invisible="discount_applicability == 'order'"/>
                             <field name="discount_product_category_id" invisible="discount_applicability == 'order'"/>
@@ -83,7 +76,7 @@
         <field name="model">loyalty.reward</field>
         <field name="arch" type="xml">
             <kanban>
-                <field name="company_id"/>
+                <field name="company_id" invisible="1"/>
                 <field name="currency_id"/>
                 <field name="reward_type"/>
                 <field name="discount_applicability"/>

addons/pos_loyalty/models/loyalty_reward.py
@@ -25,7 +25,7 @@ class LoyaltyReward(models.Model):
     def _load_pos_data_fields(self, config_id):
         return ['description', 'program_id', 'reward_type', 'required_points', 'clear_wallet', 'currency_id',
                 'discount', 'discount_mode', 'discount_applicability', 'all_discount_product_ids', 'is_global_discount',
-                'discount_max_amount', 'discount_line_product_id', 'reward_product_id', 'tax_ids',
+                'discount_max_amount', 'discount_line_product_id', 'reward_product_id',
                 'multi_product', 'reward_product_ids', 'reward_product_qty', 'reward_product_uom_id', 'reward_product_domain']
 
     def _load_pos_data(self, data):

addons/pos_loyalty/static/src/app/models/pos_order.js
@@ -2,10 +2,7 @@ import { PosOrder } from "@point_of_sale/app/models/pos_order";
 import { patch } from "@web/core/utils/patch";
 import { _t } from "@web/core/l10n/translation";
 import { loyaltyIdsGenerator } from "@pos_loyalty/app/services/pos_store";
-import {
-    computePriceForcePriceInclude,
-    getTaxesAfterFiscalPosition,
-} from "@point_of_sale/app/models/utils/tax_utils";
+import { computePriceForcePriceInclude } from "@point_of_sale/app/models/utils/tax_utils";
 const { DateTime } = luxon;
 
 function _newRandomRewardCode() {
@@ -1137,65 +1134,6 @@ patch(PosOrder.prototype, {
                     tax_ids: discountProduct.taxes_id,
                 },
             ];
-        }
-
-        if (
-            rewardAppliesTo === "order" &&
-            ["per_point", "per_order"].includes(reward.discount_mode)
-        ) {
-            const rewardLineValues = [
-                {
-                    product_id: discountProduct,
-                    price_unit: -Math.min(maxDiscount, discountable),
-                    qty: 1,
-                    reward_id: reward,
-                    is_reward_line: true,
-                    coupon_id: coupon_id,
-                    points_cost: pointCost,
-                    reward_identifier_code: rewardCode,
-                    tax_ids: [],
-                },
-            ];
-
-            let rewardTaxes = reward.tax_ids;
-            if (rewardTaxes.length > 0) {
-                if (this.fiscal_position_id) {
-                    rewardTaxes = getTaxesAfterFiscalPosition(
-                        rewardTaxes,
-                        this.fiscal_position_id,
-                        this.models
-                    );
-                }
-
-                // Check for any order line where its taxes exactly match rewardTaxes
-                const matchingLines = this.getOrderlines().filter(
-                    (line) =>
-                        !line.is_delivery &&
-                        line.tax_ids.length === rewardTaxes.length &&
-                        line.tax_ids.every((tax_id) => rewardTaxes.includes(tax_id))
-                );
-
-                if (matchingLines.length == 0) {
-                    return _t("No product is compatible with this promotion.");
-                }
-
-                const untaxedAmount = matchingLines.reduce(
-                    (sum, line) => sum + line.getPriceWithoutTax(),
-                    0
-                );
-                // Discount amount should not exceed total untaxed amount of the matching lines
-                rewardLineValues[0].price_unit = Math.max(
-                    -untaxedAmount,
-                    rewardLineValues[0].price_unit
-                );
-
-                rewardLineValues[0].tax_ids = rewardTaxes;
-            }
-            // Discount amount should not exceed the untaxed amount on the order
-            if (Math.abs(rewardLineValues[0].price_unit) > this.amount_untaxed) {
-                rewardLineValues[0].price_unit = -this.amount_untaxed;
-            }
-            return rewardLineValues;
         }
 
         const discountFactor = discountable ? Math.min(1, maxDiscount / discountable) : 1;

addons/sale/wizard/sale_order_discount.py
@@ -108,7 +108,7 @@ class SaleOrderDiscount(models.TransientModel):
                 self._prepare_discount_line_values(
                     product=discount_product,
                     amount=self.discount_amount,
-                    taxes=self.tax_ids,
+                    taxes=self.env['account.tax'],
                 )
             ]
         else: # so_discount

addons/sale/wizard/sale_order_discount_views.xml
@@ -21,13 +21,7 @@
                                 <field name="discount_percentage"
                                        invisible="discount_type not in ('so_discount', 'sol_discount')"
                                        widget="percentage" nolabel="1"/>
-                                <field
-                                    name="tax_ids"
-                                    placeholder="Untaxed discount"
-                                    widget="many2many_tax_tags"
-                                    options="{'no_create': True}"
-                                    invisible="discount_type != 'amount'"
-                                />
+                                <field name="tax_ids" invisible="1"/>
                             </group>
                         </div>
                         <div class="col-sm-7 col-md-8 col-lg-8 col-8">

addons/sale_loyalty/models/sale_order.py
@@ -598,41 +598,6 @@ class SaleOrder(models.Model):
                     })
             return [reward_line_values]
 
-        if reward_applies_on == 'order' and reward.discount_mode in ['per_point', 'per_order']:
-            reward_line_values = {
-                **base_reward_line_values,
-                'price_unit': -min(max_discount, discountable),
-                'points_cost': point_cost,
-            }
-
-            reward_taxes = reward.tax_ids._filter_taxes_by_company(self.company_id)
-            if reward_taxes:
-                mapped_taxes = self.fiscal_position_id.map_tax(reward_taxes)
-
-                # Check for any order line where its taxes exactly match reward_taxes
-                matching_lines = [
-                    line for line in self.order_line
-                    if not line._is_delivery() and set(line.tax_ids) == set(mapped_taxes)
-                ]
-
-                if not matching_lines:
-                    raise ValidationError(_("No product is compatible with this promotion."))
-
-                untaxed_amount = sum(line.price_subtotal for line in matching_lines)
-                # Discount amount should not exceed total untaxed amount of the matching lines
-                reward_line_values['price_unit'] = max(
-                    -untaxed_amount,
-                    reward_line_values['price_unit']
-                )
-
-                reward_line_values['tax_ids'] = [Command.set(mapped_taxes.ids)]
-
-            # Discount amount should not exceed the untaxed amount on the order
-            if abs(reward_line_values['price_unit']) > self.amount_untaxed:
-                reward_line_values['price_unit'] = -self.amount_untaxed
-
-            return [reward_line_values]
-
         discount_factor = min(1, (max_discount / discountable)) if discountable else 1
         reward_dict = {}
         for tax, price in discountable_per_tax.items():

addons/sale/tests/test_sale_order_discount.py
@@ -30,24 +30,6 @@ class TestSaleOrderDiscount(SaleCommon):
         self.assertEqual(discount_line.product_uom_qty, 1.0)
         self.assertFalse(discount_line.tax_ids)
 
-    def test_amount_with_manual_tax(self):
-        self.tax_15pc_excl = self.env['account.tax'].create({
-            'name': "15% Tax excl",
-            'amount_type': 'percent',
-            'amount': 15,
-        })
-        self.wizard.write({
-            'discount_amount': 55,
-            'discount_type': 'amount',
-            'tax_ids': [(6, 0, (self.tax_15pc_excl.id,))],
-        })
-        self.wizard.action_apply_discount()
-
-        discount_line = self.sale_order.order_line[-1]
-        self.assertEqual(discount_line.price_unit, -55)
-        self.assertEqual(discount_line.product_uom_qty, 1.0)
-        self.assertEqual(discount_line.price_total, -63.25)
-
     def test_so_discount(self):
         solines = self.sale_order.order_line
         amount_before_discount = self.sale_order.amount_total

addons/sale_loyalty/tests/test_program_numbers.py
@@ -613,52 +613,6 @@ class TestSaleCouponProgramNumbers(TestSaleCouponNumbersCommon):
         self._auto_rewards(order, self.all_programs)
         self.assertEqual(order.amount_total, 65.0, "The coupon should not be removed from the order")
 
-    def test_coupon_discount_with_taxes_applied(self):
-        """Ensure coupon discount with taxes applies correctly
-           and doesn't make the order total go below 0.
-        """
-
-        coupon_program = self.env['loyalty.program'].create({
-            'name': '$300 coupon',
-            'program_type': 'coupons',
-            'trigger': 'with_code',
-            'applies_on': 'current',
-            'reward_ids': [(0, 0, {
-                'reward_type': 'discount',
-                'discount_mode': 'per_point',
-                'discount': 300,
-                'discount_applicability': 'order',
-                'required_points': 1,
-                'tax_ids': [(6, 0, (self.tax_15pc_excl.id,))],
-            })],
-        })
-
-        order = self.empty_order
-        self.env['sale.order.line'].create([
-        {
-            'product_id': self.conferenceChair.id,
-            'name': 'Conference Chair',
-            'product_uom_qty': 1.0,
-            'price_unit': 100.0,
-            'order_id': order.id,
-            'tax_ids': [(6, 0, (self.tax_15pc_excl.id,))],
-        },
-        ])
-
-        self.env['loyalty.generate.wizard'].with_context(active_id=coupon_program.id).create({
-            'coupon_qty': 1,
-            'points_granted': 1,
-        }).generate_coupons()
-        coupon = coupon_program.coupon_ids
-        self._apply_promo_code(order, coupon.code)
-
-        self.assertEqual(order.amount_tax, 0.0)
-        self.assertEqual(order.amount_untaxed, 0.0, "The untaxed amount should not go below 0")
-        self.assertEqual(
-            order.amount_total, 0.0,
-            "The promotion program should not make the order total go below 0"
-        )
-
     def test_coupon_and_program_discount_fixed_amount(self):
         """ Ensure coupon and program discount both with
             minimum amount rule can cohexists without making
@@ -773,9 +727,9 @@ class TestSaleCouponProgramNumbers(TestSaleCouponNumbersCommon):
         coupon = coupon_program.coupon_ids
         self._apply_promo_code(order, coupon.code)
         self._auto_rewards(order, self.all_programs)
-        self.assertEqual(order.amount_tax, 13.5)
+        self.assertEqual(order.amount_tax, 0.0)
         self.assertEqual(order.amount_untaxed, 0.0, "The untaxed amount should not go below 0")
-        self.assertEqual(order.amount_total, 13.5, "The promotion program should not make the order total go below 0")
+        self.assertEqual(order.amount_total, 0.0, "The promotion program should not make the order total go below 0")
 
         order.order_line[3:].unlink() #remove all coupon
         order._remove_program_from_points(coupon_program)
@@ -788,13 +742,13 @@ class TestSaleCouponProgramNumbers(TestSaleCouponNumbersCommon):
         self._auto_rewards(order, self.all_programs)
         self._apply_promo_code(order, 'test_10pc')
         self._auto_rewards(order, self.all_programs)
-        self.assertAlmostEqual(order.amount_tax, 13.5, 2)
-        self.assertEqual(order.amount_untaxed, 10.35)
+        self.assertAlmostEqual(order.amount_tax, 1.13, 2)
+        self.assertEqual(order.amount_untaxed, 22.72)
         self.assertEqual(order.amount_total, 23.85, "The promotion program should not make the order total go below 0be altered after recomputation")
         # It should stay the same after a recompute, order matters
         self._auto_rewards(order, self.all_programs)
-        self.assertAlmostEqual(order.amount_tax, 13.5, 2)
-        self.assertEqual(order.amount_untaxed, 10.35)
+        self.assertAlmostEqual(order.amount_tax, 1.13, 2)
+        self.assertEqual(order.amount_untaxed, 22.72)
         self.assertEqual(order.amount_total, 23.85, "The promotion program should not make the order total go below 0be altered after recomputation")
 
     def test_coupon_and_coupon_discount_fixed_amount_tax_incl(self):
@@ -859,11 +813,11 @@ class TestSaleCouponProgramNumbers(TestSaleCouponNumbersCommon):
         }).generate_coupons()
         coupon = coupon_program.coupon_ids
         self._apply_promo_code(order, coupon.code)
-        self.assertEqual(order.amount_total, 8.18, "The promotion program should not make the order total go below 0")
-        self.assertEqual(order.amount_tax, 8.18)
+        self.assertEqual(order.amount_total, 0, "The promotion program should not make the order total go below 0")
+        self.assertEqual(order.amount_tax, 0)
         self._auto_rewards(order, self.all_programs)
-        self.assertEqual(order.amount_total, 8.18, "The promotion program should not be altered after recomputation")
-        self.assertEqual(order.amount_tax, 8.18)
+        self.assertEqual(order.amount_total, 0, "The promotion program should not be altered after recomputation")
+        self.assertEqual(order.amount_tax, 0)
 
         order.order_line[3:].unlink() #remove all coupon
         order._remove_program_from_points(coupon_program)
@@ -876,13 +830,13 @@ class TestSaleCouponProgramNumbers(TestSaleCouponNumbersCommon):
         self._apply_promo_code(order, 'test_10pc')
         self._auto_rewards(order, self.all_programs)
         self.assertEqual(order.amount_total, 9.0, "The promotion program should not make the order total go below 0")
-        self.assertEqual(order.amount_tax, 8.18)
-        self.assertEqual(order.amount_untaxed, 0.82)
+        self.assertEqual(order.amount_tax, 0.27)
+        self.assertEqual(order.amount_untaxed, 8.73)
         # It should stay the same after a recompute, order matters
         self._auto_rewards(order, self.all_programs)
         self.assertEqual(order.amount_total, 9.0, "The promotion program should not make the order total go below 0")
-        self.assertEqual(order.amount_tax, 8.18)
-        self.assertEqual(order.amount_untaxed, 0.82)
+        self.assertEqual(order.amount_tax, 0.27)
+        self.assertEqual(order.amount_untaxed, 8.73)
 
     def test_program_discount_on_multiple_specific_products(self):
         """ Ensure a discount on multiple specific products is correctly computed.
@@ -1551,13 +1505,13 @@ class TestSaleCouponProgramNumbers(TestSaleCouponNumbersCommon):
         self._auto_rewards(order, program)
 
         self.assertEqual(order.amount_total, 7, 'Price should be 12$ - 5$(discount) = 7$')
-        self.assertEqual(float_compare(order.amount_tax, 7 / 4, precision_rounding=3), 0, '20% Tax included on 7$')
+        self.assertEqual(float_compare(order.amount_tax, 7 / 12, precision_rounding=3), 0, '20% Tax included on 7$')
 
         sol.tax_ids = self.tax_10pc_base_incl + self.tax_10pc_excl
         self._auto_rewards(order, program)
 
         self.assertAlmostEqual(order.amount_total, 6, 1, msg='Price should be 11$ - 5$(discount) = 6$')
-        self.assertEqual(float_compare(order.amount_tax, 6 / 4, precision_rounding=3), 0, '20% Tax included on 6$')
+        self.assertEqual(float_compare(order.amount_tax, 6 / 12, precision_rounding=3), 0, '20% Tax included on 6$')
 
     def test_fixed_amount_taxes_attribution_multiline(self):
 
@@ -1607,8 +1561,8 @@ class TestSaleCouponProgramNumbers(TestSaleCouponNumbersCommon):
         self._auto_rewards(order, program)
 
         self.assertAlmostEqual(order.amount_total, 16, 1, msg='Price should be 21$ - 5$(discount) = 16$')
-        # Tax amount = 10% in 10$ + 10% in 11$
-        self.assertEqual(float_compare(order.amount_tax, 5 / 3, precision_rounding=3), 0)
+        # Tax amount = 10% in 10$ + 10% in 11$ - 10% in 5$ (apply on excluded)
+        self.assertEqual(float_compare(order.amount_tax, 5 / 11, precision_rounding=3), 0)
 
         sol2.tax_ids = self.tax_10pc_base_incl + self.tax_10pc_excl
         self._auto_rewards(order, program)
@@ -1905,19 +1859,19 @@ class TestSaleCouponProgramNumbers(TestSaleCouponNumbersCommon):
 
         self.assertAlmostEqual(
             order.amount_total,
-            self.pedalBin.list_price * (1 + self.tax_20pc_excl.amount / 100),
+            self.pedalBin.list_price * (1 + self.tax_20pc_excl.amount / 100),  # $56.4
             msg="Order total should equal product list price plus taxes",
         )
 
         self._auto_rewards(order, order_program)
         self.assertAlmostEqual(
             order.amount_total,
-            self.pedalBin.list_price * self.tax_20pc_excl.amount / 100,
-            msg="Order total should equal just taxes after applying $50 no-tax discount",
+            self.pedalBin.list_price * (1 + self.tax_20pc_excl.amount / 100) - 50,  # $6.4
+            msg="The order total should be $50 less than initially after the discount is applied.",
         )
 
         self._auto_rewards(order, specific_program)
         self.assertFalse(
             order.amount_total,
-            "Order total should equal zero after applying specific $10 tax-included discount",
+            "Order total should be 0, as a specific discount should have been applied.",
         )