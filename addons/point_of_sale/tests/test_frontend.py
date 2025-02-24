f7c1db8d56d5d687b328e86af375031d9ead3889
lase@odoo.com <lase@odoo.com>
2025-02-24 18:52:26 +0000

    [FIX] point_of_sale: add multiple serial numbers on the pos order
    Steps to reproduce:
    
    - Create a storable product tracked by serial number available from the
      pos and with a set pos category.
    - Open a pos session and click on your product.
    > A dialog openns for you to register serial numbers.
    - Edit a first SN: SN001 and press enter
    > A new line can now be set to create a second one.
    - Edit a second SN: SN002 and press enter
    - Confirm the dialog
    > The quantity displayed on teh POS order line is 1 rather than 2.
    
    Cause of the issue:
    
    Clicking on the product card from the pos will trigger a call of the
    `AddProductToCurrentOrder` that will add a quantity of the product set
    via the options:
    https://github.com/odoo/odoo/blob/6197233ef1611ddd974cfdb06ae2568e4af369de/addons/point_of_sale/static/src/app/store/pos_store.js#L1965-L1978
    However, the `getAddProductOptions` call sets a quantity to add of 1
    no matter if you added multiple or even removed multiple registered SN.
    https://github.com/odoo/odoo/blob/6197233ef1611ddd974cfdb06ae2568e4af369de/addons/point_of_sale/static/src/app/store/models.js#L180
    On the other hand, since at the moment of this call we do not have the
    info of the lines that were removed in the dialog it is not possible to
    always provide the correct (possibly negative) quantity to add to the
    line for the qty to be correctly handledby the posorder line.
    However, just as when you edit the line dirrectly this can be achieved
    by updating the quantity based on the relevant lots present on the line:
    https://github.com/odoo/odoo/blob/6197233ef1611ddd974cfdb06ae2568e4af369de/addons/point_of_sale/static/src/app/store/models.js#L570-L572
    
    opw-4554842
    
    closes odoo/odoo#202252
    
    X-original-commit: 14d51bd960d7dd49cb234722b7725b98549feb04
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Lancelot Semal (lase) <lase@odoo.com>

--- addons/point_of_sale/tests/test_frontend.py
+++ addons/point_of_sale/tests/test_frontend.py
@@ -1638,6 +1638,16 @@ class TestUi(TestPointOfSaleHttpCommon):
         self.main_pos_config.with_user(self.pos_user).open_ui()
         self.start_tour(f"/pos/ui?config_id={self.main_pos_config.id}", 'ProductCardUoMPrecision', login="pos_user")
 
+    def test_add_multiple_serials_at_once(self):
+        self.product_a = self.env['product.product'].create({
+            'name': 'Product A',
+            'is_storable': True,
+            'tracking': 'serial',
+            'categ_id': self.env.ref('product.product_category_all').id,
+            'available_in_pos': True,
+        })
+        self.main_pos_config.with_user(self.pos_user).open_ui()
+        self.start_tour("/pos/ui?config_id=%d" % self.main_pos_config.id, "AddMultipleSerialsAtOnce", login="pos_user")
 
 # This class just runs the same tests as above but with mobile emulation
 class MobileTestUi(TestUi):
