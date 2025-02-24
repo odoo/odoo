2286c9951924c459bea3747e00e50a628b15e342
dere-odoo <dere@odoo.com>
2025-02-24 11:49:56 +0530

    [IMP] website_sale_(stock): review changes

--- addons/website_sale_stock/models/__init__.py
+++ addons/website_sale_stock/models/__init__.py
@@ -3,9 +3,9 @@
 
 from . import website
 from . import product_product
+from . import product_ribbon
 from . import product_template
 from . import res_config_settings
 from . import sale_order
 from . import sale_order_line
 from . import stock_picking
-from . import product_ribbon
