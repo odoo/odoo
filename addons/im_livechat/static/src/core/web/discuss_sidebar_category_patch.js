1514a3941e2340e069595f739082d2deabacf809
Akash Parmar <akap@odoo.com>
2025-02-24 17:53:55 +0530

    [IMP] mail, im_livechat: option to close all live chat conversations
    Before this commit:
    
    There was no option to close and leave all the live chat conversations at once
    in discuss.
    
    For closing all conversations chat bubbles, we require two clicks
    and it throws a warning before leaving each conversation,
    which also requires a click.
    
    After this commit:
    
    Added a button in discuss using which user can leave all the live chat
    conversations of a particular live chat channel in a single click.
    
    Also we can close and leave all conversations bubbles in
    a single click now
    
    task-id:4536773

--- addons/im_livechat/static/src/core/web/discuss_sidebar_category_patch.js
+++ addons/im_livechat/static/src/core/web/discuss_sidebar_category_patch.js
@@ -1,26 +1,55 @@
 import { DiscussSidebarCategory } from "@mail/discuss/core/public_web/discuss_sidebar_categories";
+import { CloseAllConfirmation } from "@mail/core/common/close_all_confirmation";
+
 import { patch } from "@web/core/utils/patch";
+import { _t } from "@web/core/l10n/translation";
 
 /** @type {import("@mail/discuss/core/public_web/discuss_sidebar_categories").DiscussSidebarCategory} */
 const DiscussSidebarCategoryPatch = {
     get actions() {
         const actions = super.actions;
         if (this.store.has_access_livechat && this.category.livechatChannel && this.category.open) {
-            actions.push({
-                onSelect: () => {
-                    if (this.category.livechatChannel.are_you_inside) {
-                        this.category.livechatChannel.leave({ notify: false });
-                    } else {
-                        this.category.livechatChannel.join({ notify: false });
-                    }
+            actions.push(
+                {
+                    onSelect: () => {
+                        this.store.env.services.dialog.add(CloseAllConfirmation, {
+                            title: _t("Close live chat conversations"),
+                            message: _t(
+                                "This action will end all live chat conversations of %s. Proceed?",
+                                this.category.livechatChannel.name
+                            ),
+                            onConfirm: () => {
+                                const promises = [];
+                                for (const thread of this.category.livechatChannel.threads) {
+                                    promises.push(thread.leave());
+                                }
+                                Promise.all(promises);
+                            },
+                        });
+                    },
+                    label: _t("Leave all conversations"),
+                    icon: "fa fa-close text-danger",
+                    active: () =>
+                        this.category.livechatChannel.threads.filter(
+                            (thread) => thread.isLocallyPinned || thread.is_pinned
+                        ).length,
                 },
-                label: this.category.livechatChannel.are_you_inside
-                    ? this.category.livechatChannel.leaveTitle
-                    : this.category.livechatChannel.joinTitle,
-                icon: this.category.livechatChannel.are_you_inside
-                    ? "fa fa-sign-out text-danger"
-                    : "fa fa-sign-in text-success",
-            });
+                {
+                    onSelect: () => {
+                        if (this.category.livechatChannel.are_you_inside) {
+                            this.category.livechatChannel.leave({ notify: false });
+                        } else {
+                            this.category.livechatChannel.join({ notify: false });
+                        }
+                    },
+                    label: this.category.livechatChannel.are_you_inside
+                        ? this.category.livechatChannel.leaveTitle
+                        : this.category.livechatChannel.joinTitle,
+                    icon: this.category.livechatChannel.are_you_inside
+                        ? "fa fa-sign-out text-danger"
+                        : "fa fa-sign-in text-success",
+                }
+            );
         }
         return actions;
     },
