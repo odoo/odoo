6201c63053ff8bd0f3ba7c8b58af1b7963b9f29e
Krzysztof Magusiak (krma) <krma@odoo.com>
2025-02-24 15:35:30 +0000

    [IMP] crm_iap_enrich: refactor cron job
    Writing `iap_enrich` so that it supports being called both from the cron
    job and directly by the user. The core logic is the same, but we have
    different logic for committing batches depending on where we are called
    from.
    
    - from cron: use `_commit_progress`
    - during testing: use savepoints (we have one test cursor)
    - normal operation: just commit/rollback
    
    At each loop, we lock only a subset of records and try to enrich them.

--- addons/crm_iap_enrich/models/crm_lead.py
+++ addons/crm_iap_enrich/models/crm_lead.py
@@ -5,7 +5,7 @@ import logging
 
 from odoo import _, api, fields, models, modules, tools
 from odoo.addons.iap.tools import iap_tools
-from odoo.exceptions import LockError
+from odoo.tools import OrderedSet
 
 _logger = logging.getLogger(__name__)
 
@@ -25,16 +25,18 @@ class CrmLead(models.Model):
                 lead.show_enrich_button = True
 
     @api.model
-    def _iap_enrich_leads_cron(self, enrich_hours_delay=24, leads_batch_size=1000):
+    def _iap_enrich_leads_cron(self, enrich_hours_delay=24, batch_size=50):
         timeDelta = self.env.cr.now() - datetime.timedelta(hours=enrich_hours_delay)
         # Get all leads not lost nor won (lost: active = False)
         leads = self.search([
             ('iap_enrich_done', '=', False),
-            ('reveal_id', '=', False),
             '|', ('probability', '<', 100), ('probability', '=', False),
-            ('create_date', '>', timeDelta)
-        ], limit=leads_batch_size)
-        leads.iap_enrich(from_cron=True)
+            ('email_from', '!=', False),
+            ('reveal_id', '=', False),
+            ('create_date', '>', timeDelta),
+            ('active', '=', True),
+        ])
+        leads.iap_enrich(batch_size=batch_size)
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -46,70 +48,105 @@ class CrmLead(models.Model):
                 cron._trigger()
         return leads
 
-    def iap_enrich(self, from_cron=False):
-        # Split self in a list of sub-recordsets or 50 records to prevent timeouts
-        batches = [self[index:index + 50] for index in range(0, len(self), 50)]
-        for leads in batches:
+    def iap_enrich(self, *, batch_size=50):
+        from_cron = bool(self.env.context.get('cron_id'))
+        send_notification = not from_cron
+
+        def process_leads(leads):
             lead_emails = {}
+            for lead in leads:
+                # If lead is lost, active == False, but is anyway removed from the search in the cron.
+                if lead.probability == 100 or lead.iap_enrich_done:
+                    continue
+                # Skip if no email (different from wrong email leading to no email_normalized)
+                if not lead.email_from:
+                    continue
+
+                normalized_email = tools.email_normalize(lead.email_from)
+                if not normalized_email:
+                    lead.message_post_with_source(
+                        'crm_iap_enrich.mail_message_lead_enrich_no_email',
+                        subtype_xmlid='mail.mt_note',
+                    )
+                    continue
+
+                email_domain = normalized_email.split('@')[1]
+                # Discard domains of generic email providers as it won't return relevant information
+                if email_domain in iap_tools._MAIL_PROVIDERS:
+                    lead.write({'iap_enrich_done': True})
+                    lead.message_post_with_source(
+                        'crm_iap_enrich.mail_message_lead_enrich_notfound',
+                        subtype_xmlid='mail.mt_note',
+                    )
+                else:
+                    lead_emails[lead.id] = email_domain
+            if not lead_emails:
+                return
+
             try:
-                leads.lock_for_update()
-                with self.env.cr.savepoint():
-                    for lead in leads:
-                        # If lead is lost, active == False, but is anyway removed from the search in the cron.
-                        if lead.probability == 100 or lead.iap_enrich_done:
-                            continue
-                        # Skip if no email (different from wrong email leading to no email_normalized)
-                        if not lead.email_from:
-                            continue
+                iap_response = self.env['iap.enrich.api']._request_enrich(lead_emails)
+            except iap_tools.InsufficientCreditError:
+                _logger.info('Lead enrichment failed because of insufficient credit')
+                if send_notification:
+                    self.env['iap.account']._send_no_credit_notification(
+                        service_name='reveal',
+                        title=_("Not enough credits for Lead Enrichment"))
+                raise
+            except Exception as e:
+                if send_notification:
+                    self.env['iap.account']._send_error_notification(
+                        message=_('An error occurred during lead enrichment'))
+                _logger.info('An error occurred during lead enrichment: %s', e)
+                return
+            else:
+                if send_notification:
+                    self.env['iap.account']._send_success_notification(
+                        message=_("The leads/opportunities have successfully been enriched"))
+                _logger.info('Batch of %s leads successfully enriched', len(lead_emails))
+            self._iap_enrich_from_response(iap_response)
 
-                        normalized_email = tools.email_normalize(lead.email_from)
-                        if not normalized_email:
-                            lead.message_post_with_source(
-                                'crm_iap_enrich.mail_message_lead_enrich_no_email',
-                                subtype_xmlid='mail.mt_note',
-                            )
-                            continue
+        if from_cron:
+            self.env['ir.cron']._commit_progress(remaining=len(self))
+        all_lead_ids = OrderedSet(self.ids)
+        while all_lead_ids:
+            leads = self.browse(all_lead_ids).try_lock_for_update(limit=batch_size)
+            if not leads:
+                _logger.error('A batch of leads could not be enriched (locked): %s', repr(self.browse(all_lead_ids)))
+                break  # all are locked
+            all_lead_ids -= set(leads._ids)
 
-                        email_domain = normalized_email.split('@')[1]
-                        # Discard domains of generic email providers as it won't return relevant information
-                        if email_domain in iap_tools._MAIL_PROVIDERS:
-                            lead.write({'iap_enrich_done': True})
-                            lead.message_post_with_source(
-                                'crm_iap_enrich.mail_message_lead_enrich_notfound',
-                                subtype_xmlid='mail.mt_note',
-                            )
-                        else:
-                            lead_emails[lead.id] = email_domain
-
-                    if lead_emails:
-                        try:
-                            iap_response = self.env['iap.enrich.api']._request_enrich(lead_emails)
-                        except iap_tools.InsufficientCreditError:
-                            _logger.info('Lead enrichment failed because of insufficient credit')
-                            if not from_cron:
-                                self.env['iap.account']._send_no_credit_notification(
-                                    service_name='reveal',
-                                    title=_("Not enough credits for Lead Enrichment"))
-                            # Since there are no credits left, there is no point to process the other batches
-                            break
-                        except Exception as e:
-                            if not from_cron:
-                                self.env['iap.account']._send_error_notification(
-                                    message=_('An error occurred during lead enrichment'))
-                            _logger.info('An error occurred during lead enrichment: %s', e)
-                        else:
-                            if not from_cron:
-                                self.env['iap.account']._send_success_notification(
-                                    message=_("The leads/opportunities have successfully been enriched"))
-                            _logger.info('Batch of %s leads successfully enriched', len(lead_emails))
-                            self._iap_enrich_from_response(iap_response)
-            except LockError:
-                _logger.error('A batch of leads could not be enriched (locked): %s', repr(leads))
-            except Exception:
-                _logger.error('A batch of leads could not be enriched: %s', repr(leads))
-            # Commit processed batch to avoid complete rollbacks and therefore losing credits.
-            if not modules.module.current_test:
-                self.env.cr.commit()
+            if from_cron:
+                # Using commit progress for processed leads
+                try:
+                    process_leads(leads)
+                    time_left = self.env['ir.cron']._commit_progress(len(leads))
+                except iap_tools.InsufficientCreditError:
+                    # Since there are no credits left, there is no point to process the other batches
+                    # set remaining=0 to avoid being called again
+                    self.env['ir.cron']._commit_progress(remaining=0)
+                    break
+                except Exception:
+                    self.env.cr.rollback()
+                    _logger.error('A batch of leads could not be enriched: %s', repr(leads))
+                    time_left = self.env['ir.cron']._commit_progress(len(leads))
+                if not time_left:
+                    break
+            else:
+                # Commit processed batch to avoid complete rollbacks and therefore losing credits.
+                try:
+                    if modules.module.current_test:
+                        with self.env.cr.savepoint():
+                            process_leads(leads)
+                    else:
+                        process_leads(leads)
+                        self.env.cr.commit()
+                except iap_tools.InsufficientCreditError:
+                    # Since there are no credits left, there is no point to process the other batches
+                    break
+                except Exception:
+                    if not modules.module.current_test:
+                        self.env.cr.rollback()
+                    _logger.error('A batch of leads could not be enriched: %s', repr(leads))
 
     @api.model
     def _iap_enrich_from_response(self, iap_response):
