0fb75632e9202fd66bb02be88b3777964d96d0f6
Xavier Bol (xbo) <xbo@odoo.com>
2025-02-24 08:42:14 +0100

    [FIX] project: enable task dependencies feature should not reset task state
    Before this commit, when the user enables the task dependencies feature
    globally, all basic projects (that is, non fsm ones) will get the
    feature. By doing that, the open tasks state will be recomputed to set
    `Waiting` state on tasks with blocking tasks linked to them. The problem
    is the compute method of the task state will also reset the state
    instead of keeping the one set on those open tasks when there is no
    blocking task linked.
    
    This commit makes sure the compute method of task state field is no
    longer called when the task dependencies feature changes. Instead, a
    inverse method is added on allow_task_dependencies field on
    `project.project` model to correctly update the state of the tasks
    linked to the project in which the task dependencies feature changed.
    
    Steps to reproduce the issue:
    ----------------------------
    
    0. install project
    1. Create a project
    2. Create some tasks inside that project and changes the state to some
       tasks
    3. Enable the task dependencies feature in `Project > Configuration >
       Settings` menu.
    4. Go back to the tasks kanban view of that project.
    
    Current Behavior:
    ----------------
    
    The state of those tasks is reset to `In progress` (if those tasks did
    not have a state equals to `Done` or `Canceled`).
    
    Expected Behavior:
    -----------------
    
    The state of those tasks should not be altered since no dependencies are
    added in those tasks yet.
    
    task-4487922

--- addons/project/models/project_project.py
+++ addons/project/models/project_project.py
@@ -166,7 +166,7 @@ class Project(models.Model):
     date_start = fields.Date(string='Start Date')
     date = fields.Date(string='Expiration Date', index=True, tracking=True,
         help="Date on which this project ends. The timeframe defined on the project is taken into account when viewing its planning.")
-    allow_task_dependencies = fields.Boolean('Task Dependencies', default=lambda self: self.env.user.has_group('project.group_project_task_dependencies'))
+    allow_task_dependencies = fields.Boolean('Task Dependencies', default=lambda self: self.env.user.has_group('project.group_project_task_dependencies'), inverse='_inverse_allow_task_dependencies')
     allow_milestones = fields.Boolean('Milestones', default=lambda self: self.env.user.has_group('project.group_project_milestone'))
     tag_ids = fields.Many2many('project.tags', relation='project_project_project_tags_rel', string='Tags')
     task_properties_definition = fields.PropertiesDefinition('Task Properties')
@@ -381,6 +381,35 @@ class Project(models.Model):
                 project.access_instruction_message = _('Grant employees access to your project or tasks by adding them as followers. Employees automatically get access to the tasks they are assigned to.')
             else:
                 project.access_instruction_message = ''
+
+    def _inverse_allow_task_dependencies(self):
+        """ Reset state for waiting tasks in the project if the feature is disabled
+            or recompute the tasks with dependencies if the project has the feature enabled again
+        """
+        project_with_task_dependencies_feature = self.filtered('allow_task_dependencies')
+        projects_without_task_dependencies_feature = self - project_with_task_dependencies_feature
+        ProjectTask = self.env['project.task']
+        if (
+            project_with_task_dependencies_feature
+            and (
+                open_tasks_with_dependencies := ProjectTask.search([
+                    ('project_id', 'in', project_with_task_dependencies_feature.ids),
+                    ('depend_on_ids.state', 'in', ProjectTask.OPEN_STATES),
+                    ('state', 'in', ProjectTask.OPEN_STATES),
+                ])
+            )
+        ):
+            open_tasks_with_dependencies.state = '04_waiting_normal'
+        if (
+            projects_without_task_dependencies_feature
+            and (
+                waiting_tasks := ProjectTask.search([
+                    ('project_id', 'in', projects_without_task_dependencies_feature.ids),
+                    ('state', '=', '04_waiting_normal'),
+                ])
+            )
+        ):
+            waiting_tasks.state = '01_in_progress'
 
     # TODO: Remove in master
     @api.onchange('date_start', 'date')
