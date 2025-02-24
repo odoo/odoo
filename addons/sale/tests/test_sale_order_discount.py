441079a6546bfd29ac816c0c88a063df59cb3db4
plha-odoo <plha@odoo.com>
2025-02-24 15:59:48 +0100

    [FIX] sale: fix discount translation
    Steps to reproduce:
    - create a new quotation
    - select a customer with another langage than the current user
    - add a product
    - add a discount using the discount button (select "global amount" or "fixed amount")
    
    Problem:
    Inside "order lines" from the quotation, the "description" column in the line created for the discount product
    is not translated.
    
    https://github.com/odoo/odoo/blob/c62e8d90db481fb7ba28e888431b6a7dc5ed03cd/addons/sale/wizard/sale_order_discount.py#L126
    
    opw-4534846
    
    closes odoo/odoo#197892
    
    Signed-off-by: Pierre-Louis Hance (plha) <plha@odoo.com>

--- addons/sale/tests/test_sale_order_discount.py
+++ addons/sale/tests/test_sale_order_discount.py
@@ -108,6 +108,41 @@ class TestSaleOrderDiscount(SaleCommon):
         with self.assertRaises(ValidationError):
             self.wizard.write({'discount_percentage': 1.1, 'discount_type': 'sol_discount'})
 
+    def test_discount_translation(self):
+        self.env['res.lang']._activate_lang('es_AR')
+        self.wizard.write({
+            'discount_percentage': 0.1,
+            'discount_type': 'so_discount',
+        })
+        self.wizard.sale_order_id.partner_id.lang = 'es_AR'
+        self.wizard.action_apply_discount()
+        discount_line = self.sale_order.order_line[-1]
+        self.assertEqual(discount_line.name, "Descuento: 10.00%")
+
+    def test_discount_translation_tax_groups(self):
+        self.env['res.lang']._activate_lang('es_AR')
+        self.wizard.write({
+            'discount_percentage': 0.1,
+            'discount_type': 'so_discount',
+        })
+        self.wizard.sale_order_id.partner_id.lang = 'es_AR'
+        tax1, tax2 = self.env['account.tax'].create([{
+            'name': f"{percentage}% VAT",
+            'amount_type': 'percent',
+            'amount': percentage,
+        } for percentage in (10, 20)])
+        self.wizard.sale_order_id.order_line[0].tax_id = tax1
+        self.wizard.sale_order_id.order_line[1].tax_id = tax2
+        self.wizard.action_apply_discount()
+        self.assertEqual(
+            self.sale_order.order_line[-2].name,
+            "Descuento: 10.00%- En los productos con los siguientes impuestos 10% VAT",
+        )
+        self.assertEqual(
+            self.sale_order.order_line[-1].name,
+            "Descuento: 10.00%- En los productos con los siguientes impuestos 20% VAT",
+        )
+
     def test_line_and_global_discount(self):
         solines = self.sale_order.order_line
         amount_before_discount = self.sale_order.amount_untaxed
