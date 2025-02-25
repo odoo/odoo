78cebdb1616f213e77461edd1d412776a90719f3
tsm-odoo <tsm@odoo.com>
2025-02-25 13:35:20 +0100

    [IMP] mail: missing types on some patches
    Some properties added in patches are missing in the type definitions.
    The script that generates type defs did not take simple expression
    statements into account, but only assignations. This PR fixes the issue.
    
    closes odoo/odoo#199230
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/discuss/core/public/@types/models.d.ts
+++ addons/mail/static/src/discuss/core/public/@types/models.d.ts
@@ -1,6 +1,10 @@
 declare module "models" {
     export interface Store {
+        companyName: string|undefined;
         discuss_public_thread: Thread;
+        inPublicPage: boolean|undefined;
+        isChannelTokenSecret: boolean|undefined;
+        shouldDisplayWelcomeViewInitially: boolean|undefined;
     }
     export interface Thread {
         setActiveURL: () => void;
