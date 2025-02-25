545a36a1082c8fb7afb1a17a79e639df882b57e4
Bruno Boi <boi@odoo.com>
2025-02-25 10:50:21 +0000

    [FIX] base_automation: fix cron interval

--- addons/base_automation/models/base_automation.py
+++ addons/base_automation/models/base_automation.py
@@ -665,9 +665,10 @@ class BaseAutomation(models.Model):
         if automations is None:
             automations = self.with_context(active_test=True).search([('trigger', 'in', TIME_TRIGGERS)])
 
-        # Minimum 1 minute, maximum 4 hours, 10% tolerance
-        delay = min(automations.mapped(get_delay), default=0)
-        return min(max(1, delay // 10), 4 * 60) if delay else 4 * 60
+        # Minimum 1 minute, maximum 4 hours, 10% tolerance, ignore automations with no delay
+        delays = automations.mapped(get_delay)
+        delays = list(filter(lambda d: d, delays))
+        return min(max(1, min(delays) // 10), 4 * 60) if delays else 4 * 60
 
     def _compute_least_delay_msg(self):
         msg = _("Note that this automation rule can be triggered up to %d minutes after its schedule.")
