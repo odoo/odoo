f056d3bf10130728eaac9a5b72e0e45338a69f8a
rare-odoo <rare@odoo.com>
2025-02-25 14:58:58 +0530

    [IMP] sale_edi_ubl: recompute price and discount

--- addons/sale_edi_ubl/models/sale_edi_xml_ubl_bis3.py
+++ addons/sale_edi_ubl/models/sale_edi_xml_ubl_bis3.py
@@ -23,3 +23,12 @@ class SaleEdiXmlUbl_Bis3(models.AbstractModel):
 
     def _get_order_ref(self):
         return 'client_order_ref'
+
+    def _import_order_ubl(self, order, file_data):
+        # Overriding the main method to recalculate the price unit and discount
+        res = super()._import_order_ubl(order, file_data)
+        lines_with_products = order.order_line.filtered('product_id')
+        # Recompute product price and discount according to sale price
+        lines_with_products._compute_price_unit()
+        lines_with_products._compute_discount()
+        return res
