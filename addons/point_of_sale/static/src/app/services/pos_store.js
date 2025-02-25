d2c0434d4fed08948e15ac3c78754c6f05e737bd
enricodg <enrico@layani.id>
2025-02-25 15:50:46 +0000

    [FIX] point_of_sale: add condition to useIdleTimer to prevent redirection in PaymentScreen
    closes odoo/odoo#211943
    
    X-original-commit: d4a998e0c2a39f6f0c0f1c3c96767da0ab04e22f
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/point_of_sale/static/src/app/services/pos_store.js
+++ addons/point_of_sale/static/src/app/services/pos_store.js
@@ -1674,6 +1674,7 @@ export class PosStore extends WithLazyGetterTrap {
         if (newOrder) {
             this.addNewOrder();
         }
+        return true;
     }
     async printReceipt({
         basic = false,
