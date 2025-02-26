9749c11f2a28428b7c45141015ce993e341f5d85
Paolo Gatti <pgi@odoo.com>
2025-02-26 15:40:20 +0100

    [WIP]

--- addons/i18n/tools/modules.py
+++ addons/i18n/tools/modules.py
@@ -1,8 +1,3 @@
-"""
-List all modules that can be exported, given user constraints.
-"""
-
-import logging
 from os.path import sep
 from pathlib import Path
 
@@ -10,39 +5,75 @@ from odoo.cli.command import Command
 from odoo.modules import get_module_path
 
 
-_logger = logging.getLogger(__name__)
+BOOL_YES = {'1', 'yes', 'true', 'on'}
+BOOL_NO = {'0', 'no', 'false', 'off'}
+BOOL_ONLY = {'only'}
+YES_NO_ONLY = BOOL_YES | BOOL_NO | BOOL_ONLY
 
 
 class Modules(Command):
+    """
+    List all modules that can be exported, given user constraints.
 
+    This command is not shown on `help` because it's not imported by the `__init__.py` file.
+    We can use it by passing the file through the `odoo-bin shell`.
+
+    ./odoo-bin \
+        --addons-path=odoo/addons,addons,../enterprise \
+        shell                                          \
+        -c ../.odoorc                                  \
+        --log-level=error                              \
+        --                                             \
+        --delimiter='\n'                               \
+        --folder=/home/odoo/work/odoo                  \
+        < addons/i18n/tools/modules.py
+    """
     excluded = (r'%\_test', r'%\_tests', r'test\_%', r'hw\_%')
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super().__init__()
         self.env = kwargs.get('env')
-        self.parser.add_argument('folder', help="Filter results by folder")
-
-    def _parse_args(self, cmdargs):
-        parsed_args, _unknown = self.parser.parse_known_args(args=cmdargs)
-        if parsed_args.folder:
-            _logger.info("Folder selected: %s", parsed_args.folder)
-        return parsed_args
+        self.parser.add_argument('--delimiter', default=',', help="Delimiter")
+        self.parser.add_argument('--folder', help="Filter results by folder")
+        self.parser.add_argument(
+            '--l10n', choices=YES_NO_ONLY, default='yes',
+            help="Filter results by folder")
 
     def run(self, cmdargs):
         try:
-            # Ensure arguments are consistent
-            parsed_args = self._parse_args(cmdargs)
+            parsed_args, _unknown = self.parser.parse_known_args(args=cmdargs)
         except ValueError as e:
             self.parser.print_help()
             Command.die(f'\n{e}\n')
 
-        domain = (
-            [('name', 'not =ilike', pattern) for pattern in self.excluded] + [
-            '|',
-                ('name', '=ilike', r'l10n\_%'),
-                ('name', '=ilike', r'%l10n\_%'),
-            ('name', '!=', 'l10n_multilang'),
-        ])
+        match parsed_args.delimiter:
+            case '\\n':
+                parsed_args.delimiter = '\n'
+            case '\\r':
+                parsed_args.delimiter = '\r'
+            case '\\r\\n':
+                parsed_args.delimiter = '\r\n'
+
+        domain = [('name', 'not =ilike', pattern) for pattern in self.excluded]
+        match parsed_args.l10n:
+            case x if x in BOOL_ONLY:
+                domain += ['|',
+                    ('name', '=ilike', r'l10n\_%'),
+                    ('name', '=ilike', r'%l10n\_%'),
+                    ('name', '!=', 'l10n_multilang'),
+                ]
+            case x if x in BOOL_NO:
+                domain += ['|',
+                    ('name', 'not =ilike', r'l10n\_%'),
+                    ('name', 'not =ilike', r'%l10n\_%'),
+                ]
+            case x if x in BOOL_YES:
+                domain += [
+                    '|',
+                        ('name', '=ilike', r'l10n\_%'),
+                        ('name', '=ilike', r'%l10n\_%'),
+                    ('name', '!=', 'l10n_multilang'),
+                ]
         modules = self.env['ir.module.module'].search_fetch(domain, ['name'], order="name")
         module_names = modules.mapped("name")
 
@@ -59,19 +90,7 @@ class Modules(Command):
             else:
                 module_names.append(module_name)
 
-        print(",".join(module_names))
+        print(parsed_args.delimiter.join(module_names))
+
 
 Modules(env=self.env).run(cmdargs=shellargs)
-
-# match parsed_args.l10n:
-#     case x if x in BOOL_ONLY:
-#         domain += ['|',
-#             ('name', '=ilike', r'l10n\_%'),
-#             ('name', '=ilike', r'%l10n\_%'),
-#             ('name', '!=', 'l10n_multilang'),
-#         ]
-#     case x if x in BOOL_NO:
-#         domain += ['|',
-#             ('name', 'not =ilike', r'l10n\_%'),
-#             ('name', 'not =ilike', r'%l10n\_%'),
-#         ]
--- odoo/cli/shell.py
+++ odoo/cli/shell.py
@@ -1,21 +1,3 @@
-import code
-import logging
-import optparse
-import os
-import signal
-import sys
-import threading
-
-import odoo  # to expose in the shell
-from odoo import api
-from odoo.modules.registry import Registry
-from odoo.service import server
-from odoo.tools import config
-from . import Command, server as cli_server
-
-_logger = logging.getLogger(__name__)
-
-
 """
     Shell exit behaviors
     ====================
@@ -34,9 +16,26 @@ _logger = logging.getLogger(__name__)
 
 """
 
+import code
+import logging
+import optparse
+import os
+import signal
+import sys
+import threading
+from contextlib import closing
 
-def raise_keyboard_interrupt(*a):
-    raise KeyboardInterrupt()
+import odoo
+from odoo.api import Environment, SUPERUSER_ID
+from odoo.modules.registry import Registry
+from odoo.service import server
+from odoo.tools import config
+from . import Command, server as cli_server
+
+_logger = logging.getLogger(__name__)
+
+SHELLS = ['ipython', 'ptpython', 'bpython', 'python']
+SEP = '--'
 
 
 class Console(code.InteractiveConsole):
@@ -54,12 +53,16 @@ class Console(code.InteractiveConsole):
 
 class Shell(Command):
     """Start odoo in an interactive shell"""
-    supported_shells = ['ipython', 'ptpython', 'bpython', 'python']
-    shellargs = None
 
-    def init(self, cmdargs):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.local_vars = {'odoo': odoo}
+
+        self.interactive = os.isatty(sys.stdin.fileno())
+        if self.interactive:
+            self.local_vars['__name__'] = '__main__'
+
         config.parser.prog = self.prog
-
         group = optparse.OptionGroup(config.parser, "Shell options")
         group.add_option(
             '--shell-file', dest='shell_file', type='string', my_default='',
@@ -68,47 +71,38 @@ class Shell(Command):
         )
         group.add_option(
             '--shell-interface', dest='shell_interface', type='string',
-            help="Specify a preferred REPL to use in shell mode. "
-                 "Supported REPLs are: [ipython|ptpython|bpython|python]"
+            help=(
+                "Specify a preferred REPL to use in shell mode."
+                f" Supported REPLs are: [{'|'.join(SHELLS)}]"
+            )
         )
         config.parser.add_option_group(group)
 
-        if '--' in cmdargs:
-            idx = cmdargs.index('--')
-            cmdargs, self.shellargs = cmdargs[:idx], " ".join(cmdargs[idx+1:])
+    def _execute(self):
+        exec(sys.stdin.read(), self.local_vars)
 
-        config.parse_config(cmdargs, setup_logging=True)
-        cli_server.report_configuration()
-        server.start(preload=[], stop=True)
-        signal.signal(signal.SIGINT, raise_keyboard_interrupt)
+    def console(self):
+        if 'env' not in self.local_vars:
+            print('No environment set, use `%s shell -d dbname` to get one.' % sys.argv[0])
+        for i in sorted(self.local_vars):
+            print('%s: %s' % (i, self.local_vars[i]))
 
-    def console(self, local_vars):
-        if not os.isatty(sys.stdin.fileno()):
-            local_vars['__name__'] = '__main__'
-            exec(sys.stdin.read(), local_vars)
+        pythonstartup = config.options.get('shell_file') or os.environ.get('PYTHONSTARTUP')
+
+        if preferred_interface := config.options.get('shell_interface'):
+            shells_to_try = [preferred_interface, 'python']
         else:
-            if 'env' not in local_vars:
-                print('No environment set, use `%s shell -d dbname` to get one.' % sys.argv[0])
-            for i in sorted(local_vars):
-                print('%s: %s' % (i, local_vars[i]))
+            shells_to_try = SHELLS
 
-            pythonstartup = config.options.get('shell_file') or os.environ.get('PYTHONSTARTUP')
-
-            preferred_interface = config.options.get('shell_interface')
-            if preferred_interface:
-                shells_to_try = [preferred_interface, 'python']
-            else:
-                shells_to_try = self.supported_shells
-
-            for shell in shells_to_try:
-                try:
-                    shell_func = getattr(self, shell)
-                    return shell_func(local_vars, pythonstartup)
-                except ImportError:
-                    pass
-                except Exception:
-                    _logger.warning("Could not start '%s' shell.", shell)
-                    _logger.debug("Shell error:", exc_info=True)
+        for shell in shells_to_try:
+            try:
+                shell_func = getattr(self, shell)
+                return shell_func(self.local_vars, pythonstartup)
+            except ImportError:
+                pass
+            except Exception:
+                _logger.warning("Could not start '%s' shell.", shell)
+                _logger.debug("Shell error:", exc_info=True)
 
     def ipython(self, local_vars, pythonstartup=None):
         from IPython import start_ipython  # noqa: PLC0415
@@ -133,35 +127,51 @@ class Shell(Command):
                 console.runsource(f.read(), filename=pythonstartup, symbol='exec')
         console.interact(banner='')
 
-    def shell(self, dbname):
-        local_vars = {
-            'openerp': odoo,
-            'odoo': odoo,
-        }
-        if self.shellargs:
-            local_vars["shellargs"] = self.shellargs
-        if dbname:
-            threading.current_thread().dbname = dbname
-            registry = Registry(dbname)
-            with registry.cursor() as cr:
-                uid = api.SUPERUSER_ID
-                ctx = api.Environment(cr, uid, {})['res.users'].context_get()
-                env = api.Environment(cr, uid, ctx)
-                local_vars['env'] = env
-                local_vars['self'] = env.user
-                self.console(local_vars)
-                cr.rollback()
-        else:
-            self.console(local_vars)
+    def run(self, cmdargs):
+        # Set eventual non-interactive arguments after SEP ('--') as local vars to shell command
+        if not self.interactive and SEP in cmdargs:
+            idx = cmdargs.index(SEP)
+            cmdargs, additional_local_vars = cmdargs[:idx], cmdargs[idx+1:]
+            self.local_vars = {
+                **self.local_vars,
+                'shellargs': additional_local_vars,
+            }
 
-    def run(self, args):
-        self.init(args)
+        # Parse the configuration
+        config.parse_config(cmdargs, setup_logging=self.interactive)
 
         dbnames = config['db_name']
         if len(dbnames) > 1:
-            sys.exit("-d/--database/db_name has multiple database, please provide a single one")
-        if not dbnames:
-            self.shell(None)
+            Command.die("-d/--database/db_name has multiple database, please provide a single one")
+        db_name = (dbnames or [None])[0]
+        if self.interactive:
+            cli_server.report_configuration()
+
+        # Start the server
+        server.start(preload=[], stop=True)
+
+        # Trap SIGINT
+        def raise_keyboard_interrupt(*a):
+            raise KeyboardInterrupt()
+        signal.signal(signal.SIGINT, raise_keyboard_interrupt)
+
+        def start():
+            if self.interactive:
+                self.console()
+            else:
+                self._execute()
+
+        # Start the console
+        if not db_name:
+            start()
         else:
-            self.shell(dbnames[0])
-        return 0
+            threading.current_thread().dbname = db_name
+            with closing(Registry(db_name).cursor()) as cr:
+                ctx = Environment(cr, SUPERUSER_ID, {})['res.users'].context_get()
+                env = Environment(cr, SUPERUSER_ID, ctx)
+                self.local_vars = {
+                    **self.local_vars,
+                    'env': env,
+                    'self': env.user,
+                }
+                start()
