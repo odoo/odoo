30c5511660a480308853be900a3a114978e0e96e
Katherine Zaoral <kz@adhoc.com.ar>
2025-02-26 12:30:47 -0300

    [IMP] l10n_ar: Update AFIP Responsibility
    Some of them were deprecated by AFIP so we target them with that tag (we
    deactivate them) Also add new AFIP responsibilities and fix the code and
    active status of the one valid.
    
    Also update the code that do the match of the document types related to
    docs types
    
      * all cases that accepted A now also accept M documents
      * remove deprecated responsibility match
      * add match to new responsibility
    
    LATAM 1300 / Adhoc Task 47476
    
    closes odoo/odoo#198247
    
    Related: odoo/enterprise#79635
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

--- addons/l10n_ar/__manifest__.py
+++ addons/l10n_ar/__manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'Argentina - Accounting',
     'icon': '/base/static/img/country_flags/ar.png',
-    'version': "3.5",
+    'version': "3.6",
     'description': """
 Functional
 ----------
