a0a76d938149d0815aefc3102e264ccc7679f36b
nipl-odoo <nipl@odoo.com>
2025-02-26 12:25:27 +0530

    [IMP] sale: pricelist rule clarification message
    Before this commit:
    As discount policy is removed from pricelist it is becoming quite hard for user
    to tell when discount is applied and when it's not.
    
    After this commit:
    While selecting price type in pricelist rules user will able to see when
    original price is displayed on sale order line and website and when it is not.
    
    Add some alert boxes for user information based on price type selected, when
    discounts feature in enabled from settings.
    
    affected version - master
    task - 4502296
    
    closes odoo/odoo#199379
    
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/sale/__manifest__.py
+++ addons/sale/__manifest__.py
@@ -47,6 +47,7 @@ This module contains all the common features of Sales Management and eCommerce.
         'views/mail_activity_plan_views.xml',
         'views/payment_views.xml',
         'views/product_document_views.xml',
+        'views/product_pricelist_item_views.xml',
         'views/product_template_views.xml',
         'views/product_views.xml',
         'views/res_partner_views.xml',
