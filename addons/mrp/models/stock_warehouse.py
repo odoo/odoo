79360e02083e6e022408cd14a3aa31852491fae1
srap-odoo <srap@odoo.com>
2025-02-26 13:12:26 +0530

    [IMP] various: simplify prefix for default picking type
    various = stock, stock_dropshipping, repair, mrp, mrp_subcontracting,
    mrp_subcontracting_dropshipping, point_of_sale
    
    This update streamlines how the picking type's sequence prefix is generated when
    creating or updating a warehouse:
    
    Before:
    --------
    Prefix = WH Code + (Dynamic Sequence Code or Default Code)
    
    After:
    -------------
    Prefix = WH Code + Default Code
    
    Previously, the prefix could be set using either a dynamic sequence code or a
    default code, depending on how the operation type was created. However, since
    the sequence prefix is now directly tied to the reference sequence, only the
    default code is used. This ensures the prefix remains consistent and reflects
    the intended format without relying on dynamic changes.
    
    Since the sequence_code no longer triggers prefix recalculation
    (WHCode + SequenceCode), updates were made wherever this behavior conflicted.
    Now, sequence_code directly reflects the related ir.sequence prefix, ensuring
    consistency without manual computation.
    
    task-4344313
    
    Part-of: odoo/odoo#190305
    Related: odoo/upgrade#7738
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/mrp/models/stock_warehouse.py
+++ addons/mrp/models/stock_warehouse.py
@@ -212,9 +212,9 @@ class StockWarehouse(models.Model):
     def _get_sequence_values(self, name=False, code=False):
         values = super(StockWarehouse, self)._get_sequence_values(name=name, code=code)
         values.update({
-            'pbm_type_id': {'name': _('%(name)s Sequence picking before manufacturing', name=self.name), 'prefix': self.code + '/' + (self.pbm_type_id.sequence_code or 'PC') + '/', 'padding': 5, 'company_id': self.company_id.id},
-            'sam_type_id': {'name': _('%(name)s Sequence stock after manufacturing', name=self.name), 'prefix': self.code + '/' + (self.sam_type_id.sequence_code or 'SFP') + '/', 'padding': 5, 'company_id': self.company_id.id},
-            'manu_type_id': {'name': _('%(name)s Sequence production', name=self.name), 'prefix': self.code + '/' + (self.manu_type_id.sequence_code or 'MO') + '/', 'padding': 5, 'company_id': self.company_id.id},
+            'pbm_type_id': {'name': _('%(name)s Sequence picking before manufacturing', name=self.name), 'prefix': self.code + '/PC/', 'padding': 5, 'company_id': self.company_id.id},
+            'sam_type_id': {'name': _('%(name)s Sequence stock after manufacturing', name=self.name), 'prefix': self.code + '/SFP/', 'padding': 5, 'company_id': self.company_id.id},
+            'manu_type_id': {'name': _('%(name)s Sequence production', name=self.name), 'prefix': self.code + '/MO/', 'padding': 5, 'company_id': self.company_id.id},
         })
         return values
 
@@ -229,7 +229,6 @@ class StockWarehouse(models.Model):
                 'default_location_src_id': self.lot_stock_id.id,
                 'default_location_dest_id': self.pbm_loc_id.id,
                 'sequence': next_sequence + 1,
-                'sequence_code': 'PC',
                 'company_id': self.company_id.id,
             },
             'sam_type_id': {
@@ -240,7 +239,6 @@ class StockWarehouse(models.Model):
                 'default_location_src_id': self.sam_loc_id.id,
                 'default_location_dest_id': self.lot_stock_id.id,
                 'sequence': next_sequence + 3,
-                'sequence_code': 'SFP',
                 'company_id': self.company_id.id,
             },
             'manu_type_id': {
@@ -249,7 +247,6 @@ class StockWarehouse(models.Model):
                 'use_create_lots': True,
                 'use_existing_lots': True,
                 'sequence': next_sequence + 2,
-                'sequence_code': 'MO',
                 'company_id': self.company_id.id,
             },
         })
