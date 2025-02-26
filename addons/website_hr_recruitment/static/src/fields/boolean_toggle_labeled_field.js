d1f41b70c7a6a8a9fd6c0287afb1b26452fee726
Abdelrahman Mahmoud (amah) <amah@odoo.com>
2025-02-26 09:57:54 +0000

    [FIX] website_hr_recruitment: drop `BooleanToggleFieldLabeled`
    - add the _t() to translate the labels coming from options.
    - add the label values to the template to be available on the pot file
    
    Task: 4488490
    X-original-commit: a5ad3d2209ee807207a34b5d174980e14c010f3c

--- addons/website_hr_recruitment/static/src/fields/boolean_toggle_labeled_field.js
+++ addons/website_hr_recruitment/static/src/fields/boolean_toggle_labeled_field.js
@@ -24,6 +24,26 @@ export const booleanToggleFieldLabeled = {
             ),
         },
     ],
+<<<<<<< master
+||||||| 2108ad3f7c851eeceb84d7459485a18e1574fa64
+    extractProps({ options }, dynamicInfo) {
+        return {
+            autosave: "autosave" in options ? Boolean(options.autosave) : true,
+            readonly: dynamicInfo.readonly,
+            true_label: options.true_label,
+            false_label: options.false_label,
+        };
+    },
+=======
+    extractProps({ options }, dynamicInfo) {
+        return {
+            autosave: "autosave" in options ? Boolean(options.autosave) : true,
+            readonly: dynamicInfo.readonly,
+            true_label: _t(options.true_label),
+            false_label: _t(options.false_label),
+        };
+    },
+>>>>>>> 57873c5f09338f7b4e7a6193a1f13903e07a88e8
 };
 
 registry.category("fields").add("boolean_toggle_labeled", booleanToggleFieldLabeled);
