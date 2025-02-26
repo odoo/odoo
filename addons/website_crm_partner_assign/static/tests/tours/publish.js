27ab778025e006a0b41d70b96b01c99cc794a1f1
Benoit Socias <bso@odoo.com>
2025-02-26 11:27:15 +0100

    [FIX] website, website_crm_partner_assign: fix publish tests
    Commit [1] introduced tests that fail when they are run without demo
    data.  The difference comes from the fact that the demo user has
    different rights when obtained from demo data or when generated by
    `HttpCaseWithUserDemo`.
    Investigating this failure has put in light a combination of other
    mistakes:
    - the rights on the user used for the test were not the expected ones:
    asserts have been added to make sure the scenario matches the
    expectation
    - the URL of the partner that was given to `start_tour` was actually
    ignored on the Javascript side which redirected to the home page
    - the test miraculously passed on the home page because the combination
    of rights of the patched demo user worked for publishing a page (which
    is not what was supposed to be tested)
    - while fixing the test, it was noticed that the `_compute_can_publish`
    method that was updated in [2] was actually run in sudo for the specific
    scenario of the test, because it is obtained during the rendering of a
    QWeb template which reads `can_publish` to fill the `html_data`
    structure, while the `main_object` is a sudoed `res.partner` in order
    to be able to display some of its fields within the page, which leads to
    accesses always being considered as granted
    
    This commit fixes this by:
    - un-sudoing the records in `_compute_can_publish` in order to properly
    check for access rights
    - starting the test tours from a `/partners` and adding a step to browse
    to the specific partner page
    - assigning a level to the test partner so that it appears in the
    `/partners` page
    
    Additionally, in 17.4, ´/website/get_seo_data´ is called after
    publishing a record, but that operation is only allowed for website
    users. This commit also makes it allowed for users having write access
    to the record, and it that case does not use the `sudo`.
    The call should not happen if the user does not have the right to
    optimize SEO data, but this is determined by a wrong condition within
    the website page template, and cannot be changed in stable.
    
    [1]: https://github.com/odoo/odoo/commit/dbed390e645c93de1145662e9e00938fed984b5f
    [2]: https://github.com/odoo/odoo/commit/1a83b2508b9383e2b7df192f8641751f71f852da
    
    task-3175890
    runbot-114278
    runbot-114279
    runbot-114281
    runbot-114283
    runbot-114287
    runbot-114289
    
    X-original-commit: 782aa441684d6c23dc313aab9eb2444a1e14d740

--- addons/website_crm_partner_assign/static/tests/tours/publish.js
+++ addons/website_crm_partner_assign/static/tests/tours/publish.js
@@ -10,8 +10,21 @@ import wTourUtils from '@website/js/tours/tour_utils';
 wTourUtils.registerWebsitePreviewTour('test_can_publish_partner', {
     edition: false,
     test: true,
+    url: '/partners',
 }, () => [{
-    content: "Unpublish",
+    content: 'Open grade filter',
+    trigger: ':iframe .dropdown a.dropdown-toggle:contains("All Categories")',
+    run: "click",
+}, {
+    content: 'Filter on Grade Test', // needed if there are demo data
+    trigger: ':iframe .dropdown a.dropdown-item:contains("Grade Test")',
+    run: "click",
+}, {
+    content: 'Go to partner',
+    trigger: ':iframe a:contains("Agrolait")',
+    run: "click",
+}, {
+    content: 'Unpublish',
     trigger: '.o_menu_systray .o_menu_systray_item.o_website_publish_container:contains("Published")',
     run: "click",
 }, {
@@ -24,4 +37,25 @@ wTourUtils.registerWebsitePreviewTour('test_can_publish_partner', {
 }, {
     content: "Wait for Publish",
     trigger: '.o_menu_systray .o_menu_systray_item.o_website_publish_container:contains("Published"):not([data-processing])',
+}]);
+
+wTourUtils.registerWebsitePreviewTour('test_cannot_publish_partner', {
+    edition: false,
+    test: true,
+    url: '/partners',
+}, () => [{
+    content: 'Open grade filter',
+    trigger: ':iframe .dropdown a.dropdown-toggle:contains("All Categories")',
+    run: "click",
+}, {
+    content: 'Filter on Grade Test', // needed if there are demo data
+    trigger: ':iframe .dropdown a.dropdown-item:contains("Grade Test")',
+    run: "click",
+}, {
+    content: 'Go to partner',
+    trigger: ':iframe a:contains("Agrolait")',
+    run: "click",
+}, {
+    content: 'Check there is no Publish/Unpublish',
+    trigger: '.o_menu_systray:not(:has(.o_menu_systray_item.o_website_publish_container))',
 }]);
--- addons/website_crm_partner_assign/tests/test_partner_assign.py
+++ addons/website_crm_partner_assign/tests/test_partner_assign.py
@@ -6,10 +6,10 @@ from unittest.mock import patch
 
 from odoo.exceptions import AccessError
 from odoo.fields import Command
-from odoo.tests.common import tagged, TransactionCase
+from odoo.tests.common import tagged, new_test_user, TransactionCase
 from odoo.tools import mute_logger
 
-from odoo.addons.base.tests.common import HttpCaseWithUserDemo
+from odoo.addons.base.tests.common import HttpCase
 from odoo.addons.crm.tests.common import TestCrmCommon
 from odoo.addons.mail.tests.common import mail_new_test_user
 from odoo.addons.website.tools import MockRequest
@@ -298,60 +298,78 @@ class TestPartnerLeadPortal(TestCrmCommon):
 
 
 @tagged('post_install', '-at_install')
-class TestPublish(HttpCaseWithUserDemo):
+class TestPublish(HttpCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.group_partner_manager = cls.env.ref('base.group_partner_manager')
         cls.group_restricted_editor = cls.env.ref('website.group_website_restricted_editor')
-        cls.group_salesman = cls.env.ref('sales_team.group_sale_salesman')
+        cls.group_sale_salesman = cls.env.ref('sales_team.group_sale_salesman')
+        # Do not rely on HttpCaseWithUserDemo to avoid having different user
+        # definitions with and without demo data.
+        cls.user_test = new_test_user(cls.env, login='testtest', website_id=False)
+
+        # Partner Grade
+        grade = cls.env['res.partner.grade'].create({
+            'name': "Grade Test",
+            'partner_weight': 42,
+            'sequence': 3,
+        })
         cls.partner = cls.env['res.partner'].create({
             'name': "Agrolait",
             'is_company': True,
             'city': "Wavre",
             'zip': "1300",
-            'country_id': cls.env.ref("base.be").id,
+            'country_id': cls.env.ref('base.be').id,
             'street': "69 rue de Namur",
             'partner_weight': 10,
             'website_published': True,
+            'grade_id': grade.id,
         })
 
     @mute_logger('odoo.addons.http_routing.models.ir_http', 'odoo.http')
     def test_01_admin(self):
-        self.start_tour(self.env['website'].get_client_action_url(self.partner.website_url), 'test_can_publish_partner', login="admin")
+        self.start_tour(self.env['website'].get_client_action_url('/partners'), 'test_can_publish_partner', login="admin")
         self.assertTrue(self.partner.website_published, "Partner should have been published")
 
     @mute_logger('odoo.addons.http_routing.models.ir_http', 'odoo.http')
     def test_02_reditor_salesman(self):
-        self.user_demo.groups_id = [
+        self.user_test.groups_id = [
             Command.link(self.group_restricted_editor.id),
-            Command.link(self.group_salesman.id),
+            Command.link(self.group_sale_salesman.id),
         ]
-        self.start_tour(self.env['website'].get_client_action_url(self.partner.website_url), 'test_can_publish_partner', login="demo")
+        self.start_tour(self.env['website'].get_client_action_url('/partners'), 'test_can_publish_partner', login="testtest")
         self.assertTrue(self.partner.website_published, "Partner should have been published")
 
     @mute_logger('odoo.addons.http_routing.models.ir_http', 'odoo.http')
     def test_03_reditor_not_salesman(self):
-        self.user_demo.groups_id = [
+        self.user_test.groups_id = [
             Command.link(self.group_restricted_editor.id),
-            Command.unlink(self.group_salesman.id),
+            Command.unlink(self.group_sale_salesman.id),
+            Command.unlink(self.group_partner_manager.id)
         ]
-        self.start_tour(self.env['website'].get_client_action_url(self.partner.website_url), 'test_can_publish_partner', login="demo")
-        self.assertTrue(self.partner.website_published, "Partner should have been published")
+        self.assertNotIn(self.group_sale_salesman.id, self.user_test.groups_id.ids, "User should not be a group_sale_salesman")
+        self.assertNotIn(self.group_partner_manager.id, self.user_test.groups_id.ids, "User should not be a group_partner_manager")
+        self.start_tour(self.env['website'].get_client_action_url('/partners'), 'test_cannot_publish_partner', login="testtest")
 
     @mute_logger('odoo.addons.http_routing.models.ir_http', 'odoo.http')
     def test_04_not_reditor_salesman(self):
-        self.user_demo.groups_id = [
+        self.user_test.groups_id = [
             Command.unlink(self.group_restricted_editor.id),
-            Command.link(self.group_salesman.id),
+            Command.link(self.group_sale_salesman.id),
         ]
-        self.start_tour(self.env['website'].get_client_action_url(self.partner.website_url), 'test_can_publish_partner', login="demo")
+        self.assertNotIn(self.group_restricted_editor.id, self.user_test.groups_id.ids, "User should not be a group_restricted_editor")
+        self.start_tour(self.env['website'].get_client_action_url('/partners'), 'test_can_publish_partner', login="testtest")
         self.assertTrue(self.partner.website_published, "Partner should have been published")
 
     @mute_logger('odoo.addons.http_routing.models.ir_http', 'odoo.http')
     def test_05_not_reditor_not_salesman(self):
-        self.user_demo.groups_id = [
+        self.user_test.groups_id = [
             Command.unlink(self.group_restricted_editor.id),
-            Command.unlink(self.group_salesman.id),
+            Command.unlink(self.group_sale_salesman.id),
+            Command.unlink(self.group_partner_manager.id)
         ]
-        self.start_tour(self.env['website'].get_client_action_url(self.partner.website_url), 'test_can_publish_partner', login="demo")
-        self.assertTrue(self.partner.website_published, "Partner should have been published")
+        self.assertNotIn(self.group_sale_salesman.id, self.user_test.groups_id.ids, "User should not be a group_sale_salesman")
+        self.assertNotIn(self.group_partner_manager.id, self.user_test.groups_id.ids, "User should not be a group_partner_manager")
+        self.assertNotIn(self.group_restricted_editor.id, self.user_test.groups_id.ids, "User should not be a group_restricted_editor")
+        self.start_tour(self.env['website'].get_client_action_url('/partners'), 'test_cannot_publish_partner', login="testtest")
