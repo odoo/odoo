2142b6c564be66d728ea2fdbd9b7070c87f858d0
Sébastien Geelen <sge@odoo.com>
2025-02-27 14:04:05 +0100

    [REF] html_builder: AddElementOption, SpacingOption
    Rewrite `AddElementOption` and `SpacingOption` to be compatible with masonry snipet
    
    Implement `AddElementOption` button functionalities

--- addons/html_builder/static/src/plugins/add_element_option.js
+++ addons/html_builder/static/src/plugins/add_element_option.js
@@ -1,22 +1,161 @@
+import { Plugin } from "@html_editor/plugin";
+import { registry } from "@web/core/registry";
+import { _t } from "@web/core/l10n/translation";
+import { resizeGrid, setElementToMaxZindex } from "@html_builder/utils/grid_layout_utils";
+import { withSequence } from "@html_editor/utils/resource";
 import { Component } from "@odoo/owl";
-import { defaultBuilderComponents } from "../core/default_builder_components";
-import { Button } from "./button";
+import { defaultBuilderComponents } from "@html_builder/core/default_builder_components";
 
 export class AddElementOption extends Component {
     static template = "html_builder.AddElementOption";
-    static components = {
-        ...defaultBuilderComponents,
-        Button,
+    static components = { ...defaultBuilderComponents };
+    static props = {
+        level: { type: Number, optional: true },
+        applyTo: { type: String, optional: true },
     };
-    static props = {};
+    static defaultProps = {
+        level: 0,
+    };
+}
+export class AddElementOptionPlugin extends Plugin {
+    static id = "AddElementOption";
+    static dependencies = ["history", "media"];
+    resources = {
+        builder_options: [
+            withSequence(11, {
+                OptionComponent: AddElementOption,
+                selector: ".s_masonry_block",
+                applyTo: ".o_grid_mode",
+            }),
+        ],
+        builder_actions: this.getActions(),
+    };
 
-    addText() {
-        console.log("addText");
+    getActions() {
+        return {
+            addElText: {
+                apply: ({ editingElement }) => {
+                    const colSize = 4;
+                    const rowSize = 2;
+
+                    const newElement = document.createElement("p");
+                    newElement.textContent = _t("Write something...");
+
+                    this.addElement(editingElement, newElement, colSize, rowSize, [
+                        "col-lg-4",
+                        "g-col-lg-4",
+                        "g-height-2",
+                    ]);
+                },
+            },
+            addElImage: {
+                load: async () => {
+                    let selectedImage;
+                    await new Promise((resolve) => {
+                        const onClose = this.dependencies.media.openMediaDialog({
+                            onlyImages: true,
+                            save: (images) => {
+                                selectedImage = images;
+                                resolve();
+                            },
+                        });
+                        onClose.then(resolve);
+                    });
+                    if (!selectedImage) {
+                        return;
+                    }
+
+                    await new Promise((resolve) => {
+                        selectedImage.addEventListener("load", () => resolve(), {
+                            once: true,
+                        });
+                    });
+                    return selectedImage;
+                },
+                apply: ({ editingElement, loadResult: image }) => {
+                    const colSize = 6;
+                    const rowSize = 6;
+
+                    this.addElement(editingElement, image, colSize, rowSize, [
+                        "col-lg-6",
+                        "g-col-lg-6",
+                        "g-height-6",
+                        "o_grid_item_image",
+                    ]);
+                },
+            },
+            addElButton: {
+                apply: ({ editingElement }) => {
+                    const colSize = 2;
+                    const rowSize = 1;
+
+                    const newButton = document.createElement("a");
+                    newButton.href = "#";
+                    newButton.classList.add("mb-2", "btn", "btn-primary");
+                    newButton.textContent = "Button";
+
+                    this.addElement(editingElement, newButton, colSize, rowSize, [
+                        "col-lg-2",
+                        "g-col-lg-2",
+                        "g-height-1",
+                    ]);
+                },
+            },
+        };
     }
-    addImage() {
-        console.log("addImage");
-    }
-    addButton() {
-        console.log("addButton");
+
+    /**
+     * Adds an image, some text or a button in the grid.
+     *
+     * It can probaly be refactored and improved.
+     *
+     * @see this.selectClass for parameters
+     * @see based on addons/web_editor/static/src/js/editor/snippets.options.js::addElement()
+     */
+    addElement(container, element, colSize, rowSize, classes) {
+        // If it has been less than 15 seconds that we have added an element,
+        // shift the new element right and down by one cell. Otherwise, put it
+        // in the top left corner.
+        const currentTime = new Date().getTime();
+        if (this.lastAddTime && (currentTime - this.lastAddTime) / 1000 < 15) {
+            this.lastStartPosition = [this.lastStartPosition[0] + 1, this.lastStartPosition[1] + 1];
+        } else {
+            this.lastStartPosition = [1, 1]; // [rowStart, columnStart]
+        }
+        this.lastAddTime = currentTime;
+
+        // Create the new column.
+        const newColumnEl = document.createElement("div");
+        newColumnEl.classList.add("o_grid_item", ...classes);
+        newColumnEl.appendChild(element);
+
+        // Place the column in the grid.
+        const rowStart = this.lastStartPosition[0];
+        let columnStart = this.lastStartPosition[1];
+        if (columnStart + colSize > 13) {
+            columnStart = 1;
+            this.lastStartPosition[1] = columnStart;
+        }
+        newColumnEl.style.gridArea = `${rowStart} / ${columnStart} / ${rowStart + rowSize} / ${
+            columnStart + colSize
+        }`;
+
+        // Setting the z-index to the maximum of the grid.
+        setElementToMaxZindex(newColumnEl, container);
+
+        // Add the new column and update the grid height.
+        container.appendChild(newColumnEl);
+        resizeGrid(container);
+
+        const newColumnPosition = newColumnEl.getBoundingClientRect();
+        const middleX = (newColumnPosition.left + newColumnPosition.right) / 2;
+        const middleY = (newColumnPosition.top + newColumnPosition.bottom) / 2;
+        const sameCoordinatesEl = this.document.elementFromPoint(middleX, middleY);
+        if (!sameCoordinatesEl || !newColumnEl.contains(sameCoordinatesEl)) {
+            newColumnEl.scrollIntoView({ behavior: "smooth", block: "center" });
+        }
+        this.dependencies.history.addStep();
     }
 }
+
+registry.category("website-plugins").add(AddElementOptionPlugin.id, AddElementOptionPlugin);
