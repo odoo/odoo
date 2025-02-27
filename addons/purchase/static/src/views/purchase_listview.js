40308184143ee0e145b645a27c33d8680d628526
Arnold Moyaux <arm@odoo.com>
2025-02-27 18:07:13 +0100

    [IMP] purchase: upload bill
    Modify the purchase flow for billing.
    The user will not generate or print the odoo's bill but
    instead use the vendor's bill.
    
    task-4578975
    
    Part-of: odoo/odoo#206244
    Related: odoo/enterprise#83518
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/purchase/static/src/views/purchase_listview.js
+++ addons/purchase/static/src/views/purchase_listview.js
@@ -1,15 +1,26 @@
 import { registry } from "@web/core/registry";
 import { listView } from "@web/views/list/list_view";
 import { ListRenderer } from "@web/views/list/list_renderer";
+import { ListController } from "@web/views/list/list_controller";
 import { PurchaseDashBoard } from "@purchase/views/purchase_dashboard";
+import { PurchaseFileUploader } from "@purchase/components/purchase_file_uploader/purchase_file_uploader";
 
 export class PurchaseDashBoardRenderer extends ListRenderer {
-    static template = "purchase.PurchaseListView";
+    static template = "purchase.ListRenderer";
     static components = Object.assign({}, ListRenderer.components, { PurchaseDashBoard });
+}
+
+export class FileUploadListController extends ListController {
+    static template = `purchase.ListView`;
+    static components = {
+        ...ListController.components,
+        PurchaseFileUploader,
+    };
 }
 
 export const PurchaseDashBoardListView = {
     ...listView,
+    Controller: FileUploadListController,
     Renderer: PurchaseDashBoardRenderer,
 };
 
