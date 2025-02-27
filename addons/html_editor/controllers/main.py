d7a6bf9c9b18f9f1e511e0c47f2339d95cf6b7af
aath-odoo <aath@odoo.com>
2025-02-27 18:09:26 +0530

    [IMP] html_editor: search `ir.attachment` with sudo
    This commit bypasses the access rights of the attachments for
    saving/searching the images received from the media library. It is
    especially useful when a portal user wants to upload an image in the
    editor.
    
    Task-4334962

--- addons/html_editor/controllers/main.py
+++ addons/html_editor/controllers/main.py
@@ -601,7 +601,9 @@ class HTML_Editor(Controller):
         for id, url in response.json().items():
             req = requests.get(url)
             name = '_'.join([media[id]['query'], url.split('/')[-1]])
-            IrAttachment = request.env['ir.attachment']
+            # Need to bypass security check to search/write image with mimetype image/svg+xml
+            # ok because svgs come from whitelisted origin
+            IrAttachment = request.env['ir.attachment'].with_user(SUPERUSER_ID)
             attachment_data = {
                 'name': name,
                 'mimetype': req.headers['content-type'],
@@ -611,10 +613,8 @@ class HTML_Editor(Controller):
                 'res_id': 0,
             }
             attachment = get_existing_attachment(IrAttachment, attachment_data)
-            # Need to bypass security check to write image with mimetype image/svg+xml
-            # ok because svgs come from whitelisted origin
             if not attachment:
-                attachment = IrAttachment.with_user(SUPERUSER_ID).create(attachment_data)
+                attachment = IrAttachment.create(attachment_data)
             if media[id]['is_dynamic_svg']:
                 colorParams = werkzeug.urls.url_encode(media[id]['dynamic_colors'])
                 attachment['url'] = '/html_editor/shape/illustration/%s?%s' % (slug(attachment), colorParams)
