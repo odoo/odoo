769a8894b4537ac29a85c6b184b78fee1ae4dfb4
Julien Mougenot <jum@odoo.com>
2025-02-27 15:15:20 +0100

    [FIX] website,*: adapt tests
    This commit adapts website & its related addons to recent changes in
    Hoot, most notably the automatic disabling of animations & transitions.

--- addons/website/static/tests/interactions/snippets/helpers.js
+++ addons/website/static/tests/interactions/snippets/helpers.js
@@ -1,35 +1,46 @@
-import { animationFrame, advanceTime, manuallyDispatchProgrammaticEvent, scroll } from "@odoo/hoot-dom";
+import { advanceTime, animationFrame, scroll } from "@odoo/hoot-dom";
 import { defineStyle } from "@web/../tests/web_test_helpers";
 
-export const endTransition = async function () {
+export async function endTransition() {
     // Ensure we finish the transition
     await animationFrame();
     // Ensure the class "o_transitioning" is removed
     await advanceTime(500);
 }
 
-export const setupTest = async function (core, wrapwrap) {
+/**
+ * @param {any} core
+ * @param {any} wrapwrap
+ */
+export async function setupTest(core, wrapwrap) {
     wrapwrap.style.height = "800px";
     wrapwrap.style.width = "100%";
     wrapwrap.style.overflow = "scroll";
     core.interactions[0].interaction.scrollingElement = wrapwrap;
-    defineStyle(/* css */`.hidden { display: none !important; }`);
-    defineStyle(/* css */`.h20 { height: 20px; }`);
+    defineStyle(/* css */ `.hidden { display: none !important; }`);
+    defineStyle(/* css */ `.h20 { height: 20px; }`);
     await endTransition();
 }
 
-export const simpleScroll = async function (wrapwrapEl, target) {
+/**
+ * @param {Parameters<scroll>[0]} wrapwrapEl
+ * @param {Parameters<scroll>[1]} target
+ */
+export async function simpleScroll(wrapwrapEl, target) {
     await scroll(wrapwrapEl, target);
-    await manuallyDispatchProgrammaticEvent(document, "scroll");
     await endTransition();
 }
 
-// Scroll twice to correctly updates parameters used by onScroll handlers.
-// (cf. Headers)
-export const doubleScroll = async function (wrapwrapEl, target, source) {
-    await scroll(wrapwrapEl, { y: source + (target > source ? 1 : -1) })
-    await manuallyDispatchProgrammaticEvent(document, "scroll");
+/**
+ * Scroll twice to correctly updates parameters used by onScroll handlers.
+ * (cf. Headers)
+ *
+ * @param {Parameters<scroll>[0]} wrapwrapEl
+ * @param {number} target
+ * @param {number} source
+ */
+export async function doubleScroll(wrapwrapEl, target, source) {
+    await scroll(wrapwrapEl, { y: source + (target > source ? 1 : -1) });
     await scroll(wrapwrapEl, { y: target });
-    await manuallyDispatchProgrammaticEvent(document, "scroll");
     await endTransition();
 }
