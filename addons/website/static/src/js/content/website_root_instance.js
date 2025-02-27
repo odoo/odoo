99ce7c9fe0e610ecf25d0be9b9255eaecb6f4e38
Benoit Socias <bso@odoo.com>
2025-02-27 12:52:45 +0000

    [FIX] website: make iframe ready also for non-website users
    The `is-ready` attribute of the website preview iframe is only set after
    the wysiwyg lazy assets are loaded.
    Because of this, the iframe is never set as ready for users who are not
    a least website restricted editors.
    This prevents tours from matching selectors inside the iframe.
    
    This commit also makes the public root ready for non-website users.
    
    runbot-114278
    runbot-114279
    runbot-114281
    runbot-114283
    runbot-114287
    runbot-114289
    
    X-original-commit: b3e572566bd55a4534de3af05d11745ec06b9c34
    Part-of: odoo/odoo#201954
    Related: odoo/enterprise#81547
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>
    Signed-off-by: Benoit Socias (bso) <bso@odoo.com>

--- addons/website/static/src/js/content/website_root_instance.js
+++ addons/website/static/src/js/content/website_root_instance.js
@@ -8,24 +8,25 @@ import { AssetsLoadingError, loadBundle } from "@web/core/assets";
 const prom = createPublicRoot(WebsiteRoot).then(async rootInstance => {
     // This data attribute is set by the WebsitePreview client action for a
     // restricted editor user.
-    if (window.frameElement && window.frameElement.dataset.loadWysiwyg === 'true') {
-
-        try {
-            await Promise.all([
-                loadBundle("website.assets_all_wysiwyg"),
-                loadBundle("website.assets_edit_frontend")
-            ]);
-        } catch (e){
-            if (e instanceof AssetsLoadingError && e.cause instanceof TypeError) {
-                // an AssetsLoadingError caused by a TypeError means that the
-                // fetch request has been cancelled by the browser. It can occur
-                // when the user changes page, or navigate away from the website
-                // client action, so the iframe is unloaded. In this case, we
-                // don't care abour reporting the error, it is actually a normal
-                // situation.
-                return new Promise(() => {});
-            } else {
-                throw e;
+    if (window.frameElement) {
+        if (window.frameElement.dataset.loadWysiwyg === 'true') {
+            try {
+                await Promise.all([
+                    loadBundle("website.assets_all_wysiwyg"),
+                    loadBundle("website.assets_edit_frontend")
+                ]);
+            } catch (e){
+                if (e instanceof AssetsLoadingError && e.cause instanceof TypeError) {
+                    // an AssetsLoadingError caused by a TypeError means that the
+                    // fetch request has been cancelled by the browser. It can occur
+                    // when the user changes page, or navigate away from the website
+                    // client action, so the iframe is unloaded. In this case, we
+                    // don't care abour reporting the error, it is actually a normal
+                    // situation.
+                    return new Promise(() => {});
+                } else {
+                    throw e;
+                }
             }
         }
         window.dispatchEvent(new CustomEvent('PUBLIC-ROOT-READY', {detail: {rootInstance}}));
