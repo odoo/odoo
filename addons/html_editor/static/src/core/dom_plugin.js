955028b57910f0ad5299c05adf7e532788a9f1f3
adch-odoo <adch@odoo.com>
2025-02-27 18:56:05 +0530

    [FIX] html_editor: remove invisible text node
    Steps to reproduce:
    
    - Open the To-Do app.
    - Create three blocks by pressing Enter.
    - Move to the middle block using arrow keys and insert an icon.
    - An invisible text node appears as the first child.
    
    Before this commit:
    
    - A text node was added to maintain caret positioning but wasn't
    removed after insertion.
    
    After this commit:
    
    - The unnecessary invisible text node is removed.
    
    task: 3045033

--- addons/html_editor/static/src/core/dom_plugin.js
+++ addons/html_editor/static/src/core/dom_plugin.js
@@ -226,9 +226,10 @@ export class DomPlugin extends Plugin {
             }
         }
 
+        let textNode;
         if (startNode.nodeType === Node.ELEMENT_NODE) {
             if (selection.anchorOffset === 0) {
-                const textNode = this.document.createTextNode("");
+                textNode = this.document.createTextNode("");
                 if (isSelfClosingElement(startNode)) {
                     startNode.parentNode.insertBefore(textNode, startNode);
                 } else {
@@ -450,6 +451,10 @@ export class DomPlugin extends Plugin {
             { anchorNode: lastPosition[0], anchorOffset: lastPosition[1] },
             { normalize: false }
         );
+        // Remove empty textNode if it exists
+        if (textNode?.isConnected) {
+            textNode.remove();
+        }
         return firstInsertedNodes.concat(insertedNodes).concat(lastInsertedNodes);
     }
 
