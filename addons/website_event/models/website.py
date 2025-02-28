8fe4e22821633ddcb4807cf1b42a2fc01f127f74
Aurélien Warnon <awa@odoo.com>
2025-02-28 16:56:18 +0100

    [IMP] website_event: allow creating pages as part of an event
    It is currently not possible to complement your event with custom pages.
    
    This commit makes this possible by allowing to add menu entries and then
    creating new web pages from there.
    
    Note that this is relatively tricky as it plays with website.menu and
    website.event.menu relationship to try to determine the context of event page
    creation.
    
    Additional note: this was requested to be done in stable, meaning we had to
    take some shortcuts to have stable-compliant code.
    
    Task-3661323

--- addons/website_event/models/website.py
+++ addons/website_event/models/website.py
@@ -1,11 +1,47 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, _
+from odoo import api, models, _
 
 
 class Website(models.Model):
     _inherit = "website"
+
+    @api.model
+    def new_page(self, name=False, add_menu=False, template='website.default_page', ispage=True, namespace=None, page_values=None, menu_values=None, sections_arch=None):
+        """ Override the page creation in the context of events.
+
+         When creating a page for an event, the page needs to be embedded inside the
+         'website_event.layout' template, otherwise it is not visually contained within that event.
+         Note that to create an event page, one has to first create a menu entry in that event.
+
+         To determine if this page is an event page:
+         - Check that the path starts with 'event/', this should avoid extra requests in other contexts
+         - Fetch a website.menu linked to this path
+         - Check if we have a website.event.menu linked to that website.menu.
+
+         In addition, we attach the created view to the website.event.menu and adapt they view key
+         to make it unique in the context of our event, which makes it possible to find the view in
+         the event pages controller.
+
+         See: website.menu#save override """
+
+        website_event_menu = False
+        if template == 'website.default_page' and name and name.startswith('event/'):
+            website_menu = self.env["website.menu"].sudo().search([('url', '=', '/' + name)], limit=1)
+            website_event_menu = self.env["website.event.menu"].sudo().search([
+                ('menu_id', '=', website_menu.id)
+            ], limit=1)
+            if website_event_menu:
+                template = "website_event.layout"
+
+        new_page = super().new_page(name, add_menu, template, ispage, namespace, page_values, menu_values, sections_arch)
+
+        if website_event_menu and new_page.get('view_id'):
+            website_event_menu.view_id = new_page['view_id']
+            website_event_menu.view_id.key = f'website_event.{website_event_menu.event_id.name}-{name.split("/")[-1]}'
+
+        return new_page
 
     def get_suggested_controllers(self):
         suggested_controllers = super(Website, self).get_suggested_controllers()
