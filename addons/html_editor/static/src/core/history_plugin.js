ef3beb80a7a9eae5b4ec0711200974194c4d5f77
Antoine Guenet <age@odoo.com>
2025-02-28 11:46:36 +0100

    [FIX] html_editor: fix an old typo
    There was a typo in the history plugin, making an `if` statement
    useless. This fixes the typo.
    
    task-4224709
    
    Part-of: odoo/odoo#186917
    Signed-off-by: Geelen Sébastien (sge) <sge@odoo.com>

--- addons/html_editor/static/src/core/history_plugin.js
+++ addons/html_editor/static/src/core/history_plugin.js
@@ -1216,7 +1216,7 @@ export class HistoryPlugin extends Plugin {
     }
 
     _onDocumentBeforeInput(ev) {
-        if (this.editable.contains(ev.targget)) {
+        if (this.editable.contains(ev.target)) {
             return;
         }
         if (["historyUndo", "historyRedo"].includes(ev.inputType)) {
