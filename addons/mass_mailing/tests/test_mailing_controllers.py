odoo/odoo

commit 2d9da403d09ad2536b3f1dc4d18e450c69601cac

[FIX] mass_mailing: fix links in test mailings
This commit fixes an issue with the mailing_mailing_test wizard in mass_mailing.
When sending a test email the snippet `s_mail_block_header_view` contains a
placeholder link that is replaced by the actual link when rendered.
When the user sends a test mailing, the mail sent will still contain the
placeholder link without being replaced.
This means that when clicking on it the user is redirected to a 404 error as
this is a mere placeholder.

This link isn't replaced because the mail created inside testing wizard did not
give a res_id, linked to a `mailing.contact`, to the `mail.mail` created. This
led the function `_prepare_outgoing_list` to be exited prematurely without
replacing any placeholder.

This commit fixes the issue by adding a res_id linked to the current user to the
mail.mail created in order for the method to finish its execution. But we don't
want the unsubscribe links to be replaced, as the user could blacklist himself.
Thus we added a context key `send_as_test` which blocks the replacement of the
unsubscribe link.

task-3869575

closes odoo/odoo#165711

Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

addons/mass_mailing/models/mail_mail.py
@@ -82,7 +82,9 @@ class MailMail(models.Model):
 
             # replace links in body
             if not tools.is_html_empty(email_values['body']):
-                if f'{base_url}/unsubscribe_from_list' in email_values['body']:
+                # replace generic link by recipient-specific one, except if we know
+                # by advance it won't work (i.e. testing mailing scenario)
+                if f'{base_url}/unsubscribe_from_list' in email_values['body'] and not self.env.context.get('mailing_test_mail'):
                     email_values['body'] = email_values['body'].replace(
                         f'{base_url}/unsubscribe_from_list',
                         unsubscribe_url,

addons/mass_mailing/wizard/mailing_mailing_test.py
@@ -67,10 +67,12 @@ class TestMassMailing(models.TransientModel):
                 'attachment_ids': [(4, attachment.id) for attachment in mailing.attachment_ids],
                 'auto_delete': False,  # they are manually deleted after notifying the document
                 'mail_server_id': mailing.mail_server_id.id,
+                'model': 'res.users',
+                'res_id': self.env.user.id,
             }
             mail = self.env['mail.mail'].sudo().create(mail_values)
             mails_sudo |= mail
-        mails_sudo.send()
+        mails_sudo.with_context({'mailing_test_mail': True}).send()
 
         notification_messages = []
         if invalid_candidates:

addons/test_mass_mailing/tests/test_mailing_test.py
@@ -75,6 +75,45 @@ class TestMailingTest(TestMassMailCommon):
         with self.mock_mail_gateway(), self.assertRaises(Exception):
             mailing_test.send_mail_test()
 
+    @users('user_marketing')
+    @mute_logger('odoo.addons.mail.models.mail_render_mixin')
+    def test_mailing_test_button_links(self):
+        """This tests that the link provided by the View in Browser snippet is correctly replaced
+        when sending a test mailing while the Unsubscribe button's link isn't, to preserve the testing route
+        /unsubscribe_from_list.
+        This also checks that other links containing the /view route aren't replaced along the way.
+        """
+        mailing = self.test_mailing_bl.with_env(self.env)
+        mailing_test = self.env['mailing.mailing.test'].create({
+            'email_to': 'test@test.com',
+            'mass_mailing_id': mailing.id,
+        })
+        # Test if link snippets are correctly converted
+        mailing.write({
+            'body_html':
+                '''<p>
+                Hello <a href="http://www.example.com/view">World<a/>
+                    <div class="o_snippet_view_in_browser o_mail_snippet_general pt16 pb16" style="text-align: center; padding-left: 15px; padding-right: 15px;">
+                        <a href="/view">
+                            View Online
+                        </a>
+                    </div>
+                    <div class="o_mail_footer_links">
+                        <a role="button" href="/unsubscribe_from_list" class="btn btn-link">Unsubscribe</a>
+                    </div>
+                </p>''',
+            'preview': 'Preview {{ object.name }}',
+            'subject': 'Subject {{ object.name }}',
+        })
+
+        with self.mock_mail_gateway():
+            mailing_test.send_mail_test()
+
+        body_html = self._mails.pop()['body']
+        self.assertIn(f'/mailing/{mailing.id}/view', body_html)  # Is replaced
+        self.assertIn('/unsubscribe_from_list', body_html)  # Isn't replaced
+        self.assertIn('http://www.example.com/view', body_html)  # Isn't replaced
+
     def test_mailing_test_equals_reality(self):
         """ Check that both test and real emails will format the qweb and inline
         placeholders correctly in body and subject. """

commit 08899476c687ae059d5f30a2a85486635570225a

[FIX] mass_mailing: ensure 'View Online' links work in mass mailing test
Currently, sending a test mass mailing email that includes both the
'View Online' widget and a dynamic placeholder results in a link that
leads to a traceback.

Steps to Reproduce
------------------
1. Start a database **without** demo data.
2. Install the `mass_mailing` module.
3. Create a new Mailing where recipient is a mailing list
4. Ensure the mail body contains the "View Online" widget and a dynamic
   placeholder.
5. Click on the "Test" button to test the mailing.
6. Open the mail that is sent and click the "View Online" link.

You will encounter a traceback:
```
odoo.exceptions.MissingError: Record does not exist or has been deleted.
```

Cause
-----
The issue was introduced in commit
2d9da403d09ad2536b3f1dc4d18e450c69601cac, which made it so that when
sending a test mass mailing, the mail is rendered on the `res.users`
model with the `res_id` of the current user. The "View Online" widget
generates a URL with this `res_id`. However, when the link is clicked,
the system expects the `res_id` to refer to a `mailing.contact` record
because the model is taken from the mailing itself, causing a crash if
the id does not exist in `mailing.contact`, or displaying incorrect data
if it does.

opw-4487325

addons/mass_mailing/models/mail_mail.py
@@ -57,7 +57,7 @@ class MailMail(models.Model):
         view links by email-specific links. Also add headers to allow
         unsubscribe from email managers. """
         email_list = super()._prepare_outgoing_list(recipients_follower_status)
-        if not self.res_id or not self.mailing_id:
+        if (not self.res_id and self.res_id != 0) or not self.mailing_id:
             return email_list
 
         base_url = self.mailing_id.get_base_url()

addons/mass_mailing/tests/test_mailing_controllers.py
@@ -6,6 +6,7 @@ from markupsafe import Markup
 from requests import Session, PreparedRequest, Response
 
 import datetime
+import re
 import werkzeug
 
 from odoo import tools
@@ -627,6 +628,67 @@ class TestMailingControllers(TestMailingControllersCommon):
             )
         )
         self.assertEqual(res.status_code, 200)
+
+
+    def test_mailing_test_placeholder_view_online_snippet(self):
+        mailing = self.test_mailing_on_lists.with_env(self.env)
+        mailing.write({
+            'body_html': '''
+                <p>
+                    <div class="o_snippet_view_in_browser o_mail_snippet_general">
+                        <a href="/view">
+                            View Online
+                        </a>
+                    </div>
+                    <p>Hello <t t-out="object.name"/></p>
+                </p>
+            ''',
+        })
+
+        mailing_test = self.env['mailing.mailing.test'].create({
+            'email_to': 'test@test.com',
+            'mass_mailing_id': mailing.id,
+        })
+
+        self.env['mailing.contact'].search([('id', '!=', self.test_contact.id)]).unlink()
+        with self.mock_mail_gateway():
+            mailing_test.send_mail_test()
+
+        body_html = self._mails.pop()['body']
+
+        self.assertIn(f'/mailing/{mailing.id}/view', body_html)
+        self.assertIn(f"Hello {self.test_contact.name}", body_html)
+
+        match = re.search(r'<a\s+href="([^"]+)"[^>]*>\s*View Online\s*</a>', body_html)
+        view_online_url = match.group(1) if match else None
+
+        if not view_online_url:
+            self.fail("View Online url not found")
+
+        res = self.url_open(view_online_url)
+
+        self.assertIn(f"Hello {self.test_contact.name}", res.text)
+
+        # test the case where the mailing model has no records
+        self.env['mailing.contact'].search([]).unlink()
+        with self.mock_mail_gateway():
+            mailing_test.send_mail_test()
+
+        body_html = self._mails.pop()['body']
+
+        self.assertIn(f'/mailing/{mailing.id}/view', body_html)
+        self.assertIn("Hello", body_html)
+
+        match = re.search(r'<a\s+href="([^"]+)"[^>]*>\s*View Online\s*</a>', body_html)
+        view_online_url = match.group(1) if match else None
+
+        if not view_online_url:
+            self.fail("View Online url not found")
+
+        res = self.url_open(view_online_url)
+
+        self.assertIn("Hello", res.text)
+
 
 
 @tagged('link_tracker', 'mailing_portal')

addons/mass_mailing/wizard/mailing_mailing_test.py
@@ -67,8 +67,8 @@ class TestMassMailing(models.TransientModel):
                 'attachment_ids': [(4, attachment.id) for attachment in mailing.attachment_ids],
                 'auto_delete': False,  # they are manually deleted after notifying the document
                 'mail_server_id': mailing.mail_server_id.id,
-                'model': 'res.users',
-                'res_id': self.env.user.id,
+                'model': mailing.mailing_model_real,
+                'res_id': record.id or 0,
             }
             mail = self.env['mail.mail'].sudo().create(mail_values)
             mails_sudo |= mail