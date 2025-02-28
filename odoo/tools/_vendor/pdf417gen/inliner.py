e2d1a976e262fd6e3a46539aeb8d6494275a4b70
Paolo Gatti <pgi@odoo.com>
2025-02-28 17:24:22 +0100

    [IMP] core: pdf417 inlining, part 2
    console.py
    encoding.py

--- odoo/tools/_vendor/pdf417gen/inliner.py
+++ odoo/tools/_vendor/pdf417gen/inliner.py
@@ -2,7 +2,10 @@
 
 import re
 import sys
+from io import StringIO
+from collections import defaultdict
 from pathlib import Path
+
 
 if sys.argv[-1] == sys.argv[0]:
     sys.exit("Usage: inliner.py <file.py>")
@@ -12,21 +15,99 @@ path = Path(filename)
 if not path.exists():
     sys.exit(f"Filename {filename} not found")
 
+
 print(f"<<< Inlining {path}...")
 
+
 repo = {}
-with (
-    path.open("r") as infile,
-    Path("./pdf417gen.py").open("a") as outfile,
-):
-    outfile.write("\n\n")
+out = StringIO()
+outpath = Path("./pdf417gen.py")
+imports_repo = defaultdict(list)
+
+
+def get_matches(line):
+    if match := re.match(r"from ([^ ]+) import (.+)", line):
+        lib = match.group(1).strip()
+        tokens = [x.strip() for x in match.group(2).split(",")]
+        return lib, tokens
+    if match := re.match(r"import ([^ ]+)", line):
+        lib = match.group(1).strip()
+        return lib, [None]
+
+
+header, post_header = StringIO(), StringIO()
+with outpath.open("r") as infile:
+    target = header
+    for idx, line in enumerate(infile):
+        if line == '"""\n' and idx > 0:
+            target.write(line)
+            target = post_header
+        else:
+            if target == post_header:
+                if match := get_matches(line):
+                    lib, tokens = match
+                    imports_repo[lib] += tokens
+                    continue
+            target.write(line)
+    header.seek(0)
+    post_header.seek(0)
+
+
+with path.open("r") as infile:
     for line in infile:
-        if match := re.match(r"def ([^(]+)", line):
+        if match := get_matches(line):
+            lib, tokens = match
+            imports_repo[lib] += tokens
+            continue
+        if match := re.match(r"def (?<!il_)([^(]+)\(", line):
             old = match.group(1)
-            new = f'{path.stem}_{old}'
+            new = f'il_{path.stem}_{old}'
             repo[old] = new
+
+
+with path.open("r") as infile:
+    for line in infile:
+        if match := get_matches(line):
+            continue
         for old, new in repo.items():
-            line = re.sub(old, new, line)
-        outfile.write(line)
+            if old in line:
+                after = re.sub(old + '\(', new + '(', line)
+                print(f'{old} -> {new}  {line.rstrip()}')
+                line = after
+        out.write(line.rstrip() + '\n')
+    out.seek(0)
+
+
+imports_header = []
+for key, values in imports_repo.items():
+    if None in values:
+        imports_header.append(f"import {key}\n")
+    tokens = [x for x in values if x]
+    if tokens:
+        imports_header.append(f"from {key} import {', '.join(tokens)}\n")
+imports_header = '\n\n' + "".join(sorted(imports_header)) + '\n\n\n'
+
+
+with outpath.open("w") as outfile:
+    empty_count = 0
+    def write(empty_count, line):
+        if not line.strip():
+            empty_count += 1
+        else:
+            empty_count = 0
+        if empty_count == 3:
+            empty_count = 2
+        else:
+            outfile.write(line)
+        return empty_count
+
+    for line in header:
+        empty_count = write(empty_count, line)
+    for line in imports_header:
+        empty_count = write(empty_count, line)
+    for line in post_header:
+        empty_count = write(empty_count, line)
+    for line in out:
+        empty_count = write(empty_count, line)
 
 print(">>> Inlining done.")
--- odoo/tools/_vendor/pdf417gen/pdf417gen.py
+++ odoo/tools/_vendor/pdf417gen/pdf417gen.py
@@ -24,6 +24,16 @@
 """
 
 
+from argparse import ArgumentParser
+from pdf417gen import encode, render_image
+from pdf417gen.codes import map_code_word
+from pdf417gen.compaction import compact
+from pdf417gen.error_correction import compute_error_correction_code_words
+from pdf417gen.util import chunks, to_bytes
+from typing import List
+import math
+import sys
+
 # Converts high-level (base 929) code words into low-level code words (binary
 # patterns for drawing the bar code).
 CODES = [
@@ -33,10 +43,258 @@ CODES = [
 ]
 
 
-def codes_map_code_word(table, word):
+def il_codes_map_code_word(table, word):
     """Maps high-level code words to low level code words.
     """
     assert 0 <= table <= 2, "table must be between 0 and 2, given: %r" % table
     assert 0 <= word <= 929, "word must be between 0 and 929, given: %r" % word
 
     return CODES[table][word]
+
+
+def il_console_print_usage():
+    print("Usage: pdf417gen [command]")
+    print("")
+    print("Commands:")
+    print("  help    show this help message and exit")
+    print("  encode  generate a bar code from given input")
+    print("")
+    print("https://github.com/ihabunek/pdf417gen")
+
+
+def il_console_print_err(msg: str):
+    sys.stderr.write('\033[91m' + msg + '\033[0m' + "\n")
+
+
+def il_console_get_parser() -> ArgumentParser:
+    parser = ArgumentParser(epilog="https://github.com/ihabunek/pdf417gen",
+                            description="Generate a bar code from given input")
+
+    parser.add_argument("text", type=str, nargs="?",
+                        help="Text or data to encode. Alternatively data can be piped in.")
+
+    parser.add_argument("-c", "--columns", dest="columns", type=int,
+                        help="The number of columns (default is 6).",
+                        default=6)
+
+    parser.add_argument("-l", "--security-level", dest="security_level", type=int,
+                        help="Security level (default is 2).",
+                        default=2)
+
+    parser.add_argument("-e", "--encoding", dest="encoding", type=str,
+                        help="Character encoding used to decode input (default is utf-8).",
+                        default='utf-8')
+
+    parser.add_argument("-s", "--scale", dest="scale", type=int,
+                        help="Module width in pixels (default is 3).",
+                        default=3)
+
+    parser.add_argument("-r", "--ratio", dest="ratio", type=int,
+                        help="Module height to width ratio (default is 3).",
+                        default=3)
+
+    parser.add_argument("-p", "--padding", dest="padding", type=int,
+                        help="Image padding in pixels (default is 20).",
+                        default=20)
+
+    parser.add_argument("-f", "--foreground-color", dest="fg_color", type=str,
+                        help="Foreground color in hex (default is '#000000').",
+                        default="#000000")
+
+    parser.add_argument("-b", "--background-color", dest="bg_color", type=str,
+                        help="Foreground color in hex (default is '#FFFFFF').",
+                        default="#FFFFFF")
+
+    parser.add_argument("-o", "--output", dest="output", type=str,
+                        help="Target file (if not given, will just show the barcode).")
+
+    return parser
+
+
+def il_console_do_encode(raw_args: List[str]):
+    args = il_console_get_parser().parse_args(raw_args)
+    text: str = args.text
+
+    # If no text is given, check stdin
+    if not text:
+        text = sys.stdin.read()
+
+    if not text:
+        il_console_print_err("No input given")
+        return
+
+    try:
+        codes = encode(
+            text,
+            columns=args.columns,
+            security_level=args.security_level,
+            encoding=args.encoding,
+        )
+
+        image = render_image(
+            codes,
+            scale=args.scale,
+            ratio=args.ratio,
+            padding=args.padding,
+            fg_color=args.fg_color,
+            bg_color=args.bg_color,
+        )
+    except Exception as e:
+        il_console_print_err(str(e))
+        return
+
+    if args.output:
+        image.save(args.output)
+    else:
+        image.show()
+
+
+def il_console_main():
+    command = sys.argv[1] if len(sys.argv) > 1 else None
+    args = sys.argv[2:]
+
+    if command == 'encode':
+        il_console_do_encode(args)
+    else:
+        il_console_print_usage()
+
+
+START_CHARACTER = 0x1fea8
+STOP_CHARACTER = 0x3fa29
+PADDING_CODE_WORD = 900
+
+# Maximum nubmer of code words which can be contained in a bar code, including
+# the length descriptor, data, error correction and padding
+MAX_CODE_WORDS = 928
+
+# Limits on the number of rows and columns which can be contained in a bar code
+MIN_ROWS = 3
+MAX_ROWS = 90
+
+
+def il_encoding_encode(data, columns=6, security_level=2, encoding="utf-8"):
+    if columns < 1 or columns > 30:
+        raise ValueError("'columns' must be between 1 and 30. Given: %r" % columns)
+
+    if security_level < 0 or security_level > 8:
+        raise ValueError("'security_level' must be between 1 and 8. Given: %r" % security_level)
+
+    num_cols = columns  # Nomenclature
+
+    # Prepare input
+    data_bytes = to_bytes(data, encoding)
+
+    # Convert data to code words and split into rows
+    code_words = il_encoding_encode_high(data_bytes, num_cols, security_level)
+    rows = list(chunks(code_words, num_cols))
+
+    return list(il_encoding_encode_rows(rows, num_cols, security_level))
+
+
+def il_encoding_encode_rows(rows, num_cols, security_level):
+    num_rows = len(rows)
+
+    for row_no, row_data in enumerate(rows):
+        left = il_encoding_get_left_code_word(row_no, num_rows, num_cols, security_level)
+        right = il_encoding_get_right_code_word(row_no, num_rows, num_cols, security_level)
+
+        yield il_encoding_encode_row(row_no, row_data, left, right)
+
+
+def il_encoding_encode_row(row_no, row_words, left, right):
+    table_idx = row_no % 3
+
+    # Convert high level code words to low level code words
+    left_low = map_code_word(table_idx, left)
+    right_low = map_code_word(table_idx, right)
+    row_words_low = [map_code_word(table_idx, word) for word in row_words]
+
+    return [START_CHARACTER, left_low] + row_words_low + [right_low, STOP_CHARACTER]
+
+
+def il_encoding_encode_high(data, columns, security_level):
+    """Converts the input string to high level code words.
+
+    Including the length indicator and the error correction words, but without
+    padding.
+    """
+
+    # Encode data to code words
+    data_words = list(compact(data))
+    data_count = len(data_words)
+
+    # Get the padding to align data to column count
+    ec_count = 2 ** (security_level + 1)
+    padding_words = il_encoding_get_padding(data_count, ec_count, columns)
+    padding_count = len(padding_words)
+
+    # Length descriptor includes the data CWs, padding CWs and the descriptor
+    # itself but not the error correction CWs
+    length_descriptor = data_count + padding_count + 1
+
+    # Total number of code words and number of rows
+    cw_count = data_count + ec_count + padding_count + 1
+    row_count = math.ceil(cw_count / columns)
+
+    # Check the generated bar code's size is within specification parameters
+    il_encoding_validate_barcode_size(length_descriptor, row_count)
+
+    # Join encoded data with the length specifier and padding
+    extendend_words = [length_descriptor] + data_words + padding_words
+
+    # Calculate error correction words
+    ec_words = compute_error_correction_code_words(extendend_words, security_level)
+
+    return extendend_words + ec_words
+
+
+def il_encoding_validate_barcode_size(length_descriptor, row_count):
+    if length_descriptor > MAX_CODE_WORDS:
+        raise ValueError(
+            "Data too long. Generated bar code has length descriptor of %d. "
+            "Maximum is %d." % (length_descriptor, MAX_CODE_WORDS))
+
+    if row_count < MIN_ROWS:
+        raise ValueError(
+            "Generated bar code has %d rows. Minimum is %d rows. "
+            "Try decreasing column count." % (row_count, MIN_ROWS))
+
+    if row_count > MAX_ROWS:
+        raise ValueError(
+            "Generated bar code has %d rows. Maximum is %d rows. "
+            "Try increasing column count." % (row_count, MAX_ROWS))
+
+
+def il_encoding_get_left_code_word(row_no, num_rows, num_cols, security_level):
+    table_id = row_no % 3
+
+    if table_id == 0:
+        x = (num_rows - 1) // 3
+    elif table_id == 1:
+        x = security_level * 3 + (num_rows - 1) % 3
+    elif table_id == 2:
+        x = num_cols - 1
+
+    return 30 * (row_no // 3) + x
+
+
+def il_encoding_get_right_code_word(row_no, num_rows, num_cols, security_level):
+    table_id = row_no % 3
+
+    if table_id == 0:
+        x = num_cols - 1
+    elif table_id == 1:
+        x = (num_rows - 1) // 3
+    elif table_id == 2:
+        x = security_level * 3 + (num_rows - 1) % 3
+
+    return 30 * (row_no // 3) + x
+
+
+def il_encoding_get_padding(data_count, ec_count, num_cols):
+    # Total number of data words and error correction words, additionally
+    # reserve 1 code word for the length descriptor
+    total_count = data_count + ec_count + 1
+    mod = total_count % num_cols
+
+    return [PADDING_CODE_WORD] * (num_cols - mod) if mod > 0 else []
