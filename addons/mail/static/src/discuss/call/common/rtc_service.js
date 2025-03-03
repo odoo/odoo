df0eab147f58d7f221df1ec08ee136d9f2c618f7
TSO <tso@odoo.com>
2025-03-03 07:13:36 +0000

    [FIX] discuss: add odoo info to rtc logs
    This commit also fixes the filename date
    
    closes odoo/odoo#200224
    
    X-original-commit: bf5a8199f00a34c0f23fb919f694057776f8cdc7
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/discuss/call/common/rtc_service.js
+++ addons/mail/static/src/discuss/call/common/rtc_service.js
@@ -341,7 +341,7 @@ export class Rtc extends Record {
             connectionType: undefined,
             hasPendingRequest: false,
             channel: undefined,
-            globalLogs: {},
+            globalLogs: { odooInfo: odoo.info },
             logs: new Map(), // deprecated
             sendCamera: false,
             sendScreen: false,
