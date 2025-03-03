3481f2decf3244f033e3080e67819831db1abfaf
nipl-odoo <nipl@odoo.com>
2025-03-03 12:23:16 +0530

    [FIX] website_sale: cart quantity management in last removal from cart
    Steps:
    - Install Ecommerce
    - Add some products to the cart
    - Remove them using using 'remove' option
    - Go to my/orders
    - select any order then click on Order again
    - Click on the Add To Cart button
    - You'll see one confirmation dialog
    
    Issue:
    - By clicking on add to cart button that confirmation dialog should not be there
      as cart is empty
    
    Cause:
    - Cart quantity does not update when last product is removed
    
    Fix:
    - While removing last product call update value in sessionStorage to ensure that
      last removal is recorded in session storage
    
    affected version-17.0
    opw-4566505

--- addons/website_sale/static/src/js/website_sale.js
+++ addons/website_sale/static/src/js/website_sale.js
@@ -5,6 +5,7 @@ import VariantMixin from "@website_sale/js/variant_mixin";
 import wSaleUtils from "@website_sale/js/website_sale_utils";
 const cartHandlerMixin = wSaleUtils.cartHandlerMixin;
 import "@website/libs/zoomodoo/zoomodoo";
+import { browser } from "@web/core/browser/browser";
 import {extraMenuUpdateCallbacks} from "@website/js/content/menu";
 import { ProductImageViewer } from "@website_sale/js/components/website_sale_image_viewer";
 import { jsonrpc } from "@web/core/network/rpc_service";
@@ -236,6 +237,8 @@ export const WebsiteSale = publicWidget.Widget.extend(VariantMixin, cartHandlerM
                 return;
             }
             if (!data.cart_quantity) {
+                // Ensures last cart removal is recorded
+                browser.sessionStorage.setItem('website_sale_cart_quantity', 0);
                 return window.location = '/shop/cart';
             }
             $input.val(data.quantity);
