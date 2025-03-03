0e42cb1d88b464141b72eae95106e393f325179f
ramh <ramh@odoo.com>
2025-03-03 16:46:51 +0530

    [IMP] mail: add filter options for message search in chatter
    This commit introduces a filter button in the chatter's search functionality,
    allowing users to refine message searches. The available filters include:
    
    - **All activity** (default)
    - **Conversation** (returns messages and log notes)
    - **Tracked changes** (returns all other changes)
    
    Task-4614500

--- addons/mail/static/src/core/common/search_message_input.js
+++ addons/mail/static/src/core/common/search_message_input.js
@@ -1,6 +1,9 @@
 import { Component, useExternalListener, useState } from "@odoo/owl";
 import { browser } from "@web/core/browser/browser";
 import { useAutofocus } from "@web/core/utils/hooks";
+import { Dropdown } from "@web/core/dropdown/dropdown";
+import { DropdownItem } from "@web/core/dropdown/dropdown_item";
+import { _t } from "@web/core/l10n/translation";
 
 /**
  * @typedef {Object} Props
@@ -11,10 +14,15 @@ import { useAutofocus } from "@web/core/utils/hooks";
 export class SearchMessageInput extends Component {
     static template = "mail.SearchMessageInput";
     static props = ["closeSearch?", "messageSearch", "thread"];
+    static components = { Dropdown, DropdownItem };
 
     setup() {
         super.setup();
-        this.state = useState({ searchTerm: "", searchedTerm: "" });
+        this.state = useState({
+            searchTerm: "",
+            searchedTerm: "",
+            searchType: this.env.inChatter ? "all_activity" : "",
+        });
         useAutofocus();
         useExternalListener(
             browser,
@@ -30,6 +38,7 @@ export class SearchMessageInput extends Component {
 
     search() {
         this.props.messageSearch.searchTerm = this.state.searchTerm;
+        this.props.messageSearch.searchType = this.state.searchType;
         this.props.messageSearch.search();
         this.state.searchedTerm = this.state.searchTerm;
     }
@@ -50,5 +59,28 @@ export class SearchMessageInput extends Component {
         } else {
             this.search();
         }
+    }
+
+    setSearchType(searchType) {
+        if (searchType !== this.state.searchType) {
+            this.state.searchType = searchType;
+        }
+    }
+
+    get searchTypes() {
+        return [
+            {
+                name: _t("All Activity"),
+                label: "all_activity",
+            },
+            {
+                name: _t("Conversation"),
+                label: "conversation",
+            },
+            {
+                name: _t("Tracked Changes"),
+                label: "tracked_changes",
+            },
+        ];
     }
 }
