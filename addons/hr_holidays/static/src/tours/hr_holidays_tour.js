1d7006e4a9893f1db53d4e435b15da669bda57b5
gvar-odoo <gvar@odoo.com>
2025-03-04 12:36:33 +0000

    [FIX] hr_holidays: prevent crashing when openning Time off
    Crash with traceback when openning Time Off in Serbian (latin)
    
    Steps to reproduce:
    -------------------
    *Change user language to Serbian (Latin)
    *Open Time Off app
    > Observation:
    Traceback
    Why the fix:
    ------------
    Intl.NumberFormat only accept dash-case format. @ in sr@latin is not supported.
    
    opw-4583287
    
    X-original-commit: 553f2e5d94d457195488a6093408845ceea70171

--- addons/hr_holidays/static/src/tours/hr_holidays_tour.js
+++ addons/hr_holidays/static/src/tours/hr_holidays_tour.js
@@ -100,3 +100,18 @@ registry.category("web_tour.tours").add("hr_holidays_tour", {
         },
     ],
 });
+
+registry.category("web_tour.tours").add('hr_holidays_launch', {
+    url: '/web',
+    steps: () => [
+        stepUtils.showAppsMenuItem(),
+        {
+            trigger: '.o_app[data-menu-xmlid="hr_holidays.menu_hr_holidays_root"]',
+            run: "click",
+        },
+        {
+            trigger: '.o_calendar_container',
+            run: ()=>{},
+        },
+    ],
+});
