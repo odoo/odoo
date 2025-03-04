ae0b09cdb7ad4f4c0f09257e23f4a42627dfaba7
tsm-odoo <tsm@odoo.com>
2025-03-04 13:48:48 +0100

    [FIX] im_livechat: start bus on other tabs when chat is created
    Before this PR, creating a live chat in a first tab would correctly
    open the chat window in a second tab. However, messages were not
    received in real-time before the second tab reload. This issue occurs
    because the bus service is not started in the second tab.
    
    To solve this issue, the bus service is now started when there is at
    least one thread known locally. The behavior stays lazy, nothing is
    starting until required.
    
    part of task-4607689

--- addons/im_livechat/static/tests/embed/expirable_storage.test.js
+++ addons/im_livechat/static/tests/embed/expirable_storage.test.js
@@ -2,10 +2,11 @@ import { expirableStorage } from "@im_livechat/embed/common/expirable_storage";
 
 import { describe, expect, test } from "@odoo/hoot";
 import { mockDate } from "@odoo/hoot-mock";
+import { asyncStep, waitForSteps } from "@web/../tests/web_test_helpers";
 
 describe.current.tags("desktop");
 
-test("value is removed from expirable storage after expiration", async () => {
+test("value is removed from expirable storage after expiration", () => {
     mockDate("2023-01-01 00:00:00");
     const ONE_DAY = 60 * 60 * 24;
     expirableStorage.setItem("foo", "bar", ONE_DAY);
@@ -14,4 +15,21 @@ test("value is removed from expirable storage after expiration", async () => {
     expect(expirableStorage.getItem("foo")).toBe("bar");
     mockDate("2023-01-02 00:00:01");
     expect(expirableStorage.getItem("foo")).toBe(null);
+});
+
+test("subscribe/unsubscribe to storage changes", async () => {
+    const fooCallback = (value) => asyncStep(`foo - ${value}`);
+    const barCallback = (value) => asyncStep(`bar - ${value}`);
+    expirableStorage.onChange("foo", fooCallback);
+    expirableStorage.onChange("bar", barCallback);
+    expirableStorage.setItem("foo", 1);
+    await waitForSteps(["foo - 1"]);
+    expirableStorage.setItem("bar", 2);
+    await waitForSteps(["bar - 2"]);
+    expirableStorage.removeItem("foo");
+    await waitForSteps(["foo - null"]);
+    expirableStorage.offChange("foo", fooCallback);
+    expirableStorage.setItem("foo", 3);
+    expirableStorage.removeItem("bar");
+    await waitForSteps(["bar - null"]);
 });
