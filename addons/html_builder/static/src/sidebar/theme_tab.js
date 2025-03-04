6ba0cdbf9316c996efd676361ec3f8f8ae0a0485
Benoit Socias <bso@odoo.com>
2025-03-04 10:01:33 +0100

    theme options

---
+++ addons/html_builder/static/src/sidebar/theme_tab.js
@@ -0,0 +1,22 @@
+import { Component, useState } from "@odoo/owl";
+import { OptionsContainer } from "./option_container";
+import { useIsActiveItem } from "@html_builder/core/building_blocks/utils";
+import { useOptionsSubEnv } from "@html_builder/utils/utils";
+
+export class ThemeTab extends Component {
+    static template = "html_builder.ThemeTab";
+    static components = { OptionsContainer };
+    static props = {
+        // optionsContainers: { type: Array, optional: true },
+    };
+    static defaultProps = {
+        // optionsContainers: [],
+    };
+
+    setup() {
+        useOptionsSubEnv(() => [this.env.editor.document.body]);
+        this.isActiveItem = useIsActiveItem();
+        this.state = useState({});
+        this.optionsContainers = this.env.editor.resources["theme_options"];
+    }
+}
