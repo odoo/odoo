df7cead72e421b0bf05d0cba8f291100869e05c1
Sarah Bellefroid <sbel@odoo.com>
2025-03-05 08:36:54 +0000

    [FIX] pos_self_order: prevent adding to cart if option not selected
    Currently, when ordering on kiosk a menu combo, if you do not select sides
    it will crash when adding it to the cart
    
    Steps to reproduce:
    -------------------
    * Open kiosk
    * Select burger Menu Combo
    * Select Cheese burger
    * DO NOT select sides
    * Select Next button
    * Add drink
    * Add to cart
    > Observation: White screen crashes
    
    Why the fix:
    ------------
    The crash is ultimately cause by a undefined `attribute_value_ids`
    https://github.com/odoo/odoo/blob/ea67d5ad5f81c7eae85cff352bcc75290053ba20/addons/pos_self_order/static/src/app/self_order_service.js#L263-L266
    
    If we compare the behavior of the same product, outside a combo, we are not
    allowed to add the product to the cart if we did not select sides. We will
    now have a consistent behavior with/without combo.
    
    opw-4516061
    
    X-original-commit: 5aa2e223ebe5419697517b200fcb202c036775e3
    Part-of: odoo/odoo#203148
    Signed-off-by: Vlad Stroia (vlst) <vlst@odoo.com>
    Signed-off-by: Sarah Bellefroid (sbel) <sbel@odoo.com>

--- addons/pos_self_order/tests/test_self_order_kiosk.py
+++ addons/pos_self_order/tests/test_self_order_kiosk.py
@@ -80,3 +80,36 @@ class TestSelfOrderKiosk(SelfOrderCommonTest):
         self.pos_config.with_user(self.pos_user).open_ui()
         self_route = self.pos_config._get_self_order_route()
         self.start_tour(self_route, "self_order_language_changes")
+
+    def test_self_order_kiosk_combo_sides(self):
+        combo = self.env["product.combo"].create(
+            {
+                "name": "Desk Accessories Combo",
+                "combo_item_ids": [
+                    Command.create({
+                        "product_id": self.desk_organizer.id,
+                        "extra_price": 0,
+                    }),
+                ],
+            }
+        )
+        self.env["product.product"].create(
+            {
+                "available_in_pos": True,
+                "list_price": 40,
+                "name": "Office Combo",
+                "type": "combo",
+                "combo_ids": [
+                    (6, 0, [combo.id])
+                ],
+            }
+        )
+        self.pos_config.write({
+            'self_ordering_mode': 'kiosk',
+            'self_ordering_pay_after': 'each',
+            'self_ordering_service_mode': 'table',
+        })
+        self.pos_config.with_user(self.pos_user).open_ui()
+        self.pos_config.current_session_id.set_opening_control(0, "")
+        self_route = self.pos_config._get_self_order_route()
+        self.start_tour(self_route, "test_self_order_kiosk_combo_sides")
