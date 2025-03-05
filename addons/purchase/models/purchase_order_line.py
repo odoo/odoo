2c3545bcf691b8f714a0a8fa943cc118fbb323cd
Mohammad Abdulmoneim (abdu) <abdu@odoo.com>
2025-03-05 13:59:12 +0100

    [FIX] purchase: eliminate inactive vendors when suggesting quantity
    This commit makes sure that pricelists with non-matching dates are not
    picked when suggesting quntities.
    
    Task-4471379

--- addons/purchase/models/purchase_order_line.py
+++ addons/purchase/models/purchase_order_line.py
@@ -386,8 +386,12 @@ class PurchaseOrderLine(models.Model):
         '''
         if not self.product_id:
             return
+        date = self.order_id.date_order and self.order_id.date_order.date() or fields.Date.context_today(self)
         seller_min_qty = self.product_id.seller_ids\
-            .filtered(lambda r: r.partner_id == self.order_id.partner_id and (not r.product_id or r.product_id == self.product_id))\
+            .filtered(lambda r: r.partner_id == self.order_id.partner_id and
+                      (not r.product_id or r.product_id == self.product_id) and
+                      (not r.date_start or r.date_start <= date) and
+                      (not r.date_end or r.date_end >= date))\
             .sorted(key=lambda r: r.min_qty)
         if seller_min_qty:
             self.product_qty = seller_min_qty[0].min_qty or 1.0
