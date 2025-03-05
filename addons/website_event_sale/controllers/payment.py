ae7a634ab6163a7575307031d51882da22ceb4f6
Levi Siuzdak (sile) <sile@odoo.com>
2025-03-05 14:04:59 +0000

    [FIX] event, website_event_sale: check event availability before payment
    Versions
    --------
    - 17.0+
    
    Steps
    -----
    1. Create an event with an attendee limit of 1;
    2. publish the event;
    3. add a ticket to you cart;
    4. go through checkout until your reach the payment page;
    5. open a incognito window or different browser;
    6. add same ticket to cart;
    7. go to the payment step;
    8. click "Pay now" and wait for confirmation;
    9. go to previous window, and click "Pay now".
    
    Issue
    -----
    The payment gets confirmed, but you land on an internal server error due
    to a lack of available seats for the event.
    
    Cause
    -----
    Commit ffc9026361beb added a check before payment to ensure the tickets
    still had seats available. The event itself however can have a separate
    seat limit that isn't currently being checked.
    
    Solution
    --------
    Add a check on the event's seat availability to
    `_validate_transaction_for_order`.
    
    Also, in case there's a lack of seats, display the correct number of
    lacking seats for the order instead of `-available_seats`.
    
    opw-4453539
    
    X-original-commit: a5e66593ec31efd9ba380013652598090e4d2fa2

--- addons/website_event_sale/controllers/payment.py
+++ addons/website_event_sale/controllers/payment.py
@@ -9,9 +9,21 @@ class PaymentPortalOnsite(PaymentPortal):
         Throws a ValidationError if the user tries to pay for a ticket which isn't available
         """
         super()._validate_transaction_for_order(transaction, sale_order)
+        registration_domain = [
+            ('sale_order_id', '=', sale_order.id),
+            ('event_ticket_id', '!=', False),
+            ('state', '!=', 'cancel'),
+        ]
         count_per_ticket = request.env['event.registration'].sudo()._read_group(
-            [('sale_order_id', 'in', sale_order.ids), ('state', '!=', 'cancel'), ('event_ticket_id', '!=', False)],
+            registration_domain,
             ['event_ticket_id'], ['__count']
         )
         for ticket, count in count_per_ticket:
             ticket._check_seats_availability(minimal_availability=count)
+
+        count_per_event = request.env['event.registration'].sudo()._read_group(
+            registration_domain,
+            ['event_id'], ['__count']
+        )
+        for event, count in count_per_event:
+            event._check_seats_availability(minimal_availability=count)
