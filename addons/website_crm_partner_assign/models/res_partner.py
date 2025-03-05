keyboard-slayer/odoo-eval

[IMP] crm,*: display proper opportunity and its count
*= website_crm_partner_assign

Issue 1
In an opportunity if a partner is set as a customer and that
opportunity is assigned to that same partner then the
opportunity counts in the Stat button is wrong.

Issue 2
If a partner is created with this hierarchy
Parent (company) -> Child-1 (individual) -> Child-1's Child (individual)
and in an opportunity customer is set as Child-1's child then its count is
propagated to all parents but when Child-1 clicks on the stat button
it shows nothing.

Specifications
==============
1) Improve the opportunity count computation.
2) Propagate the opportunities to parents when a child is assigned,
similar to when a child is set as a customer.

Technical
=========
For issue 1:
When a partner is designated as a customer, the _compute_opportunity_count
method in CRM increases the opportunity count by 1. If the same partner is
then assigned to that opportunity, the _compute_opportunity_count method in
website_crm_partner_assign will increase the count by an additional 1,
resulting in the count being incremented twice.

For issue 2:
Currently, when a company clicks on the stat button, it can view its child
opportunities because in the commercial_partner_id that parent is set.
However, if an individual parent partner clicks on the stat button, they
will not be able to see their child opportunities because our parent is
individual and commercial_partner_id is empty so it will show no records.

After this commit
=================
So now if the above scenario happens, the opportunity count will only compute once
and now every parent will able to see their child's opportunity.

Task-3871399

addons/website_crm_partner_assign/models/res_partner.py
@@ -57,29 +57,27 @@ class ResPartner(models.Model):
         for partner in self:
             partner.partner_weight = partner.grade_id.partner_weight if partner.grade_id else 0
 
-    def _compute_opportunity_count(self):
-        super()._compute_opportunity_count()
-        opportunity_data = self.env['crm.lead'].with_context(active_test=False)._read_group(
-            [('partner_assigned_id', 'in', self.ids)],
-            ['partner_assigned_id'], ['__count']
-        )
-        assign_counts = {partner_assigned.id: count for partner_assigned, count in opportunity_data}
-        for partner in self:
-            partner.opportunity_count += assign_counts.get(partner.id, 0)
+    def _get_contact_opportunities_domain(self):
+        all_partners = self._fetch_children_partners_for_hierarchy().ids
+        return ['|', ('partner_assigned_id', 'in', all_partners), ('partner_id', 'in', all_partners)]
 
-    def action_view_opportunity(self):
-        self.ensure_one()  # especially here as we are doing an id, in, IDS domain
-        action = super().action_view_opportunity()
-        action_domain_origin = action.get('domain')
-        action_context_origin = action.get('context') or {}
-        action_domain_assign = [('partner_assigned_id', '=', self.id)]
-        if not action_domain_origin:
-            action['domain'] = action_domain_assign
-            return action
-        # perform searches independently as having OR with those leaves seems to
-        # be counter productive
-        Lead = self.env['crm.lead'].with_context(**action_context_origin, active_test=False)
-        ids_origin = Lead.search(action_domain_origin).ids
-        ids_new = Lead.search(action_domain_assign).ids
-        action['domain'] = [('id', 'in', sorted(list(set(ids_origin) | set(ids_new))))]
-        return action
+    def _compute_opportunity_count(self):
+        opportunity_data = self.env['crm.lead'].with_context(active_test=False)._read_group(
+            self._get_contact_opportunities_domain(),
+            ['partner_assigned_id', 'partner_id'], ['__count']
+        )
+        self_ids = set(self._ids)
+
+        self.opportunity_count = 0
+        for assign_partner, partner, count in opportunity_data:
+            # this variable is used to keep the track of the partner
+            seen_partners = set()
+            while partner or assign_partner:
+                if assign_partner and assign_partner.id in self_ids and assign_partner not in seen_partners:
+                    assign_partner.opportunity_count += count
+                    seen_partners.add(assign_partner)
+                if partner and partner.id in self_ids and partner not in seen_partners:
+                    partner.opportunity_count += count
+                    seen_partners.add(partner)
+                assign_partner = assign_partner.parent_id
+                partner = partner.parent_id

odoo/odoo

addons/website_crm_partner_assign/models/res_partner.py
@@ -55,32 +55,30 @@ class ResPartner(models.Model):
         for partner in self:
             partner.partner_weight = partner.grade_id.partner_weight if partner.grade_id else 0
 
+    def _get_contact_opportunities_domain(self):
+        all_partners = self._fetch_children_partners_for_hierarchy().ids
+        return ['|', ('partner_assigned_id', 'in', all_partners), ('partner_id', 'in', all_partners)]
+
     def _compute_opportunity_count(self):
-        super()._compute_opportunity_count()
-        if not self.env.user.has_group('sales_team.group_sale_salesman'):
-            return
+        if not self.ids or not self.env.user.has_group('sales_team.group_sale_salesman'):
+            return super()._compute_opportunity_count()
 
+        self.opportunity_count = 0
         opportunity_data = self.env['crm.lead'].with_context(active_test=False)._read_group(
-            [('partner_assigned_id', 'in', self.ids)],
-            ['partner_assigned_id'], ['__count']
+            self._get_contact_opportunities_domain(),
+            ['partner_assigned_id', 'partner_id'], ['__count']
         )
-        assign_counts = {partner_assigned.id: count for partner_assigned, count in opportunity_data}
-        for partner in self:
-            partner.opportunity_count += assign_counts.get(partner.id, 0)
+        current_pids = set(self._ids)
 
-    def action_view_opportunity(self):
-        self.ensure_one()  # especially here as we are doing an id, in, IDS domain
-        action = super().action_view_opportunity()
-        action_domain_origin = action.get('domain')
-        action_context_origin = action.get('context') or {'active_test': False}
-        action_domain_assign = [('partner_assigned_id', '=', self.id)]
-        if not action_domain_origin:
-            action['domain'] = action_domain_assign
-            return action
-        # perform searches independently as having OR with those leaves seems to
-        # be counter productive
-        Lead = self.env['crm.lead'].with_context(**action_context_origin)
-        ids_origin = Lead.search(action_domain_origin).ids
-        ids_new = Lead.search(action_domain_assign).ids
-        action['domain'] = [('id', 'in', sorted(list(set(ids_origin) | set(ids_new))))]
-        return action
+        for assign_partner, partner, count in opportunity_data:
+            # this variable is used to keep the track of the partner
+            seen_partners = set()
+            while partner or assign_partner:
+                if assign_partner and assign_partner.id in current_pids and assign_partner not in seen_partners:
+                    assign_partner.opportunity_count += count
+                    seen_partners.add(assign_partner)
+                if partner and partner.id in current_pids and partner not in seen_partners:
+                    partner.opportunity_count += count
+                    seen_partners.add(partner)
+                assign_partner = assign_partner.parent_id
+                partner = partner.parent_id