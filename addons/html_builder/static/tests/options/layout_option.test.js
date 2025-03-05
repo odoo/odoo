87604ace8d615a37db336540b2649ab2c9fbdbdd
FrancoisGe <fge@odoo.com>
2025-03-05 14:27:00 +0100

    REF API builder_style_actions

--- addons/html_builder/static/tests/options/layout_option.test.js
+++ addons/html_builder/static/tests/options/layout_option.test.js
@@ -1,4 +1,5 @@
 import { expect, test } from "@odoo/hoot";
+import { queryAll } from "@odoo/hoot-dom";
 import { contains } from "@web/../tests/web_test_helpers";
 import { defineWebsiteModels, setupWebsiteBuilderWithSnippet } from "../website_helpers";
 
@@ -20,6 +21,21 @@ test("switch grid layout to column layout", async () => {
     await contains("[data-label='Layout'] .dropdown-toggle").click();
     await contains("[data-action-value='5']").click();
     expect("[data-label='Layout'] .dropdown-toggle").toHaveText("5");
+});
+
+test("edit section spacing (Y,X)", async () => {
+    await setupWebsiteBuilderWithSnippet("s_banner");
+    await contains(":iframe .s_banner").click();
+    expect(":iframe .s_banner .row").toHaveStyle({ "row-gap": "normal" });
+    expect(":iframe .s_banner .row").toHaveStyle({ "column-gap": "normal" });
+
+    await contains("[data-label='Spacing (Y, X)'] input:first").edit(10);
+    expect(":iframe .s_banner .row").toHaveStyle({ "row-gap": "10px" });
+    expect(":iframe .s_banner .row").toHaveStyle({ "column-gap": "normal" });
+
+    await contains(queryAll("[data-label='Spacing (Y, X)'] input")[1]).edit(20);
+    expect(":iframe .s_banner .row").toHaveStyle({ "row-gap": "10px" });
+    expect(":iframe .s_banner .row").toHaveStyle({ "column-gap": "20px" });
 });
 
 test("switch to mobile mode should update number of columns", async () => {
