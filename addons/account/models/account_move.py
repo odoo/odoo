e8bd78b13002afcf4069d0f33dda4d2049508d2f
mega <mega@odoo.com>
2025-03-05 13:26:25 +0000

    [FIX] account: prevent error when removing accounting date from invoice bill
    Currently, An error occurs when removing an `Accounting Date` from the invoice
    bill form view.
    
    Steps to produce:
    
    - Install the 'account' module (with demo data).
    - Go to Invoicing / Vendors / Bills.
    - Create a bill, remove value from the Accounting Date, and click anywhere in
    form view.
    
    AttributeError: 'bool' object has no attribute 'year'
    
    An error occurs when the system tries to get a year part from the accounting
    date [1], but the value of accounting date is missing.
    
    Link 1: https://github.com/odoo/odoo/blob/a0205ffa3d4a42321a41503b8056a9f1c89328bb/addons/account/models/account_move.py#L3522
    
    If the accounting date is not available, then provide a default value as an
    invoice date or today's date to resolve an issue.
    
    Sentry-6188666136
    
    closes odoo/odoo#202149
    
    X-original-commit: bfe75f657282ed79ebd7b71bd4046f1b52ef053e
    Signed-off-by: John Laterre (jol) <jol@odoo.com>
    Signed-off-by: Meet Gandhi (mega) <mega@odoo.com>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -3540,17 +3540,18 @@ class AccountMove(models.Model):
     def _get_starting_sequence(self):
         # EXTENDS account sequence.mixin
         self.ensure_one()
-        year_part = "%04d" % self.date.year
+        move_date = self.date or self.invoice_date or fields.Date.context_today(self)
+        year_part = "%04d" % move_date.year
         last_day = int(self.company_id.fiscalyear_last_day)
         last_month = int(self.company_id.fiscalyear_last_month)
         is_staggered_year = last_month != 12 or last_day != 31
         if is_staggered_year:
-            max_last_day = calendar.monthrange(self.date.year, last_month)[1]
+            max_last_day = calendar.monthrange(move_date.year, last_month)[1]
             last_day = min(last_day, max_last_day)
-            if self.date > date(self.date.year, last_month, last_day):
-                year_part = "%s-%s" % (self.date.strftime('%y'), (self.date + relativedelta(years=1)).strftime('%y'))
+            if move_date > date(move_date.year, last_month, last_day):
+                year_part = "%s-%s" % (move_date.strftime('%y'), (move_date + relativedelta(years=1)).strftime('%y'))
             else:
-                year_part = "%s-%s" % ((self.date + relativedelta(years=-1)).strftime('%y'), self.date.strftime('%y'))
+                year_part = "%s-%s" % ((move_date + relativedelta(years=-1)).strftime('%y'), move_date.strftime('%y'))
         # Arbitrarily use annual sequence for sales documents, but monthly
         # sequence for other documents
         if self.journal_id.type in ['sale', 'bank', 'cash', 'credit']:
@@ -3559,7 +3560,7 @@ class AccountMove(models.Model):
             # example). Note that it's already the case for monthly sequences.
             starting_sequence = "%s/%s/%s" % (self.journal_id.code, year_part, '0000' if is_staggered_year else '00000')
         else:
-            starting_sequence = "%s/%s/%02d/0000" % (self.journal_id.code, year_part, self.date.month)
+            starting_sequence = "%s/%s/%02d/0000" % (self.journal_id.code, year_part, move_date.month)
         if self.journal_id.refund_sequence and self.move_type in ('out_refund', 'in_refund'):
             starting_sequence = "R" + starting_sequence
         if self.journal_id.payment_sequence and self.origin_payment_id or self.env.context.get('is_payment'):
