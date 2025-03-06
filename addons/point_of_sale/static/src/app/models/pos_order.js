3ca459040e20141594815f09cba4e1623f3ac413
Sarah Bellefroid <sbel@odoo.com>
2025-03-06 13:36:14 +0100

    [FIX] point_of_sale: start order tracking number at 1
    Currently, when you make a os order, close the session an then re-open
    it without going through the backend, the starting tracking number is
    related to the previous session.
    
    Steps to reproduce:
    -------------------
    * Open pos shop
    * Make an order and pay it
    * Select **New order**
    * Close the session, do not go in the backend
    * Re-open the session
    > Observation: The tracking number is like `X02`. If we make an order,
    the tracking number will then be `(X+1)01`.
    
    Why the fix:
    ------------
    When closing the register the last orders does not get cancelled. This
    order will be used in the next session with its tracking number related
    to its original session.
    
    By marking this new order (which is empty) as cancelled it will create
    a new order in `aftertProcessServerData` with  a tracking number and
    sequence number related to this current session.
    
    opw-4435140
    
    closes odoo/odoo#194666
    
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- addons/point_of_sale/static/src/app/models/pos_order.js
+++ addons/point_of_sale/static/src/app/models/pos_order.js
@@ -19,6 +19,10 @@ export class PosOrder extends Base {
 
         if (!this.session_id && (!this.finalized || typeof this.id !== "number")) {
             this.update({ session_id: this.session });
+
+            if (this.state === "draft" && this.lines.length == 0 && this.payment_ids.length == 0) {
+                this._isResidual = true;
+            }
         }
 
         // Data present in python model
--- addons/point_of_sale/tests/test_frontend.py
+++ addons/point_of_sale/tests/test_frontend.py
@@ -1628,6 +1628,12 @@ class TestUi(TestPointOfSaleHttpCommon):
             self.main_pos_config.with_user(self.pos_user).open_ui()
             self.start_tour(f"/pos/ui?config_id={self.main_pos_config.id}", 'SearchMoreCustomer', login="pos_user")
 
+    def test_tracking_number_closing_session(self):
+        self.main_pos_config.with_user(self.pos_user).open_ui()
+        self.start_tour(f"/pos/ui?config_id={self.main_pos_config.id}", 'test_tracking_number_closing_session', login="pos_user")
+        for order in self.env['pos.order'].search([]):
+            self.assertEqual(int(order.tracking_number) % 100, 1)
+
 
 # This class just runs the same tests as above but with mobile emulation
 class MobileTestUi(TestUi):
