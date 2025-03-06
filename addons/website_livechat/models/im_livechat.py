8a8fea3048e59c3195170519368c79f29deb7543
ramh <ramh@odoo.com>
2025-03-06 15:47:11 +0530

    [FIX] website_livechat: remove public statistic page This commit removes the public statistic page for Website Live Chat, which previously displayed the list of public live chat channels and their ratings.
    Task-4619149

--- addons/website_livechat/models/im_livechat.py
+++ addons/website_livechat/models/im_livechat.py
@@ -10,14 +10,6 @@ class Im_LivechatChannel(models.Model):
 
     _inherit = ['im_livechat.channel', 'website.published.mixin']
 
-    def _compute_website_url(self):
-        super()._compute_website_url()
-        for channel in self:
-            if channel.id:
-                channel.website_url = "/livechat/channel/%s" % (self.env['ir.http']._slug(channel),)
-            else:
-                channel.website_url = channel._origin.website_url
-
     website_description = fields.Html(
         "Website description", default=False, translate=html_translate,
         sanitize_overridable=True,
