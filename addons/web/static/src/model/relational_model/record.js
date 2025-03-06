1652f479ad7a1a18ba108ef8a7f1f762787ea744
Jorge Pinna Puissant <jpp@odoo.com>
2025-03-06 13:10:00 +0000

    [FIX] web: save a dirty record without changes
    - Open a record (e.g., a project task);
    - Add a tag to a `many2many_tags` field;
    - Remove the previously added tag;
    - The record should be dirty (the save icon should be visible);
    - Click on the save icon/or click to pager next.
    
    Before this commit, the UI does nothing. It doesn't save, or it doesn't go to
    the next record if you click on the pager next. This is because even if the
    record is considered dirty, because some changes have been made (adding and
    removing tags), there are no changes. And a condition prevents to call the
    `web_save` RPC from being called if there are no changes. If the `web_save`
    RPC is not called, we do nothing.
    
    In this commit, we change this condition so that if the `web_save` RPC is not
    called, we mark the record as not dirty. When we move to another record (pager
    next), we do not need to mark the current record as not dirty, but the next
    record is loaded.
    
    X-original-commit: 43fb8a45ac629c1883ca9521a8879cf19ca7538b

--- addons/web/static/src/model/relational_model/record.js
+++ addons/web/static/src/model/relational_model/record.js
@@ -1001,6 +1001,12 @@ export class Record extends DataPoint {
         const changes = this._getChanges();
         delete changes.id; // id never changes, and should not be written
         if (!creation && !Object.keys(changes).length) {
+            if (nextId) {
+                return this.model.load({ resId: nextId });
+            }
+            this._changes = markRaw({});
+            this.data = { ...this._values };
+            this.dirty = false;
             return true;
         }
         if (
