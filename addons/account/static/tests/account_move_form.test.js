a1b37f75e35efe086b849ac4d703e714fa4e2423
luvi <luvi@odoo.com>
2025-03-06 08:58:42 +0100

    [FIX] web, account: better handle records deletion from selection
    This commit fixes the wrong implementation, breaking extensions of the
    deletion feature from controllers. Now, a hook has been created instead
    of implementing the feature in the DynamicList itself. Controllers can use
    this hook easily, while having extension points like the deleteConfirmationDialogProps
    getter.
    
    A test has been added in the 'account' module for account_move deletion dialog
    when a warning must be added before the main confirmation message.
    
    closes odoo/odoo#201122
    
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/account/static/tests/account_move_form.test.js
+++ addons/account/static/tests/account_move_form.test.js
@@ -6,8 +6,8 @@ import {
     startServer,
     triggerHotkey
 } from "@mail/../tests/mail_test_helpers";
-import { test } from "@odoo/hoot";
-import { asyncStep, onRpc, waitForSteps } from "@web/../tests/web_test_helpers";
+import { expect, test } from "@odoo/hoot";
+import { asyncStep, contains, onRpc, waitForSteps } from "@web/../tests/web_test_helpers";
 import { defineAccountModels } from "./account_test_helpers";
 
 defineAccountModels();
@@ -35,4 +35,30 @@ test("When I switch tabs, it saves", async () => {
     triggerHotkey("Enter");
     await click('a[name="aml_tab"]');
     await waitForSteps(["tab saved"]);
+});
+
+test("Confirmation dialog on delete contains a warning", async () => {
+    const pyEnv = await startServer();
+    const accountMove = pyEnv["account.move"].create({ name: "move0" });
+    await start();
+    onRpc("account.move", "check_move_sequence_chain", () => {
+        return false;
+    });
+    await openFormView("account.move", accountMove, {
+        arch: `<form js_class='account_move_form'>
+            <sheet>
+                <notebook>
+                    <page id="invoice_tab" name="invoice_tab" string="Invoice Lines">
+                        <field name="name"/>
+                    </page>
+                    <page id="aml_tab" string="Journal Items" name="aml_tab"></page>
+                </notebook>
+            </sheet>
+        </form>`,
+    });
+    await contains(".o_cp_action_menus button").click();
+    await contains(".o_menu_item:contains(Delete)").click();
+    expect(".o_dialog div.text-danger").toHaveText("This operation will create a gap in the sequence.", {
+        message: "warning message has been added in the dialog"
+    });
 });
