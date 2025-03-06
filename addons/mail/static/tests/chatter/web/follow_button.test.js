7a61e4ed5faa2a2d5c7d523ae7ca89ac82505a2c
Didier (did) <did@odoo.com>
2025-03-06 12:01:40 +0100

    [IMP] mail: merge follower list and the follow/unfollow buttons
    Task-4614543
    
    closes odoo/odoo#200382
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/tests/chatter/web/follow_button.test.js
+++ addons/mail/static/tests/chatter/web/follow_button.test.js
@@ -5,74 +5,26 @@ import {
     openFormView,
     start,
     startServer,
-    triggerEvents,
 } from "@mail/../tests/mail_test_helpers";
 import { describe, test } from "@odoo/hoot";
-import { serverState } from "@web/../tests/web_test_helpers";
 
 describe.current.tags("desktop");
 defineMailModels();
 
-test("base rendering not editable", async () => {
-    await start();
-    await openFormView("res.partner", serverState.partnerId);
-    await contains(".o-mail-Chatter-follow", { text: "Follow" });
-});
-
-test("hover following button", async () => {
+test("base rendering follow and unfollow button", async () => {
     const pyEnv = await startServer();
     const threadId = pyEnv["res.partner"].create({});
-    pyEnv["mail.followers"].create({
-        is_active: true,
-        partner_id: serverState.partnerId,
-        res_id: threadId,
-        res_model: "res.partner",
-    });
     await start();
     await openFormView("res.partner", threadId);
-    await contains(".o-mail-Chatter-follow", { text: "Following" });
-    // ensure no re-rendering after mouseenter because messages can be loaded before or after data
-    await contains(".o-mail-Thread", { text: "The conversation is empty." });
-    await triggerEvents(".o-mail-Chatter-follow", ["mouseenter"], { text: "Following" });
-    await contains(".o-mail-Chatter-follow", { text: "Unfollow" });
-});
-
-test('click on "follow" button', async () => {
-    await start();
-    await openFormView("res.partner", serverState.partnerId);
-    await contains("button", { text: "Follow" });
-    await click("button", { text: "Follow" });
-    await contains("button", { text: "Following" });
-});
-
-test('Click on "follow" button should save draft record', async () => {
-    await start();
-    await openFormView("res.partner", undefined, {
-        arch: `
-            <form string="Partners">
-                <sheet>
-                    <field name="name" required="1"/>
-                </sheet>
-                <chatter/>
-            </form>`,
-    });
-    await contains("button", { text: "Follow" });
-    await contains("div.o_field_char");
-    await click("button", { text: "Follow" });
-    await contains("div.o_field_invalid");
-});
-
-test('click on "unfollow" button', async () => {
-    const pyEnv = await startServer();
-    const threadId = pyEnv["res.partner"].create({});
-    pyEnv["mail.followers"].create({
-        is_active: true,
-        partner_id: serverState.partnerId,
-        res_id: threadId,
-        res_model: "res.partner",
-    });
-    await start();
-    await openFormView("res.partner", threadId);
-    await click(".o-mail-Chatter-follow", { text: "Following" });
-    await contains("button", { text: "Follow" });
+    await contains(".o-mail-Followers-counter", { text: "0" });
+    await contains("[title='Show Followers'] .fa-user-o");
+    await click("[title='Show Followers']");
+    await click(".o-dropdown-item", { text: "Follow" });
+    await contains(".o-mail-Followers-counter", { text: "1" });
+    await contains("[title='Show Followers'] .fa-user");
+    await click("[title='Show Followers']");
+    await contains("[title='Edit subscription']");
+    await click(".o-dropdown-item", { text: "Unfollow" });
+    await contains(".o-mail-Followers-counter", { text: "0" });
+    await contains("[title='Show Followers'] .fa-user-o");
 });
