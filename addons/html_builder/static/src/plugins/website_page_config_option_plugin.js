40577e406dfcbac937fd6b394dd00197f7541cb0
Augustin (duau) <duau@odoo.com>
2025-03-06 13:49:09 +0100

    TopMenuVisibility and TopMenuColor and HideFooter
    This uses a custom history mutation system, because modification of wrapwrap node are outside of the editable scope and not detected.
    This uses a custom save system, like the old option.
    TopMenuVisibility is the only header option modifying the current page header and not every header. It is not usable on every page (like a product page), doing so by checking the input node header_visible to know if the header is customisable.

---
+++ addons/html_builder/static/src/plugins/website_page_config_option_plugin.js
@@ -0,0 +1,138 @@
+import { Plugin } from "@html_editor/plugin";
+import { registry } from "@web/core/registry";
+import { rgbToHex } from "@web/core/utils/colors";
+
+class WebsitePageConfigOptionPlugin extends Plugin {
+    static id = "websitePageConfigOptionPlugin";
+    static dependencies = ["history"];
+    resources = {
+        builder_actions: this.getActions(),
+        builder_options: [
+            {
+                template: "html_builder.TopMenuVisibilityOption",
+                selector:
+                    "[data-main-object]:has(input.o_page_option_data[name='header_visible']) #wrapwrap > header",
+            },
+            {
+                template: "html_builder.HideFooterOption",
+                selector:
+                    "[data-main-object]:has(input.o_page_option_data[name='footer_visible']) #wrapwrap > footer",
+            },
+        ],
+        save_handlers: this.onSave.bind(this),
+    };
+
+    getActions() {
+        return {
+            setWebsiteHeaderVisibilty: {
+                apply: ({ editingElement, value: headerPositionValue }) => {
+                    const lastValue = this.getVisibilityItem();
+                    this.dependencies.history.applyCustomMutation({
+                        apply: () => this.visibilityHandlers[headerPositionValue](),
+                        revert: () => this.visibilityHandlers[lastValue](),
+                    });
+
+                    this.isDirty = true;
+                },
+                isApplied: ({ editingElement, value }) => this.getVisibilityItem() === value,
+            },
+            setPageWebsiteDirty: {
+                apply: ({ editingElement }) => {
+                    this.isDirty = true;
+                },
+            },
+        };
+    }
+
+    getVisibilityItem() {
+        const isHidden = this.document
+            .querySelector("#wrapwrap > header")
+            .classList.contains("o_snippet_invisible");
+        const isOverlay = this.document
+            .getElementById("wrapwrap")
+            .classList.contains("o_header_overlay");
+        return isOverlay ? "overTheContent" : isHidden ? "hidden" : "regular";
+    }
+
+    getFooterVisibility() {
+        return this.document
+            .querySelector("#wrapwrap > footer")
+            .classList.contains("o_snippet_invisible");
+    }
+
+    getColorValue(attribute, classPrefix) {
+        const headerEl = this.document.querySelector("#wrapwrap > header");
+        const matchingClass = [...headerEl.classList].find((cls) => cls.startsWith(classPrefix));
+        return matchingClass || rgbToHex(headerEl.style.getPropertyValue(attribute));
+    }
+
+    onSave() {
+        if (!this.isDirty) {
+            return;
+        }
+        const item = this.getVisibilityItem();
+        const mainObject = this.services.website.currentWebsite.metadata.mainObject;
+        return Promise.all([
+            this.services.orm.write(mainObject.model, [mainObject.id], {
+                header_overlay: item === "overTheContent",
+                header_visible: item !== "hidden",
+                header_color: this.getColorValue("background-color", "bg-o-color-"),
+                header_text_color: this.getColorValue("color", "text-o-color-"),
+                footer_visible: !this.getFooterVisibility(),
+            }),
+        ]);
+    }
+
+    visibilityHandlers = {
+        overTheContent: () => {
+            this.setHeaderOverlay(true);
+            this.setHeaderVisible(false);
+        },
+        regular: () => {
+            this.setHeaderOverlay(false);
+            this.setHeaderVisible(false);
+            this.resetHeaderColor();
+            this.resetTextColor();
+        },
+        hidden: () => {
+            this.setHeaderOverlay(false);
+            this.setHeaderVisible(true);
+            this.resetHeaderColor();
+            this.resetTextColor();
+        },
+    };
+
+    setHeaderOverlay(shouldApply) {
+        this.document.getElementById("wrapwrap").classList.toggle("o_header_overlay", shouldApply);
+    }
+
+    setHeaderVisible(shouldApply) {
+        const headerEl = this.document.querySelector("#wrapwrap > header");
+        headerEl.classList.toggle("d-none", shouldApply);
+        headerEl.classList.toggle("o_snippet_invisible", shouldApply);
+    }
+
+    resetHeaderColor() {
+        const headerEl = this.document.querySelector("#wrapwrap > header");
+        headerEl.style.removeProperty("background-color");
+        headerEl.classList.forEach((cls) => {
+            if (cls.startsWith("bg-o-color-")) {
+                headerEl.classList.remove(cls);
+            }
+        });
+    }
+
+    resetTextColor() {
+        const headerEl = this.document.querySelector("#wrapwrap > header");
+        headerEl.style.removeProperty("color");
+        headerEl.classList.forEach((cls) => {
+            if (cls.startsWith("text-o-color-")) {
+                headerEl.classList.remove(cls);
+            }
+        });
+    }
+}
+
+registry
+    .category("website-plugins")
+    .add(WebsitePageConfigOptionPlugin.id, WebsitePageConfigOptionPlugin);
---
+++ addons/html_builder/static/tests/options/top_menu_visibility_option.test.js
@@ -0,0 +1,116 @@
+import { expect, test } from "@odoo/hoot";
+import { contains } from "@web/../tests/web_test_helpers";
+import { defineWebsiteModels, setupWebsiteBuilder } from "../website_helpers";
+import { undo, redo } from "@html_editor/../tests/_helpers/user_actions";
+import { queryOne } from "@odoo/hoot-dom";
+
+defineWebsiteModels();
+
+test("TopMenuVisibility option should appear", async () => {
+    await setupWebsiteBuilder("", {
+        openEditor: true,
+        beforeWrapwrapContent: `<input type="hidden" class="o_page_option_data" autocomplete="off" data-oe-model="ir.ui.view" data-oe-id="543" data-oe-field="arch" data-oe-xpath="/data/xpath[13]/t[1]/t[1]/input[1]" name="header_visible" value="True">`,
+        headerContent: `
+            <header id="top" data-anchor="true" data-name="Header">   
+                    Menu Content
+            </header>`,
+    });
+    await contains(":iframe #wrapwrap > header").click();
+    expect("[data-label='Header Position']").toBeVisible();
+});
+
+test("page is not customisable, TopMenuVisibility option should not appear", async () => {
+    await setupWebsiteBuilder("", {
+        openEditor: true,
+        headerContent: `
+            <header id="top" data-anchor="true" data-name="Header">   
+                    Menu Content
+            </header>`,
+    });
+    await contains(":iframe #wrapwrap > header").click();
+    expect("[data-label='Header Position']").not.toBeVisible();
+});
+
+test("undo overTheContent visibility", async () => {
+    const { getEditor } = await setupWebsiteBuilder("", {
+        openEditor: true,
+        beforeWrapwrapContent: `<input type="hidden" class="o_page_option_data" autocomplete="off" data-oe-model="ir.ui.view" data-oe-id="543" data-oe-field="arch" data-oe-xpath="/data/xpath[13]/t[1]/t[1]/input[1]" name="header_visible" value="True">`,
+        headerContent: `
+            <header id="top" data-anchor="true" data-name="Header" class="">   
+                    Menu Content
+            </header>`,
+    });
+    const editor = getEditor();
+    await contains(":iframe #wrapwrap > header").click();
+    const precedentWrapwrap = queryOne(":iframe #wrapwrap").outerHTML;
+    await contains("[data-label='Header Position'] .dropdown").click();
+    await contains("[data-action-value='overTheContent']").click();
+    undo(editor);
+    redo(editor);
+    undo(editor);
+    const modifiedWrapwrap = queryOne(":iframe #wrapwrap");
+    expect(modifiedWrapwrap).toHaveOuterHTML(precedentWrapwrap);
+});
+
+test("undo and comeback to a custom overTheContent color", async () => {
+    const { getEditor } = await setupWebsiteBuilder("", {
+        openEditor: true,
+        beforeWrapwrapContent: `<input type="hidden" class="o_page_option_data" autocomplete="off" data-oe-model="ir.ui.view" data-oe-id="543" data-oe-field="arch" data-oe-xpath="/data/xpath[13]/t[1]/t[1]/input[1]" name="header_visible" value="True">`,
+        headerContent: `
+            <header id="top" data-anchor="true" data-name="Header" class="">   
+                    Menu Content
+            </header>`,
+    });
+    const editor = getEditor();
+    await contains(":iframe #wrapwrap > header").click();
+    await contains("[data-label='Header Position'] .dropdown").click();
+    await contains("[data-action-value='overTheContent']").click();
+    await contains("[data-label='Background'] .o_we_color_preview").click();
+    await contains("[data-color='#0000FF']").click();
+    const precedentWrapwrap = queryOne(":iframe #wrapwrap").outerHTML;
+    await contains("[data-label='Header Position'] .dropdown").click();
+    await contains("[data-action-value='regular']").click();
+    undo(editor);
+    redo(editor);
+    undo(editor);
+    const modifiedWrapwrap = queryOne(":iframe #wrapwrap");
+    expect(modifiedWrapwrap).toHaveOuterHTML(precedentWrapwrap);
+});
+
+test("undo hidden and come back to regular", async () => {
+    const { getEditor } = await setupWebsiteBuilder("", {
+        openEditor: true,
+        beforeWrapwrapContent: `<input type="hidden" class="o_page_option_data" autocomplete="off" data-oe-model="ir.ui.view" data-oe-id="543" data-oe-field="arch" data-oe-xpath="/data/xpath[13]/t[1]/t[1]/input[1]" name="header_visible" value="True">`,
+        headerContent: `
+            <header id="top" data-anchor="true" data-name="Header" class="">   
+                    Menu Content
+            </header>`,
+    });
+    const editor = getEditor();
+    await contains(":iframe #wrapwrap > header").click();
+    const precedentWrapwrap = queryOne(":iframe #wrapwrap").outerHTML;
+    await contains("[data-label='Header Position'] .dropdown").click();
+    await contains("[data-action-value='hidden']").click();
+    undo(editor);
+    const modifiedWrapwrap = queryOne(":iframe #wrapwrap");
+    expect(modifiedWrapwrap).toHaveOuterHTML(precedentWrapwrap);
+});
+
+test("regular -> hidden -> regular", async () => {
+    await setupWebsiteBuilder("", {
+        openEditor: true,
+        beforeWrapwrapContent: `<input type="hidden" class="o_page_option_data" autocomplete="off" data-oe-model="ir.ui.view" data-oe-id="543" data-oe-field="arch" data-oe-xpath="/data/xpath[13]/t[1]/t[1]/input[1]" name="header_visible" value="True">`,
+        headerContent: `
+            <header id="top" data-anchor="true" data-name="Header" class="">   
+                    Menu Content
+            </header>`,
+    });
+    await contains(":iframe #wrapwrap > header").click();
+    const precedentWrapwrap = queryOne(":iframe #wrapwrap").outerHTML;
+    await contains("[data-label='Header Position'] .dropdown").click();
+    await contains("[data-action-value='hidden']").click();
+    await contains("[data-label='Header Position'] .dropdown").click();
+    await contains("[data-action-value='regular']").click();
+    const modifiedWrapwrap = queryOne(":iframe #wrapwrap");
+    expect(modifiedWrapwrap).toHaveOuterHTML(precedentWrapwrap);
+});
