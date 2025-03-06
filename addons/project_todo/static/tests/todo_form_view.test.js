016441eab03574cf09eccfafc0b0cc9088df390c
K Theja (kthe) <kthe@odoo.com>
2025-03-06 09:52:17 +0000

    [FIX] project_todo: fix opening of chatter in todo opened from activity view
    Steps to reproduce:
    
    - Open To-Do app
    - Open a todo and click on chatter
    - Go back and open another todo
    - The chatter is closed by default
    
    Issue:
    
    - The chatter should be opened if opened earlier
    - If todo opened from activity view chatter is not opened
    
    Reason:
    
    - No logic to preserve the state if chatter.
    
    Solution:
    
    - Using local storage to store the state of the chatter and thus apply changes
    accordingly.
    
    task-4592501
    
    closes odoo/odoo#202760
    
    X-original-commit: 666f1d182bb7ccc6e6ea9e90c5012c066148d163
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/project_todo/static/tests/todo_form_view.test.js
+++ addons/project_todo/static/tests/todo_form_view.test.js
@@ -1,9 +1,18 @@
 import { expect, test, beforeEach } from "@odoo/hoot";
-import { queryAllTexts } from "@odoo/hoot-dom";
+import { queryAllTexts, click } from "@odoo/hoot-dom";
 import { animationFrame } from "@odoo/hoot-mock";
 
-import { mountView, onRpc, mountWithCleanup, getService, contains } from "@web/../tests/web_test_helpers";
+import {
+    mountView,
+    onRpc,
+    mountWithCleanup,
+    getService,
+    contains,
+    MockServer,
+    fields,
+} from "@web/../tests/web_test_helpers";
 import { WebClient } from "@web/webclient/webclient";
+import { browser } from "@web/core/browser/browser";
 
 import { defineTodoModels } from "./todo_test_helpers";
 import { ProjectTask } from "./mock_server/mock_models/project_task";
@@ -23,9 +32,27 @@ beforeEach(() => {
                 <field name="priority" invisible="1"/>
                 <field name="date_deadline" widget="remaining_days"/>
             </form>`,
+        "activity, false": `
+            <activity string="MailTestActivity">
+                <field name="name" invisible="1"/>
+                <templates>
+                    <div t-name="activity-box">
+                        <field name="name"/>
+                    </div>
+                </templates>
+            </activity>`,
         search: `
             <search/>`,
     };
+
+    ProjectTask._fields.activity_state = fields.Selection({
+        string: "State",
+        selection: [
+            ["overdue", "Overdue"],
+            ["today", "Today"],
+            ["planned", "Planned"],
+        ],
+    });
 });
 
 test("Check that project_task_action_convert_todo_to_task appears in the menu actions if the user does belong to the group_project_user group", async () => {
@@ -84,4 +111,76 @@ test("Check that todo_form view contains the TodoDoneCheckmark and remaining_day
     expect(".o_field_remaining_days").toHaveCount(1, {
         message: "The todo form view should have deadline field (o_field_remaining_days)",
     });
+});
+test.tags("desktop");
+test("Check if opening form view from activity view does open with chatter visble", async() => {
+    // Basic/Minimum data needed for activity view to be displayed
+    onRpc("web_search_read", (args) => {
+        return {
+            length: 1,
+            records: [
+                {
+                    id: 1,
+                    name: "Todo"
+                }
+            ],
+        };
+    });
+    onRpc("get_activity_data", (args) => {
+        const currentEnv = MockServer.current.env;
+        return {
+            activity_res_ids: [1],
+            grouped_activities: {},
+            activity_types: currentEnv["mail.activity.type"].map((type) => {
+                const templates = (type.mail_template_ids || []).map((template_id) => {
+                    const { id, name } = currentEnv["mail.template"].browse(template_id)[0];
+                    return { id, name };
+                });
+                return {
+                    id: type.id,
+                    name: type.display_name,
+                    template_ids: templates,
+                    keep_done: type.keep_done,
+                };
+            }),
+        }
+    });
+
+    await mountWithCleanup(WebClient);
+    await getService("action").doAction({
+        res_model: "project.task",
+        type: "ir.actions.act_window",
+        views: [
+            [false, "activity"],
+            [false, "form"],
+        ],
+    });
+    expect(".o_activity_record").toHaveCount(1);
+    click(".o_activity_record");
+    // First animationFrame for rendering form view
+    await animationFrame();
+    // Second animationFrame for re-rendering as chatter is toggled by change in state
+    await animationFrame();
+    expect("a.todo_toggle_chatter.active").toHaveCount(1);
+    expect(browser.localStorage.getItem("isChatterOpened")).toBe(null);
+});
+
+test.tags("desktop");
+test("check local stored value on click of chatter toggle icon", async () => {
+    await mountView({
+        resModel: "project.task",
+        resId: 1,
+        type: "form",
+    });
+
+    expect("a.todo_toggle_chatter.active").toHaveCount(0);
+    expect(browser.localStorage.getItem("isChatterOpened")).toBe(null);
+    click("a.todo_toggle_chatter");
+    await animationFrame();
+    expect("a.todo_toggle_chatter.active").toHaveCount(1);
+    expect(browser.localStorage.getItem("isChatterOpened")).toBe("true");
+    click("a.todo_toggle_chatter");
+    await animationFrame();
+    expect("a.todo_toggle_chatter.active").toHaveCount(0);
+    expect(browser.localStorage.getItem("isChatterOpened")).toBe("false");
 });
