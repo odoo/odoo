3d5b436f86d9b8a8b2d622f23ae44a906ce3ef5c
Paolo Gatti <pgi@odoo.com>
2025-03-06 13:01:58 +0100

    [IMP] cli: Subcommands
    Introducing Subcommands, SubcommandMixin.
    
    Allow for modules to have Command extensions,
    by having a `<addon>/cli/<command>.py` file.
    These will be loaded together with `odoo/cli/<command>.py`,
    allowing for command extension (adding subcommands for example).

--- odoo/cli/command.py
+++ odoo/cli/command.py
@@ -1,20 +1,30 @@
 import argparse
 import contextlib
 import re
+import logging
 import sys
 from inspect import cleandoc
+from contextlib import closing, contextmanager
 from pathlib import Path
 
 import odoo.init  # import first for core setup
 import odoo.cli
 from odoo.modules import initialize_sys_path, load_script
 from odoo.tools import config
+from odoo.modules.registry import Registry
+
+
+__all__ = [
+    'Command',
+    'Subcommand',
+    'SubcommandsMixin',
+    'main'
+]
 
 
 COMMAND_NAME_RE = re.compile(r'^[a-z][a-z0-9_]*$', re.I)
 PROG_NAME = Path(sys.argv[0]).name
 commands = {}
-"""All loaded commands"""
 
 
 class Command:
@@ -54,6 +64,59 @@ class Command:
     @classmethod
     def is_valid_name(cls, name):
         return re.match(COMMAND_NAME_RE, name)
+
+    @contextmanager
+    def build_env(self, db_name, update_module=False, min_log_level=None):
+        if min_log_level:
+            logging.disable(min_log_level)
+        registry = Registry.new(db_name, update_module=update_module)
+        with closing(registry.cursor()) as cr:
+            yield odoo.api.Environment(cr, odoo.SUPERUSER_ID, {})
+        if min_log_level:
+            logging.disable(logging.NOTSET)
+
+    @staticmethod
+    def die(message=None):
+        if message:
+            sys.exit(message)
+        sys.exit(0)
+
+
+class SubcommandsMixin():
+    """ Command that has subcommands """
+    subcommands = None
+
+    def run(self, cmdargs):
+        """ Instead of running the command, we run the subcommands """
+        self._config = odoo.tools.config.parse_config(None, setup_logging=True)
+        self.subparsers = self.parser.add_subparsers(dest='subcommand', help='Subcommands help')
+        assert(isinstance(self.subcommands, list | tuple))
+        initialized_subcommands = {sc.name: sc for Cls in self.subcommands if (sc := Cls(self))}
+        parsed_args, _unknown = self.parser.parse_known_args(args=cmdargs)
+        if subcommand := initialized_subcommands.get(parsed_args.subcommand):
+            subcommand.run(cmdargs[1:])
+        else:
+            self.parser.print_help()
+            Command.die()
+
+
+class Subcommand:
+    description = None
+
+    @property
+    def name(self):
+        parent_name = self.parent_command.name
+        classname = self.__class__.__name__
+        return classname.lower().replace(parent_name.lower(), "")
+
+    def __init__(self, parent_command):
+        if not getattr(self, 'description', None):
+            logging.warning('No description found for class %s', self.__class__.__name__)
+        self.parent_command = parent_command
+        description = self.description or self.name
+        self.subparsers = self.parent_command.subparsers
+        self.parser = self.subparsers.add_parser(self.name, help=description)
+        self.build_env = self.parent_command.build_env
 
 
 def load_internal_commands():
@@ -135,4 +198,4 @@ def main():
             f"Unknown command {command_name!r}.\n"
             f"Use '{PROG_NAME} --help' to see the list of available commands."
         )
-        sys.exit(message)
+        Command.die(message)
