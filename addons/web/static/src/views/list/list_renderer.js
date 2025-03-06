208e09f2aafc8fcf51bb19f65b7e0d33839a8eaf
Jorge Pinna Puissant <jpp@odoo.com>
2025-03-06 13:10:00 +0000

    [FIX] web: save a dirty record without changes
    - Open a record (e.g., a project task);
    - Add a tag to a `many2many_tags` field;
    - Remove the previously added tag;
    - The record should be dirty (the save icon should be visible);
    - Click on the save icon/or click to pager next.
    
    Before this commit, the UI does nothing. It doesn't save, or it doesn't go to
    the next record if you click on the pager next. This is because even if the
    record is considered dirty, because some changes have been made (adding and
    removing tags), there are no changes. And a condition prevents to call the
    `web_save` RPC from being called if there are no changes. If the `web_save`
    RPC is not called, we do nothing.
    
    In this commit, we change this condition so that if the `web_save` RPC is not
    called, we mark the record as not dirty. When we move to another record (pager
    next), we do not need to mark the current record as not dirty, but the next
    record is loaded.
    
    X-original-commit: 1b9e9dda82296a6462502691b284e5968b992e8d

--- addons/web/static/src/views/list/list_renderer.js
+++ addons/web/static/src/views/list/list_renderer.js
@@ -27,6 +27,7 @@ import {
     Component,
     onMounted,
     onPatched,
+    status,
     onWillPatch,
     onWillRender,
     useExternalListener,
@@ -215,7 +216,9 @@ export class ListRenderer extends Component {
             // HACK: we need to wait for the next tick to be sure that the Field components are patched.
             // OWL don't wait the patch for the children components if the children trigger a patch by himself.
             await Promise.resolve();
-
+            if (status(this) === "destroyed") {
+                return;
+            }
             if (this.activeElement !== this.uiService.activeElement) {
                 return;
             }
