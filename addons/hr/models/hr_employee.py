14981615c161114f99ca8ed98f248e70312d68b8
Abdelrahman Mahmoud (amah) <amah@odoo.com>
2025-03-06 15:33:24 +0100

    [IMP] hr: enable user bulk creation from employee
    - add server action to enable bulk user creation for employees
    
    Task: 4419316

--- addons/hr/models/hr_employee.py
+++ addons/hr/models/hr_employee.py
@@ -323,6 +323,53 @@ class HrEmployee(models.Model):
             })
         }
 
+    def action_create_users(self):
+        def _get_user_creation_notification_action(message, message_type, next_action):
+            return {
+                    'type': 'ir.actions.client',
+                    'tag': 'display_notification',
+                    'params': {
+                        'title': _("User Creation Notification"),
+                        'type': message_type,
+                        'message': message,
+                        'next': next_action
+                    }
+                }
+
+        old_users = []
+        new_users = []
+        users_without_emails = []
+        for employee in self:
+            if employee.user_id:
+                old_users.append(employee.name)
+                continue
+            if not employee.work_email:
+                users_without_emails.append(employee.name)
+                continue
+            new_users.append({
+                'create_employee_id': employee.id,
+                'name': employee.name,
+                'phone': employee.work_phone,
+                'login': employee.work_email,
+                'partner_id': employee.work_contact_id.id,
+            })
+
+        next_action = {'type': 'ir.actions.act_window_close'}
+        if new_users:
+            self.env['res.users'].create(new_users)
+            message = _('Users %s creation successful', ', '.join([user['name'] for user in new_users]))
+            next_action = _get_user_creation_notification_action(message, 'success', next_action)
+
+        if old_users:
+            message = _('User already exists for Those Employees %s', ', '.join(old_users))
+            next_action = _get_user_creation_notification_action(message, 'warning', next_action)
+
+        if users_without_emails:
+            message = _("You need to set the work email address for %s", ', '.join(users_without_emails))
+            next_action = _get_user_creation_notification_action(message, 'danger', next_action)
+
+        return next_action
+
     def _compute_display_name(self):
         if self.browse().has_access('read'):
             return super()._compute_display_name()
