9d423700172b2bd7661c53cc041f5169e5138c99
Thibault Delavallée <tde@odoo.com>
2025-03-07 11:35:43 +0000

    [FIX] sms: allow to compose on non-thread models
    We can use the SMS composer on non-thread models now that some basic
    methods are available directly on BaseModel.
    
    Some features are obviously not available, like logging a message with
    the SMS content, checking notifications status, ... but we can support
    a basic SMS sending features on all BaseModel.
    
    Task-4113190
    sentry-4681535519
    
    X-original-commit: a901a4ac3a6ebb747d7eded3b778459b7d5d0a1b
    Part-of: odoo/odoo#202802
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/sms/wizard/sms_composer.py
+++ addons/sms/wizard/sms_composer.py
@@ -103,7 +103,7 @@ class SendSMS(models.TransientModel):
                 continue
 
             records = composer._get_records()
-            if records and isinstance(records, self.pool['mail.thread']):
+            if records:
                 res = records._sms_get_recipients_info(force_field=composer.number_field_name, partner_fallback=not composer.comment_single_recipient)
                 composer.recipient_valid_count = len([rid for rid, rvalues in res.items() if rvalues['sanitized']])
                 composer.recipient_invalid_count = len([rid for rid, rvalues in res.items() if not rvalues['sanitized']])
@@ -116,7 +116,7 @@ class SendSMS(models.TransientModel):
     def _compute_recipient_single_stored(self):
         for composer in self:
             records = composer._get_records()
-            if not records or not isinstance(records, self.pool['mail.thread']) or not composer.comment_single_recipient:
+            if not records or not composer.comment_single_recipient:
                 composer.recipient_single_number_itf = ''
                 continue
             records.ensure_one()
@@ -130,7 +130,7 @@ class SendSMS(models.TransientModel):
     def _compute_recipient_single_non_stored(self):
         for composer in self:
             records = composer._get_records()
-            if not records or not isinstance(records, self.pool['mail.thread']) or not composer.comment_single_recipient:
+            if not records or not composer.comment_single_recipient:
                 composer.recipient_single_description = False
                 composer.recipient_single_number = ''
                 continue
@@ -204,7 +204,14 @@ class SendSMS(models.TransientModel):
             return self._action_send_sms_mass(records)
 
     def _action_send_sms_numbers(self):
-        sms_values = [{'body': self.body, 'number': number} for number in self.sanitized_numbers.split(',')]
+        sms_values = [
+            {
+                'body': self.body,
+                'number': number
+            } for number in (
+                self.sanitized_numbers.split(',') if self.sanitized_numbers else [self.recipient_single_number_itf or self.recipient_single_number or '']
+            )
+        ]
         self.env['sms.sms'].sudo().create(sms_values).send()
         return True
 
