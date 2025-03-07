84d1d478994c13ac8a578a063aa90cb127923a56
Anh Thao Pham (pta) <pta@odoo.com>
2025-03-07 16:07:11 +0100

    [FIX] l10n_it_edi_withholding: fix edi import with enasarco lines
    Steps to reproduce:
    - Install Accounting and l10n_it_edi_withholding
    - Switch to an Italian company (e.g. IT Company)
    - Import an electronic Italian invoice (XML) containing an element like:
    
    ```xml
    <DettaglioLinee>
        <NumeroLinea>2</NumeroLinea>
        <Descrizione>Contributo ENASARCO</Descrizione>
        <PrezzoUnitario>0.00</PrezzoUnitario>
        <PrezzoTotale>0.00</PrezzoTotale>
        <AliquotaIVA>22.00</AliquotaIVA>
        <AltriDatiGestionali>
            <TipoDato>CASSA-PREV</TipoDato>
            <RiferimentoTesto>TC07 - ENASARCO</RiferimentoTesto>
            <RiferimentoNumero>10.03</RiferimentoNumero>
        </AltriDatiGestionali>
    </DettaglioLinee>
    ```
    
    Issue:
    1) When the above ENASARCO element is present and its unit price is 0,
    we consider that the ENASARCO tax should be applied to each line.
    We try to retrieve the percentage of the ENASARCO tax used by computing
    it from the amount from <RiferimentoNumero> element and the untaxed
    amount of the invoice.
    Then the ENASARCO tax is applied on all the current move lines.
    However, it is possible that all the move lines have not been imported
    yet and the tax will not be applied on all the lines.
    
    2) If a ENASARCO tax has been set on a particular move line with a unit
    price of 0, the tax is applied to all the move lines, but it shouldn't.
    
    3) When a global ENASARCO element is found in a move line, this move
    line is deleted, which can cause an error if the ENASARCO tax cannot be
    found in the database because a message is logged with the move line name.
    
    Solution:
    1-2) In "_l10n_it_edi_import_line" method, we had the ENASARCO tax to the
    current line by computing the tax rate with the unit price of the line
    if it is not 0.
    Then, in "_l10n_it_edi_import_invoice" method, we parse the XML to check
    if there was a unique ENASARCO element with an amount and without unit
    price.
    If it is the case, we considered the ENASARCO tax as global.
    Instead of using the untaxed amount of the invoice to compute the rate
    of the ENASARCO tax used, we should be able to retrieve the real taxable
    amount in a <DatiRiepilogo> element.
    
    There should be elements like:
    
    ```xml
    <DatiRiepilogo>
        <AliquotaIVA>22.00</AliquotaIVA>
        <ImponibileImporto>117.97</ImponibileImporto>
        <Imposta>25.95</Imposta>
        <EsigibilitaIVA>I</EsigibilitaIVA>
    </DatiRiepilogo>
    ```
    
    from which we sum all the "ImponibileImporto" values.
    
    3) Only remove the line if the ENASARCO tax is global and can be found
    in the database.
    
    opw-4555966

--- addons/l10n_it_edi_withholding/models/account_move.py
+++ addons/l10n_it_edi_withholding/models/account_move.py
@@ -5,6 +5,7 @@ import logging
 from collections import namedtuple
 from markupsafe import Markup
 from odoo import _, api, fields, models
+from odoo.addons.l10n_it_edi.models.account_move import get_float
 
 _logger = logging.getLogger(__name__)
 
@@ -220,27 +221,13 @@ class AccountMove(models.Model):
             data_kind, data_text = data_kind_element[0].text.lower(), text_element[0].text.lower()
             if data_kind == 'cassa-prev' and ('enasarco' in data_text or 'tc07' in data_text):
                 number_element = other_data_element.xpath("./RiferimentoNumero")
-                if not number_element:
+                if not number_element or not price_subtotal:
                     continue
-
-                if price_subtotal:
-                    base_amount = price_subtotal
-                    target_lines = move_line_form
-                else:
-                    # In case it's found on a line with a price of 0.0,
-                    # ENASARCO pension fund contribution will be applied
-                    # on the total amount of the invoice instead.
-                    # When we do, then there is no need for the zero-priced line,
-                    # so we can unlink it.
-                    base_amount = move_line_form.move_id.amount_untaxed
-                    target_lines = move_line_form.move_id.invoice_line_ids - move_line_form
-                    move_line_form.unlink()
-
                 enasarco_amount = float(number_element[0].text)
-                enasarco_percentage = -self.env.company.currency_id.round(enasarco_amount / base_amount * 100)
+                enasarco_percentage = -self.env.company.currency_id.round(enasarco_amount / price_subtotal * 100)
                 domain = [('l10n_it_pension_fund_type', '=', 'TC07')] + type_tax_use_domain
                 if enasarco_tax := self._l10n_it_edi_search_tax_for_import(company, enasarco_percentage, domain, vat_only=False):
-                    target_lines.tax_ids |= enasarco_tax
+                    move_line_form.tax_ids |= enasarco_tax
                 else:
                     messages_to_log.append(Markup("%s<br/>%s") % (
                         _("Enasarco tax not found for line with description '%s'", move_line_form.name),
@@ -252,3 +239,47 @@ class AccountMove(models.Model):
                         move_line_form.tax_ids |= pension_fund_tax
 
         return messages_to_log
+
+    def _l10n_it_edi_import_invoice(self, invoice, data, is_new):
+        """ Handle the case where ENASARCO pension fund contribution should be applied on the invoice globally.
+        In this case, there should only be one element with ENASARCO and these conditions should be fulfilled:
+         - AliquotaIVA is defined
+         - PrezzoUnitario == 0.0
+         - a corresponding DatiRiepilogo with the same AliquotaIVA and a ImponibileImporto
+        """
+        res = super()._l10n_it_edi_import_invoice(invoice=invoice, data=data, is_new=is_new)
+        if not res:
+            return
+        self = res
+        tree = data['xml_tree']
+        global_enasarco_lines = []
+        for additional_data_element in tree.xpath('//AltriDatiGestionali'):
+            data_kind = additional_data_element.xpath('./TipoDato')[0].text.lower()
+            if data_kind == 'cassa-prev':
+                data_text = additional_data_element.xpath('./RiferimentoTesto')[0].text.lower()
+                if 'enasarco' in data_text or 'tc07' in data_text:
+                    parent_element = additional_data_element.xpath('..')[0]
+                    price_unit = get_float(parent_element, './PrezzoUnitario')
+                    if price_unit == 0.0:
+                        global_enasarco_lines.append(parent_element)
+
+        if len(global_enasarco_lines) == 1:
+            parent_element = global_enasarco_lines[0]
+            enasarco_amount = get_float(parent_element, './AltriDatiGestionali/RiferimentoNumero')
+            price_unit = get_float(parent_element, './PrezzoUnitario')
+            base_amount = self._get_l10_it_edi_get_taxable_amount_from_summary_data(parent_element.xpath('..')[0])
+            enasarco_percentage = -self.currency_id.round(enasarco_amount / base_amount * 100) if base_amount else 0.0
+            type_tax_use_domain = [('type_tax_use', '=', 'purchase' if self.is_outbound(include_receipts=True) else 'sale')]
+            domain = [('l10n_it_pension_fund_type', '=', 'TC07')] + type_tax_use_domain
+            if enasarco_tax := self._l10n_it_edi_search_tax_for_import(self.company_id, enasarco_percentage, domain, vat_only=False):
+                to_remove_index = int(get_float(parent_element, './NumeroLinea')) - 1
+                self.invoice_line_ids[to_remove_index].unlink()
+                self.invoice_line_ids.tax_ids |= enasarco_tax
+
+        return self
+
+    def _get_l10_it_edi_get_taxable_amount_from_summary_data(self, element):
+        taxable_amount = 0.0
+        for summary_data_element in element.xpath('.//DatiRiepilogo'):
+            taxable_amount += get_float(summary_data_element, './/ImponibileImporto')
+        return taxable_amount
