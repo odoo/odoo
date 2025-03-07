312d583e681c3791f6af10ad75e56d1152ac6550
Thibault Delavallée <tde@odoo.com>
2025-03-07 10:14:00 +0000

    [IMP] test_mail_sms: add / improve tests on non-thread models
    Introduce a non-thread model for SMS tests. Add some tests to check
    what happens when trying to use composer on those models. Use case:
    sending an SMS to a user, which does not inherit from mail.thread
    but makes a valid use case as user profile exists and contain phone
    information.
    
    Task-4113190
    
    X-original-commit: 3aaed83a369be6725a935c4689e18bdc9595655d
    Part-of: odoo/odoo#202792
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mail/tests/common.py
+++ addons/mail/tests/common.py
@@ -1373,6 +1373,7 @@ class MailCommon(common.TransactionCase, MailCase):
                 'email': 'test.admin@test.example.com',
                 "name": "Mitchell Admin",
                 'notification_type': 'inbox',
+                'phone': '0455135790',
             })
         # have root available at hand, just in case
         cls.user_root = cls.env.ref('base.user_root')
