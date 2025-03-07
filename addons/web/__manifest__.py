040aec546d29a511d7f117b676796bd2b1cad285
suju-odoo <suju@odoo.com>
2025-03-07 10:41:25 -0500

    [IMP] base: Highlight invalid locators
        Highlights invalid XML locators (e.g., <xpath>, <field position=""/>)
        within the ir.ui.view form view's code editor. This feature provides
        visual cues to developers, making it easy to identify and fix broken
        locators that often result from database upgrades.
    
        Problem:
        Upgrading Odoo databases frequently leads to broken XML locators in
        views. Identifying these manually is slow and error-prone.
    
        Solution:
        This commit introduces a feature that highlights invalid locators
        in the code editor, allowing developers to quickly pinpoint
        and resolve issues.
    
        Benefits:
        - Increased developer productivity.
        - Reduced time spent on database upgrades.
        - Improved experience for Odoo Partners and Upgrade Projects.
        - Proactive error detection during development.
    
        Co-authored-by: Lucas Perais (lpe) <lpe@odoo>
    
    closes odoo/odoo#194649
    
    Signed-off-by: Rémy Voet (ryv) <ryv@odoo.com>

--- addons/web/__manifest__.py
+++ addons/web/__manifest__.py
@@ -22,6 +22,7 @@ This module provides the core of the Odoo Web Client.
         'views/speedscope_template.xml',
         'views/speedscope_config_wizard.xml',
         'views/neutralize_views.xml',
+        'views/ir_ui_view_views.xml',
         'data/ir_attachment.xml',
         'data/report_layout.xml',
     ],
