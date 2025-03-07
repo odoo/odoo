1ab6233b86346fb89d7ce9e46a7473366e08a87f
Julien (jula) <jula@odoo.com>
2025-03-07 10:22:53 +0100

    [FIX] point_of_sale: fix crash with problematic chars in htmlToCanvas
    __Current behavior before commit:__
    Sometimes when a Worldline terminal makes a payment with Edenred or
    Sodexo, it creates a ticket with special ascii characters like `0x0E`
    that are not supposed to be printed. Some of those characters make the
    [`createImage`][1] method throw an error with the `onerror` Event.
    
    __Description of the fix:__
    Added the `sanitizeNodeText` function to remove all problematic ascii
    characters that should not be printed anyway.
    Added a test to make sure `htmlToCanvas` can handle all ascii characters
    without crashing.
    
    opw-4322339
    
    [1]:https://github.com/odoo/odoo/blob/a98e802976f6798f3aea07231a366fbcd8002ce2/addons/point_of_sale/static/src/app/utils/html-to-image.js#L217
    
    X-original-commit: aa7aac5

--- addons/point_of_sale/static/src/app/printer/render_service.js
+++ addons/point_of_sale/static/src/app/printer/render_service.js
@@ -91,6 +91,16 @@ const applyWhenMounted = async ({ el, container, callback }) => {
     return res;
 };
 
+const sanitizeNodeText = (element) => {
+    if (element.nodeType === Node.TEXT_NODE) {
+        element.textContent = element.textContent.replace(/[\x00-\x08\x0B\x0C\x0E-\x1F]/g, ""); // eslint-disable-line no-control-regex
+        return;
+    }
+    for (const child of element.childNodes) {
+        sanitizeNodeText(child);
+    }
+};
+
 /**
  * This function assumes that the `renderer` service is available.
  */
@@ -101,6 +111,7 @@ export const htmlToCanvas = async (el, options) => {
     if (options.addEmailMargins === true) {
         $(".pos-receipt-print").css({ padding: "15px", "padding-bottom": "30px" });
     }
+    sanitizeNodeText(el);
     return await applyWhenMounted({
         el,
         container: document.querySelector(".render-container"),
