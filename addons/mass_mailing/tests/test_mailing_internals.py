976231caecadccd1ecb50be048958cdad4b6f4c7
alsh-odoo <alsh@odoo.com>
2025-03-10 10:46:02 +0530

    [FIX] mass_mailing: give fallback value when user schedule a mail without body
    This traceback occurs when the user schedules a mail without mail Body in
    email marketing.
    
    To reproduce this issue:
    1) Install "mass_mail"
    2) Create new mailings with 'contact_list_ids' and make sure to give
       the value for preview in the settings page.
    3) Don't select any mail Body templates.
    4) Schedule the record with the previous day.
    5) An Error was encountered in the terminal.
    
    Error:- "TypeError: expected string or bytes-like object"
    
    When the user schedules a mail  corn job runs, in which "action_send_mail"
    triggers.
    In that method, while composing values "body" is used to get values from
    mail.body_html.
    
    https://github.com/odoo/odoo/blob/e37c393d415d686584487f9ad92f062279504cf8/addons/mass_mailing/models/mailing.py#L1043-L1046
    
    Because when the user doesn't select the body template its value will be
    false which leads to the above traceback,
    as the re.search method.
    
    https://github.com/odoo/odoo/blob/e37c393d415d686584487f9ad92f062279504cf8/odoo/tools/mail.py#L500
    
    This commit will resolve the issue by giving a fallback value of an empty string
    when the user doesn't select the body template.
    
    sentry-5389317834
    
    closes odoo/odoo#166962
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mass_mailing/tests/test_mailing_internals.py
+++ addons/mass_mailing/tests/test_mailing_internals.py
@@ -479,6 +479,25 @@ class TestMassMailValues(MassMailCommon):
         mail_thread_attachments = mailing._get_mail_thread_data_attachments()
         self.assertSetEqual(set(mail_thread_attachments.ids), {png_duplicate_of_svg_attachment.id, original_png_attachment.id})
 
+    @users('user_marketing')
+    @mute_logger('odoo.addons.mail.models.mail_mail')
+    def test_process_mailing_queue_without_html_body(self):
+        """ Test mailing with past schedule date and without any html body """
+        mailing = self.env['mailing.mailing'].create({
+                'name': 'mailing',
+                'subject': 'some subject',
+                'mailing_model_id': self.env['ir.model']._get('res.partner').id,
+                'preview': "Check it out before its too late",
+                'body_html': False,
+                'schedule_date': datetime(2023, 2, 17, 11, 0),
+            })
+        mailing.action_put_in_queue()
+        with self.mock_mail_gateway(mail_unlink_sent=False):
+            mailing._process_mass_mailing_queue()
+
+        self.assertFalse(mailing.body_html)
+        self.assertEqual(mailing.mailing_model_name, 'res.partner')
+
 
 @tagged("mass_mailing", "utm")
 class TestMassMailUTM(MassMailCommon):
