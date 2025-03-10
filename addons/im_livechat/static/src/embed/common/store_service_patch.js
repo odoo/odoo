5923ce4e5d57ce368ca919081f5e8cc61df4c5af
tsm-odoo <tsm@odoo.com>
2025-03-10 10:23:11 +0100

    [FIX] im_livechat: subscribe to guest channel after live chat create
    Since [1], bus subscription to the guest channel is only made explicitly
    in the CORS bundle. However, race conditions can occur leading to the
    subscription not being made. This PR ensures we always subcribe to the
    guest channel.
    
    [1]: https://github.com/odoo/odoo/pull/199779
    
    closes odoo/odoo#200914
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/im_livechat/static/src/embed/common/store_service_patch.js
+++ addons/im_livechat/static/src/embed/common/store_service_patch.js
@@ -1,12 +1,35 @@
-import { SESSION_STATE } from "@im_livechat/embed/common/livechat_service";
+import { GUEST_TOKEN_STORAGE_KEY, SESSION_STATE } from "@im_livechat/embed/common/livechat_service";
 
 import { Store, storeService } from "@mail/core/common/store_service";
 
 import { patch } from "@web/core/utils/patch";
+import { expirableStorage } from "./expirable_storage";
+import { Record } from "@mail/model/record";
 
 storeService.dependencies.push("im_livechat.initialized");
 
 patch(Store.prototype, {
+    setup() {
+        super.setup(...arguments);
+        expirableStorage.onChange(GUEST_TOKEN_STORAGE_KEY, (value) => (this.guest_token = value));
+        this.guest_token = Record.attr(null, {
+            compute() {
+                return expirableStorage.getItem(GUEST_TOKEN_STORAGE_KEY);
+            },
+            onUpdate() {
+                if (this.guest_token) {
+                    expirableStorage.setItem(GUEST_TOKEN_STORAGE_KEY, this.guest_token);
+                    this.store.env.services.bus_service.addChannel(
+                        `mail.guest_${this.guest_token}`
+                    );
+                    return;
+                }
+                expirableStorage.removeItem(GUEST_TOKEN_STORAGE_KEY);
+                this.store.env.services.bus_service.deleteChannel(`mail.guest_${this.guest_token}`);
+            },
+            eager: true,
+        });
+    },
     async initialize({ force } = {}) {
         const livechatInitialized = this.env.services["im_livechat.initialized"];
         await livechatInitialized.ready;
