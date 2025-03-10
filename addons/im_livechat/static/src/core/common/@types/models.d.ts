b9b564b2d069ac654dfff5b1c4f0d20715939d68
Alexandre Kühn <aku@odoo.com>
2025-03-10 16:34:33 +0100

    [FIX] im_livechat: no duplicate t-key in channel invite
    Follow-up of https://github.com/odoo/odoo/pull/198093
    
    PR above improved channel invitation UI to show livechat
    operator languages and expertise, whereas it was only
    showing the main language.
    
    This PR introduced the following crash by mistake:
    
    ```
    OwlError: Got duplicate key in t-foreach: undefined
    ```
    
    This happens because the keys were defined as `index`,
    which is `undefined` thus an operator with at least 3
    languages or 2 expertise items made it crash.
    
    The intend of `t-key` was to use the index of item in
    the respective list, so the `t-value` variable with
    `_index`, e.g. `language_index`, which this commit fixes.

--- addons/im_livechat/static/src/core/common/@types/models.d.ts
+++ addons/im_livechat/static/src/core/common/@types/models.d.ts
@@ -1,4 +1,8 @@
 declare module "models" {
+    export interface Persona {
+        livechat_languages: String[],
+        livechat_expertise: String[],
+    }
     export interface Thread {
         livechat_operator_id: Persona,
     }
