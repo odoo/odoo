d658fa31a8eecbc872c26ec45af052cdd8a6850f
rajh-odoo <rajh@odoo.com>
2025-03-10 18:28:07 +0530

    [IMP] hr_timesheet: improve timesheet visible for portal when set as a customer
    this commit enhance the visibility of timesheet entries for the portal user.
    Now when a portal user is a customer or follower of a project or task, they can
    see all the timesheet entries related to the particular task or project.
    
    and also after this commit if timesheet user is set as follower or customer on
    the project or task they can able to see the timesheet entries related to them.
    
    Why are we doing this?
    In saas-18.2, when a message was sent, the customer was automatically added as a
    follower, allowing them to view the timesheet entry. However, in saas-18.2, the
    automatic addition of followers has been removed, so now we have to add the
    followers manually. With this commit, we are using the customer's user to ensure
    they can easily see the timesheet entry without needing to add them as follower.
    
    task - 4586667

--- addons/hr_timesheet/tests/test_portal_timesheet.py
+++ addons/hr_timesheet/tests/test_portal_timesheet.py
@@ -68,3 +68,46 @@ class TestPortalTimesheet(TestProjectSharingCommon):
                 self.assertEqual(view_id, form_view_id)
             elif view_type == 'kanban':
                 self.assertEqual(view_id, kanban_view_id)
+
+    def test_timesheet_visibility_portal(self):
+        """
+        Steps:
+        1. Retrieve the domain that determines timesheet visibility for the portal user.
+        2. Create an employee linked to the project user.
+        3. Create a timesheet entry associated with a specific project and task.
+        4. Assign the portal user as the partner on the task.
+        5. Search for timesheets using the retrieved domain.
+        6. Verify that the created timesheet is visible to the portal user.
+        7. Remove the portal user as the partner of the task.
+        8. Search for timesheets again using the same domain.
+        9. Verify that the timesheet is no longer visible to the portal user.
+        10. Assign the portal user as the partner of the project.
+        11. Search for timesheets again using the same domain.
+        12. Verify that the timesheet is now visible to the portal user.
+        """
+        AnalyticLineModel = self.env['account.analytic.line']
+        timesheet_domain = AnalyticLineModel.with_user(self.user_portal)._timesheet_get_portal_domain()
+
+        employee = self.env['hr.employee'].create({
+            'name': 'Project User Employee',
+            'user_id': self.user_projectuser.id,
+        })
+
+        timesheet_entry = AnalyticLineModel.create({
+            'name': 'Timesheet',
+            'project_id': self.project_cows.id,
+            'task_id': self.task_cow.id,
+            'employee_id': employee.id,
+        })
+
+        self.task_cow.write({'partner_id': self.user_portal.partner_id.id})
+        timesheets = AnalyticLineModel.search(timesheet_domain)
+        self.assertIn(timesheet_entry.id, timesheets.ids, "Portal user should see the timesheet when set as the partner on the task.")
+
+        self.task_cow.write({'partner_id': False})
+        timesheets = AnalyticLineModel.search(timesheet_domain)
+        self.assertNotIn(timesheet_entry.id, timesheets.ids, "Portal user should not see the timesheet when not assigned as the task's partner.")
+
+        self.project_cows.write({'partner_id': self.user_portal.partner_id.id})
+        timesheets = AnalyticLineModel.search(timesheet_domain)
+        self.assertIn(timesheet_entry.id, timesheets.ids, "Portal user should see the timesheet when set as the project’s partner.")
