a6c54c4f5bc2e353c3be1166a84f9dbab6ab3612
Levi Siuzdak (sile) <sile@odoo.com>
2025-03-10 09:24:11 +0000

    [FIX] website_sale: handle product description translation in cart
    16.0+
    
    When changing the website language from the cart page, the product description is not translated while all other elements of the page are.
    
    Install at least 2 languages available on website;
    From the shop, add a product (e.g. "Customizable Desk") to cart;
    Move to the cart and change language.
    
    Description is retrieved without being recomputed with selected language.
    
    opw-4485686
    
    closes odoo/odoo#201660
    
    X-original-commit: 002dd487de6931cfd68ab82ad315af406f88784c
    Signed-off-by: Valentin Chevalier <vcr@odoo.com>
    Co-authored-by: Pierre Lamotte (pila) <pila@odoo.com>

--- addons/website_sale/controllers/website.py
+++ addons/website_sale/controllers/website.py
@@ -67,3 +67,12 @@ class Website(main.Website):
             'symbol': request.website.currency_id.symbol,
             'position': request.website.currency_id.position,
         }
+
+    @route()
+    def change_lang(self, lang, **kwargs):
+        order_sudo = request.website.sale_get_order()
+        request.env.add_to_compute(
+            order_sudo.order_line._fields['name'],
+            order_sudo.order_line.with_context(lang=lang),
+        )
+        return super().change_lang(lang, **kwargs)
