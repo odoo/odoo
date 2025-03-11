0288256c0b6f3fbc155ac5d319932b3ae9406b27
paan-odoo <paan@odoo.com>
2025-03-11 12:36:06 +0000

    [ADD] pos_{, pine_labs}: pine labs terminal for point of sale payment methods
    In this commit:
    
    - We have introduced a new module, `pos_pine_labs`, for the `Pine Labs` payment
      terminal to facilitate point of sale payment methods.
    - We are exporting `offlineErrorHandler` from `point_of_sale`'s `error_handlers`
      to use in the `pos_pine_labs` module.
    
    task-4359883
    
    closes odoo/odoo#201312
    
    X-original-commit: 41860be3df70868b467c3cddaf975b2ffd374649
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Anantkumar Parmar (paan) <paan@odoo.com>

--- addons/point_of_sale/static/src/backend/pos_payment_provider_cards/pos_payment_provider_cards.js
+++ addons/point_of_sale/static/src/backend/pos_payment_provider_cards/pos_payment_provider_cards.js
@@ -76,6 +76,7 @@ const providers = [
     ["viva_wallet", "pos_viva_wallet", "Viva Wallet"],
     ["worldline", "pos_iot_worldline", "Worldline"],
     ["tyro", "pos_tyro", "Tyro"],
+    ["pine_labs", "pos_pine_labs", "Pine Labs"],
 ];
 
 export const PosPaymentProviderCardsParams = {
