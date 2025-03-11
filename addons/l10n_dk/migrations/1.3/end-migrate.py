038c5c58c9bfe4201158797cb94cb1df7b08ee3f
asno-odoo <asno@odoo.com>
2025-03-11 10:46:55 +0000

    [FIX] l10n_dk: check if tax exists
    to reproduce
    1) install l10n_dk with minor version 1.2 in 18.0
    2) delete tax `K-EU-Momsfritaget` from taxes while selecting l10n_dk company.
    3) bump minor version and update.
    
    closes odoo/odoo#201134
    
    Signed-off-by: Maximilien La Barre (malb) <malb@odoo.com>

--- addons/l10n_dk/migrations/1.3/end-migrate.py
+++ addons/l10n_dk/migrations/1.3/end-migrate.py
@@ -4,5 +4,6 @@ from odoo import api, SUPERUSER_ID
 def migrate(cr, version):
     env = api.Environment(cr, SUPERUSER_ID, {})
     for company in env['res.company'].search([('chart_template', '=', 'dk')], order="parent_path"):
-        tax = env.ref(f'account.{company.id}_tax_keumf')
-        tax.type_tax_use = 'purchase'
+        tax = env.ref(f'account.{company.id}_tax_keumf', raise_if_not_found=False)
+        if tax:
+            tax.type_tax_use = 'purchase'
