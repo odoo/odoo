146231fa59f4398edd8ae7c9ca6f1b66db57da9b
Aaron Bohy <aab@odoo.com>
2025-03-11 10:39:17 +0000

    [FIX] web: many2many_tags: no crash when quickly remove tags
    Have a many2many_tags field, in a form view for instance, with
    multiple tags. Focus its input, and then quickly press `Backspace`
    multiple times. Before this commit, there were 2 problems.
    
    First, if there was an onchange on that field, the onchange was
    triggered multiple times with the same forget command, especially
    on a slow-ish network.
    
    Second, there could be a crash, but to reproduce it the timing had
    to be precise: backspace should have been pressed when a previous
    tag deletion was already processed by the model (i.e. the tag is
    no longer in the list), but the DOM wasn't updated yet. This could
    be done more easily then it sounds, by quickly pressing backspace
    on a many2many_tags with a lot of tags.
    
    The related opw is about the second issue, as the first one isn't
    obversable functionally. However, testing the second one is really
    tricky, even impossible without going white-box. We thus wrote a
    test for the first issue only, as the fix for both is actually the
    same.
    
    opw-4596936
    
    X-original-commit: 0fc535928f723c60f2be042499a4c1775567ada6

--- addons/web/static/src/views/fields/many2many_tags/many2many_tags_field.js
+++ addons/web/static/src/views/fields/many2many_tags/many2many_tags_field.js
@@ -10,6 +10,7 @@ import {
     useOpenMany2XRecord,
 } from "@web/views/fields/relational_utils";
 import { registry } from "@web/core/registry";
+import { Mutex } from "@web/core/utils/concurrency";
 import { standardFieldProps } from "../standard_field_props";
 import { TagsList } from "@web/core/tags_list/tags_list";
 import { usePopover } from "@web/core/popover/popover_hook";
@@ -78,6 +79,8 @@ export class Many2ManyTagsField extends Component {
             this.deleteTagByIndex.bind(this)
         );
         this.autoCompleteRef = useRef("autoComplete");
+        this.mutex = new Mutex();
+
         const { saveRecord, removeRecord } = useX2ManyCrud(
             () => this.props.record.data[this.props.name],
             true
@@ -183,8 +186,11 @@ export class Many2ManyTagsField extends Component {
     }
 
     async deleteTagByIndex(index) {
-        const { id } = this.tags[index] || {};
-        this.deleteTag(id);
+        this.mutex.exec(() => {
+            if (this.tags[index]) {
+                return this.deleteTag(this.tags[index].id);
+            }
+        });
     }
 
     async deleteTag(id) {
