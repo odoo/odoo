8ff191231caafb9455e9f02925cc979bf7f065d3
Robin Lejeune (role) <role@odoo.com>
2025-03-11 14:08:28 +0100

    Filter invisible snippets' edit interactions

--- addons/html_builder/static/src/core/plugins/visibility_plugin.js
+++ addons/html_builder/static/src/core/plugins/visibility_plugin.js
@@ -11,8 +11,10 @@ export class VisibilityPlugin extends Plugin {
     ];
 
     resources = {
-        on_option_visibility_update: onOptionVisibilityUpdate,
+        on_option_visibility_update: this.onOptionVisibilityUpdate.bind(this),
         on_mobile_preview_clicked: this.onMobilePreviewClicked.bind(this),
+        system_attributes: ["data-invisible"],
+        system_classes: ["o_snippet_override_invisible"],
     };
 
     cleanForSaveVisibility(editingEl) {
@@ -41,12 +43,12 @@ export class VisibilityPlugin extends Plugin {
             );
             invisibleOverrideEl.classList.remove("o_snippet_override_invisible");
             const show = isMobilePreview != isMobileHidden;
-            onOptionVisibilityUpdate({ editingEl: invisibleOverrideEl, show: show });
+            this.onOptionVisibilityUpdate({ editingEl: invisibleOverrideEl, show: show });
         }
     }
 
     toggleTargetVisibility(editingEl, show) {
-        show = onOptionVisibilityUpdate({ editingEl: editingEl, show: show });
+        show = this.onOptionVisibilityUpdate({ editingEl: editingEl, show: show });
         const dispatchName = show ? "target_show" : "target_hide";
         this.dispatchTo(dispatchName, editingEl);
         return show;
@@ -64,20 +66,23 @@ export class VisibilityPlugin extends Plugin {
             snippetEl.classList.add("o_snippet_override_invisible");
         }
     }
+
+    onOptionVisibilityUpdate({ editingEl, show }) {
+        if (show === undefined) {
+            show = !isTargetVisible(editingEl);
+        }
+        if (show !== isTargetVisible(editingEl)) {
+            this.dispatchTo("option_visibility_updated", editingEl);
+        }
+        if (show) {
+            delete editingEl.dataset.invisible;
+        } else {
+            editingEl.dataset.invisible = "1";
+        }
+        return show;
+    }
 }
 
 function isTargetVisible(editingEl) {
     return editingEl.dataset.invisible !== "1";
-}
-
-function onOptionVisibilityUpdate({ editingEl, show }) {
-    if (show === undefined) {
-        show = !isTargetVisible(editingEl);
-    }
-    if (show) {
-        delete editingEl.dataset.invisible;
-    } else {
-        editingEl.dataset.invisible = "1";
-    }
-    return show;
 }
