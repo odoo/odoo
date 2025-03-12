708590e2d2a05e95cadcebbaa638a7aa46920fa2
manv-afk <manv@odoo.com>
2025-03-12 08:51:35 +0000

    [FIX] pos_self_order: fix pay after meal & notify floorplan
    - Automatically update the floorplan when an order is paid from `self order`, marking the table as empty.
    - Fix "Pay After Each Meal" to prevent redirection to the payment page after every order.
    - Add a test tour for self-order payments (pay after meal, service at table) with online payment enabled.
    
    closes odoo/odoo#202311
    
    Task-id: 4610893
    X-original-commit: 82b2b238ea0a7ad7edc12ab5b94f4da0fb758cde
    Signed-off-by: Vlad Stroia (vlst) <vlst@odoo.com>
    Signed-off-by: Manu Vaillant (manv) <manv@odoo.com>

--- addons/pos_online_payment/static/src/app/services/pos_store.js
+++ addons/pos_online_payment/static/src/app/services/pos_store.js
@@ -4,11 +4,16 @@ import { PosStore } from "@point_of_sale/app/services/pos_store";
 patch(PosStore.prototype, {
     async setup() {
         await super.setup(...arguments);
-        this.data.connectWebSocket("ONLINE_PAYMENTS_NOTIFICATION", ({ id }) => {
+        this.data.connectWebSocket("ONLINE_PAYMENTS_NOTIFICATION", async ({ id }) => {
             // The bus communication is only protected by the name of the channel.
             // Therefore, no sensitive information is sent through it, only a
             // notification to invite the local browser to do a safe RPC to
             // the server to check the new state of the order.
+            const order = this.models["pos.order"].find((o) => o.id == id);
+            const updatedOrder = await this.data.read("pos.order", [id])[0];
+            if (updatedOrder) {
+                order.state = updatedOrder.state;
+            }
             if (this.getOrder()?.id === id) {
                 this.updateOnlinePaymentsDataWithServer(this.getOrder(), false);
             }
--- addons/pos_online_payment_self_order/tests/__init__.py
+++ addons/pos_online_payment_self_order/tests/__init__.py
@@ -2,3 +2,4 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import test_self_order_frontend
+from . import test_self_order_mobile
