8c564256e195cff120b564db9210893491a810e1
Paolo Gatti <pgi@odoo.com>
2025-03-12 19:22:47 +0100

    [IMP] pdf417gen: Move tests to dedicated module

---
+++ odoo/addons/test_vendor/tests/__init__.py
@@ -0,0 +1,7 @@
+from .pdf417gen import (
+    test_compaction,  # noqa: F401
+    test_encode,  # noqa: F401
+    test_error_correction,  # noqa: F401
+    test_performance,  # noqa: F401
+    test_renderers,  # noqa: F401
+)
