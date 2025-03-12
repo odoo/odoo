cb0c003ac2f69e9d333783ce05349f69d2088471
path-odoo <path@odoo.com>
2025-03-12 18:50:28 +0530

    [ADD] point_of_sale : implement alternative products section for pos
    After this commit :
    - Added a section to display alternative products using ProductCard components,
      styled to match the ProductScreen layout with product-list and
      pos.productListViewMode classes.
    - Implemented addProductToOrder to allow adding alternative products to the
      current order via the PosStore's addLineToCurrentOrder method.
    - Enabled nested ProductInfoPopup dialogs by adding onProductInfoClick,
      triggered by the 'i' button on alternative product cards, fetching product
      info with getProductInfo and opening a new popup.
    - Replaced pos_description with public_description for product descriptions,
      using the public_desc getter with markup for HTML rendering.

--- addons/pos_customization/models/product.py
+++ addons/pos_customization/models/product.py
@@ -4,14 +4,19 @@ from odoo.exceptions import ValidationError
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    pos_description = fields.Text(
-        string = 'Pos Description',
-        help = 'Pos specific description'
-    )
-
     alternative_name = fields.Char(
         string='Alternative Product Name',
         help='An alternative name for the product to display in POS, takes priority over the original name.'
+    )
+
+    pos_alternative_product_ids = fields.Many2many(
+        comodel_name='product.product',
+        relation='product_template_pos_alternative_rel',
+        column1='product_id',
+        column2='alternative_id',
+        string='POS Alternative Products',
+        help='Alternative products to suggest in the POS for this product.',
+        domain="[('available_in_pos', '=', True),('categ_id', '=', categ_id)]",
     )
 
     @api.constrains('alternative_name')
@@ -31,6 +36,6 @@ class ProductProduct(models.Model):
 
     def _load_pos_data_fields(self, config_id):
         result = super()._load_pos_data_fields(config_id)
-        fields_to_add = ['pos_description', 'alternative_name']
+        fields_to_add = [ 'public_description','alternative_name', 'pos_alternative_product_ids']
         result.extend(field for field in fields_to_add if field not in result)
         return result
--- addons/pos_customization/static/src/app/screens/product_screen/product_info_popup/product_info_popup.js
+++ addons/pos_customization/static/src/app/screens/product_screen/product_info_popup/product_info_popup.js
@@ -1,8 +1,34 @@
 import { patch } from "@web/core/utils/patch";
-import { ProductInfoPopup as OriginalProductInfoPopup } from "@point_of_sale/app/screens/product_screen/product_info_popup/product_info_popup";
+import { ProductInfoPopup } from "@point_of_sale/app/screens/product_screen/product_info_popup/product_info_popup";
+import { usePos } from "@point_of_sale/app/store/pos_hook";
+import { useService } from "@web/core/utils/hooks";
+import { ProductCard } from "@point_of_sale/app/generic_components/product_card/product_card";
+import { markup } from "@odoo/owl";
 
-patch(OriginalProductInfoPopup.prototype, {
-    static: {
-        template: "pos_customization.ProductInfoPopup",
+patch(ProductInfoPopup, {
+    components: {
+        ...ProductInfoPopup.components,
+        ProductCard,
+    },
+});
+
+patch(ProductInfoPopup.prototype, {
+    setup() {
+        super.setup();
+        this.pos = usePos();
+        this.dialog = useService("dialog");
+    },
+    getProductImage(product) {
+        return product.getTemplateImageUrl();
+    },
+    async addProductToOrder(product) {
+        await this.pos.addLineToCurrentOrder({ product_id: product }, {});
+    },
+    async onProductInfoClick(product) {
+        const info = await this.pos.getProductInfo(product, 1);
+        this.dialog.add(ProductInfoPopup, { info: info, product: product });
+    },
+    get public_desc() {
+        return markup(this.props.product.public_description);
     },
 });
