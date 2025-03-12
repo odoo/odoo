bf86db2e44ff7d117ce8563cc5296853cfdfbcee
Joseph (jcb) <jcb@odoo.com>
2025-03-12 13:56:49 +0000

    [FIX] point_of_sale: refund order with global discount
    When invoicing a pos.order with global discount, the created invoice
    don't have the correct amount because the sign of the quantity of the
    discount line is inverted. To identify the sign of the discount_line,
    we must base it on the sign of the order, not on the line.
    
    X-original-commit: 994b3c54c6bff06a295478dc0817e8fe24dd8818

---
+++ addons/pos_discount/static/tests/tours/global_discount_tour.js
@@ -0,0 +1,56 @@
+<<<<<<< HEAD
+||||||| MERGE BASE
+=======
+import * as ProductScreen from "@point_of_sale/../tests/pos/tours/utils/product_screen_util";
+import * as PaymentScreen from "@point_of_sale/../tests/pos/tours/utils/payment_screen_util";
+import * as ReceiptScreen from "@point_of_sale/../tests/pos/tours/utils/receipt_screen_util";
+import * as Chrome from "@point_of_sale/../tests/pos/tours/utils/chrome_util";
+import * as Dialog from "@point_of_sale/../tests/generic_helpers/dialog_util";
+import { registry } from "@web/core/registry";
+
+registry.category("web_tour.tours").add("pos_global_discount_tax_group", {
+    steps: () =>
+        [
+            Chrome.startPoS(),
+            Dialog.confirm("Open Register"),
+            ProductScreen.clickDisplayedProduct("Test Product"),
+            ProductScreen.clickControlButton("Discount"),
+            Dialog.confirm(),
+            ProductScreen.totalAmountIs(90),
+        ].flat(),
+});
+
+registry.category("web_tour.tours").add("pos_global_discount_tax_group_2", {
+    steps: () =>
+        [
+            Chrome.startPoS(),
+            Dialog.confirm("Open Register"),
+            ProductScreen.clickDisplayedProduct("Test Product"),
+            ProductScreen.clickControlButton("Discount"),
+            Dialog.confirm(),
+            ProductScreen.totalAmountIs(108),
+        ].flat(),
+});
+
+registry.category("web_tour.tours").add("test_invoice_order_with_global_discount", {
+    steps: () =>
+        [
+            Chrome.startPoS(),
+            Dialog.confirm("Open Register"),
+            ProductScreen.clickDisplayedProduct("Test Product"),
+            ProductScreen.clickControlButton("Discount"),
+            Dialog.confirm(),
+            ProductScreen.totalAmountIs("90.00"),
+            ProductScreen.clickPartnerButton(),
+            ProductScreen.clickCustomer("AAAAAA"),
+            ProductScreen.clickPayButton(),
+            PaymentScreen.totalIs("90.00"),
+            PaymentScreen.clickPaymentMethod("Cash"),
+            PaymentScreen.remainingIs("0.00"),
+            PaymentScreen.clickInvoiceButton(),
+            PaymentScreen.clickValidate(),
+            ReceiptScreen.receiptAmountTotalIs("90.00"),
+        ].flat(),
+});
+
+>>>>>>> FORWARD PORTED
---
+++ addons/pos_discount/tests/test_frontend.py
@@ -0,0 +1,105 @@
+<<<<<<< HEAD
+||||||| MERGE BASE
+=======
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo.tests import tagged
+from odoo.addons.point_of_sale.tests.test_frontend import TestPointOfSaleHttpCommon
+from odoo import Command
+
+
+@tagged('post_install', '-at_install')
+class TestUi(TestPointOfSaleHttpCommon):
+    def setUp(self):
+        super().setUp()
+        self.partner_a.name = "AAAAAA"
+        self.main_pos_config.module_pos_discount = True
+        self.main_pos_config.discount_product_id = self.env.ref("pos_discount.product_product_consumable", raise_if_not_found=False)
+
+    def test_global_discount_tax_group_included(self):
+        tax_10 = self.env['account.tax'].create({
+            'name': "tax_10",
+            'amount_type': 'percent',
+            'amount': 10.0,
+            'type_tax_use': 'none',
+            'price_include_override': 'tax_included'
+        })
+        tax_20 = self.env['account.tax'].create({
+            'name': "tax_20",
+            'amount_type': 'percent',
+            'amount': 20.0,
+            'type_tax_use': 'none',
+            'price_include_override': 'tax_included'
+        })
+        tax_group_10_20 = self.env['account.tax'].create({
+            'name': "tax_group_10_20",
+            'amount_type': 'group',
+            'children_tax_ids': [Command.set((tax_10 + tax_20).ids)],
+            'type_tax_use': 'sale',
+        })
+        self.env['product.product'].create({
+            'name': 'Test Product',
+            'lst_price': 100,
+            'taxes_id': [Command.set(tax_group_10_20.ids)],
+            'available_in_pos': True,
+        })
+
+        self.main_pos_config.with_user(self.pos_user).open_ui()
+        self.start_tour("/pos/ui?config_id=%d" % self.main_pos_config.id, 'pos_global_discount_tax_group', login="pos_user")
+
+    def test_global_discount_tax_group_include_exclude(self):
+        tax_10 = self.env['account.tax'].create({
+            'name': "tax_10",
+            'amount_type': 'percent',
+            'amount': 10.0,
+            'type_tax_use': 'none',
+            'include_base_amount': True,
+            'price_include_override': 'tax_included'
+        })
+        tax_20 = self.env['account.tax'].create({
+            'name': "tax_20",
+            'amount_type': 'percent',
+            'amount': 20.0,
+            'type_tax_use': 'none',
+        })
+        tax_group_10_20 = self.env['account.tax'].create({
+            'name': "tax_group_10_20",
+            'amount_type': 'group',
+            'children_tax_ids': [Command.set((tax_10 + tax_20).ids)],
+            'type_tax_use': 'sale',
+        })
+        self.env['product.product'].create({
+            'name': 'Test Product',
+            'lst_price': 100,
+            'taxes_id': [Command.set(tax_group_10_20.ids)],
+            'available_in_pos': True,
+        })
+
+        self.main_pos_config.with_user(self.pos_user).open_ui()
+        self.start_tour("/pos/ui?config_id=%d" % self.main_pos_config.id, 'pos_global_discount_tax_group_2', login="pos_user")
+
+    def test_invoice_order_with_global_discount(self):
+        self.env['product.product'].create({
+            'name': 'Test Product',
+            'lst_price': 100,
+            'taxes_id': [],
+            'available_in_pos': True,
+        })
+
+        self.main_pos_config.with_user(self.pos_user).open_ui()
+        self.start_tour("/pos/ui?config_id=%d" % self.main_pos_config.id, 'test_invoice_order_with_global_discount', login="pos_user")
+
+        current_session = self.main_pos_config.current_session_id
+        discount_product = self.main_pos_config.discount_product_id
+        order = current_session.order_ids[0]
+        discount_line = order.account_move.line_ids.filtered(lambda l: l.product_id.id == discount_product.id)
+        self.assertRecordValues(discount_line, [{
+            'quantity': 1.00,
+            'price_unit': -10.00,
+        }])
+        self.assertRecordValues(order.account_move, [{
+            'amount_untaxed': 90.0,
+            'amount_tax': 0.0,
+            'amount_total': 90.0,
+        }])
+
+>>>>>>> FORWARD PORTED
