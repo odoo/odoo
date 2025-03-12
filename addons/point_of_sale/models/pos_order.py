d6853cf6ae3d27801c12bc0390fc456d5028a5f4
Josse Colpaert <jco@odoo.com>
2025-03-12 15:57:52 +0000

    [FIX] point_of_sale: support combo product to invoice
    Similar solution as we did before for sale orders in
    https://github.com/odoo/odoo/pull/192568
    
    This allows to not have 0 lines in the invoice that
    do not make any sense for electronic invoicing (and in general).
    
    opw-4410461
    
    X-original-commit: d4aaed9daf8f7ab4a1b466f1364b44422b51ae2d

--- addons/point_of_sale/models/pos_order.py
+++ addons/point_of_sale/models/pos_order.py
@@ -197,6 +197,16 @@ class PosOrder(models.Model):
             or (move_type == 'out_refund' and not is_refund_order)
         ) else 1
 
+        if line_values['product_id'].type == 'combo':
+            quantity = int(line_values['quantity']) if line_values['quantity'] == int(
+                line_values['quantity']) else line_values['quantity']
+            return {
+                'display_type': 'line_section',
+                'name': f"{line_values['product_id'].name} x {quantity}",
+                'quantity': qty_sign * line_values['quantity'],
+                'product_uom_id': line_values['uom_id'].id,
+            }
+
         return {
             'product_id': line_values['product_id'].id,
             'quantity': qty_sign * line_values['quantity'],
