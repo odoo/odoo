4c70393c3b17643450ea888c317303c605190c00
Adnan Saiyed <adsa@odoo.com>
2025-03-12 12:22:54 +0530

    [FIX] html_editor: remove text-align format in list
    Current behavior before commit:
    
    Steps to reproduce:
    
    - Write something in a paragraph.
    - Select the text and set text-align to right.
    - Create a bullet list from selected text.
    - Try to remove format using removeFormat button.
    
    In toolbar remove format button is disabled and text alignment
    is not getting removed when clicking on it. This issue happens
    because when creating a new list from paragraph, text-align
    style of paragraph is set to ul tag. Remove format works here
    only if text-align property it applied to closestBlock which
    is li element and not ul.
    
    Desired behavior after commit:
    
    - Now, remove format button is active and text alignment is getting
      removed from list when selecting list item.
    
    - This commit also changes implementation of insertListAfter util
      to insert single LI element instead of multiple.
    
    task-4637177
    
    Part-of: odoo/odoo#201314
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/html_editor/static/src/main/list/list_plugin.js
+++ addons/html_editor/static/src/main/list/list_plugin.js
@@ -325,7 +325,7 @@ export class ListPlugin extends Plugin {
             // possible.
             const callingNode = element.firstChild;
             const group = getAdjacents(callingNode, (n) => !isBlock(n));
-            list = insertListAfter(this.document, callingNode, mode, [group]);
+            list = insertListAfter(this.document, callingNode, mode, group);
         } else {
             const parent = element.parentNode;
             const childIndex = childNodeIndex(element);
@@ -354,9 +354,13 @@ export class ListPlugin extends Plugin {
      */
     baseContainerToList(baseContainer, mode) {
         const cursors = this.dependencies.selection.preserveSelection();
-        const list = insertListAfter(this.document, baseContainer, mode, [
-            childNodes(baseContainer),
-        ]);
+        const list = insertListAfter(this.document, baseContainer, mode, childNodes(baseContainer));
+        const textAlign = baseContainer.style.getPropertyValue("text-align");
+        if (textAlign) {
+            // Copy text-align style from base container to li.
+            list.firstElementChild.style.setProperty("text-align", textAlign);
+            baseContainer.style.removeProperty("text-align");
+        }
         this.dependencies.dom.copyAttributes(baseContainer, list);
         this.adjustListPadding(list);
         baseContainer.remove();
@@ -366,7 +370,7 @@ export class ListPlugin extends Plugin {
 
     blockContentsToList(block, mode) {
         const cursors = this.dependencies.selection.preserveSelection();
-        const list = insertListAfter(this.document, block.lastChild, mode, [[...block.childNodes]]);
+        const list = insertListAfter(this.document, block.lastChild, mode, [...block.childNodes]);
         cursors.remapNode(block, list.firstChild).restore();
         return list;
     }
