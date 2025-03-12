f1132f36b00303f8d8b4f04139f1df61bdd13e41
Hussein Ebrahim(hyhe) <hyhe@odoo.com>
2025-03-12 12:57:53 +0000

    [FIX] pos_sale: used sale.order.line read_converted in settleOrder
    Steps to reproduce the bug:
    - Create a storable product, that is tracked by serial number
    - Add to in Hand the product and set the serial number to it
    - Create a quotation of this product and confirm to be a sale order
    - Open a pos session, and select quotations and orders from pos
    - Select the sales order created and settle the order
    - The order line will be imported without asking if serial number
      can be linked
    
    Issue:
    The function settleOrder in pos_sale/pos_store.js reads the sales
    order using this.data.read, this will read the sales order with
    the sale order line but without the information of the lot numbers
    and more info. These informations are returned in a read_converted
    function on sale.order.line inherited model on pos_sale, and this
    function is not used to read the sale order lines, hence no
    information related to the lots and the pop up that asks if the lot
    number may be linked will not appear anymore.
    
    opw-4591887
    
    closes odoo/odoo#205023
    
    X-original-commit: f20a82723edc18cfe7afdcd5ee7f8c7fdd489093
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: Hussein Yasser Hussein Ebrahim (hyhe) <hyhe@odoo.com>

--- addons/pos_sale/static/tests/tours/utils/pos_sale_utils.js
+++ addons/pos_sale/static/tests/tours/utils/pos_sale_utils.js
@@ -2,7 +2,7 @@ import * as ProductScreen from "@point_of_sale/../tests/pos/tours/utils/product_
 import * as Dialog from "@point_of_sale/../tests/generic_helpers/dialog_util";
 import * as Numpad from "@point_of_sale/../tests/generic_helpers/numpad_util";
 
-function selectNthOrder(n) {
+export function selectNthOrder(n) {
     return [
         ...ProductScreen.clickControlButton("Quotation/Order"),
         {
@@ -13,18 +13,27 @@ function selectNthOrder(n) {
     ];
 }
 
-export function settleNthOrder(n) {
-    return [
+export function settleNthOrder(n, options = {}) {
+    const { loadSN } = options;
+    const step = [
         ...selectNthOrder(n),
         {
             content: `Choose to settle the order`,
             trigger: `.modal:not(.o_inactive_modal) .selection-item:contains('Settle the order')`,
             run: "click",
         },
-        {
-            trigger: "body:not(:has(.modal))",
-        },
     ];
+    if (loadSN) {
+        step.push({
+            content: `Choose to auto link the lot number to the order line`,
+            trigger: `.modal-content:contains('Do you want to load the SN/Lots linked to the Sales Order?') button:contains('Ok')`,
+            run: "click",
+        });
+    }
+    step.push({
+        trigger: "body:not(:has(.modal))",
+    });
+    return step;
 }
 
 export function downPaymentFirstOrder(amount) {
@@ -48,4 +57,13 @@ export function checkOrdersListEmpty() {
             trigger: "p:contains(No record found)",
         },
     ];
+}
+
+export function selectedOrderLinesHasLots(productName, lots) {
+    const getSerialStep = (index, serialNumber) => ({
+        content: `check lot${index} is linked`,
+        trigger: `.info-list li:contains(${serialNumber})`,
+    });
+    const lotSteps = lots.reduce((acc, serial, i) => acc.concat(getSerialStep(i, serial)), []);
+    return [...ProductScreen.selectedOrderlineHas(productName), ...lotSteps];
 }
