de7c6ca32fdc29fbf9b8873fc3ff04c59f95a05d
manv-afk <manv@odoo.com>
2025-03-12 08:51:35 +0000

    [FIX] pos_self_order: fix pay after meal & notify floorplan
    - Automatically update the floorplan when an order is paid from `self order`, marking the table as empty.
    - Fix "Pay After Each Meal" to prevent redirection to the payment page after every order.
    - Add a test tour for self-order payments (pay after meal, service at table) with online payment enabled.
    
    Task-id: 4610893
    X-original-commit: 82b2b238ea0a7ad7edc12ab5b94f4da0fb758cde

---
+++ addons/pos_online_payment_self_order/static/tests/tours/pos_self_order_mobile_online_payment_tour.js
@@ -0,0 +1,29 @@
+import { registry } from "@web/core/registry";
+import * as Utils from "@pos_self_order/../tests/tours/utils/common";
+import * as CartPage from "@pos_self_order/../tests/tours/utils/cart_page_util";
+import * as LandingPage from "@pos_self_order/../tests/tours/utils/landing_page_util";
+import * as ProductPage from "@pos_self_order/../tests/tours/utils/product_page_util";
+import * as ConfirmationPage from "@pos_self_order/../tests/tours/utils/confirmation_page_util";
+
+registry.category("web_tour.tours").add("self_mobile_online_payment_meal_table", {
+    steps: () => [
+        Utils.checkIsNoBtn("My Order"),
+        Utils.clickBtn("Order Now"),
+        LandingPage.selectLocation("Test-In"),
+        ProductPage.clickProduct("Coca-Cola"),
+        Utils.clickBtn("Order"),
+        CartPage.checkProduct("Coca-Cola", "2.53", "1"),
+        Utils.clickBtn("Order"),
+        ...CartPage.selectTable("103"),
+        Utils.clickBtn("Ok"),
+        Utils.clickBtn("Order Now"),
+        ProductPage.clickProduct("Fanta"),
+        Utils.clickBtn("Order"),
+        CartPage.checkProduct("Fanta", "2.53", "1"),
+        Utils.clickBtn("Order"),
+        ConfirmationPage.isShown(),
+        Utils.clickBtn("Ok"),
+        Utils.clickBtn("My Order"),
+        Utils.clickBtn("Pay"),
+    ],
+});
