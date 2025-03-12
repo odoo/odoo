29774164b4ada68ad073ac1926ce3f86b9347755
rare-odoo <rare@odoo.com>
2025-03-12 13:29:46 +0530

    [REF] sale_stock: refactor stock forecast data supporting multi-warehouses
    This commit refactors the data preparation logic in sale_stock to accommodate
    multiple warehouses. It aligns with the primary commit by formatting the data
    based on warehouse-specific information.

--- addons/sale_stock/report/stock_forecasted.py
+++ addons/sale_stock/report/stock_forecasted.py
@@ -30,19 +30,23 @@ class StockForecasted_Product_Product(models.AbstractModel):
             })
         return line
 
-    def _get_report_header(self, product_template_ids, product_ids, wh_location_ids):
-        res = super()._get_report_header(product_template_ids, product_ids, wh_location_ids)
-        domain = self._product_sale_domain(product_template_ids, product_ids)
-        so_lines = self.env['sale.order.line'].sudo().search(domain)
-        out_sum = 0
-        if so_lines:
-            product_uom = so_lines[0].product_id.uom_id
-            quantities = so_lines.mapped(lambda line: line.product_uom_id._compute_quantity(line.product_uom_qty, product_uom))
-            out_sum = sum(quantities)
-        res['draft_sale_qty'] = out_sum
-        res['draft_sale_orders'] = so_lines.mapped("order_id").sorted(key=lambda so: so.name).read(fields=['id', 'name'])
-        res['draft_sale_orders_matched'] = self.env.context.get('sale_line_to_match_id') in so_lines.ids
-        res['qty']['out'] += out_sum
+    def _get_report_data(self, product_template_ids=False, product_ids=False):
+        res = super()._get_report_data(product_template_ids, product_ids)
+        sale_warehouse_data = self._get_draft_sale_order_data(product_template_ids, product_ids)
+        if res['multiple_warehouses']:
+            for warehouse in res['warehouses']:
+                warehouse_sale_data = sale_warehouse_data.get(warehouse['id'])
+                if warehouse_sale_data:
+                    warehouse.update({**warehouse_sale_data})
+                    warehouse['qty']['out'] += warehouse_sale_data['draft_sale_qty']
+        else:
+            warehouse_id = res['warehouses'][0]['id']
+            warehouse_sale_data = sale_warehouse_data.get(warehouse_id)
+            if warehouse_sale_data:
+                res.update({
+                    **warehouse_sale_data
+                })
+                res['qty']['out'] += warehouse_sale_data['draft_sale_qty']
         return res
 
     def _product_sale_domain(self, product_template_ids, product_ids):
@@ -51,7 +55,30 @@ class StockForecasted_Product_Product(models.AbstractModel):
             domain += [('product_template_id', 'in', product_template_ids)]
         elif product_ids:
             domain += [('product_id', 'in', product_ids)]
-        warehouse_id = self.env.context.get('warehouse_id', False)
-        if warehouse_id:
-            domain += [('warehouse_id', '=', warehouse_id)]
         return domain
+
+    def _get_draft_sale_order_data(self, product_template_ids=False, product_ids=False):
+        """
+        Draft sale order data grouped by warehouse
+        :return: Dictionary mapping warehouse_id to draft sale data
+        :rtype: dict[int, dict[float | list | bool]]
+        """
+        domain = self._product_sale_domain(product_template_ids, product_ids)
+        grouped_data = dict(
+            self.env['sale.order.line'].sudo()._read_group(domain, ['warehouse_id'], aggregates=['id:recordset'])
+        )
+
+        sale_data_by_warehouse = {}
+        for warehouse in grouped_data:
+            so_lines = grouped_data[warehouse]
+            out_sum = 0
+            if so_lines:
+                product_uom = so_lines[0].product_id.uom_id
+                quantities = so_lines.mapped(lambda line: line.product_uom_id._compute_quantity(line.product_uom_qty, product_uom))
+                out_sum = sum(quantities)
+            sale_data_by_warehouse[warehouse.id] = {
+                'draft_sale_qty': out_sum,
+                'draft_sale_orders': so_lines.mapped("order_id").sorted(key=lambda so: so.name).read(fields=['id', 'name']),
+                'draft_sale_orders_matched': self.env.context.get('sale_line_to_match_id') in so_lines.ids
+            }
+        return sale_data_by_warehouse
