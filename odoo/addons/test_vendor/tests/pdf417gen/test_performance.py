63d89ced9425a226c75f8a259da41ed69a8c139f
Paolo Gatti <pgi@odoo.com>
2025-03-12 19:27:13 +0100

    [IMP] pdf417gen: Refactor tests to work with Odoo

--- odoo/addons/test_vendor/tests/pdf417gen/test_performance.py
+++ odoo/addons/test_vendor/tests/pdf417gen/test_performance.py
@@ -1,9 +1,8 @@
-"""
-A simple performance test.
-"""
+from datetime import datetime, timedelta
 
-from datetime import datetime
-from pdf417gen import encode, render_image, render_svg
+from odoo.tests import common, tagged
+from odoo.tools._vendor.pdf417gen import encode, render_image, render_svg
+
 
 ZEN = """
 Beautiful is better than ugly.
@@ -28,33 +27,30 @@ Namespaces are one honking great idea -- let's do more of those!
 """.strip()
 
 
-def test_encode(cycles=100):
-    start = datetime.now()
-    for _ in range(cycles):
-        encode(ZEN)
-    duration = datetime.now() - start
-    print("Encode x{}: {}".format(cycles, duration))
+@tagged('-at_install', 'post_install')
+class TestPerformance(common.TransactionCase):
+
+    def test_encode(self, cycles=100):
+        start = datetime.now()
+        for _ in range(cycles):
+            encode(ZEN)
+        duration = datetime.now() - start
+        self.assertLess(duration, timedelta(milliseconds=500))
 
 
-def test_render_image(cycles=100):
-    codes = encode(ZEN)
-    start = datetime.now()
-    for _ in range(cycles):
-        render_image(codes)
-    duration = datetime.now() - start
-    print("Render image x{}: {}".format(cycles, duration))
+    def test_render_image(self, cycles=100):
+        codes = encode(ZEN)
+        start = datetime.now()
+        for _ in range(cycles):
+            render_image(codes)
+        duration = datetime.now() - start
+        self.assertLess(duration, timedelta(milliseconds=500))
 
 
-def test_render_svg(cycles=100):
-    codes = encode(ZEN)
-    start = datetime.now()
-    for _ in range(cycles):
-        render_svg(codes)
-    duration = datetime.now() - start
-    print("Render SVG x{}: {}".format(cycles, duration))
-
-
-if __name__ == "__main__":
-    test_encode()
-    test_render_image()
-    test_render_svg()
+    def test_render_svg(self, cycles=100):
+        codes = encode(ZEN)
+        start = datetime.now()
+        for _ in range(cycles):
+            render_svg(codes)
+        duration = datetime.now() - start
+        self.assertLess(duration, timedelta(seconds=2))
--- odoo/addons/test_vendor/tests/pdf417gen/test_renderers.py
+++ odoo/addons/test_vendor/tests/pdf417gen/test_renderers.py
@@ -1,92 +1,98 @@
-from pdf417gen import render_svg, render_image, encode
-from pdf417gen.rendering import barcode_size, rgb_to_hex
 from PIL.Image import Image
 from xml.etree.ElementTree import ElementTree
 
-codes = encode("hello world!")
+from odoo.tests import common, tagged
+from odoo.tools._vendor.pdf417gen import render_svg, render_image, encode
+from odoo.tools._vendor.pdf417gen.rendering import barcode_size, rgb_to_hex
 
 
-def modules(codes):
-    """Iterates over barcode codes and yields barcode moudles.
+@tagged('-at_install', 'post_install')
+class TestRenderers(common.TransactionCase):
 
-    Yields: column number (int), row number (int), module visibility (bool).
-    """
+    @classmethod
+    def setUpClass(cls, *args, **kwargs):
+        super().setUpClass(*args, **kwargs)
+        cls.codes = encode("hello world!")
 
-    for row_id, row in enumerate(codes):
-        col_id = 0
-        for value in row:
-            for digit in format(value, 'b'):
-                yield col_id, row_id, digit == "1"
-                col_id += 1
+    def modules(self, codes):
+        """Iterates over barcode codes and yields barcode moudles.
+
+        Yields: column number (int), row number (int), module visibility (bool).
+        """
+
+        for row_id, row in enumerate(codes):
+            col_id = 0
+            for value in row:
+                for digit in format(value, 'b'):
+                    yield col_id, row_id, digit == "1"
+                    col_id += 1
+
+    def test_rgb_to_hex(self):
+        self.assertEqual(rgb_to_hex((255, 0, 0)), "#ff0000")
+        self.assertEqual(rgb_to_hex((0, 255, 0)), "#00ff00")
+        self.assertEqual(rgb_to_hex((0, 0, 255)), "#0000ff")
+
+        self.assertEqual(rgb_to_hex((100, 0, 0)), "#640000")
+        self.assertEqual(rgb_to_hex((0, 100, 0)), "#006400")
+        self.assertEqual(rgb_to_hex((0, 0, 100)), "#000064")
+
+        self.assertEqual(rgb_to_hex((111, 222, 32)), "#6fde20")
+
+    def test_render_svg(self):
+        scale = 2
+        ratio = 4
+        description = "hi there"
+
+        tree = render_svg(self.codes, scale=scale, ratio=ratio, description=description)
+        assert isinstance(tree, ElementTree)
+        self.assertEqual(tree.findtext("description"), description)
+
+        # Test expected size
+        width, height = barcode_size(self.codes)
+
+        root = tree.getroot()
+
+        self.assertEqual(root.get("width"), str(width * scale))
+        self.assertEqual(root.get("height"), str(height * scale * ratio))
+        self.assertEqual(root.get("version"), "1.1")
+        self.assertEqual(root.get("xmlns"), "http://www.w3.org/2000/svg")
+
+        # Check number of rendered modules (only visible ones)
+        expected_module_count = len([v for x, y, v in self.modules(self.codes) if v])
+        actual_module_count = len(root.findall('g/rect'))
+
+        self.assertEqual(expected_module_count, actual_module_count)
 
 
-def test_rgb_to_hex():
-    assert rgb_to_hex((255, 0, 0)) == "#ff0000"
-    assert rgb_to_hex((0, 255, 0)) == "#00ff00"
-    assert rgb_to_hex((0, 0, 255)) == "#0000ff"
+    def test_render_image(self):
+        width, height = barcode_size(self.codes)
 
-    assert rgb_to_hex((100, 0, 0)) == "#640000"
-    assert rgb_to_hex((0, 100, 0)) == "#006400"
-    assert rgb_to_hex((0, 0, 100)) == "#000064"
+        image = render_image(self.codes)
+        assert isinstance(image, Image)
 
-    assert rgb_to_hex((111, 222, 32)) == "#6fde20"
+        image = render_image(self.codes, scale=1, ratio=1, padding=0)
+        self.assertEqual(image.size, (width, height))
 
+        image = render_image(self.codes, scale=2, ratio=1, padding=0)
+        self.assertEqual(image.size, (2 * width, 2 * height))
 
-def test_render_svg():
-    scale = 2
-    ratio = 4
-    description = "hi there"
+        image = render_image(self.codes, scale=2, ratio=2, padding=0)
+        self.assertEqual(image.size, (2 * width, 4 * height))
 
-    tree = render_svg(codes, scale=scale, ratio=ratio, description=description)
-    assert isinstance(tree, ElementTree)
-    assert tree.findtext("description") == description
+        image = render_image(self.codes, scale=2, ratio=2, padding=20)
+        self.assertEqual(image.size, (2 * width + 40, 4 * height + 40))
 
-    # Test expected size
-    width, height = barcode_size(codes)
+        # Check actual pixels
+        fg_color = "LemonChiffon"
+        bg_color = "#aabbcc"
 
-    root = tree.getroot()
+        fg_parsed = (255, 250, 205)
+        bg_parsed = (170, 187, 204)
 
-    assert root.get("width") == str(width * scale)
-    assert root.get("height") == str(height * scale * ratio)
-    assert root.get("version") == "1.1"
-    assert root.get("xmlns") == "http://www.w3.org/2000/svg"
+        image = render_image(self.codes, scale=1, ratio=1, padding=0,
+                             fg_color=fg_color, bg_color=bg_color)
+        px = image.load()
 
-    # Check number of rendered modules (only visible ones)
-    expected_module_count = len([v for x, y, v in modules(codes) if v])
-    actual_module_count = len(root.findall('g/rect'))
-
-    assert expected_module_count == actual_module_count
-
-
-def test_render_image():
-    width, height = barcode_size(codes)
-
-    image = render_image(codes)
-    assert isinstance(image, Image)
-
-    image = render_image(codes, scale=1, ratio=1, padding=0)
-    assert image.size == (width, height)
-
-    image = render_image(codes, scale=2, ratio=1, padding=0)
-    assert image.size == (2 * width, 2 * height)
-
-    image = render_image(codes, scale=2, ratio=2, padding=0)
-    assert image.size == (2 * width, 4 * height)
-
-    image = render_image(codes, scale=2, ratio=2, padding=20)
-    assert image.size == (2 * width + 40, 4 * height + 40)
-
-    # Check actual pixels
-    fg_color = "LemonChiffon"
-    bg_color = "#aabbcc"
-
-    fg_parsed = (255, 250, 205)
-    bg_parsed = (170, 187, 204)
-
-    image = render_image(codes, scale=1, ratio=1, padding=0,
-                         fg_color=fg_color, bg_color=bg_color)
-    px = image.load()
-
-    for column, row, visible in modules(codes):
-        expected = fg_parsed if visible else bg_parsed
-        assert px[column, row] == expected
+        for column, row, visible in self.modules(self.codes):
+            expected = fg_parsed if visible else bg_parsed
+            self.assertEqual(px[column, row], expected)
