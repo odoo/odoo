27699bd277a13baaafb282a6fc6c5bdf048571da
Krzysztof Magusiak (krma) <krma@odoo.com>
2025-03-12 16:06:17 +0000

    [IMP] base: res.user fix access permission on change
    When running `onchange` on current user, we add permissions to
    SELF_READABLE_FIELDS for the origin record.
    
    Part-of: odoo/odoo#201565
    Related: odoo/enterprise#81338
    Signed-off-by: Raphael Collet <rco@odoo.com>

--- odoo/addons/base/models/res_users.py
+++ odoo/addons/base/models/res_users.py
@@ -510,8 +510,8 @@ class ResUsers(models.Model):
 
     def _has_field_access(self, field, operation):
         return super()._has_field_access(field, operation) or (
-            self == self.env.user
-            and operation == 'read'
+            operation == 'read'
+            and self._origin == self.env.user
             and field.name in self.SELF_READABLE_FIELDS
         )
 
