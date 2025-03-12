827afe77f74576af3096c0f504b3216c75338d6b
Sébastien Blondiau <blse@odoo.com>
2025-03-12 17:44:45 +0100

    facebookPage (.o_facebook_page)
    The values added in the snippet comes from `addons/website/static/src/snippets/s_facebook_page/options.js` > `facebookPage` > `willStart`
    
    Special handling to set the link based on data from the orm, has been made with the help of nby (I'm not that confident to leave such a warning by myself :p )

---
+++ addons/html_builder/static/src/plugins/facebook_option_plugin.js
@@ -0,0 +1,159 @@
+import { Plugin } from "@html_editor/plugin";
+import { registry } from "@web/core/registry";
+import { _t } from "@web/core/l10n/translation";
+import { getCommonAncestor } from "@html_editor/utils/dom_traversal";
+
+class FacebookOptionPlugin extends Plugin {
+    static id = "facebookOption";
+    static dependencies = ["history"];
+    closeNotif = () => {};
+    resources = {
+        builder_options: [
+            {
+                template: "html_builder.FacebookOption",
+                selector: ".o_facebook_page",
+            },
+        ],
+        builder_actions: {
+            dataAttributeListAction: {
+                isApplied: ({ editingElement, param: { mainParam } = {}, value }) =>
+                    (editingElement.dataset[mainParam]?.split(",") || []).includes(value),
+                apply: ({ editingElement, param: { mainParam } = {}, value }) => {
+                    editingElement.dataset[mainParam] = [
+                        ...(editingElement.dataset[mainParam]?.split(",") || []),
+                        value,
+                    ].join(",");
+                },
+                clean: ({ editingElement, param: { mainParam } = {}, value }) => {
+                    editingElement.dataset[mainParam] = (
+                        editingElement.dataset[mainParam]?.split(",") || []
+                    )
+                        .filter((e) => e !== value)
+                        .join(",");
+                },
+            },
+            checkFacebookLinkAction: {
+                apply: ({ editingElement, value }) => {
+                    editingElement.dataset.id = "";
+                    const id = this.idFromFacebookLink(value);
+                    if (id) {
+                        editingElement.dataset.id = id;
+                        this.checkFacebookId(id).then((ok) => {
+                            this.closeNotif();
+                            if (ok) {
+                                this.closeNotif = () => {};
+                            } else {
+                                this.closeNotif = this.services.notification.add(
+                                    _t("We couldn't find the Facebook page"),
+                                    { type: "warning" }
+                                );
+                            }
+                        });
+                    } else {
+                        this.closeNotif();
+                        this.closeNotif = this.services.notification.add(
+                            _t("You didn't provide a valid Facebook link"),
+                            { type: "warning" }
+                        );
+                    }
+                },
+            },
+        },
+        normalize_handlers: this.normalize.bind(this),
+    };
+
+    normalize(root) {
+        for (const element of [
+            ...root.querySelectorAll(".o_facebook_page"),
+            ...(root.matches(".o_facebook_page") ? [root] : []),
+        ]) {
+            let desiredHeight;
+            if (element.dataset.tabs) {
+                desiredHeight = element.dataset.tabs === "events" ? 300 : 500;
+            } else if (element.dataset.small_header) {
+                desiredHeight = 70;
+            } else {
+                desiredHeight = 150;
+            }
+            if (desiredHeight !== element.dataset.height) {
+                element.dataset.height = desiredHeight;
+            }
+        }
+
+        const SELECTOR = ".o_facebook_page:not([data-href])";
+        const nodes = [
+            ...root.querySelectorAll(SELECTOR),
+            ...(root.matches(SELECTOR) ? [root] : []),
+        ];
+        if (nodes.length) {
+            this.loadAndSetEmptyLink(nodes);
+        }
+    }
+
+    loadAndSetEmptyLink(nodes) {
+        if (this.facebookUrl) {
+            this.setEmptyLink(nodes);
+            return;
+        }
+        // Fetches the default url for facebook page from website config
+        this.services.orm
+            .searchRead("website", [], ["social_facebook"], { limit: 1 })
+            .then((res) => {
+                if (res && res[0].social_facebook) {
+                    this.facebookUrl = res[0].social_facebook;
+
+                    // WARNING: the call to disableObserver and enableObserver are very dangerous,
+                    // and should be avoided in most cases (if you think you need those, ask html_editor team)
+                    this.dependencies.history.disableObserver();
+                    const hasChanged = this.setEmptyLink(nodes);
+                    this.dependencies.history.enableObserver();
+
+                    if (hasChanged) {
+                        const commonAncestor = getCommonAncestor(nodes, this.editable);
+                        this.dispatchTo("content_manually_updated_handlers", commonAncestor);
+                        this.config.onChange({ isPreviewing: false });
+                    }
+                }
+            });
+    }
+
+    setEmptyLink(nodes) {
+        let hasChanged = false;
+        for (const element of nodes) {
+            if (!element.dataset.href) {
+                element.dataset.href = this.facebookUrl;
+                hasChanged = true;
+            }
+        }
+        return hasChanged;
+    }
+
+    idFromFacebookLink(url) {
+        // Patterns matched by the regex (all relate to existing pages,
+        // in spite of the URLs containing "profile.php" or "people"):
+        // - https://www.facebook.com/<pagewithaname>
+        // - http://www.facebook.com/<page.with.a.name>
+        // - www.facebook.com/<fbid>
+        // - facebook.com/profile.php?id=<fbid>
+        // - www.facebook.com/<name>-<fbid>  - NB: the name doesn't matter
+        // - www.fb.com/people/<name>/<fbid>  - same
+        // - m.facebook.com/p/<name>-<fbid>  - same
+        // The regex is kept as a huge one-liner for performance as it is
+        // compiled once on script load. The only way to split it on several
+        // lines is with the RegExp constructor, which is compiled on runtime.
+        const match = url
+            .trim()
+            .match(
+                /^(https?:\/\/)?((www\.)?(fb|facebook)|(m\.)?facebook)\.com\/(((profile\.php\?id=|people\/([^/?#]+\/)?|(p\/)?[^/?#]+-)(?<id>[0-9]{12,16}))|(?<nameid>[\w.]+))($|[/?# ])/
+            );
+
+        return match?.groups.nameid || match?.groups.id;
+    }
+
+    async checkFacebookId(id) {
+        const res = await fetch(`https://graph.facebook.com/${id}/picture`);
+        return res.ok;
+    }
+}
+
+registry.category("website-plugins").add(FacebookOptionPlugin.id, FacebookOptionPlugin);
