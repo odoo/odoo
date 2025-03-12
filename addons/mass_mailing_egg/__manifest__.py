4926e1cd9aeebfb87e26de7b9085a28198e9284d
abd-msyukyu-odoo <abd@odoo.com>
2025-03-12 14:56:48 +0100

    [IMP] mass_mailing_egg: to split
    - rename plugin registry
    - fix (disable_snippet_plugin ?)
    - TODO move font_plugin in website again? Isolate changes to this plugin (not
    used in mass mailing)
    - changes were made to snippetModel.snippetContentCategories, undo that as we
    now use website inner content and categories
    - various changes to html_builder for compatibility
    - fullscreen button
    - markings to remove mass_mailing_stuff
    - website changes for registry compatibility
    - fix on mass_mailing_themes
    
    implement basic editor and complete iframe style
    
    implement readonly mode
    
    Fixes to HTML FIELD
    
    [FIX] fixed the builder mounting
    
    Working on snippet options/plugins (blockquote, ...)
    
    fixing after rebase
    
    better processing
    
    fixes to send proper email
    
    mass_mailing_egg: style
    
    fix favorite theme selection
    
    make o_editable work + /field command + start fixing nbsp in templates
    
    [IMP] mass_mailing_egg: Masonry block options
    
    [IMP] mass_mailing_egg: fix name
    
    [IMP] mass_mailing_egg fix rounded corners
    
    [IMP] mass_mailing_egg fix template xpath
    
    [IMP] mass_mailing_egg use correct registry
    
    egg codeview implementation
    
    [IMP] mass_mailing_egg fix manifest warning
    
    [IMP] mass_mailing_egg: modify options mass_mailing_iframe
    
    [IMP] mass_mailing_egg: [FIX] builder_overlay_patch sizingX exclude was incorrect
    
    [IMP] mass_mailing_egg: [FIX] Snippets options that were problematic
    
    egg snippets_progress_bar fix columns, border and alert options
    
    [IMP] mass_mailing_egg [FIX] closing iframe crash
    
    egg mobile works
    
    [IMP] mass_mailing_egg: change access key for codeview
    
    [IMP] mass_mailing_egg customize_mailing_plugin
    
    egg clean theme tab modifs
    
    [IMP] mass_mailing_egg design truc
    
    [IMP] mass_mailing_egg: body width change
    
    [IMP] mass_mailing_egg customize body option for design tab
    
    [IMP] mass_mailing_egg fix for isSmall ui
    
    [IMP] mass_mailing_egg: random stuff for mass_mailing_html_field
    
    [IMP] mass_mailing_egg WIP design tab
    
    [IMP] mass_mailing_egg: syntaxic sugar for ruleStyle
    
    [IMP] mass_mailing_egg [FIX] small adjustments and cleaning
    
    [IMP] mass_mailing_egg [WIP] Design tab continues
    
    egg new api for design tab action
    
    [IMP] mass_mailing_egg [WIP] headings
    
    [IMP] mass_mailing_egg: selector is array and start design btn
    
    [IMP] mass_mailing_egg: fix selector for now (maybe do something else later)
    
    [IMP] mass_mailing_egg: fix todo about isSmall
    
    [IMP] mass_mailing_egg: [WIP] headings
    
    [IMP] mass_mailing_egg: working on variables
    
    egg almost all options design_tab
    
    [IMP] mass_mailing_egg: all text options work without font family
    
    [IMP] mass_mailing_egg: variables
    
    [IMP] mass_mailing_egg: fontfamily picker
    
    [IMP] mass_mailing egg
    
    customize variable for separator
    
    TODO: remove font_plugin in mass_mailing_builder ?
    
    [IMP] mass_mailing_egg: remove issmall handling (sidebar is just hidden, we stay in edit mode)
    
    [IMP] mass_mailing_egg: default values for design tab
    
    egg mobile + snippets + cleaning
    
    egg fixing separator plugin and better design tab

---
+++ addons/mass_mailing_egg/__manifest__.py
@@ -0,0 +1,64 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Email Marketing Egg',
+    'summary': 'Design, send and track emails',
+    'version': '1.0',
+    'sequence': 61,
+    'website': 'https://www.odoo.com/app/email-marketing',
+    'category': 'Hidden',
+    'auto_install': True,
+    'depends': [
+        'mass_mailing',
+        'html_builder',
+        'html_editor',
+    ],
+    'data': [
+        'views/mailing_mailing_views.xml'
+    ],
+    'assets': {
+        'mass_mailing_egg.assets_builder': [  # equivalent html_builder.assets in website_builder_action.xml
+            # lazy builder assets NOT applied in iframe
+            ('include', 'html_builder.assets'),
+            'mass_mailing_egg/static/src/builder/**/*',
+        ],
+        # TODO EGGMAIL: evaluate if necessary to have interactions for mass_mailing
+        # 'mass_mailing_egg.assets_iframe_core': [  # web.assets_frontend lite, minimal env to spawn interactions
+        #     # minimal JS assets required to view the mail content
+        # ],
+        'mass_mailing_egg.assets_iframe_style': [  # equivalent website.inside_builder_style in website_builder_action.js
+            # minimal style assets required to view the mail content
+            # convert_inline ONLY uses this and inline styles.
+            #
+            # TODO EGGMAIL: recreate a scss bundle that is strictly used in relation with convert_inline
+            # if there is "editor style" to include, put it in `mass_mailing_egg.assets_iframe_edit``
+            ### CSS part of wysiwyg_iframe_editor_assets
+
+            ### iframe_css_assets_edit fast selection
+            ('include', 'mass_mailing.iframe_css_assets_edit'),
+            ('include', 'web_editor.wysiwyg_iframe_editor_assets'),
+            ('include', 'html_builder.inside_builder_style'),
+            'mass_mailing_egg/static/src/iframe_assets/**/*',
+            'mass_mailing_egg/static/src/theme_assets/**/*',
+        ],
+        'mass_mailing_egg.assets_iframe_edit': [  # equivalent html_builder.assets_edit_frontend in website_builder_action.js
+            # JS and style assets required to edit the mail content
+        ],
+        'mass_mailing_egg.assets_iframe_dark': [  # separated complement of assets_iframe_style for dark mode
+            # style assets for dark mode. Not used by convert_inline.
+            # TODO EGGMAIL: investigate how this can behave properly with convert_inline (i.e. user chooses pretty colors for
+            # dark mode, but these colors are not pretty when the dark mode is not there anymore after sending the mail).
+        ],
+        'web.assets_backend': [
+            'mass_mailing_egg/static/src/fields/**/*',
+            'mass_mailing_egg/static/src/themes/**/*',
+            'mass_mailing_egg/static/src/mobile_preview_dialog/**/*',
+            'mass_mailing_egg/static/src/iframe/**/*',
+        ],
+        'web.assets_unit_tests': [
+            # 'mass_mailing_egg/static/tests/**/*',
+        ],
+    },
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
---
+++ addons/mass_mailing_egg/static/src/builder/plugins/block_alignment_option_plugin.js
@@ -0,0 +1,18 @@
+import { Plugin } from "@html_editor/plugin";
+import { registry } from "@web/core/registry";
+
+class BlockAlignmentOptionPlugin extends Plugin {
+    static id = "mass_mailing.BlockAlignmentOption";
+    resources = {
+        builder_options: [
+            {
+                template: "html_builder.BlockAlignmentOption",
+                selector: ".s_mail_alert .s_alert, .s_mail_blockquote, .s_mail_text_highlight"
+            }
+        ]
+    }
+}
+
+registry
+    .category("mass_mailing-plugins")
+    .add(BlockAlignmentOptionPlugin.id, BlockAlignmentOptionPlugin);
