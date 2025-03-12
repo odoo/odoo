5501960a2cb83529898a9dca4bfbaaedb79affcd
Xavier Bol (xbo) <xbo@odoo.com>
2025-03-12 09:51:58 +0000

    [FIX] project: only compute duration tracking when project stage feature enabled
    Before this commit, a traceback is occurred when the user would like to
    see the raw data of a specific project and the project stage feature is
    disabled.
    
    This commit adds a group on `duration_tracking` field definition to be
    sure this field will only be computed when  the project stage feature is
    enabled.
    
    Steps to reproduce the issue:
    ----------------------------
    
    - Project > Setting of a one project
    - debug mode on
    - click on the bug > data
    
    Current behavior:
    ----------------
    
    Traceback: ValueError: Field “stage_id” on model “project.project” must be of type Many2one and have tracking=True for the computation of duration
    
    Expected behavior:
    -----------------
    
    The user should see the raw data without any issue.
    
    opw-3709542
    
    Closes #197321
    
    X-original-commit: 1ea4f285aba925d437a5129cd36143d9987c6e6a

--- addons/project/models/project_project.py
+++ addons/project/models/project_project.py
@@ -167,6 +167,7 @@ class ProjectProject(models.Model):
     # Not `required` since this is an option to enable in project settings.
     stage_id = fields.Many2one('project.project.stage', string='Stage', ondelete='restrict', groups="project.group_project_stages",
         tracking=True, index=True, copy=False, default=_default_stage_id, group_expand='_read_group_expand_full')
+    duration_tracking = fields.Json(groups="project.group_project_stages")
 
     update_ids = fields.One2many('project.update', 'project_id', export_string_translation=False)
     update_count = fields.Integer(compute='_compute_total_update_ids', export_string_translation=False)
