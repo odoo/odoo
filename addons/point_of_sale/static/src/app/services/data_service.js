e8d702615b836d579674811d1ea98fb1e7a5b870
Stephane Vanmeerhaeghe (stva) <stva@odoo.com>
2025-03-13 16:43:24 +0100

    [FIX] point_of_sale: prevent IndexedDB errors on local data access
    Previously, tables defined in DataServiceOptions but missing from relations were not created, causing failures when reading local data via `getLocalDataFromIndexedDB`.
    Additionally, empty delete or add operations triggered unnecessary transactions.
    
    This fix addresses these issues by:
    - Ensuring all missing tables are correctly created.
    - Avoiding unnecessary transactions caused by empty delete/add operations.
    
    closes odoo/odoo#201690
    
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- addons/point_of_sale/static/src/app/services/data_service.js
+++ addons/point_of_sale/static/src/app/services/data_service.js
@@ -101,7 +101,10 @@ export class PosData extends Reactive {
         // This method initializes indexedDB with all models loaded into the PoS. The default key is ID.
         // But some models have another key configured in data_service_options.js. These models are
         // generally those that can be created in the frontend.
-        const models = Object.keys(relations).map((model) => {
+        const allModelNames = Array.from(
+            new Set([...Object.keys(relations), ...Object.keys(this.opts.databaseTable)])
+        );
+        const models = allModelNames.map((model) => {
             const key = this.opts.databaseTable[model]?.key || "id";
             return [key, model];
         });
