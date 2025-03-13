a93148835c497d92f4e8eb69a5e7cd3750db3765
Anton Romanova (roan) <roan@odoo.com>
2025-03-13 14:43:14 +0000

    [FIX] account_peppol: ignore archived edi users
    In lots of places within peppol we suppose that we only have one edi
    user, but we can end up in a situation where this is not the case by
    archiving a user. In some cases, where there is `active_test = False`,
    we can end up in a situation with a traceback because there are multiple
    edi users if you count archived users
    
    Steps to reproduce:
    - Select company BE Company CoA
    - Open Settings > Accounting > PEPPOL Electronic Invoicing
    - Activate Electronic Invoicing (if is not activated)
    - Go to Accounting / Configuration / EDI Proxy Users
    - Select the unique record (demo2peppol)
    - Edit the id_client of the record to another one (this must need Odoo
      Studio or Odoo Inspector > Write > {"id_client": "another"})
    - Go to tree view of EDI Proxy Users
    - Archive it
    - Open Settings > Accounting > PEPPOL Electronic Invoicing
    - Remove from PEPPOL (archived record will not be deleted)
    - Activate Electronic Invoicing Again (now you have one record active
      and another not)
    - Go to Contacts > Search "BE Company CoA" > Select Contact
    - Go to Accounting Tab > Customer Invoices section > Enable eInvoice
      Format (BIS Billing 3.0)
    - Go to Contact & Addresses Tab
    - Try to add a new Contact type (error while saving)
    
    opw-4572074
    
    closes odoo/odoo#206063
    
    X-original-commit: 4c46b696f3af73c982ba92f25d71afe8fc825ed0
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>
    Signed-off-by: Anton Romanova (roan) <roan@odoo.com>

--- addons/account_edi_proxy_client/models/res_company.py
+++ addons/account_edi_proxy_client/models/res_company.py
@@ -6,4 +6,4 @@ from odoo import fields, models
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    account_edi_proxy_client_ids = fields.One2many('account_edi_proxy_client.user', inverse_name='company_id')
+    account_edi_proxy_client_ids = fields.One2many('account_edi_proxy_client.user', inverse_name='company_id', context={'active_test': True})
