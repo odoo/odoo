a963fb20ff11a8802df52d79f755189033690289
nni@odoo.com <nni@odoo.com>
2025-03-13 09:28:06 +0000

    [IMP] l10n_id_efaktur_coretax: luxury tax group error handling
    For existing customers, when coretax is installed, the new luxury tax group and its tax will not be loaded into Odoo. During XML generation, it will look for the tax group by XML ID which will not be found and hence resulting to traceback error. To mitigate this issue, we will introduce a post_init_hook to manually create the tax group and not raise exception when env.ref() doesn't find the tax group
    
    opw-4635356
    
    closes odoo/odoo#201951
    
    X-original-commit: 868f45fad1a9be21d06d2cc6a5d6c3497406708a
    Signed-off-by: Nicolas Viseur (vin) <vin@odoo.com>

--- addons/l10n_id_efaktur_coretax/models/account_move_line.py
+++ addons/l10n_id_efaktur_coretax/models/account_move_line.py
@@ -17,7 +17,9 @@ class AccountMoveLine(models.Model):
         product = self.product_id
 
         # Separate tax into the regular and luxury component
-        luxury_tax = self.tax_ids.filtered(lambda tax: tax.tax_group_id == self.env.ref(f"account.{self.company_id.id}_l10n_id_tax_group_luxury_goods"))
+        ChartTemplate = self.env['account.chart.template'].with_company(self.company_id)
+        luxury_tax_group = ChartTemplate.ref('l10n_id_tax_group_luxury_goods', raise_if_not_found=False)
+        luxury_tax = self.tax_ids.filtered(lambda tax: tax.tax_group_id == luxury_tax_group)
         regular_tax = self.tax_ids - luxury_tax
 
         line_val = {
