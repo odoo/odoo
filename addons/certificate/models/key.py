8d80c25dae76e1f471165d07e08ed046dae5552a
Antoine Boonen <aboo@odoo.com>
2025-03-13 15:22:15 +0100

    [IMP] certificate: Add certificate docstring and some robustness
    The certificate and keys models were added in 18.0. A few docstrings
    could help devs using those records.
    
    Furthermore, a few more checks and robustness were added.
    
    closes odoo/odoo#202253
    
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- addons/certificate/models/key.py
+++ addons/certificate/models/key.py
@@ -129,7 +129,17 @@ class Key(models.Model):
     # -------------------------------------------------------
 
     def _sign(self, message, hashing_algorithm='sha256', formatting='encodebytes'):
-        """ Return the base64 encoded signature of message. """
+        ''' Compute and return the message's signature.
+
+        :param str|bytes message: The message to sign
+        :param optional,default='sha256' hashing_algorithm: The digest algorithm to use. Currently, only 'sha1' and 'sha256' are available.
+        :param optional,default='encodebytes' formatting: The formatting of the returned bytes
+            - 'encodebytes' returns a base64-encoded block of 76 characters lines
+            - 'base64' returns the raw base64-encoded data
+            - other returns non-encoded data
+        :return: The formatted signature bytes of the message
+        :rtype: bytes
+        '''
         self.ensure_one()
 
         if self.public:
@@ -148,6 +158,15 @@ class Key(models.Model):
         )
 
     def _get_public_key_numbers_bytes(self, formatting='encodebytes'):
+        ''' Get the public key's public numbers bytes.
+
+        :param optional,default='encodebytes' formatting: The formatting of the returned bytes
+            - 'encodebytes' returns a base64-encoded block of 76 characters lines
+            - 'base64' returns the raw base64-encoded data
+            - other returns non-encoded data
+        :return: A tuple containing formatted public number bytes of the public key
+        :rtype: tuple(bytes,bytes)
+        '''
         self.ensure_one()
 
         return self._numbers_public_key_bytes_with_key(
@@ -156,6 +175,18 @@ class Key(models.Model):
         )
 
     def _get_public_key_bytes(self, encoding='der', formatting='encodebytes'):
+        ''' Get the public key bytes.
+
+        :param optional,default='der' encoding: The formatting of the returned bytes
+            - 'der' returns DER public key bytes
+            - other returns PEM public key bytes
+        :param optional,default='encodebytes' formatting: The formatting of the returned bytes
+            - 'encodebytes' returns a base64-encoded block of 76 characters lines
+            - 'base64' returns the raw base64-encoded data
+            - other returns non-encoded data
+        :return: The formatted public key bytes in the corresponding format
+        :rtype: bytes
+        '''
         self.ensure_one()
 
         if self.public:
@@ -173,6 +204,13 @@ class Key(models.Model):
         )
 
     def _decrypt(self, message, hashing_algorithm='sha256'):
+        ''' Decrypt the given message using the provided digest.
+
+        :param str|bytes message: The message to encode
+        :param optional,default='sha256' hashing_algorithm: The digest algorithm to use. Currently, only 'sha1' and 'sha256' are available.
+        :return: The decrypted text
+        :rtype: str
+        '''
         self.ensure_one()
 
         if not isinstance(message, bytes):
@@ -194,14 +232,30 @@ class Key(models.Model):
                 algorithm=STR_TO_HASH[hashing_algorithm],
                 label=None
             )
-        )
+        ).decode()
 
     @api.model
     def _sign_with_key(self, message, pem_key, pwd=None, hashing_algorithm='sha256', formatting='encodebytes'):
-        """ Return the base64 encoded signature of message. """
+        ''' Compute and return the message's signature for a given private key.
+
+        :param str|bytes message: The message to sign
+        :param str|bytes pem_key: A base64 encoded private key in the PEM format
+        :param str|bytes pwd: A password to decrypt the PEM key
+        :param optional,default='sha1' hashing_algorithm: The digest algorithm to use. Currently, only 'sha1' and 'sha256' are available.
+        :param optional,default='encodebytes' formatting: The formatting of the returned bytes
+            - 'encodebytes' returns a base64-encoded block of 76 characters lines
+            - 'base64' returns the raw base64-encoded data
+            - other returns non-encoded data
+        :return: The formatted signature bytes of the message
+        :rtype: bytes
+        '''
 
         if not isinstance(message, bytes):
             message = message.encode('utf-8')
+        if not isinstance(pem_key, bytes):
+            pem_key = pem_key.encode('utf-8')
+        if pwd and not isinstance(pwd, bytes):
+            pwd = pwd.encode('utf-8')
 
         if hashing_algorithm not in STR_TO_HASH:
             raise UserError(f"Unsupported hashing algorithm '{hashing_algorithm}'. Currently supported: sha1 and sha256.")
@@ -229,6 +283,19 @@ class Key(models.Model):
 
     @api.model
     def _numbers_public_key_bytes_with_key(self, pem_key, formatting='encodebytes'):
+        ''' Get the given public key's public numbers bytes.
+
+        :param str|bytes pem_key: A base64 encoded public key in the PEM format
+        :param optional,default='encodebytes' formatting: The formatting of the returned bytes
+            - 'encodebytes' returns a base64-encoded block of 76 characters lines
+            - 'base64' returns the raw base64-encoded data
+            - other returns non-encoded data
+        :return: A tuple containing the formatted public number bytes of the public key
+        :rtype: tuple(bytes,bytes)
+        '''
+        if not isinstance(pem_key, bytes):
+            pem_key = pem_key.encode('utf-8')
+
         try:
             public_key = serialization.load_pem_public_key(base64.b64decode(pem_key))
         except ValueError:
@@ -250,7 +317,14 @@ class Key(models.Model):
 
     @api.model
     def _generate_ec_private_key(self, company, name='id_ec', curve='SECP256R1'):
+        ''' Generate an elliptic curve private key.
 
+        :param res.company company: A company record
+        :param str,optional,default='id_ec' name: The name of the newly created key.
+        :param optional,default='SECP256R1' curve: The type of elliptic curve algorithm. Currently, only SECP256R1 is supported.
+        :return: A certificate.key record
+        :rtype: certificate.key
+        '''
         if curve not in STR_TO_CURVE:
             raise UserError(f"Unsupported curve algorithm '{curve}'. Currently supported: SECP256R1.")
 
@@ -267,6 +341,19 @@ class Key(models.Model):
 
     @api.model
     def _generate_rsa_private_key(self, company, name='id_rsa', public_exponent=65537, key_size=2048):
+        ''' Generate an RSA private key.
+
+        :param res.company company: A company record
+        :param str,optional,default='id_rsa' name: The name of the newly created key.
+        :param int,optional,default=65537 public_exponent: The public exponent of the new key: either 65537 or 3 (for legacy purposes)
+        :param int,optional,default=2048 key_size: The length of the modulus in bits; it is strongly recommended to be at least 2048 and must not be less than 512
+        :return: A certificate.key record
+        :rtype: certificate.key
+        '''
+        if (public_exponent not in [65537, 3]):
+            raise UserError(_("The public exponent should be 65537 (or 3 for legacy purposes)."))
+        if key_size < 512:
+            raise UserError(_("The key size should be at least 512 bytes."))
 
         private_key = rsa.generate_private_key(
             public_exponent=public_exponent,
