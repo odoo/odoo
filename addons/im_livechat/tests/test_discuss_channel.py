7144a95ec417a7657971250a7e6f2f3abcda6fc1
tsm-odoo <tsm@odoo.com>
2025-03-13 13:13:19 +0100

    [FIX] im_livechat: do not close live chat when non member unfollows
    When the last operator of a live chat closes the discussion, the live
    chat is closed. Technically, this is done with an override of the
    `_action_unfollow` method. However, this method can be called even when
    users are not members (e.g. channel pinned locally). The override does
    not take this into account which can lead to the chat being closed while
    an operator is still present. This PR fixes the issue.
    
    closes odoo/odoo#201439
    
    X-original-commit: 35c396efbf786e39f62429821a81f9f30f91f17f
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

---
+++ addons/im_livechat/tests/test_discuss_channel.py
@@ -0,0 +1,23 @@
+from odoo.tests import new_test_user, tagged
+from odoo.addons.im_livechat.tests.common import TestImLivechatCommon
+
+
+@tagged("-at_install", "post_install")
+class TestDiscussChannel(TestImLivechatCommon):
+    def test_unfollow_from_non_member_does_not_close_livechat(self):
+        bob_user = new_test_user(
+            self.env, "bob_user", groups="base.group_user,im_livechat.im_livechat_group_manager"
+        )
+        data = self.make_jsonrpc_request(
+            "/im_livechat/get_session",
+            {
+                "channel_id": self.livechat_channel.id,
+                "anonymous_name": "Visitor",
+            },
+        )
+        chat = self.env["discuss.channel"].browse(data["discuss.channel"][0]["id"])
+        self.assertTrue(chat.livechat_active)
+        chat.with_user(bob_user).action_unfollow()
+        self.assertTrue(chat.livechat_active)
+        chat.with_user(chat.livechat_operator_id.user_ids[0]).action_unfollow()
+        self.assertFalse(chat.livechat_active)
