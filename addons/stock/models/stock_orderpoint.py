1277eb898320ba2224bf8c8f04414274e68b0a44
Aurelien MARIE (auma) <auma@odoo.com>
2025-03-13 13:49:17 +0000

    [PERF] stock: speedup get_orderpoint_action
    Description:
    
    When opening the replenishment view, the locations are checked to
    find if some products need to be refilled. If one product needs
    to be refilled, it will check if an orderpoint already exists,
    otherwise, it will create it. The issue is that the checks are
    currently done on all the orderpoints, even the ones not related
    to the product. The performances are worsened by the compute on
    `qty_to_order` triggered on all the orderpoints.
    
    Fix:
    
    To fix that, we can add a leaf to the domain so that we only
    retrieve the orderpoints related to the products that need to
    be refilled. This will reduce the number of records on which we
    call the `qty_to_order` compute.
    
    Benchmark (in 18):
    
    | # of orderpoint | Before | After |
    | --------------- | ------ | ----- |
    |           44145 |   6:52 |    6s |
    |           22145 |   3:38 |    6s |
    
    Reference:
    
    opw-4618887
    
    closes odoo/odoo#203401
    
    X-original-commit: 8400fae03a45ae772d0c86df4613c2f251c9b11f
    Signed-off-by: Tiffany Chang (tic) <tic@odoo.com>
    Signed-off-by: Aurélien Marie (auma) <auma@odoo.com>

--- addons/stock/models/stock_orderpoint.py
+++ addons/stock/models/stock_orderpoint.py
@@ -492,7 +492,7 @@ class StockWarehouseOrderpoint(models.Model):
         rounding = self.env['decimal.precision'].precision_get('Product Unit')
         # Group orderpoint by product-location
         orderpoint_by_product_location = self.env['stock.warehouse.orderpoint']._read_group(
-            [('id', 'in', orderpoints.ids)],
+            [('id', 'in', orderpoints.ids), ('product_id', 'in', product_ids)],
             ['product_id', 'location_id'],
             ['id:recordset'])
         orderpoint_by_product_location = {
@@ -511,7 +511,7 @@ class StockWarehouseOrderpoint(models.Model):
 
         # With archived ones to avoid `product_location_check` SQL constraints
         orderpoint_by_product_location = self.env['stock.warehouse.orderpoint'].with_context(active_test=False)._read_group(
-            [('id', 'in', orderpoints.ids)],
+            [('id', 'in', orderpoints.ids), ('product_id', 'in', product_ids)],
             ['product_id', 'location_id'],
             ['id:recordset'])
         orderpoint_by_product_location = {
