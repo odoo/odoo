aa3a7aeab6bd1d8d18fd3b2398e00e4295cd1b90
rare-odoo <rare@odoo.com>
2025-03-15 22:54:54 +0530

    [REF] mrp: refactor stock forecast data supporting multi-warehouses
    This commit refactors the data preparation logic in mrp to accommodate
    multiple warehouses. It aligns with the primary commit by formatting the
    data based on warehouse-specific information

--- addons/mrp/report/stock_forecasted.py
+++ addons/mrp/report/stock_forecasted.py
@@ -1,7 +1,4 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import api, models
+from odoo import models
 
 
 class StockForecasted_Product_Product(models.AbstractModel):
@@ -22,28 +19,68 @@ class StockForecasted_Product_Product(models.AbstractModel):
         out_domain += [('raw_material_production_id', '=', False)]
         return in_domain, out_domain
 
-    def _get_report_header(self, product_template_ids, product_ids, wh_location_ids):
-        res = super()._get_report_header(product_template_ids, product_ids, wh_location_ids)
-        res['draft_production_qty'] = {}
+    def _get_report_data(self, product_template_ids=False, product_ids=False):
+        res = super()._get_report_data(product_template_ids, product_ids)
+        mo_warehouse_data = self._get_draft_production_data(product_template_ids, product_ids, res['warehouse_view_locations'])
+        if res['multiple_warehouses']:
+            for warehouse in res['warehouses']:
+                warehouse['draft_production_qty'] = {
+                    'in': 0.0,
+                    'out': 0.0
+                }
+                warehouse_data = mo_warehouse_data.get(warehouse['id'])
+                if warehouse_data:
+                    warehouse['draft_production_qty'].update({
+                        'in': warehouse_data['production_qty_in'],
+                        'out': warehouse_data['production_qty_out']
+                    })
+
+                    # update total quantities
+                    warehouse['qty']['in'] += warehouse['draft_production_qty']['in']
+                    warehouse['qty']['out'] += warehouse['draft_production_qty']['out']
+        else:
+            warehouse_id = res['warehouses'][0]['id']
+            warehouse_data = mo_warehouse_data.get(warehouse_id, {})
+            res['draft_production_qty'] = {
+                'in': warehouse_data.get('production_qty_in', 0.0),
+                'out': warehouse_data.get('production_qty_out', 0.0)
+            }
+            res['qty']['in'] += res['draft_production_qty']['in']
+            res['qty']['out'] += res['draft_production_qty']['out']
+        return res
+
+    def _get_draft_production_data(self, product_template_ids=False, product_ids=False, wh_locations=False):
+        """
+        Draft production data grouped by warehouse
+        :return: Dictionary mapping warehouse_id to draft production data
+        :rtype: dict[int, dict[float]]
+        """
         domain = self._product_domain(product_template_ids, product_ids)
         domain += [('state', '=', 'draft')]
 
         # Pending incoming quantity.
-        mo_domain = domain + [('location_dest_id', 'in', wh_location_ids)]
-        [product_qty] = self.env['mrp.production']._read_group(mo_domain, aggregates=['product_qty:sum'])[0]
-        res['draft_production_qty']['in'] = product_qty or 0.0
+        mo_domain = domain + [('location_dest_id', 'in', wh_locations)]
+        grouped_in_data = dict(
+            self.env['mrp.production']._read_group(mo_domain, ['warehouse_id'], aggregates=['product_qty:sum'])
+        )
 
         # Pending outgoing quantity.
         move_domain = domain + [
             ('raw_material_production_id', '!=', False),
-            ('location_id', 'in', wh_location_ids),
+            ('location_id', 'in', wh_locations),
         ]
-        [product_qty] = self.env['stock.move']._read_group(move_domain, aggregates=['product_qty:sum'])[0]
-        res['draft_production_qty']['out'] = product_qty or 0.0
-        res['qty']['in'] += res['draft_production_qty']['in']
-        res['qty']['out'] += res['draft_production_qty']['out']
+        grouped_out_data = dict(
+            self.env['stock.move']._read_group(move_domain, ['warehouse_id'], aggregates=['product_qty:sum'])
+        )
+        all_warehouse_ids = set(grouped_in_data.keys()) | set(grouped_out_data.keys())
 
-        return res
+        production_data_by_warehouse = {}
+        for warehouse in all_warehouse_ids:
+            production_data_by_warehouse[warehouse.id] = {
+                'production_qty_in': grouped_in_data.get(warehouse, 0.0),
+                'production_qty_out': grouped_out_data.get(warehouse, 0.0),
+            }
+        return production_data_by_warehouse
 
     def _get_reservation_data(self, move):
         if move.production_id:
