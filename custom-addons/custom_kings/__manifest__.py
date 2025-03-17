c5c7262d2b020b97df24addcefd857d4429f3622
Senat Selimoski <senkiselimoski@gmail.com>
2025-03-17 01:16:24 +0100

    added custom kings module

---
+++ custom-addons/custom_kings/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Custom Kings Module",
+    "version": "1.0",
+    "summary": "Custom module for APIS client",
+    "description": "Adds custom styles to the APIS user interface.",
+    "category": "Inventory",
+    'website': 'https://ultrasoft.mk',
+    'author': 'Ultrasoft Systems',
+    'depends': ['account'],
+    'data': [
+        'views/custom_report_invoice.xml'
+    ],
+    "installable": True,
+    'application': False,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
