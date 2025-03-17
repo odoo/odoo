keyboard-slayer/odoo-eval

[IMP] mail: prettify message links
PURPOSE:

Currently, internal links that redirect to messages are displayed as raw URLs,
which makes them hard to read and understand. This commit aims to enhance
the user experience by improving the display of these message links.

SPECIFICATION:

Message links should be presented in a more user-friendly format,
displaying the `Thread name > {Comment Icon}`,
similar to how other platforms like Discord handle message links.

task-4095268

addons/mail/static/tests/message/message.test.js
@@ -1923,5 +1923,44 @@ test("Copy Message Link", async () => {
     await assertSteps([url(`/mail/message/${messageId_2}`)]);
     press(["ctrl", "v"]);
     await click(".o-mail-Composer-send:enabled");
-    await contains(".o-mail-Message", { text: url(`/mail/message/${messageId_2}`) });
+    await contains(".o-mail-Message", { text: "channel1" });
+});
+
+test("Prettify message links", async () => {
+    const pyEnv = await startServer();
+    const [channelId_1, channelId_2] = pyEnv["discuss.channel"].create([
+        { name: "channel1" },
+        { name: "channel2", channel_member_ids: [], channel_type: "chat" },
+    ]);
+    const partnerId = pyEnv["res.partner"].create({
+        email: "testpartner@odoo.com",
+        name: "TestPartner",
+    });
+    const [messageId_1, messageId_2] = pyEnv["mail.message"].create([
+        {
+            body: "Message on joined channel",
+            res_id: partnerId,
+            model: "res.partner",
+        },
+        {
+            body: "Message on non accessible channel",
+            res_id: channelId_2,
+            model: "discuss.channel",
+        },
+    ]);
+    onRpcBefore("/mail/data", async (args) => {
+        if (args.messages) {
+            await assertSteps(["before_fetchdata"]);
+        }
+    });
+    await start();
+    await openDiscuss(channelId_1);
+    await insertText(
+        ".o-mail-Composer-input",
+        `${url(`/mail/message/${messageId_1}`)} ${url(`/mail/message/${messageId_2}`)}`
+    );
+    await click(".o-mail-Composer-send:enabled");
+    await contains(".o-mail-Message", { text: "Unknown   Unknown" });
+    step("before_fetchdata");
+    await contains(".o-mail-Message", { text: "TestPartner   Unknown" });
 });

odoo/odoo

addons/mail/static/tests/message/message.test.js
@@ -1989,5 +1989,34 @@ test("Copy Message Link", async () => {
     await waitForSteps([url(`/mail/message/${messageId_2}`)]);
     await press(["ctrl", "v"]);
     await press("Enter");
-    await contains(".o-mail-Message", { text: url(`/mail/message/${messageId_2}`) });
+    await contains(".o-mail-Message", { text: "channel1" });
+});
+
+test("Prettify message links", async () => {
+    const pyEnv = await startServer();
+    const channelId = pyEnv["discuss.channel"].create({ name: "channel1" });
+    const partnerId = pyEnv["res.partner"].create({
+        email: "testpartner@odoo.com",
+        name: "TestPartner",
+    });
+    const messageId_1 = pyEnv["mail.message"].create({
+        body: "Message on joined channel",
+        res_id: partnerId,
+        model: "res.partner",
+    });
+    onRpcBefore("/mail/data", async (args) => {
+        if (args.fetch_params.find((param) => Array.isArray(param) && param[0] === "messages")) {
+            await waitForSteps(["before_fetchdata"]);
+        }
+    });
+    await start();
+    await openDiscuss(channelId);
+    await insertText(
+        ".o-mail-Composer-input",
+        `${url(`/mail/message/${messageId_1}`)} ${url(`/mail/message/100`)}`
+    );
+    await press("Enter");
+    await contains(".o-mail-Message", { text: "Unknown Unknown" });
+    asyncStep("before_fetchdata");
+    await contains(".o-mail-Message", { text: "TestPartner Unknown" });
 });