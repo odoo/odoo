8d112ba9fbf0b76801e24cf6023aa6406d933cbc
Parth Solanki[PASO] <paso@odoo.com>
2025-03-17 19:40:47 +0530

    [FIX] point_of_sale: save data while terminal installation
    In this commit, save the record when we install the payment terminal from
    the form widget.
    
    Steps:
    - Go to payment method config 
    - Click on new 
    - Journal = bank & Integration = terminal 
    - Select a payment provider to download
    - the page refreshes and loses the data that is configured
    
    task - 4653445

--- addons/point_of_sale/static/src/backend/pos_payment_provider_cards/pos_payment_provider_cards.js
+++ addons/point_of_sale/static/src/backend/pos_payment_provider_cards/pos_payment_provider_cards.js
@@ -17,6 +17,7 @@ export class PosPaymentProviderCards extends Component {
         this.container = useRef("cards_container");
         this.state = useState({
             providers: [],
+            disabled: false,
         });
 
         onWillStart(async () => {
@@ -51,13 +52,22 @@ export class PosPaymentProviderCards extends Component {
     }
 
     async installModule(moduleId) {
-        const result = await this.orm.call("ir.module.module", "button_immediate_install", [
-            moduleId,
-        ]);
-
-        if (result) {
-            window.location.reload();
+        const recordSave = await this.props.record.save();
+        if (!recordSave) {
+            return;
         }
+        this.state.disabled = true;
+        await this.orm
+            .call("ir.module.module", "button_immediate_install", [moduleId])
+            .then((result) => {
+                this.state.disabled = false;
+                if (result) {
+                    window.location.reload();
+                }
+            })
+            .finally(() => {
+                this.state.disabled = false;
+            });
     }
 
     async setupProvider(moduleId) {
