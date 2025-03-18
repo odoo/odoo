7c2722cc9b1e5efa4240ef2736164d425480ddfb
gteboul <gute@odoo.com>
2025-03-18 10:17:31 +0000

    [FIX] account, sale_stock: introduce invoiced lot check hook
    This commit introduces a hook filter method that allows to filter the amls taken into account in _get_invoiced_lot_values (verify the quantity of serial product previously delivered).
    
    opw-4644031
    
    X-original-commit: 3ad0d581bb7536d13e878c4fc00c8a4c4545b398

--- addons/sale_stock/models/account_move.py
+++ addons/sale_stock/models/account_move.py
@@ -38,7 +38,7 @@ class AccountMove(models.Model):
             return res
 
         current_invoice_amls = self.invoice_line_ids.filtered(lambda aml: aml.display_type == 'product' and aml.product_id and aml.product_id.type == 'consu' and aml.quantity)
-        all_invoices_amls = current_invoice_amls.sale_line_ids.invoice_lines.filtered(lambda aml: aml.move_id.state == 'posted').sorted(lambda aml: (aml.date, aml.move_name, aml.id))
+        all_invoices_amls = current_invoice_amls.sale_line_ids.invoice_lines.filtered(lambda aml: aml._filter_aml_lot_valuation()).sorted(lambda aml: (aml.date, aml.move_name, aml.id))
         index = all_invoices_amls.ids.index(current_invoice_amls[:1].id) if current_invoice_amls[:1] in all_invoices_amls else 0
         previous_amls = all_invoices_amls[:index]
         invoiced_qties = current_invoice_amls._get_invoiced_qty_per_product()
