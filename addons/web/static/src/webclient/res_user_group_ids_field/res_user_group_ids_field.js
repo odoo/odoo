a34ba39cb8ed0358b4bea4b8cd9f2a08bf6ebe1b
Gorash <chm@odoo.com>
2025-03-18 15:01:33 +0100

    [IMP] base: new widget to display res.users access rights & groups

--- addons/web/static/src/webclient/res_user_group_ids_field/res_user_group_ids_field.js
+++ addons/web/static/src/webclient/res_user_group_ids_field/res_user_group_ids_field.js
@@ -1,3 +1,4 @@
+import { localization } from "@web/core/l10n/localization";
 import { x2ManyCommands } from "@web/core/orm_service";
 import { registry } from "@web/core/registry";
 import { parseXML } from "@web/core/utils/xml";
@@ -5,8 +6,187 @@ import { Record } from "@web/model/record";
 import { standardFieldProps } from "@web/views/fields/standard_field_props";
 import { FormArchParser } from "@web/views/form/form_arch_parser";
 import { FormRenderer } from "@web/views/form/form_renderer";
+import { SelectionField } from "@web/views/fields/selection/selection_field";
+import { useService } from "@web/core/utils/hooks";
+import { _t } from "@web/core/l10n/translation";
 
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
+
+
+class ResGroupsInformationPopover extends Component {
+    static template = "web.ResGroupsInformationPopover";
+    static props = {
+        close: Function,
+        group: Object,
+        groups: Object,
+        disjoints: Array,
+        selectedIds: Array,
+        onClose: Function,
+    };
+
+    setup() {
+        this.group = this.props.group;
+        const privilege = this.group.privilege;
+        const groups = this.props.groups;
+
+        let implies = this.group.all_implied_ids
+            .map((g) => groups[g])
+            .filter((g) => this.group.id !== g.id && (!privilege || g.privilege?.id !== privilege.id))
+            .sort((a, b) => !a.privilege - !b.privilege);
+        implies = implies.filter((g) => !g.privilege || !implies.find((group) => group.privilege?.id === g.privilege?.id && group.level > g.level));
+
+        const others = this.props.selectedIds
+            .filter((gId) => this.group.id !== gId && !this.group.all_implied_by_ids.includes(gId))
+            .map((gId) => groups[gId].all_implied_ids)
+            .flat()
+            .map((gId) => groups[gId]);
+
+        this.impliesFromCurrent = implies.filter((g) => !others.includes(g));
+        this.implies = implies.filter((g) => others.includes(g));
+        this.impliedBy = this.group.all_implied_by_ids
+                .map((g) => groups[g])
+                .filter((g) => g.id !== this.group.id && this.props.selectedIds.includes(g.id) && (!privilege || g.privilege.id !== privilege.id));
+
+        this.state = useState({
+            showGroupWithoutPrivilege: false,
+        });
+    }
+
+    showExtraGroups () {
+        this.state.showGroupWithoutPrivilege = true;
+    }
+
+}
+
+class ResUserGroupIdsSelectionField extends SelectionField {
+    static template = "web.ResUserGroupIdsSelectionField";
+
+    setup() {
+        super.setup();
+        this.popover = useService("popover");
+        this.groupHierarchy = this.props.record.data.groupHierarchy;
+        this.isDebug = odoo.debug;
+    }
+
+    get string() {
+        const value = this.value;
+        return this.options.find((o) => o[0] === value)?.[1] || '';
+    }
+
+    get stringImpliedBy() {
+        const groups = this.impliedBy.map((g) => (g.privilege ? g.privilege?.name + '/' : '') + g.name);
+        return this.implied.name + ' (' + _t("implied by: %s", groups.join(', ')) + ')';
+    }
+
+    get selectedIds() {
+        return Object.values(this.props.record.data).filter((value) => value && typeof value === 'number');
+    }
+
+    get group() {
+        return this.groupHierarchy.getGroups()[this.value] || false;
+    }
+
+    get implied() {
+        const selectedIds = this.selectedIds;
+        const groups = this.groupHierarchy.getGroups();
+        const implied = this.options.findLast((o) => o[0] && groups[o[0]].all_implied_by_ids.find((gId) => gId !== o[0] && selectedIds.includes(gId)))?.[0] || false;
+        return groups[implied];
+    }
+
+    get infoGroup () {
+        return this.exclusivelyImplied || this.group;
+    }
+
+    get exclusivelyImplied () {
+        const implied = this.implied;
+        const group = this.group;
+        if (implied && (!group || implied.level > group.level)) {
+            return implied;
+        }
+        return false;
+    }
+
+    get impliedBy() {
+        const implied = this.implied;
+        if (!implied) {
+            return false;
+        }
+        const selectedIds = this.selectedIds;
+        const groups = this.groupHierarchy.getGroups();
+        const impliedBy = implied.all_implied_by_ids
+                .map((g) => groups[g])
+                .filter((g) => g.id !== implied.id && selectedIds.includes(g.id) && (!implied.privilege_id || g.privilege_id !== implied.privilege_id));
+        return impliedBy;
+    }
+
+    get disjoints() {
+        const group = this.implied || this.group;
+        if (!group) {
+            return false;
+        }
+        const groups = this.groupHierarchy.getGroups();
+        const selectedIds = this.selectedIds;
+        const disjoint_ids = group.disjoint_ids
+            .map((gId) => group.id === gId ? [] : groups[gId].all_implied_by_ids)
+            .flat()
+            .filter((gId) => selectedIds.includes(gId));
+        if (disjoint_ids.length) {
+            return disjoint_ids.map((gId) => groups[gId]);
+        }
+        return false;
+    }
+
+    onClickPlaceholder () {
+        if (this.props.options.length === 1) {
+            this.onClickSwitch();
+        }
+    }
+
+    onClickSwitch() {
+        const value = this.props.record.data[this.props.name] ? false : this.options[0][0];
+        this.props.record.update(
+            { [this.props.name]: value },
+            { save: this.props.autosave }
+        );
+    }
+
+    onClickInfoButton(ev) {
+        const close = () => {
+            this.popoverCloseFn();
+            this.popoverCloseFn = null;
+        }
+
+        if (this.popoverCloseFn) {
+            close();
+        }
+
+        this.popoverCloseFn = this.popover.add(
+            ev.currentTarget,
+            ResGroupsInformationPopover,
+            {
+                group: this.infoGroup,
+                groups: this.groupHierarchy.getGroups(),
+                disjoints: this.disjoints || [],
+                selectedIds: this.selectedIds,
+                onClose: close,
+            },
+            {
+                closeOnClickAway: true,
+                position: localization.direction === "rtl" ? "left" : "right",
+            },
+        );
+    }
+}
+
+class ResUserGroupIdsSwitchField extends ResUserGroupIdsSelectionField {
+    static template = "web.ResUserGroupIdsSwitchField";
+
+    get stringImpliedBy() {
+        const groups = this.impliedBy.map((g) => (g.privilege ? g.privilege?.name + '/' : '') + g.name);
+        return _t('Yes') + ' (' + _t("implied by: %s", groups.join(', ')) + ')';
+    }
+
+}
 
 /**
  * This widget is only used for the 'group_ids' field of the 'res.users' form view,
@@ -18,63 +198,100 @@ export class ResUserGroupIdsField extends Component {
     static props = { ...standardFieldProps };
 
     setup() {
-        this.sections = this.props.record.data.view_group_hierarchy;
-        this.categories = this.sections.map((section) => section.categories).flat();
-        this.groupIdsInCategories = Object.values(this.categories)
-            .map((c) => c.groups.map((g) => g[0]))
-            .flat();
+        const {groups, sections} = JSON.parse(JSON.stringify(this.props.record.data.view_group_hierarchy));
+        this.groupHierarchy = {
+            getGroups: () => groups,
+            getSections: () => sections,
+        };
 
-        this.fields = {};
-        for (const category of this.categories) {
-            this.fields[this.getFieldName(category)] = {
-                type: "selection",
-                string: category.name,
-                selection: [[false, ""]].concat(category.groups),
-                help: category.description,
-            };
+        for (const section of sections) {
+            for (const privilege of section.privileges) {
+                for (const gId of privilege.group_ids) {
+                    groups[gId].privilege = privilege;
+                }
+            }
         }
+
+        sections.push({
+            id: 'extra',
+            name: _t('Extra Rights'),
+            privileges: Object.values(groups)
+                .filter((group) => !group.privilege_id)
+                .map((group) => Object.assign({
+                    id: 'group_' + group.id,
+                    name: group.name,
+                    description: '',
+                    group_ids: [group.id],
+                })),
+        });
+
+        this.fields = {groupHierarchy: {type: "json"}};
+        for (const section of sections) {
+            for (const privilege of section.privileges) {
+                this.fields[this.getFieldName(privilege)] = {
+                    type: "selection",
+                    string: privilege.name,
+                    selection: [[0, ""]].concat(
+                                section.id === 'extra' ?
+                                [[privilege.group_ids[0], _t('Yes')]] :
+                                privilege.group_ids.map((gId) => [gId, groups[gId].name])
+                    ),
+                    help: privilege.description,
+                };
+            }
+        }
+
         const models = { main: { fields: this.fields } };
         const arch = `
             <t>
                 <group>
-                    ${this.sections.map(
+                    ${sections.map(
                         (section) => `
-                        <group string="${section.name}">
-                            ${section.categories.map((cat) => `<field name="field_${cat.id}"/>`)}
+                        <group string="${section.name}" ${section.id === 'extra' ? ' colspan="4" col="4" ' + (!odoo.debug ? ' invisible="1" ' : '') : ''}>
+                            ${section.privileges.map((cat) => `
+                                <field name="${this.getFieldName(cat)}" widget="res_user_group_ids_field_${section.id === 'extra' ? 'switch' : 'selection'}"/>`
+                            ).join('')}
                         </group>`
-                    )}
+                    ).join('')}
                 </group>
             </t>`;
         this.archInfo = new FormArchParser().parse(parseXML(arch), models, "main");
     }
 
     get values() {
-        const values = {};
+        const values = { groupHierarchy: this.groupHierarchy };
         const ids = this.props.record.data.group_ids.currentIds;
-        for (const category of this.categories) {
-            values[this.getFieldName(category)] =
-                category.groups.findLast((g) => ids.includes(g[0]))?.[0] || false;
+        for (const section of this.groupHierarchy.getSections()) {
+            for (const privilege of section.privileges) {
+                values[this.getFieldName(privilege)] = privilege.group_ids.find((gId) => ids.includes(gId)) || false;
+            }
         }
         return values;
     }
 
-    getFieldName(category) {
-        return `field_${category.id}`;
+    getFieldName(privilege) {
+        return `field_${privilege.id}`;
     }
 
     onRecordChanged(_, values) {
-        const groupIds = Object.values(values).filter((groupId) => groupId);
-        const groupIdsNotInCategories = this.props.record.data.group_ids.currentIds.filter(
-            (id) => !this.groupIdsInCategories.includes(id)
-        );
-        const allGroupIds = groupIdsNotInCategories.concat(groupIds);
-        return this.props.record.update({ group_ids: [x2ManyCommands.set(allGroupIds)] });
+        const selectedIds = Object.values(values).filter((value) => value && typeof value === 'number');
+        return this.props.record.update({ group_ids: [x2ManyCommands.set(selectedIds)] });
     }
 }
+
+const resUserGroupIdsSelectionField = {
+    component: ResUserGroupIdsSelectionField,
+};
+
+const resUserGroupIdsSwithField = {
+    component: ResUserGroupIdsSwitchField,
+};
 
 export const resUserGroupIdsField = {
     component: ResUserGroupIdsField,
     fieldDependencies: [{ name: "view_group_hierarchy", type: "json", readonly: true }],
 };
 
+registry.category("fields").add("res_user_group_ids_field_selection", resUserGroupIdsSelectionField);
+registry.category("fields").add("res_user_group_ids_field_switch", resUserGroupIdsSwithField);
 registry.category("fields").add("res_user_group_ids", resUserGroupIdsField);
--- odoo/addons/base/models/res_users_view.py
+++ odoo/addons/base/models/res_users_view.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models, tools
 
 class ResGroups(models.Model):
     _inherit = 'res.groups'
-    _order = 'category_id,sequence,name'
+    _order = 'category_id,sequence,name,id'
 
     sequence = fields.Integer(string='Sequence')
     visible = fields.Boolean(related='category_id.visible', readonly=True)
@@ -13,51 +13,45 @@ class ResGroups(models.Model):
     @api.model
     @tools.ormcache()
     def _get_view_group_hierarchy(self):
-        return [
-            {
-                'id': section.id,
-                'name': section.name,
-                'categories': [
-                    {
-                        'id': category.id,
-                        'name': category.name,
-                        'description': category.description,
-                        'groups': [[group.id, group.name]
-                                   for group in category.group_ids.sorted(lambda g: (len(g.all_implied_ids & category.group_ids), g.sequence, g.id))]
-                    } for category in section.child_ids.sorted(lambda c: c.sequence) if category.group_ids
-                ]
-            } for section in self.env['ir.module.category'].search([('parent_id', '=', False), ('child_ids.group_ids', '!=', False)], order="sequence")
-        ]
+        sections = self.env['ir.module.category'].search([('parent_id', '=', False), ('child_ids.group_ids', '!=', False)], order="sequence")
+        privileges = sections.child_ids.filtered(lambda privilege: privilege.group_ids)
+
+        return {
+            'groups': {
+                group.id: {
+                    'id': group.id,
+                    'name': group.name,
+                    'comment': group.comment,
+                    'level': (len(group.all_implied_ids & group.category_id.group_ids) if group.category_id else 0) + group.sequence/1000,
+                    'privilege_id': group.category_id.id if group.category_id in privileges else False,
+                    'disjoint_ids': group.disjoint_ids.ids,
+                    'implied_ids': group.implied_ids.ids,
+                    'all_implied_ids': group.all_implied_ids.ids,
+                    'all_implied_by_ids': group.all_implied_by_ids.ids,
+                }
+                for group in self.search([])
+            },
+            'sections': [
+                {
+                    'id': section.id,
+                    'name': section.name,
+                    'privileges': [
+                        {
+                            'id': privilege.id,
+                            'name': privilege.name,
+                            'description': privilege.description,
+                            'group_ids': [group.id for group in privilege.group_ids.sorted(lambda g: (len(g.all_implied_ids & privilege.group_ids), g.sequence, g.id))]
+                        } for privilege in section.child_ids.sorted(lambda c: c.sequence) if privilege.group_ids
+                    ]
+                } for section in sections
+            ]
+        }
 
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    # For "classic" administrators
-
     def _default_view_group_hierarchy(self):
         return self.env['res.groups']._get_view_group_hierarchy()
 
     view_group_hierarchy = fields.Json(string='Technical field for user group setting', store=False, default=_default_view_group_hierarchy)
-
-    # For "technical" administrators
-
-    view_disjoint_group_ids = fields.Many2many('res.groups', compute='_compute_view_implied_group_ids', string="Disjoint groups")
-    view_all_disjoint_group_ids = fields.Many2many('res.groups', compute='_compute_view_implied_group_ids', string="All disjoint groups")
-    view_visible_implied_group_ids = fields.Many2many('res.groups', compute='_compute_view_implied_group_ids', string="Groups added automatically")
-    view_show_technical_groups = fields.Boolean(string="Show technical groups", store=False)
-
-    @api.depends('group_ids', 'view_show_technical_groups')
-    def _compute_view_implied_group_ids(self):
-        group_definitions = self.env['res.groups']._get_group_definitions()
-
-        for user in self:
-            view_disjoint_group_ids = user.group_ids.disjoint_ids
-            view_all_disjoint_group_ids = group_definitions.get_disjoint_ids(user.all_group_ids.ids)
-            view_visible_implied_group_ids = user.group_ids.implied_ids.all_implied_ids
-            if not user.view_show_technical_groups:
-                view_visible_implied_group_ids = view_visible_implied_group_ids.filtered(lambda g: g.category_id.visible)
-
-            user.view_disjoint_group_ids = view_disjoint_group_ids
-            user.view_all_disjoint_group_ids = view_all_disjoint_group_ids
-            user.view_visible_implied_group_ids = view_visible_implied_group_ids
