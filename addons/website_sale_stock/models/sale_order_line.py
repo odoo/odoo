6d521d10e131cf5a69423956ffba276a17f5da61
Arnold Moyaux <arm@odoo.com>
2025-03-18 14:14:29 +0100

    [FIX] website_sale_stock: prevent combo out of stock
    Usecase:
    Create a combo item with products that are not allowed to be out of
    stock on ecommerce.
    In the classic process, it's okay. You are not allow to add more than
    available quantity.
    However on the checkout page, you can increase as you want. If the user
    do it, it will order more stock than available.
    
    Fix the flow to ensure that data-max is always equals to the min
    quantity of the products in combo.
    
    opw-4614309

--- addons/website_sale_stock/models/sale_order_line.py
+++ addons/website_sale_stock/models/sale_order_line.py
@@ -15,4 +15,6 @@ class SaleOrderLine(models.Model):
         return self.shop_warning
 
     def _get_max_available_qty(self):
+        if self.product_type == "combo":
+            return min(p.free_qty - p._get_cart_qty() for p in self.linked_line_ids.product_id if p.is_storable and not p.allow_out_of_stock_order)
         return self.product_id.free_qty - self.product_id._get_cart_qty()
