7e6f331e741d84c365825029b37b78b978c6493d
tsm-odoo <tsm@odoo.com>
2025-03-18 09:29:46 +0100

    [FIX] mail: non deterministic subscription test
    Before this PR, the `bus subscription is refreshed when channel is joined`
    test was sometimes failing.
    
    Since [1], opening a thread *always* result in a bus subscription
    being issued (the channel is added to the bus channels as a string).
    This is not required: bus subscriptions are based on user's channels
    (see `ir_websocket@_build_bus_channel_list`). When a new channel is
    added, the client subscribes again (see `toggleBusSubscription`).
    
    Channels should only be added explictly when a channel the user is not
    a member of is opened. This addition causes a race condition between
    `toggleBusSubscription` and the explicit addition of the channel,
    resulting in a non deterministic behavior.
    
    This PR fixes this issue: channel is only added when required, which
    is not the case in this test thus solving the issue.
    
    fixes runbot-106895
    
    [1]: https://github.com/odoo/odoo/pull/174473
    
    closes odoo/odoo#202226
    
    X-original-commit: 605e83541c8fdcc80f0d9b687f22c77998fdf86d
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/mail/static/tests/discuss/core/discuss.test.js
+++ addons/mail/static/tests/discuss/core/discuss.test.js
@@ -9,10 +9,11 @@ import {
     start,
     startServer,
     step,
+    triggerHotkey,
 } from "@mail/../tests/mail_test_helpers";
 import { describe, test } from "@odoo/hoot";
 import { mockDate } from "@odoo/hoot-mock";
-import { getService } from "@web/../tests/web_test_helpers";
+import { Command, serverState } from "@web/../tests/web_test_helpers";
 
 describe.current.tags("desktop");
 defineMailModels();
@@ -29,13 +30,45 @@ test("Member list and Pinned Messages Panel menu are exclusive", async () => {
     await contains(".o-discuss-ChannelMemberList", { count: 0 });
 });
 
+test("subscribe to known partner presences", async () => {
+    patchWebsocketWorkerWithCleanup({
+        _sendToServer({ event_name, data }) {
+            if (event_name === "subscribe") {
+                step(`subscribe - [${data.channels}]`);
+            }
+        },
+    });
+    const pyEnv = await startServer();
+    const bobPartnerId = pyEnv["res.partner"].create({
+        name: "Bob",
+        user_ids: [Command.create({ name: "bob" })],
+    });
+    const later = luxon.DateTime.now().plus({ seconds: 2 });
+    mockDate(
+        `${later.year}-${later.month}-${later.day} ${later.hour}:${later.minute}:${later.second}`
+    );
+    await start();
+    await openDiscuss();
+    const expectedPresences = [
+        `odoo-presence-res.partner_${serverState.partnerId}`,
+        `odoo-presence-res.partner_${serverState.odoobotId}`,
+    ];
+    await assertSteps([`subscribe - [${expectedPresences.join(",")}]`]);
+    await click("[title='Start a conversation']");
+    await insertText(".o-discuss-ChannelSelector input", "bob");
+    await click(".o-discuss-ChannelSelector-suggestion");
+    await triggerHotkey("Enter");
+    expectedPresences.push(`odoo-presence-res.partner_${bobPartnerId}`);
+    await assertSteps([`subscribe - [${expectedPresences.join(",")}]`]);
+});
+
 test("bus subscription is refreshed when channel is joined", async () => {
     const pyEnv = await startServer();
     pyEnv["discuss.channel"].create([{ name: "General" }, { name: "Sales" }]);
     patchWebsocketWorkerWithCleanup({
-        _sendToServer({ event_name, data }) {
+        _sendToServer({ event_name }) {
             if (event_name === "subscribe") {
-                step(`subscribe - ${JSON.stringify(data.channels)}`);
+                step("subscribe");
             }
         },
     });
@@ -44,32 +77,22 @@ test("bus subscription is refreshed when channel is joined", async () => {
         `${later.year}-${later.month}-${later.day} ${later.hour}:${later.minute}:${later.second}`
     );
     await start();
-    const expectedSubscribes = [];
-    for (const { type, id } of getService("mail.store").imStatusTrackedPersonas) {
-        const model = type === "partner" ? "res.partner" : "mail.guest";
-        expectedSubscribes.unshift(`"odoo-presence-${model}_${id}"`);
-    }
-    await assertSteps([`subscribe - [${expectedSubscribes.join(",")}]`]);
+    await assertSteps(["subscribe"]);
     await openDiscuss();
     await assertSteps([]);
     await click(".o-mail-DiscussSidebar [title='Add or join a channel']");
     await insertText(".o-discuss-ChannelSelector input", "new channel");
     await click(".o-discuss-ChannelSelector-suggestion");
-    const [newChannel] = pyEnv["discuss.channel"].search_read([["name", "=", "new channel"]]);
-    expectedSubscribes.unshift(`"discuss.channel_${newChannel.id}"`);
-    await assertSteps([
-        `subscribe - [${expectedSubscribes.join(",")}]`,
-        `subscribe - [${expectedSubscribes.join(",")}]`, // 1 is enough. The 2 comes from technical details (1: from channel_join, 2: from channel open), 2nd covers shadowing
-    ]);
+    await assertSteps(["subscribe"]);
 });
 
 test("bus subscription is refreshed when channel is left", async () => {
     const pyEnv = await startServer();
     pyEnv["discuss.channel"].create({ name: "General" });
     patchWebsocketWorkerWithCleanup({
-        _sendToServer({ event_name, data }) {
+        _sendToServer({ event_name }) {
             if (event_name === "subscribe") {
-                step(`subscribe - ${JSON.stringify(data.channels)}`);
+                step("subscribe");
             }
         },
     });
@@ -77,15 +100,10 @@ test("bus subscription is refreshed when channel is left", async () => {
     mockDate(
         `${later.year}-${later.month}-${later.day} ${later.hour}:${later.minute}:${later.second}`
     );
-    const env = await start();
-    const imStatusChannels = [];
-    for (const { type, id } of env.services["mail.store"].imStatusTrackedPersonas) {
-        const model = type === "partner" ? "res.partner" : "mail.guest";
-        imStatusChannels.unshift(`"odoo-presence-${model}_${id}"`);
-    }
-    await assertSteps([`subscribe - [${imStatusChannels.join(",")}]`]);
+    await start();
+    await assertSteps(["subscribe"]);
     await openDiscuss();
     await assertSteps([]);
     await click("[title='Leave this channel']");
-    await assertSteps([`subscribe - [${imStatusChannels.join(",")}]`]);
+    await assertSteps(["subscribe"]);
 });
