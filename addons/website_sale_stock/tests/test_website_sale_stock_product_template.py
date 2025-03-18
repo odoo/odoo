d6b34b7e6d76146e5078306e9ba60efc36e7ec92
Louis (loti) <loti@odoo.com>
2025-03-18 15:20:37 +0000

    [FIX] website_sale*: fix max combo quantity
    Before this fix, it was possible to add more combo products than available
    to the cart. Indeed, the available quantity was only checked in the combo
    configurator, but not in other places where the quantity could be updated
    (such as the product page or the cart).
    
    This fix introduces a "max quantity" concept for combo products, which is based
    on the available quantity of their combo items.
    
    opw-4378554
    
    X-original-commit: 439d2005b75e9661231152a56de1689bf18d24c4

--- addons/website_sale_stock/tests/test_website_sale_stock_product_template.py
+++ addons/website_sale_stock/tests/test_website_sale_stock_product_template.py
@@ -2,14 +2,15 @@
 
 from datetime import datetime
 
-from odoo.tests import tagged
+from odoo.fields import Command
+from odoo.tests import HttpCase, tagged
 
 from odoo.addons.website.tools import MockRequest
 from odoo.addons.website_sale_stock.tests.common import WebsiteSaleStockCommon
 
 
 @tagged('post_install', '-at_install')
-class TestWebsiteSaleStockProductTemplate(WebsiteSaleStockCommon):
+class TestWebsiteSaleStockProductTemplate(HttpCase, WebsiteSaleStockCommon):
 
     def test_website_sale_stock_get_additional_configurator_data(self):
         product = self._create_product(is_storable=True, allow_out_of_stock_order=False)
@@ -29,3 +30,51 @@ class TestWebsiteSaleStockProductTemplate(WebsiteSaleStockCommon):
             )
 
         self.assertEqual(configurator_data['free_qty'], 10)
+
+    def test_get_additional_combination_info_max_combo_quantity_with_max(self):
+        product_a = self._create_product(is_storable=True, allow_out_of_stock_order=False)
+        product_b = self._create_product(is_storable=True, allow_out_of_stock_order=False)
+        product_c = self._create_product(is_storable=True)
+        self.env['stock.quant'].create([
+            {
+                'product_id': product_a.id,
+                'location_id': self.warehouse.lot_stock_id.id,
+                'quantity': 5,
+            }, {
+                'product_id': product_b.id,
+                'location_id': self.warehouse.lot_stock_id.id,
+                'quantity': 10,
+            },
+        ])
+        combo_a, combo_b, combo_c = self.env['product.combo'].create([
+            {'name': "Combo A", 'combo_item_ids': [Command.create({'product_id': product_a.id})]},
+            {'name': "Combo B", 'combo_item_ids': [Command.create({'product_id': product_b.id})]},
+            {'name': "Combo C", 'combo_item_ids': [Command.create({'product_id': product_c.id})]},
+        ])
+        combo_product = self._create_product(
+            type='combo',
+            combo_ids=[
+                Command.link(combo_a.id), Command.link(combo_b.id), Command.link(combo_c.id)
+            ],
+        )
+        self.cart.order_line = [Command.create({'product_id': product_a.id, 'product_uom_qty': 3})]
+
+        with MockRequest(self.env, website=self.website, sale_order_id=self.cart.id):
+            combination_info = self.env['product.template']._get_additionnal_combination_info(
+                combo_product, quantity=3, date=datetime(2000, 1, 1), website=self.website
+            )
+
+        self.assertEqual(combination_info['max_combo_quantity'], 2)
+
+    def test_get_additional_combination_info_max_combo_quantity_without_max(self):
+        product = self._create_product(is_storable=True)
+        combo = self.env['product.combo'].create({
+            'name': "Test combo", 'combo_item_ids': [Command.create({'product_id': product.id})]
+        })
+        combo_product = self._create_product(type='combo', combo_ids=[Command.link(combo.id)])
+
+        combination_info = self.env['product.template']._get_additionnal_combination_info(
+            combo_product, quantity=3, date=datetime(2000, 1, 1), website=self.website
+        )
+
+        self.assertNotIn('max_combo_quantity', combination_info)
