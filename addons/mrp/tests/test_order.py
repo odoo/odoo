345a1e6d1f33e03a7f483dfb86f66f8eb491b771
David Van Droogenbroeck (DROD) <drod@odoo.com>
2025-03-18 08:32:06 +0000

    [FIX] mrp: update confirmed MO quantities of kits components
    Issues
    -----
    When using the "Update BOM" button for a confirmed manufacturing order,
    quantities for components derived from kit products are incorrectly updated when
    different variants/UoMs with common components are used.
    
    Steps to reproduce
    -----
    - Install Manufacturing app
    - Create a BoM for 2 units of the "T-Shirt" product
        - Add a "Logo" product as a consumable
    - Create a "Fabric" product, with "Material" Attribute with values "P" & "C"
    - Create a BoM for the "Fabric" product
        - Set Bom Type to "Kit"
        - Add a "Thread" product as a consumable
        - Add a "Polyester" product as a consumable for variant "P"
        - Add a "Coton" product as a consumable for variant "C"
    - Create a Manufacturing Order for "T-Shirt"
    - Confirm the MO
    - Open the BoM (click the link in the form)
    - Add "Fabric (C)" and "Fabric (P)" as components to the BoM (same quantity)
    - Go back to the MO and click "Udate BoM"
        -> Wrong quantity for the "Thread" consumable
    
    Causes
    -----
    The new bom has multiple lines with the same id. Those lines are stored into
    bom_lines_by_id which is a dictionary, using (id, qty) tuple as the key. If the
    product variants are required in the same amount, this causes problems as the
    key tuples are identical, so the last one overwrites the other.
    
    Solution
    -----
    For simplicity's sake we merge the overlapping kit variant component quantities
    instead of keeping track of each exploded kit's bom_line since it involved
    potential performance degradation for an edge use case.
    
    -----
    Follow up of:
    opw-4473337
    opw-4480039
    
    X-original-commit: 0eed6a8779360dd80cb3610c7c001ef31667c661

--- addons/mrp/tests/test_order.py
+++ addons/mrp/tests/test_order.py
@@ -4878,6 +4878,17 @@ class TestMrpOrder(TestMrpCommon):
             {'product_id': kit_bom.bom_line_ids[0].product_id.id, 'product_uom_qty': 12, 'product_uom': kit_bom.bom_line_ids[0].product_id.uom_id.id},
             {'product_id': kit_bom.bom_line_ids[1].product_id.id, 'product_uom_qty': 9, 'product_uom': kit_bom.bom_line_ids[1].product_id.uom_id.id},
         ])
+        # Check with multiple kits with different UOMs
+        with Form(self.bom_3) as main_bom:
+            with main_bom.bom_line_ids.new() as bom_line:
+                bom_line.product_id = self.product_5
+                bom_line.product_qty = 1
+                bom_line.product_uom_id = self.uom_dozen
+        mo.action_update_bom()
+        self.assertRecordValues(mo.move_raw_ids, [
+            {'product_id': kit_bom.bom_line_ids[0].product_id.id, 'product_uom_qty': 60, 'product_uom': kit_bom.bom_line_ids[0].product_id.uom_id.id},
+            {'product_id': kit_bom.bom_line_ids[1].product_id.id, 'product_uom_qty': 45, 'product_uom': kit_bom.bom_line_ids[1].product_id.uom_id.id},
+        ])
 
     def test_update_mo_from_bom_with_kit_variants(self):
         """
@@ -4935,6 +4946,20 @@ class TestMrpOrder(TestMrpCommon):
             {'product_id': self.product_1.id, 'product_qty': 1},
             {'product_id': self.product_4.id, 'product_qty': 1},
             {'product_id': paint_products[0].id, 'product_qty': 2},
+        ])
+        # Add a different variant of the kit to the main BoM
+        with Form(self.bom_4) as main_bom:
+            with main_bom.bom_line_ids.new() as bom_line:
+                bom_line.product_id = kit_product.product_variant_ids[1]
+                bom_line.product_qty = 1
+        # Update the MO
+        mo.action_update_bom()
+        self.assertEqual(len(mo.move_raw_ids), 4)
+        self.assertRecordValues(mo.move_raw_ids, [
+            {'product_id': self.product_1.id, 'product_qty': 1},
+            {'product_id': self.product_4.id, 'product_qty': 2},
+            {'product_id': paint_products[0].id, 'product_qty': 2},
+            {'product_id': paint_products[1].id, 'product_qty': 1},
         ])
 
     @freeze_time('2024-11-26 9:00')
