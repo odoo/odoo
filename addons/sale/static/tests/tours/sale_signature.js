b0573a4486fca68d06942da62b5c53f285c54350
Achraf (abz) <abz@odoo.com>
2025-03-18 13:09:41 +0000

    [FIX] portal,sale_management: Prevent crash when there is no default name on signature_form
    Steps:
    - install `contacts` and `sale_management`
    - Open a contact in a form view and add a sub-contact
    - Don't fill contact name field
    - Save & Close
    - Go to Quotations
    - Select this new contact as Customer
        (it should have a name like Parent contact, Other Address)
    - Add any product
    - Save
    - Click on `Preview`
    - Click on `Sign & Pay`
    - Traceback
    
    When there is no signature name, `NameAndSignature`
    does not set `resetSignature` and `getSignatureImage` on `this.signature`.
    So after `SignatureForm` initialization, `onMounted` raise a traceback,
    because it calls directly `this.signature.resetSignature`.
    
    This commit adds default functions to `resetSignature` and `getSignatureImage`
    to prevent the crash.
    
    opw-4504223
    
    closes odoo/odoo#203986
    
    X-original-commit: a7cf8a0eac24152c9ffad46b4d983714b2cb01ab
    Signed-off-by: Lina Eweis (liew) <liew@odoo.com>
    Signed-off-by: Achraf Ben Azzouz (abz) <abz@odoo.com>

--- addons/sale/static/tests/tours/sale_signature.js
+++ addons/sale/static/tests/tours/sale_signature.js
@@ -51,3 +51,24 @@ registry.category("web_tour.tours").add('sale_signature', {
         trigger: 'nav',
     }
 ]});
+
+registry.category("web_tour.tours").add("sale_signature_without_name", {
+    steps: () => [
+        {
+            content: "Sign & Pay",
+            trigger:
+                ".o_portal_sale_sidebar .btn-primary, :iframe .o_portal_sale_sidebar .btn-primary",
+            run: "click",
+        },
+        {
+            content: "click submit",
+            trigger: ".o_portal_sign_submit:enabled, :iframe .o_portal_sign_submit:enabled",
+            run: "click",
+        },
+        {
+            content: "check error because no name",
+            trigger:
+                '.o_portal_sign_error_msg:contains("Signature is missing."), :iframe .o_portal_sign_error_msg:contains("Signature is missing.")',
+        },
+    ],
+});
