315b7cf1b08e0247d61a480c680a985d8cadc473
Augustin (duau) <duau@odoo.com>
2025-03-19 14:12:41 +0100

    [REF] survey: rewrite SurveyQuickAccess widget as Interaction
    task-4367641

--- addons/survey/static/src/interactions/survey_quick_access.js
+++ addons/survey/static/src/interactions/survey_quick_access.js
@@ -1,95 +1,73 @@
-import publicWidget from "@web/legacy/js/public/public_widget";
+import { Interaction } from "@web/public/interaction";
 import { rpc } from "@web/core/network/rpc";
+import { registry } from "@web/core/registry";
 
-publicWidget.registry.SurveyQuickAccessWidget = publicWidget.Widget.extend({
-    selector: '.o_survey_quick_access',
-    events: {
-        'click button[type="submit"]': '_onSubmit',
-        'input #session_code': '_onSessionCodeInput',
-        'click .o_survey_launch_session': '_onLaunchSessionClick',
-    },
+export class SurveyQuickAccess extends Interaction {
+    static selector = ".o_survey_quick_access";
 
-    //--------------------------------------------------------------------------
-    // Widget
-    //--------------------------------------------------------------------------
+    dynamicContent = {
+        _document: { "t-on-keypress": this.onKeyPress },
+        "button[type='submit']": { 
+            "t-on-click.prevent": this.submitCode,
+            "t-att-class": () => ({ "d-none": this.isLaunchShown })
+        },
+        "#session_code": { "t-on-input": this.onSessionCodeInput },
+        ".o_survey_launch_session": { 
+            "t-on-click": this.onLaunchSessionClick,
+            "t-att-class": () => ({ "d-none": !this.isLaunchShown }),
+        },
+        ".o_survey_session_error_not_launched": { "t-att-class": () => ({ "d-none": this.errorCode !== "not_launched" }) },
+        ".o_survey_session_error_invalid_code": { "t-att-class": () => ({ "d-none": this.errorCode !== "invalid_code" }) },
 
-    init() {
-        this._super(...arguments);
-        this.orm = this.bindService("orm");
-    },
-    
-    /**
-    * @override
-    */
-    start: function () {
-        var self = this;
-        return this._super.apply(this, arguments).then(function () {
-            // Init event listener
-            if (!self.readonly) {
-                $(document).on('keypress', self._onKeyPress.bind(self));
-            }
-        });
-    },
+    };
 
-    // -------------------------------------------------------------------------
-    // Private
-    // -------------------------------------------------------------------------
-
-    // Handlers
-    // -------------------------------------------------------------------------
-
-    _onLaunchSessionClick: async function () {
-        const sessionResult = await this.orm.call(
+    async onLaunchSessionClick() {
+        const sessionResult = await this.waitFor(this.services.orm.call(
             "survey.survey",
             "action_start_session",
-            [[this.$(".o_survey_launch_session").data("surveyId")]]
-        );
+            [[parseInt(this.el.querySelector(".o_survey_launch_session").dataset.surveyId)]]
+        ));
         window.location = sessionResult.url;
-    },
+    }
 
-    _onSessionCodeInput: function () {
-        this.el.querySelectorAll('.o_survey_error > span').forEach((elem) => elem.classList.add('d-none'));
-        this.$('.o_survey_launch_session').addClass('d-none');
-        this.$('button[type="submit"]').removeClass('d-none');
-    },
+    onSessionCodeInput() {
+        this.errorCode = "";
+        this.isLaunchShown = false;
+    }
 
-    _onKeyPress: function (event) {
+    onKeyPress(event) {
         if (event.key === "Enter") {
             event.preventDefault();
-            this._submitCode();
+            this.submitCode();
         }
-    },
+    }
 
-    _onSubmit: function (event) {
-        event.preventDefault();
-        this._submitCode();
-    },
-
-    _submitCode: function () {
-        var self = this;
-        this.$('.o_survey_error > span').addClass('d-none');
-        const sessionCodeInputVal = this.$('input#session_code').val().trim();
+    async submitCode() {
+        this.errorCode = "";
+        const sessionCodeInputVal = encodeURIComponent(this.el.querySelector("input#session_code").value.trim());
         if (!sessionCodeInputVal) {
-            self.$('.o_survey_session_error_invalid_code').removeClass('d-none');
+            this.errorCode = "invalid_code";
             return;
         }
-        rpc(`/survey/check_session_code/${sessionCodeInputVal}`).then(function (response) {
+        const response = await this.waitFor(rpc(`/survey/check_session_code/${sessionCodeInputVal}`));
+        this.protectSyncAfterAsync(() => {
             if (response.survey_url) {
                 window.location = response.survey_url;
             } else {
-                if (response.error && response.error === 'survey_session_not_launched') {
-                    self.$('.o_survey_session_error_not_launched').removeClass('d-none');
+                if (response.error && response.error === "survey_session_not_launched") {
+                    this.errorCode = "not_launched";
                     if ("survey_id" in response) {
-                        self.$('button[type="submit"]').addClass('d-none');
-                        self.$('.o_survey_launch_session').removeClass('d-none');
-                        self.$('.o_survey_launch_session').data('surveyId', response.survey_id);
+                        this.isLaunchShown = true;
+                        this.el.querySelector(".o_survey_launch_session").dataset.surveyId = response.survey_id;
                     }
                 } else {
-                    self.$('.o_survey_session_error_invalid_code').removeClass('d-none');
+                    this.errorCode = "invalid_code";
                 }
             }
-        });
-    },
-});
+        })();
+    }
+}
 
-export default publicWidget.registry.SurveyQuickAccessWidget;
+registry
+    .category("public.interactions")
+    .add("survey.survey_quick_access", SurveyQuickAccess);
