bf5dc105f8b46a9d2e87ecee9abf9bc7460f1e58
Pierre Pulinckx <pipu@odoo.com>
2025-03-19 13:28:54 +0000

    [FIX] web_tour: return unload for helper action
    If an action is defined in a step with a string (and therefore is an
    action that comes from tour_helpers) there is no check on the
    beforeUnload event that is done. In this commit, we fix this.
    
    closes odoo/odoo#202680
    
    X-original-commit: 4685c334b194a23902341e50433979cead138c78
    Related: odoo/enterprise#81831
    Signed-off-by: Bastien Pierre (ipb) <ipb@odoo.com>
    Signed-off-by: Pierre Pulinckx (pipu) <pipu@odoo.com>

--- addons/mrp/static/tests/tours/mrp_sm_sml_synchronization.js
+++ addons/mrp/static/tests/tours/mrp_sm_sml_synchronization.js
@@ -7,7 +7,7 @@ registry.category("web_tour.tours").add('test_manufacturing_and_byproduct_sm_to_
             run: "click",
         },
         {
-            trigger: ".o_data_row > td:contains('product2')",
+            trigger: ".o_data_row:has([name=quantity]:contains(5.00)) > td:contains(product2)",
             run: "click",
         },
         {
@@ -27,7 +27,7 @@ registry.category("web_tour.tours").add('test_manufacturing_and_byproduct_sm_to_
             run: "click",
         },
         {
-            trigger: ".o_data_row > td:contains('product2')",
+            trigger: ".o_data_row:has([name=quantity]:contains(5.00)) > td:contains(product2)",
             run: "click",
         },
         {
@@ -81,7 +81,7 @@ registry.category("web_tour.tours").add('test_manufacturing_and_byproduct_sm_to_
             run: "click",
         },
         {
-            trigger: ".o_data_row > td:contains('product2')",
+            trigger: ".o_data_row:has([name=quantity]:contains(2.00)) > td:contains(product2)",
             run: "click",
         },
         {
@@ -106,8 +106,8 @@ registry.category("web_tour.tours").add('test_manufacturing_and_byproduct_sm_to_
             run: "click",
         },
         {
-            trigger: ".o_data_row > td:contains('product2')",
-            run: "click",
+            trigger: ".o_data_row:has([name=quantity]:contains(2.00)) > td[name=product_id]:contains(product2)",
+            run: "dblclick",
         },
         {
             trigger: ".o_field_widget[name=quantity] input",
@@ -135,7 +135,7 @@ registry.category("web_tour.tours").add('test_manufacturing_and_byproduct_sm_to_
             run: "click",
         },
         {
-            trigger: ".o_data_row > td:contains('10')",
+            trigger: ".o_data_row:has([name=product_uom_qty]:contains(2.00)) > td:contains(10)",
             run: "click",
         },
         {
