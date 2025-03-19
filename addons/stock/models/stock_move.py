15a379a601cc78d5844a9be72555b2c499e96771
Lulu Grimalkin (lugr) <lugr@odoo.com>
2025-03-19 10:46:18 +0100

    [IMP] stock: input validation for serial number count in generator
    On the off chance that the user inputs an invalid number for
    the number of SNs to be generated or the quantity per lot (ie
    `next_serial_count`) — for example if they try to generate a
    fractional amount of SNs — they're hit with a stack trace for an
    uncaught `InvalidNumberError`. We catch the exception and return
    a warning message instead.
    
    Task ID: 4566595

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -1368,7 +1368,6 @@ Please change the quantity done or the rounding precision in your settings.""",
                 }
             }
 
-
     def _key_assign_picking(self):
         self.ensure_one()
         keys = (self.reference_ids, self.location_id, self.location_dest_id, self.picking_type_id)
