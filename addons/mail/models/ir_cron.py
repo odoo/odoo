d827b857d9d35d9c0eada7c705a500125fd0d8c1
Bruno Boi <boi@odoo.com>
2025-03-19 12:44:29 +0000

    [FIX] mail: add thread on ir.cron (fix traceback)
    Since [1], server actions now have a chatter thread.
    The scheduled actions (ir.cron) uses delegation inheritance from the
    server action model. The ir.cron form view is also an extension of the
    server action form view, leading it to have the <chatter/> element but
    no mail.thread methods on the model itself.
    
    This commit fixes that by making the ir.cron model to also inherit:
    - mail.thread
    - mail.activity.mixin
    
    Furthermore, it adds tracking to some fields.
    
    [1]: https://github.com/odoo/odoo/commit/f499a9c9144372e3a42e2ea53c2ae65f09cf76cc
    
    closes odoo/odoo#202509
    
    Signed-off-by: Jorge Pinna Puissant (jpp) <jpp@odoo.com>

--- addons/mail/models/ir_cron.py
+++ addons/mail/models/ir_cron.py
@@ -1,10 +1,16 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, SUPERUSER_ID
+from odoo import fields, models, SUPERUSER_ID
 
 
 class IrCron(models.AbstractModel):
-    _inherit = 'ir.cron'
+    _name = 'ir.cron'
+    _inherit = ['ir.cron', 'mail.thread', 'mail.activity.mixin']
+
+    user_id = fields.Many2one(tracking=True)
+    interval_number = fields.Integer(tracking=True)
+    interval_type = fields.Selection(tracking=True)
+    priority = fields.Integer(tracking=True)
 
     def _notify_admin(self, message):
         """ Send a notification to the admin users. """
