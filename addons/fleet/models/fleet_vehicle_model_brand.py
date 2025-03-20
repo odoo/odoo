8c2507d3c86b879b80d4c9579d35ea54cb5fa695
rmun-odoo <rmun@odoo.com>
2025-03-20 15:22:13 +0530

    [IMP] fleet: improve ux and kanban card click behavior
    - Repositioned Status Group By.
    - Improved Kanban card interaction:
        - Click anywhere (except icon) → Opens Models list.
        - Click on icon → Opens Manufacturer form.
    
    task-4657598
    
    closes odoo/odoo#202675
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/fleet/models/fleet_vehicle_model_brand.py
+++ addons/fleet/models/fleet_vehicle_model_brand.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 
 class FleetVehicleModelBrand(models.Model):
@@ -28,11 +28,21 @@ class FleetVehicleModelBrand(models.Model):
     def action_brand_model(self):
         self.ensure_one()
         view = {
+            'name': _('Models'),
             'type': 'ir.actions.act_window',
             'view_mode': 'list,form',
             'res_model': 'fleet.vehicle.model',
-            'name': 'Models',
             'context': {'search_default_brand_id': self.id, 'default_brand_id': self.id}
         }
 
         return view
+
+    def action_open_brand_form(self):
+        self.ensure_one()
+        return {
+            'name': _('Manufacturer'),
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'fleet.vehicle.model.brand',
+            'res_id': self.id
+        }
