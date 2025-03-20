428e5c0a4a3e8749b6205536f0bbaddb686272fe
Renaud Thiry <reth@odoo.com>
2025-03-20 10:38:05 +0100

    [IMP] calendar,event: set ics method
    We should set a method as defined by [1] so that when the ICS is sent by email.
    It allows the recipient to confirm/decline directly via an email banner.
    
    For events we set a method for consistency; However, as we don't care about
    their response PUBLISH is used. Which won't ask for confirmation.
    
    [1]: https://www.rfc-editor.org/rfc/rfc5546.html#page-18
    
    task-4568114
    
    Part-of: odoo/odoo#203918
    Related: odoo/enterprise#82421
    Related: odoo/upgrade#7467
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/calendar/models/calendar_event.py
+++ addons/calendar/models/calendar_event.py
@@ -1494,6 +1494,7 @@ class CalendarEvent(models.Model):
 
         for meeting in self:
             cal = vobject.iCalendar()
+            cal.add('method').value = 'REQUEST'
             event = cal.add('vevent')
 
             if not meeting.start or not meeting.stop:
