2739c627c8b28fc2f1d1b8a925387793d85baf64
Adnan Saiyed <adsa@odoo.com>
2025-03-20 16:53:37 +0530

    [FIX] html_editor: hide color selector on scroll
    Current behaviour before commit:
    
    Steps to reproduce the issue:
    
    - Have a long text content in editable, making the container
      scrollable.
    - Select first line of the text, in toolbar open color selector.
    - Scroll down until the toolbar overflows and becomes hidden.
    
    Notice that the toolbar disappears when it overflows its container
    while color picker remains open, floating at the top of container.
    
    Desired behaviour after commit:
    
    Now, when the toolbar overflows and gets hidden, the color selector
    also becomes hidden along with toolbar.
    
    task-4581884

--- addons/html_editor/static/src/main/chatgpt/language_selector.js
+++ addons/html_editor/static/src/main/chatgpt/language_selector.js
@@ -1,10 +1,11 @@
 import { Component, onWillStart, useState } from "@odoo/owl";
-import { useService } from "@web/core/utils/hooks";
+import { useChildRef, useService } from "@web/core/utils/hooks";
 import { Dropdown } from "@web/core/dropdown/dropdown";
 import { DropdownItem } from "@web/core/dropdown/dropdown_item";
 import { loadLanguages } from "@web/core/l10n/translation";
 import { toolbarButtonProps } from "@html_editor/main/toolbar/toolbar";
 import { user } from "@web/core/user";
+import { useDropdownAutoVisibility } from "@html_editor/dropdown_autovisibility_hook";
 
 export class LanguageSelector extends Component {
     static template = "html_editor.LanguageSelector";
@@ -20,6 +21,7 @@ export class LanguageSelector extends Component {
         this.state = useState({
             languages: [],
         });
+        this.menuRef = useChildRef();
         onWillStart(() => {
             if (user.userId) {
                 loadLanguages(this.orm).then((res) => {
@@ -27,6 +29,7 @@ export class LanguageSelector extends Component {
                 });
             }
         });
+        useDropdownAutoVisibility(this.env.overlayState, this.menuRef);
     }
     onSelected(language) {
         this.props.onSelected(language);
