0f2261e3a901c634cea163969de0c623099ca843
Hubert Van De Walle <huvw@odoo.com>
2025-03-20 15:50:50 +0100

    [FIX] pos_online_payment: QR code should open after the first order
    Steps to reproduce
    ==================
    
    - Create a new payment method
      * Check "online payment"
      * Add the demo payment provider
    - Add that payment method to a point of sale
    - Open a new session
    - Open the customer display
    - Add a product
    - Pay with the correct payment method
    - Validate
    - The QR code appears on both displays
    - Confirm the payment
    - Make a new order with the same steps
    
    => The QR code appears on the POS, but not on the customer display
    
    Cause of the issue
    ==================
    
    Once the payment has been validated, the payment popup with the qr code
    closes on the customer display. But the variable
    `this.qrCodePopupCloser` isn't reset. This prevents following popups
    from opening.
    
    opw-4497177

--- addons/point_of_sale/static/tests/tours/customer_display_tour.js
+++ addons/point_of_sale/static/tests/tours/customer_display_tour.js
@@ -1,25 +1,32 @@
 import * as Order from "@point_of_sale/../tests/tours/utils/generic_components/order_widget_util";
 import { registry } from "@web/core/registry";
-import { run } from "@point_of_sale/../tests/tours/utils/common";
 
-function postMessage(message, description = "") {
-    return run(() => {
-        window.customerDisplayChannel.postMessage(JSON.parse(message));
-    }, `send message to customer display: ${description},  with value: ${message}`);
+export function postMessage(message, description = "") {
+    return {
+        trigger: "body",
+        in_modal: false,
+        run: () => {
+            window.customerDisplayChannel.postMessage(
+                typeof message === "string" ? JSON.parse(message) : message
+            );
+        },
+        content: `send message to customer display: ${description},  with value: ${message}`,
+    };
 }
-function amountIs(method, amount) {
+
+export function amountIs(method, amount) {
     return {
         content: `Check that the ${method} amount is ${amount}`,
         trigger: `div.row:has(div:contains('${method}')):has(div:contains('${amount}'))`,
     };
 }
-const ADD_PRODUCT =
+export const ADD_PRODUCT =
     '{"lines":[{"productName":"Letter Tray","price":"$ 2,972.75","qty":"1.00","unit":"Units","unitPrice":"$ 2,972.75","oldUnitPrice":"","customerNote":"","internalNote":"","comboParent":"","packLotLines":[],"price_without_discount":"$ 2,972.75","isSelected":true,"imageSrc":"/web/image/product.product/855/image_128"}],"finalized":false,"amount":"2,972.75","paymentLines":[],"change":0,"onlinePaymentData":{}}';
 const PAY_WITH_CASH =
     '{"lines":[{"productName":"Letter Tray","price":"$ 2,972.75","qty":"1.00","unit":"Units","unitPrice":"$ 2,972.75","oldUnitPrice":"","customerNote":"","internalNote":"","comboParent":"","packLotLines":[],"price_without_discount":"$ 2,972.75","isSelected":true,"imageSrc":"/web/image/product.product/855/image_128"}],"finalized":false,"amount":"2,972.75","paymentLines":[{"name":"Cash","amount":"2,972.75"}],"change":0,"onlinePaymentData":{}}';
-const ORDER_IS_FINALIZED =
+export const ORDER_IS_FINALIZED =
     '{"lines":[{"productName":"Letter Tray","price":"$ 2,972.75","qty":"1.00","unit":"Units","unitPrice":"$ 2,972.75","oldUnitPrice":"","customerNote":"","internalNote":"","comboParent":"","packLotLines":[],"price_without_discount":"$ 2,972.75","isSelected":false,"imageSrc":"/web/image/product.product/855/image_128"}],"finalized":true,"amount":"2,972.75","paymentLines":[{"name":"Cash","amount":"2,972.75"}],"change":0,"onlinePaymentData":{}}';
-const NEW_ORDER =
+export const NEW_ORDER =
     '{"lines":[],"finalized":false,"amount":"0.00","paymentLines":[],"change":0,"onlinePaymentData":{}}';
 
 registry.category("web_tour.tours").add("CustomerDisplayTour", {
