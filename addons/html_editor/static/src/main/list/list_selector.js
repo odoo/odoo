986e4a212bafc9151017b6a749ad7a7926b19c13
Adnan Saiyed <adsa@odoo.com>
2025-03-20 16:53:37 +0530

    [FIX] html_editor: hide color selector on scroll
    Current behaviour before commit:
    
    Steps to reproduce the issue:
    
    - Have a long text content in editable, making the container
      scrollable.
    - Select first line of the text, in toolbar open color selector.
    - Scroll down until the toolbar overflows and becomes hidden.
    
    Notice that the toolbar disappears when it overflows its container
    while color picker remains open, floating at the top of container.
    
    Desired behaviour after commit:
    
    Now, when the toolbar overflows and gets hidden, the color selector
    also becomes hidden along with toolbar.
    
    task-4581884
    
    Part-of: odoo/odoo#224857
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/html_editor/static/src/main/list/list_selector.js
+++ addons/html_editor/static/src/main/list/list_selector.js
@@ -2,6 +2,8 @@ import { Component } from "@odoo/owl";
 import { Dropdown } from "@web/core/dropdown/dropdown";
 import { toolbarButtonProps } from "../toolbar/toolbar";
 import { closestElement } from "@html_editor/utils/dom_traversal";
+import { useDropdownAutoVisibility } from "@html_editor/dropdown_autovisibility_hook";
+import { useChildRef } from "@web/core/utils/hooks";
 
 export class ListSelector extends Component {
     static template = "html_editor.ListSelector";
@@ -13,6 +15,10 @@ export class ListSelector extends Component {
     };
     static components = { Dropdown };
 
+    setup() {
+        this.menuRef = useChildRef();
+        useDropdownAutoVisibility(this.env.overlayState, this.menuRef);
+    }
     getActiveMode() {
         const { editableSelection: selection } = this.props.getSelection();
         const closestLI = closestElement(selection.anchorNode, "LI");
