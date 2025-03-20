a4df30e871ff920537709415c4c3bddf0b11dc8d
fdardenne <dafl@odoo.com>
2025-03-20 13:31:29 +0100

    Add slideshow layout customization

--- addons/html_builder/static/src/website_builder/plugins/options/image_gallery_option.js
+++ addons/html_builder/static/src/website_builder/plugins/options/image_gallery_option.js
@@ -1,7 +1,9 @@
 import { BaseOptionComponent, useDomState } from "@html_builder/core/utils";
+import { BorderConfigurator } from "@html_builder/plugins/border_configurator_option";
 
 export class ImageGalleryComponent extends BaseOptionComponent {
     static template = "html_builder.ImageGalleryOption";
+    static components = { BorderConfigurator };
     static props = {};
 
     setup() {
