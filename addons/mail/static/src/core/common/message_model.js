ff1abffb633302fa6a26c7b6de83c1f342234b4b
Alexandre Kühn <aku@odoo.com>
2025-03-20 15:16:55 +0000

    [FIX] mail: user-deleted message should not have 'Translate' feature
    When a user deletes a user-message in discuss or chatter, the message
    content is replaced by "This message has been removed". Some actions
    are still possible on the message, such as adding reactions.
    
    However some features do no make sense, like 'Translate', as the
    message has no content anymore and the "This message has been
    removed" is translated automatically to the user viewing this
    message.
    
    This commit fixes the issue by not showing the 'Translate' message
    action on deleted messages.
    
    Task-4661959
    
    X-original-commit: ffcdb90fd34b3201411f7d26aa6b2cfd8b3d4ea7

--- addons/mail/static/src/core/common/message_model.js
+++ addons/mail/static/src/core/common/message_model.js
@@ -285,6 +285,7 @@ export class Message extends Record {
 
     isTranslatable(thread) {
         return (
+            !this.isEmpty &&
             this.store.hasMessageTranslationFeature &&
             !["discuss.channel", "mail.box"].includes(thread?.model)
         );
