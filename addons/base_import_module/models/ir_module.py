95ecf6cb98c1c23b5e327a7867181d9880a1a015
vava-odoo <vava@odoo.com>
2025-03-20 10:18:59 +0100

    [FIX] base_import_module: allow dependency between industries
    Before this commit, installing an industry module that depends on
    another data module would fail, eventhough the zip file contains the
    dependent module. The reason is that the check for dependencies did not
    take into account the other modules from the zip file.
    
    This commit makes sure that other modules in the zip file are taken into
    account when installing an industry, and that a dependency to a module
    that is present in the imported zip file would not raise an error.
    
    task-4575230

--- addons/base_import_module/models/ir_module.py
+++ addons/base_import_module/models/ir_module.py
@@ -516,6 +516,7 @@ class IrModuleModule(models.Model):
                 if file.filename.count('/') == 1
                 and file.filename.split('/')[1] in MANIFEST_NAMES
             ]
+            modules_in_zip = {manifest.filename.split('/')[0] for manifest in manifest_files}
             for manifest_file in manifest_files:
                 if manifest_file.file_size > MAX_FILE_SIZE:
                     raise UserError(_("File '%s' exceed maximum allowed file size", manifest_file.filename))
@@ -524,7 +525,7 @@ class IrModuleModule(models.Model):
                         terp = ast.literal_eval(manifest.read().decode())
                 except Exception:
                     continue
-                unmet_dependencies = set(terp.get('depends', [])).difference(installed_mods)
+                unmet_dependencies = set(terp.get('depends', [])).difference(installed_mods, modules_in_zip)
                 dependencies_to_install |= known_mods.filtered(lambda m: m.name in unmet_dependencies)
                 not_found_modules |= set(
                     mod for mod in unmet_dependencies if mod not in dependencies_to_install.mapped('name')
