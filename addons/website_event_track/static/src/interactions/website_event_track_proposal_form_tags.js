13222d37eff2d8d3b66f4b7ece8cff70a0d92b47
Augustin (duau) <duau@odoo.com>
2025-03-20 11:45:26 +0100

    [REF] website_event_track: rewrite WebsiteEventTrackProposalFormTags
    This commit rewrites WebsiteEventTrackProposalFormTags as an Interaction.
    
    task-4367641

--- addons/website_event_track/static/src/interactions/website_event_track_proposal_form_tags.js
+++ addons/website_event_track/static/src/interactions/website_event_track_proposal_form_tags.js
@@ -2,8 +2,8 @@ import { Component, useState } from "@odoo/owl";
 import { _t } from "@web/core/l10n/translation";
 import { rpc } from "@web/core/network/rpc";
 import { SelectMenu } from "@web/core/select_menu/select_menu";
-import publicWidget from "@web/legacy/js/public/public_widget";
-import { attachComponent } from "@web_editor/js/core/owl_utils";
+import { Interaction } from "@web/public/interaction";
+import { registry } from "@web/core/registry";
 
 class WebsiteEventTrackProposalFormTagsWrapper extends Component {
     static template = "website_event_track.WebsiteEventTrackProposalFormTagsWrapper";
@@ -24,29 +24,27 @@ class WebsiteEventTrackProposalFormTagsWrapper extends Component {
     }
 }
 
-publicWidget.registry.websiteEventTrackProposalFormTags = publicWidget.Widget.extend({
-    selector: '.o_website_event_track_proposal_form_tags',
+export class WebsiteEventTrackProposalFormTags extends Interaction {
+    static selector = ".o_website_event_track_proposal_form_tags";
 
     async willStart() {
         const choices = await rpc("/event/track_tag/search_read", {
             fields: ["id", "name", "category_id"],
             domain: [["color", "!=", 0]],
         });
-        this.choices = choices.map(({ id, category_id, name }) => {
+        const choicesMap = choices.map(({ id, category_id, name }) => {
             return {
                 value: id,
                 label: category_id ? `${category_id[1]} : ${name}` : name,
             };
         });
-    },
-
-    async start() {
-        await this._super(...arguments);
-        await attachComponent(this, this.el, WebsiteEventTrackProposalFormTagsWrapper, {
-            defaultChoices: this.choices || [],
+        this.mountComponent(this.el, WebsiteEventTrackProposalFormTagsWrapper, {
+            defaultChoices: choicesMap || [],
             placeholder: _t("Select Categories"),
         });
-    },
-});
+    }
+}
 
-export default publicWidget.registry.websiteEventTrackProposalFormTags;
+registry
+    .category("public.interactions")
+    .add("website_event_track.website_event_track_proposal_form_tags", WebsiteEventTrackProposalFormTags);
