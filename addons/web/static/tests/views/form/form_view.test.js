3ee0d2e57c054df89da71f66c21f319f366a8bb4
Michaël Mattiello <mcm@odoo.com>
2025-03-20 10:52:54 +0100

    [IMP] web: support relatedFields on many2one
    This commit adds the support of relatedFields on many2one fields.
    It allows to load additionnal fields of the relation along with the
    display_name of the many2one.
    
    task-4658839
    task-3547961
    
    closes odoo/odoo#202534
    
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/tests/views/form/form_view.test.js
+++ addons/web/static/tests/views/form/form_view.test.js
@@ -11779,6 +11779,70 @@ test(`custom x2many with relatedFields and list view not inline`, async () => {
     expect.verifySteps(["web_read", "web_save"]);
 });
 
+test(`custom many2one with relatedFields`, async () => {
+    class CustomMany2One extends Component {
+        static template = xml`
+            <t t-set="value" t-value="props.record.data[props.name]"/>
+            <div class="content">
+                <div t-esc="value.id"/>
+                <div t-esc="value.display_name"/>
+                <div t-esc="value.foo"/>
+                <div t-esc="value.int_field"/>
+            </div>
+            <button id="update-m2o" t-on-click="() => this.update()">Update</button>
+        `;
+        static props = ["*"];
+
+        update() {
+            return this.props.record.update({ [this.props.name]: { id: 2 } });
+        }
+    }
+    fieldsRegistry.add("my_widget", {
+        component: CustomMany2One,
+        relatedFields: [
+            { name: "foo", type: "char" },
+            { name: "int_field", type: "integer" },
+        ],
+    });
+
+    onRpc("web_read", ({ kwargs }) => {
+        expect.step("web_read");
+        expect.step(kwargs.specification);
+    });
+
+    await mountView({
+        resModel: "partner",
+        type: "form",
+        arch: `<form><field name="parent_id" widget="my_widget"/></form>`,
+        resId: 1,
+    });
+    expect.verifySteps([
+        "web_read",
+        {
+            display_name: {},
+            parent_id: {
+                fields: {
+                    display_name: {},
+                    foo: {},
+                    int_field: {},
+                },
+            },
+        },
+    ]);
+    expect(`.o_field_widget[name="parent_id"] .content`).toHaveText("4\naaa\nMy little Foo Value\n0");
+
+    await contains(`#update-m2o`).click();
+    expect.verifySteps([
+        "web_read",
+        {
+            display_name: {},
+            foo: {},
+            int_field: {},
+        }
+    ]);
+    expect(`.o_field_widget[name="parent_id"] .content`).toHaveText("2\nsecond record\nblip\n9");
+});
+
 test(`existing record with falsy display_name`, async () => {
     Partner._records[0].name = "";
     await mountView({
