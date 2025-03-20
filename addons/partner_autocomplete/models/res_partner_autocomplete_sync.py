1e7f96a8d6c44283ab9bfca33033ae8c29507a02
Ricardo Gomes Rodrigues <rigr@odoo.com>
2025-03-20 09:51:45 +0100

    [IMP] partner_autocomplete: change provider
    We are switching away from Clearbit which is very
    unreliable. For that reason we had an autosync
    feature which would update IAP's data for a company
    based on the data provided in Odoo.
    
    Instead, we are now switching to Dun & Bradstreet
    which has much more reliable data. Some routes have
    been changed to target the new API on IAP.
    
    We continue to use Clearbit for the logos which are
    not provided by DnB (yet).
    
    This is a backport of https://github.com/odoo/odoo/pull/196373
    where models, fields, views, and public methods have been kept
    to respect the stable policy.
    
    task-4416928
    
    closes odoo/odoo#202546
    
    Related: odoo/enterprise#81767
    Signed-off-by: Louis Baudoux (lba) <lba@odoo.com>

--- addons/partner_autocomplete/models/res_partner_autocomplete_sync.py
+++ addons/partner_autocomplete/models/res_partner_autocomplete_sync.py
@@ -15,30 +15,7 @@ class ResPartnerAutocompleteSync(models.Model):
 
     @api.model
     def start_sync(self, batch_size=1000):
-        to_sync_items = self.search([('synched', '=', False)], limit=batch_size)
-
-        for to_sync_item in to_sync_items:
-            partner = to_sync_item.partner_id
-
-            params = {
-                'partner_gid': partner.partner_gid,
-            }
-
-            if partner.vat and partner._is_vat_syncable(partner.vat):
-                params['vat'] = partner.vat
-                _, error = self.env['iap.autocomplete.api']._request_partner_autocomplete('update', params)
-                if error:
-                    _logger.warning('Send Partner to sync failed: %s', str(error))
-
-            to_sync_item.write({'synched': True})
-        done = len(to_sync_items)
-        self.env['ir.cron']._notify_progress(
-            done=done,
-            remaining=0 if done < batch_size else self.search_count([('synched', '=', False)])
-        )
+        pass  # Deprecated since DnB
 
     def add_to_queue(self, partner_id):
-        to_sync = self.search([('partner_id', '=', partner_id)])
-        if not to_sync:
-            to_sync = self.create({'partner_id': partner_id})
-        return to_sync
+        pass  # Deprecated since DnB
