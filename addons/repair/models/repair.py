3e9e8a438cb1ad8e519faf11a21b1bd4e6f0df51
JF Aubert <ajf@odoo.com>
2025-03-20 10:12:05 +0000

    [FIX] mrp: Fix mo picking type change
    When picking type is changed, the locations on the moves should also
    be updated.
    
    closes odoo/odoo#203028
    
    Task: 4513182
    X-original-commit: 6f770ac52eb0bb4b722a787926a4d84bd545f610
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Jean-Francois Aubert (ajf) <ajf@odoo.com>

--- addons/repair/models/repair.py
+++ addons/repair/models/repair.py
@@ -383,11 +383,15 @@ class RepairOrder(models.Model):
         return super().create(vals_list)
 
     def write(self, vals):
+        moves_to_reassign = self.env['stock.move']
         if vals.get('picking_type_id'):
             picking_type = self.env['stock.picking.type'].browse(vals.get('picking_type_id'))
             for repair in self:
+                if repair.state in ('cancel', 'done'):
+                    continue
                 if picking_type != repair.picking_type_id:
                     repair.name = picking_type.sequence_id.next_by_id()
+                    moves_to_reassign |= repair.move_ids
         res = super().write(vals)
         if 'product_id' in vals and self.tracking == 'serial':
             self.write({'product_qty': 1.0})
@@ -400,6 +404,14 @@ class RepairOrder(models.Model):
                 (repair.move_id + repair.move_ids).filtered(lambda m: m.state not in ('done', 'cancel')).write({'date': repair.schedule_date})
             if 'under_warranty' in vals:
                 repair._update_sale_order_line_price()
+        if moves_to_reassign:
+            moves_to_reassign._do_unreserve()
+            moves_to_reassign = moves_to_reassign.filtered(
+                lambda move: move.state in ('confirmed', 'partially_available')
+                and (move._should_bypass_reservation()
+                    or move.picking_type_id.reservation_method == 'at_confirm'
+                    or (move.reservation_date and move.reservation_date <= fields.Date.today())))
+            moves_to_reassign._action_assign()
         return res
 
     @api.ondelete(at_uninstall=False)
