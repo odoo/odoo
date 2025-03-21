cbc0d65cc08af4e05a5552bec551389c5cd3db8b
divy-odoo <divy@odoo.com>
2025-03-21 07:26:02 +0000

    [FIX] website: show success message on form submission
    Steps to reproduce:
    1. Drop website form snippet
    2. Change the success method to `Show message`
    3. Submit the form
    > Form is not being submited on 1st click
    > Success message is not appear.
    
    With the conversion of `publicWidget` into `Interaction`, the success
    message was not displayed correctly, and the form required multiple
    clicks to submit.
    
    The issue occurred because the selector for the success message in
    `dynamicContent` was outside the scope of the Form interaction,
    preventing the state from updating properly.
    
    This commit ensures that the success message is correctly displayed
    form submission.
    
    task-4663558
    
    closes odoo/odoo#206323
    
    X-original-commit: 1fcfbda4115c222d9ee0bb78f4f13219971c9477
    Signed-off-by: Serge Bayet (seba) <seba@odoo.com>
    Signed-off-by: Divyesh Vyas (divy) <divy@odoo.com>

--- addons/website/static/src/snippets/s_website_form/form.js
+++ addons/website/static/src/snippets/s_website_form/form.js
@@ -24,6 +24,10 @@ const { DateTime } = luxon;
 
 export class Form extends Interaction {
     static selector = ".s_website_form form, form.s_website_form"; // !compatibility
+    dynamicSelectors = {
+        ...this.dynamicSelectors,
+        _endMessage: () => this.el.parentNode.querySelector(".s_website_form_end_message"),
+    };
     dynamicContent = {
         ".s_website_form_send, .o_website_form_send": { "t-on-click.prevent": this.send }, // !compatibility
         _root: {
@@ -32,7 +36,7 @@ export class Form extends Interaction {
                 "d-none": this.isHidden,
             })
         },
-        ".s_website_form_end_message": {
+        _endMessage: {
             "t-att-class": () => ({
                 "d-none": !this.isHidden,
             })
@@ -435,7 +439,7 @@ export class Form extends Interaction {
                             // Prevent double-clicking on the send button and
                             // add a upload loading effect (delay before success
                             // message)
-                            await delay(400);
+                            await this.waitFor(delay(400));
 
                             this.isHidden = true;
                             break;
