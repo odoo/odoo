odoo/odoo

commit 704a5a19499469e5a14461bb81d33c832ce00d70

[IMP] *expense*: Bye Bye reports
Most of the time, expenses are created as "standalone"s and forcing
the use of an expense report is adding frustrating unnecessary steps.

A simplification was decided and the model hr.expense.sheet is removed,
giving all of its remaining important logic to the `hr.expense`.

In addition, approval flow was simplified with the implementation of
an auto-validation process, where submitting an expense also approves it
when no expense manager is set on the employee.

See task for more details.

closes odoo/odoo#189701

Task-id: 4481615
Related: odoo/enterprise#75507
Related: odoo/upgrade#6990
Signed-off-by: Olivier Colson (oco) <oco@odoo.com>

addons/hr_expense/data/mail_templates.xml
@@ -2,7 +2,7 @@
 <odoo>
     <data noupdate="1">
         <template id="hr_expense_template_refuse_reason">
-            <p>Your Expense Report <t t-out="name"/> has been refused</p>
+            <p>Your Expense <t t-out="name"/> has been refused</p>
             <ul class="o_timeline_tracking_value_list">
                 <li>Reason: <t t-out="reason"/></li>
             </ul>
@@ -28,8 +28,17 @@
             </p>
             <p t-if="expense.employee_id.user_id">
                 <br/>
-                <a t-att-href="'/odoo/hr.expense/%s' % (expense.id)" style="background-color: #9E588B; margin-top: 10px; padding: 10px; text-decoration: none; color: #fff; border-radius: 5px; font-size: 16px;">View Expense</a>
+                <a t-att-href="'/odoo/hr.expense/%s' % (expense.id)" class="o_expense_button_mail">View Expense</a>
             </p>
+        </template>
+
+        <template id="hr_expense_template_submitted_expenses">
+            <p>Dear <t t-out="manager_name"/>,</p>
+            <p>
+                New expenses are awaiting your approval.
+                You can review them by following this link
+            </p>
+            <a t-att-href="'/odoo/expenses-to-process'" class="o_expense_button_mail">View expenses</a>
         </template>
 
         <template id="hr_expense_template_register_no_user">

addons/project_sale_expense/models/hr_expense.py
@@ -14,3 +14,16 @@ class HrExpense(models.Model):
                 if not self.sale_order_id:
                     continue
                 expense.analytic_distribution = expense.sale_order_id.project_id._get_analytic_distribution()
+
+    def action_post(self):
+        """ When creating the move of the expense, if the AA is given in the project of the SO, we take it as reference in the distribution.
+            Otherwise, we create a AA for the project of the SO and set the distribution to it.
+        """
+        for expense in self:
+            project = expense.sale_order_id.project_id
+            if not project or expense.analytic_distribution:
+                continue
+            if not project.account_id:
+                project._create_analytic_account()
+            expense.analytic_distribution = project._get_analytic_distribution()
+        return super().action_post()

addons/sale_expense/models/hr_expense.py
@@ -1,6 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+from odoo import Command, api, fields, models
 
 
 class HrExpense(models.Model):
@@ -45,9 +45,47 @@ class HrExpense(models.Model):
         to_reset.invalidate_recordset(['analytic_distribution'])
         self.env.add_to_compute(self._fields['analytic_distribution'], to_reset)
 
+    def _sale_expense_reset_sol_quantities(self):
+        """
+        Resets the quantity of a SOL created by a reinvoiced expense to 0 when the expense or its move is reset to an unfinished state
+
+        Note: Resetting the qty_delivered will raise if the product is a storable product and sale_stock is installed,
+              but it's fine as it doesn't make much sense to have a stored product in an expense.
+        """
+        self.check_access('write')
+        # If we can edit the expense, we may not be able to edit the sol without sudoing.
+        self.sudo().sale_order_line_id.write({
+            'qty_delivered': 0.0,
+            'product_uom_qty': 0.0,
+            'expense_ids': [Command.clear()],
+        })
+
     def _get_split_values(self):
         # EXTENDS hr_expense
         vals = super()._get_split_values()
         for split_value in vals:
             split_value['sale_order_id'] = self.sale_order_id.id
         return vals
+
+    def action_post(self):
+        # EXTENDS hr_expense
+        # When posting expense, we need the analytic entries to be generated, because reinvoicing uses analytic accounts.
+        # We then ensure the proper analytic acocunt is given in the distribution and if not,
+        # we create an account and set the distribution to it.
+        for expense in self:
+            if expense.sale_order_id and not expense.analytic_distribution:
+                analytic_account = self.env['account.analytic.account'].create(expense.sale_order_id._prepare_analytic_account_data())
+                expense.analytic_distribution = {analytic_account.id: 100}
+        return super().action_post()
+
+    def action_open_sale_order(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'sale.order',
+            'views': [(self.env.ref("sale.view_order_form").id, 'form')],
+            'view_mode': 'form',
+            'target': 'current',
+            'name': self.sale_order_id.display_name,
+            'res_id': self.sale_order_id.id,
+        }

addons/sale_expense/views/hr_expense_views.xml
@@ -6,6 +6,15 @@
         <field name="inherit_id" ref="hr_expense.hr_expense_view_form"/>
         <field name="priority">30</field>
         <field name="arch" type="xml">
+            <button name="action_open_account_move" position="after">
+                <button name="action_open_sale_order"
+                    class="oe_stat_button"
+                    icon="fa-money"
+                    type="object"
+                    invisible="not sale_order_id">
+                    <field name="sale_order_id" widget="statinfo" string="Sales Order"/>
+                </button>
+            </button>
             <xpath expr="//field[@name='analytic_distribution']" position="before">
                 <field name="sale_order_id" groups="!sales_team.group_sale_salesman,!account.group_account_manager"
                     invisible="not can_be_reinvoiced"
@@ -35,7 +44,7 @@
         <field name="inherit_id" ref="hr_expense.view_expenses_tree"/>
         <field name="arch" type="xml">
             <xpath expr="//field[@name='date']" position="before">
-                <field name="sale_order_id" optional="hide" invisible="not can_be_reinvoiced" readonly="state in ['done', 'refused']" options="{'no_create_edit': True, 'no_create': True, 'no_open': True}"  context="{'sale_show_partner_name': True, 'sale_expense_all_order': True}"/>
+                <field name="sale_order_id" optional="hide" invisible="not can_be_reinvoiced" readonly="state in ['posted', 'refused']" options="{'no_create_edit': True, 'no_create': True, 'no_open': True}"  context="{'sale_show_partner_name': True, 'sale_expense_all_order': True}"/>
                 <field name="can_be_reinvoiced" readonly="1" column_invisible="True"/>
             </xpath>
         </field>
@@ -49,18 +58,6 @@
             <xpath expr="//field[@name='employee_id']" position="after">
                 <field name="can_be_reinvoiced" column_invisible="True"/>
                 <field name="sale_order_id" force_save="1" readonly="not can_be_reinvoiced"/>
-            </xpath>
-        </field>
-    </record>
-
-    <record id="hr_expense_sheet_form_view_inherit_sale_expense" model="ir.ui.view">
-        <field name="name">hr.expense.sheet.form.inherit.sale.expense</field>
-        <field name="model">hr.expense.sheet</field>
-        <field name="inherit_id" ref="hr_expense.view_hr_expense_sheet_form"/>
-        <field name="arch" type="xml">
-            <xpath expr="//field[@name='expense_line_ids']/list/field[@name='vendor_id']" position="after">
-                <field name="sale_order_id" invisible="not can_be_reinvoiced" readonly="state in ['done', 'refused']" optional="show" options="{'no_create_edit': True, 'no_create': True, 'no_open': True}" context="{'sale_show_partner_name': True, 'sale_expense_all_order': True}"/>
-                <field name="can_be_reinvoiced" column_invisible="True"/>
             </xpath>
         </field>
     </record>

commit 813113b7e5b01f98ac40c2c2c4f7162bd90df4df

[FIX] hr_expense: Fix bad url
Fix a small mistake introduced by 704a5a1, where the URL
specified is hardcoded in the mail template.
This introduce the intended flow of reviewing expenses
from the manager department that are submitted
(defaulting on the full view if there are more than one).

addons/hr_expense/data/mail_templates.xml
@@ -38,7 +38,7 @@
                 New expenses are awaiting your approval.
                 You can review them by following this link
             </p>
-            <a t-att-href="'/odoo/expenses-to-process'" class="o_expense_button_mail">View expenses</a>
+            <a t-att-href="url" class="o_expense_button_mail">View expenses</a>
         </template>
 
         <template id="hr_expense_template_register_no_user">

addons/hr_expense/models/hr_expense.py
@@ -1021,9 +1021,14 @@ class HrExpense(models.Model):
                 for manager, expenses_submitted in expenses_submitted_per_company.grouped('manager_id').items():
                     manager_langs = tuple(lang for lang in manager.partner_id.mapped('lang') if lang)
                     mail_lang = (manager_langs and manager_langs[0]) or self.env.lang or 'en_US'
+                    departments = expenses_submitted.department_id
+                    if len(departments) > 1:
+                        url = '/expenses-to-process'
+                    else:
+                        url = f'/departments/{departments.id}/expenses-to-approve'
                     body = self.env['ir.qweb']._render(
                         template='hr_expense.hr_expense_template_submitted_expenses',
-                        values={'manager_name': manager.name, 'url': '/expenses-to-approve'},
+                        values={'manager_name': manager.name, 'url': url},
                         lang=mail_lang,
                     )
                     new_mails.append({

addons/hr_expense/views/hr_expense_views.xml
@@ -612,8 +612,8 @@
         </record>
 
         <record id="action_hr_expense_department_to_approve" model="ir.actions.act_window">
-            <field name="name">Expense to Approve</field>
-            <field name="path">expense-to-approve</field>
+            <field name="name">Expenses to Approve</field>
+            <field name="path">expenses-to-approve</field>
             <field name="res_model">hr.expense</field>
             <field name="view_mode">list,kanban,form,pivot,graph</field>
             <field name="search_view_id" ref="hr_expense_view_search_with_panel"/>