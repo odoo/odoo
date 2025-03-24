261ed5bc3ee9cb044ea94df9636fb60a097f715d
maad-odoo <maad@odoo.com>
2025-03-24 16:35:50 +0530

    [IMP] mail,im_livechat: shortcut to partner form view
    Add a "View Contact" action that opens then partner record form view of
    the visitor, if the visitor has a partner record linked it it.
    
    Enterprise PR: https://github.com/odoo/enterprise/pull/82059
    
    task-4607618
    
    closes odoo/odoo#203079
    
    Related: odoo/enterprise#82059
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/im_livechat/static/src/core/web/livechat_channel_info_list.js
+++ addons/im_livechat/static/src/core/web/livechat_channel_info_list.js
@@ -5,6 +5,8 @@ import { Component } from "@odoo/owl";
 
 import { rpc } from "@web/core/network/rpc";
 import { useService } from "@web/core/utils/hooks";
+import { url } from "@web/core/utils/urls";
+import { startUrl } from "@web/core/browser/router";
 
 export class LivechatChannelInfoList extends Component {
     static components = { ActionPanel };
@@ -14,11 +16,28 @@ export class LivechatChannelInfoList extends Component {
     setup() {
         super.setup();
         this.store = useService("mail.store");
+        this.ui = useService("ui");
     }
 
     onBlurNote() {
         prettifyMessageContent(this.props.thread.livechatNoteText).then((note) => {
             rpc("/im_livechat/session/update_note", { channel_id: this.props.thread.id, note });
         });
+    }
+
+    openVisitorProfile() {
+        if (this.ui.isSmall) {
+            this.store.ChatWindow.get({ thread: this.props.thread })?.fold();
+        } else {
+            this.props.thread.openChatWindow({ focus: true });
+        }
+    }
+
+    get visitorProfileURL() {
+        const visitorPersona = this.props.thread?.livechatVisitorMember?.persona;
+        if (visitorPersona?.type === "partner") {
+            return url(`/${startUrl()}/res.partner/${visitorPersona.id}`);
+        }
+        return null;
     }
 }
