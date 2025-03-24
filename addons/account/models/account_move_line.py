f82106daf77b228d3d4c85b1767f90bdc2581b58
Maira Salazar (srma) <srma@odoo.com>
2025-03-24 08:41:38 +0000

    [IMP] account: validate mandatory analytic plans when batch posting
    To replicate:
    1. [Activate](https://www.odoo.com/documentation/18.0/applications/finance/accounting/reporting/analytic_accounting.html)
    Analytic Accounting:
       a. Install `accountant`
       b. In Settings, activate Analytic Accounting
       c. Create an Analytic plan (with an Analytic account associated)
    2. Set the plan's default applicability to mandatory
    3. Create two invoices, remove the analytic distribution from one of the lines
    in one invoice.
    4. In the invoices list view, select both newly created invoices, click on
    Actions > Confirm Entries
    5. Click Confirm
    6. The invoices were posted, even though they have no analytic distributions.
    
    When we have an Analytic Plan being Mandatory, confirming an invoice from the
    form view, if it has a line without an Analytic distribution, correctly raises
    a ValidationError. Confirming invoices from the list view does not raise the
    same error, yet it should.
    
    This commit ensures the validation check for the mandatory analytic plans when
    mass posting entries, and adds a RedirectWarning to show a list view of the
    entries that should be reviewed.
    
    opw-4603919
    
    closes odoo/odoo#203153
    
    X-original-commit: 9487a6be687a5272a269296299cc0ebc1c1a9d98
    Signed-off-by: Paolo Gatti (pgi) <pgi@odoo.com>
    Signed-off-by: Maira Salazar <srma@odoo.com>

--- addons/account/models/account_move_line.py
+++ addons/account/models/account_move_line.py
@@ -5,7 +5,7 @@ import logging
 import re
 
 from odoo import api, fields, models, Command, _
-from odoo.exceptions import ValidationError, UserError
+from odoo.exceptions import ValidationError, UserError, RedirectWarning
 from odoo.osv import expression
 from odoo.tools import frozendict, format_date, float_compare, format_list, Query
 from odoo.tools.sql import create_index, SQL
@@ -3079,15 +3079,37 @@ class AccountMoveLine(models.Model):
     # -------------------------------------------------------------------------
 
     def _validate_analytic_distribution(self):
+        lines_with_missing_analytic_distribution = self.env['account.move.line']
         for line in self.filtered(lambda line: line.display_type == 'product'):
-            line._validate_distribution(**{
-                        'product': line.product_id.id,
-                        'account': line.account_id.id,
-                        'business_domain': line.move_id.move_type in ['out_invoice', 'out_refund', 'out_receipt'] and 'invoice'
-                                           or line.move_id.move_type in ['in_invoice', 'in_refund', 'in_receipt'] and 'bill'
-                                           or 'general',
-                        'company_id': line.company_id.id,
-            })
+            try:
+                line._validate_distribution(
+                    company_id=line.company_id.id,
+                    product=line.product_id.id,
+                    account=line.account_id.id,
+                    business_domain=(
+                        'invoice' if line.move_id.is_sale_document(True)
+                        else 'bill' if line.move_id.is_purchase_document(True)
+                        else 'general'
+                    ),
+                )
+            except ValidationError:
+                lines_with_missing_analytic_distribution += line
+        if lines_with_missing_analytic_distribution:
+            msg = _("One or more lines require a 100% analytic distribution.")
+            if len(self.move_id) == 1:
+                raise ValidationError(msg)
+            raise RedirectWarning(
+                message=msg,
+                action={
+                    'view_mode': 'list',
+                    'name': _('Items With Missing Analytic Distribution'),
+                    'res_model': 'account.move.line',
+                    'type': 'ir.actions.act_window',
+                    'domain': [('id', 'in', lines_with_missing_analytic_distribution.ids)],
+                    'views': [(self.env.ref('account.view_move_line_tree').id, 'list')],
+                },
+                button_text=_("See items"),
+            )
 
     def _create_analytic_lines(self):
         """ Create analytic items upon validation of an account.move.line having an analytic distribution.
