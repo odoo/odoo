a7fffecf107c641d001a1666bce2ad6906170ffe
deli-odoo <deli@odoo.com>
2025-03-24 14:52:38 +0000

    [FIX] pos_sale: fix error on weight field on sale report for pos orders
    When viewing sale report for POS orders, the gross weight number will
    be off by a factor of how much larger/smaller the UOM ratio is
    compared to the base. For example, if we sell 1 qty of product with a
    weight of 25kg and a UOM of 25kg (25 * 1kg), the gross weight field
    will be 1 * 25kg * 25kg or 625kg instead of the expected 1 * 25kg or
    25kg. This fix removes the UOM factor from being taken into account
    when calculating gross weight for POS orders. This is a valid solution
    because if we treat 'SUM(p.weight * l.product_uom_qty / u.factor *
    u2.factor)' as the ground truth from sale report for sale orders, the
    '/ u.factor * u2.factor' portion of the calculation will cancel out
    to 1 for POS orders. This is because the only time 'u2.factor/u.factor'
    is not 1 for sale orders is when the product template's UOM factor is
    different from the sale order line's UOM factor. Since we can not
    change the UOM of pos order lines, the pos order line's factor will
    always be the same as the product template's therefore
    'u2.factor / u.factor' for pos orders will always be 1 and can be
    ignored.
    
    To reproduce error on blank DB:
    1) For a product, change its UOM to a UOM with a ratio not equal to 1
    2) For the same product, change its weight in the inventory tab to
    a number not equal to 0
    3) In POS make a sale with this product and confirm the order
    4) Check the gross weight of the product just sold through POS by
    going to sales->reporting->list view and adding gross weight to the
    view through studio
    
    opw-4452892
    
    X-original-commit: eafde7c65a3765f32a00c4a0613f0d5191de25ba

--- addons/pos_sale/tests/test_pos_sale_report.py
+++ addons/pos_sale/tests/test_pos_sale_report.py
@@ -14,6 +14,18 @@ class TestPoSSaleReport(TestPoSCommon):
         self.config = self.basic_config
         self.product0 = self.create_product('Product 0', self.categ_basic, 0.0, 0.0)
         self.partner_1 = self.env['res.partner'].create({'name': 'Test Partner 1'})
+        # Ensure that adding a uom to the product with a factor != 1
+        # does not cause an error in weight and volume calculation
+        self.uom_reference = self.env['uom.uom'].create({
+            'name': 'Reference Unit',
+            'relative_factor': 1,
+        })
+        self.uom_dozen = self.env['uom.uom'].create({
+            'name': 'Dozen',
+            'relative_factor': 12,
+            'relative_uom_id': self.uom_reference.id,
+        })
+        self.product0.uom_id = self.uom_dozen
 
     def test_weight_and_volume(self):
         self.product0.product_tmpl_id.weight = 3
