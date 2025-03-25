2d18a693d2c4b32ee8d2ecf6681469c13436681b
Jitendra Prajapat <jipr@odoo.com>
2025-03-25 04:59:46 +0000

    [FIX] point_of_sale: prevent flickering of combo selection dialog
    Before this commit:
    =====================
    - The combo selection dialog flickered when a combo product had only one
      choice and was non-configurable, as it opened and closed rapidly.
    - In self-order mode, a traceback occurred in this scenario, and the auto-
      selection of the combo choice was missing, unlike in the main POS.
    
    After this commit:
    ==============
    - The dialog no longer flickers and will not open if there is only one non-
      configurable choice. It now only opens when multiple choices are available or
      configuration is required.
    - The auto-selection of the combo choice is now consistent between self-order
      mode and the main POS, preventing tracebacks.
    
    Task-4664491
    
    closes odoo/odoo#207563
    
    X-original-commit: fd63440a371ff6e40a268eba07e7bd9ea359cba5
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Jitendra Kumar Prajapat (jipr) <jipr@odoo.com>

--- addons/pos_self_order/static/src/app/components/product_card/product_card.js
+++ addons/pos_self_order/static/src/app/components/product_card/product_card.js
@@ -93,7 +93,33 @@ export class ProductCard extends Component {
         }
 
         if (product.isCombo()) {
-            this.router.navigate("combo_selection", { id: product.id });
+            const selectedCombos = [];
+            let showComboSelectionPage = false;
+            for (const combo of product.combo_ids) {
+                const { combo_item_ids } = combo;
+                if (combo_item_ids.length > 1 || combo_item_ids[0]?.product_id.isConfigurable()) {
+                    showComboSelectionPage = true;
+                    break;
+                }
+                selectedCombos.push({
+                    combo_item_id: this.selfOrder.models["product.combo.item"].get(
+                        combo_item_ids[0].id
+                    ),
+                    configuration: {
+                        attribute_custom_values: [],
+                        attribute_value_ids: [],
+                        price_extra: 0,
+                    },
+                });
+            }
+
+            if (showComboSelectionPage) {
+                this.router.navigate("combo_selection", { id: product.id });
+            } else {
+                this.flyToCart();
+                this.selfOrder.editedLine?.delete();
+                this.selfOrder.addToCart(product, 1, "", {}, {}, selectedCombos);
+            }
         } else if (product.isConfigurable()) {
             this.router.navigate("product", { id: product.id });
         } else {
