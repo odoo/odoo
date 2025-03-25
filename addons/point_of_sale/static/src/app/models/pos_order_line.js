0ac6f6afb7f36b2dce4e212a68c86adffb8dc7af
roen-odoo <roen@odoo.com>
2025-03-25 15:58:33 +0100

    [FIX] point_of_sale: Show correct tax label
    When using a fiscal position the tax group labels on the receipt order
    lines where not adapted correctly
    
    Steps to reproduce:
    -------------------
    * Create 2 taxes A and B
    * For each tax go to their corresponding tax group and set the label to
      "A" and "B"
    * Create a fiscal position that will match A on B
    * Add the fiscal position to the PoS
    * Create a new order and add a product that uses the A tax
    * Activate the fiscal position
    * Validate the order and go to the receipt
    > Observation: The orderline will show the label A but the order tax
      detail will correctly show the label B
    
    Why the fix:
    ------------
    When generating the display data for the orderline we need to first map
    the taxes according to the fiscal position.
    
    opw-4579223

--- addons/point_of_sale/static/src/app/models/pos_order_line.js
+++ addons/point_of_sale/static/src/app/models/pos_order_line.js
@@ -670,7 +670,11 @@ export class PosOrderline extends Base {
             ),
             taxGroupLabels: [
                 ...new Set(
-                    this.product_id.taxes_id
+                    getTaxesAfterFiscalPosition(
+                        this.product_id.taxes_id,
+                        this.order_id.fiscal_position_id,
+                        this.models
+                    )
                         ?.map((tax) => tax.tax_group_id.pos_receipt_label)
                         .filter((label) => label)
                 ),
--- addons/point_of_sale/static/tests/tours/product_screen_tour.js
+++ addons/point_of_sale/static/tests/tours/product_screen_tour.js
@@ -546,3 +546,27 @@ registry.category("web_tour.tours").add("AddMultipleSerialsAtOnce", {
             Chrome.endTour(),
         ].flat(),
 });
+
+registry.category("web_tour.tours").add("FiscalPositionTaxLabels", {
+    steps: () =>
+        [
+            Chrome.startPoS(),
+            Dialog.confirm("Open Register"),
+            ProductScreen.clickDisplayedProduct("Test Product"),
+            ProductScreen.totalAmountIs("100.00"),
+            ProductScreen.clickFiscalPosition("Fiscal Position Test"),
+            ProductScreen.totalAmountIs("100.00"),
+            ProductScreen.clickPayButton(),
+            PaymentScreen.clickPaymentMethod("Bank", true, { remaining: "0.00" }),
+            PaymentScreen.clickValidate(),
+            ReceiptScreen.isShown(),
+            {
+                content: "Make sure orderline tax label is correct",
+                trigger: ".orderline:contains('Tax Group 2')",
+            },
+            {
+                content: "Make sure receipt tax label is correct and correspond to the orderline",
+                trigger: ".pos-receipt-taxes:contains('Tax Group 2')",
+            },
+        ].flat(),
+});
