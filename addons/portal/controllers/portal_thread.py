e8fe73b04e5d0f085331319a0ab46ac04020197f
Giorgio Tocco (gito) <gito@odoo.com>
2025-03-25 11:58:57 +0000

    [FIX] mail: no access to reactions and copy link for public users
    Before this commit, as a public user in website chatter
    trying to add a reaction would result in a traceback.
    
    Steps to reproduce:
    - Install website module and add course page
    - As public user open a course
    - Go to reviews
    - Add a reaction to any message -> traceback
    
    This commit fixes the issues by adding a "can_react" field in
    the portal store init.
    
    task-4551910
    
    X-original-commit: 0e621e73219c3a081cce6874938474f2892b4762

--- addons/portal/controllers/portal_thread.py
+++ addons/portal/controllers/portal_thread.py
@@ -33,11 +33,17 @@ class PortalChatter(http.Controller):
         store = Store()
         thread = ThreadController._get_thread_with_access(thread_model, thread_id, **kwargs)
         partner = request.env.user.partner_id
-        if thread and request.env.user._is_public():
-            if portal_partner := get_portal_partner(
-                thread, kwargs.get("hash"), kwargs.get("pid"), kwargs.get("token")
-            ):
-                partner = portal_partner
+        if thread:
+            mode = request.env[thread_model]._get_mail_message_access([thread_id], "create")
+            has_react_access = ThreadController._get_thread_with_access(thread_model, thread_id, mode, **kwargs)
+            can_react = has_react_access
+            if request.env.user._is_public():
+                portal_partner = get_portal_partner(
+                    thread, kwargs.get("hash"), kwargs.get("pid"), kwargs.get("token")
+                )
+                can_react = has_react_access and portal_partner
+                partner = portal_partner or partner
+            store.add(thread, {"can_react": bool(can_react)}, as_thread=True)
         store.add_global_values(
             store_self=Store.One(partner, ["active", "avatar_128", "name", "user"])
         )
