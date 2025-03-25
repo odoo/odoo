e3e694d0dbb00d3347f8c56b4ffd9785093aaf3b
Mélanie <mepe@odoo.com>
2025-03-25 10:52:08 +0000

    [FIX] hr_recruitment: fix send email action
    With this commit; if a template is selected (need to be added in xml
    view in 16.0 but already display in 17.2) the body and the subject will
    be rendered.
    
    task-4664784
    
    X-original-commit: 40f1b3ffc8c611dc80cb9b806fdb28baf8df2c2c

--- addons/hr_recruitment/wizard/applicant_send_mail.py
+++ addons/hr_recruitment/wizard/applicant_send_mail.py
@@ -29,6 +29,13 @@ class ApplicantSendMail(models.TransientModel):
                 }
             }
 
+        if self.template_id:
+            subjects = self._render_field('subject', res_ids=self.applicant_ids.ids)
+            bodies = self._render_field('body', res_ids=self.applicant_ids.ids)
+        else:
+            subjects = {applicant.id: self.subject for applicant in self.applicant_ids}
+            bodies = {applicant.id: self.body for applicant in self.applicant_ids}
+
         for applicant in self.applicant_ids:
             if not applicant.partner_id:
                 applicant.partner_id = self.env['res.partner'].create({
@@ -41,9 +48,9 @@ class ApplicantSendMail(models.TransientModel):
 
             applicant.message_post(
                 author_id=self.author_id.id,
-                body=self.body,
+                body=bodies[applicant.id],
                 email_layout_xmlid='mail.mail_notification_light',
                 message_type='comment',
                 partner_ids=applicant.partner_id.ids,
-                subject=self.subject,
+                subject=subjects[applicant.id],
             )
