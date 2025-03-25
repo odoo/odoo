8398ce4f03a4b50c4dc76036b7c7355ce087819c
Augustin (duau) <duau@odoo.com>
2025-03-25 11:02:41 +0100

    [REF] website_event_track: rewrite WebsiteEventTrackProposalForm
    This commit rewrites WebsiteEventTrackProposalForm as an Interaction.
    
    task-4367641
    
    closes odoo/odoo#203238
    
    Signed-off-by: Robin Lejeune (role) <role@odoo.com>

--- addons/website_event_track/static/src/interactions/website_event_track_proposal_form.js
+++ addons/website_event_track/static/src/interactions/website_event_track_proposal_form.js
@@ -1,145 +1,123 @@
-import publicWidget from "@web/legacy/js/public/public_widget";
 import { _t } from "@web/core/l10n/translation";
+import { post } from "@web/core/network/http_service";
+import { registry } from "@web/core/registry";
 import { renderToElement } from "@web/core/utils/render";
+import { Interaction } from "@web/public/interaction";
 import { scrollTo } from "@web_editor/js/common/scrolling";
 
-publicWidget.registry.websiteEventTrackProposalForm = publicWidget.Widget.extend({
-    selector: '.o_website_event_track_proposal_form',
-    events: {
-        'click .o_wetrack_add_contact_information_checkbox': '_onAdvancedContactToggle',
-        'input input[name="partner_name"]': '_onPartnerNameInput',
-        'click .o_wetrack_proposal_submit_button': '_onProposalFormSubmit',
-    },
+export class WebsiteEventTrackProposalForm extends Interaction {
+    static selector = ".o_website_event_track_proposal_form";
 
-    /**
-     * @override
-     */
-    init: function () {
-        this._super(...arguments);
+    dynamicContent = {
+        ".o_wetrack_add_contact_information_checkbox" : { "t-on-click" : this.onAdvancedContactToggle },
+        "input[name='partner_name']" : { "t-on-input" : this.onPartnerNameInput },
+        ".o_wetrack_proposal_submit_button" : { "t-on-click.prevent.stop" : this.onProposalFormSubmit },
+        ".o_wetrack_contact_information" : { "t-att-class": () => ({
+            "d-none": !this.useAdvancedContact,
+            "o_wetrack_no_contact_mean_error": !this.hasContactMean,
+        }) },
+        ".o_wetrack_contact_mean": { "t-att-class": () => ({ "is-invalid": !this.hasContactMean }) },
+        ".o_wetrack_contact_name_input" : { "t-att-required": () => this.useAdvancedContact },
+        ".o_wetrack_proposal_error_section": { "t-att-class": () => ({ "d-none": !this.formErrors.length }) },
+    }
+
+    setup() {
         this.useAdvancedContact = false;
-    },
-
-    //--------------------------------------------------------------------------
-    // Private
-    //--------------------------------------------------------------------------
+        this.hasContactMean = true;
+        this.formErrors = [];
+    }
 
     /**
      * Evaluate and return validity of form input fields:
-     * - 1) error 'invalidFormInputs' : Invalid ones are marked as is-invalid and o_wetrack_input_error.
-     * - 2) error 'noContactMean' : Contact mean fields are marked as is-invalid and contact
+     * - 1) error "invalidFormInputs" : Invalid ones are marked as is-invalid and o_wetrack_input_error.
+     * - 2) error "noContactMean" : Contact mean fields are marked as is-invalid and contact
      * section as o_wetrack_no_contact_mean_error if none of them is filled.
      *
-     * @private
      * @returns {Boolean} - True if no error remain, false otherwise
      */
-    _isFormValid: function () {
-        var formErrors = [];
+    isFormValid() {
+        this.formErrors = [];
 
         // 1) Valid Form Inputs
-        this.$('.form-control').each(function () {
-            var $formControl = $(this);
-            // Validate current input, if not SelectMenu field.
-            var inputs = $formControl.not(".o_wetrack_select_tags");
-            var invalidInputs = inputs.toArray().filter(function (input) {
-                return !input.checkValidity();
-            });
-
-            $formControl.removeClass('o_wetrack_input_error is-invalid');
-            if (invalidInputs.length) {
-                $formControl.addClass('o_wetrack_input_error is-invalid');
-                formErrors.push('invalidFormInputs');
+        this.el.querySelectorAll(".form-control:not(.o_wetrack_select_tags)").forEach((formControl) => {
+            // Validate current input
+            const isValid = formControl.checkValidity();
+            formControl.classList.toggle("o_wetrack_input_error", !isValid);
+            formControl.classList.toggle("is-invalid", !isValid);
+            if (!isValid) {
+                this.formErrors.push("invalidFormInputs");
             }
         });
 
         // 2) Advanced Contact Must Have a Contact Mean
         if (this.useAdvancedContact) {
-            var hasContactMean = this.$('.o_wetrack_contact_phone_input').val() ||
-                this.$('.o_wetrack_contact_email_input').val();
-            if (!hasContactMean) {
-                this.$('.o_wetrack_contact_information').addClass('o_wetrack_no_contact_mean_error');
-                this.$('.o_wetrack_contact_mean').addClass('is-invalid');
-                formErrors.push('noContactMean');
-            } else {
-                this.$('.o_wetrack_contact_information').removeClass('o_wetrack_no_contact_mean_error');
-                this.$('.o_wetrack_contact_mean:not(".o_wetrack_input_error")').removeClass('is-invalid');
+            const phoneInput = this.el.querySelector(".o_wetrack_contact_phone_input");
+            const emailInput = this.el.querySelector(".o_wetrack_contact_email_input");
+    
+            this.hasContactMean = (phoneInput.value || emailInput.value);
+
+            if (!this.hasContactMean) {
+                this.formErrors.push("noContactMean");
             }
         }
 
         // Form Validity and Error Display
-        this._updateErrorDisplay(formErrors);
-        return formErrors.length === 0;
-    },
+        this.updateErrorDisplay();
+        return this.formErrors.length === 0;
+    }
 
     /**
      * If there are still errors in form, display the error section and
      * compose the error message accordingly.
-     *
-     * @private
-     * @param {Array} errors - Names of errors still present in form.
      */
-    _updateErrorDisplay: function (errors) {
-
-        this.$('.o_wetrack_proposal_error_section').toggleClass('d-none', !errors.length);
-
-        var errorMessages = [];
-        var $errorElement = this.$('.o_wetrack_proposal_error_message');
-
-        if (errors.includes('invalidFormInputs')) {
-            errorMessages.push(_t('Please fill out the form correctly.'));
+    updateErrorDisplay() {
+        const errorMessages = [];
+        
+        if (this.formErrors.includes("invalidFormInputs")) {
+            errorMessages.push(_t("Please fill out the form correctly."));
         }
 
-        if (errors.includes('noContactMean')) {
-            errorMessages.push(_t('Please enter either a contact email address or a contact phone number.'));
+        if (this.formErrors.includes("noContactMean")) {
+            errorMessages.push(_t("Please enter either a contact email address or a contact phone number."));
         }
 
-        if (errors.includes('forbidden')) {
-            errorMessages.push(_t('You cannot access this page.'));
+        if (this.formErrors.includes("forbidden")) {
+            errorMessages.push(_t("You cannot access this page."));
         }
 
-        $errorElement.text(errorMessages.join(' ')).change();
-    },
-
-    //--------------------------------------------------------------------------
-    // Handlers
-    //--------------------------------------------------------------------------
+        const errorElement = this.el.querySelector(".o_wetrack_proposal_error_message");
+        errorElement.textContent = errorMessages.join(" ");
+    }
 
     /**
      * Display / Hide Additional Contact Information section when toggling
      * the checkbox on the form o_wetrack_add_contact_information_checkbox.
      * Also empty the email to prevent hidden email format error.
      *
-     * @private
      * @param {Event} ev
      */
-    _onAdvancedContactToggle: function (ev) {
+    onAdvancedContactToggle(ev) {
         this.useAdvancedContact = !this.useAdvancedContact;
-        var $contactName = this.$(".o_wetrack_contact_name_input")[0];
-        var $advancedInformation = this.$('.o_wetrack_contact_information');
+        const contactEmailInput = this.el.querySelector(".o_wetrack_contact_email_input");
 
-        if (this.useAdvancedContact) {
-            $advancedInformation.removeClass('d-none');
-            $contactName.setAttribute("required", "True");
-        } else {
-            this.$('.o_wetrack_contact_email_input').val('').change();
-            $advancedInformation.addClass('d-none');
-            $contactName.removeAttribute("required");
+        if (!this.useAdvancedContact) {
+            contactEmailInput.value = "";
         }
-    },
+    }
 
     /**
      * Propagates the new input on speaker name to contact name, as long as the latter
      * is the start of partner name. Otherwise, do not modify existing contact name.
      *
-     * @private
      * @param {Event} ev
      */
-    _onPartnerNameInput: function (ev) {
-        var partnerNameText = $(ev.currentTarget).val();
-        var contactNameText = this.$(".o_wetrack_contact_name_input").val();
-        if (partnerNameText.startsWith(contactNameText)) {
-            this.$(".o_wetrack_contact_name_input").val(partnerNameText).change();
+    onPartnerNameInput(ev) {
+        const partnerNameText = ev.currentTarget.value;
+        const contactNameInput = this.el.querySelector(".o_wetrack_contact_name_input");
+        if (partnerNameText.startsWith(contactNameInput.value)) {
+            contactNameInput.value = partnerNameText;
         }
-    },
+    }
 
     /**
      * Submits the form if no errors are present in the form after validation.
@@ -151,47 +129,41 @@ publicWidget.registry.websiteEventTrackProposalForm = publicWidget.Widget.extend
      * To do that we have to compute the position of the beginning of the element, relatively to its
      * position and the amount already scrolled, then subtract the floating header menu.
      *
-     * @private
      * @param {Event} ev
      */
-    _onProposalFormSubmit: async function (ev) {
-        ev.preventDefault();
-        ev.stopPropagation();
-
+    async onProposalFormSubmit(ev) {
         // Prevent further clicking
-        this.$el.find('.o_wetrack_proposal_submit_button')
-            .addClass('disabled')
-            .attr('disabled', 'disabled');
+        const submitButton = this.el.querySelector(".o_wetrack_proposal_submit_button");
+        submitButton.classList.add("disabled");
+        submitButton.setAttribute("disabled", "disabled");
 
         // Submission of the form if no errors remain
-        if (this._isFormValid()) {
-            const formData = new FormData(this.$el[0]);
+        if (this.isFormValid()) {
+            const formData = new FormData(this.el);
+            const eventId = encodeURIComponent(this.el.dataset.eventId);
 
-            const response = await $.ajax({
-                url: `/event/${encodeURIComponent(this.$el.data('eventId'))}/track_proposal/post`,
-                data: formData,
-                processData: false,
-                contentType: false,
-                type: 'POST'
-            });
-
-            const jsonResponse = response && JSON.parse(response);
-            if (jsonResponse.success) {
-                // TODO we really should not remove the whole widget element
-                // like that + probably restore the widget before edit mode etc.
-                const parentEl = this.el.parentNode;
-                this.$el.replaceWith($(renderToElement('event_track_proposal_success')));
-                scrollTo(parentEl, { extraOffset: 20, duration: 50 });
-            } else if (jsonResponse.error) {
-                this._updateErrorDisplay([jsonResponse.error]);
-            }
+            const jsonResponse = await this.waitFor(post(`/event/${eventId}/track_proposal/post`, formData));
+            this.protectSyncAfterAsync(() => {
+                if (jsonResponse.success) {
+                    // TODO we really should not remove the whole widget element
+                    // like that + probably restore the widget before edit mode etc.
+                    const parentEl = this.el.parentNode;
+                    this.services["public.interactions"].stopInteractions(this.el);
+                    this.el.replaceWith(renderToElement("event_track_proposal_success"));
+                    scrollTo(parentEl, { extraOffset: 20, duration: 50 });
+                    return;
+                } else if (jsonResponse.error) {
+                    this.updateErrorDisplay([jsonResponse.error]);
+                }
+            })();
         }
 
         // Restore button
-        this.$el.find('.o_wetrack_proposal_submit_button')
-            .removeAttr('disabled')
-            .removeClass('disabled');
-    },
-});
+        submitButton.classList.remove("disabled");
+        submitButton.removeAttribute("disabled");
+    }
+}
 
-export default publicWidget.registry.websiteEventTrackProposalForm;
+registry
+    .category("public.interactions")
+    .add("website_event_track.website_event_track_proposal_form", WebsiteEventTrackProposalForm);
