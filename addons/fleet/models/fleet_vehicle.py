219107212056ca61005eae3df2625cedf7dfd95a
Mélanie <mepe@odoo.com>
2025-03-25 10:12:54 +0000

    [FIX] fleet: fix mail to driver
    With this commit, the mail's body and subject is rendered when
    it was sent to the vehicle's driver.
    
    task-4671895
    
    closes odoo/odoo#205225
    
    X-original-commit: 61221b2e6552b21508a6a36ab69352e6793d70c5
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/fleet/models/fleet_vehicle.py
+++ addons/fleet/models/fleet_vehicle.py
@@ -434,5 +434,6 @@ class FleetVehicle(models.Model):
             'res_model': 'fleet.vehicle.send.mail',
             'context': {
                 'default_vehicle_ids': self.ids,
+                'default_render_model': 'fleet.vehicle',
             }
         }
