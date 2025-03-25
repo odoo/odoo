2789a0fbfa358c5c164e44380c2a4a10e5679615
Dylan Kiss (dyki) <dyki@odoo.com>
2025-03-25 17:59:10 +0100

    [IMP] account,point_of_sale: restore excluding invoices from follow-ups
    During the [rework of the follow-up report][1], we removed the
    "No Follow-Up" field from journal items, making it impossible to exclude
    individual journal items from triggering a follow-up.
    
    In this commit, we re-introduce a field for that, since it is a common
    requirement to be able to exclude individual items from the follow-up
    reports.
    
    The field is stored on the journal item, but can also be toggled from
    the journal entry. In case of multiple installments on the invoice,
    toggling the field on one installment will toggle it for all
    installments.
    
    [1]: https://github.com/odoo/odoo/commit/67dc71588751c9f115e09959d774686c2207a1f9
    
    task-4632300
    
    closes odoo/odoo#203329
    
    Related: odoo/enterprise#82105
    Signed-off-by: Hugo Poncelet (hupo) <hupo@odoo.com>

--- addons/account/models/account_move_line.py
+++ addons/account/models/account_move_line.py
@@ -465,6 +465,15 @@ class AccountMoveLine(models.Model):
     # === Misc Information === #
     is_refund = fields.Boolean(compute='_compute_is_refund')
 
+    no_followup = fields.Boolean(
+        string="No Follow-Up",
+        compute='_compute_no_followup',
+        inverse='_inverse_no_followup',
+        store=True,
+        readonly=False,
+        help="Exclude this journal item from follow-up reports.",
+    )
+
     _check_credit_debit = models.Constraint(
         "CHECK(display_type IN ('line_section', 'line_subsection', 'line_note') OR credit * debit=0)",
         'Wrong credit or debit value in accounting entry!',
@@ -1221,6 +1230,19 @@ class AccountMoveLine(models.Model):
 
     def _search_parent_id(self, operator, value):
         return [('id', operator, value)]
+
+    @api.depends('move_id.move_type')
+    def _compute_no_followup(self):
+        for aml in self:
+            aml.no_followup = aml.move_id.is_entry() and not aml.move_id.origin_payment_id
+
+    def _inverse_no_followup(self):
+        # If one line of an invoice gets excluded from or included in the follow up report, we want all
+        # payable/receivable lines of that invoice to do the same.
+        for aml in self:
+            move = aml.move_id
+            if move.is_invoice():
+                move.no_followup = aml.no_followup
 
     def _search_payment_date(self, operator, value):
         if operator == 'in':
