odoo/odoo

commit 2afc30af3dd51886fd58bc3a764e9d262fb1cda4

[FIX] website_sale_loyalty: validate rewards before payment
Versions
--------
- 16.0+

Steps
-----
1. Have a payment provider like Demo published;
2. create a discount program that expired yesterday;
3. change time of PC to yesterday;
4. go to eCommerce, add product to cart, go to checkout;
5. follow steps until you get to the "Pay Now" button;
6. change time of PC to today;
7. finalize payment.

Issue
-----
The payment transaction proceeds, but doesn't cover the entire amount,
as the promotion was removed afterwards.

Cause
-----
When initiating payment, there's check on whether the applied rewards
are still valid.

Solution
--------
Add an override for `_validate_transaction_for_order`, which compares the
order amount before and after updating programs & rewards.

If they don't match, raise a `ValidationError`, forcing the user reload
the order without expired programs & rewards.

opw-4304241

closes odoo/odoo#193603

X-original-commit: 84c29f70e9256dff54747b40562edea6973892dd
Signed-off-by: Levi Siuzdak <sile@odoo.com>

addons/website_sale_loyalty/i18n/website_sale_loyalty.pot
@@ -48,6 +48,12 @@ msgid "Available on Website"
 msgstr ""
 
 #. module: website_sale_loyalty
+#. odoo-python
+#: code:addons/website_sale_loyalty/controllers/payment.py:0
+msgid "Cannot process payment: applied reward was changed or has expired."
+msgstr ""
+
+#. module: website_sale_loyalty
 #. odoo-javascript
 #: code:addons/website_sale_loyalty/static/src/js/portal_loyalty_card.xml:0
 #: model_terms:ir.ui.view,arch_db:website_sale_loyalty.modify_code_form

addons/website_sale_loyalty/controllers/__init__.py
@@ -2,3 +2,4 @@
 
 from . import delivery
 from . import main
+from . import payment

addons/website_sale_loyalty/controllers/payment.py
@@ -1,0 +1,25 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import _
+from odoo.exceptions import ValidationError
+
+from odoo.addons.website_sale.controllers import payment
+
+
+class PaymentPortal(payment.PaymentPortal):
+
+    def _validate_transaction_for_order(self, transaction, sale_order):
+        """Update programs & rewards before finalizing transaction.
+
+        :param payment.transaction transaction: The payment transaction
+        :param int order_id: The id of the sale order to pay
+        :raise: ValidationError if the order amount changed after updating rewards
+        """
+        super()._validate_transaction_for_order(transaction, sale_order)
+        if sale_order.exists():
+            initial_amount = sale_order.amount_total
+            sale_order._update_programs_and_rewards()
+            if initial_amount != sale_order.amount_total:
+                raise ValidationError(
+                    _("Cannot process payment: applied reward was changed or has expired.")
+                )

addons/website_sale_loyalty/tests/__init__.py
@@ -1,10 +1,11 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import test_apply_pending_coupon
-from . import test_sale_coupon_multiwebsite
-from . import test_shop_sale_coupon
-from . import test_website_sale_loyalty_delivery
-from . import test_free_product_reward
 from . import test_ewallet
-from . import test_website_sale_auto_invoice
+from . import test_free_product_reward
+from . import test_sale_coupon_multiwebsite
+from . import test_shop_loyalty_payment
 from . import test_shop_multi_reward
+from . import test_shop_sale_coupon
+from . import test_website_sale_auto_invoice
+from . import test_website_sale_loyalty_delivery

addons/website_sale_loyalty/tests/test_shop_loyalty_payment.py
@@ -1,0 +1,91 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from datetime import date, timedelta
+from freezegun import freeze_time
+
+from odoo import Command
+from odoo.tests import JsonRpcException, tagged
+from odoo.tools import mute_logger
+
+from odoo.addons.payment.tests.http_common import PaymentHttpCommon
+from odoo.addons.sale_loyalty.tests.common import TestSaleCouponCommon
+
+
+@tagged('post_install', '-at_install')
+class TestShopLoyaltyPayment(PaymentHttpCommon, TestSaleCouponCommon):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.website = cls.env.company.website_id
+        if not cls.website:
+            cls.website = cls.env.ref('website.default_website')
+            cls.website.company_id = cls.env.company
+
+    @mute_logger('odoo.http')
+    def test_expired_reward_validation(self):
+        """Ensure payments don't process if any applied reward is no longer valid."""
+        order = self.empty_order
+        program = self.program_gift_card
+
+        program.date_to = date.today()  # set program to expire after today
+
+        self.env['loyalty.generate.wizard'].with_context(active_id=program.id).create({
+            'coupon_qty': 1,
+            'points_granted': 100,
+        }).generate_coupons()
+
+        order.write({
+            'partner_id': self.portal_partner.id,
+            'website_id': self.website.id,
+            'message_partner_ids': self.portal_partner.ids,
+            'order_line': [Command.create({
+                'product_id': self.service_product.id,
+                'tax_id': None,
+            })],
+        })
+        self._apply_promo_code(order, program.coupon_ids.code)
+
+        with freeze_time(program.date_to + timedelta(days=1)):
+            self.authenticate(self.portal_user.login, self.portal_user.login)
+            with self.assertRaises(
+                JsonRpcException,
+                msg="Payment shouldn't succeed with expired reward still applied",
+            ):
+                self.make_jsonrpc_request(
+                    self._build_url(f'/shop/payment/transaction/{order.id}'),
+                    {
+                        'order_id': order.id,
+                        'access_token': None,
+                        'amount': order.amount_total,
+                        'provider_id': self.provider.id,
+                        'payment_method_id': self.payment_method.id,
+                        'flow': 'direct',
+                        'token_id': None,
+                        'tokenization_requested': False,
+                        'landing_route': order.get_portal_url(),
+                    },
+                )
+
+            # Update rewards & retry transaction
+            order._update_programs_and_rewards()
+            tx_response = self.make_jsonrpc_request(
+                self._build_url(f'/shop/payment/transaction/{order.id}'),
+                {
+                    'order_id': order.id,
+                    'access_token': None,
+                    'amount': order.amount_total,
+                    'provider_id': self.provider.id,
+                    'payment_method_id': self.payment_method.id,
+                    'flow': 'direct',
+                    'token_id': None,
+                    'tokenization_requested': False,
+                    'landing_route': order.get_portal_url(),
+                },
+            )
+            self.assertEqual(
+                tx_response['amount'],
+                self.service_product.list_price,
+                "Payment should succeed after removing expired reward",
+            )

commit be5fd0222c2d1fbc8e2579d55c4dcca27a34a5f2

[FIX] website_sale_loyalty: edit validation error message
Commit 2afc30a introduced reward validation before payment, due to this
if promotion expired during payment step during checkout, error informing
that promotion was expired was thrown, but user was not able to do
anything about. Info about refreshing page has been added.

opw-4573640

addons/website_sale_loyalty/controllers/main.py
@@ -160,5 +160,8 @@ class PaymentPortal(main.PaymentPortal):
             order_sudo.validate_taxes_on_sales_order()  # re-applies taxcloud taxes if necessary
             if order_sudo.currency_id.compare_amounts(initial_amount, order_sudo.amount_total):
                 raise ValidationError(
-                    _("Cannot process payment: applied reward was changed or has expired.")
+                    _(
+                        "Cannot process payment: applied reward was changed or has expired.\n"
+                        "Please refresh the page and try again."
+                    )
                 )

addons/website_sale_loyalty/i18n/website_sale_loyalty.pot
@@ -53,7 +53,9 @@ msgstr ""
 #. odoo-python
 #: code:addons/website_sale_loyalty/controllers/main.py:0
 #, python-format
-msgid "Cannot process payment: applied reward was changed or has expired."
+msgid ""
+"Cannot process payment: applied reward was changed or has expired.\n"
+"Please refresh the page and try again."
 msgstr ""
 
 #. module: website_sale_loyalty