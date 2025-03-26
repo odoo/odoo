23b9d6e7c16bbd4d3f1ad240132468700acee4d8
Drishti <drishti@shiperoo.com>
2025-03-26 13:48:55 +1100

    [MOD] logic added to calculate xdock qty and boolean field added to display message Error message in log

--- custom_addons/custom_odooship_decanting_process/models/stock_move_inherit.py
+++ custom_addons/custom_odooship_decanting_process/models/stock_move_inherit.py
@@ -12,8 +12,31 @@ class StockMoveLine(models.Model):
                                               string='Delivery Receipt Status', default='draft')
     packed = fields.Boolean('Packed', default=False)
     released_manual = fields.Boolean('Released', default=False)
-    xdock_qty = fields.Float(string='XDOCk Quantity')
+    xdock_qty = fields.Float(string='XDOCk Quantity', compute='_compute_xdock_packaging_quantity')
     xdock_remaining_qty = fields.Float(string='XDOCk Remaining Quantity')
+    product_packaging_id = fields.Many2one('product.packaging', 'Packaging', domain="[('product_id', '=', product_id)]",
+                                           check_company=True)
+    xdock_packaging_qty = fields.Float(
+        string="Xdock Packaging Quantity")
+
+    @api.depends('product_packaging_id', 'xdock_packaging_qty')
+    def _compute_xdock_packaging_quantity(self):
+        """
+        Computes xdock_qty by converting the xdock_packaging_qty using the packaging's conversion factor.
+        """
+        for move in self:
+            if move.product_packaging_id and move.xdock_packaging_qty:
+                move.xdock_qty = move.product_packaging_id.qty * move.xdock_packaging_qty
+            else:
+                move.xdock_qty = 0.0
+
+    @api.depends('xdock_qty')
+    def _compute_xdock_qty_remaining_qty(self):
+        """
+        Compute Xdock remaining quantity based on the XDOCK quantity ordered
+        """
+        for move in self:
+            move.xdock_remaining_qty = move.xdock_qty
 
     @api.depends('quantity')
     def _compute_remaining_qty(self):
@@ -24,7 +47,6 @@ class StockMoveLine(models.Model):
         for move in self:
             # Initial remaining qty is the product_uom_qty (expected qty)
             move.remaining_qty = move.quantity
-            # move.is_remaining_qty = move.remaining_qty > 0
 
     @api.depends('remaining_qty')
     def _compute_delivery_receipt_state(self):
