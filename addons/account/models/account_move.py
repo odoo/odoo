60c9a99e583df4ba17bd7ad6cd2d62cef4284453
william-andre <wan@odoo.com>
2025-03-27 08:27:44 +0000

    [FIX] account: synchronized between analytic item and journal item
    When modifying or deleting an Analytic Account on an Analytic Item, it
    is not reflected on the Analytic Distribution of the Journal Item.
    The user can be mislead, given on the business objects (invoice lines),
    he sees the initial Analytic Distribution but there is no guarantee
    that the related Analytic Item remains consistent.
    
    task-4378407
    
    X-original-commit: 41247837941d1e4572883cd8aa4feb71c6b8b9f8

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -5472,7 +5472,7 @@ class AccountMove(models.Model):
 
         self._check_draftable()
         # We remove all the analytics entries for this journal
-        self.mapped('line_ids.analytic_line_ids').unlink()
+        self.line_ids.analytic_line_ids.with_context(skip_analytic_sync=True).unlink()
         self.mapped('line_ids').remove_move_reconcile()
         self.state = 'draft'
 
