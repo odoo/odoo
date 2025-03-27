c507643a63f689b6efbddd5602f03313f6ac7eb8
Thibault Delavallée <tde@odoo.com>
2025-03-27 20:10:04 +0100

    [FIX] mail, various: update query counters
    Update counters due to changes in this PR. Notably default recipients computation
    used with templates, mailings, ... now checks for aliases and alias domains which
    means additional queries.
    
    Task-4668703
    
    X-Original-Commit: odoo/odoo@43bfdcb91a1a19ffe985bd4c8e11624ddf901890

--- addons/event/tests/test_event_mail_schedule.py
+++ addons/event/tests/test_event_mail_schedule.py
@@ -643,7 +643,7 @@ class TestMailSchedule(EventMailCommon):
         ]})
         self.env.invalidate_all()
         # event 50
-        with self.assertQueryCount(66), \
+        with self.assertQueryCount(69), \
              self.mock_datetime_and_now(reference_now + relativedelta(minutes=10)), \
              self.mock_mail_gateway():
             _new = self.env['event.registration'].create([
