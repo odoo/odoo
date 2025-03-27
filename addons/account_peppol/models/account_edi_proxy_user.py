7c506c4c92c25568f5b0c0d6591f57acd869ca87
Louis Gobert <loug@odoo.com>
2025-03-27 13:42:30 +0000

    [FIX] account_peppol: search only for Peppol EDI users
    Ensure only Peppol-type proxies are retrieved when performing
    Peppol operations.
    
    Steps to reproduce:
    - Install Peppol and IT EDI
    - Register Peppol and IT EDI users
    - In Accounting, on the Vendor Bill journal, click "Fetch from Peppol"
    
    Odoo will attempt requests for all users, including an invalid call
    to 'False/api/peppol/1/get_all_documents'. This is not an issue in V17
    but causes problems in V18.
    
    This fix makes sense in both versions as it prevents unnecessary
    requests and avoids error messages in the logs.
    
    opw-4624633
    
    X-original-commit: 7422b21048a34c2bcd3e33fc4f81e5577898a0bb

--- addons/account_peppol/models/account_edi_proxy_user.py
+++ addons/account_peppol/models/account_edi_proxy_user.py
@@ -98,11 +98,11 @@ class Account_Edi_Proxy_ClientUser(models.Model):
     # -------------------------------------------------------------------------
 
     def _cron_peppol_get_new_documents(self):
-        edi_users = self.search([('company_id.account_peppol_proxy_state', '=', 'receiver')])
+        edi_users = self.search([('company_id.account_peppol_proxy_state', '=', 'receiver'), ('proxy_type', '=', 'peppol')])
         edi_users._peppol_get_new_documents(skip_no_journal=True)
 
     def _cron_peppol_get_message_status(self):
-        edi_users = self.search([('company_id.account_peppol_proxy_state', 'in', self._get_can_send_domain())])
+        edi_users = self.search([('company_id.account_peppol_proxy_state', 'in', self._get_can_send_domain()), ('proxy_type', '=', 'peppol')])
         edi_users._peppol_get_message_status()
 
     def _cron_peppol_get_participant_status(self):
