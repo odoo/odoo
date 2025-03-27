35b05f0aec0c8d4caa19f010651a4a0d7af3b62c
Ethan Vincent <etvi@odoo.com>
2025-03-27 14:40:02 +0000

    [FIX] sale_project{,_stock}: report COGS invoice lines as costs
    **Current behavior:**
    Using auto valuation and anglo saxon accounting, when a project
    has some profitability report items for auto valued product, the
    COGS invoice lines will appear under the revenue section rather
    than the cost section of the project's profitability report.
    
    **Expected behavior:**
    COGS lines are shown as costs.
    
    **Steps to reproduce:**
    1. Create a service product that generates a project on sale, on
    the project template set a specific analytic account
    
    2. Create another product with real time valuation and assign
    the COGS account on the product category's expense account
    
    3. Sell some of the service product and the auto val product in
    the same order, deliver it -> generate invoice & post it
    
    4. In the project's settings, open the profitability report
    observe that the invoice line for the cost of goods sold account
    entry is displayed as a negative revenue, rather than a cost
    
    **Cause of the issue:**
    In `sale_project` there is no logic to separate the COGS AMLs
    from the rest of an invoice's line ids.
    
    **Fix:**
    Transform the loop in `_get_revenues_items_from_invoices`
    https://github.com/odoo/odoo/blob/bb6a4fbb92b1a1a1e13e32b27c4c9f2813570fda/addons/sale_project/models/project.py#L656
    into two loops such that the existing one iterates twice.
    
    First iteration collects the `revenues` items data (exactly as
    it currently does)
    
    Second iteration collects the `costs` items (cogs lines)
    
    And the method will now return a dict of data for both
    `revenues` report items and `costs` report items (and should be
    renamed/refactored in master)
    
    opw-4652472
    
    X-original-commit: 2152ac5d086c0438bc60d5ac07af54a93dfd7231

--- addons/sale_project_stock/tests/__init__.py
+++ addons/sale_project_stock/tests/__init__.py
@@ -1,3 +1,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+<<<<<<< f8dc394a37b5c969a28a00666e72edac289f4a34
 from . import test_reinvoice
+||||||| 2ac687f59f320e40d1ff0bf5915c30544f1c5e00
+=======
+from . import test_sale_project_stock_profitability
+>>>>>>> e24c61a4e12542376310b1694709b1847a8e90ab
