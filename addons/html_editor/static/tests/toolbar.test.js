be129be8bb10ccab53664c2d5e7aca3031493837
Adnan Saiyed <adsa@odoo.com>
2025-03-27 07:00:45 +0000

    [FIX] html_editor: traceback when cropping image
    Current behavior before commit:
    
    Steps to reproduce the issue:
    
    - Upload an external image.
    - Click on it to open the toolbar.
    - Try to open image cropper.
    - Clicking on "Apply" button leads to traceback.
    
    This issue happens because after merging this commit [1]
    closeCropper method closes the cropper only if cropperOpen
    flag is true. In case of external image, cropper should be
    closed before it gets fully mounted as such images are
    uncroppable. In this case cropperOpen flag is false and
    closeCropper fails to close cropper which results in traceback
    later.
    
    Desired behavior after commit:
    
    Cropper gets closed in case of external image and there is a
    toaster notification at the top-right showing "This type of
    image is not supported for cropping".
    
    [1]: df64afb4e9504413f966a153772c568b625a5e13
    
    task-4677287
    
    closes odoo/odoo#214793
    
    X-original-commit: aa44f392fe7f1c13bb2a4078ed520a29cb9c08f4
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/html_editor/static/tests/toolbar.test.js
+++ addons/html_editor/static/tests/toolbar.test.js
@@ -1120,8 +1120,7 @@ test("close the toolbar if the selection contains any nodes (traverseNode = [], 
 });
 
 test.tags("desktop");
-// TODO mysterious egg
-test.todo("should not close image cropper while loading media", async () => {
+test("should be able to close image cropper while loading the media", async () => {
     onRpc("/html_editor/get_image_info", () => ({
         original: {
             image_src: "#",
@@ -1145,24 +1144,8 @@ test.todo("should not close image cropper while loading media", async () => {
     await click('.btn[title="Discard"]');
     await animationFrame();
 
-    // cropper should not close as the cropper still loading the image.
-    expect('.btn[title="Discard"]').toHaveCount(1);
-
-    // once the image loaded we should be able to close
-    await waitFor('img[src^="blob:"]', { timeout: 2000 });
-    await click('.btn[title="Discard"]');
-    await waitForNone('.btn[title="Discard"]', { timeout: 1000 });
-
-    await click("img");
-    await waitFor(".o-we-toolbar", { timeout: 1000 });
-
-    await click('button[name="image_transform"]');
-    await animationFrame();
-
-    await waitFor('.btn[name="image_crop"]', { timeout: 1000 });
-    await click('.btn[name="image_crop"]');
-    await waitFor('.btn[title="Discard"]', { timeout: 1000 });
-    expect('.btn[title="Discard"]').toHaveCount(1);
+    // Cropper should get closed while the cropper still loading the image.
+    expect('.btn[title="Discard"]').toHaveCount(0);
 });
 
 test("toolbar shouldn't be visible if can_display_toolbar === false", async () => {
