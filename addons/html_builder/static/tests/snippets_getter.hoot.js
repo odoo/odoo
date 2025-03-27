1bcac474ecabc7892f006cd04c6abd54445361f1
Géry Debongnie <ged@odoo.com>
2025-03-27 14:26:47 +0100

    prevent loading images inside iframe

--- addons/html_builder/static/tests/snippets_getter.hoot.js
+++ addons/html_builder/static/tests/snippets_getter.hoot.js
@@ -1,14 +1,29 @@
 import { realOrm } from "@web/../tests/_framework/module_set.hoot";
 
+function removeImageSrc(xmlString) {
+    const doc = new DOMParser().parseFromString(xmlString, "text/html");
+    for (let img of doc.getElementsByTagName("img")) {
+        img.removeAttribute("src");
+    }
+    const elementsWithBackgroundImage = doc.querySelectorAll('[style*="background-image"]');
+    for (let el of elementsWithBackgroundImage) {
+        const style = el.getAttribute("style");
+        const newStyle = style.replace(/background-image\s*:\s*url\([^\)]+\);?/g, ''); // Remove background-image rule
+        el.setAttribute("style", newStyle);
+    }
+    return new XMLSerializer().serializeToString(doc);
+}
+
 let websiteSnippets;
 export const getWebsiteSnippets = async () => {
     if (!websiteSnippets) {
-        websiteSnippets = await realOrm(
+        const str = await realOrm(
             "ir.ui.view",
             "render_public_asset",
             ["website.snippets"],
             {}
         );
+        websiteSnippets = removeImageSrc(str.trim());
     }
     return websiteSnippets;
 };
