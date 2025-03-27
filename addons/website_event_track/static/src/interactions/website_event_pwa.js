91f9c6643cbd2c96034fb80880d39cb52353b3c7
Augustin (duau) <duau@odoo.com>
2025-03-27 11:42:21 +0100

    [REF] website_event_track: rewrite WebsiteEventPWA as Interaction
    This commit rewrites WebsiteEventPWAWidget as an Interaction.
    
    task-4367641
    
    closes odoo/odoo#203686
    
    Signed-off-by: Benoit Socias (bso) <bso@odoo.com>

--- addons/website_event_track/static/src/interactions/website_event_pwa.js
+++ addons/website_event_track/static/src/interactions/website_event_pwa.js
@@ -1,202 +1,121 @@
-    /*
-     * The "deferredPrompt" Promise will resolve only if the "beforeinstallprompt" event
-     * has been triggered. It allows to register this listener as soon as possible
-     * to avoid missed-events (as the browser can trigger it very early in the page lifecycle).
-     */
-    var deferredPrompt = new Promise(function (resolve, reject) {
-        if ("serviceWorker" in navigator) {
-            window.addEventListener("beforeinstallprompt", function (ev) {
-                ev.preventDefault();
-                resolve(ev);
-            });
-        } else {
-            console.log("ServiceWorker not supported");
-        }
-    });
+import { Interaction } from "@web/public/interaction";
+import { registry } from "@web/core/registry";
+import { utils as uiUtils } from "@web/core/ui/ui_service";
 
-    import publicWidget from "@web/legacy/js/public/public_widget";
-    import { utils as uiUtils } from "@web/core/ui/ui_service";
+export class WebsiteEventPWA extends Interaction {
+    static selector = "#wrapwrap.event";
 
-    var PWAInstallBanner = publicWidget.Widget.extend({
-        template: "pwa_install_banner",
-        events: {
-            "click .o_btn_install": "_onClickInstall",
-            "click .o_btn_close": "_onClickClose",
-        },
-
-        /**
-         * @private
-         */
-        _onClickClose: function () {
-            this.trigger_up("prompt_close_bar");
-        },
-
-        /**
-         * @private
-         */
-        _onClickInstall: function () {
-            this.trigger_up("prompt_install");
-        },
-    });
-
-    publicWidget.registry.WebsiteEventPWAWidget = publicWidget.Widget.extend({
-        selector: "#wrapwrap.event",
-        custom_events: {
-            prompt_install: "_onPromptInstall",
-            prompt_close_bar: "_onPromptCloseBar",
-        },
-
-        /**
-         *
-         * @override
-         */
-        start: function () {
-            var self = this;
-            return this._super.apply(this, arguments)
-                .then(this._registerServiceWorker.bind(this))
-                .then(function () {
-                    // Don't wait for the prompt's Promise as it may never resolve.
-                    deferredPrompt.then(self._showInstallBanner.bind(self));
-                })
-                .then(this._prefetch.bind(this));
-        },
-
-        /**
-         *
-         * @override
-         */
-        destroy: function () {
-            this._super.apply(this, arguments);
-        },
-
-        //--------------------------------------------------------------------------
-        // Private
-        //--------------------------------------------------------------------------
-
-        /**
-         * Returns the PWA's scope
-         *
-         * Note: this method performs a matching to handle URLs with the language prefix.
-         *       Typically this prefix is in the form of "en" or "en_US" but it can also be
-         *       any string using the customization options in the Website's settings.
-         * @private
-         * @returns {String}
-         */
-        _getScope: function () {
-            var matches = window.location.pathname.match(/^(\/(?:event|[^/]+\/event))\/?/);
-            if (matches && matches[1]) {
-                return matches[1];
-            }
-            return "/event";
-        },
-
-        /**
-         * @private
-         */
-        _hideInstallBanner: function () {
-            this.installBanner ? this.installBanner.destroy() : undefined;
-            $(".o_livechat_button").css("bottom", "0");
-        },
-
-        /**
-         * Parse the current page for first-level children pages and ask the ServiceWorker
-         * to already fetch them to populate the cache.
-         * @private
-         */
-        _prefetch: function () {
-            if (!("serviceWorker" in navigator)) {
-                return;
-            }
-            var assetsUrls = Array.from(document.querySelectorAll('link[rel="stylesheet"], script[src]')).map(function (el) {
-                return el.href || el.src;
-            });
-            navigator.serviceWorker.ready.then(function (registration) {
-                registration.active.postMessage({
-                    action: "prefetch-assets",
-                    urls: assetsUrls,
-                });
-            }).catch(function (error) {
-                console.error("Service worker ready failed, error:", error);
-            });
-            var currentPageUrl = window.location.href;
-            var childrenPagesUrls = Array.from(document.querySelectorAll('a[href^="' + this._getScope() + '/"]')).map(function (el) {
-                return el.href;
-            });
-            navigator.serviceWorker.ready.then(function (registration) {
-                registration.active.postMessage({
-                    action: "prefetch-pages",
-                    urls: childrenPagesUrls.concat(currentPageUrl),
-                });
-            }).catch(function (error) {
-                console.error("Service worker ready failed, error:", error);
-            });
-        },
-
-        /**
-         *
-         * @private
-         */
-        _registerServiceWorker: function () {
-            if (!("serviceWorker" in navigator)) {
-                return;
-            }
-            var scope = this._getScope();
-            return navigator.serviceWorker
-                .register(scope + "/service-worker.js", { scope: scope })
-                .catch(function (error) {
-                    console.error("Service worker registration failed, error:", error);
-                });
-        },
-
-        /**
-         * @private
-         */
-        _showInstallBanner: function () {
-            if (!uiUtils.isSmall()) {
-                return;
-            }
-            var self = this;
-            this.installBanner = new PWAInstallBanner(this);
-            this.installBanner.appendTo(this.$el).then(function () {
-                // If Livechat available, It should be placed above the PWA banner.
-                var height = self.$(".o_pwa_install_banner").outerHeight(true);
-                $(".o_livechat_button").css("bottom", height + "px");
-            });
-        },
-
-        //--------------------------------------------------------------------------
-        // Handlers
-        //--------------------------------------------------------------------------
-
-        /**
-         * @private
-         * @param ev {Event}
-         */
-        _onPromptCloseBar: function (ev) {
-            ev.stopPropagation();
-            this._hideInstallBanner();
-        },
-        /**
-         * @private
-         * @param ev {Event}
-         */
-        _onPromptInstall: function (ev) {
-            ev.stopPropagation();
-            this._hideInstallBanner();
-            deferredPrompt.then(function (prompt) {
-                    prompt.prompt();
-                    prompt.userChoice.then(function (choiceResult) {
-                        if (choiceResult.outcome === "accepted") {
-                            console.log("User accepted the install prompt");
-                        } else {
-                            console.log("User dismissed the install prompt");
-                        }
-                    });
-                });
-        },
-    });
-
-    export default {
-        PWAInstallBanner: PWAInstallBanner,
-        WebsiteEventPWAWidget: publicWidget.registry.WebsiteEventPWAWidget,
+    dynamicContent = {
+        _window : { "t-on-beforeinstallprompt.prevent": (event) => this.showInstallBanner(event) },
+        ".o_btn_close": { "t-on-click": this.hideInstallBanner },
+        ".o_btn_install": { "t-on-click": this.onPromptInstall },
     };
+
+    async willStart() {
+        await this.registerServiceWorker().then(this.prefetch());
+    }
+
+    /**
+     * Returns the PWA's scope
+     *
+     * Note: this method performs a matching to handle URLs with the language prefix.
+     *       Typically this prefix is in the form of "en" or "en_US" but it can also be
+     *       any string using the customization options in the Website's settings.
+     * @returns {String}
+     */
+    getScope() {
+        var matches = window.location.pathname.match(/^(\/(?:event|[^/]+\/event))\/?/);
+        if (matches && matches[1]) {
+            return matches[1];
+        }
+        return "/event";
+    }
+
+    hideInstallBanner() {
+        this.installBanner ? this.installBanner[0].remove() : undefined;
+        const shadowHostEl = document.querySelector(".o-livechat-root");
+        const livechatButtonEl = shadowHostEl?.shadowRoot.querySelector(".o-livechat-LivechatButton");
+        if (livechatButtonEl) {
+            livechatButtonEl.style.position = "";
+            livechatButtonEl.style.bottom = "0";
+        }
+    }
+
+    /**
+     * Parse the current page for first-level children pages and ask the ServiceWorker
+     * to already fetch them to populate the cache.
+     */
+    prefetch() {
+        if (!("serviceWorker" in navigator)) {
+            return;
+        }
+        var assetsUrls = Array.from(document.querySelectorAll("link[rel='stylesheet'], script[src]")).map(function (el) {
+            return el.href || el.src;
+        });
+        navigator.serviceWorker.ready.then(function (registration) {
+            registration.active.postMessage({
+                action: "prefetch-assets",
+                urls: assetsUrls,
+            });
+        }).catch(function (error) {
+            console.error("Service worker ready failed, error:", error);
+        });
+        var currentPageUrl = window.location.href;
+        var childrenPagesUrls = Array.from(document.querySelectorAll("a[href^='" + this.getScope() + "/']")).map(function (el) {
+            return el.href;
+        });
+        navigator.serviceWorker.ready.then(function (registration) {
+            registration.active.postMessage({
+                action: "prefetch-pages",
+                urls: childrenPagesUrls.concat(currentPageUrl),
+            });
+        }).catch(function (error) {
+            console.error("Service worker ready failed, error:", error);
+        });
+    }
+
+    async registerServiceWorker() {
+        if (!("serviceWorker" in navigator)) {
+            return;
+        }
+        var scope = this.getScope();
+        return navigator.serviceWorker
+            .register(scope + "/service-worker.js", { scope: scope })
+            .catch(function (error) {
+                console.error("Service worker registration failed, error:", error);
+            });
+    }
+
+    showInstallBanner(ev) {
+        if (!uiUtils.isSmall()) {
+            return;
+        }
+        this.beforeInstallEvent = ev;
+        this.installBanner = this.renderAt("website_event_track.pwa_install_banner");
+        
+        // If Livechat available, It should be placed above the PWA banner.
+        const height = document.querySelector(".o_pwa_install_banner").offsetHeight;
+        const shadowHostEl = document.querySelector(".o-livechat-root");
+        const livechatButtonEl = shadowHostEl?.shadowRoot.querySelector(".o-livechat-LivechatButton");
+        if (livechatButtonEl) {
+            livechatButtonEl.style.position = "relative";
+            livechatButtonEl.style.bottom = `${height}px`;
+        }
+    }
+
+    onPromptInstall() {
+        this.hideInstallBanner();
+        this.beforeInstallEvent.prompt();
+        this.beforeInstallEvent.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === "accepted") {
+                console.log("User accepted the install prompt");
+            } else {
+                console.log("User dismissed the install prompt");
+            }
+        });
+    }
+}
+
+registry
+    .category("public.interactions")
+    .add("website_event_track.website_event_pwa", WebsiteEventPWA);
