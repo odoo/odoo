dc258497e366fe623b438e9c64ab38453b724d3b
prep-odoo <prep@odoo.com>
2025-03-27 08:22:23 +0000

    [IMP] l10n_in_edi: Log Message in chatter for Invalid Optional Fields
    Previously, all available data was sent to the E-Invoice system,
    even if some fields contained incorrect values.
    However, certain fields (e.g., email) have validation but are not required.
    
    **After this commit:**
    - Before generating the final JSON, only correctly validated
      fields are included.
    - Ensured that incorrect or improperly formatted data does not get sent.
    - Invalid optional fields are now logged  in the chatter with error message.
    
    task-4594234
    
    closes odoo/odoo#209532
    
    X-original-commit: 2e0140dce8505331b067231e249bbabf4d7de71e
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>
    Signed-off-by: Prem Patel (prep) <prep@odoo.com>

--- addons/l10n_in_edi/models/res_partner.py
+++ addons/l10n_in_edi/models/res_partner.py
@@ -18,23 +18,12 @@ class ResPartner(models.Model):
         message = []
         if not re.match("^.{3,100}$", self.street or ""):
             message.append(_("- Street required min 3 and max 100 characters"))
-        if self.street2 and not re.match("^.{3,100}$", self.street2):
-            message.append(_("- Street2 should be min 3 and max 100 characters"))
         if not re.match("^.{3,100}$", self.city or ""):
             message.append(_("- City required min 3 and max 100 characters"))
         if self.country_id.code == "IN" and not re.match("^.{3,50}$", self.state_id.name or ""):
             message.append(_("- State required min 3 and max 50 characters"))
         if self.country_id.code == "IN" and not re.match("^([1-9][0-9]{5})$", self.zip or ""):
             message.append(_("- ZIP code required 6 digits ranging from 100000 to 999999"))
-        if self.phone and not re.match("^[0-9]{10,12}$",
-            self.env['account.move']._l10n_in_extract_digits(self.phone)
-        ):
-            message.append(_("- Mobile number should be minimum 10 or maximum 12 digits"))
-        if self.email and (
-            not re.match(r"^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$", self.email)
-            or not re.match("^.{6,100}$", self.email)
-        ):
-            message.append(_("- Email address should be valid and not more then 100 characters"))
         if message:
             message.insert(0, self.display_name)
         return message
@@ -44,15 +33,17 @@ class ResPartner(models.Model):
             'partner_address_missing': {
                 'fields': ('street', 'zip', 'city', 'state_id', 'country_id',),
                 'message': _(
-                    "Partners should have a complete address, verify their Street, City, State, "
-                    "Country and Zip code."
+                    "Partners should have a complete address, verify their Street, City, State, Country and Zip code."
                 ),
             },
         }
         return {
             f"l10n_in_edi_{key}": {
                 'message': check['message'],
-                'action_text': _("View Partners"),
+                'action_text': (
+                    _("View Partners") if len(invalid_records) > 1
+                    else _("View %s", invalid_records.name)
+                ),
                 'action': invalid_records._get_records_action(name=_("Check Partner Data")),
             }
             for key, check in checks.items()
