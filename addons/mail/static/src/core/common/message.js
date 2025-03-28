2af4c296ee811027b33a649354d527b3b2d49e0b
mkap-odoo <mkap@odoo.com>
2025-03-28 08:37:48 +0530

    [IMP] mail: enable expandable images in activities
    In this commit:
     - Users can now expand the images when attached to activities by clicking an
    image for a better viewing experience, with options to zoom, reset, print and
    download.
    
    task - 4599796

--- addons/mail/static/src/core/common/message.js
+++ addons/mail/static/src/core/common/message.js
@@ -41,6 +41,7 @@ import { discussComponentRegistry } from "./discuss_component_registry";
 import { NotificationMessage } from "./notification_message";
 import { useLongPress } from "@mail/utils/common/hooks";
 import { ActionList } from "./action_list";
+import { useFileViewer } from "@web/core/file_viewer/file_viewer_hook";
 
 /**
  * @typedef {Object} Props
@@ -130,6 +131,7 @@ export class Message extends Component {
         this.ui = useService("ui");
         this.openReactionMenu = this.openReactionMenu.bind(this);
         this.optionsDropdown = useDropdownState();
+        this.fileViewer = useFileViewer();
         useChildSubEnv({
             message: this.props.message,
             alignedRight: this.isAlignedRight,
@@ -371,6 +373,19 @@ export class Message extends Component {
      * @param {MouseEvent} ev
      */
     async onClick(ev) {
+        const img = ev.target.closest('.o_mail_activity_note_content img');
+        if (img) {
+            ev.stopPropagation();
+            const imgName = img.src ? decodeURIComponent(img.src.split('/').pop().split('?')[0]) : '';
+            const fileModel = {
+                isImage: true,
+                isViewable: true,
+                name: imgName,
+                defaultSource: img.src,
+                downloadUrl: img.src,
+            };
+        this.fileViewer.open(fileModel);
+    }
         if (this.store.handleClickOnLink(ev, this.props.thread)) {
             return;
         }
