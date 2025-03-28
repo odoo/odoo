b0417bbc84b706e2c3f1310f6bed661f3fd6c67a
vava-odoo <vava@odoo.com>
2025-03-28 14:19:17 +0100

    [MOV] partnership,website_crm_partner_assign: move grade to new module
    As part of the refactor of the way we handle partnership and membership,
    this commit moves the code linked to res.partner.grade from
    website_crm_partner_assign to a new partnership module.
    
    task-4421310
    
    Part-of: odoo/odoo#197228
    Related: odoo/enterprise#79016
    Related: odoo/documentation#12139
    Related: odoo/upgrade#7202
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>

---
+++ addons/partnership/__manifest__.py
@@ -0,0 +1,22 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Partnership / Membership',
+    'version': '1.0',
+    'category': 'Sales/CRM',
+    'description': """
+This module allows you to manage all operations for managing memberships and partnerships.
+==========================================================================================
+
+You can easily assign grade to members.
+    """,
+    'depends': ['crm'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/res_partner_grade_data.xml',
+        'views/res_partner_views.xml',
+        'views/res_partner_grade_views.xml',
+        'views/partnership_menu.xml',
+    ],
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
