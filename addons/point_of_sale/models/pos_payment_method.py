ff665ca06ea74236f476e2b2e5d686d3b1adc945
manv-afk <manv@odoo.com>
2025-03-28 14:14:17 +0000

    [FIX] point_of_sale: fix exception when creating payment method
    Fix issue when trying to create new payment methods and no `account_journal_payment_debit_account_id` exist in the current chart template.
    
    Calling `_get_outstanding_account('inbound')` on an empty `account.payment` was failing
    because `self.env.company` was not properly set. This issue only occurred when the
    outstanding account was not found in the chart template, leading to an exception.
    
    closes odoo/odoo#204063
    
    X-original-commit: e3988282f7567dd14e987412ee501732f884c2ad
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: Manu Vaillant (manv) <manv@odoo.com>

--- addons/point_of_sale/models/pos_payment_method.py
+++ addons/point_of_sale/models/pos_payment_method.py
@@ -121,7 +121,8 @@ class PosPaymentMethod(models.Model):
             if pm.journal_id and pm.journal_id.type not in ['cash', 'bank']:
                 raise UserError(_("Only journals of type 'Cash' or 'Bank' could be used with payment methods."))
             if pm.journal_id and pm.journal_id.type == 'bank':
-                pm.outstanding_account_id = self.env['account.payment']._get_outstanding_account('inbound')
+                chart_template = self.with_context(allowed_company_ids=self.env.company.root_id.ids).env['account.chart.template']
+                pm.outstanding_account_id = chart_template.ref('account_journal_payment_debit_account_id', raise_if_not_found=False) or self.company_id.transfer_account_id
         if self.is_cash_count:
             self.use_payment_terminal = False
 
