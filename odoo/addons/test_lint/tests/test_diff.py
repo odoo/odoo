9854741a57dfd5698255c7c9a91365dcbe36f473
Chong Wang (cwg) <cwg@odoo.com>
2025-03-29 00:04:00 +0100

    [IMP] add xml check

--- odoo/addons/test_lint/tests/test_diff.py
+++ odoo/addons/test_lint/tests/test_diff.py
@@ -4,11 +4,18 @@ import os
 import re
 import subprocess
 
-from odoo.tools import OrderedSet, config
-from odoo.tools.which import which
+from collections import defaultdict
+from lxml import etree
+
+from odoo.modules.module import get_manifest
 from odoo.tests import BaseCase
+from odoo.tools import OrderedSet, config
+from odoo.tools.misc import file_open, file_path
+from odoo.tools.which import which
+
 
 _logger = logging.getLogger(__name__)
+
 
 def get_repos() -> list[str]:
     """ get all git repos in the given path """
@@ -57,7 +64,7 @@ def get_diff_files(repo_path: str, base_version: str, filter: str | None = None)
     try:
         git = which('git')
         result = subprocess.run(
-            [git, 'diff', '--name-only', f'{base_version}'] + filter_cmd,
+            [git, 'diff', '--name-only', base_version] + filter_cmd,
             capture_output=True,
             check=False,
             text=True,
@@ -84,7 +91,32 @@ def get_diff_lines(repo_path: str, file: str, base_version) -> set[int]:
     return lines
 
 
-class EnvRefVisitor(ast.NodeVisitor):
+class EvalRefVisitor(ast.NodeVisitor):
+    def __init__(self, filepath, line=0):
+        self.issues = []
+        self.line = line
+        self.filepath = filepath
+
+    def _is_ref_raise_if_not_found(self, node):
+        """Check if the node is a call to env.ref with raise_if_not_found=True"""
+        raise_if_not_found = True  # default to True
+        for keyword in node.keywords:
+            if keyword.arg == 'raise_if_not_found':
+                if isinstance(keyword.value, ast.Constant) and keyword.value.value is False:
+                    raise_if_not_found = False
+        return raise_if_not_found
+
+    def visit_Call(self, node):
+        """ check for env.ref calls """
+        if isinstance(node.func, ast.Name) and node.func.id == 'ref':
+            if self._is_ref_raise_if_not_found(node):
+                # for ref() calls, check if the ref is for another module
+                self.issues.append(f'"{self.filepath}", line {self.line}')
+        
+        self.generic_visit(node)
+
+
+class FileEnvRefVisitor(EvalRefVisitor):
     def __init__(self, filepath, diff_lines):
         self.issues = []
         self.filepath = filepath
@@ -97,19 +129,10 @@ class EnvRefVisitor(ast.NodeVisitor):
         end_line = node.end_lineno
         return any(line in self.diff_lines for line in range(start_line, end_line + 1))
 
-    def _is_ref_raise_if_not_found(self, node):
-        """Check if the node is a call to env.ref with raise_if_not_found=True"""
-        raise_if_not_found = True  # default to True
-        for keyword in node.keywords:
-            if keyword.arg == 'raise_if_not_found':
-                if isinstance(keyword.value, ast.Constant) and keyword.value.value is False:
-                    raise_if_not_found = False
-        return raise_if_not_found
-
     def visit_Assign(self, node):
         """ Track assignments of env.ref to variables to handle use cases like
 
-            for name in names:
+            ref = self.env.ref
             ref('xxx', raise_if_not_found=False)
         """
         if len(node.targets) == 1 and isinstance(node.targets[0], ast.Name):
@@ -141,29 +164,69 @@ class EnvRefVisitor(ast.NodeVisitor):
                 is_env_ref = True
 
             if is_env_ref:
-                if not self._is_node_in_diff(node):
-                    return
-                if self._is_ref_raise_if_not_found(node):
+                if self._is_node_in_diff(node) and self._is_ref_raise_if_not_found(node):
                     self.issues.append(f'"{self.filepath}", line {node.lineno}')
         elif isinstance(node.func, ast.Name) and node.func.id in self.env_ref_names:
             # Handle stored env.ref calls
-            if not self._is_node_in_diff(node):
-                return
-            if self._is_ref_raise_if_not_found(node):
+            if self._is_node_in_diff(node) and self._is_ref_raise_if_not_found(node):
                 self.issues.append(f'"{self.filepath}", line {node.lineno}')
 
         self.generic_visit(node)
 
 
+def _check_data_xml_ref(odoo_path, diff_lines):
+
+    def process_issue(end_line):
+        nonlocal last_issue
+        if last_issue:
+            start_line, type, message = last_issue
+            if any(line in diff_lines for line in range(start_line, max(start_line + 1, end_line))):
+                issues[type].append(message)
+            last_issue = None
+
+    issues = defaultdict(list)
+    module_name = odoo_path.split('/')[0]
+    path = file_path(odoo_path)
+    last_issue = None  # (start_line, type, message)
+    with file_open(odoo_path, 'rb') as fp:
+        context = etree.iterparse(fp, events=("start", "end"))
+        for event, element in context:
+            if event == "start":
+                process_issue(element.sourceline)
+                if element.tag == 'record':
+                    record = element
+                    id_attr = record.get('id')
+                    force_create = record.get('force_create', 'True').lower()
+                    if id_attr and '.' in id_attr and not id_attr.startswith(f'{module_name}.') and force_create not in ('0', 'false'):
+                        last_issue = (record.sourceline, 'id', f'{path}, line {record.sourceline}')
+                elif element.tag == 'field':
+                    field = element
+                    ref_attr = field.get('ref')
+                    if ref_attr and '.' in ref_attr and not ref_attr.startswith(f'{module_name}.'):
+                        last_issue = (field.sourceline, 'ref', f'{path}, line {field.sourceline}')
+                    elif eval_attr := field.get('eval'):
+                        # parse as python ast, check function ref
+                        # add to issues if ref for another module without raise_if_not_found=False
+                        tree = ast.parse(eval_attr)
+                        visitor = EvalRefVisitor(path, line=field.sourceline)
+                        visitor.visit(tree)
+                        if visitor.issues:
+                            last_issue = (field.sourceline, 'eval', visitor.issues[0])
+            elif event == "end":
+                process_issue(element.sourceline)
+            element.clear()
+
+    return issues
+
 class TestDiff(BaseCase):
     def test_env_ref_usage(self):
-        """Check for env.ref calls without raise_if_not_found=True"""
+        """Check for env.ref calls without raise_if_not_found=False"""
         def check_file(filepath, diff_lines):
             with open(filepath, 'r') as f:
                 content = f.read()
             try:
                 tree = ast.parse(content)
-                visitor = EnvRefVisitor(filepath, diff_lines)
+                visitor = FileEnvRefVisitor(filepath, diff_lines)
                 visitor.visit(tree)
                 return visitor.issues
             except SyntaxError:
@@ -191,3 +254,56 @@ class TestDiff(BaseCase):
             bool(issues),
             "Found env.ref calls with raise_if_not_found=True:\n" + "\n".join(issues)
         )
+
+    def test_data_xml_ref_usage(self):
+        """Check for ref for other modules"""
+        def _get_odoo_path(file) -> tuple[str, str] | tuple[None, None]:
+            # test diff except odoo/odoo/addons
+            for addons_path in config['addons_path']:
+                if file.startswith(addons_path):
+                    odoo_path = file[len(addons_path):].strip('/')
+                    module = odoo_path.split('/')[0]
+                    if not module.startswith('test_'):
+                        return module, odoo_path
+            return None, None
+
+        diff_files = {}  # {repo_path: [file]}
+        repos = get_repos()
+        _logger.info(f"Found {len(repos)} repos")
+        for repo_path in get_repos():
+            base_version = get_base_version(repo_path)
+            if not base_version:
+                continue
+            diff_files[(repo_path, base_version)] = get_diff_files(repo_path, base_version, None)
+        
+        module_files = defaultdict(lambda: defaultdict(list))  # {(repo_path, base_version): {module_name: [file]}}
+        for (repo_path, base_version), files in diff_files.items():
+            for file in files:
+                file_path_ = os.path.join(repo_path, file)
+                module_name, file_path_ = _get_odoo_path(file_path_)
+                if module_name:
+                    module_files[(repo_path, base_version)][module_name].append(file_path_)
+
+        issues = defaultdict(list)
+        for (repo_path, base_version), module_files_ in module_files.items():
+            for module_name, files in module_files_.items():
+                # check if module is in the data of the manifest
+                manifest = get_manifest(module_name)
+                data_files = set(manifest.get('data'))
+                for file in files:
+                    if file.strip(module_name + '/') not in data_files:
+                        continue
+                    diff_lines = get_diff_lines(repo_path, file_path(file), base_version)
+                    file_issues = _check_data_xml_ref(file, diff_lines)
+                    for type, messages in file_issues.items():
+                        issues[type].extend(messages)
+        
+        messages = ''
+        if issues['id']:
+            messages += '\n\nFound id= for another module without force_create="0":\n' + "\n".join(issues['id'])
+        if issues['ref']:
+            messages += "\n\nFound ref= for another module:\n" + "\n".join(issues['ref'])
+        if issues['eval']:
+            messages += "\n\nFound eval uses ref( for another module without raise_if_not_found=False:\n" + "\n".join(issues['eval'])
+
+        self.assertFalse(bool(messages), messages)
