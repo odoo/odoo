66b94619de1f762361f8d6ca4b7e3bb525dc6418
João Alves <joao.horta.alves@gmail.com>
2025-03-30 10:00:50 +0000

    [FIX] survey: provide translations to frontend
    This commit addresses translations not being exposed to the frontend.
    For surveys, no translatable terms existing in JavaScript are being
    translated. This mainly concerns `survey_form.js`. Lack of translations
    there easily results in a mix of languages being shown to a user
    answering a survey.
    
    Steps to reproduce:
    
    Install survey with demo data. Open and edit the "form feedback" survey.
    Change the question "What is your birth date" question. In the "option"
    tab, check the inbox "validate input" and set a min and max date.
    
    Start a live session with 1 attendee that uses another language than
    english. On the presenter side skip the first few questions to go to the
    "what is your birth date" question. On the attendee side, write down
    a date outside of the allowed range (don't use the date picket!). A
    warning message appears, that warning is not translated.
    
    X-original-commit: f3af17315a9b90fe0803a6570f79d805a0b224e9

--- addons/survey/models/__init__.py
+++ addons/survey/models/__init__.py
@@ -6,7 +6,12 @@ from . import challenge
 from . import ir_http
 from . import res_lang
 from . import res_partner
+<<<<<<< d87b3fb55bf4578427099731daf4438cf1b6fff1
 from . import survey_question
 from . import survey_survey
 from . import survey_survey_template
 from . import survey_user_input
+||||||| b847890bf1781cac78ab5e4f9e7ae52e4599369c
+=======
+from . import ir_http
+>>>>>>> f245c8e39dbbfc50a0cb77002596b856220118e0
--- addons/survey/models/ir_http.py
+++ addons/survey/models/ir_http.py
@@ -1,4 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+<<<<<<< d87b3fb55bf4578427099731daf4438cf1b6fff1
 import re
 
 from odoo import api, models
@@ -17,3 +18,17 @@ class IrHttp(models.AbstractModel):
     @api.model
     def _is_survey_frontend(self, path):
         return bool(re.match('/survey/|/[a-z]{2}/survey/|/[a-z]{2}_[A-Z]{2}/survey/', path))
+||||||| b847890bf1781cac78ab5e4f9e7ae52e4599369c
+=======
+
+from odoo import models
+
+
+class IrHttp(models.AbstractModel):
+    _inherit = "ir.http"
+
+    @classmethod
+    def _get_translation_frontend_modules_name(cls):
+        modules = super()._get_translation_frontend_modules_name()
+        return modules + ["survey"]
+>>>>>>> f245c8e39dbbfc50a0cb77002596b856220118e0
