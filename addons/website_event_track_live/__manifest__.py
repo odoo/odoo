1181aac754736356255ae629cff890ee9bf073ef
emge-odoo <emge@odoo.com>
2025-03-31 14:12:17 +0200

    [MOV] website_event_track_live: move public widgets to interactions folder
    Part-of: odoo/odoo#204312
    Signed-off-by: Géry Debongnie <ged@odoo.com>

--- addons/website_event_track_live/__manifest__.py
+++ addons/website_event_track_live/__manifest__.py
@@ -24,9 +24,7 @@
     'assets': {
         'web.assets_frontend': [
             'website_event_track_live/static/src/scss/website_event_track_live.scss',
-            'website_event_track_live/static/src/js/website_event_track_replay_suggestion.js',
-            'website_event_track_live/static/src/js/website_event_track_suggestion.js',
-            'website_event_track_live/static/src/js/website_event_track_live.js',
+            'website_event_track_live/static/src/interactions/*.js',
             'website_event_track_live/static/src/xml/**/*',
         ],
     },
