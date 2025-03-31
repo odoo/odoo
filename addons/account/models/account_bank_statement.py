b8d8397e91f1be7fe11e9a891348584cc193eb45
Rémy Voet (ryv) <ryv@odoo.com>
2025-03-31 13:07:34 +0200

    [FIX] account: store account.bank.statement.currency_id
    Since https://github.com/odoo/odoo/pull/199958, monetary fields can now
    only be aggregated (from the web client) if the currency field is also
    aggregated. There are many monetary fields in `account.bank.statement` that can no
    longer be aggregated because the corresponding currency field is a
    compute no store field.
    
    Store the currency field and update its depends to be recompute as expected.
    
    closes odoo/odoo#203901
    
    Related: odoo/enterprise#82415
    Related: odoo/upgrade#7471
    Signed-off-by: Rémy Voet (ryv) <ryv@odoo.com>

--- addons/account/models/account_bank_statement.py
+++ addons/account/models/account_bank_statement.py
@@ -62,7 +62,7 @@ class AccountBankStatement(models.Model):
 
     currency_id = fields.Many2one(
         comodel_name='res.currency',
-        compute='_compute_currency_id',
+        compute='_compute_currency_id', store=True,
     )
 
     journal_id = fields.Many2one(
@@ -166,7 +166,7 @@ class AccountBankStatement(models.Model):
         for stmt in self:
             stmt.balance_end_real = stmt.balance_end
 
-    @api.depends('journal_id')
+    @api.depends('journal_id.currency_id', 'company_id.currency_id')
     def _compute_currency_id(self):
         for statement in self:
             statement.currency_id = statement.journal_id.currency_id or statement.company_id.currency_id
