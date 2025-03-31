87d2a3aa48721feaf4e06b0b5ea52d04f06473a3
Paolo Gatti <pgi@odoo.com>
2025-03-31 10:14:33 +0000

    [IMP] l10n_it_edi: Qualified Signature provider Namirial
    Invoices for the Italian Public Administration businesses must be signed
    with XADES before sending. We use Namirial's third-party signing
    services.
    
    - This upgrade adds configuration, a signing process, and new returns to
      handle this case. Old versions should be able to ignore this, as the
      invoice will stay blocked in the requires_user_signature state in Odoo,
      now deprecated, without being sent to the EDI.
    - Handle more error types, with dedicated server exceptions with an
      error code map. Old versions should be able to just handle them as
      before, generically.
    - When the Proxy Server signs an invoice, the XML will replace the one
      on the Odoo instance before it's actually attached.
    
    Needs: #210572
    Also see: odoo/iap-apps#978
    
    task-4477745
    
    X-original-commit: c2e65ae92eb8dd356bb1f4ce2e4a82b88779ff34

--- addons/l10n_it_edi/models/account_move_send.py
+++ addons/l10n_it_edi/models/account_move_send.py
@@ -91,15 +91,35 @@ class AccountMoveSend(models.AbstractModel):
         super()._call_web_service_after_invoice_pdf_render(invoices_data)
         attachments_vals = {}
         moves = self.env['account.move']
-        for move, move_data in invoices_data.items():
-            if 'it_edi_send' in move_data['extra_edis']:
-                if attachment := move.l10n_it_edi_attachment_file:
-                    attachments_vals[move] = {'name': move.l10n_it_edi_attachment_name, 'raw': attachment}
-                    moves |= move
-                elif edi_values := move_data.get('l10n_it_edi_values'):
-                    attachments_vals[move] = edi_values
-                    moves |= move
-        moves._l10n_it_edi_send(attachments_vals)
+
+        # Filter only l10n_it_edi attachments
+        moves_data = {
+            move: move_data
+            for move, move_data in invoices_data.items()
+            if 'it_edi_send' in move_data['extra_edis']
+        }
+
+        # Prepare attachment data
+        for move, move_data in moves_data.items():
+            if attachment := move.l10n_it_edi_attachment_file:
+                attachments_vals[move] = {'name': move.l10n_it_edi_attachment_name, 'raw': attachment}
+                moves |= move
+            elif edi_values := move_data.get('l10n_it_edi_values'):
+                attachments_vals[move] = edi_values
+                moves |= move
+
+        # Send
+        results = moves._l10n_it_edi_send(attachments_vals)
+
+        # Eventually update attachments with signed data
+        for move, move_data in moves_data.items():
+            if move.l10n_it_edi_attachment_file:
+                attachment_name = move.l10n_it_edi_attachment_name
+            elif attachment := move_data.get('l10n_it_edi_values'):
+                attachment_name = attachment['name']
+            attachment_data = results.get(attachment_name, {})
+            if attachment_data.get('signed') and (signed_data := attachment_data.get('signed_data')):
+                attachment['raw'] = signed_data.encode()
 
     def _link_invoice_documents(self, invoices_data):
         # EXTENDS 'account'
