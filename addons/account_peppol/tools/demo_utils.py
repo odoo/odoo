1782482414def110e83b981a3c7c902b5ca06278
Claire (clbr) <clbr@odoo.com>
2025-03-31 14:05:08 +0000

    [FIX] account_peppol: Fix Peppol demo mode to be closer to production behavior
    Fix the demo mode of Peppol to be closer to the behavior in production.
    
    task-no (review with TSB/PMAX 31/03/25)
    
    X-original-commit: 6e0cd3f35882d05de8cf6a0c28605335ae11cb48

--- addons/account_peppol/tools/demo_utils.py
+++ addons/account_peppol/tools/demo_utils.py
@@ -93,12 +93,14 @@ def _mock_call_peppol_proxy(func, self, *args, **kwargs):
 
 
 def _mock_get_peppol_verification_state(func, self, *args, **kwargs):
-    # in demo, we consider all partner valid if they encoded required fields
-    (endpoint, eas, format) = args
-    if endpoint and eas:
-        return 'valid' if format in self._get_peppol_formats() else 'not_valid_format'
-    else:
+    (endpoint, eas, xml_format) = args
+    if not (eas and endpoint):
+        return 'not_verified'
+    if not xml_format:
         return 'not_valid'
+    if xml_format not in self._get_peppol_formats():
+        return 'not_valid_format'
+    return 'valid'
 
 def _mock_check_peppol_participant_exists(func, self, *args, **kwargs):
     # in demo, no participant already exists
