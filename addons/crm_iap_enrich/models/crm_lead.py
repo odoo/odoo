4d95c1a6d9a34d5d9e243c34ff4e2c84b077f4d4
Adrien Schoffeniels <adsc@odoo.com>
2025-03-31 22:09:44 +0200

    [IMP] crm_iap_enrich: reschedule cron when all records are locked
    Rationale: the commit progress API makes the assumption that if no
    progress was made during the execution of the cron, the API was not used
    and therefore does not reschedule the cron.
    We reschedule it manually a few moments later, waiting for the locked
    records to be unlocked.
    
    Task-4526274

--- addons/crm_iap_enrich/models/crm_lead.py
+++ addons/crm_iap_enrich/models/crm_lead.py
@@ -112,7 +112,9 @@ class CrmLead(models.Model):
             leads = self.browse(all_lead_ids).try_lock_for_update(limit=batch_size)
             if not leads:
                 _logger.error('A batch of leads could not be enriched (locked): %s', repr(self.browse(all_lead_ids)))
-                break  # all are locked
+                # all are locked, reschedule the cron later when the records might be unlocked
+                self.env.ref('crm_iap_enrich.ir_cron_lead_enrichment')._trigger(self.env.cr.now() + datetime.timedelta(minutes=1))
+                break
             all_lead_ids -= set(leads._ids)
 
             if from_cron:
