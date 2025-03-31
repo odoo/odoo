8f46856372864b6d26fe3dd5cac2b60ce731e699
Hubert Van De Walle <huvw@odoo.com>
2025-03-31 14:06:23 +0000

    [FIX] web: use updated values in mobile pager
    Steps to reproduce
    ==================
    
    - Use a mobile viewport
    - Go to any list/kanban view
    - Open a record
    - Use the pager to switch to the next record
    => "1 / x" is displayed where x is the total amount of records
    - Click next
    => "2 / x" is displayed
    - Click previous
    => "3 / x" is displayed
    
    The pager is displaying the values before the switch has been made.
    
    Cause of the issue
    ==================
    
    When sending `this.value` on the `PAGER_UPDATED_EVENT` bus,
    the props have not been updated yet. This means that we send the
    previous value.
    
    Solution
    ========
    
    We send the event once either the offset, limit or total props have been
    updated.
    
    opw-4666878
    
    closes odoo/odoo#204671
    
    X-original-commit: c69f74bac00e472b16da55b04e1d164d3a5b32bc
    Signed-off-by: Adrien Dieudonné (adr) <adr@odoo.com>
    Signed-off-by: Hubert Van De Walle <huvw@odoo.com>

--- addons/web/static/tests/views/form/form_view.test.js
+++ addons/web/static/tests/views/form/form_view.test.js
@@ -59,6 +59,7 @@ import { browser } from "@web/core/browser/browser";
 import { cookie } from "@web/core/browser/cookie";
 import { makeErrorFromResponse } from "@web/core/network/rpc";
 import { registry } from "@web/core/registry";
+import { config as transitionConfig } from "@web/core/transition";
 import { SIZES } from "@web/core/ui/ui_service";
 import { useBus, useService } from "@web/core/utils/hooks";
 import { redirect } from "@web/core/utils/urls";
@@ -12871,4 +12872,21 @@ test("executing new action, closes dialog, and avoid reload previous view", asyn
         "get_views",
         "web_search_read",
     ]);
+});
+
+test.tags("mobile")(`pager is up to date`, async () => {
+    patchWithCleanup(transitionConfig, { disabled: true });
+    await mountView({
+        resModel: "partner",
+        type: "form",
+        arch: `<form><field name="foo"/></form>`,
+        resIds: [1, 2],
+        resId: 1,
+    });
+    await contains(`.o_pager_next`).click();
+    await animationFrame();
+    expect(".o_pager_indicator").toHaveCount(1, {
+        message: "the pager indicator should be displayed",
+    });
+    expect(".o_pager_indicator").toHaveText("2 / 2");
 });
