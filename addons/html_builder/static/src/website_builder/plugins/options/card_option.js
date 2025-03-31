85819db34bbff7dad3087e9422224829363f932b
FrancoisGe <fge@odoo.com>
2025-03-31 13:40:07 +0200

    add border on card

--- addons/html_builder/static/src/website_builder/plugins/options/card_option.js
+++ addons/html_builder/static/src/website_builder/plugins/options/card_option.js
@@ -1,10 +1,17 @@
 import { BaseOptionComponent, useGetItemValue } from "@html_builder/core/utils";
 import { WebsiteBackgroundOption } from "@html_builder/website_builder/plugins/options/background_option";
 import { CardImageOption } from "./card_image_option";
+import { BorderConfigurator } from "@html_builder/plugins/border_configurator_option";
+import { ShadowOption } from "@html_builder/plugins/shadow_option";
 
 export class CardOption extends BaseOptionComponent {
     static template = "website.CardOption";
-    static components = { CardImageOption, WebsiteBackgroundOption };
+    static components = {
+        CardImageOption,
+        WebsiteBackgroundOption,
+        BorderConfigurator,
+        ShadowOption,
+    };
     static props = {
         disableWidth: { type: Boolean, optional: true },
     };
