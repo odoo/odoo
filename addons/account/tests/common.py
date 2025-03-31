40da617ac566fc41e3ca8536c174dd2aa1b04363
William Braeckman <wbr@odoo.com>
2025-03-31 10:33:13 +0200

    [FIX] account: remove group_account_manager from group list
    The group was added within the explicit group list in account common
    class but it's actually already ref'ed and required by the groups so it
    was just duplicating it for no reason.
    
    closes odoo/odoo#204122
    
    Related: odoo/enterprise#82534
    Signed-off-by: William Braeckman (wbr) <wbr@odoo.com>

--- addons/account/tests/common.py
+++ addons/account/tests/common.py
@@ -266,7 +266,6 @@ class AccountTestInvoicingCommon(ProductCommon):
                 ('name', 'in', (
                     # TODO: Progressively remove groups from this list, hopefully no groups share the same name.
                     # This is a consequence of moving groups from data to demo data: https://github.com/odoo/odoo/pull/198078
-                    'group_account_manager', # account
                     'group_hr_manager', # hr
                     'group_mrp_manager', # mrp
                     'group_pos_manager', # point_of_sale
