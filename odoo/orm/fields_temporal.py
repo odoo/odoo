a47a6a4a6fd82a8512483d175486a972251d565d
Krzysztof Magusiak (krma) <krma@odoo.com>
2025-03-31 09:55:22 +0000

    [IMP] orm: getter for field_expr
    New function that builds a function from a field expression which given
    a record, returns the value for that expression.
    
    By default, this function returns the `Field.__get__` function.
    It can be overridden to return properties. Example:
    
        field = record._fields['create_date']
        get_value = field.expression_getter('create_date.month_number')
        month_number = get_value(record)
    
    This function will be useful to implement `filtered`, `mapped` or
    similar functions directly in python while using field expressions.
    
    Part-of: odoo/odoo#204293
    Signed-off-by: Raphael Collet <rco@odoo.com>

--- odoo/orm/fields_temporal.py
+++ odoo/orm/fields_temporal.py
@@ -10,9 +10,10 @@ from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DATETIME_FORMAT
 from odoo.tools import SQL, date_utils
 
 from .fields import Field, _logger
-from .utils import READ_GROUP_NUMBER_GRANULARITY
+from .utils import parse_field_expr, READ_GROUP_NUMBER_GRANULARITY
 
 if typing.TYPE_CHECKING:
+    from collections.abc import Callable
     from odoo.tools import Query
 
     from .models import BaseModel
@@ -30,6 +31,51 @@ class BaseDate(Field[T | typing.Literal[False]], typing.Generic[T]):
     end_of = staticmethod(date_utils.end_of)
     add = staticmethod(date_utils.add)
     subtract = staticmethod(date_utils.subtract)
+
+    def expression_getter(self, field_expr):
+        _fname, property_name = parse_field_expr(field_expr)
+        if not property_name:
+            return super().expression_getter(field_expr)
+
+        get_value = self.__get__
+        get_property = self._expression_property_getter(property_name)
+        return lambda record: (value := get_value(record)) and get_property(value)
+
+    def _expression_property_getter(self, property_name: str) -> Callable[[T], typing.Any]:
+        """ Return a function that maps a field value (date or datetime) to the
+        given ``property_name``.
+        """
+        match property_name:
+            case 'tz':
+                return lambda value: value
+            case 'year_number':
+                return lambda value: value.year
+            case 'quarter_number':
+                return lambda value: value.month // 4 + 1
+            case 'month_number':
+                return lambda value: value.month
+            case 'iso_week_number':
+                return lambda value: value.isocalendar().week
+            case 'day_of_year':
+                return lambda value: value.timetuple().tm_yday
+            case 'day_of_month':
+                return lambda value: value.day
+            case 'day_of_week':
+                return lambda value: value.timetuple().tm_wday
+            case 'hour_number' if self.type == 'datetime':
+                return lambda value: value.hour
+            case 'minute_number' if self.type == 'datetime':
+                return lambda value: value.minute
+            case 'second_number' if self.type == 'datetime':
+                return lambda value: value.second
+            case 'hour_number' | 'minute_number' | 'second_number':
+                # for dates, it is always 0
+                return lambda value: 0
+        assert property_name not in READ_GROUP_NUMBER_GRANULARITY, f"Property not implemented {property_name}"
+        raise ValueError(
+            f"Error when processing the granularity {property_name} is not supported. "
+            f"Only {', '.join(READ_GROUP_NUMBER_GRANULARITY.keys())} are supported"
+        )
 
     def property_to_sql(self, field_sql: SQL, property_name: str, model: BaseModel, alias: str, query: Query) -> SQL:
         sql_expr = field_sql
@@ -230,6 +276,22 @@ class Datetime(BaseDate[datetime]):
             the time portion will be midnight (00:00:00).
         """
         return value.strftime(DATETIME_FORMAT) if value else False
+
+    def expression_getter(self, field_expr: str) -> Callable[[BaseModel], typing.Any]:
+        if field_expr == self.name:
+            return self.__get__
+        _fname, property_name = parse_field_expr(field_expr)
+        get_property = self._expression_property_getter(property_name)
+
+        def getter(record):
+            dt = self.__get__(record)
+            if not dt:
+                return False
+            if (tz := record.env.context.get('tz')) and tz in pytz.all_timezones_set:
+                dt = dt.astimezone(pytz.timezone(tz))
+            return get_property(dt)
+
+        return getter
 
     def convert_to_cache(self, value, record, validate=True):
         return self.to_datetime(value)
