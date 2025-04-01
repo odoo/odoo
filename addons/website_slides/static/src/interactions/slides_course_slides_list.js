d6031e905c28000ccbb32d2e8d4ffa2a2a07f370
emge-odoo <emge@odoo.com>
2025-04-01 17:16:42 +0200

    [REF] website_slides, *: prepare for conversion to interactions
    *: website_sale_slides, website_slides_survey
    
    This commit contains style, naming and other adjustments that are not
    strictly related to the conversion to interactions, so that the commit
    about the conversion itself is smaller.
    
    task-5353853
    
    Part-of: odoo/odoo#205330
    Signed-off-by: Florian Charlier (flch) <flch@odoo.com>
    Co-authored-by: Patrick Hoste (pko) <pko@odoo.com>

--- addons/website_slides/static/src/interactions/slides_course_slides_list.js
+++ addons/website_slides/static/src/interactions/slides_course_slides_list.js
@@ -1,9 +1,9 @@
-import publicWidget from '@web/legacy/js/public/public_widget';
+import publicWidget from "@web/legacy/js/public/public_widget";
 import { _t } from "@web/core/l10n/translation";
-import { SlideCoursePage } from '@website_slides/interactions/slides_course_page';
+import { SlideCoursePage } from "@website_slides/interactions/slides_course_page";
 
 publicWidget.registry.websiteSlidesCourseSlidesList = SlideCoursePage.extend({
-    selector: '.o_wslides_slides_list',
+    selector: ".o_wslides_slides_list",
 
     init: function () {
         this._super(...arguments);
@@ -11,109 +11,104 @@ publicWidget.registry.websiteSlidesCourseSlidesList = SlideCoursePage.extend({
     },
 
     start: function () {
-        this._super.apply(this,arguments);
+        this._super.apply(this, arguments);
 
-        this.channelId = this.$el.data('channelId');
+        this.channelId = this.$el.data("channelId");
         this.bindedSortable = [];
 
-        this._updateHref();
-        this._bindSortable();
+        this.updateHref();
+        this.bindSortable();
     },
 
     destroy() {
-        this._unbindSortable();
+        this.unbindSortable();
         return this._super(...arguments);
     },
-
-    //--------------------------------------------------------------------------
-    // Private
-    //--------------------------------------------------------------------------,
 
     /**
      * Bind the sortable jQuery widget to both
      * - course sections
      * - course slides
-     *
-     * @private
      */
-    _bindSortable: function () {
+    bindSortable() {
         const sortableBaseParam = {
             clone: false,
-            placeholderClasses: ['o_wslides_slides_list_slide_hilight', 'position-relative', 'mb-1'],
-            onDrop: this._reorderSlides.bind(this),
-            applyChangeOnDrop: true
+            placeholderClasses: [
+                "o_wslides_slides_list_slide_hilight",
+                "position-relative",
+                "mb-1",
+            ],
+            onDrop: this.reorderSlides.bind(this),
+            applyChangeOnDrop: true,
         };
 
-        const container = this.el.querySelector('ul.o_wslides_js_slides_list_container');
-        this.bindedSortable.push(this.call(
-            "sortable",
-            "create",
-            {
+        const containerEl = this.el.querySelector("ul.o_wslides_js_slides_list_container");
+        this.bindedSortable.push(
+            this.call("sortable", "create", {
                 ...sortableBaseParam,
-                ref: { el: container },
+                ref: { el: containerEl },
                 elements: ".o_wslides_slide_list_category",
                 handle: ".o_wslides_slide_list_category_header .o_wslides_slides_list_drag",
                 sortableId: "category",
-            },
-        ).enable());
+            }).enable()
+        );
 
-        this.bindedSortable.push(this.call(
-            "sortable",
-            "create",
-            {
+        this.bindedSortable.push(
+            this.call("sortable", "create", {
                 ...sortableBaseParam,
-                ref: { el: container },
-                elements: ".o_wslides_slides_list_slide:not(.o_wslides_js_slides_list_empty):not(.o_not_editable)",
+                ref: { el: containerEl },
+                elements:
+                    ".o_wslides_slides_list_slide:not(.o_wslides_js_slides_list_empty):not(.o_not_editable)",
                 handle: ".o_wslides_slides_list_drag",
                 connectGroups: true,
                 groups: ".o_wslides_js_slides_list_container ul",
                 sortableId: "list",
-            },
-        ).enable());
+            }).enable()
+        );
     },
 
-    _unbindSortable: function () {
-        this.bindedSortable.forEach(sortable => sortable.cleanup());
+    unbindSortable() {
+        for (const sortable of this.bindedSortable) {
+            sortable.cleanup();
+        }
     },
 
     /**
      * This method will check that a section is empty/not empty
      * when the slides are reordered and show/hide the
      * "Empty category" placeholder.
-     *
-     * @private
      */
-    _checkForEmptySections: function (){
-        this.$('.o_wslides_slide_list_category').each(function (){
-            var $categoryHeader = $(this).find('.o_wslides_slide_list_category_header');
-            var categorySlideCount = $(this).find('.o_wslides_slides_list_slide:not(.o_not_editable)').length;
-            var $emptyFlagContainer = $categoryHeader.find('.o_wslides_slides_list_drag').first();
-            var $emptyFlag = $emptyFlagContainer.find('small');
-            if (categorySlideCount === 0 && $emptyFlag.length === 0){
-                $emptyFlagContainer.append($('<small>', {
-                    'class': "ms-1 text-muted fw-bold",
-                    text: _t("(empty)")
-                }));
-            } else if (categorySlideCount > 0 && $emptyFlag.length > 0){
+    checkForEmptySections() {
+        this.$(".o_wslides_slide_list_category").each(function () {
+            var $categoryHeader = $(this).find(".o_wslides_slide_list_category_header");
+            var categorySlideCount = $(this).find(
+                ".o_wslides_slides_list_slide:not(.o_not_editable)"
+            ).length;
+            var $emptyFlagContainer = $categoryHeader.find(".o_wslides_slides_list_drag").first();
+            var $emptyFlag = $emptyFlagContainer.find("small");
+            if (categorySlideCount === 0 && $emptyFlag.length === 0) {
+                $emptyFlagContainer.append(
+                    $("<small>", {
+                        class: "ms-1 text-muted fw-bold",
+                        text: _t("(empty)"),
+                    })
+                );
+            } else if (categorySlideCount > 0 && $emptyFlag.length > 0) {
                 $emptyFlag.remove();
             }
         });
     },
 
-    _getSlides: function (){
-        var categories = [];
-        this.$('.o_wslides_js_list_item').each(function (){
-            categories.push(parseInt($(this).data('slideId')));
-        });
+    getSlides() {
+        const categories = [];
+        for (const el of this.el.querySelectorAll(".o_wslides_js_list_item")) {
+            categories.push(parseInt(el.dataset.slideId));
+        }
         return categories;
     },
-    _reorderSlides: function (){
-        var self = this;
-        this.orm
-            .webResequence("slide.slide", this._getSlides())
-            .then(function (res) {
-                self._checkForEmptySections();
-            });
+    async reorderSlides() {
+        await this.orm.webResequence("slide.slide", this.getSlides());
+        this.checkForEmptySections();
     },
 
     /**
@@ -123,16 +118,14 @@ publicWidget.registry.websiteSlidesCourseSlidesList = SlideCoursePage.extend({
      * parameter for SEO purposes.
      *
      * This method then adds the parameter as soon as the page is loaded.
-     *
-     * @private
      */
-    _updateHref: function () {
-        this.$(".o_wslides_js_slides_list_slide_link").each(function (){
-            var href = $(this).attr('href');
-            var operator = href.indexOf('?') !== -1 ? '&' : '?';
-            $(this).attr('href', href + operator + "fullscreen=1");
+    updateHref() {
+        this.$(".o_wslides_js_slides_list_slide_link").each(function () {
+            var href = $(this).attr("href");
+            var operator = href.indexOf("?") !== -1 ? "&" : "?";
+            $(this).attr("href", href + operator + "fullscreen=1");
         });
-    }
+    },
 });
 
 export default publicWidget.registry.websiteSlidesCourseSlidesList;
