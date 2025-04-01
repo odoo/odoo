29227286b349880497b2f1ec292bc8aede69ccd3
Arnold Moyaux <arm@odoo.com>
2025-04-01 10:03:15 +0000

    [FIX] sale_mrp: ensure bidirectional SO-MO link with MTSO
    Currently, with an MTSO rule, the smart button from the Sale Order
    to the Manufacturing Order was available. However, the reverse link
    (from the MO to the SO) was missing.
    
    But they share the same link between object and people don't
    understand. So we copy the condition to have the same SO<->MO
    link.
    
    It was missing the sale_id on the procurement group itself. Before
    production.procurement_group_id.move_dest_ids.group_id.sale_id
    was enough since mtso share move links but with the new MTSO it's
    not the case anymore. But the sale_id is copied from a procurement
    to the new ones.
    
    Steps to reproduce:
    - Enable multi-step routes in Inventory settings
    - Unarchive the MTO route:
        - Select the rule with the production usage
            - Set its supply method to "MTSO"
    - Create a storable product “P1”:
        - routes: MTO + Manufacture
    - Create and confirm a Sale Order with one unit of P1
    - Confirm the SO
    
    Problem:
    The smart button from the SO to the MO appears correctly,
    but the reverse link from the MO to the SO is missing.
    
    opw-4619136
    opw-4557138
    opw-4875937
    opw-4854581
    opw-4887746
    
    closes odoo/odoo#217947
    
    X-original-commit: 77159d4fad15ec711e24578f33b4418dd6a87553
    Signed-off-by: Steve Van Essche <svs@odoo.com>
    Signed-off-by: Djamel Touati (otd) <otd@odoo.com>

--- addons/sale_mrp/models/mrp_production.py
+++ addons/sale_mrp/models/mrp_production.py
@@ -13,14 +13,20 @@ class MrpProduction(models.Model):
         groups='sales_team.group_sale_salesman')
     sale_line_id = fields.Many2one('sale.order.line', 'Origin sale order line')
 
-    @api.depends('procurement_group_id.mrp_production_ids.move_dest_ids.group_id.sale_id')
+    @api.depends('procurement_group_id.mrp_production_ids.move_dest_ids.group_id.sale_id', 'procurement_group_id.sale_id', 'sale_line_id.order_id')
     def _compute_sale_order_count(self):
         for production in self:
-            production.sale_order_count = len(production.procurement_group_id.mrp_production_ids.move_dest_ids.group_id.sale_id | production.sale_line_id.order_id)
+            production.sale_order_count = len(production.get_linked_sale_orders())
+
+    def get_linked_sale_orders(self):
+        return (
+            self.procurement_group_id.mrp_production_ids.move_dest_ids.group_id.sale_id |
+            self.sale_line_id.order_id |
+            self.procurement_group_id.sale_id)
 
     def action_view_sale_orders(self):
         self.ensure_one()
-        sale_order_ids = self.procurement_group_id.mrp_production_ids.move_dest_ids.group_id.sale_id.ids + self.sale_line_id.order_id.ids
+        sale_order_ids = self.get_linked_sale_orders().ids
         action = {
             'res_model': 'sale.order',
             'type': 'ir.actions.act_window',
