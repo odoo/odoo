045094f63264da3717346873e3db334e40246bc3
Mathieu Duckerts-Antoine <dam@odoo.com>
2025-04-01 09:28:11 +0200

    [REF] *: rewrite ORM in JS

--- odoo/cli/__init__.py
+++
@@ -1,2 +0,0 @@
-# Import just the command, the rest will get imported as needed
-from .command import Command, main  # noqa: F401
--- odoo/osv/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-# TODO deprecate module, show warning when importing
