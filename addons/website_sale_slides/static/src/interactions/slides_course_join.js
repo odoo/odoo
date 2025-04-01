bf3f3a9046027d47d11c3bcbdaeaa3e972da766f
emge-odoo <emge@odoo.com>
2025-04-01 17:16:42 +0200

    [REF] website_slides, *: prepare for conversion to interactions
    *: website_sale_slides, website_slides_survey
    
    This commit contains style, naming and other adjustments that are not
    strictly related to the conversion to interactions, so that the commit
    about the conversion itself is smaller.
    
    task-4367641

--- addons/website_sale_slides/static/src/interactions/slides_course_join.js
+++ addons/website_sale_slides/static/src/interactions/slides_course_join.js
@@ -16,31 +16,27 @@ CourseJoinWidget.include({
      * When the user joins the course, if it's set as "on payment" and the user is logged in,
      * we redirect to the shop page for this course.
      *
-     * @param {MouseEvent} ev
      * @override
-     * @private
      */
-    _onClickJoin: function (ev) {
+    async onJoinClick(ev) {
         ev.preventDefault();
 
-        if (this.channel.channelEnroll === 'payment' && !this.publicUser) {
-            const self = this;
-            this.beforeJoin().then(function () {
-                self.call('cart', 'add',
-                    {
-                        // TODO VCR Ensure productTemplateId is always provided to `addToCart`.
-                        // Currently, this works because the product configurator check is bypassed
-                        // when the `isBuyNow` option is `True`.
-                        productTemplateId: false,
-                        productId: self.productId,
-                    },
-                    {
-                        isBuyNow: true,
-                    },
-                );
-            });
+        if (this.channel.channelEnroll === "payment" && !this.publicUser) {
+            await this.beforeJoin();
+            this.call('cart', 'add',
+                {
+                    // TODO VCR Ensure productTemplateId is always provided to `addToCart`.
+                    // Currently, this works because the product configurator check is bypassed
+                    // when the `isBuyNow` option is `True`.
+                    productTemplateId: false,
+                    productId: this.productId,
+                },
+                {
+                    isBuyNow: true,
+                },
+            );
         } else {
-            this._super.apply(this, arguments);
+            await this._super.apply(this, arguments);
         }
     },
 });
