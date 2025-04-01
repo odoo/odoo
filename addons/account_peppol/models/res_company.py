b8f5e0de7e608c11ad74697c12ce88f81b40b9a9
Claire (clbr) <clbr@odoo.com>
2025-04-01 10:40:25 +0200

    [REF] account_peppol: factorize Peppol can_send state
    Small factorization of the code to be more readable.
    
    task-no
    
    closes odoo/odoo#204328
    
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- addons/account_peppol/models/res_company.py
+++ addons/account_peppol/models/res_company.py
@@ -87,6 +87,7 @@ class ResCompany(models.Model):
         inverse='_inverse_peppol_purchase_journal_id',
     )
     peppol_external_provider = fields.Char(tracking=True)
+    peppol_can_send = fields.Boolean(compute='_compute_peppol_can_send')
 
     # -------------------------------------------------------------------------
     # HELPER METHODS
@@ -192,6 +193,12 @@ class ResCompany(models.Model):
                     company.account_peppol_phone_number = company.phone
                 except ValidationError:
                     continue
+
+    @api.depends('account_peppol_proxy_state')
+    def _compute_peppol_can_send(self):
+        can_send_domain = self.env['account_edi_proxy_client.user']._get_can_send_domain()
+        for company in self:
+            company.peppol_can_send = company.account_peppol_proxy_state in can_send_domain
 
     # -------------------------------------------------------------------------
     # LOW-LEVEL METHODS
