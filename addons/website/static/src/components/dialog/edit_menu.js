7bbcf3fbb8a41c540ea587ec79436561eb8dc84a
Benjamin Vray <bvr@odoo.com>
2025-04-02 07:01:05 +0000

    [FIX] website, web: fix menu editor drag-and-drop
    This commit fixes the following two issues in the menu creation dialog.
    
    Issue 1:
    
    - Install the Website app and go to the homepage.
    - In the backend navbar, click on "Site" and then on "Menu Editor."
    - Drag a menu item (without dropping it) to the right to create a
    sub-menu.
    - The issue is that while dragging the menu item, when the placeholder
    is in a sub-menu position, the menu item keeps its original width
    instead of taking the width of a sub-menu item. This is confusing and
    unclear from a UX perspective.
    
    Issue 2:
    
    - Install the Website app and go to the homepage.
    - In the backend navbar, click on "Site" and then on "Menu Editor."
    - Drag and drop the second menu item to the right to create a sub-menu.
    - Drag another menu item (without dropping it) to the right to create a
    second sub-menu below the first one.
    - Without releasing it, continue dragging it to the right.
    - The issue is that the placeholder disappears at this point. To make it
    reappear, the menu item must be moved slightly to the left again. This
    results in a very poor user experience and makes sub-menu creation
    messy.
    
    task-4422810
    
    closes odoo/odoo#207391
    
    X-original-commit: 55a91ef1bb1a72280be50c0f739415f475df7937
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>
    Signed-off-by: Benjamin Vray (bvr) <bvr@odoo.com>

--- addons/website/static/src/components/dialog/edit_menu.js
+++ addons/website/static/src/components/dialog/edit_menu.js
@@ -134,6 +134,20 @@ export class EditMenuDialog extends Component {
             onDrop: this._moveMenu.bind(this),
             isAllowed: this._isAllowedMove.bind(this),
             useElementSize: true,
+            /**
+             * @param {DOMElement} element - moved element
+             * @param {DOMElement} parent - parent element of where the element was moved
+             * @param {DOMElement} placeholder - hint element showing the current position
+             */
+            onMove: ({element, placeholder, parent}) => {
+                // Adapt the dragged menu item to match the width and position
+                // of the placeholder.
+                element.style.width = getComputedStyle(placeholder).width;
+                element.style.marginLeft =
+                    parent && (element.parentElement === this.menuEditor.el)
+                    ? "2rem"
+                    : "";
+            },
         });
     }
 
