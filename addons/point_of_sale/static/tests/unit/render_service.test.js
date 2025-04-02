937139e6c3f2f46bbe061884c78c7c5264a9d510
Julien (jula) <jula@odoo.com>
2025-04-02 12:38:50 +0000

    [FIX] point_of_sale: fix crash with problematic chars in htmlToCanvas
    __Current behavior before commit:__
    Sometimes when a Worldline terminal makes a payment with Edenred or
    Sodexo, it creates a ticket with special ascii characters like `0x0E`
    that are not supposed to be printed. Some of those characters make the
    [`createImage`][1] method throw an error with the `onerror` Event.
    
    __Description of the fix:__
    Added the `sanitizeNodeText` function to remove all problematic ascii
    characters that should not be printed anyway.
    Added a test to make sure `htmlToCanvas` can handle all ascii characters
    without crashing.
    
    opw-4322339
    
    [1]:https://github.com/odoo/odoo/blob/a98e802976f6798f3aea07231a366fbcd8002ce2/addons/point_of_sale/static/src/app/utils/html-to-image.js#L217
    
    X-original-commit: 172ce48d91c41683853240511c71ee9f94487deb

--- addons/point_of_sale/static/tests/unit/render_service.test.js
+++ addons/point_of_sale/static/tests/unit/render_service.test.js
@@ -1,24 +1,53 @@
-import { expect, test } from "@odoo/hoot";
+import { describe, expect, getFixture, test } from "@odoo/hoot";
+import { mockFetch } from "@odoo/hoot-mock";
 import { Component, xml } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { clearRegistry, mountWithCleanup, patchTranslations } from "@web/../tests/web_test_helpers";
-import { renderService } from "@point_of_sale/app/services/render_service";
+import { renderService, htmlToCanvas } from "@point_of_sale/app/services/render_service";
 
-test("test the render service", async () => {
-    class ComponentToBeRendered extends Component {
-        static props = ["name"];
-        static template = xml`
-            <div> It's me, <t t-esc="props.name" />! </div>
-        `;
-    }
-    clearRegistry(registry.category("services"));
-    clearRegistry(registry.category("main_components"));
-    registry.category("services").add("render", renderService);
+describe("RenderService", () => {
+    test("test the render service", async () => {
+        class ComponentToBeRendered extends Component {
+            static props = ["name"];
+            static template = xml`
+                <div> It's me, <t t-esc="props.name" />! </div>
+            `;
+        }
+        clearRegistry(registry.category("services"));
+        clearRegistry(registry.category("main_components"));
+        registry.category("services").add("render", renderService);
 
-    patchTranslations(); // this is needed because we are not loading the localization service
-    const comp = await mountWithCleanup("none");
-    const renderedComp = await comp.env.services.render.toHtml(ComponentToBeRendered, {
-        name: "Mario",
+        patchTranslations(); // this is needed because we are not loading the localization service
+        const comp = await mountWithCleanup("none");
+        const renderedComp = await comp.env.services.render.toHtml(ComponentToBeRendered, {
+            name: "Mario",
+        });
+        expect(renderedComp).toHaveOuterHTML("<div> It's me, Mario! </div>");
     });
-    expect(renderedComp).toHaveOuterHTML("<div> It's me, Mario! </div>");
+
+    test("htmlToCanvas", async () => {
+        // htmlToCanvas fetches some fonts useless for the test, we mock it to avoid warnings
+        mockFetch(() => "");
+        const target = getFixture();
+        const node = document.createElement("div");
+        node.classList.add("render-container");
+        target.appendChild(node);
+
+        const asciiChars = Array.from({ length: 256 }, (_, i) => String.fromCharCode(i)).join("");
+        node.textContent = asciiChars;
+
+        let canvas = null;
+        try {
+            canvas = await htmlToCanvas(node, { addClass: "pos-receipt-print" });
+        } catch (error) {
+            // htmlToCanvas create an <img> by setting a svg to its src attribute
+            // if this fails, an Event of type "error" is thrown
+            if (error.constructor.name !== "Event") {
+                throw error;
+            }
+        }
+        expect(canvas).not.toBe(null, {
+            message: "htmlToCanvas should work with all ascii characters",
+        });
+    });
 });
