8bcdcfadde8df7dcf52dc4924b22bac5e040bcba
Denis Ledoux <dle@odoo.com>
2025-04-02 18:52:24 +0200

    [FIX] base: include `SELF_READABLE_FIELDS` with groups= in user view
    Fields with a `groups=` on the field definition which are
    part of the `SELF_WRITEABLE_FIELDS` list should still be
    included in the user preferences form view even
    if the user is not part of that `groups=`.
    
    e.g.
    ```py
    sign_signature = fields.Binary(..., groups="base.group_system")
    ```
    should be included in the user preferences view
    if it's included in the `SELF_WRITEABLE_FIELDS`,
    despite the user doesn't belong to the group `base.group_system`.
    
    opw-4818972

--- odoo/addons/base/models/res_users.py
+++ odoo/addons/base/models/res_users.py
@@ -17,6 +17,7 @@ from itertools import chain
 from markupsafe import Markup
 
 import pytz
+from lxml import etree
 from passlib.context import CryptContext as _CryptContext
 
 from odoo import api, fields, models, tools, _
@@ -179,7 +180,7 @@ class ResUsers(models.Model):
             'image_1024', 'image_512', 'image_256', 'image_128', 'lang', 'tz',
             'tz_offset', 'group_ids', 'partner_id', 'write_date', 'action_id',
             'avatar_1920', 'avatar_1024', 'avatar_512', 'avatar_256', 'avatar_128',
-            'share', 'device_ids', 'api_key_ids', 'phone',
+            'share', 'device_ids', 'api_key_ids', 'phone', 'display_name',
         ]
 
     @property
@@ -1291,6 +1292,16 @@ class ResUsers(models.Model):
                 for key, values in super().fields_get(missing, attributes).items()
             })
         return res
+
+    def _get_view_postprocessed(self, view, arch, **options):
+        arch, models = super()._get_view_postprocessed(view, arch, **options)
+        if view == self.env.ref('base.view_users_form_simple_modif'):
+            tree = etree.fromstring(arch)
+            for node_field in tree.xpath('//field[@__groups_key__]'):
+                if node_field.get('name') in self.SELF_READABLE_FIELDS:
+                    node_field.attrib.pop('__groups_key__')
+            arch = etree.tostring(tree)
+        return arch, models
 
 
 ResUsersPatchedInTest = ResUsers
