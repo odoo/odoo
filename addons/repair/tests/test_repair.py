f930b30e67e1198f74b6c9d3eaddc0572837e017
djameltouati <otd@odoo.com>
2025-04-02 17:44:36 +0200

    [FIX] repair: use only consumable in product catalog from Repair
    Steps to reproduce the bug:
    - Create a service product “S1”
    - Create a repair order:
        - In the parts:
            - Click on the catalog
    
    Problem:
    You can select a service product, whereas the products in the repair
    parts should be restricted to consumable-type products only:
    
    https://github.com/odoo/odoo/blob/f2923dec56b5ba2436bc244337c1e430a85e7e94/addons/stock/models/stock_move.py#L45-L48
    
    opw-4687472

--- addons/repair/tests/test_repair.py
+++ addons/repair/tests/test_repair.py
@@ -789,3 +789,12 @@ class TestRepair(common.TransactionCase):
         self.assertFalse(move.repair_id)
         self.assertEqual(move.location_id, self.stock_warehouse.lot_stock_id)
         self.assertEqual(move.location_dest_id, self.stock_location_14)
+
+    def test_add_product_from_catalog(self):
+        """Check that only consumable products are available in the catalog."""
+        catalog_action = self.repair0.action_add_from_catalog()
+        domain = catalog_action.get('domain')
+        self.assertEqual(self.product_service_order_repair.type, 'service')
+        self.assertEqual(self.product_consu_order_repair.type, 'consu')
+        self.assertTrue(self.product_consu_order_repair.filtered_domain(domain))
+        self.assertFalse(self.product_service_order_repair.filtered_domain(domain))
