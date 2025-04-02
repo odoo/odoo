6c08ea09ce3676e81fb5c4978a922d2612ce8da9
Meet Jivani [MEJI] <meji@odoo.com>
2025-04-02 17:44:13 +0530

    [FIX] point_of_sale,*: screensaver overlay issues
    * = pos_restaurant
    
    In this commit:
    - Ensured that when a popup is open and the screensaver appears, the popup no
    longer overlays the screensaver.
    - Fixed an issue in the restaurant module is installed, after the screensaver
    appears on the login screen, clicking would bring back the login screen, but
    attempting to open a session would trigger the screensaver again.
    
    task-4607035
    
    closes odoo/odoo#199528
    
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- addons/point_of_sale/static/src/app/screens/saver_screen/saver_screen.js
+++ addons/point_of_sale/static/src/app/screens/saver_screen/saver_screen.js
@@ -1,6 +1,7 @@
 import { registry } from "@web/core/registry";
 import { Component } from "@odoo/owl";
 import { useTime } from "@point_of_sale/app/utils/time_hook";
+import { useService } from "@web/core/utils/hooks";
 
 export class SaverScreen extends Component {
     static template = "point_of_sale.SaverScreen";
@@ -10,6 +11,8 @@ export class SaverScreen extends Component {
 
     setup() {
         this.time = useTime();
+        this.dialog = useService("dialog");
+        this.dialog.closeAll();
     }
 }
 
