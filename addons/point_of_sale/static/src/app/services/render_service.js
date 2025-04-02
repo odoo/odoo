e5f5dc741d42ebc2f2c550b2610930c8ad8d4a0b
Julien (jula) <jula@odoo.com>
2025-04-02 12:38:50 +0000

    [FIX] point_of_sale: fix crash with problematic chars in htmlToCanvas
    __Current behavior before commit:__
    Sometimes when a Worldline terminal makes a payment with Edenred or
    Sodexo, it creates a ticket with special ascii characters like `0x0E`
    that are not supposed to be printed. Some of those characters make the
    [`createImage`][1] method throw an error with the `onerror` Event.
    
    __Description of the fix:__
    Added the `sanitizeNodeText` function to remove all problematic ascii
    characters that should not be printed anyway.
    Added a test to make sure `htmlToCanvas` can handle all ascii characters
    without crashing.
    
    opw-4322339
    
    [1]:https://github.com/odoo/odoo/blob/a98e802976f6798f3aea07231a366fbcd8002ce2/addons/point_of_sale/static/src/app/utils/html-to-image.js#L217
    
    closes odoo/odoo#205595
    
    X-original-commit: 172ce48d91c41683853240511c71ee9f94487deb
    Signed-off-by: Vlad Stroia (vlst) <vlst@odoo.com>
    Signed-off-by: Julien Launois (jula) <jula@odoo.com>

--- addons/point_of_sale/static/src/app/services/render_service.js
+++ addons/point_of_sale/static/src/app/services/render_service.js
@@ -88,6 +88,16 @@ const applyWhenMounted = async ({ el, container, callback }) => {
     return res;
 };
 
+const sanitizeNodeText = (element) => {
+    if (element.nodeType === Node.TEXT_NODE) {
+        element.textContent = element.textContent.replace(/[\x00-\x08\x0B\x0C\x0E-\x1F]/g, ""); // eslint-disable-line no-control-regex
+        return;
+    }
+    for (const child of element.childNodes) {
+        sanitizeNodeText(child);
+    }
+};
+
 /**
  * This function assumes that the `renderer` service is available.
  */
@@ -95,6 +105,7 @@ export const htmlToCanvas = async (el, options) => {
     if (options.addClass) {
         el.classList.add(...options.addClass.split(" "));
     }
+    sanitizeNodeText(el);
     return await applyWhenMounted({
         el,
         container: document.querySelector(".render-container"),
