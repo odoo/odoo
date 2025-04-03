c5c23b851856957d0fc9fc12a3ba7925a826df1f
Robin Lejeune (role) <role@odoo.com>
2025-04-03 11:29:33 +0200

    Check each action individually for clean

--- addons/html_builder/static/tests/builder_action.test.js
+++ addons/html_builder/static/tests/builder_action.test.js
@@ -1,30 +1,83 @@
-import { expect, test } from "@odoo/hoot";
+import { beforeEach, describe, expect, test } from "@odoo/hoot";
+import { xml } from "@odoo/owl";
 import { contains, patchWithCleanup } from "@web/../tests/web_test_helpers";
 import { defineWebsiteModels, setupWebsiteBuilder } from "./website_helpers";
 import { WebsiteBuilder } from "@html_builder/website_preview/website_builder_action";
+import {
+    addBuilderAction,
+    addBuilderOption,
+    setupHTMLBuilder,
+} from "@html_builder/../tests/helpers";
 
-defineWebsiteModels();
+describe("website tests", () => {
+    beforeEach(defineWebsiteModels);
 
-test("trigger mobile view", async () => {
-    await setupWebsiteBuilder(`<h1> Homepage </h1>`);
-    expect(".o_website_preview.o_is_mobile").toHaveCount(0);
-    await contains("button[data-action='mobile']").click();
-    expect(".o_website_preview.o_is_mobile").toHaveCount(1);
+    test("trigger mobile view", async () => {
+        await setupWebsiteBuilder(`<h1> Homepage </h1>`);
+        expect(".o_website_preview.o_is_mobile").toHaveCount(0);
+        await contains("button[data-action='mobile']").click();
+        expect(".o_website_preview.o_is_mobile").toHaveCount(1);
+    });
+
+    test("top window url in action context parameter", async () => {
+        let websiteBuilder;
+        patchWithCleanup(WebsiteBuilder.prototype, {
+            setup() {
+                websiteBuilder = this;
+                this.props.action.context = {
+                    params: {
+                        path: "/web/content/",
+                    },
+                };
+                super.setup();
+            },
+        });
+        await setupWebsiteBuilder(`<h1> Homepage </h1>`);
+        expect(websiteBuilder.initialUrl).toBe("/website/force/1?path=%2F");
+    });
 });
 
-test("top window url in action context parameter", async () => {
-    let websiteBuilder;
-    patchWithCleanup(WebsiteBuilder.prototype, {
-        setup() {
-            websiteBuilder = this;
-            this.props.action.context = {
-                params: {
-                    path: "/web/content/",
+describe.tags("desktop");
+describe("HTML builder tests", () => {
+    beforeEach(() => {
+        addBuilderAction({
+            testAction: {
+                isApplied: ({ editingElement }) => editingElement.classList.contains("applied"),
+                apply: ({ editingElement }) => {
+                    editingElement.classList.toggle("applied");
+                    expect.step("apply");
                 },
-            };
-            super.setup();
-        },
+            },
+        });
     });
-    await setupWebsiteBuilder(`<h1> Homepage </h1>`);
-    expect(websiteBuilder.initialUrl).toBe("/website/force/1?path=%2F");
+
+    test("apply is called if clean is not defined", async () => {
+        addBuilderOption({
+            selector: ".s_test",
+            template: xml`<BuilderButton action="'testAction'">Click</BuilderButton>`,
+        });
+        await setupHTMLBuilder(`<section class="s_test">Test</section>`);
+        await contains(":iframe .s_test").click();
+        await contains("[data-action-id='testAction']").click();
+        expect("[data-action-id='testAction']").toHaveClass("active");
+        expect.verifySteps(["apply", "apply"]); // preview, apply
+        await contains("[data-action-id='testAction']").click();
+        expect("[data-action-id='testAction']").not.toHaveClass("active");
+        expect.verifySteps(["apply"]); // clean
+    });
+
+    test("custom action and shorthand action: clean actions are independent, apply is called on custom action if clean is not defined", async () => {
+        addBuilderOption({
+            selector: ".s_test",
+            template: xml`<BuilderButton action="'testAction'" classAction="'custom-class'">Click</BuilderButton>`,
+        });
+        await setupHTMLBuilder(`<section class="s_test">Test</section>`);
+        await contains(":iframe .s_test").click();
+        await contains("[data-action-id='testAction']").click();
+        expect("[data-action-id='testAction']").toHaveClass("active");
+        expect.verifySteps(["apply", "apply"]); // preview, apply
+        await contains("[data-action-id='testAction']").click();
+        expect("[data-action-id='testAction']").not.toHaveClass("active");
+        expect.verifySteps(["apply"]); // clean
+    });
 });
