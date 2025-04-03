3c7de268136e212baec882a8b255c36f968d2cb2
Patricia Daloze (pda) <pda@odoo.com>
2025-04-03 11:35:04 +0200

    [ADD] payment_redsys: integrate with Redsys
    Redirect flow and following payment methods: cards, bizum.
    
    task-4535224
    
    closes odoo/odoo#205135
    
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>

---
+++ addons/payment_redsys/__init__.py
@@ -0,0 +1,13 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import models
+
+from odoo.addons.payment import reset_payment_provider, setup_provider
+
+
+def post_init_hook(env):
+    setup_provider(env, 'redsys')
+
+
+def uninstall_hook(env):
+    reset_payment_provider(env, 'redsys')
