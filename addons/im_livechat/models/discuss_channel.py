811b997fa2a05e2467b83504b91b58669cf809db
ramh <ramh@odoo.com>
2025-04-04 18:53:05 +0530

    [FIX] mail, im_livechat: avoid duplicate username in pinned message
    Before this commit:
    
    When a user pinned a message in livechat, both the username and the livechat
    username were shown, resulting in a duplicate display.
    
    After this commit:
    
    Pinned notification message now show the livechat username if available.
    
    Task-4715349

--- addons/im_livechat/models/discuss_channel.py
+++ addons/im_livechat/models/discuss_channel.py
@@ -284,6 +284,11 @@ class DiscussChannel(models.Model):
                 subtype_xmlid='mail.mt_comment'
             )
 
+    def _get_user_name(self):
+        if self.env.user.livechat_username:
+            return self.env.user.livechat_username
+        return super()._get_user_name()
+
     # Rating Mixin
 
     def _rating_get_parent_field_name(self):
