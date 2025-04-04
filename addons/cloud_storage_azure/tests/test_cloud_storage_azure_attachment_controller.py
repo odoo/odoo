f96a995e245080421cfb7b3246d2635824eb2edb
Maryam Kia <maki@odoo.com>
2025-04-04 11:54:48 +0200

    [IMP] mail, *: use limited access token for attachments
    With this change we use a `limited_field_access_token` instead of `access_token`
    for attachments where it's possible. This way we avoid permanent access to an
    attachment when it's not needed which is simpler and safer in the long run.
    
    task-4676545
    
    Part-of: odoo/odoo#207009
    Related: odoo/upgrade#7595
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/cloud_storage_azure/tests/test_cloud_storage_azure_attachment_controller.py
+++ addons/cloud_storage_azure/tests/test_cloud_storage_azure_attachment_controller.py
@@ -4,7 +4,7 @@ from requests import Response
 from unittest.mock import patch
 
 import odoo
-from odoo.tools.misc import file_open
+from odoo.tools.misc import file_open, limited_field_access_token
 from odoo.addons.base.tests.common import HttpCaseWithUserDemo
 from odoo.addons.cloud_storage_azure.tests.test_cloud_storage_azure import TestCloudStorageAzureCommon
 
@@ -64,6 +64,9 @@ class TestCloudStorageAttachmentController(HttpCaseWithUserDemo, TestCloudStorag
                                     "id": attachment.id,
                                     "mimetype": "text/x-python",
                                     "name": "__init__.py",
+                                    "raw_access_token": limited_field_access_token(
+                                        attachment, "raw"
+                                    ),
                                     "res_name": False,
                                     "thread": False,
                                     "voice": False,
