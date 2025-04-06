54ecdb6cc28229f97a54c97bdd4e0f16d3c3f973
Hammad Arif <arih@odoo.com>
2025-04-06 18:20:59 +0400

    [FIX] sale_stock_margin: use SOL company context for _compute_average_price
    Current behavior before commit:
    
    The issue occurs when the product is a kit. The Procurement: run scheduler
    scheduled action updates some fields, triggering the
    `_compute_purchase_price` method. Within `_compute_purchase_price`, the
    method `_compute_average_price` is called with the company context of
    the sales order line (SOL).
    
    Company Context - correctly set to SOL company
    https://github.com/odoo/odoo/blob/a823c4ef6962672fe68726682e94ff82db211772/addons/sale_stock_margin/models/sale_order_line.py\#L14
    
    Method Call
    https://github.com/odoo/odoo/blob/a823c4ef6962672fe68726682e94ff82db211772/addons/sale_stock_margin/models/sale_order_line.py\#L18
    
    During recursion in `compute_average_price`, the company context
    incorrectly reverts to the global company instead of preserving the
    original company context from the initial call.
    
    Recursive call
    https://github.com/odoo/odoo/blob/a823c4ef6962672fe68726682e94ff82db211772/addons/mrp_account/models/product.py\#L77
    
    Since the product is a kit, its cost is derived from its components.
    However, due to the incorrect company context switch, the purchase price
    is incorrectly set to 0 (because the product does not exist in the global
    company). This distorts sales margins in ongoing sales orders.
    
    Root Cause:
    
    The stock_moves passed into the method have env.company set to the 1st
    company (due to the scheduler cron being called with sudo across all
    companies at the same time). At
    https://github.com/odoo/odoo/blob/a823c4ef6962672fe68726682e94ff82db211772/addons/mrp_account/models/product.py\#L66
    
    bom_lines are extracted, inheriting the env of the stock_moves. As a
    result, `_compute_average_price` is executed within the context of a
    company that may not match the originally set SOL company, which may
    lead to incorrect computations.
    
    Fix:
    
    Ensure that `stock_moves` is explicitly passed into the method with the
    correct company context. This guarantees that the company context remains
    consistent throughout the execution.
    
    opw-4222976
    
    closes odoo/odoo#192890
    
    Signed-off-by: Tiffany Chang (tic) <tic@odoo.com>

--- addons/sale_stock_margin/models/sale_order_line.py
+++ addons/sale_stock_margin/models/sale_order_line.py
@@ -15,7 +15,7 @@ class SaleOrderLine(models.Model):
             if not line.move_ids:
                 lines_without_moves |= line
             elif product.categ_id.property_cost_method != 'standard':
-                purch_price = product._compute_average_price(0, line.product_uom_qty, line.move_ids)
+                purch_price = product._compute_average_price(0, line.product_uom_qty, line.move_ids.with_company(line.company_id))
                 if line.product_uom and line.product_uom != product.uom_id:
                     purch_price = product.uom_id._compute_price(purch_price, line.product_uom)
                 to_cur = line.currency_id or line.order_id.currency_id
