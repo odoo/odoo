c48f359d547063d5558425c2c68d097cbc22d100
Davide Bonetto <dabo@odoo.com>
2025-04-07 10:45:20 +0200

    Event track option plugin

--- addons/html_builder/static/src/website_event_track/event_track_page_option_plugin.js
+++ addons/html_builder/static/src/website_event_track/event_track_page_option_plugin.js
@@ -3,18 +3,25 @@ import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
 
 class EventTrackPageOption extends Plugin {
-  static id = "eventTrackPageOption";
-  resources = {
-    builder_options: [
-      {
-        template: "website_event_track.EventTrackPageOption",
-        selector: "main:has(.o_wesession_index)",
-        title: _t("Event Page"),
-        editableOnly: false,
-        groups: ["website.group_website_designer"],
-      },
-    ],
-  };
+    static id = "eventTrackPageOption";
+    resources = {
+        builder_options: [
+            {
+                template: "website_event_track.eventTrackPageOptionTopbar",
+                selector: "main:has(.o_weagenda_topbar_filters)",
+                title: _t("Event Page"),
+                editableOnly: false,
+                groups: ["website.group_website_designer"],
+            },
+            {
+                template: "website_event_track.EventTrackPageOption",
+                selector: "main:has(.o_wesession_index)",
+                title: _t("Event Page"),
+                editableOnly: false,
+                groups: ["website.group_website_designer"],
+            },
+        ],
+    };
 }
 
 registry.category("website-plugins").add(EventTrackPageOption.id, EventTrackPageOption);
