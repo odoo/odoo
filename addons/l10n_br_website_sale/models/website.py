cdd9f292f9b99a0c50e395c6be0de29fc5b96208
path-odoo <path@odoo.com>
2025-04-07 10:07:17 +0530

    [FIX] l10n_*_website_sale: remove dead code
    * = `br, es`
    
    -In Odoo 18.1, the `website_sale` module included the `_display_partner_b2b_fields` method, which was used by the l10n modules which
     includes l10n_br_website_sale and l10n_es_website_sale modules as well. This method was removed in Odoo 18.2, rendering the dependent code obsolete and causing an AttributeError
     during template rendering.
    
    -Updated l10n_br_website_sale by removing [1] dead code that relied on `_display_partner_b2b_fields`.
    [1]- https://github.com/odoo/odoo/blob/e715ca59cc643e97bc6ce392bc7acb118b07209f/addons/l10n_br_website_sale/models/website.py#L16
    
    -Removed the l10n_es_website_sale module entirely, as its sole purpose was to override `_display_partner_b2b_fields` method,
     which is no longer relevant in 18.2.
    
    sentry-6505907396

--- addons/l10n_br_website_sale/models/website.py
+++ addons/l10n_br_website_sale/models/website.py
@@ -12,7 +12,3 @@ class Website(models.Model):
             if website.get('company_id') and self.env['res.company'].browse(website['company_id']).country_code == "BR":
                 website.setdefault('show_line_subtotals_tax_selection', 'tax_included')
         return super().create(vals_list)
-
-    def _display_partner_b2b_fields(self):
-        """ Brazil localization must always display b2b fields. """
-        return self.company_id.country_id.code == 'BR' or super()._display_partner_b2b_fields()
