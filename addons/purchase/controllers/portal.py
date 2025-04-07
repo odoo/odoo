cb0e683a432a31cc53ead121de8fec7fbcfe5faa
rare-odoo <rare@odoo.com>
2025-04-07 17:06:44 +0530

    [IMP] various: order edi portal access
    Implemented improvements to the access control and
    functionality for the Order EDI portal.

--- addons/purchase/controllers/portal.py
+++ addons/purchase/controllers/portal.py
@@ -196,8 +196,7 @@ class CustomerPortal(portal.CustomerPortal):
 
     @http.route(['/my/purchase/<int:order_id>/download_edi'], auth="public", website=True)
     def portal_my_purchase_order_download_edi(self, order_id=None, access_token=None, **kw):
-        """An endpoint to download EDI file representation.
-        """
+        """ An endpoint to download EDI file representation."""
         try:
             order_sudo = self._document_check_access('purchase.order', order_id, access_token=access_token)
         except (AccessError, MissingError):
@@ -208,15 +207,5 @@ class CustomerPortal(portal.CustomerPortal):
         # This handles only one builder for now, more can be added in the future
         if len(builders) != 1:
             return request.redirect('/my')
-        builder = builders[0]
 
-        xml_content = builder._export_order(order_sudo)
-
-        download_name = builder._export_purchase_order_filename(order_sudo)
-
-        http_headers = [
-            ('Content-Type', 'text/xml'),
-            ('Content-Length', len(xml_content)),
-            ('Content-Disposition', f'attachment; filename={download_name}')
-        ]
-        return request.make_response(xml_content, headers=http_headers)
+        return self._embed_edi_attachments(order_sudo, builders)
