94f897b334dc6101bc0bda72cde67e47eb68b340
lase@odoo.com <lase@odoo.com>
2025-04-08 07:50:17 +0000

    [FIX] stock: propagate move cancellations in pull chain
    Steps to reproduce:
    
    - In the settings enable Multi-Steps Rules
    - Inventory > Configuration > Warehouse Management > Warehouses
    - Put your warehouse in delivery in 2 steps and modify the rules to be
      in the old pull set up Stock 1-> Output 2-> Customer.
    - Enable the "Cancel next move" option of the pick rule.
    - Create an SO for 1 unit of a storable product
    > This should generate both a pick and a ship move.
    - Cancel the pick move
    > The ship move was not cancelled
    
    Cause of the issue:
    
    While the moves of the chain are correctly linked and the `move_dest_ids`
    of the pick move is planed to be cancelled, it does not satisfy the
    filtering condition of moves that should be cancelled because as it is
    at the end of the chain it does not have a move_dest_id it self:
    https://github.com/odoo/odoo/blob/eb43cdbfeb3d141283dbd9274fae45bf0bf641db/addons/stock/models/stock_move.py#L1966-L1968
    IMO, the condition on the locations should be set between the move we
    are cancelling and the move we plan to cancel rather than on next step
    of the chain that might not even exist.
    
    opw-4689175
    
    X-original-commit: 29ecd55e145056aafe6f96deb3d77c3f5f5bf1d2
    Part-of: odoo/odoo#209326
    Signed-off-by: Tiffany Chang (tic) <tic@odoo.com>
    Signed-off-by: Lancelot Semal (lase) <lase@odoo.com>

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -1901,7 +1901,7 @@ Please change the quantity done or the rounding precision in your settings.""",
             if move.propagate_cancel:
                 # only cancel the next move if all my siblings are also cancelled
                 if all(state == 'cancel' for state in siblings_states):
-                    move.move_dest_ids.filtered(lambda m: m.state != 'done' and m.location_dest_id == m.move_dest_ids.location_id)._action_cancel()
+                    move.move_dest_ids.filtered(lambda m: m.state != 'done' and move.location_dest_id == m.location_id)._action_cancel()
                     if cancel_moves_origin:
                         move.move_orig_ids.sudo().filtered(lambda m: m.state != 'done')._action_cancel()
             else:
