71c7b9f1eaa3ae3669995666d30ea7574e2ba61c
Louis Baudoux <lba@odoo.com>
2025-04-08 13:30:58 +0000

    [FIX] account: fix division by zero when importing a PO with != currency
    Steps to reproduce:
    - Install apps accounting, purchase and stock.
    - Create a PO with a different currency than the main currency.
    - Add a least one PO line.
    - Confirm the PO and mark the quantities as received.
    - Create a vendor bill and try to link the PO -> it crashes.
    
    Since commit 90158f6, the `balance` of the `account.move.line` is
    computed before the call to `_compute_all_tax` when importing a PO.
    Before, it wasn't computed yet and was equal to 0.
    The line `amount_currency` isn't computed yet at this point, it is
    always 0.
    
    Since the `rate` is computed as `amount_currency`/`balance`, it is
    always computed to 0, which then causes a `ZeroDivisionError` in further
    computations.
    
    opw-4710499
    
    X-original-commit: b1666c61bbc9d7e7b920483e168c6048ecde011c

--- addons/account/models/account_move_line.py
+++ addons/account/models/account_move_line.py
@@ -893,6 +893,158 @@ class AccountMoveLine(models.Model):
 
         return tax_ids
 
+<<<<<<< 35103049b14c7af77ba093b56a1b1dae1d8e7378
+||||||| 7a06f599b3d604199d08c609d2446171d4b52205
+    @api.depends('tax_ids', 'currency_id', 'partner_id', 'account_id', 'group_tax_id', 'analytic_distribution')
+    def _compute_tax_key(self):
+        for line in self:
+            if line.tax_repartition_line_id:
+                line.tax_key = frozendict({
+                    'tax_repartition_line_id': line.tax_repartition_line_id.id,
+                    'group_tax_id': line.group_tax_id.id,
+                    'account_id': line.account_id.id,
+                    'currency_id': line.currency_id.id,
+                    'analytic_distribution': line.analytic_distribution,
+                    'tax_ids': [(6, 0, line.tax_ids.ids)],
+                    'tax_tag_ids': [(6, 0, line.tax_tag_ids.ids)],
+                    'partner_id': line.partner_id.id,
+                    'move_id': line.move_id.id,
+                    'display_type': line.display_type,
+                })
+            else:
+                line.tax_key = frozendict({'id': line.id})
+
+    @api.depends('tax_ids', 'currency_id', 'partner_id', 'analytic_distribution', 'balance', 'partner_id', 'move_id.partner_id', 'price_unit', 'quantity')
+    def _compute_all_tax(self):
+        for line in self:
+            sign = line.move_id.direction_sign
+            if line.display_type == 'tax':
+                line.compute_all_tax = {}
+                line.compute_all_tax_dirty = False
+                continue
+            if line.display_type == 'product' and line.move_id.is_invoice(True):
+                amount_currency = sign * line.price_unit * (1 - line.discount / 100)
+                handle_price_include = True
+                quantity = line.quantity
+            else:
+                amount_currency = line.amount_currency
+                handle_price_include = False
+                quantity = 1
+            compute_all_currency = line.tax_ids.compute_all(
+                amount_currency,
+                currency=line.currency_id,
+                quantity=quantity,
+                product=line.product_id,
+                partner=line.move_id.partner_id or line.partner_id,
+                is_refund=line.is_refund,
+                handle_price_include=handle_price_include,
+                include_caba_tags=line.move_id.always_tax_exigible,
+                fixed_multiplicator=sign,
+            )
+            rate = line.amount_currency / line.balance if line.balance else line.currency_rate
+            line.compute_all_tax_dirty = True
+            line.compute_all_tax = {
+                frozendict({
+                    'tax_repartition_line_id': tax['tax_repartition_line_id'],
+                    'group_tax_id': tax['group'] and tax['group'].id or False,
+                    'account_id': tax['account_id'] or line.account_id.id,
+                    'currency_id': line.currency_id.id,
+                    'analytic_distribution': (tax['analytic'] or not tax['use_in_tax_closing']) and line.analytic_distribution,
+                    'tax_ids': [(6, 0, tax['tax_ids'])],
+                    'tax_tag_ids': [(6, 0, tax['tag_ids'])],
+                    'partner_id': line.move_id.partner_id.id or line.partner_id.id,
+                    'move_id': line.move_id.id,
+                    'display_type': line.display_type,
+                }): {
+                    'name': tax['name'] + (' ' + _('(Discount)') if line.display_type == 'epd' else ''),
+                    'balance': tax['amount'] / rate,
+                    'amount_currency': tax['amount'],
+                    'tax_base_amount': tax['base'] / rate * (-1 if line.tax_tag_invert else 1),
+                }
+                for tax in compute_all_currency['taxes']
+                if tax['amount']
+            }
+            if not line.tax_repartition_line_id:
+                line.compute_all_tax[frozendict({'id': line.id})] = {
+                    'tax_tag_ids': [(6, 0, compute_all_currency['base_tags'])],
+                }
+
+=======
+    @api.depends('tax_ids', 'currency_id', 'partner_id', 'account_id', 'group_tax_id', 'analytic_distribution')
+    def _compute_tax_key(self):
+        for line in self:
+            if line.tax_repartition_line_id:
+                line.tax_key = frozendict({
+                    'tax_repartition_line_id': line.tax_repartition_line_id.id,
+                    'group_tax_id': line.group_tax_id.id,
+                    'account_id': line.account_id.id,
+                    'currency_id': line.currency_id.id,
+                    'analytic_distribution': line.analytic_distribution,
+                    'tax_ids': [(6, 0, line.tax_ids.ids)],
+                    'tax_tag_ids': [(6, 0, line.tax_tag_ids.ids)],
+                    'partner_id': line.partner_id.id,
+                    'move_id': line.move_id.id,
+                    'display_type': line.display_type,
+                })
+            else:
+                line.tax_key = frozendict({'id': line.id})
+
+    @api.depends('tax_ids', 'currency_id', 'partner_id', 'analytic_distribution', 'balance', 'partner_id', 'move_id.partner_id', 'price_unit', 'quantity')
+    def _compute_all_tax(self):
+        for line in self:
+            sign = line.move_id.direction_sign
+            if line.display_type == 'tax':
+                line.compute_all_tax = {}
+                line.compute_all_tax_dirty = False
+                continue
+            if line.display_type == 'product' and line.move_id.is_invoice(True):
+                amount_currency = sign * line.price_unit * (1 - line.discount / 100)
+                handle_price_include = True
+                quantity = line.quantity
+            else:
+                amount_currency = line.amount_currency
+                handle_price_include = False
+                quantity = 1
+            compute_all_currency = line.tax_ids.compute_all(
+                amount_currency,
+                currency=line.currency_id,
+                quantity=quantity,
+                product=line.product_id,
+                partner=line.move_id.partner_id or line.partner_id,
+                is_refund=line.is_refund,
+                handle_price_include=handle_price_include,
+                include_caba_tags=line.move_id.always_tax_exigible,
+                fixed_multiplicator=sign,
+            )
+            rate = line.amount_currency / line.balance if (line.amount_currency and line.balance) else line.currency_rate
+            line.compute_all_tax_dirty = True
+            line.compute_all_tax = {
+                frozendict({
+                    'tax_repartition_line_id': tax['tax_repartition_line_id'],
+                    'group_tax_id': tax['group'] and tax['group'].id or False,
+                    'account_id': tax['account_id'] or line.account_id.id,
+                    'currency_id': line.currency_id.id,
+                    'analytic_distribution': (tax['analytic'] or not tax['use_in_tax_closing']) and line.analytic_distribution,
+                    'tax_ids': [(6, 0, tax['tax_ids'])],
+                    'tax_tag_ids': [(6, 0, tax['tag_ids'])],
+                    'partner_id': line.move_id.partner_id.id or line.partner_id.id,
+                    'move_id': line.move_id.id,
+                    'display_type': line.display_type,
+                }): {
+                    'name': tax['name'] + (' ' + _('(Discount)') if line.display_type == 'epd' else ''),
+                    'balance': tax['amount'] / rate,
+                    'amount_currency': tax['amount'],
+                    'tax_base_amount': tax['base'] / rate * (-1 if line.tax_tag_invert else 1),
+                }
+                for tax in compute_all_currency['taxes']
+                if tax['amount']
+            }
+            if not line.tax_repartition_line_id:
+                line.compute_all_tax[frozendict({'id': line.id})] = {
+                    'tax_tag_ids': [(6, 0, compute_all_currency['base_tags'])],
+                }
+
+>>>>>>> 3c88588614bc1a2516ff9f62c8d833e7d5bc3db1
     @api.depends('account_id', 'company_id')
     def _compute_discount_allocation_key(self):
         for line in self:
