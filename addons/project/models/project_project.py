feb23da12514731f3f10d0e44948d2a08e96d82b
Dhrumit Parmar <dgpa@odoo.com>
2025-04-08 10:12:26 +0530

    [IMP] project: select project manager and task assignee based on project company
    Currently, users from any company could be selected as project managers or
    task assignees.
    
    This commit adds a company-based domain to restrict user selection
    according to the project's company, ensuring only relevant users can be selected.
    
    If the project's company is listed in the user's allowed companies
    (`company_ids`), the user can be selected as the project manager or assignee.
    
    task-4598852

--- addons/project/models/project_project.py
+++ addons/project/models/project_project.py
@@ -117,7 +117,8 @@ class ProjectProject(models.Model):
     task_ids = fields.One2many('project.task', 'project_id', string='Tasks', export_string_translation=False,
                                domain="[('is_closed', '=', False)]")
     color = fields.Integer(string='Color Index', export_string_translation=False)
-    user_id = fields.Many2one('res.users', string='Project Manager', default=lambda self: self.env.user, tracking=True, falsy_value_label=_lt("👤 Unassigned"))
+    user_id = fields.Many2one('res.users', string='Project Manager', default=lambda self: self.env.user, tracking=True, falsy_value_label=_lt("👤 Unassigned"),
+        domain="[('share', '=', False), '|', ('company_id', '=?', company_id), ('company_ids', 'in', company_id)]")
     alias_id = fields.Many2one(help="Internal email associated with this project. Incoming emails are automatically synchronized "
                                     "with Tasks (or optionally Issues if the Issue Tracker module is installed).")
     privacy_visibility = fields.Selection([
