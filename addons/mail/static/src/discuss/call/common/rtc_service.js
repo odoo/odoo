f3fc6d5604ae8ec250720aa2794afa69775ccddb
TSO <tso@odoo.com>
2025-04-08 13:59:17 +0000

    [FIX] discuss: prevent peer negotiations for removed peers
    Before this commit, and since the addition of the SFU fallback feature,
    it was possible that a peer was removed while being created or
    connected, this could lead to a race condition where we no longer need
    the peer but some negotiations are still being made.
    
    This commit fixes this issue by stopping the negotiation if the
    peer no longer exist when the promises are resolved.
    
    Another race condition could lead to receiving a track from the
    failed peer-to-peer connection (as tracks are generated when
    transceiver are created, before we know if the connection is stable),
    while we already have a track from the SFU.
    
    This commit fixes this issue by:
    
    1) Awaiting that the p2p connection is ready before emitting the tracks,
    which ensures that the API only provides valid tracks (when usable).
    2) Adding a concept of sequence to prevent race conditions between
    connections and tracks.
    
    X-original-commit: 4acfa584bbcf8772648b4472ce7aa9d007c5f5c9

--- addons/mail/static/src/discuss/call/common/rtc_service.js
+++ addons/mail/static/src/discuss/call/common/rtc_service.js
@@ -16,6 +16,9 @@ import { memoize } from "@web/core/utils/functions";
 import { url } from "@web/core/utils/urls";
 import { callActionsRegistry } from "./call_actions";
 
+let sequence = 1;
+const getSequence = () => sequence++;
+
 /**
  * @typedef {'audio' | 'camera' | 'screen' } streamType
  */
@@ -700,6 +703,24 @@ export class Rtc extends Record {
             return;
         }
         switch (name) {
+            case "broadcast":
+                {
+                    const {
+                        senderId,
+                        message: { sequence },
+                    } = payload;
+                    if (!sequence) {
+                        return;
+                    }
+                    const session = await this.store.RtcSession.getWhenReady(senderId);
+                    if (!session) {
+                        return;
+                    }
+                    if (!session.sequence || session.sequence < sequence) {
+                        session.sequence = sequence;
+                    }
+                }
+                return;
             case "connection_change":
                 {
                     const { id, state } = payload;
@@ -753,7 +774,7 @@ export class Rtc extends Record {
                 return;
             case "track":
                 {
-                    const { sessionId, type, track, active } = payload;
+                    const { sessionId, type, track, active, sequence } = payload;
                     const session = await this.store["discuss.channel.rtc.session"].getWhenReady(
                         sessionId
                     );
@@ -761,6 +782,13 @@ export class Rtc extends Record {
                         this.log(
                             this.selfSession,
                             `track received for unknown session ${sessionId} (${this.state.connectionType})`
+                        );
+                        return;
+                    }
+                    if (sequence && sequence < session.sequence) {
+                        this.log(
+                            session,
+                            `track received for old sequence ${sequence} (${this.state.connectionType})`
                         );
                         return;
                     }
@@ -787,6 +815,7 @@ export class Rtc extends Record {
             case this.SFU_CLIENT_STATE.AUTHENTICATED:
                 // if we are hot-swapping connection type, we clear the p2p as late as possible
                 this.p2pService.removeALlPeers();
+                this.sfuClient.broadcast({ sequence: getSequence() });
                 break;
             case this.SFU_CLIENT_STATE.CONNECTED:
                 browser.clearTimeout(this.sfuTimeout);
@@ -859,12 +888,13 @@ export class Rtc extends Record {
         if (this.state.channel.rtcSessions.length === 0) {
             return;
         }
+        const sequence = getSequence();
         for (const session of this.state.channel.rtcSessions) {
             if (session.eq(this.selfSession)) {
                 continue;
             }
             this.log(session, "init call", { step: "init call" });
-            this.p2pService.addPeer(session.id);
+            this.p2pService.addPeer(session.id, { sequence });
         }
     }
 
@@ -1595,6 +1625,14 @@ export const rtcService = {
     start(env, services) {
         const rtc = env.services["mail.store"].rtc;
         rtc.p2pService = services["discuss.p2p"];
+        rtc.p2pService.acceptOffer = async (id, sequence) => {
+            const session = await this.store.RtcSession.getWhenReady(Number(id));
+            /**
+             * We only accept offers for new connections (higher sequence),
+             * or offers that renegotiate an existing connection (same sequence).
+             */
+            return sequence >= session?.sequence;
+        };
         services["bus_service"].subscribe(
             "discuss.channel.rtc.session/sfu_hot_swap",
             async ({ serverInfo }) => {
