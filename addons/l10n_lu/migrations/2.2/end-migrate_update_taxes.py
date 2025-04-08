cb821f7c2f9c6506aa9c763ea9cc6482e6fe7dd4
AhmedElemary57 <aele@odoo.com>
2025-04-08 16:55:41 +0200

    [FIX] account: Prevent Forced Creation of Accounts, Taxes, and Fiscal Positions During Upgrade
    - Updated UK localization template handling to avoid automatic recreation of accounts, taxes, and fiscal positions when upgrading
    
    - Added `force_create = "0"` parameter to `try_loading` to ensure that existing configurations are retained and no new records are created unless explicitly required.
    - Fixed a similar issue for all impacted localizations, following the discussion in the previous task (https://www.odoo.com/odoo/project/967/tasks/4556250).
    
    task-4712602

--- addons/l10n_lu/migrations/2.2/end-migrate_update_taxes.py
+++ addons/l10n_lu/migrations/2.2/end-migrate_update_taxes.py
@@ -4,4 +4,4 @@ from odoo import api, SUPERUSER_ID
 def migrate(cr, version):
     env = api.Environment(cr, SUPERUSER_ID, {})
     for company in env['res.company'].search([('chart_template', '=', 'lu')], order="parent_path"):
-        env['account.chart.template'].try_loading('lu', company)
+        env['account.chart.template'].try_loading('lu', company, force_create=False)
