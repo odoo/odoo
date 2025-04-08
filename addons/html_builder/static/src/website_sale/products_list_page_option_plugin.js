e66eeeebe5e9d32565956ecf069abd89b2861c3e
FrancoisGe <fge@odoo.com>
2025-04-08 11:51:11 +0200

    add product list page

---
+++ addons/html_builder/static/src/website_sale/products_list_page_option_plugin.js
@@ -0,0 +1,67 @@
+import { Plugin } from "@html_editor/plugin";
+import { withSequence } from "@html_editor/utils/resource";
+import { _t } from "@web/core/l10n/translation";
+import { rpc } from "@web/core/network/rpc";
+import { registry } from "@web/core/registry";
+
+class ProductsListPageOptionPlugin extends Plugin {
+    static id = "productsListPageOptionPlugin";
+    resources = {
+        builder_options: [
+            withSequence(15, {
+                template: "website_sale.ProductsListPageOption",
+                selector: "main:has(.o_wsale_products_page)",
+                applyTo:
+                    "#products_grid .o_wsale_products_grid_table_wrapper > .o_wsale_products_grid_table",
+                editableOnly: false,
+                title: _t("Products Page"),
+                groups: ["website.group_website_designer"],
+            }),
+        ],
+        builder_actions: this.getActions(),
+    };
+
+    getActions() {
+        return {
+            setPpg: {
+                isReload: true,
+                getValue: ({ editingElement }) => parseInt(editingElement.dataset.ppg),
+                apply: ({ value }) => {
+                    const PPG_LIMIT = 10000;
+                    let ppg = parseInt(value);
+                    if (!ppg || ppg < 1) {
+                        return false;
+                    }
+                    ppg = Math.min(ppg, PPG_LIMIT);
+                    return rpc("/shop/config/website", { shop_ppg: ppg });
+                },
+            },
+            setPpr: {
+                isReload: true,
+                isApplied: ({ editingElement, value }) =>
+                    parseInt(editingElement.dataset.ppr) === value,
+                apply: ({ value }) => {
+                    const ppr = parseInt(value);
+                    return rpc("/shop/config/website", { shop_ppr: ppr });
+                },
+            },
+            setGap: {
+                isReload: true,
+                apply: ({ value }) => {
+                    const gap = parseInt(value);
+                    return rpc("/shop/config/website", { shop_gap: gap });
+                },
+            },
+            setDefaultSort: {
+                isReload: true,
+                isApplied: ({ editingElement, value }) =>
+                    editingElement.dataset.defaultSort === value,
+                apply: ({ value }) => rpc("/shop/config/website", { shop_default_sort: value }),
+            },
+        };
+    }
+}
+
+registry
+    .category("website-plugins")
+    .add(ProductsListPageOptionPlugin.id, ProductsListPageOptionPlugin);
