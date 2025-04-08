a6e2830acaaf5430b4483b99086cb409538a43b7
drat-odoo <drat@odoo.com>
2025-04-08 14:56:04 +0530

    [IMP] resource_mail: remove color field from resource
    Before this commit:
    - The color field was visible in Resources > Materials kanban and list views.
    - The color field was also displayed in the Many2ManyAvatarUserTagsList widget.
    
    After this commit:
    - The color field has been removed from all materials-related views,
      including the materials widget.
    
    task-4678837

--- addons/resource_mail/models/resource_resource.py
+++ addons/resource_mail/models/resource_resource.py
@@ -2,19 +2,25 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from random import randint
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ResourceResource(models.Model):
     _inherit = 'resource.resource'
 
-    def _default_color(self):
-        return randint(1, 11)
-
-    color = fields.Integer(default=_default_color)
+    role_color = fields.Integer(string='Role Color', compute='_compute_role_color', store=False)
     im_status = fields.Char(related='user_id.im_status')
 
     def get_avatar_card_data(self, fields):
         return self.env['resource.resource'].search_read(
             domain=[('id', 'in', self.ids)],
         )
+
+    @api.depends("role_ids")
+    def _compute_role_color(self):
+        for slot in self:
+            if slot.role_ids:
+                slot.role_color = slot.role_ids[0].color
+            else:
+                # fallback to prevent not having color in dark mode (dont use 0)
+                slot.role_color = 1
