71a9b8637ac52be10578a40b088c2340230b18f6
ramh <ramh@odoo.com>
2025-04-09 18:01:46 +0530

    [FIX] mail,im_livechat: return guest and partner both in _get_current_persona
    Before this commit:
    
    `_get_current_persona` would return the guest record if available, falling back
    to the current partner otherwise. This made it impossible to access both
    contexts when both records were relevant.
    
    After this commit:
    
    `_get_current_persona` returns both the guest and the current partner
    when possible.
    
    Task-4715081

--- addons/mail/static/tests/mock_server/mock_models/discuss_channel.js
+++ addons/mail/static/tests/mock_server/mock_models/discuss_channel.js
@@ -1061,7 +1061,7 @@ export class DiscussChannel extends models.ServerModel {
         }
         return DiscussChannelMember._filter([
             ["channel_id", "=", id],
-            guest ? ["guest_id", "=", guest.id] : ["partner_id", "=", partner.id],
+            partner ? ["partner_id", "=", partner.id] : ["guest_id", "=", guest.id],
         ])[0];
     }
 
