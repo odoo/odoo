0ee733a61ea0e6ca2e832c74abd0ee541f331a47
Julien Castiaux <juc@odoo.com>
2025-04-09 11:25:51 +0200

    [IMP] test_lint: lint all docstrings
    Several docstrings contain `:param:` fields that we want to extract to
    feed the documentation of /doc, but parsing those docstrings fail due to
    bad rst, preventing the correct loading of parameter fields.
    
    This commit changes the docstring linter to log all warnings (and more
    severe) messages emitted by docutils while parsing the docstring.
    
    task-4572591

--- odoo/addons/test_lint/tests/test_docstring.py
+++ odoo/addons/test_lint/tests/test_docstring.py
@@ -1,5 +1,10 @@
+import contextlib
 import docutils
+import docutils.parsers.rst
+from docutils.parsers.rst.directives.admonitions import Note
+import docutils.nodes
 import inspect
+import io
 
 from odoo.modules.registry import Registry
 from odoo.tests.common import get_db_name, tagged
@@ -38,13 +43,7 @@ RST_INFO_FIELDS = {
 }
 
 
-def extract_docstring_types(method):
-    docstring = inspect.cleandoc(method.__doc__)
-    doctree = docutils.core.publish_doctree(docstring, settings_overrides={
-        'report_level': 5,
-        'halt_level': 5,
-    })
-
+def extract_docstring_types(doctree):
     types = {}
     rtype = inspect._empty
 
@@ -99,7 +98,34 @@ def extract_docstring_types(method):
 
 @tagged('-at_install', 'post_install')
 class TestDocstring(LintCase):
-    def test_docstring_params(self):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        # The docstrings can use many more roles and directives than the
+        # one present natively in docutils. That's because we use Sphinx
+        # to render them in the documentation, and Sphinx defines the
+        # "Python Domain", a set of additional rules and directive to
+        # understand the python language.
+        # 
+        # It is not desirable to add a dependency on Sphinx in
+        # community, at least not only for this linter.
+        #
+        # The following code adds a bunch of dummy elements for the
+        # missing roles and directives, so docutils is able to parse
+        # them with no warning.
+
+        def role_function(name, rawtext, text, lineno, inliner, options=None, content=None):
+            return [docutils.nodes.inline(rawtext, text)], []
+
+        for role in ('attr', 'class', 'func', 'meth', 'ref', 'const', 'samp', 'term'):
+            docutils.parsers.rst.roles.register_local_role(role, role_function)
+
+        for directive in ('attribute', ):
+            docutils.parsers.rst.directives.register_directive(
+                directive, docutils.parsers.rst.directives.admonitions.Note)
+
+    def test_docstring(self):
         """ Verify that the function signature and its docstring match. """
         registry = Registry(get_db_name())
         seen_methods = set()
@@ -111,6 +137,9 @@ class TestDocstring(LintCase):
                 if method_name in seen_methods:
                     continue
                 seen_methods.add(method_name)
+
+                # We don't care of the docstring in overrides, find the
+                # class that introduced the method.
                 reverse_mro = reversed(model_cls.mro()[1:-1])
                 for parent_class in reverse_mro:
                     method = getattr(parent_class, method_name, None)
@@ -124,21 +153,35 @@ class TestDocstring(LintCase):
                     model=parent_class._name,
                     method=method_name
                 ):
-                    doc_types, doc_rtype = extract_docstring_types(method)
+                    with contextlib.redirect_stderr(io.StringIO()) as stderr:
+                        doctree = docutils.core.publish_doctree(
+                            inspect.cleandoc(method.__doc__),
+                            settings_overrides={
+                                'report_level': 2,  # WARNING
+                                'halt_level': 5,  # CRITICAL TICAL!
+                            }
+                        )
+                        if stderr.tell():
+                            self.fail(stderr.getvalue())
 
-                    signature = inspect.signature(method)
-                    sign_types = {
-                        param.name: param.annotation
-                        for param in signature.parameters.values()
-                    }
-                    sign_rtype = signature.return_annotation
+                    self._test_docstring_params(method, doctree)
 
-                    if sign_rtype != signature.empty and doc_rtype != signature.empty:
-                        self.assertEqual(sign_rtype, doc_rtype)
+    def _test_docstring_params(self, method, doctree):
+        doc_types, doc_rtype = extract_docstring_types(doctree)
 
-                    self.assertGreaterEqual(set(sign_types), set(doc_types))
+        signature = inspect.signature(method)
+        sign_types = {
+            param.name: param.annotation
+            for param in signature.parameters.values()
+        }
+        sign_rtype = signature.return_annotation
 
-                    for param, doc_type in doc_types.items():
-                        sign_type = sign_types[param]
-                        if sign_type != signature.empty:
-                            self.assertEqual(sign_type, doc_type)
+        if sign_rtype != signature.empty and doc_rtype != signature.empty:
+            self.assertEqual(sign_rtype, doc_rtype)
+
+        self.assertGreaterEqual(set(sign_types), set(doc_types))
+
+        for param, doc_type in doc_types.items():
+            sign_type = sign_types[param]
+            if sign_type != signature.empty:
+                self.assertEqual(sign_type, doc_type)
