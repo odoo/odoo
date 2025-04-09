99d00987aa0fba0e52a8c416e1a6a3b514b9e99a
Sébastien Theys <seb@odoo.com>
2025-04-09 16:20:36 +0200

    [FIX] mail, test_mail: display followers of another company
    - have a follower with a portal partner linked to a specific company
      "portal" user is important here because internal users don't have
      multi-company rule.
    - view the chatter with a user which has no access to that company
    - the chatter is currently locked by an ACL error
    
    The follower should be displayed instead.
    
    task-4648765
    
    closes odoo/odoo#205063
    
    X-original-commit: adc6d4863f90c44346a7b86534b8de3728394934
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/models/mail_followers.py
+++ addons/mail/models/mail_followers.py
@@ -18,7 +18,6 @@ class Followers(models.Model):
     :param: res_id: ID of resource (may be 0 for every objects)
     """
     _name = 'mail.followers'
-    _rec_name = 'partner_id'
     _log_access = False
     _description = 'Document Followers'
 
@@ -70,6 +69,13 @@ class Followers(models.Model):
     _sql_constraints = [
         ('mail_followers_res_partner_res_model_id_uniq', 'unique(res_model,res_id,partner_id)', 'Error, a partner cannot follow twice the same object.'),
     ]
+
+    @api.depends("partner_id")
+    def _compute_display_name(self):
+        for follower in self:
+            # sudo: res.partner - can read partners of accessible followers, in particular allows
+            # by-passing multi-company ACL for portal partners
+            follower.display_name = follower.partner_id.sudo().display_name
 
     # --------------------------------------------------
     # Private tools methods to fetch followers data
@@ -528,7 +534,9 @@ GROUP BY fol.id%s%s""" % (
                 [field for field in fields if field not in ["partner", "thread"]], load=False
             )[0]
             if "partner" in fields:
-                data["partner"] = Store.one(follower.partner_id, fields=fields["partner"])
+                # sudo: res.partner - can read partners of found followers, in particular allows
+                # by-passing multi-company ACL for portal partners
+                data["partner"] = Store.one(follower.partner_id.sudo(), fields=fields["partner"])
             if "thread" in fields:
                 data["thread"] = Store.one(
                     self.env[follower.res_model].browse(follower.res_id),
