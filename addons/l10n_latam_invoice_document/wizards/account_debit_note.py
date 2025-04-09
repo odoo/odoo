cc70040e22c41eeadbdce4f3907c66fda0bc68f6
Katherine Zaoral <kz@adhoc.com.ar>
2025-04-09 17:08:50 -0300

    [ADD] l10n_latam_invoice_document: fix multi loc not latam

--- addons/l10n_latam_invoice_document/wizards/account_debit_note.py
+++ addons/l10n_latam_invoice_document/wizards/account_debit_note.py
@@ -7,14 +7,16 @@ class AccountDebitNote(models.TransientModel):
 
     def _prepare_default_values(self, move):
         default_values = super()._prepare_default_values(move)
+        if move.company_id._localization_use_documents():
+            # properly compute debit type
+            debit_note = self.env['account.move'].new({
+                'move_type': default_values.get('move_type'),
+                'journal_id': default_values.get('journal_id'),
+                'partner_id': move.partner_id.id,
+                'company_id': move.company_id.id,
+                'debit_origin_id': move.id,
+            })
+            document_types = debit_note.l10n_latam_available_document_type_ids.filtered(lambda x: x.internal_type == 'debit_note')
+            default_values['l10n_latam_document_type_id'] = document_types and document_types[0].ids[0]
 
-        # properly compute debit type
-        debit_note = self.env['account.move'].new({
-            'move_type': default_values.get('move_type'),
-            'journal_id': default_values.get('journal_id'),
-            'partner_id': move.partner_id.id,
-            'company_id': move.company_id.id,
-        })
-        document_types = debit_note.l10n_latam_available_document_type_ids.filtered(lambda x: x.internal_type == 'debit_note')
-        default_values['l10n_latam_document_type_id'] = document_types and document_types[0].ids[0]
         return default_values
