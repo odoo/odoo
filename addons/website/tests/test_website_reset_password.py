b31c9e86c8707277bb6ee40c7538f183884925f2
Rahil Ghanchi <rahg@odoo.com>
2025-04-09 23:00:16 +0530

    [IMP] website: adapt website_reset_password

--- addons/website/tests/test_website_reset_password.py
+++ addons/website/tests/test_website_reset_password.py
@@ -12,7 +12,7 @@ from odoo.tests.common import HttpCase
 @tagged('post_install', '-at_install')
 class TestWebsiteResetPassword(HttpCase):
 
-    @unittest.skip
+
     def test_01_website_reset_password_tour(self):
         """The goal of this test is to make sure the reset password works."""
         # We override unlink because we don't want the email to be auto deleted
