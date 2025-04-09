a4c0bfa1835e8c2da1f75107f6cad8adcae2f4a9
Rahul Prajapati <rapr@odoo.com>
2025-04-09 16:01:00 +0530

    [FIX] mail, portal_rating: empty message save opens composer in dialogbox
    Before this commit:
    
    When trying to save an empty message after editing, a confirmation dialog
    would appear asking if you want to delete the message. However, instead
    of showing a preview of the message, it displayed an empty composer.
    
    After this commit:
    
    The confirmation dialog now correctly displays the preview of the message.
    
    Task-4708569

--- addons/mail/static/src/core/common/message.js
+++ addons/mail/static/src/core/common/message.js
@@ -92,6 +92,7 @@ export class Message extends Component {
         "className?",
         "showDates?",
         "isFirstMessage?",
+        "isReadOnly?",
     ];
     static template = "mail.Message";
 
@@ -199,16 +200,16 @@ export class Message extends Component {
                 this.message.richTranslationValue,
                 this.props.messageSearch?.searchTerm,
                 this.message.richBody,
-                this.message.composer,
+                this.isEditing,
             ]
         );
         useEffect(
             () => {
-                if (!this.message.composer) {
+                if (!this.isEditing) {
                     this.prepareMessageBody(this.messageBody.el);
                 }
             },
-            () => [this.message.composer, this.message.richBody]
+            () => [this.isEditing, this.message.richBody]
         );
     }
 
@@ -228,7 +229,7 @@ export class Message extends Component {
             "px-1": this.props.isInChatWindow,
             "opacity-50": this.props.thread?.composer.replyToMessage?.notEq(this.props.message),
             "o-actionMenuMobileOpen": this.state.actionMenuMobileOpen,
-            "o-editing": this.props.message.composer,
+            "o-editing": this.isEditing,
         };
     }
 
@@ -260,7 +261,7 @@ export class Message extends Component {
     }
 
     get isEditing() {
-        return this.props.message.composer;
+        return !this.props.isReadOnly && this.props.message.composer;
     }
 
     get message() {
