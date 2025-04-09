b3e2d8677d93083c2638d7dce10c7e57e1e8c2d2
Louis Baudoux <lba@odoo.com>
2025-04-09 17:24:22 +0200

    [REM] iap: remove dead code
    The code to handle the insufficient credit errors isn't used in
    practice, it was replaced by the `iapNotification` service in
    `iap_mail` a long time ago.
    
    closes odoo/odoo#205416
    
    Signed-off-by: Ricardo Gomes Rodrigues (rigr) <rigr@odoo.com>

--- addons/iap/__init__.py
+++ addons/iap/__init__.py
@@ -6,8 +6,4 @@ from . import tools
 
 # compatibility imports
 from odoo.addons.iap.tools.iap_tools import iap_jsonrpc as jsonrpc
-from odoo.addons.iap.tools.iap_tools import iap_authorize as authorize
-from odoo.addons.iap.tools.iap_tools import iap_cancel as cancel
-from odoo.addons.iap.tools.iap_tools import iap_capture as capture
-from odoo.addons.iap.tools.iap_tools import iap_charge as charge
 from odoo.addons.iap.tools.iap_tools import InsufficientCreditError
--- addons/iap/tools/iap_tools.py
+++ addons/iap/tools/iap_tools.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import contextlib
 import logging
-import json
 import requests
 import uuid
 
@@ -88,17 +86,10 @@ def mail_prepare_for_domain_search(email, min_email_length=0):
     return email_tocheck
 
 
-#----------------------------------------------------------
-# Helpers for both clients and proxy
-#----------------------------------------------------------
-
 def iap_get_endpoint(env):
     url = env['ir.config_parameter'].sudo().get_param('iap.endpoint', DEFAULT_ENDPOINT)
     return url
 
-#----------------------------------------------------------
-# Helpers for clients
-#----------------------------------------------------------
 
 class InsufficientCreditError(Exception):
     pass
@@ -148,89 +139,3 @@ def iap_jsonrpc(url, method='call', params=None, timeout=15):
         raise exceptions.AccessError(
             _("An error occurred while reaching %s. Please contact Odoo support if this error persists.", url)
         )
-
-#----------------------------------------------------------
-# Helpers for proxy
-#----------------------------------------------------------
-
-class IapTransaction(object):
-
-    def __init__(self):
-        self.credit = None
-
-
-def iap_authorize(env, key, account_token, credit, dbuuid=False, description=None, credit_template=None, ttl=4320):
-    endpoint = iap_get_endpoint(env)
-    params = {
-        'account_token': account_token,
-        'credit': credit,
-        'key': key,
-        'description': description,
-        'ttl': ttl,
-    }
-    if dbuuid:
-        params.update({'dbuuid': dbuuid})
-    try:
-        transaction_token = iap_jsonrpc(endpoint + '/iap/1/authorize', params=params)
-    except InsufficientCreditError as e:
-        if credit_template:
-            arguments = json.loads(e.args[0])
-            arguments['body'] = env['ir.qweb']._render(credit_template)
-            e.args = (json.dumps(arguments),)
-        raise e
-    return transaction_token
-
-
-def iap_cancel(env, transaction_token, key):
-    endpoint = iap_get_endpoint(env)
-    params = {
-        'token': transaction_token,
-        'key': key,
-    }
-    r = iap_jsonrpc(endpoint + '/iap/1/cancel', params=params)
-    return r
-
-
-def iap_capture(env, transaction_token, key, credit):
-    endpoint = iap_get_endpoint(env)
-    params = {
-        'token': transaction_token,
-        'key': key,
-        'credit_to_capture': credit,
-    }
-    r = iap_jsonrpc(endpoint + '/iap/1/capture', params=params)
-    return r
-
-
-@contextlib.contextmanager
-def iap_charge(env, key, account_token, credit, dbuuid=False, description=None, credit_template=None, ttl=4320):
-    """
-    Account charge context manager: takes a hold for ``credit``
-    amount before executing the body, then captures it if there
-    is no error, or cancels it if the body generates an exception.
-
-    :param str key: service identifier
-    :param str account_token: user identifier
-    :param int credit: cost of the body's operation
-    :param description: a description of the purpose of the charge,
-                        the user will be able to see it in their
-                        dashboard
-    :type description: str
-    :param credit_template: a QWeb template to render and show to the
-                            user if their account does not have enough
-                            credits for the requested operation
-    :param int ttl: transaction time to live in hours.
-                    If the credit are not captured when the transaction
-                    expires, the transaction is canceled
-    :type credit_template: str
-    """
-    transaction_token = iap_authorize(env, key, account_token, credit, dbuuid, description, credit_template, ttl)
-    try:
-        transaction = IapTransaction()
-        transaction.credit = credit
-        yield transaction
-    except Exception as e:
-        r = iap_cancel(env,transaction_token, key)
-        raise e
-    else:
-        r = iap_capture(env,transaction_token, key, transaction.credit)
