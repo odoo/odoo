0d2fc316a6505217b361fbe0fb997f6c4b2c5990
Max Whale <mawh@odoo.com>
2025-04-09 11:10:13 +0200

    [IMP] hw_*: use iot route wrapper
    Before this commit, for routes defined on the IoT box, we had to
    manually set `auth='none'` and `save_session=False` for every route.
    While forgetting to set `save_session=False` is okay, it is what causes
    the many DEBUG logs referred to in task-4587619.
    
    After this commit, there is an `iot_route` decorator that wraps the
    existing `http.route`, defaulting `auth` and `save_session` to the
    correct values. It also allows setting `sign=True` to automatically
    wrap the route with `@route.protect`.
    
    task-4587619
    
    closes odoo/odoo#205331
    
    Signed-off-by: Louis Travaux (lotr) <lotr@odoo.com>

--- addons/hw_drivers/tools/route.py
+++ addons/hw_drivers/tools/route.py
@@ -4,6 +4,7 @@ import logging
 from odoo.addons.iot_base.tools.payload_signature import verify_hmac_signature
 from odoo.addons.hw_drivers.tools import helpers
 from odoo.http import request
+from odoo import http
 from werkzeug.exceptions import Forbidden
 
 _logger = logging.getLogger(__name__)
@@ -30,3 +31,25 @@ def protect(endpoint):
 
         return endpoint(*args, **kwargs)
     return protect_wrapper
+
+
+def iot_route(route=None, sign=False, **kwargs):
+    """A wrapper for the http.route function that sets useful defaults for IoT:
+      - `auth = 'none'`
+      - `save_session = False`
+
+    It also adds the `sign` parameter, which if `True` wraps the route with `@route.protect`.
+
+    Both auth and sessions are useless on IoT since we have no DB and no users.
+    """
+    if 'auth' not in kwargs:
+        kwargs['auth'] = 'none'
+    if 'save_session' not in kwargs:
+        kwargs['save_session'] = False
+
+    http_decorator = http.route(route, **kwargs)
+
+    def decorator(endpoint):
+        return protect(http_decorator(endpoint)) if sign else http_decorator(endpoint)
+
+    return decorator
