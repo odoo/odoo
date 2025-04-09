46a21f0ed93826f8143dd4815bac2d5202f44f05
aans-odoo <aans@odoo.com>
2025-04-09 18:08:43 +0530

    [IMP] website: adapt editable_root_as_custom_snippet tour

--- addons/website/tests/test_custom_snippets.py
+++ addons/website/tests/test_custom_snippets.py
@@ -196,7 +196,6 @@ class TestCustomSnippet(TransactionCase):
 @tagged('post_install', '-at_install')
 class TestHttpCustomSnippet(HttpCase):
 
-    @unittest.skip
     def test_editable_root_as_custom_snippet(self):
         View = self.env['ir.ui.view']
         Page = self.env['website.page']
