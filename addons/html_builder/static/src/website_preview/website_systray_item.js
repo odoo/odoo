21dfb47a30786c07f1bcc17679f81e2e3af4c419
FrancoisGe <fge@odoo.com>
2025-04-09 10:06:24 +0200

    add publish systray item

--- addons/html_builder/static/src/website_preview/website_systray_item.js
+++ addons/html_builder/static/src/website_preview/website_systray_item.js
@@ -1,6 +1,8 @@
 import { NewContentSystrayItem } from "./new_content_systray_item";
 import { EditWebsiteSystrayItem } from "./edit_website_systray_item";
 import { Component, onWillStart } from "@odoo/owl";
+import { PublishSystrayItem } from "./publish_website_systray_item";
+import { useService } from "@web/core/utils/hooks";
 
 export class WebsiteSystrayItem extends Component {
     static template = "html_builder.WebsiteSystrayItem";
@@ -12,12 +14,18 @@ export class WebsiteSystrayItem extends Component {
     static components = {
         NewContentSystrayItem,
         EditWebsiteSystrayItem,
+        PublishSystrayItem,
     };
 
     setup() {
         onWillStart(async () => {
             this.iframeEl = await this.props.iframeLoaded;
         });
+        this.website = useService("website");
+    }
+
+    get canPublish() {
+        return this.website.currentWebsite && this.website.currentWebsite.metadata.canPublish;
     }
 
     get editWebsiteSystrayItemProps() {
