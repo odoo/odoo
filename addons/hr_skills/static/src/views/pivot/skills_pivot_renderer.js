a2f301c658a84079d3ee364ba3619a1f80b156c4
Einar Andersen (eian) <eian@odoo.com>
2025-04-10 14:01:21 +0200

    [IMP] hr_recruitment: skills v2
    This PR introduces new and improved behaviours for managing skills on
    models related to the recruitment application. These changes include:
    - The ability to add expected skills to a job position.
    - A new widget for managing the expected skills on a job position
    - Improved dialog for creating skills both on an applicant and job position.
    - Improved form view for job positions
    - Improved form view for applicants
    - Added a new field, score, on education degrees for better job position
      match calculation
    - A new field to see how well an applicant matches a job position.
    - The ability to browse matching positions from an applicant.
    - A new cron to create activities for employees that are missing skills
      that are expected on a job position.
    - A new report that allows you to explore how well employees match a job
      position in respect to their skills.
    - Other small details like improved demo data, improved views, etc.
    
    task-4680302

---
+++ addons/hr_skills/static/src/views/pivot/skills_pivot_renderer.js
@@ -0,0 +1,5 @@
+import { PivotRenderer } from "@web/views/pivot/pivot_renderer";
+
+export class SkillsPivotRenderer extends PivotRenderer {
+    static template = "hr_skills.SkillsPivotRenderer";
+}
