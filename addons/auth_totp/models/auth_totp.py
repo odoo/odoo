cdb32f49b9c7c92f146e242e2e98af9912048600
thle-odoo <thle@odoo.com>
2025-04-10 09:45:43 +0200

    [IMP] auth_totp: add trusted device age setting
    Currently, when we trust a device, we trust it for 90 days.
    This value is not configurable.
    
    To meet more specific needs, we introduce an `ir.config.parameter`
    named `auth_totp.trusted_device_age` which lets us define the number
    of days a device will be trusted.
    
    task-4143609

--- addons/auth_totp/models/auth_totp.py
+++ addons/auth_totp/models/auth_totp.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import models
+from odoo.addons.auth_totp.controllers.home import TRUSTED_DEVICE_AGE_DAYS
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -20,3 +21,18 @@ class Auth_TotpDevice(models.Model):
         """Return True if device key matches given `scope` for user ID `uid`"""
         assert uid, "uid is required"
         return self._check_credentials(scope=scope, key=key) == uid
+
+    def _get_trusted_device_age(self):
+        ICP = self.env['ir.config_parameter'].sudo()
+        try:
+            nbr_days = int(ICP.get_param('auth_totp.trusted_device_age', TRUSTED_DEVICE_AGE_DAYS))
+            if nbr_days <= 0:
+                nbr_days = None
+        except ValueError:
+            nbr_days = None
+
+        if nbr_days is None:
+            _logger.warning("Invalid value for 'auth_totp.trusted_device_age', using default value.")
+            nbr_days = TRUSTED_DEVICE_AGE_DAYS
+
+        return nbr_days * 86400  # seconds
