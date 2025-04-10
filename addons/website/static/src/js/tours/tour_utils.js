07db5654cbd718d641221fbf5fcca0a0a6ed5b30
aans-odoo <aans@odoo.com>
2025-04-10 13:08:43 +0530

    [FIX] website: prevent insertSnippet from selecting incorrect snippet
    When using the insertSnippet utility in tours, snippets nested inside
    other snippets (e.g. s_text_block inside s_table_of_content) could be
    incorrectly selected, causing the wrong snippet to be inserted.
    
    This commit updates the selector logic in insertSnippet to ignore inner
    elements, ensuring only the top-level snippet is targeted.

--- addons/website/static/src/js/tours/tour_utils.js
+++ addons/website/static/src/js/tours/tour_utils.js
@@ -365,7 +365,7 @@ export function insertSnippet(snippet, position = "bottom") {
             content: markup(_t("Click on the <b>%s</b> building block.", snippet.name)),
             // FIXME `:not(.d-none)` should obviously not be needed but it seems
             // currently needed when using a tour in user/interactive mode.
-            trigger: `:iframe .o_snippet_preview_wrap ${snippetIDSelector}:not(.d-none)`,
+            trigger: `:iframe .o_snippet_preview_wrap > ${snippetIDSelector}:not(.d-none)`,
             noPrepend: true,
             tooltipPosition: "top",
             run: "click",
