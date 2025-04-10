7761cae58a1b7373f61cc3ddaa6eada213470587
vishw <vpjo@odoo.com>
2025-04-10 19:00:02 +0530

    [IMP] base: reflect field groups
    Purpose:
    Display field groups on view_model_fields_form defined in Python with a
    groups attribute (e.g., test_field =
    fields.Char(...,groups='base.group_user')) in the "Access Rights" tab.
    
    Specification:
    Display the group(s) under the Access Rights tab of base.view_model_fields_form.
    To do this, a new method _reflect_field_groups reads the groups attribute
    from base fields, finds the real group from the XML ID,
    and populates the ir_model_fields_group_rel relation.
    
    task-4285741
    
    Part-of: odoo/odoo#208233
    Related: odoo/enterprise#130801
    Signed-off-by: Raphael Collet <rco@odoo.com>

--- odoo/addons/base/tests/test_ir_model.py
+++ odoo/addons/base/tests/test_ir_model.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from psycopg2 import IntegrityError
@@ -999,3 +998,15 @@ class TestIrModelExplanation(TransactionCase):
             self.assertEqual(params['explanation'], expected)
         finally:
             del self.env.registry['mock.model']
+
+
+# at_install to check whether directly after installing base the reflection works
+@tagged('at_install', '-post_install')
+class TestFieldGroupsSync(TransactionCase):
+    def test_reflect_field_groups(self):
+        # Using a known static field with group restrictions (e.g., smtp_user = fields(...,groups='base.group_system'))
+        field = self.env['ir.mail_server']._fields['smtp_user']
+        expected_group = self.env.ref(field.groups)  # just the admin group
+        ir_field = self.env['ir.model.fields']._get('ir.mail_server', 'smtp_user')
+        assert ir_field
+        self.assertEqual(ir_field.groups.get_external_id(), {expected_group.id: field.groups}, "system group not found")
