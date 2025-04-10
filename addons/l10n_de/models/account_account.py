9817c11a9841f9990b3647ebeb862b7af15710ac
Mohamed GadAlrab <myah@odoo.com>
2025-04-10 17:39:50 +0200

    [FIX] account_accountant: allow change account code mapping for non-DE companies
    Description:
    In #172660 we prevent changing the code of accounts used in German companies as part of changes for GoBD compliance.
    This was valid before V18.0 where we introduced cross company accounts where the same account can have different code mappings for different companies.
    The constraint needs to adapt to allow changing the mapping for non-German companies even if the account itself is from a German company.
    Also for non-German accounts, the code mapping of German companies should restricted and only allow it if there is no journal item in the German company on this account.

--- addons/l10n_de/models/account_account.py
+++ addons/l10n_de/models/account_account.py
@@ -8,9 +8,12 @@ class AccountAccount(models.Model):
     def write(self, vals):
         if (
             'code' in vals
-            and 'DE' in self.company_ids.account_fiscal_country_id.mapped('code')
+            and self.env.company.root_id.account_fiscal_country_id.code == 'DE'
             and any(a.code != vals['code'] for a in self)
         ):
-            if self.env['account.move.line'].search_count([('account_id', 'in', self.ids)], limit=1):
-                raise UserError(_("You can not change the code of an account."))
+            if self.env['account.move.line'].search_count(
+                domain=[('account_id', 'in', self.ids), ("company_id", "=", self.env.company.id)],
+                limit=1
+            ):
+                raise UserError(_("You can not change a Garman company code mapping of an account that has some journal items linked to it in accordance with GoBD."))
         return super().write(vals)
