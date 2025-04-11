90be2b7892ad39196b10041e0000754a777cd715
Valentin Chevalier <vcr@odoo.com>
2025-04-11 18:12:02 +0200

    [FIX] * : Allow public access to custom product placeholders
    * : loyalty, website_sale_loyalty, website_event_sale
    
    Placeholders were introduced in c52453f for meta products such as
    discounts, gift cards, event tickets, and appointments.
    
    Previously, access rights prevented public users from viewing these
    placeholders. Even when no image was linked to a record, access was
    denied due to read restrictions, defaulting to the generic placeholder
    instead of the custom one provided by `_get_product_placeholder_filename`.
    
    This commit overrides `_can_return_content` in relevant modules to allow
    proper access for public users, ensuring the intended placeholder is
    displayed.

--- addons/website_event_sale/models/product.py
+++ addons/website_event_sale/models/product.py
@@ -9,6 +9,24 @@ class ProductProduct(models.Model):
 
     event_ticket_ids = fields.One2many('event.event.ticket', 'product_id', string='Event Tickets')
 
+    def _can_return_content(self, field_name=None, access_token=None):
+        """ Override of `orm` to give public users access to the unpublished product image.
+
+        Give access to the public users to the unpublished product images if they are linked to an
+        event ticket.
+
+        :param field_name: The name of the field to check.
+        :param access_token: The access token.
+        :return: Whether to allow the access to the image.
+        :rtype: bool
+        """
+        if (
+            field_name in ["image_%s" % size for size in [1920, 1024, 512, 256, 128]]
+            and self.sudo().event_ticket_ids
+        ):
+            return True
+        return super()._can_return_content(field_name, access_token)
+
     def _get_product_placeholder_filename(self):
         if self.event_ticket_ids:
             return 'website_event_sale/static/img/event_ticket_placeholder_thumbnail.png'
