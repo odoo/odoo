ef13a10fea8a6d35490d82f641cf953182135316
akth-odoo <akth@odoo.com>
2025-04-11 10:17:11 +0530

    [IMP] hr_timesheet:  Clarify 'Project' label in analytic reports
    In analytic reports, when grouping by 'Project' and then adding a custom group,
    searching for 'Project' shows two fields with the same label. One refers to
    the real project, and the other to the analytic account used in timesheets.
    Since both are labeled 'Project', it's unclear which is which, making the report
    difficult to interpret.
    
    Steps to Reproduce:
    1. Activate analytic accounting.
    2. Create a Sales Order (SO) with a service that automatically generates a
       project. → Analytic  Account 1 is created.
    3. Timesheet once on the task → the task has the analytic account of the project
       (AA1).
    4. Change the analytic account on the Sales Order Line (SOL) to Analytic Account
       2.
    5. Timesheet again on the task → the task now uses Analytic Account
       2 for the new timesheet.
    6. Go to Accounting > Reporting > Analytic Reporting.
    7. Group by 'Project', then also group by a custom group and search for
       'Project'.
    
    Two fields labeled 'Project' appear in the report, each representing a different
    value:
    - One refers to the real project (Source Project).
    - One refers to the analytic account used for timesheets.
    
    This commit modifies the label 'Project' used for the real project, renaming it
    to 'Source Project'. The label 'Project' is retained for the analytic account
    linked to timesheets, ensuring a clear distinction between the two. This
    improves the clarity and readability of analytic reports.
    
    Task: 4207122

--- addons/hr_timesheet/models/hr_timesheet.py
+++ addons/hr_timesheet/models/hr_timesheet.py
@@ -63,7 +63,7 @@ class AccountAnalyticLine(models.Model):
         domain="[('allow_timesheets', '=', True), ('project_id', '=?', project_id), ('has_template_ancestor', '=', False)]")
     parent_task_id = fields.Many2one('project.task', related='task_id.parent_id', store=True, index='btree_not_null')
     project_id = fields.Many2one(
-        'project.project', 'Project', domain=_domain_project_id, index=True,
+        'project.project', 'Source Project', domain=_domain_project_id, index=True,
         compute='_compute_project_id', store=True, readonly=False)
     user_id = fields.Many2one(compute='_compute_user_id', store=True, readonly=False)
     employee_id = fields.Many2one('hr.employee', "Employee", domain=_domain_employee_id, context={'active_test': False},
