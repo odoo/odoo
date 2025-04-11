90326865e30012dfb88655ec9a1704861e4c778d
panv-odoo <panv@odoo.com>
2025-04-11 18:10:21 +0530

    [IMP] website: enable and adapt tours
    Enabled following tours:
    - website_gray_color_palette
    
    closes odoo/odoo#211395
    
    Signed-off-by: Géry Debongnie <ged@odoo.com>

--- addons/website/tests/test_ui.py
+++ addons/website/tests/test_ui.py
@@ -531,7 +531,6 @@ class TestUi(HttpCaseWithWebsiteUser):
     def test_21_website_start_cloned_snippet(self):
         self.start_tour('/odoo', 'website_start_cloned_snippet', login='admin')
 
-    @unittest.skip
     def test_22_website_gray_color_palette(self):
         self.start_tour('/odoo', 'website_gray_color_palette', login='admin')
 
