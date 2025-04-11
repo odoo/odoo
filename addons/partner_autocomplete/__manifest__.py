a8b0cedd4c46a2ad12f855e5a0ce1651a1d0d005
Dhwani Patel <dwa@odoo.com>
2025-04-11 07:17:30 +0000

    [FIX] partner_autocomplete: shown proper name field for large input
    Steps to reproduce:
    - create a new contact/company with more than 25 character
    
    Issue:
    - The name field is partially shown as compared to version 16.
    
    Cause:
    - The issue occurs after this change (odoo/odoo@a68cfbb) in version 17.
    the CSS for the partner autocomplete field has been removed as the field
    has 'field_partner_autocomplete' widget.
    
    Solution:
    Reintroduce partner autocomplete CSS fix
    https://github.com/odoo/odoo/commit/b9788544b7101e5a202cf87989bc3f5fc0495249
    
    opw-4686705
    
    closes odoo/odoo#208509
    
    X-original-commit: 40ed84a6a42561df52bfb3a0803060efc5978e41
    Signed-off-by: Louis Baudoux (lba) <lba@odoo.com>
    Signed-off-by: Dhwani Patel (dwa) <dwa@odoo.com>

--- addons/partner_autocomplete/__manifest__.py
+++ addons/partner_autocomplete/__manifest__.py
@@ -21,6 +21,7 @@ Auto-complete partner companies' data
     'auto_install': True,
     'assets': {
         'web.assets_backend': [
+            'partner_autocomplete/static/src/scss/*',
             'partner_autocomplete/static/src/js/*',
             'partner_autocomplete/static/src/xml/*',
         ],
