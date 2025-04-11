bbe2ba3194a00242e1c46c2c775d3a2bcdcd2d1e
tsm-odoo <tsm@odoo.com>
2025-04-11 10:12:55 +0200

    [PERF] im_livechat: improve channel report query performance
    This commit improves the query used in the `im_livechat_report_channel`
    view which is too heavy. Indeed, it joins 2 times on the `mail_message`
    table which is one of the biggest table in Odoo and also uses an `EXISTS`
    query on the same table.
    
    Moreover, there is another join on `im_livechat_channel` which is only
    here to compute `technical_name`. As this task aims to remove it, the
    join has been removed as well.
    
    Before this commit, the query took ~25s for a database with 1M messages,
    65K channels, 130K members (all related to live chat). After this
    commit, the query takes ~1s.
    
    part of task-4614273

--- addons/im_livechat/tests/test_im_livechat_report.py
+++ addons/im_livechat/tests/test_im_livechat_report.py
@@ -1,5 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from freezegun import freeze_time
 from unittest.mock import patch
 
 from odoo.addons.im_livechat.tests.common import TestImLivechatCommon
@@ -15,8 +16,14 @@ class TestImLivechatReport(TestImLivechatCommon):
         def _compute_available_operator_ids(channel_self):
             for record in channel_self:
                 record.available_operator_ids = self.operators
-
-        with patch.object(type(self.env['im_livechat.channel']), '_compute_available_operator_ids', _compute_available_operator_ids):
+        with (
+            patch.object(
+                type(self.env["im_livechat.channel"]),
+                "_compute_available_operator_ids",
+                _compute_available_operator_ids,
+            ),
+            freeze_time("2023-03-17 06:05:54"),
+        ):
             channel_id = self.make_jsonrpc_request(
                 "/im_livechat/get_session",
                 {"anonymous_name": "Anonymous", "channel_id": self.livechat_channel.id},
@@ -25,7 +32,7 @@ class TestImLivechatReport(TestImLivechatCommon):
         channel = self.env['discuss.channel'].browse(channel_id)
         self.operator = channel.livechat_operator_id
 
-        self._create_message(channel, self.visitor_user.partner_id, '2023-03-17 06:05:54')
+        self._create_message(channel, self.visitor_user.partner_id, '2023-03-17 06:06:59')
         self._create_message(channel, self.operator, '2023-03-17 08:15:54')
         self._create_message(channel, self.operator, '2023-03-17 08:45:54')
 
@@ -41,7 +48,8 @@ class TestImLivechatReport(TestImLivechatCommon):
         self.assertEqual(len(report), 1, 'Should have one channel report for this live channel')
         # We have those messages, ordered by creation;
         # 05:05:54: wrong model
-        # 06:05:54: visitor message
+        # 06:05:54: session create
+        # 06:06:59: visitor message
         # 08:15:54: operator first answer
         # 08:45:54: operator second answer
         # 09:15:54: wrong model
