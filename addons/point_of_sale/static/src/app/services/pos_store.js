e35309679a87036cdaafc761d67252a6dd1f091f
dhruv <djip@odoo.com>
2025-04-11 17:56:12 +0530

    [IMP] pos: add manual retry for failed receipts
    Before this commit:
    - There was no option to retry printing kitchen receipts if a printer failed.
    
    After this commit:
    - Users can now manually retry printing kitchen receipts that failed due
      to printer issues.
    - The Retry button targets only the printers that previously failed, ensuring
      other successful prints are not repeated.
    
    Task: 4717776

--- addons/point_of_sale/static/src/app/services/pos_store.js
+++ addons/point_of_sale/static/src/app/services/pos_store.js
@@ -33,7 +33,7 @@ import { CashMovePopup } from "@point_of_sale/app/components/popups/cash_move_po
 import { ClosePosPopup } from "@point_of_sale/app/components/popups/closing_popup/closing_popup";
 import { SelectionPopup } from "../components/popups/selection_popup/selection_popup";
 import { user } from "@web/core/user";
-import { unaccent } from "@web/core/utils/strings";
+import { sprintf, unaccent } from "@web/core/utils/strings";
 import { WithLazyGetterTrap } from "@point_of_sale/lazy_getter";
 import { debounce } from "@web/core/utils/timing";
 import DevicesSynchronisation from "../utils/devices_synchronisation";
@@ -1929,11 +1929,16 @@ export class PosStore extends WithLazyGetterTrap {
         return receiptsData;
     }
 
-    async printChanges(order, orderChange, reprint = false) {
-        let isPrinted = false;
-        const unsuccedPrints = [];
+    async printChanges(
+        order,
+        orderChange,
+        reprint = false,
+        printers = this.unwatched.printers,
+        isPrinted = false
+    ) {
+        const unsuccedPrinters = [];
 
-        for (const printer of this.unwatched.printers) {
+        for (const printer of printers) {
             const { orderData, changes } = this.generateOrderChange(
                 order,
                 orderChange,
@@ -1948,22 +1953,48 @@ export class PosStore extends WithLazyGetterTrap {
             for (const data of receiptsData) {
                 const result = await this.printOrderChanges(data, printer);
                 if (!result.successful) {
-                    unsuccedPrints.push(printer.config.name);
+                    unsuccedPrinters.push(printer);
                 } else {
                     isPrinted = true;
                 }
             }
         }
 
-        // printing errors
-        if (unsuccedPrints.length) {
-            const failedReceipts = unsuccedPrints.join(", ");
-            this.dialog.add(AlertDialog, {
-                title: _t("Printing failed"),
-                body: _t("Failed in printing %s changes of the order", failedReceipts),
-            });
-        }
+        if (unsuccedPrinters.length) {
+            const printerNames = unsuccedPrinters
+                .map(
+                    (printer) =>
+                        `\n\t${printer.config.name} (${
+                            printer.config.printer_type === "iot"
+                                ? printer.config.proxy_ip
+                                : printer.config.epson_printer_ip
+                        })`
+                )
+                .join("");
 
+            const retry = await makeAwaitable(this.dialog, AlertDialog, {
+                title: _t("Printing failed"),
+                body: sprintf(
+                    _t(
+                        "Please note that some printers could not be contacted. The changes will be taken into account locally, but a preparation ticket may be missing. Here is the list of printers with errors: %s\nDo you want to retry ?"
+                    ),
+                    printerNames
+                ),
+                confirmLabel: _t("Retry"),
+                cancelLabel: _t("Discard"),
+                cancel: () => {},
+            });
+
+            if (retry) {
+                return await this.printChanges(
+                    order,
+                    orderChange,
+                    reprint,
+                    unsuccedPrinters,
+                    isPrinted
+                );
+            }
+        }
         return isPrinted;
     }
 
