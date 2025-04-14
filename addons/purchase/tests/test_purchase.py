b03d1daa5c85c9fba69fe523d94dc8ac4235ab2f
Hussein Ebrahim(hyhe) <hyhe@odoo.com>
2025-04-14 13:25:09 +0000

    [FIX] purchase: fixed seller domain on suggest_qty on POL
    Steps to reproduce:
    - Create a product
    - Add two purchase supplier info to that product
    - The first one has date_start < today and date_end < today (or fail on
      the other side)
    - The second one has date_start < today and date_end > today
    - The first supplier info should have min_qty < the second one
    - Add two different vendor product code on the two supplier infos
    - Make sure that the two supplier infos are of the same partner_id
    - Create a purchase order on that product with the partner_id set
    
    The POL has the qty set to the min_qty of the invalid supplierInfo and
    the vendor code of that supplier info is not displayed. And if you
    further remove the invalid supplier info from the product page, the
    vendor code will be read and the POL qty will be the min_qty of the
    valid supplier info.
    
    This is because on creating a POL, two functions are got called, the
    first one is `_suggest_quantity` which suggests the initial quantity to
    set on the POL when the PO is created. This one is based on the minimum
    min_qty of all the supplierInfos related to the same product we are
    purchasing regardless if they are completely valid or not. Hence it
    chose the invalid supplierInfo min_qty.
    https://github.com/odoo/odoo/blob/a05e39c61aaf8eb639a279229b1f803aeb6744c1/addons/purchase/models/purchase.py#L1386C9-L1388C45
    
    After the qty of the POL is set based on the `_suggest_quantity`, the
    `_compute_price_unit_and_date_planned_and_name` is called because it
    depends on the product_qty which calls the `_select_seller` function on
    the product. The `_select_seller` filters all the supplierInfo keeping
    only the valid ones BASED on the qty we are asking (so it eliminated the
    one with the smallest min_qty because the date_end is < PO.date and also
    the second one because the min_qty of the second_supplierInfo is > qty
    we are asking) ending up with empty list of supplierInfo to apply on our
    POL. Then the POL is set to the standard price and no vendor code is
    attached and read into it.
    
    https://github.com/odoo/odoo/blob/a05e39c61aaf8eb639a279229b1f803aeb6744c1/addons/product/models/product_product.py#L616-L617
    https://github.com/odoo/odoo/blob/a05e39c61aaf8eb639a279229b1f803aeb6744c1/addons/product/models/product_product.py#L603-L612
    
    opw-4640937
    
    X-original-commit: 8b654a2119dadf027810b392702104ec313046ac

--- addons/purchase/tests/test_purchase.py
+++ addons/purchase/tests/test_purchase.py
@@ -928,3 +928,36 @@ class TestPurchase(AccountTestInvoicingCommon):
         self.assertEqual(len(matching_records), 2)
         self.assertEqual(matching_records.account_move_id, vendor_bill)
         self.assertEqual(matching_records.purchase_order_id, purchase_order)
+
+    def test_purchase_suggest_qty(self):
+        """
+        Checks the suggested qty of POL is correctly set based on valid supplier-info
+        leading to correctly compute the price unit, product_qty and product_desc
+        """
+        self.env['product.supplierinfo'].create([
+            {
+                'partner_id': self.partner_a.id,
+                'product_id': self.product_a.id,
+                'min_qty': 1,
+                'price': 50,
+                'date_start': fields.Date.today() - timedelta(days=5),
+                'date_end': fields.Date.today() - timedelta(days=3),
+                'product_code': 'product_code_1',
+            },
+            {
+                'partner_id': self.partner_a.id,
+                'product_id': self.product_a.id,
+                'min_qty': 10,
+                'price': 100,
+                'date_start': fields.Date.today() - timedelta(days=5),
+                'date_end': fields.Date.today() + timedelta(days=3),
+                'product_code': 'HHH',
+            },
+        ])
+        po_form = Form(self.env['purchase.order'])
+        po_form.partner_id = self.partner_a
+        with po_form.order_line.new() as po_line:
+            po_line.product_id = self.product_a
+        po = po_form.save()
+        self.assertEqual(po.order_line.product_qty, 10.0)
+        self.assertEqual(po.order_line.name, '[HHH] product_a')
