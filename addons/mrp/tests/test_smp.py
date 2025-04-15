33cf1a6bcb6e949d543d9efea1d886b70654e960
William Henrotin <whe@odoo.com>
2025-04-15 14:05:47 +0000

    [FIX] mrp: set correct quantity when producing serial
    This commit update the finished move quantity of a production of a
    serial tracked product to 1 when set the quantity producing (always to 1
    in case of a serial tracked product) This is mainly mandatory in case
    the production need to process quality checks.
    
    Task : 4575193
    
    X-original-commit: 60ec0ba98a3f73d4720ca68c77ed4c69623ee08e

--- addons/mrp/tests/test_smp.py
+++ addons/mrp/tests/test_smp.py
@@ -31,6 +31,11 @@ class TestMrpSerialMassProduce(TestMrpCommon):
                 'location_id': mo.location_src_id.id,
             })._apply_inventory()
         mo.action_assign()
+        mo_form = Form(mo)
+        mo_form.qty_producing = 1
+        mo = mo_form.save()
+        self.assertEqual(len(mo.move_finished_ids.move_line_ids), 1)
+        mo.qty_producing = 0
         # Open the wizard
         action = mo.action_mass_produce()
         wizard_form = Form(self.env['mrp.batch.produce'].with_context(**action['context']))
@@ -40,9 +45,9 @@ class TestMrpSerialMassProduce(TestMrpCommon):
         wizard = wizard_form.save()
         wizard.action_generate_production_text()
         wizard.action_prepare()
-        # Initial MO should have a backorder-sequenced name and be in confirmed state
+        # Initial MO should have a backorder-sequenced name and be in progress state
         self.assertTrue("-001" in mo.name)
-        self.assertEqual(mo.state, "confirmed")
+        self.assertEqual(mo.state, "progress")
         # Each generated serial number should have its own mo
         self.assertEqual(len(mo.procurement_group_id.mrp_production_ids), count)
         # Check generated serial numbers
