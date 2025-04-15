42490a1632501f87ab58b536212d306b4935c346
Benjamin Vray <bvr@odoo.com>
2025-04-15 08:03:17 +0000

    [FIX] website: image gallery undo after delete
    Steps to reproduce:
    
    - Drag and drop image gallery snippet.
    - Delete the snippet.
    - Click on Undo.
    - The snippet appears but images will not be there.
    
    The issue comes from the fact that when the snippet is deleted, a
    relayout of the snippet is triggered for each image present in it. This
    is necessary when an image is deleted, but not when the snippet itself
    is deleted.
    
    task-4690318
    
    X-original-commit: 2c17bb89846e8ac7da2da4e6d1561d8f6c335a31

--- addons/website/static/src/snippets/s_image_gallery/options.js
+++ addons/website/static/src/snippets/s_image_gallery/options.js
@@ -467,6 +467,13 @@ options.registry.GalleryImageList = options.registry.GalleryLayout.extend({
         this._super(...arguments);
         this.$target.off('.gallery');
     },
+    /**
+     * @override
+     */
+    onRemove() {
+        this.isBeingRemoved = true;
+        this._super(...arguments);
+    },
 
     //--------------------------------------------------------------------------
     // Options
@@ -569,7 +576,7 @@ options.registry.GalleryImageList = options.registry.GalleryLayout.extend({
      */
     notify(name, data) {
         this._super(...arguments);
-        if (name === 'image_removed') {
+        if (name === 'image_removed' && !this.isBeingRemoved) {
             data.$image.remove(); // Force the removal of the image before reset
             this.trigger_up('snippet_edition_request', {exec: () => {
                 return this._relayout();
