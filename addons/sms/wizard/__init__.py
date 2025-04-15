dcb1ae6f07d3e1caf98b8be084f43bac5ac3c567
Thomas Josse (THJO) <thjo@odoo.com>
2025-04-15 15:14:17 +0200

    [REM] (test_mail_)sms: remove sms.resend wizards
    This commit removes the now useless sms.resend wizards as they are no longer
    used.
    
    task-4661147

--- addons/sms/wizard/__init__.py
+++ addons/sms/wizard/__init__.py
@@ -4,6 +4,5 @@ from . import sms_account_code
 from . import sms_account_phone
 from . import sms_account_sender
 from . import sms_composer
-from . import sms_resend
 from . import sms_template_preview
 from . import sms_template_reset
