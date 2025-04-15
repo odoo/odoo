cdb20c766f8cedd53782c1840db1921c230e0f04
Mohammed Shekha <msh@odoo.com>
2025-04-15 06:31:39 +0000

    [IMP] website: enable and adapt conditional visibility tours
    following tours were previously broken due to DOM structure changes
    introduced by the new website builder and were consequently disabled.
    
    - conditional_visibility_1
    - conditional_visibility_2
    - conditional_visibility_3
    - conditional_visibility_4
    - conditional_visibility_5
    
    This commit updates the tour steps to align with the new DOM and
    re-enables the associated test.
    
    closes odoo/odoo#218233
    
    X-original-commit: 287f02ac43f3ea39ccbc7cc9aadc72ba1449822f
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/website/static/tests/tours/conditional_visibility_frontend.js
+++ addons/website/static/tests/tours/conditional_visibility_frontend.js
@@ -4,7 +4,7 @@ registry.category("web_tour.tours").add('conditional_visibility_2', {
     url: '/?utm_medium=Email',
     steps: () => [{
     content: 'The content previously hidden should now be visible',
-    trigger: 'body #wrap',
+    trigger: 'body #wrapwrap',
     run: function (actions) {
         const style = window.getComputedStyle(
             this.anchor.getElementsByClassName("s_text_image")[0]
