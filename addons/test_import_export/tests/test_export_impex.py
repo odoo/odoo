7a50f37d1ea4833e617a47a55df3651f10acaf6f
pish-odoo <pish@odoo.com>
2025-04-15 17:09:16 +0530

    [FIX] core: fix language in export list of selection fields
    Reproduce:
    18.0 and 18.1
    Switch your language to French
    Go to employee and export one record
    You will see not translated value of État civil
    
    Solution:
    Modified the convert_to_export function for selection field
    
    Description of the issue/feature this PR addresses:
    In the selection field selection list is defined by a method then
    instead of exporting the localized label, it exports the raw value
    
    opw-4715430

--- addons/test_import_export/tests/test_export_impex.py
+++ addons/test_import_export/tests/test_export_impex.py
@@ -237,9 +237,9 @@ class test_selection_function(CreatorCase):
 
     def test_value(self):
         # selection functions export the *value* itself
-        self.assertEqual(self.export('1'), [['1']])
-        self.assertEqual(self.export('3'), [['3']])
-        self.assertEqual(self.export('0'), [['0']])
+        self.assertEqual(self.export('1'), [['Grault']])
+        self.assertEqual(self.export('3'), [['Moog']])
+        self.assertEqual(self.export('0'), [['Corge']])
 
 
 class test_m2o(CreatorCase):
