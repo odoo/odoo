1a9d44605cc9730ce8a5a2ddc24a256e50551a61
William Henrotin <whe@odoo.com>
2025-04-15 14:05:47 +0000

    [FIX] mrp: set correct quantity when producing serial
    This commit update the finished move quantity of a production of a
    serial tracked product to 1 when set the quantity producing (always to 1
    in case of a serial tracked product) This is mainly mandatory in case
    the production need to process quality checks.
    
    Task : 4575193
    
    closes odoo/odoo#221469
    
    X-original-commit: 2d85d122ee049c050f337716cb7ab47ccb828c10
    Related: odoo/enterprise#91474
    Signed-off-by: Quentin Wolfs (quwo) <quwo@odoo.com>
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -1273,14 +1273,13 @@ class MrpProduction(models.Model):
         # waiting for a preproduction move before assignement
         is_waiting = self.warehouse_id.manufacture_steps != 'mrp_one_step' and self.picking_ids.filtered(lambda p: p.picking_type_id == self.warehouse_id.pbm_type_id and p.state not in ('done', 'cancel'))
 
-        for move in (self.move_raw_ids.filtered(lambda m: not is_waiting or m.product_id.tracking == 'none') | self.move_byproduct_ids):
+        for move in (
+            self.move_raw_ids.filtered(lambda m: not is_waiting or m.product_id.tracking == 'none')
+            | self.move_finished_ids.filtered(lambda m: m.product_id != self.product_id or m.product_id.tracking == 'serial')
+        ):
             is_byproduct = move in self.move_byproduct_ids
             # Never update already produced by-product moves.
-            if move.picked and is_byproduct:
-                continue
-
-            # picked + manual means the user set the quantity manually
-            if move.manual_consumption and move.picked:
+            if move.picked and (is_byproduct or move.manual_consumption):
                 continue
 
             # sudo needed for portal users
@@ -1289,7 +1288,10 @@ class MrpProduction(models.Model):
 
             new_qty = move.product_uom.round((self.qty_producing - self.qty_produced) * move.unit_factor)
             move._set_quantity_done(new_qty)
-            if (not move.manual_consumption or pick_manual_consumption_moves) and move.quantity and not is_byproduct:
+            if (not move.manual_consumption or pick_manual_consumption_moves) \
+                    and move.quantity \
+                    and not is_byproduct \
+                    and (move.raw_material_production_id or move.product_id.tracking != 'serial'):
                 move.picked = True
 
     def _should_postpone_date_finished(self, date_finished):
