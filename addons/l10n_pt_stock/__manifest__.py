5a03fa73cf6f14c2f2ef079bfa265701880618d9
Maira Salazar (srma) <srma@odoo.com>
2025-04-15 16:22:31 +0200

    [IMP] stock, l10n_pt_stock: PT certification
    This commit implements the following requirements for Portugal:
    
    Hash
    ----
    Outgoing deliveries should be hashed. For this, the following fields are used:
    `date`, `l10n_pt_hashed_on`, `name` as well as the hash
    of the previous `stock.picking`. These are sent to IAP, where we'll use
    a private key (belonging to Odoo) to sign the documents.
    To avoid too many RPC calls to IAP, the documents are signed only when
    printed. At that point, pickings are batched such that we have only once
    RPC call to IAP.
    
    ATCUD
    -----
    The ATCUD code is a unique code that represents a unique document in
    Portugal. It's the simple concatenation of the AT series and the
    sequence number within that series. It must be displayed on the
    document whenever it is signed.
    
    Also, a hash report has been added. It uses the public key returned by
    IAP to verify the chain of hashes of the stock pickings.
    
    QR Code
    -------
    A QR code must also be displayed on any signed document. This QR code
    contains several info such as the customer VAT (if any), the total
    amount of the document, or the tax totals.
    
    Layout requirements
    -------------------
    
    - Documents that are not invoices must indicate
    "Este documento não serve de fatura"
    
    - Signed documents must indicate
    "E.g.: HASH - Processado por programa certificado n.º 0000/AT"
    where HASH is the 1st, 11th, 21st, 31st chars of the hash/signature.
    
    - Non-signed documents that are invoices must indicate
    "Emitido por programa certificado n.º 0000/AT"
    
    Training mode
    -------------
    A boolean field is present on the `res.company` model. If it's activated
    documents should have the mention "Documento emitido para fins de
    Formação". And indicate in the header the data identifying the software
    developer company (Odoo) instead of the customer’s company.
    
    Links
    -----
    - https://info.portaldasfinancas.gov.pt/pt/docs/Portug_tax_system/Documents/Order_No_8632_2014_of_the_3rd_July.pdf
    - https://info.portaldasfinancas.gov.pt/pt/docs/Portug_tax_system/Documents/Ordinance_363_2010_Certification_of_computer_invoicing_programs.pdf
    - https://info.portaldasfinancas.gov.pt/pt/docs/Conteudos_1pagina/Documents/QR_Code_Technical_Specifications.pdf
    
    task-id 2794952

--- addons/l10n_pt_stock/__manifest__.py
+++ addons/l10n_pt_stock/__manifest__.py
@@ -13,15 +13,12 @@
         'l10n_pt',
     ],
     'data': [
-        'data/data.xml',
         'data/ir_cron.xml',
         'views/stock_picking_views.xml',
         'views/stock_picking_type_views.xml',
+        'views/res_config_settings_views.xml',
         'report/l10n_pt_stock_hash_integrity_templates.xml',
         'report/report_deliveryslip.xml',
-    ],
-    'demo': [
-        'demo/demo_data.xml',
     ],
     'installable': True,
     'license': 'LGPL-3',
