850f6380347baade276203fd654adfaae2bb24f8
Ayanfe <Ayanfiscoss@gmail.com>
2025-04-15 09:51:04 +0100

    New Commit

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from odoo import models, fields
 
 class EstateProperty(models.Model):
@@ -7,10 +8,10 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=lambda self: Date.today() + timedelta(days = 90))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
