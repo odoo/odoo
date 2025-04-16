c637426f676c8fd142196fc8610995ebe9ef82b1
Nicolas Lempereur <nle@odoo.com>
2025-04-16 17:41:32 +0200

    [FIX] tests: watch=True working for chrome 135+
    Scenario: run a tour with watch=True on a test with chrome at least 135
    
    Result: the tour runs, but is not shown in the browser
    
    Issue: since chromium/chromium@17ee927a3b7f8641ec1f44e1c6190dcca3acf416
    the full appspot URL is priviledged for devtool URL instead of the
    local browser bundled ones. So we get an url like
    https://chrome-devtools-frontend.appspot.com/serve_rev/…/inspector.html?…
    instead of /devtools/inspector.html?… and we get a wrong URL when doing
    the concatenation with localhost and port.
    
    Fix: use the full appspot URL to get the query string for local dev
    tools.
    
    closes odoo/odoo#206431
    
    Note: issue not present in 17.0 since watch=True uses headful browser
    Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>

--- odoo/tests/common.py
+++ odoo/tests/common.py
@@ -1965,7 +1965,8 @@ class HttpCase(TransactionCase):
         if watch and self.browser.dev_tools_frontend_url:
             _logger.warning('watch mode is only suitable for local testing - increasing tour timeout to 3600')
             timeout = max(timeout*10, 3600)
-            debug_front_end = f'http://127.0.0.1:{self.browser.devtools_port}{self.browser.dev_tools_frontend_url}'
+            devtool_query_string = self.browser.dev_tools_frontend_url.partition('/inspector.html')[2]
+            debug_front_end = f'http://127.0.0.1:{self.browser.devtools_port}/devtools/inspector.html{devtool_query_string}'
             self.browser._chrome_without_limit([self.browser.executable, debug_front_end])
             time.sleep(3)
         try:
