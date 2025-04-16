93f4fd3137ea99663d44bb52ec3409d8bf318771
Julien Banken <jbn@odoo.com>
2025-04-16 15:16:49 +0200

    [IMP] web: improve property fields UI
    This commit enhances the property fields UI by:
    1. Displaying the section underline only when the section is expanded.
    2. Moving the caret indicator to the left of the section, making it
       easier for users to see whether a section is folded.
    3. Updating the property field label colors to align with the standard ones.
    4. Updating the confirmation button label for property deletion.
    
    Task-4610816
    
    Part-of: odoo/odoo#203938
    Related: odoo/enterprise#82574
    Signed-off-by: Damien Abeloos (abd) <abd@odoo.com>

--- addons/web/static/src/views/fields/properties/properties_field.js
+++ addons/web/static/src/views/fields/properties/properties_field.js
@@ -651,7 +651,7 @@ export class PropertiesField extends Component {
                     parentFieldLabel: this.props.record.fields[this.definitionRecordField].string,
                 }
             ),
-            confirmLabel: _t("Delete"),
+            confirmLabel: _t("Delete Field"),
             confirm: () => {
                 const propertiesDefinitions = this.propertiesList;
                 propertiesDefinitions.find(
