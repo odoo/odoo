70ea4c2ed5a3a92ca2432778a7d3e28b2b760e00
Louis (loco) <loco@odoo.com>
2025-04-16 15:46:42 +0200

    Introduce LoadImgComponent

--- addons/html_builder/static/src/core/utils/update_on_img_changed.js
+++ addons/html_builder/static/src/core/utils/update_on_img_changed.js
@@ -1,18 +1,23 @@
 import { BuilderComponent } from "../building_blocks/builder_component";
 import { useDomState } from "../utils";
+import { LoadImgComponent } from "./load_img_component";
 
 export class UpdateOptionOnImgChanged extends BuilderComponent {
     static template = "html_builder.UpdateOptionOnImgChanged";
-    static props = {};
+    static props = { slots: { type: Object } };
+    static components = { LoadImgComponent };
 
     setup() {
         super.setup();
         let boolean = true;
         this.state = useDomState((editingElement) => {
-            const imageEl = editingElement.querySelector("img");
-            if (imageEl && !imageEl.complete) {
-                // Rerender the slot if the image is not loaded
-                boolean = !boolean;
+            const imageEls = editingElement.querySelectorAll("img");
+            for (const imageEl of imageEls) {
+                if (!imageEl.complete) {
+                    // Rerender the slot if an image is not loaded
+                    boolean = !boolean;
+                    break;
+                }
             }
             return {
                 bool: boolean,
