0e1d8e9c734e5886b931e33631a37a0c38c8a3f5
Aayush Jain <aayushjain1475@gmail.com>
2025-04-16 15:59:32 +0530

    Created a simple custom module for database table creation

---
+++ my_custom_odoo_folder/my_custom_table_module/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# This line imports the python file containing your model definition
+from . import my_simple_model
