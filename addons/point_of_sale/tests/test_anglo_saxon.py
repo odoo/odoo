e8d59c66615778338256e809f0eb26c0529e4724
parp-odoo <parp@odoo.com>
2025-04-17 12:30:32 +0530

    [IMP] pos: extract `_prepare_paid_pos_order` helper from Anglo-Saxon flow tests

--- addons/point_of_sale/tests/test_anglo_saxon.py
+++ addons/point_of_sale/tests/test_anglo_saxon.py
@@ -64,76 +64,8 @@ class TestAngloSaxonCommon(AccountTestInvoicingCommon):
         })
         cls.pos_config.write({'payment_method_ids': [(6, 0, cls.cash_payment_method.ids)]})
 
-@tagged('post_install', '-at_install')
-class TestAngloSaxonFlow(TestAngloSaxonCommon):
-
-    def test_create_account_move_line(self):
-        # This test will check that the correct journal entries are created when a product in real time valuation
-        # is sold in a company using anglo-saxon
-        self.pos_config.open_ui()
-        current_session = self.pos_config.current_session_id
-        self.cash_journal.loss_account_id = self.account
-        current_session.set_opening_control(0, None)
-
-        # I create a PoS order with 1 unit of New product at 450 EUR
-        self.pos_order_pos0 = self.PosOrder.create({
-            'company_id': self.company.id,
-            'partner_id': self.partner.id,
-            'pricelist_id': self.company.partner_id.property_product_pricelist.id,
-            'session_id': self.pos_config.current_session_id.id,
-            'lines': [(0, 0, {
-                'name': "OL/0001",
-                'product_id': self.product.id,
-                'price_unit': 450,
-                'discount': 0.0,
-                'qty': 1.0,
-                'price_subtotal': 450,
-                'price_subtotal_incl': 450,
-            })],
-            'amount_total': 450,
-            'amount_tax': 0,
-            'amount_paid': 0,
-            'amount_return': 0,
-            'last_order_preparation_change': '{}'
-        })
-
-        # I make a payment to fully pay the order
-        context_make_payment = {"active_ids": [self.pos_order_pos0.id], "active_id": self.pos_order_pos0.id}
-        self.pos_make_payment_0 = self.PosMakePayment.with_context(context_make_payment).create({
-            'amount': 450.0,
-            'payment_method_id': self.cash_payment_method.id,
-        })
-
-        # I click on the validate button to register the payment.
-        context_payment = {'active_id': self.pos_order_pos0.id}
-        self.pos_make_payment_0.with_context(context_payment).check()
-
-        # I check that the order is marked as paid
-        self.assertEqual(self.pos_order_pos0.state, 'paid', 'Order should be in paid state.')
-        self.assertEqual(self.pos_order_pos0.amount_paid, 450, 'Amount paid for the order should be updated.')
-
-        # I close the current session to generate the journal entries
-        current_session_id = self.pos_config.current_session_id
-        current_session_id.post_closing_cash_details(450.0)
-        current_session_id.close_session_from_ui()
-        self.assertEqual(current_session_id.state, 'closed', 'Check that session is closed')
-
-        # Check if there is account_move in the order.
-        # There shouldn't be because the order is not invoiced.
-        self.assertFalse(self.pos_order_pos0.account_move, 'There should be no invoice in the order.')
-
-        # I test that the generated journal entries are correct.
-        account_output = self.category.property_stock_account_output_categ_id
-        expense_account = self.category.property_account_expense_categ_id
-        aml = current_session.move_id.line_ids
-        aml_output = aml.filtered(lambda l: l.account_id.id == account_output.id)
-        aml_expense = aml.filtered(lambda l: l.account_id.id == expense_account.id)
-        self.assertEqual(aml_output.credit, self.product.standard_price, "Cost of Good Sold entry missing or mismatching")
-        self.assertEqual(aml_expense.debit, self.product.standard_price, "Cost of Good Sold entry missing or mismatching")
-
-    def _prepare_pos_order(self):
+    def _setup_fifo_consting_method(self):
         """ Set the cost method of `self.product` as FIFO. Receive 5@5 and 5@1 and
-        create a `pos.order` record selling 7 units @ 450.
         """
         # check fifo Costing Method of product.category
         self.product.categ_id.property_cost_method = 'fifo'
@@ -152,47 +84,89 @@ class TestAngloSaxonFlow(TestAngloSaxonCommon):
         self.assertEqual(self.product.value_svl, 30, "Value should be (5*5 + 5*1) = 30")
         self.assertEqual(self.product.quantity_svl, 10)
 
+    def _prepare_paid_pos_order(self, qty=1, **kwargs):
+        """ Initiates a new POS session, creates a `pos.order` sells the specified
+        quantity of `self.product` @450 per unit, and processes the payment using the `cash_payment_method`.
 
+        :param qty: int - The number of product units to sell.
+        :return: pos.order - The newly created POS order record.
+        """
         self.pos_config.open_ui()
         pos_session = self.pos_config.current_session_id
         pos_session.set_opening_control(0, None)
 
+        order_total = qty * 450
         pos_order_values = {
             'company_id': self.company.id,
             'partner_id': self.partner.id,
             'pricelist_id': self.company.partner_id.property_product_pricelist.id,
             'session_id': self.pos_config.current_session_id.id,
+            'shipping_date': kwargs.get('shipping_date', False),
             'lines': [(0, 0, {
                 'name': "OL/0001",
                 'product_id': self.product.id,
                 'price_unit': 450,
                 'discount': 0.0,
-                'qty': 7.0,
-                'price_subtotal': 7 * 450,
-                'price_subtotal_incl': 7 * 450,
+                'qty': qty,
+                'price_subtotal': order_total,
+                'price_subtotal_incl': order_total,
             })],
-            'amount_total': 7 * 450,
+            'amount_total': order_total,
             'amount_tax': 0,
             'amount_paid': 0,
             'amount_return': 0,
             'last_order_preparation_change': '{}'
         }
 
-        return self.PosOrder.create(pos_order_values)
+        pos_order_pos0 = self.PosOrder.create(pos_order_values)
+
+        # make a payment to fully pay the order with cash payment method
+        context_make_payment = {"active_ids": [pos_order_pos0.id], "active_id": pos_order_pos0.id}
+        self.pos_make_payment_0 = self.PosMakePayment.with_context(context_make_payment).create({
+            'amount': order_total,
+            'payment_method_id': self.cash_payment_method.id,
+        })
+        # register the payment.
+        context_payment = {'active_id': pos_order_pos0.id}
+        self.pos_make_payment_0.with_context(context_payment).check()
+
+        return pos_order_pos0
+
+
+@tagged('post_install', '-at_install')
+class TestAngloSaxonFlow(TestAngloSaxonCommon):
+
+    def test_create_account_move_line(self):
+        pos_order_pos0 = self._prepare_paid_pos_order()
+
+        # I check that the order is marked as paid
+        self.assertEqual(pos_order_pos0.state, 'paid', 'Order should be in paid state.')
+        self.assertEqual(pos_order_pos0.amount_paid, 450, 'Amount paid for the order should be updated.')
+
+        # I close the current session to generate the journal entries
+        current_session_id = self.pos_config.current_session_id
+        current_session_id.post_closing_cash_details(450.0)
+        current_session_id.close_session_from_ui()
+        self.assertEqual(current_session_id.state, 'closed', 'Check that session is closed')
+
+        # Check if there is account_move in the order.
+        # There shouldn't be because the order is not invoiced.
+        self.assertFalse(pos_order_pos0.account_move, 'There should be no invoice in the order.')
+
+        # I test that the generated journal entries are correct.
+        account_output = self.category.property_stock_account_output_categ_id
+        expense_account = self.category.property_account_expense_categ_id
+        aml = current_session_id.move_id.line_ids
+        aml_output = aml.filtered(lambda l: l.account_id.id == account_output.id)
+        aml_expense = aml.filtered(lambda l: l.account_id.id == expense_account.id)
+        self.assertEqual(aml_output.credit, self.product.standard_price, "Cost of Good Sold entry missing or mismatching")
+        self.assertEqual(aml_expense.debit, self.product.standard_price, "Cost of Good Sold entry missing or mismatching")
 
     def test_fifo_valuation_no_invoice(self):
         """Register a payment and validate a session after selling a fifo
         product without making an invoice for the customer"""
-        pos_order_pos0 = self._prepare_pos_order()
-        context_make_payment = {"active_ids": [pos_order_pos0.id], "active_id": pos_order_pos0.id}
-        self.pos_make_payment_0 = self.PosMakePayment.with_context(context_make_payment).create({
-            'amount': 7 * 450.0,
-            'payment_method_id': self.cash_payment_method.id,
-        })
-
-        # register the payment
-        context_payment = {'active_id': pos_order_pos0.id}
-        self.pos_make_payment_0.with_context(context_payment).check()
+        self._setup_fifo_consting_method()
+        pos_order_pos0 = self._prepare_paid_pos_order(qty=7)
 
         # validate the session
         current_session_id = self.pos_config.current_session_id
@@ -210,16 +184,8 @@ class TestAngloSaxonFlow(TestAngloSaxonCommon):
     def test_fifo_valuation_with_invoice(self):
         """Register a payment and validate a session after selling a fifo
         product and make an invoice for the customer"""
-        pos_order_pos0 = self._prepare_pos_order()
-        context_make_payment = {"active_ids": [pos_order_pos0.id], "active_id": pos_order_pos0.id}
-        self.pos_make_payment_0 = self.PosMakePayment.with_context(context_make_payment).create({
-            'amount': 7 * 450.0,
-            'payment_method_id': self.cash_payment_method.id,
-        })
-
-        # register the payment
-        context_payment = {'active_id': pos_order_pos0.id}
-        self.pos_make_payment_0.with_context(context_payment).check()
+        self._setup_fifo_consting_method()
+        pos_order_pos0 = self._prepare_paid_pos_order(qty=7)
 
         # Create the customer invoice
         pos_order_pos0.action_pos_order_invoice()
@@ -232,54 +198,18 @@ class TestAngloSaxonFlow(TestAngloSaxonCommon):
     def test_cogs_with_ship_later_no_invoicing(self):
         # This test will check that the correct journal entries are created when a product in real time valuation
         # is sold using the ship later option and no invoice is created in a company using anglo-saxon
-        self.pos_config.open_ui()
-        current_session = self.pos_config.current_session_id
-        self.cash_journal.loss_account_id = self.account
-        current_session.set_opening_control(0, None)
 
+        self.cash_journal.loss_account_id = self.account
         # 2 step delivery method
         self.warehouse.delivery_steps = 'pick_ship'
 
-        # I create a PoS order with 1 unit of New product at 450 EUR
-        self.pos_order_pos0 = self.PosOrder.create({
-            'company_id': self.company.id,
-            'partner_id': self.partner.id,
-            'pricelist_id': self.company.partner_id.property_product_pricelist.id,
-            'session_id': self.pos_config.current_session_id.id,
-            'to_invoice': False,
-            'shipping_date': '2023-01-01',
-            'lines': [(0, 0, {
-                'name': "OL/0001",
-                'product_id': self.product.id,
-                'price_unit': 450,
-                'discount': 0.0,
-                'qty': 1.0,
-                'price_subtotal': 450,
-                'price_subtotal_incl': 450,
-            })],
-            'amount_total': 450,
-            'amount_tax': 0,
-            'amount_paid': 0,
-            'amount_return': 0,
-            'last_order_preparation_change': '{}'
-        })
-
-        # I make a payment to fully pay the order
-        context_make_payment = {"active_ids": [self.pos_order_pos0.id], "active_id": self.pos_order_pos0.id}
-        self.pos_make_payment_0 = self.PosMakePayment.with_context(context_make_payment).create({
-            'amount': 450.0,
-            'payment_method_id': self.cash_payment_method.id,
-        })
-
-        # I click on the validate button to register the payment.
-        context_payment = {'active_id': self.pos_order_pos0.id}
-        self.pos_make_payment_0.with_context(context_payment).check()
+        self._prepare_paid_pos_order(shipping_date='2023-01-01')
 
         # I close the current session to generate the journal entries
-        current_session_id = self.pos_config.current_session_id
-        current_session_id.post_closing_cash_details(450.0)
-        current_session_id.close_session_from_ui()
-        self.assertEqual(current_session_id.state, 'closed', 'Check that session is closed')
+        current_session = self.pos_config.current_session_id
+        current_session.post_closing_cash_details(450.0)
+        current_session.close_session_from_ui()
+        self.assertEqual(current_session.state, 'closed', 'Check that session is closed')
 
         self.assertEqual(len(current_session.picking_ids), 1, "There should be 2 pickings")
         current_session.picking_ids.move_ids_without_package.write({'quantity': 1, 'picked': True})
@@ -312,36 +242,13 @@ class TestAngloSaxonFlow(TestAngloSaxonCommon):
         self.company.point_of_sale_update_stock_quantities = 'closing'
 
         # Setup a running session, with a paid pos order that is not invoiced
-        self.pos_config.open_ui()
-        current_session = self.pos_config.current_session_id
-        self.pos_order_pos0 = self.PosOrder.create({
-            'company_id': self.company.id,
-            'partner_id': self.partner.id,
-            'session_id': self.pos_config.current_session_id.id,
-            'lines': [(0, 0, {
-                'product_id': self.product.id,
-                'price_unit': 450,
-                'qty': 1.0,
-                'price_subtotal': 450,
-                'price_subtotal_incl': 450,
-            })],
-            'amount_total': 450,
-            'amount_tax': 0,
-            'amount_paid': 0,
-            'amount_return': 0,
-        })
-        context_make_payment = {"active_ids": [self.pos_order_pos0.id], "active_id": self.pos_order_pos0.id}
-        self.pos_make_payment_0 = self.PosMakePayment.with_context(context_make_payment).create({
-            'amount': 450.0,
-            'payment_method_id': self.cash_payment_method.id,
-        })
-        context_payment = {'active_id': self.pos_order_pos0.id}
-        self.pos_make_payment_0.with_context(context_payment).check()
+        pos_order_pos0 = self._prepare_paid_pos_order()
 
         # Invoice the pos order afterward (session still running)
-        self.pos_order_pos0.action_pos_order_invoice()
+        pos_order_pos0.action_pos_order_invoice()
 
         # Check that the stock output journal item from the invoice is reconciled (with its counterpart from the valuation entry)
+        current_session = self.pos_config.current_session_id
         stock_output_account = self.category.property_stock_account_output_categ_id
         related_amls = current_session._get_related_account_moves().line_ids
         stock_output_amls = related_amls.filtered_domain([('account_id', '=', stock_output_account.id)])
