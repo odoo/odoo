537814ccb7cd777f59ad953508825f391b509a64
Xavier Morel <xmo@odoo.com>
2025-04-17 08:35:21 +0200

    [IMP] core: remove special cases in monkeypatches
    - `win32` is just patches to `locale`, so patch that
    - associate the unbound stuff to patching `site`, as that's normally
      imported before the user code is even invoked, and is associated with
      python system customisations
    
    closes odoo/odoo#206018
    
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- odoo/_monkeypatches/site.py
+++ odoo/_monkeypatches/site.py
@@ -1,16 +1,30 @@
-"""
-Running mode flags (gevent, prefork)
+"""Patcher for any change not strictly related to an stdlib module
 
-This should be imported as early as possible.
-It will initialize the `odoo.evented` variable.
 """
-import odoo
+
+import codecs
+import encodings.aliases
+import re
 import sys
+
+import babel.core
+
+import odoo
+
+
+def patch_module():
+    patch_evented()
+    patch_codecs()
+
 
 odoo.evented = False
 
 
-def patch_module():
+def patch_evented():
+    """Running mode flags (gevent, prefork)
+
+    This should be executed early. It will initialize the `odoo.evented` variable.
+    """
     if odoo.evented or not (len(sys.argv) > 1 and sys.argv[1] == 'gevent'):
         return
     sys.argv.remove('gevent')
@@ -35,5 +49,27 @@ def patch_module():
             else:
                 raise psycopg2.OperationalError(
                     "Bad result from poll: %r" % state)
+
     psycopg2.extensions.set_wait_callback(gevent_wait_callback)
     odoo.evented = True
+
+
+def patch_codecs():
+    # ---------------------------------------------------------
+    # some charset are known by Python under a different name
+    # ---------------------------------------------------------
+
+    encodings.aliases.aliases['874'] = 'cp874'
+    encodings.aliases.aliases['windows_874'] = 'cp874'
+
+    # ---------------------------------------------------------
+    # alias hebrew iso-8859-8-i and iso-8859-8-e on iso-8859-8
+    # https://bugs.python.org/issue18624
+    # ---------------------------------------------------------
+
+    iso8859_8 = codecs.lookup('iso8859_8')
+    iso8859_8ie_re = re.compile(r'iso[-_]?8859[-_]8[-_]?[ei]', re.IGNORECASE)
+    codecs.register(lambda charset: iso8859_8 if iso8859_8ie_re.match(charset) else None)
+
+    # To remove when corrected in Babel
+    babel.core.LOCALE_ALIASES['nb'] = 'nb_NO'
