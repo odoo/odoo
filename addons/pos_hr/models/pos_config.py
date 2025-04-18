41c841de601f755f21646116dfc776c0d6c81a29
Adrien Guilliams (adgu) <adgu@odoo.com>
2025-04-18 12:43:55 +0000

    [FIX] pos_hr: admin not set
    Before this commit, when opening a pos without admin employees set,
    the user was blocked into its open session without being able to close
    it.
    
    closes odoo/odoo#209981
    
    Task-id: 4730841
    X-original-commit: d2ed27865322d536d2ebb1289e2d465cb59610af
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/pos_hr/models/pos_config.py
+++ addons/pos_hr/models/pos_config.py
@@ -17,20 +17,30 @@ class PosConfig(models.Model):
         'hr.employee', 'pos_hr_advanced_employee_hr_employee', string="Employees with manager access",
         help='If left empty, only Odoo users have extended rights in PoS')
 
+    def write(self, vals):
+        if 'advanced_employee_ids' not in vals:
+            vals['advanced_employee_ids'] = []
+        vals['advanced_employee_ids'] += [(4, emp_id) for emp_id in self._get_group_pos_manager().user_ids.employee_id.ids]
+        return super().write(vals)
+
     @api.onchange('minimal_employee_ids')
     def _onchange_minimal_employee_ids(self):
         for employee in self.minimal_employee_ids:
-            if employee in self.basic_employee_ids:
+            if employee.user_id._has_group('point_of_sale.group_pos_manager'):
+                self.minimal_employee_ids -= employee
+            elif employee in self.basic_employee_ids:
                 self.basic_employee_ids -= employee
-            if employee in self.advanced_employee_ids:
+            elif employee in self.advanced_employee_ids:
                 self.advanced_employee_ids -= employee
 
     @api.onchange('basic_employee_ids')
     def _onchange_basic_employee_ids(self):
         for employee in self.basic_employee_ids:
-            if employee in self.advanced_employee_ids:
+            if employee.user_id._has_group('point_of_sale.group_pos_manager'):
+                self.basic_employee_ids -= employee
+            elif employee in self.advanced_employee_ids:
                 self.advanced_employee_ids -= employee
-            if employee in self.minimal_employee_ids:
+            elif employee in self.minimal_employee_ids:
                 self.minimal_employee_ids -= employee
 
     @api.onchange('advanced_employee_ids')
