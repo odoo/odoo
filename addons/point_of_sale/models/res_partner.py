9db4077660b0a636afd6d1c8fb91573c071e11a7
parp-odoo <parp@odoo.com>
2025-04-18 15:47:04 +0530

    [IMP] point_of_sale: enforce address requirement for preset orders
    Before this commit:
    - A customer without an address could be selected when ordering presets
      that require an address
    
    After this commit:
    - A warning notification is displayed if a customer with an empty address
      is selected for the address-required preset.
    - The customer edit form automatically opens to prompt user to add an address.
    
    closes odoo/odoo#203056
    
    Task: 4664774
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/point_of_sale/models/res_partner.py
+++ addons/point_of_sale/models/res_partner.py
@@ -51,7 +51,7 @@ class ResPartner(models.Model):
     @api.model
     def _load_pos_data_fields(self, config_id):
         return [
-            'id', 'name', 'street', 'city', 'state_id', 'country_id', 'vat', 'lang', 'phone', 'zip', 'email',
+            'id', 'name', 'street', 'street2', 'city', 'state_id', 'country_id', 'vat', 'lang', 'phone', 'zip', 'email',
             'barcode', 'write_date', 'property_account_position_id', 'property_product_pricelist', 'parent_name',
             'pos_contact_address', 'invoice_emails', 'company_type'
         ]
