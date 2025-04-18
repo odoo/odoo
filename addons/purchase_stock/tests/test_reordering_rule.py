15fda5a8841978649102950892eb729474705c7e
Omar (OSAH) <osah@odoo.com>
2025-04-18 15:01:50 +0000

    [FIX] purchase_stock: Add missing group permission for product_uom_id
    The `product_uom_id` field was causing test failures in `test_reordering_rule.py`
    because it was not present in the `product.supplierinfo` form view .
    
    This commit adds the group permission `uom.group_uom` to the user
    who is running the tests, ensuring that the `product_uom_id` field is available
    and preventing the test failure.
    
    build_error-111983
    
    closes odoo/odoo#220243
    
    X-original-commit: fd2310bf1b393d106707a20b37d496c8e80208e2
    Signed-off-by: Steve Van Essche <svs@odoo.com>
    Signed-off-by: Omar Sherif Ali Hassan (osah) <osah@odoo.com>

--- addons/purchase_stock/tests/test_reordering_rule.py
+++ addons/purchase_stock/tests/test_reordering_rule.py
@@ -20,6 +20,7 @@ class TestReorderingRule(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super(TestReorderingRule, cls).setUpClass()
+        cls.env.user.group_ids += cls.env.ref('uom.group_uom')
         cls.partner = cls.env['res.partner'].create({
             'name': 'Smith'
         })
