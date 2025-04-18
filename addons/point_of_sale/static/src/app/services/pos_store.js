8a5b908bb89678cf4121ccbeb973982f5a0e487a
Pedram (PEBR) <pebr@odoo.com>
2025-04-18 13:02:10 +0000

    [FIX] pos_hr: allow price control when not explicitly restricted
    Before this commit, non-manager cashiers were unable to modify product
    prices even when price control was not enabled, which was not the
    intended behavior.
    
    Additionally, when `pos_hr` was not installed, the `get_cashier` method
    returned the user object with a `role` attribute. However, when
    `pos_hr` was installed, the returned cashier used `_role`, leading to
    inconsistencies and broken functionality depending on the module's
    presence.
    
    opw-4726098
    
    closes odoo/odoo#213171
    
    X-original-commit: 7419a98530bdf633d696e80fdd24c0876f9ef121
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/point_of_sale/static/src/app/services/pos_store.js
+++ addons/point_of_sale/static/src/app/services/pos_store.js
@@ -1121,7 +1121,7 @@ export class PosStore extends WithLazyGetterTrap {
      */
     getCashier() {
         if (!this.user.role) {
-            this.user.role = this.user.raw.role;
+            this.user._role = this.user.raw.role;
         }
         return this.user;
     }
