cea90871f2aa86130a38e099c8a2fdea8ce63db2
Maryam Kia <maki@odoo.com>
2025-04-18 11:11:20 +0200

    [FIX] mail, *: enable shared methods overrides through inheritance
    *: im_livechat, portal, test_mail, test_mail_full
    
    Steps to reproduce:
    - Install a module with portal mixin
    - From a portal document (not logged in) try to delete an attachment of a
    message
    - The override of `_can_edit_message` is never called and the attachment is not
    deleted
    
    With this commit, any controller that needs the shared method of
    `ThreadController` controller inherits that control. Direct calls to imported
    controller methods could bypass overrides.
    
    closes odoo/odoo#206619
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/tests/discuss/test_discuss_attachment_controller.py
+++ addons/mail/tests/discuss/test_discuss_attachment_controller.py
@@ -1,4 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+from itertools import product
 
 import odoo
 from odoo.addons.mail.tests.common_controllers import MailControllerAttachmentCommon
@@ -13,7 +14,7 @@ class TestDiscussAttachmentController(MailControllerAttachmentCommon):
         )
         channel.add_members(guest_ids=[self.guest.id])
         channel.env.context = {**channel.env.context, "guest": self.guest}
-        self._execute_subtests(
+        self._execute_subtests_upload(
             channel,
             (
                 (self.guest, True),
@@ -22,4 +23,43 @@ class TestDiscussAttachmentController(MailControllerAttachmentCommon):
                 (self.user_portal, True),
                 (self.user_public, True),
             ),
+        )
+
+    def test_attachment_delete_linked_to_public_channel(self):
+        """Test access to delete an attachment associated with a public channel"""
+        channel = self.env["discuss.channel"].create({"name": "public channel"})
+        self._execute_subtests_delete(
+            product(
+                (self.guest, self.user_portal, self.user_public),
+                (self.WITH_TOKEN, self.NO_TOKEN),
+            ),
+            allowed=False,
+            thread=channel,
+        )
+        self._execute_subtests_delete(
+            product(
+                (self.user_admin, self.user_employee),
+                (self.WITH_TOKEN, self.NO_TOKEN),
+            ),
+            allowed=True,
+            thread=channel,
+        )
+
+    def test_attachment_delete_linked_to_private_channel(self):
+        """Test access to delete an attachment associated with a private channel"""
+        channel = self.env["discuss.channel"].create(
+            {"name": "Private Channel", "channel_type": "group"}
+        )
+        self._execute_subtests_delete(
+            product(
+                (self.guest, self.user_employee, self.user_portal, self.user_public),
+                (self.WITH_TOKEN, self.NO_TOKEN),
+            ),
+            allowed=False,
+            thread=channel,
+        )
+        self._execute_subtests_delete(
+            product(self.user_admin, (self.WITH_TOKEN, self.NO_TOKEN)),
+            allowed=True,
+            thread=channel,
         )
