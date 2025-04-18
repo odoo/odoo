6a81986e1e055a384e35d963f80cf5212d5977f9
tsm-odoo <tsm@odoo.com>
2025-04-18 09:43:43 +0000

    [FIX] bus: detect lost notifications when gc runs during disconnection
    In [1], a mechanism was introduced to detect when notifications were
    lost due to the bus table being cleared during a disconnection.
    
    However, this approach used the autovacuum cron dates to detect this
    scenario. Other actions can clear the bus table, which is typically
    the case on odoo.com where another cron runs more frequently.
    
    This PR fixes the issue by checking if the last bus received before
    the disconnection is still present in the bus table. If it's not, then
    the table was cleared and we probably missed notifications.
    
    [1]: https://github.com/odoo/odoo/pull/188003
    
    closes odoo/odoo#206745
    
    X-original-commit: 3f575ead1becb46d13828e2c3ce55e83548bc84e
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/bus/static/tests/outdated_page_watcher.test.js
+++ addons/bus/static/tests/outdated_page_watcher.test.js
@@ -10,27 +10,16 @@ import {
     waitForSteps,
 } from "@web/../tests/web_test_helpers";
 import { browser } from "@web/core/browser/browser";
-import { serializeDateTime } from "@web/core/l10n/dates";
 import { WebClient } from "@web/webclient/webclient";
 import { addBusServiceListeners, defineBusModels, startBusService } from "./bus_test_helpers";
 
 defineBusModels();
 describe.current.tags("desktop");
 
-const { DateTime } = luxon;
 test("disconnect during vacuum should ask for reload", async () => {
     browser.location.addEventListener("reload", () => asyncStep("reload"));
-    addBusServiceListeners(
-        ["connect", () => asyncStep("connect")],
-        ["disconnect", () => (lastDisconnectDt = DateTime.now())]
-    );
-    // Vacuum permanently clears notifs, so reload might be required to recover
-    // coherent state in apps like Discuss.
-    let lastDisconnectDt;
-    onRpc("/bus/get_autovacuum_info", () => ({
-        lastcall: serializeDateTime(lastDisconnectDt.plus({ minute: 1 })),
-        nextcall: serializeDateTime(DateTime.now().plus({ day: 1 })),
-    }));
+    addBusServiceListeners(["connect", () => asyncStep("connect")]);
+    onRpc("/bus/has_missed_notifications", () => true);
     await mountWithCleanup(WebClient);
     startBusService();
     await runAllTimers();
