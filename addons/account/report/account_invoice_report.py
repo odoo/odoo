ac48c993db8343c023ab4e977c233ab2784e20f3
Hussein Ebrahim(hyhe) <hyhe@odoo.com>
2025-04-18 07:13:40 +0000

    [FIX] account: wrong conversion in invoice analysis values
    Steps to reproduce:
    - Install accounting, Sales apps
    - Setup two companies for the current user with two diff currencies
    - In company A, make a SO on a product and create an invoice
    - Validate and confirm the invoice.
    - Go to accounting, reporting, invoice analysis, and add group for
      the product
    - Select the report of that specific invoice posted
    - Observe the values untaxed_total, avg_price, inventory_value and
      price_margin
    - Switch to company B, and add a currency rate between both
      currencies to observe a difference
    - Make sure that you are selecting both companies, but B as the main
      company
    - Go again to the report of the same Invoice.
    - Observe the values again.
    
    Issue:
    When changing the currency, the fields untaxed_total and
    price_avg are updated using the currency rate of the current
    main company. However, inventory_value is not updated.
    
    This inconsistency affects the calculation of price_margin,
    which relies on both untaxed_total and inventory_value.
    As a result, price_margin is computed incorrectly.
    
    opw-4582973
    
    X-original-commit: cb66d9317b46555071c29705aa9e1aa40ee355c1

--- addons/account/report/account_invoice_report.py
+++ addons/account/report/account_invoice_report.py
@@ -117,11 +117,11 @@ class AccountInvoiceReport(models.Model):
                    0.0) * account_currency_table.rate                               AS price_average,
                 CASE
                     WHEN move.move_type NOT IN ('out_invoice', 'out_receipt', 'out_refund') THEN 0.0
-                    WHEN move.move_type = 'out_refund' THEN -line.balance * account_currency_table.rate + (line.quantity / NULLIF(COALESCE(uom_line.factor, 1) / COALESCE(uom_template.factor, 1), 0.0)) * COALESCE(product.standard_price -> line.company_id::text, to_jsonb(0.0))::float
-                    ELSE -line.balance * account_currency_table.rate - (line.quantity / NULLIF(COALESCE(uom_line.factor, 1) / COALESCE(uom_template.factor, 1), 0.0)) * COALESCE(product.standard_price -> line.company_id::text, to_jsonb(0.0))::float
+                    WHEN move.move_type = 'out_refund' THEN account_currency_table.rate * (-line.balance + (line.quantity / NULLIF(COALESCE(uom_line.factor, 1) / COALESCE(uom_template.factor, 1), 0.0)) * COALESCE(product.standard_price -> line.company_id::text, to_jsonb(0.0))::float)
+                    ELSE account_currency_table.rate * (-line.balance - (line.quantity / NULLIF(COALESCE(uom_line.factor, 1) / COALESCE(uom_template.factor, 1), 0.0)) * COALESCE(product.standard_price -> line.company_id::text, to_jsonb(0.0))::float)
                 END
                                                                             AS price_margin,
-                line.quantity / NULLIF(COALESCE(uom_line.factor, 1) / COALESCE(uom_template.factor, 1), 0.0) * (CASE WHEN move.move_type IN ('out_invoice','in_refund','out_receipt') THEN -1 ELSE 1 END)
+                account_currency_table.rate * line.quantity / NULLIF(COALESCE(uom_line.factor, 1) / COALESCE(uom_template.factor, 1), 0.0) * (CASE WHEN move.move_type IN ('out_invoice','in_refund','out_receipt') THEN -1 ELSE 1 END)
                     * COALESCE(product.standard_price -> line.company_id::text, to_jsonb(0.0))::float                    AS inventory_value,
                 COALESCE(partner.country_id, commercial_partner.country_id) AS country_id,
                 line.currency_id                                            AS currency_id
