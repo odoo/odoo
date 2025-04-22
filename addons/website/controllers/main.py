51320453f1aa7ad7aadcf61bdc6a95b62e4cd031
gppa_odoo <gppa@odoo.com>
2025-04-22 17:17:52 +0530

    [IMP] website: sitemap.xml enhancements

--- addons/website/controllers/main.py
+++ addons/website/controllers/main.py
@@ -40,7 +40,7 @@ logger = logging.getLogger(__name__)
 # Completely arbitrary limits
 MAX_IMAGE_WIDTH, MAX_IMAGE_HEIGHT = IMAGE_LIMITS = (1024, 768)
 LOC_PER_SITEMAP = 45000
-SITEMAP_CACHE_TIME = datetime.timedelta(hours=12)
+SITEMAP_CACHE_TIME = datetime.timedelta(seconds=10)
 MAX_FONT_FILE_SIZE = 10 * 1024 * 1024
 SUPPORTED_FONT_EXTENSIONS = ['ttf', 'woff', 'woff2', 'otf']
 
@@ -268,15 +268,33 @@ class Website(Home):
                    ('url', '=', '%s.xml' % sitemap_base_url)]
             sitemaps = Attachment.search(dom)
             sitemaps.unlink()
-
-            pages = 0
+            valid_locs = []
             locs = request.website.with_user(request.website.user_id)._enumerate_pages()
+
+            for loc in locs:
+                relative_url = loc.get('loc')
+                full_url = url_root.rstrip('/') + relative_url
+                try:
+                    resp = requests.get(full_url)
+                    logger.info("%s -> %s", full_url, resp.status_code)
+                    if 200 <= resp.status_code < 300:
+                        valid_locs.append(loc)
+                    else:
+                        logger.warning("Skipping URL with non-2XX status: %s (%s)", full_url, resp.status_code)
+                except Exception as e:
+                    logger.error("Failed to check %s: %s", full_url, e)
+            pages = 0
             while True:
+                current_slice = list(islice(valid_locs, pages * LOC_PER_SITEMAP, (pages + 1) * LOC_PER_SITEMAP))
+                if not current_slice:
+                    break
+
                 values = {
-                    'locs': islice(locs, 0, LOC_PER_SITEMAP),
+                    'locs': current_slice,
                     'url_root': url_root[:-1],
                 }
                 urls = View._render_template('website.sitemap_locs', values)
+
                 if urls.strip():
                     content = View._render_template('website.sitemap_xml', {'content': urls})
                     pages += 1
