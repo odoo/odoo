f98a9ba03bad5b37356abd4395875a01e3198eeb
Abdelrahman Mahmoud (amah) <amah@odoo.com>
2025-04-22 11:06:11 +0200

    [IMP] test_resource: add new tests for types
    -  add new tests to ensure schedule_type system
    works fine

--- addons/test_resource/tests/__init__.py
+++ addons/test_resource/tests/__init__.py
@@ -5,5 +5,6 @@ from . import test_calendar
 from . import test_mixin
 from . import test_performance
 from . import test_resource_errors
+from . import test_resource_type
 from . import test_resource
 from . import test_timezones
