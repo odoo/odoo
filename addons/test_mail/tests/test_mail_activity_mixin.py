8f09a3a15ff16dc2cbc7d5f9359f84304cca0021
adip-odoo <adip@odoo.com>
2025-04-22 12:57:11 +0000

    [FIX] mail: prevent error while loading progress bar in CRM pipeline
    Currently, an error occurs when users attempt to group by records by
    **Activity State** in the CRM pipeline kanban view.
    
    **Steps to reproduce:**
    - Install the `crm` module.
    - Open the CRM pipeline kanban view.
    - Group records by Activity State.
    - Observe the error.
    
    **Error:** `AssertionError`
    
    The issue occurs when attempting to add a LEFT JOIN on an SQL query without
    verifying whether it has already been included. - [1]
    
    The method `_read_group_groupby` is called twice because the `activity_state`
    field is used both as a progress bar field at [2] and as a manual group by
    option in the Kanban view by the user.
    
    [1] - https://github.com/odoo/odoo/blob/6edf0ba80818fc23b8daf46d694d201e0efee133/addons/mail/models/mail_activity_mixin.py#L284
    
    [2] - https://github.com/odoo/odoo/blob/5c4e0826528ec88ea7a180a3598e7f225346bc49/addons/crm/views/crm_lead_views.xml#L505-L507
    
    This commit adds a check to ensure that the join is only added once,
    preventing redundant joins.
    
    Sentry - 6545651193
    
    closes odoo/odoo#211570
    
    X-original-commit: eaeab2dffcf7a98b232302ffb6c48b8133b1a842
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>
    Signed-off-by: Aditi Patel (adip) <adip@odoo.com>

--- addons/test_mail/tests/test_mail_activity_mixin.py
+++ addons/test_mail/tests/test_mail_activity_mixin.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime, timedelta
+from datetime import date, datetime, timedelta, timezone
 from dateutil.relativedelta import relativedelta
 from freezegun import freeze_time
 from unittest.mock import patch
@@ -591,6 +591,50 @@ class TestActivityMixin(TestActivityCommon):
 @tests.tagged('mail_activity', 'mail_activity_mixin')
 class TestORM(TestActivityCommon):
     """Test for read_progress_bar"""
+
+    def test_groupby_activity_state_progress_bar_behavior(self):
+        """ Test activity_state groupby logic on mail.test.lead when 'activity_state'
+        is present multiple times in the groupby field list. """
+        lead_timedelta_setup = [0, 0, -2, -2, -2, 2]
+
+        leads = self.env["mail.test.lead"].create([
+            {"name": f"CRM Lead {i}"}
+            for i in range(1, len(lead_timedelta_setup) + 1)
+        ])
+
+        with freeze_time("2025-05-21 10:00:00"):
+            self.env["mail.activity"].create([
+                {
+                    "date_deadline": datetime.now(timezone.utc) + timedelta(days=delta_days),
+                    "res_id": lead.id,
+                    "res_model_id": self.env["ir.model"]._get_id("mail.test.lead"),
+                    "summary": f"Test activity for CRM lead {lead.id}",
+                    "user_id": self.env.user.id,
+                } for lead, delta_days in zip(leads, lead_timedelta_setup)
+            ])
+
+            # grouping by 'activity_state' and 'activity_state' as the progress bar
+            domain = [("name", "!=", "")]
+            groupby = "activity_state"
+            progress_bar = {
+                "field": "activity_state",
+                "colors": {
+                    "overdue": "danger",
+                    "today": "warning",
+                    "planned": "success",
+                },
+            }
+            progressbars = self.env["mail.test.lead"].read_progress_bar(
+                domain=domain, group_by=groupby, progress_bar=progress_bar,
+            )
+
+            self.assertEqual(len(progressbars), 3)
+            expected_progressbars = {
+                "overdue": {"overdue": 3, "today": 0, "planned": 0},
+                "today": {"overdue": 0, "today": 2, "planned": 0},
+                "planned": {"overdue": 0, "today": 0, "planned": 1},
+            }
+            self.assertEqual(dict(progressbars), expected_progressbars)
 
     def test_week_grouping(self):
         """The labels associated to each record in read_progress_bar should match
