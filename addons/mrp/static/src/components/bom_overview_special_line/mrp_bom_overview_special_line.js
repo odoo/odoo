c5b46598b5dd7a53381955a3f56436f0b65f8125
JF Aubert <ajf@odoo.com>
2025-04-22 10:38:39 +0200

    [IMP] mrp,plm: adapt bom report and overview
    task: 4300594
    
    See odoo/enterprise#78517
    See odoo/upgrade#7157

--- addons/mrp/static/src/components/bom_overview_special_line/mrp_bom_overview_special_line.js
+++ addons/mrp/static/src/components/bom_overview_special_line/mrp_bom_overview_special_line.js
@@ -9,10 +9,7 @@ export class BomOverviewSpecialLine extends Component {
         showOptions: {
             type: Object,
             shape: {
-                availabilities: Boolean,
-                costs: Boolean,
-                operations: Boolean,
-                leadTimes: Boolean,
+                mode: String,
                 uom: Boolean,
                 attachments: Boolean,
             },
@@ -46,16 +43,8 @@ export class BomOverviewSpecialLine extends Component {
         return ["operations", "byproducts"].includes(this.props.type);
     }
 
-    get showAvailabilities() {
-        return this.props.showOptions.availabilities;
-    }
-
-    get showCosts() {
-        return this.props.showOptions.costs;
-    }
-
-    get showLeadTimes() {
-        return this.props.showOptions.leadTimes;
+    get forecastMode() {
+        return this.props.showOptions.mode == "forecast";
     }
 
     get showUom() {
--- addons/mrp/static/tests/tours/mrp_manufacture_from_bom.js
+++ addons/mrp/static/tests/tours/mrp_manufacture_from_bom.js
@@ -11,6 +11,10 @@ registry.category("web_tour.tours").add("test_manufacture_from_bom", {
             run: "click",
         },
         {
+            trigger: '.fa-toggle-off',
+            run: "click",
+        },
+        {
             trigger: 'button.btn-primary:contains("Manufacture")',
             run: "click",
         },
