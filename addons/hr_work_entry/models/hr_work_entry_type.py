58e929160b58266c6ff7147335c072869f4e70e1
rame-odoo <rame@odoo.com>
2025-04-22 18:47:36 +0530

    [IMP] hr_payroll: add rate and extra hours functionality in work entry type
    _* = hr_holidays, hr_work_entry, hr_work_entry_contract, hr_work_entry_holidays
    
    - The goal of this change is to improve the payroll system by providing a clearer
      and more flexible way to manage extra hours and overtime manually.
    - This update introduces the ability to mark specific work entry types as extra
      hours and define a custom rate to be applied during payslip computation.
    - HR managers can now manually configure both the extra hours flag and the
      associated rate on each work entry type.
    
    This PR includes:
    
    - Introduced a general rate field on work entry types to define the pay rate for
      any work entry.
    - Added a checkbox to mark specific work entry types as extra hours, so they are
      paid on top of the regular wage.
    - Updated the payslip logic to calculate the amount based on the defined rate
      for all work entry types.
    - Added a checkbox to the work entry type to determine if the amount should be
      added on top of the regular salary, based on the rate.
    - Updated the logic to handle all countries based on the new work entry type
      settings, allowing consistent rate application and extra work amount globally.
      (BE, HK, LU etc.)
    - Removed overtime rule parameters and country-specific hardcoded logic to make
      the extra hours and rate calculation more flexible across all countries.
      (PL, US, SK etc.)
    - Wrote upgrade scripts to handle old data and align it with new work entry type
      and rate logic across PL, SK, and US.
    - Removed old overtime rule parameters and updated work entries and worked days
      to match the new setup.
    
    Related Upgrade PR - https://github.com/odoo/upgrade/pull/7364
    
    Task - 4420535
    
    closes odoo/odoo#206878
    
    Related: odoo/enterprise#76506
    Related: odoo/upgrade#7364
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr_work_entry/models/hr_work_entry_type.py
+++ addons/hr_work_entry/models/hr_work_entry_type.py
@@ -27,6 +27,13 @@ class HrWorkEntryType(models.Model):
     is_work = fields.Boolean(
         compute='_compute_is_work', inverse='_inverse_is_work', string="Working Time", readonly=False,
         help="If checked, the work entry is counted as work time in the working schedule")
+    amount_rate = fields.Float(
+        string="Rate",
+        default=1.0,
+        help="If you want the hours should be paid double, the rate should be 200%.")
+    is_extra_hours = fields.Boolean(
+        string="Added to Monthly Pay",
+        help="Check this setting if you want the hours to be considered as extra time and added as a bonus to the basic salary.")
 
     @api.constrains('country_id')
     def _check_work_entry_type_country(self):
