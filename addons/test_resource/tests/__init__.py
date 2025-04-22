d3dc9fa5859bfee7f222ca82b8b7fc0257f0a8f8
Abdelrahman Mahmoud (amah) <amah@odoo.com>
2025-04-22 10:01:38 +0200

    [REF] test_resource: unpack test_resource
    - break the test calsses each to individual file

--- addons/test_resource/tests/__init__.py
+++ addons/test_resource/tests/__init__.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import test_resource
+from . import test_calendar
+from . import test_mixin
 from . import test_performance
+from . import test_resource_errors
+from . import test_resource
+from . import test_timezones
