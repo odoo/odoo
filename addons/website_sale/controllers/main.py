ba547b084db56af2536d99a1e6b270147d3ba556
Valentin Chevalier <vcr@odoo.com>
2025-04-23 11:18:03 +0000

    [FIX] website_sale: fix express checkout shipping issue
    Versions
    --------
    - 17.4+
    
    Steps
    -----
    1. Ensure that Mitchell Admin has a child delivery partner in the
    database with the following information:
    'name': 'Mitchell Admin',
    'email': 'admin@yourcompany.example.com',
    'street': '215 Vine St',
    'country': 'US',
    'city':'Scranton',
    'zip':'18503'
    2. Log in to eCommerce as Mitchell Admin
    3. Add any deliverable product to the cart
    4. Pay using Express Checkout
    
    Issue
    -----
    Express Checkout fails when customers use different billing and shipping
    addresses. If the shipping address is unknown to the system, a
    validation error blocks payment. If known, the payment goes through but
    results in a generic shipping error with no further details or options
    for the user.
    
    Cause
    -----
    `billing_address` was wrongly parsed as `shipping_address` in a5df1a7.
    
    Solution
    --------
    Parse `shipping_address` correctly.
    
    opw-4710674
    
    closes odoo/odoo#207093
    
    X-original-commit: e819b39cd47fcbc049913827853789b291465e5a
    Signed-off-by: Valentin Chevalier <vcr@odoo.com>

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -1658,7 +1658,7 @@ class WebsiteSale(payment_portal.PaymentPortal):
         if shipping_address:
             #in order to not override shippig address, it's checked separately from shipping option
             self._include_country_and_state_in_address(shipping_address)
-            shipping_address, _side_values = self._parse_form_data(billing_address)
+            shipping_address, _side_values = self._parse_form_data(shipping_address)
 
             if order_sudo.partner_shipping_id.name.endswith(order_sudo.name):
                 # The existing partner was created by `process_express_checkout_delivery_choice`, it
