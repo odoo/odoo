33b1bfdf0f33641a73e3d20c13c753cc8e6d4294
Max Whale <mawh@odoo.com>
2025-04-23 10:29:36 +0200

    [IMP] hw_drivers: show unsupported devices
    Before this commit, any devices that did not have a supported driver
    were just ignored.
    
    After this commit, if the Interface class sets
    `allow_unsupported = True`, then any devices found with no suitable
    driver will be added to a list of unsupported devices. This list is sent
    to both the IoT box homepage, as well as sent to the connected DB. This
    can help with debugging devices that are not being detected as expected
    (e.g. blackbox serial device).
    
    task-4744357
    
    closes odoo/odoo#207043
    
    Related: odoo/enterprise#83925
    Signed-off-by: Yaroslav Soroko (yaso) <yaso@odoo.com>

--- addons/hw_drivers/interface.py
+++ addons/hw_drivers/interface.py
@@ -5,19 +5,20 @@ import logging
 from threading import Thread
 import time
 
-from odoo.addons.hw_drivers.main import drivers, interfaces, iot_devices
+from odoo.addons.hw_drivers.main import drivers, interfaces, iot_devices, unsupported_devices
 
 _logger = logging.getLogger(__name__)
 
 
 class Interface(Thread):
     _loop_delay = 3  # Delay (in seconds) between calls to get_devices or 0 if it should be called only once
-    _detected_devices = {}
     connection_type = ''
+    allow_unsupported = False
     daemon = True
 
     def __init__(self):
         super().__init__()
+        self._detected_devices = set()
         self.drivers = sorted([d for d in drivers if d.connection_type == self.connection_type], key=lambda d: d.priority, reverse=True)
 
     def __init_subclass__(cls):
@@ -37,34 +38,38 @@ class Interface(Thread):
 
         added = devices.keys() - self._detected_devices
         removed = self._detected_devices - devices.keys()
-        # keys() returns a dict_keys, and the values of that stay in sync with the
-        # original dictionary if it changes. This means that get_devices needs to return
-        # a newly created dictionary every time. If it doesn't do that and reuses the
-        # same dictionary, this logic won't detect any changes that are made. Could be
-        # avoided by converting the dict_keys into a regular dict. The current logic
-        # also can't detect if a device is replaced by a different one with the same
-        # key. Also, _detected_devices starts out as a class variable but gets turned
-        # into an instance variable here. It would be better if it was an instance
-        # variable from the start to avoid confusion.
-        self._detected_devices = devices.keys()
+        self._detected_devices = set(devices.keys())
 
         for identifier in removed:
             if identifier in iot_devices:
                 iot_devices[identifier].disconnect()
                 _logger.info('Device %s is now disconnected', identifier)
+            elif self.allow_unsupported and identifier in unsupported_devices:
+                del unsupported_devices[identifier]
+                _logger.info('Unsupported device %s is now disconnected', identifier)
 
         for identifier in added:
-            for driver in self.drivers:
-                if driver.supported(devices[identifier]):
-                    _logger.info('Device %s is now connected', identifier)
-                    d = driver(identifier, devices[identifier])
-                    iot_devices[identifier] = d
-                    # Start the thread after creating the iot_devices entry so the
-                    # thread can assume the iot_devices entry will exist while it's
-                    # running, at least until the `disconnect` above gets triggered
-                    # when `removed` is not empty.
-                    d.start()
-                    break
+            supported_driver = next(
+                (driver for driver in self.drivers if driver.supported(devices[identifier])),
+                None
+            )
+            if supported_driver:
+                _logger.info('Device %s is now connected', identifier)
+                d = supported_driver(identifier, devices[identifier])
+                iot_devices[identifier] = d
+                # Start the thread after creating the iot_devices entry so the
+                # thread can assume the iot_devices entry will exist while it's
+                # running, at least until the `disconnect` above gets triggered
+                # when `removed` is not empty.
+                d.start()
+            elif self.allow_unsupported:
+                _logger.info('Unsupported device %s is now connected', identifier)
+                unsupported_devices[identifier] = {
+                    'name': f'Unknown device ({self.connection_type})',
+                    'identifier': identifier,
+                    'type': 'unsupported',
+                    'connection': 'direct' if self.connection_type == 'usb' else self.connection_type,
+                }
 
     def get_devices(self):
         raise NotImplementedError()
