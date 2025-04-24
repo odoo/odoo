cc60da3c1b2caa7c3b430f48597d12150cf513f3
Victor Piryns (pivi) <pivi@odoo.com>
2025-04-24 16:32:05 +0200

    [IMP] orm: add optimization to bypass redundant comodel lookup
    Add a domain optimization to transform expressions like:
    `[('partner_id', 'any!', [('id', 'in', (42))]]` into
    `[('partner_id', 'in', (42))]` and
    `[('partner_id', 'any!', [('id', 'any!', query)]]` into
    `[('partner_id', 'any!', query)]`.
    This avoids an unnecessary subquery on the comodel table.
    This optimization is safe when record rules don't apply.
    
    This transforms SQL WHERE clauses from:
    `rel_column IN (SELECT id FROM comodel_table WHERE id IN (42))`
    to the simpler:
    `rel_column IN (42)`
    
    Benefits of this query optimization:
    
    - Eliminates unnecessary scanning of `comodel_table`
    - Allows direct querying of `pg_stats` for `fkey_column` statistics
      (subquery results lack these statistics)
    - Removes potential optimization barriers caused by subqueries that
      Postgres may plan separately, enabling better execution plans
    
    task-4750311
    
    closes odoo/odoo#219105
    
    Related: odoo/enterprise#90270
    Signed-off-by: Raphael Collet <rco@odoo.com>

--- odoo/orm/domains.py
+++ odoo/orm/domains.py
@@ -1738,6 +1738,43 @@ def _optimize_any_with_rights(condition, model):
     return condition
 
 
+@field_type_optimization(['many2one'], level=OptimizationLevel.FULL)
+def _optimize_m2o_bypass_comodel_id_lookup(condition, model):
+    """Avoid comodel's subquery, if it can be compared with the field directly"""
+    operator = condition.operator
+    if (
+        operator in ('any!', 'not any!')
+        and isinstance(subdomain := condition.value, DomainCondition)
+        and subdomain.field_expr == 'id'
+        and (suboperator := subdomain.operator) in ('in', 'not in', 'any!', 'not any!')
+    ):
+        # We are bypassing permissions, we can transform:
+        #  a ANY (id IN X)  =>  a IN (X - {False})
+        #  a ANY (id NOT IN X)  =>  a NOT IN (X | {False})
+        #  a ANY (id ANY X)  =>  a ANY X
+        #  a ANY (id NOT ANY X)  =>  a != False AND a NOT ANY X
+        #  a NOT ANY (id IN X)  =>  a NOT IN (X - {False})
+        #  a NOT ANY (id NOT IN X)  =>  a IN (X | {False})
+        #  a NOT ANY (id ANY X)  =>  a NOT ANY X
+        #  a NOT ANY (id NOT ANY X)  =>  a = False OR a ANY X
+        val = subdomain.value
+        match suboperator:
+            case 'in':
+                domain = DomainCondition(condition.field_expr, 'in', val - {False})
+            case 'not in':
+                domain = DomainCondition(condition.field_expr, 'not in', val | {False})
+            case 'any!':
+                domain = DomainCondition(condition.field_expr, 'any!', val)
+            case 'not any!':
+                domain = DomainCondition(condition.field_expr, '!=', False) \
+                    & DomainCondition(condition.field_expr, 'not any!', val)
+        if operator == 'not any!':
+            domain = ~domain
+        return domain
+
+    return condition
+
+
 # --------------------------------------------------
 # Optimizations: nary
 # --------------------------------------------------
