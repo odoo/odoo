231a0a507a7460f8dce452f2bd5a450932810786
parp-odoo <parp@odoo.com>
2025-04-24 04:41:46 +0000

    [FIX] pos, pos_loyalty: hide special products when no regular ones are available
    Before this commit:
    In a non-demo db with the loyalty module, loading sample data was not possible.
    Only special products (e.g., gift card, top-up eWallet) were shown,
    which caused the "Load Sample Data" button to be hidden.
    
    After this commit:
    Hide the special products when no regular products are available.
    allowing the "Load Sample Data" button to remain visible.
    
    closes odoo/odoo#210086
    
    Task: 4743089
    X-original-commit: 2668181c98de4a9c6f33787372adefc01cf1d0bc
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: Parthkumar Patel (parp) <parp@odoo.com>

--- addons/pos_loyalty/models/product_template.py
+++ addons/pos_loyalty/models/product_template.py
@@ -20,6 +20,20 @@ class ProductTemplate(models.Model):
         missing_product_templates = self.env['product.template'].browse(missing_product_tmpl_ids).read(fields=fields, load=False)
         product_ids_to_hide = reward_products.product_tmpl_id - self.env['product.template'].browse(already_loaded_product_tmpl_ids)
         data['pos.session'][0]['_pos_special_products_ids'] += product_ids_to_hide.product_variant_id.ids
+
+        # Identify special loyalty products (e.g., gift cards, e-wallets) to be displayed in the POS
+        loyality_products = config_id.get_record_by_ref([
+            'loyalty.gift_card_product_50',
+            'loyalty.ewallet_product_50',
+        ])
+        special_display_products = self.env['product.product'].browse(loyality_products)
+        # Include trigger products from loyalty programs of type 'gift_card' or 'ewallet'
+        special_display_products += self.env['loyalty.program'].search([
+            ('program_type', 'in', ['gift_card', 'ewallet']),
+            ('pos_config_ids', 'in', [False, config_id.id]),
+        ]).trigger_product_ids
+
+        data['pos.session'][0]['_pos_special_display_products_ids'] = special_display_products.product_tmpl_id.ids
         res.extend(missing_product_templates)
 
         return res
