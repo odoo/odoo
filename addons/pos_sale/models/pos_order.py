de5af2291fbaad922954b2d732d0442c1c0bb90d
roen-odoo <roen@odoo.com>
2025-04-24 12:15:16 +0000

    [FIX] pos_sale: ignore payment term discount
    When you settle a sale order that has a payment term, it was always
    ignored in the PoS. But it should only be ignored when the payment term
    has an early discount.
    
    Steps to reproduce:
    -------------------
    * Create a sale order with a payment term that has no early discount
    * Go to the PoS and settle the sale order
    * Invoice and validate the order
    > Observation: In the backend, the sale order has the payment term
      applied on the invoice
    
    Why the fix:
    ------------
    Some modules needs the invoice to have payment terms set
    (l10n_mx for example). But the PoS invoice was always ignoring it. We
    should only ignore it if the payment terms has an early discount.
    
    opw-4670234
    
    X-original-commit: 17c904e687d0d7f3e96dbc40a687791cf3acc80d

--- addons/pos_sale/models/pos_order.py
+++ addons/pos_sale/models/pos_order.py
@@ -38,7 +38,9 @@ class PosOrder(models.Model):
             else:
                 addr = self.partner_id.address_get(['delivery'])
                 invoice_vals['partner_shipping_id'] = addr['delivery']
-            if sale_orders[0].payment_term_id:
+            if sale_orders[0].payment_term_id and not sale_orders[0].payment_term_id.early_discount:
+                invoice_vals['invoice_payment_term_id'] = sale_orders[0].payment_term_id.id
+            else:
                 invoice_vals['invoice_payment_term_id'] = False
             if sale_orders[0].partner_invoice_id != sale_orders[0].partner_id:
                 invoice_vals['partner_id'] = sale_orders[0].partner_invoice_id.id
