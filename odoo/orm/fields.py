e10ebc5fba25971d84eebf2fa7adaca899448012
Alvaro Fuentes <afu@odoo.com>
2025-04-24 14:48:26 +0200

    [IMP] core: fill column for required stored computed fields
    When a required stored computed field doesn't have a default value we
    may fail to add its not null constraint if some records are created
    during an upgrade. This would then cause a warning once the registry is
    fully loaded.
    
    closes odoo/odoo#215571
    
    Signed-off-by: Rémy Voet (ryv) <ryv@odoo.com>
    Co-authored-by: Raphael Collet <rco@odoo.com>
    Co-authored-by: "Krzysztof Magusiak (krma)" <krma@odoo.com>

--- odoo/orm/fields.py
+++ odoo/orm/fields.py
@@ -1163,6 +1163,12 @@ class Field(typing.Generic[T]):
                 field = model._fields[self.name]
                 if not field.required or not field.store:
                     return
+                if field.compute:
+                    records = model.browse(id_ for id_, in model.env.execute_query(SQL(
+                        "SELECT id FROM %s AS t WHERE %s IS NULL",
+                        SQL.identifier(model._table), model._field_to_sql('t', field.name),
+                    )))
+                    model.env.add_to_compute(field, records)
                 # Flush values before adding NOT NULL constraint.
                 model.flush_model([field.name])
                 model.pool.post_constraint(
