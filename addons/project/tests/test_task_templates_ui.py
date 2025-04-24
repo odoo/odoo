21d2b1c536fda81cf0a790d9f3357177a0249737
Vincent Larcin <vila@odoo.com>
2025-04-24 10:02:26 +0200

    [FIX] project: fix wrong stage in template task test
    This commit fixes a test that is erroneously using a project stage as a task stage

--- addons/project/tests/test_task_templates_ui.py
+++ addons/project/tests/test_task_templates_ui.py
@@ -9,14 +9,15 @@ class TestTaskTemplatesTour(HttpCase):
         super().setUpClass()
         cls.project_with_templates = cls.env["project.project"].create({
             "name": "Project with Task Template",
-            "type_ids": [Command.link(cls.env.ref("project.project_project_stage_0").id)],
+            "type_ids": [Command.create({
+                "name": "New",
+            })],
         })
         cls.template_task = cls.env["project.task"].create({
             "name": "Template",
             "project_id": cls.project_with_templates.id,
             "is_template": True,
             "description": "Template description",
-            "stage_id": cls.env.ref("project.project_project_stage_0").id,
         })
 
     def test_task_templates_tour(self):
