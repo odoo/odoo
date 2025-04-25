e9e5442e0f4f97628e2e4748858a90a19e97641b
Krzysztof Magusiak (krma) <krma@odoo.com>
2025-04-25 08:29:26 +0000

    [REF] tools: remove js_transpiler and sourcemap_generator import
    These modules are specialized and are not necessary when just importing
    basic tools.

--- odoo/addons/test_assetsbundle/tests/test_js_transpiler.py
+++ odoo/addons/test_assetsbundle/tests/test_js_transpiler.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo.tests import tagged
 from odoo.tests.common import TransactionCase
-from odoo.tools import transpile_javascript
+from odoo.tools.js_transpiler import transpile_javascript
 
 
 @tagged('post_install', '-at_install')
--- odoo/addons/test_assetsbundle/tests/test_js_transpiler_regex.py
+++ odoo/addons/test_assetsbundle/tests/test_js_transpiler_regex.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo.tests import tagged
 from odoo.tests.common import TransactionCase
-from odoo.tools import URL_RE, ODOO_MODULE_RE
+from odoo.tools.js_transpiler import URL_RE, ODOO_MODULE_RE
 
 @tagged('post_install', '-at_install')
 class TestJsTranspiler(TransactionCase):
