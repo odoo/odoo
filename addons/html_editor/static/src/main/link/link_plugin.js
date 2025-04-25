3b2e53f381d9390f882ee3eb11a9addcfb3f9f5d
Sébastien Geelen <sge@odoo.com>
2025-04-25 14:57:59 +0200

    [IMP] html_editor: add custom button options
    In the link popover; add the possibilities to customize the button style
    as available already in the website builder.
    
    Add options for :
    custom text color
    custom fill color
    custom border size, color and shape
    
    Also add the possibility to open the link in a "_blank" window.
    
    task-4690014

--- addons/html_editor/static/src/main/link/link_plugin.js
+++ addons/html_editor/static/src/main/link/link_plugin.js
@@ -412,27 +412,33 @@ export class LinkPlugin extends Plugin {
         const selectionTextContent = selection?.textContent();
         const isImage = !!findInSelection(selection, "img");
 
-        const applyCallback = (url, label, classes) => {
-            if (this.linkInDocument && isImage) {
+        const applyCallback = (url, label, classes, customStyle, linkTarget) => {
+            if (this.linkInDocument) {
                 if (url) {
                     this.linkInDocument.href = url;
                 } else {
                     this.linkInDocument.removeAttribute("href");
                 }
-            } else if (this.linkInDocument) {
-                if (url) {
-                    this.linkInDocument.href = url;
+                if (linkTarget) {
+                    this.linkInDocument.setAttribute("target", linkTarget);
                 } else {
-                    this.linkInDocument.removeAttribute("href");
+                    this.linkInDocument.removeAttribute("target");
                 }
-                if (classes) {
-                    this.linkInDocument.className = classes;
-                } else {
-                    this.linkInDocument.removeAttribute("class");
-                }
-                if (cleanZWChars(this.linkInDocument.innerText) !== label) {
-                    this.linkInDocument.innerText = label;
-                    cursorsToRestore = null;
+                if (!isImage) {
+                    if (classes) {
+                        this.linkInDocument.className = classes;
+                    } else {
+                        this.linkInDocument.removeAttribute("class");
+                    }
+                    if (customStyle) {
+                        this.linkInDocument.setAttribute("style", customStyle);
+                    } else {
+                        this.linkInDocument.removeAttribute("style");
+                    }
+                    if (cleanZWChars(this.linkInDocument.innerText) !== label) {
+                        this.linkInDocument.innerText = label;
+                        cursorsToRestore = null;
+                    }
                 }
             } else if (url) {
                 // prevent the link creation if the url field was empty
@@ -444,6 +450,12 @@ export class LinkPlugin extends Plugin {
                     link.append(content);
                     if (classes) {
                         link.className = classes;
+                    }
+                    if (customStyle) {
+                        link.setAttribute("style", customStyle);
+                    }
+                    if (linkTarget) {
+                        link.setAttribute("target", linkTarget);
                     }
                     link.normalize();
                     this.linkInDocument = link;
@@ -464,6 +476,12 @@ export class LinkPlugin extends Plugin {
                     const link = this.createLink(url, label);
                     if (classes) {
                         link.className = classes;
+                    }
+                    if (customStyle) {
+                        link.setAttribute("style", customStyle);
+                    }
+                    if (linkTarget) {
+                        link.setAttribute("target", linkTarget);
                     }
                     this.linkInDocument = link;
                     cursorsToRestore = null;
@@ -512,6 +530,8 @@ export class LinkPlugin extends Plugin {
             canUpload: this.config.allowFile,
             onUpload: this.config.onAttachmentChange,
             type: this.type || "",
+            allowCustomStyle: this.config.allowCustomStyle,
+            allowTargetBlank: this.config.allowTargetBlank,
         };
 
         const popover = this.getActivePopover(linkElement);
@@ -538,7 +558,16 @@ export class LinkPlugin extends Plugin {
                     focusNode: link,
                     focusOffset: nodeSize(link),
                 });
+                const saveCustomStyle = link.getAttribute("style");
+                link.removeAttribute("style");
                 this.dependencies.color.removeAllColor();
+                if (
+                    saveCustomStyle &&
+                    this.config.allowCustomStyle &&
+                    link.className.includes("custom")
+                ) {
+                    link.setAttribute("style", saveCustomStyle);
+                }
                 // Remove the current link (linkInDocument) if it has no content
                 if (cleanZWChars(link.innerText) === "" && !link.querySelector("img")) {
                     const [anchorNode, anchorOffset] = rightPos(link);
@@ -560,6 +589,10 @@ export class LinkPlugin extends Plugin {
 
     normalizeLink(root) {
         for (const anchorEl of selectElements(root, "a")) {
+            if (/btn(-[a-z0-9_-]*)custom/.test(anchorEl.className)) {
+                // if the link is a customized button, we don't want to change the color
+                continue;
+            }
             const { color } = anchorEl.style;
             const childNodes = [...anchorEl.childNodes];
             // For each anchor element, if it has an inline color style,
