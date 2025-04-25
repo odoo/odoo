a7e988f88ae77abd59d07072b85521d3b04f4362
Antoine Guenet <age@odoo.com>
2025-04-25 13:11:01 +0200

    [FIX] html_editor: addGlobalDomListener for toolbar handling
    This adds a `addGlobalDomListener` method to plugins so they can add
    listeners to the document, that will be listened to, whether the target
    of the event is valid or not. It is then used in the toolbar plugin to
    update the toolbar visibility also when clicking in non-editable
    elements.
    
    This relates to the captions feature in the it fixes a bug where the
    toolbar stayed open when doing the following steps:
    
    1. Click on an image with a caption.
    2. Click on a button in the toolbar (eg, a shape).
    3. Click on the caption.
    
    In this case, the "selectionchange" event was triggered with the
    non-editable `input` element as a target, and the toolbar's listener
    wasn't called, leading to the toolbar staying open.
    
    task-4224709

--- addons/html_editor/static/src/plugin.js
+++ addons/html_editor/static/src/plugin.js
@@ -42,14 +42,39 @@ export class Plugin {
         return !isProtecting(ev.target) && (!isProtected(ev.target) || isUnprotecting(ev.target));
     }
 
-    addDomListener(target, eventName, fn, capture = false) {
+    /**
+     * Add an event listener on a given target, that will only be executed if
+     * the target is valid (unless `isGlobal` is true), and ensure it is removed
+     * when we destroy the editor.
+     *
+     * @param {Element} target
+     * @param {string} eventName
+     * @param {function(Event):void} fn
+     * @param {boolean} [capture=false] `useCapture` flag of `addEventListener`
+     * @param {boolean} [isGlobal=false] if true, don't check target validity
+     */
+    addDomListener(target, eventName, fn, capture = false, isGlobal = false) {
         const handler = (ev) => {
-            if (this.isValidTargetForDomListener(ev)) {
+            if (isGlobal || this.isValidTargetForDomListener(ev)) {
                 fn?.call(this, ev);
             }
         };
         target.addEventListener(eventName, handler, capture);
         this._cleanups.push(() => target.removeEventListener(eventName, handler, capture));
+    }
+
+    /**
+     * Add an event listener on the editor's document, and ensure it is removed
+     * when we destroy the editor.
+     *
+     * @todo Use this function to avoid iframe problems.
+     *
+     * @param {string} eventName
+     * @param {function(Event):void} fn
+     * @param {boolean} [capture=false] `useCapture` flag of `addEventListener`
+     */
+    addGlobalDomListener(eventName, fn, capture = false) {
+        this.addDomListener(this.document, eventName, fn, capture, true);
     }
 
     /**
