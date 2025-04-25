6a40c05b445b564b3f388cd103e0c974a01a780a
tsm-odoo <tsm@odoo.com>
2025-04-25 11:14:14 +0200

    [FIX] bus: detect lost notifications after reconnecting
    In [1], a mechanism was introduced to detect lost notifications when
    the bus table is cleared during socket disconnection. However, this
    relies on the "reconnect" event. The "reconnect" event is not triggered
    when the connection is closed cleanly. In such cases, the next
    connection is treated as a new one and triggers the "connect" event,
    which does not check for missed notifications.
    
    As a result, any notifications sent while the socket was disconnected
    can be missed.
    
    This commit ensures that the missed notification check is performed
    on all connections after the initial one, not only during a "reconnect".
    
    [1]: https://github.com/odoo/odoo/pull/206106
    
    closes odoo/odoo#207392
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/bus/static/tests/legacy/outdated_page_watcher_tests.js
+++ addons/bus/static/tests/legacy/outdated_page_watcher_tests.js
@@ -4,8 +4,9 @@ import { startServer } from "@bus/../tests/helpers/mock_python_environment";
 import { addBusServicesToRegistry } from "@bus/../tests/helpers/test_utils";
 import { waitForBusEvent } from "@bus/../tests/helpers/websocket_event_deferred";
 import { outdatedPageWatcherService } from "@bus/outdated_page_watcher_service";
+import { BACK_ONLINE_RECONNECT_DELAY } from "@bus/services/bus_service";
 import { WEBSOCKET_CLOSE_CODES } from "@bus/workers/websocket_worker";
-import { patchWithCleanup } from "@web/../tests/legacy/helpers/utils";
+import { mockTimeout, patchWithCleanup } from "@web/../tests/legacy/helpers/utils";
 import { assertSteps, click, contains, step } from "@web/../tests/legacy/utils";
 import { createWebClient } from "@web/../tests/webclient/helpers";
 import { browser } from "@web/core/browser/browser";
@@ -33,4 +34,26 @@ QUnit.test("disconnect during bus gc should ask for reload", async () => {
     patchWithCleanup(browser.location, { reload: () => step("reload") });
     await click(".o_notification button", { text: "Refresh" });
     await assertSteps(["reload"]);
+});
+
+QUnit.test("reconnect after going offline after bus gc should ask for reload", async () => {
+    addBusServicesToRegistry();
+    registry.category("services").add("bus.outdated_page_watcher", outdatedPageWatcherService);
+    const { advanceTime } = mockTimeout();
+    const { env } = await createWebClient({
+        mockRPC(route) {
+            if (route === "/bus/has_missed_notifications") {
+                return true;
+            }
+        },
+    });
+    env.services.bus_service.start();
+    await waitForBusEvent(env, "connect");
+    browser.dispatchEvent(new Event("offline"));
+    await waitForBusEvent(env, "disconnect");
+    browser.dispatchEvent(new Event("online"));
+    await advanceTime(BACK_ONLINE_RECONNECT_DELAY);
+    await contains(".o_notification", {
+        text: "Save your work and refresh to get the latest updates and avoid potential issues.",
+    });
 });
