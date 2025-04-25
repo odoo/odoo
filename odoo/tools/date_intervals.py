0a24f35e785192610cd0e8811e8e553214c22d11
Krzysztof Magusiak (krma) <krma@odoo.com>
2025-04-25 13:56:09 +0000

    [REF] tools: date_intervals basic typing info

--- odoo/tools/date_intervals.py
+++ odoo/tools/date_intervals.py
@@ -1,16 +1,25 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+from __future__ import annotations
 
 import math
+import typing
 import warnings
-from datetime import time
+from datetime import datetime, time
 from itertools import chain
+
 from pytz import utc
 
 from odoo.fields import Datetime
 from odoo.tools.float_utils import float_round
 
+if typing.TYPE_CHECKING:
+    from collections.abc import Callable, Iterable, Iterator
+    from collections.abc import Set as AbstractSet
 
-def make_aware(dt):
+T = typing.TypeVar('T')
+
+
+def make_aware(dt: datetime) -> tuple[datetime, Callable[[datetime], datetime]]:
     """ Return ``dt`` with an explicit timezone, together with a function to
         convert a datetime to the same (naive or aware) timezone as ``dt``.
     """
@@ -19,19 +28,19 @@ def make_aware(dt):
     return dt.replace(tzinfo=utc), lambda val: val.astimezone(utc).replace(tzinfo=None)
 
 
-def string_to_datetime(value):
+def string_to_datetime(value) -> datetime:
     """ Convert the given string value to a datetime in UTC. """
     warnings.warn("Since 19.0, use directly Datetime.from_string", DeprecationWarning)
     return utc.localize(Datetime.from_string(value))
 
 
-def datetime_to_string(dt):
+def datetime_to_string(dt: datetime) -> str:
     """ Convert the given datetime (converted in UTC) to a string value. """
     warnings.warn("Since 19.0, use directly Datetime.to_string with astimezone", DeprecationWarning)
     return Datetime.to_string(dt.astimezone(utc))
 
 
-def float_to_time(hours):
+def float_to_time(hours: float) -> time:
     """ Convert a number of hours into a time object. """
     if hours == 24.0:
         return time.max
@@ -39,7 +48,7 @@ def float_to_time(hours):
     return time(int(integral), int(float_round(60 * fractional, precision_digits=0)), 0)
 
 
-def _boundaries(intervals, opening, closing):
+def _boundaries(intervals: Intervals[T] | Iterable[tuple[T, T, AbstractSet]], opening: str, closing: str) -> Iterator[tuple[T, str, AbstractSet]]:
     """ Iterate on the boundaries of intervals. """
     for start, stop, recs in intervals:
         if start < stop:
@@ -47,27 +56,27 @@ def _boundaries(intervals, opening, closing):
             yield (stop, closing, recs)
 
 
-class Intervals(object):
+class Intervals(typing.Generic[T]):
     """ Collection of ordered disjoint intervals with some associated records.
         Each interval is a triple ``(start, stop, records)``, where ``records``
         is a recordset.
     """
     def __init__(self, intervals=()):
-        self._items = []
+        self._items: list[tuple[T, T, AbstractSet]] = []
         if intervals:
             # normalize the representation of intervals
             append = self._items.append
             starts = []
-            recses = []
+            items = []
             for value, flag, recs in sorted(_boundaries(intervals, 'start', 'stop')):
                 if flag == 'start':
                     starts.append(value)
-                    recses.append(recs)
+                    items.append(recs)
                 else:
                     start = starts.pop()
                     if not starts:
-                        append((start, value, recses[0].union(*recses)))
-                        recses.clear()
+                        append((start, value, items[0].union(*items)))
+                        items.clear()
 
     def __bool__(self):
         return bool(self._items)
@@ -93,7 +102,7 @@ class Intervals(object):
         """ Return the difference of two sets of intervals. """
         return self._merge(other, True)
 
-    def _merge(self, other, difference):
+    def _merge(self, other: Intervals | Iterable[tuple[T, T, AbstractSet]], difference: bool) -> Intervals:
         """ Return the difference or intersection of two sets of intervals. """
         result = Intervals()
         append = result._items.append
@@ -132,39 +141,43 @@ class Intervals(object):
         warnings.warn("Deprecated since 19.0, just iterate over Intervals", DeprecationWarning)
         return self._items
 
-def sum_intervals(intervals):
-    """ Sum the intervals duration (unit : hour)"""
+
+def sum_intervals(intervals: Intervals[datetime]) -> float:
+    """ Sum the intervals duration (unit: hour)"""
     return sum(
         (stop - start).total_seconds() / 3600
-        for start, stop, meta in intervals
+        for start, stop, _ in intervals
     )
 
-def timezone_datetime(time):
+
+def timezone_datetime(time: datetime) -> datetime:
     if not time.tzinfo:
         time = time.replace(tzinfo=utc)
     return time
 
-def intervals_overlap(interval_a, interval_b):
+
+def intervals_overlap(interval_a: tuple[datetime, datetime], interval_b: tuple[datetime, datetime]) -> bool:
     """Check whether an interval of time intersects another.
 
-    :param tuple[datetime, datetime] interval_a: Time range (ignored if 0-width)
-    :param tuple[datetime, datetime] interval_b: Time range
-    :return bool: True if two non-zero intervals overlap
+    :param interval_a: Time range (ignored if 0-width)
+    :param interval_b: Time range
+    :return: True if two non-zero intervals overlap
     """
     start_a, stop_a = tuple(timezone_datetime(i) for i in interval_a)
     start_b, stop_b = tuple(timezone_datetime(i) for i in interval_b)
     return start_a < stop_b and stop_a > start_b
 
-def invert_intervals(intervals, first_start, last_stop):
+
+def invert_intervals(intervals: Iterable[tuple[T, T]], first_start: T, last_stop: T) -> list[tuple[T, T]]:
     """Return the intervals between the intervals that were passed in.
 
     The expected use case is to turn "available intervals" into "unavailable intervals".
     :examples:
     ([(1, 2), (4, 5)], 0, 10) -> [(0, 1), (2, 4), (5, 10)]
 
-    :param iter[tuple[datetime, datetime]] intervals:
-    :param <datetime> first_start: date where the first interval should start
-    :param <datetime> last_stop: date where the last interval should stop
+    :param intervals:
+    :param first_start: start of whole interval
+    :param last_stop: stop of whole interval
     """
     items = []
     prev_stop = first_start
