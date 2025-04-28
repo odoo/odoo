f3d0504f652c1d41f1399b8b6a883e5a8ea25129
tsm-odoo <tsm@odoo.com>
2025-04-28 10:01:37 +0200

    [FIX] bus: detect lost notifications after reconnecting
    In [1], a mechanism was introduced to detect lost notifications when
    the bus table is cleared during socket disconnection. However, this
    relies on the "reconnect" event. The "reconnect" event is not triggered
    when the connection is closed cleanly. In such cases, the next
    connection is treated as a new one and triggers the "connect" event,
    which does not check for missed notifications.
    
    As a result, any notifications sent while the socket was disconnected
    can be missed.
    
    This commit ensures that the missed notification check is performed
    on all connections after the initial one, not only during a "reconnect".
    
    [1]: https://github.com/odoo/odoo/pull/206106
    
    X-original-commit: c89e72820488e8be2c67bfdc234973385b02e078

--- addons/bus/websocket.py
+++ addons/bus/websocket.py
@@ -904,7 +904,7 @@ class WebsocketConnectionHandler:
     # Latest version of the websocket worker. This version should be incremented
     # every time `websocket_worker.js` is modified to force the browser to fetch
     # the new worker bundle.
-    _VERSION = "18.0-3"
+    _VERSION = "saas-18.1-1"
 
     @classmethod
     def websocket_allowed(cls, request):
