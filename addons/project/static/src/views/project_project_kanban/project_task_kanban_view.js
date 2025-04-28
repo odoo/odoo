f9dbc647b904d1c23bf930df89c095935099cbcf
K Theja (kthe) <kthe@odoo.com>
2025-04-28 11:07:27 +0000

    [FIX] project: missing extended controller in kanban view
    The controller extended in the PR https://github.com/odoo/odoo/pull/194090 is
    not added in to the view.
    
    closes odoo/odoo#207842
    
    X-original-commit: 85121452de3b35001b828abd6f3aa625e1673c53
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>
    Signed-off-by: K Theja (kthe) <kthe@odoo.com>

--- addons/project/static/src/views/project_project_kanban/project_task_kanban_view.js
+++ addons/project/static/src/views/project_project_kanban/project_task_kanban_view.js
@@ -1,9 +1,11 @@
 import { registry } from "@web/core/registry";
 import { kanbanView } from "@web/views/kanban/kanban_view";
+import { ProjectKanbanController } from "./project_project_kanban_controller";
 import { ProjectProjectKanbanRenderer } from "./project_project_kanban_renderer";
 
 export const projectProjectKanbanView = {
     ...kanbanView,
+    Controller: ProjectKanbanController,
     Renderer: ProjectProjectKanbanRenderer,
 };
 
