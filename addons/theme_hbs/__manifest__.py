9047d6d7a38715b3c9d7b23682845541626ba45d
Sanjay Sharma <shsa@odoo.com>
2025-04-28 19:13:25 +0530

    [ADD] theme-hbs: new header, images, remove unecessary code

--- addons/theme_hbs/__manifest__.py
+++ addons/theme_hbs/__manifest__.py
@@ -1,27 +1,27 @@
 {
-    'name': 'Purple HBS',
+    'name': 'HBS',
     'description': 'A custom theme for Odoo',
     'version': '1.0',
     'author': 'Sanjay Sharma',
     'category': 'Theme/Creative',
-    'depends': ['website'],
+    'depends': ['website', 'website_sale'],
     'data': [
-        'views/snippets/s_sidegrid.xml',
-        'views/snippets/s_dynamic_snippet_products_preview_data.xml',
-        'views/footer.xml',
+        # 'views/snippets/s_sidegrid.xml',
+        # 'views/snippets/s_dynamic_snippet_products_preview_data.xml',
+        # 'views/footer.xml',
         'views/header.xml',
     ],
 
     'images': [
-        'static/description/theme_hbs.jpg',
-        # 'static/description/pexels-pixabay-433989.jpg', # any screenshot of website so created can act as a preview
+        'static/description/hbs_description.jpg', # any screenshot of website so created can act as a preview
+        'static/description/hbs_screenshot.jpg',
     ],
-    'images_preview_theme': {
-        # List of images changed in the theme: urls of the images
-    },
-    'configurator_snippets': {
-        'homepage': ['s_sidegrid'],
-    },
+    # 'images_preview_theme': {
+    #     # List of images changed in the theme: urls of the images
+    # },
+    # 'configurator_snippets': {
+    #     'homepage': ['s_sidegrid'],
+    # },
     'assets': {
         'web._assets_primary_variables': [
             'theme_hbs/static/src/scss/primary_variables.scss',
