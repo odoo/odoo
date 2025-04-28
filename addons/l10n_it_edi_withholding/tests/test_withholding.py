811806e56b50643f07b1b849386370b2f386b8f7
Paolo Gatti <pgi@odoo.com>
2025-04-28 21:33:20 +0000

    [FIX] l10n_it_edi: Import Pension fund tax without AswCassPre
    PR #170012 modified the application of Pension Fund taxes to invoice lines.
    
    After the PR, only lines with:
    `.//AltriDatiGestionale/TipoDato[contains(text(), 'AswCassPre')]`
    and the correct kind in the `.//AltriDatiGestionale/RiferimentoTesto`
    tag, got their Pension Fund tax applied.
    
    'AswCassPre' is an additional tag belonging to a "best practice"
    standard agreement from AssoSoftware. These may be best practices,
    but they're not at all mandatory to use the SdI, so it happens that
    invoices do not follow these additional specifications.
    
    For everyone else, the Pension Fund tax must be applied on all lines
    where `.//DatiGeneraliDocumento/DatiCassaPrevidenziale/AliquotaIVA` tag
    matches the VAT tax of the line.
    
    This PR adds code that tells one case from the other.
    
    task-4381089
    
    closes odoo/odoo#210081
    
    X-original-commit: 3e0c511174500b11dde7be4a48c31a5e2934ca49
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: Paolo Gatti (pgi) <pgi@odoo.com>

--- addons/l10n_it_edi_withholding/tests/test_withholding.py
+++ addons/l10n_it_edi_withholding/tests/test_withholding.py
@@ -21,12 +21,14 @@ class TestWithholdingAndPensionFundTaxes(TestItEdi):
             return cls.env['account.chart.template'].with_company(cls.company).ref(ref_name)
 
         cls.withholding_sale_tax = find_tax_by_ref('20vwc')
+        cls.withholding_purchase_tax = find_tax_by_ref('20awc')
         cls.withholding_sale_tax_23 = find_tax_by_ref('23vwo')
         cls.pension_fund_sale_tax = find_tax_by_ref('4vcp')
         cls.enasarco_sale_tax = find_tax_by_ref('enasarcov')
         cls.withholding_purchase_tax_23 = find_tax_by_ref('23awo')
         cls.enasarco_purchase_tax = find_tax_by_ref('enasarcoa')
         cls.inps_tax = find_tax_by_ref('4vinps')
+        cls.inps_purchase_tax = find_tax_by_ref('4ainps')
 
         cls.zero_tax = cls.env['account.tax'].with_company(cls.company).create({
             'name': 'ZeroTax',
@@ -235,7 +237,7 @@ class TestWithholdingAndPensionFundTaxes(TestItEdi):
         """
         self._assert_export_invoice(self.pension_fund_tax_invoice, 'pension_fund_tax_invoice.xml')
 
-    def test_pension_fund_taxes_import(self):
+    def test_pension_fund_taxes_import_assosoftware_tag(self):
         invoice = self._assert_import_invoice('IT00470550013_pfund.xml', [{
             'invoice_date': fields.Date.from_string('2022-03-24'),
             'amount_untaxed': 750.0,
@@ -246,13 +248,29 @@ class TestWithholdingAndPensionFundTaxes(TestItEdi):
                 'price_unit': price_unit,
             } for name, price_unit in self.get_real_client_invoice_data().lines]
         }])
+        for line in invoice.line_ids.filtered(lambda x: x.display_type == 'product'):
+            self.assertEqual(line.tax_ids, (
+                self.inps_purchase_tax
+                | self.withholding_purchase_tax
+                | self.company.account_purchase_tax_id
+            ))
 
+    def test_pension_fund_taxes_import(self):
         invoice_data = self.get_real_client_invoice_data()
-        for line in invoice.line_ids.filtered(lambda x: x.name in [data[0] for data in invoice_data.lines]):
-            withholding_taxes = line.tax_ids.filtered(lambda x: x.l10n_it_withholding_type)
-            pension_fund_taxes = line.tax_ids.filtered(lambda x: x.l10n_it_pension_fund_type)
-            vat_taxes = line.tax_ids - withholding_taxes - pension_fund_taxes
-            self.assertEqual([1, 1, 1], [len(x) for x in (vat_taxes, withholding_taxes, pension_fund_taxes)])
+        invoice = self._assert_import_invoice('IT00470550013_pfun2.xml', [{
+            'invoice_date': datetime.date(2022, 3, 24),
+            'invoice_date_due': datetime.date(2022, 3, 24),
+            'invoice_line_ids': [{
+                'name': name,
+                'price_unit': price,
+            } for name, price in invoice_data.lines]
+        }])
+        for line in invoice.line_ids.filtered(lambda x: x.display_type == 'product'):
+            self.assertEqual(line.tax_ids, (
+                self.inps_purchase_tax
+                | self.withholding_purchase_tax
+                | self.company.account_purchase_tax_id
+            ))
 
     ####################################################
     # ENASARCO TAX
