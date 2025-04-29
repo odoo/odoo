fe150e74049751140a53e48f7eb55b8997d73978
rame-odoo <rame@odoo.com>
2025-04-29 07:01:46 +0000

    [FIX] hr_holidays: prevent unallocated time off creation via smart button
    Steps:
    - Navigate to Time Off > Configuration > Time Off Types.
    - Open a time off type that requires allocation.
    - Click on the 'Time Off' smart button and try to create a request.
    
    Issues:
    - Employees could create time off requests via the smart button even without an
      approved allocation.
    - This bypassed the existing restriction enforced in the standard time off
      request creation flow.
    
    Fix:
    - Added a constraint on the time off model to validate allocations even when
      requests are created via the smart button.
    - Ensured validation covers both allocation presence and allowed negative leaves.
    - Raised a ValidationError when no valid allocation is found.
    - Added a test case to ensure constraint behaves correctly.
    - Adjusted some of tests to comply with the new validation.
    
    Task - 4671236
    
    X-original-commit: e0b87585226d1e2301623f5886ad160432d42c2c

--- addons/hr_holidays_contract/tests/test_multi_contract.py
+++ addons/hr_holidays_contract/tests/test_multi_contract.py
@@ -285,7 +285,7 @@ class TestHolidaysMultiContract(TestHolidayContract):
         leave_type = self.env['hr.leave.type'].create({
             'name': 'Leave Type',
             'time_type': 'leave',
-            'requires_allocation': 'yes',
+            'requires_allocation': 'no',
             'leave_validation_type': 'hr',
             'request_unit': 'day',
         })
