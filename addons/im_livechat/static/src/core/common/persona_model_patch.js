16c5e19a611d4e50a70e175009bec71468224622
tsm-odoo <tsm@odoo.com>
2025-04-29 11:55:01 +0000

    [FIX] im_livechat: user custom live chat name for reactions
    Before this PR, real user name was displayed in the message reaction
    summary. We should use the custom live chat name if any not to leak
    the real name of the agent. This PR fixes this issue.
    
    closes odoo/odoo#208012
    
    X-original-commit: 1c11433abcb3b057bbd5d9bc718c4fb85d4074e0
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/im_livechat/static/src/core/common/persona_model_patch.js
+++ addons/im_livechat/static/src/core/common/persona_model_patch.js
@@ -9,5 +9,8 @@ const personaPatch = {
         this.livechat_languages = [];
         this.livechat_expertise = [];
     },
+    _computeDisplayName() {
+        return super._computeDisplayName() || this.user_livechat_username;
+    },
 };
 patch(Persona.prototype, personaPatch);
