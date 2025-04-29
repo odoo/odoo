0cba7b8935d9b5b33ad853b091b64127d71f8324
Ethan Vincent <etvi@odoo.com>
2025-04-29 15:27:53 +0200

    [FIX] sale_mrp: find `bom_line_id` for kit comp. move run_pull
    **Current behavior:**
    Using the return & exchange functionality when the product to
    exchange is a kit's component will not correctly update the SOL
    `qty_delivered` field upon validation of the exchange (out)
    picking.
    
    **Expected behavior:**
    accurate `qty_delivered`
    
    **Steps to reproduce:**
    1. Create a product w/ kit bom like:
    * 2 units componentA
    * 1 units componentB
    
    2. Sell 1 unit of the kit product, validate delivery
    
    3. Create a return for the 1 unit of componentB and select the
    exchange option on the wizard
    
    4. Validate the return, then validate the exchange
    
    5. Look at the sale order line `qty_delivered` -> it's 0
    
    **Cause of the issue:**
    The exchange picking move doesn't get a `bom_line_id` and thus
    is not properly aggregated in the computation of qty_delivered
    when incoming and outgoing quantities are being calculated
    (wherein these quantities must offset, but can't in this
    situation).
    
    **Fix:**
    When the procurement occurs to create the exchanging move, find
    the `bom_line_id` by looking at the original sale line's moves'
    bom's bom lines.
    
    opw-4676609
    
    closes odoo/odoo#207955
    
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/sale_mrp/models/stock_rule.py
+++ addons/sale_mrp/models/stock_rule.py
@@ -9,3 +9,15 @@ class StockRule(models.Model):
         if values.get('sale_line_id'):
             res['sale_line_id'] = values['sale_line_id']
         return res
+
+    def _get_stock_move_values(self, product_id, product_qty, product_uom, location_dest_id, name, origin, company_id, values):
+        move_values = super()._get_stock_move_values(product_id, product_qty, product_uom, location_dest_id, name, origin, company_id, values)
+        if (sol_id := values.get('sale_line_id')) is not None and 'product_id' in move_values:
+            # if the SOL is for a kit
+            if move_values['product_id'] != self.env['sale.order.line'].browse(sol_id).product_id.id:
+                bom_line_id = self.env['sale.order.line'].browse(sol_id).move_ids.bom_line_id.filtered(
+                    lambda bl: bl.product_id.id == move_values.get('product_id')
+                ).id
+                if bom_line_id:
+                    move_values['bom_line_id'] = bom_line_id
+        return move_values
