0cf9ba6363dfcf2d573dc546e92cbad0c0d812e3
Louis Travaux <lotr@odoo.com>
2025-04-29 12:31:36 +0000

    [FIX] point_of_sale: add iot_base to IoT Box sparse-checkout
    When checking out `saas-18.3`, IoT Box images until `25.01` are missing `iot_base`, resulting in
    odoo not being able to start.
    
    X-original-commit: bb92fd810cfac6c27ba1edfb6e792bf67f7b1750

---
+++ addons/point_of_sale/tools/posbox/configuration/posbox_update.sh
@@ -0,0 +1,29 @@
+<<<<<<< HEAD
+||||||| MERGE BASE
+=======
+#!/usr/bin/env bash
+
+sudo mount -o remount,rw /
+
+sudo service odoo stop
+
+cd /home/pi/odoo
+localbranch=$(git symbolic-ref -q --short HEAD)
+localremote=$(git config branch.$localbranch.remote)
+
+echo "addons/point_of_sale/tools/posbox/overwrite_after_init/home/pi/odoo" >> .git/info/sparse-checkout
+echo "addons/iot_base" >> .git/info/sparse-checkout
+
+git fetch "${localremote}" "${localbranch}" --depth=1
+git reset "${localremote}"/"${localbranch}" --hard
+
+git clean -dfx
+cp -a /home/pi/odoo/addons/point_of_sale/tools/posbox/overwrite_after_init/home/pi/odoo/* /home/pi/odoo/
+rm -r /home/pi/odoo/addons/point_of_sale/tools/posbox/overwrite_after_init
+
+sudo mount -o remount,ro /
+sudo mount -o remount,rw /root_bypass_ramdisks/etc/cups
+
+(sleep 5 && sudo service odoo restart) &
+
+>>>>>>> FORWARD PORTED
