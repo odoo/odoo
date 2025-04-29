71934a02083eb232cdbd182712efd3970dfefcdf
Anh Thao Pham (pta) <pta@odoo.com>
2025-04-29 08:52:48 +0000

    [FIX] account: fix invoice sync when changing currency rate and price
    Steps to reproduce:
    - Install account
    - Activate a foreign currency (e.g. EUR)
    - Set at least 2 different currency rates for date1 and date2
    - Create an invoice:
      * Customer: [any]
      * Invoice Date: date1
      * Currency: EUR
      * Invoice line: [any]
    - Save the invoice
    - Check debit and credit amounts in "Journal Items" tab
    - Change the price of the product and set "Invoice Date" to date2
    - Save the invoice
    - Check debit and credit amounts in "Journal Items" tab
    
    Issue:
    The debit and credit amounts have been recomputed with the new currency
    rate, but the conversion has been applied on the old price.
    
    Cause:
    In "_sync_invoice" method, when the currency rate has changed, balance
    (and subsequently debit/credit) is recomputed from amount_currency.
    However, in this case, balance should be recomputed completely because
    the line subtotal has changed and amount_currency is not up-to-date
    with it.
    
    Solution:
    Do not recompute balance based on amount_currency when price_subtotal
    of a line has changed.
    In that case, it will be recomputed anyway.
    
    opw-4658156
    
    X-original-commit: 596c28f80dbf0c43615842995e66c1f248c14b45
    Co-authored-by: Anh Thao Pham (pta) <pta@odoo.com>
    Co-authored-by: william-andre <wan@odoo.com>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -2965,9 +2965,19 @@ class AccountMove(models.Model):
                 # A base line has been modified.
                 round_from_tax_lines = (
                     # The changed lines don't affect the taxes.
-                    all(not line.tax_ids and not move_base_lines_values_before.get(line, {}).get('tax_ids') for line in changed_lines)
+                    all(
+                        not line.tax_ids and not move_base_lines_values_before.get(line, {}).get('tax_ids')
+                        for line in changed_lines
+                    )
                     # Keep the tax lines amounts if an amount has been manually computed.
-                    or any_field_has_changed(move_tax_lines_values_before, tax_lines)
+                    or (
+                        list(move_tax_lines_values_before) != list(tax_lines)
+                        or any(
+                            self.env.is_protected(line._fields[fname], line)
+                            for line in tax_lines
+                            for fname in move_tax_lines_values_before[line]
+                        )
+                    )
                 )
 
                 # If the move has been created with all lines including the tax ones and the balance/amount_currency are provided on
