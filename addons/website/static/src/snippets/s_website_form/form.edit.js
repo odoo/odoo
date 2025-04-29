7740a1110e36aea0b580126f97cf060b1faf888d
Mehdi Outagant (MOU) <mou@odoo.com>
2025-04-29 16:35:40 +0000

    [FIX] website, *: prevent the removal of the form's default values
    *: website_hr_recruitment
    
    Steps to reproduce [17.1]:
    
    - Add a property field to a job application in the backend.
    - Go to the linked website job application form (in edit mode).
    - The property field is inaccessible.
    
    To be able to access the new property field in the application form, the
    `job_id` value of the related job record needs to be specified by
    default. Unfortunately, these values are removed before accessing "edit"
    mode:
    
    Starting from [1], a fix was added to clear default values coming from
    `data-for` attributes when the form is edited (at `cleanForSave()`) to
    prevent saving it with changed default values.
    
    Since the code from [1] can also remove default values by the user,
    a new fix (see [2]) updated this implementation to ensure that only
    values from auto-fill and data-for fields will be removed (this time,
    when the form public widget is destroyed [A]).
    
    The behaviour from [A] will always unlink the `data-for` values in edit
    mode, and as a consequence, the related property fields won't be
    correctly retrieved.
    
    The goal of this commit is to fix this behaviour by excluding `data-for`
    values from the reset in [A]. An extra check will be added to be sure
    the form won't be saved with updated default values if it already has
    `data-for` ones [B].
    
    Important: The diff from [3] made the `email_to` field as an exception
    for the data-for/prefill priority system and only used the `data-for`
    value if nothing has been configured by the user (it also used the
    `data-for` value if what was configured by the user is the dummy default
    email: `info@yourcompany.example.com`). That's why we need to
    exceptionally allow saving the default value added by the user in the
    check from [B] to preserve the same behavior.
    
    Following [4], the `email_to` value won't be kept in edit mode to
    prevent breaking its dynamic behavior.
    
    [1]: https://github.com/odoo/odoo/commit/b637a5e32f767b62736241042f88fa0cecf9f10b
    [2]: https://github.com/odoo/odoo/commit/043e1fdf923d2037dd8da128ab99388f0c92e544
    [3]: https://github.com/odoo/odoo/commit/a08574a04ff2ce8ea2d6fd2b79f56cc57bba953b
    [4]: https://github.com/odoo/odoo/commit/6658d61e304c9dfd8e512648c4a001dcaac44bc1
    
    task-3922573
    
    X-original-commit: f0473fdefc700a4a731989d62d381f6ad20edc98

--- addons/website/static/src/snippets/s_website_form/form.edit.js
+++ addons/website/static/src/snippets/s_website_form/form.edit.js
@@ -6,28 +6,59 @@ import {
     formatDate,
     formatDateTime,
 } from "@web/core/l10n/dates";
+import wUtils from "@website/js/utils";
+
 const { DateTime } = luxon;
 
-export class FormDateFormatterEdit extends Interaction {
-    static selector = ".s_website_form form .s_website_form_field .s_website_form_input.datetimepicker-input, form.s_website_form .s_website_form_field .s_website_form_input.datetimepicker-input"; // !compatibility
-
+export class FormEdit extends Interaction {
+    static selector = ".s_website_form form, form.s_website_form"; // !compatibility
     start() {
-        // We do not initialize the datetime picker in edit mode but want the dates to be formatted.
-        const value = this.el.getAttribute("value");
-        if (value) {
-            const format =
-                this.el.closest(".s_website_form_field").dataset.type === "date"
-                    ? formatDate
-                    : formatDateTime;
-            this.el.value = format(DateTime.fromSeconds(parseInt(value)));
+        // The "data-for" values were removed (on destroy before saving),
+        // but we still need to restore them in edit mode in the case of
+        // a simple widget refresh.
+        this.dataForValues = wUtils.getParsedDataFor(this.el.id, this.el.ownerDocument);
+        for (const fieldEl of this._getDataForFields()) {
+            if (!fieldEl.getAttribute("value")) {
+                fieldEl.setAttribute("value", this.dataForValues[fieldEl.name]);
+            }
         }
+
+        // We do not initialize the datetime picker in edit mode but want the dates to be formatted.
+        for (const el of this.el.querySelectorAll(".s_website_form_input.datetimepicker-input")) {
+            const value = el.getAttribute("value");
+            if (value) {
+                const format =
+                    el.closest(".s_website_form_field").dataset.type === "date"
+                        ? formatDate
+                        : formatDateTime;
+                el.value = format(DateTime.fromSeconds(parseInt(value)));
+            }
+        }
+    }
+
+    destroy() {
+        // The "data-for" values are always correctly added to the form on the
+        // form interaction start. But if we make any change to it in "edit"
+        // mode, we need to be sure it will not be saved with the new values.
+        for (const fieldEl of this._getDataForFields()) {
+            fieldEl.removeAttribute("value");
+        }
+    }
+
+    _getDataForFields() {
+        if (!this.dataForValues) {
+            return [];
+        }
+        return Object.keys(this.dataForValues)
+            .map(name => this.el.querySelector(`[name="${CSS.escape(name)}"]`))
+            .filter(dataForValuesFieldEl => dataForValuesFieldEl && dataForValuesFieldEl.name !== "email_to");
     }
 }
 
 registry
     .category("public.interactions.edit")
-    .add("website.form_date_formatter", {
-        Interaction: FormDateFormatterEdit,
+    .add("website.form", {
+        Interaction: FormEdit,
     });
 
 // Translation mode.
