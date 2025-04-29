200cc351ecd488fdeb0ae55163d11585861428fd
Xavier-Do <xdo@odoo.com>
2025-04-29 16:32:28 +0200

    [FIX] tests: avoid TestCursor corruption
    Build error 162704
    
    Follow-up on other WebSocket fixes, this time for the execution of the
    WebSocket request inside the same test, but not after a Chrome browser.
    
    This case is quite specific and is related to a low timeout set on
    websocket_connect, leading to the request being randomly executed inside
    the test. This is a problem for the current test (fixed by increasing
    the timeout), but also for the next test: if the TestCursor rollback
    fails, the cursor_stack of the TestCursor is not emptied, leading to a
    case where an existing read-only test cursor in the stack makes the next
    TestCursor read-only, causing chain failures.
    
    closes odoo/odoo#207974
    
    Signed-off-by: Xavier Dollé (xdo) <xdo@odoo.com>

--- odoo/sql_db.py
+++ odoo/sql_db.py
@@ -580,15 +580,16 @@ class TestCursor(BaseCursor):
 
     def close(self):
         if not self._closed:
-            self.rollback()
-            self._closed = True
-            if self._savepoint:
-                self._savepoint.close(rollback=False)
-
-            tos = self._cursors_stack.pop()
-            if tos is not self:
-                _logger.warning("Found different un-closed cursor when trying to close %s: %s", self, tos)
-            self._lock.release()
+            try:
+                self.rollback()
+                if self._savepoint:
+                    self._savepoint.close(rollback=False)
+            finally:
+                self._closed = True
+                tos = self._cursors_stack.pop()
+                if tos is not self:
+                    _logger.warning("Found different un-closed cursor when trying to close %s: %s", self, tos)
+                self._lock.release()
 
     def commit(self):
         """ Perform an SQL `COMMIT` """
