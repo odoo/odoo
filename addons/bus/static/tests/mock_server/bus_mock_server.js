362dff75166e4dd56e28ac4c975663f67f868ad5
tsm-odoo <tsm@odoo.com>
2025-04-29 16:23:21 +0200

    [FIX] bus: detect lost notifications after reconnecting
    In [1], a mechanism was introduced to detect lost notifications when
    the bus table is cleared during socket disconnection. However, this
    relies on the "reconnect" event. The "reconnect" event is not triggered
    when the connection is closed cleanly. In such cases, the next
    connection is treated as a new one and triggers the "connect" event,
    which does not check for missed notifications.
    
    As a result, any notifications sent while the socket was disconnected
    can be missed.
    
    This commit ensures that the missed notification check is performed
    on all connections after the initial one, not only during a "reconnect".
    
    [1]: odoo#206106
    
    closes odoo/odoo#207940
    
    X-original-commit: c781c2b
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/bus/static/tests/mock_server/bus_mock_server.js
+++ addons/bus/static/tests/mock_server/bus_mock_server.js
@@ -1,10 +1,3 @@
-import { serializeDateTime } from "@web/core/l10n/dates";
 import { registry } from "@web/core/registry";
 
-registry
-    .category("mock_rpc")
-    .add("/bus/get_autovacuum_info", () => ({
-        lastcall: serializeDateTime(luxon.DateTime.now().minus({ days: 1 }).toUTC()),
-        nextcall: serializeDateTime(luxon.DateTime.now().plus({ days: 1 }).toUTC()),
-    }))
-    .add("/bus/has_missed_notifications", () => false);
+registry.category("mock_rpc").add("/bus/has_missed_notifications", () => false);
