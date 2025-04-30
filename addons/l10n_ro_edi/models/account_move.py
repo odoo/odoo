e1d5a4b39ad006f574c48b55dd397d47a26db5e2
Antoine Boonen <aboo@odoo.com>
2025-04-30 11:53:04 +0200

    [IMP] l10n_ro_edi: synchronize with ANAF
    This commit adds the possibility to synchronize invoice and bill status
    at once.
    
    The endpoint `ListaMesajeFactura` returns a list of messages to process.
    Those messages can:
    - Return an invoice status update with a success or an error;
    - Return a new bill from another vendor already validated by the
    authority.
    
    The messages contain a message ID to recover and download the official
    data from the ANAF server: the invoice/bill XML and the signature.
    
    (documentation)[https://mfinante.gov.ro/static/10/eFactura/prezentare%20api%20efactura.pdf]
    
    When synchronizing, we retrieve the new messages from the last 24 hours
    and update the database content accordingly:
    - in case of invoice status update, update the relevant invoice/bill
    status and download the signature in case of success
    (note that this was already done before, we only call the relevant
    methods to do so);
    - in case of a new vendor bill, we download the bill XML and signature,
    create a new in_invoice move and fill it up with the XML content. In
    case a bill with the same index is already in the database, we do
    nothing to avoid duplicate. If a similar bill (same date and amount) is
    found but not yet validated, we only validate it.
    
    The synchronization can be triggered manually through the option in the
    list view and it is automatically triggered every 4 hours with a CRON.
    
    Lastly, the created bill journal can be set up in the Accounting
    settings so that users can specify their favorite journals for automated
    ANAF bills.
    
    task-4588800
    
    closes odoo/odoo#208082
    
    Related: odoo/upgrade#7673
    Signed-off-by: Ruben Gomes (rugo) <rugo@odoo.com>

--- addons/l10n_ro_edi/models/account_move.py
+++ addons/l10n_ro_edi/models/account_move.py
@@ -3,12 +3,17 @@ import base64
 import requests
 
 from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+from odoo.fields import Domain
 
 from .utils import (
     _request_ciusro_download_answer,
     _request_ciusro_fetch_status,
     _request_ciusro_send_invoice,
+    _request_ciusro_synchronize_invoices,
 )
+
+HOLDING_DAYS = 3  # Arbitrary
 
 
 class AccountMove(models.Model):
@@ -56,6 +61,22 @@ class AccountMove(models.Model):
         for move in self:
             if move.l10n_ro_edi_state:
                 move.show_reset_to_draft_button = False
+
+    ################################################################################
+    # EDI
+    ################################################################################
+
+    def _get_import_file_type(self, file_data):
+        """ Identify OIOUBL files. """
+        # EXTENDS 'account'
+        if (
+            file_data['xml_tree'] is not None
+            and (customization_id := file_data['xml_tree'].findtext('{*}CustomizationID'))
+            and 'CIUS-RO' in customization_id
+        ):
+            return 'account.edi.xml.ubl_ro'
+
+        return super()._get_import_file_type(file_data)
 
     ################################################################################
     # Send Logics
@@ -205,3 +226,271 @@ class AccountMove(models.Model):
         self.env['l10n_ro_edi.document'].sudo().create(documents_to_create)
         if self._can_commit():
             self._cr.commit()
+
+    @api.model
+    def _l10n_ro_edi_fetch_invoices(self):
+        """ Synchronize bills/invoices from SPV """
+        result = _request_ciusro_synchronize_invoices(
+            company=self.env.company,
+            session=requests.Session(),
+        )
+        if 'error' in result:
+            raise UserError(result['error'])
+
+        if result['sent_invoices_accepted_messages']:
+            self._l10n_ro_edi_process_invoice_accepted_messages(result['sent_invoices_accepted_messages'])
+
+        if result['sent_invoices_refused_messages']:
+            self._l10n_ro_edi_process_invoice_refused_messages(result['sent_invoices_refused_messages'])
+
+        if result['received_bills_messages']:
+            self._l10n_ro_edi_process_bill_messages(result['received_bills_messages'])
+
+        # Non-indexed moves that were not processed after some time have probably been refused by the SPV. Since
+        # there is no way to recover the index for refused invoices, we simply refuse them manually without proper reason.
+        domain = (
+            Domain('l10n_ro_edi_index', '=', False)
+            & Domain('l10n_ro_edi_state', '=', 'invoice_not_indexed')
+        )
+        non_indexed_invoices = self.env['account.move'].search(domain)
+
+        document_ids_to_delete = []
+        for invoice in non_indexed_invoices:
+            # At that point, only one sent document should exists on an invoice
+            sent_document = invoice.l10n_ro_edi_document_ids
+
+            if (fields.Datetime.today() - sent_document.create_date).days > HOLDING_DAYS:
+                document_ids_to_delete += invoice.l10n_ro_edi_document_ids.ids
+
+                error_message = _(
+                    "The invoice has probably been refused by the SPV. We were unable to recover the reason of the refusal because "
+                    "the invoice had not received its index. Duplicate the invoice and attempt to send it again."
+                )
+                invoice.message_post(body=error_message)
+                self.env['l10n_ro_edi.document'].sudo().create({
+                    'invoice_id': invoice.id,
+                    'state': 'invoice_refused',
+                    'message': error_message,
+                })
+
+        self.env['l10n_ro_edi.document'].sudo().browse(document_ids_to_delete).unlink()
+
+        if self._can_commit():
+            self._cr.commit()
+
+    @api.model
+    def _l10n_ro_edi_process_invoice_accepted_messages(self, sent_invoices_accepted_messages):
+        ''' Process the validation messages of invoices sent
+
+            It will also attempt to recover the original invoices, that are missing their index,
+            by matching the name returned by the server and the one in the database.
+
+            note: There is an edge case where 2 messages have the same invoice name but different indexes in
+            their data; this could be due to a resequencing of the invoice and/or re-sending of an invoice. In
+            that case coupled with name matching where none of the two invoices received an index, all signatures
+            are added to the invoice; the user will have to manually update/select the correct one.
+
+            For example: 2 invoices in the database
+                - 11 already sent and should have gotten index AA, but did not receive it
+                - 12 not sent
+            Resequence them: 11->12 and 12->11
+            Send new 11 that has not yet been sent, it should have gotten index AB but did not receive it.
+            => In the messages, 2 invoices with name 11 and both index AA and AB.
+        '''
+        invoice_names = {message['answer']['invoice']['name'] for message in sent_invoices_accepted_messages if 'error' not in message}
+        invoice_indexes = [message['id_solicitare'] for message in sent_invoices_accepted_messages]
+        domain = (
+            Domain('move_type', 'in', self.get_sale_types())
+            & (
+                (
+                    Domain('l10n_ro_edi_index', 'in', invoice_indexes)
+                    & Domain('l10n_ro_edi_state', '=', 'invoice_sent')
+                )
+                | (
+                    Domain('name', 'in', list(invoice_names))
+                    & Domain('l10n_ro_edi_index', '=', False)
+                    & Domain('l10n_ro_edi_state', '=', 'invoice_not_indexed')
+                )
+            )
+        )
+        invoices = self.env['account.move'].search(domain)
+
+        document_ids_to_delete = []
+        index_to_move = {move.l10n_ro_edi_index: move for move in invoices}
+        name_to_move = {move.name: move for move in invoices}
+        for message in sent_invoices_accepted_messages:
+            invoice = index_to_move.get(message['id_solicitare'])
+
+            if not invoice:
+                # The move related to the message does not have an index
+                if 'error' in message or not name_to_move.get(message['answer']['invoice']['name']):
+                    continue
+
+                # An invoice with the same name has been found
+                invoice = name_to_move.get(message['answer']['invoice']['name'])
+
+                # Update the index of invoices succesfully sent but without SPV indexes due to server
+                # time-out for unknown reasons during the upload
+                invoice.l10n_ro_edi_index = message['id_solicitare']
+                invoice.l10n_ro_edi_state = 'invoice_sent'
+
+            if 'error' in message:
+                invoice.message_post(body=_(
+                    "Error when trying to download the E-Factura data from the SPV: %s",
+                    message['error']
+                ))
+                continue
+
+            # Only delete invoice_sent documents and not all because one invoice can contain several signature due to
+            # the edge case where 2 messages have the same invoice name but different indexes in their data; this could
+            # be due to a resequencing of the invoice and/or re-sending of an invoice. In that case coupled with name
+            # matching where none of the two invoices received an index, all signatures are added to the invoice; the
+            # user will have to manually update/select the correct one.
+            document_ids_to_delete += invoice.l10n_ro_edi_document_ids.filtered(lambda document: document.state == 'invoice_sent').ids
+
+            invoice.message_post(body=_("This invoice has been accepted by the SPV."))
+            self.env['l10n_ro_edi.document'].sudo().create({
+                'invoice_id': invoice.id,
+                'state': 'invoice_validated',
+                'key_download': message['id'],
+                'key_signature': message['answer']['signature']['key_signature'],
+                'key_certificate': message['answer']['signature']['key_certificate'],
+                'attachment': message['answer']['signature']['attachment_raw'],
+            })
+
+        self.env['l10n_ro_edi.document'].sudo().browse(document_ids_to_delete).unlink()
+
+    @api.model
+    def _l10n_ro_edi_process_invoice_refused_messages(self, sent_invoices_refused_messages):
+        ''' Process the refusal messages of invoices sent
+
+            For refused invoices, it is impossible to recover the original invoice from the message content like
+            in `_l10n_ro_edi_process_invoice_accepted_messages` since the message only contains the index and
+            error message (as relevant information).
+        '''
+        refused_invoice_indexes = [message['id_solicitare'] for message in sent_invoices_refused_messages]
+        domain = (
+            Domain('move_type', 'in', self.get_sale_types())
+            & Domain('l10n_ro_edi_index', 'in', refused_invoice_indexes)
+            & Domain('l10n_ro_edi_state', '=', 'invoice_sent')
+        )
+        invoices = self.env['account.move'].search(domain)
+        index_to_move = {move.l10n_ro_edi_index: move for move in invoices}
+
+        document_ids_to_delete = []
+        for message in sent_invoices_refused_messages:
+            invoice = index_to_move.get(message['id_solicitare'])
+            if not invoice:
+                continue
+
+            if 'error' in message:
+                invoice.message_post(body=_(
+                    "Error when trying to download the E-Factura data from the SPV: %s",
+                    message['error']
+                ))
+                continue
+
+            document_ids_to_delete += invoice.l10n_ro_edi_document_ids.ids
+
+            error_message = message['answer']['invoice']['error'].replace('\t', '')
+            invoice.message_post(body=_(
+                "This invoice was refused by the SPV for the following reason: %s",
+                error_message
+            ))
+            self.env['l10n_ro_edi.document'].sudo().create({
+                'invoice_id': invoice.id,
+                'state': 'invoice_refused',
+                'message': error_message,
+                'key_download': message['id'],
+                'key_signature': message['answer']['signature']['key_signature'],
+                'key_certificate': message['answer']['signature']['key_certificate'],
+                'attachment': message['answer']['signature']['attachment_raw'],
+            })
+
+        self.env['l10n_ro_edi.document'].sudo().browse(document_ids_to_delete).unlink()
+
+    @api.model
+    def _l10n_ro_edi_process_bill_messages(self, received_bills_messages):
+        ''' Create bill received on the SPV, it it does not already exists.
+        '''
+        # Search potential similar bills: similar bills either:
+        # - have an index that is present in the message data or,
+        # - the same amount and seller VAT, and optionally the same bill date
+        domain = (
+            Domain('move_type', 'in', self.get_purchase_types())
+            & (
+                (
+                    Domain('l10n_ro_edi_index', '=', False)
+                    & Domain('l10n_ro_edi_state', '=', False)
+                    & Domain.OR([
+                        Domain('amount_total', '=', message['answer']['invoice']['amount_total'])
+                        & Domain('commercial_partner_id.vat', '=', message['answer']['invoice']['seller_vat'])
+                        & Domain('invoice_date', 'in', [message['answer']['invoice']['date'], False])
+                        for message in received_bills_messages
+                        if 'error' not in message
+                    ])
+                )
+                | (
+                    Domain('l10n_ro_edi_index', 'in', [message['id_solicitare'] for message in received_bills_messages])
+                    & Domain('l10n_ro_edi_state', '=', 'invoice_validated')
+                )
+            )
+        )
+        similar_bills = self.env['account.move'].search(domain)
+
+        indexed_similar_bills = similar_bills.filtered('l10n_ro_edi_index').mapped('l10n_ro_edi_index')
+        non_indexed_similar_bills_dict = {
+            (bill.commercial_partner_id.vat, bill.amount_total, bill.invoice_date): bill
+            for bill in similar_bills
+            if not bill.l10n_ro_edi_index
+        }
+
+        for message in received_bills_messages:
+            if 'error' in message:
+                continue
+
+            if message['id_solicitare'] in indexed_similar_bills:
+                # A bill with the same SPV index was already imported, skip it as we don't want it twice.
+                continue
+
+            # Create new bills if they don't already exist, else update their content
+            bill = non_indexed_similar_bills_dict.get(
+                (message['answer']['invoice']['seller_vat'], float(message['answer']['invoice']['amount_total']), message['answer']['invoice']['date'])
+            )
+            if not bill:
+                bill = non_indexed_similar_bills_dict.get(
+                (message['answer']['invoice']['seller_vat'], float(message['answer']['invoice']['amount_total']), False)
+            )
+            if not bill:
+                bill = self.env['account.move'].create({
+                'company_id': self.env.company.id,
+                'move_type': 'in_invoice',
+                'journal_id': self.env.company.l10n_ro_edi_anaf_imported_inv_journal_id.id,
+            })
+
+            bill.l10n_ro_edi_index = message['id_solicitare']
+
+            self.env['l10n_ro_edi.document'].sudo().create({
+                'invoice_id': bill.id,
+                'state': 'invoice_validated',
+                'key_download': message['id'],
+                'key_signature': message['answer']['signature']['key_signature'],
+                'key_certificate': message['answer']['signature']['key_certificate'],
+                'attachment': base64.b64encode(message['answer']['signature']['attachment_raw']),
+            })
+            xml_attachment_id = self.env['ir.attachment'].sudo().create({
+                'name': f"ciusro_{message['answer']['invoice']['name'].replace('/', '_')}.xml",
+                'raw': message['answer']['invoice']['attachment_raw'],
+                'res_model': 'account.move',
+                'res_id': bill.id,
+            }).id
+            files_data = self._to_files_data(self.env['ir.attachment'].browse(xml_attachment_id))
+            bill._extend_with_attachments(files_data)
+            bill.message_post(body=_("Synchronized with SPV from message %s", message['id']))
+
+    def action_l10n_ro_edi_fetch_invoices(self):
+        self._l10n_ro_edi_fetch_invoices()
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'reload',
+        }
--- addons/l10n_ro_edi/models/utils.py
+++ addons/l10n_ro_edi/models/utils.py
@@ -10,7 +10,10 @@ from odoo.tools.safe_eval import json
 
 NS_STATUS = {"ns": "mfp:anaf:dgti:efactura:stareMesajFactura:v1"}
 NS_HEADER = {"ns": "mfp:anaf:dgti:efactura:mesajEroriFactuta:v1"}
-NS_DOWNLOAD = {"cbc": "urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2"}
+NS_DOWNLOAD = {
+    "cac": "urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2",
+    "cbc": "urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2",
+}
 NS_SIGNATURE = {"ns": "http://www.w3.org/2000/09/xmldsig#"}
 
 
@@ -29,7 +32,7 @@ def make_efactura_request(session, company, endpoint, params, data=None) -> dict
     url = f"https://api.anaf.ro/{send_mode}/FCTEL/rest/{endpoint}"
     if endpoint in ['upload', 'uploadb2c']:
         method = 'POST'
-    elif endpoint in ['stareMesaj', 'descarcare']:
+    elif endpoint in ['stareMesaj', 'descarcare', 'listaMesajeFactura']:
         method = 'GET'
     else:
         return {'error': _('Unknown endpoint.')}
@@ -214,3 +217,69 @@ def _request_ciusro_download_answer(company, key_download, session):
             return {'error': eroare}
 
     return {'error': _("The SPV data could not be parsed.")}
+
+
+def _request_ciusro_synchronize_invoices(company, session, nb_days=1):
+    """
+    This method makes a "Fetch Messages" (GET/listaMesajeFactura) request to the Romanian SPV.
+    After processing the response, if messages were indeed fetched, it will fetch the content
+    of said messages.
+
+    Possible returns:
+    - {'error': `str`} if there was a failing response from a bad request;
+    - {'sent_invoices_messages': [`dict`], 'sent_invoices_refused_messages': [`dict`], 'received_bills_messages': [`dict`]}
+    where `dict` is {
+        'data_creare': `str`,
+        'cif': `str`,
+        'id_solicitare': `str`,
+        'detalii': `str`,
+        'tip': 'FACTURA TRIMISA'|'ERORI FACTURA'|'FACTURA PRIMITA',
+        'id': `str`,
+        'answer': <`_request_ciusro_download_answer`>
+    } representing a message.
+    sent_invoices_messages will contain all message validating an invoice, sent_invoices_refused_messages will contain all messages refusing an invoice
+    and received_bills_messages will contain all message representing received bills.
+
+    :param company: ``res.company`` object
+    :param session: ``requests.Session()`` object
+    :param nb_days(optional,default=1): ``int`` the number of days for which the request should be made, min=1, max=60
+    :return: {'error': `str`} | {'sent_invoices_messages': [`dict`], 'sent_invoices_refused_messages': [`dict`], 'received_bills_messages': [`dict`]}
+    """
+    result = make_efactura_request(
+        session=session,
+        company=company,
+        endpoint='listaMesajeFactura',
+        params={'zile': nb_days, 'cif': company.vat.replace('RO', '')},
+    )
+    if 'error' in result:
+        return {'error': result['error']}
+
+    try:
+        msg_content = json.loads(result['content'])
+    except ValueError:
+        return {'error': _("The SPV data could not be parsed.")}
+
+    if eroare := msg_content.get('eroare'):
+        return {'error': eroare}
+
+    received_bills_messages = []
+    sent_invoices_accepted_messages = []
+    sent_invoices_refused_messages = []
+    for message in msg_content.get('mesaje'):
+        message['answer'] = _request_ciusro_download_answer(
+            key_download=message['id'],
+            company=company,
+            session=session,
+        )
+        if message['tip'] == 'FACTURA TRIMISA':
+            sent_invoices_accepted_messages.append(message)
+        elif message['tip'] == 'ERORI FACTURA':
+            sent_invoices_refused_messages.append(message)
+        elif message['tip'] == 'FACTURA PRIMITA':
+            received_bills_messages.append(message)
+
+    return {
+        'sent_invoices_accepted_messages': sent_invoices_accepted_messages,
+        'sent_invoices_refused_messages': sent_invoices_refused_messages,
+        'received_bills_messages': received_bills_messages
+    }
