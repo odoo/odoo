f09cb9b4ae3e178f6c8c99e025e6488be0ae413e
Raul Ruilova <raul.ruilova@trescloud.com>
2025-04-29 22:10:07 -0500

    [IMP] l10n_ec: updating accounts, account groups and support for 8% VAT - Improve the distribution of expense and cost of revenue accounts in Ecuadorian localization We avoided not creating subcategories of expenses by eliminating the group called "Other employee benefit plan expense", which generated duplicity in the accounts. We fixed this by merging the accounts into the main "Employee benefit plan expense" group. Additionally, the account "Other employee benefits' was added to record any other benefits granted directly by the Ecuadorian companies. This ensures a better organization of the accounting setup. Mark as non-reconcilable the expense accounts (salaries and wages, travel expenses, contributions, etc) to avoid unnecessary reconciliation processes, these accounts are income statements and they are not accounts payable/receivable, therefore, they are closed at the end of the accounting period
    - Support VAT 8%: Add Tax report lines
    The addition of tax codes 530, 533, and 534 is necessary to create and configure applicable taxes for purchases taxed at rates other than 0%, used in tax declarations via Form 104 (Last updated February 2025).
    
    - Vat Placeholder message: We remove cédula and change the message to show only one RUC example
    Update the placeholder for the company VAT number, removed the example of the cédula since companies only have RUC and put a generic number to avoid using a real company identification.

--- addons/l10n_ec/models/res_company.py
+++ addons/l10n_ec/models/res_company.py
@@ -5,6 +5,18 @@ class ResCompany(models.Model):
 
     _inherit = "res.company"
 
+    def _compute_company_vat_placeholder(self):
+        """
+        To set the placeholder for the VAT field in companies located in Ecuador (EC),
+        For Ecuadorian companies the vat is a 13-digit number (RUC)
+        """
+        companies_ec = self.filtered(lambda c: c.country_id.code == "EC")
+        placeholder = '0123456789001'
+        for company in companies_ec:
+            company.company_vat_placeholder = placeholder
+
+        super(ResCompany, self - companies_ec)._compute_company_vat_placeholder()
+
     def _localization_use_documents(self):
         self.ensure_one()
         return self.account_fiscal_country_id.code == "EC" or super(ResCompany, self)._localization_use_documents()
