d675d8fc37eeb8656c2350d086dedfd9068c07f3
Nicolas Viseur (vin) <vin@odoo.com>
2025-04-30 09:09:40 +0000

    [FIX] account: copy of account report
    Fixes an issue where copying an account report
    correctly updates the formulas of the report but
    not the subformulas.
    
    closes odoo/odoo#210073
    
    X-original-commit: f372ec5afe0b2a63453cd3ed62ce545881578173
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>
    Signed-off-by: Nicolas Viseur (vin) <vin@odoo.com>

--- addons/account/models/account_report.py
+++ addons/account/models/account_report.py
@@ -292,6 +292,12 @@ class AccountReport(models.Model):
                     for old_code, new_code in code_mapping.items():
                         copied_formula = re.sub(f"(?<=\\W){old_code}(?=\\W)", new_code, copied_formula)
                     expression.formula = copied_formula.strip()  # Remove the spaces introduced for lookahead/lookbehind
+                    # Repeat the same logic for the subformula, if it is set.
+                    if expression.subformula:
+                        copied_subformula = f" {expression.subformula} "
+                        for old_code, new_code in code_mapping.items():
+                            copied_subformula = re.sub(f"(?<=\\W){old_code}(?=\\W)", new_code, copied_subformula)
+                        expression.subformula = copied_subformula.strip()
 
             old_report.column_ids.copy({'report_id': new_report.id})
         return new_reports
