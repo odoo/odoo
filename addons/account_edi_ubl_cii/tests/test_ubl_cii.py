506bb7ba0a7ff58401428a7bcbb2083c029c075a
Mathieu Coutant <mcou@odoo.com>
2025-04-30 10:59:12 +0000

    [FIX] account_edi_ubl_cii: add start and end date in Factur-X's XML
    Steps to reproduce:
    - install "l10n_de" and switch to a German company
    - Go to a contact, in the page Accounting > Electronic Invoicing change the format to "Factur-X (CII)"
    - Create an invoice for this contact
    - Add a start and end date on the line of this invoice
    - Confirm and send to Factur-X
    - In the generated XML there is no trace of the start or end dates
    
    Solution:
    Set the variables to add the `BillingSpecifiedPeriod` in the XML. Put `invoice_date` as start date and `invoice_date_due` as end date.
    
    opw-4680890
    
    closes odoo/odoo#209060
    
    X-original-commit: 6118fc6b31671459c2c71ebde985a2f8bf6c652e
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>
    Signed-off-by: Mathieu Coutant (mcou) <mcou@odoo.com>

--- addons/account_edi_ubl_cii/tests/test_ubl_cii.py
+++ addons/account_edi_ubl_cii/tests/test_ubl_cii.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from lxml import etree
-from odoo import Command
+from odoo import fields, Command
 from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 from odoo.tests import tagged
 from odoo.tools import file_open
@@ -237,6 +237,35 @@ class TestAccountEdiUblCii(AccountTestInvoicingCommon):
             'udt': "urn:un:unece:uncefact:data:standard:UnqualifiedDataType:100"
         })
         self.assertEqual(actual_delivery_date.text, '20241231')
+
+    def test_billing_date_in_cii_xml(self):
+        invoice = self.env['account.move'].create({
+            'partner_id': self.partner_a.id,
+            'move_type': 'out_invoice',
+            'invoice_date': "2024-12-01",
+            'invoice_date_due': "2024-12-31",
+            'invoice_line_ids': [Command.create({'product_id': self.product_a.id})],
+        })
+        invoice.action_post()
+        invoice.invoice_date_due = fields.Date.from_string('2024-12-31')
+
+        xml_attachment = self.env['ir.attachment'].create({
+            'raw': self.env['account.edi.xml.cii']._export_invoice(invoice)[0],
+            'name': 'test_invoice.xml',
+        })
+        xml_tree = etree.fromstring(xml_attachment.raw)
+        start_date = xml_tree.find('.//ram:BillingSpecifiedPeriod/ram:StartDateTime/udt:DateTimeString', {
+            'rsm': "urn:un:unece:uncefact:data:standard:CrossIndustryInvoice:100",
+            'ram': "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100",
+            'udt': "urn:un:unece:uncefact:data:standard:UnqualifiedDataType:100"
+        })
+        end_date = xml_tree.find('.//ram:BillingSpecifiedPeriod/ram:EndDateTime/udt:DateTimeString', {
+            'rsm': "urn:un:unece:uncefact:data:standard:CrossIndustryInvoice:100",
+            'ram': "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100",
+            'udt': "urn:un:unece:uncefact:data:standard:UnqualifiedDataType:100"
+        })
+        self.assertEqual(start_date.text, '20241201')
+        self.assertEqual(end_date.text, '20241231')
 
     def test_import_partner_fields(self):
         """ We are going to import the e-invoice and check partner is correctly imported."""
