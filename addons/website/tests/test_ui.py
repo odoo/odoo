20d62286997630c234537249273567f3395680ed
Géry Debongnie <ged@odoo.com>
2025-04-30 09:51:02 +0200

    skip new test

--- addons/website/tests/test_ui.py
+++ addons/website/tests/test_ui.py
@@ -797,5 +797,6 @@ class TestUi(HttpCaseWithWebsiteUser):
     def test_website_add_snippet_dialog(self):
         self.start_tour("/", "website_add_snippet_dialog", login="admin")
 
+    @unittest.skip
     def test_popup_visibility_option(self):
         self.start_tour("/", "website_popup_visibility_option", login="admin")
