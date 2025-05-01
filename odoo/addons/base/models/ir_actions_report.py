de4f479169b806b17d2c50e96cca95a057e4f02e
bram1000 <brvg@odoo.com>
2025-05-01 12:14:47 +0200

    [FIX] base: increase barcode size limit
    opw-4954388

--- odoo/addons/base/models/ir_actions_report.py
+++ odoo/addons/base/models/ir_actions_report.py
@@ -692,7 +692,7 @@ class IrActionsReport(models.Model):
         if kwargs['humanReadable']:
             kwargs['fontName'] = _DEFAULT_BARCODE_FONT
 
-        if kwargs['width'] * kwargs['height'] > 400000 or max(kwargs['width'], kwargs['height']) > 10000:
+        if kwargs['width'] * kwargs['height'] > 1200000 or max(kwargs['width'], kwargs['height']) > 10000:
             raise ValueError("Barcode too large")
 
         if barcode_type == 'UPCA' and len(value) in (11, 12, 13):
