04ccbc3f7f7bb439d374dc9e7e567d2445914c03
alsh-odoo <alsh@odoo.com>
2025-05-01 06:58:02 +0000

    [FIX] stock: on hand qty can be set to zero on a product
    Steps to reproduce:
    1) Install stocks
    2) Create a product variant with storable true
    3) Change the on-hand quantity to some positive value and save
    4) Now update the on-hand quantity to 0 and refresh the page
    
    Issue:-
      The on-hand quantity doesn't update to 0.
    
    Cause:-
    When the user manually changes the on-hand quantity, an inverse method
    is triggered to create a new stock.quant, but because of this condition no
    new quant is created.
    https://github.com/odoo/odoo/blob/b2e845c46a0b152899ee630770abc15751493069/addons/stock/models/product.py#L237
    
    As a result, the qty_available value will remain same because it will
    be updated based on the stock.quant's record from the below line.
    
    https://github.com/odoo/odoo/blob/b2e845c46a0b152899ee630770abc15751493069/addons/stock/models/product.py#L185
    https://github.com/odoo/odoo/blob/b2e845c46a0b152899ee630770abc15751493069/addons/stock/models/product.py#L217
    
    Solution:-
      We can resolve this issue by modifying the condition in inverse method
      Also skip creating a move with 0 inventory diff
    
    opw-4761491
    
    closes odoo/odoo#222002
    
    X-original-commit: 743f10904ef9f5bcc0c51dd2e616fdebadc6bde7
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Altaf Shaik (alsh) <alsh@odoo.com>

--- addons/stock/models/stock_quant.py
+++ addons/stock/models/stock_quant.py
@@ -1002,6 +1002,10 @@ class StockQuant(models.Model):
         self.inventory_quantity_set = True
         move_vals = []
         for quant in self:
+            # if inventory applied from product's inverse_qty and the inventory_diff_quantity is 0,
+            # we skip creating a move with 0 quantity.
+            if quant.env.context.get('from_inverse_qty') and quant.product_uom_id.compare(quant.inventory_diff_quantity, 0) == 0:
+                continue
             # Create and validate a move so that the quant matches its `inventory_quantity`.
             if quant.product_uom_id.compare(quant.inventory_diff_quantity, 0) > 0:
                 move_vals.append(
