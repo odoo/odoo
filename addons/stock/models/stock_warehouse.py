7f190e98c30c50b4571811f3f1dc6f02022cab70
Junqi Wu <juwu@odoo.com>
2025-05-01 13:50:52 +0000

    [FIX] stock,mrp: allow warehouse creation with company-specific routes
    Before this commit, if the generic manufacture route was in a different company, the user won't be able to create a new warehouse.
    
    Steps to reproduce
    -----
    1. Set the company of the Manufacture route to company A
    2. Switch to company B
    3. Create a new warehouse
    4. Access Error
    `Due to security restrictions, you are not allowed to access 'Inventory Routes' (stock.route) records.`
    
    Cause
    -----
    There is an if-statement to check if the found global route is of the same company.
    https://github.com/odoo/odoo/blob/856409a1fb35c6c49fe4c404931587a95d99d370/addons/stock/models/stock_warehouse.py#L385
    But this conditional is likely to raise an access error if the route is in a different company,
    since we don't have read access to `route`.
    
    Solution
    -----
    Use `route.sudo()` to read the route without an access error.
    
    X-original-commit: 61d3f8460eaf4217d258bab60f1da8d3281accab

--- addons/stock/models/stock_warehouse.py
+++ addons/stock/models/stock_warehouse.py
@@ -413,7 +413,7 @@ class StockWarehouse(models.Model):
         """ return a route record set from an xml_id or its name. """
         data_route = route = self.env.ref(xml_id, raise_if_not_found=False)
         company = self.company_id[:1] or self.env.company
-        if not route or (route.company_id and route.company_id != company):
+        if not route or (route.sudo().company_id and route.sudo().company_id != company):
             route = self.env['stock.route'].with_context(active_test=False).search([
                 ('name', 'like', route_name), ('company_id', 'in', [False, company.id])
             ], order='company_id', limit=1)
