94b4f63a1f4f8142a99b996c6f805ab050891626
David Monnom <moda@odoo.com>
2025-05-02 12:26:57 +0000

    [FIX] point_of_sale, pos_event, pos_loyalty, pos_sale: improve pos sync
    Previously, record synchronization was based on the pos.order model, so
    if the write_date of the order was more recent on the server, it was
    re-downloaded with its sub-records. The problem was that by changing an
    orderline, the order object was not necessarily updated, so its
    write_date remained the same.
    
    Now all records and their write_dates are compared, so that records can
    be re-downloaded independently of their order. Trusted config commands
    are also re-downloaded.
    
    The records compared are only those that can be created from the PoS
    frontend.
    
    closes odoo/odoo#208999
    
    X-original-commit: 434b634d443c7cbee361dde6a525b4691671fa66
    Signed-off-by: Vlad Stroia (vlst) <vlst@odoo.com>
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/point_of_sale/models/pos_order.py
+++ addons/point_of_sale/models/pos_order.py
@@ -1075,7 +1075,7 @@ class PosOrder(models.Model):
         }
 
     def _get_open_order(self, order):
-        return self.env["pos.order"].search([('uuid', '=', order.get('uuid'))], limit=1)
+        return self.env["pos.order"].search([('uuid', '=', order.get('uuid'))], limit=1, order='id desc')
 
     @staticmethod
     def _get_order_log_representation(order):
@@ -1431,9 +1431,12 @@ class PosOrderLine(models.Model):
     @api.model
     def _load_pos_data_fields(self, config_id):
         return [
-            'qty', 'attribute_value_ids', 'custom_attribute_value_ids', 'price_unit', 'uuid', 'price_subtotal', 'price_subtotal_incl', 'order_id', 'note', 'price_type',
-            'product_id', 'discount', 'tax_ids', 'pack_lot_ids', 'customer_note', 'refunded_qty', 'price_extra', 'full_product_name', 'refunded_orderline_id', 'combo_parent_id', 'combo_line_ids', 'combo_item_id', 'refund_orderline_ids',
-            'extra_tax_data',
+            'qty', 'attribute_value_ids', 'custom_attribute_value_ids', 'price_unit',
+            'uuid', 'price_subtotal', 'price_subtotal_incl', 'order_id', 'note', 'price_type',
+            'product_id', 'discount', 'tax_ids', 'pack_lot_ids', 'customer_note',
+            'refunded_qty', 'price_extra', 'full_product_name', 'refunded_orderline_id',
+            'combo_parent_id', 'combo_line_ids', 'combo_item_id', 'refund_orderline_ids',
+            'extra_tax_data', 'write_date',
         ]
 
     @api.model
@@ -1772,7 +1775,7 @@ class PosPackOperationLot(models.Model):
 
     @api.model
     def _load_pos_data_fields(self, config_id):
-        return ['lot_name', 'pos_order_line_id']
+        return ['lot_name', 'pos_order_line_id', 'write_date']
 
 
 class AccountCashRounding(models.Model):
