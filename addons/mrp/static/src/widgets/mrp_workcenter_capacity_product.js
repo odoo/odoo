19b10d8d61e6c7effccc04c9943cee01fc5fffe8
JF Aubert <ajf@odoo.com>
2025-05-05 08:47:18 +0200

    [FIX] mrp: BoM Overview Fixes
    - in workcenter's capacity tab, make All Products italic and not clickable
    - selected warehouse name should appear like in forecast report
    - make 'Manufacturing Forecast' button secondary and open the BoM overview
      in forecast mode with focus on quantity field
    
    closes odoo/odoo#208417
    
    Task: 4751983
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/mrp/static/src/widgets/mrp_workcenter_capacity_product.js
+++ addons/mrp/static/src/widgets/mrp_workcenter_capacity_product.js
@@ -1,23 +1,17 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
-import { createMany2OneValue } from "@web/model/relational_model/utils";
 import { computeM2OProps, Many2One } from "@web/views/fields/many2one/many2one";
 import { buildM2OFieldDescription, Many2OneField } from "@web/views/fields/many2one/many2one_field";
 
 export class WorkcenterCapacityProduct extends Many2One {
-
-    get value() {
-        let result = super.value;
-        if (result === null && this.props.readonly) {
-            result = createMany2OneValue([false, this.props.placeholder]);
-        }
-        return result;
-    }
+    static template = "mrp.WorkcenterCapacityProduct";
+    static components = { ...Many2One.components };
+    static props = { ...Many2One.props };
 }
 
 export class WorkcenterCapacityProductField extends Component {
-    static template = "web.Many2OneField";
-    static components = { Many2One: WorkcenterCapacityProduct };
+    static template = "mrp.WorkcenterCapacityProductField";
+    static components = { WorkcenterCapacityProduct };
     static props = { ...Many2OneField.props };
 
     get m2oProps() {
