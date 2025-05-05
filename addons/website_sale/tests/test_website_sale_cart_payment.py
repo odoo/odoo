8a8d74fbf14885d6a89367be57792fa7b9af84d1
Levi Siuzdak (sile) <sile@odoo.com>
2025-05-05 11:25:24 +0200

    [FIX] website_sale: set salesperson on payment confirmation
    Versions
    --------
    - saas-18.1+
    
    Steps
    -----
    1. Ensure admin gets auto-assigned as salesperson to eCommerce orders;
    2. open an eCommerce cart and go to checkout;
    3. using Demo payment provider, create a "pending" payment.
    
    Issue
    -----
    Payment confirmation mail is sent from OdooBot instead of admin.
    
    Cause
    -----
    Commit 27bf2e55810b automatically adds admin as salesperson to eCommerce
    orders, but only on order confirmation. This means that salesperson does
    not get notified about pending payments, and the client receives an
    email from OdooBot instead of a salesperson.
    
    Solution
    --------
    Set the order's `user_id` before sending the payment confirmation mail.
    
    opw-4765346

--- addons/website_sale/tests/test_website_sale_cart_payment.py
+++ addons/website_sale/tests/test_website_sale_cart_payment.py
@@ -1,5 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from unittest.mock import patch
+
 from odoo.fields import Command
 from odoo.tests.common import JsonRpcException, tagged
 from odoo.tools import mute_logger
@@ -64,3 +66,18 @@ class WebsiteSaleCartPayment(PaymentHttpCommon):
         }
         with self.assertRaises(JsonRpcException, msg='odoo.exceptions.ValidationError'):
             self.make_jsonrpc_request(url, route_kwargs)
+
+    def test_payment_confirmation_mail(self):
+        """Check that a salesperson gets assigned when sending payment confirmation mails."""
+        salesperson = self.env.ref('base.user_admin')
+        self.website.salesperson_id = salesperson
+        self.order.user_id = False
+        self.tx._set_pending()
+        with patch.object(self.env.registry['sale.order'], '_send_order_notification_mail') as mock:
+            self.tx._post_process()
+            self.assertEqual(mock.call_count, 1, "One payment confirmation mail should be sent")
+            self.assertEqual(
+                self.order.user_id,
+                salesperson,
+                "Salesperson should get assigned when sending payment confirmation mail",
+            )
