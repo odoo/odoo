07d68a201ad2ad8e710365a5efc1d0dfc8ae45db
sbbh-odoo <sbbh@odoo.com>
2025-05-05 13:02:09 +0530

    [IMP] html_editor: add overlay on hover to reopen video settings
    Before this commit:
    
    - After uploading a video, there was no user-friendly way to reopen or change
      the video settings.
    - Users had to delete and re-upload the video to modify settings, which was
      inefficient.
    
    After this commit:
    
    - Introduced a ReadonlyEmbeddedVideoComponent to cleanly separate readonly
      rendering from interactive logic.
    - A small overlay with a settings icon is now displayed on hover.
    - Clicking the overlay opens a dropdown with `Replace` and `Remove` options,
      making it easier and more intuitive for users to manage the video settings.
    
    task-4742547

--- addons/html_editor/static/src/public/embedding_sets.js
+++ addons/html_editor/static/src/public/embedding_sets.js
@@ -1,11 +1,11 @@
 import { readonlyFileEmbedding } from "@html_editor/others/embedded_components/core/file/readonly_file";
 import { readonlyTableOfContentEmbedding } from "@html_editor/others/embedded_components/core/table_of_content/table_of_content";
 import { toggleBlockEmbedding } from "@html_editor/others/embedded_components/core/toggle_block/toggle_block";
-import { videoEmbedding } from "@html_editor/others/embedded_components/core/video/video";
+import { readonlyVideoEmbedding } from "@html_editor/others/embedded_components/core/video/readonly_video";
 
 export const PUBLIC_EMBEDDINGS = [
     readonlyFileEmbedding,
     readonlyTableOfContentEmbedding,
     toggleBlockEmbedding,
-    videoEmbedding,
+    readonlyVideoEmbedding,
 ];
