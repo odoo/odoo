e8a52a83f053b74d498818ea3bfc65cd331c37f9
David Monnom <moda@odoo.com>
2025-05-05 08:23:36 +0000

    [FIX] point_of_sale: correctly dispatch session closing message
    Before this commit, the session closing message was in the method called
    from the UI. But in case of error during the closing control, the user
    must close it from the backend. In this case, the message was not
    sent to all devices.
    
    This commit moves the message to the write method of the session to
    ensure that the message is sent to all devices, even if the session
    is closed from the backend.
    
    closes odoo/odoo#208541
    
    X-original-commit: 197ce1983da3f7700e311d20caa6c1e96f219d4a
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/point_of_sale/models/pos_session.py
+++ addons/point_of_sale/models/pos_session.py
@@ -98,6 +98,12 @@ class PosSession(models.Model):
         'The name of this POS Session must be unique!',
     )
 
+    def write(self, vals):
+        if vals.get('state') == 'closed':
+            for record in self:
+                record.config_id._notify(('CLOSING_SESSION', {'login_number': self.env.context.get('login_number', False)}))
+        return super().write(vals)
+
     @api.model
     def _load_pos_data_relations(self, model, fields):
         model_fields = self.env[model]._fields
@@ -623,7 +629,6 @@ class PosSession(models.Model):
             }
 
         self.post_close_register_message()
-        self.config_id._notify(('CLOSING_SESSION', {'login_number': self.env.context.get('login_number', False)}))
         return {'successful': True}
 
     def post_close_register_message(self):
