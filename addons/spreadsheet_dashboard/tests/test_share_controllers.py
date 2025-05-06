4f50566d5457badc2d0400b34dec79f17f0ab264
Florian Damhaut <flda@odoo.com>
2025-05-06 15:12:01 +0200

    [IMP] spreadsheet: log data extraction
    Add logging when exporting large amount of data from
    odoo sources.
    
    Logging occurs when ;
    - downloading a spreadsheet as XLSX
    - copying to clipboard large data from a spreadsheet
    - printing a spreadsheet/dashboard
    - freezing a spreadsheet/dashboard
    
    This commit also remove the ability to download frozen/xlsx spreadsheet
    to anyone without export right.
    
    closes odoo/odoo#210592
    
    Task: 4718455
    Related: odoo/enterprise#85888
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- addons/spreadsheet_dashboard/tests/test_share_controllers.py
+++ addons/spreadsheet_dashboard/tests/test_share_controllers.py
@@ -1,12 +1,19 @@
 import json
 import base64
 
-from odoo.tests.common import HttpCase
+from odoo.tests.common import HttpCase, new_test_user
 from odoo.tools import mute_logger
 
 from .common import DashboardTestCommon
 
 class TestShareController(DashboardTestCommon, HttpCase):
+
+    @classmethod
+    @mute_logger('odoo.tests', 'odoo.addons.auth_signup.models.res_users')
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.alex = new_test_user(cls.env, login='AlexPort', groups='base.group_user,base.group_allow_export')
+
     def test_dashboard_share_portal(self):
         dashboard = self.create_dashboard()
         share = self.share_dashboard(dashboard)
@@ -52,20 +59,37 @@ class TestShareController(DashboardTestCommon, HttpCase):
         dashboard = self.create_dashboard()
         share = self.share_dashboard(dashboard)
         share.excel_export = base64.b64encode(b"test")
+        self.authenticate('AlexPort', 'AlexPort')
         response = self.url_open(f"/dashboard/download/{share.id}/{share.access_token}")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, b"test")
+
+    def test_download_dashboard_no_export(self):
+        dashboard = self.create_dashboard()
+        share = self.share_dashboard(dashboard)
+        share.excel_export = base64.b64encode(b"test")
+        self.authenticate('AlexPort', 'AlexPort')
+        response = self.url_open(f"/dashboard/download/{share.id}/{share.access_token}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"test")
+
+        self.alex.groups_id -= self.env.ref('base.group_allow_export', raise_if_not_found=False)    # revoke export right
+        with mute_logger('odoo.http'):  # mute 400 warning
+            response = self.url_open(f"/dashboard/download/{share.id}/{share.access_token}")
+        self.assertEqual(response.status_code, 400)
 
     def test_download_dashboard_wrong_token(self):
         dashboard = self.create_dashboard()
         share = self.share_dashboard(dashboard)
         share.excel_export = base64.b64encode(b"test")
+        self.authenticate('AlexPort', 'AlexPort')
         with mute_logger('odoo.http'):  # mute 403 warning
             response = self.url_open(f"/dashboard/download/{share.id}/a-random-token")
         self.assertEqual(response.status_code, 403)
 
     def test_download_dashboard_revoked_access(self):
         dashboard = self.create_dashboard()
+        self.authenticate('AlexPort', 'AlexPort')
         with self.with_user(self.user.login):
             share = self.share_dashboard(dashboard)
         share.excel_export = base64.b64encode(b"test")
