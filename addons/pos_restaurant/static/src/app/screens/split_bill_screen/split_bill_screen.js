d8ea9e3682798f6a647a36f2794ec9aaaac45037
Loukas Wets (lowe) <lowe@odoo.com>
2025-05-06 14:42:40 +0000

    [FIX] point_of_sale: select orderline split bill
    Steps to reporduce:
    
    - Make two orderlines that are cannot be merged with same product (add
    a comment, combo, etc ...)
    - Open the split bill screen
    - Selecet any of the orderlines and try to unselect it
    - Impossible to unselect the orderlines with same product but different
    line
    
    Fix:
    Calculate the line quantity selected based on the linked order line quantity
    and not the total number of same product in the order.
    
    closes odoo/odoo#209633
    
    X-original-commit: 93fb556b9841d5b9f072ceeab8a1fc166197e6bd
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: Loukas Wets (lowe) <lowe@odoo.com>

--- addons/pos_restaurant/static/src/app/screens/split_bill_screen/split_bill_screen.js
+++ addons/pos_restaurant/static/src/app/screens/split_bill_screen/split_bill_screen.js
@@ -44,7 +44,7 @@ export class SplitBillScreen extends Component {
         const lines = line.getAllLinesInCombo();
 
         for (const line of lines) {
-            if (!line.isPosGroupable() && !line.isPartOfCombo()) {
+            if (!line.product_id.uom_id?.is_pos_groupable) {
                 if (this.qtyTracker[line.uuid] === line.getQuantity()) {
                     this.qtyTracker[line.uuid] = 0;
                 } else {
