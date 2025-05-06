9df2f8890384481775a258a1d7c7f09ec613506e
Florian Damhaut <flda@odoo.com>
2025-05-06 13:12:01 +0000

    [IMP] spreadsheet: log data extraction
    Add logging when exporting large amount of data from
    odoo sources.
    
    Logging occurs when ;
    - downloading a spreadsheet as XLSX
    - copying to clipboard large data from a spreadsheet
    - printing a spreadsheet/dashboard
    - freezing a spreadsheet/dashboard
    
    This commit also remove the ability to download frozen/xlsx spreadsheet
    to anyone without export right.
    
    Task: 4718455
    X-original-commit: 4f50566d5457badc2d0400b34dec79f17f0ab264

---
+++ addons/spreadsheet/controllers/main.py
@@ -0,0 +1,45 @@
+import logging
+
+
+from odoo import http
+from odoo.http import request, Controller
+
+logger = logging.getLogger(__name__)
+
+
+class SpreadsheetController(Controller):
+
+    @http.route("/spreadsheet/log", type="json", auth="user", methods=["POST"])
+    def log_action(self, action_type, datasources, **kw):
+        if datasources:
+            self._log_spreadsheet_export(action_type, request.env.uid, datasources)
+
+    def _log_spreadsheet_export(self, action_type, user_id, datasources):
+        if action_type not in ["download", "copy", "freeze", "print"]:
+            return
+        data = [src for datasource in datasources if (src := self._stringify_source(datasource))]
+        if not data:
+            return
+        logger.info(
+            "User %d exported (%s) spreadsheet data (%s) from %s",
+            user_id, action_type, "), (".join(data), request.httprequest.environ["REMOTE_ADDR"]
+        )
+
+    @staticmethod
+    def _stringify_source(source):
+        res_model = source.get("resModel")
+        if not res_model or res_model not in request.env:
+            return
+
+        if not (fields := source.get("fields", [])):
+            return
+
+        string = f"model: {res_model} with fields: [{','.join(fields)}]"
+
+        if groupby := source.get("groupby"):
+            string += f" grouped by [{','.join(groupby)}]"
+
+        if domain := source.get("domain"):
+            string += f" with domain {domain}"
+
+        return string
