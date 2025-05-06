7a0ff32ee241e44d7c8644b8f48a0c798b9f6315
Florian Damhaut <flda@odoo.com>
2025-05-06 13:12:01 +0000

    [IMP] spreadsheet: log data extraction
    Add logging when exporting large amount of data from
    odoo sources.
    
    Logging occurs when ;
    - downloading a spreadsheet as XLSX
    - copying to clipboard large data from a spreadsheet
    - printing a spreadsheet/dashboard
    - freezing a spreadsheet/dashboard
    
    This commit also remove the ability to download frozen/xlsx spreadsheet
    to anyone without export right.
    
    closes odoo/odoo#250055
    
    Task: 4718455
    X-original-commit: 5507dc46426a81583fa26eb43f01b3000aac7c2d
    Related: odoo/enterprise#108289
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>
    Signed-off-by: Florian Damhaut (flda) <flda@odoo.com>

--- addons/spreadsheet_dashboard/static/src/bundle/dashboard_action/dashboard_action.js
+++ addons/spreadsheet_dashboard/static/src/bundle/dashboard_action/dashboard_action.js
@@ -12,7 +12,7 @@ import { useSpreadsheetPrint } from "@spreadsheet/hooks";
 import { Registry } from "@odoo/o-spreadsheet";
 import { router } from "@web/core/browser/router";
 
-import { Component, onWillStart, useState, useEffect } from "@odoo/owl";
+import { Component, onWillStart, useState, useEffect, useExternalListener } from "@odoo/owl";
 import { DashboardSearchBar } from "./dashboard_search_bar/dashboard_search_bar";
 
 export const dashboardActionRegistry = new Registry();
@@ -74,6 +74,8 @@ export class SpreadsheetDashboardAction extends Component {
                 return [dashboard?.model, dashboard?.status];
             }
         );
+        useExternalListener(window, "afterprint", this.logExport.bind(this));
+
         useSetupAction({
             getLocalState: () => ({
                 activeDashboardId: this.activeDashboardId,
@@ -188,6 +190,14 @@ export class SpreadsheetDashboardAction extends Component {
                 group.id !== "favorites" && // Skip the FAVORITES group
                 group.dashboards.some((d) => d.id === this.activeDashboardId)
         )?.name;
+    }
+
+    logExport() {
+        const dashboard = this.state.activeDashboard;
+        if (!dashboard || dashboard.status !== Status.Loaded) {
+            return;
+        }
+        this.model.dispatch("LOG_DATASOURCE_EXPORT", { action: "print" });
     }
 }
 
