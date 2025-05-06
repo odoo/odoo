47c455a114ee0dfa5c872813e20948a9a609969e
Sarah Bellefroid <sbel@odoo.com>
2025-05-06 13:35:35 +0000

    [FIX] l10n_es_pos: not override manually set pricelist with simplified invoice
    Currently, when you make a simplified invoice in pos, the pricelist will
    always be set to the simplified invoice partner pricelist, even if you
    had specifically set another pricelist during the order.
    
    Steps to reproduce:
    -------------------
    * Create 2 pricelists, A and B, let's say A always adds a 10$ fee
    * Set both as available in the POS, with A being the default one
    * On the contact "Simplified invoice parner", set the pricelist A
    * Open pos
    * Change pricelist to B
    * Pay and validate order
    > Observation: Order is not validated, 10$ left to pay
    
    Why the fix:
    ------------
    Prior to the use of `set_partner`, we used to simply write the partner
    field of the order when using the simplified invoice partner.
    
    Now we are using `set_partner` which is a generic function that handles
    everything related to changing partners, such as updating pricelist.
    https://github.com/odoo/odoo/blob/69057e41fb4cd800d23401ead8ae11bf7cba7c64/addons/point_of_sale/static/src/app/models/pos_order.js#L941-L948
    
    In our example since the default pos pricelist was manually changed it
    means that the intention was to use this set pricelist.
    
    What we do now in this case is to check if the pricelist was purposefully
    changed (it differs from the default pricelist), and in this case we
    want to use this pricelist on the order.
    
    opw-4662248
    
    closes odoo/odoo#210603
    
    X-original-commit: 017ca976d646fcc10294a673d97c335569ab9c35
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Sarah Bellefroid (sbel) <sbel@odoo.com>

--- addons/l10n_es_pos/static/src/app/screens/payment_screen/payment_screen.js
+++ addons/l10n_es_pos/static/src/app/screens/payment_screen/payment_screen.js
@@ -26,7 +26,14 @@ patch(PaymentScreen.prototype, {
                     return false;
                 }
                 if (!order.partner_id) {
+                    const setPricelist =
+                        this.pos.config.pricelist_id?.id != order.pricelist_id?.id
+                            ? order.pricelist_id.id
+                            : false;
                     order.setPartner(this.pos.config.simplified_partner_id);
+                    if (setPricelist) {
+                        order.setPricelist(setPricelist);
+                    }
                 }
             }
         }
