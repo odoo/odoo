odoo/odoo

commit 3fbd74b3bb7af2b9bc9c86fddc78771ec97ce740

[IMP] base: ease sessions token fields override
  - Before this commit, we compute session token using the values of
    specific fields stored on `res.users`.

    The values only were hashed. For better security we now also hash
    the field name associated to the values.

    We also allow to easily override the fields used for the session
    computation by passing them through `_session_token_hash_compute`

    This feature will not invalidate existing session and will upgrade
    older valid session using the new computed session token.

closes odoo/odoo#204319

Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

odoo/addons/base/models/res_users.py
@@ -779,7 +779,7 @@ class ResUsers(models.Model):
             if not self._fields[fname].relational
         )
         return {
-            "select": SQL("(%s), %s", database_secret, fields),
+            "select": SQL("(%s) as database_secret, %s", database_secret, fields),
             "from": SQL("res_users"),
             "joins": SQL(""),
             "where": SQL("res_users.id = %s", self.id),
@@ -790,6 +790,10 @@ class ResUsers(models.Model):
     def _compute_session_token(self, sid):
         """ Compute a session token given a session id and a user id """
         # retrieve the fields used to generate the session token
+        field_values = self._session_token_get_values()
+        return self._session_token_hash_compute(sid, field_values)
+
+    def _session_token_get_values(self):
         self.env.cr.execute(SQL(
             "SELECT %(select)s FROM %(from)s %(joins)s WHERE %(where)s GROUP BY %(group_by)s",
             **self._get_session_token_query_params(),
@@ -798,10 +802,29 @@ class ResUsers(models.Model):
             self.env.registry.clear_cache()
             return False
         data_fields = self.env.cr.fetchone()
-        # generate hmac key
-        key = (u'%s' % (data_fields,)).encode('utf-8')
+        # create tuple with column name and value, allowing for overrides to manipulate the values
+        cr_description = self.env.cr.description
+        return tuple((column.name, data_fields[index]) for index, column in enumerate(cr_description))
+
+    def _session_token_hash_compute(self, sid, field_values):
+        # Generate hmac key using the column name and its value, only if the value is not None
+        # To avoid invalidating sessions when installing a new feature modifying the session token computation
+        # while not still being used.
+        key_tuple = tuple((k, v) for k, v in field_values if v is not None)
+        # encode the key tuple to a bytestring
+        key = str(key_tuple).encode()
         # hmac the session id
-        data = sid.encode('utf-8')
+        data = sid.encode()
+        h = hmac.new(key, data, sha256)
+        # return the session token with a prefix version
+        return h.hexdigest()
+
+    def _legacy_session_token_hash_compute(self, sid):
+        field_values = self._session_token_get_values()
+        # generate hmac key
+        key = ('%s' % (tuple(f[1] for f in field_values),)).encode()
+        # hmac the session id
+        data = sid.encode()
         h = hmac.new(key, data, sha256)
         # keep in the cache the token
         return h.hexdigest()

addons/auth_passkey/models/res_users.py
@@ -77,6 +77,9 @@ class ResUsers(models.Model):
 
     def _get_session_token_query_params(self):
         params = super()._get_session_token_query_params()
-        params['select'] = SQL("%s, ARRAY_AGG(key.id ORDER BY key.id DESC)", params['select'])
+        params['select'] = SQL(
+            "%s, ARRAY_AGG(key.id ORDER BY key.id DESC) FILTER (WHERE key.id IS NOT NULL) as auth_passkey_key_ids",
+            params['select']
+        )
         params['joins'] = SQL("%s LEFT JOIN auth_passkey_key key ON res_users.id = key.create_uid", params['joins'])
         return params

odoo/service/security.py
@@ -17,8 +17,17 @@ def compute_session_token(session, env):
 def check_session(session, env, request=None):
     self = env['res.users'].browse(session.uid)
     expected = self._compute_session_token(session.sid)
-    if expected and consteq(expected, session.session_token):
-        if request:
-            env['res.device.log']._update_device(request)
-        return True
+    if expected:
+        if consteq(expected, session.session_token):
+            if request:
+                env['res.device.log']._update_device(request)
+            return True
+        # If the session token is not valid, we check if the legacy version works
+        # and convert the session token to the new one
+        legacy_expected = self._legacy_session_token_hash_compute(session.sid)
+        if legacy_expected and consteq(legacy_expected, session.session_token):
+            session.session_token = expected
+            if request:
+                env['res.device.log']._update_device(request)
+            return True
     return False

commit dc6afa414b0b5c675fcc8c4bb1a55a85aa85c6d8

[FIX] base: session token compute crash when using invalid values
  - In the case `_session_token_get_values` returns `False`, before this
    commit the code crashes as it wasn't expecting to handle something
    else than an iterable as `field_values`.

    The bug was introduced by the commit 3fbd74b3bb7af2b9bc9c86fddc78771ec97ce740
    Before this commit, falsy values where correctly handled. We
    introduce the same behavior to fix the issue.

  - Added a test to check that computing session on non existing user do
    not crash and correctly expires the session.

odoo/addons/base/models/res_users.py
@@ -837,6 +837,8 @@ class ResUsers(models.Model):
         return tuple((column.name, data_fields[index]) for index, column in enumerate(cr_description))
 
     def _session_token_hash_compute(self, sid, field_values):
+        if not field_values:
+            return False
         # Generate hmac key using the column name and its value, only if the value is not None
         # To avoid invalidating sessions when installing a new feature modifying the session token computation
         # while not still being used.
@@ -851,6 +853,8 @@ class ResUsers(models.Model):
 
     def _legacy_session_token_hash_compute(self, sid):
         field_values = self._session_token_get_values()
+        if not field_values:
+            return False
         # generate hmac key
         key = ('%s' % (tuple(f[1] for f in field_values),)).encode()
         # hmac the session id

odoo/addons/base/tests/test_res_users.py
@@ -520,3 +520,16 @@ class TestUsersIdentitycheck(HttpCase):
 
         # In addition, the password must have been emptied from the wizard
         self.assertFalse(user_identity_check.password)
+
+
+@tagged('post_install', '-at_install')
+class TestUsersSessions(UsersCommonCase):
+
+    def test_session_non_existing_user(self):
+        """
+        Test to check the invalidation of session bound to non existing (or deleted) users.
+        """
+        User = self.env['res.users']
+        last_user_id = User.with_context(active_test=False).search([], limit=1, order="id desc")
+        non_existing_user = User.browse(last_user_id.id + 1)
+        self.assertFalse(non_existing_user._compute_session_token('session_id'))