e862f855a466963b2ab91d2d30bf93ba01bb6c26
Drishti-dev <drishti@shiperoo.com>
2025-05-06 11:20:35 +1000

    Update stock_picking_inherit.py

--- custom_addons/custom_odooship_decanting_process/models/stock_picking_inherit.py
+++ custom_addons/custom_odooship_decanting_process/models/stock_picking_inherit.py
@@ -174,14 +174,26 @@ class StockPicking(models.Model):
                 "reference_1":picking.reference_1,
                 "container_number":picking.container_number,
             }
-            # Define the URLs for Shiperoo Connect
-            is_production = self.env['ir.config_parameter'].sudo().get_param('is_production_env')
-            api_url = (
+            json_data = json.dumps(payload, indent=4)
+            if picking.tenant_code_id.name == "MYSALE":
+                api_url = "https://shiperoo-connect.uat.automation.shiperoo.com/sc-file-processor/api/receipt-completion"
+                auth = ('apiuser', 'apipass')
+            else:
+                is_production = self.env['ir.config_parameter'].sudo().get_param('is_production_env')
+                api_url = (
                 "https://shiperooconnect-prod.automation.shiperoo.com/api/discrepency_receiver"
                 if is_production == 'True'
                 else "https://shiperooconnect-dev.automation.shiperoo.com/api/discrepency_receiver"
             )
-            json_data = json.dumps(payload, indent=4)
+                auth = None
+            # Define the URLs for Shiperoo Connect
+            # is_production = self.env['ir.config_parameter'].sudo().get_param('is_production_env')
+            # api_url = (
+            #     "https://shiperooconnect-prod.automation.shiperoo.com/api/discrepency_receiver"
+            #     if is_production == 'True'
+            #     else "https://shiperooconnect-dev.automation.shiperoo.com/api/discrepency_receiver"
+            # )
+            # json_data = json.dumps(payload, indent=4)
             _logger.info(f"Sending payload to {api_url}: {json_data}")
 
              # Send the payload to the API
