be9d3e339850bee8a6a053284e9a46ef3e974440
Ruben Gomes <rugo@odoo.com>
2025-05-06 14:29:36 +0200

    [REF] core: test_testing_utilities
    test_env

--- odoo/addons/test_testing_utilities/tests/test_env.py
+++ odoo/addons/test_testing_utilities/tests/test_env.py
@@ -1,43 +1,48 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
+from odoo import Command
 from odoo.tests.common import TransactionCase
 
 
 class TestEnv(TransactionCase):
-
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         user = cls.env['res.users'].create({
             'name': 'superuser',
             'login': 'superuser',
-            'password': 'superuser',
-            'group_ids': [(6, 0, cls.env.user.group_ids.ids)],
+            'group_ids': [Command.set(cls.env.user.group_ids.ids)],
         })
-        cls.env = cls.env(user=user)
 
-        # make sure there is at least another environment in the current transaction
+        # We need at least two different environments in the current transaction.
+        cls.env = cls.env(user=user)
         cls.sudo_env = cls.env(su=True)
 
-    def test_env_company_part_01(self):
+    def test_env_lazy_properties_cleanup_between_tests_part_01(self):
         """
-        The main goal of the test is actually to check the values of the
-        environment after this test execution (see test_env_company_part_02)
+        Set up for test_env_cleanup_between_tests_part_02`.
+        Add a lazy property (company) to the environments.
         """
         company = self.env['res.company'].create({
             "name": "Test Company",
         })
+
         self.env.user.write({
             'company_id': company.id,
             'company_ids': [(4, company.id), (4, self.env.company.id)],
         })
+
         self.assertEqual(self.env.company, self.env.user.company_id)
         self.assertTrue(self.env.company.exists())
+
         self.assertEqual(self.sudo_env.company, self.env.user.company_id)
         self.assertTrue(self.sudo_env.company.exists())
 
-    def test_env_company_part_02(self):
+    def test_env_lazy_properties_cleanup_between_tests_part_02(self):
+        """
+        Follow up of test_env_lazy_properties_cleanup_between_tests_part_01.
+        Test that the lazy property (company) was cleared from the environments.
+        """
         self.assertEqual(self.env.company, self.env.user.company_id)
         self.assertTrue(self.env.company.exists())
+
         self.assertEqual(self.sudo_env.company, self.env.user.company_id)
         self.assertTrue(self.sudo_env.company.exists())
