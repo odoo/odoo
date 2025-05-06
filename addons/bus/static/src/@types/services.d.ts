b26346fcf70ed89bc9a892ef58f789b9c43b133f
tsm-odoo <tsm@odoo.com>
2025-05-06 13:36:56 +0200

    [IMP] bus: download bus logs for easier diagnostics
    Diagnosing bus issues can be challenging, as problems are often
    detected too late (e.g., discovering missed messages only after a
    reload). These issues can stem from various sources, making them
    difficult for users to pinpoint.
    
    This commit introduces a logger that stores the last 45 minutes of
    bus worker events in IndexedDB, allowing them to be downloaded when
    an issue occurs. The logs will persist even after a reload, making
    it easier to diagnose and resolve bus-related issues.
    
    A new debug menu item is added to toggle logging on/off and to
    download the logs. This replaces the `condole.debug` calls that
    bloated the console and were not easy to retrieve.

--- addons/bus/static/src/@types/services.d.ts
+++ addons/bus/static/src/@types/services.d.ts
@@ -2,10 +2,12 @@ declare module "services" {
     import { multiTabService } from "@bus/multi_tab_service";
     import { busMonitoringservice } from "@bus/services/bus_monitoring_service";
     import { busService } from "@bus/services/bus_service";
+    import { busLogsService } from "@bus/services/debug/bus_logs_service";
     import { presenceService } from "@bus/services/presence_service";
 
     export interface Services {
         bus_service: typeof busService,
+        "bus.logs_service": typeof busLogsService,
         multi_tab: typeof multiTabService,
         presence_service: typeof presenceService,
         "bus.monitoring_service": typeof busMonitoringservice,
