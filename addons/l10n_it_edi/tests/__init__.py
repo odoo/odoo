566f33e3a22adab97ae45847c435e542440dace7
Anh Thao Pham (pta) <pta@odoo.com>
2025-05-06 14:55:09 +0000

    [FIX] l10n_it_edi: recompute l10n_it_document_type for credit note
    Steps to reproduce:
    - Install l10n_it_edi
    - Switch to an Italian company (e.g. IT Company)
    - Create an invoice
    - Confirm the invoice
    => Document Type (in "Electronic Invoicing" tab) is computed
    - Create a credit note from the invoice
    - On credit note wizard, click on "Reverse and Create Invoice"
    - Check the created credit note
    
    Issue:
    The credit note is posted but its Document Type field (l10n_it_document_type)
    is empty.
    l10n_it_document_type should be computed when it doesn't have a value already
    and the state of the move is "posted".
    The credit note will be rejected when sent to SDI because this field is empty.
    
    Cause:
    In the reverse method, the field is set to False in order to be recomputed.
    However, the compute method is triggered when the state changes, but the
    credit note is already posted.
    Therefore the field will not be recomputed.
    
    Solution:
    Set the value to False before the creation of the credit note.
    So that, the field will be recomputed when posting the credit note.
    
    opw-4689755
    
    closes odoo/odoo#210642
    
    X-original-commit: 6c49d48d928ac855e25eb687e431dc48c5f64a23
    Signed-off-by: Thomas Becquevort (thbe) <thbe@odoo.com>
    Signed-off-by: Anh Thao Pham (pta) <pta@odoo.com>

--- addons/l10n_it_edi/tests/__init__.py
+++ addons/l10n_it_edi/tests/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from . import common
+from . import test_account_move_document_type
 from . import test_account_move_payment_method
 from . import test_edi_address
 from . import test_edi_export
