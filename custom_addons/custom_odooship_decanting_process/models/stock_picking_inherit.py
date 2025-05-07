6e02c7fb6c48b6b15c6df5e8f3eac24a15b60e6e
Drishti <drishti@shiperoo.com>
2025-05-07 11:43:41 +1000

    [MOD] modified discrepency receiver for MYSALE

--- custom_addons/custom_odooship_decanting_process/models/stock_picking_inherit.py
+++ custom_addons/custom_odooship_decanting_process/models/stock_picking_inherit.py
@@ -174,20 +174,35 @@ class StockPicking(models.Model):
                 "reference_1":picking.reference_1,
                 "container_number":picking.container_number,
             }
-            # Define the URLs for Shiperoo Connect
-            is_production = self.env['ir.config_parameter'].sudo().get_param('is_production_env')
-            api_url = (
+            json_data = json.dumps(payload, indent=4)
+            if picking.tenant_code_id.name == "MYSALE":
+                is_production = self.env['ir.config_parameter'].sudo().get_param('is_production_env')
+                api_url = (
+                    "https://shiperoo-connect.prod.automation.shiperoo.com/sc-file-processor/api/receipt-completion"
+                    if is_production == 'True'
+                    else "https://shiperoo-connect.uat.automation.shiperoo.com/sc-file-processor/api/receipt-completion"
+                )
+                auth = ('apiuser', 'apipass')
+
+            else:
+                is_production = self.env['ir.config_parameter'].sudo().get_param('is_production_env')
+                api_url = (
                 "https://shiperooconnect-prod.automation.shiperoo.com/api/discrepency_receiver"
                 if is_production == 'True'
                 else "https://shiperooconnect-dev.automation.shiperoo.com/api/discrepency_receiver"
             )
-            json_data = json.dumps(payload, indent=4)
+                auth = None
+
             _logger.info(f"Sending payload to {api_url}: {json_data}")
 
              # Send the payload to the API
             headers = {'Content-Type': 'application/json'}
             try:
-                response = requests.post(api_url, headers=headers, data=json_data)
+                if auth:
+                    response = requests.post(api_url, headers=headers, data=json_data, auth=auth)
+                else:
+                    response = requests.post(api_url, headers=headers, data=json_data)
+                # response = requests.post(api_url, headers=headers, data=json_data)
                 _logger.info(f"Response Status Code: {response.status_code}, Response Body: {response.text}")
 
                 if response.status_code != 200:
