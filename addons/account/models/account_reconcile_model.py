009121ea66800227a5fbfbc5e731808914c168ce
Gauthier Wala (gawa) <gawa@odoo.com>
2025-05-07 15:44:12 +0200

    [FIX] account: add default string regex transaction_details
    A new amount type match was added for reconcile lines in the form
    of `from_transaction_details`.
    There is a value that is proposed by default for `regex`, but not
    for the new one, while the matching follows the same rule.
    We do expect atleast a group in the regex currently, but the user
    has no example on which to base itself. (they currently have a
    traceback but it will be fixed in https://github.com/odoo/enterprise/pull/84769)

--- addons/account/models/account_reconcile_model.py
+++ addons/account/models/account_reconcile_model.py
@@ -90,7 +90,7 @@ class AccountReconcileModelLine(models.Model):
         self.amount_string = ''
         if self.amount_type in ('percentage', 'percentage_st_line'):
             self.amount_string = '100'
-        elif self.amount_type == 'regex':
+        elif self.amount_type in ('regex', 'from_transaction_details'):
             self.amount_string = r'([\d,]+)'
 
     @api.depends('amount_string')
