0f60cfb68acf79034a35e04398fe2824f098de75
ravb-odoo <ravb@odoo.com>
2025-05-07 14:18:23 +0530

    [IMP] hr_recruitment: improve smart button in job position form view
    This PR improves the functionality of the Employee smart button within the Job
    Position form view. Users can now directly access information related to
    employees associated with a specific job position, streamlining navigation and
    improving usability.
    
    task-4771102
    
    closes odoo/odoo#209044
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr_recruitment/models/hr_job.py
+++ addons/hr_recruitment/models/hr_job.py
@@ -52,6 +52,7 @@ class HrJob(models.Model):
             position. The Recruiter is automatically added to all meetings with the Applicant.")
     document_ids = fields.One2many('ir.attachment', compute='_compute_document_ids', string="Documents", readonly=True)
     documents_count = fields.Integer(compute='_compute_document_ids', string="Document Count")
+    employee_count = fields.Integer(compute='_compute_employee_count')
     alias_id = fields.Many2one(help="Email alias for this job position. New emails will automatically create new applicants for this job position.")
     color = fields.Integer("Color Index")
     is_favorite = fields.Boolean(compute='_compute_is_favorite', inverse='_inverse_is_favorite')
@@ -184,6 +185,20 @@ class HrJob(models.Model):
         result = {job.id: count for job, count in read_group_result}
         for job in self:
             job.application_count = result.get(job.id, 0)
+
+    def _compute_employee_count(self):
+        res = {
+            job.id: count
+            for job, count in self.env['hr.employee']._read_group(
+                domain=[
+                    ('job_id', 'in', self.ids),
+                ],
+                groupby=['job_id'],
+                aggregates=['__count'],
+            )
+        }
+        for job in self:
+            job.employee_count = res.get(job.id, 0)
 
     def _get_first_stage(self):
         self.ensure_one()
@@ -380,4 +395,25 @@ class HrJob(models.Model):
         return {
             "type": "ir.actions.client",
             "tag": "reload",
+        }
+
+    def action_open_employees(self):
+        self.ensure_one()
+        if self.env['hr.employee'].has_access('read'):
+            res_model = "hr.employee"
+        else:
+            res_model = "hr.employee.public"
+
+        return {
+            'name': _("Related Employees"),
+            'type': 'ir.actions.act_window',
+            'res_model': res_model,
+            'view_mode': 'list,kanban,form',
+            'views': [(False, 'list'), (False, 'kanban'), (False, 'form')],
+            'context': {
+                'default_job_id': self.id,
+                'search_default_group_job': 1,
+                'search_default_job_id': self.id,
+                'expand': 1
+            },
         }
