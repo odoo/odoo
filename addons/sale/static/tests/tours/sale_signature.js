7075eddb6a9334fa7e83aa5830be6fc69de0564f
Achraf (abz) <abz@odoo.com>
2025-05-07 12:21:05 +0000

    [FIX] sale: Adapt `sale_signature` test for singleapp
    Before this commit this test `test_04_portal_sale_signature_without_name_tour`
    was only working with all modules installed (classic runbot build) but
    was failing in singleapp mode (with just `sale_management`).
    
    With this commit this test works as expected in both of them.
    
    A solution was to use `alt_trigger` instead of "," to separate two css
    selector, as it was not working as expected inside tour triggers
    
    runbot error linked: https://runbot.odoo.com/odoo/runbot.build.error/161357/runbot.build.error.content/runbot.build.error.content/162311
    
    X-original-commit: b3e872a24ab218163d465900f09c8942861ae59e

--- addons/sale/static/tests/tours/sale_signature.js
+++ addons/sale/static/tests/tours/sale_signature.js
@@ -57,16 +57,19 @@ registry.category("web_tour.tours").add("sale_signature_without_name", {
         {
             content: "Sign & Pay",
             trigger: ":iframe .o_portal_sale_sidebar .btn-primary",
+            alt_trigger: ".o_portal_sale_sidebar .btn-primary",
             run: "click",
         },
         {
             content: "click submit",
             trigger: ":iframe .o_portal_sign_submit:enabled",
+            alt_trigger: ".o_portal_sign_submit:enabled",
             run: "click",
         },
         {
             content: "check error because no name",
             trigger: ':iframe .o_portal_sign_error_msg:contains("Signature is missing.")',
+            alt_trigger: '.o_portal_sign_error_msg:contains("Signature is missing.")'
         },
     ],
 });
