8f175891f0f1b410a7ace6d618ee531244864d56
Ruben Gomes <rugo@odoo.com>
2025-05-07 11:19:39 +0200

    [REF] core: test_testing_utilities
    test_lxml

--- odoo/addons/test_testing_utilities/tests/test_lxml.py
+++ odoo/addons/test_testing_utilities/tests/test_lxml.py
@@ -1,5 +1,3 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 import base64
 
 from lxml.etree import XMLSchemaError
@@ -11,26 +9,18 @@ from odoo.tools.xml_utils import _check_with_xsd
 class TestLXML(common.TransactionCase):
     def test_lxml_import_from_filestore(self):
         resolver_schema_int = b"""
-            <xsd:schema xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-                        xmlns:etype="http://codespeak.net/lxml/test/external">
+            <xsd:schema xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:etype="http://codespeak.net/lxml/test/external">
                 <xsd:import namespace="http://codespeak.net/lxml/test/external" schemaLocation="imported_schema.xsd"/>
                 <xsd:element name="a" type="etype:AType"/>
             </xsd:schema>
         """
 
-        incomplete_schema_int = b"""
-            <xsd:schema xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-                        xmlns:etype="http://codespeak.net/lxml/test/external">
-                <xsd:import namespace="http://codespeak.net/lxml/test/external" schemaLocation="non_existing_schema.xsd"/>
-                <xsd:element name="a" type="etype:AType"/>
-            </xsd:schema>
-        """
-
         imported_schema = b"""
-            <xsd:schema xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-                        targetNamespace="http://codespeak.net/lxml/test/external">
+            <xsd:schema xmlns:xsd="http://www.w3.org/2001/XMLSchema" targetNamespace="http://codespeak.net/lxml/test/external">
                 <xsd:complexType name="AType">
-                    <xsd:sequence><xsd:element name="b" type="xsd:string" minOccurs="0" maxOccurs="unbounded"/></xsd:sequence>
+                    <xsd:sequence>
+                        <xsd:element name="b" type="xsd:string" minOccurs="0" maxOccurs="unbounded"/>
+                    </xsd:sequence>
                 </xsd:complexType>
             </xsd:schema>
         """
@@ -39,17 +29,28 @@ class TestLXML(common.TransactionCase):
             'datas': base64.b64encode(resolver_schema_int),
             'name': 'resolver_schema_int.xsd',
         }, {
-            'datas': base64.b64encode(incomplete_schema_int),
-            'name': 'incomplete_schema_int.xsd',
-        }, {
             'datas': base64.b64encode(imported_schema),
             'name': 'imported_schema.xsd',
         }])
 
         _check_with_xsd("<a><b></b></a>", 'resolver_schema_int.xsd', self.env)
 
+    def test_lxml_import_from_filestore_with_incomplete_schema(self):
+        incomplete_schema_int = b"""
+            <xsd:schema xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:etype="http://codespeak.net/lxml/test/external">
+                <xsd:import namespace="http://codespeak.net/lxml/test/external" schemaLocation="non_existing_schema.xsd"/>
+                <xsd:element name="a" type="etype:AType"/>
+            </xsd:schema>
+        """
+
+        self.env['ir.attachment'].create([{
+            'datas': base64.b64encode(incomplete_schema_int),
+            'name': 'incomplete_schema_int.xsd',
+        }])
+
         with self.assertRaises(XMLSchemaError):
             _check_with_xsd("<a><b></b></a>", 'incomplete_schema_int.xsd', self.env)
 
+    def test_lxml_import_from_filestore_with_non_existing_schema(self):
         with self.assertRaises(FileNotFoundError):
             _check_with_xsd("<a><b></b></a>", 'non_existing_schema.xsd', self.env)
