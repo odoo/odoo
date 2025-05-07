43d8d41bc91e05be2d0ef4d88063db56c092206d
Brieuc-brd <brd@odoo.com>
2025-05-07 11:07:35 +0200

    [IMP] website: add `s_attributes_vertical` snippet
    This commit introduces a variant of the `s_attributes_horizontal`
    snippet using larger columns and a vertical layout.
    
    task-4778437
    Part of task-4252024

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -153,6 +153,7 @@
         'views/snippets/s_badge.xml',
         'views/snippets/s_color_blocks_2.xml',
         'views/snippets/s_attributes_horizontal.xml',
+        'views/snippets/s_attributes_vertical.xml',
         'views/snippets/s_product_list.xml',
         'views/snippets/s_mega_menu_multi_menus.xml',
         'views/snippets/s_mega_menu_menu_image_menu.xml',
