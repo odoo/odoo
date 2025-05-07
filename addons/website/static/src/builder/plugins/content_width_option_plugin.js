c71756bd6c6db8e9cb81334a60aba95b3e0e850c
Chrysanthe (chgo) <chgo@odoo.com>
2025-05-07 10:02:52 +0200

    [IMP] website: introduce s_bento_banner snippet
    This commit introduces the new `s_bento-banner` snippet.
    
    task-4777740

--- addons/website/static/src/builder/plugins/content_width_option_plugin.js
+++ addons/website/static/src/builder/plugins/content_width_option_plugin.js
@@ -13,7 +13,7 @@ class ContentWidthOptionPlugin extends Plugin {
                 template: "website.ContentWidthOption",
                 selector: "section, .s_carousel .carousel-item, .s_carousel_intro_item",
                 exclude:
-                    "[data-snippet] :not(.oe_structure) > [data-snippet],#footer > *,#o_wblog_post_content *",
+                    "[data-snippet] :not(.oe_structure) > [data-snippet],#footer > *,#o_wblog_post_content *, .s_bento_banner section[data-name='Card']",
                 applyTo:
                     ":scope > .container, :scope > .container-fluid, :scope > .o_container_small",
             }),
