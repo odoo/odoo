ceaa04d1976fd23bbcfb8b5591cc73d4ba31c649
Achraf (abz) <abz@odoo.com>
2025-05-07 12:21:05 +0000

    [FIX] sale: Adapt `sale_signature` test for singleapp
    Before this commit this test `test_04_portal_sale_signature_without_name_tour`
    was only working with all modules installed (classic runbot build) but
    was failing in singleapp mode (with just `sale_management`).
    
    With this commit this test works as expected in both of them.
    
    A solution was to use `alt_trigger` instead of "," to separate two css
    selector, as it was not working as expected inside tour triggers
    
    runbot error linked: https://runbot.odoo.com/odoo/runbot.build.error/161357/runbot.build.error.content/runbot.build.error.content/162311
    
    X-original-commit: 615681591217f710b32c6136161412d94b012e22

--- addons/sale/static/tests/tours/sale_signature.js
+++ addons/sale/static/tests/tours/sale_signature.js
@@ -58,19 +58,40 @@ registry.category("web_tour.tours").add("sale_signature_without_name", {
     steps: () => [
         {
             content: "Sign & Pay",
+<<<<<<< 4fe90167266a92c3a5941eac9a6a2084a80056ac
             trigger:
                 ".o_portal_sale_sidebar .btn-primary, :iframe .o_portal_sale_sidebar .btn-primary",
+||||||| c2187d78cf01cc3480ad82600df6ca36d3700d04
+            trigger: ":iframe .o_portal_sale_sidebar .btn-primary",
+=======
+            trigger: ":iframe .o_portal_sale_sidebar .btn-primary",
+            alt_trigger: ".o_portal_sale_sidebar .btn-primary",
+>>>>>>> 7075eddb6a9334fa7e83aa5830be6fc69de0564f
             run: "click",
         },
         {
             content: "click submit",
+<<<<<<< 4fe90167266a92c3a5941eac9a6a2084a80056ac
             trigger: ".o_portal_sign_submit:enabled, :iframe .o_portal_sign_submit:enabled",
+||||||| c2187d78cf01cc3480ad82600df6ca36d3700d04
+            trigger: ":iframe .o_portal_sign_submit:enabled",
+=======
+            trigger: ":iframe .o_portal_sign_submit:enabled",
+            alt_trigger: ".o_portal_sign_submit:enabled",
+>>>>>>> 7075eddb6a9334fa7e83aa5830be6fc69de0564f
             run: "click",
         },
         {
             content: "check error because no name",
+<<<<<<< 4fe90167266a92c3a5941eac9a6a2084a80056ac
             trigger:
                 '.o_portal_sign_error_msg:contains("Signature is missing."), :iframe .o_portal_sign_error_msg:contains("Signature is missing.")',
+||||||| c2187d78cf01cc3480ad82600df6ca36d3700d04
+            trigger: ':iframe .o_portal_sign_error_msg:contains("Signature is missing.")',
+=======
+            trigger: ':iframe .o_portal_sign_error_msg:contains("Signature is missing.")',
+            alt_trigger: '.o_portal_sign_error_msg:contains("Signature is missing.")'
+>>>>>>> 7075eddb6a9334fa7e83aa5830be6fc69de0564f
         },
     ],
 });
