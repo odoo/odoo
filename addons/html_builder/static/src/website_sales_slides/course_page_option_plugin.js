1d291b19ae4985b17522ee20c5966a1d5976bb90
Alice Gaudon <agau@odoo.com>
2025-05-09 14:19:31 +0200

    remove useless withSequence from Course Page option

--- addons/html_builder/static/src/website_sales_slides/course_page_option_plugin.js
+++ addons/html_builder/static/src/website_sales_slides/course_page_option_plugin.js
@@ -1,5 +1,4 @@
 import { Plugin } from "@html_editor/plugin";
-import { withSequence } from "@html_editor/utils/resource";
 import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
 
@@ -7,13 +6,13 @@ class CoursePageOptionPlugin extends Plugin {
     static id = "coursePageOption";
     resources = {
         builder_options: [
-            withSequence(15, {
+            {
                 template: "website_sales_slides.CoursePageOption",
                 selector: "main:has(.o_wslides_course_header)",
                 editableOnly: false,
                 title: _t("Course Page"),
                 groups: ["website.group_website_designer"],
-            }),
+            },
         ],
     };
 }
