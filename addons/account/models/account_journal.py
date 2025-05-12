c1632c0b5a0aaeedc83d8f7d78a3f6f85f56e2ab
Anh Thao Pham (pta) <pta@odoo.com>
2025-05-12 07:50:15 +0000

    [FIX] account: fix available payment methods for credit card journal
    Steps to reproduce:
    - Install account
    - Go to "Invoicing / Configuration / Accounting / Journals"
    - Create a "Credit Card" type journal
    - In "Incoming Payments" tab (or "Outgoing Payments"), try to add a line
    
    Issue:
    There is no option for "Payment Method" select field.
    If the default "Manual Payment" line is removed, it's not possible to
    add it again.
    
    Cause:
    The list of available payment methods for credit card journal is empty.
    The method that is computing the available payment methods is always
    returning False for credit card journals, but it should not.
    
    Solution:
    Compute the available payment methods normally as it is the case for
    bank and cash journals.
    
    opw-4754110
    
    closes odoo/odoo#210243
    
    X-original-commit: bee06e6966dc478ebe098d5c70a2d11da671dc2b
    Signed-off-by: Ruben Gomes (rugo) <rugo@odoo.com>
    Signed-off-by: Anh Thao Pham (pta) <pta@odoo.com>

--- addons/account/models/account_journal.py
+++ addons/account/models/account_journal.py
@@ -367,7 +367,7 @@ class AccountJournal(models.Model):
         method_information_mapping = results['method_information_mapping']
         providers_per_code = results['providers_per_code']
 
-        journal_bank_cash = self.filtered(lambda j: j.type in ('bank', 'cash'))
+        journal_bank_cash = self.filtered(lambda j: j.type in ('bank', 'cash', 'credit'))
         journal_other = self - journal_bank_cash
         journal_other.available_payment_method_ids = False
 
