7719cb424417474f702bb85fa6f98b05ef679bb8
Levi Siuzdak (sile) <sile@odoo.com>
2025-05-12 14:31:00 +0000

    [FIX] website_sale_loyalty: filter claimed rewards out of claimables
    Versions
    --------
    - 17.0+
    
    Steps
    -----
    1. Set up a Next Order Coupon program;
    2. add Free Shipping as a reward;
    3. create a coupon for current user with enough points to claim reward;
    4. add a product to your cart;
    5. go to check out;
    6. claim Free Shipping.
    
    Issue
    -----
    Free Shipping is still displayed as claimable
    
    Cause
    -----
    The `_get_claimable_and_showable_rewards` method does not factor in
    whether the rewards have already been claimed on the current order.
    
    For discounts & free products, this isn't an issue, as claiming them
    will use as many points as possible, meaning there's no points left to
    claim more, but free shipping can only be claimed once per order.
    
    Solution
    --------
    Filter out rewards that have already been claimed.
    
    opw-4784359
    
    closes odoo/odoo#210468
    
    X-original-commit: d416b517696c81d196f319b066eb80551c78d595
    Signed-off-by: Levi Siuzdak <sile@odoo.com>

--- addons/website_sale_loyalty/tests/test_free_product_reward.py
+++ addons/website_sale_loyalty/tests/test_free_product_reward.py
@@ -1,38 +1,44 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import http
-from odoo.fields import Command
-from odoo.tests import HttpCase, tagged
+from odoo import Command, http
+from odoo.tests import tagged
 
+from odoo.addons.base.tests.common import HttpCaseWithUserPortal
 from odoo.addons.website_sale.tests.common import MockRequest, WebsiteSaleCommon
 from odoo.addons.website_sale_loyalty.controllers.cart import Cart
 from odoo.addons.website_sale_loyalty.controllers.main import WebsiteSale
 
 
 @tagged('post_install', '-at_install')
-class TestFreeProductReward(HttpCase, WebsiteSaleCommon):
+class TestFreeProductReward(HttpCaseWithUserPortal, WebsiteSaleCommon):
 
-    def setUp(self):
-        super().setUp()
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
 
-        self.WebsiteSaleController = WebsiteSale()
-        self.WebsiteSaleCartController = Cart()
+        cls.WebsiteSaleCartController = Cart()
+        cls.WebsiteSaleController = WebsiteSale()
 
-        # Disable any other program
-        self.program = self.env['loyalty.program'].search([]).write({'active': False})
+        cls.website = cls.website.with_user(cls.user_portal)
+        cls.empty_cart.partner_id = cls.partner_portal
 
-        self.sofa, self.carpet = self.env['product.product'].create([
+        cls.sofa, cls.carpet = cls.env['product.product'].create([
             {
-                'name': 'Test Sofa',
+                'name': "Test Sofa",
                 'list_price': 2950.0,
                 'website_published': True,
-            }, {
-                'name': 'Test Carpet',
+            },
+            {
+                'name': "Test Carpet",
                 'list_price': 500.0,
                 'website_published': True,
             },
         ])
-        self.program = self.env['loyalty.program'].create({
+
+        # Disable any other program
+        cls.program = cls.env['loyalty.program'].search([]).write({'active': False})
+
+        cls.program = cls.env['loyalty.program'].create({
             'name': 'Get a product for free',
             'program_type': 'promotion',
             'applies_on': 'current',
@@ -42,27 +48,25 @@ class TestFreeProductReward(HttpCase, WebsiteSaleCommon):
                 'minimum_amount': 0.00,
                 'reward_point_amount': 1,
                 'reward_point_mode': 'order',
-                'product_ids': self.sofa,
+                'product_ids': cls.sofa,
             })],
             'reward_ids': [Command.create({
                 'reward_type': 'product',
-                'reward_product_id': self.carpet.id,
+                'reward_product_id': cls.carpet.id,
                 'reward_product_qty': 1,
                 'required_points': 1,
             })],
         })
 
-        installed_modules = set(self.env['ir.module.module'].search([
-            ('state', '=', 'installed'),
-        ]).mapped('name'))
-        for _ in http._generate_routing_rules(installed_modules, nodb_only=False):
+        installed_modules = cls.env['ir.module.module'].search([('state', '=', 'installed')])
+        for _ in http._generate_routing_rules(installed_modules.mapped('name'), nodb_only=False):
             pass
 
     def test_add_product_to_cart_when_it_exist_as_free_product(self):
         # This test the flow when we claim a reward in the cart page and then we
         # want to add the product again
         order = self.empty_cart
-        with MockRequest(self.env, website=self.website, sale_order_id=order.id):
+        with MockRequest(self.website.env, website=self.website, sale_order_id=order.id):
             self.WebsiteSaleCartController.add_to_cart(
                 product_template_id=self.sofa.product_tmpl_id,
                 product_id=self.sofa.id,
@@ -80,3 +84,35 @@ class TestFreeProductReward(HttpCase, WebsiteSaleCommon):
             self.assertEqual(sofa_line.product_uom_qty, 1, "Should have only 1 qty of Sofa")
             self.assertEqual(carpet_reward_line.product_uom_qty, 1, "Should have only 1 qty for the carpet as reward")
             self.assertEqual(carpet_line.product_uom_qty, 1, "Should have only 1 qty for carpet as non reward")
+
+    def test_get_claimable_free_shipping(self):
+        cart = self.empty_cart
+        self.program.write({
+            'program_type': 'next_order_coupons',
+            'applies_on': 'future',
+            'coupon_ids': [
+                Command.clear(),
+                Command.create({'partner_id': cart.partner_id.id, 'points': 100}),
+            ],
+            'reward_ids': [Command.update(self.program.reward_ids.id, {
+                'reward_type': 'shipping',
+                'reward_product_id': None,
+            })],
+        })
+        coupon = self.program.coupon_ids
+
+        with MockRequest(self.website.env, website=self.website, sale_order_id=cart.id):
+            self.assertDictEqual(cart._get_claimable_and_showable_rewards(), {
+                coupon: self.program.reward_ids,
+            })
+            self.WebsiteSaleCartController.add_to_cart(
+                product_template_id=self.sofa.product_tmpl_id,
+                product_id=self.sofa.id,
+                quantity=1,
+            )
+            self.WebsiteSaleController.claim_reward(self.program.reward_ids.id, code=coupon.code)
+            self.assertTrue(cart.order_line.reward_id)
+            self.assertFalse(
+                cart._get_claimable_and_showable_rewards(),
+                "Rewards should no longer be claimable if already claimed",
+            )
