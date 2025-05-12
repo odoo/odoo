38cbaedd7d9bd5c8f81671ecbc787443a3661aa7
hokolomopo <adrm@odoo.com>
2025-05-12 10:25:09 +0200

    [IMP] spreadsheet_dashboard: implement full screen chart
    This commit makes adaptations for the corresponding changes in
    o-spreadsheet that allows to display a chart in full screen mode.
    
    The animations of the chart is now handled by the o-spreadsheet library.
    
    Task: 4787283
    Part-of: odoo/odoo#210736
    Related: odoo/enterprise#85939
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- addons/spreadsheet_dashboard/static/src/bundle/chart/chart_js_component_dashboard.js
+++ addons/spreadsheet_dashboard/static/src/bundle/chart/chart_js_component_dashboard.js
@@ -1,61 +1,18 @@
-import { components, stores, helpers } from "@odoo/o-spreadsheet";
+import { components } from "@odoo/o-spreadsheet";
 import { patch } from "@web/core/utils/patch";
 
-const { useStore, SpreadsheetStore } = stores;
-const { deepEquals } = helpers;
-
-class ChartAnimationStore extends SpreadsheetStore {
-    animationPlayed = {};
-
-    disableAnimationForChart(chartId, chartType) {
-        this.animationPlayed[chartId] = chartType;
-    }
-}
-
 patch(components.ChartJsComponent.prototype, {
-    setup() {
-        super.setup();
-        if (this.env.model.getters.isDashboard()) {
-            this.animationStore = useStore(ChartAnimationStore);
-        }
-    },
     createChart(chartData) {
         if (this.env.model.getters.isDashboard()) {
             chartData = this.addOdooMenuPluginToChartData(chartData);
-            const chartType = this.env.model.getters.getChart(this.props.figureUI.id).type;
-            if (this.animationStore.animationPlayed[this.props.figureUI.id] !== chartType) {
-                chartData = this.enableAnimationInChartData(chartData);
-                this.animationStore.disableAnimationForChart(this.props.figureUI.id, chartType);
-            }
         }
         super.createChart(chartData);
     },
     updateChartJs(chartData) {
         if (this.env.model.getters.isDashboard()) {
             chartData = this.addOdooMenuPluginToChartData(chartData);
-            if (this.hasChartDataChanged()) {
-                chartData = this.enableAnimationInChartData(chartData);
-                this.animationStore.disableAnimationForChart(this.props.figureUI.id);
-            }
         }
         super.updateChartJs(chartData);
-    },
-    hasChartDataChanged() {
-        return !deepEquals(
-            this.currentRuntime.chartJsConfig.data,
-            this.chartRuntime.chartJsConfig.data
-        );
-    },
-    enableAnimationInChartData(chartData) {
-        return {
-            ...chartData,
-            options: {
-                ...chartData.options,
-                animation: {
-                    animateRotate: true,
-                },
-            },
-        };
     },
     addOdooMenuPluginToChartData(chartData) {
         chartData.options.plugins.chartOdooMenuPlugin = {
