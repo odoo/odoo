4fd6a558694f1b87c74c3d47dbb357bc823b553e
Victor Miguel Armenta Carrillo <vmac@odoo.com>
2025-05-13 22:20:50 +0000

    [FIX] crm: avoid cascading crm.lead2opportunity.partner wizard
    --
    Issue:
    
    When converting a lead to an opportunity, a wizard
    (`crm.lead2opportunity.partner`) is shown. This allows merging
    duplicated leads.
    
    However, if the most recently created lead is selected as the primary,
    the wizard record may be deleted unexpectedly. This usually goes
    unnoticed in the UI, as the wizard disappears after the operation.
    
    The issue becomes critical when overriding the conversion flow via a
    custom module or in future changes to the core logic. In those cases,
    the deletion of the wizard can lead to empty recordsets and errors.
    
    --
    Affected versions: 16.0 and later.
    
    --
    Explanation:
    
    The `crm.lead2opportunity.partner` model has a `Many2one` field
    `lead_id` that is being deleting on cascade which is the default
    for m2o fields on transient models.
    
    During lead conversion, action_apply() calls _action_merge() or
    _action_convert(). In _action_merge(), non-primary leads are
    unlinked. If the primary lead is the last created one, it differs
    from the one referenced by the wizard. This causes lead_id to be
    unset, triggering the cascade and deleting the wizard.
    
    --
    Impact:
    
    If a custom module overrides action_apply() or _action_merge() and
    calls super() first, it may operate on an empty recordset, leading
    to failures when trying to access data or call methods.
    
    --
    To reproduce:
    
    With debugger:
    
    - Set a breakpoint on the unlink call inside _action_merge
    - After unlink, calling self.exists() on the wizard will return
    an empty recordset
    
    With a custom module:
    
    - Override action_apply() or _action_merge()
    - Call super() first
    - Try to merge leads and select the most recent one as primary
    - The wizard record will be deleted before your custom logic executes,
    and will raise a "Record does not exist or has been deleted."
    
    X-original-commit: 5e192ba41b673116c9f05e9aefe64ab228e20cac

--- addons/crm/wizard/crm_lead_to_opportunity.py
+++ addons/crm/wizard/crm_lead_to_opportunity.py
@@ -139,6 +139,9 @@ class CrmLead2opportunityPartner(models.TransientModel):
                     'user_id': self.user_id.id,
                     'team_id': self.team_id.id,
                 })
+        if self.lead_id != result_opportunity:
+            # Prevent unwanted cascade during unlinks, keeping other operations and overrides possible
+            self.write({'lead_id': result_opportunity})
         (to_merge - result_opportunity).sudo().unlink()
         return result_opportunity
 
