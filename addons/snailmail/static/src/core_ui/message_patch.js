f352aaca511b6f43eb28f44929bd4a96e98f654a
Thomas Josse (THJO) <thjo@odoo.com>
2025-05-13 14:50:55 +0200

    [REM] snailmail: remove resend wizards
    This commit removes the handling of errors from snailmail as now these errors
    are displayed in the notification popover.
    
    It also revamps the notification error messages so that they are more explicit.
    
    task-4661147
    
    Part-of: odoo/odoo#208852
    Related: odoo/upgrade#7650
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/snailmail/static/src/core_ui/message_patch.js
+++ addons/snailmail/static/src/core_ui/message_patch.js
@@ -1,73 +1,8 @@
 import { Message } from "@mail/core/common/message";
 
-import { SnailmailError } from "./snailmail_error";
 import { SnailmailNotificationPopover } from "./snailmail_notification_popover";
-
-import { patch } from "@web/core/utils/patch";
-
-import { toRaw } from "@odoo/owl";
-
-patch(Message.prototype, {
-    onClickNotification(ev) {
-        // Keep the original notification click logic of opening the failure popover
-        if (this.message.message_type === "snailmail" && toRaw(this.message).failureNotifications.length > 0) {
-            this.onClickFailure(ev);
-        } else {
-            super.onClickNotification(ev);
-        }
-    },
-    onClickFailure() {
-        if (this.message.message_type === "snailmail") {
-            const failureType = this.message.notification_ids[0].failure_type;
-            switch (failureType) {
-                case "sn_credit":
-                case "sn_trial":
-                case "sn_price":
-                case "sn_error":
-                    this.env.services.dialog.add(SnailmailError, {
-                        failureType: failureType,
-                        messageId: this.message.id,
-                    });
-                    break;
-                case "sn_fields":
-                    this.openMissingFieldsLetterAction();
-                    break;
-                case "sn_format":
-                    this.openFormatLetterAction();
-                    break;
-            }
-        } else {
-            super.onClickFailure(...arguments);
-        }
-    },
-
-    async openMissingFieldsLetterAction() {
-        const letterIds = await this.env.services.orm.searchRead(
-            "snailmail.letter",
-            [["message_id", "=", this.message.id]],
-            ["id"]
-        );
-        this.env.services.action.doAction(
-            "snailmail.snailmail_letter_missing_required_fields_action",
-            {
-                additionalContext: {
-                    default_letter_id: letterIds[0].id,
-                },
-            }
-        );
-    },
-
-    openFormatLetterAction() {
-        this.env.services.action.doAction("snailmail.snailmail_letter_format_error_action", {
-            additionalContext: {
-                message_id: this.message.id,
-            },
-        });
-    },
-});
 
 Message.components = {
     ...Message.components,
     Popover: SnailmailNotificationPopover,
-    SnailmailError,
 };
