892fa6df5d1f6bdfd63a207fab865e9ff1e9f7fa
Max Whale <mawh@odoo.com>
2025-05-13 13:45:11 +0000

    [FIX] point_of_sale: add default weight on scale error
    In odoo/odoo#208571, a fix was made to prevent 1 unit of weight from
    added when cancelling weighing. However, this also changed the behaviour
    when a scale error (such as IoT box being disconnected) occurs,
    preventing the product from being manually entered.
    
    In this commit, we amend the condition to only stop adding the order
    line if the user closes the scale popup, but still add it on error.
    
    opw-4643243
    
    closes odoo/odoo#211234
    
    X-original-commit: 53f9539f6d8eb30434cb725f101208675ad198cb
    Related: odoo/enterprise#86214
    Signed-off-by: Yaroslav Soroko (yaso) <yaso@odoo.com>
    Signed-off-by: Max Whale (mawh) <mawh@odoo.com>

--- addons/point_of_sale/static/src/app/services/pos_store.js
+++ addons/point_of_sale/static/src/app/services/pos_store.js
@@ -891,7 +891,7 @@ export class PosStore extends WithLazyGetterTrap {
                 const weight = await makeAwaitable(this.env.services.dialog, ScaleScreen);
                 if (weight) {
                     values.qty = weight;
-                } else {
+                } else if (weight !== null) {
                     return;
                 }
             } else {
