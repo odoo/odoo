c6cc2fca1dde2c1e50cf5632a73966afe495a3fc
Shubham Thanki <shut@odoo.com>
2025-05-13 18:58:35 +0530

    [IMP] portal, *: improve login, signup and password reset UX
    *: auth_password_policy, auth_password_policy_portal,
       auth_password_policy_signup, auth_signup, web, website
    
    - We have added an eye icon that reveals the password to the user when
      they click on it.
    
    - Also added an alert message below the password input field, notifying
      the user if he has CAPS LOCK active.
    - Since we added the CAPS LOCK alert below the password input field, we
      had to move the password meter above the input field.
    - Added password strength titles (Weak, Medium, Strong) beside the
      password meter.
    - Made sure the password meter becomes visible only when we start typing
      the password.
    
    - In the reset password window, the fields with the label `Your Name` &
      `Your Email` has been removed.
    - Previously, when the user successfully reset the password, the user
      would login automatically.
    - Switched the order of the NAME and EMAIL fields in the signup form.
    
    task-3959739
    
    closes odoo/odoo#169939
    
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/web/static/tests/interactions/show_password.test.js
+++ addons/web/static/tests/interactions/show_password.test.js
@@ -1,35 +1,31 @@
-import {
-    startInteractions,
-    setupInteractionWhiteList,
-} from "@web/../tests/public/helpers";
-
+import { startInteractions, setupInteractionWhiteList } from "@web/../tests/public/helpers";
 import { describe, expect, test } from "@odoo/hoot";
-import { pointerDown, pointerUp, queryOne } from "@odoo/hoot-dom";
+import { click, queryOne } from "@odoo/hoot-dom";
 
-setupInteractionWhiteList("website.show_password");
+setupInteractionWhiteList("web.show_password");
 
 describe.current.tags("interaction_dev");
 
 const template = `
     <div class="input-group">
         <input type="password" id="password" class="form-control" required="required" name="visibility_password" />
-        <button class="btn btn-secondary" type="button" id="showPass">
+        <button class="btn btn-secondary o_show_password" type="button">
             <i class="fa fa-eye"></i>
         </button>
     </div>
 `;
 
-test("show_password is started when there is a #showPass", async () => {
+test("show_password is started when there is a .o_show_password", async () => {
     const { core } = await startInteractions(template);
     expect(core.interactions).toHaveLength(1);
 });
 
-test("input type changes on pointerdown", async () => {
+test("input type changes on clicking the eye icon", async () => {
     await startInteractions(template);
-    const showEl = queryOne("#showPass");
+    const showEl = queryOne(".o_show_password");
     expect("input").toHaveAttribute("type", "password");
-    await pointerDown(showEl);
+    await click(showEl);
     expect("input").toHaveAttribute("type", "text");
-    await pointerUp(showEl.closest("div"));
+    await click(showEl);
     expect("input").toHaveAttribute("type", "password");
 });
