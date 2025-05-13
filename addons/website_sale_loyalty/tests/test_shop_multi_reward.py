887a4edd8addd8f97171abd5a8ce96e5f177d5f2
Levi Siuzdak (sile) <sile@odoo.com>
2025-05-13 11:05:34 +0000

    [FIX] website_sale_loyalty: prevent traceback on multireward coupons
    Versions
    --------
    - 17.0+
    
    Steps
    -----
    1. Create a coupon that grants a discount;
    2. add a second reward giving free products;
    3. use a product tag to turn it into a multiproduct reward;
    4. save program, generate coupons, and copy a code;
    5. add the free product tag to two or more products;
    6. go to eCommerce;
    7. add any product to cart and go to checkout;
    8. apply coupon code;
    9. claim a reward.
    
    Issue
    -----
    - Claiming a free product results in a traceback.
    - Claiming the discount does not apply the discount.
    
    Cause
    -----
    Commit db3ffae3ef5a5 modified the reward claiming logic to allow for
    multiproduct rewards, but overlooked the possibility of the program
    having multiple rewards, multiproduct being one of them.
    
    It therefore passes all of the coupon's rewards to `_apply_reward`,
    which only expects a single reward.
    
    This is also a consequence of commit 6525d5fba40c, which moved part of
    the `claim_reward` logic to `pricelist`, without passing which reward
    was claimed to `pricelist`.
    
    Solution
    --------
    Add an optional `reward_id` parameter to `pricelist`, letting it know
    which reward we intend to claim.
    
    opw-4778945
    
    X-original-commit: dd28b84c270bafa3e475a04d9433fc8112cc1a1e

--- addons/website_sale_loyalty/tests/test_shop_multi_reward.py
+++ addons/website_sale_loyalty/tests/test_shop_multi_reward.py
@@ -1,6 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details
 
-from odoo.fields import Command
+from odoo import Command, http
 from odoo.tests import tagged
 
 from odoo.addons.website_sale.tests.common import MockRequest, WebsiteSaleCommon
@@ -10,14 +10,20 @@ from odoo.addons.website_sale_loyalty.controllers.main import WebsiteSale
 @tagged('post_install', '-at_install')
 class TestClaimReward(WebsiteSaleCommon):
 
-    def test_claim_reward_with_multi_product(self):
-        WebsiteSaleController = WebsiteSale()
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
 
-        tag = self.env['product.tag'].create({
+        cls.WebsiteSaleController = WebsiteSale()
+
+        cls.user_portal = cls._create_new_portal_user()
+        cls.partner_portal = cls.user_portal.partner_id
+
+        tag = cls.env['product.tag'].create({
             'name': 'multi reward',
         })
 
-        product1, product2 = self.env['product.product'].create([
+        cls.product1, cls.product2 = cls.env['product.product'].create([
             {
             'name': 'Test Product',
             'list_price': 10.0,
@@ -28,7 +34,7 @@ class TestClaimReward(WebsiteSaleCommon):
             'product_tag_ids': tag,
         }])
 
-        promo_program = self.env['loyalty.program'].create({
+        cls.promo_program, cls.coupon_program = cls.env['loyalty.program'].create([{
             'name': 'Free Products',
             'program_type': 'promotion',
             'applies_on': 'current',
@@ -43,14 +49,74 @@ class TestClaimReward(WebsiteSaleCommon):
                 'reward_product_tag_id': tag.id,
                 'reward_product_qty': 1,
                 'required_points': 1,
-            })]
-        })
+            })],
+        }, {
+            'name': "Multi-reward coupons",
+            'program_type': 'coupons',
+            'applies_on': 'current',
+            'trigger': 'with_code',
+            'reward_ids': [
+                Command.create({
+                    'reward_type': 'product',
+                    'reward_product_tag_id': tag.id,
+                    'reward_product_qty': 1,
+                    'required_points': 1,
+                    'discount': None,
+                }),
+                Command.create({
+                    'reward_type': 'discount',
+                    'discount': 10.0,
+                    'discount_mode': 'percent',
+                    'required_points': 1,
+                }),
+            ],
+            'coupon_ids': [Command.create({'points': 1})],
+        }])
+        cls.coupon = cls.coupon_program.coupon_ids
 
+        installed_modules = set(cls.env['ir.module.module'].search([
+            ('state', '=', 'installed'),
+        ]).mapped('name'))
+        for _ in http._generate_routing_rules(installed_modules, nodb_only=False):
+            pass
+
+    def test_claim_reward_with_multi_products(self):
+        product1, product2 = self.product2, self.product2
         order = self.empty_cart
         order.order_line = [Command.create({'product_id': product1.id})]
         order._update_programs_and_rewards()
         with MockRequest(self.env, website=self.website, sale_order_id=order.id):
-            WebsiteSaleController.claim_reward(promo_program.reward_ids[:1].id, product_id=str(product2.id))
-
+            self.WebsiteSaleController.claim_reward(
+                self.promo_program.reward_ids.id,
+                product_id=str(product2.id),
+            )
             self.assertEqual(len(order.order_line), 2, 'reward line should be added to order')
             self.assertEqual(order.order_line[1].product_id, product2, 'added reward line should should contain product 2')
+
+    def test_apply_coupon_with_multiple_rewards(self):
+        cart = self.empty_cart
+        cart.write({
+            'partner_id': self.partner_portal.id,
+            'order_line': [Command.create({'product_id': self.product1.id})],
+        })
+        cart._update_programs_and_rewards()
+        website = cart.website_id.with_user(self.user_portal)
+        discount_reward = self.coupon_program.reward_ids.filtered('discount')
+
+        with MockRequest(website.env, website=website, sale_order_id=cart.id):
+            self.WebsiteSaleController.pricelist(promo=self.coupon.code)
+            self.assertFalse(cart.order_line.reward_id)
+
+            self.WebsiteSaleController.claim_reward(discount_reward.id, code=self.coupon.code)
+            self.assertTrue(cart.order_line.reward_id)
+            self.assertIn(
+                discount_reward.discount_line_product_id,
+                cart.order_line.product_id,
+                "Discount product should be added to order",
+            )
+            self.assertAlmostEqual(
+                self.product1.list_price * 0.9,
+                cart.amount_untaxed,
+                delta=cart.currency_id.rounding,
+                msg="10% discount should be applied",
+            )
