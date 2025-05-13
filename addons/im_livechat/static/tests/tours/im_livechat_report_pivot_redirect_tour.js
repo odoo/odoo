c40f6ade8c2d43879dcca240d51edaafac250cb7
Akash Parmar <akap@odoo.com>
2025-05-13 19:07:34 +0530

    [IMP] im_livechat: open form view directly from pivot if only one record
    Purpose of this commit:
    
    For Livechat Support Channel Report when a pivot cell is clicked and the
    resulting domain returns only one record,
    the form view is now opened directly, bypassing the list view.
    
    task-4753032
    
    closes odoo/odoo#210028
    
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

---
+++ addons/im_livechat/static/tests/tours/im_livechat_report_pivot_redirect_tour.js
@@ -0,0 +1,36 @@
+import { registry } from "@web/core/registry";
+
+registry.category("web_tour.tours").add("im_livechat_report_pivot_redirect_tour", {
+    steps: () => [
+        {
+            content: "open command palette",
+            trigger: "body",
+            run: "press ctrl+k",
+        },
+        {
+            trigger: ".o_command_palette_search input",
+            run: "fill /Reporting",
+        },
+        {
+            trigger: ".o_command:contains(Agents)",
+            run: "click",
+        },
+        {
+            content: "click on a cell that has a single related record",
+            trigger: ".o_pivot table tbody tr:eq(2) td:eq(0)",
+            run: "click",
+        },
+        { trigger: ".o-mail-Discuss" },
+        {
+            content: "go back to the pivot view.",
+            trigger: ".o_back_button",
+            run: "click",
+        },
+        {
+            content: "click on a cell that has more than one related record",
+            trigger: ".o_pivot table tbody tr:eq(0) td:eq(0)",
+            run: "click",
+        },
+        { trigger: ".o_list_view" },
+    ],
+});
