0b17d7a3688220f4b71250534d6a34bf2416ba82
Levi Siuzdak (sile) <sile@odoo.com>
2025-05-13 11:05:34 +0000

    [FIX] website_sale_loyalty: prevent traceback on multireward coupons
    Versions
    --------
    - 17.0+
    
    Steps
    -----
    1. Create a coupon that grants a discount;
    2. add a second reward giving free products;
    3. use a product tag to turn it into a multiproduct reward;
    4. save program, generate coupons, and copy a code;
    5. add the free product tag to two or more products;
    6. go to eCommerce;
    7. add any product to cart and go to checkout;
    8. apply coupon code;
    9. claim a reward.
    
    Issue
    -----
    - Claiming a free product results in a traceback.
    - Claiming the discount does not apply the discount.
    
    Cause
    -----
    Commit db3ffae3ef5a5 modified the reward claiming logic to allow for
    multiproduct rewards, but overlooked the possibility of the program
    having multiple rewards, multiproduct being one of them.
    
    It therefore passes all of the coupon's rewards to `_apply_reward`,
    which only expects a single reward.
    
    This is also a consequence of commit dfd9f19da344, which moved part of
    the `claim_reward` logic to `pricelist`, without passing which reward
    was claimed to `pricelist`.
    
    Solution
    --------
    Add an optional `reward_id` parameter to `pricelist`, letting it know
    which reward we intend to claim.
    
    opw-4778945
    
    closes odoo/odoo#210817
    
    X-original-commit: 4441dab8a9a053e0bf7c6e367adcf94268ea2c51
    Signed-off-by: Levi Siuzdak <sile@odoo.com>

--- addons/website_sale_loyalty/controllers/main.py
+++ addons/website_sale_loyalty/controllers/main.py
@@ -12,7 +12,7 @@ from odoo.addons.website_sale.controllers import main
 class WebsiteSale(main.WebsiteSale):
 
     @route()
-    def pricelist(self, promo, **post):
+    def pricelist(self, promo, reward_id=None, **post):
         if not (order_sudo := request.cart):
             return request.redirect('/shop')
         coupon_status = order_sudo._try_apply_code(promo)
@@ -24,8 +24,12 @@ class WebsiteSale(main.WebsiteSale):
             reward_successfully_applied = True
             if len(coupon_status) == 1:
                 coupon, rewards = next(iter(coupon_status.items()))
-                if request.env.context.get('product_id') or (len(rewards) == 1 and not rewards.multi_product):
-                    reward_successfully_applied = self._apply_reward(order_sudo, rewards, coupon)
+                if len(rewards) == 1:
+                    reward = rewards
+                else:
+                    reward = reward_id in rewards.ids and rewards.browse(reward_id)
+                if reward and (not reward.multi_product or request.env.get('product_id')):
+                    reward_successfully_applied = self._apply_reward(order_sudo, reward, coupon)
 
             if reward_successfully_applied:
                 request.session['successful_code'] = promo
@@ -91,7 +95,7 @@ class WebsiteSale(main.WebsiteSale):
                         and program_sudo.applies_on == 'future'
                         and program_sudo.program_type not in ('ewallet', 'loyalty'))
                 ):
-                    return self.pricelist(code)
+                    return self.pricelist(code, reward_id=reward_id)
         if coupon:
             self._apply_reward(order_sudo, reward_sudo, coupon)
         return request.redirect(redirect)
