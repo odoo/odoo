89d429c9429d0e03f416cab4116e6ed230792c80
roen-odoo <roen@odoo.com>
2025-05-14 11:08:34 +0000

    [FIX] pos_sale_loyalty: ignore sale loyalty
    When a loyalty program is already applied in a sale order, the same
    loyalty program could also be triggered in the POS.
    
    Steps to reproduce:
    -------------------
    * Create a simple loyalty program with a discount of 10% on order that
      is applied automatically if the total of the order is more than 1$
    * Create a sale order with a product of 10$ and apply the loyalty
      program
    * You will have a disount of 1$ in the sale order
    * Open the PoS and settle the order
    * A new discount will appear in the PoS with a discount of 0.9$.
    > Observation: You now have 2 discounts coming from the same program
    
    Why the fix:
    ------------
    We now adapt the `_programIsApplicable` function to check if the program
    was already used in the sale order. If it was, we ignore the program in
    the PoS.
    
    opw-4381890
    
    closes odoo/odoo#213599
    
    X-original-commit: 6ce03c6d3192ef549a90ac6a670c33b96dba013f
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: Robin Engels (roen) <roen@odoo.com>

--- addons/pos_sale_loyalty/static/src/overrides/models/pos_order_line.js
+++ addons/pos_sale_loyalty/static/src/overrides/models/pos_order_line.js
@@ -1,4 +1,5 @@
 import { PosOrderline } from "@point_of_sale/app/models/pos_order_line";
+import { PosOrder } from "@point_of_sale/app/models/pos_order";
 import { patch } from "@web/core/utils/patch";
 
 patch(PosOrderline.prototype, {
@@ -17,5 +18,12 @@ patch(PosOrderline.prototype, {
         } else {
             super.setQuantityFromSOL(...arguments);
         }
+    },
+});
+
+patch(PosOrder.prototype, {
+    isLineValidForLoyaltyPoints(line) {
+        const result = super.isLineValidForLoyaltyPoints(line);
+        return !line.sale_order_origin_id && result;
     },
 });
