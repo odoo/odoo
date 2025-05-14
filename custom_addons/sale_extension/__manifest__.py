1c625c1f8bca77ea74756b710fc0febe999681a3
manucasta04 <mcastagnari@fi.uba.ar>
2025-05-13 21:05:17 -0300

    Se eliminaron los campos innecesarios de la view de customers

--- custom_addons/sale_extension/__manifest__.py
+++ custom_addons/sale_extension/__manifest__.py
@@ -5,6 +5,7 @@
     'data': [
         'views/sale_order_form_inherit.xml',
         'views/product_template_lead_time.xml',
+        'views/res_partner_form_inherit.xml'
     ],
     'installable': True,
     'application':True,
