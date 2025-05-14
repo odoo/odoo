f5eefe8210fcec326c204d136c9e9c270ced8346
Benjamin Vray <bvr@odoo.com>
2025-05-14 14:11:50 +0000

    [FIX] web_editor, website: prevent snippet tabs nav items removal
    Steps to reproduce:
    
    1. Go to Website edit mode.
    2. Add a "Tabs" block.
    3. Double-click a tab title to select it.
    4. Start typing a new title.
    
    Expected: The tab title is replaced with the new text.
    Actual: The tab is deleted and merged with the next one.
    
    This commit fixes the bug by preventing the deletion of elements with
    the `data-bs-toggle: 'tab'` attribute. These are clearly not elements
    that an user should ever be allowed to delete.
    
    This commit also adds a test for the "Tabs" snippet to prevent the bug
    from coming back. It also covers other flows that were previously known
    to be buggy in the "Tabs" snippet.
    
    opw-4791238
    
    closes odoo/odoo#210187
    
    X-original-commit: 56a9de705bf8522313dcf9785f68da173210d68e
    Signed-off-by: Serge Bayet (seba) <seba@odoo.com>
    Signed-off-by: Benjamin Vray (bvr) <bvr@odoo.com>

--- addons/website/tests/test_snippets.py
+++ addons/website/tests/test_snippets.py
@@ -117,3 +117,6 @@ class TestSnippets(HttpCase):
 
     def test_snippet_popup_open_on_top(self):
         self.start_tour(self.env['website'].get_client_action_url('/'), 'snippet_popup_open_on_top', login='admin')
+
+    def test_tabs_snippet(self):
+        self.start_tour(self.env["website"].get_client_action_url("/"), "snippet_tabs", login="admin")
