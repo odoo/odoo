f53eeb4806e8dc43e4c59e3b4c87d6f39f50bb1f
st-yes <soty@odoo.com>
2025-05-15 10:00:05 +0000

    [FIX] microsoft_calendar: prevent archive of event
    issue
    
    when you change an odoo event organizer the original gets archived and a new one is created
    this behavior should only be happening with microsoft synced events
    
    steps to reproduce:
    
    - install microsoft_calendar
    - create new event
    - change organizer and save
    
    the event is archived
    
    opw-4765254
    
    closes odoo/odoo#211525
    
    X-original-commit: e816516edb080e06e338dcd07a3a371ef0789ccc
    Signed-off-by: Arnaud Joset (arj) <arj@odoo.com>
    Signed-off-by: Soukaina Tyes (soty) <soty@odoo.com>

--- addons/microsoft_calendar/tests/test_create_events.py
+++ addons/microsoft_calendar/tests/test_create_events.py
@@ -851,3 +851,27 @@ class TestSyncOdoo2MicrosoftMail(TestCommon, MailCommon):
                 elif expect_mail:
                     mock_insert.assert_not_called()
                     self.assertMailMail(attendee, 'sent', author=(organizer or create_user).partner_id)
+
+    def test_change_organizer_pure_odoo_event(self):
+        """
+        Test that changing organizer on a pure Odoo event (not synced with Microsoft)
+        does not archive the event.
+        """
+        self.organizer_user.microsoft_synchronization_stopped = True
+        event = self.env["calendar.event"].with_user(self.organizer_user).create({
+            'name': "Pure Odoo Event",
+            'start': datetime(2024, 1, 1, 10, 0),
+            'stop': datetime(2024, 1, 1, 11, 0),
+            'user_id': self.organizer_user.id,
+            'partner_ids': [Command.set([self.organizer_user.partner_id.id, self.attendee_user.partner_id.id])],
+        })
+
+        self.assertFalse(event.microsoft_id)
+        self.assertTrue(event.active)
+
+        event.write({
+            'user_id': self.attendee_user.id,
+        })
+
+        self.assertTrue(event.active, "Pure Odoo event should not be archived when changing organizer")
+        self.assertEqual(event.user_id, self.attendee_user, "Organizer should be updated")
