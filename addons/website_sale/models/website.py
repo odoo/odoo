c81f2255a8125c66866d0985a4e3949d2c274cbe
Harrison Hutton <hahu@odoo.com>
2025-05-15 18:58:05 +0000

    [FIX] website_sale: unable to open the website of another company
    Since 382819d7a63c586692cd4b8cff4fc80d2dfa24d3, customers belonging
    to another company are not allowed to place orders through the
    ecommerce.
    
    Nevertheless, those multi-company checks are only triggered on cart
    creation or when the order customer is changed. So if you already
    had a cart created in a company before modifying the company of your
    partner, you would only get the multi-company error(s) when going
    through the checkout.
    
    Since a5cfa17e98cddf0e339406f2dbae783615786454, the process
    of finding an abandoned cart for the current customer (if they
    don't have a cart in their session) was slightly modified.
    Instead of manually recomputing the pricelist and fiscal position
    when 'resurrecting' an abandoned cart, we trigger the recomputation
    by writing on the `partner_id` field of the cart.
    
    Because of that change, if you had an 'invalid' cart whose
    company wasn't allowed for your partner, it would fail and raise
    whenever you tried to access the website of the company you weren't
    allowed to buy from anymore.
    
    This commit fixes this situation by skipping the abandoned cart
    search when the partner company doesn't match the current website one.
    
    opw-4723090
    
    closes odoo/odoo#210930
    
    X-original-commit: fdded2264b22f119f6f61509f7fa220a2591f865
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>
    Signed-off-by: Harrison Hutton (hahu) <hahu@odoo.com>
    Co-authored-by: Victor Feyens <vfe@odoo.com>

--- addons/website_sale/models/website.py
+++ addons/website_sale/models/website.py
@@ -528,6 +528,7 @@ class Website(models.Model):
         SaleOrderSudo = self.env['sale.order'].sudo()
 
         sale_order_sudo = SaleOrderSudo
+        partner_sudo = self.env.user.partner_id
         if CART_SESSION_CACHE_KEY in request.session:
             sale_order_sudo = SaleOrderSudo.browse(request.session[CART_SESSION_CACHE_KEY])
             if sale_order_sudo and (
@@ -546,13 +547,22 @@ class Website(models.Model):
             if (
                 sale_order_sudo
                 and not self.env.user._is_public()
-                and self.env.user.partner_id.id != sale_order_sudo.partner_id.id
+                and partner_sudo.id != sale_order_sudo.partner_id.id
                 and not request.env.cr.readonly
             ):
-                sale_order_sudo._update_address(self.env.user.partner_id.id, ['partner_id'])
-        elif self.env.user and not self.env.user._is_public():  # Search for abandonned cart.
+                sale_order_sudo._update_address(partner_sudo.id, ['partner_id'])
+        elif (
+            self.env.user
+            and not self.env.user._is_public()
+            # If the company of the partner doesn't allow them to buy from this website, updating
+            # the cart customer would raise because of multi-company checks.
+            # No abandoned cart should be returned in this situation.
+            and partner_sudo.filtered_domain(
+                self.env['res.partner']._check_company_domain(self.company_id.id)
+            )
+        ):  # Search for abandonned cart.
             abandonned_cart_sudo = SaleOrderSudo.search([
-                ('partner_id', '=', self.env.user.partner_id.id),
+                ('partner_id', '=', partner_sudo.id),
                 ('website_id', '=', self.id),
                 ('state', '=', 'draft'),
             ], limit=1)
@@ -560,7 +570,7 @@ class Website(models.Model):
                 if not request.env.cr.readonly:
                     # Force the recomputation of the pricelist and fiscal position when resurrecting
                     # an abandonned cart
-                    abandonned_cart_sudo._update_address(self.env.user.partner_id.id, ['partner_id'])
+                    abandonned_cart_sudo._update_address(partner_sudo.id, ['partner_id'])
                     abandonned_cart_sudo._verify_cart()
                 sale_order_sudo = abandonned_cart_sudo
 
