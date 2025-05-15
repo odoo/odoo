84af715117c2d3322ea876232c9107d40806398b
abd-msyukyu-odoo <abd@odoo.com>
2025-05-15 15:23:07 +0000

    [IMP] mail: increase the clickable area for activities in a list view
    After this commit, the full width of the Activities column in a list view will
    be clickable, for ease of use.
    
    task-4525830
    
    X-original-commit: 3160af53ea120f36875e2169a98209e12e090667

--- addons/mail/static/src/views/web/fields/list_activity/list_activity.js
+++ addons/mail/static/src/views/web/fields/list_activity/list_activity.js
@@ -6,8 +6,22 @@ import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
 import { standardFieldProps } from "@web/views/fields/standard_field_props";
 
+class ListActivityButton extends ActivityButton {
+    static props = {
+        ...ActivityButton.props,
+        slots: Object,
+    };
+    static template = "mail.ListActivityButton";
+
+    setup() {
+        super.setup();
+        this.defaultActivityStateClass = "";
+        this.defaultActivityDecorationClass = "fa-clock-o";
+    }
+}
+
 export class ListActivity extends Component {
-    static components = { ActivityButton };
+    static components = { ActivityButton: ListActivityButton };
     // also used in children, in particular in ActivityButton
     static fieldDependencies = [
         { name: "activity_exception_decoration", type: "selection", selection: [] },
