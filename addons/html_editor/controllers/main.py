3c3ce95b70997d319f242f22bc878562148256b4
Arib Ansari <aans@odoo.com>
2025-05-15 17:16:57 +0530

    [FIX] html_editor: more complete check for internal and frontend urls
    Cherry pick of two commits from 18.4
    
    Commit 1: 379d32430f9ffc7c6ab42ca6f7b4600f6dff15df
    
    Previously, the link popover did not support frontend website pages
    (e.g., /contactus, /shop, etc.).
    
    Steps to reproduce:
    - Enter edit mode.
    - Click on a link to a frontend page, such as "Contact Us".
    - An error was thrown in the browser console.
    - Also, the link popover did not show the page description (even if it
      existed).
    
    This commit:
    - Fixes the error that occurred in the browser console.
    - Adds support for frontend website pages in the link popover.
    - Displays the page description in the linkpopover, if available.
      (The page description refers to the SEO field that can be set via:
      Site > This Page > Optimize SEO > Description)
    
    Commit 2: b8908f347ee3c6aab45f2e226dd027b1b2f30161
    
    Before this commit: the condition to check if an url is internal is not complete
    as the user could user the odoo instance domain instead of the real domain. The
    check if an internal url is a frontend one is rather naive as there are cases
    where the url ends with a number but actually not leading to a record.
    
    Reproduction for the second use case:
    1. create a link with frontend url for example `/shop/category/16`
    2. click on the link, when it loads the preview, a warning pops up
    
    After this commit, the cases explained above are included.
    
    task-4971829

--- addons/html_editor/controllers/main.py
+++ addons/html_editor/controllers/main.py
@@ -7,6 +7,7 @@ import werkzeug.exceptions
 import werkzeug.urls
 import requests
 from os.path import join as opj
+from urllib.parse import urlparse
 
 from odoo import _, http, tools, SUPERUSER_ID
 from odoo.addons.html_editor.tools import get_video_url_data
@@ -589,7 +590,24 @@ class HTML_Editor(http.Controller):
         try:
             Actions = request.env['ir.actions.actions']
             context = dict(request.env.context)
-            words = preview_url.strip('/').split('/')
+            parsed_preview_url = urlparse(preview_url)
+            words = parsed_preview_url.path.strip('/').split('/')
+            last_segment = words[-1]
+
+            if not (
+                last_segment.isnumeric()
+                and (
+                    parsed_preview_url.path.startswith("/odoo")
+                    or parsed_preview_url.path.startswith("/web")
+                    or parsed_preview_url.path.startswith("/@/")
+                )
+            ):
+                # this could be a frontend or an external page
+                link_preview_data = self.link_preview_metadata(preview_url)
+                result = {}
+                if link_preview_data and link_preview_data.get('og_description'):
+                    result['description'] = link_preview_data['og_description']
+                return result
 
             record_id = int(words.pop())
             action_name = words.pop()
