e2910e67470396b0ae708f143852d0668c9b4704
“Beekan” <mmha@odoo.com>
2025-05-15 13:13:42 +0000

    [PERF] stock: compute the child locations once per move in putaway
    Previously when validating a movement with a location that is used
    in a rule, move_dest_ids was set which led to it going to check
    the putaway strategy. The bottleneck was the compuation of the
    computed field child_internal_location_ids which is the same across
    all smls.
    
    This PR utilizes the fact that the smls in the same stock_move
    would have the same destination location and by proxy the same
    child_internal_location_ids
    
    closes odoo/odoo#236923
    
    X-original-commit: a46f6463765b5331fa7be8b47d7053bdb438b2cd
    Signed-off-by: Quentin Wolfs (quwo) <quwo@odoo.com>

--- addons/stock/models/stock_move_line.py
+++ addons/stock/models/stock_move_line.py
@@ -258,18 +258,19 @@ class StockMoveLine(models.Model):
         if self.env.context.get('avoid_putaway_rules'):
             return
         self = self.with_context(do_not_unreserve=True)
-        for package, smls in groupby(self, lambda sml: sml.result_package_id.outermost_package_id):
+        for (package), smls in groupby(self, lambda sml: (sml.result_package_id.outermost_package_id)):
             smls = self.env['stock.move.line'].concat(*smls)
+            locations = smls.move_id.location_dest_id.child_internal_location_ids
             excluded_smls = set(smls.ids)
             if package.package_type_id:
-                best_loc = smls.move_id.location_dest_id.with_context(exclude_sml_ids=excluded_smls, products=smls.product_id)._get_putaway_strategy(self.env['product.product'], package=package)
+                best_loc = smls.move_id.location_dest_id.with_context(exclude_sml_ids=excluded_smls, products=smls.product_id, locations=locations)._get_putaway_strategy(self.env['product.product'], package=package)
                 smls.location_dest_id = best_loc
             elif package:
                 used_locations = set()
                 for sml in smls:
                     if len(used_locations) > 1:
                         break
-                    sml.location_dest_id = sml.move_id.location_dest_id.with_context(exclude_sml_ids=excluded_smls)._get_putaway_strategy(sml.product_id, quantity=sml.quantity)
+                    sml.location_dest_id = sml.move_id.location_dest_id.with_context(exclude_sml_ids=excluded_smls, locations=locations)._get_putaway_strategy(sml.product_id, quantity=sml.quantity)
                     excluded_smls.discard(sml.id)
                     used_locations.add(sml.location_dest_id)
                 if len(used_locations) > 1:
@@ -277,7 +278,7 @@ class StockMoveLine(models.Model):
                         grouped_smls.location_dest_id = move.location_dest_id
             else:
                 for sml in smls:
-                    putaway_loc_id = sml.move_id.location_dest_id.with_context(exclude_sml_ids=excluded_smls)._get_putaway_strategy(
+                    putaway_loc_id = sml.move_id.location_dest_id.with_context(exclude_sml_ids=excluded_smls, locations=locations)._get_putaway_strategy(
                         sml.product_id, quantity=sml.quantity, packaging=sml.move_id.packaging_uom_id,
                     )
                     if putaway_loc_id != sml.location_dest_id:
