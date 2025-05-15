odoo/odoo

commit de26b96cda16f1503de77b442e3d0143b98b0751

[IMP] mail: remove the use of read in activity _to_store
Currently the `_to_store` of `mail.activity` is actually returning all fields (just using `read()` result).
In order to improve _to_store and also to reduce the number of useless fields we
send, we should actually list only the fields we need (also taking care of
overrides if necessary). In a similar pattern as other _to_store methods.

Task-4364655

closes odoo/odoo#189598

Related: odoo/enterprise#75172
Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

addons/mail/models/mail_activity.py
@@ -588,7 +588,20 @@ class MailActivity(models.Model):
 
     def _to_store_defaults(self):
         return [
-            "all",
+            "activity_category",
+            "activity_type_id",
+            "can_write",
+            "chaining_type",
+            "create_date",
+            "create_uid",
+            "date_deadline",
+            "date_done",
+            "icon",
+            "note",
+            "res_id",
+            "res_model",
+            "state",
+            "summary",
             Store.Many("attachment_ids", ["name"]),
             Store.Attr(
                 "mail_template_ids",
@@ -596,13 +609,6 @@ class MailActivity(models.Model):
             ),
             Store.One("persona", value=lambda activity: activity.user_id.partner_id),
         ]
-
-    def _to_store(self, store: Store, fields):
-        if "all" in fields:
-            fields.remove("all")
-            for activity in self:
-                store.add(activity, activity.read()[0])
-        store.add_records_fields(self, fields)
 
     @api.readonly
     @api.model

addons/calendar/models/mail_activity.py
@@ -2,6 +2,7 @@
 
 from odoo import models, fields, tools, _
 from odoo.tools import is_html_empty
+from odoo.addons.mail.tools.discuss import Store
 
 
 class MailActivity(models.Model):
@@ -48,3 +49,6 @@ class MailActivity(models.Model):
         res = self.unlink()
         events.unlink()
         return res
+
+    def _to_store_defaults(self):
+        return super()._to_store_defaults() + [Store.One("calendar_event_id", [])]

addons/calendar/static/src/activity/@types/models.d.ts
@@ -1,0 +1,5 @@
+declare module "models" {
+    interface Activity {
+        calendar_event_id: Number
+    }
+}

addons/calendar/static/tests/mock_server/mock_models/mail_activity.js
@@ -24,4 +24,16 @@ export class MailActivity extends mailModels.MailActivity {
         this.env["calendar.event"].unlink(eventIds);
         return res;
     }
+
+    /** @param {number[]} ids */
+    _to_store(ids, store) {
+        super._to_store(...arguments);
+        for (const activity of this.browse(ids)) {
+            if (activity.calendar_event_id) {
+                store.add(this.browse(activity.id), {
+                    calendar_event_id: activity.calendar_event_id,
+                });
+            }
+        }
+    }
 }

addons/mail/static/src/core/common/activity_model.js
@@ -99,6 +99,10 @@ export class Activity extends Record {
     write_date;
     /** @type {[number, string]} */
     write_uid;
+
+    serialize() {
+        return JSON.parse(JSON.stringify(this.toData()));
+    }
 }
 
 Activity.register();

addons/mail/static/src/core/web/@types/models.d.ts
@@ -1,5 +1,3 @@
-import { DateTime } from "luxon";
-
 declare module "models" {
     export interface Discuss  {
         inbox: Thread,

addons/mail/static/src/core/web/activity_model_patch.js
@@ -1,5 +1,4 @@
 import { Activity } from "@mail/core/common/activity_model";
-import { Record } from "@mail/core/common/record";
 import { formatDate, formatDateTime } from "@web/core/l10n/dates";
 import { _t } from "@web/core/l10n/translation";
 
@@ -70,8 +69,5 @@ patch(Activity.prototype, {
                 payload: { id: this.id },
             });
         }
-    },
-    serialize() {
-        return JSON.parse(JSON.stringify(this.toData()));
     },
 });

addons/mail/static/tests/mock_server/mock_models/mail_activity.js
@@ -81,7 +81,21 @@ export class MailActivity extends models.ServerModel {
         const ResUsers = this.env["res.users"];
 
         for (const activity of this.browse(ids)) {
-            const [data] = this.read(activity.id);
+            const [data] = this._read_format(activity.id, [
+                "activity_category",
+                "activity_type_id",
+                "can_write",
+                "chaining_type",
+                "create_date",
+                "create_uid",
+                "date_deadline",
+                "date_done",
+                "note",
+                "res_id",
+                "res_model",
+                "state",
+                "summary",
+            ]);
             // simulate computes
             const activityType = data.activity_type_id
                 ? MailActivityType.find((r) => r.id === data.activity_type_id[0])

commit b5523f0022ce8e94b3d024b5da22252d20bb708d

[FIX] mail: fix info displayed on activity
This commit fixes an issue introduced by de26b96cda16 where some fields of the
activity records weren't properly read. Notably `activity_type_id` and
`create_uid` which only returned their ID instead of returning (ID, name).

This led to a wrong display of the additional informations inside the activities
in the Chatter. Where the type of activity and its creator were empty.
To fix this, we reimplement the _to_store method so that we properly read the
fields while letting the store add their specific fields.

task-4794562

addons/mail/models/mail_activity.py
@@ -609,6 +609,15 @@ class MailActivity(models.Model):
     def activity_format(self):
         return Store(self).get_result()
 
+    def _to_store(self, store, fields, **kwargs):
+        readable_fields = [field for field in fields if isinstance(field, str)]
+        store_fields = set(fields) - set(readable_fields)
+        records = self._read_format(list(readable_fields))
+        for record, record_data in zip(self, records):
+            store.add_model_values(record._name, record_data)
+        if store_fields:
+            store.add_records_fields(self, list(store_fields))
+
     def _to_store_defaults(self):
         return [
             "activity_category",