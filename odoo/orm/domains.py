00c4f9aae3a528dc7aa30e43fe8d9a5e825bd25b
Krzysztof Magusiak (krma) <krma@odoo.com>
2025-05-16 09:10:55 +0000

    [IMP] orm: compat for search with 'any!'
    This provides a compatibility layer for field search methods that do not
    implement operator 'any!'.
    
    Part-of: odoo/odoo#210601
    Related: odoo/enterprise#85894
    Signed-off-by: Raphael Collet <rco@odoo.com>

--- odoo/orm/domains.py
+++ odoo/orm/domains.py
@@ -996,6 +996,17 @@ class DomainCondition(Domain):
             computed_domain = field.determine_domain(model, inversed_opeator, value)
             if computed_domain is not NotImplemented:
                 return ~Domain(computed_domain, internal=True)
+        # compatibility for any!
+        try:
+            if operator in ('any!', 'not any!'):
+                # Not strictly equivalent! If a search is executed, it will be done using sudo.
+                computed_domain = DomainCondition(self.field_expr, operator.rstrip('!'), value)
+                computed_domain = computed_domain._optimize_field_search_method(model.sudo())
+                _logger.warning("Field %s should implement any! operator", field)
+                return computed_domain
+        except (NotImplementedError, UserError) as e:
+            if original_exception is None:
+                original_exception = e
         # backward compatibility to implement only '=' or '!='
         try:
             if operator == 'in':
