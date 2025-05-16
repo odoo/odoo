f1405d678c1e0d3b4f279e8c5d54b8d711e2692d
Xavier Morel <xmo@odoo.com>
2025-05-16 13:49:57 +0200

    [FIX] core: hide "failed to fetch" errors after tour termination
    Complement of #209902 which only fixed the "Exception received after
    termination", I missed that we had two sites: the other being "Error
    received after termination" and is in fact the more common of the
    two.
    
    For this case, downgrade the message to `logging.RUNBOT` for now so it
    remains visible in the *extremely* unlikely case someone would see it
    and want to do something about it instead of just ignoring it entirely.

--- odoo/tests/common.py
+++ odoo/tests/common.py
@@ -1286,6 +1286,8 @@ class ChromeBrowser:
 
         log_type = type
         _logger = self._logger.getChild('browser')
+        if self._result.done() and 'failed to fetch' in message.casefold():
+            log_type = 'dir'
         _logger.log(
             self._TO_LEVEL.get(log_type, logging.INFO),
             "%s%s",
@@ -1402,6 +1404,7 @@ which leads to stray network requests and inconsistencies."""
         'info': logging.INFO,
         'warning': logging.WARNING,
         'error': logging.ERROR,
+        'dir': logging.RUNBOT,
         # TODO: what do with
         # dir, dirxml, table, trace, clear, startGroup, startGroupCollapsed,
         # endGroup, assert, profile, profileEnd, count, timeEnd
