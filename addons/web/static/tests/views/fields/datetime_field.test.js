cda30fe63d7ec35a6739c6eb3d5e6f295caf2471
Aaron Bohy <aab@odoo.com>
2025-05-16 09:12:43 +0000

    [FIX] web: list: compute correct widths for all date(time) formats
    In list views, we have a custom logic for column widths which aims
    at optimizing as much as possible the available space, and freezing
    the table such that it doesn't flicker upon user interaction (like
    editing, adding records, browsing through pages...).
    
    This logic defines, for some field types, the exact width that
    values need to be properly displayed, especially for dates and
    datetimes for which we know upfront the format, i.e. the length of
    values.
    
    However, before this commit, the logic was incorrect. It didn't
    take into the account the fact that date and time formats are
    language dependant. It assumed that the required space for the
    english formats (+ a bit of security margin) was enough. Moreover,
    the fonts may obviously has an impact as well (some fonts requiring
    more space to display the same text, than others).
    
    As a consequence, on macos and, for instance, in deutch, date and
    datetime values were trimmed, which is something we never want:
    dates and datetimes should always be fully displayed. This is even
    worse in some languages, like arabic, basque or chinese, in which
    the month and/or the day of week is displayed in letters.
    
    This commit comes with a more elaborated solution to deal with
    those variable date and time formats + fonts. We no longer
    hardcode the ideal widths of dates and datetimes, but instead
    compute it (lazily) once, by rendering values in the DOM.
    
    This commit also improves the datetime and daterange cases with
    option "show_time" set to false: in that case, we know those fields
    only require the width of date values, not datetime, so we can
    shrink their columns.
    
    closes odoo/odoo#210839
    
    X-original-commit: 016e48188d3af2ff0bf7ac9ac35228ba6c4c09f3
    Signed-off-by: Adrien Dieudonné (adr) <adr@odoo.com>
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/tests/views/fields/datetime_field.test.js
+++ addons/web/static/tests/views/fields/datetime_field.test.js
@@ -1,5 +1,13 @@
-import { expect, test } from "@odoo/hoot";
-import { click, edit, queryAll, queryAllTexts, select } from "@odoo/hoot-dom";
+import { after, expect, test } from "@odoo/hoot";
+import {
+    click,
+    edit,
+    queryAll,
+    queryAllProperties,
+    queryAllTexts,
+    resize,
+    select,
+} from "@odoo/hoot-dom";
 import { animationFrame, mockTimeZone } from "@odoo/hoot-mock";
 import {
     clickSave,
@@ -10,7 +18,6 @@ import {
     mountView,
     onRpc,
 } from "@web/../tests/web_test_helpers";
-
 import {
     getPickerApplyButton,
     getPickerCell,
@@ -18,6 +25,7 @@ import {
     zoomOut,
 } from "@web/../tests/core/datetime/datetime_test_helpers";
 
+import { resetDateFieldWidths } from "@web/views/list/column_width_hook";
 class Partner extends models.Model {
     date = fields.Date({ string: "A date", searchable: true });
     datetime = fields.Datetime({ string: "A datetime", searchable: true });
@@ -561,4 +569,24 @@ test("datetime field in kanban view with condensed option", async () => {
 
     const expectedDateString = "2/8/2017 8:00:00"; // 10:00:00 without timezone
     expect(".o_kanban_record:first").toHaveText(expectedDateString);
+});
+
+test("list datetime: column widths (show_time=false)", async () => {
+    await resize({ width: 800 });
+    document.body.style.fontFamily = "sans-serif";
+    resetDateFieldWidths();
+    after(resetDateFieldWidths);
+
+    await mountView({
+        type: "list",
+        resModel: "partner",
+        arch: /* xml */ `
+            <list>
+                <field name="datetime" widget="datetime" options="{'show_time': false }" />
+                <field name="display_name" />
+            </list>`,
+    });
+
+    expect(queryAllTexts(".o_data_row:eq(0) .o_data_cell")).toEqual(["02/08/2017", "partner,1"]);
+    expect(queryAllProperties(".o_list_table thead th", "offsetWidth")).toEqual([40, 81, 679]);
 });
