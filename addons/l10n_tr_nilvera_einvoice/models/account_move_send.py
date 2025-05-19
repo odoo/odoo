159245ddd9cc314c329265d2d812086658694109
hatr-odoo <hatr@odoo.com>
2025-05-19 17:56:17 +0530

    [IMP] l10n_tr_nilvera_*: ux improvements for Türkiye localization
    This commit improves the user experience of l10n_tr_nilvera_* by enhancing
    usability of partner fields, making settings more descriptive, adding better
    support for the testing environment, and implementing minor changes for overall
    UX improvements.
    
    task-4711544
    
    closes odoo/odoo#211176
    
    Related: odoo/upgrade#7730
    Signed-off-by: Ali Alfie (alal) <alal@odoo.com>

--- addons/l10n_tr_nilvera_einvoice/models/account_move_send.py
+++ addons/l10n_tr_nilvera_einvoice/models/account_move_send.py
@@ -16,7 +16,7 @@ class AccountMoveSend(models.AbstractModel):
     def _get_all_extra_edis(self) -> dict:
         # EXTENDS 'account'
         res = super()._get_all_extra_edis()
-        res.update({'tr_nilvera': {'label': _("Send E-Invoice to Nilvera"), 'is_applicable': self._is_tr_nilvera_applicable}})
+        res.update({'tr_nilvera': {'label': _("by Nilvera"), 'is_applicable': self._is_tr_nilvera_applicable}})
         return res
 
     # -------------------------------------------------------------------------
@@ -25,14 +25,88 @@ class AccountMoveSend(models.AbstractModel):
 
     def _get_alerts(self, moves, moves_data):
         alerts = super()._get_alerts(moves, moves_data)
-        if invalid_tr_partners := moves.filtered(
-                lambda m: 'tr_nilvera' in moves_data[m]['extra_edis'] and (m.partner_id.country_code != 'TR' or not m.partner_id.city or not m.partner_id.state_id or not m.partner_id.street)
-        ).partner_id:
-            alerts["partner_data_missing"] = {
-                "message": _("The following partner(s) are either not Turkish or are missing one of those fields: city, state and street."),
-                "action_text": _("View Partner(s)"),
-                "action": invalid_tr_partners._get_records_action(name=_("Check data on Partner(s)")),
+
+        # Filter for moves that have 'tr_nilvera' in their EDI data
+        tr_nilvera_moves = moves.filtered(lambda m: 'tr_nilvera' in moves_data[m]['extra_edis'])
+
+        # Show alert if the current company is in Türkiye and test mode is enabled for Nilvera
+        if self.env.company.account_fiscal_country_id.code == 'TR' and self.env.company.l10n_tr_nilvera_use_test_env:
+            alerts['l10n_tr_nilvera_einvoice_test_mode'] = {
+                'level': 'info',
+                'message': _("Testing mode is enabled."),
             }
+
+        # Alert if company is missing required data (country = TR, and tax ID, city, state, street)
+        if tr_companies_missing_required_fields := tr_nilvera_moves.filtered(
+            lambda m: (
+                not m.company_id.vat
+                or not m.company_id.street
+                or not m.company_id.city
+                or not m.company_id.state_id
+                or m.company_id.country_code != 'TR'
+            )
+        ).company_id:
+            alerts["tr_companies_missing_required_fields"] = {
+                'level': 'danger',
+                "message": _(
+                    "The following company(s) either do not have their country set as Türkiye "
+                    "or are missing at least one of these fields: Tax ID, Street, City, or State"
+                ),
+                "action_text": _("View Company(s)"),
+                "action": tr_companies_missing_required_fields._get_records_action(name=_(
+                    "Check Tax ID, City, Street, State, and Country or Company(s)"
+                )),
+            }
+
+        # Alert if partner is missing required data (tax ID, street, city, state, country)
+        if tr_partners_missing_required_fields := tr_nilvera_moves.filtered(
+            lambda m: (
+                not m.partner_id.vat
+                or not m.partner_id.street
+                or not m.partner_id.city
+                or not m.partner_id.state_id
+                or not m.partner_id.country_id
+            )
+        ).partner_id:
+            alerts["tr_partners_missing_required_fields"] = {
+                'level': 'danger',
+                "message": _(
+                    "The following partner(s) are missing at least one of these fields: Tax ID, Street, City, State or Country"
+                ),
+                "action_text": _("View Partner(s)"),
+                "action": tr_partners_missing_required_fields._get_records_action(name=_(
+                    "Check Tax ID, City, Street, State, and Country or Partner(s)"
+                )),
+            }
+
+        # Alert if partner has Street 2 filled, which is not allowed
+        if tr_partners_with_street2 := tr_nilvera_moves.filtered(lambda m: (m.partner_id.street2)).partner_id:
+            alerts["tr_partners_with_street2"] = {
+                'level': 'danger',
+                "message": _("The following partner(s) must have Street 2 empty"),
+                "action_text": _("View Partner(s)"),
+                "action": tr_partners_with_street2._get_records_action(name=_("Check Street 2 on Partner(s)")),
+            }
+
+        # Alert if partner does not use UBL TR e-invoice format or has not checked Nilvera status
+        if tr_partners_invalid_edi_or_status := tr_nilvera_moves.filtered(
+            lambda m: (
+                m.partner_id.invoice_edi_format != 'ubl_tr'
+                or m.partner_id.l10n_tr_nilvera_customer_status == 'not_checked'
+            )
+        ).partner_id:
+            alerts["tr_partners_invalid_edi_or_status"] = {
+                'level': 'danger',
+                "message": _(
+                    "The following partner(s) either do not have the e-invoice format UBL TR 1.2 "
+                    "or have not checked their Nilvera Status"
+                ),
+                "action_text": _("View Partner(s)"),
+                "action": tr_partners_invalid_edi_or_status._get_records_action(
+                    name=_("Check e-Invoice Format or Nilvera Status on Partner(s)"
+                )),
+            }
+
         return alerts
 
     # -------------------------------------------------------------------------
