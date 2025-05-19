0432c3157a23121580d7ca5a74cdc9e54ff33414
Jaishyam (JRBR) <jrbr@odoo.com>
2025-05-19 21:11:18 +0000

    [FIX] core,base_import: Handle NULL selection fields on import
    If the selection property of a parent record is NULL, and the record
    is exported and then re-imported, a TypeError occurs.
    
    This is resolved by returning an empty dictionary during import
    whenever the selection property's value is NULL in the parent record.
    
    closes odoo/odoo#212940
    
    X-original-commit: 47a361d90afba959577a36308b196e8ff34bfa5c
    Signed-off-by: Raphael Collet <rco@odoo.com>

--- odoo/orm/models.py
+++ odoo/orm/models.py
@@ -4694,7 +4694,7 @@ class BaseModel(metaclass=MetaModel):
             if fname not in self or self._fields[fname].type != 'properties':
                 continue
             field_converter = self._fields[fname].convert_to_cache
-            to_write[fname] = dict(self[fname], **field_converter(values.pop(fname), self, validate=False))
+            to_write[fname] = dict(self[fname] or {}, **field_converter(values.pop(fname), self, validate=False))
 
         self.write(values)
         if to_write:
