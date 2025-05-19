237d98eef605ab8f8e5bcce35feb696b1be52959
Pierre Rousseau <pro@odoo.com>
2025-05-19 14:15:48 +0200

    [IMP] spreadsheet: delete placeholder on filter filter_value
    This commit removes the placeholder of the `FilterValue` component. This
    feature was introduce for the dashboard view, but will be useless with
    the introduction of the search view in the dashboard.
    
    Task: 4636967
    Part-of: odoo/odoo#211650
    Related: odoo/enterprise#86414
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- addons/spreadsheet/static/src/global_filters/components/filter_text_value/filter_text_value.js
+++ addons/spreadsheet/static/src/global_filters/components/filter_text_value/filter_text_value.js
@@ -1,12 +1,10 @@
 /** @ts-check */
 
 import { Component } from "@odoo/owl";
-import { _t } from "@web/core/l10n/translation";
 
 export class TextFilterValue extends Component {
     static template = "spreadsheet.TextFilterValue";
     static props = {
-        label: { type: String, optional: true },
         onValueChanged: Function,
         value: { type: String, optional: true },
         options: {
@@ -18,9 +16,4 @@ export class TextFilterValue extends Component {
             },
         },
     };
-
-    translate(label) {
-        // the filter label is extracted from the spreadsheet json file.
-        return _t(label);
-    }
 }
