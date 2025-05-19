39033673d4e4f4d4425fc5d62e1b15e9f0eed74b
Arib Ansari <aans@odoo.com>
2025-05-19 10:59:13 +0530

    [IMP] website: adapt snippet_editor_panel_options tour
    `snippet_editor_panel_options` tour was previously broken due to DOM
    structure changes introduced by the new website builder and was
    consequently disabled.
    
    This commit updates the tour steps to align with the new DOM and
    re-enables the associated test.
    
    Part-of: odoo/odoo#211801
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/website/static/tests/tours/snippet_editor_panel_options.js
+++ addons/website/static/tests/tours/snippet_editor_panel_options.js
@@ -1,5 +1,6 @@
 import {
     changeOption,
+    changeOptionInPopover,
     clickOnSave,
     insertSnippet,
     goBackToBlocks,
@@ -7,6 +8,23 @@ import {
     selectFullText,
 } from '@website/js/tours/tour_utils';
 import { browser } from '@web/core/browser/browser';
+
+const checkIfParagraphSelected = (trigger) => ({
+    content: "Check if the paragraph is selected.",
+    trigger: trigger,
+    run() {
+        const pText = this.anchor.textContent;
+        const selection = this.anchor.ownerDocument.getSelection().toString();
+        if (pText !== selection) {
+            console.error("The paragraph was not correctly selected.");
+        }
+    },
+});
+
+const checkIfTextToolbarVisible = {
+    content: "Check if the text toolbar is visible",
+    trigger: ".o-we-toolbar",
+};
 
 registerWebsitePreviewTour('snippet_editor_panel_options', {
     url: '/',
@@ -19,45 +37,29 @@ registerWebsitePreviewTour('snippet_editor_panel_options', {
 }),
 // Test keeping the text selection when using the width option.
 selectFullText("first paragraph", ".s_text_image p"),
+checkIfParagraphSelected(":iframe .s_text_image p"),
+checkIfTextToolbarVisible,
 {
-    content: "The text toolbar should be visible. The paragraph should be selected.",
-    trigger: '#oe_snippets .o_we_customize_panel > #o_we_editor_toolbar_container',
-    run() {
-        const iframeDocument = document.querySelector('.o_iframe').contentDocument;
-        const pText = iframeDocument.querySelector('.s_text_image p').textContent;
-        const selection = iframeDocument.getSelection().toString();
-        if (pText !== selection) {
-            console.error("The paragraph was not correctly selected.");
-        }
-    },
-}, {
     content: "Click on the width option.",
-    trigger: '[data-select-class="o_container_small"]',
+    trigger: "[data-action-param='o_container_small']",
     run: "click",
 }, {
     content: "The snippet should have the correct class.",
     trigger: ':iframe .s_text_image > .o_container_small',
-}, {
-    content: "The text toolbar should still be visible, and the text still selected.",
-    trigger: '#oe_snippets .o_we_customize_panel > #o_we_editor_toolbar_container',
-    run() {
-        const iframeDocument = document.querySelector('.o_iframe').contentDocument;
-        const pText = iframeDocument.querySelector('.s_text_image p').textContent;
-        const selection = iframeDocument.getSelection().toString();
-        if (pText !== selection) {
-            console.error("The paragraph text selection was lost.");
-        }
-    },
 },
+checkIfParagraphSelected(":iframe .s_text_image p"),
 // Test the anchor option.
 {
     content: "Click on the anchor option",
-    trigger: '#oe_snippets .snippet-option-anchor we-button',
+    trigger: "[data-container-title='Text - Image'] .oe_snippet_anchor",
     async run(helpers) {
         // Patch and ignore write on clipboard in tour as we don't have permissions
         const oldWriteText = browser.navigator.clipboard.writeText;
         browser.navigator.clipboard.writeText = () => { console.info('Copy in clipboard ignored!') };
         await helpers.click();
+        // Restore the writeText after a short delay to avoid reverting it
+        // before the plugin function has been completed
+        await new Promise(resolve => setTimeout(resolve, 100));
         browser.navigator.clipboard.writeText = oldWriteText;
     }
 }, {
@@ -72,7 +74,7 @@ selectFullText("first paragraph", ".s_text_image p"),
             console.error('The anchor option should target the frontend');
         }
 
-        const iframeDocument = document.querySelector('.o_iframe').contentDocument;
+        const iframeDocument = document.querySelector(".o_iframe_container iframe").contentDocument;
         const snippetId = iframeDocument.querySelector('.s_text_image').id;
         if (!url || url.indexOf(snippetId) < 0) {
             console.error('The anchor option does not target the correct snippet.');
@@ -87,27 +89,10 @@ goBackToBlocks(),
     groupName: "Text",
 }),
 selectFullText("first paragraph", ".s_text_block p"),
+checkIfParagraphSelected(":iframe .s_text_block p"),
+checkIfTextToolbarVisible,
+...changeOptionInPopover("Text", "Layout", "[data-action-value='3']"),
 {
-    content: "The text toolbar should be visible. The paragraph should be selected.",
-    trigger: '#oe_snippets .o_we_customize_panel > #o_we_editor_toolbar_container',
-    run() {
-        const iframeDocument = document.querySelector('.o_iframe').contentDocument;
-        const pText = iframeDocument.querySelector('.s_text_block p').textContent;
-        const selection = iframeDocument.getSelection().toString();
-        if (pText !== selection) {
-            console.error("The paragraph was not correctly selected.");
-        }
-    },
-}, {
-    content: "Click on the columns option.",
-    trigger: '.snippet-option-layout_column we-select',
-    run: "click",
-},
-{
-    content: "Change the number of columns.",
-    trigger: '.snippet-option-layout_column [data-select-count="3"]',
-    run: "click",
-}, {
     content: "The snippet should have the correct number of columns.",
     trigger: ':iframe .s_text_block .container > .row .col-lg-4:eq(3)',
     run() {
@@ -115,92 +100,28 @@ selectFullText("first paragraph", ".s_text_block p"),
             console.error("The snippet does not have the correct number of columns");
         }
     },
-}, {
-    content: "The text toolbar should still be visible, and the text still selected.",
-    trigger: '#oe_snippets .o_we_customize_panel > #o_we_editor_toolbar_container',
-    run() {
-        const iframeDocument = document.querySelector('.o_iframe').contentDocument;
-        const pText = iframeDocument.querySelector('.s_text_block p').textContent;
-        const selection = iframeDocument.getSelection().toString();
-        if (pText !== selection) {
-            console.error("The paragraph text selection was lost.");
-        }
-    },
 },
+checkIfParagraphSelected(":iframe .s_text_block p"),
 // Test keeping the text selection when removing all columns of a snippet.
+...changeOptionInPopover("Text", "Layout", "[data-action-value='0']"),
 {
-    content: "Click on the columns option.",
-    trigger: '.snippet-option-layout_column we-select',
-    run: "click",
-},
-{
-    content: "Change the number of columns.",
-    trigger: '.snippet-option-layout_column [data-select-count="0"]',
-    run: "click",
-}, {
     content: "The snippet should have the correct number of columns.",
     trigger: ':iframe .s_text_block .container:not(:has(.row))',
-}, {
-    content: "The text toolbar should still be visible, and the text still selected.",
-    trigger: '#oe_snippets .o_we_customize_panel > #o_we_editor_toolbar_container',
-    run() {
-        const iframeDocument = document.querySelector('.o_iframe').contentDocument;
-        const pText = iframeDocument.querySelector('.s_text_block p').textContent;
-        const selection = iframeDocument.getSelection().toString();
-        if (pText !== selection) {
-            console.error("The paragraph text selection was lost.");
-        }
-    },
 },
+checkIfParagraphSelected(":iframe .s_text_block p"),
 // Test keeping the text selection when toggling the grid mode.
-changeOption("layout_column", 'we-button[data-name="grid_mode"]'),
+changeOption("Text", "[data-action-id='setGridLayout']"),
 {
     content: "The snippet row should have the grid mode class.",
     trigger: ":iframe .s_text_block .row.o_grid_mode",
-}, {
-    content: "The text toolbar should still be visible, and the text still selected.",
-    trigger: "#oe_snippets .o_we_customize_panel > #o_we_editor_toolbar_container",
-    run() {
-        const iframeDocument = document.querySelector(".o_iframe").contentDocument;
-        const pText = iframeDocument.querySelector(".s_text_block p").textContent;
-        const selection = iframeDocument.getSelection().toString();
-        if (pText !== selection) {
-            console.error("The paragraph text selection was lost.");
-        }
-    },
 },
-// Test keeping the text selection when toggling back the normal mode.
-changeOption("layout_column", 'we-button[data-name="normal_mode"]'),
+checkIfParagraphSelected(":iframe .s_text_block p"),
+// Test keeping the text selection when toggling back the column mode.
+changeOption("Text", "[data-action-id='setColumnLayout']"),
 {
     content: "The snippet row should not have the grid mode class anymore.",
     trigger: ":iframe .s_text_block .row:not(.o_grid_mode)",
-}, {
-    content: "The text toolbar should still be visible, and the text still selected.",
-    trigger: "#oe_snippets .o_we_customize_panel > #o_we_editor_toolbar_container",
-    run() {
-        const iframeDocument = document.querySelector(".o_iframe").contentDocument;
-        const pText = iframeDocument.querySelector(".s_text_block p").textContent;
-        const selection = iframeDocument.getSelection().toString();
-        if (pText !== selection) {
-            console.error("The paragraph text selection was lost.");
-        }
-    },
 },
-// Test close dropdowns if click anywhere outside the dropdown
-{
-    content: "Open text style dropdown.",
-    trigger: "#style button.dropdown-toggle",
-    run: "click",
-}, {
-    content: "Check if dropdown opened correctly.",
-    trigger: "#style button[data-bs-toggle=dropdown][aria-expanded=true]",
-}, {
-    content: "Click on the first paragraph again.",
-    trigger: ":iframe .s_text_block p",
-    run: "click",
-}, {
-    content: "Check if dropdown closed correctly.",
-    trigger: "#style button[data-bs-toggle=dropdown][aria-expanded=false]",
-},
+checkIfParagraphSelected(":iframe .s_text_block p"),
 ...clickOnSave(),
 ]);
