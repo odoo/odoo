511bfe43ef66fccbccfbdedc2d7e807214befe7f
Chong Wang (cwg) <cwg@odoo.com>
2025-05-19 12:49:46 +0200

    [REM] base: remove module cancel buttons
    The ``button_install/upgrade/uninstall_cancel`` methods
    1. don't work when there is another worker updating modules
    2. cause inconsistent states for directly or indirectly dependent modules
    
    This commit removes these methods and their buttons to avoid potential issues

--- odoo/addons/base/models/ir_module.py
+++ odoo/addons/base/models/ir_module.py
@@ -462,11 +462,6 @@ class IrModuleModule(models.Model):
         return self._button_immediate_function(self.env.registry[self._name].button_install)
 
     @assert_log_admin_access
-    def button_install_cancel(self):
-        self.write({'state': 'uninstalled', 'demo': False})
-        return True
-
-    @assert_log_admin_access
     def module_uninstall(self):
         """ Perform the various steps required to uninstall a module completely
         including the deletion of all database structures created by the module:
@@ -650,10 +645,6 @@ class IrModuleModule(models.Model):
             'context': {'default_module_ids': self.ids},
         }
 
-    def button_uninstall_cancel(self):
-        self.write({'state': 'installed'})
-        return True
-
     @assert_log_admin_access
     def button_immediate_upgrade(self):
         """
@@ -710,11 +701,6 @@ class IrModuleModule(models.Model):
 
         self.browse(to_install).button_install()
         return dict(ACTION_DICT, name=_('Apply Schedule Upgrade'))
-
-    @assert_log_admin_access
-    def button_upgrade_cancel(self):
-        self.write({'state': 'installed'})
-        return True
 
     @staticmethod
     def get_values_from_terp(terp):
