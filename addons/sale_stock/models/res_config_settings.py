3f409d6ffee91eb722d7e3246d6adb1efca50446
Shiv Bhadaniya (sbha) <sbha@odoo.com>
2025-05-19 12:38:03 +0530

    [IMP] sale_stock, *: align shipping policy label and behavior
    *: stock, point_of_sale
    
    With this commit:
    ==================
    - Standardizes the terminology by renaming `Picking Policy` to
      `Shipping Policy`across Configuration, Sales Orders, and related
      Transfers to reduce confusion for users.
    - Updates `selection options and tooltips` for consistent wording and
      explanation throughout the UI.
    - Ensures that changes to the Shipping Policy in Configuration are reflected
      not only in Sales Orders but also in `Operation Types`.
    - When `creating a new Operation Type`, it now uses the Shipping Policy from
      the `configured settings` (if defined), otherwise defaults to `direct`.
    - Which ensures consistent behavior of Shipping Policy by
      `aligning it with the configuration settings`.
    
    TaskId: 4791665

--- addons/sale_stock/models/res_config_settings.py
+++ addons/sale_stock/models/res_config_settings.py
@@ -12,12 +12,23 @@ class ResConfigSettings(models.TransientModel):
         string="Security Lead Time for Sales",
         config_parameter='sale_stock.use_security_lead',
         help="Margin of error for dates promised to customers. Products will be scheduled for delivery that many days earlier than the actual promised date, to cope with unexpected delays in the supply chain.")
-    default_picking_policy = fields.Selection([
-        ('direct', 'Ship products as soon as available, with back orders'),
-        ('one', 'Ship all products at once')
-        ], "Picking Policy", default='direct', default_model="sale.order", required=True)
+    picking_policy = fields.Selection([
+        ('direct', 'As soon as available, with back orders'),
+        ('one', 'When all products are ready')
+        ], default='direct', config_parameter="sale_stock.picking_policy", required=True)
 
     @api.onchange('use_security_lead')
     def _onchange_use_security_lead(self):
         if not self.use_security_lead:
             self.security_lead = 0.0
+
+    def set_values(self):
+        old_picking_policy = self.env['ir.config_parameter'].sudo().get_param('sale_stock.picking_policy', 'direct')
+        if self.picking_policy != old_picking_policy:
+            picking_types = self.env['stock.picking.type'].search([
+                ('code', '!=', 'incoming'),
+                ('move_type', '!=', self.picking_policy)
+            ])
+            if picking_types:
+                picking_types.move_type = self.picking_policy
+        super().set_values()
