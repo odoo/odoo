0bf6b9daf009628c0b3f52f641b28bfb079bcf96
Demesmaeker <edm@odoo.com>
2025-05-19 11:48:02 +0200

    [CLN] {pos_}loyalty: clean reward description for a better translation
    Correctly format the amount + let transifex have the complete sentence
    instead of adding parts of the description thinking every language is
    constructed the same way as English.
    
    task-itjustbotheredmetoomuch

--- addons/loyalty/models/loyalty_reward.py
+++ addons/loyalty/models/loyalty_reward.py
@@ -6,6 +6,7 @@ import json
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 from odoo.osv import expression
+from odoo.tools import format_amount
 
 
 class LoyaltyReward(models.Model):
@@ -231,55 +232,68 @@ class LoyaltyReward(models.Model):
         ]
 
     @api.depends(
-        'reward_type', 'reward_product_id', 'discount_mode', 'reward_product_tag_id', 'discount',
-        'currency_id', 'discount_applicability', 'all_discount_product_ids', 'repeater'
+        'currency_id', 'reward_type', 'discount', 'discount_mode', 'discount_applicability',
+        'all_discount_product_ids', 'discount_max_amount', 'reward_product_ids', 'repeater',
     )
     def _compute_description(self):
         for reward in self:
+            currency = reward.currency_id or self.env.company.currency_id
             reward_string = ""
             if reward.program_type == 'gift_card':
-                reward_string = _("Gift Card")
+                reward_string = self.env._("Gift Card")
             elif reward.program_type == 'ewallet':
-                reward_string = _("eWallet")
+                reward_string = self.env._("eWallet")
             elif reward.reward_type == 'product':
                 products = reward.reward_product_ids
                 if len(products) == 0:
-                    reward_string = _("Free Product")
+                    reward_string = self.env._("Free Product")
                 elif len(products) == 1:
-                    reward_string = _("Free Product - %s", reward.reward_product_id.with_context(display_default_code=False).display_name)
+                    reward_string = self.env._(
+                        "Free Product - %s",
+                        products.with_context(display_default_code=False).display_name,
+                    )
                 else:
-                    reward_string = _("Free Product - [%s]", ', '.join(products.with_context(display_default_code=False).mapped('display_name')))
+                    reward_string = self.env._("Free Product - [%s]", ', '.join(
+                        products.with_context(display_default_code=False).mapped('display_name')
+                    ))
             elif reward.reward_type == 'discount':
-                format_string = "%(amount)g %(symbol)s"
-                if reward.currency_id.position == 'before':
-                    format_string = "%(symbol)s %(amount)g"
-                formatted_amount = format_string % {'amount': reward.discount, 'symbol': reward.currency_id.symbol}
+                other_reward_string = discount_string = reward_target = max_amount_details = ""
+                formatted_amount = format_amount(self.env, reward.discount, currency)
+                if reward.discount_max_amount:
+                    formatted_max = format_amount(self.env, reward.discount_max_amount, currency)
+                    max_amount_details = self.env._(" (Max %(amount)s)", amount=formatted_max)
                 if reward.discount_mode == 'percent':
-                    reward_string = _("%g%% on ", reward.discount)
+                    discount_string = self.env._("%g%%", reward.discount)
                 elif reward.discount_mode == 'per_point':
-                    reward_string = _("%s per point on ", formatted_amount)
+                    discount_string = self.env._("%s per point", formatted_amount)
                 elif reward.discount_mode == 'per_order':
-                    reward_string = _("%s on ", formatted_amount)
+                    discount_string = self.env._("%s", formatted_amount)
                 if reward.discount_applicability == 'order':
-                    reward_string += _("your order")
+                    reward_target = self.env._("your order")
                 elif reward.discount_applicability == 'cheapest':
-                    reward_string = _(
-                        "Buy %(number_of_product)s, get %(reward_string)s the cheapest",
+                    other_reward_string = self.env._(
+                        "Buy %(number_of_product)s, get %(discount_string)s on the cheapest"
+                        "%(max_amount_details)s",
                         number_of_product=reward.repeater,
-                        reward_string=reward_string,
+                        discount_string=discount_string,
+                        max_amount_details=max_amount_details,
                     )
                 elif reward.discount_applicability == 'specific':
-                    product_available = self.env['product.product'].search(reward._get_discount_product_domain(), limit=2)
+                    product_available = self.env['product.product'].search(
+                        reward._get_discount_product_domain(), limit=2
+                    )
                     if len(product_available) == 1:
-                        reward_string += product_available.with_context(display_default_code=False).display_name
+                        reward_target = product_available.with_context(
+                            display_default_code=False
+                        ).display_name
                     else:
-                        reward_string += _("specific products")
-                if reward.discount_max_amount:
-                    format_string = "%(amount)g %(symbol)s"
-                    if reward.currency_id.position == 'before':
-                        format_string = "%(symbol)s %(amount)g"
-                    formatted_amount = format_string % {'amount': reward.discount_max_amount, 'symbol': reward.currency_id.symbol}
-                    reward_string += _(" (Max %s)", formatted_amount)
+                        reward_target = self.env._("specific products")
+                reward_string = other_reward_string or self.env._(
+                    "%(discount_string)s on %(reward_target)s%(max_amount_details)s",
+                    discount_string=discount_string,
+                    reward_target=reward_target,
+                    max_amount_details=max_amount_details,
+                )
             reward.description = reward_string
 
     @api.depends('reward_type', 'discount_applicability', 'discount_mode')
