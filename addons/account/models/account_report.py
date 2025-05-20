752e374c0319f3e1ffd00101db31ca03602a9833
oco-odoo <oco@odoo.com>
2025-05-20 11:40:46 +0000

    [FIX] account: reports: properly rename tax tags instead of creating them when modifying an expression's formula
    When writing a new formula on a tax_tags expression, but also providing the 'engine' key with 'tax_tags' value, this condition https://github.com/odoo/odoo/blob/16.0/addons/account/models/account_report.py#L539 triggered and the return at the end of it caused the tag to be recreated instead of renamed.
    
    This case can happen when changing the name of a tag in a data file, then updating the module. We saw it while working on an cleaned version of a localized report for master.
    
    X-original-commit: 32e1d3f6bc06bbf5e85f9dfefbd76ffc4c18b67a

--- addons/account/models/account_report.py
+++ addons/account/models/account_report.py
@@ -641,17 +641,22 @@ class AccountReportExpression(models.Model):
 
         self._strip_formula(vals)
 
+        tax_tags_expressions = self.filtered(lambda x: x.engine == 'tax_tags')
+
         if vals.get('engine') == 'tax_tags':
-            tag_name = vals.get('formula') or self.formula
-            country = self.report_line_id.report_id.country_id
-            self._create_tax_tags(tag_name, country)
-            return super().write(vals)
+            # We already generate the tags for the expressions receiving a new engine, but keeping the same formula
+            tags_create_vals = []
+            for expression_with_new_engine in self - tax_tags_expressions:
+                tags_create_vals += self.env['account.report.expression']._get_tags_create_vals(
+                    vals.get('formula') or expression_with_new_engine.formula,
+                    expression_with_new_engine.report_line_id.report_id.country_id.id,
+                )
+            self.env['account.account.tag'].create(tags_create_vals)
 
         # In case the engine is changed we don't propagate any change to the tags themselves
         if 'formula' not in vals or (vals.get('engine') and vals['engine'] != 'tax_tags'):
             return super().write(vals)
 
-        tax_tags_expressions = self.filtered(lambda x: x.engine == 'tax_tags')
         former_formulas_by_country = defaultdict(lambda: [])
         for expr in tax_tags_expressions:
             former_formulas_by_country[expr.report_line_id.report_id.country_id].append(expr.formula)
