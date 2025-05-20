af3eabb4d7f127f98468ee1730544d2701f0cc10
David Monjoie <dmo@odoo.com>
2025-05-20 07:22:24 +0000

    [FIX] html_editor,web_editor: wait for selectionchange handlers in tests
    It looks like in some cases awaiting for a microtask tick is not
    enough for the selectionchange handlers to be called. This commit
    waits for a full tick instead. This is a stronger version of the
    original fix at https://github.com/odoo/odoo/pull/210022.
    
    X-original-commit: 3a55d2e83bda95c1502a5683053d4b8393d7864c
    Part-of: odoo/odoo#211052
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/src/js/editor/odoo-editor/test/utils.js
+++ addons/web_editor/static/src/js/editor/odoo-editor/test/utils.js
@@ -332,7 +332,7 @@ export async function testEditor(Editor = OdooEditor, spec, options = {}) {
         }
 
         // Wait for selectionchange handlers to react before any actual testing.
-        await Promise.resolve();
+        await nextTick();
 
         if (spec.stepFunction) {
             try {
