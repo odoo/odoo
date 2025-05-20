135a613e3abac6698e55a936fd395dae0424e134
Pierre-Yves Dufays <pydu@odoo.com>
2025-05-20 14:08:37 +0200

    [IMP] website_slides: allow to review "document" course
    Only "training" course were allowed to be reviewed. We extend it here to all
    type of course (i.e. also "documentation" course). As documentation courses are
    less "participative", we disable comment by default for those courses.
    
    We also add a tooltip for course type field.
    
    Task-4568069

--- addons/website_slides/models/slide_channel.py
+++ addons/website_slides/models/slide_channel.py
@@ -210,7 +210,8 @@ class Channel(models.Model):
     description_html = fields.Html('Detailed Description', translate=tools.html_translate, sanitize_attributes=False, sanitize_form=False)
     channel_type = fields.Selection([
         ('training', 'Training'), ('documentation', 'Documentation')],
-        string="Course type", default="training", required=True)
+        string="Course type", default="training", required=True,
+        help='Defines the course type (e.g., "Training" for interactive learning, or "Documentation" for resources and guides).')
     sequence = fields.Integer(default=10)
     user_id = fields.Many2one('res.users', string='Responsible', default=lambda self: self.env.uid)
     color = fields.Integer('Color Index', default=0, help='Used to decorate kanban view')
@@ -249,7 +250,7 @@ class Channel(models.Model):
     rating_avg_stars = fields.Float("Rating Average (Stars)", compute='_compute_rating_stats', digits=(16, 1), compute_sudo=True)
     # configuration
     allow_comment = fields.Boolean(
-        "Allow rating on Course", default=True,
+        "Allow rating on Course", compute="_compute_allow_comment", store=True, readonly=False, precompute=True,
         help="Allow Attendees to like and comment your content and to submit reviews on your course.")
     publish_template_id = fields.Many2one(
         'mail.template', string='New Content Notification',
@@ -405,6 +406,12 @@ class Channel(models.Model):
         super(Channel, self)._compute_rating_stats()
         for record in self:
             record.rating_avg_stars = record.rating_avg
+
+    @api.depends('channel_type')
+    def _compute_allow_comment(self):
+        """Comment allowed by default except for documentation channels."""
+        for record in self:
+            record.allow_comment = record.channel_type != 'documentation'
 
     @api.depends('slide_partner_ids', 'slide_partner_ids.completed', 'total_slides')
     @api.depends_context('uid')
