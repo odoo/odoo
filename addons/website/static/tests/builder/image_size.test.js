3f8baf744b99f6d0c74d4e03e531d68b613f9f61
Nicolas Bayet <nby@odoo.com>
2025-05-20 15:55:53 +0200

    [IMP] *: add image size info on img and backrgound images
    In the website builder, add the file size information on images and
    background images.
    
    Related to task-4367641
    
    closes odoo/odoo#211640
    
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

---
+++ addons/website/static/tests/builder/image_size.test.js
@@ -0,0 +1,40 @@
+import { expect, test } from "@odoo/hoot";
+import { waitFor } from "@odoo/hoot-dom";
+import { contains } from "@web/../tests/web_test_helpers";
+import { defineWebsiteModels, setupWebsiteBuilder } from "./website_helpers";
+import { testImg, testImgSrc } from "./image_test_helpers";
+
+defineWebsiteModels();
+
+test("the image should show its size", async () => {
+    const { waitDomUpdated } = await setupWebsiteBuilder(`
+        <div class="test-options-target">
+            ${testImg}
+        </div>
+    `);
+    await contains(":iframe .test-options-target img").click();
+    await waitDomUpdated();
+    const selector = `[data-container-title="Image"] [title="Size"]`;
+    await waitFor(selector);
+    const size = parseFloat(document.querySelector(selector).innerHTML);
+    expectAround(size, 35.6);
+});
+
+test("the background image should show its size", async () => {
+    const { waitDomUpdated } = await setupWebsiteBuilder(`
+        <div class="test-options-target">
+            <section style="background-image: url(${testImgSrc});">text</section>
+        </div>
+    `);
+    await contains(":iframe .test-options-target section").click();
+    await waitDomUpdated();
+    const selector = `[data-label="Image"] [title="Size"]`;
+    await waitFor(selector);
+    const size = parseFloat(document.querySelector(selector).innerHTML);
+    expectAround(size, 35.6);
+});
+
+function expectAround(value, expected, delta = 0.2) {
+    expect(value).toBeGreaterThan(expected - delta);
+    expect(value).toBeLessThan(expected + delta);
+}
