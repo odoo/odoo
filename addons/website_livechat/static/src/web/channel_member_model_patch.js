d2d696ccbf66a974db45b085e89e5aabb1478dfa
Didier (did) <did@odoo.com>
2025-05-20 09:10:03 +0200

    [FIX] website_livechat: rename thread to channel_id
    The rename was done by a [previous PR](https://github.com/odoo/odoo/pull/208544) and
    missed the website_livechat override.

--- addons/website_livechat/static/src/web/channel_member_model_patch.js
+++ addons/website_livechat/static/src/web/channel_member_model_patch.js
@@ -4,8 +4,8 @@ import { patch } from "@web/core/utils/patch";
 /** @type {import("models").ChannelMember} */
 const channelMemberPatch = {
     getLangName() {
-        if (this.persona.is_public && this.thread.visitor?.langName) {
-            return this.thread.visitor.langName;
+        if (this.persona.is_public && this.channel_id.visitor?.langName) {
+            return this.channel_id.visitor.langName;
         }
         return super.getLangName();
     },
