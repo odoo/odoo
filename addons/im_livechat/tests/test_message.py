2803d131215ea28da84650f53d15d4550ec4ce84
tsm-odoo <tsm@odoo.com>
2025-05-20 09:05:05 +0200

    [FIX] im_livechat: rating reason useless break line
    This commit removes a useless break line added to the end of
    chat ratign. Break line is used to put the reason on a different
    line than the rating smiley but is useless when no reason is passed.
    
    task-4791376
    
    closes odoo/odoo#210610
    
    X-original-commit: 0deee871447c380ed2a3966784562b6d0c65da90
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/im_livechat/tests/test_message.py
+++ addons/im_livechat/tests/test_message.py
@@ -273,7 +273,7 @@ class TestImLivechatMessage(ChatbotCase, MailCommon):
                                         },
                                         "body": [
                                             "markup",
-                                            '<div class="o_mail_notification o_hide_author">Rating: <img class="o_livechat_emoji_rating" src="/rating/static/src/img/rating_5.png" alt="rating"><br>Good service</div>',
+                                            '<div class="o_mail_notification o_hide_author">Rating: <img class="o_livechat_emoji_rating" src="/rating/static/src/img/rating_5.png" alt="rating"><br>\nGood service</div>',
                                         ],
                                         "create_date": fields.Datetime.to_string(
                                             message.create_date
