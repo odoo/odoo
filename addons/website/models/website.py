a9c7a8598dfd93af19ce4cdae23e9d5106877e2b
Garvish Panchal <gppa@odoo.com>
2025-05-20 10:26:20 +0530

    [FIX] website: skip 301/302 routes for controllers
    Before this commit:
    - Odoo’s sitemap could include routes that automatically redirect with HTTP
      status codes 301 Moved Permanently or 302 Found.
    - These redirecting URLs are not suitable for indexing by search engines.
      According to Google’s guidelines, sitemaps should include only content pages
      that return a 200 OK status.
    - As a result, URLs that only performed redirects might appear in the sitemap,
      while the actual target (final destination) URL would be missing.
    
    After this commit:
    - `https://www.odoo.com/event/odoo-technical-training-in-may-2025-7750/register`
      (200 Code) will be included and
      `https://www.odoo.com/event/odoo-technical-training-in-may-2025-7750`
      (301 code) will not be included in sitemap.xml.

--- addons/website/models/website.py
+++ addons/website/models/website.py
@@ -1551,16 +1551,15 @@ class Website(models.Model):
             domain += [('url', 'like', query_string)]
 
         # ==== User Defined Redirections ====
-        redirects = self.env['website.rewrite'].sudo().search([])
+        redirects = self.env["website.rewrite"].sudo().search([])
 
         pages = self._get_website_pages(domain)
 
+        redirect_map = {r.url_from: r.url_to for r in redirects}
         for page in pages:
-            user_redirect = next((r for r in redirects if r.url_from == page.url), None)
-            if user_redirect:
-                yield {'loc': user_redirect.url_to}
+            if page.url in redirect_map:
+                yield {"loc": redirect_map[page.url]}
                 continue
-
             record = {'loc': page['url'], 'id': page['id'], 'name': page['name']}
             if page.view_id.priority != 16:
                 record['priority'] = min(round(page.view_id.priority / 32.0, 1), 1)
