63c7a3f1ca297c147b2c7ac7f3fa3f40c70fbedb
Alexandre Kühn <aku@odoo.com>
2025-05-20 10:51:35 +0000

    [FIX] mail: message quick add reaction usable in mobile
    Before this commit, when a message has at least 1 reaction,
    using the quick reaction button in mobile displayed the
    emoji picker that shared the viewport of message list.
    
    This makes it hardly practical.
    
    This happens because the `useEmojiPicker()` in message reactions
    provide a ref, and this ref in mobile is used as a target to mount
    the emoji picker. This feature is useful for discuss composer to
    place emoji picker nicely under the composer input. In desktop the
    ref is used for click target of popover.
    
    In practice the passing of ref in mobile is niche use-case: most
    of the time it should open in bottom screen as a dialog in a similar
    fashion as popover.
    
    This commit fixes the issue by making 1st param of `useEmojiPicker`
    only act as the toggler part . In mobile to provide the container of
    emoji picker, one need to call `open(ref)` explicitly.
    
    Task-4800688
    
    closes odoo/odoo#211564
    
    X-original-commit: 8016a68c38b67412761d42731905a6d616618724
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/core/common/message_reactions.js
+++ addons/mail/static/src/core/common/message_reactions.js
@@ -3,6 +3,7 @@ import { Component, useRef } from "@odoo/owl";
 import { MessageReactionList } from "@mail/core/common/message_reaction_list";
 import { useService } from "@web/core/utils/hooks";
 import { useEmojiPicker } from "@web/core/emoji_picker/emoji_picker";
+import { isMobileOS } from "@web/core/browser/feature_detection";
 
 export class MessageReactions extends Component {
     static props = ["message", "openReactionMenu"];
@@ -14,6 +15,7 @@ export class MessageReactions extends Component {
         this.store = useService("mail.store");
         this.ui = useService("ui");
         this.addRef = useRef("add");
+        this.isMobileOS = isMobileOS();
         this.emojiPicker = useEmojiPicker(this.addRef, {
             onSelect: (emoji) => {
                 const reaction = this.props.message.reactions.find(
