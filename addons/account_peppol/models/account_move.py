0501b6b74498fad5e3339912bba9beb9539798d7
Anton Romanova (roan) <roan@odoo.com>
2025-05-20 14:55:20 +0000

    [FIX] account_peppol: less smp calls send & print
    Peppol send & print was performing lots of SMP calls for everything at
    every step, which didn't have that much value and to avoid overloading
    some SMPs, we prefer to limit the number of SMP calls the wizard does.
    
    no-task
    
    X-original-commit: e26d341475241c4b9ed90085841c2da3d51fa4e0

--- addons/account_peppol/models/account_move.py
+++ addons/account_peppol/models/account_move.py
@@ -23,6 +23,11 @@ class AccountMove(models.Model):
         copy=False,
     )
 
+    def action_send_and_print(self):
+        for move in self:
+            move.commercial_partner_id.button_account_peppol_check_partner_endpoint(company=move.company_id)
+        return super().action_send_and_print()
+
     def action_cancel_peppol_documents(self):
         # if the peppol_move_state is processing/done
         # then it means it has been already sent to peppol proxy and we can't cancel
