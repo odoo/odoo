e6e59c0451b019f87b70ca238e7e06dec78dd811
Lucas Perais (lpe) <lpe@odoo.com>
2025-05-20 08:15:05 +0000

    [FIX] website: add_page_dialog component may be destroyed while loading
    Doing asynchronous things in the onWillStart can be tricky.
    Methods that come from services are usually protected against those, and hang up
    if the component was destroyed during the call, but other calls aren't protected.
    
    This commit fixes a race condition where a protected call is done after a component is destroyed and crashes.
    
    runbot-error-135245
    
    closes odoo/odoo#211035
    
    X-original-commit: bfaa23dc58439fda80d026ed5761cb4f1a1232c6
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Lucas Perais (lpe) <lpe@odoo.com>

--- addons/website/static/src/components/dialog/add_page_dialog.js
+++ addons/website/static/src/components/dialog/add_page_dialog.js
@@ -8,7 +8,7 @@ import { _t } from "@web/core/l10n/translation";
 import { WebsiteDialog } from '@website/components/dialog/dialog';
 import { Switch } from '@website/components/switch/switch';
 import { applyTextHighlight } from "@website/js/text_processing";
-import { useRef, useState, useSubEnv, Component, onWillStart, onMounted } from "@odoo/owl";
+import { useRef, useState, useSubEnv, Component, onWillStart, onMounted, status } from "@odoo/owl";
 import wUtils from '@website/js/utils';
 
 const NO_OP = () => {};
@@ -358,6 +358,9 @@ export class AddPageTemplates extends Component {
         // Displaying the correct images in the previews also relies on the
         // website id having been forced.
         await this.env.getCssLinkEls();
+        if (status(this) === "destroyed") {
+            return new Promise(() => {});
+        }
 
         if (this.pages) {
             return this.pages;
