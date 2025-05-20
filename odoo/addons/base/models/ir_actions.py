43b391010ab4c50f78c48ae6a019b6ea89725a7f
Nicolas Lempereur <nle@odoo.com>
2025-05-20 15:03:44 +0200

    [FIX] base: compute object_write equation for each record
    Scenario:
    - create an object_write (Update the Record) action
    - update a field with equation evaluation (Python expression) with an
      expression like "record.id"
    - click on "Create contextual action"
    - go to a list view of the model, execute the action for several records
    
    Result: the value of the field of all records are computed based on the
    first selected record.
    
    Issue: we don't change the "record" in the evaluation context and just
    keep the first record.
    
    Fix: changing the record.
    
    Note: without the fix, the added test fails because the city of the
    second record is set to the value of the ID of the first record.
    
    opw-4491099

--- odoo/addons/base/models/ir_actions.py
+++ odoo/addons/base/models/ir_actions.py
@@ -684,6 +684,7 @@ class IrActionsServer(models.Model):
                     # run context dedicated to a particular active_id
                     run_self = action.with_context(active_ids=[active_id], active_id=active_id)
                     eval_context["env"].context = run_self._context
+                    eval_context['records'] = eval_context['record'] = records.browse(active_id)
                     res = runner(run_self, eval_context=eval_context)
             else:
                 _logger.warning(
