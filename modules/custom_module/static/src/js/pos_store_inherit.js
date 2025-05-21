96d944d2ec342dc75d016f1e3ef67d4af290c790
nourfrioui <nourfrioui88@gmail.com>
2025-05-21 16:50:55 +0100

    Feat[MR-79] :Personnaliser ticket cuisine

--- modules/custom_module/static/src/js/pos_store_inherit.js
+++ modules/custom_module/static/src/js/pos_store_inherit.js
@@ -3,7 +3,23 @@
 import { PosStore } from "@point_of_sale/app/store/pos_store";
 import { patch } from "@web/core/utils/patch";
 
+const { DateTime } = luxon;
+
 patch(PosStore.prototype, {
+     getPrintingChanges(order, diningModeUpdate) {
+        const time = DateTime.now().toFormat("dd/MM/yyyy HH:mm");
+        return {
+            table_name: order.table_id ? order.table_id.table_number : "",
+            floor_name: order.table_id?.floor_id.name || "",
+            config_name: order.config.name,
+            time: time,
+            tracking_number: order.tracking_number,
+            takeaway: order.config.takeaway && order.takeaway,
+            employee_name: order.employee_id?.name || order.user_id?.name,
+            order_note: order.general_note,
+            diningModeUpdate: diningModeUpdate,
+        };
+    },
 
     async showLoginScreen() {
         this.showScreen("FloorScreen");
