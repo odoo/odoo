f6e2b0803a6a3aa48113675f701902f7baddaee6
Pedram (PEBR) <pebr@odoo.com>
2025-05-21 13:19:14 +0000

    [FIX] point_of_sale: exclude default_code from exact match
    Before this commit, if the search term was included in a product's
    internal reference (default_code), the search results would only return
    products with an exact match on default_code, and would exclude other
    products that matched the search term in their name. This was due to
    default_code being included in the exact match logic.
    
    opw-4778729
    
    closes odoo/odoo#211999
    
    X-original-commit: abe32e5f58dcbcf20a30b28a9f1cb7eca7a9fecc
    Signed-off-by: Vlad Stroia (vlst) <vlst@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/point_of_sale/static/src/app/models/product_template.js
+++ addons/point_of_sale/static/src/app/models/product_template.js
@@ -265,7 +265,6 @@ export class ProductTemplate extends Base {
         const fields = ["barcode"];
         const variantMatch = this.product_variant_ids.some(
             (variant) =>
-                (variant.default_code && variant.default_code.toLowerCase() == searchWord) ||
                 (variant.barcode && variant.barcode.toLowerCase() == searchWord) ||
                 variant.product_template_variant_value_ids.some((vv) =>
                     vv.name.toLowerCase().includes(searchWord)
