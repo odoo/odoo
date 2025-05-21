dbfd172d812c2ef514858554e793324b4c036ebd
Julien Castiaux <juc@odoo.com>
2025-05-21 12:49:15 +0000

    [FIX] core: don't send almost same Date header twice
    The code proved unreliable, emitting warnings for genuine controllers
    because the two headers were milliseconds appart. Made the warning
    ignore some almost equal headers.
    
    X-original-commit: 68381a013073a027c0d3eed963aea768155b1d0c
    Part-of: odoo/odoo#249544
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- odoo/service/server.py
+++ odoo/service/server.py
@@ -18,6 +18,7 @@ import threading
 import time
 from collections import deque
 import contextlib
+from email.utils import parsedate_to_datetime
 from io import BytesIO
 
 import psutil
@@ -169,6 +170,12 @@ class RequestHandler(werkzeug.serving.WSGIRequestHandler):
             elif self._sent_date_header == value:
                 return  # don't send the same header twice
             else:
+                sent_datetime = parsedate_to_datetime(self._sent_date_header)
+                new_datetime = parsedate_to_datetime(value)
+                if sent_datetime == new_datetime:
+                    return  # don't send the same date twice (differ in format)
+                if abs((sent_datetime - new_datetime).total_seconds()) <= 1:
+                    return  # don't send the same date twice (jitter of 1 second)
                 _logger.warning(
                     "sending two different Date response headers: %r vs %r",
                     self._sent_date_header, value)
