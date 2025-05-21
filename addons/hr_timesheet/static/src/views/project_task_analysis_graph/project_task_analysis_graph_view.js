e162133a5f891291c69983f67252b68b191c2406
Xavier Bol (xbo) <xbo@odoo.com>
2025-05-21 14:13:01 +0200

    [IMP] hr_timesheet: correctly override graph model in task
    This commit reviews the custom graph views in project/timesheets app to
    make sure the graph of task no longer uses the custom graph view of
    timesheets for instance. By doing separate custom views, it would be
    easier to adapt a specific graph view.
    
    task-4801249
    
    Part-of: odoo/odoo#213096
    Related: odoo/enterprise#87066
    Related: odoo/upgrade#7806
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/hr_timesheet/static/src/views/project_task_analysis_graph/project_task_analysis_graph_view.js
+++
@@ -1,8 +0,0 @@
-import { registry } from "@web/core/registry";
-import { hrTimesheetGraphView } from "../timesheet_graph/timesheet_graph_view";
-import { ProjectTaskAnalysisGraphRenderer } from "@project/views/project_task_analysis_graph/project_task_analysis_graph_renderer";
-
-registry.category("views").add("hr_timesheet_project_task_analysis_graph", {
-    ...hrTimesheetGraphView,
-    Renderer: ProjectTaskAnalysisGraphRenderer,
-});
