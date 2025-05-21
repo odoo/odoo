d0e0c453573428703634a37074e8e4f00bbf5b92
Pierre-Yves Dufays <pydu@odoo.com>
2025-05-21 08:04:44 +0200

    [IMP] website_slides, portal_rating: improve rating
    We improve the review process by:
    - moving the review button inside the review tab
    - replacing the "edit" review button by the "edit" from the chatter
    - better handling of deleted messages; the user can now re-add a message after
    having deleted his message. For that we display the "Edit review" button when
    the message has been deleted as it is then no more editable through the
    chatter.
    
    Technical note: this feature requires a 2-way communication between the chatter
    (actually the chatter actions) and the rating interaction. A simple way would
    have been to patch the chatter actions inside the rating interaction but that
    was not possible because the assets of the chatter and the interaction are
    loaded independently. Instead, we use event and a data attribute in the dom to
    perform this 2-way communication.
    
    Task-4458581

--- addons/website_slides/static/src/interactions/portal_rating_composer.js
+++ addons/website_slides/static/src/interactions/portal_rating_composer.js
@@ -1,11 +1,29 @@
-import { patch } from "@web/core/utils/patch";
 import { RatingPopupComposer } from "@portal_rating/interactions/portal_rating_composer";
+import { patch } from "@web/core/utils/patch";
 
 patch(RatingPopupComposer.prototype, {
+    setup() {
+        super.setup(...arguments);
+        // When the review is emptied in the chatter, display the button as it cannot be edited in the chatter anymore.
+        this.deleteMessageEvent = "WEBSITE_SLIDES:CHANNEL_DELETE_MESSAGE";
+        this.deleteMessageListener = ({ detail }) => {
+            if (detail.id === this.documentId) {
+                this.isBtnDisplayed = true;
+                this.updateContent();
+            }
+        };
+        this.env.bus.addEventListener(this.deleteMessageEvent, this.deleteMessageListener);
+    },
+
     updateOptions(data) {
         super.updateOptions(...arguments);
         this.options.force_submit_url =
             data.force_submit_url ||
             (this.options.default_message_id && "/mail/message/update_content");
+    },
+
+    destroy() {
+        super.destroy();
+        this.env.bus.removeEventListener(this.deleteMessageEvent, this.deleteMessageListener);
     },
 });
