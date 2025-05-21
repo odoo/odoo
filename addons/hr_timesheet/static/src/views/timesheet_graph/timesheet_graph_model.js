8050408c5bd7f1f1fb56f159d4853e32719e5dfa
Xavier Bol (xbo) <xbo@odoo.com>
2025-05-21 14:13:01 +0200

    [IMP] hr_timesheet: correctly override graph model in task
    This commit reviews the custom graph views in project/timesheets app to
    make sure the graph of task no longer uses the custom graph view of
    timesheets for instance. By doing separate custom views, it would be
    easier to adapt a specific graph view.
    
    task-4801249
    
    Part-of: odoo/odoo#213096
    Related: odoo/enterprise#87066
    Related: odoo/upgrade#7806
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/hr_timesheet/static/src/views/timesheet_graph/timesheet_graph_model.js
+++ addons/hr_timesheet/static/src/views/timesheet_graph/timesheet_graph_model.js
@@ -1,35 +1,6 @@
 import { GraphModel } from "@web/views/graph/graph_model";
-import { user } from "@web/core/user";
 
-const FIELDS = [
-    'unit_amount', 'effective_hours', 'allocated_hours', 'remaining_hours', 'total_hours_spent', 'subtask_effective_hours',
-    'overtime', 'number_hours', 'difference', 'timesheet_unit_amount'
-];
+import { patchGraphModel } from "../graph_model_patch";
 
-export class hrTimesheetGraphModel extends GraphModel {
-    /**
-     * @override
-     */
-    setup(params, services) {
-        super.setup(...arguments);
-    }
-
-    //--------------------------------------------------------------------------
-    // Private
-    //--------------------------------------------------------------------------
-
-    /**
-     * Override processDataPoints to take into account the analytic line uom.
-     * @override
-     */
-    _getProcessedDataPoints() {
-        const factor = user.activeCompany.timesheet_uom_factor || 1;
-        if (factor !== 1 && FIELDS.includes(this.metaData.measure)) {
-            // recalculate the Duration values according to the timesheet_uom_factor
-            for (const dataPt of this.dataPoints) {
-                dataPt.value *= factor;
-            }
-        }
-        return super._getProcessedDataPoints(...arguments);
-    }
-}
+export class hrTimesheetGraphModel extends GraphModel {}
+patchGraphModel(hrTimesheetGraphModel);
