cdac6c06295165e30f52f36b84854d120e35a39c
svs-odoo <svs@odoo.com>
2025-05-21 17:37:41 +0000

    [FIX] purchase:  respect product UoM when selecting from catalog
    Steps to reproduce the bug
    ==========================
    - Create a storable product “P1”:
        - UoM: unit
        - Purchase tab:
            - Vendor: Azure interior
            - UoM: Pack of 6
    
    - Create a purchase order:
        - Vendor: Azure interior
        - Click the Catalog button:
            - Select 1 unit of P1 (note: UoM cannot be changed in the catalog)
    
    Problem
    =======
    The purchase order line is created using the "pack of 6" but still
    display "units" in the catalog.
    
    Fix
    ===
    If the most relevant vendor price uses a specific UoM, this UoM is used
    by the product in the purchase product catalog too. If the UoM would be
    updated when adding quantity, it will be updated in the interface too.
    
    The used UoM is sent from the catalog to be sure the PO line created in
    the backend and the UoM seen in the catalog are the same.
    
    Also, this commit fixes another minor issue where we used the discounted
    price directly. When a PO line was added on the PO through the catalog,
    at its creation, the PO line had the discounted price as its unit price
    and no discount applyed on it. That said, all was right once the line
    was updated again.
    
    OPW-4794362
    
    X-original-commit: 1759549155dae213db1adc2f90583088e6f667d8
    Co-authored-by: DjamelTouati <otd@odoo.com>

--- addons/purchase/models/purchase_order.py
+++ addons/purchase/models/purchase_order.py
@@ -1185,12 +1185,18 @@ class PurchaseOrder(models.Model):
             params=params
         )
         if seller:
+            product_uom = (seller.product_id or seller.product_tmpl_id).uom_id
             price = seller.price_discounted
             if seller.currency_id != self.currency_id:
-                price = seller.currency_id._convert(seller.price_discounted, self.currency_id)
+                price = seller.currency_id._convert(price, self.currency_id)
+            if seller.product_uom_id != product_uom:
+                # The discounted price is expressed in the product's UoM, not in the vendor
+                # price's UoM, so we need to convert it into to match the displayed UoM.
+                price = product_uom._compute_price(price, seller.product_uom_id)
             product_infos.update(
                 price=price,
                 min_qty=seller.min_qty,
+                uomDisplayName=seller.product_uom_id.display_name,
             )
 
         return product_infos
@@ -1293,10 +1299,11 @@ class PurchaseOrder(models.Model):
             if pol.selected_seller_id:
                 # Fix the PO line's price on the seller's one.
                 seller = pol.selected_seller_id
-                price = seller.price_discounted
+                price = seller.price
                 if seller.currency_id != self.currency_id:
-                    price = seller.currency_id._convert(seller.price_discounted, self.currency_id)
-                pol.price_unit = price
+                    price = seller.currency_id._convert(price, self.currency_id)
+                pol.price_unit = pol.technical_price_unit = price
+                pol.discount = seller.discount
         return pol.price_unit_discounted
 
     def _get_default_create_section_values(self):
