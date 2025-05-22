d01cd970922c1b156585b2996b03d2ae6b681067
Sébastien (blse) <blse@odoo.com>
2025-05-22 11:48:49 +0200

    [FIX] website: ignore the modal mutations when hiding/showing popup
    Before this commit, some of the mutations happening when toggling the
    visibility of a popup were tracked in the history. Thus, when toggling
    the visibility of a popup and doing undo/redo, the actual visibility of
    a popup could be inconsistent with the state of the toggle button.
    
    This commit avoids adding these mutations in the history when toggling
    the visibility, by not observing them when calling the Bootstrap
    functions causing them, so by patching the `Modal` functions and calling
    `ignoreDOMMutations` around them (`hide` and `show` are not enough
    because Bootstrap delays some operations).
    
    task-4367641

--- addons/website/static/tests/builder/website_builder/popup_option.test.js
+++ addons/website/static/tests/builder/website_builder/popup_option.test.js
@@ -2,11 +2,13 @@ import { beforeEach, describe, expect, test } from "@odoo/hoot";
 import { advanceTime, waitFor } from "@odoo/hoot-dom";
 import { contains } from "@web/../tests/web_test_helpers";
 import {
+    addPlugin,
     defineWebsiteModels,
     insertCategorySnippet,
     setupWebsiteBuilder,
     waitForEndOfOperation,
 } from "../website_helpers";
+import { Plugin } from "@html_editor/plugin";
 
 defineWebsiteModels();
 
@@ -26,9 +28,43 @@ describe("Popup options: empty page before edit", () => {
     });
 });
 describe("Popup options: popup in page before edit", () => {
+    let builder;
     // Done in `beforeEach` because frontend JS takes too much time to load.
     beforeEach(async () => {
-        await setupWebsiteBuilder(
+        addPlugin(
+            class extends Plugin {
+                static id = "ignore_d-none_on_s_popup";
+                resources = {
+                    // NOTE: this plugin is here as a workaround to make the
+                    // test pass, because (at the time of this commit):
+                    // - the website_edit service is removed for the tests, thus
+                    //   the patch that wraps interaction's functions in
+                    //   `ignoreDOMMutation` is not applied
+                    // - the interaction SharedPopup adds and removes `d-none`
+                    //   on `.s_popup` element to track the visibility of the
+                    //   modal
+                    // - one of the tests here plays with the visibility of the
+                    //   modal, and verifies that it did not add mutations
+                    // TODO: once the service website_edit runs during the
+                    // tests, this plugin should be removed
+                    savable_mutation_record_predicates: (record) => {
+                        if (
+                            record.target.matches?.(".s_popup") &&
+                            record.attributeName === "class"
+                        ) {
+                            const oldValue = new Set(record.oldValue.split(" "));
+                            const newValue = new Set(record.target.className.split(" "));
+                            const union = oldValue.union(newValue);
+                            const intersection = oldValue.intersection(newValue);
+                            const difference = union.difference(intersection);
+                            return !(difference.size === 1 && difference.has("d-none"));
+                        }
+                        return true;
+                    },
+                };
+            }
+        );
+        builder = await setupWebsiteBuilder(
             `<div class="s_popup o_snippet_invisible o_draggable" data-snippet="s_popup" data-name="Popup" id="sPopup" data-invisible="1">
                 <div class="modal fade s_popup_middle modal_shown" style="background-color: var(--black-50)  !important; display: none;" data-show-after="5000" data-display="afterDelay" data-consents-duration="7" data-bs-focus="false" data-bs-backdrop="false" tabindex="-1" aria-label="Popup" aria-hidden="true">
                     <div class="modal-dialog d-flex">
@@ -60,5 +96,8 @@ describe("Popup options: popup in page before edit", () => {
         await contains(":iframe .s_popup div.js_close_popup").click();
         expect(":iframe .s_popup").not.toBeVisible();
         expect(".o_we_invisible_entry .fa").toHaveClass("fa-eye-slash");
+        // Ensure that no mutations were registered in the history.
+        // `addStep` return the created step, or false if there was no mutations
+        expect(builder.getEditor().shared.history.addStep()).toBe(false);
     });
 });
