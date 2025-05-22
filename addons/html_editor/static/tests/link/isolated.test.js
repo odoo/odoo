c516ed0773af2e3748dc04f16dfb005c1715fc16
David Monjoie <dmo@odoo.com>
2025-05-22 12:55:03 +0200

    [FIX] html_editor: fix non-deterministic link tests on Chrome 136
    This is a follow-up to https://github.com/odoo/odoo/pull/210867/commits/c52e74066f9b43d24461e2840ea8a6ec64444727.

--- addons/html_editor/static/tests/link/isolated.test.js
+++ addons/html_editor/static/tests/link/isolated.test.js
@@ -363,14 +363,14 @@ test("should remove zwnbsp from middle of the link (2)", async () => {
 
 test("should zwnbps-pad links with .btn class", async () => {
     await testEditor({
-        contentBefore: '<p><a class="btn">content</a></p>',
-        contentBeforeEdit: '<p>\ufeff<a class="btn">\ufeffcontent\ufeff</a>\ufeff</p>',
+        contentBefore: '<p><a href="#" class="btn">content</a></p>',
+        contentBeforeEdit: '<p>\ufeff<a href="#" class="btn">\ufeffcontent\ufeff</a>\ufeff</p>',
     });
 });
 
 test("should not add visual indication to a button", async () => {
     await testEditor({
-        contentBefore: '<p><a class="btn">[]content</a></p>',
-        contentBeforeEdit: '<p>\ufeff<a class="btn">\ufeffcontent\ufeff</a>\ufeff</p>',
+        contentBefore: '<p><a href="#" class="btn">[]content</a></p>',
+        contentBeforeEdit: '<p>\ufeff<a href="#" class="btn">\ufeff[]content\ufeff</a>\ufeff</p>',
     });
 });
