f4945db21a4aa335627c8d47deb31ac4260f5ac1
Simon Goffaux (sigo) <sigo@odoo.com>
2025-05-22 07:14:16 +0000

    [FIX] hr: ensure departure wizard tracking values
    When we archive an employee that is the time off responsible of another
    employee, the `_clean_leave_responsible_users` method will clear our
    cache before we set some departure related fields. This will cause us
    to have to recompute the relation for `employee_ids` and the domain
    that ensures the suggested employees in the wizard are not already
    archived will prevent us from finding the employee we were archiving.
    
    To ensure these fields are written properly, we save the value of
    `employee_ids` before archiving the employee.
    
    opw-4729600
    
    closes odoo/odoo#217703
    
    X-original-commit: d777c9283370a3075e2fe8892fd76e7147623b2e
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>
    Signed-off-by: Simon Goffaux (sigo) <sigo@odoo.com>

--- addons/hr/wizard/hr_departure_wizard.py
+++ addons/hr/wizard/hr_departure_wizard.py
@@ -38,10 +38,11 @@ class HrDepartureWizard(models.TransientModel):
     )
 
     def action_register_departure(self):
+        employee_ids = self.employee_ids
         for employee in self.employee_ids.filtered(lambda emp: emp.active):
             if self.env.context.get('employee_termination', False):
                 employee.with_context(no_wizard=True).action_archive()
-        self.employee_ids.write({
+        employee_ids.write({
             'departure_reason_id': self.departure_reason_id,
             'departure_description': self.departure_description,
             'departure_date': self.departure_date,
