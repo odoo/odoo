41b513bc1c2fe530e5a0f567e040c747244fe75b
Krzysztof Magusiak (krma) <krma@odoo.com>
2025-05-23 12:10:42 +0000

    [IMP] lunch: fields on users are protected and copy=False
    The comodels are dependent on permissions of lunch and there is no need
    to copy these fields with the user.

--- addons/lunch/models/res_users.py
+++ addons/lunch/models/res_users.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models
@@ -7,5 +6,5 @@ from odoo import fields, models
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    last_lunch_location_id = fields.Many2one('lunch.location')
-    favorite_lunch_product_ids = fields.Many2many('lunch.product', 'lunch_product_favorite_user_rel', 'user_id', 'product_id')
+    last_lunch_location_id = fields.Many2one('lunch.location', groups='lunch.group_lunch_user', copy=False)
+    favorite_lunch_product_ids = fields.Many2many('lunch.product', 'lunch_product_favorite_user_rel', 'user_id', 'product_id', groups='lunch.group_lunch_user', copy=False)
