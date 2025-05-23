fe416d515ed77b6e3feb5ff29193708a9ee5a653
vpan-odoo <vpan@odoo.com>
2025-05-23 12:10:08 +0530

    [IMP] pos_restaurant: add guest info to the kitchen print
    Before this commit:
    ====================
    - The guest count was visible on the kitchen display but not on the kitchen
    print.
    
    After this commit:
    ===================
    - To align with the behavior of the kitchen display, the guest count is now also
     shown on the kitchen print.
    
    Task: 4816532

--- addons/pos_restaurant/static/src/app/services/pos_store.js
+++ addons/pos_restaurant/static/src/app/services/pos_store.js
@@ -896,4 +896,10 @@ patch(PosStore.prototype, {
             }
         }
     },
+    getOrderData(order, reprint) {
+        return {
+            ...super.getOrderData(order, reprint),
+            customer_count: order.getCustomerCount(),
+        };
+    },
 });
