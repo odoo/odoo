61f9ee53382318937ad02888d7d02eba06a561ba
Pierre Rousseau <pro@odoo.com>
2025-05-23 14:18:38 +0200

    [IMP] spreadsheet{_dashboard}: add searchview in dashboard
    This commit add a search view in the dashboard app to allow users to
    use the global filters easily. For now, it's only limited to the
    dialog search, but in a future work the search bar support will be
    added.
    
    As is, it's impossible to use the actual search bar from web in dashboard
    as we do not have a `resModel` nor a `SearchModel` to use it with.
    So, we created a new custom search bar but with the same style as web's
    search bar.
    
    closes odoo/odoo#211650
    
    Task: 4636967
    Related: odoo/enterprise#86414
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

---
+++ addons/spreadsheet_dashboard/static/src/bundle/dashboard_action/dashboard_search_dialog/dashboard_search_dialog.js
@@ -0,0 +1,102 @@
+import { Component, useState } from "@odoo/owl";
+import { FilterValue } from "@spreadsheet/global_filters/components/filter_value/filter_value";
+import { Dialog } from "@web/core/dialog/dialog";
+import { Dropdown } from "@web/core/dropdown/dropdown";
+import { DropdownItem } from "@web/core/dropdown/dropdown_item";
+import { _t } from "@web/core/l10n/translation";
+
+/**
+ * This component is used to display a dialog with the global filters of a
+ * dashboard. It allows the user to select the values of the filters and
+ * confirm or discard the changes.
+ */
+export class DashboardSearchDialog extends Component {
+    static template = "spreadsheet_dashboard.DashboardSearchDialog";
+    static components = {
+        Dialog,
+        Dropdown,
+        DropdownItem,
+        FilterValue,
+    };
+
+    static props = {
+        close: Function,
+        model: Object,
+    };
+
+    setup() {
+        this.state = useState({
+            activeFilters: this.globalFilters
+                .filter((globalFilter) =>
+                    this.props.model.getters.isGlobalFilterActive(globalFilter.id)
+                )
+                .map((globalFilter) => ({
+                    globalFilter,
+                    value: this.props.model.getters.getGlobalFilterValue(globalFilter.id),
+                })),
+        });
+    }
+
+    get globalFilters() {
+        return this.props.model.getters.getGlobalFilters();
+    }
+
+    get hasUnusedGlobalFilters() {
+        return this.globalFilters.length > this.state.activeFilters.length;
+    }
+
+    get unusedGlobalFilters() {
+        return this.globalFilters.filter(
+            (globalFilter) =>
+                !this.state.activeFilters.some((node) => node.globalFilter.id === globalFilter.id)
+        );
+    }
+
+    activateFilter(filter) {
+        this.state.activeFilters.push({ globalFilter: filter });
+    }
+
+    getFilterLabel(node) {
+        return _t(node.globalFilter.label); // Label is extracted from the spreadsheet json file
+    }
+
+    setGlobalFilterValue(node, value, displayNames) {
+        node.value = value;
+        node.displayNames = displayNames;
+    }
+
+    getTranslatedFilterLabel(filter) {
+        return _t(filter.label); // Label is extracted from the spreadsheet json file
+    }
+
+    removeFilter(filterId) {
+        const index = this.state.activeFilters.findIndex(
+            (node) => node.globalFilter.id === filterId
+        );
+        if (index !== -1) {
+            this.state.activeFilters.splice(index, 1);
+        }
+    }
+
+    onConfirm() {
+        for (const filter of this.globalFilters) {
+            const node = this.state.activeFilters.find(
+                (node) => node.globalFilter.id === filter.id
+            );
+            if (node) {
+                this.props.model.dispatch("SET_GLOBAL_FILTER_VALUE", {
+                    id: filter.id,
+                    value: node.value,
+                    displayNames: node.displayNames,
+                });
+            } else {
+                this.props.model.dispatch("SET_GLOBAL_FILTER_VALUE", { id: filter.id });
+            }
+        }
+        this.props.close();
+    }
+
+    onDiscard() {
+        this.props.close();
+    }
+}
