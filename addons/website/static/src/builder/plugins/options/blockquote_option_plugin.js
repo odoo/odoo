a0f255e25a3ee51dbd73153f0f93834bf828898c
Chrysanthe (chgo) <chgo@odoo.com>
2025-05-23 12:52:00 +0200

    [FIX] website: bind `s_blockquote` options on parent handler
    This commit restores the ability to bind the `s_blockquote` options to
    the parent handlers instead of the `s_blockquote` snippet itself, allowing
    us to achieve more interesting layout and provide more flexibility.
    
    This feature was previously implemented in Commit[1],
    which was reverted with the merge of Commit[2] since it did not take the
    change made to the `s_blockquote` into account.
    
    [1]: b3083dad6cb641901afb36c1b0cc892d9f17589a
    [2]: 9fe45e2b7ddbbfd0445ffe25a859e67a316d02b2
    
    task-4818325

--- addons/website/static/src/builder/plugins/options/blockquote_option_plugin.js
+++ addons/website/static/src/builder/plugins/options/blockquote_option_plugin.js
@@ -2,18 +2,37 @@ import { after, END } from "@html_builder/utils/option_sequence";
 import { ANIMATE } from "@website/builder/option_sequence";
 import { Plugin } from "@html_editor/plugin";
 import { registry } from "@web/core/registry";
+import { BlockquoteOption } from "./blockquote_option";
+import { BLOCKQUOTE_PARENT_HANDLERS } from "@website/builder/plugins/options/utils";
 import { withSequence } from "@html_editor/utils/resource";
 import { WebsiteBackgroundOption } from "@website/builder/plugins/options/background_option";
 
 class BlockquoteOptionPlugin extends Plugin {
     static id = "blockquoteOption";
-    selector = ".s_blockquote";
+    blockquoteSelector = ".s_blockquote";
+    blockquoteExclude = `div:is(${BLOCKQUOTE_PARENT_HANDLERS}) > .s_blockquote`;
+    blockquoteDisableWidthApplyTo = ":scope > .s_blockquote";
+    websiteBgApplyTo = ":scope > .s_blockquote";
     resources = {
         mark_color_level_selector_params: [{ selector: this.selector }],
         builder_options: [
+            {
+                OptionComponent: BlockquoteOption,
+                selector: this.blockquoteSelector,
+                exclude: this.blockquoteExclude,
+            },
+            {
+                OptionComponent: BlockquoteOption,
+                selector: BLOCKQUOTE_PARENT_HANDLERS,
+                applyTo: this.blockquoteDisableWidthApplyTo,
+                props: {
+                    disableWidth: true,
+                },
+            },
             withSequence(after(ANIMATE), {
-                selector: this.selector,
                 OptionComponent: WebsiteBackgroundOption,
+                selector: BLOCKQUOTE_PARENT_HANDLERS,
+                applyTo: this.websiteBgApplyTo,
                 props: {
                     withColors: true,
                     withImages: true,
