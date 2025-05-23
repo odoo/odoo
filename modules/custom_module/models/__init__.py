16bb7d778c1493201f705e5fee60aaae342626d9
nourfrioui <nourfrioui88@gmail.com>
2025-05-23 15:32:31 +0100

    feat: manage floor access per server + disable return button when offline

--- modules/custom_module/models/__init__.py
+++ modules/custom_module/models/__init__.py
@@ -7,4 +7,6 @@ from . import pos_category_inherit
 from . import pos_order_inherit
 from . import table_tags
 from . import pos_restaurant_inherit
+from . import hr_employee_inherit
+from . import pos_config_inherit
 
