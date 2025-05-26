c7f5d292b0e7c4be8c4eb72ae915d55d9d5d014e
Claire (clbr) <clbr@odoo.com>
2025-05-26 12:35:39 +0000

    [FIX] account_edi_ubl_cii_tax_extension: Fix tax exemption reason codes
    Some of the reasons were added with a wrong code [1].
    Example: VATEX_EU_AE should have been VATEX-EU-AE...
    
    [1]: https://github.com/odoo/odoo/blob/fba7abd168b99157ee9abd1c084720a90196de39/addons/account_edi_ubl_cii_tax_extension/models/account_tax.py#L76-L85
    
    Source: https://docs.peppol.eu/poacc/billing/3.0/rules/ubl-tc434/BR-CL-22/
    
    task-4817958 (part of)
    
    closes odoo/odoo#212876
    
    X-original-commit: d123df0a0aacd88d89cefb51a795e7865374a46d
    Related: odoo/upgrade#7790
    Signed-off-by: Quentin De Paoli (qdp) <qdp@odoo.com>
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

--- addons/account_edi_ubl_cii_tax_extension/models/account_edi_common.py
+++ addons/account_edi_ubl_cii_tax_extension/models/account_edi_common.py
@@ -68,10 +68,11 @@ class AccountEdiCommon(models.AbstractModel):
 
     def _get_tax_unece_codes(self, customer, supplier, tax):
         if tax.ubl_cii_tax_category_code:
-            tax_exemption_reason = TAX_EXEMPTION_MAPPING.get(tax.ubl_cii_tax_exemption_reason_code)
+            reason_code = tax.ubl_cii_tax_exemption_reason_code
+            tax_exemption_reason = TAX_EXEMPTION_MAPPING.get(reason_code)
             return {
                 'tax_category_code': tax.ubl_cii_tax_category_code,
-                'tax_exemption_reason_code': tax.ubl_cii_tax_exemption_reason_code,
+                'tax_exemption_reason_code': reason_code,
                 'tax_exemption_reason': tax_exemption_reason,
             }
         return super()._get_tax_unece_codes(customer, supplier, tax)
