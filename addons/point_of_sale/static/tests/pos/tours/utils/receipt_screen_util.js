d87aa20152f4f6ad5727496d980df7d721726920
rpka-odoo <rpka@odoo.com>
2025-05-26 16:13:16 +0530

    [IMP] pos_restaurant_preparation_display: test case for unsent order dialog
    Before this commit:
    - The Send Order to Preparation Display  confirmation dialog also appeared when
      validating a refund, which was not necessary and interrupted the expected
      flow.
    
    After this commit:
    - No dialog shown for refunds.
    
    Task-4804669
    
    Related PR - https://github.com/odoo/enterprise/pull/86413

--- addons/point_of_sale/static/tests/pos/tours/utils/receipt_screen_util.js
+++ addons/point_of_sale/static/tests/pos/tours/utils/receipt_screen_util.js
@@ -239,3 +239,12 @@ export function discardOrderWarningDialog() {
         Dialog.discard(),
     ];
 }
+
+export function confirmOrderWarningDialog() {
+    return [
+        {
+            trigger: `.modal-dialog:contains("It seems that the order has not been sent. Would you like to send it to preparation?")`,
+        },
+        Dialog.confirm(),
+    ];
+}
