745cc0cafaa6b4bc8770a8edb5530bcce6c45f09
Florian Damhaut <flda@odoo.com>
2025-05-26 15:33:46 +0200

    [IMP] spreadsheet_dashboard: add tooltips on links

--- addons/spreadsheet_dashboard/static/src/bundle/list/clickable_cell.js
+++ addons/spreadsheet_dashboard/static/src/bundle/list/clickable_cell.js
@@ -3,10 +3,26 @@ import * as spreadsheet from "@odoo/o-spreadsheet";
 import { _t } from "@web/core/l10n/translation";
 
 const { clickableCellRegistry } = spreadsheet.registries;
+const { openLink } = spreadsheet.links;
 
 clickableCellRegistry.add("list", {
     condition: SEE_RECORD_LIST_VISIBLE,
     execute: SEE_RECORD_LIST,
     sequence: 10,
     title: _t("Open record"),
+});
+
+clickableCellRegistry.add("link_with_tooltip", {
+    condition: (position, getters) => !!getters.getEvaluatedCell(position).link,
+    execute: (position, env, isMiddleClick) =>
+        openLink(env.model.getters.getEvaluatedCell(position).link, env, isMiddleClick),
+    sequence: 2,
+    titleCompute: (position, getters) => {
+        const link = getters.getEvaluatedCell(position).link;
+        if (link.isExternal) {
+            return _t("Go to url: %s", link.url);
+        } else {
+            return _t("Go to %s", link.label);
+        }
+    },
 });
