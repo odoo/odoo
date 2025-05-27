c6eb8bbc027d3f46b0ba3904daff36a708d21efe
Denis Ledoux <dle@odoo.com>
2025-05-27 17:30:47 +0200

    [FIX] core: http, facade Werkzeug Response
    opw-4838986

--- odoo/http.py
+++ odoo/http.py
@@ -167,6 +167,7 @@ from .service import security, model as service_model
 from .tools import (config, consteq, date_utils, file_path, parse_version,
                     profiler, submap, unique, ustr,)
 from .tools.geoipresolver import GeoIPResolver
+from .tools.facade import Proxy, ProxyAttr, ProxyFunc
 from .tools.func import filter_kwargs, lazy_property
 from .tools.mimetypes import guess_mimetype
 from .tools.misc import pickle
@@ -1115,7 +1116,7 @@ for attr in HTTPREQUEST_ATTRIBUTES:
     setattr(HTTPRequest, attr, property(*make_request_wrap_methods(attr)))
 
 
-class Response(werkzeug.wrappers.Response):
+class _Response(werkzeug.wrappers.Response):
     """
     Outgoing HTTP response with body, status, headers and qweb support.
     In addition to the :class:`werkzeug.wrappers.Response` parameters,
@@ -1203,6 +1204,142 @@ class Response(werkzeug.wrappers.Response):
             expires = 0
             max_age = 0
         super().set_cookie(key, value=value, max_age=max_age, expires=expires, path=path, domain=domain, secure=secure, httponly=httponly, samesite=samesite)
+
+
+class Headers(Proxy):
+    _wrapped__ = werkzeug.datastructures.Headers
+
+    __getitem__ = ProxyFunc()
+    __repr__ = ProxyFunc(str)
+    __setitem__ = ProxyFunc(None)
+    __str__ = ProxyFunc(str)
+    __contains__ = ProxyFunc(bool)
+    add = ProxyFunc(None)
+    add_header = ProxyFunc(None)
+    clear = ProxyFunc(None)
+    copy = ProxyFunc(lambda v: Headers(v))  # noqa: PLW0108
+    extend = ProxyFunc(None)
+    get = ProxyFunc()
+    get_all = ProxyFunc()
+    getlist = ProxyFunc()
+    items = ProxyFunc()
+    keys = ProxyFunc()
+    pop = ProxyFunc()
+    popitem = ProxyFunc()
+    remove = ProxyFunc(None)
+    set = ProxyFunc(None)
+    setdefault = ProxyFunc()
+    to_wsgi_list = ProxyFunc()
+    values = ProxyFunc()
+    if hasattr(werkzeug.datastructures.Headers, "setlist"):
+        # werkzeug >= 1.0
+        setlist = ProxyFunc(None)
+        setlistdefault = ProxyFunc()
+        update = ProxyFunc(None)
+
+
+class ResponseCacheControl(Proxy):
+    _wrapped__ = werkzeug.datastructures.ResponseCacheControl
+
+    __getitem__ = ProxyFunc()
+    __setitem__ = ProxyFunc(None)
+    immutable = ProxyAttr(bool)
+    max_age = ProxyAttr(int)
+    must_revalidate = ProxyAttr(bool)
+    no_cache = ProxyAttr(bool)
+    no_store = ProxyAttr(bool)
+    no_transform = ProxyAttr(bool)
+    public = ProxyAttr(bool)
+    private = ProxyAttr(bool)
+    proxy_revalidate = ProxyAttr(bool)
+    s_maxage = ProxyAttr(int)
+    pop = ProxyFunc()
+
+
+class ResponseStream(Proxy):
+    _wrapped__ = werkzeug.wrappers.ResponseStream
+
+    write = ProxyFunc(int)
+    writelines = ProxyFunc(None)
+    tell = ProxyFunc(int)
+
+
+class Response(Proxy):
+    _wrapped__ = _Response
+
+    # werkzeug.wrappers.Response attributes
+    __call__ = ProxyFunc()
+    add_etag = ProxyFunc(None)
+    age = ProxyAttr()
+    autocorrect_location_header = ProxyAttr(bool)
+    cache_control = ProxyAttr(ResponseCacheControl)
+    call_on_close = ProxyFunc()
+    charset = ProxyAttr(str)
+    content_encoding = ProxyAttr(str)
+    content_length = ProxyAttr(int)
+    content_location = ProxyAttr(str)
+    content_md5 = ProxyAttr(str)
+    content_type = ProxyAttr(str)
+    data = ProxyAttr()
+    default_mimetype = ProxyAttr(str)
+    default_status = ProxyAttr(int)
+    delete_cookie = ProxyFunc(None)
+    direct_passthrough = ProxyAttr(bool)
+    expires = ProxyAttr()
+    force_type = ProxyFunc(lambda v: Response(v))  # noqa: PLW0108
+    freeze = ProxyFunc(None)
+    get_data = ProxyFunc()
+    get_etag = ProxyFunc()
+    headers = ProxyAttr(Headers)
+    is_json = ProxyAttr(bool)
+    is_sequence = ProxyAttr(bool)
+    is_streamed = ProxyAttr(bool)
+    iter_encoded = ProxyFunc()
+    json = ProxyAttr()
+    last_modified = ProxyAttr()
+    location = ProxyAttr(str)
+    make_conditional = ProxyFunc(lambda v: Response(v))  # noqa: PLW0108
+    make_sequence = ProxyFunc(None)
+    max_cookie_size = ProxyAttr(int)
+    mimetype = ProxyAttr(str)
+    response = ProxyAttr()
+    retry_after = ProxyAttr()
+    set_cookie = ProxyFunc(None)
+    set_data = ProxyFunc(None)
+    set_etag = ProxyFunc(None)
+    status = ProxyAttr(str)
+    status_code = ProxyAttr(int)
+    stream = ProxyAttr(ResponseStream)
+    if hasattr(_Response, "get_json"):
+        # werkzeug >= 2.3.0
+        get_json = ProxyFunc()
+
+    # odoo.http._response attributes
+    load = ProxyFunc()
+    set_default = ProxyFunc(None)
+    qcontext = ProxyAttr()
+    template = ProxyAttr(str)
+    is_qweb = ProxyAttr(bool)
+    render = ProxyFunc()
+    flatten = ProxyFunc(None)
+
+    def __init__(self, *args, **kwargs):
+        response = args[0] if len(args) == 1 and isinstance(args[0], _Response) else _Response(*args, **kwargs)
+        super().__init__(response)
+        if 'set_cookie' in response.__dict__:
+            self.__dict__['set_cookie'] = response.__dict__['set_cookie']
+
+
+werkzeug_abort = werkzeug.exceptions.abort
+
+
+def abort(status, *args, **kwargs):
+    if isinstance(status, Response):
+        status = status._wrapped__
+    werkzeug_abort(status, *args, **kwargs)
+
+
+werkzeug.exceptions.abort = abort
 
 
 class FutureResponse:
