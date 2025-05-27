10ec14791b7f4a7dd16d21e1596d1beeaf1ab195
Ahmed Khater <akr@odoo.com>
2025-05-27 06:52:13 +0000

    [IMP] l10n_sa_edi: generating serial number on journal and fixing the common name to be unique per journal
    Previously, the company name was used as the common name when onboarding the journal. However, the common name has to be unique.
    The fix changes the common name to use the journal's short code, journal name, and company name to ensure uniquness.
    
    Additionally, an improvement is applied to the serial number on journals. Previously, users inputted this field manually.
    Now, the system uses the journal's id as the serial number to ensure uniqueness.
    
    A post-migration script was added to notify users that they need to re-onboard their journals.
    This is done in case users previously onboarded journals with non-unique serial numbers.
    
    task-4797124
    
    X-original-commit: fb74d9f2d6cc001ac1b5968d8cd81096d721df05

---
+++ addons/l10n_sa_edi/migrations/0.2/post-migrate.py
@@ -0,0 +1,15 @@
+from odoo import _, api, SUPERUSER_ID
+
+
+def migrate(cr, version):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    user = env.ref("base.user_admin")
+    zatca_format = env.ref('l10n_sa_edi.edi_sa_zatca')
+    journals = env["account.journal"].search([
+        ("edi_format_ids", "in", zatca_format.id),
+        ("l10n_sa_compliance_checks_passed", "=", True),
+        ("l10n_sa_production_csid_json", "!=", False)])
+    journals.activity_schedule(
+        act_type_xmlid='mail.mail_activity_data_warning',
+        user_id=user.id,
+        note=_('Please Re-Onboard the Journal for a new serial number'))
--- addons/l10n_sa_edi/models/account_journal.py
+++ addons/l10n_sa_edi/models/account_journal.py
@@ -80,7 +80,7 @@ class AccountJournal(models.Model):
                                                 readonly=True, copy=False)
 
     l10n_sa_serial_number = fields.Char("Serial Number", copy=False,
-                                        help="The serial number of the Taxpayer solution unit. Provided by ZATCA")
+                                        help="Unique Serial Number automatically filled when the journal is onboarded")
 
     l10n_sa_latest_submission_hash = fields.Char("Latest Submission Hash", copy=False,
                                                  help="Hash of the latest submitted invoice to be used as the Previous Invoice Hash (KSA-13)")
@@ -111,6 +111,16 @@ class AccountJournal(models.Model):
         stuck_moves = [move for move in move_ids if not move._l10n_sa_is_in_chain()]
         if stuck_moves:
             raise UserError(_("Oops! The journal is stuck. Please submit the pending invoices to ZATCA and try again."))
+
+    def _l10n_sa_edi_set_csr_fields(self):
+        '''
+            Sets default values for CSR generation fields in Odoo, if their values do not exist
+        '''
+        self.ensure_one()
+        # Avoid unnecessary write calls
+        if self.l10n_sa_serial_number != str(self.id):
+            self.l10n_sa_serial_number = self.id
+
     # ====== CSR Generation =======
 
     def _l10n_sa_csr_required_fields(self):
@@ -159,6 +169,7 @@ class AccountJournal(models.Model):
         # we want to perform sanity checks to ensure that the journal is ready to be onboarded
         # If the check fails, we do not want to revoke the existing PCSID because the user might still need it to post hanging invoices
         self._l10n_sa_api_onboard_sanity_checks()
+        self._l10n_sa_edi_set_csr_fields()
 
         try:
             # If the company does not have a private key, we generate it.
