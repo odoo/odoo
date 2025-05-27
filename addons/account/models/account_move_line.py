5120fb5811a97c11dfeb0369b3431ffb8dbf1e33
Trinh Ngoc Hung <ngochung207@gmail.com>
2025-05-27 08:17:51 +0000

    [FIX] account: identify lines of different account types if any of the
    lines is off-balance
    
    Issue: if only one line is edited/added and it's account is not of type
    off-balance, the check will fail to raise the warning
    
    Steps to reproduce the error:
     - Create entry
     - Account move line 1 with account off balance
     - Account move line 2 with account off balance
     - Save move
     - Edit line 2, change the account to an asset type account.
     - Save move.
    The system does not give a warning.
    
    closes odoo/odoo#213139
    
    Reason: The check/audit did not cover all accounting items
    Solution: Re-evaluate all related journal items of the journal entry.
    X-original-commit: 001506749400097b4721d695e6ae3d189521cc1d
    Signed-off-by: John Laterre (jol) <jol@odoo.com>

--- addons/account/models/account_move_line.py
+++ addons/account/models/account_move_line.py
@@ -1215,9 +1215,9 @@ class AccountMoveLine(models.Model):
 
     @api.constrains('account_id', 'tax_ids', 'tax_line_id', 'reconciled')
     def _check_off_balance(self):
-        for line in self:
+        for line in self.move_id.line_ids:
             if line.account_id.account_type == 'off_balance':
-                if any(a.internal_group != line.account_id.internal_group for a in line.move_id.line_ids.account_id):
+                if any(a.account_type != line.account_id.account_type for a in line.move_id.line_ids.account_id):
                     raise UserError(_('If you want to use "Off-Balance Sheet" accounts, all the accounts of the journal entry must be of this type'))
                 if line.tax_ids or line.tax_line_id:
                     raise UserError(_('You cannot use taxes on lines with an Off-Balance account'))
