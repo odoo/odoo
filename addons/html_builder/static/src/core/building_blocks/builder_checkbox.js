3b96f8dc633f4d5fef88ffb68066fa6116e9e8e3
Alice Gaudon <agau@odoo.com>
2025-05-27 10:57:34 +0200

    [IMP] html_builder: add preview to BuilderCheckbox
    This is done similarly to BuilderButton

--- addons/html_builder/static/src/core/building_blocks/builder_checkbox.js
+++ addons/html_builder/static/src/core/building_blocks/builder_checkbox.js
@@ -29,6 +29,8 @@ export class BuilderCheckbox extends Component {
             { onReady }
         );
         this.onChange = operation.commit;
+        this.onMouseenter = operation.preview;
+        this.onMouseleave = operation.revert;
     }
 
     getClassName() {
