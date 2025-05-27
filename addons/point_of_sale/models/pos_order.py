c5c00f7509bbba5ce3c5bcb6af24123d81d77f18
David Monnom <moda@odoo.com>
2025-05-27 11:51:23 +0200

    [FIX] point_of_sale, pos_restaurant: disable paid order editing
    Before this commit, when working with PoS on several devices, one of
    which is offline, a synchronization error occurred.
    
    Example:
    1. PoS-1 creates a command and then loses its connection
    2. PoS-2 the order is received from PoS-1, it is paid here.
    3. PoS-1 adds a command line and recovers its connection.
    
    The command returns to “draft” state, which should not be possible.
    
    Now, once a command has been paid for, it cannot be drafted again. An
    error is returned to the user indicating that the order has been
    finalized.
    
    This commit also adds a server request in case of error during orders
    synchronization.
    
    closes odoo/odoo#211771
    
    X-original-commit: 9918832
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/point_of_sale/models/pos_order.py
+++ addons/point_of_sale/models/pos_order.py
@@ -520,6 +520,9 @@ class PosOrder(models.Model):
                         country=order.partner_id.country_id or self.env.company.country_id)
             if vals.get('has_deleted_line') is not None and self.has_deleted_line:
                 del vals['has_deleted_line']
+            allowed_vals = ['paid', 'done', 'invoiced']
+            if vals.get('state') and vals['state'] not in allowed_vals and order.state in allowed_vals:
+                raise UserError(_('This order has already been paid. You cannot set it back to draft or edit it.'))
 
         list_line = self._create_pm_change_log(vals)
         res = super().write(vals)
@@ -1097,7 +1100,7 @@ class PosOrder(models.Model):
         sync_token = randrange(100_000_000)  # Use to differentiate 2 parallels calls to this function in the logs
         _logger.info("PoS synchronisation #%d started for PoS orders references: %s", sync_token, [self._get_order_log_representation(order) for order in orders])
         order_ids = []
-        session_ids = set({order.get('session_id') for order in orders})
+
         for order in orders:
             order_log_name = self._get_order_log_representation(order)
             _logger.debug("PoS synchronisation #%d processing order %s order full data: %s", sync_token, order_log_name, pformat(order))
