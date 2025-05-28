60c8c6226919eb406c33fe5a0f8ce180c0ed258b
David Monnom <moda@odoo.com>
2025-05-28 13:18:08 +0000

    [FIX] pos_self_order: Allow cancelling self order
    Prior to this change, when users cancelled their payment, they remained
    stuck on their previous order. It was not possible to cancel the order
    from the basket page.
    
    This change allows the user to cancel the order from the shopping
    cart page.
    
    Taskid: 4830007
    X-original-commit: 82ed2cade0a3c46a82c8a7c4e3187dd29978ecd1

--- addons/pos_self_order/controllers/orders.py
+++ addons/pos_self_order/controllers/orders.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 import re
 from datetime import timedelta
-from odoo import http, fields
+from odoo import http, fields, _
 from odoo.http import request
 from odoo.tools import float_round
 from odoo.osv import expression
 from werkzeug.exceptions import NotFound, BadRequest, Unauthorized
+from odoo.exceptions import MissingError
+from odoo.tools import consteq
 
 class PosSelfOrderController(http.Controller):
     @http.route("/pos-self-order/process-order/<device_type>/", auth="public", type="json", website=True)
@@ -109,6 +111,19 @@ class PosSelfOrderController(http.Controller):
                         'tax_ids': child_product.taxes_id,
                     })
                 lst_price = 0
+
+    @http.route('/pos-self-order/remove-order', auth='public', type='json', website=True)
+    def remove_order(self, access_token, order_id, order_access_token):
+        pos_config = self._verify_pos_config(access_token)
+        pos_order = pos_config.env['pos.order'].browse(order_id)
+
+        if not pos_order.exists() or not consteq(pos_order.access_token, order_access_token):
+            raise MissingError(_("Your order does not exist or has been removed"))
+
+        if pos_order.state != 'draft':
+            raise Unauthorized(_("You are not authorized to remove this order"))
+
+        pos_order.remove_from_ui([pos_order.id])
 
     @http.route('/pos-self-order/get-orders', auth='public', type='json', website=True)
     def get_orders_by_access_token(self, access_token, order_access_tokens, table_identifier=None):
