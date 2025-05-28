711e70d5f56e71f95d0eeb6b4aff5c504a34ba0a
Youssef <abyo@odoo.com>
2025-05-28 18:22:28 +0200

    [IMP] stock: move cost_method to settings
    Currently the costing method field is hard to find and the company form is crowded, This commits moves the costing method
    into the settings in order to make it easily accessed and give the company form a cleaner look.
    
    Changes:
    - cost_method moved to settings
    - cost_method field is now blank by default in category and matches with the one set by the company.
    - Moved cost_method from stock_account to stock to add it as a related field for the cost_method in settings.
    - Added an onchange method to handle the cost_method for multiple companies.
    - Hide expense and income accounts to move it to settings
    - Support multi-company cost_method changes
    - Update pos test to accept blank cost_method
    
    Task:4720952

--- addons/stock_account/models/res_config_settings.py
+++ addons/stock_account/models/res_config_settings.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models, _
 
 
 class ResConfigSettings(models.TransientModel):
@@ -13,6 +13,24 @@ class ResConfigSettings(models.TransientModel):
                                           implied_group='stock_account.group_lot_on_invoice')
     group_stock_accounting_automatic = fields.Boolean(
         "Automatic Stock Accounting", implied_group="stock_account.group_stock_accounting_automatic")
+    cost_method = fields.Selection(
+        related="company_id.cost_method",
+        string="Costing Method",
+        required=True,
+        readonly=False,
+    )
+
+    @api.onchange('cost_method')
+    def onchange_cost_method(self):
+        if self.cost_method == self.company_id.cost_method:
+            # don't display the warning when loading the settings
+            return
+        return {
+            'warning': {
+                'title': _("Warning"),
+                'message': _("You are changing your company's default costing method. Are you sure you want to make that change?"),
+            }
+        }
 
     def set_values(self):
         automatic_before = self.env.user.has_group('stock_account.group_stock_accounting_automatic')
