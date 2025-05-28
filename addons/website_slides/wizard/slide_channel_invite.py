917b7eb70ae1f2c2ca634cb2917385fb7c56596d
Thibault Delavallée <tde@odoo.com>
2025-05-28 14:23:24 +0200

    [IMP] mail, various: ease encapsulation rendering helper
    Make it more easy to call. Notably it requires a message, as its main
    use is like discuss flow which encapsulates content coming from a
    mail.message. This can be done directly in the tool method, allowing
    caller to have simpler API.
    
    Use the tool in various code places where it is done by hand, mainly
    in invite-like wizards.
    
    Task-4690371

--- addons/website_slides/wizard/slide_channel_invite.py
+++ addons/website_slides/wizard/slide_channel_invite.py
@@ -105,18 +105,13 @@ class SlideChannelInvite(models.TransientModel):
         # optional support of default_email_layout_xmlid in context
         email_layout_xmlid = self.env.context.get('default_email_layout_xmlid', self.env.context.get('notif_layout'))
         if email_layout_xmlid:
-            # could be great to use ``_notify_by_email_prepare_rendering_context`` someday
-            template_ctx = {
-                'message': self.env['mail.message'].sudo().new({'body': mail_values['body_html'], 'record_name': self.channel_id.name}),
-                'model_description': self.env['ir.model']._get('slide.channel').display_name,
-                'record': slide_channel_partner,
-                'company': self.env.company,
-                'signature': self.channel_id.user_id.signature,
-            }
-            body = self.env['ir.qweb']._render(email_layout_xmlid, template_ctx, engine='ir.qweb', minimal_qcontext=True, raise_if_not_found=False, lang=lang)
-            if body:
-                mail_values['body_html'] = self.env['mail.render.mixin']._replace_local_links(body)
-            else:
-                _logger.warning('QWeb template %s not found when sending slide channel mails. Sending without layout.', email_layout_xmlid)
+            mail_values['body_html'] = self._render_encapsulate(
+                email_layout_xmlid, mail_values['body_html'],
+                context_record=slide_channel_partner,
+                add_context={
+                    'record_name': self.channel_id.name,
+                    'signature': self.channel_id.user_id.signature,
+                },
+            )
 
         return mail_values
