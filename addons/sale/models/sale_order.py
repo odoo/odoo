bac621368f041ad443b44a9f23b8e43b0d9fe54b
Thibault Delavallée <tde@odoo.com>
2025-05-28 17:05:39 +0200

    [IMP] mail: allow to force record_name when posting / notifying
    Record name is used notably in email layouts, displayed as contextual
    reminder. It is now possible to force its value, independently from
    the message content. Rationale is that record_name on message is about
    to disappear and in some cases we would want to force a custom record
    name.
    
    Task-4690371
    
    Part-of: odoo/odoo#212083
    Related: odoo/enterprise#86621
    Related: odoo/upgrade#7784
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/sale/models/sale_order.py
+++ addons/sale/models/sale_order.py
@@ -1672,10 +1672,12 @@ class SaleOrder(models.Model):
         return groups
 
     def _notify_by_email_prepare_rendering_context(self, message, msg_vals=False, model_description=False,
-                                                   force_email_company=False, force_email_lang=False):
+                                                   force_email_company=False, force_email_lang=False,
+                                                   force_record_name=False):
         render_context = super()._notify_by_email_prepare_rendering_context(
             message, msg_vals=msg_vals, model_description=model_description,
-            force_email_company=force_email_company, force_email_lang=force_email_lang
+            force_email_company=force_email_company, force_email_lang=force_email_lang,
+            force_record_name=force_record_name,
         )
         lang_code = render_context.get('lang')
         record = render_context['record']
