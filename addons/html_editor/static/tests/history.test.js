c8e31bebed19a29a3d438a2af38f72a8e66509ba
Rodolpho Lima <rcdl@odoo.com>
2025-05-28 13:35:00 +0200

    [IMP] html_editor: prevent node duplication on unserialization
    This commit tackles an old to-do: avoid different nodes pointing to the
    same id.
    
    It does so by solving the root cause, which was creating a text node on
    unserialization, even when such node already existed in the
    `idToNodeMap`. This commit simply applies the same logic that is used
    when unserializing an element: use the existing node when available.
    
    Additionally, this commit also replaces the `nodeToIdMap` and
    `idToNodeMap` pair with the right tool for the job: a bi-directional
    map, in which only a single mapping exists between a Node and its ID,
    preventing the issue to happen again.
    
    Lastly, this commit handles edge cases involving unserialization of
    nodes of types other than text node or element.
    
    task-4678910

--- addons/html_editor/static/tests/history.test.js
+++ addons/html_editor/static/tests/history.test.js
@@ -226,7 +226,7 @@ describe("selection", () => {
         await pointerUp(pElement);
         await tick();
         const historyPlugin = plugins.get("history");
-        const nodeId = historyPlugin.nodeToIdMap.get(pElement.firstChild);
+        const nodeId = historyPlugin.nodeMap.getId(pElement.firstChild);
         expect(historyPlugin.currentStep.selection).toEqual({
             anchorNodeId: nodeId,
             anchorOffset: 0,
@@ -883,7 +883,7 @@ describe("serialization", () => {
 
         const historyPlugin = plugins.get("history");
         const mutations = historyPlugin.currentStep.mutations;
-        const idToNode = (id) => historyPlugin.idToNodeMap.get(id);
+        const idToNode = (id) => historyPlugin.nodeMap.getNode(id);
 
         expect(mutations.length).toBe(3);
 
@@ -925,7 +925,7 @@ describe("serialization", () => {
 
         const historyPlugin = plugins.get("history");
         const mutations = historyPlugin.currentStep.mutations;
-        const idToNode = (id) => historyPlugin.idToNodeMap.get(id);
+        const idToNode = (id) => historyPlugin.nodeMap.getNode(id);
 
         expect(mutations.length).toBe(5);
 
@@ -956,6 +956,18 @@ describe("serialization", () => {
         ({ nodeId, children } = mutations[4].node);
         expect(idToNode(nodeId)).toBe(c);
         expect(children.length).toBe(0);
+    });
+
+    test("unserialization of text node should not duplicate an existing one", async () => {
+        const { el, editor, plugins } = await setupEditor(`<p><br></p>`);
+        const historyPlugin = plugins.get("history");
+        const p = el.querySelector("p");
+        const textNode = editor.document.createTextNode("test");
+        p.prepend(textNode);
+        editor.shared.history.addStep();
+        const serializedNode = historyPlugin.serializeNode(textNode);
+        const unserializedTextNode = historyPlugin.unserializeNode(serializedNode);
+        expect(unserializedTextNode).toBe(textNode);
     });
 });
 
