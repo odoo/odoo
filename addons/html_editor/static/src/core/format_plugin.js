5395885564314485241363430af5937caaeb9da0
adch-odoo <adch@odoo.com>
2025-05-28 13:07:32 +0000

    [FIX] html_editor: remove font size with color styles
    Steps to reproduce:
    
    - Write and select some text .
    - Apply font size & then text/background color.
    - Click the 'Remove Format' button in the toolbar.
    - Color styles are removed, but the font size remains.
    - Only on a second click, the font size is removed.
    
    Before this commit:
    
    - Font size was not being removed when color styles were present because font
    size checks `closestElement(node`) has font-size style or font-related classes.
    - However, the <font> tag used for color styling hadn’t been removed yet at
    that point, so the font size appeared to still exist.
    - An extra history step from the align_plugin is causing issues with undo.
    
    After this commit:
    
    - Remove color styles first, then remove the font size afterward.
    - A history step is now added only once after all formatting is removed.
    
    task-4736914
    
    closes odoo/odoo#213126
    
    X-original-commit: 7dcab9ea8fa8430773ba7c6dd8b74e5b8e85bf98
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/html_editor/static/src/core/format_plugin.js
+++ addons/html_editor/static/src/core/format_plugin.js
@@ -159,6 +159,7 @@ export class FormatPlugin extends Plugin {
 
     removeFormat() {
         const targetedNodes = this.dependencies.selection.getTargetedNodes();
+        this.dispatchTo("remove_format_handlers");
         for (const format of Object.keys(formatsSpecs)) {
             if (
                 !formatsSpecs[format].removeStyle ||
@@ -168,7 +169,6 @@ export class FormatPlugin extends Plugin {
             }
             this._formatSelection(format, { applyStyle: false });
         }
-        this.dispatchTo("remove_format_handlers");
         this.dependencies.history.addStep();
     }
 
