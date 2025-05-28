df190a5caf0fa9815482096b7d42938de3c5d195
Guillaume V <gvar@odoo.com>
2025-05-28 13:14:15 +0000

    [FIX] hr: Remove greyed-out days for flexible working hours
    The calendar view in the Time Off app was displaying greyed-out days.
    These days corresponded to those from the underlying work schedule
    on which the flexible working hours were based.
    
    Steps to reproduce:
    -------------------
    * In the Work Information tab of an Employee, set the Working Hours to flexible.
    * Save and click on the Time Off smart button
    
    > Observation:
    Some days were greyed-out
    
    Why the fix:
    ------------
    `get_unusual_days()` usually return a dict of each days with True or False.
    That dict is then interpreted to display white or greyed-out days.
    Simply be returning False, every days are gonig to be white.
    
    opw-4816609
    
    closes odoo/odoo#217090
    
    X-original-commit: 72da89785e7c31ec171cff2638c591e6c94728fd
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr/tests/test_hr_employee.py
+++ addons/hr/tests/test_hr_employee.py
@@ -1,4 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from psycopg2.errors import UniqueViolation
 
@@ -528,6 +529,34 @@ class TestHrEmployee(TestHrCommon):
         self.assertNotEqual(self.employee.current_version_id, old_version)
         self.assertEqual(self.employee.resource_calendar_id, old_calendar)
         self.assertEqual(self.employee.resource_id.calendar_id, old_calendar)
+
+    def test_flexible_working_hours(self):
+        """
+        Test to verifie that get_unusual_days() return false for flexible work schedule
+        """
+
+        # Creating a flexible working schedule
+        calendar_flex = self.env['resource.calendar'].create([
+            {
+                'tz': "Europe/Brussels",
+                'name': 'flexible hours',
+                'flexible_hours': "True",
+            },
+        ])
+        employeeA = self.env['hr.employee'].create({
+            'name': 'Employee',
+        })
+
+        # Testing employeA on regular working schedule
+        days = employeeA._get_unusual_days(str(datetime(2025, 1, 1)), str(datetime(2025, 12, 31)))
+        self.assertTrue(days)
+        self.assertTrue(days['2025-01-04'])
+
+        # Assigning flexible work hours to employeeA
+        employeeA.resource_calendar_id = calendar_flex.id
+        days = employeeA._get_unusual_days(str(datetime(2025, 1, 1)), str(datetime(2025, 12, 31)))
+        self.assertTrue(days)
+        self.assertFalse(days['2025-01-04'])
 
 
 @tagged('-at_install', 'post_install')
