605fdfc19b6b2ec59bc949d33c5a90ae573817af
Garvish Panchal <gppa@odoo.com>
2025-05-28 11:19:41 +0530

    [IMP] website: allow bulk translation of selection field options
    Previously, translating selection field options required individual interaction
    for each option, resulting in a repetitive and inefficient process.
    
    This commit introduces a bulk translation dialog that displays all options
    within the `.o_translation_select` container. Users can now translate all
    options at once through editable input fields in a single dailog. Upon
    submission, the UI updates the text and translation state (`oe_translated`
    class) of each option accordingly.
    
    This significantly improves the translation workflow for selection fields.

--- addons/website/static/src/builder/translation_components/selectTranslateDialog.js
+++ addons/website/static/src/builder/translation_components/selectTranslateDialog.js
@@ -7,21 +7,29 @@ export class SelectTranslateDialog extends Component {
     static components = { WebsiteDialog };
     static template = "website_builder.SelectTranslateDialog";
     static props = {
-        node: { validate: (p) => p.nodeType === Node.ELEMENT_NODE },
+        node: {
+            validate: (p) =>
+                Array.isArray(p) && p.every(n => n.nodeType === Node.ELEMENT_NODE),
+        },
         addStep: Function,
         close: Function,
     };
     setup() {
-        this.inputEl = useRef("input");
+        this.inputEls = useRef("selectTranslateDialog");
     }
 
     onInputChange() {
-        const value = this.inputEl.el.value;
-        this.optionEl.textContent = value;
-        this.optionEl.classList.toggle(
-            "oe_translated",
-            value !== this.optionEl.dataset.initialTranslationValue
-        );
+        const inputs = this.inputEls.el.querySelectorAll(".select-translate-input");
+        const values = Array.from(inputs).map(input => input.value);
+        const options = this.optionEl;
+        values.map((value, index) => [value, options[index]])
+            .forEach(([value, option]) => {
+                option.textContent = value;
+                option.classList.toggle(
+                    "oe_translated",
+                    value !== option.dataset.initialTranslationValue
+                );
+            });
     }
 
     get optionEl() {
