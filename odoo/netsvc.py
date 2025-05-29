08fb0aac4a8227b781db98ff4cd5afd085e039dd
Krzysztof Magusiak (krma) <krma@odoo.com>
2025-05-29 22:05:15 +0000

    [IMP] netsvc: avoid importing bs4 when starting
    When setting-up logging, avoid importing bs4 (beautifulsoup) which is a
    quite large module. Add a hook to ignore the warnings if the module is
    imported.
    
    closes odoo/odoo#212408
    
    Related: odoo/enterprise#86719
    Signed-off-by: Julien Castiaux (juc) <juc@odoo.com>

--- odoo/netsvc.py
+++ odoo/netsvc.py
@@ -199,11 +199,10 @@ def init_logger():
     warnings.simplefilter('default', category=DeprecationWarning)
     # https://github.com/urllib3/urllib3/issues/2680
     warnings.filterwarnings('ignore', r'^\'urllib3.contrib.pyopenssl\' module is deprecated.+', category=DeprecationWarning)
-    # ofxparse use an html parser to parse ofx xml files and triggers a warning since bs4 4.11.0
-    # https://github.com/jseutter/ofxparse/issues/170
-    with contextlib.suppress(ImportError):
-        from bs4 import XMLParsedAsHTMLWarning
-        warnings.filterwarnings('ignore', category=XMLParsedAsHTMLWarning)
+    if bs4 := sys.modules.get('bs4'):
+        # avoid loading bs4 module during initialization; this filter is also
+        # monkeypatched if the import is done afterwards
+        warnings.filterwarnings('ignore', category=bs4.XMLParsedAsHTMLWarning)
     # ignore a bunch of warnings we can't really fix ourselves
     for module in [
         'babel.util', # deprecated parser module, no release yet
