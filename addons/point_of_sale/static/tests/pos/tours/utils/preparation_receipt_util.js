ee0437ea4ada9ee6322f742b96fcd0ecf973661a
sjai-odoo <sjai@odoo.com>
2025-05-29 09:41:41 +0000

    [FIX] point_of_sale: internal note on KOT
    Steps to reproduce:
    -------------------------
    - Install `pos_restaurant` and configure a kitchen printer.
    - Open a session and create an order.
    - Change the order level internal note.
    
    Issue:
    -------
    - The KOT for order level internal note change was not properly
    printed.
    
    Cause:
    ---------
    - In backend we are storing the array of objects to obtain color of the note
    and without parsing string from that it was shown as it is.
    
    FIX:
    -----
    - We have now parsed the note and sent the appropriate texts to print.
    - The `orderData.change` was reset wrongly, instead of removing the
    keys we just need to reset them.
    - We have also corrected the condition to show INTERNAL & CUSTOMER
    notes as hasDataChanges will be an array and only if we have something
    changed than only we need that.
    
    closes odoo/odoo#215322
    
    Task: 4830054
    X-original-commit: cb59be7f8141379c758cae6c66f2db3e0085bb87
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Jainam Shah (sjai) <sjai@odoo.com>

--- addons/point_of_sale/static/tests/pos/tours/utils/preparation_receipt_util.js
+++ addons/point_of_sale/static/tests/pos/tours/utils/preparation_receipt_util.js
@@ -2,7 +2,7 @@
 
 import { renderToElement } from "@web/core/utils/render";
 
-export function generatePreparationReceiptElement() {
+export function generatePreparationChanges() {
     const order = posmodel.getOrder();
     const orderChange = posmodel.changesToOrder(
         order,
@@ -10,13 +10,16 @@ export function generatePreparationReceiptElement() {
         false
     );
 
-    const { orderData, changes } = posmodel.generateOrderChange(
+    return posmodel.generateOrderChange(
         order,
         orderChange,
         Array.from(posmodel.config.preparationCategories),
         false
     );
+}
 
+export function generatePreparationReceiptElement() {
+    const { orderData, changes } = generatePreparationChanges();
     orderData.changes = {
         title: "new",
         data: changes.new,
