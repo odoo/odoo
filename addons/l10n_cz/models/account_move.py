9ac1d42a51f0033cfb903e6c9981542e0784f1d4
Hesham Saleh <hsal@odoo.com>
2025-05-30 11:08:10 +0000

    [ADD] l10n_pl_taxable_supply_date: Added taxable supply date for Poland
    This new module aims at adding support for taxable supply date in Poland.
    
    task-4829233
    
    closes odoo/odoo#224896
    
    X-original-commit: 1727ed688f995cff5286b1eb0c372d87f894f258
    Signed-off-by: Hesham Saleh (hsal) <hsal@odoo.com>
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/l10n_cz/models/account_move.py
+++ addons/l10n_cz/models/account_move.py
@@ -5,7 +5,12 @@ from odoo import models, fields, api
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    taxable_supply_date = fields.Date(default=fields.Date.today())
+    taxable_supply_date = fields.Date(compute='_compute_taxable_supply_date', store=True, readonly=False, precompute=True)
+
+    @api.depends('country_code')
+    def _compute_taxable_supply_date(self):
+        for move in self.filtered(lambda m: m.country_code == 'CZ' and not m.taxable_supply_date):
+            move.taxable_supply_date = fields.Date.context_today(move)
 
     @api.depends('taxable_supply_date')
     def _compute_date(self):
