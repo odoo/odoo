87fcd2966de58fbbecd1d0a860253983917ded33
paan-odoo <paan@odoo.com>
2025-05-30 17:10:02 +0530

    [IMP] point_of_sale: consistency between frontend and backend refund
    In this commit:
    
    - Improved the refund flow initiated from the backend to align with the frontend
      behavior.
    - Ensured consistency in refund order creation, state transition, and payment
      handling between the frontend and the backend.
    
    task-4831414
    
    closes odoo/odoo#213291
    
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/point_of_sale/static/tests/pos/tours/ticket_screen_tour.js
+++ addons/point_of_sale/static/tests/pos/tours/ticket_screen_tour.js
@@ -357,6 +357,31 @@ registry.category("web_tour.tours").add("OrderTimeTour", {
     },
 });
 
+registry
+    .category("web_tour.tours")
+    .add("test_consistent_refund_process_between_frontend_and_backend", {
+        steps: () =>
+            [
+                Chrome.startPoS(),
+                Dialog.confirm("Open Register"),
+                ProductScreen.addOrderline("Desk Pad", "2", "4"),
+                ProductScreen.clickPayButton(),
+                PaymentScreen.clickPaymentMethod("Bank"),
+                PaymentScreen.clickValidate(),
+                ReceiptScreen.isShown(),
+                ReceiptScreen.clickNextOrder(),
+                ...ProductScreen.clickRefund(),
+                TicketScreen.selectOrder("001"),
+                inLeftSide(Order.hasLine({ productName: "Desk Pad", withClass: ".selected" })),
+                ProductScreen.clickNumpad("1"),
+                TicketScreen.toRefundTextContains("To Refund: 1"),
+                TicketScreen.confirmRefund(),
+                PaymentScreen.clickPaymentMethod("Bank"),
+                PaymentScreen.clickValidate(),
+                ReceiptScreen.isShown(),
+            ].flat(),
+    });
+
 registry.category("web_tour.tours").add("test_paid_order_with_archived_product_loads", {
     steps: () =>
         [
