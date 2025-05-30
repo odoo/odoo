c15ffc29651b95d453c76ca5154a18e5ac3aed67
lase@odoo.com <lase@odoo.com>
2025-05-30 16:13:24 +0200

    [FIX] sale_mrp: spread sol total price amoung kit component
    Issue:
    Confirming a sale order with a line for a kit product will create and
    explode a delivery move for the kit component. However, the computation
    of the sale price of move lines related to the kit component is
    currently based solely on the generic lst_price of the component set on
    its product form rather than dividing the price of the related line
    properly. Since this sale_price is used on deliveries can influence the
    shipping cost of certain carrier e.g:
    https://github.com/odoo/enterprise/blob/32b565161bce4d37222b859ae52275030fd03af0/delivery_dhl/models/dhl_request.py#L322
    it is important to refine the computation of that value in case the sale
    price of the kit product (with taxes) do not coincide with the sum of
    the cost of its components.
    
    Steps to reproduce:
    
    Create a kit product with lst_price 50 eur and a Kit bom:
    10 x COMP (storable product with lst_price: 10 eur)
    Create and confirm an SO for 1 kit product
    > the sale_price of the delivery move line is set to 100 eur.
    
    opw-4798375
    task: 6164381

---
+++ addons/mrp_delivery/__manifest__.py
@@ -0,0 +1,12 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': "Delivery and MRP Management",
+    'category': 'Shipping Connectors',
+    'summary': 'Carrier with kits',
+    'description': "Manage datas of carriers with kit products",
+    'depends': ['mrp', 'stock_delivery'],
+    'auto_install': True,
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
