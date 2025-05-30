1727ed688f995cff5286b1eb0c372d87f894f258
Hesham Saleh <hsal@odoo.com>
2025-05-30 11:08:10 +0000

    [ADD] l10n_pl_taxable_supply_date: Added taxable supply date for Poland
    This new module aims at adding support for taxable supply date in Poland.
    
    task-4829233
    
    closes odoo/odoo#224888
    
    X-original-commit: 1038a22edbb9ad1ea7ffecf532e0ab6592b6dc26
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: Hesham Saleh (hsal) <hsal@odoo.com>

---
+++ addons/l10n_pl_taxable_supply_date/__manifest__.py
@@ -0,0 +1,18 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Poland - Taxable Supply Date',
+    'version': '1.0',
+    'author': 'Odoo S.A.',
+    'category': 'Accounting/Localizations/Account Charts',
+    'description': """
+Extension for Poland - Accounting to add support for taxable supply date
+========================================================================
+    """,
+    'depends': ['l10n_pl'],
+    'auto_install': True,
+    'data': [
+        'views/account_move_views.xml',
+        'views/report_invoice.xml',
+    ],
+    'license': 'LGPL-3',
+}
