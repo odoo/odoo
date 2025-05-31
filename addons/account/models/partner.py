odoo/odoo

commit 0a9c5fcfb9eb1b3532cd67e22b218f255f793091

[IMP] account: modify accounting entries when modifying the parent_id.
- Update accounting entries to reflect the new commercial_partner_id:
* Ensures consistency across account.move.line and partner_id fields.
* Changes are restricted to users in the Accountant group.

task-4707491

closes odoo/odoo#212456

X-original-commit: 38d43360781250415c63aba36eadcdce586fcbc0
Related: odoo/enterprise#86733
Signed-off-by: Quentin De Paoli (qdp) <qdp@odoo.com>

addons/account/models/partner.py
@@ -795,6 +795,25 @@ class ResPartner(models.Model):
             [('partner_id', 'child_of', self.commercial_partner_id.id)]
         )
 
+    def write(self, vals):
+        if 'parent_id' not in vals:
+            return super().write(vals)
+
+        if not self.env.user.has_group('account.group_account_user'):
+            raise UserError(_("You do not have permission to mark this partner as the main commercial partner."))
+        parent_vat = self.env['res.partner'].browse(vals['parent_id']).vat
+        if vals['parent_id'] and {parent_vat} != set(self.mapped('vat')):
+            raise UserError(_("You cannot set a partner as an invoicing address of another if they have a different %(vat_label)s.", vat_label=self.vat_label))
+
+        res = super().write(vals)
+        self._compute_commercial_partner()
+        moves = self.env['account.move'].search([('partner_id', 'in', self.ids)])
+        if moves:
+            for partner in self:
+                moves.filtered(lambda m: m.partner_id == partner)['commercial_partner_id'] = partner.commercial_partner_id.id
+                partner._message_log(body=_("The commercial partner has been updated for all related accounting entries."))
+        return res
+
     @api.model_create_multi
     def create(self, vals_list):
         search_partner_mode = self.env.context.get('res_partner_search_mode')

addons/account/tests/test_account_partner.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from odoo.addons.account.tests.common import AccountTestInvoicingCommon
+from odoo import Command
 from odoo.tests import tagged
+
 from freezegun import freeze_time
 
 
@@ -66,3 +68,31 @@ class TestAccountPartner(AccountTestInvoicingCommon):
         with self.enter_registry_test_mode():
             self.env.cr.postcommit.run()
         self.assertEqual(self.partner_a.customer_rank, 2)
+
+    def test_manually_write_partner_id(self):
+
+        move_vals = {
+            'move_type': 'out_invoice',
+            'invoice_date': '2025-04-29',
+            'partner_id': self.partner_a.id,
+            'invoice_line_ids': [Command.create({
+                'quantity': 1,
+                'price_unit': 500.0,
+                'tax_ids': [],
+            })],
+        }
+
+        move = self.env['account.move'].create(move_vals)
+        move.action_post()
+
+        # Initially, move's commercial partner should be partner_a
+        self.assertEqual(move.commercial_partner_id, self.partner_a)
+
+        self.partner_a['parent_id'] = self.partner_b.id
+
+        # Assert accounting move and move lines now use new commercial partner
+        self.assertEqual(move.commercial_partner_id, self.partner_b)
+        self.assertTrue(
+            all(line.partner_id == self.partner_b for line in move.line_ids),
+            "All move lines should be reassigned to the new commercial partner."
+        )

commit a4776e882e2059acc8ce03a90ecb8577d8c33676

[FIX] account: avoid raising error when it's not needed
commit 0a9c5fcfb9eb1b3532cd67e22b218f255f793091 introduced an automatic way to fix accounting entries when the commercial partner of a res.patrner would change. Only accountants should be doing that, but the error should be raised only if there exist some accounting entries

addons/account/models/partner.py
@@ -787,21 +787,18 @@ class ResPartner(models.Model):
         )
 
     def write(self, vals):
-        if 'parent_id' not in vals:
-            return super().write(vals)
-
-        if not self.env.user.has_group('account.group_account_user'):
-            raise UserError(_("You do not have permission to mark this partner as the main commercial partner."))
-        parent_vat = self.env['res.partner'].browse(vals['parent_id']).vat
-        if vals['parent_id'] and {parent_vat} != set(self.mapped('vat')):
-            raise UserError(_("You cannot set a partner as an invoicing address of another if they have a different %(vat_label)s.", vat_label=self.vat_label))
-
         res = super().write(vals)
-        self._compute_commercial_partner()
-        moves = self.env['account.move'].search([('partner_id', 'in', self.ids)])
-        if moves:
+        moves_sudo = self.sudo().env['account.move'].search([('partner_id', 'in', self.ids)])
+        if moves_sudo and 'parent_id' in vals:
+            if not self.env.user.has_group('account.group_account_user'):
+                raise UserError(_("You do not have permission to mark this partner as the main commercial partner."))
+            parent_vat = self.env['res.partner'].browse(vals['parent_id']).vat
+            if vals['parent_id'] and {parent_vat} != set(self.mapped('vat')):
+                raise UserError(_("You cannot set a partner as an invoicing address of another if they have a different %(vat_label)s.", vat_label=self.vat_label))
+
+            self._compute_commercial_partner()
             for partner in self:
-                moves.filtered(lambda m: m.partner_id == partner)['commercial_partner_id'] = partner.commercial_partner_id.id
+                moves_sudo.filtered(lambda m: m.partner_id == partner)['commercial_partner_id'] = partner.commercial_partner_id.id
                 partner._message_log(body=_("The commercial partner has been updated for all related accounting entries."))
         return res
 