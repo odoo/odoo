bffd95b27c8d2b4a76c59fc4303d1add788e06f1
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2025-06-02 14:41:24 +0200

    [FIX] purchase: convert bill amount when adding downpayment
    Users can create a downpayment bill directly from the purchase order
    or create a bill separately and then associate it with the original
    purchase order.
    However, in the latter case, the bill amount won't take into account
    the currency.
    
    Steps to reproduce:
    - Create a purchase order for a partner in company currency
    - Create a bill in foreign currency for the same partner
    - Click "Purchase Matching" smart button
    - Select PO and BILL > Add to PO > Add Down Payment
    
    Issue: Downpayment will be added into the PO without taking into account
    the different currency
    
    opw-4716949

--- addons/purchase/wizard/bill_to_po_wizard.py
+++ addons/purchase/wizard/bill_to_po_wizard.py
@@ -48,13 +48,16 @@ class BillToPO(models.TransientModel):
             self.purchase_order_id = self.env['purchase.order'].create({
                 'partner_id': lines_to_convert.partner_id.id,
             })
+        po_currency = self.purchase_order_id.currency_id
+        company = self.purchase_order_id.company_id
+        date = self.purchase_order_id.date_order or fields.Date.today()
         line_vals = [
             {
                 'name': _("Down Payment (ref: %(ref)s)", ref=aml.display_name),
                 'product_qty': 0.0,
                 'product_uom': aml.product_uom_id.id,
                 'is_downpayment': True,
-                'price_unit': aml.price_unit,
+                'price_unit': aml.currency_id._convert(aml.price_unit, po_currency, company, date) if aml.currency_id != po_currency else aml.price_unit,
                 'taxes_id': aml.tax_ids,
                 'order_id': self.purchase_order_id.id,
             }
