6df05f2b003cb52d44060c202521c8421c34dbb7
Lucas Perais (lpe) <lpe@odoo.com>
2025-06-02 08:50:05 +0000

    [FIX] im_livechat: underminism 108129: community wait mounted webclient
    runbot-error-108129
    
    closes odoo/odoo#212631
    
    X-original-commit: f414a2efdf06e376d1723b3abcb2fd45e1ff6ff3
    Signed-off-by: Pierre Pulinckx (pipu) <pipu@odoo.com>
    Signed-off-by: Lucas Perais (lpe) <lpe@odoo.com>

--- addons/im_livechat/static/tests/tours/im_livechat_history_back_and_forth.js
+++ addons/im_livechat/static/tests/tours/im_livechat_history_back_and_forth.js
@@ -12,7 +12,7 @@ registry.category("web_tour.tours").add("im_livechat_history_back_and_forth_tour
         {
             isActive: ["community"],
             content: "open command palette",
-            trigger: "body",
+            trigger: "body:has(.o_action_manager)",
             run: "press ctrl+k",
         },
         {
