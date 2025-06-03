0c35b9b794eb3eef533d4c4b40a08ff9fb73c48f
Brieuc-brd <brd@odoo.com>
2025-06-03 16:01:12 +0200

    [IMP] website, *: s_dynamic_category, add options
    * website_sale
    
    This commit improves the new `s_dynamic_category` snippet by adding new
    options:
    
    - Add "Spacing" option to control the gap between blocks.
    - Add Shadow and Rounded corners options for all blocks.
    - Add an option to switch to a "Clickable cards" layout.
    
    task-4805239

--- addons/website_sale/static/src/website_builder/dynamic_snippet_category_options.js
+++ addons/website_sale/static/src/website_builder/dynamic_snippet_category_options.js
@@ -1,10 +1,14 @@
 import { BaseOptionComponent } from "@html_builder/core/utils";
 import { onWillStart, useState } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
+import { ShadowOption } from "@html_builder/plugins/shadow_option";
 
 
 export class DynamicSnippetCategoryOption extends BaseOptionComponent {
     static template = "website_sale.DynamicSnippetCategoryOptions";
+    static components = {
+        ShadowOption,
+    };
 
     setup() {
         super.setup();
