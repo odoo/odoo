odoo/odoo

commit 5d69408548b30671b968c3c1ae1e8f444ac2d82e

[IMP] l10n_tr_nilvera_einvoice: generic einvoice fetch/notification
We start accumulating buttons for fetching electronic invoices and their
statuses with different localisations. In addition to peppol, there
l10n_tk and soon l10n_dk (with Nemhandel:
https://github.com/odoo/odoo/pull/190474) which also needs such a button

In addition to these buttons, we also need a standard way of notifying
users when receiving einvoices for these different modules

This commit adds overrides the generic einvoices (status) fetch button
methods for Türkiye localisation as well as hooking into the generic
function to send email upon einvoices reception.

related prs:
- odoo/upgrade#7205
- odoo/enterprise#80111

task-4457146

closes odoo/odoo#197233

Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

addons/l10n_tr_nilvera_einvoice/models/account_journal.py
@@ -1,14 +1,39 @@
-from odoo import models
+from odoo import models, api
 
 
 class AccountJournal(models.Model):
     _inherit = 'account.journal'
 
-    def l10n_tr_nilvera_get_documents(self):
+    @api.depends('is_nilvera_journal', 'l10n_tr_nilvera_api_key', 'type')
+    def _compute_show_fetch_in_einvoices_button(self):
+        # EXTENDS 'account'
+        super()._compute_show_fetch_in_einvoices_button()
+        self.filtered(
+            lambda j: j.is_nilvera_journal and
+                      j.l10n_tr_nilvera_api_key and
+                      j.l10n_tr_nilvera_api_key.raw_value and
+                      j.type == 'purchase'
+        ).show_fetch_in_einvoices_button = True
+
+    @api.depends('l10n_tr_nilvera_api_key', 'type')
+    def _compute_show_refresh_out_einvoices_status_button(self):
+        # EXTENDS 'account'
+        super()._compute_show_refresh_out_einvoices_status_button()
+        self.filtered(
+            lambda j: j.l10n_tr_nilvera_api_key and
+                      j.l10n_tr_nilvera_api_key.raw_value and
+                      j.type == 'sale'
+        ).show_refresh_out_einvoices_status_button = True
+
+    def button_fetch_in_einvoices(self):
+        # EXTENDS 'account'
+        super().button_fetch_in_einvoices()
         self.env['account.move']._l10n_tr_nilvera_get_documents()
 
-    def l10n_tr_nilvera_get_message_status(self):
+    def button_refresh_out_einvoices_status(self):
+        # EXTENDS 'account'
         """ Gets the status from Nilvera for all processing invoices in this journal. """
+        super().button_refresh_out_einvoices_status()
         invoices_to_update = self.env['account.move'].search([
             ('journal_id', '=', self.id),
             ('l10n_tr_nilvera_send_status', 'in', ['waiting', 'sent']),

addons/l10n_tr_nilvera_einvoice/__manifest__.py
@@ -8,7 +8,6 @@ For sending and receiving electronic invoices to Nilvera.
     'depends': ['l10n_tr_nilvera', 'account_edi_ubl_cii'],
     'data': [
         'data/cron.xml',
-        'views/account_journal_dashboard_views.xml',
         'views/account_move_views.xml',
     ],
     'auto_install': ['l10n_tr_nilvera'],

addons/l10n_tr_nilvera_einvoice/models/account_move.py
@@ -172,13 +172,16 @@ class AccountMove(models.Model):
                 groupby=['l10n_tr_nilvera_uuid'],
                 aggregates=['__count'],
             ))
+            moves = self.env['account.move']
             for document_uuid in document_uuids:
                 # Skip invoices that have already been downloaded.
                 if document_uuid in document_uuids_count:
                     continue
                 move = self._l10n_tr_nilvera_get_invoice_from_uuid(client, journal, document_uuid)
                 self._l10n_tr_nilvera_add_pdf_to_invoice(client, move, document_uuid)
+                moves |= move
                 self._cr.commit()
+            journal._notify_einvoices_received(moves)
 
     def _l10n_tr_nilvera_get_invoice_from_uuid(self, client, journal, document_uuid):
         response = client.request(

addons/l10n_tr_nilvera_einvoice/views/account_journal_dashboard_views.xml
@@ -1,31 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<odoo>
-    <record id="account_journal_dashboard_kanban_view" model="ir.ui.view">
-        <field name="name">account.journal.dashboard.kanban</field>
-        <field name="model">account.journal</field>
-        <field name="inherit_id" ref="account.account_journal_dashboard_kanban_view" />
-        <field name="arch" type="xml">
-            <data>
-                <xpath expr="//kanban" position="inside">
-                    <field name="is_nilvera_journal" invisible="1"/>
-                    <field name="l10n_tr_nilvera_api_key" invisible="1"/>
-                </xpath>
-
-                <xpath expr="//t[@id='account.JournalBodySalePurchase']//div" position="inside">
-                    <t t-if="record.l10n_tr_nilvera_api_key and record.l10n_tr_nilvera_api_key.raw_value">
-                        <t t-if="journal_type == 'sale'">
-                            <div class="w-100">
-                                <a type="object" name="l10n_tr_nilvera_get_message_status" groups="account.group_account_invoice">Fetch Nilvera invoice status</a>
-                            </div>
-                        </t>
-                        <t t-elif="journal_type == 'purchase' and record.is_nilvera_journal.raw_value">
-                            <div class="w-100">
-                                <a type="object" name="l10n_tr_nilvera_get_documents" groups="account.group_account_invoice">Fetch from Nilvera</a>
-                            </div>
-                        </t>
-                    </t>
-                </xpath>
-            </data>
-        </field>
-    </record>
-</odoo>

commit ec9bedaff222526308e0d500f462541e6bb1b99e

[FIX] l10n_tr_nilvera_einvoice: traceback after EDI fetch refactor
5d69408548b30671b968c3c1ae1e8f444ac2d82e introduced overrides for the generic EDI fetch/update logic.
It mistakenly copied a condition from the XML layer that checked
`.raw_value` on the API key field. This caused a traceback when
accessing the accounting dashboard.

No task-ID

addons/l10n_tr_nilvera_einvoice/models/account_journal.py
@@ -11,7 +11,6 @@ class AccountJournal(models.Model):
         self.filtered(
             lambda j: j.is_nilvera_journal and
                       j.l10n_tr_nilvera_api_key and
-                      j.l10n_tr_nilvera_api_key.raw_value and
                       j.type == 'purchase'
         ).show_fetch_in_einvoices_button = True
 
@@ -21,7 +20,6 @@ class AccountJournal(models.Model):
         super()._compute_show_refresh_out_einvoices_status_button()
         self.filtered(
             lambda j: j.l10n_tr_nilvera_api_key and
-                      j.l10n_tr_nilvera_api_key.raw_value and
                       j.type == 'sale'
         ).show_refresh_out_einvoices_status_button = True
 