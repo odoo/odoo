a14b1e74732c0b3566e10e60599964c6a764d2e6
David (dafr) <dafr@odoo.com>
2025-06-03 09:39:27 +0000

    [FIX] sale_mrp: Propagate dead_deadline change to byproducts
    ## Issue description:
    - When 'qty_producing' is updated, the new move takes the MO date_deadline.
    - The MO date_deadline is the minimum date of all the finished moves date_deadline.
    - When the SO commitment_date is incremented (+1day), the FNS move date_deadline is updated, but not the BP move one.
    - The MO date_deadline stay the same (because BP move is a finished move, and BP.date_deadline < FNS.date_deadline)
    - The new FNS move deadline will not be equal to the existing FNS move deadline
    - The merge is blocked
    
    ## How to reproduce:
    - Enable By-Products in setting
    - Unarchive MTO route
    - Create storable products FNS, CMP and BP
    - Set FNS route: Manufacture and MTO
    - Create BoM:
      - Producing: 1 of FNS
      - Component: 1 of CMP
      - By-Product: 1 of BP
    - Create Sale Order for 1 unit of FNS ⇾ Confirm
    - Set Delivery Date to 1 day in the future (date must be incremented)
    - Go to MO, set quantity producing to 2
      => 2 Finished move for FNS exists; the merge was not done due to a discrepancy in 'date_deadline'
    - Click "Produce All"
      !! Singleton Error
    
    ## Fix:
    On date_deadline change: propagate deadline to sibling FNS moves (byproducts)
    
    OPW-4671555
    
    X-original-commit: f7c78fdc047e0d27659b7c39cf70cbd6ac7e465f

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -542,13 +542,17 @@ Please change the quantity done or the rounding precision in your settings.""",
                 for move in mvs:
                     move.forecast_availability, move.forecast_expected_date = forecast_info[move]
 
+    def _get_moves_to_propagate_date_deadline(self):
+        self.ensure_one()
+        return self.move_dest_ids | self.move_orig_ids
+
     def _set_date_deadline(self, new_deadline):
         # Handle the propagation of `date_deadline` fields (up and down stream - only update by up/downstream documents)
         already_propagate_ids = self.env.context.get('date_deadline_propagate_ids', set())
         already_propagate_ids.update(self.ids)
         self = self.with_context(date_deadline_propagate_ids=already_propagate_ids)
         for move in self:
-            moves_to_update = (move.move_dest_ids | move.move_orig_ids)
+            moves_to_update = move._get_moves_to_propagate_date_deadline()
             if move.date_deadline:
                 delta = move.date_deadline - fields.Datetime.to_datetime(new_deadline)
             else:
