cd2334bc9589627cf05db5b84218f42d46fe5a2e
amira tilouche <ati@visto-consulting.com>
2025-06-03 11:08:10 +0100

    feat(pos):  Hide Orders and Cash In/Out buttons for cashier role in Navbar

--- modules/custom_module/static/src/js/pos_navbar_inherit.js
+++ modules/custom_module/static/src/js/pos_navbar_inherit.js
@@ -1,20 +1,18 @@
-/** @odoo-module */
-
 import { Navbar } from "@point_of_sale/app/navbar/navbar";
 import { patch } from "@web/core/utils/patch";
-import { usePos } from "@point_of_sale/app/store/pos_hook";
 
 patch(Navbar.prototype, {
-
-    setup() {
-        console.log('okayyyyyyyy');
-        super.setup();
-        this.pos = usePos();
+    
+    get showOrdersButton() {
+        const cashier = this.pos.get_cashier();
+        const userRole = cashier?._role;
+        return userRole !== 'cashier';
     },
-    async showLoginScreen() {
-        console.log('here');
-        this.pos.showScreen("FloorScreen");
-        this.pos.reset_cashier();
-        await this.pos.showTempScreen("LoginScreen");
-    },
+    
+    get showCashInOutButton() {
+        const cashier = this.pos.get_cashier();
+        const userRole = cashier?._role;
+        return userRole !== 'cashier';
+    }
+    
 });
