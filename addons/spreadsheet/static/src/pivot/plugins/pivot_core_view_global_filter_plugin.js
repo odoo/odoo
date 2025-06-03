a641c2df11d4501bd324f48e26d836a4ff4725d2
Pierre Rousseau <pro@odoo.com>
2025-06-03 07:43:56 +0000

    [FIX] spreadsheet: correctly compute month/quarter value for filters
    Steps to reproduce:
    - Create a dashboard with a pivot table with a date field and a global
    filter on it. Ensure that the date field is set to "Month" in the
    pivot table and at least a value for the month of December is present.
    - Open the dashboard
    - Click on the "December" value in the pivot
    => Only the year is selected in the global filter, not the month.
    
    Same thing happens when the date field is set to quarter and the value
    Q4 is selected.
    
    closes odoo/odoo#213076
    
    Task: 4844417
    X-original-commit: b4407e8ed11c9b2d6ea890b8be3b600c95999657
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>

--- addons/spreadsheet/static/src/pivot/plugins/pivot_core_view_global_filter_plugin.js
+++ addons/spreadsheet/static/src/pivot/plugins/pivot_core_view_global_filter_plugin.js
@@ -42,23 +42,23 @@ function pivotPeriodToFilterValue(timeRange, value) {
                 yearOffset,
             };
         case "month": {
-            const month = value.includes("/") ? Number.parseInt(value.split("/")[0]) : -1;
+            const month = value.includes("/") ? Number.parseInt(value.split("/")[0]) - 1 : -1;
             if (!(month in monthsOptions)) {
                 return { yearOffset, period: undefined };
             }
             return {
                 yearOffset,
-                period: monthsOptions[month - 1].id,
+                period: monthsOptions[month].id,
             };
         }
         case "quarter": {
-            const quarter = value.includes("/") ? Number.parseInt(value.split("/")[0]) : -1;
+            const quarter = value.includes("/") ? Number.parseInt(value.split("/")[0]) - 1 : -1;
             if (!(quarter in FILTER_DATE_OPTION.quarter)) {
                 return { yearOffset, period: undefined };
             }
             return {
                 yearOffset,
-                period: FILTER_DATE_OPTION.quarter[quarter - 1],
+                period: FILTER_DATE_OPTION.quarter[quarter],
             };
         }
     }
