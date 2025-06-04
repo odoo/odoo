08ba8faa735c2a4174054b0a333503bac55d47a0
roen-odoo <roen@odoo.com>
2025-06-04 15:28:25 +0200

    [FIX] pos_loyalty: fix buy X get Y
    When activating a loyalty program with a buy X get Y promotion. For
    example for buy 10 get 3, if you have 10 products in your cart and add 1
    more product, it will add a reward for 1 free product. But if you add
    another product, the reward will stay the same, it will not give 2 free
    product.
    
    Steps to reproduce:
    -------------------
    * Create a buy X get Y loyalty program (10 get 3 free) where X and Y are
      the same product.
    * Add 10 products to the cart.
    * Add 1 more product to the cart, it will add a reward for 1 free
      product.
    * Add another product to the cart
    > Observation: The reward will not change, it will still be 1 free
      product.
    
    Why the fix:
    ------------
    If possible we will try to maximize the reward quantity. That's why we
    don't do it in cases where multiple rewards are possible.
    
    opw-4762083
    
    closes odoo/odoo#211087
    
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/pos_loyalty/tests/test_frontend.py
+++ addons/pos_loyalty/tests/test_frontend.py
@@ -2780,3 +2780,29 @@ class TestUi(TestPointOfSaleHttpCommon):
         self.assertEqual(len(gift_card_program.coupon_ids), 1, "Gift card not generated")
         self.assertEqual(gift_card_program.coupon_ids[0].code, "test-card-1234", "Gift card code not correct")
         self.assertEqual(gift_card_program.coupon_ids[0].partner_id, partner, "Gift card partner id not correct")
+
+    def test_buy_x_get_y_reward_qty(self):
+        self.env['loyalty.program'].search([]).write({'active': False})
+        self.loyalty_program = self.env['loyalty.program'].create({
+            'name': 'Buy 10 whiteboard_pen, Take 3 whiteboard_pen',
+            'program_type': 'buy_x_get_y',
+            'applies_on': 'current',
+            'trigger': 'auto',
+            'rule_ids': [(0, 0, {
+                'product_ids': self.whiteboard_pen.ids,
+                'reward_point_mode': 'unit',
+                'minimum_qty': 10,
+                'reward_point_amount': 1,
+            })],
+            'reward_ids': [(0, 0, {
+                'reward_type': 'product',
+                'reward_product_id': self.whiteboard_pen.id,
+                'reward_product_qty': 3,
+                'required_points': 10,
+            })],
+        })
+        self.start_tour(
+            "/pos/web?config_id=%d" % self.main_pos_config.id,
+            "test_buy_x_get_y_reward_qty",
+            login="pos_user"
+        )
