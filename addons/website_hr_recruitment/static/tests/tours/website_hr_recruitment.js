8d9507d8614b93db80fe9c73092810d262a38226
rusp-odoo <rusp@odoo.com>
2025-06-04 13:05:55 +0530

    [IMP] website: remove banner for required form action fields
    In the form builder, when a field is required due to a selected form
    action (e.g., `email_from` for the "Send an E-mail" action), a banner
    was shown in the Field section of the options panel explaining that the
    field could not be removed.
    
    Steps to see the banner:
    1. Drop a form on the website.
    2. Set the action to "Send an E-mail".
    3. Click on the "Your Email" field.
    4. A banner appears in the options panel.
    
    This commit removes that banner to keep the UI cleaner and more focused
    on edition. Additionally, it improves the tooltip of disabled remove
    button by adding the name of the required field for the selected action.
    
    task-4846055

--- addons/website_hr_recruitment/static/tests/tours/website_hr_recruitment.js
+++ addons/website_hr_recruitment/static/tests/tours/website_hr_recruitment.js
@@ -131,29 +131,3 @@ registerWebsitePreviewTour('website_hr_recruitment_tour_edit_form', {
     trigger: ":iframe form input[name=job_id]:not(:visible):not([value='']):not([value=FAKE_JOB_ID_DEFAULT_VAL])",
 },
 ]);
-
-// This tour addresses an issue that occurred in a website form containing
-// the 'hide-change-model' attribute. Specifically, when a model-required
-// field is selected, the alert message should not display an undefined
-// action name.
-registerWebsitePreviewTour('model_required_field_should_have_action_name', {
-    url: '/jobs',
-}, () => [{
-    content: "Select Job",
-    trigger: ":iframe h3:contains('Guru')",
-    run: "click",
-}, {
-    content: "Apply",
-    trigger: ":iframe a:contains('Apply')",
-    run: "click",
-},
-...clickOnEditAndWaitEditMode(),
-{
-    content: "click on the your name field",
-    trigger: ":iframe #hr_recruitment_form div.s_website_form_model_required",
-    run: "click",
-}, {
-    content: "Select model-required field",
-    trigger: ".options-container .alert > span:not(:contains(undefined))",
-}
-]);
