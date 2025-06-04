e47365384c7510ef0d36d76314b3c613e2453834
Jinjiu Liu <jili@odoo.com>
2025-06-04 11:38:37 +0000

    [FIX] html_editor: close the link popover for image link after deleting
    Before this commit:
    1. Insert an image and add a link to it.
    2. Click on the image and remove it by clicking the remove button in the toolbar.
    3. The link popover remains open and only disappears after clicking away.
    
    This occurs because the default behavior of a link with text preserves the link
    element to allow the possibility of completely changing the label of the link
    directly in the editing area.
    
    After this commit: We made a override in link_plugin, to handle the delete of an
    image when its parent element is a link, e.g. image link. The link popover is
    properly closed when an image link is removed, and the link element is removed
    when the link element associated with the image is empty.
    
    We also improve the media modify handler, move it partially to normalize,
    ref: https://github.com/odoo/odoo/pull/211401
    
    From the ref pr, we unwrap the link in other media plugins when unsupported
    media type is inserted to a link by replacing the image. In this commit, we
    move that logic to the link plugin, and do it in the normalize step. By doing
    this, the dispatching in the ref pr won't be needed.
    
    The unwrapping condition is when a link has another link inside it (media type
    document), or when the link has an inframe inside (video media type, possible
    in new html_builder website). It's possible that a link has other type of child
    like img element, font element (background color), span element (icon), t
    element (template), textnode etc. It's easier to specify in which cases we want
    to unwrap a link.
    
    task-4805029
    
    X-original-commit: 02ef4fb436d19da979845075623f894d199518c7

--- addons/html_editor/static/tests/media.test.js
+++ addons/html_editor/static/tests/media.test.js
@@ -35,6 +35,36 @@ test("Can replace an image", async () => {
     expect("img[src='/web/static/img/logo2.png']").toHaveCount(1);
 });
 
+test("Replace an image with link by a document should remove the link", async () => {
+    onRpc("/web/dataset/call_kw/ir.attachment/search_read", () => [
+        {
+            id: 1,
+            name: "file.txt",
+            mimetype: "text/plain",
+            public: true,
+            image_src: "",
+        },
+    ]);
+    const env = await makeMockEnv();
+    await setupEditor(
+        `<p><a href="http://test.com"><img class="img-fluid" src="/web/static/img/logo.png"></a></p>`,
+        { env }
+    );
+    expect("img[src='/web/static/img/logo.png']").toHaveCount(1);
+    await click("img");
+    await tick(); // selectionchange
+    await waitFor(".o-we-toolbar");
+    expect("button[name='replace_image']").toHaveCount(1);
+    await click("button[name='replace_image']");
+    await animationFrame();
+    await click(".nav-link:contains('Documents')");
+    await animationFrame();
+    await click(".o_we_attachment_highlight");
+    expect(".odoo-editor-editable .o_file_box a:contains('file.txt')").toHaveCount(1);
+    expect("img[src='/web/static/img/logo.png']").toHaveCount(0);
+    expect("p a[href='http://test.com']").toHaveCount(0);
+});
+
 test.tags("focus required");
 test("Selection is collapsed after the image after replacing it", async () => {
     onRpc("ir.attachment", "search_read", () => [
