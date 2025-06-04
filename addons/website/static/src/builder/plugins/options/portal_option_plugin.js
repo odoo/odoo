e8595260a579991105fde704bbb2ccb099f1d992
assk-odoo <assk@odoo.com>
2025-06-04 10:23:21 +0530

    [IMP] portal: editing portal card using website editor
    create a new model `portal.entry` to manage customization portal cards.
    Users can now edit card content (text, image) and styling (background color,
    border, border-radius) directly from the website editor, improving flexibility
    and user experience.
    
    Enterprise Pr: https://github.com/odoo/enterprise/pull/87960
    
    task-3894113

---
+++ addons/website/static/src/builder/plugins/options/portal_option_plugin.js
@@ -0,0 +1,53 @@
+import { Plugin } from "@html_editor/plugin";
+import { registry } from "@web/core/registry";
+import { withSequence } from "@html_editor/utils/resource";
+import { StyleAction } from "@html_builder/core/core_builder_action_plugin";
+
+class PortalOptionPlugin extends Plugin {
+    static id = "PortalOption";
+    static dependencies = ["customizeWebsite"];
+
+    resources = {
+        builder_options: [
+            withSequence(1, {
+                template: "website.PortalOption",
+                selector: ".o_portal",
+            }),
+        ],
+    builder_actions: { 
+        StyleActionPortalAction 
+        },
+    };
+} 
+
+class StyleActionPortalAction extends StyleAction {
+    static id = "styleActionPortal";
+    static dependencies = ["customizeWebsite", "color"];
+    setup() {
+        this.preview = false;
+        this.dependencies.customizeWebsite.withCustomHistory(this);
+    }
+
+    async apply({ params, value }) {
+        const styleName = params.mainParam;
+
+        if (styleName === "border-color") {
+            return this.dependencies.customizeWebsite.customizeWebsiteColors({
+                "menu-border-card-color": value,
+            });
+        }
+
+        const variableMap = {
+            "border-style": "menu-border-card-style",
+            "border-radius": "menu-border-card-radius",
+            "border-width": "menu-border-card-width",
+        };
+
+        if (styleName in variableMap) {
+            return this.dependencies.customizeWebsite.customizeWebsiteVariables({
+                [variableMap[styleName]]: value,
+            });
+        }
+    }   
+}
+registry.category("website-plugins").add(PortalOptionPlugin.id, PortalOptionPlugin);
