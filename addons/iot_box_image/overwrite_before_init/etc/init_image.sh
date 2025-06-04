6a286e3209849483efff15a1ed0b016496e1bb20
Louis Travaux <lotr@odoo.com>
2025-06-04 09:01:12 +0000

    [IMP] iot_box_image: ssh certificate authority instead of standard keypair
    We recently implemented a keypair generation to connect to the IoT Box using SSH.
    This required users to have the generated private key to connect to the IoT Box.
    
    We now generate a certificate authority keypair to allow signing users' public keys
    in order to grant them access for a specific amount of time, without providing them the IoT Box private key.
    
    closes odoo/odoo#213033
    
    X-original-commit: cf3b9857aeaf2c7ac895376106821ebb1fc9e695
    Signed-off-by: Yaroslav Soroko (yaso) <yaso@odoo.com>
    Signed-off-by: Louis Travaux (lotr) <lotr@odoo.com>

--- addons/iot_box_image/overwrite_before_init/etc/init_image.sh
+++ addons/iot_box_image/overwrite_before_init/etc/init_image.sh
@@ -156,6 +156,7 @@ apt-get update
 # This will be modified by a unique password on the first start of Odoo
 password="$(openssl rand -base64 12)"
 echo "pi:${password}" | chpasswd
+echo TrustedUserCAKeys /etc/ssh/ca.pub >> /etc/ssh/sshd_config
 
 # Prevent Wi-Fi blocking
 apt-get -y remove rfkill
