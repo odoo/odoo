336c29d7e3caa859b8005bb9fd3c4fdc4720ad81
Alexandre Kühn <aku@odoo.com>
2025-06-05 10:46:15 +0000

    [FIX] mail: remove debug statements from #212793
    X-original-commit: 3358181fea8434b9bc0cdb5a4f189e6cb1f96d18

--- addons/mail/static/src/core/public_web/discuss.js
+++ addons/mail/static/src/core/public_web/discuss.js
@@ -37,7 +37,6 @@ export class Discuss extends Component {
 
     setup() {
         super.setup();
-        window.aku = this;
         this.store = useService("mail.store");
         this.messageHighlight = useMessageHighlight();
         this.contentRef = useRef("content");
