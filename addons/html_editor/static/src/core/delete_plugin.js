7820aec917731eb03357d9586f10bdb8a099549c
Walid (wasa) <wasa@odoo.com>
2025-06-05 13:16:49 +0000

    [FIX] html_editor: support macOS deletion shortcuts using Option key
    Problem:
    On macOS, pressing Option (Alt) + Backspace does not delete the
    previous word as expected. Instead, only a single character is
    removed.
    
    Solution:
    The Alt+Backspace and Cmd+Backspace hotkeys must be hooked and
    reimplemented because the default behavior of the browser might
    remove or merge nodes that it should not. However, the hotkey
    service ignores the `Alt` key on mac because hotkeys bound to it
    would shadow combinations required to input some special
    characters. In addition, the Cmd key on mac is matched to Ctrl in
    the hotkey service, but the action bound to Ctrl+Backspace is not
    the same one that is expected for Cmd+Backspace on macOS.
    Therefore, a dedicated `keydown` listener, bypassing the hokey
    service, is needed for this edge case.
    
    Steps to reproduce:
    - On macOS, enter a two-word line in the editor.
    - Press Option (Alt) + Backspace.
    - Only the last character is deleted, instead of the last word.
    
    opw-4781484
    
    closes odoo/odoo#214745
    
    X-original-commit: 938b772ab60501ac7028da0edf2f66a758004363
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Walid Sahli (wasa) <wasa@odoo.com>

--- addons/html_editor/static/src/core/delete_plugin.js
+++ addons/html_editor/static/src/core/delete_plugin.js
@@ -43,7 +43,7 @@ import {
 import { CTYPES } from "../utils/content_types";
 import { withSequence } from "@html_editor/utils/resource";
 import { compareListTypes } from "@html_editor/main/list/utils";
-import { hasTouch, isBrowserChrome } from "@web/core/browser/feature_detection";
+import { hasTouch, isBrowserChrome, isMacOS } from "@web/core/browser/feature_detection";
 
 /**
  * @typedef {Object} RangeLike
@@ -63,7 +63,7 @@ import { hasTouch, isBrowserChrome } from "@web/core/browser/feature_detection";
  */
 
 export class DeletePlugin extends Plugin {
-    static dependencies = ["baseContainer", "selection", "history", "input"];
+    static dependencies = ["baseContainer", "selection", "history", "input", "userCommand"];
     static id = "delete";
     static shared = ["deleteBackward", "deleteForward", "deleteRange", "deleteSelection", "delete"];
     resources = {
@@ -110,6 +110,36 @@ export class DeletePlugin extends Plugin {
     setup() {
         this.findPreviousPosition = this.makeFindPositionFn("backward");
         this.findNextPosition = this.makeFindPositionFn("forward");
+        if (isMacOS()) {
+            // Bypass the hotkey service for Alt+Backspace and Cmd+Backspace
+            // on macOS which would otherwise conflict with other shortcuts.
+            this.addDomListener(this.editable, "keydown", (event) => {
+                const runCommand = (commandId) => {
+                    this.dependencies.userCommand.getCommand(commandId).run();
+                    event.stopImmediatePropagation();
+                    event.preventDefault();
+                };
+                // Delete word backward: Option + Backspace
+                if (event.altKey && event.key === "Backspace") {
+                    return runCommand("deleteBackwardWord");
+                }
+
+                // Delete word forward: Option + Delete
+                if (event.altKey && event.key === "Delete") {
+                    return runCommand("deleteForwardWord");
+                }
+
+                // Delete line backward: Command + Backspace
+                if (event.metaKey && event.key === "Backspace") {
+                    return runCommand("deleteBackwardLine");
+                }
+
+                // Delete line forward: Command + Delete
+                if (event.metaKey && event.key === "Delete") {
+                    return runCommand("deleteForwardLine");
+                }
+            });
+        }
     }
 
     // --------------------------------------------------------------------------
