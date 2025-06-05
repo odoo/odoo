8f40569d0dd798824f4554a6397788de87513f8c
Rémi Rahir (rar) <rar@odoo.com>
2025-06-05 20:45:09 +0000

    [FIX] spreadsheet: Fix groups of week that spread over two years
    When grouping by week/days in a pivot, some issues can be seen for dates
    which week is between two years (for instance 12/30/2024 is both part of
    the last week of 2024 and the first week of 2025). Currently, such a
    date is assigned to week 1 of 2024 in a spreadsheet pivot, which is
    false and also messes up the group by of the first week of 2024.
    
    Technically, it's an issue with the date formatting "WW/YYYY" because
    the two parts are decorellated.
    Following the ISO calendar rules, the year of the date is obviously
    2024 but the week of 12/30/2024 is 1 only because we consider it part
    of 2025. Since both parts of the information are gathered independently,
    we end up with this misleading formatted value.
    
    Note: the bug was fixed following a refactoring of the date handling but
    we keep the tests to ensure that there are no future regressions.
    
    Task-4853825
    
    closes odoo/odoo#214971
    
    X-original-commit: ae9215b411a53f4231a53872f4bb23c850775d84
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- addons/spreadsheet/static/tests/pivots/model/pivot_plugin.test.js
+++ addons/spreadsheet/static/tests/pivots/model/pivot_plugin.test.js
@@ -2106,3 +2106,115 @@ test("can group by property", async () => {
     expect(getEvaluatedCell(model, "A1").value).toBe("hello");
     expect(getEvaluatedCell(model, "A2").value).toBe(1);
 });
+
+test("date are between two years are correctly grouped by weeks", async () => {
+    const serverData = getBasicServerData();
+    serverData.models.partner.records = [
+        {
+            active: true,
+            id: 5,
+            foo: 11,
+            bar: true,
+            product_id: 37,
+            date: "2024-01-03",
+        },
+        {
+            active: true,
+            id: 6,
+            foo: 12,
+            bar: true,
+            product_id: 41,
+            date: "2024-12-30",
+        },
+        {
+            active: true,
+            id: 7,
+            foo: 13,
+            bar: true,
+            product_id: 37,
+            date: "2024-12-31",
+        },
+        {
+            active: true,
+            id: 8,
+            foo: 14,
+            bar: true,
+            product_id: 37,
+            date: "2025-01-01",
+        },
+    ];
+    const { model } = await createSpreadsheetWithPivot({
+        serverData,
+        arch: /*xml*/ `
+            <pivot string="Partners">
+                <field name="date:year" type="col"/>
+                <field name="date:week" type="col"/>
+                <field name="foo" type="measure"/>
+            </pivot>`,
+    });
+
+    // prettier-ignore
+    expect(getFormattedValueGrid(model, "B1:D4")).toEqual({
+            B1: "2024",     C1: "",         D1: "2025",
+            B2: "W1 2024",  C2: "W1 2025",  D2: "W1 2025",
+            B3: "Foo",      C3: "Foo",      D3: "Foo",
+            B4: "11",       C4: "25",       D4: "14",
+        });
+});
+
+test("date are between two years are correctly grouped by weeks and days", async () => {
+    const serverData = getBasicServerData();
+    serverData.models.partner.records = [
+        {
+            active: true,
+            id: 5,
+            foo: 11,
+            bar: true,
+            product_id: 37,
+            date: "2024-01-03",
+        },
+        {
+            active: true,
+            id: 6,
+            foo: 12,
+            bar: true,
+            product_id: 41,
+            date: "2024-12-30",
+        },
+        {
+            active: true,
+            id: 7,
+            foo: 13,
+            bar: true,
+            product_id: 37,
+            date: "2024-12-31",
+        },
+        {
+            active: true,
+            id: 8,
+            foo: 14,
+            bar: true,
+            product_id: 37,
+            date: "2025-01-01",
+        },
+    ];
+    const { model } = await createSpreadsheetWithPivot({
+        serverData,
+        arch: /*xml*/ `
+            <pivot string="Partners">
+                <field name="date:year" type="col"/>
+                <field name="date:week" type="col"/>
+                <field name="date:day" type="col"/>
+                <field name="foo" type="measure"/>
+            </pivot>`,
+    });
+
+    // prettier-ignore
+    expect(getFormattedValueGrid(model, "B1:E5")).toEqual({
+            B1: "2024",         C1: "",             D1: "",            E1: "2025",
+            B2: "W1 2024",      C2: "W1 2025",      D2: "",            E2: "W1 2025",
+            B3: "03 Jan 2024",  C3: "30 Dec 2024",  D3: "31 Dec 2024", E3: "01 Jan 2025",
+            B4: "Foo",          C4: "Foo",          D4: "Foo",         E4: "Foo",
+            B5: "11",           C5: "12",           D5: "13",          E5: "14",
+        })
+});
