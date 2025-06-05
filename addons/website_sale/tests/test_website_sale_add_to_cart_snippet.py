d7b9038b2d16f452d49a9a8c6f178a5bad637eb4
Omar (OSAH) <osah@odoo.com>
2025-06-05 13:02:00 +0000

    [FIX] website_sale: add missing phone number
    the tour was failing because the phone number was not set in the
    website configuration, which is required for the add to cart snippet tour to pass
    because it checks the phone number in the billing address form page
    which is required to be filled in order to proceed with the tour
    and confirm order is on the next page which is not reached
    so the tour was failing due to timeout
    
    build_error-160894
    
    closes odoo/odoo#214259
    
    X-original-commit: de1ca0f425b36b8d14e13a3ebc992422976b454b
    Signed-off-by: Omar Sherif Ali Hassan (osah) <osah@odoo.com>
    Signed-off-by: Valentin Chevalier <vcr@odoo.com>

--- addons/website_sale/tests/test_website_sale_add_to_cart_snippet.py
+++ addons/website_sale/tests/test_website_sale_add_to_cart_snippet.py
@@ -78,6 +78,7 @@ class TestAddToCartSnippet(HttpCase):
             'city': "Ramillies",
             'zip': 1367,
             'country_id': self.env.ref('base.be').id,
+            'phone': "+32 123456789"
         })
         self.env.ref('base.user_admin').country_id = self.env.ref('base.be')
         self.start_tour("/", 'add_to_cart_snippet_tour', login="admin")
