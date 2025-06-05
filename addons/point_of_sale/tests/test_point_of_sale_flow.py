72706e3339f5dc77f884b752545778540a6c3bdd
roen-odoo <roen@odoo.com>
2025-06-05 15:05:23 +0000

    [FIX] point_of_sale: fix refund shiplater
    When refunding a PoS order with mulitple lines containing a product with
    a category that use FIFO/AVCO valuation method, there was a traceback
    
    Steps to reproduce:
    -------------------
    * Create a category CAT that use FIFO/AVCO valuation method
    * Create a product P1 with category CAT
    * Create a product P2 with category CAT
    * Create a PoS order with P1 and P2
    * Validate the order using the shiplater and invoice option
    * Refund the order using the shiplater and invoice option
    > Observation: You get a traceback
    
    opw-4848667
    
    closes odoo/odoo#214677
    
    X-original-commit: e1cb635c518789c4391e17bab185aba5dac42336
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Robin Engels (roen) <roen@odoo.com>

--- addons/point_of_sale/tests/test_point_of_sale_flow.py
+++ addons/point_of_sale/tests/test_point_of_sale_flow.py
@@ -2283,7 +2283,15 @@ class TestPointOfSaleFlow(TestPointOfSaleCommon):
             'name': 'Product A',
             'categ_id': categ.id,
             'lst_price': 10,
-            'standard_price': 10
+            'standard_price': 10,
+            'is_storable': True,
+        })
+        productB = self.env['product.product'].create({
+            'name': 'Product B',
+            'categ_id': categ.id,
+            'lst_price': 10,
+            'standard_price': 10,
+            'is_storable': True,
         })
         order = self.PosOrder.create({
             'company_id': self.env.company.id,
@@ -2292,6 +2300,16 @@ class TestPointOfSaleFlow(TestPointOfSaleCommon):
             'lines': [[0, 0, {
                 'name': "OL/0001",
                 'product_id': product.id,
+                'price_unit': 10,
+                'discount': 0,
+                'qty': 2,
+                'tax_ids': [[6, False, []]],
+                'price_subtotal': 20,
+                'price_subtotal_incl': 20,
+                'total_cost': 20,
+            }], [0, 0, {
+                'name': "OL/0001",
+                'product_id': productB.id,
                 'price_unit': 10,
                 'discount': 0,
                 'qty': 2,
@@ -2327,6 +2345,16 @@ class TestPointOfSaleFlow(TestPointOfSaleCommon):
                 'price_subtotal': -20,
                 'price_subtotal_incl': -20,
                 'refunded_orderline_id': order.lines[0].id,
+                'price_type': 'automatic'
+            }], [0, 0, {
+                'product_id': productB.id,
+                'price_unit': 10,
+                'discount': 0,
+                'qty': -2,
+                'tax_ids': [[6, False, []]],
+                'price_subtotal': -20,
+                'price_subtotal_incl': -20,
+                'refunded_orderline_id': order.lines[1].id,
                 'price_type': 'automatic'
             }]],
             'shipping_date': fields.Date.today(),
