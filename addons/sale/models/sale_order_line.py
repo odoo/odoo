77f0813224ae943152c486d888f4ced5304aea24
Levi Siuzdak (sile) <sile@odoo.com>
2025-06-05 17:00:45 +0200

    [FIX] sale{,_project}: only use custom line descriptions for names
    Versions
    --------
    - 18.0+
    
    Steps
    -----
    1. Have a service product that creates a task on order confirmation;
    2. give it a sales description;
    3. add it to an order and confirm.
    
    Issue
    -----
    The task is named using the sales description.
    
    Cause
    -----
    Commits 47d223759f07 & c3877b2acd74 attempted to restore previous
    task/display name behavior by using the second line of the SOL
    description as the task/display name.
    
    This behavior only happened in previous versions when the line
    description was manually modified.
    
    Solution
    --------
    Only use the new behavior if the line name isn't the same as the
    default.
    
    opw-4634149

--- addons/sale/models/sale_order_line.py
+++ addons/sale/models/sale_order_line.py
@@ -315,10 +315,17 @@ class SaleOrderLine(models.Model):
     def _compute_display_name(self):
         name_per_id = self._additional_name_per_id()
         for so_line in self.sudo():
-            product = so_line.product_id
-            parts = (so_line.name or "").split('\n', 2)
-            # if there's a description, use the first line (skipping the product name)
-            description = (parts[1:2] and parts[1]) or product.name if product else parts[0]
+            if so_line.order_partner_id.lang:
+                so_line = so_line.with_context(lang=so_line.order_id._get_lang())
+            if (product := so_line.product_id).display_name:
+                default_name = so_line._get_sale_order_line_multiline_description_sale()
+                if so_line.name == default_name:
+                    description = product.display_name
+                else:
+                    parts = (so_line.name or "").split('\n', 2)
+                    description = parts[1] if len(parts) > 1 and parts[1] else product.display_name
+            else:
+                description = (so_line.name or "").split('\n', 1)[0]
             name = f"{so_line.order_id.name} - {description}"
             additional_name = name_per_id.get(so_line.id)
             if additional_name:
