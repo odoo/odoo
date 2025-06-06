4e130faf03c2d139554af42df5c4290d7f98dd1a
Meet Jivani [MEJI] <meji@odoo.com>
2025-06-06 10:56:08 +0530

    [IMP] point_of_sale: include google address autocomplete assets in pos
    In this commit:
    - Add the `google_address_autocomplete` module's static assets to the pos
    asset bundle.
    - This ensures that the autocomplete functionality is properly available in the
    Point of Sale frontend.
    
    task-4841245
    
    closes odoo/odoo#213289
    
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/point_of_sale/__manifest__.py
+++ addons/point_of_sale/__manifest__.py
@@ -7,7 +7,7 @@
     'category': 'Sales/Point of Sale',
     'sequence': 40,
     'summary': 'Handle checkouts and payments for shops and restaurants.',
-    'depends': ['resource', 'stock_account', 'barcodes', 'web_editor', 'digest', 'phone_validation', 'partner_autocomplete', 'iot_base'],
+    'depends': ['resource', 'stock_account', 'barcodes', 'web_editor', 'digest', 'phone_validation', 'partner_autocomplete', 'iot_base', 'google_address_autocomplete'],
     'uninstall_hook': 'uninstall_hook',
     'data': [
         'security/point_of_sale_security.xml',
@@ -200,6 +200,7 @@
             ('remove', 'web/static/src/webclient/actions/reports/layout_assets/**/*'),
             ('remove', 'web/static/src/webclient/actions/**/*css'),
             'partner_autocomplete/static/src/**/*',
+            'google_address_autocomplete/static/src/**/*',
         ],
         'point_of_sale.base_tests': [
             "web/static/lib/hoot-dom/**/*",
