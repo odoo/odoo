ff68db58c554837fe80090578514d7fc0b0ca509
nikhil <njor@odoo.com>
2025-06-06 19:17:16 +0530

    [IMP] sale_project: link project created from sale order
    Ensure that a project created via the sale order action is linked
    to the related sale order.
    
    task-4797391
    
    closes odoo/odoo#213759
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/sale_project/models/project_project.py
+++ addons/sale_project/models/project_project.py
@@ -178,11 +178,12 @@ class ProjectProject(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         projects = super().create(vals_list)
-        sol_ids = {
-            vals['sale_line_id']
-            for vals in vals_list
-            if vals.get('sale_line_id')
-        }
+        sol_ids = set()
+        for project, vals in zip(projects, vals_list):
+            if (vals.get('sale_line_id')):
+                sol_ids.add(vals['sale_line_id'])
+            if project.sale_order_id and not project.sale_order_id.project_id:
+                project.sale_order_id.project_id = project.id
         if sol_ids:
             projects._ensure_sale_order_linked(list(sol_ids))
         return projects
