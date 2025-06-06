7ff6deb485967968a3df8ed2c9146dbc2ba5fe44
Alexandre Kühn <aku@odoo.com>
2025-06-06 15:01:36 +0000

    [FIX] mail: better visual of discuss sidebar and chat hub options
    Discuss sidebar options button was too shy on action mouse-hover
    when not active. This commit improves the mouse-hover effect.
    
    The chat hub options button had global opacity when not being used,
    which made it hard to see when there's content below. The hover
    effect was also lacking. This commit improves to have opacity
    change only on icon and not background, and the hover effect is
    made more obvious.
    
    Chat hub options were not dropdown items, thus keyboard navigation
    didn't work. Discuss dropdown menu use dropdown item for keyboard
    navigation, so this was weird to be a rare menu without it. This
    commit turned the buttons into `<DropdownItem>`.
    
    closes odoo/odoo#213460
    
    X-original-commit: e996aae5a2f9ac8fbdac6f3533328b751756e32c
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/im_livechat/static/tests/chat_hub_compact.test.js
+++ addons/im_livechat/static/tests/chat_hub_compact.test.js
@@ -24,8 +24,8 @@ test("Do not open chat windows automatically when chat hub is compact", async ()
         create_uid: serverState.publicUserId,
     });
     await start();
-    await click("button.oi.oi-ellipsis-h[title='Chat Options']");
-    await click("button.o-mail-ChatHub-option", { text: "Hide all conversations" });
+    await click("button[title='Chat Options']");
+    await click(".o-dropdown-item", { text: "Hide all conversations" });
     await contains(".o-mail-ChatHub-bubbleBtn .fa-comments");
     await withGuest(guestId, () =>
         rpc("/mail/message/post", {
