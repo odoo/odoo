134f78619b9ee52ff61ca7f362c67332f47a9364
Johan Tötterman <johan@ahkio.com>
2025-06-06 15:10:32 +0300

    [FIX] account: Fix autocheck_on_post logic when posting move
    The Checked- field on an Invoice/Bill is used to indicate whether a move needs to be checked or if all the information on the move is okay at the time of creation of the move.
    
    The Checked- field can be ticked / unticked by the user when editing an Invoice / Bill.
    
    In addition to the Checked- field feature, Odoo also includes an Auto-Check on Post - feature on the Journal. When the Auto-Check on Post - field is ticked on the Journal, Odoo should automatically tick the Checked field on the Invoice/Bill when posting the move.
    
    Currently this feature is broken, because the Checked- field's value on the Invoice/Bill is always overwritten with the Journal's Auto-Check on Post - field's value when a move is posted.
    
    This PR fixes this so that the Checked- field's value is automatically ticked when posting a move, only when the Journal's Auto-Check on Post - field is ticked, as we don't want the Checked- field's value to be automatically un-ticked when posting a move and the Journal's Auto-Check on Post - field is un-ticked.
    
    closes odoo/odoo#213370
    
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -4912,7 +4912,8 @@ class AccountMove(models.Model):
                 validation_msgs.add(_("A line of this move is using a deprecated account, you cannot post it."))
 
             # If the field autocheck_on_post is set, we want the checked field on the move to be checked
-            move.checked = move.journal_id.autocheck_on_post
+            if move.journal_id.autocheck_on_post:
+                move.checked = move.journal_id.autocheck_on_post
 
         if validation_msgs:
             msg = "\n".join([line for line in validation_msgs])
