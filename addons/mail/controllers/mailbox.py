a331ad0fdf59b46c4ad028ce220925f2e70213a9
Alexandre Kühn <aku@odoo.com>
2025-06-06 14:02:36 +0200

    [FIX] mail: filter_messages_exist

--- addons/mail/controllers/mailbox.py
+++ addons/mail/controllers/mailbox.py
@@ -3,6 +3,7 @@
 from odoo import http
 from odoo.http import request
 from odoo.addons.mail.tools.discuss import Store
+from odoo.addons.mail.tools.message import filter_messages_exist
 
 
 class MailboxController(http.Controller):
@@ -10,7 +11,7 @@ class MailboxController(http.Controller):
     def discuss_inbox_messages(self, search_term=None, before=None, after=None, limit=30, around=None):
         domain = [("needaction", "=", True)]
         res = request.env["mail.message"]._message_fetch(domain, search_term=search_term, before=before, after=after, around=around, limit=limit)
-        messages = res.pop("messages").filtered(lambda m: request.env[m.model].browse(m.res_id).exists())
+        messages = filter_messages_exist(request.env, res.pop("messages"))
         return {
             **res,
             "data": Store(messages, for_current_user=True, add_followers=True).get_result(),
@@ -21,7 +22,7 @@ class MailboxController(http.Controller):
     def discuss_history_messages(self, search_term=None, before=None, after=None, limit=30, around=None):
         domain = [("needaction", "=", False)]
         res = request.env["mail.message"]._message_fetch(domain, search_term=search_term, before=before, after=after, around=around, limit=limit)
-        messages = res.pop("messages").filtered(lambda m: request.env[m.model].browse(m.res_id).exists())
+        messages = filter_messages_exist(request.env, res.pop("messages"))
         return {
             **res,
             "data": Store(messages, for_current_user=True).get_result(),
@@ -32,7 +33,7 @@ class MailboxController(http.Controller):
     def discuss_starred_messages(self, search_term=None, before=None, after=None, limit=30, around=None):
         domain = [("starred_partner_ids", "in", [request.env.user.partner_id.id])]
         res = request.env["mail.message"]._message_fetch(domain, search_term=search_term, before=before, after=after, around=around, limit=limit)
-        messages = res.pop("messages").filtered(lambda m: request.env[m.model].browse(m.res_id).exists())
+        messages = filter_messages_exist(request.env, res.pop("messages"))
         return {
             **res,
             "data": Store(messages, for_current_user=True).get_result(),
