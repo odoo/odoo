4a249f8f11d53f7b910e9c1989998fe892c6b0e6
ramh <ramh@odoo.com>
2025-06-06 12:56:19 +0000

    [FIX] mail: display help message when no favorite GIFs are present
    Before this commit:
    
    When the user opened the Favorite category with no favorite GIFs,
    nothing was shown. This was caused by using .length on Map objects
    (evenGif.gifs and oddGif.gifs), which should have been .size.
    
    After this commit:
    
    Help message is correctly displayed when no favorite GIFs are available.
    
    closes odoo/odoo#214310
    
    X-original-commit: d2868da4e7d99a967a4e2ab5941329fdf5593532
    Signed-off-by: Haja Ram (ramh) <ramh@odoo.com>

--- addons/mail/static/tests/gif_picker/gif_picker.test.js
+++ addons/mail/static/tests/gif_picker/gif_picker.test.js
@@ -283,3 +283,16 @@ test("Scrolling at the bottom should trigger the search to load more gif, even a
     await scroll(".o-discuss-GifPicker-content", "bottom");
     await contains(".o-discuss-Gif", { count: 8 });
 });
+
+test("Show help when no favorite GIF", async () => {
+    const pyEnv = await startServer();
+    const channelId = pyEnv["discuss.channel"].create({ name: "" });
+    onRpc("/discuss/gif/categories", () => rpc.categories);
+    await start();
+    await openDiscuss(channelId);
+    await click("button[title='Add GIFs']");
+    // gif picker quires extra delay before click (to give time to load initial state)
+    await contains(".o-discuss-GifPicker");
+    await click(".o-discuss-GifPicker div[aria-label='list-item']", { text: "Favorites" });
+    await contains("span", { text: "So uhh... maybe go favorite some GIFs?" });
+});
