6aaf3c8d1b99a2f6ecfaeb80f0f3d61e0baf765e
Alexandre Kühn <aku@odoo.com>
2025-06-11 13:16:26 +0200

    [FIX] mail: scroll to message scroll to top of message
    Before this commit, when jumping to a message like from
    message reply-to or "jump" in pinned or searched messages,
    it was smooth scrolling to the middle of message.
    
    This is a problem on big messages, because we have to manually
    scroll to top message which is not great.
    
    This happens because the programmatic scroll is made with
    `.scrollIntoView({ block: "center" })` on the whole message.
    The `block: "center"` centers the message on the scrollable
    which is good, but it also scrolls in the middle of element,
    hence the issue.
    
    This commit fixes the issue by keeping `block: "center"` for
    nice scroll in middle of screen, but instead of using whole
    message element it uses a small invisible and click-through
    element `div.o-mail-Message-jumpTarget` that is used as
    the target for highlight and jump. This hidden element is at
    the very top of message, so `scrollIntoView({ block: "center" })`
    will scroll to top of message and put it in middle of scrollable.
    
    Task-4688158

--- addons/mail/static/tests/thread/message_highlight.test.js
+++ addons/mail/static/tests/thread/message_highlight.test.js
@@ -8,7 +8,7 @@ import {
 } from "@mail/../tests/mail_test_helpers";
 import { Thread } from "@mail/core/common/thread";
 import { describe, test } from "@odoo/hoot";
-import { advanceTime, Deferred, tick } from "@odoo/hoot-dom";
+import { advanceTime, Deferred, tick, waitFor } from "@odoo/hoot-dom";
 import { disableAnimations } from "@odoo/hoot-mock";
 import { patchWithCleanup } from "@web/../tests/web_test_helpers";
 
@@ -72,4 +72,34 @@ test("can highlight message (slow ref registration)", async () => {
     await advanceTime(1000);
     slowRegisterMessageDef.resolve();
     await isInViewportOf(".o-mail-Message:contains(message 100)", ".o-mail-Thread");
+});
+
+test("highlight scrolls to beginning of long message", async () => {
+    disableAnimations();
+    const pyEnv = await startServer();
+    const channelId = pyEnv["discuss.channel"].create({ name: "general" });
+    const [messageId1] = pyEnv["mail.message"].create([
+        {
+            body: `long message `.repeat(500),
+            model: "discuss.channel",
+            res_id: channelId,
+        },
+        {
+            body: `short message`,
+            model: "discuss.channel",
+            res_id: channelId,
+        },
+    ]);
+    await pyEnv["discuss.channel"].set_message_pin(channelId, messageId1, true);
+    await start();
+    await openDiscuss(channelId);
+    await waitFor(".o-mail-Message:contains('short message')");
+    await isInViewportOf(".o-mail-Message:contains('short message')", ".o-mail-Thread");
+    await click("a[data-oe-type='highlight']");
+    await advanceTime(1000);
+    await isInViewportOf(".o-mail-Message:contains('long message')", ".o-mail-Thread");
+    await isInViewportOf(
+        ".o-mail-Message:contains('long message') .o-mail-Message-avatar", // avatar is at beginning of message
+        ".o-mail-Thread"
+    );
 });
