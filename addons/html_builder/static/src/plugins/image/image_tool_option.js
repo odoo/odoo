696fec9c68221ee1fac1613cce6f79007470461f
Subhadeep R. Dev <sdev@odoo.com>
2025-06-11 15:39:55 +0000

    [FIX] html_builder, *: fix the image transform button
    * html_editor, website
    
    The "Transform Image" button was non-functional due to an undefined
    "transformImage" user command. To fix this a new `ImageTransformOption`
    component has been introduced. It is equivalent to the
    `ImageTransformButton` component but adapted for website. Differences
    include:
    - The reset transform logic is handled by a separate button.
    - The active state on the button is managed by the `BuilderButton`.
    - Other website specific adaptations.
    
    Steps to reproduce the error:
    1. In edit mode, click on an image
    2. Click on the "Transform Image" button
    3. Observe the error: Unknown user command id: transformImage
    
    Related to task-4367641
    
    X-original-commit: 3b6843670b0de3b446b3f5b6849e8d4c6081aba7
    Part-of: odoo/odoo#219326
    Signed-off-by: Colin Louis (loco) <loco@odoo.com>

--- addons/html_builder/static/src/plugins/image/image_tool_option.js
+++ addons/html_builder/static/src/plugins/image/image_tool_option.js
@@ -2,6 +2,7 @@ import { BaseOptionComponent } from "@html_builder/core/utils";
 import { ImageShapeOption } from "@html_builder/plugins/image/image_shape_option";
 import { ImageFilterOption } from "@html_builder/plugins/image/image_filter_option";
 import { ImageFormatOption } from "@html_builder/plugins/image/image_format_option";
+import { ImageTransformOption } from "./image_transform_option";
 
 export class ImageToolOption extends BaseOptionComponent {
     static template = "html_builder.ImageToolOption";
@@ -9,6 +10,7 @@ export class ImageToolOption extends BaseOptionComponent {
         ImageShapeOption,
         ImageFilterOption,
         ImageFormatOption,
+        ImageTransformOption,
     };
     static props = {};
 }
