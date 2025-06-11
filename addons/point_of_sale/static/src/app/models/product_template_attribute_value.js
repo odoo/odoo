17f37c979e51f0976d7a4e872ed6405b25d30669
Adrien Guilliams (adgu) <adgu@odoo.com>
2025-06-11 11:53:40 +0000

    [PERF] point_of_sale: excluding attributes values
    Before this commit, the computation of the exclusion of some attributes
    based on other selected was going through every attribute to determine
    if it was excluded or not. This is now done once at the loading of the
    pos.
    
    X-original-commit: 4ae2f4b00c798f5057fe224113c2d40d4eb95453

--- addons/point_of_sale/static/src/app/models/product_template_attribute_value.js
+++ addons/point_of_sale/static/src/app/models/product_template_attribute_value.js
@@ -3,6 +3,7 @@ import { Base } from "./related_models";
 
 export class ProductTemplateAttributeValue extends Base {
     static pythonModel = "product.template.attribute.value";
+<<<<<<< d0f75f1c4edec9905e9657d73e5eebf15765bfa6
 
     get exclusions() {
         const values = this.models["product.template.attribute.value"].filter((value) =>
@@ -16,6 +17,26 @@ export class ProductTemplateAttributeValue extends Base {
         const excludedIds = values.map(({ id }) => id);
         return this.exclusions.some(({ id }) => excludedIds.includes(id));
     }
+||||||| 216773812903f039e47ebaa8e606aac7c23361c2
+
+    setup() {
+        super.setup(...arguments);
+    }
+
+    get exclusions() {
+        const values = this.models["product.template.attribute.value"].filter((value) =>
+            value.exclude_for.some(({ value_ids }) => value_ids.some(({ id }) => id === this.id))
+        );
+
+        return [...this.exclude_for.flatMap(({ value_ids }) => value_ids), ...values];
+    }
+
+    doHaveConflictWith(values) {
+        const excludedIds = values.map(({ id }) => id);
+        return this.exclusions.some(({ id }) => excludedIds.includes(id));
+    }
+=======
+>>>>>>> a3b5ddd909ceb3b09acd0ebb1dde6f2561a8244e
 }
 
 registry
