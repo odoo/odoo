5f2f2a43d7578ea576b8fd9208f53281a39a4378
paan-odoo <paan@odoo.com>
2025-06-11 07:00:27 +0000

    [FIX] pos_{ , event }: preserve ticket price for event orderlines in PoS
    Before this commit:
    
    - When selling event tickets through the Point of Sale (PoS), if an orderline
      was created with the ticket-specific price (higher than the base product
      price), the price would reset to the base product price when a customer
      (partner) was added or the pricelist was changed.
    
    After this commit:
    
    - The event ticket orderline now retains its original ticket price even after a
      partner is selected or the pricelist is updated. This ensures pricing
      consistency and prevents unintended overrides for event-specific products.
    
    task-4862687
    
    closes odoo/odoo#223646
    
    X-original-commit: 910217fd3f6fa806350d09426185fd81d708ba4a
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Anantkumar Parmar (paan) <paan@odoo.com>

--- addons/point_of_sale/static/src/app/models/pos_order.js
+++ addons/point_of_sale/static/src/app/models/pos_order.js
@@ -385,11 +385,7 @@ export class PosOrder extends Base {
     setPricelist(pricelist) {
         this.pricelist_id = pricelist ? pricelist : false;
 
-        const lines_to_recompute = this.lines.filter(
-            (line) =>
-                line.price_type === "original" &&
-                !(line.combo_line_ids?.length || line.combo_parent_id)
-        );
+        const lines_to_recompute = this.getLinesToCompute();
 
         for (const line of lines_to_recompute) {
             const newPrice = line.product_id.getPrice(
@@ -904,6 +900,14 @@ export class PosOrder extends Base {
 
     get showChange() {
         return !this.currency.isZero(this.orderChange) && this.finalized;
+    }
+
+    getLinesToCompute() {
+        return this.lines.filter(
+            (line) =>
+                line.price_type === "original" &&
+                !(line.combo_line_ids?.length || line.combo_parent_id)
+        );
     }
 }
 
