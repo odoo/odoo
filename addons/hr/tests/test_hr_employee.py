090067aea47e6fbf27642d21363318ab74601f22
romc-odoo <romc@odoo.com>
2025-06-11 09:03:45 +0200

    [FIX] hr: correctly update job title
    Steps to reproduce the bug:
    - Select a job position for the employee, the job title should be the same
    - Change the job title (let's call it A)
    - Change the job position's name, the job title should remain as before
    - Change the job position, the job title updates correctly
    - Without saving, change the job position to A
    - The job title is not correctly updated
    
    This commit fixes the issue by correctly computing the technical field is_custom_job_title.
    
    task-4860251

--- addons/hr/tests/test_hr_employee.py
+++ addons/hr/tests/test_hr_employee.py
@@ -529,6 +529,31 @@ class TestHrEmployee(TestHrCommon):
         self.assertEqual(self.employee.resource_calendar_id, old_calendar)
         self.assertEqual(self.employee.resource_id.calendar_id, old_calendar)
 
+    def test_job_title(self):
+        first_job = self.env['hr.job'].create({'name': "first job"})
+        second_job = self.env['hr.job'].create({'name': "second job"})
+
+        with Form(self.employee_without_image) as employee_form:
+            # Assign first job to employee, job title should be job name
+            employee_form.job_id = first_job
+            self.assertEqual(employee_form.job_title, first_job.name)
+
+            # Change job title, job name should not change
+            employee_form.job_title = "custom job title"
+            self.assertEqual(first_job.name, "first job")
+
+            # Change the name of the first job, job title should not be updated
+            first_job.name = "first job modified"
+            self.assertEqual(employee_form.job_title, "custom job title")
+            employee_form.save()
+
+            # Assign second job to employee, job title should be second job name
+            employee_form.job_id = second_job
+            self.assertEqual(employee_form.job_title, second_job.name)
+
+            # Switch back to first job, job title should be first job name
+            employee_form.job_id = first_job
+            self.assertEqual(employee_form.job_title, first_job.name)
 
 @tagged('-at_install', 'post_install')
 class TestHrEmployeeWebJson(HttpCase):
