aefb3e47e9f26a066bbc0087a80a1506d75f0082
jeanschoenlaub <jesc@odoo.com>
2025-06-11 17:43:44 +0200

    [IMP] product: Improve packaging barcode domain.
    AS-IS: The current product_uom_ids (packaging barcode) domain was empty if neither
    product_id or product_ids was in context. This meant that calling product_uom_ids
    on a stock_move.product_uom would be empty for example.
    
    NOW: if neither product_id or product_ids in context we return ALL packaging
    barcodes linked to a unit of measure.
    
    task 4729518
    
    closes odoo/odoo#213852
    
    Related: odoo/enterprise#87450
    Signed-off-by: Steve Van Essche <svs@odoo.com>

--- addons/product/models/uom_uom.py
+++ addons/product/models/uom_uom.py
@@ -2,12 +2,21 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import models, fields, _
+from odoo.fields import Domain
 
 
 class UomUom(models.Model):
     _inherit = 'uom.uom'
 
-    product_uom_ids = fields.One2many('product.uom', 'uom_id', string='Barcodes', domain=lambda self: ['|', ('product_id', '=', self.env.context.get('product_id')), ('product_id', 'in', self.env.context.get('product_ids', []))])
+    def _domain_product_uoms(self):
+        domain = []
+        if self.env.context.get("product_id"):
+            domain.append(Domain('product_id', '=', self.env.context['product_id']))
+        if self.env.context.get("product_ids"):
+            domain.append(Domain('product_id', 'in', self.env.context['product_ids']))
+        return Domain.OR(domain) if domain else Domain.TRUE
+
+    product_uom_ids = fields.One2many('product.uom', 'uom_id', string='Barcodes', domain=_domain_product_uoms)
 
     def action_open_packaging_barcodes(self):
         self.ensure_one()
