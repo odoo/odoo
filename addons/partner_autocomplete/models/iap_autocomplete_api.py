0ee942cb46f172557967c2efd15a02d28bbcdc92
Ricardo Gomes Rodrigues <rigr@odoo.com>
2025-06-11 15:28:32 +0200

    [IMP] partner_autocomplete: send db version and lang to IAP
    task-4746445

--- addons/partner_autocomplete/models/iap_autocomplete_api.py
+++ addons/partner_autocomplete/models/iap_autocomplete_api.py
@@ -3,7 +3,7 @@
 
 import logging
 
-from odoo import api, models, exceptions, _
+from odoo import api, models, exceptions, _, release
 from odoo.addons.iap.tools import iap_tools
 from requests.exceptions import HTTPError
 
@@ -24,6 +24,8 @@ class IapAutocompleteEnrichAPI(models.AbstractModel):
             raise ValueError(_('No account token'))
         params.update({
             'db_uuid': self.env['ir.config_parameter'].sudo().get_param('database.uuid'),
+            'db_version': release.version,
+            'db_lang': self.env.lang,
             'account_token': account.account_token,
             'country_code': self.env.company.country_id.code,
             'zip': self.env.company.zip,
