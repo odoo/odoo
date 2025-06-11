3e0aa3e7563619eec24e2466ae72bf38a3dd0da9
nni@odoo.com <nni@odoo.com>
2025-06-11 15:56:54 +0800

    [IMP] payment_xendit: add TH,MY, VN coverage
    Xendit has expanded to TH, MY and VN supporting the local payment channels. This commit is to add the supported pamyent channels according to what they have added.
    
    task-4334511
    
    closes odoo/odoo#189527
    
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>

--- addons/payment_xendit/models/payment_transaction.py
+++ addons/payment_xendit/models/payment_transaction.py
@@ -95,6 +95,9 @@ class PaymentTransaction(models.Model):
             ],
             'currency': self.currency_id.name,
         }
+        # If it's one of FPX methods, assign the payment methods as FPX automatically
+        if self.payment_method_code == 'fpx':
+            payload['payment_methods'] = const.FPX_METHODS
         # Extra payload values that must not be included if empty.
         if self.partner_email:
             payload['customer']['email'] = self.partner_email
@@ -207,7 +210,11 @@ class PaymentTransaction(models.Model):
         self.provider_reference = notification_data.get('id')
 
         # Update payment method.
+        # If it's one of FPX Methods, assign the payment method as FPX automatically
         payment_method_code = notification_data.get('payment_method', '')
+        if payment_method_code in const.FPX_METHODS:
+            payment_method_code = 'fpx'
+
         payment_method = self.env['payment.method']._get_from_code(
             payment_method_code, mapping=const.PAYMENT_METHODS_MAPPING
         )
