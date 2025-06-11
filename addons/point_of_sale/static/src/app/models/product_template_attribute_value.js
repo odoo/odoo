0a8eede3f0c288956c0484c2c70545b89cc01217
Adrien Guilliams (adgu) <adgu@odoo.com>
2025-06-11 11:53:40 +0000

    [PERF] point_of_sale: excluding attributes values
    Before this commit, the computation of the exclusion of some attributes
    based on other selected was going through every attribute to determine
    if it was excluded or not. This is now done once at the loading of the
    pos.
    
    X-original-commit: ea6323ea99e1123e2cbec757027a97451bd79407

--- addons/point_of_sale/static/src/app/models/product_template_attribute_value.js
+++ addons/point_of_sale/static/src/app/models/product_template_attribute_value.js
@@ -3,19 +3,6 @@ import { Base } from "./related_models";
 
 export class ProductTemplateAttributeValue extends Base {
     static pythonModel = "product.template.attribute.value";
-
-    get exclusions() {
-        const values = this.models["product.template.attribute.value"].filter((value) =>
-            value.exclude_for.some(({ value_ids }) => value_ids.some(({ id }) => id === this.id))
-        );
-
-        return [...this.exclude_for.flatMap(({ value_ids }) => value_ids), ...values];
-    }
-
-    doHaveConflictWith(values) {
-        const excludedIds = values.map(({ id }) => id);
-        return this.exclusions.some(({ id }) => excludedIds.includes(id));
-    }
 }
 
 registry
