67a52e89790275db83b1b64e368d8ee709a45c86
Abdelrahman Mahmoud (amah) <amah@odoo.com>
2025-06-12 11:10:31 +0000

    [FIX] hr_contract: Fix menu parent restoration on uninstall
    This PR adds an uninstall_hook to restore the correct parent menus under the hr module that were overridden by this module.
    
    Task: 4818020
    X-original-commit: f75ef24284b74a9dd0c7c61277a86e0e11dd634f

--- addons/hr_contract/__init__.py
+++ addons/hr_contract/__init__.py
@@ -5,6 +5,7 @@ import logging
 from . import models
 from . import report
 from . import wizard
+<<<<<<< dd00b83f65abd3383e7c7f49f6d0729290403862
 
 _logger = logging.getLogger(__name__)
 
@@ -17,3 +18,15 @@ def uninstall_hook(env):
         env.ref('hr.menu_view_hr_contract_type').sequence = 2
     except ValueError as e:
         _logger.warning(e)
+||||||| f889a6f2f9d12571743a7d89ce4dbfe1191e0cd4
+=======
+
+_logger = logging.getLogger(__name__)
+
+
+def uninstall_hook(env):
+    try:
+        env.ref('hr.menu_resource_calendar_view').parent_id = env.ref("hr.menu_config_employee")
+    except ValueError as e:
+        _logger.warning(e)
+>>>>>>> 29b53cf8f34225bc6722939c89f9d19eb502529f
