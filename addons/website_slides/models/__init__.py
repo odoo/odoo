18a3d99dd924da2699cd1dbe52088c8f9659c367
Thibault Delavallée <tde@odoo.com>
2025-06-12 14:14:10 +0200

    [MOV] website_slides: split models into files
    Just to better see directory organization. This commit contains only
    code move to try to keep history.
    
    Move: channel tag and decorated m2m models (i.e. slide partner and
    channel partner) models into their own file.
    
    Task-4868193

--- addons/website_slides/models/__init__.py
+++ addons/website_slides/models/__init__.py
@@ -2,12 +2,15 @@
 
 from . import gamification_challenge
 from . import gamification_karma_tracking
+from . import slide_slide_partner  # decorated m2m, import before
 from . import slide_slide
 from . import slide_question
 from . import slide_embed
+from . import slide_channel_partner  # decorated m2m, import before
 from . import slide_channel
 from . import slide_channel_tag
 from . import slide_slide_resource
+from . import slide_tag
 from . import res_config_settings
 from . import website
 from . import res_users
