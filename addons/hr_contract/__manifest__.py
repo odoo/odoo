7fef311e46709776a5037c3ad8a285a5db64cddf
Abdelrahman Mahmoud (amah) <amah@odoo.com>
2025-06-12 11:14:10 +0000

    [FIX] hr_contract: Fix menu parent restoration on uninstall
    This PR adds an uninstall_hook to restore the correct parent menus under the hr module that were overridden by this module.
    
    Task: 4818020
    X-original-commit: 5338ffb83296c8fc85182bcaae7faf4ee312d59e

---
+++ addons/hr_contract/__manifest__.py
@@ -0,0 +1,50 @@
+<<<<<<< HEAD
+||||||| MERGE BASE
+=======
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Employee Contracts',
+    'version': '1.0',
+    'category': 'Human Resources/Contracts',
+    'sequence': 335,
+    'description': """
+Add all information on the employee form to manage contracts.
+=============================================================
+
+    * Contract
+    * Place of Birth,
+    * Medical Examination Date
+    * Company Vehicle
+
+You can assign several contracts per employee.
+    """,
+    'website': 'https://www.odoo.com/app/employees',
+    'depends': ['hr'],
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'data/mail_message_subtype_data.xml',
+        'data/hr_contract_data.xml',
+        'report/hr_contract_history_report_views.xml',
+        'views/hr_contract_views.xml',
+        'views/hr_employee_views.xml',
+        'views/resource_calendar_views.xml',
+        'views/res_config_settings_views.xml',
+        'wizard/hr_departure_wizard_views.xml',
+    ],
+    'demo': ['data/hr_contract_demo.xml'],
+    'installable': True,
+    'application': True,
+    'uninstall_hook': "uninstall_hook",
+    'assets': {
+        'web.assets_backend': [
+            'hr_contract/static/src/**/*',
+        ],
+    },
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
+
+>>>>>>> FORWARD PORTED
--- addons/l10n_account_withholding_tax_pos/__init__.py
+++ addons/l10n_account_withholding_tax_pos/__init__.py
@@ -1,3 +1,25 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import logging
+
 from . import models
+<<<<<<< 82712e8a2a05304f568c477920bc721607a012d2:addons/l10n_account_withholding_tax_pos/__init__.py
+||||||| fc8dd35cca57ee53f7fa041de5750f4b62fdeb93:addons/hr_contract/__init__.py
+from . import report
+from . import wizard
+=======
+from . import report
+from . import wizard
+
+_logger = logging.getLogger(__name__)
+
+
+def uninstall_hook(env):
+    try:
+        env.ref('hr.menu_resource_calendar_view').parent_id = env.ref("hr.menu_config_employee")
+        env.ref('hr.menu_view_hr_contract_type').parent_id = env.ref("hr.menu_config_recruitment")
+        env.ref('hr.menu_view_hr_contract_type').active = False
+        env.ref('hr.menu_view_hr_contract_type').sequence = 2
+    except ValueError as e:
+        _logger.warning(e)
+>>>>>>> 4e2b0429f6935d42a8acf75e891a59203bceadd7:addons/hr_contract/__init__.py
