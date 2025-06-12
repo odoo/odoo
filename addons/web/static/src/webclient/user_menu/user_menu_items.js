a1ba9ec1be7639b2bf55e0cd3b4f562921e39e4e
Léo Gizard <legi@odoo.com>
2025-06-12 12:51:53 +0000

    [FIX] web: user menu text overlapping checkbox
    **PROBLEM**
    In the user menu, the text of dropdown item sometimes overlap with the checkbox next to it when it's too long.
    
    **STEP TO REPRODUCE**
    (happened with the italian localization of the onboarding module, but it's no longer the case because the text was shortened)
    1. In `enterprise/web_enterprise/i18n/it.po`, for the msgid "Dark Mode" set the translation to something long.
    2. click on the user menu and notice the text for the dark mode dropdown item is overlapping with the checkbox next to it.
    
    **CAUSE**
    The checkbox in the user_menu.xml template use d-flex and flex-row-reverse, which doesn't work well with the class form-switch.
    form-switch add a negative left-margin of -2.5em to the <input> inside the checkbox template.
    
    **FIX**
    Put the description text before the checkbox in a flex div, and remove the flex-row-reverse on the checkbox.
    
    opw-4654405
    
    X-original-commit: ff3ad49f635cd60ae7c5055e931c57c458ec4b8a

--- addons/web/static/src/webclient/user_menu/user_menu_items.js
+++ addons/web/static/src/webclient/user_menu/user_menu_items.js
@@ -55,7 +55,7 @@ function shortCutsItem(env) {
         id: "shortcuts",
         hide: env.isSmall,
         description: markup(
-            `<div class="d-flex align-items-center justify-content-between">
+            `<div class="d-flex align-items-center justify-content-between p-0 w-100">
                 <span>${escape(translatedText)}</span>
                 <span class="fw-bold">${isMacOS() ? "CMD" : "CTRL"}+K</span>
             </div>`
