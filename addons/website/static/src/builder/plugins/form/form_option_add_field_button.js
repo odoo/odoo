51988596cc9a69e3ebdcc067c86c2f73d67e6b6d
Augustin (duau) <duau@odoo.com>
2025-06-12 18:04:34 +0200

    [FIX] website: add step in history when adding a new form field
    This commit ensures that adding a new field to a form correctly updates
    the history.
    
    This regression was introduced in the website refactor [1].
    
    Steps to reproduce:
    - Drop a form snippet
    - Click on `+ Field`
    - A new field is added
    - Clicking on undo should only remove the field
    
    Previously, clicking undo would remove the entire form snippet.
    
    [1]: https://github.com/odoo/odoo/commit/9fe45e2b7ddbbfd0445ffe25a859e67a316d02b2

--- addons/website/static/src/builder/plugins/form/form_option_add_field_button.js
+++ addons/website/static/src/builder/plugins/form/form_option_add_field_button.js
@@ -1,16 +1,20 @@
-import { BaseOptionComponent, useDomState } from "@html_builder/core/utils";
+import { useOperation } from "@html_builder/core/operation_plugin";
+import { BaseOptionComponent } from "@html_builder/core/utils";
 
 export class FormOptionAddFieldButton extends BaseOptionComponent {
-    // TODO create +Field template
     static template = "website.s_website_form_form_option_add_field_button";
     static props = {
         addField: Function,
         tooltip: String,
     };
+
     setup() {
-        super.setup();
-        this.domState = useDomState((el) => ({
-            el,
-        }));
+        this.callOperation = useOperation();
+    }
+
+    addField() {
+        this.callOperation(() => {
+            this.props.addField(this.env.getEditingElement());
+        });
     }
 }
