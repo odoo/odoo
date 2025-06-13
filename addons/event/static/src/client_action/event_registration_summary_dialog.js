7600927bb0b447fd59ddb504c897e32e842f0085
nihp-odoo <nihp@odoo.com>
2025-06-13 16:28:45 +0530

    [IMP] event: log when scanning ticket or manually marking as attended
    Purpose:
    =====================
    If there is a multi-day event, there's no way manage attendance for
    each day.
    
    Specifications:
    =====================
    - When ticket is scanned show log in chatter.
    - In the Registration Desk, when scanning or manually marking an
      attendee as attended, if the attendee is already marked as done
      but today's date is different from the current date_closed, update
      the warning message to 'Ticket was first scanned on <DATE>'.
    
    Task-4864066

--- addons/event/static/src/client_action/event_registration_summary_dialog.js
+++ addons/event/static/src/client_action/event_registration_summary_dialog.js
@@ -30,6 +30,7 @@ export class EventRegistrationSummaryDialog extends Component {
         this.orm = useService("orm");
         this.notification = useService("notification");
         this.continueButtonRef = useRef("continueButton");
+        this.button = useState({enabled: true});
 
         this.registrationStatus = useState({value: this.registration.status});
         const storedPrintSettings = browser.localStorage.getItem(PRINT_SETTINGS_LOCAL_STORAGE_KEY);
@@ -82,8 +83,11 @@ export class EventRegistrationSummaryDialog extends Component {
     }
 
     async onRegistrationConfirm() {
-        await this.orm.call("event.registration", "action_set_done", [this.registration.id]);
-        this.registrationStatus.value = "confirmed_registration";
+        if (this.registrationStatus.value !== "confirmed_registration") {
+            this.button.enabled = false
+            await this.orm.call("event.registration", "action_set_done", [this.registration.id]).catch(() => this.button.enabled = true);
+            this.registrationStatus.value = "confirmed_registration";
+        }
         this.props.close();
         if (this.props.model) {
             this.props.model.load();
