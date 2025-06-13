e20fd9fe93ff4dbce61f7f3b41b6ae73159dd335
Sven Fuehr <svfu@odoo.com>
2025-06-13 09:33:18 +0200

    [FIX] account: fix transfer wizard reconciled error message
    In commit 6825886d0b5d49a4d600c7ef4ad7ee63c6faf0c3 an error message
    in the Transfer Journal Items wizard (`account.automatic.entry.wizard`)
    was broken.
    
    This commit fixes that error message.
    
    task-None
    
    closes odoo/odoo#214126
    
    Signed-off-by: Habib Ayob (ayh) <ayh@odoo.com>

--- addons/account/wizard/account_automatic_entry_wizard.py
+++ addons/account/wizard/account_automatic_entry_wizard.py
@@ -147,7 +147,7 @@ class AccountAutomaticEntryWizard(models.TransientModel):
         if any(move.state != 'posted' for move in move_line_ids.mapped('move_id')):
             raise UserError(_("Oops! You can only change the period or account for posted entries! Other ones aren't up for an adventure like that!"))
         if any(move_line.reconciled for move_line in move_line_ids):
-            raise UserError(_("Oops! You can only change the period or account for posted entries! Other ones aren't up for an adventure like that!"))
+            raise UserError(_("Oops! You can only change the period or account for items that are not yet reconciled! Other ones aren't up for an adventure like that!"))
         if any(line.company_id.root_id != move_line_ids[0].company_id.root_id for line in move_line_ids):
             raise UserError(_('You cannot use this wizard on journal entries belonging to different companies.'))
         res['company_id'] = move_line_ids[0].company_id.root_id.id
