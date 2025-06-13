24f0f325fac8996fc2c115377b08e0a45bb00094
romc-odoo <romc@odoo.com>
2025-06-13 14:31:31 +0200

    [IMP] hr_*: add smartbuttons on public employee
    *=attendance,holidays,maintenance,skills_slides,timesheet
    
    With this commit, if a user has no HR rights and is looking at his own public employee record, he should see some smartbuttons linking multiple apps.
    The following smartbuttons have been added:
    - Attendances and overtime (if display overtime setting is activated)
    - Time off (Remaning leaves)
    - Maintenance (Equipment)
    - eLearning courses
    - Timesheets
    
    task-4840326
    
    closes odoo/odoo#213958
    
    Related: odoo/enterprise#87498
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

---
+++ addons/hr_maintenance/models/hr_employee_public.py
@@ -0,0 +1,9 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class HrEmployeePublic(models.Model):
+    _inherit = 'hr.employee.public'
+
+    equipment_count = fields.Integer(related='employee_id.equipment_count')
