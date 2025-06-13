60e2a243fb3180c12aafe43ef15fa0e88e3ed31a
sbbh-odoo <sbbh@odoo.com>
2025-06-13 09:20:45 +0000

    [IMP] html_editor: move switch component in html_editor from website
    Before this commit:
    
    - The `html_editor` module used the `o_switch` class to render toggle switches
      and has separate `video_selector.scss` from version 18 .
    - The actual `Switch` component and its SCSS lived separately in the
      `website` module, causing duplication and preventing reuse.
    
    After this commit:
    
    - The `Switch` component and its SCSS have been moved into the `html_editor`
      module.
    - Since `html_editor` is now in depends of `website` in master, this
      centralizes the component while keeping compatibility.
    - This allows unified behavior and styling for switches used in both
      `html_editor` (e.g., in `videoOptions`) and website dialogs
      (e.g., the "Add Page" dialog).
    
    task-4865400

--- addons/html_editor/static/src/components/switch/switch.js
+++ addons/html_editor/static/src/components/switch/switch.js
@@ -4,10 +4,11 @@ const NO_OP = () => {};
 
 export class Switch extends Component {
     static props = {
-        value: Boolean,
+        value: { type: Boolean, optional: true },
         extraClasses: String,
         disabled: { type: Boolean, optional: true },
         label: { type: String, optional: true },
+        description: { type: String, optional: true },
         onChange: { Function, optional: true },
     };
     static defaultProps = {
@@ -15,13 +16,14 @@ export class Switch extends Component {
     };
     static template = xml`
     <label t-att-class="'o_switch' + extraClasses">
-        <input type="checkbox" t-att-checked="props.value" t-att-disabled="props.disabled" t-on-change="(ev) => props.onChange(ev.target.checked)"/>
+        <input type="checkbox" name="switch" t-att-checked="props.value" t-att-disabled="props.disabled" t-on-change="(ev) => props.onChange(ev.target.checked)"/>
         <span/>
         <span t-if="props.label" t-esc="props.label" class="ms-2"/>
+        <span t-if="props.description" class="text-muted ms-2" t-esc="props.description"/>
     </label>
     `;
 
     setup() {
-        this.extraClasses = this.props.extraClasses ? ` ${this.props.extraClasses}` : '';
+        this.extraClasses = this.props.extraClasses ? ` ${this.props.extraClasses}` : "";
     }
 }
