fce25cd8f2862dfb69704955654f3d53789fea21
adip-odoo <adip@odoo.com>
2025-06-13 07:20:10 +0000

    [FIX] product_expiry: prevent error when lot/serial has no expiration date
    When selecting a **Lot/Serial Number** that does not have an expiration date set,
    it raises an error due to an invalid comparison between a datetime and a bool.
    
    **Steps to reproduce:**
    - Install `mrp` and `product_expiry` modules.
    - Create a product with **Tracking** set to _By Lots_ and enable
     **Expiration Date** in the Inventory tab.
    - Create a Lot/Serial Number for this product without setting an expiration date.
    - In an unbuild order, select this product and try to assign the Lot/Serial Number.
    
    **Error:**
    `TypeError: '>=' not supported between instances of 'datetime.datetime' and 'bool'`
    
    Here, an error occurs at [1], where the system tries to compare a `datetime`
    with `lot.expiration_date`, which is unset (`False`).
    
    [1] - https://github.com/odoo/odoo/blob/9176a8e6fdb41caf575ec007fe17aa308bde7b4f/addons/product_expiry/models/production_lot.py#L31-L33
    
    This commit ensures that comparisons involving `expiration_date` and `alert_date`
    are only made when these fields are set.
    
    Sentry - 6676608121
    
    closes odoo/odoo#214924
    
    X-original-commit: 85ca4b84e9ef9d39f0b0eb9e94e172267cb365b6
    Signed-off-by: Tiffany Chang (tic) <tic@odoo.com>
    Signed-off-by: Aditi Patel (adip) <adip@odoo.com>

--- addons/product_expiry/models/production_lot.py
+++ addons/product_expiry/models/production_lot.py
@@ -26,11 +26,11 @@ class StockLot(models.Model):
     def _compute_display_name(self):
         lots_to_process_ids = []
         for lot in self:
-            if lot.env.context.get('formatted_display_name') and lot.use_expiration_date:
+            if lot.env.context.get('formatted_display_name') and lot.use_expiration_date and lot.expiration_date:
                 name = f"{lot.name}"
                 if fields.Datetime.now() >= lot.expiration_date:
                     name += self.env._("\t--Expired--")
-                elif fields.Datetime.now() >= lot.alert_date:
+                elif lot.alert_date and fields.Datetime.now() >= lot.alert_date:
                     name += self.env._("\t--Expire on %(date)s--", date=fields.Datetime.to_string(lot.expiration_date))
                 lot.display_name = name
             else:
