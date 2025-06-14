83ec6c68ba6e2c752077d5320289d4d86a462bac
Henry Yu <ddhenry809@hotmail.com>
2025-06-14 00:28:18 -0400

    [REM] base: Remove unused imports

--- odoo/addons/base/models/report_paperformat.py
+++ odoo/addons/base/models/report_paperformat.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError
+from odoo import fields, models
 
 # see http://doc.qt.io/archives/qt-4.8/qprinter.html#PaperSize-enum
 PAPER_SIZES = [
