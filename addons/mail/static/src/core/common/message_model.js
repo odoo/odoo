9867351622715241bd670c2a47e8520f91419733
aych-odoo <aych@odoo.com>
2025-06-16 12:52:22 +0000

    [FIX] mail: hide message actions on transient messages
    Before this commit, the toggle-star action was visible
    on chatbot messages in a non-persisted livechat thread.
    Clicking these actions caused errors.
    
    This commit hides this action when the message is in non-persisted state,
    preventing such errors.
    
    task-4743758
    
    X-original-commit: 69efda95fa98164c57efca044c82cea7782eec53

--- addons/mail/static/src/core/common/message_model.js
+++ addons/mail/static/src/core/common/message_model.js
@@ -421,7 +421,8 @@ export class Message extends Record {
                 !this.isPending &&
                 this.thread &&
                 this.store.self.type === "partner" &&
-                this.store.self.isInternalUser
+                this.store.self.isInternalUser &&
+                this.persistent
         );
     }
 
