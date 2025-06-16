e2601b76ed697f4a2c5357b0f7d4b56228594475
Augustin (duau) <duau@odoo.com>
2025-06-16 08:35:58 +0000

    [FIX] website: fix `s_numbers_charts` display and prevent crash
    The `s_numbers_charts` snippet had two main issues:
    - The progress bar within `s_numbers_charts` visually appeared as 45% on
      initial rendering, despite its actual configured value being 25%. This
    was due to a mismatch between its `aria-valuenow` attribute and its
    style `width` property. The display would correct itself when the user
    manually changed the value.
    
    - A crash would occur when attempting to select the "Label" option after
      dropping the `s_numbers_charts` snippet. This happened because
    `s_progress_bar_text` was forgotten during the initial implementation of
    `s_numbers_charts` [1].
    
    This commit addresses these problems by:
    - Adding the missing `s_progress_bar_text` to the `s_numbers_charts` template.
    - Including `s_progress_bar_text` in `_computeSnippetTemplates` to
      ensure that existing databases also receive this fix, as snippets are
    stored in the database.
    - Adding a check in the `progress` option to prevent the aforementioned crash.
    
    Steps to reproduce the crash:
    - Drop the `s_numbers_charts` snippet.
    - Click on a progress bar within the snippet.
    - Select the option `Label`.
    - Hover over "Hide" without any other prior interactions.
    - A crash will occur.
    
    [1]: https://github.com/odoo/odoo/commit/6c94fd66c1db75d74588ce670fb6cf1e960cf8bd
    
    closes odoo/odoo#216094
    
    X-original-commit: 0bf014af702662016cbcc285bbdc5809a7bedb8a
    Signed-off-by: Benoit Socias (bso) <bso@odoo.com>
    Signed-off-by: Augustin Dupin (duau) <duau@odoo.com>

--- addons/website/static/src/js/editor/snippets.editor.js
+++ addons/website/static/src/js/editor/snippets.editor.js
@@ -217,6 +217,24 @@ export class WebsiteSnippetsMenu extends weSnippetEditor.SnippetsMenu {
      */
     _computeSnippetTemplates(html) {
         const $html = $(html);
+
+        // TODO remove in master: add s_progress_bar_text in progress bar where it's missing, fix the previous wrong width
+        const progressBarEls = html.querySelectorAll(".progress-bar");
+        progressBarEls.forEach((el) => {
+            if (el.style.width === "45%") {
+                el.style.width = "25%";
+            }
+            if (!el.querySelector(".s_progress_bar_text")) {
+                const textEl = document.createElement("span");
+                textEl.classList.add("s_progress_bar_text", "small");
+                textEl.textContent = el.style.width;
+                if (el.closest(".s_progress_bar_label_hidden")) {
+                    textEl.classList.add("d-none");
+                }
+                el.appendChild(textEl);
+            }
+        });
+
         const toFind = $html.find("we-fontfamilypicker[data-variable]").toArray();
         const fontVariables = toFind.map((el) => el.dataset.variable);
         FontFamilyPickerUserValueWidget.prototype.fontVariables = fontVariables;
