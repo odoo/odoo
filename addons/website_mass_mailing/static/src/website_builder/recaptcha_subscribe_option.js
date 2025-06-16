7a31c7e6ef59912d6c0d53cb5fda393b9ce5e2a0
Benoit Socias <bso@odoo.com>
2025-06-16 09:24:17 +0200

    [FIX] website[_mass_mailing]: fix template name prefixes
    When [1] introduced `html_builder`, some templates were moved across
    modules but their names were not adapted accordingly.
    
    This commit adjusts the template names so that they reflect the addon
    within which they are defined.
    
    [1]: https://github.com/odoo/odoo/commit/9fe45e2b7ddbbfd0445ffe25a859e67a316d02b2
    
    task-4367641
    
    closes odoo/odoo#214306
    
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/website_mass_mailing/static/src/website_builder/recaptcha_subscribe_option.js
+++ addons/website_mass_mailing/static/src/website_builder/recaptcha_subscribe_option.js
@@ -1,7 +1,7 @@
 import { BaseOptionComponent } from "@html_builder/core/utils";
 
 export class RecaptchaSubscribeOption extends BaseOptionComponent {
-    static template = "html_builder.RecaptchaSubscribeOption";
+    static template = "website_mass_mailing.RecaptchaSubscribeOption";
     static props = {
         hasRecaptcha: Function,
     };
