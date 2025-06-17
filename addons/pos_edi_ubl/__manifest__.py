7cf4f93cd532841140ca1cfbc1fb57bf23bb81c1
Antoine Dupuis <andu@odoo.com>
2025-06-17 14:08:03 +0000

    [ADD] pos_edi_ubl: Helpers to generate UBL in the PoS
    We introduce a new model `pos.edi.xml.ubl_21` which can be inherited
    to build UBL 2.1 Invoice and CreditNote documents from a PoS order.
    
    This generates the UBL document using sensible default values taken from
    the PoS order but can also be inherited. This calls methods defined in
    the `account.edi.xml.ubl_21` model and so can be composed with models
    that inherit from `account.edi.xml.ubl_21`.
    
    task-4038651
    
    X-original-commit: 40404fe9b2437987b45388896417a38fa9697b10

---
+++ addons/pos_edi_ubl/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Point of Sale UBL',
+    'version': '1.0',
+    'category': 'Sales/Point of Sale',
+    'sequence': 6,
+    'summary': 'UBL in the Point of Sale ',
+    'description': """
+
+This module provides helpers for exporting POS orders in various UBL 2.1 formats.
+
+""",
+    'depends': ['point_of_sale', 'account_edi_ubl_cii'],
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
