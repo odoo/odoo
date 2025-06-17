8510ebcd7b96cfb469ead28803cc0f39cb1e3e08
roen-odoo <roen@odoo.com>
2025-06-17 11:59:27 +0000

    [FIX] point_of_sale: update partner self invoice
    When doing a self invoice with the QR code on the PoS receipt, if a
    partner was linked to the order, the record would not be updated with
    the values entered in the self invoice form.
    
    Steps to reproduce:
    -------------------
    * Enable the self service invoice feature in the PoS settings.
    * Open a PoS session.
    * Make and order with a partner set on it.
    * Finalize the order and scan the QRCode on the receipt.
    * Fill the form with some new informations (like a new VAT number)
    > Observation: Check the partner on the backend, the values are not
      updated.
    
    Why the fix:
    ------------
    We update all the mandatory and optionnal fields of the partner
    record with the values entered in the self invoice form.
    
    opw-4676244
    
    closes odoo/odoo#217262
    
    X-original-commit: 4432b2f60aff39a4cca65a31c4e42fa3c3acd9cf
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Robin Engels (roen) <roen@odoo.com>

--- addons/point_of_sale/tests/test_pos_controller.py
+++ addons/point_of_sale/tests/test_pos_controller.py
@@ -150,3 +150,57 @@ class TestPoSController(TestPointOfSaleHttpCommon):
         self.main_pos_config.current_session_id.close_session_from_ui()
         self.start_tour('/pos/ticket', 'invoicePoSOrderWithSelfInvocing', login=None)
         self.assertTrue(self.pos_order.account_move, "The pos order should have an invoice after self invoicing")
+
+    def test_qr_code_receipt_user_updated(self):
+        """This test make sure that when the user is already connected he correctly gets redirected to the invoice."""
+        self.authenticate(None, None)
+        self.partner_1 = self.env['res.partner'].create({
+            'name': 'Valid Lelitre',
+            'email': 'valid.lelitre@agrolait.com',
+        })
+
+        self.product1 = self.env['product.product'].create({
+            'name': 'Test Product 1',
+            'is_storable': True,
+            'list_price': 10.0,
+            'taxes_id': False,
+        })
+        self.main_pos_config.open_ui()
+        self.pos_order = self.env['pos.order'].create({
+            'session_id': self.main_pos_config.current_session_id.id,
+            'company_id': self.env.company.id,
+            'partner_id': self.partner_1.id,
+            'access_token': '1234567890',
+            'lines': [(0, 0, {
+                'name': "OL/0001",
+                'product_id': self.product1.id,
+                'price_unit': 10,
+                'discount': 0.0,
+                'qty': 1.0,
+                'tax_ids': False,
+                'price_subtotal': 10,
+                'price_subtotal_incl': 10,
+            })],
+            'amount_tax': 10,
+            'amount_total': 10,
+            'amount_paid': 10.0,
+            'amount_return': 10.0,
+        })
+        self.main_pos_config.current_session_id.close_session_from_ui()
+        get_invoice_data = {
+            'access_token': self.pos_order.access_token,
+            'name': 'New Name',
+            'email': "test@test.com",
+            'vat': 'VAT_TEST_NUMBER_123',
+            'street': "Test street",
+            'city': "Test City",
+            'zipcode': '12345',
+            'country_id': self.company.country_id.id,
+            'phone': "123456789",
+            'state_id': self.env['res.country.state'].search([], limit=1).id,
+            'csrf_token': odoo.http.Request.csrf_token(self)
+        }
+        self.url_open(f'/pos/ticket/validate?access_token={self.pos_order.access_token}', data=get_invoice_data, timeout=30000)
+        self.assertEqual(self.partner_1.vat, 'VAT_TEST_NUMBER_123')
+        self.assertEqual(self.partner_1.name, 'New Name')
+        self.assertEqual(self.partner_1.zip, '12345')
