25aa224f6279d991aaeff65572932e34d55a6f06
Laurent Smet <las@odoo.com>
2025-06-17 13:18:46 +0000

    [IMP] account, account_tax_python: Move taxes helpers into AccountTestInvoicingCommon
    closes odoo/odoo#220012
    
    X-original-commit: 17a11b29556ab7c9c38c07bd30ad870c4bd11f14
    Related: odoo/enterprise#90730
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- addons/account_tax_python/tests/common.py
+++ addons/account_tax_python/tests/common.py
@@ -3,23 +3,6 @@ from odoo.addons.account.tests.test_tax import TestTaxCommon
 
 class TestTaxCommonAccountTaxPython(TestTaxCommon):
 
-    def python_tax(self, formula, **kwargs):
-        self.number += 1
-        vals = {
-            **kwargs,
-            'name': f"code_({self.number})",
-            'amount_type': 'code',
-            'amount': 0.0,
-            'formula': formula,
-        }
-        if 'price_include' in vals:
-            price_include = vals.pop('price_include')
-            if self.env.company.account_price_include != price_include:
-                vals['price_include_override'] = price_include
-            else:
-                vals['price_include_override'] = False
-        return self.env['account.tax'].create(vals)
-
     def _jsonify_tax(self, tax):
         values = super()._jsonify_tax(tax)
         values['formula_decoded_info'] = tax.formula_decoded_info
@@ -31,9 +14,9 @@ class TestTaxCommonAccountTaxPython(TestTaxCommon):
         price_unit,
         expected_values,
         product_values=None,
-        price_include='tax_excluded',
+        price_include_override='tax_excluded',
     ):
-        tax = self.python_tax(formula, price_include=price_include)
+        tax = self.python_tax(formula, price_include_override=price_include_override)
         if product_values:
             product = self.env['product.product'].create({
                 'name': "assert_python_taxes_computation",
