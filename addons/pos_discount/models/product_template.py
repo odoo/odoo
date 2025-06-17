d5e93b6308f4d897b85316cba45a6ec2e7bff5c2
Pedram (PEBR) <pebr@odoo.com>
2025-06-17 12:03:00 +0000

    [FIX] pos_discount: correctly load the Discount product
    Before this commit, the product.product record was loaded for the
    Discount product when loading the product templates, which caused
    several issues.
    
    opw-4876402
    
    closes odoo/odoo#214659
    
    X-original-commit: 9cfae15aeb082e0d7e877e4cf1b75f9e24e5332c
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/pos_discount/models/product_template.py
+++ addons/pos_discount/models/product_template.py
@@ -7,11 +7,11 @@ class ProductTemplate(models.Model):
     def _load_pos_data(self, data):
         res = super()._load_pos_data(data)
         config_id = self.env['pos.config'].browse(data['pos.config'][0]['id'])
-        discount_product_id = config_id.discount_product_id.id
+        discount_product_id = config_id.discount_product_id.product_tmpl_id.id
         product_ids_set = {product['id'] for product in res}
 
         if config_id.module_pos_discount and discount_product_id not in product_ids_set:
-            productModel = self.env['product.product'].with_context({**self.env.context, 'display_default_code': False})
+            productModel = self.env['product.template'].with_context({**self.env.context, 'display_default_code': False})
             fields = self.env['product.template']._load_pos_data_fields(data['pos.config'][0]['id'])
             product = productModel.search_read([('id', '=', discount_product_id)], fields=fields, load=False)
             res.extend(product)
