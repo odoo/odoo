0ca692faf4730e66f74d89104052f46c4fedb206
Laurent Smet <las@odoo.com>
2025-06-17 13:18:46 +0000

    [IMP] account, account_tax_python: Move taxes helpers into AccountTestInvoicingCommon
    closes odoo/odoo#217767
    
    X-original-commit: 717676f61d34afb2443ce531bbf929f8271f03c7
    Related: odoo/enterprise#89658
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- addons/account/tests/common.py
+++ addons/account/tests/common.py
@@ -408,6 +408,20 @@ class AccountTestInvoicingCommon(ProductCommon):
             'amount': amount,
         })
 
+    def python_tax(self, formula, **kwargs):
+        account_tax_python = self.env['ir.module.module']._get('account_tax_python')
+        if account_tax_python.state != 'installed':
+            raise SkipTest("Module 'account_tax_python' is not installed!")
+
+        self.tax_number += 1
+        return self.env['account.tax'].create({
+            **kwargs,
+            'name': f"code_({self.tax_number})",
+            'amount_type': 'code',
+            'amount': 0.0,
+            'formula': formula,
+        })
+
     @classmethod
     def setup_armageddon_tax(cls, tax_name, company_data, **kwargs):
         type_tax_use = kwargs.get('type_tax_use', 'sale')
