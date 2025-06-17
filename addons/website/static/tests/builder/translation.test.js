9fd037a7153752cf5973839f53c0ebb64253c231
Garvish Panchal <gppa@odoo.com>
2025-06-17 17:14:01 +0530

    [FIX] website: remove powerbutton plugin in translation mode
    This commit removes the powerbutton plugin from the website in
    translation mode. The powerbutton plugin was added after the
    refactoring in master, but it is not needed in translation mode as it
    does not serve a purpose there.

--- addons/website/static/tests/builder/translation.test.js
+++ addons/website/static/tests/builder/translation.test.js
@@ -6,6 +6,7 @@ import { beforeEach, describe, expect, test } from "@odoo/hoot";
 import { animationFrame, manuallyDispatchProgrammaticEvent, queryAllTexts } from "@odoo/hoot-dom";
 import { contains, mockService, onRpc, patchWithCleanup } from "@web/../tests/web_test_helpers";
 import { defineWebsiteModels, invisibleEl, setupWebsiteBuilder } from "./website_helpers";
+import { expectElementCount } from "@html_editor/../tests/_helpers/ui_expectations";
 
 defineWebsiteModels();
 
@@ -184,6 +185,21 @@ test("translate select", async () => {
     expect(queryAllTexts(":iframe [data-initial-translation-value='Option 1']")).toEqual([
         "Option fr",
     ]);
+});
+
+test("test that powerbox should not open in translate mode", async () => {
+    const { getEditor } = await setupSidebarBuilderForTranslation({
+        websiteContent: getTranslateEditable("&nbsp;"),
+    });
+    const editor = getEditor();
+    const textNode = editor.editable.querySelector("span").firstChild;
+    expect(textNode.nodeType).toBe(Node.TEXT_NODE);
+    setSelection({ anchorNode: textNode, anchorOffset: 0 });
+    await animationFrame();
+    // Simulate typing `/`
+    await insertText(editor, "/");
+    await animationFrame();
+    await expectElementCount(".o-we-powerbox", 0);
 });
 
 describe("save translation", () => {
