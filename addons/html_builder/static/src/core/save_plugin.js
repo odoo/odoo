0f1cd2cc59fe779aa0cfae8620e6ce05b689b4cf
Géry Debongnie <ged@odoo.com>
2025-06-17 14:42:55 +0200

    [FIX] html_builder: properly escape text nodes as before
    Before the recent refactoring of the website builder to owl, the text
    nodes contained in html were doubly escaped. Once because the html is
    serialized, and once more because we did it explicitely in code. This
    double escaping is somehow necessary to the server. The resulting views
    sent back to the website will have the correct escaping.
    
    This commit restores the previous behaviour, to avoid people
    unintentionally injecting html into their website.  However, it feels
    like a proper fix would be to change the server code to correctly
    process the text content. But we need a quick fix for this.
    
    When a proper fix is done, the test introduced in this commit will
    break and will have to be slightly adapted.
    
    closes odoo/odoo#214579
    
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/html_builder/static/src/core/save_plugin.js
+++ addons/html_builder/static/src/core/save_plugin.js
@@ -1,4 +1,5 @@
 import { Plugin } from "@html_editor/plugin";
+import { withSequence } from "@html_editor/utils/resource";
 import { rpc } from "@web/core/network/rpc";
 import { registry } from "@web/core/registry";
 
@@ -26,6 +27,7 @@ export class SavePlugin extends Plugin {
             //     clean DOM before save (leaving edit mode)
             //     root is the clone of a node that was o_dirty
             // }
+            withSequence(99, ({ root }) => this.escapeTextNodes(root)),
         ],
         save_element_handlers: [
             // async (el) => {
@@ -78,6 +80,30 @@ export class SavePlugin extends Plugin {
         const willSaves = this.getResource("save_handlers").map((c) => c());
         await Promise.all(saveProms.concat(willSaves));
         this.lastSavedStep = this.dependencies.history.getHistorySteps().at(-1);
+    }
+
+    escapeTextNodes(el) {
+        const walker = document.createTreeWalker(el, NodeFilter.SHOW_ALL, (node) => {
+            if (
+                node.nodeType === Node.ELEMENT_NODE &&
+                (node.matches("object,iframe,script,style") ||
+                    (node.hasAttribute("data-oe-model") &&
+                        node.getAttribute("data-oe-model") !== "ir.ui.view"))
+            ) {
+                return NodeFilter.FILTER_REJECT; // Skip this node and its descendants
+            }
+            if (node.nodeType === Node.TEXT_NODE) {
+                return NodeFilter.FILTER_ACCEPT;
+            }
+            return NodeFilter.FILTER_SKIP; // Skip other nodes, but visit their children
+        });
+
+        const escaper = document.createElement("div");
+        let node;
+        while ((node = walker.nextNode())) {
+            escaper.textContent = node.nodeValue;
+            node.nodeValue = escaper.innerHTML;
+        }
     }
 
     isAlreadySaved() {
