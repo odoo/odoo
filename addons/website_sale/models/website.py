9d1980d668337365c8478e09afed03e9de50e0c5
Lionel Piraux (lipi) <lipi@odoo.com>
2025-06-17 11:27:16 +0200

    [FIX] website_sale(,_stock): add cta when sending via cron
    The recovery email send automatically via its related cron does not
    include the Call To Action (CTA) button to view the cart on the website.
    
    This commit adds a key in the context to show the CTA button only via
    the cron as it is already displayed in the email header when sending it
    via the action on the sale order.

--- addons/website_sale/models/website.py
+++ addons/website_sale/models/website.py
@@ -750,7 +750,9 @@ class Website(models.Model):
                 email_vals = {} if template.email_to or template.partner_to else {
                     'email_to': sale_order.partner_id.email_formatted
                 }
-                template.send_mail(sale_order.id, email_values=email_vals)
+                template.with_context(
+                    show_recovery_button=True,
+                ).send_mail(sale_order.id, email_values=email_vals)
                 sale_order.cart_recovery_email_sent = True
 
     @api.model_create_multi
