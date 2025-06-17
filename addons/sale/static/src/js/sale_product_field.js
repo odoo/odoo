527f7acb270dd52efee0bcc3ba858b5cfcd0d475
Lionel Piraux (lipi) <lipi@odoo.com>
2025-06-17 13:25:49 +0200

    [FIX] sale: missing field dependency
    After [1], the translated product name was missing from the view,
    resulting in the SOL description displaying the product name twice since
    it couldn't be pruned in the displayed description.
    
    This commit simply adds the missing field dependency, ensuring the ORM
    fetches the field to the client.
    
    This commit also fixes an issue from [2] when the SOL description did
    not include a newline after the translated product name, which caused it
    to show unexpectedly.
    
    opw-4760300
    
    ---
    
    1. https://github.com/odoo/odoo/pull/214099
    2. https://github.com/odoo/odoo/pull/209141
    
    closes odoo/odoo#214571
    
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/sale/static/src/js/sale_product_field.js
+++ addons/sale/static/src/js/sale_product_field.js
@@ -134,10 +134,12 @@ export class SaleOrderLineProductField extends ProductLabelSectionAndNoteField {
     }
 
     get label() {
-        let label = super.label;
+        let label = this.props.record.data.name;
         if (this.translatedProductName && label.startsWith(this.translatedProductName)) {
             // Remove the translated name as it is already shown to the salesman on the SOL.
-            label = label.slice(label.indexOf("\n") + 1);
+            label = label.slice(this.translatedProductName.length + 1);  // + "\n"
+        } else {
+            label = super.label;
         }
         return label;
     }
@@ -147,15 +149,13 @@ export class SaleOrderLineProductField extends ProductLabelSectionAndNoteField {
     }
 
     updateLabel(value) {
-        if (this.translatedProductName === undefined) {
-            // View was not updated to include `translatedProductName`
+        if (!this.translatedProductName) {
             return super.updateLabel(value);
         }
         this.props.record.update({
             name: (
-                this.translatedProductName && value && this.translatedProductName.concat("\n", value)
-                || !value && this.translatedProductName
-                || value
+                value && this.translatedProductName.concat("\n", value)
+                || this.translatedProductName
             ),
         });
     }
