5c0718b1a38993064a553969d9ffaa84924360c7
Youssef <abyo@odoo.com>
2025-06-17 10:22:56 +0200

    [IMP] mrp: allow in progress workorders to move workcenters
    - Allow workorders that are still in progress to be moved to any other workcenter.
    - Timer doesn't get reset when moved from a workcenter to another therefore blocking them from moving while
      in progress is unnecessary.
    
     Task:4850003

--- addons/mrp/models/mrp_workorder.py
+++ addons/mrp/models/mrp_workorder.py
@@ -490,9 +490,11 @@ class MrpWorkorder(models.Model):
             new_workcenter = self.env['mrp.workcenter'].browse(values['workcenter_id'])
             for workorder in self:
                 if workorder.workcenter_id.id != values['workcenter_id']:
-                    if workorder.state in ('progress', 'done', 'cancel'):
-                        raise UserError(_('You cannot change the workcenter of a work order that is in progress or done.'))
+                    if workorder.state in ('done', 'cancel'):
+                        raise UserError(_('You cannot change the workcenter of a work order that is done.'))
                     workorder.leave_id.resource_id = new_workcenter.resource_id
+                    if workorder.state == 'progress':
+                        continue
                     workorder.duration_expected = workorder._get_duration_expected()
                     if workorder.date_start:
                         workorder.date_finished = workorder._calculate_date_finished(new_workcenter=new_workcenter)
