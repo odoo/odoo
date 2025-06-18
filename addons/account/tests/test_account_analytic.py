d380a3a42dc39451fa26f161cc0b955b2f0098fd
Léo Gizard <legi@odoo.com>
2025-06-18 09:28:26 +0000

    [FIX] account: analytic lines rounding error
    **PROBLEM**
    Sometimes, the sum of the generated analytic lines for an invoice line doesn't equal the amount on the invoice line.
    For example, in invoice line with a price of 182.25, with an analytic distribution split into 98% and 2%, the generated analytic lines amount to 182.26 (off by 0.01) because of rounding.
    
    **STEP TO REPRODUCE**
    1. install the accounting module and enable the Analytical Accounting option.
    2. create an invoice, with a line with a price of 182.25, and a distribution of 98%/2%.
    3. confirm the invoice.
    4. go to Accounting/Analytics Items and notice the sum of analytical line is 182.26 instead of 182.25.
    
    **CAUSE**
    We only apply rounding after having calculated all the analytic line amounts.
    This mean we will sum the rounding error.
    In our example, the computation is like so:
    98% of 182.25 = 178.605 rounded to 178.61 (python should round it to 178.60 because round() use the half to even rounding method, but it doesn't because of float error).
    2% of 182.25 = 3.645 rounded to 3.65 (same as above)
    178.61 + 3.65 = 182.26
    
    **FIX**
    We compute the last analytic line for each plan, relatively to the other.
    `last_line_amount = invoice_price - sum(rounded_other_line_amount)`
    This ensure that the sum of analytic lines is always equal to the invoice price.
    
    opw-4848784
    
    closes odoo/odoo#220388
    
    X-original-commit: cf8d38205c09a2724f41907fa07c5f23ff2d46a3
    Signed-off-by: Thomas Becquevort (thbe) <thbe@odoo.com>
    Signed-off-by: Léo Gizard (legi) <legi@odoo.com>
    Co-authored-by: Maira Salazar <47562677+mairasalazar@users.noreply.github.com>

--- addons/account/tests/test_account_analytic.py
+++ addons/account/tests/test_account_analytic.py
@@ -13,12 +13,39 @@ class TestAccountAnalyticAccount(AccountTestInvoicingCommon, AnalyticCommon):
         super().setUpClass()
         cls.company_data_2 = cls.setup_other_company()
 
+        cls.env.user.groups_id += cls.env.ref('analytic.group_analytic_accounting')
+
+        # By default, tests are run with the current user set on the first company.
+        cls.env.user.company_id = cls.company_data['company']
+
+        cls.default_plan = cls.env['account.analytic.plan'].create({'name': 'Default'})
+        cls.analytic_account_a = cls.env['account.analytic.account'].create({
+            'name': 'analytic_account_a',
+            'plan_id': cls.default_plan.id,
+            'company_id': False,
+        })
+        cls.analytic_account_b = cls.env['account.analytic.account'].create({
+            'name': 'analytic_account_b',
+            'plan_id': cls.default_plan.id,
+            'company_id': False,
+        })
+        cls.analytic_account_d = cls.env['account.analytic.account'].create({
+            'name': 'analytic_account_d',
+            'plan_id': cls.default_plan.id,
+            'company_id': False,
+        })
+
         cls.cross_plan = cls.env['account.analytic.plan'].create({'name': 'Cross'})
         cls.analytic_account_5 = cls.env['account.analytic.account'].create({
             'name': 'analytic_account_5',
             'plan_id': cls.cross_plan.id,
             'company_id': False,
         })
+
+    def get_analytic_lines(self, invoice):
+        return self.env['account.analytic.line'].search([
+            ('move_line_id', 'in', invoice.line_ids.ids),
+        ]).sorted('amount')
 
     def create_invoice(self, partner, product):
         return self.env['account.move'].create([{
@@ -50,10 +77,6 @@ class TestAccountAnalyticAccount(AccountTestInvoicingCommon, AnalyticCommon):
 
     def test_analytic_lines(self):
         ''' Ensures analytic lines are created when posted and are recreated when editing the account.move'''
-        def get_analytic_lines():
-            return self.env['account.analytic.line'].search([
-                ('move_line_id', 'in', out_invoice.line_ids.ids)
-            ]).sorted('amount')
 
         out_invoice = self.env['account.move'].create([{
             'move_type': 'out_invoice',
@@ -73,7 +96,7 @@ class TestAccountAnalyticAccount(AccountTestInvoicingCommon, AnalyticCommon):
         out_invoice.action_post()
 
         # Analytic lines are created when posting the invoice
-        self.assertRecordValues(get_analytic_lines(), [{
+        self.assertRecordValues(self.get_analytic_lines(out_invoice), [{
             'amount': 100,
             self.analytic_plan_2._column_name(): self.analytic_account_4.id,
             'partner_id': self.partner_a.id,
@@ -90,7 +113,7 @@ class TestAccountAnalyticAccount(AccountTestInvoicingCommon, AnalyticCommon):
             self.analytic_account_3.id: 100,
             self.analytic_account_4.id: 25,
         }
-        self.assertRecordValues(get_analytic_lines(), [{
+        self.assertRecordValues(self.get_analytic_lines(out_invoice), [{
             'amount': 50,
             self.analytic_plan_2._column_name(): self.analytic_account_4.id,
         }, {
@@ -100,7 +123,94 @@ class TestAccountAnalyticAccount(AccountTestInvoicingCommon, AnalyticCommon):
 
         # Analytic lines are deleted when resetting to draft
         out_invoice.button_draft()
-        self.assertFalse(get_analytic_lines())
+        self.assertFalse(self.get_analytic_lines(out_invoice))
+
+    def test_analytic_lines_rounding(self):
+        """ Ensures analytic lines rounding errors are spread across all lines, in such a way that summing them gives the right amount.
+        For example, when distributing 100% of the the price, the sum of analytic lines should be exactly equal to the price. """
+
+        # in this scenario,
+        # 94% of 182.25 = 171.315 rounded to 171.32
+        # 2% of 182.25 = 3.645 rounded to 3.65
+        # 3 * 3.65 + 171.32 = 182.27
+        # we remove 0.01 to two lines to counter the rounding errors.
+        out_invoice = self.env['account.move'].create([{
+            'move_type': 'out_invoice',
+            'partner_id': self.partner_a.id,
+            'date': '2017-01-01',
+            'invoice_date': '2017-01-01',
+            'invoice_line_ids': [Command.create({
+                'product_id': self.product_a.id,
+                'price_unit': 182.25,
+                'analytic_distribution': {
+                    self.analytic_account_a.id: 94,
+                    self.analytic_account_b.id: 2,
+                    self.analytic_account_5.id: 2,
+                    self.analytic_account_d.id: 2,
+                },
+            })]
+        }])
+
+        out_invoice.action_post()
+
+        self.assertRecordValues(self.get_analytic_lines(out_invoice), [
+            {
+                'amount': 3.64,
+                self.default_plan._column_name(): self.analytic_account_b.id,
+                self.cross_plan._column_name(): None,
+            },
+            {
+                'amount': 3.65,
+                self.default_plan._column_name(): self.analytic_account_d.id,
+                self.cross_plan._column_name(): None,
+            },
+            {
+                'amount': 3.65,
+                self.default_plan._column_name(): None,
+                self.cross_plan._column_name(): self.analytic_account_5.id,
+            },
+            {
+                'amount': 171.31,
+                self.default_plan._column_name(): self.analytic_account_a.id,
+                self.cross_plan._column_name(): None,
+            },
+        ])
+
+        out_invoice.button_draft()
+        # in this scenario,
+        # 25% of 182.25 = 45.5625 rounded to 45.56
+        # 45.56 * 4 = 182.24
+        # we add 0.01 to one of the line to counter the rounding errors.
+        out_invoice.invoice_line_ids[0].analytic_distribution = {
+            self.analytic_account_a.id: 25,
+            self.analytic_account_b.id: 25,
+            self.analytic_account_5.id: 25,
+            self.analytic_account_d.id: 25,
+        }
+        out_invoice.action_post()
+
+        self.assertRecordValues(self.get_analytic_lines(out_invoice), [
+            {
+                'amount': 45.56,
+                self.default_plan._column_name(): self.analytic_account_d.id,
+                self.cross_plan._column_name(): None,
+            },
+            {
+                'amount': 45.56,
+                self.default_plan._column_name(): None,
+                self.cross_plan._column_name(): self.analytic_account_5.id,
+            },
+            {
+                'amount': 45.56,
+                self.default_plan._column_name(): self.analytic_account_b.id,
+                self.cross_plan._column_name(): None,
+            },
+            {
+                'amount': 45.57,
+                self.default_plan._column_name(): self.analytic_account_a.id,
+                self.cross_plan._column_name(): None,
+            },
+        ])
 
     def test_model_score(self):
         """Test that the models are applied correctly based on the score"""
