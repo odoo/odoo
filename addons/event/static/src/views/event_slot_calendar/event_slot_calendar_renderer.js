8bf3917367915d500396956346ccbfa3d0d0d724
amdi-odoo <amdi@odoo.com>
2025-06-18 13:03:08 +0200

    [IMP] event: improve slot calendar
    Purpose
    =======
    Improve the event slot calendar view.
    
    Specification
    =============
    - Improve the slot form view by displaying the hour range
    on a single row.
    
    - Improve the slot calendar mobile view by opening the list view
    by default, improving the quick create form and hide the filters
    sidebar if there are no filters.
    
    - Ease the slots creation in batch by adding a greyed-out overlay
    to prevent creation outside of the event time range.
    
    - Preserve the slot duration in the multi create by automatically
    adjusting the end time when updating the start time.
    
    - Prevent selection over multiple days as slots cannot go
    over multiple days.
    
    - Adding a title above the slots multi creation form to make
    sure users understand what those fields are about.
    
    Task-4743999

--- addons/event/static/src/views/event_slot_calendar/event_slot_calendar_renderer.js
+++ addons/event/static/src/views/event_slot_calendar/event_slot_calendar_renderer.js
@@ -2,9 +2,51 @@ import { CalendarCommonRenderer } from "@web/views/calendar/calendar_common/cale
 import { CalendarRenderer } from "@web/views/calendar/calendar_renderer";
 import { CalendarYearRenderer } from "@web/views/calendar/calendar_year/calendar_year_renderer";
 
+import { useEffect } from "@odoo/owl";
+
 export class EventSlotCalendarCommonRenderer extends CalendarCommonRenderer {
     // Display end time and hide title on the full calendar library event.
     static eventTemplate = "event.EventSlotCalendarCommonRenderer.event";
+
+    setup() {
+        super.setup(...arguments);
+        this.rangeStartDate = this.props.model.meta.context.event_start_date.split(' ')[0];
+        this.rangeEndDate = this.props.model.meta.context.event_end_date.split(' ')[0];
+        useEffect(
+            (fcEls) => {
+                // Add overlay to disable days outside of event time range.
+                for (const fcEl of fcEls) {
+                    fcEl.querySelectorAll(".fc-day:not(.fc-col-header-cell)").forEach((dayEl) => {
+                        if (dayEl.dataset.date < this.rangeStartDate || dayEl.dataset.date > this.rangeEndDate) {
+                            // 'pe-none' to disable month view multi records creation
+                            // 'o_event_day_disabled' class to disable single record creation
+                            dayEl.classList.add("bg-secondary", "bg-opacity-25", "pe-none", "o_event_disabled_day");
+                        }
+                    });
+                }
+            },
+            () => [[this.fc.el]],
+        );
+    }
+
+    /**
+     * @override
+     * Slots cannot be created over multiple days.
+     */
+    isSelectionAllowed(event) {
+        return false;
+    }
+
+    /**
+     * @override
+     * Prevent click on disabled dates.
+    */
+    onDateClick(info) {
+        if (info.dayEl.classList.contains("o_event_disabled_day")) {
+            return;
+        }
+        return super.onDateClick(info);
+    }
 }
 
 export class EventSlotCalendarRenderer extends CalendarRenderer {
