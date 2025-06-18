f7b359da1163a0199bf825bc54adcfdd815aafeb
Yannick Tivisse <yti@odoo.com>
2025-06-18 15:40:27 +0200

    [IMP] hr: Use standard html help field instead of custom JS
    closes odoo/odoo#214810
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/hr_org_chart/static/src/views/hr_employee_hierarchy/hr_employee_hierarchy_view.js
+++ addons/hr_org_chart/static/src/views/hr_employee_hierarchy/hr_employee_hierarchy_view.js
@@ -2,11 +2,9 @@ import { registry } from "@web/core/registry";
 
 import { hierarchyView } from "@web_hierarchy/hierarchy_view";
 import { HrEmployeeHierarchyRenderer } from "./hr_employee_hierarchy_renderer";
-import { HrEmployeeHierarchyController } from "./hr_employee_hierarchy_controller";
 
 export const hrEmployeeHierarchyView = {
     ...hierarchyView,
-    Controller: HrEmployeeHierarchyController,
     Renderer: HrEmployeeHierarchyRenderer,
 };
 
