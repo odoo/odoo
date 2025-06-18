75695eb5e37eda1ed6c37f224e209dd11a35b05f
Yannick Tivisse <yti@odoo.com>
2025-06-18 15:40:27 +0200

    [IMP] hr: Use standard html help field instead of custom JS
    closes odoo/odoo#214810
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/hr/static/src/views/list_view.js
+++ addons/hr/static/src/views/list_view.js
@@ -1,11 +1,9 @@
-import { registry } from "@web/core/registry";
+import { registry } from '@web/core/registry';
 
-import { listView } from "@web/views/list/list_view";
-import { ListController } from "@web/views/list/list_controller";
-import { ListRenderer } from "@web/views/list/list_renderer";
+import { listView } from '@web/views/list/list_view';
+import { ListController } from '@web/views/list/list_controller';
 
-import { useArchiveEmployee } from "@hr/views/archive_employee_hook";
-import { patchHrEmployee } from "./patch_hr_employee";
+import { useArchiveEmployee } from '@hr/views/archive_employee_hook';
 
 export class EmployeeListController extends ListController {
     setup() {
@@ -19,8 +17,8 @@ export class EmployeeListController extends ListController {
 
         menuItems.archive.callback = this.archiveEmployee.bind(
             this,
-            selectedRecords.map(({ resId }) => resId)
-        );
+            selectedRecords.map(({resId}) => resId),
+        )
         return menuItems;
     }
 
@@ -29,13 +27,7 @@ export class EmployeeListController extends ListController {
     }
 }
 
-export class EmployeeListRenderer extends ListRenderer {
-    static template = "hr.ListRenderer";
-}
-patchHrEmployee(EmployeeListRenderer);
-
-registry.category("views").add("hr_employee_list", {
+registry.category('views').add('hr_employee_list', {
     ...listView,
     Controller: EmployeeListController,
-    Renderer: EmployeeListRenderer,
 });
