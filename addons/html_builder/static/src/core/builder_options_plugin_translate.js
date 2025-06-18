2db28fad959aadba5e4a18f35aa2276bb6e1472e
Serhii Rubanskyi <seru@odoo.com>
2025-06-18 14:12:37 +0000

    [FIX] html_builder: make visibility work while translating
    Before this commit it would crash when you clicked on the visibility
    button in the invisible elements section while translating a website.
    To reproduce the issue:
    1. Create a multilanguage website
    2. Drop a popup snippet
    3. Switch to a secondary language, click edit and select "translate"
    4. Click on the eye in the invisible elements panel below
    
    => We have an error.
    This commit follows [the html_builder refactoring].
    
    [the html_builder refactoring]: odoo/odoo@9fe45e2b7ddb
    
    Related to task-4367641
    
    closes odoo/odoo#215987
    
    X-original-commit: d82c07a12604babf715bbd041a082371146ae295
    Signed-off-by: Colin Louis (loco) <loco@odoo.com>

--- addons/html_builder/static/src/core/builder_options_plugin_translate.js
+++ addons/html_builder/static/src/core/builder_options_plugin_translate.js
@@ -2,8 +2,9 @@ import { Plugin } from "@html_editor/plugin";
 
 export class BuilderOptionsPlugin extends Plugin {
     static id = "builderOptions";
-    static shared = ["deactivateContainers", "getTarget"];
+    static shared = ["deactivateContainers", "getTarget", "updateContainers"];
 
     deactivateContainers() {}
     getTarget() {}
+    updateContainers() {}
 }
