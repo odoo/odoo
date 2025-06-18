a61a7d3e55ebdaa84087300e499a39b48f6edccb
Guillaume Jacquet <guja@odoo.com>
2025-06-18 14:19:35 +0000

    [FIX] html_builder: added tooltip popover when clicking on an image
    problem:
    The "Double-click to edit" popover tooltip when clicking once
    on an image was not displayed anymore.
    
    to reproduce:
    - In the website app, open the editor
    - Click on any editable image
    - The "Double-click to edit" popover is not displayed
    
    why:
    It has not been transferred in the new builder
    
    desired behavior:
    When clicking once on an editable image, a popover tooltip should
    be displayed below the image with "Double-click to edit" message.
    
    closes odoo/odoo#217459
    
    X-original-commit: ce533be3f71e08c06fbe0c1fdbbe9fe7f0545dc8
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/html_builder/static/src/core/media_website_plugin.js
+++ addons/html_builder/static/src/core/media_website_plugin.js
@@ -4,6 +4,8 @@ import { closestElement } from "@html_editor/utils/dom_traversal";
 import { shouldEditableMediaBeEditable } from "@html_builder/utils/utils_css";
 import { _t } from "@web/core/l10n/translation";
 import { isHtmlContentSupported } from "@html_editor/core/selection_plugin";
+import { Tooltip } from "@web/core/tooltip/tooltip";
+
 
 export class MediaWebsitePlugin extends Plugin {
     static id = "media_website";
@@ -65,14 +67,52 @@ export class MediaWebsitePlugin extends Plugin {
                 this.onDblClickEditableMedia(targetEl);
             }
         });
+
+        this.popover = this.services.popover;
+        this.tooltipTimeouts = [];
+        this.removeCurrentTooltip = () => {};
+
+        this.addDomListener(this.editable, "click", (ev) => {
+            const targetEl = ev.target.closest(mediaSelector);
+            if (!targetEl) {
+                return;
+            }
+            let isEditable = (targetEl.parentElement && targetEl.parentElement.isContentEditable);
+
+            if (!isEditable && targetEl.classList.contains("o_editable_media")) {
+                isEditable = shouldEditableMediaBeEditable(targetEl);
+            }
+            if (
+                isEditable &&
+                !isProtected(this.dependencies.selection.getEditableSelection().anchorNode)
+            ) {
+                this.openImageTooltip(targetEl);
+            }
+        });
     }
 
     onDblClickEditableMedia(mediaEl) {
+        this.removeCurrentTooltip();
+
         const params = { node: mediaEl };
         const sel = this.dependencies.selection.getEditableSelection();
 
         const editableEl =
             closestElement(params.node || sel.startContainer, ".o_editable") || this.editable;
         this.dependencies.media.openMediaDialog(params, editableEl);
+    }
+
+    openImageTooltip(mediaEl) {
+        //delete last tooltip if it's still displayed
+        this.removeCurrentTooltip();
+        
+        const removeTooltip = this.popover.add(mediaEl, Tooltip, { tooltip: _t('Double-click to edit')});
+        setTimeout(() => { removeTooltip() }, 1500);
+        this.removeCurrentTooltip = removeTooltip.bind(this);
+    }
+
+    destroy() {
+        super.destroy();
+        this.removeCurrentTooltip();
     }
 }
