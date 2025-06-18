009e75f1426abccb046a065c2e91d2cd5a9349b4
tsm-odoo <tsm@odoo.com>
2025-06-18 11:13:52 +0200

    [FIX] mail, *: avoid portal authentication conflict with live chat
    *: test_discuss_full, portal, portal_rating.
    
    Before this commit, the chat bot could stop when redirecting to a page
    where the portal is enabled.
    
    Both the portal and live chat modules rely on the discuss store’s `self`
    field which identifies the authenticated user. However, the portal also
    supports authentication via a token in the URL, which temporarily identifies
    the user on a specific thread.
    
    Previously, the portal would overwrite the global `self` value, causing
    inconsistencies for other users of the store such as messages appearing
    as if sent by another user.
    
    Since token authentication is specific to portal threads, it should only
    affect actions on the granted thread without altering the global user
    identity.
    
    This fix prevents the portal from overwriting the global `self` value and
    instead returns the thread-specific `portal_partner` field.
    
    opw-4722466
    
    X-original-commit: cab7e8dbcd622b2083261061571c9c764f4191f7

--- addons/test_discuss_full/tests/__init__.py
+++ addons/test_discuss_full/tests/__init__.py
@@ -1,4 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import test_livechat_hr_holidays
+from . import test_im_livechat_portal
 from . import test_performance
