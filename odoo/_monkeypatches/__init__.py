793083a21508ffee577c0dc4d76fadda9c996008
Renaud Thiry <reth@odoo.com>
2025-06-19 12:55:14 +0000

    [FIX] base: patch lxml to allow multiple data urls
    In lxml from 4.6.0 to 5.2.0:
    
    [1] introduces an issue where "style" tags will always be cleaned if
    there is more than one data url inside it (the regex is too greedy) in
    4.6.0
    
    [2] fixes the issue in the lxml_html_clean module version 0.1.1 which is
    the dependency that replaces lxml.html.clean from lxml 5.2.0 and up
    
    We apply the fix from [2] in odoo directly. With many checks to ensure
    we don't override any potential deviation from the original regex.
    
    The issue only appear from 18.0 onwards, as that's when "email_outgoing"
    sanitation started being applied [3]
    
    [1]: lxml/lxml@73778681f14359fe6d16644e69aaca276eba525a
    [2]: fedora-python/lxml_html_clean@97402b5f6e94583c8f1c3f6070ef19ce1df90da8
    [3]: odoo/odoo@24731938f75358fd3c72b91465b72ab80d62d208
    
    runbot-105525
    
    closes odoo/odoo#215981
    
    X-original-commit: 0de7bae77aafdc16d06ffe4e6ef4a5e728b53755
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>
    Signed-off-by: Renaud Thiry (reth) <reth@odoo.com>

--- odoo/_monkeypatches/__init__.py
+++ odoo/_monkeypatches/__init__.py
@@ -74,7 +74,7 @@ def patch_init():
     patch_module('codecs')
     patch_module('win32')
     for name in (
-        'ast', 'csv', 'email', 'mimetypes', 'num2words', 'pytz', 're', 'stdnum',
+        'ast', 'csv', 'email', 'lxml', 'mimetypes', 'num2words', 'pytz', 're', 'stdnum',
         'urllib3', 'werkzeug', 'xlsxwriter', 'xlwt', 'zeep',
     ):
         HOOK_IMPORT.add_hook(name, functools.partial(patch_module, name))
