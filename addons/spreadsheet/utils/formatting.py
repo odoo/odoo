fba333d07c3919a3a35206021cf7289e8cf83a69
hokolomopo <adrm@odoo.com>
2025-06-19 16:00:15 +0200

    [IMP] spreadsheet: add helpers to format dates
    This commit adds helpers that convert a date/datetime to a number
    equivalent to the date in a spreadsheet.
    
    Task: 3600662

--- addons/spreadsheet/utils/formatting.py
+++ addons/spreadsheet/utils/formatting.py
@@ -1,8 +1,7 @@
-import json
-import base64
 import re
+from datetime import datetime, timedelta
 
-from odoo import _
+import pytz
 
 strftime_to_spreadsheet_time_format_table = {
     "%H": "hh",
@@ -42,6 +41,7 @@ def strftime_format_to_spreadsheet_time_format(strf_format):
 
     return separator.join(parts) + (" a" if twelve_hour_system else "")
 
+
 def strftime_format_to_spreadsheet_date_format(strf_format):
     """Convert a strftime format to a spreadsheet date format.
     """
@@ -56,3 +56,23 @@ def strftime_format_to_spreadsheet_date_format(strf_format):
     separator = separator.group(1) if separator else "/"
 
     return separator.join(parts)
+
+
+INITIAL_1900_DAY = datetime(1899, 12, 30)
+SECONDS_PER_DAY = 24 * 60 * 60
+
+
+def datetime_to_spreadsheet_date_number(dt, tz_name):
+    context_tz = pytz.timezone(tz_name)
+    localized_datetime = dt.astimezone(context_tz)
+    offset = localized_datetime.utcoffset() / timedelta(seconds=1)
+    timestamp = dt.timestamp() + offset
+
+    delta = timestamp - INITIAL_1900_DAY.timestamp()
+    return delta / SECONDS_PER_DAY
+
+
+def date_to_spreadsheet_date_number(d):
+    dt = datetime.combine(d, datetime.min.time())
+    delta = dt.timestamp() - INITIAL_1900_DAY.timestamp()
+    return delta / SECONDS_PER_DAY
