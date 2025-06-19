1912512e2441421de44c5c388081b662f51ccaf1
Jitendra Prajapat <jipr@odoo.com>
2025-06-19 09:52:57 +0000

    [FIX] pos_self_order: redirect to product page on empty cart
    Before this commit:
    ================
    Users could attempt to proceed with payment even when the cart had no orderlines
    
    After this commit:
    ===============
    If the cart is empty, the user is automatically redirected back to the product
    page, preventing the error and ensuring a smoother user experience.
    
    Task-4880984
    
    closes odoo/odoo#216169
    
    X-original-commit: 283380c44beaccd11335caa6c10435467138ad9c
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Jitendra Kumar Prajapat (jipr) <jipr@odoo.com>

--- addons/pos_self_order/static/src/app/pages/cart_page/cart_page.js
+++ addons/pos_self_order/static/src/app/pages/cart_page/cart_page.js
@@ -176,6 +176,7 @@ export class CartPage extends Component {
         } else {
             this.selfOrder.removeLine(line);
         }
+        !this.lines.length && this.router.back();
     }
 
     async _changeQuantity(line, increase) {
