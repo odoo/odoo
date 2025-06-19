07570480c4b994f188f58516b21b857b477d5598
hokolomopo <adrm@odoo.com>
2025-06-19 16:00:15 +0200

    [IMP] spreadsheet: add helpers to format dates
    This commit adds helpers that convert a date/datetime to a number
    equivalent to the date in a spreadsheet.
    
    closes odoo/odoo#215380
    
    Task: 3600662
    Related: odoo/enterprise#63970
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- addons/spreadsheet/tests/test_utils.py
+++ addons/spreadsheet/tests/test_utils.py
@@ -1,5 +1,11 @@
+import datetime
+
 from odoo.tests.common import TransactionCase
 
+from odoo.addons.spreadsheet.utils.formatting import (
+    date_to_spreadsheet_date_number,
+    datetime_to_spreadsheet_date_number,
+)
 from odoo.addons.spreadsheet.utils.json import extend_serialized_json
 
 
@@ -37,3 +43,24 @@ class TestSpreadsheetUtils(TransactionCase):
             extend_serialized_json('{}', [('key1', '1'), ('key2', '2')]),
             '{"key1":1,"key2":2}'
         )
+
+    def test_date_to_spreadsheet_date_number(self):
+        d = datetime.date(1899, 12, 30)
+        self.assertEqual(date_to_spreadsheet_date_number(d), 0)
+
+        d = datetime.date(2023, 10, 1)
+        self.assertEqual(date_to_spreadsheet_date_number(d), 45200)
+
+    def test_datetime_to_spreadsheet_date_number(self):
+        test_tz_offset = 8 / 24  # Etc/GMT-8 is UTC+8
+        dt = datetime.datetime(1899, 12, 30, 0, 0, 0)
+        self.assertEqual(datetime_to_spreadsheet_date_number(dt, 'UTC'), 0)
+
+        dt = datetime.datetime(1899, 12, 30, 0, 0, 0)
+        self.assertEqual(datetime_to_spreadsheet_date_number(dt, 'Etc/GMT-8'), test_tz_offset)
+
+        dt = datetime.datetime(2023, 10, 1, 12, 0, 0)
+        self.assertEqual(datetime_to_spreadsheet_date_number(dt, 'UTC'), 45200.5)
+
+        dt = datetime.datetime(2023, 10, 1, 12, 0, 0)
+        self.assertEqual(datetime_to_spreadsheet_date_number(dt, 'Etc/GMT-8'), 45200.5 + test_tz_offset)
