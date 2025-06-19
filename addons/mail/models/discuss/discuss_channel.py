04785f53bbd55d43d6d5bd90b541955e210e21da
Sébastien Theys <seb@odoo.com>
2025-06-19 12:33:37 +0000

    [FIX] mail, im_livechat: make (live)chat create more deterministic
    runbot-112917
    runbot-112998
    
    Follow up of https://github.com/odoo/odoo/pull/214559
    
    The issue is still happening after the previous fix, because the issue
    does not only happen with the `last_interest_dt` of the member, but
    also with the `last_interest_dt` of the channel.
    
    closes odoo/odoo#215104
    
    X-original-commit: 6a08b20e53e6279cab36c6591316420bd0acba38
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/models/discuss/discuss_channel.py
+++ addons/mail/models/discuss/discuss_channel.py
@@ -1174,6 +1174,7 @@ class DiscussChannel(models.Model):
                         for partner in partners
                     ],
                     "channel_type": "chat",
+                    "last_interest_dt": last_interest_dt,
                     "name": ", ".join(partners.mapped("name")),
                 }
             )
