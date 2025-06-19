8ec67ccf27146aedd61596378aceb188368203d2
Maxime de Neuville <mane@odoo.com>
2025-06-19 16:10:00 +0200

    [FIX] project: allow partner_id on new project created from template
    Before this commit, when creating a new project from a template, the 'partner_id' field
    was being removed from the values given to 'action_create_from_template()', even if
    it was explicitly set in the 'values' parameters we want the new project to have.
    For instance when specifying the 'Customer' field from the project creation from template wizard,
    it was not being taken into account, and the new project was created without a partner.
    Same when a product is configured to generate a project from a template, the partner was not set
    on the new project. We adapt the copy method of project to do so.
    We also make projects billable by default and remove some 'default_allow_billable' contexts as it's not useful anymore, and we adapt tests accordingly.
    
    closes odoo/odoo#215114
    
    Related: odoo/enterprise#88098
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/sale_project/wizard/project_template_create_wizard.py
+++ addons/sale_project/wizard/project_template_create_wizard.py
@@ -4,4 +4,11 @@ from odoo import fields, models
 class ProjectTemplateCreateWizard(models.TransientModel):
     _inherit = 'project.template.create.wizard'
 
+    partner_id = fields.Many2one("res.partner")
     allow_billable = fields.Boolean(related="template_id.allow_billable")
+
+    def _get_template_whitelist_fields(self):
+        res = super()._get_template_whitelist_fields()
+        if self.allow_billable:
+            res.append("partner_id")
+        return res
