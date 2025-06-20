be885e8eddd90735eb97cd9fc7151f98239a97e5
msho-odoo <msho@odoo.com>
2025-06-20 15:37:22 +0000

    [FIX] purchase_stock, mrp: fix rr for unconfigured products
    Before this commit, RR for unconfigured products (no BoM/ no vendor),
    was not created until the same day of the delivery date (lead_time=0).
    Now, RR for unconfigured products is created considering
    the lead_time is incremented by 365 days.
    
    Note that this commit reverts the effect of the commit:
    https://github.com/odoo/odoo/pull/224232/commits/40d0bc0df0dc09f5138aa747cbbc715ae77f104c
    
    It's functionally decided to make the total lead_days for products
    with no bom to be 365 days without adding the security_lead_days.
    It's meant just to warn the user on the RR dashboard that the product
    needing replenshiment is not configured,
    no matter to the security_lead_days in this case.
    
    Task-4779057
    
    X-original-commit: 45184da06cf7b92a48e3e4e90bf8b285bdd9ad6a

--- addons/mrp/models/stock_rule.py
+++ addons/mrp/models/stock_rule.py
@@ -208,12 +208,17 @@ class StockRule(models.Model):
             return delays, delay_description
         manufacture_rule.ensure_one()
         bom = values.get('bom') or self.env['mrp.bom']._bom_find(product, picking_type=manufacture_rule.picking_type_id, company_id=manufacture_rule.company_id.id)[product]
+        if not bom:
+            delays['total_delay'] += 365
+            delays['no_bom_found_delay'] += 365
+            if not bypass_delay_description:
+                delay_description.append((_('No BoM Found'), _('+ %s day(s)', 365)))
         manufacture_delay = bom.produce_delay
         delays['total_delay'] += manufacture_delay
         delays['manufacture_delay'] += manufacture_delay
         if not bypass_delay_description:
             delay_description.append((_('Manufacturing Lead Time'), _('+ %d day(s)', manufacture_delay)))
-        if not bom or bom.type == 'normal':
+        if bom.type == 'normal':
             # pre-production rules
             warehouse = self.location_dest_id.warehouse_id
             for wh in warehouse:
