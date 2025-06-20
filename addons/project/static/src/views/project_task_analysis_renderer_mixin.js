8d1e9cff30395931466d956896a063a5f7d853aa
Ajit Singh <aksi@odoo.com>
2025-06-20 15:35:48 +0530

    [FIX] project: do not open view for wrong domain
    Step to reproduce
    - Go to Project
    - Go to Reporting > Task Analysis
    - Group by Tasks (not Task)
    - Click on any of the blue bars
    
    Issue:
    since odoo/odoo@c36ad1896 we now allow user to drill down to base model from
    report view, this causes issue when a field which is present in report model but
    not in base/main model.
    For now we directly pass the domain created for report view to base model.
    
    FIx:
    we only open the view, when the such fields are not present in domain
    
    opw-4798353

--- addons/project/static/src/views/project_task_analysis_renderer_mixin.js
+++ addons/project/static/src/views/project_task_analysis_renderer_mixin.js
@@ -1,15 +1,28 @@
-export const ProjectTaskAnalysisRendererMixin = (T) => class ProjectTaskAnalysisRendererMixin extends T {
-    openView(domain, views, context) {
-        this.actionService.doAction({
-            context,
-            domain,
-            name: "Tasks",
-            res_model: "project.task",
-            target: "current",
-            type: "ir.actions.act_window",
-            views,
-        }, {
-            viewType: "list",
-        });
-    }
-}
+export const ProjectTaskAnalysisRendererMixin = (T) =>
+    class ProjectTaskAnalysisRendererMixin extends T {
+        openView(domain, views, context) {
+            const fieldsNotInBaseModel = ["nbr", "rating_last_value", "rating_avg", "task_id"];
+
+            if (domain) {
+                for (const leaf of domain) {
+                    if (Array.isArray(leaf) && fieldsNotInBaseModel.includes(leaf[0])) {
+                        return;  // early return if any unwanted field is found
+                    }
+                }
+            }
+            this.actionService.doAction(
+                {
+                    context,
+                    domain,
+                    name: "Tasks",
+                    res_model: "project.task",
+                    target: "current",
+                    type: "ir.actions.act_window",
+                    views,
+                },
+                {
+                    viewType: "list",
+                }
+            );
+        }
+    };
