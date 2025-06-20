93074f0fd144aee77f9f71b22138b6840c54b9f5
Alice Gaudon <agau@odoo.com>
2025-06-20 08:46:25 +0000

    [CLN] html_builder: add missing BuilderAction docstring params and returns
    closes odoo/odoo#215852
    
    X-original-commit: cabbf302ccd0f140eaec491ec1137efcb8024f4c
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/html_builder/static/src/core/builder_action.js
+++ addons/html_builder/static/src/core/builder_action.js
@@ -46,6 +46,7 @@ export class BuilderAction {
      * @param {Object} context
      * @param {HTMLElement} context.editingElement
      * @param {Object} [context.params]
+     * @returns {any}
      */
     getValue(context) {}
 
@@ -55,14 +56,17 @@ export class BuilderAction {
      * @param {HTMLElement} context.editingElement
      * @param {any} context.value
      * @param {Object} [context.params]
+     * @returns {boolean}
      */
-    isApplied() {}
+    isApplied(context) {}
 
     /**
      * Clean/reset the value if needed.
      * @param {Object} context
      * @param {boolean} context.isPreviewing
      * @param {HTMLElement} context.editingElement
+     * @param {any} context.value
+     * @param {Object} [context.params]
      */
     clean(context) {}
 
@@ -76,6 +80,7 @@ export class BuilderAction {
     /**
      * Check if a method has been overridden.
      * @param {string} method
+     * @returns {boolean}
      */
     has(method) {
         const baseMethod = BuilderAction.prototype[method];
