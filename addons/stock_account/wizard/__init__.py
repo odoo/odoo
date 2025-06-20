9f0470b48e4e8c783513788e269ad16cbf84b7af
Mohammad Abdulmoneim (abdu) <abdu@odoo.com>
2025-06-20 15:32:36 +0200

    [IMP] stock{_*}: inventory adjustment b2b
    Revamps the inventory adjustments.
    
    Task-4681370
    
    closes odoo/odoo#216035
    
    Related: odoo/enterprise#88614
    Related: odoo/upgrade#7962
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/stock_account/wizard/__init__.py
+++ addons/stock_account/wizard/__init__.py
@@ -1,4 +1,4 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import stock_picking_return
-from . import stock_request_count
+from . import stock_inventory_adjustment_name
