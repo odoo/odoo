736f2a310373b85f73fa4a1cc164feb6f86f5bf6
Pierre Rousseau <pro@odoo.com>
2025-06-20 13:18:28 +0200

    [IMP] web: add current model to model field filter
    This commit adds the current model to the model field filter function.
    This is needed for spreadsheet in the field matching of selection filters,
    where we have to know the current model to filter the fields correctly.
    
    Task: 3619413
    Part-of: odoo/odoo#215182
    Related: odoo/enterprise#88142
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- addons/web/static/src/core/model_field_selector/model_field_selector_popover.js
+++ addons/web/static/src/core/model_field_selector/model_field_selector_popover.js
@@ -176,9 +176,9 @@ export class ModelFieldSelectorPopover extends Component {
         return fieldDef.relation;
     }
 
-    filter(fieldDefs, path) {
+    filter(fieldDefs, path, resModel) {
         const filteredKeys = Object.keys(fieldDefs).filter((k) =>
-            this.props.filter(fieldDefs[k], path)
+            this.props.filter(fieldDefs[k], path, resModel)
         );
         return Object.fromEntries(filteredKeys.map((k) => [k, fieldDefs[k]]));
     }
@@ -193,7 +193,7 @@ export class ModelFieldSelectorPopover extends Component {
         this.state.page.selectedName = fieldDef.name;
         const { resModel, fieldDefs } = modelsInfo.at(-1);
         this.openPage(
-            new Page(resModel, this.filter(fieldDefs, this.state.page.path), {
+            new Page(resModel, this.filter(fieldDefs, this.state.page.path, resModel), {
                 previousPage: this.state.page,
                 isDebugMode: this.props.isDebugMode,
                 readProperty: this.props.readProperty,
@@ -215,7 +215,7 @@ export class ModelFieldSelectorPopover extends Component {
     async loadPages(resModel, path) {
         if (typeof path !== "string" || !path.length) {
             const fieldDefs = await this.fieldService.loadFields(resModel);
-            return new Page(resModel, this.filter(fieldDefs, path), {
+            return new Page(resModel, this.filter(fieldDefs, path, resModel), {
                 isDebugMode: this.props.isDebugMode,
                 readProperty: this.props.readProperty,
                 sortFn: this.props.sort,
@@ -227,7 +227,7 @@ export class ModelFieldSelectorPopover extends Component {
                 throw new Error(`Invalid model name: ${resModel}`);
             case "path": {
                 const { resModel, fieldDefs } = modelsInfo[0];
-                return new Page(resModel, this.filter(fieldDefs, path), {
+                return new Page(resModel, this.filter(fieldDefs, path, resModel), {
                     selectedName: path,
                     isDebugMode: this.props.isDebugMode,
                     readProperty: this.props.readProperty,
@@ -239,7 +239,7 @@ export class ModelFieldSelectorPopover extends Component {
                 for (let index = 0; index < names.length; index++) {
                     const name = names[index];
                     const { resModel, fieldDefs } = modelsInfo[index];
-                    page = new Page(resModel, this.filter(fieldDefs, path), {
+                    page = new Page(resModel, this.filter(fieldDefs, path, resModel), {
                         previousPage: page,
                         selectedName: name,
                         isDebugMode: this.props.isDebugMode,
