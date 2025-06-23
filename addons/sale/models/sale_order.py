aa8b09ca61dfd405ebebbeff4e45a08bf7ad84fd
Ryan Cen <ryce@odoo.com>
2025-06-23 20:53:17 +0000

    [FIX] sale: remove default user context on confirm
    When creating a sale order through crm, default_user_id was being
    passed through the context. This was causing issues when confirming the
    sale orders when quality checks were enabled as the user on the
    quality checks would be set as the user from the CRM lead. Removing this
    from the context before confirming and thus creating any linked records
    avoids this issue.
    
    opw-4658850
    
    closes odoo/odoo#216953
    
    X-original-commit: 1540c30e87cb178806519edd5c0bc5c5ac75f097
    Signed-off-by: Ryan Cen (ryce) <ryce@odoo.com>

--- addons/sale/models/sale_order.py
+++ addons/sale/models/sale_order.py
@@ -1137,6 +1137,7 @@ class SaleOrder(models.Model):
         # We don't need it and it creates issues in the creation of linked records.
         context = self._context.copy()
         context.pop('default_name', None)
+        context.pop('default_user_id', None)
 
         self.with_context(context)._action_confirm()
         self.filtered(lambda so: so._should_be_locked()).action_lock()
