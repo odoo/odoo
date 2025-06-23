d160416ab77cfd0870f0d7f82ad8961fdcc23365
Alex Kühn <aku@odoo.com>
2025-06-23 12:31:26 +0000

    [FIX] im_livechat: improved livechat status feature
    Before this commit, new livechat status feature has some usability
    issues:
    
    - icons of status were poor: `.fa-circle` is not unique enough
      and looks like IM status
    - icons were inconsistent: info panel uses different icons
    - info panel buttons do not look good: text not aligned, border too
     harsh with strange spacing, etc.
    - feature is cumbersome to use: can only be changed in panel, so lots
     of clicks when not in discuss app and conversation not selected.
    - feature was not displayed in some contexts like chat bubbles
    - livechat status not easy to see in discuss sidebar when next to
      unread counter
    
    This commit makes the following improvements to livechat status
    feature:
    - new icons for livechat status
    - better button style in panel
    - livechat status is visible in all context with proper style
    - can quickly change livechat status from thread actions, including
    discuss sidebar more actions
    - livechat icon is floating in top-left of avatar for better
    visibility of status (exception: chat window is before avatar for
    space constraints)
    
    ----
    
    With the addition of quick action to change livechat status, this
    commit adds these new features:
    
    1. thread action icons can be defined in dedicate templates, for more
    creativity in making icon than `.fa` and `.oi` classnames.
    2. useHover() now works with nested overlay: this works by warping
     dropdown content in a `<UseHoverOverlay hover="hover">` component.
    
    ---
    
    Technical details of useHover nested overlay improvement:
    To make it work, the content of 1st dropdown should be wrapped in a
    `<UseHoverOverlay>` component and provide the `hover` object of
    `useHover()`. The overlay content is put in slot of this component.
    
    This `<UseHoverOverlay>` component works by helping `hover` object to
    make use of `contains()` function of overlay, which tells precisely
    if a target is inside the overlay including nested dropdowns.
    
    Note that sub-dropdowns menu can optionally have `<UseHoverOverlay>`:
    - without it, they are persistently displayed until click away or
     mouseleave from parented dropdown (if has `useHoverOverlay`)
    - with it: mouse-leave on it outside of whole overlay closes all the
     related overlays.
    
    This commit also do some related code-cleaning with useHover:
    - "*" suffix syntax is removed. This was used as a way to get the
    direct parent element of ref. Floating menu were disappearing
    immediately `onAway`, but there's a global 100ms now so we don't need
    2 hover triggers to be exactly next to each other
    
    closes odoo/odoo#216473
    
    X-original-commit: 7cfee855c10e5f7e8d1c20563376af5748df39a9
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/im_livechat/static/src/core/web/livechat_channel_info_list.js
+++ addons/im_livechat/static/src/core/web/livechat_channel_info_list.js
@@ -3,51 +3,22 @@ import { prettifyMessageContent } from "@mail/utils/common/format";
 
 import { Component } from "@odoo/owl";
 
-import { _t } from "@web/core/l10n/translation";
 import { rpc } from "@web/core/network/rpc";
+import { useService } from "@web/core/utils/hooks";
 
 export class LivechatChannelInfoList extends Component {
     static components = { ActionPanel };
-    static template = "im_livechat.channelInfoList";
+    static template = "im_livechat.LivechatChannelInfoList";
     static props = ["thread"];
 
     setup() {
         super.setup();
-    }
-
-    get statusButtons() {
-        return [
-            {
-                label: _t("In progress"),
-                status: "in_progress",
-                icon: "fa fa-circle-o",
-            },
-            {
-                label: _t("Waiting for customer"),
-                status: "waiting",
-                icon: "fa fa-check",
-            },
-            {
-                label: _t("Looking for help"),
-                status: "need_help",
-                icon: "fa fa-times",
-            },
-        ];
+        this.store = useService("mail.store");
     }
 
     onBlurNote() {
         prettifyMessageContent(this.props.thread.livechatNoteText).then((note) => {
             rpc("/im_livechat/session/update_note", { channel_id: this.props.thread.id, note });
-        });
-    }
-
-    updateLivechatStatus(livechat_status) {
-        if (this.props.thread.livechat_status === livechat_status) {
-            return;
-        }
-        rpc("/im_livechat/session/update_status", {
-            channel_id: this.props.thread.id,
-            livechat_status,
         });
     }
 }
