93302043083fa4ec8c32dcd1662c67801fccb4e7
asno-odoo <asno@odoo.com>
2025-06-23 14:28:14 +0200

    [FIX] hr,hr_*: check existence of menu
    To Reproduce:
    1) make a database in 16.0 and go to developer mode.
    2) Delete the menus
    3) I mocked the views with upgrade mockcrawler.
    
    it failed for these xmlids:
    ```
    hr_attendance.menu_hr_attendance_attendances_overview
    hr_timesheet.timesheet_menu_activity_use
    hr.menu_hr_employee
    ```
    but we should have a check anyways.

--- addons/hr_timesheet_attendance/models/ir_ui_menu.py
+++ addons/hr_timesheet_attendance/models/ir_ui_menu.py
@@ -9,6 +9,7 @@ class IrUiMenu(models.Model):
 
     def _load_menus_blacklist(self):
         res = super()._load_menus_blacklist()
-        if not (self.env.user.has_group('hr_attendance.group_hr_attendance_user') and self.env.user.has_group('hr_timesheet.group_hr_timesheet_user')):
-            res.append(self.env.ref('hr_timesheet_attendance.menu_hr_timesheet_attendance_report').id)
+        att_menu = self.env.ref('hr_timesheet_attendance.menu_hr_timesheet_attendance_report', raise_if_not_found=False)
+        if att_menu and not (self.env.user.has_group('hr_attendance.group_hr_attendance_user') and self.env.user.has_group('hr_timesheet.group_hr_timesheet_user')):
+            res.append(att_menu.id)
         return res
