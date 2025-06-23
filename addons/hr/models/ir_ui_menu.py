56409a47b3f5b6fd8926f8b2473dbc8d580fd3a3
asno-odoo <asno@odoo.com>
2025-06-23 14:28:14 +0200

    [FIX] hr,hr_*: check existence of menu
    To Reproduce:
    1) make a database in 16.0 and go to developer mode.
    2) Delete the menus
    3) I mocked the views with upgrade mockcrawler.
    
    it failed for these xmlids:
    ```
    hr_attendance.menu_hr_attendance_attendances_overview
    hr_timesheet.timesheet_menu_activity_use
    hr.menu_hr_employee
    ```
    but we should have a check anyways.
    
    closes odoo/odoo#215426
    
    Signed-off-by: Jurgen Gjini (jugj) <jugj@odoo.com>

--- addons/hr/models/ir_ui_menu.py
+++ addons/hr/models/ir_ui_menu.py
@@ -9,6 +9,7 @@ class IrUiMenu(models.Model):
 
     def _load_menus_blacklist(self):
         res = super()._load_menus_blacklist()
-        if self.env.user.has_group('hr.group_hr_user'):
-            res.append(self.env.ref('hr.menu_hr_employee').id)
+        emp_menu = self.env.ref('hr.menu_hr_employee', raise_if_not_found=False)
+        if emp_menu and self.env.user.has_group('hr.group_hr_user'):
+            res.append(emp_menu.id)
         return res
--- addons/hr_attendance/models/ir_ui_menu.py
+++ addons/hr_attendance/models/ir_ui_menu.py
@@ -9,6 +9,7 @@ class IrUiMenu(models.Model):
 
     def _load_menus_blacklist(self):
         res = super()._load_menus_blacklist()
-        if self.env.user.has_group('hr_attendance.group_hr_attendance_user'):
-            res.append(self.env.ref('hr_attendance.menu_hr_attendance_attendances_overview').id)
+        att_menu = self.env.ref('hr_attendance.menu_hr_attendance_attendances_overview', raise_if_not_found=False)
+        if att_menu and self.env.user.has_group('hr_attendance.group_hr_attendance_user'):
+            res.append(att_menu.id)
         return res
