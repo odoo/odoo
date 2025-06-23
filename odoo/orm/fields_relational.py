e86dff845f947588349235335b87019daab703c0
Krzysztof Magusiak (krma) <krma@odoo.com>
2025-06-23 09:47:20 +0000

    [IMP] orm: fields.Many2many auto_join
    Allow to define `auto_join` on many2many fields: this bypasses the
    record rules on the comodel; the same semantics as for one2many.

--- odoo/orm/fields_relational.py
+++ odoo/orm/fields_relational.py
@@ -36,7 +36,7 @@ class _Relational(Field[BaseModel]):
     comodel_name: str
     domain: DomainType = []         # domain for searching values
     context: ContextType = {}       # context for searching values
-    auto_join: bool = False         # whether joins are generated upon search
+    auto_join: bool = False         # whether access rights are bypassed on the comodel
     check_company: bool = False
 
     def __get__(self, records: BaseModel, owner=None):
@@ -211,8 +211,8 @@ class Many2one(_Relational):
     :param str ondelete: what to do when the referred record is deleted;
         possible values are: ``'set null'``, ``'restrict'``, ``'cascade'``
 
-    :param bool auto_join: whether JOINs are generated upon search through that
-        field (default: ``False``)
+    :param bool auto_join: whether access rights are bypassed on the comodel
+        (default: ``False``)
 
     :param bool delegate: set it to ``True`` to make fields of the target model
         accessible from the current model (corresponds to ``_inherits``)
@@ -777,7 +777,7 @@ class _RelationalMulti(_Relational):
                     )
                 #  in (False) => not any (Domain.TRUE)
                 #  not in (False) => any (Domain.TRUE)
-                value = comodel._search(Domain.TRUE)
+                value = comodel._search(Domain.TRUE, bypass_access=self.auto_join)
                 exists = not exists
             else:
                 value = comodel.browse(value)._as_query(ordered=False)
@@ -1217,8 +1217,6 @@ class Many2many(_RelationalMulti):
     def __init__(self, comodel_name: str | Sentinel = SENTINEL, relation: str | Sentinel = SENTINEL,
                  column1: str | Sentinel = SENTINEL, column2: str | Sentinel = SENTINEL,
                  string: str | Sentinel = SENTINEL, **kwargs):
-        if 'auto_join' in kwargs:
-            raise NotImplementedError("auto_join is not supported on Many2many fields")
         super().__init__(
             comodel_name=comodel_name,
             relation=relation,
@@ -1639,7 +1637,6 @@ class Many2many(_RelationalMulti):
             ])
 
     def _condition_to_sql_relational(self, model: BaseModel, alias: str, exists: bool, coquery: Query, query: Query) -> SQL:
-        assert not self.auto_join, f"auto_join not implemented for many2many fields ({self})"
         if coquery.is_empty():
             return SQL("FALSE") if exists else SQL("TRUE")
         rel_table, rel_id1, rel_id2 = self.relation, self.column1, self.column2
