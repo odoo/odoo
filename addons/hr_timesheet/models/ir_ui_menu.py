08c322db1157ea16a75cd7457a556bdc7b1ac57c
asno-odoo <asno@odoo.com>
2025-06-23 12:28:14 +0000

    [FIX] hr,hr_*: check existence of menu
    To Reproduce:
    1) make a database in 16.0 and go to developer mode.
    2) Delete the menus
    3) I mocked the views with upgrade mockcrawler.
    
    it failed for these xmlids:
    ```
    hr_attendance.menu_hr_attendance_attendances_overview
    hr_timesheet.timesheet_menu_activity_use
    hr.menu_hr_employee
    ```
    but we should have a check anyways.
    
    closes odoo/odoo#234420
    
    X-original-commit: 088643ac61f9aa14c0e308e068c44e336222a060
    Signed-off-by: Jurgen Gjini (jugj) <jugj@odoo.com>
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>
    Signed-off-by: Aslam Noman (asno) <asno@odoo.com>

--- addons/hr_timesheet/models/ir_ui_menu.py
+++ addons/hr_timesheet/models/ir_ui_menu.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import models
@@ -9,6 +8,6 @@ class IrUiMenu(models.Model):
 
     def _load_menus_blacklist(self):
         res = super()._load_menus_blacklist()
-        if self.env.user.has_group('hr_timesheet.group_hr_timesheet_approver'):
-            res.append(self.env.ref('hr_timesheet.timesheet_menu_activity_user').id)
+        if self.env.user.has_group('hr_timesheet.group_hr_timesheet_approver') and (time_menu := self.env.ref('hr_timesheet.timesheet_menu_activity_user', raise_if_not_found=False)):
+            res.append(time_menu.id)
         return res
