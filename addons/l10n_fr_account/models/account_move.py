2d4656eb4a6ab470d53ad8d7bac2f6fad8c9462b
Maximilien (malb) <malb@odoo.com>
2025-06-23 10:53:04 +0200

    [REV] DONT LOOK - l10n_fr: Show delivery date on customer invoices
    no task id

--- addons/l10n_fr_account/models/account_move.py
+++ addons/l10n_fr_account/models/account_move.py
@@ -19,17 +19,3 @@ class AccountMove(models.Model):
     def _compute_l10n_fr_is_company_french(self):
         for record in self:
             record.l10n_fr_is_company_french = record.country_code in record.company_id._get_france_country_codes()
-
-    @api.depends("country_code", "move_type")
-    def _compute_show_delivery_date(self):
-        # EXTEND 'account'
-        super()._compute_show_delivery_date()
-        for move in self.filtered(lambda m: m.country_code == 'FR'):
-            move.show_delivery_date = move.is_sale_document()
-
-    def _post(self, soft=True):
-        # EXTEND 'account'
-        res = super()._post(soft=soft)
-        for move in self.filtered(lambda m: m.show_delivery_date and not m.delivery_date):
-            move.delivery_date = move.invoice_date
-        return res
