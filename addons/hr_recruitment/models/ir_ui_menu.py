a31e4eab5047e22d22b44ca9fc637cf69eb06363
asno-odoo <asno@odoo.com>
2025-06-23 12:28:14 +0000

    [FIX] hr,hr_*: check existence of menu
    To Reproduce:
    1) make a database in 16.0 and go to developer mode.
    2) Delete the menus
    3) I mocked the views with upgrade mockcrawler.
    
    it failed for these xmlids:
    ```
    hr_attendance.menu_hr_attendance_attendances_overview
    hr_timesheet.timesheet_menu_activity_use
    hr.menu_hr_employee
    ```
    but we should have a check anyways.
    
    closes odoo/odoo#226370
    
    X-original-commit: 6e4d3d08c1517c34e66f0c627726bea5c45e5b64
    Signed-off-by: Jurgen Gjini (jugj) <jugj@odoo.com>
    Signed-off-by: Aslam Noman (asno) <asno@odoo.com>

--- addons/hr_recruitment/models/ir_ui_menu.py
+++ addons/hr_recruitment/models/ir_ui_menu.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import models
@@ -11,10 +10,13 @@ class IrUiMenu(models.Model):
         res = super()._load_menus_blacklist()
         is_interviewer = self.env.user.has_group('hr_recruitment.group_hr_recruitment_interviewer')
         is_user = self.env.user.has_group('hr_recruitment.group_hr_recruitment_user')
-        if not is_interviewer:
-            res.append(self.env.ref('hr.menu_view_hr_job').id)
-        elif is_interviewer and not is_user:
-            res.append(self.env.ref('hr_recruitment.menu_hr_job_position').id)
-        else:
-            res.append(self.env.ref('hr_recruitment.menu_hr_job_position_interviewer').id)
+        job_menu = self.env.ref('hr_recruitment.menu_hr_job', raise_if_not_found=False)
+        pos_menu = self.env.ref('hr_recruitment.menu_hr_job_position', raise_if_not_found=False)
+        int_menu = self.env.ref('hr_recruitment.menu_hr_job_position_interviewer', raise_if_not_found=False)
+        if job_menu and not is_interviewer:
+            res.append(job_menu.id)
+        elif pos_menu and is_interviewer and not is_user:
+            res.append(pos_menu.id)
+        elif int_menu:
+            res.append(int_menu.id)
         return res
