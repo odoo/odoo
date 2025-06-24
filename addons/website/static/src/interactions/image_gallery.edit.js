2f3b78e5b4a1568c8d1d870796c6c454360baaab
Benoit Socias <bso@odoo.com>
2025-06-24 12:27:44 +0000

    [FIX] website: only enable add image when there are no images
    When [1] introduced `html_builder`, the condition under which the "Add
    Images" alert appears was turned into a CSS rule.
    
    This commit restores the condition on the interaction instead to limit
    the pollution on the history.
    
    It also resets the `height` which might have been forced and hides the
    button controls while the alert is displayed.
    
    [1]: https://github.com/odoo/odoo/commit/3ced5e9232d3c5f71259f1f2b692416932e3ebd2
    
    task-4367641
    
    X-original-commit: 6e7ed4690110016d87c00d3cbb6c4592a913b05d
    Part-of: odoo/odoo#216719
    Signed-off-by: Géry Debongnie <ged@odoo.com>
    Signed-off-by: Benoit Socias (bso) <bso@odoo.com>

--- addons/website/static/src/interactions/image_gallery.edit.js
+++ addons/website/static/src/interactions/image_gallery.edit.js
@@ -3,6 +3,7 @@ import { registry } from "@web/core/registry";
 
 export class ImageGalleryEdit extends Interaction {
     static selector = ".s_image_gallery";
+    static selectorNotHas = "img, a.o_link_readonly, span.fa.object-fit-cover, div.media_iframe_video";
     dynamicContent = {
         ".o_empty_gallery_alert": {
             "t-on-click": this.onAddImage.bind(this),
