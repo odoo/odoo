7ee9566103cbf972aaa77477d32f35c11dc51a5e
Omar (OSAH) <osah@odoo.com>
2025-06-24 14:51:08 +0000

    [FIX] point_of_sale: fix test for account_tax_periodicity_journal_id
    moving test to pos enterprise as this field `account_tax_periodicity_journal_id`
    is available in the saas-18.2 version of Odoo 18.2 enterprise version.
    
    pos_enterprise commit : https://github.com/odoo/enterprise/pull/88742/commits/2acc7ab586485c50d49b8d1e21ff513041c328bc
    
    build_error-227602
    
    closes odoo/odoo#217003
    
    X-original-commit: f16a7c834b0748f4095777a9648b4af194252253
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Omar Sherif Ali Hassan (osah) <osah@odoo.com>

--- addons/point_of_sale/tests/test_res_config_settings.py
+++ addons/point_of_sale/tests/test_res_config_settings.py
@@ -5,7 +5,6 @@ import odoo
 
 from odoo import Command
 from odoo.addons.point_of_sale.tests.common import TestPoSCommon
-from odoo.tests import Form
 
 
 @odoo.tests.tagged('post_install', '-at_install')
@@ -32,53 +31,6 @@ class TestConfigureShops(TestPoSCommon):
         self.env['account.tax'].search([
             ('company_id', '=', self.env.company.id), ('tax_exigibility', '=', 'on_payment')
         ]).unlink()
-
-    def test_should_not_affect_other_pos_config(self):
-        """ Change in one pos.config should not reflect to the other.
-        """
-        self._remove_on_payment_taxes()
-
-        pos_config1 = self.env['pos.config'].create({'name': 'Shop 1', 'module_pos_restaurant': False})
-        pos_config2 = self.env['pos.config'].create({'name': 'Shop 2', 'module_pos_restaurant': False})
-        self.assertEqual(pos_config1.receipt_header, False)
-        self.assertEqual(pos_config2.receipt_header, False)
-
-        # Modify Shop 1.
-        with Form(self.env['res.config.settings']) as form:
-            form.pos_config_id = pos_config1
-            form.pos_is_header_or_footer = True
-            form.pos_receipt_header = 'xxxxx'
-            form.account_tax_return_journal_id = self.account_tax_return_journal
-
-        self.assertEqual(pos_config1.receipt_header, 'xxxxx')
-        self.assertEqual(pos_config2.receipt_header, False)
-
-        # Modify Shop 2.
-        with Form(self.env['res.config.settings']) as form:
-            form.pos_config_id = pos_config2
-            form.pos_is_header_or_footer = True
-            form.pos_receipt_header = 'yyyyy'
-
-        self.assertEqual(pos_config1.receipt_header, 'xxxxx')
-        self.assertEqual(pos_config2.receipt_header, 'yyyyy')
-
-    def test_is_header_or_footer_to_false(self):
-        self._remove_on_payment_taxes()
-
-        pos_config = self.env['pos.config'].create({
-            'name': 'Shop',
-            'is_header_or_footer': True,
-            'module_pos_restaurant': False,
-            'receipt_header': 'header val',
-            'receipt_footer': 'footer val',
-        })
-
-        with Form(self.env['res.config.settings']) as form:
-            form.pos_config_id = pos_config
-            form.pos_is_header_or_footer = False
-
-        self.assertEqual(pos_config.receipt_header, False)
-        self.assertEqual(pos_config.receipt_footer, False)
 
     def test_properly_set_pos_config_x2many_fields(self):
         """Simulate what is done from the res.config.settings view when editing x2 many fields."""
