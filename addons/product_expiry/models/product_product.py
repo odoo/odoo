152c0c786c9914d8a6dea1271c72d3317d852140
Pieter Claeys (clpi) <clpi@odoo.com>
2025-06-24 17:16:33 +0200

    [FIX] stock,product_expiry: correct forecast for in transit stock
    In [1], we added new logic for excluding any unreserved quantities with
    a removal date <= today from the calculation of the free to use and
    forecasted quantities of a product.
    
    This worked well, except for items that are currently in a 2/3 step
    receipt flow that have reached their removal date. As these quantities
    were technically reserved (for the next step of the receive flow), they
    are not to be considered as fresh stock.
    
    This commit addresses this and also factors out all the logic for
    calculating the quantities for perishable products to the product_expiry
    module where it will only run for relevant products (instead of running
    in the stock module, for all products in when product_expiry is
    installed)
    
    [1] https://github.com/odoo/odoo/pull/192896

--- addons/product_expiry/models/product_product.py
+++ addons/product_expiry/models/product_product.py
@@ -9,7 +9,49 @@ class ProductProduct(models.Model):
     _inherit = "product.product"
 
     def _compute_quantities_dict(self, lot_id, owner_id, package_id, from_date=False, to_date=False):
-        return super(ProductProduct, self.with_context(with_expiration=datetime.date.today()))._compute_quantities_dict(lot_id, owner_id, package_id, from_date, to_date)
+        """
+            For products that are perishable, we need to apply a correction to the free and the forecast quantity.
+            The following rule defines when a quantity is no longer counted towards forecast quantity:
+                It has a removal date of today or earlier (A) AND (
+                    It is not reserved (B) OR
+                    It is reserved but only because it is in transit (C, 2/3 step receipt in warehouse)
+                )
+            In short: quants to subtract = A & (B | C)
+            In this method we calculate A&B and A&C separately (for expiration enabled products only) and then subtract
+            the sum from the forecasted quantity.
+            For the free quantity, we only need A&B, as the reserved quantity in transit is never 'free'.
+        """
+        res = super()._compute_quantities_dict(lot_id, owner_id, package_id, from_date, to_date)
+        expiration_products = self.filtered(lambda p: p.use_expiration_date)
+        if not expiration_products:
+            return res
+        domain_quant = [('product_id', 'in', expiration_products.ids)] + expiration_products._get_domain_locations()[0]
+        if lot_id is not None:
+            domain_quant += [('lot_id', '=', lot_id)]
+        if owner_id is not None:
+            domain_quant += [('owner_id', '=', owner_id)]
+        if package_id is not None:
+            domain_quant += [('package_id', '=', package_id)]
+        domain_quant += [('removal_date', '<=', datetime.date.today())]
+        Quant = self.env['stock.quant'].with_context(active_test=False)
+
+        # A & B
+        expired_unreserved_quants_res = {product.id: quantity - reserved_quantity for product, quantity, reserved_quantity in Quant._read_group(domain_quant, ['product_id'], ['quantity:sum', 'reserved_quantity:sum'])}
+
+        # A & C
+        wh = self.env['stock.warehouse'].search([])
+        domain_quant[1] = ('location_id', 'in', wh.mapped('wh_qc_stock_loc_id').ids + wh.mapped('wh_input_stock_loc_id').ids)
+        to_remove_in_transit = {product.id: quantity for product, quantity in Quant._read_group(domain_quant, ['product_id'], ['reserved_quantity:sum'])}
+
+        for product in expiration_products:
+            # A&B
+            to_subtract = expired_unreserved_quants_res.get(product.id, 0.0)
+            res[product.id]['free_qty'] = product.uom_id.round(res[product.id]['free_qty'])
+
+            # A&B | A&C
+            to_subtract += to_remove_in_transit.get(product.id, 0.0)
+            res[product.id]['virtual_available'] = product.uom_id.round(res[product.id]['virtual_available'] - to_subtract)
+        return res
 
     free_qty = fields.Float(help="Available quantity (computed as Quantity On Hand "
              "- reserved quantity - quantity to remove)\n"
