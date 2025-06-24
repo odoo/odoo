b416df72fac071b673e7389c386b83796a6e9cc4
Romain ROSANO (rros) <rros@odoo.com>
2025-06-24 13:27:27 +0200

    [ADD] {crm_}survey: allow lead generation on answers
    **Motivation**
    In order to generate leads easily from survey submissions, we allow users to configure
    a lead-generation option on answers of multiple choice questions. When selecting at least
    one of those answers when taking the survey, use relevant information to generate a lead.
    
    **Specification**
    On such a submission, and only for a survey with type 'survey', 'live_session' or 'custom',
    we create a lead:
    - The lead's description is populated with the user's inputs. This is deemed sufficient
      and the link between the survey input and the lead is not stored directly in db;
    - The survey's responsible person is set as the salesperson of the lead, if the responsible
      is a salesperson;
    - The customer field is set to the contact taking the survey. Otherwise, a nickname and/or
      an email question may be added to the survey to collect these user information if he is
      not logged in. If provided, the nickname's answer is set as the contact name field and in
      the lead title;
    - The lead's medium is set as 'Survey' and the source as the survey title.
    Leads for a same survey are generated in batch.
    
    **Other changes**
    - New demo data 'Let's connect', a survey with some lead generating answers;
    - New survey template 'Lead Qualification' that can be selected in the helper;
    - New stat button 'Leads' on the survey;
    - Rewrite a context field in the xml to make ast methods work properly.
    
    **Tests**
    A few tests have been added:
    - Logged in user answering survey, selecting no lead-generating answers;
    - Logged in user answering survey, selecting lead-generating answers;
    - Public user answering survey, selecting lead-generating answers, and
      with the survey responsible being in a sales team.
    
    task-4778192

---
+++ addons/crm_survey/models/survey_question.py
@@ -0,0 +1,18 @@
+from odoo import api, models, fields
+
+
+class SurveyQuestion(models.Model):
+    _inherit = 'survey.question'
+
+    survey_type = fields.Selection(related='survey_id.survey_type')
+    is_lead_generating = fields.Boolean(
+        'Lead generating', compute='_compute_is_lead_generating',
+        help='At least one of the question answers can generate leads.')
+
+    @api.depends('question_type', 'suggested_answer_ids', 'suggested_answer_ids.is_create_lead')
+    def _compute_is_lead_generating(self):
+        for question in self:
+            if question.question_type in ['simple_choice', 'multiple_choice', 'matrix']:
+                question.is_lead_generating = any(answer.is_create_lead for answer in question.suggested_answer_ids)
+            else:
+                question.is_lead_generating = False
