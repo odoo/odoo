dee0b8935eb6b6fec8f9565beba10e3e71c6d9b2
Romain ROSANO (rros) <rros@odoo.com>
2025-06-24 13:27:27 +0200

    [ADD] {crm_}survey: allow lead generation on answers
    **Motivation**
    This feature allows managing user's survey inputs automatically, without
    having to do it manually.
    This new module overloads the survey response configurations to generate leads
    based on user's survey answers for "survey", "live_session" and "custom" types.
    This addition is accessible for questions with suggested answers.
    
    **Specification**
    When a survey is submitted, a lead is generated thanks to information provided:
    - If a lead qualification is met and at least one "Create lead"
      suggested answer has been chosen by the user, a lead is generated;
    - The lead's description is populated with the user's inputs;
    - The survey's responsible person is set as the salesperson of the lead,
      if the responsible is a salesperson;
    - The customer field is linked to the user's account.
      Otherwise, a nickname and/or an email question may be added to the survey to
      collect these user information if he is not logged in.
      If provided, the nickname's answer is set as the contact name
      field and in the lead title;
    - The lead's medium is set as "Survey" and the source as the survey title.
    Leads for a same survey are generated in batch.
    
    **Tests**
    3 tests have been added to test this module:
    - one for a logged in user with a lead qualification because of his answers;
    - one for a public user with a lead qualification and the survey
      responsible in a sales team;
    - one for a logged in user without lead qualification.
    
    **Examples hard coded**
    An example of that new use of survey has been added to the default template page
    in Survey as "Lead qualification".
    In addition, a demo survey has been added, using creating lead answers.
    
    **Current survey view changes**
    A new statbutton appears in the survey view if at least one lead has
    been generated by the current survey. It allows to access them easily.
    This action can also be accessed from the line of the survey in question
    on the survey homepage.
    Access rights have been added to lead-seen persons to access this action.
    
    **Lead module changes**
    The 'context' field of lead menu has been inlined to avoid a parsing
    issue from the 'ast' lib.
    
    task-4778192

---
+++ addons/crm_survey/models/__init__.py
@@ -0,0 +1,6 @@
+from . import crm_lead
+from . import survey_question
+from . import survey_question_answer
+from . import survey_survey
+from . import survey_user_input
+from . import templates
---
+++ addons/crm_survey/tests/test_crm_survey.py
@@ -0,0 +1,195 @@
+from markupsafe import Markup
+from odoo.addons.survey.tests import common
+from odoo.tests import tagged
+from odoo.tests.common import HttpCase
+
+
+@tagged('-at_install', 'post_install', 'functional')
+class TestCrmSurvey(common.TestSurveyCommon, HttpCase):
+    """
+    These tests will check:
+    - 1st case: if logged in user's inputs contains "Create lead" answers, then a lead is created successfully
+    - 2nd case: if logged in user's inputs do not contain "Create lead" answers, then no lead is created
+    - 3rd case: if public user's inputs contains "Create lead" answers, then a lead is created with his email answer
+    """
+    def _create_lead_qualification_survey(self, survey_type="survey"):
+        # Create lead qualification survey
+        survey = self.env['survey.survey'].with_user(self.survey_manager).create({
+            'title': 'Questionnaire for the position of software developer',
+            'survey_type': survey_type,
+            'questions_layout': 'page_per_question',
+            'access_mode': 'public',
+            'users_login_required': False,
+        })
+
+        # Create questions
+        q01 = self._add_question(
+            None, 'How old are you?', 'simple_choice',
+            sequence=1,
+            constr_mandatory=False, survey_id=survey.id,
+            labels=[
+                {'value': '18-30'},
+                {'value': '30-50'},
+                {'value': '50+'},
+            ])
+        q01.suggested_answer_ids[2].is_create_lead = True
+
+        q02 = self._add_question(
+            None, 'What programming languages do you use on a daily basis?', 'multiple_choice',
+            sequence=2,
+            constr_mandatory=True, constr_error_msg='Please select an answer', survey_id=survey.id,
+            labels=[
+                {'value': 'Assembly'},
+                {'value': 'Java'},
+                {'value': 'C'},
+                {'value': 'Python'},
+            ])
+        q02.suggested_answer_ids[0].is_create_lead = True
+        q02.suggested_answer_ids[2].is_create_lead = True
+
+        q03 = self._add_question(
+            None, 'Please add or verify your email address:', 'char_box',
+            sequence=3,
+            validation_email=True,
+            constr_mandatory=True, constr_error_msg='Please select an answer', survey_id=survey.id,
+            )
+
+        self.assertTrue(q01.is_lead_generating)
+        self.assertTrue(q02.is_lead_generating)
+        self.assertFalse(q03.is_lead_generating)
+
+        return survey
+
+    def test_survey_with_lead_generation_logged_in(self):
+        ### Logged in access + lead created
+        survey = self._create_lead_qualification_survey(survey_type="custom")
+        survey_sudo = survey.sudo()  # sudo to avoid access issues when asserting
+        self.authenticate(self.survey_user.login, self.survey_user.login)
+
+        # Start page
+        self._access_start(survey)
+        user_input = survey_sudo.user_input_ids
+        self.assertEqual(survey_sudo.lead_count, 0)
+        answer_token = user_input.access_token
+
+        # First page
+        response = self._access_page(survey, answer_token)
+        csrf_token = self._find_csrf_token(response.text)
+        self._access_begin(survey, answer_token)
+
+        # Answers
+        question_ids = list(survey.question_ids)
+        self._answer_question(question_ids[0], question_ids[0].suggested_answer_ids.ids[0], answer_token, csrf_token)
+        self._answer_question(question_ids[1], question_ids[1].suggested_answer_ids.ids[0], answer_token, csrf_token)
+        self._answer_question(question_ids[2], self.survey_user.email, answer_token, csrf_token, 'submit')
+
+        ### Check that a lead was created from the survey
+        self.assertEqual(survey_sudo.lead_count, 1)
+        lead_created = survey_sudo.lead_ids
+        self.assertEqual(lead_created.name, f"{self.survey_user.display_name}'s survey results")
+
+        # Ensure that the result values are present in lead description
+        description_expected = Markup("%s" % (
+        "<div>Answers:</div>"
+        "<ul>"
+            f"<li>{question_ids[0].title} — {question_ids[0].suggested_answer_ids[0].value}</li>"
+            f"<li>{question_ids[1].title} — {question_ids[1].suggested_answer_ids[0].value}</li>"
+            f"<li>{question_ids[2].title} — {self.survey_user.email}</li>"
+        "</ul>"
+        ))
+        self.assertEqual(description_expected, lead_created.description)
+
+        # Ensure contact, salesperson, medium, source, email and the contact name are rights
+        self.assertEqual(lead_created.partner_id, self.survey_user.partner_id)
+        self.assertFalse(lead_created.user_id.id)
+        self.assertEqual(lead_created.medium_id.name, "Survey")
+        self.assertEqual(lead_created.source_id.name, survey.title)
+        self.assertEqual(lead_created.email_from, self.survey_user.email)
+        self.assertEqual(lead_created.contact_name, self.survey_user.partner_id.name)
+
+        # Ensure Odoobot created the lead
+        self.assertTrue(all(m.author_id.id == self.env.ref('base.user_root').partner_id.id for m in lead_created.message_ids))
+
+    def test_survey_with_lead_generation_public(self):
+        ### Public access + lead created
+        # Before, add survey manager in a sales team
+        sales_team = self.env['crm.team'].create({
+            'name': 'Odoo Survey Team',
+            'use_leads': True
+        })
+        sales_team.member_ids = [(4, self.survey_manager.id)]
+        survey = self._create_lead_qualification_survey()
+        survey_sudo = survey.sudo()  # sudo to avoid access issues when asserting
+        self.authenticate(None, None)
+
+        # Start page
+        self._access_start(survey)
+        user_input = self.env['survey.user_input'].search([('survey_id', '=', survey.id)])
+        self.assertEqual(survey_sudo.lead_count, 0)
+        answer_token = user_input.access_token
+
+        # First page
+        response = self._access_page(survey, answer_token)
+        csrf_token = self._find_csrf_token(response.text)
+        self._access_begin(survey, answer_token)
+
+        # Answers
+        question_ids = list(survey.question_ids)
+        self._answer_question(question_ids[0], question_ids[0].suggested_answer_ids.ids[0], answer_token, csrf_token)
+        self._answer_question(question_ids[1], [question_ids[1].suggested_answer_ids.ids[0], question_ids[1].suggested_answer_ids.ids[1]],
+                              answer_token, csrf_token)
+        user_email_answer = "beautiful_address@example.com"
+        self._answer_question(question_ids[2], user_email_answer, answer_token, csrf_token, 'submit')
+
+        ### Check that a lead was created from the survey
+        self.assertEqual(survey_sudo.lead_count, 1)
+        lead_created = survey_sudo.lead_ids
+        self.assertEqual(lead_created.name, f"{user_email_answer}'s survey results")
+
+        # Ensure that the result values are present in lead description
+        description_expected = Markup("%s" % (
+        "<div>Answers:</div>"
+        "<ul>"
+            f"<li>{question_ids[0].title} — {question_ids[0].suggested_answer_ids[0].value}</li>"
+            f"<li>{question_ids[1].title} — {question_ids[1].suggested_answer_ids[0].value}, {question_ids[1].suggested_answer_ids[1].value}</li>"
+            f"<li>{question_ids[2].title} — {user_email_answer}</li>"
+        "</ul>"
+        ))
+        self.assertEqual(description_expected, lead_created.description)
+
+        # Ensure contact, salesperson, medium, source, email and contact name are rights
+        self.assertFalse(lead_created.partner_id.id)  # Public user
+        self.assertEqual(lead_created.user_id.id, survey.user_id.id)  # Survey created by a sales team person
+        self.assertEqual(lead_created.medium_id.name, "Survey")
+        self.assertEqual(lead_created.source_id.name, survey.title)
+        self.assertEqual(lead_created.email_from, user_email_answer)
+        self.assertEqual('', lead_created.contact_name)
+
+        # Ensure Odoobot created the lead
+        self.assertTrue(all(m.author_id.id == self.env.ref('base.user_root').partner_id.id for m in lead_created.message_ids))
+
+    def test_survey_without_lead_generation_logged_in(self):
+        ### Logged in access + no lead created
+        survey = self._create_lead_qualification_survey("live_session")
+        survey_sudo = survey.sudo()  # sudo to avoid access issues when asserting
+        self.authenticate(self.survey_user.login, self.survey_user.login)
+
+        # Start page
+        self._access_start(survey)
+        user_input = self.env['survey.user_input'].search([('survey_id', '=', survey.id)])
+        self.assertEqual(survey_sudo.lead_count, 0)
+        answer_token = user_input.access_token
+
+        # First page
+        response = self._access_page(survey, answer_token)
+        csrf_token = self._find_csrf_token(response.text)
+        self._access_begin(survey, answer_token)
+
+        # Answers
+        question_ids = list(survey.question_ids)
+        self._answer_question(question_ids[0], question_ids[0].suggested_answer_ids.ids[0], answer_token, csrf_token)
+        self._answer_question(question_ids[1], question_ids[1].suggested_answer_ids.ids[3], answer_token, csrf_token)
+        self._answer_question(question_ids[2], self.survey_user.email, answer_token, csrf_token, 'submit')
+
+        ### Check that no lead was created
+        self.assertEqual(survey_sudo.lead_count, 0)
