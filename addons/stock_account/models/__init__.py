16fa4d2a133f493e19bff5565fd1c5cd5d4926e7
lase@odoo.com <lase@odoo.com>
2025-06-24 09:37:49 +0200

    [FIX] stock{,_account},l10n_ar_stock: move country_code from stock to stock_account
    Issue:
    
    It is impossible to install stock without account.
    
    Cause of the issue:
    
    The `country_code` field of the `stock.picking.type` model was added to
    the stock module in 1021e52d02384c2a7ddb097901e9c702a6b9e5bf
    The issue is that this field is a related field via the
    `account_fiscal_country_id` of the `res.company` model:
    https://github.com/odoo/odoo/blob/36ce3430fb228a312352eb80e51dbc555f8ae5dc/addons/account/models/company.py#L209-L210
    and hence can only be initialised if the account module is not installed.
    
    closes odoo/odoo#215535
    
    Signed-off-by: Christophe Monniez (moc) <moc@odoo.com>

--- addons/stock_account/models/__init__.py
+++ addons/stock_account/models/__init__.py
@@ -11,6 +11,7 @@ from . import stock_location
 from . import stock_lot
 from . import stock_move_line
 from . import stock_picking
+from . import stock_picking_type
 from . import stock_quant
 from . import stock_valuation_layer
 from . import res_config_settings
