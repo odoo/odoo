8d40bbff8c884a2b1c58d798701b918f01f592ea
Subhadeep R. Dev <sdev@odoo.com>
2025-06-24 11:17:44 +0000

    [FIX] html_editor: correct image transform resize cursor placement
    The resize cursors for image transform in the website editor were
    incorrectly positioned.
    
    Cause:
    The cursor positioning calculation mistakenly used the iframe window's
    scroll offset (`win.pageYOffset`) instead of the global `window`'s
    scroll offset, which is the correct context for the image transform
    overlay.
    
    Steps to reproduce:
    1. In edit mode, add an image.
    2. Click on the image.
    3. Scroll down the document.
    4. Click on the image transform button.
    (Observe: Cursors are misplaced relative to the
    image.)
    
    Related to task-4367641
    
    X-original-commit: 712c5d5176780f9a6574f34f5190f30b1bf2e3f3
    Part-of: odoo/odoo#219326
    Signed-off-by: Colin Louis (loco) <loco@odoo.com>

--- addons/html_editor/static/src/main/media/image_transformation.js
+++ addons/html_editor/static/src/main/media/image_transformation.js
@@ -318,7 +318,6 @@ export class ImageTransformation extends Component {
             return { top: 0, left: 0 };
         } else {
             const rect = target.getBoundingClientRect();
-            const win = target.ownerDocument.defaultView;
             const frameElement = target.ownerDocument.defaultView.frameElement;
             const offset = { top: 0, left: 0 };
             if (frameElement) {
@@ -327,8 +326,8 @@ export class ImageTransformation extends Component {
                 offset.top += frameRect.top;
             }
             return {
-                top: rect.top + win.pageYOffset + offset.top,
-                left: rect.left + win.pageXOffset + offset.left,
+                top: rect.top + window.pageYOffset + offset.top,
+                left: rect.left + window.pageXOffset + offset.left,
             };
         }
     }
