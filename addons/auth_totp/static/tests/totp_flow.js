8cf2d951232008a6384dfcfde8886fc6a4ae572d
Pierre Pulinckx <pipu@odoo.com>
2025-06-24 06:29:42 +0000

    [FIX] auth_totp: change login page step
    In this commit, we change the login page step by a step with isActive
    property. So this step is only active when the input#login is not on
    the page and then expectUnloadPage will be only listen when this step
    is active.
    
    X-original-commit: 028832f1b26c4e8627d2b067af0f79d3b160b160

--- addons/auth_totp/static/tests/totp_flow.js
+++ addons/auth_totp/static/tests/totp_flow.js
@@ -139,7 +139,8 @@ registry.category("web_tour.tours").add('totp_login_enabled', {
     url: '/',
     steps: () => [{
     content: "check that we're on the login page or go to it",
-    trigger: 'input#login, a:contains(Sign in)',
+    isActive: ["body:not(:has(input#login))"],
+    trigger: "a:contains(Sign in)",
     run: "click",
     expectUnloadPage: true,
 }, {
@@ -180,7 +181,8 @@ registry.category("web_tour.tours").add('totp_login_device', {
     url: '/',
     steps: () => [{
     content: "check that we're on the login page or go to it",
-    trigger: 'input#login, a:contains(Sign in)',
+    isActive: ["body:not(:has(input#login))"],
+    trigger: "a:contains(Sign in)",
     run: "click",
     expectUnloadPage: true,
 }, {
@@ -286,7 +288,8 @@ registry.category("web_tour.tours").add('totp_login_disabled', {
     url: '/',
     steps: () => [{
     content: "check that we're on the login page or go to it",
-    trigger: 'input#login, a:contains(Sign in)',
+    isActive: ["body:not(:has(input#login))"],
+    trigger: "a:contains(Sign in)",
     run: "click",
     expectUnloadPage: true,
 }, {
