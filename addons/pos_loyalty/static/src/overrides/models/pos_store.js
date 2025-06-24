3ceb04bf2aa90442430f5bf321bac4a9d872752c
Pedram (PEBR) <pebr@odoo.com>
2025-06-24 13:50:43 +0200

    [FIX] pos_loyalty: auto-claim free product with multiple variants
    Before this commit, when using a free product reward on multi products
    that have several variants, adding the desired product to the order
    would not always trigger the automatic addition of the free product as
    expected.
    
    opw-4841759
    
    closes odoo/odoo#215576
    
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/pos_loyalty/static/src/overrides/models/pos_store.js
+++ addons/pos_loyalty/static/src/overrides/models/pos_store.js
@@ -429,7 +429,9 @@ patch(PosStore.prototype, {
         await this.updatePrograms();
         if (rewardsToApply.length == 1) {
             const reward = rewardsToApply[0];
-            order._applyReward(reward.reward, reward.coupon_id, { product });
+            order._applyReward(reward.reward, reward.coupon_id, {
+                product: result.product_id,
+            });
         }
         this.updateRewards();
 
