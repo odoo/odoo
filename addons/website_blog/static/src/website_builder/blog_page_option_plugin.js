9c5bb9b63e11decff02cdb02ff65d9537ab36062
ptpu-odoo <ptpu@odoo.com>
2025-06-24 11:03:57 +0530

    [IMP] website_blog: allow user to select next article manually
    Before this commit, `BuilderButton` was used to toggle next article which could show the
    next article based on the next post order under "Bottom" section
    
    After this commit, website editors can choose to either display the none, default next post or
    manually select a specific recommended blog post to promote from a dropdown.
    - On selecting none, no article will be displayed at bottom.
    - On selecting next post, default behaviour of showing next post according to post order will
      appear.
    - On selection recommended post, user can choose the blog post from a `Many2one` dropdown
      of blog post available.
    
    Additionally, a separate section was added to toggle the comments using a `BuilderCheckbox`
    labeled "Comments".
    
    task-3916105

--- addons/website_blog/static/src/website_builder/blog_page_option_plugin.js
+++ addons/website_blog/static/src/website_builder/blog_page_option_plugin.js
@@ -1,6 +1,7 @@
 import { Plugin } from "@html_editor/plugin";
 import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
+import { BuilderAction } from "@html_builder/core/builder_action";
 
 class BlogPageOption extends Plugin {
     static id = "blogPageOption";
@@ -14,7 +15,91 @@ class BlogPageOption extends Plugin {
                 groups: ["website.group_website_designer"],
             },
         ],
+        builder_actions: {
+            ToggleRecommendedBlogAction,
+            NextArticleAction,
+            RecommendedNextArticleAction,
+        },
     };
 }
+class ToggleRecommendedBlogAction extends BuilderAction {
+    static id = "toggleRecommendedBlog";
+    getValue({ editingElement }) {
+        const el = editingElement.querySelector("#o_wblog_post_footer div");
+        const id = parseInt(el?.dataset?.recommendedBlogPostId || 0);
+        return JSON.stringify({ id });
+    }
+    isApplied({ editingElement, value, loadResult }) {
+        const el = editingElement.querySelector("#o_wblog_post_footer div");
+        return parseInt(el.dataset.recommendedBlogPostId || 0);
+    }
 
+    async apply({ editingElement, value }) {
+        const el = editingElement.querySelector("#wrap");
+        await this.services.orm.write("blog.post", [parseInt(el.dataset.resId)], {
+            recommended_post_id: JSON.parse(value).id,
+        });
+        this.config.reloadEditor();
+    }
+}
+
+class NextArticleAction extends BuilderAction {
+    static id = "nextArticle";
+    static dependencies = ["builderActions"];
+
+    isApplied({ params }) {
+        return this.dependencies.builderActions.getAction("websiteConfig").isApplied({ params });
+    }
+
+    async apply(context) {
+        await this.dependencies.builderActions.getAction("websiteConfig").apply(context);
+
+        if (context.params.views[0] === "website_blog.opt_blog_post_read_next") {
+            const el = context.editingElement.querySelector("#wrap");
+            await this.services.orm.write("blog.post", [parseInt(el.dataset.resId)], {
+                recommended_post_id: false,
+            });
+            this.config.reloadEditor();
+        }
+    }
+}
+
+class RecommendedNextArticleAction extends BuilderAction {
+    static id = "recommendedNextArticle";
+    static dependencies = ["builderActions"];
+    isApplied({ params }) {
+        return this.dependencies.builderActions.getAction("websiteConfig").isApplied({ params });
+    }
+    async apply(context) {
+        await this.dependencies.builderActions.getAction("websiteConfig").apply(context);
+
+        if (context.params.views[0] === "website_blog.opt_blog_post_read_next_recommended") {
+            const el = context.editingElement.querySelector("#wrap");
+            const currentPostId = parseInt(el.dataset.resId);
+
+            // Get all blog post IDs in ascending order
+            const allPostIds = await this.services.orm.search("blog.post", [], { order: "id asc" });
+            const currentIndex = allPostIds.indexOf(currentPostId);
+            let nextPostId = null;
+
+            if (allPostIds.length > 1 && currentIndex !== -1) {
+                nextPostId = allPostIds[(currentIndex + 1) % allPostIds.length];
+            } else {
+                nextPostId = allPostIds[0]; // If no next post, loop to the first post
+            }
+
+            if (nextPostId) {
+                const [nextPost] = await this.services.orm.read(
+                    "blog.post",
+                    [nextPostId],
+                    ["name"]
+                );
+                await this.services.orm.write("blog.post", [parseInt(el.dataset.resId)], {
+                    recommended_post_id: nextPost.id,
+                });
+                this.config.reloadEditor();
+            }
+        }
+    }
+}
 registry.category("website-plugins").add(BlogPageOption.id, BlogPageOption);
