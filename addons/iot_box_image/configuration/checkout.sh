dc13ed96fb0788cbd42bec6112ead28cb730285e
Louis Travaux <lotr@odoo.com>
2025-06-24 07:47:57 +0000

    [IMP] iot_box_image: update odoo.conf on checkout
    To adapt to the merge of `hw_drivers` and `hw_posbox_homepage` into `iot_drivers`,
    we need to update the `odoo.conf` file to change the modules to load.
    
    Task: 4860513
    X-original-commit: 04f91f47a6681a9f3392c6423f5eea776a53c8ec

---
+++ addons/iot_box_image/configuration/checkout.sh
@@ -0,0 +1,37 @@
+<<<<<<< HEAD
+||||||| MERGE BASE
+=======
+#!/usr/bin/env bash
+
+cd /home/pi/odoo
+localbranch=$(git symbolic-ref -q --short HEAD)
+localremote=$(git config branch.$localbranch.remote)
+
+if [[ "$(git remote get-url "$localremote")" != *odoo/odoo* ]]; then
+    git remote set-url "${localremote}" "https://github.com/odoo/odoo.git"
+fi
+
+echo "addons/iot_base" >> .git/info/sparse-checkout
+echo "addons/iot_drivers" >> .git/info/sparse-checkout
+
+git fetch "${localremote}" "${localbranch}" --depth=1
+git reset "${localremote}"/"${localbranch}" --hard
+sudo git clean -dfx
+
+if [ -d /home/pi/odoo/addons/point_of_sale ]; then
+  # TODO: remove this when v18.0 is deprecated (point_of_sale/tools/posbox/ -> iot_box_image/)
+  sudo sed -i 's|iot_box_image|point_of_sale/tools/posbox|g' /root_bypass_ramdisks/etc/systemd/system/ramdisks.service
+
+  # TODO: Remove this code when v16 is deprecated
+  odoo_conf="addons/point_of_sale/tools/posbox/configuration/odoo.conf"
+  if ! grep -q "server_wide_modules" $odoo_conf; then
+      echo "server_wide_modules=hw_drivers,hw_posbox_homepage,web" >> $odoo_conf
+  fi
+fi
+
+if [ -d /home/pi/odoo/addons/iot_drivers ]; then
+  # TODO: remove this when v18.0 is deprecated (hw_drivers/,hw_posbox_homepage/ -> iot_drivers/)
+  sed -i 's|hw_drivers.*hw_posbox_homepage|iot_drivers|g' /home/pi/odoo.conf
+fi
+
+>>>>>>> FORWARD PORTED
