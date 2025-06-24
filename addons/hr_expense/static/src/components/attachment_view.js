48b599a690d346746af503b23c1c8b2c27228649
jond-odoo <jond@odoo.com>
2025-06-24 13:39:25 +0200

    [IMP] hr_expense: Improved List View styling
    The users can now see the attachments linked to the focused expense
    in the list view (with a lot of similarities with the journal items
    list view).
    
    The "Paid by" and "Activities" columns have been redesigned.
    
    task-id: 4492474

--- addons/hr_expense/static/src/components/attachment_view.js
+++ addons/hr_expense/static/src/components/attachment_view.js
@@ -1,5 +1,7 @@
 import { patch } from "@web/core/utils/patch";
 import { AttachmentView } from "@mail/core/common/attachment_view";
+import { onMounted } from "@odoo/owl";
+import { useBus } from "@web/core/utils/hooks";
 
 patch(AttachmentView.prototype, {
     get displayName() {
@@ -9,3 +11,18 @@ patch(AttachmentView.prototype, {
         return super.displayName;
     }
 });
+
+export class ExpenseAttachment extends AttachmentView {
+    static props = [...AttachmentView.props, "openInPopout"];
+    static components = { AttachmentView };
+
+    setup() {
+        super.setup();
+        if (this.props.openInPopout) {
+            onMounted(this.onClickPopout);
+        }
+        // In the mail_popout_service.js the function pollClosedWindow will trigger a resize event on the main window
+        // when the external window is closed. It allows us to know when to hide the preview for small screens.
+        useBus(this.uiService.bus, "resize", () => this.env.setPopout(false));
+    }
+}
