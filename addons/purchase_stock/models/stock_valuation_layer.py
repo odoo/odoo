6dcae9f09d9a13de8d7864cb0530f35745f632c9
plha-odoo <plha@odoo.com>
2025-06-25 15:19:49 +0000

    [FIX] stock_account,stock_dropshipping: create svl correct value backorder dropship
    **Problem:**
    when the backorder of the delivery (with a bill) of a dropshipped
    fifo/avco product is validated, the svl created don't have the
    right values
    
    **Steps to reproduce:**
    - create a new product, with dropship and buy routes
    - in the purchase tab select "on ordered quantities"
    - add a vendor with a price of 10
    - create a new quotation for a quantity of 10
    - confirm and confirm the purchase order
    - click on "create bill" and confirm it
    - go back to the PO and click on the "dropship" smart button
    - change the quantity to 5, validate and create backorder
    - go back to the PO, click on the "dropship" smart button
    and select the picking of the backorder (with status ready)
    - validate
    - click on the "valuation smart" button
    
    **Current behavior:**
    the svl created for the backorder have a value of 100 and
    -100
    
    **Expected behavior:**
    it should be 50 and -50
    
    **Cause of the issue:**
    inside _get_dropshipped_svl_vals
    _get_price_unit is called
    https://github.com/odoo/odoo/blob/f7c8cc76f15bc6e974969fb4ab4a93654443b973/addons/stock_account/models/stock_move.py#L219
    because we created a bill and it's a backorder
    line.qty_invoiced is higher than received_qty and this
    condition is true
    https://github.com/odoo/odoo/blob/f7c8cc76f15bc6e974969fb4ab4a93654443b973/addons/purchase_stock/models/stock_move.py#L51
    but because it's a dropship there is as much positive svl
    as negative svl linked to the move so receipt value is null
    https://github.com/odoo/odoo/blob/f7c8cc76f15bc6e974969fb4ab4a93654443b973/addons/purchase_stock/models/stock_move.py#L56-L63
    and remaining value will be 100 instead of 50 (receipt value
    should have been 50)
    https://github.com/odoo/odoo/blob/f7c8cc76f15bc6e974969fb4ab4a93654443b973/addons/purchase_stock/models/stock_move.py#L80
    
    **fix**
    the negative svl from the dropshipped move should not
    impact receipt value
    
    opw-4888827
    
    X-original-commit: abeb0e084877377f697664ffae03669ccd5956f0

--- addons/purchase_stock/models/stock_valuation_layer.py
+++ addons/purchase_stock/models/stock_valuation_layer.py
@@ -10,3 +10,16 @@ class StockValuationLayer(models.Model):
         :param layer: the layer the price unit is derived from
         """
         return self.value / self.quantity
+
+    def _should_impact_price_unit_receipt_value(self):
+        # In case of dropshipping, we only want the positive layers. When returned,
+        # only the negative one matters
+        res = super()._should_impact_price_unit_receipt_value()
+        if not self.stock_move_id:
+            return res
+
+        return (
+            res
+            and (not self.stock_move_id._is_dropshipped() or self.value > 0)
+            and (not self.stock_move_id._is_dropshipped_returned() or self.value < 0)
+        )
