4327c78724b697cce264805f78221e597e221021
Serge Bayet (seba) <seba@odoo.com>
2025-06-25 13:05:19 +0000

    [FIX] html_builder: restore custom style action handling in builder
    Following the refactoring introduced in [1], the
    `builder_style_actions` which defines custom behavior for specific CSS
    styles such as `background-color` and `color` was
    unintentionally omitted.
    
    This commit restores its implementation.
    
    The `border-radius` style implementation, that still remains untouched
    will be restored in another PR: [2]
    
    [1]: odoo@b4b2153
    [2]: https://github.com/odoo/odoo/pull/215371
    
    closes odoo/odoo#216624
    
    X-original-commit: 64d75ebd3588918d9d049a4c9148b0154c7ce76f
    Signed-off-by: Colin Louis (loco) <loco@odoo.com>
    Signed-off-by: Serge Bayet (seba) <seba@odoo.com>

--- addons/html_builder/static/src/core/color_style_plugin.js
+++ addons/html_builder/static/src/core/color_style_plugin.js
@@ -2,51 +2,34 @@ import { Plugin } from "@html_editor/plugin";
 import { registry } from "@web/core/registry";
 import { applyNeededCss } from "@html_builder/utils/utils_css";
 import { withSequence } from "@html_editor/utils/resource";
-import { BuilderAction } from "@html_builder/core/builder_action";
 
 class ColorStylePlugin extends Plugin {
     static id = "colorStyle";
     static dependencies = ["color"];
     resources = {
-        builder_style_actions: {
-            BackgroundColorAction,
-            ColorAction,
-        },
         apply_style: withSequence(5, (element, cssProp, color) => {
             applyNeededCss(element, cssProp, color);
             return true;
         }),
+        apply_custom_css_style: withSequence(20, this.applyColorStyle.bind(this)),
     };
-}
-
-export class BackgroundColorAction extends BuilderAction {
-    static id = "background-color";
-    static dependencies = ["color"];
-    getValue(el) {
-        return this.dependencies.color.getElementColors(el)["backgroundColor"];
-    }
-    apply(el, value) {
-        const match = value.match(/var\(--([a-zA-Z0-9-_]+)\)/);
-        if (match) {
-            value = `bg-${match[1]}`;
+    applyColorStyle({ editingElement, params: { mainParam: styleName = "" }, value }) {
+        if (styleName === "background-color") {
+            const match = value.match(/var\(--([a-zA-Z0-9-_]+)\)/);
+            if (match) {
+                value = `bg-${match[1]}`;
+            }
+            this.dependencies.color.colorElement(editingElement, value, "backgroundColor");
+            return true;
+        } else if (styleName === "color") {
+            const match = value.match(/var\(--([a-zA-Z0-9-_]+)\)/);
+            if (match) {
+                value = `text-${match[1]}`;
+            }
+            this.dependencies.color.colorElement(editingElement, value, "color");
+            return true;
         }
-        this.dependencies.color.colorElement(el, value, "backgroundColor");
-    }
-}
-
-export class ColorAction extends BuilderAction {
-    static id = "color";
-    static dependencies = ["color"];
-    getValue(el) {
-        console.log(el);
-        return this.dependencies.color.getElementColors(el)["color"];
-    }
-    apply(el, value) {
-        const match = value.match(/var\(--([a-zA-Z0-9-_]+)\)/);
-        if (match) {
-            value = `text-${match[1]}`;
-        }
-        this.dependencies.color.colorElement(el, value, "color");
+        return false;
     }
 }
 
