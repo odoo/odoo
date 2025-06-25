26553a6a62e1da799efb1a598ef951d97db4f1bc
Julien Mougenot <jum@odoo.com>
2025-06-25 10:54:56 +0200

    [FIX] web: Hoot - decrease processing & memory
    In this commit, the Hoot test runner has been modified in an attempt to
    free up memory and remove some needless processing during the test run.
    
    Practically, this means that:
    
    - tests filtered from the suite are removed from the test runner
    entirely;
    
    - empty suites do not run any code (previously: their 'before' and
    'after' hooks were called regardless of their content);
    
    - the "focus required" test tag function has been moved to a named
    function.
    
    Part-of: odoo/odoo#215920
    Related: odoo/enterprise#88559
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/lib/hoot/core/suite.js
+++ addons/web/static/lib/hoot/core/suite.js
@@ -9,6 +9,32 @@ import { Job } from "./job";
  * @typedef {import("./test").Test} Test
  */
 
+//-----------------------------------------------------------------------------
+// Global
+//-----------------------------------------------------------------------------
+
+const {
+    Object: { freeze: $freeze },
+} = globalThis;
+
+//-----------------------------------------------------------------------------
+// Internal
+//-----------------------------------------------------------------------------
+
+class MinimalCallbacks extends Callbacks {
+    add() {}
+    call() {}
+    callSync() {}
+    clear() {}
+}
+
+const SHARED_CALLBACKS = new MinimalCallbacks();
+const SHARED_CURRENT_JOBS = $freeze([]);
+
+//-----------------------------------------------------------------------------
+// Exports
+//-----------------------------------------------------------------------------
+
 /**
  * @param {Pick<Suite, "name" | "parent">} suite
  * @param {...string} message
@@ -49,7 +75,16 @@ export class Suite extends Job {
 
     cleanup() {
         this.parent?.reporting.add({ suites: +1 });
+        this.minimize();
+    }
+
+    minimize() {
+        super.minimize();
+
         this.callbacks.clear();
+
+        this.callbacks = SHARED_CALLBACKS;
+        this.currentJobs = SHARED_CURRENT_JOBS;
     }
 
     increaseSuiteCount() {
