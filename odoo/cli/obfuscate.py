b248dba2b1d2ddb6b127b158d0dccd1a07a15665
Paolo Gatti <pgi@odoo.com>
2025-06-25 20:52:18 +0200

    [IMP] base: `obfuscate` command improvements
    - migrated to argparse
    - conformed the code to the rest of the new commands
    - solved styling issues

--- odoo/cli/obfuscate.py
+++ odoo/cli/obfuscate.py
@@ -1,21 +1,51 @@
+import argparse
 import logging
-import optparse
 import sys
+import textwrap
 from collections import defaultdict
+from pathlib import Path
 
 from odoo.modules.registry import Registry
 from odoo.tools import SQL, config
 
-from . import Command
+from .command import PROG_NAME, Command
 
 _logger = logging.getLogger(__name__)
+
+DEFAULT_FIELDS = [
+    ('mail_tracking_value', 'old_value_char'),
+    ('mail_tracking_value', 'old_value_text'),
+    ('mail_tracking_value', 'new_value_char'),
+    ('mail_tracking_value', 'new_value_text'),
+    ('res_partner', 'name'),
+    ('res_partner', 'complete_name'),
+    ('res_partner', 'email'),
+    ('res_partner', 'phone'),
+    ('res_partner', 'mobile'),
+    ('res_partner', 'street'),
+    ('res_partner', 'street2'),
+    ('res_partner', 'city'),
+    ('res_partner', 'zip'),
+    ('res_partner', 'vat'),
+    ('res_partner', 'website'),
+    ('res_country', 'name'),
+    ('mail_message', 'subject'),
+    ('mail_message', 'email_from'),
+    ('mail_message', 'reply_to'),
+    ('mail_message', 'body'),
+    ('crm_lead', 'name'),
+    ('crm_lead', 'contact_name'),
+    ('crm_lead', 'partner_name'),
+    ('crm_lead', 'email_from'),
+    ('crm_lead', 'phone'),
+    ('crm_lead', 'mobile'),
+    ('crm_lead', 'website'),
+    ('crm_lead', 'description'),
+]
 
 
 class Obfuscate(Command):
     """Obfuscate data in a given odoo database"""
-    def __init__(self):
-        super().__init__()
-        self.cr = None
 
     def _ensure_cr(func):
         def check_cr(self, *args, **kwargs):
@@ -107,7 +137,7 @@ class Obfuscate(Command):
                     cypher_query = cyph_fct(SQL("%s->>%s", sql_field, key), pwd)
                     new_field_value = SQL(
                         """jsonb_set(%s, array[%s], to_jsonb(%s)::jsonb, FALSE)""",
-                        new_field_value, key, cypher_query
+                        new_field_value, key, cypher_query,
                     )
                 cypherings.append(SQL('%s=%s', sql_field, new_field_value))
 
@@ -119,136 +149,166 @@ class Obfuscate(Command):
                 self.begin()
 
     def confirm_not_secure(self):
-        _logger.info("The obfuscate method is not considered as safe to transfer anonymous datas to a third party.")
-        conf_y = input(f"This will alter data in the database {self.dbname} and can lead to a data loss. Would you like to proceed [y/N]? ")
-        if conf_y.upper() != 'Y':
-            self.rollback()
-            sys.exit(0)
-        conf_db = input(f"Please type your database name ({self.dbname}) in UPPERCASE to confirm you understand this operation is not considered secure : ")
-        if self.dbname.upper() != conf_db:
-            self.rollback()
-            sys.exit(0)
+        print(textwrap.dedent(  # noqa: T201
+            f"""\
+                {PROG_NAME}: obfuscation is ** not ** a secure way to anonymize data in your database before sending to a third party.
+                Please carefully review every step to avoid leaking any sensitive information.
+                This procedure will alter data in the database {self.dbname!r} and can lead to data loss.
+            """))
+        questions = ((
+            "Would you like to proceed? (y/N): "
+        ), (
+            "This operation is not considered secure.\n"
+            f"Please type your database name {self.dbname!r} in UPPERCASE to confirm: "
+        ))
+        expected_answer_sets = (
+            {'Y', 'y'},
+            {self.dbname.upper()},
+        )
+
+        for question, expected_answer_set in zip(questions, expected_answer_sets):
+            answer = (input(textwrap.dedent(question)) or '').strip()
+            print()  # noqa: T201
+            if answer not in expected_answer_set:
+                _logger.error("Operation cancelled by user")
+                return False
+
         return True
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.parser.formatter_class = argparse.RawTextHelpFormatter
+        self.parser.add_argument(
+            '-c', '--config', dest='config',
+            help="use a specific configuration file")
+        self.parser.add_argument(
+            '-d', '--database', dest='db_name', default=None,
+            help="database name, connection details will be taken from the config file")
+        self.parser.add_argument(
+            '--pwd', required=True,
+            help="Obfuscation password")
+        self.parser.add_argument(
+            '--fields', metavar='TABLE.COLUMN,...',
+            help=textwrap.dedent("""\
+                Comma separated list of 'table.columns' to obfuscate/unobfuscate.
+                i.e.: table1.column1,table2.column1,table2.column2
+            """))
+        self.parser.add_argument(
+            '--exclude', metavar='TABLE.COLUMN,...',
+            help=textwrap.dedent("""\
+                Comma separated list of 'table.columns' to exclude from obfuscate/unobfuscate.
+                i.e.: table1.column1,table2.column1,table2.column2
+            """))
+        self.parser.add_argument(
+            '--file', type=Path,
+            help="File containing the list of table.columns to obfuscate/unobfuscate")
+        self.parser.add_argument(
+            '--unobfuscate', action='store_true')
+        self.parser.add_argument(
+            '--allfields', action='store_true',
+            help=textwrap.dedent("""\
+                Used in unobfuscate mode, try to unobfuscate all fields.
+                Cannot be used in obfuscate mode. Slower than specifying fields.
+            """))
+        self.parser.add_argument(
+            '--vacuum', action='store_true',
+            help="Vacuum database after unobfuscating")
+        self.parser.add_argument(
+            '--pertablecommit', action='store_true',
+            help="Commit after each table instead of a big transaction")
+        self.parser.add_argument(
+            '-y', '--yes', action='store_true',
+            help=textwrap.dedent("""\
+                Don't ask for manual confirmation. Use it carefully as the obfuscate
+                method is not considered as safe to transfer anonymous data to a
+                third party."""))
+
     def run(self, cmdargs):
-        parser = config.parser
-        parser.prog = self.prog
-        group = optparse.OptionGroup(parser, "Obfuscate Configuration")
-        group.add_option('--pwd', dest="pwd", default=False, help="Cypher password")
-        group.add_option('--fields', dest="fields", default=False, help="List of table.columns to obfuscate/unobfuscate: table1.column1,table2.column1,table2.column2")
-        group.add_option('--exclude', dest="exclude", default=False, help="List of table.columns to exclude from obfuscate/unobfuscate: table1.column1,table2.column1,table2.column2")
-        group.add_option('--file', dest="file", default=False, help="File containing the list of table.columns to obfuscate/unobfuscate")
-        group.add_option('--unobfuscate', action='store_true', default=False)
-        group.add_option('--allfields', action='store_true', default=False, help="Used in unobfuscate mode, try to unobfuscate all fields. Cannot be used in obfuscate mode. Slower than specifying fields.")
-        group.add_option('--vacuum', action='store_true', default=False, help="Vacuum database after unobfuscating")
-        group.add_option('--pertablecommit', action='store_true', default=False, help="Commit after each table instead of a big transaction")
-        group.add_option(
-            '-y', '--yes', dest="yes", action='store_true', default=False,
-            help="Don't ask for manual confirmation. Use it carefully as the obfuscate method is not considered as safe to transfer anonymous datas to a third party.")
+        parsed_args = self.parser.parse_args(args=cmdargs)
 
-        parser.add_option_group(group)
-        if not cmdargs:
-            sys.exit(parser.print_help())
+        config_args = []
+        if parsed_args.config:
+            config_args += ['-c', parsed_args.config]
+        if parsed_args.db_name:
+            config_args += ['-d', parsed_args.db_name]
+        config.parse_config(config_args, setup_logging=True)
 
-        try:
-            opt = config.parse_config(cmdargs, setup_logging=True)
-            if not opt.pwd:
-                _logger.error("--pwd is required")
-                sys.exit("ERROR: --pwd is required")
-            if opt.allfields and not opt.unobfuscate:
-                _logger.error("--allfields can only be used in unobfuscate mode")
-                sys.exit("ERROR: --allfields can only be used in unobfuscate mode")
-            self.dbname = config['db_name']
-            self.registry = Registry(self.dbname)
-            with self.registry.cursor() as cr:
-                self.cr = cr
-                self.begin()
-                if self.check_pwd(opt.pwd):
-                    fields = [
-                            ('mail_tracking_value', 'old_value_char'),
-                            ('mail_tracking_value', 'old_value_text'),
-                            ('mail_tracking_value', 'new_value_char'),
-                            ('mail_tracking_value', 'new_value_text'),
-                            ('res_partner', 'name'),
-                            ('res_partner', 'complete_name'),
-                            ('res_partner', 'email'),
-                            ('res_partner', 'phone'),
-                            ('res_partner', 'mobile'),
-                            ('res_partner', 'street'),
-                            ('res_partner', 'street2'),
-                            ('res_partner', 'city'),
-                            ('res_partner', 'zip'),
-                            ('res_partner', 'vat'),
-                            ('res_partner', 'website'),
-                            ('res_country', 'name'),
-                            ('mail_message', 'subject'),
-                            ('mail_message', 'email_from'),
-                            ('mail_message', 'reply_to'),
-                            ('mail_message', 'body'),
-                            ('crm_lead', 'name'),
-                            ('crm_lead', 'contact_name'),
-                            ('crm_lead', 'partner_name'),
-                            ('crm_lead', 'email_from'),
-                            ('crm_lead', 'phone'),
-                            ('crm_lead', 'mobile'),
-                            ('crm_lead', 'website'),
-                            ('crm_lead', 'description'),
-                        ]
+        db_names = config['db_name']
+        if not db_names or len(db_names) > 1:
+            self.parser.error("Please provide a single database")
+        self.dbname = parsed_args.db_name = db_names[0]
 
-                    if opt.fields:
-                        if not opt.allfields:
-                            fields += [tuple(f.split('.')) for f in opt.fields.split(',')]
-                        else:
-                            _logger.error("--allfields option is set, ignoring --fields option")
-                    if opt.file:
-                        with open(opt.file, encoding='utf-8') as f:
-                            fields += [tuple(l.strip().split('.')) for l in f]
-                    if opt.exclude:
-                        if not opt.allfields:
-                            fields = [f for f in fields if f not in [tuple(f.split('.')) for f in opt.exclude.split(',')]]
-                        else:
-                            _logger.error("--allfields option is set, ignoring --exclude option")
+        # Ask for confirm when obfuscating
+        if not parsed_args.unobfuscate and not parsed_args.yes:
+            if not self.confirm_not_secure():
+                sys.exit(0)
 
-                    if opt.allfields:
-                        fields = self.get_all_fields()
-                    else:
-                        invalid_fields = [f for f in fields if not self.check_field(f[0], f[1])]
-                        if invalid_fields:
-                            _logger.error("Invalid fields: %s", ', '.join([f"{f[0]}.{f[1]}" for f in invalid_fields]))
-                            fields = [f for f in fields if f not in invalid_fields]
+        def record_split(fields_str, sep=','):
+            return [
+                tuple(record.strip().split('.'))
+                for record in (fields_str or '').split(sep)
+            ]
 
-                    if not opt.unobfuscate and not opt.yes:
-                        self.confirm_not_secure()
+        # Get fields
+        if parsed_args.allfields:
+            if not parsed_args.unobfuscate:
+                self.parser.error("--allfields can only be used in unobfuscate mode")
+            if parsed_args.fields:
+                _logger.error("--allfields option is set, the --fields option will be ignored")
+            if parsed_args.exclude:
+                _logger.error("--allfields option is set, the --exclude option will be ignored")
+            fields = []
+        else:
+            fields = [
+                field for field in [
+                    *DEFAULT_FIELDS,
+                    *record_split(parsed_args.fields),
+                    *(record_split(Path.read_text(parsed_args.file), '\n') if parsed_args.file else []),
+                ]
+                if field not in record_split(parsed_args.exclude)
+            ]
 
-                    _logger.info("Processing fields: %s", ', '.join([f"{f[0]}.{f[1]}" for f in fields]))
-                    tables = defaultdict(set)
+        with Registry(self.dbname).cursor() as cr:
+            self.cr = cr
 
-                    for t, f in fields:
-                        if t[0:3] != 'ir_' and '.' not in t:
-                            tables[t].add(f)
+            self.begin()
+            if not self.check_pwd(parsed_args.pwd):
+                self.rollback()
+                sys.exit(0)
+            if parsed_args.allfields:
+                fields = self.get_all_fields()
+            else:
+                invalid_fields = [f for f in fields if not self.check_field(f[0], f[1])]
+                if invalid_fields:
+                    _logger.error("Invalid fields: %s", ', '.join([f"{f[0]}.{f[1]}" for f in invalid_fields]))
+                    fields = [f for f in fields if f not in invalid_fields]
 
-                    if opt.unobfuscate:
-                        _logger.info("Unobfuscating datas")
-                        for table in tables:
-                            _logger.info("Unobfuscating table %s", table)
-                            self.convert_table(table, tables[table], opt.pwd, opt.pertablecommit, True)
+            _logger.info("Processing fields: %s", ', '.join([f"{f[0]}.{f[1]}" for f in fields]))
+            tables = defaultdict(set)
 
-                        if opt.vacuum:
-                            _logger.info("Vacuuming obfuscated tables")
-                            for table in tables:
-                                _logger.debug("Vacuuming table %s", table)
-                                self.cr.execute(SQL("VACUUM FULL %s", SQL.identifier(table)))
-                        self.clear_pwd()
-                    else:
-                        _logger.info("Obfuscating datas")
-                        self.set_pwd(opt.pwd)
-                        for table in tables:
-                            _logger.info("Obfuscating table %s", table)
-                            self.convert_table(table, tables[table], opt.pwd, opt.pertablecommit)
+            for t, f in fields:
+                if t[0:3] != 'ir_' and '.' not in t:
+                    tables[t].add(f)
 
-                    self.commit()
-                else:
-                    self.rollback()
+            if parsed_args.unobfuscate:
+                _logger.info("Unobfuscating data")
+                for table in tables:
+                    _logger.info("Unobfuscating table %s", table)
+                    self.convert_table(table, tables[table], parsed_args.pwd, parsed_args.pertablecommit, True)
 
-        except Exception as e:  # noqa: BLE001
-            sys.exit("ERROR: %s" % e)
+                if parsed_args.vacuum:
+                    _logger.info("Vacuuming obfuscated tables")
+                    for table in tables:
+                        _logger.debug("Vacuuming table %s", table)
+                        self.cr.execute(SQL("VACUUM FULL %s", SQL.identifier(table)))
+                self.clear_pwd()
+
+            else:
+                _logger.info("Obfuscating data")
+                self.set_pwd(parsed_args.pwd)
+                for table in tables:
+                    _logger.info("Obfuscating table %s", table)
+                    self.convert_table(table, tables[table], parsed_args.pwd, parsed_args.pertablecommit)
+
+            self.commit()
