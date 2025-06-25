945dea93460082aca2d84549b762ae69acd8bfbd
David (dafr) <dafr@odoo.com>
2025-06-25 12:39:56 +0200

    [FIX] mrp: Fix rounding issue on BoM explode
    ## How to reproduce
    - Create 3 products: "FNS" / "CMP 1" / "CMP 2", the three of them with an UoM of 2 digits precision
    - Create Kit BoM for 1 Unit of "FNS" using 10 Units of "CMP 1"
    - Create Kit BoM for 5000 Units of "CMP 1" using 50 of "CMP 2"
    - Check "FNS" BoM Overview:
      - "CMP 2" quantity shows the correct 0.10 Units (1 * 10 / 5000 * 50) (ok)
    - Create Delivery order for 1 Unit of FNS
      - "CMP 2" quantity is 0.50 Units (KO)
    
    OPW-4804958

--- addons/mrp/models/mrp_bom.py
+++ addons/mrp/models/mrp_bom.py
@@ -402,7 +402,9 @@ class MrpBom(models.Model):
                 product_ids.clear()
             bom = product_boms.get(current_line.product_id)
             if bom:
-                converted_line_quantity = current_line.product_uom_id._compute_quantity(line_quantity / bom.product_qty, bom.product_uom_id)
+                converted_line_quantity = current_line.product_uom_id._compute_quantity(
+                    line_quantity / bom.product_qty, bom.product_uom_id, round=False
+                )
                 bom_lines += [(line, current_line.product_id, converted_line_quantity, current_line) for line in bom.bom_line_ids]
                 for bom_line in bom.bom_line_ids:
                     if not bom_line.product_id in product_boms:
