78c70068f3ef8552188f08daf1abd6d1dff41dbe
Renaud Thiry <reth@odoo.com>
2025-06-25 12:01:16 +0000

    [FIX] mail: properly count activities for systray update
    Currently the systray counter is only updated at:
    - create
    - unlink
    - write, but only if the user changes
    
    We want to detect changes such as activities being archived
    or due dates being updated to properly reflect the changes
    as the user applies them.
    
    We now do a separate check for activity count per user
    before and after a write call that modifies these fields.
    
    Additionally since we now have a count we provide that
    as part of the bus notification so that it can be updated
    accurately when modifying/creation/deleting activities in batch.
    
    Common activity test utils are updated to avoid false negatives.
    
    auto-subscription on activity assignment is made slightly more efficient.
    
    task-4862215
    
    X-original-commit: 83c414b78b4565b8f48d57629fcb17565f44f6bd

--- addons/mail/static/src/core/web/mail_core_web_service.js
+++ addons/mail/static/src/core/web/mail_core_web_service.js
@@ -15,12 +15,18 @@ export class MailCoreWeb {
 
     setup() {
         this.busService.subscribe("mail.activity/updated", (payload, { id: notifId }) => {
-            if (payload.activity_created && notifId > this.store.activity_counter_bus_id) {
-                this.store.activityCounter++;
+            if (notifId <= this.store.activity_counter_bus_id) {
+                return;
             }
-            if (payload.activity_deleted && notifId > this.store.activity_counter_bus_id) {
-                this.store.activityCounter--;
+            let countDiff = 0;
+            if ("count_diff" in payload) {
+                countDiff = payload.count_diff;
+            } else if (payload.activity_created) {
+                countDiff = 1;
+            } else if (payload.activity_deleted) {
+                countDiff = -1;
             }
+            this.store.activityCounter += countDiff;
         });
         this.env.bus.addEventListener("mail.message/delete", ({ detail: { message, notifId } }) => {
             if (message.needaction && notifId > this.store.inbox.counter_bus_id) {
