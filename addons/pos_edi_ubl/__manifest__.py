ba46e364b8d0da4200d0734ef08844e927ddb179
Antoine Dupuis <andu@odoo.com>
2025-06-26 14:31:43 +0000

    [IMP] l10n_co{_pos}, pos_edi_ubl, account_edi_ubl_cii: Prepare for l10n_co_dian_pos
    - to make PoS tests work.
    
    - add the `total_tax_amount` key to the values added by
      `_add_monetary_total_vals`.
    
    - add the `ext:UBLExtensions` node to the UBL 2.1 Invoice, CreditNote
      and DebitNote templates.
    
    - in case of a vendor bill, always put the partner as supplier and the
      company as customer.
    
    - An AllowanceCharge can have an ID child tag according to the
      specification of the AllowanceCharge Tag.
    
    - removes the POS tour because it has no value in this module.
      A useful POS tour is implemented in the `l10n_co_edi_pos` module.
    
    task-4038651
    
    closes odoo/odoo#248278
    
    X-original-commit: f862e4f1839c738d2db1efd3096e9f6481d5ffe8
    Related: odoo/enterprise#107169
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- addons/pos_edi_ubl/__manifest__.py
+++ addons/pos_edi_ubl/__manifest__.py
@@ -1,6 +1,5 @@
 {
     'name': 'Point of Sale UBL',
-    'author': 'Odoo S.A.',
     'version': '1.0',
     'category': 'Sales/Point of Sale',
     'sequence': 6,
@@ -11,5 +10,6 @@ This module provides helpers for exporting POS orders in various UBL 2.1 formats
 
 """,
     'depends': ['point_of_sale', 'account_edi_ubl_cii'],
+    'author': 'Odoo S.A.',
     'license': 'LGPL-3',
 }
