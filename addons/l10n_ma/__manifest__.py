4c654283c3c72d2d4725783755311c9cb542a43e
Saif (segn) <segn@odoo.com>
2025-06-26 12:23:46 +0000

    [IMP] account,l10n_ma: Add ICE number to invoicing
    Reason: The ICE (Identifiant Commun de l'Entreprise) is an identification number assigned to businesses and legal entities for various administrative and legal purposes in Morocco. If the partner has one, it must be indicated on the invoice.
    
    The ICE number is supposed to be put on all the invoices made to Moroccan companies, whatever the country of the company issuing those invoices. We hence add that directly into the account module.
    
    Also, instead of redefining the label to be ICE number in the views, use
    invisible-add override to hide the field and re-add it with the correct
    label.
    
    task-4879950
    
    closes odoo/odoo#239135
    
    X-original-commit: 2a6f08bf26e34222a788396a2e4b6ea815f4104f
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>
    Signed-off-by: Seif Gneedy (segn) <segn@odoo.com>

--- addons/l10n_ma/__manifest__.py
+++ addons/l10n_ma/__manifest__.py
@@ -19,9 +19,8 @@ This module has been built with the help of Caudigef.
     'data': [
         'data/account_tax_report_data.xml',
         'views/portal_address_templates.xml',
-        "views/res_partner_views.xml",
-        "views/res_company_views.xml",
-        'views/report_invoice.xml',
+        'views/res_partner_views.xml',
+        'views/res_company_views.xml',
     ],
     'assets': {
         'web.assets_frontend': [
