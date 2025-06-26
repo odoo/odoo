706fe90ab0d4446b42b698900488c4c576893bd1
Anh Thao Pham (pta) <pta@odoo.com>
2025-06-26 13:41:44 +0000

    [FIX] l10n_ro_edi: prevent EDI to be sent mulitple times
    Steps to reproduce:
    From a Romanian company with the RO localization configured, when
    processing a payment transaction from the e-commerce, it can happen
    that the creation of the invoice fails due to a SerializationFailure
    but the EDI is correctly sent to the government service.
    As the creation of the invoice has failed, the system will retry to
    create it and trigger the sending of the EDI again.
    
    Solution:
    Trigger a commit before sending the EDI.
    
    opw-4844913
    
    closes odoo/odoo#216295
    
    X-original-commit: 4ce9db36ba45d322c471112cafe2cade2b9e8975
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>
    Signed-off-by: Anh Thao Pham (pta) <pta@odoo.com>

--- addons/l10n_ro_edi/models/account_move_send.py
+++ addons/l10n_ro_edi/models/account_move_send.py
@@ -66,6 +66,9 @@ class AccountMoveSend(models.AbstractModel):
                     }
                     continue
 
+                if self._can_commit():
+                    self.env.cr.commit()
+
                 invoice._l10n_ro_edi_send_invoice(xml_data)
 
                 if self._can_commit():
