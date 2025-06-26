ce27dc2ef63a007a3eb459718a432a993f75f318
SaddemAmine <sana@odoo.com>
2025-06-26 18:23:04 +0000

    [IMP] l10n_tr_nilvera{_einvoice}: add Nilvera API tests
    Currently we're lacking tests for the Nilvera API integration. This
    commit adds a test suite that primarily focuses on testing that.
    
    Task ID: 4655864
    
    X-original-commit: a646cb97a9f91a7619841dedd919de69bc781e7f

--- addons/l10n_tr_nilvera_einvoice/tests/__init__.py
+++ addons/l10n_tr_nilvera_einvoice/tests/__init__.py
@@ -1,1 +1,8 @@
+<<<<<<< cd5775258d75296aed1b09fbd305a49a03a536ad
 from . import test_xml_ubl_tr
+||||||| b1a007e4db9f6f844c6d6cee6963d6bc3ba024b8
+from . import test_xml_ubl_tr_common
+=======
+from . import test_tr_nilvera_mocked_requests
+from . import test_xml_ubl_tr_common
+>>>>>>> 100b496bafb50e2e9064475dd5fbed10c3cb8fd9
