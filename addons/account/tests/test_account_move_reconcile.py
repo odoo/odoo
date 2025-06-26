dca89d56b3653b0be2de288e6e97c6a6144c5377
gteboul <gute@odoo.com>
2025-06-26 13:41:17 +0000

    [FIX] account: consider amount_residual in ending reconciliation
    - Create an invoice with price 1000 in a non-company currency (e.g., CHF).
    - Modify the currency rate after the invoice date, then generate the payment for this invoice.
    - This will generate three journal entries (invoice, payment, and currency exchange difference).
    - Export those journal entries and include in the export the `matching_number`, `currency`, and `amount_currency` fields.
    - Import those three entries with the matching number and post them.
    - In the Journal Items, the line corresponding to the currency exchange difference is not matched with the lines from the invoice and the payment, leading to an unbalanced credit and debit.
    
    In `_prepare_reconciliation_single_partial` within `account_move_line`, the reconciliation is stopped by checking whether the debit/credit is fully matched.
    However, this check only considers `amount_residual_currency` and not `amount_residual`, which, in the case of an exchange difference, are different.
    
    opw-4776188
    
    X-original-commit: 6a683a0f3ac078d40b953756c6b464f307d73ddf

--- addons/account/tests/test_account_move_reconcile.py
+++ addons/account/tests/test_account_move_reconcile.py
@@ -5487,3 +5487,23 @@ class TestAccountMoveReconcile(AccountTestInvoicingCommon):
         # Cannot change other fields
         with self.assertRaisesRegex(UserError, "You can just change some non legal fields"):
             receivable_lines.currency_id = self.other_currency
+
+    def test_reconciliation_currency_exchange_matching_number(self):
+        """
+        Test that reconciliation assigns the same matching number to
+        invoice, payment, and currency exchange lines when those lines
+        are directly reconciled (in the case of an import, for instance).
+        """
+        currency_chf = self.env.ref('base.CHF')
+
+        account_receivable = self.company_data['default_account_receivable']
+        invoice_line = self.create_line_for_reconciliation(1000.0, 1000.0, currency_chf, '2025-01-01', account_receivable)
+        payment_line = self.create_line_for_reconciliation(-500.0, -1000.0, currency_chf, '2025-02-01')
+        currency_exchange_line = self.create_line_for_reconciliation(-500.0, -0.0, currency_chf, '2025-02-01', account_receivable)
+
+        lines = invoice_line + payment_line + currency_exchange_line
+        lines.with_context(no_exchange_difference=True, no_exchange_difference_no_recursive=True).reconcile()
+
+        self.assertEqual(invoice_line.matching_number, payment_line.matching_number)
+        self.assertEqual(payment_line.matching_number, currency_exchange_line.matching_number)
+        self.assertEqual(currency_exchange_line.amount_residual, 0)
