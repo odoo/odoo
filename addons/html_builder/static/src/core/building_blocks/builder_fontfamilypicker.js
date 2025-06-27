a46badd36a1b24441cd5a26fb22a3932a7e28e87
Thomas Josse (THJO) <thjo@odoo.com>
2025-06-27 10:51:21 +0000

    [MOV] html_builder: move all generic plugins/options
    This commit moves all the website_builder plugins that will be necessary for the
    mass_mailing conversion to the html_builder module.
    
    This lessens the load of what we would need to adapt for it.
    This move is also done ensuring that the current website_builder is still
    operational.
    
    task-4247642
    
    X-original-commit: cc9878470c9c24a507d7a6833a0f60590b6b855e

---
+++ addons/html_builder/static/src/core/building_blocks/builder_fontfamilypicker.js
@@ -0,0 +1,60 @@
+import { Component, onWillStart } from "@odoo/owl";
+import { _t } from "@web/core/l10n/translation";
+import { useService } from "@web/core/utils/hooks";
+import {
+    basicContainerBuilderComponentProps,
+    useVisibilityObserver,
+    useApplyVisibility,
+} from "@html_builder/core/utils";
+import { ConfirmationDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
+import { BuilderSelect } from "@html_builder/core/building_blocks/builder_select";
+import { BuilderSelectItem } from "@html_builder/core/building_blocks/builder_select_item";
+
+export class BuilderFontFamilyPicker extends Component {
+    static template = "html_builder.BuilderFontFamilyPicker";
+    static props = {
+        ...basicContainerBuilderComponentProps,
+        valueParamName: String,
+    };
+    static components = {
+        BuilderSelect,
+        BuilderSelectItem,
+    };
+
+    setup() {
+        this.dialog = useService("dialog");
+        this.orm = useService("orm");
+        useVisibilityObserver("content", useApplyVisibility("root"));
+        this.fonts = [];
+        onWillStart(async () => {
+            const fontsData = await this.env.editor.shared.builderFont.getFontsData();
+            this.fonts = fontsData._fonts;
+        });
+    }
+    forwardProps(fontValue) {
+        const result = Object.assign({}, this.props, {
+            [this.props.valueParamName]: fontValue.fontFamilyValue,
+        });
+        delete result.selectMethod;
+        delete result.valueParamName;
+        return result;
+    }
+    async onAddFontClick() {
+        await this.env.editor.shared.websiteFont.addFont(this.props.actionParam);
+    }
+    async onDeleteFontClick(font) {
+        const save = await new Promise((resolve) => {
+            this.env.services.dialog.add(ConfirmationDialog, {
+                body: _t(
+                    "Deleting a font requires a reload of the page. This will save all your changes and reload the page, are you sure you want to proceed?"
+                ),
+                confirm: () => resolve(true),
+                cancel: () => resolve(false),
+            });
+        });
+        if (!save) {
+            return;
+        }
+        await this.env.editor.shared.websiteFont.deleteFont(font);
+    }
+}
--- addons/website/static/src/builder/plugins/layout_option/layout_option.js
+++ addons/website/static/src/builder/plugins/layout_option/layout_option.js
@@ -1,6 +1,6 @@
+import { SelectNumberColumn } from "@html_builder/core/select_number_column";
 import { BaseOptionComponent } from "@html_builder/core/utils";
 import { AddElementOption } from "./add_element_option";
-import { SelectNumberColumn } from "./select_number_column";
 import { SpacingOption } from "./spacing_option";
 
 export class LayoutOption extends BaseOptionComponent {
@@ -18,14 +18,6 @@ export class LayoutGridOption extends BaseOptionComponent {
     static components = {
         SpacingOption,
         AddElementOption,
-    };
-    static props = {};
-}
-
-export class LayoutColumnOption extends BaseOptionComponent {
-    static template = "website.LayoutColumnOption";
-    static components = {
-        SelectNumberColumn,
     };
     static props = {};
 }
