9cb9938521b5f13079053c0ac9a9ab4352e444b2
Benoit Socias <bso@odoo.com>
2025-06-27 12:56:16 +0000

    [FIX] website: support removing a customized website color
    When `html_builder` was introduced, the `customizeWebsiteColor` action
    was created to support assigning colors to specific SCSS variables.
    However, it did not support the removal of a previously set color.
    
    This commit handles the removal of such colors.
    Note that it assigns no color, which still overrides the initial value.
    This is the same behavior as before `html_builder`.
    
    Steps to reproduce:
    - select header
    - pick a background color
    - reset the background color
    
    => The background color remained selected.
    
    This commit also:
    - limits the number of reloads of the assets bundles
    - isolates setting color combination from selector color (solid or
    gradient)
    
    task-4367641
    
    closes odoo/odoo#216948
    
    X-original-commit: b3512d76af44d3f48d8b0027495c220df550b602
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Benoit Socias (bso) <bso@odoo.com>

---
+++ addons/website/static/tests/builder/website_builder/customize_website_color.test.js
@@ -0,0 +1,111 @@
+import { expect, test } from "@odoo/hoot";
+import { animationFrame, Deferred } from "@odoo/hoot-dom";
+import { xml } from "@odoo/owl";
+import { contains, defineModels, models, onRpc } from "@web/../tests/web_test_helpers";
+import { addOption, defineWebsiteModels, setupWebsiteBuilder } from "../website_helpers";
+
+defineWebsiteModels();
+
+test("BuilderColorPicker with action “customizeWebsiteColor” is correctly displayed", async () => {
+    class WebEditorAssets extends models.Model {
+        _name = "web_editor.assets";
+        make_scss_customization(location, changes) {
+            expect.step(`${location} ${JSON.stringify(changes)}`);
+        }
+    }
+    defineModels([WebEditorAssets]);
+
+    const def = new Deferred();
+    onRpc("/website/theme_customize_bundle_reload", async (request) => {
+        expect.step("asset reload");
+        return "";
+    });
+    addOption({
+        selector: ".test-options-target",
+        template: xml`
+        <BuilderColorPicker
+            enabledTabs="['theme', 'custom', 'gradient']"
+            preview="false"
+            defaultColor="''"
+            action="'customizeWebsiteColor'"
+            actionParam="{
+                mainParam: 'test-custom',
+                gradientColor: 'test-gradient',
+                combinationColor: 'test',
+                nullValue: 'NULL',
+            }"
+        />
+        `,
+    });
+    await setupWebsiteBuilder(`<div class="test-options-target">b</div>`, {
+        loadIframeBundles: true,
+    });
+    await contains(":iframe .test-options-target").click();
+    def.resolve();
+    await animationFrame();
+    expect(".o-tab-content > .o_customize_tab").toHaveCount(1);
+
+    expect.step("set preset");
+    await contains("button.o_we_color_preview").click();
+    await contains("button[data-color='o_cc4'").click();
+    expect.verifySteps([
+        "set preset",
+        '/website/static/src/scss/options/colors/user_color_palette.scss {"test":4}',
+        "asset reload",
+    ]);
+
+    // Setting solid color does not impact preset
+    expect.step("set solid color");
+    await contains("button.o_we_color_preview").click();
+    await contains("button.custom-tab").click();
+    await contains("button[data-color='400']").click();
+    expect.verifySteps([
+        "set solid color",
+        '/website/static/src/scss/options/colors/user_color_palette.scss {"test-custom":"#CED4DA"}',
+        '/website/static/src/scss/options/user_values.scss {"test-gradient":"NULL"}',
+        "asset reload",
+    ]);
+
+    // Setting preset does not impact solid color
+    expect.step("set preset on solid color");
+    await contains("button.o_we_color_preview").click();
+    await contains("button[data-color='o_cc3'").click();
+    expect.verifySteps([
+        "set preset on solid color",
+        '/website/static/src/scss/options/colors/user_color_palette.scss {"test":3}',
+        "asset reload",
+    ]);
+
+    // Setting gradient does not impact preset
+    expect.step("set gradient");
+    await contains("button.o_we_color_preview").click();
+    await contains("button.gradient-tab").click();
+    await contains("button.o_gradient_color_button").click();
+    expect.verifySteps([
+        "set gradient",
+        '/website/static/src/scss/options/colors/user_color_palette.scss {"test-custom":"NULL"}',
+        '/website/static/src/scss/options/user_values.scss {"test-gradient":"linear-gradient(135deg, rgb(255, 204, 51) 0%, rgb(226, 51, 255) 100%)"}',
+        "asset reload",
+    ]);
+
+    // Setting preset does not impact gradient
+    expect.step("set preset on gradient");
+    await contains("button.o_we_color_preview").click();
+    await contains("button[data-color='o_cc4'").click();
+    expect.verifySteps([
+        "set preset on gradient",
+        '/website/static/src/scss/options/colors/user_color_palette.scss {"test":4}',
+        "asset reload",
+    ]);
+
+    // Clear clears everything
+    expect.step("reset");
+    await contains("button.o_we_color_preview").click();
+    await contains(".o_font_color_selector .fa-trash").click();
+    expect.verifySteps([
+        "reset",
+        '/website/static/src/scss/options/colors/user_color_palette.scss {"test-custom":"NULL","test":"NULL"}',
+        '/website/static/src/scss/options/user_values.scss {"test-gradient":"NULL"}',
+        "asset reload",
+    ]);
+});
