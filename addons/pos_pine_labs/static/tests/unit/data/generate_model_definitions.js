edd15f7e0d855626d17dd53b958a07637ea45c1a
David Monnom <moda@odoo.com>
2025-06-27 14:21:46 +0200

    [ADD] l10n_be_pos_blackbox: add support for the Belgian Blackbox v2

--- addons/pos_pine_labs/static/tests/unit/data/generate_model_definitions.js
+++
@@ -1,8 +0,0 @@
-import { patch } from "@web/core/utils/patch";
-import { PosPayment } from "@point_of_sale/../tests/unit/data/generate_model_definitions";
-
-patch(PosPayment.prototype, {
-    _load_pos_data_fields() {
-        return [...super._load_pos_data_fields(), "pine_labs_plutus_transaction_ref"];
-    },
-});
--- addons/pos_viva_com/static/tests/unit/data/generate_model_definitions.js
+++ addons/pos_viva_com/static/tests/unit/data/generate_model_definitions.js
@@ -1,14 +1,5 @@
 import { patch } from "@web/core/utils/patch";
-import {
-    PosPayment,
-    PosPaymentMethod,
-} from "@point_of_sale/../tests/unit/data/generate_model_definitions";
-
-patch(PosPayment.prototype, {
-    _load_pos_data_fields() {
-        return [...super._load_pos_data_fields(), "viva_com_session_id"];
-    },
-});
+import { PosPaymentMethod } from "@point_of_sale/../tests/unit/data/generate_model_definitions";
 
 patch(PosPaymentMethod.prototype, {
     _load_pos_data_fields() {
