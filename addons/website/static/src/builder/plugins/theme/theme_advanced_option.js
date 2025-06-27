4f0aa78dcf2c1862653f818d2fe37a1cb67081c8
Alessandro Lupo <alup@odoo.com>
2025-06-27 12:29:51 +0200

    [FIX] website: fix builder buttons in `ThemeAdvancedOption`
    Before this commit, the buttons "Code Injection" and "Google Map Custom Key" in
    `ThemeAdvancedOption` were defined with the tag `button` instead of `BuilderButton`
    and were acting via `t-on-click` instead of using a `BuilderAction`.
    
    This commit converts the two elements to `BuilderButton` using `BuilderAction`.
    In addition, unused references are removed from `ThemeTabPlugin`.
    
    **How to reproduce**
    1. In the website builder
    2. In the "Theme" tab
    3. In the "Advanced" section
    4. Problem: the two objects on the rows "Code Injection" and "Custom Key" do not
       look like buttons.
    
    task-4367641

--- addons/website/static/src/builder/plugins/theme/theme_advanced_option.js
+++ addons/website/static/src/builder/plugins/theme/theme_advanced_option.js
@@ -1,23 +1,8 @@
 import { BaseOptionComponent } from "@html_builder/core/utils";
-import { EditHeadBodyDialog } from "@website/components/edit_head_body_dialog/edit_head_body_dialog";
-import { useService } from "@web/core/utils/hooks";
 
 export class ThemeAdvancedOption extends BaseOptionComponent {
     static template = "website.ThemeAdvancedOption";
     static props = {
         grays: Object,
-        configureGMapsAPI: Function,
     };
-
-    setup() {
-        super.setup();
-        this.dialog = useService("dialog");
-    }
-
-    openCustomCodeDialog() {
-        this.dialog.add(EditHeadBodyDialog);
-    }
-    configureApiKey() {
-        this.props.configureGMapsAPI("", true);
-    }
 }
