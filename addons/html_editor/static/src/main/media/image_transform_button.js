ffe0600c0003a5458f41fac3bfaa63741a6e1dd5
Subhadeep R. Dev <sdev@odoo.com>
2025-06-30 17:46:50 +0200

    [REV] html_builder, html_editor: revert "add `transformImage` command"
    This reverts commit fa4a5617b80fa915d63305e962e2ff9984dafa04.
    
    Reason for revert:
    1. The original commit moved event listeners from the
    `ImageTransformButton` component to the `ImagePlugin`, which causes the
    listeners to persist even after the component is destroyed.
    2. An upcoming change will introduce a different implementation of
    `handleImageTransformation()` for the website module due to a functional
    difference in the reset option. As a result, this commit is no longer
    necessary.
    
    Related to task-4367641

--- addons/html_editor/static/src/main/media/image_transform_button.js
+++ addons/html_editor/static/src/main/media/image_transform_button.js
@@ -1,22 +1,110 @@
-import { Component, useState } from "@odoo/owl";
+import { Component, useExternalListener, useState } from "@odoo/owl";
 import { toolbarButtonProps } from "@html_editor/main/toolbar/toolbar";
+import { registry } from "@web/core/registry";
+import { ImageTransformation } from "./image_transformation";
+
 export class ImageTransformButton extends Component {
     static template = "html_editor.ImageTransformButton";
     static props = {
         id: String,
         icon: String,
         title: String,
+        getTargetedImage: Function,
+        resetImageTransformation: Function,
+        addStep: Function,
+        document: { validate: (p) => p.nodeType === Node.DOCUMENT_NODE },
+        editable: { validate: (p) => p.nodeType === Node.ELEMENT_NODE },
         ...toolbarButtonProps,
         activeTitle: String,
-        getTransformState: Function,
-        handleImageTransformation: Function,
     };
 
     setup() {
-        this.state = useState(this.props.getTransformState());
+        this.state = useState({ active: false });
+        this.pointerDownInsideTransform = false;
+        // We close the image transform when we click outside any element not
+        // related to it. When the pointerdown of the click is inside the image
+        // transform and pointerup is outside while resizing or rotating the
+        // image it will consider the click as being done outside image
+        // transform. So we need to keep track if the pointerdown is inside or
+        // outside to know if we want to close the image transform component or
+        // not.
+        useExternalListener(this.props.document, "pointerdown", (ev) => {
+            if (this.isNodeInsideTransform(ev.target)) {
+                this.pointerDownInsideTransform = true;
+            } else {
+                this.closeImageTransformation();
+                this.pointerDownInsideTransform = false;
+            }
+        });
+        useExternalListener(this.props.document, "click", (ev) => {
+            if (!this.isNodeInsideTransform(ev.target) && !this.pointerDownInsideTransform) {
+                this.closeImageTransformation();
+            }
+            this.pointerDownInsideTransform = false;
+        });
+        // When we click on any character the image is deleted and we need to close the image transform
+        // We handle this by selectionchange
+        useExternalListener(this.props.document, "selectionchange", (ev) => {
+            this.closeImageTransformation();
+        });
+    }
+
+    isNodeInsideTransform(node) {
+        if (!node) {
+            return false;
+        }
+        if (node.nodeType === Node.TEXT_NODE) {
+            node = node.parentElement;
+        }
+        if (node.matches('[name="image_transform"], [name="image_transform"] *')) {
+            return true;
+        }
+        if (
+            this.isImageTransformationOpen() &&
+            node.matches(
+                ".transfo-container, .transfo-container div, .transfo-container i, .transfo-container span"
+            )
+        ) {
+            return true;
+        }
+        return false;
     }
 
     onButtonClick() {
-        this.props.handleImageTransformation();
+        this.handleImageTransformation(this.props.getTargetedImage());
+    }
+
+    handleImageTransformation(image) {
+        if (this.isImageTransformationOpen()) {
+            this.props.resetImageTransformation(image);
+            this.closeImageTransformation();
+        } else {
+            this.openImageTransformation(image);
+        }
+    }
+
+    openImageTransformation(image) {
+        this.state.active = true;
+        registry.category("main_components").add("ImageTransformation", {
+            Component: ImageTransformation,
+            props: {
+                image,
+                document: this.props.document,
+                editable: this.props.editable,
+                destroy: () => this.closeImageTransformation(),
+                onChange: () => this.props.addStep(),
+            },
+        });
+    }
+
+    isImageTransformationOpen() {
+        return registry.category("main_components").contains("ImageTransformation");
+    }
+
+    closeImageTransformation() {
+        this.state.active = false;
+        if (this.isImageTransformationOpen()) {
+            registry.category("main_components").remove("ImageTransformation");
+        }
     }
 }
