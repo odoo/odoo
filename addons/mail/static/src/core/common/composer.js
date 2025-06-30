6089a7f577efb3ae9855c3ae118cffe5aaf972eb
Alex Kühn <aku@odoo.com>
2025-06-30 21:23:08 +0000

    [FIX] mail: attempt fix runbot 227618 (181952 reincarnated)
    Follow-up of https://github.com/odoo/odoo/pull/214626
    
    Runbot error 181952 is hardly reproduce-able. Don't have a exact idea
    what causes the problem.
    
    These are theories of the causes:
    1. inserText, contains, triggerHotkey of mail helpers are not robust
     enough (lack simulation, not robust against CPU load / prone to race
     conditions).
    2. focus in composer is lost non-deterministically
    
    Previous attempts were thinking the root cause are mail test helpers
    and web test helpers would fix issue, but that's probably not the
    exact cause of issue. A more likely candidate is composer loosing
    focus and therefore pressing of keys do not make proper transition
    from message with composer to message without composer.
    
    One likely cause of loosing focus is `processMessage` that reuses
    the old ref after asynchronous handling of message edition / post.
    This commit fixes by using the current ref of texarea.
    
    Test helpers can still be prone to problems. That's why this commit
    this converts the most important LOCs to web test helpers.
    The user interactions are made with `contains().press()`, where the
    `contains()` enforces focus on the targeted element, giving more
    guarantee the composer is focused for proper press of key.
    `press("Control+Enter")` were not working because this passed
    "Control+Enter" instead of "Enter" as `ev.key`. This was fixed by
    using more primitive syntax `["Control", "Enter"]` which is better
    parsed for proper `ev` object of KeyboardEvent.
    
    Runbot-error-227618
    
    X-original-commit: 25719d389e7bbb24f8046c510bf2cc23a1a0fc1a

--- addons/mail/static/src/core/common/composer.js
+++ addons/mail/static/src/core/common/composer.js
@@ -657,7 +657,6 @@ export class Composer extends Component {
     }
 
     async processMessage(cb) {
-        const el = this.ref.el;
         const attachments = this.props.composer.attachments;
         if (attachments.some(({ uploading }) => uploading)) {
             this.env.services.notification.add(_t("Please wait while the file is uploading."), {
@@ -678,7 +677,7 @@ export class Composer extends Component {
             }
             this.clear();
             this.state.active = true;
-            el.focus();
+            this.ref.el?.focus();
         }
     }
 
