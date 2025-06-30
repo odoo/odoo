35efaddbb8adc6522766e21111f65b5cd2b024fe
tsm-odoo <tsm@odoo.com>
2025-06-30 13:34:25 +0000

    [FIX] im_livechat: prevent start chat button flicker
    When the live chat is loaded on a page, it receives a value called
    `isAvailable` from the session. This name is misleading: it indicates
    availability if either an agent or a bot is present, but it doesn’t
    consider the live chat rules.
    
    These rules depend on the URL. Since `get_livechat_info` is called from
    the website template, we can’t determine which rule applies at that point.
    The goal is mainly to avoid calling `init_livechat` ("/mail/data") if we
    already know the live chat won’t be available. However, this check alone
    isn’t enough to decide whether to show the chat button.
    
    This commit renames `isAvailable` to `can_load_livechat` for clarity. If
    `can_load_livechat` is set, we can load the live chat and call `init_livechat`,
    which will then set the `livechat_available` value.
    
    task-4908197
    
    closes odoo/odoo#216748
    
    X-original-commit: 17b3e0c9594eec3a070a8f28c7e5d0578d2ec2c1
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/im_livechat/static/src/embed/common/store_service_patch.js
+++ addons/im_livechat/static/src/embed/common/store_service_patch.js
@@ -3,7 +3,6 @@ import { Store } from "@mail/core/common/store_service";
 import { fields } from "@mail/model/export";
 
 import { patch } from "@web/core/utils/patch";
-import { session } from "@web/session";
 
 export const GUEST_TOKEN_STORAGE_KEY = "im_livechat_guest_token";
 /** @type {import("models").Store} */
@@ -30,8 +29,7 @@ const StorePatch = {
             eager: true,
         });
         this.livechat_rule = fields.One("im_livechat.channel.rule");
-        /** @type {boolean} */
-        this.livechat_available = session.livechatData?.isAvailable;
+        this.livechat_available = false;
     },
 };
 patch(Store.prototype, StorePatch);
