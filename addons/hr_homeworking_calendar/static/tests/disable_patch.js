b46d8c1c8d07492f48d9d14e556790a3a6fc3b41
Michaël Mattiello <mcm@odoo.com>
2025-06-30 09:54:37 +0200

    [IMP] hr_homeworking_calendar: convert tests to HOOT
    task-4028356
    
    closes odoo/odoo#217048
    
    Signed-off-by: Julien Carion (juca) <juca@odoo.com>

--- addons/hr_homeworking_calendar/static/tests/disable_patch.js
+++
@@ -1,7 +0,0 @@
-import {
-        unpatchAttendeeCalendarCommonPopover,
-        unpatchAttendeeCalendarCommonPopoverClass
-    } from "@hr_homeworking_calendar/calendar/common/popover/calendar_common_popover";
-
-unpatchAttendeeCalendarCommonPopover();
-unpatchAttendeeCalendarCommonPopoverClass();
