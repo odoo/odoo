a1e9c8f157be0180c7dccaa8c76e750b0cd6c73c
alsh-odoo <alsh@odoo.com>
2025-06-30 08:47:36 +0000

    [FIX] sale_{project}: set project company to match the project's partner company
    Steps to reproduce:
    
    1) Install sale_project
    2) Create a service product that generates a project and a task.
    3) Add a project template by creating one from the product form view.
    4) Create an SO by creating a customer with a company
    5) Add the above-created service product and confirm the SO.
    
    Error:-
    A user exception will triggered
    "The project and the associated partner must be linked to the same company."
    
    Cause:
    When a project template is created from the product view,
    both the customer and the company_id default to empty.
    
    Later, when confirming a Sales Order for a customer that belongs to a company,
    the new project's company_id is taken from the project template, which is empty.
    
    https://github.com/odoo/odoo/blob/876e9d5e9ba87fa69188b2da098eec78f77040f5/addons/sale_project/models/sale_order.py#L140-L141
    
    However, the project’s partner_id (the customer) does have a company_id
    (since the customer value for the project will be set through SO's customer).
    
    This leads to a mismatch between the project’s company_id(which is empty)
    and its partner’s company_id. So an user exception will be triggered from the
    below lines
    https://github.com/odoo/odoo/blob/876e9d5e9ba87fa69188b2da098eec78f77040f5/addons/project/models/project_project.py#L257-L258
    
    Solution:
    
    Add an extra check for the existing of project's company will resolve this
    issue.
    Also, if there is account_id in project and the partner have a company,
    but the project dosen't, Add the partner's company to the account_id in
    the project.
    
    opw-4900741,4880495
    
    closes odoo/odoo#221626
    
    X-original-commit: 62bf58b6e8bd96f059205af38f79e50b875678f2
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/sale_project/tests/test_sale_project.py
+++ addons/sale_project/tests/test_sale_project.py
@@ -1502,3 +1502,23 @@ class TestSaleProject(TestSaleProjectCommon):
             {str(so.project_id.account_id.id): 100},
             "The analytic distribution of `sol_2` should be set to the reference AA of the SO.",
         )
+
+    def test_so_with_project_template(self):
+        """ Test that a SO with a product using a project template creates the project
+            and the task on SO confirmation, and set project's company to the product's template company.
+        """
+        product_with_project_template = self.env['product.product'].create({
+            'name': 'product with template',
+            'list_price': 1,
+            'type': 'service',
+            'service_tracking': 'project_only',
+            'project_template_id': self.project_template.id,
+        })
+        partner = self.env['res.partner'].create({'name': 'Test Partner', 'company_id': self.env.company.id})
+        sale_order = self.env['sale.order'].create({
+            'partner_id': partner.id,
+            'order_line': [Command.create({'product_id': product_with_project_template.id})],
+        })
+        sale_order.action_confirm()
+        self.assertEqual(sale_order.project_ids[0].company_id, self.project_template.company_id)
+        self.assertEqual(sale_order.project_ids[0].account_id.company_id, partner.company_id)
