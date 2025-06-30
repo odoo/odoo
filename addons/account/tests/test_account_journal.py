76b280ec4005feace60eaada8b22a18884acb3ea
Mahdi Alijani (malj) <malj@odoo.com>
2025-06-30 15:15:16 +0000

    [FIX] account: fix create invoice through email alias from a user without access to the company
    To reproduce the bug:
    1- Create a DB with two companies and accounting app
    2- Create a user and allow it to access company 2
    3- Send a email using the user email to alias from purchase
    journal alias of company 2
    4- The email will be rejected
    5- Send a email using a random email address to alias.
    6- The email will be accepted and an account.move is created.
    
    In a normal flow, when a user associated to the email not
    exists, the user_id is set to odoobot, otherwise, the user
    accosiated to the email.
    In the buggy flow the bug happens because _compute_company_id
    in account_move model, will set company_id to empty when user
    has no access to the company, as a result the account_move
    will fail.
    
    opw-4853027
    
    closes odoo/odoo#225263
    
    X-original-commit: 927348a4d6fe557404ca9fd9f8f17c370505a433
    Signed-off-by: Laurent Smet (las) <las@odoo.com>
    Signed-off-by: Mohammadmahdi Alijani (malj) <malj@odoo.com>

--- addons/account/tests/test_account_journal.py
+++ addons/account/tests/test_account_journal.py
@@ -7,7 +7,8 @@ from odoo import http
 from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 from odoo.addons.account.models.account_payment_method import AccountPaymentMethod
 from odoo.addons.mail.tests.common import MailCommon
-from odoo.tests import Form, tagged, HttpCase
+from odoo.tests import Form, tagged, HttpCase, new_test_user
+from odoo.addons.test_mail.data.test_mail_data import MAIL_EML_ATTACHMENT
 from odoo.exceptions import UserError, ValidationError
 from odoo import fields
 from odoo import Command
@@ -402,3 +403,22 @@ class TestAccountJournalAlias(AccountTestInvoicingCommon, MailCommon):
             {'balance': 15.0, 'account_id': self.company_data['default_account_tax_sale'].id},
             {'balance': -115.0, 'account_id': autobalance_account.id},
         ])
+
+    def test_send_email_to_alias_from_other_company(self):
+        user_company_2 = new_test_user(
+            self.env,
+            name='company 2 user',
+            login='company_2_user',
+            password='company_2_user',
+            email='company_2_user@test.com',
+            company_id=self.company_data_2['company'].id
+        )
+        self.format_and_process(
+            MAIL_EML_ATTACHMENT,
+            user_company_2.email,
+            self.company_data['default_journal_purchase'].alias_email,
+            subject='purchase test mail',
+            target_model='account.move',
+            msg_id='<test-account-move-alias-id>',
+        )
+        self.assertTrue(self.env['account.move'].search([('invoice_source_email', '=', 'company_2_user@test.com')]))
