0941039f0ccc6788e2dff6c606b0b5f9c9eb7129
Michaël Mattiello <mcm@odoo.com>
2025-06-30 09:54:37 +0200

    [IMP] hr_homeworking_calendar: convert tests to HOOT
    task-4028356

--- addons/hr_homeworking_calendar/static/tests/hr_org_chart_tests.js
+++
@@ -1,7 +0,0 @@
-import { registry } from "@web/core/registry";
-
-const mockRegistry = registry.category("mock_server");
-
-mockRegistry.add("get_worklocation", function(route, args) {
-    return Promise.resolve();
-});
