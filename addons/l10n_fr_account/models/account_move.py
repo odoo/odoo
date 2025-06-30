7a0668443b66dd6a6e057f7d67dd4dd3281a3bc5
Hesham Saleh <hsal@odoo.com>
2025-06-30 09:05:25 +0000

    [IMP] l10n_fr_account: delivery date only set by users
    This commit prevents the delivery date from being set to invoice date by default in French localization.
    We need the delivery date to be only set by the users.
    
    task-4900043
    
    closes odoo/odoo#217548
    
    X-original-commit: 18f7191ee3b6a61610d10f64f37f17a8f373ae30
    Signed-off-by: Maximilien La Barre (malb) <malb@odoo.com>
    Signed-off-by: Hesham Saleh (hsal) <hsal@odoo.com>

--- addons/l10n_fr_account/models/account_move.py
+++ addons/l10n_fr_account/models/account_move.py
@@ -26,10 +26,3 @@ class AccountMove(models.Model):
         super()._compute_show_delivery_date()
         for move in self.filtered(lambda m: m.country_code == 'FR'):
             move.show_delivery_date = move.is_sale_document()
-
-    def _post(self, soft=True):
-        # EXTEND 'account'
-        res = super()._post(soft=soft)
-        for move in self.filtered(lambda m: m.show_delivery_date and not m.delivery_date):
-            move.delivery_date = move.invoice_date
-        return res
