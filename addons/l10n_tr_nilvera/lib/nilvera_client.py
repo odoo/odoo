odoo/odoo

commit 3b5cb63aaf5b8e1c3ee0842d30bbc06fd43c8ddb

[IMP] l10n_tr_nilvera: improve exception handling for connectivity issues
Previously, if a request in the Nilvera Client failed due to
connectivity issues, a traceback was thrown. This behavior caused
upgrade CI failures because the internet is blocked and
check_nilvera_customer would be triggered for newly created partners.
This commit enhances the exception handling for all requests and fixes
the upgrade CI issue by silently exiting the function when a
connectivity problem is detected.

no task ID.

closes odoo/odoo#197797

Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>

addons/l10n_tr_nilvera/lib/nilvera_client.py
@@ -38,13 +38,18 @@ class NilveraClient:
         start = datetime.utcnow()
         url = self.base_url + endpoint
 
-        response = self.__session.request(
-            method, url,
-            timeout=self.timeout_limit,
-            params=params,
-            json=json,
-            files=files,
-        )
+        try:
+            response = self.__session.request(
+                method, url,
+                timeout=self.timeout_limit,
+                params=params,
+                json=json,
+                files=files,
+            )
+        except requests.exceptions.RequestException as e:
+            _logger.error("Network error during request: %s", e)
+            raise UserError("Network connectivity issue. Please check your internet connection and try again.")
+
         end = datetime.utcnow()
         self._log_request(method, start, end, url, params, json, response)
 

addons/l10n_tr_nilvera/models/res_partner.py
@@ -2,6 +2,7 @@ import logging
 import urllib.parse
 
 from odoo import api, fields, models
+from odoo.exceptions import UserError
 from odoo.addons.l10n_tr_nilvera.lib.nilvera_client import _get_nilvera_client
 
 
@@ -60,7 +61,11 @@ class ResPartner(models.Model):
     def _compute_nilvera_customer_status_and_alias_id(self):
         for partner in self:
             if partner.vat and partner.ubl_cii_format == 'ubl_tr':
-                partner.check_nilvera_customer()
+                try:
+                    partner.check_nilvera_customer()
+                except UserError:
+                    # In case of an internet connection issue, exit silently.
+                    continue
             else:
                 # Reset the alias if no VAT or UBL format changed.
                 partner.l10n_tr_nilvera_customer_status = 'not_checked'

commit 9bb00c62eba71dd4f0f7288eba63c85808ad2ebe

[IMP] l10n_tr_nilvera: log info to avoid CI failing
In 3b5cb63 we silenced Nilvera connection errors to prevent upgrade CI
failures due to blocked internet access. However, using `logger.error`
still caused CI to fail when triggered. This commit changes the log
level to info to ensure the error is recorded without impacting the CI.

runbot-227039

addons/l10n_tr_nilvera/lib/nilvera_client.py
@@ -47,7 +47,7 @@ class NilveraClient:
                 files=files,
             )
         except requests.exceptions.RequestException as e:
-            _logger.error("Network error during request: %s", e)
+            _logger.info("Network error during request: %s", e)
             raise UserError("Network connectivity issue. Please check your internet connection and try again.")
 
         end = datetime.utcnow()