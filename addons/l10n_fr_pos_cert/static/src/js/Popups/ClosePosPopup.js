5b1ce27e22ccfe4b4aa3fbdf3d69de1d2adfe13a
parp-odoo <parp@odoo.com>
2025-06-30 12:12:56 +0530

    [IMP] l10n_fr_pos_cert: allow session to span multiple days
    Before this commit:
    - Point of Sale sessions were restricted to a single calendar day, preventing
      accumulation of data across multiple days in one session.
    
    After this commit:
    - Removed the restriction that blocked sessions from overlapping several days.
    
    Task: 4735752

--- addons/l10n_fr_pos_cert/static/src/js/Popups/ClosePosPopup.js
+++ addons/l10n_fr_pos_cert/static/src/js/Popups/ClosePosPopup.js
@@ -3,18 +3,9 @@
 import { ClosePosPopup } from "@point_of_sale/app/navbar/closing_popup/closing_popup";
 import { patch } from "@web/core/utils/patch";
 
+// TODO: remove in master (saas-18.5)
 patch(ClosePosPopup.prototype, {
     sessionIsOutdated() {
-        let isOutdated = false;
-        if (this.pos.is_french_country() && this.pos.pos_session.start_at) {
-            const now = Date.now();
-            const limitDate = new Date(this.pos.pos_session.start_at);
-            limitDate.setDate(limitDate.getDate() + 1);
-            isOutdated = limitDate < now;
-        }
-        return isOutdated;
-    },
-    canCancel() {
-        return super.canCancel(...arguments) && !this.sessionIsOutdated();
+        return false;
     },
 });
