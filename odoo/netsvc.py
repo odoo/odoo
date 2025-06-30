37ffb840454fe9c16f75fa75d96c3807ea94854b
Julien Castiaux <juc@odoo.com>
2025-06-30 16:55:01 +0200

    [IMP] core: hide warnings callstack above http.py
    When we have a error inside a controller, the traceback starts at
    http.py `__call__`. But when we have a warnings, the callstack goes
    beyond http.py `__call__` and up to `threading.py` (where the thread,
    in this case, started).
    
    This changes makes the callstack logged for warnings stop at http.py
    `__call__` for consistency.
    
    Please note that exceptions/warnings outside of controllers (e.g. during
    the server bootstrap) continue to be logged the same way: up to odoo-bin
    
    closes odoo/odoo#216644
    
    Signed-off-by: Julien Castiaux (juc) <juc@odoo.com>

--- odoo/netsvc.py
+++ odoo/netsvc.py
@@ -345,6 +345,9 @@ def showwarning_with_traceback(message, category, filename, lineno, file=None, l
     # find the stack frame matching (filename, lineno)
     filtered = []
     for frame in traceback.extract_stack():
+        if frame.name == '__call__' and frame.filename.endswith('/odoo/http.py'):
+            # we don't care about the frames above our wsgi entrypoint
+            filtered.clear()
         if 'importlib' not in frame.filename:
             filtered.append(frame)
         if frame.filename == filename and frame.lineno == lineno:
