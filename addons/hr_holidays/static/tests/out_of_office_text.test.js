b08f440cec0c762cbd53e6a08c94a515953ead4f
mear-odoo <mear@odoo.com>
2025-07-01 17:03:17 +0530

    [IMP] mail, hr_holidays:  add time off indication in discuss sidebar and command palette
    Specifications:
    • Display time off indication in the Discuss sidebar and CTRL+K (command
      palette) user search results.
    
    Purpose:
    • Enhances visibility of user availability during quick searches and
      conversations, helping users avoid messaging unavailable members.
    
    task-4904156
    
    closes odoo/odoo#217064
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/hr_holidays/static/tests/out_of_office_text.test.js
+++ addons/hr_holidays/static/tests/out_of_office_text.test.js
@@ -12,6 +12,7 @@ import {
 
 import { defineHrHolidaysModels } from "@hr_holidays/../tests/hr_holidays_test_helpers";
 import { Command, serverState } from "@web/../tests/web_test_helpers";
+import { press } from "@odoo/hoot-dom";
 import { mockDate } from "@odoo/hoot-mock";
 
 describe.current.tags("desktop");
@@ -88,4 +89,42 @@ test("Show year when 'back on' is on different year than now", async () => {
     await openFormView("res.fake", fakeId);
     await click(".o-mail-Message-avatar");
     await contains(".o_avatar_card span", { text: "Back on Jan 4, 2025" });
+});
+
+test("Discuss Sidebar shows out of office indication", async () => {
+    mockDate("2025-04-08 12:00:00");
+    const pyEnv = await startServer();
+    pyEnv["hr.employee"].create({
+        user_id: serverState.userId,
+        leave_date_to: DateTime.now().plus({ days: 3 }).toISODate(),
+    });
+    const channelId = pyEnv["discuss.channel"].create({
+        channel_member_ids: [Command.create({ partner_id: serverState.partnerId })],
+        channel_type: "chat",
+    });
+    await start();
+    await openDiscuss(channelId);
+    await contains(".o-mail-DiscussSidebarChannel-itemName .text-warning", {
+        text: "Back on Apr 11",
+    });
+});
+
+test("CTRL+K command shows out of office indication", async () => {
+    mockDate("2025-04-08 12:00:00");
+    const pyEnv = await startServer();
+    pyEnv["hr.employee"].create({
+        user_id: serverState.userId,
+        leave_date_to: DateTime.now().plus({ days: 3 }).toISODate(),
+    });
+    const channelId = pyEnv["discuss.channel"].create({
+        channel_member_ids: [Command.create({ partner_id: serverState.partnerId })],
+        channel_type: "chat",
+    });
+    await start();
+    await openDiscuss(channelId);
+    await contains(".o-mail-Discuss[data-active]");
+    await press(["ctrl", "k"]);
+    await contains(".o-mail-DiscussCommand .text-warning", {
+        text: "Back on Apr 11",
+    });
 });
