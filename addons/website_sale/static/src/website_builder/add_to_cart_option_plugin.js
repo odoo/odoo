odoo/odoo

commit 0550231117b016e0b1e7e336f1c065430fa5a1c9

[IMP] html_builder, website, *: migrate actions to class-based system
*: website_event, website_mail_group, website_mass_mailing,
website_payment, website_sale

Previously, builder actions were defined as plain objects with
apply/isApplied/getValue properties.

This commit introduces a class-based system where each builder action
extends the BuilderAction base class.

Improvements include:
- Support instantiated objects, and legacy object-style actions.
- Greater consistency, encapsulation, and extensibility for action logic.

This refactor simplifies action management and prepares the system for
further enhancements like shared base logic.

closes odoo/odoo#212138

Related: odoo/enterprise#87429
Signed-off-by: Géry Debongnie <ged@odoo.com>

addons/website_sale/static/src/website_builder/add_to_cart_option.xml
@@ -5,7 +5,7 @@
     <BuilderRow label.translate="Product">
         <BuilderMany2One
             id="'product_template_picker_opt'"
-            action="'productToCartAction'"
+            action="'productToCart'"
             model="'product.template'"
             fields="['type', 'product_variant_ids']"
             domain="[['is_published', '=', true], ['sale_ok', '=', true]]"
@@ -13,7 +13,7 @@
     </BuilderRow>
     <BuilderRow label.translate="Variant" level="1" t-if="(this.getItemValueJSON('product_template_picker_opt')?.product_variant_ids.length || 0) > 1">
         <BuilderMany2One
-            action="'variantToCartAction'"
+            action="'variantToCart'"
             model="'product.product'"
             fields=""
             domain="[['product_tmpl_id', '=', this.getItemValueJSON('product_template_picker_opt').id]]"
@@ -21,7 +21,7 @@
         />
     </BuilderRow>
     <BuilderRow label.translate="Action" t-if="this.domState.shouldShowActionChoice">
-        <BuilderSelect action="'addToCartAction'">
+        <BuilderSelect action="'addToCart'">
             <BuilderSelectItem actionParam="addToCartValues.addToCart"><t t-out="addToCartValues.addToCart.label"/></BuilderSelectItem>
             <BuilderSelectItem actionParam="addToCartValues.buyNow"><t t-out="addToCartValues.buyNow.label"/></BuilderSelectItem>
         </BuilderSelect>

addons/website_sale/static/src/website_builder/add_to_cart_option_plugin.js
@@ -1,6 +1,7 @@
 import { Plugin } from "@html_editor/plugin";
 import { registry } from "@web/core/registry";
 import { AddToCartOption, addToCartValues } from "./add_to_card_option";
+import { BuilderAction } from "@html_builder/core/builder_action";
 
 class AddToCartOptionPlugin extends Plugin {
     static id = "addToCartOption";
@@ -14,118 +15,9 @@ class AddToCartOptionPlugin extends Plugin {
         ],
         so_content_addition_selector: [".s_add_to_cart"],
         builder_actions: {
-            productToCartAction: {
-                apply: ({ editingElement, value }) => {
-                    const classAction = this.dependencies.builderActions.getAction("classAction");
-
-                    const { id, type, product_variant_ids } = JSON.parse(value);
-
-                    editingElement.dataset.productTemplate = id;
-                    editingElement.dataset.productType = type;
-                    editingElement.dataset.variants = product_variant_ids.join(",");
-                    delete editingElement.dataset.productVariant;
-
-                    const buttonEl = editingElement.querySelector(".s_add_to_cart_btn");
-                    buttonEl.dataset.productTemplateId = id;
-                    buttonEl.dataset.productType = type;
-                    const oneVariant = product_variant_ids.length === 1;
-                    if (oneVariant) {
-                        buttonEl.dataset.productVariantId = product_variant_ids[0];
-                    } else {
-                        delete buttonEl.dataset.productVariantId;
-                    }
-                    classAction.clean({
-                        editingElement: buttonEl,
-                        params: { mainParam: "disabled" },
-                    });
-                    if (!oneVariant) {
-                        this.resetDefaultAction(editingElement);
-                    }
-                },
-                clean: ({ editingElement }) => {
-                    const classAction = this.dependencies.builderActions.getAction("classAction");
-                    delete editingElement.dataset.productTemplate;
-                    delete editingElement.dataset.productType;
-                    delete editingElement.dataset.variants;
-                    delete editingElement.dataset.productVariant;
-                    const buttonEl = editingElement.querySelector(".s_add_to_cart_btn");
-                    delete buttonEl.dataset.productTemplateId;
-                    delete buttonEl.dataset.productType;
-                    delete buttonEl.dataset.productVariantId;
-                    classAction.apply({
-                        editingElement: buttonEl,
-                        params: { mainParam: "disabled" },
-                    });
-                    this.resetDefaultAction(editingElement);
-                },
-                getValue: ({ editingElement }) => {
-                    const value = {};
-                    const id = editingElement.dataset.productTemplate;
-                    if (!id) {
-                        return;
-                    }
-                    value.id = parseInt(id);
-                    const type = editingElement.dataset.productType;
-                    if (type !== undefined) {
-                        value.type = type;
-                    }
-                    const product_variant_ids = editingElement.dataset.variants
-                        ?.split(",")
-                        .map((el) => parseInt(el));
-                    if (product_variant_ids !== undefined) {
-                        value.product_variant_ids = product_variant_ids;
-                    }
-                    return JSON.stringify(value);
-                },
-            },
-            variantToCartAction: {
-                apply: ({ editingElement, value }) => {
-                    const { id } = JSON.parse(value);
-                    editingElement.dataset.productVariant = id;
-                    const buttonEl = editingElement.querySelector(".s_add_to_cart_btn");
-                    buttonEl.dataset.productVariantId = id;
-                },
-                clean: ({ editingElement }) => {
-                    delete editingElement.dataset.productVariant;
-                    const buttonEl = editingElement.querySelector(".s_add_to_cart_btn");
-                    delete buttonEl.dataset.productVariantId;
-                    this.resetDefaultAction(editingElement);
-                },
-                getValue: ({ editingElement }) => {
-                    const id = editingElement.dataset.productVariant;
-                    if (id) {
-                        return JSON.stringify({ id: parseInt(id) });
-                    }
-                },
-            },
-            addToCartAction: {
-                apply: ({ editingElement, params: { action, icon, label } }) => {
-                    const classAction = this.dependencies.builderActions.getAction("classAction");
-                    editingElement.dataset.action = action;
-                    const buttonEl = editingElement.querySelector(".s_add_to_cart_btn");
-                    buttonEl.dataset.action = action;
-                    const iconEl = buttonEl.querySelector("i");
-                    classAction.apply({
-                        editingElement: iconEl,
-                        params: { mainParam: icon },
-                    });
-                    buttonEl.lastChild.textContent = label;
-                },
-                clean: ({ editingElement, params: { icon } }) => {
-                    const classAction = this.dependencies.builderActions.getAction("classAction");
-
-                    delete editingElement.dataset.action;
-                    const buttonEl = editingElement.querySelector(".s_add_to_cart_btn");
-                    delete buttonEl.dataset.action;
-                    const iconEl = buttonEl.querySelector("i");
-                    classAction.clean({
-                        editingElement: iconEl,
-                        params: { mainParam: icon },
-                    });
-                },
-                isApplied: ({ editingElement, params: { action } }) =>
-                    editingElement.dataset.action === action,
-            },
+            ProductToCartAction,
+            VariantToCartAction,
+            AddToCartAction,
         },
     };
 
@@ -135,6 +27,126 @@ class AddToCartOptionPlugin extends Plugin {
             addToCartAction.clean({ editingElement, params: addToCartValues.buyNow });
             addToCartAction.apply({ editingElement, params: addToCartValues.addToCart });
         }
+    }
+}
+
+class ProductToCartAction extends BuilderAction {
+    static id = "productToCart";
+    static dependencies = ["builderActions", "addToCartOption"];
+    apply({ editingElement, value }) {
+        const classAction = this.dependencies.builderActions.getAction("classAction");
+
+        const { id, type, product_variant_ids } = JSON.parse(value);
+
+        editingElement.dataset.productTemplate = id;
+        editingElement.dataset.productType = type;
+        editingElement.dataset.variants = product_variant_ids.join(",");
+        delete editingElement.dataset.productVariant;
+
+        const buttonEl = editingElement.querySelector(".s_add_to_cart_btn");
+        buttonEl.dataset.productTemplateId = id;
+        buttonEl.dataset.productType = type;
+        const oneVariant = product_variant_ids.length === 1;
+        if (oneVariant) {
+            buttonEl.dataset.productVariantId = product_variant_ids[0];
+        } else {
+            delete buttonEl.dataset.productVariantId;
+        }
+        classAction.clean({
+            editingElement: buttonEl,
+            params: { mainParam: "disabled" },
+        });
+        if (!oneVariant) {
+            this.dependencies.addToCartOption.resetDefaultAction(editingElement);
+        }
+    }
+    clean({ editingElement }) {
+        const classAction = this.dependencies.builderActions.getAction("classAction");
+        delete editingElement.dataset.productTemplate;
+        delete editingElement.dataset.productType;
+        delete editingElement.dataset.variants;
+        delete editingElement.dataset.productVariant;
+        const buttonEl = editingElement.querySelector(".s_add_to_cart_btn");
+        delete buttonEl.dataset.productTemplateId;
+        delete buttonEl.dataset.productType;
+        delete buttonEl.dataset.productVariantId;
+        classAction.apply({
+            editingElement: buttonEl,
+            params: { mainParam: "disabled" },
+        });
+        this.dependencies.addToCartOption.resetDefaultAction(editingElement);
+    }
+    getValue({ editingElement }) {
+        const value = {};
+        const id = editingElement.dataset.productTemplate;
+        if (!id) {
+            return;
+        }
+        value.id = parseInt(id);
+        const type = editingElement.dataset.productType;
+        if (type !== undefined) {
+            value.type = type;
+        }
+        const product_variant_ids = editingElement.dataset.variants
+            ?.split(",")
+            .map((el) => parseInt(el));
+        if (product_variant_ids !== undefined) {
+            value.product_variant_ids = product_variant_ids;
+        }
+        return JSON.stringify(value);
+    }
+}
+class VariantToCartAction extends BuilderAction {
+    static id = "variantToCart";
+    static dependencies = ["addToCartOption"];
+    apply({ editingElement, value }) {
+        const { id } = JSON.parse(value);
+        editingElement.dataset.productVariant = id;
+        const buttonEl = editingElement.querySelector(".s_add_to_cart_btn");
+        buttonEl.dataset.productVariantId = id;
+    }
+    clean({ editingElement }) {
+        delete editingElement.dataset.productVariant;
+        const buttonEl = editingElement.querySelector(".s_add_to_cart_btn");
+        delete buttonEl.dataset.productVariantId;
+        this.dependencies.addToCartOption.resetDefaultAction(editingElement);
+    }
+    getValue({ editingElement }) {
+        const id = editingElement.dataset.productVariant;
+        if (id) {
+            return JSON.stringify({ id: parseInt(id) });
+        }
+    }
+}
+class AddToCartAction extends BuilderAction {
+    static id = "addToCart";
+    static dependencies = ["builderActions"];
+    apply({ editingElement, params: { action, icon, label } }) {
+        const classAction = this.dependencies.builderActions.getAction("classAction");
+        editingElement.dataset.action = action;
+        const buttonEl = editingElement.querySelector(".s_add_to_cart_btn");
+        buttonEl.dataset.action = action;
+        const iconEl = buttonEl.querySelector("i");
+        classAction.apply({
+            editingElement: iconEl,
+            params: { mainParam: icon },
+        });
+        buttonEl.lastChild.textContent = label;
+    }
+    clean({ editingElement, params: { icon } }) {
+        const classAction = this.dependencies.builderActions.getAction("classAction");
+
+        delete editingElement.dataset.action;
+        const buttonEl = editingElement.querySelector(".s_add_to_cart_btn");
+        delete buttonEl.dataset.action;
+        const iconEl = buttonEl.querySelector("i");
+        classAction.clean({
+            editingElement: iconEl,
+            params: { mainParam: icon },
+        });
+    }
+    isApplied({ editingElement, params: { action } }) {
+        return editingElement.dataset.action === action;
     }
 }
 

commit 9638bb38665253e419946f2b6454d7a20d765cd9

[FIX] website_sale: fix "Add To Cart" snippet with product with variant
Steps to reproduce:
- Drop the "Add to Cart" button
- Select a "Product" that has variants (for example "Customizable Desk")
- Traceback

This bug was introduced by 0550231117b016e0b1e7e336f1c065430fa5a1c9
which moved the implementation of some function but did not declared the
correct dependencies.

That commit also renamed the id "addToCartAction" to "addToCart" to use
that name for the class and be coherent with the other actions whose
class are their id suffixed by "Action". But that rename is a poor
choice in this case, because "Action" in the id refers to the action
taken when a visitor clicks "Add to Cart", which is unrelated to the
concept of `BuilderAction`.

task-4367641

closes odoo/odoo#216427

X-original-commit: ad94e3dea5470ba09fbead079f19133cfd024512
Signed-off-by: Francois Georis (fge) <fge@odoo.com>

addons/website_sale/static/src/website_builder/add_to_cart_option.xml
@@ -21,7 +21,7 @@
         />
     </BuilderRow>
     <BuilderRow label.translate="Action" t-if="this.domState.shouldShowActionChoice">
-        <BuilderSelect action="'addToCart'">
+        <BuilderSelect action="'addToCartAction'">
             <BuilderSelectItem actionParam="addToCartValues.addToCart"><t t-out="addToCartValues.addToCart.label"/></BuilderSelectItem>
             <BuilderSelectItem actionParam="addToCartValues.buyNow"><t t-out="addToCartValues.buyNow.label"/></BuilderSelectItem>
         </BuilderSelect>

addons/website_sale/static/src/website_builder/add_to_cart_option_plugin.js
@@ -17,22 +17,14 @@ class AddToCartOptionPlugin extends Plugin {
         builder_actions: {
             ProductToCartAction,
             VariantToCartAction,
-            AddToCartAction,
+            AddToCartActionAction,
         },
     };
-
-    resetDefaultAction(editingElement) {
-        const addToCartAction = this.dependencies.builderActions.getAction("addToCartAction");
-        if (addToCartAction.isApplied({ editingElement, params: addToCartValues.buyNow })) {
-            addToCartAction.clean({ editingElement, params: addToCartValues.buyNow });
-            addToCartAction.apply({ editingElement, params: addToCartValues.addToCart });
-        }
-    }
 }
 
 export class ProductToCartAction extends BuilderAction {
     static id = "productToCart";
-    static dependencies = ["builderActions", "addToCartOption"];
+    static dependencies = ["builderActions"];
     apply({ editingElement, value }) {
         const classAction = this.dependencies.builderActions.getAction("classAction");
 
@@ -57,7 +49,9 @@ export class ProductToCartAction extends BuilderAction {
             params: { mainParam: "disabled" },
         });
         if (!oneVariant) {
-            this.dependencies.addToCartOption.resetDefaultAction(editingElement);
+            this.dependencies.builderActions
+                .getAction("addToCartAction")
+                .resetDefaultAction(editingElement);
         }
     }
     clean({ editingElement }) {
@@ -74,7 +68,9 @@ export class ProductToCartAction extends BuilderAction {
             editingElement: buttonEl,
             params: { mainParam: "disabled" },
         });
-        this.dependencies.addToCartOption.resetDefaultAction(editingElement);
+        this.dependencies.builderActions
+            .getAction("addToCartAction")
+            .resetDefaultAction(editingElement);
     }
     getValue({ editingElement }) {
         const value = {};
@@ -98,7 +94,7 @@ export class ProductToCartAction extends BuilderAction {
 }
 export class VariantToCartAction extends BuilderAction {
     static id = "variantToCart";
-    static dependencies = ["addToCartOption"];
+    static dependencies = ["builderActions"];
     apply({ editingElement, value }) {
         const { id } = JSON.parse(value);
         editingElement.dataset.productVariant = id;
@@ -109,7 +105,9 @@ export class VariantToCartAction extends BuilderAction {
         delete editingElement.dataset.productVariant;
         const buttonEl = editingElement.querySelector(".s_add_to_cart_btn");
         delete buttonEl.dataset.productVariantId;
-        this.dependencies.addToCartOption.resetDefaultAction(editingElement);
+        this.dependencies.builderActions
+            .getAction("addToCartAction")
+            .resetDefaultAction(editingElement);
     }
     getValue({ editingElement }) {
         const id = editingElement.dataset.productVariant;
@@ -118,8 +116,8 @@ export class VariantToCartAction extends BuilderAction {
         }
     }
 }
-export class AddToCartAction extends BuilderAction {
-    static id = "addToCart";
+export class AddToCartActionAction extends BuilderAction {
+    static id = "addToCartAction";
     static dependencies = ["builderActions"];
     apply({ editingElement, params: { action, icon, label } }) {
         const classAction = this.dependencies.builderActions.getAction("classAction");
@@ -147,6 +145,13 @@ export class AddToCartAction extends BuilderAction {
     }
     isApplied({ editingElement, params: { action } }) {
         return editingElement.dataset.action === action;
+    }
+
+    resetDefaultAction(editingElement) {
+        if (this.isApplied({ editingElement, params: addToCartValues.buyNow })) {
+            this.clean({ editingElement, params: addToCartValues.buyNow });
+            this.apply({ editingElement, params: addToCartValues.addToCart });
+        }
     }
 }
 