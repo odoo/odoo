6319480737fbafa296b95b86a4a1759507d2baf5
dhba <dhba@odoo.com>
2025-07-01 15:02:59 +0530

    [IMP] html_editor: drop should use internal odoo-editor dataTransfer
    Before this commit:
    
    When dragging and dropping elements with attributes and classes, any
    non-whitelisted attributes and classes were removed during the cleanForPaste
    process. This caused structural issues and loss of formatting after the drop.
    
    After this commit:
    
    An application/vnd.odoo.odoo-editor dataTransfer type is now set during
    dragstart for editor elements. As a result, we no longer need to clean the
    dataTransfer content during drop, preserving the original structure and
    preventing the loss of attributes and classes.
    
    task:4914451

--- addons/html_editor/static/src/core/clipboard_plugin.js
+++ addons/html_editor/static/src/core/clipboard_plugin.js
@@ -144,19 +144,30 @@ export class ClipboardPlugin extends Plugin {
      */
     onCopy(ev) {
         ev.preventDefault();
+        const clipboardData = this.getSelectionTransferData();
+        if (!clipboardData) {
+            return;
+        }
+        const [htmlContent, textContent] = clipboardData;
+        ev.clipboardData.setData("text/plain", textContent);
+        ev.clipboardData.setData("text/html", htmlContent);
+        ev.clipboardData.setData("application/vnd.odoo.odoo-editor", htmlContent);
+    }
+
+    /**
+     * Prepare HTML and plain text from the current selection.
+     */
+    getSelectionTransferData() {
         const selection = this.dependencies.selection.getEditableSelection();
         let clonedContents = selection.cloneContents();
         if (!clonedContents.hasChildNodes()) {
-            return;
+            return null;
         }
-
         // Prepare text content for clipboard.
         let textContent = selection.textContent();
         for (const processor of this.getResource("clipboard_text_processors")) {
             textContent = processor(textContent);
         }
-        ev.clipboardData.setData("text/plain", textContent);
-
         // Prepare html content for clipboard.
         for (const processor of this.getResource("clipboard_content_processors")) {
             clonedContents = processor(clonedContents, selection) || clonedContents;
@@ -166,8 +177,7 @@ export class ClipboardPlugin extends Plugin {
         dataHtmlElement.append(clonedContents);
         prependOriginToImages(dataHtmlElement, window.location.origin);
         const htmlContent = dataHtmlElement.innerHTML;
-        ev.clipboardData.setData("text/html", htmlContent);
-        ev.clipboardData.setData("application/vnd.odoo.odoo-editor", htmlContent);
+        return [htmlContent, textContent];
     }
 
     /**
@@ -564,13 +574,14 @@ export class ClipboardPlugin extends Plugin {
      * @param {DragEvent} ev
      */
     onDragStart(ev) {
-        if (ev.target.nodeName === "IMG") {
-            this.dragImage = ev.target instanceof HTMLElement && ev.target;
-            ev.dataTransfer.setData(
-                "application/vnd.odoo.odoo-editor-node",
-                this.dragImage.outerHTML
-            );
+        const dataTransferData = this.getSelectionTransferData();
+        if (!dataTransferData) {
+            return;
         }
+        const [htmlContent, textContent] = dataTransferData;
+        ev.dataTransfer.setData("text/plain", textContent);
+        ev.dataTransfer.setData("text/html", htmlContent);
+        ev.dataTransfer.setData("application/vnd.odoo.odoo-editor", htmlContent);
     }
     /**
      * Handle safe dropping of html into the editor.
@@ -598,16 +609,10 @@ export class ClipboardPlugin extends Plugin {
         }
 
         const dataTransfer = (ev.originalEvent || ev).dataTransfer;
-        const imageNodeHTML = ev.dataTransfer.getData("application/vnd.odoo.odoo-editor-node");
-        const image =
-            imageNodeHTML &&
-            this.dragImage &&
-            imageNodeHTML === this.dragImage.outerHTML &&
-            this.dragImage;
-
-        const fileTransferItems = getImageFiles(dataTransfer);
+        const odooEditorHtml = ev.dataTransfer.getData("application/vnd.odoo.odoo-editor");
+        const fileTransferItems = !odooEditorHtml && getImageFiles(dataTransfer);
         const htmlTransferItem = [...dataTransfer.items].find((item) => item.type === "text/html");
-        if (image || fileTransferItems.length || htmlTransferItem) {
+        if (fileTransferItems.length || htmlTransferItem || odooEditorHtml) {
             if (this.document.caretPositionFromPoint) {
                 const range = this.document.caretPositionFromPoint(ev.clientX, ev.clientY);
                 this.dependencies.delete.deleteSelection();
@@ -624,11 +629,12 @@ export class ClipboardPlugin extends Plugin {
                 });
             }
         }
-        if (image) {
-            const fragment = this.document.createDocumentFragment();
-            fragment.append(image);
-            this.dependencies.dom.insert(fragment);
-            this.dependencies.history.addStep();
+        if (odooEditorHtml) {
+            const fragment = parseHTML(this.document, odooEditorHtml);
+            if (fragment.hasChildNodes()) {
+                this.dependencies.dom.insert(fragment);
+                this.dependencies.history.addStep();
+            }
         } else if (fileTransferItems.length) {
             const html = await this.addImagesFiles(fileTransferItems);
             this.dependencies.dom.insert(html);
@@ -708,7 +714,6 @@ function getImageUrl(file) {
         };
     });
 }
-
 
 /**
  * Add origin to relative img src.
