edbd394ca4edfbf2b60fabe6ec9333664d01fab6
Thibault Delavallée <tde@odoo.com>
2025-07-01 08:23:08 +0200

    [FIX] test_mail_full: fix mailing test for nightly
    Not sure why we did that strange construct in subtest but it fails in
    nightly.
    
    Added order on model to try to reduce non deterministic behavior.
    
    runbot-162367

--- addons/test_mail_full/tests/test_mass_mailing.py
+++ addons/test_mail_full/tests/test_mass_mailing.py
@@ -58,89 +58,89 @@ class TestMassMailing(TestMailFullCommon):
             mailing.action_send_mail()
 
         for recipient in recipients_all:
-          with self.subTest(recipient=next(key for key, val in locals().items() if key.startswith('recipient') if val == recipient)):
-            recipient_info = {
-                'email': recipient.email_normalized,
-                'content': f'Hello {recipient.name}',
-                'mail_values': {
-                    'subject': f'Subject {recipient.name}',
-                },
-            }
-            # opt-out: cancel (cancel mail)
-            if recipient in recipients[1] | recipients[2]:
-                recipient_info['trace_status'] = "cancel"
-                recipient_info['failure_type'] = "mail_optout"
-            # blacklisted: cancel (cancel mail)
-            elif recipient in recipients[3] | recipients[4]:
-                recipient_info['trace_status'] = "cancel"
-                recipient_info['failure_type'] = "mail_bl"
-            # duplicates: cancel (cancel mail)
-            elif recipient in (recipient_dup_1, recipient_dup_2, recipient_dup_4):
-                recipient_info['trace_status'] = "cancel"
-                recipient_info['failure_type'] = "mail_dup"
-            # void: error (failed mail)
-            elif recipient == recipient_void_1:
-                recipient_info['trace_status'] = 'cancel'
-                recipient_info['failure_type'] = "mail_email_missing"
-            # falsy: error (failed mail)
-            elif recipient == recipient_falsy_1:
-                recipient_info['trace_status'] = "cancel"
-                recipient_info['failure_type'] = "mail_email_invalid"
-                recipient_info['email'] = recipient.email_from  # normalized is False but email should be falsymail
-            else:
-                # multi email -> outgoing email contains all emails
-                if recipient == recipient_dup_3:
-                    email = self._find_sent_email(self.user_marketing.email_formatted, ['test.record.09@test.example.com', 'test.multi@test.example.com'])
+            with self.subTest(recipient_from=recipient.email_from):
+                recipient_info = {
+                    'email': recipient.email_normalized,
+                    'content': f'Hello {recipient.name}',
+                    'mail_values': {
+                        'subject': f'Subject {recipient.name}',
+                    },
+                }
+                # opt-out: cancel (cancel mail)
+                if recipient in recipients[1] | recipients[2]:
+                    recipient_info['trace_status'] = "cancel"
+                    recipient_info['failure_type'] = "mail_optout"
+                # blacklisted: cancel (cancel mail)
+                elif recipient in recipients[3] | recipients[4]:
+                    recipient_info['trace_status'] = "cancel"
+                    recipient_info['failure_type'] = "mail_bl"
+                # duplicates: cancel (cancel mail)
+                elif recipient in (recipient_dup_1, recipient_dup_2, recipient_dup_4):
+                    recipient_info['trace_status'] = "cancel"
+                    recipient_info['failure_type'] = "mail_dup"
+                # void: error (failed mail)
+                elif recipient == recipient_void_1:
+                    recipient_info['trace_status'] = 'cancel'
+                    recipient_info['failure_type'] = "mail_email_missing"
+                # falsy: error (failed mail)
+                elif recipient == recipient_falsy_1:
+                    recipient_info['trace_status'] = "cancel"
+                    recipient_info['failure_type'] = "mail_email_invalid"
+                    recipient_info['email'] = recipient.email_from  # normalized is False but email should be falsymail
                 else:
-                    email = self._find_sent_email(self.user_marketing.email_formatted, [recipient.email_normalized])
-                # preview correctly integrated rendered qweb
-                self.assertIn(
-                    'Hi %s :)' % recipient.name,
-                    email['body'])
-                # rendered unsubscribe
-                self.assertIn(
-                    '%s/mailing/%s/confirm_unsubscribe' % (mailing.get_base_url(), mailing.id),
-                    email['body'])
-                unsubscribe_href = self._get_href_from_anchor_id(email['body'], "url6")
-                unsubscribe_url = werkzeug.urls.url_parse(unsubscribe_href)
-                unsubscribe_params = unsubscribe_url.decode_query().to_dict(flat=True)
-                self.assertEqual(int(unsubscribe_params['document_id']), recipient.id)
-                self.assertEqual(unsubscribe_params['email'], recipient.email_normalized)
-                self.assertEqual(
-                    mailing._generate_mailing_recipient_token(unsubscribe_params['document_id'], (unsubscribe_params['email'])),
-                    unsubscribe_params['hash_token']
-                )
-                # rendered view
-                self.assertIn(
-                    '%s/mailing/%s/view' % (mailing.get_base_url(), mailing.id),
-                    email['body'])
-                view_href = self._get_href_from_anchor_id(email['body'], "url6")
-                view_url = werkzeug.urls.url_parse(view_href)
-                view_params = view_url.decode_query().to_dict(flat=True)
-                self.assertEqual(int(view_params['document_id']), recipient.id)
-                self.assertEqual(view_params['email'], recipient.email_normalized)
-                self.assertEqual(
-                    mailing._generate_mailing_recipient_token(view_params['document_id'], (view_params['email'])),
-                    view_params['hash_token']
-                )
+                    # multi email -> outgoing email contains all emails
+                    if recipient == recipient_dup_3:
+                        email = self._find_sent_email(self.user_marketing.email_formatted, ['test.record.09@test.example.com', 'test.multi@test.example.com'])
+                    else:
+                        email = self._find_sent_email(self.user_marketing.email_formatted, [recipient.email_normalized])
+                    # preview correctly integrated rendered qweb
+                    self.assertIn(
+                        'Hi %s :)' % recipient.name,
+                        email['body'])
+                    # rendered unsubscribe
+                    self.assertIn(
+                        '%s/mailing/%s/confirm_unsubscribe' % (mailing.get_base_url(), mailing.id),
+                        email['body'])
+                    unsubscribe_href = self._get_href_from_anchor_id(email['body'], "url6")
+                    unsubscribe_url = werkzeug.urls.url_parse(unsubscribe_href)
+                    unsubscribe_params = unsubscribe_url.decode_query().to_dict(flat=True)
+                    self.assertEqual(int(unsubscribe_params['document_id']), recipient.id)
+                    self.assertEqual(unsubscribe_params['email'], recipient.email_normalized)
+                    self.assertEqual(
+                        mailing._generate_mailing_recipient_token(unsubscribe_params['document_id'], (unsubscribe_params['email'])),
+                        unsubscribe_params['hash_token']
+                    )
+                    # rendered view
+                    self.assertIn(
+                        '%s/mailing/%s/view' % (mailing.get_base_url(), mailing.id),
+                        email['body'])
+                    view_href = self._get_href_from_anchor_id(email['body'], "url6")
+                    view_url = werkzeug.urls.url_parse(view_href)
+                    view_params = view_url.decode_query().to_dict(flat=True)
+                    self.assertEqual(int(view_params['document_id']), recipient.id)
+                    self.assertEqual(view_params['email'], recipient.email_normalized)
+                    self.assertEqual(
+                        mailing._generate_mailing_recipient_token(view_params['document_id'], (view_params['email'])),
+                        view_params['hash_token']
+                    )
 
-            self.assertMailTraces(
-                [recipient_info], mailing, recipient,
-                mail_links_info=[[
-                    ('url0', 'https://www.odoo.tz/my/%s' % recipient.name, True, {}),
-                    ('url1', 'https://www.odoo.be', True, {}),
-                    ('url2', 'https://www.odoo.com', True, {}),
-                    ('url3', 'https://www.odoo.eu', True, {}),
-                    ('url4', 'https://www.example.com/foo/bar?baz=qux', True, {'baz': 'qux'}),
-                    ('url5', '%s/event/dummy-event-0' % mailing.get_base_url(), True, {}),
-                    # view is not shortened and parsed at sending
-                    ('url6', '%s/view' % mailing.get_base_url(), False, {}),
-                    ('url7', 'mailto:test@odoo.com', False, {}),
-                    # unsubscribe is not shortened and parsed at sending
-                    ('url8', '%s/unsubscribe_from_list' % mailing.get_base_url(), False, {}),
-                ]],
-                check_mail=True,
-            )
+                self.assertMailTraces(
+                    [recipient_info], mailing, recipient,
+                    mail_links_info=[[
+                        ('url0', 'https://www.odoo.tz/my/%s' % recipient.name, True, {}),
+                        ('url1', 'https://www.odoo.be', True, {}),
+                        ('url2', 'https://www.odoo.com', True, {}),
+                        ('url3', 'https://www.odoo.eu', True, {}),
+                        ('url4', 'https://www.example.com/foo/bar?baz=qux', True, {'baz': 'qux'}),
+                        ('url5', '%s/event/dummy-event-0' % mailing.get_base_url(), True, {}),
+                        # view is not shortened and parsed at sending
+                        ('url6', '%s/view' % mailing.get_base_url(), False, {}),
+                        ('url7', 'mailto:test@odoo.com', False, {}),
+                        # unsubscribe is not shortened and parsed at sending
+                        ('url8', '%s/unsubscribe_from_list' % mailing.get_base_url(), False, {}),
+                    ]],
+                    check_mail=True,
+                )
 
         # sent: 15, 2 bl, 3 opt-out, 3 invalid -> 7 remaining
         # ignored: 2 bl + 3 optout + 2 invalid + 1 duplicate; failed: 0
