a3312f797f55ab95bd6443dc32f81da704f94d79
Deependra Solanki <deso@odoo.com>
2025-07-01 19:20:20 +0530

    [IMP] html_editor: replace image description dialog with popover
    This commit replaces the image description dialog with a popover. Just like the
    dialog, the popover allows users to edit the alt and title attributes of an
    image.
    
    task-4770922

--- addons/html_editor/static/src/main/media/image_description.js
+++ addons/html_editor/static/src/main/media/image_description.js
@@ -1,34 +1,17 @@
 import { Component } from "@odoo/owl";
 import { Dialog } from "@web/core/dialog/dialog";
-import { useService } from "@web/core/utils/hooks";
 import { toolbarButtonProps } from "@html_editor/main/toolbar/toolbar";
 
 export class ImageDescription extends Component {
     static components = { Dialog };
     static props = {
         ...toolbarButtonProps,
-        getDescription: Function,
-        getTooltip: Function,
-        updateImageDescription: Function,
+        openImageDescriptionPopover: Function,
     };
     static template = "html_editor.ImageDescription";
-
-    setup() {
-        this.dialog = useService("dialog");
-    }
-
-    openDescriptionDialog() {
-        this.dialog.add(ImageDescriptionDialog, {
-            description: this.props.getDescription(),
-            onConfirm: (description, tooltip) =>
-                this.props.updateImageDescription({ description, tooltip }),
-            tooltip: this.props.getTooltip(),
-        });
-    }
 }
 
-class ImageDescriptionDialog extends Component {
-    static components = { Dialog };
+export class ImageDescriptionPopover extends Component {
     static props = {
         close: Function,
         description: {
@@ -41,7 +24,7 @@ class ImageDescriptionDialog extends Component {
             optional: true,
         },
     };
-    static template = "html_editor.ImageDescriptionDialog";
+    static template = "html_editor.ImageDescriptionPopover";
 
     setup() {
         this.state = {
@@ -51,7 +34,7 @@ class ImageDescriptionDialog extends Component {
     }
 
     onSave() {
-        this.props.onConfirm(this.state.description, this.state.tooltip);
+        this.props.onConfirm(this.state.description || "", this.state.tooltip || "");
         this.props.close();
     }
 }
