190c4adea3dafef2cf6bd58a011cb27daa354a8e
Julien Mougenot <jum@odoo.com>
2025-07-02 17:32:41 +0200

    [REF] web,*: cleanup datetime service API
    This commit is a rework of the DateTime service & hook modules in web.
    
    The API of the 'datetime_service' was too convoluted, bloated with
    exported members that were only used by the hook.
    
    Now, most of the logic is handled by the service, and the hook simply
    gives it a flag to tell the service to handle Owl lifecycle hooks. This
    effectively allows to only send the few members (methods & variables)
    that are used by callers of the service externally.
    
    Task 3495251

--- addons/web/static/src/public/datetime_picker.js
+++ addons/web/static/src/public/datetime_picker.js
@@ -1,12 +1,11 @@
-import { Interaction } from "@web/public/interaction";
-import { registry } from "@web/core/registry";
-
 import {
     deserializeDate,
     deserializeDateTime,
     parseDate,
     parseDateTime,
 } from "@web/core/l10n/dates";
+import { registry } from "@web/core/registry";
+import { Interaction } from "@web/public/interaction";
 
 export class DatetimePicker extends Interaction {
     static selector = "[data-widget='datetime-picker']";
@@ -20,19 +19,20 @@ export class DatetimePicker extends Interaction {
     start() {
         const parseFunction = this.type === "date" ? parseDate : parseDateTime;
         const deserializeFunction = this.type === "date" ? deserializeDate : deserializeDateTime;
-        this.registerCleanup(this.services.datetime_picker
-            .create({
-                target: this.el,
-                pickerProps: {
-                    type: this.type,
-                    minDate: this.minDate && deserializeFunction(this.minDate),
-                    maxDate: this.maxDate && deserializeFunction(this.maxDate),
-                    value: parseFunction(this.el.value),
-                },
-            }).enable());
+        this.registerCleanup(
+            this.services.datetime_picker
+                .create({
+                    target: this.el,
+                    pickerProps: {
+                        type: this.type,
+                        minDate: this.minDate && deserializeFunction(this.minDate),
+                        maxDate: this.maxDate && deserializeFunction(this.maxDate),
+                        value: parseFunction(this.el.value),
+                    },
+                })
+                .enable()
+        );
     }
 }
 
-registry
-    .category("public.interactions")
-    .add("web.datetime_picker", DatetimePicker);
+registry.category("public.interactions").add("web.datetime_picker", DatetimePicker);
