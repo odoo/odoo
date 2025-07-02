9aa6ccdb8535786f5f6dc4cc60ebcf1161db0dcf
Florian Vranckx <flvr@odoo.com>
2025-07-02 16:26:37 +0200

    [FIX] web_editor: prevent direct writing to history
    This commit prevent direct write to history, also,
    removes it from copy_data as it would fail anyway when trying to
    write the history

--- addons/web_editor/models/html_field_history_mixin.py
+++ addons/web_editor/models/html_field_history_mixin.py
@@ -40,7 +40,15 @@ class HtmlFieldHistory(models.AbstractModel):
                         history_metadata[field_name].append(metadata)
             rec.html_field_history_metadata = history_metadata
 
+    def copy_data(self, default=None):
+        vals = super().copy_data(default)
+        if 'html_field_history' in vals:
+            del vals['html_field_history']
+        return vals
+
     def write(self, vals):
+        if 'html_field_history' in vals:
+            del vals['html_field_history']
         new_revisions = False
         db_contents = None
         versioned_fields = self._get_versioned_fields()
