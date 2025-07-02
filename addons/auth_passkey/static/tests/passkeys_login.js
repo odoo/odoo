4239a0a1d82f8a4b22e37a97e18033d3aabbbcef
bram1000 <brvg@odoo.com>
2025-07-02 14:07:50 +0000

    [IMP] auth_passkey: add user-flow test tours
    Passkeys did not have any tour tests, and since this passkeys are
    reliant on the way the authentication framework of Odoo works, it can
    break whenever anything related to authentication is changed.
    
    Task-3663657
    
    closes odoo/odoo#220700
    
    X-original-commit: a5798038fdc62b63afc0023517b466ef6816b27c
    Signed-off-by: Brice Bartoletti (bib) <bib@odoo.com>
    Signed-off-by: Bram Van Gaal (brvg) <brvg@odoo.com>

---
+++ addons/auth_passkey/static/tests/passkeys_login.js
@@ -0,0 +1,42 @@
+import { registry } from "@web/core/registry";
+import { patch } from "@web/core/utils/patch";
+import * as passkeyLib from "../lib/simplewebauthn";
+
+registry.category("web_tour.tours").add('passkeys_tour_login', {
+    url: '/web/login',
+    steps: () => [
+        {
+            content: "Inject authenticator data",
+            trigger: 'body',
+            run: () => {
+                // Due to switching from /web/login to /odoo, the asset bundles will be different. As a result this will automatically clean up the test.
+                patch(passkeyLib, {
+                    async startAuthentication() {
+                        return {
+                            // test-keepassxc
+                            "id": "y6aJVJsvvSSkbwTeGZ1FbQP_jCDho7EBPwZq-3lAjQ0",
+                            "rawId": "y6aJVJsvvSSkbwTeGZ1FbQP_jCDho7EBPwZq-3lAjQ0",
+                            "response": {
+                                "authenticatorData": "SZYN5YgOjGh0NBcPZHZgW4_krrmihjLHmVzzuoMdl2MFAAAAAA",
+                                "clientDataJSON": "eyJjaGFsbGVuZ2UiOiJMTnBWMGRQSU10bXBTd0dlbklIX2gxVnljUXVBZ0ZnUVJKOVRQS0JvTmF5U2NOQUVyUy1yc25hVTE5bjdfQWFYemVpWVJnM25HSTN5dUgwYWk2VVBYQSIsImNyb3NzT3JpZ2luIjpmYWxzZSwib3JpZ2luIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6ODg4OCIsInR5cGUiOiJ3ZWJhdXRobi5nZXQifQ",
+                                "signature": "MEYCIQCqkh2NBQQao5uDTaBKyNhiEpnk4jgbH-PjdLAul9-d0gIhAMObtNTbaEMUILdNgCT01BKNN4NHRzkzsGaDN2Ozu0WX",
+                                "userHandle": "Ng",
+                            },
+                            "type": "public-key",
+                            "clientExtensionResults": {},
+                            "authenticatorAttachment": "platform",
+                        };
+                    },
+                });
+            },
+        }, {
+            content: 'Login with Passkey',
+            trigger: 'a:contains("Use a Passkey")',
+            run: 'click',
+            expectUnloadPage: true,
+        }, {
+            content: 'Check if we are logged in',
+            trigger: '.o_user_menu .dropdown-toggle',
+        },
+    ]
+})
