c76964ce1eea08b1f702322a184a48f4268dd7aa
fja-odoo <fja@odoo.com>
2025-07-02 18:05:51 +0200

    [IMP] pos_qfpay: Add QFPay payment terminal
    task-id: 4844129

---
+++ addons/pos_qfpay/__manifest__.py
@@ -0,0 +1,20 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'POS QFPay',
+    'version': '1.0',
+    'category': 'Sales/Point of Sale',
+    'sequence': 6,
+    'author': 'Odoo S.A.',
+    'summary': 'Integrate your POS with the QFPay terminal in Hong Kong',
+    'data': [
+        'views/pos_payment_method_views.xml',
+    ],
+    'depends': ['point_of_sale'],
+    'installable': True,
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'pos_qfpay/static/**/*',
+        ],
+    },
+    'license': 'LGPL-3',
+}
