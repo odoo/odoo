c3fe2a2bd8eb73f2f820f6c7644f8e1090939ec9
Hadi El Yakhni <hael@odoo.com>
2025-07-03 11:46:45 +0000

    [FIX] pos_restaurant: update payment line when tipping after
    Steps to reproduce:
    -------------------
    1. Enable tipping after payment
    2. Make an order, pay it with customer account, then tip an amount X
    3. On the receipt, that amount X is shown as 'change'!!
    4. Close the PoS session
    5. On backend, go to Sessions, and choose the session you just closed
    6. It won't be closed, as there are still a diff of X amount, so click
       'Close Session & Post Entries' (the purple button), and confirm the
       prompt about posting the diff X into the receivable PoS account
    7. Go to journal items (the magical button), and observe that the tip
       amount X is on a different move line labeled 'Difference at closing
       PoS session', and is not even in the name of the customer
    
    Why the problem:
    ----------------
    Before 17.4, we had a function `set_tip` on the backend that updated the
    payment line to add the tip amount whenever we tip after [1]. However,
    after commit 2a5f1ab, the logic of
    `set_tip` was moved to the frontend [2], or most of it, as it seems
    that we missed moving the logic that updates the payment line.
    
    The fix:
    --------
    We now restore the method `set_tip` that was before 2a5f1ab, which will
    update the payment line and the order.
    
    [1]: https://github.com/odoo-dev/odoo/blob/636606d12cec79a0196ed0f9b7bb71a78d4fe65a/addons/pos_restaurant/models/pos_order.py#L206
    [2]: https://github.com/odoo/odoo/blob/d5baeec9b60bdf3a5ab9d1243f46e957c0489877/addons/pos_restaurant/static/src/app/tip_screen/tip_screen.js#L90-L96
    
    opw-4736154
    
    X-original-commit: 7c2d7c6aa5f39fcebb25ac324de1e9db2a8ca5b5

--- addons/pos_restaurant/models/pos_order.py
+++ addons/pos_restaurant/models/pos_order.py
@@ -25,3 +25,22 @@ class PosOrder(models.Model):
         result = super().read_pos_data(data, config_id)
         result['restaurant.order.course'] = self.course_ids.read(self.course_ids._load_pos_data_fields(config_id), load=False) if config_id else []
         return result
+<<<<<<< b85e3fe518f6157cd403fe72dadbb998f817dc36
+||||||| 2351aa29d33903aa6c57b68d9ff501653e9d50cb
+    
+=======
+
+    def set_tip(self, tip_amount, payment_line_id):
+        """Update tip state on `self` and the tip amount on the payment line."""
+
+        self.ensure_one()
+
+        payment_line = self.payment_ids.filtered(lambda line: line.id == payment_line_id)
+        payment_line.write({
+            'amount': payment_line.amount + tip_amount,
+        })
+        self.write({
+            "is_tipped": True,
+            "tip_amount": tip_amount,
+        })
+>>>>>>> 8776b61a98e0dec071187cb8a32b43cc201a3f3d
