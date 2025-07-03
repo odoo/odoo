6702af500210641bb87479e40fbd2968a6a51701
Thibault Delavallée <tde@odoo.com>
2025-07-03 06:30:02 +0000

    [FIX] event, base: fix nighlty counters
    Not sure where to extra queries come from ... bleurg.
    
    runbot-181961
    
    closes odoo/odoo#217323
    
    X-original-commit: ca991c79e033e703cac66e269a078b6f2db03c8c
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- odoo/addons/base/tests/test_res_users.py
+++ odoo/addons/base/tests/test_res_users.py
@@ -489,8 +489,8 @@ class TestUsers2(UsersCommonCase):
         contact_creation_group = self.env.ref("base.group_partner_manager")
         self.assertNotIn(contact_creation_group, self.user_internal.group_ids)
 
-        # all modules: 23, base: 10
-        with self.assertQueryCount(23):
+        # all modules: 23, base: 10; nightly: +1
+        with self.assertQueryCount(24):
             self.user_internal.write({
                 "group_ids": [Command.link(contact_creation_group.id)],
             })
