689bd45c15abaf4563310328fe98ac56d597c950
clesgow <quwo@odoo.com>
2025-07-03 11:34:34 +0200

    [IMP] stock, *: allow multi-level packages
    Before this change, it wasn't possible to handle multiple levels of
    packages within Odoo. One simple example of this is having a few boxes
    put on top of a pallet, which is a *very* common occurence in stock.
    
    Changes summary:
    - Packages inside other packages
    - Rework of the put in pack, can display a widget if enabled in
      Operation Type
    - You can move a package containing other packages, this will move all
    of them
    - The handling of a package is *always* done on the move lines directly
    - Packages now holds several more information regarding their current
    reservation
    - A new `stock.package.history` record is added to keep track of what
      was moved in previous transfers, as package hierarchy may be different
      between the past and now
    
    Task-4314600

--- addons/stock_delivery/__manifest__.py
+++ addons/stock_delivery/__manifest__.py
@@ -21,7 +21,7 @@ When creating invoices from picking, the system is able to add and compute the s
         'views/report_deliveryslip.xml',
         'views/report_package_barcode.xml',
         'wizard/choose_delivery_carrier_views.xml',
-        'wizard/choose_delivery_package_views.xml',
+        'wizard/stock_put_in_pack_views.xml',
         'views/stock_package_type_views.xml',
         'views/stock_picking_type_views.xml',
         'views/stock_rule_views.xml',
--- addons/stock_delivery/models/stock_package.py
+++ addons/stock_delivery/models/stock_package.py
@@ -30,3 +30,15 @@ class StockPackage(models.Model):
     weight_uom_name = fields.Char(string='Weight unit of measure label', compute='_compute_weight_uom_name', readonly=True, default=_get_default_weight_uom)
     weight_is_kg = fields.Boolean("Technical field indicating whether weight uom is kg or not (i.e. lb)", compute="_compute_weight_is_kg")
     weight_uom_rounding = fields.Float("Technical field indicating weight's number of decimal places", compute="_compute_weight_is_kg")
+
+    def _pre_put_in_pack_hook(self, package_id=False, package_type_id=False, package_name=False, from_package_wizard=False):
+        res = super()._pre_put_in_pack_hook(package_id, package_type_id, package_name, from_package_wizard)
+        move_lines = self.move_line_ids
+        if res and move_lines.carrier_id:
+            if self.env.context.get('picking_id'):
+                move_lines = move_lines.filtered(lambda ml: ml.picking_id.id == self.env.context['picking_id'])
+
+            context = res.get('context', {})
+            context['default_package_carrier_type'] = move_lines._get_package_carrier_type_for_pack()
+            res['context'] = context
+        return res
