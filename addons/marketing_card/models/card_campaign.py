f35d17d256811969164a275620023e278027f3c8
Renaud Thiry <reth@odoo.com>
2025-07-03 17:00:16 +0200

    [IMP] marketing_card: set default values on campaigns
    Currently if you create a new card campaign the layout is completely
    empty until you start filling in fields.
    
    This looks really odd and doesn't give a good idea of what the template
    actually looks like. Instead we want there to be some pre-filled values
    so the layout is clear to the user.
    
    task-4247003

--- addons/marketing_card/models/card_campaign.py
+++ addons/marketing_card/models/card_campaign.py
@@ -66,7 +66,7 @@ class CardCampaign(models.Model):
     content_button = fields.Char('Button')
 
     # Dynamic Content fields
-    content_header = fields.Char('Header', translate=True)
+    content_header = fields.Char('Header', default='Card Title', translate=True)
     content_header_dyn = fields.Boolean('Is Dynamic Header')
     content_header_path = fields.Char('Header Path')
     content_header_color = fields.Char('Header Color')
@@ -76,7 +76,7 @@ class CardCampaign(models.Model):
     content_sub_header_path = fields.Char('Sub-Header Path')
     content_sub_header_color = fields.Char('Sub Header Color')
 
-    content_section = fields.Char('Section', translate=True)
+    content_section = fields.Char('Section', default='Details', translate=True)
     content_section_dyn = fields.Boolean('Is Dynamic Section')
     content_section_path = fields.Char('Section Path')
 
@@ -224,6 +224,10 @@ class CardCampaign(models.Model):
         reset_dict = dict.fromkeys(self._get_path_fields(), False)
         for campaign in self:
             campaign.update(reset_dict)
+            # set some default here to show the possible layout of the template
+            model_name = campaign.preview_record_ref and campaign.preview_record_ref._name
+            if model_name and 'image_512' in self.env[model_name] and not campaign.content_image1_path:
+                campaign.content_image1_path = 'image_512'
 
     @api.model_create_multi
     def create(self, vals_list):
