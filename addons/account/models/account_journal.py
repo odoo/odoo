a18be59efa6cde22835414102987fc8bf18e96c9
ppat-odoo <ppat@odoo.com>
2025-07-03 05:07:56 +0000

    [IMP] account: rename label of non deductible account on journal
    Change label of `non_deductible_account_id` field on `account.journal` from
    `Private Part Account` to `Private Share Account` as private part means
    something else in english.
    
    task-4868244
    
    closes odoo/odoo#217810
    
    X-original-commit: f6653072801c386e55c758e92c04943ee556d795
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: Pratham Nareshbhai Patel (ppat) <ppat@odoo.com>

--- addons/account/models/account_journal.py
+++ addons/account/models/account_journal.py
@@ -122,7 +122,7 @@ class AccountJournal(models.Model):
     non_deductible_account_id = fields.Many2one(
         comodel_name='account.account',
         check_company=True,
-        string='Private Part Account',
+        string='Private Share Account',
         readonly=False,
         store=True,
         help="Account used to register the private part of mixed expenses.",
