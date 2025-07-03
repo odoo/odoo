8de7f07a890cb31cf72295d9a0c17f65279c40cf
Majed Alhanash (malh) <malh@odoo.com>
2025-07-03 14:04:35 +0000

    [FIX] point_of_sale: improve pos inalterability check report
    ### Problem:
    Pos inalterability Check report is specific to French localization.
    It verifies whether POS orders have been modified by computing a hash
    of the order data and comparing it to the previously stored hash.
    _compute_string_to_hash method is computationally expensive and
    leads to significant performance and memory issues when processing
    more than 50,000 orders.
    
    ### Benchmark
    Before:
    
    | Orders | Time    | Memory |
    |--------|---------|--------|
    | 1k     | 15s     | 10MB   |
    | 10k    | 77s     | 81MB   |
    | 20k    | 102s    | 110MB  |
    | 40K    | timeout | 256MB  |
    
    After:
    
    | Orders | Time | Memory |
    |--------|------|--------|
    | 1k     | 5s   | 7MB    |
    | 10k    | 9s   | 42MB   |
    | 40K    | 20s  | 174MB  |
    | 100k   | 42s  | 550MB  |
    | 330k   | 126s | 1.2GB  |
    
    ### Solution:
    Fetching only required fields to compute _compute_string_to_hash
    opw-4901994
    
    closes odoo/odoo#221701
    
    X-original-commit: e8c0b86eb888765892c2a428c907c98ce99ed39e
    Signed-off-by: Stéphane Vanmeerhaeghe (stva) <stva@odoo.com>
    Signed-off-by: Majed Alhanash (malh) <malh@odoo.com>

--- addons/l10n_fr_pos_cert/models/pos.py
+++ addons/l10n_fr_pos_cert/models/pos.py
@@ -101,13 +101,61 @@ class PosOrder(models.Model):
         return hash_string.hexdigest()
 
     def _compute_string_to_hash(self):
-        def _getattrstring(obj, field_str):
-            field_value = obj[field_str]
-            if obj._fields[field_str].type == 'many2one':
-                field_value = field_value.id
-            if obj._fields[field_str].type in ['many2many', 'one2many']:
-                field_value = field_value.sorted().ids
+        def _getattrstring(field_value, field_type, model_name=None):
+            if field_type in ('many2many', 'one2many'):
+                if field_value:
+                    sorted_ids = sorted_relational_ids.get(model_name, [])
+                    value_set = set(field_value)
+                    field_value = [id for id in sorted_ids if id in value_set]
+                else:
+                    field_value = []
             return str(field_value)
+
+        def collect_sorted_relational_ids(orders_data, lines_data, order_field_defs, line_field_defs):
+            relational_ids = defaultdict(set)
+
+            for data_list, field_names, field_defs in (
+                (orders_data, fields_to_fetch, order_field_defs),
+                (lines_data, LINE_FIELDS, line_field_defs),
+            ):
+                for record in data_list:
+                    for field in field_names:
+                        field_def = field_defs.get(field)
+                        if field_def and field_def['type'] in ('many2many', 'one2many'):
+                            ids = record.get(field) or []
+                            relational_ids[field_def['comodel']].update(ids)
+
+            sorted_relational_ids = {}
+            for model_name, ids in relational_ids.items():
+                if ids:
+                    # Use search() to get IDs sorted by _order the same way Odoo ORM does for relational fields
+                    sorted_relational_ids[model_name] = self.env[model_name].search([('id', 'in', list(ids))]).ids
+
+            return sorted_relational_ids
+        fields_to_fetch = list(set(ORDER_FIELDS_BEFORE_17_4) | set(ORDER_FIELDS_FROM_17_4))
+        orders_data = self.read(fields_to_fetch + ['id'], load='')
+        lines_data = self.lines.read(LINE_FIELDS + ['id', 'order_id'], load='')
+
+        orders_by_id = {order['id']: order for order in orders_data}
+        lines_by_order = defaultdict(list)
+        for line in lines_data:
+            lines_by_order[line['order_id']].append(line)
+        order_field_defs = {
+            field: {
+                'type': self._fields[field].type,
+                'comodel': self._fields[field].comodel_name if hasattr(self._fields[field], 'comodel_name') else None
+            }
+            for field in fields_to_fetch
+        }
+        line_field_defs = {
+            field: {
+                'type': self.lines._fields[field].type,
+                'comodel': self.lines._fields[field].comodel_name if hasattr(self.lines._fields[field], 'comodel_name') else None
+            }
+            for field in LINE_FIELDS
+        }
+
+        sorted_relational_ids = collect_sorted_relational_ids(orders_data, lines_data, order_field_defs, line_field_defs)
 
         for order in self:
             values = {}
@@ -117,11 +165,18 @@ class PosOrder(models.Model):
                 order_fields = ORDER_FIELDS_BEFORE_17_4
             for field in order_fields:
                 values[field] = _getattrstring(order, field)
+            order_data = orders_by_id[order.id]
 
-            for line in order.lines:
+            for field in order_fields:
+                field_def = order_field_defs[field]
+                values[field] = _getattrstring(order_data.get(field), field_def['type'], field_def['comodel'])
+
+            for line in lines_by_order[order.id]:
                 for field in LINE_FIELDS:
-                    k = 'line_%d_%s' % (line.id, field)
-                    values[k] = _getattrstring(line, field)
+                    k = 'line_%d_%s' % (line['id'], field)
+                    field_def = line_field_defs[field]
+                    values[k] = _getattrstring(line.get(field), field_def['type'], field_def['comodel'])
+
             #make the json serialization canonical
             #  (https://tools.ietf.org/html/draft-staykov-hu-json-canonical-form-00)
             order.l10n_fr_string_to_hash = dumps(values, sort_keys=True,
--- addons/l10n_fr_pos_cert/tests/__init__.py
+++ addons/l10n_fr_pos_cert/tests/__init__.py
@@ -1,3 +1,4 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import test_frontend
+from . import test_string_to_hash
