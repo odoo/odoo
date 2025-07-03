23d1d13e167240f8c369416d553519e47cb591f2
Thibault Delavallée <tde@odoo.com>
2025-07-03 15:05:43 +0200

    [REF] mail: use encapsulation in template, improve render context
    Now that most "simple" encapsulation are now using the tool, we can
    use it in template send mail method. This one highlights that when
    using notification layout we expect quite a lot of context variables.
    Templates can be rendered with simplified context (aka does not crash)
    but it is better to craft a complete context, making it easier when
    reading code to discover potential tweaks.
    
    Those come mainly from the "message_post" notification process that
    also uses encapsulation layouts with quite a lot of options. With this
    commit we now offer the same options when doing the encapsulation
    manually.
    
    Task-4690371
    
    Part-of: odoo/odoo#212083
    Related: odoo/enterprise#86621
    Related: odoo/upgrade#7784
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mail/models/mail_render_mixin.py
+++ addons/mail/models/mail_render_mixin.py
@@ -171,21 +171,54 @@ class MailRenderMixin(models.AbstractModel):
         or 'mail_notification_light'. Also used for digest layouts. This leads
         to some default rendering values being computed here, often used in those
         templates. """
+        record_name = (add_context or {}).get('record_name', context_record.display_name if context_record else '')
+        subtype = (add_context or {}).get('subtype', self.env['mail.message.subtype'].sudo())
         template_ctx = {
             'body': html,
-            'company': context_record['company_id'] if (context_record and 'company_id' in context_record) else self.env.company,
-            'record_name': context_record.display_name if context_record else '',
-            'model_description': self.env['ir.model']._get(context_record._name).display_name if context_record else False,
             'record': context_record,
+            'record_name': record_name,
+            **(add_context or {}),
         }
-        if add_context:
-            template_ctx.update(**add_context)
         # the 'mail_notification_light' expects a mail.message 'message' context, let's give it one
         if not template_ctx.get('message'):
             msg_vals = {'body': html}
             if context_record:
                 msg_vals.update({'model': context_record._name, 'res_id': context_record.id})
             template_ctx['message'] = self.env['mail.message'].sudo().new(msg_vals)
+        # other message info
+        if not subtype:
+            template_ctx['is_discussion'] = False
+            template_ctx['subtype_internal'] = False
+        else:
+            if 'is_discussion' not in template_ctx:
+                template_ctx['is_discussion'] = subtype.id == self.env['ir.model.data']._xmlid_to_res_id('mail.mt_comment')
+            if 'subtype_internal' not in template_ctx:
+                template_ctx['subtype_internal'] = subtype.is_internal
+        template_ctx.setdefault('subtype', subtype)
+        template_ctx.setdefault('tracking_values', [])
+        # record info
+        if 'model_description' not in template_ctx:
+            template_ctx['model_description'] = self.env['ir.model']._get(context_record._name).display_name if context_record else False
+        template_ctx.setdefault('subtitles', [record_name])
+        # user / environment
+        template_ctx.setdefault('author_user', False)
+        if 'company' not in template_ctx:
+            template_ctx['company'] = context_record._mail_get_companies(default=self.env.company)[context_record.id] if context_record else self.env.company
+        template_ctx.setdefault('email_add_signature', False)
+        template_ctx.setdefault('lang', self.env.lang)
+        template_ctx.setdefault('signature', '')
+        template_ctx.setdefault('show_unfollow', False)
+        template_ctx.setdefault('website_url', '')
+        # display: actions / buttons
+        template_ctx.setdefault('button_access', False)
+        template_ctx.setdefault('has_button_access', False)
+        # display
+        template_ctx.setdefault('email_notification_force_header', self.env.context.get('email_notification_force_header', False))
+        template_ctx.setdefault('email_notification_force_footer', self.env.context.get('email_notification_force_footer', False))
+        template_ctx.setdefault('email_notification_allow_header', self.env.context.get('email_notification_allow_header', True))
+        template_ctx.setdefault('email_notification_allow_footer', self.env.context.get('email_notification_allow_footer', False))
+        # tools
+        template_ctx.setdefault('is_html_empty', is_html_empty)
 
         html = self.env['ir.qweb']._render(layout_xmlid, template_ctx, minimal_qcontext=True, raise_if_not_found=False)
         if not html:
