a3978e3a552caf5fcfabc9e9dec3b39e92b09200
roen-odoo <roen@odoo.com>
2025-07-04 15:41:53 +0200

    [FIX] pos_sale: fix double picking multiple lots
    When creating a sale order for a product, confirming it, and then
    modifying the stock move to use 2 different lots. Then paying the order
    in the POS, the picking created for the pos order will have double qty
    
    Steps to reproduce:
    -------------------
    * Create a product tracked by lot
    * Create 2 lots for that product
    * Create a sale order for that 3 quantity of that product
    * Confirm the sale order
    * Modify the stock move to use 2 different lots, for example:
      - Lot 1001: 2 units
      - Lot 1002: 1 unit
    * Pay the order in the POS
    * Check the picking created for the pos order
    > Observation: The picking created for the pos order has double the
      quantity for the lots.
    
    Why the fix:
    ------------
    A PoS line can only hold one lot, so when we import a sale order with
    multiple lots, we need to split the line into multiple lines, each with
    a single lot
    
    opw-4804704

--- addons/pos_sale/tests/test_pos_sale_flow.py
+++ addons/pos_sale/tests/test_pos_sale_flow.py
@@ -1540,3 +1540,72 @@ class TestPoSSale(TestPointOfSaleHttpCommon):
         self.assertEqual(pos_order_b.amount_total, 10, "PoS order amount should be 10 with the tax override 2")
         self.assertEqual(pos_order_b.amount_tax, 0, "PoS order untaxed amount should be 10 with the tax override 2")
         self.assertEqual(pos_order_b.lines[0].tax_ids, tax_override_2, "PoS order should have the tax override 2")
+
+    def test_multiple_lots_sale_order(self):
+        self.product = self.env['product.product'].create({
+            'name': 'Product',
+            'available_in_pos': True,
+            'is_storable': True,
+            'lst_price': 10.0,
+            'taxes_id': False,
+            'categ_id': self.product_category.id,
+            'tracking': 'lot',
+        })
+
+        self.warehouse = self.env['stock.warehouse'].search([('company_id', '=', self.env.company.id)], limit=1)
+        self.shelf_1 = self.env['stock.location'].create({
+            'name': 'Shelf 1',
+            'usage': 'internal',
+            'location_id': self.warehouse.lot_stock_id.id,
+        })
+        self.shelf_2 = self.env['stock.location'].create({
+            'name': 'Shelf 2',
+            'usage': 'internal',
+            'location_id': self.warehouse.lot_stock_id.id,
+        })
+
+        quants = self.env['stock.quant'].with_context(inventory_mode=True).create({
+            'product_id': self.product.id,
+            'inventory_quantity': 1,
+            'location_id': self.shelf_1.id,
+            'lot_id': self.env['stock.lot'].create({
+                'name': '1001',
+                'product_id': self.product.id,
+                'location_id': self.shelf_1.id,
+            }).id,
+        })
+        quants |= self.env['stock.quant'].with_context(inventory_mode=True).create({
+            'product_id': self.product.id,
+            'inventory_quantity': 2,
+            'location_id': self.shelf_2.id,
+            'lot_id': self.env['stock.lot'].create({
+                'name': '1002',
+                'product_id': self.product.id,
+                'location_id': self.shelf_2.id,
+            }).id,
+        })
+        quants.action_apply_inventory()
+
+        partner_test = self.env['res.partner'].create({'name': 'Test Partner'})
+
+        sale_order = self.env['sale.order'].create({
+            'partner_id': partner_test.id,
+            'order_line': [(0, 0, {
+                'product_id': self.product.id,
+                'name': self.product.name,
+                'product_uom_qty': 3,
+                'product_uom': self.product.uom_id.id,
+                'price_unit': self.product.lst_price,
+            })],
+        })
+        sale_order.action_confirm()
+        self.main_pos_config.open_ui()
+        self.start_tour("/pos/ui?config_id=%d" % self.main_pos_config.id, 'test_multiple_lots_sale_order', login="accountman")
+        self.main_pos_config.current_session_id.action_pos_session_close()
+        picking = sale_order.pos_order_line_ids.order_id.picking_ids
+        self.assertEqual(picking.move_ids.quantity, 3)
+        self.assertEqual(len(picking.move_ids.move_line_ids), 2)
+        self.assertEqual(picking.move_ids.move_line_ids[0].lot_id.name, '1001')
+        self.assertEqual(picking.move_ids.move_line_ids[0].quantity, 1)
+        self.assertEqual(picking.move_ids.move_line_ids[1].lot_id.name, '1002')
+        self.assertEqual(picking.move_ids.move_line_ids[1].quantity, 2)
