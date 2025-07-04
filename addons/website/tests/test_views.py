odoo/odoo

commit 97c2dd2ec569d34b044da6aa6f473277b48ada12

[IMP] website: use `_get_cached_template_info` cached value
Remove useless ormcache from 'is_view_active' and '_get_cached_visibility',
and use cache method.

Part-of: odoo/odoo#205321
Related: odoo/enterprise#83133
Signed-off-by: Raphael Collet <rco@odoo.com>

addons/website/models/ir_ui_view.py
@@ -381,6 +381,10 @@ class IrUiView(models.Model):
                     """
 
     @api.model
+    def _get_cached_template_prefetched_keys(self):
+        return super()._get_cached_template_prefetched_keys() + ['active', 'visibility']
+
+    @api.model
     def _get_template_minimal_cache_keys(self):
         return super()._get_template_minimal_cache_keys() + ['website_id']
 
@@ -411,7 +415,10 @@ class IrUiView(models.Model):
         return f"website_id asc, {super()._get_template_order()}"
 
     def _get_cached_visibility(self):
-        return self.visibility
+        info = self._get_cached_template_info(self.id, _view=self)
+        if info['error']:
+            raise info['error']
+        return info['visibility']
 
     def _handle_visibility(self, do_raise=True):
         """ Check the visibility set on the main view and raise 403 if you should not have access.

addons/website/models/website.py
@@ -1487,13 +1487,11 @@ class Website(models.Model):
         return self.env['ir.ui.view'].sudo().with_context(active_test=False)._get_template_view(view_id, raise_if_not_found=raise_if_not_found)
 
     @api.model
-    @tools.ormcache('key', 'self._context.get("website_id")', cache='templates')
     def is_view_active(self, key):
         """
             Return True if active, False if not active, None if not found
         """
-        view = self.viewref(key, raise_if_not_found=False)
-        return view.active if view else None
+        return self.env['ir.ui.view']._get_cached_template_info(key).get('active')
 
     @api.model
     def get_template(self, template):

addons/website/controllers/main.py
@@ -319,8 +319,8 @@ class Website(Home):
     def sitemap_website_info(env, rule, qs):
         website = env['website'].get_current_website()
         if not (
-            website.viewref('website.website_info', False).active
-            and website.viewref('website.show_website_info', False).active
+            website.is_view_active('website.website_info')
+            and website.is_view_active('website.show_website_info')
         ):
             # avoid 404 or blank page in sitemap
             return False

addons/website_sale/controllers/main.py
@@ -440,7 +440,7 @@ class WebsiteSale(payment_portal.PaymentPortal):
         else:
             attributes = lazy(lambda: ProductAttribute.browse(attribute_ids).sorted())
 
-        if website.viewref('website_sale.products_list_view').active:
+        if website.is_view_active('website_sale.products_list_view'):
             layout_mode = 'list'
         else:
             layout_mode = 'grid'

commit 7356ba42de579b4af0606e3a5d363270052a3b1c

[FIX] base,website: wrong is_view_active value for website_sale template
Fixed cache pollution of _get_cached_template_info and added tests
regarding the website editor, more specifically on enabling or disabling
options related to views.

Context:
- Base:
  - the xmlid and key of ir.ui.view are unique;
  - the ir.ui.view are inherited if they are active, otherwise inheritance
    is not applied;
  - _get_cached_template_info(key) returns the cached record values.

- Website:
  - the key of ir.ui.view are not unique; they can exist for different
    websites and without a website;
  - the ir.ui.view are inherited if they are active, otherwise inheritance
    is not applied. If a specific inherited view exists (for the current
    website), the default view (with same key) is not applied;
  - for t-call, the specific view are used only if the view is active,
    otherwise the default view is displayed;
  - is_view_active(key) should return False if the view of the website in
    question is inactive. If the default view is False, but the website
    view is True, then the response should be True;

Issue:
`is_view_active` returns True if a view with this key is active, ignoring
the website-specific inactive view.

The issue was seen because the editor changes the display (active) of the
views by copying them with the website reference. So when we want to
change the menu, and hide it, the view is copied and set active=False.
Part of the menu was still visible because it uses a
`t-if="is_view_active(...)"`.

Issue introduced by: 97c2dd2ec569d34b044da6aa6f473277b48ada12

closes odoo/odoo#216100

Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

addons/website/models/ir_ui_view.py
@@ -367,7 +367,7 @@ class IrUiView(models.Model):
 
     @api.model
     def _get_template_minimal_cache_keys(self):
-        return super()._get_template_minimal_cache_keys() + ['website_id']
+        return super()._get_template_minimal_cache_keys() + (self.env.context.get('website_id'),)
 
     @api.model
     def _get_template_domain(self, xmlids):

addons/website/models/website.py
@@ -1491,7 +1491,7 @@ class Website(models.Model):
         """
             Return True if active, False if not active, None if not found
         """
-        return self.env['ir.ui.view']._get_cached_template_info(key).get('active')
+        return self.env['ir.ui.view'].with_context(active_test=False)._get_cached_template_info(key).get('active')
 
     @api.model
     def get_template(self, template):

odoo/addons/base/models/ir_ui_view.py
@@ -1049,10 +1049,10 @@ actual arch.
         return ['id', 'key', 'active']
 
     def _get_template_minimal_cache_keys(self):
-        return ['active_test']
+        return (bool(self.env.context.get('active_test', True)),)
 
     @api.model
-    @tools.ormcache('id_or_xmlid', 'isinstance(id_or_xmlid, str) and tuple(self.env.context.get(k) or False for k in self._get_template_minimal_cache_keys())', cache='templates')
+    @tools.ormcache('id_or_xmlid', 'isinstance(id_or_xmlid, str) and self._get_template_minimal_cache_keys()', cache='templates')
     def _get_cached_template_info(self, id_or_xmlid, _view=None):
         """ Return the ir.ui.view id from the xml id, use `_preload_views`.
         """

addons/website/tests/test_views.py
@@ -29,6 +29,141 @@ class TestViewSavingCommon(common.TransactionCase):
         })
 
 
+@tagged('-at_install', 'post_install')
+class TestCustomizeView(common.HttpCase):
+    def url_open_authenticate(self, url, data):
+        self.authenticate('admin', 'admin')
+        response = self.url_open(url, json={'params': {**data, 'is_view_data': True}}, headers={"Content-Type": "application/json"})
+        data = json.loads(response.text)
+        self.logout()
+        return data.get("result")
+
+    def test_disabled_optional_template_t_call(self):
+        website = self.env['website'].search([], limit=1)
+        View = self.env['ir.ui.view']
+        default = View.create({
+            'name': 'test_view',
+            'type': 'qweb',
+            'key': 'website.test_view',
+            'arch_db': '<span>Default</span>'
+        })
+        custo = View.create({
+            'name': 'test_view',
+            'type': 'qweb',
+            'key': 'website.test_view',
+            'website_id': website.id,
+            'arch_db': '<span>Customized</span>'
+        })
+        template = View.create({
+            'name': 'test_root',
+            'type': 'qweb',
+            'key': 'website.test_root',
+            'arch_db': '''<div><t t-if="is_view_active('website.test_view')"> is active </t><t t-call="website.test_view"/></div>'''
+        })
+        page = self.env['website.page'].create({
+            'view_id': template.id,
+            'url': '/test_root',
+            'is_published': True,
+        })
+
+        website._force()
+
+        actives = self.url_open_authenticate('/website/theme_customize_data_get', {'keys': ['website.test_root', 'website.test_view']})
+        self.assertEqual(set(actives), {'website.test_root', 'website.test_view'})
+        self.assertEqual([custo.active, default.active], [True, True])
+        self.assertEqual(self.url_open(page.url).text, '<div> is active <span>Customized</span></div>')
+
+        self.url_open_authenticate('/website/theme_customize_data', {'enable': [], 'disable': ['website.test_view']})
+
+        actives = self.url_open_authenticate('/website/theme_customize_data_get', {'keys': ['website.test_root', 'website.test_view']})
+        self.assertEqual(set(actives), {'website.test_root'})
+        self.assertEqual([custo.active, default.active], [False, True])
+        self.assertEqual(self.url_open(page.url).text, '<div><span>Default</span></div>')
+
+        self.url_open_authenticate('/website/theme_customize_data', {'enable': ['website.test_view'], 'disable': []})
+
+        actives = self.url_open_authenticate('/website/theme_customize_data_get', {'keys': ['website.test_root', 'website.test_view']})
+        self.assertEqual(set(actives), {'website.test_root', 'website.test_view'})
+        self.assertEqual([custo.active, default.active], [True, True])
+        self.assertEqual(self.url_open(page.url).text, '<div> is active <span>Customized</span></div>')
+
+        self.url_open_authenticate('/website/theme_customize_data', {'enable': [], 'disable': ['website.test_view']})
+        custo.unlink()
+        self.assertEqual(self.url_open(page.url).text, '<div> is active <span>Default</span></div>')
+
+        # theme_customize_data will clone the default view to save the configuration
+        self.url_open_authenticate('/website/theme_customize_data', {'enable': [], 'disable': ['website.test_view']})
+        new_custo = View.with_context(active_test=False).search([('key', '=', 'website.test_view'), ('website_id', '=', website.id)])
+        self.assertEqual(bool(new_custo), True)
+        self.assertNotEqual(custo.id, new_custo.id)
+        self.assertEqual([new_custo.active, default.active], [False, True])
+        self.assertEqual(self.url_open(page.url).text, '<div><span>Default</span></div>')
+
+    def test_disabled_optional_template_xpath(self):
+        website = self.env.ref('website.default_website')
+        View = self.env['ir.ui.view']
+        template = View.create({
+            'name': 'test_root',
+            'type': 'qweb',
+            'key': 'website.test_root',
+            'arch_db': '''<div><t t-if="is_view_active('website.test_view')"> is active </t></div>'''
+        })
+
+        default = View.create({
+            'name': 'test_view',
+            'mode': 'extension',
+            'inherit_id': template.id,
+            'arch_db': '<div position="inside"><span>Default</span></div>',
+            'key': 'website.test_view',
+        })
+        custo = View.create({
+            'name': 'test_view',
+            'mode': 'extension',
+            'inherit_id': template.id,
+            'arch_db': '<div position="inside"><span>Customized</span></div>',
+            'key': 'website.test_view',
+            'website_id': website.id,
+        })
+        page = self.env['website.page'].create({
+            'view_id': template.id,
+            'url': '/test_root',
+            'is_published': True,
+        })
+
+        website._force()
+
+        actives = self.url_open_authenticate('/website/theme_customize_data_get', {'keys': ['website.test_root', 'website.test_view']})
+        self.assertEqual(set(actives), {'website.test_root', 'website.test_view'})
+        self.assertEqual([custo.active, default.active], [True, True])
+        self.assertEqual(self.url_open(page.url).text, '<div> is active <span>Customized</span></div>')
+
+        self.url_open_authenticate('/website/theme_customize_data', {'enable': [], 'disable': ['website.test_view']})
+
+        actives = self.url_open_authenticate('/website/theme_customize_data_get', {'keys': ['website.test_root', 'website.test_view']})
+        self.assertEqual(set(actives), {'website.test_root'})
+        self.assertEqual([custo.active, default.active], [False, True])
+        self.assertEqual(self.url_open(page.url).text, '<div></div>')
+
+        self.url_open_authenticate('/website/theme_customize_data', {'enable': ['website.test_view'], 'disable': []})
+
+        actives = self.url_open_authenticate('/website/theme_customize_data_get', {'keys': ['website.test_root', 'website.test_view']})
+        self.assertEqual(set(actives), {'website.test_root', 'website.test_view'})
+        self.assertEqual([custo.active, default.active], [True, True])
+        self.assertEqual(self.url_open(page.url).text, '<div> is active <span>Customized</span></div>')
+
+        self.url_open_authenticate('/website/theme_customize_data', {'enable': [], 'disable': ['website.test_view']})
+        custo.unlink()
+        self.assertEqual(self.url_open(page.url).text, '<div> is active <span>Default</span></div>')
+
+        # theme_customize_data will clone the default view to save the configuration
+        self.url_open_authenticate('/website/theme_customize_data', {'enable': [], 'disable': ['website.test_view']})
+        new_custo = View.with_context(active_test=False).search([('key', '=', 'website.test_view'), ('website_id', '=', website.id)])
+        self.assertEqual(bool(new_custo), True)
+        self.assertNotEqual(custo.id, new_custo.id)
+        self.assertEqual([new_custo.active, default.active], [False, True])
+        self.assertEqual(self.url_open(page.url).text, '<div></div>')
+
+
 class TestViewSaving(TestViewSavingCommon):
 
     def eq(self, a, b):