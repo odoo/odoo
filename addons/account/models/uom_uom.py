378a5b455467843d9732790afe7770aacc9009a6
Manav Shah <sham@odoo.com>
2025-07-04 18:19:26 +0530

    [IMP] l10n_tr_nilvera_edispatch: Add option to import E-Receipt
    This commit adds an option to import e-Receipt XML file that is
    exported from Nilvera Portal.
    It adds an "Upload e-Receipt (XML)" button in the list view of
    stock picking when the list view is for Receipts.
    It allows multiple file upload, once upload is complete, it opens
    the created Receipts list/form view accordingly.
    
    TaskID:4452521

--- addons/account/models/uom_uom.py
+++ addons/account/models/uom_uom.py
@@ -3,6 +3,35 @@
 
 from odoo import fields, models, api
 
+UOM_TO_UNECE_CODE = {
+    'uom.product_uom_unit': 'C62',
+    'uom.product_uom_dozen': 'DZN',
+    'uom.product_uom_kgm': 'KGM',
+    'uom.product_uom_gram': 'GRM',
+    'uom.product_uom_day': 'DAY',
+    'uom.product_uom_hour': 'HUR',
+    'uom.product_uom_ton': 'TNE',
+    'uom.product_uom_meter': 'MTR',
+    'uom.product_uom_km': 'KMT',
+    'uom.product_uom_cm': 'CMT',
+    'uom.product_uom_litre': 'LTR',
+    'uom.product_uom_lb': 'LBR',
+    'uom.product_uom_oz': 'ONZ',
+    'uom.product_uom_inch': 'INH',
+    'uom.product_uom_foot': 'FOT',
+    'uom.product_uom_mile': 'SMI',
+    'uom.product_uom_floz': 'OZA',
+    'uom.product_uom_qt': 'QT',
+    'uom.product_uom_gal': 'GLL',
+    'uom.product_uom_cubic_meter': 'MTQ',
+    'uom.product_uom_cubic_inch': 'INQ',
+    'uom.product_uom_cubic_foot': 'FTQ',
+    'uom.uom_square_meter': 'MTK',
+    'uom.uom_square_foot': 'FTK',
+    'uom.product_uom_yard': 'YRD',
+    'uom.product_uom_millimeter': 'MMT',
+}
+
 
 class UoM(models.Model):
     _inherit = "uom.uom"
@@ -17,34 +46,12 @@ class UoM(models.Model):
     def _get_unece_code(self):
         """ Returns the UNECE code used for international trading for corresponding to the UoM as per
         https://unece.org/fileadmin/DAM/cefact/recommendations/rec20/rec20_rev3_Annex2e.pdf"""
-        mapping = {
-            'uom.product_uom_unit': 'C62',
-            'uom.product_uom_dozen': 'DZN',
-            'uom.product_uom_kgm': 'KGM',
-            'uom.product_uom_gram': 'GRM',
-            'uom.product_uom_day': 'DAY',
-            'uom.product_uom_hour': 'HUR',
-            'uom.product_uom_ton': 'TNE',
-            'uom.product_uom_meter': 'MTR',
-            'uom.product_uom_km': 'KMT',
-            'uom.product_uom_cm': 'CMT',
-            'uom.product_uom_litre': 'LTR',
-            'uom.product_uom_lb': 'LBR',
-            'uom.product_uom_oz': 'ONZ',
-            'uom.product_uom_inch': 'INH',
-            'uom.product_uom_foot': 'FOT',
-            'uom.product_uom_mile': 'SMI',
-            'uom.product_uom_floz': 'OZA',
-            'uom.product_uom_qt': 'QT',
-            'uom.product_uom_gal': 'GLL',
-            'uom.product_uom_cubic_meter': 'MTQ',
-            'uom.product_uom_cubic_inch': 'INQ',
-            'uom.product_uom_cubic_foot': 'FTQ',
-            'uom.uom_square_meter': 'MTK',
-            'uom.uom_square_foot': 'FTK',
-            'uom.product_uom_yard': 'YRD',
-            'uom.product_uom_millimeter': 'MMT',
-        }
         xml_ids = self._get_external_ids().get(self.id, [])
-        matches = list(set(xml_ids) & set(mapping.keys()))
-        return matches and mapping[matches[0]] or 'C62'
+        matches = list(set(xml_ids) & set(UOM_TO_UNECE_CODE.keys()))
+        return matches and UOM_TO_UNECE_CODE[matches[0]] or 'C62'
+
+    @api.model
+    def _get_uom_from_unece_code(self, unece_code):
+        unece_code_to_uom = {v: k for k, v in UOM_TO_UNECE_CODE.items()}
+        uom_xmlid = unece_code_to_uom.get(unece_code, 'uom.product_uom_unit')
+        return self.env.ref(uom_xmlid, raise_if_not_found=False)
