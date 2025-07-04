f3d9600b257830e56e3cc282f3e8ec31b92a45b2
hmd-odoo <hmd@odoo.com>
2025-07-04 15:49:33 +0400

    [IMP] l10n_sa_edi_pos: Added Phase 2 QR code to POS Adds phase 2 QR code on POS receipts and shows it as a non-legal document if we are unable to verify the QR code on invoices and POS receipts.
    This change is done to accommodate for Legal Compliance of Phase 2 ZATCA (Saudi authority) E-Invoicing
    
    task-4646326

--- addons/l10n_sa_edi_pos/__manifest__.py
+++ addons/l10n_sa_edi_pos/__manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'Saudi Arabia - E-invoicing (Simplified)',
     'countries': ['sa'],
-    'version': '0.1',
+    'version': '0.2',
     'depends': [
         'l10n_sa_pos',
         'l10n_sa_edi',
@@ -18,7 +18,7 @@ E-invoice implementation for Saudi Arabia; Integration with ZATCA (POS)
     'license': 'LGPL-3',
     'assets': {
         'point_of_sale._assets_pos': [
-            'l10n_sa_edi_pos/static/src/overrides/**/*.js',
+            'l10n_sa_edi_pos/static/src/**/*',
         ],
     }
 }
