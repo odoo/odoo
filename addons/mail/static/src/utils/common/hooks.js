9b78fb5a444de9d1450d6425a8957e0368b91ad2
tsm-odoo <tsm@odoo.com>
2025-07-04 15:27:48 +0200

    [IMP] mail: add copy action on mobile
    Now that longpress is used to open the message actions menu, it's
    not possible to select text inside a message anymore. This commit
    adds a "copy" option that allows to copy the message text.tmp
    
    Related: odoo/enterprise#89402
    Related: odoo/enterprise#89584
    Part-of: odoo/odoo#217626
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/utils/common/hooks.js
+++ addons/mail/static/src/utils/common/hooks.js
@@ -531,7 +531,6 @@ export function useLongPress(refName, { action, predicate = () => true } = {}) {
         clearTimeout(timer);
         timer = null;
     }
-
     useLazyExternalListener(
         () => ref.el,
         "touchstart",
