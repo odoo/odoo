9db1ed20ccc5f6775cc6b80e8362e27ec42d1452
hmd-odoo <hmd@odoo.com>
2025-07-04 15:49:33 +0400

    [IMP] l10n_sa_edi_pos: Added Phase 2 QR code to POS Adds phase 2 QR code on POS receipts and shows it as a non-legal document if we are unable to verify the QR code on invoices and POS receipts.
    This change is done to accommodate for Legal Compliance of Phase 2 ZATCA (Saudi authority) E-Invoicing
    
    task-4646326

---
+++ addons/l10n_sa_edi_pos/static/src/overrides/models/pos_store.js
@@ -0,0 +1,20 @@
+import { PosStore } from "@point_of_sale/app/store/pos_store";
+import { qrCodeSrc } from "@point_of_sale/utils";
+import { patch } from "@web/core/utils/patch";
+
+patch(PosStore.prototype, {
+    getReceiptHeaderData(order) {
+        const result = super.getReceiptHeaderData(...arguments);
+        if (order && order.isSACompany) {
+            // is_settlement is assigned in super l10n_sa_pos
+            if (!result.is_settlement) {
+                const not_legal =
+                    !order.l10n_sa_invoice_qr_code_str ||
+                    order.l10n_sa_invoice_edi_state !== "sent";
+                result.not_legal = not_legal;
+                result.qr_code = not_legal ? "" : qrCodeSrc(order.l10n_sa_invoice_qr_code_str);
+            }
+        }
+        return result;
+    },
+});
