0f1a3e4d21ce4fb4a40564ebc57bce29ff54b49b
Meet Jivani [MEJI] <meji@odoo.com>
2025-07-04 12:45:42 +0530

    [IMP] point_of_sale: add sound for order receive
    In this commit:
    - Added new sound for the order receive for online order.
    
    task-4764948

--- addons/point_of_sale/static/src/overrides/sound_effects_service.js
+++ addons/point_of_sale/static/src/overrides/sound_effects_service.js
@@ -9,6 +9,9 @@ patch(SoundEffects.prototype, {
             bell: { path: "/point_of_sale/static/src/sounds/bell" },
             notification: { path: "/point_of_sale/static/src/sounds/notification" },
             beep: { path: "/point_of_sale/static/src/sounds/beep" },
+            "order-receive-tone": {
+                path: "/point_of_sale/static/src/sounds/order-receive-tone",
+            },
         };
         super.play(...arguments);
     },
