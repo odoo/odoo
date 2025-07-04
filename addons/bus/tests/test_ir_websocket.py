513db661539e658b6d8173f508284dfc2a6d8e93
Maryam Kia <maki@odoo.com>
2025-07-04 13:14:38 +0000

    [FIX] bus: all user's groups in building bus channels
    To target users of a group, bus notifications are sent on group records. To do
    so, user groups are added to its bus subscription. However, since
    odoo/odoo#179354, only explicit groups are added, not every implied group. It's
    incorrect. For example, sending on the user channel doesn't notify
    administrators while it should.
    
    Steps to reproduce (note that the steps are only working for admin):
    
    - Click the gear button on the sidebar in discuss page to navigate to the
    channel kanban view as admin
    - Click the `New` button and create a channel with an internal users group as
    `Auto Subscribe Groups`
    - Go back to the discuss main page. The new channel will not be pinned unless
    you reload the page
    
    See:
    https://github.com/odoo/odoo/pull/179354/files#r1954163704
    
    closes odoo/odoo#227008
    
    X-original-commit: e92c0b45ea557d491fe18ea8b85ab20719aeb2bd
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>
    Signed-off-by: Maryam Kia (maki) <maki@odoo.com>

--- addons/bus/tests/test_ir_websocket.py
+++ addons/bus/tests/test_ir_websocket.py
@@ -2,7 +2,9 @@
 import os
 import unittest
 
-from odoo.tests import tagged
+from unittest.mock import MagicMock, patch
+
+from odoo.tests import new_test_user, tagged
 from .common import WebsocketCase
 
 
@@ -14,3 +16,23 @@ class TestIrWebsocket(WebsocketCase):
             ws = self.websocket_connect()
             self.subscribe(ws, [("odoo", "discuss.channel", 5)], self.env["bus.bus"]._bus_last_id())
         self.assertIn("bus.Bus only string channels are allowed.", log.output[0])
+
+    def test_build_bus_channel_list(self):
+        test_user = new_test_user(
+            self.env, login="test_user", password="Password!1", groups="base.group_system"
+        )
+        mock_wsrequest = MagicMock()
+        mock_wsrequest.session.uid = test_user.id
+        with patch("odoo.addons.bus.models.ir_websocket.wsrequest", new=mock_wsrequest):
+            ir_websocket_model = self.env["ir.websocket"].with_user(test_user)
+            channels = set(ir_websocket_model._build_bus_channel_list(["test_channel"]))
+        expected_channels = {
+            "test_channel",
+            test_user.partner_id,
+            self.env.ref("base.group_system"),
+            self.env.ref("base.group_user"),
+        }
+        self.assertTrue(
+            expected_channels.issubset(channels),
+            f"The channels list is missing some expected values: {expected_channels - channels}.",
+        )
