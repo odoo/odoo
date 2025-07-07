dd7e08a00b26819585d4afabcb7effded2ad7b96
tsm-odoo <tsm@odoo.com>
2025-07-07 15:46:59 +0200

    [FIX] mail, *: avoid portal authentication conflict with live chat
    *: test_discuss_full, portal, portal_rating.
    
    Before this commit, the chat bot could stop when redirecting to a page
    where the portal is enabled.
    
    Both the portal and live chat modules rely on the discuss store’s `self`
    field which identifies the authenticated user. However, the portal also
    supports authentication via a token in the URL, which temporarily identifies
    the user on a specific thread.
    
    Previously, the portal would overwrite the global `self` value, causing
    inconsistencies for other users of the store such as messages appearing
    as if sent by another user.
    
    Since token authentication is specific to portal threads, it should only
    affect actions on the granted thread without altering the global user
    identity.
    
    This fix prevents the portal from overwriting the global `self` value and
    instead returns the thread-specific `portal_partner` field.
    
    opw-4722466
    
    closes odoo/odoo#216451
    
    X-original-commit: e54e8df19e7f609078798a49e3a82e11c5637d46
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/mail/static/src/core/common/composer.js
+++ addons/mail/static/src/core/common/composer.js
@@ -561,7 +561,7 @@ export class Composer extends Component {
             // Reset signature when recovering an empty body.
             composer.emailAddSignature = true;
         }
-        let signature = this.store.self.main_user_id?.signature;
+        let signature = this.thread.effectiveSelf.main_user_id?.signature;
         if (signature) {
             const divElement = document.createElement("div");
             divElement.setAttribute("data-o-mail-quote", "1");
