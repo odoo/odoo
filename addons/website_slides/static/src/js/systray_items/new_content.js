b160533345f7a8858d19304f44da976b122869f8
emge-odoo <emge@odoo.com>
2025-07-07 15:39:15 +0200

    [REF] website: replace new content modal with dropdown

--- addons/website_slides/static/src/js/systray_items/new_content.js
+++ addons/website_slides/static/src/js/systray_items/new_content.js
@@ -1,14 +1,19 @@
-import { NewContentModal } from '@website/client_actions/website_preview/new_content_modal';
-import { MODULE_STATUS } from "@website/client_actions/website_preview/new_content_element";
+import {
+    NewContentSystrayItem,
+    MODULE_STATUS,
+} from "@website/client_actions/website_preview/new_content_systray_item";
 import { patch } from "@web/core/utils/patch";
 
-patch(NewContentModal.prototype, {
+patch(NewContentSystrayItem.prototype, {
     setup() {
         super.setup();
 
-        const newSlidesChannelElement = this.state.newContentElements.find(element => element.moduleXmlId === 'base.module_website_slides');
-        newSlidesChannelElement.createNewContent = () => this.onAddContent('website_slides.slide_channel_action_add');
+        const newSlidesChannelElement = this.state.newContentElements.find(
+            (element) => element.moduleXmlId === "base.module_website_slides"
+        );
+        newSlidesChannelElement.createNewContent = () =>
+            this.onAddContent("website_slides.slide_channel_action_add");
         newSlidesChannelElement.status = MODULE_STATUS.INSTALLED;
-        newSlidesChannelElement.model = 'slide.channel';
+        newSlidesChannelElement.model = "slide.channel";
     },
 });
