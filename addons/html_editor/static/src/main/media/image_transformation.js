adfd460566e72f209c7a1ce459818ebb3deb17f9
Subhadeep R. Dev <sdev@odoo.com>
2025-07-07 08:00:32 +0000

    [FIX] html_builder, html_editor: fix image transform button active state
    In the Website Editor, the image transform button was not correctly
    reflecting the active state when an image had transformation styles
    applied.
    
    Expected behaviour:
    If the selected image has a transform style, the transform button should
    have the active class applied (highlighted in cyan).
    
    Cause:
    In the ImageTransformation component, any existing transformation on the
    image is reset during the setup phase, in order to compute new values.
    The transformation is then reapplied in the onMounted hook.
    This causes a timing issue: the TransformImageAction completes and
    triggers the "DOM_UPDATED" event before the transformation is reapplied.
    As a result, the active check for the button runs too early.
    
    Steps to reproduce:
    1. Enter edit mode and add an image.
    2. Click the image and apply a transformation.
    3. Click outside to deselect the image.
    4. Click the image again to reselect it.
    5. Click the image transform button.
    6. Observe: The button is not active (not highlighted in cyan), despite
    the image being transformed.
    
    Related to task-4367641
    
    X-original-commit: 008a850a693d873066c867021d5dd3814f05543a
    Part-of: odoo/odoo#219326
    Signed-off-by: Colin Louis (loco) <loco@odoo.com>

--- addons/html_editor/static/src/main/media/image_transformation.js
+++ addons/html_editor/static/src/main/media/image_transformation.js
@@ -38,6 +38,10 @@ export class ImageTransformation extends Component {
         image: { validate: (p) => p.tagName === "IMG" },
         destroy: { type: Function },
         onChange: { type: Function },
+        onComponentMounted: { type: Function, optional: true },
+    };
+    static defaultProps = {
+        onComponentMounted: () => {},
     };
 
     setup() {
@@ -50,6 +54,7 @@ export class ImageTransformation extends Component {
         this.computeImageTransformations();
         onMounted(() => {
             this.positionTransfoContainer();
+            this.props.onComponentMounted();
         });
         useExternalListener(window, "mousemove", this.mouseMove);
         useExternalListener(window, "mouseup", this.mouseUp);
