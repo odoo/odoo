5cf9a42ee689b00e45e0d6dac18a99a1c3358ee4
Dhwani Patel <dwa@odoo.com>
2025-07-07 13:00:04 +0000

    [FIX] pos_sale: fix UoM conversion for quantity and price from sale order
    Steps to reproduce:
    - Install pos_sale.
    - Create a sale order for a POS product with quantity 1 and UoM Dozens.
    - Confirm the order.
    - Go to Point of Sale → open a session.
    - Click Actions → Quotation/Order and select the recently created sale order.
    - Check the POS order line quantity.
    
    Issue:
    - The POS order line shows quantity as 1 unit, while the sale order line used
      1 Dozen, so it should be 12 units.
    
    Cause:
    - The POS order line was directly using the quantity and price from the
      sale_order_line without applying the proper UoM conversion.
    
    Solution:
    - Updated logic to set quantity and price_unit in the POS order line based on
      the converted sale order lines after UoM calculations.
    - Also updated value assigning of 'has_valued_move_ids' to converted line
      because quantity calculation is based on 'converted_line'.
    
    opw-4900767
    
    closes odoo/odoo#221954
    
    X-original-commit: 3e0a22bb7912396accc5263549aea789ce1e9d64
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: Dhwani Patel (dwa) <dwa@odoo.com>

--- addons/pos_repair/static/src/overrides/models/pos_order_line.js
+++ addons/pos_repair/static/src/overrides/models/pos_order_line.js
@@ -4,7 +4,7 @@ import { patch } from "@web/core/utils/patch";
 patch(PosOrderline.prototype, {
     //@override
     setQuantityFromSOL(saleOrderLine) {
-        if (saleOrderLine.is_repair_line) {
+        if (this.sale_order_line_id.is_repair_line) {
             this.setQuantity(saleOrderLine.product_uom_qty);
         } else {
             super.setQuantityFromSOL(...arguments);
