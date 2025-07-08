17bd35604345978472f25f83fe67824dc6a09b9b
Sébastien Theys <seb@odoo.com>
2025-07-08 14:13:42 +0200

    [REF] mail, *: clean up markup usage in emojis_mixin
    Use markup template and html functions to make the code cleaner and
    safer while no longer triggering ci/security flag.

--- addons/mail/static/src/js/emojis_mixin.js
+++ addons/mail/static/src/js/emojis_mixin.js
@@ -1,8 +1,9 @@
-import { htmlEscape } from "@odoo/owl";
+import { markup } from "@odoo/owl";
+
+import { htmlReplace, htmlReplaceAll } from "@web/core/utils/html";
 
 /**
  * Adds a span with a CSS class around chains of emojis in the message for styling purposes.
- * The input is first passed through 'escape' to prevent unwanted injections into the HTML
  *
  * Sequences of emojis are wrapped instead of individual ones to prevent compound emojis
  * such as 👩🏿 = 👩 + 🏿 [dark skin tone character] from being separated.
@@ -10,15 +11,15 @@ import { htmlEscape } from "@odoo/owl";
  * This will only match characters that have a different presentation from normal text, unlike ®
  * For alternatives, see: https://www.unicode.org/reports/tr51/#Emoji_Properties_and_Data_Files
  *
- * @param {String} message a text message to format
+ * @param {string|ReturnType<markup>} message a text message to format
+ * @returns {ReturnType<markup>}
  */
 export function formatText(message) {
-    message = htmlEscape(message);
-    message = message.replaceAll(
+    message = htmlReplaceAll(
+        message,
         /(\p{Emoji_Presentation}+)/gu,
-        "<span class='o_mail_emoji'>$1</span>"
+        (_, compoundEmoji) => markup`<span class='o_mail_emoji'>${compoundEmoji}</span>`
     );
-    message = message.replace(/(?:\r\n|\r|\n)/g, "<br>");
-
+    message = htmlReplace(message, /(?:\r\n|\r|\n)/g, () => markup`<br>`);
     return message;
 }
