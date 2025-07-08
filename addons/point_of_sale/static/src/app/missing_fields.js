fbde02409c3566bc5aafe011c42f5fd2ec9dc7fa
Arthur Nanson <artn@odoo.com>
2025-07-08 13:14:51 +0200

    [FIX] point_of_sale: display correct default pricelist when creating partner
    **Problem:**
    When creating a new partner from PoS, the suggested pricelist is not
    the default pricelist we defined in the configuration. It worked before
    18.0, but now the suggested pricelist is the first one that we
    defined in the configuration, not the one we set as default.
    
    **Steps to reproduce:**
    - In the configuration, enable flexible pricelists and fill some available pricelists.
    - Set a default pricelist that is not the first one you defined in the available section.
    - Open a session, click on the customer and click on create.
    - Go to the Sales and Purchase tab.
    - The pricelist is not the default one.
    
    **Why the fix:**
    If we are in PoS, we now put the default pricelist as a default for
    the new partner. We only do that if it is available, otherwise we
    fall back to the default flow.
    This is how it worked before 18.0.
    
    opw-4876573

--- addons/point_of_sale/static/src/app/missing_fields.js
+++ addons/point_of_sale/static/src/app/missing_fields.js
@@ -10,3 +10,9 @@ class DefaultField extends Component {
 }
 registry.category("fields").add("list.many2one_avatar_user", { component: DefaultField });
 registry.category("fields").add("list.list_activity", { component: DefaultField });
+registry.category("fields").add("x2many_buttons", { component: DefaultField });
+registry.category("fields").add("field_partner_autocomplete", { component: DefaultField });
+registry.category("fields").add("res_partner_many2one", { component: DefaultField });
+registry.category("fields").add("many2one_avatar_user", { component: DefaultField });
+registry.category("fields").add("auto_save_res_partner", { component: DefaultField });
+registry.category("fields").add("website_redirect_button", { component: DefaultField });
