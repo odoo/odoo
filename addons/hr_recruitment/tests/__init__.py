9227f068aee8f1c9c09243cfd9644d8bc033faca
karygauss03 <kaom@odoo.com>
2025-07-08 11:15:55 +0200

    [FIX] hr_recruitment: fix recruiter and interviewers domain when company is unset In the Job Position form, the 'Recruiter' and 'Interviewers' fields were empty when no company was selected. This was due to the static domain using 'company_id' directly without taking into consideration that company_id can be False. This fix introduces computed domain fields (, ) that dynamically adapt based on the selected company. If a company is set, users belonging to that company are shown. If not, only internal users are listed regardless their companies. Related task: 4926154.

--- addons/hr_recruitment/tests/__init__.py
+++ addons/hr_recruitment/tests/__init__.py
@@ -5,4 +5,5 @@ from . import test_recruitment_process
 from . import test_recruitment
 from . import test_utm
 from . import test_recruitment_interviewer
+from . import test_recruitment_allowed_user_ids
 from . import test_applicant
