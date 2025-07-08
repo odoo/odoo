ad6b73b47678c6e7b4c124c8cdfba2af4264e24c
Levi Siuzdak (sile) <sile@odoo.com>
2025-07-08 06:44:24 +0000

    [FIX] partnership: make partnership saleable
    Versions
    --------
    - saas-18.3+
    
    Steps
    -----
    1. Have a partnership product;
    2. publish it on eCommerce;
    3. as Portal or Public user, open /shop;
    4. if visible, buy it & go back to /shop.
    
    Issue
    -----
    Product is not available on /shop (anymore).
    
    Cause
    -----
    Partnership tracking on service products isn't made saleable.
    
    Solution
    --------
    Add `partnership` to `_get_saleable_tracking_types`.
    
    opw-4926679
    
    closes odoo/odoo#218522
    
    X-original-commit: d750dddf695120dd8b97b7f5ba6de83a9cd1d129
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>

--- addons/partnership/models/product_template.py
+++ addons/partnership/models/product_template.py
@@ -1,6 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
@@ -10,3 +10,7 @@ class ProductTemplate(models.Model):
         selection_add=[('partnership', 'Membership / Partnership')], ondelete={'partnership': 'set default'}
     )
     grade_id = fields.Many2one('res.partner.grade', string="Assigned Level")
+
+    @api.model
+    def _get_saleable_tracking_types(self):
+        return super()._get_saleable_tracking_types() + ['partnership']
