0635af99d3617511e82ebe20155296e86e87c92c
Arthur Nanson <artn@odoo.com>
2025-07-08 13:14:51 +0200

    [FIX] point_of_sale: display correct default pricelist when creating partner
    **Problem:**
    When creating a new partner from PoS, the suggested pricelist is not
    the default pricelist we defined in the configuration. It worked before
    18.0, but now the suggested pricelist is the first one that we
    defined in the configuration, not the one we set as default.
    
    **Steps to reproduce:**
    - In the configuration, enable flexible pricelists and fill some available pricelists.
    - Set a default pricelist that is not the first one you defined in the available section.
    - Open a session, click on the customer and click on create.
    - Go to the Sales and Purchase tab.
    - The pricelist is not the default one.
    
    **Why the fix:**
    If we are in PoS, we now put the default pricelist as a default for
    the new partner. We only do that if it is available, otherwise we
    fall back to the default flow.
    This is how it worked before 18.0.
    
    opw-4876573
    
    closes odoo/odoo#217913
    
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/point_of_sale/static/src/app/store/pos_store.js
+++ addons/point_of_sale/static/src/app/store/pos_store.js
@@ -1856,7 +1856,9 @@ export class PosStore extends Reactive {
         });
     }
     editPartnerContext(partner) {
-        return {};
+        return {
+            pos_config_id: this.config.id,
+        };
     }
     /**
      * @param {import("@point_of_sale/app/models/res_partner").ResPartner?} partner leave undefined to create a new partner
