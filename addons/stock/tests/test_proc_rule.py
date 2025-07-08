odoo/odoo

commit ea480703b64d88ac572c3e37da3d9fb3327b4445

[PERF] stock: batch orderpoints compute method
The method orderpoint._compute_qty_order_computed was batched in commit
17a2de9 then reverted in commit a58b995 because of a bug introduced when
purchase_stock is installed.

It should still be beneficial performance-wise to batch the calls
to orderpoint._quantity_in_progress so this commit reintroduces
the batching. The part that led to the issue was the override
of _quantity_in_progress in the purchase_stock module.

In this commit we fix the _quantity_in_progress method.
Thanks to that, the test introduced in 606dc71 is passing
even with the batched version of _compute_qty_to_order_computed.
We also fix `_get_lines_domain` in purchase_stock that was not properly
using the rfq_domain variable. The current returns all the pols in the
database in case the method is called with no locations/warehouses.

Co-authored-by: pivi-odoo <pivi@odoo.com>
Co-authored-by: mohamedkasem99 <kasm@odoo.com>

addons/stock/models/stock_orderpoint.py
@@ -339,28 +339,34 @@ class StockWarehouseOrderpoint(models.Model):
 
     @api.depends('qty_multiple', 'qty_forecast', 'product_min_qty', 'product_max_qty', 'visibility_days')
     def _compute_qty_to_order_computed(self):
-        for orderpoint in self:
-            if not orderpoint.product_id or not orderpoint.location_id:
-                orderpoint.qty_to_order_computed = False
-                continue
-            orderpoint.qty_to_order_computed = orderpoint._get_qty_to_order(qty_in_progress_by_orderpoint=orderpoint._quantity_in_progress())
+        def to_compute(orderpoint):
+            rounding = orderpoint.product_uom.rounding
+            # The check is on purpose. We only want to consider the visibility days if the forecast is negative and
+            # there is a already something to ressuply base on lead times.
+            return float_compare(orderpoint.qty_forecast, orderpoint.product_min_qty, precision_rounding=rounding) < 0
 
-    def _get_qty_to_order(self, force_visibility_days=False, qty_in_progress_by_orderpoint={}):
+        orderpoints = self.filtered(to_compute)
+        qty_in_progress_by_orderpoint = orderpoints._quantity_in_progress()
+        for orderpoint in orderpoints:
+            orderpoint.qty_to_order_computed = orderpoint._get_qty_to_order(qty_in_progress_by_orderpoint=qty_in_progress_by_orderpoint)
+        (self - orderpoints).qty_to_order_computed = False
+
+    def _get_qty_to_order(self, force_visibility_days=False, qty_in_progress_by_orderpoint=None):
         self.ensure_one()
-        if not self.product_id or not self.location_id:
-            return False
         visibility_days = self.visibility_days
         if force_visibility_days is not False:
             # Accepts falsy values such as 0.
             visibility_days = force_visibility_days
         qty_to_order = 0.0
+        qty_in_progress_by_orderpoint = qty_in_progress_by_orderpoint or {}
+        qty_in_progress = qty_in_progress_by_orderpoint.get(self.id)
+        if qty_in_progress is None:
+            qty_in_progress = self._quantity_in_progress()[self.id]
         rounding = self.product_uom.rounding
         # The check is on purpose. We only want to consider the visibility days if the forecast is negative and
         # there is a already something to ressuply base on lead times.
         if float_compare(self.qty_forecast, self.product_min_qty, precision_rounding=rounding) < 0:
-            # We want to know how much we should order to also satisfy the needs that gonna appear in the next (visibility) days
             product_context = self._get_product_context(visibility_days=visibility_days)
-            qty_in_progress = qty_in_progress_by_orderpoint.get(self.id) or self._quantity_in_progress()[self.id]
             qty_forecast_with_visibility = self.product_id.with_context(product_context).read(['virtual_available'])[0]['virtual_available'] + qty_in_progress
             qty_to_order = max(self.product_min_qty, self.product_max_qty) - qty_forecast_with_visibility
             remainder = (self.qty_multiple > 0.0 and qty_to_order % self.qty_multiple) or 0.0

addons/purchase_stock/models/product.py
@@ -69,26 +69,33 @@ class ProductProduct(models.Model):
             ('product_id', 'in', self.ids)
         ]
         if location_ids:
-            domains.append(expression.AND([rfq_domain, [
+            domains.append([
                 '|',
-                '|',
-                    ('order_id.picking_type_id.default_location_dest_id', 'in', location_ids),
                     '&',
-                        ('move_ids', '=', False),
-                        ('location_final_id', 'child_of', location_ids),
+                    ('orderpoint_id', '=', False),
+                    '|',
+                        '&',
+                            ('location_final_id', '=', False),
+                            ('order_id.picking_type_id.default_location_dest_id', 'in', location_ids),
+                        '&',
+                            ('move_ids', '=', False),
+                            ('location_final_id', 'child_of', location_ids),
                     '&',
                         ('move_dest_ids', '=', False),
                         ('orderpoint_id.location_id', 'in', location_ids)
-            ]]))
+            ])
         if warehouse_ids:
-            domains.append(expression.AND([rfq_domain, [
+            domains.append([
                 '|',
-                    ('order_id.picking_type_id.warehouse_id', 'in', warehouse_ids),
+                    '&',
+                        ('orderpoint_id', '=', False),
+                        ('order_id.picking_type_id.warehouse_id', 'in', warehouse_ids),
                     '&',
                         ('move_dest_ids', '=', False),
                         ('orderpoint_id.warehouse_id', 'in', warehouse_ids)
-            ]]))
-        return expression.OR(domains) if domains else []
+            ])
+        domains = expression.OR(domains) if domains else []
+        return expression.AND([rfq_domain, domains])
 
 
 class SupplierInfo(models.Model):

commit 6218559abb95e437057956bc9b32d0f86f5ef405

[FIX] stock: avoid traceback when creating replenishment
bug introduced in: https://github.com/odoo/odoo/pull/213154/commits/ea480703b64d88ac572c3e37da3d9fb3327b4445

Steps to reproduce the bug:
- Go to "Inventory" → "Operations" menu → "Replenishment"
- Click "New" to create a new replenishment rule

Problem:
Traceback is triggered:
```
in _float_check_precision assert precision_rounding > 0,\
^^^^^^^^^^^^^^^^^^^^^^ AssertionError: precision_rounding must be
positive, got 0.0
```

As the product is not set, the `product_uom` is not set either,
which leads to a `product_uom.rounding` of 0.0.

Opw-4925719
Opw-4928957
Opw-4926504
Opw-4925919
Opw-4928684
Opw-4925080
Opw-4928788
Opw-4926719
Opw-4927440
opw-4928540

addons/stock/models/stock_orderpoint.py
@@ -343,7 +343,11 @@ class StockWarehouseOrderpoint(models.Model):
             rounding = orderpoint.product_uom.rounding
             # The check is on purpose. We only want to consider the visibility days if the forecast is negative and
             # there is a already something to ressuply base on lead times.
-            return float_compare(orderpoint.qty_forecast, orderpoint.product_min_qty, precision_rounding=rounding) < 0
+            return (
+                orderpoint.product_id
+                and orderpoint.location_id
+                and float_compare(orderpoint.qty_forecast, orderpoint.product_min_qty, precision_rounding=rounding) < 0
+            )
 
         orderpoints = self.filtered(to_compute)
         qty_in_progress_by_orderpoint = orderpoints._quantity_in_progress()

addons/stock/tests/test_proc_rule.py
@@ -672,8 +672,15 @@ class TestProcRule(TransactionCase):
             mto_rule.rule_message,
             'The help message should correctly display information for MTSO.'
         )
-        
-        
+
+    def test_replenishment_creation(self):
+        """Test that the default replenishment order values
+        are computed correctly in the tree view."""
+        orderpoint_list_view = Form(self.env['stock.warehouse.orderpoint'], view='stock.view_warehouse_orderpoint_tree_editable')
+        self.assertEqual(orderpoint_list_view.qty_to_order, 0)
+        self.assertFalse(orderpoint_list_view.product_id)
+
+
 class TestProcRuleLoad(TransactionCase):
     def setUp(cls):
         super(TestProcRuleLoad, cls).setUp()