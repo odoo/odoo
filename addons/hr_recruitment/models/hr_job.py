89554d11086a10c70ca4d7f7e5246e480e324308
karygauss03 <kaom@odoo.com>
2025-07-08 09:15:55 +0000

    [FIX] hr_recruitment: fix recruiter and interviewers domain when company is unset
    In the Job Position form, the 'Recruiter' and 'Interviewers' fields were empty when no company was selected. This was due to the static domain using 'company_id' directly without taking into consideration that company_id can be False.
    This fix introduces computed domain fields (, ) that dynamically adapt based on the selected company. If a company is set, users belonging to that company are shown. If not, only internal users are listed regardless their companies.
    Related task: 4926154.
    
    X-original-commit: 0f981b14ea22cc154bcd93f53dc19f46c37ecb13

--- addons/hr_recruitment/models/hr_job.py
+++ addons/hr_recruitment/models/hr_job.py
@@ -55,7 +55,12 @@ class HrJob(models.Model):
     color = fields.Integer("Color Index")
     is_favorite = fields.Boolean(compute='_compute_is_favorite', inverse='_inverse_is_favorite')
     favorite_user_ids = fields.Many2many('res.users', 'job_favorite_user_rel', 'job_id', 'user_id', default=_get_default_favorite_user_ids)
-    interviewer_ids = fields.Many2many('res.users', string='Interviewers', domain="[('share', '=', False), ('company_ids', 'in', company_id)]", tracking=True, help="The Interviewers set on the job position can see all Applicants in it. They have access to the information, the attachments, the meeting management and they can refuse him. You don't need to have Recruitment rights to be set as an interviewer.")
+    interviewer_ids = fields.Many2many(
+        "res.users",
+        domain="[('id', 'in', allowed_user_ids)]",
+        string="Interviewers",
+        help="The Interviewers set on the job position can see all Applicants in it. They have access to the information, the attachments, the meeting management and they can refuse him. You don't need to have Recruitment rights to be set as an interviewer.",
+    )
     extended_interviewer_ids = fields.Many2many('res.users', 'hr_job_extended_interviewer_res_users', compute='_compute_extended_interviewer_ids', store=True)
     industry_id = fields.Many2one('res.partner.industry', 'Industry', tracking=True)
     expected_degree = fields.Many2one("hr.recruitment.degree")
