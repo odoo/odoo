35521955017018b1cb29e396aad8898ee1a73ff4
Nisarg (nipl) <nipl@odoo.com>
2025-07-08 18:15:41 +0530

    [IMP] website_sale: Remove `Magnifier on hover` zoom option from eCommerce
    Before this commit:
    - The product image zoom behavior in eCommerce was controlled by a selection
      field with options: `None`, `Magnifier on hover`, and `Zoom on click`
      and `Both`.
    - `Magnifier on hover` does not work on mobile and provides limited added value.
    
    After this commit:
    - Remove the `Magnifier on hover` option and related logic.
    - Replace the zoom mode selector with a boolean `Zoom on click` toggle.
    - During upgrade, activate `Zoom on click` for all configurations except those
      set to `None`.
    
    affected version - master
    task - 4712502

--- addons/website_sale/const.py
+++ addons/website_sale/const.py
@@ -153,8 +153,6 @@ PRODUCT_PAGE_STYLE_MAPPING = {
                 'website_sale.product_picture_magnify_click',
             ],
             'disable': [
-                'website_sale.product_picture_magnify_hover',
-                'website_sale.product_picture_magnify_both',
                 'website_sale_comparison.product_add_to_compare',  # Comparison
                 'website_sale.product_terms_and_conditions',  # Terms and Conditions
             ],
@@ -191,8 +189,6 @@ PRODUCT_PAGE_STYLE_MAPPING = {
                 'website_sale.product_picture_magnify_click',
             ],
             'disable': [
-                'website_sale.product_picture_magnify_hover',
-                'website_sale.product_picture_magnify_both',
                 'website_sale_comparison.product_add_to_compare',  # Comparison
                 'website_sale.product_terms_and_conditions',  # Terms and Conditions
             ],
