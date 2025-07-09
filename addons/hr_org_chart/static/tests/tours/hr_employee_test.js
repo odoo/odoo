807e02ac6266e0616d959f605991cd6c7efa8448
Laetitia (ldau) <ldau@odoo.com>
2025-07-09 13:15:37 +0200

    [IMP] hr: ameliorate department chart design
    This commit adjusts the design to better align with the employee
    organization chart, ensuring a more consistent and coherent visual
    representation.
    
    task-4865792
    
    closes odoo/odoo#214392
    
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- addons/hr_org_chart/static/tests/tours/hr_employee_test.js
+++ addons/hr_org_chart/static/tests/tours/hr_employee_test.js
@@ -4,7 +4,7 @@ registry.category("web_tour.tours").add("indirect_subordinates_tour", {
     steps: () => [
         {
             content: "Click the number next to employee georges",
-            trigger: "div[name='child_ids'] .o_org_chart_entry button > a:contains('2')",
+            trigger: "div[name='child_ids'] .o_org_chart_entry button > .badge:contains('2')",
             run: "click",
         },
         {
