e7ae55e54a1054959a934b5f0ad97b956b60d653
David Monjoie <dmo@odoo.com>
2025-07-09 08:29:33 +0000

    [FIX] html_editor: fix link test failing non-deterministically
    The fix at [1] did not fix the issue. This is another attempt.
    
    runbot-227759
    
    [1]: https://github.com/odoo/odoo/pull/215168/commits/9a3c9ecca14fb8701e4f934cf5285297cbe26310
    
    closes odoo/odoo#219079
    
    X-original-commit: 2951858fb8daa7f12d3bc87d2ef22ce942bd45b1
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/html_editor/static/tests/link/popover.test.js
+++ addons/html_editor/static/tests/link/popover.test.js
@@ -1445,11 +1445,13 @@ describe("apply button should be disabled when the URL is empty", () => {
 
 describe("hidden label field", () => {
     test("label field should be hidden if <a> content is not text only", async () => {
-        await setupEditor(`<a href="http://test.com/"><img src="${base64Img}">te[]xt</a>`);
+        await setupEditor(
+            `<p><a href="http://test.com/"><img src="${base64Img}">te[]xt</a></p>`
+        );
         await expectElementCount(".o-we-linkpopover", 1);
         // open edit mode and check if label input is hidden
         await click(".o_we_edit_link");
-        await waitFor(".input-group", { timeout: 1500 });
+        await waitFor(".o_we_href_input_link", { timeout: 1500 });
         expect(".o_we_label_link").not.toBeVisible();
         expect(".o_we_href_input_link").toHaveValue("http://test.com/");
     });
