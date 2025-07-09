odoo/odoo

commit 383c3a4d5a8eb60998c4979ad661bce32166d74c

[IMP] pos*: use Domain
Part-of: odoo/odoo#206894
Related: odoo/enterprise#83834
Signed-off-by: Chong Wang (cwg) <cwg@odoo.com>

addons/l10n_fr_pos_cert/models/account_closing.py
@@ -1,13 +1,11 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from datetime import datetime, timedelta
 
 from odoo import models, api, fields
-from odoo.fields import Datetime as FieldDateTime
+from odoo.fields import Datetime as FieldDateTime, Domain
 from dateutil.relativedelta import relativedelta
 from odoo.tools.translate import _
 from odoo.exceptions import UserError
-from odoo.osv.expression import AND
 
 
 class AccountSaleClosing(models.Model):
@@ -88,12 +86,12 @@ class AccountSaleClosing(models.Model):
             date_start = previous_closing.create_date
             cumulative_total += previous_closing.cumulative_total
 
-        domain = [('company_id', '=', company.id), ('state', 'in', ('paid', 'done'))]
+        domain = Domain('company_id', '=', company.id) | Domain('state', 'in', ('paid', 'done'))
         if first_order.l10n_fr_secure_sequence_number is not False and first_order.l10n_fr_secure_sequence_number is not None:
-            domain = AND([domain, [('l10n_fr_secure_sequence_number', '>', first_order.l10n_fr_secure_sequence_number)]])
+            domain &= Domain('l10n_fr_secure_sequence_number', '>', first_order.l10n_fr_secure_sequence_number)
         elif date_start:
-            #the first time we compute the closing, we consider only from the installation of the module
-            domain = AND([domain, [('date_order', '>=', date_start)]])
+            # the first time we compute the closing, we consider only from the installation of the module
+            domain &= Domain('date_order', '>=', date_start)
 
         orders = self.env['pos.order'].search(domain, order='date_order desc')
 

addons/l10n_in_pos/models/pos_bill.py
@@ -1,6 +1,6 @@
 from odoo import api, models
-from odoo.osv import expression
-from odoo.osv.expression import AND
+from odoo.fields import Domain
+
 
 class PosBill(models.Model):
     _inherit = "pos.bill"
@@ -10,6 +10,6 @@ class PosBill(models.Model):
         domain = super()._load_pos_data_domain(data)
 
         if self.env.company.country_code == 'IN':
-            domain = expression.AND([domain, [('value', '>=', 1.0), ('value', '<=', 2000.0)]])
+            domain = Domain.AND([domain, [('value', '>=', 1.0), ('value', '<=', 2000.0)]])
 
         return domain

addons/pos_hr/models/pos_config.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
-from odoo.osv.expression import AND
+from odoo.fields import Domain
 
 
 class PosConfig(models.Model):
@@ -54,7 +52,7 @@ class PosConfig(models.Model):
     def _employee_domain(self, user_id):
         domain = self._check_company_domain(self.company_id)
         if len(self.basic_employee_ids) > 0:
-            domain = AND([
+            domain = Domain.AND([
                 domain,
                 ['|', ('user_id', '=', user_id), ('id', 'in', self.basic_employee_ids.ids + self.advanced_employee_ids.ids + self.minimal_employee_ids.ids)]
             ])

addons/pos_hr/models/single_employee_sales_report.py
@@ -1,6 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from odoo import api, models
-from odoo.osv.expression import AND
+from odoo.fields import Domain
 
 
 class ReportPos_HrSingle_Employee_Sales_Report(models.AbstractModel):
@@ -12,7 +12,7 @@ class ReportPos_HrSingle_Employee_Sales_Report(models.AbstractModel):
         domain = super()._get_domain(config_ids=config_ids, session_ids=session_ids)
 
         if (employee_id):
-            domain = AND([domain, [('employee_id', '=', employee_id)]])
+            domain = Domain.AND([domain, [('employee_id', '=', employee_id)]])
 
         return domain
 

addons/pos_online_payment/tests/test_frontend.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 import uuid
 from unittest.mock import patch
 
-from odoo import Command, fields
+from odoo import fields
+from odoo.fields import Command
 from odoo.tools import mute_logger
 from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 from odoo.addons.point_of_sale.tests.test_frontend import TestPointOfSaleHttpCommon
 from odoo.addons.mail.tests.common import mail_new_test_user
 from odoo.addons.pos_online_payment.tests.online_payment_common import OnlinePaymentCommon
 from odoo.addons.account.models.account_payment_method import AccountPaymentMethod
-from odoo.osv.expression import AND
 from odoo.addons.point_of_sale.tests.common import archive_products
 
 import odoo.tests

addons/pos_online_payment_self_order/models/pos_order.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import models, fields, api, tools
-from odoo.osv import expression
 
 
 class PosOrder(models.Model):
@@ -44,7 +42,7 @@ class PosOrder(models.Model):
 
         can_change_self_order_domain = [('state', '=', 'draft')]
         if vals['use_self_order_online_payment']:
-            can_change_self_order_domain = expression.AND([can_change_self_order_domain, [('config_id.self_order_online_payment_method_id', '!=', False)]])
+            can_change_self_order_domain += [('config_id.self_order_online_payment_method_id', '!=', False)]
 
         can_change_self_order_orders = self.filtered_domain(can_change_self_order_domain)
         cannot_change_self_order_orders = self - can_change_self_order_orders

addons/pos_online_payment_self_order/models/pos_payment_method.py
@@ -1,5 +1,5 @@
 from odoo import models, api
-from odoo.osv import expression
+from odoo.fields import Domain
 
 
 class PosPaymentMethod(models.Model):
@@ -9,7 +9,7 @@ class PosPaymentMethod(models.Model):
     def _load_pos_self_data_domain(self, data):
         if data['pos.config'][0]['self_ordering_mode'] == 'kiosk':
             domain = super()._load_pos_self_data_domain(data)
-            domain = expression.OR([[('is_online_payment', '=', True)], domain])
+            domain = Domain.OR([[('is_online_payment', '=', True)], domain])
             return domain
         else:
             return [('is_online_payment', '=', True)]

addons/pos_self_order/controllers/orders.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
-from odoo import http, fields, _
+from odoo import http, fields
+from odoo.fields import Domain
 from odoo.http import request
 from odoo.tools import float_round
-from odoo.osv import expression
 from werkzeug.exceptions import NotFound, BadRequest, Unauthorized
 from odoo.exceptions import MissingError
 from odoo.tools import consteq
+
 
 class PosSelfOrderController(http.Controller):
     @http.route("/pos-self-order/process-order/<device_type>/", auth="public", type="jsonrpc", website=True)
@@ -154,10 +154,10 @@ class PosSelfOrderController(http.Controller):
         pos_order = pos_config.env['pos.order'].browse(order_id)
 
         if not pos_order.exists() or not consteq(pos_order.access_token, order_access_token):
-            raise MissingError(_("Your order does not exist or has been removed"))
+            raise MissingError(self.env._("Your order does not exist or has been removed"))
 
         if pos_order.state != 'draft':
-            raise Unauthorized(_("You are not authorized to remove this order"))
+            raise Unauthorized(self.env._("You are not authorized to remove this order"))
 
         pos_order.remove_from_ui([pos_order.id])
 
@@ -166,22 +166,19 @@ class PosSelfOrderController(http.Controller):
         pos_config = self._verify_pos_config(access_token)
         session = pos_config.current_session_id
         table = pos_config.env["restaurant.table"].search([('identifier', '=', table_identifier)], limit=1)
-        domain = False
 
         if not table_identifier:
-            domain = [(False, '=', True)]
+            domain = Domain.FALSE
         else:
-            domain = ['&', '&',
+            domain = Domain([
                 ('table_id', '=', table.id),
                 ('state', '=', 'draft'),
                 ('access_token', 'not in', [data.get('access_token') for data in order_access_tokens])
-            ]
+            ])
 
         for data in order_access_tokens:
-            domain = expression.OR([domain, ['&',
-                ('access_token', '=', data.get('access_token')),
-                ('write_date', '>', data.get('write_date'))
-            ]])
+            domain |= Domain('access_token', '=', data.get('access_token')) \
+                & Domain('write_date', '>', data.get('write_date'))
 
         orders = session.order_ids.filtered_domain(domain)
         if not orders:

commit 62d5278543d95e3d8f2830648ec36758b68d1dc6

[FIX] l10n_fr_pos_cert: & in domain
Fixing wrong operator introduced in
383c3a4d5a8eb60998c4979ad661bce32166d74c.

closes odoo/odoo#217860

Signed-off-by: Chong Wang (cwg) <cwg@odoo.com>

addons/l10n_fr_pos_cert/models/account_closing.py
@@ -86,7 +86,7 @@ class AccountSaleClosing(models.Model):
             date_start = previous_closing.create_date
             cumulative_total += previous_closing.cumulative_total
 
-        domain = Domain('company_id', '=', company.id) | Domain('state', 'in', ('paid', 'done'))
+        domain = Domain('company_id', '=', company.id) & Domain('state', 'in', ('paid', 'done'))
         if first_order.l10n_fr_secure_sequence_number is not False and first_order.l10n_fr_secure_sequence_number is not None:
             domain &= Domain('l10n_fr_secure_sequence_number', '>', first_order.l10n_fr_secure_sequence_number)
         elif date_start: