6fba1a2253dbe68176d784004d68fe6e7fe4892c
Paolo Gatti <pgi@odoo.com>
2025-07-09 14:16:54 +0200

    [REF] base: `shell` command: console function style

--- odoo/cli/shell.py
+++ odoo/cli/shell.py
@@ -66,29 +66,31 @@ class Shell(Command):
         if not os.isatty(sys.stdin.fileno()):
             local_vars['__name__'] = '__main__'
             exec(sys.stdin.read(), local_vars)
+            return
+
+        if 'env' not in local_vars:
+            _logger.warning('No environment set, use `%s shell -d dbname` to get one.', sys.argv[0])
+        for key, val in sorted(local_vars.items()):
+            print(f'{key}: {val}')  # noqa: T201
+
+        pythonstartup = config.options.get('shell_file') or os.environ.get('PYTHONSTARTUP')
+
+        preferred_interface = config.options.get('shell_interface')
+        if preferred_interface:
+            shells_to_try = [preferred_interface, 'python']
         else:
-            if 'env' not in local_vars:
-                print('No environment set, use `%s shell -d dbname` to get one.' % sys.argv[0])
-            for i in sorted(local_vars):
-                print('%s: %s' % (i, local_vars[i]))
+            shells_to_try = self.supported_shells
 
-            pythonstartup = config.options.get('shell_file') or os.environ.get('PYTHONSTARTUP')
-
-            preferred_interface = config.options.get('shell_interface')
-            if preferred_interface:
-                shells_to_try = [preferred_interface, 'python']
-            else:
-                shells_to_try = self.supported_shells
-
-            for shell in shells_to_try:
-                try:
-                    shell_func = getattr(self, shell)
-                    return shell_func(local_vars, pythonstartup)
-                except ImportError:
-                    pass
-                except Exception:
-                    _logger.warning("Could not start '%s' shell.", shell)
-                    _logger.debug("Shell error:", exc_info=True)
+        for shell in shells_to_try:
+            try:
+                shell_func = getattr(self, shell)
+                shell_func(local_vars, pythonstartup)
+                break
+            except ImportError:
+                pass
+            except Exception:  # noqa: BLE001
+                _logger.warning("Could not start '%s' shell.", shell)
+                _logger.debug("Shell error:", exc_info=True)
 
     def ipython(self, local_vars, pythonstartup=None):
         from IPython import start_ipython  # noqa: PLC0415
