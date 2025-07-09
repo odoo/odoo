a18e2d98cf0d30340760c0c1cadb467fe10a260f
Manav Shah <sham@odoo.com>
2025-07-09 11:53:54 +0530

    [FIX] l10n_tr_nilvera_edispatch: Update delivery customer in XML & modify ZIP usage
    Behaviour before this commit:
    - Previously, the picking partner was used for sending both the Delivery Customer
    and delivery address in the XML.
    - Secondly, ZIP was used in Delivery Customer regardless of the country of the
    partner (Turkish or Non-Turkish)
    
    Desired behaviour after this commit:
    - After this commit, the commercial partner of the picking partner will be used
    for sending the Delivery Customer data and picking partner itself will be used
    for sending the Delivery Address in the XML.
    - Additionally, if the commercial partner is non turkish, customs ZIP will be
    used in the Delivery Customer in the XML.
    - If the delivery is validated without giving Delivery Address, then the
    'Generate e-Dispatch (XML)' button will be hidden.
    
    TaskID:4918748

--- addons/l10n_tr_nilvera_edispatch/models/res_partner.py
+++ addons/l10n_tr_nilvera_edispatch/models/res_partner.py
@@ -27,7 +27,7 @@ class ResPartner(models.Model):
             if country_code == 'TR' and not record.vat:
                 missing_fields.append(_("TCKN/VKN"))
 
-            if (country_code == 'TR' or is_delivery_partner) and not record.zip:
+            if country_code == 'TR' and not record.zip:
                 missing_fields.append(_("ZIP"))
 
             if missing_fields:
@@ -40,9 +40,11 @@ class ResPartner(models.Model):
                 msg.append(_("Customs ZIP of 5 characters must be present"))
 
             if msg:
-                error_messages[f"invalid_{record.name.replace(' ', '_')}"] = {
-                    'message': _("%s's %s.", record.name, ', '.join(msg)),
-                    'action_text': _("View %s", record.name),
+                # Instead of using name, display_name is used, since name is not required
+                # if contact is of type "Delivery Address".
+                error_messages[f"invalid_partner_{record.id}"] = {
+                    'message': _("%(name)s's %(errors)s.", name=record.display_name, errors=', '.join(msg)),
+                    'action_text': _("View %s", record.display_name),
                     'action': record._get_records_action(name=_("View Partner"))
                 }
         return error_messages
