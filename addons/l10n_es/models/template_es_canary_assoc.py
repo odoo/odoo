d664c2dc1952a1e05c0f53bf51fe5d8bb723cde0
Antoine Boonen <aboo@odoo.com>
2025-07-10 10:46:57 +0200

    [IMP] l10n_es: Add asset models to the spanish template
    - Add the asset models to the spanish template
    
    task-4919794

--- addons/l10n_es/models/template_es_canary_assoc.py
+++ addons/l10n_es/models/template_es_canary_assoc.py
@@ -34,3 +34,7 @@ class AccountChartTemplate(models.AbstractModel):
                 del data['tax_ids']
 
         return res
+
+    @template('es_canary_assoc', 'account.asset')
+    def _get_es_canary_assoc_account_asset(self):
+        return self._parse_csv('es_assec', 'account.asset', module='l10n_es')
--- addons/l10n_es/models/template_es_canary_pymes.py
+++ addons/l10n_es/models/template_es_canary_pymes.py
@@ -34,3 +34,7 @@ class AccountChartTemplate(models.AbstractModel):
                 del data['tax_ids']
 
         return res
+
+    @template('es_canary_pymes', 'account.asset')
+    def _get_es_canary_pymes_account_asset(self):
+        return self._parse_csv('es_pymes', 'account.asset', module='l10n_es')
