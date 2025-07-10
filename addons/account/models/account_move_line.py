fca19ed4d0b22e08d236686c61434e5ccba25469
soza-odoo <soza@odoo.com>
2025-07-10 09:46:06 +0000

    [IMP] account: add default tax of account in bank statement line
    In this commit:
    - When setting the account from the bank reconciliation widget
     (using 'set_account'), the default tax set on that account will
     now be automatically applied to the bank statement line.
    
    task-4930945
    
    closes odoo/odoo#221163
    
    X-original-commit: 7219c889716d45855733863958fa2edffa7fd3e7
    Related: odoo/enterprise#91323
    Signed-off-by: Maximilien La Barre (malb) <malb@odoo.com>
    Signed-off-by: Soham Zadafiya (soza) <soza@odoo.com>

--- addons/account/models/account_move_line.py
+++ addons/account/models/account_move_line.py
@@ -929,6 +929,9 @@ class AccountMoveLine(models.Model):
                 filtered_supplier_taxes_id = self.product_id.supplier_taxes_id.filtered_domain(company_domain)
                 tax_ids = filtered_supplier_taxes_id or self.account_id.tax_ids.filtered(lambda tax: tax.type_tax_use == 'purchase')
 
+            elif self.env.context.get('account_default_taxes'):
+                tax_ids = self.account_id.tax_ids
+
             else:
                 tax_ids = False if self.env.context.get('skip_computed_taxes') or self.move_id.is_entry() else self.account_id.tax_ids
 
