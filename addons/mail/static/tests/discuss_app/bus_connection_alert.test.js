9adc160a5984b2fb6b0f55d3d96455b4dfc4f9e5
tsm-odoo <tsm@odoo.com>
2025-07-10 14:22:03 +0000

    [FIX] mail: connection alert test
    Before this commit, the `show warning when bus connection encounters
    issues` test was sometime failing. [1] attempted to fix this issue and
    seems to have greatly reduced its frequence. However, the bus monitoring
    service still listens to the "offline" event. As a result, the test can
    still fails according to the runbot network condition.
    
    This commit fixes the issue by preventing the "online"/"offline" events
    during this test.
    
    fixes runbot-226443
    
    [1]: https://github.com/odoo/odoo/pull/216085
    
    closes odoo/odoo#218789
    
    X-original-commit: 1e00feca9b88744788a70751a6e0c1e9e52be903
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/mail/static/tests/discuss_app/bus_connection_alert.test.js
+++ addons/mail/static/tests/discuss_app/bus_connection_alert.test.js
@@ -3,12 +3,26 @@ import { WEBSOCKET_CLOSE_CODES } from "@bus/workers/websocket_worker";
 import { defineMailModels, openDiscuss, start } from "@mail/../tests/mail_test_helpers";
 import { describe, expect, test } from "@odoo/hoot";
 import { animationFrame, runAllTimers, waitFor, waitForNone } from "@odoo/hoot-dom";
+
+import { browser } from "@web/core/browser/browser";
 import { asyncStep, MockServer, waitForSteps } from "@web/../tests/web_test_helpers";
 
 defineMailModels();
 describe.current.tags("desktop");
 
 test("show warning when bus connection encounters issues", async () => {
+    // The bus service listens to online/offline events. Prevent them to make the
+    // test deterministic.
+    for (const event of ["online", "offline"]) {
+        browser.addEventListener(
+            event,
+            (ev) => {
+                ev.preventDefault();
+                ev.stopImmediatePropagation();
+            },
+            { capture: true }
+        );
+    }
     addBusServiceListeners(
         ["connect", () => asyncStep("connect")],
         ["reconnect", () => asyncStep("reconnect")],
