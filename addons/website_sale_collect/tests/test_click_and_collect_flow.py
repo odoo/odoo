c838e1dbab240621feb8fa2bbb5d4b4d1db3b2f9
Valeriya(vchu) <vchu@odoo.com>
2025-07-10 14:26:59 +0000

    [FIX] website_sale_collect: focus on checking the public access
    Tours are too fast for imitating the user actions that led to
    sometimes creating 2 orders in parallel instead of reusing the first
    created. The test was added for
    b395f984b13eb83310024b9fa94d7822211ef8c1 fix, so with this commit, we
    keep the test more specific to the fix and avoid inconsistent behavior.
    
    closes odoo/odoo#229630
    
    X-original-commit: 2784ea0047335ed897e040a663451c8a743e3591
    Signed-off-by: Valeriya Chuprina (vchu) <vchu@odoo.com>

--- addons/website_sale_collect/tests/test_click_and_collect_flow.py
+++ addons/website_sale_collect/tests/test_click_and_collect_flow.py
@@ -13,27 +13,23 @@ class TestClickAndCollectFlow(HttpCase, ClickAndCollectCommon):
     def setUpClass(cls):
         super().setUpClass()
         cls.storable_product.name = "Test CAC Product"
-        cls.provider.write(
-            {
-                'state': 'enabled',
-                'is_published': True,
-            }
-        )
-        cls.in_store_dm.warehouse_ids[0].partner_id = cls.env['res.partner'].create(
-            {
-                **cls.dummy_partner_address_values,
-                'name': "Shop 1",
-                'partner_latitude': 1.0,
-                'partner_longitude': 2.0,
-            }
-        )
+        cls.provider.write({
+            'state': 'enabled',
+            'is_published': True,
+        })
+        cls.in_store_dm.warehouse_ids[0].partner_id = cls.env['res.partner'].create({
+            **cls.dummy_partner_address_values,
+            'name': "Shop 1",
+            'partner_latitude': 1.0,
+            'partner_longitude': 2.0,
+        })
 
     def test_buy_with_click_and_collect_as_public_user(self):
         """
         Test the basic flow of buying with click and collect as a public user with more than
         one delivery method available
         """
-        self.start_tour('/', 'website_sale_collect_buy_product')
+        self.start_tour('/', 'website_sale_collect_widget')
 
     def test_default_location_is_set_for_pick_up_in_store(self):
         """
