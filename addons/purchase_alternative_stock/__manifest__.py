45c2f90bc377fc2576572f6750c53647c6af7142
dhha-odoo <dhha@odoo.com>
2025-07-11 15:30:55 +0530

    [MOV] purchase_alternative_stock*: separate purchase_requisition_stock
    *purchase_requisition_stock
    
    With This Commit:
    =================================
    
    We have separated the purchase alternative–related functionality from the
    purchase_requisition_stock module and reorganised it under a newly created module, purchase_alternative_stock.
    This new module depends on both purchase_alternative and purchase_stock,
    ensuring proper modular structure and clearer separation of responsibilities.
    
    taskId :- 4737059
    
    Part-of: odoo/odoo#218612
    Related: odoo/upgrade#8052
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/purchase_alternative_stock/__manifest__.py
+++ addons/purchase_alternative_stock/__manifest__.py
@@ -1,15 +1,13 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Purchase Requisition Stock',
+    'name': 'Purchase Alternative Stock',
     'version': '1.2',
     'category': 'Supply Chain/Purchase',
     'sequence': 70,
-    'depends': ['purchase_requisition', 'purchase_stock'],
+    'depends': ['purchase_alternative', 'purchase_stock'],
     'data': [
-        'security/ir.model.access.csv',
-        'data/purchase_requisition_stock_data.xml',
-        'views/purchase_requisition_views.xml',
+        'views/purchase_views.xml',
     ],
     'auto_install': True,
     'author': 'Odoo S.A.',
