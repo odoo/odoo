6845aab3625c0a56c721ea31f38fced9be38709d
william-andre <wan@odoo.com>
2025-07-11 12:26:27 +0000

    [IMP] account,analytic: allow partial edition of analytic
    In Journal Items list view, mass edition of the Analytic Distribution on
    several records is disappointing and reported almost unusable.
    It is impossible to mass edit only one plan on multiple lines using a
    distribution. This commit is adding the possibility to do so from the
    list view of analytic items but also from the journal items.
    
    task-4937143
    
    closes odoo/odoo#222728
    
    X-original-commit: 1019e22b4f5c2182547ee3c7112f86e85b7d8c54
    Signed-off-by: John Laterre (jol) <jol@odoo.com>
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/hr_timesheet/tests/test_timesheet.py
+++ addons/hr_timesheet/tests/test_timesheet.py
@@ -117,6 +117,8 @@ class TestTimesheet(TestCommonTimesheet):
 
     def setUp(self):
         super().setUp()
+        # Make sure to clean the plan fields
+        self.env.registry._setup_models__(self.env.cr)
 
         # Crappy hack to disable the rule from timesheet grid, if it exists
         # The registry doesn't contain the field timesheet_manager_id.
@@ -853,3 +855,21 @@ class TestTimesheet(TestCommonTimesheet):
             field_name: analytic_account.id
         })
         self.assertEqual(line[field_name].id, analytic_account.id, f"The value of {field_name} shouldn't get overwritten by the project's account")
+
+    def test_split_analytic_dynamic_update(self):
+        self.empl_employee.hourly_cost = 10.0
+        another_account = self.project.account_id.copy()
+
+        line = self.env['account.analytic.line'].create({
+            'name': 'Timesheet',
+            'unit_amount': 1,
+            'project_id': self.project_customer.id,
+            'account_id': self.project.account_id.id,
+            'employee_id': self.empl_employee.id,
+        })
+        self.assertEqual(line.amount, -10)
+        line.analytic_distribution = {
+            f"{self.project.account_id.id}": 50,
+            f"{another_account.id}": 50,
+        }
+        self.assertEqual(line.amount, -5)  # the line is split in 2
