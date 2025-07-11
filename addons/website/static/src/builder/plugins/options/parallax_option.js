cbf18c8233ee00d9a0298729c57f3a09cd213cef
Denis Rygaert <dery@odoo.com>
2025-07-11 07:59:46 +0000

    [FIX] website: restore parallax options in popup
    In [1], we wrongly removed the parallax option for background image that
    are in a dialog. This lead to some problems (e.g. dropping a snippet
    with parallax enabled (s_cover) inside a popup doesn't let you remove
    it). This commit just revert the change concerning the apparition of the
    option depending on the context.
    
    [1]: 598827a
    
    X-original-commit: f756aa826213870a6af6f3ecda396f300d215177

--- addons/website/static/src/builder/plugins/options/parallax_option.js
+++ addons/website/static/src/builder/plugins/options/parallax_option.js
@@ -1,12 +1,5 @@
-import { BaseOptionComponent, useDomState } from "@html_builder/core/utils";
+import { BaseOptionComponent } from "@html_builder/core/utils";
 export class ParallaxOption extends BaseOptionComponent {
     static template = "website.ParallaxOption";
     static props = {};
-
-    setup() {
-        super.setup();
-        this.state = useDomState((el) => ({
-            InDialog: el.closest('.modal[role="dialog"]'),
-        }));
-    }
 }
