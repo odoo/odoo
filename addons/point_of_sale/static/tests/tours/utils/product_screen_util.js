de2bc48c188eaf270f31ce1ff65ee35eb84d7c65
vpan-odoo <vpan@odoo.com>
2025-07-11 14:36:35 +0530

    [FIX] pos_{,restaurant}: visiblity of tax control button and it's dialog title
    Before this commit:
    ===================
    - When only point_of_sale is installed, the tax control button is visible only
    if `Flexible Tax` is enabled. But with pos_restaurant installed, the button
    is always visible, even if Flexible Tax is disabled.
    - Also, the tax selection dialog had an incorrect title: `Please register the
    voucher number`.
    
    After this commit:
    ==================
    - In point of sale, the tax control button will be visible if `Flexible Tax`
    is enabled, and in restaurant mode, the button will be visible if the
    `Flexible Tax` or `Takeout/Delivery` is enabled.
    - The tax selection dialog title has been updated to: `Choose the tax you want
    to apply`.
    
    closes odoo/odoo#218815
    
    Task: 4937977
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/point_of_sale/static/tests/tours/utils/product_screen_util.js
+++ addons/point_of_sale/static/tests/tours/utils/product_screen_util.js
@@ -407,6 +407,14 @@ export function checkFiscalPosition(name) {
         Dialog.cancel(),
     ];
 }
+export function checkFiscalPositionButton() {
+    return [
+        {
+            content: "click fiscal position button",
+            trigger: ".o_fiscal_position_button",
+        },
+    ];
+}
 export function closeWithCashAmount(val) {
     return [
         {
