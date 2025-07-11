0667cef9397468ba529da8f2509441a0a4b01a83
Thibault Delavallée <tde@odoo.com>
2025-07-11 13:35:14 +0200

    [REF] mail: use operation check in controllers
    When checking for 'message post' (aka mail.message creation) rights
    in controller, correctly call model method allowing to tune document
    operation based on mail.message operation. Manually checking for
    '_mail_post_access' is not sufficient if we want to be coherent
    in all cases (posting message, creating message, searching messages,
    ...).
    
    See '_mail_get_operation_for_mail_message_operation'.
    
    Task-4878312

--- addons/portal/controllers/portal_thread.py
+++ addons/portal/controllers/portal_thread.py
@@ -34,8 +34,7 @@ class PortalChatter(ThreadController):
         thread = self._get_thread_with_access(thread_model, thread_id, **kwargs)
         partner = request.env.user.partner_id
         if thread:
-            mode = request.env[thread_model]._get_mail_message_access([thread_id], "create")
-            has_react_access = self._get_thread_with_access(thread_model, thread_id, mode, **kwargs)
+            has_react_access = self._get_thread_with_access_for_post(thread_model, thread_id, **kwargs)
             can_react = has_react_access
             if request.env.user._is_public():
                 portal_partner = get_portal_partner(
