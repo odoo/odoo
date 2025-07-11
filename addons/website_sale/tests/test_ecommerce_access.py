1e02df695c836cb02babe3791e28e9142bbf4550
Pierre Lamotte (pila) <pila@odoo.com>
2025-07-11 15:45:01 +0200

    [FIX] website_sale: ensure public and portal users access shop
    ## Version
    18.4+
    
    ## Steps to reproduce
    - On the `/shop` page, in editor mode:
      - Select the product display block;
      - Enable `Categories` display and set it to `Sidebar`;
      - Disable the option `Collapse Category Recursive`;
    - Open a new incognito window (or use a Portal user).
      - Try to access `/shop`.
    
    ## Issue
    When the category display is set to `Sidebar` collapse and some categories have unpublished products, the `ir.rule` introduced in commit 120a7633505891ba3e02e879f0c1a8287a690456 prevents Public and Portal users from accessing the shop.
    This results in a 403 or 500 error due to inaccessible child categories being used in the recursive sidebar rendering.
    
    ## Cause
    Commit 120a7633505891ba3e02e879f0c1a8287a690456 added a rule to hide empty categories from Public and Portal users by restricting access to categories where `has_published_products = True`.
    However, the sidebar template continues to loop over all `child_id`, including categories filtered out by access rules. Since Public and Portal users no longer have access to empty categories, this results in access errors during template rendering.
    
    ## Solution
    Apply same filters as applied here https://github.com/odoo/odoo/blob/5d25b74b9c25862fcff699ac2c78b18aaf9f8fcd/addons/website_sale/views/templates.xml#L1643-L1644
    
    opw-4937000

--- addons/website_sale/tests/test_ecommerce_access.py
+++ addons/website_sale/tests/test_ecommerce_access.py
@@ -9,6 +9,47 @@ from odoo.addons.website_sale.tests.common import WebsiteSaleCommon
 @tagged('post_install', '-at_install')
 class TestEcommerceAccess(HttpCaseWithUserDemo, WebsiteSaleCommon):
 
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        # Ensure all categories are empty
+        cls.env['product.template'].search([]).write({'active': False})
+
+        cls.productCategory = cls.env['product.public.category'].create(
+            {'name': 'Category with Product', 'website_id': cls.website.id},
+        )
+        cls.emptyCategory = cls.env['product.public.category'].create(
+            {'name': 'Empty Category', 'website_id': cls.website.id},
+        )
+
+        cls.env['product.public.category'].create([
+            {
+                'name': 'Has Products',
+                'parent_id': cls.productCategory.id,
+                'website_id': cls.website.id,
+            },
+            {
+                'name': 'Empty Subcategory',
+                'parent_id': cls.productCategory.id,
+                'website_id': cls.website.id,
+            },
+            {
+                'name': 'Empty Subcategory of Empty Category',
+                'parent_id': cls.emptyCategory.id,
+                'website_id': cls.website.id,
+            },
+        ])
+
+        # Add one dummy product in one of the subcategories
+        cls.product_a = cls._create_product(
+            public_categ_ids=[cls.productCategory.child_id[0].id],
+            is_published=True,
+        )
+
+        cls.quick_ref('website_sale.products_categories').active = True
+        cls.quick_ref('website_sale.option_collapse_products_categories').active = False
+        cls.portal_user = cls._create_new_portal_user(website_id=cls.website.id)
+
     def test_ecommerce_access_public_user(self):
         # By default, everyone has access to ecommerce
         self.assertTrue(self.website.with_user(self.public_user).has_ecommerce_access())
@@ -40,3 +81,20 @@ class TestEcommerceAccess(HttpCaseWithUserDemo, WebsiteSaleCommon):
         self.menu.with_user(self.public_user).sudo()._compute_visible()
         # Check if menu is hidden for public user when ecommerce is restricted
         self.assertFalse(self.menu.is_visible)
+
+    def test_ecommerce_public_user_access_when_empty_categories(self):
+        """
+        Ensures that the '/shop' URL returns a 200 OK status code even if categories are empty
+        when not logged.
+        """
+        response = self.url_open('/shop')
+        self.assertEqual(response.status_code, 200)
+
+    def test_ecommerce_portal_user_access_when_empty_categories(self):
+        """
+        Ensures that the '/shop' URL returns a 200 OK status code even if categories are empty
+        when logged as portal user.
+        """
+        self.authenticate(self.portal_user.login, self.portal_user.login)
+        response = self.url_open('/shop')
+        self.assertEqual(response.status_code, 200)
