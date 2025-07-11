747512bf2b4ac00d114fd52b8e4726b1d35751a3
dhha-odoo <dhha@odoo.com>
2025-07-11 15:17:36 +0530

    [MOV] purchase_requisition_sale: move purchase_requisition_sale module
    With This Commit:
    =================================
    
    move purchase_requisition_sale module into purchase_alternative_sale.
    
    task - [4737059](https://www.odoo.com/odoo/my-tasks/4737059)

--- addons/purchase_alternative_sale/wizard/purchase_alternative_create.py
+++ addons/purchase_alternative_sale/wizard/purchase_alternative_create.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, models
 
 
-class PurchaseRequisitionCreateAlternative(models.TransientModel):
-    _inherit = 'purchase.requisition.create.alternative'
+class PurchaseAlternativeCreate(models.TransientModel):
+    _inherit = 'purchase.alternative.create'
 
     @api.model
     def _get_alternative_line_value(self, order_line, product_tmpl_ids_with_description):
