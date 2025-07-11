d0103376665404430f257c0d98b1e808829fe33e
william-andre <wan@odoo.com>
2025-07-11 12:26:27 +0000

    [IMP] account,analytic: allow partial edition of analytic
    In Journal Items list view, mass edition of the Analytic Distribution on
    several records is disappointing and reported almost unusable.
    It is impossible to mass edit only one plan on multiple lines using a
    distribution. This commit is adding the possibility to do so from the
    list view of analytic items but also from the journal items.
    
    task-4937143
    
    closes odoo/odoo#222795
    
    X-original-commit: 6845aab3625c0a56c721ea31f38fced9be38709d
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: John Laterre (jol) <jol@odoo.com>

--- addons/analytic/static/tests/mock_server/mock_models/account_analytic_plan.js
+++ addons/analytic/static/tests/mock_server/mock_models/account_analytic_plan.js
@@ -5,4 +5,17 @@ export class AccountAnalyticPlan extends models.ServerModel {
 
     name = fields.Char()
     parent_id = fields.Many2one({ relation: "account.analytic.plan" })
+
+    get_relevant_plans() {
+        return this.filter((plan) => !plan.parent_id).map((plan) => {
+            return {
+                "id": plan.id,
+                "name": plan.name,
+                "color": plan.color,
+                "applicability": plan.default_applicability || "optional",
+                "all_account_count": 1,
+                "column_name": `x_plan${plan.id}_id`,
+            }
+        })
+    }
 }
