4ac37250808650d21c8e22da66461172783a2e39
khom-odoo <khom@odoo.com>
2025-07-13 19:24:09 +0000

    [IMP] l10n_sa_edi: parent company info in CSR files
    Description of the issue/feature this PR addresses:
        The current implementation of generating CSR files for EDI doesn't differnetiate between parent companies and branches.
        ZATCA, however, expects slightly different information depending on whether the company is a branch or a parent.
    
        This also includes a refactor of the tests for l10n_sa_edi.
        For 18.2+ We fix a bug where you can't onboard journals in branches if the parent company is not selected
    
        Current behavior before PR:
        The Organization Name, Organization Unit Name, and Organization Identifier of whichever company on the journal were being sent to ZATCA.
        You could also Onboard/Re-Onboard a journal in different company.
    
        The unit tests for EDI document generation were primarily using a partner based in the US, which meant that the b2b invoicing main flow
        was not being properly tested
    
        Desired behavior after PR is merged:
        - Parent/Standalone Company:
          - Organization Name: The name of the company
          - Organization Unit Name: First 10 characters of the VAT
          - Organization Identifier: The VAT of the company
        - Branch:
          - Organization Name: The name of the parent
          - Organization Unit Name: The name of the branch
          - Organization Identifier: The VAT of the parent
          - Allow the user to onboard a branch's journal if the parent company is not selected
        - Prevent users from posting invoices, credit notes, or debit notes whose invoice company doesn't match the journal company
        - The tests are easier to read and they test the b2b flow correctly
    
        opw-4794842
    
    closes odoo/odoo#222475
    
    X-original-commit: d69402a13914428fe6e05cc772157f0eec2c5627
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>
    Signed-off-by: Omar Khalid (khom) <khom@odoo.com>

---
+++ addons/l10n_sa_edi/tests/test_account_move.py
@@ -0,0 +1,49 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+import logging
+
+from odoo.exceptions import UserError
+from odoo.tests import tagged
+from odoo.addons.account_edi.tests.common import AccountEdiTestCommon
+
+from .common import TestSaEdiCommon
+
+_logger = logging.getLogger(__name__)
+
+
+@tagged('post_install_l10n', '-at_install', 'post_install')
+class TestSAEdiAccountMove(TestSaEdiCommon):
+
+    @classmethod
+    @AccountEdiTestCommon.setup_edi_format('l10n_sa_edi.edi_sa_zatca')
+    @AccountEdiTestCommon.setup_country('sa')
+    def setUpClass(cls):
+        super().setUpClass()
+        cls._setup_branches()
+
+    @classmethod
+    def _setup_branches(cls):
+        cls.sa_branch = cls.env['res.company'].create({
+            'name': 'SA Branch',
+            'parent_id': cls.company.id,
+            'country_id': cls.company.country_id.id,
+        })
+
+    def test_invoice_with_mismatched_companies(self):
+        move_data = {
+            'name': 'INV/2025/00012',
+            'invoice_date': '2025-07-05',
+            'invoice_date_due': '2025-07-12',
+            'partner_id': self.partner_sa,
+            'invoice_line_ids': [{
+                'product_id': self.product_a.id,
+                'price_unit': self.product_a.standard_price,
+                'quantity': 1,
+                'tax_ids': self.tax_15.ids,
+            }]
+        }
+
+        invoice = self._create_test_invoice(**move_data)
+        invoice.company_id = self.sa_branch
+
+        with self.assertRaises(UserError, msg="A UserError is expected when the company on the invoice doesn't match the company on the journal."):
+            invoice.action_post()
