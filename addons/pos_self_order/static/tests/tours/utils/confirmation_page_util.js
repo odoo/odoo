981865da3e6dd36135f178bebabaf017894ba3ed
manv-afk <manv@odoo.com>
2025-07-14 14:50:19 +0200

    [FIX] pos_self_order: only display `tracking_number` for restaurant
    - Fix issue where `tracking_number` was displayed for all config. We want to display this number only for `pos_restaurant` configurations.
    - Fix issue where `tracking_number` was not displayed on kiosk order receipts.
    
    task-id: 4922308

--- addons/pos_self_order/static/tests/tours/utils/confirmation_page_util.js
+++ addons/pos_self_order/static/tests/tours/utils/confirmation_page_util.js
@@ -11,3 +11,19 @@ export function orderNumberShown() {
         trigger: ".number",
     };
 }
+
+export function orderNumberIs(trackingPrefix, trackingNumber) {
+    return {
+        content: `Check that the order number start with '${trackingPrefix}', and end with number '${trackingNumber}'.`,
+        trigger: `span.number`,
+        run: function () {
+            const span = document.querySelector("span.number");
+            const text = span.textContent || "";
+            if (!text.startsWith(trackingPrefix) || !text.endsWith(trackingNumber)) {
+                throw new Error(
+                    `Order number '${text}' does not start with '${trackingPrefix}' and end with '${trackingNumber}'`
+                );
+            }
+        },
+    };
+}
