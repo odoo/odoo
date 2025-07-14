d1751b5d13941bc8adc9bae1f4c928ecd5c549f0
Harsh Modi <hamo@odoo.com>
2025-07-14 17:46:29 +0530

    [ADD] account_debit_note_sequence: and change refund sequence to stored compute
    backport of commit-https://github.com/odoo/odoo/commit/ce8690517b4b12a5271f4d374836df68e6658f9e

---
+++ addons/account_debit_note_sequence/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Debit Note Sequence',
+    'category': 'Accounting/Accounting',
+    'summary': "Debit Note Sequence",
+    'description': """
+This module allows you to add sequence for Debit note
+    """,
+    'depends': ['account_debit_note'],
+    'installable': True,
+    'auto_install': True,
+    'data': ['views/account_journal_views.xml'],
+    'license': 'LGPL-3',
+}
