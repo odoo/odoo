18ec4fa68ec99608e131f714a1d5965b462c5d74
Mathieu Coutant <mcou@odoo.com>
2025-07-14 13:56:05 +0200

    [FIX] sale: correct fixed taxes with early payment discount
    Issue:
    The fixed tax value is doubled in `sale` when an early payment discount is applied.
    
    Steps to reproduce:
    - Create a fixed tax of $20 for example
    - Create a payment term with an early discount of 10% if paid before 10 days
    - Select "Always (upon invoice)"
    - In Sale create a new quotation with a line of $100 and the fixed tax
    - Select the created payment term
    - The tax value is doubled ($40)
    
    Cause:
    To compute the payment terms `_add_base_lines_for_early_payment_discount` (added in this [commit](https://github.com/odoo/odoo/commit/781678fb07ab3dd4d132193a938bbb71e8395424)) is adding two lines:
    - one with the negative amount of the payment term and the taxes
    - one with the positive amount of the payment term
    The goal is to compute the taxes on the discounted untaxed amount of the line. The original line compute the taxes for the undiscounted amount. The first added line computes the taxes for the amount of the discount and is negative. When adding the two the result is the tax value computed from the discounted untaxed amount.
    
    The issue is that a fixed will always have the same value, this means that the tax amount of the first added line will be positive and the same as the original line. When adding the two the amount is doubled.
    
    Solution:
    We don't add taxes with fixed amount on the first add line.
    
    opw-4868011
    
    closes odoo/odoo#218816
    
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- addons/sale/tests/test_taxes_tax_totals_summary.py
+++ addons/sale/tests/test_taxes_tax_totals_summary.py
@@ -136,3 +136,86 @@ class TestTaxesTaxTotalsSummarySale(TestTaxCommonSale, TestTaxesTaxTotalsSummary
             },
             soft_checking=True,
         )
+
+    def test_apply_mixed_epd_discount_fixed_tax(self):
+        """
+        When applying an epd - mixed payment term, the fixed tax amount should be the same.
+        """
+        tax_a = self.fixed_tax(20.0)
+        early_payment_term = self.env['account.payment.term'].create({
+            'name': "early_payment_term",
+            'early_pay_discount_computation': 'mixed',
+            'discount_percentage': 10,
+            'discount_days': 10,
+            'early_discount': True,
+            'line_ids': [
+                Command.create({
+                    'value': 'percent',
+                    'value_amount': 100,
+                    'nb_days': 20,
+                }),
+            ],
+        })
+
+        sale_order = self.env['sale.order'].create({
+            'partner_id': self.partner.id,
+            'payment_term_id': early_payment_term.id,
+            'order_line': [
+                Command.create({
+                    'product_id': self.product.id,
+                    'price_unit': 100,
+                    'tax_id': [Command.set(tax_a.ids)],
+                }),
+            ],
+        })
+        self.assert_sale_order_tax_totals_summary(
+            sale_order,
+            {
+                'base_amount_currency': 100.0,
+                'tax_amount_currency': 20.0,
+                'total_amount_currency': 120.0,
+            },
+            soft_checking=True,
+        )
+
+    def test_apply_mixed_epd_discount_percent_and_fixed_tax(self):
+        """
+        When applying an epd - mixed payment term, the percent tax should be computed based on the discounted untaxed amount.
+        """
+        tax_a = self.percent_tax(15.0)
+        tax_b = self.fixed_tax(20.0)
+        early_payment_term = self.env['account.payment.term'].create({
+            'name': "early_payment_term",
+            'early_pay_discount_computation': 'mixed',
+            'discount_percentage': 10,
+            'discount_days': 10,
+            'early_discount': True,
+            'line_ids': [
+                Command.create({
+                    'value': 'percent',
+                    'value_amount': 100,
+                    'nb_days': 20,
+                }),
+            ],
+        })
+
+        sale_order = self.env['sale.order'].create({
+            'partner_id': self.partner.id,
+            'payment_term_id': early_payment_term.id,
+            'order_line': [
+                Command.create({
+                    'product_id': self.product.id,
+                    'price_unit': 100,
+                    'tax_id': [Command.set((tax_a + tax_b).ids)],
+                }),
+            ],
+        })
+        self.assert_sale_order_tax_totals_summary(
+            sale_order,
+            {
+                'base_amount_currency': 100.0,
+                'tax_amount_currency': 33.5,
+                'total_amount_currency': 133.5,
+            },
+            soft_checking=True,
+        )
