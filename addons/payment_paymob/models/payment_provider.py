ccce27fb080228f02b008b286fef5c64b43dbd7a
Lina (liew) <liew@odoo.com>
2025-07-14 14:02:38 +0200

    [FIX] payment_paymob: avoid processing duplicate methods
    Paymob allows for multiple payment methods to have the same gateway type
    Which one should be enabled on Odoo is determined based on:
    1. The most recently created payment method on the user's paymob account
    2. For card: Exclude Integration IDs where integration_type = 'moto' or
    is_auth = true.
    
    closes odoo/odoo#218825
    
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>

--- addons/payment_paymob/models/payment_provider.py
+++ addons/payment_paymob/models/payment_provider.py
@@ -3,7 +3,7 @@
 import json
 import logging
 import pprint
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 import requests
 
@@ -114,19 +114,29 @@ class PaymentProvider(models.Model):
     def _match_paymob_payment_methods(self, paymob_gateways_data):
         """ Filter gateways available in Paymob to match the payment methods enabled in Odoo.
 
-        This method takes the full list of gateways from Paymob and returns only those that:
+        This method takes the full list of gateways from Paymob, and while avoiding duplicates,
+        returns only those that:
 
         1. Have a gateway_type mapped to an Odoo payment method code.
         2. Are available for the current provider.
         3. Are not Apple Pay or Google Pay (currently unsupported for mobile-only payments).
+        4. Are not a saved card (currently unsupported).
+        5. Are not an Authorize/Capture payment methods (currently unsupported).
 
         :param list[dict] paymob_gateways_data: The gateways data returned by the Paymob API.
         :return: All the matched Paymob gateways' data.
         :rtype: list
         """
         available_payment_method_codes = self.payment_method_ids.mapped('code')
+        sorted_gateways_data = sorted(
+            paymob_gateways_data,
+            key=lambda x: datetime.strptime((x['created_at']), "%Y-%m-%dT%H:%M:%S.%f"),
+            reverse=True,
+        )
         matched_gateways_data = []
-        for gateway_data in paymob_gateways_data:
+        for gateway_data in sorted_gateways_data:
+            if not available_payment_method_codes:  # All available payment methods are now matched.
+                break
             integration_name = gateway_data.get('integration_name') or ''
             is_apple_pay = 'apple' in integration_name.lower()
             is_google_pay = 'google' in integration_name.lower()
@@ -135,8 +145,16 @@ class PaymentProvider(models.Model):
                 continue
             gateway_type = gateway_data.get('gateway_type')
             payment_method_code = const.PAYMENT_METHODS_MAPPING.get(gateway_type)
+            if payment_method_code == 'card' and (
+                # Tokenization and manual capture are not supported at the moment.
+                gateway_data['integration_type'] == 'moto' or gateway_data['is_auth']
+            ):
+                continue
             if payment_method_code in available_payment_method_codes:
                 matched_gateways_data.append(gateway_data)
+                # In some cases, paymob accounts might have multiple gateway data for the same
+                # payment method, only the most recent gateway_data should be considered
+                available_payment_method_codes.remove(payment_method_code)
         return matched_gateways_data
 
     def _update_payment_method_integration_names(self, matched_gateways_data):
