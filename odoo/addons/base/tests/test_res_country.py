69fe50b606fb26135e91379792a31455ddd17469
Ethan Vincent <etvi@odoo.com>
2025-07-15 12:23:29 +0200

    [REV] base: no transformation search tokens res_country name_search
    revert of commit: ea6d105309
    
    this commit broke some legit existing filters (multi-word
    country name, etc.)
    
    more details here: https://github.com/odoo/odoo/pull/218960

--- odoo/addons/base/tests/test_res_country.py
+++ odoo/addons/base/tests/test_res_country.py
@@ -1,56 +1,20 @@
 from odoo.tests import TransactionCase, tagged
 
 
-class TestResCountryCommon(TransactionCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.glorious_arstotzka = cls.env['res.country'].create({
-            'name': 'Arstotzka',
-            'code': 'AA',
-        })
-        cls.altan = cls.env['res.country.state'].create({
-            'country_id': cls.glorious_arstotzka.id,
-            'code': 'AL',
-            'name': 'Altan',
-        })
-
-
 @tagged('-at_install', 'post_install')
-class TestResCountry(TestResCountryCommon):
-    def test_name_search(self):
-        glorious_arstotzka_tuple = (self.glorious_arstotzka.id, self.glorious_arstotzka.display_name)
-        for (name, op) in [
-            ('ARSTOTZKA', '='),
-            ('arstotzka', '='),
-            ('ARSTOTZKA', '!='),
-            ('arstotzka', '!='),
-            (['ARSTOTZKA'], 'in'),
-            (['arstotzka'], 'in'),
-            (['ARSTOTZKA'], 'not in'),
-            (['arstotzka'], 'not in'),
-        ]:
-            with self.subTest((name, op)):
-                assertFunc = self.assertNotIn if op in ('!=', 'not in') else self.assertEqual
-                assertFunc(
-                    [glorious_arstotzka_tuple],
-                    self.env['res.country'].name_search(name, operator=op),
-                    f"Failed on operator: '{op}'"
-                )
-
-    def test_name_search_code(self):
-        glorious_arstotzka_tuple = (self.glorious_arstotzka.id, self.glorious_arstotzka.display_name)
-        res = self.env['res.country'].name_search('aA', operator='=')
-        self.assertEqual(res, [glorious_arstotzka_tuple])
-
-
-@tagged('-at_install', 'post_install')
-class TestResCountryState(TestResCountryCommon):
+class TestResCountryState(TransactionCase):
     def test_find_by_name(self):
         """It should be possible to find a state by its display name
         """
-        glorious_arstotzka = self.glorious_arstotzka
-        altan = self.altan
+        glorious_arstotzka = self.env['res.country'].create({
+            'name': 'Arstotzka',
+            'code': 'AA',
+        })
+        altan = self.env['res.country.state'].create({
+            'country_id': glorious_arstotzka.id,
+            'code': 'AL',
+            'name': 'Altan',
+        })
 
         for name in [
             altan.name,
