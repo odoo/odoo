d8d680df298b1ecc212a2dbb00aadf2a31ffbf9c
Sébastien (blse) <blse@odoo.com>
2025-07-16 11:43:18 +0200

    [FIX] html_builder: select the amount when clicking on monetary field
    The following behavior has been lost with the initial [website builder
    refactor]: When clicking on a monetary field (including on the currency
    sign), the amount gets selected
    
    [website builder refactor]: 9fe45e2b7ddbbfd0445ffe25a859e67a316d02b2
    task-4367641

--- addons/html_builder/static/src/plugins/monetary_field_plugin.js
+++ addons/html_builder/static/src/plugins/monetary_field_plugin.js
@@ -1,12 +1,42 @@
 import { Plugin } from "@html_editor/plugin";
 import { registry } from "@web/core/registry";
 
+const monetarySel = "[data-oe-field][data-oe-type=monetary]";
+
 export class MonetaryFieldPlugin extends Plugin {
     static id = "monetaryField";
+    static dependencies = ["selection"];
     resources = {
-        force_editable_selector: "[data-oe-field][data-oe-type=monetary] .oe_currency_value",
-        force_not_editable_selector: "[data-oe-field][data-oe-type=monetary]",
+        force_editable_selector: `${monetarySel} .oe_currency_value`,
+        force_not_editable_selector: monetarySel,
     };
+
+    setup() {
+        const handleEvent = (ev, condition) => {
+            const fieldEl = ev.target.closest(monetarySel);
+            const amountEl = fieldEl?.querySelector(".oe_currency_value");
+            if (amountEl?.isContentEditable && condition({ fieldEl, amountEl })) {
+                this.dependencies.selection.setSelection({
+                    anchorNode: amountEl,
+                    anchorOffset: 0,
+                    focusNode: amountEl,
+                    focusOffset: amountEl.childNodes.length,
+                });
+            }
+        };
+        this.addDomListener(this.editable, "focusin", (ev) =>
+            handleEvent(ev, ({ fieldEl }) => fieldEl !== ev.relatedTarget?.closest(monetarySel))
+        );
+        this.addDomListener(this.editable, "click", (ev) =>
+            handleEvent(
+                ev,
+                ({ amountEl }) =>
+                    !this.dependencies.selection
+                        .getTargetedNodes()
+                        .some((node) => amountEl.contains(node))
+            )
+        );
+    }
 }
 
 registry.category("website-plugins").add(MonetaryFieldPlugin.id, MonetaryFieldPlugin);
