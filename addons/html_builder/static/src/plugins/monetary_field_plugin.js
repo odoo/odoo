0dcd39e36e7d1cf57dd214e1d3bb7b34b6152aba
Sébastien (blse) <blse@odoo.com>
2025-07-16 11:43:18 +0200

    [FIX] html_builder: select the amount when clicking on monetary field
    The following behavior as been lost with the initial website builder
    refactor: When clicking on a monetary field (including on the currency
    sign), the amount gets selected
    
    Website refactor: 9fe45e2b7ddbbfd0445ffe25a859e67a316d02b2
    task-4367641

--- addons/html_builder/static/src/plugins/monetary_field_plugin.js
+++ addons/html_builder/static/src/plugins/monetary_field_plugin.js
@@ -3,10 +3,54 @@ import { registry } from "@web/core/registry";
 
 class MonetaryFieldPlugin extends Plugin {
     static id = "monetaryField";
+    static dependencies = ["selection"];
     resources = {
         force_editable_selector: "[data-oe-field][data-oe-type=monetary] .oe_currency_value",
         force_not_editable_selector: "[data-oe-field][data-oe-type=monetary]",
     };
+
+    setup() {
+        this.addDomListener(this.editable, "focusin", (ev) => {
+            const fieldEl = ev.target.closest("[data-oe-field][data-oe-type=monetary]");
+            if (!fieldEl) {
+                return;
+            }
+            const amountEl = fieldEl.querySelector(".oe_currency_value");
+            if (!amountEl.isContentEditable) {
+                return;
+            }
+            if (fieldEl !== ev.relatedTarget?.closest("[data-oe-field][data-oe-type=monetary]")) {
+                this.dependencies.selection.setSelection({
+                    anchorNode: amountEl,
+                    anchorOffset: 0,
+                    focusNode: amountEl,
+                    focusOffset: amountEl.childNodes.length,
+                });
+            }
+        });
+        this.addDomListener(this.editable, "click", (ev) => {
+            const fieldEl = ev.target.closest("[data-oe-field][data-oe-type=monetary]");
+            if (!fieldEl) {
+                return;
+            }
+            const amountEl = fieldEl.querySelector(".oe_currency_value");
+            if (!amountEl.isContentEditable) {
+                return;
+            }
+            if (
+                !this.dependencies.selection
+                    .getTargetedNodes()
+                    .some((node) => amountEl.contains(node))
+            ) {
+                this.dependencies.selection.setSelection({
+                    anchorNode: amountEl,
+                    anchorOffset: 0,
+                    focusNode: amountEl,
+                    focusOffset: amountEl.childNodes.length,
+                });
+            }
+        });
+    }
 }
 
 registry.category("website-plugins").add(MonetaryFieldPlugin.id, MonetaryFieldPlugin);
