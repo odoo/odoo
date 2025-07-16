1e03b6faac0bdcbc50887147562e7f661f58c58f
Adrien Guilliams (adgu) <adgu@odoo.com>
2025-07-16 15:11:03 +0000

    [FIX] point_of_sale: fix search with category
    This commit https://github.com/odoo/odoo/pull/217898/commits/9b1477f2693fb0678ca073d36ad6630c3034df00
    introduced a bug where, when the user had a category selected and he
    would type a word in the search bar, he would have a white screen.
    This is fixed by reverting the change that was doing it and was not
    necessary.
    
    closes odoo/odoo#219538
    
    X-original-commit: 6ba260b8031feeeda429f9bd20de00c52831fbc6
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/point_of_sale/static/src/app/services/pos_store.js
+++ addons/point_of_sale/static/src/app/services/pos_store.js
@@ -2448,7 +2448,6 @@ export class PosStore extends WithLazyGetterTrap {
         const allProducts = this.models["product.template"].getAll();
         let list = [];
         const isSearchByWord = searchWord !== "";
-        const isSelectedCategory = this.selectedCategory?.id;
 
         if (isSearchByWord) {
             if (!this._searchTriggered) {
@@ -2457,11 +2456,11 @@ export class PosStore extends WithLazyGetterTrap {
             }
             list = this.getProductsBySearchWord(
                 searchWord,
-                isSelectedCategory ? this.selectedCategory.associatedProducts : allProducts
+                this.selectedCategory?.id ? this.selectedCategory.associatedProducts : allProducts
             );
         } else {
             this._searchTriggered = false;
-            if (isSelectedCategory) {
+            if (this.selectedCategory?.id) {
                 list = this.selectedCategory.associatedProducts;
             } else {
                 list = allProducts;
@@ -2494,7 +2493,11 @@ export class PosStore extends WithLazyGetterTrap {
             filteredList.push(p);
         }
 
-        if (!isSearchByWord && !isSelectedCategory && this.areAllProductsSpecial(filteredList)) {
+        if (
+            !isSearchByWord &&
+            !this.selectedCategory?.id &&
+            this.areAllProductsSpecial(filteredList)
+        ) {
             return [];
         }
 
