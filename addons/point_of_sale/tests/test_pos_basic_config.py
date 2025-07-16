ed4ffe7e7174c761783c822ad842b55b772120b3
Pedram (PEBR) <pebr@odoo.com>
2025-07-16 11:10:46 +0000

    [FIX] point_of_sale: correct archived combinations calculation
    Before this commit, removing an attribute line from a product would
    inadvertently prevent the sale of any combination in the PoS interface.
    
    opw-4947529
    
    X-original-commit: b284b5edf8158c0689930b211e3085860f5d73cd

--- addons/point_of_sale/tests/test_pos_basic_config.py
+++ addons/point_of_sale/tests/test_pos_basic_config.py
@@ -1317,3 +1317,74 @@ class TestPoSBasicConfig(TestPoSCommon):
         self.assertEqual(order.picking_count, 1, 'Order should have one picking')
         self.assertEqual(len(order.payment_ids), 1, 'Order should have one payment')
         self.assertEqual(self.env['account.move'].search_count([('pos_order_ids', 'in', order.ids)]), 1, 'Order should have one invoice')
+
+    def test_pos_archived_combination(self):
+        product = self.env['product.template'].create({
+            'name': 'Product Test',
+            'available_in_pos': True,
+            'list_price': 10,
+            'taxes_id': False,
+        })
+
+        attribute_1, attribute_2, attribute_3 = self.env['product.attribute'].create([{
+            'name': 'Attribute 1',
+            'create_variant': 'always',
+            'value_ids': [(0, 0, {
+                'name': 'Value 1',
+            }), (0, 0, {
+                'name': 'Value 2',
+            })],
+        }, {
+            'name': 'Attribute 2',
+            'create_variant': 'always',
+            'value_ids': [(0, 0, {
+                'name': 'Value 1',
+            }), (0, 0, {
+                'name': 'Value 2',
+            })],
+        }, {
+            'name': 'Attribute 3',
+            'create_variant': 'always',
+            'value_ids': [(0, 0, {
+                'name': 'Value 1',
+            }), (0, 0, {
+                'name': 'Value 2',
+            })],
+        }])
+
+        _, _, ptal = self.env['product.template.attribute.line'].create([{
+            'product_tmpl_id': product.id,
+            'attribute_id': attribute_1.id,
+            'value_ids': [(6, 0, attribute_1.value_ids.ids)],
+            'sequence': 3,
+        }, {
+            'product_tmpl_id': product.id,
+            'attribute_id': attribute_2.id,
+            'value_ids': [(6, 0, attribute_2.value_ids.ids)],
+            'sequence': 2,
+        }, {
+            'product_tmpl_id': product.id,
+            'attribute_id': attribute_3.id,
+            'value_ids': [(6, 0, attribute_3.value_ids.ids)],
+            'sequence': 1,
+        }])
+
+        product.write({
+            'attribute_line_ids': [(2, ptal.id)],
+        })
+
+        self.open_new_session()
+        response = self.pos_session.load_data([])
+        product_data = next((item for item in response['product.template'] if item['id'] == product.id), None)
+
+        self.assertEqual(len(product_data['_archived_combinations']), 0, "There should be no archived combinations for the product")
+
+        first_variant = product.product_variant_ids[0]
+        first_variant.write({'active': False})
+
+        response = self.pos_session.load_data([])
+        product_data = next((item for item in response['product.template'] if item['id'] == product.id), None)
+
+        self.assertEqual(len(product_data['_archived_combinations']), 1, "There should be one archived combination for the product")
+        self.assertEqual(len(product_data['_archived_combinations'][0]), 2, "Archived combination should have two values")
+        self.assertTrue(all(value in product_data['_archived_combinations'][0] for value in first_variant.product_template_attribute_value_ids.ids), "Archived combination should match the first variant's attribute values")
