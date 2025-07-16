fd15f6854bbb2307ca200df8a07f16b9020c3160
Dhwani Patel <dwa@odoo.com>
2025-07-16 05:58:48 +0000

    [FIX] point_of_sale: fix multi-serial selection with 'Create New' disabled
    Steps to reproduce:
    - Install point_of_sale.
    - Go to POS -> configuration -> settings
    - Search 'Operation type' -> open picking type -> Disable 'Create new'
    - Create a storable product 'test' with serial tracking.
    - Add on-hand quantity with serial numbers.
    - In POS, select the product and choose one SN,
    - Select it again and choose another SN.
    
    Observation:
    - The order line should have 2 quantity with a list of Serial numbers chosen by
      the user, for one quantity, it's working fine, but for multiple quantities,
      an issue occurs.
    
    Issue:
    - While confirming edit serial numbers popup for multiple quantities,
      it checks whether each selected SN is valid or not.
    - The condition is that the entered SN is in the existing available SNs option.
      But the already chosen SN is not in the existing SN option,
    - Also, creating a new SN is disabled, so it's considered an invalid input.
    
    Solution:
    - Added a condition to allow SNs that are already selected
      (matched by ID) to be considered valid inputs.
    
    opw-4865902
    
    closes odoo/odoo#221589
    
    X-original-commit: 52e4d71a528c1e5ea105f879dc6888c57437d8dd
    Signed-off-by: Stéphane Vanmeerhaeghe (stva) <stva@odoo.com>
    Signed-off-by: Dhwani Patel (dwa) <dwa@odoo.com>

--- addons/point_of_sale/static/tests/pos/tours/ticket_screen_tour.js
+++ addons/point_of_sale/static/tests/pos/tours/ticket_screen_tour.js
@@ -375,3 +375,23 @@ registry.category("web_tour.tours").add("test_paid_order_with_archived_product_l
             ]),
         ].flat(),
 });
+
+registry.category("web_tour.tours").add("test_order_with_existing_serial", {
+    steps: () =>
+        [
+            Chrome.startPoS(),
+            Dialog.confirm("Open Register"),
+            ProductScreen.clickDisplayedProduct("Serial Product"),
+            ProductScreen.enterExistingLotNumber("SN1"),
+            ProductScreen.selectedOrderlineHas("Serial Product", "1.00"),
+            inLeftSide({
+                trigger: ".info-list:contains('SN SN1')",
+            }),
+            ProductScreen.clickDisplayedProduct("Serial Product"),
+            ProductScreen.enterExistingLotNumber("SN2"),
+            ProductScreen.selectedOrderlineHas("Serial Product", "2.00"),
+            inLeftSide({
+                trigger: ".info-list:contains('SN SN2')",
+            }),
+        ].flat(),
+});
