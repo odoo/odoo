odoo/odoo

commit 9b1477f2693fb0678ca073d36ad6630c3034df00

[FIX] point_of_sale: enable search for special products
After commit https://github.com/odoo/odoo/commit/2668181c98de4a9c6f33787372adefc01cf1d0bc, it was no longer possible to locate special
products via the search function. Additionally, if these products
belonged to a specific category, they would not be visible when
filtering by that category.

opw-4925867

addons/point_of_sale/static/src/app/services/pos_store.js
@@ -2397,19 +2397,21 @@ export class PosStore extends WithLazyGetterTrap {
         const searchWord = this.searchProductWord.trim();
         const allProducts = this.models["product.template"].getAll();
         let list = [];
+        const isSearchByWord = searchWord !== "";
+        const isSelectedCategory = this.selectedCategory?.id;
 
-        if (searchWord !== "") {
+        if (isSearchByWord) {
             if (!this._searchTriggered) {
                 this.setSelectedCategory(0);
                 this._searchTriggered = true;
             }
             list = this.getProductsBySearchWord(
                 searchWord,
-                this.selectedCategory?.id ? this.selectedCategory.associatedProducts : allProducts
+                isSelectedCategory ? this.selectedCategory.associatedProducts : allProducts
             );
         } else {
             this._searchTriggered = false;
-            if (this.selectedCategory?.id) {
+            if (isSelectedCategory) {
                 list = this.selectedCategory.associatedProducts;
             } else {
                 list = allProducts;
@@ -2442,11 +2444,11 @@ export class PosStore extends WithLazyGetterTrap {
             filteredList.push(p);
         }
 
-        if (this.areAllProductsSpecial(filteredList)) {
+        if (!isSearchByWord && !isSelectedCategory && this.areAllProductsSpecial(filteredList)) {
             return [];
         }
 
-        return searchWord !== ""
+        return isSearchByWord
             ? filteredList.sort((a, b) => b.is_favorite - a.is_favorite)
             : filteredList.sort((a, b) => {
                   if (b.is_favorite !== a.is_favorite) {

commit 1a440ab0ca15e3580f2e91503c24e73703c2a443

[FIX] point_of_sale: fix search with category
This commit https://github.com/odoo/odoo/pull/217898/commits/9b1477f2693fb0678ca073d36ad6630c3034df00
introduced a bug where, when the user had a category selected and he
would type a word in the search bar, he would have a white screen.
This is fixed by reverting the change that was doing it and was not
necessary.

addons/point_of_sale/static/src/app/services/pos_store.js
@@ -2398,7 +2398,6 @@ export class PosStore extends WithLazyGetterTrap {
         const allProducts = this.models["product.template"].getAll();
         let list = [];
         const isSearchByWord = searchWord !== "";
-        const isSelectedCategory = this.selectedCategory?.id;
 
         if (isSearchByWord) {
             if (!this._searchTriggered) {
@@ -2407,11 +2406,11 @@ export class PosStore extends WithLazyGetterTrap {
             }
             list = this.getProductsBySearchWord(
                 searchWord,
-                isSelectedCategory ? this.selectedCategory.associatedProducts : allProducts
+                this.selectedCategory?.id ? this.selectedCategory.associatedProducts : allProducts
             );
         } else {
             this._searchTriggered = false;
-            if (isSelectedCategory) {
+            if (this.selectedCategory?.id) {
                 list = this.selectedCategory.associatedProducts;
             } else {
                 list = allProducts;
@@ -2444,7 +2443,11 @@ export class PosStore extends WithLazyGetterTrap {
             filteredList.push(p);
         }
 
-        if (!isSearchByWord && !isSelectedCategory && this.areAllProductsSpecial(filteredList)) {
+        if (
+            !isSearchByWord &&
+            !this.selectedCategory?.id &&
+            this.areAllProductsSpecial(filteredList)
+        ) {
             return [];
         }
 

addons/point_of_sale/static/tests/unit/data/base_data.js
@@ -221,7 +221,30 @@ export const baseData = {
         },
     ],
     "pos.printer": [],
-    "pos.category": [],
+    "pos.category": [
+        {
+            id: 1,
+            name: "Category 1",
+            parent_id: false,
+            child_ids: [],
+            has_image: false,
+            color: 0,
+            sequence: 0,
+            hour_until: 0.0,
+            hour_after: 24.0,
+        },
+        {
+            id: 1,
+            name: "Category 2",
+            parent_id: false,
+            child_ids: [],
+            has_image: false,
+            color: 0,
+            sequence: 0,
+            hour_until: 0.0,
+            hour_after: 24.0,
+        },
+    ],
     "pos.bill": [
         {
             id: 1,
@@ -396,7 +419,7 @@ export const baseData = {
             display_name: "TEST",
             standard_price: 0,
             categ_id: false,
-            pos_categ_ids: [],
+            pos_categ_ids: [1],
             taxes_id: [1],
             barcode: false,
             name: "TEST",
@@ -431,7 +454,7 @@ export const baseData = {
             display_name: "TEST 2",
             standard_price: 0,
             categ_id: false,
-            pos_categ_ids: [],
+            pos_categ_ids: [2],
             taxes_id: [2],
             barcode: false,
             name: "TEST 2",

addons/point_of_sale/static/tests/unit/services/pos_service.test.js
@@ -28,4 +28,24 @@ describe("pos_store.js", () => {
         expect(order.tip_amount).toBe(50);
         expect(order.lines.length).toBe(3); // 2 original lines + 1 tip line
     });
+
+    test("productsToDisplay", async () => {
+        const store = await setupPosEnv();
+        store.selectedCategory = store.models["pos.category"].get(1);
+        let products = store.productsToDisplay;
+        expect(products.length).toBe(1);
+        expect(products[0].id).toBe(5);
+        expect(store.selectedCategory.id).toBe(1);
+        store.selectedCategory = store.models["pos.category"].get(1);
+        store.searchProductWord = "TEST";
+        products = store.productsToDisplay;
+        expect(products.length).toBe(2);
+        expect(products[0].id).toBe(5);
+        expect(products[1].id).toBe(6);
+        expect(store.selectedCategory).toBe(undefined);
+        store.searchProductWord = "TEST 2";
+        products = store.productsToDisplay;
+        expect(products.length).toBe(1);
+        expect(products[0].id).toBe(6);
+    });
 });