51740188f61025b2d63e58624e1d2d6536e4ad66
Maxime de Neuville <mane@odoo.com>
2025-07-16 08:01:47 +0000

    [FIX] sale_project,sale_pdf_quote_builder: fix wrong dependency
    * Steps to reproduce:
    1. Install sale_project in a new database
    2. Uninstall and reinstall sale_pdf_quote_builder (using test_module_operations)
    
    * Traceback:
    Element '<page name="pdf_quote_builder">' cannot be located in parent view
    
    * Issue: sale_project looks for pdf_quote_builder in the view_order_form.
    That element is introduced by pdf_quote_builder in sale_order_form_inherit_sale_pdf_quote_builder but there is no dependency
    between the two (sale_pdf_quote_builder is just auto_install on sale_management which is a dependency of sale_project).
    So if sale_pdf_quote_builder is uninstalled sale_project is not touched, and the view is broken.
    
    * Fix:
    We replace the use of the xpath in the form view of our widget (sale_project) by a context key to hide the pdf quote builder conditionally.
    
    X-original-commit: b88414d2965ed8e1f9216047e6a1bb2b88515e73

--- addons/sale_project/static/src/components/so_line_create_button/so_line_create_button.js
+++ addons/sale_project/static/src/components/so_line_create_button/so_line_create_button.js
@@ -26,6 +26,7 @@ export class SoLineCreateButton extends Component {
                 form_view_ref: context.so_form_view_ref,
                 default_company_id: context.default_company_id || user.activeCompany.id,
                 default_user_id: user.userId,
+                hide_pdf_quote_builder: true,
             },
             onRecordSaved: async (rec) => {
                 const service_line_id = await rec.model.orm.call(
