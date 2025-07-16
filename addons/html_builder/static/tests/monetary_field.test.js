1252714e36e5a2c19adad032238776664e572458
Sébastien (blse) <blse@odoo.com>
2025-07-16 11:43:18 +0200

    [FIX] html_builder: select the amount when clicking on monetary field
    The following behavior has been lost with the initial [website builder
    refactor]: When clicking on a monetary field (including on the currency
    sign), the amount gets selected
    
    [website builder refactor]: 3ced5e9232d3c5f71259f1f2b692416932e3ebd2
    task-4367641
    
    Part-of: odoo/odoo#222398
    Signed-off-by: Colin Louis (loco) <loco@odoo.com>
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/html_builder/static/tests/monetary_field.test.js
+++ addons/html_builder/static/tests/monetary_field.test.js
@@ -1,5 +1,6 @@
 import { expect, test } from "@odoo/hoot";
 import { setupHTMLBuilder } from "./helpers";
+import { click, queryOne } from "@odoo/hoot-dom";
 
 test("should not allow edition of currency sign of monetary fields", async () => {
     await setupHTMLBuilder(
@@ -9,4 +10,18 @@ test("should not allow edition of currency sign of monetary fields", async () =>
     );
     expect(":iframe span[data-oe-type]").toHaveProperty("isContentEditable", false);
     expect(":iframe span.oe_currency_value").toHaveProperty("isContentEditable", true);
+});
+
+test("clicking on the monetary field should select the amount", async () => {
+    const { editor } = await setupHTMLBuilder(
+        `<span data-oe-model="product.template" data-oe-id="9" data-oe-field="list_price" data-oe-type="monetary" data-oe-expression="product.list_price">
+            $<span class="span-in-currency"/>&nbsp;<span class="oe_currency_value">750.00</span>
+        </span>`
+    );
+    await click(":iframe span.span-in-currency");
+    expect(
+        editor.shared.selection.areNodeContentsFullySelected(
+            queryOne(":iframe span.oe_currency_value")
+        )
+    ).toBe(true, { message: "value of monetary field is selected" });
 });
