bd075284257e9e93920deb0de2db0bd6ee160fad
romo <romo@odoo.com>
2025-07-16 09:20:22 +0000

    [FIX] html_builder, website: fix font family picker with reset/remove button
    - Remove the cloud icon from the system font,
    
    - Create a new component to simplify the definition of Font Family
    Picker elements with a remove button to avoid duplicated infos,
    
    - Implement the remove / reset button and use different icons for both
    action to avoid confusion for the user. The buttons are now reactive
    to help identify when a font is specified or when it is an inherited
    value.
    
    task-4367641
    
    closes odoo/odoo#220206
    
    X-original-commit: 1b52f7aac4bf76e28e05ccd2b45374c60d9bfb61
    Signed-off-by: Robin Lejeune (role) <role@odoo.com>
    Signed-off-by: Romaric Moyeuvre (romo) <romo@odoo.com>

--- addons/website/static/src/builder/plugins/customize_website_plugin.js
+++ addons/website/static/src/builder/plugins/customize_website_plugin.js
@@ -42,7 +42,6 @@ export class CustomizeWebsitePlugin extends Plugin {
             SwitchThemeAction,
             AddLanguageAction,
             CustomizeBodyBgTypeAction,
-            RemoveFontAction,
             CustomizeButtonStyleAction,
             WebsiteConfigAction,
             PreviewableWebsiteConfigAction,
@@ -516,23 +515,6 @@ export class CustomizeBodyBgTypeAction extends BuilderAction {
                         this.dispatchTo("trigger_dom_updated");
                     })
                     .finally(() => this.services.ui.unblock());
-            },
-        });
-    }
-}
-
-export class RemoveFontAction extends BuilderAction {
-    static id = "removeFont";
-    static dependencies = ["builderActions"];
-    setup() {
-        this.preview = false;
-    }
-    async apply({ params }) {
-        // TODO
-        const getAction = this.dependencies.builderActions.getAction;
-        await getAction("customizeWebsiteVariable").load({
-            params: {
-                mainParam: params.variable,
             },
         });
     }
