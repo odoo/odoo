a426273f0b5dca57aaae03150d70f2bb156cef6b
Akash Parmar <akap@odoo.com>
2025-07-16 14:38:07 +0530

    [IMP] mail: composer stays open while closing full composer
    Before this commit:
    
    In Chatter, closing the full composer also closes the basic composer
    
    After this commit:
    
    Closing the full composer doesn't close the basic composer anymore
    
    task-4780706

--- addons/mail/static/src/chatter/web/chatter_patch.js
+++ addons/mail/static/src/chatter/web/chatter_patch.js
@@ -358,7 +358,6 @@ patch(Chatter.prototype, {
     },
 
     onCloseFullComposerCallback(isDiscard) {
-        this.toggleComposer();
         super.onCloseFullComposerCallback();
         if (!isDiscard) {
             this.reloadParentView();
