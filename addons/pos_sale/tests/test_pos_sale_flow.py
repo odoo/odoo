e770561f9b53ca8b86639d31124532e148898973
Hadi El Yakhni <hael@odoo.com>
2025-07-17 15:10:14 +0000

    [FIX] pos_sale: test reading the updated order lines when settling
    This PR just forward ports the test from the
    a4fbcc3717f4c3cfd1350eb52e58a37e713dbc3b.
    
    No need anymore for the fix from the before mentioned commit, as it's
    already fixed in saas-18.2.
    
    closes odoo/odoo#220813
    
    X-original-commit: 624bee6b869114e679ace9c3c0d71b49942487d8
    Signed-off-by: Stéphane Vanmeerhaeghe (stva) <stva@odoo.com>
    Signed-off-by: Hadi El Yakhni (hael) <hael@odoo.com>
    Co-authored-by: Arthur Nanson <artn@odoo.com>

--- addons/pos_sale/tests/test_pos_sale_flow.py
+++ addons/pos_sale/tests/test_pos_sale_flow.py
@@ -1414,3 +1414,23 @@ class TestPoSSale(TestPointOfSaleHttpCommon):
         self.assertEqual(pos_order_b.amount_total, 10, "PoS order amount should be 10 with the tax override 2")
         self.assertEqual(pos_order_b.amount_tax, 0, "PoS order untaxed amount should be 10 with the tax override 2")
         self.assertEqual(pos_order_b.lines[0].tax_ids, tax_override_2, "PoS order should have the tax override 2")
+
+    def test_quantity_updated_settle(self):
+        """
+        Tests that the quantity is updated when partially settling an order, so that the
+        settle displays the right amount that still needs to be settled.
+        """
+        product_a = self.env['product.product'].create({
+            'name': 'Product A',
+            'lst_price': 10.0,
+        })
+        self.env['sale.order'].sudo().create({
+            'partner_id': self.env['res.partner'].create({'name': 'Test Partner'}).id,
+            'order_line': [(0, 0, {
+                'product_id': product_a.id,
+                'product_uom_qty': 5,
+                'price_unit': product_a.lst_price,
+            })]
+        })
+        self.main_pos_config.open_ui()
+        self.start_pos_tour('test_quantity_updated_settle', login="accountman")
