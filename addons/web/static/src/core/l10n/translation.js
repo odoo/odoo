9dd6fae9399556baf363575c62cb3041f2eff289
Mathieu Duckerts-Antoine <dam@odoo.com>
2025-07-17 11:42:19 +0200

    [FIX] web: remove dead code
    We no longer use jquery timeago
    
    closes odoo/odoo#220596
    
    Related: odoo/enterprise#91035
    Signed-off-by: Louis Wicket (wil) <wil@odoo.com>

--- addons/web/static/src/core/l10n/translation.js
+++ addons/web/static/src/core/l10n/translation.js
@@ -83,25 +83,6 @@ class LazyTranslatedString extends String {
     }
 }
 
-/*
- * Setup jQuery timeago:
- * Strings in timeago are "composed" with prefixes, words and suffixes. This
- * makes their detection by our translating system impossible. Use all literal
- * strings we're using with a translation mark here so the extractor can do its
- * job.
- */
-_t("less than a minute ago");
-_t("about a minute ago");
-_t("%d minutes ago");
-_t("about an hour ago");
-_t("%d hours ago");
-_t("a day ago");
-_t("%d days ago");
-_t("about a month ago");
-_t("%d months ago");
-_t("about a year ago");
-_t("%d years ago");
-
 /**
  * Load the installed languages long names and code
  *
