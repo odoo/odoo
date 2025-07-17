284da60433701ea448574c8d096cc6dbefc07c9a
alap-odoo <alap@odoo.com>
2025-07-17 18:26:03 +0530

    [ADD] website_practice: added module to verify asset loaded
    Created a module and added the popover instance if the assets for
    that are loaded successfully.

---
+++ addons/website_practice/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Website Practice",
+    "version": "1.0",
+    "depends": ["website"],
+    "category": "Website",
+    "data": [
+        "views/snippets/s_dynamic_snippet.xml",
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            "website_practice/static/src/js/practice_task.js",
+        ],
+    },
+    "application": True,
+    "sequence": 1,
+    "installable": True,
+}
