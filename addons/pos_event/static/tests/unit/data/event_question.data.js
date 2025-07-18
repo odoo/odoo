e221856f5a4805c0313cdfefd47a423d7e33628c
paan-odoo <paan@odoo.com>
2025-07-18 04:48:31 +0000

    [IMP] pos_event: add hoot test cases
    In this commit:
    
    - We are adding hoot test cases to verify that components return values
      according to the passed parameters for components like
      `event_configurator_popup`, `event_registration_popup`, and
      `event_slot_selection_popup`.
    - Also, we are adding a test for the `addProductToOrder` method available in the
      `product_screen` component to ensure the order line is added to the order
      seamlessly.
    - Then, we are adding a test for the `createDummyProductForEvents` method
      available in the `pos_store` service to verify that the products are created for
      all the loaded events in the POS.
    
    task-4945631
    
    closes odoo/odoo#227525
    
    X-original-commit: 43da1bc964e328bfd1703de1b7697147b39152b5
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Anantkumar Parmar (paan) <paan@odoo.com>

--- addons/pos_event/static/tests/unit/data/event_question.data.js
+++ addons/pos_event/static/tests/unit/data/event_question.data.js
@@ -9,14 +9,57 @@ export class EventQuestion extends models.ServerModel {
         return [
             "title",
             "question_type",
-            "event_type_id",
-            "event_id",
+            "event_type_ids",
+            "event_ids",
             "sequence",
             "once_per_order",
             "is_mandatory_answer",
             "answer_ids",
         ];
     }
+
+    _records = [
+        {
+            id: 1,
+            title: "Name",
+            question_type: "name",
+            event_ids: [1],
+            sequence: 1,
+            once_per_order: false,
+            is_mandatory_answer: true,
+            answer_ids: [],
+        },
+        {
+            id: 2,
+            title: "Email",
+            question_type: "email",
+            event_ids: [1],
+            sequence: 2,
+            once_per_order: false,
+            is_mandatory_answer: true,
+            answer_ids: [],
+        },
+        {
+            id: 3,
+            title: "Phone",
+            question_type: "phone",
+            event_ids: [1],
+            sequence: 3,
+            once_per_order: false,
+            is_mandatory_answer: true,
+            answer_ids: [],
+        },
+        {
+            id: 4,
+            title: "Gender",
+            question_type: "simple_choice",
+            event_ids: [1],
+            sequence: 4,
+            once_per_order: false,
+            is_mandatory_answer: false,
+            answer_ids: [1, 2],
+        },
+    ];
 }
 
 patch(hootPosModels, [...hootPosModels, EventQuestion]);
