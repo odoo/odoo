7ce0a4f5aa433a2c8707f1c3c2e583437fa09377
Krzysztof Magusiak (krma) <krma@odoo.com>
2025-07-18 14:11:15 +0000

    [IMP] mail: _compute_sql_activity_state

--- odoo/tools/query.py
+++ odoo/tools/query.py
@@ -213,10 +213,12 @@ class Query:
 
         sql_args = map(SQL, args) if args else [SQL.identifier(self.table, 'id')]
         return SQL(
-            "(%s%s%s)",
+            "(%s%s%s%s%s)",
             SQL("SELECT %s", SQL(", ").join(sql_args)),
             SQL(" FROM %s", self.from_clause),
             SQL(" WHERE %s", self.where_clause) if self._where_clauses else SQL(),
+            SQL(" GROUP BY %s", self.groupby) if self.groupby else SQL(),
+            SQL(" HAVING %s", self.having) if self.having else SQL(),
         )
 
     def get_result_ids(self) -> tuple[int, ...]:
