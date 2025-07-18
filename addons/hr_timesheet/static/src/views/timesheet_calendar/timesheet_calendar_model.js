bc8175b497a37b2a2336f7687a141afd31ec60e7
Vincent Larcin <vila@odoo.com>
2025-07-18 17:18:19 +0200

    [IMP] hr_timesheet: add notification when creating timesheets in calendar
    When creating timesheets using the multi create feature of the calendar view, it can happen that timesheets
    cannot be created for some or even any of the selected days.
    It is also possible that the created timesheets won't be displayed, because they don't fit the domain of the view or the action.
    
    This commit adds some visual feedback in the form of two notifications to tell the user whether at least one timesheet has been created from their action.
    
    Task-4916256

--- addons/hr_timesheet/static/src/views/timesheet_calendar/timesheet_calendar_model.js
+++ addons/hr_timesheet/static/src/views/timesheet_calendar/timesheet_calendar_model.js
@@ -8,7 +8,7 @@ export class TimesheetCalendarModel extends TimesheetCalendarMyTimesheetsModel {
         this.notification = services.notification;
     }
 
-    multiCreateRecords(dates) {
+    async multiCreateRecords(dates) {
         const [section] = this.filterSections;
         if (section.filters.filter((filter) => filter.active).length === 0) {
             this.notification.add(_t("Choose an employee to create their timesheet."), {
@@ -16,6 +16,16 @@ export class TimesheetCalendarModel extends TimesheetCalendarMyTimesheetsModel {
             });
             return;
         }
-        super.multiCreateRecords(dates);
+        const records = await super.multiCreateRecords(dates);
+        if (records.length) {
+            this.notification.add(_t("Timesheets successfully created."), {
+                type: "success",
+            });
+        } else {
+            this.notification.add(_t("Could not create timesheets for any of the selected days."), {
+                type: "danger",
+            });
+        }
+        return records;
     }
 }
