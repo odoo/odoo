53a043ba06dadf1064c214d0a9b33b663dc898a5
Romain ROSANO (rros) <rros@odoo.com>
2025-07-18 16:28:10 +0200

    [IMP] {website_}event{_sale},test_event_full: adding a limit on orderable tickets
    **Currently**
    
    When purchasing tickets online, a selectbox limits the value between 0 and 9 per
    ticket type.
    
    **Implementation**
    
    We have added a new field since the event was created, allowing this old hardcoded
    maximum limit to be modified. For performance and security reasons, this limit
    cannot exceed EVENT_MAX_TICKETS. The value 0 also indicates this maximum limit.
    This limit also applies to slots, if applicable. The selectbox has been replaced
    with a numeric spinner (inputbox with -/+ buttons).
    
    **Tests**
    
    Some Website Event tours have been modified as a result of the change from the
    selectbox to the inputbox. The tours have also been completed to verify the new
    limits according to the configured constraints.
    
    task-4822074
    
    Part-of: odoo/odoo#219702
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/website_event/static/src/interactions/website_event_ticket_details.js
+++ addons/website_event/static/src/interactions/website_event_ticket_details.js
@@ -12,21 +12,68 @@ export class TicketDetails extends Interaction {
         _envBus: {
             "t-on-websiteEvent.enableSubmit": () => this.buttonDisabled = false,
         },
-        ".form-select": {
-            "t-on-change": () => {}, // use updateContent() to enable/disable submit button
+        "button[data-increment-type='minus']": {
+            "t-on-click": (ev) => this.onClick(ev, -1),
+        },
+        "button[data-increment-type='plus']": {
+            "t-on-click": (ev) => this.onClick(ev, 1),
+        },
+        ".o_wevent_input_nb_tickets": {
+            "t-on-input": this.onInput, // use updateContent() to enable/disable submit button
         },
         ".a-submit": {
             "t-on-click.prevent.stop": this.onSubmitClick,
-            "t-att-disabled": () => this.noTicketsOrdered || this.buttonDisabled ? "disabled" : false,
+            "t-att-disabled": () => this.moreTicketsOrderedThanExpected || this.noTicketsOrdered || this.buttonDisabled ? "disabled" : false,
         },
     };
 
     get noTicketsOrdered() {
         return Boolean(
-            !Array.from(this.el.querySelectorAll("select").values()).find(
-                (select) => select.value > 0
+            !Array.from(this.el.querySelectorAll(".o_wevent_input_nb_tickets")).find(
+                (input) => Number.isNaN(parseInt(input.value)) ? false : parseInt(input.value) > 0
             )
         );
+    }
+
+    get moreTicketsOrderedThanExpected() {
+        return Boolean(
+            Array.from(this.el.querySelectorAll(".o_wevent_input_nb_tickets")).find(
+                (input) => Number.isNaN(parseInt(input.value)) ? true : input.max < parseInt(input.value)
+            )
+        );
+    }
+
+    /**
+     * @param {InputEvent} ev
+     */
+    async onInput(ev) {
+        const inputComponent = ev.target;
+        const maximumBound = inputComponent.max;
+        inputComponent.value = Math.min(inputComponent.value.replace(/[^0-9]/g, ''), maximumBound);
+        
+        const spinner_buttons = this.el.querySelectorAll(`button[data-input-name=${inputComponent.name}]`);
+        spinner_buttons[0].disabled = parseInt(inputComponent.value) <= 0;
+        spinner_buttons[1].disabled = parseInt(inputComponent.value) >= maximumBound;
+
+        // Display/hide maximum ticket quantity if reached
+        this.el.querySelector(`p[name=${inputComponent.name}]`).hidden = parseInt(inputComponent.value) < maximumBound;
+    }
+
+    /**
+     * @param {MouseEvent} ev
+     */
+    onClick(ev, incrementValue) {
+        const inputComponent = this.el.querySelector(`input[name=${ev.currentTarget.dataset.inputName}]`);
+        const maximumBound = inputComponent.max;
+        const inputValue = parseInt(inputComponent.value);
+        if (0 <= inputValue && inputValue <= maximumBound) {
+            inputComponent.value = Math.max(0, Math.min(inputValue + incrementValue, maximumBound));
+        } else {
+            inputComponent.value = inputValue < 0 ? 0 : maximumBound;
+        }
+
+        // Triggers an input event to update the “Register” button accessibility
+        inputComponent.dispatchEvent(new Event("input"))
     }
 
     /**
