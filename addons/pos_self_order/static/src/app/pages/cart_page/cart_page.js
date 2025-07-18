3287eff012f827ae8a3b7554f69b9dbe1e21cad9
parp-odoo <parp@odoo.com>
2025-07-18 18:06:28 +0530

    [IMP] pos_self_order: introduce Hoot tests
    - Isolated `printer` service into a standalone file to avoid naming conflicts
      with `point_of_sale` during tests.
    - Introduced `setupSelfPosEnv` to mock and bootstrap the self-order
      testing environment.
    - Add Hoot test coverage for core components, models, and service logic
    
    Task-4945633
    
    closes odoo/odoo#220537
    
    Signed-off-by: Stéphane Vanmeerhaeghe (stva) <stva@odoo.com>

--- addons/pos_self_order/static/src/app/pages/cart_page/cart_page.js
+++ addons/pos_self_order/static/src/app/pages/cart_page/cart_page.js
@@ -230,6 +230,7 @@ export class CartPage extends Component {
         const card = event?.target.closest(".product-cart-item");
         if (!card) {
             doRemoveLine();
+            return;
         }
         const onAnimationEnd = () => {
             card.removeEventListener("animationend", onAnimationEnd);
@@ -252,7 +253,7 @@ export class CartPage extends Component {
         increase ? line.qty++ : line.qty--;
 
         if (line.qty <= 0) {
-            this.removeLine(line.uuid);
+            this.removeLine(line);
         }
     }
 
