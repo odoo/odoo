a3df185d2875d5e19c9b39c62cf3dd60035a1dd4
shag-odoo <shag@odoo.com>
2025-07-21 07:19:44 +0000

    [IMP] pos_hr: add HOOT tests for pos_hr module
    This commit introduces HOOT test coverage for the pos_hr module:
    
    - Added tests for primary model :patched pos.order
    - Added tests for patched pos_store.js.
    - Added component tests for Navbar, CashPopup, PaymentScreen, LoginScreen
    
    task-4945630
    
    closes odoo/odoo#222987
    
    X-original-commit: a8e31a4d8019dfbbabbc10fd83890278711beaa5
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Shubham Agarwal (shag) <shag@odoo.com>

---
+++ addons/pos_hr/static/tests/unit/components/screens/payment_screen.test.js
@@ -0,0 +1,20 @@
+import { test, expect, describe } from "@odoo/hoot";
+import { setupPosEnv } from "@point_of_sale/../tests/unit/utils";
+import { PaymentScreen } from "@point_of_sale/app/screens/payment_screen/payment_screen";
+import { mountWithCleanup } from "@web/../tests/web_test_helpers";
+import { definePosModels } from "@point_of_sale/../tests/unit/data/generate_model_definitions";
+
+definePosModels();
+
+describe("payment_screen.js", () => {
+    test("validateOrder", async () => {
+        const store = await setupPosEnv();
+        const orderUuid = store.getOrder().uuid;
+        const comp = await mountWithCleanup(PaymentScreen, {
+            props: { orderUuid },
+        });
+        await comp.validateOrder();
+        const order = store.getOrder();
+        expect(order.employee_id.id).toBe(2);
+    });
+});
