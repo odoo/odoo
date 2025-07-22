49aab23d562ce7a8069ca84340fe69738e2f7055
Aaron Bohy <aab@odoo.com>
2025-07-22 11:03:24 +0200

    [REF] web: RelationalModel: add onchange warning hook
    This commit is a followup of [1] which prevented an infinite loop
    when spawning a form view (with no resId) in a dialog, for which
    the first onchange returned a warning to display in a dialog. More
    details about that flow and the underlying issue can be found in
    [1].
    
    This commit provides an alternative solution to [1], which was a
    bit error prone. Indeed, if anyone keeps the reference to the
    `dialog.add` function in the setup and use it later, it crashes.
    
    Instead, we introduce a new hook in the RelationalModel, which is
    called just before displaying the warning dialog/notification.
    This hook allows the FormController to wait for itself to be
    mounted before displaying the notification/dialog, thus avoiding
    the infinite loop.
    
    [1] https://github.com/odoo/odoo/pull/213742
    
    closes odoo/odoo#219853
    
    Signed-off-by: Lucas Perais (lpe) <lpe@odoo.com>

--- addons/web/static/src/model/model.js
+++ addons/web/static/src/model/model.js
@@ -8,7 +8,6 @@ import { buildSampleORM } from "./sample_server";
 
 import {
     EventBus,
-    onMounted,
     onWillStart,
     onWillUnmount,
     onWillUpdateProps,
@@ -94,30 +93,6 @@ function getSearchParams(props) {
     return params;
 }
 
-function usePostMountedServices(services) {
-    if (services.dialog) {
-        services.dialog = Object.create(services.dialog);
-        const dialogAddOrigin = services.dialog.add;
-        let dialogRequests = [];
-        services.dialog.add = (...args) => {
-            const index = dialogRequests.push(args);
-            return () => {
-                dialogRequests[index] = null;
-            };
-        };
-        onMounted(() => {
-            services.dialog.add = dialogAddOrigin;
-            for (const req of dialogRequests) {
-                if (req) {
-                    dialogAddOrigin(...req);
-                }
-            }
-            dialogRequests = null;
-        });
-    }
-    return services;
-}
-
 /**
  * @template {typeof Model} T
  * @param {T} ModelClass
@@ -128,12 +103,11 @@ function usePostMountedServices(services) {
  */
 export function useModel(ModelClass, params, options = {}) {
     const component = useComponent();
-    let services = {};
+    const services = {};
     for (const key of ModelClass.services) {
         services[key] = useService(key);
     }
     services.orm = services.orm || useService("orm");
-    services = usePostMountedServices(services);
     const model = new ModelClass(component.env, params, services);
     onWillStart(async () => {
         await options.beforeFirstLoad?.();
@@ -157,12 +131,11 @@ export function useModelWithSampleData(ModelClass, params, options = {}) {
     if (!(ModelClass.prototype instanceof Model)) {
         throw new Error(`the model class should extend Model`);
     }
-    let services = {};
+    const services = {};
     for (const key of ModelClass.services) {
         services[key] = useService(key);
     }
     services.orm = services.orm || useService("orm");
-    services = usePostMountedServices(services);
 
     if (!("isAlive" in params)) {
         params.isAlive = () => status(component) !== "destroyed";
