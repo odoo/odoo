ec2285fada3be3f3fe5e359243c2dc65cc65f5bd
Walid (wasa) <wasa@odoo.com>
2025-07-22 09:44:14 +0200

    [FIX] html_editor: remove checkmark when deleting checked list item
    Problem:
    After commit 02ae5a645a80e6088a596b6273e70b5476e79e88, pressing
    backspace on a `li` adds the `oe-nested` class. If that `li` is a
    checked checkbox item, only the checkbox is removed, but the
    checkmark remains visible.
    
    Solution:
    Always remove the `o_checked` class when converting the list item to
    `oe-nested`.
    
    Steps to reproduce:
    - Add a checklist
    - Check an item
    - Press backspace to remove the checkbox
    -> The checkmark remains visible, even though the box is gone
    
    opw-4953981

--- addons/html_editor/static/tests/list/delete_backward.test.js
+++ addons/html_editor/static/tests/list/delete_backward.test.js
@@ -931,6 +931,14 @@ describe("Selection collapsed", () => {
                 });
             });
 
+            test("should remove the checkmark when the list item marker is deleted", async () => {
+                await testEditor({
+                    contentBefore: '<ul class="o_checklist"><li class="o_checked">[]</li></ul>',
+                    stepFunction: deleteBackward,
+                    contentAfter: '<ul class="o_checklist"><li class="oe-nested">[]</li></ul>',
+                });
+            });
+
             describe("should merge a list item with its previous list item", () => {
                 test("should merge a list item with its previous list item (1)", async () => {
                     await testEditor({
@@ -2726,7 +2734,8 @@ describe("Selection not collapsed", () => {
                         contentBefore:
                             '<ul class="o_checklist"><li class="o_checked">ab[cd</li></ul><ul><li>ef]gh</li></ul>',
                         stepFunction: deleteBackward,
-                        contentAfter: '<ul class="o_checklist"><li class="o_checked">ab[]gh</li></ul>',
+                        contentAfter:
+                            '<ul class="o_checklist"><li class="o_checked">ab[]gh</li></ul>',
                     });
                 });
                 test("should delete across an checklist list and an unordered list (2)", async () => {
@@ -2735,7 +2744,8 @@ describe("Selection not collapsed", () => {
                         contentBefore:
                             '<ul class="o_checklist"><li class="o_checked">ab]cd</li></ul><ul><li>ef[gh</li></ul>',
                         stepFunction: deleteBackward,
-                        contentAfter: '<ul class="o_checklist"><li class="o_checked">ab[]gh</li></ul>',
+                        contentAfter:
+                            '<ul class="o_checklist"><li class="o_checked">ab[]gh</li></ul>',
                     });
                 });
                 test("should delete across an checklist list and an unordered list (3)", async () => {
