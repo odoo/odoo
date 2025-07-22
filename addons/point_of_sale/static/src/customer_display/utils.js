9b7d0cbb17346fe1b43887646a3fda8e892b0cb6
Louis Travaux <lotr@odoo.com>
2025-07-22 13:26:13 +0000

    [FIX] pos_iot: update customer display using websocket
    We fixed the customer display not opening and displaying a traceback when the
    Iot Box was not reachable, by calling the action via iot_http, fallbacking on
    websocket when needed.
    We also removed the "customer display with IoT Box" logic from the PoS to
    the module pos_iot.
    
    Enterprise PR: odoo/enterprise#90657
    
    closes odoo/odoo#220234
    
    X-original-commit: 82226c834cc7feee7f80c3d97006acfb84042550
    Related: odoo/enterprise#90849
    Signed-off-by: Max Whale (mawh) <mawh@odoo.com>
    Signed-off-by: Louis Travaux (lotr) <lotr@odoo.com>

--- addons/point_of_sale/static/src/customer_display/utils.js
+++ addons/point_of_sale/static/src/customer_display/utils.js
@@ -1,24 +1,4 @@
-import { DeviceController } from "@iot_base/device_controller";
-import { _t } from "@web/core/l10n/translation";
 import { useService } from "@web/core/utils/hooks";
-
-export function openProxyCustomerDisplay(displayDeviceIp, pos, notificationService = null) {
-    if (!displayDeviceIp) {
-        return;
-    }
-
-    pos.hardwareProxy.deviceControllers.customerDisplay ??= new DeviceController(
-        pos.iotLongpolling,
-        { iot_ip: displayDeviceIp, identifier: "display" }
-    );
-
-    notificationService?.add(_t("Connecting to the IoT Box"));
-    pos.hardwareProxy.deviceControllers.customerDisplay.action({
-        action: "open",
-        access_token: pos.config.access_token,
-        pos_id: pos.config.id,
-    });
-}
 
 export function useSingleDialog() {
     let close = null;
