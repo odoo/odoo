94a42ebbd99e7dddfbb0c15bec8961d149fe143f
Pierre Lamotte (pila) <pila@odoo.com>
2025-07-22 14:35:00 +0000

    [FIX] payment_razorpay_oauth: add constraints on credentials before publication
    ## Version
    17.0+
    No OAuth in 16.0
    
    ## Issue
    Razorpay can be enabled and published without being properly configured.
    
    ## Steps to reproduce
    - Go to `Payment Providers` and open `Razorpay`;
    - Set the state to `Enabled` and publish without providing any credentials.
    
    ## Fix
    Added constraints and revised account linking logic, similar to Stripe.
    Based on 1b6a7aeb245f0007675943ed7aacf5064e46fe61 suggested by ALSH.
    
    opw-4922315
    
    closes odoo/odoo#223280
    
    X-original-commit: 9ecd1b35e87f457fc37eff0e35e979b5940cad4d
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>
    Signed-off-by: Pierre Lamotte (pila) <pila@odoo.com>

--- addons/payment_razorpay/models/payment_provider.py
+++ addons/payment_razorpay/models/payment_provider.py
@@ -6,8 +6,8 @@ import uuid
 from datetime import timedelta
 from urllib.parse import urlencode
 
-from odoo import _, fields, models
-from odoo.exceptions import RedirectWarning
+from odoo import _, api, fields, models
+from odoo.exceptions import RedirectWarning, ValidationError
 from odoo.http import request
 
 from odoo.addons.payment.logging import get_payment_logger
@@ -62,6 +62,22 @@ class PaymentProvider(models.Model):
                 lambda c: c.name in const.SUPPORTED_CURRENCIES
             )
         return supported_currencies
+
+    # === CONSTRAINT METHODS === #
+
+    @api.constrains('state')
+    def _check_razorpay_credentials_are_set_before_enabling(self):
+        """ Check that the Razorpay credentials are valid when the provider is enabled.
+
+        :raise ValidationError: If the Razorpay credentials are not valid.
+        """
+        for provider in self.filtered(lambda p: p.code == 'razorpay' and p.state != 'disabled'):
+            if not provider.razorpay_account_id:
+                if not provider.razorpay_key_id or not provider.razorpay_key_secret:
+                    raise ValidationError(_(
+                        "Razorpay credentials are missing. Click the \"Connect\" button to set up"
+                        " your account"
+                    ))
 
     # === CRUD METHODS === #
 
