d60bf8875b130c531efac053eda4861f583d093f
Arthur Nanson <artn@odoo.com>
2025-07-22 12:53:51 +0000

    [FIX] pos_self_order: prevent display of all variant with combo
    **Problem:**
    When we have a combo product that has a variant product, if we order it
    from the Kiosk, all the variants for that product will be displayed.
    The extra price will also be added, even though it is not specified on
    the combo definition.
    
    **Steps to reproduce:**
    -Create a combo that has a variant product (like aluminium chair)
    -Go to the Kiosk and order said combo
    -In the combo page, click on the variant, all the possible variants are displayed
    
    **Why the fix:**
    When creating a combo, we should specify which product variant we want,
    so that only those are displayed on the combo selection page. The way
    it is done right now, every variant is displayed whether we want them
    or not, without a way to refrain them from being displayed.
    After this commit, if the variants are specified on the combo, only
    those are displayed in the Kiosk once we click on the combo.
    If we specify a product that has variants in the combo, all variants
    will still be displayed when clicking the product in the Kiosk, as
    it was before.
    
    opw-4924019
    
    closes odoo/odoo#225611
    
    X-original-commit: 5db2befba63ae9002c39f83ea5baa771d59cffd2
    Signed-off-by: Arthur Nanson (artn) <artn@odoo.com>

--- addons/pos_self_order/static/tests/tours/self_order_combo_tour.js
+++ addons/pos_self_order/static/tests/tours/self_order_combo_tour.js
@@ -3,6 +3,7 @@ import * as Utils from "@pos_self_order/../tests/tours/utils/common";
 import * as CartPage from "@pos_self_order/../tests/tours/utils/cart_page_util";
 import * as ProductPage from "@pos_self_order/../tests/tours/utils/product_page_util";
 import * as ConfirmationPage from "@pos_self_order/../tests/tours/utils/confirmation_page_util";
+import * as LandingPage from "@pos_self_order/../tests/tours/utils/landing_page_util";
 
 registry.category("web_tour.tours").add("self_combo_selector", {
     steps: () => [
@@ -69,5 +70,25 @@ registry.category("web_tour.tours").add("self_combo_selector_category", {
         Utils.clickBtn("Order"),
         Utils.clickBtn("Ok"),
         Utils.checkIsNoBtn("Order Now"),
+    ],
+});
+
+registry.category("web_tour.tours").add("test_product_dont_display_all_variants", {
+    steps: () => [
+        Utils.clickBtn("Order Now"),
+        LandingPage.selectLocation("Test-In"),
+        ProductPage.clickCategory("Uncategorised"),
+        ProductPage.clickProduct("Meal Combo"),
+        ProductPage.clickComboProduct("Coke always never"),
+        Utils.clickBtn("Red"),
+        Utils.clickBtn("Next"),
+        Utils.clickBtn("Add to cart"),
+        ProductPage.clickProduct("Meal Combo"),
+        ProductPage.clickComboProduct("Coke always only"),
+        Utils.clickBtn("Add to cart"),
+        ProductPage.clickProduct("Meal Combo"),
+        ProductPage.clickComboProduct("Coke never only"),
+        Utils.clickBtn("Red"),
+        Utils.clickBtn("Add to cart"),
     ],
 });
