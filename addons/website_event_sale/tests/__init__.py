384f039bfdb8f93a932e0dba1263b2ad67dc5365
Erwan <erga@odoo.com>
2025-07-22 14:10:22 -0400

    [FIX] website_event_sale: convert ticket prices into website currency
    In multi-company, tickets from different companies can be added to a single
    event, even if that event belongs to one company only.
    
    When doing so, checking the ticket prices on the website will show unconverted
    prices, displaying the prices of tickets at face value, i.e. without any
    conversion. A ticket with its price being 100€ will be displayed as 100$.
    
    Steps to reproduce the issue:
    - Have 2 companies, one in dollar, one in euro
    - Create an 'event' product in each company, with 100 as unit price
    - Create an event, add the two tickets (related to the products created above)
    - Now, on the event on the website, click on "Register".
    - See the prices of both tickets, both are shown as 100, while one of the
      ticket should've been converted from one currency to the website currency.
    
    opw-4720354

--- addons/website_event_sale/tests/__init__.py
+++ addons/website_event_sale/tests/__init__.py
@@ -5,3 +5,4 @@ from . import common
 from . import test_frontend_buy_tickets
 from . import test_website_event_sale_cart
 from . import test_website_event_sale_pricelist
+from . import test_website_event_sale_tickets
