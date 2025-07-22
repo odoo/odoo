6f73f36f016c40883ad19013e17f8c14cf24701a
Francois Vasamillet (frva) <frva@odoo.com>
2025-07-22 17:24:03 +0200

    [IMP] website_partner: adding partners page to the website

---
+++ addons/website_partner/models/website.py
@@ -0,0 +1,12 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models, _
+
+
+class Website(models.Model):
+    _inherit = "website"
+
+    def get_suggested_controllers(self):
+        suggested_controllers = super(Website, self).get_suggested_controllers()
+        suggested_controllers.append((_('Partners'), self.env['ir.http']._url_for('/partners'), 'website_partner'))
+        return suggested_controllers
