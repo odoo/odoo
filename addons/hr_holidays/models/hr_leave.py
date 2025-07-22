4a41fcc3d308784450e1cb56fadf95d8df33eeb7
alsh-odoo <alsh@odoo.com>
2025-07-22 11:50:26 +0000

    [FIX] hr_holidays: skip public holidays in leave calculation for flexible hours
    Steps to reproduce:
    
    1) Create a flexible working schedule.
    2) Assign this schedule to an employee.
    3) Create a public holiday.
    4) Create a leave request for the flexible employee on the public holiday.
    5) Notice that the duration of the leave is 1 instead of 0.
    
    Issue:
    
    Due to recent changes in the commit mentioned below,
    https://github.com/odoo/odoo/commit/a826f65c2d95b796f919023d560ec9f0801090d8#diff-38469def2f870bb866f971f57797dd7c21b6a95d52a8eae72f832f0eea2434f9R427-R433
    
    When a single-day flexible leave is taken, the duration is always
    set to the real duration. However, we do not check if the leave falls
    on a public holiday, which results in the leave being set to 1 day,
    even on holidays.
    
    For example:
    If Christmas is marked as a public holiday and an employee with
    a flexible schedule requests a one-day leave on Christmas,
    the leave is recorded with a duration of 1 day.
    
    Fix:
    Check for a public holiday on leave date; if yes, set leave duration to 0.
    
    opw-4963122
    
    X-original-commit: 415456a308c2179d91b9061a74ccddc5ae44d8a1

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -12,7 +12,7 @@ from odoo.addons.base.models.ir_model import MODULE_UNINSTALL_FLAG
 
 from odoo import api, Command, fields, models
 from odoo.addons.base.models.res_partner import _tz_get
-from odoo.addons.resource.models.utils import float_to_time, HOURS_PER_DAY
+from odoo.addons.resource.models.utils import float_to_time, HOURS_PER_DAY, Intervals
 from odoo.exceptions import AccessError, UserError, ValidationError
 from odoo.tools.float_utils import float_round, float_compare
 from odoo.tools.misc import format_date
@@ -428,11 +428,26 @@ class HrLeave(models.Model):
             if leave.employee_id:
                 # For flexible employees, if it's a single day leave, we force it to the real duration since the virtual intervals might not match reality on that day, especially for custom hours
                 if leave.employee_id.is_flexible and leave.date_to.date() == leave.date_from.date():
-                    hours = (leave.date_to - leave.date_from).total_seconds() / 3600
-                    if not leave.request_unit_hours:
+                    public_holidays = self.env['resource.calendar.leaves'].search([
+                        ('resource_id', '=', False),
+                        ('date_from', '<', leave.date_to),
+                        ('date_to', '>', leave.date_from),
+                        ('calendar_id', 'in', [False, calendar.id]),
+                        ('company_id', '=', leave.company_id.id)
+                    ])
+                    if public_holidays:
+                        public_holidays_intervals = Intervals([(ph.date_from, ph.date_to, ph) for ph in public_holidays])
+                        leave_intervals = Intervals([(leave.date_from, leave.date_to, leave)])
+                        real_leave_intervals = leave_intervals - public_holidays_intervals
+                        hours = 0
+                        for start, stop, meta in real_leave_intervals:
+                            hours += (stop - start).total_seconds() / 3600
+                    else:
+                        hours = (leave.date_to - leave.date_from).total_seconds() / 3600
+                    if not leave.request_unit_hours and not public_holidays:
                         days = 1 if not leave.request_unit_half else 0.5
                     else:
-                        days = (leave.date_to - leave.date_from).total_seconds() / 3600 / 24
+                        days = hours / 24
                 elif leave.leave_type_request_unit == 'day' and check_leave_type:
                     # list of tuples (day, hours)
                     work_time_per_day_list = work_time_per_day_mapped[(leave.date_from, leave.date_to, calendar)][leave.employee_id.id]
