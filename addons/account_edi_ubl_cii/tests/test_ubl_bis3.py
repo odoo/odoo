fae00f29d498399d6d88e4573af830b35699e077
Antoine Dupuis <andu@odoo.com>
2025-07-22 14:04:11 +0000

    [FIX] account_edi_ubl_cii: Add SellerItemIdentification and add tests
    Before the UBL refactor, the SellerItemIdentification node was populated
    with the product code. We restore this behaviour.
    
    In addition, the refactor added the StandardItemIdentification node
    without specifying the schemeID (which is required by the schematron),
    then commit f46c10f03e59e8 added `schemeID="0160"`.
    
    This commit adds tests to enforce this behaviour.
    
    task-none
    
    X-original-commit: a2bd2f7b2143645faa95da512bfb3de90befeb00
    Part-of: odoo/odoo#220681
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>

--- addons/account_edi_ubl_cii/tests/test_ubl_bis3.py
+++ addons/account_edi_ubl_cii/tests/test_ubl_bis3.py
@@ -115,6 +115,32 @@ class TestUblBis3(AccountTestInvoicingCommon):
             self.get_xml_tree_from_string(expected_content),
         )
 
+    def test_product_code_and_barcode(self):
+        self.setup_partner_as_be1(self.env.company.partner_id)
+        self.setup_partner_as_be2(self.partner_a)
+        tax_21 = self.percent_tax(21.0)
+
+        self.product_a.write({
+            'default_code': 'P123',
+            'barcode': '1234567890123',
+        })
+
+        invoice = self.env['account.move'].create({
+            'move_type': 'out_invoice',
+            'partner_id': self.partner_a.id,
+            'invoice_date': '2017-01-01',
+            'invoice_line_ids': [
+                Command.create({
+                    'product_id': self.product_a.id,
+                    'price_unit': 100.0,
+                    'tax_ids': [Command.set(tax_21.ids)],
+                }),
+            ],
+        })
+        invoice.action_post()
+        self.env['account.move.send']._generate_and_send_invoices(invoice, sending_methods=['manual'])
+        self._assert_invoice_ubl_file(invoice, 'bis3/test_product_code_and_barcode')
+
     # -------------------------------------------------------------------------
     # TAXES
     # -------------------------------------------------------------------------
