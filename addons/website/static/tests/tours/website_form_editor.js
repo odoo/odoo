786bbae655a26a59bff2c956528e5decd10111d8
Xavier Morel <xmo@odoo.com>
2025-07-22 14:06:24 +0200

    [REM] website: test case for a form key ending with `\`
    In that case Werkzeug 3.0.4 and later will drop the entire
    name (following pallets/werkzeug#2939, cf pallets/werkzeug#3032),
    we'll end up with a field named `None` on the python side, and then
    dispatching will blow up because `None` is not a valid kwarg.
    
    The exact semantics of that case are unclear (see also
    curl/curl#7789), my reading is that [RFC 7578][1] specifies
    percent-encoding and thus that should be used when encoding and
    decoding, and `\` should be irrelevant because it's neither `%` nor
    `"` so it's not a metacharacter for multipart/form-data headers.
    
    However the [whatwg living standard][2] rejects full blown
    percent-encoding, and instead uses percent-encoding on just a highly
    restricted set of inputs (which includes neither `\` nor `%`).
    
    And while it seems like we should be able to ignore RFC 6266 (the
    content-disposition header) who's to say that there are no real-world
    deployments which follow its strictures?
    
    Meh.
    
    [1]: https://datatracker.ietf.org/doc/html/rfc7578#section-2
    [2]: https://html.spec.whatwg.org/multipage/form-control-infrastructure.html#multipart-form-data
    
    Part-of: odoo/odoo#219478
    Related: odoo/enterprise#90447
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- addons/website/static/tests/tours/website_form_editor.js
+++ addons/website/static/tests/tours/website_form_editor.js
@@ -1142,7 +1142,6 @@ registerWebsitePreviewTour("website_form_special_characters", {
         run: "click",
     },
     ...addCustomField("char", "text", `Test1"'`, false),
-    ...addCustomField("char", "text", 'Test2`\\', false),
     ...clickOnSave(),
     ...essentialFieldsForDefaultFormFillInSteps,
     {
@@ -1153,10 +1152,6 @@ registerWebsitePreviewTour("website_form_special_characters", {
         content: "Complete the first added field",
         trigger: `:iframe input[name="${CSS.escape("Test1&quot;'")}"]`,
         run: "edit test1",
-    }, {
-        content: "Complete the second added field",
-        trigger: `:iframe input[name="${CSS.escape("Test2`\\")}"]`,
-        run: "edit test2",
     }, {
         content: "Click on 'Submit'",
         trigger: ":iframe a.s_website_form_send",
