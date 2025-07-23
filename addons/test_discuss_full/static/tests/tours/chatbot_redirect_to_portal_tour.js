8f24da78f60529ea0b1840e48de30e15d17ddb77
kdeb@odoo.com <kdeb@odoo.com>
2025-07-23 15:48:33 +0200

    [FIX] test_discuss_full: add missing dependency
    remove the dependency on `TestPortal` from `test_discuss_full`, as it is not needed.
    
    Related to commit: https://github.com/odoo/odoo/pull/211204/commits/0e9adb36fb17311bca0cec695ad8c9db9c8dbc11
    
    runbot-226366
    
    closes odoo/odoo#213161
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/test_discuss_full/static/tests/tours/chatbot_redirect_to_portal_tour.js
+++ addons/test_discuss_full/static/tests/tours/chatbot_redirect_to_portal_tour.js
@@ -1,6 +1,6 @@
 import { registry } from "@web/core/registry";
 
-registry.category("web_tour.tours").add("test_mail_full.chatbot_redirect_to_portal", {
+registry.category("web_tour.tours").add("chatbot_redirect_to_portal", {
     url: "/contactus",
     steps: () => [
         {
@@ -19,6 +19,7 @@ registry.category("web_tour.tours").add("test_mail_full.chatbot_redirect_to_port
         {
             trigger: ".o-livechat-root:shadow .o-mail-Message:contains('Go to the portal page')",
         },
+        { trigger: "#chatterRoot:shadow .o-mail-Chatter" },
         {
             trigger: ".o-livechat-root:shadow .o-mail-Message:last:contains('Tadam')",
         },
