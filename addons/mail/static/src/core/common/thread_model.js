91a52741432df8726ee54f1670a7fb77744ce16a
Didier (did) <did@odoo.com>
2025-07-23 11:15:54 +0000

    [FIX] mail: always use authorName
    Before this commit, using author.name directly could lead to not showing
    anything to livechat visitors. This is because the information is not send to
    the visitor.
    By using the authorName getter, we leverable the override in livechat and
    correctly use the livechat_username.
    
    task-4965911
    
    X-original-commit: f7b6630a04ea4805944664dd39120664da8fd12e

--- addons/mail/static/src/core/common/thread_model.js
+++ addons/mail/static/src/core/common/thread_model.js
@@ -328,6 +328,7 @@ export class Thread extends Record {
      * thread.
      *
      * @param {import("models").Persona} persona
+     * @returns {string}
      */
     getPersonaName(persona) {
         return persona.displayName;
@@ -344,6 +345,7 @@ export class Thread extends Record {
     get supportsCustomChannelName() {
         return this.isChatChannel && this.channel_type !== "group";
     }
+
     get displayName() {
         return this.display_name;
     }
