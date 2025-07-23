c83f9d4018669946d8b286d42754bd50ae2f729e
Martin Doyen <mdoy@odoo.com>
2025-07-23 16:52:15 +0200

    [FIX] mrp_subcontracting_dropshipping: fixes test_kit_dropshipped_change_qty_SO Administrator doesn't have an email address set anymore and this causes an error in the test because it needs a sender's email address configured.

--- addons/mrp_subcontracting_dropshipping/tests/test_sale_dropshipping.py
+++ addons/mrp_subcontracting_dropshipping/tests/test_sale_dropshipping.py
@@ -326,6 +326,6 @@ class TestSaleDropshippingFlows(TestMrpSubcontractingCommon):
         })
         so.action_confirm()
 
-        user_admin = self.env['res.users'].search([('login', '=', 'admin')])
+        user_admin = self.env['res.users'].search([('name', '=', 'Odoobot')])
         sol.with_user(user_admin).write({'product_uom_qty': 10})
         self.assertEqual(sol.purchase_line_ids.mapped('product_uom_qty'), [10, 10])
