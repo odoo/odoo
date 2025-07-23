aaa72418862312cd49540e21a96ad017dddc9a04
hatr-odoo <hatr@odoo.com>
2025-07-23 05:42:06 +0000

    [IMP] account: document layout config wizard consistency
    Previously, the document layout wizard used when sending invoices included
    fields for Tax ID, Bank Account Number, and QR Code. However, these fields were
    missing from the wizard accessed via the `Print` button and from the layout
    configurator under `General Settings > Configure Document Layout`. This was
    because only the send flow was using the inherited report layout from the
    `account` module, while the print and settings flows were using the base layout
    from `web` directly. Additionally, the preview template in web did not include
    the bank account number or QR code
    
    With this commit:
    
    1. The `Print` button now uses the inherited layout from `account`.
    2. The settings configurator button also uses the correct layout from `account`.
    3. The bank account number and QR code fields are now dependencies of the
       _compute_preview method, making the preview responsive to their changes.
    4. Both fields are now shown in the preview when configuring the layout in
       settings.
    
    This ensures the document layout behaves consistently across send, print, and
    settings configurator flows.
    
    task-4954030
    
    closes odoo/odoo#222470
    
    X-original-commit: a00a2340165cf377f52cf91b490bc2b2baf7f29d
    Signed-off-by: Habib Ayob (ayh) <ayh@odoo.com>

--- addons/account/wizard/base_document_layout.py
+++ addons/account/wizard/base_document_layout.py
@@ -31,14 +31,22 @@ class BaseDocumentLayout(models.TransientModel):
 
     def _get_render_information(self, styles):
         res = super()._get_render_information(styles)
+
         if (
             self.env.context.get('active_model') == 'account.move'
-            and self.env.context.get('active_id')
+            and (active_id := self.env.context.get('active_id'))
         ):
+            res['o'] = self.env['account.move'].browse(active_id)
+
+        if self._get_preview_template() in [
+            'web.report_invoice_wizard_preview',
+            'account.report_invoice_wizard_iframe'
+        ]:
             res.update({
-                'o': self.env['account.move'].browse(self.env.context.get('active_id')),
                 'qr_code': self.qr_code,
+                'account_number': self.account_number,
             })
+
         return res
 
     @api.depends('partner_id', 'account_number')
@@ -48,6 +56,11 @@ class BaseDocumentLayout(models.TransientModel):
                 record.account_number = record.partner_id.bank_ids[0].acc_number or ''
             else:
                 record.account_number = ''
+
+    @api.depends('qr_code', 'account_number')
+    def _compute_preview(self):
+        # EXTENDS 'web' to add dependencies
+        super()._compute_preview()
 
     def _inverse_account_number(self):
         for record in self:
