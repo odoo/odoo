6f0556ce04c32cbe2598c8a2feac094b73b244d0
Robin Lejeune (role) <role@odoo.com>
2025-07-24 13:42:02 +0200

    [IMP] website_blog, web: make button-like div accessible
    At the end of a blog post's page, there is a section with the next blog
    post's cover and title. This leads to that blog post when clicked. It
    wasn't focusable before this commit and the semantics of the markup were
    wrong.
    
    task-3680626
    
    closes odoo/odoo#183407
    
    Signed-off-by: Serge Bayet (seba) <seba@odoo.com>

--- addons/website_blog/static/tests/interactions/snippets/website_blog.test.js
+++ addons/website_blog/static/tests/interactions/snippets/website_blog.test.js
@@ -11,6 +11,7 @@ test("click on next blog updates URL", async () => {
     const { core } = await startInteractions(`
         <section class="website_blog">
             <div id="o_wblog_next_container" style="width:100px; height: 100px;">
+                <button class="o_wblog_next_button o_button_area btn z-1"/>
                 <div class="o_record_cover_container h-100 o_wblog_post_page_cover o_wblog_post_page_cover_footer o_record_has_cover">
                     <a id="o_wblog_next_post_info" class="d-none" data-size="o_record_has_cover o_half_screen_height" data-url="/some/blog"></a>
                 </div>
@@ -19,7 +20,7 @@ test("click on next blog updates URL", async () => {
     `);
     expect(core.interactions).toHaveLength(1);
     expect(browser.location.pathname).toBe("/")
-    await click("#o_wblog_next_container");
+    await click(".o_wblog_next_button");
     await advanceTime(300);
     expect(browser.location.pathname).toBe("/some/blog");
 });
