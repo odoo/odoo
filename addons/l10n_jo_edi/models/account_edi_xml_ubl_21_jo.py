9537dc6cc004b04733c7e90f7049adc1a75ef6b0
elhayyany <azey@odoo.com>
2025-07-24 09:58:03 +0000

    [IMP] l10n_jo_edi: clean up buyer phone for e‑invoices
    Strip all non-digit characters from the partner phone so that the UBL XML
    complies with JoFotara’s XSD (which requires ^[0-9]{1,15}$). This prevents validation errors when
    submitting e‑invoices.
    
    Steps to reproduce:
    - Install l10n_jo_edi
    - Set partner phone to “+962 79 123 4567”
    - Create & post an invoice
    - Send to JoFotara
    
    OPW-4945688
    
    closes odoo/odoo#221931
    
    X-original-commit: 115e166e29d124eb9e2eb8842dac072d10c083f2
    Signed-off-by: Abdelaziz EL HAYYANY (azey) <azey@odoo.com>
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>

--- addons/l10n_jo_edi/models/account_edi_xml_ubl_21_jo.py
+++ addons/l10n_jo_edi/models/account_edi_xml_ubl_21_jo.py
@@ -1,4 +1,5 @@
 import functools
+import re
 from types import SimpleNamespace
 
 from odoo import models
@@ -93,6 +94,9 @@ class AccountEdiXmlUBL21JO(models.AbstractModel):
             return "S"
         return "O"
 
+    def _sanitize_phone(self, raw):
+        return re.sub(r'[^0-9]', '', raw or '')[:15]
+
     # -------------------------------------------------------------------------
     # EXPORT: Templates for document header nodes
     # -------------------------------------------------------------------------
@@ -165,7 +169,7 @@ class AccountEdiXmlUBL21JO(models.AbstractModel):
             # For non-refund invoices, use the standard party node
             document_node['cac:AccountingCustomerParty']['cac:AccountingContact'] = {
                 'cbc:Telephone': {
-                    '_text': invoice.partner_id.phone
+                    '_text': self._sanitize_phone(invoice.partner_id.phone)
                 }
             }
 
