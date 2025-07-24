2126d2984445567af96327bea1d0c546fd345a9a
Einar Andersen (eian) <eian@odoo.com>
2025-07-24 16:33:54 +0200

    [IMP] website_hr_recruitment: warning on application page
    When applying to a job with an email, phone number, or LinkedIn profile
    that is already associated with an applicant, a warning would appear
    showing that there is either an existing applicant or an existing refused
    applicant. This can cause problems as anyone can use this method to see
    if a person has applied and deduce the status of their application.
    
    This PR replaces these warnings in the frontend by creating activities
    on the applicant that are assigned to either the applicant's recruiter
    or the applicant's job's recruiter.
    
    Task-4943060
    
    closes odoo/odoo#220398
    
    Signed-off-by: Jurgen Gjini (jugj) <jugj@odoo.com>

--- addons/website_hr_recruitment/controllers/main.py
+++ addons/website_hr_recruitment/controllers/main.py
@@ -207,66 +207,6 @@ class WebsiteHrRecruitment(WebsiteForm):
             'default': default,
         })
 
-    @http.route('/website_hr_recruitment/check_recent_application', type='jsonrpc', auth="public", website=True)
-    def check_recent_application(self, field, value, job_id):
-        def refused_applicants_condition(applicant):
-            return not applicant.active \
-                and applicant.job_id.id == int(job_id) \
-                and applicant.create_date >= (datetime.now() - relativedelta(months=6))
-
-        field_domain = {
-            'name': [('partner_name', '=ilike', escape_psql(value))],
-            'email': [('email_normalized', '=', email_normalize(value))],
-            'phone': [('partner_phone', '=', value)],
-            'linkedin': [('linkedin_profile', '=ilike', escape_psql(value))],
-        }.get(field, [])
-
-        applications_by_status = http.request.env['hr.applicant'].sudo().search(Domain.AND([
-            field_domain,
-            [
-                ('job_id.website_id', 'in', [http.request.website.id, False]),
-                '|',
-                    ('application_status', '=', 'ongoing'),
-                    '&',
-                        ('application_status', '=', 'refused'),
-                        ('active', '=', False),
-            ]
-        ]), order='create_date DESC').grouped('application_status')
-        refused_applicants = applications_by_status.get('refused', http.request.env['hr.applicant'])
-        if any(applicant for applicant in refused_applicants if refused_applicants_condition(applicant)):
-            return {
-                'message':  _(
-                    'We\'ve found a previous closed application in our system within the last 6 months.'
-                    ' Please consider before applying in order not to duplicate efforts.'
-                )
-            }
-
-        if 'ongoing' not in applications_by_status:
-            return {'message': None}
-
-        ongoing_application = applications_by_status.get('ongoing')[0]
-        if ongoing_application.job_id.id == int(job_id):
-            recruiter_contact = "" if not ongoing_application.user_id else _(
-                ' In case of issue, contact %(contact_infos)s',
-                contact_infos=", ".join(
-                    [value for value in itemgetter('name', 'email', 'phone')(ongoing_application.user_id) if value]
-                ))
-            return {
-                'message':  _(
-                    'An application already exists for %(value)s.'
-                    ' Duplicates might be rejected. %(recruiter_contact)s',
-                    value=value,
-                    recruiter_contact=recruiter_contact
-                )
-            }
-
-        return {
-            'message':  _(
-                'We found a recent application with a similar name, email, phone number.'
-                ' You can continue if it\'s not a mistake.'
-            )
-        }
-
     def extract_data(self, model_sudo, values):
         short_introduction = values.get("short_introduction", None)
         data = super().extract_data(model_sudo, values)
--- addons/website_hr_recruitment/models/hr_applicant.py
+++ addons/website_hr_recruitment/models/hr_applicant.py
@@ -1,8 +1,13 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models
+from collections import defaultdict
+
+from dateutil.relativedelta import relativedelta
+from markupsafe import Markup
+
+from odoo import api, fields, models
 from odoo.exceptions import UserError
+from odoo.fields import Domain
 
 
 class HrApplicant(models.Model):
@@ -20,3 +25,103 @@ class HrApplicant(models.Model):
             if stage:
                 values['stage_id'] = stage.id
         return values
+
+    def _generate_duplicate_applicant_message(self, message, matching_applicants):
+        return Markup("<p>{message}</p><ul>{applicants}</ul>").format(
+            message=message,
+            applicants=Markup().join(
+                [
+                    Markup("<li><a href='/odoo/hr.applicant/{id}'>{name}</li>").format(
+                        id=applicant.id,
+                        name=applicant.partner_name,
+                    )
+                    for applicant in matching_applicants
+                ],
+            ),
+        )
+
+    def _handle_potential_duplicate_web_applications(self):
+        """
+        This method checks if any newly created applicants in self can be considered duplicates of existing applicants
+        and adds an activity on the duplicate record. An applicant is considered a duplicate if it belongs to the same
+        job position and shares any or all of the following fields: email address, phone number and linkedin profile.
+        """
+        existing_applicants_domain = Domain.AND(
+            [
+                self._get_similar_applicants_domain(ignore_talent=True),
+                Domain("job_id", "in", [job for job in self.mapped("job_id.id") if job]),
+                Domain.OR(
+                    [
+                        Domain("refuse_date", ">=", fields.Datetime.now() - relativedelta(months=6)),
+                        Domain("refuse_date", "=", False),
+                    ],
+                ),
+                Domain("id", "not in", self.ids),
+            ],
+        )
+        existing_applicants = self.with_context(active_test=False).search(
+            domain=existing_applicants_domain,
+            order="create_date desc",
+        )
+
+        refused_existing_applicants = defaultdict(lambda: defaultdict(lambda: self.env["hr.applicant"]))
+        ongoing_existing_applicants = defaultdict(lambda: defaultdict(lambda: self.env["hr.applicant"]))
+
+        for applicant in existing_applicants:
+            if applicant.application_status == "refused":
+                refused_existing_applicants[applicant.job_id.id][applicant.email_normalized] += applicant
+                refused_existing_applicants[applicant.job_id.id][applicant.partner_phone_sanitized] += applicant
+                refused_existing_applicants[applicant.job_id.id][applicant.linkedin_profile] += applicant
+            elif applicant.application_status == "ongoing":
+                ongoing_existing_applicants[applicant.job_id.id][applicant.email_normalized] += applicant
+                ongoing_existing_applicants[applicant.job_id.id][applicant.partner_phone_sanitized] += applicant
+                ongoing_existing_applicants[applicant.job_id.id][applicant.linkedin_profile] += applicant
+
+        for applicant in self:
+            refused_job_matches = refused_existing_applicants.get(applicant.job_id.id)
+            ongoing_job_matches = ongoing_existing_applicants.get(applicant.job_id.id)
+
+            if refused_job_matches:
+                if matching_applicants := (
+                    refused_job_matches.get(applicant.email_normalized, self.env["hr.applicant"])
+                    | refused_job_matches.get(applicant.partner_phone_sanitized, self.env["hr.applicant"])
+                    | refused_job_matches.get(applicant.linkedin_profile, self.env["hr.applicant"])
+                ):
+                    applicant.activity_schedule(
+                        act_type_xmlid="mail_activity_data_todo",
+                        summary=self.env._("Potential Duplicate Detected: Refused Application"),
+                        note=self._generate_duplicate_applicant_message(
+                            self.env._(
+                                "The following recently refused applications share an email address, phone number and/or linkedin profile with this application:",
+                            ),
+                            matching_applicants,
+                        ),
+                        user_id=applicant.recruiter_id.user_id.id or applicant.job_id.recruiter_id.user_id.id,
+                    )
+            elif ongoing_job_matches:
+                if matching_applicants := (
+                    ongoing_job_matches.get(applicant.email_normalized, self.env["hr.applicant"])
+                    | ongoing_job_matches.get(applicant.partner_phone_sanitized, self.env["hr.applicant"])
+                    | ongoing_job_matches.get(applicant.linkedin_profile, self.env["hr.applicant"])
+                ):
+                    applicant.activity_schedule(
+                        act_type_xmlid="mail_activity_data_todo",
+                        summary=self.env._("Potential Duplicate Detected: Ongoing Application"),
+                        note=self._generate_duplicate_applicant_message(
+                            self.env._(
+                                "The following ongoing applications share an email address, phone number and/or linkedin profile with this application:",
+                            ),
+                            matching_applicants,
+                        ),
+                        user_id=applicant.recruiter_id.user_id.id or applicant.job_id.recruiter_id.user_id.id,
+                    )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        applicants = super().create(vals_list)
+
+        applicants_applied_through_website = "website_id" in self.env.context
+        if applicants_applied_through_website:
+            applicants._handle_potential_duplicate_web_applications()
+
+        return applicants
