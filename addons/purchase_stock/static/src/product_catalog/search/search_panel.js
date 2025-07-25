4c766315ea77a180bb3d709b037aa5add95d21bb
jeanschoenlaub <jesc@odoo.com>
2025-07-25 15:12:01 +0200

    [IMP] purchase_stock: Suggest UX - Step 1: Search Panel UI In this first commit we remove all purchase.order.suggest front end code and setup a hard coded Suggest UI JS component, by:
        - Extending the purchaseProductCatalogKanbanView to override Search Panel
    (for now only the SearchPanel but later the KanbanController, KanbanRenderer ...).
        - Patching the search panel template to x-path the Suggest UI above ALL other Search
    Panel sections (eg. product category filter).
         - Changing the product_view product_view_kanban_catalog_purchase_only to inherit from
    purchase instead of stock to avoid redifing the js class // kanban attr.

---
+++ addons/purchase_stock/static/src/product_catalog/search/search_panel.js
@@ -0,0 +1,9 @@
+import { ProductCatalogSearchPanel } from "@product/product_catalog/search/search_panel";
+
+export class PurchaseSuggestCatalogSearchPanel extends ProductCatalogSearchPanel {
+    static template = "PurchaseSuggest.SearchPanel";
+
+    setup() {
+        super.setup();
+    }
+}
