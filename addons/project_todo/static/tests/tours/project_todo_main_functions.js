2414504c8ad14a4071ea46a8bcabda5f21e459f0
Abdullah (abye) <abye@odoo.com>
2025-07-25 11:10:11 +0200

    [REV] project_todo: converting to-do into a task, keep breadcrumbs
    When converting a to-do into a task, the breadcrumb is kept to return to the to-do.
    
    reverting old commit 1acaf3f46075dcf9d50892ce00c52466976587fb
    
    task-4781811
    
    closes odoo/odoo#215033
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/project_todo/static/tests/tours/project_todo_main_functions.js
+++ addons/project_todo/static/tests/tours/project_todo_main_functions.js
@@ -163,10 +163,9 @@ registry.category("web_tour.tours").add('project_todo_main_functions', {
     trigger: 'button[name="action_convert_to_task"]',
     content: 'Convert the todo to a task',
     run: "click",
-    expectUnloadPage: true,
 }, {
-    trigger: ".o_form_view .breadcrumb-item:last-child",
-    content: markup`Let's go back to the <b>kanban view</b> to have an overview of tasks linked to project chosen.`,
+    trigger: ".o_form_view .breadcrumb-item:nth-child(1)",
+    content: markup`Let's go back to the <b>kanban view</b> to have an overview of your next tasks.`,
     run: "click",
 }, {
     trigger: ".o_kanban_view",
