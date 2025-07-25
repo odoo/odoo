0f9f5f00835568709434fd0aa1ebe61ea79cb776
David Beguin <dbe@odoo.com>
2025-07-25 11:06:50 +0200

    [IMP] fleet: notify fleet manager when (future) driver changes
    Before this commit, the fleet manager received an activity for the vehicle
    if the vehicle got a new driver.
    
    After this commit, the fleet manager receives an activity for the vehicle
    when the driver or the future driver changes (so new driver or no driver).
    
    Task-4969392
    
    closes odoo/odoo#220529
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/fleet/models/fleet_vehicle.py
+++ addons/fleet/models/fleet_vehicle.py
@@ -2,7 +2,7 @@
 
 from collections import defaultdict
 from dateutil.relativedelta import relativedelta
-from datetime import datetime
+from datetime import date, datetime
 
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
@@ -405,11 +405,6 @@ class FleetVehicle(models.Model):
             driver_id = vals['driver_id']
             for vehicle in self.filtered(lambda v: v.driver_id.id != driver_id):
                 vehicle.create_driver_history(vals)
-                if vehicle.driver_id:
-                    vehicle.activity_schedule(
-                        'mail.mail_activity_data_todo',
-                        user_id=vehicle.manager_id.id or self.env.user.id,
-                        note=_('Specify the End date of %s', vehicle.driver_id.name))
 
         if 'future_driver_id' in vals and vals['future_driver_id']:
             future_driver = vals['future_driver_id']
@@ -427,6 +422,23 @@ class FleetVehicle(models.Model):
                     vehicle_read_group['bike'].write({'plan_to_change_bike': True})
                 if 'car' in vehicle_read_group:
                     vehicle_read_group['car'].write({'plan_to_change_car': True})
+
+        if 'future_driver_id' in vals or 'driver_id' in vals:
+            # delete existing open activities for vehicles in self.
+            # must delete because cannot update date_deadline
+            self.env['mail.activity'].sudo().search([
+                ('res_model_id', '=', self._name),
+                ('res_id', 'in', self.ids),
+                ('note', 'ilike', _('Review driver change')),
+                ('user_id', 'in', self.manager_id.ids or [self.env.user.id])
+            ]).action_cancel()
+            for vehicle in self:
+                vehicle.activity_schedule(
+                    'mail.mail_activity_data_todo',
+                    date_deadline=date.today() + relativedelta(weeks=1),
+                    note=_('Review driver change of %s and specify End date', vehicle.display_name),
+                    user_id=vehicle.manager_id.id or self.env.user.id,
+                )
 
         if 'active' in vals and not vals['active']:
             self.env['fleet.vehicle.log.contract'].search([('vehicle_id', 'in', self.ids)]).active = False
