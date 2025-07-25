210e52dbca19158e3e36d0e40c78f468585f5e45
tsm-odoo <tsm@odoo.com>
2025-07-25 10:41:18 +0000

    [FIX] im_livechat: fix session navigation tour
    The `test_session_history_navigation_back_and_forth` test
    sometimes fail. The test ensures we can navigate between
    the session list view and discuss using the browser history.
    
    To do so, the tour first accesses the list view using the
    command palette. However, command palette sometimes doesn't
    open.
    
    The command palette part of the tour is useless as the view
    can directly be accessed by passing the correct url to the
    `start_tour` function.
    
    This commit removes the command palette part of the tour
    thus fixing the issue.
    
    fixes runbot-108129
    
    closes odoo/odoo#220675
    
    X-original-commit: e3020b7f4d5aa841a08c0761508a441a3a771fed
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/im_livechat/static/tests/tours/im_livechat_history_back_and_forth.js
+++ addons/im_livechat/static/tests/tours/im_livechat_history_back_and_forth.js
@@ -4,30 +4,6 @@ import { registry } from "@web/core/registry";
 registry.category("web_tour.tours").add("im_livechat_history_back_and_forth_tour", {
     steps: () => [
         {
-            isActive: ["enterprise"],
-            content: "open command palette",
-            trigger: ".o_home_menu",
-            run: "click && press ctrl+k",
-        },
-        {
-            isActive: ["community"],
-            content: "open command palette",
-            trigger: "body:has(.o_action_manager)",
-            run: "press ctrl+k",
-        },
-        {
-            trigger: ".o_command_palette_search input",
-            run: "fill /",
-        },
-        {
-            trigger: ".o_command_palette_search input",
-            run: "fill Live Chat",
-        },
-        {
-            trigger: ".o_command:contains(Sessions)",
-            run: "click",
-        },
-        {
             trigger: "button.o_switch_view.o_list",
             run: "click",
         },
