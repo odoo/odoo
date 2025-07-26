9240148406929cce5e093621135f777be7a2f8a1
David Monnom <moda@odoo.com>
2025-07-26 18:42:55 +0200

    [FIX] point_of_sale: prevent duplicate preparation ticket
    Before this change, when a device sent an order in preparation via the
    ticket printer, this could result in the same order being printed by
    multiple devices, as the order was not synchronized after it was sent.
    
    The error is a bit tricky, because if the user had installed a
    preparation screen, the order was sent to the preparation screen via
    syncAllOrders. In this case, the order was correctly synchronized and
    the other devices were informed of the changes.
    
    This commit adds two things.
    - We check the server before sending the order to preparation to make
    sure it has not already been sent.
    - Even when the user does not have a preparation display, the order
    will be synchronized after being sent to a printer.
    
    closes odoo/odoo#220706
    
    Related: odoo/enterprise#91090
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/point_of_sale/static/src/app/screens/receipt_screen/receipt_screen.js
+++ addons/point_of_sale/static/src/app/screens/receipt_screen/receipt_screen.js
@@ -34,7 +34,7 @@ export class ReceiptScreen extends Component {
             this.currentOrder.uiState.locked = true;
 
             if (!this.pos.config.module_pos_restaurant) {
-                this.pos.sendOrderInPreparation(order, { orderDone: true });
+                this.pos.checkPreparationStateAndSentOrderInPreparation(order, { orderDone: true });
             }
         });
     }
