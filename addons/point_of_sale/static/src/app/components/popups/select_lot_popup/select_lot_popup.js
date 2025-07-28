143901a70f5f9d4c7568551154acd4c491281997
Pedram (PEBR) <pebr@odoo.com>
2025-07-28 17:13:22 +0200

    [FIX] point_of_sale: correctly use existing Serial/Lot numbers
    After commit https://github.com/odoo/odoo/commit/cb31a37508d32110731ad61b04d5d05dc7b31825, when the PoS picking type was configured
    to use existing lot numbers and not creating new ones, it was not
    possible to correctly add products with existing lot numbers.
    
    opw-4974345
    
    closes odoo/odoo#220866
    
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/point_of_sale/static/src/app/components/popups/select_lot_popup/select_lot_popup.js
+++ addons/point_of_sale/static/src/app/components/popups/select_lot_popup/select_lot_popup.js
@@ -79,7 +79,7 @@ export class SelectLotPopup extends Component {
                 sticky: false,
             });
         }
-        if (!lot.create || !this.props.customInput) {
+        if (!lot.create) {
             return this.notification.add(_t("The Lot/Serial number is not valid"), {
                 type: "warning",
                 sticky: false,
@@ -100,7 +100,8 @@ export class SelectLotPopup extends Component {
             return (
                 itemValue !== "" &&
                 !this.props.isLotNameUsed(itemValue) &&
-                (this.props.customInput || this.props.options.includes(itemValue))
+                (this.props.customInput ||
+                    this.props.options.map((o) => o.name).includes(itemValue))
             );
         });
         const filteredValues = this.props.uniqueValues
