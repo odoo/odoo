95e9cde4473866bfcaffeb0db9e501b63156bfc8
Adrien Minne (adrm) <adrm@odoo.com>
2025-07-28 09:03:11 +0000

    [FIX] spreadsheet: apply global filters on chart update
    The global filters were not re-applied when updating an oodo chart
    domain.
    
    Task: 4965683
    X-original-commit: 9a35914ff3cd985297f3ed9b15bb9bac5e22e5e4

--- addons/spreadsheet/static/src/chart/plugins/odoo_chart_ui_plugin.js
+++ addons/spreadsheet/static/src/chart/plugins/odoo_chart_ui_plugin.js
@@ -182,6 +182,7 @@ export class OdooChartUIPlugin extends OdooUIPlugin {
         const definition = this.getters.getChart(chartId).getDefinitionForDataSource();
         const dataSourceId = this._getOdooChartDataSourceId(chartId);
         this.charts[dataSourceId] = new ChartDataSource(this.custom, definition);
+        this._addDomain(chartId);
     }
 
     /**
