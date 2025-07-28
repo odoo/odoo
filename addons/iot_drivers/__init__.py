966939902dedaa991217e4ed5ee898a201749a4f
Max Whale <mawh@odoo.com>
2025-07-28 09:15:11 +0000

    [FIX] iot*: fix checkout pre 18.4, fix SSL verify
    This commit fixes the following issues:
    - The hw_drivers and hw_posbox_homepage folders were missing from the
      sparse checkout, meaning that connected to version prior to 18.4
      would prevent Odoo from starting.
    - As the IoT box can only sync time with the network once it gets an
      internet connection, some early requests could fail due to it thinking
      the SSL certificates were invalid. We disable verification of the
      requests to bypass this.
    - The request to download the certificate for the IoT box only had a
      timeout of 5 seconds, sometimes this could not be enough so the
      timeout has been increased to 10 seconds.
    - Fix an error when calling the `update_wifi` route, caused by
      `helpers.get_ip()` now being able to return `None`.
    
    closes odoo/odoo#220920
    
    X-original-commit: d43ebae9efd59f93f99514f50736780110374df4
    Signed-off-by: Louis Travaux (lotr) <lotr@odoo.com>
    Signed-off-by: Max Whale (mawh) <mawh@odoo.com>

--- addons/iot_drivers/__init__.py
+++ addons/iot_drivers/__init__.py
@@ -24,19 +24,20 @@ _get = requests.get
 _post = requests.post
 
 
-def set_user_agent(func):
+def set_default_options(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         headers = kwargs.pop('headers', None) or {}
+        verify = kwargs.pop('verify', False)
         headers['User-Agent'] = 'OdooIoTBox/1.0'
         server_url = tools.helpers.get_odoo_server_url()
         db_name = tools.helpers.get_conf('db_name')
         if server_url and db_name and args[0].startswith(server_url) and '/web/login?db=' not in args[0]:
             headers['X-Odoo-Database'] = db_name
-        return func(*args, headers=headers, **kwargs)
+        return func(*args, headers=headers, verify=verify, **kwargs)
 
     return wrapper
 
 
-requests.get = set_user_agent(_get)
-requests.post = set_user_agent(_post)
+requests.get = set_default_options(_get)
+requests.post = set_default_options(_post)
