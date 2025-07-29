a2a6456a38e15c7330f7a37b69693c0238735aa1
Benjamin Vray <bvr@odoo.com>
2025-07-29 09:33:20 +0200

    [FIX] website: fix parallax in nested block
    Steps to reproduce:
    
    - In Website edit mode.
    - Drag and drop a "Tabs" block.
    - Drag and drop a "Cover" block inside the "Tabs".
    - Add a background image to the "Tabs" block.
    - Add a scroll effect (Parallax) to the "Tabs" block.
    - Bug: The scroll effect is not applied.
    
    This bug happened because the code checked for a parallax effect in the
    whole block structure, not just on the block itself. It wrongly detected
    a parallax because a child block had one, even if the parent didn’t.
    
    task-4926420
    
    closes odoo/odoo#220979
    
    Signed-off-by: Serge Bayet (seba) <seba@odoo.com>

--- addons/website/static/src/builder/plugins/options/parallax_option_plugin.js
+++ addons/website/static/src/builder/plugins/options/parallax_option_plugin.js
@@ -45,7 +45,7 @@ class WebsiteParallaxPlugin extends Plugin {
         } else {
             delete editingElement.dataset.parallaxType;
         }
-        let parallaxEl = editingElement.querySelector(".s_parallax_bg");
+        let parallaxEl = editingElement.querySelector(":scope > .s_parallax_bg");
         if (isParallax) {
             if (!parallaxEl) {
                 parallaxEl = document.createElement("span");
@@ -68,7 +68,7 @@ class WebsiteParallaxPlugin extends Plugin {
         }
     }
     removeParallax(editingEl) {
-        const parallaxEl = editingEl.querySelector(".s_parallax_bg");
+        const parallaxEl = editingEl.querySelector(":scope > .s_parallax_bg");
         const bgImage = parallaxEl?.style.backgroundImage;
         if (
             !parallaxEl ||
