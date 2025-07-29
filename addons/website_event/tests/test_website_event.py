5aadbfca39ab32cc9a23c451ffcb1fbb94850fbd
Levi Siuzdak (sile) <sile@odoo.com>
2025-07-29 06:48:16 +0000

    [FIX] base, http_routing: use 404 instead of 403 for public users
    Versions
    --------
    - 16.0+
    
    Steps
    -----
    1. Have an unpublished product;
    2. as admin, go to its `/shop` page;
    3. copy URL;
    4. open URL in private window.
    
    Issue
    -----
    > 403: Forbidden
    >
    > Uh-oh! Looks like you have stumbled upon some top-secret records.
    >
    > Sorry, Public user (id=4) doesn't have 'read' access to:
    > - Product (product.template)
    >
    > If you really, really need access, perhaps you can win over your
    > friendly administrator with a batch of freshly baked cookies.
    
    This is information should not be accessible to public users.
    
    Cause
    -----
    16.0:
    - Commit 06cc322e7e2f added a `_pre_dispatch` override which displays
      the product name in the URL for SEO purposes. However, when the user
      does not have access to the product record, it throws an `AccessError`,
      leading to the HTTP 403 response.
    
    17.0+:
    - Commit 91979b0fcf33added an explicit access rights check to the base
      `_pre_dispatch` method, throwing an `AccessError` for any/all users.
    
    Solution
    --------
    If an `AccessError` is raised when building the URL for a public user,
    return a HTTP 404 response, as if the record doesn't exist.
    
    opw-4936500
    
    X-original-commit: 93e653093e73bb2da00efd72ec1a87124efe710c

--- addons/website_event/tests/test_website_event.py
+++ addons/website_event/tests/test_website_event.py
@@ -264,7 +264,7 @@ class TestWebsiteAccess(HttpCaseWithUserDemo, OnlineEventCase):
 
         unpublished_events = self.events.filtered(lambda event: not event.website_published)
         resp = self.url_open('/event/%i' % unpublished_events[0].id)
-        self.assertEqual(resp.status_code, 403, 'Public must not have access to unpublished event')
+        self.assertEqual(resp.status_code, 404, 'Public must not have access to unpublished event')
 
         resp = self.url_open('/event')
         self.assertTrue(published_events[0].name in resp.text, 'Public must see the published events.')
