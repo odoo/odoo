6f958210bce38272d338ad719d415b3908355f4c
Woojang (WOPY) <wopy@odoo.com>
2025-07-29 18:08:45 +0800

    [IMP] l10n_tw: refine chart of accounts
    Updates the default Chart of Accounts for Taiwan to better match common business practices.
    
    The changes were referenced from the accounting structures used by publicly listed companies in Taiwan to ensure relevance and practicality.
    
    Updates:
    - Taiwanese chart of accounts
    - Taiwanese default accounts
    
    Task-4915057

--- addons/l10n_tw/models/template_tw.py
+++ addons/l10n_tw/models/template_tw.py
@@ -9,10 +9,10 @@ class AccountChartTemplate(models.AbstractModel):
     @template('tw')
     def _get_tw_template_data(self):
         return {
-            'code_digits': '6',
-            'property_account_receivable_id': 'tw_119100',
-            'property_account_payable_id': 'tw_217100',
-            'property_stock_valuation_account_id': 'tw_123100',
+            'code_digits': '4',
+            'property_account_receivable_id': 'l10n_tw_account_1156',
+            'property_account_payable_id': 'l10n_tw_account_2171',
+            'property_stock_account_production_cost_id': 'l10n_tw_account_5101',
         }
 
     @template('tw', 'res.company')
@@ -21,19 +21,39 @@ class AccountChartTemplate(models.AbstractModel):
             self.env.company.id: {
                 'anglo_saxon_accounting': True,
                 'account_fiscal_country_id': 'base.tw',
-                'bank_account_code_prefix': '1113',
-                'cash_account_code_prefix': '1111',
-                'transfer_account_code_prefix': '1114',
-                'account_default_pos_receivable_account_id': 'tw_119150',
-                'income_currency_exchange_account_id': 'tw_718100',
-                'expense_currency_exchange_account_id': 'tw_718200',
-                'account_journal_early_pay_discount_loss_account_id': 'tw_411400',
-                'account_journal_early_pay_discount_gain_account_id': 'tw_512400',
-                'default_cash_difference_income_account_id': 'tw_718500',
-                'default_cash_difference_expense_account_id': 'tw_718600',
+                'bank_account_code_prefix': '1100',
+                'cash_account_code_prefix': '1101',
+                'transfer_account_code_prefix': '1103',
+                'account_journal_suspense_account_id': 'l10n_tw_account_1102',
+                'transfer_account_id': 'l10n_tw_account_1103',
+                'deferred_expense_account_id': 'l10n_tw_account_1411',
+                'deferred_revenue_account_id': 'l10n_tw_account_2141',
+                'account_default_pos_receivable_account_id': 'l10n_tw_account_1157',
+                'income_currency_exchange_account_id': 'l10n_tw_account_7171',
+                'expense_currency_exchange_account_id': 'l10n_tw_account_7541',
+                'account_journal_early_pay_discount_loss_account_id': 'l10n_tw_account_4821',
+                'account_journal_early_pay_discount_gain_account_id': 'l10n_tw_account_7191',
+                'default_cash_difference_income_account_id': 'l10n_tw_account_7181',
+                'default_cash_difference_expense_account_id': 'l10n_tw_account_7561',
                 'account_sale_tax_id': 'tw_tax_sale_5',
                 'account_purchase_tax_id': 'tw_tax_purchase_5',
-                'expense_account_id': 'tw_511100',
-                'income_account_id': 'tw_411100',
+                'expense_account_id': 'l10n_tw_account_5601',
+                'income_account_id': 'l10n_tw_account_4111',
+                'account_production_wip_account_id': 'l10n_tw_account_1314',
+                'account_production_wip_overhead_account_id': 'l10n_tw_account_5601',
+                'tax_calculation_rounding_method': 'round_globally',
+            },
+        }
+
+    @template('tw', 'account.journal')
+    def _get_tw_account_journal(self):
+        return {
+            'bank': {
+                'default_account_id': 'l10n_tw_account_1100',
+            },
+            'cash': {
+                'name': self.env._("Cash"),
+                'type': 'cash',
+                'default_account_id': 'l10n_tw_account_1101',
             },
         }
