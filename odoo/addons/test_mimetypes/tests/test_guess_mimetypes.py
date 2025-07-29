2f59951eb11e59b4efae43954d0088ecde1e8a27
Christophe Monniez <moc@odoo.com>
2025-07-29 15:23:31 +0200

    [FIX] base, test_mimetypes: fix tests when magic is installed
    Those tests are failing when python-magic is installed.
    Since 26f9c82b99 Odoo > saas-18.4 has this lib as a requirement and
    comes with appropriate fixes.
    
    This commit adapts some test for versions prior to saas-18.4 to also work
    when the python-magic lib is installed.
    
    closes odoo/odoo#221045
    
    Related: odoo/enterprise#91267
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- odoo/addons/test_mimetypes/tests/test_guess_mimetypes.py
+++ odoo/addons/test_mimetypes/tests/test_guess_mimetypes.py
@@ -2,7 +2,7 @@
 import os.path
 
 from odoo.tests.common import BaseCase
-from odoo.tools.mimetypes import guess_mimetype
+from odoo.tools.mimetypes import guess_mimetype, magic
 
 def contents(extension):
     with open(os.path.join(
@@ -15,14 +15,16 @@ def contents(extension):
 
 class TestMimeGuessing(BaseCase):
     def test_doc(self):
-        self.assertEqual(
+        expected_mimetypes = ['application/msword'] if magic is None else ['application/x-ole-storage', 'application/CDFV2']
+        self.assertIn(
             guess_mimetype(contents('doc')),
-            'application/msword'
+            expected_mimetypes
         )
     def test_xls(self):
-        self.assertEqual(
+        expected_mimetypes = ['application/vnd.ms-excel'] if magic is None else ['application/x-ole-storage', 'application/CDFV2']
+        self.assertIn(
             guess_mimetype(contents('xls')),
-            'application/vnd.ms-excel'
+            expected_mimetypes
         )
     def test_docx(self):
         self.assertEqual(
@@ -63,7 +65,8 @@ class TestMimeGuessing(BaseCase):
         )
 
     def test_unknown(self):
+        expected_mimetype = 'application/octet-stream' if magic is None else 'text/csv'
         self.assertEqual(
             guess_mimetype(contents('csv')),
-            'application/octet-stream'
+            expected_mimetype
         )
