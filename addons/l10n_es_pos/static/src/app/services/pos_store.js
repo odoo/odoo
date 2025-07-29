85de60caa72c8dcf818166849d9c37da29028a28
bsra-odoo <bsra@odoo.com>
2025-07-29 05:40:19 +0000

    [FIX] l10n_es_pos: fix blank screen when processing POS orders
    Steps to Reproduce:
    - Install the l10n_es_pos module in version 18.0.
    - Navigate to Contacts.
    - Search for Simplified Invoice Partner (ES) and archive the record.
    - Open the Point of Sale (POS) and proceed to place an order.
    - Select a product.
    - Proceed to the payment screen.
    - Choose a payment method and click Validate.
    
    In version 18.0, when the user proceeds with placing an order,
    a blank screen appears without any error message,
    leading to confusion for the customer.
    
    This issue occurs because during the pos_load_data process,
    https://github.com/odoo/odoo/blob/cd4c1d599ea9403ce3791e239ad765d946446a47/addons/point_of_sale/models/pos_session.py#L181
    while preparing the pos.config, the simplified_partner_id is
    fetched. However, the corresponding res.partner record is not
    included during the preparation of the res.partner model data, as
    the record is archived. As a result, when the system attempts
    to link the related partner record to pos.config.simplified_partner_id
    on the JavaScript side,
    https://github.com/odoo/odoo/blob/cd4c1d599ea9403ce3791e239ad765d946446a47/addons/point_of_sale/static/src/app/models/related_models.js#L1063-L1131
    https://github.com/odoo/odoo/blob/cd4c1d599ea9403ce3791e239ad765d946446a47/addons/point_of_sale/static/src/app/models/related_models.js#L1114
    the simplified_partner_id remains undefined. This eventually
    leads to an error when attempting to access simplified_partner_id.id
    https://github.com/odoo/odoo/blob/cd4c1d599ea9403ce3791e239ad765d946446a47/addons/l10n_es_pos/static/src/overrides/models/pos_store.js#L8
    
    Opw: 4953896
    X-original-commit: bbc8a63cfebf3f6335c8b70cd6e016d04c98219c

--- addons/l10n_es_pos/static/src/app/services/pos_store.js
+++ addons/l10n_es_pos/static/src/app/services/pos_store.js
@@ -2,6 +2,32 @@ import { patch } from "@web/core/utils/patch";
 import { PosStore } from "@point_of_sale/app/services/pos_store";
 
 patch(PosStore.prototype, {
+<<<<<<< 1c4a9c4884e140652d791d81f68bdf48d56d5086:addons/l10n_es_pos/static/src/app/services/pos_store.js
+||||||| 95ed5c75582631c7cc417b000569ff6451cc5006:addons/l10n_es_pos/static/src/overrides/models/pos_store.js
+    getReceiptHeaderData(order) {
+        const result = super.getReceiptHeaderData(...arguments);
+        result.is_spanish = this.config.is_spanish;
+        result.simplified_partner_id = this.config.simplified_partner_id.id;
+        if (order) {
+            result.is_l10n_es_simplified_invoice = order.is_l10n_es_simplified_invoice;
+            result.partner = order.get_partner();
+            result.invoice_name = order.invoice_name;
+        }
+        return result;
+    },
+=======
+    getReceiptHeaderData(order) {
+        const result = super.getReceiptHeaderData(...arguments);
+        result.is_spanish = this.config.is_spanish;
+        result.simplified_partner_id = this.config.simplified_partner_id?.id;
+        if (order) {
+            result.is_l10n_es_simplified_invoice = order.is_l10n_es_simplified_invoice;
+            result.partner = order.get_partner();
+            result.invoice_name = order.invoice_name;
+        }
+        return result;
+    },
+>>>>>>> 63975034c43bef7550f86ca6fc1c9af17825134c:addons/l10n_es_pos/static/src/overrides/models/pos_store.js
     _getCreateOrderContext(orders, options) {
         let context = super._getCreateOrderContext(...arguments);
         if (this.config.is_spanish) {
