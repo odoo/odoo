d607416c6c595fbc6d794b4eb80e584de367891e
Sébastien (blse) <blse@odoo.com>
2025-07-29 12:54:25 +0000

    [FIX] website_blog: fix update of author's avatar on author change
    The test that check avatar update on author's changes has been skipped during the merge of the initial website builder refactor, and was not re-enabled. During this time, the change to use classes for actions has been merged but broke the patch in `website_blog`.
    
    This commit adapts the patch to apply to the action instead of the plugin
    
    Actions to classes: 0550231117b016e0b1e7e336f1c065430fa5a1c9
    Website refactor: 3ced5e9232d3c5f71259f1f2b692416932e3ebd2
    task-4367641
    
    closes odoo/odoo#221117
    
    X-original-commit: 39f6868a66cf14e3f4754887b2e81846bde69fdb
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/website_blog/static/src/website_builder/author_avatar_many2one_plugin.js
+++ addons/website_blog/static/src/website_builder/author_avatar_many2one_plugin.js
@@ -1,32 +1,28 @@
 import { patch } from "@web/core/utils/patch";
-import { Many2OneOptionPlugin } from "@website/builder/plugins/options/many2one_option_plugin";
+import { Many2OneAction } from "@website/builder/plugins/options/many2one_option_plugin";
 
-patch(Many2OneOptionPlugin, {
-    dependencies: [...Many2OneOptionPlugin.dependencies, "history"],
+patch(Many2OneAction, {
+    dependencies: [...Many2OneAction.dependencies, "history"],
 });
 
-patch(Many2OneOptionPlugin.prototype, {
-    getActions() {
-        const actions = super.getActions();
-        const newApply = (args) => {
-            actions.many2One.apply(args);
-            const { editingElement, value } = args;
-            const { id } = JSON.parse(value);
-            const { oeId, oeField } = editingElement.dataset;
+patch(Many2OneAction.prototype, {
+    apply(args) {
+        super.apply(args);
+        const { editingElement, value } = args;
+        const { id } = JSON.parse(value);
+        const { oeId, oeField } = editingElement.dataset;
 
-            if (oeField === "author_id") {
-                for (const node of this.editable.querySelectorAll(
-                    `[data-oe-model="blog.post"][data-oe-id="${oeId}"][data-oe-field="author_avatar"]`
-                )) {
-                    node.querySelector("img").src = `/web/image/res.partner/${id}/avatar_1024`;
-                    // We do not want to save it to the server
-                    // TODO: a more general approach for editing records that are used at different parts of the page
-                    this.dependencies.history.ignoreDOMMutations(() => {
-                        node.classList.remove("o_dirty");
-                    });
-                }
+        if (oeField === "author_id") {
+            for (const node of this.editable.querySelectorAll(
+                `[data-oe-model="blog.post"][data-oe-id="${oeId}"][data-oe-field="author_avatar"]`
+            )) {
+                node.querySelector("img").src = `/web/image/res.partner/${id}/avatar_1024`;
+                // We do not want to save it to the server
+                // TODO: a more general approach for editing records that are used at different parts of the page
+                this.dependencies.history.ignoreDOMMutations(() => {
+                    node.classList.remove("o_dirty");
+                });
             }
-        };
-        return { ...actions, many2One: { ...actions.many2One, apply: newApply } };
+        }
     },
 });
