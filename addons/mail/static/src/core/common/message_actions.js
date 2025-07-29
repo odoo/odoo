2cd0f6128351706ac4b8d1cf5398c4b6ea315bf4
Giorgio Tocco (gito) <gito@odoo.com>
2025-07-29 13:15:38 +0000

    [FIX] mail: disable copy link in portal chatter if no read access
    Before this commit, it was possible for a user to copy the link of
    messages in the chatter of a portal document accessed via share link
    with token.
    
    This could lead to unintentionally leaking the access token of the
    document.
    
    This commit fixes the issue by removing the possibility to copy links
    of messages inside portal documents for which a user has no read
    access.
    
    discussed in task-4551910
    
    closes odoo/odoo#221146
    
    X-original-commit: 7cc8b939dbce1efdf9e93c540fc4f282baa74d2c
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/static/src/core/common/message_actions.js
+++ addons/mail/static/src/core/common/message_actions.js
@@ -168,7 +168,8 @@ messageActionsRegistry
     .add("copy-link", {
         condition: (component) =>
             component.message.message_type &&
-            component.message.message_type !== "user_notification",
+            component.message.message_type !== "user_notification" &&
+            (!component.props.thread.access_token || component.props.thread.hasReadAccess),
         icon: "fa fa-link",
         title: _t("Copy Link"),
         onClick: (component) => component.message.copyLink(),
