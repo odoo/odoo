97bc6842fd6237e7a8a4b68099d2f5f0cbeb1f63
Christophe Monniez <moc@odoo.com>
2025-07-29 13:23:31 +0000

    [FIX] test_mimetypes: fix tests when magic is installed
    Those tests are failing when python-magic is installed.
    Since 26f9c82b99 Odoo > saas-18.4 has this lib as a requirement and
    comes with appropriate fixes.
    
    This commit adapts some test for versions prior to saas-18.4 to also work
    when the python-magic lib is installed.
    
    Related: odoo/enterprise#91267
    Related: odoo/enterprise#91375
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>
    Signed-off-by: Christophe Monniez (moc) <moc@odoo.com>
    X-original-commit: a03af4409c559f27986024ecc0805e734dd0fbac

--- odoo/addons/test_mimetypes/tests/test_guess_mimetypes.py
+++ odoo/addons/test_mimetypes/tests/test_guess_mimetypes.py
@@ -4,7 +4,13 @@ import unittest
 from odoo.tests import BaseCase
 from odoo.tools.mimetypes import _odoo_guess_mimetype, guess_mimetype
 from odoo.tools.misc import file_open
+<<<<<<< 08b62a4bbcc6f9a391b2cc00a621ef4c76100229
 
+||||||| 612ac9212eb3b5802d882325c475a2f727e8b0ca
+from odoo.tools.mimetypes import guess_mimetype
+=======
+from odoo.tools.mimetypes import guess_mimetype, magic
+>>>>>>> 9dc347b560cb315546f4920258ced808708a40cc
 
 def contents(extension):
     with file_open(os.path.join(
@@ -25,8 +31,17 @@ class MimeGuessingCases:
             'application/msword',
         )
 
+<<<<<<< 08b62a4bbcc6f9a391b2cc00a621ef4c76100229
     def test_xls(self):
+||||||| 612ac9212eb3b5802d882325c475a2f727e8b0ca
+    def test_xlsx_2025(self):
+        # only work when python-magic is not installed
+=======
+    def test_xlsx_2025(self):
+        # only work when python-magic is not installed otherwise seen as a zip
+>>>>>>> 9dc347b560cb315546f4920258ced808708a40cc
         self.assertEqual(
+<<<<<<< 08b62a4bbcc6f9a391b2cc00a621ef4c76100229
             self.guess_mimetype(contents('xls')),
             'application/vnd.ms-excel',
         )
@@ -41,6 +56,13 @@ class MimeGuessingCases:
         self.assertEqual(
             self.guess_mimetype(contents('xlsx')),
             'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+||||||| 612ac9212eb3b5802d882325c475a2f727e8b0ca
+            guess_mimetype(contents('2025.xlsx')),
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+=======
+            guess_mimetype(contents('2025.xlsx')),
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' if magic is None else 'application/zip',
+>>>>>>> 9dc347b560cb315546f4920258ced808708a40cc
         )
 
     def test_odt(self):
@@ -67,8 +89,16 @@ class MimeGuessingCases:
             'image/gif',
         )
 
+<<<<<<< 08b62a4bbcc6f9a391b2cc00a621ef4c76100229
     def test_jpeg(self):
+||||||| 612ac9212eb3b5802d882325c475a2f727e8b0ca
+    def test_unknown(self):
+=======
+    def test_unknown(self):
+        expected_mimetype = 'text/plain' if magic is None else 'text/csv'
+>>>>>>> 9dc347b560cb315546f4920258ced808708a40cc
         self.assertEqual(
+<<<<<<< 08b62a4bbcc6f9a391b2cc00a621ef4c76100229
             self.guess_mimetype(contents('jpg')),
             'image/jpeg',
         )
@@ -104,4 +134,11 @@ class TestMimeGuessingMagic(BaseCase, MimeGuessingCases):
         self.assertEqual(
             self.guess_mimetype(contents('csv')),
             'text/csv',
+||||||| 612ac9212eb3b5802d882325c475a2f727e8b0ca
+            guess_mimetype(contents('csv')),
+            'text/plain'
+=======
+            guess_mimetype(contents('csv')),
+            expected_mimetype
+>>>>>>> 9dc347b560cb315546f4920258ced808708a40cc
         )
