eb91fbc511ebda494a319aed9ce9e2b306cf94ef
Serhii Rubanskyi <seru@odoo.com>
2025-07-30 11:37:06 +0200

    [FIX] website: update ToC navbar items when translating headings
    When we translate headings in the Table of Content snippet, the
    snippet's navbar items aren't responsive, they stay connected to the
    original language headings.
    Steps to reproduce the problem:
    - Add a second language to your website
    - Open Website and start editing
    - Drop the table of content snippet
    - Save, switch to your secondary language and start translating
    - Modify the table's heading
    
    => Navbar headings aren't updated.
    This commit follows the [html_builder refactoring].
    Related to task-4367641
    
    [html_builder refactoring]: https://github.com/odoo/odoo/commit/9fe45e2b7ddb
    
    closes odoo/odoo#221186
    
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

---
+++ addons/website/static/src/builder/plugins/options/table_of_content_option_plugin_translate.js
@@ -0,0 +1,40 @@
+import { Plugin } from "@html_editor/plugin";
+import { applyFunDependOnSelectorAndExclude } from "../utils";
+
+export class TranslateTableOfContentOptionPlugin extends Plugin {
+    static id = "tableOfContentOption";
+
+    resources = {
+        normalize_handlers: this.normalize.bind(this),
+        force_not_editable_selector: [".s_table_of_content_navbar"],
+    };
+
+    normalize(root) {
+        applyFunDependOnSelectorAndExclude(this.updateTableOfContentNavbar.bind(this), root, {
+            selector: ".s_table_of_content_main",
+        });
+    }
+
+    updateTableOfContentNavbar(tableOfContentMain) {
+        const tableOfContent = tableOfContentMain.closest(".s_table_of_content");
+        const tableOfContentNavbar = tableOfContent.querySelector(".s_table_of_content_navbar");
+        const currentNavbarItems = [...tableOfContentNavbar.children].map((el) => el.firstChild);
+
+        const targetedElements = "h1, h2";
+        const currentHeadingItems = [
+            ...tableOfContentMain.querySelectorAll(targetedElements),
+        ].filter((el) => !el.closest(".o_snippet_desktop_invisible"));
+
+        currentNavbarItems.map((el, i) => {
+            const newText = currentHeadingItems[i]?.textContent || "";
+            if (el.textContent !== newText) {
+                el.textContent = newText;
+            }
+
+            const newHref = `#${currentHeadingItems[i]?.id}`;
+            if (newHref && el.parentElement.getAttribute("href") !== newHref) {
+                el.parentElement.setAttribute("href", newHref);
+            }
+        });
+    }
+}
--- addons/website/static/src/builder/website_builder.js
+++ addons/website/static/src/builder/website_builder.js
@@ -24,6 +24,7 @@ import { BuilderActionsPlugin } from "@html_builder/core/builder_actions_plugin"
 import { CoreBuilderActionPlugin } from "@html_builder/core/core_builder_action_plugin";
 import { CarouselOptionTranslationPlugin } from "./plugins/carousel_option_translation_plugin";
 import { ThemeTab } from "./plugins/theme/theme_tab";
+import { TranslateTableOfContentOptionPlugin } from "./plugins/options/table_of_content_option_plugin_translate";
 import { BuilderContentEditablePlugin } from "@html_builder/core/builder_content_editable_plugin";
 import { ImageFieldPlugin } from "@html_builder/plugins/image_field_plugin";
 import { MonetaryFieldPlugin } from "@html_builder/plugins/monetary_field_plugin";
@@ -47,6 +48,7 @@ const TRANSLATION_PLUGINS = [
     HighlightPlugin,
     OperationPlugin,
     EditInteractionPlugin,
+    TranslateTableOfContentOptionPlugin,
     BuilderContentEditablePlugin,
     CarouselOptionTranslationPlugin,
     ImageFieldPlugin,
