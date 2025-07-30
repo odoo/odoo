f7ccf6b6e9551fb8b15545801675e98371a70b80
Francois Vasamillet (frva) <frva@odoo.com>
2025-07-30 14:00:18 +0200

    [FIX] website_partner: Cleaning 1

--- addons/website_crm_partner_assign/models/res_partner.py
+++ addons/website_crm_partner_assign/models/res_partner.py
@@ -1,27 +1,10 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+from odoo import models
 
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
-
-    @api.model
-    def default_get(self, fields_list):
-        default_vals = super().default_get(fields_list)
-        if self.env.context.get('partner_set_default_grade_activation'):
-            # sets the lowest grade and activation if no default values given, mainly useful while
-            # creating assigned partner on the fly (to make it visible in same m2o again)
-            if 'grade_id' in fields_list and not default_vals.get('grade_id'):
-                default_vals['grade_id'] = self.env['res.partner.grade'].search([], order='sequence', limit=1).id
-            if 'activation' in fields_list and not default_vals.get('activation'):
-                default_vals['activation'] = self.env['res.partner.activation'].search([], order='sequence', limit=1).id
-        return default_vals
-
-    activation = fields.Many2one('res.partner.activation', 'Activation', index='btree_not_null', tracking=True)
-    date_partnership = fields.Date('Partnership Date')
-    date_review = fields.Date('Latest Review')
-    date_review_next = fields.Date('Next Review')
 
     def _get_contact_opportunities_domain(self):
         all_partners = self._fetch_children_partners_for_hierarchy().ids
--- addons/website_partner/__manifest__.py
+++ addons/website_partner/__manifest__.py
@@ -10,6 +10,7 @@
 This is a base module. It holds website-related stuff for Contact model (res.partner).
     """,
     'depends': [
+        'base_geolocalize',
         'contacts',
         'partnership',
         'website',
@@ -18,6 +19,13 @@ This is a base module. It holds website-related stuff for Contact model (res.par
         'views/res_partner_views.xml',
         'views/website_partner_templates.xml',
         'data/website_partner_data.xml',
+        'data/res_partner_activation_data.xml',
+        'security/ir.model.access.csv',
+        'security/ir_rule.xml',
+        'views/res_partner_grade_views.xml',
+        'views/res_partner_activation_views.xml',
+        'views/partner_assign_menus.xml',
+        'views/res_partner_views_copy.xml',
         'views/snippets.xml',
     ],
     'demo': [
