c3cbd2ab6ef050fa2db938b024df9177cafc46ad
Abdo Mongy <abmn@odoo.com>
2025-07-30 16:33:13 +0200

    [IMP] account: Export Invoices in all supported extensions in ZIP
    With PEPPOL, many clients use Odoo for invoicing while their accountant uses another tool. To easily send invoices to the accountant, it’s important to export invoices for both sales and purchase.
    
    Adding a `Export ZIP` option to download invoices in all supported extensions (pdf, xml, ..etc) in the same zip.
    
    task-4946367

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -5411,6 +5411,13 @@ class AccountMove(models.Model):
 
         return report_action
 
+    def action_invoice_download_all(self):
+        return {
+            'type': 'ir.actions.act_url',
+            'url': f'/account/download_invoice_documents/{",".join(map(str, self.ids))}/all?allow_fallback=true',
+            'target': 'download',
+        }
+
     def action_invoice_download_pdf(self, target = "download"):
         return {
             'type': 'ir.actions.act_url',
@@ -6566,7 +6573,7 @@ class AccountMove(models.Model):
         """ Helper to dynamically add items in the 'Print' menu of list and form of account.move.
         """
         # TO OVERRIDE
-        return []
+        return [{'key': 'download_all', 'description': _('Export ZIP'), **self.action_invoice_download_all()}]
 
     @staticmethod
     def _can_commit():
