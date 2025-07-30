bbc7f548c68101bd9b88bd48258f0c18f713ff69
Max Whale <mawh@odoo.com>
2025-07-30 09:26:26 +0000

    [FIX] hw_drivers: fix devices always being sent when unsupported
    In the forward port of odoo/odoo#218109, a bug was introduced where if
    an unsupported device is connected, the device list is always detected
    as being changed, so the IoT box sends devices to the database every
    3 seconds. This doesn't cause any problems other than spamming the logs
    and the DB with requests, but should be fixed regardless.
    
    After this commit, the unsupported devices are taken into account
    correctly when detecting device changes.
    
    closes odoo/odoo#221272
    
    X-original-commit: 2c6814e7c88afdc6ef4cb5beec81d45eceda2f30
    Signed-off-by: Louis Travaux (lotr) <lotr@odoo.com>
    Signed-off-by: Max Whale (mawh) <mawh@odoo.com>

--- addons/iot_drivers/main.py
+++ addons/iot_drivers/main.py
@@ -32,6 +32,7 @@ class Manager(Thread):
         self.token = helpers.get_token()
         self.version = helpers.get_version(detailed_version=True)
         self.previous_iot_devices = {}
+        self.previous_unsupported_devices = {}
 
     def _get_domain(self):
         """
@@ -51,8 +52,10 @@ class Manager(Thread):
         changed = False
 
         current_devices = set(iot_devices.keys()) | set(unsupported_devices.keys())
-        if current_devices != set(self.previous_iot_devices.keys()):
+        previous_devices = set(self.previous_iot_devices.keys()) | set(self.previous_unsupported_devices.keys())
+        if current_devices != previous_devices:
             self.previous_iot_devices = iot_devices.copy()
+            self.previous_unsupported_devices = unsupported_devices.copy()
             changed = True
 
         # IP address change
@@ -90,7 +93,7 @@ class Manager(Thread):
                 'connection': device.device_connection,
                 'subtype': device.device_subtype if device.device_type == 'printer' else '',
             }
-        devices_list.update(unsupported_devices)
+        devices_list.update(self.previous_unsupported_devices)
         devices_list_to_send = {
             key: value for key, value in devices_list.items() if key != 'distant_display'
         }  # Don't send distant_display to the db
