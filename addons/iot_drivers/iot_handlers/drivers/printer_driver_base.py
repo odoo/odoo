caa811ae5ae6f7dfbeeb554cf78e2eac24b4cef2
Louis Travaux <lotr@odoo.com>
2025-07-30 16:48:13 +0200

    [FIX] iot_drivers: ensure websocket event for printers
    We now provide the session_id to printer drivers to ensure
    we create an event when the printers notifies it job completion.
    
    Enterprise PR: odoo/enterprise#91356
    
    closes odoo/odoo#221255
    
    Signed-off-by: Max Whale (mawh) <mawh@odoo.com>

--- addons/iot_drivers/iot_handlers/drivers/printer_driver_base.py
+++ addons/iot_drivers/iot_handlers/drivers/printer_driver_base.py
@@ -86,7 +86,7 @@ class PrinterDriverBase(Driver, ABC):
         """
         self.data['status'] = status
         self.data['message'] = message
-        event_manager.device_changed(self)
+        event_manager.device_changed(self, {'session_id': self.data.get('owner')})
 
     def print_receipt(self, data):
         _logger.debug("print_receipt called for printer %s", self.device_name)
