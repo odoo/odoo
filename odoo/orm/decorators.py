f85d9cd1124ba7bf48f7918a9d0f07282991efa5
Krzysztof Magusiak (krma) <krma@odoo.com>
2025-07-30 13:31:19 +0000

    [IMP] core: @api.deprecated
    Like `warnings.deprecated` but available before python 3.13. The
    simplified version is implemented only for callables and does not work
    for classes.
    
    The decorator adds a `__deprecated__` attribute on the caller containing
    the message. This can be used for documentation purposes.

--- odoo/orm/decorators.py
+++ odoo/orm/decorators.py
@@ -6,6 +6,7 @@ from __future__ import annotations
 
 import logging
 import typing
+import warnings
 from collections.abc import Mapping
 from functools import wraps
 
@@ -13,6 +14,41 @@ try:
     from decorator import decoratorx as decorator
 except ImportError:
     from decorator import decorator
+
+try:
+    # available since python 3.13
+    from warnings import deprecated
+except ImportError:
+    # simplified version
+    class deprecated:
+        def __init__(
+            self,
+            message: str,
+            /,
+            *,
+            category: type[Warning] | None = DeprecationWarning,
+            stacklevel: int = 1,
+        ) -> None:
+            self.message = message
+            self.category = category
+            self.stacklevel = stacklevel
+
+        def __call__(self, obj, /):
+            message = self.message
+            category = self.category
+            stacklevel = self.stacklevel
+            if category is None:
+                obj.__deprecated__ = message
+                return obj
+            if callable(obj):
+                @wraps(obj)
+                def wrapper(*args, **kwargs):
+                    warnings.warn(message, category=category, stacklevel=stacklevel + 1)
+                    return obj(*args, **kwargs)
+
+                obj.__deprecated__ = wrapper.__deprecated__ = message
+                return wrapper
+            raise TypeError(f"@deprecated decorator cannot be applied to {obj!r}")
 
 if typing.TYPE_CHECKING:
     from collections.abc import Callable, Collection
