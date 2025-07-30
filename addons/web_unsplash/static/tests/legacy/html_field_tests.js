504d443e7fdc089c4d88aedbc5c0169cc14e0311
Panagiotis Kyriakou <paky@odoo.com>
2025-07-30 16:05:04 +0200

    [REM] web_editor: removed legacy QUnit tests
    This PR removes legacy qunit tests that interfere with new features
    implemented now.
    The whole web_editor module will be removed for 19, thus the removal of
    the tests will not have an impact in the future.
    
    closes odoo/odoo#221221
    
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/web_unsplash/static/tests/legacy/html_field_tests.js
+++
@@ -1,5 +0,0 @@
-import { unsplashService } from "@web_unsplash/unsplash_service";
-import { mediaDialogServices } from "@web_editor/../tests/html_field_tests";
-
-// update the list of required services to open the media dialog
-mediaDialogServices.unsplash = unsplashService;
