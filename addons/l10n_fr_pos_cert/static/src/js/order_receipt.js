671737f3c6e3dc9eac41710f890f6788d815dd5b
Hadi El Yakhni <hael@odoo.com>
2025-07-31 15:50:17 +0200

    [FIX] l10n_fr_pos_cert: backport enable self-service invoicing for french companies
    This commit backports parts of 4fd28add5c22614a4e5a386a440617edefed6d67,
    which originally was only merged into master, since it's unstable.
    
    Here however, we only backport the bare minimum to make the invoice QR
    code visible on the receipt, while keeping the changes stable. We also
    backport the test.
    
    opw-4981573

--- addons/l10n_fr_pos_cert/static/src/js/order_receipt.js
+++ addons/l10n_fr_pos_cert/static/src/js/order_receipt.js
@@ -3,9 +3,6 @@ import { patch } from "@web/core/utils/patch";
 
 patch(OrderReceipt.prototype, {
     get qrCode() {
-        if (this.order.is_french_country()) {
-            return false;
-        }
         return super.qrCode;
     },
 });
