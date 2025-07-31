01a17333596af071001930afff62a6a8eec8d642
Corentin Heinix (cohe) <cohe@odoo.com>
2025-07-31 15:23:39 +0200

    [FIX] stock: avoid false SN duplication warning in Physical Inventory
    A warning appears in Physical Inventory when transferring a product with a serial number between two locations
    
    The `_compute_sn_duplicated()` method incorrectly considered `stock.quant` records with quantity 0
    That mean that it will considered a duplication even if the quant indicate that the product isn't in that location
    
    Additionally, the system parameter `stock.skip_quant_tasks` is enabled on all SaaS environments since version 18.2
    It was added for performance issues:
    (see: https://github.com/odoo/odoo/commit/207ba01819577cf6a2a686fae39dd3d673aec6dd)
    When enabled, the quant merging and removal of zero quants are skipped when opening a Physical Inventory, causing this issue to appear
    
    - Enable Developer Mode
    - Create a New System Parameter (Key: stock.skip_quant_tasks, Value: True)
    - In Settings, enable Storage Locations and Lots & Serial Numbers
    - Duplicate WH/Stock as WH/Stock (copy)
    - Create a product tracked by Unique Serial Number
    - Update its quantity: one at WH/Stock
    - Process an Internal Transfert from WH/Stock to WH/Stock (copy)
    - Go in Operations > Adjustments > Physical Inventory
    
    opw-4944011
    
    closes odoo/odoo#221578
    
    Signed-off-by: Adrien Widart (awt) <awt@odoo.com>

--- addons/stock/tests/test_quant.py
+++ addons/stock/tests/test_quant.py
@@ -912,6 +912,12 @@ class StockQuant(TransactionCase):
         with self.assertRaises(UserError):
             quant_2.with_context(inventory_mode=True).write({'location_id': self.stock_subloc2})
 
+        self.env['stock.quant']._update_available_quantity(product, self.stock_subloc2, -1.0, lot_id=sn1)
+        self.assertRecordValues(product.stock_quant_ids.sorted('id'), [
+            {'location_id': self.stock_subloc2.id, 'quantity': 0.0, 'sn_duplicated': False},
+            {'location_id': self.stock_subloc3.id, 'quantity': 1.0, 'sn_duplicated': False},
+        ])
+
     def test_update_quant_with_forbidden_field_02(self):
         """
         Test that updating the package from the quant raise an error
