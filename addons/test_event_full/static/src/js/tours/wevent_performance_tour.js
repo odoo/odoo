946bf5587572f70e01a605911958b7ac1fabdf80
Romain ROSANO (rros) <rros@odoo.com>
2025-07-31 17:27:13 +0200

    [FIX] test_event_full,website_sale: reactivation of a test
    To go along with a recent commit, this old commented test has been reactivated
    and modified to match the recent component change on the Website Event.
    
    task-4822074
    
    Part-of: odoo/odoo#219702
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/test_event_full/static/src/js/tours/wevent_performance_tour.js
+++ addons/test_event_full/static/src/js/tours/wevent_performance_tour.js
@@ -1,4 +1,3 @@
-import { queryOne } from "@odoo/hoot-dom";
 import { registry } from "@web/core/registry";
 import * as wsTourUtils from '@website_sale/js/tours/tour_utils';
 
@@ -7,56 +6,52 @@ var registerSteps = [{
     trigger: 'button.btn-primary:contains("Register")',
     run: "click",
 }, {
-    content: "Select 2 units of 'Ticket1' ticket type",
-    trigger: '#o_wevent_tickets_collapse .row.o_wevent_ticket_selector[name="Ticket1"] select',
-    run: "select 2",
+    content: "Add 2 units of 'Ticket1' ticket type by clicking the '+' button",
+    trigger: 'button[data-increment-type*="plus"]',
+    run: "dblclick",
 }, {
-    content: "Select 1 unit of 'Ticket2' ticket type",
-    trigger: '#o_wevent_tickets_collapse .row.o_wevent_ticket_selector[name="Ticket2"] select',
-    run: "select 1",
+    content: "Edit 1 unit of 'Ticket2' ticket type",
+    trigger: '.modal input:eq(2)',
+    run: "edit 1",
 }, {
     content: "Click on 'Register' button",
     trigger: '#o_wevent_tickets .btn-primary:contains("Register"):not(:disabled)',
     run: 'click',
 }, {
-    content: "Fill attendees details",
-    trigger: 'form[id="attendee_registration"] .btn[type=submit]',
-    run: function () {
-            document.querySelector("input[name*='1-name']").value = "Raoulette Poiluchette";
-            document.querySelector("input[name*='1-phone']").value = "0456112233";
-            document.querySelector("input[name*='1-email']").value = "raoulette@example.com";
-            document.querySelector("div[name*='Question1'] select[name*='1-simple_choice']").value =
-                queryOne("select[name*='1-simple_choice'] option:contains('Q1-Answer2')").value;
-            document.querySelector("div[name*='Question2'] select[name*='1-simple_choice']").value =
-                queryOne("select[name*='1-simple_choice'] option:contains('Q2-Answer1')").value;
-            document.querySelector("input[name*='2-name']").value = "Michel Tractopelle";
-            document.querySelector("input[name*='2-phone']").value = "0456332211";
-            document.querySelector("input[name*='2-email']").value = "michel@example.com";
-            document.querySelector("div[name*='Question1'] select[name*='2-simple_choice']").value =
-                queryOne("select[name*='2-simple_choice'] option:contains('Q1-Answer1')").value;
-            document.querySelector("div[name*='Question2'] select[name*='2-simple_choice']").value =
-                queryOne("select[name*='2-simple_choice'] option:contains('Q2-Answer2')").value;
-            document.querySelector("input[name*='3-name']").value = "Hubert Boitaclous";
-            document.querySelector("input[name*='3-phone']").value = "0456995511";
-            document.querySelector("input[name*='3-email']").value = "hubert@example.com";
-            document.querySelector("div[name*='Question1'] select[name*='3-simple_choice']").value =
-                queryOne("select[name*='3-simple_choice'] option:contains('Q1-Answer2')").value;
-            document.querySelector("div[name*='Question2'] select[name*='3-simple_choice']").value =
-                queryOne("select[name*='3-simple_choice'] option:contains('Q2-Answer2')").value;
-            document.querySelector("textarea[name*='question_answer']").textContent =
-                "Random answer from random guy";
-    },
-},
-{
-    trigger: "input[name*='1-name'], input[name*='2-name'], input[name*='3-name']",
-},
-{
+    content: "Choose the 'Q1-Answer2' answer of the 'Question1' for the first ticket",
+    trigger: 'select[name*="1-simple_choice"]',
+    run: "selectByIndex 2",
+}, {
+    content: "Choose the 'Q2-Answer1' answer of the 'Question2' for the first ticket",
+    trigger: 'select[name*="1-simple_choice"]:last',
+    run: "selectByIndex 1",
+}, {
+    content: "Choose the 'Q1-Answer1' answer of the 'Question1' for the second ticket",
+    trigger: 'select[name*="2-simple_choice"]',
+    run: "selectByIndex 1",
+}, {
+    content: "Choose the 'Q2-Answer2' answer of the 'Question2' for the second ticket",
+    trigger: 'select[name*="2-simple_choice"]:last',
+    run: "selectByIndex 2",
+}, {
+    content: "Choose the 'Q1-Answer2' answer of the 'Question1' for the third ticket",
+    trigger: 'select[name*="3-simple_choice"]',
+    run: "selectByIndex 2",
+}, {
+    content: "Choose the 'Q2-Answer2' answer of the 'Question2' for the third ticket",
+    trigger: 'select[name*="3-simple_choice"]:last',
+    run: "selectByIndex 2",
+}, {
+    content: "Fill the text content of the 'Question3' for the third ticket",
+    trigger: 'textarea[name*="text_box"]',
+    run: "edit Random answer from random guy",
+}, {
     content: "Validate attendees details",
-    trigger: 'button[type=submit]',
+    trigger: 'button[type=submit]:last',
     run: 'click',
     expectUnloadPage: true,
 },
-wsTourUtils.fillAdressForm({
+...wsTourUtils.fillAdressForm({
     name: "Raoulette Poiluchette",
     phone: "0456112233",
     email: "raoulette@example.com",
@@ -64,6 +59,12 @@ wsTourUtils.fillAdressForm({
     city: "CheeseCity",
     zip: "8888",
 }),
+{
+    content: "Confirm address",
+    trigger: 'a[name="website_sale_main_button"]',
+    run: "click",
+    expectUnloadPage: true,
+},
 ...wsTourUtils.payWithDemo(),
 ];
 
