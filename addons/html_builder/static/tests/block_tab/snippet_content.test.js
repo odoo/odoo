0cff8200ed3f2cc1f609cb87af61081e945ec454
ahmedgamal2212 <ahsl@odoo.com>
2025-07-31 10:07:25 +0000

    [MOV] website, html_builder: move block_tab tests from website to html_builder
    Move block_tab test module from website_builder to html_builder to
    separate concerns and reduce dependencies when testing html_builder
    
    Adjustments made for the move:
    - Moved conflicting test "click just after drop is redispatched in
    next operation" to snippet_content.test instead of having separate file
    
    - Removed o_colored_levels class from expected structures and helper
      parameters since HTMLBuilder doesn't add it by default like
      websiteBuilder does
    
    - Mocked installSnippetModule method (exists in websiteBuilder but not
    HTMLBuilder) instead of mocking the RPC call as it won't success
    
    - Updated to use setupHTMLBuilder's direct contentEl return instead of
    getEditableContent function
    
    - Fixed getSnippetStructure to include missing moduleId,
    moduleDisplayName, and data attributes
    
    - Added snippets parameter to setupHTMLBuilder along with the default
    fallback for more usage flexibility
    
    - Moved web.assets_frontend loading to waitForSnippetDialog to
    fix timing issues as it was taking too much time to load and causes
    tests to fail
    
    X-original-commit: ed3d0e7d82f4428d42760b5138ca2f3029f735f3
    Part-of: odoo/odoo#222734
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Ahmed Gamal Saleh (ahsl) <ahsl@odoo.com>

--- addons/html_builder/static/tests/block_tab/snippet_content.test.js
+++ addons/html_builder/static/tests/block_tab/snippet_content.test.js
@@ -1,8 +1,18 @@
 import { describe, expect, test } from "@odoo/hoot";
-import { animationFrame, click, queryAll, queryAllTexts } from "@odoo/hoot-dom";
-import { contains } from "@web/../tests/web_test_helpers";
-import { setupHTMLBuilder } from "@html_builder/../tests/helpers";
-import { getDragHelper, waitForEndOfOperation } from "../website_helpers";
+import {
+    animationFrame,
+    click,
+    Deferred,
+    queryAll,
+    queryAllTexts,
+    queryOne,
+    waitFor,
+} from "@odoo/hoot-dom";
+import { contains, patchWithCleanup } from "@web/../tests/web_test_helpers";
+import { setupHTMLBuilder, getDragHelper, waitForEndOfOperation } from "../helpers";
+import { Operation } from "@html_builder/core/operation";
+import { BuilderOptionsPlugin } from "@html_builder/core/builder_options_plugin";
+import { loadBundle } from "@web/core/assets";
 
 describe.current.tags("desktop");
 
@@ -122,4 +132,66 @@ test("A snippet should appear disabled if there is nowhere to drop it", async ()
     });
     expect(contentEl).toHaveInnerHTML("");
     expect(".o_block_tab .o_snippet.o_disabled").toHaveCount(2);
+});
+
+test.tags("desktop");
+test("click just after drop is redispatched in next operation", async () => {
+    const nextDef = new Deferred();
+    patchWithCleanup(Operation.prototype, {
+        next(fn, ...args) {
+            const originalFn = fn;
+            fn = async () => {
+                await originalFn();
+                nextDef.resolve();
+            };
+            expect.step(`next${args[0]?.shouldInterceptClick ? " should intercept" : ""}`);
+            const res = super.next(fn, ...args);
+            return res;
+        },
+    });
+    patchWithCleanup(BuilderOptionsPlugin.prototype, {
+        async onClick(ev) {
+            expect.step("onClick");
+            super.onClick(ev);
+        },
+        updateContainers(...args) {
+            expect.step("updateContainers");
+            super.updateContainers(...args);
+        },
+    });
+    await setupHTMLBuilder("", {
+        styleContent: /*css*/ `
+            .o_loading_screen {
+                position: absolute;
+                inset: 0;
+            }
+            section {
+                height: 100%; /* to easily target */
+            }`,
+    });
+
+    // TODO: the next lines replicate website's `insertCategorySnippet` helper.
+    // It should be moved to html_builder.
+    await contains(".o-snippets-menu #snippet_groups .o_snippet_thumbnail_area").click();
+    await animationFrame();
+    await loadBundle("html_builder.iframe_add_dialog", {
+        targetDoc: queryOne("iframe.o_add_snippet_iframe").contentDocument,
+        js: false,
+    });
+    await waitFor(".o_add_snippet_dialog iframe.show.o_add_snippet_iframe");
+    await contains(
+        ".o_add_snippet_dialog .o_add_snippet_iframe:iframe .o_snippet_preview_wrap"
+    ).click();
+    await animationFrame();
+    expect.verifySteps(["next should intercept"]); // On snippet selected
+
+    await waitFor(":iframe .o_loading_screen");
+    await click(":iframe", { position: { x: 200, y: 50 }, relative: true });
+    expect.verifySteps(["next"]); // On click
+    await nextDef;
+    expect.verifySteps(["updateContainers"]); // End of drop, on addStep()
+    await animationFrame();
+    expect.verifySteps(["onClick", "next", "updateContainers"]); // On click redispatched
+    await animationFrame();
+    expect(".o-snippets-tabs .o-hb-btn.active").toHaveText("Edit");
 });
