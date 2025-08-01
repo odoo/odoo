7cf754d0ddb98d7ba196e1223d3f0872f7abcff5
Mohamed GadAlrab <myah@odoo.com>
2025-08-01 16:54:47 +0200

    [FIX] account: prevent sending auto email with payment register
    When creating an invoice payment with the "automatic invoice" option
    enabled in Sales settings and using a saved payment token,
    an email is sent to the customer before the transaction move
    is posted and the invoice payment status is updated to "In Payment."
    This results in the email incorrectly asking the customer to remit payment,
    even though the payment is already being processed.
    
    Steps to Reproduce:
    1. Enable "Automatic Invoicing" from Sales settings.
    2. Enable and publish any payment provider (e.g., Demo) in test mode.
    3. Create an invoice and generate a payment link.
       Open the link in a new incognito tab and pay using any dummy card number
       (ensure the "Save my payment details" checkbox is checked).
       This saves the payment token for the partner.
    4. Create a new invoice with the same partner, then register payment.
       Select the payment method and the previously saved token, then confirm.
    5. Observe that the payment status is "In Payment," but the email sent
    to the customer incorrectly asks them to remit payment.
    
    Fix:
    This change prevents payment notification emails from being sent automatically
    when the payment is manually created from the payment register wizard.
    
    opw-4850293
    
    closes odoo/odoo#221587
    
    Related: odoo/enterprise#91570
    Signed-off-by: Paolo Gatti (pgi) <pgi@odoo.com>

--- addons/sale/models/payment_transaction.py
+++ addons/sale/models/payment_transaction.py
@@ -124,7 +124,7 @@ class PaymentTransaction(models.Model):
             # even if only a partial payment was made.
             self._invoice_sale_orders()
         super()._reconcile_after_done()
-        if auto_invoice:
+        if auto_invoice and not self.env.context.get('skip_sale_auto_invoice_send'):
             if (
                 str2bool(self.env['ir.config_parameter'].sudo().get_param('sale.async_emails'))
                 and (send_invoice_cron := self.env.ref('sale.send_invoice_cron', raise_if_not_found=False))
