odoo/odoo

commit b75394102bec3e25b23c75fd82ca0d8dec2726c5

[IMP] purchase: apply improvements on the upload bills functionality
This commit will apply the following improvements in the Upload Bills functionality:
1- change validation Error message to use 'vendor' instead of 'partner' when uploading bills to multiple vendors.
2- make this validation error appears once the user click on 'Upload Bills' instead of appearing after uploading the files.
3- auto-fill the vendor when uploading bills from the 'Vendor Bills' smart button in the PO.

Task-4748285

closes odoo/odoo#209851

Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

addons/purchase/models/purchase_order.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from pytz import timezone
-
+from ast import literal_eval
 from markupsafe import escape, Markup
 from werkzeug.urls import url_encode
 
@@ -711,7 +711,7 @@ class PurchaseOrder(models.Model):
             return self.action_view_invoice(invoices)
 
         if len(invoices) != 1:
-            raise ValidationError(_("You can only upload a bill for a single partner at a time."))
+            raise ValidationError(_("You can only upload a bill for a single vendor at a time."))
         invoices.with_context(skip_is_manually_modified=True)._extend_with_attachments(attachments, new=True)
 
         invoices.with_context(
@@ -855,6 +855,8 @@ class PurchaseOrder(models.Model):
         else:
             result = {'type': 'ir.actions.act_window_close'}
 
+        result['context'] = literal_eval(result['context'])
+        result['context']['default_partner_id'] = self.partner_id.id
         return result
 
     @api.model

addons/purchase/static/src/components/purchase_file_uploader/purchase_file_uploader.js
@@ -2,6 +2,8 @@ import { useService } from "@web/core/utils/hooks";
 import { registry } from "@web/core/registry";
 import { standardWidgetProps } from "@web/views/widgets/standard_widget_props";
 import { FileUploader } from "@web/views/fields/file_handler";
+import { WarningDialog } from "@web/core/errors/error_dialogs";
+import { _t } from "@web/core/l10n/translation";
 
 import { Component } from "@odoo/owl";
 
@@ -17,6 +19,7 @@ export class PurchaseFileUploader extends Component {
     setup() {
         this.orm = useService("orm");
         this.action = useService("action");
+        this.dialog = useService("dialog");
         this.attachmentIdsToProcess = [];
     }
 
@@ -33,6 +36,20 @@ export class PurchaseFileUploader extends Component {
             return this.props.record.data.id;
         }
         return this.props.list.getResIds(true);
+    }
+
+    onClick(ev) {
+        if (this.env.config.viewType !== "list") {
+            return;
+        }
+        const vendorSet = new Set(this.props.list.selection.map((record) => record.data.partner_id.id));
+        if (vendorSet.size > 1) {
+            this.dialog.add(WarningDialog, {
+                title: _t("Validation Error"),
+                message: _t("You can only upload a bill for a single vendor at a time."),
+            });
+            return false;
+        }
     }
 
     async onFileUploaded(file) {

addons/purchase/static/src/components/purchase_file_uploader/purchase_file_uploader.xml
@@ -4,6 +4,7 @@
             acceptedFileExtensions="props.acceptedFileExtensions"
             fileUploadClass="'document_file_uploader'"
             multiUpload="true"
+            onClick.bind="onClick"
             onUploaded.bind="onFileUploaded"
             onUploadComplete.bind="onUploadComplete">
             <t t-set-slot="toggler">

commit e04bf9515825149c2f03a8ff947e310c03826085

[FIX] purchase: prevent traceback if creating bill for multiple vendors
Bug introduced by: https://github.com/odoo/odoo/commit/b75394102bec3e25b23c75fd82ca0d8dec2726c5#diff-1281da5f4d0a3daaf162a2e6456469537d1b74b8034437783c9c717307aa8fcdR859

Steps to reproduce:
- Create two purchase orders with different vendors
- Add any product and confirm both orders
- From the purchase order list view, select both orders
- Click on "Create Bill"

Issue:
A traceback occurs when selecting multiple purchase orders with
different vendors and clicking Create Bill from the list view. This
is due to default_partner_id being forcibly set in the context, which
assumes a single vendor.

This bug was introduced by commit b753941, where default_partner_id is
always injected into the context, even when multiple vendors are
involved: https://github.com/odoo/odoo/commit/b75394102bec3e25b23c75fd82ca0d8dec2726c5#diff-1281da5f4d0a3daaf162a2e6456469537d1b74b8034437783c9c717307aa8fcdR714

Why it breaks:
When creating a bill, action_view_invoice() is called without verifying
that only one vendor is selected. Setting a default_partner_id in such
cases leads to invalid assumptions and crashes.

Difference with attachments case:
When uploading bills from attachments, a check does ensure a single
vendor is selected before calling action_view_invoice() (see b753941#R714).

Fix:
Only inject default_partner_id into the context if all selected
purchase orders share the same vendor.

opw-4948142

addons/purchase/models/purchase_order.py
@@ -926,7 +926,8 @@ class PurchaseOrder(models.Model):
             result = {'type': 'ir.actions.act_window_close'}
 
         result['context'] = literal_eval(result['context'])
-        result['context']['default_partner_id'] = self.partner_id.id
+        if len(self.partner_id) == 1:
+            result['context']['default_partner_id'] = self.partner_id.id
         return result
 
     @api.model

addons/purchase/tests/test_purchase_invoice.py
@@ -1164,3 +1164,41 @@ class TestInvoicePurchaseMatch(TestPurchaseToInvoiceCommon):
         move_form.purchase_vendor_bill_id = self.env['purchase.bill.union'].browse(-po2.id)
         invoice2 = move_form.save()
         self.assertFalse(invoice2.invoice_user_id)
+
+    def test_create_invoice_from_multiple_purchase_orders(self):
+        """ Test that invoices can be created from purchase orders with different
+        vendors without raising errors and with correct vendor mapping per invoice.
+        """
+        purchase_orders = self.env['purchase.order'].with_context(tracking_disable=True).create([
+            {
+                'partner_id': self.partner_a.id,
+                'order_line': [
+                    Command.create({
+                        'product_id': self.product_order.id,
+                        'product_qty': 1.0,
+                        'price_unit': self.product_order.list_price,
+                        'tax_ids': False,
+                    }),
+                ],
+            },
+            {
+                'partner_id': self.partner_b.id,
+                'order_line': [
+                    Command.create({
+                        'product_id': self.product_deliver.id,
+                        'product_qty': 2.0,
+                        'price_unit': self.product_deliver.list_price,
+                        'tax_ids': False,
+                    }),
+                ],
+            },
+        ])
+        purchase_orders.button_confirm()
+        purchase_orders.action_create_invoice()
+
+        self.assertEqual(len(purchase_orders.invoice_ids), 2, "Each PO should generate one invoice")
+        self.assertEqual(
+            set(purchase_orders.invoice_ids.partner_id.ids),
+            set(purchase_orders.partner_id.ids),
+            "Each invoice should be linked to the correct vendor"
+        )