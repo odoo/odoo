c27067a98cc5790db37bf59da003e04b1a4ffb8d
Martin Quinteros <maq@adhoc.com.ar>
2025-08-01 11:26:29 -0300

    [IMP] l10n_latam_check : add split lines in 3 party checks We've added the liquidity line split for third-party checks, just like we do for our own checks.
    The reasons are:
    
    Traceability in the General Ledger
    It's essential to be able to identify which specific checks were deposited in each operation within the check account's general ledger. With the current grouping method, this information is lost.
    
    Operational Control and Reconciliation
    In situations involving errors, rejections, or claims, it's necessary to have clear traceability for each check from the moment it's received until it's deposited. Splitting the payments allows for this tracking directly from the accounting records, eliminating the need to rely on external information or indirect tracing methods.
    
    Facilitates Internal and External Audits
    Having a separate entry for each check simplifies the accounting review process. It allows for a direct cross-reference between the movements recorded in the check account and the physical or digitized documents.
    
    Consistency with the Entry Record
    When a check is received (collected), it is recorded individually in the check account. By maintaining this same level of detail at the time of deposit, we ensure consistency across all movements.

--- addons/l10n_latam_check/models/account_payment.py
+++ addons/l10n_latam_check/models/account_payment.py
@@ -181,10 +181,10 @@ class AccountPayment(models.Model):
     def _prepare_move_line_default_vals(self, write_off_line_vals=None, force_balance=None):
         """ Add check name and operation on liquidity line """
         res = super()._prepare_move_line_default_vals(write_off_line_vals=write_off_line_vals, force_balance=force_balance)
-
-        if self.payment_method_code == 'own_checks' and self.payment_type == 'outbound':
+        latam_checks = self.l10n_latam_new_check_ids | self.l10n_latam_move_check_ids
+        if latam_checks:
             del res[0]
-            for check_id in self.l10n_latam_new_check_ids:
+            for check_id in latam_checks:
                 if self.payment_type == 'inbound':
                     # Receive money.
                     liquidity_amount_currency = check_id.amount
@@ -215,17 +215,6 @@ class AccountPayment(models.Model):
                             'account_id': self.outstanding_account_id.id,
                             'l10n_latam_check_ids': [Command.link(check_id.id)],
                 })
-        # we dont check the payment method code because when deposited on bank/cash journals pay method is manual but we still change the label
-        # we dont want this names on the own checks because it doesn't add value, already each split/check line will have it name
-        elif (self.l10n_latam_new_check_ids or self.l10n_latam_move_check_ids) and self.payment_method_code != 'own_checks':
-            check_name = [check_name for check_name in (self.l10n_latam_new_check_ids | self.l10n_latam_move_check_ids).mapped('name') if check_name]
-            document_name = (
-                _('Checks %s received') if self.payment_type == 'inbound' else _('Checks %s delivered')) % (
-                ', '.join(check_name)
-            )
-            res[0].update({
-                'name': document_name + ' - ' + ''.join([item[1] for item in self._get_aml_default_display_name_list()]),
-            })
         return res
 
     @api.depends('l10n_latam_move_check_ids')
