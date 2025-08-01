3b988e802529da4696620fda3c76c4fae3bb1be9
Sébastien (blse) <blse@odoo.com>
2025-08-01 11:12:24 +0200

    [FIX] html_builder, *: replicate changes to occurences of the same field
    *: website
    
    In the previous website builder, editing one field that occured
    several times on the page used to change all occurences. With the
    initial [website builder refactor], that behavior was lost. This commit
    reintroduces it.
    
    To replicate changes, there is one listener for `handleNewRecords` to
    know which fields were edited. And a `normalize_handler` that replicates
    the content of the fields.
    
    We do not do the whole operation during normalize because we do not have
    the information about which occurence is the source change if the root
    is a parent of several of them. And we do not do the whole operation
    during `handleNewRecords` to avoid getting called for the mutations
    caused by replications, and avoid doing extra work.
    
    Steps to reproduce:
    - Open website builder
    - Edit one of the menu in the header
    - Switch to mobile view
    - Open the hamburger menu
    - Bug: the change in the menu is not replicated in the menu of mobile
    
    [website builder refactor]: 3ced5e9232d3c5f71259f1f2b692416932e3ebd2
    task-4367641
    
    Part-of: odoo/odoo#225171
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Sébastien Blondiau (blse) <blse@odoo.com>

---
+++ addons/html_builder/static/tests/field_change_replication_plugin.test.js
@@ -0,0 +1,139 @@
+import { describe, expect, test } from "@odoo/hoot";
+import { setupHTMLBuilder } from "./helpers";
+import { queryOne } from "@odoo/hoot-dom";
+import { undo } from "@html_editor/../tests/_helpers/user_actions";
+
+describe("replicate changes", () => {
+    test("translated elements", async () => {
+        const { editor } = await setupHTMLBuilder("", {
+            headerContent: `
+            <div class="test-1">
+                <span data-oe-model="ir.ui.view" data-oe-id="600" data-oe-field="arch_db" data-oe-translation-state="translated" data-oe-translation-source-sha="4242">Contactez-nous</span>
+            </div>
+            <div class="test-2">
+                <span data-oe-model="ir.ui.view" data-oe-id="600" data-oe-field="arch_db" data-oe-translation-state="translated" data-oe-translation-source-sha="4242">Contactez-nous</span>
+            </div>
+        `,
+        });
+        queryOne(":iframe .test-2 span").append(" ici");
+        editor.shared.history.addStep();
+        expect(":iframe span:contains(Contactez-nous ici)").toHaveCount(2);
+    });
+
+    test("menu items", async () => {
+        const { editor } = await setupHTMLBuilder("", {
+            headerContent: `
+            <div class="test-1">
+                <span data-oe-model="website.menu" data-oe-id="5" data-oe-field="name" data-oe-type="char" data-oe-expression="submenu.name">Home</span>
+            </div>
+            <div class="test-2">
+                <span data-oe-model="website.menu" data-oe-id="5" data-oe-field="name" data-oe-type="char" data-oe-expression="submenu.name">Home</span>
+            </div>
+        `,
+        });
+        queryOne(":iframe .test-1 span").append("y");
+        editor.shared.history.addStep();
+        expect(":iframe span:contains(Homey)").toHaveCount(2);
+    });
+
+    test("contact", async () => {
+        const { editor } = await setupHTMLBuilder("", {
+            headerContent: `
+            <div class="test-1">
+                <span data-oe-xpath="/t[1]/div[1]/div[2]/span[1]" data-oe-model="blog.post" data-oe-id="1" data-oe-field="author_id" data-oe-type="contact" data-oe-expression="blog_post.author_id" data-oe-many2one-id="3" data-oe-many2one-model="res.partner" data-oe-contact-options="{&quot;widget&quot;: &quot;contact&quot;, &quot;fields&quot;: [&quot;name&quot;], &quot;tagName&quot;: &quot;span&quot;, &quot;expression&quot;: &quot;blog_post.author_id&quot;, &quot;type&quot;: &quot;contact&quot;, &quot;inherit_branding&quot;: true, &quot;translate&quot;: false}">
+                    <address class="o_portal_address mb-0">
+                        <div>
+                                <span itemprop="name">YourCompany, Mitchell Admin</span>
+                        </div>
+                        <div class="gap-2" itemscope="itemscope" itemtype="http://schema.org/PostalAddress">
+                            <div itemprop="telephone"></div>
+                        </div>
+                    </address>
+                </span>
+            </div>
+            <div class="test-2">
+                <span data-oe-xpath="/t[1]/div[1]/div[2]/span[1]" data-oe-model="blog.post" data-oe-id="1" data-oe-field="author_id" data-oe-type="contact" data-oe-expression="blog_post.author_id" data-oe-many2one-id="3" data-oe-many2one-model="res.partner" data-oe-contact-options="{&quot;widget&quot;: &quot;contact&quot;, &quot;fields&quot;: [&quot;name&quot;], &quot;tagName&quot;: &quot;span&quot;, &quot;expression&quot;: &quot;blog_post.author_id&quot;, &quot;type&quot;: &quot;contact&quot;, &quot;inherit_branding&quot;: true, &quot;translate&quot;: false}">
+                    <address class="o_portal_address mb-0">
+                        <div>
+                                <span itemprop="name">YourCompany, Mitchell Admin</span>
+                        </div>
+                        <div class="gap-2" itemscope="itemscope" itemtype="http://schema.org/PostalAddress">
+                            <div itemprop="telephone"></div>
+                        </div>
+                    </address>
+                </span>
+            </div>
+        `,
+        });
+        queryOne(":iframe .test-1 > *").append("changed");
+        editor.shared.history.addStep();
+        expect(":iframe .test-1 > *").toHaveText(/changed/);
+        expect(":iframe .test-2 > *").toHaveText(/changed/);
+    });
+
+    test("should not add o_dirty marks on the ones receiving the replicated changes", async () => {
+        const { editor } = await setupHTMLBuilder("", {
+            headerContent: `
+            <div class="test-1">
+                <span data-oe-model="ir.ui.view" data-oe-id="600" data-oe-field="arch_db" data-oe-translation-state="translated" data-oe-translation-source-sha="4242">Contactez-nous</span>
+            </div>
+            <div class="test-2">
+                <span data-oe-model="ir.ui.view" data-oe-id="600" data-oe-field="arch_db" data-oe-translation-state="translated" data-oe-translation-source-sha="4242">Contactez-nous</span>
+            </div>
+            <div class="test-3">
+                <span data-oe-model="ir.ui.view" data-oe-id="600" data-oe-field="arch_db" data-oe-translation-state="translated" data-oe-translation-source-sha="4242">Contactez-nous</span>
+            </div>
+        `,
+        });
+        const span1 = queryOne(":iframe .test-1 span");
+        const span2 = queryOne(":iframe .test-2 span");
+        const span3 = queryOne(":iframe .test-3 span");
+
+        span2.append(" ici");
+        editor.shared.history.addStep();
+        expect(span1).not.toHaveClass("o_dirty");
+        expect(span2).toHaveClass("o_dirty");
+        expect(span3).not.toHaveClass("o_dirty");
+        expect([span1, span2, span3]).toHaveText("Contactez-nous ici");
+
+        span1.append("!");
+        editor.shared.history.addStep();
+        expect(span1).toHaveClass("o_dirty");
+        expect(span2).toHaveClass("o_dirty");
+        expect(span3).not.toHaveClass("o_dirty");
+        expect([span1, span2, span3]).toHaveText("Contactez-nous ici!");
+
+        undo(editor);
+        expect(span1).not.toHaveClass("o_dirty");
+        expect(span2).toHaveClass("o_dirty");
+        expect(span3).not.toHaveClass("o_dirty");
+        expect([span1, span2, span3]).toHaveText("Contactez-nous ici");
+    });
+
+    test("changing several of occurences at the same time should converge to the same value", async () => {
+        const { editor } = await setupHTMLBuilder("", {
+            headerContent: `
+            <div class="test-1">
+                <span data-oe-model="ir.ui.view" data-oe-id="600" data-oe-field="arch_db" data-oe-translation-state="translated" data-oe-translation-source-sha="4242">Contactez-nous</span>
+            </div>
+            <div class="test-2">
+                <span data-oe-model="ir.ui.view" data-oe-id="600" data-oe-field="arch_db" data-oe-translation-state="translated" data-oe-translation-source-sha="4242">Contactez-nous</span>
+            </div>
+            <div class="test-3">
+                <span data-oe-model="ir.ui.view" data-oe-id="600" data-oe-field="arch_db" data-oe-translation-state="translated" data-oe-translation-source-sha="4242">Contactez-nous</span>
+            </div>
+        `,
+        });
+        const span1 = queryOne(":iframe .test-1 span");
+        const span2 = queryOne(":iframe .test-2 span");
+        const span3 = queryOne(":iframe .test-3 span");
+
+        span2.append(" ici");
+        span1.append("!");
+        editor.shared.history.addStep();
+        expect(span1).toHaveClass("o_dirty");
+        expect(span2).toHaveClass("o_dirty");
+        expect(span3).not.toHaveClass("o_dirty");
+        expect([span2, span3]).toHaveText(span1.textContent); // all the same text
+    });
+});
