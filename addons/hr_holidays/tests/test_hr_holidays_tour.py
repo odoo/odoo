85028d6630ef8753e7297c7f913ac214338de472
Mahdi Alijani (malj) <malj@odoo.com>
2025-08-04 08:52:43 +0000

    [FIX] hr_holidays: fix the tour employee_holidays_archived_tour
    The #217517 PR is causing a test failure on runbot.
    The problem was due to use of
    ```$('.o_stat_info:contains("Time Off")')```
    to get the element.
    To avoid unnecessary use of tour, the test is re-written.
    
    runbot-230486
    
    X-original-commit: 830ed3319efebd8ea7d54d3a5997ad3803b54ddc

--- addons/hr_holidays/tests/test_hr_holidays_tour.py
+++ addons/hr_holidays/tests/test_hr_holidays_tour.py
@@ -43,37 +43,3 @@ class TestHrHolidaysTour(HttpCase):
         })
 
         self.start_tour('/odoo', 'hr_holidays_tour', login="admin")
-
-    @freeze_time('01/17/2022')
-    def test_employee_holidays_archived_tour(self):
-        admin_user = self.env.ref('base.user_admin')
-
-        test_user = self.env['res.users'].create({
-            'name': 'test_user',
-            'login': 'test_user',
-            'email': 'test_user@yourcompany.com',
-            'company_id': admin_user.company_id.id
-        })
-
-        employee = self.env['hr.employee'].create({
-            'name': 'test_user',
-            'user_id': test_user.id,
-        })
-
-        leave_type = self.env['hr.leave.type'].with_user(admin_user)
-
-        holidays_type_1 = leave_type.create({
-            'name': 'archived_holidays',
-            'active': False
-        })
-
-        self.env['hr.leave.allocation'].create({
-            'name': 'archived_holidays_allocation',
-            'employee_id': employee.id,
-            'holiday_status_id': holidays_type_1.id,
-            'number_of_days': 10,
-            'state': 'confirm',
-            'date_from': '2022-01-01',
-        })
-
-        self.start_tour('/web', 'employee_holidays_archived_tour', login="admin")
