08eb8b77dcec004a3ed5e02e1a3bfdea3c2dc2ad
Valeriya(vchu) <vchu@odoo.com>
2025-08-04 16:49:31 +0200

    [IMP] website_sale: update shop onboarding templates selection
    task-4863066
    
    closes odoo/odoo#214744
    
    Related: odoo/upgrade#8017
    Signed-off-by: Valentin Chevalier <vcr@odoo.com>

--- addons/website_sale/models/theme_utils.py
+++ addons/website_sale/models/theme_utils.py
@@ -1,8 +1,27 @@
-from odoo import models
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, models
 
 
 class ThemeUtils(models.AbstractModel):
     _inherit = 'theme.utils'
+
+    category_style_templates = [
+        'website_sale.filmstrip_categories_bordered',
+        'website_sale.filmstrip_categories_tabs',
+        'website_sale.filmstrip_categories_pills',
+        'website_sale.filmstrip_categories_images',
+        'website_sale.filmstrip_categories_grid',
+        'website_sale.filmstrip_categories_large_images',
+    ]
+
+    @api.model
+    def enable_view(self, xml_id):
+        """Override of `theme.utils` to disable all category style templates when enabling one."""
+        if xml_id in self.category_style_templates:
+            for template in self.category_style_templates:
+                self.disable_view(template)
+        super().enable_view(xml_id)
 
     @property
     def _footer_templates(self):
