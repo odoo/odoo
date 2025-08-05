407ae7ba9a92e49a086d28f4024ee23f1841bc1f
Imbar Budiman <imbarbudiman@yahoo.com>
2025-08-05 14:37:49 +0700

    [FIX] l10n_id_efaktur_coretax: fixing coretax product code search in product template

--- addons/l10n_id_efaktur_coretax/models/product_code.py
+++ addons/l10n_id_efaktur_coretax/models/product_code.py
@@ -1,10 +1,12 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.osv import expression
 
 class EfakturProductCode(models.Model):
     _name = "l10n_id_efaktur_coretax.product.code"
     _description = "Product categorization according to E-Faktur"
+    _rec_name = "code"
 
     code = fields.Char()
     description = fields.Text()
@@ -14,3 +16,22 @@ class EfakturProductCode(models.Model):
         for record in self:
             result.append((record.id, f"{record.code} - {record.description}"))
         return result
+    
+    @api.model
+    def _name_search(self, name='', args=None, operator='ilike', limit=100, name_get_uid=None):
+        args = args or []
+
+        # Try to reverse the `name_get` structure
+        parts = name.split(' - ')
+        if len(parts) == 2:
+            domain = [('code', operator, parts[0]), ('description', operator, parts[1])]
+            return self._search(expression.AND([domain, args]), limit=limit, access_rights_uid=name_get_uid)
+        
+        if name and operator == 'ilike':
+            domain = ['|',
+                ('code', operator, name),
+                ('description', operator, name),
+            ]
+            return self._search(expression.AND([domain, args]), limit=limit, access_rights_uid=name_get_uid)
+        
+        return super()._name_search(name=name, args=args, operator=operator, limit=limit, name_get_uid=name_get_uid)
