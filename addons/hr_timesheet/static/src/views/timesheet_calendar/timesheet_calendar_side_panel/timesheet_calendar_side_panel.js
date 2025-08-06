306f723cd78183fd99b7d2c8a2b8520860718efa
Mathieu Duckerts-Antoine <dam@odoo.com>
2025-08-06 13:55:17 +0200

    [FIX] hr_timesheet: extensions of calendar side panel
    In https://github.com/odoo/odoo/pull/219098 some buttons have been
    removed from the calendar side panel. We did forget to check all extensions
    of the side panel template, so that some crash occur in some views.
    We fix that by removing some templates/components that are now useless.
    
    closes odoo/odoo#222058
    
    Related: odoo/enterprise#91808
    Signed-off-by: Vincent Larcin (vila) <vila@odoo.com>

--- addons/hr_timesheet/static/src/views/timesheet_calendar/timesheet_calendar_side_panel/timesheet_calendar_side_panel.js
+++
@@ -1,5 +0,0 @@
-import { TimesheetCalendarMyTimesheetsSidePanel } from "../../timesheet_calendar_my_timesheets/timesheet_calendar_my_timesheets_side_panel/timesheet_calendar_my_timesheets_side_panel";
-
-export class TimesheetCalendarSidePanel extends TimesheetCalendarMyTimesheetsSidePanel {
-    static template = "hr_timesheet.TimesheetCalendarSidePanel";
-}
--- addons/hr_timesheet/static/src/views/timesheet_calendar/timesheet_calendar_view.js
+++ addons/hr_timesheet/static/src/views/timesheet_calendar/timesheet_calendar_view.js
@@ -1,13 +1,9 @@
 import { registry } from "@web/core/registry";
-
 import { timesheetCalendarMyTimesheetsView } from "../timesheet_calendar_my_timesheets/timesheet_calendar_my_timesheets_view";
-
-import { TimesheetCalendarController } from "./timesheet_calendar_controller";
 import { TimesheetCalendarModel } from "./timesheet_calendar_model";
 
 export const timesheetCalendarView = {
     ...timesheetCalendarMyTimesheetsView,
-    Controller: TimesheetCalendarController,
     Model: TimesheetCalendarModel,
 };
 
