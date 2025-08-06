94e7eb60f91ef08aa80dbda14f82ded19754a954
Borna Bešić <borna.besic@jobrad.org>
2025-08-06 12:15:49 +0200

    CPT-628 Remove Werkzeug version check

--- odoo/http.py
+++ odoo/http.py
@@ -257,13 +257,13 @@ ROUTING_KEYS = {
     'alias', 'host', 'methods',
 }
 
-if parse_version(werkzeug.__version__) >= parse_version('2.0.2'):
-    # Werkzeug 2.0.2 adds the websocket option. If a websocket request
-    # (ws/wss) is trying to access an HTTP route, a WebsocketMismatch
-    # exception is raised. On the other hand, Werkzeug 0.16 does not
-    # support the websocket routing key. In order to bypass this issue,
-    # let's add the websocket key only when appropriate.
-    ROUTING_KEYS.add('websocket')
+# JobRad: Removed Werkzeug version check
+
+# Werkzeug 2.0.2 adds the websocket option. If a websocket request
+# (ws/wss) is trying to access an HTTP route, a WebsocketMismatch
+# exception is raised. On the other hand, Werkzeug 0.16 does not
+# support the websocket routing key.
+ROUTING_KEYS.add('websocket')
 
 # The default duration of a user session cookie. Inactive sessions are reaped
 # server-side as well with a threshold that can be set via an optional
