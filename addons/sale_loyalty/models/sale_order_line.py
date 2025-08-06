c8511c4327b349757feb0faaeec59ee74a753308
Levi Siuzdak (sile) <sile@odoo.com>
2025-08-06 11:14:21 +0000

    [FIX] sale_loyalty: keep track of coupon changes on confirmed orders
    Versions
    --------
    - 17.0+
    
    Steps
    -----
    1. Have a coupon program;
    2. add a 10% discount on order reward for 1 point;
    3. add a 50% discount on order reward for 5 points;
    4. generate a coupon with 10 points;
    5. use coupon code on a confirmed order;
    6. select 10% discount reward;
    7. change to a 50% discount reward;
    8. check coupon point total.
    
    Issue
    -----
    Even though the 5 point reward was used, only 4 out of 10 points remain.
    
    Cause
    -----
    When updating the reward line of a confirmed order, it keeps track of
    point cost changes before & after a write. Its purpose is to restore
    back the point difference on the coupon record.
    
    The issue is that while point changes are stored, coupon changes are
    not. When updating reward lines, `_reset_loyalty` is used, which removes
    the `coupon_id` from the lines. As a consequence, attempting to restore
    the point difference on `line.coupon_id` after an update, it writes to
    an empty record.
    
    Solution
    --------
    Store both coupons & their used points before write. After write,
    restore the previous points to the previous coupon, and subtract the
    current point cost from the current coupon. This way, any combination of
    coupon/point changes should have the points updated as expected.
    
    opw-4910922
    
    closes odoo/odoo#230359
    
    X-original-commit: fa643adad3686929a1dd06c7d2271b2003960cc1
    Signed-off-by: Valeriya Chuprina (vchu) <vchu@odoo.com>
    Signed-off-by: Levi Siuzdak <sile@odoo.com>

--- addons/sale_loyalty/models/sale_order_line.py
+++ addons/sale_loyalty/models/sale_order_line.py
@@ -89,13 +89,16 @@ class SaleOrderLine(models.Model):
     def write(self, vals):
         cost_in_vals = 'points_cost' in vals
         if cost_in_vals:
-            previous_cost = {l: l.points_cost for l in self}
+            previous_vals = {line: (line.points_cost, line.coupon_id) for line in self}
         res = super().write(vals)
         if cost_in_vals:
             # Update our coupon points if the order is in a confirmed state
-            for line in self:
-                if previous_cost[line] != line.points_cost and line.state == 'sale':
-                    line.coupon_id.points += (previous_cost[line] - line.points_cost)
+            for line, (previous_cost, previous_coupon) in previous_vals.items():
+                if line.state != 'sale':
+                    continue
+                if line.points_cost != previous_cost or line.coupon_id != previous_coupon:
+                    previous_coupon.points += previous_cost
+                    line.coupon_id.points -= line.points_cost
         return res
 
     def unlink(self):
