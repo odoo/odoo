60081cf0f3f3f9dab1fab23e2529d837eb9e2783
Sven Fuehr <svfu@odoo.com>
2025-08-06 17:35:22 +0200

    [FIX] l10n_es_edi_verifactu_pos,l10_sa_pos: refund reason popup
    In commit 809b9d4e9a16694c7fadda610e9f105eb7451e19 the way data
    is loaded for the session was changed.
    I.e. the refund reasons are not loaded anymore for the
    refund reason popup. Thus the popup is not loaded.
    
    This commit corrects the data loading and makes the popup work again.
    (Both modules use basically the same code.)
    
    closes odoo/odoo#222109
    
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>

--- addons/l10n_sa_pos/models/pos_config.py
+++ addons/l10n_sa_pos/models/pos_config.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import models
+from odoo import api, models
 from odoo.exceptions import UserError
 from odoo.tools.translate import _
 
@@ -13,3 +13,16 @@ class PosConfig(models.Model):
             if not config.company_id.country_id:
                 raise UserError(_("You have to set a country in your company setting."))
         return super().open_ui()
+
+    @api.model
+    def _load_pos_data_read(self, records, config):
+        data = super()._load_pos_data_read(records, config)
+
+        if data and self.env.company.country_id.code == 'SA':
+            l10n_sa_reason_field = self.env['ir.model.fields']._get('account.move', 'l10n_sa_reason')
+            data[0]['_zatca_refund_reasons'] = [
+                {'value': refund_reason.value, 'name': refund_reason.name}
+                for refund_reason in l10n_sa_reason_field.selection_ids
+            ]
+
+        return data
