6e9d9b35c41e8337b9cc17060fea4764416bf8a3
Sven Fuehr <svfu@odoo.com>
2025-08-06 17:35:22 +0200

    [FIX] l10n_es_edi_verifactu_pos,l10_sa_pos: refund reason popup
    In commit 809b9d4e9a16694c7fadda610e9f105eb7451e19 the way data
    is loaded for the session was changed.
    I.e. the refund reasons are not loaded anymore for the
    refund reason popup. Thus the popup is not loaded.
    
    This commit corrects the data loading and makes the popup work again.
    (Both modules use basically the same code.)
    
    closes odoo/odoo#222109
    
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>

--- addons/l10n_es_edi_verifactu_pos/models/pos_config.py
+++ addons/l10n_es_edi_verifactu_pos/models/pos_config.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class PosConfig(models.Model):
@@ -8,3 +8,16 @@ class PosConfig(models.Model):
         string="Veri*Factu Required",
         related='company_id.l10n_es_edi_verifactu_required',
     )
+
+    @api.model
+    def _load_pos_data_read(self, records, config):
+        data = super()._load_pos_data_read(records, config)
+
+        if data and config.l10n_es_edi_verifactu_required:
+            verifactu_refund_reason_field = self.env['ir.model.fields']._get('pos.order', 'l10n_es_edi_verifactu_refund_reason')
+            data[0]['_verifactu_refund_reasons'] = [
+                {'value': refund_reason.value, 'name': refund_reason.name}
+                for refund_reason in verifactu_refund_reason_field.selection_ids
+            ]
+
+        return data
--- addons/l10n_sa_pos/models/__init__.py
+++ addons/l10n_sa_pos/models/__init__.py
@@ -1,6 +1,4 @@
-# -*- encoding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import pos_order
 from . import pos_config
-from . import pos_session
