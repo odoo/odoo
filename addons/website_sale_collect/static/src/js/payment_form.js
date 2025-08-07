01d4f9bd65f49e58c5b9298ee9a0ab558f60860c
Levi Siuzdak (sile) <sile@odoo.com>
2025-08-07 17:46:32 +0200

    [IMP] payment, delivery, website_sale_collect: change Pay button label
    Steps
    -----
    1. Enable the "Pay on site" payment method;
    2. in eCommerce, add a product to your cart you can pick up in store;
    3. go to payment;
    4. select "Pay on site".
    
    Issue
    -----
    The payment button displays "Pay now", even though we're paying later.
    
    Solution
    --------
    Add a `_isPayLaterMethod` hook, which returns `true` for `pay_on_site`
    and `cash_on_delivery`, changing the label from the default to
    "Confirm".
    
    opw-4986791

--- addons/website_sale_collect/static/src/js/payment_form.js
+++ addons/website_sale_collect/static/src/js/payment_form.js
@@ -3,12 +3,12 @@ import PaymentForm from '@payment/js/payment_form';
 PaymentForm.include({
 
     /**
-     * Configure 'cash_on_delivery' as a pay-later method.
+     * Configure 'pay_on_site' as a pay-later method.
      *
      * @override
      */
     _isPayLaterMethod(paymentMethodCode) {
-        return paymentMethodCode === 'cash_on_delivery' || this._super(...arguments);
+        return paymentMethodCode === 'pay_on_site' || this._super(...arguments);
     }
 
 });
