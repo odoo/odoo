669a621c9a69d1b40d094d49933265aa2b9ab13f
romo <romo@odoo.com>
2025-08-07 14:01:46 +0000

    [FIX] website: fix layout option position in sidebar
    The layout option was placed after the background option in the sidebar
    while it was the other way before. Since layout is more important, it
    should be placed at the start of the block.
    
    This commit moves the layout option before the background option.
    
    task-4367641
    
    X-original-commit: 46e5137b4134be0e25788164a6345a4f9ef9ed4e

--- addons/website/static/src/builder/option_sequence.js
+++ addons/website/static/src/builder/option_sequence.js
@@ -9,26 +9,54 @@ import {
 } from "@html_builder/utils/option_sequence";
 
 // Gives names to website options sequence.
-const [WEBSITE_BACKGROUND_OPTIONS, BOX_BORDER_SHADOW, LAYOUT, ...__DETECT_ERROR_WEBSITE_0__] =
-    splitBetween(SNIPPET_SPECIFIC_AFTER, LAYOUT_COLUMN, 3);
+const [
+    LAYOUT, 
+    ...__DETECT_ERROR_WEBSITE_0__
+] = splitBetween(SNIPPET_SPECIFIC_AFTER, LAYOUT_COLUMN, 1);
 if (__DETECT_ERROR_WEBSITE_0__.length > 0) {
     console.error("Wrong count in website split after specific");
 }
-const [LAYOUT_GRID, ...__DETECT_ERROR_WEBSITE_1__] = splitBetween(
-    LAYOUT_COLUMN,
-    VERTICAL_ALIGNMENT,
-    1
-);
+const [
+    WEBSITE_BACKGROUND_OPTIONS,
+    BOX_BORDER_SHADOW,
+    ...__DETECT_ERROR_WEBSITE_1__
+] = splitBetween(VERTICAL_ALIGNMENT, SNIPPET_SPECIFIC_NEXT, 2);
 if (__DETECT_ERROR_WEBSITE_1__.length > 0) {
+    console.error("Wrong count in website split after vertical alignment");
+}
+const [
+    LAYOUT_GRID, 
+    ...__DETECT_ERROR_WEBSITE_2__
+] = splitBetween(LAYOUT_COLUMN, VERTICAL_ALIGNMENT, 1);
+if (__DETECT_ERROR_WEBSITE_2__.length > 0) {
     console.error("Wrong count in website split after column layout");
 }
-const [GRID_COLUMNS, ...__DETECT_ERROR_WEBSITE_2__] = splitBetween(
-    VERTICAL_ALIGNMENT,
-    SNIPPET_SPECIFIC_NEXT,
-    1
-);
-if (__DETECT_ERROR_WEBSITE_2__.length > 0) {
+const [
+    GRID_COLUMNS, 
+    ...__DETECT_ERROR_WEBSITE_3__
+] = splitBetween(VERTICAL_ALIGNMENT, SNIPPET_SPECIFIC_NEXT, 1);
+if (__DETECT_ERROR_WEBSITE_3__.length > 0) {
     console.error("Wrong count in website split after vertical alignment");
+}
+const [
+    COVER_PROPERTIES,
+    CONTAINER_WIDTH,
+    SCROLL_BUTTON,
+    ...__DETECT_ERROR_WEBSITE_4__
+] = splitBetween(SNIPPET_SPECIFIC_NEXT, SNIPPET_SPECIFIC_END, 3);
+if (__DETECT_ERROR_WEBSITE_4__.length > 0) {
+    console.error("Wrong count in website split before specific end");
+}
+const [
+    GRID_IMAGE,
+    TEXT_HIGHLIGHT,
+    ANIMATE,
+    CONDITIONAL_VISIBILITY,
+    DEVICE_VISIBILITY,
+    ...__DETECT_ERROR_WEBSITE_5__
+] = splitBetween(SNIPPET_SPECIFIC_END, END, 5);
+if (__DETECT_ERROR_WEBSITE_5__.length > 0) {
+    console.error("Wrong count in website split after specific end");
 }
 export {
     WEBSITE_BACKGROUND_OPTIONS,
@@ -37,22 +65,12 @@ export {
     LAYOUT_COLUMN,
     LAYOUT_GRID,
     GRID_COLUMNS,
+    COVER_PROPERTIES, 
+    CONTAINER_WIDTH, 
+    SCROLL_BUTTON,
+    GRID_IMAGE, 
+    TEXT_HIGHLIGHT, 
+    ANIMATE, 
+    CONDITIONAL_VISIBILITY, 
+    DEVICE_VISIBILITY
 };
-const [COVER_PROPERTIES, CONTAINER_WIDTH, SCROLL_BUTTON, ...__DETECT_ERROR_WEBSITE_3__] =
-    splitBetween(SNIPPET_SPECIFIC_NEXT, SNIPPET_SPECIFIC_END, 3);
-if (__DETECT_ERROR_WEBSITE_3__.length > 0) {
-    console.error("Wrong count in website split before specific end");
-}
-export { COVER_PROPERTIES, CONTAINER_WIDTH, SCROLL_BUTTON };
-const [
-    GRID_IMAGE,
-    TEXT_HIGHLIGHT,
-    ANIMATE,
-    CONDITIONAL_VISIBILITY,
-    DEVICE_VISIBILITY,
-    ...__DETECT_ERROR_WEBSITE_4__
-] = splitBetween(SNIPPET_SPECIFIC_END, END, 5);
-if (__DETECT_ERROR_WEBSITE_4__.length > 0) {
-    console.error("Wrong count in website split after specific end");
-}
-export { GRID_IMAGE, TEXT_HIGHLIGHT, ANIMATE, CONDITIONAL_VISIBILITY, DEVICE_VISIBILITY };
