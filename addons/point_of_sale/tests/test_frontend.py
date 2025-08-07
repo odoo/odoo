4cbad072b54805c96b0340dbf2470058d80cd78a
manv-afk <manv@odoo.com>
2025-08-07 11:45:26 +0000

    [FIX] point_of_sale: use empty order if available
    - When clicking `New order` on the receipt screen, we now want to reuse an empty order (not finalized and no order lines) before creating a new one. This avoid creating many useless empty orders.
    
    closes odoo/odoo#222891
    
    Task-id: 1737
    X-original-commit: f44614bb2fd116119de66ebaf88ff3c76cb06ee7
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Manu Vaillant (manv) <manv@odoo.com>

--- addons/point_of_sale/tests/test_frontend.py
+++ addons/point_of_sale/tests/test_frontend.py
@@ -1873,6 +1873,11 @@ class TestUi(TestPointOfSaleHttpCommon):
         self.assertEqual(len(self.main_pos_config.current_session_id.statement_line_ids), 1, "There should be one cash in/out statement line")
         self.assertEqual(self.main_pos_config.current_session_id.statement_line_ids[0].amount, -5, "The cash in/out amount should be -5")
 
+    def test_reuse_empty_floating_order(self):
+        """ Verify that after a payment, POS should reuse an existing empty floating order if available, instead of always creating new ones """
+        self.main_pos_config.with_user(self.pos_user).open_ui()
+        self.start_tour(f"/pos/ui?config_id={self.main_pos_config.id}", 'test_reuse_empty_floating_order', login="pos_user")
+
     def test_add_multiple_serials_at_once(self):
         self.product_a = self.env['product.product'].create({
             'name': 'Product A',
