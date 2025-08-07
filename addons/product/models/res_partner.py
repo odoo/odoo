4d29645f3d45ac57e661d294845ffe25c86d5a57
Levi Siuzdak (sile) <sile@odoo.com>
2025-08-07 09:29:31 +0000

    [FIX] product: sync pricelist when adding company to a partner
    Versions
    --------
    - 18.0+
    
    Steps
    -----
    1. Create an individual partner;
    2. assign them a pricelist A;
    3. create a new company partner for them;
    4. create a new pricelist A & sort it on top;
    5. open a new quotation;
    6. set new company as customer;
    7. change customer to the individual partner.
    
    Issue
    -----
    The quotation's pricelist changed from B to A. The pricelist used for
    the individual should be identical to the one used for their company.
    
    Cause
    -----
    Commit a4d469aba95c0 added the `specific_property_product_pricelist`
    field to `res.partner`, as the way company-dependent fields was changed
    on the database-level.
    
    Commit 67cf577cd0a0 added the `_company_dependent_commercial_fields`
    method to enable syncing company-dependent commercial fields. The base
    method fetches all fields retrieved via `_commercial_fields`, and
    selects those whose `company_dependent` attribute is `True`.
    
    In previous versions, the `_company_dependent_commercial_fields`
    override in `product` adds `property_product_pricelist`, as this field
    does not have the `company_dependent` attribute set, but it behaves as a
    company-dependent field. Starting from 18.0, the override adds
    `specific_property_product_pricelist` instead, which does have the
    `company_dependent` property set.
    
    As a consequence, when `_company_dependent_commercial_sync` gets called,
    it does not sync the `specific_property_product_pricelist` as it's not
    included in the `_commercial_fields` override, nor does it sync when
    retrieving it from `_company_dependent_commercial_fields`, as it skips
    the current company, assuming the fields was already handled by
    `_commercial_sync_from_company`.
    
    Solution
    --------
    - Move `specific_property_product_pricelist` to `_commercial_fields`
        - This enables it to sync in `_commercial_sync_from_company`
    - Remove the `_company_dependent_commercial_fields` override
        - `property_product_pricelist` shouldn't get synced by itself
        - `specific_property_product_pricelist` is already included by the
          base method
    
    opw-4988736
    
    X-original-commit: 9fe5a9ad99dd761425750f9579f3a85f8df47282
    Part-of: odoo/odoo#224715
    Signed-off-by: Levi Siuzdak <sile@odoo.com>

--- addons/product/models/res_partner.py
+++ addons/product/models/res_partner.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
@@ -49,10 +48,7 @@ class ResPartner(models.Model):
                 partner.specific_property_product_pricelist = False if partner.property_product_pricelist.id == default_for_country.id else partner.property_product_pricelist.id
 
     def _synced_commercial_fields(self):
-        return super()._synced_commercial_fields() + ['property_product_pricelist']
-
-    def _company_dependent_commercial_fields(self):
         return [
-            *super()._company_dependent_commercial_fields(),
-            'specific_property_product_pricelist'
+            *super()._synced_commercial_fields(),
+            'specific_property_product_pricelist',
         ]
