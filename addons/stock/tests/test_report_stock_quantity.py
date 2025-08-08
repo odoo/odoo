odoo/odoo

commit 5b40fb086a0e5677678c312b42dc1f2c8991dc9e

[FIX] stock: prevent internal transfers from affecting available quantity in past inventory reports
Before this commit, the available quantity was incorrect when using the "Inventory At" feature with a past date in the inventory report

For products using multi-step delivery routes, the `_compute_quantities_dict()` method incorrectly treated internal moves as outgoing moves
As a result, the same outgoing quantity was added multiple times, leading to an overestimation of the available stock

This commit change the domain returned by `_get_domain_locations_new()` in product.py to exclude the locations where ('location_dest_id', 'any', paths_domain) in all the case and not just when the ('location_final_id', '=', False)

Steps to reproduce:
- Create a new product
- Active the multi-step routes in Settings
- Set the Warehouse's Outgoing Shipments to Pick, Pack, then Deliver
- Create a RFQ for 100 products and Receive Products
- Create a Quotation for 20 products
- Validate each delivery steps
- Go to Inventory -> Report -> Stock
- Click on Inventory At
- Set the date to 2025-01-01
- Search for your product
- The `In Hand` quantity should be 0 but is 40 before the fix

opw-4848473

closes odoo/odoo#217732

Signed-off-by: William Henrotin (whe) <whe@odoo.com>

addons/stock/models/product.py
@@ -321,8 +321,8 @@ class Product(models.Model):
             loc_domain = [('location_id', 'any', paths_domain)]
             dest_loc_domain = [
                 '|',
+                ('location_dest_id', 'any', paths_domain),
                 '&', ('location_final_id', '!=', False), ('location_final_id', 'any', paths_domain),
-                '&', ('location_final_id', '=', False), ('location_dest_id', 'any', paths_domain),
             ]
 
         # returns: (domain_quant_loc, domain_move_in_loc, domain_move_out_loc)

addons/stock/tests/test_report_stock_quantity.py
@@ -238,3 +238,58 @@ class TestReportStockQuantity(tests.TransactionCase):
             1.0, 2.0,   # in two days
         ]):
             self.assertEqual(qty_rd, qty, f"Incorrect qty for Date '{date_day}' Warehouse '{warehouse.display_name}'")
+
+    def test_past_date_quantity_with_multistep_delivery(self):
+        """
+        Verify that available quantities are correctly computed at different past dates
+        when using multi-step delivery.
+        """
+        def get_inv_qty_at_date(product_id, inv_datetime):
+            inventory_at_date_wizard = self.env['stock.quantity.history'].create({'inventory_datetime': inv_datetime})
+            r = inventory_at_date_wizard.open_at_date()
+            return self.env[r['res_model']].with_context(r['context']).search_read(
+                domain=(r['domain'] + [('id', '=', product_id)]),
+                fields=['qty_available']
+            )[0]['qty_available']
+
+        warehouse = self.wh
+        warehouse.delivery_steps = 'pick_ship'
+        product = self.env['product.product'].create({'name': 'Test', 'is_storable': True})
+
+        with freeze_time(fields.Date.today() - timedelta(days=7)):
+            move_in = self.env['stock.move'].create({
+                'name': 'test_in',
+                'picking_type_id': warehouse.in_type_id.id,
+                'location_id': self.supplier_location.id,
+                'location_dest_id': warehouse.lot_stock_id.id,
+                'product_id': product.id,
+                'product_uom_qty': 100.0,
+            })
+            move_in._action_confirm()
+            move_in.write({'quantity': 100.0, 'picked': True})
+            move_in._action_done()
+
+        move_pick = self.env['stock.move'].create({
+            'name': 'pick',
+            'picking_type_id': warehouse.pick_type_id.id,
+            'location_id': warehouse.lot_stock_id.id,
+            'location_dest_id': warehouse.wh_output_stock_loc_id.id,
+            'location_final_id': self.customer_location.id,
+            'product_id': product.id,
+            'product_uom_qty': 60.0,
+        })
+        move_pick._action_confirm()
+        move_pick.write({'quantity': 60.0, 'picked': True})
+        move_pick._action_done()
+
+        move_out = move_pick.move_dest_ids
+        move_out.write({'quantity': 25.0, 'picked': True})
+        move_out._action_done()
+
+        for date, expected_qty in (
+            (move_in.date - timedelta(days=1), 0.0),
+            (move_out.date - timedelta(days=1), 100.0),
+            (move_out.date, 75.0)
+        ):
+            qty = get_inv_qty_at_date(product.id, date)
+            self.assertEqual(qty, expected_qty)

commit ad53028806dadc9fe1f3ef8f02209cfdd394f7f2

[FIX] stock: consider push move chains in product forecast
Steps to reproduce:

- In the settings enable Multi-Steps routes
- Put your warehouse in delivery in 2 steps
- Create and confirm a sale order for 1 units of a storable product
> While the pick move was created and confirmed the forecast is still at
> 0 even tho it should be at -1 and the outgoing pick move should be
> matched with the SO line in the forecast report.

Cause of the issue:

The issue has been introduced by commit 5b40fb086a0e5677678c312b42dc1f2c8991dc9e
The issue being that since the `location_final_id` should not have been
considered for the past forecast based on done move chains (because
each done move of the chain will refer to the same external
`final_dest_id`). The proposed fix was therefore to change the
dest_loc_domain as such:
https://github.com/odoo/odoo/commit/5b40fb086a0e5677678c312b42dc1f2c8991dc9e#diff-1f24ce9f94c5795040749acca5924384d7d17c0ac39b1993cef3b484e4bd30afR324-R326
However, the new domain:
https://github.com/odoo/odoo/blob/995a7072cb3315fc03544b281b1ed5ca4e81e901/addons/stock/models/product.py#L322-L326
ignores completely the part of the condition refering to `final_dest_id`
for outgoing moves since the condition is negated here:
https://github.com/odoo/odoo/blob/995a7072cb3315fc03544b281b1ed5ca4e81e901/addons/stock/models/product.py#L328-L333
The logical `OR` (`|`) becoming an `AND` (`&`) for the
`domain_move_out_loc`.

opw-4997982

addons/stock/models/product.py
@@ -316,20 +316,34 @@ class Product(models.Model):
         if self.env.context.get('strict'):
             loc_domain = [('location_id', 'in', locations.ids)]
             dest_loc_domain = [('location_dest_id', 'in', locations.ids)]
+            dest_loc_domain_out = [('location_dest_id', 'in', locations.ids)]
         elif locations:
             paths_domain = expression.OR([[('parent_path', '=like', loc.parent_path + '%')] for loc in locations])
             loc_domain = [('location_id', 'any', paths_domain)]
+            # The condition should be split for done and not-done moves as the final_dest_id only make sense
+            # for the part of the move chain that is not done yet.
+            dest_loc_domain_done = ('location_dest_id', 'any', paths_domain)
+            dest_loc_domain_in_progress = [
+                '|',
+                    '&', ('location_final_id', '!=', False), ('location_final_id', 'any', paths_domain),
+                    '&', ('location_final_id', '=', False), ('location_dest_id', 'any', paths_domain),
+            ]
             dest_loc_domain = [
                 '|',
-                ('location_dest_id', 'any', paths_domain),
-                '&', ('location_final_id', '!=', False), ('location_final_id', 'any', paths_domain),
-            ]
+                    '&', ('state', '=', 'done'), dest_loc_domain_done,
+                    '&', ('state', '!=', 'done'),
+            ] + dest_loc_domain_in_progress
+            dest_loc_domain_out = [
+                '|',
+                    '&', ('state', '=', 'done'), '!', dest_loc_domain_done,
+                    '&', ('state', '!=', 'done'),
+            ] + ['!'] + dest_loc_domain_in_progress
 
         # returns: (domain_quant_loc, domain_move_in_loc, domain_move_out_loc)
         return (
             loc_domain,
             dest_loc_domain + ['!'] + loc_domain,
-            loc_domain + ['!'] + dest_loc_domain,
+            loc_domain + dest_loc_domain_out,
         )
 
     def _search_qty_available(self, operator, value):

addons/stock/tests/test_report_stock_quantity.py
@@ -4,6 +4,7 @@
 from datetime import datetime, timedelta
 
 from odoo import fields, tests
+from odoo.fields import Command
 from odoo.tests import Form
 from freezegun import freeze_time
 
@@ -242,54 +243,84 @@ class TestReportStockQuantity(tests.TransactionCase):
     def test_past_date_quantity_with_multistep_delivery(self):
         """
         Verify that available quantities are correctly computed at different past dates
-        when using multi-step delivery.
+        when using multi-step reciept/delivery.
         """
         def get_inv_qty_at_date(product_id, inv_datetime):
             inventory_at_date_wizard = self.env['stock.quantity.history'].create({'inventory_datetime': inv_datetime})
             r = inventory_at_date_wizard.open_at_date()
-            return self.env[r['res_model']].with_context(r['context']).search_read(
-                domain=(r['domain'] + [('id', '=', product_id)]),
-                fields=['qty_available']
-            )[0]['qty_available']
-
-        warehouse = self.wh
-        warehouse.delivery_steps = 'pick_ship'
+            return next((product['qty_available'], product['virtual_available']) for product in self.env[r['res_model']].with_context(r['context']).search_read(
+                    domain=(r['domain'] + [('id', '=', product_id)]),
+                    fields=['qty_available', 'virtual_available']
+                ))
+        # We add a second warehouse and put the resuplying flow in push mechanic to test receipt in 2 steps with an external transfer
+        warehouse, warehouse_2 = self.wh, self.env['stock.warehouse'].create({
+            'name': 'Resupplier warehouse',
+            'code': 'WH02',
+        })
+        transit_loc = self.wh.company_id.internal_transit_location_id
+        warehouse.write({
+            'resupply_wh_ids': [Command.set(warehouse_2.ids)],
+            'delivery_steps': 'pick_ship',
+        })
+        warehouse.resupply_route_ids.rule_ids.filtered(lambda r: r.location_src_id == transit_loc).action = 'push'
         product = self.env['product.product'].create({'name': 'Test', 'is_storable': True})
-
-        with freeze_time(fields.Date.today() - timedelta(days=7)):
-            move_in = self.env['stock.move'].create({
-                'name': 'test_in',
+        today = fields.Date.today()
+        with freeze_time(today - timedelta(days=8)):
+            move_transit = self.env['stock.move'].create({
+                'name': 'test transit',
+                'warehouse_id': warehouse.id,
                 'picking_type_id': warehouse.in_type_id.id,
                 'location_id': self.supplier_location.id,
-                'location_dest_id': warehouse.lot_stock_id.id,
+                'location_dest_id': transit_loc.id,
+                'location_final_id': warehouse.lot_stock_id.id,
+                'route_ids': [Command.set(warehouse.resupply_route_ids.ids)],
                 'product_id': product.id,
-                'product_uom_qty': 100.0,
+                'product_uom_qty': 150.0,
             })
+            move_transit._action_confirm()
+            move_transit.write({'quantity': 150.0, 'picked': True})
+            move_transit._action_done()
+            self.assertRecordValues(product.with_context(warehouse_id=warehouse.id), [{'qty_available': 0.0, 'virtual_available': 150.0}])
+            move_transit._action_done()
+            self.assertRecordValues(product.with_context(warehouse_id=warehouse.id), [{'qty_available': 0.0, 'virtual_available': 150.0}])
+
+        with freeze_time(today - timedelta(days=6)):
+            move_in = move_transit.move_dest_ids
             move_in._action_confirm()
             move_in.write({'quantity': 100.0, 'picked': True})
+            self.assertRecordValues(product.with_context(warehouse_id=warehouse.id), [{'qty_available': 0.0, 'virtual_available': 150.0}])
             move_in._action_done()
+            self.assertRecordValues(product.with_context(warehouse_id=warehouse.id), [{'qty_available': 100.0, 'virtual_available': 150.0}])
 
-        move_pick = self.env['stock.move'].create({
-            'name': 'pick',
-            'picking_type_id': warehouse.pick_type_id.id,
-            'location_id': warehouse.lot_stock_id.id,
-            'location_dest_id': warehouse.wh_output_stock_loc_id.id,
-            'location_final_id': self.customer_location.id,
-            'product_id': product.id,
-            'product_uom_qty': 60.0,
-        })
-        move_pick._action_confirm()
-        move_pick.write({'quantity': 60.0, 'picked': True})
-        move_pick._action_done()
+        with freeze_time(today - timedelta(days=4)):
+            move_pick = self.env['stock.move'].create({
+                'name': 'pick',
+                'picking_type_id': warehouse.pick_type_id.id,
+                'location_id': warehouse.lot_stock_id.id,
+                'location_dest_id': warehouse.wh_output_stock_loc_id.id,
+                'location_final_id': self.customer_location.id,
+                'product_id': product.id,
+                'product_uom_qty': 60.0,
+            })
+            move_pick._action_confirm()
+            self.assertRecordValues(product.with_context(warehouse_id=warehouse.id), [{'qty_available': 100.0, 'virtual_available': 90.0}])
+            move_pick.write({'quantity': 60.0, 'picked': True})
+            move_pick._action_done()
+            self.assertRecordValues(product.with_context(warehouse_id=warehouse.id), [{'qty_available': 100.0, 'virtual_available': 90.0}])
 
-        move_out = move_pick.move_dest_ids
-        move_out.write({'quantity': 25.0, 'picked': True})
-        move_out._action_done()
+        with freeze_time(today - timedelta(days=2)):
+            move_out = move_pick.move_dest_ids
+            move_out.write({'quantity': 25.0, 'picked': True})
+            self.assertRecordValues(product.with_context(warehouse_id=warehouse.id), [{'qty_available': 100.0, 'virtual_available': 90.0}])
+            move_out._action_done()
+            self.assertRecordValues(product.with_context(warehouse_id=warehouse.id), [{'qty_available': 75.0, 'virtual_available': 90.0}])
 
-        for date, expected_qty in (
-            (move_in.date - timedelta(days=1), 0.0),
-            (move_out.date - timedelta(days=1), 100.0),
-            (move_out.date, 75.0)
+        for date, expected_qties in (
+            (move_transit.date - timedelta(days=1), (0.0, 0.0)),
+            (move_in.date - timedelta(days=1), (0.0, 50.0)),  # The backorder of move_in contributes in the incoming qty
+            (move_pick.date - timedelta(days=1), (100.0, 150.0)),
+            (move_out.date - timedelta(days=1), (100.0, 115.0)),  # The backorder of move_out contributes in the outgoing qty
+            (today - timedelta(days=1), (75.0, 90.0)),
         ):
             qty = get_inv_qty_at_date(product.id, date)
-            self.assertEqual(qty, expected_qty)
+            self.assertEqual(qty, expected_qties)