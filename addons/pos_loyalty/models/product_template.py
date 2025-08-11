98962db8cee4deec636b43301ae692721150f301
Pedram (PEBR) <pebr@odoo.com>
2025-08-11 12:34:36 +0200

    [FIX] pos_loyalty: prevent hiding a reward product
    Before this commit, a product used as a loyalty reward could be hidden
    in the PoS.
    
    opw-5005934
    
    closes odoo/odoo#222451
    
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/pos_loyalty/models/product_template.py
+++ addons/pos_loyalty/models/product_template.py
@@ -19,6 +19,9 @@ class ProductTemplate(models.Model):
 
         missing_product_templates = self.env['product.template'].browse(missing_product_tmpl_ids).read(fields=fields, load=False)
         product_ids_to_hide = reward_products.product_tmpl_id - self.env['product.template'].browse(already_loaded_product_tmpl_ids)
+        if self.env.context.get('pos_limited_loading', True):
+            # Filter out products that can be loaded in the PoS but are not loaded yet
+            product_ids_to_hide = product_ids_to_hide - product_ids_to_hide.filtered_domain(self._load_pos_data_domain(data))
         data['pos.session'][0]['_pos_special_products_ids'] += product_ids_to_hide.product_variant_id.ids
         res.extend(missing_product_templates)
 
