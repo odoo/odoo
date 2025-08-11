7ed353dd863cf95bac5c9a9e08ecc45eedfc0e05
bona-odoo <bona@odoo.com>
2025-08-11 05:40:51 +0000

    [FIX] website_sale: Fix memory error
    Issue:
    -------
    Loading product data in Google Merchant Center was causing memory error
    when handling a large number of products (14k+ in this case).
    The problem occurred because `self.image_1920` was used to check if a product
    had an image. This field is computed and non-stored, so accessing it for many
    records loads the full high-resolution image into memory, leading to excessive
    memory usage.
    
    Solution:
    ------------
    Use `image_128` instead of `image_1920` for image existence checks,
    significantly reducing memory usage and preventing the error.
    
    upg-3063427
    opw-4981983
    
    closes odoo/odoo#223925
    
    X-original-commit: 09698f6d8f5f6d2741981626ab0721227edfc258
    Signed-off-by: Lionel Piraux (lipi) <lipi@odoo.com>

--- addons/website_sale/models/product_product.py
+++ addons/website_sale/models/product_product.py
@@ -279,7 +279,7 @@ class ProductProduct(models.Model):
         self.ensure_one()
         return {
             # Don't send any image link if there isn't. Google does not allow placeholder
-            'image_link': url_join(base_url, self._get_image_1920_url()) if self.image_1920 else '',
+            'image_link': url_join(base_url, self._get_image_1920_url()) if self.image_128 else '',
             # Supports up to 10 extra images
             'additional_image_link': [
                 url_join(base_url, url) for url in self._get_extra_image_1920_urls()[:10]
