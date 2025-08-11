061f2f7c161ca80e7ad1daf1810966b9d941ae14
David Monnom <moda@odoo.com>
2025-08-11 12:22:40 +0000

    [FIX] point_of_sale, pos_restaurant: fix method arguments
    In a previous commit, some method signatures were changed to use an
    options object instead of multiple parameters.
    
    This commit updates the affected method calls to use the new options
    object format.
    
    X-original-commit: 90241de621498b625cf019ad1f92e1657033ccda

--- addons/point_of_sale/static/tests/pos/tours/utils/preparation_receipt_util.js
+++ addons/point_of_sale/static/tests/pos/tours/utils/preparation_receipt_util.js
@@ -47,3 +47,107 @@ export async function generateFireCourseReceipts() {
     };
     return await generateReceiptsToPrint(order, orderChange);
 }
+
+export function checkPreparationTicketData(
+    data,
+    opts = {
+        visibleInDom: [],
+        invisibleInDom: [],
+        lineOrder: [],
+        fireCourse: false,
+    }
+) {
+    const check = async () => {
+        let tickets = [];
+
+        if (opts.fireCourse) {
+            tickets = await generateFireCourseReceipts();
+        } else {
+            tickets = await generatePreparationReceipts();
+        }
+
+        if (
+            !tickets[0] &&
+            !data.length &&
+            !opts.invisibleInDom?.length &&
+            !opts.visibleInDom?.length &&
+            !opts.lineOrder?.length &&
+            !opts.fireCourse
+        ) {
+            return true;
+        }
+
+        const lines = tickets[0].querySelectorAll(".orderline");
+        const lineNames = [];
+
+        let idx = 0;
+        for (const line of lines) {
+            const name = line.firstChild.children[1].innerHTML;
+            const qty = line.firstChild.children[0].innerHTML;
+            const domAttrs = Object.values(line.children[1]?.children || []);
+            const attrs = domAttrs.map((c) => c.innerHTML).filter(Boolean);
+            const values = data[idx];
+
+            if (values.qty != qty) {
+                throw new Error(
+                    `Ticket data mismatch for ${name}: expected ${values.qty}, got ${qty}`
+                );
+            }
+
+            if (values.name != name) {
+                throw new Error(
+                    `Ticket data mismatch for ${name}: expected ${values.name}, got ${name}, maybe lines ordering ?`
+                );
+            }
+
+            if (values.attributes) {
+                for (const attr of values.attributes) {
+                    const found = attrs.find((a) => a.includes(attr));
+                    if (!found) {
+                        throw new Error(
+                            `Attribute ${attr} not found in printed receipt for ${name}`
+                        );
+                    }
+                }
+            }
+
+            if (!values) {
+                throw new Error(`Received ${name} but no check data found`);
+            }
+
+            lineNames.push(name);
+            idx++;
+        }
+
+        if (opts.visibleInDom) {
+            for (const inDom of opts.visibleInDom) {
+                let found = false;
+                for (const ticket of tickets) {
+                    if (ticket.innerHTML.includes(inDom)) {
+                        found = true;
+                    }
+                }
+                if (!found) {
+                    throw new Error(`${inDom} not found in printed receipt`);
+                }
+            }
+        }
+
+        if (opts.invisibleInDom) {
+            for (const notInDom of opts.invisibleInDom) {
+                for (const ticket of tickets) {
+                    if (ticket.innerHTML.includes(notInDom)) {
+                        throw new Error(`${notInDom} should not be in printed receipt`);
+                    }
+                }
+            }
+        }
+    };
+
+    return [
+        {
+            trigger: "body",
+            run: async () => await check(),
+        },
+    ];
+}
