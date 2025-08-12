af3212867c3dceed9825bec8fcdf17ca16e2996a
Woojang (WOPY) <wopy@odoo.com>
2025-08-12 06:03:51 +0000

    [IMP] l10n_anz_ubl_pint: prevent PINT incompliant invoice XML data from sending
    This commit fixes XML generation logic and adds constraints to filter out invoice data that would fail to pass Austrailia & New Zealand's jurisdictional PINT rules. Checks regarding the new Q2 2025 PINT rule updates has also been done.
    
    Task-4865446
    
    X-original-commit: 0e582263f74310a73a1bd1e0d5adf1cff4a1eceb
    Part-of: odoo/odoo#224175
    Signed-off-by: Nicolas Viseur (vin) <vin@odoo.com>
    Signed-off-by: Woojang Pyeon (wopy) <wopy@odoo.com>

--- addons/l10n_anz_ubl_pint/models/account_edi_xml_pint_anz.py
+++ addons/l10n_anz_ubl_pint/models/account_edi_xml_pint_anz.py
@@ -1,5 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from odoo import models, _
+from odoo.tools import float_is_zero
 
 ANZ_TAX_CATEGORIES = {'S', 'E', 'Z', 'G', 'O'}
 
@@ -33,7 +34,7 @@ class AccountEdiXmlPint_Anz(models.AbstractModel):
         # OVERRIDE account_edi_ubl_cii
         # A business not registered for GST cannot issue tax invoices.
         # In this case, the tax category code should be O (Outside scope of tax).
-        if not supplier.vat:
+        if not supplier.ref_company_ids[:1].l10n_au_is_gst_registered:
             return 'O'
         return super()._get_tax_category_code(customer, supplier, tax)
 
@@ -58,6 +59,12 @@ class AccountEdiXmlPint_Anz(models.AbstractModel):
         # EXTENDS account.edi.xml.ubl_bis3
         tax_category_node = super()._get_tax_category_node(vals)
         tax_category_node['cac:TaxScheme']['cbc:ID']['_text'] = 'GST'
+
+        # As of PINT A-NZ v1.1.0: [aligned-ibrp-o-05-aunz] tax categories of type "Not Subject to tax" (i.e. tax_category_code == 'O') must NOT have
+        # tax rate ('cbc:Percent').
+        if tax_category_node['cbc:ID']['_text'] == 'O' and float_is_zero(tax_category_node['cbc:Percent']['_text'], precision_digits=2):
+            del tax_category_node['cbc:Percent']
+
         return tax_category_node
 
     def _get_party_node(self, vals):
@@ -101,6 +108,39 @@ class AccountEdiXmlPint_Anz(models.AbstractModel):
         for tax_subtotal_node in tax_total_node['cac:TaxSubtotal']:
             if tax_subtotal_node['cac:TaxCategory']['cbc:ID']['_text'] not in ANZ_TAX_CATEGORIES:
                 constraints['sg_vat_category_required'] = _("You must set a tax category on each taxes of the invoice.\nValid categories are: S, E, Z, G, O")
+
+        # Tax category of type "Not subject to tax" must have tax amount 0
+        count_outside_of_scope_breakdown = 0
+        for tax_subtotal in tax_total_node['cac:TaxSubtotal']:
+            if tax_subtotal['cac:TaxCategory']['cbc:ID']['_text'] != 'O':
+                continue
+            count_outside_of_scope_breakdown += 1
+
+            if float_is_zero(tax_subtotal['cbc:TaxAmount']['_text'], precision_digits=2):
+                continue
+
+            if vals['supplier'].ref_company_ids[:1].l10n_au_is_gst_registered:
+                constraints['anz_tax_breakdown_amount'] = \
+                    self.env._("A tax category of type 'Not subject to tax' must have tax"
+                               " amount set to 0")
+            else:
+                # If a company is not GST registered, this module remaps all tax categories
+                # to 'O' (Other/Out of Scope). This causes an issue when a line contained a
+                # non-zero tax, as it would create a tax subtotal with (Code: 'O', Amount:
+                # non-zero), which violates PINT rules.
+                # Since the code silently remaps tax classification code, the original error
+                # message might be misleading for users. This constraint contains better
+                # explaination of error and intructions on how to prevent it.
+                constraints['anz_non_gst_supplier_tax_scope'] = \
+                    self.env._("Suppliers not registered for GST cannot use taxes that are"
+                               " not zero-rated. Please ensure the tax category is set to"
+                               " 'O' (Services outside scope of tax) with a tax amount of 0.")
+
+        # There should be at most one tax breakdown of type "Not subject to tax".
+        if count_outside_of_scope_breakdown > 1 and vals['supplier'].ref_company_ids[:1].l10n_au_is_gst_registered:
+            constraints['anz_duplicate_tax_breakdown'] = \
+                self.env._("A tax breakdown of type 'Not subject to tax' should appear at most"
+                           " once in the tax breakdown")
 
         # ALIGNED-IBR-001-AUNZ and ALIGNED-IBR-002-AUNZ
         for partner_type in ('supplier', 'customer'):
