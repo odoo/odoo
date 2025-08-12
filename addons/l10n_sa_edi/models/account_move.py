2b31317623b70cdc36cf77ec195b6628d9d64c63
khom-odoo <khom@odoo.com>
2025-08-12 06:47:50 +0000

    [REM] l10n_sa_edi: remove restriction for standard branches
    This commit removes the previously enforced restriction in d6175d0552c18006913e969eac7006666164609c that
    required the company specified on an invoice to match the company specified on the associated journal
    and makes sure that the company used to create the QR is the appropriate company whether in a branch
    or in a main company
    
    task-5005477
    
    closes odoo/odoo#223976
    
    X-original-commit: 06e21763130d8fcefe9bb62a8b6525fd660874b4
    Signed-off-by: Khaled Fahmy (khfa) <khfa@odoo.com>
    Signed-off-by: Omar Khalid (khom) <khom@odoo.com>

--- addons/l10n_sa_edi/models/account_move.py
+++ addons/l10n_sa_edi/models/account_move.py
@@ -6,7 +6,6 @@ from odoo.tools import float_repr
 from datetime import datetime
 from base64 import b64decode, b64encode
 from lxml import etree
-from odoo.exceptions import UserError
 
 
 class AccountMove(models.Model):
@@ -42,7 +41,7 @@ class AccountMove(models.Model):
                 if move._l10n_sa_is_simplified():
                     x509_cert_sudo = move.journal_id.sudo().l10n_sa_production_csid_certificate_id
                     xml_content = self.env.ref('l10n_sa_edi.edi_sa_zatca')._l10n_sa_generate_zatca_template(move)
-                    qr_code_str = move._l10n_sa_get_qr_code(move.journal_id, xml_content, x509_cert_sudo,
+                    qr_code_str = move._l10n_sa_get_qr_code(move.company_id, xml_content, x509_cert_sudo,
                                                             move.l10n_sa_invoice_signature, True)
                     qr_code_str = b64encode(qr_code_str).decode()
                 elif zatca_document.state == 'sent' and zatca_document.sudo().attachment_id.datas:
@@ -74,7 +73,7 @@ class AccountMove(models.Model):
         return self.reversed_entry_id or self.ref
 
     @api.model
-    def _l10n_sa_get_qr_code(self, journal_id, unsigned_xml, certificate, signature, is_b2c=False):
+    def _l10n_sa_get_qr_code(self, company_id, unsigned_xml, certificate, signature, is_b2c=False):
         """
             Generate QR code string based on XML content of the Invoice UBL file, X509 Production Certificate
             and company info.
@@ -96,14 +95,14 @@ class AccountMove(models.Model):
         invoice_time = xpath_ns('//cbc:IssueTime')
         invoice_datetime = datetime.strptime(invoice_date + ' ' + invoice_time, '%Y-%m-%d %H:%M:%S')
 
-        if invoice_datetime and journal_id.company_id.vat and certificate and signature:
+        if invoice_datetime and company_id.vat and certificate and signature:
             prehash_content = etree.tostring(root)
             invoice_hash = edi_format._l10n_sa_generate_invoice_xml_hash(prehash_content, 'digest')
 
             amount_total = float(xpath_ns('//cbc:PayableAmount'))
             amount_tax = float(xpath_ns('//cac:TaxTotal/cbc:TaxAmount'))
-            seller_name_enc = self._l10n_sa_get_qr_code_encoding(1, journal_id.company_id.display_name.encode())
-            seller_vat_enc = self._l10n_sa_get_qr_code_encoding(2, journal_id.company_id.vat.encode())
+            seller_name_enc = self._l10n_sa_get_qr_code_encoding(1, company_id.display_name.encode())
+            seller_vat_enc = self._l10n_sa_get_qr_code_encoding(2, company_id.vat.encode())
             timestamp_enc = self._l10n_sa_get_qr_code_encoding(3,
                                                                invoice_datetime.strftime("%Y-%m-%dT%H:%M:%S").encode())
             amount_total_enc = self._l10n_sa_get_qr_code_encoding(4, float_repr(abs(amount_total), 2).encode())
@@ -274,11 +273,6 @@ class AccountMove(models.Model):
             'total_amount': invoice_vals['vals']['monetary_total_vals']['tax_inclusive_amount'],
             'total_tax': invoice_vals['vals']['tax_total_vals'][-1]['tax_amount'],
         }
-
-    def action_post(self):
-        if self.filtered(lambda move: move.country_code == "SA" and move.move_type in ('out_invoice', 'out_refund') and move.company_id != move.journal_id.company_id):
-            raise UserError(_("Please make sure that the invoice company matches the journal company on all invoices you wish to confirm"))
-        return super().action_post()
 
 
 class AccountMoveLine(models.Model):
