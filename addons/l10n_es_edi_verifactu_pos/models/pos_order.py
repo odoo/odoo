81a37783efb3c22dac823bebbd7878f3aa58e40b
Sven Fuehr <svfu@odoo.com>
2025-08-12 12:53:12 +0000

    [FIX] l10n_es_edi_verifactu_pos: no cancel; sending vs states
    - The "Send Veri*Factu" button is currently also visible
      for registered orders (but we can not send documents for such orders
      anyway; we do not support subsanacion)
    - We do not allow generating Veri*Factu documents for posted orders.
      That makes it impossible to (Veri*Factu) send orders
      after the session is closed.
    - We should not allow to Veri*Factu cancel orders.
      They would still be included in the closing move.
      The user should create refund instead.
    
    task-None
    
    closes odoo/odoo#222927
    
    X-original-commit: 41979d7d62dd203c1d26a82d65d070dd187db72f
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>
    Signed-off-by: Sven Führ (svfu) <svfu@odoo.com>

--- addons/l10n_es_edi_verifactu_pos/models/pos_order.py
+++ addons/l10n_es_edi_verifactu_pos/models/pos_order.py
@@ -131,8 +131,11 @@ class PosOrder(models.Model):
         )
         return selected_clave_regimen and selected_clave_regimen.split('_', 1)[0]
 
+    # TODO: remove in master (19.0)
     def l10n_es_edi_verifactu_button_cancel(self):
-        self._l10n_es_edi_verifactu_mark_for_next_batch(cancellation=True)
+        # We should not Veri*Factu cancel orders. There is no clean way to cancel PoS orders.
+        # So they would still be reflected in the accounting.
+        pass
 
     def l10n_es_edi_verifactu_button_send(self):
         self._l10n_es_edi_verifactu_mark_for_next_batch()
@@ -141,8 +144,8 @@ class PosOrder(models.Model):
         self.ensure_one()
         errors = []
 
-        if self.state != 'paid':
-            errors.append(_("Veri*Factu documents can only be generated for paid Point of Sale Orders."))
+        if self.state not in ('paid', 'done'):
+            errors.append(_("Veri*Factu documents can only be generated for paid or posted Point of Sale Orders."))
 
         return errors
 
