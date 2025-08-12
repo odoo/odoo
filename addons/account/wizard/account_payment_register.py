47cd13f2a6ff413c7e7f85d3e1bca48fc5f7830d
gteboul <gute@odoo.com>
2025-08-12 17:16:30 +0200

    [FIX] account: fix payment wizard round issue
    With a company where the main currency is, for instance, MXN (easier to reproduce with l10n_mx):
    
    - Create a currency exchange rate for USD: 0.051908143350 unit per MXN and 19.264800000000 MXN per unit.
    - Create an invoice in USD with a product priced at 432, quantity of 2, and 16% taxes.
    
    In the journal items of the invoice, the balance is 19307.96 MXN.
    However, in the payment wizard, the amount shown when switching to MXN currency is 19307.95.
    
    In _get_total_amount_in_wizard_currency_to_full_reconcile, we convert the source amount currency (which includes line + tax not yet converted), then perform the conversion.
    Whereas in the invoice, we first convert the product line and tax line separately and then sum them up.
    
    opw-4846090

--- addons/account/wizard/account_payment_register.py
+++ addons/account/wizard/account_payment_register.py
@@ -531,12 +531,15 @@ class AccountPaymentRegister(models.TransientModel):
             return self._get_total_amount_using_same_currency(batch_result, early_payment_discount=early_payment_discount)
         elif self.source_currency_id != comp_curr and self.currency_id == comp_curr:
             # Foreign currency on source line but the company currency one on the opposite line.
-            return self.source_currency_id._convert(
-                self.source_amount_currency,
-                comp_curr,
-                self.company_id,
-                self.payment_date,
-            ), False
+            payment_rate = self.currency_id._get_conversion_rate(self.source_currency_id, comp_curr, self.company_id, self.payment_date)
+            residual_amount = 0.0
+            for aml in batch_result['lines']:
+                aml_rate = self.currency_id._get_conversion_rate(self.currency_id, comp_curr, self.company_id, aml.date)
+                if aml_rate == payment_rate:
+                    residual_amount += aml.amount_residual
+                else:
+                    residual_amount += comp_curr.round(aml.amount_residual_currency * payment_rate)
+            return abs(residual_amount), False
         elif self.source_currency_id == comp_curr and self.currency_id != comp_curr:
             # Company currency on source line but a foreign currency one on the opposite line.
             residual_amount = 0.0
