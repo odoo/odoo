6fc369b2654bd76a09c9cd3a01aa3f8e1b847bb0
Sven Fuehr <svfu@odoo.com>
2025-08-12 12:53:12 +0000

    [FIX] l10n_es_edi_verifactu_pos: no cancel; sending vs states
    - The "Send Veri*Factu" button is currently also visible
      for registered orders (but we can not send documents for such orders
      anyway; we do not support subsanacion)
    - We do not allow generating Veri*Factu documents for posted orders.
      That makes it impossible to (Veri*Factu) send orders
      after the session is closed.
    - We should not allow to Veri*Factu cancel orders.
      They would still be included in the closing move.
      The user should create refund instead.
    
    task-None
    
    closes odoo/odoo#223050
    
    X-original-commit: 81a37783efb3c22dac823bebbd7878f3aa58e40b
    Related: odoo/upgrade#8243
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>
    Signed-off-by: Sven Führ (svfu) <svfu@odoo.com>

--- addons/l10n_es_edi_verifactu_pos/models/verifactu_document.py
+++ addons/l10n_es_edi_verifactu_pos/models/verifactu_document.py
@@ -1,9 +1,4 @@
-import logging
-
 from odoo import fields, models
-from odoo.exceptions import UserError
-
-_logger = logging.getLogger(__name__)
 
 
 class L10nEsEdiVerifactuDocument(models.Model):
@@ -14,14 +9,3 @@ class L10nEsEdiVerifactuDocument(models.Model):
         comodel_name='pos.order',
         readonly=True,
     )
-
-    def _cancel_after_sending(self, info):
-        super()._cancel_after_sending(info)
-        for document in self:
-            order = document.pos_order_id
-            if order.l10n_es_edi_verifactu_state == 'cancelled' and order.state != 'cancel':
-                try:
-                    order.l10n_es_edi_verifactu_button_cancel()
-                except UserError as error:
-                    _logger.error("Error while canceling order %(name)s (id %(record_id)s) after Veri*Factu cancellation:\n%(error)s",
-                                  record_id=order.id, name=order.name, error=error)
