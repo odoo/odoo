0f67b2d7f039d411ca6e17c16597a7ee37a0dbd2
guva-odoo <guva@odoo.com>
2025-08-12 13:06:42 +0200

    [FIX] project_sale_expense: wrong analityc distrib computation
    When changing the expense policy of an expense product, the
    compute of analytic distribution of all expenses linked to the product
    is triggered.
    
    Steps:
    
    - Have an expense product X with expense policy 'at_sales'
    - Create several expenses with an expense product X and  any analytic account
    - Create an analytic distribution model that link the expense account of
      X with a specific analytic account AA
    - Create a new expense for product X, the analytic account AA should be set
      from the distribution model
    - Go to the form view of product x and change the expense policy to
      'cost'
    - Go back to the expense list view
    -> All expenses having the product X have the AA account
    
    Cause:
    
    `sale_order_id` has been added to the `depends` of `hr_expense._compute_analytic_distribution`
    by 2b3bf5e0fe31d4b4ef6b487da493657f695b14e1 but this wrong since we have
    the `sale_expense._onchange_sale_order_id` that add the
    `analytic_dostribution` field to the fields to be computed.
    The compute is triggered since we change `product_id.expense_policy`,
    which triggers the `_compute_can_be_reinvoiced` which triggers
    the `_compute_sale_order_id`
    
    Fix:
    
    With this commit, we emove the depends on the compute and we also adapt
    `test_compute_analytic_distribution_expense` in a way that it triggers the onchange
    as we do in the original flow.
    
    opw-4998899

--- addons/project_sale_expense/models/hr_expense.py
+++ addons/project_sale_expense/models/hr_expense.py
@@ -1,16 +1,13 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models
+from odoo import models
 
 
 class Expense(models.Model):
     _inherit = "hr.expense"
 
-    @api.depends('sale_order_id')
     def _compute_analytic_distribution(self):
         super()._compute_analytic_distribution()
         if not self.env.context.get('project_id'):
-            for expense in self:
-                if not self.sale_order_id:
-                    continue
+            for expense in self.filtered('sale_order_id'):
                 expense.analytic_distribution = expense.sale_order_id.project_id._get_analytic_distribution()
