bf541f55aeecb22356b4dd9859b9af23df0c1050
Xavier-Do <xdo@odoo.com>
2025-08-13 11:49:20 +0000

    [FIX] sql_db: only enable faketime on current_database
    Filtering the postgres database was not enough, the log_db was also
    affected. Since only the order was impacted, this was creating strange
    side effect in databases like the log-db where it was only modified
    when the function exists.
    
    Checking the -d is more reliable and safer to avoid any side effect when
    testing with faketime.
    
    closes odoo/odoo#222824
    
    X-original-commit: df2ff05435769addcefdb2d463106769297e8496
    Signed-off-by: Xavier Dollé (xdo) <xdo@odoo.com>

--- odoo/service/db.py
+++ odoo/service/db.py
@@ -100,7 +100,7 @@ def _initialize_db(db_name, demo, lang, user_password, login='admin', country_co
 
 
 def _check_faketime_mode(db_name):
-    if os.getenv('ODOO_FAKETIME_TEST_MODE') and db_name != 'postgres':
+    if os.getenv('ODOO_FAKETIME_TEST_MODE') and db_name in odoo.tools.config['db_name'].split(','):
         try:
             db = odoo.sql_db.db_connect(db_name)
             with db.cursor() as cursor:
