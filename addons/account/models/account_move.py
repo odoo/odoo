72232f6f477dd850f20b496074ba0ff4018b3868
jorv-odoo <jorv@odoo.com>
2025-08-13 08:35:46 +0000

    [FIX] account: reset sending data for canceled invoices
    This commit makes the asynchronous invoice sending process more
    robust. Previously, the `_cron_account_move_send` method would fail
    if it encountered an invoice that was not in a 'posted' state. This
    could happen if a user changed the state of an invoice back to
    'draft' or 'canceled' after it was selected for sending but before
    the cron job ran.
    
    To fix this, we added an extra condition to the search domain,
    so that only posted invoices are accounted for.
    We also added an explicit reset of `sending_data` in the draft button
    method, so that setting a posted invoiced back to draft does not
    retain (outdated) sending data.
    
    Steps to reproduce:
    1. Select posted invoices and send.
    2. Before cron runs, set one invoice to 'draft'.
    3. Manually run the cron job `_cron_account_move_send`.
    4. Observe the traceback.
    
    FW 18.4:
    * adapted just the domain given refactor
    * adapted audit trail test. There seems to be a side effect
    of the `self.sending_data = False` on how the tracked values
    get triggered. But from a business logic side, the extra message
    was actually redundant, since the inform `"Updated\nTrue ⇨ False (Reviewed)"` was already included in a previous audit trail. So we
    remove that message in the expected list.
    
    OPW-4985528
    
    X-original-commit: 427f75a3842c759f9a21c2ab8923c484302f1b87

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -5639,6 +5639,7 @@ class AccountMove(models.Model):
         # We remove all the analytics entries for this journal
         self.line_ids.analytic_line_ids.with_context(skip_analytic_sync=True).unlink()
         self.state = 'draft'
+        self.sending_data = False
 
         self._detach_attachments()
 
@@ -5833,7 +5834,10 @@ class AccountMove(models.Model):
                 },
             ]
 
-        domain = [('sending_data', '!=', False)]
+        domain = [
+            ('sending_data', '!=', False),
+            ('state', '=', 'posted'),
+        ]
         to_process = self.search(domain, limit=job_count).try_lock_for_update()
         if not to_process:
             return
