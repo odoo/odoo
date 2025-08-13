5871bb087d1f2ebb658c3122b740e2ddcda3fa0c
Mélanie <mepe@odoo.com>
2025-08-13 15:48:00 +0200

    [FIX] hr: fix access to field related to hr.employee in hr.employee.public
    STEP TO REPRODUCE:
    ==================
        0- Log as Marc Demo (so without hr right)
        1- Go on Employees
        2- Click on the search bar
        3- Click on custom filter
        4- Search employee with email ([('email', '!=', False)])
        You will have a notification with invalid domain (in reality you have a traceback)
    
    REASON:
    =======
        Steps of a search on hr.employee.public with this domain = [('email', '!=', False)] (for employee without hr right):
    
            1- The field email on hr.employee.public is related to hr.employee:
                So (during the call to the method `condition_to_sql` defined in fields_relational.py)
                the domain [('email', '!=', False)]
                become
                [('employee_id', 'any!', [('email', '!=', False)]), ('employee_id', '!=', False)]
    
            2- A search method in defined for 'employee_id':
                the domain [('employee_id', 'any!', [('email', '!=', False)]), ('employee_id', '!=', False)]
                become
                [('id', 'any!', [('email', '!=', False)]), ('employee_id', '!=', False)]
    
            3- But this domain have a 'any' operator so the method `_optimize_any_domain` defined in domains.py will be called:
                the domain [('id', 'any!', [('email', '!=', False)]), ('employee_id', '!=', False)]
                will be simplify in this method and will become
                [(email', '!=', False), ('employee_id', '!=', False)]
    
        You can see an infinite loop, because the first part of the domain is the same as the initial domain.
    
    SOLUTION:
    =========
        Store the field employee_id to remove the search method defined on this field.
    
    task-5016333

--- addons/hr/tests/__init__.py
+++ addons/hr/tests/__init__.py
@@ -1,9 +1,9 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import test_attendances
 from . import test_calendar_sync
 from . import test_hr_employee
+from . import test_hr_employee_public
 from . import test_channel
 from . import test_self_user_access
 from . import test_mail_activity_plan
