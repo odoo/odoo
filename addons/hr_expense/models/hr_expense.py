c01b6a29492e4c24411b372f3c0fb87ed4d5ba1f
Louis Baudoux <lba@odoo.com>
2025-08-14 14:25:36 +0000

    [FIX] hr_expense: fix expense name not being set by OCR
    Introduces the function `_get_untitled_expense_name` that will be used
    by the OCR to check whether the expense still has its default name, in
    which case it can be overwritten.
    
    task-4684825
    
    closes odoo/odoo#223303
    
    X-original-commit: 01ec843e3af31cd0ef8e2569d47961fe22772ada
    Related: odoo/enterprise#92585
    Signed-off-by: Hugo Poncelet (hupo) <hupo@odoo.com>
    Signed-off-by: Louis Baudoux (lba) <lba@odoo.com>

--- addons/hr_expense/models/hr_expense.py
+++ addons/hr_expense/models/hr_expense.py
@@ -1195,6 +1195,11 @@ class HrExpense(models.Model):
         self._message_set_main_attachment_id(self.env["ir.attachment"].browse(kwargs['attachment_ids'][-1:]), force=True)
 
     @api.model
+    def _get_untitled_expense_name(self, *args):
+        """ Done in a specific function to be called by hr_expense_extract to keep the same translation """
+        return _("Untitled Expense %s", *args)
+
+    @api.model
     def create_expense_from_attachments(self, attachment_ids=None, view_type='list'):
         """
             Create the expenses from files.
@@ -1217,7 +1222,7 @@ class HrExpense(models.Model):
 
         for attachment in attachments:
             vals = {
-                'name': _("Untitled Expense %s", format_date(self.env, fields.Date.context_today(self))),
+                'name': self._get_untitled_expense_name(format_date(self.env, fields.Date.context_today(self))),
                 'price_unit': 0,
                 'product_id': product.id,
             }
