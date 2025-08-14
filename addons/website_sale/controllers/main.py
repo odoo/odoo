1d7edb69682d52915c44ac918a1c5f5ac44e8aff
Julien Segers (juse) <juse@odoo.com>
2025-08-14 15:20:30 +0000

    [PERF] website_sale: disable prefetcher for sitemap products
    This commit disables the prefetcher to avoid MemoryErrors
    when generating the sitemap for large product catalogs
    as web crawlers would continously crash the server
    when requesting the sitemap.
    
    opw-5001680
    opw-4955333
    
    closes odoo/odoo#228076
    
    X-original-commit: 47be0b5329a0057bac59ba8d6bca2db79f0408e5
    Signed-off-by: Julien Segers (juse) <juse@odoo.com>

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -175,7 +175,7 @@ class WebsiteSale(payment_portal.PaymentPortal):
         ProductTemplate = env['product.template']
         dom = sitemap_qs2dom(qs, '/shop', ProductTemplate._rec_name)
         dom += website.sale_product_domain()
-        for product in ProductTemplate.search(dom):
+        for product in ProductTemplate.with_context(prefetch_fields=False).search(dom):
             loc = '/shop/%s' % env['ir.http']._slug(product)
             if not qs or qs.lower() in loc:
                 yield {'loc': loc}
