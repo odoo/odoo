0095a74f56b99d6b9b2d947ad7c0f1db1573bc56
Loukas Wets (lowe) <lowe@odoo.com>
2025-08-14 09:05:40 +0000

    [FIX] point_of_sale: no prep category selected
    Steps to reproduce:
    - Open a pos restaurant config that has no prep printer/display.
    - Add an orderline.
    - The order button appear and if you try to pay the popup ask for send to
    preparation is shown.
    
    Issue:
    If there is no preparationCategories for a config getOrderChanges consider
    that all the available categories are the preparationCategories.
    
    Fix:
    If there is no preparationCategories, set the orderline uiState hasChange
    to false.
    
    Note:
    When no preparation printer category is defined, no categories is to be
    return by default. For the preparation display, if no preparation
    categories is selected preparationCategories will return all the available
    categories.
    
    Task-5016231
    
    X-original-commit: dd2cfb1b161ee8e0e5584f091e5ff004be45e309

--- addons/point_of_sale/static/tests/unit/data/pos_printer.data.js
+++ addons/point_of_sale/static/tests/unit/data/pos_printer.data.js
@@ -6,4 +6,14 @@ export class PosPrinter extends models.ServerModel {
     _load_pos_data_fields() {
         return ["id", "name", "proxy_ip", "product_categories_ids", "printer_type"];
     }
+
+    _records = [
+        {
+            id: 1,
+            name: "Printer",
+            proxy_ip: false,
+            product_categories_ids: [1, 2],
+            printer_type: "epson_epos",
+        },
+    ];
 }
