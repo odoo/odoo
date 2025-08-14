d217d08cb54fc9968603ddc281deba4087c3cac3
Mehdi Outagant (MOU) <mou@odoo.com>
2025-08-14 17:12:48 +0200

    [FIX] website, website_blog, *: fix dynamic snippets drag and drop test
    *: website_event
    
    Starting from [1], the various dynamic snippet layouts are directly
    accessible from the snippet selection dialog instead of using the
    "Templates" option.
    
    The goal of this commit is to simply add the new snippets keys in the
    "sub-snippet templates" config to prevent the snippets drag and drop
    test from failing.
    
    [1]: https://github.com/odoo/odoo/commit/e3b062e5d3820ddfcee2eb669f21edc0c53c3330
    
    runbot-230822
    
    closes odoo/odoo#223010
    
    Related: odoo/enterprise#92392
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

---
+++ addons/website_blog/static/tests/tours/sub_templates_config.js
@@ -0,0 +1,12 @@
+import { SUB_SNIPPET_TEMPLATES } from "@website/../tests/tours/snippets_all_drag_and_drop";
+
+Object.assign(SUB_SNIPPET_TEMPLATES, {
+    s_blog_posts_big_picture: "s_blog_posts",
+    s_blog_posts_card: "s_blog_posts",
+    s_blog_posts_horizontal: "s_blog_posts",
+    s_blog_posts_list: "s_blog_posts",
+    s_blog_posts_single_aside: "s_blog_posts",
+    s_blog_posts_single_full: "s_blog_posts",
+    s_blog_posts_single_circle: "s_blog_posts",
+    s_blog_posts_single_badge: "s_blog_posts",
+});
