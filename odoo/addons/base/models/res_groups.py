da5b5c47bb9e58090396e4847af20cae0d5760be
Gorash <chm@odoo.com>
2025-08-18 11:30:03 +0000

    [FIX] base: Unable to import res.groups with '/' due to full_name search
    If a group does not have privilege id and contains a /, the group search
    can return a list of group instead of the group itself. The import display
    an error message "Found multiple matches for..."
    
    opw-4885648
    
    closes odoo/odoo#224812
    
    X-original-commit: 5509c3cf861053acfa31110d6e771646fb6979ba
    Signed-off-by: Raphael Collet <rco@odoo.com>

--- odoo/addons/base/models/res_groups.py
+++ odoo/addons/base/models/res_groups.py
@@ -128,23 +128,35 @@ class ResGroups(models.Model):
         if operator in Domain.NEGATIVE_OPERATORS:
             return NotImplemented
 
-        lst = True
         if isinstance(operand, str):
-            lst = False
-            operand = [operand]
+            def make_operand(val): return val
+            operands = [operand]
+        else:
+            def make_operand(val): return [val]
+            operands = operand
 
         where_domains = [Domain('name', operator, operand)]
-        for group in operand:
+        for group in operands:
             if not group:
                 continue
-            values = [v for v in group.split('/') if v]
-            group_name = values.pop().strip() if values else ''
-            privilege_name = '/'.join(values).strip() if values else group_name
-            group_domain = Domain('name', operator, [group_name] if lst else group_name)
-            privilege_ids = self.env['res.groups.privilege'].sudo()._search(
-                Domain('name', operator, [privilege_name] if lst else privilege_name))
-            privilege_domain = Domain('privilege_id', 'in', privilege_ids)
-            where_domains.append(group_domain | privilege_domain)
+            domain = Domain('name', operator, make_operand(group))
+            where_domains.append(domain)
+
+            if '/' in group:
+                privilege_name, _, group_name = group.partition('/')
+                group_name = group_name.strip()
+                privilege_name = privilege_name.strip()
+            else:
+                privilege_name = group
+                group_name = None
+
+            if privilege_name:
+                domain = Domain(
+                    'privilege_id', 'any!', Domain('name', operator, make_operand(privilege_name)),
+                )
+                if group_name:
+                    domain &= Domain('name', operator, make_operand(group_name))
+                where_domains.append(domain)
 
         return Domain.OR(where_domains)
 
