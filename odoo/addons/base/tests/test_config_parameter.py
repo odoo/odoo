22fa8e67b927c9605f783ea298ed0123338374b0
Chong Wang (cwg) <cwg@odoo.com>
2025-08-18 10:09:23 +0200

    [IMP] base: improve config parameter
    There are different ways to modify the ir.config_parameter
    1. set_param(key, value)
    2. import from xml
            <record id="xml_id" model="ir.config_parameter">
            <field name="key">key_name</field>
            <field name="value">value</field>
        </record>
        <function model="ir.config_parameter" name="set_param" eval="('key_name', 'value')"/>
    3. ['res.config.settings'].create({"key": value}).execute()
    4. change 'res.config.settings' from UI
    
    Tricky use case 1
    If the ir.config_parameter is set by 2: when the user set it to a falsy
    value (`False`, `""`), the database will remove it as well as its
    ir.model.data. So if we hope the config won't be changed after upgrade,
    the record tag in must have both `noupdate="1"` and `forcecreate="0"`
    
    Tricky use case 2
    In the ir.config.settings, if a field has both config_parameter and
    truthy default attributes, the developers have to promise all use cases
    for `get_param()` for the key must have the same default value. The call
    `get_param(key, default)` will return a `str` if config has a truthy
    value, and `default` otherwise. So the return type is unknown and the
    developer must call str2bool/int/float to manually convert the type.
    `bool(get_param(key, True))` which is always truthy, is a very intuitive
    but wrong code.
    
    Tricky use case 3
    In the ir.config.settings if a boolean field has both config_parameter and
    default=True attributes, the developer must be very careful about
    `set_param(key, value)`, because:
     - `set_param(key, False)` means fallback the config to the default,
       `str2bool(get_param(key, 'True'))` will return `True`
     - `set_param(key, 'False')` means set the value to 'False',
       `str2bool(get_param(key, 'True'))` will return `False`
    From the UI of settings, there is no way to actually set the parameter
    to `'False'`
    
    For compatibility, the old methods `get_param` and `set_param` are still
    usable in most cases, but when set it to a falsy value, the record as
    well as its ir.model.data will be kept.
    
    New apis
    get_bool, get_int, get_float, get_str
    set_bool, set_int, set_float, set_str
    
    Task-id: 5176738
    Part-of: odoo/odoo#223180
    Related: odoo/enterprise#95537
    Related: odoo/upgrade#8647
    Signed-off-by: Chong Wang (cwg) <cwg@odoo.com>

--- odoo/addons/base/tests/test_config_parameter.py
+++ odoo/addons/base/tests/test_config_parameter.py
@@ -4,6 +4,7 @@
 from odoo.addons.base.models.ir_config_parameter import _default_parameters
 from odoo.exceptions import ValidationError
 from odoo.tests.common import TransactionCase
+from odoo.tools import mute_logger
 
 
 class TestIrConfigParameter(TransactionCase):
@@ -19,3 +20,114 @@ class TestIrConfigParameter(TransactionCase):
             new_key = f"{key}_updated"
             with self.assertRaises(ValidationError):
                 config_parameter.write({'key': new_key})
+
+    def test_str_falsy_value(self):
+        ICP = self.env['ir.config_parameter'].sudo()
+        self.assertEqual(ICP.get_str('config_key'), '')
+        self.assertEqual(ICP.get_str('config_key', 'undefined value'), 'undefined value')
+
+        ICP.set_str('config_key', 'value')
+        self.assertEqual(ICP.get_str('config_key', 'undefined value'), 'value')
+        ICP.set_str('config_key', False)
+        self.assertEqual(ICP.get_str('config_key', 'undefined value'), 'undefined value')
+
+        ICP.set_str('config_key', 'value')
+        self.assertEqual(ICP.get_str('config_key', 'undefined value'), 'value')
+        # recommended way to explicitly set the  to undefined
+        ICP.set_str('config_key', None)
+        self.assertEqual(ICP.get_str('config_key', 'undefined value'), 'undefined value')
+
+        ICP.set_str('config_key', 'value')
+        config_record = ICP.search([('key', '=', 'config_key')], limit=1)
+        self.assertEqual(config_record.value, 'value')
+        config_record.value = False  # set the configuration to undefined
+        self.assertIs(config_record.value, False)
+        self.assertEqual(ICP.get_int('config_key', 'undefined value'), 'undefined value')
+
+    def test_int_falsy_value(self):
+        ICP = self.env['ir.config_parameter'].sudo()
+        self.assertEqual(repr(ICP.get_int('config_key')), '0')
+        self.assertEqual(ICP.get_int('config_key', 100), 100)
+
+        ICP.set_int('config_key', 10)
+        self.assertEqual(ICP.get_int('config_key', 100), 10)
+        ICP.set_int('config_key', False)
+        self.assertEqual(ICP.get_int('config_key', 100), 100)
+
+        ICP.set_int('config_key', 10)
+        self.assertEqual(ICP.get_int('config_key', 100), 10)
+        # recommended way to explicitly set the configuration to undefined
+        ICP.set_int('config_key', None)
+        self.assertEqual(ICP.get_int('config_key', 100), 100)
+
+        ICP.set_int('config_key', 0)
+        self.assertEqual(ICP.get_int('config_key', 100), 0)
+
+        config_record = ICP.search([('key', '=', 'config_key')], limit=1)
+        self.assertEqual(config_record.value, '0')
+        config_record.value = False  # set the configuration to undefined
+        self.assertIs(config_record.value, False)
+        self.assertEqual(ICP.get_int('config_key', 100), 100)
+
+    def test_float_falsy_value(self):
+        ICP = self.env['ir.config_parameter'].sudo()
+        self.assertEqual(repr(ICP.get_float('config_key')), '0.0')
+        self.assertEqual(ICP.get_float('config_key', 100.0), 100.0)
+
+        ICP.set_float('config_key', 3.14)
+        self.assertEqual(ICP.get_float('config_key', 100.0), 3.14)
+        ICP.set_float('config_key', False)
+        self.assertEqual(ICP.get_float('config_key', 100.0), 100.0)
+
+        ICP.set_float('config_key', 3.14)
+        self.assertEqual(ICP.get_float('config_key', 100.0), 3.14)
+        # recommended way to explicitly set the configuration to undefined
+        ICP.set_float('config_key', None)
+        self.assertEqual(ICP.get_float('config_key', 100.0), 100.0)
+
+        ICP.set_float('config_key', 0)
+        self.assertEqual(repr(ICP.get_float('config_key', 100.0)), '0.0')
+
+        config_record = ICP.search([('key', '=', 'config_key')], limit=1)
+        self.assertEqual(config_record.value, '0.0')
+        config_record.value = False  # set the configuration to undefined
+        self.assertIs(config_record.value, False)
+        self.assertEqual(ICP.get_float('config_key', 100.0), 100.0)
+
+    def test_bool_falsy_value(self):
+        ICP = self.env['ir.config_parameter'].sudo()
+        self.assertIs(ICP.get_bool('config_key'), False)
+        self.assertEqual(ICP.get_bool('config_key', True), True)
+
+        ICP.set_bool('config_key', True)
+        self.assertEqual(ICP.get_bool('config_key', True), True)
+        # set_bool(key, False) means explicitly setting the configuration to False
+        ICP.set_bool('config_key', False)
+        self.assertEqual(ICP.get_bool('config_key', True), False)
+
+        ICP.set_bool('config_key', True)
+        self.assertEqual(ICP.get_bool('config_key', True), True)
+        ICP.set_bool('config_key', None)
+        self.assertEqual(ICP.get_bool('config_key', True), True)
+
+        ICP.set_bool('config_key', True)
+        config_record = ICP.search([('key', '=', 'config_key')], limit=1)
+        self.assertEqual(config_record.value, 'True')
+        config_record.value = False  # set the configuration to undefined
+        self.assertIs(config_record.value, False)
+        self.assertEqual(ICP.get_int('config_key', True), True)
+
+    def test_invalid_value_fixup(self):
+        ICP = self.env['ir.config_parameter'].sudo()
+        ICP.create({'key': 'config_key', 'value': 'invalid int value'})
+        with self.assertLogs('odoo.addons.base.models.ir_config_parameter', level='WARNING') as logs:
+            self.env.registry.clear_cache('stable')
+            self.assertEqual(ICP.get_int('config_key', 100), 100)
+            self.assertEqual(logs.output[0], "WARNING:odoo.addons.base.models.ir_config_parameter:ir.config_parameter with key config_key has invalid value 'invalid int value' for type int")
+
+        with mute_logger('odoo.addons.base.models.ir_config_parameter'):
+            ICP.set_int('config_key', None)
+
+        with self.assertNoLogs('odoo.addons.base.models.ir_config_parameter', 'WARNING'):
+            self.env.registry.clear_cache('stable')
+            self.assertEqual(ICP.get_int('config_key', 100), 100)
