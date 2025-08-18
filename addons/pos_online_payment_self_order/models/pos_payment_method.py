3ea5fcf4eabba1f8100facf96a23e93dc3236241
Sarah Bellefroid <sbel@odoo.com>
2025-08-18 14:35:01 +0000

    [FIX] pos_{online_payment_}self_order: enable online payments in kiosk
    Currently, you are able to see online payment methods in the kiosk if
    you have at least a terminal method registered. Since you're able to see
    them you can also select it. Once paid you are redirected to the confirmation
    screen saying that the order is being prepared but the order is not sent
    to the preparation display, although the order is paid in the backend.
    
    Steps to reproduce:
    -------------------
    * Create a payent method using terminal, for easier setup use stripe,
    you'll only need to setup serial number on the payment method but you
    do not need to do the whole stripe setup
    * Create an online payment method (use demo for example)
    * Change the kiosk settings to use those two payment methods
    * Change the settings of the preparation display to use the kiosk
    * Open preparation display
    * Open kiosk, make on order, and select the online payment method
    * Scan QR code and pay
    > You are redirected to the confirmation page saying that the order
    is being prepared while not sent to the preparation display
    
    Why the fix:
    ------------
    In this version the worflow was already sending the order to the
    kitchen. We just push the following part of the previous fix:
    
    "We decide to only allow to use the online payment methods that
    have been set on the config. Previously if two were existing but only
    one setup you were able to select any of them."
    
    opw-5001998
    
    closes odoo/odoo#226272
    
    X-original-commit: 66f6bf80980e24c63950a8d033f1860f17b705cd
    Signed-off-by: Stéphane Vanmeerhaeghe (stva) <stva@odoo.com>
    Signed-off-by: Sarah Bellefroid (sbel) <sbel@odoo.com>

--- addons/pos_online_payment_self_order/models/pos_payment_method.py
+++ addons/pos_online_payment_self_order/models/pos_payment_method.py
@@ -9,7 +9,7 @@ class PosPaymentMethod(models.Model):
     def _load_pos_self_data_domain(self, data, config):
         if config.self_ordering_mode == 'kiosk':
             domain = super()._load_pos_self_data_domain(data, config)
-            domain = Domain.OR([[('is_online_payment', '=', True)], domain])
+            domain = Domain.OR([[('is_online_payment', '=', True), ('id', 'in', config.payment_method_ids.ids)], domain])
             return domain
         else:
             return [('is_online_payment', '=', True)]
