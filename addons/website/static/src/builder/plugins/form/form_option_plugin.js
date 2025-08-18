odoo/odoo

commit 69e45976106c93d887d4fe31cdada3a6a2a393c2

[FIX] website: refresh visibility option names
Steps to reproduce the problem:
- Add a form field on the website
- Add a "Multiple Checkboxes" field in the form and make the first field
depend on this one ("Visible only if", "Custom Text", "Is equal to",
"Option 1").
- Select "Option 1" and change its name.
- Click on the first field.

-> The UI shows "Visible only if", "Custom Text", "Is equal to", "None".

The problem is that since the [website refactoring], the update of
dependencies done when a field was replaced was missing.

Related to task-4367641

[website refactoring]: https://github.com/odoo/odoo/commit/9fe45e2b7ddbbfd0445ffe25a859e67a316d02b2

closes odoo/odoo#222210

Signed-off-by: Benoit Socias (bso) <bso@odoo.com>

addons/website/static/src/builder/plugins/form/form_option_plugin.js
@@ -525,6 +525,27 @@ export class FormOptionPlugin extends Plugin {
                 field.id = targetEl.id;
             }
         }
+
+        // Synchronize the possible values with the fields whose visibility
+        // depends on the current field
+        const newValuesText = field.records.map((record) => record.id);
+        const inputEls = oldFieldEl.querySelectorAll(".s_website_form_input, option");
+        const inputName = oldFieldEl.querySelector(".s_website_form_input")?.name;
+        const formEl = oldFieldEl.closest(".s_website_form");
+        for (let i = 0; i < inputEls.length; i++) {
+            const input = inputEls[i];
+            if (newValuesText[i] && input.value && !newValuesText.includes(input.value)) {
+                for (const dependentEl of formEl.querySelectorAll(
+                    `[data-visibility-condition="${CSS.escape(
+                        input.value
+                    )}"][data-visibility-dependency="${CSS.escape(inputName)}"]`
+                )) {
+                    dependentEl.dataset.visibilityCondition = newValuesText[i];
+                }
+                break;
+            }
+        }
+
         const fieldEl = renderField(field);
         replaceFieldElement(oldFieldEl, fieldEl);
     }

addons/website/static/tests/builder/website_builder/form_option.test.js
@@ -301,13 +301,16 @@ const formWithConditionOnChexbox = `
 </form></section>
 `;
 
-const changeFieldAndCheckDependency = async (changeFieldAction) => {
+const changeFieldAndCheckDependency = async (
+    changeFieldAction,
+    fieldDependencyName = "Option 1"
+) => {
     onRpc("get_authorized_fields", () => ({}));
     await setupWebsiteBuilder(formWithConditionOnChexbox);
     await contains(":iframe input[value='Option 2']").click();
     await changeFieldAction();
     await contains(":iframe input[name='b']").click();
-    await contains("#hidden_condition_no_text_opt:contains(Option 1)").click();
+    await contains(`#hidden_condition_no_text_opt:contains(${fieldDependencyName})`).click();
 };
 
 test("Correctly set field dependency name at field rename", async () => {
@@ -325,4 +328,15 @@ test("Correctly set field dependency name at field addition", async () => {
     expect(".o-main-components-container  .o-dropdown-item:contains('Option 2')").toHaveCount(1);
     expect(".o-main-components-container  .o-dropdown-item:contains('Option 3')").toHaveCount(1);
     expect(".o-main-components-container  .o-dropdown-item:contains('Item')").toHaveCount(1);
+});
+
+test("Correctly set field dependency name at selected field rename", async () => {
+    const newName = "newName";
+    await changeFieldAndCheckDependency(
+        async () => await contains("input[data-id='0']").edit(newName),
+        newName
+    );
+    expect(".o-main-components-container  .o-dropdown-item:contains('Option 3')").toHaveCount(1);
+    expect(".o-main-components-container  .o-dropdown-item:contains('Option 2')").toHaveCount(1);
+    expect(`.o-main-components-container  .o-dropdown-item:contains('${newName}')`).toHaveCount(1);
 });

commit 88cca903dd6a4102b6ae6fbc9f3d0131560f16a1

[FIX] website: avoid error when replacing form fields without options
Steps to reproduce:
1. Open the website form snippet in edit mode.
2. Add a new field and replace it with a field without options
   (e.g., `email_cc`, `contents`, `reply_to`, `date`, etc.).
3. You will receive a traceback for a TypeError.

Root cause:
Issue started appearing after commit https://github.com/odoo/odoo/commit/69e45976106c93d887d4fe31cdada3a6a2a393c2.
In the `replaceField` method inside `FormOptionPlugin`, there is a
refresh-visibility logic where we traverse the records using
`field.records.map(...)`.
For backend fields like `datetime`, `html`, or other free-text
types, `field.records` is not set because they don’t have predefined
option values. Calling `.map()` on `undefined` caused a TypeError.

Fix:
Added a fallback to return an empty array when `field.records` is not
defined.

Related to task-4367641

addons/website/static/src/builder/plugins/form/form_option_plugin.js
@@ -528,7 +528,7 @@ export class FormOptionPlugin extends Plugin {
 
         // Synchronize the possible values with the fields whose visibility
         // depends on the current field
-        const newValuesText = field.records.map((record) => record.id);
+        const newValuesText = field.records ? field.records.map((record) => record.id) : [];
         const inputEls = oldFieldEl.querySelectorAll(".s_website_form_input, option");
         const inputName = oldFieldEl.querySelector(".s_website_form_input")?.name;
         const formEl = oldFieldEl.closest(".s_website_form");