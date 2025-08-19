608c68b37ce86b8fd2ad351b2e5820d57ea93013
Sébastien Theys <seb@odoo.com>
2025-08-19 12:47:50 +0200

    [FIX] mail, *: always assert uploading state when adding files
    * = im_livechat, website_slides
    
    Not waiting for the uploading state to be resolved can lead to
    unexpected behavior, such as deleting the attachment immediately when
    clicking on the delete button rather than showing the confirm dialog.
    
    This might also prevent the composer from being sent if the attachment
    is still uploading. This is checked when checking the enabled state of
    the send button when clicking on it, but it is not checked when pressing
    enter directly.
    
    Some tests also checked uploading was finished but didn't target the
    correct attachment.
    
    https://runbot.odoo.com/odoo/error/230901

--- addons/im_livechat/static/tests/embed/chat_window.test.js
+++ addons/im_livechat/static/tests/embed/chat_window.test.js
@@ -73,9 +73,9 @@ test("internal users can upload file to temporary thread", async () => {
     await contains(".o-mail-Composer");
     await contains("button[title='Attach files']");
     await inputFiles(".o-mail-Composer-coreMain .o_input_file", [file]);
-    await contains(".o-mail-AttachmentCard", { text: "text.txt", contains: [".fa-check"] });
+    await contains(".o-mail-AttachmentCard:not(.o-isUploading):contains(text.txt) .fa-check");
     await triggerHotkey("Enter");
-    await contains(".o-mail-Message .o-mail-AttachmentCard", { text: "text.txt" });
+    await contains(".o-mail-Message .o-mail-AttachmentCard:contains(text.txt)");
 });
 
 test("Conversation name is operator livechat user name", async () => {
