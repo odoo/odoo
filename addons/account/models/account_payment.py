3c319e38e8d91f943ebbdc3026cae0d5eb83dcfc
Ian <iada@odoo.com>
2025-08-19 17:10:51 +0000

    [FIX] account: Update move ref when payment memo is changed
    Issue: In previous versions, the memo field of account payments and ref field of account move were related
    Now that payments do not required to have generated account move's when the memo is updated the ref remains unchanged.
    
    Purpose of this PR:
    To update the account move ref when the payment memo is updated.
    
    Steps to Reproduce on Runbot:
    install accounting
    go to bank account and assign outstanding accounts for manual payment methods
    create a payment -- a related account move should be created
    update the memo on the payment -- the ref on the related account move is unchanged.
    
    opw-4989260
    
    closes odoo/odoo#226479
    
    X-original-commit: eb2602771f9c3bd2d471d0e3b4fe6d583fadddb2
    Signed-off-by: Habib Ayob (ayh) <ayh@odoo.com>

--- addons/account/models/account_payment.py
+++ addons/account/models/account_payment.py
@@ -103,7 +103,7 @@ class AccountPayment(models.Model):
         ('customer', 'Customer'),
         ('supplier', 'Vendor'),
     ], default='customer', tracking=True, required=True)
-    memo = fields.Char(string="Memo", tracking=True)
+    memo = fields.Char(string="Memo", tracking=True, inverse='_inverse_memo')
     payment_reference = fields.Char(string="Payment Reference", copy=False, tracking=True,
         help="Reference of the document used to issue this payment. Eg. check number, file name, etc.")
     currency_id = fields.Many2one(
@@ -777,6 +777,16 @@ class AccountPayment(models.Model):
             payment_id: self.env['account.payment'].browse(duplicate_ids)
             for payment_id, duplicate_ids in self.env.execute_query(query)
         }
+
+    # -------------------------------------------------------------------------
+    # ONCHANGE METHODS
+    # -------------------------------------------------------------------------
+
+    def _inverse_memo(self):
+        for payment in self:
+            move = payment.move_id
+            if move:
+                move.ref = payment.memo
 
     # -------------------------------------------------------------------------
     # CONSTRAINT METHODS
