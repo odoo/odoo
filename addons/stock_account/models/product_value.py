c57f8482c5e4283c9d6548ef7314af96bd9c6f03
Arnold Moyaux <arm@odoo.com>
2025-08-19 15:54:25 +0200

    [FIX] stock_account: correct move value after price adjustement

--- addons/stock_account/models/product_value.py
+++ addons/stock_account/models/product_value.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProductValue(models.Model):
@@ -24,3 +24,29 @@ class ProductValue(models.Model):
     date = fields.Datetime(string='Date', default=fields.Datetime.now)
 
     description = fields.Char(string='Description')
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        lot_ids = set()
+        product_ids = set()
+        move_ids = set()
+
+        for vals in vals_list:
+            if vals.get('move_id'):
+                move_ids.add(vals['move_id'])
+            elif vals.get('lot_id'):
+                lot_ids.add(vals['lot_id'])
+            else:
+                product_ids.add(vals['product_id'])
+        if lot_ids:
+            move_ids.update(self.env['stock.move'].search([('lot_id', 'in', lot_ids)]).ids)
+        products = self.env['product.product'].browse(product_ids)
+        if products:
+            moves_by_product = products._get_remaining_moves()
+            for qty_by_move in moves_by_product.values():
+                move_ids.update(self.env['stock.move'].concat(*qty_by_move.keys()).ids)
+
+        res = super().create(vals_list)
+        if move_ids:
+            self.env['stock.move'].browse(move_ids)._set_value()
+        return res
