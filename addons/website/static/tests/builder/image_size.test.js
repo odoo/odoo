70ec144b3a351e99f8f421916cc909af63b40f40
Benoit Socias <bso@odoo.com>
2025-08-19 16:02:27 +0200

    [FIX] html_editor, website: not fail on gif upload
    When uploading a gif image file, no transformation occurs and therefore
    no data URL is being generated. Because of this, trying to compute the
    size from the data URL fails.
    
    This commit avoids computing the size from non transformed images, and
    hides the size info when it is unavailable. (vs "NaN kb")
    
    task-4367641

--- addons/website/static/tests/builder/image_size.test.js
+++ addons/website/static/tests/builder/image_size.test.js
@@ -2,7 +2,7 @@ import { expect, test } from "@odoo/hoot";
 import { waitFor } from "@odoo/hoot-dom";
 import { contains } from "@web/../tests/web_test_helpers";
 import { defineWebsiteModels, setupWebsiteBuilder } from "./website_helpers";
-import { testImg, testImgSrc } from "./image_test_helpers";
+import { testImg, testImgSrc, testGifImg, testGifImgSrc } from "./image_test_helpers";
 
 defineWebsiteModels();
 
@@ -38,3 +38,25 @@ function expectAround(value, expected, delta = 0.2) {
     expect(value).toBeGreaterThan(expected - delta);
     expect(value).toBeLessThan(expected + delta);
 }
+
+test("the GIF image should NOT show its size", async () => {
+    const { waitDomUpdated } = await setupWebsiteBuilder(`
+        <div class="test-options-target">
+            ${testGifImg}
+        </div>
+    `);
+    await contains(":iframe .test-options-target img").click();
+    await waitDomUpdated();
+    expect(`[data-label="Image"] [title="Size"]`).toHaveCount(0);
+});
+
+test("the GIF background image should NOT show its size", async () => {
+    const { waitDomUpdated } = await setupWebsiteBuilder(`
+        <div class="test-options-target">
+            <section style="background-image: url(${testGifImgSrc});">text</section>
+        </div>
+    `);
+    await contains(":iframe .test-options-target section").click();
+    await waitDomUpdated();
+    expect(`[data-label="Image"] [title="Size"]`).toHaveCount(0);
+});
--- addons/website/static/tests/builder/image_test_helpers.js
+++ addons/website/static/tests/builder/image_test_helpers.js
@@ -15,23 +15,47 @@ export const testImg = `
         >
     `;
 
+export const testGifImgSrc = "/web/image/456-test/test.gif";
+
+export const testGifImg = `
+    <img src='/web/image/456-test/test.gif'>
+    `;
+
 export function mockImageRequests() {
     before(() => {
-        onRpc("/html_editor/get_image_info", async (data) => ({
-            attachment: {
-                id: 1,
-            },
-            original: {
-                id: 1,
-                image_src: "/website/static/src/img/snippets_demo/s_text_image.jpg",
-                mimetype: "image/jpeg",
-            },
-        }));
+        onRpc("/html_editor/get_image_info", async (data) => {
+            const body = await data.body.getReader().read();
+            const { src } = JSON.parse(new TextDecoder().decode(body.value)).params;
+            if (src === testGifImgSrc) {
+                return {
+                    attachment: {
+                        id: 456,
+                    },
+                    original: {
+                        id: 456,
+                        image_src:
+                            "/website/static/src/img/snippets_options/header_effect_fade_out.gif",
+                        mimetype: "image/gif",
+                    },
+                };
+            }
+            return {
+                attachment: {
+                    id: 1,
+                },
+                original: {
+                    id: 1,
+                    image_src: "/website/static/src/img/snippets_demo/s_text_image.jpg",
+                    mimetype: "image/jpeg",
+                },
+            };
+        });
         onRpcReal("/html_builder/static/image_shapes/geometric/geo_shuriken.svg");
         onRpcReal("/html_builder/static/image_shapes/pattern/pattern_wave_4.svg");
         onRpcReal("/html_builder/static/image_shapes/geometric/geo_tetris.svg");
         onRpcReal("/web/image/website.s_text_image_default_image");
         onRpcReal("/website/static/src/img/snippets_demo/s_text_image.jpg");
+        onRpcReal("/website/static/src/img/snippets_options/header_effect_fade_out.gif");
         onRpcReal("/web/image/123/transparent.png");
         onRpcReal("/website/static/src/svg/hover_effects.svg");
         onRpcReal("/html_builder/static/image_shapes/geometric/geo_square.svg");
