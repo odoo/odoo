1c5b86f23f5df8358446ba1a324dc188aa6bfc80
Thomas Becquevort (thbe) <thbe@odoo.com>
2025-08-19 10:44:51 +0200

    [FIX] l10n_it: Missing 0% EU S tax
    Description of the issue this commit addresses:
    
    The export tax for services is missing in the italian localization.
    It's required because it has a different legal note than the usual 0% EU tax.
    
    Desired behavior after this commit is merged:
    
    The italian localization has a 0% EU S for exported services in EU with the
    right legal note.
    
    task-4715771
    
    closes odoo/odoo#205521
    
    Signed-off-by: John Laterre (jol) <jol@odoo.com>

--- addons/l10n_it_edi/__init__.py
+++ addons/l10n_it_edi/__init__.py
@@ -18,6 +18,13 @@ def _l10n_it_edi_update_export_tax(env):
                     'l10n_it_kind_exoneration': 'N3.2',
                     'l10n_it_law_reference': 'Art. 41, DL n. 331/93',
                 })
+            service_tax = env.ref(f'l10n_it.{company.id}_00eus', raise_if_not_found=False)
+            if service_tax:
+                service_tax.write({
+                    'l10n_it_has_exoneration': True,
+                    'l10n_it_kind_exoneration': 'N3.2',
+                    'l10n_it_law_reference': 'Art. 7ter, DPR 633/1972',
+                })
 
 
 def _l10n_it_edi_post_init(cr, registry):
