03781a5511674685113815a95fd13bc4ab13812a
Julien (jula) <jula@odoo.com>
2025-08-20 11:28:02 +0000

    [FIX] website_sale: prevent reload on ProductReplaceMainImageAction
    __Current behavior before commit:__
    When trying to replace the main image on a product page, the iframe
    reloads before the user can choose an image in the dialog. Therefore,
    when the user chooses on an image the editing element is already
    removed, leading to a crash.
    
    __Description of the fix:__
    - Disable the automatic reload for `ProductReplaceMainImageAction` like
    it was done in saas-18.3.
    - Use `MediaWebsitePlugin` instead of dispatching "dblclick" event for
    cleaner code.
    
    __Steps to reproduce the issue:__
    1. Go to a product page.
    2. Open the editor.
    3. Click on the "Replace" button next to "Main Image".
    4. Click on an image in the dialog.
    5. Traceback:
    `TypeError: Cannot read properties of null (reading 'getComputedStyle')`
    
    opw-5010040
    
    X-original-commit: 1283c2d3ce69795af562692a6a50b8165ac3327e
    Part-of: odoo/odoo#223842
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Julien Launois (jula) <jula@odoo.com>

---
+++ addons/website_sale/static/tests/builder/product_page_option.test.js
@@ -0,0 +1,53 @@
+import { expect, test } from "@odoo/hoot";
+import { waitForNone } from "@odoo/hoot-dom";
+import { contains, defineModels, models, onRpc } from "@web/../tests/web_test_helpers";
+import {
+    defineWebsiteModels,
+    setupWebsiteBuilder,
+} from "@website/../tests/builder/website_helpers";
+
+class ProductRibbon extends models.Model {
+    _name = "product.ribbon";
+}
+
+defineWebsiteModels();
+defineModels([ProductRibbon]);
+
+test("Replace Main Image", async () => {
+    onRpc("ir.attachment", "search_read", () => [
+        {
+            mimetype: "image/png",
+            image_src: "/web/static/img/logo2.png",
+            access_token: false,
+            public: true,
+        },
+    ]);
+    await setupWebsiteBuilder(`
+        <main>
+            <div class="o_wsale_product_page">
+                <div id="product_detail_main">
+                    <div class="o_wsale_product_images">
+                        <div id="o-carousel-product">
+                            <div class="carousel-item h-100 text-center active o_colored_level" style="min-height: 693px;">
+                                <div name="o_img_with_max_suggested_width"
+                                    class="d-flex align-items-start justify-content-center h-100 oe_unmovable o_editable"
+                                    data-oe-xpath="/t[1]/div[2]/div[1]" data-oe-model="product.product" data-oe-id="13"
+                                    data-oe-field="image_1920" data-oe-type="image"
+                                    data-oe-expression="product_image.image_1920" contenteditable="false">
+                                    <img>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>`);
+
+    await contains(":iframe .o_wsale_product_page").click();
+    await contains("[data-action-id=productReplaceMainImage]").click();
+    await contains(".o_select_media_dialog .o_existing_attachment_cell button").click();
+    await waitForNone(".o_select_media_dialog");
+
+    expect(":iframe #product_detail_main img[src^='data:image/webp;base64,']").toHaveCount(1);
+    expect(":iframe img").toHaveCount(1);
+});
