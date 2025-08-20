27c89396123ce6bfdd7adf8d5aefacbdaeeb5113
Nicolas Lempereur <nle@odoo.com>
2025-08-20 14:19:03 +0200

    [FIX] http_routing,website: use request lang for dynamic bundle
    Scenario:
    
    - install right-to-left (eg. arabic) language on website
    - open a portal record with chatter (eg. /my/invoice/1)
    - switch to right-to-left language
    - scroll horizontally to the left
    
    Result: there is a huge amount of whitespace scrollable to the left.
    
    Cause:
    
    The chatter has an hidden textarea .o-mail-Composer-fake with
    position "left: -10000px; top: -10000px;".
    
    But the chatter assets (portal.assets_chatter_style) are called
    dynamically with getBundle which is using the session lang instead of
    the website lang.
    
    So the bundle is gotten with the wrong lang and the CSS is not rtlcss'ed
    and this create big whitespace to the left of the page.
    
    Fix: set the website request language when getting bundle for the
    frontend.
    
    Note: this PR also create a TestLangUrlCommon to prevent TestLangUrl
    tests of being run a second time in TestControllerRedirect.
    
    opw-5013485

--- addons/http_routing/models/ir_http.py
+++ addons/http_routing/models/ir_http.py
@@ -295,10 +295,10 @@ class IrHttp(models.AbstractModel):
 
         IrHttpModel = request.env['ir.http'].sudo()
         modules = IrHttpModel.get_translation_frontend_modules()
-        user_context = request.session.context if request.session.uid else {}
-        lang = user_context.get('lang')
+        lang = request.lang._get_cached('code')
         translation_hash = request.env['ir.http'].get_web_translations_hash(modules, lang)
 
+        session_info['bundle_params']['lang'] = lang
         session_info.update({
             'translationURL': '/website/translations',
             'cache_hashes': {
