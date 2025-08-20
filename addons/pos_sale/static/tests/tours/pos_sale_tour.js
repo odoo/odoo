bc9f48a9d2da5ffc67298b31a364990f4df0ea88
Pedram (PEBR) <pebr@odoo.com>
2025-08-20 15:35:03 +0200

    [FIX] pos_sale: allow settling order when stock moves are not created
    Before this commit, if an order was not confirmed and stock moves were
    not created, if products are tracked, the order lines would not be
    added to the PoS.
    
    opw-5026892

--- addons/pos_sale/static/tests/tours/pos_sale_tour.js
+++ addons/pos_sale/static/tests/tours/pos_sale_tour.js
@@ -495,11 +495,20 @@ registry.category("web_tour.tours").add("test_sale_order_fp_different_from_partn
         ].flat(),
 });
 
-registry.category("web_tour.tours").add("test_multiple_lots_sale_order", {
+registry.category("web_tour.tours").add("test_multiple_lots_sale_order_1", {
     steps: () =>
         [
             Chrome.startPoS(),
             Dialog.confirm("Open Register"),
+            PosSale.settleNthOrder(1),
+            Order.hasLine({ productName: "Product", quantity: "3.0" }),
+        ].flat(),
+});
+
+registry.category("web_tour.tours").add("test_multiple_lots_sale_order_2", {
+    steps: () =>
+        [
+            Chrome.startPoS(),
             PosSale.settleNthOrder(1, { loadSN: true }),
             PosSale.selectedOrderLinesHasLots("Product", ["1002"]),
             Utils.negateStep(...PosSale.selectedOrderLinesHasLots("Product", ["1001"])),
