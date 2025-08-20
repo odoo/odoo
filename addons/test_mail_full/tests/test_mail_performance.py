ea9746c5ef446551f8b5bd2064fc883c9b7200fb
Christophe Monniez <moc@odoo.com>
2025-08-20 14:59:54 +0200

    [FIX] test_mail*: fix tests when magic is installed
    Those tests are failing when python-magic is installed. Since 26f9c82b99
    Odoo > saas-18.4 has this lib as a requirement and comes with
    appropriate fixes.
    
    This commit adapts some test from 17.0 up to saas-18.4 to also
    work when the python-magic lib is installed.

--- addons/test_mail_full/tests/test_mail_performance.py
+++ addons/test_mail_full/tests/test_mail_performance.py
@@ -9,6 +9,7 @@ from odoo.addons.test_mail.tests.test_performance import BaseMailPerformance
 from odoo.tests.common import users, warmup
 from odoo.tests import tagged
 from odoo.tools import mute_logger
+from odoo.tools.mimetypes import magic
 
 
 @tagged('mail_performance', 'post_install', '-at_install')
@@ -247,6 +248,7 @@ class TestPortalFormatPerformance(FullBaseMailPerformance):
         self.assertEqual(len(res), len(messages_all))
         for format_res, message, record in zip(res, messages_all, self.messages_records):
             self.assertEqual(len(format_res['attachment_ids']), 2)
+            expected_mimetype = 'text/plain' if magic else 'application/octet-stream'
             self.assertEqual(
                 format_res['attachment_ids'],
                 [
@@ -255,7 +257,7 @@ class TestPortalFormatPerformance(FullBaseMailPerformance):
                         'checksum': message.attachment_ids[0].checksum,
                         'filename': 'Test file 1',
                         'id': message.attachment_ids[0].id,
-                        'mimetype': 'application/octet-stream',
+                        'mimetype': expected_mimetype,
                         'name': 'Test file 1',
                         'res_id': record.id,
                         'res_model': record._name,
@@ -264,7 +266,7 @@ class TestPortalFormatPerformance(FullBaseMailPerformance):
                         'checksum': message.attachment_ids[1].checksum,
                         'filename': 'Test file 0',
                         'id': message.attachment_ids[1].id,
-                        'mimetype': 'application/octet-stream',
+                        'mimetype': expected_mimetype,
                         'name': 'Test file 0',
                         'res_id': record.id,
                         'res_model': record._name,
