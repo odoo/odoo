00bf701a8c7817666799fcebdfacc0730e1d8aa4
Florian(flg) <flg@odoo.com>
2025-08-20 13:51:15 +0000

    [FIX] web: Mock call to Odoo Fin favorite institution
    The aim of this commit is making sure that the click all
    won't try to contact our external server odoo fin when the
    accounting application is opened. Indeed, the accounting
    application is displaying the favorite institutions for
    a particular country in the accounting dashboard which is
    doing a call to production.odoofin.com.
    
    This commit adds a mock using _request_handler to patch
    the call to odoo fin.
    
    runbot-error-231151
    
    X-original-commit: 8002bb126cac0a71419d0ff8a0f532b5e66253a3

--- addons/web/tests/test_click_everywhere.py
+++ addons/web/tests/test_click_everywhere.py
@@ -14,6 +14,18 @@ _logger = logging.getLogger(__name__)
 
 @odoo.tests.tagged('click_all', 'post_install', '-at_install', '-standard')
 class TestMenusAdmin(odoo.tests.HttpCase):
+    allow_end_on_form = True
+
+    @classmethod
+    def _request_handler(cls, s: Session, r: PreparedRequest, /, **kw):
+        # mock odoofin requests
+        if 'proxy/v1/get_dashboard_institutions' in r.url:
+            r = Response()
+            r.status_code = 200
+            r.json = lambda: {'result': {}}
+            return r
+        return super()._request_handler(s, r, **kw)
+
     def test_01_click_everywhere_as_admin(self):
         if 'tour_enabled' in self.env['res.users']._fields:
             self.env.ref('base.user_admin').tour_enabled = False
