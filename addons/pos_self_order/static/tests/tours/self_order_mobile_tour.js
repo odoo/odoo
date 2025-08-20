ac6c5c60f4dd28277ce3b7fd8fbdb1a772bbf87c
parp-odoo <parp@odoo.com>
2025-08-20 06:57:13 +0000

    [FIX] pos_self_order: avoid highlighting order button after sending for prep
    In the POS UI, the 'Order' button was wrongly highlighted when
    loading a self-order, even though it had already been sent
    
    Steps to reproduce:
    - Create an order using self-order mobile.
    - Open the related POS terminal.
    - Load the self-order from the ticket screen.
    - Notice the "Order" button remains highlighted.
    
    Fix:
    - Ensure the last order changes updated when loading self-order in pos
    
    closes odoo/odoo#229246
    
    Task: 5005161
    X-original-commit: b6d47859cce22a5ad74a3779049d427882def7f6
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Parthkumar Patel (parp) <parp@odoo.com>

--- addons/pos_self_order/static/tests/tours/self_order_mobile_tour.js
+++ addons/pos_self_order/static/tests/tours/self_order_mobile_tour.js
@@ -382,3 +382,20 @@ registry.category("web_tour.tours").add("test_order_table_assignement_meal", {
             Utils.clickBtn("Ok"),
         ].flat(),
 });
+
+registry.category("web_tour.tours").add("test_mobile_self_order_preparation_changes", {
+    steps: () =>
+        [
+            Utils.checkIsNoBtn("My Order"),
+            Utils.clickBtn("Order Now"),
+            ProductPage.clickProduct("Coca-Cola"),
+            ProductPage.clickProduct("Fanta"),
+            Utils.clickBtn("Checkout"),
+            CartPage.checkProduct("Fanta", "2.53", "1"),
+            CartPage.checkProduct("Coca-Cola", "2.53", "1"),
+            Utils.clickBtn("Order"),
+            ...CartPage.selectTable("1"),
+            ConfirmationPage.isShown(),
+            Utils.clickBtn("Ok"),
+        ].flat(),
+});
