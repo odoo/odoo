cf405229ec7300dd9557e52b98cb56eda8a5b1fa
ahta <ahta@odoo.com>
2025-08-20 19:48:14 +0000

    [FIX] hr_org_chart: fix employee org chart bug
    A buggy behavior when there are more than 5 managers was fixed.
    The org chart now updates before the employee record is saved.
    
    task-4609465
    
    closes odoo/odoo#223782
    
    X-original-commit: 0d185ef66c195ef5b476050ebc9f5282931cc1ce
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- addons/hr_org_chart/controllers/hr_org_chart.py
+++ addons/hr_org_chart/controllers/hr_org_chart.py
@@ -54,17 +54,20 @@ class HrOrgChartController(http.Controller):
         # compute employee data for org chart
         ancestors, current = request.env['hr.employee.public'].sudo(), employee.sudo()
         current_parent = new_parent if new_parent_id is not None else current.parent_id
-        while current_parent and len(ancestors) < self._managers_level + 1 and current != current_parent:
-            ancestors += current_parent
+        max_level = (kw.get('context')['max_level'] or self._managers_level) + 1
+        while current_parent and current != current_parent and employee.sudo() != current_parent and len(ancestors) < max_level:
             current = current_parent
             current_parent = current.parent_id if current != employee or not new_parent else new_parent
+            if current_parent in ancestors:
+                break
+            ancestors += current
 
         values = dict(
             self=self._prepare_employee_data(employee),
             managers=[
                 self._prepare_employee_data(ancestor)
                 for idx, ancestor in enumerate(ancestors)
-                if idx < self._managers_level
+                if idx < max_level - 1
             ],
             managers_more=len(ancestors) > self._managers_level,
             children=[self._prepare_employee_data(child) for child in employee.child_ids if child != employee],
