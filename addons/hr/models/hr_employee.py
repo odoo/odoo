578b06bd43d4ca1a1ff62bf63c13917a6d5cd514
Xavier Bol (xbo) <xbo@odoo.com>
2025-08-21 22:12:36 +0200

    [FIX] hr: don't add unavailable fields in employee default values
    Before this commit, when a HR user creates an employee, he will get an
    Access Error saying he does not have access to a specific field in
    `hr.version` model. The reason is because some fields are only
    accessible to HR manager or Payroll user, like `commission_target`
    field. Since `hr.employee` inheritS of `hr.version`, all version fields
    could be added in the values given by the user to add missing default
    values before creating the employee in db. The problem is the method
    adding the missing default values into initial `vals` of the create
    method, does not take into account the groups of those inherited
    fields and so, during the creation of the version for that new employee,
    the check access fields will be triggered and raise an error because in
    the inherited fields added, some of them are inaccessible by the user.
    
    This commit overrides `_prepare_create_vals` to remove the fields
    inaccessible by the current user to be sure the `vals_list` given to
    create method of `hr.version` model will only contain accessible fields
    by the user. Normally, the missing default values will alter that by
    added in hr.version and so the values initially added during the
    creation of `hr.employee` record will just be done in `hr.version`
    instead.

--- addons/hr/models/hr_employee.py
+++ addons/hr/models/hr_employee.py
@@ -219,6 +219,26 @@ class HrEmployee(models.Model):
         'A user cannot be linked to multiple employees in the same company.',
     )
 
+    def _prepare_create_values(self, vals_list):
+        result = super()._prepare_create_values(vals_list)
+        new_vals_list = []
+        Version = self.env['hr.version']
+        version_fields = [fname for fname, field in Version._fields.items() if Version._has_field_access(field, 'write')]
+        for vals in result:
+            employee_vals = {}
+            version_vals = {}
+            for fname, value in vals.items():
+                employee_field = self._fields.get(fname)
+                if not (employee_field and employee_field.inherited and employee_field.related_field.model_name == 'hr.version'):
+                    employee_vals[fname] = value
+                else:
+                    version_vals[fname] = value
+            new_vals_list.append({
+                **employee_vals,
+                **{k: v for k, v in version_vals.items() if k in version_fields},
+            })
+        return new_vals_list
+
     @api.model
     def _create(self, data_list):
         versions = [vals['stored'].pop('version_id', None) for vals in data_list]
