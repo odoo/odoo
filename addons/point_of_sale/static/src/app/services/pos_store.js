bc8c7170fe554b65f8c4037953d0ac16b3bfef53
Loukas Wets (lowe) <lowe@odoo.com>
2025-08-21 09:02:21 +0000

    [FIX] point_of_sale: no name popup when default preset
    Steps to reproduce:
    - go to the setting of your pos.config
    - set the default preset as takeout (or any other that require the name)
    - open your pos
    - click on new order
    - the slot selection opens (only if set so it's ok)
    - but then no dialog to enter the name of the client
    
    Issue:
    The handleSelectNamePreset method in the POS Restaurant module attempts
    to access the selected preset through the order, but the preset has not
    yet been set on the order at that point.
    
    Fix:
    Make sure the preset is set on the order before calling handleSelectNamePreset.
    
    Task-5030520
    
    X-original-commit: 106c2573b90d841c97b87ce9edce1f6b3ab278f1

--- addons/point_of_sale/static/src/app/services/pos_store.js
+++ addons/point_of_sale/static/src/app/services/pos_store.js
@@ -2238,10 +2238,10 @@ export class PosStore extends WithLazyGetterTrap {
                     }
                 }
             }
+            order.setPreset(preset);
             if (preset.identification === "name") {
                 await this.handleSelectNamePreset(order);
             }
-            order.setPreset(preset);
 
             if (preset.use_timing && !order.preset_time) {
                 await this.openPresetTiming(order);
