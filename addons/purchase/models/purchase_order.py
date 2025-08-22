b567350f022f36dc8e76f32b5429e1a55ae8a3d3
hapt-odoo <hapt@odoo.com>
2025-08-22 12:46:55 +0000

    [FIX] purchase: correct 'Not Acknowledged' and 'Late Receipt'
    Before this commit:
    -------------------------
    - On the Purchase Dashboard, the 'Not Acknowledged' and 'Late Receipt' counters
      also included purchase orders whose receipts had already been done.
    - For Not Acknowledged:
      - Such POs should be implicitly considered acknowledged once their receipts
        are done, but they were still shown at the counter.
      - Additionally, unacknowledged POs that were locked were incorrectly excluded
        from the counter.
    - For Late Receipt:
      - POs with received products were incorrectly counted as late, even
        though their receipts were already done.
      - Locked POs with late receipts were also excluded from the counter, which was
        an incorrect behavior.
    
    Steps to reproduce:
    -------------------------
    1. Install the 'purchase_stock' module.
    2. Open Purchase module.
    3. On the Purchase Dashboard, click on the 'Not Acknowledged' or 'Late
       Receipt' card.
    4. Notice that POs whose receipts are already completed still appear in the
       counters and some locked POs that are unacknowledged or have late receipts
       are missing from the counters
    
    Cause of the issue
    -------------------------
    - The domains for both Not Acknowledged and Late Receipt counters only relied on
      the purchase order state without checking whether the receipts were done.
    - As a result, POs with done receipts were still included in the counters, while
      some locked POs were incorrectly excluded.
    
    After this commit:
    -----------------------
    - Updated the search domains for both counters to properly exclude POs whose
      receipts are done.
    - The action_done method now triggers action_acknowledged, ensuring that any
      unacknowledged POs with completed receipts are automatically marked as
      acknowledged and excluded from the Not Acknowledged counter. Locked POs that
      remain unacknowledged will still correctly appear in the counter.
    - Now the POs with received products are no longer shown as late once their
      receipts are completed and Locked POs with late receipts will now correctly
      continue to appear in the counter.
    - This ensures that the dashboard accurately reflects only pending
      acknowledgments and genuinely late receipts, helping users track POs in
      the correct state.
    
    Task ID: 4874116
    
    closes odoo/odoo#237696
    
    X-original-commit: a16ca7c61ca34f24abd1c2c8712112c528e05321
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>
    Signed-off-by: Harshil Patel (hapt) <hapt@odoo.com>

--- addons/purchase/models/purchase_order.py
+++ addons/purchase/models/purchase_order.py
@@ -363,13 +363,22 @@ class PurchaseOrder(models.Model):
             self.order_line.filtered(lambda line: not line.display_type).date_planned = self.date_planned
 
     def _search_is_late(self, operator, value):
-        if operator != 'in':
-            return NotImplemented
-        purchase_domain = Domain('state', '=', 'purchase') & Domain('date_planned', '<=', fields.Datetime.now())
-        line_domain = Domain('order_id', 'any', purchase_domain) & Domain.custom(
-            to_sql=lambda table: SQL("%s < %s", table.qty_received, table.product_qty),
-        )
-        return Domain('order_line', 'any', line_domain)
+        if operator not in ["=", "!="]:
+            raise ValidationError(self.env._("Unsupported operator"))
+        purchase_domain = self._get_domain_is_late(operator, value)
+        if operator == "=" and value or operator == "!=" and not value:
+            purchase_lines_late = Domain('order_id', 'any', purchase_domain) & Domain.custom(
+                to_sql=lambda table: SQL("%s < %s", table.qty_received, table.product_qty),
+            )
+            return Domain('order_line', 'any', purchase_lines_late)
+        else:
+            purchase_lines_on_time = Domain('order_id', 'any', purchase_domain) & Domain.custom(
+                to_sql=lambda table: SQL("%s >= %s", table.qty_received, table.product_qty),
+            )
+            return Domain('order_line', 'any', purchase_lines_on_time)
+
+    def _get_domain_is_late(self, operator, value):
+        return Domain([('state', '=', 'purchase'), ('date_planned', '<=', fields.Datetime.now())])
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -1006,11 +1015,11 @@ class PurchaseOrder(models.Model):
         rfq_late_group = self.env['purchase.order']._read_group(rfq_late_domain, groupby, aggregate)
         _update('late', result, rfq_late_group)
 
-        rfq_not_acknowledge = [('state', '=', 'purchase'), ('acknowledged', '=', False)]
+        rfq_not_acknowledge = [('state', 'in', ['purchase', 'done']), ('acknowledged', '=', False)]
         rfq_not_acknowledge_group = self.env['purchase.order']._read_group(rfq_not_acknowledge, groupby, aggregate)
         _update('not_acknowledged', result, rfq_not_acknowledge_group)
 
-        rfq_late_receipt = [('is_late', '=', True)]
+        rfq_late_receipt = [('state', 'in', ['purchase', 'done']), ('is_late', '=', True)]
         rfq_late_receipt_group = self.env['purchase.order']._read_group(rfq_late_receipt, groupby, aggregate)
         _update('late_receipt', result, rfq_late_receipt_group)
 
