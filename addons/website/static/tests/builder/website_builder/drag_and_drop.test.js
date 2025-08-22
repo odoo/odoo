73c374b110a23048fd090db476ae72ef96a8025c
Soukéina Bojabza <sobo@odoo.com>
2025-08-22 20:57:31 +0200

    [IMP] html_builder, website, *: allow to drop inner snippets in grids
    *: website_mail_group, website_mass_mailing
    
    This commit adds the possibility to drop inner snippets directly as grid
    items.
    
    More precisely, if a snippet is dragged over a grid, it is wrapped in a
    grid item, which then becomes the dragged element. When dragged out of
    the dropzone, the snippet is unwrapped and becomes the dragged element
    again. When dropped near (so not over) a grid dropzone, the snippet is
    also wrapped in a grid item and placed on the top/left of the grid. This
    wrapping was made in a way such that we can reach the same situation as
    when dragging a grid item from the page, in order to reuse the original
    grid mode code (which is therefore common).
    
    Additionally:
    - Since the previous commit of this PR, the snippets that are alone in a
      column do not have their overlay options anymore (it is the column
      ones that are activated instead). Given that we can therefore not
      resize them anymore (in order to change their vertical padding), and
      in order to align them properly, the original padding (pt/pb classes)
      and the vertical margins (but not the bottom if still necessary) of
      the snippets are now removed if they are dropped as grid items. The
      space around them can still be adjusted thanks to the grid item
      "Padding (Y, X)" option.
    
    - When dropping the "Image" snippet as a grid item, it must become a
      grid image, in order to fill the grid area and to use the "Position"
      grid image option.
    
    - In order to not pollute the UI, when dragging the snippet over an
      inner dropzone that would overlap with a grid dropzone, the grid one
      is hidden temporarily. This allows to properly see the snippet when it
      is considered as a content.
    
    - The `on_snippet_preview_handlers` resource (used to properly preview
      the snippet when over a dropzone) was removed. Indeed, now that the
      `onDrag` hook is present, we cannot rely on the savepoint mechanism
      to revert the preview, as changes could happen in it and would
      therefore be reverted too. In order to still preview the snippets, we
      can still use the `on_snippet_over/out_dropzone_handlers` resources,
      by adapting the snippet when over a dropzone and providing a restore
      function to call when out of it. As a consequence of this change, the
      "Button" snippet preview was adapted by this commit.
    
    Note: only the drag and drop from the sidebar allows to drop an inner
    snippet in a grid. This means that a snippet already dropped on the page
    will only be dropped in normal dropzones and will not display any grid
    dropzone. Indeed, since the overlapping grid and inner dropzones really
    pollute the UI, it was decided that if the user dropped it as a simple
    content, it will stay a content. He can simply drop another snippet as a
    grid item if he wants to.
    
    task-3369611

--- addons/website/static/tests/builder/website_builder/drag_and_drop.test.js
+++ addons/website/static/tests/builder/website_builder/drag_and_drop.test.js
@@ -3,7 +3,9 @@ import { contains } from "@web/../tests/web_test_helpers";
 import {
     defineWebsiteModels,
     dummyBase64Img,
+    getDragHelper,
     getDragMoveHelper,
+    setupWebsiteBuilder,
     setupWebsiteBuilderWithSnippet,
     waitForEndOfOperation,
 } from "../website_helpers";
@@ -118,4 +120,115 @@ test("A column in mobile view should not be draggable", async () => {
 
     await contains(":iframe section.s_text_image .row > div:nth-child(1)").click();
     expect(".overlay .o_overlay_options .o_move_handle").toHaveCount(0);
+});
+
+test("Drag and drop an inner content as a grid item", async () => {
+    await setupWebsiteBuilder(
+        `<section class="s_dummy" style="width: 600px;">
+            <div class="container">
+                <div class="row o_grid_mode" data-row-count="3">
+                    <div class="o_grid_item g-height-3 g-col-lg-6 col-lg-6" style="grid-area: 1 / 1 / 4 / 7; z-index: 1;">
+                        <p>Text</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        `,
+        { loadIframeBundles: true }
+    );
+    // Drag over the grid and drop it as an inner content.
+    let dragUtils = await contains("#snippet_content [name='Alert'] .o_snippet_thumbnail").drag();
+    expect(":iframe .oe_drop_zone").toHaveCount(3);
+    expect(":iframe .oe_grid_zone").toHaveCount(1);
+    await dragUtils.moveTo(":iframe .oe_grid_zone", { position: { x: 555, y: 55 } });
+    expect(":iframe .o_we_background_grid").toHaveCount(1);
+    expect(":iframe .o_grid_item").toHaveCount(2);
+    await dragUtils.moveTo(":iframe .oe_drop_zone:not(.oe_grid_zone)");
+    expect(":iframe .o_we_background_grid").toHaveCount(0);
+    expect(":iframe .o_grid_item").toHaveCount(1);
+    expect(":iframe .oe_grid_zone").toHaveClass("invisible");
+    await dragUtils.drop(getDragHelper());
+    await waitForEndOfOperation();
+    expect(":iframe .o_grid_item:only-child div.s_alert").toHaveCount(1);
+    await contains(".o-website-builder_sidebar .fa-undo").click();
+    expect(":iframe div.s_alert").toHaveCount(0);
+
+    // Drag over the grid and drop it as a grid item.
+    dragUtils = await contains("#snippet_content [name='Alert'] .o_snippet_thumbnail").drag();
+    expect(":iframe .oe_drop_zone").toHaveCount(3);
+    expect(":iframe .oe_grid_zone").toHaveCount(1);
+    await dragUtils.moveTo(":iframe .oe_grid_zone", { position: { x: 555, y: 55 } });
+    expect(":iframe .o_we_background_grid").toHaveCount(1);
+    expect(":iframe .o_grid_item").toHaveCount(2);
+    expect(":iframe .o_we_drag_helper").toHaveStyle({
+        gridRowStart: 2,
+        gridColumnStart: 7,
+        gridColumnEnd: 13,
+    });
+    await dragUtils.moveTo(":iframe .oe_grid_zone", { position: { x: 5, y: 205 } });
+    await dragUtils.drop(getDragHelper());
+    await waitForEndOfOperation();
+    expect(":iframe .o_grid_item:nth-child(2)").toHaveStyle({
+        zIndex: 2,
+        gridRowStart: 5,
+        gridColumnStart: 1,
+        gridColumnEnd: 7,
+    });
+    await contains(".o-website-builder_sidebar .fa-undo").click();
+    expect(":iframe div.s_alert").toHaveCount(0);
+
+    // Drop near the grid (should become a grid item in the top left corner).
+    dragUtils = await contains("#snippet_content [name='Alert'] .o_snippet_thumbnail").drag();
+    expect(":iframe .oe_drop_zone").toHaveCount(3);
+    expect(":iframe .oe_grid_zone").toHaveCount(1);
+    await dragUtils.moveTo({ position: { x: 700, y: 55 } });
+    await dragUtils.drop(getDragHelper());
+    await waitForEndOfOperation();
+    expect(":iframe .o_grid_item").toHaveCount(2);
+    expect(":iframe .o_grid_item:nth-child(2)").toHaveStyle({
+        zIndex: 2,
+        gridRowStart: 1,
+        gridColumnStart: 1,
+        gridColumnEnd: 7,
+    });
+});
+
+test("Dragging an inner content from the sidebar in mobile view should not make grid dropzones appear", async () => {
+    await setupWebsiteBuilderWithSnippet("s_banner", { loadIframeBundles: true });
+    let dragUtils = await contains("#snippet_content [name='Alert'] .o_snippet_thumbnail").drag();
+    expect(":iframe .oe_grid_zone").toHaveCount(1);
+    await dragUtils.drop(getDragHelper());
+    await waitForEndOfOperation();
+    expect(":iframe .s_alert").toHaveCount(0);
+
+    // Toggle the mobile preview.
+    await contains(".o-snippets-top-actions [data-action='mobile']").click();
+    expect(".o_website_preview").toHaveClass("o_is_mobile");
+    dragUtils = await contains("#snippet_content [name='Alert'] .o_snippet_thumbnail").drag();
+    expect(":iframe .oe_grid_zone").toHaveCount(0);
+});
+
+test("Dragging an inner content from the page should not make grid dropzones appear", async () => {
+    await setupWebsiteBuilderWithSnippet("s_banner", { loadIframeBundles: true });
+    // Add an inner snippet in the first column.
+    let dragUtils = await contains("#snippet_content [name='Alert'] .o_snippet_thumbnail").drag();
+    expect(":iframe .oe_grid_zone").toHaveCount(1);
+    await dragUtils.moveTo(":iframe .oe_drop_zone");
+    await dragUtils.drop(getDragHelper());
+    await waitForEndOfOperation();
+    expect(":iframe .o_grid_item:nth-child(1) > .s_alert").toHaveCount(1);
+
+    // Redrag the snippet.
+    await contains(":iframe .s_alert").click();
+    dragUtils = await contains(".o_overlay_options .o_move_handle").drag();
+    expect(":iframe .oe_grid_zone").toHaveCount(0);
+    await dragUtils.moveTo(":iframe .oe_drop_zone");
+    await dragUtils.drop(getDragMoveHelper());
+    await waitForEndOfOperation();
+
+    // Check in mobile view.
+    await contains(".o-snippets-top-actions [data-action='mobile']").click();
+    expect(".o_website_preview").toHaveClass("o_is_mobile");
+    await contains(".o_overlay_options .o_move_handle").drag();
+    expect(":iframe .oe_grid_zone").toHaveCount(0);
 });
