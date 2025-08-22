db271903ee700da684f577b3d2ccb98d1c82651d
Meet Jivani [MEJI] <meji@odoo.com>
2025-08-22 12:35:18 +0530

    [IMP] pos_restaurant: add HOOT tests for core restaurant features
    This commit introduces HOOT test coverage for the POS Restaurant module:
    - Added tests for primary models: restaurant.table, restaurant.order.course,
    and patched pos.order
    - Added tests for patched pos_store.js.
    - Added component tests for SplitBillScreen, FloorScreen and TipScreen.
    
    task-4945629
    
    closes odoo/odoo#223628
    
    X-original-commit: 5e754bc92bd321141928ee9c3065bc2dd0b57b39
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Meet Jivani (meji) <meji@odoo.com>

--- addons/pos_restaurant/static/tests/unit/data/pos_config.data.js
+++ addons/pos_restaurant/static/tests/unit/data/pos_config.data.js
@@ -4,4 +4,8 @@ PosConfig._records = PosConfig._records.map((record) => ({
     ...record,
     module_pos_restaurant: true,
     floor_ids: [2, 3],
+    iface_tipproduct: true,
+    tip_product_id: 1,
+    set_tip_after_payment: true,
+    default_screen: "tables",
 }));
