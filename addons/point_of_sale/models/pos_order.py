f60872c1025fb69013b8514b6b0a1dbe5def8678
Pedram (PEBR) <pebr@odoo.com>
2025-08-22 15:14:45 +0000

    [FIX] point_of_sale: prevent access error when reading cost_currency_id
    Before this commit, if a product was assigned to company A and a user
    from one of its branches tried to create an order with real-time
    inventory valuation enabled, the system would raise an access error
    when reading the product's cost_currency_id field.
    
    opw-4969390
    
    closes odoo/odoo#231815
    
    X-original-commit: cf13d09b76d7150664f35784ae40dba2d1a3736b
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/point_of_sale/models/pos_order.py
+++ addons/point_of_sale/models/pos_order.py
@@ -1755,14 +1755,15 @@ class PosOrderLine(models.Model):
         """
         for line in self.filtered(lambda l: not l.is_total_cost_computed):
             product = line.product_id
+            cost_currency = product.sudo().cost_currency_id
             if line._is_product_storable_fifo_avco() and stock_moves:
                 moves = line._get_stock_moves_to_consider(stock_moves, product)
                 product_cost = moves._get_price_unit()
-                if (product.cost_currency_id.is_zero(product_cost) and line.order_id.shipping_date and line.refunded_orderline_id):
+                if (cost_currency.is_zero(product_cost) and line.order_id.shipping_date and line.refunded_orderline_id):
                     product_cost = line.refunded_orderline_id.total_cost / line.refunded_orderline_id.qty
             else:
                 product_cost = product.standard_price
-            line.total_cost = line.qty * product.cost_currency_id._convert(
+            line.total_cost = line.qty * cost_currency._convert(
                 from_amount=product_cost,
                 to_currency=line.currency_id,
                 company=line.company_id or self.env.company,
