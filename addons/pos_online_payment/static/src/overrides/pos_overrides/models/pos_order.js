120b25de551239465787dc0e9f709bdf5dac6130
Elier Ayala Bernal <elier@wclik.com>
2025-08-23 01:03:15 -0400

    [FIX] pos_online_payment: synchronize the order partner in the backend

--- addons/pos_online_payment/static/src/overrides/pos_overrides/models/pos_order.js
+++ addons/pos_online_payment/static/src/overrides/pos_overrides/models/pos_order.js
@@ -1,5 +1,6 @@
 import { patch } from "@web/core/utils/patch";
 import { PosOrder } from "@point_of_sale/app/models/pos_order";
+import { rpc } from "@web/core/network/rpc";
 
 patch(PosOrder.prototype, {
     getCustomerDisplayData() {
@@ -7,5 +8,14 @@ patch(PosOrder.prototype, {
             ...super.getCustomerDisplayData(),
             onlinePaymentData: { ...this.onlinePaymentData },
         };
+    },
+    set_partner(partner) {
+        super.set_partner(...arguments);
+        return rpc("/web/dataset/call_kw/pos.order/write", {
+            model: "pos.order",
+            method: "write",
+            args: [this.id, { partner_id: partner ? partner.id : false }],
+            kwargs: {},
+        });
     },
 });
