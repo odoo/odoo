1eead143099c5e9514bfe4f7c34e435912b8c53e
Pedram (PEBR) <pebr@odoo.com>
2025-08-25 10:30:59 +0000

    [FIX] point_of_sale: prevent syncing draft orders
    Before this commit, in a non-restaurant PoS, when selecting a partner
    in a draft order, validating an order would also sync the draft order.
    
    opw-5037759
    
    closes odoo/odoo#224710
    
    X-original-commit: a5d1330cdea7add222f6edbbfdc5fe6cdc00905c
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/pos_restaurant/static/src/app/services/pos_store.js
+++ addons/pos_restaurant/static/src/app/services/pos_store.js
@@ -990,4 +990,10 @@ patch(PosStore.prototype, {
         }
         await super.validateOrderFast(...arguments);
     },
+    setPartnerToCurrentOrder(partner) {
+        super.setPartnerToCurrentOrder(partner);
+        if (this.config.module_pos_restaurant) {
+            this.addPendingOrder([this.getOrder().id]);
+        }
+    },
 });
