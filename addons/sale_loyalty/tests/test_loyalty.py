fc75c3753361d488376d48b8f13b1cce0b22d38a
alsh-odoo <alsh@odoo.com>
2025-08-25 05:38:00 +0000

    [FIX] sale_loyalty: use reward description as SOL name for free product
    Currently, the reward's product name is used as the SOL name
    for the free product, which can be confusing.
    
    To reproduce this issue:
    1) Install the sale_loyalty module.
    2) Create a loyalty program that grants a free product.
    3) Manually update the reward's description.
    4) Create an SO with a SOL containing that product
    5) Apply the reward and observe the behavior.
    
    Issue / Cause:
    
    The free product’s description is taken from the reward product’s name,
    not the manually updated description.
    At the point of sale, the name is retrieved from
    discount_line_product_id rather than reward_product_ids
    
    https://github.com/odoo/odoo/blob/0abdcd9ef6ad3fc932dc0eb46d8aa973b00c34c2/addons/pos_loyalty/static/src/overrides/models/pos_order.js#L1162
    
    Solution:
    To resolve this inconsistent behavior, the free product name in the
    sale order line will now be taken from the reward description.
    
    opw-4982774
    
    closes odoo/odoo#229370
    
    X-original-commit: 27d9d9dbb6da135477782dff5957fb996d5b61f1
    Signed-off-by: anko-odoo <anko@odoo.com>
    Signed-off-by: Altaf Shaik (alsh) <alsh@odoo.com>

--- addons/sale_loyalty/tests/test_loyalty.py
+++ addons/sale_loyalty/tests/test_loyalty.py
@@ -1155,3 +1155,31 @@ class TestLoyalty(TestSaleCouponCommon):
         self._apply_promo_code(order, '10discount')
         msg = "Discount should be applied to the line with the cheapest valid product."
         self.assertEqual(order.order_line[3].price_total, -5.0, msg)
+
+    def test_sol_free_product_description_equals_reward_description(self):
+        """
+        Ensure that if a "Free Product" reward is added to a sale order,
+        its line description matches the reward description.
+        """
+        loyalty_program = self.env['loyalty.program'].create(
+            self.env['loyalty.program']._get_template_values()['buy_x_get_y']
+        )
+        reward = loyalty_program.reward_ids[0]
+        updated_description = f"{reward.description} Adding manual description"
+        reward.description = updated_description
+
+        order = self.empty_order
+        order.write({
+            'order_line': [
+                Command.create({
+                    'product_id': reward.reward_product_id.id,
+                    'name': '1 Product',
+                    'product_uom_qty': 4.0,
+                }),
+            ]
+        })
+        order._update_programs_and_rewards()
+        self._claim_reward(order, loyalty_program)
+
+        self.assertEqual(len(order.order_line.ids), 2)
+        self.assertEqual(order.order_line[1].name, updated_description)
