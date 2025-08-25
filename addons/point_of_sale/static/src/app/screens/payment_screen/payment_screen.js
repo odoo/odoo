0ad53c57851a86d68782aae433d6193aca1a5848
Max Whale <mawh@odoo.com>
2025-08-25 08:15:06 +0000

    [FIX] point_of_sale: offline automatic receipt printing
    Steps to reproduce:
    1. Configure a POS to use a receipt printer with automatic receipt
       printing.
    2. Confirm that the receipt is printed automatically after a order is
       made as expected.
    3. Disconnect from the internet so that POS continues in Offline mode
       (but ensure you still have access to the receipt printer on the
        local network).
    4. Make an order in offline mode.
    
    EXPECTED: The receipt is printed automatically as before
    ACTUAL: The receipt is not printed.
    
    The fix is to still run the `afterOrderValidation` method in offline
    mode, as previously it was being bypassed and the receipt screen being
    shown directly.
    
    task-4946305
    
    closes odoo/odoo#224211
    
    X-original-commit: 55289f244d739764c5db9e35570e7f8ec675a550
    Signed-off-by: Louis Travaux (lotr) <lotr@odoo.com>
    Signed-off-by: Max Whale (mawh) <mawh@odoo.com>

--- addons/point_of_sale/static/src/app/screens/payment_screen/payment_screen.js
+++ addons/point_of_sale/static/src/app/screens/payment_screen/payment_screen.js
@@ -364,7 +364,7 @@ export class PaymentScreen extends Component {
     }
     handleValidationError(error) {
         if (error instanceof ConnectionLostError) {
-            this.pos.showScreen(this.nextScreen);
+            this.afterOrderValidation();
             Promise.reject(error);
         } else if (error instanceof RPCError) {
             this.currentOrder.state = "draft";
