eee57e8739e48157511cdf128a643ec262b90ef4
Pedram (PEBR) <pebr@odoo.com>
2025-08-25 10:30:59 +0000

    [FIX] point_of_sale: prevent syncing draft orders
    Before this commit, in a non-restaurant PoS, when selecting a partner
    in a draft order, validating an order would also sync the draft order.
    
    opw-5037759
    
    X-original-commit: a5d1330cdea7add222f6edbbfdc5fe6cdc00905c

--- addons/pos_restaurant/static/src/app/services/pos_store.js
+++ addons/pos_restaurant/static/src/app/services/pos_store.js
@@ -1038,4 +1038,10 @@ patch(PosStore.prototype, {
             }
         }
     },
+    setPartnerToCurrentOrder(partner) {
+        super.setPartnerToCurrentOrder(partner);
+        if (this.config.module_pos_restaurant) {
+            this.addPendingOrder([this.getOrder().id]);
+        }
+    },
 });
