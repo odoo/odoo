904bf3551ef12930e58a74722f44aafe8baebb31
qsm-odoo <qsm@odoo.com>
2025-08-25 15:50:33 +0200

    [MOV] website: rename "new content modal" test files
    The feature will be changed to not be a modal anymore, this commit
    prepares the change by renaming the test modal files to systray files.
    
    task-2941442
    
    Part-of: odoo/odoo#217751
    Related: odoo/enterprise#89851
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -357,7 +357,7 @@
             ('include', 'website.website_builder_assets'),
             'website/static/tests/mock_server/**/*',
             'website/static/tests/redirect_field.test.js',
-            'website/static/tests/new_content_modal.test.js',
+            'website/static/tests/new_content_systray_item.test.js',
         ],
         'web.assets_unit_tests_setup': [
             'html_builder/static/src/utils/scrolling.js',
