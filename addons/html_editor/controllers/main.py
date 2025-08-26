57f7a3537bff44c56928a1db4df413465e92dff7
Rahil Ghanchi <rahg@odoo.com>
2025-08-26 11:04:31 +0530

    [IMP] website, html_editor: add option to toggle aspect-ratio
    Specification:
    - Introduced a "Vertical" toggle button in the media dialog to switch
    between horizontal and vertical view for Instagram, YouTube, Facebook.
    
    After this commit:
    - The media dialog includes a "Vertical" toggle button for switching
    video types.
    - When the "Vertical" toggle is ON, the vertical video selector
    is displayed.
    - When the "Vertical" toggle is OFF, the regular horizontal selector
    is shown.
    - The "Vertical" toggle is hidden when the media dialog is opened for
    selecting a background video.
    - New url for Instagram works well which has reel in it.
    example: https://www.instagram.com/reel/{video_id}/
    
    task-4645422
    
    Part-of: odoo/odoo#214124
    Signed-off-by: Benoit Socias (bso) <bso@odoo.com>

--- addons/html_editor/controllers/main.py
+++ addons/html_editor/controllers/main.py
@@ -351,7 +351,7 @@ class HTML_Editor(http.Controller):
     def video_url_data(self, video_url, autoplay=False, loop=False,
                        hide_controls=False, hide_fullscreen=False,
                        hide_dm_logo=False, hide_dm_share=False,
-                       start_from=False):
+                       start_from=False, **kwargs):
         return get_video_url_data(
             video_url, autoplay=autoplay, loop=loop,
             hide_controls=hide_controls, hide_fullscreen=hide_fullscreen,
