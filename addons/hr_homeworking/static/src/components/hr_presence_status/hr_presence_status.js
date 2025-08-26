0b2b7117c078cad944fc70395b09f7449531c0cb
ahta <ahta@odoo.com>
2025-08-26 11:33:35 +0200

    [IMP] hr{,_holidays,_homeworking,_presence}: create custom form widget for hr_presence_status/private
    This commit improves `hr_presence_status` & `hr_presence_status_private`
    widgets by adding custom widgets (hr_presence_status_pill &
    hr_presence_status_private_pill) to the form view modifying the to allow
    it to render a pill instead of an icon. Moreover, `view_employee_form`
    and `hr_employee_public_view_form` ui views have been updated to include
    the `hr_presence_status` widget in the top right side in the form view.
    
    The `Out of Working hours` state is changed to `Off-Hours`
    in `hr_icon_display` and `hr_presence_state` selection field.
    
    task-4930995
    
    closes odoo/odoo#218194
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr_homeworking/static/src/components/hr_presence_status/hr_presence_status.js
+++ addons/hr_homeworking/static/src/components/hr_presence_status/hr_presence_status.js
@@ -2,6 +2,8 @@ import { patch } from "@web/core/utils/patch";
 
 import { HrPresenceStatus, hrPresenceStatus } from "@hr/components/hr_presence_status/hr_presence_status";
 import { HrPresenceStatusPrivate, hrPresenceStatusPrivate } from "@hr/components/hr_presence_status_private/hr_presence_status_private";
+import { HrPresenceStatusPill, hrPresenceStatusPill } from "@hr/components/hr_presence_status_pill/hr_presence_status_pill";
+import { HrPresenceStatusPrivatePill, hrPresenceStatusPrivatePill } from "@hr/components/hr_presence_status_private_pill/hr_presence_status_private_pill";
 import { _t } from "@web/core/l10n/translation";
 
 const patchHrPresenceStatus = () => ({
@@ -42,9 +44,35 @@ const patchHrPresenceStatus = () => ({
     },
 });
 
+const patchHrPresenceStatusPill = () => ({
+    get color() {
+        if (this.location) {
+            let color = "btn-outline-secondary text-muted";
+            if (this.props.record.data.hr_presence_state !== "out_of_working_hour") {
+                color =
+                    this.props.record.data.hr_presence_state === "present"
+                        ? "btn-outline-success"
+                        : "btn-outline-warning";
+            }
+            return color;
+        }
+        return super.color;
+    },
+
+    get label() {
+        if (this.location) {
+            return this.props.record.data.work_location_name || _t("Unspecified");
+        }
+        return super.label;
+    },
+});
+
 // for the both components: first applies the common patch and then applies patch for label
 patch(HrPresenceStatus.prototype, patchHrPresenceStatus());
 patch(HrPresenceStatusPrivate.prototype, patchHrPresenceStatus());
+
+patch(HrPresenceStatusPill.prototype, patchHrPresenceStatusPill());
+patch(HrPresenceStatusPrivatePill.prototype, patchHrPresenceStatusPill());
 
 const additionalFieldDependencies = [
     { name: "hr_presence_state", type: "selection" },
@@ -66,5 +94,23 @@ if (typeof hrPresenceStatus.fieldDependencies === "function") {
 }
 hrPresenceStatusPrivate.fieldDependencies = [
     ...(hrPresenceStatusPrivate.fieldDependencies || []),
+    ...additionalFieldDependencies,
+];
+
+if (typeof hrPresenceStatusPill.fieldDependencies === "function") {
+    const oldFieldDependencies = hrPresenceStatusPill.fieldDependencies;
+    hrPresenceStatusPill.fieldDependencies = (widgetInfo) => {
+        const fieldDependencies = oldFieldDependencies(widgetInfo);
+        fieldDependencies.push(...additionalFieldDependencies);
+        return fieldDependencies;
+    };
+} else {
+    hrPresenceStatusPill.fieldDependencies = [
+        ...(hrPresenceStatusPill.fieldDependencies || []),
+        ...additionalFieldDependencies,
+    ];
+}
+hrPresenceStatusPrivatePill.fieldDependencies = [
+    ...(hrPresenceStatusPrivatePill.fieldDependencies || []),
     ...additionalFieldDependencies,
 ];
