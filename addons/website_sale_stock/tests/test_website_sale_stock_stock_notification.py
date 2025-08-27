4f509be5d1560308e8ba74e27a8ce5b0720320e3
Laetitia (ldau) <ldau@odoo.com>
2025-08-27 09:38:30 +0200

    [IMP] mail, (website_)sale: mails design update
    This commit improves the layout of email templates for abandoned carts
    and order confirmations and for the product availability email.
    
    Specifically, it includes the following enhancements:
    - Abandoned Cart: The recovery button now only appears when necessary.
    - Order Confirmation:
        - The website logo is used instead of the company logo if the sale
        occurred through the website.
        - A color display bug has been fixed.
        - Additional information is now displayed for rental and subscription
        products.
        - The total for combo products is displayed in the title instead of
        at the end of the product details.
        - The total price is now displayed instead of the unit price for
        order lines with multiple quantities.
    - Product Availability:
        - The design has been modified to be consistent with the new
        order confirmation design.
        - The website salesperson is now used as the sender.
    
    Additional improvements:
    - The behavior now aligns with the `shop_cart_recovery` tour and ensures
    consistency by adding the recovery button by default. Users can hide
    this button by setting a flag in the context, which is particularly
    useful when the email layout already includes the same button in the
    header.
    
    task-4826618
    
    closes odoo/odoo#213186
    
    Related: odoo/upgrade#7839
    Signed-off-by: Valentin Chevalier <vcr@odoo.com>
    Co-authored-by: Lionel Piraux (lipi) <lipi@odoo.com>

--- addons/website_sale_stock/tests/test_website_sale_stock_stock_notification.py
+++ addons/website_sale_stock/tests/test_website_sale_stock_stock_notification.py
@@ -1,58 +1,53 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from contextlib import contextmanager
+
 from odoo.tests import tagged
 from odoo.tests.common import HttpCase
 
+from odoo.addons.website_sale_stock.tests.common import WebsiteSaleStockCommon
+
 
 @tagged('post_install', '-at_install')
-class TestStockNotificationProduct(HttpCase):
+class TestStockNotificationProduct(WebsiteSaleStockCommon, HttpCase):
+
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        warehouse = cls.env['stock.warehouse'].create({
-            'name': 'Wishlist Warehouse',
-            'code': 'W_WH'
-        })
-        current_website = cls.env['website'].get_current_website()
-        current_website.warehouse_id = warehouse
-
-        cls.warehouse = warehouse
-        cls.current_website = current_website
-
-        cls.product = cls.env['product.product'].create({
-            'name': 'Macbook Pro',
-            'website_published': True,
-            'is_storable': True,
-            'allow_out_of_stock_order': False,
-
-        })
-        cls.pricelist = cls.env['product.pricelist'].create({
-            'name': 'Public Pricelist',
-        })
-        cls.currency = cls.env.ref("base.USD")
+        cls.macbook = cls._create_product(name="Macbook Pro")
 
     def test_back_in_stock_notification_product(self):
-        self.start_tour(self.product.website_url, 'website_sale_stock.subscribe_to_stock_notification')
+        self.start_tour(self.macbook.website_url, 'website_sale_stock.subscribe_to_stock_notification')
 
         partner = self.env['mail.thread']._partner_find_from_emails_single(['test@test.test'], no_create=True)
-        ProductProduct = self.env['product.product']
-        product = ProductProduct.browse(self.product.id)
-        self.assertTrue(product._has_stock_notification(partner))
+        self.assertTrue(self.macbook._has_stock_notification(partner))
 
-        # No email should be sent
-        ProductProduct._send_availability_email()
+        with self.setup_cron_env() as env:
+            env['product.product']._send_availability_email()
+
         emails = self.env['mail.mail'].search([('email_to', '=', partner.email_formatted)])
         self.assertEqual(len(emails), 0)
 
-        # Replenish Product
-        quants = self.env['stock.quant'].with_context(inventory_mode=True).create({
-            'product_id': self.product.id,
-            'inventory_quantity': 10.0,
-            'location_id': self.warehouse.lot_stock_id.id,
-        })
-        quants.action_apply_inventory()
+        self._add_product_qty_to_wh(self.macbook.id, 10.0, self.warehouse.lot_stock_id.id)
 
-        ProductProduct._send_availability_email()
+        with self.setup_cron_env() as env:
+            env['product.product']._send_availability_email()
+
         emails = self.env['mail.mail'].search([('email_to', '=', partner.email_formatted)])
-        self.assertEqual(emails[0].subject, "The product 'Macbook Pro' is now available")
-        self.assertFalse(product._has_stock_notification(partner))
+        self.assertEqual(emails[0].subject, "Macbook Pro is back in stock")
+        self.assertFalse(self.macbook._has_stock_notification(partner))
+
+    @contextmanager
+    def setup_cron_env(self):
+        """Setup the `TestCursor` required to execute a cron job and ensures proper handling of the
+        environment before and after the operation.
+
+        Note: In `HttpCase`, `TransactionCase.enter_registry_test_mode` is enabled by default.
+        """
+        env = self.env
+        env.flush_all()
+        try:
+            with self.registry.cursor() as cr:  # Creates a temporary `TestCursor`
+                yield env(cr=cr)
+        finally:
+            env.invalidate_all()
