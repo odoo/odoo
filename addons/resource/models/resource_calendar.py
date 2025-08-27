1f016496f27b97e9947e755044bd9b6c63d4967b
st-yes <soty@odoo.com>
2025-08-27 10:56:07 +0200

    [FIX] resource, hr_holidays: expand multi-day leaves in flexible schedules
    Issue:
    
    Multi-day public holidays only display as single days in calendar views when using flexible working schedules.
    A 3-day holiday appears as only 1 day blocked, though time-off requests are still correctly prevented for all 3 days.
    
    Cause:
    
    In `_get_unusual_days()`, the implementation for flexible schedules only captures the start date of each leave interval
    
    https://github.com/odoo/odoo/blob/132938929d46c8248a9e3a7e2972174ae38eacfa/addons/resource/models/resource_calendar.py#L683-L685
    
    Steps to reproduce:
    
    1. Configure a flexible working schedule for an employee or company
    2. Go to Time Off > Public Holidays
    3. Create a 3 day public holiday
    4. Check Time Off calendar view
    
    Only 1 day appears blocked instead of all 3 days
    
    opw-4997757
    
    closes odoo/odoo#224668
    
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- addons/resource/models/resource_calendar.py
+++ addons/resource/models/resource_calendar.py
@@ -681,7 +681,10 @@ class ResourceCalendar(models.Model):
         if company_id:
             domain = [('company_id', 'in', (company_id.id, False))]
         if self.flexible_hours:
-            works = {d[0].date() for d in self._leave_intervals_batch(start_dt, end_dt, domain=domain)[False]}
+            leave_intervals = self._leave_intervals_batch(start_dt, end_dt, domain=domain)[False]
+            works = set()
+            for start_int, end_int, _ in leave_intervals:
+                works.update(start_int.date() + timedelta(days=i) for i in range((end_int.date() - start_int.date()).days + 1))
             return {fields.Date.to_string(day.date()): (day.date() in works) for day in rrule(DAILY, start_dt, until=end_dt)}
         works = {d[0].date() for d in self._work_intervals_batch(start_dt, end_dt, domain=domain)[False]}
         return {fields.Date.to_string(day.date()): (day.date() not in works) for day in rrule(DAILY, start_dt, until=end_dt)}
