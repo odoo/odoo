e22e8c3c9ee0b4b598663747dee78a4143ce8e9d
Meet Jivani [MEJI] <meji@odoo.com>
2025-08-27 11:04:00 +0530

    [IMP] pos_restaurant: add HOOT tests for core restaurant features
    This commit introduces HOOT test coverage for the POS Restaurant module:
    - Added tests for primary models: restaurant.table, restaurant.order.course,
    and patched pos.order
    - Added tests for patched pos_store.js.
    - Added component tests for SplitBillScreen, FloorScreen and TipScreen.
    
    task-4945629
    
    closes odoo/odoo#223895
    
    X-original-commit: db271903ee700da684f577b3d2ccb98d1c82651d
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Meet Jivani (meji) <meji@odoo.com>

---
+++ addons/pos_restaurant/static/tests/unit/models/restaurant_table.test.js
@@ -0,0 +1,64 @@
+import { test, describe, expect } from "@odoo/hoot";
+import { setupPosEnv } from "@point_of_sale/../tests/unit/utils";
+import { definePosModels } from "@point_of_sale/../tests/unit/data/generate_model_definitions";
+
+definePosModels();
+
+describe("restaurant.table", () => {
+    test("getOrders", async () => {
+        const store = await setupPosEnv();
+        const table = store.models["restaurant.table"].get(2);
+        const order = store.addNewOrder({ table_id: table });
+        const tableOrders = table.getOrders();
+        expect(tableOrders.length).toBe(1);
+        expect(tableOrders[0].id).toBe(order.id);
+    });
+
+    test("getParent and isParent", async () => {
+        const store = await setupPosEnv();
+        const models = store.models;
+        const parent = models["restaurant.table"].get(2);
+        const child = models["restaurant.table"].get(3);
+        child.parent_id = parent;
+        const result = child.getParent();
+        expect(parent.isParent(child)).toBe(true);
+        expect(result.id).toBe(2);
+    });
+
+    test("getParentSide", async () => {
+        const store = await setupPosEnv();
+        const models = store.models;
+        const parent = models["restaurant.table"].get(2);
+        const child = models["restaurant.table"].get(3);
+        child.parent_id = parent;
+        child.position_h = parent.position_h + 50;
+        child.position_v = parent.position_v;
+        const side = child.getParentSide();
+        expect(side).toBe("left");
+    });
+
+    test("getX and getY", async () => {
+        const store = await setupPosEnv();
+        const table1 = store.models["restaurant.table"].get(2);
+        const table2 = store.models["restaurant.table"].get(3);
+        expect(table1.getX()).toBe(407);
+        expect(table1.getY()).toBe(88);
+        table2.parent_id = table1;
+        table2.parent_side = "left";
+        expect(table2.getX()).toBe(497);
+        expect(table2.getY()).toBe(88);
+        table2.parent_side = "top";
+        expect(table2.getX()).toBe(407);
+        expect(table2.getY()).toBe(-2);
+    });
+
+    test("rootTable", async () => {
+        const store = await setupPosEnv();
+        const table1 = store.models["restaurant.table"].get(2);
+        const table2 = store.models["restaurant.table"].get(3);
+        const table3 = store.models["restaurant.table"].get(4);
+        table2.parent_id = table1;
+        table3.parent_id = table2;
+        expect(table3.rootTable.id).toBe(table1.id);
+    });
+});
