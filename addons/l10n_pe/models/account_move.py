9a330f194717151a742e6e44f696a430dafe4279
unka-odoo <unka@odoo.com>
2025-08-27 10:54:26 +0530

    [IMP] l10n_pe:  Autofill correct doc type for credit/debit notes of boletas
    Before this commit:
    - In Peru localization, users have to manually set the correct
      l10n_latam_document_type when creating credit/debit notes.
    
    After this commit:
    - The correct l10n_latam_document_type based on the originating document will be
      set automatically while creating credit/debit notes.
    
    Task: 4284986

--- addons/l10n_pe/models/account_move.py
+++ addons/l10n_pe/models/account_move.py
@@ -1,6 +1,11 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from odoo import api, models
 
+PE_DOC_SUBTYPES = [
+    ("l10n_pe.document_type01", "l10n_pe.document_type07", "l10n_pe.document_type08"),  # e-invoice
+    ("l10n_pe.document_type02", "l10n_pe.document_type07b", "l10n_pe.document_type08b"),  # e-boleta
+]
+
 
 class AccountMove(models.Model):
     _inherit = "account.move"
@@ -10,6 +15,15 @@ class AccountMove(models.Model):
         result = super()._get_l10n_latam_documents_domain()
         if self.company_id.country_id.code != "PE" or not self.l10n_latam_use_documents or self.journal_id.type != "sale":
             return result
+        if self.debit_origin_id:
+            result.append(("internal_type", "=", "debit_note"))
+        if (original_move := self.reversed_entry_id or self.debit_origin_id) and (doc_type := original_move.l10n_latam_document_type_id):
+            doc_type_xml_id = doc_type.get_external_id().get(doc_type.id)
+            for doc_subtype_group in PE_DOC_SUBTYPES:
+                if doc_type_xml_id in doc_subtype_group:
+                    doc_subtype_group_ids = tuple(self.env.ref(type).id for type in doc_subtype_group)
+                    result.append(("id", "in", doc_subtype_group_ids))
+                    break
         result.append(("code", "in", ("01", "03", "07", "08", "20", "40")))
         if self.partner_id.l10n_latam_identification_type_id.l10n_pe_vat_code != '6' and self.move_type == 'out_invoice':
             result.append(('id', 'in', (
