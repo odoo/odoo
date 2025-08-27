b1868af4c000d8689b0f899206968306178f7a99
Meet Jivani [MEJI] <meji@odoo.com>
2025-08-27 11:04:00 +0530

    [IMP] pos_restaurant: add HOOT tests for core restaurant features
    This commit introduces HOOT test coverage for the POS Restaurant module:
    - Added tests for primary models: restaurant.table, restaurant.order.course,
    and patched pos.order
    - Added tests for patched pos_store.js.
    - Added component tests for SplitBillScreen, FloorScreen and TipScreen.
    
    task-4945629
    
    X-original-commit: db271903ee700da684f577b3d2ccb98d1c82651d

--- addons/pos_restaurant/static/tests/unit/data/pos_session.data.js
+++ addons/pos_restaurant/static/tests/unit/data/pos_session.data.js
@@ -3,6 +3,11 @@ import { patch } from "@web/core/utils/patch";
 
 patch(PosSession.prototype, {
     _load_pos_data_models() {
-        return [...super._load_pos_data_models(), "restaurant.floor", "restaurant.table"];
+        return [
+            ...super._load_pos_data_models(),
+            "restaurant.floor",
+            "restaurant.table",
+            "restaurant.order.course",
+        ];
     },
 });
