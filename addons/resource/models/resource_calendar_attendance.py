77f860f5d3757e5a56861ac1de95b9ad29ea0dff
Abdelrahman Mahmoud (amah) <amah@odoo.com>
2025-08-27 12:42:05 +0200

    [IMP] resource: Resource attendance rework
    - Added 'full_day' as a selection value for `day_period`
    - Displayed `duration_hours` in the list view
    - Made `duration_hours` editable and compute `hours` based on it
    - Removed `date_from` and `date_to` fields for simplification
    
    closes odoo/odoo#217895
    
    Task: 4929940
    Related: odoo/enterprise#92500
    Related: odoo/upgrade#8030
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/resource/models/resource_calendar_attendance.py
+++ addons/resource/models/resource_calendar_attendance.py
@@ -2,7 +2,8 @@
 
 import math
 
-from odoo import api, fields, models, _
+from odoo import api, fields, models
+from odoo.exceptions import UserError
 
 
 class ResourceCalendarAttendance(models.Model):
@@ -20,23 +21,22 @@ class ResourceCalendarAttendance(models.Model):
         ('5', 'Saturday'),
         ('6', 'Sunday')
         ], 'Day of Week', required=True, index=True, default='0')
-    date_from = fields.Date(string='Starting Date')
-    date_to = fields.Date(string='End Date')
-    hour_from = fields.Float(string='Work from', required=True, index=True,
+    hour_from = fields.Float(string='Work from', default=0, required=True, index=True,
         help="Start and End time of working.\n"
              "A specific value of 24:00 is interpreted as 23:59:59.999999.")
-    hour_to = fields.Float(string='Work to', required=True)
+    hour_to = fields.Float(string='Work to', default=0, required=True)
     # For the hour duration, the compute function is used to compute the value
     # unambiguously, while the duration in days is computed for the default
     # value based on the day_period but can be manually overridden.
-    duration_hours = fields.Float(compute='_compute_duration_hours', string='Duration (hours)')
+    duration_hours = fields.Float(compute='_compute_duration_hours', inverse='_inverse_duration_hours', string='Duration (hours)', store=True, readonly=False)
     duration_days = fields.Float(compute='_compute_duration_days', string='Duration (days)', store=True, readonly=False)
     calendar_id = fields.Many2one("resource.calendar", string="Resource's Calendar", required=True, index=True, ondelete='cascade')
+    duration_based = fields.Boolean(related="calendar_id.duration_based")
     day_period = fields.Selection([
         ('morning', 'Morning'),
         ('lunch', 'Break'),
-        ('afternoon', 'Afternoon')], required=True, default='morning')
-    resource_id = fields.Many2one('resource.resource', 'Resource')
+        ('afternoon', 'Afternoon'),
+        ('full_day', 'Full Day')], required=True, default='morning')
     week_type = fields.Selection([
         ('1', 'Second'),
         ('0', 'First')
@@ -58,6 +58,12 @@ class ResourceCalendarAttendance(models.Model):
         # avoid wrong order
         self.hour_to = max(self.hour_to, self.hour_from)
 
+    @api.constrains('day_period')
+    def _check_day_period(self):
+        for attendance in self:
+            if attendance.day_period == 'lunch' and attendance.duration_based:
+                raise UserError(self.env._("%(att)s is a break attendance, You should not have such record on duration based calendar", att=attendance.name))
+
     @api.model
     def get_week_type(self, date):
         # week_type is defined by
@@ -70,14 +76,32 @@ class ResourceCalendarAttendance(models.Model):
 
     @api.depends('hour_from', 'hour_to')
     def _compute_duration_hours(self):
-        for attendance in self:
+        for attendance in self.filtered('hour_to'):
             attendance.duration_hours = (attendance.hour_to - attendance.hour_from) if attendance.day_period != 'lunch' else 0
 
-    @api.depends('day_period', 'duration_hours')
+    def _inverse_duration_hours(self):
+        for calendar, attendances in self.grouped('calendar_id').items():
+            if not calendar.duration_based:
+                continue
+            for attendance in attendances:
+                if attendance.day_period == 'full_day':
+                    period_duration = attendance.duration_hours / 2
+                    attendance.hour_to = 12 + period_duration
+                    attendance.hour_from = 12 - period_duration
+                elif attendance.day_period == 'morning':
+                    attendance.hour_to = 12
+                    attendance.hour_from = 12 - attendance.duration_hours
+                elif attendance.day_period == 'afternoon':
+                    attendance.hour_to = 12 + attendance.duration_hours
+                    attendance.hour_from = 12
+
+    @api.depends('day_period')
     def _compute_duration_days(self):
         for attendance in self:
             if attendance.day_period == 'lunch':
                 attendance.duration_days = 0
+            elif attendance.day_period == 'full_day':
+                attendance.duration_days = 1
             else:
                 attendance.duration_days = 0.5 if attendance.duration_hours <= attendance.calendar_id.hours_per_day * 3 / 4 else 1
 
@@ -85,8 +109,8 @@ class ResourceCalendarAttendance(models.Model):
     def _compute_display_name(self):
         super()._compute_display_name()
         this_week_type = str(self.get_week_type(fields.Date.context_today(self)))
-        section_names = {'0': _('First week'), '1': _('Second week')}
-        section_info = {True: _('this week'), False: _('other week')}
+        section_names = {'0': self.env._('First week'), '1': self.env._('Second week')}
+        section_info = {True: self.env._('this week'), False: self.env._('other week')}
         for record in self.filtered(lambda l: l.display_type == 'line_section'):
             section_name = f"{section_names[record.week_type]} ({section_info[this_week_type == record.week_type]})"
             record.display_name = section_name
@@ -96,8 +120,6 @@ class ResourceCalendarAttendance(models.Model):
         return {
             'name': self.name,
             'dayofweek': self.dayofweek,
-            'date_from': self.date_from,
-            'date_to': self.date_to,
             'hour_from': self.hour_from,
             'hour_to': self.hour_to,
             'day_period': self.day_period,
@@ -107,4 +129,4 @@ class ResourceCalendarAttendance(models.Model):
         }
 
     def _is_work_period(self):
-        return self.day_period != 'lunch'
+        return self.day_period != 'lunch' and not self.display_type
