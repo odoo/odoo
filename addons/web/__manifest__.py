94b9596efe35868837df6ba6277a5f91da211852
Julien Mougenot <jum@odoo.com>
2025-08-27 12:51:05 +0200

    [FIX] web: Hoot - lazy load UI libraries
    This commit separates the DiffMatchPatch lib from the Hoot directory, to
    put it with the other libs in web/src/libs, and adds a system to lazy-
    load certain "UI" libs in non-headless mode. This should speed up the
    assets loading and the test errors processing on Runbot.

--- addons/web/__manifest__.py
+++ addons/web/__manifest__.py
@@ -459,6 +459,10 @@ This module provides the core of the Odoo Web Client.
             'web/static/src/public/public_component_service.js',
             'web/static/src/webclient/clickbot/clickbot.js',
         ],
+        # Lazy-loaded assets needed by test framework when not in headless mode
+        'web.assets_unit_tests_setup_ui': [
+            "web/static/lib/diff_match_patch/diff_match_patch.js",
+        ],
         # Unit test files
         'web.assets_unit_tests': [
             'web/static/tests/**/*',
