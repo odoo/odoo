7411d5e57b2fc3324f291a3db7901d8814011886
Rémy Voet (ryv) <ryv@odoo.com>
2025-08-28 17:09:30 +0200

    [IMP] core: Add currency conversion aggregator
    Since odoo/odoo#216597, the sum of monetary fields is displayed even
    when multiple currencies are involved in the computation. A question
    mark tooltip is shown and no currency symbol is displayed in this case.
    This information isn't very meaningful for an end-user in a
    multicurrency environment, but it's still better than the 18.0
    behavior, where there was no tooltip and no currency symbol, even when
    only one currency was involved.
    
    We want to display more relevant information when multiple currencies
    are implied. We'll show the sum of the monetary values converted into
    the current company currency, based on the closest currency rate. This
    is done with a new type of `_read_group` aggregator targeting monetary
    fields (`:sum_currency`).
    
    The web client should request this new aggregate alongside
    `currency_id:array_agg_distinct`.

--- odoo/orm/models.py
+++ odoo/orm/models.py
@@ -1967,10 +1967,45 @@ class BaseModel(metaclass=MetaModel):
             raise ValueError(f"Invalid field {fname!r} on model {self._name!r} for {aggregate_spec!r}.")
         if not func:
             raise ValueError(f"Aggregate method is mandatory for {fname!r}")
+
+        field = self._fields[fname]
+        if func == 'sum_currency':
+            if field.type != 'monetary':
+                raise ValueError(f'Aggregator "sum_currency" only works on currency field for {fname!r}')
+
+            CurrencyRate = self.env['res.currency.rate']
+            rate_subquery_table = SQL(
+                """(SELECT DISTINCT ON (%(currency_field_sql)s) %(currency_field_sql)s, %(rate_field_sql)s
+                    FROM "res_currency_rate"
+                    WHERE %(company_field_sql)s IS NULL OR %(company_field_sql)s = %(company_id)s
+                    ORDER BY
+                        %(currency_field_sql)s,
+                        %(company_field_sql)s,
+                        CASE WHEN %(name_field_sql)s <= %(today)s THEN %(name_field_sql)s END DESC,
+                        CASE WHEN %(name_field_sql)s > %(today)s THEN %(name_field_sql)s END ASC)
+                """,
+                currency_field_sql=CurrencyRate._field_to_sql(CurrencyRate._table, 'currency_id'),
+                rate_field_sql=CurrencyRate._field_to_sql(CurrencyRate._table, 'rate'),
+                company_field_sql=CurrencyRate._field_to_sql(CurrencyRate._table, 'company_id'),
+                company_id=self.env.company.root_id.id,
+                name_field_sql=CurrencyRate._field_to_sql(CurrencyRate._table, 'name'),
+                today=Date.context_today(self),
+            )
+            alias_rate = query.make_alias(self._table, 'rates')
+            currency_field_name = field.get_currency_field(self)
+            currency_field_sql = self._field_to_sql(self._table, currency_field_name, query)
+            condition = SQL("%s = %s", currency_field_sql, SQL.identifier(alias_rate, "currency_id"))
+            query.add_join('LEFT JOIN', alias_rate, rate_subquery_table, condition)
+
+            return SQL(
+                "SUM(%s / COALESCE(%s, 1.0))",
+                self._field_to_sql(self._table, fname, query),
+                SQL.identifier(alias_rate, "rate"),
+            )
+
         if func not in READ_GROUP_AGGREGATE:
             raise ValueError(f"Invalid aggregate method {func!r} for {aggregate_spec!r}.")
 
-        field = self._fields[fname]
         if func == 'recordset' and not (field.relational or fname == 'id'):
             raise ValueError(f"Aggregate method {func!r} can be only used on relational field (or id) (for {aggregate_spec!r}).")
 
