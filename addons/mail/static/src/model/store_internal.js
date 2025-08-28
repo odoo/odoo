9cff30c32fcc34495b475c7b9a753637d831d6a4
Alex Kühn <aku@odoo.com>
2025-08-28 11:44:00 +0000

    [FIX] mail: no soft-deleted records in Model.records and Model.get()
    Before this commit, business code could leak records that are soft
    deleted, i.e. the representation of the record is still in the store
    but conceptually this record is deleted.
    
    These soft-deleted records are present in the code for a very short
    time, just enough time to warn business code in the onDelete()
    hooks.
    
    The problem for Model.records and Model.get() to return these
    records, is that the business code thinks these records are not
    deleted, which can lead to misuses and crashes.
    
    For example: if a computed field returns this soft-deleted record,
    then the field value has this soft-deleted record. When the record is
    hard-deleted, this field will be recomputed, but when diffing with
    its old this will crash because it will still have a trace of the
    record in the internal code.
    
    This commit fixes the issue by removing the record from Model.records
    when soft-deleted, so that Model.records and Model.get() doesn't
    return this record. The onDelete() hooks will returned the soft-
    deleted like before because the deleted records are stored in the
    data queue of the hooks.
    
    This solution also solves another problem: if a record is deleted
    and there's an immediate side-effect computed field to insert this
    record, the previous implementation would hard-delete the record
    at the end which is wrong: the record should be deleted but the
    insert part should apply, thus the resulting record is a fresh one
    with same identity but only partial data from the insert. All other
    field values must be lost from the deletion. The changes in this
    commit also fixes this issue.
    
    Commit also fixes issue for `.exists()` that should be `false` for
    soft-deleted records.
    
    Task-4860196
    
    X-original-commit: e38325505f322f8722e540804dc1631121517646

--- addons/mail/static/src/model/store_internal.js
+++ addons/mail/static/src/model/store_internal.js
@@ -5,7 +5,7 @@ import { markup, toRaw } from "@odoo/owl";
 import { RecordInternal } from "./record_internal";
 import { deserializeDate, deserializeDateTime } from "@web/core/l10n/dates";
 import { htmlEscape } from "@web/core/utils/html";
-import { IS_DELETING_SYM, isCommand, isMany } from "./misc";
+import { IS_DELETED_SYM, IS_DELETING_SYM, isCommand, isMany } from "./misc";
 
 const Markup = markup().constructor;
 
@@ -123,6 +123,8 @@ export class StoreInternal extends RecordInternal {
                 /** @type {import("./record").Record} */
                 const [record] = params;
                 record._[IS_DELETING_SYM] = true;
+                record._[IS_DELETED_SYM] = true;
+                delete record.Model.records[record.localId];
                 if (!this.RHD_QUEUE.has(record)) {
                     this.RHD_QUEUE.set(record, true);
                 }
