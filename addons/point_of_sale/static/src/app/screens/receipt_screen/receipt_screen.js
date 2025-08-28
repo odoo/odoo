b4b44259253af53e983f599911ed9010c4e71a3b
Hadi El Yakhni <hael@odoo.com>
2025-08-28 13:06:53 +0000

    [FIX] point_of_sale: send order to PD also when skipping receipt screen
    Steps to reproduce:
    -------------------
    - Enable "Automatic Receipt Printing"
    - Make a PoS order, with "Invoiced" checked, and validate it
    
    -> Observe that the command is not sent to the preparation display.
    
    Reason:
    -------
    When skipping the receipt screen, we don't call
    `checkPreparationStateAndSentOrderInPreparation`, which then doesn't
    send the new command to the preparation display.
    
    The fix:
    --------
    Backporting 26425ab712a3269beec98722 but without any refactoring.
    
    opw-5000406
    
    closes odoo/odoo#225411
    
    X-original-commit: baa97d8bba94fc04039e6a9d89aca2352ff3f50b
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: Hadi El Yakhni (hael) <hael@odoo.com>

--- addons/point_of_sale/static/src/app/screens/receipt_screen/receipt_screen.js
+++ addons/point_of_sale/static/src/app/screens/receipt_screen/receipt_screen.js
@@ -2,7 +2,7 @@ import { _t } from "@web/core/l10n/translation";
 import { useErrorHandlers, useTrackedAsync } from "@point_of_sale/app/hooks/hooks";
 import { registry } from "@web/core/registry";
 import { OrderReceipt } from "@point_of_sale/app/screens/receipt_screen/receipt/order_receipt";
-import { useState, Component, onMounted } from "@odoo/owl";
+import { useState, Component } from "@odoo/owl";
 import { usePos } from "@point_of_sale/app/hooks/pos_hook";
 import { useService } from "@web/core/utils/hooks";
 import { ConfirmationDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
@@ -30,15 +30,6 @@ export class ReceiptScreen extends Component {
         this.sendReceipt = useTrackedAsync(this._sendReceiptToCustomer.bind(this));
         this.doFullPrint = useTrackedAsync(() => this.pos.printReceipt());
         this.doBasicPrint = useTrackedAsync(() => this.pos.printReceipt({ basic: true }));
-        onMounted(() => {
-            const order = this.pos.getOrder();
-
-            if (!this.pos.config.module_pos_restaurant) {
-                this.pos.checkPreparationStateAndSentOrderInPreparation(order, false, {
-                    orderDone: true,
-                });
-            }
-        });
     }
     actionSendReceiptOnEmail() {
         this.sendReceipt.call({
