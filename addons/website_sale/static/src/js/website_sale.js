odoo/odoo

commit 7e5d3852cc206495bc62dc20790071e681508382

[IMP] *: improve product page's component design
*: website_sale, website_sale_collect, website_sale_comparison,
website_sale_stock, website_sale_wishlist, website_sale_stock_wishlist,
portal_rating.

This commit modernizes the design on the components of the product page.
(Typography, variants selectors, buttons, ratings, accordions, terms
and conditions...)

Other key changes:
- Added support for three CTA wrapper templates: default, boxed, and
large.
- Prevents customers from selecting impossible combinations and informs
them via tooltips, rather than allowing the selection and displaying an
error message afterward.
- Added option to show/hide separators in the sidebar.
- Made the search bar on the product page independently hideable from
the shop page (now hidden by default).
- Updated product details layout: now displayed in two columns for
`none` and `100_pc` image sizes, with a sticky CTA section.
- Displayed selected attribute names for color and image display types.
- Improved design out-of-stock and low-stock messages.

task-4527182

closes odoo/odoo#201019

Related: odoo/enterprise#81065
Related: odoo/upgrade#7512
Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>
Co-authored-by: "Lionel Piraux (lipi)" <lipi@odoo.com>

addons/website_sale/static/src/js/website_sale.js
@@ -1,10 +1,11 @@
 import { hasTouch, isBrowserFirefox } from "@web/core/browser/feature_detection";
+import { registry } from "@web/core/registry";
 import { utils as uiUtils } from "@web/core/ui/ui_service";
 import publicWidget from "@web/legacy/js/public/public_widget";
+import { Interaction } from "@web/public/interaction";
 import "@website/libs/zoomodoo/zoomodoo";
 import { ProductImageViewer } from "@website_sale/js/components/website_sale_image_viewer";
 import VariantMixin from "@website_sale/js/sale_variant_mixin";
-
 
 export const WebsiteSale = publicWidget.Widget.extend(VariantMixin, {
     selector: '.oe_website_sale',
@@ -161,7 +162,12 @@ export const WebsiteSale = publicWidget.Widget.extend(VariantMixin, {
      */
     _changeAttribute: function (valueSelectors) {
         valueSelectors.forEach((selector) => {
-            $(selector).removeClass("active").filter(":has(input:checked)").addClass("active");
+            $(selector)
+                .removeClass("active")
+                .filter(":has(input:checked)")
+                .addClass("active")
+                .find('input')
+                .trigger("change");
         });
     },
     /**
@@ -278,7 +284,7 @@ export const WebsiteSale = publicWidget.Widget.extend(VariantMixin, {
      * @override
      * @private
      */
-    _updateProductImage: function ($productContainer, displayImage, productId, productTemplateId, newImages, isCombinationPossible) {
+    _updateProductImage: function ($productContainer, displayImage, productId, productTemplateId, newImages) {
         let $images = $productContainer.find(this._getProductImageContainerSelector());
         // When using the web editor, don't reload this or the images won't
         // be able to be edited depending on if this is done loading before
@@ -300,7 +306,6 @@ export const WebsiteSale = publicWidget.Widget.extend(VariantMixin, {
             // fix issue with carousel height
             this.trigger_up('widgets_start_request', {$target: $images});
         }
-        $images.toggleClass('css_not_available', !isCombinationPossible);
     },
     /**
      * @private
@@ -342,7 +347,7 @@ export const WebsiteSale = publicWidget.Widget.extend(VariantMixin, {
         const max = parseFloat(input.dataset.max || Infinity);
         const previousQty = parseFloat(input.value || 0, 10);
         const quantity = (
-            ev.currentTarget.querySelector('i').classList.contains('fa-minus') ? -1 : 1
+            ev.currentTarget.querySelector('i').classList.contains('oi-minus') ? -1 : 1
         ) + previousQty;
         const newQty = quantity > min ? (quantity < max ? quantity : max) : min;
 
@@ -701,6 +706,52 @@ publicWidget.registry.WebsiteSaleAccordionProduct = publicWidget.Widget.extend({
         this.target.classList.remove('o_accordion_not_initialized');
     },
 });
+
+export class WebsiteSaleProductStickyCol extends Interaction {
+    static selector = ".oe_website_sale";
+
+    dynamicContent = {
+        ".o_wsale_product_sticky_col": {
+            "t-att-style": () => ({
+                "opacity": "1",
+                "top": `${this.position || 16}px`,
+            }),
+        }
+    };
+
+    setup() {
+        this.position = 16;
+    }
+
+    start() {
+        this._adaptToHeaderChange();
+        this.registerCleanup(this.services.website_menus.registerCallback(this._adaptToHeaderChange.bind(this)));
+    }
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * @private
+     */
+
+    _adaptToHeaderChange() {
+        let position = 16; // Add 1rem equivalent in px to provide a visual gap by default
+
+        for (const el of this.el.ownerDocument.querySelectorAll(".o_top_fixed_element")) {
+            position += el.offsetHeight;
+        }
+
+        if (this.position !== position) {
+            this.position = position;
+            this.updateContent();
+        }
+    }
+}
+registry
+    .category("public.interactions")
+    .add("website.website_sale_product_sticky_col", WebsiteSaleProductStickyCol);
 
 export default {
     WebsiteSale: publicWidget.registry.WebsiteSale,

addons/portal_rating/static/src/scss/portal_rating.scss
@@ -1,7 +1,14 @@
 /* static stars */
+// portal_rating
 $o-w-rating-star-color: #FACC2E;
-.o_website_rating_static{
-    color: $o-w-rating-star-color;
+
+.o_website_rating_static {
+    color: var(--WebsiteRating__star-color,  #{$o-w-rating-star-color});
+}
+
+// website_sale
+#product_details {
+    --WebsiteRating__star-color: currentColor;
 }
 
 .o_website_rating_card_container {

addons/website_sale/const.py
@@ -140,7 +140,7 @@ PRODUCT_PAGE_STYLE_MAPPING = {
             'disable': [
                 'website_sale.carousel_product_indicators_bottom',  # Layout/Thumbnails
                 'website_sale_comparison.product_add_to_compare',  # Comparison
-                'website_sale.product_custom_text',  # Terms and Conditions
+                'website_sale.product_terms_and_conditions',  # Terms and Conditions
             ],
         },
         'website_fields': {},  # Default
@@ -156,7 +156,7 @@ PRODUCT_PAGE_STYLE_MAPPING = {
                 'website_sale.product_picture_magnify_hover',
                 'website_sale.product_picture_magnify_both',
                 'website_sale_comparison.product_add_to_compare',  # Comparison
-                'website_sale.product_custom_text',  # Terms and Conditions
+                'website_sale.product_terms_and_conditions',  # Terms and Conditions
             ],
         },
         'website_fields': {
@@ -176,7 +176,7 @@ PRODUCT_PAGE_STYLE_MAPPING = {
                 'website_sale.products_carousel_16x9',
                 'website_sale.products_carousel_21x9',
                 'website_sale_comparison.product_add_to_compare',  # Comparison
-                'website_sale.product_custom_text',  # Terms and Conditions
+                'website_sale.product_terms_and_conditions',  # Terms and Conditions
             ],
         },
         'website_fields': {
@@ -194,7 +194,7 @@ PRODUCT_PAGE_STYLE_MAPPING = {
                 'website_sale.product_picture_magnify_hover',
                 'website_sale.product_picture_magnify_both',
                 'website_sale_comparison.product_add_to_compare',  # Comparison
-                'website_sale.product_custom_text',  # Terms and Conditions
+                'website_sale.product_terms_and_conditions',  # Terms and Conditions
             ],
         },
         'website_fields': {

addons/website_sale/static/src/js/product/product.xml
@@ -51,7 +51,7 @@
 
     <t t-name="website_sale.strikethrough_product_price">
         <h5
-            class="oe_striked_price text-muted small"
+            class="oe_striked_price text-muted small text-decoration-line-through text-nowrap"
             t-if="this.props.strikethrough_price"
             t-out="formattedStrikethroughPrice"
         />

addons/website_sale/static/src/js/sale_variant_mixin.js
@@ -227,7 +227,10 @@ var VariantMixin = {
         $parent
             .find('option, input, label, .o_variant_pills')
             .removeClass('css_not_available')
-            .attr('title', function () { return $(this).data('value_name') || ''; })
+            .removeAttr('disabled')
+            .filter('option, input')
+            .closest('li')
+            .removeAttr('title')
             .data('excluded-by', '');
 
         // exclusion rules: array of ptav
@@ -326,15 +329,16 @@ var VariantMixin = {
     _disableInput: function ($parent, attributeValueId, excludedBy, attributeNames, productName) {
         var $input = $parent
             .find('option[value=' + attributeValueId + '], input[value=' + attributeValueId + ']');
-        $input.addClass('css_not_available');
+        $input.addClass('css_not_available').prop('disabled', true);
         $input.closest('label').addClass('css_not_available');
         $input.closest('.o_variant_pills').addClass('css_not_available');
 
+        const $li = $input.closest('li');
+
         if (excludedBy && attributeNames) {
-            var $target = $input.is('option') ? $input : $input.closest('label').add($input);
             var excludedByData = [];
-            if ($target.data('excluded-by')) {
-                excludedByData = JSON.parse($target.data('excluded-by'));
+            if ($li.data('excluded-by')) {
+                excludedByData = $li.data('excluded-by');
             }
 
             var excludedByName = attributeNames[excludedBy];
@@ -343,8 +347,8 @@ var VariantMixin = {
             }
             excludedByData.push(excludedByName);
 
-            $target.attr('title', _t('Not available with %s', excludedByData.join(', ')));
-            $target.data('excluded-by', JSON.stringify(excludedByData));
+            $li.attr('title', _t('Not available with %s', excludedByData.join(', ')));
+            $li.data('excluded-by', excludedByData);
         }
     },
     /**
@@ -433,7 +437,6 @@ var VariantMixin = {
                 combination.product_id,
                 combination.product_template_id,
                 combination.carousel,
-                isCombinationPossible
             );
             $parent
                 .find('.o_product_tags')
@@ -545,11 +548,16 @@ var VariantMixin = {
      * @param {MouseEvent} ev
      */
     _onChangeColorAttribute: function (ev) {
-        var $parent = $(ev.target).closest('.js_product');
+        let $eventTarget = $(ev.target);
+        var $parent = $eventTarget.closest('.js_product');
         $parent.find('.css_attribute_color')
             .removeClass("active")
             .filter(':has(input:checked)')
             .addClass("active");
+        let $attrValueEl = $eventTarget.closest('.variant_attribute').find('.attribute_value')[0];
+        if ($attrValueEl) {
+            $attrValueEl.innerText = $eventTarget.data('value_name');
+        }
     },
 
     _onChangePillsAttribute: function (ev) {
@@ -557,9 +565,9 @@ var VariantMixin = {
         radio.click();  // Trigger onChangeVariant.
         var $parent = $(ev.target).closest('.js_product');
         $parent.find('.o_variant_pills')
-            .removeClass("active")
+            .removeClass("active border-primary text-primary-emphasis bg-primary-subtle")
             .filter(':has(input:checked)')
-            .addClass("active");
+            .addClass("active border-primary text-primary-emphasis bg-primary-subtle");
     },
 
     /**

addons/website_sale/static/src/js/website_sale.editor.js
@@ -835,7 +835,7 @@ options.registry.WebsiteSaleProductPage = options.Class.extend({
         const isGrid = !!this.productDetailMain.querySelector('#o-grid-product');
         switch (widgetName) {
             case 'o_wsale_thumbnail_pos':
-                return Boolean(this.productPageCarousel) && hasImages;
+                return Boolean(this.productPageCarousel) && hasImages && multipleImages;
             case 'o_wsale_grid_spacing':
                 return isGrid && multipleImages;
             case 'o_wsale_grid_columns':

addons/website_sale/static/src/scss/product_configurator.scss
@@ -1,31 +1,41 @@
 .css_attribute_color {
+    --o-wsale-css-attribute-color__height: 2rem;
+    --o-wsale-css-attribute-color__border-width: .25rem;
+
     position: relative;
     display: inline-block;
-    border: 5px solid $input-border-color;
+    height: calc(var(--o-wsale-css-attribute-color__height) - var(--o-wsale-css-attribute-color__border-width));
+    width: calc(var(--o-wsale-css-attribute-color__height) - var(--o-wsale-css-attribute-color__border-width));
+    border: var(--o-wsale-css-attribute-color__border-width) solid var(--body-bg);
     border-radius: 50%;
-    text-align: center;
     transition: $input-transition;
-
-    @include o-field-pointer();
-
-    &:before {
-        content: "";
-        display: block;
-        @include o-position-absolute(-3px, -3px, -3px, -3px);
-        border: 4px solid white;
-        border-radius: 50%;
-        box-shadow: inset 0 0 3px rgba(black, 0.3);
-    }
+    outline: $border-width solid $input-border-color;
+    box-shadow: inset 0 0 0 $border-width $border-color;
+    box-sizing: content-box;
 
     input {
-        margin: 8px;
-        height: 13px;
-        width: 13px;
         opacity: 0;
     }
 
+    &.active, &:hover {
+        border-color: var(--body-bg);
+        outline-color: map-get($theme-colors, 'primary');
+    }
+
+    &:has(input:focus-visible)::before {
+        @include o-position-absolute($top: 50%, $left: 50%);
+
+        content:" ";
+        width: 160%;
+        height: 160%;
+        border-radius: 100%;
+        z-index: -1;
+        background-color: $focus-ring-color;
+        transform: translate(-50%, -50%);
+    }
+
     &.active {
-        border: 5px solid map-get($theme-colors, 'primary');
+        outline-color: map-get($theme-colors, 'primary');
     }
 
     &.custom_value {
@@ -37,52 +47,49 @@
     }
 }
 
-.css_not_available_msg {
-    display: none;
-}
-
-.css_not_available.js_product {
-    .css_quantity {
-        display: none !important;
-    }
-
-    .css_not_available_msg {
-        display: block;
-    }
-
-    .availability_messages {
-        display: none;
-    }
-
-    .oe_price,
-    .oe_default_price {
-        display: none;
-    }
-}
-
 .css_quantity {
     width: initial; // We don't want the quantity form to be full-width
-
-    .btn, input {
-        border-color: $input-border-color;
-    }
+    border-radius: $btn-border-radius;
 
     input {
         // Needs !important because themes customize btns' padding direclty
         // rather than change '$input-btn-padding-X' (shared with inputs).
         height: auto !important;
-        max-width: 5ch;
+        max-width: 4ch;
     }
-}
 
-option.css_not_available {
-    color: #ccc;
+    &.input-group-lg {
+        &:not(.css_quantity_50) {
+            @include media-breakpoint-up(lg) {
+                width: 100%;
+            }
+
+            @include media-breakpoint-up(xl) {
+                width: auto;
+            }
+
+            input {
+                @include media-breakpoint-up(lg)  {
+                    max-width: 100%;
+                }
+
+                @include media-breakpoint-up(xl)  {
+                    max-width: 4ch;
+                }
+            }
+        }
+
+        .btn {
+            padding-top: $btn-padding-y-lg;
+            padding-bottom: $btn-padding-y-lg;
+        }
+    }
 }
 
 $-arrow-url: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='175' height='100' fill='#000'><polygon points='0,0 100,0 50,50'/></svg>");
 
 select.form-select.css_attribute_select {
-    max-width: 400px;
+    max-width: 26rem;
 
     &:not([multiple]):where(:not([size]), [size="1"]) {
         background-image: str-replace($-arrow-url, "#000", str-replace(#{$input-color}, "#", "%23"));
@@ -107,124 +114,33 @@ select.form-select.css_attribute_select {
     }
 }
 
-label, .o_variant_pills {
-    &.css_not_available {
-        opacity: 0.6;
+.variant_attribute {
+    .o_variant_pills {
+        border: $border-width solid $border-color;
+
+        &:has(input:focus-visible) {
+            box-shadow: $btn-focus-box-shadow;
+        }
+    }
+
+    label, .o_variant_pills {
+        &.css_not_available {
+            opacity: 0.6;
+            pointer-events: none;
+        }
     }
 }
 
 label.css_attribute_color.css_not_available {
-    opacity: 1;
-
     &:after {
         content: "";
-        @include o-position-absolute(-5px, -5px, -5px, -5px);
-        border: 2px solid map-get($theme-colors, 'danger');
-        border-radius: 50%;
-        background: str-replace(url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='39' height='39'><line y2='0' x2='39' y1='39' x1='0' style='stroke:#{map-get($theme-colors, 'danger')};stroke-width:2'/><line y2='1' x2='40' y1='40' x1='1' style='stroke:rgb(255,255,255);stroke-width:1'/></svg>"), "#", "%23") ;
-        background-position: center;
-        background-repeat: no-repeat;
-    }
-}
-
-.variant_attribute {
-    padding-bottom: 1rem;
-
-    .attribute_name {
-        display: flex;
-        align-items: center;
-        @include font-size(0.9rem);
-        text-transform: uppercase;
-        padding-bottom: 0.5rem;
-
-        &:after {
-            content: '';
-            margin-left: $spacer;
-            flex-grow: 1;
-            border-bottom: 1px solid $border-color;
-        }
-    }
-
-    .radio_input_value {
-        font-weight: 400;
-
-        &:not(.o_variant_pills_input_value) {
-            margin-right: $spacer;
-
-            &, > span {
-                vertical-align: middle;
-            }
-        }
-        &.o_variant_pills_input_value {
-            .badge {
-                color: map-get($grays, '600');
-                background: white;
-                border: 1px solid map-get($theme-colors, 'primary');
-
-                &, > span {
-                    vertical-align: middle;
-                }
-
-                .sign_badge_price_extra {
-                    @include font-size(1.1rem);
-                }
-            }
-        }
-    }
-
-    .variant_custom_value {
-        margin-bottom: 0.7rem;
-
-        &.custom_value_radio {
-            display: inline-block;
-        }
-    }
-
-    select {
-        margin-bottom: 0.5rem;
-    }
-
-    ul.list-inline {
-        margin-left: 0;
-    }
-
-    .o_variant_pills {
-        padding: $spacer/2 $spacer;
-        margin-right: 0.2rem;
-        border: none;
-        cursor: default !important;
-
-        &.btn.active {
-            background-color: map-get($theme-colors, 'primary');
-        }
-        &:not(.active) {
-            color: map-get($grays, '600');
-            background-color: map-get($grays, '200');
-        }
-
-        input {
-            -moz-appearance: none;
-            -webkit-appearance: none;
-            appearance: none;
-            opacity: 0;
-        }
-    }
-
-    .radio_input_value, select, label {
-        .badge {
-            margin-left: 3px;
-            padding-left: 3px;
-        }
-
-        .sign_badge_price_extra {
-            display: inline-block;
-            width: 1rem;
-            height: 1rem;
-            color: map-get($theme-colors, 'primary');
-            background: white;
-            line-height: 1rem;
-            border-radius: 50%;
-        }
+        @include o-position-absolute($top: 50%, $left: 50%);
+        width: 2px;
+        height: 110%;
+        background-color: var(--body-bg);
+        rotate: 45deg;
+        transform: translate(-50%, -50%);
+        transform-origin: top;
     }
 }
 
@@ -277,9 +193,4 @@ label.css_attribute_color.css_not_available {
             width: 60px;
         }
     }
-}
-
-.oe_striked_price {
-    text-decoration: line-through;
-    white-space: nowrap;
 }

addons/website_sale/static/src/scss/website_sale.scss
@@ -588,12 +588,6 @@ $input-border-color: $gray-400;
         font-weight: $font-weight-bold;
     }
 
-    // Extra price badge
-    .text-bg-light .variant_price_extra.text-muted {
-        // Needed to be visible on a dark <body> background.
-        color: adjust-color-to-background($text-muted, $light, mute-color($color-contrast-light), mute-color($color-contrast-dark)) !important;
-    }
-
     input.js_quantity {
         min-width: 48px;
         text-align: center;
@@ -1196,12 +1190,24 @@ a.no-decoration {
     }
 }
 
+#o_wsale_cta_wrapper {
+    &.o_wsale_cta_wrapper_boxed {
+        max-width: 26rem;
+    }
+
+    &.o_wsale_cta_wrapper_large .css_quantity {
+        border-radius: $btn-border-radius-lg;
+    }
+
+    &:not(.o_wsale_cta_wrapper_boxed) #product_option_block:has(button:only-child) {
+        @include media-breakpoint-up(lg) {
+            flex-grow: 0 !important;
+        }
+    }
+}
+
 #o-carousel-product {
     transition: top 200ms;
-
-    &.css_not_available {
-        opacity: 0.2;
-    }
 
     .carousel-inner {
         img {

commit a17d5459df2a9b14976066e370ca989967c0506a

[FIX] website_sale{,_wishlist}: locate form relative to button when outside standard hierarchy
## Version
saas-18.4+

## Steps to reproduce
- Open the shop;
- Select any product;
- Open the Editor:
  - Select the product's main image;
  - Change the image width to either `100 percent` or `None`, then save;
- Click on `Add to cart`.

## Issue
Commit 7e5d3852cc206495bc62dc20790071e681508382 introduced a UI update that reorganizes the layout of the product configurator, placing the `Add to cart` button next to the form rather than below it.

Although the button remains inside the form in the original template, using the Editor to adjust the layout can result in the button being saved outside the `<form>` element in the final DOM.

This breaks the logic that relies on `closest('form')` to locate the surrounding form, since the button is no longer a descendant of the form element.

## Solution
Find the first product form relative to the button, since it may be a sibling rather than an ancestor in the DOM.

opw-4942986

closes odoo/odoo#224460

X-original-commit: b84158ba0f6f66cc7a6f581a5a8da24b69b2573d
Related: odoo/enterprise#93306
Signed-off-by: Valeriya Chuprina (vchu) <vchu@odoo.com>
Signed-off-by: Pierre Lamotte (pila) <pila@odoo.com>

addons/website_sale/static/src/js/website_sale.js
@@ -4,6 +4,7 @@ import publicWidget from "@web/legacy/js/public/public_widget";
 import "@website/libs/zoomodoo/zoomodoo";
 import { ProductImageViewer } from "@website_sale/js/components/website_sale_image_viewer";
 import VariantMixin from "@website_sale/js/sale_variant_mixin";
+import wSaleUtils from '@website_sale/js/website_sale_utils';
 
 export const WebsiteSale = publicWidget.Widget.extend(VariantMixin, {
     selector: '.oe_website_sale',
@@ -290,7 +291,8 @@ export const WebsiteSale = publicWidget.Widget.extend(VariantMixin, {
     async _onClickAdd(ev) {
         ev.preventDefault();
         var def = () => {
-            this._updateRootProduct((ev.currentTarget).closest('form'));
+            const form = wSaleUtils.getClosestProductForm(ev.currentTarget);
+            this._updateRootProduct(form);
             const isBuyNow = ev.currentTarget.classList.contains('o_we_buy_now');
             const isConfigured = ev.currentTarget.parentElement.id === 'add_to_cart_wrap';
             const showQuantity = Boolean(ev.currentTarget.dataset.showQuantity);
@@ -371,7 +373,7 @@ export const WebsiteSale = publicWidget.Widget.extend(VariantMixin, {
      * @returns {void}
      */
     _onChangeAddQuantity: function (ev) {
-        const $parent = $(ev.currentTarget).closest('form');
+        const $parent = $(wSaleUtils.getClosestProductForm($(ev.currentTarget)[0]));
         if ($parent.length > 0) {
             this.triggerVariantChange($parent);
         }
@@ -387,7 +389,7 @@ export const WebsiteSale = publicWidget.Widget.extend(VariantMixin, {
             if (productGrid) {
                 productGrid.classList.add("opacity-50");
             }
-            const form = ev.currentTarget.closest('form');
+            const form = wSaleUtils.getClosestProductForm(ev.currentTarget);
             const filters = form.querySelectorAll('input:checked, select');
             const attributeValues = new Map();
             const tags = new Set();
@@ -534,7 +536,8 @@ export const WebsiteSale = publicWidget.Widget.extend(VariantMixin, {
         const productId = parseInt(
             form.querySelector('input[type="hidden"][name="product_id"]')?.value
         );
-        const quantity = parseFloat(form.querySelector('input[name="add_qty"]')?.value);
+        const productEl = form.closest('.js_product') ?? form;
+        const quantity = parseFloat(productEl.querySelector('input[name="add_qty"]')?.value);
         const uomId = this._getUoMId(form);
         const isCombo = form.querySelector(
             'input[type="hidden"][name="product_type"]'

addons/website_sale/static/src/js/website_sale_utils.js
@@ -49,8 +49,19 @@ function animateClone($cart, $elem, offsetTop, offsetLeft) {
 }
 
 /**
- * Updates both navbar cart.
+ * Returns the closest product form to a given element if exists.
+ * Required for product pages with full-width or no images where the "Add to cart" button can be 
+ * outside of the form.
  *
+ * @param { HTMLElement } element - Reference to an HTML element in the DOM.
+ * @returns { HTMLFormElement|undefined }
+ */
+function getClosestProductForm(element){
+    return element.closest('form') ?? element.closest('.js_product')?.querySelector('form');
+}
+
+/**
+ * Updates both navbar cart
  * @param {Object} data
  * @return {void}
  */
@@ -157,6 +168,7 @@ function getSelectedAttributeValues(container) {
 
 export default {
     animateClone: animateClone,
+    getClosestProductForm: getClosestProductForm,
     updateCartNavBar: updateCartNavBar,
     showWarning: showWarning,
     getSelectedAttributeValues: getSelectedAttributeValues,

addons/website_sale_comparison/static/src/interactions/product_comparison.js
@@ -34,7 +34,7 @@ export class ProductComparison extends Interaction {
 
         const el = ev.currentTarget;
         let productId = parseInt(el.dataset.productProductId);
-        const form = el.closest('form');
+        const form = wSaleUtils.getClosestProductForm(el);
         if (!productId) {
             productId = await this.waitFor(rpc('/sale/create_product_variant', {
                 product_template_id: parseInt(el.dataset.productTemplateId),

addons/website_sale_wishlist/static/src/interactions/add_product_to_wishlist_button.js
@@ -18,7 +18,7 @@ export class AddProductToWishlistButton extends Interaction {
     async addProduct(ev) {
         const el = ev.currentTarget;
         let productId = parseInt(el.dataset.productProductId);
-        const form = el.closest('form');
+        const form = wSaleUtils.getClosestProductForm(el);
         if (!productId) {
             productId = await this.waitFor(rpc('/sale/create_product_variant', {
                 product_template_id: parseInt(el.dataset.productTemplateId),