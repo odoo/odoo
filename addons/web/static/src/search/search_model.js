93a0961a6a91edfd0a5e506278a0d7da44c033a4
Achraf (abz) <abz@odoo.com>
2025-08-28 06:37:50 +0000

    [FIX] web: Allow users to use '&' as value in condition tree type
    Example of steps:
    - Open any domain selector with an archive checkbox (via custom filter for example)
    - Try to add char `&` in value input
    - focus out or try to apply the filter
    - Traceback
    
    ```
    UncaughtPromiseError > OwlError
    Uncaught Promise > The following error occurred in onWillUpdateProps: "Cannot read properties of undefined (reading 'filter')"
    ```
    
    The problem comes from the fact that we expect to have multiple nodes in
    our domain if we have an `&` as a value.
    
    However, there is a difference between using `&` in a text search for
    example (type `condition`), and using `&` as an AND between two nodes
    (type `connector`).
    
    The solution is to restrict the condition so that it not only checks
    that the value is equal to `&`, but also checks that the tree type is
    indeed a connector and not a condition.
    
    opw-5015281
    
    X-original-commit: 7deb0e02ae12b45d560d278edfbd1144c239d717

--- addons/web/static/src/search/search_model.js
+++ addons/web/static/src/search/search_model.js
@@ -731,7 +731,12 @@ export class SearchModel extends EventBus {
         const getFieldDef = await this.makeGetFieldDef(this.resModel, constructTree(domain));
         const tree = treeFromDomain(domain, { distributeNot: !this.isDebugMode, getFieldDef });
         const trees =
-            !tree.negate && tree.value === "&" && tree.children.length > 0 ? tree.children : [tree];
+            !tree.negate &&
+            tree.type === "connector" &&
+            tree.value === "&" &&
+            tree.children.length > 0
+                ? tree.children
+                : [tree];
         const promises = trees.map(async (tree) => {
             const [description, tooltip] = await Promise.all([
                 this.getDomainTreeDescription(this.resModel, tree),
