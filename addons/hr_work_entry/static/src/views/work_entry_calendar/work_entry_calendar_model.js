d658d379fc250fcaeaa0435f65886b58dfaad97b
Yassin Abdelkarim <yabd@odoo.com>
2025-08-29 06:35:41 +0200

    [FIX] hr_work_entry: fix work entries replace in calendar view
    The code now replaces the selected cells with a new entry with the
    selected type, where the duration of the new type is the sum of the
    duration(s) of the previously existing entries in the corrresponding
    cell. In case the cell is empty, the new entry is created according to
    the expected hours of work for that day for that employee (depending
    on the work entry source).
    
    task-5043872
    
    closes odoo/odoo#224736
    
    Related: odoo/enterprise#93459
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr_work_entry/static/src/views/work_entry_calendar/work_entry_calendar_model.js
+++ addons/hr_work_entry/static/src/views/work_entry_calendar/work_entry_calendar_model.js
@@ -27,19 +27,39 @@ export class WorkEntryCalendarModel extends CalendarModel {
         }
         const new_records = [];
         const values = await multiCreateData.record.getChanges();
-
+        const quickreplace = (values.duration < 0);
+        const newly_generated_entries = [];
         for (const date of dates) {
             const rawRecord = this.buildRawRecord({ start: date });
+            if (quickreplace) {
+                const selected_date_records = records.filter((r) => r.date === rawRecord.date);
+                const existing_duration = selected_date_records.reduce((acc, r) => acc + r.duration, 0);
+                if (existing_duration > 0)
+                    values.duration = existing_duration;
+                else {
+                    const generated_work_entry = await this.orm.call(
+                        "hr.employee",
+                        "generate_work_entries",
+                        [values.employee_id, date, date, true]
+                    );
+                    if (generated_work_entry.length > 0)
+                        newly_generated_entries.push(generated_work_entry[0]);
+                    continue
+                }
+            }
             new_records.push({
                 ...rawRecord,
                 ...values,
             });
         }
+        await this.orm.write("hr.work.entry", newly_generated_entries, {
+            work_entry_type_id: values.work_entry_type_id
+        });
         const created = await this.orm.create(this.meta.resModel, new_records, {
             context: this.meta.context,
         });
         if (records.length && created) {
-            await this.orm.unlink(this.meta.resModel, records);
+            await this.orm.unlink(this.meta.resModel, records.map((r) => r.id));
         }
         return this.load();
     }
