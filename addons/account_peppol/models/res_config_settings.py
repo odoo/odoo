da1bfb4e1634da5b935c49b8b5ce49b8db35f3ee
Laurent Smet (LAS) <las@odoo.com>
2025-08-29 13:16:51 +0000

    [IMP] account_peppol: Branch Company Peppol Connection
    This commit implements the functionality to allow all branch company to
    use Peppol.
    
    With this commit, the user can register a branch company in the peppol
    network in two ways:
    - By setting the same EAS/Endpoint than the one set on the parent company,
    the branch will be registered as a sender for the parent company.
    - By setting another EAS/Endpoint than the one set on the parent company,
    the branch will do a new registration.
    
    task-4852830
    
    closes odoo/odoo#243467
    
    X-original-commit: 1aeae63bdaed444a9ea96ec6a0bda7602a7a907c
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- addons/account_peppol/models/res_config_settings.py
+++ addons/account_peppol/models/res_config_settings.py
@@ -9,11 +9,7 @@ from odoo.addons.account_peppol.tools.demo_utils import handle_demo
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    account_peppol_edi_user = fields.Many2one(
-        comodel_name='account_edi_proxy_client.user',
-        string='EDI user',
-        compute='_compute_account_peppol_edi_user',
-    )
+    account_peppol_edi_user = fields.Many2one(related='company_id.account_peppol_edi_user')
     account_peppol_edi_mode = fields.Selection(related='account_peppol_edi_user.edi_mode')
     account_peppol_contact_email = fields.Char(related='company_id.account_peppol_contact_email', readonly=False)
     account_peppol_eas = fields.Selection(related='company_id.peppol_eas', readonly=False)
@@ -24,22 +20,41 @@ class ResConfigSettings(models.TransientModel):
     account_peppol_proxy_state = fields.Selection(related='company_id.account_peppol_proxy_state', readonly=False)
     account_peppol_purchase_journal_id = fields.Many2one(related='company_id.peppol_purchase_journal_id', readonly=False)
     peppol_external_provider = fields.Char(related='company_id.peppol_external_provider', readonly=False)
+    peppol_use_parent_company = fields.Boolean(compute='_compute_peppol_use_parent_company')
+    peppol_parent_company_name = fields.Char(compute='_compute_peppol_use_parent_company')
 
     # -------------------------------------------------------------------------
     # COMPUTE METHODS
     # -------------------------------------------------------------------------
 
-    @api.depends("company_id.account_edi_proxy_client_ids")
-    def _compute_account_peppol_edi_user(self):
-        for config in self:
-            config.account_peppol_edi_user = config.company_id.account_edi_proxy_client_ids.filtered(
-                lambda u: u.proxy_type == 'peppol')
+    @api.depends('company_id.peppol_parent_company_id')
+    def _compute_peppol_use_parent_company(self):
+        for setting in self:
+            setting.peppol_use_parent_company = (
+                setting.company_id != setting.company_id.peppol_parent_company_id
+                and setting.company_id.peppol_can_send
+                and setting.company_id.peppol_parent_company_id.peppol_can_send
+            )
+            if setting.peppol_use_parent_company:
+                setting.peppol_parent_company_name = setting.company_id.peppol_parent_company_id.name
+            else:
+                setting.peppol_parent_company_name = None
+
+    @api.depends('is_account_peppol_eligible', 'account_peppol_edi_user')
+    def _compute_account_peppol_mode_constraint(self):
+        mode_constraint = self.env['ir.config_parameter'].sudo().get_param('account_peppol.mode_constraint')
+        trial_param = self.env['ir.config_parameter'].sudo().get_param('saas_trial.confirm_token')
+        self.account_peppol_mode_constraint = trial_param and 'demo' or mode_constraint or 'prod'
 
     # -------------------------------------------------------------------------
     # BUSINESS ACTIONS
     # -------------------------------------------------------------------------
 
     def action_open_peppol_form(self):
+        view = self.env.ref('account_peppol.peppol_registration_form').sudo()
+        # TODO remove in master this hack to get the branches management
+        if 'use_parent_connection' not in view.arch_db:
+            view.reset_arch(mode="hard")
         registration_wizard = self.env['peppol.registration'].create({'company_id': self.company_id.id})
         registration_action = registration_wizard._action_open_peppol_form(reopen=False)
         return registration_action
@@ -61,3 +76,18 @@ class ResConfigSettings(models.TransientModel):
         """Register the existing user as a receiver."""
         self.ensure_one()
         return self.env['peppol.config.wizard'].new().button_peppol_register_sender_as_receiver()
+
+    def button_peppol_disconnect_branch_from_parent(self):
+        self.ensure_one()
+        previous_parent_company_name = self.company_id.peppol_parent_company_id.name
+        self.account_peppol_edi_user._peppol_deregister_participant()
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'title': None,
+                'type': 'success',
+                'message': _("Disconnected this branch company peppol configuration from %s.", previous_parent_company_name),
+                'next': {'type': 'ir.actions.act_window_close'},
+            }
+        }
