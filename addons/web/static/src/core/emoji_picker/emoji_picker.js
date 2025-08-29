75569b948a966c3393724924c9604fe09efbf114
akth-odoo <akth@odoo.com>
2025-08-29 10:43:33 +0000

    [FIX] web: emoji picker crash for non-English
    In some non-English translations, emoji strings include \n, which breaks JSON
    parsing in emoji_data.js. As a result, emojis.length === 0 and the empty
    state is displayed instead of the main emoji picker UI. Meanwhile, some UI logic
    still executes on missing DOM elements, which triggers errors.
    
    This commit fixes the issue by escaping translated strings in emoji_data.js
    using JSON.stringify, ensuring the emoji data is safely parsed and preventing
    the emoji picker from crashing.
    
    Task-4978824
    
    closes odoo/odoo#230249
    
    X-original-commit: 31673c2ef3db2420c1c43a3bb3016d534c7d4c19
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/web/static/src/core/emoji_picker/emoji_picker.js
+++ addons/web/static/src/core/emoji_picker/emoji_picker.js
@@ -136,12 +136,12 @@ export class EmojiPicker extends Component {
                 : this.categories[0].sortId;
         });
         onMounted(() => {
-            this.navbarResizeObserver = new ResizeObserver(() => this.adaptNavbar());
-            this.navbarResizeObserver.observe(this.navbarRef.el);
-            this.adaptNavbar();
             if (this.emojis.length === 0) {
                 return;
             }
+            this.navbarResizeObserver = new ResizeObserver(() => this.adaptNavbar());
+            this.navbarResizeObserver.observe(this.navbarRef.el);
+            this.adaptNavbar();
             this.highlightActiveCategory();
             if (this.props.storeScroll) {
                 this.gridRef.el.scrollTop = this.props.storeScroll.get();
@@ -204,7 +204,7 @@ export class EmojiPicker extends Component {
             () => [this.searchTerm]
         );
         onWillUnmount(() => {
-            this.navbarResizeObserver.disconnect();
+            this.navbarResizeObserver?.disconnect();
             if (!this.gridRef.el) {
                 return;
             }
@@ -335,6 +335,9 @@ export class EmojiPicker extends Component {
      * navigation of the emoji picker.
      */
     updateEmojiPickerRepr() {
+        if (this.emojis.length === 0) {
+            return;
+        }
         const emojiEls = Array.from(this.gridRef.el.querySelectorAll(".o-Emoji"));
         const emojiRects = emojiEls.map((el) => el.getBoundingClientRect());
         this.emojiMatrix = [];
