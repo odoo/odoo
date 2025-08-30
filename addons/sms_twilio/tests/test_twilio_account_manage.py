44e7e23538b85b8718c912661013093be1110ed4
Thibault Delavallée <tde@odoo.com>
2025-08-30 20:28:08 +0200

    [IMP] sms_twilio: improve mock tools, allowing to choose error code
    Purpose is to test support and propagation of various errors in tests.
    
    Task-5053537

--- addons/sms_twilio/tests/test_twilio_account_manage.py
+++ addons/sms_twilio/tests/test_twilio_account_manage.py
@@ -33,16 +33,16 @@ class TestSmsTwilio(MockSmsTwilio, TransactionCase):
         wizard = self.env["sms.twilio.account.manage"].create({
             'test_number': '+32455001122',
         })
-        for has_error, notif_params in zip(
-            (False, True),
+        for twilio_error, notif_params in zip(
+            (False, "wrong_number_format"),
             ({}, {
                 'message': 'sms_number_format: None',
                 'type': 'danger',
             }),
             strict=True,
         ):
-            with self.subTest(has_error=has_error):
-                with self.mock_sms_twilio_send(ok=not has_error):
+            with self.subTest(twilio_error=twilio_error):
+                with self.mock_sms_twilio_send(mock_error_type=twilio_error):
                     notif = wizard.action_send_test()
                 params = {
                     'title': "Twilio SMS",
