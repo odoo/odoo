42262511473382b7129538a02624ee96542ee798
khfa <khfa@odoo.com>
2025-09-01 01:11:17 +0400

    [IMP] l10n_sa_edi: consider 409 as successful submission
    ZATCA introduced a new response code (409) to handle duplicate invoice submissions for B2C.
    This is helpful because when the submission timesout, we are left unsure whether ZATCA
    successfuly received the invoice or not. The next time Odoo tries to send the same invoice,
    Zatca will respond with a 409 error if it was received earlier. In which case, we mark
    the invoice as successfully sent.
    
    A similar flow applies for B2B, with a response code of 208 for duplicate invoices.
    However, for B2B, ZATCA accepts the duplicate with a warning instead of an error,
    So the invoice gets marked as sent in Odoo, and no changes need to be done there.
    
    Task-id: 5027612
    X-original-commit: 182e760f64688ba2d032b15656b6bc4b76eba4df

--- addons/l10n_sa_edi/models/account_move.py
+++ addons/l10n_sa_edi/models/account_move.py
@@ -198,10 +198,10 @@ class AccountMove(models.Model):
             Save submitted invoice XML hash in case of either Rejection or Acceptance.
         """
         self.ensure_one()
-        self.journal_id.l10n_sa_latest_submission_hash = self.env['account.edi.xml.ubl_21.zatca']._l10n_sa_generate_invoice_xml_hash(
-            xml_content)
-        bootstrap_cls, title, content = ("success", _("Success: Invoice accepted by ZATCA"),
-                                         "" if (not error or not response_data) else response_data)
+        if not response_data.get("excepted"):
+            self.journal_id.l10n_sa_latest_submission_hash = self.env['account.edi.xml.ubl_21.zatca']._l10n_sa_generate_invoice_xml_hash(xml_content)
+        bootstrap_cls, title, subtitle, content = ("success", _("Success: Invoice accepted by ZATCA"), "", "" if (not error or not response_data) else response_data)
+        status_code = response_data.get('status_code')
         attachment = False
         if error:
             xml_filename = self.env['account.edi.xml.ubl_21.zatca']._export_invoice_filename(self)
@@ -216,36 +216,49 @@ class AccountMove(models.Model):
                 'mimetype': 'application/xml',
             })
             bootstrap_cls, title = ("danger", _("Error: Invoice rejected by ZATCA"))
-            error_msg = response_data['error']
-            content = Markup("""
-                <p class='mb-0 mt-1'>
-                    %s
-                </p>
-                <hr>
-                <p class='mb-0'>
-                    %s
-                </p>
-            """) % (_("Please check the details below and retry after addressing them:"), error_msg)
+            subtitle = _('Please check the details below and retry after addressing them:')
+            content = response_data['error']
         if response_data and response_data.get('validationResults', {}).get('warningMessages'):
-            status_code = response_data.get('status_code')
             bootstrap_cls, title = ("warning", _("Warning: Invoice accepted by ZATCA with warnings"))
-            content = Markup("""
-                <p class='mb-0 mt-1'>
-                    %s
-                </p>
-                <hr>
-                <p class='mb-0'>
-                    <b>%s</b>%s
-                </p>
-            """) % (_("Please check the details below:"),
-                    f"[{status_code}] " if status_code else "",
-                    Markup("<br/>").join([Markup("<b>%s</b> : %s") % (m['code'], m['message']) for m in response_data['validationResults']['warningMessages']]))
+            subtitle = _('Please check the details below:')
+            content = Markup("""<b>%(status_code)s</b>%(errors)s""") % {
+                "status_code": f"[{status_code}] " if status_code else "",
+                "errors": Markup("<br/>").join([
+                    Markup("<b>%(code)s</b> : %(message)s") % {
+                        "code": m['code'],
+                        "message": m['message'],
+                    } for m in response_data['validationResults']['warningMessages']
+                ]),
+            }
+        if response_data.get("error") and response_data.get("excepted"):
+            bootstrap_cls, title = ("warning", _("Warning: Unable to Retrieve a Response from ZATCA"))
+            subtitle = _('Please check the details below:')
+            content = response_data['error']
+        if status_code == 409:
+            bootstrap_cls, title = ("warning", _("Warning: Invoice was already successfully reported to ZATCA"))
+            subtitle = _("Please check the details below:")
+            content = Markup("""<b>%(status_code)s</b>%(errors)s""") % {
+                "status_code": f"[{status_code}] " if status_code else "",
+                "errors": Markup("<br/>").join([
+                    Markup("<b>%(code)s</b> : %(message)s") % {
+                        "code": m['code'],
+                        "message": m['message'],
+                    } for m in response_data['validationResults']['errorMessages']
+                ])
+            }
         self.message_post(body=Markup("""
                 <div role='alert' class='alert alert-%s'>
-                    <h4 class='alert-heading my-0'>%s</h4>%s
+                    <h4 class='alert-heading my-0'>%s</h4>
+                    <p class='mb-0 mt-1'>
+                        %s
+                    </p>
+                    %s
+                    <p class='mb-0'>
+                        %s
+                    </p>
                 </div>
-            """) % (bootstrap_cls, title, content),
-            attachment_ids=attachment and [attachment.id] or []
+            """) % (bootstrap_cls, title, subtitle, Markup("<hr>") if content else "", content),
+            attachment_ids=(attachment and [attachment.id]) or [],
         )
 
     def _is_l10n_sa_eligibile_invoice(self):
