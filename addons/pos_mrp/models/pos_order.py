3f12e08ab7d7347f4987baec2618c5f6530ba49b
roen-odoo <roen@odoo.com>
2025-09-01 15:17:12 +0000

    [FIX] pos_mrp: fix cost computation bom variants
    When you create a product with atleast one variant that has 2 value,
    and create a BoM for this product that has 2 lines with each line
    having one of the two values, then create a POS order with one unit
    of each variant, the cost of the first line not correctly computed.
    
    Steps to reproduce:
    -------------------
    * Create a prodcut P with one attribute A that has two values A1 and A2
    * Create a product C with no attribute and a cost of 10$
    * Create a BoM for P with two lines:
      - Line 1: product C, quantity 1, only for attribute value A1
      - Line 2: product C, quantity 2, only for attribute value A2
    * Open a PoS session
    * Add Product P with attribute value A1 to the order
    * Add Product P with attribute value A2 to the order
    * Validate the order and close the session
    * Go to the order and check the cost of each line
    > Observation: The cost are not correct, they should be 10$ and 20$
    
    Why the fix:
    ------------
    Before this fix we were not taking the
    `bom_product_template_attribute_value_ids` into account when filtering
    the stock moves to consider for the cost computation. This value
    represent the attribute values that the product must have for
    this BoM line to be considered.
    
    opw-4765234
    
    closes odoo/odoo#236442
    
    X-original-commit: b8446cb27b885c60c94efdd907bda2ec67970d50
    Signed-off-by: Stéphane Vanmeerhaeghe (stva) <stva@odoo.com>
    Signed-off-by: Robin Engels (roen) <roen@odoo.com>

--- addons/pos_mrp/models/pos_order.py
+++ addons/pos_mrp/models/pos_order.py
@@ -14,7 +14,7 @@ class PosOrderLine(models.Model):
             return super()._get_stock_moves_to_consider(stock_moves, product)
         boms, components = bom.explode(product, self.qty)
         #Get a flat list of all bom_line_ids
-        bom_line_ids = [item for x in boms for item in x[0].bom_line_ids.ids]
+        bom_line_ids = [item.id for x in boms for item in x[0].bom_line_ids if set(item.bom_product_template_attribute_value_ids.ids).issubset(product.product_template_variant_value_ids.ids)]
         ml_product_to_consider = (product.bom_ids and [comp[0].product_id.id for comp in components]) or [product.id]
         return stock_moves.filtered(lambda ml: ml.product_id.id in ml_product_to_consider and (ml.bom_line_id.id in bom_line_ids))
 
