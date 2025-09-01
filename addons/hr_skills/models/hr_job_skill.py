8a6f3a86678b015813b92f2c6c48458050b253d3
Mélanie <mepe@odoo.com>
2025-09-01 14:07:44 +0200

    [IMP] hr_skills: restrict validity check for hr job skills
    When the user add a skill on a job he can't set the certification validity range.
    With this commit, certification will have the same constraints as a regular skill.
    
    task-5038947
    
    Part-of: odoo/odoo#224616
    Related: odoo/enterprise#93157
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr_skills/models/hr_job_skill.py
+++ addons/hr_skills/models/hr_job_skill.py
@@ -1,5 +1,4 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from collections import defaultdict
 
 from odoo import fields, models
 
@@ -21,16 +20,5 @@ class HrJobSkill(models.Model):
     def _linked_field_name(self):
         return "job_id"
 
-    def _get_current_skills_by_job(self):
-        job_skill_grouped = self.grouped(lambda j_s: (j_s.job_id, j_s.skill_id))
-        result_dict = defaultdict(lambda: self.env["hr.job.skill"])
-        for (job, skill), job_skills in job_skill_grouped.items():
-            filtered_job_skills = job_skills.filtered(
-                lambda j_s: not j_s.valid_to or j_s.valid_to >= fields.Date.today(),
-            )
-            if skill.skill_type_id.is_certification and not filtered_job_skills:
-                most_recent_certification = max(job_skills, key=lambda j_s: j_s.valid_to)
-                result_dict[job.id] += most_recent_certification
-                continue
-            result_dict[job.id] += filtered_job_skills
-        return result_dict
+    def _can_edit_certification_validity_period(self):
+        return False
