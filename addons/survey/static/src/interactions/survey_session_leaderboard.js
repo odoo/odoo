a92ec479561d650823a26a74ffb7084a54c0b71d
Pierre-Yves Dufays <pydu@odoo.com>
2025-09-01 15:41:41 +0200

    [FIX] survey: fix leaderboard score display
    The way the score bars were animated was a bit confusing. We simplify the
    animation by:
    - Showing the score accumulated so far without the question
    - Animating the score bar towards the accumulated score with the question
    - Animating the numerical score on the left towards the accumulated score with
    the question while fading the numerical score increment on the bar ("+ x p")
    - (reordering participants)
    
    Task-4893763
    
    closes odoo/odoo#226431
    
    Signed-off-by: Stéphane Debauche (std) <std@odoo.com>

--- addons/survey/static/src/interactions/survey_session_leaderboard.js
+++ addons/survey/static/src/interactions/survey_session_leaderboard.js
@@ -15,79 +15,27 @@ export class SurveySessionLeaderboard extends Interaction {
             "t-on-showLeaderboard": this.showLeaderboard,
             "t-on-hideLeaderboard": this.hideLeaderboard,
         },
-        ".o_survey_session_leaderboard_bar": {
-            "t-att-style": this.getBarStyle,
-        },
         ".o_survey_session_leaderboard_bar_question": {
             "t-att-style": this.getBarQuestionStyle,
         },
     };
-
-    getBarStyle(barEl) {
-        if (this.leaderboardAnimationPhase === "prepareScores") {
-            // See also this.prepareScores
-            const currentScore = parseInt(
-                barEl.closest(".o_survey_session_leaderboard_item").dataset.currentScore
-            );
-            if (currentScore && currentScore !== 0) {
-                return {
-                    transition: "width 1s cubic-bezier(.4,0,.4,1)",
-                    width: this.BAR_MIN_WIDTH,
-                };
-            }
-        } else if (this.leaderboardAnimationPhase === "sumScores") {
-            // See also this.sumScores
-            const { baseRatio, questionRatio } = this.getBarRatios(barEl);
-            const updatedScoreRatio = 1 - questionRatio;
-            const updatedScoreWidth = `calc(calc(100% - ${this.BAR_WIDTH}) * ${
-                updatedScoreRatio * baseRatio
-            })`;
-            return {
-                transition: "width ease .5s cubic-bezier(.5,0,.66,1.11)",
-                width: updatedScoreWidth,
-                minWidth: "0px",
-            };
-        }
-        return {};
-    }
 
     getBarQuestionStyle(barEl) {
         if (this.leaderboardAnimationPhase === "showQuestionScores") {
             // See also this.showQuestionScores
             return {
                 transition: "width 1s ease-out",
-                width: `calc(calc(100% - ${this.BAR_WIDTH}) * ${barEl.dataset.widthRatio} + ${this.BAR_MIN_WIDTH})`,
-            };
-        } else if (this.leaderboardAnimationPhase === "sumScores") {
-            // See also this.sumScores
-            const { baseRatio, questionRatio } = this.getBarRatios(barEl);
-            // we keep a min fixed width of 3rem to be able to display "+ 5 p"
-            // even if the user already has 1,000,000 points
-            const questionWidth = `calc(calc(calc(100% - ${this.BAR_WIDTH}) * ${
-                questionRatio * baseRatio
-            }) + ${this.BAR_MIN_WIDTH})`;
-            return {
-                transition: "width ease .5s cubic-bezier(.5,0,.66,1.11)",
-                width: questionWidth,
+                width: `max(calc(calc(100% - ${this.BAR_RESERVED_WIDTH_REM}rem) * ${barEl.dataset.widthRatio}), ${this.BAR_QUEST_MIN_WIDTH_REM}rem)`,
             };
         }
         return {};
     }
 
-    getBarRatios(barEl) {
-        const item = barEl.closest(".o_survey_session_leaderboard_item");
-        const updatedScore = parseInt(item.dataset.updatedScore);
-        const questionScore = parseInt(item.dataset.questionScore);
-        const maxUpdatedScore = parseInt(item.dataset.maxUpdatedScore);
-        const baseRatio = maxUpdatedScore ? updatedScore / maxUpdatedScore : 1;
-        const questionRatio = questionScore / (updatedScore || 1);
-        return { baseRatio, questionRatio };
-    }
-
     setup() {
         this.fadeInOutTime = 400;
-        this.BAR_MIN_WIDTH = "3rem";
-        this.BAR_WIDTH = "24rem";
+        this.BAR_QUEST_MIN_WIDTH_REM = 3.7;
+        // Score width + margin + Bar score min width (css) + BAR_QUEST_MIN_WIDTH_REM + margin + nickname width
+        this.BAR_RESERVED_WIDTH_REM = 6.5 + 1 + 3.7 + this.BAR_QUEST_MIN_WIDTH_REM + 1 + 7.5;
         this.BAR_HEIGHT = "3.8rem";
         this.surveyAccessToken = this.el.closest(
             ".o_survey_session_manage"
@@ -153,7 +101,6 @@ export class SurveySessionLeaderboard extends Interaction {
                     });
                 fadeIn(this.el, this.fadeInOutTime, async () => {
                     if (ev.detail.isScoredQuestion) {
-                        await this.waitFor(this.prepareScores());
                         await this.waitFor(this.showQuestionScores());
                         await this.waitFor(this.sumScores());
                         await this.waitFor(this.reorderScores());
@@ -216,27 +163,6 @@ export class SurveySessionLeaderboard extends Interaction {
     }
 
     /**
-     * Takes the leaderboard prior to the current question results
-     * and reduce all scores bars to a small width (3rem).
-     * We keep the small score bars on screen for 1s.
-     *
-     * This visually prepares the display of points for the current question.
-     *
-     * @private
-     */
-    async prepareScores() {
-        let animationDone;
-        const animationPromise = new Promise(function (resolve) {
-            animationDone = resolve;
-        });
-        this.waitForTimeout(() => {
-            this.leaderboardAnimationPhase = "prepareScores";
-            this.waitForTimeout(animationDone, 1000);
-        }, 300);
-        return animationPromise;
-    }
-
-    /**
      * Now that we have summed the score for the current question to the total score
      * of the user and re-weighted the bars accordingly, we need to re-order everything
      * to match the new ranking.
@@ -280,7 +206,7 @@ export class SurveySessionLeaderboard extends Interaction {
      *   (faded out bar right next to the global score one)
      * - animate the score for the question (ex: from + 0 p to + 40 p)
      *
-     * (We keep a minimum width of 3rem to be able to display '+30 p' within the bar).
+     * (We keep a minimum width to be able to display '+30 p' within the bar, see getBarQuestionStyle).
      *
      * @private
      */
@@ -316,28 +242,9 @@ export class SurveySessionLeaderboard extends Interaction {
     }
 
     /**
-     * After displaying the score for the current question, we sum the total score
-     * of the user so far with the score of the current question.
-     *
-     * Ex:
-     * We have ('#' for total score before question and '=' for current question score):
-     * 210 p ####=================================== +30 p John
-     * We want:
-     * 240 p ###################################==== +30 p John
-     *
-     * Of course, we also have to weight the bars based on the maximum score.
-     * So if John here has 50% of the points of the leader user, both the question score bar
-     * and the total score bar need to have their width divided by 2:
-     * 240 p ##################== +30 p John
-     *
-     * The width of both bars move at the same time to reach their new position,
-     * with an animation on the width property.
-     * The new width of the "question bar" should represent the ratio of won points
-     * when compared to the total points.
-     * (We keep a minimum width of 3rem to be able to display '+30 p' within the bar).
-     *
-     * The updated total score is animated towards the new value.
-     * we keep this on screen for 500ms before reordering the bars.
+     * After displaying the score for the current question on top of the current score,
+     * we update the total score on the left with an animation by summing both and fade
+     * out the "+ x p" on the question score bar.
      *
      * @private
      */
@@ -351,7 +258,7 @@ export class SurveySessionLeaderboard extends Interaction {
             this.el.querySelectorAll(".o_survey_session_leaderboard_item").forEach((item) => {
                 const currentScore = parseInt(item.dataset.currentScore);
                 const updatedScore = parseInt(item.dataset.updatedScore);
-                let increment = parseInt(item.dataset.maxQuestionScore / 40);
+                let increment = parseInt(item.dataset.maxQuestionScore) / 40;
                 if (!increment || increment === 0) {
                     increment = 1;
                 }
@@ -361,6 +268,10 @@ export class SurveySessionLeaderboard extends Interaction {
                     updatedScore,
                     increment,
                     false
+                );
+                fadeOut(
+                    item.querySelector(".o_survey_session_leaderboard_bar_question_score"),
+                    500
                 );
                 this.waitForTimeout(animationDone, 500);
             });
