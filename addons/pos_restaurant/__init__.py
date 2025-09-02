c573f5d283531a30147b0230af0c157e3624a41a
shag-odoo <shag@odoo.com>
2025-09-02 12:29:26 +0530

    [IMP] pos_restaurant: auto-install pos_urban_piper with demo data
    Following this commit:
    - Installing pos_restaurant with demo data will also install pos_urban_piper.
    
    task-5025350
    
    closes odoo/odoo#225843
    
    Related: odoo/enterprise#94060
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/pos_restaurant/__init__.py
+++ addons/pos_restaurant/__init__.py
@@ -2,3 +2,11 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import models
+
+
+def _auto_install_pos_urban_piper_with_demo(env):
+    """If pos_restaurant is installed with demo data, also install pos_urban_piper."""
+    module_pos_urban_piper = env['ir.module.module']._get('pos_urban_piper')
+    module_pos_restaurant = env['ir.module.module']._get('pos_restaurant')
+    if module_pos_restaurant.demo and module_pos_urban_piper and module_pos_urban_piper.state == 'uninstalled':
+        module_pos_urban_piper.button_install()
