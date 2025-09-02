9d76bf5bf071b01f04a70f10ab4723f1f8d24e26
dija-odoo <dija@odoo.com>
2025-09-02 12:46:46 +0530

    [FIX] website{,_sale}: fix color configuration related issues in snippets
    This PR https://github.com/odoo/odoo/pull/203436 introduced support for
    non-website snippets from the configurator with theme customizations,
    but some color configuration related issues were found because a required
    class was missing.
    
    Fix:
    - Added the missing class to ensure snippets work properly with customizations
    - Removed unnecessary addition of a class related to s_dynamic_snippet_products
    
    Part-of: odoo/odoo#225079
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/website/models/website.py
+++ addons/website/models/website.py
@@ -528,6 +528,12 @@ class Website(models.Model):
             if template_class not in snippet_classes:
                 snippet_classes.append(template_class)
 
+        # Add 'o_colored_level' to maintain correct color configuration.
+        snippet_classes.append('o_colored_level')
+
+        # Apply class modifications (add/remove) to the snippet or its children.
+        # - If dict is found, apply to the first child matching the selector.
+        # - Otherwise, treated as direct modification on the snippet element.
         class_modifications = [
             ('remove', customizations.get('remove_classes', []) or default_settings.get('remove_classes', [])),
             ('add', customizations.get('add_classes', []) or default_settings.get('add_classes', [])),
