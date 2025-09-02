86e418b2e7f8546c8049d1f5ed73517d5e2c7314
svs-odoo <svs@odoo.com>
2025-09-02 15:19:02 +0200

    [IMP] stock_account,*: don't display location if no account
    *: mrp_account
    
    In the Stock Valuation report, don't display the "Inventory Loss" line
    if there is not at least one location of "Inventory Loss" type linked to
    an valuation account.
    
    Same for the "Cost of Production" and the "Location" type location.
    
    In addition of that, this commit cleans a little bit the Stock Valuation
    report code.

--- addons/mrp_account/report/stock_valuation_report.py
+++ addons/mrp_account/report/stock_valuation_report.py
@@ -7,9 +7,10 @@ class StockValuationReport(models.AbstractModel):
     _inherit = 'stock_account.stock.valuation.report'
 
     def _get_report_data(self, date=False, product_category=False, warehouse=False):
-        company = self.env.company
-        data = super()._get_report_data(date=date, product_category=product_category, warehouse=warehouse)
-        production_locations_valuation_vals = company._get_location_valuation_vals(
+        report_data = super()._get_report_data(date=date, product_category=product_category, warehouse=warehouse)
+        if not self._must_include_cost_of_production():
+            return report_data
+        production_locations_valuation_vals = self.env.company._get_location_valuation_vals(
             location_domain=[('usage', '=', 'production')]
         )
         cost_of_production = {
@@ -24,7 +25,7 @@ class StockValuationReport(models.AbstractModel):
         for vals in production_locations_valuation_vals:
             account = self.env['account.account'].browse(vals['account_id'])
             account_vals = account.read(['name', 'code', 'display_name'])[0]
-            data['accounts_by_id'][account.id] = account_vals
+            report_data['accounts_by_id'][account.id] = account_vals
             cost_of_production['value'] -= vals['debit']
             lines_by_account_id[account.id]['debit'] += vals['debit']
             lines_by_account_id[account.id]['credit'] += vals['credit']
@@ -34,5 +35,11 @@ class StockValuationReport(models.AbstractModel):
             'debit': vals['debit'],
             'credit': vals['credit'],
         } for (account_id, vals) in lines_by_account_id.items()]
-        data['cost_of_production'] = cost_of_production
-        return data
+        report_data['cost_of_production'] = cost_of_production
+        return report_data
+
+    def _must_include_cost_of_production(self):
+        return bool(self.env['stock.location'].search_count([
+            ('usage', '=', 'production'),
+            ('valuation_account_id', '!=', False),
+        ], limit=1))
--- addons/mrp_account/static/src/stock_valuation/controller.js
+++ addons/mrp_account/static/src/stock_valuation/controller.js
@@ -5,9 +5,11 @@ import { StockValuationReportController } from "@stock_account/stock_valuation/c
 patch(StockValuationReportController.prototype, {
     async loadReportData() {
         const data = await super.loadReportData();
-        // Prepare the "Cost of Production" lines.
-        for (const line of this.data.cost_of_production.lines) {
-            line.account = this.data.accounts_by_id[line.account_id];
+        if (this.data.cost_of_production) {
+            // Prepare the "Cost of Production" lines.
+            for (const line of this.data.cost_of_production.lines) {
+                line.account = this.data.accounts_by_id[line.account_id];
+            }
         }
         return data;
     },
