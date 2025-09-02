71f20684c50efd52f5fc0f53ab079e341156c001
Paul Morelle <pmo@odoo.com>
2025-09-02 14:59:42 +0200

    [ADD] base_setup,account: add model kpi.provider
    This commit introduces a new abstract model `kpi.provider` that allows
    different modules to contribute their Key Performance Indicators (KPIs)
    in a modular and extensible way.
    
    The `kpi.provider` model defines a base structure for KPI reporting and
    includes a `get_kpis_summary` method that should be overridden in
    inheriting models. This method is responsible for returning a list of
    KPI data specific to the module.
    
    KPI data are identified by a unique name and a type allowing for the
    caller to know how to present the corresponding value.
    
    The `account` module inherits from `kpi.provider` to include the amount
    of draft `account.move` for each `move_type`.
    
    closes odoo/odoo#225153
    
    Task-id: 5062431
    Related: odoo/enterprise#93720
    Signed-off-by: Brice Bartoletti (bib) <bib@odoo.com>

--- addons/account/tests/__init__.py
+++ addons/account/tests/__init__.py
@@ -23,6 +23,7 @@ from . import test_account_move_line_tax_details
 from . import test_account_journal_dashboard
 from . import test_chart_template
 from . import test_fiscal_position
+from . import test_kpi_provider
 from . import test_sequence_mixin
 from . import test_settings
 from . import test_tax
