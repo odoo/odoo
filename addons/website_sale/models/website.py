a2982c03928d9f2cb62190e9f9afc842a22206f9
Valeriya(vchu) <vchu@odoo.com>
2025-09-03 09:10:29 +0200

    [IMP] website_sale: improve category snippet and generation
    1. Set the cover_image of a generated category during the onboarding
    2. For the user with website editing rights show unpublished categories
    in the category snippet to improve the experience.
    
    closes odoo/odoo#225133
    
    Signed-off-by: Valentin Chevalier <vcr@odoo.com>

--- addons/website_sale/models/website.py
+++ addons/website_sale/models/website.py
@@ -519,6 +519,7 @@ class Website(models.Model):
                     'name': cat['name'],
                     'website_description': cat['description'],
                     'image_1920': image_base64,
+                    'cover_image': image_base64,
                 })
             self.env['product.public.category'].sudo().create(categories)
         return res
