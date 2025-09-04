93517026e6b9b1f93334d73d84e594dc7090e0d1
ksja-odoo <ksja@odoo.com>
2025-09-04 12:37:53 +0530

    [FIX] project_mrp_account: remove-child-cost-from-mo
    Steps to reproduce:
    - Install the project_mrp_account module.
    - Enable the 'Routes' and 'Replenish on Order' options in Inventory.
    - Create products with a multilevel MO.
    - Create a Sale Order that triggers Manufacturing Orders (MO), then validate it.
    - Go to the Project Dashboard and check the costs in the profitability section.
    
    Issue:
      Currently, the system sums the amount of all MOs without differentiating
      between source and child MOs. This results in double-counting of manufacturing
      costs in the profitability report.
    
    Cause:
      In 'account.analytic.line', the domain is only based on 'auto_account_id'
      and 'category'. There is no relation or differentiation between source and
      child MOs, so costs are aggregated incorrectly.
    
    Fix:
      We can filter out all the child MO's by checking out all MO's linked with
      that specific project. Only these source MOs are considered for cost
      calculation in the project profitability dashboard, avoiding duplication.
    
    task-4969489
    
    Co-authored-by: K Theja <kthe@odoo.com>

--- addons/project_mrp_account/models/project_project.py
+++ addons/project_mrp_account/models/project_project.py
@@ -30,10 +30,25 @@ class Project(models.Model):
     def _get_profitability_items(self, with_action=True):
         profitability_items = super()._get_profitability_items(with_action)
         mrp_category = 'manufacturing_order'
-        mrp_aal_read_group = self.env['account.analytic.line'].sudo()._read_group(
-            [('auto_account_id', 'in', self.account_id.ids), ('category', '=', mrp_category)],
-            ['currency_id'],
-            ['amount:sum'],
+        aal_domain = [("auto_account_id", "in", self.account_id.ids), ("category", "=", mrp_category)]
+
+        all_project_mos = self.env['mrp.production'].sudo().search([
+            ('project_id', 'in', self.ids)
+        ])
+        child_mos = self.env['mrp.production']
+        if all_project_mos:
+            child_mos = all_project_mos.filtered(lambda mo: mo._get_sources() & all_project_mos)
+
+            if child_mos:
+                child_moves = child_mos.move_raw_ids | child_mos.move_finished_ids
+                if child_moves.analytic_account_line_ids:
+                    aal_domain = expression.AND([
+                        aal_domain,
+                        [("id", "not in", child_moves.analytic_account_line_ids.ids)],
+                    ])
+
+        mrp_aal_read_group = self.env["account.analytic.line"].sudo()._read_group(
+            aal_domain, ["currency_id"], ["amount:sum"]
         )
         if mrp_aal_read_group:
             can_see_manufactoring_order = with_action and len(self) == 1 and self.env.user.has_group('mrp.group_mrp_user')
