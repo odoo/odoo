904f476708b59b0a9ed0610187c225a93a642d48
Pierre Lamotte (pila) <pila@odoo.com>
2025-09-04 14:35:57 +0000

    [FIX] website_sale: setup markup price with website tax configuration
    ## Version
    18.2+
    SEO Schema refactoring from task-3866937
    
    ## Issue
    The markup always contains the price without taxes, no matter the website settings for pricing display (with or without taxes).
    
    ## Steps to reproduce
    - Go to Website settings:
      - Choose "Taxes Included" for "Display Product Prices".
    - Got to the shop and select any product on which taxes apply (e.g. Customizable Desk):
      - Open console and execute `JSON.parse(document.querySelectorAll('[type="application/ld+json"]')[1].innerHTML)[0]['hasVariant'][0]['offers']['price']`;
      - The found price and the displayed price (visible on the page) are different
    
    ## Fix
    Allow tax inclusion based on parameter to mimic template's behavior on variants too depending on the website.
    https://github.com/odoo/odoo/blob/52a6d88a188d5456262428847aed229f117da8ed/addons/website_sale/models/product_template.py#L377-L430
    
    opw-4923780
    
    test
    
    X-original-commit: eb39d0f1a481f155f45af27ceb006e5bfb549d2a

--- addons/website_sale/models/product_product.py
+++ addons/website_sale/models/product_product.py
@@ -163,6 +163,16 @@ class ProductProduct(models.Model):
         """
         self.ensure_one()
 
+        product_price = request.pricelist._get_product_price(
+            self, quantity=1, target_currency=website.currency_id
+        )
+        # Use sudo to access cross-company taxes.
+        product_taxes_sudo = self.sudo().taxes_id._filter_taxes_by_company(self.env.company)
+        taxes = request.fiscal_position.map_tax(product_taxes_sudo)
+        price = self.product_tmpl_id._apply_taxes_to_price(
+            product_price, website.currency_id, product_taxes_sudo, taxes, self, website=website
+        )
+
         base_url = website.get_base_url()
         markup_data = {
             '@context': 'https://schema.org',
@@ -172,9 +182,7 @@ class ProductProduct(models.Model):
             'image': f'{base_url}{website.image_url(self, "image_1920")}',
             'offers': {
                 '@type': 'Offer',
-                'price': float_round(request.pricelist._get_product_price(
-                    self, quantity=1, target_currency=website.currency_id
-                ), 2),
+                'price': price,
                 'priceCurrency': website.currency_id.name,
             },
         }
