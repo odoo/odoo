9dcf93a2e78f8cbc65562f625c416ed38d438b78
Mathieu Duckerts-Antoine <dam@odoo.com>
2025-09-04 15:24:11 +0200

    [REF] *: calendar: multiCreateRecords API
    Part-of: odoo/odoo#225416
    Related: odoo/enterprise#93836
    Signed-off-by: Michaël Mattiello (mcm) <mcm@odoo.com>

--- addons/hr_timesheet/static/src/views/timesheet_calendar_my_timesheets/timesheet_calendar_my_timesheets_model.js
+++ addons/hr_timesheet/static/src/views/timesheet_calendar_my_timesheets/timesheet_calendar_my_timesheets_model.js
@@ -7,6 +7,6 @@ export class TimesheetCalendarMyTimesheetsModel extends CalendarModel {
     async multiCreateRecords(multiCreateData, dates) {
         this.meta.context = this.meta.context || {};
         this.meta.context.timesheet_calendar = true;
-        return await super.multiCreateRecords(multiCreateData, dates);
+        return super.multiCreateRecords(multiCreateData, dates);
     }
 }
