08c7c874d2a567808baa164d63915021b3c6cd30
Martin Doyen <mdoy@odoo.com>
2025-09-04 12:40:24 +0000

    [FIX] l10n_fr_pos_cert: hash should be created when fully processed
    Description of the issue/feature this PR addresses:
    
    Starting from version 18.0, inalterability hashes in the FR localization are sometimes calculated with incomplete data,
    leading to incorrect `l10n_fr_hash`. Related orders are then wrongly flagged as altered when running the POS Inalterability Check.
    
    Current behavior before PR:
    
    When you post a POS order and a related draft order exists, Odoo updates the existing order with the new vals.
    Because `{'state': 'paid'}` is amongst the new vals, it triggers the generation of the `l10n_fr_hash` before the order is fully processed.
    For instance, the hash will be generated before a new payment line is added for change with `_process_payment_lines()`.
    
    Desired behavior after PR is merged:
    
    The hash should be generated at the end of the order processing, with the final write in `action_pos_order_paid()`.
    
    X-original-commit: dde26e5372b8328bbc9e050f0fdedadd1de649fd

--- addons/l10n_fr_pos_cert/tests/__init__.py
+++ addons/l10n_fr_pos_cert/tests/__init__.py
@@ -2,3 +2,10 @@
 
 from . import test_frontend
 from . import test_string_to_hash
+<<<<<<< 52892ae9a5440636ad4e9cbb196eda247a3b82f1
+||||||| b84741d494f12b8d595b2977bc5bc40da39eed89
+from . import test_fr_pos
+=======
+from . import test_fr_pos
+from . import test_hash
+>>>>>>> e44543378be868b66b197f7b71996ecda6e501a8
