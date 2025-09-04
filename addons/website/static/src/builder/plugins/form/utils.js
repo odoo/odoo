edcc231240bfe9ec2fd3dd6880ce7b7f6f5c07f3
Mehdi Outagant (MOU) <mou@odoo.com>
2025-09-04 11:15:39 +0200

    [IMP] website: add new checkbox specific form options
    This commit introduces layout flexibility to easily add checkboxes such
    as "Accept terms" or "Send me emails" in forms.
    
    A field will also be "required" by default when the "Checkbox" type is
    used, and a marker (*) will be added to the checkbox description when
    "required" and its label is hidden.
    
    task-3675302

--- addons/website/static/src/builder/plugins/form/utils.js
+++ addons/website/static/src/builder/plugins/form/utils.js
@@ -2,7 +2,9 @@ import { _t } from "@web/core/l10n/translation";
 import { renderToElement } from "@web/core/utils/render";
 import { generateHTMLId } from "@html_builder/utils/utils_css";
 import { isSmallInteger } from "@html_builder/utils/utils";
+import { markup } from "@odoo/owl";
 
+const DESCRIPTION_POSITION_PREFIX = "s_website_form_description_";
 export const VISIBILITY_DATASET = [
     "visibilityDependency",
     "visibilityCondition",
@@ -88,6 +90,7 @@ export function getDefaultFormat(el) {
         requiredMark: isRequiredMark(el),
         optionalMark: isOptionalMark(el),
         mark: getMark(el),
+        textPosition: getFieldType(el) === "boolean" ? "top" : "stacked",
     };
 }
 
@@ -136,7 +139,12 @@ export function renderField(field, resetId = false) {
         params.field.string = field.name;
     }
     if (field.description) {
-        params.default_description = _t("Describe your field here.");
+        params.default_description =
+            field.type === "boolean"
+                ? markup`<span>${_t(
+                      "I agree to the"
+                  )} <a class="o_translate_inline" href="#bottom" target="_blank">Terms & Conditions</a></span>`
+                : _t("Describe your field here.");
     } else if (["email_cc", "email_to"].includes(field.name)) {
         params.default_description = _t("Separate email addresses with a comma.");
     }
@@ -215,6 +223,8 @@ export function getFieldFormat(fieldEl) {
         requiredMark: requiredMark,
         optionalMark: optionalMark,
         mark: mark && mark.textContent,
+        textPosition:
+            getFieldType(fieldEl) == "boolean" ? getDescriptionPosition(fieldEl) : "stacked",
     };
     return format;
 }
@@ -542,4 +552,22 @@ export function rerenderField(fieldEl, fields) {
     delete field.id;
     const newFieldEl = renderField(field);
     replaceFieldElement(fieldEl, newFieldEl);
+}
+
+/**
+ * Returns the field description layout (currently used for checkbox fields).
+ *
+ * @param {HTMLElement} fieldEl
+ */
+export function getDescriptionPosition(fieldEl) {
+    if (!fieldEl.querySelector(".s_website_form_field_description")) {
+        return "none";
+    } else {
+        const descriptionPositionClass = [...fieldEl.classList].find((cls) =>
+            cls.startsWith(DESCRIPTION_POSITION_PREFIX)
+        );
+        return descriptionPositionClass
+            ? descriptionPositionClass.replace(DESCRIPTION_POSITION_PREFIX, "")
+            : "stacked";
+    }
 }
