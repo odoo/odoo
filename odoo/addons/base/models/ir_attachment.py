odoo/odoo

commit 279bca567599438bb6564037935757b4f65b61e7

[IMP] base: inverse `datas` of `ir.attachment` during `create`
Before this commit, the inverse of `datas` would be computed after the `create`,
which would lead to unnecessary queries, not only for writing the new values,
but also because of the re-computation of fields that depend on these values.

odoo/addons/base/models/ir_attachment.py
@@ -195,29 +195,29 @@ class IrAttachment(models.Model):
                 attach.datas = attach.db_datas
 
     def _inverse_datas(self):
-        location = self._storage()
         for attach in self:
-            # compute the fields that depend on datas
-            value = attach.datas
-            bin_data = base64.b64decode(value) if value else b''
-            vals = {
-                'file_size': len(bin_data),
-                'checksum': self._compute_checksum(bin_data),
-                'index_content': self._index(bin_data, attach.mimetype),
-                'store_fname': False,
-                'db_datas': value,
-            }
-            if value and location != 'db':
-                # save it to the filestore
-                vals['store_fname'] = self._file_write(value, vals['checksum'])
-                vals['db_datas'] = False
-
+            vals = self._get_datas_related_values(attach.datas, attach.mimetype)
             # take current location in filestore to possibly garbage-collect it
             fname = attach.store_fname
             # write as superuser, as user probably does not have write access
             super(IrAttachment, attach.sudo()).write(vals)
             if fname:
                 self._file_delete(fname)
+
+    def _get_datas_related_values(self, data, mimetype):
+        # compute the fields that depend on datas
+        bin_data = base64.b64decode(data) if data else b''
+        values = {
+            'file_size': len(bin_data),
+            'checksum': self._compute_checksum(bin_data),
+            'index_content': self._index(bin_data, mimetype),
+            'store_fname': False,
+            'db_datas': data,
+        }
+        if data and self._storage() != 'db':
+            values['store_fname'] = self._file_write(data, values['checksum'])
+            values['db_datas'] = False
+        return values
 
     def _compute_checksum(self, bin_data):
         """ compute the checksum for the given datas
@@ -506,6 +506,8 @@ class IrAttachment(models.Model):
             for field in ('file_size', 'checksum'):
                 values.pop(field, False)
             values = self._check_contents(values)
+            if 'datas' in values:
+                values.update(self._get_datas_related_values(values.pop('datas'), values['mimetype']))
             # 'check()' only uses res_model and res_id from values, and make an exists.
             # We can group the values by model, res_id to make only one query when 
             # creating multiple attachments on a single record.

commit af23e0a4b48cb0b64a2866409253de257ec05a7c

[FIX] base: race attachment create and gc_file_store
There is a race condition between `ir.attachment.create` and
`ir.attachment._gc_file_store` that can lead to the creation of an
attachment with no related file in the file store.

Race
----

The race runs as follow:

1) `_gc_file_store` acquires a write exlusif lock on the ir_attachment
   table, but is interrupted by the OS process scheduler before it can
   load the file gc checklist.
2) `create` kicks in, write a new file in the file store, adds an entry
   in the checklist, but can'tcreate the ir.attachment record yet as the
   table is locked.
3) `_gc_file_store` resumes, it loads the checklist, sees that
   `create`'s file as no attachment and removes it. It then releases the
   write exclusif table lock.
4) `create` resumes, it creates the `ir.attachment` record.

Subsequent access on the ir.attachment created record `raw` or `datas`
fields fails because the file doesn't exist on disk.

Description
-----------

The bug was introduced by commit 279bca5675994 in 13.0. Before that
commit the create override in ir.attachment would first create the
record, and only then write the file on disk. This way the `create`
function acquired first a row exclusif lock and only then wrote the file
and the checklist.

That the error went undercover for several years can be explained as
follow:

1. The ir.autovacuum model and its cron has been unreliable for many
   years, it was often reported that the `_gc_file_store` (being the
   last in the list) did not run.
2. That the cron did not run made for a huge checklist that couldn't be
   processed all at once.
3. The cron usually only run once per night, when there are less people
   connected on the database to create attachments.
4. Finally, embarassing, we did discard some "missing file" reports and
   instead blamed the customers who reported them.

That we discover the error now is another chain of events:

odoo/addons/base/models/ir_attachment.py
@@ -264,17 +264,31 @@ class IrAttachment(models.Model):
         self._set_attachment_data(lambda attach: base64.b64decode(attach.datas or b''))
 
     def _set_attachment_data(self, asbytes):
+        old_fnames = []
+        checksum_raw_map = {}
+
         for attach in self:
             # compute the fields that depend on datas
             bin_data = asbytes(attach)
             vals = self._get_datas_related_values(bin_data, attach.mimetype)
+            if bin_data:
+                checksum_raw_map[vals['checksum']] = bin_data
 
             # take current location in filestore to possibly garbage-collect it
-            fname = attach.store_fname
+            if attach.store_fname:
+                old_fnames.append(attach.store_fname)
+
             # write as superuser, as user probably does not have write access
             super(IrAttachment, attach.sudo()).write(vals)
-            if fname:
+
+        if self._storage() != 'db':
+            # before touching the filestore, flush to prevent the GC from
+            # running until the end of the transaction
+            self.flush_recordset(['checksum', 'store_fname'])
+            for fname in old_fnames:
                 self._file_delete(fname)
+            for checksum, raw in checksum_raw_map.items():
+                self._file_write(raw, checksum)
 
     def _get_datas_related_values(self, data, mimetype):
         checksum = self._compute_checksum(data)
@@ -290,7 +304,7 @@ class IrAttachment(models.Model):
             'db_datas': data,
         }
         if data and self._storage() != 'db':
-            values['store_fname'] = self._file_write(data, values['checksum'])
+            values['store_fname'], _full_path = self._get_path(data, checksum)
             values['db_datas'] = False
         return values
 
@@ -717,6 +731,7 @@ class IrAttachment(models.Model):
             in vals.items()
             if key not in ('file_size', 'checksum', 'store_fname')
         } for vals in vals_list]
+        checksum_raw_map = {}
 
         for values in vals_list:
             values = self._check_contents(values)
@@ -725,10 +740,11 @@ class IrAttachment(models.Model):
                 if isinstance(raw, str):
                     # b64decode handles str input but raw needs explicit encoding
                     raw = raw.encode()
-                values.update(self._get_datas_related_values(
-                    raw or base64.b64decode(datas or b''),
-                    values['mimetype']
-                ))
+                elif not raw:
+                    raw = base64.b64decode(datas or b'')
+                values.update(self._get_datas_related_values(raw, values['mimetype']))
+                if raw:
+                    checksum_raw_map[values['checksum']] = raw
 
             # 'check()' only uses res_model and res_id from values, and make an exists.
             # We can group the values by model, res_id to make only one query when
@@ -743,6 +759,9 @@ class IrAttachment(models.Model):
         if any(self._inaccessible_comodel_records(model_and_ids, 'write')):
             raise AccessError(_("Sorry, you are not allowed to access this document."))
         records = super().create(vals_list)
+        if self._storage() != 'db':
+            for checksum, raw in checksum_raw_map.items():
+                self._file_write(raw, checksum)
         records._check_serving_attachments()
         return records
 

odoo/addons/test_http/tests/test_static.py
@@ -424,6 +424,7 @@ class TestHttpStatic(TestHttpStaticCommon):
         for field in ('glyph_attach', 'glyph_inline', 'glyph_related', 'glyph_compute'):
             earth[field] = data
             res = self.url_open(f'/web/image/test_http.stargate/{earth.id}/{field}')
+            res.raise_for_status()
             self.assertEqual(res.headers['Content-Type'], 'application/octet-stream')  # Shouldn't be text/html
             self.assertEqual(res.headers['Content-Security-Policy'], "default-src 'none'")
 