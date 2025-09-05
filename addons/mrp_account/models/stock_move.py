eeccc6df9f90d83d09c4ff8bd140a6d8f8138e06
Adrien Widart (awt) <awt@odoo.com>
2025-09-05 14:48:59 +0200

    [REV] {mrp,stock}_account, purchase_mrp: unbuild with basic valo
    This commit reverts [1], [2], [3], and [4]. (It actually results in
    minimal changes since those commits were already removing parts of
    each other.)
    
    Issue before those commits:
    1. Setup a auto-fifo category and two storable products (a component
       and a finished product)
    2. Receive one compo at 10, then one at 25
    3. Produce two MO with one finished product
    4. Unbuild the second one
    
    Error:
    - For the component, we just use the value of the consumed
      components: IN 1 @ 25
    - For the finished product, we process it as a classic out. Reminder,
      we are in FIFO: OUT 1 @ 10
    
    As a result, thanks to the unbuild, we have created
    - A over-valuation of the stock (+15)
    - An outstanding balance of the "Cost of Production"
    
    This is why [1] has been merged. However, it brought some other
    issues, cf [2], [3] and [4]. Unfortunately, it still has some issues
    - After the above use case, the difference between the debit and the
      credit of the stock valuation account is no longer the sum of the
      remaining values of the layers
    - Adding some landed costs on MOs will lead to a traceback when
      undbuilding
    - The over-valuation of the stock (that was already present before [1],
      cf above) is still present
    
    Following some discussions with R&D and the product owners, we have
    decided to start over from scratch, which means:
    - Revert all commits
    - Try another approach (if so, the new PR will be linked to the PR
      related with this commit)
    
    [2], [3], and [4] are partially reverted: the tests can remain, as
    they were only failing due to a sequence of changes.
    
    [1] 84dda968146d2f3743ab7fc516300e50780725e3
    [2] 49565cdd9007ac66a3b835dc073777e2e6c48f2c
    [3] 3a69456a291da593748475c86e7efc6234019e47
    [4] fb30cde9a320c245cf1321c9dc2ea2e67a53d0a0
    
    OPW-5036574
    
    closes odoo/odoo#225728
    
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/mrp_account/models/stock_move.py
+++ addons/mrp_account/models/stock_move.py
@@ -47,43 +47,6 @@ class StockMove(models.Model):
         self.ensure_one()
         return self.location_dest_id.usage == 'production' and self.location_id._should_be_valued()
 
-    def _create_out_svl(self, forced_quantity=None):
-        svls = super()._create_out_svl(forced_quantity)
-        unbuild_svls = svls.filtered('stock_move_id.unbuild_id')
-        unbuild_cost_correction_move_list = list()
-        for svl in unbuild_svls:
-            build_time_unit_cost = svl.stock_move_id.unbuild_id.mo_id.move_finished_ids.filtered(
-                lambda m: m.product_id == svl.product_id
-            ).stock_valuation_layer_ids.unit_cost
-            unbuild_difference = svl.unit_cost - build_time_unit_cost
-            if svl.product_id.valuation == 'real_time' and not svl.currency_id.is_zero(unbuild_difference):
-                product_accounts = svl.product_id.product_tmpl_id.get_product_accounts()
-                valuation_account, production_account = (
-                    product_accounts['stock_valuation'],
-                    product_accounts['production'],
-                )
-                desc = _('%s - Unbuild Cost Difference', svl.stock_move_id.unbuild_id.name)
-                unbuild_cost_correction_move_list.append({
-                    'journal_id': product_accounts['stock_journal'].id,
-                    'date': fields.Date.context_today(self),
-                    'ref': desc,
-                    'move_type': 'entry',
-                    'line_ids': [Command.create({
-                        'name': desc,
-                        'ref': desc,
-                        'account_id': account.id,
-                        'balance': balance,
-                        'product_id': svl.product_id.id,
-                    }) for account, balance in (
-                        (valuation_account, unbuild_difference),
-                        (production_account, -unbuild_difference),
-                    )],
-                })
-        if unbuild_cost_correction_move_list:
-            unbuild_cost_correction_moves = self.env['account.move'].sudo().create(unbuild_cost_correction_move_list)
-            unbuild_cost_correction_moves._post()
-        return svls
-
     def _get_all_related_sm(self, product):
         moves = super()._get_all_related_sm(product)
         return moves | self.filtered(
