d28f2b51bcf5f6f2bff943d35529f5020cad148d
Julien Mougenot <jum@odoo.com>
2025-09-05 10:30:15 +0000

    [FIX] web: allow date field to re-apply same value
    This commit allows date (i.e. date, datetime & daterange) fields to
    apply a value from the props (e.g. coming from an `onchange`), even if
    that value is the same as the initial one.
    
    Before this commit, it was not possible due to the fact that the date
    service responsible for the reactivity of the field was updating the
    input in an incorrect order, causing the field to display the 'input'
    value, and not the one enforced by the props.
    
    Task 4978896
    
    X-original-commit: ee15163fe516817da277760752892ea76a699e22

--- addons/web/static/src/views/fields/datetime/datetime_field.js
+++ addons/web/static/src/views/fields/datetime/datetime_field.js
@@ -139,7 +139,7 @@ export class DateTimeField extends Component {
             onChange: () => {
                 this.state.range = this.isRange(this.state.value);
             },
-            onApply: () => {
+            onApply: async () => {
                 const toUpdate = {};
                 if (Array.isArray(this.state.value)) {
                     // Value is already a range
@@ -156,7 +156,7 @@ export class DateTimeField extends Component {
                 }
 
                 if (Object.keys(toUpdate).length) {
-                    this.props.record.update(toUpdate);
+                    await this.props.record.update(toUpdate);
                 }
             },
         });
