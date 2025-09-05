5a7f3519517452939bfd8248c3dc82464eded179
romo <romo@odoo.com>
2025-09-05 08:35:23 +0200

    [IMP] website: update some header options availability
    - The "Link Style" option is now available for all templates.
    - Choosing a template no longer changes the "Link Style" option. The
      value set by the user will be kept.
    - The "Content Width" option *preview* was removed for some templates
      because it was broken.
    
    Part-of: odoo/odoo#225672
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/website/static/src/builder/plugins/options/header/header_template_option.js
+++ addons/website/static/src/builder/plugins/options/header/header_template_option.js
@@ -3,4 +3,8 @@ import { BaseOptionComponent } from "@html_builder/core/utils";
 export class HeaderTemplateOption extends BaseOptionComponent {
     static template = "website.HeaderTemplateOption";
     static props = {};
+
+    hasSomeOptions(opts) {
+        return opts.some((opt) => this.isActiveItem(opt));
+    }
 }
