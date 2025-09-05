56b38815a73d2ba7ff71534ba90a105bbd67efbd
Zelong Lin <zel@odoo.com>
2025-09-05 10:35:37 +0200

    [IMP] mail,*: use data-* attributes for test
    This commit updates the test selectors to use data-as-tab-id to avoid
    ambiguity. This change enhances the robustness of the tests by reducing
    their dependency on CSS classes.
    
    Using data-as-tab-id attributes provides a more stable and reliable
    way to select elements in the DOM for testing purposes.
    
    backport - https://github.com/odoo/odoo/pull/224974
    
    closes odoo/odoo#225571
    
    X-original-commit: c998e34dc7fb6456c8c51ea903e1bbb284510104
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>
    Signed-off-by: Zelong Lin (zel) <zel@odoo.com>

--- addons/mail/static/tests/chat_bubble/chat_bubble.test.js
+++ addons/mail/static/tests/chat_bubble/chat_bubble.test.js
@@ -220,15 +220,15 @@ test("chat bubbles are synced between tabs", async () => {
     setupChatHub({ folded: [channelId] });
     const tab1 = await start({ asTab: true });
     const tab2 = await start({ asTab: true });
-    await contains(".o-mail-ChatBubble", { target: tab1 });
-    await contains(".o-mail-ChatBubble", { target: tab2 });
+    await contains(`${tab1.selector} .o-mail-ChatBubble`);
+    await contains(`${tab2.selector} .o-mail-ChatBubble`);
     await runAllTimers(); // Wait for bus service to fully load
-    await click(".o-mail-ChatBubble[name='Marc']", { target: tab1 });
-    await contains(".o-mail-ChatWindow", { target: tab2 }); // open sync
-    await click(".o-mail-ChatWindow-command[title='Fold']", { target: tab2 });
-    await contains(".o-mail-ChatWindow", { target: tab1, count: 0 }); // fold sync
-    await click(".o-mail-ChatBubble[name='Marc'] .o-mail-ChatBubble-close", { target: tab1 });
-    await contains(".o-mail-ChatBubble[name='Marc']", { target: tab2, count: 0 }); // close sync
+    await click(`${tab1.selector} .o-mail-ChatBubble[name='Marc']`);
+    await contains(`${tab2.selector} .o-mail-ChatWindow`); // open sync
+    await click(`${tab2.selector} .o-mail-ChatWindow-command[title='Fold']`);
+    await contains(`${tab1.selector} .o-mail-ChatWindow`, { count: 0 }); // fold sync
+    await click(`${tab1.selector} .o-mail-ChatBubble[name='Marc'] .o-mail-ChatBubble-close`);
+    await contains(`${tab2.selector} .o-mail-ChatBubble[name='Marc']`, { count: 0 }); // close sync
 });
 
 test("Chat bubbles do not fetch messages until becoming open", async () => {
@@ -354,13 +354,13 @@ test("Compact chat hub is crosstab synced", async () => {
     setupChatHub({ folded: channelIds });
     const env1 = await start({ asTab: true });
     const env2 = await start({ asTab: true });
-    await contains(".o-mail-ChatBubble", { count: 2, target: env1 });
-    await contains(".o-mail-ChatBubble", { count: 2, target: env2 });
-    await hover(".o-mail-ChatBubble:eq(0)", { target: env1 });
-    await click("button[title='Chat Options']", { target: env1 });
-    await click(".o-dropdown-item", { text: "Hide all conversations", target: env1 });
-    await contains(".o-mail-ChatBubble .fa-comments", { target: env1 });
-    await contains(".o-mail-ChatBubble .fa-comments", { target: env2 });
+    await contains(`${env1.selector} .o-mail-ChatBubble`, { count: 2 });
+    await contains(`${env2.selector} .o-mail-ChatBubble`, { count: 2 });
+    await hover(`${env1.selector} .o-mail-ChatBubble:eq(0)`);
+    await click(`${env1.selector} button[title='Chat Options']`);
+    await click(`${env1.selector} .o-dropdown-item`, { text: "Hide all conversations" });
+    await contains(`${env1.selector} .o-mail-ChatBubble .fa-comments`);
+    await contains(`${env2.selector} .o-mail-ChatBubble .fa-comments`);
 });
 
 test("Compacted chat hub shows badge with amount of hidden chats with important messages", async () => {
