odoo/odoo

commit 4a5e460bf07a0ae8ca9e408d601841209d1f2b7e

[IMP] sale,website_sale: combo improvements
prior to this commit :
- Optional products did not work with combos
- the header line for combo did not show discount/quantity
- in sale backend single choice combo required user to select the choice

post this commit :
- the combo header line in sale is no longer a section
- single choice options will be preselected in the combo dialog
- added the feature of optional products for combos (only in backend for now)

task-4314424

closes odoo/odoo#194496

Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

addons/sale/static/src/js/sale_order_line_field/sale_order_line_field.js
@@ -45,17 +45,15 @@ export class SaleOrderLineListRenderer extends ProductLabelSectionAndNoteListRen
      * Combo logic
      */
 
-    /**
-     * Whether the provided record is a section, a note, or a combo.
-     *
-     * This method's name isn't ideal since it doesn't mention combos, but we'd have to override a
-     * few other methods to fix this, and the added complexity isn't worth it.
-     *
-     * @param record The record to check
-     * @return {Boolean} Whether the record is a section, a note, or a combo.
-     */
-    isSectionOrNote(record=null) {
-        return super.isSectionOrNote(record) || this.isCombo(record);
+    getCellClass(column, record) {
+        const classNames = super.getCellClass(column, record);
+        if (
+            this.isCombo(record)
+            && ![this.titleField, 'product_uom_qty', 'discount'].includes(column.name)
+        ) {
+            return `${classNames} opacity-0 pe-none`;
+        }
+        return classNames;
     }
 
     getRowClass(record) {

addons/sale/controllers/combo_configurator.py
@@ -146,6 +146,7 @@ class SaleComboConfiguratorController(Controller):
         return {
             'id': combo_item.id,
             'extra_price': combo_item.extra_price,
+            'is_preselected': is_preselected,
             'is_selected': bool(selected_combo_item) or is_preselected,
             'is_configurable': is_configurable,
             'product': {
@@ -153,6 +154,7 @@ class SaleComboConfiguratorController(Controller):
                 'product_tmpl_id': combo_item.product_id.product_tmpl_id.id,
                 'display_name': combo_item.product_id.display_name,
                 'ptals': self._get_ptals_data(combo_item.product_id, selected_combo_item),
+                'description': combo_item.product_id.description_sale,
                 **request.env['product.template']._get_additional_configurator_data(
                     combo_item.product_id, date, currency, pricelist, **kwargs
                 ),

addons/sale/models/sale_order.py
@@ -895,8 +895,10 @@ class SaleOrder(models.Model):
     @api.onchange('order_line')
     def _onchange_order_line(self):
         for index, line in enumerate(self.order_line):
-            if line.product_type == 'combo' and line.selected_combo_items:
-                linked_lines = line._get_linked_lines()
+            if line.product_type != 'combo':
+                continue
+            combo_item_lines = line._get_linked_lines().filtered('combo_item_id')
+            if line.selected_combo_items:
                 selected_combo_items = json.loads(line.selected_combo_items)
                 if (
                     selected_combo_items
@@ -908,7 +910,7 @@ class SaleOrder(models.Model):
                     ))
 
                 # Delete any existing combo item lines.
-                delete_commands = [Command.delete(linked_line.id) for linked_line in linked_lines]
+                delete_commands = [Command.delete(linked_line.id) for linked_line in combo_item_lines]
                 # Create a new combo item line for each selected combo item.
                 create_commands = [Command.create({
                     'product_id': combo_item['product_id'],
@@ -938,6 +940,11 @@ class SaleOrder(models.Model):
                 # Clear `selected_combo_items` to avoid applying the same changes multiple times.
                 line.selected_combo_items = False
                 self.order_line = delete_commands + create_commands + update_commands
+            elif combo_item_lines:
+                combo_item_lines.update({
+                    'product_uom_qty': line.product_uom_qty,
+                    'discount': line.discount,
+                })
 
     #=== CRUD METHODS ===#
 

addons/sale/models/sale_order_line.py
@@ -1674,4 +1674,7 @@ class SaleOrderLine(models.Model):
         """ A combo product line always has a zero price (by design). The actual price of the combo
         product can be computed by summing the prices of its combo items (i.e. its linked lines).
         """
-        return self.linked_line_ids if self.product_type == 'combo' else self
+        if self.product_type == 'combo':
+            # Only consider combo item lines (not optional product lines)
+            return self.linked_line_ids.filtered('combo_item_id')
+        return self

addons/sale/static/src/js/combo_configurator_dialog/combo_configurator_dialog.js
@@ -54,6 +54,9 @@ export class ComboConfiguratorDialog extends Component {
         this._initSelectedComboItems();
         this.getPriceUrl = '/sale/combo_configurator/get_price';
         useSubEnv({ currency: { id: this.props.currency_id } });
+
+        this.unconfigurableCombos = this.props.combos.filter(combo => !combo.isConfigurable);
+        this.configurableCombos = this.props.combos.filter(combo => combo.isConfigurable);
     }
 
     /**

addons/sale/static/src/js/combo_configurator_dialog/combo_configurator_dialog.xml
@@ -5,10 +5,48 @@
             title="props.display_name"
             contentClass="'sale-combo-configurator-dialog'"
         >
-            <div t-foreach="props.combos" t-as="combo" t-key="combo.id">
+            <div t-if="unconfigurableCombos.length" class="mb-4">
+                <span class="d-inline-block mb-3 h4">
+                    Included
+                </span>
+                <div class="container">
+                    <div
+                        t-foreach="unconfigurableCombos"
+                        t-as="combo"
+                        t-key="combo.id"
+                        class="row mb-3"
+                    >
+                        <t t-set="product" t-value="combo.combo_items[0].product"/>
+                        <div class="col-2 p-0">
+                            <img
+                                t-attf-src="/web/image/product.product/{{product.id}}/image_256"
+                                alt="Product Image"
+                                role="img"
+                                class="img-thumbnail rounded"
+                            />
+                        </div>
+                        <div
+                            name="preselected_product_name"
+                            class="col d-flex justify-content-center text-break flex-column"
+                        >
+                            <span
+                                name="preselected_product_title"
+                                t-out="product.display_name"
+                                class="h5"
+                            />
+                            <div
+                                t-if="product.description"
+                                t-out="product.description"
+                                class="text-muted small text-truncate"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div t-foreach="configurableCombos" t-as="combo" t-key="combo.id">
                 <span
                     name="sale_combo_configurator_title"
-                    class="d-inline-block mt-4 mb-3 h4"
+                    t-attf-class="d-inline-block mb-3 h4 {{combo_index !== 0? 'mt-4' : ''}}"
                     t-out="combo.name"
                 />
                 <div class="combo-item-grid d-flex d-md-grid gap-4 pb-2 pb-md-0 overflow-x-auto">
@@ -18,6 +56,7 @@
                             extraPrice="comboItem.extra_price"
                             onClick="() => this.selectComboItem(combo.id, comboItem)"
                             isSelected="state.selectedComboItems.get(combo.id)?.id === comboItem.id"
+                            isConfigurable="comboItem.is_configurable"
                         />
                     </t>
                 </div>

addons/sale/static/src/js/models/product_combo.js
@@ -20,4 +20,22 @@ export class ProductCombo {
     get selectedComboItem() {
         return this.combo_items.find(item => item.is_selected);
     }
+
+    /**
+    * Return the preselected combo item, if any.
+    *
+    * @return {ProductComboItem|undefined} The preselected combo items, if any.
+    */
+    get preselectedComboItem() {
+        return this.combo_items.find(item => item.is_preselected);
+    }
+
+    /**
+     * Check whether this combo is configurable.
+     *
+     * @return {Boolean} Whether this combo is configurable.
+     */
+    get isConfigurable() {
+        return !this.combo_items.some(item => item.is_preselected);
+    }
 }

addons/sale/static/src/js/models/product_combo_item.js
@@ -4,13 +4,15 @@ export class ProductComboItem {
     /**
      * @param {number} id
      * @param {number} extra_price
+     * @param {boolean} is_preselected
      * @param {boolean} is_selected
      * @param {boolean} is_configurable
      * @param {ProductProduct|object} product
      */
-    constructor({id, extra_price, is_selected, is_configurable, product}) {
+    constructor({id, extra_price, is_preselected, is_selected, is_configurable, product}) {
         this.id = id;
         this.extra_price = extra_price;
+        this.is_preselected = is_preselected;
         this.is_selected = is_selected;
         this.is_configurable = is_configurable;
         this.product = new ProductProduct(product);

commit f38d5f093859570dd9f83f2a3cc246e163b236d4

[IMP] sale: support combo resequence and clean `sol_o2m` widget
Before this commit:
- Combo product blocks could not be moved, which prevented inserting lines below
  them.
- In dark mode, combo product lines displayed incorrectly because some columns
  had opacity set to 0 (introduced in 4a5e460bf07a0ae8ca9e408d601841209d1f2b7e).
- The delete icon on combo product items was hidden with an XPath, instead of
  using the existing renderer hook.

After this commit:
- Added move up/down actions on combo product blocks, similar to section lines.
  - When moving, if another combo block is encountered, the whole blocks are
    swapped.
  - Options are hidden when a move is not possible (e.g., no line after the
    block).
- Centralized conditions for making combo-specific fields invisible, reducing
  the need for repeated XML invisible conditions in SO views.
- Used the renderer hook to hide the delete icon on combo product items,
  removing the need for XPath in the XML.

task-5009037

Part-of: odoo/odoo#223686
Related: odoo/enterprise#92822
Signed-off-by: Habib Ayob (ayh) <ayh@odoo.com>

addons/sale/static/src/js/sale_order_line_field/sale_order_line_field.js
@@ -13,12 +13,55 @@ import {
 import { registry } from '@web/core/registry';
 import { CharField } from '@web/views/fields/char/char_field';
 
+function getComboRecords(listRecords, record) {
+    const comboRecords = [];
+
+    if (record.data.product_type === 'combo') {
+        // if currernt record is combo then we move forward util we find non combo line
+        comboRecords.push(record);
+        let index = listRecords.indexOf(record) + 1;
+
+        while (index < listRecords.length) {
+            const r = listRecords[index];
+            if (!r.data.combo_item_id?.id || r.data.linked_line_id?.id !== record.resId) {
+                break;
+            }
+            comboRecords.push(r);
+            index++;
+        }
+
+    } else if (record.data.combo_item_id?.id) {
+        // if current record is combo item then we move backward util we find associated combo line
+        // Here we assume that the record we get is the last item of the combo 
+        let index = listRecords.indexOf(record);
+        while (index >= 0) {
+            const r = listRecords[index];
+            comboRecords.unshift(r);
+
+            if (r.data.product_type === 'combo' && r.resId === record.data.linked_line_id?.id) {
+                break;
+            }
+            index--;
+        }
+    }
+
+    return comboRecords;
+}
+
 export class SaleOrderLineListRenderer extends ProductLabelSectionAndNoteListRender {
     static recordRowTemplate = 'sale.ListRenderer.RecordRow';
 
     setup(){
         super.setup();
         this.priceColumns.push('discount');
+    }
+
+    /**
+     * Little hack to make sure we get correct title field everytime
+     * while accessing comboColumns
+     */
+    get comboColumns() {
+        return [this.titleField, 'product_uom_qty', 'discount'];
     }
 
     /**
@@ -46,21 +89,6 @@ export class SaleOrderLineListRenderer extends ProductLabelSectionAndNoteListRen
         return activeColumns;
     }
 
-    /**
-     * Combo logic
-     */
-
-    getCellClass(column, record) {
-        const classNames = super.getCellClass(column, record);
-        if (
-            this.isCombo(record)
-            && ![this.titleField, 'product_uom_qty', 'discount'].includes(column.name)
-        ) {
-            return `${classNames} opacity-0 pe-none`;
-        }
-        return classNames;
-    }
-
     getRowClass(record) {
         let classNames = super.getRowClass(record);
         if (this.isCombo(record) || this.isComboItem(record)) {
@@ -81,6 +109,53 @@ export class SaleOrderLineListRenderer extends ProductLabelSectionAndNoteListRen
             await record.update({ selected_combo_items: JSON.stringify([]) });
         }
         await super.onDeleteRecord(record);
+    }
+
+    async moveCombo(record, direction) {
+        const canProceed = await this.props.list.leaveEditMode({ canAbandon: false });
+        if (!canProceed) return;
+
+        const { movingProducts, targetProducts } = this.getComboSwapPairs(record, direction);
+        return this.swapSections(movingProducts, targetProducts);
+    }
+
+    getComboSwapPairs(record, direction) {
+        const comboProducts = getComboRecords(this.props.list.records, record);
+
+        if (direction === 'up') {
+            return {
+                movingProducts: this.getPreviousRecords(record),
+                targetProducts: comboProducts,
+            };
+        }
+        if (direction === 'down') {
+            return {
+                movingProducts: comboProducts,
+                targetProducts: this.getNextRecords(record),
+            };
+        }
+        return { movingProducts: [], targetProducts: [] };
+    }
+
+    getPreviousRecords(record) {
+        const { records } = this.props.list;
+        const previousRecord = records[records.indexOf(record) - 1];
+
+        if (previousRecord?.data.combo_item_id?.id){
+            return getComboRecords(records, previousRecord);
+        }
+        return previousRecord ? [previousRecord] : false;
+    }
+
+    getNextRecords(record) {
+        const { records } = this.props.list;
+        const comboRecords = getComboRecords(records, record);
+
+        const nextRecord = records[records.indexOf(record) + comboRecords.length];
+        if (nextRecord?.data.product_type === 'combo'){
+            return getComboRecords(records, nextRecord);
+        }
+        return nextRecord ? [nextRecord] : false;
     }
 
     isCombo(record) {

addons/sale/static/src/js/sale_order_line_field/sale_order_line_field.xml
@@ -5,11 +5,51 @@
         t-inherit="account.SectionAndNoteListRenderer.RecordRow"
         t-inherit-mode="primary"
     >
-        <!-- Remove the drag-and-drop handle for combo lines and combo item lines. -->
-        <Field position="attributes">
-            <attribute name="t-if">
-                !((isCombo(record) || isComboItem(record)) &amp;&amp; column.widget === 'handle')
-            </attribute>
-        </Field>
+        <t t-set="isInvisible" position="attributes">
+            <attribute
+                name="t-value"
+                separator=" or "
+                add="isCombo(record) and !this.comboColumns.includes(column.name)"
+            />
+        </t>
+
+        <xpath expr="//td[hasclass('o_list_record_remove')]" position="attributes">
+            <attribute name="t-if" separator=" and " add="!isCombo(record)"/>
+        </xpath>
+
+        <xpath expr="//td[hasclass('o_list_section_options')]" position="before">
+            <td t-elif="isCombo(record)" class="o_list_section_options w-print-0 p-print-0 text-center">
+                <Dropdown position="'bottom-end'" t-if="!props.readonly">
+                    <button class="btn px-1cursor-pointer">
+                        <i class="fa fa-ellipsis-v"/>
+                    </button>
+                    <t t-set-slot="content">
+                        <DropdownItem
+                            t-if="!record.isNew and this.getPreviousRecords(record)"
+                            onSelected="() => this.moveCombo(record, 'up')"
+                        >
+                            <i class="me-1 fa fa-fw fa-arrow-up"/>
+                            <span>Move Up</span>
+                        </DropdownItem>
+                        <DropdownItem
+                            t-if="!record.isNew and this.getNextRecords(record)"
+                            onSelected="() => this.moveCombo(record, 'down')"
+                        >
+                            <i class="me-1 fa fa-fw fa-arrow-down"/>
+                            <span>Move Down</span>
+                        </DropdownItem>
+                        <t t-if="hasDeleteButton">
+                            <DropdownItem
+                                onSelected="() => this.onDeleteRecord(record)"
+                                class="'text-danger'"
+                            >
+                                <i class="me-1 fa fa-fw fa-trash"/>
+                                <span>Delete</span>
+                            </DropdownItem>
+                        </t>
+                    </t>
+                </Dropdown>
+            </td>
+        </xpath>
     </t>
 </templates>
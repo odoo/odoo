f38d5f093859570dd9f83f2a3cc246e163b236d4
Nisarg (nipl) <nipl@odoo.com>
2025-09-02 17:43:44 +0530

    [IMP] sale: support combo resequence and clean `sol_o2m` widget
    Before this commit:
    - Combo product blocks could not be moved, which prevented inserting lines below
      them.
    - In dark mode, combo product lines displayed incorrectly because some columns
      had opacity set to 0 (introduced in 4a5e460bf07a0ae8ca9e408d601841209d1f2b7e).
    - The delete icon on combo product items was hidden with an XPath, instead of
      using the existing renderer hook.
    
    After this commit:
    - Added move up/down actions on combo product blocks, similar to section lines.
      - When moving, if another combo block is encountered, the whole blocks are
        swapped.
      - Options are hidden when a move is not possible (e.g., no line after the
        block).
    - Centralized conditions for making combo-specific fields invisible, reducing
      the need for repeated XML invisible conditions in SO views.
    - Used the renderer hook to hide the delete icon on combo product items,
      removing the need for XPath in the XML.
    
    task-5009037
    
    Part-of: odoo/odoo#223686
    Related: odoo/enterprise#92822
    Signed-off-by: Habib Ayob (ayh) <ayh@odoo.com>

--- addons/sale/static/src/js/sale_order_line_field/sale_order_line_field.js
+++ addons/sale/static/src/js/sale_order_line_field/sale_order_line_field.js
@@ -13,12 +13,55 @@ import {
 import { registry } from '@web/core/registry';
 import { CharField } from '@web/views/fields/char/char_field';
 
+function getComboRecords(listRecords, record) {
+    const comboRecords = [];
+
+    if (record.data.product_type === 'combo') {
+        // if currernt record is combo then we move forward util we find non combo line
+        comboRecords.push(record);
+        let index = listRecords.indexOf(record) + 1;
+
+        while (index < listRecords.length) {
+            const r = listRecords[index];
+            if (!r.data.combo_item_id?.id || r.data.linked_line_id?.id !== record.resId) {
+                break;
+            }
+            comboRecords.push(r);
+            index++;
+        }
+
+    } else if (record.data.combo_item_id?.id) {
+        // if current record is combo item then we move backward util we find associated combo line
+        // Here we assume that the record we get is the last item of the combo 
+        let index = listRecords.indexOf(record);
+        while (index >= 0) {
+            const r = listRecords[index];
+            comboRecords.unshift(r);
+
+            if (r.data.product_type === 'combo' && r.resId === record.data.linked_line_id?.id) {
+                break;
+            }
+            index--;
+        }
+    }
+
+    return comboRecords;
+}
+
 export class SaleOrderLineListRenderer extends ProductLabelSectionAndNoteListRender {
     static recordRowTemplate = 'sale.ListRenderer.RecordRow';
 
     setup(){
         super.setup();
         this.priceColumns.push('discount');
+    }
+
+    /**
+     * Little hack to make sure we get correct title field everytime
+     * while accessing comboColumns
+     */
+    get comboColumns() {
+        return [this.titleField, 'product_uom_qty', 'discount'];
     }
 
     /**
@@ -46,21 +89,6 @@ export class SaleOrderLineListRenderer extends ProductLabelSectionAndNoteListRen
         return activeColumns;
     }
 
-    /**
-     * Combo logic
-     */
-
-    getCellClass(column, record) {
-        const classNames = super.getCellClass(column, record);
-        if (
-            this.isCombo(record)
-            && ![this.titleField, 'product_uom_qty', 'discount'].includes(column.name)
-        ) {
-            return `${classNames} opacity-0 pe-none`;
-        }
-        return classNames;
-    }
-
     getRowClass(record) {
         let classNames = super.getRowClass(record);
         if (this.isCombo(record) || this.isComboItem(record)) {
@@ -81,6 +109,53 @@ export class SaleOrderLineListRenderer extends ProductLabelSectionAndNoteListRen
             await record.update({ selected_combo_items: JSON.stringify([]) });
         }
         await super.onDeleteRecord(record);
+    }
+
+    async moveCombo(record, direction) {
+        const canProceed = await this.props.list.leaveEditMode({ canAbandon: false });
+        if (!canProceed) return;
+
+        const { movingProducts, targetProducts } = this.getComboSwapPairs(record, direction);
+        return this.swapSections(movingProducts, targetProducts);
+    }
+
+    getComboSwapPairs(record, direction) {
+        const comboProducts = getComboRecords(this.props.list.records, record);
+
+        if (direction === 'up') {
+            return {
+                movingProducts: this.getPreviousRecords(record),
+                targetProducts: comboProducts,
+            };
+        }
+        if (direction === 'down') {
+            return {
+                movingProducts: comboProducts,
+                targetProducts: this.getNextRecords(record),
+            };
+        }
+        return { movingProducts: [], targetProducts: [] };
+    }
+
+    getPreviousRecords(record) {
+        const { records } = this.props.list;
+        const previousRecord = records[records.indexOf(record) - 1];
+
+        if (previousRecord?.data.combo_item_id?.id){
+            return getComboRecords(records, previousRecord);
+        }
+        return previousRecord ? [previousRecord] : false;
+    }
+
+    getNextRecords(record) {
+        const { records } = this.props.list;
+        const comboRecords = getComboRecords(records, record);
+
+        const nextRecord = records[records.indexOf(record) + comboRecords.length];
+        if (nextRecord?.data.product_type === 'combo'){
+            return getComboRecords(records, nextRecord);
+        }
+        return nextRecord ? [nextRecord] : false;
     }
 
     isCombo(record) {
