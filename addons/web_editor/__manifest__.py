519dc558344653ce766c6a4227486926a547bb19
David Monjoie <dmo@odoo.com>
2025-09-05 15:46:10 +0200

    [MOV] html_editor,web_editor: move vkbeautify to html_editor

--- addons/web_editor/__manifest__.py
+++ addons/web_editor/__manifest__.py
@@ -204,7 +204,6 @@ Odoo Web Editor widget.
         ],
         'web.assets_backend': [
             'web_editor/static/src/js/editor/odoo-editor/src/base_style.scss',
-            'web_editor/static/lib/vkbeautify/**/*',
             'web_editor/static/src/js/common/**/*',
             'web_editor/static/src/js/editor/odoo-editor/src/utils/utils.js',
             'web_editor/static/src/js/wysiwyg/fonts.js',
