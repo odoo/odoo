98ad526763b30ed60bd17f4c4f103db90e2e9cbc
Alessandro Lupo <alup@odoo.com>
2025-09-08 10:38:48 +0200

    [REF] website_mass_mailing: refactor `fix_newsletter_list_class` as interaction
    This commit converts the public widget `fix_newsletter_list_class` to interaction.
    
    task-4367641
    
    closes odoo/odoo#225846
    
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/website_mass_mailing/static/src/interactions/fix_newsletter_list_class.edit.js
+++ addons/website_mass_mailing/static/src/interactions/fix_newsletter_list_class.edit.js
@@ -1,21 +1,28 @@
-import publicWidget from "@web/legacy/js/public/public_widget";
+import { Interaction } from "@web/public/interaction";
+import { registry } from "@web/core/registry";
 
 /**
- * This widget tries to fix snippets that were malformed because of a missing
+ * This interaction tries to fix snippets that were malformed because of a missing
  * upgrade script. Without this, some newsletter snippets coming from users
  * upgraded from a version lower than 16.0 may not be able to update their
  * newsletter block.
  *
  * TODO an upgrade script should be made to fix databases and get rid of this.
  */
-publicWidget.registry.fixNewsletterListClass = publicWidget.Widget.extend({
-    selector: '.s_newsletter_subscribe_form:not(.s_subscription_list), .s_newsletter_block',
 
-    /**
-     * @override
-     */
-    start() {
-        this.$target[0].classList.add('s_newsletter_list');
-        return this._super(...arguments);
-    },
-});
+export class fixNewsletterListClass extends Interaction {
+    static selector = ".s_newsletter_subscribe_form:not(.s_subscription_list), .s_newsletter_block";
+    dynamicContent = {
+        _root: {
+            "t-att-class": () => ({
+                s_newsletter_list: true,
+            }),
+        },
+    };
+}
+
+registry
+    .category("public.interactions.edit")
+    .add("website_mass_mailing.fix_newsletter_list_class", {
+        Interaction: fixNewsletterListClass,
+    });
