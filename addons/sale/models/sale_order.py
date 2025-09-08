c68159f7e594424b4adf381d9d310524191c3abb
Nisarg (nipl) <nipl@odoo.com>
2025-09-08 12:12:23 +0530

    [FIX] sale: fix SO to Invoice (sub)sections flow
    Steps to Reproduce:
    - Install Sale and create a nested (sub)section structure as shown:
      - Section1
      - Note1
      - Subsection11
         - Invoicable line
      - Subsection12
         - Invoicable line
    - Confirm the SO and generate an invoice
    - You’ll notice that Section1 and Note1 are missing on the invoice, even though
      they should appear since at least one subsection contained an invoicable line.
    
    Issue:
    - Sections and notes should be invoiced if at least one of their direct or
      indirect children is invoicable.
    
    Root Cause:
    - `_get_invoiceable_lines` does not treat subsections as children of sections.
    
    Fix:
    - In addition to tracking `section_line_ids`, also keep track of
      `subsection_line_ids`.
    - These subsection lines are appended alongside their parent section once an
      invoicable line is encountered.
    
    Affected Version-19.0
    task-5009037

--- addons/sale/models/sale_order.py
+++ addons/sale/models/sale_order.py
@@ -1449,11 +1449,16 @@ class SaleOrder(models.Model):
         down_payment_line_ids = []
         invoiceable_line_ids = []
         section_line_ids = []
+        subsection_line_ids = []
         precision = self.env['decimal.precision'].precision_get('Product Unit')
 
         for line in self.order_line:
-            if line.display_type in ('line_section', 'line_subsection'):
+            if line.display_type == 'line_section':
                 section_line_ids = [line.id]  # Start a new section.
+                subsection_line_ids = []
+                continue
+            if line.display_type == 'line_subsection':
+                subsection_line_ids = [line.id]  # Start a new subsection.
                 continue
             if line.display_type != 'line_note' and float_is_zero(line.qty_to_invoice, precision_digits=precision):
                 continue
@@ -1463,12 +1468,23 @@ class SaleOrder(models.Model):
                     # at the end of the invoice, in a specific dedicated section.
                     down_payment_line_ids.append(line.id)
                     continue
-                if section_line_ids:
+                # If the invoicable line is under subsection
+                if subsection_line_ids:
+                    if line.display_type:
+                        subsection_line_ids.append(line.id)
+                        continue
+                    # Extend the subsection lines too if altleast one invoicable line is under subsection
+                    invoiceable_line_ids.extend(section_line_ids + subsection_line_ids)
+                    subsection_line_ids = []
+                    section_line_ids = []
+                # If the invoicable line is under section
+                elif section_line_ids:
                     if line.display_type:
                         section_line_ids.append(line.id)
                         continue
                     invoiceable_line_ids.extend(section_line_ids)
                     section_line_ids = []
+                    subsection_line_ids = []
                 invoiceable_line_ids.append(line.id)
 
         return self.env['sale.order.line'].browse(invoiceable_line_ids + down_payment_line_ids)
