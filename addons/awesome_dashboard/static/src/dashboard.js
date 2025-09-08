caeff342e18b95dc695c5072c03512498021660b
Géry Debongnie <ged@odoo.com>
2025-09-08 11:07:59 +0200

    [COMMIT 4] load and display data with a rpc

--- addons/awesome_dashboard/static/src/dashboard.js
+++ addons/awesome_dashboard/static/src/dashboard.js
@@ -1,12 +1,19 @@
 import { Layout } from "@web/search/layout";
-import { Component } from "@odoo/owl";
+import { Component, onWillStart } from "@odoo/owl";
 import { registry } from "@web/core/registry";
-import { Counter } from "./counter";
 import { DashboardItem } from "./dashboard_item";
+import { rpc } from "@web/core/network/rpc";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Layout, Counter, DashboardItem };
+    static components = { Layout, DashboardItem };
+
+    setup() {
+        this.statistics = false;
+        onWillStart(async () => {
+            this.statistics = await rpc("/awesome_dashboard/statistics");
+        });
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
