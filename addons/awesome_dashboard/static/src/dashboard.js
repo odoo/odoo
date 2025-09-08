c8cf1ac0c779decc651257aa477220973523d236
Géry Debongnie <ged@odoo.com>
2025-09-08 11:03:17 +0200

    [COMMIT 3] add DashboardItem

--- addons/awesome_dashboard/static/src/dashboard.js
+++ addons/awesome_dashboard/static/src/dashboard.js
@@ -2,10 +2,11 @@ import { Layout } from "@web/search/layout";
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Counter } from "./counter";
+import { DashboardItem } from "./dashboard_item";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Layout, Counter };
+    static components = { Layout, Counter, DashboardItem };
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
