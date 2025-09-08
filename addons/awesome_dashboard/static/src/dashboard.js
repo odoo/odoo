53878044dce0e5d8e1d4caddf7b3275a316da22b
Géry Debongnie <ged@odoo.com>
2025-09-08 10:59:13 +0200

    [COMMIT 2] use a Layout Component

--- addons/awesome_dashboard/static/src/dashboard.js
+++ addons/awesome_dashboard/static/src/dashboard.js
@@ -1,10 +1,11 @@
+import { Layout } from "@web/search/layout";
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Counter } from "./counter";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Counter };
+    static components = { Layout, Counter };
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
