ac76c82c9a8b8c291351110949ec2a4bf5178304
manv-afk <manv@odoo.com>
2025-09-08 10:44:56 +0000

    [FIX] point_of_sale: fix validate button should disabling on mobile
    - Fix issue where the `Validate` button (in the payment screen) was not correctly disabled on mobile devices when no payment methods was selected.
    
    closes odoo/odoo#225975
    
    Task-id: 5072759
    X-original-commit: 9c2e1c6542ee3ac6121844bee49481566e45a10c
    Related: odoo/enterprise#94132
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Manu Vaillant (manv) <manv@odoo.com>

--- addons/point_of_sale/static/tests/pos/tours/utils/product_screen_util.js
+++ addons/point_of_sale/static/tests/pos/tours/utils/product_screen_util.js
@@ -6,6 +6,7 @@ import * as TextInputPopup from "@point_of_sale/../tests/generic_helpers/text_in
 import * as Dialog from "@point_of_sale/../tests/generic_helpers/dialog_util";
 import * as Chrome from "@point_of_sale/../tests/pos/tours/utils/chrome_util";
 import { queryFirst, waitFor } from "@odoo/hoot-dom";
+import * as PaymentScreen from "@point_of_sale/../tests/pos/tours/utils/payment_screen_util";
 
 export function firstProductIsFavorite(name) {
     return [
@@ -706,18 +707,7 @@ export function closePos() {
 
 export function finishOrder() {
     return [
-        {
-            isActive: ["desktop"],
-            content: "validate the order",
-            trigger: ".payment-screen .button.next.highlight:visible",
-            run: "click",
-        },
-        {
-            isActive: ["mobile"],
-            content: "validate the order",
-            trigger: ".payment-screen .btn-switchpane:contains('Validate')",
-            run: "click",
-        },
+        ...PaymentScreen.clickValidate(),
         Chrome.isSyncStatusConnected(),
         {
             isActive: ["desktop"],
