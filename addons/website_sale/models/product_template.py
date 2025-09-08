31bc76eb86843155a38ae28a16a9366446bd6003
Victor Feyens <vfe@odoo.com>
2025-09-08 14:19:57 +0200

    [FIX] website_sale: prune invalid values in product page get params
    Attributes values selected by user (and provided as search params) could
    contain values not available on the given product template. We only know
    that one of the searched attribute values is present on the template and
    only the first one should be specified in the query parameters, as other
    would only be ignored and potentially confuse the customer (or any bot
    crawling the website, leading to a poorer SEO).
    
    Fixes the logic introduced by 98546fa530717b4f430a89647164b84421dca080,
    wrongly considering all the searched attribute values in the product
    page url.
    
    closes odoo/odoo#225949
    
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/website_sale/models/product_template.py
+++ addons/website_sale/models/product_template.py
@@ -1081,9 +1081,24 @@ class ProductTemplate(models.Model):
     def _allow_publish_rating_stats(self):
         return True
 
-    def _get_product_url(self, category=None, query_params=None):
+    def _get_product_url(self, category=None, query_params=None, grouped_attributes_values=None):
         self.ensure_one()
         slug = self.env['ir.http']._slug
-        base_url = (category and f"/shop/{slug(category)}/{slug(self)}") or self.website_url
-        query_string = (query_params and urls.url_encode(query_params)) or ''
-        return (query_string and f"{base_url}?{query_string}") or base_url
+
+        url = (category and f'/shop/{slug(category)}/{slug(self)}') or self.website_url
+
+        query_params = query_params or {}
+        if grouped_attributes_values:
+            product_grouped_values = self.attribute_line_ids.value_ids.grouped('attribute_id')
+            available_pav_ids = [
+                next(v.id for v in pavs if v in product_grouped_values[pa])
+                for pa, pavs in grouped_attributes_values.items()
+                if pa in product_grouped_values
+            ]
+            available_pav_ids.sort()
+            query_params['attribute_values'] = ','.join(str(i) for i in available_pav_ids)
+
+        if query_params:
+            url = f'{url}?{urls.url_encode(query_params)}'
+
+        return url
