8be2be1303aac31c1a4b5306b438760ba5dae414
Adnan Saiyed <adsa@odoo.com>
2025-09-09 09:00:35 +0000

    [FIX] html_editor: fix ctrl + backspace behavior
    Current behavior before commit:
    
    In Firefox or Safari, `<p>abc def</p><p>[]<br></p>` => `ctrl + backspace`
    ends up with `<p>abc []</p>` which is different o/p than Chrome
    (`<p>abc def[]</p>`).
    This happens because Firefox's Selection.modify("extend",
    "backward|forward", "word") behaves differently than Chrome when
    the cursor is at the start or end of a block (or in an empty block).
    This behavior breaks the output when pressing ctrl + backspace.
    
    Desired bahavior after coommit:
    
    This commit ensures that in such case deletion behavior is same
    across browsers as Chrome.
    
    task-5055135
    
    closes odoo/odoo#228248
    
    X-original-commit: 57c822f1b3e0471772b5cc225aa148c6efb4f1b9
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Adnan Saiyed (adsa) <adsa@odoo.com>

--- addons/html_editor/static/src/core/delete_plugin.js
+++ addons/html_editor/static/src/core/delete_plugin.js
@@ -268,6 +268,22 @@ export class DeletePlugin extends Plugin {
 
     getRangeForDelete(node, offset, direction, granularity) {
         let destContainer, destOffset;
+        if (granularity === "word") {
+            // In some browsers such as Firefox or Safari, if the cursor
+            // is at the start of a block (when direction is "backward") or
+            // at the end of a block (when direction is "forward"),
+            // Selection.modify("extend", direction, "word") ends up
+            // selecting the previous or next adjacent text node, respectively.
+            // To handle such cases, the granularity should be "character".
+            const blockEl = closestBlock(node);
+            if (
+                (direction === "backward" &&
+                    this.isCursorAtStartOfElement(blockEl, node, offset)) ||
+                (direction === "forward" && this.isCursorAtEndOfElement(blockEl, node, offset))
+            ) {
+                granularity = "character";
+            }
+        }
         switch (granularity) {
             case "character":
                 [destContainer, destOffset] = this.findAdjacentPosition(node, offset, direction);
