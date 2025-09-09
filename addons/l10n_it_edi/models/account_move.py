f9ff907418b37d46a17c59ef496756c4a3be86f9
Paolo Gatti <pgi@odoo.com>
2025-09-09 11:17:50 +0000

    [FIX] l10n_it*: Enasarco is both withholding and pension fund
    * = l10n_it, l10n_it_edi, l10n_it_edi_withholding
    
    The ENASARCO tax works both as a Withholding (negative) tax and as a
    Pension Fund tax. It must appear in the XML in both sections
    DatiRitenuta and CassaPrevidenziale.
    
    - We modified the master data to amend this error.
      Now the correct Withholding type appears right after installing the module.
    - Since there was an explicit check on taxes being both, we removed it.
    - Removed an exception where ENASARCO taxes were allowed to have positive values
    
    X-original-commit: b63f07df6ac57b43946853c55c38b49c32a81b0a

--- addons/l10n_it_edi/models/account_move.py
+++ addons/l10n_it_edi/models/account_move.py
@@ -1440,18 +1440,27 @@ class AccountMove(models.Model):
         return errors
 
     def _l10n_it_edi_export_taxes_check(self):
-        if move_lines := self.mapped("invoice_line_ids").filtered(lambda line:
+        return self._l10n_it_edi_check_lines_for_tax_kind('vat', _('VAT'))
+
+    def _l10n_it_edi_check_lines_for_tax_kind(self, kind_code, kind_desc, min_len=1):
+        assert min_len in (0, 1)
+        if self.invoice_line_ids.filtered(lambda line:
             line.display_type == 'product'
-            and len(line.tax_ids.flatten_taxes_hierarchy()._l10n_it_filter_kind('vat')) != 1
+            and not (min_len <= len(line.tax_ids._l10n_it_filter_kind(kind_code)) <= 1)
         ):
             return {
-                'l10n_it_edi_move_only_one_vat_tax_per_line': {
-                    'message': _("Invoices must have exactly one VAT tax set per line."),
+                f'l10n_it_edi_move_{kind_code}_tax_per_line': {
+                    'message': _(
+                        "Invoices must have %(number)s one %(kind)s tax set per line.",
+                        number=_("exactly") if min_len == 1 else _("at most"),
+                        kind=kind_desc
+                    ),
                     **({
                         'action_text': _("View invoice(s)"),
-                        'action': move_lines.mapped("move_id")._get_records_action(name=_("Check taxes on invoice lines")),
+                        'action': self._get_records_action(name=_("Check taxes on invoice lines")),
                     } if len(self) > 1 else {})
-                }}
+                }
+            }
         return {}
 
     def _l10n_it_edi_get_formatters(self):
