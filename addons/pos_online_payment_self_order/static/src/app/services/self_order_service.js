610ee3dc5d8e4c2f5b50df8469c1b6b766c327f7
Pedram (PEBR) <pebr@odoo.com>
2025-09-09 08:41:03 +0000

    [FIX] pos_online_payment_self_order: wrong redirect with 2 devices
    Before this commit, if two devices were using the mobile menu, when one
    order was paid through online payment, the other device was redirected
    to the confirmation page, but the payment was not for its order.
    
    Steps to reproduce:
    1. Enable self ordering QR menu + Ordering
    2. Create an online PoS payment method with demo payment provider
    3. Open the QR code menu on 2 devices
    4. On one device, make an order and pay
       => the other device is redirected to the confirmation page
    
    opw-5046207
    
    X-original-commit: f0c1954823ceb61dc6c9073308f78ae5796193d8

--- addons/pos_online_payment_self_order/static/src/app/services/self_order_service.js
+++ addons/pos_online_payment_self_order/static/src/app/services/self_order_service.js
@@ -7,20 +7,34 @@ patch(SelfOrder.prototype, {
         await super.setup(...args);
         this.onlinePaymentStatus = null;
         this.data.connectWebSocket("ONLINE_PAYMENT_STATUS", ({ status, data }) => {
+<<<<<<< fe4c097c342b84b2fd1ba7e93094b4f2427d4e3b:addons/pos_online_payment_self_order/static/src/app/services/self_order_service.js
             if (
                 data["pos.order"].length === 0 ||
                 data["pos.order"][0].uuid !== this.currentOrder.uuid
             ) {
                 return;
             }
+||||||| 178dff30131a93680dfd994fd22b29a766ee9354:addons/pos_online_payment_self_order/static/src/self_order_service.js
+=======
+            // Ignore updates for orders from other devices
+            let order = this.models["pos.order"].find((o) => o.uuid === data["pos.order"][0].uuid);
+            if (!order) {
+                return;
+            }
+>>>>>>> 8f1077c211feeb9ee8676f395ab4ae2a25f91fe3:addons/pos_online_payment_self_order/static/src/self_order_service.js
             this.models.loadData(data, [], false);
             this.onlinePaymentStatus = status;
             this.paymentError = status === "fail";
 
-            const order = this.models["pos.order"].find(
+            order = this.models["pos.order"].find(
                 (o) => o.access_token === data["pos.order"][0].access_token
             );
-            if (status === "success" && !this.currentOrder.access_token && order) {
+            if (
+                status === "success" &&
+                !this.currentOrder.access_token &&
+                order &&
+                order.uuid === this.currentOrder.uuid
+            ) {
                 this.confirmationPage("order", this.config.self_ordering_mode, order.access_token);
             }
         });
