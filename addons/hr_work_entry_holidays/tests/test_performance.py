bc6e35640fe32f3657fe6a76968e096106fbb7e8
KAEI <kaei@odoo.com>
2025-09-09 12:38:47 +0000

    [FIX] hr_work_entry: work entries with different calendar types
    purpose:
    - fix work entries generation for employees with different working schedules types
    - made work entries generation check for employee timezone in case the employee is fully flexible and doesn't have a calendar
    - added method `_get_calendar_at` to get the calendar of the resource at a given date
    - added `test_work_entry_different_calendars` for work entries generation for employees with contracts with different calendar types
    
    Task-id: 5065160
    X-original-commit: 79a559c9741410ad861c107e395b2fc486da95e8

--- addons/hr_work_entry_holidays/tests/test_performance.py
+++ addons/hr_work_entry_holidays/tests/test_performance.py
@@ -32,7 +32,7 @@ class TestWorkEntryHolidaysPerformance(TestWorkEntryHolidaysBase):
         self.richard_emp.generate_work_entries(date(2018, 1, 1), date(2018, 1, 2))
         leave = self.create_leave(datetime(2018, 1, 1, 7, 0), datetime(2018, 1, 1, 18, 0))
 
-        with self.assertQueryCount(__system__=109, admin=110):
+        with self.assertQueryCount(__system__=112, admin=113):
             leave.action_validate()
         leave.action_refuse()
 
@@ -41,14 +41,14 @@ class TestWorkEntryHolidaysPerformance(TestWorkEntryHolidaysBase):
     def test_performance_leave_write(self):
         leave = self.create_leave(datetime(2018, 1, 1, 7, 0), datetime(2018, 1, 1, 18, 0))
 
-        with self.assertQueryCount(__system__=10, admin=10):
+        with self.assertQueryCount(__system__=12, admin=12):
             leave.date_to = datetime(2018, 1, 1, 19, 0)
         leave.action_refuse()
 
     @users('__system__', 'admin')
     @warmup
     def test_performance_leave_create(self):
-        with self.assertQueryCount(__system__=53, admin=53):
+        with self.assertQueryCount(__system__=56, admin=56):
             leave = self.create_leave(datetime(2018, 1, 1, 7, 0), datetime(2018, 1, 1, 18, 0))
         leave.action_refuse()
 
