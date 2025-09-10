6b84ee6d5112ceaa42ad49216ff91890704ce0eb
Anita (anko) <anko@odoo.com>
2025-09-10 14:03:59 +0000

    [FIX] payment_mercado_pago: show access token
    Enable old account configuration for new users.
    
    OAuth was not configured yet on Odoo side, which means that new users
    couldn't connect their account. Access token is now visible and editable
    for all users.
    
    X-original-commit: c173204649321ab359b4dc09477aadb5c743d301
    Part-of: odoo/odoo#227035
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>
    Signed-off-by: anko-odoo <anko@odoo.com>

--- addons/payment_mercado_pago/models/payment_provider.py
+++ addons/payment_mercado_pago/models/payment_provider.py
@@ -35,6 +35,8 @@ class PaymentProvider(models.Model):
         required_if_provider='mercado_pago',
         copy=False,
     )
+    # TODO anko remove in 19.1
+    mercado_pago_is_oauth_supported = fields.Boolean(compute='_compute_mercado_pago_is_oauth_supported')
 
     # OAuth fields
     mercado_pago_access_token = fields.Char(
@@ -76,6 +78,10 @@ class PaymentProvider(models.Model):
                 active_test=False,
             ).search([('name', '=', currency_code)], limit=1)
             provider.available_currency_ids = [Command.set(currency.ids)]
+
+    def _compute_mercado_pago_is_oauth_supported(self):
+        """Return current state of OAuth support by Odoo. To be removed in future versions."""
+        self.mercado_pago_is_oauth_supported = False
 
     # === CONSTRAINT METHODS === #
 
