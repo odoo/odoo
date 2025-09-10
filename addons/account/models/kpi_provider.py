7b7f5f8efac8dcfc8f5dea49c10c590d938277e7
Paul Morelle <pmo@odoo.com>
2025-09-11 00:17:31 +0200

    [FIX] account: make get_account_kpi_summary include moves to check
    The kpi.provider:get_account_kpi_summary method should count draft moves
    by category, but also include posted moves that still are to be checked
    by the accountant.
    
    The categories should not be the move_type, but the journal_type.
    
    closes odoo/odoo#226411
    
    Task-id: 5062431
    Signed-off-by: Brice Bartoletti (bib) <bib@odoo.com>

--- addons/account/models/kpi_provider.py
+++ addons/account/models/kpi_provider.py
@@ -6,21 +6,31 @@ class KpiProvider(models.AbstractModel):
 
     @api.model
     def get_account_kpi_summary(self):
-        AccountMove = self.env['account.move']
-        grouped_draft_moves = AccountMove._read_group([('state', '=', 'draft')], ['move_type'], ['move_type'])
+        grouped_moves_to_report = self.env['account.move']._read_group([
+            '|', ('state', '=', 'draft'),
+            '&', ('state', '=', 'posted'), ('to_check', '=', True),
+        ], ['journal_id'], ['journal_id'])
 
         FieldsSelection = self.env['ir.model.fields.selection'].with_context(lang=self.env.user.lang)
-        move_type_names = {x.value: x.name for x in FieldsSelection.search([
-            ('field_id.model', '=', 'account.move'),
-            ('field_id.name', '=', 'move_type'),
+        journal_type_names = {x.value: x.name for x in FieldsSelection.search([
+            ('field_id.model', '=', 'account.journal'),
+            ('field_id.name', '=', 'type'),
         ])}
 
+        journals = self.env['account.journal'].browse(x['journal_id'][0] for x in grouped_moves_to_report)
+        journal_type_by_journal_id = dict(journals.mapped(lambda j: (j.id, j.type)))
+        count_by_type = {}
+        for group in grouped_moves_to_report:
+            journal_id = group['journal_id'][0]
+            journal_type = journal_type_by_journal_id[journal_id]
+            count_by_type[journal_type] = count_by_type.get(journal_type, 0) + group['journal_id_count']
+
         return [{
-            'id': f'account_move_type.{group["move_type"]}',
-            'name': move_type_names[group['move_type']],
+            'id': f'account_journal_type.{journal_type}',
+            'name': journal_type_names[journal_type],
             'type': 'integer',
-            'value': group['move_type_count'],
-        } for group in grouped_draft_moves]
+            'value': count,
+        } for journal_type, count in count_by_type.items()]
 
     @api.model
     def get_kpi_summary(self):
