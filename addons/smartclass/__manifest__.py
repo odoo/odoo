7235dcf25b930100720518c4d05eb7dbc2c9a678
Pierre Pulinckx <pipu@odoo.com>
2025-09-10 14:47:19 +0200

    solution

--- addons/smartclass/__manifest__.py
+++ addons/smartclass/__manifest__.py
@@ -10,6 +10,12 @@
         "web.assets_backend": [
             "smartclass/static/src/**",
         ],
+        'web.assets_tests': [
+            'smartclass/static/tests/tours/**/*',
+        ],
+        'web.assets_unit_tests': [
+            'smartclass/static/tests/**/*',
+        ],
     },
     "application": True,
     'installable': True,
