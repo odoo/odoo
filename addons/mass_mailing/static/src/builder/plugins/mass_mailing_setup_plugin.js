36637649a9b7b473a30eb78c584dde4a8fe9e506
abd-msyukyu-odoo <abd@odoo.com>
2025-09-10 08:00:10 +0200

    [IMP] mass_mailing: improve onboarding in an empty email
    Ensure that a user typing directly in the `o_editable` element will not result
    in a corrupted content, and that the UX for an empty email hints at the user
    what to do.
    
    This results in a better experience when a user uses the `empty theme` for a new
    mailing.
    
    task-5078825
    
    Part-of: odoo/odoo#226224
    Signed-off-by: Damien Abeloos (abd) <abd@odoo.com>
    Co-authored-by: Damien Abeloos <abd@odoo.com>
    Co-authored-by: Thomas Josse <thjo@odoo.com>

--- addons/mass_mailing/static/src/builder/plugins/mass_mailing_setup_plugin.js
+++ addons/mass_mailing/static/src/builder/plugins/mass_mailing_setup_plugin.js
@@ -1,4 +1,5 @@
 import { Plugin } from "@html_editor/plugin";
+import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
 
 export class MassMailingSetupPlugin extends Plugin {
@@ -9,7 +10,23 @@ export class MassMailingSetupPlugin extends Plugin {
             "mass_mailing.assets_iframe_style",
             "mass_mailing.iframe_add_dialog",
         ],
+        clean_for_save_handlers: this.cleanForSave.bind(this),
+        powerbox_blacklist_selectors: ".o_mail_wrapper_td",
     };
+
+    setup() {
+        const wrapperTd = this.editable.querySelector(".o_mail_wrapper_td");
+        wrapperTd?.classList.add("oe_empty");
+        wrapperTd?.setAttribute("data-editor-message-default", true);
+        wrapperTd?.setAttribute("data-editor-message", _t("DRAG BUILDING BLOCKS HERE"));
+    }
+
+    cleanForSave({ root }) {
+        const wrapperTd = root.querySelector(".o_mail_wrapper_td.oe_empty");
+        wrapperTd?.classList.remove("oe_empty");
+        wrapperTd?.removeAttribute("data-editor-message-default");
+        wrapperTd?.removeAttribute("data-editor-message");
+    }
 }
 
 registry.category("mass_mailing-plugins").add(MassMailingSetupPlugin.id, MassMailingSetupPlugin);
