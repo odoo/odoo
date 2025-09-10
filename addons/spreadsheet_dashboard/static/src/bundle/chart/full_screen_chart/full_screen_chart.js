3d949cf7ea056203bdb00828e2457266c455e968
Adrien Minne (adrm) <adrm@odoo.com>
2025-09-10 14:23:05 +0200

    [IMP] carousels: improve behaviour with with chart menu
    With this commit:
    - when a chart is linked to an odoo menu, only the chart is now
      clickable, and not the whole carousel
    - since now the odoo menu click is handled in the chart component
      (and not the figure), we don't have to do a separate patch for
      full screen charts
    
    Task: 5078831
    Part-of: odoo/odoo#230257
    Signed-off-by: Adrien Minne (adrm) <adrm@odoo.com>

--- addons/spreadsheet_dashboard/static/src/bundle/chart/full_screen_chart/full_screen_chart.js
+++
@@ -1,8 +0,0 @@
-import { components } from "@odoo/o-spreadsheet";
-import { patch } from "@web/core/utils/patch";
-
-patch(components.FullScreenChart.prototype, {
-    get hasOdooMenu() {
-        return this.chartId && this.env.model.getters.getChartOdooMenu(this.chartId) !== undefined;
-    },
-});
