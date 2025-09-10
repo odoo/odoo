5670c36079c33b364b25e4f6f65d89b70fea6c9c
rusp-odoo <rusp@odoo.com>
2025-09-10 13:14:40 +0000

    [REF] html_builder, web, website: replace WebsiteUrlPicker
    When website is installed, all URL pickers should suggest existing
    website pages and anchors.
    
    Before this commit, the `WebsiteUrlPicker` used `autocompleteWithPages`
    to suggest internal links.
    
    This commit removes the usage of `autocompleteWithPages` and
    replace `WebsiteUrlPicker` with `BuilderUrlPicker` with an Autocomplete
    component completely to provide internal link and anchor
    suggestions.
    
    Key Changes:
    - Introduce `AutoCompleteBuilderUrlPicker` component extending the base
      `AutoComplete` component.
    - Patch BuilderUrlPicker to:
      - Replace its input with AutoCompleteBuilderUrlPicker.
      - Provide a `sources` getter delegating suggestions to
        loadOptionsSource.
      - Handle selection vs input through `isOptionSelected`.
    - Extract and reuse loadOptionsSource in website utils.
    - Move `loadOptionsSource` to utils.
    - Add `title` prop and `isOptionSelected` flag to AutoComplete to
      properly differentiate selection vs input.
    
    task-5260613
    
    X-original-commit: 8582880eaf5732cbd37fd4dfc38c3063a91c371d

--- addons/web/static/src/core/autocomplete/autocomplete.js
+++ addons/web/static/src/core/autocomplete/autocomplete.js
@@ -23,6 +23,7 @@ export class AutoComplete extends Component {
             },
         },
         placeholder: { type: String, optional: true },
+        title: { type: String, optional: true },
         autocomplete: { type: String, optional: true },
         autoSelect: { type: Boolean, optional: true },
         resetOnSelect: { type: Boolean, optional: true },
@@ -45,6 +46,7 @@ export class AutoComplete extends Component {
     static defaultProps = {
         value: "",
         placeholder: "",
+        title: "",
         autocomplete: "new-password",
         autoSelect: false,
         dropdown: true,
@@ -357,6 +359,7 @@ export class AutoComplete extends Component {
         }
         this.props.onChange({
             inputValue: this.inputRef.el.value,
+            isOptionSelected: this.ignoreBlur,
         });
     }
     async onInput() {
