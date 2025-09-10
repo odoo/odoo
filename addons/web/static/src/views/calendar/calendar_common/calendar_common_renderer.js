55a829015db20e2017c280e30fab11286e17ee16
Michaël Mattiello <mcm@odoo.com>
2025-09-10 11:38:25 +0200

    [IMP] web: remove calendar attribute "date_delay"
    This commit removes the attribute "date_delay" from calendar view arch.
    This attribute was "mandatory" if the attribute "date_stop" was not set.
    Now, the attribute "date_stop" is completely optional. If not given, we
    consider the "date_stop" to be "date_start". With these two attributes
    ("date_start" and "date_stop") we can calculate the duration
    ("date_delay").
    
    task-4609678

--- addons/web/static/src/views/calendar/calendar_common/calendar_common_renderer.js
+++ addons/web/static/src/views/calendar/calendar_common/calendar_common_renderer.js
@@ -333,7 +333,7 @@ export class CalendarCommonRenderer extends Component {
     }
     onEventDrop(info) {
         this.fc.api.unselect();
-        this.props.model.updateRecord(this.fcEventToRecord(info.event), { moved: true });
+        this.props.model.updateRecord(this.fcEventToRecord(info.event));
     }
     onEventResize(info) {
         this.fc.api.unselect();
