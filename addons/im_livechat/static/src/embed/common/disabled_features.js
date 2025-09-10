13cf868ee0adeb7b0ea1b8d88a6bd1668938294b
tsm-odoo <tsm@odoo.com>
2025-09-10 16:18:38 +0200

    [FIX] im_livechat: allow to open meeting chat on embed live chat
    The embed live chat has a subset of allowed thread actions. The
    meeting chat action was added but not added to the allowed thread
    actions.
    
    In the future, we will allow most of the thread actions, at least
    the ones allowed for guests. Environment should not impact feature
    availability.
    
    However for now, let's just enable the missing action.
    
    closes odoo/odoo#226553
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/im_livechat/static/src/embed/common/disabled_features.js
+++ addons/im_livechat/static/src/embed/common/disabled_features.js
@@ -12,7 +12,13 @@ patch(Thread.prototype, {
     },
 });
 
-const allowedThreadActions = new Set(["fold-chat-window", "close", "restart", "call-settings"]);
+const allowedThreadActions = new Set([
+    "fold-chat-window",
+    "close",
+    "restart",
+    "call-settings",
+    "meeting-chat",
+]);
 for (const [actionName] of threadActionsRegistry.getEntries()) {
     if (!allowedThreadActions.has(actionName)) {
         threadActionsRegistry.remove(actionName);
