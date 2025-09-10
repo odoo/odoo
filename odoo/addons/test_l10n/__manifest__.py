82e813e0b79cd0e7d44d0ec05d092feac2b818c7
william-andre <wan@odoo.com>
2025-09-10 13:29:05 +0200

    [IMP] account,test_l10n: test loading the l10n CoA only post install
    * do not require demo data anymore to test l10n
    * move as much as possible from the load from at_install to post_install
    * allow making a common setup for multiple generic tests having to run
      on all the countries, for instance testing/opening all the reports
      related to the country
    * move in a module-agnostic module: l10n is not only accounting anymore
    
    This should allow reducing the incompressible time (at_install with demo) from often almost 1h to around 10 minutes. The rest can be run in parallel post install, speeding up the staging process.
    
    closes odoo/odoo#230835
    
    Related: odoo/enterprise#96766
    Signed-off-by: Xavier Dollé (xdo) <xdo@odoo.com>

---
+++ odoo/addons/test_l10n/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Test Localization',
+    'version': '0.1',
+    'category': 'Hidden/Tests',
+    'description': """A module to test all localizations.""",
+    'depends': ['base'],
+    'installable': True,
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
