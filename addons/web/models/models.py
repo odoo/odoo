63457cc6d004fbe32db0d9cb73e5630e6113e202
Krzysztof Magusiak (krma) <krma@odoo.com>
2025-09-10 11:43:12 +0000

    [IMP] base, web: remove SELF_READABLE_FIELDS and SELF_WRITEABLE_FIELDS
    We change field write access so that by default no field is writeable on
    `res.users` except for user managers. Then, we add a field property
    `user_writeable` that indicates that non-manager users can write the
    field (record rules still apply).
    
    **How it used to work:**
    
    Most users could read all users, but write none.
    *Reading*: It is allowed on self through going into sudo when only
    SELF_READABLE_FIELDS are read. If a field is in that list, it can be
    read even if it has groups on it otherwise, default groups apply.
    *Writing*: It is allowed on self through going into sudo when only
    SELF_WRITEABLE_FIELDS are set.
    
    **New approach:**
    
    Record rules indicate which users you can read (most of them) and which
    users you can write (mostly yourself).
    *Reading*: Use the record rules and default field access.
    *Writing*: Use the record rules and default field access. If the user is
    not a manager, allow only on `user_writeable` fields.
    
    **Removed hacks:**
    
    - User overwrites of `onchange`, `read`, `fields_get`.
      Note that reading a field directly could lead to a different
      behaviour.
    - The check of writing the `company_id` field is already defined as a
      constraint.
    - website_profile: usage of accessible fields
    
    task-5107679

--- addons/web/models/models.py
+++ addons/web/models/models.py
@@ -1909,12 +1909,7 @@ class Base(models.AbstractModel):
         env = self.env
         first_call = not field_names
 
-        if not (self and self._name == 'res.users'):
-            # res.users defines SELF_WRITEABLE_FIELDS to give access to the user
-            # to modify themselves, we skip the check in that case because the
-            # user does not have write permission on themselves
-            # TODO update res.users
-            self.check_access('write' if self else 'create')
+        self.check_access('write' if self else 'create')
 
         if any(fname not in self._fields for fname in field_names):
             return {}
