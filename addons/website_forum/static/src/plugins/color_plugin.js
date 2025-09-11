1cab4a71e9938eba8eae9786b51273be9b1496e9
Rodolpho Lima <rcdl@odoo.com>
2025-09-11 16:17:50 +0200

    [REF] website_forum: remove custom color plugin
    Following a refactor in html_editor, in order to remove color features
    in the editor, one can simply remove the ColorSelector plugin (and
    IconColorPlugin, that depends on it).
    
    This allows us to remove the custom ForumColorPlugin.
    
    task-5043928
    
    closes odoo/odoo#226634
    
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/website_forum/static/src/plugins/color_plugin.js
+++
@@ -1,9 +0,0 @@
-import { ColorPlugin } from "@html_editor/main/font/color_plugin";
-
-export class ForumColorPlugin extends ColorPlugin {
-    resources = {
-        ...this.resources,
-        // Remove toolbar buttons
-        toolbar_items: [],
-    };
-}
