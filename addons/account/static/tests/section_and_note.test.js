fa4a5673590f5f3b72322ba0d12439ba2a9c76b0
Nisarg (nipl) <nipl@odoo.com>
2025-09-11 13:34:19 +0000

    [IMP] account: add HOOT tests for collapse_ fields' logic in sections widget
    This commit adds HOOT tests for the recently introduced behavior in the sections
    widget:
    - Logic for Hide Composition and Hide Prices actions.
    - Duplicating sections now also duplicates invisible collapse_ fields.
    - Dragging and dropping under a hidden section resets collapse_ field values for
      subsections.
    
    task-5083152
    
    X-original-commit: 3b2b7653f53f331a9ef1ee0644d2b2b834f39e77

--- addons/account/static/tests/section_and_note.test.js
+++ addons/account/static/tests/section_and_note.test.js
@@ -45,6 +45,8 @@ class InvoiceLine extends models.Model {
     });
     sequence = fields.Integer();
     m2m = fields.Many2many({ relation: "bar" });
+    collapse_composition = fields.Boolean({ default: false });
+    collapse_prices = fields.Boolean({ default: false });
 }
 class Invoice extends models.Model {
     _records = [
@@ -69,6 +71,29 @@ class Bar extends models.Model {
 
 defineModels([Invoice, InvoiceLine, Bar]);
 defineMailModels();
+
+const LINE_COLLAPSE_ARCH = `
+    <form>
+        <field
+            name="invoice_line_ids"
+            widget="section_and_note_one2many"
+            options="{'subsections': True, 'hide_composition': True, 'hide_prices': True}"
+        >
+            <list editable="bottom">
+                <control>
+                    <create name="add_line_control" string="Add a line"/>
+                    <create name="add_section_control" string="Add a section" context="{'default_display_type': 'line_section'}"/>
+                    <create name="add_note_control" string="Add a note" context="{'default_display_type': 'line_note'}"/>
+                </control>
+                <field name="sequence" widget="handle"/>
+                <field name="name"/>
+                <field name="display_type" column_invisible="1"/>
+                <field name="collapse_composition" column_invisible="1"/>
+                <field name="collapse_prices" column_invisible="1"/>
+            </list>
+        </field>
+    </form>
+`;
 
 onRpc("has_group", () => true);
 
@@ -1240,3 +1265,252 @@ test("swap sections and subsections", async () => {
         "C1",
     ]);
 });
+
+test("check collapse_ fields' muting logic for widget", async () => {
+    InvoiceLine._fields.aggregated_field = fields.Float({ default: 3.00 });
+    InvoiceLine._records = [
+        { id: 1, name: "sec1", display_type: "line_section", sequence: 1, m2m: [3], collapse_composition: true },
+        { id: 2, name: "sec1-r1", display_type: false, sequence: 2, m2m: [2, 3] },
+        { id: 3, name: "sec1-sub1", display_type: "line_subsection", sequence: 3, m2m: [1] },
+        { id: 4, name: "sec1-sub1-r1", display_type: false, sequence: 4, m2m: [3] },
+        { id: 5, name: "sec1-sub2", display_type: "line_subsection", sequence: 5, m2m: [1] },
+        { id: 6, name: "sec1-sub2-r1", display_type: false, sequence: 6, m2m: [] },
+        { id: 7, name: "sec2", display_type: "line_section", sequence: 7, m2m: [1], collapse_prices: true },
+        { id: 8, name: "sec2-r1", display_type: false, sequence: 8, m2m: [2, 3] },
+        { id: 9, name: "sec2-r2", display_type: false, sequence: 9, m2m: [3] },
+        { id: 10, name: "sec2-sub1", display_type: "line_subsection", sequence: 10, m2m: [1] },
+        { id: 11, name: "sec2-sub1-r1", display_type: false, sequence: 11, m2m: [3] },
+        { id: 12, name: "sec2-sub2", display_type: "line_subsection", sequence: 12, m2m: [1] },
+        { id: 13, name: "sec2-sub2-r1", display_type: false, sequence: 13, m2m: [] },
+        { id: 14, name: "sec3", display_type: "line_section", sequence: 14, m2m: [1] },
+        { id: 15, name: "sec3-r1", display_type: false, sequence: 15, m2m: [2, 3] },
+        { id: 16, name: "sec3-sub1", display_type: "line_subsection", sequence: 16, m2m: [1], collapse_prices: true },
+        { id: 17, name: "sec3-sub1-r1", display_type: false, sequence: 17, m2m: [3] },
+        { id: 18, name: "sec3-sub2", display_type: "line_subsection", sequence: 18, m2m: [1], collapse_composition: true },
+        { id: 19, name: "sec3-sub2-r1", display_type: false, sequence: 19, m2m: [] },
+    ]
+
+    await mountView({
+        type: "form",
+        resModel: "invoice",
+        resId: 1,
+        arch: `
+            <form>
+                <field
+                    name="invoice_line_ids"
+                    widget="section_and_note_one2many"
+                    options="{'subsections': True, 'hide_composition': True, 'hide_prices': True}"
+                    aggregated_fields="aggregated_field"
+                >
+                    <list editable="bottom">
+                        <control>
+                            <create name="add_line_control" string="Add a line"/>
+                            <create name="add_section_control" string="Add a section" context="{'default_display_type': 'line_section'}"/>
+                            <create name="add_note_control" string="Add a note" context="{'default_display_type': 'line_note'}"/>
+                        </control>
+                        <field name="sequence" widget="handle"/>
+                        <field name="name"/>
+                        <field name="aggregated_field"/>
+                        <field name="display_type" column_invisible="1"/>
+                        <field name="collapse_composition" column_invisible="1"/>
+                        <field name="collapse_prices" column_invisible="1"/>
+                    </list>
+                </field>
+            </form>
+        `,
+    });
+
+    expect(queryAllTexts(".o_data_row .o_list_char")).toEqual([
+        "sec1",
+            "sec1-r1",
+            "sec1-sub1",
+                "sec1-sub1-r1",
+            "sec1-sub2",
+                "sec1-sub2-r1",
+        "sec2",
+            "sec2-r1",
+            "sec2-r2",
+            "sec2-sub1",
+                "sec2-sub1-r1",
+            "sec2-sub2",
+                "sec2-sub2-r1",
+        "sec3",
+            "sec3-r1",
+            "sec3-sub1",
+                "sec3-sub1-r1",
+            "sec3-sub2",
+                "sec3-sub2-r1",
+    ]);
+
+    await contains(".o_list_section_options:first button").click();
+    expect(".o-dropdown-item:contains(Show Composition)").toHaveCount(1, {
+        message: "Sections should always show hide composition button"
+    });
+    expect(".o-dropdown-item:contains(Hide Prices)").toHaveCount(1, {
+        message: "Sections should always show hide prices button"
+    });
+
+    await contains(".o_data_row:contains(sec1-sub1) .o_list_section_options button").click();
+    expect(".o-dropdown-item:contains(Hide Composition)").toHaveClass("disabled", {
+        message: "Subsection under hidden section should have disabled hide composition button"
+    });
+    expect(".o-dropdown-item:contains(Hide Prices)").toHaveClass("disabled", {
+        message: "Subsection under hidden section should have disabled Hide Prices button"
+    });
+
+    expect(".o_data_row:contains(sec1-r1)").toHaveClass("text-muted", {
+        message: "Line under hidden section should be muted"
+    });
+    expect(".o_data_row:contains(sec1-sub1)").toHaveClass("text-muted", {
+        message: "Subsection under hidden section should be muted"
+    });
+    expect(".o_data_row:contains(sec1-sub1-r1)").toHaveClass("text-muted", {
+        message: "Line under subsection(which is under hidden section) should be muted"
+    });
+
+    expect(".o_data_row:contains(sec2-r1) > td[name='aggregated_field']").toHaveClass("text-muted", {
+        message: "Aggregated field column of Line under hidden prices section should be muted"
+    });
+    expect(".o_data_row:contains(sec2-sub1) > td[name='aggregated_field']").toHaveClass("text-muted", {
+        message: "Aggregated field column of Subsection under hidden prices section should be muted"
+    });
+    expect(".o_data_row:contains(sec2-sub1-r1) > td[name='aggregated_field']").toHaveClass("text-muted", {
+        message: "Aggregated field column of Line under subsection(which is under hidden prices section) should be muted"
+    });
+
+    expect(".o_data_row:contains(sec3-sub1-r1) > td[name='aggregated_field']").toHaveClass("text-muted", {
+        message: "Aggregated field column of Line under hidden prices subsection should be muted"
+    });
+    expect(".o_data_row:contains(sec3-sub2-r1)").toHaveClass("text-muted", {
+        message: "Line under hidden subsection should be muted"
+    });
+})
+
+test("check collapse_ fields' duplicating logic", async () => {
+    [2, 5, 8].forEach((i) => {
+        InvoiceLine._records[i].collapse_composition = true;
+        InvoiceLine._records[i].collapse_prices = true;
+    });
+
+    onRpc("web_save", ({ args }) => {
+        expect.step("web_save");
+
+        const [, { invoice_line_ids }] = args;
+
+        // Filter section/subsection records created via (0, 0, values)
+        const createdSectionRecords = invoice_line_ids.filter(([cmd, , values]) =>
+            cmd === 0 && ['line_section', 'line_subsection'].includes(values.display_type)
+        );
+
+        for (const [, , values] of createdSectionRecords) {
+            expect(values.collapse_composition).toBe(true, {
+                message: `collapse_composition should be true for (sub)section ${values.name}`,
+            });
+            expect(values.collapse_prices).toBe(true, {
+                message: `collapse_prices should be true for (sub)section ${values.name}`,
+            });
+        }
+    });
+
+    await mountView({
+        type: "form",
+        resModel: "invoice",
+        resId: 1,
+        arch: LINE_COLLAPSE_ARCH,
+    });
+
+    expect(queryAllTexts(".o_data_row")).toEqual([
+        "r1",
+        "r2",
+        "A",
+            "A1",
+            "A2",
+        "B",
+            "B1",
+            "B2",
+            "Ba",
+                "Ba1",
+                "Ba2",
+        "C",
+            "C1",
+    ]);
+
+    await contains(".o_data_row:contains(A):first .o_list_section_options button").click();
+    await contains(".o-dropdown-item:contains(Duplicate)").click();
+
+    await contains(".o_data_row:contains(B):first .o_list_section_options button").click();
+    await contains(".o-dropdown-item:contains(Duplicate)").click();
+
+    expect(queryAllTexts(".o_data_row")).toEqual([
+        "r1",
+        "r2",
+        "A",
+            "A1",
+            "A2",
+        "A",
+            "A1",
+            "A2",
+        "B",
+            "B1",
+            "B2",
+            "Ba",
+                "Ba1",
+                "Ba2",
+        "B",
+            "B1",
+            "B2",
+            "Ba",
+                "Ba1",
+                "Ba2",
+        "C",
+            "C1",
+    ]);
+
+    await clickSave();
+    expect.verifySteps(["web_save"]);
+})
+
+test("check subsections' collapse_ fields' drag and drop logic", async () => {
+    InvoiceLine._records[2].collapse_composition = true;
+    InvoiceLine._records[8].collapse_composition = true;
+    InvoiceLine._records[8].collapse_prices = true;
+
+    onRpc("web_save", ({ args }) => {
+        expect.step("web_save");
+
+        expect(args[1].invoice_line_ids[0][2].collapse_composition).toBe(false, {
+            message: `collapse_composition should be reset to false for section 'Ba'`,
+        });
+        expect(args[1].invoice_line_ids[0][2].collapse_prices).toBe(false, {
+            message: `collapse_prices should be reset to false for section 'Ba'`,
+        });
+    });
+
+    await mountView({
+        type: "form",
+        resModel: "invoice",
+        resId: 1,
+        arch: LINE_COLLAPSE_ARCH,
+    });
+
+    expect(queryAllTexts(".o_data_row")).toEqual([
+        "r1",
+        "r2",
+        "A",
+            "A1",
+            "A2",
+        "B",
+            "B1",
+            "B2",
+            "Ba",
+                "Ba1",
+                "Ba2",
+        "C",
+            "C1",
+    ]);
+
+    await contains(".o_data_row:contains(Ba):first .o_row_handle").dragAndDrop(".o_data_row:contains(A1):first");
+
+    await clickSave();
+    expect.verifySteps(["web_save"]);
+})
