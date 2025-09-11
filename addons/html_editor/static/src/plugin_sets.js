06e67bc886865d6ee75ef1f4c0ca58b45ebbb624
Rodolpho Lima <rcdl@odoo.com>
2025-09-11 15:33:53 +0200

    [REF] html_editor: split color plugin in two
    This commit splits the current color plugin into two plugins:
    - ColorPlugin: contains the code to get and apply colors to elements or
    selections. This is a technical plugin that other plugins can depend on
    to handle color-related formatting.
    - ColorUIPlugin: provides a user interface for color formatting by
    adding the color selector to the toolbar. This plugin holds the state
    needed for the color selector and its previewable operations.
    
    Other than improving modularity, this split allows us to have an editor
    without the color selector, for the cases where we don't want any color
    formatting by the user.
    
    task-5043928
    
    Part-of: odoo/odoo#226634
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/html_editor/static/src/plugin_sets.js
+++ addons/html_editor/static/src/plugin_sets.js
@@ -23,6 +23,7 @@ import { ChatGPTTranslatePlugin } from "./main/chatgpt/chatgpt_translate_plugin"
 import { ColumnPlugin } from "./main/column_plugin";
 import { EmojiPlugin } from "./main/emoji_plugin";
 import { ColorPlugin } from "./main/font/color_plugin";
+import { ColorUIPlugin } from "./main/font/color_ui_plugin";
 import { FeffPlugin } from "./main/feff_plugin";
 import { FontPlugin } from "./main/font/font_plugin";
 import { FontFamilyPlugin } from "./main/font/font_family_plugin";
@@ -98,6 +99,7 @@ import { ContentEditablePlugin } from "./core/content_editable_plugin";
  *
  * Main
  * @property { import("./main/font/color_plugin").ColorShared } color
+ * @property { import("./main/font/color_ui_plugin").ColorUIShared } colorUi
  * @property { import("./main/link/link_plugin").LinkShared } link
  * @property { import ("./main/link/link_selection_plugin").LinkSelectionShared } linkSelection
  * @property { import ("./main/media/media_plugin").MediaShared } media
@@ -147,6 +149,7 @@ export const MAIN_PLUGINS = [
     BannerPlugin,
     ChatGPTTranslatePlugin,
     ColorPlugin,
+    ColorUIPlugin,
     SeparatorPlugin,
     ColumnPlugin,
     EmojiPlugin,
