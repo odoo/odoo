7091949a9ca49c871a012ce87428e32a2ca3a2c2
Arnold Moyaux <arm@odoo.com>
2025-09-12 15:14:53 +0200

    [IMP] mrp_account: Automatic labor entries for production
    When the product is in realtime accounting and the production location
    has a variation account. The labor expense will be created and posted
    directly as in 18.0
    
    Part-of: odoo/odoo#226899
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/mrp_account/models/mrp_production.py
+++ addons/mrp_account/models/mrp_production.py
@@ -87,3 +87,45 @@ class MrpProduction(models.Model):
         res = super()._get_backorder_mo_vals()
         res['extra_cost'] = self.extra_cost
         return res
+
+    def _post_labour(self):
+        for mo in self:
+            production_location = self.product_id.with_company(self.company_id).property_stock_production
+            if mo.with_company(mo.company_id).product_id.valuation != 'real_time' or not production_location.valuation_account_id:
+                continue
+
+            product_accounts = mo.product_id.product_tmpl_id.get_product_accounts()
+            labour_amounts = defaultdict(float)
+            workorders = defaultdict(self.env['mrp.workorder'].browse)
+            for wo in mo.workorder_ids:
+                account = wo.workcenter_id.expense_account_id or product_accounts['expense']
+                labour_amounts[account] += wo.company_id.currency_id.round(wo._cal_cost())
+                workorders[account] |= wo
+            workcenter_cost = sum(labour_amounts.values())
+
+            if mo.company_id.currency_id.is_zero(workcenter_cost):
+                continue
+
+            desc = _('%s - Labour', mo.name)
+            account = production_location.valuation_account_id
+            labour_amounts[account] -= workcenter_cost
+            account_move = self.env['account.move'].sudo().create({
+                'journal_id': product_accounts['stock_journal'].id,
+                'date': fields.Date.context_today(self),
+                'ref': desc,
+                'move_type': 'entry',
+                'line_ids': [(0, 0, {
+                    'name': desc,
+                    'ref': desc,
+                    'balance': -amt,
+                    'account_id': acc.id,
+                }) for acc, amt in labour_amounts.items()]
+            })
+            account_move._post()
+            for line in account_move.line_ids[:-1]:
+                workorders[line.account_id].time_ids.write({'account_move_line_id': line.id})
+
+    def _post_inventory(self, cancel_backorder=False):
+        res = super()._post_inventory(cancel_backorder=cancel_backorder)
+        self.filtered(lambda mo: mo.state == 'done')._post_labour()
+        return res
