5dc771a4cb457f32bda685e5dcd1956fa87acd7f
alap-odoo <alap@odoo.com>
2025-09-12 11:43:43 +0530

    [IMP] html_builder, *: enhanced social media and share snippets
    *=web, website, website_blog, website_event,  website_hr_recruitment
    
    1. In both the share and social media snippets, conflicting options
    such as size, style, border, round corners, alignment, padding,
    animation, and background color have been removed from the icons.
    Animation and background color options were added only to the
    social media snippet.
    
    2. Added a link style option for social media icons, similar to the
    links option in the theme tab.
    
    3. In the social media snippet, refactored and expanded the available
    icon sizes. Additionally, added a tooltip for the icon color option.
    If this option is disabled, the icons will automatically inherit the
    current text color of the snippet’s container.
    
    4. In the share snippet, the icon sizes are now aligned with those in
    the social media snippet.
    
    5. Replaced the large minus buttons in the social media options with
    smaller ones, consistent with the "Form > Field > Selection" option.
    
    6. Replaced the social media buttons with standard snippets on the
    following pages:
    a. Added "Share" snippet on blog post page (/blog/b-1/p-1).
    b. Added "Social Media" snippet on event list page (/event).
    c. Added "Share" snippet on event page (/event/evt-1/register).
    d. Added "Social Media" snippet on job list page (/jobs).
    
    task-4879376

--- addons/website/static/src/builder/plugins/font_awesome_option_plugin.js
+++ addons/website/static/src/builder/plugins/font_awesome_option_plugin.js
@@ -3,13 +3,17 @@ import { registry } from "@web/core/registry";
 import { ClassAction } from "@html_builder/core/core_builder_action_plugin";
 import { withSequence } from "@html_editor/utils/resource";
 import { FONT_AWESOME } from "@html_builder/utils/option_sequence";
+import { BaseOptionComponent, useDomState } from "@html_builder/core/utils";
+import { BorderConfigurator } from "@html_builder/plugins/border_configurator_option";
 
 class FontAwesomeOptionPlugin extends Plugin {
     static id = "fontAwesomeOptionPlugin";
     resources = {
         builder_options: [
             withSequence(FONT_AWESOME, {
-                template: "website.FontAwesomeOption",
+                // converted to option component to handle rendering of
+                // options based on the context(social media and share icons).
+                OptionComponent: FontAwesomeOptionComponent,
                 selector: "span.fa, i.fa",
                 exclude: "[data-oe-xpath]",
             }),
@@ -26,6 +30,22 @@ export class FaResizeAction extends ClassAction {
         const { editingElement } = context;
         editingElement.classList.remove("fa-1x", "fa-lg");
         super.apply(context);
+    }
+}
+export class FontAwesomeOptionComponent extends BaseOptionComponent {
+    static template = "website.FontAwesomeOption";
+    static components = { BorderConfigurator };
+    setup() {
+        super.setup();
+        this.state = useDomState((editingElement) => {
+            const hasRestrictedClass =
+                editingElement.closest(".s_social_media") || editingElement.closest(".s_share");
+            return {
+                showBackground: !hasRestrictedClass,
+                showBorder: !hasRestrictedClass,
+                showSize: !hasRestrictedClass,
+            };
+        });
     }
 }
 
