35aa52fa06a4eae9131ff32bbd48d46ac732699f
Serhii Rubanskyi <seru@odoo.com>
2025-09-12 11:43:31 +0000

    [FIX] website: remove iframe's flickering after page style change
    In this commit, fallback iframe was added to avoid flickering between
    iframe reloads. But the flicker still happens if we change page's style,
     since we do not apply the new styles to the fallback iframe.
    
    To reproduce the issue:
    - Open Website and start editing
    - Move to the 'Theme' tab
    - Change page layout to 'Boxed'
    - Set background color to not transparent, e.g. red
    - Save, and do any action to reload the iframe, e.g. go to /contactus
    
    => The iframe flickers from the previous color to red, which shouldn't
    be the case.
    Task-4985472
    
    [commit]: github.com/odoo/odoo/commit/1109443
    
    closes odoo/odoo#229286
    
    X-original-commit: 31127f441934b5ef1e34a03189d7a9b0bbbdce52
    Signed-off-by: Benjamin Vray (bvr) <bvr@odoo.com>
    Signed-off-by: Serhii Rubanskyi (seru) <seru@odoo.com>

--- addons/website/static/src/client_actions/website_preview/website_builder_action.js
+++ addons/website/static/src/client_actions/website_preview/website_builder_action.js
@@ -617,11 +617,23 @@ export class WebsiteBuilderClientAction extends Component {
         const websiteDoc = this.websiteContent.el?.contentDocument;
         const fallBackDoc = this.iframefallback.el?.contentDocument;
         if (!this.state.isEditing && websiteDoc && fallBackDoc) {
-            fallBackDoc.body.replaceWith(websiteDoc.body.cloneNode(true));
-            const currentScrollEl = getScrollingElement(websiteDoc);
-            const scrollElement = getScrollingElement(fallBackDoc);
-            scrollElement.scrollTop = currentScrollEl.scrollTop;
-            this.cleanIframeFallback();
+            if (websiteDoc.head) {
+                fallBackDoc.head
+                    .querySelectorAll("link[rel='stylesheet'], style")
+                    .forEach((el) => el.remove());
+                for (const el of websiteDoc.head.querySelectorAll(
+                    "link[rel='stylesheet'], style"
+                )) {
+                    fallBackDoc.head.appendChild(el.cloneNode(true));
+                }
+            }
+            if (websiteDoc.body) {
+                fallBackDoc.body.replaceWith(websiteDoc.body.cloneNode(true));
+                const currentScrollEl = getScrollingElement(websiteDoc);
+                const scrollElement = getScrollingElement(fallBackDoc);
+                scrollElement.scrollTop = currentScrollEl.scrollTop;
+                this.cleanIframeFallback();
+            }
         }
     }
 
