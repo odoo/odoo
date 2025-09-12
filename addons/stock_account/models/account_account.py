aed620a48f2cc9c6e8c10b52e60678bd95ca56fe
Arnold Moyaux <arm@odoo.com>
2025-09-12 16:20:17 +0200

    [FIX] stock_account: Expense account with expense label
    On stock valuation account rename the expense account as expense.
    It will be easier for customer to understand.
    
    closes odoo/odoo#226899
    
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/stock_account/models/account_account.py
+++ addons/stock_account/models/account_account.py
@@ -8,5 +8,5 @@ class AccountAccount(models.Model):
         'account.account', string='Variation Account',
         help="At closing, register the inventory variation of the period into a specific account")
     account_stock_expense_id = fields.Many2one(
-        'account.account', string='Adjustment Account',
+        'account.account', string='Expense Account',
         help="Counterpart used at closing for accounting adjustments to inventory valuation.")
