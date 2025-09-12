45aebb17d50aa5a1d5f4662003f087cefb618091
“Victoria <hav@adhoc.inc>
2025-09-12 14:50:51 +0000

    [IMP] l10n_ar: demo data for internal, national and other taxes
    --amend
    
    closes odoo/odoo#239547
    
    X-original-commit: f795608e679d0b04cf629be901f742358f2f3aef
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>

--- addons/l10n_ar/__manifest__.py
+++ addons/l10n_ar/__manifest__.py
@@ -112,6 +112,7 @@ Master Data:
         'demo/respinsc_demo.xml',
         'demo/res_partner_demo.xml',
         'demo/product_product_demo.xml',
+        'demo/account_tax_demo.xml',
         'demo/account_customer_invoice_demo.xml',
         'demo/account_customer_refund_demo.xml',
         'demo/account_supplier_invoice_demo.xml',
