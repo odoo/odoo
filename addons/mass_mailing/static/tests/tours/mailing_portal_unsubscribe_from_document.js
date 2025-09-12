661317198535b48022bbd895a94fe58d14ddb833
guce <guce@odoo.com>
2025-09-12 12:28:00 +0200

    [IMP] mass_mailing: update mailing subscription page tours
    To accomodate the mailing subscription list page revamp, the tours
    testing the page have to be updated.
    
    Moved into another commit to make the main one smaller, but
    can easily be fixed up.
    
    Co-authored-by: David Beguin <dbe@odoo.com>
    Co-authored-by: Noé Antoine <nan@odoo.com>
    
    task-3546018

--- addons/mass_mailing/static/tests/tours/mailing_portal_unsubscribe_from_document.js
+++ addons/mass_mailing/static/tests/tours/mailing_portal_unsubscribe_from_document.js
@@ -5,80 +5,21 @@ import { registry } from "@web/core/registry";
  * mailing lists). We assume email is not member of any mailing list in this test.
  */
 registry.category("web_tour.tours").add('mailing_portal_unsubscribe_from_document', {
+    // add new steps, as the tour will now begin on another page
     steps: () => [
         {
-            content: "Confirmation unsubscribe is done",
-            trigger: "div#o_mailing_subscription_info span:contains('You are no longer part of our services and will not be contacted again.')",
+            content: "Click on unsubscription button to unsubscribe",
+            trigger: "form button#button_confirm_unsubscribe",
             run: "click",
-        }, {
-            content: "No warning should be displayed",
-            trigger: "div#o_mailing_subscription_form_blocklisted:not(:has(p:contains('You will not receive any news from those mailing lists you are a member of')))",
-            run: "click",
-        }, {
-            content: "Warning will not receive anything anymore",
-            trigger: "div#o_mailing_subscription_form_blocklisted p:contains('You will not hear from us anymore.')",
-            run: "click",
-        }, {
-            content: "Feedback textarea not displayed (see data)",
-            trigger: "div#o_mailing_portal_subscription:not(textarea)",
-            run: "click",
-        }, {
-            content: "Choose 'Other' reason",
-            trigger: "fieldset label:contains('Other')",
-            run: "click",
-        }, {
-            content: "This should display the Feedback area",
-            trigger: "div#o_mailing_portal_subscription textarea",
-        }, {
-            content: "Write feedback reason",
-            trigger: "textarea[name='feedback']",
-            run: "edit My feedback",
-        }, {
-            content: "Hit Send",
-            trigger: "button#button_feedback",
-            run: "click",
-        }, {
-            content: "Confirmation feedback is sent",
-            trigger: "div#o_mailing_subscription_feedback_info span:contains('Sent. Thanks you for your feedback!')",
-            run: "click",
-        }, {
-            content: "Revert exclusion list",
-            trigger: "div#button_blocklist_remove",
-            run: "click",
-        }, {
-            content: "Confirmation exclusion list is removed",
-            trigger: "div#o_mailing_subscription_update_info span:contains('Email removed from our blocklist')",
-            run: "click",
-        }, {
-            content: "Now exclude me (again)",
-            trigger: "div#button_blocklist_add",
-            run: "click",
-        }, {
-            content: "Confirmation exclusion is done",
-            trigger: "div#o_mailing_subscription_update_info span:contains('Email added to our blocklist')",
+            expectUnloadPage: true,
         },
-    ],
-});
-
-
-/*
- * Tour: unsubscribe from a mailing done on documents (aka not on contacts or
- * mailing lists). We assume email is member of mailing lists in this test.
- */
-registry.category("web_tour.tours").add('mailing_portal_unsubscribe_from_document_with_lists', {
-    steps: () => [
         {
             content: "Confirmation unsubscribe is done",
             trigger: "div#o_mailing_subscription_info span:contains('You are no longer part of our services and will not be contacted again.')",
             run: "click",
         }, {
-            content: "Display warning about mailing lists",
-            trigger: "div#o_mailing_subscription_form_blocklisted p:contains('You will not receive any news from those mailing lists you are a member of')",
-            run: "click",
-        }, {
-            content: "Warning should contain reference to memberships",
-            trigger: "div#o_mailing_subscription_form_blocklisted li strong:contains('List1')",
-            run: "click",
+            content: "Warning you will not receive anything anymore",
+            trigger: "div#o_mailing_subscription_form_blocklisted div:contains('You won't receive marketing emails.')",
         }, {
             content: "Feedback textarea not displayed (see data)",
             trigger: "div#o_mailing_portal_subscription:not(textarea)",
@@ -100,8 +41,7 @@ registry.category("web_tour.tours").add('mailing_portal_unsubscribe_from_documen
             run: "click",
         }, {
             content: "Confirmation feedback is sent",
-            trigger: "div#o_mailing_subscription_feedback_info span:contains('Sent. Thanks you for your feedback!')",
-            run: "click",
+            trigger: "div#o_mailing_subscription_feedback_info span:contains('Thank you for your feedback!')",
         }, {
             content: "Revert exclusion list",
             trigger: "div#button_blocklist_remove",
