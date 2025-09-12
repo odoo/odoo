0cae099b37fa0dea1cace5ac96347d44ad177d53
rmbh-odoo <rmbh@odoo.com>
2025-09-12 11:59:00 +0530

    [FIX] spreadsheet_dashboard: enable chart click navigation on mobile
    Before this commit:
    - Clicking on a chart in mobile did nothing,
      Unlike on desktop, where it redirects to the linked Odoo menu.
    
    After this commit:
    - Clicking on a chart in mobile also redirects to the corresponding Odoo menu.
    - The test has been refactored to remove duplication and improve readability.
    
    Task: 4884509

--- addons/spreadsheet_dashboard/static/src/bundle/dashboard_action/mobile_figure_container/mobile_figure_container.js
+++ addons/spreadsheet_dashboard/static/src/bundle/dashboard_action/mobile_figure_container/mobile_figure_container.js
@@ -3,11 +3,15 @@
 import * as spreadsheet from "@odoo/o-spreadsheet";
 
 import { Component, useSubEnv } from "@odoo/owl";
+import { _t } from "@web/core/l10n/translation";
+import { useService } from "@web/core/utils/hooks";
 const { registries } = spreadsheet;
 const { figureRegistry } = registries;
 
 export class MobileFigureContainer extends Component {
     setup() {
+        this.actionService = useService("action");
+        this.notificationService = useService("notification");
         useSubEnv({
             model: this.props.spreadsheetModel,
             isDashboard: () => this.props.spreadsheetModel.getters.isDashboard(),
@@ -33,6 +37,29 @@ export class MobileFigureContainer extends Component {
     isBefore(f1, f2) {
         // TODO be smarter
         return f1.x < f2.x ? f1.y < f2.y : f1.y < f2.y;
+    }
+
+    async navigateToOdooMenu(figureId) {
+        const menu = this.props.spreadsheetModel.getters.getChartOdooMenu(figureId);
+        if (!menu) {
+            this.notificationService.add(
+                _t(
+                    "This chart is not linked to any menu. Please link it to a menu to enable navigation."
+                ),
+                { type: "warning" }
+            );
+            return;
+        }
+        if (!menu.actionID) {
+            this.notificationService.add(
+                _t(
+                    "The menu linked to this chart does not have a corresponding action. Please link the chart to another menu."
+                ),
+                { type: "danger" }
+            );
+            return;
+        }
+        await this.actionService.doAction(menu.actionID);
     }
 }
 
