23cdbea12f874e88c44b6d7129f7084a633d7a1e
Pieter Claeys (clpi) <clpi@odoo.com>
2025-09-12 10:11:42 +0200

    [FIX] mrp_subcontracting: avoid creating ghost SBC MO on update PO qty
    To reproduce:
    - Create SBC BOM for untracked product P, no components
    - Create PO for 5x P and confirm
    - Update the quantity from 5 to 10 and save
    - Go to Inventory, filter on Archived and open Subcontracting
    
    Current behaviour:
    There are two SBC MOs created for P, both with quantity 10.
    
    Expected behaviour:
    There should only be one SBC MO, with quantity 10.
    
    closes odoo/odoo#226534
    
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/mrp_subcontracting/models/stock_picking.py
+++ addons/mrp_subcontracting/models/stock_picking.py
@@ -140,20 +140,23 @@ class StockPicking(models.Model):
         self.ensure_one()
         group_by_company = defaultdict(lambda: ([], []))
         for move, bom in subcontract_details:
-            # do not create extra production for move that have their quantity updated
             if move.move_orig_ids.production_id:
-                # Magic spicy sauce for the backorder case:
-                # To ensure correct splitting of the component moves of the SBC MO, we will invoke a split of the SBC
-                # MO here directly and then link the backorder MO to the backorder move.
-                # If we would just run _subcontracted_produce as usual for the newly created SBC receipt move, any
-                # reservations of raw component moves of the SBC MO would not be preserved properly (for example when
-                # using resupply subcontractor on order)
-                production_to_split = move.move_orig_ids[0].production_id
-                original_qty = move.move_orig_ids[0].product_qty
-                move.move_orig_ids = False
-                _, new_mo = production_to_split.with_context(allow_more=True)._split_productions({production_to_split: [original_qty, move.product_qty]})
-                new_mo.move_finished_ids.move_dest_ids = move
-                continue
+                if len(move.move_orig_ids.move_dest_ids) > 1:
+                    # Magic spicy sauce for the backorder case:
+                    # To ensure correct splitting of the component moves of the SBC MO, we will invoke a split of the SBC
+                    # MO here directly and then link the backorder MO to the backorder move.
+                    # If we would just run _subcontracted_produce as usual for the newly created SBC receipt move, any
+                    # reservations of raw component moves of the SBC MO would not be preserved properly (for example when
+                    # using resupply subcontractor on order)
+                    production_to_split = move.move_orig_ids[0].production_id
+                    original_qty = move.move_orig_ids[0].product_qty
+                    move.move_orig_ids = False
+                    _, new_mo = production_to_split.with_context(allow_more=True)._split_productions({production_to_split: [original_qty, move.product_qty]})
+                    new_mo.move_finished_ids.move_dest_ids = move
+                    continue
+                else:
+                    # do not create extra production for move that have their quantity updated
+                    return
             quantity = move.product_qty or move.quantity
             if move.product_uom.compare(quantity, 0) <= 0:
                 # If a subcontracted amount is decreased, don't create a MO that would be for a negative value.
