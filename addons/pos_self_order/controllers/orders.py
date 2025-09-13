559563024624b98425510ece926e0b4c263aa191
David Monnom <moda@odoo.com>
2025-09-13 21:22:31 +0200

    [FIX] pos_self_order: remove table_access_token from order retrieval
    Before this commit, `table_access_token` was used to retrieve a
    customer's orders, making it possible to “share” orders between multiple
    devices by scanning the same QR code.
    
    This caused comprehension and synchronization issues. This commit
    rectifies this by only taking into account order-related information.
    - `write_date`
    - `state`
    - access_token`
    
    If a user's device contains this information, they will be able to
    retrieve their orders.
    
    These changes are related to commit
    b903acd72142757bfdfc31c51d78b39c8e8786b9,
    which adds the `self_ordering_table_id` field, allowing multiple orders
    from Self Order to be linked to the same table at the same time.
    
    closes odoo/odoo#226842
    
    Related: odoo/enterprise#94568
    Signed-off-by: Stéphane Vanmeerhaeghe (stva) <stva@odoo.com>

--- addons/pos_self_order/controllers/orders.py
+++ addons/pos_self_order/controllers/orders.py
@@ -158,29 +158,22 @@ class PosSelfOrderController(http.Controller):
     @http.route('/pos-self-order/get-user-data', auth='public', type='jsonrpc', website=True)
     def get_orders_by_access_token(self, access_token, order_access_tokens, table_identifier=None):
         pos_config = self._verify_pos_config(access_token)
-        session = pos_config.current_session_id
-        table = pos_config.env["restaurant.table"].search([('identifier', '=', table_identifier)], limit=1)
 
-        if not table_identifier:
-            domain = Domain.FALSE
-        else:
-            domain = Domain([
-                ('table_id', '=', table.id),
-                ('state', '=', 'draft'),
-                ('access_token', 'not in', [data.get('access_token') for data in order_access_tokens])
-            ])
-
-        for data in order_access_tokens:
-            domain |= Domain([
-                ('access_token', '=', data.get('access_token')),
+        domain = [
+            Domain([
+                ('access_token', '=', data['access_token']),
                 '|',
-                ('write_date', '>', data.get('write_date')),
-                ('state', '!=', data.get('state'))
-            ])
-            domain |= Domain('access_token', '=', data.get('access_token')) \
-                & Domain('write_date', '>', data.get('write_date'))
+                ('write_date', '>', data['write_date']),
+                ('state', '!=', data['state']),
+            ]) for data in order_access_tokens
+        ]
 
-        orders = session.order_ids.filtered_domain(domain)
+        domain = Domain.OR(domain) if domain else False
+        if not domain:
+            return {}
+
+        # Do not use session.order_ids, it may fail if there is shared sessions
+        orders = pos_config.env['pos.order'].search(domain)
         if not orders:
             return {}
 
