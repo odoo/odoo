a4944fe6ad5a1e2db2bb9e279b9319e8f50e0941
Mathieu Duckerts-Antoine <dam@odoo.com>
2025-09-15 10:12:26 +0200

    [IMP] hr_work_entry: calendar: fetch favorite work entries in updateData
    Fetch userFavoritesWorkEntries in onWillRender is not appropriate. We
    fetch that info along the calendar data.

--- addons/hr_work_entry/static/src/views/work_entry_calendar/work_entry_calendar_model.js
+++ addons/hr_work_entry/static/src/views/work_entry_calendar/work_entry_calendar_model.js
@@ -1,6 +1,9 @@
-import { CalendarModel } from "@web/views/calendar/calendar_model";
-import { useService } from "@web/core/utils/hooks";
 import { serializeDate } from "@web/core/l10n/dates";
+import { user } from "@web/core/user";
+import { useService } from "@web/core/utils/hooks";
+import { CalendarModel } from "@web/views/calendar/calendar_model";
+
+const { DateTime } = luxon;
 
 export class WorkEntryCalendarModel extends CalendarModel {
     setup() {
@@ -18,7 +21,40 @@ export class WorkEntryCalendarModel extends CalendarModel {
             serializeDate(start),
             serializeDate(end),
         ]);
-        await super.updateData(...arguments);
+        await Promise.all([
+            super.updateData(...arguments),
+            this._fetchUserFavoritesWorkEntries(),
+        ]);
+    }
+
+    async _fetchUserFavoritesWorkEntries() {
+        const userFavoritesWorkEntriesIds = await this.orm.formattedReadGroup(
+            "hr.work.entry",
+            [
+                ["create_uid", "=", user.userId],
+                ["create_date", ">", serializeDate(DateTime.local().minus({ months: 3 }))],
+            ],
+            ["work_entry_type_id", "create_date:day"],
+            [],
+            {
+                order: "create_date:day desc",
+                limit: 6,
+            }
+        );
+        if (userFavoritesWorkEntriesIds.length) {
+            this.userFavoritesWorkEntries = await this.orm.read(
+                "hr.work.entry.type",
+                userFavoritesWorkEntriesIds.map((r) => r.work_entry_type_id[0]),
+                ["display_name", "display_code", "color"]
+            );
+            this.userFavoritesWorkEntries = this.userFavoritesWorkEntries.sort((a, b) =>
+                a.display_code
+                    ? a.display_code.localeCompare(b.display_code)
+                    : a.display_name.localeCompare(b.display_name)
+            );
+        } else {
+            this.userFavoritesWorkEntries = [];
+        }
     }
 
     async multiReplaceRecords(values, dates, records) {
