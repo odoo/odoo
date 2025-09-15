e102db1b925c911b2d165f448cb36838dc20effe
Antoine Guenet <age@odoo.com>
2025-09-15 06:58:46 +0000

    [IMP] html_editor: add syntax highlighting to code blocks
    This integrates the PrismJS library to provide syntax highlighting to
    code blocks. Whenever a `pre` element is in the editor, it gets wrapped
    into a `contenteditable=false` container, with an invisible editable
    `textarea` element overlayed on top of the `pre`. Whenever the contents
    of the `textarea` change, they pass through the library to get
    highlighted in the `pre`. A special toolbar appears at the top of the
    code block, for the user to pick a language and copy the code in plain
    text.
    
    task-4585835
    
    X-original-commit: 222090ad3ba3eb99b1077c86b5eee194f291441e

--- odoo/tools/mail.py
+++ odoo/tools/mail.py
@@ -80,6 +80,7 @@ safe_attrs = defs.safe_attrs | frozenset(
      'data-ai-field', 'data-ai-record-id',
      'data-heading-link-id',
      'data-mimetype-before-conversion',
+     'data-language-id', 'data-syntax-highlighting-value'
      ])
 SANITIZE_TAGS = {
     # allow new semantic HTML5 tags
