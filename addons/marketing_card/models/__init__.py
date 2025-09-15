ebe1c36ec52c2795e5043e1db04fefe998e5a4cf
sesn-odoo <sesn@odoo.com>
2025-09-15 14:24:03 +0200

    [IMP] marketing_card: use the new robots.txt override method

--- addons/marketing_card/models/__init__.py
+++ addons/marketing_card/models/__init__.py
@@ -2,6 +2,7 @@ from . import card_campaign
 from . import card_campaign_tag
 from . import card_template
 from . import card_card
+from . import ir_http
 from . import ir_model
 from . import mailing_mailing
 from . import utm_source
