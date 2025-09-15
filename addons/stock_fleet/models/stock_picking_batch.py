7cbe808449c2898b308d3e3c2f16b712aa0ce7a7
jodh-odoo <jodh@odoo.com>
2025-09-15 12:53:12 +0530

    [IMP] stock_picking_batch*: enhance batch/wave Kanban view
    *: stock_fleet
    
    Purpose
    =======
    The current Kanban view for Batches/Waves in Inventory
    doesn’t provide enough information for effective planning.
    When used with Dispatch Management,
    it becomes harder for users to quickly see key details like
    schedules, vehicles, or transfer counts.
    This makes planning less efficient and forces users
    to open each record individually.
    
    With This Commit
    ================
    The Kanban view for Batches/Waves is enhanced with clearer details:
    - Priority field
    - Proper batch name and description,
    - Scheduled date and time,
    - Number of transfers in each batch,
    - Allowed scheduling of activities directly from the card,
    - If Dispatch Management is enabled, also displays:
      - Vehicle with admitted weight/volume,
      - Dock location.
    
    Users now can see key batch details directly in kanban card,
    reducing the need to open each record individually and improving
    planning efficiency.
    
    Task - 4766636

--- addons/stock_fleet/models/stock_picking_batch.py
+++ addons/stock_fleet/models/stock_picking_batch.py
@@ -17,10 +17,8 @@ class StockPickingBatch(models.Model):
         compute='_compute_dock_id', store=True, readonly=False)
     vehicle_weight_capacity = fields.Float(string="Vehcilce Payload Capacity",
                               related='vehicle_category_id.weight_capacity')
-    weight_uom_name = fields.Char(string='Weight unit of measure label', compute='_compute_weight_uom_name')
     vehicle_volume_capacity = fields.Float(string="Max Volume (m³)",
                               related='vehicle_category_id.volume_capacity')
-    volume_uom_name = fields.Char(string='Volume unit of measure label', compute='_compute_volume_uom_name')
     driver_id = fields.Many2one(
         'res.partner', compute="_compute_driver_id", string="Driver", store=True, readonly=False)
     used_weight_percentage = fields.Float(
@@ -49,12 +47,6 @@ class StockPickingBatch(models.Model):
                 batch.dock_id = False
             if batch.picking_ids and len(batch.picking_ids.location_id) == 1 and batch.picking_ids.location_id in batch.allowed_dock_ids:
                 batch.dock_id = batch.picking_ids.location_id
-
-    def _compute_weight_uom_name(self):
-        self.weight_uom_name = self.env['product.template']._get_weight_uom_name_from_ir_config_parameter()
-
-    def _compute_volume_uom_name(self):
-        self.volume_uom_name = self.env['product.template']._get_volume_uom_name_from_ir_config_parameter()
 
     @api.depends('vehicle_id')
     def _compute_driver_id(self):
