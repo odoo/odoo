e6e6fc5f86d130b7f80f2b423c97f8eeed72434d
Vincent Larcin <vila@odoo.com>
2025-09-15 13:00:14 +0200

    [FIX] project: fix template mixins context propagation
    Reproduction Steps
    ------------------
    
    1. Go to the Project app
    2. Enable the "Templates" Filter
    3. Click on one of the displayed tempaltes
    
    The 'render_templates' key is still in the context, meaning that
    the tasks mixin will also prevent normal tasks from being shown
    and only show templates instead.
    
    To fix this, this commit makes each mixin use a different context key
    to make sure they won't interfere with each other.
    
    Task-5087632
    
    Part-of: odoo/odoo#227177
    Related: odoo/enterprise#94849
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/project/static/src/views/project_model_mixin.js
+++ addons/project/static/src/views/project_model_mixin.js
@@ -2,7 +2,7 @@ import { Domain } from "@web/core/domain";
 
 export const ProjectModelMixin = (T) => class ProjectModelMixin extends T {
     _processSearchDomain(domain) {
-        if (this.env.searchModel.context?.render_templates) {
+        if (this.env.searchModel.context?.render_project_templates) {
             return Domain.and([
                 Domain.removeDomainLeaves(domain, ['is_template']).toList(),
                 [['is_template', '=', true]],
