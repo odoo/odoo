0447431216434bbe0d8c23e9efd7144d71ab8c5b
josv-odoo <josv@odoo.com>
2025-09-15 10:37:05 +0000

    [FIX] point_of_sale: show variant in combo order
    In POS, if in a combo, one of option has instantly created variant it will not show the variant name in the order
    
    Steps to reproduce:
    -------------------
    * Create a product with instantly generated variants
    * Create a combo choice and add one of his variants
    * Create a combo product and add the combo
    * Open an store that sells this combo
    * Select the combo and choose the product with variant and confirm the selection
    
    Issue : On the left size in the order the variant name will not appear.
    
    Observation:
    ------------
    When it tries to construct the name of the product it need the attribute_value_ids:
    https://github.com/odoo/odoo/blob/35c77b89e4c0db035ad25fef9a34be6ef74db7b5/addons/point_of_sale/static/src/utils.js#L36-L39
    Since in our case the product is a variant "always" the attribute are not inside attribute_value_ids, it will not have them :
    https://github.com/odoo/odoo/blob/942b7a21d7e0eb15133f4af1b47e68f48dec70c7/addons/point_of_sale/static/src/app/models/utils/compute_combo_items.js#L31-L33
    
    opw-5037355
    
    X-original-commit: f070848889a0a566869e79cfd1619f9870eab051

--- addons/point_of_sale/static/src/utils.js
+++ addons/point_of_sale/static/src/utils.js
@@ -52,6 +52,13 @@ export function constructAttributeString(line) {
         }
 
         attributeString = attributeString.slice(0, -2);
+    } else if (
+        attributeString === "" &&
+        line?.product_id?.product_template_variant_value_ids?.length > 0
+    ) {
+        attributeString = line.product_id.product_template_variant_value_ids
+            ?.map((attr) => attr.name)
+            .join(", ");
     }
 
     return attributeString;
