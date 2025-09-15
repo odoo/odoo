7015b28c5b07f0b263cf7676e03aeeb153192816
reka-odoo <reka@odoo.com>
2025-09-15 12:24:39 +0000

    [FIX] hr_holidays: fix error when leave is linked to refused allocation
    When a leave is linked to a refused allocation and
    the cron "Time Off: Cancel invalid leaves" runs, a traceback occurs.
    
    Steps to reproduce the error:
    - Install "hr_holidays" without demo data
    - Create a new Time Off Type > Time Off Requests, Approval: No Validation >
      Allow Negative Cap: True > Maximum Excess Amount: 2
    - Create an Accrual Plan using default values
    - Create a New Allocation
          - Allocation Type: Accrual Allocation
          - Set the Time Off Type and Accrual Plan created above
          - Allocation: 1 day > Approve
    - Create a new Time Off in the near future (in the current month) and select the
      Time Off Type created above
    - Go back to the Allocation > Refuse
    - Run the cron "Time Off: Cancel invalid leaves"
    
    Traceback:
    "IndexError: list index out of range"
    
    https://github.com/odoo/odoo/blob/bc5f24195a486112574900015ecbcf0e3ba32145/addons/hr_holidays/models/hr_leave.py#L1535-L1536
    Here, "leave_type_data" becomes "defaultdict(<class 'list'>, {})"
    because when the allocation is refused and the cron runs,
    the "get_allocation_data" method returns an empty defaultdict.
    As a result, accessing the index leads to the above traceback.
    
    sentry-6874651972
    
    X-original-commit: 00727ed6fd406eaaee2589bd331b52d22b47bc54

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -670,8 +670,11 @@ class HrLeave(models.Model):
             leave_data = leave_type.get_allocation_data(employees, date_from)
             if leave_type.allows_negative:
                 max_excess = leave_type.max_allowed_negative
+                is_cancellation = all(leave.state in ('cancel', 'refuse') for leave in leaves)
                 for employee in employees:
-                    if not leave_data[employee]:
+                    if is_cancellation:
+                        continue
+                    if not leave_data[employee][0][1]['max_leaves']:
                         raise ValidationError(_("You do not have any allocation for this time off type.\n"
                                                 "Please request an allocation before submitting your time off request."))
                     if leave_data[employee] and leave_data[employee][0][1]['virtual_remaining_leaves'] < -max_excess:
@@ -684,7 +687,7 @@ class HrLeave(models.Model):
             for employee in employees:
                 previous_emp_data = previous_leave_data[employee] and previous_leave_data[employee][0][1]['virtual_excess_data']
                 emp_data = leave_data[employee] and leave_data[employee][0][1]['virtual_excess_data']
-                if not leave_data[employee]:
+                if not leave_data[employee][0][1]['max_leaves']:
                     raise ValidationError(_("You do not have any allocation for this time off type.\n"
                                             "Please request an allocation before submitting your time off request."))
                 if not previous_emp_data and not emp_data:
@@ -838,7 +841,8 @@ class HrLeave(models.Model):
                 values['request_date_to'] = values['date_to']
         result = super().write(values)
         if any(field in values for field in ['request_date_from', 'date_from', 'request_date_from', 'date_to', 'holiday_status_id', 'employee_id', 'state']):
-            self._check_validity()
+            if not values.get('state') or values.get('state') not in ('refuse', 'cancel'):
+                self._check_validity()
             self.env['hr.leave.allocation'].invalidate_model(['leaves_taken', 'max_leaves'])  # missing dependency on compute
         if not self.env.context.get('leave_fast_create'):
             for holiday in self:
@@ -1559,6 +1563,9 @@ is approved, validated or refused.')
             leave_type = leave.holiday_status_id
             date = leave.date_from.date()
             leave_type_data = leave_type.get_allocation_data(leave.employee_id, date)
+            if not leave_type_data[leave.employee_id][0][1]['max_leaves']:
+                leave._force_cancel(reason, 'mail.mt_note')
+                continue
             exceeding_duration = leave_type_data[leave.employee_id][0][1]['total_virtual_excess']
             excess_limit = leave_type.max_allowed_negative if leave_type.allows_negative else 0
             if exceeding_duration <= excess_limit:
