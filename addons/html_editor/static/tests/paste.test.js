a063d46c03318ffc6864bb8caaa279c968b3f949
adch-odoo <adch@odoo.com>
2025-09-16 11:10:29 +0000

    [FIX] html_editor: base container in empty table cells on paste
    Steps to Reproduce:
    
    - Go to the ToDo app.
    - Copy a table from Google Docs.
    - Paste the table into the editor.
    - Observe that `<td>` elements remain empty.
    
    Purpose of this commit:
    
    - Insert a base container into empty `<td>` elements when
    pasting tables from external sources.
    
    task-5078904
    
    closes odoo/odoo#228272
    
    X-original-commit: 731610118265796690be5bf145ba1700ecf38026
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Adnan Chaudhary (adch) <adch@odoo.com>

--- addons/html_editor/static/tests/paste.test.js
+++ addons/html_editor/static/tests/paste.test.js
@@ -78,6 +78,38 @@ describe("Html Paste cleaning - whitelist", () => {
         });
     });
 
+    test("should insert a base container inside empty <td> on paste", async () => {
+        await testEditor({
+            contentBefore: `
+                <p>[]<br></p>
+            `,
+            stepFunction: async (editor) => {
+                pasteHtml(
+                    editor,
+                    unformat(`
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    `)
+                );
+            },
+            contentAfter: unformat(`
+                <table class="table table-bordered">
+                    <tbody>
+                        <tr>
+                            <td><p><br></p></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <p>[]<br></p>
+            `),
+        });
+    });
+
     test("should not keep span", async () => {
         await testEditor({
             contentBefore: "<p>123[]</p>",
