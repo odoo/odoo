0787fa5aeed522d801855763723a19edf07d7e75
Pedram (PEBR) <pebr@odoo.com>
2025-09-16 10:27:17 +0000

    [FIX] pos_sale: show only selected customer orders in POS
    Before this commit, when selecting a customer in the POS and clicking
    Actions → Quotation/Order, all quotations and orders were displayed
    instead of filtering by the selected customer.
    
    This commit ensures that only the quotations/orders of the selected
    customer are shown.
    
    opw-5074052
    
    closes odoo/odoo#229465
    
    X-original-commit: e927c33f316828b8be64fb98546926af1f0cd274
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/pos_sale/static/src/app/components/screens/product_screen/control_buttons/control_buttons.js
+++ addons/pos_sale/static/src/app/components/screens/product_screen/control_buttons/control_buttons.js
@@ -9,15 +9,23 @@ patch(ControlButtons.prototype, {
             context["search_default_partner_id"] = this.partner.id;
         }
 
+        let domain = [
+            ["state", "!=", "cancel"],
+            ["invoice_status", "!=", "invoiced"],
+            ["currency_id", "=", this.pos.currency.id],
+        ];
+        if (this.pos.getOrder()?.getPartner()) {
+            domain = [
+                ...domain,
+                ["partner_id", "any", [["id", "child_of", [this.pos.getOrder().getPartner().id]]]],
+            ];
+        }
+
         this.dialog.add(SelectCreateDialog, {
             resModel: "sale.order",
             noCreate: true,
             multiSelect: false,
-            domain: [
-                ["state", "!=", "cancel"],
-                ["invoice_status", "!=", "invoiced"],
-                ["currency_id", "=", this.pos.currency.id],
-            ],
+            domain,
             context: context,
             onSelected: async (resIds) => {
                 await this.pos.onClickSaleOrder(resIds[0]);
