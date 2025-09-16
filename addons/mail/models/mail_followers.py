odoo/odoo

commit c349bef9c089b8f1d13ba6132d0e1e51273cf812

[FIX] mail: add back implied groups to recipients data
**Original issue**
The "View tasks" button was missing in the notification
mail sent to users with "Administrator" rights in the
Project app.

**Root cause**
Issue since commit 59ed9427859ac3cb7371e291ac2045fe8bed2822
after which implied groups are no longer added to the user.

The query in `_get_recipient_data` using the `res_groups_users_rel`
table now doesn't fetch the implied groups.

In `_notify_get_recipients_groups` of `project.task`, button
access was given to project users, this group is missing from
the data returned by `_get_recipient_data`.

**Solution**
Add the implied groups in the query, which should return the same
groups as the `all_group_ids` field of the user.

opw-4711892

closes odoo/odoo#210135

X-original-commit: 9cf4b77fd41e242be355969496da0ffe7d4645aa
Signed-off-by: Raphael Collet <rco@odoo.com>

addons/mail/models/mail_followers.py
@@ -205,17 +205,28 @@ class MailFollowers(models.Model):
       JOIN sub_followers ON sub_followers.pid = partner.id
                         AND (sub_followers.internal IS NOT TRUE OR partner.partner_share IS NOT TRUE)
  LEFT JOIN LATERAL (
-        SELECT users.id AS uid,
-               users.share AS share,
-               users.notification_type AS notification_type,
-               ARRAY_AGG(groups_rel.gid) FILTER (WHERE groups_rel.gid IS NOT NULL) AS groups
-          FROM res_users users
-     LEFT JOIN res_groups_users_rel groups_rel ON groups_rel.uid = users.id
-         WHERE users.partner_id = partner.id AND users.active
-      GROUP BY users.id,
-               users.share,
-               users.notification_type
-      ORDER BY users.share ASC NULLS FIRST, users.id ASC
+        WITH RECURSIVE all_groups AS (
+            SELECT users.id AS uid,
+                   users.share AS share,
+                   users.notification_type AS notification_type,
+                   groups_rel.gid
+              FROM res_users users
+         LEFT JOIN res_groups_users_rel groups_rel ON groups_rel.uid = users.id
+             WHERE users.partner_id = partner.id AND users.active
+          UNION
+            SELECT ag.uid, ag.share, ag.notification_type, implied.hid
+              FROM all_groups ag
+              JOIN res_groups_implied_rel implied ON ag.gid = implied.gid
+        )
+        SELECT uid,
+               share,
+               notification_type,
+               ARRAY_AGG(DISTINCT gid) AS groups
+          FROM all_groups
+      GROUP BY uid,
+               share,
+               notification_type
+      ORDER BY share ASC NULLS FIRST, uid ASC
          FETCH FIRST ROW ONLY
          ) sub_user ON TRUE
 
@@ -244,17 +255,28 @@ class MailFollowers(models.Model):
                               AND fol.res_model = %s
                               AND fol.res_id IN %s
  LEFT JOIN LATERAL (
-        SELECT users.id AS uid,
-               users.share AS share,
-               users.notification_type AS notification_type,
-               ARRAY_AGG(groups_rel.gid) FILTER (WHERE groups_rel.gid IS NOT NULL) AS groups
-          FROM res_users users
-     LEFT JOIN res_groups_users_rel groups_rel ON groups_rel.uid = users.id
-         WHERE users.partner_id = partner.id AND users.active
-      GROUP BY users.id,
-               users.share,
-               users.notification_type
-      ORDER BY users.share ASC NULLS FIRST, users.id ASC
+        WITH RECURSIVE all_groups AS (
+            SELECT users.id AS uid,
+                   users.share AS share,
+                   users.notification_type AS notification_type,
+                   groups_rel.gid
+              FROM res_users users
+         LEFT JOIN res_groups_users_rel groups_rel ON groups_rel.uid = users.id
+             WHERE users.partner_id = partner.id AND users.active
+          UNION
+            SELECT ag.uid, ag.share, ag.notification_type, implied.hid
+              FROM all_groups ag
+              JOIN res_groups_implied_rel implied ON ag.gid = implied.gid
+        )
+        SELECT uid,
+               share,
+               notification_type,
+               ARRAY_AGG(DISTINCT gid) AS groups
+          FROM all_groups
+      GROUP BY uid,
+               share,
+               notification_type
+      ORDER BY share ASC NULLS FIRST, uid ASC
          FETCH FIRST ROW ONLY
          ) sub_user ON TRUE
 
@@ -296,17 +318,28 @@ class MailFollowers(models.Model):
            FALSE as is_follower
       FROM res_partner partner
  LEFT JOIN LATERAL (
-        SELECT users.id AS uid,
-               users.share AS share,
-               users.notification_type AS notification_type,
-               ARRAY_AGG(groups_rel.gid) FILTER (WHERE groups_rel.gid IS NOT NULL) AS groups
-          FROM res_users users
-     LEFT JOIN res_groups_users_rel groups_rel ON groups_rel.uid = users.id
-         WHERE users.partner_id = partner.id AND users.active
-      GROUP BY users.id,
-               users.share,
-               users.notification_type
-      ORDER BY users.share ASC NULLS FIRST, users.id ASC
+        WITH RECURSIVE all_groups AS (
+            SELECT users.id AS uid,
+                   users.share AS share,
+                   users.notification_type AS notification_type,
+                   groups_rel.gid
+              FROM res_users users
+         LEFT JOIN res_groups_users_rel groups_rel ON groups_rel.uid = users.id
+             WHERE users.partner_id = partner.id AND users.active
+          UNION
+            SELECT ag.uid, ag.share, ag.notification_type, implied.hid
+              FROM all_groups ag
+              JOIN res_groups_implied_rel implied ON ag.gid = implied.gid
+        )
+        SELECT uid,
+               share,
+               notification_type,
+               ARRAY_AGG(DISTINCT gid) AS groups
+          FROM all_groups
+      GROUP BY uid,
+               share,
+               notification_type
+      ORDER BY share ASC NULLS FIRST, uid ASC
          FETCH FIRST ROW ONLY
          ) sub_user ON TRUE
 

addons/test_mail/tests/test_mail_followers.py
@@ -624,7 +624,7 @@ class RecipientsNotificationTest(MailCommon):
                 self.assertEqual(partner_data['lang'], partner.lang)
                 self.assertEqual(partner_data['name'], partner.name)
                 if user:
-                    self.assertEqual(partner_data['groups'], set(user.group_ids.ids))
+                    self.assertEqual(partner_data['groups'], set(user.all_group_ids.ids))
                     self.assertEqual(partner_data['notif'], user.notification_type)
                     self.assertEqual(partner_data['uid'], user.id)
                 else:

commit 6736e316d2fdfb366f33ddc4d4c5ac989a92fc78

[FIX] mail: performance of _get_recipient_data()
The commit c349bef9c089b8f1d13ba6132d0e1e51273cf812 introduced a severe
performance regression in some SQL query, going from 1.2ms to 450ms!
Instead of computing the transitive closure of collected groups in pure
SQL with a "WITH RECURSIVE", we use the computed field all_implied_ids.
As the latter is based on ormcache'd data, the new solution has no
marginal cost in terms of SQL queries.

closes odoo/odoo#227317

X-original-commit: e71edb6a3a4747f18a661a49d5523fd58552ac11
Signed-off-by: Raphael Collet <rco@odoo.com>

addons/mail/models/mail_followers.py
@@ -206,28 +206,17 @@ class MailFollowers(models.Model):
       JOIN sub_followers ON sub_followers.pid = partner.id
                         AND (sub_followers.internal IS NOT TRUE OR partner.partner_share IS NOT TRUE)
  LEFT JOIN LATERAL (
-        WITH RECURSIVE all_groups AS (
-            SELECT users.id AS uid,
-                   users.share AS share,
-                   users.notification_type AS notification_type,
-                   groups_rel.gid
-              FROM res_users users
-         LEFT JOIN res_groups_users_rel groups_rel ON groups_rel.uid = users.id
-             WHERE users.partner_id = partner.id AND users.active
-          UNION
-            SELECT ag.uid, ag.share, ag.notification_type, implied.hid
-              FROM all_groups ag
-              JOIN res_groups_implied_rel implied ON ag.gid = implied.gid
-        )
-        SELECT uid,
-               share,
-               notification_type,
-               ARRAY_AGG(DISTINCT gid) AS groups
-          FROM all_groups
-      GROUP BY uid,
-               share,
-               notification_type
-      ORDER BY share ASC NULLS FIRST, uid ASC
+        SELECT users.id AS uid,
+               users.share AS share,
+               users.notification_type AS notification_type,
+               ARRAY_AGG(groups_rel.gid) FILTER (WHERE groups_rel.gid IS NOT NULL) AS groups
+          FROM res_users users
+     LEFT JOIN res_groups_users_rel groups_rel ON groups_rel.uid = users.id
+         WHERE users.partner_id = partner.id AND users.active
+      GROUP BY users.id,
+               users.share,
+               users.notification_type
+      ORDER BY users.share ASC NULLS FIRST, users.id ASC
          FETCH FIRST ROW ONLY
          ) sub_user ON TRUE
 
@@ -256,28 +245,17 @@ class MailFollowers(models.Model):
                               AND fol.res_model = %s
                               AND fol.res_id IN %s
  LEFT JOIN LATERAL (
-        WITH RECURSIVE all_groups AS (
-            SELECT users.id AS uid,
-                   users.share AS share,
-                   users.notification_type AS notification_type,
-                   groups_rel.gid
-              FROM res_users users
-         LEFT JOIN res_groups_users_rel groups_rel ON groups_rel.uid = users.id
-             WHERE users.partner_id = partner.id AND users.active
-          UNION
-            SELECT ag.uid, ag.share, ag.notification_type, implied.hid
-              FROM all_groups ag
-              JOIN res_groups_implied_rel implied ON ag.gid = implied.gid
-        )
-        SELECT uid,
-               share,
-               notification_type,
-               ARRAY_AGG(DISTINCT gid) AS groups
-          FROM all_groups
-      GROUP BY uid,
-               share,
-               notification_type
-      ORDER BY share ASC NULLS FIRST, uid ASC
+        SELECT users.id AS uid,
+               users.share AS share,
+               users.notification_type AS notification_type,
+               ARRAY_AGG(groups_rel.gid) FILTER (WHERE groups_rel.gid IS NOT NULL) AS groups
+          FROM res_users users
+     LEFT JOIN res_groups_users_rel groups_rel ON groups_rel.uid = users.id
+         WHERE users.partner_id = partner.id AND users.active
+      GROUP BY users.id,
+               users.share,
+               users.notification_type
+      ORDER BY users.share ASC NULLS FIRST, users.id ASC
          FETCH FIRST ROW ONLY
          ) sub_user ON TRUE
 
@@ -319,28 +297,17 @@ class MailFollowers(models.Model):
            FALSE as is_follower
       FROM res_partner partner
  LEFT JOIN LATERAL (
-        WITH RECURSIVE all_groups AS (
-            SELECT users.id AS uid,
-                   users.share AS share,
-                   users.notification_type AS notification_type,
-                   groups_rel.gid
-              FROM res_users users
-         LEFT JOIN res_groups_users_rel groups_rel ON groups_rel.uid = users.id
-             WHERE users.partner_id = partner.id AND users.active
-          UNION
-            SELECT ag.uid, ag.share, ag.notification_type, implied.hid
-              FROM all_groups ag
-              JOIN res_groups_implied_rel implied ON ag.gid = implied.gid
-        )
-        SELECT uid,
-               share,
-               notification_type,
-               ARRAY_AGG(DISTINCT gid) AS groups
-          FROM all_groups
-      GROUP BY uid,
-               share,
-               notification_type
-      ORDER BY share ASC NULLS FIRST, uid ASC
+        SELECT users.id AS uid,
+               users.share AS share,
+               users.notification_type AS notification_type,
+               ARRAY_AGG(groups_rel.gid) FILTER (WHERE groups_rel.gid IS NOT NULL) AS groups
+          FROM res_users users
+     LEFT JOIN res_groups_users_rel groups_rel ON groups_rel.uid = users.id
+         WHERE users.partner_id = partner.id AND users.active
+      GROUP BY users.id,
+               users.share,
+               users.notification_type
+      ORDER BY users.share ASC NULLS FIRST, users.id ASC
          FETCH FIRST ROW ONLY
          ) sub_user ON TRUE
 
@@ -364,6 +331,10 @@ class MailFollowers(models.Model):
             pshare, uid, ushare, notif, groups, res_id, is_follower
         ) in res:
             to_update = [res_id] if res_id else res_ids
+            # add transitive closure of implied groups; note that the field
+            # all_implied_ids relies on ormcache'd data, which shouldn't add
+            # more queries
+            groups = self.env['res.groups'].browse(set(groups or [])).all_implied_ids.ids
             for res_id_to_update in to_update:
                 # avoid updating already existing information, unnecessary dict update
                 if not res_id and partner_id in doc_infos[res_id_to_update]: