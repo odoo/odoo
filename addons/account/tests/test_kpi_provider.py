b1c64b09135493fe33cb2dbe933e1575b1206193
Paul Morelle <pmo@odoo.com>
2025-09-17 06:48:23 +0200

    [REF] account: reorganize kpi.provider tests
    The account test_kpi_provider was already getting complicated, and this
    commit aims to simplify it by breaking it into smaller tests that show
    more clearly what is expected.
    
    Task-id: 5062431
    Forward-port-of: odoo/odoo#227605
    Part-of: odoo/odoo#227661
    Signed-off-by: Brice Bartoletti (bib) <bib@odoo.com>
    Signed-off-by: Paul Morelle (pmo) <pmo@odoo.com>

--- addons/account/tests/test_kpi_provider.py
+++ addons/account/tests/test_kpi_provider.py
@@ -5,25 +5,28 @@ from odoo.tests import tagged, TransactionCase
 @tagged('post_install', '-at_install')
 class TestKpiProvider(TransactionCase):
 
-    def test_kpi_summary(self):
-        """
-        - Ensure that nothing is reported when there is nothing to report
-        - All <account.move> in draft or to_check should be reported
-        - Posting one <account.move> should reduce the number reported
-        - Posting all <account.move> of a journal_type should remove that journal_type from the reporting
-        """
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.partner_id = cls.env['res.partner'].create({'name': 'Someone'})
+
         # Clean things for the test
-        self.env['account.move'].search([('state', '=', 'draft')]).unlink()
+        cls.env['account.move'].search([('state', '=', 'draft')]).unlink()
+
+    def test_empty_kpi_summary(self):
+        # Ensure that nothing gets reported when there is nothing to report
         self.assertCountEqual(self.env['kpi.provider'].get_account_kpi_summary(), [])
 
+    def test_kpi_summary(self):
         company_id = self.ref('base.main_company')
         account_id = self.env['account.account'].search([('company_id', '=', company_id)], limit=1)
         base_move = {
             'company_id': company_id,
-            'line_ids': [Command.create({'account_id': account_id.id, 'quantity': 15, 'price_unit': 10})],
-            'partner_id': self.env.user.partner_id.id,
+            'invoice_line_ids': [Command.create({'account_id': account_id.id, 'quantity': 15, 'price_unit': 10})],
+            'partner_id': self.partner_id.id,
         }
-        all_moves = self.env['account.move'].create(
+        self.env['account.move'].create(
             [{**base_move, 'move_type': 'entry'}] * 2 +
             [{**base_move, 'move_type': 'out_invoice'}] * 3 +
             [{**base_move, 'move_type': 'out_refund'}] * 4 +
@@ -38,28 +41,36 @@ class TestKpiProvider(TransactionCase):
             {'id': 'account_journal_type.purchase', 'name': 'Purchase', 'type': 'integer', 'value': 5 + 6 + 8},
         ])
 
-        all_moves[0].action_post()
+    def test_kpi_summary_shouldnt_report_posted_moves(self):
+        company_id = self.ref('base.main_company')
+        account_id = self.env['account.account'].search([('company_id', '=', company_id)], limit=1).id
+        move = self.env['account.move'].create({
+            'company_id': company_id,
+            'invoice_line_ids': [Command.create({'account_id': account_id, 'quantity': 15, 'price_unit': 10})],
+            'partner_id': self.partner_id.id,
+            'move_type': 'out_invoice',
+        })
         self.assertCountEqual(self.env['kpi.provider'].get_account_kpi_summary(), [
-            {'id': 'account_journal_type.general', 'name': 'Miscellaneous', 'type': 'integer', 'value': 1},
-            {'id': 'account_journal_type.sale', 'name': 'Sales', 'type': 'integer', 'value': 14},
-            {'id': 'account_journal_type.purchase', 'name': 'Purchase', 'type': 'integer', 'value': 19},
+            {'id': 'account_journal_type.sale', 'name': 'Sales', 'type': 'integer', 'value': 1},
         ])
 
-        all_moves[1].action_post()
+        move.action_post()
+        self.assertCountEqual(self.env['kpi.provider'].get_account_kpi_summary(), [])
+
+    def test_kpi_summary_reports_posted_but_to_check_moves(self):
+        company_id = self.ref('base.main_company')
+        account_id = self.env['account.account'].search([('company_id', '=', company_id)], limit=1).id
+        move = self.env['account.move'].create({
+            'company_id': company_id,
+            'invoice_line_ids': [Command.create({'account_id': account_id, 'quantity': 15, 'price_unit': 10})],
+            'partner_id': self.partner_id.id,
+            'move_type': 'out_invoice',
+        })
+        move.action_post()
+        move.to_check = True
         self.assertCountEqual(self.env['kpi.provider'].get_account_kpi_summary(), [
-            {'id': 'account_journal_type.sale', 'name': 'Sales', 'type': 'integer', 'value': 14},
-            {'id': 'account_journal_type.purchase', 'name': 'Purchase', 'type': 'integer', 'value': 19},
+            {'id': 'account_journal_type.sale', 'name': 'Sales', 'type': 'integer', 'value': 1},
         ])
 
-        all_moves[2].action_post()
-        all_moves[2].to_check = True
-        self.assertCountEqual(self.env['kpi.provider'].get_account_kpi_summary(), [
-            {'id': 'account_journal_type.sale', 'name': 'Sales', 'type': 'integer', 'value': 14},
-            {'id': 'account_journal_type.purchase', 'name': 'Purchase', 'type': 'integer', 'value': 19},
-        ])
-
-        all_moves[2].button_set_checked()
-        self.assertCountEqual(self.env['kpi.provider'].get_account_kpi_summary(), [
-            {'id': 'account_journal_type.sale', 'name': 'Sales', 'type': 'integer', 'value': 13},
-            {'id': 'account_journal_type.purchase', 'name': 'Purchase', 'type': 'integer', 'value': 19},
-        ])
+        move.button_set_checked()
+        self.assertCountEqual(self.env['kpi.provider'].get_account_kpi_summary(), [])
