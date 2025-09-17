510a1a41dfd2e7b94dd353d037109a427074d885
Mehdi Outagant (MOU) <mou@odoo.com>
2025-09-17 16:14:34 +0200

    [IMP] website, html_builder: make the form field types list searchable
    This commit introduces a visibility filter with fuzzy matching to the
    `BuilderSelect` component, allowing users to search options more easily.
    
    It will be initially enabled for the "form field types" selector to
    simplify picking a field type option.
    
    task-3675302

--- addons/html_builder/static/src/core/building_blocks/builder_select.js
+++ addons/html_builder/static/src/core/building_blocks/builder_select.js
@@ -1,4 +1,4 @@
-import { Component, onMounted, useRef, useSubEnv, xml } from "@odoo/owl";
+import { Component, onMounted, useRef, useSubEnv, xml, useState } from "@odoo/owl";
 import { _t } from "@web/core/l10n/translation";
 import { Dropdown } from "@web/core/dropdown/dropdown";
 import {
@@ -10,6 +10,8 @@ import {
 import { BuilderComponent } from "./builder_component";
 import { useDropdownState } from "@web/core/dropdown/dropdown_hooks";
 import { setElementContent } from "@web/core/utils/html";
+import { useDebounced, useThrottleForAnimation } from "@web/core/utils/timing";
+import { fuzzyTest } from "@web/core/utils/search";
 
 export class WithIgnoreItem extends Component {
     static template = xml`<t t-slot="default"/>`;
@@ -38,6 +40,7 @@ export class BuilderSelect extends Component {
             },
         },
         dropdownClass: { type: String, optional: true },
+        searchable: { type: Boolean, optional: true },
     };
     static defaultProps = { dropdownClass: "o-hb-select-dropdown" };
     static components = {
@@ -47,6 +50,11 @@ export class BuilderSelect extends Component {
     };
 
     setup() {
+        this.state = useState({
+            searchString: "",
+        });
+        this.inputRef = useRef("inputRef");
+
         useVisibilityObserver("content", useApplyVisibility("root"));
 
         this.dropdown = useDropdownState();
@@ -67,11 +75,42 @@ export class BuilderSelect extends Component {
                 updateCurrentLabel();
             },
         });
+        this.debouncedOnSearchInput = useDebounced((ev) => {
+            const searchString = ev.target.value;
+            this.state.searchString = searchString || "";
+        }, 200);
+        // Ensure the dropdown content is re-rendered / repositioned correctly
+        // when applying a search filter.
+        this.throttledDropdownUpdate = useThrottleForAnimation(() => {
+            this.inputRef.el?.focus();
+            this.inputRef.el?.ownerDocument.dispatchEvent(new Event("scroll"));
+        });
         onMounted(updateCurrentLabel);
         useSubEnv({
             onSelectItem: () => {
                 this.dropdown.close();
             },
+            searchFilterItem: this.searchFilterItem.bind(this),
+            throttledDropdownUpdate: this.throttledDropdownUpdate.bind(this),
         });
+    }
+    /**
+     * Determines whether a dropdown item should be visible based on the current
+     * search string.
+     *
+     * @param {String} itemLabel The dropdown option label.
+     */
+    searchFilterItem(itemLabel) {
+        return !this.state.searchString || fuzzyTest(this.state.searchString.trim(), itemLabel);
+    }
+    /**
+     * Adapts the search input when the dropdown is opened / closed.
+     *
+     * @param {Boolean} open
+     */
+    onStateChanged(open) {
+        if (this.props.searchable) {
+            return open ? this.inputRef.el.focus() : (this.state.searchString = "");
+        }
     }
 }
