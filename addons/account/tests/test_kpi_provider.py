e0aa23b4c35ed6b5d376c123db0471ce87b217b0
Paul Morelle <pmo@odoo.com>
2025-09-17 06:57:38 +0200

    [IMP] account: make kpi.provider report unreconciled bank statements
    The kpi.provider:get_account_kpi_summary method should include posted
    moves of a bank journal that are related to an unreconciled bank
    statement.
    
    closes odoo/odoo#227605
    
    closes odoo/odoo#227661
    
    Task-id: 5062431
    Forward-port-of: #227605
    Signed-off-by: Brice Bartoletti (bib) <bib@odoo.com>
    Signed-off-by: Paul Morelle (pmo) <pmo@odoo.com>

--- addons/account/tests/test_kpi_provider.py
+++ addons/account/tests/test_kpi_provider.py
@@ -12,7 +12,10 @@ class TestKpiProvider(TransactionCase):
         cls.partner_id = cls.env['res.partner'].create({'name': 'Someone'})
 
         # Clean things for the test
-        cls.env['account.move'].search([('state', '=', 'draft')]).unlink()
+        cls.env['account.move'].search([
+            '|', ('state', '=', 'draft'),
+            ('statement_line_id.is_reconciled', '=', False),
+        ])._unlink_or_reverse()
 
     def test_empty_kpi_summary(self):
         # Ensure that nothing gets reported when there is nothing to report
@@ -73,4 +76,42 @@ class TestKpiProvider(TransactionCase):
         ])
 
         move.button_set_checked()
+        self.assertCountEqual(self.env['kpi.provider'].get_account_kpi_summary(), [])
+
+    def test_kpi_summary_reports_unreconciled_bank_statements(self):
+        company_id = self.ref('base.main_company')
+        account_id = self.env['account.account'].search([('company_id', '=', company_id), ('account_type', '=', 'income')], limit=1).id
+        move = self.env['account.move'].create({
+            'company_id': company_id,
+            'line_ids': [Command.create({'account_id': account_id, 'quantity': 15, 'price_unit': 10})],
+            'partner_id': self.env.user.partner_id.id,
+            'move_type': 'out_invoice',
+        })
+        move.action_post()
+
+        journal_id = self.env['account.journal'].create({
+            'name': 'Bank',
+            'type': 'bank',
+        })
+        bank_statement = self.env['account.bank.statement'].create({
+            'name': 'test_statement',
+            'line_ids': [Command.create({
+                'date': '2025-09-15',
+                'payment_ref': 'line_1',
+                'journal_id': journal_id.id,
+                'amount': move.amount_total,
+            })],
+        })
+
+        self.assertEqual(bank_statement.line_ids.move_id.state, 'posted')
+        self.assertFalse(bank_statement.line_ids.is_reconciled)
+        self.assertCountEqual(self.env['kpi.provider'].get_account_kpi_summary(), [
+            {'id': 'account_journal_type.bank', 'name': 'Bank', 'type': 'integer', 'value': 1},
+        ])
+
+        move_line = move.line_ids.filtered(lambda line: line.account_type == 'asset_receivable')
+        _st_liquidity_lines, st_suspense_lines, _st_other_lines = bank_statement.line_ids._seek_for_lines()
+        st_suspense_lines.account_id = move_line.account_id
+        (move_line + st_suspense_lines).reconcile()
+        self.assertTrue(bank_statement.line_ids.is_reconciled)
         self.assertCountEqual(self.env['kpi.provider'].get_account_kpi_summary(), [])
