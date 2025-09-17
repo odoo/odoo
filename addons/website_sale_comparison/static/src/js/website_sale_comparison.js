80dd909dcc0f1406ee8d99cd68ec6eb5eb2c77ce
khaj-odoo <khaj@odoo.com>
2025-09-17 18:57:47 +0530

    [FIX] website_sale_comparison: show `Comparator` button correctly on mobile
    Description:
    This commit removes desktop-only visibility classes and ensures that
    show/hide is handled consistently with `d-none`/`d-block` across all views.
    
    Steps to Reproduce on versions 16+:
    1. Enable the `Product Comparison Tool` button from Website.
    2. Open any product grid or product page.
    3. Switch between desktop and mobile view.
    
    Before the Merge:
    - The `Compare` button was not visible  in mobile view. The underlying cause
      was the use of Bootstrap classes restricted to desktop (`d-md-block`,
      `d-md-inline-block`)and inconsistent toggling in JS.
    - Buttons appeared in desktop view but hidden in mobile view.
    
    After the Merge:
    -`Compare` button are visible and usable in mobile view as well.
    - The bottom  `Compare` panel also visible in mobile view.
    opw-5091746

--- addons/website_sale_comparison/static/src/js/website_sale_comparison.js
+++ addons/website_sale_comparison/static/src/js/website_sale_comparison.js
@@ -172,7 +172,7 @@ var ProductComparison = publicWidget.Widget.extend(VariantMixin, {
     },
     _addNewProductsImpl: function (product_id) {
         var self = this;
-        $('.o_product_feature_panel').addClass('d-md-block');
+        $('.o_product_feature_panel').addClass('d-block');
         if (!_.contains(self.comparelist_product_ids, product_id)) {
             self.comparelist_product_ids.push(product_id);
             if (_.has(self.product_data, product_id)){
@@ -233,14 +233,14 @@ var ProductComparison = publicWidget.Widget.extend(VariantMixin, {
      */
     _updateComparelistView: function () {
         this.$('.o_product_circle').text(this.comparelist_product_ids.length);
-        this.$('.o_comparelist_button').removeClass('d-md-block');
+        this.$('.o_comparelist_button').addClass('d-none');
         if (_.isEmpty(this.comparelist_product_ids)) {
-            $('.o_product_feature_panel').removeClass('d-md-block');
+            $('.o_product_feature_panel').addClass('d-none').removeClass('d-block');
         } else {
-            $('.o_product_feature_panel').addClass('d-md-block');
-            this.$('.o_comparelist_products').addClass('d-md-block');
+            $('.o_product_feature_panel').removeClass('d-none').addClass('d-block');
+            this.$('.o_comparelist_products').addClass('d-block');
             if (this.comparelist_product_ids.length >=2) {
-                this.$('.o_comparelist_button').addClass('d-md-block');
+                this.$('.o_comparelist_button').removeClass('d-none').addClass('d-block');
                 this.$('.o_comparelist_button a').attr('href',
                     '/shop/compare?products=' + encodeURIComponent(this.comparelist_product_ids));
             }
