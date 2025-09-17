f08167b3568e311899213817d69863df2c172da3
Félix DENIS <felix.denis@outlook.com>
2025-09-17 23:01:59 +0200

    update project odoo odoo-17

--- commit_to_server_config.cmd
+++ commit_to_server_config.cmd
@@ -3,7 +3,7 @@ cd C:\Users\felix\Documents\Projet\odoo\odoo17
 robocopy . ..\..\server_config\images\odoo\17\data /mir /XD ".git" /NP /MT:16
 
 cd ..\server_config\images\odoo\17\data
-git add ./*
+git add *
 git commit -m "update project odoo odoo-17" -- .\
 git push
 
