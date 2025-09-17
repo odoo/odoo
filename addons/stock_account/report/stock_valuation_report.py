odoo/odoo

commit c5768f4c84e2b1fe88a29d5a5555912ebb99ce51

[FIX] stock_account: wrong value for variation
The report always add the add at_date context everywhere.
But when the date is today it will add an extra domain everywhere
and it could cause issue since it compares datetime with date.

Part-of: odoo/odoo#227567
Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

addons/stock_account/report/stock_valuation_report.py
@@ -30,8 +30,9 @@ class StockValuationReport(models.AbstractModel):
         # Check if date is a string instance
         if isinstance(date, str):
             date = fields.Date.from_string(date)
-
         if date == fields.Date.today():
+            date = False
+        if date:
             inventory_data = company.stock_value()
             accounting_data = company.stock_accounting_value()
         else:

commit 21f14c6e6a5cf25f647e7818462bd94efcaaed66

[FIX] stock_account: valuation report at date
commit c5768f4c84e2b1fe88a29d5a5555912ebb99ce51 introduced an error
We want to pass the date as arguement if it's set only.

addons/stock_account/report/stock_valuation_report.py
@@ -32,7 +32,7 @@ class StockValuationReport(models.AbstractModel):
             date = fields.Date.from_string(date)
         if date == fields.Date.today():
             date = False
-        if date:
+        if not date:
             inventory_data = company.stock_value()
             accounting_data = company.stock_accounting_value()
         else: