a1e9d5fa303a9b4ac19b0448259f1c970b9409cc
JF Aubert <ajf@odoo.com>
2025-09-17 14:34:55 +0200

    [FIX] stock: Fix quants selection
    Steps to reproduce the bug:
    
    Create a storable product “P” tracked by serial number
    
    Create a receipt for 100 units with serial numbers sn.001 to sn.100
    
    Create a delivery for 10 units,
    Odoo assigns serial numbers from sn.001 to sn.010,
    Validate
    
    Create a delivery for 3 units,
    Odoo assigns serial numbers from sn.011 to sn.013,
    Delete the 3 move lines,
    Add a line:
    you will see again the serial numbers sn.001 to sn.010 in the list
    
    Origin:
    
    This pr : https://github.com/odoo/odoo/pull/216035 removed the
    'on_hand' & 'in_stock' without removing their uses (search_default_*)
    
    Fix:
    Ensure a correct domain when adding a line.
    
    opw-5075144
    
    closes odoo/odoo#227689
    
    Related: odoo/enterprise#95032
    Signed-off-by: Quentin Wolfs (quwo) <quwo@odoo.com>

--- addons/stock/static/src/fields/stock_move_line_x2_many_field.js
+++ addons/stock/static/src/fields/stock_move_line_x2_many_field.js
@@ -44,15 +44,17 @@ export class SMLX2ManyField extends X2ManyField {
             ...context,
             single_product: true,
             list_view_ref: "stock.view_stock_quant_tree_simple",
-            search_default_on_hand: this.quantListViewShowOnHandOnly,
-            search_default_in_stock: true,
         };
         const productName = this.props.record.data.product_id.display_name;
         const title = _t("Add line: %s", productName);
         let domain = [
             ["product_id", "=", this.props.record.data.product_id.id],
             ["location_id", "child_of", this.props.context.default_location_id],
+            ["quantity", ">", 0.0],
         ];
+        if (this.quantListViewShowOnHandOnly) {
+            domain.push(["on_hand", "=", true]);
+        }
         if (this.dirtyQuantsData.size) {
             const notFullyUsed = [];
             const fullyUsed = [];
