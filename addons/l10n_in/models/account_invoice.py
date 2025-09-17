559c96b026e2054934e2d8393779d9eb0a8120d0
hatr-odoo <hatr@odoo.com>
2025-09-17 18:32:03 +0530

    [IMP] l10n_in_*: streamline HSN warnings
    With this commit, we remove the warning about mismatches between HSN code and
    product type. From now on, the product type will be derived directly from the
    HSN code: if the HSN starts with '99', it will be considered a 'service';
    otherwise, it will be treated as 'goods'. This simplifies the user experience by
    reducing unnecessary warnings. This will also cover cases where the product is
    service but the HSN is of goods for e.g. Job Work, Discount Line etc.
    
    Additionally, invalid/missing HSN on invoice line will now raise a blocking
    warning.
    
    task-5061450

--- addons/l10n_in/models/account_invoice.py
+++ addons/l10n_in/models/account_invoice.py
@@ -679,9 +679,13 @@ class AccountMove(models.Model):
     @api.model
     def _l10n_in_extract_digits(self, string):
         if not string:
-            return string
+            return ""
         matches = re.findall(r"\d+", string)
         return "".join(matches)
+
+    @api.model
+    def _l10n_in_is_service_hsn(self, hsn_code):
+        return self._l10n_in_extract_digits(hsn_code).startswith('99')
 
     @api.model
     def _l10n_in_round_value(self, amount, precision_digits=2):
