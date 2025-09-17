69b0c25c8c3d492d2d06d13d16c4e752379ee7d2
Alex Kühn <aku@odoo.com>
2025-09-17 16:37:17 +0000

    [IMP] mail: show linked thread preview below message
    Before this commit, when a thread was created from a message, the
    message lack visual to tell a thread is linked to it.
    
    This commit adds a preview of thread at the bottom of message when
    this is linked to a thread. Click on preview opens the linked thread.
    
    Task-4656448
    
    closes odoo/odoo#227896
    
    X-original-commit: 521bdcbac0fea9e008f10e7897364923ce43479e
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

---
+++ addons/mail/static/src/discuss/core/public_web/message_patch.js
@@ -0,0 +1,4 @@
+import { Message } from "@mail/core/common/message";
+import { SubChannelPreview } from "@mail/discuss/core/public_web/sub_channel_preview";
+
+Object.assign(Message.components, { SubChannelPreview });
