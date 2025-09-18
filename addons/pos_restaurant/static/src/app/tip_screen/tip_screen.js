fc90249ce337d5d00a1c1f17da0e581f44318a40
Meet Jivani [MEJI] <meji@odoo.com>
2025-09-18 14:54:50 +0530

    [FIX] point_of_sale: prevent tb when no payment line is selected in tipscreen
    Steps:
    - Enable Tips and "Add tip after payment" in restaurant.
    - Open a session and add any product(s).
    - Go to payment and add two card lines (first with full amount, second with 0).
    - Click on "Close Tab".
    
    Issue:
    - After redirecting to the Tip Screen, a traceback appears.
    
    Cause:
    - Clicking "Close Tab" clears all 0-amount payment lines. This can leave no
    selected payment line, but the Tip Screen still tries to access it.
    
    Fix:
    - Use the selected payment line if available, otherwise fallback to the first
    payment line.
    
    task-5095825
    
    closes odoo/odoo#227797
    
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/pos_restaurant/static/src/app/tip_screen/tip_screen.js
+++ addons/pos_restaurant/static/src/app/tip_screen/tip_screen.js
@@ -109,10 +109,10 @@ export class TipScreen extends Component {
     }
     async printTipReceipt() {
         const order = this.currentOrder;
-        const receipts = [
-            order.get_selected_paymentline().ticket,
-            order.get_selected_paymentline().cashier_receipt,
-        ];
+        const selectedPaymentLine = order.get_selected_paymentline() || order.payment_ids[0];
+        const receipts = [selectedPaymentLine?.ticket, selectedPaymentLine?.cashier_receipt].filter(
+            Boolean
+        );
         for (let i = 0; i < receipts.length; i++) {
             await this.printer.print(
                 TipReceipt,
