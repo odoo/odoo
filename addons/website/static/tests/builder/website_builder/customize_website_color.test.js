9494e37866f2b0d2b871d6473a7e028328eb080f
romo <romo@odoo.com>
2025-09-18 08:36:17 +0200

    [IMP] website: reset background when applying color combination
    It was difficult for the user to understand how the background
    option of the header and the footer works.
    
    The colorpickers using the CustomizeWebsite actions (header, footer)
    would keep the solid color or gradient when applying a color
    combination.
    The colorpickers not using these actions would reset the solid color
    or gradient when applying color combination.
    
    It was decided to adopt the same behavior for both type or colorpickers:
    both of them now reset the previous selection when applying a color
    combination. The user would have to choose the background color after.
    
    This commit completes the commit [c2126f00].
    
    task-5062183
    
    closes odoo/odoo#227124
    
    Signed-off-by: Robin Lejeune (role) <role@odoo.com>

--- addons/website/static/tests/builder/website_builder/customize_website_color.test.js
+++ addons/website/static/tests/builder/website_builder/customize_website_color.test.js
@@ -52,7 +52,8 @@ test("BuilderColorPicker with action “customizeWebsiteColor” is correctly di
     await def;
     expect.verifySteps([
         "set preset",
-        '/website/static/src/scss/options/colors/user_color_palette.scss {"test":4}',
+        '/website/static/src/scss/options/colors/user_color_palette.scss {"test-custom":"NULL","test":4}',
+        '/website/static/src/scss/options/user_values.scss {"test-gradient":"NULL"}',
         "asset reload",
     ]);
 
@@ -80,7 +81,8 @@ test("BuilderColorPicker with action “customizeWebsiteColor” is correctly di
     await def;
     expect.verifySteps([
         "set preset on solid color",
-        '/website/static/src/scss/options/colors/user_color_palette.scss {"test":3}',
+        '/website/static/src/scss/options/colors/user_color_palette.scss {"test-custom":"NULL","test":3}',
+        '/website/static/src/scss/options/user_values.scss {"test-gradient":"NULL"}',
         "asset reload",
     ]);
 
@@ -108,7 +110,8 @@ test("BuilderColorPicker with action “customizeWebsiteColor” is correctly di
     await def;
     expect.verifySteps([
         "set preset on gradient",
-        '/website/static/src/scss/options/colors/user_color_palette.scss {"test":4}',
+        '/website/static/src/scss/options/colors/user_color_palette.scss {"test-custom":"NULL","test":4}',
+        '/website/static/src/scss/options/user_values.scss {"test-gradient":"NULL"}',
         "asset reload",
     ]);
 
