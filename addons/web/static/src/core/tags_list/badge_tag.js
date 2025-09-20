e79fcbfba7d4b29752fdf1080387c93df3ff269b
Michaël Mattiello <mcm@odoo.com>
2025-09-20 12:17:56 +0200

    [IMP] web: introduce tag components
    This commit introduce two new components: BadgeTag and AvatarTag.
    These components should be used to render tags instead of relying on
    TagsList. TagsList will soon not render tags anymore.
    
    task-4660360

---
+++ addons/web/static/src/core/tags_list/badge_tag.js
@@ -0,0 +1,32 @@
+import { Component } from "@odoo/owl";
+import { mergeClasses } from "@web/core/utils/classname";
+import { useForwardRefToParent } from "@web/core/utils/hooks";
+
+export class BadgeTag extends Component {
+    static template = "web.BadgeTag";
+    static props = {
+        color: { type: Number, optional: true },
+        cssClass: { type: [String, Object], optional: true },
+        onClick: { type: Function, optional: true },
+        onDelete: { type: Function, optional: true },
+        ref: { optional: true },
+        slots: { optional: true },
+        text: { type: String, optional: true },
+        tooltip: { type: String, optional: true },
+    };
+    static defaultProps = {
+        color: 0,
+    };
+
+    get cssClass() {
+        return mergeClasses(
+            `o_badge badge rounded-pill lh-1 o_tag_color_${this.props.color}`,
+            { "cursor-pointer": this.props.onClick },
+            this.props.cssClass
+        );
+    }
+
+    setup() {
+        useForwardRefToParent("ref");
+    }
+}
