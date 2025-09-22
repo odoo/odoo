c73e0499b9d0d036fcea0d0addf4f6f634080a31
Giorgio Tocco (gito) <gito@odoo.com>
2025-09-22 11:38:40 +0200

    [IMP] mail, portal, *: enable emoji suggestions in portal chatter
    * = im_livechat
    
    This commit enables emoji suggestions in the portal chatters.
    
    task-4661468
    
    closes odoo/odoo#227951
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/im_livechat/static/src/core/web/suggestion_service_patch.js
+++ addons/im_livechat/static/src/core/web/suggestion_service_patch.js
@@ -4,7 +4,7 @@ import { patch } from "@web/core/utils/patch";
 
 patch(SuggestionService.prototype, {
     /** @override */
-    getSupportedDelimiters(thread) {
+    getSupportedDelimiters(thread, env) {
         const res = super.getSupportedDelimiters(...arguments);
         return thread.channel_type === "livechat"
             ? res.filter((delimiter) => delimiter.at(0) !== "#")
