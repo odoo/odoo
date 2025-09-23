df3c440a823c3667a1906b2ea6e914e315ff7f25
hapt-odoo <hapt@odoo.com>
2025-09-23 16:29:26 +0530

    [FIX] mrp: preserve product sequence when adding from catalog
    Before this commit:
    -------------------------
    When adding products to a Manufacturing Order (MO) or Bill of Materials (BOM)
    Via the catalog, the newly added products did not follow the order in which they
    were selected. Instead, they could appear in the middle or even at the top of
    the existing lines. This inconsistent behavior made it difficult for users to
    track the intended sequence of products, adding unnecessary complexity in
    planning.
    
    Steps to reproduce:
    -------------------------
    1. Install the 'mrp' module.
    2. Create or open an existing BOM or MO.
    3. Click on Catalog and add random products.
    4. Return to the form view → the product sequence is disrupted.
    
    After this commit:
    -----------------------
    - Products added from the catalog to an MO or BOM now retain the order in which
      they were selected. A proper sequence number is assigned to each new line,
      ensuring that the sequence remains consistent with user input.
    - This ensures a cleaner and more intuitive view, making it easier for users to
      Follow the creation process.
    
    Task Id: 4737887

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -3014,7 +3014,7 @@ class MrpProduction(models.Model):
             else:
                 entity.unlink()
         elif quantity > 0:
-            new_line_vals = self._get_new_catalog_line_values(product_id, quantity, **kwargs)
+            new_line_vals = self._get_new_catalog_line_values(product_id, quantity, child_field=child_field, **kwargs)
             command = Command.create(new_line_vals)
             self.write({child_field: [command]})
             new_line = self[child_field].filtered(lambda mv: mv.product_id.id == product_id)[-1:]
@@ -3025,10 +3025,11 @@ class MrpProduction(models.Model):
     def _update_catalog_line_quantity(self, line, quantity, **kwargs):
         line.product_uom_qty = quantity
 
-    def _get_new_catalog_line_values(self, product_id, quantity, **kwargs):
+    def _get_new_catalog_line_values(self, product_id, quantity, child_field=False, **kwargs):
         return {
             'product_id': product_id,
             'product_uom_qty': quantity,
+            'sequence': (self[child_field][-1:].sequence or 1) + 1,
         }
 
     def _is_display_stock_in_catalog(self):
