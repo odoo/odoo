758889e20e5cbcbddffb6dba6e12a8468cd43d0a
Max Whale <mawh@odoo.com>
2025-09-23 17:01:38 +0200

    [FIX] iot_drivers: fix blank homepage after checkout
    Due to the `odoo.service` starting before the `rc.local` file has finished
    updating the Odoo code, you can end up with the old Python code running
    but the updated JS code loading in the browser. This can lead to a blank
    screen as a traceback occurs trying to read the MAC address.
    
    We fix this in two ways:
    - A quick fix, which is just making the JS code robust to the missing
      value to avoid crashing.
    - A proper fix that will require a new image, as it modifies the
      `odoo.service` file to add `rc-local.service` as a dependency. This
      means Odoo will not start until `rc.local` has finished executing.

--- addons/iot_drivers/static/src/app/components/SingleData.js
+++ addons/iot_drivers/static/src/app/components/SingleData.js
@@ -21,7 +21,7 @@ export class SingleData extends Component {
             /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[\w]*))?)/;
 
         const regex = new RegExp(expression);
-        if (this.props.value.match(regex)) {
+        if (this.props.value?.match(regex)) {
             return true;
         } else {
             return false;
