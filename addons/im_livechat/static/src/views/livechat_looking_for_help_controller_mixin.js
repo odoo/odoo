372bde5ca1911eb87de778a53d1978e1a278b191
Giorgio Tocco (gito) <gito@odoo.com>
2025-09-23 15:55:34 +0200

    [IMP] im_livechat, mail: info side panel tags improvements
    This commit introduces the following improvements to the livechat
    info side panel tags feature:
    - Characters in the tags matching the search term are highlighted.
    - The list of tags is navigable and the active item is highlighted.
    - Small UI changes on edit and add icons.
    - Tags are kept synchronized in the "looking for help" sessions view.
    - On mobile the tag list is now show in the bottom sheet.
    
    This commit also fixes the issue of creating a tag with only
    whitespace.
    
    task-5065034

--- addons/im_livechat/static/src/views/livechat_looking_for_help_controller_mixin.js
+++ addons/im_livechat/static/src/views/livechat_looking_for_help_controller_mixin.js
@@ -14,16 +14,19 @@ export const LivechatLookingForHelpReloadMixin = (ViewController) =>
             const busService = useService("bus_service");
             this.reloadDebounced = useDebounced(() => this.model.load(), RELOAD_DEBOUNCE_DELAY);
             this.lookingForHelpOnUpdate = this.lookingForHelpOnUpdate.bind(this);
+            this.tagsOnUpdate = this.tagsOnUpdate.bind(this);
             busService.addChannel("im_livechat.looking_for_help");
             busService.subscribe(
                 "im_livechat.looking_for_help/update",
                 this.lookingForHelpOnUpdate
             );
+            busService.subscribe("im_livechat.looking_for_help/tags", this.tagsOnUpdate);
             onWillDestroy(() => {
                 busService.unsubscribe(
                     "im_livechat.looking_for_help/update",
                     this.lookingForHelpOnUpdate
                 );
+                busService.unsubscribe("im_livechat.looking_for_help/tags", this.tagsOnUpdate);
                 busService.deleteChannel("im_livechat.looking_for_help");
             });
         }
@@ -42,6 +45,16 @@ export const LivechatLookingForHelpReloadMixin = (ViewController) =>
                 .filter(Boolean);
             if (recordIdsToRemove.length) {
                 this.model.root._removeRecords(recordIdsToRemove);
+            }
+        }
+
+        tagsOnUpdate({ channel_id, tag_ids }) {
+            const channel = this.model.root.records.find((r) => r.resId === channel_id);
+            if (
+                JSON.stringify(channel?.data.livechat_conversation_tag_ids._currentIds) !==
+                JSON.stringify(tag_ids)
+            ) {
+                channel?.load();
             }
         }
     };
