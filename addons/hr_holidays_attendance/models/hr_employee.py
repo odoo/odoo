19e58230ee8e78848cf3b320dd7ac5f8b55d9c36
Martin Lefèvre <mlef@odoo.com>
2025-09-23 13:31:22 +0200

    [FIX] hr_holidays_attendance: Fix monthly hours stack trace
    A stack trace was appearing when clicking the monthly hours smart button
    when "Display Extra Hours" was checking in the settings.
    It was due to a typo when combiniing domains and also because the same
    domain was reused for both attendances and allocations.
    Since the date fields in the attendances and allocations models don't
    have the same name, I had to convert it accordingly.
    
    task-5106638

--- addons/hr_holidays_attendance/models/hr_employee.py
+++ addons/hr_holidays_attendance/models/hr_employee.py
@@ -11,15 +11,32 @@ class HrEmployee(models.Model):
     def get_overtime_data(self, domain=None, employee_id=None):
         overtime_data = super().get_overtime_data(domain, employee_id)
         domain = [] if domain is None else domain
+
+        # Given domain will use attendance fields
+        # We have to convert the dates to the allocation's fields name
+        def attendance_to_allocation_domain(d):
+            if d[0] == 'check_in':
+                return ['date_from', *d[1:]]
+            elif d[0] == 'check_out':
+                return ['date_to', *d[1:]]
+            else:
+                return d
+
+        domain = [attendance_to_allocation_domain(d) for d in domain]
+
+        combined_domain = Domain.AND(
+            [
+                domain,
+                [
+                    ('holiday_status_id.overtime_deductible', '=', True),
+                    ('state', '=', 'confirm'),
+                ],
+            ]
+        )
         overtime_adjustments = {
             allocation[0].id: allocation[1]
             for allocation in self.env["hr.leave.allocation"]._read_group(
-                domain=Domain.AND(
-                    domain,
-                    [
-                        ('holiday_status_id.overtime_deductible', '=', True),
-                        ('state', '=', 'confirm'),
-                    ]),
+                domain=combined_domain,
                 groupby=['employee_id'],
                 aggregates=['number_of_hours_display:sum']
             )
