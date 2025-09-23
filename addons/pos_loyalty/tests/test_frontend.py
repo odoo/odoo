89224bd44625401adacb0b0b270cb4733a366e06
Jitendra Prajapat <jipr@odoo.com>
2025-09-23 13:22:02 +0000

    [FIX] point_of_sale, pos_loyalty: always display Gift Card and E-Wallet products
    Before this commit:
    ===================
    If the PoS configuration is restricted to specific categories, but the
    Gift Card and E-Wallet products do not have any category assigned, these
    products will not be visible in the PoS UI.
    
    After this commit:
    ====================
    Gift Card and E-Wallet products are now always visible in the PoS UI, regardless
    of whether a PoS category is assigned or not.
    
    Purpose:
    ============
    Gift Card and E-Wallet products should always be available
    for sale in the PoS. This ensures they can be sold physically from the PoS UI
    without requiring category assignment.
    
    Task-5103770
    
    X-original-commit: f2722bc888762eae99a2e36a8dfc81b554438bfd

--- addons/pos_loyalty/tests/test_frontend.py
+++ addons/pos_loyalty/tests/test_frontend.py
@@ -507,6 +507,16 @@ class TestUi(TestPointOfSaleHttpCommon):
         """
         Test for gift card program.
         """
+        # Ensure Gift Card product is displayed in the PoS session when the selected category is enabled in the configuration.
+        self.main_pos_config.current_session_id.close_session_from_ui()
+        pos_category = self.env['pos.category'].search([], limit=1)
+        self.main_pos_config.write({
+            'limit_categories': True,
+            'iface_available_categ_ids': [(6, 0, [pos_category.id])],
+        })
+        self.whiteboard_pen.write({'pos_categ_ids': [(6, 0, [pos_category.id])]})
+        self.main_pos_config.open_ui()
+
         self.pos_user.write({
             'group_ids': [
                 (4, self.env.ref('stock.group_stock_user').id),
@@ -541,6 +551,16 @@ class TestUi(TestPointOfSaleHttpCommon):
         - Collect points in EWalletProgramTour1.
         - Use points in EWalletProgramTour2.
         """
+        # Ensure eWallet product is displayed in the PoS session when the selected category is enabled in the configuration.
+        self.main_pos_config.current_session_id.close_session_from_ui()
+        pos_category = self.env['pos.category'].search([], limit=1)
+        self.main_pos_config.write({
+            'limit_categories': True,
+            'iface_available_categ_ids': [(6, 0, [pos_category.id])],
+        })
+        self.whiteboard_pen.write({'pos_categ_ids': [(6, 0, [pos_category.id])]})
+        self.main_pos_config.open_ui()
+
         LoyaltyProgram = self.env['loyalty.program']
         # Deactivate all other programs to avoid interference
         (LoyaltyProgram.search([])).write({'pos_ok': False})
@@ -567,6 +587,7 @@ class TestUi(TestPointOfSaleHttpCommon):
         ewallet_bbb = self.env['loyalty.card'].search([('partner_id', '=', partner_bbb.id), ('program_id', '=', ewallet_program.id)])
         self.assertEqual(len(ewallet_bbb), 1)
         self.assertAlmostEqual(ewallet_bbb.points, 10, places=2)
+        self.desk_pad.write({'pos_categ_ids': [(6, 0, [pos_category.id])]})
         # Run the tour consume ewallets.
         self.start_pos_tour("EWalletProgramTour2")
         # Check that ewallets are consumed for partner_aaa.
