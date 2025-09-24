70b7856d5faba9e509f55a5d665691ebcab5979d
Julien Mougenot <jum@odoo.com>
2025-09-24 13:41:12 +0000

    [FIX] web: Hoot - issue levels
    This commit associates separate "issue levels" to the test runner's
    internal logger. These affect the logging and reporting of issues, i.e.
    errors and warnings:
    
    - suppressed (by 'test.todo'): issues are traced in the console but not
    reported in test results;
    
    - trace (default in test runs): issues are traced in the console and
    reported in test results;
    
    - global: issues are warned/errored in the console with "HOOT" prefix
    (i.e. won't interrupt the test run);
    
    - critical (default outside of test runs): issues are warned/errored in
    the console without "HOOT" prefix, thus interrupting the whole test run.
    
    This fix should hopefully solve errors that were too quiet before test
    run, or too "important" during the run.
    
    closes odoo/odoo#228771
    
    X-original-commit: af53dc3a95289960f4643000394c59abc254a670
    Signed-off-by: Michaël Mattiello (mcm) <mcm@odoo.com>
    Signed-off-by: Julien Mougenot (jum) <jum@odoo.com>

--- addons/web/static/lib/hoot/core/logger.js
+++ addons/web/static/lib/hoot/core/logger.js
@@ -20,6 +20,7 @@ const {
         trace: $trace,
         warn: $warn,
     },
+    Object: { entries: $entries, getOwnPropertyDescriptors: $getOwnPropertyDescriptors },
 } = globalThis;
 
 //-----------------------------------------------------------------------------
@@ -57,63 +58,27 @@ function unstyledArguments(args) {
     return [args.join(" ")];
 }
 
-const DEBUG_PREFIX = ["DEBUG", getColorHex("purple")];
-const DEFAULT_PREFIX = ["HOOT", getColorHex("primary")];
-const ERROR_PREFIX = ["ERROR", getColorHex("rose")];
-const WARNING_PREFIX = ["WARNING", getColorHex("amber")];
-let nextNetworkLogId = 1;
-
-//-----------------------------------------------------------------------------
-// Exports
-//-----------------------------------------------------------------------------
-
-/**
- * @param {string} prefix
- * @param {string} title
- */
-export function makeNetworkLogger(prefix, title) {
-    const id = nextNetworkLogId++;
-    return {
-        /**
-         * Request logger: blue-ish.
-         * @param {() => any} getData
-         */
-        async logRequest(getData) {
-            if (!logger.allows("debug")) {
-                return;
-            }
-            const color = `color: #66e`;
-            const styles = [`${color}; font-weight: bold;`, color];
-            $groupCollapsed(`-> %c${prefix}#${id}%c<${title}>`, ...styles, await getData());
-            $trace("request trace");
-            $groupEnd();
-        },
-        /**
-         * Response logger: orange.
-         * @param {() => any} getData
-         */
-        async logResponse(getData) {
-            if (!logger.allows("debug")) {
-                return;
-            }
-            const color = `color: #f80`;
-            const styles = [`${color}; font-weight: bold;`, color];
-            $log(`<- %c${prefix}#${id}%c<${title}>`, ...styles, await getData());
-        },
-    };
-}
-
-export const LOG_LEVELS = {
-    runner: 0,
-    suites: 1,
-    tests: 2,
-    debug: 3,
-};
-
-export const logger = {
+class Logger {
     /** @private */
-    currentLevel: urlParams.loglevel ?? LOG_LEVELS.runner,
-    suppressed: "",
+    issueLevel;
+    /** @private */
+    logLevel;
+
+    constructor(logLevel, issueLevel) {
+        this.logLevel = logLevel;
+        this.issueLevel = issueLevel;
+
+        // Pre-bind all methods for ease of use
+        for (const [key, desc] of $entries($getOwnPropertyDescriptors(Logger.prototype))) {
+            if (key !== "constructor" && typeof desc.value === "function") {
+                this[key] = this[key].bind(this);
+            }
+        }
+    }
+
+    get global() {
+        return new Logger(this.logLevel, ISSUE_LEVELS.global);
+    }
 
     // Standard console methods
 
@@ -122,19 +87,32 @@ export const logger = {
      */
     debug(...args) {
         $debug(...styledArguments(args));
-    },
+    }
     /**
      * @param {...any} args
      */
     error(...args) {
-        if (logger.suppressed) {
-            $groupCollapsed(...styledArguments([logger.suppressed], ...ERROR_PREFIX));
-            $trace(...args);
-            $groupEnd();
-        } else {
-            $trace(...styledArguments(args, ...ERROR_PREFIX));
+        switch (this.issueLevel) {
+            case ISSUE_LEVELS.suppressed: {
+                $groupCollapsed(...styledArguments(["suppressed"], ...ERROR_PREFIX));
+                $trace(...args);
+                $groupEnd();
+                break;
+            }
+            case ISSUE_LEVELS.trace: {
+                $trace(...styledArguments(args, ...ERROR_PREFIX));
+                break;
+            }
+            case ISSUE_LEVELS.global: {
+                $error(...styledArguments(args));
+                break;
+            }
+            default: {
+                $error(...args);
+                break;
+            }
         }
-    },
+    }
     /**
      * @param {any} arg
      * @param {() => any} callback
@@ -143,31 +121,40 @@ export const logger = {
         $groupCollapsed(...styledArguments([title]));
         callback();
         $groupEnd();
-    },
+    }
     /**
      * @param  {...any} args
      */
     table(...args) {
         $table(...args);
-    },
+    }
     /**
      * @param  {...any} args
      */
     trace(...args) {
         $trace(...args);
-    },
+    }
     /**
      * @param {...any} args
      */
     warn(...args) {
-        if (logger.suppressed) {
-            $groupCollapsed(...styledArguments([logger.suppressed], ...WARNING_PREFIX));
-            $trace(...args);
-            $groupEnd();
-        } else {
-            $warn(...styledArguments(args));
+        switch (this.issueLevel) {
+            case ISSUE_LEVELS.suppressed: {
+                $groupCollapsed(...styledArguments(["suppressed"], ...WARNING_PREFIX));
+                $trace(...args);
+                $groupEnd();
+                break;
+            }
+            case ISSUE_LEVELS.global: {
+                $warn(...styledArguments(args));
+                break;
+            }
+            default: {
+                $warn(...args);
+                break;
+            }
         }
-    },
+    }
 
     // Level-specific methods
 
@@ -175,16 +162,16 @@ export const logger = {
      * @param {...any} args
      */
     logDebug(...args) {
-        if (!logger.allows("debug")) {
+        if (!this.canLog("debug")) {
             return;
         }
         $debug(...styledArguments(args, ...DEBUG_PREFIX));
-    },
+    }
     /**
      * @param {import("./suite").Suite} suite
      */
     logSuite(suite) {
-        if (!logger.allows("suites")) {
+        if (!this.canLog("suites")) {
             return;
         }
         const args = [`${stringify(suite.fullName)} ended`];
@@ -208,12 +195,12 @@ export const logger = {
             );
         }
         $log(...styledArguments(args));
-    },
+    }
     /**
      * @param {import("./test").Test} test
      */
     logTest(test) {
-        if (!logger.allows("tests")) {
+        if (!this.canLog("tests")) {
             return;
         }
         const { fullName, lastResults } = test;
@@ -226,63 +213,165 @@ export const logger = {
                 `ms)`,
             ])
         );
-    },
+    }
     /**
      * @param {[label: string, color: string]} prefix
      * @param {...any} args
      */
     logTestEvent(prefix, ...args) {
         $log(...styledArguments(args, ...prefix));
-    },
+    }
     /**
      * @param {...any} args
      */
     logRun(...args) {
-        if (!logger.allows("runner")) {
+        if (!this.canLog("runner")) {
             return;
         }
         $log(...styledArguments(args));
-    },
+    }
     /**
      * @param {...any} args
      */
     logGlobal(...args) {
         $dir(...unstyledArguments(args));
-    },
-    /**
-     * @param {...any} args
-     */
-    logGlobalError(...args) {
-        $error(...styledArguments(args));
-    },
-    /**
-     * @param {...any} args
-     */
-    logGlobalWarning(...args) {
-        $warn(...styledArguments(args));
-    },
+    }
 
     // Other methods
 
     /**
      * @param {keyof typeof LOG_LEVELS} level
      */
-    allows(level) {
-        return logger.currentLevel >= LOG_LEVELS[level];
-    },
+    canLog(level) {
+        return this.logLevel >= LOG_LEVELS[level];
+    }
+    /**
+     * @param {keyof typeof ISSUE_LEVELS} level
+     */
+    setIssueLevel(level) {
+        const restoreIssueLevel = () => {
+            this.issueLevel = previous;
+        };
+        const previous = this.issueLevel;
+        this.issueLevel = ISSUE_LEVELS[level];
+        return restoreIssueLevel;
+    }
     /**
      * @param {keyof typeof LOG_LEVELS} level
      */
-    setLevel(level) {
-        logger.currentLevel = LOG_LEVELS[level];
-    },
-    /**
-     * @param {string} reason
-     */
-    suppressIssues(reason) {
-        logger.suppressed = reason || "(suppressed)";
-        return function restore() {
-            logger.suppressed = "";
+    setLogLevel(level) {
+        const restoreLogLevel = () => {
+            this.logLevel = previous;
         };
-    },
+        const previous = this.logLevel;
+        this.logLevel = LOG_LEVELS[level];
+        return restoreLogLevel;
+    }
+}
+
+const DEBUG_PREFIX = ["DEBUG", getColorHex("purple")];
+const DEFAULT_PREFIX = ["HOOT", getColorHex("primary")];
+const ERROR_PREFIX = ["ERROR", getColorHex("rose")];
+const WARNING_PREFIX = ["WARNING", getColorHex("amber")];
+let nextNetworkLogId = 1;
+
+//-----------------------------------------------------------------------------
+// Exports
+//-----------------------------------------------------------------------------
+
+/**
+ * @param {string} prefix
+ * @param {string} title
+ */
+export function makeNetworkLogger(prefix, title) {
+    const id = nextNetworkLogId++;
+    return {
+        /**
+         * Request logger: blue-ish.
+         * @param {() => any} getData
+         */
+        async logRequest(getData) {
+            if (!logger.canLog("debug")) {
+                return;
+            }
+            const color = `color: #66e`;
+            const styles = [`${color}; font-weight: bold;`, color];
+            $groupCollapsed(`-> %c${prefix}#${id}%c<${title}>`, ...styles, await getData());
+            $trace("request trace");
+            $groupEnd();
+        },
+        /**
+         * Response logger: orange.
+         * @param {() => any} getData
+         */
+        async logResponse(getData) {
+            if (!logger.canLog("debug")) {
+                return;
+            }
+            const color = `color: #f80`;
+            const styles = [`${color}; font-weight: bold;`, color];
+            $log(`<- %c${prefix}#${id}%c<${title}>`, ...styles, await getData());
+        },
+    };
+}
+
+export const ISSUE_LEVELS = {
+    /**
+     * Suppressed:
+     *
+     * Condition:
+     *  - typically: in "todo" tests where issues should be ignored
+     *
+     * Effect:
+     *  - all errors and warnings are replaced by 'trace' calls
+     */
+    suppressed: 0,
+    /**
+     * Trace:
+     *
+     * Condition:
+     *  - default level within a test run
+     *
+     * Effect:
+     *  - warnings are left as-is;
+     *  - errors are replaced by 'trace' calls, so that the actual console error
+     *    comes from the test runner with a summary of all failed reasons.
+     */
+    trace: 1,
+    /**
+     * Global:
+     *
+     * Condition:
+     *  - errors which should be reported globally but not interrupt the run
+     *
+     * Effect:
+     *  - warnings are left as-is;
+     *  - errors are wrapped with a "HOOT" prefix, as to not stop the current test
+     *    run. Can typically be used to log test failed reasons.
+     */
+    global: 2,
+    /**
+     * Critical:
+     *
+     * Condition:
+     *  - any error compromising the whole test run and should cancel or interrupt it
+     *  - default level outside of a test run (import errors, module root errors, etc.)
+     *
+     * Effect:
+     *  - warnings are left as-is;
+     *  - errors are left as-is, as to tell the server test to stop the current
+     *    (Python) test.
+     */
+    critical: 3,
 };
+export const LOG_LEVELS = {
+    runner: 0,
+    suites: 1,
+    tests: 2,
+    debug: 3,
+};
+
+export const logger = new Logger(
+    urlParams.loglevel ?? LOG_LEVELS.runner,
+    ISSUE_LEVELS.critical // by default, all errors are "critical", i.e. should abort the whole run
+);
