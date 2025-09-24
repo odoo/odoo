aab5cc5e074080ebf1f28ccf83d77d49ee3e79b1
jeanschoenlaub <jesc@odoo.com>
2025-09-24 15:02:58 +0200

    [IMP] purchase_stock: catalog suggest X section
    Adding all suggestions with a selected section, now
    adds the products to the section and updates
    section line count.
    
    BEFORE: "Add all" button would add suggestions to
    the top of the po and remove products in domain but
    not suggested from the entire po
    
    NOW: "Add all" button adds suggestion to the selected
    sections and removes products in domain but not suggested
    only in the selected section (incl. the no section).
    Also line count is now updated when using "Add all"
    
    Also added some test for the suggestion categories.
    
    task#5114649
    
    (cherry picked from commit 19b2ea12b36c70c931d27efe6eeb57939ee88bb8)
    
    Part-of: odoo/odoo#234044
    Signed-off-by: Steve Van Essche <svs@odoo.com>

--- addons/purchase_stock/models/purchase_order.py
+++ addons/purchase_stock/models/purchase_order.py
@@ -180,17 +180,24 @@ class PurchaseOrder(models.Model):
                 self.partner_id,
                 self
             )
-            existing_po_lines = self.order_line.filtered(lambda pol: pol.product_id == product)
-            if existing_po_lines:
+            existing_lines = self.order_line.filtered(lambda pol: pol.product_id == product)
+            if section_id := ctx.get("section_id"):
+                existing_lines = existing_lines.filtered(lambda pol: pol.get_parent_section_line().id == section_id)
+                suggest_line["sequence"] = self._get_new_line_sequence("order_line", section_id)
+            else:
+                existing_lines = existing_lines.filtered(lambda pol: not pol.parent_id)  # lines with no sections
+            if existing_lines:
                 # Collapse into 1 or 0 po line, discarding previous data in favor of suggested qtys
-                to_unlink = existing_po_lines if product.suggested_qty == 0 else existing_po_lines[:-1]
+                to_unlink = existing_lines if product.suggested_qty == 0 else existing_lines[:-1]
                 po_lines_commands += [Command.unlink(line.id) for line in to_unlink]
                 if product.suggested_qty > 0:
-                    po_lines_commands.append(Command.update(existing_po_lines[-1].id, suggest_line))
+                    po_lines_commands.append(Command.update(existing_lines[-1].id, suggest_line))
             elif product.suggested_qty > 0:
                 po_lines_commands.append(Command.create(suggest_line))
 
         self.order_line = po_lines_commands
+        # Return the change in number of po_lines for the given section
+        return sum({"CREATE": 1, "UNLINK": -1}.get(line[0].name, 0) for line in po_lines_commands)
 
     def button_approve(self, force=False):
         self.order_line._set_date_promised()
