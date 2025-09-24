c49a43cb69359daa61ac48059dd7dc314dc2fe5b
dhba <dhba@odoo.com>
2025-09-24 14:22:50 +0530

    [FIX] html_editor: enter before star or icon elements
    Before this commit:
    
    - Pressing Enter twice before a star or icon caused the previous paragraph
      to incorrectly end up containing a duplicate element.
    
    After this commit:
    
    - Pressing Enter before or after a star or icon no longer duplicates the
      element in the previous paragraph.
    
    task-5079270

--- addons/html_editor/static/tests/icon.test.js
+++ addons/html_editor/static/tests/icon.test.js
@@ -3,7 +3,7 @@ import { click, tick, waitFor } from "@odoo/hoot-dom";
 import { setupEditor } from "./_helpers/editor";
 import { animationFrame } from "@odoo/hoot-mock";
 import { getContent, setContent, setSelection } from "./_helpers/selection";
-import { undo } from "./_helpers/user_actions";
+import { splitBlock, undo } from "./_helpers/user_actions";
 import { contains } from "@web/../tests/web_test_helpers";
 import { expectElementCount } from "./_helpers/ui_expectations";
 
@@ -324,4 +324,18 @@ describe("selection", () => {
             `<p contenteditable="false">abc[<span class="fa fa-glass" contenteditable="false">\u200b</span>]def</p>`
         );
     });
+});
+
+test("should insert two empty paragraphs when Enter is pressed twice before the icon element", async () => {
+    const { el, editor } = await setupEditor(
+        `<p>[]<span class="fa fa-glass" contenteditable="false"></span></p>`
+    );
+    splitBlock(editor);
+    expect(getContent(el)).toBe(
+        `<p><br></p><p>\ufeff[]<span class="fa fa-glass" contenteditable="false">\u200B</span>\ufeff</p>`
+    );
+    splitBlock(editor);
+    expect(getContent(el)).toBe(
+        `<p><br></p><p><br></p><p>\ufeff[]<span class="fa fa-glass" contenteditable="false">\u200B</span>\ufeff</p>`
+    );
 });
