1207291db6d4dcada003207f8452376efc8662eb
Julien (jula) <jula@odoo.com>
2025-09-24 13:48:25 +0200

    [FIX] website: scroll on new entry in faq horizontal
    __Current behavior before commit:__
    Since [this PR][1], the scrolling is not done on `#wrapwrap` anymore.
    Now when adding a new entry to the snippet faq horizontal, the page
    doesn't scroll automatically to the new entry.
    
    This behavior has actually never worked because [the PR that introduced
    faq horizontal][2] was merged just after [the PR that moved the
    scrolling to the document element][1].
    
    __Description of the fix:__
    Get `scrollTop` and `scrollHeight` from `scrollingElement` instead of
    `#wrapwrap` and add a test tour.
    
    __Steps to reproduce:__
    1. Open the Website builder.
    2. Drag a Text block
    3. Choose the FAQ horizontal snippet
    4. Click on "Add New"
    => The page doesn't scroll to the new entry.
    
    [1]: https://github.com/odoo/odoo/pull/98429
    [2]: https://github.com/odoo/odoo/pull/176438
    
    closes odoo/odoo#228396
    
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- addons/website/static/src/snippets/s_faq_horizontal/options.js
+++ addons/website/static/src/snippets/s_faq_horizontal/options.js
@@ -5,17 +5,16 @@ options.registry.faqHorizontalMultipleItems = options.registry.MultipleItems.ext
         // Find the iframe and its #wrapwrap
         const iframe = document.querySelector('.o_iframe');
         const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-        const wrapwrap = iframeDocument.getElementById('wrapwrap');
+        const scrollingEl = iframeDocument.scrollingElement;
 
         const topics = this.$target[0].getElementsByClassName('s_faq_horizontal_entry');
         const newTopic = topics[topics.length - 1];
         const newTopicRect = newTopic.getBoundingClientRect();
-        const wrapwrapRect = wrapwrap.getBoundingClientRect();
 
-        const scrollTop = wrapwrap.scrollTop;
-        const centerY = (newTopicRect.top - wrapwrapRect.top) + scrollTop - (wrapwrap.clientHeight / 2) + (newTopicRect.height / 2);
+        const scrollTop = scrollingEl.scrollTop;
+        const centerY = newTopicRect.top + scrollTop - (scrollingEl.clientHeight / 2) + (newTopicRect.height / 2);
 
-        wrapwrap.scrollTo({
+        scrollingEl.scrollTo({
             top: centerY,
             behavior: 'smooth'
         });
