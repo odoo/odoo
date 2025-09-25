ff12c1d2e1719e80223fe27f9ea1f2131b370767
Abdelouahab (abla) <abla@odoo.com>
2025-09-25 14:16:05 +0000

    [FIX] hr_holidays: only list time off types requiring allocation
    To reproduce:
    =============
    - Create time off tyoe that does not require allocation
    - Go to timeoff > Allocation > Group allocation
    -> you can select the created time off type
    
    Problem:
    ========
    we didn't have a domain on `holiday_status_id` field, so all time off types were
    available for selection.
    
    Solution:
    =========
    Add a domain on `holiday_status_id` field to only show time off types that require
    allocations the same way as when creating a single allocation.
    
    opw-5113959
    
    X-original-commit: 43f1d68b4b4b721d0a7dc3da668b014471a34e01

--- addons/hr_holidays/wizard/hr_leave_allocation_generate_multi_wizard.py
+++ addons/hr_holidays/wizard/hr_leave_allocation_generate_multi_wizard.py
@@ -2,17 +2,27 @@
 
 from odoo import _, api, fields, models
 from odoo.tools.date_intervals import HOURS_PER_DAY
+from odoo.osv import expression
 
 
 class HrLeaveAllocationGenerateMultiWizard(models.TransientModel):
     _name = 'hr.leave.allocation.generate.multi.wizard'
     _description = 'Generate time off allocations for multiple employees'
 
+    def _domain_holiday_status_id(self):
+        domain = [
+            ('company_id', 'in', self.env.companies.ids + [False]),
+            ('requires_allocation', '=', True),
+        ]
+        if self.env.user.has_group('hr_holidays.group_hr_holidays_user'):
+            return domain
+        return expression.AND([domain, [('employee_requests', '=', True)]])
+
     name = fields.Char("Description", compute="_compute_name", store=True, readonly=False)
     duration = fields.Float(string="Allocation")
     holiday_status_id = fields.Many2one(
         "hr.leave.type", string="Time Off Type", required=True,
-        domain="[('company_id', 'in', [company_id, False])]")
+        domain=_domain_holiday_status_id)
     request_unit = fields.Selection(related="holiday_status_id.request_unit")
     allocation_mode = fields.Selection([
         ('employee', 'By Employee'),
