5304f0f849351dae83dbb4e64892797bfa3774bc
Alex Kühn <aku@odoo.com>
2025-09-25 06:57:59 +0000

    [FIX] im_livechat: message translation for agent when not member
    Before this commit, when an agent that is not member of livechat but
    is peeking the conversation, the agent could not translate the
    message.
    
    This happens because the translation feature is limited to internal
    users, but this was determined based on the self member relational
    field. This works when the agent is a member but when not a member
    this was arbitrarily disabling the feature.
    
    This commit fixes the issue by looking at whether the user is
    internal or not, based on self persona independently on whether the
    agent is member or not of the conversation.
    
    Task-5111383
    
    closes odoo/odoo#229035
    
    X-original-commit: 28beea1be95bb9f50947d25fe2efc0be5d28cf7f
    Related: odoo/enterprise#95767
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/im_livechat/static/tests/translation.test.js
+++ addons/im_livechat/static/tests/translation.test.js
@@ -6,14 +6,14 @@ import { defineLivechatModels } from "./livechat_test_helpers";
 describe.current.tags("desktop");
 defineLivechatModels();
 
-test("message translation in livechat", async () => {
+test("message translation in livechat (agent is member)", async () => {
     const pyEnv = await startServer();
     const channelId = pyEnv["discuss.channel"].create({
         anonymous_name: "Visitor",
         channel_type: "livechat",
         channel_member_ids: [
             Command.create({ partner_id: serverState.partnerId }),
-            Command.create({ partner_id: serverState.publicPartnerId }),
+            Command.create({ guest_id: pyEnv["mail.guest"].create({ name: "Mario" }) }),
         ],
     });
     pyEnv["mail.message"].create({
@@ -26,4 +26,24 @@ test("message translation in livechat", async () => {
     await contains(".o-mail-Message");
     await click("[title='Expand']");
     await contains(".o-dropdown-item:contains('Translate')");
+});
+
+test("message translation in livechat (agent is not member)", async () => {
+    const pyEnv = await startServer();
+    const channelId = pyEnv["discuss.channel"].create({
+        anonymous_name: "Visitor",
+        channel_type: "livechat",
+        channel_member_ids: [
+            Command.create({ guest_id: pyEnv["mail.guest"].create({ name: "Mario" }) }),
+        ],
+    });
+    pyEnv["mail.message"].create({
+        body: "Mai mettere l'ananas sulla pizza!",
+        model: "discuss.channel",
+        res_id: channelId,
+    });
+    await start();
+    await openDiscuss(channelId);
+    await contains(".o-mail-Message");
+    await contains("[title='Translate']");
 });
