14ac8c9aaa03778a3bcfa78c7b64e714e7781c36
Yagnik ✗ (yagp) <yagp@odoo.com>
2025-09-25 12:54:35 +0530

    [IMP] l10n_tw*: rely on VAT for company/individual distinction
    Use the 'vat' field to compute 'is_company' to determine whether a partner
    is a company or an individual:
    
    The Unified Business Number (UBN, 統一編號) is the number assigned to businesses
    within Taiwan for tax (VAT) purposes. The number consists of 8 digits, the
    last being a check digit.
    
    Task-4714892

--- addons/l10n_tw_edi_ecpay/models/res_partner.py
+++ addons/l10n_tw_edi_ecpay/models/res_partner.py
@@ -7,3 +7,12 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     invoice_edi_format = fields.Selection(selection_add=[('tw_ecpay', "ECPay")])
+
+    def _compute_is_company(self):
+        l10n_tw_partners = self.filtered(lambda p: p.vat and p.country_code == 'TW')
+        for partner in l10n_tw_partners:
+            partner.is_company = False
+            if partner.vat.isdigit() and len(partner.vat) == 8:
+                partner.is_company = True
+
+        super(ResPartner, self - l10n_tw_partners)._compute_is_company()
