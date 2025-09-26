cf2358bd35dde81ec4ae30bd1d895d9e13a21605
kcv-odoo <kcv@odoo.com>
2025-09-26 11:32:57 +0530

    [IMP] payment_iyzico: support webhook
    This commit add webhook for the Iyzico payment provider.
    
    task-5067754

--- addons/payment_iyzico/tests/test_processing_flows.py
+++ addons/payment_iyzico/tests/test_processing_flows.py
@@ -31,3 +31,18 @@ class TestProcessingFlows(IyzicoCommon, PaymentHttpCommon):
         ) as process_mock:
             self._make_http_post_request(url, data=self.return_data)
         self.assertEqual(process_mock.call_count, 1)
+
+    @mute_logger('odoo.addons.payment_iyzico.controllers.main')
+    def test_webhook_notification_triggers_processing(self):
+        """Test that receiving a valid webhook notification triggers the processing of the
+        payment data."""
+        self._create_transaction('redirect')
+        url = self._build_url(const.WEBHOOK_ROUTE)
+        with patch(
+            'odoo.addons.payment.models.payment_provider.PaymentProvider._send_api_request',
+            return_value=self.payment_data
+        ), patch(
+            'odoo.addons.payment.models.payment_transaction.PaymentTransaction._process'
+        ) as process_mock:
+            self._make_json_request(url, data=self.webhook_data)
+        self.assertEqual(process_mock.call_count, 1)
