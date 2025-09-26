f613dfe1bc0fa5a863009e22ee6532b2a685479f
Adrien Guilliams (adgu) <adgu@odoo.com>
2025-09-26 12:45:58 +0000

    [FIX] point_of_sale: cash in/out deletion rights
    Before this commit, a user with "account.group_account_invoice" group
    could try to delete a cash in/out in the pos but had no feedback.
    Actually, his request was refused but we never tell the user why.
    We now display the error but also do not give the possibility to the
    user to delete a cash in/out if he does not have the right group.
    
    There was also errors appearing when printing the CashMoveReceipt.
    We call the ReceiptHeader in the CashMoveReceipt but without giving it
    a real order which could cause problems cause in the ReceiptHeader we
    consider the order that is given as a real one and we can call methods
    and stuff from the model. We now create a dummy order to give to the
    ReceiptHeader.
    
    closes odoo/odoo#228954
    
    X-original-commit: fa169e1e2a86f57336bebedd18fc03a0d23dfae3
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/point_of_sale/static/src/app/components/popups/cash_move_popup/cash_move_list_popup/cash_move_list_popup.js
+++ addons/point_of_sale/static/src/app/components/popups/cash_move_popup/cash_move_list_popup/cash_move_list_popup.js
@@ -4,6 +4,7 @@ import { useTrackedAsync } from "@point_of_sale/app/hooks/hooks";
 import { usePos } from "@point_of_sale/app/hooks/pos_hook";
 import { Dialog } from "@web/core/dialog/dialog";
 import { _t } from "@web/core/l10n/translation";
+import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 
 export class CashMoveListPopup extends Component {
     static template = "point_of_sale.CashMoveListPopup";
@@ -17,6 +18,7 @@ export class CashMoveListPopup extends Component {
         super.setup();
         this.pos = usePos();
         this.ui = useService("ui");
+        this.dialog = useService("dialog");
         this.callbacks = this.props.cashMoves.reduce(
             (acc, cm) => ({
                 ...acc,
@@ -35,13 +37,25 @@ export class CashMoveListPopup extends Component {
     }
 
     async onDeleteCm(cm) {
-        await this.pos.data.call(
-            "pos.session",
-            "delete_cash_in_out",
-            [[this.pos.session.id], cm.id, this.props.partnerId],
-            {},
-            true
-        );
-        this.props.cashMoves = this.props.cashMoves.filter((cashMove) => cashMove.id !== cm.id);
+        try {
+            await this.pos.data.call(
+                "pos.session",
+                "delete_cash_in_out",
+                [[this.pos.session.id], cm.id, this.props.partnerId],
+                {},
+                true
+            );
+            this.props.cashMoves = this.props.cashMoves.filter((cashMove) => cashMove.id !== cm.id);
+        } catch (error) {
+            this.dialog.add(AlertDialog, {
+                title: _t("Odoo Server Error"),
+                body: error.data.message,
+            });
+            throw error;
+        }
+    }
+
+    get hasCashDeletePerm() {
+        return this.pos.config._has_cash_delete_perm;
     }
 }
