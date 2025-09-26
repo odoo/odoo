b3878d0ce9204f20ff5e8822c4d140207cdb1342
Alex Kühn <aku@odoo.com>
2025-09-26 13:06:28 +0200

    [FIX] mail: show ellipsis icon in message email read more/less
    Before this commit, when a message is of type "email" and contains
    parts are in foldable with more/less (like `<quote>`), the icon of
    the button for fold/unfold is missing.
    
    This happens because the icon is `.oi` and requires `odoo_ui_icons`.
    Message of type "email" have their content inside a shadow DOM,
    because we want to preserve the style of email inside the web client,
    at least in white theme. Shadow-DOM prevents the parent document to
    pass `odoo_ui_icons` thus the Shadow-DOM could not apply expected
    style on `.oi` icons.
    
    This commit fixes the issue by passing the required CSS as stylesheet
    to the shadow DOM, so that `.oi` icons are working inside the shadow
    DOM of message content of type "email".
    
    closes odoo/odoo#228741
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/__manifest__.py
+++ addons/mail/__manifest__.py
@@ -212,6 +212,9 @@ For more specific needs, you may also assign custom-defined actions
         'mail.assets_lamejs': [
             'mail/static/lib/lame/lame.js',
         ],
+        "mail.assets_message_email": [
+            "web/static/lib/odoo_ui_icons/style.css",
+        ],
         'mail.assets_public': [
             'web/static/lib/jquery/jquery.js',
             'web/static/lib/odoo_ui_icons/style.css',
