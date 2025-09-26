26c5ef4b5bdae7d0ff6e474a0225142e35aa7270
jeanschoenlaub <jeanschoen@hotmail.com>
2025-09-26 10:08:31 +0200

    [IMP] purchase_stock: Add filter on suggest toggle
    Adds filters (suggested qty > 0 OR product in PO) on activating
    suggestion feature (toggle), making sure ALL suggested products
    are shown first. Removes record highlighing for suggested products.
    
    BEFORE: Toggling suggestions would just reorder the Kanban
    cards to show suggested products first, but it would not
    work if the number of products was greater than pagination
    (can't order by non stored fields or bypass web_search_read limit).
    
    NOW: On suggestion toggle, we now filter on products with suggested
    qtys or in the order. Only products in the order are highlighted.
    
    TECHNICAL:
    - Reload only with searchModel._notify() preventing double
    web_search_read and select_range RPCs (previously reloading
    through this.env.model.root.load() AND ._notify())
    --> We don't control reload anymore -> Change the reordering
    logic to be abstracted in the kanban model loading (ie.
    order as soon as we get the data).
    --> Using _notify() also allows keeping the categories shown
    in search panel correct
    --> Using the search model more is further improved in a
    later commit
    
    task#5114649
    
    Part-of: odoo/odoo#228807
    Signed-off-by: Steve Van Essche <svs@odoo.com>

--- addons/purchase_stock/static/src/product_catalog/kanban_model.js
+++ addons/purchase_stock/static/src/product_catalog/kanban_model.js
@@ -1,11 +1,70 @@
 import { ProductCatalogKanbanModel } from "@product/product_catalog/kanban_model";
+import { getSuggestToggleState, toggleFilters, editSuggestContext } from "./utils";
 
 export class PurchaseSuggestCatalogKanbanModel extends ProductCatalogKanbanModel {
-    /* Pass suggest context to /product/catalog/order_lines_info RPC
+    setup() {
+        super.setup(...arguments);
+        this.firstLoad = true;
+    }
+
+    /**
+     * @override  to reorder records with suggested_qty > 0 to the top, keeping original order.
+     */
+    async _loadData(params) {
+        const sortBySuggested = (list) => {
+            const suggestProducts = list.filter((record) => record.suggested_qty > 0);
+            const notSuggestedProducts = list.filter((record) => record.suggested_qty == 0);
+            return [...suggestProducts, ...notSuggestedProducts];
+        };
+        const suggest = getSuggestToggleState(this.config.context.po_state);
+        this._suggestOnFirstLoad(suggest, params);
+        const result = await super._loadData(params);
+        if (!suggest.isOn || !result.records.some((r) => r.suggested_qty > 0)) {
+            return result;
+        }
+        if (!params.isMonoRecord) {
+            if (params.groupBy?.length) {
+                for (const group of result.groups) {
+                    group.list.records = sortBySuggested(group.list.records);
+                }
+            } else {
+                result.records = sortBySuggested(result.records);
+            }
+        }
+        return result;
+    }
+
+    /** Pass suggest context to /product/catalog/order_lines_info RPC
      * to add computed ["suggested_qty"] key to productCatalogData */
     _getOrderLinesInfoParams(loadParams, productIds) {
         const base = super._getOrderLinesInfoParams(loadParams, productIds);
-        const suggestCtx = this.config.context || {}; // Controller sets context in config
-        return { ...base, ...suggestCtx };
+        return { ...base, ...loadParams.context };
+    }
+
+    /**
+     *  Adds suggest_context and toggles filters if first load with suggest on.
+     *  IMPROVE: action_add_from_catalog should pass context and activate filters
+     *  using default_search filters (not easy bc toggle state in local storage)
+     */
+    _suggestOnFirstLoad(suggestToggle, params) {
+        if (!this.firstLoad) {
+            return;
+        }
+        const ctx = this.config.context;
+        const sm = this.env.searchModel;
+        if (suggestToggle.isOn && this.firstLoad) {
+            const suggestCtx = {
+                suggest_domain: this.env.searchModel.domain,
+                suggest_days: ctx.vendor_suggest_days,
+                suggest_based_on: ctx.vendor_suggest_based_on,
+                suggest_percent: ctx.vendor_suggest_percent,
+                warehouse_id: ctx.warehouse_id,
+            };
+            this.config.context = editSuggestContext(this.config.context, true, suggestCtx);
+            sm.globalContext = editSuggestContext(this.config.context, true, suggestCtx);
+            toggleFilters(sm, ["suggested", "products_in_purchase_order"], true);
+            params.domain = sm.domain; // TODO safer
+            this.firstLoad = false;
+        }
     }
 }
--- addons/purchase_stock/static/src/product_catalog/record/kanban_record.js
+++ addons/purchase_stock/static/src/product_catalog/record/kanban_record.js
@@ -2,17 +2,13 @@ import { ProductCatalogKanbanRecord } from "@product/product_catalog/kanban_reco
 import { ProductCatalogPurchaseSuggestOrderLine } from "./purchase_order_line";
 
 export class ProductCatalogPurchaseSuggestKanbanRecord extends ProductCatalogKanbanRecord {
-    /* Highlights product card if suggest_qty > 0,
-     * hiding suggest line if suggest_qty == qty in PO */
+    /* Hides suggest line if suggest_qty == qty in PO */
     getRecordClasses(...args) {
         const classes = super.getRecordClasses(args) || "";
-
         const catalogData = this.productCatalogData || {};
-        if (catalogData.suggested_qty) {
-            if (catalogData.suggested_qty == catalogData.quantity) {
-                return classes + " o_suggest_highlight" + " o_hide_suggest_qty";
-            }
-            return classes + " o_suggest_highlight";
+
+        if (catalogData.suggested_qty && catalogData.suggested_qty == catalogData.quantity) {
+            return classes + " o_hide_suggest_qty";
         }
         return classes;
     }
