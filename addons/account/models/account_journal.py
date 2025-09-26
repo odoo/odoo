odoo/odoo

commit 65e7a6727597bf7896c864dbd13c873ce4ae6e30

[IMP] account: remove `__get_bank_statements_available_sources`
The function `__get_bank_statements_available_sources` is pointless
and can just be replaced with `_get_bank_statements_available_sources`
(single underscore version).

See related community PR.

task-None

closes odoo/odoo#226347

Related: odoo/enterprise#94334
Related: odoo/upgrade#8428
Signed-off-by: Maximilien La Barre (malb) <malb@odoo.com>

addons/account/models/account_journal.py
@@ -55,11 +55,8 @@ class AccountJournal(models.Model):
     def _default_outbound_payment_methods(self):
         return self.env.ref('account.account_payment_method_manual_out')
 
-    def __get_bank_statements_available_sources(self):
-        return [('undefined', _('Undefined Yet'))]
-
     def _get_bank_statements_available_sources(self):
-        return self.__get_bank_statements_available_sources()
+        return [('undefined', _('Undefined Yet'))]
 
     def _default_invoice_reference_model(self):
         """Get the invoice reference model according to the company's country."""

commit 33e36f7ad04001461b7724fda030a2dae55b01f7

[FIX] account: fix `bank_statements_source` selection
Currently the selection options from other modules are missing in field
`bank_statements_source`.
It broke in commit 65e7a6727597bf7896c864dbd13c873ce4ae6e30.

The issue is that a function is passed directly as `selection`
argument to the field (`selection=_get_bank_statements_available_sources`).
But that way the inheritance is ignored / the extending functions
of other modules are not called / ignored.

After this commit the function name is passed to `selection`.
That way the inheritance / extending functions work again.

task-None

addons/account/models/account_journal.py
@@ -240,7 +240,7 @@ class AccountJournal(models.Model):
         index='btree_not_null',
         check_company=True,
         domain="[('partner_id','=', company_partner_id)]")
-    bank_statements_source = fields.Selection(selection=_get_bank_statements_available_sources, string='Bank Feeds', default='undefined', help="Defines how the bank statements will be registered")
+    bank_statements_source = fields.Selection(selection='_get_bank_statements_available_sources', string='Bank Feeds', default='undefined', help="Defines how the bank statements will be registered")
     bank_acc_number = fields.Char(related='bank_account_id.acc_number', readonly=False)
     bank_id = fields.Many2one('res.bank', related='bank_account_id.bank_id', readonly=False)
 