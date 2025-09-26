1ab21d820eb2962329f8e6cec385b3d864b102f0
Harsh Modi <hamo@odoo.com>
2025-09-26 18:29:10 +0530

    [FIX] account: map taxes when the fiscal xml_id is new
    closes odoo/odoo#213931
    
    Related: odoo/enterprise#87486
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>

--- addons/account/models/chart_template.py
+++ addons/account/models/chart_template.py
@@ -342,8 +342,9 @@ class AccountChartTemplate(models.AbstractModel):
             for xmlid, values in records.items():
                 if model_name == 'account.fiscal.position':
                     # if xmlid is not in xmlid2fiscal_position and we do not force create so we will skip_update for that record
-                    if xmlid not in xmlid2fiscal_position and not force_create:
-                        skip_update.add((model_name, xmlid))
+                    if xmlid not in xmlid2fiscal_position:
+                        if not force_create:
+                            skip_update.add((model_name, xmlid))
                         continue
                     # Only add accounts and taxes mappings containing new records
                     for model in ['account', 'tax']:
