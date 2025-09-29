64a57978c9bda7cd705116e11d7788715e404b9c
Louis Travaux <lotr@odoo.com>
2025-09-29 09:41:48 +0000

    [FIX] hw_drivers: browser url saved in conf
    The browser on the IoT Box was always reopening on the url saved
    in the database instead of the one saved in odoo.conf.
    
    When opening the browser, we used to set the orientation and save it
    in the configuration. As we only set the orientation and not the url
    at this point, we were mistakenly removing the url from odoo.conf.
    
    As it was not set anymore, when reopening the browser later (e.g. after reboot),
    no url was available in conf, so we fell back on the db's one.
    
    Task: 5103536
    X-original-commit: d3e04805d7cc946d24b3ee64a385951366e13c99

--- addons/hw_drivers/iot_handlers/drivers/DisplayDriver_L.py
+++ addons/hw_drivers/iot_handlers/drivers/DisplayDriver_L.py
@@ -33,15 +33,12 @@ class DisplayDriver(Driver):
         self.device_name = device['name']
         self.owner = False
         self.customer_display_data = {}
-        self.url, self.orientation = helpers.load_browser_state()
         if self.device_identifier != 'distant_display':
+            saved_url, self.orientation = helpers.load_browser_state()
+            self.url = saved_url or self.get_url_from_db() or 'http://localhost:8069/status/'
             self._x_screen = device.get('x_screen', '0')
-            self.browser = Browser(
-                self.url or 'http://localhost:8069/status/',
-                self._x_screen,
-                os.environ.copy(),
-            )
-            self.update_url(self.get_url_from_db())
+            self.browser = Browser(self.url, self._x_screen, os.environ.copy())
+            self.set_orientation(self.orientation)
 
         self._actions.update({
             'update_url': self._action_update_url,
@@ -49,8 +46,6 @@ class DisplayDriver(Driver):
             'open_kiosk': self._action_open_kiosk,
             'rotate_screen': self._action_rotate_screen,
         })
-
-        self.set_orientation(self.orientation)
 
     @classmethod
     def supported(cls, device):
@@ -97,6 +92,7 @@ class DisplayDriver(Driver):
 
     def _action_update_url(self, data):
         if self.device_identifier != 'distant_display':
+            helpers.save_browser_state(url=data.get('url'))
             self.update_url(data.get('url'))
 
     def _action_display_refresh(self, data):
