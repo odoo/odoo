730492c06d164b789e6979b704c4eeee992c2001
Claire (clbr) <clbr@odoo.com>
2025-09-29 11:01:20 +0000

    [FIX] l10n_fr_facturx_chorus_pro: fixes for Peppol Chorus Pro invoicing
    1. We didn't met one of the specific requirement of the BIS3 invoices
    to be sent through Chorus Pro.
    
    > The SIRET number for the final recipient, behind Chorus Pro should always be sent in the following tag:
    Invoice/cac:AccountingCustomerParty/PartyLegalEntity/CompanyID
    
    2. One of the attributes of the CompanyID was set to schemeName which is not a valid
    attribute of the Peppol BIS3 specification. -> fixed in 18.0+
    https://github.com/odoo/odoo/commit/c3ae4b29c51c7b0cff11aac5f4bf4aab5fad16c9
    
    3. Rename "Service exécutant" to "Code de Service" to better match the naming
    from Chorus Pro.
    
    Source: https://www.pagero.com/onboarding/aife/aife-en
    
    task-none (possible opw-4972189, but might not be the only problem)
    
    closes odoo/odoo#234618
    
    X-original-commit: 4155257dbfb73f28ccc0cae5b2302fc8acde3826
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

--- addons/l10n_fr_facturx_chorus_pro/models/account_edi_xml_ubl_bis3.py
+++ addons/l10n_fr_facturx_chorus_pro/models/account_edi_xml_ubl_bis3.py
@@ -1,9 +1,6 @@
-from odoo import models, _
-from odoo.addons.account_edi_ubl_cii.models.account_edi_common import EAS_MAPPING
-
+from odoo import api, models, _
 
 CHORUS_PRO_PEPPOL_ID = "0009:11000201100044"
-FR_SCHEME_IDS = {v: k for k, v in EAS_MAPPING['FR'].items()}
 
 
 class AccountEdiXmlUbl_Bis3(models.AbstractModel):
@@ -14,18 +11,28 @@ class AccountEdiXmlUbl_Bis3(models.AbstractModel):
     Chorus Pro documentation: https://communaute.chorus-pro.gouv.fr/wp-content/uploads/2017/07/Specifications_Externes_Annexe_EDI_V4.22.pdf
     """
 
+    @api.model
+    def _is_customer_behind_chorus_pro(self, customer):
+        return customer.peppol_eas and customer.peppol_endpoint and f"{customer.peppol_eas}:{customer.peppol_endpoint}" == CHORUS_PRO_PEPPOL_ID
+
     def _export_invoice_constraints(self, invoice, vals):
         constraints = super()._export_invoice_constraints(invoice, vals)
         customer, supplier = vals['customer'].commercial_partner_id, vals['supplier']
-        if customer.peppol_eas and customer.peppol_endpoint and customer.peppol_eas + ":" + customer.peppol_endpoint == CHORUS_PRO_PEPPOL_ID:
-            if customer.company_registry:
-                constraints['chorus_customer'] = _("The company_registry is mandatory for the customer when invoicing to Chorus Pro.")
-            if supplier.company_registry:
-                constraints['chorus_supplier'] = _("The company_registry is mandatory for french suppliers when invoicing to Chorus Pro.")
+        if self._is_customer_behind_chorus_pro(customer):
+            if not customer.company_registry:
+                constraints['chorus_customer'] = _("The Company Registry (Siret) of the final recipient is mandatory for the customer when invoicing through Chorus Pro.")
+            if supplier.country_code == 'FR' and not supplier.company_registry:
+                constraints['chorus_supplier_fr'] = _("The Company Registry (Siret) is mandatory for french suppliers when invoicing through Chorus Pro.")
+            if supplier.country_code != 'FR' and not supplier.vat:
+                constraints['chorus_supplier_not_fr'] = _("The VAT is mandatory for non-french suppliers when invoicing through Chorus Pro.")
         return constraints
 
     def _add_invoice_header_nodes(self, document_node, vals):
         super()._add_invoice_header_nodes(document_node, vals)
+
+        customer = vals['customer'].commercial_partner_id
+        if not self._is_customer_behind_chorus_pro(customer):
+            return
 
         invoice = vals['invoice']
 
@@ -45,27 +52,27 @@ class AccountEdiXmlUbl_Bis3(models.AbstractModel):
         # * Chorus Pro doc states that french suppliers should mention their siret, and european non-french suppliers
         # should put their VAT
         party_node = super()._get_party_node(vals)
-
         customer = vals['customer'].commercial_partner_id
-        partner = vals['partner']
-        commercial_partner = partner.commercial_partner_id
+        if not self._is_customer_behind_chorus_pro(customer):
+            return party_node
 
-        if (
-            customer.peppol_eas and
-            customer.peppol_endpoint and
-            customer.peppol_eas + ":" + customer.peppol_endpoint == CHORUS_PRO_PEPPOL_ID
-        ):
-            party_node['cac:PartyIdentification'] = {
-                'cbc:ID': {
-                    '_text': (
-                        commercial_partner.company_registry
-                        if partner.company_registry and partner.country_code == 'FR'
-                        else commercial_partner.vat
-                    ),
-                    'schemeID': (
-                        FR_SCHEME_IDS['company_registry'] if partner.company_registry and partner.country_code == 'FR' else FR_SCHEME_IDS['vat']
-                    ),
-                }
+        partner = vals['partner'].commercial_partner_id
+        party_node['cac:PartyIdentification'] = {
+            'cbc:ID': {
+                '_text': (
+                    partner.company_registry
+                    if partner.company_registry and partner.country_code == 'FR'
+                    else partner.vat
+                ),
+                'schemeID': (
+                    '0009' if partner.company_registry and partner.country_code == 'FR' else None
+                ),
+            },
+        }
+        if partner.company_registry and partner.country_code == 'FR':
+            party_node['cac:PartyLegalEntity']['cbc:CompanyID'] = {
+                '_text': partner.company_registry,
+                'schemeID': '0009',
             }
 
         return party_node
