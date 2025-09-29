0788ef8843d16aeb4e730519f468a24f011f6ea3
Aurelien van Delft (avd) <avd@odoo.com>
2025-09-29 15:44:09 +0000

    [PERF] product: speedup name_search with positive operators
    When doing a name_search with positive operators (=, ilike, in)
    the resulting query combines domains with the OR operator. This works
    fine when the leaves are all on the same table (product_product or
    product_template) as postgresql uses a Bitmap OR when everything
    is properly indexed.
    
    When leaves are on multiple tables however postgresql has to plan a
    Seq Scan. For instance, let's take a simple domain on product.product
    of the form `['|', ('name', 'ilike', 'test'), ('default_code', 'ilike',
    'test')]`. Because `name` is an inherited field via `product_tmpl_id`,
    the resulting query has the where clause `join_table.name ilike %s OR
    product_product.default_code ilike %s` with `join_table` the table you
    get after joining product_product and product_template. Since it's an
    `OR` condition, postgresql does not know in advance whether a given row
    will pass this condition. There's no way to filter the tables before the
    join. The condition moves therefore to a `Join Filter` node and
    postgresql has to scan the whole join table to fetch the correct tuples.
    
    Same thing when there's a subquery. In case of a where clause `cond OR
    cond OR subquery`, postgresql does not know in advance whether or not a
    given row is gonna pass the subquery condition. So it has to scan the
    whole table.
    
    In both cases this becomes a bottlneck when the number of products
    increaases. This commit introduces the use of `UNION ALL` instead of
    `OR`. There's one SubPlan for each individual table in the domain. The
    results are then appended to get the final products matching the
    conditions. Thanks to each table having its own SubPlan postgresql can
    now properly hit indexes for each table, greatly improving the
    performances.
    
    speedup
    
    In a database with 2.5M product_product, the name_search on product with
    a partner_id in the context and the ilike operator goes from 8s -> 5ms.
    
    In another database with 500k product_template, the name_search on
    template with a partner_id in the context and the ilike operator goes
    from 1.8s -> 5ms.
    
    opw-4921944
    opw-5103287
    opw-5049054
    
    closes odoo/odoo#239361
    
    X-original-commit: 1d4aec3db0998644e1039fc025c17066b40a81e1
    Signed-off-by: Aurélien van Delft (avd) <avd@odoo.com>
    Co-authored-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/product/models/product_product.py
+++ addons/product/models/product_product.py
@@ -9,6 +9,7 @@ from odoo.osv import expression
 from odoo.tools import float_compare, groupby
 from odoo.tools.image import is_image_size_above
 from odoo.tools.misc import unique
+from odoo.tools.sql import SQL
 
 
 class ProductProduct(models.Model):
@@ -606,18 +607,18 @@ class ProductProduct(models.Model):
     @api.model
     def _search_display_name(self, operator, value):
         is_positive = operator not in expression.NEGATIVE_TERM_OPERATORS
-        combine = expression.OR if is_positive else expression.AND
-        domains = [
-            [('name', operator, value)],
-            [('default_code', operator, value)],
-        ]
+        template_domains = [[('name', operator, value)]]
+        product_domains = [[('default_code', operator, value)]]
+
         if operator == 'in':
-            domains.append([('barcode', 'in', value)])
+            product_domains.append([('barcode', 'in', value)])
             for v in value:
                 if isinstance(v, str) and (m := re.search(r'(\[(.*?)\])', v)):
-                    domains.append([('default_code', '=', m.group(2))])
+                    product_domains.append([('default_code', '=', m.group(2))])
         elif operator.endswith('like') and is_positive:
-            domains.append([('barcode', 'in', [value])])
+            product_domains.append([('barcode', 'in', [value])])
+
+        supplier_domain = []
         if partner_id := self.env.context.get('partner_id'):
             supplier_domain = [
                 ('partner_id', '=', partner_id),
@@ -625,8 +626,40 @@ class ProductProduct(models.Model):
                 ('product_code', operator, value),
                 ('product_name', operator, value),
             ]
-            domains.append([('product_tmpl_id.seller_ids', 'any', supplier_domain)])
-        return combine(domains)
+
+        # AND clauses properly hit indexes so no need for custom sql in this case.
+        if operator in expression.NEGATIVE_TERM_OPERATORS:
+            domains = template_domains + product_domains
+            if supplier_domain:
+                domains.append([('product_tmpl_id.seller_ids', 'any', supplier_domain)])
+            return expression.AND(domains)
+
+        # Disable active_test to simplify subqueries
+        self_no_active_test = self.with_context(active_test=False)
+        queries = [
+            self_no_active_test._search([
+                ('product_tmpl_id', 'in', self_no_active_test.env['product.template']._search(expression.OR(template_domains)))
+            ]),
+            self_no_active_test._search(expression.OR(product_domains)),
+        ]
+        if supplier_domain:
+            queries.append(
+                self_no_active_test._search([
+                    (
+                        'product_tmpl_id',
+                        'in',
+                        self_no_active_test.env['product.supplierinfo']._search(supplier_domain).subselect('product_tmpl_id'),
+                    )
+                ])
+            )
+        query = SQL(
+            """(%s)""",
+            SQL("UNION ALL").join(
+                [SQL("(%s)", query.select()) for query in queries]
+            )
+        )
+
+        return [('id', 'in', query)]
 
     @api.model
     def name_search(self, name='', domain=None, operator='ilike', limit=100):
