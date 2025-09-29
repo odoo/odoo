cb21a08ce535643f301bac74096693b4dd575794
Claire (clbr) <clbr@odoo.com>
2025-09-29 11:01:20 +0000

    [FIX] l10n_fr_facturx_chorus_pro: fixes for Peppol Chorus Pro invoicing
    1. We didn't met one of the specific requirement of the BIS3 invoices
    to be sent through Chorus Pro.
    
    > The SIRET number for the final recipient, behind Chorus Pro should always be sent in the following tag:
    Invoice/cac:AccountingCustomerParty/PartyLegalEntity/CompanyID
    
    2. One of the attributes of the CompanyID was set to schemeName which is not a valid
    attribute of the Peppol BIS3 specification. -> fixed in 18.0+
    https://github.com/odoo/odoo/commit/c3ae4b29c51c7b0cff11aac5f4bf4aab5fad16c9
    
    3. Rename "Service exécutant" to "Code de Service" to better match the naming
    from Chorus Pro.
    
    Source: https://www.pagero.com/onboarding/aife/aife-en
    
    task-none (possible opw-4972189, but might not be the only problem)
    
    closes odoo/odoo#229584
    
    X-original-commit: b7da60e1c13cfe6ce9c42bcd4431efc531e703e8
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

--- addons/account/models/partner.py
+++ addons/account/models/partner.py
@@ -355,7 +355,10 @@ class ResPartner(models.Model):
     def _compute_fiscal_country_codes(self):
         for record in self:
             allowed_companies = record.company_id or self.env.companies
-            record.fiscal_country_codes = ",".join(allowed_companies.mapped('account_fiscal_country_id.code'))
+            country_codes = allowed_companies.mapped('account_fiscal_country_id.code')
+            if record.country_code:
+                country_codes.append(record.country_code)
+            record.fiscal_country_codes = ",".join(country_codes)
 
     @property
     def _order(self):
