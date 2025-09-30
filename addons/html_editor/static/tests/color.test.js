48fff2020b9f3ca007580c7aa30206ea7cd7f8e4
Serhii Rubanskyi <seru@odoo.com>
2025-09-30 14:03:20 +0000

    [FIX] html_editor: change color properly when there's an icon
    After this [commit], we'd have an issue when we tried to change a color
    and there was a `.fa` icon next to our selection. Instead of changing
    the color of only the selection it would change it for the closest
    element with `color`, `background-color`, or `background-image`
    style properties.
    To reproduce the bug:
    - Set selection on an element that has a color style property on its
    parent, and the parent has .fa icon but not directly on our element
    - Try to change its color
    
    => Color of the whole parent changes
    
    task-5107147
    
    [commit]: https://github.com/odoo/odoo/commit/927f4b973932d14961c148e13473017651a60dc0
    
    closes odoo/odoo#231892
    
    X-original-commit: b51f069496d03dafae976049cbf20010185335b3
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Serhii Rubanskyi (seru) <seru@odoo.com>

--- addons/html_editor/static/tests/color.test.js
+++ addons/html_editor/static/tests/color.test.js
@@ -910,6 +910,27 @@ test("should be able to remove color of an icon", async () => {
     });
 });
 
+test("doesn't change the color of the whole section when there's an icon next to the selection", async () => {
+    await testEditor({
+        contentBefore: `
+        <section style="color: rgb(255, 0, 0);">
+            <p>a[bc]d</p>
+            <span class="fa fa-glass" contenteditable="false">\u200b</span>
+        </section>`,
+        stepFunction: setColor("rgb(0, 0, 255)", "color"),
+        contentAfterEdit: `
+        <section style="color: rgb(255, 0, 0);">
+            <p>a<font style="color: rgb(0, 0, 255);">[bc]</font>d</p>
+            <span class="fa fa-glass" contenteditable="false">\u200b</span>
+        </section>`,
+        contentAfter: `
+        <section style="color: rgb(255, 0, 0);">
+            <p>a<font style="color: rgb(0, 0, 255);">[bc]</font>d</p>
+            <span class="fa fa-glass"></span>
+        </section>`,
+    });
+});
+
 test("should remove remove color from `td`", async () => {
     await testEditor({
         contentBefore: unformat(`
