49cdc77c402287d348d2f6b9b05931a777f1a755
Robin Lejeune (role) <role@odoo.com>
2025-09-30 12:34:11 +0000

    [IMP] html_builder: document BuilderAction
    This commit adds JSDoc for `BuilderAction` and related concepts.
    
    X-original-commit: 0e86296489f607ea35b916b76fef9fa9b6dd951d

--- addons/html_builder/static/src/core/dependency_manager.js
+++ addons/html_builder/static/src/core/dependency_manager.js
@@ -1,6 +1,15 @@
 import { EventBus } from "@odoo/owl";
 import { batched } from "@web/core/utils/timing";
 
+/**
+ * @typedef { Object } BuilderOptionDependency
+ * @property { () => boolean } isActive
+ * @property { Function } [getActions]
+ * @property { Function } [getValue]
+ * @property { Function } [cleanSelectedItem]
+ * @property { string } [type]
+ */
+
 export class DependencyManager extends EventBus {
     constructor() {
         super();
@@ -22,7 +31,11 @@ export class DependencyManager extends EventBus {
         }
         this.dirty = false;
     }
-
+    /**
+     * @param {string} id
+     * @param {BuilderOptionDependency} value
+     * @param {Boolean} ignored - should not add the dependency to the map
+     */
     add(id, value, ignored = false) {
         // In case the dependency is added after a dependent try to get it
         // an event is scheduled to notify the dependent about it.
@@ -32,14 +45,19 @@ export class DependencyManager extends EventBus {
         this.dependencies.push([id, value, ignored]);
         this.dirty = true;
     }
-
+    /**
+     * @param {string} id
+     * @returns {BuilderOptionDependency}
+     */
     get(id) {
         if (this.dirty) {
             this.update();
         }
         return this.dependenciesMap[id];
     }
-
+    /**
+     * @param {BuilderOptionDependency} value
+     */
     removeByValue(value) {
         this.dependencies = this.dependencies.filter(([, v]) => v !== value);
         this.dirty = true;
