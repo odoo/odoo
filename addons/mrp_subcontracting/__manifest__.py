f2f9cd310dc2742b5110cae6cf5650d240cc5c5e
Stéphane Diez <snd@odoo.com>
2025-09-30 14:00:34 +0200

    [FIX] mrp_subcontracting: portal access rights
    Make sure that portal subcontractors can do what they need to:
    - create new serial numbers and assign them to SMLs
    	-> new ACL for sequences (READ and WRITE)
    	-> new ACL for mrp.production.serials to open the wizard
    - see SML serials in the Detailed Operations popup
    	-> add context to portal view
    - open the MO list view for registering components for multiple MOs
    	-> add the missing js file in the manifest
    - save the changes on the DB
    	-> call `_has_workorders` before doing some workorder operations
    	-> add sudo for MO unlink because a portal user doesn't have DELETE ACL
    	-> add sudo for MO assign when using lots/serials
    	-> skip writing the state field if there's no record to write
    - correctly increment sequence
    	-> added the changes from odoo/odoo#227095 to the subcontracting serial wizard
    
    task 5107873
    
    closes odoo/odoo#229930
    
    Related: odoo/enterprise#98611
    Signed-off-by: Steve Van Essche <svs@odoo.com>

--- addons/mrp_subcontracting/__manifest__.py
+++ addons/mrp_subcontracting/__manifest__.py
@@ -136,6 +136,7 @@
             'stock/static/src/widgets/*',
             'stock/static/src/fields/*',
             'mrp_subcontracting/static/src/components/subcontracting_production_form_controller.js',
+            'mrp_subcontracting/static/src/components/subcontracting_production_list_controller.js',
         ],
     },
     'uninstall_hook': 'uninstall_hook',
