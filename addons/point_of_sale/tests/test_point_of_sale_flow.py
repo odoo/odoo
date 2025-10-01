09f3037ca26307c57d476e000821c51c06833f3c
Pedram (PEBR) <pebr@odoo.com>
2025-10-01 13:56:35 +0000

    [FIX] point_of_sale: prevent gaps in session names
    Before this commit, if an issue occurred while posting the cash
    details, the session sequence would still increment even though
    the operation failed, leading to gaps in session names.
    
    With this commit, the sequence only increments when the operation
    succeeds, ensuring continuous session naming without gaps.
    
    opw-5100163
    
    X-original-commit: 43473ccd0ff780c8ceb8300c9bc25a6a6c787969

--- addons/point_of_sale/tests/test_point_of_sale_flow.py
+++ addons/point_of_sale/tests/test_point_of_sale_flow.py
@@ -3,6 +3,7 @@
 
 from datetime import timedelta
 from freezegun import freeze_time
+from unittest.mock import patch
 
 import odoo
 from odoo import fields
@@ -2805,3 +2806,23 @@ class TestPointOfSaleFlow(TestPointOfSaleCommon):
         order_ids = [oi[0] for oi in self.PosOrder.search_paid_order_ids(other_pos_config.id, [('partner_id.complete_name', 'ilike', self.partner1.complete_name)], 80, 0)['ordersInfo']]
         self.assertNotIn(paid_order_1.id, order_ids)
         self.assertIn(paid_order_2.id, order_ids)
+
+    def test_session_name_gap(self):
+        self.pos_config.open_ui()
+        session = self.pos_config.current_session_id
+        session.set_opening_control(0, None)
+        current_session_name = session.name
+        session.action_pos_session_closing_control()
+
+        self.pos_config.open_ui()
+        session = self.pos_config.current_session_id
+
+        def _post_cash_details_message_patch(*_args, **_kwargs):
+            raise UserError('Test Error')
+
+        with patch.object(self.env.registry.models['pos.session'], "_post_cash_details_message", _post_cash_details_message_patch):
+            with self.assertRaises(UserError):
+                session.set_opening_control(0, None)
+
+        session.set_opening_control(0, None)
+        self.assertEqual(int(session.name.split('/')[1]), int(current_session_name.split('/')[1]) + 1)
