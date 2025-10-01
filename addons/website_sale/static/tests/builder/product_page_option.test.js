00bcf823569fe7a1878dfaf51f5ce1ecff652319
Julien (jula) <jula@odoo.com>
2025-10-01 08:23:28 +0000

    [FIX] html_builder, *: fix non-deterministic crash on editor reload
    *: website_sale
    
    __Current behavior before commit:__
    Some builder actions (e.g `ProductPageImageLayoutAction`) reload the
    editor after being applied. If another button in the builder is pressed
    rapidly, `refreshCurrentItem` might be called after the editor is
    destroyed leading to the following error in [`isApplied`]:
    
    `TypeError: Cannot read properties of undefined (reading 'getAction')`.
    
    The "Product page options" test fails in rare occasion due to this
    issue.
    
    __Description of the fix:__
    - Add a safety guard to make sure the editor is not destroyed before
    calling `refreshCurrentItem`.
    - Add some checks at the end of the test in order for the crash to
    appear consistently (if the fix is not applied).
    - Make the test more robust (some code is backported from
    [this commit]).
    
    [this commit]: https://github.com/odoo/odoo/commit/670b1daa2254d76
    [`isApplied`]: https://github.com/odoo/odoo/blob/f258b263136f606f7896/addons/html_builder/static/src/core/utils.js#L939
    
    Runbot error: https://runbot.odoo.com/odoo/runbot.build.error/232649
    
    X-original-commit: c3bbd7509b381fce59fc9e1aad43d5147e5b9af4

--- addons/website_sale/static/tests/builder/product_page_option.test.js
+++ addons/website_sale/static/tests/builder/product_page_option.test.js
@@ -97,22 +97,21 @@ test("Product page options", async () => {
     await contains(":iframe .o_wsale_product_page").click();
     await contains("[data-action-id=productReplaceMainImage]").click();
     await contains(".o_select_media_dialog .o_existing_attachment_cell button").click();
+    await expect.waitForSteps(["theme_customize_data_get", "get_image_info"]);
     await waitForNone(".o_select_media_dialog");
 
     expect(":iframe #product_detail_main img[src^='data:image/webp;base64,']").toHaveCount(1);
     expect(":iframe img").toHaveCount(2);
-    expect.verifySteps(["theme_customize_data_get", "get_image_info"]);
-
     await contains("button#o_wsale_image_width").click();
     // Avoid selecting the first option to prevent the image layout option from disappearing
     await contains("[data-action-id=productPageImageWidth][data-action-value='50_pc']").click();
+    await expect.waitForSteps(["config"]);
     await waitSidebarUpdated();
-    expect.verifySteps(["config"]);
 
     await contains("button#o_wsale_image_layout").click();
     await contains("[data-action-id=productPageImageLayout]").click();
     await waitSidebarUpdated();
-    expect.verifySteps([
+    await expect.waitForSteps([
         // Activate the carousel view and change the shop config
         "config",
         // Shop config changes don't trigger the `savePlugin`; image edits are saved because of the
@@ -125,4 +124,10 @@ test("Product page options", async () => {
         // Reload the view
         "theme_customize_data_get"
     ]);
+
+    // Make sure that clicking quickly on a builder button after an clicking on
+    // an action that reloads the editor does not produce a crash.
+    await contains("[data-action-id=websiteConfig].o_we_buy_now_btn").click();
+    await contains("button#o_wsale_image_layout").click();
+    await expect.waitForSteps(["theme_customize_data", "theme_customize_data_get"]);
 });
