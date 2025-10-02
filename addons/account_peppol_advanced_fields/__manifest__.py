e1d75b2e3a953f44df258ad40725c63e2abd0b18
Claire (clbr) <clbr@odoo.com>
2025-10-02 09:14:45 +0200

    [FIX] account_peppol_advanced_fields: deprecate the module
    This module was merged prematurly, it's not fully working and still
    lacks important features.
    
    We'll come up with a better solution, in the meantime we try to avoid
    that people install it.
    
    task-none
    
    closes odoo/odoo#229635
    
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

--- addons/account_peppol_advanced_fields/__manifest__.py
+++ addons/account_peppol_advanced_fields/__manifest__.py
@@ -1,13 +1,6 @@
 {
-    'name': "Account Peppol Advanced Fields",
-    'summary': "Adds specific Peppol fields to invoices under an 'Additional Information' tab.",
-    'description': """
-        This module introduces several advanced Peppol-related fields to the Odoo
-        invoice model (`account.move`) and makes them available on a new 'Additional
-        Information' tab within the 'Other Info' section of the invoice form view.
-        This is designed as a separate, optional module to keep core functionality
-        clean and allow for flexible use cases.
-    """,
+    'name': "[DEPRECATED] Account Peppol Advanced Fields",
+    'summary': "Merged prematurly, not working correctly. Please don't use. Better solution coming soon.",
     'author': "Odoo S.A.",
     'category': 'Accounting/Accounting',
     'version': '1.0',
--- addons/account_peppol_advanced_fields/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import test_ubl_bis3_peppol
