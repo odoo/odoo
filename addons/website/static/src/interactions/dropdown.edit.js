5872d5e70a62a81fbb4009d721f9809955f39af4
Sébastien (blse) <blse@odoo.com>
2025-10-02 10:55:25 +0200

    [FIX] website: remove selection in dropdown when it closes
    When the user has the selection inside a dropdown that closes, the
    selection was kept. If the toolbar was shown on the selection, then used
    to stays floating where the selection used to be.
    
    This commits listen for the closing of the dropdown to remove the
    selection if it was in the dropdown.
    
    Steps to reproduce:
    - Open website builder
    - Click on the user's name to open the dropdown
    - Select some text in the dropdown (the toolbar should appear)
    - Press "esc" (the dropdown should close)
    - Bug: the toolbar is left hanging where the selection used to be
    
    task-4367641

--- addons/website/static/src/interactions/dropdown.edit.js
+++ addons/website/static/src/interactions/dropdown.edit.js
@@ -8,6 +8,19 @@ export class DropdownEdit extends Interaction {
             // We want dropdown menus not to close when clicking inside them in
             // edit mode.
             "t-att-data-bs-auto-close": () => "outside",
+            "t-on-hidden.bs.dropdown": () => {
+                const selection = this.el.ownerDocument.getSelection();
+                if (
+                    this.el.parentElement
+                        ?.querySelector(".dropdown-menu")
+                        ?.contains(selection.anchorNode)
+                ) {
+                    // If the selection is in a closed dropdown, we remove it so
+                    // that overlays appearing around the selection go away
+                    // (like toolbar, or link tools)
+                    selection.empty();
+                }
+            },
         },
     };
 }
