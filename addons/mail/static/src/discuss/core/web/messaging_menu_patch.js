a731d79a5669bd76ebf9263916cfdfddd4517769
Alex Kühn <aku@odoo.com>
2025-10-02 15:48:24 +0000

    [IMP] mail: PWA shows discuss badge counter on app icon
    This commit adds a badge counter with global discuss counter on the
    PWA app icon, which helps a lot in knowing how many chat
    notifications are pending in the PWA when the Odoo app is not open.
    
    Task-5136354
    
    closes odoo/odoo#235003
    
    X-original-commit: c04c4cb715902fe95618a15e18233cd001bdb304
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/discuss/core/web/messaging_menu_patch.js
+++ addons/mail/static/src/discuss/core/web/messaging_menu_patch.js
@@ -21,22 +21,4 @@ patch(MessagingMenu.prototype, {
             this.dropdown.close();
         }
     },
-    get counter() {
-        const count = super.counter;
-        const channelsContribution =
-            this.store.channels.status !== "fetched"
-                ? this.store.initChannelsUnreadCounter
-                : Object.values(this.store["discuss.channel"].records).filter(
-                      (channel) =>
-                          channel.displayToSelf &&
-                          !channel.self_member_id?.mute_until_dt &&
-                          (channel.self_member_id?.message_unread_counter ||
-                              channel.message_needaction_counter)
-                  ).length;
-        // Needactions are already counted in the super call, but we want to discard them for channel so that there is only +1 per channel.
-        const channelsNeedactionCounter = Object.values(
-            this.store["discuss.channel"].records
-        ).reduce((acc, channel) => acc + channel.message_needaction_counter, 0);
-        return count + channelsContribution - channelsNeedactionCounter;
-    },
 });
