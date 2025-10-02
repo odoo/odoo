28332088196984c9c3763e9e60e1ebc99706ca42
Julien (jula) <jula@odoo.com>
2025-10-02 14:55:47 +0200

    [IMP] html_builder, website: standardize image positioning with overlay
    This commit refactors the `BackgroundPositionOverlay` into a more
    generic `ImagePositionOverlay` component.
    
    The previous implementation for aligning card cover images used a range
    slider, which was not intuitive for users. This change replaces the
    slider with a button that opens an overlay, allowing users to visually
    drag and drop the image to adjust its position.
    
    The new `ImagePositionOverlay` is now used for both the card cover image
    and background image position feature, improving user experience by
    helping them find the option they need.
    
    The term `Alignment` for the card image option has been replaced by
    `Position` to standardize nomenclature.
    
    Tests have been updated to cover the new drag-and-drop functionality.
    
    task-5062074

--- addons/website/static/src/builder/plugins/options/carousel_cards_item_option.js
+++ addons/website/static/src/builder/plugins/options/carousel_cards_item_option.js
@@ -1,10 +1,10 @@
 import { BaseOptionComponent } from "@html_builder/core/utils";
-import { CardImageAlignmentOption } from "./card_image_alignment_option";
+import { CardImagePositionOption } from "./card_image_position_option";
 
 export class CarouselCardsItemOption extends BaseOptionComponent {
     static template = "website.CarouselCardsItemOption";
     static components = {
-        CardImageAlignmentOption,
+        CardImagePositionOption,
     };
     static props = {};
 }
