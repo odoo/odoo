2bb9e69b32ca95a6fe40078891df6a8f9d21c1aa
Pierre Paridans <app@odoo.com>
2025-10-03 16:21:50 +0200

    [FIX] point_of_sale,*: not all dialog's buttons should be default
    *: pos_restaurant,pos_loyalty,pos_event
    
    `o-default-button` class marks the default, kind of placeholder, button
    present in some dialogs to allow the user to easily close it. This class
    shouldn't be used on every dialog's buttons.
    
    This commit removes the `o-default-button` classes used in point of sale
    related modules where it doesn't fulfill the goal exposed above.

--- addons/point_of_sale/static/tests/pos/tours/pos_combo_tour.js
+++ addons/point_of_sale/static/tests/pos/tours/pos_combo_tour.js
@@ -24,8 +24,7 @@ registry.category("web_tour.tours").add("ProductComboPriceTaxIncludedTour", {
             Dialog.is("Attribute selection"),
             {
                 content: "dialog discard",
-                trigger:
-                    ".modal-footer .o-default-button:contains(/^Add$/) + .o-default-button:contains(/^Discard$/)",
+                trigger: ".modal-footer .btn:contains(/^Add$/) + .btn:contains(/^Discard$/)",
                 run: "click",
             },
             combo.select("Combo Product 5"),
