5662adacbcf1d46d8019d89031623fab2fa137fb
ramh <ramh@odoo.com>
2025-10-03 06:44:51 +0000

    [FIX] mail: allow using `@everyone` without mentioning specific users
    Before this commit:
    
    Since [1] if a user wrote a message with only `@everyone` and did not mention any
    individual user, the message failed to send. This happened because the
    system tried to combine the list of mentioned users with the list of
    channel members, but the mentioned users list was None and caused an
    error.
    
    After this commit:
    
    `@everyone` can be used on its own without also tagging specific users.
    When no other users are mentioned, the system simply treats it as an
    empty list and continues normally, allowing the message to be sent to
    everyone in the channel.
    
    [1]: https://github.com/odoo/odoo/pull/219637
    Task-5136984
    
    closes odoo/odoo#229901
    
    X-original-commit: 3db1a6a4eb7207a383d93bbcfe1f28c60cffc326
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/tests/discuss/test_discuss_channel.py
+++ addons/mail/tests/discuss/test_discuss_channel.py
@@ -298,6 +298,16 @@ class TestChannelInternals(MailCommon, HttpCase):
         ])
         self.assertEqual(len(mentions_notif), 1, "Shoule have Inbox notification for the mentioned internal user")
 
+    @mute_logger("odoo.models.unlink")
+    def test_channel_special_mention(self):
+        """ Posting a message on a channel should support special mention """
+        self.test_channel._add_members(users=self.user_employee | self.user_employee_nomail)
+        with self.mock_mail_gateway():
+            new_msg = self.test_channel.message_post(
+                body="Test", special_mentions=["everyone"],
+                message_type="comment", subtype_xmlid="mail.mt_comment")
+        self.assertEqual(new_msg.partner_ids, self.test_channel.channel_member_ids.partner_id)
+
     @mute_logger('odoo.models.unlink')
     def test_channel_user_synchronize(self):
         """Archiving / deleting a user should automatically unsubscribe related partner from group restricted channels"""
