48bac9823760d6c3d2f9a609deabca1ce3af8da2
amdi-odoo <amdi@odoo.com>
2025-10-03 15:48:18 +0200

    [IMP] event: adjust event time range depending on slots
    Purpose
    =======
    Automatically adjust the event time range if the user tries
    to create slots outside of the event time range.
    
    Specification
    =============
    Previously, the slot calendar had an overlay preventing the selection of days
    outside of the event time range. Slots thus couldn't be created outside of it.
    This was implemented to reduce the number of validation error and ease slots creation.
    
    => After much thoughts, this solution was found too restrictive.
    
    Now, when the user tries to create slots outside of the event time range
    a confirmation popup is displayed. On confirm, the event time range is updated to
    include the newly created slots.
    Keeping the overlay for visual purpose.
    
    Task-5114075

--- addons/event/static/src/views/event_slot_calendar/event_slot_calendar_controller.js
+++ addons/event/static/src/views/event_slot_calendar/event_slot_calendar_controller.js
@@ -1,16 +1,126 @@
 import { CalendarController } from "@web/views/calendar/calendar_controller";
+import { ConfirmationDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
+import { formatFloatTime } from "@web/views/fields/formatters";
+import { parseTime } from "@web/core/l10n/time";
 import { _t } from "@web/core/l10n/translation";
+import { useService } from "@web/core/utils/hooks";
+
+const { DateTime } = luxon;
 
 export class EventSlotCalendarController extends CalendarController {
+
+    setup() {
+        super.setup();
+        this.dialogService = useService("dialog");
+    }
+
     /**
-     * Rename mobile quick create dialog.
-     * Load model after save to show created record.
+     * Check if user tries to create slots outside of the event time range.
+     * If so, ask for confirmation to extend the event time range and include the new slots.
+     *
+     * @param {Array} slotDates
+     * @param {Time} slotStart
+     * @param {Time} slotEnd
+     * @returns {boolean} Whether or not the given slots can be created.
+     */
+    async checkAndConfirmSlotsCreation(slotDates, slotStart, slotEnd) {
+        const { start: eventStartStr, end: eventEndStr, tz: eventTz } = this.model.data.event || {};
+        if (!eventStartStr || !eventEndStr || !eventTz || !slotDates?.length || !slotStart || !slotEnd) {
+            return true;
+        }
+        // Check if slots are within the event time range.
+        // NB: Event and slot datetimes are already in the event timezone.
+        const serverDatetimeFormat = 'yyyy-MM-dd HH:mm:ss';
+        const eventStartDatetime = DateTime.fromFormat(eventStartStr, serverDatetimeFormat);
+        const eventEndDatetime = DateTime.fromFormat(eventEndStr, serverDatetimeFormat);
+        let earliestSlotStart = null;
+        let latestSlotEnd = null;
+        for (const date of slotDates) {
+            const start = date.plus(slotStart.toObject());
+            const end = date.plus(slotEnd.toObject());
+            if (start < eventStartDatetime) {
+                earliestSlotStart = earliestSlotStart ? DateTime.min(earliestSlotStart, start) : start;
+            }
+            if (eventEndDatetime < end) {
+                latestSlotEnd = latestSlotEnd ? DateTime.max(latestSlotEnd, end) : end;
+            }
+        }
+        if (!earliestSlotStart && !latestSlotEnd) {
+            return true;
+        }
+        // Some slots are out of event time range, ask for confirmation to extend the range.
+        return await new Promise((resolve) => {
+            this.dialogService.add(ConfirmationDialog, {
+                title: _t("Confirmation"),
+                body: _t(
+                    "Adding these slots will automatically adjust the event's time range to include them.\n\n" +
+                    "From:\t%(start)s - %(end)s\n" +
+                    "To:\t\t%(new_start)s - %(new_end)s\n" +
+                    "\t\t(%(tz)s)"
+                , {
+                    start: eventStartDatetime.toLocaleString(DateTime.DATETIME_MED),
+                    end: eventEndDatetime.toLocaleString(DateTime.DATETIME_MED),
+                    new_start: (earliestSlotStart || eventStartDatetime).toLocaleString(DateTime.DATETIME_MED),
+                    new_end: (latestSlotEnd || eventEndDatetime).toLocaleString(DateTime.DATETIME_MED),
+                    tz: eventTz,
+                }),
+                confirmLabel: _t("Update Event"),
+                confirm: async () => {
+                    const formatForSave = (dt) => dt.setZone(eventTz, { keepLocalTime: true }).toUTC().toFormat(serverDatetimeFormat);
+                    // Update event time range.
+                    await this.orm.write("event.event", [this.props.context.default_event_id], {
+                        ...(earliestSlotStart && { date_begin: formatForSave(earliestSlotStart) }),
+                        ...(latestSlotEnd && { date_end: formatForSave(latestSlotEnd) }),
+                    });
+                    // Update the calendar event date range overlay.
+                    this.model.data.event.start = (earliestSlotStart || eventStartDatetime).toFormat(serverDatetimeFormat);
+                    this.model.data.event.end = (latestSlotEnd || eventEndDatetime).toFormat(serverDatetimeFormat);
+                    // Allow slots creation.
+                    resolve(true);
+                },
+                cancel: () => resolve(false),
+            });
+        });
+    }
+
+    /**
+     * @override
+     * On mobile:
+     * - Rename quick create dialog.
+     * - Ask for confirmation when user tries to create slots outside of the event time range.
      */
     getQuickCreateFormViewProps(record) {
         return {
             ...super.getQuickCreateFormViewProps(record),
-            onRecordSaved: () => this.model.load(),
+            onRecordSave: async (record) => {
+                const slotStartTime = parseTime(formatFloatTime(record.data.start_hour), false);
+                const slotEndTime = parseTime(formatFloatTime(record.data.end_hour), false);
+                return this.checkAndConfirmSlotsCreation([record.data.date], slotStartTime, slotEndTime)
+                    .then(async (canCreate) => {
+                        if (canCreate) {
+                            const saved = await record.save({ reload: false });
+                            await this.model.load();
+                            return saved;
+                        }
+                        return false;
+                    });
+            },
             title: _t("New Slot"),
         };
+    }
+
+    /**
+     * @override
+     * On desktop multi create mode:
+     * - Ask for confirmation when user tries to create slots outside of the event time range.
+     */
+    onMultiCreate(multiCreateData, selectedCells) {
+        const dates = this.getDates(selectedCells);
+        const timeRange = multiCreateData.timeRange;
+        this.checkAndConfirmSlotsCreation(dates, timeRange.start, timeRange.end).then((canCreate) => {
+            if (canCreate) {
+                this.model.multiCreateRecords(multiCreateData, dates);
+            }
+        });
     }
 }
