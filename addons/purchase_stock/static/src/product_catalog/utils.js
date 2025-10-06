b1906f5e3bae3914cb31b4c590c1af850c966740
jeanschoenlaub <jesc@odoo.com>
2025-10-06 17:39:13 +0200

    [IMP] purchase_stock: use search model for suggestions
    Moves the suggestion context building logic from the catalog kanban
    controller to the catalog searchModel (which is the first point of entry),
    removing an extra rpc and logic on kanban model for first load.
    
    BEFORE: We built the suggest context in the kanban controller and
    intercepted the first load on the kanbanModel, to build context there.
    
    NOW: We build the context in the searchModel, trigger the compute
    of the total price from the search model ...
    
    TECHNICAL:
    - Before we just build the suggest context before triggering _notify()
    from the kanban controller, but simply overriding _notify doesn't work
    because it's sometimes called when searchPanel doesn't exist.
    - Removed the state from the suggest values to prevent double load
    now that they are on the searchModel
    
    closes odoo/odoo#228807
    
    Signed-off-by: Steve Van Essche <svs@odoo.com>

--- addons/purchase_stock/static/src/product_catalog/utils.js
+++ addons/purchase_stock/static/src/product_catalog/utils.js
@@ -1,30 +1,3 @@
-/**
- * Adds / Removes the suggest parameters if suggest feature is ON / OFF
- * @param {Object} baseContext Object to modify in place
- * @param {boolean} suggestOn
- * @param {Object} suggestContext must contain at least based_on key / val pair
- * @returns {Object} base context if suggest is OFF or base + suggest context
- */
-export function editSuggestContext(baseContext, suggestOn, suggestContext) {
-    if (!suggestOn) {
-        return removeSuggestContext(baseContext, suggestContext);
-    }
-    return {
-        ...baseContext,
-        ...suggestContext,
-    };
-}
-
-function removeSuggestContext(baseContext, suggestContext) {
-    const suggestKeys = new Set([
-        ...Object.keys(suggestContext || {}).filter((k) => k != "warehouse_id"),
-    ]);
-    for (const k of suggestKeys) {
-        delete baseContext[k];
-    }
-    return baseContext;
-}
-
 /** False if PO is not draft, otherwise loads last toggle state from local storage (defaults to false)  */
 export function getSuggestToggleState(poState) {
     if (poState == "draft") {
@@ -32,30 +5,4 @@ export function getSuggestToggleState(poState) {
         return toggle ?? { isOn: false };
     }
     return { isOn: false };
-}
-
-/** Toggles searchModel filters based on filter name and desired state
- * @param {SearchModel} sm the view's searchModel
- * @param {Array[string]} filterNames eg. "suggested_or_ordered"
- * @param {boolean} turnOn eg. toggles filter "On" if turnOn = true and filter is currently "Off"
- */
-export function toggleFilters(sm, filterNames, turnOn) {
-    const searchFilters = new Map(Object.values(sm.searchItems).map((i) => [i.name, i]));
-    const activeFilters = new Set(sm.query.map((q) => q.searchItemId));
-
-    const toToggle = [];
-    for (const name of filterNames) {
-        const item = searchFilters.get(name);
-        const isOn = activeFilters.has(item.id);
-        if ((turnOn && !isOn) || (!turnOn && isOn)) {
-            toToggle.push(item.id);
-        }
-    }
-
-    // Prevent toggleSearchItem from trying to reload with partial domain
-    for (let i = 0; i < toToggle.length; i++) {
-        const isLast = i === toToggle.length - 1;
-        sm.blockNotification = !isLast;
-        sm.toggleSearchItem(toToggle[i]);
-    }
 }
