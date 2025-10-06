afed818130025963f4fa6d244be756e6510cac7a
jeanschoenlaub <jesc@odoo.com>
2025-10-06 17:39:13 +0200

    [IMP] purchase_stock: use search model for suggestions
    Moves the suggestion context building logic from the catalog kanban
    controller to the catalog searchModel (which is the first point of entry),
    removing an extra rpc and logic on kanban model for first load.
    
    BEFORE: We built the suggest context in the kanban controller and
    intercepted the first load on the kanbanModel, to build context there.
    
    NOW: We build the context in the searchModel, trigger the compute
    of the total price from the search model ...
    
    TECHNICAL:
    - Before we just build the suggest context before triggering _notify()
    from the kanban controller, but simply overriding _notify doesn't work
    because it's sometimes called when searchPanel doesn't exist.
    - Removed the state from the suggest values to prevent double load
    now that they are on the searchModel
    
    (cherry picked from commit 623f4b7bbc99f26acd697b7d06e47129418e8ec1)
    
    Part-of: odoo/odoo#234044
    Signed-off-by: Steve Van Essche <svs@odoo.com>

--- addons/purchase_stock/models/purchase_order.py
+++ addons/purchase_stock/models/purchase_order.py
@@ -150,7 +150,7 @@ class PurchaseOrder(models.Model):
             'vendor_suggest_days': self.partner_id.suggest_days,
             'vendor_suggest_based_on': self.partner_id.suggest_based_on,
             'vendor_suggest_percent': self.partner_id.suggest_percent,
-            'po_state': self.state,
+            'product_catalog_order_state': self.state,
         }
 
     def action_purchase_order_suggest(self):
--- addons/purchase_stock/static/src/product_catalog/kanban_controller.js
+++ addons/purchase_stock/static/src/product_catalog/kanban_controller.js
@@ -1,52 +1,21 @@
 import { ProductCatalogKanbanController } from "@product/product_catalog/kanban_controller";
-import { onWillStart, useState, useSubEnv } from "@odoo/owl";
+import { useSubEnv, useEnv } from "@odoo/owl";
 import { useDebounced } from "@web/core/utils/timing";
-import { getSuggestToggleState, editSuggestContext, toggleFilters } from "./utils";
-import { useBus } from "@web/core/utils/hooks";
 
-/* Controller reacts to most UI events on product catalog view (eg. next page, filters, suggestion changes),
- * pass suggestion inputs to backend through context, and reorders kanban records based on backend computations.
- * Eg. Toggle suggest OFF -> loose suggest ctx -> product.suggested_qty set to 0 -> re-render normal catalog
- * Context passed to product.product AND purchase.order (see ./kaban_model.js) because record card is based on
- * both product.product (eg. monthly demand, forecast) and purchase.order (card highlighted, Add button) */
 export class PurchaseSuggestCatalogKanbanController extends ProductCatalogKanbanController {
     setup() {
         super.setup();
-        this.state = useState({
-            numberOfDays: this.props.context.vendor_suggest_days,
-            basedOn: this.props.context.vendor_suggest_based_on,
-            percentFactor: this.props.context.vendor_suggest_percent,
-            totalEstimatedPrice: 0.0,
+        this.suggest = useEnv().suggest;
+        this._computeTotalEstimatedPrice = useEnv()._computeTotalEstimatedPrice;
+        Object.assign(this.suggest, {
             currencyId: this.props.context.product_catalog_currency_id,
             digits: this.props.context.product_catalog_digits,
-            poState: this.props.context.po_state,
-            recomputeTotalPriceFlag: true,
+            poState: this.props.context.product_catalog_order_state,
             vendorName: this.props.context.vendor_name,
             warehouse_id: this.props.context.warehouse_id,
-            suggestToggle: getSuggestToggleState(this.props.context.po_state),
-        });
-
-        onWillStart(async () => {
-            this._baseContext = { ...this.model.config.context }; // For resetting when suggest is off
-            this._kanbanReload(); // Double rpc on first visit to give context to search model
-        });
-
-        // Recompute Kanban on filter changes (incl. sidebar category filters)
-        useBus(this.env.searchModel, "update", () => {
-            if (this.state.suggestToggle.isOn && this.state.recomputeTotalPriceFlag) {
-                this._computeTotalEstimatedPrice(); // To do only recompute if necessary
-            }
-            this.state.recomputeTotalPriceFlag = true;
-        });
-
-        // Recompute Kanban on filter changes (incl. sidebar category filters)
-        useBus(this.env.searchModel, "update", () => {
-            this.env.searchModel.globalContext = this._editSuggestContext(); // Check with slow internet before removing
-            this._kanbanReload();
         });
 
         useSubEnv({
-            suggest: this.state,
             addAllProducts: () => this.onAddAll(),
             toggleSuggest: () => this.toggleSuggest(),
             reloadKanban: () => this._kanbanReload(),
@@ -58,14 +27,9 @@ export class PurchaseSuggestCatalogKanbanController extends ProductCatalogKanban
 
     // Reloads catalog with suggestions
     async _kanbanReload() {
-        if (this.state.suggestToggle.isOn) {
-            await this._computeTotalEstimatedPrice();
-            this.state.recomputeTotalPriceFlag = false;
-        }
-        this.env.searchModel.globalContext = this._editSuggestContext(this._baseContext);
         this.env.searchModel.searchPanelInfo.shouldReload = true; // Changing suggestion might change categories available
         await this.env.searchModel._notify(); // Reload through searchModel with ctx (without double reload)
-        this.env.searchModel.searchPanelInfo.shouldReload = false;
+        await this._computeTotalEstimatedPrice();
     }
 
     /** Add all suggested products to the purchase order */
@@ -75,10 +39,10 @@ export class PurchaseSuggestCatalogKanbanController extends ProductCatalogKanban
         const lineCountChange = await this.model.orm.call(
             "purchase.order",
             "action_purchase_order_suggest",
-            [this._baseContext["product_catalog_order_id"]],
-            { context: this._editSuggestContext() }
+            [this.props.context.product_catalog_order_id],
+            { context: searchModel.globalContext }
         );
-        this._toggleSuggestFilters(true);
+        searchModel.toggleFilters(["suggested", "products_in_purchase_order"], true);
         searchModel.trigger("section-line-count-change", {
             sectionId,
             lineCountChange,
@@ -86,53 +50,14 @@ export class PurchaseSuggestCatalogKanbanController extends ProductCatalogKanban
     }
 
     toggleSuggest() {
-        this.state.suggestToggle.isOn = !this.state.suggestToggle.isOn;
+        this.suggest.suggestToggle.isOn = !this.suggest.suggestToggle.isOn;
         localStorage.setItem(
             "purchase_stock.suggest_toggle_state",
-            JSON.stringify({ isOn: this.state.suggestToggle.isOn })
+            JSON.stringify({ isOn: this.suggest.suggestToggle.isOn })
         );
-        this._kanbanReload();
-    }
-
-    /** Calculate estimated price (might differ from actual PO price on purpose)
-     *  if all suggestions where added to PO (ie. not only the ones displayed) */
-    async _computeTotalEstimatedPrice() {
-        const product_prices = await this.orm.searchRead(
-            "product.product",
-            this.env.searchModel.domain,
-            ["suggest_estimated_price"],
-            { context: this._editSuggestContext() }
+        this.env.searchModel.toggleFilters(
+            ["suggested", "products_in_purchase_order"],
+            this.suggest.suggestToggle.isOn
         );
-        this.state.totalEstimatedPrice = product_prices.reduce(
-            (sum, p) => sum + Number(p.suggest_estimated_price || 0),
-            0
-        );
-    }
-
-    /**
-     * Helper to toggle filters on Suggestion feature activation / deactivation
-     * @param {boolean} toggleOn true to activate filters, false to deactivate
-     * @return {none} Triggers a reload if filters didn't change
-     */
-    _toggleSuggestFilters(toggleOn) {
-        const sm = this.env.searchModel;
-        toggleFilters(sm, ["suggested", "products_in_purchase_order"], toggleOn);
-        this._kanbanReload();
-    }
-
-    /**
-     * Adds the suggest parameters to context if suggest feature is activated
-     * @returns {Object} base context if suggest is OFF or base + suggest context
-     */
-    _editSuggestContext() {
-        const suggestCtx = {
-            suggest_domain: this.env.searchModel.domain,
-            suggest_based_on: this.state.basedOn,
-            suggest_days: this.state.numberOfDays,
-            suggest_percent: this.state.percentFactor,
-            warehouse_id: this.state.warehouse_id,
-            sectionId: this.env.searchModel.selectedSection.sectionId ?? false,
-        };
-        return editSuggestContext(this._baseContext, this.state.suggestToggle.isOn, suggestCtx);
     }
 }
