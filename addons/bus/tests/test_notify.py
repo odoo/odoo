6496eeb24b8ce7e13af5c902217c197e37e60059
tsm-odoo <tsm@odoo.com>
2025-10-06 08:21:10 +0000

    [FIX] bus: fix postcommit test
    This commit fixes the `test_postcommit` that fails in a non
    deterministic fashion. This test ensures bus notifications
    created in the post commit hook result in only one batch.
    
    However, the listener only consider the first notification of
    the batch (`conn.notifies.pop()`) and ignore the rest. When
    the expected notifications come as part of a bigger batch,
    they can be ignored thus making the test fail.
    
    This commit ensures we read every notification received.
    
    fixes runbot-233185
    
    closes odoo/odoo#230094
    
    X-original-commit: b403d5d74dd545f926a38a6aa6d18118d34e83b7
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/bus/tests/test_notify.py
+++ addons/bus/tests/test_notify.py
@@ -63,24 +63,28 @@ class NotifyTests(TransactionCase):
 
         def single_listen():
             nonlocal channels
-            with odoo.sql_db.db_connect(
-                "postgres"
-            ).cursor() as cr, selectors.DefaultSelector() as sel:
+            with (
+                odoo.sql_db.db_connect("postgres").cursor() as cr,
+                selectors.DefaultSelector() as sel,
+            ):
                 cr.execute("listen imbus")
                 cr.commit()
                 conn = cr._cnx
                 sel.register(conn, selectors.EVENT_READ)
                 selector_ready_event.set()
-                while not stop_event.is_set():
+                found = False
+                while not stop_event.is_set() and not found:
                     if sel.select(timeout=5):
                         conn.poll()
-                        if notify_channels := [
-                            c
-                            for c in json.loads(conn.notifies.pop().payload)
-                            if c[0] == self.env.cr.dbname
-                        ]:
-                            channels = notify_channels
-                            break
+                        while conn.notifies:
+                            if notify_channels := [
+                                c
+                                for c in json.loads(conn.notifies.pop().payload)
+                                if c[0] == self.env.cr.dbname
+                            ]:
+                                channels = notify_channels
+                                found = True
+                                break
 
         thread = threading.Thread(target=single_listen)
         thread.start()
