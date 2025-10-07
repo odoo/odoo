18cc8df7bb2ceccbf8c5dc7e453c810ad8c943fa
Bastien Fafchamps (bafa) <bafa@odoo.com>
2025-10-07 14:01:37 +0200

    [FIX] web: Navigation system focuses items on hover when it should not
    This commit fixes an issue where some navigation items would be focused
    after a patch if it was hovered before. Now navigation items are not set
    as active when hovered, only if the container already was navigable.
    
    This commit also remove the "focus" class from any navigable items when
    the focus moves to a target that's outside of navigatable container.
    
    closes odoo/odoo#230544
    
    Signed-off-by: Michaël Mattiello (mcm) <mcm@odoo.com>

--- addons/web/static/src/search/search_bar/search_bar.js
+++ addons/web/static/src/search/search_bar/search_bar.js
@@ -12,7 +12,7 @@ import { useDropdownState } from "@web/core/dropdown/dropdown_hooks";
 import { hasTouch } from "@web/core/browser/feature_detection";
 import { Dropdown } from "@web/core/dropdown/dropdown";
 import { DropdownItem } from "@web/core/dropdown/dropdown_item";
-import { ACTIVE_ELEMENT_CLASS, useNavigation } from "@web/core/navigation/navigation";
+import { useNavigation } from "@web/core/navigation/navigation";
 
 const parsers = registry.category("parsers");
 
@@ -470,7 +470,6 @@ export class SearchBar extends Component {
                 }
                 return [];
             },
-            isNavigationAvailable: ({ navigator, target }) => navigator.contains(target),
             hotkeys: {
                 enter: {
                     isAvailable: () => !this.inputDropdownState.isOpen,
@@ -544,6 +543,7 @@ export class SearchBar extends Component {
                 this.inputDropdownState.isOpen &&
                 (this.facetContainerRef.el?.contains(target) || navigator.contains(target)),
             onUpdated: (navigator) => (this.navigator = navigator),
+            onItemActivated: (itemEl) => (this.lastActiveItemId = parseInt(itemEl.id, 10)),
             hotkeys: {
                 escape: {
                     callback: () => {
@@ -657,7 +657,7 @@ export class SearchBar extends Component {
             this.resetState();
         }
     }
-    
+
     /**
      * @param {CompositionEvent} ev
      */
@@ -673,14 +673,9 @@ export class SearchBar extends Component {
         if (!this.state.query.length) {
             this.env.searchModel.search();
         } else {
-            const els = [
-                ...this.root.el.ownerDocument.querySelectorAll(
-                    ".o_searchview_autocomplete .o-dropdown-item"
-                ),
-            ];
-            const index = els.findIndex((el) => el.classList.contains(ACTIVE_ELEMENT_CLASS));
-            if (this.items[index]) {
-                this.selectItem(this.items[index]);
+            const item = this.items.find((item) => item.id === this.lastActiveItemId);
+            if (item) {
+                this.selectItem(item);
             }
         }
     }
