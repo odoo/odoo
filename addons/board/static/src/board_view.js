e5b9c87b7301d0ab97810a9d33edbbc08dd01c05
Julien (jdeh) <jdeh@odoo.com>
2025-10-07 11:10:24 +0200

    [IMP] board: review "My Dashboard" design
    This commit aims to improve the overall design of the "My Dashboard"
    layout:
    
    - Better alignement and spacing
    - Adding "reload" button for each card
    - Change Font Awesome icons to Odoo Icons
    - Better UX for the cards: grab pointer on hover, grabbing pointer and
    ghostbox on moving, chevron icons for the size toggle
    - Improved layout dropdown: adding a label for options, streamline
    select functionality, adding aria attribute
    
    task-5111581

--- addons/board/static/src/board_view.js
+++ addons/board/static/src/board_view.js
@@ -21,7 +21,7 @@ export class BoardArchParser {
                     archInfo.title = node.getAttribute("string");
                     break;
                 case "board":
-                    archInfo.layout = node.getAttribute("style");
+                    archInfo.layout = node.getAttribute("layout") || "1";
                     archInfo.colNumber = archInfo.layout.split("-").length;
                     break;
                 case "column":
