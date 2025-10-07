d6e576d63dfb2c42973f6e842847681af8d66bed
Mazen <mamr@odoo.com>
2025-10-07 15:43:00 +0200

    [IMP] payment: log generating payment links
    Log in the chatter a message that a payment link has been generated
    with the amount it was generated for, because it used to be difficult
    to track if a payment link was generated and for how much.
    
    task-5062371

--- addons/payment/static/src/js/payment_wizard_copy_clipboard_field.js
+++ addons/payment/static/src/js/payment_wizard_copy_clipboard_field.js
@@ -6,12 +6,44 @@ import {
     CopyClipboardButtonField,
 } from "@web/views/fields/copy_clipboard/copy_clipboard_field";
 
+import { browser } from "@web/core/browser/browser";
 import { CopyButton } from "@web/core/copy_button/copy_button";
+import { formatCurrency } from "@web/core/currency"
+import { useService } from "@web/core/utils/hooks";
+import { _t } from "@web/core/l10n/translation";
 
 class PaymentWizardCopyButton extends CopyButton {
+    setup() {
+        super.setup();
+        this.orm = useService("orm")
+    }
+
     async onClick() {
         await this.env.model.mutex.getUnlockedDef();
+        await this.logLink();
         return super.onClick();
+    }
+
+    async logLink() {
+        try {
+            const res_model = this.env.model.root.data.res_model;
+            const res_id = this.env.model.root.data.res_id;
+            const amount = this.env.model.root.data.amount;
+            const currency_id = this.env.model.root.data.currency_id;
+            await this.orm.call(
+                res_model,
+                "message_post",
+                [[res_id]],
+                {
+                    body: _t(
+                        "Payment link of %s has been generated",
+                        formatCurrency(amount, currency_id.id)
+                    ),
+                }
+            );
+        } catch (error) {
+            return browser.console.warn(error);
+        }
     }
 }
 
