0d6765a125619a88d0d1d94d39802fd5926e4e37
Giorgio Tocco (gito) <gito@odoo.com>
2025-10-07 14:39:58 +0200

    [FIX] im_livechat: prevent some thread actions for live chat visitors
    This commit removes the possibility for live chat
    visitors to start a call and invite guests.
    
    task-4849019
    
    closes odoo/odoo#230245
    
    X-original-commit: e98842577bec6379b73778b1f4cfea2ca919c394
    Signed-off-by: Giorgio Tocco (gito) <gito@odoo.com>

--- addons/im_livechat/static/src/core/common/thread_actions_patch.js
+++ addons/im_livechat/static/src/core/common/thread_actions_patch.js
@@ -1,5 +1,25 @@
-import { threadActionsRegistry } from "@mail/core/common/thread_actions";
+import { ThreadAction, threadActionsRegistry } from "@mail/core/common/thread_actions";
 import { patch } from "@web/core/utils/patch";
+
+patch(ThreadAction.prototype, {
+    _condition({ action, store, thread }) {
+        const visitorActions = [
+            "fold-chat-window",
+            "close",
+            "restart",
+            "call-settings",
+            "meeting-chat",
+        ];
+        if (
+            thread?.channel_type === "livechat" &&
+            !store.self_partner &&
+            !visitorActions.includes(action.id)
+        ) {
+            return false;
+        }
+        return super._condition(...arguments);
+    },
+});
 
 patch(threadActionsRegistry.get("invite-people"), {
     condition({ thread }) {
