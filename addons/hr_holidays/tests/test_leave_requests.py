f88955d0122990384e6bb865eb6151672011f6a5
Youssef Bashandy <yoba@odoo.com>
2025-10-07 17:35:16 +0200

    [FIX] hr_holidays : Use request_date_to and request_date_from to check dates
    Steps to reproduce:
    	- Create an employee with Flexible working schedule
    	- Set the timezone for this employee very far from yours
    	- Create an Unpaid leave with Custom Hours for this employee
    	- Set the hours of the leave from 8 to 21
    	- Notice the duration is just 8 hours
    
    Cause:
    When calculating the duration of the flexible employee leave we check
    if the date_from and the date_to has the same date and if so we get the
    difference between the hour_to - hour_from but sometime when the tz is
    different when we convert it to UTC the dates overlap in two days
    so the condition sets to false so we get the working hours of the employee.
    
    Fix:
    Using the request_date_from and request_date_to in this condition
    where it will always be accurate in terms of days
    
    opw-5118689

--- addons/hr_holidays/tests/test_leave_requests.py
+++ addons/hr_holidays/tests/test_leave_requests.py
@@ -1575,3 +1575,27 @@ class TestLeaveRequests(TestHrHolidaysCommon):
 
         self.assertEqual(sick_leave.duration_display, '3 days', "hr_holidays: duration_display should not update after adding an overlapping holiday")
         self.assertEqual(sick_leave_hr.duration_display, '2 days', "hr_holidays: duration_display should update after adding an overlapping holiday")
+
+    @freeze_time("2025-10-07")
+    def test_duration_flexible_employee_different_timezone(self):
+        calendar = self.env['resource.calendar'].create({
+            'name': 'Test calendar',
+            'hours_per_day': 8,
+            'full_time_required_hours': 56,
+            'flexible_hours': True
+        })
+
+        self.employee_emp.tz = 'Australia/Darwin'
+        self.employee_emp.resource_calendar_id = calendar
+        self.env.user.tz = 'Europe/Brussels'
+
+        leave = self.env['hr.leave'].with_user(self.env.user).create({
+            'name': 'Test',
+            'employee_id': self.employee_emp.id,
+            'holiday_status_id': self.holidays_type_1.id,
+            'request_unit_hours': True,
+            'request_hour_from': 8,
+            'request_hour_to': 21,
+        })
+
+        self.assertEqual(leave.number_of_hours, 13.0)
