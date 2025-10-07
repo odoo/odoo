f41bec1ccf222146b967d228917d19a7a45ce5a5
Richard deMeester <richard.demeester@willdooit.com>
2025-10-07 15:05:27 +0000

    [FIX] stock_account: fifo move val incorrect on neg
    Before this commit:
    
    If there are not enough FIFO in valuations, (i.e. going -ve) the extra
    value comes from the last known move.
    
    But we should not use the whole move value - just the unit value from
    it.
    
    After this commit:
    
    The move value is made a unit before multiplying.
    
    X-original-commit: 8ccc79333815ef3d495b44a3a39a62ce8a4bcd14

--- addons/stock_account/models/product.py
+++ addons/stock_account/models/product.py
@@ -287,6 +287,80 @@ class ProductProduct(models.Model):
 
         return avco_value, avco_total_value
 
+<<<<<<< 384908d2c7ec418d35214c787426804d37ec9717
+||||||| 609ce29d5232707f7c6d05a7b82909cd3b09c75a
+    def _run_fifo(self, quantity, lot=None, at_date=None, location=None):
+        """ Returns the value for the next outgoing product base on the qty give as argument."""
+        self.ensure_one()
+        external_location = location and location.is_valued_external
+
+        fifo_cost = 0
+        fifo_stack, qty_on_first_move = self._run_fifo_get_stack(lot=lot, at_date=at_date, location=location)
+        last_move = False
+        # Going up to get the quantity in the argument
+        while quantity > 0 and fifo_stack:
+            move = fifo_stack.pop(0)
+            last_move = move
+            if qty_on_first_move:
+                valued_qty = move._get_valued_qty()
+                in_qty = qty_on_first_move
+                in_value = move.value * in_qty / valued_qty
+                qty_on_first_move = 0
+            else:
+                in_qty = move._get_valued_qty()
+                in_value = move.value
+            if at_date and not external_location:
+                in_value = move._get_value(at_date=at_date)
+            if in_qty > quantity:
+                in_value = in_value * quantity / in_qty
+                in_qty = quantity
+            fifo_cost += in_value
+            quantity -= in_qty
+        # When we required more quantity than available we extrapolate with the last known price
+        if quantity > 0:
+            if last_move:
+                fifo_cost += quantity * last_move.value
+            else:
+                fifo_cost += quantity * self.standard_price
+        return fifo_cost
+
+=======
+    def _run_fifo(self, quantity, lot=None, at_date=None, location=None):
+        """ Returns the value for the next outgoing product base on the qty give as argument."""
+        self.ensure_one()
+        external_location = location and location.is_valued_external
+
+        fifo_cost = 0
+        fifo_stack, qty_on_first_move = self._run_fifo_get_stack(lot=lot, at_date=at_date, location=location)
+        last_move = False
+        # Going up to get the quantity in the argument
+        while quantity > 0 and fifo_stack:
+            move = fifo_stack.pop(0)
+            last_move = move
+            if qty_on_first_move:
+                valued_qty = move._get_valued_qty()
+                in_qty = qty_on_first_move
+                in_value = move.value * in_qty / valued_qty
+                qty_on_first_move = 0
+            else:
+                in_qty = move._get_valued_qty()
+                in_value = move.value
+            if at_date and not external_location:
+                in_value = move._get_value(at_date=at_date)
+            if in_qty > quantity:
+                in_value = in_value * quantity / in_qty
+                in_qty = quantity
+            fifo_cost += in_value
+            quantity -= in_qty
+        # When we required more quantity than available we extrapolate with the last known price
+        if quantity > 0:
+            if last_move and last_move.quantity:
+                fifo_cost += quantity * (last_move.value / last_move.quantity)
+            else:
+                fifo_cost += quantity * self.standard_price
+        return fifo_cost
+
+>>>>>>> 89901a393cdd070ba32fbbe52a3d0b0d75ca6f11
     def _run_fifo_get_stack(self, lot=None, at_date=None, location=None):
         external_location = location and location.is_valued_external
         fifo_stack = []
