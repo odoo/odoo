odoo/odoo

commit f4230d74c2209d7902df60d9fed91d8000aa267b

[IMP] website: add record fields as field visibility conditions
This commit fixes the test_form_conditional_visibility_record_field tour.

During the refactoring, the logic to handle record fields in conditional
visibility was unintentionally removed. This functionality was
originally introduced in commit [1].

This commit reintroduces the missing logic to ensure record fields are
correctly handled in conditional visibility conditions.

[1]: https://github.com/odoo/odoo/commit/24a7112d7b85d045ffb0629f7feb6e5556e809a1

closes odoo/odoo#215851

X-original-commit: 8c3404c5075a484c5c7e97e98e575de8f878d4b6
Signed-off-by: Francois Georis (fge) <fge@odoo.com>
Signed-off-by: Divyesh Vyas (divy) <divy@odoo.com>

addons/website/static/src/builder/plugins/form/form_option_plugin.js
@@ -202,12 +202,17 @@ export class FormOptionPlugin extends Plugin {
             this._fetchAuthorizedFields.bind(this),
             ({ cacheKey }) => cacheKey
         );
+        this.visibilityConditionCachedRecords = new Cache(
+            this._getVisibilityConditionCachedRecords.bind(this),
+            JSON.stringify
+        );
     }
     destroy() {
         super.destroy();
         this.modelsCache.invalidate();
         this.fieldRecordsCache.invalidate();
         this.authorizedFieldsCache.invalidate();
+        this.visibilityConditionCachedRecords.invalidate();
     }
     getModelsCache(formEl) {
         // Through a method so that it can be overridden.
@@ -273,7 +278,7 @@ export class FormOptionPlugin extends Plugin {
         return field.records;
     }
     async prepareFormModel(el, activeForm) {
-        const formKey = activeForm.website_form_key;
+        const formKey = activeForm?.website_form_key;
         const formInfo = registry.category("website.form_editor_actions").get(formKey, null);
         if (formInfo) {
             const formatInfo = getDefaultFormat(el);
@@ -404,6 +409,12 @@ export class FormOptionPlugin extends Plugin {
             model,
             propertyOrigins,
         ]);
+    }
+    async _getVisibilityConditionCachedRecords(model, domain, fields, kwargs = {}) {
+        return this.services.orm.searchRead(model, domain, fields, {
+            ...kwargs,
+            limit: 1000, // Safeguard to not crash DBs
+        });
     }
     async whitelistForms(el) {
         for (const sigEl of el.querySelectorAll("input[name=website_form_signature]")) {
@@ -552,9 +563,7 @@ export class FormOptionPlugin extends Plugin {
         // Update available visibility dependencies
         const existingDependencyNames = [];
         const conditionInputs = [];
-        for (const el of formEl.querySelectorAll(
-            ".s_website_form_field:not(.s_website_form_dnone)"
-        )) {
+        for (const el of formEl.querySelectorAll(".s_website_form_field")) {
             const inputEl = el.querySelector(".s_website_form_input");
             if (
                 el.querySelector(".s_website_form_label_content") &&
@@ -576,9 +585,12 @@ export class FormOptionPlugin extends Plugin {
         const dependencyEl = getDependencyEl(fieldEl);
         const conditionValueList = [];
         if (dependencyEl) {
+            const containerEl = dependencyEl.closest(".s_website_form_field");
+            const fieldType = containerEl?.dataset.type;
             if (
                 ["radio", "checkbox"].includes(dependencyEl.type) ||
-                dependencyEl.nodeName === "SELECT"
+                dependencyEl.nodeName === "SELECT" ||
+                fieldType === "record"
             ) {
                 // Update available visibility options
                 const inputContainerEl = fieldEl;
@@ -592,6 +604,26 @@ export class FormOptionPlugin extends Plugin {
                     if (!inputContainerEl.dataset.visibilityCondition) {
                         inputContainerEl.dataset.visibilityCondition =
                             dependencyEl.querySelector("option").value;
+                    }
+                } else if (fieldType === "record") {
+                    const model = containerEl.dataset.model;
+                    const idField = containerEl.dataset.idField || "id";
+                    const displayNameField = containerEl.dataset.displayNameField || "display_name";
+                    const records = await this.visibilityConditionCachedRecords.read(
+                        model,
+                        [],
+                        [idField, displayNameField]
+                    );
+                    for (const record of records) {
+                        conditionValueList.push({
+                            value: String(record[idField]),
+                            textContent: record[displayNameField],
+                        });
+                    }
+                    if (!inputContainerEl.dataset.visibilityCondition) {
+                        inputContainerEl.dataset.visibilityCondition = String(
+                            records[0]?.[idField]
+                        );
                     }
                 } else {
                     // DependencyEl is a radio or a checkbox

addons/test_website/static/tests/tours/form.js
@@ -2,6 +2,7 @@ import {
     clickOnEditAndWaitEditMode,
     clickOnSave,
     registerWebsitePreviewTour,
+    changeOptionInPopover,
 } from "@website/js/tours/tour_utils";
 
 registerWebsitePreviewTour(
@@ -16,24 +17,15 @@ registerWebsitePreviewTour(
             trigger: ":iframe .s_website_form .s_website_form_input[name=name]",
             run: "click",
         },
-        {
-            content: "Open visibility dropdown",
-            trigger: 'we-select:has([data-set-visibility="conditional"])',
-            run: "click",
-        },
-        {
-            content: "Set visibility to conditional",
-            trigger: '[data-set-visibility="conditional"]',
-            run: "click",
-        },
+        ...changeOptionInPopover("Field", "Visibility", "Visible only if"),
         {
             content: "Open model selector",
-            trigger: 'we-select:has([data-select-data-attribute="2"])',
+            trigger: "button[id='hidden_condition_record_opt']:contains('Test Tag')",
             run: "click",
         },
         {
             content: "Set model to tag #2",
-            trigger: '[data-select-data-attribute="2"]',
+            trigger: ".o_popover div.o-dropdown-item:contains('Test Tag #2')",
             run: "click",
         },
         ...clickOnSave(),
@@ -50,12 +42,12 @@ registerWebsitePreviewTour(
         },
         {
             content: "Open comparator dropdown",
-            trigger: 'we-select:has([data-select-data-attribute="!selected"])',
+            trigger: "button[id='hidden_condition_record_opt']:contains('Is equal to')",
             run: "click",
         },
         {
             content: "Set comparator to Is not equal",
-            trigger: '[data-select-data-attribute="!selected"]',
+            trigger: ".o_popover div.o-dropdown-item:contains('Is not equal to')",
             run: "click",
         },
         ...clickOnSave(),

addons/test_website/tests/test_form.py
@@ -1,11 +1,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo.tests import tagged, HttpCase
-import unittest
 
 
-# TODO master-mysterious-egg fix error
-@unittest.skip("prepare mysterious-egg for merging")
 @tagged('-at_install', 'post_install')
 class TestForm(HttpCase):
 

commit 09238277645c93d32757db7876a816d627fc599b

[FIX] website: use name as value for radio options
Radio options and checkboxes for the website form were changed such that
the values that were linked to their html elements were set as their
ID's instead of their display names. This caused issues with sending an
email from the form as these ID's are nonsensical to the end user.

This change reverts the functionality to instead set the value to the
display name in order to give meaningful values back on the email.

issue introduced in: f4230d74c2209d7902df60d9fed91d8000aa267b
value was set to the record idField instead of the record
displayNameField causing this issue

opw-4994387

addons/website/static/src/builder/plugins/form/form_option_plugin.js
@@ -599,13 +599,13 @@ export class FormOptionPlugin extends Plugin {
                     );
                     for (const record of records) {
                         conditionValueList.push({
-                            value: String(record[idField]),
+                            value: String(record[displayNameField]),
                             textContent: record[displayNameField],
                         });
                     }
                     if (!inputContainerEl.dataset.visibilityCondition) {
                         inputContainerEl.dataset.visibilityCondition = String(
-                            records[0]?.[idField]
+                            records[0]?.[displayNameField]
                         );
                     }
                 } else {