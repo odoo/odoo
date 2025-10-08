7eb2a4bc18286f41d2d8e9916454386f1e5adf31
utma-odoo <utma@odoo.com>
2025-10-08 12:23:59 +0000

    [FIX] sale_pdf_quote_builder: prevent error when uploading invalid pdf file
    Currently, an error occurs when uploading encrypted or incomplete
    PDF files (missing EOF marker) while creating a quotation document
    header or footer.
    
    Steps to reproduce:
    - Install the sale_pdf_quote_builder module.
    - Navigate to: Sales > Configuration > Headers/Footers.
    - Upload encrypted file [1], or incomplete file [2].
    
    Error:
    PyPDF2.errors.DependencyError: PyCryptodome is required for AES algorithm
    PyPDF2.errors.PdfReadError: EOF marker not found
    
    Root cause:
    At [3], `_get_form_fields_from_pdf` and `_ensure_document_not_encrypted` directly
    call `pdf.PdfFileReader`, when it fails to read or decrypt the file, Python raises
    an error.
    
    Fix:
    Flush the recordset immediately so all computated fields and constraint
    are computed inside the try/except. Use the opportunity to send valid
    HTTP response (status-code and content-type wise).
    
    [1]:
    https://drive.google.com/file/d/1moSlwXHkqcV6_7zHBNhLMLi-9Ye_xDGJ/view?usp=sharing
    
    [2]:
    https://drive.google.com/file/d/16O4LLH8dL0RWmbOx4HrcooFUyesWaVi-/view?usp=sharing
    
    [3]:
    https://github.com/odoo/odoo/blob/694f1d0fb03b56dd41a59eb676e56622634cc91b/addons/sale_pdf_quote_builder/utils.py#L11
    
    opw-5227601
    sentry-6928220164
    
    Helped-by: Julien Castiaux <juc@odoo.com>
    X-original-commit: 4dd1821980d551a8db2d0046ac5e50146217c210

--- addons/sale_pdf_quote_builder/models/sale_pdf_form_field.py
+++ addons/sale_pdf_quote_builder/models/sale_pdf_form_field.py
@@ -191,6 +191,19 @@ class SalePdfFormField(models.Model):
         self._create_or_update_form_fields_on_pdf_records(quote_documents, 'quotation_document')
 
     @api.model
+    def _ensure_document_not_encrypted(self, document):
+        document_is_invalid = False
+        try:
+            document_is_invalid = pdf.PdfFileReader(io.BytesIO(document), strict=False).isEncrypted
+        except (pdf.DependencyError, pdf.PdfReadError):
+            document_is_invalid = True
+        if document_is_invalid:
+            raise ValidationError(_(
+                "It seems that we're not able to process this pdf inside a quotation. It is either"
+                " encrypted, or encoded in a format we do not support."
+            ))
+
+    @api.model
     def _create_or_update_form_fields_on_pdf_records(self, records, doc_type):
         existing_form_fields = self.env['sale.pdf.form.field'].search(
             [('document_type', '=', doc_type)]
@@ -203,6 +216,7 @@ class SalePdfFormField(models.Model):
 
         for document in records:
             if document.raw:
+                self._ensure_document_not_encrypted(document.raw)
                 reader = pdf.PdfFileReader(io.BytesIO(document.raw), strict=False)
                 for field in unique(reader.getFormTextFields()):
                     if field not in existing_form_fields_name:
