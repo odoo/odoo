895e8e29e6873fe244fc0655ef9c73e53969413c
Krzysztof Magusiak (krma) <krma@odoo.com>
2025-10-08 14:15:53 +0000

    [FIX] orm: reading m2m filter after read
    Reading `attachment_ids` on a Many2many field uses `_search` on the
    comodel, but does not necessarily use the `bypass_search_access` flag
    because we are reading (not searching).
    On some models, such as attachments, the `_search` method may start
    filtering all data in memory. To avoid such cases, if the method is
    overwritten, generate the query on the comodel with bypassing accesses,
    then join with the model ids, retrieve the records and filter them.

--- odoo/orm/fields_relational.py
+++ odoo/orm/fields_relational.py
@@ -1358,10 +1358,14 @@ class Many2many(_RelationalMulti):
         context.update(self.context)
         comodel = records.env[self.comodel_name].with_context(**context)
 
+        # bypass the access during search if method is overwriten to avoid
+        # possibly filtering all records of the comodel before joining
+        filter_access = self.bypass_search_access and type(comodel)._search is not BaseModel._search
+
         # make the query for the lines
         domain = self.get_comodel_domain(records)
         try:
-            query = comodel._search(domain, order=comodel._order)
+            query = comodel._search(domain, order=comodel._order, bypass_access=filter_access)
         except AccessError as e:
             raise AccessError(records.env._("Failed to read field %s", self) + '\n' + str(e)) from e
 
@@ -1377,6 +1381,16 @@ class Many2many(_RelationalMulti):
         group = defaultdict(list)
         for id1, id2 in records.env.execute_query(query.select(sql_id1, sql_id2)):
             group[id1].append(id2)
+
+        # filter using record rules
+        if filter_access and group:
+            corecord_ids = OrderedSet(id_ for ids in group.values() for id_ in ids)
+            accessible_corecords = comodel.browse(corecord_ids)._filtered_access('read')
+            if len(accessible_corecords) < len(corecord_ids):
+                # some records are inaccessible, remove them from groups
+                corecord_ids = set(accessible_corecords._ids)
+                for id1, ids in group.items():
+                    group[id1] = [id_ for id_ in ids if id_ in corecord_ids]
 
         # store result in cache
         values = [tuple(group[id_]) for id_ in records._ids]
