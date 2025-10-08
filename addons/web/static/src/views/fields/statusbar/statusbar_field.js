b6e17d42f16b26181a3fa8db0e4c146e02cc9443
Aaron Bohy <aab@odoo.com>
2025-10-08 13:45:21 +0200

    [FIX] web: statusbar field: update value if it changed
    Before this commit, there was a race condition with the statusbar
    field because of which the current status wasn't correctly updated
    when the rpc returned. For instance, in CRM pipeline, open the
    records in form view (to put them in cache). Then, go back in
    kanban and drag a record from a column to another. Re-open a record
    in form view, and use to pager to browse to the updated record. It
    still displayed the former value. This was due to an optimization
    attempt to prevent from re-computing too often the items of the
    statusbar.
    
    closes odoo/odoo#230548
    
    Signed-off-by: Jorge Pinna Puissant (jpp) <jpp@odoo.com>

--- addons/web/static/src/views/fields/statusbar/statusbar_field.js
+++ addons/web/static/src/views/fields/statusbar/statusbar_field.js
@@ -1,5 +1,4 @@
 import { Component, onWillRender, useEffect, useExternalListener, useRef } from "@odoo/owl";
-import { browser } from "@web/core/browser/browser";
 import { useCommand } from "@web/core/commands/command_hook";
 import { Domain } from "@web/core/domain";
 import { Dropdown } from "@web/core/dropdown/dropdown";
@@ -72,7 +71,6 @@ export class StatusBarField extends Component {
             status = "adjusting";
             this.adjustVisibleItems();
             this.render();
-            browser.requestAnimationFrame(() => (status = "idle"));
         };
 
         useEffect(
@@ -80,18 +78,22 @@ export class StatusBarField extends Component {
             () => [status]
         );
 
+        let forceRecomputeItems = false;
         onWillRender(() => {
-            if (status !== "adjusting") {
+            if (status !== "adjusting" || forceRecomputeItems) {
                 Object.assign(this.items, this.getSortedItems());
                 status = "shouldAdjust";
+            } else {
+                status = "idle";
             }
+            forceRecomputeItems = false;
         });
 
         useExternalListener(window, "resize", throttleForAnimation(adjust));
 
         // Special data
         if (this.field.type === "many2one") {
-            this.specialData = useSpecialData((orm, props) => {
+            this.specialData = useSpecialData(async (orm, props) => {
                 const { foldField, name: fieldName, record } = props;
                 const { relation } = record.fields[fieldName];
                 const fieldNames = ["display_name"];
@@ -106,7 +108,9 @@ export class StatusBarField extends Component {
                         record.evalContext
                     );
                 }
-                return orm.searchRead(relation, domain, fieldNames);
+                const res = orm.searchRead(relation, domain, fieldNames);
+                forceRecomputeItems = true;
+                return res;
             });
         }
 
@@ -149,7 +153,7 @@ export class StatusBarField extends Component {
                         }
                         const items = this.getAllItems();
                         return items.length && !items.at(-1).isSelected;
-                    }
+                    },
                 }
             );
         }
