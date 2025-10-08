8789482bb7ce55dd8f9cc54a71ac322ab4efb8fc
divy-odoo <divy@odoo.com>
2025-10-08 11:56:41 +0000

    [FIX] website: add fullscreen loader when loading more themes
    Steps to reproduce:
    
    1. Create a new website and proceed to the theme configuration step.
    2. Click on View More Themes.
    -> You’ll notice a loading effect on the button, but the existing themes
    remain selectable.
    
    Before this commit:
    Users could still select existing themes while additional themes were
    being loaded.
    
    After this commit:
    A fullscreen loader is displayed while loading more themes via the View
    More Themes button, preventing any unintended interactions.
    
    task-4661292
    
    closes odoo/odoo#231829
    
    X-original-commit: a5a3cd1f3830f7778fd3275ba6f1562e80094f18
    Signed-off-by: Benoit Socias (bso) <bso@odoo.com>
    Signed-off-by: Divyesh Vyas (divy) <divy@odoo.com>

--- addons/website/static/src/client_actions/configurator/configurator.js
+++ addons/website/static/src/client_actions/configurator/configurator.js
@@ -24,7 +24,6 @@ import {
     useExternalListener,
 } from "@odoo/owl";
 import { standardActionServiceProps } from "@web/webclient/actions/action_service";
-import { addLoadingEffect as addButtonLoadingEffect } from "@web/core/utils/ui";
 
 export const ROUTES = {
     descriptionScreen: 2,
@@ -561,7 +560,7 @@ export class ThemeSelectionScreen extends ApplyConfiguratorScreen {
     }
 
     async getMoreThemes() {
-        const removeLoadingEffect = addButtonLoadingEffect(this.extraThemesButtonRef.el);
+        this.uiService.block();
         const themes = await getRecommendedThemes(
             this.orm,
             this.state,
@@ -572,7 +571,7 @@ export class ThemeSelectionScreen extends ApplyConfiguratorScreen {
         const mainThemeNames = this.state.themes.map((theme) => theme.name);
         this.state.extraThemes = themes.filter((extraTheme) => !mainThemeNames.includes(extraTheme.name));
         this.state.extraThemesLoaded = true;
-        removeLoadingEffect();
+        this.uiService.unblock();
     }
 
     getExtraThemeName(idx) {
