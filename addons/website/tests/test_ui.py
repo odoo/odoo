d0fe70b7ab8f8cad27fb1084bee8ee4dd151650e
Arib Ansari <aans@odoo.com>
2025-10-08 11:37:48 +0000

    [FIX] website: use tour helper to switch website
    `snippet_translation_switching_website` was previously using custom
    steps to switch the website. Though it worked, it was not very
    deterministic and broke the tour in a few runbot builds.
    
    This commit updates the `snippet_translation_switching_website` tour to
    use the `testSwitchWebsite` helper function instead of custom steps,
    providing a more reliable and deterministic way to switch websites.
    
    runbot-229696
    
    closes odoo/odoo#230889
    
    X-original-commit: ac077b0cb3127f0b4a01d3aeae53dc02f774fbbc
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/website/tests/test_ui.py
+++ addons/website/tests/test_ui.py
@@ -283,7 +283,7 @@ class TestUiTranslate(odoo.tests.HttpCase):
             'language_ids': [(6, 0, [self.env.ref('base.lang_en').id, parseltongue.id])],
             'default_lang_id': parseltongue.id,
         })
-        self.env['website'].create({
+        website_3 = self.env['website'].create({
             'name': 'website fu_GB',
             'language_ids': [Command.set([fake_user_lang.id])],
             'default_lang_id': fake_user_lang.id,
@@ -291,7 +291,9 @@ class TestUiTranslate(odoo.tests.HttpCase):
 
         self.start_tour(f"/website/force/{website.id}", 'snippet_translation', login='admin')
         self.start_tour(f"/website/force/{website_2.id}", 'snippet_translation_changing_lang', login='admin')
-        self.start_tour(f"/website/force/{website_2.id}", 'snippet_translation_switching_website', login='admin')
+        self.start_tour(f"/website/force/{website_2.id}", 'snippet_translation_switching_website', login='admin', cookies={
+            'websiteIdMapping': json.dumps({website_3.name: website_3.id})
+        })
         self.start_tour(f"/website/force/{website.id}", 'snippet_dialog_rtl', login='admin')
 
 
