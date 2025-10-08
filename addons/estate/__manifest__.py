f5e7a252cae4f921f94d3f6dd967585afd90b276
LoganApp <contactloganyt@gmail.com>
2025-10-07 21:43:24 -0500

    Add basic model, view, menu items

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -11,6 +11,7 @@
     'application': True,
     'data': [
         'security/ir.model.access.csv',
+        # 'views/estate_property_view_tree.xml',
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
     ],
