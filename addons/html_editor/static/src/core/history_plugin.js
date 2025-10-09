d554a1aa739f8b34e8ec7e52fe1e1df2deaf9a4b
FrancoisGe <fge@odoo.com>
2025-10-09 11:43:24 +0200

    [FIX] html_editor,...: improve perf with setAttribute ..., mail, website, html_builder
    The goal of this commit is to improve performance when using setAttribute.
    Using the native setAttribute causes the layout to be recalculated
    each time it is used, even if the same value is set. We are therefore
    introducing the setAttribute utils, which will check that the value
    is different before applying it.
    
    These performance issues are noticeable in situations where many
    elements need to be modified.

--- addons/html_editor/static/src/core/history_plugin.js
+++ addons/html_editor/static/src/core/history_plugin.js
@@ -4,7 +4,7 @@ import { childNodes, descendants, getCommonAncestor } from "../utils/dom_travers
 import { hasTouch } from "@web/core/browser/feature_detection";
 import { withSequence } from "@html_editor/utils/resource";
 import { Deferred } from "@web/core/utils/concurrency";
-import { toggleClass } from "@html_editor/utils/dom";
+import { setAttribute, toggleClass } from "@html_editor/utils/dom";
 import { omit, pick } from "@web/core/utils/objects";
 import { trackOccurrences, trackOccurrencesPair } from "../utils/tracking";
 
@@ -1803,7 +1803,7 @@ export class HistoryPlugin extends Plugin {
             }
 
             for (const node of this._onKeyupResetContenteditableNodes) {
-                node.setAttribute("contenteditable", false);
+                setAttribute(node, "contenteditable", "false");
             }
         }
     }
@@ -1814,7 +1814,7 @@ export class HistoryPlugin extends Plugin {
             this._onKeyupResetContenteditableNodes.length
         ) {
             for (const node of this._onKeyupResetContenteditableNodes) {
-                node.setAttribute("contenteditable", true);
+                setAttribute(node, "contenteditable", "true");
             }
             this._onKeyupResetContenteditableNodes = [];
         }
