63cb2255dcb411890f125ed97faf3fb5c801c576
tsm-odoo <tsm@odoo.com>
2025-10-09 14:45:34 +0200

    [IMP] mail: convert followers to py model

--- addons/mail/tools/js_model_generation.py
+++ addons/mail/tools/js_model_generation.py
@@ -34,6 +34,7 @@ MODELS_TO_INCLUDE = [
 # so new models can be progressively exposed without fetching everything.
 MODELS_TO_EXPOSE = [
     "mail.activity",
+    "mail.followers",
     "mail.message",
     "mail.message.link.preview",
 ]
