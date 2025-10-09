d2c68a316ad773c45c4481da19b70c0368c9f171
Levi Siuzdak (sile) <sile@odoo.com>
2025-10-09 08:13:56 +0000

    [FIX] website_sale: remove random error test in versions 18.*
    Versions
    --------
    - 18.0
    - saas-18.2
    - saas-18.3
    - saas-18.4
    
    Issue
    -----
    The `test_toggle_contact_us_button_visibility` causes random errors in
    runbot.
    
    Cause
    -----
    Unsure, but with commit 26d22fc975e3f removing jQuery from
    `VariantMixin`, the random error no longer seems to pop up.
    
    Solution
    --------
    Remove the test for versions before 19.0.
    
    runbot-145473
    
    closes odoo/odoo#230977
    
    X-original-commit: c23ed790cfa9b0c2e5ae68e8b2876cda76b96a63
    Signed-off-by: Levi Siuzdak <sile@odoo.com>

--- addons/website_sale/tests/test_website_sale_product_page.py
+++ addons/website_sale/tests/test_website_sale_product_page.py
@@ -16,20 +16,6 @@ class TestWebsiteSaleProductPage(HttpCase, ProductVariantsCommon, WebsiteSaleCom
 
         cls.product_template_sofa.website_published = True
 
-    def test_toggle_contact_us_button_visibility(self):
-        """Check that the "Contact Us" button:
-          - is shown for zero-priced products
-          - is hidden for other products
-          - is not displayed at the same time as the "Add to Cart" button
-        """
-        self.website.prevent_zero_price_sale = True
-
-        self.product_template_sofa.list_price = 0
-        red_sofa, blue_sofa = self.product_template_sofa.product_variant_ids[:2]
-        blue_sofa.product_template_attribute_value_ids.price_extra = 20
-
-        self.start_tour(red_sofa.website_url, 'website_sale_contact_us_button')
-
     def test_product_reviews_reactions_public(self):
         """ Check that public users can not react to reviews """
         password = "Pl1bhD@2!kXZ"
