1b467971dd1fe1989b95e831932e8400a67dda4e
Robin Engels (roen) <roen@odoo.com>
2025-10-09 14:08:06 +0200

    [FIX] point_of_sale: serial refund
    When refunding an order that contains a product tracked by serial number
    if you refund less than the total quantity of the product in the order
    all the serial numbers of the original order would be set anyway. Also
    you were not able to select the serial numbers to refund.
    
    Steps to reproduce:
    -------------------
    * Create product P1, storable, tracked by serial number
    * Create 2 Serial Numbers SN1, SN2 for P1
    * Create a POS order with 2x P1 (SN1, SN2)
    * Pay and validate the order
    * Refund the order, set quantity of P1 to 1
    * Validate the refund
    > Observation: The refund order contains P1 with SN1 and SN2, and you
    cannot edit the serial numbers.
    
    Why the fix:
    ------------
    We now allow to select the serial numbers when doing a refund and only
    show the serial numbers that were in the original order. We also select
    by default the same number of serial numbers as the quantity to refund.
    
    opw-4965171
    
    closes odoo/odoo#230778
    
    Signed-off-by: Stéphane Vanmeerhaeghe (stva) <stva@odoo.com>

--- addons/point_of_sale/static/src/app/services/pos_store.js
+++ addons/point_of_sale/static/src/app/services/pos_store.js
@@ -2245,6 +2245,43 @@ export class PosStore extends WithLazyGetterTrap {
 
         return payload;
     }
+    async editLotsRefund(line) {
+        const product = line.getProduct();
+        const packLotLinesToEdit = line.pack_lot_ids.map((p) => p.lot_name);
+        const alreadyRefundedLots = line.refunded_orderline_id.refund_orderline_ids
+            .filter((item) => !["cancel", "draft"].includes(item.order_id.state))
+            .flatMap((item) => item.pack_lot_ids)
+            .map((pack_lot) => pack_lot.lot_name);
+        const options = line.refunded_orderline_id.pack_lot_ids
+            .map((p) => p.lot_name)
+            .filter((lotName) => !alreadyRefundedLots.includes(lotName));
+
+        const payload = await makeAwaitable(this.dialog, EditListPopup, {
+            title: _t("Lot/Serial number(s) required for"),
+            name: product.display_name,
+            isSingleItem: product.isAllowOnlyOneLot(),
+            array: packLotLinesToEdit,
+            options: options,
+            customInput: false,
+            uniqueValues: product.tracking === "serial",
+            isLotNameUsed: () => false,
+        });
+        if (payload) {
+            const modifiedPackLotLines = {};
+            const newPackLotLines = [];
+            for (const item of payload) {
+                if (item.id) {
+                    modifiedPackLotLines[item.id] = item.text;
+                } else {
+                    newPackLotLines.push({ lot_name: item.text });
+                }
+            }
+            return { modifiedPackLotLines, newPackLotLines };
+        } else {
+            return null;
+        }
+    }
+
     async editLots(product, packLotLinesToEdit) {
         const isAllowOnlyOneLot = product.isAllowOnlyOneLot();
         let canCreateLots = this.pickingType.use_create_lots || !this.pickingType.use_existing_lots;
