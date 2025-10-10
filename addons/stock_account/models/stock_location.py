e25dd200cb2a534009acf81a23954157e017bf69
Arnold Moyaux <arm@odoo.com>
2025-10-10 16:03:47 +0200

    [FIX] stock_account: Add a search function for valued location
    `qty_available` is used now instead of sum of `stock.valuation.layer`
    (18.0). However `qty_available` has an implicit domain on
    `stock.location` under a `stock.warehouse`. In case of inventory
    valuation we want to consider all the locations under the company.
    
    In order to call `qty_available` using `location` key in context.
    We created this function
    
    Part-of: odoo/odoo#230725
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/stock_account/models/stock_location.py
+++ addons/stock_account/models/stock_location.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models
+from odoo.fields import Domain
 
 
 class StockLocation(models.Model):
@@ -16,8 +17,17 @@ class StockLocation(models.Model):
              "This has no effect for internal locations."
              "In real-time: The product's account will be used as counterparty account."
              "During closing: The company's stock valuation account will be used as counterparty account.")
-    is_valued_internal = fields.Boolean('Is valued inside the company', compute="_compute_is_valued")
+    is_valued_internal = fields.Boolean('Is valued inside the company', compute="_compute_is_valued", search="_search_is_valued")
     is_valued_external = fields.Boolean('Is valued outside the company', compute="_compute_is_valued")
+
+    def _search_is_valued(self, operator, value):
+        if operator not in ['=', '!=']:
+            raise NotImplementedError(self.env._("Invalid search operator or value"))
+        positive_operator = (operator == '=' and value) or (operator == '!=' and not value)
+        domain = Domain([('company_id', '!=', False), ('usage', 'in', ['internal', 'transit'])])
+        if positive_operator:
+            return domain
+        return ~domain
 
     def _compute_is_valued(self):
         for location in self:
