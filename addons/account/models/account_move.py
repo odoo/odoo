ed2a28471d84c514314e34c68bc22bee8160ba8c
thle-odoo <thle@odoo.com>
2025-10-10 09:14:28 +0200

    [IMP] tools: reimplement frozendict
    Before this commit, Odoo had two implementations for creating
    an immutable dictionary. These two implementations have different
    behaviors and coexist for backward compatibility reasons.
    
    The purpose of this commit is to leave only one implementation
    for the immutable dictionary, called `frozendict`.
    
    This implementation makes it truly impossible to modify the mapping.
    
    In addition, by default, we don't override internal methods of the
    dictionary which improves performance (despite the cost during creation).
    
    Note 1:
    Classes `LangData` and `LangDataDict` are adapted as `Mapping` subclasses
    instead of `ReadonlyDict`. This simplifies their implementation, which is
    not performance-critical.
    
    Note 2:
    This change is possible because the `MappingProxyType` class implements
    fallback on the `__hash__` method since version 3.12
    (thanks to https://github.com/odoo/odoo/commit/d357125e09b8f3613269bb8627972588c20900aa).
    
    task-4808836

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -3,6 +3,7 @@
 import ast
 import calendar
 from collections import Counter, defaultdict
+from collections.abc import Mapping
 from contextlib import ExitStack, contextmanager
 from datetime import date, timedelta
 from dateutil.relativedelta import relativedelta
@@ -4794,7 +4795,7 @@ class AccountMove(models.Model):
             for grouping_key, values in aggregated_values.items():
                 if not grouping_key:
                     continue
-                if isinstance(grouping_key, dict):
+                if isinstance(grouping_key, Mapping):
                     values.update(grouping_key)
                 tax_details[grouping_key] = values
 
@@ -4803,7 +4804,7 @@ class AccountMove(models.Model):
         for grouping_key, values in values_per_grouping_key.items():
             if not grouping_key:
                 continue
-            if isinstance(grouping_key, dict):
+            if isinstance(grouping_key, Mapping):
                 values.update(grouping_key)
             tax_details[grouping_key] = values
 
