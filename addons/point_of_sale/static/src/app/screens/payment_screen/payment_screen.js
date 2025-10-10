9505ab62813c953fd36178aac8ef4bcf8e75acfb
zorbmax <orma@odoo.com>
2025-10-10 09:40:05 +0000

    [FIX] point_of_sale: sendForceDone set paymentTerminalInProgress to false
    Before this commit, the function sendForceDone was not setting paymentTerminalInProgress to false, which blocked the user from doing another terminal payment as the payment methods linked to a terminal are not clickable.
    
    After this commit, sendForceDone can be used without blocking future terminal payments.
    
    opw-4978772
    
    closes odoo/odoo#231098
    
    X-original-commit: 46270c75beed81af4a9da3729e342beee532d0f4
    Signed-off-by: Yaroslav Soroko (yaso) <yaso@odoo.com>
    Signed-off-by: Maxime Orban (orma) <orma@odoo.com>

--- addons/point_of_sale/static/src/app/screens/payment_screen/payment_screen.js
+++ addons/point_of_sale/static/src/app/screens/payment_screen/payment_screen.js
@@ -646,6 +646,7 @@ export class PaymentScreen extends Component {
     }
     async sendForceDone(line) {
         line.setPaymentStatus("done");
+        this.pos.paymentTerminalInProgress = false;
         const config = this.pos.config;
         const currentOrder = line.pos_order_id;
         if (
