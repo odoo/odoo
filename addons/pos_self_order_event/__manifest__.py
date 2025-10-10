52aeca289b4295b2307986e2c2f2936fae862344
vpan-odoo <vpan@odoo.com>
2025-10-10 16:25:41 +0530

    [IMP] pos_self_order_event: hootify this module

--- addons/pos_self_order_event/__manifest__.py
+++ addons/pos_self_order_event/__manifest__.py
@@ -14,6 +14,9 @@
             'pos_event/static/src/app/models/**/*',
             'pos_event/static/src/app/utils/**/*',
         ],
+        'web.assets_unit_tests': [
+            'pos_self_order_event/static/tests/unit/**/*',
+        ],
     },
     'author': 'Odoo IN Pvt Ltd',
     'license': 'LGPL-3',
