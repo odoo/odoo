64899e59c7459bd71305c7ebcc45655d2466380b
akth-odoo <akth@odoo.com>
2025-10-13 09:38:33 +0000

    [FIX] mail: restore mentioned channels when editing via ArrowUp
    The logic to restore mentioned channels was only applied in the `onClick()`
    handler of the Edit message action. As a result, editing a message via the
    ArrowUp shortcut did not restore channel mentions.
    
    This commit introduces `enterEditModeAsync()` to restore mentioned channels for
    both click and ArrowUp edits.
    
    Task-5133698
    
    X-original-commit: 387d06350585c2233f8df4b5e02677d70833523f

--- addons/mail/static/src/discuss/core/common/message_model_patch.js
+++ addons/mail/static/src/discuss/core/common/message_model_patch.js
@@ -63,12 +63,8 @@ const messagePatch = {
         attachments = [],
         { mentionedChannels = [], mentionedPartners = [], mentionedRoles = [] } = {}
     ) {
-        const validChannels = (await Promise.all(this.mentionedChannelPromises)).filter(
-            (channel) => channel !== undefined
-        );
-        const allChannels = this.store.Thread.insert([...validChannels, ...mentionedChannels]);
         return await super.edit(body, attachments, {
-            mentionedChannels: allChannels,
+            mentionedChannels,
             mentionedPartners,
             mentionedRoles,
         });
--- addons/mail/static/tests/message/message.test.js
+++ addons/mail/static/tests/message/message.test.js
@@ -175,6 +175,16 @@ test("Editing message keeps the mentioned channels", async () => {
     await contains(".o-mail-Message-content", { text: "other bye (edited)" });
     await click(".o_channel_redirect", { text: "other" });
     await contains(".o-mail-Discuss-threadName", { value: "other" });
+    // Test editing via arrow up shortcut
+    await click(".o-mail-DiscussSidebarChannel", { text: "general" });
+    await contains(".o-mail-Message");
+    await press("ArrowUp");
+    await contains(".o-mail-Message .o-mail-Composer-input", { value: "#other bye" });
+    await insertText(".o-mail-Message .o-mail-Composer-input", "#other hello", { replace: true });
+    await click(".o-mail-Message a", { text: "save" });
+    await contains(".o-mail-Message-content", { text: "other hello (edited)" });
+    await click(".o_channel_redirect", { text: "other" });
+    await contains(".o-mail-Discuss-threadName", { value: "other" });
 });
 
 test("Can edit message comment in chatter", async () => {
