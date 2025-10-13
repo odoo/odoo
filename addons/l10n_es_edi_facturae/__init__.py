882be0de92fa7ec5b301f52725e1b2853fca3402
odoo <aizu@odoo.com>
2025-10-13 15:21:32 +0000

    [FIX] l10n_es_edi_facturae: tax chart template
    Currently the tax data defined in l10n_es_edi_facturae is incomplete and never loaded. This commit makes sure that the l10n_es_edi_facturae_tax_type is loaded correctly and adds the appropriate templates for es_common_mainland and es_canary_common. The default is also removed on the field l10n_es_edi_facturae_tax_type to avoid the value being assigned for unrelated taxes.
    
    task-4981325
    
    closes odoo/odoo#232432
    
    X-original-commit: 372c56fa188cd29e95b7dc0731e1926a1cae229f
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>
    Signed-off-by: Ai Lin Zuccarello (aizu) <aizu@odoo.com>

--- addons/l10n_es_edi_facturae/__init__.py
+++ addons/l10n_es_edi_facturae/__init__.py
@@ -10,8 +10,20 @@ def _l10n_es_edi_facturae_post_init_hook(env):
     """
     We need to replace the existing spanish taxes following the template so the new fields are set properly
     """
-    for company in env['res.company'].search([('chart_template', '=like', 'es_%'), ('parent_id', '=', False)]):
+    for company in env['res.company'].search([('chart_template', 'like', r'es\_%'), ('parent_id', '=', False)]):
         Template = env['account.chart.template'].with_company(company)
+        if company.chart_template.startswith("es_canary"):
+            tax_data = Template._get_es_facturae_account_tax_es_canary_common()
+        else:
+            tax_data = Template._get_es_facturae_account_tax_es_common_mainland()
+        # Filter out data for non-existing taxes; else this function will raise.
+        # In case of data for a non-existing tax we would try to create that tax.
+        # This would fail because we don't supply enough information in this module (just `l10n_es_edi_facturae_tax_type`).
+        tax_data = {
+            xmlid: value
+            for xmlid, value in tax_data.items()
+            if Template.ref(xmlid, raise_if_not_found=False)
+        }
         Template._load_data({
-            'account.tax': Template._get_es_facturae_account_tax(),
+            'account.tax': tax_data,
         })
