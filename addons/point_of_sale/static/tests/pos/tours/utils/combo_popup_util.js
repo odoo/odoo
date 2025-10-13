65cd5e2eb15b8d731c713cb9f864f72fedbe6f19
Jitendra Prajapat <jipr@odoo.com>
2025-10-13 11:53:18 +0000

    [FIX] point_of_sale: respect product image configuration in combo product also
    Before this commit:
    ====================
    In POS, when product images were configured to be hidden, the setting was
    correctly applied to normal products. However, in the combo product
    configurator, product images were still displayed, causing inconsistency with
    the configured behavior.
    
    After this commit:
    ======================
    The combo product configurator now respects the product image visibility
    configuration, ensuring consistent behavior across all product types in the
    POS interface.
    
    Task-5163955
    
    closes odoo/odoo#241346
    
    X-original-commit: 7d0ba632a05674c3b3ca9a476de59b5767f51d60
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Jitendra Kumar Prajapat (jipr) <jipr@odoo.com>

--- addons/point_of_sale/static/tests/pos/tours/utils/combo_popup_util.js
+++ addons/point_of_sale/static/tests/pos/tours/utils/combo_popup_util.js
@@ -68,3 +68,14 @@ export function checkProductQty(productName, expectedQty) {
         },
     };
 }
+export function checkImgAndSelect(productName, checkImg = false) {
+    const productArticleSelector = productTrigger(productName);
+    const withImg = `${productArticleSelector}:has(.product-img)`;
+    const withoutImg = `${productArticleSelector}:not(:has(.product-img))`;
+    const trigger = `.modal ${checkImg ? withImg : withoutImg}`;
+    return {
+        content: `Check image & select combo item ${productName}`,
+        trigger: trigger,
+        run: "click",
+    };
+}
