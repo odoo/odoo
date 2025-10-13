818d895a813027bc827d3945be3cceb54db0e8d1
Gauthier Wala (gawa) <gawa@odoo.com>
2025-10-13 11:34:36 +0200

    [FIX] account_edi_proxy_client: proxy_type for nemhandel is nemhandel
    The value to neutralize a user should be nemhandel and not
    l10n_dk_nemhandel
    
    closes odoo/odoo#231264
    
    Signed-off-by: Maximilien La Barre (malb) <malb@odoo.com>

--- addons/account_edi_proxy_client/data/neutralize.sql
+++ addons/account_edi_proxy_client/data/neutralize.sql
@@ -2,7 +2,7 @@
 -- for malaysian edi, this script can cause issue as you could have both a demo and prod user, in which case it breaks the unique constrain. Another neutralize in the malaysian module disable the clients instead.
 UPDATE account_edi_proxy_client_user
 SET edi_mode = CASE
-                    WHEN proxy_type IN ('l10n_it_edi', 'peppol', 'l10n_dk_nemhandel') THEN 'demo'
+                    WHEN proxy_type IN ('l10n_it_edi', 'peppol', 'nemhandel') THEN 'demo'
                     ELSE 'test'
                END
 WHERE proxy_type != 'l10n_my_edi';
