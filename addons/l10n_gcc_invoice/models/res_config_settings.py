f77ef982868770b8a7d90345da4cd56d136c0d3f
hatr-odoo <hatr@odoo.com>
2025-10-13 11:34:08 +0530

    [IMP] l10n_gcc_invoice: remove l10n_gcc_country_is_gcc
    l10n_gcc_country_is_gcc is used to control visibility in views. However,
    it is not required, as the same functionality can be achieved using the
    generic fields based on country_group_codes.
    
    ref - https://github.com/odoo/odoo/pull/220167/commits/39ba5962923f04eef85174aaf2667481510cfaf4

--- addons/l10n_gcc_invoice/models/res_config_settings.py
+++ addons/l10n_gcc_invoice/models/res_config_settings.py
@@ -5,4 +5,3 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     l10n_gcc_dual_language_invoice = fields.Boolean(related='company_id.l10n_gcc_dual_language_invoice', readonly=False)
-    l10n_gcc_country_is_gcc = fields.Boolean(related='company_id.l10n_gcc_country_is_gcc')
