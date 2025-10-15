0edfccd27381b6032b304d08038c3e7ea6b95b90
Gorash <chm@odoo.com>
2025-10-15 16:50:45 +0200

    [FIX] test_website_modules: Adjust performance tests for /shop (frontend)
    The performance tests for the /shop page were adjusted to account for
    variations in the number of SQL queries caused by:
    - Product configurations (taxes, pricelists) from demo data.
    - The installation of specific modules (addons) (Community or Enterprise).
    
    To prevent unjustified test failures, necessary dependencies were added
    and the expected query count is now conditional.
    
    The main objective is to ensure that the number of queries does not
    increase over time, as any rise negatively impacts the loading time of
    the /shop page.
    
    The logging performed by the website performance tests has been enhanced
    to quickly provide the difference (diff) in the query count, which
    facilitates debugging.
    
    closes odoo/odoo#232052
    
    Signed-off-by: Raphael Collet <rco@odoo.com>

--- addons/website/tests/test_performance.py
+++ addons/website/tests/test_performance.py
@@ -93,11 +93,29 @@ class UtilPerf(HttpCaseWithUserPortal, HttpCaseWithUserDemo):
                 _logger.warning("Query type %s for query %s is not supported by _check_url_hot_query", query_type, query)
             log_target.setdefault(table, 0)
             log_target[table] = log_target[table] + 1
+
+        if not select_tables_perf:
+            select_tables_perf = {}
+        select = {}
+        for key in (set(sql_from_tables) | set(select_tables_perf)):
+            value = sql_from_tables.get(key, 0) - select_tables_perf.get(key, 0)
+            if value:
+                select[key] = value
+
+        if not insert_tables_perf:
+            insert_tables_perf = {}
+        insert = {}
+        for key in (set(sql_into_tables) | set(insert_tables_perf)):
+            value = sql_into_tables.get(key, 0) - insert_tables_perf.get(key, 0)
+            if value:
+                insert[key] = value
+
         if query_count != expected_query_count:
-            msq = f"Expected {expected_query_count} queries but {query_count} where ran: {query_separator}{queries}{query_separator}"
+            msq = f"Expected {expected_query_count} queries but {query_count} where ran:\nDiff of select queries: {select}\nDiff of insert queries: {insert}{query_separator}{queries}{query_separator}"
             self.fail(msq)
-        self.assertEqual(sql_from_tables, select_tables_perf or {}, f'Select queries does not match: {query_separator}{queries}{query_separator}')
-        self.assertEqual(sql_into_tables, insert_tables_perf or {}, f'Insert queries does not match: {query_separator}{queries}{query_separator}')
+
+        self.assertDictEqual(sql_from_tables, select_tables_perf, f'Select queries does not match: {select}{query_separator}{queries}{query_separator}')
+        self.assertDictEqual(sql_into_tables, insert_tables_perf, f'Insert queries does not match: {insert}{query_separator}{queries}{query_separator}')
 
 
 class TestStandardPerformance(UtilPerf):
