bf434be9e257f2ea51d8ac88fe535766cf95c1a3
Adrien Schoffeniels <adsc@odoo.com>
2025-10-15 14:48:32 +0200

    [IMP] cloud_storage: disable cloud storage for chats with ai agents
    Purpose:
    --------
    The related enterprise PR introduces the ability to upload files when
    chatting with ai agents. This feature is not available with attachments
    of type url. This commit disables cloud storage when chatting with an ai
    agent. Changes have been done in the cloud_storage module to avoid a bridge
    module.
    
    Task-5031785
    
    closes odoo/odoo#231695
    
    Related: odoo/enterprise#92885
    Signed-off-by: Adrien Schoffeniels (adsc) <adsc@odoo.com>

--- addons/cloud_storage/static/src/core/common/composer_patch.js
+++ addons/cloud_storage/static/src/core/common/composer_patch.js
@@ -4,8 +4,10 @@ import { patch } from "@web/core/utils/patch";
 import { session } from "@web/session";
 
 patch(Composer.prototype, {
-    setup() {
-        super.setup();
-        this.cloudStorageUsable = session.cloud_storage_min_file_size !== undefined;
+    get cloudStorageUsable() {
+        return (
+            !this.thread?.channel?.ai_agent_id && // ai_agent (enterprise) does not support url files
+            session.cloud_storage_min_file_size !== undefined
+        );
     },
 });
