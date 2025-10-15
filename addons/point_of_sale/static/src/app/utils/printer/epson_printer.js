907cc2fd05bef5faf2bc255ccd829c376ce8bc9e
yaso <yaso@odoo.com>
2025-10-15 15:49:37 +0000

    [IMP] point_of_sale: test button for epos printers
    This PR adds a test button to all the views in point of sale where the
    epos printer can be configured (pos config / res config and preparation
    printer)
    
    When clicked a test receipt is printed on the epos printer or else
    an error is shown like "no paper" / "failed to reach printer" etc.
    
    Finally it adds a timeout of 15s to Epos printing which isn't there now
    
    Task-5153542
    
    X-original-commit: 72a8af7e386bcbb13628868ee90b51e812d18da5

--- addons/point_of_sale/static/src/app/utils/printer/epson_printer.js
+++ addons/point_of_sale/static/src/app/utils/printer/epson_printer.js
@@ -108,6 +108,7 @@ export class EpsonPrinter extends BasePrinter {
             const res = await fetch(this.address, {
                 method: "POST",
                 body: img,
+                signal: AbortSignal.timeout(15000),
             });
             const body = await res.text();
             const parser = new DOMParser();
