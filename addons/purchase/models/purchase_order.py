7b3603f8d1e53264a381a194fb2d0af4cb0ad938
thle-odoo <thle@odoo.com>
2025-10-15 17:02:37 +0100

    [FIX] purchase: check internal user
    Verify that the user is an internal user
    to avoid unexpected behavior.
    
    task-5349128

--- addons/purchase/models/purchase_order.py
+++ addons/purchase/models/purchase_order.py
@@ -12,7 +12,7 @@ from odoo import api, Command, fields, models, _
 from odoo.osv import expression
 from odoo.tools import format_amount, format_date, format_list, formatLang, groupby
 from odoo.tools.float_utils import float_is_zero
-from odoo.exceptions import UserError, ValidationError
+from odoo.exceptions import AccessDenied, UserError, ValidationError
 
 
 class PurchaseOrder(models.Model):
@@ -866,6 +866,8 @@ class PurchaseOrder(models.Model):
         """ This function returns the values to populate the custom dashboard in
             the purchase order views.
         """
+        if not self.env.user._is_internal():
+            raise AccessDenied()
         self.browse().check_access('read')
 
         result = {
