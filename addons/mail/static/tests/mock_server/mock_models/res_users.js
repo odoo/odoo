9cb7983a46f164718501669b7ef74182f3f5fd04
defl <defl@odoo.com>
2025-10-16 14:27:46 +0000

    [FIX] mail: ensure only one expander for fullComposer signature
    **Steps to reproduce:**
    - Go to the current user preferences
    - Go to its signature field
    - The current state should be something like:
    ```
    --
    Mitchell Admin
    ```
    - Apply bold formatting on the text
    - Save the changes
    - Refresh
    - Remove the bold formatting
    - Press enter between the two lines (at the end of `--`)
    - Save the changes
    - Go to the Contact app
    - Select any record
    - Go to its chatter
    - Click on `Send Message` and then the `Full Composer` expand button
    - Send the mail
    - In the chatter multiple `Read More` are added for the same signature
    
    (I think it can appears in multiple operations,
    this is just an example related to the <strong>
    element becoming <span> on removal)
    
    **Issue:**
    Playing with the html editor on the signature field can break the
    `tag_quote` flow due to the added elements.
    
    **Fix:**
    Explicitly add `"data-o-mail-quote"` to the signature container which
    is added when opening the `fullComposer`.
    It could also be an issue related to the html_editor but
    this seems cleaner to fix it.
    
    This issue was fixed in 19.0 in the same way by adding a common div
    around the signature and adding the same attribute.
    
    related: https://github.com/odoo/odoo/commit/6eb55c42158b08652c4c533bf56b5333c162bd3a
    
    opw-5149505
    
    X-original-commit: 34737d203c7f9d5a180d9f2b34a49b9cf1b38075

--- addons/mail/static/tests/mock_server/mock_models/res_users.js
+++ addons/mail/static/tests/mock_server/mock_models/res_users.js
@@ -62,6 +62,7 @@ export class ResUsers extends webModels.ResUsers {
                                 })
                             ),
                             "notification_type",
+                            "signature",
                         ],
                     })
                 ),
