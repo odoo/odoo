4f0af7ed2c361776e6eb8a4252531907ea324c95
Louis (loti) <loti@odoo.com>
2025-10-16 15:20:43 +0200

    [IMP] website_sale*: remove useless "add to cart" forms
    task-5116935
    
    Part-of: odoo/odoo#229931
    Related: odoo/enterprise#96294
    Signed-off-by: Louis Tinel (loti) <loti@odoo.com>

--- addons/website_sale/static/src/interactions/add_to_cart.js
+++ addons/website_sale/static/src/interactions/add_to_cart.js
@@ -1,6 +1,5 @@
 import { Interaction } from '@web/public/interaction';
 import { registry } from '@web/core/registry';
-import wSaleUtils from '@website_sale/js/website_sale_utils';
 
 export class AddToCart extends Interaction {
     static selector = '#add_to_cart, #buy_now, .o_wsale_products_page button[name="add_to_cart"]';
@@ -14,27 +13,31 @@ export class AddToCart extends Interaction {
      * @param {MouseEvent} ev
      */
     async addToCart(ev) {
-        const el = ev.currentTarget;
-        const form = wSaleUtils.getClosestProductForm(el);
+        const button = ev.currentTarget;
+
+        const productEl = button.closest('.js_product');
+        const productPageData = productEl ? {
+            quantity: parseFloat(productEl.querySelector('input[name="add_qty"]')?.value) || 1,
+            uomId: parseInt(productEl.querySelector('input[name="uom_id"]:checked')?.value),
+            ptavs: this._getSelectedPtavs(productEl),
+            productCustomAttributeValues: this._getCustomPtavValues(productEl),
+            noVariantAttributeValues: this._getSelectedNoVariantPtavs(productEl),
+        } : {};
 
         const quantity = await this.waitFor(this.services['cart'].add({
-            productTemplateId: parseInt(el.dataset.productTemplateId),
-            productId: parseInt(el.dataset.productId),
-            isCombo: el.dataset.productType === 'combo',
-            quantity: parseFloat(form.querySelector('input[name="add_qty"]')?.value) || 1,
-            uomId: parseInt(form.querySelector('input[name="uom_id"]:checked')?.value),
-            ptavs: this._getSelectedPtavs(form),
-            productCustomAttributeValues: this._getCustomPtavValues(form),
-            noVariantAttributeValues: this._getSelectedNoVariantPtavs(form),
-            ...this._getOptionalParams(form),
+            productTemplateId: parseInt(button.dataset.productTemplateId),
+            productId: parseInt(button.dataset.productId),
+            isCombo: button.dataset.productType === 'combo',
+            ...productPageData,
+            ...this._getOptionalParams(productEl ?? button.closest('#products_grid')),
         }, {
-            isBuyNow: el.id === 'buy_now',
-            isConfigured: el.parentElement.id === 'add_to_cart_wrap',
-            showQuantity: el.dataset.showQuantity === 'True',
+            isBuyNow: button.id === 'buy_now',
+            isConfigured: button.parentElement.id === 'add_to_cart_wrap',
+            showQuantity: button.dataset.showQuantity === 'True',
         }));
 
         if (quantity > 0) {
-            el.dispatchEvent(new CustomEvent('product_added_to_cart', { bubbles: true }));
+            button.dispatchEvent(new CustomEvent('product_added_to_cart', { bubbles: true }));
         }
 
         return quantity;
@@ -43,38 +46,38 @@ export class AddToCart extends Interaction {
     /**
      * Hook to add optional params when adding a product to the cart.
      *
-     * @param {HTMLFormElement} form - The product form.
+     * @param {HTMLElement} el - The element containing the product.
      */
-    _getOptionalParams(form) {
+    _getOptionalParams(el) {
         return {};
     }
 
     /**
-     * Return the selected stored PTAVs in the provided form.
+     * Return the selected stored PTAVs in the provided element.
      *
-     * @param {HTMLFormElement} form - The product form.
+     * @param {HTMLElement} el - The element containing the product.
      *
      * @returns {Number[]} - The selected stored PTAVs, as a list of
      *     `product.template.attribute.value` ids.
      */
-    _getSelectedPtavs(form) {
-        const selectedPtavElements = form.querySelectorAll(
+    _getSelectedPtavs(el) {
+        const selectedPtavElements = el.querySelectorAll(
             'input.js_variant_change:not(.no_variant):checked, select.js_variant_change:not(.no_variant)'
         );
         return Array.from(selectedPtavElements).map(el => parseInt(el.value));
     }
 
     /**
-     * Return the custom PTAV values in the provided form.
+     * Return the custom PTAV values in the provided element.
      *
-     * @param {HTMLFormElement} form - The product form.
+     * @param {HTMLElement} el - The element containing the product.
      *
      * @returns {{id: number, value: string}[]} - An array of objects where each object contains:
      *     - `custom_product_template_attribute_value_id`: The ID of the custom PTAV.
      *     - `custom_value`: The value assigned to the custom PTAV.
      */
-    _getCustomPtavValues(form) {
-        const customPtavValueElements = form.querySelectorAll('.variant_custom_value');
+    _getCustomPtavValues(el) {
+        const customPtavValueElements = el.querySelectorAll('.variant_custom_value');
         return Array.from(customPtavValueElements).map(el => ({
             'custom_product_template_attribute_value_id': parseInt(
                 el.dataset.customProductTemplateAttributeValueId
@@ -84,15 +87,15 @@ export class AddToCart extends Interaction {
     }
 
     /**
-     * Return the selected non-stored PTAVs in the provided form.
+     * Return the selected non-stored PTAVs in the provided element.
      *
-     * @param {HTMLFormElement} form - The product form.
+     * @param {HTMLElement} el - The element containing the product.
      *
      * @returns {Number[]} - The selected non-stored PTAVs, as a list of
      *     `product.template.attribute.value` ids.
      */
-    _getSelectedNoVariantPtavs(form) {
-        const selectedNoVariantPtavElements = form.querySelectorAll(
+    _getSelectedNoVariantPtavs(el) {
+        const selectedNoVariantPtavElements = el.querySelectorAll(
             'input.no_variant.js_variant_change:checked, select.no_variant.js_variant_change'
         );
         return Array.from(selectedNoVariantPtavElements).map(el => parseInt(el.value));
