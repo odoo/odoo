44e688e0e084e0155c2236a4f5168ef75d3fa5db
Elier Ayala Bernal <elier@wclik.com>
2025-10-17 15:23:03 +0000

    [FIX] website_sale: TypeError in DynamicSnippetCategory when accessing SIZE_CONFIG[nodeData.size].span
    X-original-commit: ae67de387f8a88f542e8835b58c51c22aff940b3

--- addons/website_sale/static/src/snippets/s_dynamic_snippet_categories/dynamic_snippet_category.js
+++ addons/website_sale/static/src/snippets/s_dynamic_snippet_categories/dynamic_snippet_category.js
@@ -27,7 +27,7 @@ export class DynamicSnippetCategory extends DynamicSnippet {
         const colSpanTwo = colsCount !== 1 && (nodeData.size !== 'small' || colsCount === 5);
         // Pass custom data to the template.
         nodeData.customTemplateData = JSON.stringify({
-            size: SIZE_CONFIG[nodeData.size].span,
+            size: SIZE_CONFIG[nodeData.size]?.span,
             alignmentClass: ALIGNMENT_CLASSES_MAPPING[nodeData.alignment],
             buttonText: nodeData.button,
             colSpanTwo: colSpanTwo,
