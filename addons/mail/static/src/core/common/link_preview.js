776b75309ee4d04c3136561c9a1b6b2cd6254d27
Alex Kühn <aku@odoo.com>
2025-10-17 17:33:19 +0200

    [REF] mail: simplify props to LinkPreview component
    The component was named LinkPreview but is actually linked quite
    heavily to message link preview, i.e. the instance of link preview on
    a mail message, as shown by the many props that are deduced from
    message link preview object.
    
    This commit simplifies the component by passing message link preview
    rather than link preview, so that now only 1 prop is provided.
    
    closes odoo/odoo#232148
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/core/common/link_preview.js
+++ addons/mail/static/src/core/common/link_preview.js
@@ -7,16 +7,12 @@ import { useService } from "@web/core/utils/hooks";
 
 /**
  * @typedef {Object} Props
- * @property {import("models").LinkPreview} linkPreview
- * @property {import("models").Message} [message]
- * @property {Boolean} [gifPaused]
- * @property {function} [delete] Function bound to the delete button
- * @property {function} [deleteAll] Function bound to the delete all button
+ * @property {import("models").MessageLinkPreview} messageLinkPreview
  * @extends {Component<Props, Env>}
  */
 export class LinkPreview extends Component {
     static template = "mail.LinkPreview";
-    static props = ["linkPreview", "delete?", "deleteAll?", "gifPaused?", "message?"];
+    static props = ["messageLinkPreview"];
     static components = { Gif };
 
     setup() {
@@ -34,12 +30,14 @@ export class LinkPreview extends Component {
         );
     }
 
+    get linkPreview() {
+        return this.props.messageLinkPreview.link_preview_id;
+    }
+
     onClick() {
         this.dialogService.add(LinkPreviewConfirmDelete, {
-            linkPreview: this.props.linkPreview,
-            delete: this.props.delete,
-            deleteAll: this.props.deleteAll,
             LinkPreview,
+            messageLinkPreview: this.props.messageLinkPreview,
         });
     }
 
