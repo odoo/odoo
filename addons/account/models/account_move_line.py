403f2f7da729a365068a8a5cb87b82bf1d89094e
h4818 <ayh@odoo.com>
2025-10-20 14:18:40 +0000

    [FIX] account: tax lock tags
    Despite the tax lock date, users are able to modify the tax report by adding tags.
    
    Steps to reproduce:
    Ensure the tax lock date is set
    1. Journal Items list view
    2. Edit one/many lines that
        - have a date before the tax lock date,
        - don't have a tax,
        - nor tax tags,
        - and is not a tax line.
    3. Add a new tax tag
    
    Issue:
    The tax tags are added and might impact a tax report, when you should have received a user error.
    
    Cause:
    The `write` function calls the `_check_tax_lock_date` which in turn only checks the existing line
    instead of the values given in the `write` parameters.
    Since the line has no existing tax tags the check does not fail.
    
    Solution:
    Call the tax lock check both before and after writing the move line.
    
    Task-5169152
    
    closes odoo/odoo#232924
    
    X-original-commit: 7abe2af42d24ca0cc324023b1809f2377394e78f
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: Habib Ayob (ayh) <ayh@odoo.com>

--- addons/account/models/account_move_line.py
+++ addons/account/models/account_move_line.py
@@ -1597,6 +1597,7 @@ class AccountMoveLine(models.Model):
         line_to_write = self
         vals = self._sanitize_vals(vals)
         matching2lines = None
+        tax_lock_check_ids = []
         for line in self:
             if not any(self.env['account.move']._field_will_change(line, vals, field_name) for field_name in vals):
                 line_to_write -= line
@@ -1611,7 +1612,7 @@ class AccountMoveLine(models.Model):
 
             # Check the tax lock date.
             if line.parent_state == 'posted' and any(self.env['account.move']._field_will_change(line, vals, field_name) for field_name in protected_fields['tax']):
-                line._check_tax_lock_date()
+                tax_lock_check_ids.append(line.id)
 
             # Check the reconciliation.
             if changing_fields := {
@@ -1630,6 +1631,8 @@ class AccountMoveLine(models.Model):
                     or line.matching_number and not all(reconciled_line in self for reconciled_line in matching2lines[line.matching_number])
                 ):
                     line._check_reconciliation()
+
+        self.browse(tax_lock_check_ids)._check_tax_lock_date()
 
         move_container = {'records': self.move_id}
         with self.move_id._check_balanced(move_container),\
@@ -1664,6 +1667,9 @@ class AccountMoveLine(models.Model):
             self.move_id._synchronize_business_models(['line_ids'])
             if any(field in vals for field in ['account_id', 'currency_id']):
                 self._check_constrains_account_id_journal_id()
+
+            # double check modified lines in case a tax field was changed on a line that didn't previously affect tax
+            self.browse(tax_lock_check_ids)._check_tax_lock_date()
 
             if not self.env.context.get('tracking_disable', False):
                 # Log changes to move lines on each move
