a098f7ed45918d3b9fe9521596c367d5aad635bb
Ruben Gomes <rugo@odoo.com>
2025-10-20 11:46:58 +0200

    [MOV] test_orm: constraint.js to test_translation
    Part-of: odoo/odoo#231381
    Signed-off-by: Krzysztof Magusiak (krma) <krma@odoo.com>

--- odoo/addons/test_orm/tests/test_ui.py
+++ odoo/addons/test_orm/tests/test_ui.py
@@ -1,5 +1,4 @@
 import odoo.tests
-from odoo.tools import mute_logger
 
 from odoo.addons.base.tests.common import HttpCaseWithUserDemo
 
@@ -17,23 +16,3 @@ class TestUi(HttpCaseWithUserDemo):
         # too small or there are too many items preceding it in the tests menu
         self.start_tour("/odoo/action-test_orm.action_discussions",
             'widget_x2many', login="admin", timeout=120)
-
-
-@odoo.tests.tagged('-at_install', 'post_install')
-class TestUiTranslation(odoo.tests.HttpCase):
-
-    @mute_logger('odoo.sql_db', 'odoo.http')
-    def test_01_sql_constraints(self):
-        # Raise an SQL constraint and test the message
-        self.env['res.lang']._activate_lang('fr_FR')
-        self.env.ref('base.module_test_orm')._update_translations(['fr_FR'])
-        constraint = self.env.ref('test_orm.constraint_test_orm_category_positive_color')
-        message = constraint.with_context(lang='fr_FR').message
-        self.assertEqual(message, "La couleur doit être une valeur positive !")
-
-        # TODO: make the test work with French translations. As the transaction
-        # is rollbacked at insert and a new cursor is opened, can not test that
-        # the message is translated (_load_module_terms is also) rollbacked.
-        # Test individually the external id and loading of translation.
-        self.start_tour("/odoo/action-test_orm.action_categories",
-            'sql_constaint', login="admin")
