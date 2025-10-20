8bc4be995d15602313e347f4b45083c26c98d432
Jairo Llopis <jairo@moduon.team>
2025-10-20 11:04:11 +0100

    [FIX] account_edi_ubl_cii*: update 'invoice_edi_format' when uninstalling
    Before this fix, if you uninstalled any of these modules and navigated to any partner
    that had a e-invoice format defined within, you'd have a traceback.
    
    @moduon MT-12168 OPW-5172861

--- addons/account_edi_ubl_cii/__init__.py
+++ addons/account_edi_ubl_cii/__init__.py
@@ -1,1 +1,12 @@
 from . import models
+
+
+def uninstall_hook(env):
+    env["res.partner"]._clear_removed_edi_formats(
+        "facturx",
+        "nlcius",
+        "ubl_a_nz",
+        "ubl_bis3",
+        "ubl_sg",
+        "xrechnung",
+    )
