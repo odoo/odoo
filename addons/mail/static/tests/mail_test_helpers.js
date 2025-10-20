39cd4a7c76a41dfa27133e929ed6dedfb5efd0ca
Sébastien (blse) <blse@odoo.com>
2025-10-20 14:19:52 +0200

    [FIX] mail: clean the "beforeunload" listener in tests
    Some tests trigger a call to the function `Rtc.joinCall` which adds a
    listener on `beforeunload` event that cancels the event. Some of these
    tests do not trigger a later call to `Rpc.clear`, thus the listener
    stays registered. This could mess with other unrelated tests that check
    their own handlers of `beforeunload` are correct.
    
    This commit adds a cleanup on the helper used by the problematic tests.
    
    task-5138313

--- addons/mail/static/tests/mail_test_helpers.js
+++ addons/mail/static/tests/mail_test_helpers.js
@@ -76,7 +76,7 @@ import { ResRole } from "./mock_server/mock_models/res_role";
 import { ResUsers } from "./mock_server/mock_models/res_users";
 import { ResUsersSettings } from "./mock_server/mock_models/res_users_settings";
 import { ResUsersSettingsVolumes } from "./mock_server/mock_models/res_users_settings_volumes";
-import { Network } from "@mail/discuss/call/common/rtc_service";
+import { Network, Rtc } from "@mail/discuss/call/common/rtc_service";
 import { UPDATE_EVENT } from "@mail/discuss/call/common/peer_to_peer";
 
 export * from "./mail_test_helpers_contains";
@@ -309,6 +309,12 @@ let discussAsTabId = 0;
  * }} [options]
  */
 export async function start(options) {
+    patchWithCleanup(Rtc.prototype, {
+        start() {
+            super.start();
+            after(() => this.clear());
+        },
+    });
     if (!MockServer.current) {
         await startServer();
     }
