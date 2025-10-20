65ab8935701c8dcafbcf84936b66929c2c6f258e
Josse Colpaert <jco@odoo.com>
2025-10-20 15:06:25 +0200

    [FIX] l10n_es_edi_tbai: regime_key 08 for Canarias
    We check that if the partner is from Canary Islands
    and we put a no sujeto per reglas de localizacion
    tax (so for services) we put the 08 key.
    
    opw-5099749
    
    closes odoo/odoo#232341
    
    Signed-off-by: Sven Führ (svfu) <svfu@odoo.com>

--- addons/l10n_es_edi_tbai/models/l10n_es_edi_tbai_document.py
+++ addons/l10n_es_edi_tbai/models/l10n_es_edi_tbai_document.py
@@ -439,6 +439,10 @@ class L10n_Es_Edi_TbaiDocument(models.Model):
             **self._get_regime_code_value(values['taxes'], values['is_simplified']),
             **self._get_refunded_values(values),
         }
+        # Regime key override for Canarias/Ceuta/Melilla and no_sujeto_loc
+        if values['partner'] and values['partner'].country_id.code == 'ES' and values['partner'].state_id.code in ('TF', 'GC', 'CE', 'ME'):
+            if any(t.l10n_es_type == 'no_sujeto_loc' for t in values['taxes']):
+                sale_values.update({'regime_key': ['08']})
 
         if not values['partner'] or not values['partner']._l10n_es_is_foreign() or values["is_simplified"]:
             sale_values.update(**self._get_importe_desglose_es_partner(values['base_lines'], values['is_refund']))
