a0bc0ba695b2be43c070c719492822cb7ebe93b4
Julien Mougenot <jum@odoo.com>
2025-10-21 12:55:22 +0200

    [FIX] web: tests - properly handle server errors
    Before this commit, errors dispatched from the mock server itself (e.g.
    when trying to fetch an unimplemented route), error messages were lost
    by the `rpc` module, which considers any actual server error as a
    "connection lost" error, and as such ignores the error message.
    
    To prevent this kind of issue, the 'mockedFetch' internal function has
    been adapted to parse errors in a "smarter" way, i.e. recognize which
    requests/responses are in the JSON-RPC format (based on their
    "Content-Type"), and as such should have the error thrown as-is, or
    wrapped inside a JSON-RPC object, that will itself be thrown by the
    `rpc` function.
    
    This change also means that request handlers not having a JSON content
    type do not have to be explicitly marked as "pure", and so tests have
    been simplified in these cases.

--- addons/web/static/tests/core/l10n/translation.test.js
+++ addons/web/static/tests/core/l10n/translation.test.js
@@ -59,14 +59,10 @@ test("url is given by the session", async () => {
     patchWithCleanup(session, {
         translationURL: "/get_translations",
     });
-    onRpc(
-        "/get_translations/*",
-        function (request) {
-            expect(request.url).toInclude("/get_translations/");
-            return this.loadTranslations();
-        },
-        { pure: true }
-    );
+    onRpc("/get_translations/*", function (request) {
+        expect(request.url).toInclude("/get_translations/");
+        return this.loadTranslations();
+    });
     await makeMockEnv();
 });
 
