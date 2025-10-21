6ae1a1aef2f543a59fe83ad4c3894d27af7a3974
Levi Siuzdak (sile) <sile@odoo.com>
2025-10-21 14:05:13 +0000

    [FIX] account: ensure fixed taxes can be marshaled
    Versions
    --------
    - 18.0+
    
    Steps
    -----
    1. Create a fixed tax;
    2. create a sale order using only the fixed tax;
    3. read the sale order via XML-RPC.
    
    Issue
    -----
    TypeError: cannot marshal None unless allow_none is enabled
    
    Cause
    -----
    If only a fixed tax is used, the `_get_tax_totals_summary` method
    returns a dict where the `display_base_amount_currency` and
    `display_base_amount` values are `None`, leading to an error serializing
    the result.
    
    Solution
    --------
    As the result gets serialized by `OdooMarshaller(allow_none=False)`, we
    should use `False` instead of `None`.
    
    opw-5173477
    
    closes odoo/odoo#233279
    
    X-original-commit: 7cf308cad4c0749abfb4f2d969b861ae6f8a70cf
    Signed-off-by: Ali Alfie (alal) <alal@odoo.com>
    Signed-off-by: Levi Siuzdak <sile@odoo.com>

--- addons/account/models/account_tax.py
+++ addons/account/models/account_tax.py
@@ -2725,8 +2725,8 @@ class AccountTax(models.Model):
 
             # Compute the display base amounts.
             if set(involved_taxes.mapped('amount_type')) == {'fixed'}:
-                display_base_amount = None
-                display_base_amount_currency = None
+                display_base_amount = False
+                display_base_amount_currency = False
             elif set(involved_taxes.mapped('amount_type')) == {'division'} and all(involved_taxes.mapped('price_include')):
                 display_base_amount = 0.0
                 display_base_amount_currency = 0.0
@@ -2748,7 +2748,7 @@ class AccountTax(models.Model):
                 display_base_amount = values['base_amount']
                 display_base_amount_currency = values['base_amount_currency']
 
-            if display_base_amount_currency is not None:
+            if display_base_amount_currency is not False:
                 encountered_base_amounts.add(float_repr(display_base_amount_currency, currency.decimal_places))
 
             # Order of the subtotals.
