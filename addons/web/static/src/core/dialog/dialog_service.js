9729e685d73b52dcc8af17ed71b1b289e0087d23
Lucas Perais (lpe) <lpe@odoo.com>
2025-10-21 16:19:30 +0200

    [FIX] web: core: overlay: static overlays

--- addons/web/static/src/core/dialog/dialog_service.js
+++ addons/web/static/src/core/dialog/dialog_service.js
@@ -83,6 +83,7 @@ export const dialogService = {
                             document.body.classList.remove("modal-open");
                         }
                     },
+                    sequence: 0,
                     rootId: options.context?.root?.el?.getRootNode()?.host?.id,
                 }
             );
--- addons/web/static/src/core/overlay/overlay_container.js
+++ addons/web/static/src/core/overlay/overlay_container.js
@@ -1,6 +1,17 @@
-import { Component, onWillDestroy, useChildSubEnv, useEffect, useRef, useState } from "@odoo/owl";
+import {
+    Component,
+    onWillDestroy,
+    onWillRender,
+    toRaw,
+    useChildSubEnv,
+    useEffect,
+    useRef,
+    useState,
+    xml,
+} from "@odoo/owl";
 import { sortBy } from "@web/core/utils/arrays";
 import { ErrorHandler } from "@web/core/utils/components";
+import { sprintf } from "../utils/strings";
 
 const OVERLAY_ITEMS = [];
 export const OVERLAY_SYMBOL = Symbol("Overlay");
@@ -46,12 +57,32 @@ class OverlayItem extends Component {
     }
 }
 
+const STATIC_OVERLAYS = 10;
+const tmp = `
+    <t t-set="overlay" t-value="this.staticList[%(index)s]"/>
+    <ErrorHandler t-if="overlay and isVisible(overlay)" onError="(error) => this.handleError(overlay, error)">
+        <OverlayItem env="overlay.env" component="overlay.component" props="overlay.props"/>
+    </ErrorHandler>`;
+
+const tmps = [];
+for (let i = 0; i < STATIC_OVERLAYS; i++) {
+    tmps.push(sprintf(tmp, { index: i }));
+}
+const STATICS_TEMPLATE = xml`<t>${tmps.join("\n")}</t>`;
+
 export class OverlayContainer extends Component {
     static template = "web.OverlayContainer";
     static components = { ErrorHandler, OverlayItem };
     static props = { overlays: Object };
 
     setup() {
+        this.staticTemplate = STATICS_TEMPLATE;
+        this.static = useState({ list: new Array(STATIC_OVERLAYS) });
+
+        onWillRender(() => {
+            this.computeOverlays(this._sortedOverlays);
+        });
+
         this.root = useRef("root");
         this.state = useState({ rootEl: null });
         useEffect(
@@ -62,7 +93,44 @@ export class OverlayContainer extends Component {
         );
     }
 
-    get sortedOverlays() {
+    computeOverlays(overlayList) {
+        const prevMap = this.map || {};
+        this.map = {};
+        this.sortedOverlays = [];
+        for (const o of overlayList) {
+            this.map[o.id] = o;
+        }
+
+        const staticList = toRaw(this.static.list);
+        const freeIndeces = [];
+        const staticMap = {};
+        for (let i = staticList.length - 1; i >= 0; i--) {
+            const o = staticList[i];
+            if (!o) {
+                freeIndeces.push(i);
+            } else if (o.id in prevMap && !(o.id in this.map)) {
+                staticList[i] = undefined;
+                freeIndeces.push(i);
+            } else {
+                staticMap[o.id] = true;
+            }
+        }
+        for (const id in this.map) {
+            if (id in staticMap) {
+                continue;
+            }
+            const o = this.map[id];
+            if (freeIndeces.length && o.sequence === 0) {
+                const i = freeIndeces.pop();
+                staticList[i] = o;
+            } else {
+                this.sortedOverlays.push(o);
+            }
+        }
+        this.staticList = this.static.list;
+    }
+
+    get _sortedOverlays() {
         return sortBy(Object.values(this.props.overlays), (overlay) => overlay.sequence);
     }
 
