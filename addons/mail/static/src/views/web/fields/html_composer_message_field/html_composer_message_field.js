6c4f46b1a1c024496bc1193e99066dc159f211fc
Zelong Lin <zel@odoo.com>
2025-10-21 11:11:56 +0200

    [REF] mail: rename MentionPlugin
    Rename MentionPlugin to MailFullComposerSuggestionPlugin to better
    reflect its purpose in the full composer context.
    
    This gives a chance to free the "mention" id for a more generic mention
    plugin that would be introduced later.
    
    closes odoo/odoo#232457
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/views/web/fields/html_composer_message_field/html_composer_message_field.js
+++ addons/mail/static/src/views/web/fields/html_composer_message_field/html_composer_message_field.js
@@ -3,7 +3,7 @@ import { isEmpty } from "@html_editor/utils/dom_info";
 import { registry } from "@web/core/registry";
 import { useBus } from "@web/core/utils/hooks";
 import { HtmlMailField, htmlMailField } from "../html_mail_field/html_mail_field";
-import { MentionPlugin } from "./mention_plugin";
+import { MailFullComposerSuggestionPlugin } from "./mail_full_composer_suggestion_plugin";
 import { ContentExpandablePlugin } from "./content_expandable_plugin";
 import { fillEmpty } from "@html_editor/utils/dom";
 import { markup } from "@odoo/owl";
@@ -39,7 +39,7 @@ export class HtmlComposerMessageField extends HtmlMailField {
 
     getConfig() {
         const config = super.getConfig(...arguments);
-        config.Plugins = [...config.Plugins, MentionPlugin];
+        config.Plugins = [...config.Plugins, MailFullComposerSuggestionPlugin];
         if (this.props.record.data.composition_comment_option === "reply_all") {
             config.Plugins.push(ContentExpandablePlugin);
         }
