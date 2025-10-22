8e395117fb7610ba9b2d13ef88c7c2144ee4783d
Trinh Ngoc Hung <ngochung207@gmail.com>
2025-10-22 16:45:55 +0200

    [FIX] l10n_vn: Preserve user data when updating taxes in migration
    Problem
    The previous implementation used try_loading() which would reload the
    entire chart template, potentially overwriting user-customized
    configurations settings.
    
    Solution
    Replaced try_loading() with a more targeted approach usin _load_data for
    account.tax.group and account.tax
    
    closes odoo/odoo#229476
    
    X-original-commit: b2c1af2e60968ee745e0a1238602aafe12f486b0
    Signed-off-by: Thomas Becquevort (thbe) <thbe@odoo.com>

--- addons/l10n_vn/migrations/2.0.3/end-migrate_update_taxes.py
+++ addons/l10n_vn/migrations/2.0.3/end-migrate_update_taxes.py
@@ -4,4 +4,10 @@ from odoo import api, SUPERUSER_ID
 def migrate(cr, version):
     env = api.Environment(cr, SUPERUSER_ID, {})
     for company in env['res.company'].search([('chart_template', '=', 'vn')], order="parent_path"):
-        env['account.chart.template'].try_loading('vn', company, force_create=False)
+        ChartTemplate = env['account.chart.template'].with_company(company)
+        data = {
+            'account.tax.group': ChartTemplate._get_account_tax_group(company.chart_template),
+            'account.tax': ChartTemplate._get_account_tax(company.chart_template)
+        }
+        ChartTemplate._pre_reload_data(company, {}, data, force_create=False)
+        ChartTemplate._load_data(data)
