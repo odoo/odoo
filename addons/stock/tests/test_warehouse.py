cfc5954ed361c91bdb4f37b0e9169ecb981c9a82
David (dafr) <dafr@odoo.com>
2025-10-22 08:32:30 +0000

    [FIX] stock: Prevent global route duplicates on warehouse creation
    In _find_or_create_global_route, use the asked 'route_name' instead of the potentially modified name of `data_route`.
    This ensures that if '_find_or_create_global_route' is called with the exact same values a new route will not be re-created.
    
    OPW-5149842
    
    closes odoo/odoo#233231
    
    X-original-commit: 6020522bc0d02d456542fe5bd5a0203f02bdcb5a
    Signed-off-by: Quentin Wolfs (quwo) <quwo@odoo.com>
    Signed-off-by: David Fesquet (dafr) <dafr@odoo.com>

--- addons/stock/tests/test_warehouse.py
+++ addons/stock/tests/test_warehouse.py
@@ -882,3 +882,26 @@ class TestWarehouse(TestStockCommon):
         sequence.prefix += end_of_prefix
         self.warehouse_1.delivery_steps = 'pick_ship'
         self.assertTrue(sequence.prefix.endswith(end_of_prefix))
+
+    def test_modified_global_route(self):
+        """ Ensure that _find_or_create_global_route, if called multiple time, only creates the route once
+
+        If a global route was renamed, and has a company setup,
+          then the system would create multiple duplicates with the new name when creating a new warehouse;
+          one duplicate for each call _find_or_create_global_route.
+        """
+        company_2 = self.env["res.company"].create({"name": "Company 2"})
+
+        mto_route = self.warehouse_1.mto_pull_id.route_id
+        mto_route.rule_ids.unlink()  # Quick patch to be able to set a company on the route
+        mto_route.write({"name": "New Name (MTO)", "company_id": self.warehouse_1.company_id.id})
+
+        self.env["stock.warehouse"].with_company(company_2).create({"name": "Warehouse 2", "code": "2"})
+
+        route_sudo = self.env["stock.route"].sudo().with_context(active_test=False)
+        renamed_route_count = route_sudo.search_count([("name", "=", "New Name (MTO)")])
+        self.assertEqual(renamed_route_count, 1)
+
+        new_mto_route = route_sudo.search([("name", "=", "Replenish on Order (MTO)")])
+        self.assertEqual(len(new_mto_route), 1)
+        self.assertEqual(new_mto_route.company_id.id, company_2.id)
