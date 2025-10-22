fe4e0522a7140d1a018f2d95aecb04584fa48e15
Anh Thao Pham (pta) <pta@odoo.com>
2025-10-22 13:55:10 +0000

    [FIX] account: fix totals onchange with product and unit price
    Steps to reproduce:
    - Install Accounting
    
    - Create an invoice with an invoice line
    - Save the invoice
    
    - Change the unit price
    => The totals should change
    - Change the product
    => The unit price and the totals should change
    - Change the unit price again
    
    Issue:
    After this point, the totals don't change anymore, even if the unit
    price is changed several times.
    Only saving the form will adapt the totals correctly.
    
    Cause:
    When changing the unit price the first time, a "price_unit "key is
    added in the onchange values.
    When changing the product, a "product_id" key is added after the
    price key.
    Changing the product triggers an onchange of the price with the
    price of the new product.
    However, when the price is changed again, as the "price_unit" key
    already exists, it's reused and its position is still before
    "product_id" key even if it should be computed after.
    This order results in the use of the price of the second product
    instead of the one entered manually when computing the "tax_totals".
    
    Solution:
    If "product_id" and "price_unit" are the values of the onchange
    method, the list of values is reordered to make sure that "product_id"
    is computed first.
    
    opw-5012125
    
    closes odoo/odoo#239353
    
    X-original-commit: 3777c54df7d773ed648297eb97b8bd29cbf9ede2
    Signed-off-by: Hugo Poncelet (hupo) <hupo@odoo.com>
    Signed-off-by: Anh Thao Pham (pta) <pta@odoo.com>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -3794,6 +3794,18 @@ class AccountMove(models.Model):
         elif 'invoice_line_ids' in field_names:
             values = {key: val for key, val in values.items() if key != 'line_ids'}
             fields_spec = {key: val for key, val in fields_spec.items() if key != 'line_ids'}
+            # When product_id and price_unit are in values, values is reordered to make sure
+            # that product_id is before price_unit because product_id is triggering an onchange
+            # of price_unit that could override the one defined here if the product_id is set
+            # after price_unit
+            invoice_line_ids = values.get('invoice_line_ids')
+            for invoice_line_idx, invoice_line in enumerate(invoice_line_ids):
+                if (len(invoice_line) == 3 and invoice_line[0] == 1 and isinstance(invoice_line[2], dict) and
+                    'product_id' in invoice_line[2] and 'price_unit' in invoice_line[2]
+                ):
+                    if isinstance(invoice_line, tuple):
+                        invoice_line_ids[invoice_line_idx] = invoice_line = list(invoice_line)
+                    invoice_line[2] = dict(sorted(invoice_line[2].items(), key=lambda item: item[0] != 'product_id'))
         return super().onchange(values, field_names, fields_spec)
 
     # -------------------------------------------------------------------------
