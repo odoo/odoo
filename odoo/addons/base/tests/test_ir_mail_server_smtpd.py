8c25b2bbc0debd9493b13aaa2a0bbe5b797c8527
Julien Castiaux <juc@odoo.com>
2025-10-22 17:03:05 +0200

    [FIX] base: patch.dict want an object in trixie
    `patch.dict('odoo.tools.config.options', {new dict})` works until 3.12,
    starting 3.13 (debian trixie) it doesn't anymore.

--- odoo/addons/base/tests/test_ir_mail_server_smtpd.py
+++ odoo/addons/base/tests/test_ir_mail_server_smtpd.py
@@ -12,7 +12,7 @@ from unittest.mock import patch
 from socket import getaddrinfo  # keep a reference on the non-patched function
 
 from odoo.exceptions import UserError
-from odoo.tools import file_path, mute_logger
+from odoo.tools import config, file_path, mute_logger
 from .common import TransactionCaseWithUserDemo
 
 try:
@@ -62,7 +62,7 @@ class Certificate:
 # fail fast for timeout errors
 @patch('odoo.addons.base.models.ir_mail_server.SMTP_TIMEOUT', .1)
 # prevent the CLI from interfering with the tests
-@patch.dict('odoo.tools.config.options', {'smtp_server': ''})
+@patch.dict(config.options, {'smtp_server': ''})
 class TestIrMailServerSMTPD(TransactionCaseWithUserDemo):
     @classmethod
     def setUpClass(cls):
