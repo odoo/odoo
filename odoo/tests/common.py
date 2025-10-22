a37b1a30fddee7cf79ae980f6baa38f1d89eef21
Xavier Morel <xmo@odoo.com>
2025-10-22 07:43:38 +0000

    [IMP] odoo/tests: reporting & handling of chrome start issues
    If we can't find the devtools port file after 10 seconds we skip the
    test, however
    
    - this is an invisible error because the runbot does not surface
      skipped tests, so this specific variant should also have a warning
      (also the skip from not finding a chrome executable)
    - if chrome started but got stuck before it managed to create the port
      file we don't `stop` the chromebrowser as it never finished
      starting, however this means we leave a chrome running in the void
      a `user_data_dir` littering the machine / vm's tempdir
    
    So improve the handling of that bit. Also have chrome write its stderr
    to a file (in the `user_data_dir`) so we can log that out in case it
    contains useful information as to why chrome didn't finish booting.
    
    While at it backport the spawn changes from #206574 for consistency
    and simplicity: we don't need to test the platform for every single
    tour and the indirection makes updating the `Popen` parameters
    unnecessary complicated.
    
    closes odoo/odoo#233015
    
    X-original-commit: fd0302e559efef06035dbbbdaa1434013c2a2acc
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- odoo/tests/common.py
+++ odoo/tests/common.py
@@ -1378,17 +1378,37 @@ class ChromeBrowser:
 
     @property
     def executable(self):
-        return _find_executable()
+        try:
+            return _find_executable()
+        except Exception:
+            self._logger.warning('Chrome executable not found')
+            raise
 
     def _spawn_chrome(self, cmd):
-        # pylint: disable=subprocess-popen-preexec-fn
-        proc = subprocess.Popen(cmd, stderr=subprocess.DEVNULL, preexec_fn=_preexec)  # noqa: PLW1509
+        log_path = pathlib.Path(self.user_data_dir, 'err.log')
+        with log_path.open('wb') as log_file:
+            # pylint: disable=subprocess-popen-preexec-fn
+            proc = subprocess.Popen(cmd, stderr=log_file, preexec_fn=_preexec)  # noqa: PLW1509
+
         port_file = pathlib.Path(self.user_data_dir, 'DevToolsActivePort')
         for _ in range(CHECK_BROWSER_ITERATIONS):
             time.sleep(CHECK_BROWSER_SLEEP)
             if port_file.is_file() and port_file.stat().st_size > 5:
                 with port_file.open('r', encoding='utf-8') as f:
                     return proc, int(f.readline())
+
+        if proc.poll() is None:
+            proc.terminate()
+            try:
+                proc.wait(5)
+            except subprocess.TimeoutExpired:
+                proc.kill()
+                proc.wait()
+        self._logger.warning('Chrome headless failed to start:\n%s', log_path.read_text(encoding="utf-8"))
+        # since the chrome never started, it's not going to be `stop`-ed so we
+        # need to cleanup the directory here
+        shutil.rmtree(self.user_data_dir, ignore_errors=True)
+
         raise unittest.SkipTest(f'Failed to detect chrome devtools port after {BROWSER_WAIT :.1f}s.')
 
     def _chrome_start(
