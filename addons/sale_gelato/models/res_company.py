d17147c4550d20b137439fa3fa2222967639e00b
Thomas Becquevort (thbe) <thbe@odoo.com>
2025-10-23 13:15:01 +0100

    [FIX] *: api keys group managment
    *: account_peppol, pos_razorpay, sale_gelato
    
    Make sure all api keys fields are assigned the right groups so that users
    always have access to the field if they need to.
    
    task-5358002

--- addons/sale_gelato/models/res_company.py
+++ addons/sale_gelato/models/res_company.py
@@ -6,5 +6,5 @@ from odoo import fields, models
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    gelato_api_key = fields.Char(string="Gelato API Key")
-    gelato_webhook_secret = fields.Char(string="Gelato Webhook Secret")
+    gelato_api_key = fields.Char(string="Gelato API Key", groups="base.group_system")
+    gelato_webhook_secret = fields.Char(string="Gelato Webhook Secret", groups="base.group_system")
