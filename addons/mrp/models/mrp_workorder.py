0dc95945a0c29aa6daeb06bf94b512bbe0f0c3b5
mohannedah <admd@odoo.com>
2025-10-24 16:46:05 +0000

    [PERF] mrp_workorder: speed up button_finish
    - The `button_finish` method contained a variable intended to filter out moves
      whose `operation_id` matched the `operation_id` of the entire recordset of
      work orders passed to the function. However, this filtering was performed
      inside a loop iterating over all work orders, even though the result of the
      filtration did not depend on any single work order.
    
    Before this commit:
    - The filtration was executed repeatedly for each work order, despite being
      deterministic.
    - This unnecessary repetition caused performance degradation and multiple
      redundant updates to the `picked` field of the same moves, resulting in
      fake or redundant database writes.
    
    After this commit:
    - The filtration logic has been moved outside the iteration, ensuring that the
      update to the moves is performed only once, improving overall performance
      and preventing redundant updates.
    - The `end_all` method is now executed on the entire recordset of work orders
      at once, instead of being called individually for each iteration.
    
    The benchmark below is done on a recordset of workorders of size **500** and the number of moves returned from the filter were **100**. It set the picked field to be **True** for every workorder in the recordset, potentially triggering recomputation of some of the fields and doing more redundant SQL queries.
    
    opw-5092636
    
    | Scenario | Execution Time |
    | :--- | :--- |
    | **Before this Commit** | **Memory Error**
    | **After this Commit** | **22 seconds**
    
    X-original-commit: a1f7e30ec4d400287221e7c94da4e9bb7fe0580e

--- addons/mrp/models/mrp_workorder.py
+++ addons/mrp/models/mrp_workorder.py
@@ -688,20 +688,23 @@ class MrpWorkorder(models.Model):
     def button_finish(self):
         date_finished = fields.Datetime.now()
         all_vals_dict = defaultdict(lambda: self.env['mrp.workorder'])
-        for workorder in self:
-            if workorder.state in ('done', 'cancel'):
-                continue
-            moves = (self.move_raw_ids + self.production_id.move_byproduct_ids.filtered(lambda m: m.operation_id == self.operation_id))
-            for move in moves:
-                if not move.picked:
-                    if workorder.production_id.product_uom_id.is_zero(workorder.production_id.qty_producing):
-                        qty_available = workorder.production_id.product_qty
-                    else:
-                        qty_available = workorder.production_id.qty_producing
-                    new_qty = move.product_uom.round(qty_available * move.unit_factor)
-                    move._set_quantity_done(new_qty)
-            moves.picked = True
-            workorder.end_all()
+        workorders_to_end = self.filtered(lambda workorder: workorder.state not in ('done', 'cancel'))
+        operations = workorders_to_end.operation_id
+        moves_to_pick = workorders_to_end.move_raw_ids.filtered(lambda move: not move.picked)
+        moves_to_pick += workorders_to_end.production_id.move_byproduct_ids.filtered(lambda move: not move.picked and move.operation_id in operations)
+
+        for move in moves_to_pick:
+            production_id = move.raw_material_production_id or move.production_id
+            if production_id.product_uom_id.is_zero(production_id.qty_producing):
+                qty_available = production_id.product_qty
+            else:
+                qty_available = production_id.qty_producing
+            new_qty = move.product_uom.round(qty_available * move.unit_factor)
+            move._set_quantity_done(new_qty)
+
+        moves_to_pick.picked = True
+        workorders_to_end.end_all()
+        for workorder in workorders_to_end:
             vals = {
                 'qty_produced': workorder.qty_produced or workorder.qty_producing or workorder.qty_production,
                 'state': 'done',
