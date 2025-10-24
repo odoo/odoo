fd449f27dd9adba9f0dc85e76345613a4d3d82d3
Mehdi Outagant (MOU) <mou@odoo.com>
2025-10-24 17:20:18 +0000

    [FIX] website, test_website: make video upload "sanitize friendly"
    Steps to reproduce the current behaviour:
    
    - Update the DEMO user to be a website "restricted editor" and sales
    "admin" who cannot bypass HTML field sanitization.
    
    - As ADMIN, add a YouTube video to a product page > Save.
    
    - As DEMO, try to update the content on the product page > You cannot
    (a dialog informs you that you cannot edit the content because an admin
    edited it previously).
    
    Explanation:
    
    Starting from [1], an HTML field can be flagged as `sanitize_overridable`
    which allowed users with the `base.group_sanitize_override` group to
    skip the HTML field sanitize process.
    
    If such users added some content that is not considered "sanitize
    friendly" (e.g. YouTube iframe), a restricted user won't be allowed to
    add content in the fields, since the sanitizer will remove the original
    content from the DOM.
    
    For this case, the code from [2] added an implementation to consider the
    field as none editable and warn the user once he tries to update it.
    
    Implementation:
    
    The goal of this commit it to fix the current limitation for video
    upload that currently prevents non admin users to edit a website record
    once an admin adds a video on it...
    
    The idea of the fix is the following:
    
    - We already have a technical fallback when uploading a video to save
    the iframe `src` to an attribute: `data-oe-expression`.
    - The public widget is now destroying the video iframes so they are
    never saved in the DOM.
    - A non-lazy code will build the iframes immediately on page load.
    - The public widget can always create the iframes if they are not
    already created (for compatibility).
    
    [1]: cf844e3
    [2]: cb80c15
    
    task-3757205
    
    X-original-commit: 0740fa7ecc6a18375498af186fa40362edbeafdc

--- addons/website/static/src/interactions/video/media_video.edit.js
+++ addons/website/static/src/interactions/video/media_video.edit.js
@@ -1,8 +1,14 @@
 import { MediaVideo } from "./media_video";
 import { registry } from "@web/core/registry";
 
-registry
-    .category("public.interactions.edit")
-    .add("website.media_video", {
-        Interaction: MediaVideo,
-    });
+export const MediaVideoEdit = (I) => class extends I {
+    destroy() {
+        // Destroy video iframes so they are never saved in the DOM.
+        this.el?.replaceChildren();
+    }
+};
+
+registry.category("public.interactions.edit").add("website.media_video", {
+    Interaction: MediaVideo,
+    mixin: MediaVideoEdit,
+});
