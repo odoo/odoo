b933730be011da6bb65ec973a7a35a209bf240ca
Benoit Socias (bso) <bso@odoo.com>
2025-10-24 16:17:18 +0200

    [MOV] website_blog: adjust names and ids
    Adjust names and ids in the files moved by the previous commit.
    
    task-2449620

--- addons/website_blog/static/src/website_builder/blog_post_list_page_option_plugin.js
+++ addons/website_blog/static/src/website_builder/blog_post_list_page_option_plugin.js
@@ -3,20 +3,22 @@ import { registry } from "@web/core/registry";
 import { _t } from "@web/core/l10n/translation";
 import { BaseOptionComponent } from "@html_builder/core/utils";
 
-export class BlogPostPageOption extends BaseOptionComponent {
-    static template = "website_blog.blogPostPageOption";
+export class BlogPostListPageOption extends BaseOptionComponent {
+    static template = "website_blog.blogPostListPageOption";
     static selector = "main:has(#o_wblog_index_content)";
     static title = _t("Blogs Page");
     static groups = ["website.group_website_designer"];
     static editableOnly = false;
 }
 
-export class BlogPostPageOptionPlugin extends Plugin {
-    static id = "blogPostPageOption";
+export class BlogPostListPageOptionPlugin extends Plugin {
+    static id = "blogPostPageListOption";
     /** @type {import("plugins").WebsiteResources} */
     resources = {
-        builder_options: [BlogPostPageOption],
+        builder_options: [BlogPostListPageOption],
     };
 }
 
-registry.category("website-plugins").add(BlogPostPageOptionPlugin.id, BlogPostPageOptionPlugin);
+registry
+    .category("website-plugins")
+    .add(BlogPostListPageOptionPlugin.id, BlogPostListPageOptionPlugin);
