24c3748c704607b96c222f68e01860b245f309a9
joseph <josv@odoo.com>
2025-10-24 09:25:49 +0000

    [FIX] purchase_stock: keep the smart link PO <-> RO
    The link between purchase order and a repair order break at when the PO is confirmed.
    
    Steps to reproduce:
    -------------------
    * Install the Repair and Purchase modules
    * Activate multi-steps routes
    * Unarchive the MTO route
    * Create a product with the MTO route enabled
    * Create a Repair
    * On the Repair Order, in part add:
    	- type : ADD
    	- product : mto product
    * Save the RO
    * Go to Purchase Order
    * Confirm the PO
    -> Issue Smart link between PO and RO broken.
    
    Observation:
    ------------
    The smart link is defined on:
    RO -> PO:
    https://github.com/odoo/odoo/blob/a2be8182010613c6f92f59e686a2fbf066cc6b68/addons/purchase_repair/models/repair_order.py#L15-L17 PO -> RO:
    https://github.com/odoo/odoo/blob/a2be8182010613c6f92f59e686a2fbf066cc6b68/addons/purchase_repair/models/purchase_order.py#L15-L17
    
    When we confirm the PO, from the picking information it will create new moves:
    https://github.com/odoo/odoo/blob/2c87f3b2b397f268f0e50cb73cd81de992ddd42e/addons/purchase_stock/models/purchase_order.py#L293-L298
    
    To create those stock moves, we go into_create_stock_moves where, for each POL, we will generate their values and erase the smart link:
    https://github.com/odoo/odoo/blob/a2be8182010613c6f92f59e686a2fbf066cc6b68/addons/purchase_stock/models/purchase_order_line.py#L362-L365
    
    Origin:
    ------------
    In this commit https://github.com/odoo/odoo/commit/9d98c43581e2579f43b35541b43264866dede5a5:
    "`created_purchase_line_id` is cleared after confirming the RFQ. This
    allows to merge more in `_merge_moves`."
    This breaks the link between PO <-> RO to maybe merge the move in the future.
    
    This issue is not present in 19.0 since it was solve in this commit :
    https://github.com/odoo/odoo/commit/2713876dbc70d3984e584a9037a2206dcda4e84a
    
    About the fix:
    -------------------
    The root cause of this issue remains ambiguous despite the analysis. Therefore, in the interest of stability and caution, we opted to implement the fix in a safer location.
    
    opw-5121816
    
    X-original-commit: a453c266b8f86c35c6ba086887b60d97de428873

--- addons/purchase_repair/models/purchase_order.py
+++ addons/purchase_repair/models/purchase_order.py
@@ -10,11 +10,17 @@ class PurchaseOrder(models.Model):
     @api.depends('order_line.move_dest_ids.repair_id')
     def _compute_repair_count(self):
         for purchase in self:
-            purchase.repair_count = len(purchase.order_line.move_dest_ids.repair_id)
+            purchase.repair_count = len(purchase._get_linked_repair_orders())
+
+    def _get_linked_repair_orders(self):
+        return (
+            self.order_line.move_dest_ids.repair_id |
+            self.order_line.move_ids.move_dest_ids.repair_id
+        )
 
     def action_view_repair_orders(self):
         self.ensure_one()
-        repair_ids = self.order_line.move_dest_ids.repair_id
+        repair_ids = self._get_linked_repair_orders()
         action = {
             'type': 'ir.actions.act_window',
             'res_model': 'repair.order',
