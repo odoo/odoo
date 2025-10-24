b24e2b2e18e7a6f981dbe885cfb567c1275ecec7
anko-odoo <anko@odoo.com>
2025-10-24 13:33:36 +0000

    [FIX] sale_gelato: truncate too long address fields
    Additional check to ensure all address fields required by Gelato is added.
    Address fields are trimmed to fit the Gelato character limit for shipping
    address fields.
    
    opw-5207430
    
    X-original-commit: da7618b1630e328fd48c74df0b8f96bde09d9471

--- addons/sale_gelato/models/res_partner.py
+++ addons/sale_gelato/models/res_partner.py
@@ -9,15 +9,19 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     def _gelato_prepare_address_payload(self):
+        """Trim address fields according to maximum length allowed by Gelato."""
         first_name, last_name = payment_utils.split_partner_name(self.name)
+        address_2 = self.street2 or ''
+        if remaining_address := self.street[35:]:
+            address_2 = remaining_address + ' ' + address_2
         return {
-            'companyName': self.commercial_company_name or '',
-            'firstName': first_name or last_name,  # Gelato require a first name.
-            'lastName': last_name,
-            'addressLine1': self.street,
-            'addressLine2': self.street2 or '',
+            'companyName': (self.commercial_company_name or '')[:60],
+            'firstName': (first_name or last_name)[:25],  # Gelato require a first name.
+            'lastName': last_name[:25],
+            'addressLine1': self.street[:35],
+            'addressLine2': address_2[:35],
             'state': self.state_id.code,
-            'city': self.city,
+            'city': self.city[:30],
             'postCode': self.zip,
             'country': self.country_id.code,
             'email': self.email,
