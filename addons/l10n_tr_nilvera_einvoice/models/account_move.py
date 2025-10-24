3b2688a96d48286269f28df9a8d72d1bdb4973b5
Khumam Alzagim <alkh@odoo.com>
2025-10-24 11:53:55 +0000

    [FIX] l10n_tr_nilvera_einvoice: group out moves by company on sync
    The sync process previously used the wrong company when synchronizing
    invoices. This fix groups invoices by their respective companies and
    performs the sync for each company individually.
    
    task-5207338
    
    closes odoo/odoo#235016
    
    X-original-commit: 1d2896925fbc40814892211e113ef7fc9419bef1
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>
    Signed-off-by: Khumam Alzagim (alkh) <alkh@odoo.com>

--- addons/l10n_tr_nilvera_einvoice/models/account_move.py
+++ addons/l10n_tr_nilvera_einvoice/models/account_move.py
@@ -169,33 +169,34 @@ class AccountMove(models.Model):
         )
 
     def _l10n_tr_nilvera_get_submitted_document_status(self):
-        with _get_nilvera_client(self.env.company) as client:
-            for invoice in self:
-                invoice_channel = invoice.partner_id.l10n_tr_nilvera_customer_status
-                document_category = invoice._l10n_tr_get_document_category(invoice_channel)
-                if not document_category or not invoice_channel:
-                    continue
+        for company, invoices in self.grouped("company_id").items():
+            with _get_nilvera_client(company) as client:
+                for invoice in invoices:
+                    invoice_channel = invoice.partner_id.l10n_tr_nilvera_customer_status
+                    document_category = invoice._l10n_tr_get_document_category(invoice_channel)
+                    if not document_category or not invoice_channel:
+                        continue
 
-                response = client.request(
-                    "GET",
-                    f"/{invoice_channel}/{quote(document_category)}/{invoice.l10n_tr_nilvera_uuid}/Status",
-                )
+                    response = client.request(
+                        "GET",
+                        f"/{invoice_channel}/{quote(document_category)}/{invoice.l10n_tr_nilvera_uuid}/Status",
+                    )
 
-                nilvera_status = response.get('InvoiceStatus', {}).get('Code') or response.get('StatusCode')
-                if nilvera_status in dict(invoice._fields['l10n_tr_nilvera_send_status'].selection):
-                    invoice.l10n_tr_nilvera_send_status = nilvera_status
-                    if nilvera_status == 'error':
-                        invoice.message_post(
-                            body=Markup(
-                                "%s<br/>%s - %s<br/>"
-                            ) % (
-                                _("The invoice couldn't be sent to the recipient."),
-                                response.get('InvoiceStatus', {}).get('Description') or response.get('StatusDetail'),
-                                response.get('InvoiceStatus', {}).get('DetailDescription') or response.get('ReportStatus'),
+                    nilvera_status = response.get('InvoiceStatus', {}).get('Code') or response.get('StatusCode')
+                    if nilvera_status in dict(invoice._fields['l10n_tr_nilvera_send_status'].selection):
+                        invoice.l10n_tr_nilvera_send_status = nilvera_status
+                        if nilvera_status == 'error':
+                            invoice.message_post(
+                                body=Markup(
+                                    "%s<br/>%s - %s<br/>"
+                                ) % (
+                                    _("The invoice couldn't be sent to the recipient."),
+                                    response.get('InvoiceStatus', {}).get('Description') or response.get('StatusDetail'),
+                                    response.get('InvoiceStatus', {}).get('DetailDescription') or response.get('ReportStatus'),
+                                )
                             )
-                        )
-                else:
-                    invoice.message_post(body=_("The invoice status couldn't be retrieved from Nilvera."))
+                    else:
+                        invoice.message_post(body=_("The invoice status couldn't be retrieved from Nilvera."))
 
     def _l10n_tr_nilvera_get_documents(self, invoice_channel="einvoice", document_category="Purchase", journal_type="in_invoice"):
         with _get_nilvera_client(self.env.company) as client:
