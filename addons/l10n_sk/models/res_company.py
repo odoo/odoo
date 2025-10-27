d6263aa0e7db7fa73a8d01618db6d02fa9fcb160
Ai Lin Zuccarello (aizu) <aizu@odoo.com>
2025-10-27 18:54:18 +0100

    [IMP] l10n_sk: P&L + Balance Sheet
    Adding the Slovakian P&L and Balance Sheet reports, as well as the combined version under Annual Statements with the xml for form UZPODv14.
    
    odoo/enterprise/pull/97156
    
    task-5105102

--- addons/l10n_sk/models/res_company.py
+++ addons/l10n_sk/models/res_company.py
@@ -1,6 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import fields, models, api, _
+from odoo.exceptions import UserError
 
 
 class ResCompany(models.Model):
@@ -8,6 +9,13 @@ class ResCompany(models.Model):
 
     trade_registry = fields.Char()
     income_tax_id = fields.Char(string="Income Tax ID")
+    l10n_sk_nace_code = fields.Char(string="SK NACE Code")
+
+    @api.constrains('l10n_sk_nace_code')
+    def _check_l10n_sk_nace_code(self):
+        for company in self:
+            if company.l10n_sk_nace_code and len(company.l10n_sk_nace_code) != 5:
+                raise UserError(_("Please make sure that the copmany's SK NACE Code has 5 digits."))
 
 
 class BaseDocumentLayout(models.TransientModel):
