8e6e156f9d112a9034ab29c8502517dc8624d471
Julien Mougenot <jum@odoo.com>
2025-10-27 09:30:08 +0100

    [REF] *: remove unnecessary 'describe' calls
    This commit removes test files using a single 'describe' call to wrap
    its tests, with a name being either redundant with the test file itself
    or is too misleading.
    
    Regular expression used to find isolated suites:
    ```js
    /^\s*import.*(?:\n(?!describe\()(?!test\().*)+\ndescribe\("[\w\s.]+",.*(?:\n(?!describe\().*)+\n\}\);\n(?!\n)/
    ```
    
    closes odoo/odoo#233155
    
    Related: odoo/enterprise#98137
    Signed-off-by: Michaël Mattiello (mcm) <mcm@odoo.com>
    Signed-off-by: Julien Mougenot (jum) <jum@odoo.com>

--- addons/point_of_sale/static/tests/unit/render_service.test.js
+++ addons/point_of_sale/static/tests/unit/render_service.test.js
@@ -1,53 +1,51 @@
-import { describe, expect, getFixture, test } from "@odoo/hoot";
+import { expect, getFixture, test } from "@odoo/hoot";
 import { mockFetch } from "@odoo/hoot-mock";
 import { Component, xml } from "@odoo/owl";
-import { registry } from "@web/core/registry";
+import { htmlToCanvas, renderService } from "@point_of_sale/app/services/render_service";
 import { clearRegistry, mountWithCleanup, patchTranslations } from "@web/../tests/web_test_helpers";
-import { renderService, htmlToCanvas } from "@point_of_sale/app/services/render_service";
+import { registry } from "@web/core/registry";
 
-describe("RenderService", () => {
-    test("test the render service", async () => {
-        class ComponentToBeRendered extends Component {
-            static props = ["name"];
-            static template = xml`
+test("test the render service", async () => {
+    class ComponentToBeRendered extends Component {
+        static props = ["name"];
+        static template = xml`
                 <div> It's me, <t t-esc="props.name" />! </div>
             `;
-        }
-        clearRegistry(registry.category("services"));
-        clearRegistry(registry.category("main_components"));
-        registry.category("services").add("render", renderService);
+    }
+    clearRegistry(registry.category("services"));
+    clearRegistry(registry.category("main_components"));
+    registry.category("services").add("render", renderService);
 
-        patchTranslations(); // this is needed because we are not loading the localization service
-        const comp = await mountWithCleanup("none");
-        const renderedComp = await comp.env.services.render.toHtml(ComponentToBeRendered, {
-            name: "Mario",
-        });
-        expect(renderedComp).toHaveOuterHTML("<div> It's me, Mario! </div>");
+    patchTranslations(); // this is needed because we are not loading the localization service
+    const comp = await mountWithCleanup("none");
+    const renderedComp = await comp.env.services.render.toHtml(ComponentToBeRendered, {
+        name: "Mario",
     });
+    expect(renderedComp).toHaveOuterHTML("<div> It's me, Mario! </div>");
+});
 
-    test("htmlToCanvas", async () => {
-        // htmlToCanvas fetches some fonts useless for the test, we mock it to avoid warnings
-        mockFetch(() => "");
-        const target = getFixture();
-        const node = document.createElement("div");
-        node.classList.add("render-container");
-        target.appendChild(node);
+test("htmlToCanvas", async () => {
+    // htmlToCanvas fetches some fonts useless for the test, we mock it to avoid warnings
+    mockFetch(() => "");
+    const target = getFixture();
+    const node = document.createElement("div");
+    node.classList.add("render-container");
+    target.appendChild(node);
 
-        const asciiChars = Array.from({ length: 256 }, (_, i) => String.fromCharCode(i)).join("");
-        node.textContent = asciiChars;
+    const asciiChars = Array.from({ length: 256 }, (_, i) => String.fromCharCode(i)).join("");
+    node.textContent = asciiChars;
 
-        let canvas = null;
-        try {
-            canvas = await htmlToCanvas(node, { addClass: "pos-receipt-print" });
-        } catch (error) {
-            // htmlToCanvas create an <img> by setting a svg to its src attribute
-            // if this fails, an Event of type "error" is thrown
-            if (error.constructor.name !== "Event") {
-                throw error;
-            }
+    let canvas = null;
+    try {
+        canvas = await htmlToCanvas(node, { addClass: "pos-receipt-print" });
+    } catch (error) {
+        // htmlToCanvas create an <img> by setting a svg to its src attribute
+        // if this fails, an Event of type "error" is thrown
+        if (error.constructor.name !== "Event") {
+            throw error;
         }
-        expect(canvas).not.toBe(null, {
-            message: "htmlToCanvas should work with all ascii characters",
-        });
+    }
+    expect(canvas).not.toBe(null, {
+        message: "htmlToCanvas should work with all ascii characters",
     });
 });
