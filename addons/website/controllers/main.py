ed41f78b05c6806d3d6c996e525e84be02afc0e7
Mathieu Walravens <wama@odoo.com>
2025-10-27 15:29:07 +0100

    [FIX] website: use urllib's urlencode
    Werkzeug's url_encode does not have consistent behavior between
    versions, and gives three different result when running Odoo under
    Ubuntu Jammy, Ubuntu Noble or a virtual environment.
    
    After this commit:
    Use the standard library urllib, which has consistent behavior across
    versions.
    
    opw-5187047

--- addons/website/controllers/main.py
+++ addons/website/controllers/main.py
@@ -5,6 +5,7 @@ import os
 import logging
 import re
 import requests
+import urllib.parse
 import werkzeug.urls
 import werkzeug.utils
 import werkzeug.wrappers
@@ -66,9 +67,9 @@ class QueryURL:
                     paths[key] = "%s" % value
             elif value:
                 if isinstance(value, (list, set)):
-                    fragments.append(werkzeug.urls.url_encode([(key, item) for item in value]))
+                    fragments.append(urllib.parse.urlencode([(key, item) for item in value]))
                 else:
-                    fragments.append(werkzeug.urls.url_encode([(key, value)]))
+                    fragments.append(urllib.parse.urlencode([(key, value)]))
         for key in path_args:
             value = paths.get(key)
             if value is not None:
