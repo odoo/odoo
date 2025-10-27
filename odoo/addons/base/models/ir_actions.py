7fe71e761f32b45555787ba55060d4ccef9b8a7b
Jigar Vaghela <jva@odoo.com>
2025-10-27 13:05:22 +0530

    fixup! [IMP] base*: Improve logging traceability by linking to Odoo records

--- odoo/addons/base/models/ir_actions.py
+++ odoo/addons/base/models/ir_actions.py
@@ -1116,9 +1116,9 @@ class IrActionsServer(models.Model):
         def log(message, level="info"):
             with self.pool.cursor() as cr:
                 cr.execute("""
-                    INSERT INTO ir_logging(create_date, create_uid, type, dbname, name, level, message, path, line, func)
-                    VALUES (NOW() at time zone 'UTC', %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                """, (self.env.uid, 'server', self.env.cr.dbname, __name__, level, message, "action", action.id, action.name))
+                    INSERT INTO ir_logging(create_date, create_uid, type, dbname, name, level, message, path, line, func, res_model, res_id)
+                    VALUES (NOW() at time zone 'UTC', %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                """, (self.env.uid, 'server', self.env.cr.dbname, __name__, level, message, "action", action.id, action.name, action.id, action._name))
 
         eval_context = super(IrActionsServer, self)._get_eval_context(action=action)
         model_name = action.model_id.sudo().model
