b117c8dca1edc0174b2c54b11f02cfa8d9604ff0
roto-odoo <roto@odoo.com>
2025-10-27 14:06:00 +0100

    [IMP] l10n_es_edi_verifactu: Alert banner resequence under veri*factu
    If Veri*Factu is enabled in production mode, display a banner if a user
    wants to resequence moves because that's illegal.
    
    task-5187419

---
+++ addons/l10n_es_edi_verifactu/wizard/account_resequence.py
@@ -0,0 +1,15 @@
+from odoo import fields, models
+
+
+class AccountResequenceWizard(models.TransientModel):
+    _inherit = 'account.resequence.wizard'
+
+    # Technical field to display (or not) banner in wizard
+    l10n_es_edi_verifactu_required_in_prod = fields.Boolean(compute='_compute_l10n_es_edi_verifactu_required_in_prod')
+
+    def _compute_l10n_es_edi_verifactu_required_in_prod(self):
+        for wizard in self:
+            wizard.l10n_es_edi_verifactu_required_in_prod = (
+                any(move.l10n_es_edi_verifactu_required for move in wizard.move_ids)
+                and any(not company.l10n_es_edi_verifactu_test_environment for company in wizard.move_ids.company_id)
+            )
