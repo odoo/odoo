0fae57c16c943915d24f949957baf899357105b2
Pedram (PEBR) <pebr@odoo.com>
2025-10-27 11:00:04 +0000

    [FIX] point_of_sale, pos_hr: prevent gaps in session names
    Before this commit, if an issue occurred while posting the cash
    details, the session sequence would still increment even though
    the operation failed, leading to gaps in session names.
    
    With this commit, the sequence only increments when the operation
    succeeds, ensuring continuous session naming without gaps.
    
    opw-5180712
    
    closes odoo/odoo#233997
    
    X-original-commit: e12ce9098c8a518063a1937284376f3f09db50fa
    Related: odoo/enterprise#98596
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

---
+++ addons/pos_hr/tests/test_point_of_sale_flow.py
@@ -0,0 +1,30 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from unittest.mock import patch
+
+import odoo
+from odoo.addons.point_of_sale.tests.common import CommonPosTest
+from odoo.exceptions import UserError
+
+
+@odoo.tests.tagged('post_install', '-at_install')
+class TestPointOfSaleFlow(CommonPosTest):
+    def test_pos_hr_session_name_gap(self):
+        self.pos_config_usd.open_ui()
+        session = self.pos_config_usd.current_session_id
+        session.set_opening_control(0, None)
+        current_session_name = session.name
+        session.action_pos_session_closing_control()
+
+        self.pos_config_usd.open_ui()
+        session = self.pos_config_usd.current_session_id
+
+        def _message_post_patch(*_args, **_kwargs):
+            raise UserError('Test Error')
+
+        with patch.object(self.env.registry.models['pos.session'], "message_post", _message_post_patch):
+            with self.assertRaises(UserError):
+                session.set_opening_control(0, None)
+
+        session.set_opening_control(0, None)
+        self.assertEqual(int(session.name.split('/')[1]), int(current_session_name.split('/')[1]) + 1)
