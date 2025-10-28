f411cfbfbd9a671f9589d7aa908fd718b0a40a6c
Adnan Sabbir <adns@odoo.com>
2025-10-28 06:50:46 +0000

    [FIX] l10n_sa_edi_pos: skip forced zatca invoice for settlement due
    In PoS, when ZATCA is enabled, invoicing is enforced on
    all orders. However, this creates a problem for "settle due" payments
    for two reasons:
    
    1. "Settle due" orders do not contain any lines, so an invoice
       cannot be generated by standard Odoo(From 18.2 it has lines).
    2. The original order (which created the due amount) was already
       posted to ZATCA, so the settlement payment should not be sent again.
    
    This commit skips the mandatory invoice enforcement specifically for
    "settle due" payment orders. And sets to_invoice = false by default
    
    task-5144679
    
    closes odoo/odoo#243998
    
    closes odoo/odoo#244654
    
    X-original-commit: 77cfce802baa35449740f3badf322e561784e985
    Related: odoo/enterprise#104438
    Related: odoo/enterprise#104866
    Signed-off-by: Stéphane Vanmeerhaeghe (stva) <stva@odoo.com>
    Signed-off-by: Adnan Sabbir (adns) <adns@odoo.com>

---
+++ addons/l10n_sa_edi_pos/static/tests/tours/test_zatca_pos_invoice.js
@@ -0,0 +1,47 @@
+/* global posmodel */
+
+import * as Dialog from "@point_of_sale/../tests/generic_helpers/dialog_util";
+import * as PaymentScreen from "@point_of_sale/../tests/pos/tours/utils/payment_screen_util";
+import * as Chrome from "@point_of_sale/../tests/pos/tours/utils/chrome_util";
+import { registry } from "@web/core/registry";
+
+registry.category("web_tour.tours").add("ZATCA_invoice_not_mandatory_if_settlement", {
+    steps: () =>
+        [
+            Chrome.startPoS(),
+            Dialog.confirm("Open Register"),
+            {
+                content: "Set the pos_settle_due to True and open payment screen",
+                trigger: "body",
+                run: () => {
+                    posmodel.selectedOrder.is_settling_account = true;
+                    posmodel.navigate("PaymentScreen", { orderUuid: posmodel.selectedOrderUuid });
+                },
+            },
+            PaymentScreen.clickPartnerButton(),
+            PaymentScreen.clickCustomer("AAA Partner"),
+            PaymentScreen.isInvoiceButtonUnchecked(),
+        ].flat(),
+});
+
+registry.category("web_tour.tours").add("ZATCA_invoice_mandatory_if_not_settlement", {
+    steps: () =>
+        [
+            Chrome.startPoS(),
+            Dialog.confirm("Open Register"),
+            {
+                content: "Set the pos_settle_due to False and open payment screen",
+                trigger: "body",
+                run: () => {
+                    posmodel.selectedOrder.is_settling_account = false;
+                    posmodel.navigate("PaymentScreen", { orderUuid: posmodel.selectedOrderUuid });
+                },
+            },
+            PaymentScreen.clickPartnerButton(),
+            PaymentScreen.clickCustomer("AAA Partner"),
+            PaymentScreen.isInvoiceButtonChecked(),
+            // Try to uncheck it and verify it remains checked
+            PaymentScreen.clickInvoiceButton(),
+            PaymentScreen.isInvoiceButtonChecked(),
+        ].flat(),
+});
