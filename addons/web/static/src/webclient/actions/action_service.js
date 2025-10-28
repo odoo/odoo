52f71e112447c83444d20731876dc9259c7588d0
Aaron Bohy <aab@odoo.com>
2025-10-28 14:42:19 +0100

    [FIX] web: action service: push state directly when mounted
    By default, router.pushState batches the calls to the next tick,
    such that multiple consecutive calls do not create several entries
    in the history. In the action service, when a ControllerComponent
    is mounted (i.e. when the action, e.g. the kanban view, has been
    mounted into the DOM), we call pushState to update the router with
    the new current action.
    
    Before this commit, the router state was thus updated a tick after
    the action was mounted into the DOM, and a test was randomly
    failing because of that. The test highlighted the fact that the
    state of several, different actions could be mixed if those actions
    were run in the same tick, which could hardly occur in practice,
    but which is not that unlikely in a testing environment.
    
    This commit fixes that issue by using the `sync` option of pushState.
    When the ControllerComponent is mounted, it is safe to assume that
    its sub-components have already pushed their own state (mounted
    being called bottom-up). So calling pushState with `sync` true acts
    as "committing" the state of the new current action, thus ensuring
    that it can't be mixed with the state of another action.
    
    runbot error~233361

--- addons/web/static/src/webclient/actions/action_service.js
+++ addons/web/static/src/webclient/actions/action_service.js
@@ -1022,7 +1022,7 @@ export function makeActionManager(env, router = _router) {
                     };
 
                     controllerStack = nextStack; // the controller is mounted, commit the new stack
-                    pushState();
+                    pushState(controllerStack, { sync: true });
                     this.titleService.setParts({ action: controller.displayName });
                     browser.sessionStorage.setItem(
                         "current_action",
@@ -1844,7 +1844,7 @@ export function makeActionManager(env, router = _router) {
         return Object.assign(newState, pick(newState.actionStack.at(-1), ...stateKeys));
     }
 
-    function pushState(cStack = controllerStack) {
+    function pushState(cStack = controllerStack, options) {
         if (!cStack.length) {
             return;
         }
@@ -1853,7 +1853,7 @@ export function makeActionManager(env, router = _router) {
         browser.sessionStorage.setItem("current_state", JSON.stringify(newState));
 
         cStack.at(-1).state = newState;
-        router.pushState(newState, { replace: true });
+        router.pushState(newState, Object.assign({ replace: true }, options));
     }
     return {
         doAction,
