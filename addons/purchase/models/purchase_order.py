d2c152cf4edd7dae96929a8fd4865113fd8bea24
bve-odoo <bve@odoo.com>
2025-10-28 16:50:53 +0100

    [FIX] purchase: get currency of current order
    On the tax computation, when trying to compute the
    total tax, customer was having an error
    " Expected singleton: res.currency(1, 69) "
    
    As Odoo is trying to get the currency of all the
    records instead of the one in the current order
    
    opw-5177551

--- addons/purchase/models/purchase_order.py
+++ addons/purchase/models/purchase_order.py
@@ -249,7 +249,7 @@ class PurchaseOrder(models.Model):
                 company=order.company_id,
             )
             if order.currency_id != order.company_currency_id:
-                order.tax_totals['amount_total_cc'] = f"({formatLang(self.env, order.amount_total_cc, currency_obj=self.company_currency_id)})"
+                order.tax_totals['amount_total_cc'] = f"({formatLang(self.env, order.amount_total_cc, currency_obj=order.company_currency_id)})"
 
     @api.depends('company_id.account_fiscal_country_id', 'fiscal_position_id.country_id', 'fiscal_position_id.foreign_vat')
     def _compute_tax_country_id(self):
