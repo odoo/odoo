d2706e67666edbf750ce74a15c288c4a33059f69
Louis Travaux <lotr@odoo.com>
2025-10-28 12:59:47 +0000

    [IMP] hw_drivers: remove crypt library usage
    To ensure compatibility with python 3.13+, we updated the method
    to generate the rpi's password to avoid using the removed `crypt`
    lib.
    
    In addition, we ensure that Wi-Fi configuration is saved to the right
    path, nmcli saving path changed in latest versions. (see:
    https://github.com/raspberrypi/trixie-feedback/issues/3).
    
    Finally, we ensure compatibility with old `cryptography` versions,
    by using `not_valid_after_utc` or `not_valid_after` depending on
    on image version.
    
    closes odoo/odoo#234250
    
    X-original-commit: a3b6bff90745c5e339cc6baa93c62c405b962062
    Signed-off-by: Max Whale (mawh) <mawh@odoo.com>
    Signed-off-by: Louis Travaux (lotr) <lotr@odoo.com>

--- addons/iot_drivers/tools/helpers.py
+++ addons/iot_drivers/tools/helpers.py
@@ -39,9 +39,6 @@ from odoo.tools.misc import file_path
 lock = Lock()
 _logger = logging.getLogger(__name__)
 
-if IS_RPI:
-    import crypt
-
 
 class Orientation(Enum):
     """xrandr/wlr-randr screen orientation for kiosk mode"""
@@ -177,15 +174,13 @@ def save_conf_server(url, token, db_uuid, enterprise_code, db_name=None):
 
 
 def generate_password():
+    """Resets (pi) user password generating a new random one.
+
+    :return: The new generated password
     """
-    Generate an unique code to secure raspberry pi
-    """
-    alphabet = 'abcdefghijkmnpqrstuvwxyz23456789'
-    password = ''.join(secrets.choice(alphabet) for i in range(12))
+    password = secrets.token_urlsafe(16)
     try:
-        shadow_password = crypt.crypt(password, crypt.mksalt())
-        subprocess.run(('sudo', 'usermod', '-p', shadow_password, 'pi'), check=True)
-        subprocess.run(('sudo', 'cp', '/etc/shadow', '/root_bypass_ramdisks/etc/shadow'), check=True)
+        subprocess.run(['sudo', 'chpasswd'], input=f"pi:{password}", text=True, check=True)
         return password
     except subprocess.CalledProcessError as e:
         _logger.exception("Failed to generate password: %s", e.output)
