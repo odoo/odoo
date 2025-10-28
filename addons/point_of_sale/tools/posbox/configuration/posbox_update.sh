5773a809b440ffcadf343831aa69e682ec7fbbe7
Louis Travaux <lotr@odoo.com>
2025-10-28 12:59:47 +0000

    [IMP] hw_drivers: remove crypt library usage
    To ensure compatibility with python 3.13+, we updated the method
    to generate the rpi's password to avoid using the removed `crypt`
    lib.
    
    We also ensure that files moved between `point_of_sale/tools/posbox/`,
    `addons/iot_box_image/` and `setup/iot_box_builder` can still be
    found by system processes using symlinks.
    
    closes odoo/odoo#233676
    
    X-original-commit: f417f35bcc63ae3177a369e9a11d8f6f63b92a77
    Signed-off-by: Max Whale (mawh) <mawh@odoo.com>
    Signed-off-by: Louis Travaux (lotr) <lotr@odoo.com>

--- addons/point_of_sale/tools/posbox/configuration/posbox_update.sh
+++ addons/point_of_sale/tools/posbox/configuration/posbox_update.sh
@@ -13,6 +13,8 @@ fi
 echo "addons/point_of_sale/tools/posbox/overwrite_after_init/home/pi/odoo" >> .git/info/sparse-checkout
 echo "addons/iot_base" >> .git/info/sparse-checkout
 echo "addons/iot_drivers" >> .git/info/sparse-checkout
+echo "addons/iot_box_image/configuration" >> .git/info/sparse-checkout
+echo "setup/iot_box_builder/configuration" >> .git/info/sparse-checkout
 
 git fetch "${localremote}" "${localbranch}" --depth=1
 git reset "${localremote}"/"${localbranch}" --hard
@@ -32,6 +34,23 @@ fi
 if [ -d /home/pi/odoo/addons/iot_drivers ]; then
   # TODO: remove this when v18.0 is deprecated (hw_drivers/,hw_posbox_homepage/ -> iot_drivers/)
   sed -i 's|hw_drivers.*hw_posbox_homepage|iot_drivers|g' /home/pi/odoo.conf
+fi
+
+# we create a symlinks in case the image uses hardcoded paths (ramdisks.service for example)
+if [ -d /home/pi/odoo/addons/iot_box_image ]; then
+  # if we have the iot_box_image module, it means configuration files are not in point_of_sale anymore
+  mkdir -p /home/pi/odoo/addons/point_of_sale/tools/posbox
+  ln -sf /home/pi/odoo/addons/iot_box_image/configuration /home/pi/odoo/addons/point_of_sale/tools/posbox
+fi
+
+if [ -d /home/pi/odoo/setup/iot_box_builder ]; then
+  # if we have the iot_box_builder module, it means configuration files are not in point_of_sale anymore
+  # in case ramdisks.service points to point_of_sale, we create a symlink
+  mkdir -p /home/pi/odoo/addons/point_of_sale/tools/posbox
+  ln -sf /home/pi/odoo/setup/iot_box_builder/configuration /home/pi/odoo/addons/point_of_sale/tools/posbox
+  # in case ramdisks.service points to iot_box_image, we create a symlink
+  mkdir -p /home/pi/odoo/addons/iot_box_image
+  ln -sf /home/pi/odoo/setup/iot_box_builder/configuration /home/pi/odoo/addons/iot_box_image
 fi
 
 sudo service led-status start
