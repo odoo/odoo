f402549403006559454f9b01a01e113fd521cb40
Julien Mougenot <jum@odoo.com>
2025-10-28 15:58:54 +0100

    [REF] *: remove unnecessary 'describe' calls
    This commit removes test files using a single 'describe' call to wrap
    its tests, with a name being either redundant with the test file itself
    or is too misleading.
    
    closes odoo/odoo#233412
    
    X-original-commit: 3084c7f
    Related: odoo/enterprise#98254
    Signed-off-by: Michaël Mattiello (mcm) <mcm@odoo.com>
    Signed-off-by: Julien Mougenot (jum) <jum@odoo.com>

--- addons/pos_restaurant/static/tests/unit/components/actionpad_widget.test.js
+++ addons/pos_restaurant/static/tests/unit/components/actionpad_widget.test.js
@@ -1,4 +1,4 @@
-import { describe, expect, test } from "@odoo/hoot";
+import { expect, test } from "@odoo/hoot";
 import { getFilledOrder, setupPosEnv } from "@point_of_sale/../tests/unit/utils";
 import { mountWithCleanup } from "@web/../tests/web_test_helpers";
 import { ActionpadWidget } from "@point_of_sale/app/screens/product_screen/action_pad/action_pad";
@@ -6,44 +6,42 @@ import { definePosModels } from "@point_of_sale/../tests/unit/data/generate_mode
 
 definePosModels();
 
-describe("actionpad_widget", () => {
-    test("highlightPay", async () => {
-        const store = await setupPosEnv();
-        const order = await getFilledOrder(store);
-        const comp = await mountWithCleanup(ActionpadWidget, {
-            props: {
-                actionName: "Payment",
-                actionToTrigger: () => {},
-            },
-        });
-
-        expect(comp.highlightPay).toBe(false);
-        // simulating order send
-        order.updateLastOrderChange();
-        expect(comp.highlightPay).toBe(true);
-
-        // orderline qty change
-        order.lines[1].qty = 21;
-        expect(comp.highlightPay).toBe(false);
-        order.updateLastOrderChange();
-        expect(comp.highlightPay).toBe(true);
-
-        // orderline note update
-        order.lines[0].note = "Test Orderline Note";
-        expect(comp.highlightPay).toBe(false);
-        order.updateLastOrderChange();
-        expect(comp.highlightPay).toBe(true);
-
-        // general customer note
-        order.general_customer_note = "Test Order Customer Note";
-        expect(comp.highlightPay).toBe(false);
-        order.updateLastOrderChange();
-        expect(comp.highlightPay).toBe(true);
-
-        // internal note
-        order.internal_note = "Test Order Internal Note";
-        expect(comp.highlightPay).toBe(false);
-        order.updateLastOrderChange();
-        expect(comp.highlightPay).toBe(true);
+test("highlightPay", async () => {
+    const store = await setupPosEnv();
+    const order = await getFilledOrder(store);
+    const comp = await mountWithCleanup(ActionpadWidget, {
+        props: {
+            actionName: "Payment",
+            actionToTrigger: () => {},
+        },
     });
+
+    expect(comp.highlightPay).toBe(false);
+    // simulating order send
+    order.updateLastOrderChange();
+    expect(comp.highlightPay).toBe(true);
+
+    // orderline qty change
+    order.lines[1].qty = 21;
+    expect(comp.highlightPay).toBe(false);
+    order.updateLastOrderChange();
+    expect(comp.highlightPay).toBe(true);
+
+    // orderline note update
+    order.lines[0].note = "Test Orderline Note";
+    expect(comp.highlightPay).toBe(false);
+    order.updateLastOrderChange();
+    expect(comp.highlightPay).toBe(true);
+
+    // general customer note
+    order.general_customer_note = "Test Order Customer Note";
+    expect(comp.highlightPay).toBe(false);
+    order.updateLastOrderChange();
+    expect(comp.highlightPay).toBe(true);
+
+    // internal note
+    order.internal_note = "Test Order Internal Note";
+    expect(comp.highlightPay).toBe(false);
+    order.updateLastOrderChange();
+    expect(comp.highlightPay).toBe(true);
 });
