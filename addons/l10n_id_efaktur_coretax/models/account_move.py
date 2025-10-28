53d4cdee936031bf1c0531e676fbe6042b3a8c2f
kaju-odoo <kaju@odoo.com>
2025-10-28 13:23:44 +0700

    [IMP] l10n_id_efaktur_coretax: Update VAT calculation and add restriction

--- addons/l10n_id_efaktur_coretax/models/account_move.py
+++ addons/l10n_id_efaktur_coretax/models/account_move.py
@@ -185,6 +185,51 @@ class AccountMove(models.Model):
                 if digits:
                     move.l10n_id_coretax_add_info_08 = f"TD.005{digits[-2:]}"
 
+    def _validate_tax_groups(self):
+        err_messages = []
+        allowed_codes = {'01', '02', '03', '04', '05', '06', '09', '10'}
+        must_be_zero_codes = {'07', '08'}
+
+        for move in self:
+            kode = move.l10n_id_kode_transaksi
+            non_luxury_group = self.env['account.chart.template'].with_company(move.company_id.id).ref("l10n_id_tax_group_non_luxury_goods", raise_if_not_found=False)
+            luxury_group = self.env['account.chart.template'].with_company(move.company_id.id).ref("l10n_id_tax_group_luxury_goods", raise_if_not_found=False)
+            zero_group = self.env['account.chart.template'].with_company(move.company_id.id).ref("l10n_id_tax_group_0", raise_if_not_found=False)
+            exempt_group = self.env['account.chart.template'].with_company(move.company_id.id).ref("l10n_id_tax_group_exempt", raise_if_not_found=False)
+            stlg_group = self.env['account.chart.template'].with_company(move.company_id.id).ref("l10n_id_tax_group_stlg", raise_if_not_found=False)
+            product_lines = move.line_ids.filtered(lambda line: line.display_type == 'product')
+            all_taxes = product_lines.mapped('tax_ids')
+            tax_groups = set(all_taxes.mapped('tax_group_id'))
+
+            # Multiple tax groups check
+            if len([g for g in tax_groups if g not in {stlg_group}]) > 1:
+                err_messages.append(_("Invoice %s: can only have one tax group (excluding STLG).", move.name or ''))
+            if len([g for g in tax_groups if g == stlg_group]) > 1:
+                err_messages.append(_("Invoice %s: can only have one STLG group.", move.name or ''))
+
+            # Allowed codes (01-06, 09, 10)
+            if kode in allowed_codes:
+                for line in product_lines:
+                    line_tax_groups = set(line.tax_ids.mapped('tax_group_id'))
+                    if luxury_group and non_luxury_group and {luxury_group, non_luxury_group}.issubset(line_tax_groups):
+                        err_messages.append(_("Invoice %s: line '%s' contains both Luxury-Goods and Non-Luxury-Goods taxes.", move.name or '', line.product_id.display_name or ''))
+                    if non_luxury_group and stlg_group and {non_luxury_group, stlg_group}.issubset(line_tax_groups):
+                        err_messages.append(_("Invoice %s: line '%s' contains both Non-Luxury-Goods and STLG taxes.", move.name or '', line.product_id.display_name or ''))
+                    if stlg_group and stlg_group in line_tax_groups:
+                        if not (luxury_group and luxury_group in line_tax_groups):
+                            err_messages.append(_("Invoice %s: line '%s' has STLG tax but missing the required Luxury-Goods tax.", move.name or '', line.product_id.display_name or ''))
+                    for tax in line.tax_ids:
+                        if ((hasattr(tax, 'amount') and float(tax.amount) == 0.0) or (tax.tax_group_id in {zero_group, exempt_group})):
+                            err_messages.append(_("Invoice %s: transaction code %s does not allow 0%% (Zero-rated or Exempt) taxes.", move.name or '', kode))
+
+            # Must-be-zero codes (07-08)
+            elif kode in must_be_zero_codes:
+                for line in product_lines:
+                    for tax in line.tax_ids:
+                        if hasattr(tax, 'amount') and float(tax.amount) != 0.0:
+                            err_messages.append(_("Invoice %s: transaction code %s must always have tax amount 0%%.", move.name or '', kode))
+        return err_messages
+
     def download_efaktur(self):
         """OVERRIDE l10n_id_efaktur
 
@@ -232,8 +277,11 @@ class AccountMove(models.Model):
                 if not (record.l10n_id_coretax_add_info_08 and record.l10n_id_coretax_facility_info_08):
                     err_messages.append(_("Invoice %s doesn't contain the Additional info and Facility Stamp yet (Kode 08)", record.name))
 
+        # Check tax groups
+        err_messages.extend(self._validate_tax_groups())
+
         if err_messages:
-            err_messages = [_('Unable to download E-faktur fot he following reasons(s):')] + err_messages
+            err_messages = [_('Unable to download E-faktur for the following reason(s):')] + err_messages
             raise ValidationError('\n - '.join(err_messages))
 
         # All invoices in self have no documents; we can create a new one for them.
