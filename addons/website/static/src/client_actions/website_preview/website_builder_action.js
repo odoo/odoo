f80e69021401a69075cd0a73af0b5a73dd7d0cfc
Sébastien (blse) <blse@odoo.com>
2025-10-28 08:25:06 +0000

    [FIX] website: remove content of fallback iframe after load
    Since commit 7b19831e1c624b483008feb526ba773ec8b23009, an fallback
    iframe is shown behind the website preview to avoid flicker on
    navigation. Since commit 3036c7dc4720a88f2717b96a29d45d923eb6ec75, the
    preview for mobile has some transparency on its scrollbar. Thus the part
    of the fallback iframe behind the scrollbar when previewing mobile was
    slightly visible.
    
    This commit fixes it by removing the fallback iframe's content after the
    website has loaded (and the fallback is not needed anymore).
    
    Steps to reproduce:
    - On website, activate "Mobile preview"
    - Navigate to a page long enough to have a scrollbar
    - Navigate to another page long enough to have a scrollbar
    - Scroll a bit
    - Bug: The scrollbar of the fallback is slightly visible
    
    task-5212287
    
    closes odoo/odoo#238211
    
    X-original-commit: 5e4b7dadfb1522b83823c8b954f79895828ca188
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Sébastien Blondiau (blse) <blse@odoo.com>

--- addons/website/static/src/client_actions/website_preview/website_builder_action.js
+++ addons/website/static/src/client_actions/website_preview/website_builder_action.js
@@ -627,6 +627,7 @@ export class WebsiteBuilderClientAction extends Component {
                 );
 
                 this.addListeners(this.websiteContent.el.contentDocument);
+                this.iframefallback.el?.contentDocument.documentElement.replaceChildren();
                 resolve(this.websiteContent.el);
             };
         });
