21eaff49354e93c0c1c09871227ac21a3acd8a1e
Sébastien (blse) <blse@odoo.com>
2025-10-28 08:25:06 +0000

    [FIX] website: remove content of fallback iframe after load
    Since commit 7b19831e1c624b483008feb526ba773ec8b23009, an fallback
    iframe is shown behind the website preview to avoid flicker on
    navigation. Since commit 3036c7dc4720a88f2717b96a29d45d923eb6ec75, the
    preview for mobile has some transparency on its scrollbar. Thus the part
    of the fallback iframe behind the scrollbar when previewing mobile was
    slightly visible.
    
    This commit fixes it by removing the fallback iframe's content after the
    website has loaded (and the fallback is not needed anymore).
    
    Steps to reproduce:
    - On website, activate "Mobile preview"
    - Navigate to a page long enough to have a scrollbar
    - Navigate to another page long enough to have a scrollbar
    - Scroll a bit
    - Bug: The scrollbar of the fallback is slightly visible
    
    task-5212287
    
    X-original-commit: 56274f78dd741a643b7f13c7458ad075eb739f24

--- addons/website/static/src/client_actions/website_preview/website_preview.js
+++ addons/website/static/src/client_actions/website_preview/website_preview.js
@@ -489,6 +489,7 @@ export class WebsitePreview extends Component {
         this.iframe.el.contentDocument.addEventListener('keyup', ev => {
             this.iframe.el.dispatchEvent(new KeyboardEvent('keyup', ev));
         });
+        this.iframefallback.el?.contentDocument.documentElement.replaceChildren();
     }
 
     /**
