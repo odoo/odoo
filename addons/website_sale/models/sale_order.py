e074972ae15f157380498f85ddf7317bdd37a1e5
Levi Siuzdak (sile) <sile@odoo.com>
2025-10-28 11:02:42 +0000

    [FIX] website_sale: find single-value no-variant-attribute product line
    Versions
    --------
    - 18.0+
    
    Steps
    -----
    1. Have a product attribute with variant creation set to never;
    2. have product using only this attribute, with only a single value;
    3. open website editor on /shop;
    4. enable add-to-cart button & save;
    5. use button on the aforementioned product at least twice.
    
    Issue
    -----
    Each time the product gets added, a new line is created.
    
    Cause
    -----
    Because there's only a single value, the product configurator doesn't
    pop up by default when adding the product to the cart. Without the
    configurator pop-up, the `_cart_update` method doesn't receive any
    `no_variant_attribute_value_ids`, which are used find a matching line if
    the product template has any `no_variant` attribute.
    
    Solution
    --------
    Instead of checking `product.product_tmpl._has_no_variant_attributes()`,
    to see if we should attempt to match `no_variant_attribute_value_ids`,
    check if the added product has any `no_variant` attributes that have
    more than one value or are optional (multi-checkbox). If not, a
    `no_variant` product behaves the same as a single-variant product.
    
    opw-5093175
    
    X-original-commit: cb2d707420fdc44eafeaa78d71af08f893375e25

--- addons/website_sale/models/sale_order.py
+++ addons/website_sale/models/sale_order.py
@@ -14,8 +14,8 @@ from odoo.tools import float_is_zero
 
 from odoo.addons.website_sale.models.website import (
     FISCAL_POSITION_SESSION_CACHE_KEY,
-    PRICELIST_SESSION_CACHE_KEY,
     PRICELIST_SELECTED_SESSION_CACHE_KEY,
+    PRICELIST_SESSION_CACHE_KEY,
 )
 
 
@@ -390,7 +390,12 @@ class SaleOrder(models.Model):
         if not filtered_sol:
             return self.env['sale.order.line']
 
-        if product.product_tmpl_id._has_no_variant_attributes():
+        has_configurable_no_variant_attributes = any(
+            len(line.value_ids) > 1 or line.attribute_id.display_type == 'multi'
+            for line in product.attribute_line_ids
+            if line.attribute_id.create_variant == 'no_variant'
+        )
+        if has_configurable_no_variant_attributes:
             filtered_sol = filtered_sol.filtered(
                 lambda sol:
                     sol.product_no_variant_attribute_value_ids.ids == no_variant_attribute_value_ids
