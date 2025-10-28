b6cf5a72bc5fdaa3d3787b4308916f22e72f79fe
thha-odoo <thha@odoo.com>
2025-10-28 12:22:04 +0000

    [FIX] hr_holidays: fix carryover expiring days count
    To reproduce:
    =============
    - Create an accrual plan:
        - Carryover date: allocation
        - One level:
            - Accrues 2 days.
            - Accrual date: monthly on 1st of each month - Starts immediately on allocation start date - Carryover policy: all days carry over - Carried over days validity: 3 months.
    - Create an allocation that uses the above accrual plan on 23/09/2025:
        - Starts on 01/07/2024
    
    We should have 30 days in total with 24 days expiring on 01/10/2025 but we only have 22 days expiring on 01/10/2025.
    
    Problem:
    ========
    When `accrued_gain_time` of the accrual plan is 'start', in the `_process_accrual_plans` method, the property `expiring_days` is set when the first accrual still hasn't been added to the `number_of_days`.
    
    Solution:
    =========
    In the `_process_accrual_plans`, add the accrued days to the `number_of_days` before the `expiring_days` is set.
    
    opw-4963163
    
    X-original-commit: 2832a34cf2e8a88b37eeafd30582f55d3a934359

--- addons/hr_holidays/tests/common.py
+++ addons/hr_holidays/tests/common.py
@@ -2,12 +2,12 @@
 
 from datetime import datetime
 
-from odoo import Command
-from odoo.addons.mail.tests.common import mail_new_test_user
-from odoo.tests import common
+from odoo import Command, fields
+from odoo.tests import common, Form
 from odoo.tests.common import TransactionCase
 from odoo.fields import Datetime
 from odoo.addons.hr.tests.test_utils import get_admin_employee
+from odoo.addons.mail.tests.common import mail_new_test_user
 
 
 class TestHrHolidaysCommon(common.TransactionCase):
@@ -103,6 +103,32 @@ class TestHrHolidaysCommon(common.TransactionCase):
 
         cls.rd_dept.write({'manager_id': cls.employee_hruser_id})
         cls.hours_per_day = cls.employee_emp.resource_id.calendar_id.hours_per_day or 8
+
+    def assert_remaining_leaves_equal(self, leave_type, value, employee, date=None, digits=None):
+        allocation_data = leave_type.get_allocation_data(employee, date)
+        if not date:
+            date = fields.Date.today()
+        if digits:
+            self.assertAlmostEqual(allocation_data[employee][0][1]['remaining_leaves'], value,
+                digits, f"Remaining leaves for date '{date}' are incorrect.")
+        else:
+            self.assertEqual(allocation_data[employee][0][1]['remaining_leaves'],
+                value, f"Remaining leaves for date '{date}' are incorrect.")
+
+    def _create_form_test_accrual_allocation(self, leave_type, date_from, employee, accrual_plan, date_to=None, creator_user=None):
+        allocation = self.env['hr.leave.allocation']
+        if creator_user:
+            allocation = allocation.with_user(creator_user)
+        with Form(allocation, 'hr_holidays.hr_leave_allocation_view_form_manager') as form:
+            form.name = 'Test accrual allocation'
+            form.allocation_type = 'accrual'
+            form.accrual_plan_id = accrual_plan
+            form.employee_id = employee
+            form.holiday_status_id = leave_type
+            form.date_from = date_from
+            if date_to:
+                form.date_to = date_to
+        return form.record
 
 
 class TestHolidayContract(TransactionCase):
