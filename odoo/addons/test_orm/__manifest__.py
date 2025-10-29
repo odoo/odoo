bdbaa047e746af9400c7a3e7a411c83e6f30a5ba
Ruben Gomes <rugo@odoo.com>
2025-10-29 10:21:32 +0100

    [REF] test_company_checks

--- odoo/addons/test_orm/__manifest__.py
+++ odoo/addons/test_orm/__manifest__.py
@@ -22,7 +22,7 @@
         'security/test_attributes/ir.model.access.csv',
         'security/test_autovacuum/ir.model.access.csv',
         'security/test_check_access/ir.model.access.csv',
-        # 'security/test_company_checks/ir.model.access.csv',
+        'security/test_company_checks/ir.model.access.csv',
         # 'security/test_config_settings/ir.model.access.csv',
         # 'security/test_convert/ir.model.access.csv',
         # 'security/test_convert_env/ir.model.access.csv',
