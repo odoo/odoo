fb4ed976fb0c4c78a676dda9542fc8a2cdf3909b
Julien Mougenot <jum@odoo.com>
2025-10-29 12:18:17 +0100

    [REF] *: remove unnecessary 'describe' calls
    This commit removes test files using a single 'describe' call to wrap
    its tests, with a name being either redundant with the test file itself
    or is too misleading.
    
    X-original-commit: ed55f3d

--- addons/pos_hr/static/tests/unit/services/select_cashier_mixin.test.js
+++ addons/pos_hr/static/tests/unit/services/select_cashier_mixin.test.js
@@ -1,34 +1,32 @@
-import { test, expect, describe } from "@odoo/hoot";
+import { test, expect } from "@odoo/hoot";
 import { setupPosEnv } from "@point_of_sale/../tests/unit/utils";
 import { definePosModels } from "@point_of_sale/../tests/unit/data/generate_model_definitions";
 import { CashierSelector } from "@pos_hr/app/utils/select_cashier_mixin";
 
 definePosModels();
 
-describe("select_cashier_mixin", () => {
-    test("checkPin", async () => {
-        const store = await setupPosEnv();
-        store.resetCashier();
-        const selector = new CashierSelector(store, false, () => {});
-        const emp = store.models["hr.employee"].get(2);
-        const result = await selector.checkPin(emp, "1234");
-        expect(result).toBe(true);
-    });
+test("checkPin", async () => {
+    const store = await setupPosEnv();
+    store.resetCashier();
+    const selector = new CashierSelector(store, false, () => {});
+    const emp = store.models["hr.employee"].get(2);
+    const result = await selector.checkPin(emp, "1234");
+    expect(result).toBe(true);
+});
 
-    test("selectCashier", async () => {
-        const store = await setupPosEnv();
-        store.resetCashier();
-        const selector = new CashierSelector(store, false, () => {});
-        const emp = store.models["hr.employee"].get(2);
-        // with correct pin
-        const selected = await selector.selectCashier("1234", true);
-        expect(selected.id).toBe(emp.id);
-        expect(store.hasLoggedIn).toBe(true);
-        expect(store.getCashier().id).toBe(selected.id);
+test("selectCashier", async () => {
+    const store = await setupPosEnv();
+    store.resetCashier();
+    const selector = new CashierSelector(store, false, () => {});
+    const emp = store.models["hr.employee"].get(2);
+    // with correct pin
+    const selected = await selector.selectCashier("1234", true);
+    expect(selected.id).toBe(emp.id);
+    expect(store.hasLoggedIn).toBe(true);
+    expect(store.getCashier().id).toBe(selected.id);
 
-        // with wrong pin
-        store.resetCashier();
-        const result = await selector.selectCashier("wrongpin", true);
-        expect(result).toBeEmpty();
-    });
+    // with wrong pin
+    store.resetCashier();
+    const result = await selector.selectCashier("wrongpin", true);
+    expect(result).toBeEmpty();
 });
