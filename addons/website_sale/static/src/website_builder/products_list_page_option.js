680500362843048ea45f32e3003ac1772333c37f
FrancoisGe <fge@odoo.com>
2025-10-29 09:19:01 +0000

    [FIX] website_sale: remove bad applyTo
    In PR https://github.com/odoo/odoo/pull/230283, an unnecessary applyTo was added to ProductsListPageOption
    Because of this, the option is never visible.
    In this commit, we will therefore remove this applyT
    
    X-original-commit: d2178d30ec5c11d6db472fdd119cbbdb55d39b9a

--- addons/website_sale/static/src/website_builder/products_list_page_option.js
+++ addons/website_sale/static/src/website_builder/products_list_page_option.js
@@ -3,6 +3,19 @@ import { products_sort_mapping } from "@website_sale/website_builder/shared";
 
 export class ProductsListPageOption extends BaseOptionComponent {
     static template = "website_sale.ProductsListPageOption";
+<<<<<<< 189f0506f09249c5a7c2f7b7a5b02d9bd996014d
+||||||| a5c2f140db215765d628c9aa730bf13bf9cd2e6e
+    static selector = "#o_wsale_container";
+    static applyTo = "#o_wsale_container";
+    static title = _t("Products Page");
+    static groups = ["website.group_website_designer"];
+    static editableOnly = false;
+=======
+    static selector = "#o_wsale_container";
+    static title = _t("Products Page");
+    static groups = ["website.group_website_designer"];
+    static editableOnly = false;
+>>>>>>> 71ee7df12f1e181720cd9810c3fba7ea88fd9206
 
     setup() {
         super.setup();
