2c4ac173be24c0edc938898ce3dd315ec6a01fa0
Ruben Gomes <rugo@odoo.com>
2025-10-29 11:35:14 +0100

    [REF] test_read_group_override

--- odoo/addons/test_orm/__manifest__.py
+++ odoo/addons/test_orm/__manifest__.py
@@ -40,7 +40,7 @@
         'security/test_performance/ir.model.access.csv',
         # 'security/test_properties/ir.model.access.csv',
         'security/test_qweb_float/ir.model.access.csv',
-        # 'security/test_read_group_override/ir.model.access.csv',
+        'security/test_read_group_override/ir.model.access.csv',
         # 'security/test_read_group_private/ir.model.access.csv',
         # 'security/test_read_grouping_sets/ir.model.access.csv',
         # 'security/test_related_translation/ir.model.access.csv',
