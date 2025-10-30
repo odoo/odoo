30ef93d91db108c71aa2e5051fcf8a60cb206ec5
FrancoisGe <fge@odoo.com>
2025-10-30 16:33:38 +0100

    [FIX] website: fix bg color selector
    During the FW-port PR https://github.com/odoo/odoo/pull/230283,
    the background color selectors were mistakenly modified.
    This commit restores their correct values.

--- addons/website/static/src/builder/plugins/options/utils.js
+++ addons/website/static/src/builder/plugins/options/utils.js
@@ -1,7 +1,15 @@
 export const CARD_PARENT_HANDLERS =
     ".s_three_columns .row > div, .s_comparisons .row > div, .s_cards_grid .row > div, .s_cards_soft .row > div, .s_product_list .row > div, .s_newsletter_centered .row > div, .s_company_team_spotlight .row > div, .s_comparisons_horizontal .row > div, .s_company_team_grid .row > div, .s_company_team_card .row > div, .s_carousel_cards_item";
 
-export const BASE_ONLY_BG_IMAGE_SELECTOR = `.s_col_no_bgcolor, .s_col_no_bgcolor.row > div, .s_masonry_block .row > div, .s_color_blocks_2 .row > div, .s_image_gallery .row > div, .s_text_cover .row > .o_not_editable, [data-snippet] :not(.oe_structure) > .s_hr, ${CARD_PARENT_HANDLERS}, .s_website_form_cover .row > .o_not_editable, .s_bento_grid .row > div`;
+// To remove in master, move it on the Option
+export const ONLY_BG_COLOR_SELECTOR =
+    "section .row > div, .s_text_highlight, .s_mega_menu_thumbnails_footer, .s_hr, .s_cta_badge";
+export const ONLY_BG_COLOR_EXCLUDE = `.s_col_no_bgcolor, .s_col_no_bgcolor.row > div, .s_masonry_block .row > div, .s_color_blocks_2 .row > div, .s_image_gallery .row > div, .s_text_cover .row > .o_not_editable, [data-snippet] :not(.oe_structure) > .s_hr, ${CARD_PARENT_HANDLERS}, .s_website_form_cover .row > .o_not_editable, .s_bento_grid .row > div, .s_banner_categories .row > div`;
+
+export const BASE_ONLY_BG_IMAGE_SELECTOR =
+    ".s_tabs .oe_structure > *, footer .oe_structure > *:not(.o_footer_bottom_part)";
+export const ONLY_BG_IMAGE_SELECTOR = BASE_ONLY_BG_IMAGE_SELECTOR;
+export const ONLY_BG_IMAGE_EXCLUDE = "";
 
 export const BOTH_BG_COLOR_IMAGE_SELECTOR =
     "section, .carousel-item, .s_masonry_block .row > div, .s_color_blocks_2 .row > div, .parallax, .s_text_cover .row > .o_not_editable, .s_website_form_cover .row > .o_not_editable, .s_split_intro .row > .o_not_editable, .s_bento_grid .row > div, .s_banner_categories .row > div, .s_ecomm_categories_showcase_block";
