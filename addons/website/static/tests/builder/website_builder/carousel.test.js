4fccd95a7256ecd83d7572902a1fe298f0704df4
Sébastien (blse) <blse@odoo.com>
2025-10-30 15:09:31 +0100

    [IMP] website, website_payment: re-use real snippets in tests
    A few tests used copies of real snippet. This commit directly uses the
    snippets, to prevent mismatches between the tested copy and the real
    snippet. To do that, it leverages `setupWebsiteBuilderWithSnippet`.
    
    task-5215644
    
    closes odoo/odoo#237149
    
    Signed-off-by: Romaric Moyeuvre (romo) <romo@odoo.com>
    Signed-off-by: Sébastien Blondiau (blse) <blse@odoo.com>

--- addons/website/static/tests/builder/website_builder/carousel.test.js
+++ addons/website/static/tests/builder/website_builder/carousel.test.js
@@ -1,80 +1,14 @@
 import { expect, test } from "@odoo/hoot";
 import {
     defineWebsiteModels,
-    setupWebsiteBuilder,
+    setupWebsiteBuilderWithSnippet,
 } from "@website/../tests/builder/website_helpers";
 import { contains } from "@web/../tests/web_test_helpers";
 
 defineWebsiteModels();
 
-const defaultCarouselStyleSnippet = `
-    <section class="s_carousel_wrapper p-0" data-snippet="s_carousel" data-vcss="001">
-        <div id="slideshow_sample" class="s_carousel s_carousel_default carousel slide o_colored_level" data-bs-ride="carousel" data-bs-interval="10000">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img class="img img-fluid d-block mh-100 mw-100 mx-auto rounded object-fit-cover" src="/web/image/website.library_image_08" data-name="Image" data-index="0" alt=""/>
-                </div>
-                <div class="carousel-item">
-                    <img class="img img-fluid d-block mh-100 mw-100 mx-auto rounded object-fit-cover" src="/web/image/website.library_image_03" data-name="Image" data-index="1" alt=""/>
-                </div>
-                <div class="carousel-item">
-                    <img class="img img-fluid d-block mh-100 mw-100 mx-auto rounded object-fit-cover" src="/web/image/website.library_image_02" data-name="Image" data-index="2" alt=""/>
-                </div>
-            </div>
-            <div class="o_carousel_controllers">
-                <button class="carousel-control-prev o_not_editable" contenteditable="false" data-bs-target="#slideshow_sample" data-bs-slide="prev" aria-label="Previous" title="Previous">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"/>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#slideshow_sample" data-bs-slide-to="0" style="background-image: url(/web/image/website.library_image_08)" class="active" aria-label="Carousel indicator"/>
-                    <button type="button" style="background-image: url(/web/image/website.library_image_03)" data-bs-target="#slideshow_sample" data-bs-slide-to="1" aria-label="Carousel indicator"/>
-                    <button type="button" style="background-image: url(/web/image/website.library_image_02)" data-bs-target="#slideshow_sample" data-bs-slide-to="2" aria-label="Carousel indicator"/>
-                </div>
-                <button class="carousel-control-next o_not_editable" contenteditable="false" data-bs-target="#slideshow_sample" data-bs-slide="next" aria-label="Next" title="Next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"/>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
-        </div>
-    </section>`;
-
-const imageGalleryCarouselStyleSnippet = `
-    <section class="s_image_gallery o_slideshow pt24 pb24 s_image_gallery_controllers_outside s_image_gallery_controllers_outside_arrows_right s_image_gallery_indicators_dots s_image_gallery_arrows_default" data-snippet="s_image_gallery" data-vcss="002" data-columns="3">
-        <div class="o_container_small overflow-hidden">
-            <div id="slideshow_sample" class="carousel carousel-dark slide" data-bs-ride="carousel" data-bs-interval="10000">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img class="img img-fluid d-block mh-100 mw-100 mx-auto rounded object-fit-cover" src="/web/image/website.library_image_08" data-name="Image" data-index="0" alt=""/>
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img img-fluid d-block mh-100 mw-100 mx-auto rounded object-fit-cover" src="/web/image/website.library_image_03" data-name="Image" data-index="1" alt=""/>
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img img-fluid d-block mh-100 mw-100 mx-auto rounded object-fit-cover" src="/web/image/website.library_image_02" data-name="Image" data-index="2" alt=""/>
-                    </div>
-                </div>
-                <div class="o_carousel_controllers">
-                    <button class="carousel-control-prev o_not_editable" contenteditable="false" data-bs-target="#slideshow_sample" data-bs-slide="prev" aria-label="Previous" title="Previous">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"/>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#slideshow_sample" data-bs-slide-to="0" style="background-image: url(/web/image/website.library_image_08)" class="active" aria-label="Carousel indicator"/>
-                        <button type="button" style="background-image: url(/web/image/website.library_image_03)" data-bs-target="#slideshow_sample" data-bs-slide-to="1" aria-label="Carousel indicator"/>
-                        <button type="button" style="background-image: url(/web/image/website.library_image_02)" data-bs-target="#slideshow_sample" data-bs-slide-to="2" aria-label="Carousel indicator"/>
-                    </div>
-                    <button class="carousel-control-next o_not_editable" contenteditable="false" data-bs-target="#slideshow_sample" data-bs-slide="next" aria-label="Next" title="Next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"/>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
-            </div>
-        </div>
-    </section>`;
-
 test("Test Carousel Option (s_carousel)", async () => {
-    const { getEditableContent } = await setupWebsiteBuilder(defaultCarouselStyleSnippet);
+    const { getEditableContent } = await setupWebsiteBuilderWithSnippet("s_carousel");
     const carouselEl = getEditableContent().querySelector(".carousel");
     await contains(":iframe .carousel").click();
 
@@ -82,17 +16,17 @@ test("Test Carousel Option (s_carousel)", async () => {
 
     await contains(".hb-row[data-label='Transition'] button").click();
     await contains(".o-hb-select-dropdown-item:contains('None')").click();
-    expect(carouselEl).toHaveAttribute("data-bs-ride", "carousel");
+    expect(carouselEl).toHaveAttribute("data-bs-ride", "true");
     expect(carouselEl).toHaveAttribute("data-bs-interval", "10000");
 
     await contains(".hb-row[data-label='Transition'] button").click();
     await contains(".o-hb-select-dropdown-item:contains('Slide')").click();
-    expect(carouselEl).toHaveAttribute("data-bs-ride", "carousel");
+    expect(carouselEl).toHaveAttribute("data-bs-ride", "true");
     expect(carouselEl).toHaveAttribute("data-bs-interval", "10000");
 
     await contains(".hb-row[data-label='Transition'] button").click();
     await contains(".o-hb-select-dropdown-item:contains('Fade')").click();
-    expect(carouselEl).toHaveAttribute("data-bs-ride", "carousel");
+    expect(carouselEl).toHaveAttribute("data-bs-ride", "true");
     expect(carouselEl).toHaveAttribute("data-bs-interval", "10000");
 
     // Editing the Autoplay
@@ -131,7 +65,7 @@ test("Test Carousel Option (s_carousel)", async () => {
 });
 
 test("Test Carousel Option (s_image_gallery)", async () => {
-    const { getEditableContent } = await setupWebsiteBuilder(imageGalleryCarouselStyleSnippet);
+    const { getEditableContent } = await setupWebsiteBuilderWithSnippet("s_image_gallery");
     const carouselEl = getEditableContent().querySelector(".carousel");
     await contains(":iframe .carousel").click();
 
@@ -140,34 +74,34 @@ test("Test Carousel Option (s_image_gallery)", async () => {
     await contains(".hb-row[data-label='Transition'] button").click();
     await contains(".o-hb-select-dropdown-item:contains('None')").click();
     expect(carouselEl).toHaveAttribute("data-bs-ride", "carousel");
-    expect(carouselEl).toHaveAttribute("data-bs-interval", "10000");
+    expect(carouselEl).toHaveAttribute("data-bs-interval", "0");
 
     await contains(".hb-row[data-label='Transition'] button").click();
     await contains(".o-hb-select-dropdown-item:contains('Slide')").click();
     expect(carouselEl).toHaveAttribute("data-bs-ride", "carousel");
-    expect(carouselEl).toHaveAttribute("data-bs-interval", "10000");
+    expect(carouselEl).toHaveAttribute("data-bs-interval", "0");
 
     await contains(".hb-row[data-label='Transition'] button").click();
     await contains(".o-hb-select-dropdown-item:contains('Fade')").click();
     expect(carouselEl).toHaveAttribute("data-bs-ride", "carousel");
-    expect(carouselEl).toHaveAttribute("data-bs-interval", "10000");
+    expect(carouselEl).toHaveAttribute("data-bs-interval", "0");
 
     // Editing the Autoplay
 
     await contains(".hb-row[data-label='Autoplay'] button").click();
     await contains(".o-hb-select-dropdown-item:contains('Always')").click();
     expect(carouselEl).toHaveAttribute("data-bs-ride", "carousel");
-    expect(carouselEl).toHaveAttribute("data-bs-interval", "10000");
+    expect(carouselEl).toHaveAttribute("data-bs-interval", "0");
 
     await contains(".hb-row[data-label='Autoplay'] button").click();
     await contains(".o-hb-select-dropdown-item:contains('Never')").click();
     expect(carouselEl).toHaveAttribute("data-bs-ride", "false");
-    expect(carouselEl).toHaveAttribute("data-bs-interval", "10000");
+    expect(carouselEl).toHaveAttribute("data-bs-interval", "0");
 
     await contains(".hb-row[data-label='Autoplay'] button").click();
     await contains(".o-hb-select-dropdown-item:contains('After First Hover')").click();
     expect(carouselEl).toHaveAttribute("data-bs-ride", "true");
-    expect(carouselEl).toHaveAttribute("data-bs-interval", "10000");
+    expect(carouselEl).toHaveAttribute("data-bs-interval", "0");
 
     // Editing the Speed
 
