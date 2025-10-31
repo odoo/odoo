34a8f4064f3fe344ddcd8ea737a8bd0da9cf0582
jeanschoenlaub <jesc@odoo.com>
2025-10-31 18:06:47 +0000

    [IMP] purchase_stock: Suggest readability & performance
    Cleans up, with minimal logic change, the suggestion code.
    
    LOGIC CHANGES:
    - Remove the exclusion of interwarehouse transfers in monthly
    demand calculation (was useful for the "all warehouses option,
    which isn't available now that warehouse is selected on PO)
    
    PERF CHANGES:
    - Improved the perf of product compute suggested quantity and estimated price
    (using the profiler and a 2000 suggested DB, not much else could be improve).
    
    UI/UX CHANGES:
    - Add a non debounced kanban reload, the debounced version now only called
    on numeric input changes. (faster UX if toggling / dropdown  or filters).
    Slightly longer debounce for slow typers.
    - On selecting suggestion numerical inputs, selects all the text, mimicking
    standard odoo inputs (some user confused with cursor placement otherwise).
    - Re added back shopping cart icon to the suggested product.
    (I had misunderstood the spec)
    MISC:
    - Separate some JS function into utils.js to make controller leaner.
    (more useful in later commits)
    - Add back some of the tour testing that as removed here #227682
    (also add steps to check no suggest on PO that are not in draft)
    
    task#5114649
    
    Part-of: odoo/odoo#234044
    Signed-off-by: Steve Van Essche <svs@odoo.com>

--- addons/purchase/static/tests/tours/purchase_catalog.js
+++ addons/purchase/static/tests/tours/purchase_catalog.js
@@ -1,15 +1,11 @@
 import { addSectionFromProductCatalog } from "@account/js/tours/tour_utils";
-import { selectPOVendor } from "./tour_helper";
+import { purchaseForm } from "./tour_helper";
 import { registry } from "@web/core/registry";
 
 registry.category("web_tour.tours").add('test_add_section_from_product_catalog_on_purchase_order', {
     steps: () => [
-        {
-            content: "Create a new PO",
-            trigger: '.o_list_button_add',
-            run: 'click',
-        },
-        ...selectPOVendor("Test Vendor"),
+        ...purchaseForm.createNewPO(),
+        ...purchaseForm.selectVendor("Test Vendor"),
         ...addSectionFromProductCatalog(),
     ],
 });
