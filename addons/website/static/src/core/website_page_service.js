6c910850630c201fa5acc4c80f378e8672d226ba
Robin Lejeune (role) <role@odoo.com>
2025-11-03 16:54:24 +0100

    [IMP] html_builder, web, website: document services and registries
    We make use of existing `web` TypeScript interfaces and types (Services
    and GlobalRegistryCategories) to better document html_builder and
    website services and registry categories, as well as the interactions'
    ones.
    
    Backport of https://github.com/odoo/odoo/commit/1c12f840
    
    task-5249231
    
    Part-of: odoo/odoo#236065
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/website/static/src/core/website_page_service.js
+++ addons/website/static/src/core/website_page_service.js
@@ -2,7 +2,7 @@ import { jsToPyLocale } from "@web/core/l10n/utils";
 import { registry } from "@web/core/registry";
 import { user } from "@web/core/user";
 
-registry.category("services").add("website_page", {
+export const websitePageService = {
     start() {
         const htmlEl = document.querySelector("html");
         // TODO this is duplicated in website_service.js at least... to share
@@ -21,4 +21,6 @@ registry.category("services").add("website_page", {
             },
         };
     },
-});
+};
+
+registry.category("services").add("website_page", websitePageService);
