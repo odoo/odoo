d4f05fca62805834b19b70b2a357bf2f9d8801bd
Gauthier Wala (gawa) <gawa@odoo.com>
2025-11-03 16:57:43 +0000

    [FIX] account: re-put rounding where it should have been
    Also fixes the comments that were wrong.
    
    X-original-commit: 9a185706830e3d5c1ee6af186baf8faa2f6c4823

--- addons/account/tests/test_account_move_out_refund.py
+++ addons/account/tests/test_account_move_out_refund.py
@@ -590,6 +590,8 @@ class TestAccountMoveOutRefundOnchanges(AccountTestInvoicingCommon):
         })
 
     def test_out_refund_line_onchange_currency_1(self):
+        self.other_currency.rounding = 0.001
+
         move_form = Form(self.invoice)
         move_form.currency_id = self.other_currency
         move_form.save()
