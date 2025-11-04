674187c6dcf3eedfe7ef556ba4656c8a3b4aa24b
Akash Parmar <akap@odoo.com>
2025-11-04 18:15:32 +0530

    [IMP] mail: redirect from Configuration to Settings > Discuss
    Purpose of this commit:
    
    A new menu item `Settings` has been added under the Discuss app `Configuration`
    menu, which redirects users to Settings > General Settings > Discuss
    for easier access to Discuss related settings.
    
    Part of task-5227387
    
    closes odoo/odoo#234470
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/__manifest__.py
+++ addons/mail/__manifest__.py
@@ -102,7 +102,7 @@ For more specific needs, you may also assign custom-defined actions
         'data/mail_activity_type_data.xml',
         'data/security_notifications_templates.xml',
         'data/ir_cron_data.xml',
-        'data/ir_actions_client.xml',
+        'data/ir_actions_data.xml',
         'security/mail_security.xml',
         'security/ir.model.access.csv',
         'views/discuss_public_templates.xml',
