9bedd783a6fc6dc001e5b5f7630416db85dabee8
Mahdi Alijani (malj) <malj@odoo.com>
2025-11-04 10:00:58 +0000

    [FIX] mrp_account: failing test due to duration inverse change
    Issue:
    This is caused by changing the duration inverse in #233777.
    
    Cause:
    Before #233777, in duration inverse, only sum of `time_ids` was
    taken into account to calculate the `duration`.
    After that PR, `time_ids` with `get_working_duration`
    are also taken into account which cause this test fail.
    
    runbot-233742
    
    closes odoo/odoo#235127
    
    X-original-commit: 70ea4328f33755f476453853465cb8feb33076b4
    Signed-off-by: Tiffany Chang (tic) <tic@odoo.com>
    Signed-off-by: Mohammadmahdi Alijani (malj) <malj@odoo.com>

--- addons/mrp_account/tests/test_mrp_account.py
+++ addons/mrp_account/tests/test_mrp_account.py
@@ -819,7 +819,6 @@ class TestMrpAccountMove(TestStockValuationCommon):
         })
         mo.action_confirm()
         workorder = mo.workorder_ids
-        workorder.button_start()
         workorder.duration = 60
         self.assertEqual(workorder._cal_cost(), 20)
         # Simulate missing finished moves
