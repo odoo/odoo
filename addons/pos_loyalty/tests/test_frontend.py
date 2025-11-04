e7d0b9322ef83aea2276061f25dc093438769068
paan-odoo <paan@odoo.com>
2025-11-04 06:03:37 +0000

    [FIX] pos_loyalty: skip rewards with archived or invalid reward products
    Steps:
    
    - Install pos_loyalty.
    - Add a product to a loyalty reward’s reward product, or assign a product tag
      with no actual products as a reward product tag.
    - Archive/delete the reward product.
    - Open session.
    
    Issue:
    - A traceback appears when attempting to apply the affected reward, due to the
      archived/deleted or invalid reward product is being loaded in the POS.
    
    FIX:
    - Skip loading loyalty rewards whose reward product is archived/deleted or whose
      reward product tag contains no valid products.
    
    Task-5226654
    
    closes odoo/odoo#247126
    
    X-original-commit: 7cd82b529ed7154e31b65e1883d6b0a22386409e
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Anantkumar Parmar (paan) <paan@odoo.com>

--- addons/pos_loyalty/tests/test_frontend.py
+++ addons/pos_loyalty/tests/test_frontend.py
@@ -2299,8 +2299,8 @@ class TestUi(TestPointOfSaleHttpCommon):
             'reward_product_tag_id': free_product_tag.id,
         })
 
-        self.product_b.active = False
-        product_c.active = False
+        self.product_b.product_tmpl_id.active = False
+        product_c.product_tmpl_id.active = False
 
         self.start_tour(
             "/pos/ui/%d" % self.main_pos_config.id,
@@ -2308,7 +2308,7 @@ class TestUi(TestPointOfSaleHttpCommon):
             login="pos_user",
         )
 
-        product_c.active = True
+        product_c.product_tmpl_id.active = True
         self.start_tour(
             "/pos/ui/%d" % self.main_pos_config.id,
             "PosLoyaltyArchivedRewardProductsActive",
