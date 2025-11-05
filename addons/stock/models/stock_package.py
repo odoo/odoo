a5520b68a48efd2690dc48c3b4223c7a54d2fb32
Quentin Wolfs <quwo@odoo.com>
2025-11-05 17:02:59 +0000

    [FIX] stock{,_delivery}: properly compute weight with multi-level packs
    When using multi-level packages, when the weight of the package is
    computed, the base weight of the package type is only counted for the
    top-level package, not for the children packages.
    
    Task-5166120
    
    closes odoo/odoo#236522
    
    X-original-commit: ad2ebbc5491d816df13605b64ce0832af13b7b9c
    Signed-off-by: Steve Van Essche <svs@odoo.com>
    Signed-off-by: Quentin Wolfs (quwo) <quwo@odoo.com>

--- addons/stock/models/stock_package.py
+++ addons/stock/models/stock_package.py
@@ -434,8 +434,16 @@ class StockPackage(models.Model):
         res = {}
         if picking_id:
             package_weights = defaultdict(float)
+            # If we check the weight of an ongoing package, we may need to check its current child dest as well to known their own weight.
+            children_by_dest_pack, all_pack_ids = self._get_all_children_package_dest_ids()
+            base_weight_per_package_group = self.env['stock.package']._read_group(
+                domain=[('id', 'in', all_pack_ids)],
+                groupby=['id', 'package_type_id.base_weight']
+            )
+            base_weight_per_package = {pack.id: weight for pack, weight in base_weight_per_package_group}
+
             res_groups = self.env['stock.move.line']._read_group(
-                [('result_package_id', 'child_of', self.ids), ('product_id', '!=', False), ('picking_id', '=', picking_id)],
+                [('result_package_id', 'in', all_pack_ids), ('product_id', '!=', False), ('picking_id', '=', picking_id)],
                 ['result_package_id', 'product_id', 'product_uom_id', 'quantity'],
                 ['__count'],
             )
@@ -449,7 +457,11 @@ class StockPackage(models.Model):
             weight = package.package_type_id.base_weight or 0.0
             if picking_id:
                 res[package] = weight + package_weights[package.id]
+                for child_id in children_by_dest_pack.get(package, []):
+                    res[package] += base_weight_per_package.get(child_id, 0) + package_weights.get(child_id, 0)
             else:
+                # Take the base_weight of every contained package, so we include package only containing packages
+                weight += sum(package.all_children_package_ids.mapped(lambda p: p.package_type_id.base_weight))
                 for quant in package.contained_quant_ids:
                     weight += quant.quantity * quant.product_id.weight
                 res[package] = weight
