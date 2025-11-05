2f0d48b285bc3766d8f1db018f08d890d1dd1d4e
Didier (did) <did@odoo.com>
2025-11-05 14:18:55 +0100

    [IMP] mail,im_livechat: update models.d.ts

--- addons/mail/static/src/core/public_web/@types/models.d.ts
+++ addons/mail/static/src/core/public_web/@types/models.d.ts
@@ -1,12 +1,7 @@
 declare module "models" {
-    import { DiscussApp as DiscussAppClass } from "@mail/core/public_web/discuss_app/discuss_app_model";
-
-    export interface DiscussApp extends DiscussAppClass {}
-
     export interface Store {
         action_discuss_id: number|undefined;
         discuss: DiscussApp;
-        DiscussApp: StaticMailRecord<DiscussApp, typeof DiscussAppClass>;
     }
     export interface Thread {
         askLeaveConfirmation: (body: string) => Promise<void>;
@@ -16,9 +11,5 @@ declare module "models" {
         setActiveURL: () => void;
         setAsDiscussThread: (pushState: boolean) => void;
         unpin: () => Promise<void>;
-    }
-
-    export interface Models {
-        DiscussApp: DiscussApp;
     }
 }
