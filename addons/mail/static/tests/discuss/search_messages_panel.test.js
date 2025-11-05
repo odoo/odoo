ff0b0586e9e77c92fb88193258b885397c448818
Alex Kühn <aku@odoo.com>
2025-11-05 11:57:13 +0000

    [FIX] mail: can search and jump to messages in mailboxes
    Before this commit, we could not jump to messages in mailboxes.
    
    This happens because the `useMessageHighlight()` hook prevented
    jumping to messages in thread that were not their origin thread.
    
    This commit fixes the issue by removing this limitation specifically
    for mailboxes.
    
    opw-4948798
    opw-5087102
    
    X-original-commit: 23ab6546b3476f00587cba709068012369f9480d

--- addons/mail/static/tests/discuss/search_messages_panel.test.js
+++ addons/mail/static/tests/discuss/search_messages_panel.test.js
@@ -173,6 +173,8 @@ test("Search a message in history (desktop)", async () => {
     await insertText(".o_searchview_input", "message");
     triggerHotkey("Enter");
     await contains(".o-mail-SearchMessagesPanel .o-mail-Message");
+    await click(".o-mail-SearchMessagesPanel .o-mail-MessageCard-jump");
+    await contains(".o-mail-Thread .o-mail-Message.o-highlighted");
 });
 
 test.tags("mobile");
