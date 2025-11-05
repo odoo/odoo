17c84361899f67a4b6ff4cb997dfee1cfb98f000
Mehdi Outagant (MOU) <mou@odoo.com>
2025-11-05 09:59:53 +0100

    [FIX] website, test_website: make video upload "sanitize friendly"
    Steps to reproduce the current behaviour:
    
    - Update the DEMO user to be a website "restricted editor" and sales
    "admin" who cannot bypass HTML field sanitization.
    
    - As ADMIN, add a YouTube video to a product page > Save.
    
    - As DEMO, try to update the content on the product page > You cannot
    (a dialog informs you that you cannot edit the content because an admin
    edited it previously).
    
    Explanation:
    
    Starting from [1], an HTML field can be flagged as `sanitize_overridable`
    which allowed users with the `base.group_sanitize_override` group to
    skip the HTML field sanitize process.
    
    If such users added some content that is not considered "sanitize
    friendly" (e.g. YouTube iframe), a restricted user won't be allowed to
    add content in the fields, since the sanitizer will remove the original
    content from the DOM.
    
    For this case, the code from [2] added an implementation to consider the
    field as none editable and warn the user once he tries to update it.
    
    Implementation:
    
    The goal of this commit it to fix the current limitation for video
    upload that currently prevents non admin users to edit a website record
    once an admin adds a video on it...
    
    The idea of the fix is the following:
    
    - We already have a technical fallback when uploading a video to save
    the iframe `src` to an attribute: `data-oe-expression`.
    - The public widget is now destroying the video iframes so they are
    never saved in the DOM.
    - A non-lazy code will build the iframes immediately on page load.
    - The public widget can always create the iframes if they are not
    already created (for compatibility).
    
    [1]: cf844e3
    [2]: cb80c15
    
    task-3757205
    
    closes odoo/odoo#234414
    
    X-original-commit: 8124bd6419012b2a42a49fe33adc6c26f0dc3108
    Signed-off-by: Robin Lejeune (role) <role@odoo.com>
    Signed-off-by: Outagant Mehdi (mou) <mou@odoo.com>

--- addons/website/static/src/interactions/video/media_video.js
+++ addons/website/static/src/interactions/video/media_video.js
@@ -1,10 +1,9 @@
-import { htmlEscape } from "@odoo/owl";
-
 import { Interaction } from "@web/public/interaction";
 import { registry } from "@web/core/registry";
 
 import { _t } from "@web/core/l10n/translation";
 import { setupAutoplay, triggerAutoplay } from "@website/utils/videos";
+import { generateVideoIframe } from "@website/js/content/generate_video_iframe";
 
 export class MediaVideo extends Interaction {
     static selector = ".media_iframe_video";
@@ -42,11 +41,15 @@ export class MediaVideo extends Interaction {
     start() {
         let iframeEl = this.el.querySelector(":scope > iframe");
 
-        // The following code is only there to ensure compatibility with
-        // videos added before bug fixes or new Odoo versions where the
-        // <iframe/> element is properly saved.
+        // Generate the video `<iframe/>` element when restarting interacions.
+        // In some cases (e.g., when adding a new video block), we don’t need
+        // to rebuild the same iframe while starting the widget.
         if (!iframeEl) {
-            iframeEl = this.generateIframe();
+            iframeEl = generateVideoIframe(this.el, this.services.website_cookies.manageIframeSrc);
+        }
+
+        if (iframeEl && !iframeEl.getAttribute("aria-label")) {
+            iframeEl.setAttribute("aria-label", _t("Media video"));
         }
 
         if (iframeEl?.hasAttribute("src")) {
@@ -60,59 +63,6 @@ export class MediaVideo extends Interaction {
                 );
             }
         }
-    }
-
-    generateIframe() {
-        // Bug fix / compatibility: empty the <div/> element as all information
-        // to rebuild the iframe should have been saved on the <div/> element
-        this.el.textContent = "";
-
-        // Add extra content for size / edition
-        const div1 = document.createElement("div");
-        div1.classList.add("css_editable_mode_display");
-        div1.innerHTML = "&nbsp;";
-        const div2 = document.createElement("div");
-        div2.classList.add("media_iframe_video_size");
-        div2.innerHTML = "&nbsp;";
-        this.insert(div1);
-        this.insert(div2);
-
-        // Rebuild the iframe. Depending on version / compatibility / instance,
-        // the src is saved in the 'data-src' attribute or the
-        // 'data-oe-expression' one (the latter is used as a workaround in 10.0
-        // system but should obviously be reviewed in master).
-
-        const src = htmlEscape(
-            this.el.getAttribute("data-oe-expression") || this.el.getAttribute("data-src")
-        );
-        // Validate the src to only accept supported domains we can trust
-
-        const m = src.match(/^(?:https?:)?\/\/([^/?#]+)/);
-        if (!m) {
-            return;
-        }
-
-        const domain = m[1].replace(/^www\./, "");
-        const supportedDomains = [
-            "youtu.be",
-            "youtube.com",
-            "youtube-nocookie.com",
-            "instagram.com",
-            "player.vimeo.com",
-            "vimeo.com",
-            "dailymotion.com",
-        ];
-        if (!supportedDomains.includes(domain)) {
-            return;
-        }
-
-        const iframeEl = document.createElement("iframe");
-        iframeEl.frameborder = "0";
-        iframeEl.allowFullscreen = "allowfullscreen";
-        iframeEl.ariaLabel = _t("Media video");
-        this.insert(iframeEl);
-        this.services.website_cookies.manageIframeSrc(iframeEl, src);
-        return iframeEl;
     }
 }
 
