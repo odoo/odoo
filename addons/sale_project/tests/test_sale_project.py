d8f0c23f0aaf035c6fb40dfcb57714c6d1ab12b6
Abdelouahab Laaroussi (abla) <abla@odoo.com>
2025-11-05 12:41:28 +0100

    [FIX] sale_project: set allocated hours on task creation
    To reproduce:
    =============
    - Create service product with `service_tracking = task_in_project`
    and `service_type = manual`
    - Create a SO with this product and set quantity on the line
    - Confirm the SO
    - check the created task, allocated hours is 0.0 instead of the
    quantity of the SO line
    
    Problem:
    ========
    When creating tasks from SO lines, allocated hours is initialized to 0
    then computed based on the SOL quantity except when the product's
    service_type is 'milestones' or 'manual'.
    
    Solution:
    =========
    Following the logic in `write` method of `sale.order.line`, the allocated
    hours should be set to the SOL quantity.
    
    opw-5153467
    
    closes odoo/odoo#234524
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/sale_project/tests/test_sale_project.py
+++ addons/sale_project/tests/test_sale_project.py
@@ -1329,3 +1329,18 @@ class TestSaleProject(HttpCase, TestSaleProjectCommon):
         so.action_confirm()
         self.assertFalse(self.product_order_service2.project_id.task_ids)
         self.assertFalse(sol.task_id)
+
+    def test_allocated_hours_manual_delivery_service(self):
+        """
+        Test that allocated_hours match the quantity ordered on creation
+        when the service product has 'delivered_manual' service policy.
+        """
+        self.product_service_delivered_manual.service_tracking = 'task_in_project'
+        so = self.env['sale.order'].create({'partner_id': self.partner.id})
+        sol = self.env['sale.order.line'].create({
+            'order_id': so.id,
+            'product_id': self.product_service_delivered_manual.id,
+            'product_uom_qty': 10,
+        })
+        so.action_confirm()
+        self.assertEqual(sol.task_id.allocated_hours, 10, "The allocated hours should be 10.")
