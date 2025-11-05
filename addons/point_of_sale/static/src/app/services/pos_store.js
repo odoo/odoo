2630e4ac6a16f55ba7a7fcde1dba256df2b73f96
Pedram (PEBR) <pebr@odoo.com>
2025-11-05 14:48:25 +0000

    [FIX] point_of_sale: keep the price type of combo line to original
    After commit https://github.com/odoo/odoo/commit/69057e41fb4cd800d23401ead8ae11bf7cba7c64, the price type of combo lines was
    changed to 'automatic' when creating the combo line. This caused the
    set pricelist logic to update the price of combo lines with automatic
    price type, which conflicts with the intended behavior.
    
    This commit ensures that the price type of combo lines is preserved as
    original in cases where we want to update the price, and only set to
    automatic in cases like loading a sale order or refunding an order.
    
    opw-5240429
    
    closes odoo/odoo#241795
    
    X-original-commit: 02104878d825522ebdf313379bf117e0c46b207d
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/point_of_sale/static/src/app/services/pos_store.js
+++ addons/point_of_sale/static/src/app/services/pos_store.js
@@ -1070,7 +1070,7 @@ export class PosStore extends WithLazyGetterTrap {
                     ]),
                     combo_item_id: comboItem.combo_item_id,
                     price_unit: comboItem.price_unit,
-                    price_type: "automatic",
+                    price_type: "original",
                     order_id: order,
                     qty: comboItem.qty * values.qty,
                     attribute_value_ids: comboItem.attribute_value_ids?.map((attr) => [
