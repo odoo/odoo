96c8a46bcd67bb005d0050ffd3d25a4feb3b595e
Meet <meba@odoo.com>
2025-11-05 23:34:27 +0530

    [IMP] snailmail_service: letter_status webhook integration
    Before this commit
    - sometimes letters send by snailmail were not received by receiver
      but there was no way to know about the status of the letter
    
    After this commit
    - added webhook that notify the user with the status of letter
    
    task-4942960

--- addons/snailmail/models/res_partner.py
+++ addons/snailmail/models/res_partner.py
@@ -18,7 +18,7 @@ class ResPartner(models.Model):
 
         if letter_address_vals:
             letters = self.env['snailmail.letter'].search([
-                ('state', 'not in', ['sent', 'canceled']),
+                ('state', 'not in', ['sent', 'canceled', 'delivered', 'undeliverable']),
                 ('partner_id', 'in', self.ids),
             ])
             letters.write(letter_address_vals)
