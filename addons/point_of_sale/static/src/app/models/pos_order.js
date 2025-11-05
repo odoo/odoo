b08544517f84eb1a44b81d82c488b58a2065b5cf
Pedram (PEBR) <pebr@odoo.com>
2025-11-05 15:48:25 +0100

    [FIX] point_of_sale: keep the price type of combo line to original
    After commit https://github.com/odoo/odoo/commit/69057e41fb4cd800d23401ead8ae11bf7cba7c64, the price type of combo lines was
    changed to 'automatic' when creating the combo line. This caused the
    set pricelist logic to update the price of combo lines with automatic
    price type, which conflicts with the intended behavior.
    
    This commit ensures that the price type of combo lines is preserved as
    original in cases where we want to update the price, and only set to
    automatic in cases like loading a sale order or refunding an order.
    
    opw-5240429
    
    closes odoo/odoo#234575
    
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/point_of_sale/static/src/app/models/pos_order.js
+++ addons/point_of_sale/static/src/app/models/pos_order.js
@@ -525,7 +525,7 @@ export class PosOrder extends Base {
             );
         }
         const combo_children_lines = this.lines.filter(
-            (line) => line.price_type === "automatic" && line.combo_parent_id
+            (line) => line.price_type === "original" && line.combo_parent_id
         );
         combo_children_lines.forEach((line) => {
             line.set_unit_price(
