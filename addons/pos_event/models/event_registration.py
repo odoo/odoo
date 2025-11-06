cb36a5a09ee352904bd0d8fbab3600702fbb13be
Sarah Bellefroid <sbel@odoo.com>
2025-11-06 15:28:42 +0100

    [FIX] pos_event: use partner details if not filled in
    Currently, when a customer is set on the order and buys a event ticket,
    the information is not set as partner on the registration.
    
    Steps to reproduce:
    -------------------
    * Create an event that asks for name but is not required
    * Open pos and sell on ticket
    * Do not put name info
    * Select a customer for the order
    * Validate order
    * Check registrations
    > Observation: The customer is not registered as the attendee
    
    Why the fix:
    ------------
    We compare the scenarios with the same flow but from website registrations.
    
    On the website if there is no user registered:
    -If information is not filled, nothing will be registered on the registration
    -If information is filled it will be used to populate attendee fields
    
    If there is a user registered while on website:
    -If no information is filled, attendee fields will be populated with the
    data from the connected user
    -If information is filled, it will be used for attendee fields
    -If partial information is filled, it will be used for attendee fields but
     will also be completed with data coming from the connected user
    
    To acheive the same behavior from the pos we first need to register the
    customer as the partner for the event.
    
    During event creation we also remove values regarding attendee name, email,
    phone and company if they were not provided during the order. From the website
    they are not used during creation if they were not given by the customer.
    However, in the pos this information is present anyway (as empty string or False)
    as they are fields on the model "event.registration" and are still send to the
    backennd even if we remove the information here
    https://github.com/odoo/odoo/blob/787621e44a9cef30469849929df267cae9e977f2/addons/pos_event/static/src/app/screens/product_screen/product_screen.js#L127
    
    We do the fix server-side as a fix in the frontend would not be as straightforward.
    A customer migth be on the order before selecting event ticket and it's easy but
    one might also add the customer after the ticket was selected.
    
    opw-5137197
    
    closes odoo/odoo#234871
    
    Signed-off-by: Stéphane Vanmeerhaeghe (stva) <stva@odoo.com>

--- addons/pos_event/models/event_registration.py
+++ addons/pos_event/models/event_registration.py
@@ -20,6 +20,7 @@ class EventRegistration(models.Model):
 
     @api.model_create_multi
     def create(self, vals_list):
+        self._populate_creation_vals(vals_list)
         result = super().create(vals_list)
         result._update_available_seat()
         return result
@@ -28,6 +29,17 @@ class EventRegistration(models.Model):
         result = super().write(vals)
         self._update_available_seat()
         return result
+
+    def _populate_creation_vals(self, vals_list):
+        for vals in vals_list:
+            if 'pos_order_line_id' in vals:
+                if 'partner_id' not in vals:
+                    pol = self.env['pos.order.line'].browse(vals['pos_order_line_id']).exists()
+                    if pol and pol.order_id.partner_id:
+                        vals['partner_id'] = pol.order_id.partner_id.id
+                for field in ["name", "email", "phone", "company_name"]:
+                    if field in vals and not vals[field]:
+                        vals.pop(field)
 
     def _update_available_seat(self):
         # Here sudo is used in order for pos_event to update the available seats to all open pos session when a ticket is sold in website for example
