9bf8d417cb39f78ec2ddcd113e155938cf922dcb
Vict0rPL <wiktorniedopytalski@gmail.com>
2025-11-06 09:16:34 +0100

    Fix - cant create new user from employee

--- addons/oh_employee_creation_from_user/models/res_users.py
+++ addons/oh_employee_creation_from_user/models/res_users.py
@@ -34,13 +34,42 @@ class ResUsers(models.Model):
                                        ' data of the user')
 
     @api.model_create_multi
-    def create(self, vals):
+    def create(self, vals_list):
         """Overrides the default 'create' method to create an employee record
-        when a new user is created."""
-        result = super(ResUsers, self).create(vals)
-        result['employee_id'] = self.env['hr.employee'].sudo().create({
-            'name': result['name'],
-            'user_id': result['id'],
-            'private_street': result['partner_id'].id
-        })
+        when a new user is created.
+
+        Note: This method will NOT auto-create an employee if:
+        - create_employee_id is set (user being created FROM an existing employee)
+        - create_employee flag is explicitly False
+        - An employee already exists for this user in the current company
+        """
+        result = super(ResUsers, self).create(vals_list)
+
+        for user, vals in zip(result, vals_list):
+            # Skip employee creation if user is being linked to an existing employee
+            # (this happens when clicking "Create User" from an employee form)
+            if vals.get('create_employee_id'):
+                continue
+
+            # Skip if create_employee flag is explicitly False
+            if vals.get('create_employee') is False:
+                continue
+
+            # Check if an employee already exists for this user in the current company
+            existing_employee = self.env['hr.employee'].sudo().search([
+                ('user_id', '=', user.id),
+                ('company_id', '=', user.company_id.id)
+            ], limit=1)
+
+            if existing_employee:
+                # Link to existing employee instead of creating a new one
+                user.employee_id = existing_employee
+            else:
+                # Create a new employee only if one doesn't exist
+                user.employee_id = self.env['hr.employee'].sudo().create({
+                    'name': user.name,
+                    'user_id': user.id,
+                    'company_id': user.company_id.id,
+                })
+
         return result
