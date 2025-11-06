d1faf379afe3290f4b8c0ed9d7e4c782605e6799
Laurent Smet (LAS) <las@odoo.com>
2025-11-06 10:03:14 +0000

    [FIX] account_edi_ubl_cii: Fix import totals
    This commit refactors the import code of BIS3 Invoice to fix various
    issues about unsynchronized values between the imported invoice and the
    source XML file.
    
    The new way we import BIS3 invoice can be categorized as:
    
    - collecting all the values from the XML to a dictionary object
    - prepare the values and amounts to write to the invoice in its entirety
      using the tax computation engine helpers
    - write the whole processed values to the invoice (as a single write)
    - (in 18.0 ~ 18.2) recalculate discrepancies and update the invoice lines
      (if needed) with the corrected amounts
    
    This commit also includes a new test suite for BIS3 import, and a new
    approach of import testing, "Partial Imports", is introduced to better
    atomize the big import test files (and make it understandable). In the long term,
    `l10n_account_edi_ubl_cii_tests` will eventually be removed in favor of
    these small-but-many partial tests.
    
    closes odoo/odoo#262545
    
    Task-id: 5058687
    X-original-commit: 138031bf3a4e438fe83c209196b5f8063ddfe8da
    Related: odoo/enterprise#116016
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>
    Signed-off-by: Laurent Smet (las) <las@odoo.com>
    Signed-off-by: William André (wan) <wan@odoo.com>
    Co-authored-by: Yosua Nicolaus <yoni@odoo.com>

---
+++ addons/account_edi_ubl_cii/tests/test_ubl_import_bis3_invoice_be_retrieve_vehicle.py
@@ -0,0 +1,67 @@
+from odoo.addons.account_edi_ubl_cii.tests.test_ubl_import_bis3_invoice_be import TestUblImportBis3InvoiceBE
+from odoo.tests import tagged
+
+
+@tagged('post_install_l10n', 'post_install', '-at_install')
+class TestUblImportBis3InvoiceBEVehicle(TestUblImportBis3InvoiceBE):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.ensure_installed('fleet')
+        cls.env.user.group_ids |= cls.env.ref('fleet.fleet_group_manager')
+        # create car datas
+        brand = cls.env['fleet.vehicle.model.brand'].create({  # noqa: OLS03001
+            'name': 'Test Brand',
+        })
+        model = cls.env['fleet.vehicle.model'].create({  # noqa: OLS03001
+            'name': 'Test Model',
+            'brand_id': brand.id,
+        })
+        cls.car = cls.env['fleet.vehicle'].create({  # noqa: OLS03001
+            'model_id': model.id,
+            'vin_sn': 'ABCDEF012345GHJKL',
+            'license_plate': '1-ABC-123',
+        })
+        cls.car2 = cls.env['fleet.vehicle'].create({  # noqa: OLS03001
+            'model_id': model.id,
+            'vin_sn': 'ABCDEF012346GHJKL',
+        })
+        cls.car3 = cls.env['fleet.vehicle'].create({  # noqa: OLS03001
+            'model_id': model.id,
+            'vin_sn': 'ABCDEF012347GHJKL',
+        })
+
+        cls.env.ref('base.EUR').active = True  # EU
+
+    def test_vehicle_import_generic_invoice(self):
+        """
+        Test that we get the common value on the full invoice for each lines
+        """
+        invoice = self._import_invoice_as_attachment_on(
+            test_name='test_partial_import_vehicle_invoice',
+            journal=self.company_data['default_journal_sale'],
+        )
+
+        # One VIN SN for the move, all lines should link the vehicle
+        self.assertRecordValues(invoice.invoice_line_ids, [{'vehicle_id': self.car.id} for _ in range(3)])
+
+    def test_vehicle_import_different_lines(self):
+        """
+        Test that vehicle id is set on invoice lines if fleet is installed
+        with each invoice line having its own defined value
+        """
+        # Multiple VIN SN, every line should link its own vehicle
+        invoice = self._import_invoice_as_attachment_on(
+            test_name='test_partial_import_vehicle_invoice_lines',
+            journal=self.company_data['default_journal_sale'],
+        )
+
+        self.assertRecordValues(invoice.invoice_line_ids, [
+            {'vehicle_id': self.car.id},  # match VIN in AdditionalItemProperty/Value where AdditionalItemProperty/Name == 'SerialNumber'
+            {'vehicle_id': self.car2.id},  # match VIN in AdditionalItemProperty/Value where AdditionalItemProperty/Name == 'VIN'
+            {'vehicle_id': self.car3.id},  # search VIN in Item/Description
+            {'vehicle_id': self.car.id},  # search License Plate in Item/Description
+            {'vehicle_id': self.car.id},  # search combined License Plate and VIN in Item/Description
+            {'vehicle_id': False},  # Double vin -> no vehicle linked
+        ])
