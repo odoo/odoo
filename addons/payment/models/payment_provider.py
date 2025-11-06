cd05e8c744c2938f1584d23c4d47f80dd584b525
Levi Siuzdak (sile) <sile@odoo.com>
2025-11-06 13:23:34 +0000

    [FIX] payment: allow branch companies to use parent's payment providers
    Versions
    --------
    - 17.0+
    
    Steps
    -----
    1. Have a published payment provider;
    2. create a branch company;
    3. create a sales order in branch company;
    4. enable online payment for sales order;
    5. open sales order in portal view;
    6. attempt to pay.
    
    Issue
    -----
    No compatible payment providers found.
    
    Cause
    -----
    The `payment.provider._check_company_domain` is set to the default exact
    match, so when it's used in `_get_compatible_payment_providers`, it's
    unable to find any providers for the branch company.
    
    Solution
    --------
    Set `_check_company_domain` to `check_company_domain_parent_of`.
    
    opw-5214269
    
    closes odoo/odoo#235144
    
    X-original-commit: 1d15b0237610053917c234a6a6cea106e55e745f
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>
    Signed-off-by: Levi Siuzdak <sile@odoo.com>

--- addons/payment/models/payment_provider.py
+++ addons/payment/models/payment_provider.py
@@ -13,7 +13,6 @@ from odoo.addons.payment import utils as payment_utils
 from odoo.addons.payment.const import REPORT_REASONS_MAPPING, SENSITIVE_KEYS
 from odoo.addons.payment.logging import get_payment_logger
 
-
 # Pass the possibly empty set of sensitive keys to the logger in case a provider module extends it.
 _logger = get_payment_logger(__name__, sensitive_keys=SENSITIVE_KEYS)
 
@@ -23,6 +22,7 @@ class PaymentProvider(models.Model):
     _description = 'Payment Provider'
     _order = 'module_state, state desc, sequence, name'
     _check_company_auto = True
+    _check_company_domain = models.check_company_domain_parent_of
 
     def _valid_field_parameter(self, field, name):
         return name == 'required_if_provider' or super()._valid_field_parameter(field, name)
