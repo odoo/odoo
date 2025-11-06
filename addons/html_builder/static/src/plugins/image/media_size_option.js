3d427e72bde5e45281be84e272c05c81a628fbcd
Subhadeep R. Dev <sdev@odoo.com>
2025-11-06 10:24:11 +0100

    [IMP] html_builder: group image transformation options
    This commit reorganizes the image transformation options into a single
    group, making the interface more intuitive.
    
    task-5214095

--- addons/html_builder/static/src/plugins/image/media_size_option.js
+++ addons/html_builder/static/src/plugins/image/media_size_option.js
@@ -2,4 +2,10 @@ import { BaseOptionComponent } from "@html_builder/core/utils";
 
 export class MediaSizeOption extends BaseOptionComponent {
     static template = "html_builder.MediaSizeOption";
+    static props = {
+        level: { type: Number, optional: true },
+    };
+    static defaultProps = {
+        level: 0,
+    };
 }
