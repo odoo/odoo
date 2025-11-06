5dc43a2156e5b176e3236583b45b4838a987ee7d
pajo <pajo@odoo.com>
2025-11-06 17:33:14 +0530

    [FIX] account: incorrect vendor bill payment state on deleting payment
    Steps to reproduce:
    -------------------------
    1. Install the Accounting module.
    2. Create and confirm a Vendor Bill.
    3. Click on Pay and create a payment for the bill.
    4. Open the created payment using the smart button.
    5. Delete the payment or click on Reset to Draft.
    
    Observation:
    -------------------------
    1. On deleting the payment:
    The Vendor Bill still shows the "In Payment" status even after the payment is
    deleted.
    2. On resetting the payment to draft:
    The Vendor Bill also remains in the "In Payment" status instead of reverting to
    "Not Paid".
    
    This behavior is not observed for customer invoices, where the payment state
    updates correctly in both cases.
    
    Issue:
    -------------------------
    1. Delete case:
    In the `unlink` method,
    https://github.com/odoo/odoo/blob/b991f766e28dc71f8627fdfbf2d59589d9707d3a/addons/account/models/account_payment.py#L938-L945
    the `linked_invoices` variable only includes invoices that are reconciled
    (i.e., their journal items are matched). Since the Vendor Bill is not yet
    reconciled, it is excluded from recomputation. Hence, its `payment_state`
    remains unchanged.
    
    2. Reset to draft case:
    In the `_compute_payment_state` method,
    https://github.com/odoo/odoo/blob/b991f766e28dc71f8627fdfbf2d59589d9707d3a/addons/account/models/account_move.py#L1162-L1163
    the compute depends on the state of reconciled payments. However, since these
    payments are not reconciled, the compute method is not triggered, and the
    payment state remains outdated.
    
    Solution:
    -------------------------
    Added `matched_payment_ids.state` in the depends of the
    `_compute_reconciled_payment_ids` method to ensure it recomputes correctly when
    payment state changes on setting payment to draft or deleting payment
    
    opw-5208772

--- addons/account/tests/test_account_payment.py
+++ addons/account/tests/test_account_payment.py
@@ -427,6 +427,30 @@ class TestAccountPayment(AccountTestInvoicingCommon, MailCommon):
         self.assertRecordValues(invoice, [{'payment_state': 'not_paid'}])
         self.assertRecordValues(payment.move_id.line_ids, [{'reconciled': True}] * 2)
 
+    def test_bill_state_change_on_payment_state(self):
+        """Test that bill payment state changes correctly when payment state transitions occur.
+        • Draft payment case: Bill state reverts to 'not_paid' when payment is drafted
+        • Payment unlink case: Bill state reverts to 'not_paid' when payment is deleted
+        """
+        bill = self.init_invoice('in_invoice', post=True, partner=self.partner_a, products=self.product_a)
+
+        # We have to test it without any Outstanding Payment account set in Journal
+        self.bank_journal_1.outbound_payment_method_line_ids.payment_account_id = False
+
+        payment = self.env['account.payment.register']\
+            .with_context(active_model='account.move', active_ids=bill.ids)\
+            .create({})\
+            ._create_payments()
+        self.assertEqual(bill.payment_state, self.env['account.move']._get_invoice_in_payment_state())
+
+        payment.action_draft()
+        self.assertEqual(payment.state, 'draft')
+        self.assertEqual(payment.invoice_ids.payment_state, 'not_paid')
+
+        payment.action_post()
+        payment.unlink()
+        self.assertEqual(bill.payment_state, 'not_paid')
+
     def test_payment_without_default_company_account(self):
         """ The purpose of this test is to check the specific behavior when duplicating an inbound payment, then change
         the copy to an outbound payment when we set the outstanding accounts (payments and receipts) on a journal but
