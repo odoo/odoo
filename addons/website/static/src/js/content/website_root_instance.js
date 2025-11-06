c63935e80cdbde59dee908293b660ec98509b7a0
Julien Launois (jula) <jula@odoo.com>
2025-11-06 17:55:47 +0100

    [FIX] website: fix snippet dialog for rtl languages
    __Before this commit__:
    1. Set a right-to-left language (e.g. arabic) as default language on the
    website
    2. Open the website editor
    3. Select an element of the page
    => The editor overlay border are not in the correct place
    4. Open the snippet dialog
    => The snippets are not visible.
    
    __Cause__:
    Since [this commit][1], `lang` in `session.bundle_params` is (correctly)
    set to the `request.lang.code`.
    Consequently, `website.assets_all_wysiwyg_inside` is fetched with the
    frontend language code in the `searchParams` which shouldn't be the case
    especially if the website language direction is not the same as the
    backend one.
    
    __Description of the fix:__
    The `getBundle` method from outside the iframe is used instead of the
    frontend one to retrieve the URL of the bundle that matches the language
    direction of the builder.
    
    Backport and adapt the test from [this commit][2].
    
    [1]: https://github.com/odoo/odoo/commit/e10852a45f10c55a833c33a51956e5f
    [2]: https://github.com/odoo/odoo/commit/7bcd56c07852b77849b05184200c48a
    
    task-5246916
    opw-5214703
    
    closes odoo/odoo#234782
    
    Signed-off-by: Robin Lejeune (role) <role@odoo.com>

--- addons/website/static/src/js/content/website_root_instance.js
+++ addons/website/static/src/js/content/website_root_instance.js
@@ -3,14 +3,24 @@
 import { createPublicRoot } from "@web/legacy/js/public/public_root";
 import lazyloader from "@web/legacy/js/public/lazyloader";
 import { WebsiteRoot } from "./website_root";
-import { loadBundle } from "@web/core/assets";
+import { loadCSS, loadJS } from "@web/core/assets";
 
 const prom = createPublicRoot(WebsiteRoot).then(async rootInstance => {
     // This data attribute is set by the WebsitePreview client action for a
     // restricted editor user.
     if (window.frameElement) {
         if (window.frameElement.dataset.loadWysiwyg === 'true') {
-            await loadBundle("website.assets_all_wysiwyg_inside");
+            // `getBundle` fetches the URL of the bundle by including
+            // `session.bundle_params` as search params. The `lang` search param
+            // in particular determines if the CSS bundles are fetch in their
+            // RTL version or not. 
+            // For `website.assets_all_wysiwyg_inside`, it needs to match the
+            // builder language direction and not the frontend one. Therefore we
+            // must use `getBundle` from outside the iframe.
+            const { getBundle } = window.parent.odoo.loader.modules.get("@web/core/assets");
+            await getBundle("website.assets_all_wysiwyg_inside").then(({ cssLibs, jsLibs }) =>
+                Promise.all([...cssLibs.map(loadCSS), ...jsLibs.map(loadJS)])
+            );
         }
         window.dispatchEvent(new CustomEvent('PUBLIC-ROOT-READY', {detail: {rootInstance}}));
     }
