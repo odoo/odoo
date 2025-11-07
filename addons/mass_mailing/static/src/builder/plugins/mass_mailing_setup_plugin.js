854dcf561864343cd22503ad5eae94d571f5a07c
Louis (loco) <loco@odoo.com>
2025-11-07 13:52:40 +0100

    [IMP] html_builder, *: rename resource and class
    *: mass_mailing, website
    
    As the `o_editable` class has been renamed into `o_savable`, this commit
    renames the `o_editable_attribute` class by `o_savable_attribute` and
    the `o_editable_selectors` resource by `savable_selectors`.
    
    task-5248483
    
    Part-of: odoo/odoo#234395
    Related: odoo/enterprise#98812
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/mass_mailing/static/src/builder/plugins/mass_mailing_setup_plugin.js
+++ addons/mass_mailing/static/src/builder/plugins/mass_mailing_setup_plugin.js
@@ -5,7 +5,7 @@ import { registry } from "@web/core/registry";
 export class MassMailingSetupPlugin extends Plugin {
     static id = "mass_mailing_setup_plugin";
     resources = {
-        o_editable_selectors: ".o_mail_wrapper_td",
+        savable_selectors: ".o_mail_wrapper_td",
         snippet_preview_dialog_bundles: [
             "mass_mailing.assets_iframe_style",
             "mass_mailing.iframe_add_dialog",
