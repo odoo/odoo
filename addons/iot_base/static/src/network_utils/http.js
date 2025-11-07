2bf93661e64dea940a1aef04c493584f3aa41f8c
Max Whale <mawh@odoo.com>
2025-11-07 07:47:17 +0000

    [FIX] iot_base: event listener requests not cancelling
    Steps to reproduce:
    1. Pair an IoT box
    2. In the IoT form view, click on any device, then click back to return
       to the IoT form view.
    3. Repeat this step multiple times. If you have devtools open, you can
       see a `/event` fetch request every time you open the device form.
    
    Expected behaviour:
    - When a new request is made, the previous request is cancelled.
    
    Actual behaviour:
    - The previous requests remain active, and eventually no further
      requests are possible due to browser limits.
    
    This behaviour was broken when the longpolling was changed to use the
    `fetch` method instead of Odoo's `rpc` method. This commit restores the
    behaviour by using an `AbortController` instance which is aborted when
    `stopPolling` is called.
    
    closes odoo/odoo#234966
    
    X-original-commit: fda587b1af559cfe1fbcdbb0b04fca4f18fd5d0d
    Signed-off-by: Max Whale (mawh) <mawh@odoo.com>

--- addons/iot_base/static/src/network_utils/http.js
+++ addons/iot_base/static/src/network_utils/http.js
@@ -24,15 +24,17 @@ export function formatEndpoint(ip, route) {
  * @param params Parameters to send with the request (optional)
  * @param timeout Time before the request times out (default: 6000ms)
  * @param headers HTTP headers to send with the request (optional)
+ * @param abortSignal AbortSignal used to cancel the request early (optional)
  * @returns {Promise<any>}
  */
-export async function post(ip, route, params = {}, timeout = 6000, headers = {}) {
+export async function post(ip, route, params = {}, timeout = 6000, headers = {}, abortSignal = null) {
     const endpoint = formatEndpoint(ip, route);
+    const timeoutSignal = AbortSignal.timeout(timeout);
     const response = await browser.fetch(endpoint, {
         body: JSON.stringify({'params': params}),
         method: "POST",
         headers: {"Content-Type": "application/json", ...headers},
-        signal: AbortSignal.timeout(timeout),
+        signal: abortSignal ? AbortSignal.any([abortSignal, timeoutSignal]) : timeoutSignal,
     });
 
     return response.json();
