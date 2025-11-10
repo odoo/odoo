10e91330c82d080581d998b09b35a38c03671caf
tsm-odoo <tsm@odoo.com>
2025-11-10 09:17:45 +0100

    [IMP] im_livechat: manage expertises from info panel
    The live chat info panel shows expertises linked to the
    chat. However, it's not possible to assign expertises to
    the chat (only available from the chat bot script).
    
    Expertises are useful to quickly identify which kind of help
    is needed on a chat, or to find conversations about the same
    topic.
    
    This commit allows to add expertises from the channel info panel.
    
    task-5190361
    
    X-original-commit: a4e98809f2c9107850a5ab01aeb76b3dac2b2a6e

---
+++ addons/im_livechat/static/src/core/web/expertise_tags_autocomplete.js
@@ -0,0 +1,83 @@
+import { Component } from "@odoo/owl";
+
+import { _t } from "@web/core/l10n/translation";
+import { rpc } from "@web/core/network/rpc";
+import { x2ManyCommands } from "@web/core/orm_service";
+import { useTagNavigation } from "@web/core/record_selectors/tag_navigation_hook";
+import { BadgeTag } from "@web/core/tags_list/badge_tag";
+import { useService } from "@web/core/utils/hooks";
+import { Many2XAutocomplete } from "@web/views/fields/relational_utils";
+
+/**
+ * @typedef {Object} Props
+ * @property {import("models").DiscussChannel} channel
+ * @extends {Component<Props, Env>}
+ */
+export class ExpertiseTagsAutocomplete extends Component {
+    static template = "im_livechat.ExpertiseTagsAutocomplete";
+    static props = ["channel"];
+    static components = { BadgeTag, Many2XAutocomplete };
+
+    setup() {
+        super.setup(...arguments);
+        this.orm = useService("orm");
+        this.store = useService("mail.store");
+        useTagNavigation("root", {
+            delete: (index) => {
+                const expertise = this.props.channel.livechat_expertise_ids[index];
+                if (expertise) {
+                    this.unlinkExpertise(expertise);
+                }
+            },
+        });
+    }
+
+    /** @param {(ReturnType<typeof x2ManyCommands.link>|ReturnType<typeof x2ManyCommands.unlink>)[]} ormCommands */
+    writeExpertises(ormCommands) {
+        rpc("/im_livechat/conversation/write_expertises", {
+            channel_id: this.props.channel.id,
+            orm_commands: ormCommands,
+        });
+    }
+
+    /** @param {string} name */
+    createAndLinkExpertise(name) {
+        if (
+            this.props.channel.livechat_expertise_ids.some(
+                (expertise) => expertise.name === name.trim()
+            )
+        ) {
+            return;
+        }
+        rpc("/im_livechat/conversation/create_and_link_expertise", {
+            channel_id: this.props.channel.id,
+            expertise_name: name,
+        });
+    }
+
+    /** @param {{id: number, display_name: string}[]} expertises */
+    addExpertises(expertises) {
+        const toAdd = expertises.filter((expertise) => !this.isSelected(expertise.id));
+        if (!toAdd.length) {
+            return;
+        }
+        this.writeExpertises(toAdd.map((expertise) => x2ManyCommands.link(expertise.id)));
+    }
+
+    /** @param {import("models").LivechatExpertise} expertise */
+    unlinkExpertise(expertise) {
+        this.writeExpertises([x2ManyCommands.unlink(expertise.id)]);
+    }
+
+    get placeholder() {
+        if (this.props.channel.livechat_expertise_ids.length === 0) {
+            return _t("Add expertise");
+        }
+        return "";
+    }
+
+    /** @param {number} expertiseId */
+    isSelected(expertiseId) {
+        return this.props.channel.livechat_expertise_ids.some((e) => e.id === expertiseId);
+    }
+}
