f009bc3e92a0715eef15fd5a34c95e5495d9635b
sagu-odoo <sagu@odoo.com>
2025-11-10 11:38:25 +0000

    [FIX] fields: Fix Related column type
    Steps to Reproduce:
    
    1. create test ``Float`` field in model ``A`` with ``digits`` args
    2. create ``Many2One`` field with comodel ``A``  and then create
       Float Field in ``B Model`` with related ``A`` model test and store
      True
    
    Issue:
      1. ``column_type`` for both model table will be different.
         For ``test field in model A`` the ``column_type`` will be ``numeric``.
         But for the related field ``column_type`` ``float`` it should be
         ``numeric``. This happen because the @lazy_propery it hold the
         ``column_type`` which is ``float8`` and other related attributes
         from ``setup_related`` before that ``_digits`` have the null value.
         So, from [here](https://github.com/odoo-dev/odoo/blob/a9398502260fa57573b88fd62ca3f554e0685c7b/odoo/fields.py#L772)
         it remains ``float8`` it should update with ``numeric``
    
       Second issue comes From odoo 18.3
      2. during upgrade if any new ``module`` is intalled due to dependency change
         and inherits the same model that is ``A``. Due to ``_auto_init`` it will recompute
         this related field  because due to this newly
         [commit](https://github.com/odoo/odoo/commit/08964eb6710e15ec453fdd16c9e1da494a91877c)
         clear the columns column becomes [``False``]
         and [``not column``] becomes true  from ``update_db``
         and same reason as above  it didn't return from [here]
         (https://github.com/odoo/odoo/commit/08964eb6710e15ec453fdd16c9e1da494a91877c#diff-956d895aa67961bac940841f7c3d1e10eb8ecabec82ef017803c4a6a3bb7cd22R1074)
         because column type is ``float8`` which leads to  memory of unecessary
         compute which shouldn't do in first place.
    
    FIX:
    
    reseting the property and letting it cache again.
    
    before fix:-
    ```
    SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'account_move_line' AND column_name = 'test_line_id';
     column_name  |    data_type
    --------------+------------------
     test_line_id | double precision
    (1 row)
    
    ```
    
    After fix:-
    ```
    SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'account_move_line' AND column_name = 'test_line_id';
     column_name  | data_type
    --------------+-----------
     test_line_id | numeric
    (1 row)
    
    ```
    opw-5222760
    upg-3253635
    
    closes odoo/odoo#237944
    
    X-original-commit: ccaa86d2c6b959a51a294a58ad91fa202f84e6eb
    Signed-off-by: Rémy Voet (ryv) <ryv@odoo.com>
    Signed-off-by: Raphael Collet <rco@odoo.com>

--- odoo/orm/fields.py
+++ odoo/orm/fields.py
@@ -17,7 +17,7 @@ from operator import attrgetter
 from psycopg2.extras import Json as PsycopgJson
 
 from odoo.exceptions import AccessError, MissingError
-from odoo.tools import Query, SQL, sql
+from odoo.tools import Query, SQL, reset_cached_properties, sql
 from odoo.tools.constants import PREFETCH_MAX
 from odoo.tools.misc import SENTINEL, ReadonlyDict, Sentinel, unique
 
@@ -550,7 +550,8 @@ class Field(typing.Generic[T]):
                 warnings.warn(f'Property {self}.readonly should be a boolean ({self.readonly}).', stacklevel=1)
 
             self._setup_done = True
-
+            # column_type might be changed during Field.setup
+            reset_cached_properties(self)
     #
     # Setup of non-related fields
     #
