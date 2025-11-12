54866e6d1ae0042df6f2ffd3a490dd468d0c4ca9
Julien Mougenot <jum@odoo.com>
2025-11-12 16:15:30 +0100

    [FIX] web: tests - cancel pending drag sequences
    Before this commit, when initiating an unfinished drag sequence (i.e.
    calling `drag` without `drop` or `cancel`), the drag sequence was ended
    by destroying the component, which could throw asynchronous errors after
    the test finished.
    
    One possible fix would be to wait for these errors upon ending a test,
    but this process is handled by Owl and through a possible chain of
    promises, which would have to be accounted for by an arbitrary amount
    of 'animationFrame' or 'advanceTime' calls.
    
    Instead, the `contains...drag` and `contains...dragAndDrop` now handle
    a single drag sequence at a time, with a systematic cleanup after each
    test in case a drag sequence has been left pending. In such cases, the
    sequence is canceled.
    
    closes odoo/odoo#235359
    
    Related: odoo/enterprise#99369
    Signed-off-by: Michaël Mattiello (mcm) <mcm@odoo.com>

--- addons/web/static/tests/_framework/dom_test_helpers.js
+++ addons/web/static/tests/_framework/dom_test_helpers.js
@@ -124,12 +124,18 @@ const waitForTouchDelay = async (delay) => {
     }
 };
 
-let unconsumedContains = [];
+/** @type {(() => any) | null} */
+let cancelCurrentDragSequence = null;
+/** @type {Target[]} */
+const unconsumedContains = [];
 
-afterEach(() => {
+afterEach(async () => {
+    if (cancelCurrentDragSequence) {
+        await cancelCurrentDragSequence();
+    }
     if (unconsumedContains.length) {
         const targets = unconsumedContains.map(String).join(", ");
-        unconsumedContains = [];
+        unconsumedContains.length = 0;
         throw new Error(
             `called 'contains' on "${targets}" without any action: use 'waitFor' if no interaction is intended`
         );
@@ -202,11 +208,11 @@ export function contains(target, options) {
          * @returns {Promise<DragHelpers>}
          */
         drag: async (options) => {
-            consumeContains();
             /** @type {typeof cancel} */
             const cancelWithDelay = async (options) => {
                 await cancel(options);
                 await advanceFrame();
+                cancelCurrentDragSequence = null;
             };
 
             /** @type {typeof drop} */
@@ -216,6 +222,7 @@ export function contains(target, options) {
                 }
                 await drop();
                 await advanceFrame();
+                cancelCurrentDragSequence = null;
             };
 
             /** @type {typeof moveTo} */
@@ -225,6 +232,11 @@ export function contains(target, options) {
 
                 return helpersWithDelay;
             };
+
+            consumeContains();
+
+            await cancelCurrentDragSequence?.();
+            cancelCurrentDragSequence = cancelWithDelay;
 
             const { cancel, drop, moveTo } = await drag(nodePromise, options);
             const helpersWithDelay = {
@@ -246,6 +258,9 @@ export function contains(target, options) {
          */
         dragAndDrop: async (target, dropOptions, dragOptions) => {
             consumeContains();
+
+            await cancelCurrentDragSequence?.();
+
             const [from, to] = await Promise.all([nodePromise, waitFor(target)]);
             const { drop, moveTo } = await drag(from, dragOptions);
 
