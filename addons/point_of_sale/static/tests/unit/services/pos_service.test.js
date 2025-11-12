9a638be2ab481ff7c1a992accf5a352764a86711
Hadi El Yakhni <hael@odoo.com>
2025-11-12 15:17:05 +0000

    [FIX] point_of_sale: `price_subtotal` is the total line price not unit
    Steps to reproduce
    ------------------
    1. Create a product with price 1, no need to add taxes, and add it to
       PoS
    2. In PoS, select this product, and set the quantity to 2, the price is
       2 as expected
    3. Finalize the order
    4. In the backend, open this order and observe that the "Tax Excl." for
       that line shows $1 instead of $2. While "Tax Incl." shows $2 as
       expected.
    
    The client has reproduced this issue in a different way, by printing the
    sales report for that order, he found out that the total is 1 instead of
    2.
    
    Why it's happening
    ------------------
    This issue was introduced in 9538698f13d5763b49b00f4c06a1a2afc0d6b39e.
    We are setting the field `price_subtotal` to the unit price of the
    product, and not to the actual price per line, i.e. we're ignoring the
    quantity!!
    
    The Fix
    -------
    We now set the `price_subtotal` to `priceExcl` which takes into account
    the quantity on that line.
    
    opw-5242022
    
    closes odoo/odoo#236515
    
    X-original-commit: dc9d5795cba2b5091bf5e779963b042e49038f78
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Hadi El Yakhni (hael) <hael@odoo.com>

--- addons/point_of_sale/static/tests/unit/services/pos_service.test.js
+++ addons/point_of_sale/static/tests/unit/services/pos_service.test.js
@@ -473,11 +473,11 @@ describe("pos_store.js", () => {
             expect(order.amount_tax).toEqual(2.85);
             expect(order.lines[0].qty).toEqual(3);
             expect(order.lines[0].price_unit).toEqual(3);
-            expect(order.lines[0].price_subtotal).toEqual(3);
+            expect(order.lines[0].price_subtotal).toEqual(9);
             expect(order.lines[0].price_subtotal_incl).toEqual(10.35);
             expect(order.lines[1].qty).toEqual(2);
             expect(order.lines[1].price_unit).toEqual(3);
-            expect(order.lines[1].price_subtotal).toEqual(3);
+            expect(order.lines[1].price_subtotal).toEqual(6);
             expect(order.lines[1].price_subtotal_incl).toEqual(7.5);
 
             order.is_refund = true;
@@ -488,11 +488,11 @@ describe("pos_store.js", () => {
             expect(order.amount_tax).toEqual(-2.85);
             expect(order.lines[0].qty).toEqual(-3);
             expect(order.lines[0].price_unit).toEqual(3);
-            expect(order.lines[0].price_subtotal).toEqual(3);
+            expect(order.lines[0].price_subtotal).toEqual(9);
             expect(order.lines[0].price_subtotal_incl).toEqual(10.35);
             expect(order.lines[1].qty).toEqual(-2);
             expect(order.lines[1].price_unit).toEqual(3);
-            expect(order.lines[1].price_subtotal).toEqual(3);
+            expect(order.lines[1].price_subtotal).toEqual(6);
             expect(order.lines[1].price_subtotal_incl).toEqual(7.5);
         });
     });
