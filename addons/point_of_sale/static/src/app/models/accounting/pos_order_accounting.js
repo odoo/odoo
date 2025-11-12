odoo/odoo

commit 9538698f13d5763b49b00f4c06a1a2afc0d6b39e

[REF] point_of_sale, pos_*: improve tax computation in PoS
*: l10n_es_pos, l10n_sa_pos, point_of_sale, pos_discount,
  pos_loyalty, pos_razorpay, pos_restaurant, pos_self_order

Before this commit taxes computation was a mess in PoS, with differents
implementations in different places, and some of them not taking into
account all the complexity of the tax system (taxes included in price,
taxes on taxes, etc.).

This commit aims to unify all the tax computation logic in a single
place, and to make it more robust and easier to understand.

---

What's changes:

Rounding methods available in PoS are now:
1) Rounding applied only on cash payments
    In this case the remaining due is rounded only if there is at least
    one cash payment line and the remaining due is less than the
    rounding tolerance.

2) Rounding applied on all payment methods
    In this case the remaining due is always rounded even if a card
    payment method is used. The remaining due is rounded if it is less
    than the rounding tolerance. No payment method is rounded in this
    case, the whole order is rounded instead.

Taxes computation is now done globally on the order, and not on each
line. This way we ensure that the total tax amount is always correct,
even if there is some rounding issues on the lines.

---

Changes in tests:

`test_cash_rounding_down_add_invoice_line_not_only_round_cash_method_with_residual_rounding`
`test_cash_rounding_up_add_invoice_line_not_only_round_cash_method`

Are removed because the tested behavior is not longer present. Now when
rounding is enabled with only_round_cash_method=False, the whole order
is always rounded.

Accounting tests are now principally tested with Hoot instead of tours.

---

Developer note:

No prices should be calculated manually in the code base. Getters have
been created for this purpose in the following files:
- `product_template_accounting.js`
- `pos_order_accounting.js`
- `pos_order_line_accounting.js`

addons/point_of_sale/static/src/app/models/accounting/pos_order_accounting.js
@@ -1,0 +1,261 @@
+import { Base } from "../related_models";
+import { accountTaxHelpers } from "@account/helpers/account_tax";
+import { logPosMessage } from "../../utils/pretty_console_log";
+import { formatCurrency } from "@web/core/currency";
+
+const CONSOLE_COLOR = "#4EFF4D";
+
+export class PosOrderAccounting extends Base {
+    /**
+     * Currency formatted prices, these getters already handle included/excluded tax configuration.
+     * They must be used each time a price is displayed to the user.
+     */
+    get currencyDisplayPrice() {
+        return formatCurrency(this.displayPrice, this.currency.id);
+    }
+
+    get currencyAmountTaxes() {
+        return formatCurrency(this.amountTaxes, this.currency.id);
+    }
+
+    /**
+     * Display price depending on the tax configuration (included or excluded).
+     */
+    get displayPrice() {
+        return this.config.iface_tax_included === "total"
+            ? this.currency.round(this.priceIncl)
+            : this.currency.round(this.priceExcl);
+    }
+
+    /**
+     * Remaining due take into account the rounding of the order, the rounding can be configured
+     * in two different ways:
+     *
+     * 1) Rounding applied only on cash payments
+     *    In this case the remaining due is rounded only if there is at least one cash payment line.
+     *    and the remaining due is less than the rounding tolerance.
+     *
+     * 2) Rounding applied on all payment methods
+     *    In this case the remaining due is always rounded even if a card payment method is used.
+     *    The remaining due is rounded if it is less than the rounding tolerance. No payment method
+     *    is rounded in this case, the whole order is rounded instead.
+     *
+     * !!! Keep in mind that from 19.0 only one cash payment line can be used in an order !!!
+     */
+    get remainingDue() {
+        const isNegative = this.totalDue < 0;
+        const total = this.totalDue;
+        const remaining = total - this.amountPaid;
+
+        // Amount paid covers the total due
+        if ((isNegative && remaining >= 0) || (!isNegative && remaining <= 0)) {
+            return 0;
+        }
+
+        const tolerance = this.orderIsRounded ? this.config.rounding_method.rounding : 0;
+        const amount = Math.abs(total - this.amountPaid) <= tolerance ? 0 : Math.abs(remaining);
+        return isNegative ? this.currency.round(-amount) : this.currency.round(amount);
+    }
+    get change() {
+        const isNegative = this.totalDue < 0;
+        const roundingSanatizer = this.orderIsRounded ? this.appliedRounding : 0;
+        const remaining = this.totalDue - this.amountPaid;
+
+        // Amount paid covers the total due
+        if ((isNegative && remaining <= 0) || (!isNegative && remaining >= 0)) {
+            return 0;
+        }
+
+        const total =
+            Math.abs(this.displayPrice) -
+            Math.abs(this.amountPaid) +
+            (isNegative ? -roundingSanatizer : roundingSanatizer);
+
+        const amount = isNegative ? -this.currency.round(total) : this.currency.round(total);
+        return this.config.cash_rounding ? this.config.rounding_method.round(amount) : amount;
+    }
+    get orderIsRounded() {
+        const cashPm = this.payment_ids.some((p) => p.payment_method_id.is_cash_count);
+        return this.config.hasGlobalRounding || (cashPm && this.config.hasCashRounding);
+    }
+    get appliedRounding() {
+        const total = this.prices.taxDetails.total_amount_no_rounding;
+        const isNegative = this.amountPaid > total;
+        const remaining = total - this.amountPaid;
+        const tolerance = this.orderIsRounded ? this.config.rounding_method.rounding : 0;
+        const amount = Math.abs(total - this.amountPaid) <= tolerance ? Math.abs(remaining) : 0;
+        return isNegative ? this.currency.round(amount) : this.currency.round(-amount);
+    }
+
+    /**
+     * Getters are preferred to methods because they are cached.
+     * These getters must be used each time the order prices are needed.
+     *
+     * Do not try to make your own price computation outside these getters.
+     */
+    get prices() {
+        return this._constructPriceData();
+    }
+    get priceIncl() {
+        return this.prices.taxDetails.total_amount_no_rounding;
+    }
+    get priceExcl() {
+        return this.prices.taxDetails.base_amount;
+    }
+    get totalDue() {
+        return this.config.hasCashRounding
+            ? this.currency.round(this.prices.taxDetails.total_amount_no_rounding)
+            : this.currency.round(this.prices.taxDetails.total_amount);
+    }
+    get amountTaxes() {
+        return this.prices.taxDetails.tax_amount_currency;
+    }
+    get orderHasZeroRemaining() {
+        return this.currency.isZero(this.remainingDue);
+    }
+    get amountPaid() {
+        return this.currency.round(
+            this.payment_ids.reduce(function (sum, paymentLine) {
+                // Return lines are created after the sync, should not be taken into account in
+                // the paid amount otherwise, the change would be wrong.
+                if (paymentLine.isDone() && paymentLine.name !== "return") {
+                    sum += paymentLine.getAmount();
+                }
+                return sum;
+            }, 0)
+        );
+    }
+    get orderSign() {
+        return this.prices.taxDetails.order_sign;
+    }
+
+    /**
+     * Determine if the amount to pay should be rounded depending on the payment method
+     * and the cash rounding configuration.
+     *
+     * Rounding on payment methods happen only when cash_rounding is enabled and
+     * only_round_cash_method is enabled too. In this case only cash payment methods
+     * will have a rounded amount to pay.
+     *
+     * If only_round_cash_method is disabled, no payment method will have a rounded amount to pay.
+     * The whole order is rounded instead.
+     */
+    shouldRound(paymentMethod) {
+        return paymentMethod.is_cash_count && this.config.hasCashRounding;
+    }
+
+    /**
+     * Get the amount to pay by default when creating a new payment.
+     * @param paymentMethod: The payment method of the payment to be created.
+     * @returns A monetary value.
+     */
+    getDefaultAmountDueToPayIn(paymentMethod) {
+        const amount = this.shouldRound(paymentMethod)
+            ? this.config_id.rounding_method.round(this.remainingDue)
+            : this.remainingDue;
+        return amount || this.change;
+    }
+
+    /**
+     * Since prices are computed on the fly, we need to set them before sending
+     * the order to the backend.
+     *
+     * This method is called when the order is pushed to the backend.
+     */
+    setOrderPrices() {
+        this.amount_paid = this.amountPaid; // Already rounded by the getter
+        this.amount_tax = this.amountTaxes; // Already rounded by the getter
+        this.amount_total = this.currency.round(this.priceIncl);
+        this.amount_return = this.change; // Already rounded by the getter
+        this.lines.forEach((line) => {
+            line.price_subtotal = line.displayPriceUnit;
+            line.price_subtotal_incl = line.displayPrice;
+        });
+    }
+    /**
+     * This method is used when extra options need to be passed to the price computation.
+     * All these options will finally reach these methods:
+     * - prepareBaseLineForTaxesComputationExtraValues
+     * - prepare_base_line_for_taxes_computation (accounting helpers)
+     *
+     * For example you can pass a specific set of lines to compute the price of
+     * only these lines instead of the whole order.
+     */
+    getPriceWithOptions(opts = {}) {
+        return this._constructPriceData(opts);
+    }
+
+    /**
+     * @private
+     *
+     * Private method computing all prices and tax details.
+     * DO NOT USE THIS METHOD OUTSIDE THIS FILE !!!
+     */
+    _constructPriceData(opts = {}) {
+        const data = this._computeAllPrices(opts);
+        const noDiscount = this._computeAllPrices({ baseLineOpts: { discount: 0.0 }, ...opts });
+        const currency = this.currency;
+
+        for (const key of Object.keys(data.baseLineByLineUuids)) {
+            const ndData = noDiscount.baseLineByLineUuids[key].tax_details;
+            const dData = data.baseLineByLineUuids[key].tax_details;
+
+            Object.assign(data.baseLineByLineUuids[key].tax_details, {
+                discount_amount: currency.round(ndData.total_included - dData.total_included),
+                no_discount_total_excluded: ndData.total_excluded,
+                no_discount_total_included: ndData.total_included,
+                no_discount_total_included_currency: ndData.total_included_currency,
+                no_discount_total_excluded_currency: ndData.total_excluded_currency,
+                no_discount_taxes_data: ndData.taxes_data,
+                no_discount_delta_total_excluded: ndData.delta_total_excluded,
+                no_discount_delta_total_included: ndData.delta_total_included,
+            });
+        }
+
+        logPosMessage("Accounting", "_constructPriceData", "Recompute allPrices", CONSOLE_COLOR, [
+            data,
+        ]);
+        return data;
+    }
+
+    /**
+     * @private
+     *
+     * Private method computing all prices and tax details.
+     * DO NOT USE THIS METHOD OUTSIDE THIS FILE !!!
+     */
+    _computeAllPrices(opts = {}) {
+        const currency = this.currency;
+        const lines = opts.lines || this.lines;
+        const documentSign = this.isRefund ? -1 : 1;
+        const company = this.company;
+        const baseLines = lines.map((l) =>
+            l.getBaseLine({
+                quantity: l.qty,
+                price_unit: l.price_unit,
+                ...(opts.baseLineOpts || {}),
+            })
+        );
+
+        accountTaxHelpers.add_tax_details_in_base_lines(baseLines, company);
+        accountTaxHelpers.round_base_lines_tax_details(baseLines, company);
+
+        // Cash rounding is added only if the document needs to be globaly rounded.
+        // See cash_rounding and only_round_cash_method config fields.
+        const cashRounding = this.config.cash_rounding ? this.config.rounding_method : null;
+        const data = accountTaxHelpers.get_tax_totals_summary(baseLines, currency, company, {
+            cash_rounding: cashRounding,
+        });
+        const total = data.total_amount_currency - (data.cash_rounding_base_amount_currency || 0.0);
+
+        data.order_sign = documentSign;
+        data.total_amount_no_rounding = total;
+
+        const baseLineByLineUuids = baseLines.reduce((acc, line) => {
+            acc[line.record.uuid] = line;
+            return acc;
+        }, {});
+
+        return { taxDetails: data, baseLines: baseLines, baseLineByLineUuids: baseLineByLineUuids };
+    }
+}

addons/point_of_sale/static/tests/unit/services/pos_service.test.js
@@ -8,21 +8,6 @@ import { imageUrl } from "@web/core/utils/urls";
 definePosModels();
 
 describe("pos_store.js", () => {
-    test("getProductPrice", async () => {
-        const store = await setupPosEnv();
-        const order = store.addNewOrder();
-        const product = store.models["product.template"].get(5);
-        const price = store.getProductPrice(product);
-        expect(price).toBe(3.45);
-        order.setPricelist(null);
-
-        const newPrice = store.getProductPrice(product);
-        expect(newPrice).toBe(115.0);
-
-        const formattedPrice = store.getProductPrice(product, false, true);
-        expect(formattedPrice).toBe("$\u00a0115.00");
-    });
-
     test("setTip", async () => {
         const store = await setupPosEnv();
         const order = await getFilledOrder(store); // Should have 2 lines
@@ -330,7 +315,7 @@ describe("pos_store.js", () => {
         store.selectedCategory = store.models["pos.category"].get(1);
         store.searchProductWord = "TEST";
         products = store.productsToDisplay;
-        expect(products.length).toBe(2);
+        expect(products.length).toBe(4);
         expect(products[0].id).toBe(5);
         expect(products[1].id).toBe(6);
         expect(store.selectedCategory).toBe(undefined);
@@ -348,7 +333,7 @@ describe("pos_store.js", () => {
         let grouped = store.productToDisplayByCateg;
         expect(grouped.length).toBe(1); //Only one group
         expect(grouped[0][0]).toBe(0);
-        expect(grouped[0][1].length).toBe(10); //10 products in same group
+        expect(grouped[0][1].length).toBe(12); //10 products in same group
 
         // Case 2: Grouping enabled
         store.config.iface_group_by_categ = true;

addons/pos_loyalty/static/tests/unit/services/pos_service.test.js
@@ -65,32 +65,4 @@ describe("PosStore - loyalty essentials", () => {
 
         expect(card.id).toBe(2);
     });
-
-    test("preSyncAllOrders", async () => {
-        const store = await setupPosEnv();
-        const models = store.models;
-
-        const coupon = models["loyalty.card"].create({
-            id: -5,
-            code: "X",
-            program_id: 1,
-            points: 0,
-        });
-        const rewardProduct = models["product.product"].get(1);
-
-        const fakeOrderData = {
-            lines: [
-                {
-                    uuid: "uuid-1",
-                    coupon_id: coupon,
-                    _reward_product_id: rewardProduct,
-                },
-            ],
-        };
-
-        await store.preSyncAllOrders([fakeOrderData]);
-
-        expect(store.couponByLineUuidCache["uuid-1"]).toBe(coupon.id);
-        expect(store.rewardProductByLineUuidCache["uuid-1"]).toBe(rewardProduct.id);
-    });
 });

commit 7e530db943c598cf091fe173f760701eef019f51

[FIX] point_of_sale: `price_subtotal` is the total line price not unit
Steps to reproduce
------------------
1. Create a product with price 1, no need to add taxes, and add it to
   PoS
2. In PoS, select this product, and set the quantity to 2, the price is
   2 as expected
3. Finalize the order
4. In the backend, open this order and observe that the "Tax Excl." for
   that line shows $1 instead of $2. While "Tax Incl." shows $2 as
   expected.

The client has reproduced this issue in a different way, by printing the
sales report for that order, he found out that the total is 1 instead of
2.

Why it's happening
------------------
This issue was introduced in 9538698f13d5763b49b00f4c06a1a2afc0d6b39e.
We are setting the field `price_subtotal` to the unit price of the
product, and not to the actual price per line, i.e. we're ignoring the
quantity!!

The Fix
-------
We now set the `price_subtotal` to `priceExcl` which takes into account
the quantity on that line.

opw-5242022

addons/point_of_sale/static/src/app/models/accounting/pos_order_accounting.js
@@ -193,7 +193,7 @@ export class PosOrderAccounting extends Base {
         this.amount_total = this.currency.round(this.priceIncl);
         this.amount_return = this.change; // Already rounded by the getter
         this.lines.forEach((line) => {
-            line.price_subtotal = line.displayPriceUnit;
+            line.price_subtotal = line.priceExcl;
             line.price_subtotal_incl = line.displayPrice;
         });
     }

addons/point_of_sale/static/tests/unit/services/pos_service.test.js
@@ -466,11 +466,11 @@ describe("pos_store.js", () => {
             expect(order.amount_tax).toEqual(2.85);
             expect(order.lines[0].qty).toEqual(3);
             expect(order.lines[0].price_unit).toEqual(3);
-            expect(order.lines[0].price_subtotal).toEqual(3);
+            expect(order.lines[0].price_subtotal).toEqual(9);
             expect(order.lines[0].price_subtotal_incl).toEqual(10.35);
             expect(order.lines[1].qty).toEqual(2);
             expect(order.lines[1].price_unit).toEqual(3);
-            expect(order.lines[1].price_subtotal).toEqual(3);
+            expect(order.lines[1].price_subtotal).toEqual(6);
             expect(order.lines[1].price_subtotal_incl).toEqual(7.5);
 
             order.is_refund = true;
@@ -481,11 +481,11 @@ describe("pos_store.js", () => {
             expect(order.amount_tax).toEqual(-2.85);
             expect(order.lines[0].qty).toEqual(-3);
             expect(order.lines[0].price_unit).toEqual(3);
-            expect(order.lines[0].price_subtotal).toEqual(3);
+            expect(order.lines[0].price_subtotal).toEqual(9);
             expect(order.lines[0].price_subtotal_incl).toEqual(10.35);
             expect(order.lines[1].qty).toEqual(-2);
             expect(order.lines[1].price_unit).toEqual(3);
-            expect(order.lines[1].price_subtotal).toEqual(3);
+            expect(order.lines[1].price_subtotal).toEqual(6);
             expect(order.lines[1].price_subtotal_incl).toEqual(7.5);
         });
     });