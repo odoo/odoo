odoo/odoo

commit f76752a96224f1897e3495077aee8c24d47827a3

[FIX] stock_account: Move description logic to stock.move
The valuation description was done in the product.value object. However
it's very specific to the stock.move and it could be call by different
object. This just move the logic to the appropriate place.

Part-of: odoo/odoo#233488
Related: odoo/enterprise#98289
Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

addons/stock_account/models/stock_move.py
@@ -24,6 +24,10 @@ class StockMove(models.Model):
     value = fields.Monetary(
         "Value", currency_field='company_currency_id',
         help="The current value of the move. It's zero if the move is not valued.")
+    value_justification = fields.Text(
+        "Value Description", compute="_compute_value_justification")
+    value_computed_justification = fields.Text(
+        "Computed Value Description", compute="_compute_value_justification")
     # Useful for testing and custom valuation
     value_manual = fields.Monetary(
         "Manual Value", currency_field='company_currency_id',
@@ -78,6 +82,22 @@ class StockMove(models.Model):
     def _compute_value_manual(self):
         for move in self:
             move.value_manual = move.value
+
+    def _compute_value_justification(self):
+        self.value_justification = False
+        self.value_computed_justification = False
+        for move in self:
+            if not move.is_in:
+                continue
+            move.value_justification = move._get_value_data()['description']
+            computed_value_data = move._get_value_data(ignore_manual_update=True)
+            if computed_value_data['description'] == move.value_justification:
+                move.value_computed_justification = False
+            else:
+                value = move.currency_id.format(computed_value_data['value'])
+                move.value_computed_justification = self.env._(
+                    'Computed value: %(value)s\n%(description)s',
+                    value=value, description=computed_value_data['description'])
 
     @api.depends('quantity', 'product_id.stock_move_ids.value')
     def _compute_remaining_qty(self):

addons/stock_account/models/product_value.py
@@ -66,13 +66,8 @@ class ProductValue(models.Model):
                 product_value.current_value_description = False
                 product_value.computed_value_description = False
                 continue
-            product_value.current_value_description = product_value.move_id._get_value_data()['description']
-            computed_value_data = product_value.move_id._get_value_data(ignore_manual_update=True)
-            if computed_value_data['description'] == product_value.current_value_description:
-                product_value.computed_value_description = False
-            else:
-                value = product_value.currency_id.format(computed_value_data['value'])
-                product_value.computed_value_description = _('Computed value: %(value)s\n%(description)s', value=value, description=computed_value_data['description'])
+            product_value.current_value_description = product_value.move_id.value_justification
+            product_value.computed_value_description = product_value.move_id.value_computed_justification
 
     @api.model_create_multi
     def create(self, vals_list):

commit 1e321fb51b7dd57a2cabdc99843022a2dfdfc18d

[FIX] stock_account: invalid field currency_id
commit f76752a96224f1897e3495077aee8c24d47827a3
Introduced a bug since it uses the field `currency_id` instead of
the correct field `company_currency_id`

closes odoo/odoo#235256

Signed-off-by: William Henrotin (whe) <whe@odoo.com>

addons/stock_account/models/stock_move.py
@@ -94,7 +94,7 @@ class StockMove(models.Model):
             if computed_value_data['description'] == move.value_justification:
                 move.value_computed_justification = False
             else:
-                value = move.currency_id.format(computed_value_data['value'])
+                value = move.company_currency_id.format(computed_value_data['value'])
                 move.value_computed_justification = self.env._(
                     'Computed value: %(value)s\n%(description)s',
                     value=value, description=computed_value_data['description'])