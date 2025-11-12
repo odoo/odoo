f069ae1bc1ae4133e0bce925231cbc16a1384bc7
David Monjoie <dmo@odoo.com>
2025-11-12 13:09:21 +0000

    [FIX] html_editor: fix non-deterministic tests
    Split the test timer between the four tests rather than applying
    a single timer over all of them, for when the runbot is slower.
    
    runbot-233772
    
    X-original-commit: 2346d0048e1287821bf5cf6787f4128b70ebd43a

--- addons/html_editor/static/tests/list/delete_backward.test.js
+++ addons/html_editor/static/tests/list/delete_backward.test.js
@@ -1925,7 +1925,7 @@ describe("Selection collapsed", () => {
             });
         });
         describe("Unordered to checklist", () => {
-            test("should merge an unordered list into an checklist list", async () => {
+            test("should merge an unordered list into an checklist list (1)", async () => {
                 await testEditor({
                     contentBefore:
                         '<ul class="o_checklist"><li class="o_checked">a</li></ul><ul><li>[]b</li></ul>',
@@ -1936,6 +1936,8 @@ describe("Selection collapsed", () => {
                     },
                     contentAfter: '<ul class="o_checklist"><li class="o_checked">a[]b</li></ul>',
                 });
+            });
+            test("should merge an unordered list into an checklist list (2)", async () => {
                 await testEditor({
                     contentBefore:
                         '<ul class="o_checklist"><li class="o_checked">a</li></ul><ul><li><p>[]b</p></li></ul>',
@@ -1947,6 +1949,8 @@ describe("Selection collapsed", () => {
                     // Paragraphs in list items are treated as nonsense.
                     contentAfter: '<ul class="o_checklist"><li class="o_checked">a[]b</li></ul>',
                 });
+            });
+            test("should merge an unordered list into an checklist list (3)", async () => {
                 await testEditor({
                     contentBefore:
                         '<ul class="o_checklist"><li class="o_checked"><p>a</p></li></ul><ul><li>[]b</li></ul>',
@@ -1959,6 +1963,8 @@ describe("Selection collapsed", () => {
                     contentAfter:
                         '<ul class="o_checklist"><li class="o_checked"><p>a[]b</p></li></ul>',
                 });
+            });
+            test("should merge an unordered list into an checklist list (4)", async () => {
                 await testEditor({
                     contentBefore:
                         '<ul class="o_checklist"><li class="o_checked"><p>a</p></li></ul><ul><li><p>[]b</p></li></ul>',
