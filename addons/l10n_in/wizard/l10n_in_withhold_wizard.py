88b11ed3a8cbdab4d389323dfbfc91e9772d57fb
ijja-odoo <ijja@odoo.com>
2025-11-13 05:57:55 +0000

    [FIX] l10n_in_withholding: simplify lower TDS tax warning if PAN missing
    Simplified the condition to show warning whenever the partner’s PAN is missing
    in the TDS entry wizard, instead of checking for lower TDS rate. The warning’s
    purpose is only to alert users to collect PAN from vendors, so rate based check
    was removed.
    
    task-5245353
    
    closes odoo/odoo#240637
    
    X-original-commit: f17ae0ac5b8fb77d1b18844dc54424a65839bd23
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>
    Signed-off-by: Ishan Jigneshkumar Jagani (ijja) <ijja@odoo.com>

--- addons/l10n_in/wizard/l10n_in_withhold_wizard.py
+++ addons/l10n_in/wizard/l10n_in_withhold_wizard.py
@@ -144,10 +144,9 @@ class L10n_InWithholdWizard(models.TransientModel):
     def _compute_l10n_in_withholding_warning(self):
         for wizard in self:
             warnings = {}
-            if wizard.tax_id and wizard.l10n_in_tds_tax_type == 'tds_purchase' and not wizard.related_move_id.commercial_partner_id.l10n_in_pan_entity_id \
-                and wizard.tax_id.amount != max(wizard.tax_id.l10n_in_section_id.l10n_in_section_tax_ids, key=lambda t: abs(t.amount)).amount:
+            if wizard.tax_id and wizard.l10n_in_tds_tax_type == 'tds_purchase' and not wizard.related_move_id.commercial_partner_id.l10n_in_pan_entity_id:
                 warnings['lower_tds_tax'] = {
-                    'message': _("As the Partner's PAN missing/invalid, it's advisable to apply TDS at the higher rate.")
+                    'message': _("Please deduct TDS at higher rate if PAN is missing. Ignore if already applied.")
                 }
             precision = self.currency_id.decimal_places
             if wizard.related_move_id and float_compare(wizard.related_move_id.amount_untaxed, wizard.base, precision_digits=precision) < 0:
