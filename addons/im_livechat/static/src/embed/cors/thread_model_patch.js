33d7e0a5555c9d1e192890fd21869a607e16006f
ramh <ramh@odoo.com>
2025-11-13 12:52:36 +0000

    [IMP] im_livechat: add transcript download button to info panel
    Previously, the 'Download Transcript' button was only visible to visitors at the
    end of a live chat session. This limitation caused confusion since internal
    users could not download the conversation transcript from the interface.
    
    This commit adds the 'download transcript' button to the info panel,
    ensuring it is accessible from within the chat session as well.
    
    Task-5262296
    
    closes odoo/odoo#236754
    
    X-original-commit: 7aa05e492e0e65615d921cf96610b1810c52022d
    Signed-off-by: Haja Ram (ramh) <ramh@odoo.com>

--- addons/im_livechat/static/src/embed/cors/thread_model_patch.js
+++ addons/im_livechat/static/src/embed/cors/thread_model_patch.js
@@ -1,14 +1,14 @@
+import { Thread } from "@mail/core/common/thread_model";
 import { expirableStorage } from "@im_livechat/core/common/expirable_storage";
-import { FeedbackPanel } from "@im_livechat/embed/common/feedback_panel/feedback_panel";
 import { GUEST_TOKEN_STORAGE_KEY } from "@im_livechat/embed/common/store_service_patch";
 
 import { patch } from "@web/core/utils/patch";
 import { url } from "@web/core/utils/urls";
 
-patch(FeedbackPanel.prototype, {
+patch(Thread.prototype, {
     get transcriptUrl() {
         const guestToken = expirableStorage.getItem(GUEST_TOKEN_STORAGE_KEY);
-        return url(`/im_livechat/cors/download_transcript/${this.props.thread.id}`, {
+        return url(`/im_livechat/cors/download_transcript/${this.id}`, {
             guest_token: guestToken,
         });
     },
