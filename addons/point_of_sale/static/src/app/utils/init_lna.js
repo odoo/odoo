c72f119dceb390e8c4e054905f359b835861f0ba
David Monnom <moda@odoo.com>
2025-11-14 09:15:50 +0000

    [IMP] point_of_sale, pos_*: add LNA support to PoS and Self-ordering
    *: pos_self_order, pos_epson_printer
    
    Feature: https://chromestatus.com/feature/5152728072060928
    
    Since Chrome 142, a new feature called “Local Network Access” has been
    added, allowing local IPs to be contacted via HTTP without mixed content
    errors.
    
    This means that certificates are no longer required to contact Epson
    printers or the black box.
    
    closes odoo/odoo#240877
    
    X-original-commit: 825af160288be078bb12b1c20ca5b943c63a9a08
    Signed-off-by: Stéphane Vanmeerhaeghe (stva) <stva@odoo.com>
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

---
+++ addons/point_of_sale/static/src/app/utils/init_lna.js
@@ -0,0 +1,28 @@
+import { _t } from "@web/core/l10n/translation";
+
+export const initLNA = async (notificationService) => {
+    try {
+        const result = await navigator.permissions.query({ name: "local-network-access" });
+        if (["granted", "prompt"].includes(result?.state)) {
+            return;
+        }
+
+        const message = _t(
+            "Local Network Access permission is denied. Some hardware devices might not work properly. Please allow Local Network Access in your browser settings."
+        );
+        notificationService.add(message, { type: "warning" });
+    } catch {
+        const isChromiumBased = navigator.userAgent.includes("Chromium") || !!window.chrome;
+        let message;
+        if (!isChromiumBased) {
+            message = _t(
+                "Local Network Access configuration is enabled, but your browser is not Chromium-based. Please use a Chromium-based browser to benefit from this feature."
+            );
+        } else {
+            message = _t(
+                "Local Network Access is enabled for this POS, but your browser version does not support it. Please update your browser to the latest version."
+            );
+        }
+        notificationService.add(message, { type: "warning" });
+    }
+};
