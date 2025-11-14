df6865cbf707c7741f98b95fd11e95849af2358a
adsh-odoo <adsh@odoo.com>
2025-11-14 12:27:58 +0530

    [IMP] sms_twilio: remove overrides of fields_get
    - Remove the overrides of fields_get, which were added here https://github.com/odoo/odoo/pull/225036/commits/c4255062ecc2789522516ab988b0474f6b49f589 for
      translation purposes in a stable.
    
    Task-5085099

--- addons/mass_mailing_sms/models/mailing_trace.py
+++ addons/mass_mailing_sms/models/mailing_trace.py
@@ -70,34 +70,6 @@ class MailingTrace(models.Model):
                 values['sms_code'] = self._get_random_code()
         return super().create(vals_list)
 
-    @api.model
-    def fields_get(self, allfields=None, attributes=None):
-        # As we are adding keys in stable, better be sure no-one is getting crashes
-        # due to missing translations
-        # TODO: remove in master
-        res = super().fields_get(allfields=allfields, attributes=attributes)
-
-        existing_selection = res.get('failure_type', {}).get('selection')
-        if existing_selection is None:
-            return res
-
-        updated_stable = {
-            'twilio_authentication', 'twilio_callback',
-            'twilio_from_missing', 'twilio_from_to',
-        }
-        need_update = updated_stable - set(dict(self._fields['failure_type'].selection))
-        if need_update:
-            self.env['ir.model.fields'].invalidate_model(['selection_ids'])
-            self.env['ir.model.fields.selection']._update_selection(
-                self._name,
-                'failure_type',
-                self._fields['failure_type'].selection,
-            )
-            self.env.registry.clear_cache()
-            return super().fields_get(allfields=allfields, attributes=attributes)
-
-        return res
-
     def _get_random_code(self):
         """ Generate a random code for trace. Uniqueness is not really necessary
         as it serves as obfuscation when unsubscribing. A valid trio
--- addons/sms_twilio/models/mail_notification.py
+++ addons/sms_twilio/models/mail_notification.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class MailNotification(models.Model):
@@ -13,34 +13,3 @@ class MailNotification(models.Model):
             ('twilio_wrong_credentials', 'Twilio Wrong Credentials'),
         ],
     )
-
-    # CRUD
-    # ------------------------------------------------------------
-
-    @api.model
-    def fields_get(self, allfields=None, attributes=None):
-        # As we are adding keys in stable, better be sure no-one is getting crashes
-        # due to missing translations
-        # TODO: remove in master
-        res = super().fields_get(allfields=allfields, attributes=attributes)
-
-        existing_selection = res.get('failure_type', {}).get('selection')
-        if existing_selection is None:
-            return res
-
-        updated_stable = {
-            'twilio_authentication', 'twilio_callback',
-            'twilio_from_missing', 'twilio_from_to',
-        }
-        need_update = updated_stable - set(dict(self._fields['failure_type'].selection))
-        if need_update:
-            self.env['ir.model.fields'].invalidate_model(['selection_ids'])
-            self.env['ir.model.fields.selection']._update_selection(
-                self._name,
-                'failure_type',
-                self._fields['failure_type'].selection,
-            )
-            self.env.registry.clear_cache()
-            return super().fields_get(allfields=allfields, attributes=attributes)
-
-        return res
