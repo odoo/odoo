c652931593f03b2a15b2c8ebdbba131da4e3c343
Alex Kühn <aku@odoo.com>
2025-11-14 18:07:49 +0000

    [FIX] mail: can add message reaction in mobile
    Before this commit, mobile uses could not practically add reactions.
    
    Steps to reproduce:
    - open a discuss conversation on mobile device
    - post a message
    - click on "..."
    - click on "Add a reaction"
    => No emoji picker is shown.
    
    This happens because the emoji picker opens in a modal in mobile. The
    modal has a z-index lower than chat window, and because of this it
    is actually shown below the chat window.
    
    The chat window being above modal is sometimes desirable, like for
    AI chat windows triggered from a modal in desktop, but sometimes the
    opposite is desirable, like in mobile. Chat window had z-index for
    desktop use of above modal, but in mobile the opposite is desirable.
    
    This commit fixes the issue by reducing the chat window z-index in
    mobile, so that modals are above chat windows.
    
    Note that current desktop style for chat window being necessarily
    above modals is not exactly correct, but this is a tricker part to
    fix therefore this PR focuses on the immediate usability issue in
    mobile that makes using any modal in chat window unusable.
    
    Task-5261880
    
    This PR also disables the showing of chat bubbles in discuss app
    similarly to the desktop counter-part.
    
    Task-4607436
    
    X-original-commit: 26a5384af0af8fc6e6b5a10bea277f937e2b3481
    Part-of: odoo/odoo#235946
    Related: odoo/enterprise#99636
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

---
+++ addons/mail/static/tests/mail_shared_tests.js
@@ -0,0 +1,35 @@
+import { click, contains, openDiscuss, start, startServer } from "@mail/../tests/mail_test_helpers";
+import { expect, mockTouch, mockUserAgent, queryFirst } from "@odoo/hoot";
+
+export async function mailCanAddMessageReactionMobile() {
+    mockTouch(true);
+    mockUserAgent("android");
+    const pyEnv = await startServer();
+    const channelId = pyEnv["discuss.channel"].create({ name: "General" });
+    pyEnv["mail.message"].create({
+        body: "Hello world",
+        res_id: channelId,
+        message_type: "comment",
+        model: "discuss.channel",
+    });
+    await start();
+    await openDiscuss(channelId);
+    await contains(".o-mail-Message", { text: "Hello world" });
+    await click(".o-mail-Message [title='Expand']");
+    await click(".o-dropdown-item:contains('Add a Reaction')");
+    await contains(".o-overlay-item:has(.modal .o-EmojiPicker)");
+    const emojiPickerZIndex = parseInt(
+        getComputedStyle(queryFirst(".o-overlay-item:has(.modal .o-EmojiPicker)")).zIndex
+    );
+    const chatWindowZIndex = parseInt(getComputedStyle(queryFirst(".o-mail-ChatWindow")).zIndex);
+    expect(chatWindowZIndex).toBeLessThan(emojiPickerZIndex, {
+        message: "emoji picker modal should be above chat window",
+    });
+    await click(".modal .o-EmojiPicker .o-Emoji:contains('😀')");
+    await contains(".o-mail-MessageReaction:contains('😀')");
+    // Can quickly add new reactions
+    await click(".o-mail-MessageReactions button[title='Add a Reaction']");
+    await click(".modal .o-EmojiPicker .o-Emoji:contains('🤣')");
+    await contains(".o-mail-MessageReaction:contains('🤣')");
+    await contains(".o-mail-MessageReaction:contains('😀')");
+}
