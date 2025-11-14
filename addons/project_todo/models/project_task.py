75aff266276a3caede44a9b31cce785be5fcf98b
maap-odoo <maap@odoo.com>
2025-11-14 14:09:20 +0530

    [IMP] project_todo: hide “View Task” button for portal users in emails
    This commit hides the “View Task” button from To-Do notification emails sent to
    portal users. Since portal users do not have access to view To-Dos, the button
    is no longer displayed for them.
    
    task-4908548

--- addons/project_todo/models/project_task.py
+++ addons/project_todo/models/project_task.py
@@ -46,3 +46,14 @@ class ProjectTask(models.Model):
             (self.env['ir.model.data']._xmlid_to_res_id("project_todo.project_task_view_todo_calendar"), "calendar"),
             (self.env['ir.model.data']._xmlid_to_res_id("project_todo.project_task_view_todo_activity"), "activity"),
         ]
+
+    def _notify_get_recipients_groups(self, message, model_description, msg_vals=False):
+        groups = super()._notify_get_recipients_groups(
+            message, model_description, msg_vals=msg_vals
+        )
+        if not self.project_id:
+            for group_name, _group_method, group_data in groups:
+                if group_name == 'portal':
+                    group_data['has_button_access'] = False
+
+        return groups
--- addons/project_todo/tests/__init__.py
+++ addons/project_todo/tests/__init__.py
@@ -2,6 +2,7 @@
 
 from . import test_access_rights
 from . import test_mail_activity_todo_create
+from . import test_todo_mail_features
 from . import test_todo_onboarding_for_users
 from . import test_mail_activity_systray_counter
 from . import test_todo_ui
