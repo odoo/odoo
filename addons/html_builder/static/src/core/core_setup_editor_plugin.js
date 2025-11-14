1159f98c8c70d805a5a5dff6500d739713afcfe6
Louis (loco) <loco@odoo.com>
2025-11-14 15:01:47 +0000

    [FIX] html_builder,  website: allow user to translate a field
    Steps to reproduce the problem:
    - With french installed on a website, go to `/fr/shop`.
    - Enter in translate mode.
    
    -> You can not edit product names.
    
    The problem is that since [1], regular fields are not translatable. This
    commit does two things:
    - It modifies the `o_editable_selectors` selector in edit mode to allow
    translation of regular fields.
    - Because we revert the logic of [1], the problem that was solved by
    this commit (translation of a mega menu item) still has to be fixed. To
    do so, the `o_editable` or `o_editable_attribute` is removed when
    setting the `data-oe-readonly` attribute on cascaded branded elements.
    The idea is to follow the same logic than in the `SetupEditorPlugin`
    where elements with the `data-oe-readonly` attribute are filtered before
    adding the `o_editable` class.
    
    task-5265949
    
    [1]: https://github.com/odoo/odoo/commit/d4d428ff1d5be46135973aa806f206a1076bfcf7
    
    closes odoo/odoo#236595
    
    X-original-commit: f09dc4d9d35e1e3707c9483fc6004cf162a3f4ca
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>

--- addons/html_builder/static/src/core/core_setup_editor_plugin.js
+++ addons/html_builder/static/src/core/core_setup_editor_plugin.js
@@ -1,6 +1,7 @@
 import { Plugin } from "@html_editor/plugin";
 
 export class CoreSetupEditorPlugin extends Plugin {
+    // TODO: remove in master
     static id = "core_setup_editor_plugin";
     /** @type {import("plugins").BuilderResources} */
     resources = {
