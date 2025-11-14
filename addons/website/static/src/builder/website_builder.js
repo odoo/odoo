d858357a1116f5352498293731ecf48fea63d837
Louis (loco) <loco@odoo.com>
2025-11-14 15:01:47 +0000

    [FIX] html_builder,  website: allow user to translate a field
    Steps to reproduce the problem:
    - With french installed on a website, go to `/fr/shop`.
    - Enter in translate mode.
    
    -> You can not edit product names.
    
    The problem is that since [1], regular fields are not translatable. This
    commit does two things:
    - It modifies the `o_editable_selectors` selector in edit mode to allow
    translation of regular fields.
    - Because we revert the logic of [1], the problem that was solved by
    this commit (translation of a mega menu item) still has to be fixed. To
    do so, the `o_editable` or `o_editable_attribute` is removed when
    setting the `data-oe-readonly` attribute on cascaded branded elements.
    The idea is to follow the same logic than in the `SetupEditorPlugin`
    where elements with the `data-oe-readonly` attribute are filtered before
    adding the `o_editable` class.
    
    task-5265949
    
    [1]: https://github.com/odoo/odoo/commit/d4d428ff1d5be46135973aa806f206a1076bfcf7
    
    X-original-commit: f09dc4d9d35e1e3707c9483fc6004cf162a3f4ca

--- addons/website/static/src/builder/website_builder.js
+++ addons/website/static/src/builder/website_builder.js
@@ -5,7 +5,6 @@ import { DisableSnippetsPlugin } from "@html_builder/core/disable_snippets_plugi
 import { OperationPlugin } from "@html_builder/core/operation_plugin";
 import { SavePlugin } from "@html_builder/core/save_plugin";
 import { SetupEditorPlugin } from "@html_builder/core/setup_editor_plugin";
-import { TranslateSetupEditorPlugin } from "./plugins/translate_setup_editor_plugin";
 import { VisibilityPlugin } from "@html_builder/core/visibility_plugin";
 import { removePlugins } from "@html_builder/utils/utils";
 import { closestElement } from "@html_editor/utils/dom_traversal";
@@ -54,7 +53,6 @@ const TRANSLATION_PLUGINS = [
     DisableSnippetsPlugin,
     SavePlugin,
     SetupEditorPlugin,
-    TranslateSetupEditorPlugin,
     WebsiteSetupEditorPlugin,
     VisibilityPlugin,
     PopupVisibilityPlugin,
