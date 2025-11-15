676ec9e4d609d18a2d57b97fa6ce00bced156bc7
Ajit Singh <aksi@odoo.com>
2025-11-15 07:09:40 +0000

    [FIX] html_editor: add end tags to element except void elements
    **Step to Reproduce:**
    - install Subscription (with demo data)
    - try to edit `Subscription: Payment Reminder` email template
    
    **Observation:**
    - Traceback for faulty template
    
    **Cause**
    For outgoing mails, we are using output_method = 'xml' when normalizing html
    content. when this content is parsed using DOMParser in browser,
    
    we get different result.
    For a very basic template like this:
    ```
    <div>
        <t t-if="ctx.get('error')">
            <pre t-out="ctx['error'] or ''" />.
        </t>
        <t t-else="">
            <span>some text</span>
        </t>
    </div>
    ```
    when parsed using Domparser(), return a faulty template:
    ```
    <div>
        <t t-if="ctx.get('error')">
            <pre t-out="ctx['error'] or ''">.
            <t t-else="">
                <span>some text</span>
        </t>
    </pre>
        </t>
    </div>
    ```
    
    Issue roots because of use of self-closing tags, which are valid for xml but not
    for html
    
    **Fix:**
    - we forcefully replace all self-closing tags(which are not void elements)
    with a closing tag.
    
    opw-5234345
    
    X-original-commit: 17b47c6690875feebf000e2374c169cca3bd290a

--- addons/html_editor/static/src/utils/sanitize.js
+++ addons/html_editor/static/src/utils/sanitize.js
@@ -32,8 +32,8 @@ export function initElementForEdition(element, options = {}) {
 }
 
 /**
- * Properly close common XML-like self-closing elements to avoid HTML parsing
- * issues.
+ * Converts XML-style self-closing tags (e.g., <div/> <span/> <t/>) into proper
+ * HTML start/end tag pairs, except for true HTML void elements.
  *
  * @param {string | ReturnType<markup>} content
  * @returns {ReturnType<markup>}
@@ -42,8 +42,11 @@ export function fixInvalidHTML(content) {
     if (!content) {
         return content;
     }
+    // Match self-closing tags EXCEPT HTML void elements.
+    // We do not use selfClosingElementTags because it includes XML-only tags
+    // such as <t>, which must not be treated as void in HTML.
     const regex =
-        /<\s*(a|strong|t|span)\s*((?:(?:\s+[\w:-]+(?:\s*=\s*(?:"[^"]*"|'[^']*'|[^\s"'=<>`]+))?)*))\s*\/>/g;
+        /<\s*(?!area\b|base\b|br\b|col\b|embed\b|hr\b|img\b|input\b|link\b|meta\b|param\b|source\b|track\b|wbr\b)([a-zA-Z0-9:-]+)\s*((?:(?:\s+[\w:-]+(?:\s*=\s*(?:"[^"]*"|'[^']*'|[^\s"'=<>`]+))?)*))\s*\/>/g;
     return htmlReplace(content, regex, (match, tag, attributes) => {
         // markup: content is either already markup or escaped in htmlReplace
         attributes = markup(attributes);
