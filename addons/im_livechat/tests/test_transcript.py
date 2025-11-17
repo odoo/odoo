3e378de80ca935b21ad6f2848260c3512d48f0c4
Giorgio Tocco (gito) <gito@odoo.com>
2025-11-17 13:53:18 +0100

    [FIX] im_livechat: restrict emailing transcript to internal users
    This commit restricts the possibility of emailing a transcript of the
    live chat conversation to internal users.
    
    task-5262277

--- addons/im_livechat/tests/test_transcript.py
+++ addons/im_livechat/tests/test_transcript.py
@@ -1,11 +1,11 @@
-from odoo.tests.common import tagged
+from odoo.tests.common import JsonRpcException, tagged
 from odoo.tools import mute_logger
-from odoo.addons.base.tests.common import HttpCaseWithUserDemo
+from odoo.addons.base.tests.common import HttpCaseWithUserDemo, HttpCaseWithUserPortal
 from odoo.addons.im_livechat.tests.common import TestImLivechatCommon
 
 
 @tagged("-at_install", "post_install")
-class TestImLivechatTranscript(TestImLivechatCommon, HttpCaseWithUserDemo):
+class TestImLivechatTranscript(TestImLivechatCommon, HttpCaseWithUserDemo, HttpCaseWithUserPortal):
     def test_download_transcript(self):
         data = self.make_jsonrpc_request(
             "/im_livechat/get_session",
@@ -27,3 +27,15 @@ class TestImLivechatTranscript(TestImLivechatCommon, HttpCaseWithUserDemo):
         with mute_logger("odoo.http"):
             res = self.url_open(f"/im_livechat/download_transcript/{data['channel_id']}")
         self.assertEqual(res.status_code, 404)
+
+    def test_email_transcript_portal_user(self):
+        self.authenticate(self.user_portal.login, self.user_portal.login)
+        data = self.make_jsonrpc_request(
+            "/im_livechat/get_session",
+            {"channel_id": self.livechat_channel.id},
+        )
+        with self.assertRaises(JsonRpcException, msg="werkzeug.exceptions.NotFound"):
+            self.make_jsonrpc_request(
+                "/im_livechat/email_livechat_transcript",
+                {"channel_id": data["channel_id"], "email": self.partner_portal.email},
+            )
