252e28f2212f02eb6d2975a01fc28f68dbb172e2
Krzysztof Magusiak (krma) <krma@odoo.com>
2025-11-18 09:03:35 +0000

    [REF] base, web: company_logo as attachment
    Store as attachment means the file is on disk and can be streamed by the
    web server that is in from of Odoo. This also avoids decoding base64 and
    guessing the mimetype, however the attachment table is bigger, but we
    keep a single query.
    
    closes odoo/odoo#236225
    
    Signed-off-by: Julien Castiaux (juc) <juc@odoo.com>

--- addons/web/controllers/binary.py
+++ addons/web/controllers/binary.py
@@ -264,43 +264,28 @@ class Binary(http.Controller):
         imgname = 'logo'
         imgext = '.png'
         dbname = request.db
-        uid = (request.session.uid if dbname else None) or odoo.SUPERUSER_ID
 
         if not dbname:
             response = http.Stream.from_path(file_path('web/static/img/logo.png')).get_response()
         else:
             try:
-                company = int(kw['company']) if kw and kw.get('company') else False
-                if company:
-                    request.env.cr.execute("""
-                        SELECT logo_web, write_date
-                          FROM res_company
-                         WHERE id = %s
-                    """, (company,))
+                attachment = request.env(user=request.session.uid or odoo.api.SUPERUSER_ID, su=True)['ir.attachment']
+                if company_id := (kw or {}).get('company'):
+                    company_ids = [int(company_id)]
                 else:
-                    request.env.cr.execute("""
-                        SELECT c.logo_web, c.write_date
-                          FROM res_users u
-                     LEFT JOIN res_company c
-                            ON c.id = u.company_id
-                         WHERE u.id = %s
-                    """, (uid,))
-                row = request.env.cr.fetchone()
-                if row and row[0]:
-                    image_base64 = base64.b64decode(row[0])
-                    image_data = io.BytesIO(image_base64)
-                    mimetype = guess_mimetype(image_base64, default='image/png')
-                    imgext = '.' + mimetype.split('/')[1]
-                    if imgext == '.svg+xml':
-                        imgext = '.svg'
-                    response = send_file(
-                        image_data,
-                        request.httprequest.environ,
-                        download_name=imgname + imgext,
-                        mimetype=mimetype,
-                        last_modified=row[1],
-                        response_class=Response,
-                    )
+                    # keeping a single query
+                    company_ids = attachment.env.user._as_query(ordered=False).subselect('company_id')
+                attachment = attachment.search_fetch([
+                    ('res_model', '=', 'res.company'),
+                    ('res_field', '=', 'logo_web'),
+                    ('res_id', 'in', company_ids),
+                ], limit=1)
+                if attachment:
+                    stream = attachment._to_http_stream()
+                    download_ext = '.svg' if '/svg' in attachment.mimetype else imgext
+                    stream.download_name = f'{imgname}{download_ext}'
+                    stream.public = True
+                    response = stream.get_response()
                 else:
                     response = http.Stream.from_path(file_path('web/static/img/nologo.png')).get_response()
             except Exception:
