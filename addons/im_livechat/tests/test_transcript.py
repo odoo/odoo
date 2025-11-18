8fedd6674a3208953ceb343c0f2c252c01d49702
Giorgio Tocco (gito) <gito@odoo.com>
2025-11-18 10:54:35 +0100

    [FIX] im_livechat: restrict emailing transcript to internal users
    This commit restricts the possibility of emailing a transcript of the
    live chat conversation to internal users.
    
    task-5262277
    
    closes odoo/odoo#236147
    
    X-original-commit: fc3642aa778f339133e2fc10c5c38f6ac3239885
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>
    Signed-off-by: Giorgio Tocco (gito) <gito@odoo.com>

--- addons/im_livechat/tests/test_transcript.py
+++ addons/im_livechat/tests/test_transcript.py
@@ -1,11 +1,12 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from odoo.tests.common import JsonRpcException
 from odoo.tools import mute_logger
-from odoo.addons.base.tests.common import HttpCaseWithUserDemo
+from odoo.addons.base.tests.common import HttpCaseWithUserDemo, HttpCaseWithUserPortal
 from odoo.addons.im_livechat.tests.common import TestImLivechatCommon
 
 
-class TestImLivechatTranscript(TestImLivechatCommon, HttpCaseWithUserDemo):
+class TestImLivechatTranscript(TestImLivechatCommon, HttpCaseWithUserDemo, HttpCaseWithUserPortal):
     def test_download_transcript(self):
         data = self.make_jsonrpc_request(
             "/im_livechat/get_session",
@@ -27,3 +28,15 @@ class TestImLivechatTranscript(TestImLivechatCommon, HttpCaseWithUserDemo):
         with mute_logger("odoo.http"):
             res = self.url_open(f"/im_livechat/download_transcript/{data['channel_id']}")
         self.assertEqual(res.status_code, 404)
+
+    def test_email_transcript_portal_user(self):
+        self.authenticate(self.user_portal.login, self.user_portal.login)
+        data = self.make_jsonrpc_request(
+            "/im_livechat/get_session",
+            {"channel_id": self.livechat_channel.id},
+        )
+        with self.assertRaises(JsonRpcException, msg="werkzeug.exceptions.NotFound"):
+            self.make_jsonrpc_request(
+                "/im_livechat/email_livechat_transcript",
+                {"channel_id": data["channel_id"], "email": self.partner_portal.email},
+            )
