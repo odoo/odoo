1ea2c431713498f6a80f9dce28ae42c85a4d4de2
mojem <mojem@odoo.com>
2025-11-18 12:15:10 +0000

    [FIX] purchase_mrp: UoM category error on kit
    Steps to reproduce
    ------------------
    
    1. Enable Units of Measure and Automatic Valuation.
    2. Create:
      Product KIT, stockable, UoM category Unit, UoM = Units.
      BoM for KIT with at least one component whose UoM is in a different
         category (e.g. m from Length).
    3. Go to the product's category and set the Costing Method to Average
       Cost (AVCO) and the Inventory Valuation to Automated.
    4. Create a PO for KIT in a currency different from the company currency.
    5. Confirm the PO and validate the receipt.
    
    Issue
    -----
    
    Validating the receipt raises:
    
    > The unit of measure m defined on the order line doesn't belong to the
    > same category as the unit of measure kit defined on the product…
    
    If you keep the PO currency equal to the company currency, the same kit
    and BoM work and the receipt posts correctly.
    
    Cause of the issue
    ------------------
    
    Validating the receipt will call the `_action_done` of stock.move's and generate the related accounting entries. During this call and the currency of the PO is different from the company currency the `_generate_valuation_lines_data` will call the `_get_currency_convert_date` method:
    https://github.com/odoo/odoo/blob/751d54207c6214a25a5a1def57137e2f2f9106e3/addons/purchase_stock/models/stock_move.py#L134-L140
    This call will in turn call the `_get_qty_received_without_self`:
    https://github.com/odoo/odoo/blob/751d54207c6214a25a5a1def57137e2f2f9106e3/addons/purchase_stock/models/stock_move.py#L121-L122
    which was not written to handle kit products since it assumes that the product of the PO is the same as the one of the related move:
    https://github.com/odoo/odoo/blob/751d54207c6214a25a5a1def57137e2f2f9106e3/addons/purchase_stock/models/stock_move.py#L102-L108
    
    Fix
    ---
    
    The qty_received is relevant to the _get_currency_convert_date as the method compares the qty_invoiced with the qty_received to determine whether to use the Invoice Date (when qty_invoiced > qty_received) or the Receipt Date.
    
    https://github.com/odoo/odoo/blob/888e086dc6c7823b07993e90f70e2849e988fa7a/addons/purchase_stock/models/stock_move.py#L122-L126
    
      For kits, `qty_received` must be calculated by aggregating component
      moves to accurately determine this status. Since the standard logic
      crashes due to UoM mismatch, the override in `purchase_mrp` is
      necessary to provide the correct quantity for this date selection.
    
    opw-5030761
    
    closes odoo/odoo#248318
    
    X-original-commit: c9339e560e4e39ef35a253d77fb78b70da445c3b
    Signed-off-by: Lancelot Semal (lase) <lase@odoo.com>
    Signed-off-by: Mohamed Jemai (mojem) <mojem@odoo.com>

--- addons/purchase_mrp/tests/test_anglo_saxon_valuation.py
+++ addons/purchase_mrp/tests/test_anglo_saxon_valuation.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo.fields import Date, Datetime
+from odoo.fields import Command, Date, Datetime
 from odoo.tools import mute_logger
 from odoo.tests import Form, tagged
 from odoo.addons.account.tests.common import AccountTestInvoicingCommon
@@ -232,6 +232,61 @@ class TestAngloSaxonValuationPurchaseMRP(AccountTestInvoicingCommon):
         self.assertEqual(svl.value, 50)  # USD
         self.assertEqual(input_aml.amount_currency, 100)  # EUR
         self.assertEqual(input_aml.balance, 50)  # USD
+
+    def test_multicurrency_kit_different_uom_categories(self):
+        """
+            Create a kit with an UoM belonging to a different category than its component UoM.
+            Purchase that kit in a different currency than the company currency and validate the receipt.
+            Check the generated account entries.
+        """
+        eur = self.env.ref('base.EUR')
+
+        uom_unit = self.env.ref('uom.product_uom_unit')
+        uom_meter = self.env.ref('uom.product_uom_meter')
+
+        kit, component = self.env['product.product'].create([
+            {
+                'name': 'Kit multi UoM',
+                'is_storable': True,
+                'uom_id': uom_unit.id,
+                'categ_id': self.avco_category.id,
+            },
+            {
+                'name': 'Component meter',
+                'is_storable': True,
+                'uom_id': uom_meter.id,
+                'categ_id': self.avco_category.id,
+            },
+        ])
+
+        self.env['mrp.bom'].create({
+            'product_tmpl_id': kit.product_tmpl_id.id,
+            'product_uom_id': uom_unit.id,
+            'product_qty': 1.0,
+            'type': 'phantom',
+            'bom_line_ids': [Command.create({
+                'product_id': component.id,
+                'product_qty': 1.0,
+                'product_uom_id': uom_meter.id,
+            })],
+        })
+
+        po = self.env['purchase.order'].create({
+            'partner_id': self.vendor01.id,
+            'currency_id': eur.id,
+            'order_line': [Command.create({
+                'product_id': kit.id,
+                'product_qty': 1.0,
+                'product_uom_id': kit.uom_id.id,
+                'price_unit': 100.0,
+            })],
+        })
+        po.button_confirm()
+
+        receipt = po.picking_ids
+        receipt.button_validate()
+
+        self.assertEqual(receipt.move_ids.state, 'done')
 
     def test_fifo_cost_adjust_mo_quantity(self):
         """ An MO using a FIFO cost method product as a component should not zero-out the std cost
