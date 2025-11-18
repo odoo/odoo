e8e65d1da7f4b4daa55e0024d439c70d2a1b32ee
Parth Pujara <ptpu@odoo.com>
2025-11-18 22:48:50 +0530

    [IMP] website: auto-focus first invalid field on form submission
    Improve website form usability by automatically focusing and scrolling
    to the first invalid field when validation fails. This helps users
    quickly identify and correct errors, enhancing the overall form
    interaction experience.
    
    task-4629851
    
    closes odoo/odoo#226417
    
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/website/static/src/snippets/s_website_form/form.js
+++ addons/website/static/src/snippets/s_website_form/form.js
@@ -550,6 +550,7 @@ export class Form extends Interaction {
 
     checkErrorFields(errorFields) {
         let formValid = true;
+        let firstInvalidInput = null;
         // Loop on all fields
         for (const fieldEl of this.el.querySelectorAll(".form-field, .s_website_form_field")) {
             // !compatibility
@@ -644,8 +645,16 @@ export class Form extends Interaction {
                     });
                     popover.show();
                 }
+                if (!firstInvalidInput) {
+                    firstInvalidInput = invalidInputs[0] || controlEls[0];
+                }
                 formValid = false;
             }
+        }
+        // Highlight the first invalid field
+        if (firstInvalidInput) {
+            firstInvalidInput.focus();
+            firstInvalidInput.scrollIntoView({ behavior: "smooth", block: "center" });
         }
         return formValid;
     }
