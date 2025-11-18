f0bf7c6b2905da991e60c6d12e47bae5e4a18734
Maryam Kia <maki@odoo.com>
2025-11-18 10:14:20 +0100

    [FIX] rating, portal_rating: move the override to the correct folder
    Since #234356, `_get_allowed_message_post_params` method of the `PortalChatter`
    has been overridden in the `rating` module. As the `rating` has no dependency on
     the portal, the current change moves this override to the `portal_rating`
     module.
    
    closes odoo/odoo#236249
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/rating/controllers/portal_chatter.py
+++ addons/rating/controllers/portal_chatter.py
@@ -1,13 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo.osv import expression
-
 from odoo.addons.portal.controllers import mail
 
 
 class PortalChatter(mail.PortalChatter):
-
-    def _get_non_empty_message_domain(self):
-        return expression.OR(
-            [super()._get_non_empty_message_domain(), [("rating_value", "!=", False)]]
-        )
+    pass
