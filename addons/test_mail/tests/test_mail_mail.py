f511af814ac43d0b2ff5704d5ef6d33efb07b90a
Xavier Morel <xmo@odoo.com>
2025-11-18 10:57:28 +0000

    [FIX] *: support for pylint 4
    - astroid 4 deprecates toplevel exports of nodes, thankfully that was
      never actually necessary so we can just import that unconditionally
    - remove support for pre-jammy pylint / astroid, specifically
      `astroid.nodes` was added in astroid 2.7.0 and
      `astroid.node_classes` deprecated then and removed in 3.0, this can
      affect Bullseye users as it shipped with astroid 2.5
    - Astroid 4 changes `spec.Finder.find_module` in order to cache it
      (pylint-dev/astroid#2509), we can just make our method static for
      all versions as we don't need `self` anyway.
    - The mail test triggers `function-redefined` (E0102), fix it.
    - Skip the escpos script thing which triggers a bunch of
      `undefined-variable` (E0602) false positives.
    
    closes odoo/odoo#236682
    
    X-original-commit: 70bfdf4ac825f9456288881c8707027aaa339d63
    Related: odoo/enterprise#100026
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- addons/test_mail/tests/test_mail_mail.py
+++ addons/test_mail/tests/test_mail_mail.py
@@ -1,3 +1,4 @@
+
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import pytz
@@ -740,6 +741,8 @@ class TestMailMail(MailCommon):
         # SMTP sending issues
         with self.mock_mail_gateway():
             _send_current = self.send_email_mocked.side_effect
+            self.addCleanup(setattr, self.send_email_mocked, 'side_effect', _send_current)
+
             self._reset_data()
             mail.write({'email_to': 'test@example.com'})
 
@@ -747,9 +750,7 @@ class TestMailMail(MailCommon):
             for error, error_class in [
                     (smtplib.SMTPServerDisconnected("Some exception"), smtplib.SMTPServerDisconnected),
                     (MemoryError("Some exception"), MemoryError)]:
-                def _send_email(*args, **kwargs):
-                    raise error
-                self.send_email_mocked.side_effect = _send_email
+                self.send_email_mocked.side_effect = error
 
                 with self.assertRaises(error_class):
                     mail.send(raise_exception=False)
@@ -765,9 +766,7 @@ class TestMailMail(MailCommon):
             for error, msg in [
                     (MailDeliveryException("Some exception"), 'Some exception'),
                     (ValueError("Unexpected issue"), 'Unexpected issue')]:
-                def _send_email(*args, **kwargs):
-                    raise error
-                self.send_email_mocked.side_effect = _send_email
+                self.send_email_mocked.side_effect = error
 
                 self._reset_data()
                 mail.send(raise_exception=False)
@@ -777,8 +776,6 @@ class TestMailMail(MailCommon):
                 self.assertEqual(notification.failure_reason, msg)
                 self.assertEqual(notification.failure_type, 'unknown', 'Mail: generic failure type')
                 self.assertEqual(notification.notification_status, 'exception')
-
-            self.send_email_mocked.side_effect = _send_current
 
     def test_mail_mail_values_misc(self):
         """ Test various values on mail.mail, notably default values """
