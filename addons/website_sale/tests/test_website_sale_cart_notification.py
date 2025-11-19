650e15e92d537537e8a93d0a5997f527043d69c1
Patricia Daloze (pda) <pda@odoo.com>
2025-11-19 13:00:31 +0100

    [FIX] website_sale: notification with price incl/excl tax
    The displayed price in the notification pop-up has to follow the website
    settings (included/excluded tax) and not the backend invoicing settings.
    
    Part-of: odoo/odoo#226320
    Signed-off-by: Patricia Daloze (pda) <pda@odoo.com>

--- addons/website_sale/tests/test_website_sale_cart_notification.py
+++ addons/website_sale/tests/test_website_sale_cart_notification.py
@@ -4,7 +4,6 @@ from odoo.fields import Command
 from odoo.tests import HttpCase, tagged
 
 from odoo.addons.product.tests.common import ProductVariantsCommon
-from odoo.addons.website_sale.controllers.cart import Cart
 
 
 @tagged('post_install', '-at_install')
@@ -13,15 +12,15 @@ class TestWebsiteSaleCartNotification(HttpCase, ProductVariantsCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.WebsiteSaleCartController = Cart()
+        cls.website = cls.env.company.website_id
         cls.size_attribute.create_variant = 'no_variant'
-        cls.env['product.template'].create({
+        cls.product_tmpl_1 = cls.env['product.template'].create({
             'name': 'website_sale_cart_notification_product_1',
             'type': 'consu',
             'website_published': True,
             'list_price': 1000,
         })
-        cls.env['product.template'].create({
+        cls.product_tmpl_2 = cls.env['product.template'].create({
             'name': 'website_sale_cart_notification_product_2',
             'type': 'consu',
             'website_published': True,
@@ -36,9 +35,15 @@ class TestWebsiteSaleCartNotification(HttpCase, ProductVariantsCommon):
             })],
         })
 
-    def test_website_sale_cart_notification(self):
+    def test_website_sale_cart_notification_tax_included(self):
         self.env.ref('website_sale.product_search').active = True
-        self.start_tour("/", 'website_sale_cart_notification')
+        self.website.show_line_subtotals_tax_selection = 'tax_included'
+        self.start_tour("/", 'website_sale_cart_notification_tax_included')
+
+    def test_website_sale_cart_notification_tax_excluded(self):
+        self.env.ref('website_sale.product_search').active = True
+        self.website.show_line_subtotals_tax_selection = 'tax_excluded'
+        self.start_tour("/", 'website_sale_cart_notification_tax_excluded')
 
     def test_website_sale_cart_notification_qty_and_total(self):
         """ Check that adding product into cart which is already in the cart only display newly
@@ -46,29 +51,3 @@ class TestWebsiteSaleCartNotification(HttpCase, ProductVariantsCommon):
 
         self.env.ref('website_sale.product_search').active = True
         self.start_tour("/", 'website_sale_cart_notification_qty_and_total')
-
-    def test_website_sale_cart_notification_product_price(self):
-        """Check that the cart notification displays the correct total price included/excluded taxes
-        depending on the website settings.
-        """
-        website = self.env['website'].get_current_website()
-        sale_order = self.env['sale.order'].create({
-            'partner_id': self.env.user.partner_id.id,
-            'website_id': website.id,
-            'order_line': [
-                Command.create({
-                    'product_id': self.product.id,
-                }),
-            ]
-        })
-        added_qty_per_line = {sale_order.order_line.id: 1.0}
-        website.show_line_subtotals_tax_selection = 'tax_included'
-        price_tax_included = self.WebsiteSaleCartController._get_cart_notification_information(
-            sale_order, added_qty_per_line
-        )['lines'][0]['price_total']
-        self.assertEqual(price_tax_included, 23)
-        website.show_line_subtotals_tax_selection = 'tax_excluded'
-        price_tax_excluded = self.WebsiteSaleCartController._get_cart_notification_information(
-            sale_order, added_qty_per_line
-        )['lines'][0]['price_total']
-        self.assertEqual(price_tax_excluded, 20)
