fc035304b0eab301551a99e118fc1f0435791b8e
Pierre Pulinckx <pipu@odoo.com>
2025-11-19 16:02:57 +0100

    [IMP] web: edit date and datetime fields with operation
    This commit introduces support for applying relative date/time
    operations on datetime field.
    With this feature, a record can be updated according to a specified
    operation.
    The operation should be like :
    [operator]=[amount][unit]
    with:
    operator: - | +
    amount: integer
    unit: d (days), w (weeks), m (months), y (years)
        H (hours), M (minutes), S (seconds)
    
    task~5156124
    
    closes odoo/odoo#232783
    
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/src/model/relational_model/operation.js
+++ addons/web/static/src/model/relational_model/operation.js
@@ -1,7 +1,95 @@
+import { smartDateUnits } from "@web/core/l10n/dates";
+import { localization } from "@web/core/l10n/localization";
+import { escapeRegExp } from "@web/core/utils/strings";
 export class Operation {
+    static supportedOperators = [];
+    static parse() {}
+    /**
+     * @param {string} operator
+     * @param {*} operand
+     */
     constructor(operator, operand) {
         this.operator = operator;
         this.operand = operand;
+        if (!this.constructor.supportedOperators.includes(this.operator)) {
+            throw new Error(`Operator ${this.operator} not supported on ${this.constructor.name}`);
+        }
+        this.setup();
+    }
+
+    setup() {}
+
+    toString() {
+        return `${this.operator} ${this.operand}`;
+    }
+}
+
+export class DateTimeOperation extends Operation {
+    static supportedOperators = ["+", "-"];
+    static dateTimeUnits = Object.keys(smartDateUnits).join();
+
+    static parse(operation) {
+        if (!operation) {
+            return false;
+        }
+        const regex = new RegExp(
+            `^(?<operator>[+\\-])\\s*=\\s*(?<amount>\\d+)\\s*(?<unit>[${DateTimeOperation.dateTimeUnits}])?$`
+        );
+        const match = operation.match(regex);
+        if (match?.groups) {
+            const amount = match.groups.amount;
+            const unit = match.groups.unit || "d";
+            const operand = {
+                amount: parseInt(amount, 10),
+                unit,
+            };
+            const operator = match.groups.operator;
+            return new DateTimeOperation(operator, operand);
+        }
+        return false;
+    }
+
+    setup() {
+        this.amount = this.operand.amount;
+        this.unit = this.operand.unit;
+        this.luxonUnit = smartDateUnits[this.unit];
+    }
+
+    compute(value) {
+        if (!value) {
+            return;
+        }
+        const delta = { [this.luxonUnit]: this.amount };
+        return this.operator === "+" ? value.plus(delta) : value.minus(delta);
+    }
+
+    toString() {
+        return `${this.operator} ${this.amount} ${this.luxonUnit}`;
+    }
+}
+
+export class ArithmeticOperation extends Operation {
+    static supportedOperators = ["+", "-", "/", "*"];
+
+    static parse(operation, parseValueFn) {
+        if (!operation) {
+            return false;
+        }
+        if (typeof parseValueFn !== "function") {
+            return false;
+        }
+        const regex = new RegExp(
+            `^(?<operator>[+\\-*/])\\s*=\\s*(?<operand>-?\\d+(?:[${escapeRegExp(
+                localization.decimalPoint
+            )}]\\d+)?)$`
+        );
+        const match = operation.match(regex);
+        if (match?.groups) {
+            const operand = parseValueFn(match.groups.operand);
+            const operator = match.groups.operator;
+            return new ArithmeticOperation(operator, operand);
+        }
+        return false;
     }
 
     compute(value) {
