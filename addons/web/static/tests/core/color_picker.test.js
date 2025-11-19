066bd6454b3c8cd54dc20b6f61fd8174a3bcc013
Adnan Saiyed <adsa@odoo.com>
2025-11-19 06:46:29 +0000

    [FIX] web: color slider getting reset
    Current behavior before commit:
    
    In editor's custom gradient picker, if `#FFFFFF` is picked
    from the very top of picker area, the color slider is reset
    to color `red`. This happens because when moving picker pointer
    to the top of area, `selectedColor` prop is updated to `#FFFFFF`,
    which calls `onWillUpdateProps` callback. As result, `convertRgbToHsl`
    sets hue value 0 for `#FFFFFF`, setting color slider to red.
    
    Desired behavior after commit:
    
    This commit ensures that in `onWillUpdateProps` callback,
    `setSelectedColor` should not get called if `newSelectedColor`
    is the same as `this.colorComponents.cssColor` to prevent updating
    UI twice while picking the color.
    
    task-5170041
    
    X-original-commit: b7e6e41c20e9f5551a662400dec98af38b05250c

---
+++ addons/web/static/tests/core/color_picker.test.js
@@ -0,0 +1,41 @@
+import { test, expect, animationFrame } from "@odoo/hoot";
+import { queryOne, manuallyDispatchProgrammaticEvent } from "@odoo/hoot-dom";
+import { Component, useState, xml } from "@odoo/owl";
+import { mountWithCleanup } from "@web/../tests/web_test_helpers";
+import { ColorPicker } from "@web/core/color_picker/color_picker";
+
+test("should preserve color slider when picking max lightness color", async () => {
+    class TestColorPicker extends Component {
+        static template = xml`
+            <div style="width: 222px">
+                <ColorPicker selectedColor="state.color" onColorPreview.bind="onColorChange" onColorSelect.bind="onColorChange"/>
+            </div>`;
+        static components = { ColorPicker };
+        static props = ["*"];
+        setup() {
+            this.state = useState({
+                color: "#FFFF00",
+            });
+        }
+        onColorChange({ cssColor }) {
+            this.state.color = cssColor;
+        }
+    }
+    await mountWithCleanup(TestColorPicker);
+    const colorPickerArea = queryOne(".o_color_pick_area");
+    const colorPickerRect = colorPickerArea.getBoundingClientRect();
+
+    const clientX = colorPickerRect.left + colorPickerRect.width / 2;
+    const clientY = colorPickerRect.top; // Lightness 100%
+    manuallyDispatchProgrammaticEvent(colorPickerArea, "mousedown", {
+        clientX,
+        clientY,
+    });
+    manuallyDispatchProgrammaticEvent(colorPickerArea, "mouseup", {
+        clientX,
+        clientY,
+    });
+
+    await animationFrame();
+    expect(colorPickerArea).toHaveStyle({ backgroundColor: "rgb(255, 255, 0)" });
+});
