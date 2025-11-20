b39b862dfb84cb97918d102c708c0bbc76a8530c
qucol-odoo <qucol@odoo.com>
2025-11-20 09:59:03 +0000

    [FIX] account_edi_ubl_cii: allow bill upload with empty descriptions
    In the Accounting app, users can upload vendor bills from XML files.
    The XML files describe the content of the bill, such as the name and
    address of the customer, the delivey and payment information,
    or the invoice lines.
    
    Whenever the `<cac:OrderReference>`/`<cbc:ID>` is missing **and** all
    the `<cac:Item>`/`<cbc:Description>` are empty, the upload of the bill
    fails and an *Odoo Server Error* is shown to the user.
    This happens because of the following loop:
    
    https://github.com/odoo/odoo/blob/2217d5220640531828eb5b2ae7a9d41ba9c97e78/addons/account_edi_ubl_cii/models/account_edi_xml_ubl_20.py#L1166-L1169
    
    If the `<cbc:Description>` field is empty, the attribute `desc.text` is
    `None`, and calling `str.join` on it raises the following TypeError:
    
    ```
    TypeError: sequence item 0: expected str instance, NoneType found
    ```
    
    This fix prevents the error and allows the bill to be created as expected.
    
    Steps to reproduce:
    1. Install the Accounting (`accountant`) app
    2. Go to Accounting > Vendors > Bills
    3. Click *Upload*
    4. Upload an XML file with no `<cac:OrderReference>` and where all the
    items have empty `<cbc:Description>` fields.
    An example of such file can be found is
    `addons/account_edi_ubl_cii/tests/test_files/bis3/test_vendor_bill_empty_description.xml`,
    which is an edited version of the `test_vendor_bill.xml` from the same directory
    5. An *Odoo Server Error* appears
    
    opw-5269474
    
    X-original-commit: 74d7a0f9f9141545353c64f7a68a2b4ae7843652

--- addons/account_edi_ubl_cii/tests/test_ubl_bis3.py
+++ addons/account_edi_ubl_cii/tests/test_ubl_bis3.py
@@ -2,8 +2,10 @@ from contextlib import contextmanager
 from unittest import mock
 
 from odoo import Command, fields
-from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 from odoo.tests import tagged
+from odoo.tools import misc
+
+from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 
 
 @tagged('post_install_l10n', 'post_install', '-at_install')
@@ -770,3 +772,13 @@ class TestUblBis3(AccountTestInvoicingCommon):
         with self.allow_sending_vendor_bills():
             self.env['account.move.send']._generate_and_send_invoices(invoice, sending_methods=['manual'])
         self._assert_invoice_ubl_file(invoice, 'test_vendor_credit_note')
+
+    def test_import_vendor_bill_empty_description(self):
+        with misc.file_open(f'{self.test_module}/tests/test_files/bis3/test_vendor_bill_empty_description.xml', 'rb') as file:
+            file_read = file.read()
+        attachment_id = self.env['ir.attachment'].create({
+            'name': 'test_file_no_item_description.xml',
+            'raw': file_read,
+        }).id
+        imported_bill = self.company_data['default_journal_purchase']._create_document_from_attachment(attachment_id)
+        self.assertTrue(imported_bill)
