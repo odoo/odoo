6c3e024150390caa6610162e434d19dec14ddb9e
Zelong Lin <zel@odoo.com>
2025-11-20 12:59:04 +0000

    [FIX] mail: handle mentions without base container
    In some cases, mentions can be inserted directly into the editable area
    without being wrapped in a base container. This can lead to issues when
    trying to edit or delete the mention, as the mention is a protected node.
    
    This commit updates the MentionPlugin to ensure that any mentions found
    directly under the editable area are wrapped in a base container.
    
    X-original-commit: 578f8c99cf4cad5f5494e5013dff1655f2c8a669

--- addons/mail/static/src/core/common/plugin/mention_plugin.js
+++ addons/mail/static/src/core/common/plugin/mention_plugin.js
@@ -4,7 +4,7 @@ import { generateThreadMentionElement } from "@mail/utils/common/format";
 
 export class MentionPlugin extends Plugin {
     static id = "mention";
-    static dependencies = ["selection", "history"];
+    static dependencies = ["baseContainer", "selection", "history", "protectedNode"];
     resources = {
         selectionchange_handlers: this.detectMentions.bind(this),
         is_node_editable_predicates: (node) => {
@@ -81,7 +81,24 @@ export class MentionPlugin extends Plugin {
             )
                 .filter(({ isValid }) => isValid)
                 .map(({ el }) => el);
+            this.prepareValidMentionLinks(validMentionLinks);
             validMentionsHandler?.(validMentionLinks);
+        }
+    }
+
+    prepareValidMentionLinks(validMentionLinks) {
+        for (const el of validMentionLinks) {
+            this.dependencies.protectedNode.setProtectingNode(el, true);
+            // if el's parent is odoo-editor-editable, which happens when the html is computed or set with setContent,
+            // considering the mention blocks are protected and not editable.
+            // This will lead to issues where the mention cannot be deleted or edited properly.
+            // In this case, we wrap the mention with a base container.
+            if (el.parentElement === this.editable) {
+                const baseContainer = this.dependencies.baseContainer.createBaseContainer();
+                baseContainer.appendChild(el.cloneNode(true));
+                this.editable.replaceChild(baseContainer, el);
+                this.dependencies.history.addStep();
+            }
         }
     }
 
