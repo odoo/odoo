e4f5df89d7012f1146c7323a5bb9388be9aa7bca
GACI Jugurtha (jugac) <jugac@odoo.com>
2025-11-20 14:15:27 +0100

    [IMP] l10n_id : add asset models, taxes, depreciation and expense account to fixed assets
    As odoo wants to simplify the using of its app for users in Indonesia, the assets models for each asset was added so that the users don't have to register the model manually each time.
    
    Also, each fixed asset was linked to an expense account and its corresponding depreciation account,
    
    Moreover, a default tax (TVA) was added to each fixed asset.
    
    task-5249077

--- addons/l10n_id/models/template_id.py
+++ addons/l10n_id/models/template_id.py
@@ -25,8 +25,8 @@ class AccountChartTemplate(models.AbstractModel):
                 'cash_account_code_prefix': '1111',
                 'transfer_account_code_prefix': '1999999',
                 'account_default_pos_receivable_account_id': 'l10n_id_11210011',
-                'income_currency_exchange_account_id': 'l10n_id_81100010',
-                'expense_currency_exchange_account_id': 'l10n_id_91100010',
+                'income_currency_exchange_account_id': 'l10n_id_81100030',
+                'expense_currency_exchange_account_id': 'l10n_id_91100020',
                 'account_journal_early_pay_discount_loss_account_id': 'l10n_id_99900003',
                 'account_journal_early_pay_discount_gain_account_id': 'l10n_id_99900004',
                 'account_sale_tax_id': 'tax_ST4',
@@ -41,7 +41,7 @@ class AccountChartTemplate(models.AbstractModel):
     def _get_id_account_account(self):
         return {
             'l10n_id_11300180': {
-                'account_stock_expense_id': 'l10n_id_51000020',
-                'account_stock_variation_id': 'l10n_id_42500010',
+                'account_stock_expense_id': 'l10n_id_66110100',
+                'account_stock_variation_id': 'l10n_id_66110130',
             },
         }
