3da231a5bb535f2822e7a1567e8f662a59186b04
Sébastien (blse) <blse@odoo.com>
2025-11-21 14:55:06 +0100

    [FIX] html_editor, website: avoid copy ancestors outside contenteditable
    In website builder, when the user copies a range of the html in the
    page, they may copy nodes with attributes that mark them to be saved.
    This happens if they select around a savable node, or if they select
    inside a savable node and the format plugin include clones of the
    ancestors (to keep matching style).
    
    This commit prevents that by not copying clones at the ancestor which
    sets `contenteditable` to true (if any), and by treating copy with a
    selection outside of `contenteditable` the same as a foreign copy (for
    which any non-whitelisted attributes are removed).
    
    Steps to reproduce:
    - Open website builder
    - Select a link of the menu in the header
    - Copy
    - Move the selection to "normal" text (like in the footer)
    - Paste
    - Bug: editing the pasted text changes the text in the menu
    - Save
    - Bug: the website cannot render
    
    - Open website builder in translate mode, on a blog post
    - Select a word in the middle of a paragraph
    - Copy
    - Paste
    - Bug: The paragraph gets replicated inside itself (it has the
      attributes with translation id, any changes are replicated)
    
    - Open website builder in translate mode, on a blog post
    - Select from the author name to the first paragraph
    - Copy
    - Paste inside the paragraph
    - Bug: The paragraph gets replicated inside itself (it has the
      attributes with translation id, any changes are replicated)
    
    opw-5053872
    task-5222402

--- addons/html_editor/static/src/core/clipboard_plugin.js
+++ addons/html_editor/static/src/core/clipboard_plugin.js
@@ -1,4 +1,9 @@
-import { isTextNode, isParagraphRelatedElement, isEmptyBlock } from "../utils/dom_info";
+import {
+    isTextNode,
+    isParagraphRelatedElement,
+    isEmptyBlock,
+    isContentEditable,
+} from "../utils/dom_info";
 import { Plugin } from "../plugin";
 import { closestBlock } from "../utils/blocks";
 import { unwrapContents, wrapInlinesInBlocks, splitTextNode, fillEmpty } from "../utils/dom";
@@ -165,7 +170,9 @@ export class ClipboardPlugin extends Plugin {
             clonedContents = processor(clonedContents, selection) || clonedContents;
         }
         this.dependencies.dom.removeSystemProperties(clonedContents);
-        fillClipboardData(ev, clonedContents);
+        fillClipboardData(ev, clonedContents, {
+            fillEditorClipboard: isContentEditable(selection.commonAncestorContainer),
+        });
     }
 
     /**
