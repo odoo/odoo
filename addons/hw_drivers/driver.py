9bcb617642fc16dcda726fa8677d4bc4ce732d79
Louis Travaux <lotr@odoo.com>
2025-11-21 13:46:47 +0000

    [FIX] hw_drivers: prevent duplicate report printing
    In order to prevent duplicate IoT actions, we now ensure that websocket
    actions have not already been called through longpolling.
    
    odoo/enterprise#100161
    
    X-original-commit: ec4e8290ad6566b2f566b33fa14375a5833290cb

--- addons/hw_drivers/driver.py
+++ addons/hw_drivers/driver.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import logging
 from threading import Thread, Event
 
 from odoo.addons.hw_drivers.main import drivers, iot_devices
 from odoo.addons.hw_drivers.tools.helpers import toggleable
 
 from odoo.tools.lru import LRU
+
+_logger = logging.getLogger(__name__)
 
 
 class Driver(Thread):
@@ -56,17 +59,26 @@ class Driver(Thread):
         self._stopped.set()
         del iot_devices[self.device_identifier]
 
-    def _check_idempotency(self, iot_idempotent_id, session_id):
-        """
-        Some IoT requests for the same action might be received several times.
+    def _check_idempotency(self, iot_idempotent_id=None, session_id="unknown", **_kwargs):
+        """Some IoT requests for the same action might be received several times.
         To avoid duplicating the resulting actions, we check if the action was "recently" executed.
         If this is the case, we will simply ignore the action
 
+        :param str iot_idempotent_id: the idempotent ID received from the controller
+        :param session_id: the session ID of the current request
+        :param dict _kwargs: only here to allow providing the whole websocket/longpolling received dict
         :return: the `session_id` of the same `iot_idempotent_id` if any. False otherwise,
         which means that it is the first time that the IoT box received the request with this ID
         """
+        if not iot_idempotent_id:
+            return False
+
         cache = self._iot_idempotent_ids_cache
         if iot_idempotent_id in cache:
+            _logger.debug(
+                "Ignored request from '%s' as iot_idempotent_id '%s' already received",
+                iot_idempotent_id, cache[iot_idempotent_id]
+            )
             return cache[iot_idempotent_id]
         cache[iot_idempotent_id] = session_id
         return False
