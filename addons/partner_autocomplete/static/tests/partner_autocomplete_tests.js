odoo/odoo

commit 5bb57a916d3069cd609e07d401dd3b4c39f85954

[IMP] partner_autocomplete: change provider
We are switching away from Clearbit which is very
unreliable. For that reason we had an autosync
feature which would update IAP's data for a company
based on the data provided in Odoo.

Instead, we are now switching to Dun & Bradstreet
which has much more reliable data. Some routes have
been changed to target the new API on IAP.

We continue to use Clearbit for the logos which are
not provided by DnB (yet).

This is a backport of https://github.com/odoo/odoo/pull/196373
where models, fields, views, and public methods have been kept
to respect the stable policy.

task-4416928

closes odoo/odoo#202546

Related: odoo/enterprise#81767
Signed-off-by: Louis Baudoux (lba) <lba@odoo.com>

addons/partner_autocomplete/static/tests/partner_autocomplete_tests.js
@@ -20,6 +20,44 @@ async function editInputNoChangeEvent(input, value) {
     await triggerEvent(input, null, "input");
 }
 
+
+const iapSuggestions = [
+    {
+        "name": "First Company",
+        "duns": "123",
+        "city": "FirstCity",
+        "country_id": {"id": 1, "display_name": "Belgium"},
+    },
+    {
+        "name": "Second Company",
+        "duns": "456",
+        "city": "SecondCity",
+        "country_id": {"id": 1, "display_name": "Belgium"},
+    },
+    {
+        "name": "Third Company",
+        "duns": "789",
+        "city": "ThirdCity",
+        "country_id": {"id": 1, "display_name": "Belgium"},
+    },
+];
+
+const clearbitSuggestions = [
+    {
+        "name": "First Company",
+        "domain": "firstcompany.com",
+    },
+    {
+        "name": "MyCompany",
+        "domain": "mycompany.com",
+    },
+    {
+        "name": "YourCompany",
+        "domain": "yourcompany.com",
+    },
+];
+
+
 QUnit.module('partner_autocomplete', {
     async before() {
         // Load the lib before the tests to prevent them from
@@ -52,9 +90,11 @@ QUnit.module('partner_autocomplete', {
                         name: {string: "Name", type: "char", searchable: true},
                         parent_id: {string: "Company", type: "many2one", relation: "res.partner", searchable: true},
                         website: {string: "Website", type: "char", searchable: true},
+                        email: {string: "Email", type: "char", searchable: true},
                         image_1920: {string: "Image", type: "binary", searchable: true},
                         phone: {string: "Phone", type: "char", searchable: true},
                         street: {string: "Street", type: "char", searchable: true},
+                        street2: {string: "Street2", type: "char", searchable: true},
                         city: {string: "City", type: "char", searchable: true},
                         zip: {string: "Zip", type: "char", searchable: true},
                         state_id: {string: "State", type: "many2one", relation: "res.country.state", searchable: true},
@@ -62,7 +102,6 @@ QUnit.module('partner_autocomplete', {
                         comment: {string: "Comment", type: "char", searchable: true},
                         vat: {string: "Vat", type: "char", searchable: true},
                         is_company: {string: "Is company", type: "bool", searchable: true},
-                        partner_gid: {string: "Company data ID", type: "integer", searchable: true},
                     },
                     records: [],
                     onchanges: {
@@ -77,16 +116,16 @@ QUnit.module('partner_autocomplete', {
                     },
                     records: [{
                         id: 1,
-                        name: 'United States',
+                        display_name: 'Belgium',
                     }],
                 },
                 'res.country.state': {
                     fields: {
-                        display_name: {string: "Name", type: "char", searchable: true},
+                        name: {string: "Name", type: "char", searchable: true},
                     },
                     records: [{
                         id: 1,
-                        name: 'California (US)',
+                        name: 'Walloon Brabant',
                     }],
                 },
             },
@@ -100,85 +139,51 @@ QUnit.module('partner_autocomplete', {
                 <field name="parent_id" widget="res_partner_many2one"/>
                 <field name="website"/>
                 <field name="image_1920" widget="image"/>
+                <field name="email"/>
                 <field name="phone"/>
                 <field name="street"/>
+                <field name="street2"/>
                 <field name="city"/>
                 <field name="state_id"/>
                 <field name="zip"/>
                 <field name="country_id"/>
-                <field name="comment"/>
                 <field name="vat" widget="field_partner_autocomplete"/>
             </form>`,
         async mockRPC(route, args) {
-            if (route === "/web/dataset/call_kw/res.partner/autocomplete" || route === "/web/dataset/call_kw/res.partner/read_by_vat") {
-                return Promise.resolve([
-                    {
-                        "partner_gid": 1,
-                        "website": "firstcompany.com",
-                        "name": "First company",
-                        "ignored": false,
-                        "vat": ""
-                    },
-                    {
-                        "partner_gid": 2,
-                        "website": "secondcompany.com",
-                        "name": "Second company",
-                        "ignored": false,
-                        "vat": ""
-                    },
-                    {
-                        "partner_gid": 3,
-                        "website": "thirdcompany.com",
-                        "name": "Third company",
-                        "ignored": false,
-                        "vat": ""
-                    },
-                ]);
+            if (route === "/web/dataset/call_kw/res.partner/autocomplete_by_name" || route === "/web/dataset/call_kw/res.partner/autocomplete_by_vat") {
+                return Promise.resolve(iapSuggestions);
             }
-            else if (route === "/web/dataset/call_kw/res.partner/enrich_company") {
+            else if (route === "/web/dataset/call_kw/res.partner/enrich_by_duns") {
                 return Promise.resolve({
-                    "partner_gid": args.args[1],
-                    "website": args.args[0],
-                    "name": args.args[1] === 1 ? "First company" : "Second company",
-                    'logo': false,
-                    "ignored": false,
-                    "vat": "Some VAT number",
-                    "street": "Some street",
-                    "city": "Some city",
-                    "zip": "1234",
-                    "phone": "+0123456789",
-                    "email": "info@firstcompany.com",
+                    "name": iapSuggestions.filter((sugg) => sugg.duns === args.args[0])[0].name,
+                    "vat": "BE0477472701",
+                    "duns": "372441183",
+                    "city": "Ramillies",
+                    "zip": "1367",
+                    "street": "Chaussée de Namur 40",
+                    "street2": false,
+                    "email": "hello@odoo.com",
+                    "phone": "3281813700",
+                    "website": "www.odoo.com",
+                    "domain": "odoo.com",
                     "country_id": {
-                        'id': 1,
-                        'display_name': "United States",
+                        "id": 1,
+                        "name": "Belgium"
                     },
                     "state_id": {
-                        'id': 1,
-                        'display_name': "California (US)",
+                        "id": 1,
+                        "name": "Walloon Brabant"
                     },
                 });
             }
             else if (route.startsWith("https://autocomplete.clearbit.com/v1/companies/suggest")) {
-                return Promise.resolve([
-                    {
-                        "name": "Odoo",
-                        "domain": "odoo.com",
-                    },
-                    {
-                        "name": "MyCompany",
-                        "domain": "mycompany.com",
-                    },
-                    {
-                        "name": "YourCompany",
-                        "domain": "yourcompany.com",
-                    },
-                ])
+                return Promise.resolve(clearbitSuggestions)
             }
         }
     }
 
     QUnit.test("Partner autocomplete : Company type = Individual", async function (assert) {
-        assert.expect(13);
+        assert.expect(12);
         await makeView(makeViewParams);
 
         // Set company type to Individual
@@ -208,8 +213,8 @@ QUnit.module('partner_autocomplete', {
         assert.containsN(
             autocompleteContainer,
             ".o-autocomplete--dropdown-item.partner_autocomplete_dropdown_many2one",
-            6,
-            "Clearbit and Odoo autocomplete options should be shown"
+            3,
+            "Odoo autocomplete options should be shown"
         );
 
         // Click on the first option - "First company"
@@ -218,15 +223,14 @@ QUnit.module('partner_autocomplete', {
         const modalContent = target.querySelector('.modal-content');
         // Check that the fields of the modal have been pre-filled
         const expectedValues = {
-            "website": "firstcompany.com",
-            "name": "First company",
-            "vat": "Some VAT number",
-            "street": "Some street",
-            "city": "Some city",
-            "zip": "1234",
-            "phone": "+0123456789",
-            "country_id": "United States",
-            "state_id": "California (US)",
+            "name": "First Company",
+            "vat": "BE0477472701",
+            "street": "Chaussée de Namur 40",
+            "city": "Ramillies",
+            "zip": "1367",
+            "phone": "3281813700",
+            "country_id": "Belgium",
+            "state_id": "Walloon Brabant",
         };
         for (const [fieldName, expectedValue] of Object.entries(expectedValues)) {
             assert.strictEqual(modalContent.querySelector(`[name=${fieldName}] input`).value, expectedValue, `${fieldName} should be pre-filled`);
@@ -234,7 +238,7 @@ QUnit.module('partner_autocomplete', {
     });
 
     QUnit.test("Partner autocomplete : Company type = Company / Name search", async function (assert) {
-        assert.expect(12);
+        assert.expect(11);
         await makeView(makeViewParams);
 
         // Set company type to Company
@@ -261,24 +265,23 @@ QUnit.module('partner_autocomplete', {
         assert.containsN(
             autocompleteContainer,
             ".o-autocomplete--dropdown-item",
-            6,
-            "Clearbit and Odoo autocomplete options should be shown"
+            4,  // 3 options + 1 for the worldwide option
+            "Odoo autocomplete options should be shown"
         );
 
-        // Click on the first option - "First company"
+        // Click on the first option - "First Company"
         await click(autocompleteContainer.querySelectorAll('ul li')[0], null);
 
         // Check that the fields have been filled
         const expectedValues = {
-            "website": "firstcompany.com",
-            "name": "First company",
-            "vat": "Some VAT number",
-            "street": "Some street",
-            "city": "Some city",
-            "zip": "1234",
-            "phone": "+0123456789",
-            "country_id": "United States",
-            "state_id": "California (US)",
+            "name": "First Company",
+            "vat": "BE0477472701",
+            "street": "Chaussée de Namur 40",
+            "city": "Ramillies",
+            "zip": "1367",
+            "phone": "3281813700",
+            "country_id": "Belgium",
+            "state_id": "Walloon Brabant",
         };
         for (const [fieldName, expectedValue] of Object.entries(expectedValues)) {
             assert.strictEqual(target.querySelector(`[name=${fieldName}] input`).value, expectedValue, `${fieldName} should be filled`);
@@ -286,7 +289,7 @@ QUnit.module('partner_autocomplete', {
     });
 
     QUnit.test("Partner autocomplete : Company type = Company / VAT search", async function (assert) {
-        assert.expect(12);
+        assert.expect(11);
 
         await makeView(makeViewParams);
 
@@ -314,7 +317,7 @@ QUnit.module('partner_autocomplete', {
         assert.containsN(
             autocompleteContainer,
             ".o-autocomplete--dropdown-item",
-            3,
+            4,  // 3 options + 1 for the worldwide option
             "Odoo read_by_vat options should be shown"
         );
 
@@ -323,15 +326,14 @@ QUnit.module('partner_autocomplete', {
 
         // Check that the fields have been filled
         const expectedValues = {
-            "website": "firstcompany.com",
-            "name": "First company",
-            "vat": "Some VAT number",
-            "street": "Some street",
-            "city": "Some city",
-            "zip": "1234",
-            "phone": "+0123456789",
-            "country_id": "United States",
-            "state_id": "California (US)",
+            "name": "First Company",
+            "vat": "BE0477472701",
+            "street": "Chaussée de Namur 40",
+            "city": "Ramillies",
+            "zip": "1367",
+            "phone": "3281813700",
+            "country_id": "Belgium",
+            "state_id": "Walloon Brabant",
         };
         for (const [fieldName, expectedValue] of Object.entries(expectedValues)) {
             assert.strictEqual(target.querySelector(`[name=${fieldName}] input`).value, expectedValue, `${fieldName} should be filled`);
@@ -388,8 +390,8 @@ QUnit.module('partner_autocomplete', {
         assert.containsN(
             autocompleteContainer,
             ".o-autocomplete--dropdown-item",
-            8,
-            "Clearbit and Odoo autocomplete options should be shown"
+            6,  //3 suggestions + create + create & edit + search worldwide
+            "Odoo autocomplete options should be shown"
         );
     });
 
@@ -407,8 +409,8 @@ QUnit.module('partner_autocomplete', {
         assert.containsN(
             autocompleteContainer,
             ".o-autocomplete--dropdown-item",
-            6,
-            "Clearbit and Odoo autocomplete options should be shown"
+            4,  // 3 options + 1 for the worldwide option
+            "Odoo autocomplete options should be shown"
         );
         // Click on the second option (include realistic events) - "Second company"
         await triggerEvent(
@@ -427,15 +429,14 @@ QUnit.module('partner_autocomplete', {
 
         // Check that the fields have been filled
         const expectedValues = {
-            "website": "secondcompany.com",
-            "name": "Second company",
-            "vat": "Some VAT number",
-            "street": "Some street",
-            "city": "Some city",
-            "zip": "1234",
-            "phone": "+0123456789",
-            "country_id": "United States",
-            "state_id": "California (US)",
+            "name": "Second Company",
+            "vat": "BE0477472701",
+            "street": "Chaussée de Namur 40",
+            "city": "Ramillies",
+            "zip": "1367",
+            "phone": "3281813700",
+            "country_id": "Belgium",
+            "state_id": "Walloon Brabant",
         };
         for (const [fieldName, expectedValue] of Object.entries(expectedValues)) {
             assert.strictEqual(target.querySelector(`[name=${fieldName}] input`).value, expectedValue, `${fieldName} should be filled`);

addons/l10n_in/models/res_partner.py
@@ -99,28 +99,16 @@ class ResPartner(models.Model):
 
     @api.model
     def _l10n_in_get_partner_vals_by_vat(self, vat):
-        partner_details = self.read_by_vat(vat)
-        partner_data = partner_details[0] if partner_details else {}
-        if partner_data:
-            partner_gid = partner_data.get('partner_gid')
-            if partner_gid:
-                partner_data = self.enrich_company(company_domain=None, partner_gid=partner_gid, vat=partner_data.get('vat'))
-                partner_data = self._iap_replace_logo(partner_data)
-            return {
-                'name': partner_data.get('name'),
-                'company_type': 'company',
-                'partner_gid': partner_gid,
-                'vat': partner_data.get('vat'),
-                'l10n_in_gst_treatment': 'regular',
-                'image_1920': partner_data.get('image_1920'),
-                'street': partner_data.get('street'),
-                'street2': partner_data.get('street2'),
-                'city': partner_data.get('city'),
-                'state_id': partner_data.get('state_id', {}).get('id', False),
-                'country_id': partner_data.get('country_id', {}).get('id', False),
-                'zip': partner_data.get('zip'),
-            }
-        return {}
+        partner_data = self.enrich_by_gst(vat)
+        partner_data.pop('domain', None)
+        partner_data.pop('unspsc_codes', None)
+        partner_data.update({
+            'country_id': partner_data.get('country_id', {}).get('id'),
+            'state_id': partner_data.get('state_id', {}).get('id'),
+            'company_type': 'company',
+            'l10n_in_gst_treatment': 'regular',
+        })
+        return partner_data
 
     def action_update_state_as_per_gstin(self):
         self.ensure_one()

addons/partner_autocomplete/__manifest__.py
@@ -24,7 +24,6 @@ Auto-complete partner companies' data
     'auto_install': True,
     'assets': {
         'web.assets_backend': [
-            'partner_autocomplete/static/src/scss/*',
             'partner_autocomplete/static/src/js/*',
             'partner_autocomplete/static/src/xml/*',
         ],

addons/partner_autocomplete/i18n/partner_autocomplete.pot
@@ -162,7 +162,7 @@ msgstr ""
 #. module: partner_autocomplete
 #. odoo-javascript
 #: code:addons/partner_autocomplete/static/src/xml/partner_autocomplete.xml:0
-msgid "Placeholder"
+msgid "Search Worldwide 🌎"
 msgstr ""
 
 #. module: partner_autocomplete
@@ -182,4 +182,10 @@ msgstr ""
 #. odoo-python
 #: code:addons/partner_autocomplete/models/iap_autocomplete_api.py:0
 msgid "Test mode"
+msgstr ""
+
+#. module: partner_autocomplete
+#. odoo-python
+#: code:addons/partner_autocomplete/models/res_partner.py:0
+msgid "Unable to enrich company (no credit was consumed)."
 msgstr ""

addons/partner_autocomplete/models/iap_autocomplete_api.py
@@ -38,7 +38,7 @@ class IapAutocompleteEnrichAPI(models.AbstractModel):
         :return tuple: results, error code
         """
         try:
-            results = self._contact_iap('/iap/partner_autocomplete', action, params, timeout=timeout)
+            results = self._contact_iap('/api/dnb/1', action, params, timeout=timeout)
         except exceptions.ValidationError:
             return False, 'Insufficient Credit'
         except (ConnectionError, HTTPError, exceptions.AccessError, exceptions.UserError) as exception:

addons/partner_autocomplete/models/res_company.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import json
 import logging
 import threading
-
-from lxml.builder import E
 
 from odoo.addons.iap.tools import iap_tools
 from odoo import api, fields, models, _
@@ -20,12 +17,8 @@ class ResCompany(models.Model):
     _name = 'res.company'
     _inherit = 'res.company'
 
-    partner_gid = fields.Integer('Company database ID', related="partner_id.partner_gid", inverse="_inverse_partner_gid", store=True)
+    partner_gid = fields.Integer('Company database ID', related="partner_id.partner_gid",store=True)
     iap_enrich_auto_done = fields.Boolean('Enrich Done')
-
-    def _inverse_partner_gid(self):
-        for company in self:
-            company.partner_id.partner_gid = company.partner_gid
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -40,9 +33,6 @@ class ResCompany(models.Model):
 
         if view_type == 'form':
             for i, node in enumerate(arch.xpath("//field[@name='name' or @name='vat']")):
-                if i == 0:
-                    node.addnext(E.field(name='partner_gid', invisible='1'))
-
                 node.set('widget', 'field_partner_autocomplete')
 
         return arch, view
@@ -68,40 +58,17 @@ class ResCompany(models.Model):
         if not company_domain:
             return False
 
-        company_data = self.env['res.partner'].enrich_company(company_domain, False, self.vat, timeout=COMPANY_AC_TIMEOUT)
-        if company_data.get('error'):
-            return False
-        additional_data = company_data.pop('additional_info', False)
+        company_data = self.env['res.partner'].enrich_by_domain(company_domain, timeout=COMPANY_AC_TIMEOUT)
+        if not company_data or company_data.get("error"):
+            return
 
-        # Keep only truthy values that are not already set on the target partner
-        # Erase image_1920 even if something is in it. Indeed as partner_autocomplete is probably installed as a
-        # core app (mail -> iap -> partner_autocomplete auto install chain) it is unlikely that people already
-        # updated their company logo.
-        self.env['res.partner']._iap_replace_logo(company_data)
         company_data = {field: value for field, value in company_data.items()
                         if field in self.partner_id._fields and value and (field == 'image_1920' or not self.partner_id[field])}
 
-        # for company and childs: from state_id / country_id display_name like to IDs
+        # for company: from state_id / country_id display_name like to IDs
         company_data.update(self._enrich_extract_m2o_id(company_data, ['state_id', 'country_id']))
-        if company_data.get('child_ids'):
-            company_data['child_ids'] = [
-                dict(child_data, **self._enrich_extract_m2o_id(child_data, ['state_id', 'country_id']))
-                for child_data in company_data['child_ids']
-            ]
-
-        # handle o2m values, e.g. {'bank_ids': ['acc_number': 'BE012012012', 'acc_holder_name': 'MyWebsite']}
-        self._enrich_replace_o2m_creation(company_data)
 
         self.partner_id.write(company_data)
-
-        if additional_data:
-            template_values = json.loads(additional_data)
-            template_values['flavor_text'] = _("Company auto-completed by Odoo Partner Autocomplete Service")
-            self.partner_id.message_post_with_source(
-                'iap_mail.enrich_company',
-                render_values=template_values,
-                subtype_xmlid='mail.mt_note',
-            )
         return True
 
     def _enrich_extract_m2o_id(self, iap_data, m2o_fields):
@@ -112,18 +79,6 @@ class ResCompany(models.Model):
             if relation_data and isinstance(relation_data, dict):
                 extracted_data[m2o_field] = relation_data.get('id', False)
         return extracted_data
-
-    def _enrich_replace_o2m_creation(self, iap_data):
-        for o2m_field, values in iap_data.items():
-            if isinstance(values, list):
-                commands = [(
-                    0, 0, create_value
-                ) for create_value in values if isinstance(create_value, dict)]
-                if commands:
-                    iap_data[o2m_field] = commands
-                else:
-                    iap_data.pop(o2m_field, None)
-        return iap_data
 
     def _get_company_domain(self):
         """ Extract the company domain to be used by IAP services.

commit ca0e3230750c63c83602498cd238253d809faf41

[FIX] partner_autocomplete: display the placeholder of the res_partner_many2one field
Steps to reproduce:
- Go to Contacts > Contacts > New
- Change it to "individual" type
> The "parent_id" field should have a place holder "Company Name..."

Cause of the issue:

The `parent_id` field of the res.partner form uses the
`res_partner_many2one` widget:
https://github.com/odoo/odoo/blob/58b888992f80a58fecdb92e23fea0050f2178faf/odoo/addons/base/views/res_partner_views.xml#L164-L166
and is therefore relying on the `PartnerAutoCompleteMany2XField`.
However, this template tries to recover its placeholder from the
`placeholder` attribute of the Component:
https://github.com/odoo/odoo/blob/58b888992f80a58fecdb92e23fea0050f2178faf/addons/partner_autocomplete/static/src/xml/partner_autocomplete.xml#L41-L50
even thought this attribute is not defined and the placeholder should
be recovered from its props:
https://github.com/odoo/odoo/blob/60e0529b98098b019ade09aad97cc6e359bc0755/addons/web/static/src/views/fields/relational_utils.xml#L35-L38

Note:

The issue starts in 18.0 as it was introduced with the xpath in 5bb57a916d3069cd609e07d401dd3b4c39f85954

opw-5316203

addons/partner_autocomplete/static/tests/partner_autocomplete_tests.js
@@ -136,7 +136,7 @@ QUnit.module('partner_autocomplete', {
             `<form>
                 <field name="company_type"/>
                 <field name="name" widget="field_partner_autocomplete"/>
-                <field name="parent_id" widget="res_partner_many2one"/>
+                <field name="parent_id" widget="res_partner_many2one" placeholder="Company Name..."/>
                 <field name="website"/>
                 <field name="image_1920" widget="image"/>
                 <field name="email"/>
@@ -183,7 +183,7 @@ QUnit.module('partner_autocomplete', {
     }
 
     QUnit.test("Partner autocomplete : Company type = Individual", async function (assert) {
-        assert.expect(12);
+        assert.expect(13);
         await makeView(makeViewParams);
 
         // Set company type to Individual
@@ -194,6 +194,11 @@ QUnit.module('partner_autocomplete', {
 
         const companyInput = target.querySelector("[name='parent_id'] input");
         const autocompleteContainer = companyInput.parentElement;
+        assert.strictEqual(
+            companyInput.placeholder,
+            "Company Name...",
+            "The placeholder should be displayed"
+        );
 
         await click(companyInput, null);
         assert.containsNone(