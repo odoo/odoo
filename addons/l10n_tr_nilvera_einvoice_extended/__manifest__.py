fc0f1aa4b357d801f12203ce52b2bbf004bfccf8
william-andre <wan@odoo.com>
2025-11-24 15:33:41 +0100

    [FIX] l10n_tr_nilvera_einvoice_extended: remove useless post init hook
    It is not needed since 64f9dcb045231ea8cf2edb14de536decb8b9b508
    
    Part-of: odoo/odoo#237020
    Related: odoo/enterprise#100260
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/l10n_tr_nilvera_einvoice_extended/__manifest__.py
+++ addons/l10n_tr_nilvera_einvoice_extended/__manifest__.py
@@ -26,7 +26,6 @@ Features include:
         'views/res_config_settings_views.xml',
     ],
     'auto_install': ['l10n_tr_nilvera_einvoice'],
-    'post_init_hook': '_l10n_tr_nilvera_einvoice_extended_post_init',
     'author': 'Odoo S.A.',
     'license': 'LGPL-3',
 }
