a3ace9663cf91d954d337319fdb3c0746d6426a7
Adrien Guilliams (adgu) <adgu@odoo.com>
2025-11-25 10:18:41 +0100

    [FIX] point_of_sale: better quantity change handling
    Before this commit, when changing the quantities of a line, we would not
    automatically change the quantities of the children lines if any.
    This is now the case. We also handle the prices in a better way such
    that changing the quantities of a combo product does not affect the
    price unit of the lines computed previously.
    
    closes odoo/odoo#237277
    
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/point_of_sale/static/src/app/models/pos_order_line.js
+++ addons/point_of_sale/static/src/app/models/pos_order_line.js
@@ -244,6 +244,7 @@ export class PosOrderline extends Base {
     // product's unity of measure properties. Quantities greater than zero will not get
     // rounded to zero
     setQuantity(quantity, keep_price) {
+        const oldQty = this.qty;
         if (this.order_id.preset_id?.is_return) {
             quantity = -Math.abs(quantity);
         }
@@ -313,6 +314,10 @@ export class PosOrderline extends Base {
                     )
                 );
             }
+        }
+        for (const comboLine of this.combo_line_ids) {
+            // If each combo contains 2 qty of a product, we wanna keep this ratio after setting the new quantity on the parent product.
+            comboLine.setQuantity((comboLine.qty / oldQty || 1) * quantity, true);
         }
         return true;
     }
