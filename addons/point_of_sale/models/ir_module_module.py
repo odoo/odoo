f4e7941681b6d635362bf9ef2b8399bae3ff2d50
Quirino Leone <quirinoleone95@gmail.com>
2025-11-25 17:37:34 +0100

    [FIX] point_of_sale: fix check module installation

--- addons/point_of_sale/models/ir_module_module.py
+++ addons/point_of_sale/models/ir_module_module.py
@@ -5,17 +5,17 @@ class IrModuleModule(models.Model):
     _inherit = 'ir.module.module'
 
     @api.model
-    def _load_pos_data_fields(self):
+    def _load_pos_data_fields(self, config_id):
         return ['id', 'name', 'state']
 
     @api.model
-    def _load_pos_data_domain(self):
+    def _load_pos_data_domain(self, data):
         return [('name', '=', 'pos_settle_due')]
 
     def _load_pos_data(self, data):
-        domain = self._load_pos_data_domain()
-        fields = self._load_pos_data_fields()
+        domain = self._load_pos_data_domain(data)
+        fields = self._load_pos_data_fields(data)
         return {
             'data': self.search_read(domain, fields, load=False),
-            'fields': self._load_pos_data_fields(),
+            'fields': self._load_pos_data_fields(data),
         }
