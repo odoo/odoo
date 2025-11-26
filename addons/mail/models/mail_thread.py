c31ff8a7ae2770999f68a3bc3af1da03cf9807ad
Thibault Delavallée <tde@odoo.com>
2025-11-26 23:27:50 +0100

    [REM] mail, various: remove tracking html generation in payloads

--- addons/mail/models/mail_thread.py
+++ addons/mail/models/mail_thread.py
@@ -4005,7 +4005,7 @@ class MailThread(models.AbstractModel):
         return {
             'title': title,
             'options': {
-                'body': html2plaintext(body, include_references=False) + self._generate_tracking_message(message),
+                'body': html2plaintext(body, include_references=False),
                 'icon': icon,
                 'data': {
                     'model': model if model else '',
@@ -4505,33 +4505,6 @@ class MailThread(models.AbstractModel):
         elif link_type not in ['view', 'assign', 'follow', 'unfollow']:
             return {}
         return params
-
-    @api.model
-    def _generate_tracking_message(self, message, return_line='\n'):
-        """
-        Format the tracking values like in the chatter
-        :param message: current mail.message record
-        :param return_line: type of return line
-        :return: a string with the new text if there is one or more tracking value
-        """
-        tracking_message = ''
-        if message.subtype_id and message.subtype_id.description:
-            tracking_message = return_line + message.subtype_id.description + return_line
-
-        for tracking in message.sudo().tracking_value_ids._filter_free_field_access():
-            if tracking.field_id.ttype == 'boolean':
-                old_value = str(bool(tracking.old_value_integer))
-                new_value = str(bool(tracking.new_value_integer))
-            else:
-                old_value = tracking.old_value_char or str(tracking.old_value_integer)
-                new_value = tracking.new_value_char or str(tracking.new_value_integer)
-
-            tracking_message += tracking.field_id.field_description + ': ' + old_value
-            if old_value != new_value:
-                tracking_message += ' → ' + new_value
-            tracking_message += return_line
-
-        return tracking_message
 
     @api.model
     def _get_model_description(self, model_name):
