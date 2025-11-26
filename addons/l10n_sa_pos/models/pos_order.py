9a94b894f9ee76876e77f83dae57647252a6fb2c
utma-odoo <utma@odoo.com>
2025-11-26 16:24:10 +0530

    [FIX] l10n_sa_pos: prevent error when creating consolidated POS invoices
    Currently, an error occurs when trying to create a consolidated invoice for
    multiple POS orders associated with the same customer.
    
    Steps to reproduce:
    
    - Install the l10n_sa_pos module and switch to the SA company.
    - Create two POS orders for the same customer without invoicing at checkout.
    - Close the POS session and go to Point of Sale > Orders.
    - Select both orders > click Create Invoice > confirm the action.
      (Make sure Consolidated Billing is enabled)
    
    Error:
    ValueError: Expected singleton: pos.order(8, 7)
    
    Root cause:
    At [1], the code accesses `self.date_order`, but when consolidated billing is
    enabled, self contains multiple POS orders, causing an error.
    
    Fix:
    This commit prevents the error by ensuring that the current datetime is
    assigned when creating a consolidated invoice, same as [2].
    
    [1]:
    https://github.com/odoo/odoo/blob/583bacdc8ad2b87b99b11d1e12dacf6e42edf22b/addons/l10n_sa_pos/models/pos_order.py#L13
    
    [2]:
    https://github.com/odoo/odoo/blob/583bacdc8ad2b87b99b11d1e12dacf6e42edf22b/addons/point_of_sale/models/pos_order.py#L828-L832
    
    opw-5266908
    
    closes odoo/odoo#237677
    
    Signed-off-by: Stéphane Vanmeerhaeghe (stva) <stva@odoo.com>

--- addons/l10n_sa_pos/models/pos_order.py
+++ addons/l10n_sa_pos/models/pos_order.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models
+from odoo import fields, models
 
 
 class PosOrder(models.Model):
@@ -10,5 +10,6 @@ class PosOrder(models.Model):
     def _prepare_invoice_vals(self):
         vals = super()._prepare_invoice_vals()
         if self.company_id.country_id.code == 'SA':
-            vals.update({'l10n_sa_confirmation_datetime': self.date_order})
+            confirmation_datetime = self.date_order if len(self) == 1 else fields.Datetime.now()
+            vals['l10n_sa_confirmation_datetime'] = confirmation_datetime
         return vals
