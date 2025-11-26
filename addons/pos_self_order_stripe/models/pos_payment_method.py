07631d205c8aae87bd53073d41cbc33fecac961a
parp-odoo <parp@odoo.com>
2025-11-26 05:11:52 +0000

    [FIX] pos_self_order: restrict kiosk to only configured payment methods
    Ensure the kiosk only uses payment methods that are explicitly configured.
    
    Steps to reproduce:
    - Set up an online payment method (do not assign it to the kiosk)
    - Open a kiosk session.
    - Try to validate an order.
    
    Issue:
    - The kiosk prompts for an online payment method,
      even though none are configured.
    
    Fix:
    - Restrict the kiosk to use only the payment methods
      explicitly configured in its settings.
    - Prevent loading of any unconfigured payment methods to the kiosk.
    
    Task: 4911495
    X-original-commit: fa1bbb2e3bc4c6685304a4f7992d0cd65d67c522

--- addons/pos_self_order_stripe/models/pos_payment_method.py
+++ addons/pos_self_order_stripe/models/pos_payment_method.py
@@ -1,4 +1,7 @@
-from odoo import models
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, models
+from odoo.osv import expression
 
 
 class PosPaymentMethod(models.Model):
@@ -9,3 +12,13 @@ class PosPaymentMethod(models.Model):
             return super()._payment_request_from_kiosk(order)
         else:
             return self.stripe_payment_intent(order.amount_total)
+
+    @api.model
+    def _load_pos_self_data_domain(self, data):
+        domain = super()._load_pos_self_data_domain(data)
+        if data['pos.config'][0]['self_ordering_mode'] == 'kiosk':
+            domain = expression.OR([
+                [('use_payment_terminal', '=', 'stripe'), ('id', 'in', data['pos.config'][0]['payment_method_ids'])],
+                domain
+            ])
+        return domain
