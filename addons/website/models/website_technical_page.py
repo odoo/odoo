934a392ae038a3c39eaf48215afdc755cf35d6fb
pkgu-odoo <pkgu@odoo.com>
2025-11-26 06:05:16 +0000

    [FIX] website: escape single quote in technical page title
    **Steps to reproduce:**
    * Install **Website Sale** modules with demo data.
    * Change the language to **French (fr)** from settings.
    * Open  **Website → Site → Technical Page**.
    * The technical pages view fails to load.
    
    **Observed behavior:**
    
    An Odoo **RPC_ERROR** is raised.
    
    ```
    self._obj.execute(query, params)
    psycopg2.errors.UndefinedColumn: column ''S'inscrire'' does not exist
    LINE 5: ...'), ('Tableau de bord utilisateur', '/my/home'), (''S'inscrir...
    ```
    
    **Cause:**
    * Method `website.technical.page()._table_query` uses raw values without
      correctly escaping them since 25efaf49b6691d1b5aeba40a016134660fe2358c
    * A new route title translation ("S'inscrire" for "Sign Up") was added
      as french translation.
    
    **Fix:**
    
    Use SQL class to correctly escape litterals when using them.
    
    opw-5354982
    
    closes odoo/odoo#237858
    
    X-original-commit: 4dc159852663e52d0f73384d65cc8cbb120f326b
    Signed-off-by: Soukéina Bojabza (sobo) <sobo@odoo.com>
    Signed-off-by: Pawan Kumar Gupta (pkgu) <pkgu@odoo.com>

--- addons/website/models/website_technical_page.py
+++ addons/website/models/website_technical_page.py
@@ -48,11 +48,14 @@ class WebsiteTechnicalPage(models.Model):
     @property
     def _table_query(self):
         routes = self._get_static_routes()
-        values = ", ".join(str(route) for route in routes)
+        values = SQL(", ").join(
+            SQL('(%s, %s)', route_title, route_path)
+            for route_title, route_path in routes
+        )
 
         return SQL("""
             SELECT row_number() OVER (ORDER BY UPPER(column1) ASC) AS id,
                 column1 AS name,
                 column2 AS website_url
             FROM (VALUES %s) AS t(column1, column2)
-        """ % values)
+        """, values)
