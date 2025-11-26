4219fc02d491953b5a920f23862ddbaaec520653
yush-odoo <yush@odoo.com>
2025-11-26 17:58:05 +0400

    [IMP] l10n_tr_nilvera, l10n_tr_nilvera_envoice: Add Helpers
    Add contextual help tooltips for newly introduced fields across both
    modules. Several fields were added without sufficient explanations,
    causing confusion and validation errors. Expanded and refined the help
    attributes on all relevant fields to provide clear guidance and prevent
    future data entry issues.

--- addons/l10n_tr_nilvera_einvoice/models/res_config_settings.py
+++ addons/l10n_tr_nilvera_einvoice/models/res_config_settings.py
@@ -5,7 +5,11 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     l10n_tr_nilvera_export_alias = fields.Char(
-        related='company_id.l10n_tr_nilvera_export_alias',
+        related="company_id.l10n_tr_nilvera_export_alias",
         string="Nilvera Export Alias",
         readonly=False,
+        help="Set the default GİB 'Posta Kutusu' (mailbox) alias for sending "
+        "export invoices (İhracat Faturası). \nThis is almost always "
+        "'urn:mail:ihracatpk@gtb.gov.tr' (Turkish Ministry of Trade) and "
+        "should not be changed unless you have a specific reason.",
     )
