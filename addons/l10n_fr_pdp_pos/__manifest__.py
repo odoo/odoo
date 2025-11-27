c9838ce66f66fed393329baf8edd99fb95ef9d04
Jérémy Bazin (baje) <baje@odoo.com>
2025-11-27 08:07:08 +0100

    [IMP] l10n_fr_pdp: Flux 10 e-reporting
    Implements French PDP Flux 10 e-reporting (B2C domestic + international B2B transactions)
    based on a defined periodicity for transactions and payements.
    
    Task-5065162
    
    Part-of: odoo/odoo#267151
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>

---
+++ addons/l10n_fr_pdp_pos/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': "France - E-reporting for POS",
+    'category': 'Accounting/Localizations/Reporting',
+    'summary': 'PDP Flux 10 e-reporting for POS',
+    'depends': ['l10n_fr_pdp', 'point_of_sale'],
+    'auto_install': True,
+    'license': 'LGPL-3',
+    'author': 'Odoo SA',
+}
