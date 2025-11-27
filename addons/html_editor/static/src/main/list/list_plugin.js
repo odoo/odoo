1bacb0467076550cd395e969f809dcdbdcf66d92
Adnan Saiyed <adsa@odoo.com>
2025-11-27 06:58:14 +0000

    [FIX] html_editor: fix tripleclick on checkbox
    Current behavior before commit:
    
    Currently, triple clicking on a checkbox in a checklist
    item ends up selecting the list content. This happens because
    in `selection_plugin` `onTripleClick` handler selects the whole
    list item.
    
    Desired behavior after commit:
    
    This commit ensures that `onTripleClick` does nothing if
    tripleclick is triggered when checking a box.
    
    task-5361579
    
    X-original-commit: 5fda00a25de3e0ccb3450eaa72445407099f613d

--- addons/html_editor/static/src/main/list/list_plugin.js
+++ addons/html_editor/static/src/main/list/list_plugin.js
@@ -137,6 +137,7 @@ export class ListPlugin extends Plugin {
         node_to_insert_processors: this.processNodeToInsert.bind(this),
         move_node_whitelist_selectors: "ol, ul",
         before_insert_within_pre_processors: this.insertListWithinPre.bind(this),
+        triple_click_overrides: this.handleTripleClick.bind(this),
     };
 
     setup() {
@@ -963,6 +964,16 @@ export class ListPlugin extends Plugin {
             }
             ev.preventDefault();
             this.dependencies.history.addStep();
+        }
+    }
+
+    handleTripleClick(ev) {
+        const node = ev.target;
+        const isChecklistItem =
+            node.tagName === "LI" && this.getListMode(node.parentElement) === "CL";
+        if (isChecklistItem && this.isPointerInsideCheckbox(node, ev.offsetX, ev.offsetY)) {
+            // If pointer is inside checkbox, prevent tripleclick selection.
+            return true;
         }
     }
 
