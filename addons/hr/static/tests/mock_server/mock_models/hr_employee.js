41e0fa19b417a3a8e288513e59813dbcc51e563c
K Theja (kthe@odoo.com) <kthe@odoo.com>
2025-11-27 12:24:25 +0530

    [IMP] hr: add method to fetch employee contract periods #191917
    In this commit a method is added to find contract working periods in a specific
    intervals.
    
    Moved in a test case covering the new method.
    
    task-3885152

--- addons/hr/static/tests/mock_server/mock_models/hr_employee.js
+++ addons/hr/static/tests/mock_server/mock_models/hr_employee.js
@@ -1,5 +1,6 @@
 import { mailDataHelpers } from "@mail/../tests/mock_server/mail_mock_server";
 
+import { Domain } from "@web/core/domain";
 import { fields, models } from "@web/../tests/web_test_helpers";
 
 export class HrEmployee extends models.ServerModel {
@@ -39,6 +40,45 @@ export class HrEmployee extends models.ServerModel {
             );
         }
         return res;
+    }
+
+    _get_working_periods_by_field(employeeIds, start_time, end_time, field_key) {
+        const employeeToFieldId = Object.fromEntries(
+            this.env["hr.employee"]
+                .browse(employeeIds)
+                .map((employee) => [employee.id, employee[field_key]])
+        );
+        const periodsByFieldId = Object.fromEntries(
+            Object.values(employeeToFieldId).map((fieldId) => [fieldId, []])
+        );
+        const fieldPath = `employee_id.${field_key}`;
+        if (employeeIds.size) {
+            const contractGroups = this.env["hr.version"].formatted_read_group(
+                new Domain([["employee_id", "in", [...employeeIds]]]).toList(),
+                ["employee_id", fieldPath, "contract_date_start:day", "contract_date_end:day"],
+                [],
+                "",
+                "",
+                ""
+            );
+            contractGroups.forEach((group) => {
+                const fieldId = group[fieldPath][0];
+                periodsByFieldId[fieldId].push({
+                    start: group["contract_date_start:day"][1],
+                    end: group["contract_date_end:day"][1],
+                });
+            });
+
+            const employeesWithContracts = new Set(contractGroups.map((g) => g.employee_id[0]));
+
+            employeeIds.difference(employeesWithContracts).forEach((employeeId) => {
+                periodsByFieldId[employeeToFieldId[employeeId]].push({
+                    start: start_time,
+                    end: end_time,
+                });
+            });
+        }
+        return periodsByFieldId;
     }
 
     _views = {
