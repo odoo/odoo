164b08c881c7ceb2c9a8353473cc2ba82aa77d99
paan-odoo <paan@odoo.com>
2025-11-28 11:58:40 +0530

    [IMP] pos_{, restaurant}: allow early receipt print in retails
    *: pos_self_order
    
    Before this commit:
    
    - Early receipt printing was only available in restaurant POS configs.
    
    After this commit:
    
    - Early receipt printing is now allowed in both retail and restaurant POS setups.
    
    Related PR:
    - Upgrade: https://github.com/odoo/upgrade/pull/8981
    
    Task-5365571
    
    closes odoo/odoo#238298
    
    Related: odoo/upgrade#8981
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/pos_restaurant/static/src/app/screens/product_screen/control_buttons/control_buttons.js
+++ addons/pos_restaurant/static/src/app/screens/product_screen/control_buttons/control_buttons.js
@@ -1,7 +1,6 @@
 import { ControlButtons } from "@point_of_sale/app/screens/product_screen/control_buttons/control_buttons";
 import { SelectPartnerButton } from "@point_of_sale/app/screens/product_screen/control_buttons/select_partner_button/select_partner_button";
 import { useService } from "@web/core/utils/hooks";
-import { useAsyncLockedMethod } from "@point_of_sale/app/hooks/hooks";
 import { patch } from "@web/core/utils/patch";
 
 patch(ControlButtons.prototype, {
@@ -9,13 +8,6 @@ patch(ControlButtons.prototype, {
         super.setup(...arguments);
         this.alert = useService("alert");
         this.printer = useService("printer");
-        this.clickPrintBill = useAsyncLockedMethod(this.clickPrintBill);
-    },
-    async clickPrintBill() {
-        // Need to await to have the result in case of automatic skip screen.
-        await this.pos.printReceipt({
-            printBillActionTriggered: true,
-        });
     },
     clickTableGuests() {
         this.pos.setCustomerCount();
