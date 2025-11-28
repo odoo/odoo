3537c492e948156963c6728538ac2c9e4d568e6e
JF Aubert <ajf@odoo.com>
2025-11-28 12:45:38 +0100

    [FIX] mrp: fix overview for bom with decimal quantity
    When a BoM has a decimal quantity, the operations' times are not
    correctly computed.
    
    task: 5366973

--- addons/mrp/tests/test_bom.py
+++ addons/mrp/tests/test_bom.py
@@ -2813,6 +2813,32 @@ class TestBoM(TestMrpCommon):
         mo.button_mark_done()
         self.assertEqual(mo.finished_move_line_ids.product_uom_id, self.uom_dozen)
 
+    def test_bom_overview_with_decimal_quantity(self):
+        """
+        Checks that the times for a bom with a decimal quantity
+        are correctly computed in the bom overview
+        """
+        product_in_square_meter = self.env['product.template'].create({
+            'name': 'test',
+            'uom_id': self.env.ref('uom.product_uom_square_meter').id,
+        })
+        workcenter = self.env['mrp.workcenter'].create({
+            'name': 'workcenter',
+        })
+        bom = self.env['mrp.bom'].create({
+            'product_tmpl_id': product_in_square_meter.id,
+            'product_qty': 1.5,
+            'operation_ids': [
+                Command.create({'name': 'operation', 'workcenter_id': workcenter.id, 'time_cycle_manual': 16}),
+            ],
+        })
+        bom_overview = self.env['report.mrp.report_bom_structure']._get_report_data(bom.id, 1.4)['lines']
+        self.assertEqual(bom_overview['operations_time'], 16)
+        bom_overview = self.env['report.mrp.report_bom_structure']._get_report_data(bom.id, 1.5)['lines']
+        self.assertEqual(bom_overview['operations_time'], 16)
+        bom_overview = self.env['report.mrp.report_bom_structure']._get_report_data(bom.id, 1.6)['lines']
+        self.assertEqual(bom_overview['operations_time'], 32)
+
 
 @tagged('-at_install', 'post_install')
 class TestTourBoM(HttpCase):
