69d759e3ba2495f40df79ffb6d35afeb982a8984
Rémy Voet (ryv) <ryv@odoo.com>
2025-11-30 12:08:14 +0100

    [IMP] core: remove exists from ref

--- addons/base_import_module/models/ir_module.py
+++ addons/base_import_module/models/ir_module.py
@@ -383,6 +383,7 @@ class IrModuleModule(models.Model):
                         path = opj(module_dir, mod_name)
                         self.sudo()._import_module(mod_name, path, force=force, with_demo=with_demo)
                     except Exception as e:
+                        self.env.registry.clear_cache()  # _xmlid_lookup for test_import_zip_invalid_data
                         raise UserError(_(
                             "Error while importing module '%(module)s'.\n\n %(error_message)s \n\n",
                             module=mod_name, error_message=traceback.format_exc(),
