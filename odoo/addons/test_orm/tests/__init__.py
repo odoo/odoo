55a8a2df7c48c75857da181a8758d6468ca52122
Ruben Gomes <rugo@odoo.com>
2025-12-01 10:50:46 +0100

    [MOV] test_orm: test_attributes into test_fields
    closes odoo/odoo#238102
    
    Signed-off-by: Krzysztof Magusiak (krma) <krma@odoo.com>

--- odoo/addons/test_orm/tests/__init__.py
+++ odoo/addons/test_orm/tests/__init__.py
@@ -2,7 +2,6 @@ from . import (
     test_access_feedback,
     test_access_monetary_related,
     test_action_bindings,
-    test_attributes,
     test_autovacuum,
     test_check_access,
     test_company_checks,
