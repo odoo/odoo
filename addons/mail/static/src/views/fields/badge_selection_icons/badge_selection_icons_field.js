e465e6a1069a7fa13065dcd0e07a358ac47190ed
Jérôme Belpaire <beje@odoo.com>
2025-12-01 17:19:33 +0100

    [IMP] mail: genericize badge selection icon widget
    Prior to this commit, the `selection_badge_icons` widget
    (`BadgeSelectionWithIconsField`) was designed exclusively for `Many2one`
    fields. It relied on fetching a specific icon field from the related
    model via `specialData`.
    
    However, other modules (such as Survey) require similar UI capabilities
    for simple `Selection` fields, specifically:
    1. Displaying icons on badges (either defined statically in the view or
       computed dynamically via a field).
    2. Filtering visible options based on another field (similar to
       `BadgeSelectionWithFilterField` in `web`).
    
    This commit enhances `BadgeSelectionWithIconsField` to support `Selection`
    fields alongside the existing `Many2one` support.
    
    Changes:
    - Added support for `Selection` fields by mapping standard options to
      icons provided via a static `icon_mapping` option or a dynamic
      `related_icon_field`.
    - Integrated the filtering logic from `BadgeSelectionWithFilterField`
      as an optional feature via the `allowedSelectionField` prop.
    - Changed the initial attrs to options as they are now optional
    
    This allows a single widget to handle Many2one icons, Selection icons,
    and option filtering, preventing code duplication across modules.
    
    Task-4778050
    
    Part-of: odoo/odoo#210137
    Related: odoo/enterprise#101055
    Related: odoo/upgrade#8955
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mail/static/src/views/fields/badge_selection_icons/badge_selection_icons_field.js
+++ addons/mail/static/src/views/fields/badge_selection_icons/badge_selection_icons_field.js
@@ -8,44 +8,70 @@ import {
 } from "@web/views/fields/badge_selection/badge_selection_field";
 
 /**
- * @typedef BadgeSelectionIconsField
- * Overrides the standard BadgeSelectionField and inserts FontAwesome icons before each option's title.
- * Only compatible with Many2one selectors. Related options should have an "icon" field, the name
- * of which should be specified through the iconField prop.
- *
- * Special props:
- * @param {String} iconField The name of the field on which the icon is stored on the many2one option
- * @param {String} defaultIcon If the field pointed through iconField is empty on the related record, a default fa icon can be specified.
+ * Extends the standard BadgeSelectionField to support icons and filtering.
+ * 1. Many2one: Icons fetched from a related field on the co-model (defined by `relatedIconField`).
+ * 2. Selection: Icons mapped via static options (`iconMapping`).
+ *    In addition, selection options can be filtered by providing an `allowedSelectionField` field,
+ *    a mapping of values to icons (that will be overridden by `iconMapping` if provided).
  */
 export class BadgeSelectionWithIconsField extends BadgeSelectionField {
     static props = {
         ...BadgeSelectionField.props,
-        iconField: { type: String },
-        defaultIcon: { type: String, optional: true, default: "fa-check" },
+        defaultIcon: { type: String, optional: true },
+        // --- Many2one ---
+        relatedIconField: { type: String, optional: true },
+        // --- Selection ---
+        // Static mapping from XML options: { 'selection_key': 'fa-icon' }
+        iconMapping: { type: Object, optional: true },
+        // Field name used to filter the visible selection options
+        allowedSelectionField: { type: String, optional: true },
+    };
+    static defaultProps = {
+        defaultIcon: "fa-check",
+        iconMapping: {},
     };
     static template = "mail.BadgeSelectionIconsField";
 
-    /**
-     * @override
-     * many2one fields use attribute "specialData" to store information pertaining to many2one relations.
-     * As such, this.specialData is used by the inherited BadgeSelectionField to store the Many2one selection options for this field.
-     */
     async setup() {
+        super.setup();
         this.type = this.props.record.fields[this.props.name].type;
-        this.specialData = useSpecialData(async (orm, props) => {
-            const domain = getFieldDomain(props.record, props.name, props.domain);
-            const { relation } = props.record.fields[props.name];
-            const ret = await orm.call(relation, "search_read", [], {
-                domain: domain,
-                fields: ["id", "name", props.iconField],
+
+        if (this.type === "many2one") {
+            this.specialData = useSpecialData(async (orm, props) => {
+                const domain = getFieldDomain(props.record, props.name, props.domain);
+                const { relation } = props.record.fields[props.name];
+
+                const ret = await orm.call(relation, "search_read", [], {
+                    domain: domain,
+                    fields: ["id", "name", props.relatedIconField],
+                });
+
+                return ret.map((opt) => {
+                    const iconValue = opt[props.relatedIconField] || props.defaultIcon;
+                    return [opt.id, opt.name, iconValue];
+                });
             });
-            return ret.map((opt) => {
-                const option = Object.values(opt);
-                if (!option[2]) {
-                    option[2] = props.defaultIcon;
-                }
-                return option;
-            });
+        }
+    }
+
+    get options() {
+        if (this.type === "many2one") {
+            return this.specialData.data;
+        }
+
+        let baseOptions = super.options;
+        let icons = {};
+
+        if (this.props.allowedSelectionField) {
+            icons = this.props.record.data[this.props.allowedSelectionField];
+            const allowedValues = Object.keys(icons);
+            baseOptions = baseOptions.filter(([value]) => allowedValues.includes(value));
+        }
+        Object.assign(icons, this.props.iconMapping);
+
+        return baseOptions.map(([value, label]) => {
+            const icon = icons[value] || this.props.defaultIcon;
+            return [value, label, icon];
         });
     }
 }
@@ -53,12 +79,19 @@ export class BadgeSelectionWithIconsField extends BadgeSelectionField {
 export const badgeSelectionWithIconsField = {
     ...badgeSelectionField,
     component: BadgeSelectionWithIconsField,
-    supportedTypes: ["many2one"],
+    supportedTypes: ["many2one", "selection"],
     displayName: _t("Badges with Icons"),
-    extractProps: (fieldInfo, dynamicInfo) => ({
-        ...badgeSelectionField.extractProps(fieldInfo, dynamicInfo),
-        iconField: fieldInfo.attrs.iconField,
-        defaultIcon: fieldInfo.attrs.defaultIcon,
-    }),
+    extractProps: (fieldInfo, dynamicInfo) => {
+        const baseProps = badgeSelectionField.extractProps(fieldInfo, dynamicInfo);
+        const { options } = fieldInfo;
+        return {
+            ...baseProps,
+            defaultIcon: options.default_icon,
+            relatedIconField: options.related_icon_field,
+            iconMapping: options.icon_mapping,
+            allowedSelectionField: options.allowed_selection_field,
+        };
+    },
 };
+
 registry.category("fields").add("selection_badge_icons", badgeSelectionWithIconsField);
