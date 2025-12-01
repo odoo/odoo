480246b1632ca76aa50feb010b73ecd01cd2dc38
Xavier Morel <xmo@odoo.com>
2025-12-01 13:56:02 +0100

    [FIX] base: ensure ir.attachment doesn't guess_mimetypes str
    `IrAttachment.create` first `_check_contents` which
    `_compute_mimetype` which `guess_mimetype` which formerly decoded an
    `str` input and now errors, *then* encodes the `raw` anyway in order
    to checksum and index it.
    
    Decode `raw` upfront if present.
    
    While at it decode and move `datas` to `raw` that way downstream
    functions (`_compute_mimetype`, `_postprocess_contents`) have no need
    to repeatedly deal with and b64decode `datas`.

--- odoo/tools/mimetypes.py
+++ odoo/tools/mimetypes.py
@@ -150,6 +150,8 @@ _mime_mappings = (
     # zip, but will include jar, odt, ods, odp, docx, xlsx, pptx, apk
     _Entry('application/zip', [b'PK\x03\x04'], [_check_ooxml, _check_open_container_format]),
 )
+
+
 def _odoo_guess_mimetype(bin_data: bytes, default='application/octet-stream'):
     """ Attempts to guess the mime type of the provided binary data, similar
     to but significantly more limited than libmagic
