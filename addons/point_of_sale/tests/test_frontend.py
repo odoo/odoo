ac6612474854915e56dc047fbf66dc90c19e71a2
Arthur Nanson <artn@odoo.com>
2025-12-01 10:29:51 +0000

    [FIX] point_of_sale: prevent year sensitive runbot error
    **Why the fix:**
    The date was frozen in the backend but it did not work in
    the case where we are offline. This happens because when
    offline we get the date in the frontend by creating a
    *new Date()*, which is not affected by either the
    backend or the frontend freezeDate.
    This means that when the year changes, this test would fail,
    as we check that the year is 2025 by hardcoding it.
    
    To avoid this, we now use the pos_reference value to build
    this part back together.
    
    runbot-233565
    
    closes odoo/odoo#240373
    
    X-original-commit: bb7cf201e77874606890493173c1163f8e54978e
    Signed-off-by: Xavier Dollé (xdo) <xdo@odoo.com>
    Signed-off-by: Arthur Nanson (artn) <artn@odoo.com>

--- addons/point_of_sale/tests/test_frontend.py
+++ addons/point_of_sale/tests/test_frontend.py
@@ -3106,7 +3106,6 @@ class TestUi(TestPointOfSaleHttpCommon):
         self.pos_user.write({'lang': 'fr_BE'})
         self.start_tour(f"/pos/ui?config_id={self.main_pos_config.id}", 'test_add_money_button_with_different_decimal_separator', login="pos_user")
 
-    @freeze_time("2025-08-28 04:15:00")
     def test_consistent_order_receipt_number_offline(self):
         """
         Tests that an order will have the same receipt number if it was done
@@ -3119,9 +3118,9 @@ class TestUi(TestPointOfSaleHttpCommon):
 
         orders = self.env['pos.order'].search([], order="id desc", limit=2)
         # Online, with a 0
-        self.assertEqual(orders[1].pos_reference, f"2501-{orders[1].session_id.id:03d}-00001")
+        self.assertEqual(orders[1].pos_reference, f"{orders[1].pos_reference[:2]}01-{orders[1].session_id.id:03d}-00001")
         # Offline, with a 1 and no Order
-        self.assertEqual(orders[0].pos_reference, f"2501-{orders[0].session_id.id:03d}-10001")
+        self.assertEqual(orders[0].pos_reference, f"{orders[0].pos_reference[:2]}01-{orders[0].session_id.id:03d}-10001")
 
     def test_sync_from_ui_one_by_one(self):
         """
