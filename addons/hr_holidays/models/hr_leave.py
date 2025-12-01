2ff24740a21c589bda9d57fb0781fb312aaf789b
nsirjacobs <nosir@odoo.com>
2025-12-01 17:08:15 +0100

    [FIX] hr_holidays: Allow holiday responsible to unlink resource_calendar_leaves of related employees
    Behavior:
    When a user who is Time off Responsible but not Officer or Admin and is marked as responsible for an employee tries to
    refuse a fully accepted leave an access error occurs.
    This occurs because approving a leave creates a resource.calendar.leave linked to the hr.leave, and when refusing it, the system will try to unlink it.
    According to access rules of r.calendar.leaves, Time-off Responsibles are not allowed any access for entries of other employees.
    Approving them doesnt cause an error because a the create operation uses sudo(), assuming that the user must have had valid rights to access the function.
    This is not the case for the unlink.
    
    Solution:
    This fix checks wether the user has write access to the related hr.leave record, then uses sudo().
    The idea is that if they are allowed to modify the state of the leave, they can refuse the leave, and therefore should be able to go through the following operations.
    This is in a similar fashion as the behavior for approving the leaves.
    
    Steps to Reproduce:
    - Create a Leave Type that requires validation from a manager, and assign one to an employee.
    - Remove any access rights to Time-Off for the user, but leave Time off Responsible checked.
    - Assign the user as time-off responsible for the employee
    - Log in as the user and approve the leave
    - When refusing the leave, you should get an access error.
    
    opw-5178783

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -902,6 +902,8 @@ Attempting to double-book your time off won't magically make your vacation 2x be
 
     def _remove_resource_leave(self):
         """ This method will create entry in resource calendar time off object at the time of holidays cancel/removed """
+        if self.has_access('write'):
+            return self.env['resource.calendar.leaves'].search([('holiday_id', 'in', self.ids)]).sudo().unlink()
         return self.env['resource.calendar.leaves'].search([('holiday_id', 'in', self.ids)]).unlink()
 
     def _validate_leave_request(self):
