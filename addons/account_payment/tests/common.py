a88cf9bb3b3d03752766d698c7885eda2c3f1e61
Laurent Smet (LAS) <las@odoo.com>
2025-12-01 16:42:53 +0000

    [IMP] account: Add test ensuring peppol status updated automatically
    When creating a SO on e-commerce leading to an automatic invoice,
    the invoice is automatically sent using Peppol upon the payment.
    This test ensures the peppol status of the partner is updated
    during the process.
    
    task_id: 5025176
    
    closes odoo/odoo#239965
    
    X-original-commit: 897dd0c08019c27d60eb4fcb27bc9cca2eb660f3
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

--- addons/account_payment/tests/common.py
+++ addons/account_payment/tests/common.py
@@ -12,13 +12,10 @@ class AccountPaymentCommon(PaymentCommon, AccountTestInvoicingCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        with cls.mocked_get_payment_method_information(cls):
-            cls.dummy_provider_method = cls.env['account.payment.method'].sudo().create({
-                'name': 'Dummy method',
-                'code': 'none',
-                'payment_type': 'inbound'
-            })
-            cls.dummy_provider.journal_id = cls.company_data['default_journal_bank']
+        cls.dummy_provider_method = cls._create_dummy_payment_method_for_provider(
+            provider=cls.dummy_provider,
+            journal=cls.company_data['default_journal_bank'],
+        )
 
         cls.account = cls.outbound_payment_method_line.payment_account_id
         cls.invoice = cls.env['account.move'].create({
@@ -47,17 +44,3 @@ class AccountPaymentCommon(PaymentCommon, AccountTestInvoicingCommon):
     def setUp(self):
         self.enable_post_process_patcher = False
         super().setUp()
-
-    #=== Utils ===#
-
-    @contextmanager
-    def mocked_get_payment_method_information(self):
-        Method_get_payment_method_information = self.env['account.payment.method']._get_payment_method_information
-
-        def _get_payment_method_information(*args, **kwargs):
-            res = Method_get_payment_method_information()
-            res['none'] = {'mode': 'electronic', 'type': ('bank',)}
-            return res
-
-        with patch.object(self.env.registry['account.payment.method'], '_get_payment_method_information', _get_payment_method_information):
-            yield
