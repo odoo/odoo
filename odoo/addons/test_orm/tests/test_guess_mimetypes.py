2de136cc694f32f2d7c293000e761748efaefae3
Ruben Gomes <rugo@odoo.com>
2025-12-02 13:17:45 +0100

    RUGO

--- odoo/addons/test_orm/tests/test_guess_mimetypes.py
+++ odoo/addons/test_orm/tests/test_guess_mimetypes.py
@@ -2,7 +2,7 @@ import os.path
 import unittest
 
 from odoo.tests import tagged, BaseCase
-from odoo.tools.mimetypes import _odoo_guess_mimetype, guess_mimetype
+from odoo.tools.mimetypes import _odoo_guess_mimetype, fix_filename_extension, get_extension, guess_mimetype
 from odoo.tools.misc import file_open
 
 
@@ -85,12 +85,99 @@ class MimeGuessingCases:
             'text/plain',
         )
 
-    def test_unkown(self):
+    def test_unknown(self):
         self.assertEqual(
             self.guess_mimetype(b"\1\2\3\1\2\3\1\2\3\1\2\3\1\2\3"),
             'application/octet-stream',
         )
 
+    def test_ppt(self):
+        self.assertEqual(
+            self.guess_mimetype(contents('ppt')),
+            'application/vnd.ms-powerpoint',
+        )
+
+    def test_pptx(self):
+        self.assertEqual(
+            self.guess_mimetype(contents('pptx')),
+            'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+        )
+
+    def test_pdf(self):
+        self.assertEqual(
+            self.guess_mimetype(contents("pdf")),
+            "application/pdf",
+        )
+
+    def test_ico(self):
+        self.assertEqual(
+            self.guess_mimetype(contents("ico")),
+            "image/vnd.microsoft.icon",
+        )
+
+    def test_webp(self):
+        self.assertEqual(
+            self.guess_mimetype(contents('webp')),
+            'image/webp',
+        )
+
+    def test_xml(self):
+        self.assertEqual(
+            self.guess_mimetype(contents('xml')),
+            'text/xml',
+        )
+
+    def test_png(self):
+        self.assertEqual(
+            self.guess_mimetype(contents('png')),
+            'image/png',
+        )
+
+    def test_bmp(self):
+        # mimetype should match image/bmp, image/x-ms-bmp, ...
+        # r'image/.*\bbmp'
+        self.assertEqual(
+            self.guess_mimetype(contents('bmp')),
+            'image/bmp',
+        )
+
+    def test_svg(self):
+        self.assertEqual(
+            self.guess_mimetype(contents('svg')),
+            'image/svg+xml',
+        )
+
+    def test_namespaced_svg(self):
+        self.assertEqual(
+            self.guess_mimetype(contents('namespaced.svg')),
+            'image/svg+xml',
+        )
+
+    def test_empty(self):
+        self.assertEqual(
+            self.guess_mimetype(b''),
+            'application/x-empty',
+        )
+
+    def test_mimetype_octet_stream(self):
+        self.assertEqual(
+            self.guess_mimetype(b'\0'),
+            "application/octet-stream",
+        )
+
+    def test_whitespace_svg(self):
+        # TODO
+        # Tests that whitespace padded SVG are not detected as SVG in odoo implementation
+        # if not magic:
+        #     mimetype = guess_mimetype(b"   " + content, default='test')
+        #     self.assertNotIn("svg", mimetype)
+        pass
+
+    def test_default_mimetype_empty(self):
+        self.assertEqual(
+            self.guess_mimetype(b"""Hello world!"""),
+            'text/plain',
+       )
 
 @tagged('at_install', '-post_install')  # LEGACY at_install
 class TestMimeGuessingOdoo(BaseCase, MimeGuessingCases):
@@ -102,6 +189,11 @@ class TestMimeGuessingOdoo(BaseCase, MimeGuessingCases):
             'text/plain',
         )
 
+    def test_default(self):
+        self.assertEqual(
+            self.guess_mimetype(b"\1\2\3\1\2\3\1\2\3\1\2\3\1\2\3", default='test'),
+            'test',
+        )
 
 @unittest.skipIf(guess_mimetype is _odoo_guess_mimetype, "python-magic not installed")
 @tagged('at_install', '-post_install')  # LEGACY at_install
@@ -113,3 +205,45 @@ class TestMimeGuessingMagic(BaseCase, MimeGuessingCases):
             self.guess_mimetype(contents('csv')),
             'text/csv',
         )
+
+    def test_default(self):
+        self.assertEqual(
+            self.guess_mimetype(b"", default='test'),
+            "application/x-empty",
+        )
+
+@tagged('at_install', '-post_install')  # LEGACY at_install
+class test_guess_mimetype(BaseCase):
+    def test_mimetype_get_extension(self):
+        self.assertEqual(get_extension('filename.Abc'), '.abc')
+        self.assertEqual(get_extension('filename.scss'), '.scss')
+        self.assertEqual(get_extension('filename.torrent'), '.torrent')
+        self.assertEqual(get_extension('filename.ab_c'), '.ab_c')
+        self.assertEqual(get_extension('.htaccess'), '')
+        # enough to suppose that extension is present and don't suffix the filename
+        self.assertEqual(get_extension('filename.tar.gz'), '.gz')
+        self.assertEqual(get_extension('filename'), '')
+        self.assertEqual(get_extension('filename.'), '')
+        self.assertEqual(get_extension('filename.not_alnum'), '')
+        self.assertEqual(get_extension('filename.with space'), '')
+        self.assertEqual(get_extension('filename.notAnExtension'), '')
+
+    def test_mimetype_fix_extension(self):
+        fix = fix_filename_extension
+        self.assertEqual(fix('words.txt', 'text/plain'), 'words.txt')
+        self.assertEqual(fix('image.jpg', 'image/jpeg'), 'image.jpg')
+        self.assertEqual(fix('image.jpeg', 'image/jpeg'), 'image.jpeg')
+        self.assertEqual(fix('sheet.xls', 'application/vnd.ms-excel'), 'sheet.xls')
+        self.assertEqual(fix('sheet.xls', 'application/CDFV2'), 'sheet.xls')
+        xlsx_mime = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        self.assertEqual(fix('sheet.xlsx', xlsx_mime), 'sheet.xlsx')
+        self.assertEqual(fix('sheet.xlsx', 'application/zip'), 'sheet.xlsx')
+        with self.assertLogs('odoo.tools.mimetypes', 'WARNING') as capture:
+            self.assertEqual(fix('image.txt', 'image/jpeg'), 'image.txt.jpg')
+            self.assertEqual(fix('words.jpg', 'text/plain'), 'words.jpg.txt')
+        self.assertEqual(capture.output, [
+            "WARNING:odoo.tools.mimetypes:File 'image.txt' has an invalid "
+                "extension for mimetype 'image/jpeg', adding '.jpg'",
+            "WARNING:odoo.tools.mimetypes:File 'words.jpg' has an invalid "
+                "extension for mimetype 'text/plain', adding '.txt'",
+        ])
--- odoo/tools/mimetypes.py
+++ odoo/tools/mimetypes.py
@@ -26,6 +26,241 @@ _ooxml_dirs = {
     'pt/': 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
     'xl/': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
 }
+
+_Signature = collections.namedtuple('_Signature', ['magic_number', 'offset'])
+
+
+def _match_magic_number(bin_data, *, all=(), any=()):
+    for signature in all:
+        if bin_data[signature.offset:signature.offset + len(signature.magic_number)] != signature.magic_number:
+            return False
+
+    if not any:
+        return True
+
+    for signature in any:
+        if bin_data[signature.offset:signature.offset + len(signature.magic_number)] == signature.magic_number:
+            return True
+
+    return False
+
+
+def _match_office_open_xml(bin_data, *, dirname):
+    if not _is_zip(bin_data):
+        return False
+
+    with io.BytesIO(bin_data) as file, zipfile.ZipFile(file) as zip:
+        filenames = zip.namelist()
+
+        if '[Content_Types].xml' not in filenames:
+            return False
+
+        return any(entry.startswith(dirname) for entry in filenames)
+
+
+def _match_open_document(bin_data, *, dirname):
+    if not _is_zip(bin_data):
+        return False
+
+    mime_validator = re.compile(
+        r"""
+        [\w-]+ # type-name
+        / # subtype separator
+        [\w-]+ # registration facet or subtype
+        (?:\.[\w-]+)* # optional faceted name
+        (?:\+[\w-]+)? # optional structured syntax specifier
+    """, re.VERBOSE)
+
+    with io.BytesIO(bin_data) as file, zipfile.ZipFile(file) as zip:
+        filenames = zip.namelist()
+
+        if 'mimetype' not in filenames:
+            return False
+
+        marcel = zip.read('mimetype').decode('ascii')
+
+        return len(marcel) < 256 and mime_validator.match(marcel) and dirname in marcel
+
+
+def _is_cfb(bin_data):
+    return _match_magic_number(bin_data, all=[_Signature(bytes.fromhex('D0 CF 11 E0 A1 B1 1A E1'), 0)])
+
+
+def _is_empty(bin_data):
+    return not bin_data
+
+
+def _is_jpg(bin_data):
+    return _match_magic_number(bin_data, all=[_Signature(bytes.fromhex('FF D8 FF'), 0)])
+
+
+def _is_png(bin_data):
+    return _match_magic_number(bin_data, all=[_Signature(bytes.fromhex('89 50 4E 47'), 0)])
+
+
+def _is_gif(bin_data):
+    return _match_magic_number(bin_data, all=[_Signature(b'GIF', 0)])
+
+
+def _is_bmp(bin_data):
+    return _match_magic_number(bin_data, all=[_Signature(b'BM', 0)])
+
+
+def _is_ico(bin_data):
+    return _match_magic_number(bin_data, all=[_Signature(bytes.fromhex('00 00 01 00'), 0)])
+
+
+def _is_webp(bin_data):
+    return _match_magic_number(bin_data, all=[
+        _Signature(b'RIFF', 0),
+        _Signature(b'WEBP', 8),
+    ])
+
+
+def _is_zip(bin_data):
+    return _match_magic_number(bin_data, all=[_Signature(bytes.fromhex('50 4B 03 04'), 0)])
+
+
+def _is_docx(bin_data):
+    return _match_office_open_xml(bin_data, dirname='word/')
+
+
+def _is_pptx(bin_data):
+    return _match_office_open_xml(bin_data, dirname='ppt/')
+
+
+def _is_xlsx(bin_data):
+    return _match_office_open_xml(bin_data, dirname='xl/')
+
+
+def _is_odt(bin_data):
+    return _match_open_document(bin_data, dirname='text')
+
+
+def _is_ods(bin_data):
+    return _match_open_document(bin_data, dirname='spreadsheet')
+
+
+def _is_ppt(bin_data):
+    if not _is_cfb(bin_data):
+        return False
+
+    any_signatures_1 = [
+        _Signature(bytes.fromhex('00 6E 1E F0'), 512),
+        _Signature(bytes.fromhex('0F 00 E8 03'), 512),
+        _Signature(bytes.fromhex('A0 46 1D F0'), 512),
+    ]
+
+    all_signatures_2 = [
+        _Signature(bytes.fromhex('FD FF FF FF'), 512),
+        _Signature(bytes.fromhex('00 00'), 522),
+    ]
+
+    all_signatures_3 = [
+        _Signature(b'\x00\xB9\x29\xE8\x11\x00\x00\x00MS PowerPoint 97', 2072),
+    ]
+
+    return any([
+        _match_magic_number(bin_data, any=any_signatures_1),
+        _match_magic_number(bin_data, all=all_signatures_2),
+        _match_magic_number(bin_data, all=all_signatures_3),
+    ])
+
+
+def _is_pdf(bin_data):
+    return _match_magic_number(bin_data, all=[_Signature(b'%PDF-', 0)])
+
+
+def _is_doc(bin_data):
+    if _is_cfb(bin_data):
+        return _match_magic_number(bin_data, all=[_Signature(bytes.fromhex('EC A5 C1 00'), 512)])  # Word
+
+    return _match_magic_number(bin_data, all=[_Signature(bytes.fromhex('0D 44 4F 43'), 0)])  # Deskmate
+
+
+def _is_xml(bin_data):
+    return _match_magic_number(bin_data, all=[_Signature(b'<', 0)])
+
+
+def _is_xls(bin_data):
+    if not _is_cfb(bin_data):
+        return False
+
+    any_signatures_1 = [
+        _Signature(bytes.fromhex('09 08 10 00 00 06 05 00'), 512),
+    ]
+    all_signatures_2 = [
+        _Signature(bytes.fromhex('FD FF FF FF'), 512),
+    ]
+    any_signatures_2 = [
+        _Signature(bytes.fromhex('00'), 518),
+        _Signature(bytes.fromhex('02'), 518),
+    ]
+
+    return any([
+        _match_magic_number(bin_data, any=any_signatures_1),
+        _match_magic_number(bin_data, all=all_signatures_2, any=any_signatures_2),
+        b'\xE2\x00\x00\x00\x5C\x00\x70\x00\x04\x00\x00Calc' in bin_data[1568:2095],
+    ])
+
+
+def _is_svg(bin_data):
+    return b'<svg' in bin_data and b'/svg' in bin_data
+
+
+def _is_txt(bin_data):
+    return all(c >= ' ' or c in '\t\n\r' for c in bin_data[:1024].decode())
+
+_Mimetype = collections.namedtuple('_Mimetype', ['mimetype', 'extension', 'match'])  # noqa: PYI024
+SUPPORTED_MIMETYPES = {
+    'empty': _Mimetype('application/x-empty', None, _is_empty),
+    'pdf': _Mimetype('application/pdf', 'pdf', _is_pdf),
+    'jpg': _Mimetype('image/jpeg', 'jpg', _is_jpg),
+    'png': _Mimetype('image/png', 'png', _is_png),
+    'gif': _Mimetype('image/gif', 'gif', _is_gif),
+    'bmp': _Mimetype('image/bmp', 'bmp', _is_bmp),
+    'svg': _Mimetype('image/svg+xml', 'svg', _is_svg),
+    'xml': _Mimetype('text/xml', 'xml', _is_xml),
+    'ico': _Mimetype('image/vnd.microsoft.icon', 'ico', _is_ico),
+    'webp': _Mimetype('image/webp', 'webp', _is_webp),
+    'doc': _Mimetype('application/msword', 'doc', _is_doc),
+    'xls': _Mimetype('application/vnd.ms-excel', 'xls', _is_xls),
+    'ppt': _Mimetype('application/vnd.ms-powerpoint', 'ppt', _is_ppt),
+    'docx': _Mimetype('application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'docx', _is_docx),
+    'xlsx': _Mimetype('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'xlsx', _is_xlsx),
+    'pptx': _Mimetype('application/vnd.openxmlformats-officedocument.presentationml.presentation', 'pptx', _is_pptx),
+    'odt': _Mimetype('application/vnd.oasis.opendocument.text', 'odt', _is_odt),
+    'ods': _Mimetype('application/vnd.oasis.opendocument.spreadsheet', 'ods', _is_ods),
+    'zip': _Mimetype('application/zip', 'zip', _is_zip),
+    'txt': _Mimetype('text/plain', 'txt', _is_txt),
+}
+
+# SUPPORTED_ZIP_MIMETYPES = {
+#     'docx': _Mimetype('application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'docx', _is_docx),
+#     'xlsx': _Mimetype('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'xlsx', _is_xlsx),
+#     'pptx': _Mimetype('application/vnd.openxmlformats-officedocument.presentationml.presentation', 'pptx', _is_pptx),
+#     'odt': _Mimetype('application/vnd.oasis.opendocument.text', 'odt', _is_ods),
+#     'ods': _Mimetype('application/vnd.oasis.opendocument.spreadsheet', 'ods', _is_ods),
+# }
+
+
+def _odoo_guess_file_mimetype():
+    pass
+
+
+def _odoo_guess_binary_mimetype():
+    pass
+
+
+def _odoo_guess_mimetype(bin_data, default='application/octet-stream'):
+    for supported_mimetype in SUPPORTED_MIMETYPES.values():
+        if supported_mimetype.match(bin_data):
+            return supported_mimetype.mimetype
+
+    return default
+
+
+
 def _check_ooxml(data):
     with io.BytesIO(data) as f, zipfile.ZipFile(f) as z:
         filenames = z.namelist()
@@ -42,56 +277,15 @@ def _check_ooxml(data):
 
         return False
 
+_ppt_pattern = re.compile(rb'''
+    \x00\x6E\x1E\xF0  # PowerPoint presentation subheader_1
+  | \x0F\x00\xE8\x03  # PowerPoint presentation subheader_2
+  | \xA0\x46\x1D\xF0  # PowerPoint presentation subheader_3
+  | \xFD\xFF\xFF\xFF\x0E\x00\x00\x00  # PowerPoint presentation subheader_4
+  | \xFD\xFF\xFF\xFF\x1C\x00\x00\x00  # PowerPoint presentation subheader_5
+  | \xFD\xFF\xFF\xFF\x43\x00\x00\x00  # PowerPoint presentation subheader_6
+''', re.VERBOSE)
 
-# checks that a string looks kinda sorta like a mimetype
-_mime_validator = re.compile(r"""
-    [\w-]+ # type-name
-    / # subtype separator
-    [\w-]+ # registration facet or subtype
-    (?:\.[\w-]+)* # optional faceted name
-    (?:\+[\w-]+)? # optional structured syntax specifier
-""", re.VERBOSE)
-def _check_open_container_format(data):
-    # Open Document Format for Office Applications (OpenDocument) Version 1.2
-    #
-    # Part 3: Packages
-    # 3 Packages
-    # 3.3 MIME Media Type
-    with io.BytesIO(data) as f, zipfile.ZipFile(f) as z:
-        # If a MIME media type for a document exists, then an OpenDocument
-        # package should contain a file with name "mimetype".
-        if 'mimetype' not in z.namelist():
-            return False
-
-        # The content of this file shall be the ASCII encoded MIME media type
-        # associated with the document.
-        marcel = z.read('mimetype').decode('ascii')
-        # check that it's not too long (RFC6838 § 4.2 restricts type and
-        # subtype to 127 characters each + separator, strongly recommends
-        # limiting them to 64 but does not require it) and that it looks a lot
-        # like a valid mime type
-        if len(marcel) < 256 and _mime_validator.match(marcel):
-            return marcel
-
-        return False
-
-
-_old_ms_office_mimetypes = {
-    '.doc': 'application/msword',
-    '.xls': 'application/vnd.ms-excel',
-    '.ppt': 'application/vnd.ms-powerpoint',
-}
-_olecf_mimetypes = ('application/x-ole-storage', 'application/CDFV2')
-_xls_pattern = re.compile(b"""
-    \x09\x08\x10\x00\x00\x06\x05\x00
-  | \xFD\xFF\xFF\xFF(\x10|\x1F|\x20|"|\\#|\\(|\\))
-""", re.VERBOSE)
-_ppt_pattern = re.compile(b"""
-    \x00\x6E\x1E\xF0
-  | \x0F\x00\xE8\x03
-  | \xA0\x46\x1D\xF0
-  | \xFD\xFF\xFF\xFF(\x0E|\x1C|\x43)\x00\x00\x00
-""", re.VERBOSE)
 def _check_olecf(data):
     """ Pre-OOXML Office formats are OLE Compound Files which all use the same
     file signature ("magic bytes") and should have a subheader at offset 512
@@ -106,84 +300,11 @@ def _check_olecf(data):
         return 'application/msword'
     # the _xls_pattern stuff doesn't seem to work correctly (the test file
     # only has a bunch of \xf* at offset 0x200), that apparently works
-    elif b'Microsoft Excel' in data:
+    if b'Microsoft Excel' in data:
         return 'application/vnd.ms-excel'
-    elif _ppt_pattern.match(data, offset):
+    if _ppt_pattern.search(data):
         return 'application/vnd.ms-powerpoint'
     return False
-
-
-def _check_svg(data):
-    """This simply checks the existence of the opening and ending SVG tags"""
-    if b'<svg' in data and b'/svg' in data:
-        return 'image/svg+xml'
-
-def _check_webp(data):
-    """This checks the presence of the WEBP and VP8 in the RIFF"""
-    if data[8:15] == b'WEBPVP8':
-        return 'image/webp'
-
-# for "master" formats with many subformats, discriminants is a list of
-# functions, tried in order and the first non-falsy value returned is the
-# selected mime type. If all functions return falsy values, the master
-# mimetype is returned.
-_Entry = collections.namedtuple('_Entry', ['mimetype', 'signatures', 'discriminants'])
-_mime_mappings = (
-    # pdf
-    _Entry('application/pdf', [b'%PDF'], []),
-    # jpg, jpeg, png, gif, bmp, jfif
-    _Entry('image/jpeg', [b'\xFF\xD8\xFF\xE0', b'\xFF\xD8\xFF\xE2', b'\xFF\xD8\xFF\xE3', b'\xFF\xD8\xFF\xE1', b'\xFF\xD8\xFF\xDB'], []),
-    _Entry('image/png', [b'\x89PNG\r\n\x1A\n'], []),
-    _Entry('image/gif', [b'GIF87a', b'GIF89a'], []),
-    _Entry('image/bmp', [b'BM'], []),
-    _Entry('text/xml', [b'<'], [
-        _check_svg,
-    ]),
-    _Entry('image/x-icon', [b'\x00\x00\x01\x00'], []),
-    _Entry('image/webp', [b'RIFF'], [
-        _check_webp,
-    ]),
-    # OLECF files in general (Word, Excel, PPT, default to word because why not?)
-    _Entry('application/msword', [b'\xD0\xCF\x11\xE0\xA1\xB1\x1A\xE1', b'\x0D\x44\x4F\x43'], [
-        _check_olecf
-    ]),
-    # zip, but will include jar, odt, ods, odp, docx, xlsx, pptx, apk
-    _Entry('application/zip', [b'PK\x03\x04'], [_check_ooxml, _check_open_container_format]),
-)
-
-
-def _odoo_guess_mimetype(bin_data: bytes, default='application/octet-stream'):
-    """ Attempts to guess the mime type of the provided binary data, similar
-    to but significantly more limited than libmagic
-
-    :param bin_data: binary data to try and guess a mime type for
-    :returns: matched mimetype or ``application/octet-stream`` if none matched
-    """
-    # by default, guess the type using the magic number of file hex signature (like magic, but more limited)
-    # see http://www.filesignatures.net/ for file signatures
-    for entry in _mime_mappings:
-        for signature in entry.signatures:
-            if bin_data.startswith(signature):
-                for discriminant in entry.discriminants:
-                    try:
-                        guess = discriminant(bin_data)
-                        if guess: return guess
-                    except Exception:
-                        # log-and-next
-                        _logger_guess_mimetype.warning(
-                            "Sub-checker '%s' of type '%s' failed",
-                            discriminant.__name__, entry.mimetype,
-                            exc_info=True
-                        )
-                # if no discriminant or no discriminant matches, return
-                # primary mime type
-                return entry.mimetype
-    try:
-        if bin_data and all(c >= ' ' or c in '\t\n\r' for c in bin_data[:1024].decode()):
-            return 'text/plain'
-    except ValueError:
-        pass
-    return default
 
 
 try:
@@ -228,15 +349,9 @@ try:
 except ImportError:
     guess_mimetype = _odoo_guess_mimetype
 
-
-def neuter_mimetype(mimetype, user):
-    wrong_type = 'ht' in mimetype or 'xml' in mimetype or 'svg' in mimetype
-    if wrong_type and not user._is_system():
-        return 'text/plain'
-    return mimetype
-
-
 _extension_pattern = re.compile(r'\w+')
+
+
 def get_extension(filename):
     # A file has no extension if it has no dot (ignoring the leading one
     # of hidden files) or that what follow the last dot is not a single
@@ -262,6 +377,11 @@ def get_extension(filename):
     return ''
 
 
+_old_ms_office_mimetypes = {'.doc', '.xls', '.ppt'}
+_new_ms_office_mimetypes = {'.docx', '.xlsx', '.pptx'}
+_olecf_mimetypes = {'application/x-ole-storage', 'application/CDFV2'}
+
+
 def fix_filename_extension(filename, mimetype):
     """
     Make sure the filename ends with an extension of the mimetype.
@@ -280,7 +400,7 @@ def fix_filename_extension(filename, mimetype):
     if mimetype in _olecf_mimetypes and extension in _old_ms_office_mimetypes:
         return filename
 
-    if mimetype == 'application/zip' and extension in {'.docx', '.xlsx', '.pptx'}:
+    if mimetype == 'application/zip' and extension in _new_ms_office_mimetypes:
         return filename
 
     if extension := mimetypes.guess_extension(mimetype):
