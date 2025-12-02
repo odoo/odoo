77aa874accdc9fc96afeeb9c0fde0658090ea35b
Anh Thao Pham (pta) <pta@odoo.com>
2025-12-02 12:33:44 +0000

    [IMP] uom, account{_edi_ubl_cii,}: add 2 UoM from UNECE Recommendation No.20
    Issue:
    2 UoM that is in the UNECE Recommendation No.20 for Peppol don't
    exist in Odoo:
    - MIN: Minute
    - KWH: Kilowatt hour
    
    Even if they are created manually, they are not used in the UBL/CII
    electronic invoices.
    Instead, the default code (i.e. "C62" for "Units" is used).
    
    Some localization modules create the "Kilowatt hour" UoM as they need it.
    (l10n_cl and l10n_tr_nilvera)
    So it's better to have a "generic" one available for every module.
    
    opw-5269119
    
    closes odoo/odoo#243333
    
    X-original-commit: d9b2e5ee730a4e79586bfd3f09adfb794d2dc1f3
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>
    Signed-off-by: Anh Thao Pham (pta) <pta@odoo.com>

--- addons/account/models/uom_uom.py
+++ addons/account/models/uom_uom.py
@@ -10,6 +10,7 @@ UOM_TO_UNECE_CODE = {
     'uom.product_uom_gram': 'GRM',
     'uom.product_uom_day': 'DAY',
     'uom.product_uom_hour': 'HUR',
+    'uom.product_uom_minute': 'MIN',
     'uom.product_uom_ton': 'TNE',
     'uom.product_uom_meter': 'MTR',
     'uom.product_uom_km': 'KMT',
@@ -30,6 +31,7 @@ UOM_TO_UNECE_CODE = {
     'uom.uom_square_foot': 'FTK',
     'uom.product_uom_yard': 'YRD',
     'uom.product_uom_millimeter': 'MMT',
+    'uom.product_uom_kwh': 'KWH',
 }
 
 
--- addons/account_edi_ubl_cii/models/account_edi_common.py
+++ addons/account_edi_ubl_cii/models/account_edi_common.py
@@ -19,6 +19,7 @@ UOM_TO_UNECE_CODE = {
     'uom.product_uom_gram': 'GRM',
     'uom.product_uom_day': 'DAY',
     'uom.product_uom_hour': 'HUR',
+    'uom.product_uom_minute': 'MIN',
     'uom.product_uom_ton': 'TNE',
     'uom.product_uom_meter': 'MTR',
     'uom.product_uom_km': 'KMT',
@@ -39,6 +40,7 @@ UOM_TO_UNECE_CODE = {
     'uom.uom_square_foot': 'FTK',
     'uom.product_uom_yard': 'YRD',
     'uom.product_uom_millimeter': 'MMT',
+    'uom.product_uom_kwh': 'KWH',
 }
 
 # -------------------------------------------------------------------------
