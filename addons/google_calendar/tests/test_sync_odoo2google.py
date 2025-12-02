471ae7d2fae0bc24f8a53160c5ef32e43e658e02
Rémy Voet (ryv) <ryv@odoo.com>
2025-12-02 10:27:41 +0100

    [FIX] *: update query count

--- addons/google_calendar/tests/test_sync_odoo2google.py
+++ addons/google_calendar/tests/test_sync_odoo2google.py
@@ -94,7 +94,7 @@ class TestSyncOdoo2Google(TestSyncOdoo2GoogleCommon):
         })
         partner_model = self.env.ref('base.model_res_partner')
         partner = self.env['res.partner'].search([], limit=1)
-        with self.assertQueryCount(__system__=526):
+        with self.assertQueryCount(__system__=26):
             events = self.env['calendar.event'].create([{
                 'name': "Event %s" % (i),
                 'start': datetime(2020, 1, 15, 8, 0),
@@ -109,7 +109,7 @@ class TestSyncOdoo2Google(TestSyncOdoo2GoogleCommon):
 
             events._sync_odoo2google(self.google_service)
 
-        with self.assertQueryCount(__system__=27):
+        with self.assertQueryCount(__system__=32):
             events.unlink()
 
     @patch_api
@@ -123,7 +123,7 @@ class TestSyncOdoo2Google(TestSyncOdoo2GoogleCommon):
             'duration': 18,
         })
         partner_model = self.env.ref('base.model_res_partner')
-        with self.assertQueryCount(__system__=105):
+        with self.assertQueryCount(__system__=88):
             event = self.env['calendar.event'].create({
                 'name': "Event",
                 'start': datetime(2020, 1, 15, 8, 0),
@@ -140,7 +140,7 @@ class TestSyncOdoo2Google(TestSyncOdoo2GoogleCommon):
                 'res_id': self.partner_jean_luc.id,
             })
 
-        with self.assertQueryCount(__system__=35):
+        with self.assertQueryCount(__system__=34):
             event.unlink()
 
     def test_event_without_user(self):
