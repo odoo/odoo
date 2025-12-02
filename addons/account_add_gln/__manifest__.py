2ce121b217e5fb3225dff88a9cc1393ac0f12257
Allesio <peal@odoo.com>
2025-12-02 15:11:08 +0000

    [ADD] account_add_gln,account_edi_ubl_cii: GLN number
    This commit adds the Global Location Number (GLN) to delivery partners,
    which is required to be reported during eInvoicing.
    
    Task: 5237220
    X-original-commit: 85117efccd6b2f3b7e9168648b56aff266138b2c

---
+++ addons/account_add_gln/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Add Partner GLN",
+    'summary': "This module adds the Global Location Number to the partner. Used on delivery addresses, it is used to identify stock locations and is mandatory on the UBL/CII eInvoices (but not only). The module is intended be merged with account, later on, in master",
+    'category': 'Accounting/Accounting',
+    'depends': ['account'],
+    'auto_install': True,
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
