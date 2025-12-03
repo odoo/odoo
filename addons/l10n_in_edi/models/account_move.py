f4d53a931496872ca51e862e60e6d54588a26c67
Jigar Vaghela <jva@odoo.com>
2025-12-03 14:32:50 +0000

    [IMP] l10n_in_edi: Add Tax value in Total for Export with payment of tax
    Here we are not collecting tax from the customer but the company has pay taxes to the goverment
    and after that the company can claim refund for the taxes.
    So when we are sending data to goverment then we need to include tax part into the
    invoice total
    
    task-5369117
    
    closes odoo/odoo#238942
    
    X-original-commit: a1881f6bfa3f430ae33dc7525dcd431e7aa44d84
    Related: odoo/enterprise#101525
    Signed-off-by: Habib Ayob (ayh) <ayh@odoo.com>
    Signed-off-by: Harsh Modi (hamo) <hamo@odoo.com>
    Co-authored-by: Harsh Modi <hamo@odoo.com>

--- addons/l10n_in_edi/models/account_move.py
+++ addons/l10n_in_edi/models/account_move.py
@@ -678,6 +678,29 @@ class AccountMove(models.Model):
                 json_payload['ExpDtls']['ShipBDt'] = shipping_bill_date.strftime("%d/%m/%Y")
             if shipping_port_code_id := self.l10n_in_shipping_port_code_id:
                 json_payload['ExpDtls']['Port'] = shipping_port_code_id.code
+            json_valdtls = json_payload['ValDtls']
+            base_and_tax_amount = tax_details.get("base_amount") + tax_details.get("tax_amount")
+            # For Export If with payment of Tax then we need to include Tax in Total Invoice Value
+            if json_payload['TranDtls']['SupTyp'] == 'EXPWP' and json_valdtls['AssVal'] == base_and_tax_amount:
+                json_payload["ValDtls"]["TotInvVal"] = self._l10n_in_round_value(sum([
+                    json_valdtls['TotInvVal'],
+                    json_valdtls['IgstVal'],
+                    json_valdtls['CgstVal'],
+                    json_valdtls['SgstVal'],
+                    json_valdtls['CesVal'],
+                    json_valdtls['StCesVal'],
+                ]))
+                for line in json_payload["ItemList"]:
+                    line["TotItemVal"] = self._l10n_in_round_value(sum([
+                        line["TotItemVal"],
+                        line["IgstAmt"],
+                        line["CgstAmt"],
+                        line["SgstAmt"],
+                        line["CesAmt"],
+                        line["CesNonAdvlAmt"],
+                        line["StateCesAmt"],
+                        line["StateCesNonAdvlAmt"],
+                    ]))
         return self._l10n_in_edi_generate_invoice_json_managing_negative_lines(json_payload)
 
     def _l10n_in_get_supply_type(self, is_igst_amount):
