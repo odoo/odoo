2e045ea0bfacd9ff38df18bfe413f39395b9dc51
Quentin Wolfs <quwo@odoo.com>
2025-12-04 12:46:18 +0000

    [FIX] stock_account: consider already processed qty in fifo
    Steps to reproduce:
    - Have a product valued in fifo
    - Create 3 PO for it, each for 1 qty of price 10, 20 and 30.
    - Confirm these PO & validate their receipts
    - Create 2 SO for this product, each for 1 qty
    - Confirm these SO & validate their deliveries together
    
    Issue:
    The value associated to the delivery moves (and so the cogs generated
    from them) is 10 for both.
    
    When calling `_action_done()` on the moves, we'll set the value of each
    move before moving them.
    To get the correct value from the fifo stack, we rely on the
    `qty_available` at the time. However, since we're going to set the value
    of multiple moves before validating them, the `qty_available` won't be
    updated between each call.
    
    opw-5359484
    
    X-original-commit: 048d9fe40cb88482c0105a896fa0b8f971795399

--- addons/stock_account/models/stock_move.py
+++ addons/stock_account/models/stock_move.py
@@ -256,6 +256,7 @@ class StockMove(models.Model):
         """
         products_to_recompute = set()
         lots_to_recompute = set()
+        fifo_qty_processed = defaultdict(float)
 
         for move in self:
             # Incoming moves
@@ -285,7 +286,9 @@ class StockMove(models.Model):
                 continue
 
             if move.product_id.cost_method == 'fifo':
-                move.value = move.product_id._run_fifo(move._get_valued_qty())
+                valued_qty = move._get_valued_qty()
+                move.value = move.product_id.with_context(fifo_qty_already_processed=fifo_qty_processed[move.product_id])._run_fifo(valued_qty)
+                fifo_qty_processed[move.product_id] += valued_qty
             else:
                 move.value = move.product_id.standard_price * move._get_valued_qty()
 
