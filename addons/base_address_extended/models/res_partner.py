c51fc48e909ca0f236b5dd45784ca80744e157eb
prep-odoo <prep@odoo.com>
2025-12-04 12:09:13 +0000

    [FIX] _*: add widget on street fields for address autocomplete
    _= base,base_address_extended,google_address_autocomplete,website_sale_autocomplete
    
    The Google autocomplete feature was breaking when modules
    such as base_extended_address modified the address form
    (e.g., replacing street with street_name, street_number, etc.).
    
    In this commit:
    - Add the Google autocomplete widget to street-related fields.
    - Also same thing handled in frontened (e-commerce address autocomplete).
    - and update 'city_id' from available cities records if matches with google result.
    
    - update access_rights for RecCity Model - grant read access to public user
    
    task-5382984
    opw-5362597
    
    closes odoo/odoo#255960
    
    X-original-commit: b2b9c355b7aa4f9bfbc9acbb6b2a400dced5362e
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>
    Signed-off-by: Deep I Joshi (dijo) <dijo@odoo.com>

--- addons/base_address_extended/models/res_partner.py
+++ addons/base_address_extended/models/res_partner.py
@@ -60,3 +60,17 @@ class ResPartner(models.Model):
         super()._onchange_country_id()
         if self.country_id and self.country_id != self.city_id.country_id:
             self.city_id = False
+
+    @api.model
+    def _get_res_city_by_name(self, name, country_id):
+        ResCity = self.env['res.city']
+        if not name or not country_id:
+            return ResCity
+
+        if self.env.user._is_public():
+            ResCity = ResCity.sudo()
+
+        return ResCity.search([
+            ('name', '=ilike', name),
+            ('country_id', '=', country_id),
+        ], limit=1)
