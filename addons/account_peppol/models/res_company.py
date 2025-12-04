37304985e54a21d0f9f452bdaa06237592529163
Claire (clbr) <clbr@odoo.com>
2025-12-04 12:10:12 +0000

    [FIX] account_peppol: remove deprecated services from default services
    When creating a new connection to the Peppol network, we add multiple services
    by default. This commit remove from the default (they can still be manually enabled):
    - the ANZ BIS3 Invoice & CreditNote that is deprecated in favor of the PINT version,
    - the SG BIS3 Invoice & CreditNote that will also be deprecated soon by its PINT version.
    
    Note that anyway for the moment we don't allow to register user from AU/NZ/SG on Peppol, so
    we were in any case registering those services for all participants, and none of them were
    relevant for those two local formats ...
    
    https://github.com/odoo/odoo/blob/0af9d32e305c1f1afb51e126c1e6747879e78225/addons/account/models/company.py#L35-L50
    
    I checked on our AP, and only 8-10 invoices were sent with these formats, between
    Belgians... so it is most likely errors. Let's reduce the confusion.
    
    Source: https://docs.peppol.eu/edelivery/codelists/v9.4/Peppol%20Code%20Lists%20-%20Document%20types%20v9.4.json
    X-original-commit: 1bd57f678cd1b00ce739539d7ecc23fa44695c59

--- addons/account_peppol/models/res_company.py
+++ addons/account_peppol/models/res_company.py
@@ -259,18 +259,10 @@ class ResCompany(models.Model):
                     "SI-UBL 2.0 Invoice",
                 "urn:oasis:names:specification:ubl:schema:xsd:CreditNote-2::CreditNote##urn:cen.eu:en16931:2017#compliant#urn:fdc:nen.nl:nlcius:v1.0::2.1":
                     "SI-UBL 2.0 CreditNote",
-                "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2::Invoice##urn:cen.eu:en16931:2017#conformant#urn:fdc:peppol.eu:2017:poacc:billing:international:sg:3.0::2.1":
-                    "SG Peppol BIS Billing 3.0 Invoice",
-                "urn:oasis:names:specification:ubl:schema:xsd:CreditNote-2::CreditNote##urn:cen.eu:en16931:2017#conformant#urn:fdc:peppol.eu:2017:poacc:billing:international:sg:3.0::2.1":
-                    "SG Peppol BIS Billing 3.0 Credit Note",
                 "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2::Invoice##urn:cen.eu:en16931:2017#compliant#urn:xeinkauf.de:kosit:xrechnung_3.0::2.1":
                     "XRechnung UBL Invoice V2.0",
                 "urn:oasis:names:specification:ubl:schema:xsd:CreditNote-2::CreditNote##urn:cen.eu:en16931:2017#compliant#urn:xeinkauf.de:kosit:xrechnung_3.0::2.1":
                     "XRechnung UBL CreditNote V2.0",
-                "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2::Invoice##urn:cen.eu:en16931:2017#conformant#urn:fdc:peppol.eu:2017:poacc:billing:international:aunz:3.0::2.1":
-                    "AU-NZ Peppol BIS Billing 3.0 Invoice",
-                "urn:oasis:names:specification:ubl:schema:xsd:CreditNote-2::CreditNote##urn:cen.eu:en16931:2017#conformant#urn:fdc:peppol.eu:2017:poacc:billing:international:aunz:3.0::2.1":
-                    "AU-NZ Peppol BIS Billing 3.0 CreditNote",
             }
         }
 
