45ab464e39411e4047e58191fbefb7b6f13735a8
Pedram (PEBR) <pebr@odoo.com>
2025-12-05 10:30:13 +0000

    [PERF] point_of_sale: Optimize price computations for paid orders
    Before this commit, the POS system would recompute product prices even
    for orders that were already paid. This led to unnecessary calculations.
    
    opw-5263663
    
    X-original-commit: 8225f01f62783a0415f01baddb21fe994a5f8e48

--- addons/point_of_sale/static/src/app/models/accounting/pos_order_line_accounting.js
+++ addons/point_of_sale/static/src/app/models/accounting/pos_order_line_accounting.js
@@ -174,7 +174,7 @@ export class PosOrderlineAccounting extends Base {
         return accountTaxHelpers.prepare_base_line_for_taxes_computation(
             this,
             this.prepareBaseLineForTaxesComputationExtraValues({
-                price_unit: this.productProductPrice,
+                price_unit: this.price_unit,
                 quantity: this.getQuantity(),
                 tax_ids: this.tax_ids,
                 ...opts,
