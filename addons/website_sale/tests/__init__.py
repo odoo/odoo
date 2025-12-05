0eaf02feb76726e77dff022df6eea4218f3a17bb
krip-odoo <krip@odoo.com>
2025-12-05 12:52:30 +0530

    [FIX] website_sale: ensure correct confirmation email template is used
    Steps to produce:
    - Install the `website_sale` module.
    - Create an order from ecommerce and make a payment.
    - Check the sent mail > it uses the default Sale Order confirmation template.
    - Go to Settings > Website > eCommerce > change the
      `Email in order confirmation` template.
    - Again, create a Sales Order and make a payment from the website.
    
    Issue:
    - The confirmation email still uses the default template, ignoring the
      updated template set in Website settings.
    
    Root cause:
    - During payment, `action_confirm` is called in `website_sale` [1].
    - The super call delegates to `sale.order` [2], which fetches the default
      confirmation template [3].
    
    Solution:
    - Override `_get_confirmation_template` in `website_sale` to return the template
      defined in Website settings.
    - This ensures that the updated template is respected when sending confirmation
      emails after payment.
    
    [1]: https://github.com/odoo/odoo/blob/1fcba6fe896acfef7c409eac7af002740e05a63c/addons/website_sale/models/sale_order.py#L240
    [2]: https://github.com/odoo/odoo/blob/2ec06924ac50e82460b7a2856f74a115d3b874f9/addons/sale/models/sale_order.py#L1149
    [3]: https://github.com/odoo/odoo/blob/2ec06924ac50e82460b7a2856f74a115d3b874f9/addons/sale/models/sale_order.py#L1123-L1137
    
    opw-5352437

--- addons/website_sale/tests/__init__.py
+++ addons/website_sale/tests/__init__.py
@@ -26,6 +26,7 @@ from . import test_website_sale_gmc
 from . import test_website_sale_image
 from . import test_website_sale_invoice
 from . import test_website_sale_mail
+from . import test_website_sale_mail_template
 from . import test_website_sale_pricelist
 from . import test_website_sale_product_attribute_value_config
 from . import test_website_sale_product_configurator
