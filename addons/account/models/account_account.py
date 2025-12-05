a256372020ae437b3feac474157dcf3ee14bdf9d
Corentin Lombard <clom@odoo.com>
2025-12-05 10:02:19 +0000

    [FIX] account: allow group by account root with full optimization
    Issue:
    Access records in a grouped by account_root list fails.
    
    Step to reproduce
    - Accounting -> Chart of account
    - Group By "Root"
    - open any grouped record set
    
    Current behavior:
    Show an error message "Unsupported operator on Root 'Account' (account.account) in [('root_id', 'any', [('display_name', 'in', [10])])]"
    
    Expected behavior:
    Show the nested records
    
    Solution:
    Opening the record set fetch data using the domain : [('id', '=', '10')]
    This domain goes through optimization as follows:
    		[('id', '=', '10')]
    basic : [('id', 'in', ['10'])]
    full :	[('root_id', 'any', [('display_name', 'in', [10])])]
    
    However, the _search_account_root method protect the technical field account_root. Therefore, this commit adapts it to allow full optimized domain search.
    
    opw-5178896
    
    closes odoo/odoo#244606
    
    X-original-commit: 0997af0dd19768859b8e122d12770df5300a683b
    Signed-off-by: Paolo Gatti (pgi) <pgi@odoo.com>

--- addons/account/models/account_account.py
+++ addons/account/models/account_account.py
@@ -402,11 +402,17 @@ class AccountAccount(models.Model):
         )
 
     def _search_account_root(self, operator, value):
-        if operator not in ('in', 'child_of'):
+        if operator not in ('in', 'child_of', 'any'):
             return NotImplemented
-        roots = self.env['account.root'].browse(value)
+        if operator == 'any':
+            if isinstance(value, Domain) and value.field_expr == 'display_name' and value.operator == 'in':
+                roots = self.env['account.root'].browse(value.value)
+            else:
+                return NotImplemented
+        else:
+            roots = self.env['account.root'].browse(value)
         return Domain.OR(
-            Domain('placeholder_code', '=ilike', root.name + ('' if operator == 'in' and not root.parent_id else '%'))
+            Domain('placeholder_code', '=ilike', root.name + ('' if operator in ['in', 'any'] and not root.parent_id else '%'))
             for root in roots
         )
 
