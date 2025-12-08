1a58b955dd51161a49090ee2c35bd19f62903dbc
Hesham Saleh <hsal@odoo.com>
2025-12-08 17:14:53 +0100

    [FIX] account: Fix stock_account post init hook
    Repro steps:
    1. Install a localization module with defined account_stock_expense_id or account_stock_variation_id
    2. Add a branch to that localization company
    3. Attempt to install stock_account
    
    Problem:
    A traceback shows preventing the user from installing stock_account
    ERROR: null value in column "account_type" of relation "account_account" violates not-null constraint
    
    Root cause:
    In stock_account post init hook, stock related fields data are attempted to be added from localizations.
    The problem is that branches don't have accounts by default, they own their parent company accounts.
    
    Solution:
    This commit fixes this issue by using AccountChartTemplate.ref in _load_data.
    This ref would find the record (i.e., account.account record) instead of attempting to create a new one
    for child companies.
    
    opw-5391764
    
    closes odoo/odoo#239104
    
    Related: odoo/enterprise#103640
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/account/models/chart_template.py
+++ addons/account/models/chart_template.py
@@ -299,7 +299,7 @@ class AccountChartTemplate(models.AbstractModel):
                 if journal:
                     del data['account.journal'][xmlid]
                     self.env['ir.model.data']._update_xmlids([{
-                        'xml_id': f"account.{company.id}_{xmlid}",
+                        'xml_id': self.company_xmlid(xmlid, company),
                         'record': journal,
                         'noupdate': True,
                     }])
@@ -437,7 +437,7 @@ class AccountChartTemplate(models.AbstractModel):
                         existing_account = accounts.sorted(key=lambda x: x.code != normalized_code)[0] if accounts else None
                         if existing_account:
                             self.env['ir.model.data']._update_xmlids([{
-                                'xml_id': f"account.{company.id}_{xmlid}",
+                                'xml_id': self.company_xmlid(xmlid, company),
                                 'record': existing_account,
                                 'noupdate': True,
                             }])
@@ -680,11 +680,14 @@ class AccountChartTemplate(models.AbstractModel):
                         del record_vals[key]
 
                 # Manage ids given as database id or xml_id
+                if isinstance(xml_id, str) and (record := self.ref(xml_id, raise_if_not_found=False)):
+                    xml_id = record.id
+
                 if isinstance(xml_id, int):
                     record_vals['id'] = xml_id
                     xml_id = False
                 else:
-                    xml_id = f"{('account.' + str(self.env.company.id) + '_') if '.' not in xml_id else ''}{xml_id}"
+                    xml_id = self.company_xmlid(xml_id)
 
                 all_records_vals.append({
                     'xml_id': xml_id,
@@ -895,7 +898,7 @@ class AccountChartTemplate(models.AbstractModel):
         else:
             accounts = self.env['account.account']._load_records([
                 {
-                    'xml_id': f"account.{company.id}_{xml_id}",
+                    'xml_id': self.company_xmlid(xml_id, company),
                     'values': values,
                     'noupdate': True,
                 }
@@ -927,7 +930,7 @@ class AccountChartTemplate(models.AbstractModel):
         }
         self.env['account.account']._load_records([
             {
-                'xml_id': f"account.{company.id}_{xml_id}",
+                'xml_id': self.company_xmlid(xml_id, company),
                 'values': values,
                 'noupdate': True,
             }
@@ -1207,12 +1210,16 @@ class AccountChartTemplate(models.AbstractModel):
     # Tooling
     # --------------------------------------------------------------------------------
 
-    def ref(self, xmlid, raise_if_not_found=True):
+    def company_xmlid(self, xmlid, company=None):
         if '.' in xmlid:
-            return self.env.ref(xmlid, raise_if_not_found)
+            return xmlid
+        company = company or self.env.company
+        return f"account.{company.id}_{xmlid}"
+
+    def ref(self, xmlid, raise_if_not_found=True):
         return (
-            self.env.ref(f"account.{self.env.company.id}_{xmlid}", raise_if_not_found=False)
-            or self.env.ref(f"account.{self.env.company.parent_ids[0].id}_{xmlid}", raise_if_not_found)
+            self.env.ref(self.company_xmlid(xmlid), raise_if_not_found=False)
+            or self.env.ref(self.company_xmlid(xmlid, self.env.company.parent_ids[0]), raise_if_not_found)
         )
 
     def _get_parent_template(self, code):
@@ -1474,7 +1481,7 @@ class AccountChartTemplate(models.AbstractModel):
                                 continue
                             field_translation = self._get_field_translation(record, fname, lang)
                             if field_translation:
-                                xml_id = _xml_id if '.' in _xml_id else f"account.{company.id}_{_xml_id}"
+                                xml_id = _xml_id if '.' in _xml_id else self.company_xmlid(_xml_id, company)
                                 translation_importer.model_translations[mname][fname][xml_id][lang] = field_translation
 
         # Gather translations for the TEMPLATE_MODELS records that are not created from the chart_template data
