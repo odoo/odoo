dd9bdf1cf79f3da869684f733c9f05af969286d2
defl <defl@odoo.com>
2025-12-08 17:52:09 +0100

    [FIX] mail: fix reactive issue in firefox caused by datetime sort
    **Steps to reproduce:**
    - (Firefox only)
    - Go to any record which uses a chatter (e.g. Contact)
    - Send message > Full composer > click the schedule message icon (lower right corner)
    - Schedule the message in the future and click send
    - You should see now a post in the chatter indicating that the message will be sent
    - Now click Send message and repeat the above steps again to schedule a second message
    - Whole page will be freezed
    - Reloading doesn't help
    
    **Issue:**
    Infinite loop in reactive callback on firefox.
    The code gets stuck in
    ```js
    for (const callback of [...callbacks]) {
        clearReactivesForCallback(callback);
        callback();
    }
    ```
    because of
    ```js
    const sortProxy2 = reactive(recordProxy, function sortObserver() {
        self.requestSort(record, fieldName);
    });
    this.fieldsSortProxy2.set(fieldName, sortProxy2);
    ```
    which loops over `store._.ADD_QUEUE("sort", record, fieldName);`
    
    (Forcing the `requestSort` only change the infinite loop into a recursion error)
    
    The recomputation seems to be caused by a bad sorting here:
    ```
    this.scheduledMessages = Record.many("ScheduledMessage", {
        sort: (a, b) => {
            if (a.scheduled_date === b.scheduled_date) {
                return a.id - b.id;
            }
            return a.scheduled_date < b.scheduled_date ? -1 : 1;
        },
    ```
    In the case both datetimes are equal the first condition doesn't properly
    catches it:
    ```
    > a.scheduled_date - b.scheduled_date
    > 0
    > a.scheduled_date === b.scheduled_date
    > false
    > a.scheduled_date < b.scheduled_date
    > false
    > a.scheduled_date > b.scheduled_date
    > false
    ```
    Which make the ordering change on each sort iteration:
    ```
    >  Array [ "ScheduledMessage,14", "ScheduledMessage,13" ]
    > recordsFullProxy.sort(func);
    >  Array [ "ScheduledMessage,13", "ScheduledMessage,14" ]
    > recordsFullProxy.sort(func);
    >  Array [ "ScheduledMessage,14", "ScheduledMessage,13" ]
    ```
    Chromium based browsers probably use a different sorting algorithm than
    Firefox, which seems to prevent the issue.
    
    **Fix:**
    Use `compareDatetime` to ensure the ordering is constant for the same
    datetime values.
    
    opw-5367371
    
    closes odoo/odoo#239199
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/chatter/web/thread_model_patch.js
+++ addons/mail/static/src/chatter/web/thread_model_patch.js
@@ -1,5 +1,6 @@
 import { Record } from "@mail/core/common/record";
 import { Thread } from "@mail/core/common/thread_model";
+import { compareDatetime } from "@mail/utils/common/misc";
 import "@mail/chatter/web_portal/thread_model_patch";
 
 import { patch } from "@web/core/utils/patch";
@@ -8,12 +9,7 @@ patch(Thread.prototype, {
     setup() {
         super.setup();
         this.scheduledMessages = Record.many("ScheduledMessage", {
-            sort: (a, b) => {
-                if (a.scheduled_date === b.scheduled_date) {
-                    return a.id - b.id;
-                }
-                return a.scheduled_date < b.scheduled_date ? -1 : 1;
-            },
+            sort: (a, b) => compareDatetime(a.scheduled_date, b.scheduled_date) || a.id - b.id,
             inverse: "thread",
         });
     },
