912efd1bedd4063b0e253620cc5ed15f4701760a
Louis Travaux <lotr@odoo.com>
2025-12-08 14:48:27 +0000

    [IMP] point_of_sale: print stock reports on validation
    Validating a `pos.order` creates a stock move in inventory.
    However, when configuring reports to automatically print on
    validation, the print job wasn't triggered from the pos.
    
    We added a way to retrieve report actions and execute them.
    
    Task: 5392414
    X-original-commit: 0e262c1909009a8c952cc8edf8820e92f7271caa

---
+++ addons/pos_stock/static/tests/unit/components/payment_screen.test.js
@@ -0,0 +1,44 @@
+import { test, expect } from "@odoo/hoot";
+import { mountWithCleanup, mockService } from "@web/../tests/web_test_helpers";
+import { setupPosEnv, getFilledOrder } from "@point_of_sale/../tests/unit/utils";
+import { definePosStockModels } from "../data/generate_model_definitions";
+import { PaymentScreen } from "@point_of_sale/app/screens/payment_screen/payment_screen";
+
+definePosStockModels();
+
+test("Print stock report on validation", async () => {
+    mockService("action", {
+        doAction: async (action) => {
+            expect(action.type).toBe("ir.actions.report");
+            expect(action.report_name).toBe("stock.report_return_document");
+        },
+    });
+    const store = await setupPosEnv();
+    const order = await getFilledOrder(store);
+    order.picking_type_id.has_stock_reports_to_print = true;
+
+    const comp = await mountWithCleanup(PaymentScreen, {
+        props: { orderUuid: order.uuid },
+    });
+    const firstPm = store.models["pos.payment.method"].getFirst();
+    await comp.addNewPaymentLine(firstPm);
+    await comp.onClickValidate();
+});
+
+test("Do not print stock report if not configured", async () => {
+    mockService("action", {
+        doAction: async () => {
+            throw new Error("Action service should not be called");
+        },
+    });
+    const store = await setupPosEnv();
+    const order = await getFilledOrder(store);
+
+    const comp = await mountWithCleanup(PaymentScreen, {
+        props: { orderUuid: order.uuid },
+    });
+    const firstPm = store.models["pos.payment.method"].getFirst();
+    await comp.addNewPaymentLine(firstPm);
+    await comp.onClickValidate();
+    expect(order.picking_type_id.has_stock_reports_to_print).toBeEmpty();
+});
