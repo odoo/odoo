61446232279a0b8c784a07884c5498b35d5eae4a
William Henrotin <whe@odoo.com>
2025-12-08 07:43:38 +0000

    [FIX] stock_account: exclude other location at closing
    The search on valuated location made at the closing doesn't filter on
    location company_id's. Making the closing running for every company
    while being launched by only one.
    
    X-original-commit: 1c4d1e1d17c874f1b01fa5de28bac32c57cc3a0f

--- addons/stock_account/models/res_company.py
+++ addons/stock_account/models/res_company.py
@@ -168,7 +168,11 @@ class ResCompany(models.Model):
         return extra_balance
 
     def _get_location_valuation_vals(self, at_date=None, location_domain=False):
-        location_domain = (location_domain or []) + [('valuation_account_id', '!=', False)]
+        location_domain = Domain.AND([
+            location_domain or [],
+            [('valuation_account_id', '!=', False)],
+            [('company_id', '=', self.id)],
+        ])
         amls_vals_list = []
         valued_location = self.env['stock.location'].search(location_domain)
         last_closing_date = self._get_last_closing_date()
