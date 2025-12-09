28cdbb123ac4b04c05398f5f6832d653e33aa362
Hadi El Yakhni <hael@odoo.com>
2025-12-09 14:44:29 +0100

    [FIX] point_of_sale: customer display order price display enhancement
    Steps to reproduce
    ------------------
    1. In PoS settings, set the "Tax Display" to "Tax-Excluded Price"
    2. Enable customer display
    3. Open a PoS session, and open the customer display simultaneously
    4. Add products, notice that the total in PoS is price included as
       expected. However, the total in cusromer display is tax excluded, so
       customer thinks he will be pay less.
    
    Why it happens
    --------------
    In the refactors done in 9538698f13d5763b4, we mistakenly set the
    order.amount (i.e. total amount) to follow the config "Tax Display".
    However, that price should always be tax incl.
    
    In this PR, we now use the `currencyDisplayPriceIncl` getter, instead of
    the config dependent `currencyDisplayPrice`.
    
    Additional enhancements
    -----------------------
    In case of "Tax-Excluded" price config, we now display the order
    subtotal (total price excluded) and the taxes amount as well, for more
    detailed breakdown of the prices. It follows the cart view in the main
    PoS products display.
    
    opw-5401560
    
    closes odoo/odoo#239250
    
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/point_of_sale/static/tests/unit/customer_display/customer_display_adapter.test.js
+++ addons/point_of_sale/static/tests/unit/customer_display/customer_display_adapter.test.js
@@ -1,5 +1,5 @@
 import { test, expect } from "@odoo/hoot";
-import { getFilledOrder, setupPosEnv } from "../utils";
+import { getFilledOrder, setupPosEnv, expectFormattedPrice } from "../utils";
 import { definePosModels } from "../data/generate_model_definitions";
 import { CustomerDisplayPosAdapter } from "@point_of_sale/app/customer_display/customer_display_adapter";
 
@@ -15,4 +15,22 @@ test("getOrderlineData", async () => {
     expect(adapter.data.lines).toHaveLength(2);
     expect(adapter.data.lines[0].isSelected).toBe(false);
     expect(adapter.data.lines[1].isSelected).toBe(true);
+});
+
+test("order amounts summary", async () => {
+    const store = await setupPosEnv();
+    const order = await getFilledOrder(store);
+
+    const adapter = new CustomerDisplayPosAdapter();
+
+    adapter.formatOrderData(order);
+    expectFormattedPrice(adapter.data.amount, "$ 17.85");
+    expectFormattedPrice(adapter.data.amountTaxes, "$ 2.85");
+    expect(adapter.data.subtotal).toBe(false);
+
+    store.config.iface_tax_included = "subtotal";
+    adapter.formatOrderData(order);
+    expectFormattedPrice(adapter.data.amount, "$ 17.85");
+    expectFormattedPrice(adapter.data.amountTaxes, "$ 2.85");
+    expectFormattedPrice(adapter.data.subtotal, "$ 15.00");
 });
