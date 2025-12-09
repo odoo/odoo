238f45d54fdd076060be60565abe6094e67239ac
akus-odoo <akus@odoo.com>
2025-12-09 18:35:30 +0530

    [FIX] discuss: hide combined mic+camera button when only one permission needed
    Currently, when requesting microphone permission in video calls, the Use
    microphone and camera button is shown even though only microphone access
    is needed or vice-versa. This creates confusion as users see an option to request
    permissions that aren't relevant to their current action.
    
    This commit changes the condition to only show the combined button when
    neither camera nor microphone permissions have been granted yet. Now, when
    users click the microphone button, they will only see microphone-related
    options unless they also need camera permissions.
    
    Task-5367786
    
    closes odoo/odoo#239241
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/tests/discuss/call/call_permissions.test.js
+++ addons/mail/static/tests/discuss/call/call_permissions.test.js
@@ -96,3 +96,23 @@ test("Turn on both microphone and camera from permission dialog", async () => {
     await contains(".o-discuss-CallActionList button[title='Stop camera']");
     await contains(".o-discuss-CallActionList button[title='Mute']");
 });
+
+test("Combined mic+camera button only shown when both permissions not granted", async () => {
+    const pyEnv = await startServer();
+    const channelId = pyEnv["discuss.channel"].create({ name: "General" });
+    mockGetMedia();
+    mockPermissionsPrompt();
+    const env = await start();
+    const rtc = env.services["discuss.rtc"];
+    await openDiscuss(channelId);
+    await click("[title='Start Call']");
+    await click(".o-discuss-CallActionList button[title='Turn camera on']");
+    await contains(".modal-footer button", { count: 2 });
+    await contains(".modal-footer button", { text: "Use microphone and camera" });
+    await contains(".modal-footer button", { text: "Use Camera" });
+    rtc.cameraPermission = "granted";
+    await click(".modal-footer button", { text: "Use Camera" });
+    await click(".o-discuss-CallActionList button[title='Unmute']");
+    await contains(".modal-footer button");
+    await contains(".modal-footer button", { text: "Use Microphone" });
+});
