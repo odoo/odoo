61e8fa420008d14c081089ba85f7242382da80a7
Quentin De Paoli <qdp@odoo.com>
2025-12-09 13:32:00 +0100

    [FIX] account: added setting to enable peppol
    This was supposed to be already the case, since the field is_account_peppol_eligible was present but, during the backport, we forgot to use it in the config view.

--- addons/account/models/res_config_settings.py
+++ addons/account/models/res_config_settings.py
@@ -120,6 +120,7 @@ class ResConfigSettings(models.TransientModel):
     module_account_taxcloud = fields.Boolean(string="Account TaxCloud")
     module_account_invoice_extract = fields.Boolean(string="Document Digitization")
     module_snailmail_account = fields.Boolean(string="Snailmail")
+    module_account_peppol = fields.Boolean(string='PEPPOL Invoicing')
     tax_exigibility = fields.Boolean(string='Cash Basis', related='company_id.tax_exigibility', readonly=False)
     tax_cash_basis_journal_id = fields.Many2one('account.journal', related='company_id.tax_cash_basis_journal_id', string="Tax Cash Basis Journal", readonly=False)
     account_cash_basis_base_account_id = fields.Many2one(
