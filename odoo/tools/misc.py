5c0a44e5feb44b30639a29da74dc77ba98b850b9
Xavier Morel <xmo@odoo.com>
2025-12-10 08:44:33 +0100

    [FIX] lower_logging: support mapping args
    When `lower_logging` encounters a `LogRecord.args: Mapping`, it fucks
    up and strips out all the values keeping only the mapping keys (as a
    tuple), which then breaks when trying to format it in `LogRecord.msg`.
    
    Fix the issue by moving the entire message munging into,
    appropriately, the formatter: `getMessage` will do the `str.__mod__`
    call at which point we don't need to deal with the args at all, then
    `formatMessage` generates the full message line (not including the
    stack traces from `exc_info` and `stack_info`, those are added in the
    second half of `Formatter.format`).
    
    https://runbot.odoo.com/odoo/error/234669

--- odoo/tools/misc.py
+++ odoo/tools/misc.py
@@ -854,8 +854,8 @@ class lower_logging(logging.Handler):
         if record.levelno > self.max_level:
             record.levelname = f'_{record.levelname}'
             record.levelno = self.to_level
+            record.munge_traceback = True
             self.had_error_log = True
-            record.args = tuple(arg.replace('Traceback (most recent call last):', '_Traceback_ (most recent call last):') if isinstance(arg, str) else arg for arg in record.args)
 
         if logging.getLogger(record.name).isEnabledFor(record.levelno):
             for handler in self.old_handlers:
