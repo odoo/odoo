odoo/odoo

commit 791d068a33bf5f85381944c7766a799627becfb6

[IMP] stock: remove name field on stock.move
Remove this field as it is not really relevant as we already have the
reference field.
We make sure to adapt the `compute_reference` to catch all cases where a
custom name used to be set on the move

task-4783553

Part-of: odoo/odoo#211488
Related: odoo/enterprise#86304
Related: odoo/upgrade#7736
Signed-off-by: Claeys Pieter (clpi) <clpi@odoo.com>

addons/repair/models/stock_move.py
@@ -61,6 +61,15 @@ class StockMove(models.Model):
                 ids_to_super.add(move.id)
         return super(StockMove, self.browse(ids_to_super))._compute_location_dest_id()
 
+    @api.depends('repair_id.name')
+    def _compute_reference(self):
+        moves_with_reference = set()
+        for move in self:
+            if move.repair_id and move.repair_id.name:
+                move.reference = move.repair_id.name
+                moves_with_reference.add(move)
+        super(StockMove, self - self.env['stock.move'].browse(moves_with_reference))._compute_reference()
+
     def copy_data(self, default=None):
         default = dict(default or {})
         vals_list = super().copy_data(default=default)
@@ -84,14 +93,13 @@ class StockMove(models.Model):
             if not vals.get('repair_id') or 'repair_line_type' not in vals:
                 continue
             repair_id = self.env['repair.order'].browse([vals['repair_id']])
-            vals['name'] = repair_id.name
+            vals['origin'] = repair_id.name
         moves = super().create(vals_list)
         repair_moves = self.env['stock.move']
         for move in moves:
             if not move.repair_id:
                 continue
             move.group_id = move.repair_id.procurement_group_id.id
-            move.origin = move.name
             move.picking_type_id = move.repair_id.picking_type_id.id
             repair_moves |= move
         no_repair_moves = moves - repair_moves

addons/mrp/models/stock_move.py
@@ -270,7 +270,7 @@ class StockMove(models.Model):
             else:
                 move.unit_factor = 1.0
 
-    @api.depends('raw_material_production_id', 'raw_material_production_id.name', 'production_id', 'production_id.name')
+    @api.depends('raw_material_production_id', 'raw_material_production_id.name', 'production_id', 'production_id.name', 'unbuild_id', 'unbuild_id.name')
     def _compute_reference(self):
         moves_with_reference = self.env['stock.move']
         for move in self:
@@ -279,6 +279,9 @@ class StockMove(models.Model):
                 moves_with_reference |= move
             if move.production_id and move.production_id.name:
                 move.reference = move.production_id.name
+                moves_with_reference |= move
+            if move.unbuild_id and move.unbuild_id.name:
+                move.reference = move.unbuild_id.name
                 moves_with_reference |= move
         super(StockMove, self - moves_with_reference)._compute_reference()
 
@@ -365,7 +368,6 @@ class StockMove(models.Model):
                 if not mo:
                     mo = mo.browse(mo_id)
                     mo_id_to_mo[mo_id] = mo
-                values['name'] = mo.name
                 values['origin'] = mo._get_origin()
                 values['group_id'] = mo.procurement_group_id.id
                 values['propagate_cancel'] = mo.propagate_cancel
@@ -441,7 +443,7 @@ class StockMove(models.Model):
                 origin = move._prepare_procurement_origin()
                 procurements.append(self.env['procurement.group'].Procurement(
                     move.product_id, procurement_qty, move.product_uom,
-                    move.location_id, move.name, origin, move.company_id, values))
+                    move.location_id, move.reference, origin, move.company_id, values))
 
         to_assign._action_assign()
         if procurements:
@@ -560,7 +562,6 @@ class StockMove(models.Model):
             'product_uom': bom_line.product_uom_id.id,
             'product_uom_qty': product_qty,
             'quantity': quantity_done,
-            'name': self.name,
             'picked': self.picked,
             'bom_line_id': bom_line.id,
         }

addons/sale_project_stock/models/stock_move.py
@@ -46,7 +46,7 @@ class StockMove(models.Model):
 
         return {
             'order_id': order.id,
-            'name': self.name,
+            'name': self.reference,
             'sequence': last_sequence,
             'price_unit': price,
             'tax_ids': [x.id for x in taxes],

addons/stock/models/stock_move.py
@@ -7,7 +7,7 @@ from datetime import timedelta
 from operator import itemgetter
 from re import findall as regex_findall
 
-from odoo import _, api, Command, fields, models
+from odoo import _, api, Command, fields, models, SUPERUSER_ID
 from odoo.exceptions import UserError, ValidationError
 from odoo.osv import expression
 from odoo.tools.float_utils import float_compare, float_is_zero, float_round
@@ -20,13 +20,13 @@ class StockMove(models.Model):
     _name = 'stock.move'
     _description = "Stock Move"
     _order = 'sequence, id'
+    _rec_name = 'reference'
 
     def _default_group_id(self):
         if self.env.context.get('default_picking_id'):
             return self.env['stock.picking'].browse(self.env.context['default_picking_id']).group_id.id
         return False
 
-    name = fields.Char('Description', required=True)
     sequence = fields.Integer('Sequence', default=10)
     priority = fields.Selection(
         PROCUREMENT_PRIORITIES, 'Priority', default='0',
@@ -151,6 +151,7 @@ class StockMove(models.Model):
     delay_alert_date = fields.Datetime('Delay Alert Date', help='Process at this date to be on time', compute="_compute_delay_alert_date", store=True)
     picking_type_id = fields.Many2one('stock.picking.type', 'Operation Type', compute='_compute_picking_type_id', store=True, readonly=False, check_company=True)
     is_inventory = fields.Boolean('Inventory')
+    inventory_name = fields.Char(readonly=True)
     move_line_ids = fields.One2many('stock.move.line', 'move_id')
     origin_returned_move_id = fields.Many2one(
         'stock.move', 'Origin return move', copy=False, index=True,
@@ -330,10 +331,20 @@ class StockMove(models.Model):
         for move in self:
             move.is_quantity_done_editable = move.product_id
 
-    @api.depends('name', 'picking_id.name', 'scrap_id.name', 'scrapped')
+    @api.depends('picking_id.name', 'scrap_id.name', 'scrapped', 'is_inventory', 'inventory_name')
     def _compute_reference(self):
         for move in self:
-            move.reference = move.scrap_id.name if move.scrapped else move.picking_id.name or move.name
+            if move.scrapped:
+                move.reference = move.scrap_id.name
+            elif move.is_inventory:
+                if move.inventory_name:
+                    move.reference = move.inventory_name
+                else:
+                    move.reference = _('Product Quantity Confirmed') if float_is_zero(move.quantity, precision_rounding=move.product_uom.rounding) else _('Product Quantity Updated')
+                    if move.create_uid and move.create_uid.id != SUPERUSER_ID:
+                        move.reference += f' ({move.create_uid.display_name})'
+            else:
+                move.reference = move.picking_id.name
 
     @api.depends('move_line_ids')
     def _compute_move_lines_count(self):
@@ -1286,7 +1297,6 @@ Please change the quantity done or the rounding precision in your settings.""",
     @api.onchange('product_id', 'picking_type_id')
     def _onchange_product_id(self):
         product = self.product_id.with_context(lang=self._get_lang())
-        self.name = product.partner_ref
         if product:
             self.description_picking = product._get_description(self.picking_type_id)
 

addons/stock_account/models/stock_move.py
@@ -260,7 +260,7 @@ class StockMove(models.Model):
             for val in vals:
                 val.update(move._prepare_common_svl_vals())
                 if forced_quantity and not move.scrapped:
-                    val['description'] = _('Correction of %s (modification of past move)', move.picking_id.name or move.name)
+                    val['description'] = _('Correction of %s (modification of past move)', move.reference)
                 val['description'] += val.pop('rounding_adjustment', '')
             svl_vals_list += vals
         return svl_vals_list
@@ -299,7 +299,7 @@ class StockMove(models.Model):
 
             common_vals = dict(move._prepare_common_svl_vals(), remaining_qty=0)
             if forced_quantity:
-                common_vals['description'] = _('Correction of %s (modification of past move)', move.picking_id.name or move.name)
+                common_vals['description'] = _('Correction of %s (modification of past move)', move.picking_id.name or move.reference)
 
             # create the in if it does not come from a valued location (eg subcontract -> customer)
             if not move.location_id._should_be_valued():
@@ -524,7 +524,7 @@ class StockMove(models.Model):
             for val in vals:
                 val.update(move._prepare_common_svl_vals())
                 if forced_quantity:
-                    val['description'] = _('Correction of %s (modification of past move)', move.picking_id.name or move.name)
+                    val['description'] = _('Correction of %s (modification of past move)', move.picking_id.name or move.reference)
             svl_vals_list += vals
         return svl_vals_list
 
@@ -599,7 +599,7 @@ class StockMove(models.Model):
     def _prepare_analytic_line_values(self, account_field_values, amount, unit_amount):
         self.ensure_one()
         return {
-            'name': self.name,
+            'name': self.reference,
             'amount': amount,
             **account_field_values,
             'unit_amount': unit_amount,

commit 1fd60d55523650d4bb2bb8be691b45cdba9f6d22

[FIX] repair: add reference to repair order move
Steps to reproduce:

- Create and confirm a repair order for a storable product
- Go to Inventory > Reporting > Moves History
> The related move line appears without reference

Issue:

The issue has been introduced with 791d068a33bf5f85381944c7766a799627becfb6
which purpose is was to remove the `name` field of the `stock.move`
model. However, prior to saas-18.4, the reference of the move relied
on its name:
https://github.com/odoo/odoo/blob/404cb10283cbc706eae67dd793ced363273f3602/addons/stock/models/stock_move.py#L325-L328
To not lose this reference an override of the `_compute_reference`
compute method was introduced for repair orders. But it is currently
ineffective since `moves_with_reference` is a set of records and not a
set of ides which makes it the method call its super method on every
records including the repair orders.

opw-5385004

closes odoo/odoo#239360

X-original-commit: dfe1f1c0788c5c66f9a11efb5e2a3cd295f35d0f
Signed-off-by: William Henrotin (whe) <whe@odoo.com>
Signed-off-by: Lancelot Semal (lase) <lase@odoo.com>

addons/repair/models/stock_move.py
@@ -67,7 +67,7 @@ class StockMove(models.Model):
         for move in self:
             if move.repair_id and move.repair_id.name:
                 move.reference = move.repair_id.name
-                moves_with_reference.add(move)
+                moves_with_reference.add(move.id)
         super(StockMove, self - self.env['stock.move'].browse(moves_with_reference))._compute_reference()
 
     def copy_data(self, default=None):

addons/repair/tests/test_repair.py
@@ -649,16 +649,20 @@ class TestRepair(TestRepairCommon):
         repair_order._action_repair_confirm()
         move = repair_order.move_ids[0]
         self.assertEqual(repair_order.name, "PT1/00001")
+        self.assertEqual(move.reference, "PT1/00001")
         self.assertEqual(move.location_id, stock_location_1)
         self.assertEqual(move.quantity, 0.0)
         repair_order.picking_type_id = picking_type_2
         self.assertEqual(repair_order.name, "PT2/00001")
+        self.assertEqual(move.reference, "PT2/00001")
         self.assertEqual(move.location_id, stock_location_2)
         self.assertEqual(move.quantity, 1.0)
         repair_order.picking_type_id = picking_type_1
         self.assertEqual(repair_order.name, "PT1/00002")
+        self.assertEqual(move.reference, "PT1/00002")
         repair_order.picking_type_id = picking_type_1
         self.assertEqual(repair_order.name, "PT1/00002")
+        self.assertEqual(move.reference, "PT1/00002")
 
     def test_repair_components_lots_show_in_invoice(self):
         """