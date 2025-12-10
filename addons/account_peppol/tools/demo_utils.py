73ae51a4a4db093f8aec1daf58fd7fd319523f3f
Gauthier Wala (gawa) <gawa@odoo.com>
2025-12-10 15:16:25 +0000

    [IMP] account_peppol: unregister to sender by default
    Allow unregistering to sender instead of unlinking directly.
    
    task-5395262
    
    X-original-commit: 25e4dd42d44ed8f366250dc678d8868c45d65e8e

--- addons/account_peppol/tools/demo_utils.py
+++ addons/account_peppol/tools/demo_utils.py
@@ -70,6 +70,10 @@ def _mock_call_peppol_proxy(func, self, *args, **kwargs):
             } for i in args[1]['documents']],
         }
 
+    def _mock_unregister_to_sender(user, args, kwargs):
+        user.company_id.account_peppol_proxy_state = 'sender'
+        return True
+
     endpoint = args[0].split('/')[-1]
     return {
         # participant routes
@@ -78,6 +82,7 @@ def _mock_call_peppol_proxy(func, self, *args, **kwargs):
         'register_sender_as_receiver': lambda _user, _args, _kwargs: {},
         'update_user': lambda _user, _args, _kwargs: {},
         'cancel_peppol_registration': lambda _user, _args, _kwargs: {},
+        'unregister_to_sender': _mock_unregister_to_sender,
         'migrate_peppol_registration': lambda _user, _args, _kwargs: {'migration_key': 'demo_migration_key'},
         'participant_status': lambda _user, _args, _kwargs: {'peppol_state': 'receiver'},
         'set_webhook': lambda _user, _args, _kwargs: {},
