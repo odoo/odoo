2bbb16b41a1d6317e8aa3e8ba44bf49173793786
tsm-odoo <tsm@odoo.com>
2025-12-10 08:27:35 +0100

    [IMP] bus: add confirm dialog when enabling bus logging
    Bus logging contains sensitive information. It wouldn't hurt
    to add a confirm dialog when downloading the logs to warn the user
    about it.

--- addons/bus/static/src/debug/bus_logs_menu_item.js
+++ addons/bus/static/src/debug/bus_logs_menu_item.js
@@ -1,5 +1,8 @@
 import { Component, useRef } from "@odoo/owl";
+
+import { ConfirmationDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 import { DropdownItem } from "@web/core/dropdown/dropdown_item";
+import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 
@@ -11,14 +14,20 @@ export class BusLogsMenuItem extends Component {
     setup() {
         this.busLogsService = useService("bus.logs_service");
         this.downloadButton = useRef("downloadButton");
-    }
-
-    onClickToggle() {
-        this.busLogsService.toggleLogging();
+        this.dialog = useService("dialog");
     }
 
     onClickDownload() {
-        this.env.services.bus_service.downloadLogs();
+        this.dialog.add(ConfirmationDialog, {
+            body: _t(
+                "Bus logs contain confidential information and must only be shared with trusted recipients."
+            ),
+            title: _t("You're about to download the bus logs"),
+            confirm: () => this.env.services.bus_service.downloadLogs(),
+            cancel() {},
+            confirmLabel: _t("Download"),
+            cancelLabel: _t("Discard"),
+        });
     }
 }
 
