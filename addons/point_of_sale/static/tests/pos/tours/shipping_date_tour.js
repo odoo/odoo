dedbb273f914a31c6bf06b9cabfc357b79edfe0d
dhruv <djip@odoo.com>
2025-12-12 12:54:37 +0530

    [FIX] point_of_sale: fix traceback with ship later
    steps:
    - Enable Allow Ship Later in POS configuration.
    - Open POS and add any product.
    - Proceed to the Payment screen.
    - Click Ship Later, clear the date field, and confirm
    
    issue:
    - A traceback occurs:
     `TypeError: this.state.shippingDate.toISODate is not a function`
    
    reason:
    - Here, shippingDate is a Luxon DateTime object when provided. when cleared,
      it becomes null, so converting it to ISO format is casung the error.
    
    fix:
    - Removed unnecessary conversion using `.toISODate()`.
    - Removed unnecessary hoot test.
    - Added validation on shippingDate to prevent selecting a past date.
    
    task-5406969
    
    closes odoo/odoo#239827
    
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/point_of_sale/static/tests/pos/tours/shipping_date_tour.js
+++ addons/point_of_sale/static/tests/pos/tours/shipping_date_tour.js
@@ -6,6 +6,9 @@ import { registry } from "@web/core/registry";
 import * as FeedbackScreen from "@point_of_sale/../tests/pos/tours/utils/feedback_screen_util";
 import * as PartnerList from "@point_of_sale/../tests/pos/tours/utils/partner_list_util";
 
+const { DateTime } = luxon;
+const nextYear = DateTime.now().year + 1;
+
 registry.category("web_tour.tours").add("test_pos_order_shipping_date", {
     steps: () =>
         [
@@ -15,47 +18,37 @@ registry.category("web_tour.tours").add("test_pos_order_shipping_date", {
             ProductScreen.addOrderline("Whiteboard Pen", "1"),
             ProductScreen.clickPayButton(),
             PaymentScreen.clickPaymentMethod("Cash"),
-            {
-                content: "click ship later button",
-                trigger: ".button:contains('Ship Later')",
-                run: "click",
-            },
-            {
-                content: "pick a date",
-                trigger: ".modal-body .o_datetime_input",
-                run: () => {
-                    const input = document.querySelector(".modal-body .o_datetime_input");
-                    const nextYear = new Date().getFullYear() + 1;
-                    input.value = `${nextYear}-05-30`;
-                    input.dispatchEvent(new Event("input", { bubbles: true }));
-                    input.dispatchEvent(new Event("change", { bubbles: true }));
-                },
-            },
-            {
-                content: "click confirm button",
-                trigger: ".btn:contains('Confirm')",
-                run: "click",
-            },
-            {
-                content: "Assert shipping date was set",
-                trigger: ".payment-buttons .d-flex .btn span",
-                run: () => {
-                    const spans = [
-                        ...document.querySelectorAll(".payment-buttons .d-flex .btn span"),
-                    ];
-                    const nextYear = new Date().getFullYear() + 1;
-                    const expectedDate = `5/30/${nextYear}`;
-                    if (!spans.some((span) => span.innerText === expectedDate)) {
-                        throw new Error("Expected shipping date is not set");
-                    }
-                },
-            },
+            setShipLaterDate(""),
+            setShipLaterDate(`${nextYear}-05-30`),
             PaymentScreen.clickValidate(),
             Dialog.confirm(),
-            PartnerList.clickPartner("Partner Test with Address"),
+            PartnerList.clickPartner("Partner Full"),
             FeedbackScreen.isShown(),
             FeedbackScreen.checkTicketData({
                 is_shipping_date: true,
             }),
         ].flat(),
 });
+
+const setShipLaterDate = (date) => [
+    {
+        content: "click ship later button",
+        trigger: ".button:contains('Ship Later')",
+        run: "click",
+    },
+    {
+        content: "pick a date",
+        trigger: ".modal-body .o_datetime_input",
+        run: () => {
+            const input = document.querySelector(".modal-body .o_datetime_input");
+            input.value = date;
+            input.dispatchEvent(new Event("input", { bubbles: true }));
+            input.dispatchEvent(new Event("change", { bubbles: true }));
+        },
+    },
+    {
+        content: "click confirm button",
+        trigger: ".btn:contains('Confirm')",
+        run: "click",
+    },
+];
