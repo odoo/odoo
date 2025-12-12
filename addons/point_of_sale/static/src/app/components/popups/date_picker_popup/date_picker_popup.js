a665511a54932f63b01796bf07191f7e4a0ead5b
dhruv <djip@odoo.com>
2025-12-12 12:54:37 +0530

    [FIX] point_of_sale: fix traceback with ship later
    steps:
    - Enable Allow Ship Later in POS configuration.
    - Open POS and add any product.
    - Proceed to the Payment screen.
    - Click Ship Later, clear the date field, and confirm
    
    issue:
    - A traceback occurs:
     `TypeError: this.state.shippingDate.toISODate is not a function`
    
    reason:
    - Here, shippingDate is a Luxon DateTime object when provided. when cleared,
      it becomes null, so converting it to ISO format is casung the error.
    
    fix:
    - Removed unnecessary conversion using `.toISODate()`.
    - Removed unnecessary hoot test.
    - Added validation on shippingDate to prevent selecting a past date.
    
    task-5406969

--- addons/point_of_sale/static/src/app/components/popups/date_picker_popup/date_picker_popup.js
+++ addons/point_of_sale/static/src/app/components/popups/date_picker_popup/date_picker_popup.js
@@ -2,6 +2,8 @@ import { Dialog } from "@web/core/dialog/dialog";
 import { DateTimeInput } from "@web/core/datetime/datetime_input";
 import { _t } from "@web/core/l10n/translation";
 import { Component, onMounted, useState } from "@odoo/owl";
+import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
+import { useService } from "@web/core/utils/hooks";
 const { DateTime } = luxon;
 
 export class DatePickerPopup extends Component {
@@ -9,6 +11,7 @@ export class DatePickerPopup extends Component {
     static components = { Dialog, DateTimeInput };
     static props = {
         title: { type: String, optional: true },
+        defaultValue: { type: DateTime, optional: true },
         confirmLabel: { type: String, optional: true },
         getPayload: Function,
         close: Function,
@@ -20,9 +23,8 @@ export class DatePickerPopup extends Component {
 
     setup() {
         super.setup();
-        this.state = useState({
-            shippingDate: DateTime.now(),
-        });
+        this.dialog = useService("dialog");
+        this.state = useState({ shippingDate: this.props.defaultValue ?? DateTime.now() });
         onMounted(() => {
             const input = document.querySelector(".shipping-date-selector input");
             if (input) {
@@ -33,11 +35,19 @@ export class DatePickerPopup extends Component {
         });
     }
     onDateChange(date) {
-        this.state.shippingDate = date;
+        const today = DateTime.now().startOf("day");
+        if (date && date < today) {
+            this.dialog.add(AlertDialog, {
+                title: _t("Validation Error"),
+                body: _t("Selected date cannot be in the past."),
+            });
+            this.state.shippingDate = today;
+        } else {
+            this.state.shippingDate = date;
+        }
     }
     confirm() {
-        const selected = this.state.shippingDate.toISODate();
-        this.props.getPayload(selected);
+        this.props.getPayload(this.state.shippingDate);
         this.props.close();
     }
 }
