cdc763891fbca2e7b0c70a144277088deee8ab0a
manv-afk <manv@odoo.com>
2025-12-12 11:33:26 +0100

    [FIX] point_of_sale: disallow only taxes update when product in cart
    - When a product is already in the cart, prevent updating its taxes from the frontend product edit popup. This avoids inconsistencies between the product taxes and the taxes applied to the order line.
    - Now we can still edit other fields of the product.
    
    closes odoo/odoo#239867
    
    Task-id: 4943650
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/point_of_sale/static/src/app/services/pos_store.js
+++ addons/point_of_sale/static/src/app/services/pos_store.js
@@ -2275,6 +2275,7 @@ export class PosStore extends WithLazyGetterTrap {
      * @param {import("@point_of_sale/app/models/product_product").ProductProduct?} product leave undefined to create a new product
      */
     async editProduct(product) {
+        const orderContainsProduct = product && this.orderContainsProduct(product);
         this.action.doAction(
             product
                 ? "point_of_sale.product_template_action_edit_pos"
@@ -2291,6 +2292,9 @@ export class PosStore extends WithLazyGetterTrap {
                             type: "ir.actions.act_window_close",
                         });
                     },
+                },
+                additionalContext: {
+                    taxes_readonly: orderContainsProduct,
                 },
             }
         );
