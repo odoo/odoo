d4fb601af2d167e28f3b7c0c2db275c9916a119d
pish-odoo <pish@odoo.com>
2025-12-12 14:20:28 +0530

    [FIX] mail: load default template body in full composer
    Issue:
    When opening the full composer from the chatter, the body of the default email
    template is not loaded. Only the subject line from the template appears,
    while the body remains empty or contains only the user's signature.
    
    Steps to reproduce:
    1. Install `contact`
    2. Open any contact form.
    3. In the chatter, click 'Send message' and then expand button
    4. Write a something in body, then save this as a new template.
    5. Set this new template as the default (using Debug Mode > Set Default Values).
    6. Click 'Send message' in the chatter,
    7. Click the 'Full composer' (expand) button without typing anything.
    
    Observed behavior:
    The full composer opens with the correct subject from the default template,
    but the body is empty.
    
    Cause:
    The `onClickFullComposer` method always passes a `default_body` value
    in the context to the mail.compose.message wizard. Even if the chatter input is empty
    
    Solution:
    Forward isBodyEmpty in the context from onClickFullComposer.
    If the user typed content, do nothing. If the body is empty and a default template is available,
    allow the backend to apply the default template by removing default_body.
    
    opw-5405056
    
    closes odoo/odoo#239851
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/mail/wizard/mail_compose_message.py
+++ addons/mail/wizard/mail_compose_message.py
@@ -53,8 +53,9 @@ class MailComposer(models.TransientModel):
         may have to give a huge list of IDs that won't fit into res_ids field.
         """
         # support subtype xmlid, like ``message_post``, when easier than using ``ref``
+        composer = self
         if self.env.context.get('default_subtype_xmlid'):
-            self = self.with_context(
+            composer = composer.with_context(
                 default_subtype_id=self.env['ir.model.data']._xmlid_to_res_id(
                     self.env.context['default_subtype_xmlid']
                 )
@@ -62,8 +63,17 @@ class MailComposer(models.TransientModel):
         # deprecated record context management
         if 'default_res_id' in self.env.context:
             raise ValueError(_("Deprecated usage of 'default_res_id', should use 'default_res_ids'."))
+        if (
+            'body' in fields_list
+            and self.env.context.get('default_body')
+            and self.env.context.get('body_contains_signature_only')
+            and super().default_get(['template_id']).get('template_id')
+        ):
+            ctx = dict(composer.env.context)
+            ctx.pop('default_body', None)
+            composer = composer.with_context(ctx)
 
-        result = super().default_get(fields_list)
+        result = super(MailComposer, composer).default_get(fields_list)
 
         # when being in new mode, create_uid is not granted -> ACLs issue may arise
         if 'create_uid' in fields_list and 'create_uid' not in result:
