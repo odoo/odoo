9b61135a29eaf2deb178b54918c364ddcfe0d869
Allesio <peal@odoo.com>
2025-12-15 12:53:58 +0100

    [IMP] account_edi_ubl_cii: Multi-level constraints
    This commit allows the constraint dict to be nested arbitrarily
    deep. Nested constraints allows for a more structured error
    message.
    
    task: 4625404

--- addons/account_edi_ubl_cii/tests/__init__.py
+++ addons/account_edi_ubl_cii/tests/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from . import test_autopost_bills
+from . import test_multilevel_constraints
 from . import test_partner_peppol_fields
 from . import test_ubl_cii
 from . import test_download_docs
