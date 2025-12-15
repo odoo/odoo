127c12044c8e84399a5e869b9cc94fc9bbec8ca2
Laurent Smet (LAS) <las@odoo.com>
2025-12-15 15:06:10 +0100

    [FIX] account_peppol: registration of a branch as a parent
    Fix various bugs and align the behavior in 19.0 with the one backported in 18.0.
    
    Rework the test suite TestPeppolParticipant.
    
    task-4852830
    
    closes odoo/odoo#242577
    
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- addons/account_peppol/models/res_company.py
+++ addons/account_peppol/models/res_company.py
@@ -240,12 +240,16 @@ class ResCompany(models.Model):
         self.peppol_parent_company_id = False
         for company in self:
             for parent_company in company.parent_ids[::-1][1:]:
-                if all((
-                    company.peppol_eas,
-                    company.peppol_endpoint,
-                    company.peppol_eas == parent_company.peppol_eas,
-                    company.peppol_endpoint == parent_company.peppol_endpoint,
-                )):
+                if (
+                    company.peppol_eas
+                    and company.peppol_endpoint
+                    and company.peppol_eas == parent_company.peppol_eas
+                    and company.peppol_endpoint == parent_company.peppol_endpoint
+                ) or (
+                    not company.peppol_endpoint
+                    and parent_company.peppol_eas
+                    and parent_company.peppol_endpoint
+                ):
                     company.peppol_parent_company_id = parent_company
                     break
 
