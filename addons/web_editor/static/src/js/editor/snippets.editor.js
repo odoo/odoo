38f19de854724b6f573db2908a5596674908f13d
Adnan Saiyed <adsa@odoo.com>
2025-12-16 13:17:05 +0000

    [FIX] web_editor: colorpicker getting closed
    Current behaviour before commit:
    
    Steps to reproduce:
    
    - Go to website, activate edit mode.
    - Open custom colorpicker from toolbar (or colorpicker widget
      to change background of a snippet).
    - In the custom colorpicker, trigger a mousedown event to select
      a color, then move the pointer far outside the color palette
      and release it by triggering a mouseup event.
    
    The color palette gets closed.
    
    Desired behaviour after commit:
    
    Colorpalette should not get closed on mouseup while picking
    a color from custom colorpicker.
    
    task-5170041
    
    closes odoo/odoo#245056
    
    X-original-commit: f58ab4b5b1d73e043626d71479df9b3ff7c7c140
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/static/src/js/editor/snippets.editor.js
+++ addons/web_editor/static/src/js/editor/snippets.editor.js
@@ -4167,9 +4167,10 @@ class SnippetsMenu extends Component {
             this.lastElement = false;
         });
 
-        if (!$target.closest('we-button, we-toggler, we-select, .o_we_color_preview').length) {
+        if (!$target.closest('we-button, we-toggler, we-select, .o_we_color_preview').length && !this._isColorpickerClick) {
             this._closeWidgets();
         }
+        delete this._isColorpickerClick;
         if (!$target.closest('body > *').length || $target.is('#iframe_target')) {
             return;
         }
@@ -4481,6 +4482,9 @@ class SnippetsMenu extends Component {
             clearTimeout(enableTimeoutID);
             reenable();
         });
+        if (ev.target.closest(".o_colorpicker_widget")) {
+            this._isColorpickerClick = true;
+        }
     }
     /**
      * @private
