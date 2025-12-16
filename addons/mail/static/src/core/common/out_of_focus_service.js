edb3ace8192694f02c65d6d7f054cc2628f962bc
shmn-odoo <shmn@odoo.com>
2025-12-16 12:56:05 +0000

    [FIX] mail: push notification content for inbox users
    Steps to reproduce,
    Log in as admin and ensure notifications are handled in Odoo.
    Enable push notifications and the 'Task Created' subtype for a project.
    As a second user, create a new task in that project.
    
    Before this commit,
    admin received escaped content in the push notification
    (e.g., A new task has been created in the &quot;Project Name&quot; project.)
    
    Cause,
    For inbox users, we send a client-side push notification using `previewText` as
    the body. This field returns a markup object containing escaped characters,
    which are converted to a string when passed to the notification body. Since the
    notification body only accepts plain text, these characters are displayed
    literally to the user.
    
    After this commit,
    admin receives the unescaped content in the push notification.
    (e.g., A new task has been created in the "Project Name" project.)
    
    task-5112810
    
    X-original-commit: 7fc5edc29f854d619dbcb5fcc3503fb18ca05335

--- addons/mail/static/src/core/common/out_of_focus_service.js
+++ addons/mail/static/src/core/common/out_of_focus_service.js
@@ -1,3 +1,5 @@
+import { htmlToTextContentInline } from "@mail/utils/common/format";
+
 import { browser } from "@web/core/browser/browser";
 import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
@@ -51,9 +53,10 @@ export class OutOfFocusService {
                 notificationTitle = message.authorName;
             }
         }
-        const notificationContent = message.previewText
-            .toString()
-            .substring(0, PREVIEW_MSG_MAX_SIZE);
+        const notificationContent = htmlToTextContentInline(message.previewText).substring(
+            0,
+            PREVIEW_MSG_MAX_SIZE
+        );
         await this.sendNotification({
             message: notificationContent,
             sound: Boolean(message.channel_id),
