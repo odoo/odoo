152a9c3c0f57a0f903185e2914779b078eb2af3c
bona-odoo <bona@odoo.com>
2025-12-16 21:04:53 +0530

    [FIX] project: prevent null task_id in task share wizard
    Steps to reproduce:
    -------------------
    1. Install Project with demo data
    2. Go to Tasks → cog menu → click on "Share Task"
    3. Click on the "Send" button
    
    Issue:
    ------
    The `task_id` becomes NULL in the task share wizard.
    
    ```psql
    test_m19=> select id,res_id,task_id,res_model from task_share_wizard;
     id | res_id | task_id |  res_model
    ----+--------+---------+--------------
      1 |     34 |         | project.task
    (1 row)
    ```
    In master (19.1), this leads to a crash:
    ```AssertionError: Invalid falsy real id```
    This happens since commit [4290724](https://github.com/odoo/odoo/commit/4290724a4c8c57fba4f4d3d688d38f65dadcc38f), where falsy IDs are no longer allowed.
    
    **Cause**:
    When clicking on the "Send" button, the wizard relies on default_get
    to populate default field values. However, a falsy value is passed to the
    `task_id` from here since `self` is a empty record set and later during
    related field computation, resulting in an invalid falsy record.
    
    **Solution**:
    Ensure that task_id is properly set during `default_get`.
    
    opw-5342875

--- addons/project/wizard/project_task_share_wizard.py
+++ addons/project/wizard/project_task_share_wizard.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class TaskShareWizard(models.TransientModel):
@@ -6,5 +6,14 @@ class TaskShareWizard(models.TransientModel):
     _inherit = ['portal.share']
     _description = 'Task Sharing'
 
-    task_id = fields.Many2one('project.task', default=lambda self: self.res_id)
+    @api.model
+    def default_get(self, fields):
+        result = super().default_get(fields)
+        if 'task_id' in fields and not result.get('task_id') and result['res_id']:
+            result['task_id'] = result['res_id']
+        else:
+            result['task_id'] = False
+        return result
+
+    task_id = fields.Many2one('project.task')
     project_privacy_visibility = fields.Selection(related='task_id.project_privacy_visibility')
