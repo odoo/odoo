93a329b1026c2f987c43d9cdc5fbb6cb48512a5c
Quentin Colla (qucol) <qucol@odoo.com>
2025-12-16 13:02:21 +0100

    [FIX] mrp_subcontracting: allow custom serials list
    Related: https://github.com/odoo/odoo/pull/238678/commits/0de3b8f024d86c1e6df55d1bb88d1eb98d04d4de
    
    In the Manufacturing app (`mrp`), when a list of serial numbers was
    provided, it was overwritten by another list generated from the
    *First SN* field of the `mrp.production.serials` wizard.
    
    The issue was also replicated in the subcontracting module
    (`mrp_subcontracting`). The related commit fixes the issue in the
    Manufacturing app, while this one fixes its replication in the
    subcontracting module.
    
    (related to)
    opw-5373745
    
    closes odoo/odoo#240868
    
    Signed-off-by: Lancelot Semal (lase) <lase@odoo.com>
    Signed-off-by: Quentin Colla (qucol) <qucol@odoo.com>

--- addons/mrp_subcontracting/wizard/mrp_production_serial_numbers.py
+++ addons/mrp_subcontracting/wizard/mrp_production_serial_numbers.py
@@ -20,14 +20,12 @@ class MrpProductionSerials(models.TransientModel):
         ])
         existing_lot_names = existing_lots.mapped('name')
         new_lots = []
-        for lot_name in lots:
+        for lot_name in sorted(lots):
             if lot_name in existing_lot_names:
                 continue
-            if self.lot_name == self.production_id.product_id.serial_prefix_format + self.production_id.product_id.next_serial:
+            if lot_name == self.production_id.product_id.serial_prefix_format + self.production_id.product_id.next_serial:
                 if self.production_id.product_id.lot_sequence_id:
-                    lot_name = self.production_id.product_id.lot_sequence_id.next_by_id()
-                else:
-                    lot_name = self.env['ir.sequence'].next_by_code('stock.lot.serial')
+                    self.production_id.product_id.lot_sequence_id.number_next_actual += 1
             new_lots.append({
                 'name': lot_name,
                 'product_id': self.production_id.product_id.id
