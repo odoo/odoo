0f65177599c48f1a1a4ddf254ee9e9b92f6c27fb
Adnan Saiyed <adsa@odoo.com>
2025-12-16 13:17:05 +0000

    [FIX] web_editor: colorpicker getting closed
    Current behaviour before commit:
    
    Steps to reproduce:
    
    - Go to website, activate edit mode.
    - Open custom colorpicker from toolbar (or colorpicker widget
      to change background of a snippet).
    - In the custom colorpicker, trigger a mousedown event to select
      a color, then move the pointer far outside the color palette
      and release it by triggering a mouseup event.
    
    The color palette gets closed.
    
    Desired behaviour after commit:
    
    Colorpalette should not get closed on mouseup while picking
    a color from custom colorpicker.
    
    task-5170041
    
    X-original-commit: f58ab4b5b1d73e043626d71479df9b3ff7c7c140

--- addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
+++ addons/web_editor/static/src/js/wysiwyg/wysiwyg.js
@@ -2168,7 +2168,9 @@ export class Wysiwyg extends Component {
         return backgroundGradient || $(targetElement).css(colorType === "text" ? 'color' : 'backgroundColor');
     }
     onColorpaletteDropdownHide(ev) {
-        return !(ev.clickEvent && ev.clickEvent.__isColorpickerClick);
+        const shouldHide = !(ev.clickEvent && this._isColorpickerClick);
+        delete this._isColorpickerClick;
+        return shouldHide;
     }
     onColorpaletteDropdownShow(colorType) {
         const selectedColor = this._getSelectedColor($, colorType);
@@ -3053,6 +3055,9 @@ export class Wysiwyg extends Component {
                 this._pendingBlur = false;
             }
             this._shouldDelayBlur = false;
+        }
+        if (e.target.closest(".o_colorpicker_widget")) {
+            this._isColorpickerClick = true;
         }
     }
     _onBlur() {
