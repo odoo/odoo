312c52283d8fe645b7722639c9e63dc366f656f8
Loukas Wets (lowe) <lowe@odoo.com>
2025-12-16 18:35:26 +0100

    [FIX] pos_discount: improve POS discount handling
    This commit uses an effect to update the global discount when changing
    the order. Fix the refound in when global discount since it was handeling
    only on discoud line where there could be various (one discount line per
    tax).
    
    Task-5421479
    
    closes odoo/odoo#241605
    
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/pos_discount/static/tests/unit/models/pos_order_line.test.js
+++ addons/pos_discount/static/tests/unit/models/pos_order_line.test.js
@@ -1,4 +1,4 @@
-import { expect, test } from "@odoo/hoot";
+import { animationFrame, expect, test } from "@odoo/hoot";
 import { setupPosEnv, getFilledOrder } from "@point_of_sale/../tests/unit/utils";
 import { definePosModels } from "@point_of_sale/../tests/unit/data/generate_model_definitions";
 
@@ -16,6 +16,7 @@ test("isDiscountLine", async () => {
         order
     );
     await store.applyDiscount(10);
+    await animationFrame();
     const orderline = order.getSelectedOrderline();
     expect(Math.abs(orderline.price_subtotal_incl).toString()).toBe(
         ((order.amount_total + order.amount_tax) * 0.1).toPrecision(2)
@@ -28,7 +29,8 @@ test("Test taxes after fiscal position with discount product (should not change)
     const order = await getFilledOrder(store);
     order.fiscal_position_id = store.models["account.fiscal.position"].get(1);
     await store.applyDiscount(20);
-    const discountLine = order.getDiscountLine();
+    await animationFrame();
+    const discountLine = order.discountLines[0];
     const lineValues = discountLine.prepareBaseLineForTaxesComputationExtraValues();
     const recomputedTaxes = order.fiscal_position_id.getTaxesAfterFiscalPosition(
         discountLine.product_id.taxes_id
