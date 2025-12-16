41cdbc492e05d6ffe9e16bb8e6fb9a5c3c0d6722
AhmedElemary57 <aele@odoo.com>
2025-12-16 13:09:28 +0000

    [FIX] account: allow duplicating entries with deprecated accounts
    Before this commit, duplicating a journal entry that contained a deprecated account raised a validation error immediately. This blocked the duplication process entirely, preventing users from using historical entries as templates.
    
    This commit changes the validation to be done in write function and post.
    
    Now, users can duplicate an entry with a deprecated account, correct the account in the draft, and post successfully. Validation only blocks the user if they attempt to post the entry without fixing the deprecated account.
    or if the user inserted a deprecating account while editing the move.
    
    task-5417810
    
    closes odoo/odoo#242223
    
    X-original-commit: ad989d95cfb8ae7a795810d6953002652c34b441
    Signed-off-by: John Laterre (jol) <jol@odoo.com>

--- addons/account/models/account_move_line.py
+++ addons/account/models/account_move_line.py
@@ -1567,7 +1567,6 @@ class AccountMoveLine(models.Model):
                         )
 
         lines.move_id._synchronize_business_models(['line_ids'])
-        lines._check_constrains_account_id_journal_id()
         # Remove analytic lines created for draft AMLs, after analytic_distribution has been updated
         lines.filtered(lambda l: l.parent_state == 'draft').analytic_line_ids.with_context(skip_analytic_sync=True).unlink()
         return lines
