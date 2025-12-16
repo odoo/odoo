efd18bf8e390a040982b2ad499c208cf4cfe89dc
AhmedElemary57 <aele@odoo.com>
2025-12-16 13:09:28 +0000

    [FIX] account: allow duplicating entries with deprecated accounts
    Before this commit, duplicating a journal entry that contained a deprecated account raised a validation error immediately. This blocked the duplication process entirely, preventing users from using historical entries as templates.
    
    This commit changes the validation to be done in write function and post.
    
    Now, users can duplicate an entry with a deprecated account, correct the account in the draft, and post successfully. Validation only blocks the user if they attempt to post the entry without fixing the deprecated account.
    or if the user inserted a deprecating account while editing the move.
    
    task-5417810
    
    X-original-commit: 8c337a4b964c29af8e3b3312caa7949ca59a1291

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -5348,6 +5348,7 @@ class AccountMove(models.Model):
                     invoice.invoice_date = fields.Date.context_today(self)
 
         for move in self:
+            move.line_ids._check_constrains_account_id_journal_id()
             if move.state in ['posted', 'cancel']:
                 validation_msgs.add(_('The entry %(name)s (id %(id)s) must be in draft.', name=move.name, id=move.id))
             if not move.line_ids.filtered(lambda line: line.display_type not in ('line_section', 'line_note')):
