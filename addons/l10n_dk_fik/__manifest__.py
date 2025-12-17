b9a7e39e217c925533351b2af121bc7f26e93b00
dvsh-odoo <dvsh@odoo.com>
2025-12-17 12:48:58 +0530

    [ADD] l10n_dk_fik: Add Danish FIK payment reference on customer invoices
    Before:
    - Danish companies had to rely on manual or non-standard payment
      communication on invoices.
    - They could not generate official FIK payment references.
    
    After:
    - Sales journals can now generate Danish FIK payment references automatically.
    - Users configure an 8-digit bank-issued FIK creditor number on sales journal.
    
    Impact:
    - Enables compliant Danish FIK payments without changing user workflows.
    
    Related PR:https://github.com/odoo/enterprise/pull/102612
    taskID-5401553

---
+++ addons/l10n_dk_fik/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Denmark - FIK Number",
+    "version": "1.0",
+    "category": "Accounting/Localizations",
+    "description": """
+Support Danish FIK number as payment references on customer invoices.
+""",
+    "summary": "Use FIK Number as Payment reference",
+    "depends": [
+        "l10n_dk",
+    ],
+    "data": [
+        "views/account_journal_view.xml",
+    ],
+    "auto_install": True,
+    "license": "LGPL-3",
+}
