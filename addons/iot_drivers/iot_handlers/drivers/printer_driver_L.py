305daa7faeae05d31d407028d2e537ab0f3aa237
yaso <yaso@odoo.com>
2025-12-17 14:42:42 +0000

    [FIX] iot_drivers: add additional exceptions
    With python escpos library we are currently not catching some
    exceptions.
    
    This PR adds more exceptions to catch to keep the printer working
    
    X-original-commit: b0a683fb796f17a627255f36ef9a9b5d781bb383

--- addons/iot_drivers/iot_handlers/drivers/printer_driver_L.py
+++ addons/iot_drivers/iot_handlers/drivers/printer_driver_L.py
@@ -59,7 +59,6 @@ class PrinterDriver(PrinterDriverBase):
             return
         try:
             self.escpos_device.open()
-            self.escpos_device.set_with_default(align='center')
             self.escpos_device.close()
         except escpos.exceptions.Error as e:
             _logger.info("%s - Could not initialize escpos class: %s", self.device_name, e)
@@ -159,7 +158,7 @@ class PrinterDriver(PrinterDriverBase):
                     dev.printer.qr(f"http://{system.get_ip()}", size=6)
                 self.send_status(status='success')
                 return
-            except (escpos.exceptions.Error, OSError, AssertionError):
+            except (escpos.exceptions.Error, OSError, AssertionError, AttributeError):
                 _logger.warning("Failed to print QR status receipt, falling back to simple receipt")
 
         title = commands['title'] % title
