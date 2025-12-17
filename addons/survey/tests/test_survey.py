odoo/odoo

commit d67b5632777ce09fe77a4e7e29a6474eb26887a7

[IMP] survey: improve leaderboard bars width
Width computation was sometimes grossly inconsistent.
Different decompositions into of identical
"updated score" lead to different score bar widths
depending on their "current score" + "question score".

Additionally, much space was taken for the "0" position and
the "0p" placeholder.

With this commit, we can use the available width more
efficiently by using the current minimal score
as 0-width, and reserve no minimum width for the
question score by moving the question score outside the
bar.

Removing the increasing counter for question score
improves performances, allows to simplify
showQuestionScore, and increases readability as the
value is now moving along with the growing question score
bar. "+ 0" is shown for non-increasing scores for style
consistency (separator between old and score question).

Adding `scoring_total` as fallback for `max_updated_score`
is used for the proper rendering of the leaderboard on the
"results" page.

Task-4742937

Part-of: odoo/odoo#236336
Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

addons/survey/views/survey_templates_user_input_session.xml
@@ -79,7 +79,7 @@
         <t t-set="is_scored_question" t-value="any(answer.answer_score for answer in question.suggested_answer_ids)" />
         <t t-set="show_bar_chart" t-value="question.question_type in ['simple_choice', 'multiple_choice', 'scale']" />
         <t t-set="show_text_answers" t-value="question.question_type in ['char_box', 'date', 'datetime'] and not question.save_as_email and not question.save_as_nickname" />
-        <t t-set="bar_reserved_with_rem" t-value="23.4"/> <!-- See setup of SurveySessionLeaderboard -->
+        <t t-set="bar_reserved_with_rem" t-value="25"/> <!-- See setup of SurveySessionLeaderboard -->
         <div class="wrap min-vh-100 align-items-center justify-content-center d-flex flex-column o_survey_session_manage invisible"
             t-att-style="'display: none;' if is_rpc_call else ''"
             t-att-data-is-rpc-call="is_rpc_call"
@@ -194,12 +194,13 @@
     </template>
 
     <template id="user_input_session_leaderboard" name="Survey User Input Leaderboard">
-        <t t-set="bar_reserved_with_rem" t-value="23.4"/> <!-- See setup of SurveySessionLeaderboard -->
+        <t t-set="bar_reserved_with_rem" t-value="25"/> <!-- See setup of SurveySessionLeaderboard -->
         <div t-if="leaderboard" class="position-relative mb-5 o_survey_leaderboard_scores" t-attf-style="height: calc(3.8rem * #{len(leaderboard)});">
-            <t t-set="max_updated_score" t-value="max(score.get('updated_score', 1) for score in leaderboard) or 1" />
+            <t t-set="max_updated_score" t-value="max(score.get('updated_score', score.get('scoring_total', 1)) for score in leaderboard) or 1" />
             <t t-foreach="leaderboard" t-as="score">
                 <t t-set="current_score" t-value="score.get('scoring_total', 0)"/>
                 <t t-set="question_score" t-value="score.get('question_score', 0)"/>
+                <t t-set="min_current_score" t-value="score.get('min_current_score', 0)"/>
                 <t t-set="max_question_score" t-value="score.get('max_question_score', 1)"/>
                 <div class="o_survey_session_leaderboard_item ms-2 d-flex position-absolute"
                     t-attf-style="top: calc(#{score_index} * 3.8rem);"
@@ -218,19 +219,20 @@
                             <span t-else="">Anonymous</span>
                         </div>
                     </div>
-                    <t t-set="width_ratio" t-value="round(current_score / max_updated_score, 3)"/>
-                    <div class="o_survey_session_leaderboard_bar ms-2 align-top d-inline-block text-end fw-bold"
+                    <t t-set="width_ratio" t-value="round((current_score - min_current_score) / (max_updated_score - min_current_score), 3)"/>
+                    <div class="o_survey_session_leaderboard_bar_landmark ms-2"/>
+                    <div class="o_survey_session_leaderboard_bar align-top d-inline-block"
                         t-attf-style="width: calc(calc(100% - #{bar_reserved_with_rem}rem) * #{width_ratio})">
                     </div>
                     <div class="o_survey_session_leaderboard_bar_question me-2 align-top d-inline-block text-end fw-bold position-relative"
                         style="width: 0;"
-                        t-att-data-width-ratio="str(round(question_score / max_updated_score, 3))"
+                        t-att-data-width-ratio="str(round(question_score / (max_updated_score - min_current_score), 3))"
                         t-att-data-max-question-score="str(max_question_score)"
                         t-att-data-question-score="str(question_score)">
-                        <div class="o_survey_session_leaderboard_bar_question_score position-absolute"></div>
                     </div>
                     <div class="o_survey_session_leaderboard_score d-inline-block"
                         t-out="'%.0f p' % score['scoring_total']"/>
+                    <div class="o_survey_session_leaderboard_bar_question_score text-nowrap"></div>
                 </div>
             </t>
         </div>

addons/survey/models/survey_survey.py
@@ -1002,17 +1002,23 @@ class SurveySurvey(models.Model):
                     question_scores.get(input_line['user_input_id'][0], 0) + input_line['answer_score']
 
             score_position = 0
+            max_question_score = sum(
+                score for score in self.session_question_id.suggested_answer_ids.mapped('answer_score')
+                if score > 0
+            ) or 1
+            min_current_score = max(min(
+                leaderboard_item['scoring_total'] - question_scores.get(leaderboard_item['id'], 0)
+                for leaderboard_item in leaderboard
+            ), 0)
             for leaderboard_item in leaderboard:
                 question_score = question_scores.get(leaderboard_item['id'], 0)
                 leaderboard_item.update({
                     'updated_score': leaderboard_item['scoring_total'],
                     'scoring_total': leaderboard_item['scoring_total'] - question_score,
                     'leaderboard_position': score_position,
-                    'max_question_score': sum(
-                        score for score in self.session_question_id.suggested_answer_ids.mapped('answer_score')
-                        if score > 0
-                    ) or 1,
-                    'question_score': question_score
+                    'max_question_score': max_question_score,
+                    'min_current_score': min_current_score,
+                    'question_score': question_score,
                 })
                 score_position += 1
             leaderboard = sorted(

addons/survey/static/src/interactions/survey_session_leaderboard.js
@@ -25,7 +25,7 @@ export class SurveySessionLeaderboard extends Interaction {
             // See also this.showQuestionScores
             return {
                 transition: "width 1s ease-out",
-                width: `max(calc(calc(100% - ${this.BAR_RESERVED_WIDTH_REM}rem) * ${barEl.dataset.widthRatio}), ${this.BAR_QUEST_MIN_WIDTH_REM}rem)`,
+                width: `calc(calc(100% - ${this.BAR_RESERVED_WIDTH_REM}rem) * ${barEl.dataset.widthRatio})`,
             };
         }
         return {};
@@ -33,9 +33,7 @@ export class SurveySessionLeaderboard extends Interaction {
 
     setup() {
         this.fadeInOutTime = 400;
-        this.BAR_QUEST_MIN_WIDTH_REM = 3.7;
-        // Score width + margin + Bar score min width (css) + BAR_QUEST_MIN_WIDTH_REM + margin + nickname width
-        this.BAR_RESERVED_WIDTH_REM = 6.5 + 1 + 3.7 + this.BAR_QUEST_MIN_WIDTH_REM + 1 + 7.5;
+        this.BAR_RESERVED_WIDTH_REM = 25;
         this.BAR_HEIGHT = "3.8rem";
         this.surveyAccessToken = this.el.closest(
             ".o_survey_session_manage"
@@ -94,14 +92,17 @@ export class SurveySessionLeaderboard extends Interaction {
                             ".o_survey_session_leaderboard_bar"
                         ).style.backgroundColor = `${rgb}99`;
                         item.querySelector(
+                            ".o_survey_session_leaderboard_bar_landmark"
+                        ).style.backgroundColor = `${rgb}99`;
+                        item.querySelector(
                             ".o_survey_session_leaderboard_bar_question"
                         ).style.backgroundColor = `${rgb}FF`;
                     });
                 fadeIn(this.el, this.fadeInOutTime, async () => {
                     if (ev.detail.isScoredQuestion) {
-                        await this.waitFor(this.showQuestionScores());
+                        await this._waitForAnimation(() => this.showQuestionScores(), 300, 1400);
                         this.updateAutoScrollStyle();
-                        await this._waitForAnimation(() => this.sumScores(), 1400, 500);
+                        await this._waitForAnimation(() => this.sumScores(), 500, 500);
                         await this._waitForAnimation(() => this.animatePositionLabels(), 1200);
                         await this._waitForAnimation(() => this.reorderScores(), 1000);
                         this.leaderboardAnimationPhase = null;
@@ -220,7 +221,7 @@ export class SurveySessionLeaderboard extends Interaction {
             if (currentPosition !== newPosition) {
                 const offset = newPosition > currentPosition ? 2 : -2;
                 await this._waitForAnimation(() => this.moveTo(item, newPosition, offset), 300);
-                item.style.transition = "top ease-in-out .1s";
+                item.style.transition = "top ease-in-out .25s";
                 await this._waitForAnimation(() => this.moveTo(item, newPosition, offset * -0.3), 100);
                 await this._waitForAnimation(() => this.moveTo(item, newPosition, 0), 0);
             }
@@ -263,34 +264,16 @@ export class SurveySessionLeaderboard extends Interaction {
      * @private
      */
     async showQuestionScores() {
-        let animationDone;
-        const animationPromise = new Promise(function (resolve) {
-            animationDone = resolve;
-        });
-        this.waitForTimeout(() => {
-            this.leaderboardAnimationPhase = "showQuestionScores";
-            this.el
-                .querySelectorAll(".o_survey_session_leaderboard_bar_question")
-                .forEach((barEl) => {
-                    const scoreEl = barEl.querySelector(
-                        ".o_survey_session_leaderboard_bar_question_score"
-                    );
-                    scoreEl.textContent = "0 p";
-                    const questionScore = parseInt(barEl.dataset.questionScore);
-                    if (questionScore && questionScore > 0) {
-                        let increment = parseInt(barEl.dataset.maxQuestionScore / 40);
-                        if (!increment || increment === 0) {
-                            increment = 1;
-                        }
-                        scoreEl.textContent = "+ 0 p";
-                        this.waitForTimeout(() => {
-                            this.animateScoreCounter(scoreEl, 0, questionScore, increment, true);
-                        }, 400);
-                    }
-                    this.waitForTimeout(animationDone, 1400);
-                });
-        }, 300);
-        return animationPromise;
+        this.leaderboardAnimationPhase = "showQuestionScores";
+        this.el
+            .querySelectorAll(".o_survey_session_leaderboard_item")
+            .forEach((itemEl) => {
+                const scoreEl = itemEl.querySelector(
+                    ".o_survey_session_leaderboard_bar_question_score"
+                );
+                const questionScore = parseInt(itemEl.dataset.questionScore);
+                scoreEl.textContent = `+ ${questionScore || 0} p`;
+            });
     }
 
     /**

addons/survey/static/src/scss/survey_templates_form.scss
@@ -461,23 +461,24 @@ _::-webkit-full-page-media, _:future, :root .o_survey_wrap {
         .o_survey_session_leaderboard_bar, .o_survey_session_leaderboard_bar_question {
             height: 2.8rem;
         }
+        .o_survey_session_leaderboard_bar_landmark {
+            width: 0.5rem;
+            background-color: #007A77;
+        }
 
         .o_survey_session_leaderboard_bar {
-            min-width: 3.7rem;
             background-color: #007A77;
-            z-index: 2;
         }
 
         .o_survey_session_leaderboard_bar_question_score {
-            top: .2rem;
-            right: .5rem;
-            width: 20rem;
+            padding-top: .2rem;
             z-index: 1;
         }
 
         .o_survey_session_leaderboard_score {
             padding-top: .2rem;
-            width: 7.5rem;
+            padding-left: .2rem;
+            margin-right: 1rem;
             overflow: hidden;
             text-overflow: ellipsis;
             white-space: nowrap;

commit 359bad8a4eb51c7f03f99db22ee73336db723ed7

[FIX] survey: avoid new divisionByZero's on leaderboard
During a survey live session, if all participants answer incorrectly to a question,
our new logic just introduced in d67b5632 trimming bar width to emphasize 
the difference between participants score will cause a template rendering 
crash due to a division by zero.

Test included.

Task-4742937

closes odoo/odoo#240288

Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>

addons/survey/views/survey_templates_user_input_session.xml
@@ -220,14 +220,14 @@
                             <span t-else="">Anonymous</span>
                         </div>
                     </div>
-                    <t t-set="width_ratio" t-value="round((current_score - min_current_score) / (max_updated_score - min_current_score), 3)"/>
+                    <t t-set="width_ratio" t-value="round((current_score - min_current_score) / ((max_updated_score - min_current_score) or 1), 3)"/>
                     <div class="o_survey_session_leaderboard_bar_landmark ms-2"/>
                     <div class="o_survey_session_leaderboard_bar align-top d-inline-block"
                         t-attf-style="width: calc(calc(100% - #{bar_reserved_with_rem}rem) * #{width_ratio})">
                     </div>
                     <div class="o_survey_session_leaderboard_bar_question me-2 align-top d-inline-block text-end fw-bold position-relative"
                         style="width: 0;"
-                        t-att-data-width-ratio="str(round(question_score / (max_updated_score - min_current_score), 3))"
+                        t-att-data-width-ratio="str(round(question_score / ((max_updated_score - min_current_score) or 1), 3))"
                         t-att-data-max-question-score="str(max_question_score)"
                         t-att-data-question-score="str(question_score)">
                     </div>

addons/survey/tests/test_survey.py
@@ -3,6 +3,7 @@
 
 import datetime
 
+from dateutil.relativedelta import relativedelta
 from freezegun import freeze_time
 
 from odoo import _, Command, fields
@@ -800,7 +801,7 @@ class TestSurveyInternals(common.TestSurveyCommon, MailCase):
         self.assertEqual(question_and_page_ids - invalid_records, returned_questions_and_pages)
 
     def test_survey_session_leaderboard(self):
-        """Check leaderboard rendering with small (max) scores values."""
+        """Check leaderboard rendering with small (max) and 0 scores values."""
         start_time = datetime.datetime(2023, 7, 7, 12, 0, 0)
         test_survey = self.env['survey.survey'].create({
             'title': 'Test This Survey',
@@ -816,12 +817,12 @@ class TestSurveyInternals(common.TestSurveyCommon, MailCase):
                         Command.create({'value': 'In Europe', 'answer_score': 0., 'is_correct': False}),
                     ],
                     'title': 'Where is india?',
-                }),
+                }) for _ in range(2)
             ]
         })
-        question_1 = test_survey.question_and_page_ids[0]
-        answer_correct = question_1.suggested_answer_ids[0]
+        question_1, question_2 = test_survey.question_and_page_ids
         user_input = self.env['survey.user_input'].create({'survey_id': test_survey.id, 'is_session_answer': True})
+        answer_correct = question_1.suggested_answer_ids[0]
         user_input_line = self.env['survey.user_input.line'].create({
             'user_input_id': user_input.id,
             'question_id': question_1.id,
@@ -829,6 +830,23 @@ class TestSurveyInternals(common.TestSurveyCommon, MailCase):
             'suggested_answer_id': answer_correct.id,
         })
         self.assertEqual(user_input_line.answer_score, 0.125)
+        self.env['ir.qweb']._render('survey.user_input_session_leaderboard', {
+            'animate': True,
+            'leaderboard': test_survey._prepare_leaderboard_values()
+        })
+        # Switch to the next question and answer incorrectly (everyone)
+        test_survey.write({
+            "session_question_id": question_2.id,
+            "session_question_start_time": start_time + relativedelta(seconds=10),
+        })
+        answer_wrong = question_2.suggested_answer_ids[1]
+        user_input_line = self.env['survey.user_input.line'].create({
+            'user_input_id': user_input.id,
+            'question_id': question_2.id,
+            'answer_type': 'suggestion',
+            'suggested_answer_id': answer_wrong.id,
+        })
+        self.assertEqual(user_input_line.answer_score, 0)
         self.env['ir.qweb']._render('survey.user_input_session_leaderboard', {
             'animate': True,
             'leaderboard': test_survey._prepare_leaderboard_values()