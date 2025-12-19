7597e9b517700722aba4eae3e648add94bcb4ded
Benoit Socias (bso) <bso@odoo.com>
2025-12-19 10:52:33 +0100

    [FIX] html_editor: add button to convert syntax highlight to paragraph
    When syntax highlighting is activated on code blocks, neither the
    powerbox nor the toolbar is available inside the block, making it
    impossible to convert it back into a paragraph.
    
    This commit adds a button to convert such code blocks back into
    paragraphs.
    
    Steps to reproduce:
    - Go to a "To Do" note
    - Insert a code block with `/code`
    
    => There was no way to convert it back to a paragraph
    
    task-5241467
    
    Part-of: odoo/odoo#240692
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/html_editor/static/src/others/embedded_components/backend/syntax_highlighting/syntax_highlighting.js
+++ addons/html_editor/static/src/others/embedded_components/backend/syntax_highlighting/syntax_highlighting.js
@@ -20,6 +20,7 @@ export class EmbeddedSyntaxHighlightingComponent extends Component {
         value: { type: String },
         languageId: { type: String },
         onTextareaFocus: { type: Function },
+        convertToParagraph: { type: Function },
         host: { type: Object },
     };
 
