5bb4ead78794b642dd1aafbe0f8457f68cd88d96
santiagopim <santiagopim@gmail.com>
2025-12-19 20:51:24 +0100

    [FIX] l10n_es_edi_facturae: incorrect tax when negative amounts
    Issue:
    When using negative amounts, for example to explicitly show a
    discount, the tax calculation is incorrect due to the application of
    the `abs` function. Furthermore, the way to find out if a tax is of
    the withholding type is based on the sign of the value, which can lead
    to error in these cases.
    
    Cause:
    A previous change (#40697d4) added the `abs` function so the
    'TotalTaxesWithheld' would be always with positive value. But this
    also affects the calculation of taxes 'TotalTaxOutputs' in some cases,
    such as if the invoice line has negative values.
    
    Steps to reproduce:
    - Install `l10n_es_edi_facturae`
    - With the ES company, create an invoice with some standard lines and
      one line with negative amounts, as an explicit discount
    - Confirm the invoice and send (facturae)
    - Open the XML attached in the chatter
    - Observe that the taxes amounts (VAT and WITHHOLDING) are erroneous
    
    A correct invoice should be for example:
    Product        Price       Taxes       Amount
    ---------------------------------------------
    PRODUCT-A       1000    21%VAT 15%WHI    1000
    Discount        -100    21%VAT 15%WHI    -100
    ---------------------------------------------
                           Untaxed amount     900
                          Withholding 15%    -135
                                  VAT 21%     189
                          -----------------------
                                     TOTAL    954

--- addons/l10n_es_edi_facturae/models/account_move.py
+++ addons/l10n_es_edi_facturae/models/account_move.py
@@ -469,10 +469,12 @@ class AccountMove(models.Model):
             if not tax:
                 continue
 
-            is_withholding = tax.amount < 0.0
+            is_withholding = tax.l10n_es_edi_facturae_tax_type == '04' # IRPF
             tax_data = self._l10n_es_edi_facturae_get_tax_node_from_tax_data({**values, 'grouping_key': tax})
             invoice_values['TaxesWithheld' if is_withholding else 'TaxOutputs'].append(tax_data)
-            invoice_values['TotalTaxesWithheld' if is_withholding else 'TotalTaxOutputs'] += abs(values['tax_amount_currency'])
+            invoice_values['TotalTaxesWithheld' if is_withholding else 'TotalTaxOutputs'] += values['tax_amount_currency']
+
+        invoice_values['TotalTaxesWithheld'] = abs(invoice_values['TotalTaxesWithheld'])
 
         invoice_values['TotalGrossAmountBeforeTaxes'] = (
             invoice_values['TotalGrossAmount']
