226694302e4e3649361d1f2f46f6d6d24cb3c373
Lancelot Semal <lase@odoo.com>
2025-12-19 11:06:03 +0100

    [FIX] *: avoid resetting kit price unit at validation
    * mrp_account, sale_mrp{,_margin}
    
    Steps to reproduce:
    
    - Install sale_margin, sale_management, mrp
    - Create a kit product with an avco product category:
        - 1 x COMP1, cost 10
        - 1 x COMP2, cost 20
    - On the kit product update the cost accroding to the bom
    - Create an SO for 1 unit of your kit
    - Validate the associated receipt
    > The cost (purchase_price) of the sol of the kit has been reset from 30
    to 0.
    
    Cause of the issue:
    
    Validating the receipt will trigger the dependence of the
    `purchase_price` compute method since the moves are now done:
    https://github.com/odoo/odoo/blob/18dc738c710f87d1f937b0d1fbb619c75f5ad952/addons/sale_margin/models/sale_order_line.py#L15-L18
    However, the `_get_price_unit` will `return 0` since `sol` is linked to
    both component moves:
    https://github.com/odoo/odoo/blob/18dc738c710f87d1f937b0d1fbb619c75f5ad952/addons/sale_stock_margin/models/sale_order_line.py#L10-L20
    
    opw-5420977
    
    Part-of: odoo/odoo#242052
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/mrp_account/models/stock_move.py
+++ addons/mrp_account/models/stock_move.py
@@ -1,5 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from collections import defaultdict
+
 from odoo import models
 
 
@@ -27,3 +29,16 @@ class StockMove(models.Model):
             m.bom_line_id.bom_id.type == 'phantom' and
             m.bom_line_id.bom_id == moves.bom_line_id.bom_id
         )
+
+    def _get_kit_price_unit(self, product, kit_bom, valuated_quantity):
+        """ Override the value for kit products """
+        _dummy, exploded_lines = kit_bom.explode(product, valuated_quantity)
+        total_price_unit = 0
+        component_qty_per_kit = defaultdict(float)
+        for line in exploded_lines:
+            component_qty_per_kit[line[0].product_id] += line[1]['qty']
+        for component, valuated_moves in self.grouped('product_id').items():
+            price_unit = super(StockMove, valuated_moves)._get_price_unit()
+            qty_per_kit = component_qty_per_kit[component] / kit_bom.product_qty
+            total_price_unit += price_unit * qty_per_kit
+        return total_price_unit
