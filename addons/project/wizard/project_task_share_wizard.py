cc899a7a7f790fa2cf1582b06f952a4a2a5a095b
njor-odoo <njor@odoo.com>
2025-12-22 18:14:13 +0530

    [IMP] project: remove task_id from task share wizard
    - Remove the task_id field since the active task is already provided
      through res_model and res_id.
    
    task-5423455

--- addons/project/wizard/project_task_share_wizard.py
+++ addons/project/wizard/project_task_share_wizard.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class TaskShareWizard(models.TransientModel):
@@ -6,5 +6,14 @@ class TaskShareWizard(models.TransientModel):
     _inherit = ['portal.share']
     _description = 'Task Sharing'
 
-    task_id = fields.Many2one('project.task', default=lambda self: self.env.context.get('active_id'))
-    project_privacy_visibility = fields.Selection(related='task_id.project_privacy_visibility')
+    @api.model
+    def default_get(self, fields):
+        result = super().default_get(fields)
+        res_model = result.get('res_model')
+        res_id = result.get('res_id')
+        if res_model and res_id:
+            record = self.env[res_model].browse(res_id)
+            result['project_privacy_visibility'] = record.project_privacy_visibility
+        return result
+
+    project_privacy_visibility = fields.Selection(selection=lambda self: self.env['project.task']._fields['project_privacy_visibility']._description_selection(self.env))
