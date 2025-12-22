06ca31cacd27a94ab994a7d8b23cd29346d67d5e
Robin Engels (roen) <roen@odoo.com>
2025-12-22 14:20:20 +0100

    [FIX] pos_sale: fix qty_delivered with refund
    This commit fixes an issue introduced here : https://github.com/odoo/odoo/pull/240945
    If the multi steps delivery was enabled, the flow would give a traceback
    
    The qty_delivered on sale.order.line was not correctly computed
    when the original order was refunded with a ship later.
    
    Steps to reproduce:
    -------------------
    * Create a sale order for 5 quantities of any product
    * Confirm the sale order
    * Settle the order in the PoS
    * At this point the qty_delivered on the sale order line is 5
    * Now go back to the PoS and refund partially the order for 3 quantities
      and use the "Ship Later" option
    > Observation: The qty_delivered is 0 instead of 2
    
    Why the fix:
    ------------
    This line would crash because picking would contain more than one record
    https://github.com/odoo-dev/odoo/blob/efd415b88d4e793318053138beadd3291a551e97/addons/pos_sale/models/sale_order.py#L95
    
    Instead of doing a groupby we just apply the logic separately for the
    normal lines and the refund lines.
    
    opw-5059560
    
    closes odoo/odoo#245013
    
    Signed-off-by: Robin Engels (roen) <roen@odoo.com>

--- addons/pos_sale/models/sale_order.py
+++ addons/pos_sale/models/sale_order.py
@@ -87,13 +87,17 @@ class SaleOrderLine(models.Model):
     @api.depends('pos_order_line_ids.qty', 'pos_order_line_ids.order_id.picking_ids', 'pos_order_line_ids.order_id.picking_ids.state', 'pos_order_line_ids.refund_orderline_ids.order_id.picking_ids.state')
     def _compute_qty_delivered(self):
         super()._compute_qty_delivered()
+
+        def update_qty_delivered_from_pickings(sale_line, pos_lines):
+            if all(picking.state == 'done' for picking in pos_lines.order_id.picking_ids):
+                sale_line.qty_delivered += sum((self._convert_qty(sale_line, pos_line.qty, 'p2s') for pos_line in pos_lines if sale_line.product_id.type != 'service'), 0)
+
         for sale_line in self:
-            pos_lines = (sale_line.pos_order_line_ids | sale_line.pos_order_line_ids.refund_orderline_ids).filtered(lambda order_line: order_line.order_id.state not in ['cancel', 'draft'])
-            # Group lines by procurement_id
-            for key, lines in groupby(pos_lines, lambda l: l.order_id.picking_ids.group_id):
-                pickings = [line.order_id.picking_ids for line in lines]
-                if all(picking.state == 'done' for picking in pickings):
-                    sale_line.qty_delivered += sum((self._convert_qty(sale_line, pos_line.qty, 'p2s') for pos_line in lines if sale_line.product_id.type != 'service'), 0)
+            pos_lines = sale_line.pos_order_line_ids.filtered(lambda order_line: order_line.order_id.state not in ['cancel', 'draft'])
+            update_qty_delivered_from_pickings(sale_line, pos_lines)
+
+            refund_lines = sale_line.pos_order_line_ids.refund_orderline_ids.filtered(lambda order_line: order_line.order_id.state not in ['cancel', 'draft'])
+            update_qty_delivered_from_pickings(sale_line, refund_lines)
 
     @api.depends('pos_order_line_ids.qty', 'pos_order_line_ids.order_id.state')
     def _compute_qty_invoiced(self):
