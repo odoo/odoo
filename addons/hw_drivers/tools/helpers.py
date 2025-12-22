4bf916f537469765e780093db20367acc953b6b5
yaso <yaso@odoo.com>
2025-12-22 16:37:47 +0000

    [FIX] hw_drivers: allow upgrades to >= 19.1
    This PR adds 2 migration scripts which allow older iot box images (<= 25_07) to work with databases in
    saas-19.1 and later.
    
    It updates os to debian trixie and installs all of the necessary
    packages to allow working after upgrades or with new databases.
    
    closes odoo/odoo#241663
    
    X-original-commit: 5316a5967f457740fd3c4426c8974af7b69a42b9
    Signed-off-by: Louis Travaux (lotr) <lotr@odoo.com>

--- addons/hw_drivers/tools/helpers.py
+++ addons/hw_drivers/tools/helpers.py
@@ -131,6 +131,37 @@ def check_certificate():
         return {"status": CertificateStatus.OK, "message": message}
 
 
+def check_version_upgrades(local_branch, db_branch):
+    """
+    Check if the iot is < 19.1 and upgrading to >= saas-19.1
+    If so and current python version is less than 3.12, run the scripts
+    located in upgrade_scripts/ to upgrade the python version
+    :param local_branch: The local git branch (Ex: "19.0" / "17.0-hw-drivers-compatibility-with-trixie-yaso")
+    :param db_branch: The git branch of the connected Odoo database (Ex: "saas-19.1" / "master" etc.)
+    """
+    if platform.system() != 'Linux':
+        _logger.debug("Ignoring version upgrade check on non-Linux system")
+        return
+
+    try:
+        # 1. Check if the upgrade script needs to be ran
+        # Needed if local branch is < 19.1 and db branch is >= 19.1 + python version < 3.12
+        _logger.info("Checking for version upgrades for local branch %s / db_branch %s", local_branch, db_branch)
+        version_db = db_branch[-4:] if db_branch != 'master' else db_branch  # master is currently always >= 19.1
+        version_local = local_branch[-4:] if local_branch != 'master' else local_branch
+        local_python_version = tuple(int(x) for x in platform.python_version_tuple()[:2])
+        if version_local >= '19.1' or version_db < '19.1' or local_python_version >= (3, 12):
+            _logger.info("Ignoring unnecessary upgrade for local branch %s / db_branch %s with python version %s", local_branch, db_branch, local_python_version)
+            return
+        with writable():
+            _logger.warning("Updating to Debian Trixie for >= 19.1")
+            subprocess.run(
+                ['/home/pi/odoo/addons/hw_drivers/tools/upgrade_scripts/upgrade_trixie/upgrade_trixie.sh'], check=True,
+            )
+    except subprocess.CalledProcessError:
+        _logger.exception("Failed to upgrade to debian Trixie. Check /home/pi/upgrade.log file for more details")
+
+
 def check_git_branch():
     """
     Check if the local branch is the same than the connected Odoo DB and
@@ -156,7 +187,6 @@ def check_git_branch():
             db_branch = json.loads(response.data)['result']['server_serie'].replace('~', '-')
             if not subprocess.check_output(git + ['ls-remote', 'origin', db_branch]):
                 db_branch = 'master'
-
             local_branch = (
                 subprocess.check_output(git + ['symbolic-ref', '-q', '--short', 'HEAD']).decode('utf-8').rstrip()
             )
@@ -168,6 +198,8 @@ def check_git_branch():
 
             if db_branch != local_branch:
                 try:
+                    check_version_upgrades(local_branch, db_branch)
+
                     with writable():
                         subprocess.run(git + ['branch', '-m', db_branch], check=True)
                         subprocess.run(git + ['remote', 'set-branches', 'origin', db_branch], check=True)
