b9a0c0e48e6dff47132bd30e16e61eb316091967
Arnav Varshney <avar@odoo.com>
2025-12-23 11:53:07 +0000

    [FIX] l10n_th: make branch name translatable according to language set on partner.
    Currently, l10n_th_branch_name is not translatable. Regardless of the language setting, it is printed in English on the tax invoice. This PR addresses that.
    
    Task-5438534
    
    Signed-off-by: Arnav Varshney <avar@odoo.com>
    X-original-commit: 76717e588bfd012b42e859bfc829257d899c6165

--- addons/l10n_th/models/res_partner.py
+++ addons/l10n_th/models/res_partner.py
@@ -14,4 +14,5 @@ class ResPartner(models.Model):
                 partner.l10n_th_branch_name = ""
             else:
                 code = partner.company_registry
-                partner.l10n_th_branch_name = f"Branch {code}" if code else "Headquarter"
+                partner.l10n_th_branch_name = partner.env._("Branch %(code)s", code=code) if code else partner.env._(
+                    "Headquarter")
