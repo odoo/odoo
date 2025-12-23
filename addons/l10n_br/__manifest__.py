85cbc36cd8c5bf99c6d2989e7321e8f87f30bc5a
Frantisek Binovsky <frbin@odoo.com>
2025-12-23 13:52:11 +0100

    [IMP] base_address_extended,*: add city form
    *: ,web, l10n_br, l10n_pe
    
    Part of a larger UI rework task.
    
    Cities previously used a default form view generated by the framework,
    this task adds a custom one (Updated inheriting l10n modules as well).
    
    When the city is updated, it should not clear the Zip code field if the selected
     city doesn't have a specified Zip. Same for the state.
    
    Incorrect behaviour before: in Mexico postal codes are not assigned to
    cities they're assigned to colonies => The city zip was always empty and always
    erasing valid data
    
    Added the state name to the city dropdown dialog - had to extend the dialog
    min-width to account for the required space. For longer city/state names, the
    state name is displayed on a second line.
    
    Task-5262571

--- addons/l10n_br/__manifest__.py
+++ addons/l10n_br/__manifest__.py
@@ -74,6 +74,7 @@ Create electronic sales invoices with Avatax.
         'views/res_company_views.xml',
         'views/account_journal_views.xml',
         'views/res_bank_views.xml',
+        'views/res_city_view.xml',
         'views/report_invoice.xml',
     ],
     'demo': [
