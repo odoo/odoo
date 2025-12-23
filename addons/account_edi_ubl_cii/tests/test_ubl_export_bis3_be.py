95ccef8d91a915553a35f816ff9edac805124b5d
AhmedElemary57 <aele@odoo.com>
2025-12-23 10:06:59 +0000

    [FIX] account_edi_ubl_cii: allow negative discounts for Peppol BIS 3
    In Peppol BIS 3 (UBL 2.1), negative discounts are logically treated as Surcharges (Charges) rather than Allowances.
    
    Previously, Odoo exported negative discounts as an Allowance with a negative amount and a numeric reason code (95). This violated:
    
    BR-CL-20 / PEPPOL-EN16931-CL003: Coded charge reasons must belong to the UNCL 7161 (alphabetic) list when ChargeIndicator is 'true'.
    
    PEPPOL-EN16931-R120: The line net amount calculation failed due to the negative sign in the calculation.
    
    Solution:
    Dynamically toggles ChargeIndicator based on the discount sign.
    Swaps AllowanceChargeReasonCode to 'ADK' (Surcharge) for negative discounts.
    
    task-5432024
    
    closes odoo/odoo#243926
    
    X-original-commit: 0328845bbbf5987306dafbf3fd63b7a654171a59
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- addons/account_edi_ubl_cii/tests/test_ubl_export_bis3_be.py
+++ addons/account_edi_ubl_cii/tests/test_ubl_export_bis3_be.py
@@ -462,3 +462,22 @@ class TestUblExportBis3BE(TestUblBis3Common, TestUblCiiBECommon):
         wizard.action_send_and_print()
 
         self._assert_invoice_ubl_file(invoice, 'test_invoice_send_and_print_additional_documents')
+
+    def test_invoice_negative_discount_upsell(self):
+        """ Ensure a negative discount (upsell) is correctly handled as a Charge
+        with the appropriate UNCL 7161 reason code (ADK) instead of an Allowance.
+        """
+        tax_21 = self.percent_tax(21.0)
+        product = self._create_product(lst_price=10.0, taxes_id=tax_21)
+        invoice = self._create_invoice_one_line(
+            product_id=product,
+            quantity=10.0,
+            price_unit=5.76,
+            discount=-1.09,
+            partner_id=self.partner_be,
+            post=True,
+        )
+
+        self._generate_invoice_ubl_file(invoice)
+
+        self._assert_invoice_ubl_file(invoice, 'test_invoice_negative_discount_upsell')
