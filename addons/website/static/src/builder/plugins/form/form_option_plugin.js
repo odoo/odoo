0022bc36700e36d93817bd773691674462afc98d
Rudra Patel <paru@odoo.com>
2025-12-23 10:29:13 +0000

    [FIX] website,*: restrict submit button options in form builder
    *:html_builder, website_mass_mailing
    
    Prevent unsupported actions and options on form submit buttons to avoid
    cloning/deletion issues and UI inconsistencies.
    
    - Remove Duplicate and Remove actions from the sidebar for submit
      buttons (newsletter popup, forms, etc.) instead of disabling them.
    - Hide the animation option for submit buttons, since it is already
      handled in the "Button" section.
    - Remove the Visibility option for submit buttons.
    
    task-4702429
    
    closes odoo/odoo#248918
    
    X-original-commit: cc64d0928072a461c5bd47d67f3f7bdf165aff70
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Rudra Patel (paru) <paru@odoo.com>

--- addons/website/static/src/builder/plugins/form/form_option_plugin.js
+++ addons/website/static/src/builder/plugins/form/form_option_plugin.js
@@ -121,9 +121,6 @@ export class FormOptionPlugin extends Plugin {
             ) {
                 reasons.push(_t("You cannot duplicate this field."));
             }
-            if (el.classList.contains("s_website_form_submit")) {
-                reasons.push(_t("You can't duplicate the submit button of the form."));
-            }
         },
         remove_disabled_reason_providers: ({ el, reasons }) => {
             if (el.classList.contains("s_website_form_model_required")) {
@@ -141,9 +138,6 @@ export class FormOptionPlugin extends Plugin {
                               fieldName,
                           })
                 );
-            }
-            if (el.classList.contains("s_website_form_submit")) {
-                reasons.push(_t("You can't remove the submit button of the form"));
             }
         },
         builder_options: [FormOption, FormFieldOptionRedraw, WebsiteFormSubmitOption],
@@ -206,6 +200,7 @@ export class FormOptionPlugin extends Plugin {
         so_content_addition_selector: [".s_website_form"],
         on_snippet_dropped_handlers: this.onSnippetDropped.bind(this),
         on_cloned_handlers: this.onCloned.bind(this),
+        is_unremovable_selector: ".s_website_form_send, .s_website_form_submit",
     };
     setup() {
         this.modelsCache = new SyncCache(this._fetchModels.bind(this));
