3e5244add96afd8caa0cc4dda6855c3082338ea0
Robin Engels (roen) <roen@odoo.com>
2025-12-23 12:02:19 +0000

    [FIX] point_of_sale: fix runbot error
    In certain cases, the product selected in the tour would be a product
    that require a configuration (having attribute lines), which would lead
    opening a popup that is not handled in the tour, leading to a failure.
    
    When selecting the product we now make sure that we select a product
    that does not require any configuration.
    
    runbot-234734
    
    X-original-commit: 9a781258ae5e5abc27fda34233dac25997f27441

--- addons/point_of_sale/static/tests/pos/tours/synchronization_tour.js
+++ addons/point_of_sale/static/tests/pos/tours/synchronization_tour.js
@@ -18,9 +18,17 @@ registry.category("web_tour.tours").add("test_sync_from_ui_one_by_one", {
                 run: async () => {
                     // Create 5 orders that will be synced one by one
                     for (let i = 0; i < 5; i++) {
+<<<<<<< babd9219dc387fe47ce7d1f37cadb0adeed789b7:addons/point_of_sale/static/tests/pos/tours/synchronization_tour.js
                         const product = posmodel.models["product.template"].find(
                             (p) => p.name === "Desk Pad"
                         );
+||||||| bb82c24295ae582053a2e46a6c5a3cd9358c5e86:addons/point_of_sale/static/tests/tours/synchronization_tour.js
+                        const product = posmodel.models["product.product"].getFirst();
+=======
+                        const product = posmodel.models["product.product"].find(
+                            (el) => el.attribute_line_ids.length == 0
+                        );
+>>>>>>> 1359a89250dbd81530d390a2c0da90621b30240d:addons/point_of_sale/static/tests/tours/synchronization_tour.js
                         const order = posmodel.createNewOrder();
                         await posmodel.addLineToOrder({ product_tmpl_id: product }, order);
                         posmodel.addPendingOrder([order.id]);
