3a036a463aabcb5c98ce6edd4ffbd7a66e88f0b9
prep-odoo <prep@odoo.com>
2025-12-23 12:41:09 +0000

    [FIX] l10n_tw_edi_ecpay: exclude company name in EDI address
    In this commit:
    ---
    Update EDI address formatting to remove the company name
    and send a comma-separated single-line address.
    
    task-5410619
    
    closes odoo/odoo#242230
    
    X-original-commit: 028592a69f3fc310d71085cb73e0fb9f52f0b13f
    Signed-off-by: Nicolas Viseur (vin) <vin@odoo.com>
    Signed-off-by: Prem Patel (prep) <prep@odoo.com>

--- addons/l10n_tw_edi_ecpay/models/res_partner.py
+++ addons/l10n_tw_edi_ecpay/models/res_partner.py
@@ -7,3 +7,7 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     invoice_edi_format = fields.Selection(selection_add=[('tw_ecpay', "ECPay")])
+
+    def _l10n_tw_edi_formatted_address(self):
+        address = self._display_address(without_company=True)
+        return ", ".join(filter(None, map(str.strip, address.splitlines())))
