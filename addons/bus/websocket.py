9d7be0940464c42c0243ac5a60274ba95e8b2f8b
tsm-odoo <tsm@odoo.com>
2025-12-24 13:51:46 +0100

    [PERF] bus: improve cursor usage efficiency
    Websocket greenlets require a cursor to fetch notifications. Under high
    load, a thundering herd effect occurs where many greenlets wake up
    simultaneously to compete for a limited cursor pool.
    
    Websocket throughput is currently bottlenecked by two specific gevent
    scheduling issues that lead to pool error and high latency:
    
    - greenlets that are done fetching notifications are ready to exit their
    context manager and return the cursor to the pool. However, they are not
    always scheduled immediately because the event loop is busy.
    - once a greenlet frees a cursor, it immediately moves to cpu bound tasks
    such as JSON encoding/decoding. This is not efficient, because cursors
    should be taken as soon as available.
    
    To solve this issue, this commit adds two explicit yields:
    - before trying to acquire a cursor to ensure "done" greenlets can
    actually release their cursor.
    - after cursor release to allow waiting greenlets to acquire the cursors
    that were freed.
    
    For 4k websockets dispatching 1 message per second:
    - Cursor acquisition latency: 1-10s before, ~20ms after.
    - Pool errors: half of the websocket getting disconnected, having a hard
      time to reconnect before, lots of pool errors. No pool error, no
    disconnection after.
    
    Part-of: odoo/odoo#241330
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/bus/websocket.py
+++ addons/bus/websocket.py
@@ -13,7 +13,7 @@ import selectors
 import threading
 import time
 from collections import defaultdict, deque
-from contextlib import closing, suppress
+from contextlib import contextmanager, suppress
 from enum import IntEnum
 from psycopg2.pool import PoolError
 from urllib.parse import urlparse
@@ -39,16 +39,26 @@ MAX_TRY_ON_POOL_ERROR = 10
 DELAY_ON_POOL_ERROR = 0.15
 JITTER_ON_POOL_ERROR = 0.3
 
+
+@contextmanager
 def acquire_cursor(db):
     """ Try to acquire a cursor up to `MAX_TRY_ON_POOL_ERROR` """
     delay = DELAY_ON_POOL_ERROR
-    for _ in range(MAX_TRY_ON_POOL_ERROR):
-        with suppress(PoolError):
-            return odoo.registry(db).cursor()
-        time.sleep(delay + random.uniform(0, JITTER_ON_POOL_ERROR))
-        delay *= 1.5
-    raise PoolError('Failed to acquire cursor after %s retries' % MAX_TRY_ON_POOL_ERROR)
-
+    try:
+        for _ in range(MAX_TRY_ON_POOL_ERROR):
+            # Yield before trying to acquire the cursor to let other
+            # greenlets release their cursor.
+            time.sleep(0)
+            with suppress(PoolError), odoo.registry(db).cursor() as cr:
+                yield cr
+                return
+            time.sleep(delay + random.uniform(0, JITTER_ON_POOL_ERROR))
+            delay *= 1.5
+        raise PoolError('Failed to acquire cursor after %s retries' % MAX_TRY_ON_POOL_ERROR)
+    finally:
+        # Yield after releasing the cursor to let waiting greenlets
+        # immediately pick up the freed connection.
+        time.sleep(0)
 
 # ------------------------------------------------------
 # EXCEPTIONS
@@ -655,7 +665,7 @@ class Websocket:
         """
         if not self.__event_callbacks[event_type]:
             return
-        with closing(acquire_cursor(self._db)) as cr:
+        with acquire_cursor(self._db) as cr:
             env = api.Environment(cr, self._session.uid, self._session.context)
             for callback in self.__event_callbacks[event_type]:
                 try:
@@ -825,7 +835,7 @@ class WebsocketRequest:
         ) as exc:
             raise InvalidDatabaseException() from exc
 
-        with closing(acquire_cursor(self.db)) as cr:
+        with acquire_cursor(self.db) as cr:
             self.env = api.Environment(cr, self.session.uid, self.session.context)
             threading.current_thread().uid = self.env.uid
             service_model.retrying(
