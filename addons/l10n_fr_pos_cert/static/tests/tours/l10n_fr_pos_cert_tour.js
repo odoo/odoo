53ae18b9766f19e47d95c6228daccc4d480eca87
Hadi El Yakhni <hael@odoo.com>
2025-12-24 13:32:26 +0000

    [FIX] point_of_sale,l10n_fr_pos_cert: fix displaying old price
    Steps to reproduce
    ------------------
    1. Insall `l10n_fr_pos_cert` and set the company to the french one
    2. Open PoS, and select a product, it will have a price say X
    3. Using the numpad, change the price of that product to Y
    
    Observe that the line no says "Old price unit: Y * Qty / Unit", instead
    of it being "X / Unit".
    
    Why it's happening:
    -------------------
    We were displaying the `currencyDisplayPrice`, which is the total amount
    to be paid, instead.
    
    The fix
    -------
    We add a getter on the product model `displayPriceUnit`, it shows the
    original product unit price.
    
    opw-5365735
    
    closes odoo/odoo#241437
    
    X-original-commit: 4f74620a55fc3fd834365fe19e1dda9e283ce99f
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Hadi El Yakhni (hael) <hael@odoo.com>
    Co-authored-by: Arhtur Nanson <artn@odoo.com>

--- addons/l10n_fr_pos_cert/static/tests/tours/l10n_fr_pos_cert_tour.js
+++ addons/l10n_fr_pos_cert/static/tests/tours/l10n_fr_pos_cert_tour.js
@@ -3,6 +3,7 @@ import * as Dialog from "@point_of_sale/../tests/generic_helpers/dialog_util";
 import * as FeedbackScreen from "@point_of_sale/../tests/pos/tours/utils/feedback_screen_util";
 import * as PaymentScreen from "@point_of_sale/../tests/pos/tours/utils/payment_screen_util";
 import * as ProductScreen from "@point_of_sale/../tests/pos/tours/utils/product_screen_util";
+import * as Numpad from "@point_of_sale/../tests/generic_helpers/numpad_util";
 import { registry } from "@web/core/registry";
 
 registry.category("web_tour.tours").add("l10nFrPosCertSelfInvoicingTour", {
@@ -19,6 +20,36 @@ registry.category("web_tour.tours").add("l10nFrPosCertSelfInvoicingTour", {
                 cssRules: [
                     {
                         css: ".pos-receipt #posqrcode",
+                    },
+                ],
+            }),
+        ].flat(),
+});
+
+registry.category("web_tour.tours").add("test_correct_old_price_upon_price_change_fr", {
+    steps: () =>
+        [
+            Chrome.startPoS(),
+            Dialog.confirm("Open Register"),
+            ProductScreen.clickDisplayedProduct("Desk Pad"),
+            ProductScreen.selectedOrderlineHas("Desk Pad", "1", "1.98"),
+            Numpad.click("Price"),
+            Numpad.isActive("Price"),
+            Numpad.click("5"),
+            ProductScreen.selectedOrderlineHas("Desk Pad", "1", "5.00"),
+            {
+                content: "Old unit price is correctly shown",
+                trigger: ".order-container .orderline.selected:has(.oldPrice:contains(1.98))",
+            },
+            ProductScreen.clickPayButton(),
+            PaymentScreen.clickPaymentMethod("Cash"),
+            PaymentScreen.clickValidate(),
+            FeedbackScreen.isShown(),
+            FeedbackScreen.checkTicketData({
+                orderlines: [
+                    {
+                        name: "Desk Pad",
+                        cssRules: [{ css: ".oldPrice", text: "1.98" }],
                     },
                 ],
             }),
