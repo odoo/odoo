1180631bad6b4c36b93d3ba2e878d2bcc17c0d2d
Mahamadasif Ansari <maan@odoo.com>
2025-12-26 07:26:02 +0000

    [FIX] web: hadle empty data when exporting pivot xlsx
    Currently an exception is generated when controlled `/web/pivot/export_xlsx`
    tries to export xlsx with empty data.
    
    `KeyError: 'title'`
    
    This commit resolves the issue by raising an `UnprocessableEntity` exception
    when empty data is provided. The resulting 422 response indicates that
    the server understood the request and its syntax, but cannot process it
    because the data is invalid.
    
    sentry-6321555617
    
    closes odoo/odoo#242677
    
    X-original-commit: 1f7b0f2ae6e2154004ccda035c0483586f3e40f3
    Signed-off-by: Julien Castiaux (juc) <juc@odoo.com>
    Signed-off-by: ANSARI MAHAMADASIF (maan) <maan@odoo.com>

--- addons/web/controllers/pivot.py
+++ addons/web/controllers/pivot.py
@@ -4,6 +4,7 @@ import json
 from collections import deque
 
 from werkzeug.datastructures import FileStorage
+from werkzeug.exceptions import UnprocessableEntity
 
 from odoo import http, _
 from odoo.http import content_disposition, request
@@ -16,6 +17,8 @@ class TableExporter(http.Controller):
     def export_xlsx(self, data, **kw):
         import xlsxwriter  # noqa: PLC0415
         jdata = json.load(data) if isinstance(data, FileStorage) else json.loads(data)
+        if not jdata:
+            raise UnprocessableEntity(_('No data to export'))
         output = io.BytesIO()
         workbook = xlsxwriter.Workbook(output, {'in_memory': True})
         worksheet = workbook.add_worksheet(jdata['title'])
