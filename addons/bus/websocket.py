4084579632b63c2731efe9ea397f391a4ade4d60
tsm-odoo <tsm@odoo.com>
2025-12-26 09:29:36 +0000

    [FIX] bus: explicit close pollable queue sockets
    Before this commit, `_putsocket/_getsocket` were never explicitly
    closed. This commit ensures those sockets are closed as a part
    of websocket termination.
    
    X-original-commit: 2e11be3e6977d331f5a53ddc3277140b12c91ffc
    Part-of: odoo/odoo#244013
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/bus/websocket.py
+++ addons/bus/websocket.py
@@ -162,6 +162,10 @@ class PollablePriorityQueue(PriorityQueue):
         self._getsocket.recv(1)
         return super().get(*args, **kwargs)
 
+    def close(self):
+        self._putsocket.close()
+        self._getsocket.close()
+
 
 # ------------------------------------------------------
 # WEBSOCKET LIFECYCLE
@@ -409,7 +413,9 @@ class Websocket:
         if self.state is not ConnectionState.OPEN or self._waiting_for_dispatch:
             return
         self._waiting_for_dispatch = True
-        self._send_control_command(ControlCommand.DISPATCH)
+        # Ignore if the socket was closed in the meantime.
+        with suppress(OSError):
+            self._send_control_command(ControlCommand.DISPATCH)
 
     # ------------------------------------------------------
     # PRIVATE METHODS
@@ -635,6 +641,7 @@ class Websocket:
             self.__selector.unregister(self.__socket)
         self.__selector.close()
         self.__socket.close()
+        self.__cmd_queue.close()
         self.state = ConnectionState.CLOSED
         dispatch.unsubscribe(self)
         self._trigger_lifecycle_event(LifecycleEvent.CLOSE)
