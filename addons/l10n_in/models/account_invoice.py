35642c44097c038a3a557d2f7f127eac516f54a9
Jay Savaliya <jasa@odoo.com>
2025-12-27 16:16:54 +0000

    [IMP] l10n_in: invoice label as per Section 31 of the CGST Act
    Previously, the invoice label was always displayed as “Tax Invoice” or “Invoice”
    However, as per Section 31 of the CGST Act, the document label must
    depend on the nature of supplies and the customer type:
     - If the document contains only taxable supplies => “Tax Invoice”
     - If the document contains only exempt supplies => “Bill of Supply”
     - If the customer is unregistered and the document contains both taxable and
       exempt supplies => “Invoice-cum-Bill of Supply”
     - If the customer is registered and the document contains both taxable and
       exempt supplies, separate documents must be issued as per the law.
    
    With this commit, the invoice label is generated strictly in accordance with
    Section 31 of the CGST Act.
    In the case of a registered customer where both taxable and exempt supplies
    exist in a single document, the label will be printed as “Invoice”.
    
    task-5468323
    
    X-original-commit: 52b2434e37eef81630e9adff8a5a69d3a0c0a7f9

--- addons/l10n_in/models/account_invoice.py
+++ addons/l10n_in/models/account_invoice.py
@@ -693,3 +693,22 @@ class AccountMove(models.Model):
         tax_tags_dict = self.env['account.move.line']._get_l10n_in_tax_tag_ids()
         # we set the section on the invoice lines
         moves.line_ids._set_l10n_in_gstr_section(tax_tags_dict)
+
+    def _get_l10n_in_invoice_label(self):
+        self.ensure_one()
+        exempt_types = {'exempt', 'nil_rated', 'non_gst'}
+        if self.country_code != 'IN' or not self.is_sale_document(include_receipts=False):
+            return
+        gst_treatment = self.l10n_in_gst_treatment
+        company = self.company_id
+        tax_types = set(self.invoice_line_ids.tax_ids.mapped('l10n_in_tax_type'))
+        if company.l10n_in_is_gst_registered and tax_types:
+            if gst_treatment in ['overseas', 'special_economic_zone']:
+                return 'Tax Invoice'
+            elif tax_types.issubset(exempt_types):
+                return 'Bill of Supply'
+            elif tax_types.isdisjoint(exempt_types):
+                return 'Tax Invoice'
+            elif gst_treatment in ['unregistered', 'consumer']:
+                return 'Invoice-cum-Bill of Supply'
+        return 'Invoice'
