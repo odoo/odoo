abf4b61f6250ef7dcab1054e4976b3c01453bfd2
hatr-odoo <hatr@odoo.com>
2025-12-29 17:55:32 +0530

    [ADD] l10n_uz: basic accounting package for uzbekistan
    This commit introduces basic accounting package including Demo Data,
    Chart of Accounts, Account Groups, Taxes and Tax Groups for Uzbekistan.
    
    task-3927927
    
    Part-of: odoo/odoo#241811
    Related: odoo/enterprise#103136
    Signed-off-by: Maximilien La Barre (malb) <malb@odoo.com>

---
+++ addons/l10n_uz/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': 'Uzbekistan - Accounting',
+    'icon': '/account/static/description/l10n.png',
+    'countries': ['uz'],
+    'description': """
+Uzbekistan Accounting: Chart of Account.
+========================================
+
+Uzbekistan accounting chart and localization.
+  """,
+    'category': 'Accounting/Localizations/Account Charts',
+    'depends': [
+        'account',
+        'base_vat',
+    ],
+    'demo': [
+        'demo/demo_company.xml'
+    ],
+    'data': [
+        'data/account.account.tag.csv',
+        'views/res_company_views.xml',
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
