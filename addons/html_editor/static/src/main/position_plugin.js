d00323addf04ee34d65456e3a02544798def3ff1
Deependra Solanki <deso@odoo.com>
2025-12-30 13:05:21 +0000

    [FIX] html_editor: update powerbuttons position on toggling block
    Before this commit:
    
    When a toggle block was toggled, the selection stayed on the same block, but
    its on-screen position changed because the block above it was toggled.
    
    After this commit:
    
    Dispatch a layout geometry change when the MutationObserver detects classList
    or attribute changes that may affect layout, ensuring the powerbuttons
    position is updated accordingly.
    
    task-5240942
    
    closes odoo/odoo#248104
    
    X-original-commit: 1ed211846b8ecc32f637bed1c5670efb939f94b9
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Deependra Solanki (deso) <deso@odoo.com>

--- addons/html_editor/static/src/main/position_plugin.js
+++ addons/html_editor/static/src/main/position_plugin.js
@@ -1,6 +1,6 @@
 import { ancestors } from "@html_editor/utils/dom_traversal";
 import { Plugin } from "../plugin";
-import { throttleForAnimation } from "@web/core/utils/timing";
+import { debounce, throttleForAnimation } from "@web/core/utils/timing";
 import { couldBeScrollableX, couldBeScrollableY } from "@web/core/utils/scrolling";
 
 /**
@@ -18,10 +18,15 @@ export class PositionPlugin extends Plugin {
         external_history_step_handlers: this.layoutGeometryChange.bind(this),
         history_reset_from_steps_handlers: this.layoutGeometryChange.bind(this),
         step_added_handlers: this.layoutGeometryChange.bind(this),
+        before_filter_mutation_record_handlers: this.handlePotentialLayoutGeometryChange.bind(this),
     };
 
     setup() {
         this.layoutGeometryChange = throttleForAnimation(this.layoutGeometryChange.bind(this));
+        this.debouncedLayoutGeometryChange = debounce(
+            this.layoutGeometryChange.bind(this),
+            "animationFrame"
+        );
         this.resizeObserver = new ResizeObserver(this.layoutGeometryChange);
         this.resizeObserver.observe(this.document.body);
         this.resizeObserver.observe(this.editable);
@@ -40,6 +45,17 @@ export class PositionPlugin extends Plugin {
         }
     }
 
+    handlePotentialLayoutGeometryChange(records) {
+        for (const record of records) {
+            if (
+                record.type === "classList" ||
+                (record.type === "attributes" && record.attributeName === "style")
+            ) {
+                this.debouncedLayoutGeometryChange();
+                return;
+            }
+        }
+    }
     destroy() {
         this.resizeObserver.disconnect();
         super.destroy();
