ead863d56bbe34d8559a2237c0d7fd36d890cb10
Louis Travaux <lotr@odoo.com>
2025-12-30 12:30:41 +0100

    [IMP] iot_drivers: move status page pairing code to left
    For IoT Boxes with many devices connected, the pairing code is pushed
    to the top of the status page, making it invisible/unreadable.
    We moved it to the left so that it's never pushed to the top.
    
    In addition, we add a check on the operating system to avoid loading
    drivers/interfaces on "Test" system, to avoid errors when running the
    IoT Service locally.
    
    closes odoo/odoo#241622
    
    Signed-off-by: Max Whale (mawh) <mawh@odoo.com>

--- addons/iot_drivers/iot_handlers/__init__.py
+++ addons/iot_drivers/iot_handlers/__init__.py
@@ -1,20 +1,20 @@
 import logging
-import platform
 import importlib
 from pathlib import Path
 from itertools import chain
+from ..tools.system import IS_WINDOWS, IS_TEST
 
 _logger = logging.getLogger(__name__)
 
 # Only import files that correspond to the current platform (Linux or Windows)
-exclude_suffix = '_L' if platform.system() == 'Windows' else '_W'
+exclude_suffix = '_L' if IS_WINDOWS else '_W'
 
 drivers_path = Path(__file__).parent / 'drivers'
 interfaces_path = Path(__file__).parent / 'interfaces'
 
 for file_path in chain(drivers_path.glob('*.py'), interfaces_path.glob('*.py')):
     module = file_path.stem
-    if any(module.endswith(suffix) for suffix in ['__', exclude_suffix]):
+    if IS_TEST or any(module.endswith(suffix) for suffix in ['__', exclude_suffix]):
         continue
 
     try:
