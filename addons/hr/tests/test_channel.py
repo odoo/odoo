e9b4cb2dfd5c2b50065b1d2562198bbb49e76824
shmn-odoo <shmn@odoo.com>
2025-12-30 09:13:56 +0000

    [FIX] hr: ensure updating department correctly auto-subscribes to channels
    Steps to reproduce:
    navigate to 'Discuss' > 'Channels'
    create a new channel and set 'Auto Subscribe Departments' to 'Administration'
    create a new user and a corresponding employee record
    go to 'Employees' > locate the employee
    set the employee's department to 'Administration'
    
    Before this commit:
    The user is not automatically added to the channel. This occurs because the
    auto-subscription logic is triggered before the  department change is committed
    to the database.
    
    After this commit:
    The user is correctly auto-subscribed to the channel once the department update
    is saved.
    
    task-5448649
    
    closes odoo/odoo#261110
    
    X-original-commit: 5af0b9c60202dd97245ca8fb4b1d506c14dd6926
    Signed-off-by: Romain Carlier (romc) <romc@odoo.com>
    Signed-off-by: Mann Alpeshbhai Shah (shmn) <shmn@odoo.com>

--- addons/hr/tests/test_channel.py
+++ addons/hr/tests/test_channel.py
@@ -13,19 +13,32 @@ class TestChannel(TestHrCommon):
 
         cls.channel = cls.env['discuss.channel'].create({'name': 'Test'})
 
-        emp0 = cls.env['hr.employee'].create({
+        cls.emp0 = cls.env['hr.employee'].create({
             'user_id': cls.res_users_hr_officer.id,
         })
         cls.department = cls.env['hr.department'].create({
             'name': 'Test Department',
-            'member_ids': [(4, emp0.id)],
         })
 
     def test_auto_subscribe_department(self):
         self.assertEqual(self.channel.channel_partner_ids, self.env['res.partner'])
+        self.emp0.write({'department_id': self.department.id})
+        self.department.invalidate_recordset(['member_ids'])
+        self.assertEqual(self.department.member_ids, self.emp0)
 
         self.channel.write({
             'subscription_department_ids': [(4, self.department.id)]
         })
 
-        self.assertEqual(self.channel.channel_partner_ids, self.department.mapped('member_ids.user_id.partner_id'))
+        self.assertEqual(self.channel.channel_partner_ids, self.emp0.user_id.partner_id)
+
+    def test_auto_subscribe_when_updating_employee_department(self):
+        self.channel.write({
+            'subscription_department_ids': [(4, self.department.id)],
+        })
+        self.assertEqual(self.channel.channel_partner_ids, self.env['res.partner'])
+        self.department.invalidate_recordset(['member_ids'])
+
+        self.emp0.write({'department_id': self.department.id})
+
+        self.assertEqual(self.channel.channel_partner_ids, self.emp0.user_id.partner_id)
