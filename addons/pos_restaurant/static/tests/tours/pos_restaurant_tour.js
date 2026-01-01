0595785808c646cee112b114a74c450be89a80ed
dhruv <djip@odoo.com>
2026-01-01 12:48:36 +0000

    [FIX] pos_restaurant: correct course timer in preparation display
    before this commit:
    - In the preparation display (PDIS), the first course shows a preparation
      time of 0 untill the second course is fired.
    - After course 2 fired, course 1 and course 2 have same preparation time value.
    
    after this commit:
    - Each course shows its correct preparation time when fired.
    
    task-5421616
    
    closes odoo/odoo#243397
    
    X-original-commit: c229111b82dfa871da1939377f3c0a684dd13a9a
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Dhruv Jitendrakumar Patel (djip) <djip@odoo.com>

--- addons/pos_restaurant/static/tests/tours/pos_restaurant_tour.js
+++ addons/pos_restaurant/static/tests/tours/pos_restaurant_tour.js
@@ -241,6 +241,11 @@ registry.category("web_tour.tours").add("test_pos_restaurant_course", {
             // Check empty course gets remove after fire course.
             ProductScreen.clickCourseButton(),
             ProductScreen.selectCourseLine("Course 2"),
+            {
+                content: "Wait atleast 1 sec so that courses have different fired_date timestamps",
+                trigger: "body",
+                run: async () => await delay(1000),
+            },
             ProductScreen.fireCourseButton(),
             Chrome.closePrintingWarning(),
             FloorScreen.clickTable("5"),
