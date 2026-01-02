1cae8223b881f4afb78f057404478f21f28c0385
Harsh Shah <hash@odoo.com>
2026-01-02 09:06:34 +0000

    [FIX] l10n_in_edi_ewaybill: include rc in total invoice value for export
    For export invoices, the total invoice value in the eWaybill JSON did not
    include reverse charge amounts for GST, leading to a mismatch with the value
    shown in Odoo and the eWaybill generated by the Indian government system.
    
    This commit adjusts the JSON computation to include reverse charge amounts
    in the total invoice value for exports, aligning it with the government
    generated eWaybill, while preserving the existing reverse charge flow.
    
    task-5068199
    
    X-original-commit: a2fb5efec6ac426416f8bfd31d956108704230fa

--- addons/l10n_in_ewaybill/models/l10n_in_ewaybill.py
+++ addons/l10n_in_ewaybill/models/l10n_in_ewaybill.py
@@ -672,6 +672,15 @@ class L10nInEwaybill(models.Model):
         invoice_line_tax_details = tax_details.get("tax_details_per_record")
         sign = self.account_move_id.is_inbound() and -1 or 1
         rounding_amount = sum(line.balance for line in self.account_move_id.line_ids if line.display_type == 'rounding') * sign
+        total_invoice_value = tax_details.get("base_amount", 0.00) + tax_details.get("tax_amount", 0.00) + rounding_amount
+        if self.account_move_id.l10n_in_gst_treatment == 'overseas' and self.partner_ship_to_id.country_id.code != 'IN':
+            # For exports without LUT, the e-waybill total invoice value must include Reverse Charges.
+            # Reverse charge amounts are stored as a negative value,
+            # so we subtract it here to effectively add it to the total. (i.e. -(-x) = +x).
+            adjusting_rc_amount = sum(
+                tax_details_by_code.get(code, 0.00) for code in ("cgst_rc_amount", "sgst_rc_amount", "igst_rc_amount")
+            )
+            total_invoice_value -= adjusting_rc_amount
         return {
             "itemList": list(starmap(self._get_l10n_in_ewaybill_line_details, invoice_line_tax_details.items())),
             "totalValue": round_value(tax_details.get("base_amount", 0.00)),
@@ -681,7 +690,7 @@ class L10nInEwaybill(models.Model):
             },
             "cessNonAdvolValue": round_value(tax_details_by_code.get("cess_non_advol_amount", 0.00)),
             "otherValue": round_value(tax_details_by_code.get("other_amount", 0.00) + rounding_amount),
-            "totInvValue": round_value(tax_details.get("base_amount", 0.00) + tax_details.get("tax_amount", 0.00) + rounding_amount),
+            "totInvValue": round_value(total_invoice_value),
         }
 
     def _ewaybill_generate_direct_json(self):
