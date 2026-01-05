728e4ac2c03be68bb42dc7533aa7ef44fe54e4df
Mahdi Alijani (malj) <malj@odoo.com>
2026-01-05 13:11:57 +0000

    [FIX] purchase, sale: show sale warning message from partner's company
    Issue:
    Due to this issue, the sale warning message is only shown when
    the warning message is set on partner itself, not partner's company.
    
    Steps to reproduce (with demo data):
    1- Enable `Sale warnings` from setting.
    2- On `Contacts` app, open `Azure Interior`, and add a sale warning
       from `Notes` tab.
    3- Create a SO with `Brandon Freeman` from `Azure Interior` as
       the customer.
    4- No sale warning is shown.
    
    Cause:
    The IMP #192211 replaces warning popup with a message. However, it
    doesn't check for the warning from `partner_id.parent_id`, which was
    the case before that PR.
    
    This is the case with purchase as well.
    
    opw-5404983
    
    closes odoo/odoo#244305
    
    X-original-commit: e2c6e30cf314387ea691ed29f9e6eeb6826d5445
    Signed-off-by: Mohammadmahdi Alijani (malj) <malj@odoo.com>

--- addons/purchase/models/purchase_order.py
+++ addons/purchase/models/purchase_order.py
@@ -327,6 +327,9 @@ class PurchaseOrder(models.Model):
             warnings = OrderedSet()
             if partner_msg := order.partner_id.purchase_warn_msg:
                 warnings.add((order.partner_id.name or order.partner_id.display_name) + ' - ' + partner_msg)
+            if partner_parent_msg := order.partner_id.parent_id.purchase_warn_msg:
+                parent = order.partner_id.parent_id
+                warnings.add((parent.name or parent.display_name) + ' - ' + partner_parent_msg)
             for line in order.order_line:
                 if product_msg := line.purchase_line_warn_msg:
                     warnings.add(line.product_id.display_name + ' - ' + product_msg)
