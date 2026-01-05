4a65f3bf8a3e79675e76c7706d33a967ca0a8b07
Martin Doyen <mdoy@odoo.com>
2026-01-05 12:30:25 +0000

    [FIX] account_edi_ubl_cii: prioritize partner legal entity name
    When we create an account.move from an XML file, Odoo
    first searches for an existing partner via some values fetched
    with `_import_retrieve_partner_vals()`.
    
    Amongst those vals, the name can be fetched from either the `Contact:Name`
    or the `PartyLegalEntity:RegistrationName`.
    
    Currently, the contact name is prioritized over the legal entity name.
    After discussion with CHKL, this should be the opposite.
    
    Related ticket: opw-5269360
    
    closes odoo/odoo#242205
    
    X-original-commit: 3d2b6dfa1c222ba4ffc9654dbab3fb2140a68223
    Signed-off-by: de Wouters de Bouchout Jean-Benoît (jbw) <jbw@odoo.com>
    Signed-off-by: Martin Doyen (mdoy) <mdoy@odoo.com>

--- addons/account_edi_ubl_cii/models/account_edi_xml_ubl_20.py
+++ addons/account_edi_ubl_cii/models/account_edi_xml_ubl_20.py
@@ -811,8 +811,8 @@ class AccountEdiXmlUBL20(models.AbstractModel):
             'vat': self._find_value(f'.//cac:{role}Party/cac:Party//cbc:CompanyID[string-length(text()) > 5]', tree),
             'phone': self._find_value(f'.//cac:{role}Party/cac:Party//cbc:Telephone', tree),
             'email': self._find_value(f'.//cac:{role}Party/cac:Party//cbc:ElectronicMail', tree),
-            'name': self._find_value(f'.//cac:{role}Party/cac:Party//cbc:Name', tree) or
-                    self._find_value(f'.//cac:{role}Party/cac:Party//cbc:RegistrationName', tree),
+            'name': self._find_value(f'.//cac:{role}Party/cac:Party//cbc:RegistrationName', tree) or
+                    self._find_value(f'.//cac:{role}Party/cac:Party//cbc:Name', tree),
             'country_code': self._find_value(f'.//cac:{role}Party/cac:Party//cac:Country//cbc:IdentificationCode', tree),
             'street': self._find_value(f'.//cac:{role}Party/cac:Party//cbc:StreetName', tree),
             'street2': self._find_value(f'.//cac:{role}Party/cac:Party//cbc:AdditionalStreetName', tree),
