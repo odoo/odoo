cae2ceaef55f729d2aec770d758ef146e8ec8f36
nape-odoo <nape@odoo.com>
2026-01-05 10:29:32 +0000

    [FIX] l10n_hr_edi: Flow test and SQL queyr fixes
    - Fixing runbot errors raised by the flow tests, consistent with 19.0+ version
    - Fixing neutralize.sql
    runbot-237635
    runbot-237637
    
    X-original-commit: c116212fcafdca4d1d268a31c11c9f28a41ddca9

---
+++ addons/l10n_hr_edi/data/neutralize.sql
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+||||||| MERGE BASE
+=======
+-- reset MojEracun status
+UPDATE res_company
+SET l10n_hr_mer_username = NULL,
+    l10n_hr_mer_password = NULL,
+    l10n_hr_mer_company_ident = NULL,
+    l10n_hr_mer_software_ident = NULL,
+    l10n_hr_mer_connection_state = 'inactive',
+    l10n_hr_mer_connection_mode = 'test';
+
+>>>>>>> FORWARD PORTED
