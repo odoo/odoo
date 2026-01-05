29a04721c8dfbb6b484fea25078486f8a7d1676b
dhruv <djip@odoo.com>
2026-01-05 12:36:02 +0000

    [FIX] point_of_sale: tb with preset with identification and one-click payment
    steps:
    - Configure a default preset as Delivery or Takeout and enable One-click Payment
    - Open the POS restaurant.
    - Open any table and add a product.
    - Click the One-click payment method.
    
    issue:
    - Unnecessary dialogs appear on the ReceiptScreen (e.g., customer selection or
      time-slot selection), followed by traceback.
    
    reason:
    - The order is validated on the ProductScreen during One-click Payment and
      ProductScreen creates new draft order.
    
    fix:
    - After this commit, new order will not be created on the ProductScreen with
      One-click Payment..
    - If validation fails (e.g., an RPC error), it will redirected back
      to the previous screen.
    - In case of Delivery preset, on discard do not remove Delivery preset.
    
    task-5391389
    
    closes odoo/odoo#242185
    
    X-original-commit: fe7c2747af249d8b355474018f592fa29ffcabbe
    Signed-off-by: Stéphane Vanmeerhaeghe (stva) <stva@odoo.com>
    Signed-off-by: Dhruv Jitendrakumar Patel (djip) <djip@odoo.com>

--- addons/point_of_sale/static/src/app/services/pos_store.js
+++ addons/point_of_sale/static/src/app/services/pos_store.js
@@ -2263,6 +2263,8 @@ export class PosStore extends WithLazyGetterTrap {
         }
 
         if (preset) {
+            order.setPreset(preset);
+
             if (preset.needsPartner) {
                 const partner = order.partner_id || (await this.selectPartner(order));
                 if (!partner) {
@@ -2276,7 +2278,7 @@ export class PosStore extends WithLazyGetterTrap {
                     }
                 }
             }
-            order.setPreset(preset);
+
             if (preset.identification === "name") {
                 await this.handleSelectNamePreset(order);
             }
