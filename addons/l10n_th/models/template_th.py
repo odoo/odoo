442f25592995bece8d1de52a031e4941907cf07c
Arnav Varshney <avar@odoo.com>
2026-01-06 18:29:07 +0800

    [IMP] l10n_th: update chart of accounts and taxes
    Expanded and updated the Thai localization with an enhanced chart of accounts, updated tax templates, asset templates, and tax group configuration for better compliance and usability.
    
    Task-5395076
    
    closes odoo/odoo#240668
    
    Signed-off-by: Arnav Varshney <avar@odoo.com>
    Signed-off-by: Nicolas Viseur (vin) <vin@odoo.com>

--- addons/l10n_th/models/template_th.py
+++ addons/l10n_th/models/template_th.py
@@ -9,8 +9,11 @@ class AccountChartTemplate(models.AbstractModel):
     @template('th')
     def _get_th_template_data(self):
         return {
-            'property_account_receivable_id': 'a_recv',
-            'property_account_payable_id': 'a_pay',
+            'code_digits': '6',
+            'property_account_receivable_id': 'l10n_th_account_112100',
+            'property_account_payable_id': 'l10n_th_account_212100',
+            'property_stock_valuation_account_id': 'l10n_th_account_113100',
+            'downpayment_account_id': 'l10n_th_account_212400',
         }
 
     @template('th', 'res.company')
@@ -18,26 +21,23 @@ class AccountChartTemplate(models.AbstractModel):
         return {
             self.env.company.id: {
                 'account_fiscal_country_id': 'base.th',
-                'bank_account_code_prefix': '1110',
-                'cash_account_code_prefix': '1100',
-                'transfer_account_code_prefix': '16',
-                'account_default_pos_receivable_account_id': 'a_recv_pos',
-                'income_currency_exchange_account_id': 'a_income_gain',
-                'expense_currency_exchange_account_id': 'a_exp_loss',
+                'bank_account_code_prefix': '11120',
+                'cash_account_code_prefix': '11110',
+                'transfer_account_code_prefix': 'l10n_th_account_11120',
+                'account_default_pos_receivable_account_id': 'l10n_th_account_112101',
+                'income_currency_exchange_account_id': 'l10n_th_account_421300',
+                'expense_currency_exchange_account_id': 'l10n_th_account_621200',
+                'account_journal_suspense_account_id': 'l10n_th_account_111201',
+                'account_journal_early_pay_discount_loss_account_id': 'l10n_th_account_411400',
+                'account_journal_early_pay_discount_gain_account_id': 'l10n_th_account_421500',
                 'account_sale_tax_id': 'tax_output_vat',
                 'account_purchase_tax_id': 'tax_input_vat',
-                'expense_account_id': 'a_exp_cogs',
-                'income_account_id': 'a_sales',
-                'account_stock_journal_id': 'inventory_valuation',
-                'account_stock_valuation_id': 'a_invent',
-            },
-        }
-
-    @template('th', 'account.account')
-    def _get_th_account_account(self):
-        return {
-            'a_invent': {
-                'account_stock_expense_id': 'a_exp_cogs',
-                'account_stock_variation_id': 'a_stock_variation',
+                'default_cash_difference_income_account_id': 'l10n_th_account_421600',
+                'default_cash_difference_expense_account_id': 'l10n_th_account_622200',
+                'transfer_account_id': 'l10n_th_account_111202',
+                'expense_account_id': 'l10n_th_account_511100',
+                'income_account_id': 'l10n_th_account_411100',
+                'account_stock_valuation_id': 'l10n_th_account_113100',
+                'tax_exigibility': 'True'
             },
         }
