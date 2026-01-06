12758c2fbbfe705151a5d0782959768feaed35ca
Krzysztof Magusiak (krma) <krma@odoo.com>
2026-01-06 11:32:12 +0000

    [FIX] core: flush and precommit interdependency
    In the existing implementation sometimes precommit hooks need to flush
    manually because they are ran after the flush is executed in the cursor.
    A precommit that triggers ORM changes may be not flushed unless it is
    done explicitly, however an explicit call may turn into an infinite
    loop.
    
    This removes the need to flush in precommit hooks and let the ORM handle
    it safely. Flushing of the environment was already done in #220379.

--- odoo/tools/misc.py
+++ odoo/tools/misc.py
@@ -1177,6 +1177,9 @@ class Callbacks:
         self._funcs.clear()
         self.data.clear()
 
+    def __len__(self) -> int:
+        return len(self._funcs)
+
 
 class ReversedIterable(Reversible[T], typing.Generic[T]):
     """ An iterable implementing the reversal of another iterable. """
