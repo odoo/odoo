2718a48efd063debd1b3208676d323f14fa54ca8
jorv-odoo <jorv@odoo.com>
2026-01-06 13:55:39 +0000

    [FIX] mail: handle wildcard content-type
    In some rare cases it would seem that some systems construct emails with
    attachments reporting '*/*' as the Content-Type.
    
    While trying to parse such content in Odoo, it causes issues with the standard
    CPython email library, as no standard handler exists for '*/*' content-types:
    
    Example error:
    ```
    File "/usr/lib/python3.13/email/contentmanager.py", line 25, in get_content
    raise KeyError(content_type)
    KeyError: '*/*'
    ```
    
    This is not compliant with valid MIME types defined in RFC2046/section-3,
    but in real life scenarios, blocking the processing of an incoming email in Odoo
    because of this might be excessive.
    
    While not a perfect solution, we will assume that attachments falsly reported
    as '*/*' are to be processed as 'application/octet-stream' content types.
    This should cover most use-cases, and if it still fails, we will consider that
    it's up to the original email sender to be RFC compliant.
    
    OPW-5425093
    
    closes odoo/odoo#243667
    
    X-original-commit: 8f92694b1f1152383adaa41aa1c07fe8c06eaf6f
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>
    Signed-off-by: Joel Rodrigues Vitória (jorv) <jorv@odoo.com>

--- addons/mail/models/mail_thread.py
+++ addons/mail/models/mail_thread.py
@@ -1578,6 +1578,9 @@ class MailThread(models.AbstractModel):
                 if part.get_content_type() == 'binary/octet-stream':
                     _logger.warning("Message containing an unexpected Content-Type 'binary/octet-stream', assuming 'application/octet-stream'")
                     part.replace_header('Content-Type', 'application/octet-stream')
+                if part.get_content_type() == '*/*':
+                    _logger.warning("Message containing an unexpected Content-Type '*/*', assuming 'application/octet-stream'")
+                    part.replace_header('Content-Type', 'application/octet-stream')
                 if part.get_content_type() == 'multipart/alternative':
                     alternative = True
                 if part.get_content_type() == 'multipart/mixed':
