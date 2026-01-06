53fe663443fcf73165fc96b3625b322cf9f71fda
Robin Lejeune (role) <role@odoo.com>
2026-01-06 10:38:43 +0100

    [FIX] website: preload available carousel images
    As images are lazy loaded, it means that in the context of a carousel or
    an image gallery, they only start loading once the user clicks either on
    its indicator or on the previous / next button (or after completing an
    auto-slide). While Chrome seems to optimize that to make it seemless, on
    Firefox this causes the carousel slide to appear blank for a moment
    before the image suddenly pops up, as the sliding animation arrives to
    its end.
    In effect, this causes a flicker and a feeling that the carousels, and
    especially the gallery, is extremely laggy.
    
    To mitigate that while trying to keep the advantages of image lazy
    loading, this commit partially backports [08d837e], which loads the
    images of the next and the previous carousel items.
    
    Additionally, we prefetch the target images on pointerdown / keydown on
    an indicator. That may seem like too small of a difference to be
    interesting, but it actually gives a little bit of time between the
    pointerdown and pointerup (which triggers the slide event) to start
    loading the images, which with a correct connexion already goes a long
    way towards mitigating the laggy feeling.
    
    [08d837e]: https://github.com/odoo/odoo/commit/08d837e70f28a84a9bd97974f5d15d387a42b7c0
    
    task-5245513

--- addons/website/static/src/interactions/carousel/carousel_slider.js
+++ addons/website/static/src/interactions/carousel/carousel_slider.js
@@ -1,9 +1,13 @@
 import { Interaction } from "@web/public/interaction";
+import { getActiveHotkey } from "@web/core/hotkeys/hotkey_service";
 import { registry } from "@web/core/registry";
 
 export class CarouselSlider extends Interaction {
     static selector = ".carousel";
     dynamicContent = {
+        _root: {
+            "t-on-slid.bs.carousel": this.onSlidCarousel,
+        },
         "img": {
             "t-on-load": this.computeMaxHeight,
         },
@@ -15,11 +19,26 @@ export class CarouselSlider extends Interaction {
                 "min-height": this.maxHeight ? `${this.maxHeight}px` : "",
             }),
         },
+        ".carousel-indicators button, .carousel-indicators li": {
+            "t-on-pointerdown": (ev) => {
+                const toLoadEl = this.carouselItemEls.at(ev.currentTarget.dataset.bsSlideTo);
+                this.prefetchImages([toLoadEl]);
+            },
+            "t-on-keydown": (ev) => {
+                const hotkey = getActiveHotkey(ev);
+                if (["space", "enter"].includes(hotkey)) {
+                    const toLoadEl = this.carouselItemEls.at(ev.currentTarget.dataset.bsSlideTo);
+                    this.prefetchImages([toLoadEl]);
+                }
+            },
+        },
     };
     carouselOptions = undefined;
 
     setup() {
         this.maxHeight = undefined;
+        this.carouselInnerEl = this.el.querySelector(".carousel-inner");
+        this.carouselItemEls = [...this.carouselInnerEl.querySelectorAll(".carousel-item")];
     }
 
     start() {
@@ -27,6 +46,18 @@ export class CarouselSlider extends Interaction {
         this.updateContent();
         const carouselBS = window.Carousel.getOrCreateInstance(this.el, this.carouselOptions);
         this.registerCleanup(() => carouselBS.dispose());
+
+        // Preload first items only when carousel is on screen
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    this.loadItemsToAppear();
+                    observer.unobserve(this.el);
+                }
+            });
+        });
+        observer.observe(this.el);
+        this.registerCleanup(() => observer.unobserve(this.el));
     }
 
     computeMaxHeight() {
@@ -42,6 +73,74 @@ export class CarouselSlider extends Interaction {
                 this.maxHeight = height;
             }
             itemEl.classList.toggle("active", isActive);
+        }
+    }
+
+    /**
+     * Handles the 'slid' event of the carousel. Called *after* a slide
+     * transition.
+     *
+     * @param {Event} ev The Bootstrap Carousel slid event.
+     */
+    onSlidCarousel(ev) {
+        this.loadItemsToAppear(); // Preload future items after a slide
+    }
+
+    /**
+     * Loads images of the carousel-item necessary for both 'prev' and 'next'
+     * animations. Loads images for items that are about to become visible.
+     *
+     * @param {number} [nbItemsToLoad=1] The number of items to preload on each
+     * side.
+     */
+    loadItemsToAppear(nbItemsToLoad = 1) {
+        const index = this.carouselItemEls.findIndex((el) => el.classList.contains("active"));
+        const activeItemIndex = index >= 0 ? index : 0;
+
+        // Load "Next" items: nbItemsToLoad items after the active element
+        const nextEndIndex = Math.min(
+            activeItemIndex + nbItemsToLoad + 1,
+            this.carouselItemEls.length
+        );
+        const nextItemElsToLoad = this.carouselItemEls.slice(activeItemIndex, nextEndIndex);
+
+        // load "Prev" items : nbItemsToLoad items before the active element
+        // (circular wrapping)
+        // if currentIndex is 0, then the nbItemsToLoad items are the last
+        // elements of the carousel
+        let prevItemElsToLoad = [];
+        if (activeItemIndex - nbItemsToLoad < 0) {
+            const wrapAmount = Math.abs(activeItemIndex - nbItemsToLoad);
+            prevItemElsToLoad = this.carouselItemEls
+                .slice(this.carouselItemEls.length - wrapAmount, this.carouselItemEls.length)
+                .concat(this.carouselItemEls.slice(0, activeItemIndex))
+                .reverse();
+        } else {
+            prevItemElsToLoad = this.carouselItemEls
+                .slice(activeItemIndex - nbItemsToLoad, activeItemIndex)
+                .reverse();
+        }
+
+        this.prefetchImages(nextItemElsToLoad.concat(prevItemElsToLoad));
+    }
+    /**
+     * Replaces loading from `lazy` to `eager` for all images of the given
+     * carousel slides. This forces the browser to load the images immediately.
+     * The goal is to avoid the flicker (mainly on Firefox) when the carousel
+     * slides.
+     *
+     * @param {HTMLElement[]} toLoadEls
+     */
+    prefetchImages(toLoadEls) {
+        for (const carouselItemEl of toLoadEls) {
+            const imageEls = carouselItemEl.querySelectorAll("img[loading='lazy']");
+            for (const imageEl of imageEls) {
+                // Note that we remove the attribute with the goal of forcing it
+                // to the "eager" value. Removing the attribute is better so
+                // that the attribute is not saved as eager in edit mode (the
+                // lazy value is auto added on page rendering).
+                imageEl.removeAttribute("loading");
+            }
         }
     }
 }
