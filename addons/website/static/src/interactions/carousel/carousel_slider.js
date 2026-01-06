2bae7e733cdf78c8d711b296b40c6cf81f7bed16
Robin Lejeune (role) <role@odoo.com>
2026-01-06 12:34:24 +0100

    [FIX] website: improve carousel accessibility
    To improve keyboard accessibility on carousels, the indicators container
    is not focusable anymore, and the indicators themselves now only have
    one focusable button at a time.
    The tab order is thus: previous button > active indicator > next button.
    (The previous and next button may both appear before.)
    You can still navigate among indicators with the left and right arrows,
    which also moves the focus to the newly targetted indicator.
    
    Note that other accessibility improvements remain to be done on the
    carousels (add a pause/play button on auto-sliding carousels, place the
    buttons before the carousel slide in the tab order, add some aria
    attributes (roledescription, live), adaptative labels...).
    
    task-5470023
    
    X-original-commit: https://github.com/odoo/odoo/commit/ce1cbc1a239dc7b4150cfbf14e8a6ef3b3c8a392

--- addons/website/static/src/interactions/carousel/carousel_slider.js
+++ addons/website/static/src/interactions/carousel/carousel_slider.js
@@ -22,6 +22,8 @@ export class CarouselSlider extends Interaction {
             }),
         },
         ".slide-link": { "t-att-class": () => ({ "d-none": !this.showClickableSlideLinks }) },
+        // Stable-friendly fix.
+        ".carousel-indicators": { "t-att-tabindex": () => "-1" },
         ".carousel-indicators button, .carousel-indicators li": {
             "t-on-pointerdown": (ev) => {
                 const toLoadEl = this.carouselItemEls.at(ev.currentTarget.dataset.bsSlideTo);
@@ -34,6 +36,7 @@ export class CarouselSlider extends Interaction {
                     this.prefetchImages([toLoadEl]);
                 }
             },
+            "t-att-tabindex": (el) => (el.classList.contains("active") ? undefined : "-1"),
         },
     };
     carouselOptions = undefined;
@@ -45,6 +48,9 @@ export class CarouselSlider extends Interaction {
         if (this.carouselInnerEl) {
             this.carouselItemEls = [...this.carouselInnerEl.querySelectorAll(".carousel-item")];
         }
+        this.carouselIndicatorEls = this.el.querySelectorAll(
+            ".carousel-indicators > :is(button, li)"
+        );
 
         this.hasInterval = ![undefined, "false", "0"].includes(this.el.dataset.bsInterval);
         if (!["true", "carousel", "false"].includes(this.el.dataset.bsRide)) {
@@ -124,6 +130,24 @@ export class CarouselSlider extends Interaction {
         }
         if (this.options.scrollMode === "single") {
             this.onSlideSingleScroll(ev);
+        }
+        this.focusNextIndicator(ev.to);
+    }
+
+    /**
+     * @param {number} nextItemIndex
+     */
+    focusNextIndicator(nextItemIndex) {
+        const nextActiveIndicatorEl = this.carouselIndicatorEls.item(nextItemIndex);
+        // If before the slide, the focused element was another indicator, move
+        // the focus to the newly active indicator.
+        if (
+            this.el.contains(document.activeElement) &&
+            document.activeElement.matches(
+                `.carousel-indicators > :is(button, li):not([data-bs-slide-to="${nextItemIndex}"])`
+            )
+        ) {
+            nextActiveIndicatorEl.focus();
         }
     }
 
