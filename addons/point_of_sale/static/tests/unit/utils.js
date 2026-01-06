12cbd268b1fcf610058220d190a10740159601c5
Léopold Cantraine <lecan@odoo.com>
2026-01-06 12:55:04 +0100

    [FIX] point_of_sale : Fix combo lines quantity update
    Before this commit when a combo product was added to the the order, if a note was added and then the product was ordered. And after we change the quantity of this product and the note, the combo children lines were not updated correctly.
    
    After this commit, the combo children lines quantities are correctly updated when the parent line quantity is changed.
    
    Step to reproduce :
    
    1. Add a combo item to the order line in Point of Sale (Restaurant).
    2. Add a Note to the main order line and place the order(Send to Kitchen).
    3. Now increase the quantity of the main order line and update the note as well.
    4. A popup appears to select the combo again make your selection. You'll notice that a new order line is added.
    5. However, in the previous order line, the main product quantity is reverted back to the original quantity, but the combo item order line quantity is not reverted, resulting in an inconsistency.
    
    Task id : 5163287
    
    closes odoo/odoo#232069
    
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/point_of_sale/static/tests/unit/utils.js
+++ addons/point_of_sale/static/tests/unit/utils.js
@@ -1,6 +1,6 @@
 import { uuidv4 } from "@point_of_sale/utils";
 import { getService, makeDialogMockEnv, mountWithCleanup } from "@web/../tests/web_test_helpers";
-import { tick, waitUntil } from "@odoo/hoot-dom";
+import { animationFrame, tick, waitFor, waitUntil } from "@odoo/hoot-dom";
 import { Deferred } from "@odoo/hoot-mock";
 import { MainComponentsContainer } from "@web/core/main_components_container";
 import { patch } from "@web/core/utils/patch";
@@ -105,4 +105,28 @@ export const patchDialogComponent = (component) => {
 
 export const expectFormattedPrice = (value, expected) => {
     expect(value).toBe(expected.replaceAll(" ", "\u00a0"));
+};
+
+export const dialogActions = async (action, steps = []) => {
+    // Launch the action in a promise to be able to await the end of the steps
+    await mountWithCleanup(MainComponentsContainer);
+    const promise = new Promise((resolve) => {
+        const call = async (fn) => {
+            const result = await fn();
+            resolve(result);
+        };
+        call(action);
+    });
+
+    // Wait for the dialog to be mounted
+    await waitFor(".o_dialog");
+
+    // Execute the steps one by one
+    for (const step of steps) {
+        await step();
+        await animationFrame();
+    }
+
+    // Return the result of the action
+    return await promise;
 };
