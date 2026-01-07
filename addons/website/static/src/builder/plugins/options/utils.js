9a0d085f527765b52eecd0dcb650622728c3f657
romo <romo@odoo.com>
2026-01-07 14:07:07 +0100

    TABS SNIPPET - Restore background color

--- addons/website/static/src/builder/plugins/options/utils.js
+++ addons/website/static/src/builder/plugins/options/utils.js
@@ -1,8 +1,7 @@
 export const CARD_PARENT_HANDLERS =
     ".s_three_columns .row > div, .s_comparisons .row > div, .s_cards_grid .row > div, .s_cards_soft .row > div, .s_product_list .row > div, .s_newsletter_centered .row > div, .s_company_team_spotlight .row > div, .s_comparisons_horizontal .row > div, .s_company_team_grid .row > div, .s_company_team_card .row > div, .s_carousel_cards_item";
 
-export const BASE_ONLY_BG_IMAGE_SELECTOR =
-    ".s_tabs .oe_structure > *, footer .oe_structure > *:not(.o_footer_bottom_part)";
+export const BASE_ONLY_BG_IMAGE_SELECTOR = "footer .oe_structure > *:not(.o_footer_bottom_part)";
 
 export const CARD_DISABLE_WIDTH_APPLY_TO = ":scope > .s_card:not(.s_carousel_cards_card)";
 export const WEBSITE_BG_APPLY_TO = ":scope > .s_carousel_cards_card";
