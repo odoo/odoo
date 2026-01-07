99375403ae5fa67274d2efbf55157a5f6db28a06
sjai-odoo <sjai@odoo.com>
2026-01-07 14:31:54 +0530

    [MOV] point_of_sale,*: split `pos_order_line` from `pos_order`
    *: pos_mrp, pos_sale, pos_self_order
    
    In this commit:
    ------------------
    - We have separated the `pos_order_line` model from the `pos_order` model to
    better organize the files and improve the overall code structure, making the
    codebase easier to understand, maintain, and navigate.
    
    task: 5473161

--- addons/point_of_sale/models/__init__.py
+++ addons/point_of_sale/models/__init__.py
@@ -16,6 +16,7 @@ from . import digest
 from . import pos_category
 from . import pos_config
 from . import pos_order
+from . import pos_order_line
 from . import pos_session
 from . import pos_snooze
 from . import product_pricelist
