f4ef6b8d6c86a9f64369d1770062a748eae5126a
Radu Macocian (admac) <admac@odoo.com>
2026-01-07 11:37:17 +0000

    [FIX] point_of_sale: catch error when loading POS
    Inconsistent data in the localDB or local storage can cause the POS to
    fail before it can get initialized. When this happens the page just
    stays showing the loading screen forever.
    
    Now when an error happens while initializing the POS the loader will
    disappear and the app will give the user the option to clear all local
    data and refresh the page. This should be especially useful for users on
    mobile where it's a pain to refresh the local data manually.
    
    Task-[5420544](https://www.odoo.com/odoo/project/1737/tasks/5420544)
    
    closes odoo/odoo#246217
    
    X-original-commit: 9775b4439ce9fc023e9c3d2951ea63b83886f387
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/point_of_sale/static/src/app/main.js
+++ addons/point_of_sale/static/src/app/main.js
@@ -9,10 +9,8 @@ import { session } from "@web/session";
 import { mountComponent } from "@web/env";
 import { Chrome } from "@point_of_sale/app/pos_app";
 
-const loader = reactive({ isShown: true });
+const loader = reactive({ isShown: true, error: false });
 whenReady(() => {
-    // Show loader as soon as the page is ready, do not wait for services to be started
-    // as some services load data over RPC and this is why we want to show a loader.
     mount(Loader, document.body, {
         getTemplate,
         props: { loader },
@@ -20,7 +18,7 @@ whenReady(() => {
         translateFn: appTranslateFn,
     });
 });
-// The following is mostly a copy of startWebclient but without any of the legacy stuff
+
 (async function startPosApp() {
     odoo.info = {
         db: session.db,
@@ -29,36 +27,41 @@ whenReady(() => {
         isEnterprise: session.server_version_info.slice(-1)[0] === "e",
     };
     await whenReady();
-    const app = await mountComponent(Chrome, document.body, {
-        name: "Odoo Point of Sale",
-        props: { disableLoader: () => (loader.isShown = false) },
-    });
-    window.addEventListener("beforeunload", function (event) {
-        if (app.env.services.pos_data.network.offline) {
-            var confirmationMessage = _t(
-                "You are currently offline. Reloading the page may cause you to lose unsaved data."
-            );
-            event.returnValue = confirmationMessage;
-            return confirmationMessage;
+    try {
+        const app = await mountComponent(Chrome, document.body, {
+            name: "Odoo Point of Sale",
+            props: { disableLoader: () => (loader.isShown = false) },
+        });
+        window.addEventListener("beforeunload", function (event) {
+            if (app.env.services.pos_data.network.offline) {
+                var confirmationMessage = _t(
+                    "You are currently offline. Reloading the page may cause you to lose unsaved data."
+                );
+                event.returnValue = confirmationMessage;
+                return confirmationMessage;
+            }
+        });
+        const classList = document.body.classList;
+        if (localization.direction === "rtl") {
+            classList.add("o_rtl");
         }
-    });
-    const classList = document.body.classList;
-    if (localization.direction === "rtl") {
-        classList.add("o_rtl");
-    }
-    if (user.userId === 1) {
-        classList.add("o_is_superuser");
-    }
-    if (app.env.debug) {
-        classList.add("o_debug");
-    }
-    if (hasTouch()) {
-        classList.add("o_touch_device");
-        classList.add("o_mobile_overscroll");
-        document.documentElement.classList.add("o_mobile_overscroll");
-    }
+        if (user.userId === 1) {
+            classList.add("o_is_superuser");
+        }
+        if (app.env.debug) {
+            classList.add("o_debug");
+        }
+        if (hasTouch()) {
+            classList.add("o_touch_device");
+            classList.add("o_mobile_overscroll");
+            document.documentElement.classList.add("o_mobile_overscroll");
+        }
 
-    registerServiceWorker();
+        registerServiceWorker();
+    } catch (e) {
+        loader.error = e;
+        throw e;
+    }
 })();
 
 function registerServiceWorker() {
