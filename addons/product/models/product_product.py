1fc6b27eb9a49f263e247a20dc9cfcc7f94cdd7b
ELCO <elco@odoo.com>
2026-01-07 09:16:09 +0100

    [FIX] *: Fix python docstrings
    This commit fixes public docstrings following changes in `test_lint` and
    specifically `test_docstring`.
    
    * = account, barcodes, base, calendar, crm, event, mail, mass_mailing,
    mass_mailing_sms, point_of_sale, pos_self_order, product, project,
    rating, sms, spreadsheet_account, survey, utm, web, website,
    website_sale
    
    Part-of: odoo/odoo#232673
    Related: odoo/enterprise#101655
    Signed-off-by: Julien Castiaux (juc) <juc@odoo.com>

--- addons/product/models/product_product.py
+++ addons/product/models/product_product.py
@@ -887,8 +887,8 @@ class ProductProduct(models.Model):
 
     def get_product_multiline_description_sale(self):
         """ Compute a multiline description of this product, in the context of sales
-                (do not use for purchases or other display reasons that don't intend to use "description_sale").
-            It will often be used as the default description of a sale order line referencing this product.
+        (do not use for purchases or other display reasons that don't intend to use "description_sale").
+        It will often be used as the default description of a sale order line referencing this product.
         """
         name = self.display_name
         if self.description_sale:
