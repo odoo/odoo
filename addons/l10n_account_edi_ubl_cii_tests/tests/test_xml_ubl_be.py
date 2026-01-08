37a199cbdcbdb93bfd496b81328f193a586ed87a
devp-odoo <devp@odoo.com>
2026-01-08 06:11:07 +0000

    [FIX] account_edi_ubl_cii, l10n_hr_edi: remove outdated config param
    The configuration parameter `account_edi_ubl_cii.use_new_dict_to_xml_helpers`
    was removed in saas-18.4.
    
    However, this parameter was still being set in various tests across
    `account_edi_ubl_cii`, `l10n_account_edi_ubl_cii_tests`, `l10n_hr_edi`, and `l10n_co_dian`.
    
    This commit removes these unnecessary configuration lines to clean up the tests.
    
    Related PR : https://github.com/odoo/enterprise/pull/103766
    
    closes odoo/odoo#243014
    
    X-original-commit: df413fdce37fac19a27fe0344490ac42efb281be
    Related: odoo/enterprise#103782
    Signed-off-by: Sven Führ (svfu) <svfu@odoo.com>
    Signed-off-by: Dev Patel (devp) <devp@odoo.com>

--- addons/l10n_account_edi_ubl_cii_tests/tests/test_xml_ubl_be.py
+++ addons/l10n_account_edi_ubl_cii_tests/tests/test_xml_ubl_be.py
@@ -107,7 +107,6 @@ class TestUBLBE(TestUBLCommon, TestAccountMoveSendCommon):
     ####################################################
 
     def test_export_import_invoice(self):
-        self.env['ir.config_parameter'].sudo().set_param('account_edi_ubl_cii.use_new_dict_to_xml_helpers', True)
         invoice = self._generate_move(
             self.partner_1,
             self.partner_2,
@@ -167,7 +166,6 @@ class TestUBLBE(TestUBLCommon, TestAccountMoveSendCommon):
         self._assert_imported_invoice_from_etree(invoice, attachment)
 
     def test_export_import_refund(self):
-        self.env['ir.config_parameter'].sudo().set_param('account_edi_ubl_cii.use_new_dict_to_xml_helpers', True)
         refund = self._generate_move(
             self.partner_1,
             self.partner_2,
