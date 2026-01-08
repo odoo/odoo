d0ef1dd1b102ec3bfa2b357d3f3f6cdbe1a8142b
Sébastien (blse) <blse@odoo.com>
2026-01-08 13:37:56 +0000

    [FIX] html_builder: revert preview on undo from the builder
    Since the [website builder refactor], all user actions from the builder
    that can change the state of the editable revert the preview (if any),
    except undo/redo.
    
    Using the shortcuts for undo while a preview is ongoing may lead to
    inconsistent state and behavior. Thus with this commit, it reverts any
    preview before undo/redo from the builder.
    
    Steps to reproduce:
    - Open website bulider
    - Click on a column (for example in the footer)
    - Change the color in the sidebar
    - Move the pointer to hover a previewable option (like shadows)
    - Press `ctrl+z` 2 times (to undo the color change)
    - Move the pointer to end the preview
    - Bug: The color is restored and cannot be undone
        (despite the undo button being enabled)
    
    [website builder refactor]: https://github.com/odoo/odoo/commit/9fe45e2b7ddbbfd0445ffe25a859e67a316d02b2
    task-5374435
    
    closes odoo/odoo#243656
    
    X-original-commit: ff7aa99e2a493f15c73350ad9918dab8fbac81b5
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Sébastien Blondiau (blse) <blse@odoo.com>

--- addons/html_builder/static/src/builder.js
+++ addons/html_builder/static/src/builder.js
@@ -316,11 +316,11 @@ export class Builder extends Component {
     }
 
     undo() {
-        this.editor.shared.history.undo();
+        this.editor.shared.operation.next(() => this.editor.shared.history.undo());
     }
 
     redo() {
-        this.editor.shared.history.redo();
+        this.editor.shared.operation.next(() => this.editor.shared.history.redo());
     }
 
     onMobilePreviewClick() {
