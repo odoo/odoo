255566c7433214b9a23cfb33d183c05dcdc92e1e
Jigar Vaghela <jva@odoo.com>
2026-01-09 20:38:04 +0530

    [IMP]: l10n_in: Change GSP Provider

--- addons/l10n_in/models/res_config_settings.py
+++ addons/l10n_in/models/res_config_settings.py
@@ -8,3 +8,10 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     group_l10n_in_reseller = fields.Boolean(implied_group='l10n_in.group_l10n_in_reseller', string="Manage Reseller(E-Commerce)")
+    l10n_in_gsp = fields.Selection(selection=[
+        ('bvm_it_consulting', 'BVM IT Consulting'),
+        ('tera_software', 'Tera Software (Deprecated)'),
+    ], string="GSP", default='tera_software', required=True,
+        help="Select the GST Suvidha Provider (GSP) you want to use to file your GST returns electronically.",
+        config_parameter='l10n_in.gst_gsp_provider',
+    )
