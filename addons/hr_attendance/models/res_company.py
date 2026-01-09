bb8bcbb365441c5d0e88c280f973f240b687f254
Martin Trigaux (mat) <mat@odoo.com>
2026-01-09 15:14:41 +0000

    [FIX] hr_attendance: make less OSM requests
    Use the _get_localisation helper that uses geoip when available
    Disable geolocalisation of device by default (i.e. in the context of an
    office/event, all requests are in the same spot)
    
    X-original-commit: d55ff59a74c97d34b5fa166781f0dd9c66cc3ecb

--- addons/hr_attendance/models/res_company.py
+++ addons/hr_attendance/models/res_company.py
@@ -40,7 +40,7 @@ class ResCompany(models.Model):
     auto_check_out = fields.Boolean(string="Automatic Check Out", default=False)
     auto_check_out_tolerance = fields.Float(default=2, export_string_translation=False)
     absence_management = fields.Boolean(string="Absence Management", default=False)
-    attendance_device_tracking = fields.Boolean(string="Device & Location Tracking", default=True)
+    attendance_device_tracking = fields.Boolean(string="Device & Location Tracking", default=False)
 
     @api.depends("attendance_kiosk_key")
     def _compute_attendance_kiosk_url(self):
