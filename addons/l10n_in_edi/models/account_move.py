c0a9c6bc996a326d471dfa1ea7229cbfb232a472
Jigar Vaghela <jva@odoo.com>
2026-01-09 15:08:04 +0000

    [IMP] l10n_in: change GSP provider for Indian EDI
    Update the Indian localization to switch the GST e-invoice GSP (GST
    Suvidha Provider) configuration to a new provider as required by recent
    integration updates.
    
    This change ensures that GST credentials uses the
    new GSP configuration and aligns with updated operational requirements.
    
    closes odoo/odoo#245489
    
    Task: 5379879
    X-original-commit: 5801f2cf78a2f4b6cc5ec2bd7e38213b01fd09f4
    Related: odoo/enterprise#105317
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>

--- addons/l10n_in_edi/models/account_move.py
+++ addons/l10n_in_edi/models/account_move.py
@@ -78,6 +78,36 @@ class AccountMove(models.Model):
                 )
             )
 
+    def _compute_l10n_in_warning(self):
+        super()._compute_l10n_in_warning()
+        gsp_provider = self.env["ir.config_parameter"].sudo().get_str("l10n_in.gsp_provider", "tera")
+        if gsp_provider != "tera":
+            return
+        indian_invoice = self.filtered(lambda m: m.country_code == 'IN' and m.move_type != 'entry' and
+            m.l10n_in_edi_status in ('to_send', 'sent') and not m.l10n_in_edi_error
+        )
+        edi_error_message = _(
+            "⚠️ Important Notice – GSP Deprecation \n"
+            "The currently selected GSP (Tera Soft) will be deprecated soon.\n"
+            "To ensure uninterrupted e-Invoice and E-way operations, please switch to BVM GSP as per the"
+        )
+        if not self.env.is_admin():
+            edi_error_message += _(
+                "\n\nYou must contact your system administrator to update the GSP."
+            )
+        for move in indian_invoice:
+            l10n_in_warning = move.l10n_in_warning or {}
+            l10n_in_warning['in_edi_gsp_deprecation'] = {
+                'message': edi_error_message,
+                'action_text': _("Documentation"),
+                'action': {
+                    'name': _("Documentation"),
+                    'type': 'ir.actions.act_url',
+                    'url': 'https://www.odoo.com/documentation/saas-19.1/applications/finance/fiscal_localizations/india.html#gsp-configuration',
+                }
+            }
+            move.l10n_in_warning = l10n_in_warning
+
     #  Action Methods
     def action_export_l10n_in_edi_content_json(self):
         self.ensure_one()
