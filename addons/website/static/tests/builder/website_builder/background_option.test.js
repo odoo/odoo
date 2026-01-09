d4be66b49efad8a8a024707a96dc5a08089b7869
FrancoisGe <fge@odoo.com>
2026-01-09 08:03:02 +0000

    [FIX] website: image edit tests due to async sidebar updates
    Image edit tests using the builder sidebar were failing intermittently
    because image elements rely on async actions with non-deterministic
    timing, causing the sidebar options to be unavailable sometimes.
    
    Use `waitSidebarUpdated` to ensure the sidebar is fully updated before
    asserting on its content.
    
    Here is an example of an error:
    error-234951
    
    closes odoo/odoo#243135
    
    X-original-commit: 367d5c1c9e741039480c314782dc25bc1510b3f5
    Signed-off-by: Robin Lejeune (role) <role@odoo.com>
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/website/static/tests/builder/website_builder/background_option.test.js
+++ addons/website/static/tests/builder/website_builder/background_option.test.js
@@ -339,7 +339,7 @@ async function dragAndDropBgImage() {
 }
 
 test("change the main color of a background image of type '/html_editor/shape'", async () => {
-    await setupWebsiteBuilder(
+    const { waitSidebarUpdated } = await setupWebsiteBuilder(
         `
             <section style="background-image: url('/html_editor/shape/http_routing/404.svg?c2=o-color-2');">
                 AAAA
@@ -350,6 +350,7 @@ test("change the main color of a background image of type '/html_editor/shape'",
         }
     );
     await contains(":iframe section").click();
+    await waitSidebarUpdated();
     await contains("[data-label='Main Color'] .o_we_color_preview").click();
     await contains(
         ".o-main-components-container .o_colorpicker_section [data-color='o-color-5']"
@@ -385,6 +386,7 @@ test("remove the background image of a snippet", async () => {
             </div>
         </section>`);
     await contains(":iframe section").click();
+    await waitSidebarUpdated();
     expect(":iframe section").toHaveStyle("backgroundImage");
     await contains("[data-action-id='toggleBgImage']").click();
     await waitSidebarUpdated();
@@ -392,7 +394,7 @@ test("remove the background image of a snippet", async () => {
 });
 
 test("changing shape's background color doesn't hide the shape itself", async () => {
-    await setupWebsiteBuilder(
+    const { waitSidebarUpdated } = await setupWebsiteBuilder(
         `<section style="background-image: url('/html_editor/shape/http_routing/404.svg?c2=o-color-2');">
             AAAA
         </section>`,
@@ -401,6 +403,7 @@ test("changing shape's background color doesn't hide the shape itself", async ()
         }
     );
     await contains(":iframe section").click();
+    await waitSidebarUpdated();
     await contains("button[data-action-id='toggleBgShape']").click();
     await contains(
         ".o_pager_container .o-hb-bg-shape-btn [data-action-value='html_builder/Connections/01'][data-action-id='setBackgroundShape']"
