ae37326fc1548f0072a1f9ac60641e1958b7bc08
Pedram (PEBR) <pebr@odoo.com>
2026-01-09 11:23:39 +0000

    [FIX] point_of_sale: ensure user role is set without pos_hr
    When the pos_hr was not installed, the user role was not set, and if the
    role was accessed with cashier._role, it was undefined.
    
    opw-5479742
    
    closes odoo/odoo#248305
    
    X-original-commit: 69ce78f595214d98964ec670bfc2d83527f7b9eb
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/point_of_sale/static/src/app/services/pos_store.js
+++ addons/point_of_sale/static/src/app/services/pos_store.js
@@ -1285,9 +1285,6 @@ export class PosStore extends WithLazyGetterTrap {
      * @returns {name: string, id: int, role: string}
      */
     getCashier() {
-        if (!this.user.role) {
-            this.user._role = this.user.raw.role;
-        }
         return this.user;
     }
     getCashierUserId() {
