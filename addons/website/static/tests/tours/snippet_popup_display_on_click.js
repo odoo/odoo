9825a08f808211377f8850feb0c2359ae2bccdb2
Pierre Paridans <app@odoo.com>
2026-01-12 12:00:11 +0000

    [FIX] website: properly mock Clipboard API calls in tours
    Reliably mocking Clipboard API calls in tours should be done in two
    steps:
    - the step that will actually do the call should do the patching,
      followed by the actual action.
    - the cleanup should only be done in the following step to ensure the
      action's listener has actually finished.
    
    This commit applies this principle to avoid the "cleanup" to be executed
    before the action's listener has actually reached the call to the
    Clipboard API (because of slower processing, slower network...), which
    would defeat the mocking purpose (and either get the browser to
    indefinitely wait for the user's clipboard usage approval or a
    permission error depending on the browser's default behavior).
    
    Note: this was mainly brought to light by the new Chrome 143+ default
    policy which revoke all permissions in headless mode.
    
    closes odoo/odoo#243360
    
    X-original-commit: 59a8c461fb1a405b744121ca0a74814533bb1268
    Related: odoo/enterprise#104031
    Signed-off-by: Pierre Pulinckx (pipu) <pipu@odoo.com>
    Signed-off-by: Pierre Paridans (app) <app@odoo.com>

--- addons/website/static/tests/tours/snippet_popup_display_on_click.js
+++ addons/website/static/tests/tours/snippet_popup_display_on_click.js
@@ -10,6 +10,8 @@ import {
 } from '@website/js/tours/tour_utils';
 import { browser } from "@web/core/browser/browser";
 
+const oldWriteText = browser.navigator.clipboard.writeText;
+
 registerWebsitePreviewTour("snippet_popup_display_on_click", {
     url: "/",
     edition: true,
@@ -27,16 +29,17 @@ registerWebsitePreviewTour("snippet_popup_display_on_click", {
         trigger: "#oe_snippets we-button[data-name='onclick_opt']",
         async run(helpers) {
             // Patch and ignore write on clipboard in tour as we don't have permissions
-            const oldWriteText = browser.navigator.clipboard.writeText;
             browser.navigator.clipboard.writeText = () => { console.info('Copy in clipboard ignored!') };
             await helpers.click();
-            browser.navigator.clipboard.writeText = oldWriteText;
         }
     },
     {
         content: "Check the copied anchor from the notification toast",
         trigger: ".o_notification_manager .o_notification_content",
         run() {
+            // Cleanup the patched clipboard method
+            browser.navigator.clipboard.writeText = oldWriteText;
+
             const notificationContent = this.anchor.innerText;
             const anchor = notificationContent.substring(notificationContent.indexOf("#"));
 
