e2aeb7794b291ca47d05f938f459852b2a97389e
Hadi El Yakhni <hael@odoo.com>
2026-01-13 11:33:38 +0100

    [FIX] point_of_sale: fix computing getTotalDiscount
    We were mistakenly doing `str - number` in 9538698f13d5763b49b00f4c06a1a2afc0d6b39e.
    We fix that now, by following what was done before the beformentioned commit. See:
    https://github.com/odoo/odoo/blob/7f20097575dde3d9d73c3b84b77ee82ead53d68e/addons/point_of_sale/static/src/app/models/pos_order.js#L645-L646.

--- addons/point_of_sale/static/src/app/models/pos_order.js
+++ addons/point_of_sale/static/src/app/models/pos_order.js
@@ -508,8 +508,7 @@ export class PosOrder extends PosOrderAccounting {
                         orderLine.discount == 0
                     ) {
                         sum +=
-                            (orderLine.currencyDisplayPriceUnitExcl -
-                                orderLine.unitPrices.no_discount_total_included) *
+                            (orderLine.displayPriceUnit - orderLine.displayPriceUnitNoDiscount) *
                             orderLine.getQuantity();
                     }
                 }
