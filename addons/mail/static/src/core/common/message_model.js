f3863a769da11164d5bc9a80771772c7308f672d
nees-odoo <nees@odoo.com>
2026-01-13 09:35:13 +0000

    [FIX] mail: allow starring persistent messages without thread
    Purpose of this Commit:
    Starring a message should depend on whether the message is persistent and
    whether the current user is allowed to star it, not on whether the message is
    attached to a thread.
    
    Before this commit:
    Some persistent messages (such as user notifications) can be displayed in Inbox
    without an associated thread. In such cases, the star action was unavailable
    even though the message itself was valid and persistent.
    
    This commit ensures:
    This change removes the dependency on thread presence when determining whether
    a message can be starred. As a result, users can now star persistent messages
    without a thread, including Inbox notifications such as sign requests.
    
    task-5473438
    
    closes odoo/odoo#247061
    
    X-original-commit: be07b6445f3ea16d60a38f22785da821d78a0021
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/core/common/message_model.js
+++ addons/mail/static/src/core/common/message_model.js
@@ -467,7 +467,6 @@ export class Message extends Record {
         return Boolean(
             !this.is_transient &&
                 !this.isPending &&
-                this.thread &&
                 this.store.self_user?.share === false &&
                 this.persistent
         );
