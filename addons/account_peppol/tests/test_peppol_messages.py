464e8b9b4c5ff1c392e715e7f0a17b2e7982ca92
Pierrot (prro) <prro@odoo.com>
2026-01-13 13:12:23 +0100

    [ADD] account_peppol_response: add the Peppol business level responses
    Peppol offers a response system to the document received through it.
    These are called Business Level Responses, and their documentation
    can be found here:
    https://docs.peppol.eu/poacc/upgrade-3/profiles/63-invoiceresponse/#introduction-to-openpeppol-and-bis
    
    The specific BLR implemented in this commit is targeted to invoices and
    credit notes.
    
    3 types of responses are mandatory for a Peppol participant to correctly
    adhere to the BLR service: acknowledgement (different to the transport
    ack), confirmation and rejection. More response's types are available
    but were not implemented as they're not mandatory/needed (for now).
    
    iap PR: https://github.com/odoo/iap-apps/pull/1364
    
    task-5237698

--- addons/account_peppol/tests/test_peppol_messages.py
+++ addons/account_peppol/tests/test_peppol_messages.py
@@ -141,7 +141,7 @@ class TestPeppolMessageCommon(TestAccountMoveSendCommon):
                 }
                 return response
 
-            if peppol_identifier == '0198:dk16356706':
+            if peppol_identifier in '0198:dk16356706':
                 response.status_code = 200
                 response.json = lambda: {"result": {
                         'identifier': peppol_identifier,
@@ -202,9 +202,20 @@ class TestPeppolMessageCommon(TestAccountMoveSendCommon):
                     'state': 'done' if not cls.env.context.get('error') else 'error',
                     'direction': 'incoming',
                     'document_type': 'Invoice',
+                    'origin_message_uuid': FAKE_UUID[1],
                 }
 
             response.json = lambda: {'result': {uuid: response_content}}
+            return response
+
+        if url == '/api/peppol/1/send_response':
+            # This will be called if account_peppol_response is installed, to be overridden in that module
+            num_responses = len(body['params']['reference_uuids'])
+            response.json = lambda: {
+                'result': {
+                    'messages': [{'message_uuid': 'rrrrrrrr-rrrr-rrrr-rrrr-rrrrrrrrrrrr'}] * num_responses
+                }
+            }
             return response
 
         return super()._request_handler(s, r, **kw)
