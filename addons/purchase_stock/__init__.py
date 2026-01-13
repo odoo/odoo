542f08d5e5871cc5b6ed0d7541796f0adaa75894
Stéphane Diez <snd@odoo.com>
2026-01-13 10:39:37 +0000

    [FIX] sale_stock,purchase_stock: ministock pickings
    Make sure that the quantity received is in the unit of measure of the purchase order line.
    Also, when installing stock, create pickings for partial and empty sale/purchase orders.
    Finally, since creating we're creating more pickings, we need to raise the demo stock
    of product_product_12 to not be in negative stock for other modules demo data.
    
    closes odoo/odoo#261337
    
    X-original-commit: 84417b2b7924eb474e3d5afeccb152e720f3ad66
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Stéphane Diez (snd) <snd@odoo.com>

--- addons/purchase_stock/__init__.py
+++ addons/purchase_stock/__init__.py
@@ -5,6 +5,34 @@ from . import models
 from . import report
 from . import wizard
 
+from odoo.fields import Domain
+
+
+def _post_init_hook(env):
+    _create_buy_rules(env)
+    _create_pickings_for_open_purchase_orders(env)
+
+
+def _split_partial_purchase_order_lines(env):
+    """ Pre-init hook: before installing stock, split partial order lines
+    so that there's no unnecessary stock trigger. """
+    to_adjust = Domain.AND([
+        Domain('state', '=', 'purchase'),
+        Domain('receipt_status', '=', 'partial')
+    ])
+    partial_orders = env['purchase.order'].search([to_adjust])
+    lines_to_create = []
+
+    for line in partial_orders.order_line:
+        if 0 < line.qty_received < line.product_qty:
+            new_line = line.copy_data({
+                'product_qty': line.product_qty - line.qty_received,
+            })
+            lines_to_create.extend(new_line)
+            line.product_qty = line.qty_received
+    if lines_to_create:
+        env['purchase.order.line'].create(lines_to_create)
+
 
 def _create_buy_rules(env):
     """ This hook is used to add a default buy_pull_id on every warehouse. It is
@@ -13,3 +41,19 @@ def _create_buy_rules(env):
     """
     warehouse_ids = env['stock.warehouse'].search([('buy_pull_id', '=', False)])
     warehouse_ids.write({'buy_to_resupply': True})
+
+
+def _create_pickings_for_open_purchase_orders(env):
+    """ Upon installing stock create pickings for all open POs (confirmed but not received). """
+    to_adjust = Domain.AND([
+        Domain('state', '=', 'purchase'),
+        Domain('receipt_status', '!=', 'full')
+    ])
+    open_purchase_orders = env['purchase.order'].search([to_adjust])
+    partial_orders = open_purchase_orders.filtered(lambda o: o.receipt_status == 'partial')
+    empty_lines = open_purchase_orders.order_line.filtered(lambda l: l.uom_id.is_zero(l.qty_received))
+    if empty_lines:
+        empty_lines._compute_qty_received_method()
+        empty_lines._set_date_promised()
+        empty_lines._create_or_update_picking()
+    partial_orders.receipt_status = 'partial'   # because the delivery_status is recomputed based on pickings state
