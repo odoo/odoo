0bde339644f9de8acbe44389ea652f21a3e808df
Giorgio Tocco (gito) <gito@odoo.com>
2026-01-14 09:10:30 +0000

    [FIX] mail: remove correspondent for group type channels
    Before this commit, the "correspondent" property of Thread would be
    computed for channels of type group (group DMs) having less than 3
    members.
    
    This would lead to various confusing behaviours, including:
    1. The "back on" banner being shown.
    2. The chat bubble showing an IM status.
    3. The notification item not showing the message author's name.
    
    This commit fixes the issues by not computing `correspondent` for
    channels of type group.
    
    task-5462395
    
    X-original-commit: fe289a1537d5f65c1c2493f16af640ba5545a30a

--- addons/mail/static/src/discuss/core/common/thread_model_patch.js
+++ addons/mail/static/src/discuss/core/common/thread_model_patch.js
@@ -229,7 +229,7 @@ const threadPatch = {
     },
     /** @returns {import("models").ChannelMember} */
     computeCorrespondent() {
-        if (this.channel_type === "channel") {
+        if (["channel", "group"].includes(this.channel_type)) {
             return undefined;
         }
         const correspondents = this.correspondents;
