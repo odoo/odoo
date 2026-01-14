60dd6c99d5eaa7185b4a1d86f671bc5363490c58
Lancelot Semal <lase@odoo.com>
2026-01-14 07:49:41 +0000

    [FIX] stock: push to location_final_id when it is a sublocation
    Issue:
    
    Applying a push rule in attempt to reach a precise sublocation will not
    adapt the destination of the move based on the `final_location_id` but
    solely base the destination based on the push rule.
    
    This is sub-optimal as the existence of an other push rule from this
    parent location to the sublocation is quite unlikely and validating that
    new move will therefore not push the product to its `location_final_id`.
    
    Concrete problematic example:
    
    For instance, the `property_stock_customer` (Customer location) provides
    a precise `final_dest_id` to deliver the customer. While, in one step
    delivery (not relying on push mechanic), confirming a delivery will
    generate a delivery with `location_dest` set to the precise Customer
    location of pull rules, in two steps, the final delivery ends up
    targeting the general `Partner/Customer` location.
    
    This is particularly impactful in case of automatic batching based on
    destination location.
    
    Steps to reproduce:
    
    - In the settings enable: Multi-steps routes
    - Inventory > Configuration > Warehouse Management > Warehouses
    - Set your warehouse to delivery in 2 steps
    - Create a contact say BOB and in the sale & purchase tab of its form
      set the Customer Location to a sublocation of Partners/Customers say:
      Partners/Customers/BOB
    - Create and confirm a sale order for BOB for 1 unit of a any product
    - Validate the pick
    > The delivery and its move have `Partner/Customer` as `move_dest_id`.
    
    Cause of the issue:
    
    During the `_run_push`, we only adapt the `location_dest_id` to match
    the `location_final_id` in the case where the new move skip push:
    https://github.com/odoo/odoo/blob/61d2fb3bb3281f3455490ac6033b95e3c50b4e2b/addons/stock/models/stock_rule.py#L241-L245
    However, in the present case where you push to a parent location of
    `location_final_id` the move does not `skip_push`:
    https://github.com/odoo/odoo/blob/61d2fb3bb3281f3455490ac6033b95e3c50b4e2b/addons/stock/models/stock_move.py#L2034-L2037
    and the `location_dest` is the one of the rule:
    https://github.com/odoo/odoo/blob/61d2fb3bb3281f3455490ac6033b95e3c50b4e2b/addons/stock/models/stock_rule.py#L241-L242
    https://github.com/odoo/odoo/blob/61d2fb3bb3281f3455490ac6033b95e3c50b4e2b/addons/stock/models/stock_rule.py#L266
    
    opw-5070855
    
    X-original-commit: c9314e73f060efd0857fd8b06c3434b3e02f3dd7

--- addons/stock/models/stock_rule.py
+++ addons/stock/models/stock_rule.py
@@ -257,8 +257,11 @@ class StockRule(models.Model):
         company_id = self.company_id.id
         copied_quantity = move_to_copy.quantity
         final_location_id = False
+        location_dest_id = self.location_dest_id.id
         if move_to_copy.location_final_id and not move_to_copy.location_dest_id._child_of(move_to_copy.location_final_id):
             final_location_id = move_to_copy.location_final_id.id
+        if move_to_copy.location_final_id and move_to_copy.location_final_id._child_of(self.location_dest_id):
+            location_dest_id = move_to_copy.location_final_id.id
         if move_to_copy.product_uom.compare(move_to_copy.product_uom_qty, 0) < 0:
             copied_quantity = move_to_copy.product_uom_qty
         if not company_id:
@@ -267,7 +270,7 @@ class StockRule(models.Model):
             'product_uom_qty': copied_quantity,
             'origin': move_to_copy.origin or move_to_copy.picking_id.name or "/",
             'location_id': move_to_copy.location_dest_id.id,
-            'location_dest_id': self.location_dest_id.id,
+            'location_dest_id': location_dest_id,
             'location_final_id': final_location_id,
             'rule_id': self.id,
             'date': new_date,
