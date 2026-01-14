991e3feed2aa0a3323cf6ef3fd6981ccc40a61f4
Arnaud Sibille <arsi@odoo.com>
2026-01-14 15:50:08 +0000

    [IMP] core,*: make 'Product Price' a minimum precision
    Imported invoices can show different totals than what the original
    document show.  This especially an issue for Peppol.
    Because `price_unit` is rounded, by computing the total of a line with
    `quantity * price_unit`, it may not be possible to obtain the same
    total as the one from the original document.
    
    To face this issue, the float fields related to "Product Price" don't
    have a decimal precision set anymore.
    To keep the UI clean, the precision set on "Product Price" is now
    interpreted as a "minimal precision".
    So if it set to 3, we'll see at least 3 digits.  If there's more, all
    the digits are shown.
    As so:
    `4.0`     -> `'4.000'`
    `4.23`    -> `'4.230'`
    `4.235`   -> `'4.235'`
    `4.2358`  -> `'4.23458'`
    
    task-4895014
    
    closes odoo/odoo#245660
    
    X-original-commit: 8c199f7783527735b35c9fbda334cbdcd55a004f
    Related: odoo/enterprise#105448
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: Arnaud Sibille (arsi) <arsi@odoo.com>

--- addons/web/static/tests/core/utils/numbers.test.js
+++ addons/web/static/tests/core/utils/numbers.test.js
@@ -252,6 +252,42 @@ test("floatIsZero", () => {
 });
 
 describe("formatFloat", () => {
+    test("precision", () => {
+        patchWithCleanup(localization, {
+            decimalPoint: ".",
+            grouping: [3, 0],
+            thousandsSep: ",",
+        });
+
+        let options = {};
+        expect(formatFloat(3, options)).toBe("3.00");
+        expect(formatFloat(3.1, options)).toBe("3.10");
+        expect(formatFloat(3.12, options)).toBe("3.12");
+        expect(formatFloat(3.129, options)).toBe("3.13");
+
+        options = { digits: [15, 3] };
+        expect(formatFloat(3, options)).toBe("3.000");
+        expect(formatFloat(3.1, options)).toBe("3.100");
+        expect(formatFloat(3.123, options)).toBe("3.123");
+        expect(formatFloat(3.1239, options)).toBe("3.124");
+
+        options = { minDigits: 3 };
+        expect(formatFloat(0, options)).toBe("0.000");
+        expect(formatFloat(3, options)).toBe("3.000");
+        expect(formatFloat(3.1, options)).toBe("3.100");
+        expect(formatFloat(3.123, options)).toBe("3.123");
+        expect(formatFloat(3.1239, options)).toBe("3.1239");
+        expect(formatFloat(3.1231239, options)).toBe("3.123124");
+        expect(formatFloat(1234567890.1234567890, options)).toBe("1,234,567,890.12346");
+
+        options = { minDigits: 3, digits: [15, 4] };
+        expect(formatFloat(3, options)).toBe("3.000");
+        expect(formatFloat(3.1, options)).toBe("3.100");
+        expect(formatFloat(3.123, options)).toBe("3.123");
+        expect(formatFloat(3.1239, options)).toBe("3.1239");
+        expect(formatFloat(3.1234567, options)).toBe("3.1235");
+    });
+
     test("localized", () => {
         patchWithCleanup(localization, {
             decimalPoint: ".",
@@ -331,7 +367,6 @@ describe("formatFloat", () => {
         expect(formatFloat(1.0045e22, options)).toBe("1.005e+22");
         expect(formatFloat(-1.0045e22, options)).toBe("-1.004e+22");
 
-        Object.assign(options, { humanReadable: false });
-        expect(formatFloat(-0.0000001, options)).toBe("0.00");
+        expect(formatFloat(-0.0000001, { digits: [16, 2], humanReadable: false })).toBe("0.00");
     });
 });
