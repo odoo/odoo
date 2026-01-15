38917af5eda028e8482490a4d1432d9629a5b070
hamza <hasaa@odoo.com>
2026-01-15 09:58:47 +0100

    [FIX] hr_attendance :  Fix quantity overtime
    This PR aims to fix how the overtime is being calculated with quantity based rules by changing this behaviors :
    
    The expected _hours didn't take into account the leaves of the day , so if the user was on a leave, it would considered him absent ( give him negative overtime) , because he has no attendances that day.
    if the employee took a whole day off, and decided to work on it, all the hours should be considered as overtime, the brake shouldn't be included
    (ex : his normal work sched is morning (9h-12h) , break(12h-13h), afternoon(13-17h) , he's already getting paid for that time , so every single hour worked that day from 12am to 12 pm is added time , and there is no break in added time)
    
    task - 5490182

--- addons/hr_holidays_attendance/tests/test_holidays_overtime.py
+++ addons/hr_holidays_attendance/tests/test_holidays_overtime.py
@@ -291,7 +291,7 @@ class TestHolidaysOvertime(TransactionCase):
             })
 
         self.assertEqual(self.employee.total_overtime, 0, 'Should have 0 hours of overtime')
-        self.assertEqual(self.manager.total_overtime, 0, "Should have 0 hours of overtime (because the employee ruleset doesn't have any timing rule)")
+        self.assertEqual(self.manager.total_overtime, 9, "Should have 9 hours of overtime (because of the quantity rule)")
 
     def test_worked_leave_type_overtime(self):
         """ Test that an attendance during a worked time off doesn't count as overtime. """
