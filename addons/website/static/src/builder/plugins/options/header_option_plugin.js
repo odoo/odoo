58118ca771b7eabaacf75c68e2e42b62483c2c2d
Jinjiu Liu <jili@odoo.com>
2026-01-15 16:18:42 +0100

    [FIX] html_editor*: prevent removing unremovable on pasting in iframe *: website
    Before this commit: when a editable container is wrapped inside a
    non-contenteditable, which could happen inside an iframe, pasting on a
    selection including an unremovable element will remove the element. This
    is because the config parameter `allowInlineAtRoot` is false by default,
    the editable container of Contact us button is considered as the edition
    boundary, and then `wrapInlinesInBlocks` is called on it at insert,
    which removes invisible nodes in the wrapping process.
    
    After this commit: we now use predicates to decide areInlinesAllowedAtRoot
    We add a predicate specifically for the container of Contact Us button,
    to allow inline element in the root. The `wrapInlinesInBlocks` won't be
    called on the container on paste anymore.
    
    task-5109662

--- addons/website/static/src/builder/plugins/options/header_option_plugin.js
+++ addons/website/static/src/builder/plugins/options/header_option_plugin.js
@@ -75,7 +75,7 @@ export class HeaderScrollEffectOption extends BaseOptionComponent {
     static editableOnly = false;
 }
 
-class HeaderOptionPlugin extends Plugin {
+export class HeaderOptionPlugin extends Plugin {
     static id = "headerOption";
     static dependencies = ["customizeWebsite"];
 
@@ -95,6 +95,11 @@ class HeaderOptionPlugin extends Plugin {
             SetShadowModeHeaderAction,
             SetShadowHeaderAction,
         },
+        // we consider the container of Contact Us allows inline element at root
+        // to avoid wrapping the button in a <p> or <div>, which would remove
+        // this button if it's empty
+        are_inlines_allowed_at_root_predicates: (node) =>
+            node.matches("#o_main_nav .oe_structure_solo .oe_unremovable [contenteditable='true']"),
     };
 }
 
