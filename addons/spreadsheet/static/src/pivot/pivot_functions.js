8bca3f954987ed56b4d20a46c57492d2c634b797
Lucas Lefèvre (lul) <lul@odoo.com>
2026-01-16 10:21:52 +0100

    [FIX] spreadsheet: ODOO.FILTER.VALUE backward compatibility
    Task-5477426
    
    closes odoo/odoo#244218
    
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>

--- addons/spreadsheet/static/src/pivot/pivot_functions.js
+++ addons/spreadsheet/static/src/pivot/pivot_functions.js
@@ -4,7 +4,7 @@ import { _t } from "@web/core/l10n/translation";
 
 import * as spreadsheet from "@odoo/o-spreadsheet";
 
-const { arg, toString } = spreadsheet.helpers;
+const { arg, isMatrix, toJsDate, toString } = spreadsheet.helpers;
 const { functionRegistry } = spreadsheet.registries;
 
 /**
@@ -15,6 +15,8 @@ const { functionRegistry } = spreadsheet.registries;
 //--------------------------------------------------------------------------
 // Spreadsheet functions
 //--------------------------------------------------------------------------
+
+// ODOO.FILTER.VALUE
 
 const ODOO_FILTER_VALUE = /** @satisfies {CustomFunctionDescription} */ ({
     description: _t("Return the current value of a spreadsheet filter."),
@@ -27,8 +29,55 @@ const ODOO_FILTER_VALUE = /** @satisfies {CustomFunctionDescription} */ ({
         const unEscapedFilterName = toString(filterName).replaceAll('\\"', '"');
         return this.getters.getFilterDisplayValue(unEscapedFilterName);
     },
-    returns: ["STRING"],
+});
+
+// ODOO.FILTER.VALUE.V18
+
+const ODOO_FILTER_VALUE_V18 = /** @satisfies {CustomFunctionDescription} */ ({
+    description: _t("Compatibility version of ODOO.FILTER.VALUE for v18 spreadsheets. Required for date filters. Optional for others."),
+    args: [arg("filter_name (string)", _t("The label of the filter whose value to return."))],
+    category: "Odoo",
+    hidden: true,
+    compute: function (filterName) {
+        const filter = this.getters.getGlobalFilterByName(toString(filterName, this.locale));
+        const value = this["ODOO.FILTER.VALUE"](filterName);
+        if (filter?.type === "relation") {
+            const csvIds = toString(value[0][0])
+            if (!csvIds) {
+                return value;
+            }
+            const ids = csvIds.split(",").map((id) => parseInt(id, 10));
+            const result =  this.odooDataProvider.serverData.get(filter.modelName, "web_search_read", [
+                [["id", "in", ids]],
+                { display_name: {} },
+            ])
+            return result.records.map((record) => record.display_name).join(", ");
+        }
+        if (filter?.type !== "date" || !isMatrix(value)) {
+            return value;
+        }
+        const startValue = value[0][0];
+        const endValue = value[1][0];
+        if (!toString(startValue) && !toString(endValue)) {
+            return "";
+        }
+        const start = toJsDate(startValue, this.locale);
+        const end = toJsDate(endValue, this.locale);
+        const endOfMonth = toJsDate(this["MONTH.END"](endValue), this.locale);
+        if (start.getDate() !== 1 || end.getDate() !== endOfMonth.getDate()) {
+            return value;
+        } else if (start.getMonth() === end.getMonth()) {
+            return String(start.getMonth() + 1).padStart(2, "0") + "/" + start.getFullYear();
+        } else if (end.getMonth() - start.getMonth() === 2) {
+            const quarter = Math.floor(start.getMonth() / 3) + 1;
+            return "Q" + quarter + "/" + start.getFullYear();
+        } else if (start.getFullYear() === end.getFullYear()) {
+            return toString(start.getFullYear(), this.locale);
+        }
+        return value;
+    },
 });
 
 functionRegistry
-    .add("ODOO.FILTER.VALUE", ODOO_FILTER_VALUE);
+    .add("ODOO.FILTER.VALUE", ODOO_FILTER_VALUE)
+    .add("ODOO.FILTER.VALUE.V18", ODOO_FILTER_VALUE_V18);
