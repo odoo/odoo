5f44afbb6c7e20bead9ba34d39d52d052ad5edc2
Sébastien (blse) <blse@odoo.com>
2026-01-16 14:34:54 +0000

    [FIX] website: avoid switching away from customize tab in translate
    With commit 3a80ac79c5b0193911e8ddab442660b315562639, the builder option
    plugin is not a custom one in translate. And the builder option plugin
    automatically switches to a fallback tab (instead of customize tab)
    when containers are de-activated. This happens when the user hides an
    invisible element.
    
    This commit fixes it by skipping swithing to fallback tab if the builder
    is in translation mode. It also simplifies the logic about the fallback
    tab, and add test for the desired behavior of the fallback.
    
    Steps to reproduce:
    - Open website builder
    - Drop a section
    - Make it invisible on desktop
    - Add a language to the website
    - Open builder in translate mode
    - Click on the eye of invisible elements to show the invisible section
    - Click again to hide it
    - Bug: the sidebar left the "customize" tab and switched to "block" tab
    
    task-5475107
    
    closes odoo/odoo#250676
    
    X-original-commit: 7617487ca694ec5be6a0f659aa430fdc725e4800
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Sébastien Blondiau (blse) <blse@odoo.com>

--- addons/html_builder/static/src/builder.js
+++ addons/html_builder/static/src/builder.js
@@ -162,7 +162,7 @@ export class Builder extends Component {
                     },
                     change_current_options_containers_listeners: (currentOptionsContainers) => {
                         this.state.currentOptionsContainers = currentOptionsContainers;
-                        if (currentOptionsContainers.length) {
+                        if (currentOptionsContainers.length || this.props.onlyCustomizeTab) {
                             this.activeTargetEl = null;
                             this.setTab("customize");
                         } else if (this.state.activeTab === "customize") {
