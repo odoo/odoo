44dff7e3447df71fb37c2a80c92102c8002771b5
VincentJanssen-Code <vijan@odoo.com>
2026-01-16 10:12:23 +0100

    [FIX] project_timesheet_holidays: Exclude archived employees from time-off timesheets
    ## Steps to Reproduce:
    1. Create a time off for Employee A (it should affect the timesheets).
    2. Create a new public holiday (global time off) that overlaps with Employee A’s time off.
    3. Archive Employee A.
    4. Delete the public holiday created in step 2.
    5. An error related to timesheet generation appears.
    
    ## Expected Behavior:
    - The public holiday / global time off should be deleted without any error.
    
    ## Fix (Implemented):
    When regenerating timesheets due to changes in holidays or time off, leaves related to archived employees should not be taken into account.
    A check was added inside the `_generate_timesheets` function in
    `project_timesheet_holidays/models/hr_holidays.py` to exclude leaves belonging to archived employees.
    
    ## Alternative Fix (Not Implemented):
    Instead of filtering out leaves linked to archived employees, we could delete those leaves when an employee is archived. However, this approach is not ideal, as archived employees may be reactivated later and would still need their previously requested time off to be preserved.
    
     ## Version:
     This bug appears in both version 17.0 and 19.0. I assumed that it also appears in 18.0  but didn't directly test

--- addons/project_timesheet_holidays/models/hr_holidays.py
+++ addons/project_timesheet_holidays/models/hr_holidays.py
@@ -71,6 +71,8 @@ class Holidays(models.Model):
         calendar_leaves_data = self.env['resource.calendar.leaves']._read_group([('holiday_id', 'in', self.ids)], ['holiday_id'], ['id:array_agg'])
         mapped_calendar_leaves = {leave: calendar_leave_ids[0] for leave, calendar_leave_ids in calendar_leaves_data}
         for leave in self:
+            if not leave.employee_id.active:
+                continue
             if leave.holiday_type != 'employee' or not leave.holiday_status_id.timesheet_generate:
                 continue
 
