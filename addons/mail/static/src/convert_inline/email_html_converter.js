21919062721dd3e7c39c8ad603877b453d18413a
abd-msyukyu-odoo <abd@odoo.com>
2026-01-16 14:07:05 +0100

    stop using 'editable' terminology

--- addons/mail/static/src/convert_inline/email_html_converter.js
+++ addons/mail/static/src/convert_inline/email_html_converter.js
@@ -1,4 +1,4 @@
-import { getCSSRules, toInline } from "@mail/views/web/fields/html_mail_field/convert_inline";
+// import { getCSSRules, toInline } from "@mail/views/web/fields/html_mail_field/convert_inline";
 import { renderToElement, renderToFragment } from "@web/core/utils/render";
 import { loadIframe } from "@mail/convert_inline/iframe_utils";
 import { isBrowserSafari } from "@web/core/browser/feature_detection";
@@ -9,7 +9,7 @@ export class EmailHtmlConverter extends PluginManager {
         super.setup();
         this.iframe = null;
         /** @type { HTMLElement } **/
-        this.editable = null;
+        this.previewContainer = null;
         /** @type { Document } **/
         this.document = null;
     }
@@ -18,19 +18,19 @@ export class EmailHtmlConverter extends PluginManager {
         this.iframe.remove();
     }
 
-    setupEditableIframe() {
-        this.iframe = renderToElement("mail.EmailHtmlConverterEditableIframe", {
+    setupPreviewContainerIframe() {
+        this.iframe = renderToElement("mail.EmailHtmlConverterPreviewIframe", {
             isBrowserSafari,
         });
         this.config.referenceDocument.body.append(this.iframe);
     }
 
-    setupEditable() {
+    setupPreviewContainer() {
         this.iframe.contentDocument.head.append(renderToFragment("mail.EmailHtmlConverterHead"));
-        this.editable = this.iframe.contentDocument.body;
+        this.previewContainer = this.iframe.contentDocument.body;
         this.document = this.iframe.contentDocument;
         // The iframe body must exactly have the iframe horizontal dimensions.
-        this.editable.setAttribute(
+        this.previewContainer.setAttribute(
             "style",
             `margin: 0 !important;
             padding: 0 !important;`
@@ -46,8 +46,8 @@ export class EmailHtmlConverter extends PluginManager {
         }
         this.setup();
         this.config = config;
-        this.setupEditableIframe();
-        await loadIframe(this.iframe, () => this.setupEditable());
+        this.setupPreviewContainerIframe();
+        await loadIframe(this.iframe, () => this.setupPreviewContainer());
 
         this.preparePlugins();
         this.startPlugins();
@@ -61,7 +61,7 @@ export class EmailHtmlConverter extends PluginManager {
     getPluginContext() {
         return Object.assign(super.getPluginContext(...arguments), {
             document: this.document,
-            editable: this.editable,
+            previewContainer: this.previewContainer,
         });
     }
 
@@ -82,18 +82,12 @@ export class EmailHtmlConverter extends PluginManager {
         // 2 notify plugins that the reference is ready to be used as such (e.g. for style computations)
         this.dispatchTo("reference_content_loaded_handlers");
 
-        // TODO EGGMAIL: rename
-        this.dispatchTo("specific_block_changes");
-
-        // TODO EGGMAIL: rename
-        this.dispatchTo("ensure_responsivity_handlers");
-
         const emailTemplate = this.getEmailTemplate();
 
         // Old toInline
         // TODO EGGMAIL: adapt usage, use plugin instead of old method
-        const cssRules = getCSSRules(this.config.referenceDocument);
-        await toInline(this.config.reference, cssRules);
+        // const cssRules = getCSSRules(this.config.referenceDocument);
+        // await toInline(this.config.reference, cssRules);
         return emailTemplate.innerHTML;
     }
 }
