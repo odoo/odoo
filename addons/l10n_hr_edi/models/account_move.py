9bd7db4979c093944a0c8d9d73fa9dcec4ba1ab4
nape-odoo <nape@odoo.com>
2026-01-16 15:58:42 +0100

    [FIX] l10n_hr_edi: Minor UI, API and error handling fixes
    - Adjusting error handling for receiving an empty response from MER for a
    document fiscalization status.
    - Adding additional checks for running multi-company-wide MER API methods.
    - Adjusting how approval API call is handled when confirming a bill.
    - Adding a tooltip about Company BU in MER settings and missing
    "company dependent" indicators for the credentials.
    - Fixing a minor XML generation error.
    Continuation of task-4925745
    Related to opw-5477846
    
    closes odoo/odoo#244023
    
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>

--- addons/l10n_hr_edi/models/account_move.py
+++ addons/l10n_hr_edi/models/account_move.py
@@ -177,9 +177,18 @@ class AccountMove(models.Model):
             if move.country_code == 'HR' and move.is_sale_document():
                 if not move.l10n_hr_fiscal_user_id:
                     move.l10n_hr_fiscal_user_id = move.env.user.partner_id
-            if move.company_id.country_code == 'HR' and move.is_purchase_document() and move.l10n_hr_business_document_status == '1':
-                raise UserError(self.env._("This vendor bill is already rejected according to the Tax Authority."))
-        return super()._post(soft)
+            if move.l10n_hr_mer_document_eid and move.is_purchase_document():
+                if move.l10n_hr_business_document_status == '1':
+                    raise UserError(self.env._("This vendor bill is already rejected according to the Tax Authority."))
+                elif move.l10n_hr_business_document_status in ('4', '99'):
+                    _mer_api_update_document_process_status(
+                        move.company_id,
+                        move.l10n_hr_mer_document_eid,
+                        0,
+                    )
+                    move.l10n_hr_edi_addendum_id.business_document_status = '0'
+                    _logger.info("Document eID %s reported as approved by recepient.", move.l10n_hr_mer_document_eid)
+        return super()._post(soft=soft)
 
     def l10n_hr_edi_mer_action_reject(self):
         self.ensure_one()
