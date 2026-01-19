31f3ccb2df1fbe9f0d69b8de2a0ef06adfb31186
Parth Solanki[PASO] <paso@odoo.com>
2026-01-19 17:04:54 +0530

    [FIX] point_of_sale: correct discount amount on session report
    Steps:
    - Create a fiscal position with 2 different taxes
    - Add a line in POS
    - Apply fiscal position and add line discount
    - Finish the order cycle
    - Download the session report
    
    Issue:
    - The discount amount was calculated incorrectly in the session report
    
    Cause:
    - The discount amount calculation used taxes before applying the fiscal position
    
    Fix:
    - Used `tax_ids_after_fiscal_position` for tax calculation while computing the
    discount amount
    
    task-5421215
    
    closes odoo/odoo#244650
    
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/point_of_sale/models/pos_order.py
+++ addons/point_of_sale/models/pos_order.py
@@ -1764,7 +1764,7 @@ class PosOrderLine(models.Model):
 
     def _get_discount_amount(self):
         self.ensure_one()
-        original_price = self.tax_ids.compute_all(self.price_unit, self.currency_id, self.qty, product=self.product_id, partner=self.order_id.partner_id)['total_included']
+        original_price = self.tax_ids_after_fiscal_position.compute_all(self.price_unit, self.currency_id, self.qty, product=self.product_id, partner=self.order_id.partner_id)['total_included']
         return original_price - self.price_subtotal_incl
 
 
