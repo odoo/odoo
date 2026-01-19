6fe536a1024304db5c0af1160386baa0c263c522
dijo-odoo <dijo@odoo.com>
2026-01-19 12:01:31 +0530

    [IMP] l10n_cn: change fapiao field size to 20
    with this commit:-
     - We're changing the char size to 20 from 8.
     - We remove the constraint which prevent to save the record if the size is not
       8.
    
    task-5501485

--- addons/l10n_cn/models/account_move.py
+++ addons/l10n_cn/models/account_move.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError
+from odoo import api, fields, models
 from odoo.osv import expression
 
 try:
@@ -13,13 +12,7 @@ except ImportError:
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    fapiao = fields.Char(string='Fapiao Number', size=8, copy=False, tracking=True)
-
-    @api.constrains('fapiao')
-    def _check_fapiao(self):
-        for record in self:
-            if record.fapiao and (len(record.fapiao) != 8 or not record.fapiao.isdecimal()):
-                raise ValidationError(_("Fapiao number is an 8-digit number. Please enter a correct one."))
+    fapiao = fields.Char(string='Fapiao Number', size=20, copy=False, tracking=True)
 
     @api.model
     def check_cn2an(self):
