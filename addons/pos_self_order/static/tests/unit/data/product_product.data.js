57655ea80223f9ca110e580ebe29007df7ccd52d
Hadi El Yakhni <hael@odoo.com>
2026-01-19 14:04:02 +0100

    [FIX] pos_self_order: respect pricelist variant prices in checkout
    Setup
    -----
    1. Make a product with 2 variants, size S and M for example.
    2. Create 2 pricelists, A and B, and make them available in PoS. The
       default one should be A.
    3. For the created product, create 2 price rules:
        1. One changing the price of the variant S for the pricelist B
        2. One changing the price of the variant M for the pricelist B
    4. Enable mobile self order and create a peset that applies the
       pricelist B
    5. Open self order, and select that preset (it should apply the
       pricelist B).
    6. Select the product of step 1, and choose the variant M.
    7. Go to checkout and notice the price.
    
    The bug
    -------
    We expect the checkout page to reflect the variant price as defined by
    the price rule. That didn't happen. The price on the checkout page is
    that of the variant before applying the pricelist rule!!
    
    Reason
    -------
    This is because, when creating the new line, `addToCart` was setting its
    `price_unit` the variant `lst_price`, and it's not using
    `productTemplate.getPrice` which means any price rules defined by the
    current pricelist ("B") will not be taken into consideration.
    
    The fix
    -------
    We now use `getProductPriceInfo` to know the price of that variant
    product after all the pricelist rules are being applied.
    
    Note that we already use this approach (i.e. `getProductPriceInfo`) to
    find the price of the original product before even checking if a
    variant is selected. So our fix is just an extension to the normal case.
    
    opw-5467593
    
    Part-of: odoo/odoo#243304
    Signed-off-by: Stéphane Vanmeerhaeghe (stva) <stva@odoo.com>

--- addons/pos_self_order/static/tests/unit/data/product_product.data.js
+++ addons/pos_self_order/static/tests/unit/data/product_product.data.js
@@ -7,7 +7,33 @@ patch(ProductProduct.prototype, {
     },
 });
 
-ProductProduct._records = ProductProduct._records.map((record) => ({
+ProductProduct._records = [
+    ...ProductProduct._records,
+    {
+        id: 19,
+        product_tmpl_id: 19,
+        lst_price: 10,
+        standard_price: 0,
+        display_name: "T-Shirt (S)",
+        product_tag_ids: [],
+        barcode: false,
+        default_code: false,
+        product_template_attribute_value_ids: [],
+        product_template_variant_value_ids: [1],
+    },
+    {
+        id: 20,
+        product_tmpl_id: 19,
+        lst_price: 15,
+        standard_price: 0,
+        display_name: "T-Shirt (M)",
+        product_tag_ids: [],
+        barcode: false,
+        default_code: false,
+        product_template_attribute_value_ids: [],
+        product_template_variant_value_ids: [2],
+    },
+].map((record) => ({
     ...record,
     self_order_available: true,
 }));
