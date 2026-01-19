f3949afa50709b457278863a3cc9c7a0791417d2
djameltouati <otd@odoo.com>
2026-01-19 16:27:26 +0000

    [FIX] sale_stock: allow access to serial numbers linked to sale orders
    Steps to reproduce the bug
    
    - Create a storable product P1:
      - Tracking: Serial Number
    - Log in as Marc Demo
    - Create a sales order with 1 unit of P1
    - Validate the delivery using serial number SN1
    - Log in as Mitchell Admin
    - Go to Settings:
      - Manage Users
        - Mitchell Admin
          - Sales: User: own documents only
    - Go to the Serial Numbers list view:
      - Try to open SN1
    
    Problem
    
    An access error is triggered:
    
    ```
    Uh-oh! Looks like you have stumbled upon some top-secret records.
    
    Sorry, Mitchell Admin (id=2) doesn't have 'read' access to:
    - Sales Order Line, S00025 - P1 (Deco Addict) (sale.order.line: 51)
    
    Blame the following rules:
    - Personal Order Lines
    ```
    
    When clicking on SN1, the `stock.lot` form view.
    it's contains the field "sale_order_count", which is a computed field
    that needs to access all `sale.order` records using the serial number
    in order to compute the count.
    
    Since Mitchell Admin is restricted to his own sales orders only,
    an access error is raised during the computation.
    
    There is also a many2many view widget that triggers an access errors.
    This widget can be removed since the smart button is now available.
    The widget has already been removed in v19.
    
    Solution
    
    In this view, any stock user, admin or not, must be able to see how
    many sales orders use a given serial number, regardless of whether
    those sales orders belong to them or not.
    
    opw-5400731
    
    closes odoo/odoo#246210
    
    X-original-commit: 95d1ce0f60e79e915628136ee66c013edef2645e
    Signed-off-by: Quentin Wolfs (quwo) <quwo@odoo.com>

--- addons/sale_stock/models/stock.py
+++ addons/sale_stock/models/stock.py
@@ -321,14 +321,21 @@ class StockLot(models.Model):
 
     @api.depends('name')
     def _compute_sale_order_ids(self):
-        sale_orders = defaultdict(lambda: self.env['sale.order'])
-        for move_line in self.env['stock.move.line'].search([('lot_id', 'in', self.ids), ('state', '=', 'done')]):
-            move = move_line.move_id
-            if move.picking_id.location_dest_id.usage in ('customer', 'transit') and move.sale_line_id.order_id:
-                sale_orders[move_line.lot_id.id] |= move.sale_line_id.order_id
+        sale_orders = defaultdict(set)
+        move_lines = self.env['stock.move.line'].search([
+            ('lot_id', 'in', self.ids),
+            ('state', '=', 'done'),
+            ('move_id.sale_line_id.order_id', '!=', False),
+            ('move_id.picking_id.location_dest_id.usage', 'in', ('customer', 'transit')),
+        ])
+        for ml in move_lines:
+            so = ml.move_id.sale_line_id.order_id
+            if so.with_user(self.env.user).has_access('read'):
+                sale_orders[ml.lot_id.id].add(so.id)
         for lot in self:
-            lot.sale_order_ids = sale_orders[lot.id]
-            lot.sale_order_count = len(lot.sale_order_ids)
+            so_ids = sale_orders.get(lot.id, set())
+            lot.sale_order_ids = [Command.set(list(so_ids))]
+            lot.sale_order_count = len(so_ids)
 
     def action_view_so(self):
         self.ensure_one()
