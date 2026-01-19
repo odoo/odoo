b6efb1eefdcb4a7cd73ebc14e5aabd61647a5246
Meet Jivani [MEJI] <meji@odoo.com>
2026-01-19 16:18:29 +0530

    [FIX] point_of_sale: incorrect tax names shown in info popup
    Before this commit:
    - The POS product info popup displayed only the first tax from `tax_details`,
      which meant multiple applied taxes were not visible.
    - The logic fetched a single tax name instead of all tax applied to the product.
    
    After this commit:
    - The POS store sends a full list of tax names instead of a single name.
    - The popup template displays all tax names, comma-separated.
    
    task-5406826
    
    closes odoo/odoo#244453
    
    X-original-commit: 5b738da1beba668032e323f182869442889d3ecf
    Signed-off-by: Stéphane Vanmeerhaeghe (stva) <stva@odoo.com>
    Signed-off-by: Meet Jivani (meji) <meji@odoo.com>

--- addons/point_of_sale/tests/test_frontend.py
+++ addons/point_of_sale/tests/test_frontend.py
@@ -2473,10 +2473,21 @@ class TestUi(TestPointOfSaleHttpCommon):
     def test_product_long_press(self):
         """ Test the long press on product to open the product info """
         archive_products(self.env)
+        group_tax = self.env['account.tax'].create({
+            'name': 'Parent Tax',
+            'amount_type': 'group',
+            'children_tax_ids': [(0, 0, {
+                'name': 'Child Tax 1',
+                'amount': 10,
+            }), (0, 0, {
+                'name': 'Child Tax 2',
+                'amount': 5,
+            })],
+        })
         self.env['product.product'].create({
             'name': 'Test Product',
             'list_price': 100,
-            'taxes_id': False,
+            'taxes_id': [(6, 0, [group_tax.id])],
             'available_in_pos': True,
         })
         self.main_pos_config.with_user(self.pos_user).open_ui()
