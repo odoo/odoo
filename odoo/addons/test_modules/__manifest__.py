fe70dd6a075529919b21182280f8236dc4e828ef
Ruben Gomes (rugo) <rugo@odoo.com>
2026-01-20 11:09:52 +0100

    wip

---
+++ odoo/addons/test_modules/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Test Modules',
+    'category': 'Hidden/Tests',
+    'description': """A module to test Modules.""",
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
