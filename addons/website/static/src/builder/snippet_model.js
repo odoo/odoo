33edc0d8f8ef95dd4398d13949a6dc7f9cd1a77c
Benjamin Vray <bvr@odoo.com>
2026-01-20 10:37:23 +0100

    [FIX] web_editor: keep labels in custom previews
    Steps to reproduce:
    
    - Open the website editor.
    - Save a "Carousel" snippet as a custom snippet.
    - Open the snippet dialog.
    => The custom snippet shows no label.
    
    Before this commit, custom previews dropped the base label so the dialog
    showed no tag for custom snippets.
    
    After this commit, custom previews reuse the base label.
    
    task-5156137
    
    closes odoo/odoo#248929
    
    X-original-commit: https://github.com/odoo/odoo/commit/34e9dc3778223a6ca044832ace060e0847661086
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Benjamin Vray (bvr) <bvr@odoo.com>

--- addons/website/static/src/builder/snippet_model.js
+++ addons/website/static/src/builder/snippet_model.js
@@ -19,16 +19,30 @@ patch(SnippetModel.prototype, {
     /**
      * @override
      */
-    getSnippetLabel(snippetEl) {
-        const label = super.getSnippetLabel(snippetEl);
-        // Check if any element in the snippet has the "parallax" class to show
-        // the "Parallax" label. This must be done this way because a theme or
-        // custom snippet may add or remove parallax elements. Note that if a
-        // label is already set, we do not change it.
+    getSnippetLabel(snippetEl, isCustom = false) {
+        let label = super.getSnippetLabel(snippetEl);
         if (!label) {
             const contentEl = snippetEl.children[0];
-            if (contentEl.matches(".parallax") || !!contentEl.querySelector(".parallax")) {
-                return _t("Parallax");
+            const parallaxLabel = _t("Parallax");
+            // Retrieve the original snippet label when a snippet is a custom
+            // snippet.
+            if (isCustom) {
+                const originalSnippetLabel = this.getOriginalSnippet(
+                    contentEl.dataset.snippet
+                )?.label;
+                if (originalSnippetLabel && originalSnippetLabel !== parallaxLabel) {
+                    label = originalSnippetLabel;
+                }
+            }
+            // Check if any element in the snippet has the "parallax" class to
+            // show the "Parallax" label. This must be done this way because a
+            // theme or custom snippet may add or remove parallax elements. Note
+            // that if a label is already set, we do not change it.
+            if (
+                !label &&
+                (contentEl.matches(".parallax") || !!contentEl.querySelector(".parallax"))
+            ) {
+                label = parallaxLabel;
             }
         }
         return label;
