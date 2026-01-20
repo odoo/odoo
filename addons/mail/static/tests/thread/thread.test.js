42759629c32441358b4fb35f4bccd86c46c8e08d
tsm-odoo <tsm@odoo.com>
2026-01-20 11:20:28 +0100

    [FIX] mail: fix inbox mark as read test
    The `Opening thread with needaction messages should mark all
    messages of thread as read` ensures that opening a channel marks
    the related inbox messages as read.
    
    However, this test can fail in a non-deterministic fashion. There
    are two flows that can mark inbox messages as read: fetching the
    messages or a manual request after opening the channel.
    
    The test expects the rpc to be issued, but if the messages
    are fetched before, it won't happen thus making the test fail.
    
    This commit awaits the initial fetch to prevent this issue. As a
    bonus, the second flow is also tested.
    
    runbot-237970
    
    closes odoo/odoo#244693
    
    Signed-off-by: Didier Debondt (did) <did@odoo.com>

--- addons/mail/static/tests/thread/thread.test.js
+++ addons/mail/static/tests/thread/thread.test.js
@@ -760,8 +760,16 @@ test("Opening thread with needaction messages should mark all messages of thread
             ["res_id", "=", channelId],
         ]);
     });
+    pyEnv["mail.message"].create({
+        body: "Hello there!",
+        model: "discuss.channel",
+        res_id: channelId,
+        author_id: partnerId,
+    });
     await start();
     await openDiscuss(channelId);
+    await contains(".o-mail-Message:contains('Hello there!)");
+    await contains("button", { text: "Inbox", contains: [".badge", { count: 0 }] });
     await contains(".o-mail-Composer-input");
     await triggerEvents(".o-mail-Composer-input", ["blur", "focusout"]);
     await click("button", { text: "Inbox" });
