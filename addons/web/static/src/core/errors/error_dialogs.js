odoo/odoo

commit e566570e82e95d68288d3c8cba8c7fa954a7f4fe

[FIX] web: web_save RedirectWarning with action and additionalContext
Have a web_save that raises a RedirectWarning which has the ID of an action and
an additional context in its parameters.

Trigger the warning in the form view by clicking on the save button in the form view.
Before this commit, this feature did not work like at all.
- The additional context was not taken into account
- the path taken by clicking on the form's save button was not able to handle
interacting with the main form view
- The error dialog did not handle going into an action in target other than new

After this commit, all this is fixed and the whole flow, that allow an error to
be enriched such that the user could do the correct action to correct the error
now works.

opw-4742952

closes odoo/odoo#224312

X-original-commit: 06716818621a248237eeee4dd19c5a77d2eac314
Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>
Signed-off-by: Lucas Perais (lpe) <lpe@odoo.com>

addons/web/static/src/views/form/form_controller.js
@@ -164,6 +164,7 @@ export class FormController extends Component {
 
     setup() {
         this.evaluateBooleanExpr = evaluateBooleanExpr;
+        this.actionService = useService("action");
         this.dialogService = useService("dialog");
         this.orm = useService("orm");
         this.viewService = useService("view");
@@ -433,6 +434,17 @@ export class FormController extends Component {
                     discard();
                     resolve(true);
                 },
+                onRedirect: async ({ action, additionalContext }) => {
+                    this.allowLeavingWithoutSaving = true;
+                    try {
+                        await this.actionService.doAction(action, {
+                            additionalContext,
+                        });
+                    } finally {
+                        this.allowLeavingWithoutSaving = false;
+                        resolve(false);
+                    }
+                },
                 onStayHere: () => resolve(false),
             });
         });
@@ -471,7 +483,7 @@ export class FormController extends Component {
     }
 
     async beforeLeave() {
-        if (this.model.root.dirty) {
+        if (this.model.root.dirty && !this.allowLeavingWithoutSaving) {
             return this.save({
                 reload: false,
                 onError: this.onSaveError.bind(this),
@@ -654,6 +666,9 @@ export class FormController extends Component {
     }
 
     saveButtonClicked(params = {}) {
+        if (!("onError" in params)) {
+            params.onError = this.onSaveError.bind(this);
+        }
         return executeButtonCallback(this.ui.activeElement, () => this.save(params));
     }
 

addons/web/static/tests/views/form/form_view.test.js
@@ -7,6 +7,7 @@ import {
     queryAllAttributes,
     queryAllTexts,
     queryFirst,
+    waitFor,
 } from "@odoo/hoot-dom";
 import {
     animationFrame,
@@ -9169,8 +9170,6 @@ test(`multiple clicks on save should reload only once`, async () => {
 });
 
 test(`form view is not broken if save operation fails`, async () => {
-    expect.errors(1);
-
     onRpc("web_save", ({ args }) => {
         if (args[1].foo === "incorrect value") {
             throw makeServerError();
@@ -9189,7 +9188,6 @@ test(`form view is not broken if save operation fails`, async () => {
     await contains(`.o_form_button_save`).click();
     await animationFrame();
     expect(`.o_dialog`).toHaveCount(1);
-    expect.verifyErrors(["RPC_ERROR: Odoo Server Error"]);
     expect.verifySteps(["web_save"]); // write on save (it fails, does not trigger a read)
 
     await contains(`.o_dialog .modal-footer .btn-primary`).click();
@@ -9244,6 +9242,76 @@ test(`form view is not broken if save operation fails with redirect warning`, as
 
     // RedirectWarning dialog
     expect(`.modal-title`).toHaveText("Sub view");
+});
+
+test.tags("desktop");
+test("Redirect Warning full feature: additional context, action_id, leaving while dirty", async function () {
+    defineActions([
+        {
+            id: 1,
+            name: "Partner",
+            res_model: "partner",
+            type: "ir.actions.act_window",
+            views: [[false, "form"]],
+            res_id: 1,
+        },
+        {
+            id: 2,
+            name: "Partner List",
+            res_model: "partner",
+            type: "ir.actions.act_window",
+            domain: "[['id', 'in', [active_id]]]",
+            views: [[false, "list"]],
+        },
+    ]);
+    Partner._views = {
+        list: `
+                <list>
+                    <field name="name"/>
+                </list>`,
+        form: `
+                <form>
+                    <group>
+                        <field name="name"/>
+                    </group>
+                </form>`,
+        "partner,false,search": "<search></search>",
+    };
+    onRpc("partner", "web_save", () => {
+        expect.step("web_save");
+        throw makeServerError({
+            type: `RedirectWarning`,
+            args: [
+                "The message",
+                2,
+                "Button Label",
+                {
+                    active_id: 4,
+                },
+            ],
+            description: "Beep boop server stuff and technical string",
+        });
+    });
+    onRpc("partner", "web_search_read", (args) => {
+        expect.step("web_search_read");
+        expect(args.kwargs.domain).toEqual([["id", "in", [4]]]);
+    });
+
+    await mountWithCleanup(WebClient);
+    await getService("action").doAction(1);
+
+    await contains(".o_field_widget[name='name'] input").edit("some invalid input");
+    await contains(".o_form_button_save").click();
+    expect.verifySteps(["web_save"]);
+
+    await waitFor(".o_error_dialog");
+    expect(".o_error_dialog .btn-primary").toHaveCount(1);
+    expect(".o_error_dialog .btn-secondary").toHaveCount(2);
+
+    await contains(".o_error_dialog .btn-secondary").click();
+    await waitFor(".o_list_view");
+    expect.verifySteps(["web_search_read"]);
+    expect(".o_breadcrumb").toHaveText("first record\nPartner List");
 });
 
 test(`form view is not broken if save failed in readonly mode on field changed`, async () => {

addons/web/static/src/views/form/form_error_dialog/form_error_dialog.js
@@ -11,6 +11,7 @@ export class FormErrorDialog extends Component {
         data: { type: Object },
         onDiscard: Function,
         onStayHere: Function,
+        onRedirect: { type: Function, optional: true },
         close: Function,
     };
 
@@ -21,12 +22,23 @@ export class FormErrorDialog extends Component {
             this.message = this.props.data.arguments[0];
             this.redirectAction = this.props.data.arguments[1];
             this.redirectBtnLabel = this.props.data.arguments[2];
+            this.additionalContext = this.props.data.arguments[3];
         }
     }
 
-    onRedirectBtnClicked() {
-        this.action.doAction(this.redirectAction);
-        this.stay();
+    async onRedirectBtnClicked() {
+        if (this.props.onRedirect) {
+            await this.props.onRedirect({
+                action: this.redirectAction,
+                additionalContext: this.additionalContext,
+            });
+            this.props.close();
+        } else {
+            await this.action.doAction(this.redirectAction, {
+                additionalContext: this.additionalContext,
+            });
+            this.stay();
+        }
     }
 
     async discard() {

commit 9011feba0289c9cde400d9f867c2d959341d9897

[FIX] web: display invalid operation instead of oh snap for user errors This commit https://github.com/odoo/odoo/commit/e566570e82e9 introduced a regression where the saveButtonClicked method automatically bound onSaveError as the error handler. This displays FormErrorDialog with "Oh snap!" for all save errors, causing UserErrors and ValidationErrors to show the generic "Oh snap!" message instead of the proper "Invalid Operation" dialog.
If you compare the original commit to the one here for the forward port 18.3+: https://github.com/odoo/odoo/pull/224620, you can see that the author reverted the problematic onError binding in saveButtonClicked and added forceLeave support to handle RedirectWarning redirects when the form is dirty. This PR backports those same changes to 18.0.

Steps To Reproduce:
1. Go to Time Off > Allocations > Create new allocation.
2. Set an End Date before the Start Date.
3. Click Save.
4. Error displays as "Oh snap!" instead of "Invalid Operation".
5. Switching tabs then shows the expected "Invalid Operation" dialog.

Ticket [link](https://www.odoo.com/odoo/project.task/5488127)
opw-5488127

addons/web/static/src/core/errors/error_dialogs.js
@@ -184,7 +184,7 @@ export class RedirectWarningDialog extends Component {
         this.additionalContext = additionalContext;
     }
     async onClick() {
-        const options = {};
+        const options = { forceLeave: true };
         if (this.additionalContext) {
             options.additionalContext = this.additionalContext;
         }

addons/web/static/src/views/form/form_controller.js
@@ -289,7 +289,7 @@ export class FormController extends Component {
         useSetupAction({
             rootRef: this.rootRef,
             beforeVisibilityChange: () => this.beforeVisibilityChange(),
-            beforeLeave: () => this.beforeLeave(),
+            beforeLeave: (options) => this.beforeLeave(options),
             beforeUnload: (ev) => this.beforeUnload(ev),
             getLocalState: () => {
                 return {
@@ -482,8 +482,8 @@ export class FormController extends Component {
         }
     }
 
-    async beforeLeave() {
-        if (this.model.root.dirty && !this.allowLeavingWithoutSaving) {
+    async beforeLeave({ forceLeave } = {}) {
+        if (this.model.root.dirty && !forceLeave && !this.allowLeavingWithoutSaving) {
             return this.save({
                 reload: false,
                 onError: this.onSaveError.bind(this),
@@ -666,9 +666,6 @@ export class FormController extends Component {
     }
 
     saveButtonClicked(params = {}) {
-        if (!("onError" in params)) {
-            params.onError = this.onSaveError.bind(this);
-        }
         return executeButtonCallback(this.ui.activeElement, () => this.save(params));
     }
 

addons/web/static/src/webclient/actions/action_service.js
@@ -73,12 +73,13 @@ const actionRegistry = registry.category("actions");
  * @property {ViewType} [viewType]
  * @property {"replaceCurrentAction" | "replacePreviousAction"} [stackPosition]
  * @property {number} [index]
+ * @property {boolean} [forceLeave]
  */
 
-export async function clearUncommittedChanges(env) {
+export async function clearUncommittedChanges(env, { forceLeave } = {}) {
     const callbacks = [];
     env.bus.trigger("CLEAR-UNCOMMITTED-CHANGES", callbacks);
-    const res = await Promise.all(callbacks.map((fn) => fn()));
+    const res = await Promise.all(callbacks.map((fn) => fn({ forceLeave })));
     return !res.includes(false);
 }
 
@@ -1236,7 +1237,7 @@ export function makeActionManager(env, router = _router) {
         action.path ||= clientAction.path;
         if (clientAction.prototype instanceof Component) {
             if (action.target !== "new") {
-                const canProceed = await clearUncommittedChanges(env);
+                const canProceed = await clearUncommittedChanges(env, pick(options, "forceLeave"));
                 if (!canProceed) {
                     return;
                 }
@@ -1405,7 +1406,7 @@ export function makeActionManager(env, router = _router) {
                 return _executeActURLAction(action, options);
             case "ir.actions.act_window":
                 if (action.target !== "new") {
-                    const canProceed = await clearUncommittedChanges(env);
+                    const canProceed = await clearUncommittedChanges(env, pick(options, "forceLeave"));
                     if (!canProceed) {
                         return new Promise(() => {});
                     }

addons/web/static/tests/views/form/form_view.test.js
@@ -9180,6 +9180,7 @@ test(`multiple clicks on save should reload only once`, async () => {
 });
 
 test(`form view is not broken if save operation fails`, async () => {
+    expect.errors(1);
     onRpc("web_save", ({ args }) => {
         if (args[1].foo === "incorrect value") {
             throw makeServerError();
@@ -9198,6 +9199,7 @@ test(`form view is not broken if save operation fails`, async () => {
     await contains(`.o_form_button_save`).click();
     await animationFrame();
     expect(`.o_dialog`).toHaveCount(1);
+    expect.verifyErrors(["RPC_ERROR: Odoo Server Error"]);
     expect.verifySteps(["web_save"]); // write on save (it fails, does not trigger a read)
 
     await contains(`.o_dialog .modal-footer .btn-primary`).click();
@@ -9256,6 +9258,7 @@ test(`form view is not broken if save operation fails with redirect warning`, as
 
 test.tags("desktop");
 test("Redirect Warning full feature: additional context, action_id, leaving while dirty", async function () {
+    expect.errors(1);
     defineActions([
         {
             id: 1,
@@ -9315,10 +9318,11 @@ test("Redirect Warning full feature: additional context, action_id, leaving whil
     expect.verifySteps(["web_save"]);
 
     await waitFor(".o_error_dialog");
+    expect.verifyErrors(["RPC_ERROR: Odoo Server Error"]);
     expect(".o_error_dialog .btn-primary").toHaveCount(1);
-    expect(".o_error_dialog .btn-secondary").toHaveCount(2);
+    expect(".o_error_dialog .btn-secondary").toHaveCount(1);
 
-    await contains(".o_error_dialog .btn-secondary").click();
+    await contains(".o_error_dialog .btn-primary").click();
     await waitFor(".o_list_view");
     expect.verifySteps(["web_search_read"]);
     expect(".o_breadcrumb").toHaveText("first record\nPartner List");