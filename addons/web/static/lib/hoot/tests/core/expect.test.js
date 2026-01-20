7eabb1b505d192cde6cc02106c18064322b174e3
Julien Mougenot <jum@odoo.com>
2026-01-20 15:46:35 +0000

    [FIX] web: tests - fix type error message in tests
    Before this commit, the error thrown when incorrectly using a matcher
    showed a message with too many commas.
    
    This was because its content was produced with lists that were
    implicitly concatenated, resulting in the extra commas.
    
    This commit fixes these error messages and ensures that their value is
    properly displayed.
    
    X-original-commit: e9dcc44beef0d75ab782bd0d26ed1857a4ce6087
    Part-of: odoo/odoo#246764
    Signed-off-by: Michaël Mattiello (mcm) <mcm@odoo.com>
    Signed-off-by: Julien Mougenot (jum) <jum@odoo.com>

--- addons/web/static/lib/hoot/tests/core/expect.test.js
+++ addons/web/static/lib/hoot/tests/core/expect.test.js
@@ -228,6 +228,20 @@ describe(parseUrl(import.meta.url), () => {
         expect(testResult.events.map(({ label }) => label)).toEqual(matchers.map(([name]) => name));
     });
 
+    test("'expect' error handling", async () => {
+        const [customExpect, hooks] = makeExpect({ headless: true });
+
+        hooks.before();
+
+        expect(() => customExpect(undefined).toInclude("3")).toThrow(
+            "expected received value to be of type string, any[] or object, got undefined"
+        );
+
+        const testResult = hooks.after();
+
+        expect(testResult.pass).toBe(false);
+    });
+
     test("assertions are prevented after an error", async () => {
         const [customExpect, hooks] = makeExpect({ headless: true });
 
