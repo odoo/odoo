5f6adf6ec64840ff7518c698f466442d41c12156
Lucas Perais (lpe) <lpe@odoo.com>
2026-01-20 11:41:56 +0100

    [FIX] tests

---
+++ addons/sale_pdf_quote_builder/tests/test_controllers.py
@@ -0,0 +1,19 @@
+from lxml import html
+from odoo import http
+from odoo.tests import tagged, HttpCase
+from odoo.tools import mute_logger
+
+
+@tagged("-at_install", "post_install")
+class TestUpload(HttpCase):
+    @mute_logger("odoo.addons.sale_pdf_quote_builder.controllers.quotation_document", "odoo.http")
+    def test_wrong_pdf(self):
+        self.authenticate("admin", "admin")
+        data = {
+            'csrf_token': http.Request.csrf_token(self),
+        }
+        files = [('ufile', ('test.pdf', b'test', 'application/pdf'))]
+        resp = self.url_open("/sale_pdf_quote_builder/quotation_document/upload", data=data, files=files)
+        self.assertEqual(resp.status_code, 400)
+        self.assertTrue("It is either encrypted, or encoded in a format we do not support" in resp.text)
+        self.assertTrue(html.fromstring(resp.text) is not None)
