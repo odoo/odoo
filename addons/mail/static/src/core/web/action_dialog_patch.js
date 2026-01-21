2014a25c52aa8e29f9f82ef86f3a8231f89b3561
pmah-odoo <pmah@odoo.com>
2026-01-21 14:32:53 +0530

    [IMP] mail: add size expand/collapse button to full message composer dialog
    Before this commit, the full message composer (`mail.compose.message` form view
    dialog) opened with a large dialog size by default, which was still restrictive
    for comfortably writing long messages.
    
    This commit adds a size expand / collapse toggle button to the full message
    composer dialog, allowing users to switch between the default size and `fs`
    size while composing their message, providing a wider and more comfortable
    layout similar to common email clients.
    
    task-5163977
    
    closes odoo/odoo#242468
    
    Signed-off-by: Mahavir Patel (pmah) <pmah@odoo.com>

---
+++ addons/mail/static/src/core/web/action_dialog_patch.js
@@ -0,0 +1,29 @@
+import { useState } from "@odoo/owl";
+
+import { _t } from "@web/core/l10n/translation";
+import { patch } from "@web/core/utils/patch";
+import { ActionDialog } from "@web/webclient/actions/action_dialog";
+
+patch(ActionDialog.prototype, {
+    setup() {
+        super.setup();
+        this.expanded = useState({ value: false });
+    },
+
+    get canExpand() {
+        const actionProps = this.props.actionProps || {};
+        return actionProps.resModel === "mail.compose.message" && actionProps.type === "form";
+    },
+
+    get size() {
+        return this.expanded.value ? "fs" : super.size;
+    },
+
+    get toggleSizeTitle() {
+        return this.expanded.value ? _t("Compress") : _t("Expand");
+    },
+
+    toggleExpand() {
+        this.expanded.value = !this.expanded.value;
+    },
+});
