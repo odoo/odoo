72dc525f69b28b8cf5d56b52dc59660253a740f3
Pierre Rousseau <pro@odoo.com>
2026-01-21 14:28:01 +0100

    [FIX] spreadsheet: update adaptRanges args
    See https://github.com/odoo/o-spreadsheet/commit/6533266efa
    
    Task: 5095364
    Part-of: odoo/odoo#244981
    Related: odoo/enterprise#105056
    Signed-off-by: Vincent Schippefilt (vsc) <vsc@odoo.com>

--- addons/spreadsheet/static/src/global_filters/plugins/global_filters_core_plugin.js
+++ addons/spreadsheet/static/src/global_filters/plugins/global_filters_core_plugin.js
@@ -108,7 +108,7 @@ export class GlobalFiltersCorePlugin extends OdooCorePlugin {
         }
     }
 
-    adaptRanges(applyChange) {
+    adaptRanges({ applyChange }) {
         for (const filterIndex in this.globalFilters) {
             const filter = this.globalFilters[filterIndex];
             if (filter.type === "text" && filter.rangesOfAllowedValues) {
