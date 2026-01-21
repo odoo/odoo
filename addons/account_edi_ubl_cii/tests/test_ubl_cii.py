999ae72f29432cc36a3084919f9df9a5640ca7b2
roto-odoo <roto@odoo.com>
2026-01-21 14:40:50 +0000

    [IMP] account_edi_ubl_cii: (un)group lines by tax
    Once an invoice is imported, a server action allows the user to group
    lines by tax*, and then if the same action is triggered again it will
    ungroup all lines from the origin file
    
    This feature is useful because accountants don't always need the
    detail of the vendor bills, and also all the lines clutter up the
    journal items
    
    * it's grouped by tax, deferred date and account
    
    task-5047859
    
    X-original-commit: fea28e01f8aba6de3ee18fb37993ba59caac7568
    Part-of: odoo/odoo#249812
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- addons/account_edi_ubl_cii/tests/test_ubl_cii.py
+++ addons/account_edi_ubl_cii/tests/test_ubl_cii.py
@@ -711,3 +711,86 @@ comment-->1000.0</TaxExclusiveAmount></xpath>"""
         # Ensure the created move has 2 attachments: the original XML and a generated PDF
         self.assertEqual(len(bill.attachment_ids), 2)
         self.assertTrue(any('pdf' in attachment.mimetype for attachment in bill.attachment_ids))
+
+    def test_import_and_group_lines_by_tax(self):
+        """
+        Test the group/ungroup lines action on account.move
+        """
+        self.env.ref('base.EUR').active = True
+        tax_16 = self.env["account.tax"].create({
+            'name': '16 %',
+            'amount_type': 'percent',
+            'type_tax_use': 'purchase',
+            'amount': 16.0,
+        })
+        tax_21 = self.env["account.tax"].create({
+            'name': '21 %',
+            'amount_type': 'percent',
+            'type_tax_use': 'purchase',
+            'amount': 21.0,
+        })
+
+        file_path = "bis3_bill_group_by_tax.xml"
+        file_path = f"{self.test_module}/tests/test_files/{file_path}"
+        with file_open(file_path, 'rb') as file:
+            xml_attachment = self.env['ir.attachment'].create({
+                'mimetype': 'application/xml',
+                'name': 'bis3_bill_group_by_tax.xml',
+                'raw': file.read(),
+            })
+        bill = self._import_as_attachment_on(attachment=xml_attachment, journal=self.company_data["default_journal_purchase"])
+
+        # Should group lines by tax
+        bill.action_group_ungroup_lines_by_tax()
+        self.assertRecordValues(bill.invoice_line_ids, [
+            {
+                'quantity': 1.0,
+                'price_unit': 600.0,
+                'price_subtotal': 600.0,
+                'price_total': 696.00,
+                'tax_ids': tax_16.ids,
+            },
+            {
+                'quantity': 1.0,
+                'price_unit': 1300.0,
+                'price_subtotal': 1300.0,
+                'price_total': 1573.00,
+                'tax_ids': tax_21.ids,
+            },
+        ])
+        self.assertRecordValues(bill, [{
+            'amount_untaxed': 1900.0,
+            'amount_tax': 369,
+            'amount_total': 2269.00,
+        }])
+
+        # Should ungroup lines from xml
+        bill.action_group_ungroup_lines_by_tax()
+        self.assertRecordValues(bill.invoice_line_ids, [
+            {
+                'quantity': 1.0,
+                'price_unit': 600.0,
+                'price_subtotal': 600.0,
+                'price_total': 696.00,
+                'tax_ids': tax_16.ids,
+            },
+            {
+                'quantity': 1.0,
+                'price_unit': 300.0,
+                'price_subtotal': 300.0,
+                'price_total': 363.00,
+                'tax_ids': tax_21.ids,
+            },
+            {
+                'quantity': 2.0,
+                'price_unit': 500.0,
+                'price_subtotal': 1000.0,
+                'price_total': 1210.00,
+                'tax_ids': tax_21.ids,
+            },
+        ])
+        self.assertRecordValues(bill, [{
+            'amount_untaxed': 1900.0,
+            'amount_tax': 369,
+            'amount_total': 2269.00,
+        }])
