d99c07a594263c40f8838f2bdf63b1d8a6c60cb1
Arthur Nanson <artn@odoo.com>
2026-01-21 08:34:31 +0000

    [FIX] point_of_sale: display correct combo price with multiple quantity
    **Steps to reproduce:**
    - Open the PoS
    - Select a combo and choose whatever
    - Use the numpad or keyboard to set the combo's qty to 10
    - Choose another customer or preset
    - The price is all wrong
    
    **why the fix:**
    Whenever we change the customer or the preset, the
    **setPriceList** function is triggered. In this
    function we recompute the combo's children lines' price.
    
    Before this commit, we assumed that the combo's parent
    line's qty would always be one, and the logic was written
    on this assumption. Meaning that when it's manually
    changed, the data is wrong. What really happens is that
    most of the children line's qty end up being treated as
    an extra price.
    
    This happens because in our exemple, the parent and children
    lines have a qty of 10, but we only set the free qty based
    on the assumption that the parent line has a qty of 1 on those
    lines https://github.com/odoo/odoo/blob/f5d5783b6a0c908127aa620ad0ec5b0008d7adf4/addons/point_of_sale/static/src/app/models/pos_order.js#L482-L485
    
    This means that, as the free qty is rapidly depleted,
    we fall back on the extra products, and their unit price
    end up becoming the combo's base_price.
    
    We now set the right amount in the free qty based
    on the parent line's qty.
    
    When doing this, another problem arises, we have
    to multiply the children's unit price by the parent's
    qty, as theunit price's computation was based on the
    fact that weonly have a qty of one on the parent line.
    This is because we were still basing the unit_price
    the the parent's lstPrice, but this price was not
    using the parent's qty at all, so we need to
    multiply it by the parent line's qty to make it
    work.
    
    The unit price handling has to be done in the
    **computeComboItems** function, as we also need to
    adjust the remaining price accordingly. An access
    to the parent's qty was added onto the child line,
    to avoid adding a default parameter to the function,
    which should be avoided in stable if possible.
    
    opw-5266483
    
    X-original-commit: fa836e815da25ce89b30386d823b84a11400b3c2

--- addons/point_of_sale/static/src/app/models/pos_order.js
+++ addons/point_of_sale/static/src/app/models/pos_order.js
@@ -390,7 +390,7 @@ export class PosOrder extends PosOrderAccounting {
         for (const cLine of pLine.combo_line_ids) {
             if (!(cLine.combo_item_id.combo_id.id in comboRemainingFree)) {
                 comboRemainingFree[cLine.combo_item_id.combo_id.id] =
-                    cLine.combo_item_id.combo_id.qty_free;
+                    cLine.combo_item_id.combo_id.qty_free * pLine.qty;
             }
             const newQty = comboRemainingFree[cLine.combo_item_id.combo_id.id] - cLine.qty;
             const baseData = { combo_item_id: cLine.combo_item_id };
@@ -400,7 +400,7 @@ export class PosOrder extends PosOrderAccounting {
             if (cLine.qty) {
                 if (newQty >= 0) {
                     comboRemainingFree[cLine.combo_item_id.combo_id.id] = newQty;
-                    childLineFree.push({ ...baseData, qty: cLine.qty });
+                    childLineFree.push({ ...baseData, qty: cLine.qty, parentQty: pLine.qty });
                 } else {
                     childLineExtra.push({ ...baseData, qty: cLine.qty });
                 }
