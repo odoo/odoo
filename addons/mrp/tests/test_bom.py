4919cc401ee35dd3d344d47ba25285ff485cb9cb
Youssef (abyo) <abyo@odoo.com>
2026-01-22 10:43:56 +0100

    [IMP] mrp: adapt status column in BoM overview
    For a cleaner bom overview forecast:
    - Status is adapted to replace availability when
    applicable
    - Availability column was removed to make it less
    crowded
    - Phantom boms now have improved status computation
    - Clickable tags to show the forecast
    - Forecasted quantity is used for all status computation,
    previously it was only used for components not BoM lines,
    which caused inaccurate calculation because it wasn't
    considering the quantity incoming or outgoing.
    - Adapt tests to the new changes.
    
    Additonally, existing bugs with the status (or availability) were
    fixed within this commit as well.
    
    Task: 5035254

--- addons/mrp/tests/test_bom.py
+++ addons/mrp/tests/test_bom.py
@@ -5,7 +5,7 @@ from odoo import exceptions, fields
 from odoo.exceptions import UserError
 from odoo.fields import Command
 from odoo.tests import Form, HttpCase, freeze_time, tagged
-from odoo.tools import float_compare, float_repr, float_round
+from odoo.tools import float_compare, float_repr, float_round, format_date
 
 from odoo.addons.mrp.tests.common import TestMrpCommon
 
@@ -2761,8 +2761,7 @@ class TestBoM(TestMrpCommon):
 
     def test_bom_overview_forecasted_component_status(self):
         """This test case is for verifying that BoM overview availability respects forecasted stock and future
-        replenishments, and ensures that when the availability state is 'expected', the status column remains empty
-        (not 'To Order').
+        replenishments, and ensures that when the availability state is 'expected', the status column shows "Expected + date".
         """
         main_bom = self.make_bom(self.productA, self.productB)
         self.env['mrp.production'].create({
@@ -2775,11 +2774,12 @@ class TestBoM(TestMrpCommon):
 
         comp_line = self._get_component_line(main_bom, self.productB)
         self.assertEqual(comp_line['availability_state'], 'expected')
-        self.assertFalse(comp_line.get('status'))  # Empty status when availability is 'expected'
+        expected_date = f"Expected {format_date(self.env, fields.Date.today() + timedelta(days=comp_line['availability_delay']))}"
+        self.assertEqual(comp_line.get('status'), expected_date)  # Status = "Expected" + today's date + delay
 
         comp_line = self._get_component_line(main_bom, self.productB, qty=2)
         self.assertEqual(comp_line['availability_state'], 'unavailable')  # Reserved for the MO
-        self.assertEqual(comp_line.get('status'), '1.00 To Order')
+        self.assertEqual(comp_line.get('status'), 'Not Available')
 
     def test_bom_with_operations_for_kit_variant(self):
         """
--- addons/mrp_subcontracting/report/mrp_report_bom_structure.py
+++ addons/mrp_subcontracting/report/mrp_report_bom_structure.py
@@ -98,14 +98,14 @@ class ReportMrpReport_Bom_Structure(models.AbstractModel):
         return res
 
     @api.model
-    def _get_quantities_info(self, product, bom_uom, product_info, parent_bom=False, parent_product=False):
-        quantities_info = super()._get_quantities_info(product, bom_uom, product_info, parent_bom, parent_product)
+    def _get_quantities_info(self, product, bom_uom, product_info, bom=False, parent_bom=False, parent_product=False):
+        quantities_info = super()._get_quantities_info(product, bom_uom, product_info, bom, parent_bom, parent_product)
         if parent_product and parent_bom and parent_bom.type == 'subcontract' and product.is_storable:
             route_info = product_info.get(parent_product.id, {}).get(parent_bom.id, {})
             if route_info and route_info['route_type'] == 'subcontract':
                 subcontracting_loc = route_info['supplier'].partner_id.property_stock_subcontractor
                 subloc_product = product.with_context(location=subcontracting_loc.id, warehouse_id=False)
-                subloc_product.fetch(['free_qty', 'qty_available'])
+                subloc_product.fetch(['free_qty', 'qty_available', 'virtual_available'])
                 stock_loc = f"subcontract_{subcontracting_loc.id}"
                 if not product_info[product.id]['consumptions'].get(stock_loc, False):
                     product_info[product.id]['consumptions'][stock_loc] = 0
@@ -113,6 +113,7 @@ class ReportMrpReport_Bom_Structure(models.AbstractModel):
                 quantities_info['free_qty'] = quantities_info['free_to_manufacture_qty']
                 quantities_info['on_hand_qty'] = product.uom_id._compute_quantity(subloc_product.qty_available, bom_uom)
                 quantities_info['stock_loc'] = stock_loc
+                quantities_info['forecasted_qty'] = subloc_product.virtual_available
 
         return quantities_info
 
