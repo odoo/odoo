61347d4b25ebab78b54526ec43a8e7258a6f7b79
Adnan Saiyed <adsa@odoo.com>
2026-01-22 13:08:50 +0000

    [FIX] html_editor: focus editable after deleting column
    Current behavior before commit:
    
    When a user deletes a row or column from table menu, the editor
    loses focus. As a result, actions like Undo do not behave
    as expected and require multiple attempts to restore the
    original table state. This breaks the editing flow, causes
    confusion when performing table-related actions.
    
    Desired behavior after commit:
    
    This commit ensures that editable is focused after deleting
    row or column from table menu. This commit also ensures that
    cursor moves to the end of sibling cell (in case of removing
    column) or to the end of first cell of sibling row (in case
    of removing row) after removing column or row respectively.
    
    task-5725593
    
    X-original-commit: 9b6d6d722bc10ed11180e99f049c89ba0fd27e7b

--- addons/html_editor/static/tests/delete/backward.test.js
+++ addons/html_editor/static/tests/delete/backward.test.js
@@ -1864,7 +1864,7 @@ describe("Selection not collapsed", () => {
             stepFunction: deleteBackward,
             contentAfter: unformat(
                 `<table><tbody>
-                    <tr><td>[]ef</td><td>gh</td></tr>
+                    <tr><td>ef[]</td><td>gh</td></tr>
                 </tbody></table>`
             ),
         });
@@ -1881,7 +1881,7 @@ describe("Selection not collapsed", () => {
             stepFunction: deleteBackward,
             contentAfter: unformat(
                 `<table><tbody>
-                    <tr><td>[]cd</td></tr>
+                    <tr><td>cd[]</td></tr>
                     <tr><td>gh</td></tr>
                 </tbody></table>`
             ),
