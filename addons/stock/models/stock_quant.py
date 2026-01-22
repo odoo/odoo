2368397b14d5b4a2e47833ab2ef5567c07b7af96
reka-odoo <reka@odoo.com>
2026-01-22 12:15:39 +0000

    [FIX] stock: prevent error when setting inventory quantity to 0
    When a user sets the inventory quantity to 0 on a quant while the product’s
    Inventory Location is unset, a traceback is raised.
    
    Steps to reproduce the error:
    - Install "stock" module with demo data
    - Open "Cabinet with Doors" product
    - In Inventory tab, unset Inventory Location > Open forecast report >
      click the On Hand quantity
    - Select the quant > Actions > Set to 0
    
    Traceback:
    'ValueError: NotNullViolation('null value in column "location_dest_id"
    of relation "stock_move" violates not-null constraint'
    
    https://github.com/odoo/odoo/blob/bc790e13ddf3ceacead40cc6ff8d27f1a5f5364d/addons/stock/models/stock_quant.py#L1005-L1016
    When property_stock_inventory is unset, the
    "_get_inventory_move_values" method assigns a NULL value to
    "location_dest_id" in "move_vals".
    As a result, creating the stock move with a NULL "location_dest_id"
    leads to the above traceback.
    
    sentry-7117991902
    
    closes odoo/odoo#252591
    
    X-original-commit: 51cc1523d70838ed996740bd20dbaa262e607777
    Signed-off-by: Tiffany Chang (tic) <tic@odoo.com>
    Signed-off-by: Renilkumar Kajavadra (reka) <reka@odoo.com>

--- addons/stock/models/stock_quant.py
+++ addons/stock/models/stock_quant.py
@@ -997,22 +997,31 @@ class StockQuant(models.Model):
         # Consider the inventory_quantity as set => recompute the inventory_diff_quantity if needed
         self.inventory_quantity_set = True
         move_vals = []
+        default_loss_locations = {}
+        quants_with_missing_loss_locations = self.filtered(lambda quant: not quant.product_id.with_company(quant.company_id).property_stock_inventory)
+        if quants_with_missing_loss_locations:
+            for company in quants_with_missing_loss_locations.mapped('company_id'):
+                loss_location_id = self.env['ir.default'].with_company(company)._get_model_defaults(
+                    'product.template').get('property_stock_inventory')
+                default_loss_locations[company.id] = self.env['stock.location'].browse(loss_location_id)
         for quant in self:
             # if inventory applied from product's inverse_qty and the inventory_diff_quantity is 0,
             # we skip creating a move with 0 quantity.
             if quant.env.context.get('from_inverse_qty') and quant.product_uom_id.compare(quant.inventory_diff_quantity, 0) == 0:
                 continue
+            inventory_location = quant.product_id.with_company(quant.company_id).property_stock_inventory or\
+                default_loss_locations.get(quant.company_id.id)
             # Create and validate a move so that the quant matches its `inventory_quantity`.
             if quant.product_uom_id.compare(quant.inventory_diff_quantity, 0) > 0:
                 move_vals.append(
                     quant._get_inventory_move_values(quant.inventory_diff_quantity,
-                                                     quant.product_id.with_company(quant.company_id).property_stock_inventory,
+                                                     inventory_location,
                                                      quant.location_id, package_dest_id=quant.package_id))
             else:
                 move_vals.append(
                     quant._get_inventory_move_values(-quant.inventory_diff_quantity,
                                                      quant.location_id,
-                                                     quant.product_id.with_company(quant.company_id).property_stock_inventory,
+                                                     inventory_location,
                                                      package_id=quant.package_id))
         moves = self.env['stock.move'].with_context(inventory_mode=False).create(move_vals)
         moves.with_context(ignore_dest_packages=True)._action_done()
