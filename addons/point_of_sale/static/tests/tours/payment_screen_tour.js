01da96440243efcec03d14c73cf2942951d5d83f
Renaud Thiry <reth@odoo.com>
2026-01-22 14:30:41 +0100

    [FIX] *: replace "Deco Addict" with "Acme Corporation"
    A company that happens to be named "Deco Addict" has complained some of
    our users thought they had business with them due to test and demo data
    containing that name.
    
    It will now be named Acme Corporation.
    
    task-5865502
    
    Part-of: odoo/odoo#245500
    Related: odoo/enterprise#105330
    Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>
    Signed-off-by: Renaud Thiry (reth) <reth@odoo.com>

--- addons/point_of_sale/static/tests/tours/payment_screen_tour.js
+++ addons/point_of_sale/static/tests/tours/payment_screen_tour.js
@@ -194,7 +194,7 @@ registry.category("web_tour.tours").add("InvoiceShipLaterAccessRight", {
             ProductScreen.clickHomeCategory(),
             ProductScreen.addOrderline("Whiteboard Pen", "1"),
             ProductScreen.clickPartnerButton(),
-            ProductScreen.clickCustomer("Deco Addict"),
+            ProductScreen.clickCustomer("Acme Corporation"),
             ProductScreen.clickPayButton(),
 
             PaymentScreen.clickPaymentMethod("Cash"),
