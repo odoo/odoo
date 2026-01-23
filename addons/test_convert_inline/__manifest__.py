661c2fecde291f1b2a269859618b20e3e9f14d82
abd-msyukyu-odoo <abd@odoo.com>
2026-01-23 11:17:09 +0100

    WIP

---
+++ addons/test_convert_inline/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Debug Convert Inline',
+    'version': '1.0',
+    'sequence': 9999,
+    'category': 'Hidden',
+    'depends': [
+        'mass_mailing',
+    ],
+    'data': [],
+    'assets': {
+        'web.assets_backend': [
+            
+        ],
+    },
+    'installable': True,
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
