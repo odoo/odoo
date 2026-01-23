9086c7386a615f1834ac07aeca8b9e10f3c0550b
Laurent Smet (LAS) <las@odoo.com>
2026-01-23 15:42:42 +0100

    [FIX] account_edi_ubl_cii: Missing schemeID for Belgium Company registry
    1) This commit adds the new generic methods to build an UBL file.
    
    The big objective is to remove the hierarchy of UBL later on and make BIS3
    completely standalone. In this commit, BIS3 is not independent of UBL 2.1 & 2.0.
    
    2) Missing schemeID for Belgium Company registry
    
    Since https://github.com/odoo/odoo/commit/d40652c17b43b6a9fd4d19ce84b943b2a51fad9c,
    in Belgium, we set by default the company registry.
    However the schemeID is missing and should be part of the ISO 6523 ICD list:
    https://docs.peppol.eu/poacc/billing/3.0/codelist/ICD/
    
    3) Fix PartyLegalEntity when the country is NL and there is no company registry/EAS/Endpoint
    
    That case was raising a traceback before this commit.
    
    closes odoo/odoo#246377
    
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- addons/account_edi_ubl_cii/models/account_edi_xml_ubl_a_nz.py
+++ addons/account_edi_ubl_cii/models/account_edi_xml_ubl_a_nz.py
@@ -98,15 +98,6 @@ class AccountEdiXmlUBLANZ(models.AbstractModel):
     # EXPORT: New (dict_to_xml) helpers
     # -------------------------------------------------------------------------
 
-    def _ubl_default_tax_category_grouping_key(self, base_line, tax_data, vals, currency):
-        # EXTENDS account.edi.xml.ubl_bis3
-        grouping_key = super()._ubl_default_tax_category_grouping_key(base_line, tax_data, vals, currency)
-        if not grouping_key:
-            return
-
-        grouping_key['scheme_id'] = 'GST'
-        return grouping_key
-
     def _ubl_get_line_allowance_charge_discount_node(self, vals, discount_values):
         # EXTENDS account.edi.xml.ubl_bis3
         discount_node = super()._ubl_get_line_allowance_charge_discount_node(vals, discount_values)
@@ -138,29 +129,57 @@ class AccountEdiXmlUBLANZ(models.AbstractModel):
         super()._add_invoice_header_nodes(document_node, vals)
         document_node['cbc:CustomizationID'] = {'_text': self._get_customization_ids()['ubl_a_nz']}
 
-    def _get_party_node(self, vals):
-        # EXTENDS account.edi.xml.ubl_bis3
-        party_node = super()._get_party_node(vals)
-        partner = vals['partner']
+    def _ubl_add_party_endpoint_id_node(self, vals):
+        # EXTENDS
+        super()._ubl_add_party_endpoint_id_node(vals)
+        partner = vals['party_vals']['partner']
         commercial_partner = partner.commercial_partner_id
 
         if commercial_partner.country_code == 'AU' and commercial_partner.vat:
             vat = commercial_partner.vat.replace(" ", "")
-            party_node['cbc:EndpointID']['_text'] = vat
-            party_node['cac:PartyTaxScheme'][0]['cbc:CompanyID']['_text'] = vat
-            party_node['cac:PartyLegalEntity']['cbc:CompanyID'] = {
-                '_text': vat,
-                'schemeID': '0151',
-            }
+            vals['party_node']['cbc:EndpointID']['_text'] = vat
+        elif commercial_partner.country_code == 'NZ' and commercial_partner.company_registry:
+            vals['party_node']['cbc:EndpointID']['_text'] = commercial_partner.company_registry
 
-        elif commercial_partner.country_code == 'NZ':
-            party_node['cbc:EndpointID']['_text'] = commercial_partner.company_registry
-            party_node['cac:PartyTaxScheme'][0]['cbc:CompanyID']['_text'] = commercial_partner.company_registry
-            party_node['cac:PartyLegalEntity']['cbc:CompanyID'] = {
-                '_text': commercial_partner.company_registry,
-                'schemeID': '0088',
-            }
+    def _ubl_add_party_tax_scheme_nodes(self, vals):
+        # EXTENDS
+        super()._ubl_add_party_tax_scheme_nodes(vals)
+        partner = vals['party_vals']['partner']
+        commercial_partner = partner.commercial_partner_id
 
-        party_node['cac:PartyTaxScheme'][0]['cac:TaxScheme']['cbc:ID']['_text'] = 'GST'
+        if (
+            (commercial_partner.country_code == 'AU' and commercial_partner.vat)
+            or (commercial_partner.country_code == 'NZ' and commercial_partner.company_registry)
+        ):
+            vals['party_node']['cac:PartyTaxScheme'] = [{
+                'cbc:CompanyID': {
+                    '_text': vals['party_node']['cbc:EndpointID']['_text'],
+                    'schemeID': None,
+                },
+                'cac:TaxScheme': {
+                    'cbc:ID': {'_text': 'GST'},
+                },
+            }]
 
-        return party_node
+    def _ubl_add_party_legal_entity_nodes(self, vals):
+        # EXTENDS
+        super()._ubl_add_party_legal_entity_nodes(vals)
+        partner = vals['party_vals']['partner']
+        commercial_partner = partner.commercial_partner_id
+
+        if commercial_partner.country_code == 'AU' and commercial_partner.vat:
+            vals['party_node']['cac:PartyLegalEntity'] = [{
+                'cbc:RegistrationName': {'_text': commercial_partner.name},
+                'cbc:CompanyID': {
+                    '_text': vals['party_node']['cbc:EndpointID']['_text'],
+                    'schemeID': '0151',
+                },
+            }]
+        elif commercial_partner.country_code == 'NZ' and commercial_partner.company_registry:
+            vals['party_node']['cac:PartyLegalEntity'] = [{
+                'cbc:RegistrationName': {'_text': commercial_partner.name},
+                'cbc:CompanyID': {
+                    '_text': vals['party_node']['cbc:EndpointID']['_text'],
+                    'schemeID': '0088',
+                },
+            }]
