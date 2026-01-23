0e78448783b8b35c1e1f9b7dfba51c6709c76735
Walid (wasa) <wasa@odoo.com>
2026-01-23 12:59:20 +0000

    [FIX] html_editor: replace empty or fully selected links on paste
    Problem:
    When pasting text or a image URL inside an empty link, or when the link
    is fully selected, the link is not removed and replaced by the pasted
    content. Instead, the text or URL is inserted inside the link, which is
    not the intended behavior.
    
    Cause:
    When pasting text or a image URL, and a command is triggered from the
    powerbox via `openPowerboxOnUrlPaste`, we call `addStep` after the
    content insertion. This step not only inserts the content but also
    removes the link if it was fully selected (via `before_paste_handlers`).
    
    However, `onApplyCommand` then restores the state to before the content
    insertion and link removal. The command (e.g. “Embed Image”) is executed
    on that restored state, where the selection is collapsed inside the
    link. As a result, the inserted content ends up inside the link instead
    of replacing it.
    
    Solution:
    In this situation, fully select the link before inserting the content.
    This allows the `insert` logic to correctly replace the link with the
    pasted text or URL.
    
    task-5386862
    
    X-original-commit: efd0c84d836731cd989b697c2fe717cde5b00bcc

--- addons/html_editor/static/src/core/clipboard_plugin.js
+++ addons/html_editor/static/src/core/clipboard_plugin.js
@@ -199,8 +199,9 @@ export class ClipboardPlugin extends Plugin {
         }
         ev.preventDefault();
 
-        this.dispatchTo("before_paste_handlers", selection, ev);
         this.dependencies.history.stageSelection();
+
+        this.dispatchTo("before_paste_handlers", selection, ev);
         // refresh selection after potential changes from `before_paste` handlers
         selection = this.dependencies.selection.getEditableSelection();
 
