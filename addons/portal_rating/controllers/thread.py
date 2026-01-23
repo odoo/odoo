a8543e02c928358620a0928313ba6322dfe11707
Maryam Kia <maki@odoo.com>
2026-01-23 11:18:48 +0100

    [IMP] portal, *: merge portal chatter controllers
    *: portal_rating, project
    
    Since #214792, the `PortalChatter` has been changed so that it inherits
    `ThreadController` instead of `http.Controller`. The original change was made to
     the stable, so the controllers were kept intact.
    
    Both `PortalChatter` and `PortalThreadController` inherit from ThreadController.
     With this change, the two were merged as `PortalThreadController`.
    `portal_message_update_is_internal` is dead code so it's been removed rather
    than moved.
    
    closes odoo/odoo#245495
    
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/portal_rating/controllers/thread.py
+++ addons/portal_rating/controllers/thread.py
@@ -1,8 +1,11 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 from odoo.fields import Domain
-from odoo.addons.portal.controllers import portal_thread
+
+from odoo.addons.portal.controllers.thread import PortalThreadController
 
 
-class PortalChatter(portal_thread.PortalChatter):
+class PortalRatingThreadController(PortalThreadController):
     def _get_non_empty_message_domain(self):
         return super()._get_non_empty_message_domain() | Domain("rating_value", "!=", False)
 
