ee5eb1857421a565d5e65ec4f1b0511cf82640ba
plha-odoo <plha@odoo.com>
2026-01-23 18:46:52 +0000

    [FIX] purchase_stock: use manual invoice currency rate in valuation
    **Problem:**
    The value of a move does not take the correct currency rate if it
    was changed manually on the invoice.
    
    **Steps to reproduce:**
    - make sure dollar is your main currency
    - activate euro and set a rate of 1 USD -> 2 euros
    - create an avco perpetual storable product with no tax
    - confirm a purchase order for 1 product at 100 euros with no tax
    - validate move
    - create a bill for this purchase order
    - set the currency to euro
    - below 'EUR' change the currency to 4
    (the bug also happens if you open the widget calendar and
    select a date with another rate than the invoice date)
    - in the journal items tab you should see the value going
    from 50 to 25 for the account move lines.
    - confirm the invoice
    - open stock and search your product
    
    **Current behavior:**
    the unit price is 50
    
    **Expected behavior:**
    it should be 25
    
    **Cause of the issue:**
    In _get_value_from_account_move when computing the value,
    we convert the value of the account move line using the _convert
    method on the currency at the date of the
    invoice, but if the rate was changed manually this is no longer correct.
    https://github.com/odoo/odoo/blob/4ad397e94b4661cd64515e137b3e650dc330269c/addons/purchase_stock/models/stock_move.py#L170
    
    **fix**
    When manually changing the rate on the widget, the account move lines
    are updated using currency_rate
    https://github.com/odoo/odoo/blob/8d7194128b26a09be75bf5bfe4e63c8bbfaac69f/addons/account/models/account_move_line.py#L1634
    Using the same compution for the valuation of the move ensures
    alignment.
    
    currency_rate is computed based on invoice_currency_rate which
    is updated in case the rate was set manually.
    https://github.com/odoo/odoo/blob/4ad397e94b4661cd64515e137b3e650dc330269c/addons/account/models/account_move_line.py#L726
    
    opw-5416654
    
    X-original-commit: 1dcff5f39ebaea59a2768819373090f7e9f5f8dd

--- addons/purchase_stock/models/stock_move.py
+++ addons/purchase_stock/models/stock_move.py
@@ -181,10 +181,10 @@ class StockMove(models.Model):
             aml_ids.add(aml.id)
             if aml.move_type == 'in_invoice':
                 aml_quantity += aml.product_uom_id._compute_quantity(aml.quantity, self.product_id.uom_id)
-                value += aml.currency_id._convert(aml.price_subtotal, self.company_id.currency_id, date=aml.date)
+                value += aml.company_id.currency_id.round(aml.price_subtotal / aml.currency_rate)
             elif aml.move_type == 'in_refund':
                 aml_quantity -= aml.product_uom_id._compute_quantity(aml.quantity, self.product_id.uom_id)
-                value -= aml.currency_id._convert(aml.price_subtotal, self.company_id.currency_id, date=aml.date)
+                value -= aml.company_id.currency_id.round(aml.price_subtotal / aml.currency_rate)
 
         if aml_quantity <= 0:
             return valuation_data
