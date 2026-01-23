dc0f52b820c6e73efe8e50396329e04ba436f5fb
Jugurtha <jugac@odoo.com>
2026-01-23 17:38:42 +0000

    [IMP] account_edi_ubl_cii: Generate PDF when missing on XML vendor bills
    Some vendor bills received via Peppol as XML do not include an attached PDF, preventing
    users from previewing the document and verifying the values parsed by Odoo.
    
    This commit generates and attaches a PDF when the XML does not provide one.
    
    task-5500236
    
    closes odoo/odoo#249532
    
    X-original-commit: 67d07e41a7db240071cba6367add953ac7eff961
    Related: odoo/enterprise#108225
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

--- addons/account_edi_ubl_cii/__manifest__.py
+++ addons/account_edi_ubl_cii/__manifest__.py
@@ -25,6 +25,7 @@ Pro rules and show the errors.
     'data': [
         'data/cii_22_templates.xml',
         'views/res_partner_views.xml',
+        'report/account_edi_ubl_cii_report_templates.xml',
     ],
     'assets': {
         'web.assets_backend': [
