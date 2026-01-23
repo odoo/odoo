49ea728a2bfd62ec0c4f4ed2f892c10d3a803729
Victorien (viso) <viso@odoo.com>
2026-01-23 14:42:05 +0100

    [FIX] purchase: suggest the right account for downpayments
    Steps to reproduce:
    1/ install purchase and accountant
    2/ create and setup an expense account dedicated to your downpayments
    (typically code 60-, account type "expense"), ACC1.
    3/ setup a service type product named "downpayment"
    4/ set the default expense account on that product to be ACC1.
    5/ create a PO for any product other than the downpayment (PO1). note
    the partner.
    6/ create a bill for the same partner as the one set on PO1. Call it
    BILL1.
    7/ On BILL1, add one invoice line with the "downpayment" product. Set
    a unit price.
    8/ Confirm BILL1 and match it with PO1 via the "bill matching" smart
    button. Add it as a downpayment.
    9/ Back on PO1, receive the products. Create a bill (BILL2).
    
    => The account suggested for the downpayment line in BILL2 will use
    the default expense account instead of ACC1.
    
    This commit proposes a way to automatically suggest the account
    used on the downpayment invoice lines if there is one.
    
    opw-5253877

--- addons/purchase/tests/test_purchase_downpayment.py
+++ addons/purchase/tests/test_purchase_downpayment.py
@@ -8,11 +8,15 @@ from odoo.tests import tagged
 class TestPurchaseDownpayment(TestPurchaseToInvoiceCommon):
 
     def test_downpayment_basic(self):
+        custom_expense_account = self.company_data['default_account_expense'].copy()
+        self.product_order.property_account_expense_id = custom_expense_account
+
         po = self.init_purchase(confirm=False, products=[self.product_order])
         po.order_line.product_qty = 10.0
         po.button_confirm()
 
         dp_bill = self.init_invoice('in_invoice', amounts=[69.00], post=True)
+        dp_bill.invoice_line_ids.account_id = custom_expense_account
 
         match_lines = self.env['purchase.bill.line.match'].search([('partner_id', '=', self.partner_a.id)])
         action = match_lines.action_add_to_po()
@@ -32,9 +36,9 @@ class TestPurchaseDownpayment(TestPurchaseToInvoiceCommon):
 
         self.assertRecordValues(generated_bill.invoice_line_ids, [
             # pylint: disable=C0326
-            {'product_id': self.product_order.id, 'display_type': 'product',      'quantity': 10, 'is_downpayment': False, 'balance': 10.0 * self.product_order.standard_price},
-            {'product_id': False,                 'display_type': 'line_section', 'quantity': 0,  'is_downpayment': True,  'balance': 0.0},
-            {'product_id': False,                 'display_type': 'product',      'quantity': -1, 'is_downpayment': True,  'balance': -69.0},
+            {'product_id': self.product_order.id, 'display_type': 'product',      'quantity': 10, 'is_downpayment': False, 'balance': 10.0 * self.product_order.standard_price, 'account_id': custom_expense_account.id},
+            {'product_id': False,                 'display_type': 'line_section', 'quantity': 0,  'is_downpayment': True,  'balance': 0.0, 'account_id': False},
+            {'product_id': False,                 'display_type': 'product',      'quantity': -1, 'is_downpayment': True,  'balance': -69.0, 'account_id': custom_expense_account.id},
         ])
 
         # Normal flow: New bill with negative down payment line
