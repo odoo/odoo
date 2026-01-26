211eae6ba87979d3603c048c9d8e3c229e7b3377
Mathieu Coutant <mcou@odoo.com>
2026-01-26 15:07:04 +0100

    [FIX] planning, resource, hr_attendance_gantt: return no gantt unavailabilities for flexible calendar
    Issue:
    When an employee has a flexible calendar, its gantt view in planning and other module, like project or time off, shows incorrect grayed out days.
    In general, employees with a flexible calendar should not have any grayed-out days other than public holidays.
    
    Steps to reproduce:
    - Create a new work schedule:
        - Flexible Hours
        - 24 hours per week
        - 8 average hours per day
    - Create an employee with this work schedule
    - Open Planning app
    - Only the three first days of the week are not grayed out
    
    Cause:
    With 24 hours per week, the worked time can be done in three days. Odoo considers that the first three days of the given time frame are worked and the others aren't.
    This result is returned by `_work_intervals_batch()`. In `_unavailable_intervals_batch()` we then build the unavailabilities by taking the inverse of the work intervals. The only exception was for fully flexible resources (i.e. without a calendar).
    
    Solution:
    The case we need to consider is for flexible resources: they are available anytime, all week long except on public holidays.
    
    To do this we fix the method `_unavailable_intervals_batch()` which is then called by each module.
    If the resource is flexible, then we only return the interval of leaves overlapping with the time frame.
    
    Works in `appointment`, `hr_holiday_gantt`and `project_enterprise`.
    
    In `planning`, we need to skip the case where nothing is returned in `leaves_mapping` otherwise it will consider the company_leave during weeks with no public holiday.
    
    In `hr_attendances_gantt`, `_unavailable_intervals_batch()` is not called. So we change the way the unavailabilities were computed to call _unavailable_intervals_batch() instead.
    
    opw-4879481
    
    closes odoo/odoo#245741
    
    Related: odoo/enterprise#105521
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- addons/resource/models/resource_calendar.py
+++ addons/resource/models/resource_calendar.py
@@ -559,7 +559,10 @@ class ResourceCalendar(models.Model):
         resources_work_intervals = self._work_intervals_batch(start_dt, end_dt, resources, domain, tz)
         result = {}
         for resource in resources_list:
-            if resource and resource._is_fully_flexible():
+            if resource and resource._is_flexible():
+                leaves = self._leave_intervals_batch(start_dt, end_dt, resource, domain, tz=tz)
+                if res_leaves := leaves.get(resource.id, []):
+                    result[resource.id] = [(i[0], i[1]) for i in res_leaves]
                 continue
             work_intervals = [(start, stop) for start, stop, meta in resources_work_intervals[resource.id]]
             # start + flatten(intervals) + end
