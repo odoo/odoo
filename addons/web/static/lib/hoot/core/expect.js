b4e71eabe4f72322116550c416c4f25002f8e382
Julien Mougenot <jum@odoo.com>
2026-01-26 14:14:21 +0100

    [IMP] web: tests - 'raw' option for 'toHaveValue'
    This commit introduces a new `raw` option to the `toHaveValue` matcher,
    performing the assertion on the "raw" value (i.e. string) of an input,
    instead of the value retrieved from its inferred type (e.g. 'number'
    inputs returning the numeric value instead of the string).
    
    Part-of: odoo/odoo#244726
    Signed-off-by: Michaël Mattiello (mcm) <mcm@odoo.com>

--- addons/web/static/lib/hoot/core/expect.js
+++ addons/web/static/lib/hoot/core/expect.js
@@ -2059,13 +2059,15 @@ export class Matcher {
      * - contain file objects matching the given `files` list.
      *
      * @param {ReturnType<typeof getNodeValue>} [value]
-     * @param {ExpectOptions} [options]
+     * @param {ExpectOptions & { raw?: boolean }} [options]
      * @example
-     *  expect("input[type=email]").toHaveValue("john@doe.com");
+     *  expect("input[name=age]").toHaveValue(29);
      * @example
      *  expect("input[type=file]").toHaveValue(new File(["foo"], "foo.txt"));
      * @example
      *  expect("select[multiple]").toHaveValue(["foo", "bar"]);
+     * @example
+     *  expect("input[name=age]").toHaveValue("29", { raw: true });
      */
     toHaveValue(value, options) {
         this._ensureArguments(arguments, [
@@ -2082,7 +2084,7 @@ export class Matcher {
         return this._resolve(() => ({
             name: "toHaveValue",
             acceptedType: ["string", "node", "node[]"],
-            mapElements: (el) => getNodeValue(el),
+            mapElements: (el) => getNodeValue(el, options?.raw),
             predicate: (elValue, el) => {
                 if (isCheckable(el)) {
                     throw new HootError(
