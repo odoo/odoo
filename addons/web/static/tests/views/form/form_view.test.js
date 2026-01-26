97b05a0d78dbf31b71daa4ebb514e4a3f11a41b6
Julien Mougenot <jum@odoo.com>
2026-01-26 15:14:04 +0000

    [FIX] web: tests - fix illegal 'verifyErrors'
    Before this commit, `expect.verifyErrors` could be called without
    calling `expect.errors(n)` beforehand.
    
    In most cases, calling `expect.errors` is necessary, as it is that
    instruction which is responsible for smothering actual errors, and
    register them as assertions. On the other hand, errors triggered without
    `expect.errors` are supposed to be breaking, and as such any subsequent
    assertion is simply ignored.
    
    This effectively means that in rare cases, errors (asynchronous and
    non-breaking) were thrown, the following assertions were ignored, and
    because `verifyErrors` was called at the end of the test, the test could
    still pass, e.g.:
    
    ```js
    test("...", async () => {
        expect(true).toBe(true);
    
        Promise.reject("boom");
        await Promise.resolve(); // allow error to be processed
    
        expect(true).not.toBe(true); // falsy assertion -> ignored
    
        expect.verifyErrors(["boom"]); // -> valid
    });
    ```
    
    With this commit, calling `verifyErrors` without having called
    `expect.errors` will now throw a global error, preventing such cases to
    happen.
    
    closes odoo/odoo#246764
    
    X-original-commit: a6fae68cf478547f5127805064feb889b8a0de27
    Signed-off-by: Michaël Mattiello (mcm) <mcm@odoo.com>
    Signed-off-by: Julien Mougenot (jum) <jum@odoo.com>

--- addons/web/static/tests/views/form/form_view.test.js
+++ addons/web/static/tests/views/form/form_view.test.js
@@ -9122,6 +9122,7 @@ test(`form view is not broken if save operation fails with redirect warning`, as
 
 test.tags("desktop");
 test("Redirect Warning full feature: additional context, action_id, leaving while dirty", async function () {
+    expect.errors(1);
     defineActions([
         {
             id: 1,
