2a554101bb546b9a33c740d9d372d6d54aae4a5a
adch-odoo <adch@odoo.com>
2026-01-27 13:59:05 +0530

    [FIX] html_editor: handle empty text nodes inside list item
    Steps to Reproduce:
    
    - Create a list item and type some text.
    - Convert the text into a link.
    - Copy the link.
    - Press Enter and paste the link.
    - Select the entire list using the mouse.
    - Apply font size & observe that font size is not applied to some list items.
    
    Before this commit:
    
    - Creating links inside list items & repeated copy-paste operations left empty
    text nodes (feff cleanup). Manual selection doesn't include these nodes,
    `areNodeContentsFullySelected` reports that list item is not fully selected.
    
    As a result, some list items were not considered fully selected, and font size
    was not applied.
    
    After this commit:
    
    - Empty text nodes are removed before applying font size, ensuring full
    list item selection and consistent font-size application.
    
    task-5454639

--- addons/html_editor/static/src/main/list/list_plugin.js
+++ addons/html_editor/static/src/main/list/list_plugin.js
@@ -2,6 +2,7 @@ import { Plugin } from "@html_editor/plugin";
 import { closestBlock, isBlock } from "@html_editor/utils/blocks";
 import {
     removeClass,
+    removeEmptyTextNodes,
     toggleClass,
     unwrapContents,
     wrapInlinesInBlocks,
@@ -1012,6 +1013,11 @@ export class ListPlugin extends Plugin {
             return;
         }
         for (const list of targetedNodes) {
+            // Remove empty text nodes without breaking the current selection.
+            const cursors = this.dependencies.selection.preserveSelection();
+            removeEmptyTextNodes(list, cursors);
+            cursors.restore();
+
             if (this.dependencies.selection.isNodeContentsFullySelected(list)) {
                 for (const node of descendants(list)) {
                     if (node.nodeType === Node.ELEMENT_NODE && node.style.color) {
@@ -1039,6 +1045,11 @@ export class ListPlugin extends Plugin {
             if ([...descendants(listItem)].some(isBlock)) {
                 continue;
             }
+
+            // Remove empty text nodes without breaking the current selection.
+            const cursors = this.dependencies.selection.preserveSelection();
+            removeEmptyTextNodes(listItem, cursors);
+            cursors.restore();
 
             if (this.dependencies.selection.isNodeContentsFullySelected(listItem)) {
                 for (const node of [listItem, ...descendants(listItem)]) {
