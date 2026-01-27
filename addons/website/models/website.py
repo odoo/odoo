odoo/odoo

commit de302c2d36305c0d7562572a30587641eabfe914

[IMP] core: jsonb company dependent fields
use jsonb for company dependent fields
1. field type
	before: company dependent fields were non-stored and computed
	now:    company dependent fields are stored fields whose column type is
	        jsonb with the format like '{"1": val1}'::jsonb

2. fallback
	before: fallbacks of company dependent fields were stored in
	        ir_property.value_xxx `WHERE res_id IS NULL`
	now:    fallbacks are stored in ir_default.json_value in the format of
	        jsonfied write format

3. many2one
	field.ondelete
		before: only field.ondelete='set null'
		now:    support field.ondelete='set null' and field.ondelete='restrict'
	field.index
		before: no index
		now:    default 'btree_not_null' which index (col IS NOT NULL) for all
		        NOT NULL rows
	reference exists
		before: checked when get data from ir_property
		now:    cleaned when orm `unlink`. As a side effect, if a model is
		        referenced by a many2one company dependent field, this model
		        cannot have any field.ondelete='cascade' field. The developer
		        must override the `unlink` method to do the logical on delete
		        cascade in the ORM

4. _field_to_sql
	before: not supported for company dependent fields
	now:    can be used by company dependent fields like normal fields to
	        generate reports

closes odoo/odoo#175627

Task: 3954610
Related: odoo/enterprise#67925
Related: odoo/upgrade#6366
Related: odoo/upgrade-util#124
Signed-off-by: Chong Wang (cwg) <cwg@odoo.com>

addons/website/models/website.py
@@ -1118,11 +1118,11 @@ class Website(models.Model):
             search_criteria.append((url, website.website_domain()))
 
         # Search the URL in every relevant field
-        html_fields_attributes = self._get_html_fields_attributes() + [
-            ('website.menu', 'website_menu', 'url', False),
+        html_fields = self._get_html_fields() + [
+            ('website.menu', 'url'),
         ]
-        for model, _table, column, _translate in html_fields_attributes:
-            Model = self.env[model]
+        for model_name, field_name in html_fields:
+            Model = self.env[model_name]
             if not Model.has_access('read'):
                 continue
 
@@ -1130,21 +1130,21 @@ class Website(models.Model):
             domains = []
             for url, website_domain in search_criteria:
                 domains.append(AND([
-                    [(column, 'ilike', url)],
+                    [(field_name, 'ilike', url)],
                     website_domain if hasattr(Model, 'website_id') else [],
                 ]))
 
             dependency_records = Model.search(OR(domains))
-            if model == 'ir.ui.view':
+            if model_name == 'ir.ui.view':
                 dependency_records = _handle_views_and_pages(dependency_records)
             if dependency_records:
-                model_name = self.env['ir.model']._display_name_for([model])[0]['display_name']
-                field_name = Model.fields_get()[column]['string']
+                model_name = self.env['ir.model']._display_name_for([model_name])[0]['display_name']
+                field_string = Model.fields_get()[field_name]['string']
                 dependencies.setdefault(model_name, [])
                 dependencies[model_name] += [{
-                    'field_name': field_name,
+                    'field_name': field_string,
                     'record_name': rec.display_name,
-                    'link': 'website_url' in rec and rec.website_url or f'/odoo/{model}/{rec.id}',
+                    'link': 'website_url' in rec and rec.website_url or f'/odoo/{model_name}/{rec.id}',
                     'model_name': model_name,
                 } for rec in dependency_records]
 
@@ -1582,18 +1582,17 @@ class Website(models.Model):
     def _get_cached(self, field):
         return self._get_cached_values()[field]
 
-    def _get_html_fields_attributes_blacklist(self):
+    def _get_html_fields_blacklist(self):
         return (
             'mail.message', 'mail.activity', 'digest.tip',
         )
 
-    def _get_html_fields_attributes(self):
-        html_fields = [('ir.ui.view', 'ir_ui_view', 'arch_db', True)]
+    def _get_html_fields(self):
+        html_fields = [('ir.ui.view', 'arch_db')]
         cr = self.env.cr
         cr.execute("""
             SELECT f.model,
-                   f.name,
-                   f.translate
+                   f.name
               FROM ir_model_fields f
               JOIN ir_model m
                 ON m.id = f.model_id
@@ -1602,14 +1601,20 @@ class Website(models.Model):
                AND m.transient = false
                AND f.model NOT LIKE 'ir.actions%%'
                AND f.model NOT IN %s
-        """, ([self._get_html_fields_attributes_blacklist()]))
-        for model, name, translate in cr.fetchall():
-            table = self.env[model]._table
-            if sqltools.table_exists(cr, table) and sqltools.column_exists(cr, table, name):
-                html_fields.append((model, table, name, translate))
+        """, ([self._get_html_fields_blacklist()]))
+        for model_name, field_name, in cr.fetchall():
+            try:
+                model = self.env[model_name]
+                field = model._fields[field_name]
+                if model._abstract or model._table_query is not None or not field.store:
+                    continue
+            except KeyError:
+                continue
+
+            html_fields.append((model_name, field_name))
         return html_fields
 
-    def _is_snippet_used(self, snippet_module, snippet_id, asset_version, asset_type, html_fields_attributes):
+    def _is_snippet_used(self, snippet_module, snippet_id, asset_version, asset_type, html_fields):
         snippet_occurences = []
         # Check snippet template definition to avoid disabling its related assets.
         # This special case is needed because snippet template definitions do not
@@ -1622,18 +1627,18 @@ class Website(models.Model):
         if self._check_snippet_used(snippet_occurences, asset_type, asset_version):
             return True
 
+        html_fields = [(self.env[model_name], field_name) for model_name, field_name in html_fields]
         # As well as every snippet dropped in html fields
-        result = self.env.execute_query(SQL(" UNION ").join(
-            SQL(
-                "SELECT regexp_matches(%s%s, %s, 'g') FROM %s",
-                SQL.identifier(column),
-                SQL("->>'en_US'") if translate else SQL(),
+        self.env.cr.execute(SQL(" UNION ").join(
+            SQL("SELECT regexp_matches(%s, %s, 'g') FROM %s",
+                model._field_to_sql(model._table, field_name),
                 f'<([^>]*data-snippet="{snippet_id}"[^>]*)>',
-                SQL.identifier(table),
+                SQL.identifier(model._table)
             )
-            for _model, table, column, translate in html_fields_attributes
+            for model, field_name in html_fields
         ))
-        snippet_occurences = [r[0][0] for r in result]
+
+        snippet_occurences = [r[0][0] for r in self.env.cr.fetchall()]
         return self._check_snippet_used(snippet_occurences, asset_type, asset_version)
 
     def _check_snippet_used(self, snippet_occurences, asset_type, asset_version):
@@ -1661,7 +1666,7 @@ class Website(models.Model):
         snippet_re = re.compile(r'(\w*)\/.*\/snippets\/(\w*)\/(\d{3})(?:_\w*)?\.(js|scss)')
         # regex will match /module/static/[.../]/snippets/snippet_id/XXX[_variable].asset_type
         # _variable is not kept since only module, snippet_id, asset_version (XXX), asset_type are relevant
-        html_fields_attributes = self._get_html_fields_attributes()
+        html_fields = self._get_html_fields()
         snippet_used = {}
         for snippet_asset in snippet_assets:
             match = snippet_re.match(snippet_asset.path)
@@ -1672,7 +1677,7 @@ class Website(models.Model):
                 asset_type = 'css'
             key = (snippet_id, asset_version, asset_type)  # module is not relevant, we want the first one in the asset id order to filter module extension
             if key not in snippet_used:
-                snippet_used[key] = self._is_snippet_used(snippet_module, snippet_id, asset_version, asset_type, html_fields_attributes)
+                snippet_used[key] = self._is_snippet_used(snippet_module, snippet_id, asset_version, asset_type, html_fields)
             is_snippet_used = snippet_used[key]
             if is_snippet_used != snippet_asset.active:
                 snippet_asset.active = is_snippet_used

commit 4031b58e78a750c6031a859b081ff8f302a5a32e

[FIX] website: delete dialog links
**Steps to reproduce:**

1. Go to the list view of website pages.
2. Select the page "ContactUs".
3. Click Delete.
4. A warning dialog appears.
5. Unfold one of the lists of records where the page is used.

**Issue**

Clicking on a record link (for example, "View") does not redirect anywhere.
This is due to the use of the model display name in url.
caused by https://github.com/odoo/odoo/commit/de302c2d36305c0d7562572a30587641eabfe914

**Fix**

Use the model_name instead of the display name in the URL.

task-5880458

addons/website/models/website.py
@@ -1314,14 +1314,14 @@ class Website(models.Model):
             if model_name == 'ir.ui.view':
                 dependency_records = _handle_views_and_pages(dependency_records)
             if dependency_records:
-                model_name = self.env['ir.model']._display_name_for([model_name])[0]['display_name']
+                model_display_name = self.env['ir.model']._display_name_for([model_name])[0]['display_name']
                 field_string = Model.fields_get()[field_name]['string']
-                dependencies.setdefault(model_name, [])
-                dependencies[model_name] += [{
+                dependencies.setdefault(model_display_name, [])
+                dependencies[model_display_name] += [{
                     'field_name': field_string,
                     'record_name': rec.display_name,
                     'link': 'website_url' in rec and rec.website_url or f'/odoo/{model_name}/{rec.id}',
-                    'model_name': model_name,
+                    'model_name': model_display_name,
                 } for rec in dependency_records]
 
         return dependencies