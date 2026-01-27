be34ac5d77f0ddcda052f0329fd7e274e01fc915
utma-odoo <utma@odoo.com>
2026-01-27 16:26:24 +0000

    [FIX] sale_pdf_quote_builder: prevent PyPDF warnings by using a real invalid pdf in tests
    The test_wrong_pdf test fails on runbot in the Nightly build when
    executed under Distro Builds > Testing distro PureTrixie without
    requirements for config Parallel testing.
    
    Steps to reproduce:
    - Install sale_pdf_quote_builder.
    - Run test_wrong_pdf (Make sure PyPDF==5.4.0 is installed).
    - Observe PyPDF warnings in the logs.
    
    Warnings:
    WARNING	server	pypdf._reader:435 invalid pdf header: b'test'
    WARNING	server	pypdf._reader:435 EOF marker not found
    WARNING	server	pypdf._reader:435 EOF marker not found
    
    Root cause:
    Odoo officially supports the PyPDF version defined in requirements.txt [1].
    The test passes with PyPDF2, but PyPDF is stricter and emits warnings when
    parsing arbitrary byte content. Because the test uses raw bytes instead of a
    structurally valid (but unsupported or encrypted) PDF, PyPDF logs warnings.
    
    Fix:
    This commit updates the test to use a real encrypted PDF
    instead of raw bytes, similar to [2].
    
    This commit fixes test failures caused by [3].
    
    Note:
    Refer to [4] for generating the encrypted file.
    
    [1]:
    https://github.com/odoo/odoo/blob/a6af7df429f9cd3bfb60347e9b30cc48fb5bfaff/requirements.txt#L63-L65
    
    [2]:
    https://github.com/odoo/odoo/blob/0499dbd8944b4002883afab5d3ed76d8e55dab9c/addons/mail/tests/discuss/test_discuss_attachment_controller.py#L63-L75
    
    [3]:
    https://github.com/odoo/odoo/pull/230712
    
    [4]:
    https://colab.research.google.com/drive/1UfWSKDluObEkKxCg6eX-nHoe04_FGrEq#scrollTo=A5hO11Eh1qvV
    
    runbot-238399
    
    X-original-commit: a3e56dbf499527835ead80d66bef3687be3fd740

--- addons/sale_pdf_quote_builder/tests/test_controllers.py
+++ addons/sale_pdf_quote_builder/tests/test_controllers.py
@@ -1,5 +1,6 @@
 from odoo.tests import HttpCase, tagged
 from odoo.tools import mute_logger
+from odoo.tools.misc import file_open
 
 
 @tagged("-at_install", "post_install")
@@ -9,7 +10,9 @@ class TestUpload(HttpCase):
     def test_wrong_pdf(self):
         self.authenticate("admin", "admin")
         data = {'csrf_token': self.csrf_token()}
-        files = [('ufile', ('test.pdf', b'test', 'application/pdf'))]
+        # Structurally valid but AES-encrypted PDF file (generated with PyPDF)
+        with file_open('sale_pdf_quote_builder/tests/files/test_AES.pdf', 'rb') as f:
+            files = [('ufile', ('test_AES.pdf', f.read(), 'application/pdf'))]
         resp = self.url_open("/sale_pdf_quote_builder/quotation_document/upload", data=data, files=files)
         self.assertEqual(resp.status_code, 422)
         self.assertEqual(resp.headers['Content-Type'], 'application/json; charset=utf-8')
