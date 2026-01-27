ec21e627694e223238974d7a537d13d418076128
Jugurtha <jugac@odoo.com>
2026-01-27 14:10:02 +0000

    [IMP] account_peppol, account_edi_ubl_cii: Make the error message more accurate when sending via Peppol.
    When a user sends a move via Peppol to a customer that has a VAT number set but not a Peppol endpoint
    We show the user a generic error ("no VAT")
    This makes the user confused, as he already filled the VAT field of his customer,
    It's the Peppol VAT that is missing (it could be: Belgian Company Registry, France SIRET, ...etc, depending on the customer's country)
    
    This commit makes the error message more accurate by showing exactly the missing required field.
    
    task-5499707
    
    closes odoo/odoo#263685
    
    X-original-commit: 92fbd723d23f12d34337ad321570615c8234e644
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>
    Signed-off-by: Jugurtha Gaci (jugac) <jugac@odoo.com>

--- addons/account_peppol/wizard/account_move_send_wizard.py
+++ addons/account_peppol/wizard/account_move_send_wizard.py
@@ -18,10 +18,22 @@ class AccountMoveSendWizard(models.TransientModel):
             if peppol_checkbox := wizard.sending_method_checkboxes.get('peppol'):
                 peppol_partner = wizard.move_id.partner_id.commercial_partner_id.with_company(wizard.company_id)
                 peppol_proxy_mode = wizard.company_id._get_peppol_edi_mode()
+                if not peppol_partner.peppol_eas or not peppol_partner.peppol_endpoint:
+                    peppol_partner._compute_peppol_endpoint()  # Try to recompute the Peppol credentials.
+                eas_label = dict(peppol_partner._fields['peppol_eas']._description_selection(self.env)).get(peppol_partner.peppol_eas)
                 if peppol_partner.peppol_verification_state == 'not_valid':
                     addendum_disable_reason = _(' (Customer not on Peppol)')
                 elif peppol_partner.peppol_verification_state == 'not_verified':
-                    addendum_disable_reason = _(' (no VAT)')
+                    # The recomputation of the Peppol credentials did not manage to fill these fields.
+                    addendum_disable_reason = _(' (Customer not on Peppol)')
+                    if not peppol_partner.peppol_eas or not peppol_partner.peppol_endpoint:
+                        if not peppol_partner.vat:
+                            addendum_disable_reason = _(' (no VAT)')
+                        elif eas_label:
+                            addendum_disable_reason = _(
+                                ' (Missing %(eas)s)',
+                                eas=eas_label,
+                            )
                 else:
                     addendum_disable_reason = ''
                 vals_not_valid = {'readonly': True, 'checked': False} if addendum_disable_reason else {}
