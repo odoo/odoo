4bc85f587cafdd9062a0c06f30677a76fed92048
tau-odoo <tau@odoo.com>
2026-01-28 16:04:49 +0800

    [FIX] l10n_tw_edi_ecpay_website_sale: Fix disabled input box for
    carrier type
    
    After the user clicks on "Validate" button to validate the mobile barcode,
    the carrier type selection is disabled and the carrier type pass to the
    SO is None.
    
    This commit fixes the issue by instead of disabling the carrier type
    selection, we just set the input box of carrier number to readonly
    and set back the carrier number to not readonly when the user changes
    the carrier type to ensure the carrier number input is editable.
    
    Also update some labels to be more user readable.
    
    task-5880421
    
    closes odoo/odoo#246053
    
    Signed-off-by: Nicolas Viseur (vin) <vin@odoo.com>

--- addons/l10n_tw_edi_ecpay_website_sale/static/tests/tours/test_checkout.js
+++ addons/l10n_tw_edi_ecpay_website_sale/static/tests/tours/test_checkout.js
@@ -286,3 +286,83 @@ registry.category("web_tour.tours").add("test_checkout_b2b", {
         },
     ],
 });
+
+registry.category("web_tour.tours").add("test_checkout_b2c_mobile_barcode", {
+    url: "/shop",
+    steps: () => [
+        ...tourUtils.addToCart({ productName: "Test Product", expectUnloadPage: true }),
+        tourUtils.goToCart({ quantity: 1 }),
+        {
+            content: "Go to checkout",
+            trigger: "a[name='website_sale_main_button']",
+            run: "click",
+            expectUnloadPage: true,
+        },
+        {
+            trigger: "select[name=country_id]",
+            run: "selectByLabel Taiwan",
+        },
+        {
+            trigger: "input[name=name]",
+            run: "edit Test Customer B2C",
+        },
+        {
+            trigger: "input[name=phone]",
+            run: "edit +886 123 456 789",
+        },
+        {
+            trigger: "input[name=email]",
+            run: "edit test@odoo.com",
+        },
+        {
+            trigger: "input[name=street]",
+            run: "edit Xinyi Road 33",
+        },
+        {
+            trigger: "input[name=zip]",
+            run: "edit 10000",
+        },
+        {
+            trigger: "input[name=city]",
+            run: "edit Taipei",
+        },
+        {
+            trigger: "select[name=l10n_tw_edi_require_paper_format]",
+            run: "selectByLabel No",
+        },
+        {
+            content: "Continue Checkout",
+            trigger: '.btn-primary:contains("Continue checkout")',
+            run: "click",
+            expectUnloadPage: true,
+        },
+        {
+            content: "Confirm",
+            trigger: "a[name='website_sale_main_button']",
+            run: "click",
+            expectUnloadPage: true,
+        },
+        // E-invoice info
+        {
+            trigger: "select[name=l10n_tw_edi_carrier_type]",
+            run: "selectByLabel Mobile Barcode",
+        },
+        {
+            trigger: "input[name=l10n_tw_edi_carrier_number]",
+            run: "edit /1234567",
+        },
+        // Valid mobile barcode, after the fix the l10n_tw_edi_carrier_type should remain "3"
+        {
+            content: "Validate Mobile Barcode",
+            trigger: "#validate_carrier_number",
+            run: "click",
+            expectUnloadPage: false,
+        },
+        {
+            content: "Continue checkout",
+            trigger: "a[name='website_sale_main_button']",
+            run: "click",
+            expectUnloadPage: true,
+        },
+    ],
+});
