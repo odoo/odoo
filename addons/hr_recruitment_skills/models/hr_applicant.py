303208f7816581113e524ff030dbd09d9b80d785
abdrahmanrashed <aras@odoo.com>
2026-01-28 16:06:07 +0100

    [IMP] hr_recruitment: Sync fields between talent and applicants.
    This commit improves the synchronization between talent and applicant by replacing the existing 1 way sync from applicant to talent to a sync between all `hr.applicant`s in the same pool.

--- addons/hr_recruitment_skills/models/hr_applicant.py
+++ addons/hr_recruitment_skills/models/hr_applicant.py
@@ -2,7 +2,7 @@
 
 from ast import literal_eval
 
-from odoo import fields, models, Command, api
+from odoo import Command, api, fields, models
 
 
 class HrApplicant(models.Model):
@@ -91,29 +91,30 @@ class HrApplicant(models.Model):
         ]
         return vals
 
-    def _map_applicant_skill_ids_to_talent_skill_ids(self, vals):
+    def _map_applicant_skill_ids_to_other_applicant_skill_ids(self, other_applicant, vals):
         """
         The applicant_skills_ids contains a list of ORM tuples i.e (command, record ID, {values})
         The challenge lies in the uniqueness of the record ID in this tuple. Each skill (e.g., 'arabic')
         has a distinct ID per applicant, i.e arabic in applicant 1 will have a different id from arabic in
         applicant 2. This means the content of applicant_skills_ids is unique for each record and attempting
         to pass it directly (e.g., applicant.pool_applicant_id.write(vals)) won't yield results so we must
-        update each tuple to have the correct command and record ID for the talent pool applicant
+        update each tuple to have the correct command and record ID for the other applicant
 
+        :param other_applicant: the other @hr.applicant the skill_ids will be mapped to
         :param vals: list of CREATE, WRITE or UNLINK commands with skill_ids relevant to the applicant
-        :return: list of CREATE, WRITE or UNLINK commands with skill_ids relevant to the pool_applicant
+        :return: list of CREATE, WRITE or UNLINK commands with skill_ids relevant to the other applicant
         """
         applicant_skills = {a.id: a.skill_id.id for a in self.applicant_skill_ids}
         applicant_skills_type = {a.id: a.skill_type_id.id for a in self.applicant_skill_ids}
-        talent_skills = {a.skill_id.id: a.id for a in self.pool_applicant_id.applicant_skill_ids}
+        other_applicant_skills = {a.skill_id.id: a.id for a in other_applicant.applicant_skill_ids}
         mapped_commands = []
         for command in vals.get("applicant_skill_ids"):
             command_number = command[0]
             record_id = command[1]
             if command_number == Command.UPDATE:
                 values = command[2]
-                if applicant_skills[record_id] in talent_skills:
-                    mapped_command = Command.update(talent_skills[applicant_skills[record_id]], values)
+                if applicant_skills[record_id] in other_applicant_skills:
+                    mapped_command = Command.update(other_applicant_skills[applicant_skills[record_id]], values)
                     mapped_commands.append(mapped_command)
                 else:
                     mapped_command = Command.create(
@@ -125,8 +126,8 @@ class HrApplicant(models.Model):
                     )
                     mapped_commands.append(mapped_command)
             elif command_number == Command.DELETE:
-                if applicant_skills[record_id] in talent_skills:
-                    mapped_command = Command.delete(talent_skills[applicant_skills[record_id]])
+                if applicant_skills[record_id] in other_applicant_skills:
+                    mapped_command = Command.delete(other_applicant_skills[applicant_skills[record_id]])
                     mapped_commands.append(mapped_command)
             else:
                 mapped_commands.append(command)
@@ -158,9 +159,12 @@ class HrApplicant(models.Model):
             original_vals = vals.copy()
             original_vals["applicant_skill_ids"] = skills
             vals["applicant_skill_ids"] = self.env["hr.applicant.skill"]._get_transformed_commands(skills, self)
-            for applicant in self:
-                # Modify the skill values for the talent if it exists
-                if applicant.pool_applicant_id and (not applicant.is_pool_applicant):
-                    mapped_skills = applicant._map_applicant_skill_ids_to_talent_skill_ids(original_vals)
-                    applicant.pool_applicant_id.write({"applicant_skill_ids": mapped_skills})
+            if not self.env.context.get('skills_synced'):
+                for pool_applicant, current_applicants in self.grouped('pool_applicant_id').items():
+                    # Modify the skill values for the remaining applicants in the talent pool
+                    source_applicant = current_applicants[0]
+                    other_applicants = pool_applicant.linked_applicant_ids - current_applicants
+                    for applicant in other_applicants:
+                        mapped_skills = source_applicant._map_applicant_skill_ids_to_other_applicant_skill_ids(applicant, original_vals)
+                        applicant.with_context(skills_synced=True).write({"applicant_skill_ids": mapped_skills})
         return super().write(vals)
