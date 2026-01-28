a7c6e1b99217511897d868e3d537709bba7bd90d
Farah (ahmfa) <ahmfa@odoo.com>
2026-01-28 12:23:44 +0100

    [FIX] l10n_fr_hr_holidays: Unable to create PTO for employee with 2 week calendar
    Steps to reproduce:
        Choose France as the company location, and download "France - Work Entries Time Off" module.
        From Employees > Configuration > Settings > French Time Off Localization, select Paid time Off.
        Create a new employee and a new contract (in running state) for that employee that starts on 01/01/2025.
        While in the contract screen, create a new schedule that has 2 weeks calendar and Europe/Paris timezone.
        From Time Off > Management > Allocations, allocate 1+ paid time off days for the newly created employee that's valid from 01/01/2025..
        From the employee's profile > Time Off, try to take a Monday off with the paid time off leave.
    Issue:
        The user gets a Validation error stating that the "start date" is later than the "end date".
    Fix:
        In a 2 weeks calendar, there are 2 lines that are there to separate the first week from the second week (for aesthetic purposes).
        These lines have "hour_from" and "hour_to" = 0, which are taken into account when calulating the minimum hour to start the day off.
        Add a check to remove lines from calendar that are just there for display purposes.
    
    opw-5387347
    
    closes odoo/odoo#246094
    
    Signed-off-by: Mélanie Peyrat (mepe) <mepe@odoo.com>

--- addons/l10n_fr_hr_holidays/models/hr_leave.py
+++ addons/l10n_fr_hr_holidays/models/hr_leave.py
@@ -38,10 +38,12 @@ class HrLeave(models.Model):
             def adjust_date_range(date_from, date_to, period, attendance_ids, employee_id):
                 period_ids_from = attendance_ids.filtered(lambda a: a.day_period in period
                                                                     and int(a.dayofweek) == date_from.weekday()
-                                                                    and (not a.two_weeks_calendar or int(a.week_type) == a.get_week_type(date_from)))
+                                                                    and (not a.two_weeks_calendar or int(a.week_type) == a.get_week_type(date_from))
+                                                                    and not a.display_type)
                 period_ids_to = attendance_ids.filtered(lambda a: a.day_period in period
                                                                     and int(a.dayofweek) == date_to.weekday()
-                                                                    and (not a.two_weeks_calendar or int(a.week_type) == a.get_week_type(date_to)))
+                                                                    and (not a.two_weeks_calendar or int(a.week_type) == a.get_week_type(date_to))
+                                                                    and not a.display_type)
                 if period_ids_from:
                     min_hour = min(attendance.hour_from for attendance in period_ids_from)
                     date_from = self._to_utc(date_from, min_hour, employee_id)
