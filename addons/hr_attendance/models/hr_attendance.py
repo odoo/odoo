e1c91cfc1dc80ec255eeb137b9d335ccad7ae456
Mathieu Coutant <mcou@odoo.com>
2026-01-29 08:53:53 +0000

    [FIX] hr_holidays_attendance, hr_attendance: include attendance between two days in hourly accrual plans
    Issue:
    Attendances overlapping on two days are ignored for hourly accrual plans based on attendances.
    
    Steps to reproduce:
    - Install 'hr_holidays_attendance'
    - In Time Off > Configuration > Accrual Plan, create a new plan
    	- Based on worked time
    	- Hourly rule
    	- Attendances as Source
    - In Management > Allocations, create an allocation for an employee using the new accrual plan
    - Create an Attendance for this employee in the period of the Accrual Plan
    	- Check-in at 22pm for example
    	- Check-out at 7am
    - Run the cron "Accrual Time Off: Updates the number of time off"
    - The Allocation ignores the worked time from the attendance
    
    Cause:
    `_get_accrual_plan_level_work_entry_prorata()` is called on each day of the accrual period.
    So `start_dt` is `datetime.datetime(2026, 1, 2, 0, 0)` and `end_dt` is `datetime.datetime(2026, 1, 3, 0, 0)` for example.
    This means that the search will always excludes attendances overlapping on two days.
    
    https://github.com/odoo/odoo/blob/26f3026ed45cc409cd7f67fa219d44f1adbac9b7/addons/hr_holidays_attendance/models/hr_leave_allocation.py#L79-L83
    
    Solution:
    To count the attendances on several days, we need to split these attendances by day because `_get_accrual_plan_level_work_entry_prorata()` is only called with an interval of one day from midnight to midnight.
    
    First we get all attendances overlapping with the day by changing the domain in the search.
    
    Then we could simply take the difference between `max(attendance.check_in, start_dt)` and `min(attendance.check_out, end_dt)` but we also need to remove the lunch breaks (they were not counted in `attendance.worked_hours`).
    This would mean duplicating the code present in `_compute_worked_hours()`.
    To avoid this we create a new method for `hr.attendance` named `_get_worked_hours_in_range()`. That returns the number of hours worked due to this attendance in a given time frame.
    This new method can be used in both cases to get the needed value.
    
    opw-5172669
    
    X-original-commit: 97a7e949d87a19e3a02a6a0832b1780ebfd1f5a4

--- addons/hr_attendance/models/hr_attendance.py
+++ addons/hr_attendance/models/hr_attendance.py
@@ -17,6 +17,7 @@ from odoo.tools.float_utils import float_is_zero
 from odoo.exceptions import AccessError
 from odoo.tools import convert, format_duration, format_time, format_datetime
 from odoo.tools.intervals import Intervals
+from odoo.tools.date_utils import sum_intervals
 from odoo.tools.float_utils import float_compare
 
 def get_google_maps_url(latitude, longitude):
@@ -202,19 +203,33 @@ class HrAttendance(models.Model):
             between check_in and check_out, without taking into account the lunch_interval"""
         for attendance in self:
             if attendance.check_out and attendance.check_in and attendance.employee_id:
-                calendar = attendance._get_employee_calendar()
-                resource = attendance.employee_id.resource_id
-                tz = timezone(resource.tz) if not calendar else timezone(calendar.tz)
-                check_in_tz = attendance.check_in.astimezone(tz)
-                check_out_tz = attendance.check_out.astimezone(tz)
-                lunch_intervals = []
-                if not attendance.employee_id.is_flexible:
-                    lunch_intervals = attendance.employee_id._employee_attendance_intervals(check_in_tz, check_out_tz, lunch=True)
-                attendance_intervals = Intervals([(check_in_tz, check_out_tz, attendance)]) - lunch_intervals
-                delta = sum((i[1] - i[0]).total_seconds() for i in attendance_intervals)
-                attendance.worked_hours = delta / 3600.0
+                attendance.worked_hours = attendance._get_worked_hours_in_range(attendance.check_in, attendance.check_out)
             else:
                 attendance.worked_hours = False
+
+    def _get_worked_hours_in_range(self, start_dt, end_dt):
+        """Returns the amount of hours worked because of this attendance during the
+        interval defined by [start_dt, end_dt]
+
+        :param start_dt: datetime starting the interval.
+        :param end_dt: datetime ending the interval.
+        :returns: float, hours worked
+        """
+        self.ensure_one()
+        calendar = self._get_employee_calendar()
+        resource = self.employee_id.resource_id
+        tz = timezone(resource.tz) if not calendar else timezone(calendar.tz)
+        start_dt_tz = max(self.check_in, start_dt).astimezone(tz)
+        end_dt_tz = min(self.check_out, end_dt).astimezone(tz)
+
+        if end_dt_tz < start_dt_tz:
+            return 0.0
+
+        lunch_intervals = []
+        if not self.employee_id.is_flexible:
+            lunch_intervals = self.employee_id._employee_attendance_intervals(start_dt_tz, end_dt_tz, lunch=True)
+        attendance_intervals = Intervals([(start_dt_tz, end_dt_tz, self)]) - lunch_intervals
+        return sum_intervals(attendance_intervals)
 
     @api.constrains('check_in', 'check_out')
     def _check_validity_check_in_check_out(self):
