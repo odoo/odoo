9e7f9415319eda26194c01280df7f3571d2ddde1
Arnold Moyaux <arm@odoo.com>
2026-01-29 17:03:17 +0000

    [PERF] stock_account: avoid useless recomputation
    _get_report_data for stock valuation report. Compute the inventory
    twice. One for the header then inside
    `_get_stock_valuation_account_vals` (in order to create variation
    lines).
    
    However this method is costly and reusing the data has a huge benefits
    
    closes odoo/odoo#248692
    
    X-original-commit: 9154cf18b6bc56486e2e33276bffb3c5acb58f2b
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>
    Co-authored-by: "Beekoan" <mmha@odoo.com>

--- addons/stock_account/report/stock_valuation_report.py
+++ addons/stock_account/report/stock_valuation_report.py
@@ -77,7 +77,7 @@ class StockValuationReport(models.AbstractModel):
         location_valuation_vals = company._get_location_valuation_vals(
             date, location_domain=[('usage', '=', 'inventory')],
         )
-        stock_valuation_account_vals = company._get_stock_valuation_account_vals(
+        stock_valuation_account_vals = company.with_context(inventory_data=inventory_data)._get_stock_valuation_account_vals(
             accounts_by_product, date, company._get_location_valuation_vals(date))
 
         report_data = {
