5e364bd600020eb590b2997eeffb8bf203824d9d
khom-odoo <khom@odoo.com>
2026-01-29 11:38:50 +0000

    [FIX] l10n_sa{_edi}: fix future invoicing bug
    In odoo/odoo#236865 we decided to allow clients to backdate invoices
    by letting them use the `invoice_date` field for the invoice date and
    use the current time as the issue time because we are not supposed to
    use a dummy value for time.
    
    This created an issue where if a user in a timezone before SA tries to
    invoice a document around midnight using the current date in SA the
    datetime created will be in the future which will lead to the invoice
    being rejected by ZATCA.
    
    This commit makes sure we normalize the selected date wrt to the current
    datetime in saudi arabia so that we never accidentally invoice into
    the future.
    
    task-5890423
    opw-5373067
    
    X-original-commit: 65fe45bd41610768c004a718d1cc7c30c8c6d1e8

--- addons/l10n_sa_edi/models/account_edi_format.py
+++ addons/l10n_sa_edi/models/account_edi_format.py
@@ -444,7 +444,7 @@ class AccountEdiFormat(models.Model):
                 )
             )
         if invoice.invoice_date > fields.Date.context_today(self.with_context(tz='Asia/Riyadh')):
-            errors.append(_("- Please, make sure the invoice date is set to either the same as or before Today."))
+            errors.append(_("- Please set the Invoice Date to be either less than or equal to today as per the Asia/Riyadh time zone, since ZATCA does not allow future-dated invoicing."))
 
         if invoice.l10n_sa_show_reason and not invoice.l10n_sa_reason:
             errors.append(_("- Please make sure the 'ZATCA Reason' for the issuance of the Credit/Debit Note is specified."))
