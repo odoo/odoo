9584493a305c82f96b8320ff554d791de17e8bbe
Ze Chen (zeche) <zeche@odoo.com>
2026-01-30 18:49:40 +0000

    [FIX] website_event_sale: Prevent auto registration
    **Before this commit**
    If a gift card balance fully covers a shopping cart containing multiple
    events, Odoo auto-confirms the order as soon as the last event is added,
    skipping the final checkout step.
    
    **After this commit**
    Sale orders will no longer be automatically confirmed when a customer
    registers for a paid event, even if an applied gift card brings the
    total balance to zero.
    
    opw-5896626
    
    closes odoo/odoo#264599
    
    X-original-commit: 83698e89718129b5b7686def55191c9dea4506af
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>
    Signed-off-by: Ze Chen (zeche) <zeche@odoo.com>

--- addons/website_event_sale/tests/test_website_event_sale.py
+++ addons/website_event_sale/tests/test_website_event_sale.py
@@ -61,3 +61,63 @@ class TestWebsiteEventSale(HttpCaseWithUserPortal, TestWebsiteEventSaleCommon):
             ('order_line.event_ticket_id', '=', self.ticket.id),
             ('order_line.event_ticket_id', '=', free_ticket.id)
         ]), "Sale order should be created for the free/paid tickets mix")
+
+    def test_website_event_sale_giftcard_covers_full_cost(self):
+        """Test saleorder is not auto confirmed if the gift card in the card can fully cover the cost"""
+        self.authenticate(None, None)
+        event_questions = self.event.question_ids
+        event_registration_count = len(self.event.registration_ids)
+        name_question = event_questions.filtered(lambda q: q.question_type == 'name')
+        email_question = event_questions.filtered(lambda q: q.question_type == 'email')
+        phone_question = event_questions.filtered(lambda q: q.question_type == 'phone')
+
+        paid_ticket_1 = self.env['event.event.ticket'].create({
+            'event_id': self.event.id,
+            'name': 'Paid_1',
+            'product_id': self.product_event.id,
+            'price': 50,
+        })
+
+        paid_ticket_2 = self.env['event.event.ticket'].create({
+            'event_id': self.event.id,
+            'name': 'Paid_2',
+            'product_id': self.product_event.id,
+            'price': 200,
+        })
+
+        self.pricelist.write({
+                    'item_ids': [(5, 0, 0), (0, 0, {
+                        'applied_on': '3_global',
+                        'compute_price': 'percentage',
+                        'percent_price': 100,
+                    })],
+                    'name': 'Full discount',
+        })
+        # There must be an event in the cart to apply the pricelist on it
+        self.url_open(f'/event/{self.event.id}/registration/confirm', data={
+            f'1-name-{name_question.id}': 'Bob',
+            f'1-email-{email_question.id}': 'bob@test.lan',
+            f'1-phone-{phone_question.id}': '8989898989',
+            '1-event_ticket_id': paid_ticket_1.id,
+            'csrf_token': http.Request.csrf_token(self),
+        })
+
+        self.url_open('/shop/change_pricelist', data={
+            'pricelist_id': self.pricelist.id,
+            'csrf_token': http.Request.csrf_token(self),
+        })
+
+        last_so = self.env['sale.order'].search([], order='id desc', limit=1)
+        self.assertTrue(last_so.state == 'draft', "Apply pricelist with 100% discount should not auto-confirm the SO")
+
+        self.url_open(f'/event/{self.event.id}/registration/confirm', data={
+            f'2-name-{name_question.id}': 'Bob',
+            f'2-email-{email_question.id}': 'bob@test.lan',
+            f'2-phone-{phone_question.id}': '8989898989',
+            '2-event_ticket_id': paid_ticket_2.id,
+            'csrf_token': http.Request.csrf_token(self),
+        })
+
+        last_so = self.env['sale.order'].search([], order='id desc', limit=1)
+        self.assertEqual(len(self.event.registration_ids), event_registration_count + 2)
+        self.assertTrue(last_so.state == 'draft', "The status of unpaid events should be draft")
