f4559db742d853827d9e117ee48a2dec6d483e51
reka-odoo <reka@odoo.com>
2026-01-30 07:39:26 +0000

    [FIX] website_sale: prevent error when opening shop when variants are deleted
    When all variants of a product template are deleted, opening the Shop page on
    the website raises a traceback.
    
    Steps to reproduce the error:
    - Install "website_sale" module
    - Go to settings > Enable variants
    - Create a product template > In Attributes & Variants Tab >
      add one attribute with two values > Save
    - Click on the variants smart button > select all > delete
    - Go to Website > Shop
    
    Traceback:
    "AssertionError: Invalid falsy real id"
    
    https://github.com/odoo/odoo/blob/b6598c11a3580cfc6ff1ffeb67b0d388c55383d1/addons/website_sale/controllers/main.py#L456
    After commit [1], falsy IDs are no longer allowed in "browse()".
    When all variants of a product template are deleted,
    "product._get_first_possible_variant_id()" returns False.
    This falsy value is later passed to "browse()",
    which triggers the above traceback.
    
    [1]: https://github.com/odoo/odoo/commit/4290724a4c8c57fba4f4d3d688d38f65dadcc38f
    
    sentry-7201563878
    
    X-original-commit: 2d3ee13476357d2bd5f4518d856ea4649b48c39d

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -468,9 +468,12 @@ class WebsiteSale(payment_portal.PaymentPortal):
         products.fetch()
 
         # map each product to its variant, and prefetch the variants
-        variants = request.env['product.product'].sudo().browse(product._get_first_possible_variant_id() for product in products)
+        Product = request.env['product.product']
+        product_variant_ids = [product._get_first_possible_variant_id() for product in products]
+        variants = Product.sudo().browse(vid for vid in product_variant_ids if vid)
         variants.fetch()
-        product_variants = dict(zip(products, variants))
+        variant_by_id = {v.id: v for v in variants}
+        product_variants = dict(zip(products, (variant_by_id.get(vid, Product) for vid in product_variant_ids)))
 
         ProductAttribute = request.env['product.attribute']
         if products:
