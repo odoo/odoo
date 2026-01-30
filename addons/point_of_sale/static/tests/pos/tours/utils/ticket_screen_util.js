d09b2abec2dbd5413004c0e4b71c4746c09c86f7
tong-odoo <tong@odoo.com>
2026-01-30 16:20:04 +0800

    [FIX] point_of_sale: fix tour ticket_screen_util functions

--- addons/point_of_sale/static/tests/pos/tours/utils/ticket_screen_util.js
+++ addons/point_of_sale/static/tests/pos/tours/utils/ticket_screen_util.js
@@ -44,7 +44,7 @@ export function selectOrderByPrice(price) {
 export function doubleClickOrder(orderName) {
     return [
         {
-            trigger: `.ticket-screen .order-row > .col:nth-child(2):contains("${orderName}")`,
+            trigger: `.ticket-screen .order-row:contains("${orderName}")`,
             run: "dblclick",
         },
     ];
@@ -278,4 +278,13 @@ export function isReady() {
         content: "Wait for the Ticket Screen to be ready",
         trigger: ".ticket-screen:not(.loading-orders)",
     };
+}
+
+export function isShown() {
+    return [
+        {
+            content: "ticket screen is shown",
+            trigger: ".pos .ticket-screen",
+        },
+    ];
 }
