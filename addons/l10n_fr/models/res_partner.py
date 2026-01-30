419aa0e778e1aaf7106e80b00b782f9a807cda0e
dvsh-odoo <dvsh@odoo.com>
2026-01-30 17:43:43 +0530

    [IMP] l10n_fr:  add SIRET format validation warning on partners
    - Display a non-blocking warning on the partner form when a French
      company SIRET is invalid.
    
    - The validation relies on stdnum.fr.siret to check the official
      SIRET rules (length, format, checksum, La Poste exceptions), and
      surfaces the library error message with contextual information.
    
    TaskID-5882793

--- addons/l10n_fr/models/res_partner.py
+++ addons/l10n_fr/models/res_partner.py
@@ -1,3 +1,6 @@
+from stdnum.exceptions import ValidationError as StdnumValidationError
+from stdnum.fr import siret
+
 from odoo import api, fields, models
 
 
@@ -5,8 +8,29 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     l10n_fr_is_french = fields.Boolean(compute='_compute_l10n_fr_is_french')
+    l10n_fr_siret_warning = fields.Char(compute='_compute_l10n_fr_siret_warning')
 
     @api.depends('country_code')
     def _compute_l10n_fr_is_french(self):
         for partner in self:
             partner.l10n_fr_is_french = partner.country_code in self.env['res.company']._get_france_country_codes()
+
+    @api.depends('company_registry', 'l10n_fr_is_french')
+    def _compute_l10n_fr_siret_warning(self):
+        for partner in self:
+            partner.l10n_fr_siret_warning = False
+            if (not partner.company_registry or not partner.l10n_fr_is_french):
+                continue
+
+            try:
+                siret.validate(partner.company_registry)
+            except StdnumValidationError as e:
+                partner.l10n_fr_siret_warning = self.env._(
+                    "Invalid SIRET: %(error)s",
+                    error=str(e),
+                )
+
+    def _get_company_registry_labels(self):
+        labels = super()._get_company_registry_labels()
+        labels['FR'] = self.env._("SIRET")
+        return labels
