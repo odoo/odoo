odoo/odoo

commit 8c199f7783527735b35c9fbda334cbdcd55a004f

[IMP] core,*: make 'Product Price' a minimum precision
Imported invoices can show different totals than what the original
document show.  This especially an issue for Peppol.
Because `price_unit` is rounded, by computing the total of a line with
`quantity * price_unit`, it may not be possible to obtain the same
total as the one from the original document.

To face this issue, the float fields related to "Product Price" don't
have a decimal precision set anymore.
To keep the UI clean, the precision set on "Product Price" is now
interpreted as a "minimal precision".
So if it set to 3, we'll see at least 3 digits.  If there's more, all
the digits are shown.
As so:
`4.0`     -> `'4.000'`
`4.23`    -> `'4.230'`
`4.235`   -> `'4.235'`
`4.2358`  -> `'4.23458'`

task-4895014

closes odoo/odoo#243987

Related: odoo/enterprise#104728
Signed-off-by: William André (wan) <wan@odoo.com>

addons/web/static/src/core/utils/numbers.js
@@ -214,9 +214,14 @@ export function formatFloat(value, options = {}) {
     let precision;
     if (options.digits && options.digits[1] !== undefined) {
         precision = options.digits[1];
+    } else if (options.minDigits) {
+        // When no precision is set, 12 is chosen as the precision. It is high enough to keep it precise,
+        // but not too high in order to avoid rounding errors..
+        precision = 12;
     } else {
         precision = 2;
     }
+    const minPrecision = options.minDigits || precision;
     if (floatIsZero(value, precision)) {
         value = 0.0;
     }
@@ -228,8 +233,11 @@ export function formatFloat(value, options = {}) {
     const decimalPoint = "decimalPoint" in options ? options.decimalPoint : l10n.decimalPoint;
     const formatted = value.toFixed(precision).split(".");
     formatted[0] = insertThousandsSep(formatted[0], thousandsSep, grouping);
-    if (options.trailingZeros === false && formatted[1]) {
+    if (formatted[1]) {
         formatted[1] = formatted[1].replace(/0+$/, "");
+        if (options.trailingZeros !== false) {
+            formatted[1] = formatted[1].padEnd(minPrecision, "0");
+        }
     }
     return formatted[1] ? formatted.join(decimalPoint) : formatted[0];
 }

odoo/addons/base/models/ir_qweb_fields.py
@@ -182,33 +182,35 @@ class FloatConverter(models.AbstractModel):
 
     @api.model
     def value_to_html(self, value, options):
+        min_precision = options.get('min_precision')
         if 'decimal_precision' in options:
             precision = self.env['decimal.precision'].precision_get(options['decimal_precision'])
+        elif options.get('precision') is None:
+            precision = 6
+            min_precision = min_precision or 1
         else:
             precision = options['precision']
 
-        if precision is None:
-            fmt = '%f'
-        else:
-            value = float_utils.float_round(value, precision_digits=precision)
-            fmt = '%.{precision}f'.format(precision=precision)
+        fmt = f'%.{precision}f'
+        if min_precision and min_precision < precision:
+            _, dec_part = float_utils.float_split_str(value, precision)
+            digits_count = len(dec_part.rstrip('0'))
+            if digits_count < min_precision:
+                fmt = f'%.{min_precision}f'
+            elif digits_count < precision:
+                fmt = f'%.{digits_count}f'
 
-        formatted = self.user_lang().format(fmt, value, grouping=True).replace(r'-', '-\N{ZERO WIDTH NO-BREAK SPACE}')
-
-        # %f does not strip trailing zeroes. %g does but its precision causes
-        # it to switch to scientific notation starting at a million *and* to
-        # strip decimals. So use %f and if no precision was specified manually
-        # strip trailing 0.
-        if precision is None:
-            formatted = re.sub(r'(?:(0|\d+?)0+)$', r'\1', formatted)
-
-        return formatted
+        value = float_utils.float_round(value, precision_digits=precision)
+        return self.user_lang().format(fmt, value, grouping=True).replace(r'-', '-\N{ZERO WIDTH NO-BREAK SPACE}')
 
     @api.model
     def record_to_html(self, record, field_name, options):
         if 'precision' not in options and 'decimal_precision' not in options:
             _, precision = record._fields[field_name].get_digits(record.env) or (None, None)
             options = dict(options, precision=precision)
+        if 'min_precision' not in options:
+            min_precision = record._fields[field_name].get_min_display_digits(record.env)
+            options = dict(options, min_precision=min_precision)
         return super(FloatConverter, self).record_to_html(record, field_name, options)
 
 

addons/web/static/tests/core/utils/numbers.test.js
@@ -331,7 +331,6 @@ describe("formatFloat", () => {
         expect(formatFloat(1.0045e22, options)).toBe("1.005e+22");
         expect(formatFloat(-1.0045e22, options)).toBe("-1.004e+22");
 
-        Object.assign(options, { humanReadable: false });
-        expect(formatFloat(-0.0000001, options)).toBe("0.00");
+        expect(formatFloat(-0.0000001, { digits: [16, 2], humanReadable: false })).toBe("0.00");
     });
 });

odoo/addons/base/tests/test_qweb_field.py
@@ -58,6 +58,41 @@ class TestQwebFieldInteger(common.TransactionCase):
             "125.125k"
         )
 
+
+class TestQwebFieldFloatConverter(common.TransactionCase):
+    def value_to_html(self, value, options=None):
+        options = options or {}
+        return self.env['ir.qweb.field.float'].value_to_html(value, options)
+
+    def test_float_value_to_html_no_precision(self):
+        self.assertEqual(self.value_to_html(3), '3.0')
+        self.assertEqual(self.value_to_html(3.1), '3.1')
+        self.assertEqual(self.value_to_html(3.1231239), '3.123124')
+
+    def test_float_value_to_html_with_precision(self):
+        options = {'precision': 3}
+        self.assertEqual(self.value_to_html(3, options), '3.000')
+        self.assertEqual(self.value_to_html(3.1, options), '3.100')
+        self.assertEqual(self.value_to_html(3.123, options), '3.123')
+        self.assertEqual(self.value_to_html(3.1239, options), '3.124')
+
+    def test_float_value_to_html_with_min_precision(self):
+        options = {'min_precision': 3}
+        self.assertEqual(self.value_to_html(3, options), '3.000')
+        self.assertEqual(self.value_to_html(3.1, options), '3.100')
+        self.assertEqual(self.value_to_html(3.123, options), '3.123')
+        self.assertEqual(self.value_to_html(3.1239, options), '3.1239')
+        self.assertEqual(self.value_to_html(3.1231239, options), '3.123124')
+
+    def test_float_value_to_html_with_precision_and_min_precision(self):
+        options = {'min_precision': 3, 'precision': 4}
+        self.assertEqual(self.value_to_html(3, options), '3.000')
+        self.assertEqual(self.value_to_html(3.1, options), '3.100')
+        self.assertEqual(self.value_to_html(3.123, options), '3.123')
+        self.assertEqual(self.value_to_html(3.1239, options), '3.1239')
+        self.assertEqual(self.value_to_html(3.12349, options), '3.1235')
+
+
 class TestQwebFieldContact(common.TransactionCase):
     @classmethod
     def setUpClass(cls):

addons/account/models/account_move_line.py
@@ -356,7 +356,7 @@ class AccountMoveLine(models.Model):
     price_unit = fields.Float(
         string='Unit Price',
         compute="_compute_price_unit", store=True, readonly=False, precompute=True,
-        digits='Product Price',
+        min_display_digits='Product Price',
     )
     price_subtotal = fields.Monetary(
         string='Subtotal',

addons/account/tests/test_account_move_in_invoice.py
@@ -1050,7 +1050,7 @@ class TestAccountMoveInInvoiceOnchanges(AccountTestInvoicingCommon):
             {
                 **self.product_line_vals_1,
                 'quantity': 0.1,
-                'price_unit': 0.05,
+                'price_unit': 0.045,
                 'price_subtotal': 0.005,
                 'price_total': 0.006,
                 'currency_id': self.other_currency.id,
@@ -1099,11 +1099,11 @@ class TestAccountMoveInInvoiceOnchanges(AccountTestInvoicingCommon):
             {
                 **self.product_line_vals_1,
                 'quantity': 0.1,
-                'price_unit': 0.05,
-                'price_subtotal': 0.01,
-                'price_total': 0.01,
-                'amount_currency': 0.01,
-                'debit': 0.01,
+                'price_unit': 0.045,
+                'price_subtotal': 0.0,
+                'price_total': 0.0,
+                'amount_currency': 0.0,
+                'debit': 0.0,
             },
             self.product_line_vals_2,
             {
@@ -1114,17 +1114,17 @@ class TestAccountMoveInInvoiceOnchanges(AccountTestInvoicingCommon):
             self.tax_line_vals_2,
             {
                 **self.term_line_vals_1,
-                'amount_currency': -208.01,
-                'credit': 208.01,
+                'amount_currency': -208.0,
+                'credit': 208.0,
                 'date_maturity': fields.Date.from_string('2016-01-01'),
             },
         ], {
             **self.move_vals,
             'currency_id': self.company_data['currency'].id,
             'date': fields.Date.from_string('2016-01-01'),
-            'amount_untaxed': 160.01,
+            'amount_untaxed': 160.0,
             'amount_tax': 48.0,
-            'amount_total': 208.01,
+            'amount_total': 208.0,
         })
 
     def test_in_invoice_onchange_past_invoice_1(self):

addons/account/tests/test_account_move_in_refund.py
@@ -703,7 +703,7 @@ class TestAccountMoveInRefundOnchanges(AccountTestInvoicingCommon):
             {
                 **self.product_line_vals_1,
                 'quantity': 0.1,
-                'price_unit': 0.05,
+                'price_unit': 0.045,
                 'price_subtotal': 0.005,
                 'price_total': 0.006,
                 'currency_id': self.other_currency.id,
@@ -754,11 +754,11 @@ class TestAccountMoveInRefundOnchanges(AccountTestInvoicingCommon):
             {
                 **self.product_line_vals_1,
                 'quantity': 0.1,
-                'price_unit': 0.05,
-                'price_subtotal': 0.01,
-                'price_total': 0.01,
-                'amount_currency': -0.01,
-                'credit': 0.01,
+                'price_unit': 0.045,
+                'price_subtotal': 0.0,
+                'price_total': 0.0,
+                'amount_currency': -0.0,
+                'credit': 0.0,
             },
             self.product_line_vals_2,
             {
@@ -769,8 +769,8 @@ class TestAccountMoveInRefundOnchanges(AccountTestInvoicingCommon):
             self.tax_line_vals_2,
             {
                 **self.term_line_vals_1,
-                'amount_currency': 208.01,
-                'debit': 208.01,
+                'amount_currency': 208.0,
+                'debit': 208.0,
                 'date_maturity': fields.Date.from_string('2016-01-01'),
             },
         ], {
@@ -778,9 +778,9 @@ class TestAccountMoveInRefundOnchanges(AccountTestInvoicingCommon):
             'currency_id': self.company_data['currency'].id,
             'date': fields.Date.from_string('2016-01-01'),
             'invoice_date': fields.Date.from_string('2016-01-01'),
-            'amount_untaxed': 160.01,
+            'amount_untaxed': 160.0,
             'amount_tax': 48.0,
-            'amount_total': 208.01,
+            'amount_total': 208.0,
         })
 
     def test_in_refund_onchange_past_invoice_1(self):

addons/account/tests/test_account_move_out_invoice.py
@@ -350,11 +350,6 @@ class TestAccountMoveOutInvoiceOnchanges(AccountTestInvoicingCommon):
             'amount_total': 2760.0,
         })
 
-        # Check rounding.
-        decimal_precision_name = self.env['account.move.line']._fields['price_unit']._digits
-        decimal_precision = self.env['decimal.precision'].search([('name', '=', decimal_precision_name)])
-        decimal_precision.digits = 4
-
         product.lst_price = 90.0034
         invoice = self.env['account.move'].create({
             'move_type': 'out_invoice',
@@ -371,7 +366,7 @@ class TestAccountMoveOutInvoiceOnchanges(AccountTestInvoicingCommon):
             ],
         })
         self.assertRecordValues(invoice.invoice_line_ids, [{
-            'price_unit': 163.6425, # 90.0034 / 1.10 * 2
+            'price_unit': 163.64254545454546,  # 90.0034 / 1.10 * 2
             'tax_ids': tax_price_exclude.ids,
             'price_subtotal': 163.643,
             'price_total': 188.189,
@@ -780,13 +775,7 @@ class TestAccountMoveOutInvoiceOnchanges(AccountTestInvoicingCommon):
         ''' Seek for rounding issue on the price_subtotal when dealing with a price_unit having more digits than the
         foreign currency one.
         '''
-        decimal_precision_name = self.env['account.move.line']._fields['price_unit']._digits
-        decimal_precision = self.env['decimal.precision'].search([('name', '=', decimal_precision_name)])
-
-        self.assertTrue(decimal_precision, "Decimal precision '%s' not found" % decimal_precision_name)
-
         self.other_currency.rounding = 0.01
-        decimal_precision.digits = 4
 
         def check_invoice_values(invoice):
             self.assertInvoiceValues(invoice, [
@@ -1633,7 +1622,7 @@ class TestAccountMoveOutInvoiceOnchanges(AccountTestInvoicingCommon):
             {
                 **self.product_line_vals_1,
                 'quantity': 0.1,
-                'price_unit': 0.05,
+                'price_unit': 0.045,
                 'price_subtotal': 0.005,
                 'price_total': 0.006,
                 'currency_id': self.other_currency.id,
@@ -1683,11 +1672,11 @@ class TestAccountMoveOutInvoiceOnchanges(AccountTestInvoicingCommon):
             {
                 **self.product_line_vals_1,
                 'quantity': 0.1,
-                'price_unit': 0.05,
-                'price_subtotal': 0.01,
-                'price_total': 0.01,
-                'amount_currency': -0.01,
-                'credit': 0.01,
+                'price_unit': 0.045,
+                'price_subtotal': 0.0,
+                'price_total': 0.0,
+                'amount_currency': -0.0,
+                'credit': 0.0,
             },
             self.product_line_vals_2,
             {
@@ -1698,17 +1687,17 @@ class TestAccountMoveOutInvoiceOnchanges(AccountTestInvoicingCommon):
             self.tax_line_vals_2,
             {
                 **self.term_line_vals_1,
-                'amount_currency': 260.01,
-                'debit': 260.01,
+                'amount_currency': 260.0,
+                'debit': 260.0,
                 'date_maturity': fields.Date.from_string('2016-01-01'),
             },
         ], {
             **self.move_vals,
             'currency_id': self.company_data['currency'].id,
             'date': fields.Date.from_string('2016-01-01'),
-            'amount_untaxed': 200.01,
+            'amount_untaxed': 200.0,
             'amount_tax': 60.0,
-            'amount_total': 260.01,
+            'amount_total': 260.0,
         })
 
     def test_out_invoice_line_tax_fixed_price_include_free_product(self):

commit 79ceeed707ef274f19a04e741f6cb8ac60c44321

[FIX] base, web: use dynamic precision when using minimum precision
8c199f7783527735b35c9fbda334cbdcd55a004f introduced minimum precision
for float fields, but it has a display issue.

Steps to reproduce:
- create an invoice, set the price of an an invoice line to `9417.2`
-> the web client displays `'9,417.200000000001'`

Issue:
In case of a minimum precision, the precision was set to 12.
But it should be dynamic in regards to the number of integer digits.

Solution:
The precision is set to 6 digits, but if the number of significant
decimal digits is lower, then it is set to that number.
The number of significant decimal digits is computed using:
15 - number of integer digits.
--

Also implementing the same logic in qweb.

To reproduce:
- create a an invoice with an invoice line of `300000000031.3`
- post and print
-> the price unit on the pdf shows "300,000,000,031.300232"

opw-5880951

addons/web/static/src/core/utils/numbers.js
@@ -199,6 +199,7 @@ export function humanNumber(number, options = { decimals: 0, minDigits: 1 }) {
  * @param {Object} [options]
  * @param {number[]} [options.digits] the number of digits that should be used,
  *   instead of the default digits precision in the field.
+ * @param {nummber} [optinos.minDigits] the minimum number of decimal digits to display.
  * @param {boolean} [options.humanReadable] if true, large numbers are formatted
  *   to a human readable format.
  * @param {string} [options.decimalPoint] decimal separating character
@@ -215,9 +216,12 @@ export function formatFloat(value, options = {}) {
     if (options.digits && options.digits[1] !== undefined) {
         precision = options.digits[1];
     } else if (options.minDigits) {
-        // When no precision is set, 12 is chosen as the precision. It is high enough to keep it precise,
-        // but not too high in order to avoid rounding errors..
-        precision = 12;
+        const intDigitsCount = (value !== 0) ? Math.floor(Math.log10(Math.abs(value))) + 1 : 1;
+        // We estimate the maximum decimal digits we can display without showing rounding errors,
+        // by substracting the number of integer digits to 15, as floats have 15-16 digits precision.
+        const maxDecDigits = Math.max(15 - intDigitsCount, 0);
+        // We display maximum 6 digits or the number of significant digits (if it's lower)
+        precision = Math.min(6, maxDecDigits);
     } else {
         precision = 2;
     }

odoo/addons/base/models/ir_qweb_fields.py
@@ -3,6 +3,7 @@ import base64
 import binascii
 from datetime import time
 import logging
+import math
 import re
 from io import BytesIO
 
@@ -186,7 +187,10 @@ class FloatConverter(models.AbstractModel):
         if 'decimal_precision' in options:
             precision = self.env['decimal.precision'].precision_get(options['decimal_precision'])
         elif options.get('precision') is None:
-            precision = 6
+            int_digits = int(math.log10(abs(value))) + 1 if value != 0 else 1
+            max_dec_digits = max(15 - int_digits, 0)
+            # We display maximum 6 decimal digits or the number of significant decimal digits if it's lower
+            precision = min(6, max_dec_digits)
             min_precision = min_precision or 1
         else:
             precision = options['precision']

addons/web/static/tests/core/utils/numbers.test.js
@@ -252,6 +252,42 @@ test("floatIsZero", () => {
 });
 
 describe("formatFloat", () => {
+    test("precision", () => {
+        patchWithCleanup(localization, {
+            decimalPoint: ".",
+            grouping: [3, 0],
+            thousandsSep: ",",
+        });
+
+        let options = {};
+        expect(formatFloat(3, options)).toBe("3.00");
+        expect(formatFloat(3.1, options)).toBe("3.10");
+        expect(formatFloat(3.12, options)).toBe("3.12");
+        expect(formatFloat(3.129, options)).toBe("3.13");
+
+        options = { digits: [15, 3] };
+        expect(formatFloat(3, options)).toBe("3.000");
+        expect(formatFloat(3.1, options)).toBe("3.100");
+        expect(formatFloat(3.123, options)).toBe("3.123");
+        expect(formatFloat(3.1239, options)).toBe("3.124");
+
+        options = { minDigits: 3 };
+        expect(formatFloat(0, options)).toBe("0.000");
+        expect(formatFloat(3, options)).toBe("3.000");
+        expect(formatFloat(3.1, options)).toBe("3.100");
+        expect(formatFloat(3.123, options)).toBe("3.123");
+        expect(formatFloat(3.1239, options)).toBe("3.1239");
+        expect(formatFloat(3.1231239, options)).toBe("3.123124");
+        expect(formatFloat(1234567890.1234567890, options)).toBe("1,234,567,890.12346");
+
+        options = { minDigits: 3, digits: [15, 4] };
+        expect(formatFloat(3, options)).toBe("3.000");
+        expect(formatFloat(3.1, options)).toBe("3.100");
+        expect(formatFloat(3.123, options)).toBe("3.123");
+        expect(formatFloat(3.1239, options)).toBe("3.1239");
+        expect(formatFloat(3.1234567, options)).toBe("3.1235");
+    });
+
     test("localized", () => {
         patchWithCleanup(localization, {
             decimalPoint: ".",

odoo/addons/base/tests/test_qweb_field.py
@@ -78,11 +78,13 @@ class TestQwebFieldFloatConverter(common.TransactionCase):
 
     def test_float_value_to_html_with_min_precision(self):
         options = {'min_precision': 3}
+        self.assertEqual(self.value_to_html(0, options), '0.000')
         self.assertEqual(self.value_to_html(3, options), '3.000')
         self.assertEqual(self.value_to_html(3.1, options), '3.100')
         self.assertEqual(self.value_to_html(3.123, options), '3.123')
         self.assertEqual(self.value_to_html(3.1239, options), '3.1239')
         self.assertEqual(self.value_to_html(3.1231239, options), '3.123124')
+        self.assertEqual(self.value_to_html(1234567890.1234567890, options), '1,234,567,890.12346')
 
     def test_float_value_to_html_with_precision_and_min_precision(self):
         options = {'min_precision': 3, 'precision': 4}