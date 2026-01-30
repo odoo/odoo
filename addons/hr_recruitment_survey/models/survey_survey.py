3bea351258bf0489b3dd18a5e509a9e093e8b21f
ravb-odoo <ravb@odoo.com>
2026-01-30 16:22:44 +0530

    [IMP] hr_recruitment_survey, _*: improve interview survey config
    _*= website_hr_recruitment
    
    This PR improves the interview survey configuration for the recruitment process.
    - Updates to the Survey list and form views
    - Minor form view improvements on the Job Position
    - Added a state button on the Job form view
    
    task-5890312

--- addons/hr_recruitment_survey/models/survey_survey.py
+++ addons/hr_recruitment_survey/models/survey_survey.py
@@ -8,6 +8,7 @@ class SurveySurvey(models.Model):
 
     survey_type = fields.Selection(selection_add=[('recruitment', 'Recruitment')], ondelete={'recruitment': 'set default'})
     hr_job_ids = fields.One2many("hr.job", "survey_id", string="Job Position")
+    job_count = fields.Integer("Job Count", compute='_compute_job_count')
 
     @api.depends_context('uid')
     def _compute_allowed_survey_types(self):
@@ -16,6 +17,19 @@ class SurveySurvey(models.Model):
                 self.env.user.has_group('survey.group_survey_user'):
             for survey in self:
                 survey.allowed_survey_types = (survey.allowed_survey_types or {}) | {'recruitment': 'fa-suitcase'}
+
+    def _compute_job_count(self):
+        job_read_group = self.env['hr.job']._read_group(
+            [
+                ('survey_id', 'in', self.ids),
+                '|',
+                ('company_id', '=', False),
+                ('company_id', 'in', self.env.companies.ids)
+            ], ['survey_id'], ['__count']
+        )
+        job_dict = {survey.id: count for survey, count in job_read_group}
+        for survey in self:
+            survey.job_count = job_dict.get(survey.id, 0)
 
     def get_formview_id(self, access_uid=None):
         if self.survey_type == 'recruitment':
@@ -32,3 +46,25 @@ class SurveySurvey(models.Model):
                 'domain': [('survey_id.survey_type', '=', 'recruitment')]
             })
         return action
+
+    def action_open_jobs(self):
+        self.ensure_one()
+        jobs = self.hr_job_ids.filtered(
+            lambda j: not j.company_id or j.company_id in self.env.companies
+        )
+        if self.job_count > 1:
+            return {
+                'name': self.env._('Related Jobs'),
+                'type': 'ir.actions.act_window',
+                'res_model': 'hr.job',
+                'view_mode': 'list,kanban,form',
+                'domain': [('id', 'in', jobs.ids)],
+            }
+        else:
+            return {
+                'name': self.env._('Job'),
+                'type': 'ir.actions.act_window',
+                'res_model': 'hr.job',
+                'view_mode': 'form',
+                'res_id': jobs.id,
+            }
