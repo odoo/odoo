4feb7f1aa845f78bdaa687e9aa3fd9101c6515b6
nape-odoo <nape@odoo.com>
2026-01-30 09:27:46 +0000

    [FIX] l10n_hr_edi: Fixes for XML generation, views and error handling
    - Adjusting the hrextac xmlns to the updated one
    - Adjusting MER payment report button to not show incorrectly
    - Improving error handling for MER payment reporting
    task-none
    
    closes odoo/odoo#246654
    
    X-original-commit: 9bda6e196e4262543c3945a32e13ad933aeb1be5
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>
    Signed-off-by: Petr Nazin (nape) <nape@odoo.com>

--- addons/l10n_hr_edi/models/account_move.py
+++ addons/l10n_hr_edi/models/account_move.py
@@ -235,6 +235,7 @@ class AccountMove(models.Model):
         })
 
     def l10n_hr_edi_mer_action_report_paid(self):
+        batch = len(self) != 1
         for move in self:
             if not move.l10n_hr_mer_document_eid:
                 continue
@@ -250,19 +251,25 @@ class AccountMove(models.Model):
                         move.l10n_hr_payment_method_type,
                     )
                 except MojEracunServiceError:
-                    _logger.error("Failed to report payments document: %s", move.l10n_hr_mer_document_eid)
-                    continue
+                    if batch:
+                        _logger.error("Failed to report payments document: %s", move.l10n_hr_mer_document_eid)
+                        continue
+                    else:
+                        raise
                 move.l10n_hr_edi_addendum_id.payment_reported_amount += amount_to_report
                 move.l10n_hr_edi_addendum_id.fiscalization_request = response.get('fiscalizationRequestId')
-                attachment = self.env["ir.attachment"].create(
-                    {
-                        "name": f"mojeracun_{response['electronicId']}_payment.xml",
-                        "raw": response['encodedXml'],
-                        "type": "binary",
-                        "mimetype": "application/xml",
-                    }
-                )
-                attachment.write({'res_model': 'account.move', 'res_id': move.id})
+                if response.get('encodedXml'):
+                    attachment = self.env["ir.attachment"].create(
+                        {
+                            "name": f"mojeracun_{response['electronicId']}_payment.xml",
+                            "raw": response['encodedXml'],
+                            "type": "binary",
+                            "mimetype": "application/xml",
+                        }
+                    )
+                    attachment.write({'res_model': 'account.move', 'res_id': move.id})
+                else:
+                    attachment = False
                 move._message_log(
                     body=self.env._(
                         "%(ts)s: Payments for eRacun document (ElectroicId: %(eid)s) in the amount of %(mnt)s EUR has been reported successfully. (Request ID: %(req_id)s)",
@@ -271,7 +278,7 @@ class AccountMove(models.Model):
                         mnt=amount_to_report,
                         req_id=response['fiscalizationRequestId'],
                     ),
-                    attachment_ids=attachment.ids,
+                    attachment_ids=attachment.ids if attachment else False,
                 )
                 _mer_api_update_document_process_status(
                     move.company_id,
