cde9f7d0ea5629c03a950fc6b531515ef7582ad3
Tanguy Quéguineur <taqu@odoo.com>
2026-02-02 15:32:06 +0100

    [FIX] hr_holidays: correct timezone for time off calendar event
    **Steps to reproduce**
    - Have all timezones (browser, employee, working schedule...) aligned
    on an diffrent timezone than UTC (normal flow).
    - Take a time off for a half day, or in custom hours and validate it.
    Issue: the associated calendar event created doesn't match the time
    off start/end.
    
    **Cause**
    Previous commit converts the time off (in UTC) to the leave's timezone,
    this is an issue because `start` and `stop` of `calendar.event` should
    be in UTC.
    
    **Change**
    The conversion makes sense for allday events, as the `start`/`stop` are
    not in UTC (see `_inverse_dates` in `calendar_event.py`,  they represent
    a date used for the display of the event, but for non-allday events we
    have to make sure the `start`/`stop` are the actual times of the leave.
    
    Also, full days leaves taken with a leave type in half days are displayed
    as fullday events in the calendar.
    
    opw-5870397
    
    closes odoo/odoo#246786
    
    Signed-off-by: Abdelrahman Mahmoud (amah) <amah@odoo.com>

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -1050,13 +1050,18 @@ Versions:
                 "%(employee)s on Time Off : %(duration)s",
                 employee=holiday.employee_id.name or holiday.category_id.name,
                 duration=holiday.duration_display)
-            allday_value = not holiday.request_unit_half
+            allday_value = not holiday.request_unit_half or holiday.request_date_from_period == 'am' and holiday.request_date_to_period == 'pm'
             if holiday.leave_type_request_unit == 'hour':
                 allday_value = float_compare(holiday.number_of_days, 1.0, 1) >= 0
 
-            leave_tz = pytz.timezone(holiday.tz) if holiday.tz else pytz.UTC
-            start_value = pytz.UTC.localize(holiday.date_from).astimezone(leave_tz).replace(tzinfo=None)
-            stop_value = pytz.UTC.localize(holiday.date_to).astimezone(leave_tz).replace(tzinfo=None)
+            if allday_value:
+                # `start` and `stop` are not in UTC for allday events
+                leave_tz = pytz.timezone(holiday.tz) if holiday.tz else pytz.UTC
+                start_value = pytz.UTC.localize(holiday.date_from).astimezone(leave_tz).replace(tzinfo=None)
+                stop_value = pytz.UTC.localize(holiday.date_to).astimezone(leave_tz).replace(tzinfo=None)
+            else:
+                start_value = holiday.date_from
+                stop_value = holiday.date_to
 
             meeting_values = {
                 'name': meeting_name,
