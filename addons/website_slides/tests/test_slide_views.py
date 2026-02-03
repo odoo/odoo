odoo/odoo

commit 8ceea093815ba9c8d3dd2ced736a9010ccc5b779

[IMP] website*: remove useless invisible fields
Invisible field from views are useless since:

closes odoo/odoo#230098

See: https://github.com/odoo/odoo/commit/5639ed865c5a3b82bab25b0ecac28c68c02e93065639ed8
See: https://github.com/odoo/odoo/pull/137031
Related: odoo/enterprise#96402
Related: odoo/upgrade#8586
Signed-off-by: Rémy Voet (ryv) <ryv@odoo.com>

addons/website_slides/views/slide_question_views.xml
@@ -18,7 +18,6 @@
                     </div>
                     <field name="answer_ids">
                         <list editable="bottom" create="true" delete="true">
-                            <field name="display_name" column_invisible="True"/>
                             <field name="text_value"/>
                             <field name="is_correct"/>
                             <field name="comment"/>

addons/website/views/ir_actions_server_views.xml
@@ -11,7 +11,6 @@
                     <xpath expr="//page[@name='usage']" position="inside">
                         <group invisible="state != 'code' or context.get('is_modal')">
                             <field name="website_published" string="Use in website"/>
-                            <field name="xml_id" invisible="1"/>
                             <field name="website_path" invisible="not website_published"/>
                             <field name="website_url" widget="url" readonly="1" invisible="not website_published"/>
                         </group>

addons/website/views/res_config_settings_views.xml
@@ -45,7 +45,6 @@
                                  documentation="/applications/websites/website/configuration/translate.html">
                             <field name="language_ids" widget="many2many_tags" options="{'no_create': True, 'no_open': True}" required="website_id"/>
                             <div class="row mt8" invisible="website_language_count &lt; 2">
-                                <field name="website_language_count" invisible="1"/>
                                 <label class="col-lg-3" string="Default" for="website_default_lang_id"/>
                                 <field name="website_default_lang_id" options="{'no_open': True, 'no_create': True}" required="website_id"/>
                             </div>

addons/website/views/website_controller_pages_views.xml
@@ -21,7 +21,6 @@
                     </group>
                     <group colspan="1" string="Settings">
                         <field name="model_id" readonly="id"/>
-                        <field name="model" invisible="1" />
                         <field name="record_domain" widget="domain" options="{'in_dialog': True, 'model': 'model'}"/>
                         <field name="default_layout"/>
                     </group>
@@ -54,7 +53,6 @@
             when the group is enabled, but we need the field to be there all the
             time for `PageRendererMixin`'s `recordFilter' to be able to filter
             correctly. -->
-            <field name="website_id" column_invisible="True"/>
             <field name="website_id" groups="website.group_multi_website"/>
             <field name="url_demo"/>
             <field name="is_published" widget="boolean_toggle"/>
@@ -68,7 +66,6 @@
     <field name="arch" type="xml">
         <kanban class="o-website-controller-page-kanban">
             <t t-name="card">
-                <field name="website_published" invisible="1" />
                 <widget name="web_ribbon" text="Published" invisible="not website_published" />
                 <widget name="web_ribbon" text="Unpublished" invisible="website_published" bg_color="text-bg-danger" />
                     <div data-section="data" class="d-flex align-items-center">

addons/website/views/website_pages_views.xml
@@ -54,8 +54,6 @@
     <field name="arch" type="xml">
         <xpath expr="//group/*" position="before"> <!-- Top -->
             <field name="name" string="Page Title" placeholder="e.g. Home Page"/>
-            <field name="website_id" invisible="1"/>
-
             <label for="url" string="Page URL"/>
             <div class="o_col">
                 <field name="url" widget="page_url"/>
@@ -98,7 +96,7 @@
     <field name="priority">99</field>
     <field name="arch" type="xml">
         <list js_class="website_pages_list" type="object" action="open_website_url" multi_edit="1" default_order="url asc, id asc">
-            <field name="is_homepage" column_invisible="True"/>
+            <field name="is_homepage" column_invisible="True"/>  <!-- Use to to display the small house icon from javascript ui -->
             <field name="name" string="Page Title"/>
             <field name="website_url" string="Page URL"/>
 
@@ -212,7 +210,6 @@
                 <field name="key"/>
                 <button title="Show site map" invisible="type != 'qweb'" name="website.action_website_view_hierarchy" icon="fa-sitemap" type="action" class="btn btn-link"/>
             </div>
-            <field name="page_ids" invisible="1" />
             <field name="first_page_id" invisible="not page_ids" />
             <label for="visibility"/>
             <div>
@@ -244,7 +241,6 @@
             <attribute name="decoration-muted">not active</attribute>
         </list>
         <field name="name" position="after">
-            <field name="active" column_invisible="True"/>
             <field name="website_id" groups="website.group_multi_website"/>
         </field>
         <field name="xml_id" position="before">

addons/website/views/website_views.xml
@@ -28,7 +28,6 @@
                             <field name="name"/>
                             <field name="domain"/>
                             <field name="logo" widget="image" class="oe_avatar w-auto float-start"/>
-                            <field name="language_count" invisible="1"/>
                             <field name="company_id" options="{'no_open': True, 'no_create': True}" groups="base.group_multi_company"/>
                             <field name="language_ids" widget="many2many_tags" options="{'no_open': True, 'no_create': True}"/>
                             <field name="default_lang_id" options="{'no_open': True, 'no_create': True}"

addons/website/views/website_visitor_views.xml
@@ -180,7 +180,6 @@
                     </div>
                     <group id="general_info">
                         <group string="Details">
-                            <field name="is_connected" invisible="1"/>
                             <field name="partner_id"/>
                             <field name="email"/>
                             <field name="mobile" class="o_force_ltr"/>
@@ -213,8 +212,6 @@
                 <field name="visit_count"/>
                 <field name="page_ids" widget="many2many_tags" string="Pages"/>
                 <field name="last_visited_page_id" string="Last Page" optional="hide"/>
-                <field name="is_connected" column_invisible="True"/>
-                <field name="email" column_invisible="True"/>
                 <button string="Email" name="action_send_mail" type="object"
                     icon="fa-envelope" invisible="not email"/>
             </list>

addons/website_blog/views/blog_post_add.xml
@@ -7,7 +7,7 @@
     <field name="arch" type="xml">
         <form js_class="website_new_content_form">
             <group>
-                <field name="website_url" invisible="1"/>
+                <field name="website_url" invisible="1"/>  <!-- We need this for `computePath` used in `onAddContent` to redirect to the blog after creation. -->
                 <field name="blog_id" string="Select Blog"/>
                 <field name="name" placeholder="Blog Post Title"/>
             </group>

commit 27fe0a496a0f2e022ad24a5c720659d9a238ccab

[FIX] website_slide: update quiz answer tags without page reload
After adding new answers or editing existing ones in a quiz question
and clicking 'Save & Close', the question overview displays incorrect
data. New answers appear as empty tags, and modified answers continue
to show their old values. The correct data only appears after a full
page reload.

This regression was introduced in commit 8ceea093, where invisible
fields were removed during code cleanup. The `display_name` field
is required in the `answer_ids` list view for the web client to
correctly update its local cache. Without this field, the client
cannot refresh the display names of the tags immediately after
modification.

This commit restores the `display_name` field as `column_invisible`
in the `slide.question` form view. A server-side regression test
is also added to prevent this field from being removed in future
cleanups.

Task-5449335

addons/website_slides/views/slide_question_views.xml
@@ -18,6 +18,7 @@
                     </div>
                     <field name="answer_ids">
                         <list editable="bottom" create="true" delete="true">
+                            <field name="display_name" column_invisible="True"/> <!-- 'display_name' is necessary for the many2many_tags to work on slide_slide_views -->
                             <field name="text_value"/>
                             <field name="is_correct"/>
                             <field name="comment"/>

addons/website_slides/tests/__init__.py
@@ -11,6 +11,7 @@ from . import test_slide_channel
 from . import test_slide_question
 from . import test_slide_resource
 from . import test_slide_slide
+from . import test_slide_views
 from . import test_statistics
 from . import test_ui_wslides
 from . import test_website_slides_technical_page

addons/website_slides/tests/test_slide_views.py
@@ -1,0 +1,12 @@
+from lxml import etree
+from odoo.tests import tagged, TransactionCase
+
+
+@tagged("at_install", "-post_install")
+class TestSlideViews(TransactionCase):
+    def test_slide_question_view_form_has_display_name(self):
+        """Regression Test: Ensure 'display_name' is present in the slide.question form view."""
+        view = self.env.ref("website_slides.slide_question_view_form")
+        doc = etree.fromstring(view.arch_db)
+        nodes = doc.xpath("//field[@name='answer_ids']//list//field[@name='display_name']")
+        self.assertTrue(nodes)