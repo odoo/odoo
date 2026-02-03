da615847b5bf29998bcd03c93b2dbb0024b691de
Laurent Smet (LAS) <las@odoo.com>
2026-02-03 11:34:27 +0000

    [IMP] account_edi_ubl_cii: Prevent BIS3 without EndpointID to be send
    If EndpointID is not set, the generated file is invalid due to the 2
    following rules:
    [PEPPOL-EN16931-R010] Buyer electronic address MUST be provided.
    [PEPPOL-EN16931-R020] Seller electronic address MUST be provided.
    
    Since this is a configuration issue, there is no point of sending such files
    to be rejected right away. Instead, let's add those 2 contraints ODOO-side.
    
    task-5890887
    
    X-original-commit: 876b81bad116a28f0b1ee2089e026462fe95f2fe

--- addons/l10n_account_edi_ubl_cii_tests/tests/test_xml_ubl_sg.py
+++ addons/l10n_account_edi_ubl_cii_tests/tests/test_xml_ubl_sg.py
@@ -22,6 +22,8 @@ class TestUBLSG(TestUBLCommon):
             'bank_ids': [(0, 0, {'acc_number': '000099998B57'})],
             'ref': 'ref_partner_1',
             'invoice_edi_format': 'ubl_sg',
+            'peppol_eas': '0195',
+            'peppol_endpoint': '197401143C',
         })
 
         cls.partner_2 = cls.env['res.partner'].create({
@@ -35,6 +37,8 @@ class TestUBLSG(TestUBLCommon):
             'bank_ids': [(0, 0, {'acc_number': '93999574162167'})],
             'ref': 'ref_partner_2',
             'invoice_edi_format': 'ubl_sg',
+            'peppol_eas': '0195',
+            'peppol_endpoint': 'S16FC0121D',
         })
 
     ####################################################
