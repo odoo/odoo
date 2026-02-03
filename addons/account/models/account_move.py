odoo/odoo

commit 2490d32f2a28cc05c7cd5d8450701232aede2c29

[IMP] account: improve form and list views of bills
This commit improves the UI of bills, and how duplicates bills are
handled and detected, the following changes were made:
1- the "Drag and drop" icon size was too small, the icon size was increased
2- the outstanding debits section needed clearer information  when displaying
   info about the other bills/transactions, dates are now always visible and
   hovering shows the reference
3- There was no use for the print button on a bill, it was removed
4- A bill could be a duplicate even if the reference was not set, before this
   commit, that behavior was not recognized, Also the displayed name for
   duplicate bills was not clear, making it difficult to recognize a bill just
   from its name
5- A delete all button (for duplicate bills) was added

This commit is the first part of this task, second part targetting master
task-5258726

closes odoo/odoo#244266

X-original-commit: 19db5f40b81e25315b85548b1c3979693f379d0b
Related: odoo/enterprise#104609
Signed-off-by: Hugo Poncelet (hupo) <hupo@odoo.com>

addons/account/models/account_move.py
@@ -751,6 +751,7 @@ class AccountMove(models.Model):
         groups="account.group_account_invoice,account.group_account_readonly",
     )
     duplicated_ref_ids = fields.Many2many(comodel_name='account.move', compute='_compute_duplicated_ref_ids')
+    is_draft_duplicated_ref_ids = fields.Boolean(compute="_compute_is_draft_duplicated_ref_ids")
     need_cancel_request = fields.Boolean(compute='_compute_need_cancel_request')
 
     show_update_fpos = fields.Boolean(string="Has Fiscal Position Changed", store=False)  # True if the fiscal position was changed
@@ -1476,13 +1477,14 @@ class AccountMove(models.Model):
                     continue
 
                 payments_widget_vals['content'].append({
-                    'journal_name': line.ref or line.move_id.name,
+                    'move_name': line.ref or line.move_id.name,
                     'amount': amount,
                     'currency_id': move.currency_id.id,
                     'id': line.id,
                     'move_id': line.move_id.id,
                     'date': fields.Date.to_string(line.date),
                     'account_payment_id': line.payment_id.id,
+                    'move_ref': line.ref or "",
                 })
 
             if payments_widget_vals['content']:
@@ -2041,7 +2043,7 @@ class AccountMove(models.Model):
             move.duplicated_ref_ids = move_to_duplicate_move.get(move._origin, self.env['account.move'])
 
     def _fetch_duplicate_reference(self, matching_states=('draft', 'posted')):
-        moves = self.filtered(lambda m: m.is_sale_document() or m.is_purchase_document() and m.ref)
+        moves = self.filtered(lambda m: m.is_sale_document() or m.is_purchase_document())
 
         if not moves:
             return {}
@@ -2090,13 +2092,23 @@ class AccountMove(models.Model):
                 move.move_type in ('in_invoice', 'in_refund')
                 AND duplicate_move.move_type in ('in_invoice', 'in_refund')
                 AND (
-                   move.ref = duplicate_move.ref
-                   AND (
-                       move.invoice_date IS NULL
-                       OR
-                       duplicate_move.invoice_date IS NULL
-                       OR
-                       date_part('year', move.invoice_date) = date_part('year', duplicate_move.invoice_date)
+                   -- case 1: same ref and (no date or same year)
+                     (
+                         move.ref = duplicate_move.ref
+                         AND (
+                             move.invoice_date IS NULL
+                             OR
+                             duplicate_move.invoice_date IS NULL
+                             OR
+                             date_part('year', move.invoice_date) = date_part('year', duplicate_move.invoice_date)
+                         )
+                     )
+                     -- case 2: different refs, same partner, amount and date
+                     OR (
+                            move.commercial_partner_id = duplicate_move.commercial_partner_id
+                            AND move.amount_total = duplicate_move.amount_total
+                            AND move.amount_total != 0.0
+                            AND move.invoice_date = duplicate_move.invoice_date
                    )
                 )
             """)
@@ -2130,6 +2142,11 @@ class AccountMove(models.Model):
             self.env['account.move'].browse(move_id): self.env['account.move'].browse(duplicate_ids)
             for move_id, duplicate_ids in result
         }
+
+    @api.depends('duplicated_ref_ids')
+    def _compute_is_draft_duplicated_ref_ids(self):
+        for move in self:
+            move.is_draft_duplicated_ref_ids = any(duplicate_move.state == 'draft' for duplicate_move in move.duplicated_ref_ids)
 
     @api.depends('company_id')
     def _compute_display_qr_code(self):
@@ -5236,7 +5253,10 @@ class AccountMove(models.Model):
         if self.env.context.get('name_as_amount_total'):
             currency_amount = self.currency_id.format(self.amount_total)
             if self.state == 'posted':
-                return _("%(ref)s (%(currency_amount)s)", ref=(self.ref or self.name), currency_amount=currency_amount)
+                ref = f" - {self.ref}" if self.ref else ""
+                return _("%(name)s%(ref)s at %(currency_amount)s", name=(self.name), ref=ref, currency_amount=currency_amount)
+            if self.name:
+                return _("%(name)s - Draft at (%(currency_amount)s)", name=(self.name), currency_amount=currency_amount)
             else:
                 return _("Draft (%(currency_amount)s)", currency_amount=currency_amount)
         name = ''
@@ -6144,6 +6164,10 @@ class AccountMove(models.Model):
 
     def action_activate_currency(self):
         self.currency_id.filtered(lambda currency: not currency.active).write({'active': True})
+
+    def action_delete_duplicates(self):
+        for move in self:
+            move.duplicated_ref_ids.unlink()
 
     def _get_mail_template(self):
         """

addons/account/static/src/components/account_payment_field/account_payment.xml
@@ -5,64 +5,67 @@
     <t t-name="account.AccountPaymentField">
         <div>
             <t t-set="info" t-value="this.getInfo()"/>
-            <t t-if="info.outstanding">
-                <div>
-                    <strong class="float-start" id="outstanding" t-out="info.title"/>
-                </div>
-            </t>
-            <table style="width:100%;">
-                <t t-foreach="info.lines" t-as="line" t-key="line_index">
-                    <tr>
+            <div class="d-flex flex-column align-items-end">
+                <table class="w-auto">
                     <t t-if="info.outstanding">
-                        <td>
-                            <a title="assign to invoice"
-                               role="button"
-                               class="oe_form_field btn btn-secondary outstanding_credit_assign d-print-none"
-                               t-att-data-id="line.id"
-                               style="margin-right: 0px; padding-left: 5px; padding-right: 5px;"
-                               href="#"
-                               data-bs-toggle="tooltip"
-                               t-on-click.prevent="() => this.assignOutstandingCredit(info.moveId, line.id)">Add</a>
-                        </td>
-                        <td style="max-width: 11em;">
-                            <a t-att-title="(line.bank_label ? line.bank_label + ' - ' : '') + line.formattedDate"
-                               role="button"
-                               class="oe_form_field btn btn-link open_account_move"
-                               t-on-click="() => this.openMove(line.move_id)"
-                               style="margin-right: 5px; text-overflow: ellipsis; overflow: hidden; white-space: nowrap; padding-left: 0px; width:100%; text-align:left;"
-                               data-bs-toggle="tooltip"
-                               t-att-payment-id="account_payment_id"
-                               t-out="line.journal_name"/>
-                        </td>
+                        <tr>
+                            <td colspan="2" class="text-start">
+                                <strong id="outstanding" t-out="info.title"/>
+                            </td>
+                        </tr>
                     </t>
-                    <t t-if="!info.outstanding">
-                        <td>
-                           <a role="button" tabindex="0" class="js_payment_info fa fa-info-circle" t-att-index="line_index" style="margin-right:5px;" aria-label="Info" title="Journal Entry Info" data-bs-toggle="tooltip" t-on-click.stop="(ev) => this.onInfoClick(ev, line)"></a>
+                    <t t-foreach="info.lines" t-as="line" t-key="line_index">
+                        <tr>
+                        <t t-if="info.outstanding">
+                            <td t-out="line.formattedDate"/>
+                            <td style="max-width: 9rem;">
+                                <a t-att-title="(line.bank_label ? line.bank_label + ' - ' : '') + (line.move_ref ? line.move_ref : '')"
+                                   role="button"
+                                   class="open_account_move oe_form_field btn btn-link w-100 text-start"
+                                   t-on-click="() => this.openMove(line.move_id)"
+                                   data-bs-toggle="tooltip"
+                                   t-att-payment-id="account_payment_id"
+                                   t-out="line.move_name"/>
+                            </td>
+                            <td class="ps-2">
+                                <a title="assign to invoice"
+                                   role="button"
+                                   class="oe_form_field btn btn-secondary outstanding_credit_assign d-print-none text-truncate w-100 text-start"
+                                   t-att-data-id="line.id"
+                                   href="#"
+                                   data-bs-toggle="tooltip"
+                                   t-on-click.prevent="() => this.assignOutstandingCredit(info.moveId, line.id)">Add</a>
+                            </td>
+                        </t>
+                        <t t-if="!info.outstanding">
+                            <td>
+                               <a role="button" tabindex="0" class="js_payment_info fa fa-info-circle" t-att-index="line_index" style="margin-right:5px;" aria-label="Info" title="Journal Entry Info" data-bs-toggle="tooltip" t-on-click.stop="(ev) => this.onInfoClick(ev, line)"></a>
+                            </td>
+                            <td t-if="!line.is_exchange">
+                                <i class="o_field_widget text-start o_payment_label">
+                                    <t t-if="line.is_refund">Reversed on </t>
+                                    <t t-else="">Paid on </t>
+                                    <t t-out="line.formattedDate"></t>
+                                </i>
+                            </td>
+                            <td t-if="line.is_exchange" colspan="2">
+                                <i class="o_field_widget text-start text-muted text-start">
+                                    <span class="oe_form_field oe_form_field_float oe_form_field_monetary fw-bold">
+                                        <t t-out="line.amount_formatted"/>
+                                    </span>
+                                    <span> Exchange Difference</span>
+                                </i>
+                            </td>
+                        </t>
+                        <td t-if="!line.is_exchange" class="text-end ps-2 text-nowrap">
+                            <span class="oe_form_field oe_form_field_float oe_form_field_monetary">
+                                <t t-out="line.amount_formatted"/>
+                            </span>
                         </td>
-                        <td t-if="!line.is_exchange">
-                            <i class="o_field_widget text-start o_payment_label">
-                                <t t-if="line.is_refund">Reversed on </t>
-                                <t t-else="">Paid on </t>
-                                <t t-out="line.formattedDate"></t>
-                            </i>
-                        </td>
-                        <td t-if="line.is_exchange" colspan="2">
-                            <i class="o_field_widget text-start text-muted text-start">
-                                <span class="oe_form_field oe_form_field_float oe_form_field_monetary fw-bold">
-                                    <t t-out="line.amount_formatted"/>
-                                </span>
-                                <span> Exchange Difference</span>
-                            </i>
-                        </td>
+                        </tr>
                     </t>
-                    <td t-if="!line.is_exchange" style="text-align:right;">
-                        <span class="oe_form_field oe_form_field_float oe_form_field_monetary" style="margin-left: -10px;">
-                            <t t-out="line.amount_formatted"/>
-                        </span>
-                    </td>
-                    </tr>
-                </t>
-            </table>
+                </table>
+            </div>
         </div>
     </t>
 
@@ -96,7 +99,7 @@
                         <tr>
                             <td class="fw-bolder">Journal:</td>
                             <td class="ps-1">
-                                <t t-out="props.journal_name"/>
+                                <t t-out="props.move_name"/>
                                 <span t-if="props.payment_method_name">
                                     (<t t-out="props.payment_method_name"/>)
                                 </span>

addons/account/static/src/components/bill_guide/bill_guide.js
@@ -46,6 +46,17 @@ export class BillGuide extends Component {
             type: 'object',
         });
     }
+
+    openVendorBill() {
+        return this.action.doAction({
+            type: "ir.actions.act_window",
+            res_model: "account.move",
+            views: [[false, "form"]],
+            context: {
+                default_move_type: "in_invoice",
+            },
+        });
+    }
 }
 
 

addons/account/static/src/components/bill_guide/bill_guide.scss
@@ -18,6 +18,16 @@
     }
 }
 
+.bill-guide-img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+}
+
+.mb-9 {
+    margin-bottom: 9rem;
+}
+
 .account_drag_drop_btn {
     border-style: dashed !important;
     border-color: $o-brand-primary;

addons/account/static/src/components/bill_guide/bill_guide.xml
@@ -1,24 +1,25 @@
 <templates>
 
     <t t-name="account.BillGuide">
-        <div class="d-flex flex-row bill_guide_container mb-3">
-            <div class="bill_guide_left flex-shrink-0 py-3">
-                <div class="text-center">
-                    <img class="img-fluid" src="/web/static/img/folder.svg"/>
-                </div>
-                <div class="text-center mt-2">
-                    <span class="btn account_drag_drop_btn pe-none">Drag &amp; drop</span>
-                    <div>
-                        <span class="btn pe-none px-1 fw-normal">or</span>
-                        <a class="btn btn-link px-0 fw-normal"
-                            href="#"
-                            type="object"
-                            name="action_create_vendor_bill"
-                            journal_type="purchase"
-                            t-on-click="() => this.handleButtonClick('action_create_vendor_bill')"
-                        >
-                            try our sample
-                        </a>
+         <div class="d-flex flex-row bill_guide_container mb-9">
+            <div class="bill_guide_left d-flex align-items-center justify-content-center py-3">
+                <div>
+                    <img class="bill-guide-img" src="/web/static/img/folder.svg"/>
+                    <div class="text-center mt-2">
+                        <span class="btn btn-lg account_drag_drop_btn pe-none">Drag &amp; drop</span>
+                        <div>
+                            <span class="btn pe-none px-1 fw-normal">or</span>
+                            <a class="btn btn-lg btn-link px-0 fw-normal"
+                                href="#"
+                                type="object"
+                                name="action_create_vendor_bill"
+                                journal_type="purchase"
+                                groups="account.group_account_invoice"
+                                t-on-click="() => this.handleButtonClick('action_create_vendor_bill')"
+                            >
+                                try our sample
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -31,11 +32,9 @@
                     <div class="vertical-line border-start flex-grow-1 mb-2"/>
                 </div>
             </div>
-            <div class="bill_guide_right flex-shrink-0 py-3">
+            <div class="bill_guide_right d-flex align-items-center justify-content-center py-3">
                 <div t-if="alias">
-                    <div class="text-center">
-                        <img class="img-fluid" src="/account/static/src/img/bill.svg" alt="Email bills"/>
-                    </div>
+                    <img class="bill-guide-img" src="/account/static/src/img/bill.svg" alt="Email bills"/>
                     <div class="text-center mt-2">
                         <div class="">
                             <span class="btn pe-none px-1 fw-normal">Send a bill to</span>
@@ -54,17 +53,14 @@
                     </div>
                 </div>
                 <div t-else="">
-                    <div class="text-center">
-                        <img class="img-fluid"
-                            src="/web/static/img/bill.svg"
-                            alt="Create bill manually"/>
-                    </div>
+                    <img class="bill-guide-img" src="/web/static/img/bill.svg" alt="Create bill manually"/>
                     <div class="text-center mt-2">
                         <div class="">
                             <a href="#"
                                 type="object"
                                 class="o_invoice_new"
-                                t-on-click="() => this.handleButtonClick('action_create_new')"
+                                t-on-click="() => this.openVendorBill()"
+                                groups="account.group_account_invoice"
                             >
                                 Create a bill manually
                             </a>

addons/account/static/src/components/x2many_buttons/x2many_buttons.js
@@ -9,6 +9,7 @@ class X2ManyButtons extends Component {
     static props = {
         ...standardFieldProps,
         treeLabel: { type: String },
+        nbRecordsShown: { type: Number, optional: true },
     };
 
     setup() {
@@ -50,5 +51,8 @@ X2ManyButtons.template = "account.X2ManyButtons";
 registry.category("fields").add("x2many_buttons", {
     component: X2ManyButtons,
     relatedFields: [{ name: "display_name", type: "char" }],
-    extractProps: ({ string }) => ({ treeLabel: string || _t("Records") }),
+    extractProps: ({ attrs, string }) => ({
+        treeLabel: string || _t("Records"),
+        nbRecordsShown: attrs.nb_records_shown ? parseInt(attrs.nb_records_shown) : 3,
+    }),
 });

addons/account/static/src/components/x2many_buttons/x2many_buttons.xml
@@ -2,14 +2,14 @@
 <templates>
     <t t-name="account.X2ManyButtons">
         <div class="d-flex align-items-center">
-            <t t-foreach="this.currentField.records.slice(0, 3)" t-as="record" t-key="record.id">
+            <t t-foreach="this.currentField.records.slice(0, props.nbRecordsShown)" t-as="record" t-key="record.id">
                 <button class="btn btn-link p-0"
                         t-on-click="() => this.openFormAndDiscard(record.resId)"
                         t-att-data-hotkey="`shift+${record_index + 1}`"
                         t-out="record.data.display_name"/>
                 <span t-if="!record_last" class="pe-1">,</span>
             </t>
-            <t t-if="this.currentField.count gt 3">
+            <t t-if="this.currentField.count gt props.nbRecordsShown">
                 <button class="btn btn-link p-0" t-on-click="() => this.openTreeAndDiscard()" data-hotkey="shift+4">... (View all)</button>
             </t>
         </div>

addons/account/tests/test_account_move_duplicate.py
@@ -88,7 +88,7 @@ class TestAccountMoveDuplicate(AccountTestInvoicingCommon):
         # reassign to trigger the compute method
         invoices_new.ref = invoice_1.ref
         self.assertRecordValues(invoices_new, [
-            {'duplicated_ref_ids': []},
+            {'duplicated_ref_ids': (invoices_new[1]).ids},
             {'duplicated_ref_ids': (invoices_new[0]).ids},
         ])
 
@@ -100,8 +100,8 @@ class TestAccountMoveDuplicate(AccountTestInvoicingCommon):
         # Same ref but different year -> Not duplicated
         bill2 = bill1.copy({'invoice_date': '2020-01-01'})
         bill2.ref = bill1.ref
-        self.assertFalse(bill1.duplicated_ref_ids)
-        self.assertFalse(bill2.duplicated_ref_ids)
+        self.assertNotIn(bill1, bill2.duplicated_ref_ids)
+        self.assertNotIn(bill2, bill1.duplicated_ref_ids)
 
         # Same ref and same year -> Duplicated
         bill3 = bill1.copy({'invoice_date': f"{bill1.invoice_date.year}-04-11"})
@@ -118,3 +118,29 @@ class TestAccountMoveDuplicate(AccountTestInvoicingCommon):
         bill5 = bill4.copy()
         bill5.ref = bill4.ref
         self.assertEqual(bill5.duplicated_ref_ids, bill4)
+
+    def test_in_invoice_single_duplicate_no_reference(self):
+        """ Ensure duplicated bills are recognized with or without
+            a reference when the amount, date, partner are equal"""
+        bill1 = self.invoice.copy({'invoice_date': '2020-01-01'})
+        bill2 = bill1.copy()
+
+        # trigger compute method
+        all_bills = bill1 + bill2
+        all_bills.invoice_date = self.invoice.invoice_date
+
+        # Assert duplicates when there is no ref
+        self.assertIn(bill1, bill2.duplicated_ref_ids)
+        self.assertIn(bill2, bill1.duplicated_ref_ids)
+
+        # Assert duplicates when there is one ref
+        bill1.update({'ref': "bill1 ref"})
+        bill2.update({'ref': bill2.ref})
+        self.assertIn(bill1, bill2.duplicated_ref_ids)
+        self.assertIn(bill2, bill1.duplicated_ref_ids)
+
+        # Assert duplicated when different refs
+        bill1.update({'ref': bill1.ref})
+        bill2.update({'ref': "bill2 ref"})
+        self.assertIn(bill1, bill2.duplicated_ref_ids)
+        self.assertIn(bill2, bill1.duplicated_ref_ids)

commit a3840cfe0886a7786ce4917fee6495fa87486dfa

[FIX] account: payment widget title hover
The forward port https://github.com/odoo/odoo/commit/2490d32f2a28cc05c7cd5d8450701232aede2c29
introduced some minor issues in the payment widget on the
account move form view, such as the title on hover. When
viewing a Bill, for example, with a possible match on a bank
statement, on hovering, we should see both the ref and the line name.

This commit also removes useless added classes in the template.

Finally, when viewing the reconciled widget (click on the
info popup beside the "Paid on ..."), the "Journal" section
was missing.

addons/account/models/account_move.py
@@ -1476,7 +1476,7 @@ class AccountMove(models.Model):
                     'move_id': line.move_id.id,
                     'date': fields.Date.to_string(line.date),
                     'account_payment_id': line.payment_id.id,
-                    'move_ref': line.ref or "",
+                    'ref': line.ref or "",
                 })
 
             if payments_widget_vals['content']:

addons/account/static/src/components/account_payment_field/account_payment.xml
@@ -19,7 +19,7 @@
                         <t t-if="info.outstanding">
                             <td t-out="line.formattedDate"/>
                             <td style="max-width: 9rem;">
-                                <a t-att-title="(line.bank_label ? line.bank_label + ' - ' : '') + (line.move_ref ? line.move_ref : '')"
+                                <a t-att-title="line.bank_label ? (line.ref ? line.bank_label + ' - ' + line.ref : line.bank_label) : (line.ref or '')"
                                    role="button"
                                    class="open_account_move oe_form_field btn btn-link w-100 text-start"
                                    t-on-click="() => this.openMove(line.move_id)"
@@ -30,7 +30,7 @@
                             <td class="ps-2">
                                 <a title="assign to invoice"
                                    role="button"
-                                   class="oe_form_field btn btn-secondary outstanding_credit_assign d-print-none text-truncate w-100 text-start"
+                                   class="oe_form_field btn btn-secondary outstanding_credit_assign d-print-none"
                                    t-att-data-id="line.id"
                                    href="#"
                                    data-bs-toggle="tooltip"
@@ -99,7 +99,7 @@
                         <tr>
                             <td class="fw-bolder">Journal:</td>
                             <td class="ps-1">
-                                <t t-out="props.move_name"/>
+                                <t t-out="props.journal_name"/>
                                 <span t-if="props.payment_method_name">
                                     (<t t-out="props.payment_method_name"/>)
                                 </span>