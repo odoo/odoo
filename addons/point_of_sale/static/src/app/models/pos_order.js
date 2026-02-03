29da955da6f917b9a360a05d04082d7085a3ade7
Pedram (PEBR) <pebr@odoo.com>
2026-02-03 15:21:00 +0000

    [FIX] point_of_sale: prevent setting non available pricelist on order
    When changing the customer on a POS order, if the customer's
    pricelist is not in the list of available pricelists for the POS, but
    the pricelist was loaded due to loading a paid order, the POS
    would still set that pricelist on the order.
    
    opw-5461556
    
    closes odoo/odoo#251083
    
    X-original-commit: 1fc512e19ffbf688299cf5b99d093fd04556d4d3
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/point_of_sale/static/src/app/models/pos_order.js
+++ addons/point_of_sale/static/src/app/models/pos_order.js
@@ -660,7 +660,7 @@ export class PosOrder extends PosOrderAccounting {
                   )
                 : defaultFiscalPosition;
             newPartnerPricelist =
-                this.models["product.pricelist"].find(
+                this.config.available_pricelist_ids.find(
                     (pricelist) => pricelist.id === newPartner.property_product_pricelist?.id
                 ) || this.config.pricelist_id;
         } else {
