8a2a039a590f7ed29bf5c7a0d657354d69576d62
Alex Kühn <aku@odoo.com>
2026-02-04 17:17:11 +0000

    [FIX] mail: restore formatting of full composer content
    Before this commit, when using the full composer and accidentally
    closing it with some content, the composer content was recovered on
    the basic composer and formatting was lost.
    
    This happens because restore of content only works in the basic
    composer, and basic composer does not support rich HTML like the full
    composer.
    
    This commit adds a new UX/UI to restore formatting when accidentally
    leaving the full composer:
    
    When some content has been restored from full composer, opening the
    chatter composer momentarily disables everything but the full
    composer button, in addition to show a popover suggesting the user
    to decide to either continue with Full Composer and restore
    formatting, or restore content in the small composer without the
    formatting.
    
    Most of the time people want to restore formatting from the full
    composer, but at the same time the full composer can be a frustrating
    experience that may incite to just continue with the more reliable
    small composer. This new popover support both use-cases.
    
    Task-5910961
    
    closes odoo/odoo#252318
    
    X-original-commit: fda52539b8423c25f2a1762aa9777ede378b6fbe
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/core/common/action.js
+++ addons/mail/static/src/core/common/action.js
@@ -8,6 +8,7 @@ import { Reactive } from "@web/core/utils/reactive";
 export const ACTION_TAGS = Object.freeze({
     DANGER: "DANGER",
     SUCCESS: "SUCCESS",
+    PRIMARY: "PRIMARY",
     IMPORTANT_BADGE: "IMPORTANT_BADGE",
     WARNING_BADGE: "WARNING_BADGE",
     CALL_LAYOUT: "CALL_LAYOUT",
@@ -48,6 +49,7 @@ export const ACTION_TAGS = Object.freeze({
  * @property {DropdownState|(action: Action) => DropdownState} [dropdownState]
  * @property {string|(action: Action) => string} [dropdownTemplate]
  * @property {Object|(action: Action) => Object} [dropdownTemplateParams]
+ * @property {boolean|(action: Action) => boolean} [hasBtnBg]
  * @property {string|(action: Action) => string} [hotkey]
  * @property {string|(action: Action) => string} [icon]
  * @property {boolean|(action: Action) => boolean} [inlineName=false]
@@ -368,6 +370,17 @@ export class Action {
             (typeof this.definition.dropdownTemplateParams === "function"
                 ? this.definition.dropdownTemplateParams.call(this, this.params)
                 : this.definition.dropdownTemplateParams)
+        );
+    }
+
+    /** @param {Action} action @returns {boolean|undefined} */
+    _hasBtnBg(action) {}
+    get hasBtnBg() {
+        return (
+            this._hasBtnBg(this.params) ??
+            (typeof this.definition.hasBtnBg === "function"
+                ? this.definition.hasBtnBg.call(this, this.params)
+                : this.definition.hasBtnBg)
         );
     }
 
