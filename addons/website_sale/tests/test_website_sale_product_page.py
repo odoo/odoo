07fccdf990501d42f278d47ec212a5eb6a7ade18
Farah (ahmfa) <ahmfa@odoo.com>
2026-02-04 14:58:36 +0100

    [FIX] website_sale: show contact us button for zero-priced variants
    Steps to reproduce:
    ===================
    - Download eCommerce app.
    - Create a product priced at zero.
    - Add an attribute and make the FIRST value of the attribute priced at more than 0.
    - Check Configuration > Settings > Prevent Sale of Zero Priced Product.
    - Open the created product on the website, choose the zero-priced variant.
    
    Issue:
    =====
    The 'Contact Us' button doesn't appear when you view the zero-priced variant. This happens because when you initially load the product that has a positive price, the contact us component isn't loaded because it has an if condition that checks if the price is above 0.
    https://github.com/odoo/odoo/blob/d68322a7a21cf1f73223c7a38aa621f3464a0184/addons/website_sale/views/templates.xml#L2147-L2154
    Consequently, when https://github.com/odoo/odoo/blob/20e09fe9e9061365f2a47f76208f00f1079d1f0a/addons/website_sale/static/src/js/variant_mixin.js#L302-L315 tries to load the 'Contact Us' button via the query selector, it gets null as a return value.
    The test made for this issue loaded the variant that has the zero-price first, so the 'Contact Us' button was loaded normally.
    
    Note about the test:
    ====================
    Edit the test so that the positive priced variant is loaded first, which will fail in case that the fix isn't applied, as the 'contact-us-wrapper' component won't be loaded at the start of the test.
    
    opw-5491935
    
    closes odoo/odoo#247220
    
    Signed-off-by: Valeriya Chuprina (vchu) <vchu@odoo.com>

--- addons/website_sale/tests/test_website_sale_product_page.py
+++ addons/website_sale/tests/test_website_sale_product_page.py
@@ -25,8 +25,8 @@ class TestWebsiteSaleProductPage(HttpCase, ProductVariantsCommon, WebsiteSaleCom
         self.website.prevent_zero_price_sale = True
 
         self.product_template_sofa.list_price = 0
-        red_sofa, blue_sofa = self.product_template_sofa.product_variant_ids[:2]
-        blue_sofa.product_template_attribute_value_ids.price_extra = 20
+        red_sofa = self.product_template_sofa.product_variant_ids[:1]
+        red_sofa.product_template_attribute_value_ids.price_extra = 20
 
         self.start_tour(red_sofa.website_url, 'website_sale_contact_us_button')
 
