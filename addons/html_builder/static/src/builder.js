9690e7fe75befa4c52864dadb93200bf78982630
Sébastien (blse) <blse@odoo.com>
2026-02-04 14:57:57 +0000

    [FIX] html_builder, *: allow inline at root for a, span, ...
    Since [website builder refactor], the editor wraps inline elements at
    the root of an editable boundary. This is causing issues in website
    builder when the root element is not supposed to contain `p` or `div`
    elements (added to wrap the inline elements).
    
    This behavior can be configured globally with `allowInlineAtRoot`. And
    since 6bc5946796a364634cb5eb217e61a5b882a1b6b2, it can be configured
    per-element with a "predicate" resource.
    
    This commit adds a predicate to allow inline at root of editable when
    the root is (according to its tag) not supposed to contain `p` or `div`
    elements.
    
    Steps to reproduce:
    - Open website builder on a product page
    - Select text in "Add to cart" button
    - Copy
    - Paste
    - Bug: a `p` element is created
    
    (on master)
    - Open website builder
    - Select "Company" below footer
    - Copy
    - Paste
    - Bug: a `p` element is created
    
    [website builder refactor]: 9fe45e2b7ddbbfd0445ffe25a859e67a316d02b2
    
    task-5868460
    
    closes odoo/odoo#257704
    
    X-original-commit: 38b04461bb4ec3691a591c825ecacde7f5db6a6e
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Sébastien Blondiau (blse) <blse@odoo.com>

--- addons/html_builder/static/src/builder.js
+++ addons/html_builder/static/src/builder.js
@@ -27,6 +27,14 @@ import { getHtmlStyle } from "@html_editor/utils/formatting";
 import { isVisible } from "@html_builder/utils/utils";
 import { localization } from "@web/core/l10n/localization";
 
+// These elements should only have inline content (even if they have a `block`
+// display style, for example if they are in a flex)
+// NOTE: h1, h2, ..., p, pre already prevents wrapping their children into block
+const ONLY_ALLOW_INLINE_TAGS = new Set([
+    ...["a", "em", "strong", "small", "s", "cite", "q", "abbr", "data", "time", "code"],
+    ...["samp", "sub", "sup", "i", "b", "u", "mark", "bdi", "span", "label", "button"],
+]);
+
 /**
  * @typedef {(() => void)[]} on_mobile_preview_clicked
  * @typedef {(() => void)[]} trigger_dom_updated
@@ -174,6 +182,8 @@ export class Builder extends Component {
                     }),
                     unsplittable_node_predicates: (/** @type {Node} */ node) =>
                         node.querySelector?.("[data-oe-translation-source-sha]"),
+                    are_inlines_allowed_at_root_predicates: (el) =>
+                        ONLY_ALLOW_INLINE_TAGS.has(el.tagName.toLowerCase()) || undefined,
                 },
                 localOverlayContainers: {
                     key: this.env.localOverlayContainerKey,
