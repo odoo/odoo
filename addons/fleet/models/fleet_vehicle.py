a0affbc4fdc4e5a35eec41a80cfdec9b9eec419e
usseif <yoahm@odoo.com>
2026-02-04 14:04:18 +0100

    [IMP] l10n_be_hr_payroll:  Car Co2 Fee and BIK . Add missing health index parameter values . Convert indexed parameters to non-indexed values for better adaptability to health index changes . Add greening parameter values . Update CO₂ fee calculations to apply the greening factor for vehicles ordered after July 2023 . Update minimum CO₂ fee calculations . Add employee_driver_date_start and employee_driver_date_end to track the employee’s vehicle usage period . Update ATN (BIK) car calculations . Compute yearly ATN based on used calendar days / total days in the year (365 or 366) . Prorate monthly ATN based on the actual vehicle usage period within the month . Convert CAR.ATN to CAR.BIK
    task-5473706

--- addons/fleet/models/fleet_vehicle.py
+++ addons/fleet/models/fleet_vehicle.py
@@ -397,17 +397,17 @@ class FleetVehicle(models.Model):
         if 'odometer' in vals and any(vehicle.odometer > vals['odometer'] for vehicle in self):
             raise UserError(_('The odometer value cannot be lower than the previous one.'))
 
-        if 'driver_id' in vals and vals['driver_id']:
-            driver_id = vals['driver_id']
-            for vehicle in self.filtered(lambda v: v.driver_id.id != driver_id):
-                # We need to set an end date for the vehicle assignation log
-                driver_assignation = self.env["fleet.vehicle.assignation.log"].search([
-                    ("vehicle_id", "=", vehicle.id),
-                    ("driver_id", "=", vehicle.driver_id.id),
-                ], order="write_date desc", limit="1")
-                if not driver_assignation.date_end:
-                    driver_assignation.date_end = datetime.today().date()
-                vehicle.create_driver_history(vals)
+        if 'driver_id' in vals:
+            new_driver_id = vals['driver_id']
+            vehicles_to_update = self.filtered(lambda v: v.driver_id.id != new_driver_id)
+            if vehicles_to_update:
+                self.env['fleet.vehicle.assignation.log'].search([
+                    ('vehicle_id', 'in', vehicles_to_update.ids),
+                    ('date_end', '=', False)
+                ]).write({'date_end': fields.Date.today()})
+                if new_driver_id:
+                    for vehicle in vehicles_to_update:
+                        vehicle.create_driver_history(vals)
 
         if 'future_driver_id' in vals and vals['future_driver_id']:
             future_driver = vals['future_driver_id']
@@ -467,7 +467,7 @@ class FleetVehicle(models.Model):
         return {
             'vehicle_id': self.id,
             'driver_id': vals['driver_id'],
-            'date_start': fields.Date.today(),
+            'date_start': fields.Date.today()
         }
 
     def create_driver_history(self, vals):
