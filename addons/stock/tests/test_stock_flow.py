f688c6b66310438fa3e36a207770a63d0d8fffa5
David Van Droogenbroeck (DROD) <drod@odoo.com>
2026-02-05 15:28:36 +0100

    [FIX] stock: make putaway strategy default only to child locations
    Steps to reproduce
    -----
    - Enable locations & by products
    - Create 4 locations
    	- View A of type view (parent: Stock)
    	- View B of type view (parent: Stock)
    	- Storage A of type internal location (parent: View A)
    	- Storage B of type internal location (parent: View B)
    - Create a "Manufacture to A" route
    	- Rule 1: Manufacture, Stock -> Stock
    	- Rule 2: Push To, Internal Transfer, Stock -> View A
    - Create a "By Products to B" route
    	- Rule 1: Push To, Internal Transfer, Stock -> View B
    - Create a "Bonus" product with route "By Products to B"
    - Create a "Finished" product with route "Manufacture to A"
    	- BoM with "Bonus" as by product
    - Create a MO for Finished, and produce it
    - Open the linked transfers' list view
    - Open the "Bonus" transfer
    
    > "Destination Location" states View B
    
    - Open the line's details (hamburger button)
    
    > "Store To" states View A/Storage A
    
    Cause
    -----
    When retrieving the location in
    
    https://github.com/odoo/odoo/blob/26dbbdaf460a91ef4b33392c27a28951d5de2c57/addons/stock/models/stock_move_line.py#L280-L282
    
    we pass `locations` as a context key. The problem is that `locations` contains
    the childs of **all** of the SMLs' locations
    
    https://github.com/odoo/odoo/blob/26dbbdaf460a91ef4b33392c27a28951d5de2c57/addons/stock/models/stock_move_line.py#L260
    
    This means that, in `_get_putaway_strategy`, when not `putaway_location` is
    found, we end up defaulting to the first element of the list
    
    https://github.com/odoo/odoo/blob/26dbbdaf460a91ef4b33392c27a28951d5de2c57/addons/stock/models/stock_location.py#L370-L371
    
    which might not be a child of the location.
    
    Solution
    -----
    By removing the context key, locations get populated as such
    
    https://github.com/odoo/odoo/blob/26dbbdaf460a91ef4b33392c27a28951d5de2c57/addons/stock/models/stock_location.py#L328-L330
    
    This is ok because we know the call to `_get_putaway_strategy` is made on a
    single record (`sml.move_id.location_dest_id`) so the default will correctly be
    a child of said location.
    
    -----
    Ticket:
    opw-5359945
    
    closes odoo/odoo#247403
    
    Signed-off-by: Quentin Wolfs (quwo) <quwo@odoo.com>

--- addons/stock/tests/test_stock_flow.py
+++ addons/stock/tests/test_stock_flow.py
@@ -4,7 +4,7 @@ from odoo.addons.stock.tests.common import TestStockCommon
 from odoo.exceptions import ValidationError
 from odoo.tests import Form, tagged
 from odoo.tools import mute_logger, float_round
-from odoo import fields
+from odoo import Command, fields
 
 
 class TestStockFlow(TestStockCommon):
@@ -2598,6 +2598,58 @@ class TestStockFlow(TestStockCommon):
         backorder_wizard.process()
         bo = self.env['stock.picking'].search([('backorder_id', '=', picking.id)])
         self.assertEqual(bo.state, 'assigned')
+
+    def test_multiple_moves_with_different_destinations_putaway_strategy(self):
+        '''
+        Ensure that, when assigning a batch of moves with different destinations,
+        putaway strategy correctly defaults to child locations.
+        '''
+        view_a, view_b = self.env['stock.location'].create([{
+            'name': 'View A',
+            'usage': 'view',
+            'location_id': self.stock_location,
+        }, {
+            'name': 'View B',
+            'usage': 'view',
+            'location_id': self.stock_location,
+        }])
+        child_a, child_b = self.env['stock.location'].create([{
+            'name': 'Child A',
+            'usage': 'internal',
+            'location_id': view_a.id,
+        }, {
+            'name': 'Child B',
+            'usage': 'internal',
+            'location_id': view_b.id,
+        }])
+        picking_1, picking_2 = self.env['stock.picking'].create([{
+            'location_id': self.supplier_location,
+            'location_dest_id': view_a.id,
+            'picking_type_id': self.picking_type_in,
+            'move_ids': [Command.create({
+                'name': 'SML should end up in Child A',
+                'location_id': self.supplier_location,
+                'location_dest_id': view_a.id,
+                'product_id': self.productA.id,
+                'product_uom_qty': 1.0,
+            })],
+        }, {
+            'location_id': self.supplier_location,
+            'location_dest_id': view_b.id,
+            'picking_type_id': self.picking_type_in,
+            'state': 'draft',
+            'move_ids': [Command.create({
+                'name': 'SML should end up in Child B',
+                'location_id': self.supplier_location,
+                'location_dest_id': view_b.id,
+                'product_id': self.productB.id,
+                'product_uom_qty': 1.0,
+            })],
+        }])
+        (picking_1 | picking_2).action_confirm()
+        self.assertEqual(picking_1.move_ids.move_line_ids.location_dest_id, child_a)
+        self.assertEqual(picking_2.move_ids.move_line_ids.location_dest_id, child_b)
+
 
 @tagged('-at_install', 'post_install')
 class TestStockFlowPostInstall(TestStockCommon):
