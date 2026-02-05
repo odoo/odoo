a2da44c416f8aac53d7a5097fb1c8dc1acb9870c
Ai Lin Zuccarello (aizu) <aizu@odoo.com>
2026-02-05 14:10:19 +0100

    [IMP] l10n_ro_edi: download official bill PDF from ANAF
    --Before--
    
    For bills from the Romanian ANAF we only downloaded an XML and imported the data. There is no visual aid for the accountant to see and compare the received document. 
    
    --Now--
    
    We use the ANAF service to get the  official "PDF" version of the invoice. This is requested for any new bill we get through ANAF that doesn't already contain a pdf from the vendor and it is set as the main attachment. 
    
    task-5877171
    
    closes odoo/odoo#247382
    
    Signed-off-by: Antoine Boonen (aboo) <aboo@odoo.com>

--- addons/l10n_ro_edi/models/ciusro_document.py
+++ addons/l10n_ro_edi/models/ciusro_document.py
@@ -17,16 +17,22 @@ def make_efactura_request(session, company, endpoint, method, params, data=None)
 
     :param session: ``requests`` or ``requests.Session()`` object
     :param company: ``res.company`` object containing l10n_ro_edi_test_env, l10n_ro_edi_access_token
-    :param endpoint: ``upload`` (for sending) | ``stareMesaj`` (for fetching status) | ``descarcare`` (for downloading answer)
+    :param endpoint: ``upload`` (for sending) | ``stareMesaj`` (for fetching status) | ``descarcare`` (for downloading answer)  | ``transformare`` (to get the official PDF from efactura)
     :param method: ``post`` (for `upload`) | ``get`` (for `stareMesaj` | `descarcare`)
     :param params: Dictionary of query parameters
     :param data: XML data for ``upload`` request
     :return: Dictionary of {'error': <str>} or {'content': <response.content>} from E-Factura
     """
     send_mode = 'test' if company.l10n_ro_edi_test_env else 'prod'
-    url = f"https://api.anaf.ro/{send_mode}/FCTEL/rest/{endpoint}"
-    headers = {'Content-Type': 'application/xml',
-               'Authorization': f'Bearer {company.l10n_ro_edi_access_token}'}
+    if endpoint == 'transformare':
+        url = "https://webservicesp.anaf.ro/prod/FCTEL/rest/transformare/FACT1/DA"
+        headers = {'Content-Type': 'text/plain'}
+    else:
+        url = f"https://api.anaf.ro/{send_mode}/FCTEL/rest/{endpoint}"
+        headers = {
+            'Content-Type': 'application/xml',
+            'Authorization': f'Bearer {company.l10n_ro_edi_access_token}',
+        }
 
     try:
         response = session.request(method=method, url=url, params=params, data=data, headers=headers, timeout=60)
