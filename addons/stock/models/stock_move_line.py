d8ae369ccbc73c68e531ae0d65af2b12074e7a18
David Van Droogenbroeck (DROD) <drod@odoo.com>
2026-02-05 14:28:36 +0000

    [FIX] stock: make putaway strategy default only to child locations
    Steps to reproduce
    -----
    - Enable locations & by products
    - Create 4 locations
    	- View A of type view (parent: Stock)
    	- View B of type view (parent: Stock)
    	- Storage A of type internal location (parent: View A)
    	- Storage B of type internal location (parent: View B)
    - Create a "Manufacture to A" route
    	- Rule 1: Manufacture, Stock -> Stock
    	- Rule 2: Push To, Internal Transfer, Stock -> View A
    - Create a "By Products to B" route
    	- Rule 1: Push To, Internal Transfer, Stock -> View B
    - Create a "Bonus" product with route "By Products to B"
    - Create a "Finished" product with route "Manufacture to A"
    	- BoM with "Bonus" as by product
    - Create a MO for Finished, and produce it
    - Open the linked transfers' list view
    - Open the "Bonus" transfer
    
    > "Destination Location" states View B
    
    - Open the line's details (hamburger button)
    
    > "Store To" states View A/Storage A
    
    Cause
    -----
    When retrieving the location in
    
    https://github.com/odoo/odoo/blob/26dbbdaf460a91ef4b33392c27a28951d5de2c57/addons/stock/models/stock_move_line.py#L280-L282
    
    we pass `locations` as a context key. The problem is that `locations` contains
    the childs of **all** of the SMLs' locations
    
    https://github.com/odoo/odoo/blob/26dbbdaf460a91ef4b33392c27a28951d5de2c57/addons/stock/models/stock_move_line.py#L260
    
    This means that, in `_get_putaway_strategy`, when not `putaway_location` is
    found, we end up defaulting to the first element of the list
    
    https://github.com/odoo/odoo/blob/26dbbdaf460a91ef4b33392c27a28951d5de2c57/addons/stock/models/stock_location.py#L370-L371
    
    which might not be a child of the location.
    
    Solution
    -----
    By removing the context key, locations get populated as such
    
    https://github.com/odoo/odoo/blob/26dbbdaf460a91ef4b33392c27a28951d5de2c57/addons/stock/models/stock_location.py#L328-L330
    
    This is ok because we know the call to `_get_putaway_strategy` is made on a
    single record (`sml.move_id.location_dest_id`) so the default will correctly be
    a child of said location.
    
    -----
    Ticket:
    opw-5359945
    
    X-original-commit: 37c787ed7ca129bbccd02a073b1e03729b22c2b8

--- addons/stock/models/stock_move_line.py
+++ addons/stock/models/stock_move_line.py
@@ -278,7 +278,7 @@ class StockMoveLine(models.Model):
                         grouped_smls.location_dest_id = move.location_dest_id
             else:
                 for sml in smls:
-                    putaway_loc_id = sml.move_id.location_dest_id.with_context(exclude_sml_ids=excluded_smls, locations=locations)._get_putaway_strategy(
+                    putaway_loc_id = sml.move_id.location_dest_id.with_context(exclude_sml_ids=excluded_smls)._get_putaway_strategy(
                         sml.product_id, quantity=sml.quantity, packaging=sml.move_id.packaging_uom_id,
                     )
                     if putaway_loc_id != sml.location_dest_id:
