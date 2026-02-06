6d62128069088630323969d3b91ffe2dfefb5c62
vpan-odoo <vpan@odoo.com>
2026-02-06 18:03:08 +0530

    [IMP] l10n_in_pos: improve refund receipt label
    Before this commit:
    ===================
    - Both normal orders and refund orders showed `INVOICE`
    - When invoiced, both showed `Tax Invoice` on `l10n_in_pos` receipts.
    
    After this commit:
    ==================
    - Refund receipts now show `R-INVOICE`
    - When an order is invoiced and then refunded, it will show `Tax R-Invoice`.
    
    Task: 5895213

--- addons/l10n_in_pos/tests/test_in_pos.py
+++ addons/l10n_in_pos/tests/test_in_pos.py
@@ -1,10 +1,11 @@
 from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 from odoo.addons.point_of_sale.tests.test_generic_localization import TestGenericLocalization
+from odoo.addons.point_of_sale.tests.common import CommonPosTest
 from odoo.tests import tagged
 
 
 @tagged('post_install', '-at_install', 'post_install_l10n')
-class TestGenericIN(TestGenericLocalization):
+class TestGenericIN(TestGenericLocalization, CommonPosTest):
 
     @classmethod
     @AccountTestInvoicingCommon.setup_country('in')
@@ -32,3 +33,32 @@ class TestGenericIN(TestGenericLocalization):
         _, html = super().test_generic_localization()
         self.assertTrue("HSN Code" in html)
         self.assertTrue("Tax Invoice" in html)
+
+    def _get_refund_receipt_html(self, to_invoice=False):
+        _, refund = self.create_backend_pos_order({
+            'pos_config': self.main_pos_config,
+            'order_data': {
+                'partner_id': self.partner_a.id,
+                'to_invoice': to_invoice,
+            },
+            'line_data': [
+                {'product_id': self.product_a.id},
+            ],
+            'payment_data': [
+                {
+                    'payment_method_id': self.main_pos_config.payment_method_ids[0].id,
+                }
+            ],
+            'refund_data': [
+                {
+                    'payment_method_id': self.main_pos_config.payment_method_ids[0].id,
+                }
+            ],
+        })
+        return refund.order_receipt_generate_html()
+
+    def test_refund_order_receipt(self):
+        self.assertIn("R-INVOICE", self._get_refund_receipt_html())
+
+    def test_refund_invoice_order_receipt(self):
+        self.assertIn("Tax R-Invoice", self._get_refund_receipt_html(to_invoice=True))
