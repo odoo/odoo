59c994a5226149f438673df962b29a8fe52da1bf
Michaël Mattiello <mcm@odoo.com>
2026-02-06 08:44:55 +0100

    [IMP] owl: upgrade code for onRendered migration

--- odoo/upgrade_code/19.1-00-owl3-migration.py
+++ odoo/upgrade_code/19.1-00-owl3-migration.py
@@ -244,11 +244,32 @@ def upgrade_onwillrender(file_manager, log_info, log_error):
         file_manager.print_progress(fileno, len(js_files))
 
 
+def upgrade_onrendered(file_manager, log_info, log_error):
+    """Sub-task: Migrate onRendered, ignoring comments."""
+    js_files = [
+        file for file in file_manager
+        if '/static/src/' in file.path._str
+        and file.path.suffix == '.js'
+        and file.path.name not in EXCLUDED_FILES
+    ]
+
+    for fileno, file in enumerate(js_files, start=1):
+        try:
+            if not JSTooling.has_active_usage(file.content, 'onRendered'):
+                continue
+            file.content = JSTooling.remove_import(file.content, 'onRendered', '@odoo/owl')
+            file.content = JSTooling.add_import(file.content, 'onRendered', '@web/owl2/utils')
+        except Exception as e:
+            log_error(file.path, e)
+        file_manager.print_progress(fileno, len(js_files))
+
+
 def upgrade(file_manager) -> str:
     """Main upgrade_code entry point."""
     collector = MigrationCollector(file_manager)
 
     collector.run_sub("Migrating useEffect", upgrade_useeffect)
     collector.run_sub("Migrating onWillRender", upgrade_onwillrender)
+    collector.run_sub("Migrating onRendered", upgrade_onrendered)
 
     collector.finalize()
