0825f396a01b0fafc0d4d597ee9c905e3627e02c
unka-odoo <unka@odoo.com>
2026-02-06 06:03:22 +0000

    [IMP] l10n_es_pos: add deprecated tag in the name of the app
    - We are waiting for the `saas-19.2` version to be released with the new
      Spain POS module with SII compatibility.
    - Until then, we are just adding `[Deprecated]` tag to the
      `Spain - Point of Sale` module name.
    
    - TODO: remove this `l10n_es_pos` module in the master, after `saas-19.2`
      version release.
    
    Task-5354717
    
    closes odoo/odoo#247671
    
    X-original-commit: 85f3a4a0cd475af68e69998c1eb68cf48ee90b30
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>

--- addons/l10n_es_pos/__manifest__.py
+++ addons/l10n_es_pos/__manifest__.py
@@ -1,6 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Spain - Point of Sale',
+    'name': '[Deprecated] Spain - Point of Sale',
     'category': 'Accounting/Localizations/Point of Sale',
     'summary': """Spanish localization for Point of Sale""",
     'depends': ['point_of_sale', 'l10n_es'],
