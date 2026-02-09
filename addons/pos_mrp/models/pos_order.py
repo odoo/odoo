6c21b2461735af33ca73178bb887632307a44184
Pedram (PEBR) <pebr@odoo.com>
2026-02-09 16:52:14 +0100

    [FIX] pos_mrp: correctly compute the cost of a kit
    Before this commit, if a kit had multiple components, the cost of the
    line was counted as zero if the product cost method was FIFO or average.
    
    opw-5911338
    
    closes odoo/odoo#247833
    
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/pos_mrp/models/pos_order.py
+++ addons/pos_mrp/models/pos_order.py
@@ -16,7 +16,7 @@ class PosOrderLine(models.Model):
         #Get a flat list of all bom_line_ids
         bom_line_ids = [item.id for x in boms for item in x[0].bom_line_ids if set(item.bom_product_template_attribute_value_ids.ids).issubset(product.product_template_variant_value_ids.ids)]
         ml_product_to_consider = (product.bom_ids and [comp[0].product_id.id for comp in components]) or [product.id]
-        return stock_moves.filtered(lambda ml: ml.product_id.id in ml_product_to_consider and (ml.bom_line_id.id in bom_line_ids))
+        return stock_moves.filtered(lambda ml: ml.product_id.id in ml_product_to_consider and (ml.bom_line_id.id in bom_line_ids)).with_context(pos_order_line_id=self.id, bom_id=bom.id)
 
 
 class PosOrder(models.Model):
