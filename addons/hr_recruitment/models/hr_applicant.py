05658927a6bfb540aa63dbc9fe99850da45f1f74
pajo <pajo@odoo.com>
2026-02-09 12:58:41 +0000

    [FIX] hr_recruitment: fix applicant priority issue
    Steps to reproduce:
    ----------------------------------------
    1. Install the Recruitment module
    2. Navigate to Recruitment > Applications > All Applications
    3. Go to Kanban view
    4. Make any application Very High Priority (3 star)
    5. Refresh the page
    
    Observation:
    ----------------------------------------
    Applications marked as Very High Priority are not displayed on top, Applicants
    with higher priority should be displayed first within the same stage.
    
    Issue:
    ----------------------------------------
    The `_order` attribute on the applicant model is declared twice, causing the
    intended priority-based ordering to be overridden by `sequence`.
    https://github.com/odoo/odoo/blob/6fed805389d878558e4139b270ee7a70269af767/addons/hr_recruitment/models/hr_applicant.py#L28-L41
    
    Solution:
    ----------------------------------------
    Reorder the fields in the `_order` attribute so that priority is applied before
    sequence, ensuring higher-priority applicants are shown first
    
    opw-5893371
    
    X-original-commit: 587e90707c476ca624d888e0d7e4eee08bb5fa87

--- addons/hr_recruitment/models/hr_applicant.py
+++ addons/hr_recruitment/models/hr_applicant.py
@@ -23,7 +23,7 @@ AVAILABLE_PRIORITIES = [
 class HrApplicant(models.Model):
     _name = 'hr.applicant'
     _description = "Applicant"
-    _order = "priority desc, id desc"
+    _order = "priority desc, sequence, id desc"
     _inherit = [
                'mail.thread.main.attachment',
                'mail.thread.blacklist',
