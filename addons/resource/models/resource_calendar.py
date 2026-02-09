27c58fa8cb9dd86494e57fbca23ecfea4faa2c8b
ksar-odoo <ksar@odoo.com>
2026-02-09 10:47:24 +0000

    [FIX] resource: resource half day is not correct if day period is full day
    Steps to reproduce:
    - Go to any working schedule of an employee.
    - Add a working hour line for any day and choose day period as full day.
    - Change work from 10:00, and work to 18:00.
    
    Issue:
    - Resource was explicitly setting 12  if any hour_from/hour_to was missing.
    - Resource always consider that the working time is 8AM-5PM.
    
    Fix:
    - We will calculate the avg of working hours( hour_from + hour_to)/2
    - Doing this we will always get the middle of day.
    
    Task: 5912748
    X-original-commit: bc3b4541232e31423549dde1b60527d5b2be32a1

--- addons/resource/models/resource_calendar.py
+++ addons/resource/models/resource_calendar.py
@@ -882,6 +882,88 @@ class ResourceCalendar(models.Model):
 
     def _is_duration_based_on_date(self, date):
         self.ensure_one()
+<<<<<<< 3e43f9e1c0d099f69d26cff82c38f73784bd67ec
+||||||| 52180c9ff3c3a3372a118fb40e133265bbaab22b
+        if not target_date:
+            err = "Target Date cannot be empty"
+            raise ValueError(err)
+        if self.flexible_hours:
+            # Quick calculation to center flexible hours around 12PM midday
+            datetimes = [12.0 - self.hours_per_day / 2.0, 12.0, 12.0 + self.hours_per_day / 2.0]
+            if day_period:
+                return (datetimes[0], datetimes[1]) if day_period == 'morning' else (datetimes[1], datetimes[2])
+            return (datetimes[0], datetimes[2])
+
+        domain = [
+            ('calendar_id', '=', self.id),
+            ('display_type', '=', False),
+            ('day_period', '!=', 'lunch'),
+        ]
+
+        init_attendances = self.env['resource.calendar.attendance']._read_group(domain=domain,
+        groupby=['week_type', 'dayofweek', 'day_period'],
+        aggregates=['hour_from:min', 'hour_to:max'],
+        order='dayofweek,hour_from:min')
+
+        init_attendances = [DummyAttendance(hour_from, hour_to, dayofweek, day_period, week_type)
+            for week_type, dayofweek, day_period, hour_from, hour_to in init_attendances]
+
+        if day_period:
+            attendances = [att for att in init_attendances if att.day_period == day_period]
+            for attendance in filter(lambda att: att.day_period == 'full_day', init_attendances):
+                attendances.append(attendance._replace(
+                    hour_from=attendance.hour_from if day_period == 'morning' else 12,
+                    hour_to=attendance.hour_to if day_period == 'afternoon' else 12,
+                ))
+
+        else:
+            attendances = init_attendances
+
+        default_start = min((att.hour_from for att in attendances), default=0.0)
+        default_end = max((att.hour_to for att in attendances), default=0.0)
+
+=======
+        if not target_date:
+            err = "Target Date cannot be empty"
+            raise ValueError(err)
+        if self.flexible_hours:
+            # Quick calculation to center flexible hours around 12PM midday
+            datetimes = [12.0 - self.hours_per_day / 2.0, 12.0, 12.0 + self.hours_per_day / 2.0]
+            if day_period:
+                return (datetimes[0], datetimes[1]) if day_period == 'morning' else (datetimes[1], datetimes[2])
+            return (datetimes[0], datetimes[2])
+
+        domain = [
+            ('calendar_id', '=', self.id),
+            ('display_type', '=', False),
+            ('day_period', '!=', 'lunch'),
+        ]
+
+        init_attendances = self.env['resource.calendar.attendance']._read_group(domain=domain,
+        groupby=['week_type', 'dayofweek', 'day_period'],
+        aggregates=['hour_from:min', 'hour_to:max'],
+        order='dayofweek,hour_from:min')
+
+        init_attendances = [DummyAttendance(hour_from, hour_to, dayofweek, day_period, week_type)
+            for week_type, dayofweek, day_period, hour_from, hour_to in init_attendances]
+
+        if day_period:
+            attendances = [att for att in init_attendances if att.day_period == day_period]
+            for attendance in filter(lambda att: att.day_period == 'full_day', init_attendances):
+                # Split full-day attendances at their midpoint.
+                half_time = (attendance.hour_from + attendance.hour_to) / 2
+                attendances.append(attendance._replace(
+                    hour_from=attendance.hour_from if day_period == 'morning' else half_time,
+                    hour_to=attendance.hour_to if day_period == 'afternoon' else half_time,
+                ))
+
+        else:
+            attendances = init_attendances
+
+        default_start = min((att.hour_from for att in attendances), default=0.0)
+        default_end = max((att.hour_to for att in attendances), default=0.0)
+
+>>>>>>> b82dc948d45d3df28bd953c45400564491bd91d1
         week_type = False
         if self.two_weeks_calendar:
             week_type = str(self.env['resource.calendar.attendance'].get_week_type(date))
