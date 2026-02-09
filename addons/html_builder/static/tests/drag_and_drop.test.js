954ad74ebf0a31373b4ac6a348a641ebe90f6026
Serhii Rubanskyi <seru@odoo.com>
2026-02-09 10:52:34 +0000

    [FIX] html_builder, *: detect drop outside of dropzone in RTL language
    *: website
    When dropping outside a dropzone but still on the page, the code checks
    if the drop happened well outside of the sidebar (so on its left).
    However, in RTL languages, the sidebar is positioned on the left, so we
    need to check if the drop is on the right side of it instead.
    
    The fix checks if the sidebar is at the left edge (the body of the
    document should have the o_rtl class) and verifies the drop position
    is on the right of the sidebar.
    
    Steps to reproduce:
    
    Set your profile to Arabic
    Drag and drop a snippet outside of a dropzone
    => It's not dropped, but it should, as it would with an LTR language.
    
    This commit also moves the drag and drop test from website to
    html_builder, as it tests html_builder's functionality, not website's.
    
    task-5484936
    
    X-original-commit: 06252459527a875761e6eeb5df0e5f235c8842d8

--- addons/html_builder/static/tests/drag_and_drop.test.js
+++ addons/html_builder/static/tests/drag_and_drop.test.js
@@ -1,17 +1,20 @@
-import { expect, test } from "@odoo/hoot";
-import { contains } from "@web/../tests/web_test_helpers";
+import { expect, getFixture, test } from "@odoo/hoot";
+import { contains, patchWithCleanup } from "@web/../tests/web_test_helpers";
+import { localization } from "@web/core/l10n/localization";
 import {
+    getDragHelper,
     getDragMoveHelper,
     setupHTMLBuilder,
     waitForEndOfOperation,
+    waitForSnippetDialog,
 } from "@html_builder/../tests/helpers";
 
-const dropzoneSelectors = {
-    selector: "section",
-    dropNear: "section",
-};
-
 test("Drag and drop basic test", async () => {
+    const dropzoneSelectors = {
+        selector: "section",
+        dropNear: "section",
+    };
+
     await setupHTMLBuilder(
         `
             <section class="section-1"><div><p>Text 1</p></div></section>
@@ -38,4 +41,29 @@ test("Drag and drop basic test", async () => {
     expect(":iframe section.section-1:nth-child(2)").toHaveCount(1);
     await waitForEndOfOperation();
     expect(".o-website-builder_sidebar .fa-undo").toBeEnabled();
+});
+
+test("Can drop a snippet outside a dropzone in a rtl language", async () => {
+    // Simulate rtl
+    patchWithCleanup(localization, { direction: "rtl" });
+    // Visual styling to run the test in debug.
+    getFixture().style.setProperty("direction", "rtl");
+    const dropzoneSelectors = {
+        selector: "section",
+        dropIn: "*",
+    };
+    await setupHTMLBuilder("", { dropzoneSelectors });
+    document.body.classList.add("o_rtl");
+    const { moveTo, drop } = await contains(
+        ".o_snippets_container .o_snippet.o_draggable .o_snippet_thumbnail"
+    ).drag();
+    expect(":iframe .oe_drop_zone").toHaveCount(1);
+    // Move the snippet out of the sidebar, but not over a dropzone
+    await moveTo(":iframe body", {
+        position: { x: 500, y: 500 },
+    });
+    expect(":iframe .oe_drop_zone").not.toHaveClass("o_dropzone_highlighted");
+    await drop(getDragHelper());
+    await waitForSnippetDialog();
+    expect(".o_add_snippet_dialog").toHaveCount(1);
 });
