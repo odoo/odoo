a234337b01e59e12089d088c92934de5db808ba5
plha-odoo <plha@odoo.com>
2026-02-11 14:16:38 +0100

    [FIX] stock{,_landed_costs},mrp_subcontracting: create aml correct values landed cost sbc
    **Problem:**
    account move line created from a landed cost on a  subcontracted
    receipt do not take into account already out quantity.
    
    **Steps to reproduce:**
    - create a tracked product with avco auto category
    - create a subcontracted bom for this product with no comp
    - create and confirm a PO for 10 unit of this product
    with the same partner as the subcontractor of the bom
    - validate the receipt
    - create and validate a delivery for 4 unit of your product
    - navigate to inventory/operations/adjustments/landed costs
    - create a new landed cost
    - select the receipt from the PO
    - add a landed cost of 10$ and validate
    - select the valuation smart button
    - a 6$ svl was created (which is correct because 6 out 10 products
    of the receipt are still in stock)
    - click on the book widget to open the account move view
    
    **Current behavior:**
    Only two account move lines were created both with a value
    of 10
    One crediting sotck interim received
    On debiting stock valuation
    
    **Expected behavior:**
    4 extra account move lines (all with a value of 4) should have been
    created to compensate the out quantity like it is the case for
    non subcontracted product.
    One debiting stock interim delivered
    One crediting stock valuation
    One debiting expenses
    One crediting stock interim delivered
    
    **Cause of the issue:**
    _is_in() will return false for the move of a subcontracted receipt
    https://github.com/odoo/odoo/blob/7462ec423e8b106a5175a71ac009176d16cdf225/addons/stock_landed_costs/models/stock_landed_cost.py#L185
    This is wanted and happens because _should_be_valued()
    will return true when called on the subcontracted location
    https://github.com/odoo/odoo/blob/7462ec423e8b106a5175a71ac009176d16cdf225/addons/stock_account/models/stock_move.py#L129
    
    As a consequence, qty_out stays 0
    https://github.com/odoo/odoo/blob/7462ec423e8b106a5175a71ac009176d16cdf225/addons/stock_landed_costs/models/stock_landed_cost.py#L185-L186
    and we do not append the values for the extra amls
    inside _create_account_move_line()
    https://github.com/odoo/odoo/blob/7462ec423e8b106a5175a71ac009176d16cdf225/addons/stock_landed_costs/models/stock_landed_cost.py#L465-L466
    
    **fix:**
    if we make sure the the adjustment line is linked to the move
    of the MO instead of the move of the receipt, this problem does
    not happen because _is_in() returns true for the move of the MO.
    Also in this case we don't need _get_stock_valuation_layer_ids()
    which was introduced by this PR https://github.com/odoo/odoo/pull/166107
    to solve the same issue.
    That is because the move used in button_validate is the move linked
    to the adjustment line, which will, after this fix, be the one of
    the MO, so we can directly take its stock valuation layers.
    
    opw-5723126
    
    closes odoo/odoo#248231
    
    Signed-off-by: Tiffany Chang (tic) <tic@odoo.com>

--- addons/mrp_subcontracting_landed_costs/models/stock_move.py
+++ addons/mrp_subcontracting_landed_costs/models/stock_move.py
@@ -6,8 +6,4 @@ class StockMove(models.Model):
 
     def _get_stock_valuation_layer_ids(self):
         self.ensure_one()
-        stock_valuation_layer_ids = super()._get_stock_valuation_layer_ids()
-        subcontracted_productions = self._get_subcontract_production()
-        if self.is_subcontract and subcontracted_productions:
-            return subcontracted_productions.move_finished_ids.stock_valuation_layer_ids
-        return stock_valuation_layer_ids
+        return super()._get_stock_valuation_layer_ids()
