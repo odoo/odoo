dzdzdzdz1010/OdooStandard

[FIX] event, website_event: allow outlook calendar to render HTML event descriptions
Behavior:
Current:
When exporting an event for Outlook Calendar, the file returned is the same as for ICalendar(.ics), however Outlook cannot handle the HTML description of the event when passed through the 'DESCRIPTION:' field which causes the event description to display raw HTML tags.

Solution:
Outlook can use the 'X-ALT-DESC:' field with 'text/html' parameter to handle HTML. A new route was created to generate the event's ics file with the correct parameters for Outlook.

Steps to reproduce:
- Go to an event from Website
- Select the Outlook icon under 'Add to calendar'
- Import the file to Outlook Calendar
- You'll notice HTML tags in the description of the event.

opw-5116354

addons/website_event/controllers/main.py
@@ -195,6 +195,7 @@ class WebsiteEventController(http.Controller):
             'range': range,
             'google_url': lazy(lambda: urls.get('google_url')),
             'iCal_url': lazy(lambda: urls.get('iCal_url')),
+            'outlook_url': lazy(lambda: urls.get('outlook_url')),
             'registration_error_code': post.get('registration_error_code'),
         }
 
@@ -401,7 +402,8 @@ class WebsiteEventController(http.Controller):
             'attendees': attendees_sudo,
             'event': event,
             'google_url': urls.get('google_url'),
-            'iCal_url': urls.get('iCal_url')
+            'iCal_url': urls.get('iCal_url'),
+            'outlook_url': urls.get('outlook_url'),
         }
 
     # ------------------------------------------------------------

addons/website_event/models/event_event.py
@@ -483,7 +483,8 @@ class Event(models.Model):
         encoded_params = werkzeug.urls.url_encode(params)
         google_url = GOOGLE_CALENDAR_URL + encoded_params
         iCal_url = f'/event/{self.id:d}/ics?{encoded_params}'
-        return {'google_url': google_url, 'iCal_url': iCal_url}
+        outlook_url = f'/event/{self.id:d}/outlook_ics?{encoded_params}'
+        return {'google_url': google_url, 'iCal_url': iCal_url, 'outlook_url': outlook_url}
 
     def _default_website_meta(self):
         res = super(Event, self)._default_website_meta()

odoo/odoo

addons/website_event/controllers/main.py
@@ -195,6 +195,7 @@ class WebsiteEventController(http.Controller):
             'range': range,
             'google_url': lazy(lambda: urls.get('google_url')),
             'iCal_url': lazy(lambda: urls.get('iCal_url')),
+            'iCal_url_outlook': lazy(lambda: urls.get('iCal_url_outlook')),
             'registration_error_code': post.get('registration_error_code'),
         }
 
@@ -401,7 +402,8 @@ class WebsiteEventController(http.Controller):
             'attendees': attendees_sudo,
             'event': event,
             'google_url': urls.get('google_url'),
-            'iCal_url': urls.get('iCal_url')
+            'iCal_url': urls.get('iCal_url'),
+            'iCal_url_outlook': urls.get('iCal_url_outlook'),
         }
 
     # ------------------------------------------------------------

addons/website_event/models/event_event.py
@@ -483,7 +483,8 @@ class Event(models.Model):
         encoded_params = werkzeug.urls.url_encode(params)
         google_url = GOOGLE_CALENDAR_URL + encoded_params
         iCal_url = f'/event/{self.id:d}/ics?{encoded_params}'
-        return {'google_url': google_url, 'iCal_url': iCal_url}
+        iCal_url_outlook = f'/event/{self.id:d}/outlook_ics?{encoded_params}'
+        return {'google_url': google_url, 'iCal_url': iCal_url, 'iCal_url_outlook': iCal_url_outlook}
 
     def _default_website_meta(self):
         res = super(Event, self)._default_website_meta()