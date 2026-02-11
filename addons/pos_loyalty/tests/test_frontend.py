cc7524979af6415d3830720d29934564981f9c92
Arthur Nanson <artn@odoo.com>
2026-02-11 11:28:43 +0100

    [FIX] point_of_sale: prevent invoice from being credit note with ewallet
    **Steps to reproduce:**
    - Make a $10 product, the tax must be tax included
    - Set up an ewallet
    - Open the PoS, order the created product and add the ewallet
    - Ask for an invoice and complete the payment
    - The generated invoice is a Credit Note
    
    **Why the fix:**
    When chosing the invoice's move_type (invoice or credit note),
    we check if the total amount is greater or equal to 0,
    and if it is, we make an invoice, else we make a credit
    note.
    
    This did not work correctly as with the tax included
    product there was a tiny bit of difference between
    the product's price and what the ewallet covered.
    This means that the total was just a tiny bit under
    zero, and was treated as a refund, so a credit note
    was then created.
    
    The same problem was observed when preparing the
    invoice lines, the order was treated as a refund,
    so the lines' qty were inverted.
    
    We now use float_compare to avoid such rounding
    issues.
    
    opw-5490407

--- addons/pos_loyalty/tests/test_frontend.py
+++ addons/pos_loyalty/tests/test_frontend.py
@@ -3277,3 +3277,30 @@ class TestUi(TestPointOfSaleHttpCommon):
             login="pos_user",
         )
         self.assertEqual(len(gift_card_program.coupon_ids), 2)
+
+    def test_ewallet_tax_included_invoice(self):
+        LoyaltyProgram = self.env['loyalty.program']
+        (LoyaltyProgram.search([])).write({'pos_ok': False})
+        self.env.ref('loyalty.ewallet_product_50').write({'active': True})
+        ewallet_program = self.create_programs([('arbitrary_name', 'ewallet')])['arbitrary_name']
+        partner_aaa = self.env['res.partner'].create({'name': 'AAAA'})
+        self.env['loyalty.card'].create({
+            'partner_id': partner_aaa.id,
+            'program_id': ewallet_program.id,
+            'points': 50,
+        })
+        tax_inc = self.env['account.tax'].create({
+            'name': 'Included',
+            'amount': 10,
+            'price_include_override': 'tax_included',
+        })
+        self.whiteboard_pen.write({
+            'lst_price': 10.0,
+            'taxes_id': [Command.set(tax_inc.ids)],
+        })
+        self.main_pos_config.open_ui()
+        self.start_tour(f"/pos/ui?config_id={self.main_pos_config.id}", 'test_ewallet_tax_included_invoice', login="pos_user")
+        invoice = self.env['pos.order'].search([('session_id', '=', self.main_pos_config.current_session_id.id)], limit=1).account_move
+        self.assertEqual(invoice.move_type, 'out_invoice')
+        self.assertEqual(invoice.line_ids[0].quantity, 1)
+        self.assertEqual(invoice.line_ids[1].quantity, 1)
