b042326852171f27c62e76a694ab8011c42526c4
Augustin (duau) <duau@odoo.com>
2026-02-11 11:28:31 +0100

    [REF] website: generate HeaderTemplateOption from resource
    This commit refactors `HeaderTemplateOption` to use a provider-based
    resource instead of a hardcoded list in the template.
    
    Previously, adding new header templates required using XPath to inject
    elements into the 'website.HeaderTemplateOption' template. This was
    inconsistent with the footer's implementation, which uses a resource
    provider.
    
    This commit introduces the 'header_templates_providers' resource. The
    header option now iterates over provided templates, aligning the API
    with 'footer_templates_providers' and offering a cleaner extension point
    for customisation.
    
    No functional changes are introduced.
    
    task-5928790

--- addons/website/static/src/builder/plugins/options/header/header_option_plugin.js
+++ addons/website/static/src/builder/plugins/options/header/header_option_plugin.js
@@ -5,12 +5,28 @@ import {
 } from "@html_builder/utils/option_sequence";
 import { Plugin } from "@html_editor/plugin";
 import { withSequence } from "@html_editor/utils/resource";
+import { reactive } from "@odoo/owl";
+import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
 import { HeaderElementsOption } from "./header_elements_option";
 import { HeaderFontOption } from "./header_font_option";
-import { HeaderTemplateOption } from "./header_template_option";
+import { HeaderTemplateChoice, HeaderTemplateOption } from "./header_template_option";
 import { HeaderIconBackgroundOption } from "./header_icon_background_option";
 import { HeaderTopOptions } from "./header_top_options";
+
+/** @typedef {import("@odoo/owl").Component} Component */
+
+/**
+ * @typedef { Object } HeaderOptionShared
+ * @property { HeaderOptionPlugin['getHeaderTemplates'] } getHeaderTemplates
+ */
+/**
+ * @typedef {(() => Promise<{
+ *     key: string,
+ *     Component: Component,
+ *     props: any,
+ * }[]>)[]} header_templates_providers
+ */
 
 const [
     HEADER_TEMPLATE,
@@ -39,6 +55,7 @@ export {
 class HeaderOptionPlugin extends Plugin {
     static id = "headerOption";
     static dependencies = ["customizeWebsite", "menuDataPlugin"];
+    static shared = ["getHeaderTemplates"];
 
     /** @type {import("plugins").WebsiteResources} */
     resources = {
@@ -58,7 +75,63 @@ class HeaderOptionPlugin extends Plugin {
             withSequence(HEADER_ELEMENTS, HeaderElementsOption),
             withSequence(HEADER_ICON_BACKGROUND, HeaderIconBackgroundOption),
         ],
+        header_templates_providers: [
+            () =>
+                [
+                    { name: "default", title: _t("Default") },
+                    {
+                        name: "hamburger",
+                        title: _t("Hamburger Menu"),
+                        extraViews: ["website.no_autohide_menu"],
+                    },
+                    { name: "boxed", title: _t("Rounded Box Menu") },
+                    { name: "stretch", title: _t("Stretch Menu") },
+                    { name: "vertical", title: _t("Vertical") },
+                    { name: "search", title: _t("Menu with Search Bar") },
+                    { name: "sales_one", title: _t("Menu - Sales 1") },
+                    { name: "sales_two", title: _t("Menu - Sales 2") },
+                    { name: "sales_three", title: _t("Menu - Sales 3") },
+                    { name: "sales_four", title: _t("Menu - Sales 4") },
+                    {
+                        name: "sidebar",
+                        title: _t("Sidebar"),
+                        extraViews: ["website.no_autohide_menu"],
+                        menuShadowClass: "shadow-lg",
+                    },
+                ].map((info) => {
+                    const view = info.view ?? `website.template_header_${info.name}`;
+                    return {
+                        key: info.name,
+                        Component: HeaderTemplateChoice,
+                        props: {
+                            id: `header_${info.name}_opt`,
+                            imgSrc: `/website/static/src/img/snippets_options/header_template_${info.name}.svg`,
+                            menuShadowClass: info.menuShadowClass ?? "shadow-sm",
+                            title: info.title,
+                            varName: info.name,
+                            views: [view, ...(info.extraViews || [])],
+                        },
+                    };
+                }),
+        ],
     };
+
+    getHeaderTemplates() {
+        const templates = reactive([]);
+
+        // we don't wait for all promises to resolve and show the ones available
+        // as soon as they are (and keep them in the order of the providers)
+        const templatesByProvider = this.getResource("header_templates_providers").map((p) => {
+            const provided = [];
+            Promise.resolve(p()).then((t) => {
+                provided.push(...t);
+                templates.splice(0, Infinity, ...templatesByProvider.flat());
+            });
+            return provided;
+        });
+
+        return templates;
+    }
 }
 
 registry.category("website-plugins").add(HeaderOptionPlugin.id, HeaderOptionPlugin);
--- addons/website/static/src/builder/plugins/options/header/header_template_option.js
+++ addons/website/static/src/builder/plugins/options/header/header_template_option.js
@@ -1,8 +1,15 @@
 import { BaseOptionComponent } from "@html_builder/core/utils";
+import { useState } from "@odoo/owl";
 import { basicHeaderOptionSettings } from "./basicHeaderOptionSettings";
 
 export class HeaderTemplateOption extends BaseOptionComponent {
     static template = "website.HeaderTemplateOption";
+    static dependencies = ["headerOption"];
+
+    setup() {
+        super.setup();
+        this.headerTemplates = useState(this.dependencies.headerOption.getHeaderTemplates());
+    }
 
     hasSomeOptions(opts) {
         return opts.some((opt) => this.isActiveItem(opt));
@@ -10,3 +17,15 @@ export class HeaderTemplateOption extends BaseOptionComponent {
 }
 
 Object.assign(HeaderTemplateOption, basicHeaderOptionSettings);
+
+export class HeaderTemplateChoice extends BaseOptionComponent {
+    static template = "website.HeaderTemplateChoice";
+    static props = {
+        title: String,
+        views: Array,
+        varName: String,
+        imgSrc: String,
+        id: String,
+        menuShadowClass: String,
+    };
+}
