9007eda88b60ea84be9d3168d5d64018a470a3ef
Sébastien Theys <seb@odoo.com>
2026-02-11 10:24:57 +0000

    [FIX] mail: properly sort mention suggestions in the full composer
    Fetch/sort suggestion in the full composer don't receive the thread
    param which leads to follower not being sorted at the top.
    
    task-5917226
    
    X-original-commit: d3ff32a44b86010c73ed321a2757dd71bca6c487

--- addons/mail/static/src/core/common/suggestion_service.js
+++ addons/mail/static/src/core/common/suggestion_service.js
@@ -164,7 +164,7 @@ export class SuggestionService {
      * @param {String} [param0.delimiter] can be one one of the following: ["@", "#"]
      * @param {String} [param0.term]
      * @param {Object} [options={}]
-     * @param {Integer} [options.thread] prioritize and/or restrict
+     * @param {import("models").Thread} [options.thread] prioritize and/or restrict
      *  result in the context of given thread
      * @returns {{ type: String, suggestions: Array }}
      */
