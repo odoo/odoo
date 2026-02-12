55cd747692ad2dad122ea2b7a39eef2940bcdbfd
Géry Debongnie <ged@odoo.com>
2026-02-12 14:19:29 +0100

    [IMP] upgrade code: Fixes and refactor
    Refactor script to process node by node, and recompute vars per node
    and various small fixes.
    
    BEFORE: The script was iterating multiple times over the same template
    assuming only the bound variables where active.
    
    NOW: The script iterates once over the templatem node by node, and
    interactively changes the bound variables (for example if inside
    a t inherit).
    
    Example: Before this new test was breaking:
    ```
    inside_vars": {"web.ListView": {"item"}}
    <t t-name="web.SettingsRadioField" t-inherit="web.RadioField" t-inherit-mode="primary">
        <xpath expr="//label" position="replace">
            <label class="form-check-label o_form_label" t-att-for="`${id}_${item[0]}`">
                <HighlightText originalText="item[1]"/>
            </label>
        </xpath>
    </t>
    ```
    The issue was that in the first pass of the fix template in
    ```
    for attr in DIRECTIVES:
            for node in root.xpath(f"descendant-or-self::*[@{attr}]"):
                node.set(attr, compile_expr(node.get(attr), bound_variables))
    ```
    the bound variables did not have the inside vars, and therefore added
    `this.` to the `t-att-for="`${this.id}_${this.item[0]}`">`. So even if the
    logic inside the
    `for inherit_node in root.xpath("descendant-or-self::*[@t-inherit]"):`
    was correct, by the time we got to the loop the data was already wrong
    
    Passing inside vars to the statement outside the inherit block wouldn't
    work either because there could be multiple inherits within the same
    templatem and we wouldn't know which ones to pick

--- odoo/upgrade_code/19.2-00-owl3-migration.py
+++ odoo/upgrade_code/19.2-00-owl3-migration.py
@@ -636,21 +636,27 @@ def upgrade_this(file_manager, log_info, log_error):
 
     web_files = [
         f for f in file_manager
-        if 'addons/web/static/src' in f.path._str
+        if 'static/src' in f.path._str
         and f.path.suffix == '.xml'
         and not any(f.path._str.endswith(p) for p in EXCLUDED_PATH)
     ]
 
     # Step 1: Gather all variables in the web module
-    outside_vars = {}  # vars defined under t-call
-    inside_vars = {}  # vars defined inside template, eg. using t-set
+    outside_vars = {
+        "mail.Composer.quickActions": {'partitionedActions'},
+        "web.Breadcrumb.Name": {'breadcrumb'},
+        "web.SearchPanel.Category": {'section'},
+        "web.ListRenderer.RecordRow": {'record'},
+    }  # vars defined under t-call
+    inside_vars = {} # vars defined inside template, eg. using t-set
+
     for fileno, file in enumerate(web_files, start=1):
         aggregate_vars(file.content, outside_vars, inside_vars)
 
     # Step 2: Add this. to all non local template vars (except those coming from external t-call)
     for fileno, file in enumerate(web_files, start=1):
         try:
-            file.content = update_template(file.content, outside_vars)
+            file.content = update_template(file.content, outside_vars, inside_vars)
         except Exception as e:
             log_error(file.path, e)
 
