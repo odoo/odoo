d084cb936f175895f8f3400d0ead027e3e951618
Omar Khalid <khom@odoo.com>
2026-02-12 05:11:49 +0000

    [IMP] l10n_tr_nilvera{_einvoice}: Improvements and bug fixes
    This commit adds the following improvements and bug fixes to the
    turkish localization:
    - Make shipping method required for GiB export invoices
    - Make Exemption Reason mandatory when Invoice Type is Tax Exempt
    - Ensure Invoice Type is selected when Invoice Scenario is set
    - Hide the nilvera send status for Vendor Bills
    - Adjust the error message for missing CTSP numbers on invoice lines
    - Allow resetting Vendor Bills to draft
    - Give priority to the currency rate on the invoice when generating UBL
    - Create a new bridge module to bypass the VAT validation for test VAT numbers
    - Fix bug in bulk customer verification
    
    task-5868201
    
    closes odoo/odoo#253242
    
    X-original-commit: b37bbf0881e927d0a1277a59b426c35029d7345e
    Signed-off-by: de Wouters de Bouchout Jean-Benoît (jbw) <jbw@odoo.com>

---
+++ addons/l10n_tr_nilvera_base_vat/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Türkiye - Nilvera/Base VAT',
+    'category': 'Accounting/Accounting',
+    'description': "Bridge module for Nilvera and Base VAT integration.",
+    'depends': ['l10n_tr_nilvera', 'base_vat'],
+    'auto_install': True,
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
