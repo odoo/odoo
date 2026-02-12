8e0f22ee8aaf65e2a4475c7a9131de10943edb5a
pmah-odoo <pmah@odoo.com>
2026-02-12 15:51:24 +0530

    [IMP] mail: refine ChannelActionDialog header spacing
    This commit adjusts the header spacing of the ChannelActionDialog
    and fixes the miscentered video camera icon to improve visual
    alignment and overall appearance.
    
    task-5382647
    
    closes odoo/odoo#248414
    
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/mail/static/src/discuss/core/common/channel_action_dialog.js
+++ addons/mail/static/src/discuss/core/common/channel_action_dialog.js
@@ -6,7 +6,7 @@ export class ChannelActionDialog extends Component {
     static components = { Dialog };
     static props = ["title", "contentComponent", "contentProps", "close?", "contentClass?"];
     static template = xml`
-        <Dialog size="'md'" title="props.title" footer="false" contentClass="'o-bg-body '+ props.contentClass" bodyClass="'p-1'">
+        <Dialog size="'md'" title="props.title" footer="false" contentClass="'o-bg-body o-discuss-ChannelActionDialog '+ props.contentClass" bodyClass="'p-1'">
             <t t-component="props.contentComponent" t-props="props.contentProps"/>
         </Dialog>
     `;
