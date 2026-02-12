cd8d9718427e48aaa79be21f9a08e89b79b573f9
AhmedElemary57 <aele@odoo.com>
2026-02-12 11:30:26 +0100

    [FIX] account: allow duplicate code check to include archived accounts
    Previously, the uniqueness constraint on account codes only checked
    active records. This allowed users to create a new account with the
    same code as an archived one, leading to potential data integrity
    issues if the archived account was later restored.
    
    Updated the search logic to include both active and archived accounts
     when validating account code uniqueness
    
     task-5417765

--- addons/account/models/account_account.py
+++ addons/account/models/account_account.py
@@ -1101,7 +1101,7 @@ class AccountAccount(models.Model):
                 duplicate_codes = [code for code, accounts in accounts_by_code.items() if len(accounts) > 1]
 
             # Check 2.2: Check that there are no duplicates in database
-            elif duplicates := self.with_company(company).sudo().search_fetch(
+            elif duplicates := self.with_company(company).sudo().with_context(active_test=False).search_fetch(
                 [
                     ('code', 'in', list(accounts_by_code)),
                     ('id', 'not in', self.ids),
