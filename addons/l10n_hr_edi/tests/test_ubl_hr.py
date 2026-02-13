3579759560273fb1c75786deb73c45564b37e69f
nape-odoo <nape@odoo.com>
2026-02-13 09:31:34 +0000

    [IMP] l10n_hr_edi: Improvements to XML generation, test coverage, API
    - Improving the ubl_hr method chain to include less overrides in favor of extensions
    - Adding proper handling of multiple tax total and tax category nodes for hrextac
    - Refining docstrings and comments for ubl_hr
    - Adding a test with multiple tax types
    - Adding a test for account_edi_ubl_cii_tax_extension integration
    - Adjusting MER methods to correspond to API changes: fiscalization queries are now only available for own documents
    task-none
    
    X-original-commit: 10131763d8ba3a082cc29f5650a2ef70d8e5345a

--- addons/l10n_hr_edi/tests/test_ubl_hr.py
+++ addons/l10n_hr_edi/tests/test_ubl_hr.py
@@ -50,6 +50,55 @@ class TestL10nHrEdiXml(TestL10nHrEdiCommon, AccountTestInvoicingCommon):
             self.get_xml_tree_from_string(expected_content),
         )
 
+    def test_export_invoice_with_multiple_taxes(self):
+        """
+        Test content of a generated invoice with multiple taxes in Croation UBL format.
+        """
+        self.setup_partner_as_hr(self.env.company.partner_id)
+        self.setup_partner_as_hr_alt(self.partner_a)
+        tax_1 = self.env['account.chart.template'].ref('VAT_S_IN_ROC_25')
+        tax_2 = self.env['account.chart.template'].ref('VAT_S_IN_ROC_13')
+        tax_3 = self.env['account.chart.template'].ref('VAT_S_EU_G')
+
+        invoice = self.env['account.move'].create({
+            'move_type': 'out_invoice',
+            'partner_id': self.partner_a.id,
+            'invoice_date': '2025-01-01',
+            'l10n_hr_process_type': 'P99',
+            'l10n_hr_customer_defined_process_name': 'Test custom process type',
+            'invoice_line_ids': [
+                Command.create({
+                    'product_id': self.product_a.id,
+                    'price_unit': 100.0,
+                    'tax_ids': [Command.set(tax_1.ids)],
+                }),
+                Command.create({
+                    'product_id': self.product_a.id,
+                    'price_unit': 200.0,
+                    'tax_ids': [Command.set(tax_2.ids)],
+                }),
+                Command.create({
+                    'product_id': self.product_a.id,
+                    'price_unit': 300.0,
+                    'tax_ids': [Command.set(tax_3.ids)],
+                }),
+            ],
+        })
+        invoice.action_post()
+        # Manually create the addendum which normally happens during sending
+        invoice.l10n_hr_edi_addendum_id = self.env['l10n_hr_edi.addendum'].create({
+            'move_id': invoice.id,
+            'invoice_sending_time': '2025-01-02',
+            'fiscalization_number': self.env['account.move']._get_l10n_hr_fiscalization_number(invoice.name),
+        })
+        actual_content, _dummy = self.env['account.edi.xml.ubl_hr'].with_context(lang='en_US')._export_invoice(invoice)
+        with misc.file_open(f'addons/{self.test_module}/tests/test_files/test_invoice_multiple.xml', 'rb') as file:
+            expected_content = file.read()
+        self.assertXmlTreeEqual(
+            self.get_xml_tree_from_string(actual_content),
+            self.get_xml_tree_from_string(expected_content),
+        )
+
     def test_export_invoice_with_cash_basis(self):
         """
         Test content of a generated invoice with an 'on_payment' exigibility tax in Croation UBL format.
@@ -125,11 +174,19 @@ class TestL10nHrEdiXml(TestL10nHrEdiCommon, AccountTestInvoicingCommon):
 
     def test_export_invoice_with_exemption_e(self):
         """
-        Test the content of a generated invoice with HR:E exemption in Croation UBL format.
+        Test the content of a generated invoice with HR:E exemption,
+        filled from 'account_edi_ubl_cii_tax_extension' fields, in Croation UBL format.
         """
-        self.setup_partner_as_hr(self.env.company.partner_id)
+        tax_extension_module = self.env['ir.module.module']._get('account_edi_ubl_cii_tax_extension')
+        if tax_extension_module.state != 'installed':
+            self.skipTest("This test will fail if account_edi_ubl_cii_tax_extension is not installed")
+
+        self.setup_partner_as_hr(self.company_data['company'].partner_id)
         self.setup_partner_as_hr_alt(self.partner_a)
-        tax = self.env['account.chart.template'].ref('VAT_S_other_exempt_O')
+        company_id = self.company_data['company'].id
+        tax = self.env.ref(f'account.{company_id}_VAT_S_other_exempt_O')
+        tax.ubl_cii_tax_category_code = 'E'
+        tax.ubl_cii_tax_exemption_reason_code = 'VATEX-EU-143'
 
         invoice = self.env['account.move'].create({
             'move_type': 'out_invoice',
