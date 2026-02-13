7d89993cfa494bea392de98aab1d65e0cd01e721
nape-odoo <nape@odoo.com>
2026-02-13 10:31:34 +0100

    [IMP] l10n_hr_edi: Improvements to XML generation, test coverage, API
    - Improving the ubl_hr method chain to include less overrides in favor of extensions
    - Adding proper handling of multiple tax total and tax category nodes for hrextac
    - Refining docstrings and comments for ubl_hr
    - Adding a test with multiple tax types
    - Adding a test for account_edi_ubl_cii_tax_extension integration
    - Adjusting MER methods to correspond to API changes: fiscalization queries are now only available for own documents
    task-none
    
    closes odoo/odoo#247339
    
    Signed-off-by: Antoine Dupuis (andu) <andu@odoo.com>

--- addons/l10n_hr_edi/models/account_move.py
+++ addons/l10n_hr_edi/models/account_move.py
@@ -206,21 +206,29 @@ class AccountMove(models.Model):
         self.ensure_one()
         if self.is_sale_document():
             response_mer = _mer_api_query_document_process_status_outbox(self.company_id, electronic_id=self.l10n_hr_mer_document_eid)[0]
-            response_fisc = _mer_api_check_fiscalization_status_outbox(self.company_id, electronic_id=self.l10n_hr_mer_document_eid)[0]
+            response_fisc = _mer_api_check_fiscalization_status_outbox(self.company_id, electronic_id=self.l10n_hr_mer_document_eid)
+            if isinstance(response_fisc, list):
+                response_fisc = {} if len(response_fisc) == 0 else response_fisc[0]
         elif self.is_purchase_document():
             response_mer = _mer_api_query_document_process_status_inbox(self.company_id, electronic_id=self.l10n_hr_mer_document_eid)[0]
-            response_fisc = _mer_api_check_fiscalization_status_inbox(self.company_id, electronic_id=self.l10n_hr_mer_document_eid)[0]
+            response_fisc = _mer_api_check_fiscalization_status_inbox(self.company_id, electronic_id=self.l10n_hr_mer_document_eid)
+            if isinstance(response_fisc, list):
+                response_fisc = {} if len(response_fisc) == 0 else response_fisc[0]
         else:
             return
-        self.l10n_hr_edi_addendum_id.write({
+        write_dict = {
             'mer_document_status': str(response_mer.get('StatusId')),
             'business_document_status': str(response_mer.get('DocumentProcessStatusId')),
-            'fiscalization_status': str(response_fisc['messages'][-1].get('status')),
-            'fiscalization_error': str(response_fisc['messages'][-1].get('errorCode') + ' - ' + response_fisc['messages'][-1].get('errorCodeDescription')),
-            'fiscalization_request': str(response_fisc['messages'][-1].get('fiscalizationRequestId')),
-            'business_status_reason': str(response_fisc['messages'][-1].get('businessStatusReason')),
-            'fiscalization_channel_type': str(response_fisc.get('channelType')),
-        })
+        }
+        if response_fisc:
+            write_dict.update({
+                'fiscalization_status': str(response_fisc['messages'][-1].get('status')),
+                'fiscalization_error': str(response_fisc['messages'][-1].get('errorCode') + ' - ' + response_fisc['messages'][-1].get('errorCodeDescription')),
+                'fiscalization_request': str(response_fisc['messages'][-1].get('fiscalizationRequestId')),
+                'business_status_reason': str(response_fisc['messages'][-1].get('businessStatusReason')),
+                'fiscalization_channel_type': str(response_fisc.get('channelType')),
+            })
+        self.l10n_hr_edi_addendum_id.write(write_dict)
 
     def l10n_hr_edi_mer_action_report_paid(self):
         batch = len(self) != 1
