16d23341d46aa2ac29c74fa0b3eee4ebe6d8cfab
Xavier Morel <xmo@odoo.com>
2026-02-13 10:41:31 +0100

    [IMP] core: mark pypdf shims as deprecated
    We have enabled the warnings in PyPDF2, however clients and partners
    who are already on 3.x and going through the shims we
    provided (because it's old code on the old API) probably also need to
    be warned to update. So mark every shim / backwards compatibility
    function as deprecated.
    
    closes odoo/odoo#248197
    
    Related: odoo/enterprise#107138
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- odoo/tools/pdf/_pypdf.py
+++ odoo/tools/pdf/_pypdf.py
@@ -1,7 +1,9 @@
-import pypdf
 import typing
+
 from pypdf import errors, filters, generic, PdfReader as _Reader, PdfWriter as _Writer
 from pypdf.generic import create_string_object
+
+from odoo.tools.func import deprecated
 
 __all__ = [
     "PdfReader",
@@ -13,70 +15,81 @@ __all__ = [
 ]
 
 
-pypdf.PageObject.mergePage = lambda self, page2: self.merge_page(page2)
-pypdf.PageObject.mediaBox = property(lambda self: self.mediabox)
-# use lambdas (rather than copying) to allow overrides of the base method
-generic.PdfObject.getObject = lambda self: self.get_object()
-generic.StreamObject.getData = lambda self: self.get_data()
-generic.StreamObject.setData = lambda self, data: self.set_data(data)
-generic.RectangleObject.getWidth = lambda self: self.width
-generic.RectangleObject.getHeight = lambda self: self.height
+deprecate = deprecated("PyPDF2 1.x compatibility shims are deprecated, switch to modern API")
 
 
 class PdfReader(_Reader):
     @property
+    @deprecate
     def isEncrypted(self):
         return self.is_encrypted
 
+    @deprecate
     def getPage(self, pageNumber):
         return self.pages[pageNumber]
 
+    @deprecate
     def getNumPages(self):
         return len(self.pages)
 
     @property
+    @deprecate
     def numPages(self):
         return len(self.pages)
 
+    @deprecate
     def getDocumentInfo(self):
         return self.metadata
 
+    @deprecate
     def getFormTextFields(self):
         return self.get_form_text_fields()
 
 
 class PdfWriter(_Writer):
+    # NOTE: can drop this when pypdf2 shims are removed: issue was fixed in
+    # pypdf 5.2 and debian and ubuntu jumped directly to 5.4
     def add_metadata(self, infos: dict[str, typing.Any]) -> None:
         if hasattr(self, '_info') and self._info is None:
             self._info = generic.DictionaryObject()
         super().add_metadata(infos)
 
+    @deprecate
     def getPage(self, pageNumber):
         return self.pages[pageNumber]
 
+    @deprecate
     def getNumPages(self):
         return len(self.pages)
 
+    @deprecate
     def addPage(self, page):
         return self.add_page(page)
 
+    @deprecate
     def appendPagesFromReader(self, reader):
         return self.append_pages_from_reader(reader)
 
+    @deprecate
     def addBlankPage(self, width=None, height=None):
         return self.add_blank_page(width=width, height=height)
 
+    @deprecate
     def addAttachment(self, fname, data):
         return self.add_attachment(fname, data)
 
+    @deprecate
     def addMetadata(self, infos):
         return self.add_metadata(infos)
 
+    @deprecate
     def cloneReaderDocumentRoot(self, reader):
         return self.clone_reader_document_root(reader)
 
+    @deprecate
     def getFields(self, *args, **kwargs):
         return self.get_fields(*args, **kwargs)
 
+    @deprecate
     def _addObject(self, *args, **kwargs):
         return self._add_object(*args, **kwargs)
