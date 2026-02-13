cfa804a8c5ec075d4c49de87a01a01de570fe298
Jinjiu Liu <jili@odoo.com>
2026-02-13 17:37:41 +0100

    [FIX] html_editor: set cursor after zws for ios safari collapsed bold
    Before this commit: when we applying format on collapsed cursor, we
    create a formatted element with ZWS, and set the cursor before the ZWS
    
    After this commit: we set the cursor after the ZWS, cause otherwise
    safari doesn't update the cursor properly leading to unformatted input
    
    task-4243977
    
    closes odoo/odoo#249253
    
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/html_editor/static/src/core/format_plugin.js
+++ addons/html_editor/static/src/core/format_plugin.js
@@ -399,7 +399,14 @@ export class FormatPlugin extends Plugin {
             selectedTextNodes[0] &&
             selectedTextNodes[0].textContent === "\u200B"
         ) {
-            this.dependencies.selection.setCursorStart(selectedTextNodes[0]);
+            // We set the cursor at the end of the selected ZWS text node, to
+            // avoid an issue on ios safari where the selection is collapsed,
+            // and set the format as bold/italic, the cursor is not properly
+            // updated. Even though the selection is properly set, safari seems
+            // to force the cursor to stay at the old position at rendering
+            // if there's no node between the old and new cursor position,
+            // e.g. <div>[]<p>\u200B</p></div> -> <div><p>[]\u200B</p></div>.
+            this.dependencies.selection.setCursorEnd(selectedTextNodes[0]);
         } else if (selectedTextNodes.length) {
             const firstNode = selectedTextNodes[0];
             const lastNode = selectedTextNodes[selectedTextNodes.length - 1];
