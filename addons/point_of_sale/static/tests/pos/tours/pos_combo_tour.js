dzdzdzdz1010/OdooStandard

[FIX] point_of_sale: compute combo price correctly with no free item
If a combo product is configured with no free item, the parent product's
price was not computed in total correctly.

opw-5241225

addons/point_of_sale/static/tests/pos/tours/pos_combo_tour.js
@@ -223,3 +223,34 @@ registry.category("web_tour.tours").add("ProductComboChangePricelist", {
             ProductScreen.isShown(),
         ].flat(),
 });
+
+registry.category("web_tour.tours").add("test_combo_no_free_item", {
+    steps: () =>
+        [
+            Chrome.startPoS(),
+            Dialog.confirm("Open Register"),
+
+            // Desk accessories combo
+            ProductScreen.clickDisplayedProduct("Office Combo"),
+            combo.select("Combo Product 1"),
+            combo.select("Combo Product 2"),
+            combo.select("Combo Product 3"),
+            combo.checkTotal(`${10 * 3 + 2 + 40}.00`),
+            combo.select("Combo Product 4"),
+            combo.select("Combo Product 5"),
+            combo.checkTotal(`${72 + 20 * 2 + 2}.00`),
+            combo.select("Combo Product 6"),
+            combo.select("Combo Product 7"),
+            combo.select("Combo Product 8"),
+            combo.checkTotal(`${114 + 30 * 3 + 5}.00`),
+            Dialog.confirm(),
+            inLeftSide([
+                ...ProductScreen.selectedOrderlineHasDirect("Office Combo", "1", "232.10"),
+            ]),
+            ProductScreen.totalAmountIs("232.10"),
+            ProductScreen.clickPayButton(),
+            PaymentScreen.clickPaymentMethod("Bank"),
+            PaymentScreen.clickValidate(),
+            ReceiptScreen.isShown(),
+        ].flat(),
+});

odoo/odoo

X-original-commit: e898d3c6ecb6567cb63990cb2e8330db6bba3bdc

addons/point_of_sale/static/tests/pos/tours/pos_combo_tour.js
@@ -337,3 +337,34 @@ registry.category("web_tour.tours").add("test_combo_item_image_not_display", {
             Dialog.confirm(),
         ].flat(),
 });
+
+registry.category("web_tour.tours").add("test_combo_no_free_item", {
+    steps: () =>
+        [
+            Chrome.startPoS(),
+            Dialog.confirm("Open Register"),
+
+            // Desk accessories combo
+            ProductScreen.clickDisplayedProduct("Office Combo"),
+            combo.select("Combo Product 1"),
+            combo.select("Combo Product 2"),
+            combo.select("Combo Product 3"),
+            combo.checkTotal(`${10 * 3 + 2 + 40}.00`),
+            combo.select("Combo Product 4"),
+            combo.select("Combo Product 5"),
+            combo.checkTotal(`${72 + 20 * 2 + 2}.00`),
+            combo.select("Combo Product 6"),
+            combo.select("Combo Product 7"),
+            combo.select("Combo Product 8"),
+            combo.checkTotal(`${114 + 30 * 3 + 5}.00`),
+            Dialog.confirm(),
+            inLeftSide([
+                ...ProductScreen.selectedOrderlineHasDirect("Office Combo", "1", "232.10"),
+            ]),
+            ProductScreen.totalAmountIs("232.10"),
+            ProductScreen.clickPayButton(),
+            PaymentScreen.clickPaymentMethod("Bank"),
+            PaymentScreen.clickValidate(),
+            FeedbackScreen.isShown(),
+        ].flat(),
+});