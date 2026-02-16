dfeaf3d50cce9bd8a6d5b25664066b91008c4996
akth-odoo <akth@odoo.com>
2026-02-16 11:14:10 +0530

    [FIX] mail: display Invitation to follow notification correctly in inbox
    Steps to reproduce:
    
    - Configure user A to receive inbox notifications.
    - As user B, invite user A to follow a record with Notify recipients enabled.
    - Open the inbox of user A.
    
    The Invitation to follow notification is not displayed in the inbox when no
    additional comment is provided. This happens because the notification body is
    empty unless extra comments are added.
    
    This commit fixes the issue by displaying only the subject when the body is
    empty.
    
    Task-5485727
    
    closes odoo/odoo#244653
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/models/mail_thread.py
+++ addons/mail/models/mail_thread.py
@@ -4690,6 +4690,8 @@ class MailThread(models.AbstractModel):
             msg_values.update({
                 'partner_ids': list(partner_ids or [])
             })
+        if "subject" in kwargs:
+            msg_values["subject"] = kwargs["subject"]
         if msg_values:
             message.write(msg_values)
 
@@ -4715,6 +4717,8 @@ class MailThread(models.AbstractModel):
             "recipients": Store.many(message.partner_ids, fields=["avatar_128", "name"]),
             "write_date": message.write_date,
         }
+        if "subject" in kwargs:
+            res["subject"] = message.subject
         if body is not None:
             # sudo: mail.message.translation - discarding translations of message after editing it
             self.env["mail.message.translation"].sudo().search([("message_id", "=", message.id)]).unlink()
@@ -4805,7 +4809,7 @@ class MailThread(models.AbstractModel):
 
     @api.model
     def _get_allowed_message_update_params(self):
-        return {"attachment_ids", "body", "partner_ids"}
+        return {"attachment_ids", "body", "partner_ids", "subject"}
 
     @api.model
     def _get_thread_with_access(self, thread_id, mode="read", **kwargs):
