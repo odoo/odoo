ddf588412dd5a32e74847d266dc542964febfb68
Sébastien (blse) <blse@odoo.com>
2026-02-16 11:48:58 +0100

    [FIX] website: avoid empty option for grid
    Commit 64d35ccd6fade9e0473686b8484f561b5f4215ce added folding for groups
    of options, and folds them automatically, except the last group. But
    in some cases, the last container with options did not have any options
    displayed, and thus the last group shown was not unfolded.
    
    This commit fixes the issue for the case it was found by avoiding
    having the option completely (instead of having it, but being empty), by
    changing the selector.
    
    Steps to reproduce:
    - Open website builder on `/jobs`
    - Click on the bottom of the right column
    - Bug: the last group is not unfolded
    
    task-5946692

--- addons/website/static/src/builder/plugins/layout_option/grid_column_option.js
+++ addons/website/static/src/builder/plugins/layout_option/grid_column_option.js
@@ -1,13 +1,6 @@
-import { BaseOptionComponent, useDomState } from "@html_builder/core/utils";
+import { BaseOptionComponent } from "@html_builder/core/utils";
 
 export class GridColumnsOption extends BaseOptionComponent {
     static template = "website.GridColumnsOption";
-    static selector = ".row:not(.s_col_no_resize) > div";
-
-    setup() {
-        super.setup();
-        this.state = useDomState((editingElement) => ({
-            isGridMode: editingElement.parentElement.classList.contains("o_grid_mode"),
-        }));
-    }
+    static selector = ".row.o_grid_mode > div.o_grid_item";
 }
