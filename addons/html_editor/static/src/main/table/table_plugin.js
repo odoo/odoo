d2dcefb35e872e3cb00f89cfa40adea85501aed9
Adnan Saiyed <adsa@odoo.com>
2026-02-16 08:50:40 +0000

    [FIX] html_editor: fix toolbar position on selected table
    Current behavior before commit:
    
    Steps to reproduce:
    
    - Create a 3 x 3 table.
    - Select 3rd column and wait for toolbar.
    
    Currently, the toolbar is positioned at the start of the table even
    when the last column is selected. This happens because selecting cells
    in the 3rd column creates a DOM selection range that starts at the first
    cell and ends at the last cell of the column, traversing all intermediate
    elements. As a result, browser's range rectangle does not match the
    actual custom-selection rect.
    
    Desired behavior after commit:
    
    Now, Toolbar is positioned correctly above the custom selected cells.
    
    task-5935587
    
    closes odoo/odoo#257779
    
    X-original-commit: c0f818ce31dcf1b5fb935f045e895792ca80fe18
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Adnan Saiyed (adsa) <adsa@odoo.com>

--- addons/html_editor/static/src/main/table/table_plugin.js
+++ addons/html_editor/static/src/main/table/table_plugin.js
@@ -119,6 +119,7 @@ export class TablePlugin extends Plugin {
         traversed_nodes_processors: this.adjustTraversedNodes.bind(this),
         move_node_whitelist_selectors: "table",
         normalize_handlers: this.distributeTableColorsToAllCells.bind(this),
+        overlay_selection_target_rect_providers: this.getTableSelectionRangeRect.bind(this),
     };
 
     setup() {
@@ -664,6 +665,25 @@ export class TablePlugin extends Plugin {
         }
 
         return false;
+    }
+
+    getTableSelectionRangeRect() {
+        const selection = this.dependencies.selection.getEditableSelection();
+        if (closestElement(selection.commonAncestorContainer, "table.o_selected_table")) {
+            let [startTd, endTd] = [
+                closestElement(selection.anchorNode, "td"),
+                closestElement(selection.focusNode, "td"),
+            ];
+            if (selection.direction === DIRECTIONS.LEFT) {
+                [startTd, endTd] = [endTd, startTd];
+            }
+            const startTdRect = startTd.getBoundingClientRect();
+            const endTdRect = endTd.getBoundingClientRect();
+            const { left, top } = startTdRect;
+            const { bottom, right } = endTdRect;
+            const rect = new DOMRect(left, top, right - left, bottom - top);
+            return rect;
+        }
     }
 
     /**
