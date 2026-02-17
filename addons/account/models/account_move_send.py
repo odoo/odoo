4e5a7f47530adb4c7445719be77a45e25e41e565
Claire (clbr) <clbr@odoo.com>
2026-02-17 09:45:28 +0000

    [IMP] account: Add warning when send invoices cron is disabled
    When the Cron responsible of sending the invoices asynchronously is disabled,
    the user has no clear indication that his invoices won't be sent as they should.
    Let's add a warning in the batch wizard that notify the user something is wrong.
    
    task-none (feedback from SBR)
    
    X-original-commit: 0911b3d957e2fdaa70870ef5aa5f77f953a00a66

--- addons/account/models/account_move_send.py
+++ addons/account/models/account_move_send.py
@@ -117,7 +117,17 @@ class AccountMoveSend(models.AbstractModel):
         - action the action to run when the link is clicked
         """
         alerts = {}
-
+        send_cron = self.env.ref('account.ir_cron_account_move_send', raise_if_not_found=False)
+        if len(moves) > 1 and send_cron and not send_cron.sudo().active:
+            has_cron_access = send_cron.has_access('write')
+            has_access_message = _("The scheduled action 'Send Invoices automatically' is archived. You won't be able to send invoices in batch.")
+            no_access_addendum = _("\nPlease contact your administrator.")
+            alerts['account_send_cron_archived'] = {
+                'level': 'warning',
+                'message': has_access_message if has_cron_access else has_access_message + no_access_addendum,
+                'action_text': _("Check") if has_cron_access else None,
+                'action': send_cron._get_records_action() if has_cron_access else None,
+            }
         # Filter moves that are trying to send via email
         email_moves = moves.filtered(lambda m: 'email' in moves_data[m]['sending_methods'])
         if email_moves:
