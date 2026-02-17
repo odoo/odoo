fa3e8ad32ef49a7dff7289faca083a3deaabf2da
Antoine Boonen <aboo@odoo.com>
2026-02-17 09:58:25 +0000

    [FIX] _ubl_pint, account_edi_ubl_cii: Fix `_get_customization_ids`
    inheritence
    
    no-task
    
    X-original-commit: d5bfac1f24f7be40efc8c47b65135b9793516d2a

--- addons/l10n_jp_ubl_pint/models/account_edi_xml_pint_jp.py
+++ addons/l10n_jp_ubl_pint/models/account_edi_xml_pint_jp.py
@@ -1,6 +1,15 @@
 from odoo import models
 
 
+class AccountEdiXmlUBL21(models.AbstractModel):
+    _inherit = 'account.edi.xml.ubl_21'
+
+    def _get_customization_ids(self):
+        vals = super()._get_customization_ids()
+        vals['pint_jp'] = 'urn:peppol:pint:billing-1@jp-1'
+        return vals
+
+
 class AccountEdiXmlPint_Jp(models.AbstractModel):
     _name = 'account.edi.xml.pint_jp'
     _inherit = ["account.edi.xml.ubl_bis3"]
