742a01bb1846883dc191a4fe844390f14fadd5ec
niyp-odoo <niyp@odoo.com>
2026-02-17 17:33:47 +0530

    [FIX] stock_dropshipping : Fix false value
    The assertion is failing at this point
    https://github.com/odoo/odoo/blob/b5d3970e6b05e2c35ce16e972e659d152c4de70e/odoo/orm/models.py#L5207
    because of the condition related to is_dropship. When is_dropship is True,
    the partner_id is expected to be:
    p.sale_id.partner_shipping_id.id
    https://github.com/odoo/odoo/blob/b5d3970e6b05e2c35ce16e972e659d152c4de70e/addons/stock_dropshipping/models/stock.py#L95
    However, for the specific picking record in some cases, sale_id is not set.
    https://github.com/odoo/odoo/blob/b5d3970e6b05e2c35ce16e972e659d152c4de70e/addons/sale_stock/models/stock.py#L190
    As a result, the expression returns False. That False value is then passed to
    browse(), which is not meaningful and leads to the assertion error.
    To resolve this issue, I added a condition that is the value is appended in
    list only if the values are not false.
    
    opw: 5922525
    upg: 3889582
    tgb: 2449

--- addons/stock_dropshipping/models/stock.py
+++ addons/stock_dropshipping/models/stock.py
@@ -89,10 +89,11 @@ class StockLot(models.Model):
 
     def _compute_partner_ids(self):
         delivery_ids_by_lot = self._find_delivery_ids_by_lot()
+        all_picking_ids = tuple(id_ for ids in delivery_ids_by_lot.values() for id_ in ids)
         for lot in self:
             if delivery_ids_by_lot.get(lot.id, []):
-                picking_ids = self.env['stock.picking'].browse(delivery_ids_by_lot[lot.id]).sorted(key='date_done', reverse=True)
-                lot.partner_ids = list(p.sale_id.partner_shipping_id.id if p.is_dropship else p.partner_id.id for p in picking_ids)
+                picking_ids = self.env['stock.picking'].browse(delivery_ids_by_lot[lot.id]).with_prefetch(all_picking_ids).sorted(key='date_done', reverse=True)
+                lot.partner_ids = picking_ids.mapped(lambda p: p.sale_id.partner_shipping_id if p.is_dropship else p.partner_id)
             else:
                 lot.partner_ids = False
 
