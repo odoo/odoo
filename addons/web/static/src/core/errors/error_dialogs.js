dc924af5ee06f2657dc3533e20cbe78fc006a7e1
MAGREMENT <zavan@odoo.com>
2026-02-17 12:51:54 +0100

    [FIX] web : notify user when request body size exceeds server limit
    # How to reproduce
    - A reverse proxy needs to be set up between the client and the backend (for localhost, you can use nginx)
    - This reverse proxy needs to have a request max body size set below 128mb (for nginx : client_max_body_size)
    - If the system parameter web.max_file_upload_size is set, delete it and refresh your page
    - Pick any form view and add a file field with studio
    - Upload a file larger than the limit set in the proxy, but smaller than 128mb
    - Save the form
    
    # The problem
    The form is not saved and depending on the version, a Traceback will be shown (18.X) or a Connection Lost notification will be shown for a short period of time (19.0+)
    
    # Why
    When the system parameter web.max_file_upload_size is not set, the check for file size uses the default 128mb. A binary field added to a form via studio will upload its file in the json of the post request. This is done by encoding the file in base64. Our nginx servers set a limit for the request body size (usually 64mb).
    
    So if you add a file between 64mb and 128mb, it will bypass the default front-end size check but be stopped by the nginx reverse proxy. The proxy will send back an HTTP response with error code 413 to the client. Theses http responses are not correctly handled by the framework and are interpreted as a Connection Lost error because the response content cannot be parsed to json.
    
    Additionally, since we use base64 for the encoding and then use gzip to compress the json request, it's not really feasible to synchronize the front-end limit with the nginx one.
    
    opw-5891662
    
    closes odoo/odoo#249025
    
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/src/core/errors/error_dialogs.js
+++ addons/web/static/src/core/errors/error_dialogs.js
@@ -95,6 +95,12 @@ export class NetworkErrorDialog extends ErrorDialog {}
 NetworkErrorDialog.title = _t("Odoo Network Error");
 
 // -----------------------------------------------------------------------------
+// Request Entity Too Large Dialog
+// -----------------------------------------------------------------------------
+export class RequestEntityTooLargeErrorDialog extends ErrorDialog {}
+RequestEntityTooLargeErrorDialog.title = _t("The request sent to the server was too large");
+
+// -----------------------------------------------------------------------------
 // RPC Error Dialog
 // -----------------------------------------------------------------------------
 export class RPCErrorDialog extends ErrorDialog {
