dzdzdzdz1010/OdooStandard

[ADD] pos_payconiq, ...: support QR code payments via Payconiq/Wero
..., l10n_id_pos, l10n_in_pos, l10n_test_pos_qr_payment, point_of_sale,
pos_adyen, pos_dpopay, pos_glory_cash, pos_mercado_pago,pos_online_payment,
pos_pine_labs, pos_qfpay, pos_razorpay, pos_restaurant, pos_self_order,
pos_self_order_payconiq, pos_self_order_pine_labs, pos_self_order_qfpay,
pos_self_order_razorpay, pos_stripe, pos_viva_com

---

This commit introduces a new payment method type, `external_qr`, to handle QR-based payment flows directly in the Point of Sale.
With the widespread adoption of QR code payments in Belgium and the evolution of Payconiq under Wero, POS systems need native support for these new payment experiences.

A new module, `pos_payconiq`, is added to provide a full integration with the Payconiq platform, allowing customers to pay by scanning a QR code generated in the POS.
The module manages the creation, verification, and tracking of these payments.

Some internal refactoring was also done to align the payment terminal logic with this new `external_qr` method type and ensure consistency across payment flows in POS.

---

Task: [#4875917](https://www.odoo.com/odoo/project/1737/tasks/4875917)

addons/point_of_sale/static/src/app/models/pos_payment.js
@@ -1,5 +1,6 @@
 import { registry } from "@web/core/registry";
 import { Base } from "./related_models";
+import { formatCurrency } from "@point_of_sale/app/models/utils/currency";
 const { DateTime } = luxon;
 
 export class PosPayment extends Base {
@@ -10,6 +11,51 @@ export class PosPayment extends Base {
         this.payment_date = DateTime.now();
         this.amount = vals.amount || 0;
         this.ticket = vals.ticket || "";
+        this.canBeReversed = false;
+    }
+
+    initState() {
+        super.initState();
+        this.uiState = { qrCode: null, initStateDate: DateTime.now() };
+    }
+
+    get qrPaymentData() {
+        return {
+            qrCode: this.uiState.qrCode,
+            amount: this.pos_order_id?.currency
+                ? formatCurrency(this.getAmount(), this.pos_order_id.currency)
+                : this.getAmount(), // With the customer display, the pos_order_id may be undefined --> Fallback to raw amount
+        };
+    }
+
+    updateCustomerDisplayQrCode(qrCode) {
+        this.uiState.qrCode = qrCode;
+    }
+
+    /**
+     * Use camelCase naming for consistency with other models
+     */
+    get payment_interface() {
+        return this.payment_method_id.payment_interface;
+    }
+
+    /**
+     * Use camelCase naming for consistency with other models
+     */
+    get payment_provider() {
+        return this.payment_method_id.payment_provider;
+    }
+
+    get useTerminal() {
+        return this.payment_method_id.useTerminal;
+    }
+
+    get useQr() {
+        return this.payment_method_id.useQr;
+    }
+
+    get useBankQrCode() {
+        return this.payment_method_id.useBankQrCode;
     }
 
     isSelected() {
@@ -31,12 +77,33 @@ export class PosPayment extends Base {
 
     setPaymentStatus(value) {
         this.payment_status = value;
+
+        // canBeReversed
+        if (value === "done") {
+            this.canBeReversed = this.payment_interface?.supports_reversals || false;
+        } else if (value === "reversed") {
+            this.canBeReversed = false;
+        }
     }
 
     isDone() {
-        return this.getPaymentStatus()
-            ? this.getPaymentStatus() === "done" || this.getPaymentStatus() === "reversed"
-            : true;
+        const status = this.getPaymentStatus();
+        return status ? status === "done" || status === "reversed" : true;
+    }
+
+    isProcessing() {
+        const status = this.getPaymentStatus();
+        return status
+            ? [
+                  "waiting",
+                  "waitingCancel",
+                  "waitingCard",
+                  "waitingScan",
+                  "reversing",
+                  "force_done",
+                  "waitingCapture",
+              ].includes(status)
+            : false;
     }
 
     setCashierReceipt(value) {
@@ -51,24 +118,70 @@ export class PosPayment extends Base {
         return Boolean(this.getPaymentStatus());
     }
 
+    // ----- Payment Request -----
     async pay() {
         this.setPaymentStatus("waiting");
-
-        return this.handlePaymentResponse(
-            await this.payment_method_id.payment_terminal.sendPaymentRequest(this.uuid)
-        );
+        try {
+            const success = await this.payment_interface.sendPaymentRequest(this.uuid);
+            return this.handlePaymentResponse(success);
+        } catch (error) {
+            this.handlePaymentResponse(false);
+            throw error;
+        }
     }
 
     handlePaymentResponse(isPaymentSuccessful) {
-        if (isPaymentSuccessful) {
-            this.setPaymentStatus("done");
-            if (this.payment_method_id.payment_method_type !== "qr_code") {
-                this.can_be_reversed = this.payment_method_id.payment_terminal.supports_reversals;
-            }
-        } else {
-            this.setPaymentStatus("retry");
-        }
+        const status = isPaymentSuccessful ? "done" : "retry";
+        this.setPaymentStatus(status);
         return isPaymentSuccessful;
+    }
+
+    // ----- Payment Cancel -----
+    async cancelPayment(order) {
+        this.setPaymentStatus("waitingCancel");
+        try {
+            const success = await this.payment_interface.sendPaymentCancel(order, this.uuid);
+            return this.handlePaymentCancelResponse(success);
+        } catch (error) {
+            this.handlePaymentCancelResponse(false);
+            throw error;
+        }
+    }
+
+    handlePaymentCancelResponse(isCancelSuccessful) {
+        if (isCancelSuccessful) {
+            this.setPaymentStatus("retry");
+        } else if (this.useTerminal) {
+            this.setPaymentStatus("waitingCard");
+        } else if (this.useQr) {
+            this.setPaymentStatus("waitingScan");
+        }
+
+        return isCancelSuccessful;
+    }
+
+    // ----- Payment Reversal -----
+    async reversePayment() {
+        this.setPaymentStatus("reversing");
+        try {
+            const success = await this.payment_interface.sendPaymentReversal(this.uuid);
+            return this.handlePaymentReversalResponse(success);
+        } catch (error) {
+            this.handlePaymentReversalResponse(false);
+            throw error;
+        }
+    }
+
+    handlePaymentReversalResponse(isReversalSuccessful) {
+        if (isReversalSuccessful) {
+            this.setAmount(0);
+            this.setPaymentStatus("reversed");
+        } else {
+            this.setPaymentStatus("done");
+            this.canBeReversed = false;
+        }
+
+        return isReversalSuccessful;
     }
 
     /**
@@ -78,19 +191,16 @@ export class PosPayment extends Base {
     updateRefundPaymentLine(refundedPaymentLine) {}
 
     canBeAdjusted() {
-        if (this.payment_method_id.payment_terminal) {
-            return this.payment_method_id.payment_terminal.canBeAdjusted(this.uuid);
+        if (this.payment_interface) {
+            return this.payment_interface.canBeAdjusted(this.uuid);
         }
-        return (
-            !this.payment_method_id.is_cash_count &&
-            this.payment_method_id.payment_method_type != "qr_code"
-        );
+        return !this.payment_method_id.is_cash_count && !this.useBankQrCode;
     }
 
     async adjustAmount(amount) {
-        if (this.payment_method_id.payment_terminal) {
+        if (this.payment_interface) {
             this.amount += amount;
-            await this.payment_method_id.payment_terminal.sendPaymentAdjust(this.uuid);
+            await this.payment_interface.sendPaymentAdjust(this.uuid);
         }
     }
 }

odoo/odoo

addons/point_of_sale/static/src/app/models/pos_payment.js
@@ -1,5 +1,6 @@
 import { registry } from "@web/core/registry";
 import { Base } from "./related_models";
+import { formatCurrency } from "@point_of_sale/app/models/utils/currency";
 const { DateTime } = luxon;
 
 export class PosPayment extends Base {
@@ -10,6 +11,55 @@ export class PosPayment extends Base {
         this.payment_date = DateTime.now();
         this.amount = vals.amount || 0;
         this.ticket = vals.ticket || "";
+    }
+
+    initState() {
+        super.initState();
+        this.uiState = { qrCode: null, initStateDate: DateTime.now() };
+    }
+
+    getQrPopupProps(customerDisplay = false) {
+        return customerDisplay
+            ? {
+                  qrCode: this.uiState.qrCode,
+                  amount: this.getAmount(),
+                  isCustomerDisplay: true,
+                  footer: false,
+              }
+            : {
+                  qrCode: this.qr_code,
+                  amount: formatCurrency(this.getAmount(), this.pos_order_id.currency),
+              };
+    }
+
+    updateCustomerDisplayQrCode(qrCode) {
+        this.uiState.qrCode = qrCode;
+    }
+
+    /**
+     * Kept in snake_case for consistency with existing model fields.
+     */
+    get payment_interface() {
+        return this.payment_method_id.payment_interface;
+    }
+
+    /**
+     * Kept in snake_case for consistency with existing model fields.
+     */
+    get payment_provider() {
+        return this.payment_method_id.payment_provider;
+    }
+
+    get useTerminal() {
+        return this.payment_method_id.useTerminal;
+    }
+
+    get useQr() {
+        return this.payment_method_id.useQr;
+    }
+
+    get useBankQrCode() {
+        return this.payment_method_id.useBankQrCode;
     }
 
     isSelected() {
@@ -34,9 +84,17 @@ export class PosPayment extends Base {
     }
 
     isDone() {
-        return this.getPaymentStatus()
-            ? this.getPaymentStatus() === "done" || this.getPaymentStatus() === "reversed"
-            : true;
+        const status = this.getPaymentStatus();
+        return status ? status === "done" : true;
+    }
+
+    isProcessing() {
+        const status = this.getPaymentStatus();
+        return status
+            ? ["waiting", "waitingCancel", "waitingCard", "waitingScan", "waitingCapture"].includes(
+                  status
+              )
+            : false;
     }
 
     setCashierReceipt(value) {
@@ -51,24 +109,57 @@ export class PosPayment extends Base {
         return Boolean(this.getPaymentStatus());
     }
 
+    // ----- Payment Request -----
     async pay() {
         this.setPaymentStatus("waiting");
-
-        return this.handlePaymentResponse(
-            await this.payment_method_id.payment_terminal.sendPaymentRequest(this.uuid)
-        );
+        try {
+            const success = await this.payment_interface.sendPaymentRequest(this);
+            return this.handlePaymentResponse(success);
+        } catch (error) {
+            this.handlePaymentResponse(false);
+            throw error;
+        }
     }
 
     handlePaymentResponse(isPaymentSuccessful) {
-        if (isPaymentSuccessful) {
-            this.setPaymentStatus("done");
-            if (this.payment_method_id.payment_method_type !== "qr_code") {
-                this.can_be_reversed = this.payment_method_id.payment_terminal.supports_reversals;
-            }
-        } else {
-            this.setPaymentStatus("retry");
-        }
+        const status = isPaymentSuccessful ? "done" : "retry";
+        this.setPaymentStatus(status);
         return isPaymentSuccessful;
+    }
+
+    // ----- Payment Cancel -----
+    async cancelPayment() {
+        this.setPaymentStatus("waitingCancel");
+        try {
+            const success = await this.payment_interface.sendPaymentCancel(this);
+            return this.handlePaymentCancelResponse(success);
+        } catch (error) {
+            this.handlePaymentCancelResponse(false);
+            throw error;
+        }
+    }
+
+    handlePaymentCancelResponse(isCancelSuccessful) {
+        if (isCancelSuccessful) {
+            this.setPaymentStatus("retry");
+        } else if (this.useTerminal) {
+            this.setPaymentStatus("waitingCard");
+        } else if (this.useQr) {
+            this.setPaymentStatus("waitingScan");
+        } else {
+            this.setPaymentStatus("waiting");
+        }
+
+        return isCancelSuccessful;
+    }
+
+    // ----- Payment Force State -----
+    forceDone() {
+        this.setPaymentStatus("done");
+    }
+
+    forceCancel() {
+        this.setPaymentStatus("retry");
     }
 
     /**
@@ -78,19 +169,16 @@ export class PosPayment extends Base {
     updateRefundPaymentLine(refundedPaymentLine) {}
 
     canBeAdjusted() {
-        if (this.payment_method_id.payment_terminal) {
-            return this.payment_method_id.payment_terminal.canBeAdjusted(this.uuid);
+        if (this.payment_interface) {
+            return this.payment_interface.canBeAdjusted(this.uuid);
         }
-        return (
-            !this.payment_method_id.is_cash_count &&
-            this.payment_method_id.payment_method_type != "qr_code"
-        );
+        return !this.payment_method_id.is_cash_count && !this.useBankQrCode;
     }
 
     async adjustAmount(amount) {
-        if (this.payment_method_id.payment_terminal) {
+        if (this.payment_interface) {
             this.amount += amount;
-            await this.payment_method_id.payment_terminal.sendPaymentAdjust(this.uuid);
+            await this.payment_interface.sendPaymentAdjust(this.uuid);
         }
     }
 }