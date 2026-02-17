bf7a1f938edf273cc375cce7e6e4fe61bca0beb6
Tanguy Quéguineur <taqu@odoo.com>
2026-02-17 17:56:34 +0000

    [FIX] hr_attendance: use browser's geolocalisation in barcode scan
    **Issue**
    Check-in and check-out performed in kiosk mode via the barcode
    scanner were less accurate than those made via the manual
    selection.
    The reported inacurracy between the actual and real locations
    was several kilometers.
    
    **Cause**
    `attendance_barcode_scanned` was called without a location
    coming from the browser's geolocation API.
    In that case, the location was determined by the geoip
    database
    https://github.com/odoo/odoo/blob/51f59a293de1e86f66f30257f8fc0c419463d18c/addons/hr_attendance/controllers/main.py#L69-L70
    which is generally not as accurate as the location provided
    by the browser.
    
    opw-5889102
    
    closes odoo/odoo#273502
    
    X-original-commit: 85a7d3690c21a412eb41709670941e26c8dc8f1e
    Signed-off-by: Tanguy Quéguineur (taqu) <taqu@odoo.com>

--- addons/hr_attendance/controllers/main.py
+++ addons/hr_attendance/controllers/main.py
@@ -263,14 +263,17 @@ class HrAttendance(http.Controller):
                 return self._get_employee_info_response(employee)
         return {}
 
-    @http.route('/hr_attendance/attendance_barcode_scanned', type="jsonrpc", auth="public")
     def scan_barcode(self, token, barcode, check_in_image=None):
+        return self.scan_barcode_with_geolocation(token, barcode, check_in_image)
+
+    @http.route('/hr_attendance/attendance_barcode_scanned', type="jsonrpc", auth="public")
+    def scan_barcode_with_geolocation(self, token, barcode, latitude=False, longitude=False, check_in_image=None):
         company = self._get_company(token)
         if company:
             employee = request.env['hr.employee'].sudo().search([('barcode', '=', barcode), ('company_id', '=', company.id)], limit=1)
             if employee:
                 notification = employee._attendance_action_change(
-                    self._get_geoip_response('kiosk', device_tracking_enabled=company.attendance_device_tracking),
+                    self._get_geoip_response('kiosk', latitude=latitude, longitude=longitude, device_tracking_enabled=company.attendance_device_tracking),
                     self._get_validated_check_in_image_and_type(check_in_image, company.attendance_capture_check_in),
                 )
                 return self._get_attendance_action_response(employee, notification)
