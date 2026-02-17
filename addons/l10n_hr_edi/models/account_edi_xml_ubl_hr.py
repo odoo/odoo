975609b7456ac375f025347ee2acb0bbfc843681
Claire (clbr) <clbr@odoo.com>
2026-02-17 15:01:29 +0000

    [FIX] l10n_{hr,ro}_edi: Fix dependency to Peppol BIS3 constraints
    The CIUS RO and CIUS HR depends on the BIS3 which is fundamentally incorrect.
    This was probably made out of lazyness to redefine things that are almost the same
    in both these CIUS and the BIS3.
    Now, in previous PR [1], we added contraints for the Peppol BIS 3 that are impacting those
    formats. Indeed, the EndpointID can be empty in the context of CIUS RO and CIUS HR.
    In particular, it's breaking the sending to physical person at the moment.
    
    [1]: https://github.com/odoo/odoo/pull/246961
    
    opw-5943698
    
    closes odoo/odoo#249596
    
    X-original-commit: 7b9e596403a2baada30ef3ff0cd165b5282e9712
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

--- addons/l10n_hr_edi/models/account_edi_xml_ubl_hr.py
+++ addons/l10n_hr_edi/models/account_edi_xml_ubl_hr.py
@@ -63,7 +63,11 @@ class AccountEdiXmlUBLHR(models.AbstractModel):
         return nsmap
 
     def _export_invoice_constraints(self, invoice, vals):
-        constraints = super()._export_invoice_constraints(invoice, vals)
+        # OVERRIDE 'account.edi.xml.ubl_bis3': don't apply Peppol rules
+        constraints = self.env['account.edi.xml.ubl_20']._export_invoice_constraints(invoice, vals)
+        constraints.update(
+            self._invoice_constraints_cen_en16931_ubl(invoice, vals)
+        )
         constraints.update(
             self._invoice_constraints_eracun_new(invoice, vals)
         )
