21e3ff8ae520236252200af0760c9cdb590f5895
Farah (ahmfa) <ahmfa@odoo.com>
2026-02-17 08:53:08 +0000

    [FIX] calendar: prevent blocking viewing a contact in multicompany environment
    Steps to reproduce:
    ===================
    - Download Calendar and Contacts app
    - Go to the Setting -> Companies -> Manage companies; make sure there are at least two companies
    - Go to the Setting -> Users -> Manage users; make sure the current logged-in user has access to both companies
    - Create a another user who also have access to both companies
    - Search for the new user in contacts -> Assign the current company to the contact -> Through the internal link of the company, go to sales and purchase tab -> assign the same company in the company field
    - Switch the company of the logged in user to the other company
    - Open the test contact form, and use the meeting smart button to create a new meeting
    - Open the kanban contact view
    
     > Access Error: Uh-oh! Looks like you have stumbled upon some top-secret records.
    
    Cause of Issue:
    ==============
    When trying to view the search results in kanban view, the `meeting_count` is calculated for each contact. Hence,`_compute_meeting_count()` is called which calls `_compute_meeting()`.
    https://github.com/odoo/odoo/blob/f39785bcddd1eb5b7fb503d053c9bb66e2a0f15c/addons/calendar/models/res_partner.py#L49-L54
    Since the above section tries to access `partner.parent_id` each loop, it reaches a `parent_id` that's not accessible for the current user.
    
    Fix:
    ====
    Instead of trying to bypass the `Access Error` or using `sudo()` access, only the records available
    for the current user are accessed.
    
    opw-5874204
    
    closes odoo/odoo#272433
    
    X-original-commit: 2996ad0d0a0f36d00a73daead1653c878ce218a9
    Signed-off-by: Farah Ahmed (ahmfa) <ahmfa@odoo.com>

--- addons/calendar/models/res_partner.py
+++ addons/calendar/models/res_partner.py
@@ -47,7 +47,7 @@ class ResPartner(models.Model):
             # Add the events linked to the children of the partner
             for p in self.browse(meetings.keys()):
                 partner = p
-                while partner.parent_id:
+                while partner.parent_id and partner.parent_id in all_partners:
                     partner = partner.parent_id
                     if partner in self:
                         meetings[partner.id] = meetings.get(partner.id, set()) | meetings[p.id]
