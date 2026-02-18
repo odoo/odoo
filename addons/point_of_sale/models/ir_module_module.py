odoo/odoo

commit 282a667bae5409818a59fb40198c68edb9f9fe38

[IMP] point_of_sale: notify missing balance followup
If a user uses the "customer account" payment method without having the
`pos_settle_due` module installed or w\o invoicing the order, they will
not be able to observe the due balance computation.

In this pr we add a notification that explains this to the user.

closes odoo/odoo#183540

Task: 4251746
Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

addons/point_of_sale/models/ir_module_module.py
@@ -1,0 +1,21 @@
+from odoo import api, models
+
+
+class IrModuleModule(models.Model):
+    _inherit = 'ir.module.module'
+
+    @api.model
+    def _load_pos_data_fields(self):
+        return ['id', 'name', 'state']
+
+    @api.model
+    def _load_pos_data_domain(self):
+        return [('name', '=', 'pos_settle_due')]
+
+    def _load_pos_data(self, data):
+        domain = self._load_pos_data_domain()
+        fields = self._load_pos_data_fields()
+        return {
+            'data': self.search_read(domain, fields, load=False),
+            'fields': self._load_pos_data_fields(),
+        }

addons/point_of_sale/models/__init__.py
@@ -23,6 +23,7 @@ from . import product_combo_item
 from . import res_partner
 from . import res_company
 from . import res_config_settings
+from . import ir_module_module
 from . import stock_picking
 from . import stock_rule
 from . import stock_warehouse

addons/point_of_sale/models/pos_session.py
@@ -133,7 +133,7 @@ class PosSession(models.Model):
             'pos.category', 'pos.bill', 'res.company', 'account.tax', 'account.tax.group', 'product.product', 'product.attribute', 'product.attribute.custom.value',
             'product.template.attribute.line', 'product.template.attribute.value', 'product.combo', 'product.combo.item', 'product.packaging', 'res.users', 'res.partner',
             'decimal.precision', 'uom.uom', 'uom.category', 'res.country', 'res.country.state', 'res.lang', 'product.pricelist', 'product.pricelist.item', 'product.category',
-            'account.cash.rounding', 'account.fiscal.position', 'account.fiscal.position.tax', 'stock.picking.type', 'res.currency', 'pos.note', 'ir.ui.view']
+            'account.cash.rounding', 'account.fiscal.position', 'account.fiscal.position.tax', 'stock.picking.type', 'res.currency', 'pos.note', 'ir.ui.view', 'ir.module.module']
 
     @api.model
     def _load_pos_data_domain(self, data):

addons/point_of_sale/static/src/app/screens/payment_screen/payment_screen.js
@@ -111,6 +111,19 @@ export class PaymentScreen extends Component {
         return this.currentOrder.get_selected_paymentline();
     }
     async addNewPaymentLine(paymentMethod) {
+        if (
+            paymentMethod.type === "pay_later" &&
+            (!this.currentOrder.to_invoice ||
+                this.pos.data["ir.module.module"].find((m) => m.name === "pos_settle_due")
+                    ?.state !== "installed")
+        ) {
+            this.notification.add(
+                _t(
+                    "To ensure due balance follow-up, generate an invoice or download the accounting application. "
+                ),
+                { autocloseDelay: 7000, title: _t("Warning") }
+            );
+        }
         if (this.pos.paymentTerminalInProgress && paymentMethod.use_payment_terminal) {
             this.dialog.add(AlertDialog, {
                 title: _t("Error"),

commit 078c661bd3742188955bc23ddcf3b8a2d4e24bb6

[FIX] point_of_sale: missing parameter in module model field listing
This fixes an issue introduced in commit 282a667b, by adding a `config_id`
parameter to `IrModuleModule._load_pos_data_fields()` method, as used
elsewhere in PoS modules.  An invocation in `_load_pos_data()` is also fixed.

The missing parameter caused invocations of these methods to fail, for
instance when `PosSession.load_data()` got an `AccessError` (as demonstrated
in OCA/pos#1429).

addons/point_of_sale/models/ir_module_module.py
@@ -5,7 +5,7 @@ class IrModuleModule(models.Model):
     _inherit = 'ir.module.module'
 
     @api.model
-    def _load_pos_data_fields(self):
+    def _load_pos_data_fields(self, config_id):
         return ['id', 'name', 'state']
 
     @api.model
@@ -14,8 +14,8 @@ class IrModuleModule(models.Model):
 
     def _load_pos_data(self, data):
         domain = self._load_pos_data_domain()
-        fields = self._load_pos_data_fields()
+        fields = self._load_pos_data_fields(data['pos.config']['data'][0]['id'])
         return {
             'data': self.search_read(domain, fields, load=False),
-            'fields': self._load_pos_data_fields(),
+            'fields': fields,
         }