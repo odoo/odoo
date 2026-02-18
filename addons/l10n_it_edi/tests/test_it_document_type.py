0a3350fb05c40d761f5a143968f4ad9e534cd987
Hazem Ibrahim <ibrha@odoo.com>
2026-02-18 13:38:11 +0000

    [FIX] l10n_it_edi_ndd_account_dn: refactor typo for field DatiFattureCollegate
    When sending debit notes to the SDI, they are rejected with the following error:
    `File non conforme al formato : Invalid content was found starting with element 'Datifatturecollegate'. One of '{DatiOrdineAcquisto, DatiContratto, DatiConvenzione, DatiRicezione, DatiFattureCollegate, DatiSAL, DatiDDT, DatiTrasporto, FatturaPrincipale}' is expected.`
    
    In the `account_invoice_it_FatturaPA_export_debit_note` there's a typo in 'Datifatturecollegate' as it should be 'DatiFattureCollegate'
    https://github.com/odoo/odoo/blob/17.0/addons/l10n_it_edi_ndd_account_dn/data/invoice_it_template.xml
    
    opw-5930596
    
    closes odoo/odoo#253905
    
    X-original-commit: a2bddb6577ff6fb69330b3de9212cb74f60483c9
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/l10n_it_edi/tests/test_it_document_type.py
+++ addons/l10n_it_edi/tests/test_it_document_type.py
@@ -5,6 +5,12 @@ from odoo.addons.l10n_it_edi.tests.common import TestItEdi
 @tagged('post_install_l10n', 'post_install', '-at_install')
 class TestItDocumentType(TestItEdi):
 
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.module = 'l10n_it_edi_ndd_account_dn'
+
     def test_l10n_it_edi_debit_note_document_type(self):
         original_move = self.init_invoice('out_invoice', amounts=[1000], post=True)
 
@@ -28,3 +34,26 @@ class TestItDocumentType(TestItEdi):
 
         # when debit note is posted, the IT document type changes to correct type of debit note
         self.assertEqual(debit_note.l10n_it_document_type.code, 'TD05')
+
+    def test_l10n_it_edi_export_debit_note(self):
+        """Test that a Debit Note generates the 'DatiFattureCollegate' tag correctly."""
+        original_invoice = self._create_invoice(
+            invoice_date='2022-03-24',
+            invoice_date_due='2022-03-24',
+            post=True,
+            partner_id=self.italian_partner_a,
+            company_id=self.company,
+            invoice_line_ids=[self._prepare_invoice_line(price_unit=1000, tax_ids=self.default_tax, name='Original Service')]
+        )
+        debit_note = self._create_invoice(
+            move_type='out_invoice',
+            name='DN/2022/00001',
+            invoice_date='2022-03-25',
+            invoice_date_due='2022-03-25',
+            post=True,
+            partner_id=self.italian_partner_a,
+            debit_origin_id=original_invoice,
+            company_id=self.company,
+            invoice_line_ids=[self._prepare_invoice_line(price_unit=100, tax_ids=self.default_tax, name='Debit Note Adjustment')]
+        )
+        self._assert_export_invoice(debit_note, 'it_edi_debit_note.xml')
