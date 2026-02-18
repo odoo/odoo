5b6b676ae4be9a7687fd3839bef09a15b761174f
Lancelot Semal <lase@odoo.com>
2026-02-18 15:21:21 +0000

    [FIX] {purchase_,}mrp: remove cost_share rounding
    Steps to reproduce:
    
    - Create a kit product with 6 BoM lines (with a `cost_share` of `0.0%`)
    - Create + confirm a purchase order for 1 unit of the kit product at 100
    - Validate the delivery
    > One stock moves is at 16.65 the others at 16.67
    
    Expected behavior:
    
    The value of purchase order line should be equidistributed among
    component moves to 16.67 and the associated rounding issue should be
    handled at closing.
    
    Cause of the issue:
    
    Kit products rely on the `cost_share` field of the `stock.move`'s to
    determine the cost distribution of a kit product  among its component:
    https://github.com/odoo/odoo/blob/39fe1b3713857f5bbd14118b5f0ad358c17d6aa6/addons/purchase_stock/models/stock_move.py#L232-L235
    https://github.com/odoo/odoo/blob/39fe1b3713857f5bbd14118b5f0ad358c17d6aa6/addons/purchase_mrp/models/stock_move.py#L25
    However, as this field is rounded  to the second decimal:
    https://github.com/odoo/odoo/blob/39fe1b3713857f5bbd14118b5f0ad358c17d6aa6/addons/mrp/models/stock_move.py#L56-L57
    it leads to unavoidable `cost_share` rounding issue when equidistributed
    (for unset `mrp.bom.line`'s`cost_share`). A rounding issue that is
    propagated to the last move cost share to sum up to `100.0%`:
    https://github.com/odoo/odoo/blob/39fe1b3713857f5bbd14118b5f0ad358c17d6aa6/addons/mrp/models/mrp_bom.py#L469-L474
    https://github.com/odoo/odoo/blob/39fe1b3713857f5bbd14118b5f0ad358c17d6aa6/addons/purchase_mrp/models/mrp_bom.py#L26-L31
    Now the issue is that this rounding issue is unavoidable and can be of
    the size of a relevant percentage which is propagated on the move value.
    
    opw-5085457
    
    X-original-commit: 41c21fa1c4e5f9ee62ade311335022653294af74
    Part-of: odoo/odoo#253734
    Signed-off-by: Tiffany Chang (tic) <tic@odoo.com>
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/mrp/models/stock_move.py
+++ addons/mrp/models/stock_move.py
@@ -54,7 +54,7 @@ class StockMove(models.Model):
     order_finished_lot_ids = fields.Many2many('stock.lot', string="Finished Lot/Serial Number", related="raw_material_production_id.lot_producing_ids")
     should_consume_qty = fields.Float('Quantity To Consume', compute='_compute_should_consume_qty', digits='Product Unit')
     cost_share = fields.Float(
-        "Cost Share (%)", digits=(5, 2),  # decimal = 2 is important for rounding calculations!!
+        "Cost Share (%)", digits=0,
         help="The percentage of the final production cost for this by-product. The total of all by-products' cost share must be smaller or equal to 100.")
     product_qty_available = fields.Float('Product On Hand Quantity', related='product_id.qty_available', depends=['product_id'])
     product_virtual_available = fields.Float('Product Forecasted Quantity', related='product_id.virtual_available', depends=['product_id'])
