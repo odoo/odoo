35cb59192110dba4f128887a2754d98c8c842f57
bona-odoo <bona@odoo.com>
2026-02-18 07:08:31 +0000

    [FIX] project: prevent crash on 'Search More' in task relational fields
    Steps to reproduce:
    --------------------------
    1. Install Project and create a portal user.
    2. Create a project and a task with tags added to it.
    3. Share the project with the portal user (Edit access).
    4. Log in as the portal user and open the task.
    5. Click on the 'Tags' field and select 'Search More'
    
    Issue:
    ----------
    A traceback occurs:
    ```ValueError: Invalid field project.tags.display_in_project in condition ('display_in_project', '=', True)```
    
    Cause:
    -----------
    The 'ProjectTaskRelationalModel' calls '_processSearchDomain'.
    This method automatically appends a domain when 'showSubtasks' is not enabled.
    
    When clicking 'Search More' on the 'Tags' field, the same domain is
    applied to the 'project.tags' model. However, since 'project.tags'
    does not have the 'display_in_project' field, the ORM raises a ValueError.
    
    In portal, only project.webclient assets are loaded, causing
    ProjectTaskRelationalModel to be used for all relational fields.
    In contrast, internal users use the standard RelationalModel, so the
    issue does not occur.
    
    Solution:
    ------------
    Restrict the domain modification in _processSearchDomain to apply only
    when the current model is `project.task` and `report.project.task.user`.
    
    opw-5930427
    
    X-original-commit: a65c216e32993a441fa4389befef135d6ab66f9d

--- addons/project/static/src/views/project_task_model_mixin.js
+++ addons/project/static/src/views/project_task_model_mixin.js
@@ -5,7 +5,7 @@ export const ProjectTaskModelMixin = (T) => class ProjectTaskModelMixin extends 
     _processSearchDomain(domain) {
         const { my_tasks, subtask_action, activity_action } = this.env.searchModel.globalContext;
         const showSubtasks = my_tasks || subtask_action || activity_action || JSON.parse(browser.localStorage.getItem("showSubtasks"));
-        if (!showSubtasks) {
+        if (['project.task', 'report.project.task.user'].includes(this.env.searchModel.resModel) && !showSubtasks) {
             domain = Domain.and([
                 domain,
                 [['display_in_project', '=', true]],
