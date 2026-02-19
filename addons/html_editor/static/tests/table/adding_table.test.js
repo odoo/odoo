81f2e6992a8fe59d2986213cc458261833b80c07
Benoit Socias (bso) <bso@odoo.com>
2026-02-19 12:50:21 +0100

    [IMP] html_editor: add table size selection for mobile
    When adding a table on mobile, it is always created as 3x3 without
    asking for the size to the user.
    
    This commit adds a popover where the user can specify the number of rows
    and the number of columns.
    
    task-5951362

--- addons/html_editor/static/tests/table/adding_table.test.js
+++ addons/html_editor/static/tests/table/adding_table.test.js
@@ -4,7 +4,7 @@ import { setupEditor } from "../_helpers/editor";
 import { getContent } from "../_helpers/selection";
 import { insertText } from "../_helpers/user_actions";
 import { unformat } from "../_helpers/format";
-import { press, waitFor, queryOne } from "@odoo/hoot-dom";
+import { click, press, waitFor, queryOne } from "@odoo/hoot-dom";
 import { expectElementCount } from "../_helpers/ui_expectations";
 import { findInSelection } from "@html_editor/utils/selection";
 
@@ -63,6 +63,108 @@ test("can add a table using the powerbox and keyboard", async () => {
     );
 });
 
+test.tags("mobile");
+test("can add a table using the powerbox and keyboard (mobile)", async () => {
+    const { el, editor } = await setupEditor("<p>a[]</p>");
+    await expectElementCount(".o-we-powerbox", 0);
+    expectContentToBe(el, `<p>a[]</p>`);
+
+    // open powerbox
+    await insertText(editor, "/");
+    await waitFor(".o-we-powerbox");
+    await expectElementCount(".o-we-tablesizepopover", 0);
+
+    // filter to get table command in first position
+    await insertText(editor, "table");
+    await animationFrame();
+
+    // press enter to open tablepicker
+    await press("Enter");
+    await waitFor(".o-we-tablesizepopover");
+    await expectElementCount(".o-we-powerbox", 0);
+
+    // press enter to validate current dimension (3x3)
+    await press("Enter");
+    await animationFrame();
+    await expectElementCount(".o-we-powerbox", 0);
+    await expectElementCount(".o-we-tablesizepopover", 0);
+    expectContentToBe(
+        el,
+        `<p>a</p>
+        <table class="table table-bordered o_table">
+            <tbody>
+                <tr>
+                    <td><p o-we-hint-text='Type "/" for commands' class="o-we-hint">[]<br></p></td>
+                    <td><p><br></p></td>
+                    <td><p><br></p></td>
+                </tr>
+                <tr>
+                    <td><p><br></p></td>
+                    <td><p><br></p></td>
+                    <td><p><br></p></td>
+                </tr>
+                <tr>
+                    <td><p><br></p></td>
+                    <td><p><br></p></td>
+                    <td><p><br></p></td>
+                </tr>
+            </tbody>
+        </table>
+        <p data-selection-placeholder="" style="margin: -9px 0px 8px;"><br></p>`
+    );
+});
+
+test.tags("mobile");
+test("can add a table using the powerbox and apply (mobile)", async () => {
+    const { el, editor } = await setupEditor("<p>a[]</p>");
+    await expectElementCount(".o-we-powerbox", 0);
+    expectContentToBe(el, `<p>a[]</p>`);
+
+    // open powerbox
+    await insertText(editor, "/");
+    await waitFor(".o-we-powerbox");
+    await expectElementCount(".o-we-tablesizepopover", 0);
+
+    // filter to get table command in first position
+    await insertText(editor, "table");
+    await animationFrame();
+
+    // press enter to open tablepicker
+    await press("Enter");
+    await waitFor(".o-we-tablesizepopover");
+    await expectElementCount(".o-we-powerbox", 0);
+
+    // click apply to validate current dimension (3x3)
+    await click(".o-we-tablesizepopover .btn-primary");
+    await animationFrame();
+    await expectElementCount(".o-we-powerbox", 0);
+    await expectElementCount(".o-we-tablesizepopover", 0);
+    expectContentToBe(
+        el,
+        `<p>a</p>
+        <table class="table table-bordered o_table">
+            <tbody>
+                <tr>
+                    <td><p o-we-hint-text='Type "/" for commands' class="o-we-hint">[]<br></p></td>
+                    <td><p><br></p></td>
+                    <td><p><br></p></td>
+                </tr>
+                <tr>
+                    <td><p><br></p></td>
+                    <td><p><br></p></td>
+                    <td><p><br></p></td>
+                </tr>
+                <tr>
+                    <td><p><br></p></td>
+                    <td><p><br></p></td>
+                    <td><p><br></p></td>
+                </tr>
+            </tbody>
+        </table>
+        <p data-selection-placeholder="" style="margin: -9px 0px 8px;"><br></p>`
+    );
+});
+
 test.tags("desktop");
 test("can close table picker with escape", async () => {
     const { el, editor } = await setupEditor("<p>a[]</p>");
@@ -77,6 +179,38 @@ test("can close table picker with escape", async () => {
     await press("escape");
     await animationFrame();
     await expectElementCount(".o-we-tablepicker", 0);
+});
+
+test.tags("mobile");
+test("can close table picker with escape (mobile)", async () => {
+    const { el, editor } = await setupEditor("<p>a[]</p>");
+    await insertText(editor, "/");
+    await waitFor(".o-we-powerbox");
+    await insertText(editor, "table");
+    expectContentToBe(el, "<p>a/table[]</p>");
+    await animationFrame();
+    await press("Enter");
+    await expectElementCount(".o-we-tablesizepopover", 1);
+    await press("escape");
+    await animationFrame();
+    expectContentToBe(el, "<p>a[]</p>");
+    await expectElementCount(".o-we-tablesizepopover", 0);
+});
+
+test.tags("mobile");
+test("can close table picker with discard (mobile)", async () => {
+    const { el, editor } = await setupEditor("<p>a[]</p>");
+    await insertText(editor, "/");
+    await waitFor(".o-we-powerbox");
+    await insertText(editor, "table");
+    expectContentToBe(el, "<p>a/table[]</p>");
+    await animationFrame();
+    await press("Enter");
+    await expectElementCount(".o-we-tablesizepopover", 1);
+    await click(".o-we-tablesizepopover .btn-secondary");
+    await animationFrame();
+    expectContentToBe(el, "<p>a[]</p>");
+    await expectElementCount(".o-we-tablesizepopover", 0);
 });
 
 test.tags("iframe", "desktop");
