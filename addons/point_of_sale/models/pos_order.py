86647c218e0aaf5fd59f2649b45dee65ff280696
Pedram (PEBR) <pebr@odoo.com>
2026-02-19 17:30:50 +0100

    [FIX] point_of_sale: keep the backend refund consistent with UI
    Before this commit, when creating a refund from backend, the refunded
    lines had negative price, which is not the case when creating a refund
    from the UI. This commit makes sure that the refunded lines have
    positive price.
    
    opw-5459378

--- addons/point_of_sale/models/pos_order.py
+++ addons/point_of_sale/models/pos_order.py
@@ -1671,10 +1671,11 @@ class PosOrderLine(models.Model):
 
     def _compute_amount_line_all(self):
         self.ensure_one()
+        sign = -1 if self.order_id.is_refund else 1
         fpos = self.order_id.fiscal_position_id
         tax_ids_after_fiscal_position = fpos.map_tax(self.tax_ids)
         price = self.price_unit * (1 - (self.discount or 0.0) / 100.0)
-        taxes = tax_ids_after_fiscal_position.compute_all(price, self.order_id.currency_id, self.qty, product=self.product_id, partner=self.order_id.partner_id)
+        taxes = tax_ids_after_fiscal_position.compute_all(price, self.order_id.currency_id, self.qty * sign, product=self.product_id, partner=self.order_id.partner_id)
         return {
             'price_subtotal_incl': taxes['total_included'],
             'price_subtotal': taxes['total_excluded'],
