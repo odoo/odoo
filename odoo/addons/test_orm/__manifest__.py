642155a62d0efe2206d99c128dda25c31c510df8
Ruben Gomes (rugo) <rugo@odoo.com>
2026-02-20 13:37:20 +0100

    test_tools (test_convert and test_convert_env)

--- odoo/addons/test_orm/__manifest__.py
+++ odoo/addons/test_orm/__manifest__.py
@@ -8,8 +8,6 @@
         'data/test_action_bindings.xml',
         'data/test_inherits.xml',
         'data/test_orm_data.xml',
-        'data/test_translated_field/test_model_data.xml',
-        'security/ir.model.access.csv',
         'security/test_access_rights_security.xml',
         'security/test_orm_security.xml',
         'views/test_orm_views.xml',
--- odoo/addons/test_orm/models/__init__.py
+++ odoo/addons/test_orm/models/__init__.py
@@ -2,7 +2,6 @@ from . import (
     test_access_rights,
     test_action_bindings,
     test_config,
-    test_convert,
     test_html_converter,
     test_inherits,
     test_orm,
