dzdzdzdz1010/OdooStandard

[IMP] hr_work_entry: Working schedule display based on work entries
The calendar now correctly grays out days based on work entry types.
Work entries that are considered as absence will grey out a block in the
calendar, while others will make the sections white. This was already
implemented in l10n_hk_hr_payroll so this commit is just moving this
feature into standard.

task-5085284

addons/hr_work_entry/models/resource_calendar.py
@@ -23,3 +23,30 @@ class ResourceCalendar(models.Model):
                 ],
             ),
         )
+
+    def _work_intervals_batch(self, start_dt, end_dt, resources_per_tz=None, domain=None, compute_leaves=True):
+        work_intervals = super()._work_intervals_batch(
+            start_dt,
+            end_dt,
+            resources_per_tz=resources_per_tz,
+            domain=domain,
+            compute_leaves=compute_leaves,
+        )
+
+        if not compute_leaves:
+            return work_intervals
+
+        all_resources = set()
+        if not resources_per_tz or self:
+            all_resources.add(self.env["resource.resource"])
+        if resources_per_tz:
+            for _, resources in resources_per_tz.items():
+                all_resources |= set(resources)
+
+        leave_attendance_intervals = self.sudo()._attendance_intervals_batch(
+            start_dt,
+            end_dt,
+            resources_per_tz=resources_per_tz,
+            domain=[("work_entry_type_id.category", "=", "absence")],
+        )
+        return {r.id: (work_intervals[r.id] - leave_attendance_intervals[r.id]) for r in all_resources}

odoo/odoo

addons/hr_work_entry/models/resource_calendar.py
@@ -23,3 +23,30 @@ class ResourceCalendar(models.Model):
                 ],
             ),
         )
+
+    def _work_intervals_batch(self, start_dt, end_dt, resources_per_tz=None, domain=None, compute_leaves=True):
+        work_intervals = super()._work_intervals_batch(
+            start_dt,
+            end_dt,
+            resources_per_tz=resources_per_tz,
+            domain=domain,
+            compute_leaves=compute_leaves,
+        )
+
+        if not compute_leaves:
+            return work_intervals
+
+        all_resources = set()
+        if not resources_per_tz or self:
+            all_resources.add(self.env["resource.resource"])
+        if resources_per_tz:
+            for _, resources in resources_per_tz.items():
+                all_resources |= set(resources)
+
+        leave_attendance_intervals = self.sudo()._attendance_intervals_batch(
+            start_dt,
+            end_dt,
+            resources_per_tz=resources_per_tz,
+            domain=[("work_entry_type_id.count_as", "=", "absence")],
+        )
+        return {r.id: (work_intervals[r.id] - leave_attendance_intervals[r.id]) for r in all_resources}