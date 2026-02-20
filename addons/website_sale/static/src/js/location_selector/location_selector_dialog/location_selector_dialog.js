dzdzdzdz1010/OdooStandard

[MOV] delivery->website_sale: location_selector
This commit moves all the code related to pickup points feature from
`delivery` module to `website_sale`. This is because there's no need to
have the feature in `delivery`, since all delivery methods that
currently support this feature depend on `website_sale`, namely:
Sendcloud, FedEx and Click&Collect.

Task-3645144

addons/website_sale/static/src/js/location_selector/location_selector_dialog/location_selector_dialog.js
@@ -1,61 +1,199 @@
-import {
-    LocationSelectorDialog
-} from '@delivery/js/location_selector/location_selector_dialog/location_selector_dialog';
-import { patch } from '@web/core/utils/patch';
+import { LocationList } from '@website_sale/js/location_selector/location_list/location_list';
+import { MapContainer } from '@website_sale/js/location_selector/map_container/map_container';
+import { Component, onMounted, onWillUnmount, useEffect, useState } from '@odoo/owl';
+import { browser } from '@web/core/browser/browser';
+import { Dialog } from '@web/core/dialog/dialog';
 import { _t } from '@web/core/l10n/translation';
+import { rpc } from '@web/core/network/rpc';
+import { useDebounced } from '@web/core/utils/timing';
 
-patch(LocationSelectorDialog, {
-    props: {
-        ...LocationSelectorDialog.props,
+export class LocationSelectorDialog extends Component {
+    static components = { Dialog, LocationList, MapContainer };
+    static template = 'website_sale.locationSelector.dialog';
+    static props = {
         orderId: { type: Number, optional: true },
         isFrontend: { type: Boolean, optional: true },
-    },
-});
+        zipCode: String,
+        selectedLocationId: { type: String, optional: true },
+        save: Function,
+        close: Function, // This is the close from the env of the Dialog Component
+    };
+    static defaultProps = {
+        selectedLocationId: false,
+    };
 
-patch(LocationSelectorDialog.prototype, {
     setup() {
-        super.setup(...arguments);
+        this.state = useState({
+            locations: [],
+            error: false,
+            viewMode: 'list',
+            zipCode: this.props.zipCode,
+            // Some APIs like FedEx use strings to identify locations.
+            selectedLocationId: String(this.props.selectedLocationId),
+            isSmall: this.env.isSmall,
+        });
 
-        if (this.props.isFrontend) {
-            this.getLocationUrl = '/website_sale/get_pickup_locations';
+        this.getLocationUrl = '/website_sale/get_pickup_locations';
+
+        this.debouncedOnResize = useDebounced(this.updateSize, 300);
+        this.debouncedSearchButton = useDebounced((zipCode) => {
+            this.state.locations = [];
+            this._updateLocations(zipCode);
+        }, 300);
+
+        onMounted(() => {
+            browser.addEventListener('resize', this.debouncedOnResize);
+            this.updateSize();
+        });
+        onWillUnmount(() => browser.removeEventListener('resize', this.debouncedOnResize));
+
+        // Fetch new locations when the zip code is updated.
+        useEffect(
+            (zipCode) => {
+                this._updateLocations(zipCode)
+                return () => {
+                    this.state.locations = []
+                };
+            },
+            () => [this.state.zipCode]
+        );
+    }
+
+    //--------------------------------------------------------------------------
+    // Data Exchanges
+    //--------------------------------------------------------------------------
+
+    /**
+     * Fetch the closest pickup locations based on the zip code.
+     *
+     * @private
+     * @param {String} zip - The zip code used to look for close locations.
+     * @return {Object} The result values.
+     */
+    async _getLocations(zip) {
+        return rpc(this.getLocationUrl, { order_id: this.props.orderId, zip_code: zip });
+    }
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * Get the locations based on the zip code.
+     *
+     * Select the first location available if no location is currently selected or if the currently
+     * selected location is not on the list anymore.
+     *
+     * @private
+     * @param {String} zip - The zip code used to look for close locations.
+     * @return {void}
+     */
+    async _updateLocations(zip) {
+        this.state.error = false;
+        const { pickup_locations, error } = await this._getLocations(zip);
+        if (error) {
+            this.state.error = error;
+            console.error(error);
+        } else {
+            this.state.locations = pickup_locations;
+            if (!this.state.locations.find(l => String(l.id) === this.state.selectedLocationId)) {
+                this.state.selectedLocationId = this.state.locations[0]
+                    ? String(this.state.locations[0].id)
+                    : false;
+            }
         }
-    },
+    }
+
+    get showListView() {
+        return this.state.locations.length !== 1;
+    }
+
+    /**
+     * Find the selected location based on its id.
+     *
+     * @return {Object} The selected location.
+     */
+    get selectedLocation() {
+        return this.state.locations.find(l => String(l.id) === this.state.selectedLocationId);
+    }
+
+    /**
+     * Set the selectedLocationId in the state.
+     *
+     * @param {String} locationId
+     * @return {void}
+     */
+    setSelectedLocation(locationId) {
+        this.state.selectedLocationId = String(locationId);
+    }
+
+    /**
+     * Confirm the current selected location.
+     *
+     * @return {void}
+     */
+    async validateSelection() {
+        if (!this.state.selectedLocationId) return;
+        const selectedLocation = this.state.locations.find(
+            l => String(l.id) === this.state.selectedLocationId
+        );
+        await this.props.save(selectedLocation);
+        this.props.close();
+    }
+
+    //--------------------------------------------------------------------------
+    // User Interface
+    //--------------------------------------------------------------------------
+
+    /**
+     * Determines the component to show in mobile view based on the current state.
+     *
+     * Returns the MapContainer component if `viewMode` is strictly equal to `map`, else return the
+     * List component.
+     *
+     * @return {Component} The component to show in mobile view.
+     */
+    get mobileComponent() {
+        if (this.state.viewMode === 'map') return MapContainer;
+        return LocationList;
+    }
 
     get title() {
-        // The original definition of this getter is in `delivery` module which is not a frontend module. This problem happens in the context of the website. So, it should be repeated here as translations are only fetched in the context of a frontend module, which is `website_sale` in this case.
         if (this.state.locations.length === 1) {
             return _t("Pickup Location")
         }
         return _t("Choose a pick-up point");
-    },
+    }
 
     get validationButtonLabel() {
-        // The original definition of this getter is in `delivery` module which is not a frontend module. This problem happens in the context of the website. So, it should be repeated here as translations are only fetched in the context of a frontend module, which is `website_sale` in this case.
         return _t("Choose this location");
-    },
+    }
 
     get postalCodePlaceholder() {
-        // The original definition of this getter is in `delivery` module which is not a frontend module. This problem happens in the context of the website. So, it should be repeated here as translations are only fetched in the context of a frontend module, which is `website_sale` in this case.
         return _t("Your postal code");
-    },
+    }
 
     get listViewButtonLabel() {
-        // The original definition of this getter is in `delivery` module which is not a frontend module. This problem happens in the context of the website. So, it should be repeated here as translations are only fetched in the context of a frontend module, which is `website_sale` in this case.
         return _t("List view");
-    },
+    }
 
     get mapViewButtonLabel() {
-        // The original definition of this getter is in `delivery` module which is not a frontend module. This problem happens in the context of the website. So, it should be repeated here as translations are only fetched in the context of a frontend module, which is `website_sale` in this case.
         return _t("Map view");
-    },
+    }
 
     get errorMessage() {
-        // The original definition of this getter is in `delivery` module which is not a frontend module. This problem happens in the context of the website. So, it should be repeated here as translations are only fetched in the context of a frontend module, which is `website_sale` in this case.
         return _t("No result");
-    },
+    }
 
     get loadingMessage() {
-        // The original definition of this getter is in `delivery` module which is not a frontend module. This problem happens in the context of the website. So, it should be repeated here as translations are only fetched in the context of a frontend module, which is `website_sale` in this case.
         return _t("Loading...");
-    },
-});
+    }
+
+    /**
+     *
+     * @return {void}
+     */
+    updateSize() {
+        this.state.isSmall = this.env.isSmall;
+    }
+}

odoo/odoo

addons/website_sale/static/src/js/location_selector/location_selector_dialog/location_selector_dialog.js
@@ -1,60 +1,212 @@
-import {
-    LocationSelectorDialog
-} from '@delivery/js/location_selector/location_selector_dialog/location_selector_dialog';
-import { patch } from '@web/core/utils/patch';
+import { LocationList } from '@website_sale/js/location_selector/location_list/location_list';
+import { MapContainer } from '@website_sale/js/location_selector/map_container/map_container';
+import { Component, onMounted, onWillUnmount, useEffect, useState } from '@odoo/owl';
+import { browser } from '@web/core/browser/browser';
+import { Dialog } from '@web/core/dialog/dialog';
 import { _t } from '@web/core/l10n/translation';
+import { rpc } from '@web/core/network/rpc';
+import { useDebounced } from '@web/core/utils/timing';
 
-patch(LocationSelectorDialog, {
-    props: {
-        ...LocationSelectorDialog.props,
+export class LocationSelectorDialog extends Component {
+    static components = { Dialog, LocationList, MapContainer };
+    static template = 'website_sale.locationSelector.dialog';
+    static props = {
         isFrontend: { type: Boolean, optional: true },
-    },
-});
+        zipCode: String,
+        selectedLocationId: { type: String, optional: true },
+        save: Function,
+        close: Function, // This is the close from the env of the Dialog Component
+    };
+    static defaultProps = {
+        selectedLocationId: false,
+    };
 
-patch(LocationSelectorDialog.prototype, {
     setup() {
-        super.setup(...arguments);
+        this.state = useState({
+            locations: [],
+            error: false,
+            viewMode: 'list',
+            zipCode: this.props.zipCode,
+            // Some APIs like FedEx use strings to identify locations.
+            selectedLocationId: String(this.props.selectedLocationId),
+            isSmall: this.env.isSmall,
+        });
 
-        if (this.props.isFrontend) {
-            this.getLocationUrl = '/website_sale/get_pickup_locations';
+        this.getLocationUrl = '/website_sale/get_pickup_locations';
+
+        this.debouncedOnResize = useDebounced(this.updateSize, 300);
+        this.debouncedSearchButton = useDebounced(() => {
+            this.state.locations = [];
+            this._loadLocations();
+        }, 300);
+
+        onMounted(() => {
+            browser.addEventListener('resize', this.debouncedOnResize);
+            this.updateSize();
+        });
+        onWillUnmount(() => browser.removeEventListener('resize', this.debouncedOnResize));
+
+        // Fetch new locations when the zip code is updated.
+        useEffect(
+            () => {
+                this._loadLocations()
+                return () => {
+                    this.state.locations = []
+                };
+            },
+            () => [this.state.zipCode]
+        );
+    }
+
+    //--------------------------------------------------------------------------
+    // Data Exchanges
+    //--------------------------------------------------------------------------
+
+    get locations() { return this.state.locations; }
+
+    /**
+     * Fetch the closest pickup locations based on the zip code.
+     *
+     * @private
+     * @return {Object} The result values.
+     */
+    async _getLocations() {
+        return await rpc(this.getLocationUrl, this._getLocationsParams());
+    }
+
+    /**
+     * Fetch the information needed to get the closest pickup locations
+     *
+     * @private
+     * @return {Object} The result values.
+     */
+    _getLocationsParams() {
+        return { zip_code: this.state.zipCode };
+    }
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * Get the locations based on the zip code.
+     *
+     * Select the first location available if no location is currently selected or if the currently
+     * selected location is not on the list anymore.
+     *
+     * @private
+     * @return {void}
+     */
+    async _loadLocations() {
+        this.state.error = false;
+        const { pickup_locations, error } = await this._getLocations();
+        if (error) {
+            this.state.error = error;
+            console.error(error);
+        } else {
+            this._updateLocations(pickup_locations);
+            this._selectLocation();
         }
-    },
+    }
+
+    _updateLocations(locations) {
+        this.state.locations = locations;
+    }
+
+    _selectLocation() {
+        if (!this.locations.find(l => String(l.id) === this.state.selectedLocationId)) {
+            this.state.selectedLocationId = this.locations[0]
+                ? String(this.locations[0].id)
+                : false;
+        }
+    }
+
+    /**
+     * Find the selected location based on its id.
+     *
+     * @return {Object} The selected location.
+     */
+    get selectedLocation() {
+        return this.state.locations.find(l => String(l.id) === this.state.selectedLocationId);
+    }
+
+    /**
+     * Set the selectedLocationId in the state.
+     *
+     * @param {String} locationId
+     * @return {void}
+     */
+    setSelectedLocation(locationId) {
+        this.state.selectedLocationId = String(locationId);
+    }
+
+    /**
+     * Confirm the current selected location.
+     *
+     * @return {void}
+     */
+    async validateSelection() {
+        if (!this.state.selectedLocationId) return;
+        const selectedLocation = this.state.locations.find(
+            l => String(l.id) === this.state.selectedLocationId
+        );
+        await this.props.save(selectedLocation);
+        this.props.close();
+    }
+
+    //--------------------------------------------------------------------------
+    // User Interface
+    //--------------------------------------------------------------------------
+
+    /**
+     * Determines the component to show in mobile view based on the current state.
+     *
+     * Returns the MapContainer component if `viewMode` is strictly equal to `map`, else return the
+     * List component.
+     *
+     * @return {Component} The component to show in mobile view.
+     */
+    get mobileComponent() {
+        if (this.state.viewMode === 'map') return MapContainer;
+        return LocationList;
+    }
 
     get title() {
-        // The original definition of this getter is in `delivery` module which is not a frontend module. This problem happens in the context of the website. So, it should be repeated here as translations are only fetched in the context of a frontend module, which is `website_sale` in this case.
         if (this.state.locations.length === 1) {
             return _t("Pickup Location")
         }
         return _t("Choose a pick-up point");
-    },
+    }
 
     get validationButtonLabel() {
-        // The original definition of this getter is in `delivery` module which is not a frontend module. This problem happens in the context of the website. So, it should be repeated here as translations are only fetched in the context of a frontend module, which is `website_sale` in this case.
         return _t("Choose this location");
-    },
+    }
 
     get postalCodePlaceholder() {
-        // The original definition of this getter is in `delivery` module which is not a frontend module. This problem happens in the context of the website. So, it should be repeated here as translations are only fetched in the context of a frontend module, which is `website_sale` in this case.
         return _t("Your postal code");
-    },
+    }
 
     get listViewButtonLabel() {
-        // The original definition of this getter is in `delivery` module which is not a frontend module. This problem happens in the context of the website. So, it should be repeated here as translations are only fetched in the context of a frontend module, which is `website_sale` in this case.
-        return _t("List");
-    },
+        return _t("List view");
+    }
 
     get mapViewButtonLabel() {
-        // The original definition of this getter is in `delivery` module which is not a frontend module. This problem happens in the context of the website. So, it should be repeated here as translations are only fetched in the context of a frontend module, which is `website_sale` in this case.
-        return _t("Map");
-    },
+        return _t("Map view");
+    }
 
     get errorMessage() {
-        // The original definition of this getter is in `delivery` module which is not a frontend module. This problem happens in the context of the website. So, it should be repeated here as translations are only fetched in the context of a frontend module, which is `website_sale` in this case.
         return _t("No result");
-    },
+    }
 
     get loadingMessage() {
-        // The original definition of this getter is in `delivery` module which is not a frontend module. This problem happens in the context of the website. So, it should be repeated here as translations are only fetched in the context of a frontend module, which is `website_sale` in this case.
         return _t("Loading...");
-    },
-});
+    }
+
+    /**
+     *
+     * @return {void}
+     */
+    updateSize() {
+        this.state.isSmall = this.env.isSmall;
+    }
+}