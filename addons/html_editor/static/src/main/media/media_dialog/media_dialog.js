c4997ff0962d12b449c58ac79356b1121a2578ed
dtda-odoo <dtda@odoo.com>
2026-02-20 06:21:28 +0000

    [FIX] html_editor: fix image upload error handling
    Steps to Reproduce:
    1. Open the website module.
    2. Open the media upload dialog to upload an image by either
    double-clicking the logo or replacing the existing image.
    3. Upload a large file.
    4. Abort the upload before it finishes by clicking the 'Discard'
    button in the media dialog box.
    After performing these steps, a traceback is observed.
    
    Before this commit:
    - Image upload failures would throw uncaught exceptions.
    - These exceptions would interrupt the flow and result in a poor user
    experience with no clear feedback.
    - Even after clicking the discard button the image was still
    getting uploaded.
    
    After this commit:
    - Uploads can be safely aborted when the media dialog is discarded.
    - Ongoing XHR requests and RPC calls are properly cancelled.
    - The upload loop stops immediately when an abort is triggered with
    no traceback.
    - Users get a predictable and clean exit instead of a broken state.
    - Files are no longer uploaded after clicking Discard.
    
    task-4752497
    
    X-original-commit: fc66f7031c81613cd7f09b496494338d10cff172

--- addons/html_editor/static/src/main/media/media_dialog/media_dialog.js
+++ addons/html_editor/static/src/main/media/media_dialog/media_dialog.js
@@ -81,6 +81,7 @@ export class MediaDialog extends Component {
             },
             () => [this.selectedMedia[this.state.activeTab].length, this.state.isSaving]
         );
+         this.abortUploads = null;
     }
 
     get initialActiveTab() {
@@ -117,6 +118,7 @@ export class MediaDialog extends Component {
                 selectMedia: (...args) =>
                     this.selectMedia(...args, tab.id, additionalProps.multiSelect),
                 save: this.save.bind(this),
+                setAbortUploadsCallback: (abortFunc) => this.abortUploads = abortFunc,
                 onAttachmentChange: this.props.onAttachmentChange,
                 errorMessages: (errorMessage) => (this.errorMessages[tab.id] = errorMessage),
                 modalRef: this.modalRef,
@@ -324,5 +326,13 @@ export class MediaDialog extends Component {
 
     onTabChange(tab) {
         this.state.activeTab = tab;
+    }
+    async close() {
+        if (this.abortUploads) {
+            this.abortUploads();
+            delete this.abortUploads;
+        }
+        this.state.isSaving = false;
+        await this.props.close();
     }
 }
