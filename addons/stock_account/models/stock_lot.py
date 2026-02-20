5204bb9af9508ba36b45cdbbf1ee5dddaa898998
Arnold Moyaux <arm@odoo.com>
2026-02-20 09:51:27 +0100

    [FIX] stock_account: remove store from avg cost on lot
    The field is computed stored. However it's impacted by the context and
    should be recomputed each time we switch the company or add a date in
    the context and so on. Also it's not used in any search nor filter.
    
    It was made by mistake and it's now recomputed at the same time than the
    total value of the lot. We know that removing a store in stable is not
    recommended. However in practice, it correctly retrieve the value from
    the compute and not the database. The column still exists and should be
    removed during an upgrade script.
    
    Part-of: odoo/odoo#249624
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>
    (cherry picked from commit 6640b9cf35a0da77e7ca9623bc02feb31bb3d4d0)
    
    Part-of: odoo/odoo#260715
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/stock_account/models/stock_lot.py
+++ addons/stock_account/models/stock_lot.py
@@ -7,7 +7,7 @@ class StockLot(models.Model):
     _inherit = 'stock.lot'
 
     lot_valuated = fields.Boolean(related='product_id.lot_valuated', readonly=True, store=False)
-    avg_cost = fields.Monetary(string="Average Cost", compute='_compute_value', compute_sudo=True, store=False, readonly=True, currency_field='company_currency_id')
+    avg_cost = fields.Monetary(string="Average Cost", compute='_compute_value', compute_sudo=True, readonly=True, currency_field='company_currency_id')
     total_value = fields.Monetary(string="Total Value", compute='_compute_value', compute_sudo=True, currency_field='company_currency_id')
     company_currency_id = fields.Many2one('res.currency', 'Valuation Currency', compute='_compute_value', compute_sudo=True)
     standard_price = fields.Float(
@@ -25,7 +25,6 @@ class StockLot(models.Model):
         company_id = self.env.company
         self.company_currency_id = company_id.currency_id
         at_date = fields.Datetime.to_datetime(self.env.context.get('to_date'))
-
         for lot in self:
             if not lot.lot_valuated:
                 lot.total_value = 0.0
@@ -39,15 +38,11 @@ class StockLot(models.Model):
                 lot.avg_cost = 0.0
             elif valuated_product.cost_method == 'standard' or valuated_product.uom_id.is_zero(qty_available):
                 lot.total_value = lot.standard_price * qty_valued
-                lot.avg_cost = lot.standard_price
             elif valuated_product.cost_method == 'average':
-                avco_result = valuated_product.with_context(warehouse_id=False)._run_avco(at_date=at_date, lot=lot.with_context(warehouse_id=False))
-                lot.total_value = avco_result[1] * qty_valued / qty_available
-                lot.avg_cost = avco_result[0]
+                lot.total_value = valuated_product.with_context(warehouse_id=False)._run_avco(at_date=at_date, lot=lot)[1] * qty_valued / qty_available
             else:
-                fifo_value = valuated_product.with_context(warehouse_id=False)._run_fifo(qty_available, at_date=at_date, lot=lot.with_context(warehouse_id=False))
-                lot.total_value = fifo_value * qty_valued / qty_available
-                lot.avg_cost = fifo_value / qty_available if qty_available else 0.0
+                lot.total_value = valuated_product.with_context(warehouse_id=False)._run_fifo(qty_available, at_date=at_date, lot=lot) * qty_valued / qty_available
+            lot.avg_cost = lot.total_value / qty_valued if qty_valued else 0.0
 
     # TODO: remove avg cost column in master and merge the two compute methods
     def _compute_avg_cost(self):
