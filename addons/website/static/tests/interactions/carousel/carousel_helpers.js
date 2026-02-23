cc8dbcbd8aba306f733c47e58a58606e3ad866a9
Robin Lejeune (role) <role@odoo.com>
2026-02-23 10:26:02 +0100

    [FIX] website: move carousel buttons 1st in DOM order
    In order for the main actions to be focused first when browsing a page
    with the keyboard (specifically Tab / Shift+Tab), carousel controller
    buttons are moved at the beginning of all carousel templates.
    
    That way, a user is first given the opportunity to slide/choose a slide,
    and only then to focus on buttons inside the content of the active
    slide.
    
    task-5470023

--- addons/website/static/tests/interactions/carousel/carousel_helpers.js
+++ addons/website/static/tests/interactions/carousel/carousel_helpers.js
@@ -1,17 +1,6 @@
-export const defaultCarouselStyleSnippet = (bsRide, bsInterval) => `
+export const defaultCarouselStyleSnippet = (bsRide, bsInterval) => /*xml*/ `
     <section class="s_carousel_wrapper p-0" data-snippet="s_carousel" data-vcss="001">
         <div id="slideshow_sample" class="s_carousel s_carousel_default carousel slide o_colored_level" data-bs-ride="${bsRide}" data-bs-interval="${bsInterval}">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img class="img img-fluid d-block mh-100 mw-100 mx-auto rounded object-fit-cover" src="/web/image/website.library_image_08" data-name="Image" data-index="0" alt=""/>
-                </div>
-                <div class="carousel-item">
-                    <img class="img img-fluid d-block mh-100 mw-100 mx-auto rounded object-fit-cover" src="/web/image/website.library_image_03" data-name="Image" data-index="1" alt=""/>
-                </div>
-                <div class="carousel-item">
-                    <img class="img img-fluid d-block mh-100 mw-100 mx-auto rounded object-fit-cover" src="/web/image/website.library_image_02" data-name="Image" data-index="2" alt=""/>
-                </div>
-            </div>
             <div class="o_carousel_controllers">
                 <button class="carousel-control-prev o_not_editable" contenteditable="false" data-bs-target="#slideshow_sample" data-bs-slide="prev" aria-label="Previous" title="Previous">
                     <span class="carousel-control-prev-icon" aria-hidden="true"/>
@@ -36,24 +25,24 @@ export const defaultCarouselStyleSnippet = (bsRide, bsInterval) => `
                     <span class="visually-hidden">Next</span>
                 </button>
             </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="img img-fluid d-block mh-100 mw-100 mx-auto rounded object-fit-cover" src="/web/image/website.library_image_08" data-name="Image" data-index="0" alt=""/>
+                </div>
+                <div class="carousel-item">
+                    <img class="img img-fluid d-block mh-100 mw-100 mx-auto rounded object-fit-cover" src="/web/image/website.library_image_03" data-name="Image" data-index="1" alt=""/>
+                </div>
+                <div class="carousel-item">
+                    <img class="img img-fluid d-block mh-100 mw-100 mx-auto rounded object-fit-cover" src="/web/image/website.library_image_02" data-name="Image" data-index="2" alt=""/>
+                </div>
+            </div>
         </div>
     </section>`;
 
-export const imageGalleryCarouselStyleSnippet = (bsRide, bsInterval) => `
+export const imageGalleryCarouselStyleSnippet = (bsRide, bsInterval) => /*xml*/ `
     <section class="s_image_gallery o_slideshow pt24 pb24 s_image_gallery_controllers_outside s_image_gallery_controllers_outside_arrows_right s_image_gallery_indicators_dots s_image_gallery_arrows_default" data-snippet="s_image_gallery" data-vcss="002" data-columns="3">
         <div class="o_container_small overflow-hidden">
             <div id="slideshow_sample" class="carousel carousel-dark slide" data-bs-ride="${bsRide}" data-bs-interval="${bsInterval}">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img class="img img-fluid d-block mh-100 mw-100 mx-auto rounded object-fit-cover" src="/web/image/website.library_image_08" data-name="Image" data-index="0" alt=""/>
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img img-fluid d-block mh-100 mw-100 mx-auto rounded object-fit-cover" src="/web/image/website.library_image_03" data-name="Image" data-index="1" alt=""/>
-                    </div>
-                    <div class="carousel-item">
-                        <img class="img img-fluid d-block mh-100 mw-100 mx-auto rounded object-fit-cover" src="/web/image/website.library_image_02" data-name="Image" data-index="2" alt=""/>
-                    </div>
-                </div>
                 <div class="o_carousel_controllers">
                     <button class="carousel-control-prev o_not_editable" contenteditable="false" data-bs-target="#slideshow_sample" data-bs-slide="prev" aria-label="Previous" title="Previous">
                         <span class="carousel-control-prev-icon" aria-hidden="true"/>
@@ -77,6 +66,17 @@ export const imageGalleryCarouselStyleSnippet = (bsRide, bsInterval) => `
                         <span class="carousel-control-next-icon" aria-hidden="true"/>
                         <span class="visually-hidden">Next</span>
                     </button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="img img-fluid d-block mh-100 mw-100 mx-auto rounded object-fit-cover" src="/web/image/website.library_image_08" data-name="Image" data-index="0" alt=""/>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="img img-fluid d-block mh-100 mw-100 mx-auto rounded object-fit-cover" src="/web/image/website.library_image_03" data-name="Image" data-index="1" alt=""/>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="img img-fluid d-block mh-100 mw-100 mx-auto rounded object-fit-cover" src="/web/image/website.library_image_02" data-name="Image" data-index="2" alt=""/>
+                    </div>
                 </div>
             </div>
         </div>
