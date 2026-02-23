6623074cb5077008e0141dc9603854afbaa2cdae
livaios <liew@odoo.com>
2026-02-23 14:28:36 +0100

    [IMP] sale_margin: make margin editable
    Make Margin & Margin % editable on SOL.
    
    Impact it on Unit Price as if you were editing it manually.
    
    Details:
    1. Separated compute_margin from compute_margin_percent to avoid unnecessary double computations.
    2. Factorized the root of the original compute function to avoid duplicating code.
    3. For tax included prices, a rounding was needed because tax computation resulted in weird results like 100.0004 when the user typed in 100. 
    4. Read only margin percent when cost is 0
    
    task-5956031
    
    closes odoo/odoo#250109
    
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/sale_margin/tests/test_sale_margin.py
+++ addons/sale_margin/tests/test_sale_margin.py
@@ -1,13 +1,69 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+
 from odoo.fields import Command
-from odoo.tests import tagged
+from odoo.tests import Form, tagged
 
 from odoo.addons.sale.tests.common import SaleCommon
+
+DISCOUNT = 50
+NO_TAX_INCL_VALUES = [
+    {"margin": 16.67, "margin_percent": 0.25, "price_unit": 66.67},
+    {"margin": 25.0, "margin_percent": 1 / 3, "price_unit": 75},
+    {"margin": 50.0, "margin_percent": 0.5, "price_unit": 100},
+    {"margin": 75.0, "margin_percent": 0.6, "price_unit": 125},
+    {"margin": 150.0, "margin_percent": 0.75, "price_unit": 200},
+]
+NO_TAX_INCL_DISCOUNT_VALUES = [
+    {**values, "price_unit": values["price_unit"] / (1 - DISCOUNT / 100)}
+    for values in NO_TAX_INCL_VALUES
+]
+
+TAX_INCL_VALUES = [
+    {"margin": -16.67, "margin_percent": -0.50, "price_unit": 50, "purchase_price": 50},
+    {"margin": 16.67, "margin_percent": 0.25, "price_unit": 100, "purchase_price": 50},
+    {"margin": 33.33, "margin_percent": 0.4, "price_unit": 125, "purchase_price": 50},
+    {"margin": 50.0, "margin_percent": 0.5, "price_unit": 150, "purchase_price": 50},
+    {"margin": 83.33, "margin_percent": 0.625, "price_unit": 200, "purchase_price": 50},
+    {"margin": 283.33, "margin_percent": 0.85, "price_unit": 500, "purchase_price": 50},
+]
+TAX_INCL_DISCOUNT_VALUES = [
+    {**values, "price_unit": values["price_unit"] / (1 - DISCOUNT / 100)}
+    for values in TAX_INCL_VALUES
+]
 
 
 @tagged("at_install", "-post_install")  # LEGACY at_install
 class TestSaleMargin(SaleCommon):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls._enable_discounts()
+
+        cls.product_50_margin = cls._create_product(
+            list_price=100.0, standard_price=50.0, taxes_id=[Command.set([])]
+        )
+        tax_group = cls.env["account.tax.group"].create({"name": "Tax Group A"})
+        cls.tax_included, cls.tax_excluded = cls.env["account.tax"].create([
+            {
+                "name": "Tax with price include",
+                "amount": 50,
+                "price_include_override": "tax_included",
+                "tax_group_id": tax_group.id,
+            },
+            {
+                "name": "Tax with price exclude",
+                "amount": 50,
+                "price_include_override": "tax_excluded",
+                "tax_group_id": tax_group.id,
+            },
+        ])
+
+        cls.so = cls._create_so(
+            order_line=[Command.create({"product_id": cls.product_50_margin.id})]
+        )
+        cls.sol = cls.so.order_line
+
     def test_sale_margin(self):
         """Test the sale_margin module in Odoo."""
         self.product.standard_price = 700.0
@@ -148,3 +204,103 @@ class TestSaleMargin(SaleCommon):
         self.assertAlmostEqual(750.0, following_sale.order_line.purchase_price)
         self.assertAlmostEqual(2500.0, following_sale.order_line.margin)
         self.assertAlmostEqual(0.25, following_sale.order_line.margin_percent)
+
+    def test_margin_onchanges_no_tax(self):
+        self.assertRecordValues(
+            self.sol,
+            [
+                {
+                    "price_unit": 100.0,
+                    "purchase_price": 50,
+                    "margin": 50,
+                    "margin_percent": 0.5,
+                    "tax_ids": [],
+                }
+            ],
+        )
+        self._test_margin_onchange("margin", NO_TAX_INCL_VALUES)
+        self._test_margin_onchange("margin_percent", NO_TAX_INCL_VALUES)
+
+        self.sol.discount = DISCOUNT
+        self._test_margin_onchange("margin", NO_TAX_INCL_DISCOUNT_VALUES)
+        self._test_margin_onchange("margin_percent", NO_TAX_INCL_DISCOUNT_VALUES)
+
+    def test_margin_onchanges_tax_excl(self):
+        self.product_50_margin.taxes_id = [Command.link(self.tax_excluded.id)]
+        self.so._recompute_taxes()
+        self.assertRecordValues(
+            self.sol,
+            [
+                {
+                    "price_unit": 100.0,
+                    "purchase_price": 50.0,
+                    "margin": 50,
+                    "margin_percent": 0.5,
+                    "tax_ids": [self.tax_excluded.id],
+                }
+            ],
+        )
+        # Price excluded taxes should not have any impact on the margin computation
+        self._test_margin_onchange("margin", NO_TAX_INCL_VALUES)
+        self._test_margin_onchange("margin_percent", NO_TAX_INCL_VALUES)
+
+        self.sol.discount = DISCOUNT
+        self._test_margin_onchange("margin", NO_TAX_INCL_DISCOUNT_VALUES)
+        self._test_margin_onchange("margin_percent", NO_TAX_INCL_DISCOUNT_VALUES)
+
+    def test_margin_onchanges_tax_incl(self):
+        self.product_50_margin.taxes_id = [Command.link(self.tax_included.id)]
+        self.so._recompute_taxes()
+        self.assertRecordValues(
+            self.sol,
+            [
+                {
+                    "price_unit": 100.0,
+                    "purchase_price": 50.0,
+                    "margin": 16.67,
+                    "margin_percent": 0.25,
+                    "tax_ids": [self.tax_included.id],
+                    "price_tax": 33.33,
+                }
+            ],
+        )
+        self._test_margin_onchange("margin", TAX_INCL_VALUES)
+        self._test_margin_onchange("margin_percent", TAX_INCL_VALUES)
+        self.sol.discount = 50
+        self._test_margin_onchange("margin", TAX_INCL_DISCOUNT_VALUES)
+        self._test_margin_onchange("margin_percent", TAX_INCL_DISCOUNT_VALUES)
+
+    def test_margin_onchanges_tax_incl_excl(self):
+        self.product_50_margin.taxes_id = [
+            Command.link(self.tax_excluded.id),
+            Command.link(self.tax_included.id),
+        ]
+        self.so._recompute_taxes()
+        self.assertRecordValues(
+            self.sol,
+            [
+                {
+                    "price_unit": 100.0,
+                    "purchase_price": 50.0,
+                    "margin": 16.67,
+                    "margin_percent": 0.25,
+                    "tax_ids": [self.tax_excluded.id, self.tax_included.id],
+                }
+            ],
+        )
+        # Price excluded taxes should not have any impact on the margin computation
+        self._test_margin_onchange("margin", TAX_INCL_VALUES[1:])
+        self._test_margin_onchange("margin_percent", TAX_INCL_VALUES[1:])
+
+    def _test_margin_onchange(self, fname, vals_list):
+        with Form(self.so) as so_form, so_form.order_line.edit(0) as sol_form:
+            for values in vals_list:
+                sol_form[fname] = values[fname]
+                for k, v in values.items():
+                    delta = 0.02 if k != "margin_percent" else 0.01
+                    self.assertAlmostEqual(
+                        sol_form[k],
+                        v,
+                        msg=f"{k} doesn't match ({fname}: {values[fname]})",
+                        delta=delta,
+                    )
