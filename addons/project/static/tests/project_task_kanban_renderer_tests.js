60a8973508b6077c82f5200c1c11d77107fc1533
Aurélien Bertrand <auber@odoo.com>
2026-02-23 14:32:13 +0100

    [FIX] project: incorrect view change after deleting task stage
    # Steps to reproduce
    1. Create a project
    2. Create a stage
    4. Remove the stage
    
    # Current behavior
    Instead of remaining in the project tasks view, it switches to the tasks view
    filtered with the current project. Additionally, it displays archived tasks
    because no filter is selected, thereby discarding original ones.
    
    This also applies to stage deletion in other views (e.g., My Tasks), where the
    search filters are completely discarded.
    
    # Expected behavior
    The dialog should be closed, the stage should be deleted, and the original view
    should remain active. This is done through a soft-reload of the page, ensuring
    the original view is kept, together with original breadcrumbs, and the stage is
    visually disappearing.
    
    task-5498274

--- addons/project/static/tests/project_task_kanban_renderer_tests.js
+++ addons/project/static/tests/project_task_kanban_renderer_tests.js
@@ -6,11 +6,12 @@ import { start } from "@mail/../tests/helpers/test_utils";
 
 import { registry } from "@web/core/registry";
 import { makeFakeUserService } from "@web/../tests/helpers/mock_services";
-import { getFixture, nextTick } from "@web/../tests/helpers/utils";
+import { click, editInput, getFixture, nextTick } from "@web/../tests/helpers/utils";
 import { setupViewRegistries } from "@web/../tests/views/helpers";
 
 let target;
 let serverData;
+let mockRPC;
 
 const serviceRegistry = registry.category("services");
 
@@ -40,6 +41,15 @@ QUnit.module("project", (hooks) => {
             },
             records: [],
         };
+        pyEnv.mockServer.models["project.task.type.delete.wizard"] = {
+            fields: {
+                tasks_count: { string: "Number of Tasks", type: "integer" },
+                stages_active: { string: "Stages Active", type: "boolean" },
+                project_ids: { string: "Projects", type: "many2many", relation: "project.project" },
+                stage_ids: { string: "Stages To Delete", type: "many2many", relation: "project.task.type" },
+            },
+            records: [],
+        };
         serverData = {
             views: {
                 "project.task,false,kanban": `
@@ -54,17 +64,46 @@ QUnit.module("project", (hooks) => {
                         </templates>
                     </kanban>
                 `,
+                "project.task.type.delete.wizard,false,form": `
+                    <form string="Delete Stage">
+                        <footer>
+                            <button string="Delete" type="object" name="action_unlink" class="btn btn-primary"/>
+                            <button string="Discard" special="cancel"/>
+                        </footer>
+                    </form>
+                `,
             },
+        };
+        mockRPC = async (route, args) => {
+            if (args.method === "unlink_wizard") {
+                return {
+                    type: "ir.actions.act_window",
+                    name: "Delete Stage",
+                    res_model: "project.task.type.delete.wizard",
+                    view_mode: "form",
+                    views: [[false, "form"]],
+                    target: "new",
+                    res_id: 1,
+                };
+            }
+            if (args.method === "action_unlink") {
+                return {
+                    type: "ir.actions.act_window_close",
+                    infos: {
+                        success: true,
+                    },
+                }
+            }
         };
         target = getFixture();
         setupViewRegistries();
-    });
-    QUnit.test("delete a column in grouped on m2o", async function (assert) {
         serviceRegistry.add(
             "user",
             makeFakeUserService((group) => group === "project.group_project_manager"),
             { force: true }
         );
+    });
+    QUnit.test("delete a column in grouped on m2o", async function (assert) {
         const { openView } = await start({ serverData });
 
         await openView({
@@ -86,5 +125,62 @@ QUnit.module("project", (hooks) => {
             1,
             "ghost column visible"
         );
+    });
+    QUnit.test("delete just created stage", async function (assert) {
+        const { openView } = await start({ serverData, mockRPC });
+
+        await openView({
+            res_model: "project.task",
+            views: [[false, "kanban"]],
+            context: {
+                active_model: "project.project",
+                default_project_id: 1,
+            },
+        });
+
+        await editInput(target.querySelector(".o_column_quick_create input"), null, "Stage 1");
+        await click(target, ".o_kanban_add");
+
+        await click(target, ".o_kanban_group:first-child .o_kanban_config.dropdown .dropdown-toggle");
+        await click(target, ".dropdown-item.o_column_delete");
+        await click(target, "button[special=cancel]");
+        assert.containsOnce(target, ".o_column_title", "The stage should not have been deleted.");
+
+        await click(target, ".o_kanban_group:first-child .o_kanban_config.dropdown .dropdown-toggle");
+        await click(target, ".dropdown-item.o_column_delete");
+        await click(target, "button[name=action_unlink]");
+        assert.containsNone(target, ".o_column_title", "The stage should have been deleted.");
+    });
+    QUnit.test("delete existing stage", async function (assert) {
+        const originalMockRPC = mockRPC;
+        mockRPC = async (route, args) => {
+            if (args.method === "web_read_group") {
+                return {
+                    groups: [{ stage_id: [1, "Stage 1"] }],
+                    length: 1,
+                };
+            }
+            return originalMockRPC(route, args);
+        };
+        const { openView } = await start({ serverData, mockRPC });
+
+        await openView({
+            res_model: "project.task",
+            views: [[false, "kanban"]],
+            context: {
+                active_model: "project.project",
+                default_project_id: 1,
+            },
+        });
+
+        await click(target, ".o_kanban_group:first-child .o_kanban_config.dropdown .dropdown-toggle");
+        await click(target, ".dropdown-item.o_column_delete");
+        await click(target, "button[special=cancel]");
+        assert.containsOnce(target, ".o_column_title", "The stage should not have been deleted.");
+
+        await click(target, ".o_kanban_group:first-child .o_kanban_config.dropdown .dropdown-toggle");
+        await click(target, ".dropdown-item.o_column_delete");
+        await click(target, "button[name=action_unlink]");
+        assert.containsNone(target, ".o_column_title", "The stage should have been deleted.");
     });
 });
