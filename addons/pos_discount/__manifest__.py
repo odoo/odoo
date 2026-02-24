14967d21a210d3eb665f78e688d2947d0ce422ed
Léopold Cantraine <lecan@odoo.com>
2026-02-24 15:55:04 +0100

    [IMP] point_of_sale: Global discount exclude from gift receipt
    Global discount line applied to an order was visible in the basic receipt. We souldn't see these lines.
    
    task-id: 5944518

--- addons/pos_discount/__manifest__.py
+++ addons/pos_discount/__manifest__.py
@@ -18,6 +18,7 @@ discount to a customer.
         'views/res_config_settings_views.xml',
         'views/pos_config_views.xml',
         'views/pos_session_sales_details.xml',
+        'receipt/pos_order_receipt.xml',
         ],
     'assets': {
         'point_of_sale._assets_pos': [
