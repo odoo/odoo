ffc0b4bb8b1e8fb5c2030451991549cdbfa6b9b4
Léo Gizard <legi@odoo.com>
2026-02-24 13:51:07 +0000

    [FIX] account_edi_ubl_cii: Changing tax exemption reason when no tax
    **PROBLEM**
    Here is the client use case:
    The client uses fixed tax to do 'vidange/consignage'.
    When selling a product with a 'consigne/vidange', they add a fixed tax which amount correspond to the 'consigne/vidange'.
    When returning the 'consigne', you would create an invoice line, with a product with a price of 0, negative quantity,a 0% tax and the fixed tax for the 'consigne'.
    When an invoice contains such lines, it fails peppol validations.
    
    **STEP TO REPRODUCE**
    1. Create a fixed tax used for 'vidange/consigne'.
    2. Create an invoice, with a line with unit price of 0, negative quantity, a 0% tax and the fixed tax for the 'consigne'.
    3. Send the invoice using peppol, use a validator to validate the xml and notice you get the following errors: [BR-E-01] [BR-E-08].
    
    **CAUSE**
    Fixed tax (like the one used for vidange) are aggregated in new invoice lines by the function `_ubl_turn_emptying_taxes_as_new_base_lines()`.
    Let say we have the following invoice:
    line 1: qty=2, unit_price=3, taxes: 21% & vidange(fixed tax of 1).
    line 2: qty=-1, unit_price=0, taxes: 0% & vidange.
    
    After calling `_ubl_turn_emptying_taxes_as_new_base_lines()`, we got:
    line 1: qty=2, unit_price=3, taxes: 21%.
    line 2: qty=-1, unit_price=0, taxes: 0%.
    line 3: qty=1, unit_price=1(amount of the fixed tax), taxes:None.
    
    When generating the VAT breakdown, we have 2 line will end up being tax exempted (line 2 and 3).
    Because the `tax_exemption_reason` differs, they will not be merged in the same entry in the breakdown, which break the constraint of peppol saying we can only have one VAT breakdown with code 'Exempt from tax'.
    
    opw-5912986
    
    X-original-commit: a41df53aefc63db937786206ca281c6ab50b04f2

--- addons/account_edi_ubl_cii/models/account_edi_common.py
+++ addons/account_edi_ubl_cii/models/account_edi_common.py
@@ -271,7 +271,7 @@ class AccountEdiCommon(models.AbstractModel):
         if supplier.country_id == customer.country_id:
             if not tax or tax.amount == 0:
                 # in theory, you should indicate the precise law article
-                return create_dict(tax_category_code='E', tax_exemption_reason=_('Articles 226 items 11 to 15 Directive 2006/112/EN'))
+                return create_dict(tax_category_code='E')
             elif tax.has_negative_factor:
                 # Special case: Purchase reverse-charge taxes for self-billed invoices.
                 # From the buyer's perspective, this is a standard tax with a non-zero percentage but
@@ -306,7 +306,7 @@ class AccountEdiCommon(models.AbstractModel):
         if tax.amount != 0:
             return create_dict(tax_category_code='S')
         else:
-            return create_dict(tax_category_code='E', tax_exemption_reason=_('Articles 226 items 11 to 15 Directive 2006/112/EN'))
+            return create_dict(tax_category_code='E')
 
     def _get_tax_category_code(self, customer, supplier, tax):
         if not tax:
