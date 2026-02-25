d2a5abe560c164ce477e97d1dd0537b6014c9068
Xavier Morel <xmo@odoo.com>
2026-02-25 14:59:43 +0100

    [IMP] google_calendar: move data setup to setUpClass
    Having lots of tests which do the exact same setup is
    expensive (especially when they keep generating expensive-to-create
    objects e.g. users, partners, companies).
    
    This can be done just once in setUpClass and at least shared between
    the tests of the same leaf test class.
    
    Tried to do the same for microsoft_calendar which is also quite costly
    but it does weird shit all over and that made a bunch of tests fail.
    
    (cherry picked from commit 2289afacb407d002f659e14b0bc40472166c0dec)
    
    Part-of: odoo/odoo#250887
    Related: odoo/enterprise#108872
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- addons/google_calendar/tests/test_sync_google2odoo.py
+++ addons/google_calendar/tests/test_sync_google2odoo.py
@@ -16,23 +16,23 @@ from unittest.mock import patch
 
 @patch.object(User, '_get_google_calendar_token', lambda user: 'dummy-token')
 class TestSyncGoogle2Odoo(TestSyncGoogle):
-
-    def setUp(self):
-        super().setUp()
-        self.other_company = self.env['res.company'].create({'name': 'Other Company'})
-        self.public_partner = self.env['res.partner'].create({
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.other_company = cls.env['res.company'].create({'name': 'Other Company'})
+        cls.public_partner = cls.env['res.partner'].create({
             'name': 'Public Contact',
             'email': 'public_email@example.com',
             'type': 'contact',
         })
-        self.env.ref('base.partner_admin').write({
+        cls.env.ref('base.partner_admin').write({
             'name': 'Mitchell Admin',
             'email': 'admin@yourcompany.example.com',
         })
-        self.private_partner = self.env['res.partner'].create({
+        cls.private_partner = cls.env['res.partner'].create({
             'name': 'Private Contact',
             'email': 'private_email@example.com',
-            'company_id': self.other_company.id,
+            'company_id': cls.other_company.id,
         })
 
     def generate_recurring_event(self, mock_dt, **values):
