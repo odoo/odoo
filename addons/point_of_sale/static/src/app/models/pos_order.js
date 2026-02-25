bcc142e32ba2b7e7eb4a86ede411c69682ef31c3
paan-odoo <paan@odoo.com>
2026-02-25 06:58:44 +0000

    [IMP] point_of_sale: allow multiple cash payment lines
    Before this commit:
    
    - The user is not able to process multiple cash payment lines. An error pop-up
      appears saying `There is already a cash payment line.`
    
    After this commit:
    
    - The user can process multiple cash payment lines.
    
    Use Case:
    
    - If a group of people goes to a restaurant and one person leaves earlier, he
      decides to pay $10 at the cashier and leave. When the others pay later, the
      cashier will see that $10 has already been paid and can add another cash
      payment line for the remaining amount.
    
    Task-5969853
    
    X-original-commit: 99ae6d45c0dd19bd6af7dc4cea69366ecb41ef40

--- addons/point_of_sale/static/src/app/models/pos_order.js
+++ addons/point_of_sale/static/src/app/models/pos_order.js
@@ -475,16 +475,11 @@ export class PosOrder extends PosOrderAccounting {
     /* ---- Payment Lines --- */
     addPaymentline(payment_method) {
         this.assertEditable();
-        const existingCash = this.payment_ids.find((pl) => pl.payment_method_id.is_cash_count);
 
         const { status: canSend, message } = payment_method.getPaymentInterfaceStates();
         if (!canSend) {
             return { status: false, data: message };
         }
-        if (existingCash && payment_method.is_cash_count) {
-            return { status: false, data: _t("There is already a cash payment line.") };
-        }
-
         const totalAmountDue = this.getDefaultAmountDueToPayIn(payment_method);
         const newPaymentLine = this.models["pos.payment"].create({
             pos_order_id: this,
