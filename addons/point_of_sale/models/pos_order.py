5db3da01b2317e8310a8a536a446f47f8a1e3d0b
Léopold Cantraine <lecan@odoo.com>
2026-02-25 09:07:13 +0000

    [FIX] point_of_sale: Warning when cancelling a paid order
    From 18.2, the warning to prevent users to cancel paid or posted orders from backend was no longer displayed.
    
    It's caused because it was handled in the write() method from pos.order model. But, since a recent change, we call write() method only on draft orders. So, if there is no draft orders selected, the warning doesn't appear.
    
    closes odoo/odoo#250836
    
    Task: 5959917
    X-original-commit: 5a38963c6261a6efef64904a71ca3d60d40fd06e
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Léopold Cantraine (lecan) <lecan@odoo.com>

--- addons/point_of_sale/models/pos_order.py
+++ addons/point_of_sale/models/pos_order.py
@@ -1178,13 +1178,15 @@ class PosOrder(models.Model):
         (payment_receivable_lines | invoice_receivable_lines).sudo().with_company(invoice.company_id).reconcile()
 
     def action_pos_order_cancel(self):
+        draft_orders = self.filtered(lambda o: o.state == 'draft')
         if self.env.context.get('active_ids'):
             orders = self.browse(self.env.context.get('active_ids'))
             order_is_in_futur = any(order.preset_time and order.preset_time.date() > fields.Date.today() for order in orders)
             if order_is_in_futur:
                 raise UserError(_('The order delivery / pickup date is in the future. You cannot cancel it.'))
+            if not draft_orders:
+                raise UserError(_('This order has already been paid. You cannot set it back to draft or edit it.'))
 
-        draft_orders = self.filtered(lambda o: o.state == 'draft')
         if draft_orders:
             draft_orders.write({'state': 'cancel'})
             for config in draft_orders.mapped('config_id'):
