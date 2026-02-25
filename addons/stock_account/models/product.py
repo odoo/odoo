4cb3879eda60996484258cd622c3b40b0b97676c
Kirra Hyde <kihy@odoo.com>
2026-02-24 16:47:38 -0800

    [PERF] stock_account: Prevent memory error when computing average cost
    For databases with millions of stock.move records, computing the
    average cost for all AVCO products in a company at once (e.g. when
    opening the inventory valuation report) can cause a memory error due
    to loading too many moves into memory.
    
    To prevent this, moves are now fetched in batches of 50,000 records,
    with the cache for stock.move and stock.move.line cleared between
    each batch.
    
    | # Input data                       | Before PR | After PR |
    |:----------------------------------:|:---------:|:--------:|
    | 100 Products with 61,024 moves     | 280 MB    | 272 MB   |
    | 500 Products with 535,250 moves    | 984 MB    | 282 MB   |
    | 500 Products with 912,405 moves    | 1.7 GB    | 281 MB   |
    | 1000 Products with 1,447,655 moves | Mem error | 293 MB   |
    
    opw-5416006
    
    Co-authored-by: Cooper Spinelli <spco@odoo.com>

--- addons/stock_account/models/product.py
+++ addons/stock_account/models/product.py
@@ -6,7 +6,7 @@ from datetime import date, datetime, time
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError
 from odoo.fields import Domain
-from odoo.tools import SQL
+from odoo.tools import SQL, split_every
 
 
 class ProductTemplate(models.Model):
@@ -389,6 +389,7 @@ class ProductProduct(models.Model):
         std_price_by_product_id = {}
         value_by_product_id = {}
         quantity_by_product_id = {}
+        date_by_product_id = {}
 
         if not at_date and not force_recompute:
             std_price_by_product_id = {p.id: p.standard_price for p in self}
@@ -409,22 +410,11 @@ class ProductProduct(models.Model):
                 ('date', '<=', at_date),
             ])
 
-        # PERF avoid memoryerror
-        move_fields = ['date', 'is_dropship', 'is_in', 'is_out', 'location_dest_id', 'location_id', 'move_line_ids',
-                       'picked', 'value', 'product_id']
         last_manual_value_by_product = self._get_last_product_value(at_date, lot=lot)
         oldest_manual_value = min(pv.date for pv in last_manual_value_by_product.values()) if last_manual_value_by_product else False
-
         if oldest_manual_value:
             moves_domain &= Domain([('date', '>=', oldest_manual_value)])
-        moves = self.env['stock.move'].search_fetch(
-            moves_domain,
-            field_names=move_fields,
-            order='date, id'
-        )
-        moves.move_line_ids.fetch(['company_id', 'location_id', 'location_dest_id', 'lot_id', 'owner_id', 'picked', 'quantity_product_uom'])
-        moves_by_product = moves.grouped(key=lambda m: m.product_id)
-        # Start from last user input values.
+
         for manual_value in last_manual_value_by_product.values():
             product = manual_value.product_id
             if lot:
@@ -435,47 +425,80 @@ class ProductProduct(models.Model):
             std_price_by_product_id[product.id] = manual_value.value
             quantity_by_product_id[product.id] = quantity
             value_by_product_id[product.id] = manual_value.value * quantity
+            date_by_product_id[product.id] = manual_value.date
 
-            moves = moves_by_product.get(product, self.env['stock.move'])
-            index = bisect(moves, manual_value.date, key=lambda m: m.date)
-            moves = moves[index:]
-            moves_by_product[product] = moves
+        self.env['product.value'].invalidate_model()  # Avoid keeping too many records in cache
 
-        # Replay the valuation history
-        for product, moves in moves_by_product.items():
-            # Start from last manual input
+        moves = self.env['stock.move'].search_fetch(
+            moves_domain,
+            field_names=['id'],
+            order='product_id, date, id'
+        )
+        # PERF avoid memoryerror
+        move_fields = ['date', 'is_dropship', 'is_in', 'is_out', 'location_dest_id', 'location_id', 'move_line_ids', 'picked', 'value', 'product_id']
+        move_line_fields = ['company_id', 'location_id', 'location_dest_id', 'lot_id', 'owner_id', 'picked', 'quantity_product_uom']
+
+        product, valuation_from_date = False, False
+        batch_size = 50000
+
+        move_ids_by_product = defaultdict(list)
+        # Limit the memory usage since it's possible to have millions of stock.move
+        for moves_batch in split_every(batch_size, moves.ids):
+            moves_batch = self.env['stock.move'].browse(moves_batch)
+            moves_batch.fetch(['product_id', 'date'])
+
+            for move in moves_batch:
+                if move.product_id != product:
+                    product = move.product_id
+                    valuation_from_date = date_by_product_id.get(product.id)
+                if valuation_from_date and move.date <= valuation_from_date:
+                    continue
+                move_ids_by_product[product].append(move.id)
+
+            self.env['stock.move'].invalidate_model()
+
+        for product, move_ids in move_ids_by_product.items():
             quantity = quantity_by_product_id.get(product.id, 0)
             average_cost = std_price_by_product_id.get(product.id, 0)
             value = value_by_product_id.get(product.id, 0)
-            for move in moves:
-                if move.is_in or move.is_dropship:
-                    in_qty = move._get_valued_qty()
-                    in_value = move.value
-                    if at_date or move.is_dropship:
-                        in_value = move._get_value(at_date=at_date)
-                    if lot:
-                        lot_qty = move._get_valued_qty(lot)
-                        in_value = (in_value * lot_qty / in_qty) if in_qty else 0
-                        in_qty = lot_qty
-                    previous_qty = quantity
-                    quantity += in_qty
-                    # Regular case, value from accumulation
-                    if previous_qty > 0:
-                        value += in_value
-                        average_cost = value / quantity
-                    # From negative quantity case, value from last_in
-                    elif previous_qty <= 0:
-                        average_cost = in_value / in_qty if in_qty else average_cost
-                        value = average_cost * quantity
-                if move.is_out or move.is_dropship:
-                    out_qty = move._get_valued_qty()
-                    out_value = out_qty * average_cost
-                    if lot:
-                        lot_qty = move._get_valued_qty(lot)
-                        out_value = (out_value * lot_qty / out_qty) if out_qty else 0
-                        out_qty = lot_qty
-                    value -= out_value
-                    quantity -= out_qty
+
+            product_moves = self.env['stock.move'].browse(move_ids)
+            for moves_batch in split_every(batch_size, product_moves.ids):
+                moves_batch = self.env['stock.move'].browse(moves_batch)
+                moves_batch.fetch(move_fields)
+                moves_batch.move_line_ids.fetch(move_line_fields)
+                for move in moves_batch:
+                    if move.is_in or move.is_dropship:
+                        in_qty = move._get_valued_qty()
+                        in_value = move.value
+                        if at_date or move.is_dropship:
+                            in_value = move._get_value(at_date=at_date)
+                        if lot:
+                            lot_qty = move._get_valued_qty(lot)
+                            in_value = (in_value * lot_qty / in_qty) if in_qty else 0
+                            in_qty = lot_qty
+                        previous_qty = quantity
+                        quantity += in_qty
+                        # Regular case, value from accumulation
+                        if previous_qty > 0:
+                            value += in_value
+                            average_cost = value / quantity
+                        # From negative quantity case, value from last_in
+                        elif previous_qty <= 0:
+                            average_cost = in_value / in_qty if in_qty else average_cost
+                            value = average_cost * quantity
+                    if move.is_out or move.is_dropship:
+                        out_qty = move._get_valued_qty()
+                        out_value = out_qty * average_cost
+                        if lot:
+                            lot_qty = move._get_valued_qty(lot)
+                            out_value = (out_value * lot_qty / out_qty) if out_qty else 0
+                            out_qty = lot_qty
+                        value -= out_value
+                        quantity -= out_qty
+
+                self.env['stock.move'].invalidate_model()  # Avoid keeping too many records in cache
+                self.env['stock.move.line'].invalidate_model()
 
             std_price_by_product_id[product.id] = average_cost
             value_by_product_id[product.id] = value
