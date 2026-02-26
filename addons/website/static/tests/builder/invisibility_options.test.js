5b0173e6db61d6dad69359d830d0c8e90e5e6ecb
Benjamin Vray <bvr@odoo.com>
2026-02-26 10:19:21 +0000

    [FIX] html_builder, website: preserve dropzone messages on save
    Steps to reproduce:
    - Open the website shop page in edit mode.
    - Drag and drop a block in the shop header dropzone.
    - Save the page and re-enter edit mode.
    - Check the shop header dropzone message.
    => The custom message is replaced by the default one.
    
    Before this commit, `SetupEditorPlugin.cleanForSave()` removed
    `data-editor-message` and `data-editor-message-default` on the saved
    HTML clone, including custom messages defined in website templates.
    
    This cleanup was originally introduced in `web_editor` in [1]. Since the
    builder refactor in [2], website page saves now use the shared
    `html_builder` save cleanup flow and wrongly inherited that behavior,
    which introduced this regression in website.
    
    This commit removes that cleanup from `html_builder`, as it does not
    provide useful value in this save flow and drops custom dropzone
    messages.
    
    After this commit, custom dropzone messages are preserved after saving
    website pages.
    
    [1]: bab673488e185ddd7792aedecc3870663290fed3
    [2]: 3ced5e9232d3c5f71259f1f2b692416932e3ebd2
    
    task-5921283
    
    closes odoo/odoo#251518
    
    X-original-commit: 20b2e3f79912095b8df5e732b8d98b4b97320b1b
    Signed-off-by: Soukéina Bojabza (sobo) <sobo@odoo.com>
    Signed-off-by: Benjamin Vray (bvr) <bvr@odoo.com>

--- addons/website/static/tests/builder/invisibility_options.test.js
+++ addons/website/static/tests/builder/invisibility_options.test.js
@@ -86,7 +86,7 @@ test("check invisible element after save", async () => {
     await contains(".o_we_invisible_el_panel .o_we_invisible_entry").click();
     await contains(".o-snippets-top-actions button:contains(Save)").click();
     expect(resultSave[0]).toBe(
-        `<div id="wrap" class="oe_structure oe_empty" data-oe-model="ir.ui.view" data-oe-id="539" data-oe-field="arch">
+        `<div id="wrap" class="oe_structure oe_empty" data-oe-model="ir.ui.view" data-oe-id="539" data-oe-field="arch" data-editor-message-default="true" data-editor-message="DRAG BUILDING BLOCKS HERE">
         <section class="o_colored_level">
             <div class="container">
                 <div class="row">
