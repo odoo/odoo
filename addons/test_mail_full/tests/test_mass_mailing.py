dzdzdzdz1010/OdooStandard

[IMP] mass_mailing, *: quicksend mailing to contacts also modified: test_mass_mailing,
Odoo users can currently select several of their contacts through
their contact list for a mail to multiple addresses; this opens a
minimal composer that can serve these needs.

While this serves as a fast way to contact multiple people, it doesn't
make use of the powerful mass_mailing builder and bundled options.

With this commit, users with mass_mailing installed will instead be
redirected to the mailing interface, with their selected contacts
immediately added as the target domain.

Additionally, the Send Email action is now available for any model
with the '_is_mailing_enabled' attribute set, such as employees or
mailing lists.

task-5423645

addons/test_mail_full/tests/test_mass_mailing.py
@@ -154,3 +154,25 @@ class TestMassMailing(TestMailFullCommon):
         # sent: 15, 2 bl, 3 opt-out, 3 invalid -> 7 remaining
         # ignored: 2 bl + 3 optout + 2 invalid + 1 duplicate; failed: 0
         self.assertMailingStatistics(mailing, expected=16, delivered=7, sent=7, canceled=9, failed=0)
+
+    # tests should be a little more fleshed out as the feature reaches maturity,
+    # for now these are mostly to underline what I think should be tested
+    # The client action should be tested as well
+    def test_send_mailing_action_presence(self):
+        """
+        Test that _mailing_enabled models have access to the Send Email toolbar action.
+        """
+        send_mail_action = self.env.ref('mass_mailing.action_toolbar_mass_mail')
+        contact_views = self.env['mailing.contact'].get_views([(False, 'form'), (False, 'list')], {'toolbar': True})['views']
+        portal_views = self.env['mail.test.portal'].get_views([(False, 'form'), (False, 'list')], {'toolbar': True})['views']
+
+        contact_list_actions = contact_views['list']['toolbar'].get('action', {})
+        contact_form_actions = contact_views['form']['toolbar'].get('action', {})
+        portal_list_actions = portal_views['list']['toolbar'].get('action', {})
+
+        self.assertTrue(any(a['id'] == send_mail_action.id for a in contact_list_actions),
+            'Action should be present in mailing-enabled models for list and kanban views')
+        self.assertFalse(any(a['id'] == send_mail_action.id for a in contact_form_actions),
+            'Action should not be present for other types of views')
+        self.assertFalse(any(a['id'] == send_mail_action.id for a in portal_list_actions),
+            'Action should not be present for non-mailing-enabled models')

odoo/odoo

addons/test_mail_full/tests/test_mass_mailing.py
@@ -154,3 +154,25 @@ class TestMassMailing(TestMailFullCommon):
         # sent: 15, 2 bl, 3 opt-out, 3 invalid -> 7 remaining
         # ignored: 2 bl + 3 optout + 2 invalid + 1 duplicate; failed: 0
         self.assertMailingStatistics(mailing, expected=16, delivered=7, sent=7, canceled=9, failed=0)
+
+    # tests should be a little more fleshed out as the feature reaches maturity,
+    # for now these are mostly to underline what I think should be tested
+    # The client action should be tested as well
+    def test_send_mailing_action_presence(self):
+        """
+        Test that _mailing_enabled models have access to the Send Email toolbar action.
+        """
+        send_mail_action = self.env.ref('mass_mailing.action_client_toolbar_mass_mailing')
+        contact_views = self.env['mailing.contact'].get_views([(False, 'form'), (False, 'list')], {'toolbar': True})['views']
+        portal_views = self.env['mail.test.portal'].get_views([(False, 'form'), (False, 'list')], {'toolbar': True})['views']
+
+        contact_list_actions = contact_views['list']['toolbar'].get('action', {})
+        contact_form_actions = contact_views['form']['toolbar'].get('action', {})
+        portal_list_actions = portal_views['list']['toolbar'].get('action', {})
+
+        self.assertTrue(any(a['id'] == send_mail_action.id for a in contact_list_actions),
+            'Action should be present in mailing-enabled models for list and kanban views')
+        self.assertFalse(any(a['id'] == send_mail_action.id for a in contact_form_actions),
+            'Action should not be present for other types of views')
+        self.assertFalse(any(a['id'] == send_mail_action.id for a in portal_list_actions),
+            'Action should not be present for non-mailing-enabled models')