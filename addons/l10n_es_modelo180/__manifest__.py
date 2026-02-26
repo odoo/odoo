ba5f574f7db53d133a888da84e67884d64dba2de
hatr-odoo <hatr@odoo.com>
2026-02-26 11:03:36 +0530

    [ADD] l10n_es: Modelo 180 Tax Report

---
+++ addons/l10n_es_modelo180/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Spain - Modelo 180',
+    'website': 'https://www.odoo.com/documentation/17.0/applications/finance/fiscal_localizations/spain.html',
+    'version': '1.0',
+    'author': 'Odoo S.A.',
+    'icon': '/account/static/description/l10n.png',
+    'countries': ['es'],
+    'category': 'Accounting/Localizations/Account Charts',
+    'depends': [
+        'l10n_es',
+    ],
+    'data': [
+        'data/mod180.xml',
+    ],
+    'license': 'LGPL-3',
+}
