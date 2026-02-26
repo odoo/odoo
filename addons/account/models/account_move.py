f8ad2994378b29bfb393d13cb6d270a0e37a1993
william-andre <wan@odoo.com>
2026-02-26 13:38:02 +0000

    [FIX] account: do not block incoming payments
    The last iteration[^1] was preventing automated flows from running
    smoothly because it was raising an error.
    Instead, we now just remove the unsure data.
    
    [^1]: 1794fce234735ed174599891435d4e2accc16324
    
    X-original-commit: 2e2dd364f665c65cbe2eb57221aaa4067f7dc1db

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -5083,7 +5083,10 @@ class AccountMove(models.Model):
                     "So you cannot confirm the invoice."
                 ))
             if invoice.partner_bank_id and invoice.is_inbound() and not invoice.partner_bank_id.allow_out_payment:
-                if invoice.partner_bank_id._user_can_trust():
+                if self.env.user.id == SUPERUSER_ID or self.env.user.has_group('base.group_public') or self.env.user.has_group('base.group_portal'):
+                    # Do not block in case of automated flows, simply remove the information
+                    invoice.partner_bank_id = False
+                elif invoice.partner_bank_id._user_can_trust():
                     raise RedirectWarning(
                         _(
                             "The company bank account (%(account_number)s) linked to this invoice is not trusted. "
@@ -5093,7 +5096,8 @@ class AccountMove(models.Model):
                         invoice.partner_bank_id._get_records_action(),
                         _("Bank settings")
                     )
-                raise UserError(_("The bank account of your company is not trusted. Please ask an admin or someone with approval rights to check it."))
+                else:
+                    raise UserError(_("The bank account of your company is not trusted. Please ask an admin or someone with approval rights to check it."))
             if float_compare(invoice.amount_total, 0.0, precision_rounding=invoice.currency_id.rounding) < 0:
                 validation_msgs.add(_(
                     "You cannot validate an invoice with a negative total amount. "
