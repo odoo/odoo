c5e0437b7bccc07d0efa2cf84a0966644c2d6a7f
utma-odoo <utma@odoo.com>
2026-02-26 12:43:01 +0530

    [ADD] l10n_gt{,_city}: move city data into separate module
    Steps to reproduce:
    - Create a new database.
    - Install the l10n_gt module.
    
    Error:
    KeyError: 'res.city'
    
    Root Cause:
    In PR [1], the file `res.city.csv` was added in the `l10n_gt` module,
    which relies on the `base_address_extended` module, but this
    dependency is not declared in the manifest file.
    
    Fix:
    This commit adds a new bridge module between `l10n_gt` and
    `base_address_extended`, and moves `res.city.csv` into this new
    module (l10n_gt_city). This approach is chosen because modifying
    dependencies in a stable version is not recommended.
    
    [1]:
    https://github.com/odoo/odoo/pull/247719

---
+++ addons/l10n_gt_city/__manifest__.py
@@ -0,0 +1,17 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Guatemala - City Data',
+    'category': 'Localization',
+    'description': """
+        Includes the following data for the Guatemala localization
+        ========================================================
+        City Data
+    """,
+    'author': 'Odoo S.A.',
+    'depends': ['l10n_gt', 'base_address_extended'],
+    'data': [
+        'data/res.city.csv'
+    ],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
