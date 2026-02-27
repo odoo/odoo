c492c881e9eb941aba836294810c70621919c3ff
David Monjoie <dmo@odoo.com>
2026-02-27 08:39:40 +0000

    [FIX] html_editor: fix non-deterministic table selection tests
    The custom table selection relies on the selectionchange event
    which is triggered after one tick.
    
    runbot-238876
    
    X-original-commit: 07418ff06a055d6be943d17cc38b9aba24655eab

--- addons/html_editor/static/tests/selection.test.js
+++ addons/html_editor/static/tests/selection.test.js
@@ -1138,6 +1138,9 @@ describe("getTargetedNodes", () => {
                 const { el: editable, editor } = await setupEditor(
                     "<table><tbody><tr><td>abcd[e</td><td>f]g</td></tr></tbody></table>"
                 );
+                // Table selection happens on selectionchange
+                // event which is fired in the next tick.
+                await tick();
                 // The special table selection implies the two table cells are
                 // fully marked as selected.
                 const td1 = editable.querySelector("td"); // The selection crossed `</td>` -> include it.
@@ -1152,6 +1155,9 @@ describe("getTargetedNodes", () => {
                 const { el: editable, editor } = await setupEditor(
                     "<table><tbody><tr><td>abcd<br>[<br>e</td><td>f]g</td></tr></tbody></table>"
                 );
+                // Table selection happens on selectionchange
+                // event which is fired in the next tick.
+                await tick();
                 // The special table selection implies the two table cells are
                 // fully marked as selected.
                 const td1 = editable.querySelector("td"); // The selection crossed `</td>` -> include it.
