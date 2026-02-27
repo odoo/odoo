dzdzdzdz1010/OdooStandard

[IMP] account: Replace account groups with account parent
Problems of account groups:
1. It's defined by code range.
This means that it forces the use of codes and it creates the constraint that an account must be within a code range
to be part of a group. This makes sense in countries like Belgium where there is a normalized plan, but this is a
problem in countries where there is no normalized plan or in countries where the use of codes is optional (eg. USA).
2. It shows the code in front of the group in accounting reports (not always desirable).
3. It's difficult to discover.
4. It doesn't allow to use a group as an "intermediary account" in the hierarchy. (eg. using the account "Automobile"
on a bill even though "Automobile" is a parent).
5. It doesn't display the CoA hierarchy.

task-5055586

addons/account/models/account_report.py
@@ -164,7 +164,7 @@ class AccountReport(models.Model):
         precompute=True, store=True, depends=['root_report_id', 'section_main_report_ids'],
     )
     filter_hierarchy = fields.Selection(
-        string="Account Groups",
+        string="Account Subtotals",
         selection=[('by_default', "Enabled by Default"), ('optional', "Optional"), ('never', "Never")],
         compute=lambda x: x._compute_report_option_filter('filter_hierarchy', 'optional'), readonly=False,
         precompute=True, store=True, depends=['root_report_id', 'section_main_report_ids'],

odoo/odoo

addons/account/models/account_report.py
@@ -170,7 +170,7 @@ class AccountReport(models.Model):
         precompute=True, store=True, depends=['root_report_id', 'section_main_report_ids'],
     )
     filter_hierarchy = fields.Selection(
-        string="Account Groups",
+        string="Account Parents",
         selection=[('by_default', "Enabled by Default"), ('optional', "Optional"), ('never', "Never")],
         compute=lambda x: x._compute_report_option_filter('filter_hierarchy', 'optional'), readonly=False,
         precompute=True, store=True, depends=['root_report_id', 'section_main_report_ids'],