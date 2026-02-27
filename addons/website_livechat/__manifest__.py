d6f23acdfc90514adae45d9feb140e4adf00a62b
Arib Ansari <aans@odoo.com>
2026-02-27 04:44:06 +0000

    [CLN] website, website_livechat: remove leftover cookies toggle SCSS
    After [1], the cookies bar toggle was reworked and is no longer an
    absolutely positioned button injected via interaction. The related SCSS
    became obsolete but was still loaded.
    
    This commit removes those unused styles.
    
    [1]: https://github.com/odoo/odoo/commit/7ffc121c777d36d1f62a61ca32838f2f18c2abc7
    
    X-original-commit: f9fd2c0de144670335508c906924c638b852c9b9

--- addons/website_livechat/__manifest__.py
+++ addons/website_livechat/__manifest__.py
@@ -41,7 +41,6 @@
         ],
         'web.assets_frontend': [
             "website_livechat/static/src/core/common/**/*",
-            'website_livechat/static/src/**/frontend/**/*',
         ],
         'web.assets_backend': [
             "website_livechat/static/src/core/common/**/*",
