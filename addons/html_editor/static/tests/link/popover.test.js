41100a20a71444beee200ccfa6fea60ca9ee9821
adch-odoo <adch@odoo.com>
2026-02-27 04:33:52 +0000

    [FIX] html_editor: use static file box instead of embedded
    Purpose of this commit:
    
    - Restore the static file box implementation and drop the embedded
    component, as it breaks printing with wkhtmltopdf.
    - The original issue with the static file box was fixed in
    https://github.com/odoo/odoo/pull/241591
    
    closes odoo/odoo#251752
    
    Reverts: https://github.com/odoo/odoo/pull/216572
    Enterprise: https://github.com/odoo/enterprise/pull/108999
    X-original-commit: cf1dc7908b2cb376e9a3f3589f5ac2f95e4f75c9
    Related: odoo/enterprise#109376
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Adnan Chaudhary (adch) <adch@odoo.com>

--- addons/html_editor/static/tests/link/popover.test.js
+++ addons/html_editor/static/tests/link/popover.test.js
@@ -19,7 +19,6 @@ import { getContent, setContent, setSelection } from "../_helpers/selection";
 import { expectElementCount } from "../_helpers/ui_expectations";
 import { insertLineBreak, insertText, splitBlock, undo } from "../_helpers/user_actions";
 import { execCommand } from "../_helpers/userCommands";
-import { MAIN_PLUGINS, NO_EMBEDDED_COMPONENTS_FALLBACK_PLUGINS } from "@html_editor/plugin_sets";
 
 const base64Img =
     "data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUA\n        AAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO\n            9TXL0Y4OHwAAAABJRU5ErkJggg==";
@@ -1185,6 +1184,9 @@ describe("shortcut", () => {
         // Tab through all focusable elements
         await press("Tab");
         await animationFrame();
+        expect("button:has(i.fa-upload)").toBeFocused();
+        await press("Tab");
+        await animationFrame();
         expect("button[name='link_type']").toBeFocused();
         await press("Tab");
         await animationFrame();
@@ -1658,9 +1660,7 @@ describe("link in contenteditable=false", () => {
 
 describe("upload file via link popover", () => {
     test("should display upload button when url input is empty", async () => {
-        const { editor } = await setupEditor("<p>[]<br></p>", {
-            config: { Plugins: [...MAIN_PLUGINS, ...NO_EMBEDDED_COMPONENTS_FALLBACK_PLUGINS] },
-        });
+        const { editor } = await setupEditor("<p>[]<br></p>");
         execCommand(editor, "openLinkTools");
         await waitFor(".o-we-linkpopover");
         // Upload button should be visible
@@ -1687,9 +1687,7 @@ describe("upload file via link popover", () => {
         return mockedUploadPromise;
     };
     test("can create a link to an uploaded file", async () => {
-        const { editor, el } = await setupEditor("<p>[]<br></p>", {
-            config: { Plugins: [...MAIN_PLUGINS, ...NO_EMBEDDED_COMPONENTS_FALLBACK_PLUGINS] },
-        });
+        const { editor, el } = await setupEditor("<p>[]<br></p>");
         const mockedUpload = patchUpload(editor);
         execCommand(editor, "openLinkTools");
         await waitFor(".o-we-linkpopover");
@@ -1733,9 +1731,7 @@ describe("upload file via link popover", () => {
     });
 
     test("label input does not get filled on file upload if it is already filled", async () => {
-        const { editor } = await setupEditor("<p>[]<br></p>", {
-            config: { Plugins: [...MAIN_PLUGINS, ...NO_EMBEDDED_COMPONENTS_FALLBACK_PLUGINS] },
-        });
+        const { editor } = await setupEditor("<p>[]<br></p>");
         const mockedUpload = patchUpload(editor);
         execCommand(editor, "openLinkTools");
         await waitFor(".o-we-linkpopover");
