066609612effafea1ae3ee50129e92ada4269d4e
David Monnom <moda@odoo.com>
2026-02-28 12:06:37 +0100

    [FIX] pos_self_order: missing fields in whitelist load data
    Missing `other_devices` in `pos.config` model in whitelist load data,
    which causes the field to be missing in the form view of pos config.

--- addons/pos_self_order/static/src/app/services/self_order_service.js
+++ addons/pos_self_order/static/src/app/services/self_order_service.js
@@ -439,7 +439,9 @@ export class SelfOrder extends Reactive {
     }
 
     initProducts() {
-        this.productCategories = this.models["pos.category"].getAll();
+        this.productCategories = this.config.limit_categories
+            ? this.config.iface_available_categ_ids
+            : this.models["pos.category"].getAll();
         this.productByCategIds = this.models["product.template"].getAllBy("pos_categ_ids");
 
         const excludedProductTemplateIds = new Set(
