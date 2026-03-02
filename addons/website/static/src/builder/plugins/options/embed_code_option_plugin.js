ee3e2af07eaa3d76571f09f3b1edc32f16017431
Nicolas Lempereur <nle@odoo.com>
2026-03-02 16:33:13 +0000

    [FIX] website: remove embed field if saved empty
    Scenario:
    
    - drop embedded code snippet
    - edit it to add a value
    - edit it to put it empty
    - save
    
    Result: on reload the embed is not removed and the old value is restored
    
    In 18.2, setting it empty would work and you would see an info alert:
    
    "Your Embed Code snippet doesn't have anything to display. Click on Edit
    to modify it."
    
    Fix: delete the embed field if it is saved empty.
    
    opw-5454289
    
    closes odoo/odoo#288761
    
    X-original-commit: b079f1ec8c108aeb705e419e170dce44a8436518
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/website/static/src/builder/plugins/options/embed_code_option_plugin.js
+++ addons/website/static/src/builder/plugins/options/embed_code_option_plugin.js
@@ -22,11 +22,14 @@ export class EmbedCodeOptionPlugin extends Plugin {
         // elements are removed in edit mode.
         for (const embedCodeEl of root.querySelectorAll(".s_embed_code")) {
             const embedTemplateEl = embedCodeEl.querySelector(".s_embed_code_saved");
-            if (embedTemplateEl) {
-                embedCodeEl
-                    .querySelector(".s_embed_code_embedded")
-                    .replaceChildren(cloneContentEls(embedTemplateEl.content, true));
+            // Remove snippet if embed was saved empty
+            if (!embedTemplateEl.innerHTML.trim()) {
+                embedCodeEl.remove();
+                continue;
             }
+            embedCodeEl
+                .querySelector(".s_embed_code_embedded")
+                .replaceChildren(cloneContentEls(embedTemplateEl.content, true));
         }
         return root;
     }
@@ -53,9 +56,6 @@ export class EditCodeAction extends BuilderAction {
         return newContent;
     }
     apply({ editingElement, loadResult: content }) {
-        if (!content) {
-            return;
-        }
         // Remove scripts tags from the DOM as we don't want them to
         // interfere during edition, but keeps them in a
         // `<template>` that will be saved to the database.
