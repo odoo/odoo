ab3f264e5d2a158585b682a35e9487f946635f8b
divy-odoo <divy@odoo.com>
2026-03-02 16:37:37 +0530

    [IMP] website: add disclaimer header option
    This commit introduces the new Headline snippet. Now Users will
    have an option to add a page specific headlines/warnings in the website.
    
    task-3658333

--- addons/website/static/src/builder/plugins/options/website_background_option_plugin.js
+++ addons/website/static/src/builder/plugins/options/website_background_option_plugin.js
@@ -7,7 +7,7 @@ export const WEBSITE_BACKGROUND_BG_COLOR_IMAGE_OPTION_SELECTOR =
     "section, .carousel-item, .s_masonry_block .row > div, .s_color_blocks_2 .row > div, .parallax, .s_text_cover .row > .o_not_editable, .s_website_form_cover .row > .o_not_editable, .s_split_intro .row > .o_not_editable, .s_bento_grid .row > div, .s_banner_categories .row > div, .s_ecomm_categories_showcase_block, .s_bento_grid_avatars .row > div";
 export const WEBSITE_BACKGROUND_BG_COLOR_IMAGE_OPTION_EXCLUDE = `${BASE_ONLY_BG_IMAGE_SELECTOR}, .s_carousel_wrapper, .s_image_gallery .carousel-item, .s_google_map, .s_map, [data-snippet] :not(.oe_structure) > [data-snippet], .s_masonry_block .s_col_no_resize, .s_quotes_carousel_wrapper, .s_carousel_intro_wrapper, .s_carousel_cards_item, .s_dynamic_snippet_category .s_dynamic_snippet_title, .o_no_background_option`;
 export const WEBSITE_BACKGROUND_BG_COLOR_OPTION_SELECTOR =
-    "section .row > div, .s_text_highlight, .s_mega_menu_thumbnails_footer, .s_hr, .s_cta_badge";
+    "section .row > div, .s_text_highlight, .s_mega_menu_thumbnails_footer, .s_hr, .s_cta_badge, .s_headline";
 export const WEBSITE_BACKGROUND_BG_COLOR_OPTION_EXCLUDE = `.s_col_no_bgcolor, .s_col_no_bgcolor.row > div, .s_masonry_block .row > div, .s_color_blocks_2 .row > div, .s_image_gallery .row > div, .s_text_cover .row > .o_not_editable, [data-snippet] :not(.oe_structure) > .s_hr, ${CARD_PARENT_HANDLERS}, .s_website_form_cover .row > .o_not_editable, .s_bento_grid .row > div, .s_banner_categories .row > div, ${BLOCKQUOTE_PARENT_HANDLERS}, .s_bento_grid_avatars .row > div`;
 
 export class WebsiteBackgroundOptionPlugin extends Plugin {
