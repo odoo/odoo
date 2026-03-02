7a65696278c59f66e66a746c2b6907fc1d56aae4
Majed Alhanash (malh) <malh@odoo.com>
2026-03-02 10:13:38 +0400

    [FIX] pos_sale: settle sale order with down payment and extra lines
    The following commit introduced a change in compute_unpaid_amount.
    https://github.com/odoo/odoo/commit/b8b50a797cdc0053643f959eb2d04800163fe005
    
    The unpaid_amount is now computed from the PoS order total
    instead of the settle payment order line.
    
    This causes an issue when the PoS order contains additional
    product lines besides the settle payment line.
    
    In such cases, the total amount may exceed the sale order
    amount, preventing the sale order from being settled again
    from the PoS.
    
    How to reproduce:
    - Create a sale order.
    - Apply a down payment in the PoS.
    - Add other products before validating the payment.
    - Ensure the total exceeds the sale order amount.
    - Pay the order.
    - Try to settle the same sale order from PoS, cannot find it.
    
    opw-5821232

--- addons/pos_sale/models/sale_order.py
+++ addons/pos_sale/models/sale_order.py
@@ -72,9 +72,8 @@ class SaleOrder(models.Model):
             online_payments_invoices = sale_order.transaction_ids.filtered(lambda tx_move: tx_move.state in ('authorized', 'done')).mapped('invoice_ids')
             invoice_lines = sale_order.order_line.invoice_lines.filtered(lambda l: l.parent_state in ('draft', 'posted') and l.move_id not in online_payments_invoices)
             total_invoices_paid = sum(invoice_lines.mapped(lambda l: math.copysign(l.price_total, -l.balance)))
-            pos_orders = sale_order.order_line.pos_order_line_ids.order_id
-            total_pos_orders_paid = sum(pos_orders.mapped('amount_total'))
-            sale_order.amount_unpaid = max(sale_order.amount_total - total_invoices_paid - total_pos_orders_paid - sale_order.amount_paid, 0.0)
+            total_pos_paid = sum(sale_order.order_line.filtered(lambda l: not l.display_type).mapped('pos_order_line_ids.price_subtotal_incl'))
+            sale_order.amount_unpaid = max(sale_order.amount_total - total_invoices_paid - total_pos_paid - sale_order.amount_paid, 0.0)
 
     @api.depends('order_line.pos_order_line_ids')
     def _compute_amount_to_invoice(self):
