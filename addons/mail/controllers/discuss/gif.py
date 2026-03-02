ad296bf3b8b7a26e9261aaf9625e8809f52ab573
Alex Kühn <aku@odoo.com>
2026-03-02 11:45:55 +0000

    [FIX] mail: replace Tenor GIF API key to KLIPY GIF API key
    Tenor API will be terminated on June 30, 2026:
    https://developers.google.com/tenor/guides/quickstart
    
    This commit makes the Tenor API key input settings use a Klipy GIF
    API key instead of a Tenor GIF API key. To keep GIF working after
    this commit, the API key must necessarily be changed to a Klipy GIF
    API key, as the old Tenor API key would be considered as an invalid
    Klipy API key.
    
    Task-5491965
    
    closes odoo/odoo#271393
    
    Upgrade: https://github.com/odoo/upgrade/pull/10516
    X-original-commit: 7ca7331707ef33734b40d9ccd586f08c7a66a224
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/controllers/discuss/gif.py
+++ addons/mail/controllers/discuss/gif.py
@@ -19,7 +19,7 @@ class DiscussGifController(Controller):
         response = None
         try:
             response = requests.get(
-                f"https://tenor.googleapis.com/v2/{endpoint}", timeout=3
+                f"https://api.klipy.com/v2/{endpoint}", timeout=3
             )
             response.raise_for_status()
         except (urllib3.exceptions.MaxRetryError, requests.exceptions.HTTPError):
@@ -36,7 +36,7 @@ class DiscussGifController(Controller):
         query_string = werkzeug.urls.url_encode(
             {
                 "q": search_term,
-                "key": ir_config.get_param("discuss.tenor_api_key"),
+                "key": ir_config.get_param("discuss.klipy_api_key"),
                 "client_key": request.env.cr.dbname,
                 "limit": TENOR_GIF_LIMIT,
                 "contentfilter": TENOR_CONTENT_FILTER,
@@ -56,7 +56,7 @@ class DiscussGifController(Controller):
         ir_config = request.env["ir.config_parameter"].sudo()
         query_string = werkzeug.urls.url_encode(
             {
-                "key": ir_config.get_param("discuss.tenor_api_key"),
+                "key": ir_config.get_param("discuss.klipy_api_key"),
                 "client_key": request.env.cr.dbname,
                 "limit": TENOR_GIF_LIMIT,
                 "contentfilter": TENOR_CONTENT_FILTER,
@@ -77,8 +77,8 @@ class DiscussGifController(Controller):
         ir_config = request.env["ir.config_parameter"].sudo()
         query_string = werkzeug.urls.url_encode(
             {
-                "ids": ",".join(ids),
-                "key": ir_config.get_param("discuss.tenor_api_key"),
+                "ids": ",".join(ids) or None,
+                "key": ir_config.get_param("discuss.klipy_api_key"),
                 "client_key": request.env.cr.dbname,
                 "media_filter": "tinygif",
             }
@@ -92,6 +92,8 @@ class DiscussGifController(Controller):
         tenor_gif_ids = request.env["discuss.gif.favorite"].search(
             [("create_uid", "=", request.env.user.id)], limit=20, offset=offset
         )
+        if not tenor_gif_ids.mapped("tenor_gif_id"):
+            return ([],)
         return (self._gif_posts(tenor_gif_ids.mapped("tenor_gif_id")) or [],)
 
     @route("/discuss/gif/remove_favorite", type="jsonrpc", auth="user")
