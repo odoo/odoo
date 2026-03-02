13f093c47d945b5d2ea555f6b5cbbdc2199449cc
tsm-odoo <tsm@odoo.com>
2026-03-02 14:03:43 +0000

    [FIX] base, bus, mail: do not rotate session from websocket routes
    Before this commit, calling `/websocket/peek_notifications` or
    `/websocket/update_bus_presence` could rotate the session.
    
    Since those routes are not called by the client, the cookie
    is unchanged on the client side, leading to expire sessions
    error later on.
    
    This commit ensure we won't rotate the sessions for those routes.
    
    opw-5445323
    
    Co-authored-by: Julien Castiaux <juc@odoo.com>
    X-original-commit: 7c7ec1dc3d128e7e43836abf88766056b556cfd5

--- addons/mail/tests/test_websocket_controller.py
+++ addons/mail/tests/test_websocket_controller.py
@@ -1,8 +1,11 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo.addons.base.tests.common import HttpCaseWithUserDemo
+from freezegun import freeze_time
+
+from odoo.http.session import SESSION_ROTATION_INTERVAL
 from odoo.tests import tagged
 
+from odoo.addons.base.tests.common import HttpCaseWithUserDemo
 from odoo.addons.bus.models.bus import channel_with_db, json_dump
 
 
@@ -60,3 +63,26 @@ class TestWebsocketController(HttpCaseWithUserDemo):
         self.assertEqual(notification["message"]["payload"]["partner_id"], self.partner_demo.id)
         self.assertEqual(notification["message"]["payload"]["im_status"], "online")
         self.assertEqual(notification["message"]["payload"]["presence_status"], "online")
+
+    @freeze_time("2026-03-03", as_kwarg='clock')
+    def test_do_not_rotate_session_when_updating_presence(self, clock):
+        self.authenticate('admin', 'admin')
+        self.url_open('/odoo').raise_for_status()
+        original_session = self.opener.cookies['session_id']
+
+        clock.tick(SESSION_ROTATION_INTERVAL + 1)
+        self.make_jsonrpc_request('/websocket/peek_notifications', {
+            'channels': [],
+            'last': 0,
+            'is_first_poll': True,
+        })
+        self.make_jsonrpc_request('/websocket/update_bus_presence', {
+            'inactivity_period': 0,
+        })
+        self.assertEqual(self.opener.cookies['session_id'], original_session,
+            "Session rotation must not occur at the websocket routes "
+            "that are re-exposed on HTTP for convenience.")
+
+        self.url_open('/odoo').raise_for_status()
+        self.assertNotEqual(self.opener.cookies['session_id'], original_session,
+            "Session rotation should occur with other URLs.")
