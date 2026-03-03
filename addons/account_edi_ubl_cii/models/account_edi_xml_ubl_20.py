a9f2b5da8522eb2e047f5d0a50004378e5245bdb
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2026-03-03 15:15:11 +0000

    [FIX] account_edi_ubl_cii: add fallback for partner info
    Currently, invoices created by uploading an e-invoice xml
    may not have the partner correctly filled in.
    
    Steps to reproduce:
    - Upload a customer invoice XML in format PEPPOL BIS 4a
    
    Issue:
    Partner is not set on the newly created record.
    On the chatter can be observed the message
    "Could not retrieve partner with details: Name: None, Vat: None, Phone: None, Email: None"
    
    Analysis:
    After https://github.com/odoo/odoo/commit/b4da4a77da3e0dc18707b935786b1924ca473796
    we look for partner information under AccountingSupplierParty/Contact
    However older format may still be used to encode partner info under
    AccountingSupplierParty/Party
    
    opw-5970610
    
    X-original-commit: d8436b32f5a37494946831726bf60cebd1da8162

--- addons/account_edi_ubl_cii/models/account_edi_xml_ubl_20.py
+++ addons/account_edi_ubl_cii/models/account_edi_xml_ubl_20.py
@@ -1116,10 +1116,12 @@ class AccountEdiXmlUBL20(models.AbstractModel):
         """ Returns a dict of values that will be used to retrieve the partner """
         return {
             'vat': self._find_value(f'.//cac:{role}Party//cbc:CompanyID[string-length(text()) > 5]', tree),
-            'phone': self._find_value(f'.//cac:{role}Party//cac:Contact//cbc:Telephone', tree),
-            'email': self._find_value(f'.//cac:{role}Party//cac:Contact//cbc:ElectronicMail', tree),
-            'name': self._find_value(f'.//cac:{role}Party//cbc:RegistrationName', tree) or
-                    self._find_value(f'.//cac:{role}Party//cac:Contact//cbc:Name', tree),
+            'phone': self._find_value(f'.//cac:{role}Party//cac:Contact/cbc:Telephone', tree),
+            'email': self._find_value(f'.//cac:{role}Party//cac:Contact/cbc:ElectronicMail', tree),
+            'name': self._find_value(f'.//cac:{role}Party//cac:PartyTaxScheme/cbc:RegistrationName', tree) or
+                    self._find_value(f'.//cac:{role}Party//cac:PartyLegalEntity/cbc:RegistrationName', tree) or
+                    self._find_value(f'.//cac:{role}Party//cac:PartyName/cbc:Name', tree) or
+                    self._find_value(f'.//cac:{role}Party//cac:Contact/cbc:Name', tree),
             'postal_address': self._get_postal_address(tree, role),
         }
 
