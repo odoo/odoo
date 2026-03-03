f587855630218d040be9d11ee3ea973bccfef675
Eve Lin (evlin) <evlin@odoo.com>
2026-03-03 14:38:20 -0500

    [FIX] point_of_sale: avoid cash rounding on non-cash consolidation
    When consolidating POS orders into a single invoice, the cash rounding
    adjustment logic was triggered whenever cash rounding was enabled on
    the POS configuration, even if the payment methods were not cash.
    
    In scenarios where only non-cash payment methods (card or
    customer account) were used, 'invoice.invoice_cash_rounding_id'
    could legitimately be unset as 'only_round_cash_method'
    is enabled. However, the rounding adjustment code still attempted to
    create a rounding line using accounts from this field.
    
    This resulted in a NULL `account_id` on a rounding line
    ("Missing required account on accountable line")
    
    This fix ensures that the rounding adjustment logic only executes when
    there is cash payment (meaning that 'invoice_cash_rounding_id' exsists).
    
    Related to opw-5969706
    
    closes odoo/odoo#251862
    
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/point_of_sale/models/pos_order.py
+++ addons/point_of_sale/models/pos_order.py
@@ -805,7 +805,7 @@ class PosOrder(models.Model):
         amount_total = sum(order.amount_total for order in self)
         payment_total = sum(order.amount_paid for order in self)
 
-        if self.config_id.cash_rounding:
+        if self.config_id.cash_rounding and invoice.invoice_cash_rounding_id:
             line_ids_commands = []
             rate = invoice.invoice_currency_rate
             sign = invoice.direction_sign
