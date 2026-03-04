94d1e21840a39f99088d7c4d2bc04d469e0569de
Arnav Varshney <avar@odoo.com>
2026-03-04 17:27:18 +0800

    [FIX] l10n_tw_edi_ecpay_*: check fiscal country instead of company country
    Previously, the module checked `company_id.country_id.code == 'TW'` to determine if Taiwan's ECPay logic should be applied. However, `country_id` only represents the physical address of the company.
    
    This commit replaces `country_id` with `account_fiscal_country_id` across the sale order model and website controllers. This ensures that the ECPay integration correctly triggers for any company using the Taiwan fiscal localization.
    
    Task-6002433
    
    closes odoo/odoo#251924
    
    Signed-off-by: Arnav Varshney <avar@odoo.com>
    Signed-off-by: Nicolas Viseur (vin) <vin@odoo.com>

--- addons/l10n_tw_edi_ecpay_website_sale/controllers/main.py
+++ addons/l10n_tw_edi_ecpay_website_sale/controllers/main.py
@@ -151,7 +151,7 @@ class WebsiteSaleL10nTW(WebsiteSale):
             address_values, partner_sudo, address_type, *args, **kwargs
         )
 
-        if address_type == 'billing' and request.website.company_id.country_id.code == 'TW' and request.website.company_id._is_ecpay_enabled():
+        if address_type == 'billing' and request.website.company_id.account_fiscal_country_id.code == 'TW' and request.website.company_id._is_ecpay_enabled():
             phone = address_values.get('phone')
             if phone:
                 formatted_phone = request.env['account.move']._reformat_phone_number(phone)
@@ -169,7 +169,7 @@ class WebsiteSaleL10nTW(WebsiteSale):
     def _handle_extra_form_data(self, extra_form_data, address_values):
         super()._handle_extra_form_data(extra_form_data, address_values)
 
-        if request.website.company_id.country_id.code == 'TW' and request.website.company_id._is_ecpay_enabled():
+        if request.website.company_id.account_fiscal_country_id.code == 'TW' and request.website.company_id._is_ecpay_enabled():
             order = request.website.sale_get_order()
             if address_values.get('company_name'):
                 l10n_tw_edi_is_print = True
