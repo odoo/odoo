960f405e8d9d28e3c4152f9bfed959630866ce58
Alex Kühn <aku@odoo.com>
2026-03-04 11:53:27 +0000

    [IMP] mail: show end datetime of poll on mouse-hover time remaining
    This commit adds showing of datetime when the poll will end when
    mouse-hovering on the Remaining time text of the poll. This is useful
    to put a reminder for later just before the poll ends, let's say to
    see if involvement is fine or we need to push pressure for people to
    vote.
    
    X-original-commit: e64edf7358284e39a200053dbcc55690e24c3fc1

--- addons/mail/static/src/core/common/poll.js
+++ addons/mail/static/src/core/common/poll.js
@@ -6,6 +6,8 @@ import { Component } from "@odoo/owl";
 
 import { _t } from "@web/core/l10n/translation";
 
+const { DateTime } = luxon;
+
 /**
  * @typedef {Object} Props
  * @property {import("models").MailPollModel} poll
@@ -48,6 +50,15 @@ export class Poll extends Component {
             },
             () => [this.props.poll.poll_end_dt]
         );
+    }
+
+    get remainingTimeTextTitle() {
+        if (!this.props.poll.poll_end_dt) {
+            return "";
+        }
+        return _t("Poll ends on %(date)s", {
+            date: this.props.poll.poll_end_dt.toLocaleString(DateTime.DATETIME_MED),
+        });
     }
 
     showResults() {
