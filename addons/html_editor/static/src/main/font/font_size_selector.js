95480ac267379a891736773999e17437c709778c
Benoit Socias (bso) <bso@odoo.com>
2026-03-04 13:49:03 +0000

    [FIX] html_editor: not lose font size on toolbar redraw
    Since [1], the font size in editor's toolbar is a `button` that contains
    an `iframe`. An `input` is put within this `iframe` so that when it is
    focused, the selection in the edited document is not lost.
    Unfortunately, when an `iframe` is moved in the DOM, it is restarted. In
    this case, the `iframe` has no source, so it becomes empty, and the
    content that was added into it `onMounted` is lost.
    
    This commit solves this by listening to every `load` events on the
    `iframe` instead of only the initial one.
    
    Steps to reproduce:
    - Go to a "To Do" note
    - Create a table with `/table`
    - Press Enter to confirm the 3x3 size
    - Select the last two cells of the first column
    - Move the mouse upwards to the next cell
    
    => The font size disappeared
    
    [1]: https://github.com/odoo/odoo/commit/a468de9d1099931d8f553c8569359996e7b694f2
    
    task-6003539
    
    X-original-commit: 5be300670d31a1ab27f47e8de7feaf6b82bfe7e0

--- addons/html_editor/static/src/main/font/font_size_selector.js
+++ addons/html_editor/static/src/main/font/font_size_selector.js
@@ -39,8 +39,8 @@ export class FontSizeSelector extends Component {
             const initFontSizeInput = () => {
                 const iframeDoc = iframeEl.contentWindow.document;
 
-                // Skip if already initialized.
-                if (this.fontSizeInput || !iframeDoc.body) {
+                // Skip if already/still initialized.
+                if (this.fontSizeInput?.closest("body") === iframeDoc.body || !iframeDoc.body) {
                     return;
                 }
 
@@ -85,17 +85,9 @@ export class FontSizeSelector extends Component {
             };
             if (iframeEl.contentDocument.readyState === "complete") {
                 initFontSizeInput();
-            } else {
-                // in firefox, iframe is not immediately available. we need to wait
-                // for it to be ready before mounting.
-                iframeEl.addEventListener(
-                    "load",
-                    () => {
-                        initFontSizeInput();
-                    },
-                    { once: true }
-                );
             }
+            // If iframe is moved around in DOM, it restarts from scratch and needs to be repopulated.
+            iframeEl.addEventListener("load", initFontSizeInput);
         });
         useEffect(
             () => {
