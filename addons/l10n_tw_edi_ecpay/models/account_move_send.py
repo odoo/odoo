6df671cab0905132dc056649d3000f853597a12b
Arnav Varshney <avar@odoo.com>
2026-03-04 09:27:18 +0000

    [FIX] l10n_tw_edi_ecpay_*: check fiscal country instead of company country
    Previously, the module checked `company_id.country_id.code == 'TW'` to determine if Taiwan's ECPay logic should be applied. However, `country_id` only represents the physical address of the company.
    
    This commit replaces `country_id` with `account_fiscal_country_id` across the sale order model and website controllers. This ensures that the ECPay integration correctly triggers for any company using the Taiwan fiscal localization.
    
    Task-6002433
    
    closes odoo/odoo#252395
    
    X-original-commit: f702914d4a20a91c65f7b1b13d30d4fb1294138e
    Signed-off-by: Arnav Varshney <avar@odoo.com>
    Signed-off-by: Nicolas Viseur (vin) <vin@odoo.com>
    Signed-off-by: Arnav Varshney (avar) <avar@odoo.com>

--- addons/l10n_tw_edi_ecpay/models/account_move_send.py
+++ addons/l10n_tw_edi_ecpay/models/account_move_send.py
@@ -14,7 +14,7 @@ class AccountMoveSend(models.AbstractModel):
         return (move.move_type == 'out_invoice'
                 and ('debit_origin_id' not in self.env['account.move']._fields or not move.debit_origin_id)
                 and move.state == 'posted'
-                and move.company_id.country_id.code == 'TW'
+                and move.company_id.account_fiscal_country_id.code == 'TW'
                 and not move.l10n_tw_edi_state
                 and move.company_id._is_ecpay_enabled())
 
@@ -22,7 +22,7 @@ class AccountMoveSend(models.AbstractModel):
     def _is_tw_edi_issue_allowance_applicable(self, move):
         return (move.move_type == 'out_refund'
                 and move.state == 'posted'
-                and move.company_id.country_id.code == 'TW'
+                and move.company_id.account_fiscal_country_id.code == 'TW'
                 and move.reversed_entry_id
                 and move.reversed_entry_id.l10n_tw_edi_ecpay_invoice_id
                 and not move.l10n_tw_edi_refund_invoice_number
