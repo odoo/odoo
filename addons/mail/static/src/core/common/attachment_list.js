20b7cd02106966d32493681ecc13e293e59d9d1e
Renaud Thiry <reth@odoo.com>
2026-03-04 15:25:24 +0100

    [IMP] mail: use attClassObjectToString for attachment list
    It's a bit nicer to work with.
    Also adapt a css rule to ease forwardport, as the backport removed a
    space.
    
    task-5013894

--- addons/mail/static/src/core/common/attachment_list.js
+++ addons/mail/static/src/core/common/attachment_list.js
@@ -14,6 +14,8 @@ import { useService } from "@web/core/utils/hooks";
 import { url } from "@web/core/utils/urls";
 import { useRef } from "@web/owl2/utils";
 
+import { attClassObjectToString } from "@mail/utils/common/format";
+
 class Actions extends Component {
     static components = { Dropdown, DropdownItem };
     static props = ["actions"];
@@ -36,6 +38,9 @@ export class AttachmentList extends Component {
     static components = { Actions, Gif };
     static props = ["attachments", "unlinkAttachment", "messageSearch?"];
     static template = "mail.AttachmentList";
+
+    // make this available for class evaluation in the template
+    attClassObjectToString = attClassObjectToString;
 
     setup() {
         super.setup();
