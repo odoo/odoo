odoo/odoo

commit c5a266a6d919ab73727d001c5c0a4c75d78e6946

[FIX] sale_mrp{,_margin}: prevent cost reset on validating delivery
Currently when the user validates a sale order delivery which has a kit the cost
gets reset to 0.

Steps to produce:
* Install sale_margin, sale_management, mrp
* Create a product with an AVCO or FIFO product category
* Create a BOM of type ‘Kit’ for that product.
* Add components A and B to the BOM, each with an on-hand quantity and a cost of
  10.
* On the kit product compute the cost according to the BOM
* Create and confirm SO for 1 unit of the kit product
* Delivery > Validate the Delivery  and go back to the Sale Order

Observed Behavior:
The product cost on the sale order is set to 0, but it should be 20.

Root cause:
After commit [1], an override for _get_price_unit was added to correctly
calculate kit prices for BOM products.

The goal was to prevent the cost from being set to 0 after a
delivery is validated. However, when this logic is triggered during
delivery validation, it doesn’t behave as intended.

After the delivery is validated, compute [2] runs and calls function [3].
However, `self` at [3] refers to a stock move record, so we get the
components of the kit instead of the kit product itself.

The function _bom_find expects the main kit product, not the components.
Because of this, the kit’s unit price isn’t calculated, and the parent
function [4] ends up setting the purchase price to 0.

Solution:
Use the kit product IDs from the sale order lines instead of the move lines,
ensuring the main kit product (not its components) is used and the BOM is found
properly.

Return unit price for kit products instead of total value for the function
`_get_kit_price_unit`

Update the test case to account for delivered quantities
  Before:
   The sales order used Units as the unit of measure while the product and stock
   moves use a pack of 10. Since only 3 units get delivered and one product
   equals 10 units, this was not counted as a full delivery.

addons/sale_mrp_margin/tests/test_sale_mrp_flow.py
@@ -109,14 +109,16 @@ class TestSaleMrpFlow(test_sale_mrp_flow.TestSaleMrpFlowCommon):
             'order_line': [Command.create({
                 'product_id': kit.id,
                 'product_uom_qty': 3,
-                'product_uom_id': self.uom_unit.id
+                'product_uom_id': self.uom_ten.id
             })]
         })
-        self.assertEqual(so.order_line.purchase_price, 60)
+        self.assertEqual(so.order_line.purchase_price, 600)
         so.action_confirm()
-        self.assertEqual(so.order_line.purchase_price, 60)
+        self.assertEqual(so.order_line.purchase_price, 600)
         for move in so.picking_ids.move_ids:
             move.quantity = move.product_uom_qty
+        self.assertRecordValues(so.order_line, [{'purchase_price': 600, 'qty_delivered': 0.0}])
         so.picking_ids.button_validate()
         self.assertEqual(so.picking_ids.state, 'done')
-        self.assertEqual(so.order_line.purchase_price, 60)
+        self.assertRecordValues(so.order_line, [{'purchase_price': 600, 'qty_delivered': 3.0}])
+        self.assertEqual(so.order_line.purchase_price, 600)

addons/mrp_account/models/stock_move.py
@@ -41,4 +41,4 @@ class StockMove(models.Model):
             price_unit = super(StockMove, valuated_moves)._get_price_unit()
             qty_per_kit = component_qty_per_kit[component] / kit_bom.product_qty
             total_price_unit += price_unit * qty_per_kit
-        return total_price_unit
+        return total_price_unit / valuated_quantity if not product.uom_id.is_zero(valuated_quantity) else 0

addons/sale_mrp/models/stock_move.py
@@ -9,7 +9,7 @@ class StockMove(models.Model):
     def _get_price_unit(self):
         order_line = self.sale_line_id
         if order_line and all(move.sale_line_id == order_line for move in self) and any(move.product_id != order_line.product_id for move in self):
-            product = self.product_id.with_company(order_line.company_id)
+            product = order_line.product_id.with_company(order_line.company_id)
             bom = product.env['mrp.bom']._bom_find(product, company_id=self.company_id.id, bom_type='phantom')[product]
             if bom:
                 return self._get_kit_price_unit(product, bom, order_line.qty_delivered)

commit 93dd187ca15ba7ef6a6439d95f1c9f800001b37f

[FIX] sale_mrp: compute COGS correctly for kits (avoid double counting)
Bug introduced by: c5a266a6d919ab73727d001c5c0a4c75d78e6946

Steps to reproduce:
- Create a storable product “C1”
  - Cost: $1
- Create a storable product “P1”
  - Type: Kit
  - Category: AVCO
  - BoM:
    - Component: C1 (qty: 20)

- Create a sale order for 1 unit of P1
- Confirm the SO
- Validate the delivery
- Create and post the invoice
- Check the journal items

Problem:
The total COGS posted is $400 instead of $200.

When posting the invoice, `_get_cogs_value` is triggered. It retrieves
the price unit of each component based on the stock move quantity of
the sale order:

https://github.com/odoo/odoo/blob/8b7a07692366dc465ace409235693a1c106613ef/addons/sale_mrp/models/account_move.py#L39

However, since commit: https://github.com/odoo/odoo/commit/c5a266a6d919ab73727d001c5c0a4c75d78e6946#diff-bb93bc1c1d7fd07978c5081adc6dc21dc3eb4c37b5e8f44342fe34b61ee060b5R44

, `_get_price_unit` returns the total cost of the component required
to produce one kit (i.e., 20 units * $1 = $20).

Later, `_get_cogs_value` multiplies this value again by the component
quantity defined in the BoM (20), resulting in a double multiplication:
(20 units * $1) * 20 = $400

opw-5946749

closes odoo/odoo#251509

Signed-off-by: William Henrotin (whe) <whe@odoo.com>

addons/sale_mrp/models/account_move.py
@@ -34,9 +34,8 @@ class AccountMoveLine(models.Model):
                 components_qty = so_line._get_bom_component_qty(bom)
                 storable_components = self.env['product.product'].search([('id', 'in', list(components_qty.keys())), ('is_storable', '=', True)])
                 for product in storable_components:
-                    factor = components_qty[product.id]['qty']
                     prod_moves = moves.filtered(lambda m: m.product_id == product)
                     product = product.with_company(self.company_id)
-                    average_price_unit += factor * prod_moves._get_price_unit()
+                    average_price_unit += prod_moves._get_price_unit()
                 price_unit = average_price_unit / bom.product_qty or price_unit
         return price_unit

addons/sale_mrp_margin/tests/test_sale_mrp_flow.py
@@ -66,6 +66,7 @@ class TestSaleMrpFlow(test_sale_mrp_flow.TestSaleMrpFlowCommon):
         sub_kit, kit = self._cls_create_product('Sub Kit', self.uom_unit), self._cls_create_product('Lovely Kit', self.uom_ten)
         kit.uom_ids = [Command.set([self.uom_unit.id, self.uom_ten.id])]
         self.product_category.property_cost_method = 'average'
+        self.product_category.property_valuation = 'real_time'
         (kit + sub_kit + self.component_a + self.component_b).categ_id = self.product_category
         self.env['mrp.bom'].create([
             {
@@ -122,3 +123,15 @@ class TestSaleMrpFlow(test_sale_mrp_flow.TestSaleMrpFlowCommon):
         self.assertEqual(so.picking_ids.state, 'done')
         self.assertRecordValues(so.order_line, [{'purchase_price': 600, 'qty_delivered': 3.0}])
         self.assertEqual(so.order_line.purchase_price, 600)
+        # create an invoice to check that the cost price on the invoice line is correct
+        inv_1 = so._create_invoices()
+        inv_1.action_post()
+        self.assertEqual(inv_1.state, 'posted', 'invoice should be in posted state')
+        # The cost price on the invoice line should be 600 * 3 = 1800
+        self.assertRecordValues(inv_1.line_ids, [
+            {'debit': 0.0, 'credit': 3.0},
+            {'debit': 0.0, 'credit': 0.45},
+            {'debit': 3.45, 'credit': 0.0},
+            {'debit': 0.0, 'credit': 1800},
+            {'debit': 1800, 'credit': 0.0},
+        ])