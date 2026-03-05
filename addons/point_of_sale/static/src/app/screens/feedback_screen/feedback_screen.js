52bd03e7198120b98e9f1a8c3dded9d8a503f5f4
manv-afk <manv@odoo.com>
2026-03-05 10:29:45 +0100

    [FIX] point_of_sale: remove `paymentMethodId``prop from FeedbackScreen
    - Remove unused `paymentMethodId` prop from FeedbackScreen which caused incorrect redirect to receipt screen instead of feedback screen when using eWallet with "Automatic Receipt Printing" and "Skip Preview Screen" enabled.
    - The prop `paymentMethodId` is already removed in versions > `19.0`.
    
    closes odoo/odoo#252176
    
    Task-id: 6008245
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/point_of_sale/static/src/app/screens/feedback_screen/feedback_screen.js
+++ addons/point_of_sale/static/src/app/screens/feedback_screen/feedback_screen.js
@@ -12,7 +12,6 @@ export class FeedbackScreen extends Component {
     static props = {
         orderUuid: String,
         waitFor: { type: Object, optional: true },
-        paymentMethodId: { type: Number, optional: true, default: null },
     };
 
     setup() {
