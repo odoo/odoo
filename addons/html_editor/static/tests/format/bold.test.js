51db2f48f07942e1dfbc1e339a3931adefde5d5a
dhba <dhba@odoo.com>
2026-03-05 04:48:21 +0000

    [FIX] html_editor: prevent formatting data-selection-placeholder nodes
    Steps to Reproduce
    - Go to To-Do
    - Add a table
    - Type content inside the table
    - Select all using Ctrl + A
    - Apply formatting (e.g., Bold)
    
    Issue:
    An empty line appears above the table and the layout shifts unexpectedly after
    applying formatting.
    
    Cause:
    `data-selection-placeholder` nodes exist before and after the table. These nodes
    normally have zero height.
    When selecting all content, these placeholder nodes are also included in the
    selection. Applying formatting styles them as well, which changes their height
    from zero. As a result, they become visible and appear as newly added blank
    lines, even though no new line was actually inserted.
    
    Solution
    Exclude system nodes, such as selection placeholder elements, from formatting
    operations. This prevents them from being styled and avoids unintended layout
    shifts.
    
    task-5976361
    
    closes odoo/odoo#256232
    
    X-original-commit: 7f60e6ec6c3896f5e7484f52383700b297497e55
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: DHAVAL BARAIYA (dhba) <dhba@odoo.com>

--- addons/html_editor/static/tests/format/bold.test.js
+++ addons/html_editor/static/tests/format/bold.test.js
@@ -587,3 +587,46 @@ test("should toggle bold across nested spans", async () => {
     bold(editor);
     expect(getContent(el)).toBe(`<p><span><span>[A</span> </span></p><p>B]</p>`);
 });
+
+test("should not apply bold to selection placeholder nodes", async () => {
+    const { el } = await setupEditor(
+        unformat(`
+            <table>
+                <tbody>
+                    <tr>
+                        <td>1[]</td>
+                    </tr>
+                </tbody>
+            </table>
+        `)
+    );
+    await press(["ctrl", "a"]);
+    await animationFrame();
+    expect(getContent(el)).toBe(
+        unformat(`
+            <p data-selection-placeholder="">[<br></p>
+            <table class="o_selected_table">
+                <tbody>
+                    <tr>
+                        <td class="o_selected_td">1</td>
+                    </tr>
+                </tbody>
+            </table>
+            <p data-selection-placeholder="">]<br></p>
+        `)
+    );
+    await press(["ctrl", "b"]);
+    expect(getContent(el)).toBe(
+        unformat(`
+            <p data-selection-placeholder=""><br></p>
+            <table class="o_selected_table">
+                <tbody>
+                    <tr>
+                        <td class="o_selected_td"><strong>[1]</strong></td>
+                    </tr>
+                </tbody>
+            </table>
+            <p data-selection-placeholder=""><br></p>
+        `)
+    );
+});
