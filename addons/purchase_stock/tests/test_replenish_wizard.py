odoo/odoo

commit 524bce9876c3fd5f7c3e06c64c84b6660e71bef8

[FIX] purchase_stock,*: correct route domain for stock replenishment wizard
*mrp,stock

Issue
-----
MTO gets suggested by default as the replenishment route for products.

Steps to reproduce
-----
- Enable routes
- Unarchive MTO
- Create a product with a vendor and MTO route
- Open the replenishment wizard

> The default/suggested replenishment route is MTO

Cause
-----
When creating the widget, we go through its' `default_get` to set its' default
route

https://github.com/odoo/odoo/blob/edbaabc234cadff2c3603ae0a37571b75841068d/addons/stock/wizard/product_replenish.py#L76-L77

the domain is computed by `_get_route_domain`, which in turn calls
`_get_allowed_route_domain`

https://github.com/odoo/odoo/blob/edbaabc234cadff2c3603ae0a37571b75841068d/addons/stock/wizard/product_replenish.py#L165-L170

However, since we are creating a new, it does not have a `warehouse_id` yet, so
we don't go into the `if` condition.

https://github.com/odoo/odoo/blob/edbaabc234cadff2c3603ae0a37571b75841068d/addons/stock/models/stock_replenish_mixin.py#L26-L39

We only get routes from the product, but since buy is set on the warehouse and
not the product anymore, it doesn't get included.

Solution
-----
There is an override of `_get_route_domain` in `purchase_stock`

https://github.com/odoo/odoo/blob/edbaabc234cadff2c3603ae0a37571b75841068d/addons/purchase_stock/wizard/product_replenish.py#L91-L96

it filters out the buy route if there is no `seller_ids` for the product, but
since the buy route cannot be a part of the domain anymore, the condition should
be changed to include buy if the product has a seller.

Note: `_get_route_domain` is only ever called in `default_get`.

Other changes
-----
The same change is also applied for the manufacture route, as the logic is the
same.

It makes no sens to include route that do not lead to an internal location.

-----
Ticket:
opw-5388728

closes odoo/odoo#246304

Signed-off-by: William Henrotin (whe) <whe@odoo.com>

addons/mrp/wizard/product_replenish.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
+from odoo.fields import Domain
 
 
 class ProductReplenish(models.TransientModel):
@@ -41,3 +42,11 @@ class ProductReplenish(models.TransientModel):
         if product_tmpl_id and product_tmpl_id.bom_ids:
             delay += product_tmpl_id.bom_ids[0].produce_delay + product_tmpl_id.bom_ids[0].days_to_prepare_mo
         return fields.Datetime.add(date, days=delay)
+
+    def _get_route_domain(self, product_tmpl_id):
+        domain = super()._get_route_domain(product_tmpl_id)
+        company = product_tmpl_id.company_id or self.env.company
+        manufacture_route = self.env['stock.rule'].search([('action', '=', 'manufacture'), ('company_id', '=', company.id)]).route_id
+        if manufacture_route and product_tmpl_id.bom_ids:
+            domain = Domain.OR([domain, Domain('id', '=', manufacture_route.id)])
+        return domain

addons/purchase_stock/wizard/product_replenish.py
@@ -90,7 +90,8 @@ class ProductReplenish(models.TransientModel):
 
     def _get_route_domain(self, product_tmpl_id):
         domain = super()._get_route_domain(product_tmpl_id)
-        buy_route = self.env.ref('purchase_stock.route_warehouse0_buy', raise_if_not_found=False)
-        if buy_route and not product_tmpl_id.seller_ids:
-            domain = Domain.AND([domain, Domain('id', '!=', buy_route.id)])
+        company = product_tmpl_id.company_id or self.env.company
+        buy_route = self.env['stock.rule'].search([('action', '=', 'buy'), ('company_id', '=', company.id)]).route_id
+        if buy_route and product_tmpl_id.seller_ids:
+            domain = Domain.OR([domain, Domain('id', '=', buy_route.id)])
         return domain

addons/purchase_mrp/tests/test_replenishment.py
@@ -104,11 +104,16 @@ class TestReplenishment(TestStockCommon):
         })
         manufacture_route = self.warehouse_1.route_ids.filtered(lambda r: any(rule.action == 'manufacture' for rule in r.rule_ids))
         buy_route = self.warehouse_1.route_ids.filtered(lambda r: any(rule.action == 'buy' for rule in r.rule_ids))
+        self.productA.route_ids = False
 
         # No resupply methods should be set for Product A
         self.assertRecordValues(self.productA, [{'route_ids': self.env['stock.route'], 'seller_ids': self.env['product.supplierinfo'], 'bom_ids': self.env['mrp.bom']}])
         replenish_empty = create_replenish_wizard(self.warehouse_1, self.productA)
         self.assertFalse(replenish_empty.allowed_route_ids)
+
+        # Add the MTO route to the product, it should never show in allowed_route_ids
+        self.warehouse_1.mto_pull_id.route_id.active = True
+        self.productA.route_ids = self.warehouse_1.mto_pull_id.route_id
 
         # Add a BoM for Product A. This should make it eligible for Manufacture routes
         self.env['mrp.bom'].create({

addons/stock/models/stock_replenish_mixin.py
@@ -36,4 +36,5 @@ class StockReplenishMixin(models.AbstractModel):
             base_domain,
             Domain('rule_ids.location_src_id', '!=', stock_location_inter_company_id),
             Domain('rule_ids.location_dest_id', '!=', stock_location_inter_company_id),
+            Domain('rule_ids.location_dest_id.warehouse_id', '!=', False),
         ])

commit 87271cfedb797e1ed524061cb45becb225d27500

[FIX] mrp,purchase_stock,stock_dropshipping: protect wizard replenish regression
Fix 524bce9 caused a `Singleton Error`, which was then addressed in af141ba.

This commit improves the follow up fix and adds a test.

closes odoo/odoo#250983

Signed-off-by: William Henrotin (whe) <whe@odoo.com>

addons/mrp/wizard/product_replenish.py
@@ -46,7 +46,7 @@ class ProductReplenish(models.TransientModel):
     def _get_route_domain(self, product_tmpl_id):
         domain = super()._get_route_domain(product_tmpl_id)
         company = product_tmpl_id.company_id or self.env.company
-        manufacture_route = self.env['stock.rule'].search([('action', '=', 'manufacture'), ('company_id', '=', company.id)], limit=1).route_id
+        manufacture_route = self.env['stock.rule'].search([('action', '=', 'manufacture'), ('company_id', '=', company.id)]).route_id
         if manufacture_route and product_tmpl_id.bom_ids:
-            domain = Domain.OR([domain, Domain('id', '=', manufacture_route.id)])
+            domain = Domain.OR([domain, Domain('id', 'in', manufacture_route.ids)])
         return domain

addons/purchase_stock/wizard/product_replenish.py
@@ -91,7 +91,11 @@ class ProductReplenish(models.TransientModel):
     def _get_route_domain(self, product_tmpl_id):
         domain = super()._get_route_domain(product_tmpl_id)
         company = product_tmpl_id.company_id or self.env.company
-        buy_route = self.env['stock.rule'].search([('action', '=', 'buy'), ('company_id', '=', company.id)], limit=1).route_id
+        buy_route = self.env['stock.rule'].search([
+            ('action', '=', 'buy'),
+            ('company_id', '=', company.id),
+            ('picking_type_id.code', '=', 'incoming'),
+        ]).route_id
         if buy_route and product_tmpl_id.seller_ids:
-            domain = Domain.OR([domain, Domain('id', '=', buy_route.id)])
+            domain = Domain.OR([domain, Domain('id', 'in', buy_route.ids)])
         return domain

addons/mrp/tests/test_replenish.py
@@ -384,3 +384,15 @@ class TestMrpReplenish(TestMrpCommon):
             'product_id': self.bom_2.product_id.id,
         })
         self.assertEqual(orderpoint.company_id, company_2)
+
+    def test_product_replenish_wizard_multiple_manufacture_routes(self):
+        self.route_manufacture.copy()
+        wizard_form = Form(self.env['product.replenish'].with_context(
+            default_product_tmpl_id=self.product_4.product_tmpl_id.id
+        ))
+        manufacture_route = self.env['stock.rule'].search([
+            ('action', '=', 'manufacture'),
+            ('company_id', '=', self.company.id),
+            ('location_dest_id.usage', '=', 'internal'),
+        ], limit=1).route_id
+        self.assertEqual(wizard_form.route_id, manufacture_route)

addons/purchase_stock/tests/test_replenish_wizard.py
@@ -555,3 +555,21 @@ class TestReplenishWizard(PurchaseTestCommon):
         self.assertEqual(po.order_line.product_uom_id, replenish_wizard.product_uom_id, 'Generated PO line must respect the requested UOM from the wizard')
         self.assertEqual(po.order_line.price_unit, 5, 'Generated PO line must respect the supplier price of UoM "Pack of 6" because the quantity matches the "Pack of 6" pricelist')
         po.button_cancel()
+
+    def test_product_replenish_wizard_multiple_buy_routes(self):
+        self.route_buy.copy()
+        self.product.write({
+            'seller_ids': [Command.create({
+                'partner_id': self.vendor.id,
+            })]
+        })
+
+        replenish_wizard = Form(self.env['product.replenish'].with_context(
+            default_product_tmpl_id=self.product.product_tmpl_id.id
+        ))
+        buy_route = self.env['stock.rule'].search([
+            ('action', '=', 'buy'),
+            ('company_id', '=', self.company.id),
+            ('location_dest_id.usage', '=', 'internal'),
+        ], limit=1).route_id
+        self.assertEqual(replenish_wizard.route_id, buy_route)

addons/stock_dropshipping/tests/test_dropship.py
@@ -558,3 +558,20 @@ class TestDropshipPostInstall(common.TransactionCase):
         purchase_order.button_confirm()
         sale_order._action_cancel()
         self.assertEqual(len(purchase_order.activity_ids), 1)
+
+    def test_product_replenish_wizard_excludes_dropship_routes(self):
+        '''
+        Ensure the dropship route is not included in the replenish wizard.
+        '''
+        buy_route = self.env['stock.rule'].search([
+            ('action', '=', 'buy'),
+            ('company_id', '=', self.env.company.id),
+            ('location_dest_id.usage', '=', 'internal'),
+        ], limit=1).route_id
+        # Ensure no buy route so the widget tries to default to dropship
+        buy_route.action_archive()
+        self.dropship_product.route_ids = False
+        replenish_wizard = Form(self.env['product.replenish'].with_context(
+            default_product_tmpl_id=self.dropship_product.product_tmpl_id.id
+        ))
+        self.assertNotEqual(replenish_wizard.route_id, self.env.ref('stock_dropshipping.route_drop_shipping'))