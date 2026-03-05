2ed3af6abdb1dd48e1dd411de649ad928b0c13a9
adch-odoo <adch@odoo.com>
2026-03-05 12:22:22 +0000

    [FIX] html_editor: clear image shape classes when replacing with icon
    Steps to Reproduce:
    - Go to the To-do app and create a new task.
    - Upload an image.
    - Apply shape styling to the image (e.g., rounded, shadow, img-thumbnail).
    - Replace the image with an icon.
    
    Before this commit:
    
    - When an image had shape applied (such as rounded, rounded-circle,
     shadow, or img-thumbnail) and was replaced with an icon, those classes
    were carried over to the icon.
    
    After this commit:
    
    - Since these classes are specific to image shape styling, they are now
    removed when an image is replaced with an icon.
    
    task-6007631
    
    X-original-commit: 8638dbc21a7a3ebb3c9cc195d2249b4eb5c264ab

--- addons/html_editor/static/src/main/media/media_dialog/icon_selector.js
+++ addons/html_editor/static/src/main/media/media_dialog/icon_selector.js
@@ -7,15 +7,7 @@ import { Component } from "@odoo/owl";
 export class IconSelector extends Component {
     static mediaSpecificClasses = ["fa"];
     static mediaSpecificStyles = ["color", "background-color"];
-    static mediaExtraClasses = [
-        "rounded-circle",
-        "rounded",
-        "img-thumbnail",
-        "shadow",
-        /^text-\S+$/,
-        /^bg-\S+$/,
-        /^fa-\S+$/,
-    ];
+    static mediaExtraClasses = [/^text-\S+$/, /^bg-\S+$/, /^fa-\S+$/];
     static tagNames = ["SPAN", "I"];
     static template = "html_editor.IconSelector";
     static components = {
