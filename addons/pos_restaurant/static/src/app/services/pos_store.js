d0991d05f3008064d67521f0016ee5f734f26350
parp-odoo <parp@odoo.com>
2026-03-05 12:37:20 +0000

    [FIX] pos_restaurant: tb when merging a table with an empty order
    Steps to reproduce:
    - On an empty table, change the guest count
    - Create an order and send it to the kitchen
    - Open another table without an order
    - Merge the first table with the second one
    
    Issue:
    - A server error occurs during table merge
    
    Fix:
    - Wait for the merge order to sync before returning the result
    
    Task-5502511
    
    X-original-commit: a0067ede221d07b9c114c7307846501c93fbf923

--- addons/pos_restaurant/static/src/app/services/pos_store.js
+++ addons/pos_restaurant/static/src/app/services/pos_store.js
@@ -232,8 +232,10 @@ patch(PosStore.prototype, {
             }
         }
 
-        await this.deleteOrders([sourceOrder], [], true);
-        this.syncAllOrders({ orders: [destOrder] });
+        if (typeof destOrder.id === "number") {
+            await this.syncAllOrders({ orders: [destOrder] });
+        }
+        await this.deleteOrders([sourceOrder], [], typeof sourceOrder.id === "number");
         return destOrder;
     },
     mergeCourses(sourceOrder, destOrder) {
