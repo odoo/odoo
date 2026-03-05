3cad07a0d91b8e8c1bce7b77331c75bae3904cfc
Robin Engels (roen) <roen@odoo.com>
2026-03-05 17:20:40 +0000

    [FIX] pos_hr: make sure employey is correct
    When writing to a PoS config, it will automatically set an
    `advanced_employee_ids` if none is set. But it will take any employee
    that is part of `point_of_sale.group_pos_manager`. If the employee
    selected is not part of the same company as the PoS config, and the
    current user doesn't have HR employee access it will trigger an ir.rule
    that block the user from opening the settings.
    
    Steps to reproduce:
    -------------------
    * Create a new company
    * Create a new user that only have access to this company and no HR
      access
    * Create a PoS in the new company
    * Login as the new user
    * Try to open the settings
    > Observation: You will get an access error because the employee set in
      `advanced_employee_ids` is from the other company
    
    Why the fix:
    ------------
    We make sure that when automatically setting the advanced_employee_ids
    we filter out the ones that are not from the correct company. If no
    employee exist that satisfies the requirements, we take a user from the
    `group_pos_manager` and create an employee for him.
    
    opw-5885417
    
    closes odoo/odoo#254026
    
    X-original-commit: 361aa8505506f9686b1cdb244ba1723ee3f06f7b
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: Robin Engels (roen) <roen@odoo.com>

--- addons/pos_hr/tests/test_res_config_settings.py
+++ addons/pos_hr/tests/test_res_config_settings.py
@@ -20,3 +20,25 @@ class TestConfigureShopsPoSHR(TestPosHrHttpCommon, TestConfigureShops):
 
         self.assertListEqual(self.main_pos_config.basic_employee_ids.ids, [])
         self.assertListEqual(self.main_pos_config.minimal_employee_ids.ids, [])
+
+    def test_write_create_employee_if_none(self):
+        """This test make sure that the employee set on advanced_employee_ids is from
+           the current company. And if none exists it create one for the user."""
+
+        # First test that an employee is created if none exists
+        self.env['hr.employee'].search([]).unlink()
+        self.main_pos_config.with_context(from_settings_view=True).write({
+            'basic_employee_ids': [],
+        })
+
+        self.assertEqual(len(self.main_pos_config.advanced_employee_ids), 1)
+        self.assertEqual(self.main_pos_config.advanced_employee_ids.company_id, self.main_pos_config.company_id)
+        advanced_employee = self.main_pos_config.advanced_employee_ids
+
+        # Test that the previously employee is used
+        self.main_pos_config.with_context(from_settings_view=True).write({
+            'advanced_employee_ids': [],
+        })
+        self.assertEqual(len(self.main_pos_config.advanced_employee_ids), 1)
+        self.assertEqual(self.main_pos_config.advanced_employee_ids.company_id, self.main_pos_config.company_id)
+        self.assertEqual(self.main_pos_config.advanced_employee_ids, advanced_employee)
