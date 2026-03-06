715b1158974201963f5e9315ed4471e7850d4563
makb-odoo <makb@odoo.com>
2026-03-06 08:55:05 +0000

    [FIX] project: AccessError when project user sets partner on task
    Steps to Reproduce:
    1. Log in with a user having only Project > User access.
    2. Create a new task in project.
    3. Add a customer on the task.
    4. Access error is raised.
    
    Issue:
    - Project users could not create a task with a customer.
    - An access error appeared during task creation.
    
    Cause:
    - When a customer was added to the task, the partner_phone inverse method
      was triggered.
    - This method attempted to write on the partner record.
    
    Solution:
    - Added a check before writing to avoid unnecessary writes.
    - Used sudo() to update the partner phone securely.
    - Added view-level restriction using base.group_partner_manager to control
      who can edit the phone number.
    
    task-5039657
    
    closes odoo/odoo#256921
    
    X-original-commit: 9b071683e0eb270811302a4dd0ad10e0baaa0834
    Related: odoo/enterprise#112650
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>
    Signed-off-by: Manthan Dineshbhai Akbari (makb) <makb@odoo.com>

--- addons/project/models/project_task.py
+++ addons/project/models/project_task.py
@@ -655,8 +655,8 @@ class ProjectTask(models.Model):
 
     def _inverse_partner_phone(self):
         for task in self:
-            if task.partner_id:
-                task.partner_id.phone = task.partner_phone
+            if task.partner_id and task.partner_phone != task.partner_id.phone:
+                task.partner_id.sudo().phone = task.partner_phone
 
     @api.onchange('company_id')
     def _onchange_task_company(self):
