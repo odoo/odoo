333fe8b2b0844b569ad6471050ecadba92f1d3e6
Hesham Saleh <hsal@odoo.com>
2026-03-06 17:25:12 +0000

    [FIX] l10n_jo_edi_pos: Fix payment methods issues
    This commit fixes the following issues
    1) Having more than one payment method on a pos.order led to an error
    2) Customer should be required if payment method is receivable, or cash with amount > 10,000 JOD
    
    task-6005832
    
    X-original-commit: 82453ca16994ba4ec8bc08ced5d1911fa259c677

--- addons/l10n_jo_edi_pos/models/pos_order.py
+++ addons/l10n_jo_edi_pos/models/pos_order.py
@@ -50,8 +50,16 @@ class PosOrder(models.Model):
     def _get_order_scope_code(self):
         return '0'
 
+    def _l10n_jo_edi_pos_get_payment_type(self):
+        """
+        :returns: 'cash', 'receivable', or None if payments are of different types or missing.
+        """
+        is_cash_set = set(self.payment_ids.payment_method_id.mapped('l10n_jo_edi_pos_is_cash'))
+        if len(is_cash_set) == 1:
+            return 'cash' if is_cash_set.pop() else 'receivable'
+
     def _get_order_payment_method_code(self):
-        return '1' if any(self.payment_ids.payment_method_id.mapped('l10n_jo_edi_pos_is_cash')) else '2'
+        return '1' if self._l10n_jo_edi_pos_get_payment_type() == 'cash' else '2'
 
     def _get_order_tax_payer_type_code(self):
         return {
@@ -90,8 +98,14 @@ class PosOrder(models.Model):
                 error_msgs.append(self.env._("Refund order must have a return reason"))
         if any(line.price_unit < 0 for line in self.lines) or (not self.refunded_order_id and any(line.qty < 0 for line in self.lines)):
             error_msgs.append(self.env._("Downpayments, global discounts, and negative lines are not supported at the moment. To revert this order, please go to Orders > Select the Order > Refund or create a Return from the backend by going to Orders > Select the Order > Return"))
-        if len(self.payment_ids.payment_method_id.mapped('l10n_jo_edi_pos_is_cash')) > 1:
+        if not self._l10n_jo_edi_pos_get_payment_type():
             error_msgs.append(self.env._("Please select the payment methods that are consistent with the value set in 'JoFotara Cash'. If set, the payment method is Cash. If empty, then it is Receivable."))
+        else:
+            jod = self.env.ref('base.JOD')
+            amount_total_jod = self.amount_total if self.currency_id == jod else self.currency_id._convert(self.amount_total, jod, self.company_id, self.date_order)
+            amount_total_requires_customer = jod.compare_amounts(amount_total_jod, 10_000) == 1
+            if not self.partner_id and (self._l10n_jo_edi_pos_get_payment_type() == 'receivable' or amount_total_requires_customer):
+                error_msgs.append(self.env._("Customer is required on either Receivable or more than 10,000 JOD Cash Orders."))
 
         for line in self.lines:
             if self.company_id.l10n_jo_edi_taxpayer_type == 'income' and len(line.tax_ids) != 0:
