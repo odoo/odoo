9ec049fdc1c65140b0d7510d0d48139fc8209387
Yash Vaishnav <yava@odoo.com>
2026-03-06 12:34:18 +0000

    [FIX] l10_in_hr_holiday: fixed sandwich leave with public holiday
    Steps to Reproduce:
    
    1. Install the `l10n_in_hr_holidays` module.
    2. Enable the "sandwich leave" option for the time off type.
    3. Create public holidays that last the entire day, for example
    from 00:00 to 23:59.
    4. Create a leave around the public holiday
    5. Duration should be 3 days instead of 1
    
    Cause:
    
    When creating a dictionary for company-specific public holidays,
    the dates from and to are not converted to the company's timezone
    when calculating the days between public holidays.
    
    Fix:
    
    To resolve this, the first step is to localize the `date_from` and `date_to`
    to the company's timezone before counting the days between the public holidays.
    
    Task-6012992
    
    closes odoo/odoo#253267
    
    X-original-commit: 8c00553996819d82ef35ab619326c12b82702db7
    Signed-off-by: Romain Carlier (romc) <romc@odoo.com>
    Signed-off-by: Yash Vaishnav (yava) <yava@odoo.com>

--- addons/l10n_in_hr_holidays/models/hr_leave.py
+++ addons/l10n_in_hr_holidays/models/hr_leave.py
@@ -124,6 +124,10 @@ class HrLeave(models.Model):
             - Filters Indian, full-day, sandwich-enabled leaves.
             - Prepares dicts for sibling employee leaves and company public holidays.
         """
+        def _to_local_date(datetime, tz):
+            datetime = pytz.utc.localize(datetime, is_dst=False)
+            return datetime.astimezone(tz).date()
+
         indian_leaves = self.filtered(
             lambda leave: leave.company_id.country_id.code == "IN"
             and leave.holiday_status_id.l10n_in_is_sandwich_leave
@@ -153,22 +157,23 @@ class HrLeave(models.Model):
             }
 
         tz = pytz.timezone(self.env.context.get("tz") or self.env.user.tz or "UTC")
-        public_holidays_dates_by_company = {
-            company_id: {
-                (datetime.date(holiday.date_from.astimezone(tz)) + timedelta(days=offset)): holiday
-                for holiday in recs
-                for offset in range((holiday.date_to.date() - holiday.date_from.date()).days + 1)
-            }
-            for company_id, recs in self.env['resource.calendar.leaves']._read_group(
-                domain=[
-                    ('resource_id', '=', False),
-                    ('company_id', 'in', indian_leaves.company_id.ids),
-                ],
-                groupby=['company_id'],
-                aggregates=['id:recordset'],
-            )
-        }
-
+        public_holidays_dates_by_company = {}
+        for company_id, recs in self.env['resource.calendar.leaves']._read_group(
+            domain=[
+                ('resource_id', '=', False),
+                ('company_id', 'in', indian_leaves.company_id.ids),
+            ],
+            groupby=['company_id'],
+            aggregates=['id:recordset'],
+        ):
+            company_dates = {}
+            tz = pytz.timezone(company_id.resource_calendar_id.tz or self.env.context.get("tz") or self.env.user.tz or "UTC")
+            for holiday in recs:
+                local_start = _to_local_date(holiday.date_from, tz)
+                local_end = _to_local_date(holiday.date_to, tz)
+                for offset in range((local_end - local_start).days + 1):
+                    company_dates[local_start + timedelta(days=offset)] = holiday
+            public_holidays_dates_by_company[company_id] = company_dates
         return indian_leaves, leaves_dates_by_employee, public_holidays_dates_by_company
 
     def _l10n_in_apply_sandwich_rule(self, public_holidays_date_by_company, leaves_dates_by_employee):
