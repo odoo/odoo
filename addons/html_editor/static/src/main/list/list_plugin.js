375b886c44fece25ad0ea565c3d6e20cd22b352a
dhba <dhba@odoo.com>
2026-03-06 17:14:04 +0530

    [FIX] html_editor: prevent traceback with invalid nested list html
    Steps to reproduce:
    - Open a record containing the following invalid HTML:
    `<ul>
        <li>1</li>
        <li class=oe-nested></li>
    </ul>`
    
    Issue:
    - A traceback occurs when opening a record with this html.
    
    Cause:
    
    The provided HTML is invalid because an <li> element must contain at least one
    child node. In this case, the `<li>` element has no children. As a result, when
    isListElement tries to access the nodeName of its child, a traceback occurs
    because the `<li>` has no child node.
    
    Solution:
    
    During normalization. If an `<li>` element has no children, call `fillEmpty` so
    that a `<br>` element is inserted. This ensures the `<li>` contains at least one
    child and is no longer invalid.
    
    task-6012663

--- addons/html_editor/static/src/main/list/list_plugin.js
+++ addons/html_editor/static/src/main/list/list_plugin.js
@@ -1,7 +1,13 @@
 import { reactive } from "@web/owl2/utils";
 import { Plugin } from "@html_editor/plugin";
 import { closestBlock, isBlock } from "@html_editor/utils/blocks";
-import { removeClass, removeStyle, toggleClass, unwrapContents } from "@html_editor/utils/dom";
+import {
+    fillEmpty,
+    removeClass,
+    removeStyle,
+    toggleClass,
+    unwrapContents,
+} from "@html_editor/utils/dom";
 import {
     getDeepestEditablePosition,
     getDeepestPosition,
@@ -317,6 +323,9 @@ export class ListPlugin extends Plugin {
             root = closestNestedLI.parentElement;
         }
         for (let element of selectElements(root, "ul, ol, li")) {
+            if (element.nodeName === "LI" && !element.hasChildNodes()) {
+                fillEmpty(element);
+            }
             if (isProtected(element) || isProtecting(element)) {
                 continue;
             }
