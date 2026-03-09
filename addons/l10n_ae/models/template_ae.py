a0a31713d5e55cfa788ff028e38c045ac99c8ce4
khfa <khfa@odoo.com>
2026-03-09 16:49:50 +0400

    [IMP] l10n_ae: chart of accounts redesign To address the market feedback, few changes were made to the ae chart of accounts. The following are the core improvements and corrections:
      - Overall, sequencing changed to reflect reverse liquidity (from fixed/non-current assets and liabilities to cash/payables).
    
      - We are adding parent accounts (21) to structure CoA and allow flexibility in report building
    
      - Standard sequencing reworked: 6-digit code, where the first 2 digits mark the account group,
        the second 2 digits mark the subgroup, and the last 2 digits make the account ID.
        For example: 130102 - 13 - prepayments, 01 - insurance prepayments, 02 - life insurance prepayment
    
      - Equity accounts are reworked - new accounts are added.
    
      - Minor name corrections to make them more generic
    
    task-5455978

--- addons/l10n_ae/models/template_ae.py
+++ addons/l10n_ae/models/template_ae.py
@@ -26,22 +26,22 @@ class AccountChartTemplate(models.AbstractModel):
         return {
             self.env.company.id: {
                 'account_fiscal_country_id': 'base.ae',
-                'bank_account_code_prefix': '101',
+                'bank_account_code_prefix': '1601',
                 'cash_account_code_prefix': '105',
-                'transfer_account_code_prefix': '100',
-                'account_default_pos_receivable_account_id': 'uae_account_102012',
-                'income_currency_exchange_account_id': 'uae_account_500011',
-                'expense_currency_exchange_account_id': 'uae_account_400053',
-                'account_journal_early_pay_discount_loss_account_id': 'uae_account_400071',
-                'account_journal_early_pay_discount_gain_account_id': 'uae_account_500014',
+                'transfer_account_code_prefix': '1701',
+                'account_default_pos_receivable_account_id': 'uae_account_150200',
+                'income_currency_exchange_account_id': 'uae_account_510200',
+                'expense_currency_exchange_account_id': 'uae_account_430700',
+                'account_journal_early_pay_discount_loss_account_id': 'uae_account_430800',
+                'account_journal_early_pay_discount_gain_account_id': 'uae_account_510500',
                 'account_sale_tax_id': sales_tax_xmlid,
                 'account_purchase_tax_id': 'uae_purchase_tax_5',
-                'expense_account_id': 'uae_account_400001',
-                'income_account_id': 'uae_account_500001',
-                'receivable_account_id': 'uae_account_102011',
-                'payable_account_id': 'uae_account_201002',
+                'expense_account_id': 'uae_account_400100',
+                'income_account_id': 'uae_account_500100',
+                'receivable_account_id': 'uae_account_150100',
+                'payable_account_id': 'uae_account_230100',
                 'tax_calculation_rounding_method': 'round_per_line',
-                'account_stock_valuation_id': 'uae_account_131100',
+                'account_stock_valuation_id': 'uae_account_120100',
             },
         }
 
@@ -62,7 +62,10 @@ class AccountChartTemplate(models.AbstractModel):
                 "type": "general",
                 "show_on_dashboard": True,
                 "sequence": 11,
-            }
+            },
+            "bank": {
+                "default_account_id": "uae_account_160100",
+            },
         }
 
     @template('ae', 'account.fiscal.position')
@@ -85,15 +88,15 @@ class AccountChartTemplate(models.AbstractModel):
     @template('ae', 'account.account')
     def _get_ae_account_account(self):
         return {
-            'uae_account_106002': {'asset_depreciation_account_id': 'uae_account_106002', 'asset_expense_account_id': 'uae_account_400064'},
-            'uae_account_106003': {'asset_depreciation_account_id': 'uae_account_106003', 'asset_expense_account_id': 'uae_account_400065'},
-            'uae_account_106004': {'asset_depreciation_account_id': 'uae_account_106004', 'asset_expense_account_id': 'uae_account_400066'},
-            'uae_account_106013': {'asset_depreciation_account_id': 'uae_account_106013', 'asset_expense_account_id': 'uae_account_400092'},
-            'uae_account_106014': {'asset_depreciation_account_id': 'uae_account_106014', 'asset_expense_account_id': 'uae_account_400093'},
-            'uae_account_106015': {'asset_depreciation_account_id': 'uae_account_106015', 'asset_expense_account_id': 'uae_account_400094'},
-            'uae_account_106016': {'asset_depreciation_account_id': 'uae_account_106016', 'asset_expense_account_id': 'uae_account_400095'},
-            'uae_account_106017': {'asset_depreciation_account_id': 'uae_account_106017', 'asset_expense_account_id': 'uae_account_400096'},
-            'uae_account_131100': {
-                'account_stock_variation_id': 'uae_account_400001',
+            'uae_account_110800': {'asset_depreciation_account_id': 'uae_account_110800', 'asset_expense_account_id': 'uae_account_424100'},
+            'uae_account_111000': {'asset_depreciation_account_id': 'uae_account_111000', 'asset_expense_account_id': 'uae_account_424200'},
+            'uae_account_111200': {'asset_depreciation_account_id': 'uae_account_111200', 'asset_expense_account_id': 'uae_account_424300'},
+            'uae_account_110200': {'asset_depreciation_account_id': 'uae_account_110200', 'asset_expense_account_id': 'uae_account_424400'},
+            'uae_account_110300': {'asset_depreciation_account_id': 'uae_account_110300', 'asset_expense_account_id': 'uae_account_424500'},
+            'uae_account_110400': {'asset_depreciation_account_id': 'uae_account_110400', 'asset_expense_account_id': 'uae_account_424600'},
+            'uae_account_111600': {'asset_depreciation_account_id': 'uae_account_111600', 'asset_expense_account_id': 'uae_account_424700'},
+            'uae_account_110500': {'asset_depreciation_account_id': 'uae_account_110500', 'asset_expense_account_id': 'uae_account_424800'},
+            'uae_account_120100': {
+                'account_stock_variation_id': 'uae_account_400100',
             },
         }
