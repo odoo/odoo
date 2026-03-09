40f2a1e87e2f5c4750b9fe2a6926168f03d166f5
Robin Engels (roen) <roen@odoo.com>
2026-03-09 11:59:51 +0100

    [REV] pos_sale,sale: set tax on down payment
    This reverts commit d96fe780b9bfea1c9d7161b6f6f2239c5d674c7b.

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -6927,7 +6927,3 @@ class AccountMove(models.Model):
         :returns: True if commit is acceptable, False otherwise.
         """
         return not modules.module.current_test
-
-    @api.model
-    def require_tax_ids_on_invoice_lines(self):
-        return False
