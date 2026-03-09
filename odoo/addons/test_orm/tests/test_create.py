57cb45b09ef061229e07acc6bd68cee1647bb70c
Ruben Gomes (rugo) <rugo@odoo.com>
2026-03-09 13:13:42 +0100

    test_create

--- odoo/addons/test_orm/tests/test_create.py
+++ odoo/addons/test_orm/tests/test_create.py
@@ -8,15 +8,15 @@ class TestCreate(TransactionCase):
         vals_list = [{'foo': foo} for foo in ('Foo', 'Bar', 'Baz')]
         vals_list[0]['text'] = 'TEXT EXAMPLE'
         for vals in vals_list:
-            record = self.env['test_orm.mixed'].create(vals)
+            record = self.env['test_create.mixed'].create(vals)
             self.assertEqual(len(record), 1)
             self.assertEqual(record.foo, vals['foo'])
             self.assertEqual(record.text, vals.get('text', False))
 
-        records = self.env['test_orm.mixed'].create([])
+        records = self.env['test_create.mixed'].create([])
         self.assertFalse(records)
 
-        records = self.env['test_orm.mixed'].create(vals_list)
+        records = self.env['test_create.mixed'].create(vals_list)
         self.assertEqual(len(records), len(vals_list))
         for record, vals in zip(records, vals_list):
             self.assertEqual(record.foo, vals['foo'])
