495f07270b146c4f4aec4bbe8ff2393e02dd440b
adip-odoo <adip@odoo.com>
2026-03-10 13:18:30 +0000

    [FIX] l10n_my_edi_pos: fix year-range sequence error in MyInvois
    Currently an error is produced when sending invoices to MyInvois if the invoice
    number uses a year-range sequence (e.g., INV/2025-2026/00001).
    
    Steps to Reproduce(v-19.0):
    1. Install the `accountant` and `l10n_my_edi` modules (with demo data).
    2. Switch to "MY Company"(Malaysian company).
    3. Enable "Quick Encoding" for Customer Invoices in Settings.
    4. Create a customer invoice with customer "MY Company", set a Malaysian
       classification code and taxes on the invoice line, and confirm the invoice.
    5. Set the invoice back to Draft and modify the invoice number with a
       year-range sequence (e.g., INV/2025-2026/00001), then confirm it again.
    6. Open the invoice list view and click "Send to MyInvois".
    
    Error:
    ValueError: not enough values to unpack (expected 4, got 2)
    
    The `_get_sequence_date_range()` method on 'myinvois.document' overrides the
    method from `sequence.mixin` and returns only two values from
    `date_utils.get_fiscal_year()`. However, it expects the method to return four
    values at [1].
    
    [1]: https://github.com/odoo/odoo/blob/57b6b8d63b038ede32dfcc833c30e93d0cf4166c/addons/account/models/sequence_mixin.py#L146
    Ref: https://github.com/odoo/odoo/blob/1ce06257f877711bd5de5487364909d72b476318/addons/account/models/account_move.py#L4263
    
    sentry-7320998540
    
    closes odoo/odoo#266634
    
    X-original-commit: 0683fb59839e3214d67c440844ed760a71e64cfc
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: Aditi Patel (adip) <adip@odoo.com>

--- addons/l10n_my_edi_pos/tests/test_myinvois_pos.py
+++ addons/l10n_my_edi_pos/tests/test_myinvois_pos.py
@@ -956,6 +956,24 @@ class TestMyInvoisPoS(TestPoSCommon, HttpCase):
             f"Unexpected MyInvois state: {myinvois_document.myinvois_state}",
         )
 
+    def test_consolidate_invoices_with_year_range_sequence(self):
+        with freeze_time("2026-01-01"):
+            # Create the orders
+            with self.with_pos_session():
+                first_order = self._create_order({'pos_order_lines_ui_args': [(self.product_one, 1.0)]})
+            # Consolidate them
+            wizard = self.env['myinvois.consolidate.invoice.wizard'].create({
+                'date_from': '2026-01-01',
+                'date_to': '2026-01-31',
+                'consolidation_type': 'pos',
+            })
+            wizard.button_consolidate()
+            consolidated_invoice = first_order.consolidated_invoice_ids
+            consolidated_invoice.name = "POS/2025-2026/000001"
+            with patch(CONTACT_PROXY_METHOD, new=self._mock_successful_submission):
+                consolidated_invoice.action_submit_to_myinvois()
+            self.assertTrue(consolidated_invoice.myinvois_file_id)
+
     #################
     # Patched methods
     #################
