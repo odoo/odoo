a2e0b67d5d91008211da56aad121a307486f02a3
David Monjoie <dmo@odoo.com>
2026-03-10 14:06:12 +0100

    [FIX] html_editor: fix non-deterministic html field test
    The second `querySelector` call did not wait for the editable to
    be loaded after switching back to the first tab. Most of the time
    the editor loads fast enough, but when the runbot is slower it
    might not be loaded yet, at which point the `editable` variable
    might not be defined.
    
    runbot-241258
    
    closes odoo/odoo#253051
    
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/html_editor/static/tests/html_field.test.js
+++ addons/html_editor/static/tests/html_field.test.js
@@ -2622,6 +2622,7 @@ describe("save image", () => {
         await delay(50);
         // reswitch tab, and check the image was saved properly.
         await contains(".o_notebook_headers .nav-link:not(.active)").click();
+        await waitFor(".odoo-editor-editable");
         const savedImg = htmlEditor.editable.querySelector("img");
         expect(savedImg.getAttribute("src")).toBe("/test_image_url.png?access_token=1234");
         expect(savedImg).not.toHaveClass("o_b64_image_to_save");
