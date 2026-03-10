265c3dfd54a0dbaa99d973dc8d8a0ce4998cc240
Léo Gizard <legi@odoo.com>
2026-03-10 08:56:26 +0000

    [FIX] account_peppol_selfbilling: Use delivery contact on selfbilling
    **PROBLEM**
    When selfbilling with peppol, we have no way of providing a GLN number, or modifying the delivery address.
    Even if we create a delivery address partner on the current company partner, it's not taken into account.
    
    **STEP TO REPRODUCE**
    1. Create a delivery address on the current company, set up a GLN number.
    2. Configure the purchase journal to do selfbilling.
    3. Create a vendor bill with this journal and send it using peppol.
    4. Download the xml, and look for the Delivery tag, and notice it doesn't have the GLN number.
    
    **FIX**
    We search for a delivery address on the current company.
    If there is one, we use it for the Delivery tag.
    
    opw-6014374
    
    X-original-commit: 50dc626df57f2784058163c66e54b5f6b8edfdd8

--- addons/account_edi_ubl_cii/tests/test_ubl_export_bis3_selfbilling_be.py
+++ addons/account_edi_ubl_cii/tests/test_ubl_export_bis3_selfbilling_be.py
@@ -34,7 +34,17 @@ class TestUblExportBis3SelfbillingBE(TestUblBis3Common, TestUblCiiBECommon):
     def subfolder(self):
         return super().subfolder().replace('export', 'export/bis3/invoice_selfbilling')
 
-    def test_invoice_selfbilling(self):
+    def test_export_selfbilling(self):
+        self.env['res.partner'].create({
+            'name': 'custom delivery address',
+            'parent_id': self.company.partner_id.id,
+            'type': 'delivery',
+            'street': 'Chaussée de Namur 40',
+            'city': 'Ramillies',
+            'zip': '1367',
+            'global_location_number': '0123456789',
+            'country_id': self.ref('base.be'),
+        })
         tax_21 = self.percent_tax(21.0, type_tax_use='purchase')
         product = self._create_product(standard_price=100.0, supplier_taxes_id=tax_21.ids)
         invoice = self._create_invoice_one_line(
