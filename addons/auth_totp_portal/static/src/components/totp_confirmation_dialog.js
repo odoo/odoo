37e8f96097be123965abce8ca875975972db2a0a
Julien Launois (jula) <jula@odoo.com>
2026-03-11 09:31:04 +0100

    [FIX] auth_totp_portal: fix secret copy button
    __Problem__
    Since odoo/odoo@e3da5f1 the onclick listener set on `copyButton` is lost
    because we give the HTML of the body as argument at the dialog creation.
    
    __Steps to reproduce__
    1. Go to `/my/security`
    2. Click on "Enable two-factor authentication"
    3. Confirm password
    4. Click on "Cannot scan it?"
    5. The "Copy" button doesn't work
    
    __Fix__
    - Inherit from `InputConfirmationDialog` to add a listener to the
      button.
    - At the same time, remove the remaining jQuery dependency in this part
      of the code
    
    closes odoo/odoo#251852
    
    X-original-commit: c33698215cb8a245a19e2ec00414fdc0437cddf4
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Julien Launois (jula) <jula@odoo.com>

---
+++ addons/auth_totp_portal/static/src/components/totp_confirmation_dialog.js
@@ -0,0 +1,38 @@
+/** @odoo-module **/
+
+import { useEffect } from "@odoo/owl";
+import { InputConfirmationDialog } from "@portal/js/components/input_confirmation_dialog/input_confirmation_dialog";
+import { browser } from "@web/core/browser/browser";
+import { _t } from "@web/core/l10n/translation";
+import { usePopover } from "@web/core/popover/popover_hook";
+import { Tooltip } from "@web/core/tooltip/tooltip";
+
+/**
+ * This is a quick-and-dirty fix to enable the copy of the TOTP secret in the 
+ * portal.
+ */
+export class TotpConfirmationDialog extends InputConfirmationDialog {
+    setup() {
+        super.setup();
+        this.tooltip = usePopover(Tooltip, { position: "bottom" });
+
+        const onClickClipboardButton = async (ev) => {
+            ev.preventDefault();
+            const clipboardButtonEl = ev.currentTarget;
+            const secretSpan = this.modalRef.el.querySelector("span[name='secret']");
+            browser.navigator.clipboard.writeText(secretSpan.textContent).then(() => {
+                this.tooltip.open(clipboardButtonEl, { tooltip: _t("Copied!") });
+                setTimeout(this.tooltip.close, 800);
+            });
+        };
+        useEffect(
+            (clipboardButtonEl) => {
+                if (clipboardButtonEl) {
+                    clipboardButtonEl.addEventListener("click", onClickClipboardButton);
+                    return () => clipboardButtonEl.removeEventListener("click", onClickClipboardButton);
+                }
+            },
+            () => [this.modalRef.el?.querySelector("#collapseTotpSecret .o_clipboard_button")]
+        );
+    }
+}
