2ce41e21c2645074ce85409fea643a0cd545ebeb
Benoit Socias (bso) <bso@odoo.com>
2026-03-11 13:11:39 +0100

    [IMP] html_editor: automatically expand toolbar in mobile
    This commit always keeps the toolbar in mobile expanded.
    It also adds a fading effect at the end of the mobile toolbar to help
    discoverability.
    
    task-6009844

--- addons/html_editor/static/tests/toolbar.test.js
+++ addons/html_editor/static/tests/toolbar.test.js
@@ -1513,11 +1513,19 @@ test("toolbar does not evaluate isActive when namespace does not match", async (
 });
 
 describe("compact toolbar", () => {
+    test.tags("desktop");
     test("toolbar opens in 'compact' namespace by default", async () => {
         await setupEditor("<p>[test]</p>");
         await waitFor(".o-we-toolbar");
         expect(".o-we-toolbar").toHaveAttribute("data-namespace", "compact");
         await expandToolbar();
+        expect(".o-we-toolbar").toHaveAttribute("data-namespace", "expanded");
+    });
+
+    test.tags("mobile");
+    test("toolbar opens in 'expanded' namespace by default on mobile", async () => {
+        await setupEditor("<p>[test]</p>");
+        await waitFor(".o-we-toolbar");
         expect(".o-we-toolbar").toHaveAttribute("data-namespace", "expanded");
     });
 
@@ -1545,6 +1553,7 @@ describe("compact toolbar", () => {
     });
     const repeat = (count, fn) => range(count).map(fn);
 
+    test.tags("desktop");
     test("toolbar should not open in compact mode if expanded toolbar has less than 7 items", async () => {
         class TestPlugin extends Plugin {
             static id = "TestPlugin";
@@ -1566,6 +1575,7 @@ describe("compact toolbar", () => {
         await waitFor(".o-we-toolbar");
         expect(".o-we-toolbar").toHaveAttribute("data-namespace", "expanded");
     });
+    test.tags("desktop");
     test("toolbar should open in compact mode if expanded toolbar is big enough (>= 7 items)", async () => {
         class TestPlugin extends Plugin {
             static id = "TestPlugin";
@@ -1587,6 +1597,7 @@ describe("compact toolbar", () => {
         await waitFor(".o-we-toolbar");
         expect(".o-we-toolbar").toHaveAttribute("data-namespace", "compact");
     });
+    test.tags("desktop");
     test("toolbar should not open in compact mode if expanded toolbar has only one extra item", async () => {
         class TestPlugin extends Plugin {
             static id = "TestPlugin";
@@ -1608,6 +1619,7 @@ describe("compact toolbar", () => {
         await waitFor(".o-we-toolbar");
         expect(".o-we-toolbar").toHaveAttribute("data-namespace", "expanded");
     });
+    test.tags("desktop");
     test("toolbar should open in compact mode if expanded toolbar has more than one extra item", async () => {
         class TestPlugin extends Plugin {
             static id = "TestPlugin";
@@ -1650,6 +1662,7 @@ test("expanded toolbar reopens in 'compact' namespace by default after closing",
     expect(".o-we-toolbar").toHaveAttribute("data-namespace", "compact");
 });
 
+test.tags("desktop");
 test("toolbar items without namespace default to 'expanded'", async () => {
     class TestPlugin extends Plugin {
         static id = "TestPlugin";
@@ -1678,6 +1691,7 @@ test("toolbar items without namespace default to 'expanded'", async () => {
     expect(".o-we-toolbar .btn[name='test_btn']").toHaveCount(1);
 });
 
+test.tags("desktop");
 test("toolbar should open with image namespace the selection spans an image and whitespace", async () => {
     const { el } = await setupEditor(`<p>[abc]</p>`);
     // Make sure we start with a compact toolbar so we know that at the end when
@@ -1686,6 +1700,27 @@ test("toolbar should open with image namespace the selection spans an image and 
     await animationFrame();
     await expectElementCount(".o-we-toolbar", 1);
     expect(".o-we-toolbar").toHaveAttribute("data-namespace", "compact");
+    expect(".o-we-toolbar .btn-group[name='font']").toHaveCount(1);
+    expect(".o-we-toolbar .btn-group[name='decoration']").toHaveCount(1);
+    setContent(
+        el,
+        `<p>[
+            <img>
+        ]</p>`
+    );
+    await waitFor(".o-we-toolbar[data-namespace='image']");
+    expect(".o-we-toolbar").toHaveAttribute("data-namespace", "image");
+    expect(".o-we-toolbar .btn-group[name='font']").toHaveCount(0);
+    expect(".o-we-toolbar .btn-group[name='decoration']").toHaveCount(0);
+});
+
+test.tags("mobile");
+test("toolbar should open with image namespace the selection spans an image and whitespace (mobile)", async () => {
+    const { el } = await setupEditor(`<p>[abc]</p>`);
+    // In mobile toolbar starts as expanded.
+    await animationFrame();
+    await expectElementCount(".o-we-toolbar", 1);
+    expect(".o-we-toolbar").toHaveAttribute("data-namespace", "expanded");
     expect(".o-we-toolbar .btn-group[name='font']").toHaveCount(1);
     expect(".o-we-toolbar .btn-group[name='decoration']").toHaveCount(1);
     setContent(
