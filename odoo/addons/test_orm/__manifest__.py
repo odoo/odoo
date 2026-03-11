c9e1ff8b71cc8dfcd12784f9960b59ca3f1bde0e
Ruben Gomes (rugo) <rugo@odoo.com>
2026-03-11 11:25:15 +0100

    test_schema

--- odoo/addons/test_orm/__manifest__.py
+++ odoo/addons/test_orm/__manifest__.py
@@ -50,6 +50,7 @@
         'security/test_read_group_private/ir.model.access.csv',
         'security/test_read_grouping_sets/ir.model.access.csv',
         'security/test_related_translation/ir.model.access.csv',
+        'security/test_schema/ir.model.access.csv',
 
         # VIEWS
     ],
