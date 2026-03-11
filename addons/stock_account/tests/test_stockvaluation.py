odoo/odoo

commit db133acfb8dad35217946f20d2d37b903ff1722b

[FIX] stock_account: closing same days
usecase (all during the same day):
- Create a product avco periodic, set cost to 100$
- Set an account on inventory loss location
- Do an inventory to 10 units
- Do an inventory closing (in accountant) (1)
- Do anoher inventory and add 10 units again
- Do another closing.

Expected behavior:
As in step (1) we expected an entry from inventory loss account to stock
valuation account. However we get an entry from stock variation account
to stock valuation account.

It happens because the account.move uses a date field. So when we close,
we don't expect to have anohter closing the same day. So the system skip
the account location part and directly create an entry with the
difference between the stock estimated valuation and the account stock
valuation as a variation.

closes odoo/odoo#238510

Signed-off-by: William Henrotin (whe) <whe@odoo.com>

addons/stock_account/models/res_company.py
@@ -51,7 +51,7 @@ class ResCompany(models.Model):
         if at_date and isinstance(at_date, str):
             at_date = fields.Date.from_string(at_date)
         last_closing_date = self._get_last_closing_date()
-        if at_date and last_closing_date and at_date < last_closing_date:
+        if at_date and last_closing_date and at_date < fields.Date.to_date(last_closing_date):
             raise UserError(self.env._('It exists closing entries after the selected date. Cancel them before generate an entry prior to them'))
         aml_vals_list = self._action_close_stock_valuation(at_date=at_date)
 
@@ -179,6 +179,7 @@ class ResCompany(models.Model):
             moves_base_domain &= Domain([('date', '<=', at_date)])
         moves_in_domain = Domain([
             ('is_out', '=', True),
+            ('company_id', '=', self.id),
             ('location_dest_id', 'in', valued_location.ids),
         ]) & moves_base_domain
         moves_in_by_location = self.env['stock.move']._read_group(
@@ -188,6 +189,7 @@ class ResCompany(models.Model):
         )
         moves_out_domain = Domain([
             ('is_in', '=', True),
+            ('company_id', '=', self.id),
             ('location_id', 'in', valued_location.ids),
         ]) & moves_base_domain
         moves_out_by_location = self.env['stock.move']._read_group(
@@ -327,7 +329,11 @@ class ResCompany(models.Model):
             closing_id = closing_ids.pop(-1)
             closing_id = int(closing_id)
             closing = self.env['account.move'].browse(closing_id).exists().filtered(lambda am: am.state == 'posted')
-        return closing.date if closing else False
+        if not closing:
+            return False
+        am_state_field = self.env['ir.model.fields'].search([('model', '=', 'account.move'), ('name', '=', 'state')], limit=1)
+        state_tracking = closing.message_ids.tracking_value_ids.filtered(lambda t: t.field_id == am_state_field).sorted('id')
+        return state_tracking[-1:].create_date or fields.Datetime.to_datetime(closing.date)
 
     def _save_closing_id(self, move_id):
         self.ensure_one()

addons/stock_account/tests/common.py
@@ -285,6 +285,10 @@ class TestStockValuationCommon(BaseCommon):
         cls.stock_location = cls.warehouse.lot_stock_id
         cls.customer_location = cls.env.ref('stock.stock_location_customers')
         cls.supplier_location = cls.env.ref('stock.stock_location_suppliers')
+        cls.inventory_location = cls.env['stock.location'].search([
+            ('usage', '=', 'inventory'),
+            ('company_id', '=', cls.company.id)
+        ], limit=1)
 
         cls.account_expense = cls.company.expense_account_id
         cls.account_stock_valuation = cls.company.account_stock_valuation_id

addons/stock_account/tests/test_account_move.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
+from datetime import timedelta
 from freezegun import freeze_time
 
 from odoo.addons.stock_account.tests.common import TestStockValuationCommon
@@ -313,3 +313,40 @@ class TestAccountMove(TestStockValuationCommon):
             receipt.scheduled_date = prior_to_lock_date
         with self.assertRaises(UserError):
             receipt_done.date_done = prior_to_lock_date
+
+    def test_closing_same_day(self):
+        product = self.product_avco
+        product.standard_price = 10.0
+        self._use_inventory_location_accounting()
+        with freeze_time(fields.Datetime.now() - timedelta(seconds=10)):
+            self._make_in_move(product, 10, location_id=self.inventory_location.id)
+            mail_context = {
+                'tracking_disable': False,
+                'mail_create_nolog': False,
+                'mail_create_nosubscribe': False,
+                'mail_notrack': False,
+            }
+            action = self.company.with_context(mail_context).action_close_stock_valuation()
+            closing = self.env['account.move'].with_context(mail_context).browse(action['res_id'])
+            # First flush to clean precommit data
+            self.env.cr.flush()
+            closing._post()
+            # Second flush to post the tracking values
+            self.env.cr.flush()
+            # Update the tracking value create_date since it used the cursor one and not the frozen time
+            am_state_field = self.env['ir.model.fields'].search(
+                [('model', '=', 'account.move'), ('name', '=', 'state')], limit=1)
+            state_tracking = closing.message_ids.tracking_value_ids.filtered(
+                lambda t: t.field_id == am_state_field)
+            state_tracking.create_date = fields.Datetime.now()
+            self.assertRecordValues(closing.line_ids, [
+                {'account_id': self.account_inventory.id, 'debit': 0.0, 'credit': 100.0},
+                {'account_id': self.account_stock_valuation.id, 'debit': 100.0, 'credit': 0.0},
+            ])
+
+        self._make_in_move(product, 10, location_id=self.inventory_location.id)
+        closing = self._close()
+        self.assertRecordValues(closing.line_ids, [
+            {'account_id': self.account_inventory.id, 'debit': 0.0, 'credit': 100.0},
+            {'account_id': self.account_stock_valuation.id, 'debit': 100.0, 'credit': 0.0},
+        ])

commit 6ef19a606e7ed6641b2af717a1f777ce59deb0cd

[FIX] stock_account: use accounting date for stock valuation closing validation
**Issue**:
Making two stock valuation closings with different accounting dates in the past,
on the same day, leads to an "Invalid Operation" error.

**Steps to reproduce**:
- In settings, set inventory valuation at invoicing
- Create 2 bills of a storable product:
	- `Accounting date` = `bill date` = today - 2 days
	- `Accounting date` = `bill date` = today - 4 days
- Go to Accounting > Review > Inventory > Inventory Valuation
- Set the day to today - 3 days, generate and post the entry
- Go back the Inventory Valuation
- Set the day to today - 1 day, generate the entry
-> Invalid Operation error: the system thinks it exists a closing entry after the selected date.

**Cause**:
The regression was introduced by commit db133acfb8dad35217946f20d2d37b903ff1722b
Indeed, `_get_last_closing_date` was modified to prioritize the creation date
of the closing instead of its accounting date:
https://github.com/odoo/odoo/blob/db133acfb8dad35217946f20d2d37b903ff1722b/addons/stock_account/models/res_company.py#L336

As a consequence, the second closing with at_date = today - 1 day
was compared against last_closing_date = today (creation date):
https://github.com/odoo/odoo/blob/ec7f3477d472fb0bc01d605fc2b50828d8392442/addons/stock_account/models/res_company.py#L53-L55

**Solution**
To avoid to revert this commit db133acfb8dad35217946f20d2d37b903ff1722b fix, both creation and accounting date are used.

opw-5559264

closes odoo/odoo#249734

Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

addons/stock_account/models/res_company.py
@@ -340,7 +340,10 @@ class ResCompany(models.Model):
             return False
         am_state_field = self.env['ir.model.fields'].search([('model', '=', 'account.move'), ('name', '=', 'state')], limit=1)
         state_tracking = closing.message_ids.sudo().tracking_value_ids.filtered(lambda t: t.field_id == am_state_field).sorted('id')
-        return state_tracking[-1:].create_date or fields.Datetime.to_datetime(closing.date)
+        create_date = state_tracking[-1:].create_date
+        if create_date and create_date.date() == closing.date:
+            return create_date
+        return fields.Datetime.to_datetime(closing.date)
 
     def _save_closing_id(self, move_id):
         self.ensure_one()

addons/stock_account/tests/test_stockvaluation.py
@@ -764,6 +764,43 @@ class TestStockValuation(TestStockValuationCommon):
         self._make_in_move(product, 1, unit_cost=77)
         self.assertEqual(product.standard_price, 77)
 
+    def test_post_multiple_stock_valuation_closings_in_past(self):
+        """
+        Ensure multiple stock valuation closings with different accounting dates in the past
+        can be posted.
+
+        Example scenario:
+        - Move 1: Accounting date = today - 4 days
+        - Move 2: Accounting date = today - 2 days
+        - First closing: at_date = today - 3 days
+        - Second closing: at_date = today - 1 day
+        Both closings should post successfully and calculate the correct valuation amounts.
+        """
+        self.env = self.env['base'].with_context(
+            tracking_disable=False,
+            mail_create_nolog=False,
+            mail_notrack=False,
+        ).env
+
+        product = self.product_fifo
+        today = Datetime.now()
+        with freeze_time(today - timedelta(days=4)):
+            move = self._make_in_move(product, 1, 10, create_picking=True, owner=self.env.company.partner_id)
+        with freeze_time(today - timedelta(days=2)):
+            move_2 = self._make_in_move(product, 1, 10, create_picking=True, owner=self.env.company.partner_id)
+
+        closing_move = self.env['account.move'].browse(move.company_id.action_close_stock_valuation(at_date=Date.to_date(today - timedelta(days=3)))['res_id'])
+        self.env.cr.flush()
+        closing_move._post()
+        self.env.cr.flush()
+        closing_move_2 = self.env['account.move'].browse(move_2.company_id.action_close_stock_valuation(at_date=Date.to_date(today - timedelta(days=1)))['res_id'])
+        closing_move_2._post()
+
+        self.assertEqual(move.value, 10)
+        self.assertEqual(closing_move.amount_total, 10)
+        self.assertEqual(move_2.value, 10)
+        self.assertEqual(closing_move_2.amount_total, 10)
+
     def test_create_done_move(self):
         """Stock Move created directly in Done state must impact de valuation."""
         product = self.product_avco