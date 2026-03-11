odoo/odoo

commit 13567aa27250f5798bbe42648eeac82241dbb780

[PERF] stock: Batch create move lines in _action_assign
Description:

When using serial numbers, pickings with many move lines can
cause performance issues during validation. This is due to
`_update_reserved_quantity` updating quantities and creating
move lines individually. By moving the creation outside
`_update_reserved_quantity`, we can batch create move lines
per move, significantly improving performances.

Benchmark:

| N° of mls |  Before | After |
|-----------|---------|-------|
|       500 |    1:37 |  1:34 |
|     15000 | Timeout |  3:30 |
|     30000 | Timeout |  4:29 |

Reference:

opw-4889581

closes odoo/odoo#223420

X-original-commit: de50d04a4db0df0b800ffcb516c2e933a35b50da
Signed-off-by: Quentin Wolfs (quwo) <quwo@odoo.com>
Signed-off-by: Aurélien Marie (auma) <auma@odoo.com>

addons/stock/models/stock_move.py
@@ -1719,6 +1719,13 @@ Please change the quantity done or the rounding precision of your unit of measur
             is performed and reservation is done on the passed quants set
         """
         self.ensure_one()
+        move_line_vals, taken_quantity = self._update_reserved_quantity_vals(need, location_id, lot_id, package_id, owner_id, strict)
+        if move_line_vals:
+            self.env['stock.move.line'].create(move_line_vals)
+        return taken_quantity
+
+    def _update_reserved_quantity_vals(self, need, location_id, lot_id=None, package_id=None, owner_id=None, strict=True):
+        self.ensure_one()
         if not lot_id:
             lot_id = self.env['stock.lot']
         if not package_id:
@@ -1762,9 +1769,7 @@ Please change the quantity done or the rounding precision of your unit of measur
                         move_line_vals += vals_list
                 else:
                     move_line_vals.append(self._prepare_move_line_vals(quantity=quantity, reserved_quant=reserved_quant))
-        if move_line_vals:
-            self.env['stock.move.line'].create(move_line_vals)
-        return taken_quantity
+        return move_line_vals, taken_quantity
 
     def _add_serial_move_line_to_vals_list(self, reserved_quant, quantity):
         return [self._prepare_move_line_vals(quantity=1, reserved_quant=reserved_quant) for i in range(int(quantity))]
@@ -1953,17 +1958,24 @@ Please change the quantity done or the rounding precision of your unit of measur
                     for move_line in move.move_line_ids.filtered(lambda m: m.quantity_product_uom):
                         if available_move_lines.get((move_line.location_id, move_line.lot_id, move_line.package_id, move_line.owner_id)):
                             available_move_lines[(move_line.location_id, move_line.lot_id, move_line.package_id, move_line.owner_id)] -= move_line.quantity_product_uom
+
+                    taken_quantities = {}
+                    all_move_line_vals = []
                     for (location_id, lot_id, package_id, owner_id), quantity in available_move_lines.items():
-                        need = move.product_qty - sum(move.move_line_ids.mapped('quantity_product_uom'))
+                        need = move.product_qty - sum(move.move_line_ids.mapped('quantity_product_uom')) - sum(taken_quantities.values())
+                        move_line_vals, taken_quantity = move._update_reserved_quantity_vals(min(quantity, need), location_id, lot_id, package_id, owner_id, strict=True)
+                        all_move_line_vals += move_line_vals
+                        taken_quantities[need, location_id, lot_id, package_id, owner_id] = taken_quantity
+                    if all_move_line_vals:
+                        self.env['stock.move.line'].create(all_move_line_vals)
+
+                    for (need, location_id, lot_id, package_id, owner_id), taken_quantity in taken_quantities.items():
                         # `quantity` is what is brought by chained done move lines. We double check
                         # here this quantity is available on the quants themselves. If not, this
                         # could be the result of an inventory adjustment that removed totally of
                         # partially `quantity`. When this happens, we chose to reserve the maximum
                         # still available. This situation could not happen on MTS move, because in
                         # this case `quantity` is directly the quantity on the quants themselves.
-
-                        taken_quantity = move.with_context(quants_cache=quants_cache)._update_reserved_quantity(
-                            min(quantity, need), location_id, lot_id, package_id, owner_id)
                         if float_is_zero(taken_quantity, precision_rounding=rounding):
                             continue
                         moves_to_redirect.add(move.id)

commit 9cabaa8d9a4ca96be657918eeeb7082714433807

[FIX] stock: allow reservation from sub locations for returns
Issue
-----
Returning products doesn't work if they were transfered to a sub location.

Steps to reproduce
-----
- Enable storage locations
- Create a new sub location to WH/Stock (eg WH/Stock/Shelf)
- Receive a product in WH/Stock & confirm
- Transfer the product to WH/Stock/Shelf
- Go to the reception transfer and return it

> The return cannot reserve the product from WH/Stock/Shelf

Cause
-----
The problem was introduced by 13567aa.

https://github.com/odoo/odoo/blob/f86baa6ba1c915145dbfe43b67de0eff13959e91/addons/stock/models/stock_move.py#L1966

The strategy used to find available quants is set as strict, so the domain
contains an exact match for the location instead of `child_of` which would
include sub locations.

https://github.com/odoo/odoo/blob/f86baa6ba1c915145dbfe43b67de0eff13959e91/addons/stock/models/stock_quant.py#L770-L787

-----
Ticket:
opw-5364331

addons/stock/models/stock_move.py
@@ -1963,7 +1963,9 @@ Please change the quantity done or the rounding precision of your unit of measur
                     all_move_line_vals = []
                     for (location_id, lot_id, package_id, owner_id), quantity in available_move_lines.items():
                         need = move.product_qty - sum(move.move_line_ids.mapped('quantity_product_uom')) - sum(taken_quantities.values())
-                        move_line_vals, taken_quantity = move._update_reserved_quantity_vals(min(quantity, need), location_id, lot_id, package_id, owner_id, strict=True)
+                        # Allow return moves to search for quants in child locations as well
+                        strict = not move.picking_id.return_id
+                        move_line_vals, taken_quantity = move._update_reserved_quantity_vals(min(quantity, need), location_id, lot_id, package_id, owner_id, strict)
                         all_move_line_vals += move_line_vals
                         if move_line_vals:  # Only subtract for new lines (updates are already reflected in sum(move_line_ids))
                             taken_quantities[need, location_id, lot_id, package_id, owner_id] = taken_quantity

addons/stock/tests/test_stock_return_picking.py
@@ -346,3 +346,55 @@ class TestReturnPicking(TestStockCommon):
         # 2 exchanged products received: both on-hand and forecasted quantities should be 10
         self.assertEqual(self.productA.qty_available, 10)
         self.assertEqual(self.productA.virtual_available, 10)
+
+    def test_return_to_partner_from_sub_location(self):
+        '''
+        Ensure that products moved to a sub location can still be reserved when creating a return.
+        '''
+        self.env.user.groups_id += self.env.ref('stock.group_stock_multi_locations')
+        wh_stock = self.env['stock.location'].browse(self.stock_location)
+        sub_loc = wh_stock.child_ids and wh_stock.child_ids[0] or self.env['stock.location'].create({
+            'name': 'New sub location',
+            'usage': 'internal',
+            'location_id': wh_stock.id,
+        })
+
+        # Receive 13 of the product in stock
+        receipt_picking = self.PickingObj.create({
+            'picking_type_id': self.picking_type_in,
+            'location_id': self.customer_location,
+            'location_dest_id': self.stock_location,
+        })
+        in_move = self.MoveObj.create({
+            'name': "OUT move",
+            'product_id': self.productA.id,
+            'product_uom_qty': 13,
+            'picking_id': receipt_picking.id,
+            'location_id': self.customer_location,
+            'location_dest_id': self.stock_location,
+        })
+        in_move.quantity = 1
+        receipt_picking.button_validate()
+
+        # Transfer the product to the sub location
+        transfer_move = self.env['stock.move'].create({
+            'name': 'Move 1 product',
+            'product_id': self.productA.id,
+            'product_uom_qty': 13,
+            'product_uom': self.product.uom_id.id,
+            'location_id': self.stock_location,
+            'location_dest_id': sub_loc.id,
+        })
+        transfer_move._action_confirm()
+        transfer_move._action_assign()
+        transfer_move.quantity = 13
+        transfer_move.picked = True
+        transfer_move._action_done()
+
+        # Create a return on the reception transfer
+        return_wizard = self.env['stock.return.picking'].with_context(active_id=receipt_picking.id, active_model='stock.picking').create({})
+        return_wizard.product_return_moves.quantity = 13
+        res = return_wizard.action_create_returns()
+        return_picking = self.PickingObj.browse(res["res_id"])
+        self.assertEqual(return_picking.location_dest_id.id, self.customer_location)
+        self.assertEqual(return_picking.state, 'assigned')