562b56f76b86873ab60280e9c4f97edffee4d0e1
guce <guce@odoo.com>
2026-03-11 14:06:13 +0000

    [FIX] mail, test_mail: loosen "sent email" blacklist
    e39bf6c6ab31e57d472a552cb9b902496e6323ae introduced a blacklist to email sending,
    to filter out "alias emails" (catchall...) & the root partner email
    (odoobot) from being sent certain emails, such as mailings.
    
    By default, odoobot email is odoobot@example.com. However, there are
    cases where users will have a legitimate partner that matches the
    odoobot email; an example of this is saas config, which automatically
    changes the odoobot email to the admin email set on database spin-up.
    
    To fix this, the root partner email is now only added to the blacklist
    if no active partner has the same email.
    
    Steps to reproduce:
    - Add a mailing contact with the same email as the root partner
    - Send a mailing to that mailing contact
    
    task-4893615
    
    closes odoo/odoo#278039
    
    X-original-commit: 38630e0c41664c359530ae1218acbb2b92fe343f
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>
    Signed-off-by: Céleste Guilleux (guce) <guce@odoo.com>

--- addons/test_mass_mailing/tests/test_performance.py
+++ addons/test_mass_mailing/tests/test_performance.py
@@ -50,7 +50,7 @@ class TestMassMailPerformance(TestMassMailPerformanceBase):
         with (
             self.mock_mail_gateway(mail_unlink_sent=True),
             # contains notably 1 query / record for unlink in mail
-            self.assertQueryCount(__system__=1378, marketing=1380),  # 1229, 1230
+            self.assertQueryCount(__system__=1379, marketing=1381),  # 1230, 1232
         ):
             mailing.action_send_mail()
 
@@ -95,7 +95,7 @@ class TestMassMailBlPerformance(TestMassMailPerformanceBase):
 
         # runbot needs +153 compared to local
         # contains notably 1 query / record for unlink in mail
-        with self.assertQueryCount(__system__=1409, marketing=1411):  # 1256, 1258
+        with self.assertQueryCount(__system__=1411, marketing=1413):  # 1258, 1260
             mailing.action_send_mail()
 
         self.assertEqual(mailing.sent, 50)
