b6a94431bbff9a0343d97458a8d4923a393eaf3c
bona-odoo <bona@odoo.com>
2026-03-11 12:10:57 +0000

    [FIX] sale_project{,_stock}: handle multiple invoices in COGS action
    Steps to reproduce:
    -------------------
    1. Install `sale_project_stock` and Accounting.
    2. Create a storable product with *Real-time valuation* and configure the
       COGS account in the product category expense account.
    (Ensure you have enabled automatic accounting from accounting>config.)
    3. Create a project with a specific analytic account and ensure
       the project is billable.
    4. Create a sale order with the created product and set the same
       analytic account in the analytic distribution.
    5. Confirm the order, deliver the product, generate the invoice,
       and post it.
    6. Open the project and go to the *Profitability* report.
    7. Click on the **Cost of Goods Sold** dashboard item.
    8. Repeat steps 4–7 with multiple invoices.
    
    Issue:
    ------
    When there is only one invoice, clicking the COGS dashboard item
    correctly displays the related move lines. However, when there are
    multiple invoices, the action opens with empty results.
    
    Cause:
    ------
    `_get_action_for_profitability_section` sets `res_id` only when a
    single record exists. When multiple records are present, `res_id`
    becomes `False`, which causes the action to open without results.
    https://github.com/odoo/odoo/blob/8f79d407724f40ba8e48f1747b2e87311b7fb49e/addons/project_account/models/project_project.py#L78-L83
    
    Solution:
    ---------
    When res_id is not set, search account.move records using the
    domain to retrieve the relevant move IDs, then apply a proper domain
    to display all related COGS journal items.
    
    opw-5949261
    
    X-original-commit: 2b346227474fb35cb1116c3a6e1bbb041e7c24da

--- addons/sale_project/models/project_project.py
+++ addons/sale_project/models/project_project.py
@@ -273,6 +273,99 @@ class ProjectProject(models.Model):
             action['views'] = [(self.env.ref('sale_project.project_milestone_view_tree').id, view_type) if view_type == 'list' else (view_id, view_type) for view_id, view_type in action['views']]
         return action
 
+<<<<<<< 84017b2b27f678b922aa39ee412a72358d030994
+||||||| 0b17840fb3cc72935e1a6302a057fb55c253c498
+    def action_profitability_items(self, section_name, domain=None, res_id=False):
+        if section_name in ['service_revenues', 'materials']:
+            view_types = ['list', 'kanban', 'form']
+            action = {
+                'name': _('Sales Order Items'),
+                'type': 'ir.actions.act_window',
+                'res_model': 'sale.order.line',
+                'context': {'create': False, 'edit': False},
+            }
+            if res_id:
+                action['res_id'] = res_id
+                view_types = ['form']
+            else:
+                action['domain'] = domain
+            action['views'] = [(False, v) for v in view_types]
+            return action
+
+        if section_name in ['other_invoice_revenues', 'downpayments']:
+            action = self.env["ir.actions.actions"]._for_xml_id("account.action_move_out_invoice_type")
+            action['domain'] = domain if domain else []
+            action['context'] = {
+                **ast.literal_eval(action['context']),
+                'default_partner_id': self.partner_id.id,
+                'project_id': self.id,
+            }
+            if res_id:
+                action['views'] = [(False, 'form')]
+                action['view_mode'] = 'form'
+                action['res_id'] = res_id
+            return action
+
+        if section_name == 'cost_of_goods_sold':
+            action = {
+                'name': _('Cost of Goods Sold Items'),
+                'type': 'ir.actions.act_window',
+                'res_model': 'account.move.line',
+                'views': [[False, 'list'], [False, 'form']],
+                'domain': [('move_id', '=', res_id), ('display_type', '=', 'cogs')],
+                'context': {'create': False, 'edit': False},
+            }
+            return action
+
+        return super().action_profitability_items(section_name, domain, res_id)
+
+=======
+    def action_profitability_items(self, section_name, domain=None, res_id=False):
+        if section_name in ['service_revenues', 'materials']:
+            view_types = ['list', 'kanban', 'form']
+            action = {
+                'name': _('Sales Order Items'),
+                'type': 'ir.actions.act_window',
+                'res_model': 'sale.order.line',
+                'context': {'create': False, 'edit': False},
+            }
+            if res_id:
+                action['res_id'] = res_id
+                view_types = ['form']
+            else:
+                action['domain'] = domain
+            action['views'] = [(False, v) for v in view_types]
+            return action
+
+        if section_name in ['other_invoice_revenues', 'downpayments']:
+            action = self.env["ir.actions.actions"]._for_xml_id("account.action_move_out_invoice_type")
+            action['domain'] = domain if domain else []
+            action['context'] = {
+                **ast.literal_eval(action['context']),
+                'default_partner_id': self.partner_id.id,
+                'project_id': self.id,
+            }
+            if res_id:
+                action['views'] = [(False, 'form')]
+                action['view_mode'] = 'form'
+                action['res_id'] = res_id
+            return action
+
+        if section_name == 'cost_of_goods_sold':
+            move_ids = [res_id] if res_id else self.env['account.move'].search(domain).ids
+            action = {
+                'name': _('Cost of Goods Sold Items'),
+                'type': 'ir.actions.act_window',
+                'res_model': 'account.move.line',
+                'views': [[False, 'list'], [False, 'form']],
+                'domain': [('move_id', 'in', move_ids), ('display_type', '=', 'cogs')],
+                'context': {'create': False, 'edit': False},
+            }
+            return action
+
+        return super().action_profitability_items(section_name, domain, res_id)
+
+>>>>>>> 5a2a570192b316b05f9c6dd324ddf59515108775
     @api.depends('sale_order_id.invoice_status', 'tasks.sale_order_id.invoice_status')
     def _compute_has_any_so_with_nothing_to_invoice(self):
         """Has any Sale Order whose invoice_status is set as No"""
