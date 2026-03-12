351bc90ec5e46da13787f9dcf54e067bd1bd52f9
akus-odoo <akus@odoo.com>
2026-03-12 09:44:17 +0000

    [FIX] im_livechat: disable @mentions for visitors in livechat
    Visitors in livechat can currently trigger partner mention suggestions
    using the @ delimiter. However, visitors can only mention themselves
    or odoobot, which does not provide any meaningful functionality and
    adds unnecessary noise to the composer suggestions.
    
    This change disables the @ delimiter for visitors in livechat threads,
    preventing partner mention suggestions from appearing while keeping the
    feature available for internal users.
    
    Task-5119068
    
    X-original-commit: ab6f4e96d579ece1a2b5603e2448c7a62cddc7fb

---
+++ addons/im_livechat/static/tests/embed/suggestion.test.js
@@ -0,0 +1,30 @@
+import { LivechatButton } from "@im_livechat/embed/common/livechat_button";
+import {
+    defineLivechatModels,
+    loadDefaultEmbedConfig,
+} from "@im_livechat/../tests/livechat_test_helpers";
+import { SuggestionService } from "@mail/core/common/suggestion_service";
+import { click, contains, insertText, start } from "@mail/../tests/mail_test_helpers";
+import { describe, expect, test } from "@odoo/hoot";
+import { mountWithCleanup, patchWithCleanup } from "@web/../tests/web_test_helpers";
+
+describe.current.tags("desktop");
+defineLivechatModels();
+
+test("Visitor cannot use @ mentions in livechat", async () => {
+    await loadDefaultEmbedConfig();
+    await start({ authenticateAs: false });
+    await mountWithCleanup(LivechatButton);
+    await click(".o-livechat-LivechatButton");
+    await contains(".o-mail-Message", { text: "Hello, how may I help you?" });
+    patchWithCleanup(SuggestionService.prototype, {
+        getSupportedDelimiters() {
+            const delimiters = super.getSupportedDelimiters(...arguments);
+            expect.step(delimiters.map((d) => d[0]).join(","));
+            return delimiters;
+        },
+    });
+    await insertText(".o-mail-Composer-input", "@");
+    await expect.waitForSteps(["#,::,:,/"]);
+    await contains(".o-mail-Composer-suggestion", { count: 0 });
+});
