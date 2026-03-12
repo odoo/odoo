e315c39c6c3a309fc00357fe8210830bf526614c
MAGREMENT <zavan@odoo.com>
2026-03-12 12:40:49 +0000

    [FIX] mrp : allow component consumption on new move lines
    # Product Configuration
    *Manufactured Product*
    - Storable
    - Tracked by Quantity
    - Manufacture Route
    - Has a BOM with atleast 1 component
    
    *Component Product*
    - Storable
    - Tracked By Lot
    
    # How to reproduce
    - Ensure there is available stock for the component product in a lot
    - Create a MO for the Manufatured Product
    - Confirm the MO
    - Click "Details" on the component product
    - Remove the reserved quant and add a new one
    - Increase the quantity of this new quant to more than "To Consume"
    - Save
    - Observe that "Consumed" = The quantity you just set on the quant
    - Click on "Produce All"
    
    # The issue
    - The Consumed quantity is reset to the "To Consume" quantity.
    - Furthermore, a warning popup should be displayed when clicking on "Produce All" but there is none.
    - Finally, depending on the version you may get this error message : "You need to supply Lot/Serial Number for products and 'consume' them: - Component Product" even though a lot is already assigned
    
    # Why
    All these issues stem from the fact that move_raw_ids.picked from mrp.production is set to False instead of True.
    
    This issue was introduced by this commit (https://github.com/odoo/odoo/commit/ef592464983d66ac76bc71a9886462f1f47dc28d) that changed the way the picked value is set.
    
    In write(self, vals) de stock_move, we have :
    ```py
    if self.env.context.get('force_manual_consumption') and 'quantity' in vals:
                moves_to_update = self.filtered(lambda move: move.product_uom_qty != vals['quantity'])
                if moves_to_update:
                    moves_to_update.write({'manual_consumption': True, 'picked': True})
    ```
    Followed a bit later by :
    ```py
    res = super().write(vals)
    ```
    
    This usually works fine except when vals contains edition commands for move_line_ids. Then, the first write will correclty set picked to True, but then picked will be reevaluted after the second write with :
    ```py
        def _compute_picked(self):
            for move in self:
                if move.state == 'done' or any(ml.picked for ml in move.move_line_ids):
                    move.picked = True
                else:
                    move.picked = False
    ```
    
    If all the resulting move_line_ids from the commands edition have picked set to False, then move.picked will also be set to False.
    
    opw-5937171
    
    X-original-commit: 7c87ea7fa31f30110fa5eafdcb1fe26f7aaceaf9

--- addons/mrp/models/stock_move.py
+++ addons/mrp/models/stock_move.py
@@ -291,10 +291,9 @@ class StockMove(models.Model):
                 updated_product_move._action_confirm()
                 move_to_unlink.unlink()
                 self = other_move + updated_product_move
+        moves_to_update = False
         if self.env.context.get('force_manual_consumption') and 'quantity' in vals:
             moves_to_update = self.filtered(lambda move: move.product_uom_qty != vals['quantity'])
-            if moves_to_update:
-                moves_to_update.write({'manual_consumption': True, 'picked': True})
         if 'product_uom_qty' in vals and 'move_line_ids' in vals:
             # first update lines then product_uom_qty as the later will unreserve
             # so possibly unlink lines
@@ -302,6 +301,8 @@ class StockMove(models.Model):
             super().write({'move_line_ids': move_line_vals})
         old_demand = {move.id: move.product_uom_qty for move in self}
         res = super().write(vals)
+        if moves_to_update:
+            moves_to_update.write({'manual_consumption': True, 'picked': True})
         if 'product_uom_qty' in vals and not self.env.context.get('no_procurement', False):
             # when updating consumed qty need to update related pickings
             # context no_procurement means we don't want the qty update to modify stock i.e create new pickings
