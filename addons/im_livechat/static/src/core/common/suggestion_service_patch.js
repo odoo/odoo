1ed5d982f91cfb66a59051152af1fdf2097a56b8
akus-odoo <akus@odoo.com>
2026-03-12 09:44:17 +0000

    [FIX] im_livechat: disable @mentions for visitors in livechat
    Visitors in livechat can currently trigger partner mention suggestions
    using the @ delimiter. However, visitors can only mention themselves
    or odoobot, which does not provide any meaningful functionality and
    adds unnecessary noise to the composer suggestions.
    
    This change disables the @ delimiter for visitors in livechat threads,
    preventing partner mention suggestions from appearing while keeping the
    feature available for internal users.
    
    Task-5119068
    
    X-original-commit: 330619437ce5abc4a2a8cecc8306593994385e2e

---
+++ addons/im_livechat/static/src/core/common/suggestion_service_patch.js
@@ -0,0 +1,13 @@
+import { SuggestionService } from "@mail/core/common/suggestion_service";
+
+import { patch } from "@web/core/utils/patch";
+
+patch(SuggestionService.prototype, {
+    /** @override */
+    getSupportedDelimiters(thread) {
+        const res = super.getSupportedDelimiters(...arguments);
+        return thread?.channel?.channel_type === "livechat" && this.store.self_user?.share !== false
+            ? res.filter((delimiter) => delimiter.at(0) !== "@")
+            : res;
+    },
+});
