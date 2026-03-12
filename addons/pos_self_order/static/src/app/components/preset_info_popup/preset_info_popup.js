d236e7a565996f820910fbde32ff0586b004bca4
David Monnom <moda@odoo.com>
2026-03-12 10:32:25 +0000

    [FIX] pos_self_order: optimize partner data loading for self-ordering
    Partner data loading for self-ordering was not optimized, leading to
    performance issues when the partner had a lot of data.
    
    This commit optimizes the partner data loading by only loading the
    necessary fields for self-ordering.
    
    X-original-commit: 039c5139119c9f59bcd999afebf61145be1f5b27

--- addons/pos_self_order/static/src/app/components/preset_info_popup/preset_info_popup.js
+++ addons/pos_self_order/static/src/app/components/preset_info_popup/preset_info_popup.js
@@ -16,17 +16,22 @@ export class PresetInfoPopup extends Component {
 
     setup() {
         this.selfOrder = useService("self_order");
+
+        const partner = this.selfOrder.currentOrder.partner_id;
+        const companyStateId = this.selfOrder.config.company_id.country_id.state_ids[0]?.id;
+        const companyCountryId = this.selfOrder.config.company_id.country_id.id;
+
         this.state = useState({
-            selectedPartnerId: null,
-            name: "",
-            email: "",
-            phone: "",
+            selectedPartnerId: partner?.id || null,
+            name: partner?.name || "",
+            email: partner?.email || "",
+            phone: partner?.phone || "",
             phoneError: "",
-            street: "",
-            countryId: this.selfOrder.config.company_id.country_id.id,
-            stateId: this.selfOrder.config.company_id.country_id.state_ids[0]?.id || null,
-            city: "",
-            zip: "",
+            street: partner?.street || "",
+            countryId: partner?.country_id?.id || companyCountryId || null,
+            stateId: partner?.state_id?.id || companyStateId || null,
+            city: partner?.city || "",
+            zip: partner?.zip || "",
         });
 
         onWillStart(async () => {
@@ -36,55 +41,41 @@ export class PresetInfoPopup extends Component {
 
     async setInformations() {
         if (this.preset.needsPartner || this.state.phone) {
-            const data = {
-                access_token: this.selfOrder.access_token,
+            const countryId = parseInt(this.state.countryId, 10) || null;
+            const stateId = parseInt(this.state.stateId, 10) || null;
+            const partnerData = {
                 name: this.state.name,
                 email: this.state.email,
                 phone: this.state.phone,
                 street: this.state.street,
                 city: this.state.city,
-                country_id: this.state.countryId,
-                state_id: this.state.stateId,
+                country_id: countryId,
+                state_id: stateId,
                 zip: this.state.zip,
             };
             const result = await rpc(`/pos-self-order/validate-partner`, {
                 access_token: this.selfOrder.access_token,
                 partner_id: this.state.selectedPartnerId,
-                ...data,
+                ...partnerData,
             });
 
+            const partnerId = result?.["res.partner"]?.[0]?.id;
+            if (!partnerId) {
+                return;
+            }
             // The endpoint doesn't return private informations
-            Object.assign(result["res.partner"][0], data);
+            partnerData.id = partnerId;
+            result["res.partner"][0] = partnerData;
             this.selfOrder.data.synchronizeServerDataInIndexedDB(result);
-
-            const partner = this.selfOrder.models.connectNewData(result);
-            this.selfOrder.currentOrder.floating_order_name = `${this.preset.name} - ${partner["res.partner"][0].name}`;
-            this.selfOrder.currentOrder.partner_id = partner["res.partner"][0];
+            const connectedData = this.selfOrder.models.connectNewData(result);
+            const partner = connectedData["res.partner"][0];
+            this.selfOrder.currentOrder.floating_order_name = `${this.preset.name} - ${partner.name}`;
+            this.selfOrder.currentOrder.partner_id = partner;
         } else {
             this.selfOrder.currentOrder.floating_order_name = this.state.name;
         }
         this.props.getPayload(this.state);
         this.props.close();
-    }
-
-    selectExistingPartner(event) {
-        const partner = this.selfOrder.models["res.partner"].get(event.target.value);
-        this.state.name = partner?.name || "";
-        this.state.email = partner?.email || "";
-        this.state.phone = partner?.phone || "";
-        this.state.street = partner?.street || "";
-        this.state.city = partner?.city || "";
-        this.state.countryId = partner?.country_id?.id || null;
-        this.state.stateId = partner?.state_id?.id || null;
-        this.state.zip = partner?.zip || "";
-    }
-
-    get existingPartners() {
-        return this.selfOrder.models["res.partner"].getAll();
-    }
-
-    get partnerIsSelected() {
-        return this.state.selectedPartnerId && this.state.selectedPartnerId !== "0";
     }
 
     close() {
