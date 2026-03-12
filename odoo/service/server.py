a019658397028086ffd7dddf60d7be892c7897cd
Beekoan <mmha@odoo.com>
2026-03-12 12:37:35 +0000

    [FIX] memory: change tracemalloc to psutil and heuristic
    The previous memory profiler was causing a lot of performance issues.
    This is because tracemalloc tracks the allocations that happens at the
    python interpreter level by attaching to the cpython allocators. This
    first meant each allocation that happens through python has to go
    through a callstack while holding the GIL and preventing the thread and
    other threads from operating. This callstack does multiple things,
    first is walking the allocation back from the current frame up until
    the specified frame depth at the start of collection. The other is
    updating the internal object that keeps track of the allocations and
    what cause them up until now which degrades the performance even more
    when the allocator keeps running for a long time. Increasing the frame
    depth also means the partitioning becomes even more fragmented in the
    internal object and leads to higher memory usage. This in turns means
    lower performance as well. The issue becomes more evident when the
    overhead of tracemalloc blocks any execution even turning it off
    because the gil cannot be released until the full allocation execution
    happens.
    
    Currently this would happen on long enough requests or a high enough
    depth.
    
    Two PRs were made to try to address this issue.
    
    1- https://github.com/odoo/odoo/pull/251950 : This PR tries the
    solution of having a lower frame depth but matching the frames based on
    a window of frames so that we can reconstruct an approximation of the
    flamegraph, for example: matching window of 2 frames
    1 - > 2 - > 3 - > 4
    2 - > 3 - > 4 - > 5
    would mean that we would match frames 2 and 3 in both stack traces and
    append the first frame to the second callstack which would look like
    1 - > 2 - > 3 - > 4 - > 5
    Neverthless this was deemed to have too big of an assumption in the
    building heuristic.
    
    2- https://github.com/odoo/odoo/pull/253120: This PR was supposed to be
    introducing memray as a profiler. Memray is the best tool for this
    usecase. First because it attaches on the native system allocation
    calls, and uses a file to append to on allocations. This solves both of
    the issues that we had in the beginning but the issue with memray is
    that it's an external tool that was deemed unnecessary to add.
    
    The final solution is this PR:
    The PR assumes a heuristic that in worker mode, a single worker handles
    one thread which mean that the process memory can be fully attributed
    to the request. The heuristic is also based that on a high enough
    sampling rate, the delta can be fully attributed to the current frame.
    This is a close enough approximation to know where to look but not what
    is the actual memory usage by line.
    
    X-original-commit: b6613ce03c4c4e28083cbe5f6531386d6d98d19a

--- odoo/service/server.py
+++ odoo/service/server.py
@@ -61,6 +61,7 @@ from odoo.release import nt_service_name
 from odoo.tools import config, gc, osutil, OrderedSet, profiler
 from odoo.tools.cache import log_ormcache_stats
 from odoo.tools.misc import stripped_sys_argv, dumpstacks, mute_logger
+from odoo.tools.osutil import memory_info
 
 _logger = logging.getLogger(__name__)
 
@@ -72,18 +73,6 @@ thread_local = threading.local()
 
 # the model and method name that was called via rpc, for logging
 thread_local.rpc_model_method = ''
-
-
-def memory_info(process):
-    """
-    :return: the relevant memory usage according to the OS in bytes.
-    """
-    # psutil < 2.0 does not have memory_info, >= 3.0 does not have get_memory_info
-    pmem = (getattr(process, 'memory_info', None) or process.get_memory_info)()
-    # MacOSX allocates very large vms to all processes so we only monitor the rss usage.
-    if platform.system() == 'Darwin':
-        return pmem.rss
-    return pmem.vms
 
 
 def set_limit_memory_hard():
