e9ac03687d4dfe105afe4301cfb231c36e009ce1
smjo-odoo <smjo@odoo.com>
2026-03-12 10:26:03 +0530

    [IMP] l10n_ph: add field for entity type
    Before this commit:
    - from version saas-19.1, the field 'Company Type' which was earlier used to
      distinguish between 'Person' and 'Company' contacts is removed.
    
    After this commit:
    - added a field 'Entity type' which is required by Phillipines tax authorities
      for the distinction between individual and corporation entities.
    
    task-5928297

--- addons/l10n_ph/models/res_partner.py
+++ addons/l10n_ph/models/res_partner.py
@@ -7,6 +7,13 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     branch_code = fields.Char("Branch Code", default='000', compute='_compute_branch_code', store=True)
+    l10n_ph_entity_type = fields.Selection([
+            ('individual', 'Individual'),
+            ('corporation', 'Corporation'),
+        ],
+        string='Type of Entity',
+        help='Philippines: Defines the type of entity.'
+    )
     first_name = fields.Char("First Name")
     middle_name = fields.Char("Middle Name")
     last_name = fields.Char("Last Name")
@@ -23,3 +30,17 @@ class ResPartner(models.Model):
                 match = partner._check_vat_ph_re.match(partner.vat)
                 branch_code = match and match.group(1) and match.group(1)[1:] or branch_code
             partner.branch_code = branch_code
+
+    @api.depends('vat', 'commercial_partner_id', 'country_code', 'l10n_ph_entity_type')
+    def _compute_is_company(self):
+        ph_partners = self.filtered(lambda partner: partner.country_code == 'PH' and partner.l10n_ph_entity_type)
+        for partner in ph_partners:
+            partner.is_company = partner.l10n_ph_entity_type == 'corporation'
+        super(ResPartner, self - ph_partners)._compute_is_company()
+
+    @api.onchange('l10n_ph_entity_type')
+    def _onchange_entity_type(self):
+        if self.l10n_ph_entity_type == 'corporation':
+            self.first_name = False
+            self.middle_name = False
+            self.last_name = False
