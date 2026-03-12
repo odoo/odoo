1e62d45fce8cfae84852bd0dc95fb48c345b4175
Pierre Paridans <app@odoo.com>
2026-03-12 13:47:31 +0000

    [FIX] web: new File() should receive a correct mimetype
    The `type` option passed to the `File` constructor should be a string
    representing the MIME type of the content that will be put into the
    file.
    
    Chrome 146 actually follows the Fetch Standard and preserve the data URL
    MIME type parameter.
    
    This commit fixes the malformed MIME types passed to the `File`
    constructor to ensure proper compatibility with pre/post Chrome version
    146 (and actually follow the spec).
    
    References:
    - https://chromestatus.com/feature/4874471565557760
    - https://developer.mozilla.org/en-US/docs/Web/API/File/File#type
    
    runbot-241901
    
    closes odoo/odoo#254236
    
    X-original-commit: 17296c5fb24280014d216ab51f567ee57531a804
    Related: odoo/enterprise#110876
    Signed-off-by: Christophe Monniez (moc) <moc@odoo.com>
    Signed-off-by: Pierre Paridans (app) <app@odoo.com>

--- addons/web/static/tests/views/fields/image_field.test.js
+++ addons/web/static/tests/views/fields/image_field.test.js
@@ -227,7 +227,7 @@ test("url should not use the record last updated date when the field is related"
         new File(
             [Uint8Array.from([...atob(MY_IMAGE)].map((c) => c.charCodeAt(0)))],
             "fake_file.png",
-            { type: "png" }
+            { type: "image/png" }
         ),
         "related"
     );
@@ -324,7 +324,7 @@ test("ImageField preview is updated when an image is uploaded", async () => {
     const imageFile = new File(
         [Uint8Array.from([...atob(MY_IMAGE)].map((c) => c.charCodeAt(0)))],
         "fake_file.png",
-        { type: "png" }
+        { type: "image/png" }
     );
     await mountView({
         type: "form",
@@ -385,7 +385,7 @@ test("clicking save manually after uploading new image should change the unique 
         new File(
             [Uint8Array.from([...atob(MY_IMAGE)].map((c) => c.charCodeAt(0)))],
             "fake_file.png",
-            { type: "png" }
+            { type: "image/png" }
         )
     );
     expect("div[name=document] img").toHaveAttribute(
@@ -410,7 +410,7 @@ test("clicking save manually after uploading new image should change the unique 
         new File(
             [Uint8Array.from([...atob(PRODUCT_IMAGE)].map((c) => c.charCodeAt(0)))],
             "fake_file2.gif",
-            { type: "gif" }
+            { type: "image/gif" }
         )
     );
     expect("div[name=document] img").toHaveAttribute(
@@ -703,7 +703,7 @@ test("ImageField is reset when changing record", async () => {
         `,
     });
 
-    const imageFile = new File([imageData], "fake_file.png", { type: "png" });
+    const imageFile = new File([imageData], "fake_file.png", { type: "image/png" });
     expect("img[alt='Binary file']").toHaveAttribute(
         "data-src",
         "/web/static/img/placeholder.png",
@@ -862,7 +862,7 @@ test("convert image to webp", async () => {
         `,
     });
 
-    const imageFile = new File([imageData], "fake_file.jpeg", { type: "jpeg" });
+    const imageFile = new File([imageData], "fake_file.jpeg", { type: "image/jpeg" });
     expect("img[alt='Binary file']").toHaveAttribute(
         "data-src",
         "/web/static/img/placeholder.png",
