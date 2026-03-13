32c2e521b160c9961274acd35108ced380149324
Pedram (PEBR) <pebr@odoo.com>
2026-03-13 15:52:08 +0000

    [FIX] point_of_sale: handle IndexedDB connection loss on iOS/Safari
    On iOS/Safari, the WebKit IDB server process can be killed by the OS
    (e.g. due to memory pressure when the app is backgrounded), resulting
    in an UnknownError: "Connection to Indexed Database server lost".
    Additionally, returning from background can leave the connection in an
    InvalidStateError "closing" state while this.db remains non-null.
    
    opw-5121896
    
    closes odoo/odoo#259697
    
    X-original-commit: cf8049b40a3a9d71d93681293d9592dc267100c0
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/point_of_sale/static/src/app/services/data_service.js
+++ addons/point_of_sale/static/src/app/services/data_service.js
@@ -150,7 +150,7 @@ export class PosData extends Reactive {
         });
 
         return new Promise((resolve) => {
-            this.indexedDB = new IndexedDB(this.databaseName, false, models, resolve);
+            this.indexedDB = new IndexedDB(this.databaseName, false, models, resolve, this.dialog);
         });
     }
 
