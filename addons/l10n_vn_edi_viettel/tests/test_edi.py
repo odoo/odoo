5bd7fecc795652f40cb941a2f6203d9a8b60db83
jand-odoo <jand@odoo.com>
2026-03-13 03:22:50 +0000

    [FIX] l10n_vn: fix wrong account for WIP
    Update account_production_wip_account_id from
    chart154 to chart1541 so the default production
    WIP account is correctly configured.
    
    task-5875158
    
    X-original-commit: c65fcef97e1ab0e06c34dc466b32ad17396ff23b

--- addons/l10n_vn_edi_viettel/tests/test_edi.py
+++ addons/l10n_vn_edi_viettel/tests/test_edi.py
@@ -532,6 +532,7 @@ class TestVNEDI(AccountTestInvoicingCommon):
         Requires the 'point_of_sale' module to be installed.
         """
         self.ensure_installed('pos_discount')
+        self.env.user.group_ids += self.env.ref('point_of_sale.group_pos_manager')
         discount_product = self.env['product.product'].create({
             'name': 'POS Discount Product',
             'type': 'service',
@@ -587,7 +588,7 @@ class TestVNEDI(AccountTestInvoicingCommon):
         Requires the 'sale' module to be installed.
         """
         self.ensure_installed('sale')
-        self.env.user.groups_id += self.env.ref('sales_team.group_sale_salesman')
+        self.env.user.group_ids += self.env.ref('sales_team.group_sale_salesman')
         sale_order = self.env['sale.order'].create({
             'partner_id': self.partner_a.id,
             'order_line': [Command.create({
@@ -631,7 +632,7 @@ class TestVNEDI(AccountTestInvoicingCommon):
         Requires the 'sale_loyalty' module.
         """
         self.ensure_installed('sale_loyalty')
-        self.env.user.groups_id += self.env.ref('sales_team.group_sale_salesman')
+        self.env.user.group_ids += self.env.ref('sales_team.group_sale_manager')
         program = self.env['loyalty.program'].create({
             'name': '10% Discount',
             'program_type': 'coupons',
@@ -652,7 +653,7 @@ class TestVNEDI(AccountTestInvoicingCommon):
                 'product_id': self.product_a.id,
                 'product_uom_qty': 1,
                 'price_unit': 1000.0,
-                'tax_id': [Command.set(self.tax_sale_a.ids)],
+                'tax_ids': [Command.set(self.tax_sale_a.ids)],
             })],
         })
 
