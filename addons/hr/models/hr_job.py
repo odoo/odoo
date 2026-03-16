26d045196cff3183af3629092b077f080d376361
AlienAtSystem <gerald.malsch@manatec.de>
2026-03-16 09:31:39 +0100

    [FIX] hr_job: No Rights issue in Recruitment list view

--- addons/hr/models/hr_job.py
+++ addons/hr/models/hr_job.py
@@ -14,9 +14,9 @@ class HrJob(models.Model):
     active = fields.Boolean(default=True)
     name = fields.Char(string='Job Position', required=True, index='trigram', translate=True)
     sequence = fields.Integer(default=10)
-    expected_employees = fields.Integer(compute='_compute_employees', string='Total Forecasted Employees',
+    expected_employees = fields.Integer(compute='_compute_employees', string='Total Forecasted Employees', compute_sudo=True,
         help='Expected number of employees for this job position after new recruitment.', groups="hr.group_hr_user")
-    no_of_employee = fields.Integer(compute='_compute_employees', string="Current Number of Employees",
+    no_of_employee = fields.Integer(compute='_compute_employees', string="Current Number of Employees", compute_sudo=True,
         help='Number of employees currently occupying this job position.', groups="hr.group_hr_user")
     no_of_recruitment = fields.Integer(string='Target', copy=False,
         help='Number of new employees you expect to recruit.', default=1)
