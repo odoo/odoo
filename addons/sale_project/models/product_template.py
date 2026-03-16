fleetapps/odoo3

[IMP] sale: add % delivered quantity

addons/sale_project/models/product_template.py
@@ -12,7 +12,7 @@ class ProductTemplate(models.Model):
         service_policies = [
             # (service_policy, string)
             ('ordered_prepaid', _('Prepaid/Fixed Price')),
-            ('delivered_manual', _('Based on Delivered Quantity (Manual)')),
+            ('delivered_manual', _('Based on Delivered (Manual)')),
         ]
 
         if self.env['res.groups']._is_feature_enabled('project.group_project_milestone'):

odoo/odoo

addons/sale_project/models/product_template.py
@@ -12,7 +12,7 @@ class ProductTemplate(models.Model):
         service_policies = [
             # (service_policy, string)
             ('ordered_prepaid', _('Prepaid/Fixed Price')),
-            ('delivered_manual', _('Based on Delivered Quantity (Manual)')),
+            ('delivered_manual', _('Based on Delivery (Manual)')),
         ]
 
         if self.env['res.groups']._is_feature_enabled('project.group_project_milestone'):