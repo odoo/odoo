4245b03b6eee8283489f4c589aaefd8a04b835f6
Laurent Smet (LAS) <las@odoo.com>
2026-03-16 08:14:29 +0000

    [IMP] account_edi_ubl_cii: Introduce the new UBL/BIS3 test suite for imports
    X-original-commit: bee2618c50a1534d6a28239c2b651e6c75e9db77

--- addons/account_edi_ubl_cii/tests/__init__.py
+++ addons/account_edi_ubl_cii/tests/__init__.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from . import common
 from . import test_autopost_bills
 from . import test_multilevel_constraints
@@ -7,4 +5,12 @@ from . import test_partner_peppol_fields
 from . import test_ubl_cii
 from . import test_download_docs
 from . import test_ubl_export_bis3_be
-from . import test_ubl_export_bis3_selfbilling_be
+from . import test_ubl_import_bis3_invoice_be
+from . import test_ubl_import_bis3_invoice_be_auto_generate_pdf
+from . import test_ubl_import_bis3_invoice_be_cash_rounding
+from . import test_ubl_import_bis3_invoice_be_decode_invoice_line
+from . import test_ubl_import_bis3_invoice_be_retrieve_account
+from . import test_ubl_import_bis3_invoice_be_retrieve_partner
+from . import test_ubl_import_bis3_invoice_be_retrieve_product
+from . import test_ubl_import_bis3_invoice_be_retrieve_tax
+from . import test_ubl_export_bis3_invoice_selfbilling_be
