0b020913425a773b45ef88852ec60ab04939c73c
rugot-odoo <rugot@odoo.com>
2026-03-16 12:08:27 +0000

    [FIX] base: prevent assigning groups to inherited views via res.groups
    **Steps to Reproduce:**
    - Create a new database.
    - Install any module (for example, Payroll).
    - Go to Settings → Technical → Security → Groups.
    - Open any group and assign an inherited view in the Views section.
    - Upgrade the module.
    
    **Issue:**
    - Odoo already prevents assigning groups directly on inherited view records in
      ir.ui.view. However, it is still possible to indirectly assign groups to
      inherited views through res.groups via the view_access relation.
    
    - This creates entries in ir_ui_view_group_rel and can trigger a ValidationError
      during module upgrades. As a result, users may unknowingly create invalid
      group-view relations, leading to errors and confusion.
    
    **Solution:**
    - Add a validation constraint on res.groups to prevent linking groups to
      inherited views via the view_access relation.
    
    - Raise a ValidationError when such an assignment is attempted, with the
      message: Groups should instead be defined using the 'groups' attribute inside
      the view XML definition.
    
    This ensures that inherited views cannot be assigned to groups,
    avoiding invalid configurations and preventing errors during module upgrades.
    
    **opw-6015526**
    
    closes odoo/odoo#255660
    
    X-original-commit: c8bf62c79fe47c0b08daeb5e648f9f9ed195b322
    Signed-off-by: Krzysztof Magusiak (krma) <krma@odoo.com>

--- odoo/addons/base/models/res_groups.py
+++ odoo/addons/base/models/res_groups.py
@@ -85,6 +85,10 @@ class ResGroups(models.Model):
         self.env.registry.clear_cache('groups')
         self.all_implied_by_ids._check_user_disjoint_groups()
 
+    @api.constrains('view_access')
+    def _check_inherited_view_groups(self):
+        self.view_access._check_groups()
+
     @api.constrains('user_ids')
     def _check_user_disjoint_groups(self):
         # Here we should check all the users in any group of 'self':
