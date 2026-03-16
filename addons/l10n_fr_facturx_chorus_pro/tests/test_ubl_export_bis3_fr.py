d430cf38487e59c9dabefe8a2f531478495bcce4
Laurent Smet (LAS) <las@odoo.com>
2026-03-16 08:14:29 +0000

    [IMP] account_edi_ubl_cii: Introduce the new UBL/BIS3 test suite for imports
    X-original-commit: bee2618c50a1534d6a28239c2b651e6c75e9db77

--- addons/l10n_fr_facturx_chorus_pro/tests/test_ubl_export_bis3_fr.py
+++ addons/l10n_fr_facturx_chorus_pro/tests/test_ubl_export_bis3_fr.py
@@ -6,8 +6,10 @@ from odoo.tests import tagged
 @tagged('post_install_l10n', 'post_install', '-at_install', *TestUblBis3Common.extra_tags)
 class TestUblExportBis3FRChorusPro(TestUblBis3Common, TestUblCiiCommonChorusPro, TestUblCiiFRCommon):
 
-    def subfolder(self):
-        return super().subfolder().replace('export', 'export/bis3/invoice')
+    @classmethod
+    def subfolders(cls):
+        subfolder_format, _subfolder_document, subfolder_country = super().subfolders()
+        return subfolder_format, 'invoice', subfolder_country
 
     def _assert_invoice_partner_party_identifiers(self, partner, test_file):
         tax_20 = self.percent_tax(20.0)
