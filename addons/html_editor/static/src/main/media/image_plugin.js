5ff8f5fac1cc3eac6495084c119a8556f1f9be95
Julien Launois (jula) <jula@odoo.com>
2026-03-17 14:14:40 +0000

    [IMP] html_editor: use CSSOM to remove transform styles on image reset
    __Before commit__
    Transform-related CSS properties are removed from an image element
    by manipulating the raw `style` attribute string with a regex which is
    fragile and costly in performance.
    
    __Steps to reproduce (saas-19.2)__
    This regex was added in [18.0][1] but is only a real issue since
    `resetImageTransformation` was called in
    `on_will_save_media_dialog_handlers` in [saas-19.2][2]:
    1. Add a section block.
    2. Add a background image.
    3. Open the media dialog and replace the background image.
    => The website will hang for a while.
    
    __Fix:__
    Use `.style.removeProperty()` instead to speed up the process and to
    make it less error prone.
    
    [1]: https://github.com/odoo/odoo/commit/85698bf4f591fc9280f054b9a255a7d
    [2]: https://github.com/odoo/odoo/commit/781c0ae77de01f0df8667e0191f6c34
    
    X-original-commit: 4e4166fc1d7b3a566af1ec268185e404edb623ce

--- addons/html_editor/static/src/main/media/image_plugin.js
+++ addons/html_editor/static/src/main/media/image_plugin.js
@@ -388,12 +388,17 @@ export class ImagePlugin extends Plugin {
     }
 
     resetImageTransformation(image, { addStep = true } = {}) {
-        image.setAttribute(
-            "style",
-            (image.getAttribute("style") || "").replace(/[^;]*transform[\w:]*;?/g, "")
-        );
-        image.style.removeProperty("width");
-        image.style.removeProperty("height");
+        const stylePropertiesToRemove = [
+            "transform",
+            "transform-box",
+            "transform-origin",
+            "transform-style",
+            "width",
+            "height",
+        ];
+        for (const styleProperty of stylePropertiesToRemove) {
+            image.style.removeProperty(styleProperty);
+        }
         if (addStep) {
             this.dependencies.history.addStep();
         }
