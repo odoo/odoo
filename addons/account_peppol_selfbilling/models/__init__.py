fc646b1b6510817f6adad1b5394b41f3df3c681b
Gauthier Wala (gawa) <gawa@odoo.com>
2026-03-17 09:12:17 +0000

    [FIX] account_peppol: selfbilling added by default
    Everybody is now able to receive self billing invoices even without
    the additional module.
    So the service should be added to the base module.
    
    Also remove xRechung because users are not supposed to receive it,
    only government.
    
    X-original-commit: 53e583c6e5443b374c9dab5ea25d3824835a61ca

---
+++ addons/account_peppol_selfbilling/models/__init__.py
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+||||||| MERGE BASE
+=======
+from . import account_edi_xml_ubl_bis3
+from . import account_journal
+from . import account_move_send
+from . import account_move
+
+>>>>>>> FORWARD PORTED
