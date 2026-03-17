c34de7b883358c4558cae4ec9bb1305311c0acde
Ziri <zirah@odoo.com>
2026-03-17 11:25:48 +0100

    [PERF] hr: call of hr_version without a domain
    The widget versions_timeline called a search on hr_version without a domain.
    This made the search take 6 seconds in prod as it is fetching all versions for
    all employees instead of just the employee we are looking at.
    
    It also removed the cache and made the same call multiple times when it was not
    needed. In the commit we improve the way we can override the original functions
    to prevent this from happening.
    
    task-6044049

--- addons/web/static/src/views/fields/statusbar/statusbar_field.js
+++ addons/web/static/src/views/fields/statusbar/statusbar_field.js
@@ -98,7 +98,7 @@ export class StatusBarField extends Component {
             this.specialData = useSpecialData(async (orm, props) => {
                 const { foldField, name: fieldName, record } = props;
                 const { relation } = record.fields[fieldName];
-                const fieldNames = ["display_name"];
+                const fieldNames = this.getFieldNames();
                 if (foldField) {
                     fieldNames.push(foldField);
                 }
@@ -175,6 +175,13 @@ export class StatusBarField extends Component {
      */
     getDomain() {
         return [];
+    }
+
+    /**
+     * Override this to change the fields to fetch
+     */
+    getFieldNames() {
+        return ['display_name'];
     }
 
     /**
