982b07871ee1c8b0ec68364ecd7d285a3f18925c
djameltouati <otd@odoo.com>
2026-03-17 09:59:34 +0000

    [FIX] mrp_subcontracting: prevent duplicate RFQs by reusing stock ref
    Steps to reproduce:
    - Go to Azure Interior contact form:
      - Sales & Purchase tab > Group RFQs > Always
    - Create a storable product "P1":
      - Route: Resupply Subcontractor
      - BoM:
        - Type: Subcontracting
        - Component: C1 with Dropship route and azure interior as vendor
    - Create a storable product "P2":
      - Route: Resupply Subcontractor
      - BoM:
        - Type: Subcontracting
        - Component: C2 with Dropship route and azure interior as vendor
    - Create a purchase order with one unit of P1 and P2
    - Confirm the purchase order
    
    Problem:
    Two RFQs are generated (one for C1 and one for C2) instead of a single
    grouped RFQ.
    
    When confirming the purchase order, a stock reference is created and
    assigned to it. This reference is correctly propagated to the picking
    and stock moves.
    
    However, when the two manufacturing orders are created, new stock
    references are generated instead of reusing the original one.
    As a result, each MO ends up with a different reference:
    
    https://github.com/odoo/odoo/blob/2713876dbc70d3984e584a9037a2206dcda4e84a/addons/mrp_subcontracting/models/stock_picking.py#L69-L72
    
    This prevents from grouping RFQs, since the grouping logic relies on
    identical references. When the first RFQ is created for C1, it uses the
    MO reference, and when searching for a candidate RFQ for C2, none is
    found due to the reference mismatch:
    https://github.com/odoo/odoo/commit/77dc92e4a0db0575a1a0e865cdd0c82978d593f6#diff-201f9fd7ec1067f7530aafeaa88420021930be82ce89e912d595ed55bb885f26R370-R372
    
    Solution:
    Reuse existing stock references when creating subcontracting MOs instead
    of creating new ones.
    
    opw-5895378
    
    X-original-commit: 4f0bdaf6285c4cf9b39ee66e723026426e3bcefe

--- addons/mrp_subcontracting/models/stock_picking.py
+++ addons/mrp_subcontracting/models/stock_picking.py
@@ -104,10 +104,14 @@ class StockPicking(models.Model):
 
     def _prepare_subcontract_mo_vals(self, subcontract_move, bom):
         subcontract_move.ensure_one()
-        reference = self.env['stock.reference'].create({
-            'name': self.name,
-            'move_ids': [Command.link(subcontract_move.id)],
-        })
+        if not self.reference_ids:
+            references = self.env['stock.reference'].create({
+                'name': self.name,
+                'move_ids': [Command.link(subcontract_move.id)],
+            })
+        else:
+            references = self.reference_ids
+
         product = subcontract_move.product_id
         warehouse = self._get_warehouse(subcontract_move)
         subcontracting_location = \
@@ -126,7 +130,7 @@ class StockPicking(models.Model):
             'picking_type_id': warehouse.subcontracting_type_id.id,
             'date_start': subcontract_move.date - relativedelta(days=bom.produce_delay),
             'origin': self.name,
-            'reference_ids': [Command.link(reference.id)],
+            'reference_ids': [Command.link(ref.id) for ref in references],
         }
         return vals
 
