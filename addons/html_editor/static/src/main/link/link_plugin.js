be558831ac5f9c92d80d2c065350c05f0c3e972d
Deependra Solanki <deso@odoo.com>
2026-03-18 10:18:06 +0000

    [PERF] html_editor: improve performance of normalizeLink
    This commit improves performance of normalizeLink by replacing the usage of
    isBlock with isPhrasingContent which aligns more with the usecase.
    For 50 such non base container divs (`<div>test</div>`) time of
    normalizeDivBaseContainers reduced from around 110.09ms to 1.79ms.
    
    task-5245157
    
    X-original-commit: 8948255520f81a28cb1d3c7089dbb4fff76723c7

--- addons/html_editor/static/src/main/link/link_plugin.js
+++ addons/html_editor/static/src/main/link/link_plugin.js
@@ -9,6 +9,7 @@ import { EMAIL_REGEX, URL_REGEX, cleanZWChars, deduceURLfromText } from "./utils
 import {
     isElement,
     isStylable,
+    isPhrasingContent,
     isProtected,
     isProtecting,
     isVisible,
@@ -749,7 +750,7 @@ export class LinkPlugin extends Plugin {
             // create a font tag inside it, and move the color to the font tag.
             // This ensures the color is applied to the font element instead of
             // the anchor element itself.
-            if (color && childNodes.every((n) => !isBlock(n))) {
+            if (color && childNodes.every(isPhrasingContent)) {
                 anchorEl.style.removeProperty("color");
                 const font =
                     anchorEl.nodeName === "FONT" ? anchorEl : anchorEl.querySelector("font");
