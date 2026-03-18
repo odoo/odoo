dc9c18c312f28e4408f409fd229238e5499774b4
igbe <igbe@odoo.com>
2026-03-18 09:00:14 +0000

    [FIX] account_payment: No partial reconcile on payments from providers
    When we receive a payment from a provider, we allow partial reconciliations
    to be done on this move, but we shouldn't.
    
    Payments coming from providers are always either fully paid, or not paid
    at all.
    
    task-5893189
    
    closes odoo/odoo#266528
    
    X-original-commit: 12da2b1783221a4b06d26d184fd54baae389be4f
    Signed-off-by: Maximilien La Barre (malb) <malb@odoo.com>
    Signed-off-by: Igor Bertrand (igbe) <igbe@odoo.com>

--- addons/account_payment/models/__init__.py
+++ addons/account_payment/models/__init__.py
@@ -1,5 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import account_bank_statement_line
 from . import account_journal
 from . import account_move
 from . import account_payment
---
+++ addons/account_payment/models/account_bank_statement_line.py
@@ -0,0 +1,15 @@
+import re
+
+from odoo import models
+
+
+class AccountBankStatementLine(models.Model):
+    _inherit = 'account.bank.statement.line'
+
+    def _get_partial_amounts(self, current_balance, move_line, open_amount_currency, open_balance):
+        # If a payment comes from a provider or is an iso/sepa payment, we don't want to allow a partial reconciliation on it
+        for payment in move_line.move_id.payment_ids:
+            if re.match(r"^iso20022.*|^sepa_ct$", payment.payment_method_code) or payment.payment_transaction_id:
+                return None
+
+        return super()._get_partial_amounts(current_balance, move_line, open_amount_currency, open_balance)
