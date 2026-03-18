d8fae4db2066f85528da7ea353da4017ef7cf30d
unka-odoo <unka@odoo.com>
2026-03-18 10:02:16 +0000

    [IMP] account,l10n_ma,l10n_in: default outstanding accounts on bank journals
    Reason:
    - Moroccan companies usually use cash basis accounting. And the cash basis
      entries are only made when the invoices are reconciled with the bank
      transactions. However, in Morocco, there is no Moroccan bank available for
      bank synchronization.
    
    Before this commit:
    - We are not assigning the outstanding accounts on the bank journals by default.
    - Which is causing the issues when the user creates a payment without an entry
      and without having any bank transactions to reconcile it with. Therefore, the
      tax report won't show the moves and taxes that occurred in the period.
    
    After this commit:
    - Introduced a method for updating the accounts on the payment method lines of
      the bank journal in the account module, as we need the same functionalities in
      l10n_in as well.
    - For Moroccan localization, from now on, we are setting the outstanding accounts
      automatically on the bank journals.
    - The payment accounts are applied by default during CoA loading and whenever
      payment method lines are recomputed, ensuring accounts remain consistent.
    
    Task-6041119
    
    X-original-commit: 876f08b648003f5b801ce0910f9e0ba3e228b172

---
+++ addons/l10n_ma/models/account_journal.py
@@ -0,0 +1,13 @@
+from odoo import models
+
+
+class AccountJournal(models.Model):
+    _inherit = 'account.journal'
+
+    def _compute_inbound_payment_method_line_ids(self):
+        super()._compute_inbound_payment_method_line_ids()
+        self._assign_outsanding_account_to_payment_method_lines('inbound', chart_template='ma')
+
+    def _compute_outbound_payment_method_line_ids(self):
+        super()._compute_outbound_payment_method_line_ids()
+        self._assign_outsanding_account_to_payment_method_lines('outbound', chart_template='ma')
