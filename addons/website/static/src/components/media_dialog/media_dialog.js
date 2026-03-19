71ce80a09a0e9e33cdc3a10c41362a8427a8bef7
assk-odoo <assk@odoo.com>
2026-03-19 05:33:08 +0000

    [FIX] html_editor, *: prevent traceback on media dialog double-click
    *:website, html_builder
    
    Steps to reproduce:
    1.Open the website in Edit mode.
    2.Add a snippet with an image.
    3.Replace the image.
    4.After replacing image, double-click on it to open the Media Dialog.
    5.Inside the Media Dialog, double-click on the same or any other image.
    6.Traceback occurs.
    
    Before this commit:
    parentElement was becoming null because parentElement is a DOM
    position check.it requires the element to be physically attached in
    the DOM tree to find its parent.Since this.props.node is not yet in
    the DOM at that point, parentElement returns null.
    
    After this commit:
    1.The issue is fixed using a boolean flag: isProcessingClick. Initially,
    the flag is set to false. When the first click starts, it is updated to
    true. If another click occurs during the same flow
    (e.g during a double click), isProcessingClick is already true, so the
    function immediately returns and prevents the action from being executed
    multiple times.
    2.we now pass closestSnippetEl directly from replaceMedia() through
    params.this allows the dialog taken snippet reference using
    (closestElement(mediaEl , "[data-snippet]")) without depending on the
    media node being attached to the DOM.
    
    task-6033320
    
    closes odoo/odoo#266820
    
    X-original-commit: d92e82e5dc3cf0df0f3abed1a54444bbf9bffbe4
    Signed-off-by: Colin Louis (loco) <loco@odoo.com>
    Signed-off-by: Sujal Kamleshbhai Asodariya (assk) <assk@odoo.com>

--- addons/website/static/src/components/media_dialog/media_dialog.js
+++ addons/website/static/src/components/media_dialog/media_dialog.js
@@ -5,10 +5,10 @@ import { TABS } from "@html_editor/main/media/media_dialog/media_dialog_utils";
 patch(MediaDialog.prototype, {
     extraClassesToAdd() {
         const classes = super.extraClassesToAdd();
-        const closestDivEl = this.props.node?.parentElement?.closest("div");
+        const closestSnippetEl = this.props.closestSnippetEl;
         if (
             this.state.activeTab == TABS.IMAGES.id &&
-            closestDivEl?.matches(".s_social_media, .s_share")
+            closestSnippetEl?.matches(".s_social_media, .s_share")
         ) {
             classes.push("social_media_img");
             for (const element of this.props.node.classList) {
