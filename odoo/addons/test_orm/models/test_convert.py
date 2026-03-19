2395fc9f79f0e9216da33e8f9db50f07431f5608
Ruben Gomes (rugo) <rugo@odoo.com>
2026-03-19 11:11:58 +0100

    test_convert (simple)

--- odoo/addons/test_orm/models/test_convert.py
+++ odoo/addons/test_orm/models/test_convert.py
@@ -1,5 +1,3 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 from odoo import api, fields, models
 
 
@@ -10,27 +8,13 @@ class TestConvertTestModel(models.Model):
     name = fields.Char(translate=True)
     usered_ids = fields.One2many('test_convert.usered', 'test_id')
 
-    @api.model
-    def action_test_date(self, today_date):
-        return True
-
-    @api.model
-    def action_test_time(self, cur_time):
-        return True
-
-    @api.model
-    def action_test_timezone(self, timezone):
-        return True
-
 
 class TestConvertUsered(models.Model):
     _name = 'test_convert.usered'
     _description = "z test model ignore"
 
     name = fields.Char()
-    user_id = fields.Many2one('res.users', default=lambda self: self.env.user)
     test_id = fields.Many2one('test_convert.test_model')
-    tz = fields.Char(default=lambda self: self.env.context.get('tz') or self.env.user.tz)
 
     @api.model
     def model_method(self, *args, **kwargs):
