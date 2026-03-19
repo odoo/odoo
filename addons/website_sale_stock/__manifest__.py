b12d8d9af160b8a5ddbbb10c6596612a257ce455
Florian Malfroid <flmal@odoo.com>
2026-03-19 12:44:55 +0100

    [IMP] website_sale(_stock): Out of stock improvements
    **Purpose:**
    Out of stock was not completely managed in eCommerce.
    
    The out of stock ribbon was not reflecting correctly the status of the partially out of stock products.
    The notification and wishlists buttons were inconsistents with the rest of the features.
    
    **Specification:**
    Shop page:
    - Make the out of stock ribbon appear only if all variants are out of stock.
    - If a product if completely out of stock, hide the add to cart button.
    - If a variant is still available, select the first available variant in
    the configurator or when clicking on the tile to open the product page.
    
    Product page:
    - Make the notification button replace the Add to cart button.
    - Rationalise the wishlist button of out of stock products.
    - When clicking notify me, show form to enter email if not logged in, subscribe directly if logged in
    
    Task-6026756

--- addons/website_sale_stock/__manifest__.py
+++ addons/website_sale_stock/__manifest__.py
@@ -29,7 +29,6 @@ Then it can be made specific at the product level.
             "website_sale_stock/static/src/js/models/*",
             "website_sale_stock/static/src/js/product/*",
             "website_sale_stock/static/src/js/product_card/*",
-            "website_sale_stock/static/src/js/product_configurator_dialog/*",
             "website_sale_stock/static/src/js/location_selector/**/*",
         ],
         "web.assets_backend": [
