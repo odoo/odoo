eeb09a304a6d25c9cb7fc19e3a0fb1c0977b9161
Achraf (abz) <abz@odoo.com>
2026-03-20 10:15:05 +0000

    [FIX] web: Allows users to use selection field hotkeys in list view
    Steps:
    - Open any editable list view (for example sub-list view in sales)
    - Either it has a selection field or you add it via studio
    - - With two values (for example "true" and "false")
    - Add a record to your list view
    - Try to edit the selection field
    - Popover is opened
    - - You can select any value with a mouse click
    - - You can navigate through values with arrows
    - - You can't select values with `Enter` and `Tab`
    
    So list cell in edit mode has a function for all theses hotkeys:
    - `tab`
    - `shift+tab`
    - `enter`
    - `escape`
    
    Because `ListRenderer.onCellKeydown` is called before `hotkeyService.onKeyDown`,
    if any hotkeys is handled in cell edit mode it will be prevented and the
    hotkeyService will not propagate it to `select_menu`.
    That's why arrows are working, because there are not listed in cell edit
    mode keys.
    
    opw-6025476
    
    closes odoo/odoo#260002
    
    X-original-commit: 6c9c0303bb798af8e0c75ae5b4033b0ee707e158
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/tests/views/list/list_view.test.js
+++ addons/web/static/tests/views/list/list_view.test.js
@@ -20736,3 +20736,46 @@ test(`Grouped list: count and pager behavior - mobile`, async () => {
     expect(`.o_group_header:eq(1) th:eq(-1) .btn-group`).toBeVisible();
     expect(`.o_group_header:eq(1) th:eq(-1) .o_pager_counter`).toHaveCount(0);
 });
+
+test.tags("desktop");
+test(`select menu navigation with hot keys`, async () => {
+    Bar._fields.stage = fields.Selection({
+        selection: [
+            ["aab", "aab"],
+            ["aac", "aac"],
+        ],
+    });
+
+    await mountView({
+        resModel: "foo",
+        type: "form",
+        arch: `
+            <form>
+                <field name="o2m">
+                    <list editable="top">
+                        <field name="stage"/>
+                    </list>
+                </field>
+            </form>
+        `,
+        resId: 1,
+    });
+    await contains(".o_field_x2many_list_row_add button").click();
+    await contains(`.o_field_widget[name=o2m] .o_data_row [name=stage] input`).click();
+    await press("Tab");
+    await animationFrame();
+    await press("Enter");
+    await animationFrame();
+
+    await contains(".o_field_x2many_list_row_add button").click();
+    await contains(`.o_field_widget[name=o2m] .o_data_row [name=stage] input`).click();
+    await press("ArrowLeft");
+    await animationFrame();
+    await press("ArrowLeft");
+    await animationFrame();
+    await press("Enter");
+    await animationFrame();
+
+    await contains(`.o_form_button_save`).click();
+    expect(queryAllTexts(`.o_field_x2many_list .o_data_row`)).toEqual(["aab", "aac"]);
+});
