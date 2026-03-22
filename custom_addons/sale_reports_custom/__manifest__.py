6cbc687af2f4cdeff9a8b51b8c28aaeaf08222ce
jose gomez <jose.gs1574@gmail.com>
2026-03-22 19:37:24 -0400

    se  creo un reporte PDF personalizado para cotizaciones y ventas

---
+++ custom_addons/sale_reports_custom/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Goca, Reporte Personalizado para Cotizaciones',
+    'version': '1.0',
+    'author': 'GOCA C.A.',
+    'category': 'Sales',
+    'summary': 'Reporte Personalizado para cotizaciones',
+    'depends': ['sale'],
+    'data': [
+        'report/ir_actions_report.xml',
+        'report/sale_report_template.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
