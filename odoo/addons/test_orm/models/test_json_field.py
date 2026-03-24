54245892ce8ed5bd88886f3f460e12cdab641dfa
Ruben Gomes (rugo) <rugo@odoo.com>
2026-03-24 10:38:29 +0100

    test_json_field (refactor)

--- odoo/addons/test_orm/models/test_json_field.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class TestJsonFieldDiscussion(models.Model):
-    _name = 'test_json_field.discussion'
-    _description = 'Test ORM Discussion'
-
-    name = fields.Char(required=True)
-    history = fields.Json('History', default={'delete_messages': []})
--- odoo/addons/test_orm/tests/test_json_field.py
+++ odoo/addons/test_orm/tests/test_json_field.py
@@ -7,31 +7,29 @@ class JsonFieldTest(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.discussion_1 = cls.env['test_json_field.discussion'].create({
-            'name': 'Test Discussion JSON field',
-        })
+        cls.discussion_1 = cls.env['test_fields.misc'].create({})
 
     def test_json_field_read_write(self):
         random_str = "tVpajMuSvaR94DwSRVtRSLGNkKViNbWHt2hq13"
         random_str_2 = "Crypto-money base"
 
-        self.assertEqual(self.discussion_1.history, {'delete_messages': []})
+        self.assertEqual(self.discussion_1.json_default, {'values': []})
 
         # Check that it is not the value of the cache return by convert_to_record
-        self.assertIsNot(self.discussion_1.history, self.discussion_1._cache['history'])
+        self.assertIsNot(self.discussion_1.json_default, self.discussion_1._cache['json_default'])
 
-        self.assertEqual(self.discussion_1.history, {'delete_messages': []})
+        self.assertEqual(self.discussion_1.json_default, {'values': []})
 
-        self.discussion_1.history = {'delete_messages': [random_str]}
+        self.discussion_1.json_default = {'values': [random_str]}
         self.discussion_1.flush_recordset()
-        self.assertEqual(self.discussion_1.history, {'delete_messages': [random_str]})
+        self.assertEqual(self.discussion_1.json_default, {'values': [random_str]})
 
-        self.discussion_1.history = {'delete_messages': [random_str, random_str_2]}
-        self.discussion_1.flush_recordset()
-
-        self.assertEqual(self.discussion_1.history, {'delete_messages': [random_str, random_str_2]})
-
-        self.discussion_1.history = (random_str, random_str_2)
+        self.discussion_1.json_default = {'values': [random_str, random_str_2]}
         self.discussion_1.flush_recordset()
 
-        self.assertEqual(self.discussion_1.history, [random_str, random_str_2])
+        self.assertEqual(self.discussion_1.json_default, {'values': [random_str, random_str_2]})
+
+        self.discussion_1.json_default = (random_str, random_str_2)
+        self.discussion_1.flush_recordset()
+
+        self.assertEqual(self.discussion_1.json_default, [random_str, random_str_2])
