fleetapps/odoo3

[FIX] hr_attendance: handle timezone overlaps in overtime intervals
Steps to reproduce (singleton crash):

Create an employee in a UTC+ timezone (e.g. Asia/Shanghai or Australia/Adelaide) with an overtime
ruleset containing a paid rule. Generate work entries for a period, then create two consecutive
midnight-to-midnight attendances in the employee's local timezone. Creating the second attendance
crashes with: "ValueError: Expected singleton: hr.attendance.overtime.line(...)".

Steps to reproduce (stale overtime lines):

With the same setup, delete the attendance after it generated overtime lines. The overtime lines
remain in the database instead of being removed.

The singleton crash occurred because `end_of_day` in `_get_overtime_intervals` was computed as a
naive datetime, implicitly treated as UTC. For UTC+ timezones, the actual local end of day is
earlier than UTC midnight. As a result, overtime intervals were computed with a stop time extending
past the real local midnight into UTC time. When consecutive attendances were processed together,
these extended intervals overlapped. The `Intervals` class (`keep_distinct=True`) merges
overlapping intervals into a single entry with a multi-record recordset as payload. The subsequent
`overtime.rule_ids.work_entry_type_id` and `overtime.status` calls expected a singleton but
received a multi-record set, causing the crash. The same multi-record issue also affected the
iteration in `_set_real_overtime_intervals` and the overtime work entry loop in
`_get_attendance_intervals`.

The stale overtime lines issue occurred because `_get_overtimes_to_update_domain` built its search
date range from raw UTC `.date()` values instead of the employee's local timezone. For UTC+
employees whose attendance spans local midnight, the overtime line is dated in the next local
calendar day. Since the domain was derived from UTC dates, that next local day fell outside the
search range, so the overtime line was never found and deleted when the a

addons/hr_attendance/models/hr_attendance.py
@@ -182,8 +182,8 @@ class HrAttendance(models.Model):
         calendar = self._get_employee_calendar()
         resource = self.employee_id.resource_id
         tz = timezone(resource.tz) if not calendar else timezone(calendar.tz)
-        start_dt_tz = max(self.check_in, start_dt).astimezone(tz)
-        end_dt_tz = min(self.check_out, end_dt).astimezone(tz)
+        start_dt_tz = utc.localize(max(self.check_in, start_dt)).astimezone(tz)
+        end_dt_tz = utc.localize(min(self.check_out, end_dt)).astimezone(tz)
 
         if end_dt_tz < start_dt_tz:
             return 0.0
@@ -268,11 +268,19 @@ class HrAttendance(models.Model):
     def _get_overtimes_to_update_domain(self):
         if not self:
             return Domain.FALSE
-        domain_list = [Domain.AND([
-            Domain('employee_id', '=', employee.id),
-            Domain('date', '<=', max(attendances.mapped('check_out')).date() + relativedelta(SU)),
-            Domain('date', '>=', min(attendances.mapped('check_in')).date() + relativedelta(MO(-1))),
-        ]) for employee, attendances in self.filtered(lambda att: att.check_out).grouped('employee_id').items()]
+        domain_list = []
+        for employee, attendances in self.filtered(lambda att: att.check_out).grouped('employee_id').items():
+            tz = timezone(employee._get_tz())
+            local_check_in = utc.localize(min(attendances.mapped('check_in'))).astimezone(tz)
+            local_check_out = utc.localize(max(attendances.mapped('check_out'))).astimezone(tz)
+            date_from = local_check_in.date() + relativedelta(MO(-1))
+            date_to = local_check_out.date() + relativedelta(SU)
+
+            domain_list.append(Domain.AND([
+                Domain('employee_id', '=', employee.id),
+                Domain('date', '<=', date_to),
+                Domain('date', '>=', date_from),
+            ]))
         if not domain_list:
             return Domain.FALSE
         return Domain.OR(domain_list) if len(domain_list) > 1 else domain_list[0]

odoo/odoo

X-original-commit: 5726a4dee866a83dc3509ea52f55221b61dd354e

addons/hr_attendance/models/hr_attendance.py
@@ -194,8 +194,8 @@ class HrAttendance(models.Model):
         """
         self.ensure_one()
         tz = ZoneInfo(self.employee_id._get_tz(self.check_in))
-        start_dt_tz = max(self.check_in, start_dt).astimezone(tz)
-        end_dt_tz = min(self.check_out, end_dt).astimezone(tz)
+        start_dt_tz = max(self.check_in, start_dt).replace(tzinfo=UTC).astimezone(tz)
+        end_dt_tz = min(self.check_out, end_dt).replace(tzinfo=UTC).astimezone(tz)
 
         if end_dt_tz < start_dt_tz:
             return 0.0
@@ -277,11 +277,19 @@ class HrAttendance(models.Model):
     def _get_overtimes_to_update_domain(self):
         if not self:
             return Domain.FALSE
-        domain_list = [Domain.AND([
-            Domain('employee_id', '=', employee.id),
-            Domain('date', '<=', max(attendances.mapped('check_out')).date() + relativedelta(weekday=SU(1))),
-            Domain('date', '>=', min(attendances.mapped('check_in')).date() + relativedelta(weekday=MO(-1))),
-        ]) for employee, attendances in self.filtered(lambda att: att.check_out).grouped('employee_id').items()]
+        domain_list = []
+        for employee, attendances in self.filtered(lambda att: att.check_out).grouped('employee_id').items():
+            tz = ZoneInfo(employee._get_tz())
+            local_check_in = min(attendances.mapped('check_in')).replace(tzinfo=UTC).astimezone(tz)
+            local_check_out = max(attendances.mapped('check_out')).replace(tzinfo=UTC).astimezone(tz)
+            date_from = local_check_in.date() + relativedelta(weekday=MO(-1))
+            date_to = local_check_out.date() + relativedelta(weekday=SU)
+
+            domain_list.append(Domain.AND([
+                Domain('employee_id', '=', employee.id),
+                Domain('date', '<=', date_to),
+                Domain('date', '>=', date_from),
+            ]))
         if not domain_list:
             return Domain.FALSE
         return Domain.OR(domain_list) if len(domain_list) > 1 else domain_list[0]