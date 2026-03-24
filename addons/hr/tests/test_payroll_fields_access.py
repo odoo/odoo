56fb9b9548fb11911d2ebdb78226af1b56b834be
william-andre <wan@odoo.com>
2026-03-24 11:51:55 +0100

    [FIX] hr: test hr groups
    The test was comparing the `groups` attribute of the `hr.employee` with
    the field of the `hr.version` instead of it's groups

--- addons/hr/tests/test_payroll_fields_access.py
+++ addons/hr/tests/test_payroll_fields_access.py
@@ -28,9 +28,9 @@ class TestPayrollFieldsAccess(TransactionCase):
         fields_readonly = []
         for f_name, field in employee_fields.items():
             v_field = version_fields[f_name]
-            if not (field.groups and field.groups != v_field):
+            if field.groups != v_field.groups:
                 fields_without_group.append(f_name)
-            elif not (field.related and field.related == f'version_id.{f_name}'):
+            elif field.related != f'version_id.{f_name}':
                 fields_without_related.append(f_name)
             elif field.readonly != v_field.readonly:
                 fields_readonly.append(f_name)
