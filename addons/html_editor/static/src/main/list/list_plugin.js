77fff5237c2c578364f761950039ae983d7c98b1
dhba <dhba@odoo.com>
2026-03-25 18:38:07 +0530

    [FIX] html_editor: prevent checklist pasting in email marketing
    In email marketing, checklist creation is disabled. However, it was still
    possible to paste checklist content from other sources.
    
    This fix ensures that when a checklist is pasted into the email editor, only its
    content gets pasted and not the entire checklist.
    
    task-6048027

--- addons/html_editor/static/src/main/list/list_plugin.js
+++ addons/html_editor/static/src/main/list/list_plugin.js
@@ -191,6 +191,7 @@ export class ListPlugin extends Plugin {
         node_to_insert_processors: this.processNodeToInsert.bind(this),
         clipboard_content_processors: this.processContentForClipboard.bind(this),
         before_insert_within_pre_processors: this.insertListWithinPre.bind(this),
+        before_insert_processors: this.handleInsert.bind(this),
         triple_click_overrides: this.handleTripleClick.bind(this),
 
         fully_selected_node_predicates: (node, selection, range) => {
@@ -1300,5 +1301,14 @@ export class ListPlugin extends Plugin {
                     description: command.title,
                 };
             });
+    }
+
+    handleInsert(container, block) {
+        if (!this.config.allowChecklist) {
+            for (const list of container.querySelectorAll(".o_checklist > li")) {
+                this.liToBlocks(list);
+            }
+        }
+        return container;
     }
 }
