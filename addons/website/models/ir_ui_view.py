1c1319bd161e913d273735124ecf3d1294823d4a
Christophe Matthieu <chm@odoo.com>
2026-03-25 17:41:11 +0100

    [FIX] website: The depends_context of arch related crushes the depends
    The value of website page may be incorrect if it has been cached in
    another language because `depends_context=('website_id',)` overwrites
    the value of `@api.depends_context`.

--- addons/website/models/ir_ui_view.py
+++ addons/website/models/ir_ui_view.py
@@ -38,6 +38,10 @@ class IrUiView(models.Model):
     visibility_password = fields.Char(groups='base.group_system', copy=False)
     visibility_password_display = fields.Char(compute='_get_pwd', inverse='_set_pwd', groups='website.group_website_designer')
 
+    @api.depends_context('website_id')
+    def _compute_arch(self):
+        super()._compute_arch()
+
     @api.depends('visibility_password')
     def _get_pwd(self):
         for r in self:
