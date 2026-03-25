cf0a65ad53a8478f565f653dec8c77bc994c8c38
kcv-odoo <kcv@odoo.com>
2026-03-25 12:50:26 +0530

    [REF] base,portal{,address_extended},*: refactor portal address
    This commit improve portal address layout and refactor address logic:
    
    - Improve countrty's form view to make title bigger.
    - Make zip_required (boolean) field a selection field zip_applicability
    with not_applicable, optional and required values and update countries
    data, now in portal page zip will only be visible if country does not
    have zip not_applcable.
    - Merge `portal_address_extended` module in portal module
    to make it easier to maintain by making `portal` module dependent
    on `base_address_extended`.
    - Improve portal address layout and adapt localisation.
    - Also updated base_address_extended dependency to make
    it independent of contacts app and move `Cities` menu in
    contacts app and make it dependent on `base_address_extended`.
    
    task-5208254

---
+++ addons/portal/models/res_country.py
@@ -0,0 +1,98 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, models
+from odoo.fields import Domain
+
+
+class ResCountry(models.Model):
+    _inherit = "res.country"
+
+    def _is_zip_before_city(self, default_address_fields=()):
+        if not self or self.zip_applicability == "not_applicable":
+            return False
+
+        address_fields = default_address_fields or self._get_address_fields()
+        if not address_fields or "zip" not in address_fields:
+            return False
+
+        return address_fields.index("zip") < address_fields.index(
+            self._get_partner_city_field()
+        )
+
+    def _get_address_fields(self):
+        address_format_fields = (self and self.get_address_fields()) or [
+            "street",
+            "zip",
+            "city",
+        ]
+
+        # Add state_name in address fields even though it's not in `address_format` if country has
+        # states.
+        if (
+            "state_name" not in address_format_fields
+            and "state_code" not in address_format_fields
+            and self.state_ids
+        ):
+            address_format_fields.append("state_id")
+
+        if self.zip_applicability == "not_applicable":
+            address_format_fields.remove("zip")
+
+        # Maps `res.country` 'address_format' fields to `res.partner` fields that have to be set on
+        # the address page.
+        mapping = self._get_address_format_fields_mapping()
+        return [mapping.get(fname, fname) for fname in address_format_fields]
+
+    @api.model
+    def _get_address_format_fields_mapping(self):
+        """Return mapping of `res.country` 'address_format' fields to `res.partner` fields that
+        have to be set on the address page.
+        """
+        fields_mapping = {
+            "state_name": "state_id",
+            "state_code": "state_id",
+            "country_name": "country_id",
+        }
+        if self._enforce_city_choice():
+            fields_mapping["city"] = "city_id"
+
+        return fields_mapping
+
+    def _get_cities_data(self, state_id=False):
+        self.ensure_one()
+        domain = Domain("country_id", "=", self.id)
+        if state_id:
+            domain = Domain.AND([domain, Domain("state_id", "in", [state_id, False])])
+
+        return self.env["res.city"].sudo().search_read(
+            domain=domain,
+            fields=self._get_cities_fields_to_fetch(),
+            load='',  # we only want the ids of relational fields
+        )
+
+    @api.model
+    def _get_cities_fields_to_fetch(self):
+        return ["id", "name", "zipcode", "state_id"]
+
+    def _get_partner_city_field(self):
+        """Return city field based on `_enforce_city_choice`."""
+        if self._enforce_city_choice():
+            return "city_id"
+        return "city"
+
+    def _enforce_city_choice(self):
+        if not self:
+            return False
+        self.ensure_one()
+
+        # Only enabled on frontend for those countries for now
+        # Feature has to be adapted to be more generic and less blocking
+        # before being enabled for other countries
+        if self.code not in {"BR", "CL", "PE", "CO", "TW"}:
+            return False
+        return self.enforce_cities and bool(
+            self.env["res.city"].search_count([("country_id", "=", self.id)], limit=1)
+        )
+
+    def _get_vat_label(self):
+        return self.vat_label or self.env.company.country_id.vat_label or self.env._("VAT")
