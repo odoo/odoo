3cce7e452c9d3c8e387878f0c7af6ace77879278
Léo Gizard <legi@odoo.com>
2026-03-26 10:09:51 +0000

    [FIX] orm,account,l10n_it_edi: stops rounding the discount on import
    **PROBLEM**
    When importing an invoice, we don't want to round the discounts, to avoid
    discrepancy between the subtotal computed by Odoo, and the subtotal of the file we import.
    To do this, we change the decimal precision of discount to 100 digits when importing files.
    However, float_round wasn't built with this in mind, in float round, we add a small epsilon to fix some rounding issue.
    This small epsilon changes the amount of the discount (50.0 -> 0.5000000000004) and this changes the subtotal.
    
    **STEP TO REPRODUCE**
    1. Install l10n_edi_it.
    2. Change the VAT number of IT Company to 05098540288 (to match the one on the file to import).
    3. Import the file present in the bug ticket.
    4. Notice the subtotal of the line doesn't match what's in the invoice.
    
    opw-6046324
    
    closes odoo/odoo#262251
    
    X-original-commit: 7ebce9bbef8d29cacabb6b82ea7d4002edca4ff9
    Signed-off-by: Paolo Gatti (pgi) <pgi@odoo.com>
    Signed-off-by: Léo Gizard (legi) <legi@odoo.com>

--- addons/account/models/decimal_precision.py
+++ addons/account/models/decimal_precision.py
@@ -6,5 +6,5 @@ class DecimalPrecision(models.Model):
     def precision_get(self, application):
         stackmap = self.env.cr.cache.get('account_disable_recursion_stack', {})
         if stackmap.get('ignore_discount_precision') and application in ('Discount', 'Product Unit of Measure'):
-            return 14
+            return 13
         return super().precision_get(application)
