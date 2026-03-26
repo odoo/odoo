e972866807d94f16f2caea01dd20a7e56c191707
Max Whale <mawh@odoo.com>
2026-03-26 09:31:13 +0100

    [FIX] pos_self_order: payment terminals not working from kiosk
    Since odoo/odoo#249455 the order payload from the self order frontend is
    being checked to ensure the data is valid. However, these checks did not
    account for the situation where payment data is included, as is the case
    when using a self order kiosk with a payment terminal connected. The
    result was that using the kiosk with a payment terminal would result in
    an error even though the payment was successful on the terminal.
    
    Steps to reproduce:
    - Configure a self order kiosk POS
    - Connect a payment terminal (Adyen, Stripe, Viva etc.)
    - Try to pay for an order from the kiosk
    
    EXPECTED:
    - The order is made successfully.
    
    ACTUAL:
    - A generic error message is shown and the order fails. The payment
      goes through successfully on the payment terminal.
    
    The fix is to allow a payment to be included in the self order data, but
    only in Kiosk mode and only if the payment amount is valid.
    
    We also now validate that the self ordering type is correct, this
    ensures that a kiosk payment cannot be spoofed for a mobile self order
    POS.
    
    Finally, there is also a small change for Viva.com to use the PoS config
    name instead of UUID, since UUID is no longer loaded in self order.

--- addons/pos_viva_com/models/pos_payment.py
+++ addons/pos_viva_com/models/pos_payment.py
@@ -1,7 +1,11 @@
-from odoo import models, fields
+from odoo import api, models, fields
 
 
 class PosPayment(models.Model):
     _inherit = "pos.payment"
 
     viva_com_session_id = fields.Char(help="Session ID of the transaction, stored so that it can be used to refund the payment.")
+
+    @api.model
+    def _get_additional_payment_fields(self):
+        return super()._get_additional_payment_fields() + ["viva_com_session_id"]
