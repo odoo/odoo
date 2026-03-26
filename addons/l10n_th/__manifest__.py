02d40bd4d69758f6bd4688486025e83bc23c7aa7
dijo-odoo <dijo@odoo.com>
2026-03-26 14:56:16 +0530

    [IMP] l10n_th: is_company as per vat
    In this commit:
    - For Thailand, TIN is distinguishable between an individual and a company
      by a prefix.
    - TIN starting with '0' means the contact is a registered corporate entity,
      such as a company or partnership. If TIN begins with any digit from 1-9,
      it indicates that the contact should be treated as an Individual.
    
    task-6002111
    
    closes odoo/odoo#256027
    
    Related: odoo/enterprise#112434
    Signed-off-by: Nicolas Viseur (vin) <vin@odoo.com>

--- addons/l10n_th/__manifest__.py
+++ addons/l10n_th/__manifest__.py
@@ -22,6 +22,7 @@ Thai accounting chart and localization.
     'data': [
         'data/account_tax_report_data.xml',
         'views/report_invoice.xml',
+        'views/res_partner_view.xml',
     ],
     'demo': [
         'demo/demo_company.xml',
--- addons/l10n_th/tests/__init__.py
+++ addons/l10n_th/tests/__init__.py
@@ -1,1 +1,2 @@
+from . import test_res_partner
 from . import test_l10n_th_emv_qr
