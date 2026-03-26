04141b4b0ba4971c9d2d431500a7fc43542cd3b7
Walid (wasa) <wasa@odoo.com>
2026-03-26 10:31:25 +0000

    [FIX] html_builder: fix table row menu position in RTL layout
    Problem:
    In RTL websites, the table row menu is positioned incorrectly.
    
    Cause:
    The `inlineStartOffset` calculation in `table_menu` depends on the
    `direction` parameter, which was not passed during the editor
    initialization.
    
    Solution:
    Ensure the `direction` parameter is properly passed during editor
    initialization so the `inlineStartOffset` is computed correctly in
    RTL layouts.
    
    Steps to reproduce:
    - Change the website language to Arabic.
    - Add a text block and insert a table.
    - Hover over the first table row.
    - Observe that the row menu is misplaced.
    
    opw-6049260
    
    X-original-commit: d2844b6da82730a89936bd6f2c83998dd843be00

--- addons/html_builder/static/src/builder.js
+++ addons/html_builder/static/src/builder.js
@@ -23,6 +23,7 @@ import { setBuilderCSSVariables } from "@html_builder/utils/utils_css";
 import { withSequence } from "@html_editor/utils/resource";
 import { getHtmlStyle } from "@html_editor/utils/formatting";
 import { isVisible } from "@html_builder/utils/utils";
+import { localization } from "@web/core/l10n/localization";
 
 /**
  * @typedef {(() => void)[]} on_mobile_preview_clicked_handlers
@@ -202,6 +203,7 @@ export class Builder extends Component {
                 cleanEmptyStructuralContainers: false,
                 isEditableRTL: false,
                 publicAttachments: true,
+                direction: localization.direction || "ltr",
             },
             this.env.services
         );
