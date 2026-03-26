0d6ae0ac250da9b39bc440e4f66942b655eb962c
Aaron Bohy <aab@odoo.com>
2026-03-26 21:11:32 +0100

    [FIX] web: hoot: cleanup mediaDevices listeners
    This commit fixes a memory leak in the test_unit_desktop suite, in
    the voip tests. The voip AudioManager registers event listeners on
    `navigator.mediaDevices`, but those listeners were never removed.
    As a consequence, audio managers and a bunch of other voip stuff,
    services, components... were leaking.
    
    Part-of: odoo/odoo#256376
    Signed-off-by: Julien Mougenot (jum) <jum@odoo.com>

--- addons/web/static/lib/hoot/mock/window.js
+++ addons/web/static/lib/hoot/mock/window.js
@@ -202,6 +202,7 @@ function getWatchedEventTargets(view) {
         // Other event targets
         EventBus.prototype,
         MockEventTarget.prototype,
+        view.MediaDevices.prototype,
     ];
 }
 
