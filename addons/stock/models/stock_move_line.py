0f9861688e9fb6511f66b77e02a0816df299c2a7
Ester Andreetto <esand@odoo.com>
2026-03-27 17:10:01 +0000

    [FIX] stock: restrict reference field to readonly, not writable by users
    Issue before this commit:
    Before this commit, the Reference field displayed on stock.move.line was
    editable in the user interface, even if the modification made by the
    user was not persisted. After saving and reloading the Move History
    view, the original value was restored.
    
    Steps to reproduce the issue:
    1. Go to Move history in Inventory app
    2. Try to change the name of a line and save
    3. If you go back to the Move history you can see that the name is not
    changed
    
    Cause of the issue:
    The issue was caused by a mismatch between the stock.move.line.reference
    field and its target field stock.move.reference. The field on
    stock.move.line is a related field that appears editable
    (readonly=False), but the underlying stock.move.reference field was not
    writable. As a result, user could edit the field but the modifications
    were ignored, preventing changes from being effectively saved.
    
    Reason to introduce the fix:
    The fix ensures that the reference field in stock.move and in
    stock.move.line are readonly.
    
    opw-6055708
    
    X-original-commit: 6e6bf71f542a06800feadcb38c8065e9ee935447

--- addons/stock/models/stock_move_line.py
+++ addons/stock/models/stock_move_line.py
@@ -86,7 +86,7 @@ class StockMoveLine(models.Model):
     is_scrap = fields.Boolean(related='move_id.is_scrap', readonly=True)
     consume_line_ids = fields.Many2many('stock.move.line', 'stock_move_line_consume_rel', 'consume_line_id', 'produce_line_id')
     produce_line_ids = fields.Many2many('stock.move.line', 'stock_move_line_consume_rel', 'produce_line_id', 'consume_line_id')
-    reference = fields.Char(related='move_id.reference', readonly=False)
+    reference = fields.Char(related='move_id.reference')
     tracking = fields.Selection(related='product_id.tracking', readonly=True)
     origin = fields.Char(related='move_id.origin', string='Source')
     description_picking = fields.Text(related='move_id.description_picking')
