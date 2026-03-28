697471b6ac271cadd35fb0d0c04504121c2e5af7
cesarorozcom <cesarorozcomanotas@gmail.com>
2026-03-28 09:55:30 -0500

    Startup and Spanish translation

--- addons/hr_schedule_erp/hooks.py
+++ addons/hr_schedule_erp/hooks.py
@@ -4,9 +4,9 @@ def post_init_set_home_action(env):
     action = env.ref('hr_schedule_erp.hr_schedule_servicio_action', raise_if_not_found=False)
     if not action:
         return
+
     users = env['res.users'].search([('share', '=', False)])
     users.write({'action_id': action.id})
 
-def post_init_hook(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
+def post_init_hook(env):
     post_init_set_home_action(env)
