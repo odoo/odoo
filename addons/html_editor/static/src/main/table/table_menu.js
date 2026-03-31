41f7175d43956ce41b09f9ddc015ec812b98ee7f
dhba <dhba@odoo.com>
2026-03-31 10:05:14 +0000

    [FIX] html_editor: ensure table menu updates on target change
    After this [1], setup is executed only on the initial mount of the table menu
    and not on subsequent target cell changes.
    
    As a result, colItems, rowItems, and other values found in setup become stale,
    causing the menu to display options that do not reflect the current target cell.
    
    This commit moves the necessary values from setup into useEffect so they
    update correctly when the target cell changes.
    
    task-6111986
    
    [1]: 7d523d6402c9bff3c2e4bcd0329f486a2d0f45ec
    
    Backport of Commit 729c45ddf3d1e377507d93997c5ca45984d64d75
    
    X-original-commit: d1adface2eae9c0a37e7bc1dcce3246dca6ef065

--- addons/html_editor/static/src/main/table/table_menu.js
+++ addons/html_editor/static/src/main/table/table_menu.js
@@ -2,8 +2,8 @@ import { closestElement } from "@html_editor/utils/dom_traversal";
 import {
     Component,
     onMounted,
-    onWillUpdateProps,
     onWillUnmount,
+    useEffect,
     useExternalListener,
     useRef,
 } from "@odoo/owl";
@@ -44,22 +44,11 @@ export class TableMenu extends Component {
     static components = { Dropdown, DropdownItem };
 
     setup() {
-        if (this.props.type === "column") {
-            this.isFirst = this.props.target.cellIndex === 0;
-            this.isLast = !this.props.target.nextElementSibling;
-        } else {
-            const tr = this.props.target.parentElement;
-            this.isFirst = !tr.previousElementSibling;
-            this.isLast = !tr.nextElementSibling;
-            this.isTableHeader = [...tr.children][0].nodeName === "TH";
-        }
-        this.items = this.props.type === "column" ? this.colItems() : this.rowItems();
         this.menuRef = useRef("menuRef");
         const onPointerDown = (ev) => this.onPointerDown(ev);
         onMounted(() => {
             this.overlayEl = this.menuRef.el;
             this.overlayEl.addEventListener("pointerdown", onPointerDown);
-            this.updatePosition(this.props);
         });
         onWillUnmount(() => {
             this.menuRef?.el.removeEventListener("pointerdown", onPointerDown);
@@ -69,12 +58,25 @@ export class TableMenu extends Component {
             // Listen outside the iframe.
             useExternalListener(document, "pointerup", this.onPointerUp);
         }
-        onWillUpdateProps((newProps) => {
-            this.updatePosition(newProps);
-        });
+        useEffect(
+            () => {
+                if (this.props.type === "column") {
+                    this.isFirst = this.props.target.cellIndex === 0;
+                    this.isLast = !this.props.target.nextElementSibling;
+                } else {
+                    const tr = this.props.target.parentElement;
+                    this.isFirst = !tr.previousElementSibling;
+                    this.isLast = !tr.nextElementSibling;
+                    this.isTableHeader = [...tr.children][0].nodeName === "TH";
+                }
+                this.items = this.props.type === "column" ? this.colItems() : this.rowItems();
+                this.updatePosition();
+            },
+            () => [this.props.target]
+        );
         if (this.props.document.defaultView.frameElement) {
             useExternalListener(this.props.document, "scroll", () => {
-                this.updatePosition(this.props);
+                this.updatePosition();
             });
             useExternalListener(this.props.document, "pointerdown", (ev) => {
                 if (!this.overlayEl.contains(ev.target)) {
@@ -120,7 +122,8 @@ export class TableMenu extends Component {
         });
     }
 
-    updatePosition({ target, type, direction }) {
+    updatePosition() {
+        const { target, type, direction } = this.props;
         if (!this.overlayEl || !target) {
             return;
         }
