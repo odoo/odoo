c66ce0db81edd215ca4f76bba3755fadfa2045a0
Thomas (thsc) <thsc@odoo.com>
2026-03-31 16:41:39 +0000

    [FIX] l10n_it_pos: prevent printer deadlock
    Module: l10n_it_pos
    
    Steps to reproduce:
    - In the POS settings, enable "Automatic Receipt Printing";
    - Enable "ePos Printer" to make the "Skip Preview Screen" option appear;
    - Disable "Skip Preview Screen";
    - Disable "ePos Printer";
    - Set up an Italian Fiscal Printer;
    - Open a POS session and process a first order.
    Issue: After the first receipt, no other messages (price display, receipt, open register) are sent to the fiscal printer. A page reload is required.
    
    Cause:
    When "Automatic Receipt Printing" is true but "Skip Preview Screen" is false, a race condition occurs. `afterOrderValidation` triggers a print job while simultaneously transitioning to the `ReceiptScreen`. When the `ReceiptScreen` mounts, it triggers a second fiscal print job before the first has resolved. This creates a deadlock in `toHtml` of `renderService`, permanently blocking the printer queue.
    
    Solution:
    Since the italian localisation sending the receipt to the fiscal printer is mandatory, the printing route is now tied to the "Skip Preview Screen" option.
    
    opw-5979212
    
    X-original-commit: 0ca2fd40756d4abb05beecc2bd1ea581af0b2b0b

--- addons/point_of_sale/static/src/app/screens/payment_screen/payment_screen.js
+++ addons/point_of_sale/static/src/app/screens/payment_screen/payment_screen.js
@@ -389,11 +389,7 @@ export class PaymentScreen extends Component {
         let nextScreen = this.nextScreen;
         let switchScreen = true;
 
-        if (
-            nextScreen === "ReceiptScreen" &&
-            this.currentOrder.nb_print === 0 &&
-            this.pos.config.iface_print_auto
-        ) {
+        if (nextScreen === "ReceiptScreen" && this.currentOrder.nb_print === 0 && this.autoPrint) {
             const invoiced_finalized = this.currentOrder.isToInvoice()
                 ? this.currentOrder.finalized
                 : true;
@@ -421,6 +417,9 @@ export class PaymentScreen extends Component {
                 orderDone: true,
             });
         }
+    }
+    get autoPrint() {
+        return this.pos.config.iface_print_auto;
     }
     selectNextOrder() {
         if (this.currentOrder.originalSplittedOrder) {
