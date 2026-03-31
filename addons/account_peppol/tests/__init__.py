8c8194fcc97e08881cee4b4e073f22fd5fa5f9e6
Louis Gobert <loug@odoo.com>
2026-03-31 12:05:55 +0200

    [FIX] account_peppol: prevent Peppol journal type change
    Prevent changing a Peppol journal to a non-purchase type,
    to avoid import errors when receiving Peppol invoices.
    
    Step to reproduce:
    - Setup a company with Peppol
    - Change the Peppol reception journal type to non-purchase
    - Try to run Peppol cron to import invoice, it fails with
      "Cannot create a purchase document in a non purchase journal"
    
    opw-6071992

--- addons/account_peppol/tests/__init__.py
+++ addons/account_peppol/tests/__init__.py
@@ -1,4 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import test_account_journal
 from . import test_peppol_messages
 from . import test_peppol_participant
