78e3a70f10c64d70c9b6651809b66c0abb7dfc0b
Sébastien Laurent <selau@odoo.com>
2026-03-31 13:37:35 +0000

    [FIX] l10n_pl: Avoiding modification of reconcilation flag on accounts during upgrade
    Context:
    Clients can have some existing accounts with `reconcilable` flag
    set as True. Some of these accounts also have partially reconcilated
    transactions. In Odoo 19.0, it is not authorized to toggle the
    `reconcilable` flag from True to False on accounts that contain
    partially reconcilated transactions.
    
    When the migration script `l10n_pl/migrations/2.1/end-migrate.py` is executed
    and try to update the CoA by adding/updating accounts,
    using the accounts in the file
    `l10n_pl/data/template/account.account-pl.csv`. This CSV file contains
    a reconcilable flag per account.
    
    Problem:
    Before this modification, the upgrade script was trying to update the
    CoA using `_load_data`, which try to overwrite the reconcilation flag of
    accounts in the client DB. A traceback occurs during the upgrade if an
    account's `reconcilable` flag is toggled to False while it still contains
    partially reconciled transactions.
    
    Solution:
    I have sanitized the dict `data` using the _pre_reload_data method.
    
    Notes:
    `_pre_reload_data` method sanitizes the dict `data` by avoiding the
    creation of duplicated accounts, the creation of duplicated fields for a
    given record, the toggling of the `reconcilable` flag, etcs.
    
    Back port of: https://github.com/odoo/odoo/commit/23ed5448a13255e68ff4d3ca87884c2ca2f97ba3
    Reason: the issue was originally introduced in `18.0` (see
    https://github.com/odoo/odoo/commit/d5109a45d610916530b4b74cf3e2fa440ee7ed63)
    
    X-original-commit: 7cf7403ff1f61d572e69743fed9f94f35bc49e5b

--- addons/l10n_pl/migrations/2.1/end-migrate.py
+++ addons/l10n_pl/migrations/2.1/end-migrate.py
@@ -5,4 +5,9 @@ def migrate(cr, version):
     env = api.Environment(cr, SUPERUSER_ID, {})
     for company in env['res.company'].search([('chart_template', '=', 'pl'), ('parent_id', '=', False)]):
         Template = env['account.chart.template'].with_company(company)
-        Template._load_data({'account.account': Template._get_account_account('pl')})
+
+        template_data = Template._get_chart_template_data('pl').get('template_data', {})
+        data = {'account.account': Template._get_account_account('pl')}
+
+        Template._pre_reload_data(company, template_data, data, True)
+        Template._load_data(data)
