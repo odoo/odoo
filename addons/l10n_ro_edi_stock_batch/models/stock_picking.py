7bd44b6f11037341f56ddee6617e5e0226bb199c
Manav Shah <sham@odoo.com>
2026-04-01 10:59:35 +0000

    [FIX] l10n_ro_edi_stock: Modify Send to eTransport button visibility
    Currently, the Send to eTransport button on `stock.picking` is only
    visible when picking is done.
    This commit fixes this behaviour and makes it visible when picking
    is ready or done both.
    
    task-5930984
    
    X-original-commit: 5ec0b6e922066d19027ec6188ea70a730a04a107

--- addons/l10n_ro_edi_stock_batch/models/stock_picking.py
+++ addons/l10n_ro_edi_stock_batch/models/stock_picking.py
@@ -6,19 +6,14 @@ class Picking(models.Model):
 
     @api.depends('batch_id', 'company_id', 'picking_type_code')
     def _compute_l10n_ro_edi_stock_enable(self):
-        # OVERRIDES 'l10n_ro_edi_stock'
+        super()._compute_l10n_ro_edi_stock_enable()
         for picking in self:
-            picking.l10n_ro_edi_stock_enable = (
-                (not picking.batch_id or picking.batch_id.state != 'done')
-                and picking.picking_type_code != 'internal'
-                and picking.company_id.country_id.code == 'RO'
-            )
+            picking.l10n_ro_edi_stock_enable &= not picking.batch_id
 
     @api.model
     def _l10n_ro_edi_stock_validate_carrier_filter(self, picking):
-        # OVERRIDE l10n_ro_edi_stock
-
-        # Override for when the batch picking calls this function to validate the carriers
         validate_carrier = self.env.context.get('l10n_ro_edi_stock_validate_carrier', False)
-
-        return picking.company_id.account_fiscal_country_id.code == 'RO' and (picking.l10n_ro_edi_stock_enable or validate_carrier)
+        return (
+            super()._l10n_ro_edi_stock_validate_carrier_filter(picking)
+            or (picking.company_id.account_fiscal_country_id.code == 'RO' and validate_carrier)
+        )
