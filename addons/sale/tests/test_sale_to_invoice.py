feeef04f58244b47ec003ae88986eafbd50755e4
krip-odoo <krip@odoo.com>
2026-04-06 13:22:45 +0000

    [FIX] sale: ensure invoiced quantity is computed correctly
    Steps to reproduce:
    - Install the Sales and Inventory modules.
    - Enable Units of Measure & Packagings from Settings.
    - Go to Sales > Configuration > Products > Units & Packagings.
    - Create a new unit > Pack of 12 with quantity 12 and unit Units.
    - Create a new product > enable Track Inventory, set Quantity On Hand to
      100, and under Upsell & Cross-Sell set packaging as Pack of 12.
    - Create a Sales Order > add the product and set the quantity to 3
      With a unit Pack of 12.
    - Confirm the order and validate the delivery.
    - Create and confirm the invoice.
    - Return 1 unit from the delivery and validate it.
    - From the invoice, create a credit note with quantity 1 unit and confirm it.
    - Go back to the Sales Order.
    
    Issue:
    - On the Sales Order: Delivered Quantity shows 2.92 while Invoiced Quantity
      shows 2.91.
    
    Root cause:
    - At [1] & [2], In _compute_qty_invoiced, the _compute_quantity method is
      called without specifying a rounding_method, so it defaults to UP
      (1/12 rounded to 0.09), while _compute_qty_delivered explicitly uses
      HALF-UP (1/12 rounded to 0.08), leading to inconsistent rounding.
    
    Solution:
    - Since the rounding of product quantity is no longer applied during
      the computation of the qty_invoiced field on the sale order line,
      we now explicitly pass `round=False` to avoid unintended rounding.
    
    [1]: https://github.com/odoo/odoo/blob/64c1f1a7d3b24ca613d0b29a2412f1094fea08d9/addons/sale/models/sale_order_line.py#L896-L912
    [2]: https://github.com/odoo/odoo/blob/64c1f1a7d3b24ca613d0b29a2412f1094fea08d9/addons/sale_stock/models/sale_order_line.py#L193-L209
    
    opw-6073690
    
    closes odoo/odoo#265083
    
    X-original-commit: 7a55b584568edaa4c028825e78911de038fad8e8
    Signed-off-by: Krishna Arvindkumar Patel (krip) <krip@odoo.com>

--- addons/sale/tests/test_sale_to_invoice.py
+++ addons/sale/tests/test_sale_to_invoice.py
@@ -1010,10 +1010,10 @@ class TestSaleToInvoice(TestSaleCommon):
         sol_prod_deliver.env.add_to_compute(qty_invoiced_field, sol_prod_deliver)
         self.assertEqual(sol_prod_deliver.qty_invoiced, quantity)
 
-        # Rounding to 0.1, should be rounded with UP (ceil) rounding_method
-        # Not floor or half up rounding.
+        # If rounding of used uom is different from decimal precision, it's the decimal precision
+        # that is used for 'qty_invoiced'. No rounding is done.
         self.env["decimal.precision"].search([("name", "=", "Product Unit")]).digits = 1
-        expected_qty = 5.2
+        expected_qty = 5.1
         qty_invoiced_field = sol_prod_deliver._fields.get("qty_invoiced")
         sol_prod_deliver.env.add_to_compute(qty_invoiced_field, sol_prod_deliver)
         self.assertEqual(sol_prod_deliver.qty_invoiced, expected_qty)
