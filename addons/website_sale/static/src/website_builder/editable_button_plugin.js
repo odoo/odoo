dc19c4eb6ef442e8cdc048f16b4ac0859269e883
Sébastien (blse) <blse@odoo.com>
2026-04-07 09:24:04 +0000

    [FIX] website_sale: wrap content of editable button instead of button
    Commit 072a8e4fd061ff23902e6e448a577624cb27e188 fixes edition of buttons
    by wrapping the editable button in an editable span. But it does not fix
    the issue on Firefox, and it allows user to write outside the savable
    node, and allows user to copy website builder's branding attributes
    (which causes issues when pasted).
    
    This commit adds a span inside the button instead of outside (to wrap
    only the content of the button), and removes it before saving the
    content.
    
    Steps to reproduce:
    - Open website builder on `/product/item-1`
    - Place the cursor in the "Add to cart" button
    - Press left arrow, repeat until out of the button
    - Bug: You can write text there, but it won't be saved
    
    - Open website builder on `/product/item-1`
    - Select some text in the "Add to cart" button
    - Copy (`ctrl+c`)
    - Paste (`ctrl+v`)
    - Bug: Some content seems duplicated: the branding of the savable button
      has been duplicated inside it and the automatic replication between
      identical fields is based on this branding, thus replicates the
      button's content inside it
    
    Same with the "Logout" button, which is a `<button>` since
    c18fb61e9528fabbd3ea46f71c5a02f424c39255
    
    Similar issue about copy+pasted branding:
    ec93d48ab17b4a72f61fa358e5a81d2abcb18897
    
    task-6102661
    
    X-original-commit: 17f443ce89572c173d252182ae3bb125d14393a7
    Part-of: odoo/odoo#263516
    Signed-off-by: Colin Louis (loco) <loco@odoo.com>
    Signed-off-by: Sébastien Blondiau (blse) <blse@odoo.com>

--- addons/website_sale/static/src/website_builder/editable_button_plugin.js
+++ addons/website_sale/static/src/website_builder/editable_button_plugin.js
@@ -1,35 +1,42 @@
 import { Plugin } from "@html_editor/plugin";
+import { unwrapContents } from "@html_editor/utils/dom";
+import { selectElements } from "@html_editor/utils/dom_traversal";
 import { registry } from "@web/core/registry";
 
 class EditableButtonPlugin extends Plugin {
     static id = "editableButton";
+    /** @type {import("plugins").WebsiteResources} */
     resources = {
         normalize_handlers: this.wrapEditableButtons.bind(this),
+        clean_for_save_handlers: this.cleanEditableButtons.bind(this),
     };
 
     /**
      * Buttons with `contenteditable="true"` cannot receive spaces because the
      * browser natively triggers a click on them when the space key is pressed.
-     * To work around this, we move the `contenteditable` attribute up to a
-     * wrapping <span>, allowing text editing (including spaces) to work
+     * To work around this, we move the `contenteditable` attribute down to a
+     * inner <span>, allowing text editing (including spaces) to work
      * correctly without triggering the button's click handler.
      */
     wrapEditableButtons(root) {
-        const editableButtons = root.querySelectorAll("button.o_savable[contenteditable='true']");
+        const editableButtons = selectElements(root, "button[contenteditable='true']");
         for (const button of editableButtons) {
-            const parent = button.parentElement;
-            const isOnlyChildOfEditableParent =
-                parent.childNodes.length === 1 && parent.getAttribute("contenteditable") === "true";
+            button.removeAttribute("contenteditable");
 
-            if (isOnlyChildOfEditableParent) {
+            if (button.querySelector(":scope > .o_inner_button_editable_span")) {
                 continue;
             }
 
             const span = this.document.createElement("span");
             span.setAttribute("contenteditable", "true");
-            button.removeAttribute("contenteditable");
-            button.after(span);
-            span.append(button);
+            span.classList.add("o_inner_button_editable_span");
+            span.append(...button.childNodes);
+            button.append(span);
+        }
+    }
+    cleanEditableButtons({ root }) {
+        for (const span of root.querySelectorAll(".o_inner_button_editable_span")) {
+            unwrapContents(span);
         }
     }
 }
