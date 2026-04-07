91c17c8064b9aedacb06f061f2b1d90598f74d90
Ajit Singh <aksi@odoo.com>
2026-04-07 06:50:44 +0000

    [FIX] point_of_sale, pos_self_order, web: fix custom attr behivour in kiosk mode
        this pr fixes 3 bug, as all are closely related.
    
        Step to reproduce (hide is_custom attr in kiosk mode):
        - have two attributes A and B
        - A has only 1 attribute value with is_custom = True
        - B can have any two value ( ex. gender: male/female)
        - use it on a product  and make it available in POS for kiosk
        - start kiosk and open that product
    
        Observation:
        - we do not get option to select option from A but the heading is visible
        - when we select from B, Add to cart is disabled.
    
        Cause:
        - we do not allow attribute values with is_custom = True in kiosk
        - but we display the attribute regardless
        - the Add to cart btn depends on `selectedValues`, which requires
        value from each attribute, in this case, we are not seletion anything from A
        - so it is disabled
    
        Fix:
        - we introduced  `attributesToDisplay` which will hide heading in case of single
        custom value for any attribute
        - for Add to cart, wenow do not expect value from `is_custom`  attribute values.
    
         Allow product with 1 attr which is `is_custom` to be
         configurable in configs other than kiosk) correct fix for commit
    
         Step to reproduce
        - have attributes A
        - A has only 1 attribute value with is_custom = True
        - use it on a product and make it available in POS
        - start pos and open that product
    
        Observation:
        - we do not get option to select add text for A
    
        Cause:
        - in pos, we consider product to be configurable only it has more than 1
        attributes, which misses is_custom attr
    
        Fix:
        - we backport commit[1] and also considers its side effect by introducing
        `isProductConfigurable` for pos_self_order, which will still avoid
        `is_custom` attrs for kiosk
    
        [1] https://github.com/odoo/odoo/commit/5155c77a03ed2ff6c914eac41cc81ccb34b1f3c7
    
        Empty page is displayed if product has only `is_custom` attribute value
        and other attribute with type other then 'no_variant' for combo item
    
         Step to reproduce
        - have attributes A and B
        - A has only 1 attribute value with is_custom = True
        - B has two values with type "always"
        - use it on a product and add that product in combo item and make it available
          in Kisok
        - start kiosk and open that combo and select that product
    
        Observation:
        - we do not get option to select
    
        Cause:
        - `availableAttributeValue` only show `no_variant` and non `is_custom` attribute
           values in attributeSelection component.
    
        Fix:
        - before mounting Attributeselection component, we check if product has required
          attribute or not.
    
        opw-6100965
    
    X-original-commit: c5531b6a5b7107a3a73b87881990f5bf861e5705

--- addons/pos_self_order/static/src/app/pages/product_list_page/product_list_page.js
+++ addons/pos_self_order/static/src/app/pages/product_list_page/product_list_page.js
@@ -214,7 +214,7 @@ export class ProductListPage extends Component {
                     }))
                 );
             }
-        } else if (product.isConfigurable()) {
+        } else if (this.selfOrder.isProductConfigurable(product)) {
             this.router.navigate("product", { id: product.id });
         } else {
             if (!this.selfOrder.ordering) {
