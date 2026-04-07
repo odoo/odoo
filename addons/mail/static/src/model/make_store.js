7ef22b8630872197f75c59c8de7811cd2d7b687c
Alex Kühn <aku@odoo.com>
2026-04-07 19:11:26 +0200

    [FIX] mail: preserve 'in-need' flag of compute and sort fields
    Before this commit, computed and sorted fields could have their
    "in-need" flag dropped while the fields are still being "in-need".
    
    This happens because there's internal `onChange` to reset the
    "in-need" flag when the field has changed from computing / sorting,
    and "in-need" flag is expected to be set again in case the observers
    still need it.
    
    This reset of "in-need" flag makes sense, but there's a scenario
    where this is not desirable: when the field is computed or sorted on
    the fly by an "in-need". When this happens, the flag "in-need" is
    set, but immediately after the `onChange` of computing / sorting is
    triggering and reset the flag, which incorrectly flag the field as
    not "in-need". This leads to reactivity problems.
    
    This commit fixes the issue by forcing the "in-need" flag at the
    end of computing /sorting when the action is triggered from
    "in-need". This preserves the automatic reset of "in-need" flag from
    change of value computing / sorting while ensuring the "in-need" flag
    stays when there are still ongoing observers.
    
    Note that this "worked" before with owl2 because the observers were
    more aggressively re-rendering, as shown with tests that show initial
    render done twice instead or once, or lazy field still continuing
    computing many steps later even though the observer has stopped
    observing. This commit fixes the issue.
    
    Note that this commit adds an extra "LAZY", i.e. when an observer
    doesn't register again the lazy field is still computed again at
    least once. While this looks inefficient, this makes sense with the
    strategy being used to detect "in-need" observers, which can be
    immediate when compute is triggered from the "on-need", but if this
    comes from "in-need" as the flag has to be kept, the "in-need" flag
    is kept until next compute / sort that updates the value accordingly.

--- addons/mail/static/src/model/make_store.js
+++ addons/mail/static/src/model/make_store.js
@@ -88,13 +88,13 @@ export function makeStore(env, { localRegistry } = {}) {
                             if (Model._.fieldsCompute.get(name) && !Model._.fieldsEager.get(name)) {
                                 record._.fieldsComputeInNeed.set(name, true);
                                 if (record._.fieldsComputeOnNeed.get(name)) {
-                                    record._.compute(record, name);
+                                    record._.compute(record, name, { fromInNeed: true });
                                 }
                             }
                             if (Model._.fieldsSort.get(name) && !Model._.fieldsEager.get(name)) {
                                 record._.fieldsSortInNeed.set(name, true);
                                 if (record._.fieldsSortOnNeed.get(name)) {
-                                    record._.sort(record, name);
+                                    record._.sort(record, name, { fromInNeed: true });
                                 }
                             }
                             record._.gettingField = true;
