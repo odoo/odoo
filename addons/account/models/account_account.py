7ca03147899e7c3d500924959688f43629d82e32
Manav Shah <sham@odoo.com>
2026-04-07 05:20:32 +0000

    [FIX] account: Fix the account search behaviour on invoices and bills
    Currently, on invoices, the account_id many2one field overrides
    `name_search` to limit search results based on account types, but
    that is applied only if the account is searched by name. If the
    account is searched by code, all the accounts appear in search results
    which is not intended.
    This commit fixes that issue.
    
    Additionally, this commit also ensures that "Search more..." is always
    visible in the suggestions for Account.
    
    task-6075233
    
    X-original-commit: b8477b0b9bbb5c4957bd9b8d8296780e311d602b

--- addons/account/models/account_account.py
+++ addons/account/models/account_account.py
@@ -850,19 +850,14 @@ class AccountAccount(models.Model):
         if not name and suggested_accounts:
             return [(record.id, record.display_name) for record in self.sudo().browse(suggested_accounts)]
 
-        digit_in_search_term = any(c.isdigit() for c in name)
         search_domain = Domain('display_name', 'ilike', name) if name else []
-
-        if digit_in_search_term:
-            domain = Domain.AND([search_domain, domain])
-        else:
-            move_type_accounts = {
-                'out': ['income'],
-                'in': ['expense', 'asset_fixed'],
-            }
-            allowed_account_types = move_type_accounts.get(move_type.split('_')[0])
-            type_domain = [('account_type', 'in', allowed_account_types)] if allowed_account_types else []
-            domain = Domain.AND([search_domain, type_domain, domain])
+        move_type_accounts = {
+            'out': ['income'],
+            'in': ['expense', 'asset_fixed'],
+        }
+        allowed_account_types = move_type_accounts.get(move_type.split('_')[0])
+        type_domain = [('account_type', 'in', allowed_account_types)] if allowed_account_types else []
+        domain = Domain.AND([search_domain, type_domain, domain])
 
         records = self.with_context(preferred_account_ids=suggested_accounts).search_fetch(domain, ['display_name'], limit=limit)
         return [(record.id, record.display_name) for record in records]
--- addons/account/static/src/components/m2o_cell_with_extra_fields/m2o_cell_with_extra_fields.js
+++ addons/account/static/src/components/m2o_cell_with_extra_fields/m2o_cell_with_extra_fields.js
@@ -1,8 +1,40 @@
+import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
-import { computeM2OProps } from "@web/views/fields/many2one/many2one";
+import { Many2One, computeM2OProps } from "@web/views/fields/many2one/many2one";
+import { Many2XAutocomplete } from "@web/views/fields/relational_utils";
 import { Many2OneField, extractM2OFieldProps } from "@web/views/fields/many2one/many2one_field";
 
+export class Many2XAccountAccountAutocomplete extends Many2XAutocomplete {
+    addSearchMoreSuggestion(options) {
+        return true;
+    }
+
+    async onSearchMore(request) {
+        const { getDomain, context, fieldString } = this.props;
+        if (request.length) {
+            context["search_default_name"] = request;
+        }
+        const title = _t("Search: %s", fieldString);
+        this.selectCreate({
+            domain: getDomain(),
+            context,
+            title,
+        });
+    }
+}
+
+export class Many2OneAccountAccount extends Many2One {
+    static components = {
+        ...Many2One.components,
+        Many2XAutocomplete: Many2XAccountAccountAutocomplete,
+    };
+}
+
 export class M2OCellWithExtraFields extends Many2OneField {
+    static components = {
+        ...Many2OneField.components,
+        Many2One: Many2OneAccountAccount,
+    };
     static template = "account.M2OCellWithExtraFields";
     static props = {
         ...Many2OneField.props,
