MoLeap/odooo

[IMP] html_editor: make `FontSizeSelector` responsive
Before this commit, when a user changed the font size using the toolbar
input, the editor saved a fixed pixel value (eg `font-size: 120px`).
This looked correct on desktop but was too large on mobile because the
value never changed with the screen width.

After this commit, the editor saves a responsive CSS value instead
(`font-size: clamp(8px, 1em + Xvw, MAX_FONT_SIZE)`). The size still
matches what the user sees on screen when they type a number, but it now
scales down on narrow screens and up on wide screens.
The span that holds this style is marked with the class `o_rfs` so
it can be identified and targeted for migration and maintenance in the
future.

task-6041270

addons/html_editor/static/src/main/font/font_plugin.js
@@ -34,7 +34,7 @@ import {
     SUPPORTED_BASE_CONTAINER_NAMES,
 } from "@html_editor/utils/base_container";
 import { READ, withSequence } from "@html_editor/utils/resource";
-import { FontSizeSelector } from "./font_size_selector";
+import { FontSizeSelector, MAX_FONT_SIZE } from "./font_size_selector";
 import { isHtmlContentSupported } from "@html_editor/core/selection_plugin";
 import { weakMemoize } from "@html_editor/utils/functions";
 
@@ -180,10 +180,18 @@ export class FontPlugin extends Plugin {
                     getItems: () => this.fontSizeItems,
                     getDisplay: () => this.fontSize,
                     onFontSizeInput: (size) => {
-                        this.dependencies.format.formatSelection("fontSize", {
-                            formatProps: { size },
-                            applyStyle: true,
-                        });
+                        const resolvedSize = this._resolveFontSize(parseFloat(size));
+                        if (resolvedSize === null) {
+                            // Desired size matches the inherited value
+                            this.dependencies.format.formatSelection("fontSize", {
+                                applyStyle: false,
+                            });
+                        } else {
+                            this.dependencies.format.formatSelection("fontSize", {
+                                formatProps: { size: resolvedSize },
+                                applyStyle: true,
+                            });
+                        }
                         this.updateFontSizeSelectorParams();
                     },
                     onSelected: (item) => {
@@ -319,7 +327,7 @@ export class FontPlugin extends Plugin {
         before_insert_processors: this.handleInsertWithinPre.bind(this),
 
         is_format_class_predicates: (className) => {
-            if ([...FONT_SIZE_CLASSES, "o_default_font_size"].includes(className)) {
+            if ([...FONT_SIZE_CLASSES, "o_default_font_size", "o_rfs"].includes(className)) {
                 return true;
             }
         },
@@ -575,6 +583,52 @@ export class FontPlugin extends Plugin {
         closestHandledElement.remove();
         this.dependencies.selection.setCursorStart(baseContainer);
         return true;
+    }
+
+    /**
+     * Resolves the CSS font-size value to apply for a desired pixel size typed
+     * by the user in the toolbar input.
+     *
+     * Strategy:
+     *  - If the value falls within the range of system-defined font sizes,
+     *    a plain `px` value is returned, those sizes are already designed by
+     *    the user to work at any viewport.
+     *  - If the value is outside that range (smaller than the smallest system
+     *    size, or larger than the largest), a responsive `clamp()` expression
+     *    is returned so the text scales gracefully with the viewport:
+     *      `clamp(8px, 1em + UIvalue*vw, MAX_FONT_SIZE)`
+     *    where `UIvalue` is derived from the difference between what the user
+     *    wants and what the parent block already provides at the current viewport.
+     *
+     * Returns `null` when the desired size matches the inherited size (within a
+     * 0.01 vw tolerance), signalling that any existing inline override should
+     * simply be removed.
+     *
+     * @param {number} desiredPx  The pixel value the user typed in the toolbar.
+     * @returns {string|null}     A CSS font-size value, or `null`.
+     */
+    _resolveFontSize(desiredPx) {
+        const items = this.fontSizeItems;
+        if (items.length) {
+            const minPx = items[0].name;
+            const maxPx = items[items.length - 1].name;
+            if (desiredPx >= minPx && desiredPx <= maxPx) {
+                // Within the system-defined range: plain px is fine.
+                return `${desiredPx}px`;
+            }
+        }
+        // Outside the system range => produce responsive value.
+        const sel = this.dependencies.selection.getEditableSelection();
+        const blockEl = closestBlock(sel.anchorNode);
+        const parentComputedPx = parseFloat(this.window.getComputedStyle(blockEl).fontSize);
+        const viewportWidth = this.window.innerWidth;
+        const uiValue = (desiredPx - parentComputedPx) / (viewportWidth / 100);
+        if (Math.abs(uiValue) < 0.01) {
+            // Desired size matches the inherited size — clear any existing
+            // existing inline override.
+            return null;
+        }
+        return `clamp(8px, 1em + ${uiValue.toFixed(4)}vw, ${MAX_FONT_SIZE}px)`;
     }
 
     updateFontSelectorParams() {

odoo/odoo

addons/html_editor/static/src/main/font/font_plugin.js
@@ -34,7 +34,7 @@ import {
     SUPPORTED_BASE_CONTAINER_NAMES,
 } from "@html_editor/utils/base_container";
 import { READ, withSequence } from "@html_editor/utils/resource";
-import { FontSizeSelector } from "./font_size_selector";
+import { FontSizeSelector, MAX_FONT_SIZE } from "./font_size_selector";
 import { isHtmlContentSupported } from "@html_editor/core/selection_plugin";
 import { weakMemoize } from "@html_editor/utils/functions";
 
@@ -180,10 +180,18 @@ export class FontPlugin extends Plugin {
                     getItems: () => this.fontSizeItems,
                     getDisplay: () => this.fontSize,
                     onFontSizeInput: (size) => {
-                        this.dependencies.format.formatSelection("fontSize", {
-                            formatProps: { size },
-                            applyStyle: true,
-                        });
+                        const resolvedSize = this.resolveFontSize(parseFloat(size));
+                        if (resolvedSize === null) {
+                            // Desired size matches the inherited value
+                            this.dependencies.format.formatSelection("fontSize", {
+                                applyStyle: false,
+                            });
+                        } else {
+                            this.dependencies.format.formatSelection("fontSize", {
+                                formatProps: { size: resolvedSize },
+                                applyStyle: true,
+                            });
+                        }
                         this.updateFontSizeSelectorParams();
                     },
                     onSelected: (item) => {
@@ -319,7 +327,7 @@ export class FontPlugin extends Plugin {
         before_insert_processors: this.handleInsertWithinPre.bind(this),
 
         is_format_class_predicates: (className) => {
-            if ([...FONT_SIZE_CLASSES, "o_default_font_size"].includes(className)) {
+            if ([...FONT_SIZE_CLASSES, "o_default_font_size", "o_rfs"].includes(className)) {
                 return true;
             }
         },
@@ -577,6 +585,52 @@ export class FontPlugin extends Plugin {
         closestHandledElement.replaceWith(baseContainer);
         this.dependencies.selection.setCursorStart(baseContainer);
         return true;
+    }
+
+    /**
+     * Resolves the CSS font-size value to apply for a desired pixel size typed
+     * by the user in the toolbar input.
+     *
+     * Strategy:
+     *  - If the value falls within the range of system-defined font sizes,
+     *    a plain `px` value is returned, those sizes are already designed by
+     *    the user to work at any viewport.
+     *  - If the value is outside that range (smaller than the smallest system
+     *    size, or larger than the largest), a responsive `clamp()` expression
+     *    is returned so the text scales gracefully with the viewport:
+     *      `clamp(8px, 1em + UIvalue*vw, MAX_FONT_SIZE)`
+     *    where `UIvalue` is derived from the difference between what the user
+     *    wants and what the parent block already provides at the current viewport.
+     *
+     * Returns `null` when the desired size matches the inherited size (within a
+     * 0.01 vw tolerance), signalling that any existing inline override should
+     * simply be removed.
+     *
+     * @param {number} desiredPx  The pixel value the user typed in the toolbar.
+     * @returns {string|null}     A CSS font-size value, or `null`.
+     */
+    resolveFontSize(desiredPx) {
+        const items = this.fontSizeItems;
+        if (items.length) {
+            const minPx = items[0].name;
+            const maxPx = items[items.length - 1].name;
+            if (desiredPx >= minPx && desiredPx <= maxPx) {
+                // Within the system-defined range: plain px is fine.
+                return `${desiredPx}px`;
+            }
+        }
+        // Outside the system range => produce responsive value.
+        const sel = this.dependencies.selection.getEditableSelection();
+        const blockEl = closestBlock(sel.anchorNode);
+        const parentComputedPx = parseFloat(this.window.getComputedStyle(blockEl).fontSize);
+        const viewportWidth = this.window.innerWidth;
+        const uiValue = (desiredPx - parentComputedPx) / (viewportWidth / 100);
+        if (Math.abs(uiValue) < 0.01) {
+            // Desired size matches the inherited size — clear any existing
+            // existing inline override.
+            return null;
+        }
+        return `clamp(8px, 1em + ${uiValue.toFixed(4)}vw, ${MAX_FONT_SIZE}px)`;
     }
 
     updateFontSelectorParams() {