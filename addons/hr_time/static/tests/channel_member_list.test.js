MoLeap/odooo

[IMP] hr_*: rename module hr_holidays to hr_time
- Renamed module `hr_holidays` to `hr_time`
- Renamed model `hr.leave` to `hr.time`
- Updated all references from `hr_holidays` and `hr_leave` accordingly.

Task-5976238

addons/hr_time/static/tests/channel_member_list.test.js
@@ -1,0 +1,39 @@
+import { describe, test } from "@odoo/hoot";
+import { Command, serverState } from "@web/../tests/web_test_helpers";
+import { contains, openDiscuss, start, startServer } from "@mail/../tests/mail_test_helpers";
+import { defineHrHolidaysModels } from "@hr_time/../tests/hr_time_test_helpers";
+
+describe.current.tags("desktop");
+defineHrHolidaysModels();
+
+test("on leave members are categorised correctly in online/offline", async () => {
+    const pyEnv = await startServer();
+    const [partnerId1, partnerId2, partnerId3] = pyEnv["res.partner"].create([
+        { name: "Online Partner", im_status: "online" },
+        { name: "On Leave Online", im_status: "online" },
+        { name: "On Leave Idle", im_status: "away" },
+    ]);
+    const [userId2, userId3] = pyEnv["res.users"].create([
+        { partner_id: partnerId2 },
+        { partner_id: partnerId3 },
+    ]);
+    pyEnv["hr.employee"].create([
+        { user_id: serverState.userId, leave_date_to: "2023-01-02" },
+        { user_id: userId2, leave_date_to: "2023-01-03" },
+        { user_id: userId3, leave_date_to: "2023-01-04" },
+    ]);
+    const channelId = pyEnv["discuss.channel"].create({
+        name: "TestChanel",
+        channel_member_ids: [
+            Command.create({ partner_id: serverState.partnerId }),
+            Command.create({ partner_id: partnerId1 }),
+            Command.create({ partner_id: partnerId2 }),
+            Command.create({ partner_id: partnerId3 }),
+        ],
+        channel_type: "channel",
+    });
+    await start();
+    await openDiscuss(channelId);
+    await contains(".o-discuss-ChannelMemberList h6", { text: "Online - 3" });
+    await contains(".o-discuss-ChannelMemberList h6", { text: "Offline - 1" });
+});

addons/hr_time/static/tests/hr_time_test_helpers.js
@@ -1,0 +1,22 @@
+import { defineModels } from "@web/../tests/web_test_helpers";
+import { ResUsers } from "@hr_time/../tests/mock_server/mock_models/res_users";
+import { ResPartner } from "@hr_time/../tests/mock_server/mock_models/res_partner";
+import { HrEmployee } from "@hr_time/../tests/mock_server/mock_models/hr_employee";
+import { HrTime } from "@hr_time/../tests/mock_server/mock_models/hr_time";
+import { HrDepartment } from "@hr_time/../tests/mock_server/mock_models/hr_department";
+import { HrWorkEntryType } from "@hr_time/../tests/mock_server/mock_models/hr_work_entry_type";
+import { hrModels } from "@hr/../tests/hr_test_helpers";
+
+export function defineHrHolidaysModels() {
+    return defineModels(hrHolidaysModels);
+}
+
+export const hrHolidaysModels = {
+    ...hrModels,
+    ResUsers,
+    ResPartner,
+    HrEmployee,
+    HrDepartment,
+    HrWorkEntryType,
+    HrTime,
+};

addons/hr_time/static/tests/leave_card.test.js
@@ -1,0 +1,85 @@
+import { HrTime } from "@hr_time/../tests/mock_server/mock_models/hr_time";
+import { ResUsers } from "@hr_time/../tests/mock_server/mock_models/res_users";
+import { defineHrHolidaysModels } from "@hr_time/../tests/hr_time_test_helpers";
+import { mountView, onRpc, patchWithCleanup } from "@web/../tests/web_test_helpers";
+import { clickDate } from "@web/../tests/views/calendar/calendar_test_helpers";
+import { describe, test } from "@odoo/hoot";
+import { mockDate } from "@odoo/hoot-mock";
+import { click, waitFor } from "@odoo/hoot-dom";
+import { user } from "@web/core/user";
+
+describe.current.tags("desktop");
+defineHrHolidaysModels();
+
+test("Test request creator buttons", async() => {
+    mockDate("2024-01-03 12:00:00", 0);
+    patchWithCleanup(user, { userId: 100 });
+
+    HrTime._views = {
+         "form,hr_time_view_form_dashboard_new_time_off": `
+            <form>
+                <field name="state"/>
+                <field name="work_entry_type_id"/>
+                <field name="employee_id"/>
+                <field name="user_id"/>
+                <field name="can_cancel"/>
+            </form>
+        `,
+    };
+
+    HrTime._records = [
+        {
+            'id': 1, 'state': 'confirm', 'work_entry_type_id': 55, 'employee_id': 100,
+            'user_id': 100, 'date_from': '2024-01-09 09:00:00', 'date_to': '2024-01-09 18:00:00'
+        },
+        {
+            'id': 2, 'state': 'validate1', 'work_entry_type_id': 55, 'employee_id': 100,
+            'can_cancel': true, 'user_id': 100, 'date_from': '2024-01-10 09:00:00', 'date_to': '2024-01-10 18:00:00'
+        },
+    ]
+
+    ResUsers._records = [
+        ...ResUsers._records,
+        { 'id': 100, 'name': "User 1", 'employee_id': 100 },
+    ]
+
+    onRpc("get_mandatory_days", () => ({}));
+    onRpc("get_unusual_days", () => ({}));
+    onRpc("get_allocation_data_request", () => ({}));
+    onRpc("get_special_days_data", () => ({bankHolidays: [], mandatoryDays: []}));
+    onRpc("hr.employee", "get_time_off_dashboard_data", () => (
+        {has_accrual_allocation: true, allocation_data: {}, allocation_request_amount: 0}
+    ));
+
+    await mountView({
+            type: "calendar",
+            resModel: "hr.time",
+            arch: `
+            <calendar js_class="time_off_calendar_dashboard"
+                    string="Time Off Request"
+                    form_view_id="hr_time_view_form_dashboard_new_time_off"
+                    event_open_popup="true"
+                    date_start="date_from"
+                    date_stop="date_to"
+                    quick_create="0"
+                    show_date_picker="0"
+                    show_unusual_days="True"
+                    hide_time="True"
+                    mode="year">
+                <field name="display_name" string=""/>
+                <field name="work_entry_type_id" filters="1" invisible="1" color="color"/>
+                <field name="state" invisible="1"/>
+                <field name="is_hatched" invisible="1" />
+                <field name="is_striked" invisible="1"/>
+            </calendar>`,
+            context: user.context
+        });
+    await clickDate("2024-01-09");
+    await click(".o_cw_popover_link");
+    await waitFor("button:contains(Delete Time Off)");
+    await click(".btn-close");
+    await clickDate("2024-01-10");
+    await click(".o_cw_popover_link");
+    await waitFor("button:contains(Cancel Time Off)");
+})
+

addons/hr_time/static/tests/time_off_dashboard.test.js
@@ -1,0 +1,108 @@
+import { describe, expect, test } from "@odoo/hoot";
+import { mockDate } from "@odoo/hoot-mock";
+import { defineModels, fields, getService, models, mountWebClient, onRpc } from "@web/../tests/web_test_helpers";
+import { defineHrHolidaysModels } from "./hr_time_test_helpers";
+
+describe.current.tags("desktop");
+
+class HrTime extends models.Model {
+    _views = {
+        calendar: `
+            <calendar js_class="time_off_calendar_dashboard"
+                      string="Time Off Request"
+                      form_view_id="%(hr_time.hr_time_view_form_dashboard_new_time_off)d"
+                      event_open_popup="1"
+                      date_start="date_from"
+                      date_stop="date_to"
+                      quick_create="0"
+                      show_unusual_days="1"
+                      color="color"
+                      hide_time="1"
+                      mode="year"
+            >
+                <field name="name"/>
+                <field name="work_entry_type_id" filters="1" invisible="1" color="color"/>
+                <field name="state" invisible="1"/>
+            </calendar>
+        `,
+    };
+
+    color = fields.Integer({ related: "work_entry_type_id.color" });
+    date_from = fields.Datetime();
+    date_to = fields.Datetime();
+    department_id = fields.Many2one({ relation: "hr.department" });
+    employee_id =  fields.Many2one({ relation: "hr.employee" });
+    work_entry_type_id = fields.Many2one({ relation: "hr.work.entry.type" });
+    holiday_type = fields.Char();
+    name = fields.Char();
+    number_of_days = fields.Integer();
+    state = fields.Char();
+}
+
+class HrWorkEntryType extends models.Model {
+    name = fields.Char();
+    color = fields.Integer();
+}
+
+defineHrHolidaysModels();
+defineModels([HrTime, HrWorkEntryType]);
+
+onRpc("hr.employee", "get_time_off_dashboard_data", () => (
+    {has_accrual_allocation: true, allocation_data: {}, allocation_request_amount: 0}
+));
+onRpc("hr.employee", "get_mandatory_days", () => ({}));
+onRpc("hr.employee", "get_special_days_data", () => ({ mandatoryDays: [], bankHolidays: [] }));
+onRpc("hr.time", "get_unusual_days", () => ({}));
+onRpc("hr.time", "has_access", () => true);
+onRpc("hr.work.entry.type", "has_accrual_allocation", () => true);
+
+test(`test employee is passed to get_time_off_dashboard_data`, async () => {
+    onRpc("hr.employee", "get_time_off_dashboard_data", ({ kwargs }) => {
+        expect.step(kwargs.context.employee_id);
+    });
+
+    await mountWebClient();
+    await getService("action").doAction({
+        id: 1,
+        res_model: "hr.time",
+        type: "ir.actions.act_window",
+        views: [[false, "calendar"]],
+        context: { employee_id: [200] },
+        domain: [["employee_id", "in", [200]]],
+    });
+    expect.verifySteps([200]);
+});
+
+test(`test basic rendering`, async () => {
+    mockDate("2025-03-18 08:00:00");
+    onRpc("hr.employee", "get_mandatory_days", () => ({ "2025-03-17": 5 }));
+    onRpc("hr.employee", "get_special_days_data", () => ({
+        mandatoryDays: [
+            {
+                id: -2,
+                colorIndex: 5,
+                end: "2025-03-17T23:59:59.999999",
+                endType: "datetime",
+                isAllDay: true,
+                start: "2025-03-17T00:00:00",
+                startType: "datetime",
+                title: "Test Mandatory Day",
+            },
+        ],
+        bankHolidays: [],
+    }));
+
+    await mountWebClient();
+    await getService("action").doAction({
+        id: 1,
+        res_model: "hr.time",
+        type: "ir.actions.act_window",
+        views: [[false, "calendar"]],
+        context: { employee_id: [200] },
+        domain: [["employee_id", "in", [200]]],
+    });
+    expect(`.o_calendar_filter:contains("Legend")`).toHaveCount(1);
+    expect(`.o_calendar_filter:contains("To Approve")`).toHaveCount(1);
+    expect(`.o_calendar_filter:contains("Mar 17, 2025 : Test Mandatory Day")`).toHaveCount(1);
+    expect(`.fc-day.hr_mandatory_day_5[data-date="2025-03-17"]`).toHaveCount(1);
+});

odoo/odoo

addons/hr_time/static/tests/channel_member_list.test.js
@@ -1,0 +1,39 @@
+import { describe, test } from "@odoo/hoot";
+import { Command, serverState } from "@web/../tests/web_test_helpers";
+import { contains, openDiscuss, start, startServer } from "@mail/../tests/mail_test_helpers";
+import { defineHrTimeModels } from "@hr_time/../tests/hr_time_test_helpers";
+
+describe.current.tags("desktop");
+defineHrTimeModels();
+
+test("on leave members are categorised correctly in online/offline", async () => {
+    const pyEnv = await startServer();
+    const [partnerId1, partnerId2, partnerId3] = pyEnv["res.partner"].create([
+        { name: "Online Partner", im_status: "online" },
+        { name: "On Leave Online", im_status: "online" },
+        { name: "On Leave Idle", im_status: "away" },
+    ]);
+    const [userId2, userId3] = pyEnv["res.users"].create([
+        { partner_id: partnerId2 },
+        { partner_id: partnerId3 },
+    ]);
+    pyEnv["hr.employee"].create([
+        { user_id: serverState.userId, leave_date_to: "2023-01-02" },
+        { user_id: userId2, leave_date_to: "2023-01-03" },
+        { user_id: userId3, leave_date_to: "2023-01-04" },
+    ]);
+    const channelId = pyEnv["discuss.channel"].create({
+        name: "TestChanel",
+        channel_member_ids: [
+            Command.create({ partner_id: serverState.partnerId }),
+            Command.create({ partner_id: partnerId1 }),
+            Command.create({ partner_id: partnerId2 }),
+            Command.create({ partner_id: partnerId3 }),
+        ],
+        channel_type: "channel",
+    });
+    await start();
+    await openDiscuss(channelId);
+    await contains(".o-discuss-ChannelMemberList h6", { text: "Online - 3" });
+    await contains(".o-discuss-ChannelMemberList h6", { text: "Offline - 1" });
+});

addons/hr_time/static/tests/hr_time_test_helpers.js
@@ -1,0 +1,22 @@
+import { defineModels } from "@web/../tests/web_test_helpers";
+import { ResUsers } from "@hr_time/../tests/mock_server/mock_models/res_users";
+import { ResPartner } from "@hr_time/../tests/mock_server/mock_models/res_partner";
+import { HrEmployee } from "@hr_time/../tests/mock_server/mock_models/hr_employee";
+import { HrTime } from "@hr_time/../tests/mock_server/mock_models/hr_time";
+import { HrDepartment } from "@hr_time/../tests/mock_server/mock_models/hr_department";
+import { HrWorkEntryType } from "@hr_time/../tests/mock_server/mock_models/hr_work_entry_type";
+import { hrModels } from "@hr/../tests/hr_test_helpers";
+
+export function defineHrTimeModels() {
+    return defineModels(hrHolidaysModels);
+}
+
+export const hrHolidaysModels = {
+    ...hrModels,
+    ResUsers,
+    ResPartner,
+    HrEmployee,
+    HrDepartment,
+    HrWorkEntryType,
+    HrTime,
+};

addons/hr_time/static/tests/leave_card.test.js
@@ -1,0 +1,85 @@
+import { HrTime } from "@hr_time/../tests/mock_server/mock_models/hr_time";
+import { ResUsers } from "@hr_time/../tests/mock_server/mock_models/res_users";
+import { defineHrTimeModels } from "@hr_time/../tests/hr_time_test_helpers";
+import { mountView, onRpc, patchWithCleanup } from "@web/../tests/web_test_helpers";
+import { clickDate } from "@web/../tests/views/calendar/calendar_test_helpers";
+import { describe, test } from "@odoo/hoot";
+import { mockDate } from "@odoo/hoot-mock";
+import { click, waitFor } from "@odoo/hoot-dom";
+import { user } from "@web/core/user";
+
+describe.current.tags("desktop");
+defineHrTimeModels();
+
+test("Test request creator buttons", async() => {
+    mockDate("2024-01-03 12:00:00", 0);
+    patchWithCleanup(user, { userId: 100 });
+
+    HrTime._views = {
+         "form,hr_time_view_form_dashboard_new_time_off": `
+            <form>
+                <field name="state"/>
+                <field name="work_entry_type_id"/>
+                <field name="employee_id"/>
+                <field name="user_id"/>
+                <field name="can_cancel"/>
+            </form>
+        `,
+    };
+
+    HrTime._records = [
+        {
+            'id': 1, 'state': 'confirm', 'work_entry_type_id': 55, 'employee_id': 100,
+            'user_id': 100, 'date_from': '2024-01-09 09:00:00', 'date_to': '2024-01-09 18:00:00'
+        },
+        {
+            'id': 2, 'state': 'validate1', 'work_entry_type_id': 55, 'employee_id': 100,
+            'can_cancel': true, 'user_id': 100, 'date_from': '2024-01-10 09:00:00', 'date_to': '2024-01-10 18:00:00'
+        },
+    ]
+
+    ResUsers._records = [
+        ...ResUsers._records,
+        { 'id': 100, 'name': "User 1", 'employee_id': 100 },
+    ]
+
+    onRpc("get_mandatory_days", () => ({}));
+    onRpc("get_unusual_days", () => ({}));
+    onRpc("get_allocation_data_request", () => ({}));
+    onRpc("get_special_days_data", () => ({bankHolidays: [], mandatoryDays: []}));
+    onRpc("hr.employee", "get_time_off_dashboard_data", () => (
+        {has_accrual_allocation: true, allocation_data: {}, allocation_request_amount: 0}
+    ));
+
+    await mountView({
+            type: "calendar",
+            resModel: "hr.time",
+            arch: `
+            <calendar js_class="time_off_calendar_dashboard"
+                    string="Time Off Request"
+                    form_view_id="hr_time_view_form_dashboard_new_time_off"
+                    event_open_popup="true"
+                    date_start="date_from"
+                    date_stop="date_to"
+                    quick_create="0"
+                    show_date_picker="0"
+                    show_unusual_days="True"
+                    hide_time="True"
+                    mode="year">
+                <field name="display_name" string=""/>
+                <field name="work_entry_type_id" filters="1" invisible="1" color="color"/>
+                <field name="state" invisible="1"/>
+                <field name="is_hatched" invisible="1" />
+                <field name="is_striked" invisible="1"/>
+            </calendar>`,
+            context: user.context
+        });
+    await clickDate("2024-01-09");
+    await click(".o_cw_popover_link");
+    await waitFor("button:contains(Delete Time Off)");
+    await click(".btn-close");
+    await clickDate("2024-01-10");
+    await click(".o_cw_popover_link");
+    await waitFor("button:contains(Cancel Time Off)");
+})
+

addons/hr_time/static/tests/time_off_dashboard.test.js
@@ -1,0 +1,108 @@
+import { describe, expect, test } from "@odoo/hoot";
+import { mockDate } from "@odoo/hoot-mock";
+import { defineModels, fields, getService, models, mountWebClient, onRpc } from "@web/../tests/web_test_helpers";
+import { defineHrTimeModels } from "./hr_time_test_helpers";
+
+describe.current.tags("desktop");
+
+class HrTime extends models.Model {
+    _views = {
+        calendar: `
+            <calendar js_class="time_off_calendar_dashboard"
+                      string="Time Off Request"
+                      form_view_id="%(hr_time.hr_time_view_form_dashboard_new_time_off)d"
+                      event_open_popup="1"
+                      date_start="date_from"
+                      date_stop="date_to"
+                      quick_create="0"
+                      show_unusual_days="1"
+                      color="color"
+                      hide_time="1"
+                      mode="year"
+            >
+                <field name="name"/>
+                <field name="work_entry_type_id" filters="1" invisible="1" color="color"/>
+                <field name="state" invisible="1"/>
+            </calendar>
+        `,
+    };
+
+    color = fields.Integer({ related: "work_entry_type_id.color" });
+    date_from = fields.Datetime();
+    date_to = fields.Datetime();
+    department_id = fields.Many2one({ relation: "hr.department" });
+    employee_id =  fields.Many2one({ relation: "hr.employee" });
+    work_entry_type_id = fields.Many2one({ relation: "hr.work.entry.type" });
+    holiday_type = fields.Char();
+    name = fields.Char();
+    number_of_days = fields.Integer();
+    state = fields.Char();
+}
+
+class HrWorkEntryType extends models.Model {
+    name = fields.Char();
+    color = fields.Integer();
+}
+
+defineHrTimeModels();
+defineModels([HrTime, HrWorkEntryType]);
+
+onRpc("hr.employee", "get_time_off_dashboard_data", () => (
+    {has_accrual_allocation: true, allocation_data: {}, allocation_request_amount: 0}
+));
+onRpc("hr.employee", "get_mandatory_days", () => ({}));
+onRpc("hr.employee", "get_special_days_data", () => ({ mandatoryDays: [], bankHolidays: [] }));
+onRpc("hr.time", "get_unusual_days", () => ({}));
+onRpc("hr.time", "has_access", () => true);
+onRpc("hr.work.entry.type", "has_accrual_allocation", () => true);
+
+test(`test employee is passed to get_time_off_dashboard_data`, async () => {
+    onRpc("hr.employee", "get_time_off_dashboard_data", ({ kwargs }) => {
+        expect.step(kwargs.context.employee_id);
+    });
+
+    await mountWebClient();
+    await getService("action").doAction({
+        id: 1,
+        res_model: "hr.time",
+        type: "ir.actions.act_window",
+        views: [[false, "calendar"]],
+        context: { employee_id: [200] },
+        domain: [["employee_id", "in", [200]]],
+    });
+    expect.verifySteps([200]);
+});
+
+test(`test basic rendering`, async () => {
+    mockDate("2025-03-18 08:00:00");
+    onRpc("hr.employee", "get_mandatory_days", () => ({ "2025-03-17": 5 }));
+    onRpc("hr.employee", "get_special_days_data", () => ({
+        mandatoryDays: [
+            {
+                id: -2,
+                colorIndex: 5,
+                end: "2025-03-17T23:59:59.999999",
+                endType: "datetime",
+                isAllDay: true,
+                start: "2025-03-17T00:00:00",
+                startType: "datetime",
+                title: "Test Mandatory Day",
+            },
+        ],
+        bankHolidays: [],
+    }));
+
+    await mountWebClient();
+    await getService("action").doAction({
+        id: 1,
+        res_model: "hr.time",
+        type: "ir.actions.act_window",
+        views: [[false, "calendar"]],
+        context: { employee_id: [200] },
+        domain: [["employee_id", "in", [200]]],
+    });
+    expect(`.o_calendar_filter:contains("Legend")`).toHaveCount(1);
+    expect(`.o_calendar_filter:contains("To Approve")`).toHaveCount(1);
+    expect(`.o_calendar_filter:contains("Mar 17, 2025 : Test Mandatory Day")`).toHaveCount(1);
+    expect(`.fc-day.hr_mandatory_day_5[data-date="2025-03-17"]`).toHaveCount(1);
+});