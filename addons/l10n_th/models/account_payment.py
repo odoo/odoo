MoLeap/odooo

[IMP] l10n_th: withholding tax reporting prep
This pr prepares the Thai localization for two other tasks
that concern reporting withholding taxes.
As the two tasks require the same fields and data, we will
create these first to pave the way.

task-6021683

addons/l10n_th/models/account_payment.py
@@ -1,0 +1,38 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import api, fields, models
+
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+
+    # ------------------
+    # Fields declaration
+    # ------------------
+
+    l10n_th_wth_condition = fields.Selection(
+        string="Withholding Condition",
+        selection=[
+            ('at_source', 'Withhold at source'),
+            ('forever', 'Paid by payer (Forever/Gross-up)'),
+            ('once_time', 'Paid by payer (One-time)'),
+        ],
+        compute='_compute_l10n_th_wth_condition',
+        store=True,
+        readonly=False,
+        tracking=True,
+    )
+
+    # --------------------------------
+    # Compute, inverse, search methods
+    # --------------------------------
+
+    @api.depends('should_withhold_tax', 'country_code')
+    def _compute_l10n_th_wth_condition(self):
+        """
+        Compute the default value only if relevant for the current payment's country.
+        """
+        for payment in self:
+            if payment.should_withhold_tax and payment.country_code == 'TH':
+                payment.l10n_th_wth_condition = payment.l10n_th_wth_condition or 'at_source'
+            else:
+                payment.l10n_th_wth_condition = False

addons/l10n_th/tests/test_wth_payment.py
@@ -1,0 +1,58 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import fields
+from odoo.fields import Command
+from odoo.tests import tagged
+
+from odoo.addons.account.tests.common import AccountTestInvoicingCommon
+
+
+@tagged('post_install_l10n', 'post_install', '-at_install')
+class TestL10nTHWTHPayment(AccountTestInvoicingCommon):
+    """ Test the payment flows """
+
+    @classmethod
+    @AccountTestInvoicingCommon.setup_country('th')
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.company_data['company'].withholding_tax_base_account_id = cls.env['account.account'].create({
+            'code': 'WITHB',
+            'name': 'Withholding Tax Base Account',
+            'reconcile': True,
+            'account_type': 'asset_current',
+        })
+        cls.withholding_sequence = cls.env['ir.sequence'].create({
+            'implementation': 'no_gap',
+            'name': 'Withholding Sequence',
+            'padding': 4,
+            'number_increment': 1,
+        })
+        cls.outstanding_account = cls.env['account.account'].create({
+            'name': "Outstanding Payments",
+            'code': 'OSTP420',
+            'reconcile': False,
+            'account_type': 'asset_current',
+        })
+
+    def test_withholding_condition_is_propagated(self):
+        withholding_tax = self.percent_tax(-1, is_withholding_tax_on_payment=True, withholding_sequence_id=self.withholding_sequence.id)
+
+        invoice = self.env['account.move'].create({
+            'move_type': 'in_invoice',
+            'partner_id': self.partner_a.id,
+            'invoice_date': fields.Date.today(),
+            'invoice_line_ids': [Command.create({
+                'product_id': self.product_a.id,
+                'price_unit': 1000.0,
+                'tax_ids': [Command.set(withholding_tax.ids)],
+            })],
+        })
+        invoice.action_post()
+        payment_register = self.env['account.payment.register']\
+            .with_context(active_model='account.move', active_ids=invoice.ids)\
+            .create({})
+        # Assert that the computation set the default as expected.
+        self.assertEqual(payment_register.l10n_th_wth_condition, 'at_source')
+        payment_register.l10n_th_wth_condition = 'once_time'
+        payment = payment_register._create_payments()
+        # Assert that the manually set condition was propagated to the payment as expected.
+        self.assertEqual(payment.l10n_th_wth_condition, 'once_time')

addons/l10n_th/wizards/account_payment_register.py
@@ -1,0 +1,55 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import api, fields, models
+
+
+class AccountPaymentRegister(models.TransientModel):
+    _inherit = 'account.payment.register'
+
+    # ------------------
+    # Fields declaration
+    # ------------------
+
+    l10n_th_wth_condition = fields.Selection(
+        string="Withholding Condition",
+        selection=[
+            ('at_source', 'Withhold at source'),
+            ('forever', 'Paid by payer (Forever/Gross-up)'),
+            ('once_time', 'Paid by payer (One-time)'),
+        ],
+        compute='_compute_l10n_th_wth_condition',
+        store=True,
+        readonly=False,
+    )
+
+    # --------------------------------
+    # Compute, inverse, search methods
+    # --------------------------------
+
+    @api.depends('can_edit_wizard', 'should_withhold_tax', 'country_code')
+    def _compute_l10n_th_wth_condition(self):
+        """
+        Compute the default value only if relevant for the current payment's country.
+        """
+        for wizard in self:
+            if wizard.can_edit_wizard and wizard.should_withhold_tax and wizard.country_code == 'TH':
+                wizard.l10n_th_wth_condition = wizard.l10n_th_wth_condition or 'at_source'
+            else:
+                wizard.l10n_th_wth_condition = False
+
+    # -----------------------
+    # CRUD, inherited methods
+    # -----------------------
+
+    def _create_payment_vals_from_wizard(self, batch_result):
+        """
+        Update the computation of the payment vals in order to correctly set the outstanding account as well as the
+        withholding line when needed.
+        """
+        # EXTEND 'account'
+        payment_vals = super()._create_payment_vals_from_wizard(batch_result)
+
+        if not self.should_withhold_tax or self.country_code != 'TH':
+            return payment_vals
+
+        payment_vals['l10n_th_wth_condition'] = self.l10n_th_wth_condition
+        return payment_vals

odoo/odoo

addons/l10n_th/models/account_payment.py
@@ -1,0 +1,38 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import api, fields, models
+
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+
+    # ------------------
+    # Fields declaration
+    # ------------------
+
+    l10n_th_wth_condition = fields.Selection(
+        string="Withholding Condition",
+        selection=[
+            ('at_source', 'Withhold at source'),
+            ('forever', 'Paid by payer (Forever/Gross-up)'),
+            ('one_time', 'Paid by payer (One-time)'),
+        ],
+        compute='_compute_l10n_th_wth_condition',
+        store=True,
+        readonly=False,
+        tracking=True,
+    )
+
+    # --------------------------------
+    # Compute, inverse, search methods
+    # --------------------------------
+
+    @api.depends('should_withhold_tax', 'country_code')
+    def _compute_l10n_th_wth_condition(self):
+        """
+        Compute the default value only if relevant for the current payment's country.
+        """
+        for payment in self:
+            if payment.should_withhold_tax and payment.country_code == 'TH':
+                payment.l10n_th_wth_condition = payment.l10n_th_wth_condition or 'at_source'
+            else:
+                payment.l10n_th_wth_condition = False

addons/l10n_th/tests/test_wth_payment.py
@@ -1,0 +1,58 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import fields
+from odoo.fields import Command
+from odoo.tests import tagged
+
+from odoo.addons.account.tests.common import AccountTestInvoicingCommon
+
+
+@tagged('post_install_l10n', 'post_install', '-at_install')
+class TestL10nTHWTHPayment(AccountTestInvoicingCommon):
+    """ Test the payment flows """
+
+    @classmethod
+    @AccountTestInvoicingCommon.setup_country('th')
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.company_data['company'].withholding_tax_base_account_id = cls.env['account.account'].create({
+            'code': 'WITHB',
+            'name': 'Withholding Tax Base Account',
+            'reconcile': True,
+            'account_type': 'asset_current',
+        })
+        cls.withholding_sequence = cls.env['ir.sequence'].create({
+            'implementation': 'no_gap',
+            'name': 'Withholding Sequence',
+            'padding': 4,
+            'number_increment': 1,
+        })
+        cls.outstanding_account = cls.env['account.account'].create({
+            'name': "Outstanding Payments",
+            'code': 'OSTP420',
+            'reconcile': False,
+            'account_type': 'asset_current',
+        })
+
+    def test_withholding_condition_is_propagated(self):
+        withholding_tax = self.percent_tax(-1, is_withholding_tax_on_payment=True, withholding_sequence_id=self.withholding_sequence.id)
+
+        invoice = self.env['account.move'].create({
+            'move_type': 'in_invoice',
+            'partner_id': self.partner_a.id,
+            'invoice_date': fields.Date.today(),
+            'invoice_line_ids': [Command.create({
+                'product_id': self.product_a.id,
+                'price_unit': 1000.0,
+                'tax_ids': [Command.set(withholding_tax.ids)],
+            })],
+        })
+        invoice.action_post()
+        payment_register = self.env['account.payment.register']\
+            .with_context(active_model='account.move', active_ids=invoice.ids)\
+            .create({})
+        # Assert that the computation set the default as expected.
+        self.assertEqual(payment_register.l10n_th_wth_condition, 'at_source')
+        payment_register.l10n_th_wth_condition = 'one_time'
+        payment = payment_register._create_payments()
+        # Assert that the manually set condition was propagated to the payment as expected.
+        self.assertEqual(payment.l10n_th_wth_condition, 'one_time')

addons/l10n_th/wizards/account_payment_register.py
@@ -1,0 +1,55 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import api, fields, models
+
+
+class AccountPaymentRegister(models.TransientModel):
+    _inherit = 'account.payment.register'
+
+    # ------------------
+    # Fields declaration
+    # ------------------
+
+    l10n_th_wth_condition = fields.Selection(
+        string="Withholding Condition",
+        selection=[
+            ('at_source', 'Withhold at source'),
+            ('forever', 'Paid by payer (Forever/Gross-up)'),
+            ('one_time', 'Paid by payer (One-time)'),
+        ],
+        compute='_compute_l10n_th_wth_condition',
+        store=True,
+        readonly=False,
+    )
+
+    # --------------------------------
+    # Compute, inverse, search methods
+    # --------------------------------
+
+    @api.depends('can_edit_wizard', 'should_withhold_tax', 'country_code')
+    def _compute_l10n_th_wth_condition(self):
+        """
+        Compute the default value only if relevant for the current payment's country.
+        """
+        for wizard in self:
+            if wizard.can_edit_wizard and wizard.should_withhold_tax and wizard.country_code == 'TH':
+                wizard.l10n_th_wth_condition = wizard.l10n_th_wth_condition or 'at_source'
+            else:
+                wizard.l10n_th_wth_condition = False
+
+    # -----------------------
+    # CRUD, inherited methods
+    # -----------------------
+
+    def _create_payment_vals_from_wizard(self, batch_result):
+        """
+        Update the computation of the payment vals in order to correctly set the outstanding account as well as the
+        withholding line when needed.
+        """
+        # EXTEND 'account'
+        payment_vals = super()._create_payment_vals_from_wizard(batch_result)
+
+        if not self.should_withhold_tax or self.country_code != 'TH':
+            return payment_vals
+
+        payment_vals['l10n_th_wth_condition'] = self.l10n_th_wth_condition
+        return payment_vals