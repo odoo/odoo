d59d93dd30645db32e1342a8788db2e1a09a4008
hossamelgendy1 <hoelg@odoo.com>
2026-04-08 14:27:23 +0200

    [PERF] stock: speedup _free_reservation within stock quants validation action
    Applying stock quants validation was performing poorly due to multiple bottlenecks in `Picking._check_entire_pack` and `StockMoveLine._apply_putaway_strategy`:
    - Redundant updates were performed on `location_dest_id` in the move lines and the package levels (which internally update all related move lines too), even when the location remained unchanged.
    - The main loop inside `_check_entire_pack` was O(N^2) time relative to the number of move lines due to internal filtering logic.
    - Cache misses triggered unnecessary SQL queries when retrieving `move_line_ids` from `package levels`, while they are already cached via the pickings and can be grouped by `package_level`
    
    Benchmark conducted on a customer database with 400k `stock_move_line` records within 800 `pickings`, testing performance of the action `StockQuant.action_validate` with different sizes of move lines. Each test was run multiple times and shown is the average mean, all with negligible variance. Here're the numbers:
    Benchmark (1k lines): 10.5s -> 2.2s (-80%)
    Benchmark (5k lines): 121s -> 8.5s (-93%)
    Benchmark (50k lines): 887s -> 56s (-94%)
    Benchmark (400k lines): timeout -> 777s (within time limit)
    
    OPW-6045513
    
    closes odoo/odoo#257829
    
    Signed-off-by: Tiffany Chang (tic) <tic@odoo.com>

--- addons/stock/models/stock_move_line.py
+++ addons/stock/models/stock_move_line.py
@@ -261,20 +261,23 @@ class StockMoveLine(models.Model):
             excluded_smls = set(smls.ids)
             if package.package_type_id:
                 best_loc = smls.move_id.location_dest_id.with_context(exclude_sml_ids=excluded_smls, products=smls.product_id, locations=locations)._get_putaway_strategy(self.env['product.product'], package=package)
-                smls.location_dest_id = smls.package_level_id.location_dest_id = best_loc
+                smls.package_level_id.filtered(lambda pl: pl.location_dest_id != best_loc).location_dest_id = best_loc
             elif package:
                 used_locations = set()
                 for sml in smls:
                     if len(used_locations) > 1:
                         break
-                    sml.location_dest_id = sml.move_id.location_dest_id.with_context(exclude_sml_ids=excluded_smls, locations=locations)._get_putaway_strategy(sml.product_id, quantity=sml.quantity)
+                    putaway_loc_id = sml.move_id.location_dest_id.with_context(exclude_sml_ids=excluded_smls, locations=locations)._get_putaway_strategy(sml.product_id, quantity=sml.quantity)
+                    if putaway_loc_id != sml.location_dest_id:
+                        sml.location_dest_id = putaway_loc_id
                     excluded_smls.discard(sml.id)
                     used_locations.add(sml.location_dest_id)
                 if len(used_locations) > 1:
                     for move, grouped_smls in smls.grouped('move_id').items():
                         grouped_smls.location_dest_id = move.location_dest_id
                 else:
-                    smls.package_level_id.location_dest_id = smls.location_dest_id
+                    smls_location_dest_id = smls.location_dest_id
+                    smls.package_level_id.filtered(lambda pl: pl.location_dest_id != smls_location_dest_id).location_dest_id = smls_location_dest_id
             else:
                 for sml in smls:
                     putaway_loc_id = sml.move_id.location_dest_id.with_context(exclude_sml_ids=excluded_smls)._get_putaway_strategy(
