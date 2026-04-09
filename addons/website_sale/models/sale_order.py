4ea3fd50ca5076ed36b448d8490fc094271df046
Josse Colpaert <jco@odoo.com>
2026-04-09 11:02:20 +0000

    [FIX] website_sale: allow to skip billing address for services like event tickets
    https://github.com/odoo/odoo/pull/248464 and commit
    4bcdaaa4bd1da4baabcb17fc56176347f0556b63 removed the possibility to
    sell event tickets without entering the billing address.
    
    It says that we need the fiscal position from the billing address,
    but that is most often not the case when you sell services.
    
    Here we are not going to revert entirely so it would change again for
    the user, but provide a system parameter for the case the user sells
    event tickets and filling in the address is just a burden on the user.
    (e.g. Eventbrite, specialist in event tickets does not require an address)
    
    See further conversation on task-5143124
    
    X-original-commit: 3a40c7a8e01d5b835db19b46bb45dfbeac7c16cd

--- addons/website_sale/models/sale_order.py
+++ addons/website_sale/models/sale_order.py
@@ -311,11 +311,17 @@ class SaleOrder(models.Model):
     def _needs_customer_address(self):  # noqa: PLR6301
         """Return whether we need the address details of the customer (country, street, ...).
 
-        Make it true by default as it's required before payment for taxes based on fiscal position.
+        Orders with physical goods always require full customer address.
+        Orders without goods (services only) require customer address by default to correctly
+        determine fiscal position, taxes, pricelists (if based on country and geoip cannot be
+        trusted).
+
+        A dedicated system parameter can be set to False/0 to speed up the checkout process
+        and skip the address requirement for services.
         """
-        # TODO: should probably be removed in master, as we cannot skip the address form
-        # when the taxes applied on product/service depend on the fiscal position.
-        return True
+        return not self.only_services or self.env["ir.config_parameter"].sudo().get_bool(
+            "website_sale.require_billing_details_for_services", True
+        )
 
     def _update_address(self, partner_id, fnames=None):
         if not fnames:
