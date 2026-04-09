60266edd45312889ce1f5f2b00247cd7964c6d96
Guillaume V <gvar@odoo.com>
2026-04-09 09:53:00 +0000

    [FIX] pos_viva_wallet: send cashRegisterId for Viva when cashier name is missing
    When validating a payment in POS Kiosk with Viva payment method
    we get a Viva.com error
    Viva’s card-terminal API validates the JSON body with Pydantic and
    requires a non-empty ``cashRegisterId``.
    
    Steps to reproduce:
    -------------------
    * Open POS in kiosk
    * Make an order and pay with Viva
    > Observation:
    Viva returns a validation error: ``cashRegisterId`` is missing
    or required in the request body (Pydantic ``missing`` on ``body.cashRegisterId``).
    
    Why the fix:
    ------------
    Compute ``cashRegisterId`` in the POS client as cashier name, then
    ``pos.config.name`` so the value is always a non-empty string sent
    to ``viva_wallet_send_payment_request``.
    
    opw-6091223
    
    closes odoo/odoo#267857
    
    X-original-commit: 1846566391968342a84bff796f6ff3145dfc1599
    Signed-off-by: Stéphane Vanmeerhaeghe (stva) <stva@odoo.com>
    Signed-off-by: Guillaume Vardakis (gvar) <gvar@odoo.com>

--- addons/pos_viva_com/static/src/app/payment_viva_com.js
+++ addons/pos_viva_com/static/src/app/payment_viva_com.js
@@ -46,6 +46,10 @@ export class PaymentVivaCom extends PaymentInterface {
         return await this._viva_com_cancel(line);
     }
 
+    getCashRegisterId() {
+        return this.pos.getCashier().name?.trim() || this.pos.config.name;
+    }
+
     _call_viva_com(data, action, paymentLine) {
         return this.callPaymentMethod(action, [[this.payment_method_id.id], data]).catch(
             this._handleOdooConnectionFailure.bind(this, paymentLine)
@@ -84,11 +88,12 @@ export class PaymentVivaCom extends PaymentInterface {
         }
 
         line.viva_com_session_id = order.uuid + " - " + uuidv4();
+        const cashRegisterId = this.getCashRegisterId();
         var data = {
             sessionId: line.viva_com_session_id,
             parentSessionId: line.uiState.vivaComParentSessionId,
             terminalId: line.payment_method_id.viva_com_terminal_id,
-            cashRegisterId: this.pos.config.name,
+            cashRegisterId,
             amount: roundPrecision(Math.abs(line.amount * 100)),
             currencyCode: this.pos.currency.iso_numeric.toString(),
             merchantReference: line.viva_com_session_id + "/" + this.pos.session.id,
@@ -107,9 +112,10 @@ export class PaymentVivaCom extends PaymentInterface {
     }
 
     async _viva_com_cancel(line) {
+        const cashRegisterId = this.getCashRegisterId();
         var data = {
             sessionId: line.viva_com_session_id,
-            cashRegisterId: this.pos.config.name,
+            cashRegisterId,
         };
         return this._call_viva_com(data, "viva_com_send_payment_cancel", line).then((data) => {
             if (data.error) {
