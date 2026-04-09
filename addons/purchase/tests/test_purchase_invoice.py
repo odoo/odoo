950c1ff9bc5eff7c4a9a55fe693b29bdfb4daa47
Maeva De Keyser (MDEK) <mdek@odoo.com>
2026-04-09 13:05:50 +0000

    [FIX] purchase, sale: keep analytic distribution on analytic account change
    __
    
    When confirming a Purchase Order holding lines with an analytic
    distribution that has been manually added, and creating a vendor bill
    out of this PO using the Auto-Complete field. When we change the
    account of that line, the line loses the manually added Analytic
    Distribution.
    
    1. Go to Accounting. Click on the tab Configuration; under the Analytic
    Accounting section, click on Analytic Distribution Models.
    2. Create an Analytic Distribution Model for a product.
    3. Go to Purchase. Create a new PO, set a Vendor and select the product
    you created the Analytic Distribution Model for. On the right side of
    the form, click on the view menu and check Analytic Distribution to
    make it appear.
    4. Click on the Analytic Distribution of the product and add a new
    one; for example, select Administrative in the Departments section.
    5. Confirm order.
    6. Go to Accounting and click on the Vendors tab > Bills. Create a new
    bill, and in the field Auto-Complete, select the PO you just created.
    7. Change the account of the line.
    
    Only the account should be changed on the line.
    
    The manually added Analytic Distribution has disappeared.
    
    When we change the `account_id` field, the compute method
    `_compute_analytic_distribution` is triggered. This method retrieves
    the related distributions of the line:
    https://github.com/odoo/odoo/blob/af32885ec5f07d492f3b8e8fff1785996a739f72/addons/account/models/account_move_line.py#L1154
    which, in the context of Purchase, calls this method:
    https://github.com/odoo/odoo/blob/af32885ec5f07d492f3b8e8fff1785996a739f72/addons/purchase/models/account_invoice.py#L540-L545
    We retrieve the distribution of the related line using
    `self.purchase_line_id.analytic_distribution`. However, this code isn't
    triggered when the move line has an analytic distribution, even though
    the related line `purchase_line_id` might have one! Therefore, we need
    to execute that code whether or not our move line has an analytic
    distribution.
    Note: the same behavior is to avoid when creating invoices for
    quotations.
    
    __
    opw-6062466
    
    closes odoo/odoo#267039
    
    X-original-commit: fd897daa95a6e7bfec494551463307f8169bf6bd
    Signed-off-by: Quentin Wolfs (quwo) <quwo@odoo.com>

--- addons/purchase/tests/test_purchase_invoice.py
+++ addons/purchase/tests/test_purchase_invoice.py
@@ -764,6 +764,46 @@ class TestPurchaseToInvoice(TestPurchaseToInvoiceCommon):
         self.assertEqual(inv.invoice_line_ids[2].name, f"{pol_prod_product_in_name.name}", "When description contains the product name, the invoice line name should only be the description")
         self.assertEqual(inv.invoice_line_ids[3].name, f"{pol_prod_name_in_product.product_id.display_name}\n{pol_prod_name_in_product.name}", "When the product name contains the description, the invoice line name should be the product name and the description")
 
+    def test_keep_distribution_on_analytic_account_change(self):
+        """
+        Checks that when we create a vendor bill from a PO on which we set an analytic distribution manually,
+        that analytic distribution doesn't change when we change the analytic account of the product on the bill.
+        """
+        analytic_plan = self.env['account.analytic.plan'].create({
+            'name': 'default',
+            'applicability_ids': [Command.create({
+                'business_domain': 'bill',
+                'applicability': 'optional',
+            })]
+        })
+        analytic_account = self.env['account.analytic.account'].create({'name': 'default', 'plan_id': analytic_plan.id})
+        distribution_model_product = self.env['account.analytic.distribution.model'].create({
+            'product_id': self.product_order.id,
+            'analytic_distribution': {str(analytic_account.id): 100},
+            'company_id': self.company.id,
+        })
+        analytic_plan_2 = self.env['account.analytic.plan'].create({'name': 'Plan Test'})
+        analytic_account_2 = self.env['account.analytic.account'].create({'name': 'manual', 'plan_id': analytic_plan_2.id})
+        purchase_order = self.env['purchase.order'].create({
+            'partner_id': self.partner_a.id,
+            'order_line': [
+                Command.create({'product_id': self.product_order.id}),
+            ],
+        })
+        self.assertEqual(purchase_order.order_line.analytic_distribution, distribution_model_product.analytic_distribution)
+        analytic_distribution_manual = {str(analytic_account.id) + "," + str(analytic_account_2.id): 100}
+        purchase_order.order_line.write({'analytic_distribution': analytic_distribution_manual})
+        purchase_order.button_confirm()
+        purchase_order.order_line.qty_received = 12
+
+        move_form = Form(self.env['account.move'].with_context(default_move_type='in_invoice'))
+        move_form.purchase_vendor_bill_id = self.env['purchase.bill.union'].browse(-purchase_order.id)
+        invoice = move_form.save()
+
+        invoice.line_ids[0].account_id = self.cash_rounding_a.profit_account_id
+        # The analytic distribution of the line shouldn't change if its account changes.
+        self.assertEqual(invoice.line_ids[0].analytic_distribution, analytic_distribution_manual)
+
     def test_compute_po_count_with_different_plan(self):
         analytic_plan_1, analytic_plan_2 = self.env['account.analytic.plan'].create([
             {'name': 'Plan 1'}, {'name': 'Plan 2'}
