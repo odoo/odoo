04b19944219e1c9de23e86dfb09e90ad066ee8af
Mahdi Alijani (malj) <malj@odoo.com>
2026-04-10 10:50:05 +0000

    [FIX] account_payment: allow bookkeeper register payment
    Issue:
    ---
    Bookkeeper user cannot register payment using a payment method
    due to access issues.
    
    Steps to reproduce:
    1- Setup a payment provider. e.g. Demo
    2- Set Demo user accounting access as bookkeeper.
    3- Login using Demo user.
    4- Create an invoice and register a payment.
    5- Choose Demo payment method.
    
    An access error is raised.
    
    Cause:
    ---
    Before cb7fba0782d8af365e29832ac7dd166590d80b16,
    `provider.capture_manually` was checked inside the search filter.
    As the search was `sudo`, there was no access issue.
    After that commit, the `capture_manually` is checked with no sudo,
    causing this access issue if the user doesn't have read access on
    the payment provider.
    
    opw-6001170
    
    X-original-commit: 6d153fd7480da6848d2fd72146df6e3ed68fdef3

--- addons/account_payment/wizards/account_payment_register.py
+++ addons/account_payment/wizards/account_payment_register.py
@@ -38,7 +38,7 @@ class AccountPaymentRegister(models.TransientModel):
                 wizard.can_edit_wizard
                 and wizard.use_electronic_payment_method
                 and (provider := wizard.payment_method_line_id.payment_provider_id)
-                and not provider.capture_manually
+                and not provider.sudo().capture_manually
             ):
                 token_partners = wizard.partner_id
                 batch = wizard._get_batches()[0]
