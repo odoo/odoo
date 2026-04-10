ba3dbefbebad9cd60ed633b3f31b053acc7f047b
Maryam Kia <maki@odoo.com>
2026-04-10 11:10:34 +0200

    [FIX] mail: cleaning up inbox queries `TestInboxPerformance`
    The queries relating to fetching messages in inbox have changed over time. This
    commit cleans up the summary of these queries.
    
    Part-of: odoo/odoo#258603
    Signed-off-by: Maryam Kia (maki) <maki@odoo.com>

--- addons/test_discuss_full/tests/test_performance_inbox.py
+++ addons/test_discuss_full/tests/test_performance_inbox.py
@@ -16,54 +16,51 @@ class TestInboxPerformance(HttpCase, MailCommon):
         # Queries (in order):
         #   - search website (get_current_website by domain)
         #   - search website (get_current_website default)
-        #   - search website_rewrite (_get_rewrites) sometimes occurs depending on the routing cache
-        #   - insert res_device_log
+        #   - sometimes could occur depending on the routing cache (website_rewrite, ir_config_parameter, res.lang flag_image)
         #   - _xmlid_lookup (_get_public_users)
-        #   - fetch website (_cached_data)
-        #   - _get ir_config_parameter (_pre_dispatch website_sale)
+        #   - user authentication(_get_lock_timeouts)
         #   4 _message_fetch:
         #       2 _search_needaction:
         #           - fetch res_users (current user)
         #           - search ir_rule (_get_rules for mail.notification)
         #       - search ir_rule (_get_rules)
         #       - search mail_message
-        #   30 store add message:
-        #       - search mail_message_schedule
+        #   - search bus_bus (_bus_last_id in bus.py)
+        #   31 store add message:
         #       - fetch mail_message
+        #       - search mail_message_schedule
         #       - search mail_followers
-        #       2 thread:
+        #       - search ir_rule (_get_rules for rating.rating)
+        #       - read group rating_rating (_rating_get_stats_per_record for slide.channel)
+        #       - read group rating_rating (_rating_get_stats_per_record for product.template)
+        #       2 thread :
         #           - fetch slide_channel
         #           - fetch product_template
         #       - search mail_message_res_partner_starred_rel (_compute_starred)
         #       - search message_attachment_rel
-        #       - search mail_link_preview
-        #       - search mail_message_reaction
         #       - search mail_message_res_partner_rel
-        #       - fetch mail_message_subtype
-        #       - search mail_notification
-        #       7 _filtered_for_web_client:
+        #       - search mail_message_reaction
+        #       - search mail_poll (start_message_id)
+        #       - search mail_poll (end_message_id)
+        #       - search mail_message_link_preview
+        #       4 _filtered_for_web_client:
+        #           - search mail_notification
         #           - fetch mail_notification
-        #           4 _compute_domain:
-        #               - search ir_rule (_get_rules for res.partner)
-        #               - search res_groups_users_rel
-        #               - search rule_group_rel
-        #               - fetch ir_rule
-        #           - fetch res_company
         #           - fetch res_partner
+        #           - fetch mail_message_subtype
+        #       - search mail_tracking_value
         #       2 _compute_rating_id:
         #           - search rating_rating
         #           - fetch rating_rating
-        #       - search mail_tracking_value
         #       3 author:
         #           - fetch res_partner
         #           - search res_users
         #           - fetch res_users
-        #       - search ir_rule (_get_rules for rating.rating)
-        #       - read group rating_rating (_rating_get_stats_per_record for slide.channel)
+        #       2 compute message_needaction for slide.channel (one query per record due to the lack of batching)
+        #       2 compute message_needaction for product.template (one query per record due to the lack of batching)
         #       - read group rating_rating (_compute_rating_stats for slide.channel)
-        #       - read group rating_rating (_rating_get_stats_per_record for product.template)
         #       - read group rating_rating (_compute_rating_stats for product.template)
-        #   - _get ir_config_parameter (_save_session)
+
         first_model_records = self.env["product.template"].create(
             [{"name": "Product A1"}, {"name": "Product A2"}]
         )
@@ -78,5 +75,5 @@ class TestInboxPerformance(HttpCase, MailCommon):
                 rating_value="4",
             )
         self.authenticate(self.user_employee.login, self.user_employee.password)
-        with self.assertQueryCount(43):
+        with self.assertQueryCount(41):
             self.make_jsonrpc_request("/mail/inbox/messages")
