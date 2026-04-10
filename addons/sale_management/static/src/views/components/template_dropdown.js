7753a4b253aba0fe8ffac99e6583dbf03f388676
Corentin Deruyck <coder@odoo.com>
2026-04-10 10:07:50 +0200

    [IMP] sale,sale_management: Adding quotation template via new button
    The goal is to add the ability to choose which template to use
    when clicking the “New” button. The idea is to replicate what
    has already been implemented in the Project application. When
    clicking the “New” button and SO templates are available, users
    will be able to select a template from that first click. It will
    also be possible, directly from this menu, to navigate to the view
    for editing the template. Additionally, users will be able to
    delete the template from this menu.
    
    Furthermore, the “Quotation Template” field has been moved to the
    “Other Info” page, and three fields from the “Other Info” page
    have been moved into a new column.
    
    Finally, the Default Template option in the Sales app’s settings
    has been removed because it duplicates the purpose of the task.
    
    task-6088445

---
+++ addons/sale_management/static/src/views/components/template_dropdown.js
@@ -0,0 +1,120 @@
+import { useState } from "@web/owl2/utils";
+import { Component, onWillStart } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
+import { ConfirmationDialog, deleteConfirmationMessage } from "@web/core/confirmation_dialog/confirmation_dialog";
+import { Dropdown } from "@web/core/dropdown/dropdown";
+import { DropdownItem } from "@web/core/dropdown/dropdown_item";
+import { user } from "@web/core/user";
+
+export class SaleTemplateDropdown extends Component {
+    static template = "sale_management.SaleTemplateDropdown";
+    static components = {
+        Dropdown,
+        DropdownItem,
+    };
+
+    static props = {
+        hotkey: {
+            type: String,
+            optional: true,
+        },
+        newButtonClasses: String,
+        record: {
+            type: Object,
+            optional: true,
+        },
+    };
+    static defaultProps = {
+        hotkey: "c",
+        isDisabled: false,
+    };
+
+    setup() {
+        this.action = useService("action");
+        this.dialogService = useService("dialog");
+        this.orm = useService("orm");
+        this.state = useState({
+            hasQuotationTemplateFeature: false,
+            canManageTemplates: false,
+            quotationTemplates: [],
+        });
+        onWillStart(this.onWillStart);
+    }
+
+    async onWillStart() {
+        this.state.hasQuotationTemplateFeature = await user.hasGroup(
+            "sale_management.group_sale_order_template"
+        );
+        this.state.canManageTemplates = await user.hasGroup("sales_team.group_sale_manager");
+        if (!this.state.hasQuotationTemplateFeature) {
+            return;
+        }
+        this.state.quotationTemplates = await this.orm.searchRead(
+            "sale.order.template",
+            [["template_type", "=", "quotation"]],
+            ["id", "name"]
+        ).catch(() => []);
+    }
+
+    async _saveIfNeeded() {
+        if (!this.saveRecord) {
+            return true;
+        }
+        return await this.saveRecord();
+    }
+
+    get saveRecord() {
+        return this.props.record?.save?.bind(this.props.record);
+    }
+
+    get isFormView() {
+        return this.env.config?.viewType === "form";
+    }
+
+    async createQuotation({ additionalContext = {} } = {}) {
+        const saved = await this._saveIfNeeded();
+        if (!saved) {
+            return;
+        }
+
+        if (!additionalContext.default_sale_order_template_id) {
+            // If the user doesn't specify a template, we remove the previous one from the context
+            additionalContext.default_sale_order_template_id = false;
+        }
+
+        if (this.isFormView) {
+            await this.props.record.model.load({
+                resId: false,
+                mode: "edit",
+                context: additionalContext,
+            });
+            return;
+        }
+
+        await this.action.doAction(this.action.currentAction, {
+            viewType: "form",
+            additionalContext,
+        });
+    }
+
+    onEditClick(templateId) {
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            res_model: "sale.order.template",
+            res_id: templateId,
+            views: [[false, "form"]],
+        });
+    }
+
+    async onDeleteClick(templateId) {
+        this.dialogService.add(ConfirmationDialog, {
+            body: deleteConfirmationMessage,
+            confirm: async () => {
+                await this.orm.unlink("sale.order.template", [templateId]);
+                this.action.doAction("soft_reload");
+            },
+            cancel: () => {},
+        });
+    }
+
+}
