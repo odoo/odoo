70acf0d9e82e2524c8fe01be6abba0926fa71d2e
Walid (wasa) <wasa@odoo.com>
2026-04-10 17:51:07 +0200

    [FIX] html_editor: prevent color picker from closing when coloring icons
    Problem:
    Trying to change the color of an icon causes the color picker to close
    when hovering over colors.
    
    Cause:
    When the icon (`span`) is wrapped inside a `font` element, the
    `toolbar_namespace_providers` for the icon return `false` because the
    `font` wrapper was not handled. As a result, the `namespace` becomes
    `undefined`, which causes the toolbar to close during selection changes.
    
    Solution:
    Handle cases where an icon is wrapped by styling elements (such as
    `font`) so the correct toolbar namespace is preserved.
    
    Steps to reproduce:
    - Insert a Font Awesome icon using `/media`.
    - Click on the icon to open the toolbar.
    - Try to apply a color or background color.
    - Notice the color picker closes instantly when hovering over colors.
    
    task-6109153
    
    closes odoo/odoo#258729
    
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/html_editor/static/src/main/font/color_plugin.js
+++ addons/html_editor/static/src/main/font/color_plugin.js
@@ -8,6 +8,7 @@ import {
 } from "@html_editor/utils/color";
 import { fillEmpty, unwrapContents } from "@html_editor/utils/dom";
 import {
+    isElement,
     isEmptyBlock,
     isIconElement,
     isRedundantElement,
@@ -253,7 +254,12 @@ export class ColorPlugin extends Plugin {
                 const children = font && descendants(font);
                 const hasInlineGradient = font && isColorGradient(font.style["background-image"]);
                 const isFullySelected =
-                    children && children.every((child) => selectedNodes.includes(child));
+                    children &&
+                    children.every(
+                        (child) =>
+                            selectedNodes.includes(child) ||
+                            selectedNodes.some((node) => isElement(node) && node.contains(child))
+                    );
                 const isTextGradient =
                     hasInlineGradient && font.classList.contains("text-gradient");
                 const shouldReplaceExistingGradient =
