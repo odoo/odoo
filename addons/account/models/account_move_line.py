fleetapps/odoo3

[IMP] *: show price per product unit in product catalog
* = account, mrp, purchase, sale

When a product is being sold/purchased/manufactured/invoiced
in a different unit than the product unit's, the price per
product unit will be shown to faciliate comparison.

This commit also improves the catalog;
- Keep consistent pricing when changing quantity.
- Match with correct vendor for purchase orders.
- Create correct purchase order line and align it with the catalog.
- Correctly propagate unit, price and quantity between catalog and lines.
- Account for currency in price calculations.
- Avoid matching with random seller in unit computation in accounting.
- In accounting, the catalog will use the standard price,
just like its created in the vendor bill line.
- On deleting a product from the catalog and re-adding it
without reloading, the same unit will be used instead of
resetting it everytime we remove a product except in purchase
we match with a venodr and different quantities could match
with a different vendor pricelist.

Task:4736022

addons/account/models/account_move_line.py
@@ -1029,7 +1029,7 @@ class AccountMoveLine(models.Model):
         for line in self.filtered(lambda l: l.parent_state == 'draft'):
             # vendor bills should have the product purchase UOM
             if line.move_id.is_purchase_document():
-                seller_ids = line.product_id.seller_ids._get_filtered_supplier(line.company_id, line.product_id, False)
+                seller_ids = line.product_id.seller_ids._get_filtered_supplier(line.company_id, line.product_id, {'partner_id': line.partner_id})
                 line.product_uom_id = seller_ids[:1].uom_id or line.product_id.uom_id
             else:
                 line.product_uom_id = line.product_id.uom_id
@@ -3740,13 +3740,18 @@ class AccountMoveLine(models.Model):
 
         :param products: Recordset of `product.product`.
         :param dict kwargs: additional values given for inherited models.
+        :raise odoo.exceptions.ValueError: ``len(self.product_id) != 1``
         :rtype: dict
         :return: A dict with the following structure:
             {
                 'quantity': float,
                 'price': float,
                 'readOnly': bool,
-                'min_qty': int, (optional)
+                'min_qty': int (optional),
+                'uomDisplayName': string,
+                'uomId': int,
+                'uomConversion': float (optional),
+                'productUomDisplayName': string (optional),
             }
         """
         if self:
@@ -3763,6 +3768,10 @@ class AccountMoveLine(models.Model):
                 ),
                 'readOnly': self.move_id._is_readonly() or len(self) > 1,
                 'uomDisplayName': len(self) == 1 and self.product_uom_id.display_name or self.product_id.uom_id.display_name,
+                'uomId': self[0].product_uom_id.id,
+                'uomConversion': self[0].product_id.uom_id.factor / self[0].product_uom_id.factor,
+                'price': self[0].price_unit,
+                'productUomDisplayName': self.product_id.uom_id.display_name,
             }
         return {
             'quantity': 0,

odoo/odoo

addons/account/models/account_move_line.py
@@ -1073,7 +1073,7 @@ class AccountMoveLine(models.Model):
         for line in self.filtered(lambda l: l.parent_state == 'draft'):
             # vendor bills should have the product purchase UOM
             if line.move_id.is_purchase_document():
-                seller_ids = line.product_id.seller_ids._get_filtered_supplier(line.company_id, line.product_id, False)
+                seller_ids = line.product_id.seller_ids._get_filtered_supplier(line.company_id, line.product_id, {'partner_id': line.partner_id})
                 line.product_uom_id = seller_ids[:1].uom_id or line.product_id.uom_id
             else:
                 line.product_uom_id = line.product_id.uom_id
@@ -3858,29 +3858,31 @@ class AccountMoveLine(models.Model):
 
         :param products: Recordset of `product.product`.
         :param dict kwargs: additional values given for inherited models.
+        :raise odoo.exceptions.ValueError: ``len(self.product_id) != 1``
         :rtype: dict
         :return: A dict with the following structure:
             {
                 'quantity': float,
                 'price': float,
                 'readOnly': bool,
-                'min_qty': int, (optional)
+                'min_qty': int (optional),
+                'uomDisplayName': string,
+                'uomId': int,
+                'productUomFactor': float (optional),
+                'productUomDisplayName': string (optional),
             }
         """
         if self:
             self.product_id.ensure_one()
             return {
                 **self[0].move_id._get_product_price_and_data(self[0].product_id),
-                'quantity': sum(
-                    self.mapped(
-                        lambda line: line.product_uom_id._compute_quantity(
-                            qty=line.quantity,
-                            to_unit=line.product_id.uom_id,
-                        )
-                    )
-                ),
+                'quantity': self.quantity,
                 'readOnly': self.move_id._is_readonly() or len(self) > 1,
                 'uomDisplayName': len(self) == 1 and self.product_uom_id.display_name or self.product_id.uom_id.display_name,
+                'uomId': self[0].product_uom_id.id,
+                'productUomFactor': self[0].product_id.uom_id.factor / self[0].product_uom_id.factor,
+                'price': self[0].price_unit,
+                'productUomDisplayName': self.product_id.uom_id.display_name,
             }
         return {
             'quantity': 0,