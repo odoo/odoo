fed3ab0c078bba3569bccac626ebba198e612857
Sébastien (blse) <blse@odoo.com>
2026-04-13 08:12:35 +0200

    [MOV] website, *: move snippets dialog's logic for website visibility
    *: html_builder
    
    The logic for the snippet viewer (which takes care of preview of
    snippets in "Insert block" dialog) related to invisible snippets from
    `website` was defined in `html_builder` (with the rest of the snippet
    viewer logic).
    
    This commit moves those parts to the `website` module.
    
    task-5149984

--- addons/website/static/src/builder/snippet_viewer.js
+++ addons/website/static/src/builder/snippet_viewer.js
@@ -1,7 +1,8 @@
 import { SnippetViewer } from "@html_builder/snippets/snippet_viewer";
-import { onMounted, onPatched, onWillPatch, onWillUnmount } from "@odoo/owl";
+import { markup, onMounted, onPatched, onWillPatch, onWillUnmount } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
 import { patch } from "@web/core/utils/patch";
+import { Image } from "@html_builder/core/img";
 
 patch(SnippetViewer.prototype, {
     setup() {
@@ -29,5 +30,41 @@ patch(SnippetViewer.prototype, {
             onWillPatch(stopPreview);
             onWillUnmount(stopPreview);
         }
+    },
+
+    getPrefixIcons(snippetContentEl) {
+        /** @type {PrefixIconInfo[]} */
+        const icons = super.getPrefixIcons(snippetContentEl);
+        const styleProps = { style: "height: 1em", attrs: { fill: "var(--body-color)" } };
+        if (snippetContentEl.matches(".o_snippet_desktop_invisible")) {
+            icons.push({
+                keyClass: "o_prefix_desktop_invisible",
+                title: "Invisible on desktop",
+                Component: Image,
+                props: {
+                    src: "/html_builder/static/img/options/desktop_invisible.svg",
+                    ...styleProps,
+                },
+            });
+        }
+        if (snippetContentEl.matches(".o_snippet_mobile_invisible")) {
+            icons.push({
+                keyClass: "o_prefix_mobile_invisible",
+                title: "Invisible on mobile",
+                Component: Image,
+                props: {
+                    src: "/html_builder/static/img/options/mobile_invisible.svg",
+                    ...styleProps,
+                },
+            });
+        }
+        if (snippetContentEl.matches(".o_conditional_hidden")) {
+            icons.push({
+                keyClass: "o_prefix_conditional",
+                title: "Conditionally visible",
+                content: markup`<span class="fa fa-eye-slash"/>`,
+            });
+        }
+        return icons;
     },
 });
