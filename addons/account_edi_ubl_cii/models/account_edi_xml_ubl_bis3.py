f64e938e7a46176bbc96267c707c6b2b3b96122b
Louis Gobert <loug@odoo.com>
2026-04-13 12:53:14 +0000

    [FIX] account_edi_ubl_cii: ensure BR-*-08 TaxableAmount consistency
    Steps to reproduce:
    1. Create an invoice with a 0% tax (Exempt from VAT, category E)
    2. Add two lines with 20% discount and fractional prices:
       - qty=4, price_unit=39.615 and qty=4 with price_unit=0.84
    3. Send via Peppol
    4. Peppol rejects with: [BR-E-08] VAT category taxable amount shall
       equal the sum of Invoice line net amounts
    
    The TaxableAmount recalculation in _ubl_get_tax_subtotal_node was only
    applied for tax category 'S' (Standard Rate). However, Peppol schematron
    has identical rules for all tax categories: BR-E-08 (Exempt), BR-Z-08
    (Zero), BR-AE-08 (Reverse Charge), etc.
    
    When lines have discounts and fractional prices, the individually rounded
    LineExtensionAmount values can sum to a different total than the tax
    base_amount. This affects both rounding modes. For 'S' taxes this was
    already handled, but for 'E' (and others) it caused Peppol rejection.
    
    Remove the 'S'-only filter and match dynamically against the actual tax
    category code so the recalculation applies universally.
    
    opw-6093243
    
    X-original-commit: ea84f35ebaaaf115c28c997eae889c268a51a2b6

--- addons/account_edi_ubl_cii/models/account_edi_xml_ubl_bis3.py
+++ addons/account_edi_ubl_cii/models/account_edi_xml_ubl_bis3.py
@@ -917,43 +917,41 @@ class AccountEdiXmlUbl_Bis3(models.AbstractModel):
         # EXTENDS account.edi.xml.ubl
         node = super()._ubl_get_tax_subtotal_node(vals, tax_subtotal)
 
-        # [BR-S-08] cac:TaxSubtotal -> cbc:TaxableAmount should be computed based on the
-        # cbc:LineExtensionAmount of each line linked to the tax when cac:TaxCategory -> cbc:ID is S
-        # (Standard Rate).
+        # [BR-S-08]/[BR-E-08]/[BR-Z-08]/... cac:TaxSubtotal -> cbc:TaxableAmount should be
+        # computed based on the cbc:LineExtensionAmount of each line linked to the tax.
+        # This applies to all tax category codes (S, E, Z, AE, etc.) as each has a
+        # corresponding BR-*-08 schematron rule requiring this consistency.
         currency = tax_subtotal['currency']
         corresponding_line_node_amounts = [
             line_node['cbc:LineExtensionAmount']['_text']
             for tax_category_node in node['cac:TaxCategory']
-            if tax_category_node['cbc:ID']['_text'] == 'S'
             for line_key in ('cac:InvoiceLine', 'cac:CreditNoteLine', 'cac:DebitNoteLine')
             for line_node in vals['document_node'].get(line_key, [])
             for line_node_tax_category_node in line_node['cac:Item']['cac:ClassifiedTaxCategory']
             if (
-                line_node_tax_category_node['cbc:ID']['_text'] == 'S'
+                line_node_tax_category_node['cbc:ID']['_text'] == tax_category_node['cbc:ID']['_text']
                 and line_node_tax_category_node['cbc:Percent']['_text'] == tax_category_node['cbc:Percent']['_text']
                 and line_node_tax_category_node['_currency'] == tax_category_node['_currency']
             )
         ] + [
             -allowance_node['cbc:Amount']['_text']
             for tax_category_node in node['cac:TaxCategory']
-            if tax_category_node['cbc:ID']['_text'] == 'S'
             for allowance_node in vals['document_node']['cac:AllowanceCharge']
             if allowance_node['cbc:ChargeIndicator']['_text'] == 'false'
             for allowance_node_tax_category_node in allowance_node['cac:TaxCategory']
             if (
-                allowance_node_tax_category_node['cbc:ID']['_text'] == 'S'
+                allowance_node_tax_category_node['cbc:ID']['_text'] == tax_category_node['cbc:ID']['_text']
                 and allowance_node_tax_category_node['cbc:Percent']['_text'] == tax_category_node['cbc:Percent']['_text']
                 and allowance_node_tax_category_node['_currency'] == tax_category_node['_currency']
             )
         ] + [
             allowance_node['cbc:Amount']['_text']
             for tax_category_node in node['cac:TaxCategory']
-            if tax_category_node['cbc:ID']['_text'] == 'S'
             for allowance_node in vals['document_node']['cac:AllowanceCharge']
             if allowance_node['cbc:ChargeIndicator']['_text'] == 'true'
             for allowance_node_tax_category_node in allowance_node['cac:TaxCategory']
             if (
-                allowance_node_tax_category_node['cbc:ID']['_text'] == 'S'
+                allowance_node_tax_category_node['cbc:ID']['_text'] == tax_category_node['cbc:ID']['_text']
                 and allowance_node_tax_category_node['cbc:Percent']['_text'] == tax_category_node['cbc:Percent']['_text']
                 and allowance_node_tax_category_node['_currency'] == tax_category_node['_currency']
             )
