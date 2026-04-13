cc5deb5bd79465a2090643bb2e85196ae4d94424
Zachary (zavan) <zavan@odoo.com>
2026-04-13 15:38:07 +0000

    [FIX] hr_holidays_attendance: handle ignore public holidays leave
    # Setup
    You'll need a User with :
    - An active contract (for easiness of testing, a contract that started long ago with 8hrs/day)
    
    # How to reproduce
    - Create a new Time Off type with "Ignore Public Holidays" enabled
    - Create a Public Holiday for Period X
    - Create A Time Off request for the User for a Period Y that contains Period X
    - Go to the Time Off Ledger
    - Remove the Missing Hours filter and search for the dates in Period Y
    
    Exemple of periods :
    - Period X => Feb 10 2026 - Feb 10 2026
    - Period Y => Feb 9 2026 - Feb 11 2026
    
    # The problem
    The "Approved Time Off" and "Difference" values are wrong.
    
    With the given exemples, we'll see Feb 9 and Feb 11 with "Approved Time Off" values
    of 12hrs, which is wrong since the employee is supposed to work 8hrs a day, so he
    should have a time off of also 8hrs.
    
    # Cause
    The calculation for "Approved Time Off" is the following :
    Divide the `number_of_hours` of a hr_leave
    By the number of working days during the period of the leave
    
    Using our exemple, we get :
    `number_of_hours` = 24hrs
    number of working days = 2
    Approved Time Off = 24hrs / 2 => 12hrs, but we expect 8hrs
    
    The `number_of_hours` is correct since we checked "Ignore Public Holidays" (which actually means : include the public holidays in the number of hours of a leave)
    
    The problem is that the aggregation for the number of working days excludes automatically
    public holidays, without paying attention to the value of "Ignore Public Holidays" :
    https://github.com/odoo/odoo/blob/5e623af55fba64e812db6bcaf06d8f7c5d08f055/addons/hr_holidays_attendance/report/hr_leave_attendance_report.py#L193-L202
    Explanation for this part of the query : we only keep days where there is no record in
    resource_calendar_leaves (`WHERE rcl2.id IS NULL`)  that contains that day
    and that are considered public (`AND rcl2.resource_id IS NULL`)
    
    # Proposed Solution
    We add a `JOIN hr_leave_type` to be able to get the value for
    `include_public_holidays_in_duration` ("Ignore Public Holidays").
    
    Then, we make it so we exclude the Public Holidays only if that value is
    false (`AND NOT lvt.include_public_holidays_in_duration`)
    
    opw-6082422
    
    X-original-commit: a381d44baac9ec94011ce47e0f9d43e410ff69f9

---
+++ addons/hr_holidays_attendance/tests/test_leave_attendance_report.py
@@ -0,0 +1,42 @@
+from freezegun import freeze_time
+
+from odoo.tests import tagged
+
+from odoo.addons.hr_holidays.tests.common import TestHrHolidaysCommon
+
+
+@tagged('-at_install', 'post_install', 'holidays_attendance')
+class TestLeaveAttendanceReport(TestHrHolidaysCommon):
+
+    @freeze_time('2026-02-28')
+    def test_overlap_leave_and_public_holiday(self):
+        self.employee_emp.contract_date_start = "2026-02-01"
+        self.env['resource.calendar.leaves'].create({
+            'name': 'Some Public Holiday',
+            'calendar_id': self.employee_emp.resource_calendar_id.id,
+            'date_from': '2026-02-10 00:00:00',
+            'date_to': '2026-02-10 18:00:00',
+            'resource_id': False
+        })
+        work_entry_type = self.env['hr.work.entry.type'].create({
+            'code': 'LEAVE',
+            'name': 'Ignore Public Holiday Leave',
+            'count_as': 'absence',
+            'requires_allocation': False,
+            'include_public_holidays_in_duration': True,
+        })
+        leave = self.env['hr.leave'].create({
+            'name': 'Some leave',
+            'employee_id': self.employee_emp.id,
+            'work_entry_type_id': work_entry_type.id,
+            'request_date_from': "2026-02-09",
+            'request_date_to': "2026-02-11",
+        })
+        leave.action_approve()
+        non_overlap_days = (self.env["hr.leave.attendance.report"].search(
+            ['&', '|', ('date', '=', '2026-02-09'), ('date', '=', '2026-02-11'), ('employee_id', '=', self.employee_emp.id)]))
+        self.assertRecordValues(non_overlap_days, [{
+            'expected_hours': 8.0,
+            'leave_hours': 8.0,
+            'difference_hours': 0.0,
+        } for _ in range(2)])
