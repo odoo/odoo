MoLeap/odooo

[IMP] spreadsheet: sync sheet_id with URL in public readonly spreadsheet
Add support for syncing the active sheet with the URL in public
readonly spreadsheets.

On initialization, read `sheet_id` from the URL and activate the
corresponding sheet if it exists. Fallback to the first sheet when the
parameter is missing or invalid.

Update the URL when the active sheet changes, ensuring it stays in
sync with user navigation.

This allows opening a specific sheet directly via URL and preserves
the active sheet on page refresh in public readonly mode.

Task: 6068613

addons/spreadsheet/static/tests/public_spreadsheet/public_spreadsheet.test.js
@@ -1,12 +1,13 @@
-import { contains, mockService } from "@web/../tests/web_test_helpers";
+import { contains, mockService, patchWithCleanup } from "@web/../tests/web_test_helpers";
 import { defineSpreadsheetModels } from "@spreadsheet/../tests/helpers/data";
-import { expect, test } from "@odoo/hoot";
+import { beforeEach, describe, expect, test } from "@odoo/hoot";
 
 import { mountPublicSpreadsheet } from "@spreadsheet/../tests/helpers/ui";
 import { THIS_YEAR_GLOBAL_FILTER } from "@spreadsheet/../tests/helpers/global_filter";
 import { addGlobalFilter } from "@spreadsheet/../tests/helpers/commands";
 import { freezeOdooData } from "@spreadsheet/helpers/model";
 import { createModelWithDataSource } from "@spreadsheet/../tests/helpers/model";
+import { browser } from "@web/core/browser/browser";
 
 defineSpreadsheetModels();
 
@@ -23,7 +24,7 @@ test("show spreadsheet in readonly mode", async function () {
     const { model } = await createModelWithDataSource();
     await addGlobalFilter(model, THIS_YEAR_GLOBAL_FILTER);
     data = await freezeOdooData(model);
-    const fixture = await mountPublicSpreadsheet("dashboardDataUrl", "spreadsheet");
+    const { fixture } = await mountPublicSpreadsheet("dashboardDataUrl", "spreadsheet");
     const filterButton = fixture.querySelector(".o-public-spreadsheet-filter-button");
     expect(filterButton).toBe(null);
 });
@@ -32,7 +33,7 @@ test("show dashboard in dashboard mode when there are global filters", async fun
     const { model } = await createModelWithDataSource();
     await addGlobalFilter(model, THIS_YEAR_GLOBAL_FILTER);
     data = await freezeOdooData(model);
-    const fixture = await mountPublicSpreadsheet("dashboardDataUrl", "dashboard");
+    const { fixture } = await mountPublicSpreadsheet("dashboardDataUrl", "dashboard");
     const filterButton = fixture.querySelector(".o-public-spreadsheet-filter-button");
     expect(filterButton).toBeVisible();
 });
@@ -40,7 +41,7 @@ test("show dashboard in dashboard mode when there are global filters", async fun
 test("show dashboard in dashboard mode when there are no global filters", async function () {
     const { model } = await createModelWithDataSource();
     data = await freezeOdooData(model);
-    const fixture = await mountPublicSpreadsheet("dashboardDataUrl", "dashboard");
+    const { fixture } = await mountPublicSpreadsheet("dashboardDataUrl", "dashboard");
     const filterButton = fixture.querySelector(".o-public-spreadsheet-filter-button");
     expect(filterButton).toBe(null);
 });
@@ -49,7 +50,7 @@ test("click filter button can show all filters", async function () {
     const { model } = await createModelWithDataSource();
     await addGlobalFilter(model, THIS_YEAR_GLOBAL_FILTER);
     data = await freezeOdooData(model);
-    const fixture = await mountPublicSpreadsheet("dashboardDataUrl", "dashboard");
+    const { fixture } = await mountPublicSpreadsheet("dashboardDataUrl", "dashboard");
     await contains(".o-public-spreadsheet-filter-button").click();
     expect(fixture.querySelector(".o-public-spreadsheet-filters")).toBeVisible();
     expect(fixture.querySelector(".o-public-spreadsheet-filter-button")).toBe(null);
@@ -59,7 +60,7 @@ test("click close button in filter panel will close the panel", async function (
     const { model } = await createModelWithDataSource();
     await addGlobalFilter(model, THIS_YEAR_GLOBAL_FILTER);
     data = await freezeOdooData(model);
-    const fixture = await mountPublicSpreadsheet("dashboardDataUrl", "dashboard");
+    const { fixture } = await mountPublicSpreadsheet("dashboardDataUrl", "dashboard");
     await contains(".o-public-spreadsheet-filter-button").click();
     await contains(".o-public-spreadsheet-filters-close-button").click();
     expect(fixture.querySelector(".o-public-spreadsheet-filter-button")).toBeVisible();
@@ -70,7 +71,53 @@ test.tags("desktop");
 test("Hides the download button when the downloadExcelUrl is not provided", async function () {
     const { model } = await createModelWithDataSource();
     data = await freezeOdooData(model);
-    const fixture = await mountPublicSpreadsheet("dashboardDataUrl", "spreadsheet", false);
+    const { fixture } = await mountPublicSpreadsheet("dashboardDataUrl", "spreadsheet", false);
     await contains(".o-topbar-menu[data-id='file']").click();
     expect(fixture.querySelector(".o-menu-item[data-name='download_public_excel']")).toBe(null);
+});
+
+describe("sheet_id URL synchronization", () => {
+    beforeEach(async () => {
+        const { model } = await createModelWithDataSource({
+            spreadsheetData: {
+                sheets: [
+                    { id: "sheet1", name: "Sheet1" },
+                    { id: "sheet2", name: "Sheet2" },
+                ],
+            },
+        });
+        data = await freezeOdooData(model);
+    });
+
+    test("activates sheet from URL on initialization", async () => {
+        patchWithCleanup(browser.location, {
+            href: `${browser.location.href}?sheet_id=sheet2`,
+        });
+        const { comp } = await mountPublicSpreadsheet("dashboardDataUrl", "spreadsheet");
+        expect(comp.model.getters.getActiveSheetId()).toBe("sheet2");
+    });
+
+    test("falls back to the first sheet and syncs the URL when sheet_id is invalid", async () => {
+        patchWithCleanup(browser.location, {
+            href: `${browser.location.href}?sheet_id=unknown`,
+        });
+        const { comp } = await mountPublicSpreadsheet("dashboardDataUrl", "spreadsheet");
+        expect(new URL(browser.location.href).searchParams.get("sheet_id")).toBe("sheet1");
+        expect(comp.model.getters.getActiveSheetId()).toBe("sheet1");
+    });
+
+    test("falls back to the first sheet and syncs the URL when sheet_id is absent", async () => {
+        const { comp } = await mountPublicSpreadsheet("dashboardDataUrl", "spreadsheet");
+        expect(comp.model.getters.getActiveSheetId()).toBe("sheet1");
+        expect(new URL(browser.location.href).searchParams.get("sheet_id")).toBe("sheet1");
+    });
+
+    test("syncs the URL when the active sheet changes", async () => {
+        const { comp } = await mountPublicSpreadsheet("dashboardDataUrl", "spreadsheet");
+        comp.model.dispatch("ACTIVATE_SHEET", {
+            sheetIdFrom: "sheet1",
+            sheetIdTo: "sheet2",
+        });
+        expect(new URL(browser.location.href).searchParams.get("sheet_id")).toBe("sheet2");
+    });
 });

odoo/odoo

addons/spreadsheet/static/tests/public_spreadsheet/public_spreadsheet.test.js
@@ -1,12 +1,13 @@
-import { contains, mockService } from "@web/../tests/web_test_helpers";
+import { contains, mockService, patchWithCleanup } from "@web/../tests/web_test_helpers";
 import { defineSpreadsheetModels } from "@spreadsheet/../tests/helpers/data";
-import { expect, test } from "@odoo/hoot";
+import { beforeEach, describe, expect, test } from "@odoo/hoot";
 
 import { mountPublicSpreadsheet } from "@spreadsheet/../tests/helpers/ui";
 import { THIS_YEAR_GLOBAL_FILTER } from "@spreadsheet/../tests/helpers/global_filter";
 import { addGlobalFilter } from "@spreadsheet/../tests/helpers/commands";
 import { freezeOdooData } from "@spreadsheet/helpers/model";
 import { createModelWithDataSource } from "@spreadsheet/../tests/helpers/model";
+import { browser } from "@web/core/browser/browser";
 
 defineSpreadsheetModels();
 
@@ -23,7 +24,7 @@ test("show spreadsheet in readonly mode", async function () {
     const { model } = await createModelWithDataSource();
     await addGlobalFilter(model, THIS_YEAR_GLOBAL_FILTER);
     data = await freezeOdooData(model);
-    const fixture = await mountPublicSpreadsheet("dashboardDataUrl", "spreadsheet");
+    const { fixture } = await mountPublicSpreadsheet("dashboardDataUrl", "spreadsheet");
     const filterButton = fixture.querySelector(".o-public-spreadsheet-filter-button");
     expect(filterButton).toBe(null);
 });
@@ -32,7 +33,7 @@ test("show dashboard in dashboard mode when there are global filters", async fun
     const { model } = await createModelWithDataSource();
     await addGlobalFilter(model, THIS_YEAR_GLOBAL_FILTER);
     data = await freezeOdooData(model);
-    const fixture = await mountPublicSpreadsheet("dashboardDataUrl", "dashboard");
+    const { fixture } = await mountPublicSpreadsheet("dashboardDataUrl", "dashboard");
     const filterButton = fixture.querySelector(".o-public-spreadsheet-filter-button");
     expect(filterButton).toBeVisible();
 });
@@ -40,7 +41,7 @@ test("show dashboard in dashboard mode when there are global filters", async fun
 test("show dashboard in dashboard mode when there are no global filters", async function () {
     const { model } = await createModelWithDataSource();
     data = await freezeOdooData(model);
-    const fixture = await mountPublicSpreadsheet("dashboardDataUrl", "dashboard");
+    const { fixture } = await mountPublicSpreadsheet("dashboardDataUrl", "dashboard");
     const filterButton = fixture.querySelector(".o-public-spreadsheet-filter-button");
     expect(filterButton).toBe(null);
 });
@@ -49,7 +50,7 @@ test("click filter button can show all filters", async function () {
     const { model } = await createModelWithDataSource();
     await addGlobalFilter(model, THIS_YEAR_GLOBAL_FILTER);
     data = await freezeOdooData(model);
-    const fixture = await mountPublicSpreadsheet("dashboardDataUrl", "dashboard");
+    const { fixture } = await mountPublicSpreadsheet("dashboardDataUrl", "dashboard");
     await contains(".o-public-spreadsheet-filter-button").click();
     expect(fixture.querySelector(".o-public-spreadsheet-filters")).toBeVisible();
     expect(fixture.querySelector(".o-public-spreadsheet-filter-button")).toBe(null);
@@ -59,7 +60,7 @@ test("click close button in filter panel will close the panel", async function (
     const { model } = await createModelWithDataSource();
     await addGlobalFilter(model, THIS_YEAR_GLOBAL_FILTER);
     data = await freezeOdooData(model);
-    const fixture = await mountPublicSpreadsheet("dashboardDataUrl", "dashboard");
+    const { fixture } = await mountPublicSpreadsheet("dashboardDataUrl", "dashboard");
     await contains(".o-public-spreadsheet-filter-button").click();
     await contains(".o-public-spreadsheet-filters-close-button").click();
     expect(fixture.querySelector(".o-public-spreadsheet-filter-button")).toBeVisible();
@@ -70,7 +71,7 @@ test.tags("desktop");
 test("Hides the download button when the downloadExcelUrl is not provided", async function () {
     const { model } = await createModelWithDataSource();
     data = await freezeOdooData(model);
-    const fixture = await mountPublicSpreadsheet("dashboardDataUrl", "spreadsheet", false);
+    const { fixture } = await mountPublicSpreadsheet("dashboardDataUrl", "spreadsheet", false);
     await contains(".o-topbar-menu[data-id='file']").click();
     expect(fixture.querySelector(".o-menu-item[data-name='download_public_excel']")).toBe(null);
 });
@@ -80,7 +81,53 @@ test("Disable copy button in public spreadsheets", async function () {
     const { model } = await createModelWithDataSource();
     await addGlobalFilter(model, THIS_YEAR_GLOBAL_FILTER);
     data = await freezeOdooData(model);
-    const fixture = await mountPublicSpreadsheet("dashboardDataUrl", "spreadsheet");
+    const { fixture } = await mountPublicSpreadsheet("dashboardDataUrl", "spreadsheet");
     await contains(".o-topbar-menu[data-id='edit']").click();
     expect(fixture.querySelector(".o-menu-item[data-name='copy']")).toHaveClass("disabled");
+});
+
+describe("sheet_id URL synchronization", () => {
+    beforeEach(async () => {
+        const { model } = await createModelWithDataSource({
+            spreadsheetData: {
+                sheets: [
+                    { id: "sheet1", name: "Sheet1" },
+                    { id: "sheet2", name: "Sheet2" },
+                ],
+            },
+        });
+        data = await freezeOdooData(model);
+    });
+
+    test("activates sheet from URL on initialization", async () => {
+        patchWithCleanup(browser.location, {
+            href: `${browser.location.href}?sheet_id=sheet2`,
+        });
+        const { model } = await mountPublicSpreadsheet("dashboardDataUrl", "spreadsheet");
+        expect(model.getters.getActiveSheetId()).toBe("sheet2");
+    });
+
+    test("falls back to the first sheet and syncs the URL when sheet_id is invalid", async () => {
+        patchWithCleanup(browser.location, {
+            href: `${browser.location.href}?sheet_id=unknown`,
+        });
+        const { model } = await mountPublicSpreadsheet("dashboardDataUrl", "spreadsheet");
+        expect(new URL(browser.location.href).searchParams.get("sheet_id")).toBe("sheet1");
+        expect(model.getters.getActiveSheetId()).toBe("sheet1");
+    });
+
+    test("falls back to the first sheet and syncs the URL when sheet_id is absent", async () => {
+        const { model } = await mountPublicSpreadsheet("dashboardDataUrl", "spreadsheet");
+        expect(model.getters.getActiveSheetId()).toBe("sheet1");
+        expect(new URL(browser.location.href).searchParams.get("sheet_id")).toBe("sheet1");
+    });
+
+    test("syncs the URL when the active sheet changes", async () => {
+        const { model } = await mountPublicSpreadsheet("dashboardDataUrl", "spreadsheet");
+        model.dispatch("ACTIVATE_SHEET", {
+            sheetIdFrom: "sheet1",
+            sheetIdTo: "sheet2",
+        });
+        expect(new URL(browser.location.href).searchParams.get("sheet_id")).toBe("sheet2");
+    });
 });