9abdfd1070b8dcb12acd47d4e4fbdf3cb1eb7361
adch-odoo <adch@odoo.com>
2026-04-14 06:01:38 +0000

    [FIX] html_editor: prevent editor removal when deleting a button
    Steps to reproduce:
    
    - Create a button, set its URL to #, and click Apply.
    - Place the cursor right after the link.
    - Press Backspace until the button/link is removed.
    - Entire editor also gets removed.
    
    In previous version:
    
    - Issue is due to [1] change in `isEmptyBlock` (because of `isButton`).
    - When we had `<a>#[]</a>` and pressed backspace, `deleteRange` was called.
    - Then `fillShrunkBlocks` ran and `isEmptyBlock` returned true (no isButton).
    - So `<br>` was added and block never became fully empty.
    
    In current version:
    
    - Because of `isButton` condition, some empty blocks are not treated as empty.
    - So `<br>` is not added & block stays actually empty. Then `removeFEFF` runs &
    `nodeSize` becomes false &  `cleanEmptyAncestors` removes parent even editable.
    
    After this commit:
    
    - Case like `<a>[]</a>`, backspace is handled by override which directly removes
    `<a>`. Since this skips `deleteRange`, manually call `fillShrunkBlocks` there.
    - Now after removing `<a>`, block is properly detected as empty and `<br>`
    gets added.
    
    [1] - https://github.com/odoo/odoo/commit/7685e562b1036d08724ba91ed5064b6fe20c2ce2
    
    task-6109145
    
    X-original-commit: 79bfd68eb593ca88d437ac99e7d3183d7deed24b

--- addons/html_editor/static/src/core/delete_plugin.js
+++ addons/html_editor/static/src/core/delete_plugin.js
@@ -1398,6 +1398,7 @@ export class DeletePlugin extends Plugin {
             !this.isUnremovable(closestUnmergeable)
         ) {
             closestUnmergeable.remove();
+            this.fillShrunkBlocks(commonAncestor);
             this.dependencies.selection.setSelection({
                 anchorNode: destContainer,
                 anchorOffset: destOffset,
