babde269db50d4b8d40c61ebd536e7334c2c93d0
Sébastien (blse) <blse@odoo.com>
2026-04-14 16:27:06 +0200

    [FIX] html_builder, website: completely ignore popup when moving an el
    When moving a snippet with the "Move down" or "Move up" buttons in the
    overlay, if a neighboring snippet is hidden, it is skipped over. If
    there is not another snippet to move over (or if they are all skipped),
    the button is not shown.
    
    The popup is skipped because it is hidden when there is the overlay
    on one of its neighbor. But this is not always correct, when undo+redo
    refocus to a target outside the popup, the popup is hidden after
    some delay. During this delay, the plugin showing the "Move" buttons
    considers the popup as a valid neighbor to move around, and shows the
    button. If without the popup there would be no neighbors, it should
    *not* have shown the button.
    
    This commit adds a predicate resource `is_move_neighbor_predicates` to
    tell whether an element is a valid neighbor to consider to move around.
    The `move` plugin uses the resource to skip the popup for which it is
    implemented to tell that `.s_popup` is not a good neighbor.
    
    Steps to reproduce:
    - Open website builder
    - Add a section
    - Add a popup
    - Close the popup
    - Click on the section
    - Change an option on the section
    - Show the popup by clicking the eye in "Invisible Elements" panel
    - Undo
    - Bug: the overlay buttons of the section have the "Move down" arrow 
    
    task-5149984

--- addons/website/static/tests/builder/website_builder/popup_option.test.js
+++ addons/website/static/tests/builder/website_builder/popup_option.test.js
@@ -37,10 +37,11 @@ async function expectToTriggerEvent(target, type, callback) {
 }
 
 describe("Popup options: empty page before edit", () => {
+    let builder;
     // Note: for some reason, `before()` doesn't work.
     // Done in `beforeEach` because frontend JS takes too much time to load.
     beforeEach(async () => {
-        await setupWebsiteBuilder("", {
+        builder = await setupWebsiteBuilder("", {
             loadIframeBundles: true,
             loadAssetsFrontendJS: true,
         });
@@ -89,6 +90,37 @@ describe("Popup options: empty page before edit", () => {
 
         await contains("button[data-action=save]").click();
         await expect.waitForSteps(["save"]);
+    });
+    test("hidden popup are not taken into account when moving other snippets", async () => {
+        await insertCategorySnippet({ group: "intro", snippet: "s_cover" });
+        await insertCategorySnippet({ group: "content", snippet: "s_popup" });
+        await expectToTriggerEvent(":iframe .s_popup .modal", "hidden.bs.modal", () =>
+            contains(".o_we_invisible_entry [data-icon='visibility']").click()
+        );
+        await insertCategorySnippet({ group: "intro", snippet: "s_cover" });
+        await contains(":iframe .s_cover:last").click();
+        expect(".o_overlay_options button[data-icon=keyboard_arrow_up]").toHaveCount(1);
+        expect(".o_overlay_options button[data-icon=keyboard_arrow_down]").toHaveCount(0);
+        await contains(".o_overlay_options button[data-icon=keyboard_arrow_up]").click();
+        expect(".o_overlay_options button[data-icon=keyboard_arrow_up]").toHaveCount(0);
+        expect(".o_overlay_options button[data-icon=keyboard_arrow_down]").toHaveCount(1);
+    });
+    test("popup are never taken into account to show arrow to move another snippet", async () => {
+        await insertCategorySnippet({ group: "intro", snippet: "s_cover" });
+        await insertCategorySnippet({ group: "content", snippet: "s_popup" });
+        await expectToTriggerEvent(":iframe .s_popup .modal", "hidden.bs.modal", () =>
+            contains(".o_we_invisible_entry [data-icon='visibility']").click()
+        );
+        await contains(":iframe .s_cover").click();
+        await contains("button:contains(Grid)").click(); // arbitrary thing to undo
+        await expectToTriggerEvent(":iframe .s_popup .modal", "shown.bs.modal", () =>
+            contains(".o_we_invisible_entry [data-icon='visibility_off']").click()
+        );
+        await expectToTriggerEvent(":iframe .s_popup .modal", "hidden.bs.modal", () =>
+            undo(builder.getEditor())
+        );
+        expect(".o_overlay_options button[data-icon=keyboard_arrow_up]").toHaveCount(0);
+        expect(".o_overlay_options button[data-icon=keyboard_arrow_down]").toHaveCount(0);
     });
 });
 
