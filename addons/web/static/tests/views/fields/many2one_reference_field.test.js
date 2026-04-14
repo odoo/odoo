MoLeap/odooo

[FIX] web: re-render Many2OneReferenceField on model_field change
When the model_field of a Many2OneReference changes, the underlying Many2One field fails to refresh its data source. This ensures the Many2One component re-renders to fetch data from the updated model.

Steps to reproduce:
- Create a new fields.Many2oneReference linked to a model_field
- Change the value of the model_field
> The values of the Many2oneReference field are not updated in the dropdown

task-5993046

addons/web/static/tests/views/fields/many2one_reference_field.test.js
@@ -3,6 +3,7 @@ import { queryAllTexts } from "@odoo/hoot-dom";
 import { runAllTimers } from "@odoo/hoot-mock";
 
 import {
+    clickFieldDropdown,
     clickFieldDropdownItem,
     clickSave,
     contains,
@@ -27,6 +28,7 @@ class Partner extends models.Model {
     _records = [
         { id: 1, model: "partner.type", res_id: 10 },
         { id: 2, res_id: false },
+        { id: 3 },
     ];
 }
 
@@ -40,7 +42,18 @@ class PartnerType extends models.Model {
     ];
 }
 
-defineModels([Partner, PartnerType]);
+class JobTitle extends models.Model {
+    id = fields.Integer();
+    name = fields.Char();
+
+    _records = [
+        { id: 20, name: "dev" },
+        { id: 24, name: "kitchen" },
+        { id: 27, name: "ba" },
+    ];
+}
+
+defineModels([Partner, PartnerType, JobTitle]);
 
 onRpc("has_group", () => true);
 
@@ -85,7 +98,7 @@ test("Many2OneReferenceField in list view", async () => {
             </list>`,
     });
 
-    expect(queryAllTexts(".o_data_cell")).toEqual(["gold", ""]);
+    expect(queryAllTexts(".o_data_cell")).toEqual(["gold", "", ""]);
 });
 
 test("Many2OneReferenceField with no_open option", async () => {
@@ -102,6 +115,32 @@ test("Many2OneReferenceField with no_open option", async () => {
 
     expect(".o_field_widget input").toHaveValue("gold");
     expect(".o_field_widget[name=res_id] .o_external_button").toHaveCount(0);
+});
+
+test.tags("desktop");
+test("Many2OneReferenceField with model_field change in form view", async () => {
+    expect.assertions(2);
+
+     await mountView({
+        type: "form",
+        resModel: "partner",
+        resId: 3,
+        arch: `
+            <form>
+                <field name="model"/>
+                <field name="res_id"/>
+            </form>`,
+    });
+
+    await contains(".o_field_widget[name=model] input").edit("partner.type");
+    await runAllTimers();
+    await clickFieldDropdown("res_id");
+    expect(".o_field_widget[name=res_id] .o-autocomplete--dropdown-item").toHaveCount(3);
+
+    await contains(".o_field_widget[name=model] input").edit("job.title");
+    await runAllTimers();
+    await clickFieldDropdown("res_id");
+    expect(".o_field_widget[name=res_id] .o-autocomplete--dropdown-item").toHaveCount(4);
 });
 
 test.tags("desktop");

odoo/odoo

addons/web/static/tests/views/fields/many2one_reference_field.test.js
@@ -1,8 +1,9 @@
-import { expect, test } from "@odoo/hoot";
+import { animationFrame, expect, test } from "@odoo/hoot";
 import { queryAllTexts } from "@odoo/hoot-dom";
 import { runAllTimers } from "@odoo/hoot-mock";
 
 import {
+    clickFieldDropdown,
     clickFieldDropdownItem,
     clickSave,
     contains,
@@ -27,6 +28,7 @@ class Partner extends models.Model {
     _records = [
         { id: 1, model: "partner.type", res_id: 10 },
         { id: 2, res_id: false },
+        { id: 3 },
     ];
 }
 
@@ -40,7 +42,18 @@ class PartnerType extends models.Model {
     ];
 }
 
-defineModels([Partner, PartnerType]);
+class JobTitle extends models.Model {
+    id = fields.Integer();
+    name = fields.Char();
+
+    _records = [
+        { id: 20, name: "dev" },
+        { id: 24, name: "kitchen" },
+        { id: 27, name: "ba" },
+    ];
+}
+
+defineModels([Partner, PartnerType, JobTitle]);
 
 onRpc("has_group", () => true);
 
@@ -85,7 +98,7 @@ test("Many2OneReferenceField in list view", async () => {
             </list>`,
     });
 
-    expect(queryAllTexts(".o_data_cell")).toEqual(["gold", ""]);
+    expect(queryAllTexts(".o_data_cell")).toEqual(["gold", "", ""]);
 });
 
 test("Many2OneReferenceField with no_open option", async () => {
@@ -102,6 +115,30 @@ test("Many2OneReferenceField with no_open option", async () => {
 
     expect(".o_field_widget input").toHaveValue("gold");
     expect(".o_field_widget[name=res_id] .o_external_button").toHaveCount(0);
+});
+
+test.tags("desktop");
+test("Many2OneReferenceField with model_field change in form view", async () => {
+     await mountView({
+        type: "form",
+        resModel: "partner",
+        resId: 3,
+        arch: `
+            <form>
+                <field name="model"/>
+                <field name="res_id"/>
+            </form>`,
+    });
+
+    await contains(".o_field_widget[name=model] input").edit("partner.type");
+    await animationFrame();
+    await clickFieldDropdown("res_id");
+    expect(".o_field_widget[name=res_id] .o-autocomplete--dropdown-item").toHaveCount(3);
+
+    await contains(".o_field_widget[name=model] input").edit("job.title");
+    await animationFrame();
+    await clickFieldDropdown("res_id");
+    expect(".o_field_widget[name=res_id] .o-autocomplete--dropdown-item").toHaveCount(4);
 });
 
 test.tags("desktop");