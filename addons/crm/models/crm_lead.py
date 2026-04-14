745a217365806b837c82e1c9ae803e3640b16301
bhna-odoo <bhna@odoo.com>
2026-04-14 12:48:16 +0000

    [FIX] crm: prevent error when opening unassigned opportunities
    Currently, an error occurs when user tries to open unassigned opportunities
    assigned to a team.
    
    Steps to replicate:
    - Install `crm` with demo.
    - Open `CRM > Sales > Teams` and click `Sales` team.
    - Remove the `salesperson` from any lead, then return to Teams via
      breadcrumbs.
    - On the kanban card for Sales, click “Unassigned Leads”.
    
    Error:
    ```
      File '/home/odoo/src/odoo/saas-19.2/addons/crm/models/crm_team.py', line 728, in action_open_unassigned_opportunities
        context = self.env['crm.lead']._evaluate_context_from_action(action)
      File '/home/odoo/src/odoo/saas-19.2/addons/crm/models/crm_lead.py', line 727, in _evaluate_context_from_action
        return literal_eval(context_str)
      File '/home/odoo/src/odoo/saas-19.2/odoo/_monkeypatches/ast.py', line 28, in literal_eval
        return orig_literal_eval(expr)
      File 'ast.py', line 66, in literal_eval
        node_or_string = parse(node_or_string.lstrip(' \t'), mode='eval')
      File 'ast.py', line 52, in parse
        return compile(source, filename, mode, flags,
    IndentationError: unexpected indent (<unknown>, line 8)
    ```
    
    Cause:
    - Error occurs after a recent [PR].
    - As we called `literal_eval()` on the context string that we passed on to the
      `act_window` [1], it tries to parse the string using python like rules,
      the context is received as this:
    
    ```
    "{\n 'search_default_team_id': [False],
     \n'default_team_id': False,
     \n'default_type': 'opportunity',
     \n'default_user_id': 2,
     \n'show_lead_gen_button': True
     }\n       "
        ^^^^
    ```
    
    - The extra whitespace/indentation (coming from the `act_window` context
      definition) makes the string invalid for strict parsing, causing
     `literal_eval()` to fail.
    
    - Additionally, in the above given context string the `search_default_team_id`
      and `default_team_id`are  both `False`  because we called
      `_evaluate_context_from_action()` method on an empty recordset and
      when we try to [substitute] `active_id` with `self.id`(which is False because
      we dont have any record) we get another JS Error that is caused by not
      receiving any results for the search default on team.
    
    Solution:
    - Using `strip()` function removed the extra whitespaces.
    - Passed the `team_id` through context (as we cant add new parameters to
      a function in stable) and assigned it in place of `active_id`.
    
    [1]: https://github.com/odoo/odoo/blob/746ea418da2af2a6d36daea4dc544bdf3bc28495/addons/crm/views/crm_team_views.xml#L41-L48
    [PR]: https://github.com/odoo/odoo/pull/240202/changes#diff-595d3dbbabdc4f766a380a320c1c1a43b143385bc7487c7275e80f76a9fbabc2R724
    [substitute]: https://github.com/odoo/odoo/blob/e00dd21880c3c4e5c22d65567c700e02541f7259/addons/crm/models/crm_lead.py#L726
    
    sentry-7404817458
    
    closes odoo/odoo#260958
    
    X-original-commit: 38e039dea2cf2b7263cbb03ac74c0ef8653f4e63
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>
    Signed-off-by: Bhavya Ashesh Nanavati (bhna) <bhna@odoo.com>

--- addons/crm/models/crm_lead.py
+++ addons/crm/models/crm_lead.py
@@ -724,7 +724,7 @@ class CrmLead(models.Model):
     def _evaluate_context_from_action(self, action):
         context_str = action.get('context', '{}').strip()
         context_str = re.sub(r'\buid\b', str(self.env.uid), context_str)
-        context_str = re.sub(r'\bactive_id\b', str(self.id), context_str)
+        context_str = re.sub(r'\bactive_id\b', str(self.id or self.env.context.get('force_active_id')), context_str)
         return literal_eval(context_str)
 
     # ------------------------------------------------------------
