7e2a5c225add1a162bf23ef6a151820df6e3f6bd
srap-odoo <srap@odoo.com>
2026-04-14 06:58:21 +0000

    [FIX] purchase_stock, product: avoid supplier access error
    Issue before this commit:
    ------------------------------------------
    In replenishment (multi-company setup), a 'read' access error occurs while
    setting the supplier, changing the replenishment unit, or changing the min/max
    when there are suppliers across multiple companies.
    
    Steps to reproduce:
    ------------------------------------------
    1. Install purchase_stock.
    2. Create a storable product.
    3. Add one vendor for company A and one vendor for company B.
    4. Make sure that only company A is active.
    5. Create an orderpoint for company A (min/max = 0) and set the route to 'Buy'.
    6. Try to set the vendor.
       Notice that an access error occurs.
    
    Cause of the issue:
    ------------------------------------------
    This issue occurs because the `seller_ids` cache gets polluted with suppliers
    from multiple companies. As a result, when `self.seller_ids` is accessed [1],
    it returns vendors from both active and non-active companies, ignoring the
    current company context.
    [1] https://github.com/odoo/odoo/blob/47deb9f13627a26d6a3179399bd965f639fde436/addons/product/models/product_product.py#L1016
    
    Since users are not allowed to read records from non-active companies, this
    leads to a read access error when those supplier records are accessed.
    
    * Cause of the cache pollution:
    The cache gets polluted with records from another company because
    `product.seller_ids` [2] is accessed in a `sudo()` environment. This happens due
    to the `qty_to_order_computed` field, which is stored and computed. By default,
    stored computed fields use `compute_sudo=True`, causing the computation to run
    in a `sudo()` environment and cache cross-company sellers.
    [2] https://github.com/odoo/odoo/blob/d11b56f9272be39169d382007d19d63739617a8a/addons/purchase_stock/models/stock_rule.py#L169
    
    Solution:
    ------------------------------------------
    Filter the sellers in sudo mode as well, since the `seller_ids` cache will
    otherwise always be polluted due to the sudo environment.
    
    opw-6034990
    
    X-original-commit: dd089590526d6108afc4bdd73784e4c31f40c3ec

--- addons/purchase_stock/tests/test_stock_orderpoint.py
+++ addons/purchase_stock/tests/test_stock_orderpoint.py
@@ -31,6 +31,7 @@ class TestStockWarehouseOrderpoint(HttpCase):
 
         self.start_tour("/odoo/replenishment", "test_product_replenishment", login='admin')
 
+<<<<<<< b02a39d43e58118e92a794377e6a739015035d44
         self.assertEqual(len(self.product.orderpoint_ids), 1)
         self.assertEqual(self.product.orderpoint_ids[0].route_id.name, 'Buy')
 
@@ -44,3 +45,21 @@ class TestStockWarehouseOrderpoint(HttpCase):
         routes.warehouse_ids = False
         orderpoint = self.env['stock.warehouse.orderpoint'].create({'product_id': self.product.id})
         self.assertEqual(routes[0].name, orderpoint.route_id_placeholder)
+||||||| 942cbbbf243ff28f84fdaa40ed73b6572e0032a6
+        self.assertEqual(len(product.orderpoint_ids), 1)
+        self.assertEqual(product.orderpoint_ids[0].route_id.name, 'Buy')
+=======
+        self.assertEqual(len(product.orderpoint_ids), 1)
+        self.assertEqual(product.orderpoint_ids[0].route_id.name, 'Buy')
+
+    def test_replenishment_supplier_multicompany_access(self):
+        partner_a, partner_b = self.env['res.partner'].create([{'name': 'Partner A'}, {'name': 'Partner B'}])
+        company_a, company_b = self.env.company, self.env['res.company'].create({'name': 'Company B'})
+        product = self.env['product.product'].create({'name': 'Product A', 'is_storable': True})
+        self.env['product.supplierinfo'].create([
+            {'partner_id': partner.id, 'product_id': product.id, 'company_id': company.id, 'price': price}
+            for company, price, partner in [(company_a, 10.0, partner_a), (company_b, 20.0, partner_b)]
+        ])
+        self.env.user.company_ids = company_a
+        self.start_tour('/odoo/replenishment', 'test_replenishment_supplier_multicompany_access', login='admin')
+>>>>>>> 189c14f6134ce32d6e7b54d16c7f5b7c7076923f
