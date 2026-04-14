77e29198713820ebabc5262ab2807aec5babb76b
srap-odoo <srap@odoo.com>
2026-04-14 12:28:21 +0530

    [FIX] purchase_stock, product: avoid supplier access error
    Issue before this commit:
    ------------------------------------------
    In replenishment (multi-company setup), a 'read' access error occurs while
    setting the supplier, changing the replenishment unit, or changing the min/max
    when there are suppliers across multiple companies.
    
    Steps to reproduce:
    ------------------------------------------
    1. Install purchase_stock.
    2. Create a storable product.
    3. Add one vendor for company A and one vendor for company B.
    4. Make sure that only company A is active.
    5. Create an orderpoint for company A (min/max = 0) and set the route to 'Buy'.
    6. Try to set the vendor.
       Notice that an access error occurs.
    
    Cause of the issue:
    ------------------------------------------
    This issue occurs because the `seller_ids` cache gets polluted with suppliers
    from multiple companies. As a result, when `self.seller_ids` is accessed [1],
    it returns vendors from both active and non-active companies, ignoring the
    current company context.
    [1] https://github.com/odoo/odoo/blob/0fd85973e25dd8ec1601f22a4153c564f830fc0d/addons/product/models/product_product.py#L1016
    
    Since users are not allowed to read records from non-active companies, this
    leads to a read access error when those supplier records are accessed.
    
    * Cause of the cache pollution:
    The cache gets polluted with records from another company because
    `product.seller_ids` [2] is accessed in a `sudo()` environment. This happens due
    to the `qty_to_order_computed` field, which is stored and computed. By default,
    stored computed fields use `compute_sudo=True`, causing the computation to run
    in a `sudo()` environment and cache cross-company sellers.
    [2] https://github.com/odoo/odoo/blob/d11b56f9272be39169d382007d19d63739617a8a/addons/purchase_stock/models/stock_rule.py#L169
    
    Solution:
    ------------------------------------------
    Filter the sellers in sudo mode as well, since the `seller_ids` cache will
    otherwise always be polluted due to the sudo environment.
    
    opw-6034990
    
    closes odoo/odoo#259403
    
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/product/models/product_product.py
+++ addons/product/models/product_product.py
@@ -1014,7 +1014,7 @@ class ProductProduct(models.Model):
     #=== BUSINESS METHODS ===#
 
     def _prepare_sellers(self, params=False):
-        sellers = self.seller_ids._get_filtered_supplier(self.env.company, self, params)
+        sellers = self.sudo().seller_ids._get_filtered_supplier(self.env.company, self, params)
         return sellers.sorted(lambda s: (s.sequence, -s.min_qty, s.price, s.id))
 
     def _get_filtered_sellers(self, partner_id=False, quantity=0.0, date=None, uom_id=False, params=False):
