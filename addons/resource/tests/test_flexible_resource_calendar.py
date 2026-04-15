e4e2912049388ff2db00fb243610329b728d5160
Tanguy Quéguineur <taqu@odoo.com>
2026-04-15 16:05:27 +0000

    [FIX] resource: use first week day if different from locale's default
    **Steps to reproduce**
    - Install planning
    - Switch to English (UK) and change the "First day of the week" to
    Sunday in the technical settings
    - Have an employee with a flexible schedule with a total of 40h/week,
    average 8h/day
    - In the planning app, after creating a shift to display the employee
    in the gantt view, notice that when hovering over the progress bar on
    the left, 48 worked hours are expected for the current week, which is
    more than what is defined in the employee's calendar
    
    **Cause**
    The displayed week, starting on Sunday, could accumulate more hours
    than the weekly cap due to the Sunday being part of another week with
    the locale default first day (Monday).
    
    opw-6110395
    
    closes odoo/odoo#270926
    
    X-original-commit: f15bc457f16cf5a5e59a9aea32ae513f53f3a495
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>
    Signed-off-by: Tanguy Quéguineur (taqu) <taqu@odoo.com>

--- addons/resource/tests/test_flexible_resource_calendar.py
+++ addons/resource/tests/test_flexible_resource_calendar.py
@@ -149,6 +149,28 @@ class TestFlexibleResourceCalendar(TransactionCase):
                 (datetime(2025, 8, 31, 0, 0, tzinfo=UTC), datetime(2025, 8, 31, 17, 0, 0, tzinfo=UTC), self.env['resource.calendar.attendance']),
             ])
 
+    def test_flexible_resource_work_hours_first_week_day(self):
+        """
+        Check the case where the language's first day of the week is changed from
+        the language's default.
+        """
+        self.env['res.lang']._activate_lang(code='en_GB')
+        self.env['res.lang'].search([('code', '=', 'en_GB')]).week_start = "7"
+        self.flex_calendar.hours_per_week = 40
+
+        # Sunday to Saturday
+        start_dt = datetime(2026, 4, 12, 0, 0, 0).astimezone(UTC)
+        end_dt = datetime(2026, 4, 18, 23, 59, 59).astimezone(UTC)
+
+        flex_resource = self.flex_resource.with_context(lang='en_GB')
+        work_intervals, hours_per_day, hours_per_week = flex_resource._get_flexible_resource_valid_work_intervals(start_dt, end_dt)
+        hours = flex_resource._get_flexible_resource_work_hours(
+            work_intervals[self.flex_resource.id],
+            hours_per_day[self.flex_resource.id],
+            hours_per_week[self.flex_resource.id],
+        )
+        self.assertAlmostEqual(hours, 40.0, 2)
+
     def test_hours_per_week_for_different_years(self):
         start_dt = datetime(2025, 12, 26).astimezone(UTC)
         end_dt = datetime(2026, 1, 1, 17).astimezone(UTC)
