4ba7e09261a6a6783ab8ae0ee91ea8b22f6741cd
Pierre Paridans <app@odoo.com>
2026-04-15 13:36:46 +0200

    [FIX] core,mail: lxml compatibility v2.14.0+ (handling of <p>)
    In version 2.14.0, libxml2 fixed a long standing quirk in its HTML
    handling where it always implies <p> start tags [1].
    
    As a result, there is a difference in behavior between pre and post
    2.14.0 produced HTML when no start tag is provided:
    - pre: always has a <p> tag
    - post: depending on the case, could have either a <span> or <p> tag.
    
    This commit introduces a monkeypatch of the lxml's HTML parser when
    built with libxml2 2.14.0+ to maintain a similar behavior with older
    versions.
    
    It also adapts tests due to differences how malformed HTML is handled
    depending on the libxml2 version (cf. 2.14.0+ behavior is closer to
    browsers).
    
    [1]: https://gitlab.gnome.org/GNOME/libxml2/-/commit/8cf6129bbd836e666e7eda8c9e61c00387ae388b
    
    closes odoo/odoo#264273
    
    Signed-off-by: Pierre Paridans (app) <app@odoo.com>

--- odoo/_monkeypatches/lxml.py
+++ odoo/_monkeypatches/lxml.py
@@ -1,7 +1,8 @@
-import lxml.html.clean
+import lxml.html
 import re
 
 from importlib.metadata import version
+from lxml.etree import LIBXML_VERSION
 
 from odoo.tools import parse_version
 
@@ -11,3 +12,31 @@ def patch_lxml():
     # or style node removes the attribute or node erroneously
     if parse_version("4.6.0") <= parse_version(version('lxml')) < parse_version("5.2.0"):
         lxml.html.clean._find_image_dataurls = re.compile(r'data:image/(.+?);base64,').findall
+
+    # libxml2 >= 2.14.0 stopped implicitly wrapping plain text in <p> tags.
+    # We patch lxml.html parsers here to maintain compatibility across versions.
+    if LIBXML_VERSION >= (2, 14, 0):
+        RE_STARTS_WITH_TAG = r'^\s*<[\w!-]'
+        RE_STARTS_WITH_TAG_STR = re.compile(RE_STARTS_WITH_TAG)
+        RE_STARTS_WITH_TAG_BYTES = re.compile(RE_STARTS_WITH_TAG.encode('ascii'))
+
+        orig_fromstring = lxml.html.fromstring
+        orig_document_fromstring = lxml.html.document_fromstring
+
+        def _wrap_text_node(html):
+            if isinstance(html, bytes):
+                if not RE_STARTS_WITH_TAG_BYTES.match(html):
+                    return b"<p>%s</p>" % html.lstrip()
+            else:
+                if not RE_STARTS_WITH_TAG_STR.match(html):
+                    return "<p>%s</p>" % html.lstrip()
+            return html
+
+        def fromstring(html, *args, **kwargs):
+            return orig_fromstring(_wrap_text_node(html), *args, **kwargs)
+
+        def document_fromstring(html, *args, **kwargs):
+            return orig_document_fromstring(_wrap_text_node(html), *args, **kwargs)
+
+        lxml.html.fromstring = fromstring
+        lxml.html.document_fromstring = document_fromstring
