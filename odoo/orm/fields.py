5f1e415b8e265d54d05f42906075c78ddf4b05f3
Mathieu Walravens <wama@odoo.com>
2026-04-15 14:58:42 +0200

    [FIX] base: inconsistent field representation
    opw-5953417

--- odoo/orm/fields.py
+++ odoo/orm/fields.py
@@ -281,9 +281,7 @@ class Field(typing.Generic[T]):
         return "%s.%s" % (self.model_name, self.name)
 
     def __repr__(self):
-        if self.name is None:
-            return f"{'<%s.%s>'!r}" % (__name__, type(self).__name__)
-        return f"{'%s.%s'!r}" % (self.model_name, self.name)
+        return repr(str(self))
 
     def __init_subclass__(cls):
         super().__init_subclass__()
