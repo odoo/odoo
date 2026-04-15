b1a4eb22eed886e791169a52ecfe99f28f71858d
Laurent Smet (LAS) <las@odoo.com>
2026-04-15 14:10:14 +0000

    [FIX] account_edi_ubl_cii: Fix PayableRoundingAmount
    'total_excluded_currency' is for the base amount.
    'base_amount_currency' should be used only when getting the base per tax.
    When dealing with a down payment, a distortion in the taxes amounts
    might be introduced to ensure the total of the invoice is exactly the expected one.
    
    opw-6120645
    
    X-original-commit: 3f8c04931f18cc287440af6b4bbc892521d1c612

--- addons/account_edi_ubl_cii/tests/__init__.py
+++ addons/account_edi_ubl_cii/tests/__init__.py
@@ -5,6 +5,7 @@ from . import test_partner_peppol_fields
 from . import test_ubl_cii
 from . import test_download_docs
 from . import test_ubl_export_bis3_be
+from . import test_ubl_export_bis3_invoice_be_down_payment
 from . import test_ubl_import_bis3_invoice_be
 from . import test_ubl_import_bis3_invoice_be_auto_generate_pdf
 from . import test_ubl_import_bis3_invoice_be_cash_rounding
