3556bfcea8aa4771ae689a7d85a03ab10d878b93
Paul Morelle <pmo@odoo.com>
2026-04-15 10:10:12 +0000

    [FIX] base: report actually failing rules in AccessError
    When accessing an archived record directly, if access is prevented by a
    record rule other than a multi-company global rule, the error message
    incorrectly reports that all rules are failing, suggesting a company
    issue even though it is not the actual cause.
    
    The problem is that when access is denied, the diagnostic method
    `_get_failing` is used to determine which rules are failing. This method
    performs several count queries with different rule domains. However,
    `active_test` is True by default, excluding archived records from the
    count, causing the rule evaluation to miss some records and incorrectly
    mark rules as failing.
    
    With this commit, `_get_failing` evaluates rules with `active_test=False`,
    ensuring that only actually failing rules are reported.
    
    X-original-commit: 6fc5a2402619ddaa9ceb58f8c07389c4ff1647d7

--- odoo/addons/test_orm/models/test_access_rights.py
+++ odoo/addons/test_orm/models/test_access_rights.py
@@ -15,6 +15,7 @@ class Test_Access_RightSome_Obj(models.Model):
     )
     forbidden2 = fields.Integer(groups='test_orm.test_group')
     forbidden3 = fields.Integer(groups=fields.NO_ACCESS)
+    active = fields.Boolean(default=True)
 
 
 class Test_Access_RightContainer(models.Model):
