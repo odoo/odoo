142a27f35a59efc0a7eae719ee28c8636b1a9702
Hadi El Yakhni <hael@odoo.com>
2026-04-16 14:22:26 +0200

    [FIX] point_of_sale: handle default tax on cash difference account
    Steps to reproduce
    ------------------
    1. Set a default tax on the "Cash Difference Gain" account (e.g. a 25%
       sales tax) -- required in some countries like Denmark (cf 5972690).
    2. Open a PoS session, count more cash than expected at closing.
    3. Try to close the session.
    
    -> Error message shows up "The journal entry reached an invalid
    state..." ... "The journal entry must always have exactly one journal
    item involving the bank/cash account"
    
    What's happening
    ----------------
    PoS creates a bank statement line with the gain account as counterpart,
    resulting in 2 lines: cash +10, gain -10. Since the gain account has a
    default tax, `_sync_tax_lines` adds a tax line of -2.5 on top, which
    makes the move unbalanced by 2.5. Then `_sync_unbalanced_lines` adds a
    4th line to fix it, on the line returned by
    `_get_automatic_balancing_account`, which is `journal.default_account_id`,
    i.e. the cash account itself for a cash journal.
    
    So we end up with 2 lines on that same cash account, which a bank
    statement line move doesn't allow -> Error.
    
    The fix
    -------
    In `_post_statement_difference`, precompute the base and tax split
    ourselves and build the statement line's `line_ids` directly (e.g. for
    +10 and a 25% tax: cash +10, gain -8, tax -2). The move is balanced
    from creation, so `_sync_tax_lines` and `_sync_unbalanced_lines` don't
    have to touch it.
    
    Note that we force the tax computation to be in 'force_price_include'
    mode, as the counted cash difference is a gross amount (physical money
    in the drawer). This way the tax is always extracted from the cash
    amount, regardless of how the tax is configured (included or excluded
    in price). Same pattern is already used by `hr_expense` (cf
    `hr_expense.models.account_move_line._compute_totals`).
    
    opw-5972690
    
    closes odoo/odoo#257892
    
    Signed-off-by: Hadi El Yakhni (hael) <hael@odoo.com>

---
+++ addons/point_of_sale/tests/test_pos_cash_difference_tax.py
@@ -0,0 +1,61 @@
+from odoo import Command
+from odoo.tests import tagged
+
+from odoo.addons.point_of_sale.tests.common import TestPointOfSaleCommon
+
+
+@tagged('post_install', '-at_install')
+class TestPosCashDifferenceTax(TestPointOfSaleCommon):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.pos_config.cash_control = True
+        cls.pos_config.open_ui()
+        cls.session = cls.pos_config.current_session_id
+        cls.cash_journal = cls.session.cash_journal_id
+        cls.profit_account = cls.cash_journal.profit_account_id
+        cls.tax_account = cls.env['account.account'].create({
+            'name': 'Sales Tax',
+            'code': 'TTAX',
+            'account_type': 'liability_current',
+        })
+        cls.tax_included = cls._create_tax('tax_included')
+        cls.tax_excluded = cls._create_tax('tax_excluded')
+
+    @classmethod
+    def _create_tax(cls, price_include_override):
+        return cls.env['account.tax'].create({
+            'name': f'Tax 25% {price_include_override}',
+            'amount_type': 'percent',
+            'amount': 25.0,
+            'price_include_override': price_include_override,
+            'invoice_repartition_line_ids': [
+                Command.create({'repartition_type': 'base'}),
+                Command.create({'repartition_type': 'tax', 'account_id': cls.tax_account.id}),
+            ],
+        })
+
+    def test_no_tax_on_counterpart(self):
+        self.session._post_statement_difference(10)
+        st_line = self.env['account.bank.statement.line'].search(
+            [('pos_session_id', '=', self.session.id)], order='id desc', limit=1)
+        self.assertRecordValues(st_line.move_id.line_ids, [
+            {'balance': 10.0, 'account_id': self.cash_journal.default_account_id.id},
+            {'balance': -10.0, 'account_id': self.profit_account.id},
+        ])
+
+    def test_tax_on_counterpart_splits_amount(self):
+        # The tax is always treated as included, i.e. as part of the gross
+        # counted difference, whatever the price_include configuration is.
+        for tax in (self.tax_included, self.tax_excluded):
+            with self.subTest(tax=tax.name):
+                self.profit_account.tax_ids = [Command.set(tax.ids)]
+                self.session._post_statement_difference(10)
+                st_line = self.env['account.bank.statement.line'].search(
+                    [('pos_session_id', '=', self.session.id)], order='id desc', limit=1)
+                self.assertRecordValues(st_line.move_id.line_ids, [
+                    {'balance': 10.0, 'account_id': self.cash_journal.default_account_id.id},
+                    {'balance': -8.0, 'account_id': self.profit_account.id},
+                    {'balance': -2.0, 'account_id': self.tax_account.id},
+                ])
