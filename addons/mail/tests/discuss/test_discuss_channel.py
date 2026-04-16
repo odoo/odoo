stxavierhighschoolkmd-lab/Odoo-school1

[FIX] mail, *: adapt presence tests to enterprise changes *: im_livechat, test_discuss_full, website_livechat
When a user changes their manual IM status, VOIP should resend
`has_active_call` on the `presence` notification.

Add a mail test override point so enterprise can extend the common
presence assertions, then adapt the related tests to the enterprise
behaviour. Also keep `has_active_call` limited to the manual status path
and hidden when the effective status is offline.

task-5478885

addons/mail/tests/discuss/test_discuss_channel.py
@@ -215,6 +215,7 @@ class TestChannelInternals(MailCommon, HttpCase):
                         ),
                         "res.users": self._filter_users_fields(
                             {
+                                "has_active_call": False,
                                 "employee_ids": [],
                                 "id": self.test_user.id,
                                 "partner_id": self.test_partner.id,
@@ -265,6 +266,7 @@ class TestChannelInternals(MailCommon, HttpCase):
                         "res.users": self._filter_users_fields(
                             {
                                 "active": True,
+                                "has_active_call": False,
                                 "id": self.test_user.id,
                                 "employee_ids": [],
                                 "partner_id": self.test_partner.id,
@@ -511,6 +513,7 @@ class TestChannelInternals(MailCommon, HttpCase):
                         ),
                         "res.users": self._filter_users_fields(
                             {
+                                "has_active_call": False,
                                 "employee_ids": [],
                                 "id": self.test_user.id,
                                 "partner_id": self.test_partner.id,
@@ -565,6 +568,7 @@ class TestChannelInternals(MailCommon, HttpCase):
                         ),
                         "res.users": self._filter_users_fields(
                             {
+                                "has_active_call": False,
                                 "employee_ids": [],
                                 "id": self.test_user.id,
                                 "partner_id": self.test_partner.id,

addons/mail/tests/discuss/test_rtc.py
@@ -112,6 +112,7 @@ class TestChannelRTC(MailCommon, HttpCase):
                         "res.partner": self._filter_partners_fields(partner_data),
                         "res.users": self._filter_users_fields(
                             {
+                                "has_active_call": False,
                                 "id": self.user_employee.id,
                                 "employee_ids": [],
                                 "partner_id": self.partner_employee.id,
@@ -144,6 +145,7 @@ class TestChannelRTC(MailCommon, HttpCase):
                 "res.partner": self._filter_partners_fields(partner_data),
                 "res.users": self._filter_users_fields(
                     {
+                        "has_active_call": False,
                         "id": self.user_employee.id,
                         "employee_ids": [],
                         "partner_id": self.partner_employee.id,
@@ -252,6 +254,7 @@ class TestChannelRTC(MailCommon, HttpCase):
                         "res.users": self._filter_users_fields(
                             {
                                 "employee_ids": [],
+                                "has_active_call": False,
                                 "id": self.user_employee.id,
                                 "partner_id": self.partner_employee.id,
                             },
@@ -303,6 +306,7 @@ class TestChannelRTC(MailCommon, HttpCase):
                         "res.users": self._filter_users_fields(
                             {
                                 "employee_ids": [],
+                                "has_active_call": False,
                                 "id": self.user_employee.id,
                                 "partner_id": self.user_employee.partner_id.id,
                             },
@@ -354,6 +358,7 @@ class TestChannelRTC(MailCommon, HttpCase):
                         "res.users": self._filter_users_fields(
                             {
                                 "employee_ids": [],
+                                "has_active_call": False,
                                 "id": test_user.id,
                                 "partner_id": test_user.partner_id.id,
                             },
@@ -466,6 +471,7 @@ class TestChannelRTC(MailCommon, HttpCase):
                         "res.users": self._filter_users_fields(
                             {
                                 "employee_ids": [],
+                                "has_active_call": False,
                                 "id": self.user_employee.id,
                                 "partner_id": self.partner_employee.id,
                             },
@@ -517,6 +523,7 @@ class TestChannelRTC(MailCommon, HttpCase):
                         "res.users": self._filter_users_fields(
                             {
                                 "employee_ids": [],
+                                "has_active_call": False,
                                 "id": self.user_employee.id,
                                 "partner_id": self.user_employee.partner_id.id,
                             },
@@ -636,6 +643,7 @@ class TestChannelRTC(MailCommon, HttpCase):
                         "res.users": self._filter_users_fields(
                             {
                                 "employee_ids": [],
+                                "has_active_call": False,
                                 "id": test_user.id,
                                 "partner_id": test_user.partner_id.id,
                             },
@@ -722,6 +730,7 @@ class TestChannelRTC(MailCommon, HttpCase):
                         "res.users": self._filter_users_fields(
                             {
                                 "employee_ids": [],
+                                "has_active_call": False,
                                 "id": test_user.id,
                                 "partner_id": test_user.partner_id.id,
                             },
@@ -779,6 +788,7 @@ class TestChannelRTC(MailCommon, HttpCase):
                         "res.users": self._filter_users_fields(
                             {
                                 "employee_ids": [],
+                                "has_active_call": False,
                                 "id": test_user.id,
                                 "partner_id": test_user.partner_id.id,
                             },
@@ -973,6 +983,7 @@ class TestChannelRTC(MailCommon, HttpCase):
                         "res.users": self._filter_users_fields(
                             {
                                 "employee_ids": [],
+                                "has_active_call": False,
                                 "id": test_user.id,
                                 "partner_id": test_user.partner_id.id,
                             },
@@ -1162,6 +1173,7 @@ class TestChannelRTC(MailCommon, HttpCase):
                         "res.users": self._filter_users_fields(
                             {
                                 "employee_ids": [],
+                                "has_active_call": False,
                                 "id": test_user.id,
                                 "partner_id": test_user.partner_id.id,
                             },
@@ -1268,6 +1280,7 @@ class TestChannelRTC(MailCommon, HttpCase):
                             {
                                 "id": self.user_employee.id,
                                 "employee_ids": [],
+                                "has_active_call": False,
                                 "partner_id": self.partner_employee.id,
                             },
                         ),
@@ -1385,6 +1398,7 @@ class TestChannelRTC(MailCommon, HttpCase):
                         "res.users": self._filter_users_fields(
                             {
                                 "employee_ids": [],
+                                "has_active_call": False,
                                 "id": test_user.id,
                                 "partner_id": test_user.partner_id.id,
                             },

addons/test_discuss_full/tests/test_performance.py
@@ -458,12 +458,14 @@ class TestDiscussFullPerformance(HttpCase, MailCommon):
             "res.users": self._filter_users_fields(
                 {
                     "employee_ids": [],
+                    "has_active_call": False,
                     "id": self.user_root.id,
                     "partner_id": self.partner_root.id,
                     "share": False,
                     "active": False,
                 },
                 {
+                    "has_active_call": False,
                     "id": user_0.id,
                     "employee_ids": [self.employees[0].id],
                     "is_admin": False,
@@ -2005,6 +2007,7 @@ class TestDiscussFullPerformance(HttpCase, MailCommon):
                 "active": True,
                 "id": user.id,
                 "employee_ids": user.employee_ids.ids,
+                "has_active_call": False,
                 "partner_id": partner.id,
                 "share": False,
             }
@@ -2012,6 +2015,7 @@ class TestDiscussFullPerformance(HttpCase, MailCommon):
             return {
                 "id": user.id,
                 "employee_ids": user.employee_ids.ids,
+                "has_active_call": False,
                 "partner_id": partner.id,
                 "share": False,
             }
@@ -2020,12 +2024,14 @@ class TestDiscussFullPerformance(HttpCase, MailCommon):
                 return {
                     "id": user.id,
                     "employee_ids": user.employee_ids.ids,
+                    "has_active_call": False,
                     "partner_id": partner.id,
                 }
             return {
                 "active": True,
                 "id": user.id,
                 "employee_ids": user.employee_ids.ids,
+                "has_active_call": False,
                 "partner_id": partner.id,
                 "share": False,
             }
@@ -2034,6 +2040,7 @@ class TestDiscussFullPerformance(HttpCase, MailCommon):
                 "active": True,
                 "id": user.id,
                 "employee_ids": user.employee_ids.ids,
+                "has_active_call": False,
                 "partner_id": partner.id,
                 "share": False,
             }
@@ -2042,6 +2049,7 @@ class TestDiscussFullPerformance(HttpCase, MailCommon):
                 "active": True,
                 "id": user.id,
                 "employee_ids": user.employee_ids.ids,
+                "has_active_call": False,
                 "partner_id": partner.id,
                 "share": False,
             }
@@ -2050,6 +2058,7 @@ class TestDiscussFullPerformance(HttpCase, MailCommon):
                 "active": True,
                 "id": user.id,
                 "employee_ids": user.employee_ids.ids,
+                "has_active_call": False,
                 "partner_id": partner.id,
                 "share": False,
             }
@@ -2058,6 +2067,7 @@ class TestDiscussFullPerformance(HttpCase, MailCommon):
                 "active": True,
                 "id": user.id,
                 "employee_ids": user.employee_ids.ids,
+                "has_active_call": False,
                 "partner_id": partner.id,
                 "share": False,
             }

addons/website_livechat/tests/test_livechat_basic_flow.py
@@ -276,6 +276,7 @@ class TestLivechatBasicFlowHttpCase(HttpCaseWithUserDemo, TestLivechatCommon):
                 ),
                 "res.users": self._filter_users_fields(
                     {
+                        "has_active_call": False,
                         "id": self.operator.id,
                         "employee_ids": [],
                         "partner_id": self.operator.partner_id.id,

odoo/odoo

addons/mail/tests/discuss/test_discuss_channel.py
@@ -214,6 +214,7 @@ class TestChannelInternals(MailCommon, HttpCase):
                         ),
                         "res.users": self._filter_users_fields(
                             {
+                                "should_display_in_call_im_status": False,
                                 "employee_ids": [],
                                 "id": self.test_user.id,
                                 "im_status": "offline",
@@ -266,6 +267,7 @@ class TestChannelInternals(MailCommon, HttpCase):
                         "res.users": self._filter_users_fields(
                             {
                                 "active": True,
+                                "should_display_in_call_im_status": False,
                                 "id": self.test_user.id,
                                 "im_status": "offline",
                                 "im_status_access_token": self.test_user._get_im_status_access_token(),
@@ -512,6 +514,7 @@ class TestChannelInternals(MailCommon, HttpCase):
                         ),
                         "res.users": self._filter_users_fields(
                             {
+                                "should_display_in_call_im_status": False,
                                 "employee_ids": [],
                                 "id": self.test_user.id,
                                 "im_status": self.test_user.im_status,
@@ -567,6 +570,7 @@ class TestChannelInternals(MailCommon, HttpCase):
                         ),
                         "res.users": self._filter_users_fields(
                             {
+                                "should_display_in_call_im_status": False,
                                 "employee_ids": [],
                                 "id": self.test_user.id,
                                 "im_status": self.test_user.im_status,

addons/mail/tests/discuss/test_rtc.py
@@ -1236,6 +1236,7 @@ class TestChannelRTC(MailCommon, HttpCase):
     def _res_for_user(self, user, internal=False):
         res = {
             "employee_ids": [],
+            "should_display_in_call_im_status": False,
             "id": user.id,
             "partner_id": user.partner_id.id,
         }

addons/test_discuss_full/tests/test_performance.py
@@ -462,6 +462,7 @@ class TestDiscussFullPerformance(HttpCase, MailCommon):
                     "active": False,
                 },
                 {
+                    "should_display_in_call_im_status": False,
                     "id": user_0.id,
                     "im_status": "online",
                     "im_status_access_token": user_0._get_im_status_access_token(),
@@ -1993,6 +1994,7 @@ class TestDiscussFullPerformance(HttpCase, MailCommon):
                 "im_status": "online",
                 "im_status_access_token": user._get_im_status_access_token(),
                 "employee_ids": user.employee_ids.ids,
+                "should_display_in_call_im_status": False,
                 "partner_id": partner.id,
                 "share": False,
             }
@@ -2002,6 +2004,7 @@ class TestDiscussFullPerformance(HttpCase, MailCommon):
                 "im_status": "offline",
                 "im_status_access_token": user._get_im_status_access_token(),
                 "employee_ids": user.employee_ids.ids,
+                "should_display_in_call_im_status": False,
                 "partner_id": partner.id,
                 "share": False,
             }
@@ -2015,6 +2018,7 @@ class TestDiscussFullPerformance(HttpCase, MailCommon):
                     "im_status": "offline",
                     "im_status_access_token": user._get_im_status_access_token(),
                     "employee_ids": user.employee_ids.ids,
+                    "should_display_in_call_im_status": False,
                     "partner_id": partner.id,
                 }
             return {
@@ -2023,6 +2027,7 @@ class TestDiscussFullPerformance(HttpCase, MailCommon):
                 "im_status": "offline",
                 "im_status_access_token": user._get_im_status_access_token(),
                 "employee_ids": user.employee_ids.ids,
+                "should_display_in_call_im_status": False,
                 "partner_id": partner.id,
                 "share": False,
             }
@@ -2033,6 +2038,7 @@ class TestDiscussFullPerformance(HttpCase, MailCommon):
                 "im_status": "offline",
                 "im_status_access_token": user._get_im_status_access_token(),
                 "employee_ids": user.employee_ids.ids,
+                "should_display_in_call_im_status": False,
                 "partner_id": partner.id,
                 "share": False,
             }
@@ -2043,6 +2049,7 @@ class TestDiscussFullPerformance(HttpCase, MailCommon):
                 "im_status": "offline",
                 "im_status_access_token": user._get_im_status_access_token(),
                 "employee_ids": user.employee_ids.ids,
+                "should_display_in_call_im_status": False,
                 "partner_id": partner.id,
                 "share": False,
             }
@@ -2053,6 +2060,7 @@ class TestDiscussFullPerformance(HttpCase, MailCommon):
                 "im_status": "offline",
                 "im_status_access_token": user._get_im_status_access_token(),
                 "employee_ids": user.employee_ids.ids,
+                "should_display_in_call_im_status": False,
                 "partner_id": partner.id,
                 "share": False,
             }
@@ -2063,6 +2071,7 @@ class TestDiscussFullPerformance(HttpCase, MailCommon):
                 "im_status": "offline",
                 "im_status_access_token": user._get_im_status_access_token(),
                 "employee_ids": user.employee_ids.ids,
+                "should_display_in_call_im_status": False,
                 "partner_id": partner.id,
                 "share": False,
             }

addons/website_livechat/tests/test_livechat_basic_flow.py
@@ -275,6 +275,7 @@ class TestLivechatBasicFlowHttpCase(HttpCaseWithUserDemo, TestLivechatCommon):
                 ),
                 "res.users": self._filter_users_fields(
                     {
+                        "should_display_in_call_im_status": False,
                         "id": self.operator.id,
                         "im_status": "online",
                         "im_status_access_token": self.operator._get_im_status_access_token(),