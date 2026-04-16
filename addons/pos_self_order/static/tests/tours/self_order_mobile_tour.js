MoLeap/odooo

[IMP] pos_self_order: ux order note
In this commit we improve the ux for adding an order not in the
pos_self_order app.

addons/pos_self_order/static/tests/tours/self_order_mobile_tour.js
@@ -372,7 +372,7 @@ registry.category("web_tour.tours").add("self_order_mobile_0_price_order", {
             Utils.clickOrderNoteBtn(),
             Utils.clickTextArea(),
             Utils.textInput("test"),
-            Utils.clickOrderNoteBtn(),
+            Utils.clickBtn("Add"),
             Utils.clickBtn("Order"),
             ConfirmationPage.isShown(),
             Utils.clickBtn("Ok"),

odoo/odoo

addons/pos_self_order/static/tests/tours/self_order_mobile_tour.js
@@ -372,7 +372,7 @@ registry.category("web_tour.tours").add("self_order_mobile_0_price_order", {
             Utils.clickOrderNoteBtn(),
             Utils.clickTextArea(),
             Utils.textInput("test"),
-            Utils.clickOrderNoteBtn(),
+            Utils.clickBtn("Apply"),
             Utils.clickBtn("Order"),
             ConfirmationPage.isShown(),
             Utils.clickBtn("Ok"),