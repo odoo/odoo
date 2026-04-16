9f71907eb3b1c76114d4ac0b6b9681f1893787bd
Raphael Collet <rco@odoo.com>
2026-04-16 14:20:35 +0200

    [FIX] website: make website.controller.page ignore parent model accesses
    Reproduce the existing behavior of access checks on this model: public
    users actually don't have access to the parent model (ir.ui.view) but
    the absence of ir.rule makes the access domain non-falsy.
    
    Part-of: odoo/odoo#166359
    Related: odoo/enterprise#120158
    Related: odoo/upgrade#10466
    Related: odoo/upgrade-util#442
    Signed-off-by: Christophe Monniez (moc) <moc@odoo.com>

--- addons/website/models/website_controller_page.py
+++ addons/website/models/website_controller_page.py
@@ -51,6 +51,11 @@ class WebsiteControllerPage(models.Model):
         default="grid",
     )
 
+    def _access_domain(self, operation):
+        # ignore access on parent model ir.ui.view, in order to reproduce the
+        # existing behavior of ir.rules with parent models
+        return self.env['ir.access']._get_domain_for(self._name, operation, include_inherits=False)
+
     def _check_user_has_model_access(self):
         for model_id in self.mapped("model_id"):
             Model = self.env[model_id.model]
