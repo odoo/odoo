93c5410a4590a1e60c416b201ba1adcd917bd302
Jay Panchal <panj@odoo.com>
2026-04-17 14:07:56 +0000

    [FIX] html_builder, *: set data-name on generated pages/emails snippets
    * = website
    
    Issue:
    When a page is created either through the configurator or from an
    existing page template, block-level snippets do not display the correct
    title in the builder sidebar. Instead, "Block" is shown for all
    snippets.
    
    Steps to Reproduce:
    
    - Configurator:
      1. Install the website module or create a new website from Settings.
      2. Complete all configurator steps. Do not use "Skip and start from
         scratch".
    
    - Page template:
      1. Open the website and click the "New" button in the systray.
      2. Click on "Page" and choose any template other than a blank page.
    
    Observed behavior:
    The builder sidebar shows "Block" in the option container for all
    snippets instead of their actual names.
    
    Reason:
    Previously, just before the builder was opened, the `data-name`
    attribute was injected through `_computeSnippetTemplates()` for any
    snippet that did not already have it. This behavior was lost after the
    plugin refactoring.
    
    Fix:
    As before, we now inject the `data-name` attribute during builder setup
    for snippets that do not already have it.
    
    task-6087348
    
    closes odoo/odoo#269749
    
    X-original-commit: 206230b545438d8e01fd651613e224d9c33c9cfb
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Jay Panchal (panj) <panj@odoo.com>

--- addons/website/static/tests/tours/editable_root_as_custom_snippet.js
+++ addons/website/static/tests/tours/editable_root_as_custom_snippet.js
@@ -17,7 +17,7 @@ registerWebsitePreviewTour(
         ...clickOnSnippet(
             ".s_title.o_savable.custom[data-oe-model][data-oe-id][data-oe-field][data-oe-xpath]"
         ),
-        changeOption("Block", ".oe_snippet_save"),
+        changeOption("Title", ".oe_snippet_save"),
         goBackToBlocks(),
         {
             content: "Wait for the custom category to appear in the panel",
