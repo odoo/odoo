cc05c032a9aba0d4409c163f8cc5b80e3bda1378
Walid (wasa) <wasa@odoo.com>
2026-04-17 07:45:29 +0000

    [FIX] html_editor: cache selection to handle incorrect selection from
    SwiftKey keyboard
    
    Problem:
    When using the Microsoft SwiftKey keyboard, placing the caret at the
    beginning of a table cell and triggering a `beforeinput` event can
    result in `getSelection()` returning an incorrect selection.
    
    Notably, the selection immediately before the event is correct, but it
    changes unexpectedly without firing a `selection_change` event.
    
    Solution:
    Cache the selection whenever a `selection_change` event fires, ensuring
    we keep the last correct selection set by the user or editor.
    
    Steps to reproduce:
    - Edit a table with an empty cell.
    - Place the caret inside the empty cell.
    - Press Backspace.
    - Observe that the focus moves to the previous cell unexpectedly.
    
    task-6150731
    
    closes odoo/odoo#264618
    
    X-original-commit: 543895e25c51bcc31ce40926b0401d2ebba0269b
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Walid Sahli (wasa) <wasa@odoo.com>

--- addons/html_editor/static/src/core/selection_plugin.js
+++ addons/html_editor/static/src/core/selection_plugin.js
@@ -215,6 +215,8 @@ export class SelectionPlugin extends Plugin {
         "isSelectionInEditable",
         "isNodeEditable",
         "selectAroundNonEditable",
+        "getCachedSelection",
+        "setCachedSelection",
     ];
     /** @type {import("plugins").EditorResources} */
     resources = {
@@ -297,6 +299,14 @@ export class SelectionPlugin extends Plugin {
         super.destroy();
     }
 
+    getCachedSelection() {
+        return this._cachedSelection;
+    }
+
+    setCachedSelection(value) {
+        this._cachedSelection = value;
+    }
+
     selectAll() {
         const selection = this.getEditableSelection();
         const containerSelector = '#wrap > *, .oe_structure > *, [contenteditable="true"]';
@@ -341,6 +351,12 @@ export class SelectionPlugin extends Plugin {
      * Update the active selection to the current selection in the editor.
      */
     updateActiveSelection() {
+        if (this.getCachedSelection()) {
+            // `before_input_handler` may change the selection, which would
+            // invalidate the cached selection. Keep it in sync as long as
+            // the cache is active.
+            this.setCachedSelection(this.document.getSelection());
+        }
         this.previousActiveSelection = this.activeSelection;
         // getSelectionData sets this.activeSelection to the current selection
         const selectionData = this.getSelectionData();
@@ -496,7 +512,7 @@ export class SelectionPlugin extends Plugin {
      * @return { SelectionData }
      */
     getSelectionData() {
-        const selection = this.document.getSelection();
+        const selection = this.getCachedSelection() || this.document.getSelection();
         const documentSelectionIsInEditable = selection && this.isSelectionInEditable(selection);
         const documentSelection =
             selection?.anchorNode && selection?.focusNode
