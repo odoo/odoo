c03574ec3f0df241b71da1b095cdcf30a8d1e104
Aurélien Bertrand <auber@odoo.com>
2026-04-20 13:43:14 +0000

    [FIX] project: missing parameter in call
    This commit adds a missing parameters to the parent call.
    
    task-5498274
    
    closes odoo/odoo#260212
    
    X-original-commit: 708e03fc61394a03880637c3002b4a68d4af0f4b
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/project/static/src/views/project_task_kanban/project_task_kanban_model.js
+++ addons/project/static/src/views/project_task_kanban/project_task_kanban_model.js
@@ -21,7 +21,7 @@ export class ProjectTaskKanbanDynamicGroupList extends RelationalModel.DynamicGr
                 });
             });
         }
-        return super._unlinkGroups();
+        return super._unlinkGroups(groups);
     }
 }
 
