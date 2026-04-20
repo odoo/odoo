a7bf117f6e2b4ab76ca77ed4bb55900cc9f3359d
Zachary (zavan) <zavan@odoo.com>
2026-04-20 13:37:53 +0000

    [FIX] website_sale : enforce website product search
    # Setup
    Have 2 companies : A & B
    
    # How to reproduce
    - Set your website's company to Company B
    - Create product X :
    	- Company : Company A
    	- Published
    	- Name : xyz
    - Go to Users > Any User > Acces Rights > Allowed Companies => leave only Company A
    - Connect as that user on the website
    - Go to the Shop tab and search xyz
    
    # The problem
    The product X is displayed, even though we currently use the company B's website and the product is limited to company A.
    This causes problem later when Sales Order are created using that product.
    
    If you set the Allowed Companies of the user to both Company A and Company B, then the product is correctly hidden
    
    # Why
    When you search something in the search bar, the server does a `_search_with_fuzzy()` that ends up calling a simple `model.search()`.
    In our case, this search should not return product X because there is an `ir.rule` that hides product not in the current company :
    https://github.com/odoo/odoo/blob/0bb5ac6c1a87367c1ebb343ad6e6e6e56188cf13/addons/product/security/product_security.xml#L34-L38
    
    But the `website` module has some particular rule about setting the current company :
    https://github.com/odoo/odoo/blob/0bb5ac6c1a87367c1ebb343ad6e6e6e56188cf13/addons/website/models/ir_http.py#L249-L261
    
    So, in our case, since the user does not have company B in its allowed companies, then
    `allowed_company_ids` = Company A. So `('company_id', 'parent_of', company_ids)` is trucy and the product is displayed
    
    # Proposed solution
    Doing the search with `with_company` raise an AccessError because the company is not present in the allowed_companies. Chaging the allowed companies logic seems risky because it
    may lead to unintended side effects.
    
    We instead enforce the website's company in the search's domain
    
    opw-6115647
    
    closes odoo/odoo#262366
    
    X-original-commit: 9ce0bf477b4490e654dcdd7e73c7813f1f68248c
    Signed-off-by: Zachary Vanvlasselaer (zavan) <zavan@odoo.com>

--- addons/website_sale/models/website.py
+++ addons/website_sale/models/website.py
@@ -658,7 +658,8 @@ class Website(models.Model):
                 ('is_published', '=', True),
                 ('service_tracking', 'in', self.env['product.template']._get_saleable_tracking_types()),
             ]
-        return Domain.AND([self._product_domain(), website_domain, user_domain])
+        company_domain = [('company_id', 'in', [False, self.company_id.id])]
+        return Domain.AND([self._product_domain(), website_domain, user_domain, company_domain])
 
     def _product_domain(self):
         return [('sale_ok', '=', True)]
