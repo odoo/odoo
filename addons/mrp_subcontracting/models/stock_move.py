410ab16025d715157d65164be8c65c3362d6dcc8
MaximeNoirhomme <noma@odoo.com>
2026-04-20 13:34:28 +0000

    [FIX] *: Keep mo quantity when a quality check failed
    *:mrp_subcontracting{,_quality}, quality_control
    **Issue**:
    In subcontracting, if a quality check of type quantity failed, the associated mo quantity produced is reduced. This is an issue since it will, by extension, reduce the consumption of the component. Therefore, the stock of the component become incorrect.
    
    **Steps to reproduce**:
    - Create two tracked products (final and component)
    - Create a BoM for the final product using the component (1:1 ratio), with subcontracting
    - Create a quality check for that product:
    	- Control per quantity
    	- Operations type: receipts
    - Create a PO for the final product:
    	- With a quantity of 10
    	- With the associated partner be the one mentioned in the subcontracting BOM
    	- Confirm it
    - Open the associated receipt
    - Perform the quality check and fail 4 products
    - Validate the receipt by creating a backorder
    - Confirm the backorder
    - Check move history
    -> The move associated to the component will have a quantity of 6 instead of 10.
    
    **Cause**:
    While failing a quality check:
    https://github.com/odoo/enterprise/blob/8cd7fffde3c9e73fa95080a82c5750d1a74370cb/quality_mrp/models/quality.py#L56
    https://github.com/odoo/enterprise/blob/8cd7fffde3c9e73fa95080a82c5750d1a74370cb/quality_control/models/quality.py#L471
    
    The `product_uom_qty` of the move of the po will be reduced. Therefore, the subcontracting mo quantity is reduced too:
    https://github.com/odoo/odoo/blob/ca01e606928a7704c6b2e4f430710f895be2653d/addons/mrp_subcontracting/models/stock_move.py#L140-L143
    https://github.com/odoo/odoo/blob/ca01e606928a7704c6b2e4f430710f895be2653d/addons/mrp_subcontracting/models/stock_move.py#L116-L119
    
    And ultimately reduces the raw move quantity:
    https://github.com/odoo/odoo/blob/ca01e606928a7704c6b2e4f430710f895be2653d/addons/mrp/wizard/change_production_qty.py#L65
    
    **Additional note**:
    This commit also reverts the fix of c4e10e9fa90562b16d515f62910e094f77497f66 since it ensured the correct production was reduced. However, this commit ensures that production is not reduced at all, making the previous fix obsolete.
    
    opw-5427873
    
    X-original-commit: 02c9aec7e5882c6aeff2537f67194fb1ec88fbfe

--- addons/mrp_subcontracting/models/stock_move.py
+++ addons/mrp_subcontracting/models/stock_move.py
@@ -136,7 +136,7 @@ class StockMove(models.Model):
         subcontract order to the new quantity.
         """
         self._check_access_if_subcontractor(values)
-        if 'product_uom_qty' in values and self.env.context.get('cancel_backorder') is not False and not self._context.get('extra_move_mode'):
+        if 'product_uom_qty' in values and self.env.context.get('cancel_backorder') is not False and not self._context.get('extra_move_mode') and not self.env.context.get('do_not_unreserve'):
             self.filtered(
                 lambda m: m.is_subcontract and m.state not in ['draft', 'cancel', 'done']
                 and float_compare(m.product_uom_qty, values['product_uom_qty'], precision_rounding=m.product_uom.rounding) != 0
