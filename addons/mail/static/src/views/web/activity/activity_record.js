00077700d427324afa3099cfd8f274b900f83023
Aaron Bohy <aab@odoo.com>
2026-04-20 14:14:34 +0200

    [REF] web,mail: move getImageSrcFromRecordInfo
    This function is only used by the ActivityRecord, which overrides
    KanbanRecord. We thus move this function there.
    
    Part of task~5262907

--- addons/mail/static/src/views/web/activity/activity_record.js
+++ addons/mail/static/src/views/web/activity/activity_record.js
@@ -5,10 +5,47 @@ import { Component, props, types } from "@odoo/owl";
 import { evaluateBooleanExpr } from "@web/core/py_js/py";
 import { user } from "@web/core/user";
 import { isHtmlEmpty } from "@web/core/utils/html";
+import { imageUrl } from "@web/core/utils/urls";
 import { Field } from "@web/views/fields/field";
-import { getFormattedRecord, getImageSrcFromRecordInfo } from "@web/views/kanban/kanban_record";
+import { fileTypeMagicWordMap } from "@web/views/fields/image/image_field";
+import { getFormattedRecord } from "@web/views/kanban/kanban_record";
 import { useViewCompiler } from "@web/views/view_compiler";
 import { Record } from "@web/model/relational_model/record";
+
+/**
+ * Returns the image URL of a given field on the record.
+ *
+ * @param {Record} record
+ * @param {string} [model] model name
+ * @param {string} [field] field name
+ * @param {number | [number, ...any[]]} [idOrIds] id or array
+ *      starting with the id of the desired record.
+ * @param {string} [placeholder] fallback when the image does not
+ *  exist
+ * @returns {string}
+ */
+export function getImageSrcFromRecordInfo(record, model, field, idOrIds, placeholder) {
+    const id = (Array.isArray(idOrIds) ? idOrIds[0] : idOrIds) || null;
+    const isCurrentRecord =
+        record.resModel === model && (record.resId === id || (!record.resId && !id));
+    const fieldVal = record.data[field];
+    if (isCurrentRecord && fieldVal && !isBinSize(fieldVal)) {
+        // Use magic-word technique for detecting image type
+        const type = fileTypeMagicWordMap[fieldVal[0]];
+        return `data:image/${type};base64,${fieldVal}`;
+    } else if (placeholder && (!model || !field || !id || !fieldVal)) {
+        // Placeholder if either the model, field, id or value is missing or null.
+        return placeholder;
+    } else {
+        // Else: fetches the image related to the given id.
+        const unique = isCurrentRecord && record.data.write_date;
+        return imageUrl(model, id, field, { unique });
+    }
+}
+
+function isBinSize(value) {
+    return /^\d+(\.\d*)? [^0-9]+$/.test(value);
+}
 
 export class ActivityRecord extends Component {
     static components = {
