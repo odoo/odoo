c627f4ac84d9962532615ced69df1e1e471887c0
Aaron Bohy <aab@odoo.com>
2026-04-20 14:14:34 +0200

    [REF] web,mail: move getImageSrcFromRecordInfo
    This function is only used by the ActivityRecord, which overrides
    KanbanRecord. We thus move this function there.
    
    Part of task~5262907
    
    Part-of: odoo/odoo#260098
    Related: odoo/enterprise#114328
    Related: odoo/documentation#18462
    Related: odoo/upgrade#10489
    Signed-off-by: Adrien Dieudonné (adr) <adr@odoo.com>

--- addons/web/static/src/views/kanban/kanban_record.js
+++ addons/web/static/src/views/kanban/kanban_record.js
@@ -7,9 +7,7 @@ import { Dropdown } from "@web/core/dropdown/dropdown";
 import { DropdownItem } from "@web/core/dropdown/dropdown_item";
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
-import { imageUrl } from "@web/core/utils/urls";
 import { Field } from "@web/views/fields/field";
-import { fileTypeMagicWordMap } from "@web/views/fields/image/image_field";
 import { ViewButton } from "@web/views/view_button/view_button";
 import { useViewCompiler } from "@web/views/view_compiler";
 import { Widget } from "@web/views/widgets/widget";
@@ -101,41 +99,6 @@ export function getFormattedRecord(record) {
         };
     }
     return formattedRecord;
-}
-
-/**
- * Returns the image URL of a given field on the record.
- *
- * @param {Record} record
- * @param {string} [model] model name
- * @param {string} [field] field name
- * @param {number | [number, ...any[]]} [idOrIds] id or array
- *      starting with the id of the desired record.
- * @param {string} [placeholder] fallback when the image does not
- *  exist
- * @returns {string}
- */
-export function getImageSrcFromRecordInfo(record, model, field, idOrIds, placeholder) {
-    const id = (Array.isArray(idOrIds) ? idOrIds[0] : idOrIds) || null;
-    const isCurrentRecord =
-        record.resModel === model && (record.resId === id || (!record.resId && !id));
-    const fieldVal = record.data[field];
-    if (isCurrentRecord && fieldVal && !isBinSize(fieldVal)) {
-        // Use magic-word technique for detecting image type
-        const type = fileTypeMagicWordMap[fieldVal[0]];
-        return `data:image/${type};base64,${fieldVal}`;
-    } else if (placeholder && (!model || !field || !id || !fieldVal)) {
-        // Placeholder if either the model, field, id or value is missing or null.
-        return placeholder;
-    } else {
-        // Else: fetches the image related to the given id.
-        const unique = isCurrentRecord && record.data.write_date;
-        return imageUrl(model, id, field, { unique });
-    }
-}
-
-function isBinSize(value) {
-    return /^\d+(\.\d*)? [^0-9]+$/.test(value);
 }
 
 export class KanbanRecord extends Component {
