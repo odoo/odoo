ce644239dbdeea691de08658915f60688f0b26b2
Jinjiu Liu <jili@odoo.com>
2026-04-20 15:13:00 +0200

    [FIX] html_editor: revert fix of button's popover style initialization
    This reverts commit cec156cc93a95e9398d33761d9abd92c60e8bf67.
    
    After 19.0, the custom button style is introduced again. The previous
    fix causes another problem by setting the button style as custom in
    normal editor. Thus it should be fixed differently.
    
    task-6061443
    
    X-original-commit: 9ef76a4d6010191ab7ab1a0d1085972901280dda

--- addons/html_editor/static/src/main/link/link_popover.js
+++ addons/html_editor/static/src/main/link/link_popover.js
@@ -97,14 +97,6 @@ export class LinkPopover extends Component {
             linkElement.hash?.length && this.isAbsoluteURLInCurrentDomain(linkElement.href)
                 ? "_self"
                 : "_blank";
-        const hasButtonSizeOrShapeOrCustom = this.props.linkElement.className.match(
-            /btn-(lg|sm|xs|custom)|rounded-circle|(?:^|\\s)(outline|fill|flat)(?:\\s|$)/
-        );
-        const buttonType = hasButtonSizeOrShapeOrCustom
-            ? "custom"
-            : this.props.linkElement.className
-                  .match(/btn(-[a-z0-9_-]*)(primary|secondary)/)
-                  ?.pop() || "link";
         this.state = useState({
             editing: this.props.LinkPopoverState.editing,
             // `.getAttribute("href")` instead of `.href` to keep relative url
@@ -119,7 +111,10 @@ export class LinkPopover extends Component {
             urlDescription: "",
             linkPreviewName: "",
             imgSrc: "",
-            type: this.props.type || buttonType,
+            type:
+                this.props.type ||
+                linkElement.className.match(/btn(-[a-z0-9_-]*)(primary|secondary|custom)/)?.pop() ||
+                "link",
             linkTarget: linkElement.target === "_blank" ? "_blank" : "",
             directDownload: true,
             isDocument: false,
