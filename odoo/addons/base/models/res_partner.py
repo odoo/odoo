b20b9e88f0b3e413da0b8d8444e897311428dd79
guva-odoo <guva@odoo.com>
2026-04-20 08:17:37 +0000

    [REV] l10n_de, base: fix display name for child contact
    This reverts commit 0ef4c1d06fdf999ad5cdad696069aec8f2f943c5.
    
    opw-5900567
    
    X-original-commit: 0c766bf596242cb2ab00a7637c80edf164cfa382

--- odoo/addons/base/models/res_partner.py
+++ odoo/addons/base/models/res_partner.py
@@ -1007,7 +1007,7 @@ class ResPartner(models.Model):
 
     @api.depends('complete_name', 'email', 'vat', 'state_id', 'country_id')
     @api.depends_context(
-        'show_address', 'partner_display_name_hide_company', 'partner_show_db_id',
+        'show_address', 'partner_show_db_id',
         'show_email', 'show_vat', 'lang', 'formatted_display_name'
     )
     def _compute_display_name(self):
