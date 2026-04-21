dfea624d0a7ace870f6cc1b284da8b44afbe32b3
Tanguy Quéguineur <taqu@odoo.com>
2026-04-21 14:48:22 +0200

    [FIX] hr_homeworking: allow user to edit own location
    **Steps to reproduce**
    - Have a user without HR rights and linked to an employee
    - With this user, open Preferences and in the calendar tab and try
    to change the work location for one of the days
    - Error: You do not have enough rights to access the field "version_id"
    on Employee (hr.employee).
    
    **Cause**
    Issue after 72ac4b03657d617644ae75f2957aaec7acf6c1a8 which removed
    SELF_READABLE_FIELDS and SELF_WRITEABLE_FIELDS.
    
    **Change**
    Use the `field_employee` function introduced in 9605045313953b4c8c734c0d52e8032e3c36bf3a
    (commit message contains the explanation as to why it is necessary for
    fields coming from the employee model).
    
    opw-6127522

--- addons/hr_homeworking/models/res_users.py
+++ addons/hr_homeworking/models/res_users.py
@@ -1,5 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from odoo import models, fields
+from odoo.addons.hr.models.res_users import field_employee
 
 from .hr_homeworking import DAYS
 
@@ -7,13 +8,13 @@ from .hr_homeworking import DAYS
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    monday_location_id = fields.Many2one("hr.work.location", related="employee_id.monday_location_id", readonly=False, string='Mondays', user_writeable=True)
-    tuesday_location_id = fields.Many2one("hr.work.location", related="employee_id.tuesday_location_id", readonly=False, string='Tuesdays', user_writeable=True)
-    wednesday_location_id = fields.Many2one("hr.work.location", related="employee_id.wednesday_location_id", readonly=False, string='Wednesdays', user_writeable=True)
-    thursday_location_id = fields.Many2one("hr.work.location", related="employee_id.thursday_location_id", readonly=False, string='Thursdays', user_writeable=True)
-    friday_location_id = fields.Many2one("hr.work.location", related="employee_id.friday_location_id", readonly=False, string='Fridays', user_writeable=True)
-    saturday_location_id = fields.Many2one("hr.work.location", related="employee_id.saturday_location_id", readonly=False, string='Saturdays', user_writeable=True)
-    sunday_location_id = fields.Many2one("hr.work.location", related="employee_id.sunday_location_id", readonly=False, string='Sundays', user_writeable=True)
+    monday_location_id = field_employee(fields.Many2one, 'monday_location_id', comodel_name='hr.work.location', string='Mondays', user_writeable=True)
+    tuesday_location_id = field_employee(fields.Many2one, 'tuesday_location_id', comodel_name='hr.work.location', string='Tuesdays', user_writeable=True)
+    wednesday_location_id = field_employee(fields.Many2one, 'wednesday_location_id', comodel_name='hr.work.location', string='Wednesdays', user_writeable=True)
+    thursday_location_id = field_employee(fields.Many2one, 'thursday_location_id', comodel_name='hr.work.location', string='Thursdays', user_writeable=True)
+    friday_location_id = field_employee(fields.Many2one, 'friday_location_id', comodel_name='hr.work.location', string='Fridays', user_writeable=True)
+    saturday_location_id = field_employee(fields.Many2one, 'saturday_location_id', comodel_name='hr.work.location', string='Saturdays', user_writeable=True)
+    sunday_location_id = field_employee(fields.Many2one, 'sunday_location_id', comodel_name='hr.work.location', string='Sundays', user_writeable=True)
 
     def _get_employee_fields_to_sync(self):
         return super()._get_employee_fields_to_sync() + DAYS
