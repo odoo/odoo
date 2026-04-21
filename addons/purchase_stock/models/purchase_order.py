6082719513fb7aaaf86c2eb060822adc86d3ef82
Zachary (zavan) <zavan@odoo.com>
2026-04-21 12:50:49 +0000

    [FIX] purchase_stock : align dashboard OTD with patner's
    # Setup
    For easiness of testing : have no purchase order in your dashboard
    
    # How to reproduce
    - Create a Purchase Order for Vendor X and with Product Y
    - Click on Confirm Order -> Receive -> Validate
    - Go back to the dashboard
    
    # The problem
    The displayed OTD is 0%, but when you go to the Vendor X form view and check his On-time Rate, it is 100%
    
    # Cause
    The computation for the On-time rate in the dashboard uses the whole datetime value, so if there is even a second of difference between `effective_date` and `date_planned`,
    the PO is not counted as on-time :
    https://github.com/odoo/odoo/blob/942cbbbf243ff28f84fdaa40ed73b6572e0032a6/addons/purchase_stock/models/purchase_order.py#L253
    
    That is not the case for the partner On-time rate computation, where we round the datetime value to the date value :
    https://github.com/odoo/odoo/blob/942cbbbf243ff28f84fdaa40ed73b6572e0032a6/addons/purchase_stock/models/res_partner.py#L57
    
    opw-6128510
    
    X-original-commit: 50ec37c40a7a2dcc6ceb53ad47799a88fd3f833b

--- addons/purchase_stock/models/purchase_order.py
+++ addons/purchase_stock/models/purchase_order.py
@@ -274,7 +274,7 @@ class PurchaseOrder(models.Model):
         for po in purchases:
             if po.user_id == self.env.user:
                 my_purchase_count += 1
-            if not po.effective_date or po.effective_date > po.date_promised:
+            if not po.effective_date or po.effective_date.date() > po.date_promised.date():
                 continue
             otd_purchase_count += 1
             if po.user_id == self.env.user:
