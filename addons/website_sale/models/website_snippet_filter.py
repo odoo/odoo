47a4de30e2ab623742aa20193eea1666cf750d80
Zachary (zavan) <zavan@odoo.com>
2026-04-21 10:49:23 +0000

    [FIX] website,website_sale: correct product carousel when a lot of variants
    # How to reproduce
    - Create a Product with more than 256 variants
    - Publish it to the website
    - Go to the website and add a Product Carousel
    - Set it to display newest products & hide variants
    
    # The problem
    Fewer than 16 products are displayed
    
    # Cause
    This issue was already adressed by this commit :
    https://github.com/odoo/odoo/pull/195857
    
    But because of limitations for specifically the "Newest Products" filter
    (cf. original commit message), the fix was only a workaround. Indeed, it
    increased the search limit to 256 before filtering out the variants,
    which caused problems when there was more than 256 variants.
    
    But since then, big changes made in 19.0 has allowed us to implement a
    better fix :
    https://github.com/odoo/odoo/commit/e3b062e5d3820
    
    # Proposed Solution
    Since we can now pass the model in the options :
    https://github.com/odoo/odoo/blob/b5069328734e623c12b0de50a177d501f7f0c995/addons/website/models/website_snippet_filter.py#L90
    
    We give "product.template" when the `hide_variants` option is enabled
    and we filter on products. This allows use to remove the whole
    workaround that needed increase the limit on product searchs then
    getting their templates.
    
    A recent commit blocks passing "product.template" since it does not have
    a dedicated snippet filter :
    https://github.com/odoo/odoo/pull/257208/changes/8e264ae4ea6ae16bbd410ac384733ff8759938a6
    
    We move part of this logic in a new helper function and override it in
    `website_sale` to specifically allow 'product.template'
    
    opw-6054059
    
    X-original-commit: 3bf17962a12581c0b96ece61428cb749c7780569

--- addons/website_sale/models/website_snippet_filter.py
+++ addons/website_sale/models/website_snippet_filter.py
@@ -1,7 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from functools import partial
-
 from odoo import _, api, fields, models
 from odoo.fields import Domain
 from odoo.http import request
@@ -27,22 +25,16 @@ class WebsiteSnippetFilter(models.Model):
         if search_domain and 'hide_variants' in search_domain:
             hide_variants = True
             search_domain.remove('hide_variants')
-        update_limit_cache = False
-        product_limit = limit or self.limit
         if hide_variants and self.filter_id.model_id == 'product.product':
-            # When hiding variants, temporarily update cache to increase `self.limit`
-            # so we hopefully end up with the correct amount of product templates
-            update_limit_cache = partial(self._fields['limit']._update_cache, self)
-            limit = product_limit ** 2  # heuristic, may still be inadequate in some cases
-            stored_limit = self.limit
-            update_limit_cache(limit)
+            kwargs['res_model'] = "product.template"
         res = super(
             WebsiteSnippetFilter,
-            self.with_context(hide_variants=hide_variants, product_limit=product_limit),
+            self.with_context(hide_variants=hide_variants),
         )._prepare_values(limit=limit, search_domain=search_domain, **kwargs)
-        if update_limit_cache:
-            update_limit_cache(stored_limit)
         return res
+
+    def _get_website_filter_models(self):
+        return super()._get_website_filter_models() + ['product.template']
 
     @api.model
     def _get_website_currency(self):
@@ -107,9 +99,6 @@ class WebsiteSnippetFilter(models.Model):
 
     def _filter_records_to_values(self, records, **options):
         hide_variants = self.env.context.get('hide_variants') and not isinstance(records, list)
-        if hide_variants:
-            product_limit = self.env.context.get('product_limit') or self.limit
-            records = records.product_tmpl_id[:product_limit]
         res_products = super()._filter_records_to_values(records, **options)
         if (self.model_name or options.get('res_model')) == 'product.product':
             for res_product in res_products:
