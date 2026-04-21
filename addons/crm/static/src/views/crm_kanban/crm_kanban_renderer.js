a5c1c13406548d5c2eff8741d38b885fa061faa5
Aaron Bohy <aab@odoo.com>
2026-04-21 14:45:56 +0200

    [REF] crm,mail,project: remove unnnecessary RottingKanbanRecord
    The RottingKanbanRecord is an override of KanbanRecord which doesn't
    actually override any function. It can then be safely removed.
    
    Part of task~5262907

--- addons/crm/static/src/views/crm_kanban/crm_kanban_renderer.js
+++ addons/crm/static/src/views/crm_kanban/crm_kanban_renderer.js
@@ -1,5 +1,4 @@
 import { CrmColumnProgress } from "./crm_column_progress";
-import { RottingKanbanRecord } from "@mail/js/rotting_mixin/rotting_kanban_record";
 import { RottingKanbanHeader } from "@mail/js/rotting_mixin/rotting_kanban_header";
 import { RottingKanbanRenderer } from "@mail/js/rotting_mixin/rotting_kanban_renderer";
 
@@ -14,6 +13,5 @@ export class CrmKanbanRenderer extends RottingKanbanRenderer {
     static components = {
         ...RottingKanbanRenderer.components,
         KanbanHeader: CrmKanbanHeader,
-        KanbanRecord: RottingKanbanRecord,
     };
 }
