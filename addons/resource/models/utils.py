0d8dfc2c747a378474302260b7e3b38ee72887cf
Abderraouf Ghrissi (abgh) <abgh@odoo.com>
2026-04-21 09:01:29 +0200

    [IMP] resource: add 2 functions in utils to use it in service apps
    This commit adds 2 functions in utils file in resource module to be able
    to use it in project and planning apps.
    
    task-5139517

--- addons/resource/models/utils.py
+++ addons/resource/models/utils.py
@@ -80,3 +80,53 @@ def extract_comodel_domain(
     if rest:
         return extract_comodel_domain(comodel, domain, rest)
     return domain.optimize_full(comodel)
+
+
+def get_color_from_code(color, is_open_shift):
+    """Take a color code from Odoo's Kanban view and returns an hex code compatible with the fullcalendar library"""
+    # if the shift is an open shift, we use the '80' affix at the end of the hex code to modify the transparency
+    if is_open_shift:
+        switch_color = {
+            0: '#00878480',   # No color (doesn't work actually...)
+            1: '#EE4B3980',   # Red
+            2: '#F2964880',   # Orange
+            3: '#F4C60980',   # Yellow
+            4: '#55B7EA80',   # Light blue
+            5: '#71405B80',   # Dark purple
+            6: '#E8686980',   # Salmon pink
+            7: '#00878480',   # Medium blue
+            8: '#26728380',   # Dark blue
+            9: '#BF125580',   # Fushia
+            10: '#2BAF7380',  # Green
+            11: '#8754B080',  # Purple
+        }
+    else:
+        switch_color = {
+            0: '#008784',   # No color (doesn't work actually...)
+            1: '#EE4B39',   # Red
+            2: '#F29648',   # Orange
+            3: '#F4C609',   # Yellow
+            4: '#55B7EA',   # Light blue
+            5: '#71405B',   # Dark purple
+            6: '#E86869',   # Salmon pink
+            7: '#008784',   # Medium blue
+            8: '#267283',   # Dark blue
+            9: '#BF1255',   # Fushia
+            10: '#2BAF73',  # Green
+            11: '#8754B0',  # Purple
+        }
+    return switch_color[color]
+
+
+def get_light_color(color, factor, is_open_shift):
+    factor = max(0.0, min(factor, 1.0))
+    color = get_color_from_code(color, is_open_shift)
+
+    r = int(color[1:3], 16)
+    g = int(color[3:5], 16)
+    b = int(color[5:7], 16)
+
+    r = round(r + (255 - r) * factor)
+    g = round(g + (255 - g) * factor)
+    b = round(b + (255 - b) * factor)
+    return f"#{r:02x}{g:02x}{b:02x}"
