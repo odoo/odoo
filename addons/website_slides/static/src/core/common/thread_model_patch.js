78a80cc13022000b5d76ee835e03df6c34ea050a
Maryam Kia <maki@odoo.com>
2026-04-21 11:17:03 +0000

    [FIX] website_slides: comments for non fullscreen slide view
    Steps to reproduce:
    - Open a slide of a course in non fullscreen mode (website).
    - Go to the comments tab and add a comment in the chatter.
    - The comments count does not change in the tab.
    - Another way to see the incorrect counter is to add a note in the slide form
    view (backend).
    
    Before this change, `website_slides` used `website_message_ids` to calculate the
     comments. Since #138233 the old portal chatter has been replaced with the mail
    chatter and the way messages are displayed on the portal has changed. For
    example notes are no longer considered portal messages and also deleted messages
     should not be displayed or counted as such.
    
    This change ensures that comments calculations are based on a domain that
    considers those changes meaning that comments will be synced with the
    actual number of available comments.
    
    closes odoo/odoo#271347
    
    X-original-commit: 80944e9e9f0044a9860ed33d5c907a4e712522a0
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>
    Signed-off-by: Maryam Kia (maki) <maki@odoo.com>

---
+++ addons/website_slides/static/src/core/common/thread_model_patch.js
@@ -0,0 +1,11 @@
+import { Thread } from "@mail/core/common/thread_model";
+
+import { patch } from "@web/core/utils/patch";
+
+patch(Thread.prototype, {
+    setup() {
+        super.setup();
+        /** @type {number|undefined} */
+        this.comments_count;
+    },
+});
