fda16eb1c7695dc07d9778f27b87b5a3816dd83f
roto-odoo <roto@odoo.com>
2026-04-21 16:06:56 +0200

    [FIX] account_fleet,hr_expense: vehicle sent in XML
    when an invoice line has a vehicle linked, the vehicle license plate
    will be in the export XML file only if the enterprise module
    `account_accountant_fleet` is installed.
    Any community db will then not have the ref included
    This commit moves the vehicle data in `account_fleet` to expose it
    to community dbs
    
    Also fix a bug when expense is installed: super was not called in a
    method about tax lines, causing side effects (splitting repartition
    lines when it shouldn't)
    
    runbot-242562
    
    closes odoo/odoo#261066
    
    Related: odoo/enterprise#114902
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/account_fleet/models/account_move.py
+++ addons/account_fleet/models/account_move.py
@@ -1,6 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models, _
+from odoo.tools import SQL
 
 
 class AccountMove(models.Model):
@@ -61,6 +62,17 @@ class AccountMoveLine(models.Model):
     def _compute_need_vehicle(self):
         for line in self:
             line.need_vehicle = line.account_id.is_vehicle_account
+
+    @api.model
+    def _get_extra_query_base_tax_line_mapping(self) -> SQL:
+        """Override to add vehicle_id matching condition for tax details query.
+        This ensures that tax lines are matched with base lines having the same vehicle_id when
+        both are set, while allowing the match when either side has no vehicle_id. This avoids
+        inconsistencies when a single tax line is shared across base lines with mixed vehicle
+        assignments (one set, one NULL).
+        """
+        query = super()._get_extra_query_base_tax_line_mapping()
+        return SQL("%s AND (base_line.vehicle_id = account_move_line.vehicle_id OR account_move_line.vehicle_id IS NULL)", query)
 
     def _prepare_fleet_log_service(self):
         vendor_bill_service = self.env.ref('account_fleet.data_fleet_service_type_vendor_bill', raise_if_not_found=False)
