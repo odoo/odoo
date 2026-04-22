668256606eda58f715f05af1a9c166282d37ae33
Hesham Saleh <hsal@odoo.com>
2026-04-22 13:59:52 +0200

    [FIX] account: Don't hide taxes that replace themselves
    If a tax replaces itself, it's not redundant and must appear on account moves.
    This commit solves this issue by including self-replacing taxes in the name_search.
    
    task-6147767

--- addons/account/models/account_tax.py
+++ addons/account/models/account_tax.py
@@ -262,7 +262,16 @@ class AccountTax(models.Model):
         if fp_id := self.env.context.get('dynamic_fiscal_position_id'):
             domain &= Domain('fiscal_position_ids', 'in', [False, int(fp_id)])
         if self.env.context.get('hide_original_tax_ids') and fp_id:
-            domain &= Domain('replacing_tax_ids', 'not any', domain)
+            domain &= Domain('replacing_tax_ids', 'not any', domain) | Domain.custom(
+                to_sql=lambda model, alias, query: SQL(
+                    "EXISTS (SELECT 1 FROM %s WHERE %s = %s AND %s = %s)",
+                    SQL.identifier('account_tax_alternatives'),
+                    SQL.identifier('src_tax_id'),
+                    SQL.identifier(alias, 'id'),
+                    SQL.identifier('dest_tax_id'),
+                    SQL.identifier(alias, 'id'),
+                ),
+            )
         return super().name_search(name, domain, operator, limit)
 
     @api.depends('company_id')
