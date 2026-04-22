718f8c81e1a0cda361788e37df448fed35910bdc
Earth Patel <eapa@odoo.com>
2026-04-22 06:24:59 +0000

    [FIX] account: fix email lookup when retrieving customers
    The email-based lookup was mistakenly checking the phone field (`phone = email`)
    instead of the email field. Because of this, customers could not be correctly
    found using their email address.
    
    This change fixes the domain to properly match on the email field.
    
    closes odoo/odoo#261921
    
    X-original-commit: 59d6232979b8499fde6cb700df1870e2e38d0d3e
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>

--- addons/account/models/partner.py
+++ addons/account/models/partner.py
@@ -1017,7 +1017,7 @@ class ResPartner(models.Model):
 
         return {
             'criteria': [{
-                'domain': [('phone', '=', email)],
+                'domain': [('email', '=', email)],
             }],
         }
 
