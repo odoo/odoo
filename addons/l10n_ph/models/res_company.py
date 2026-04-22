c997db7485e9896717d1991b51d7c35d5db4649f
Nicolas Viseur (vin) <vin@odoo.com>
2026-04-22 14:06:51 +0800

    [IMP] l10n_ph*: clean partner fields
    Clean up the fields stored on the res.partner model when using a Philippine company.
    The first, middle and last name fields are now computed fields that which are automatically
    populated based on the partner's name.
    
    Examples:
        - "Jose Rizal" -> First: "Jose", Middle: "", Last: "Rizal"
        - "Juan Miguel dela Cruz Jr." -> First: "Juan", Middle: "Miguel", Last: "dela Cruz"
        - "Maria Anna de los Reyes Santos" -> First: "Maria Anna", Middle: "de los Reyes", Last: "Santos"
        - "Jose Maria Santos Reyes III" -> First: "Jose Maria III", Middle: "Santos", Last: "Reyes"
    
    We also move the fields right below the name field itself to keep them grouped
    and reduce the vertical space taken by these.
    
    The branch code is now left empty by default, with fallbacks to '000' when
    needed to generate a report. It is still automatically computed from the VAT
    number if possible.
    all of these fields' technical name is updated in order to better follow our
    localization standards.
    
    task-6146199

--- addons/l10n_ph/models/res_company.py
+++ addons/l10n_ph/models/res_company.py
@@ -5,5 +5,5 @@ from odoo import fields, models
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    branch_code = fields.Char(string='Company Branch Code', related='partner_id.branch_code')
+    l10n_ph_branch_code = fields.Char(string='Company Branch Code', related='partner_id.l10n_ph_branch_code')
     l10n_ph_rdo = fields.Char("RDO", help="Revenue District Office")
