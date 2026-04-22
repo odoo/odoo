9f329b9cb5de756d8baa1fb2dbb831b08023d19a
dvsh-odoo <dvsh@odoo.com>
2026-04-22 18:51:15 +0530

    [FIX] account: handle empty pdf content properly
    * When determining the file type (_get_import_file_type) of an attachment,
      some localizations rely on file_data['raw'] to infer the file type.
    * For empty attachments, file_data['raw'] is falsy, leading to errors when
      used in the detection logic.
    * This fix ensures a proper fallback to the binary content when the raw data
      is empty.
    
    runbot error: https://runbot.odoo.com/runbot/build/108173490,
    https://runbot.odoo.com/runbot/build/108172946
    
    Related PR: https://github.com/odoo/odoo/pull/252208

--- addons/account/models/account_document_import_mixin.py
+++ addons/account/models/account_document_import_mixin.py
@@ -443,7 +443,7 @@ class AccountDocumentImportMixin(models.AbstractModel):
         for attachment in attachments:
             file_data = {
                 'name': attachment.name,
-                'raw': attachment.raw,
+                'raw': attachment.raw or b'',
                 'mimetype': attachment.mimetype,
                 'origin_attachment': attachment,
                 'attachment': attachment,
