0a23500d5875e6b27721ec111c440e6aab5000f0
pssh-odoo <pssh@odoo.com>
2026-04-22 11:55:48 +0530

    [REF] account: bank reco widget, technical clean up
    This commit cleans up the bank reconciliation widget.
    
    Implementation details:
    - Avoid redundant `_seek_for_lines` calls in `_compute_is_reconciled` by passing
      resolved lines into `_get_statement_line_residual_amounts` and
      `_get_accounting_amounts_and_currencies` as optional parameters.
    - Changed the return type of `_get_accounting_amounts_and_currencies` from a
      tuple to a dictionary.
    - Replaced `self.filtered(lambda..)` in `_compute_running_balance` with
      `self.grouped('journal_id')`, as the former caused repeated iterations. Also
      replaced `pending_items` recordset subtraction with a set of IDs.
    - Avoid unnecessary filtering in `_compute_is_valid` by building a set of
      invalid IDs.
    - Consolidated the two equivalent branches in `_synchronize_from_moves` into a
      single `journal_currency or company_currency` check.
    - Replace raw ORM command tuples with `Command.update/create/delete` in
      `_synchronize_to_moves`.
    - Add `default=0` to the `amount` field, removing the guard in `create()`.
    - Remove redundant label element for `statement_line_id` from `account_move`
      form view.
    
    Related ENT PR: https://github.com/odoo/enterprise/pull/119505
    task-6030613

--- addons/account/models/account_bank_statement.py
+++ addons/account/models/account_bank_statement.py
@@ -201,9 +201,9 @@ class AccountBankStatement(models.Model):
         if len(self) == 1:
             self.is_valid = self._get_statement_validity()
         else:
-            invalids = self.filtered(lambda s: s.id in self._get_invalid_statement_ids())
-            invalids.is_valid = False
-            (self - invalids).is_valid = True
+            invalid_ids = set(self._get_invalid_statement_ids())
+            for stmt in self:
+                stmt.is_valid = stmt.id not in invalid_ids
 
     @api.depends('is_valid', 'is_complete')
     def _compute_problem_description(self):
