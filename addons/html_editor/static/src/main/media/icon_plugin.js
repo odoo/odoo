f4a2ef10a4b3d9c1719b3754a7c02485c01053a5
Jinjiu Liu <jili@odoo.com>
2026-04-22 13:51:16 +0200

    [FIX] html_editor: add background color indicator for icons
    Before this commit: the background color isn't properly indicated in the
    toolbar.
    
    After this commit: we create a provider for icon's background color, and
    use the shared function getElementColors to calculate the background
    color.
    
    Also merged duplicated imports
    
    task-5976046

--- addons/html_editor/static/src/main/media/icon_plugin.js
+++ addons/html_editor/static/src/main/media/icon_plugin.js
@@ -16,7 +16,7 @@ import { closestElement } from "@html_editor/utils/dom_traversal";
 
 export class IconPlugin extends Plugin {
     static id = "icon";
-    static dependencies = ["history", "selection", "dialog"];
+    static dependencies = ["history", "selection", "color", "dialog"];
     toolbarNamespace = "icon";
     /** @type {import("plugins").EditorResources} */
     resources = {
@@ -139,6 +139,11 @@ export class IconPlugin extends Plugin {
                 return true;
             }
         },
+        /** Providers */
+        selected_background_color_providers: withSequence(
+            5,
+            this.computeBackgroundColorForIcon.bind(this)
+        ),
     };
 
     onClickIcon(ev) {
@@ -224,5 +229,19 @@ export class IconPlugin extends Plugin {
             prevIcon.setAttribute(attribute.nodeName, attribute.nodeValue);
         }
         this.dependencies.history.commit();
+    }
+
+    computeBackgroundColorForIcon() {
+        const nodes = this.dependencies.selection
+            .getTargetedNodes()
+            .filter((node) => node.classList?.contains("fa"));
+        if (nodes.length === 0) {
+            return;
+        }
+        const el = closestElement(nodes[0], "font");
+        if (!el) {
+            return;
+        }
+        return this.dependencies.color.getElementColors(el).backgroundColor;
     }
 }
