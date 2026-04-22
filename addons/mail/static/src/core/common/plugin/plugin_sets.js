68ff89e58eced5dd249c55cf78ba7d73ae489ca1
Zelong Lin <zel@odoo.com>
2026-04-22 12:41:43 +0200

    [IMP] mail,*: add code block support in Discuss composer
    splitting the code block plugin from the font plugin, and make it
    available in mail composer.
    
    use the syntax highlighting embedded component to render code blocks in
    readonly mode in the message bodies.
    
    task-5262721

--- addons/mail/static/src/core/common/plugin/plugin_sets.js
+++ addons/mail/static/src/core/common/plugin/plugin_sets.js
@@ -1,19 +1,23 @@
 import { ColorPlugin } from "@html_editor/main/font/color_plugin";
 import { CORE_PLUGINS } from "@html_editor/plugin_sets";
+import { EmbeddedComponentPlugin } from "@html_editor/others/embedded_component_plugin";
 import { EmojiPlugin } from "@html_editor/main/emoji_plugin";
 import { FeffPlugin } from "@html_editor/main/feff_plugin";
 import { HintPlugin } from "@html_editor/main/hint_plugin";
 import { InlineCodePlugin } from "@html_editor/main/inline_code";
 import { LinkPastePlugin } from "@html_editor/main/link/link_paste_plugin";
 import { LinkPlugin } from "@html_editor/main/link/link_plugin";
+import { ProtectedNodePlugin } from "@html_editor/core/protected_node_plugin";
+import { SelectionPlaceholderPlugin } from "@html_editor/main/selection_placeholder_plugin";
 import { ShortCutPlugin } from "@html_editor/core/shortcut_plugin";
+import { SyntaxHighlightingPlugin } from "@html_editor/others/embedded_components/plugins/syntax_highlighting_plugin/syntax_highlighting_plugin";
 import { TabulationPlugin } from "@html_editor/main/tabulation_plugin";
 import { ToolbarPlugin } from "@html_editor/main/toolbar/toolbar_plugin";
 import { TranslatePlugin } from "@html_editor/main/translate/translate_plugin";
 
+import { MailCodeBlockPlugin } from "@mail/core/common/plugin/mail_code_block_plugin";
 import { MailComposerPlugin } from "@mail/core/common/plugin/mail_composer_plugin";
 import { MentionPlugin } from "@mail/core/common/plugin/mention_plugin";
-import { ProtectedNodePlugin } from "@html_editor/core/protected_node_plugin";
 
 export const MAIL_CORE_PLUGINS = [
     ...CORE_PLUGINS,
@@ -27,10 +31,17 @@ export const MAIL_CORE_PLUGINS = [
     MailComposerPlugin,
     MentionPlugin,
     ProtectedNodePlugin,
+    SelectionPlaceholderPlugin,
     ShortCutPlugin,
     TabulationPlugin,
     TranslatePlugin,
 ];
 
-export const MAIL_PLUGINS = [...MAIL_CORE_PLUGINS, ToolbarPlugin];
+export const MAIL_PLUGINS = [
+    ...MAIL_CORE_PLUGINS,
+    EmbeddedComponentPlugin,
+    MailCodeBlockPlugin,
+    SyntaxHighlightingPlugin,
+    ToolbarPlugin,
+];
 export const MAIL_SMALL_UI_PLUGINS = MAIL_CORE_PLUGINS;
