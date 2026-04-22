48052d4da6f0d44c558a58e2f3677f1ddbfeec4f
Didier (did) <did@odoo.com>
2026-04-22 13:07:35 +0000

    [FIX] mail: disable dropzone for user without write access
    This commit disables the drag&drop of files into the chatter if the user
    cannot post on the thread.
    
    Part of task-6071789
    
    closes odoo/odoo#262813
    
    X-original-commit: 3d1f1db2d1329a3e0da75db678c0660d121c3091
    Related: odoo/enterprise#116242
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>
    Signed-off-by: Didier Debondt (did) <did@odoo.com>

--- addons/mail/static/src/chatter/web/chatter_patch.js
+++ addons/mail/static/src/chatter/web/chatter_patch.js
@@ -134,7 +134,9 @@ const chatterPatch = {
                     }
                 },
             },
-            () => !this.store.meetingViewOpened || this.env.inMeetingView
+            () =>
+                (!this.store.meetingViewOpened || this.env.inMeetingView) &&
+                (this.state.thread?.isTransient || this.state.thread?.canPostMessage)
         );
         useEffect(
             () => {
