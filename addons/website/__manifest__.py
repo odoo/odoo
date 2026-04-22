c607620d003f8e4f2f5739d02cd302732adfbb44
Chrysanthe (chgo) <chgo@odoo.com>
2026-04-22 17:43:57 +0200

    [IMP] website: introduce `s_website_form_bold` snippet
    part of task-6054072
    
    Part-of: odoo/odoo#259990
    Related: odoo/enterprise#115215
    Related: odoo/documentation#17550
    Related: odoo/upgrade#10046
    Related: odoo/design-themes#1256
    Signed-off-by: Soukéina Bojabza (sobo) <sobo@odoo.com>

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -176,6 +176,7 @@
         'views/snippets/s_banner_contained.xml',
         'views/snippets/s_duo_images.xml',
         'views/snippets/s_hero_about.xml',
+        'views/snippets/s_website_form_bold.xml',
         'views/snippets/s_dynamic_snippet.xml',
         'views/snippets/s_dynamic_snippet_carousel.xml',
         'views/snippets/s_banner_product.xml',
