f765c98ce1fe120ecade33864a4a8cd5abd1e819
Xavier Bol (xbo) <xbo@odoo.com>
2026-04-22 16:33:05 +0200

    [FIX] project: improve perf of project list view
    Before this commit, to render the table, the project list view computed
    the list of selected records once for each cell, and then iterated over
    that selection to check whether selected projects were all associated
    with the same company (to set the stage_id field readonly if not).
    However, computing the selection requires to iterate over all records,
    so the rendering was O(n^2).
    As a consequence, the rendering of (not so) large tables was very
    slow (~1s for 80 records).
    
    With this commit, we compute only once for the whole table whether
    the selection contains records from different companies.
    
    closes odoo/odoo#260752
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/project/static/src/views/project_project_list/project_project_list_renderer.js
+++ addons/project/static/src/views/project_project_list/project_project_list_renderer.js
@@ -8,14 +8,33 @@ export class ProjectProjectListRenderer extends ListRenderer {
         GroupConfigMenu: ProjectTaskGroupConfigMenu,
     };
 
+    /**
+     * This method prevents from computing the selection once for each cell when
+     * rendering the list. Indeed, `selection` is a getter which browses all
+     * records, so computing it for each cell slows down the rendering a lot on
+     * large tables. Moreover, it also prevents from iterating over the selection
+     * to compare projects' companies.
+     *
+     * Returns true if all selected projects have the same value for the specified field.
+     */
+    haveAllSelectedProjectsSameField(field) {
+        if (this._areSelectedProjectsInSameCompany === undefined) {
+            const selection = this.props.list.selection;
+            const companyId = selection.length && getRawValue(selection[0], field);
+            this._areSelectedProjectsInSameCompany = selection.every(
+                (project) => getRawValue(project, field) === companyId
+            );
+            Promise.resolve().then(() => {
+                delete this._areSelectedProjectsInSameCompany;
+            });
+        }
+        return this._areSelectedProjectsInSameCompany;
+    }
+
     isCellReadonly(column, record) {
         let readonly = super.isCellReadonly(column, record);
-        const { selection } = this.props.list;
-        if (column.name === "stage_id" && selection.length) {
-            const companyId = getRawValue(selection[0], "company_id");
-            readonly = selection.some(
-                (task) => getRawValue(task, "company_id") !== companyId
-            );
+        if (!readonly && column.name === "stage_id") {
+            readonly = !this.haveAllSelectedProjectsSameField('company_id');
         }
         return readonly;
     }
