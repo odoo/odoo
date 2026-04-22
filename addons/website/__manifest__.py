e2fdd678a056663f344adb06d0b577da1c74b230
Chrysanthe (chgo) <chgo@odoo.com>
2026-04-22 16:04:12 +0200

    [IMP] website: introduce `s_duo_images` snippet
    part of task-6054072
    
    Part-of: odoo/odoo#259990
    Related: odoo/enterprise#115215
    Related: odoo/documentation#17550
    Related: odoo/upgrade#10046
    Related: odoo/design-themes#1256
    Signed-off-by: Soukéina Bojabza (sobo) <sobo@odoo.com>

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -174,6 +174,7 @@
         'views/snippets/s_bento_features.xml',
         'views/snippets/s_projects_grid.xml',
         'views/snippets/s_banner_contained.xml',
+        'views/snippets/s_duo_images.xml',
         'views/snippets/s_dynamic_snippet.xml',
         'views/snippets/s_dynamic_snippet_carousel.xml',
         'views/snippets/s_banner_product.xml',
