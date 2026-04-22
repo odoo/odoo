61dc77b920e8a5fde24b5f7f83e96e71c37f9733
Pierre-Yves Dufays <pydu@odoo.com>
2026-04-22 13:44:28 +0200

    [IMP] base: mark ir.mail_server max_email_size field as deprecated
    In odoo/odoo#256685, we remove the use of ir.mail_server max_email_size field
    by making the method _get_max_email_size only rely on the ICP parameter and
    not any more on that field. As we were too late to remove the field in 19.3, we
    add a comment to indicate that the field is deprecated and will be removed in
    19.4.
    
    Task-5912830
    
    closes odoo/odoo#261550
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- odoo/addons/base/models/ir_mail_server.py
+++ odoo/addons/base/models/ir_mail_server.py
@@ -190,6 +190,7 @@ class IrMail_Server(models.Model):
     smtp_debug = fields.Boolean(string='Debugging', help="If enabled, the full output of SMTP sessions will "
                                                          "be written to the server log at DEBUG level "
                                                          "(this is very verbose and may include confidential info!)")
+    # deprecated: will be removed in 19.4. Only base.default_max_email_size ICP is taken into account.
     max_email_size = fields.Float(string="Max Email Size")
     sequence = fields.Integer(string='Priority', default=10, help="When no specific mail server is requested for a mail, the highest priority one "
                                                                   "is used. Default priority is 10 (smaller number = higher priority)")
