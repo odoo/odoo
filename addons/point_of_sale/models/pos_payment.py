fleetapps/odoo3

[IMP] point_of_sale: accept multiple currencies in pos
In some countries it's common to accept multiple currencies when making
sales, but we don't let users open POS configs if they assigned to the
POS payment methods for journals which use different currencies.

This should let users create payment methods assigned to multiple
currencies and then still open the POS.

If there are multiple currencies set up on the POS, the payment screen
will show the corresponding currency for each payment method. The totals
are still shown in the config currency, but when selecting a payment
method in a different currency there will be the price in the new
currency between brackets on the payment line.

Task-[5110998](https://www.odoo.com/odoo/project/1737/tasks/5110998)

addons/point_of_sale/models/pos_payment.py
@@ -22,8 +22,8 @@ class PosPayment(models.Model):
     amount = fields.Monetary(string='Amount', required=True, currency_field='currency_id', help="Total amount of the payment.")
     payment_method_id = fields.Many2one('pos.payment.method', string='Payment Method', required=True)
     payment_date = fields.Datetime(string='Date', required=True, readonly=True, default=lambda self: fields.Datetime.now())
-    currency_id = fields.Many2one('res.currency', string='Currency', related='pos_order_id.currency_id')
-    currency_rate = fields.Float(string='Conversion Rate', related='pos_order_id.currency_rate', help='Conversion rate from company currency to order currency.')
+    currency_id = fields.Many2one('res.currency', string='Currency', compute='_compute_currency')
+    currency_rate = fields.Float(string='Conversion Rate', compute='_compute_conversion_rate', help='Conversion rate from config currency to payment currency.')
     partner_id = fields.Many2one('res.partner', string='Customer', related='pos_order_id.partner_id')
     session_id = fields.Many2one('pos.session', string='Session', related='pos_order_id.session_id', store=True, index=True)
     user_id = fields.Many2one('res.users', string='Employee', related='session_id.user_id')
@@ -57,6 +57,21 @@ class PosPayment(models.Model):
                 payment.display_name = f'{payment.name} {formatLang(self.env, payment.amount, currency_obj=payment.currency_id)}'
             else:
                 payment.display_name = formatLang(self.env, payment.amount, currency_obj=payment.currency_id)
+
+    @api.depends('payment_method_id', 'pos_order_id.currency_id')
+    def _compute_conversion_rate(self):
+        for payment in self:
+            toCurrency = self.currency_id
+            fromCurrency = self.pos_order_id.currency_id
+            payment.currency_rate = toCurrency._get_conversion_rate(fromCurrency, toCurrency)
+
+    @api.depends('payment_method_id.journal_id', 'payment_method_id.journal_id.currency_id')
+    def _compute_currency(self):
+        for payment in self:
+            if payment.payment_method_id.journal_id.currency_id:
+                payment.currency_id = payment.payment_method_id.journal_id.currency_id
+            else:
+                payment.currency_id = payment.pos_order_id.currency_id
 
     @api.constrains('amount')
     def _check_amount(self):

odoo/odoo

addons/point_of_sale/models/pos_payment.py
@@ -22,8 +22,8 @@ class PosPayment(models.Model):
     amount = fields.Monetary(string='Amount', required=True, currency_field='currency_id', help="Total amount of the payment.")
     payment_method_id = fields.Many2one('pos.payment.method', string='Payment Method', required=True)
     payment_date = fields.Datetime(string='Date', required=True, readonly=True, default=lambda self: fields.Datetime.now())
-    currency_id = fields.Many2one('res.currency', string='Currency', related='pos_order_id.currency_id')
-    currency_rate = fields.Float(string='Conversion Rate', related='pos_order_id.currency_rate', help='Conversion rate from company currency to order currency.')
+    currency_id = fields.Many2one('res.currency', string='Currency', compute='_compute_currency')
+    currency_rate = fields.Float(string='Conversion Rate', compute='_compute_conversion_rate', help='Conversion rate from config currency to payment currency.')
     partner_id = fields.Many2one('res.partner', string='Customer', related='pos_order_id.partner_id')
     session_id = fields.Many2one('pos.session', string='Session', related='pos_order_id.session_id', store=True, index=True)
     user_id = fields.Many2one('res.users', string='Employee', related='session_id.user_id')
@@ -66,6 +66,21 @@ class PosPayment(models.Model):
                 payment.display_name = f'{payment.name} {formatLang(self.env, payment.amount, currency_obj=payment.currency_id)}'
             else:
                 payment.display_name = formatLang(self.env, payment.amount, currency_obj=payment.currency_id)
+
+    @api.depends('payment_method_id', 'pos_order_id.currency_id')
+    def _compute_conversion_rate(self):
+        for payment in self:
+            fromCurrency = self.currency_id
+            toCurrency = self.pos_order_id.currency_id
+            payment.currency_rate = toCurrency._get_conversion_rate(fromCurrency, toCurrency)
+
+    @api.depends('payment_method_id.journal_id', 'payment_method_id.journal_id.currency_id')
+    def _compute_currency(self):
+        for payment in self:
+            if payment.payment_method_id.journal_id.currency_id:
+                payment.currency_id = payment.payment_method_id.journal_id.currency_id
+            else:
+                payment.currency_id = payment.pos_order_id.currency_id
 
     @api.constrains('amount')
     def _check_amount(self):