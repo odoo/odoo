77532680eda59b179ff2b2a65826256800d0140a
adgu-odoo <adgu@odoo.com>
2026-04-22 07:35:56 +0000

    [IMP] pos_self_order: display extra price for combo
    The display for the extra price during the combo selection was not
    very clear. The customer were not aware of the additional cost that were
    applied when choosing some elements that were not included but extra.
    This led to customer asking cashier if there was a problem because they
    were paying "too much" when the computation was actually correct but
    not clear enough.
    
    closes odoo/odoo#261264
    
    Task-id: 6142095
    X-original-commit: 6528ee6cbd4ba9cf9b41379eb6b9848e4a46e6f7
    Signed-off-by: Stéphane Vanmeerhaeghe (stva) <stva@odoo.com>
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/pos_self_order/tests/test_self_order_combo.py
+++ addons/pos_self_order/tests/test_self_order_combo.py
@@ -416,6 +416,35 @@ class TestSelfOrderCombo(SelfOrderCommonTest):
         self.assertEqual(orders[1].amount_total, 24)
         self.assertEqual(orders[0].amount_total, 36)
 
+    def test_self_combo_extra_price_selection_and_confirmation(self):
+        """
+        Test extra price display in combo selection and confirmation.
+        - Combo with qty_free=0: All items show "+ €X" price badge
+        - Combo with qty_free>0: Free items have no extra badge, paid items show "Extra: €X"
+        - Confirmation page displays extra prices correctly
+        """
+
+        setup_product_combo_items(self)
+        self.desks_combo.is_upsell = True
+        self.desks_combo.qty_free = 0
+        self.desks_combo.qty_max = 3
+
+        self.desk_accessories_combo.qty_free = 1
+        self.desk_accessories_combo.qty_max = 3
+
+        self.pos_config.write({
+            'self_ordering_default_user_id': self.pos_admin.id,
+            'self_ordering_mode': 'mobile',
+            'self_ordering_pay_after': 'each',
+            'self_ordering_service_mode': 'counter',
+            'available_preset_ids': [(5, 0)],
+        })
+        self.pos_config.with_user(self.pos_user).open_ui()
+        self.pos_config.current_session_id.set_opening_control(0, "")
+        self_route = self.pos_config._get_self_order_route()
+
+        self.start_tour(self_route, "test_self_combo_extra_price_selection_and_confirmation")
+
     def test_combo_price_unit_mulitple_qty(self):
         """
         Tests that the unit price of combos ordered multiple times through the self
