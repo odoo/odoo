d471cb7e4bf7a3f1fecc821f7529068ba40b84d3
Subhadeep R. Dev <sdev@odoo.com>
2026-04-23 10:49:09 +0200

    [IMP] website: add card border radius controls to Theme tab
    Add new card border radius controls to the Website Builder Theme tab
    allowing users to customize Bootstrap `card-border-radius` variables and
    adjust the roundness of card components independently from the global
    border radius settings.
    
    task-3641683

--- addons/website/static/src/builder/plugins/theme/theme_tab_plugin.js
+++ addons/website/static/src/builder/plugins/theme/theme_tab_plugin.js
@@ -63,9 +63,10 @@ export const OPTION_POSITIONS = {
     BUTTON: 50,
     LINK: 60,
     INPUT: 70,
-    ROUNDNESS: 80,
-    SHADOW: 90,
-    ADVANCED: 100,
+    CARD: 80,
+    ROUNDNESS: 90,
+    SHADOW: 100,
+    ADVANCED: 110,
 };
 
 const FONT_WEIGHT_OPTIONS = [
@@ -183,6 +184,16 @@ export class ThemeTabPlugin extends Plugin {
                     _t("Input Fields"),
                     class ThemeInputOption extends BaseOptionComponent {
                         static template = "website.ThemeInputOption";
+                    }
+                )
+            ),
+            withSequence(
+                OPTION_POSITIONS.CARD,
+                this.getThemeOptionBlock(
+                    "theme-card",
+                    _t("Card"),
+                    class ThemeCardOption extends BaseOptionComponent {
+                        static template = "website.ThemeCardOption";
                     }
                 )
             ),
