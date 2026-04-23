a0de210abc47b9fb803723e34044369aae7b86c0
agbr-odoo <agbr@odoo.com>
2026-04-23 17:51:47 +0530

    [FIX] sale_{mrp,mrp_margin}, purchase_mrp: avoid dropship kits to become zero
    Currently, when a user dropships a kit product with FIFO/AVCO category costing,
    the computed cost of the kit on the Sales Order becomes 0.
    
    Steps to replicate:
    - Install Sales, Purchase, and Manufacturing modules.
    - Enable Margins and Dropshipping in settings.
    - Go to Product Categories > Goods and set costing method to FIFO
    - Create two products MOBO and CPU:
       -Cost $300
       -Category: Goods
       -Add a vendor in Purchase section with unit price same as cost
       -Enable Dropshipping route
    - Create a kit product Computer Kit with the same configuration as above
      (except cost) and add MOBO and CPU as components on its BoM.
    - Open the Computer Kit and click Compute Price from BoM.
    - Create and confirm a Sales Order with the Computer Kit.
    - Confirm the related Purchase Order and validate the dropship picking.
    - Return to the Sales Order > make the Cost field visible on SO lines.
    
    Observed Behavior:
    The product cost appears as 0 on the Sales Order, even though a price is set on
    the related Purchase Order.
    
    Root cause:
    When the dropshipping picking is confirmed, the method _compute_purchase_price
    is triggered to compute the cost on the Sales Order line. It calls
    _get_price_unit_delivery at [1], which then calls _get_price_unit_dropshipped at
    [2] since the products are dropshipped. Because dropshipping moves do not carry
    stock values, it calls _get_value at [3] to determine an appropriate value.
    
    This method uses _get_value_data at [4], which retrieves the value from the
    quotation via _get_value_from_quotation at [5]. Here, a cost ratio is applied at
    [6] to distribute the cost based on the BoM cost share
    (i.e., the percentage split of cost across kit components).
    Since no cost share is defined on the BoM, the ratio is 0, causing the
    final computed cost ratio to also be 0 at [7] and the cost value being returned
    as zero as shown in at [6].
    
    Why not in lower versions?
    This issue did not occur in versions 18.4 and earlier due to the presence of
    the stock valuation layer and the defined logic for kit products to calculate
    cost, as shown in [8].
    
    Solution:
    It should not be assumed that users will always define a cost share on the Bill
    of Materials. In many cases, they may expect the kit price to be derived
    directly from the costs of its component products.
    
    To support this, we can override _get_price_unit_dropshipped to properly
    handle kit products.
    
    [1]:
    https://github.com/odoo/odoo/blob/d92feb4a6f463cae59aa107f46b5699f83f8ef9c/addons/sale_stock_margin/models/sale_order_line.py#L18-L21
    [2]:
    https://github.com/odoo/odoo/blob/d92feb4a6f463cae59aa107f46b5699f83f8ef9c/addons/stock_account/models/stock_move.py#L672
    [3]:
    https://github.com/odoo/odoo/blob/d92feb4a6f463cae59aa107f46b5699f83f8ef9c/addons/stock_account/models/stock_move.py#L681-L684
    [4]:
    https://github.com/odoo/odoo/blob/d92feb4a6f463cae59aa107f46b5699f83f8ef9c/addons/stock_account/models/stock_move.py#L336
    [5]:
    https://github.com/odoo/odoo/blob/d92feb4a6f463cae59aa107f46b5699f83f8ef9c/addons/stock_account/models/stock_move.py#L392-L397
    [6]:
    https://github.com/odoo/odoo/blob/d92feb4a6f463cae59aa107f46b5699f83f8ef9c/addons/purchase_stock/models/stock_move.py#L225-L241
    [7]:
    https://github.com/odoo/odoo/blob/d92feb4a6f463cae59aa107f46b5699f83f8ef9c/addons/purchase_mrp/models/stock_move.py#L12-L25
    [8]:
    https://github.com/odoo/odoo/blob/7f1cd04259202bcafc94965d6420df360f9152c1/addons/mrp_account/models/product.py#L65-L89
    
    opw-6113398
    
    closes odoo/odoo#261705
    
    Signed-off-by: Quentin Wolfs (quwo) <quwo@odoo.com>

--- addons/sale_mrp/models/stock_move.py
+++ addons/sale_mrp/models/stock_move.py
@@ -13,14 +13,27 @@ class StockMove(models.Model):
             if move.bom_line_id and move.bom_line_id.bom_id.type == 'phantom':
                 move.packaging_uom_id = move.product_uom
 
-    def _get_price_unit(self):
+    def _get_kit_value_per_unit(self):
         order_line = self.sale_line_id
         if order_line and all(move.sale_line_id == order_line for move in self) and any(move.product_id != order_line.product_id for move in self):
             product = order_line.product_id.with_company(order_line.company_id)
             bom = product.env['mrp.bom']._bom_find(product, company_id=self.company_id.id, bom_type='phantom')[product]
             if bom:
                 return self._get_kit_price_unit(product, bom, order_line.product_uom_qty)
+        return 0
+
+    def _get_price_unit(self):
+        kit_unit_price = self._get_kit_value_per_unit()
+        if kit_unit_price:
+            return kit_unit_price
         return super()._get_price_unit()
+
+    def _get_price_unit_dropshipped(self):
+        """ Overridden to handle Kit dropship products correctly. """
+        kit_unit_price = self._get_kit_value_per_unit()
+        if kit_unit_price:
+            return kit_unit_price
+        return super()._get_price_unit_dropshipped()
 
     def _get_source_document(self):
         return self.production_id or self.raw_material_production_id or super()._get_source_document()
