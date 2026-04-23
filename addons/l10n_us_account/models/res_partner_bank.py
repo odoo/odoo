0953a2b2bcde86ebba756b415da587a76e84cab8
utma-odoo <utma@odoo.com>
2026-04-23 16:03:47 +0530

    [FIX] l10n_us_account: keep intermediary SWIFT visible for US company
    Steps to reproduce:
    - Install the `l10n_us_account` module and switch to a My US company
    - Navigate to Invoicing > Configuration > Journals > Bank
    - Click the internal link of the `Bank Account Number`
    - Go to the `Bank Information` tab
    - Observe that the `Intermediary SWIFT` field is visible
    - Remove `United States` from the bank address
    
    Observation:
    The `Intermediary SWIFT` field becomes invisible when the country
    is not set to United States.
    
    Root Cause:
    At [1], the field Intermediary SWIFT( `intermediary_bank_bic`) is defined with
    a condition that makes it invisible when the country is not 'US'.
    
    Fix:
    This commit ensures that the `Intermediary SWIFT` field is remains visible when
    the company uses the US fiscal localization package, even if the bank country
    is not set to United States.
    
    [1]:
    https://github.com/odoo/odoo/blob/1d4715e58613912bfd0ded4fd370c50c131ff164/addons/l10n_us_account/views/res_partner_bank_views.xml#L9
    
    opw-6145872

--- addons/l10n_us_account/models/res_partner_bank.py
+++ addons/l10n_us_account/models/res_partner_bank.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ResPartnerBank(models.Model):
@@ -8,3 +8,9 @@ class ResPartnerBank(models.Model):
         "Intermediary SWIFT",
         help="An intermediary bank facilitates international wire transfers between your bank and the beneficiary's bank when they don't have a direct relationship.",
     )
+    show_intermediary_bank_bic = fields.Boolean(compute="_compute_show_intermediary_bank_bic")
+
+    @api.depends_context('company')
+    def _compute_show_intermediary_bank_bic(self):
+        for bank in self:
+            bank.show_intermediary_bank_bic = bank.env.company.account_fiscal_country_id.code == 'US'
