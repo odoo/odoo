fffe0c1726ce80068b484ef720f53ab5103e781a
Chrysanthe (chgo) <chgo@odoo.com>
2026-04-23 09:03:39 +0200

    [IMP] website: introduce `s_hero_maintenance` snippet
    part of task-6054072
    
    Part-of: odoo/odoo#259990
    Related: odoo/enterprise#115215
    Related: odoo/documentation#17550
    Related: odoo/upgrade#10046
    Related: odoo/design-themes#1256
    Signed-off-by: Soukéina Bojabza (sobo) <sobo@odoo.com>

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -178,6 +178,7 @@
         'views/snippets/s_hero_about.xml',
         'views/snippets/s_website_form_bold.xml',
         'views/snippets/s_title_split.xml',
+        'views/snippets/s_hero_maintenance.xml',
         'views/snippets/s_dynamic_snippet.xml',
         'views/snippets/s_dynamic_snippet_carousel.xml',
         'views/snippets/s_banner_product.xml',
