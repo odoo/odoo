f4bfd38cc2fdb0613c06b63e8a62fec18ec14795
Louis (loco) <loco@odoo.com>
2026-04-23 09:47:41 +0000

    [FIX] html_builder: apply image optimisation when possible
    Since [1], when users select multiple images through the media dialog,
    subsequent images of a CORS protected image are not converted to webp.
    This commit fixes that issue.
    
    Related to task-5405262
    
    [1]: https://github.com/odoo/odoo/commit/422b073bcc6406c76339a1ccaa0c40dc3f42801c
    
    X-original-commit: 9d01b05b801c9861eadea3faef8a709b5e67c466

--- addons/html_builder/static/src/plugins/image/image_tool_option_plugin.js
+++ addons/html_builder/static/src/plugins/image/image_tool_option_plugin.js
@@ -70,7 +70,7 @@ class ImageToolOptionPlugin extends Plugin {
                 if (image && image.tagName === "IMG") {
                     const imgInfo = await loadImageInfo(image);
                     if (!imgInfo.originalSrc || !imgInfo.originalId) {
-                        return;
+                        continue;
                     }
                     const isImgSupportedForProcessing = await isImageSupportedForProcessing(
                         image,
