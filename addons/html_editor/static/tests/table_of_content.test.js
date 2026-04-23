7e1c5d4bad2498977465845d67df07372a1244e1
dhba <dhba@odoo.com>
2026-04-23 18:05:35 +0530

    [FIX] html_editor: table of contents not updating
    Steps to Reproduce :
    Go to To-Do → Create New and add a Table of Content block
    Type text → in new line create /h1  → it appears in ToC
    Place cursor before /h1 and press Backspace → it merges with paragraph
    
    Issue:
    Table of Content block does not update accordingly
    
    Cause:
    After the heading is merged with the previous paragraph,
    `delayedUpdateTableOfContents` is triggered, but at that time no heading is
    available in the editable area. As a result, instead of updating the
    Table of Contents, it returns without making any changes.
    
    Solution:
    If Table of content already contains heading, then update regardless of whether
    editable contains heading elements or not.
    
    task-6150579
    
    closes odoo/odoo#261675
    
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

---
+++ addons/html_editor/static/tests/table_of_content.test.js
@@ -0,0 +1,54 @@
+import { advanceTime, press, queryAll, queryAllTexts, queryOne } from "@odoo/hoot-dom";
+import { setupEditor } from "./_helpers/editor";
+import { setSelection } from "./_helpers/selection";
+import { deleteBackward, insertText } from "./_helpers/user_actions";
+import { expectElementCount } from "./_helpers/ui_expectations";
+import { expect, test } from "@odoo/hoot";
+import { contains } from "@web/../tests/web_test_helpers";
+import { nodeSize } from "@html_editor/utils/position";
+import { EMBEDDED_COMPONENT_PLUGINS, MAIN_PLUGINS } from "@html_editor/plugin_sets";
+import { MAIN_EMBEDDINGS } from "@html_editor/others/embedded_components/embedding_sets";
+
+const configWithEmbeddedTableOfContent = {
+    Plugins: [...MAIN_PLUGINS, ...EMBEDDED_COMPONENT_PLUGINS],
+    resources: { embedded_components: MAIN_EMBEDDINGS },
+};
+
+test("should update table of contents when heading is removed with backspace", async () => {
+    const { editor } = await setupEditor("<p>[]first</p><h1>second</h1>", {
+        config: configWithEmbeddedTableOfContent,
+    });
+    await insertText(editor, "/tableofcontent");
+    await expectElementCount(".o-we-powerbox", 1);
+    expect(queryAllTexts(".o-we-command-name")[0]).toBe("Table Of Content");
+    await press("Enter");
+    // TOC update is debounced
+    await advanceTime(500);
+    expect(queryAll(".o_embedded_toc_link")).toHaveCount(1);
+    setSelection({ anchorNode: queryOne("h1"), anchorOffset: 0 });
+    deleteBackward(editor);
+    // TOC update is debounced
+    await advanceTime(500);
+    expect(queryAll(".o_embedded_toc_link")).toHaveCount(0);
+});
+
+test("should update table of contents when heading is converted to paragraph", async () => {
+    const { editor } = await setupEditor("<p>[]first</p><h1>second</h1>", {
+        config: configWithEmbeddedTableOfContent,
+    });
+    await insertText(editor, "/tableofcontent");
+    await expectElementCount(".o-we-powerbox", 1);
+    expect(queryAllTexts(".o-we-command-name")[0]).toBe("Table Of Content");
+    await press("Enter");
+    // TOC update is debounced
+    await advanceTime(500);
+    expect(queryAll(".o_embedded_toc_link")).toHaveCount(1);
+    const h1 = queryOne("h1");
+    setSelection({ anchorNode: h1, anchorOffset: 0, focusNode: h1, focusOffset: nodeSize(h1) });
+    await expectElementCount(".o-we-toolbar", 1);
+    await contains(".o-we-toolbar [name='font'] .dropdown-toggle").click();
+    await contains(".o_font_selector_menu .dropdown-item:contains('Paragraph')").click();
+    // TOC update is debounced
+    await advanceTime(500);
+    expect(queryAll(".o_embedded_toc_link")).toHaveCount(0);
+});
