1378e4e58265640b73ff1838ece387d48361f7c6
Harrison Hutton <hahu@odoo.com>
2026-04-23 16:25:42 -0400

    [FIX] pos_hr: order details dialog crash when not in English
    **Before this commit**
    When attempting to open up the "Details" of a POS order, when the
    database is not in English, we would crash here because there wouldn't
    be any fields that match the string "Served By" exactly. For example, in
    Spanish, this field is labeled "Atendido por".
    
    **After this commit**
    Compare the field labels to the database's language-translated string,
    "Served By".
    
    opw-6145087
    
    closes odoo/odoo#261119
    
    Signed-off-by: Stéphane Vanmeerhaeghe (stva) <stva@odoo.com>

--- addons/pos_hr/static/src/app/screens/order_details_dialog.js
+++ addons/pos_hr/static/src/app/screens/order_details_dialog.js
@@ -1,10 +1,11 @@
 import { patch } from "@web/core/utils/patch";
 import { OrderDetailsDialog } from "@point_of_sale/app/screens/ticket_screen/order_details_dialog/order_details_dialog";
+import { _t } from "@web/core/l10n/translation";
 
 patch(OrderDetailsDialog.prototype, {
     getOrderFields() {
         const fields = super.getOrderFields();
-        fields.find((f) => f.label === "Served By").value = this.props.order.employee_id.name;
+        fields.find((f) => f.label === _t("Served By")).value = this.props.order.employee_id.name;
         return fields;
     },
 });
