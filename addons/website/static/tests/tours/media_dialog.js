MoLeap/odooo

[FIX] html_editor: target documents with right class
The class `.o_image` was still associated with documents (in the context
of the file selector) and the expected tag of a document was `A`, in
spite of it not being true in `html_editor` since the introduction of
the file box in [1].

This has also been updated in the website builder since the introduction
of the `html_builder` module in 18.4, which swapped uses of `web_editor`
components for `html_editor`.

As a side-effect, in website, a double click on a file did not open the
media dialog on the "document" tab, unlike other media (images, videos,
icons). In such a case, the file was also not shown as already selected
in the media dialog (because it targetted the wrong tag name). Both of
those behaviors were lost as we used the new file box design in 18.4.

[1]: https://github.com/odoo/odoo/commit/7f9afa21dffba2f74f9fb8a68809db4af6c7c225

task-5876278

addons/website/static/tests/tours/media_dialog.js
@@ -223,3 +223,62 @@ registerWebsitePreviewTour("website_media_dialog_insert_media", {
         trigger: ":iframe .s_text_block p > span.fa",
     },
 ]);
+
+registerWebsitePreviewTour(
+    "website_media_dialog_insert_file",
+    {
+        url: "/",
+        edition: true,
+    },
+    () => [
+        ...insertSnippet({
+            id: "s_text_block",
+            name: "Text",
+            groupName: "Text",
+        }),
+        {
+            content: "Click on the first paragraph",
+            trigger: ":iframe .s_text_block p",
+            run: "editor test",
+        },
+        {
+            content: "Show the powerbox",
+            trigger: ":iframe .s_text_block p:last-child",
+            async run(actions) {
+                await actions.editor(`/`);
+                const wrapwrap = this.anchor.closest("#wrapwrap");
+                wrapwrap.dispatchEvent(
+                    new InputEvent("input", {
+                        inputType: "insertText",
+                        data: "/",
+                    })
+                );
+            },
+        },
+        {
+            content: "Click on the media item from powerbox",
+            trigger: "div.o-we-command-name:contains('Media')",
+            run: "click",
+        },
+        {
+            content: "Click on the 'Documents' tab",
+            trigger: ".o_select_media_dialog a.nav-link:contains('Documents')",
+            run: "click",
+        },
+        {
+            content: "Click on the first document",
+            trigger: ".o_select_media_dialog .o_existing_attachment_cell",
+            run: "click",
+        },
+        {
+            content:
+                "Verify that the document was inserted and reopen the media dialog with a double click",
+            trigger: ":iframe .s_text_block p > .o_file_box",
+            run: "dblclick",
+        },
+        {
+            content: "Verify that the dialog opened on the Documents tab",
+            trigger: ".o_select_media_dialog a.nav-link.active:contains('Documents')",
+        },
+    ]
+);

odoo/odoo

X-original-commit: 13ffc4157004e6f026ba1e5f3a68c444c7cd1476

addons/website/static/tests/tours/media_dialog.js
@@ -252,3 +252,61 @@ registerWebsitePreviewTour(
         },
     ]
 );
+
+registerWebsitePreviewTour(
+    "website_media_dialog_insert_file",
+    {
+        edition: true,
+    },
+    () => [
+        ...insertSnippet({
+            id: "s_text_block",
+            name: "Text",
+            groupName: "Text",
+        }),
+        {
+            content: "Click on the first paragraph",
+            trigger: ":iframe .s_text_block p",
+            run: "editor test",
+        },
+        {
+            content: "Show the powerbox",
+            trigger: ":iframe .s_text_block p:last-child",
+            async run(actions) {
+                await actions.editor(`/`);
+                const wrapwrap = this.anchor.closest("#wrapwrap");
+                wrapwrap.dispatchEvent(
+                    new InputEvent("input", {
+                        inputType: "insertText",
+                        data: "/",
+                    })
+                );
+            },
+        },
+        {
+            content: "Click on the media item from powerbox",
+            trigger: "div.o-we-command-name:contains('Media')",
+            run: "click",
+        },
+        {
+            content: "Click on the 'Documents' tab",
+            trigger: ".o_select_media_dialog button.nav-link:contains('Documents')",
+            run: "click",
+        },
+        {
+            content: "Click on the first document",
+            trigger: ".o_select_media_dialog .o_existing_attachment_cell .o_button_area",
+            run: "click",
+        },
+        {
+            content:
+                "Verify that the document was inserted and reopen the media dialog with a double click",
+            trigger: ":iframe .s_text_block p > .o_file_box",
+            run: "dblclick",
+        },
+        {
+            content: "Verify that the dialog opened on the Documents tab",
+            trigger: ".o_select_media_dialog button.nav-link.active:contains('Documents')",
+        },
+    ]
+);