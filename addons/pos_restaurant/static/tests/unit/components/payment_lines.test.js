e3f33e25f5b139c2d096f9989e7fd7dbb62e97db
Jean Schoenlaub <jesc@odoo.com>
2026-04-24 16:20:48 +0200

    [FIX] pos_restaurant: use proxy for mutable props in payment_lines test
    OWL 3 creates props with getter-only descriptors, so direct assignment to
    comp.props throws a TypeError. Use owl.proxy() to create a reactive props
    object and mutate it directly instead.

--- addons/pos_restaurant/static/tests/unit/components/payment_lines.test.js
+++ addons/pos_restaurant/static/tests/unit/components/payment_lines.test.js
@@ -8,6 +8,7 @@ import {
 } from "@point_of_sale/../tests/unit/utils";
 import { definePosModels } from "@point_of_sale/../tests/unit/data/generate_model_definitions";
 import { PaymentScreenPaymentLines } from "@point_of_sale/app/screens/payment_screen/payment_lines/payment_lines";
+import { proxy } from "@odoo/owl";
 
 definePosModels();
 
@@ -16,19 +17,18 @@ test("getPaymentActionState", async () => {
     const order = await getFilledOrder(store);
     const card = store.models["pos.payment.method"].get(2);
     const paymentline = createPaymentLine(store, order, card, { payment_status: "done" });
-    const comp = await mountWithCleanup(PaymentScreenPaymentLines, {
-        props: {
-            paymentLines: [paymentline],
-            deleteLine: () => {},
-            selectLine: () => {},
-            sendForceDone: () => {},
-            sendForceCancel: () => {},
-            sendPaymentCancel: () => {},
-            sendPaymentRequest: () => {},
-            updateSelectedPaymentline: () => {},
-            isRefundOrder: false,
-        },
+    const props = proxy({
+        paymentLines: [paymentline],
+        deleteLine: () => {},
+        selectLine: () => {},
+        sendForceDone: () => {},
+        sendForceCancel: () => {},
+        sendPaymentCancel: () => {},
+        sendPaymentRequest: () => {},
+        updateSelectedPaymentline: () => {},
+        isRefundOrder: false,
     });
+    const comp = await mountWithCleanup(PaymentScreenPaymentLines, { props });
 
     // Helper
     const normalizeActionState = (state) => {
@@ -61,7 +61,7 @@ test("getPaymentActionState", async () => {
 
     // Done + canBeAdjusted + amountPaid (10) < priceIncl (15)
     // --> adjust_amount action
-    comp.props.isRefundOrder = false;
+    props.isRefundOrder = false;
     paymentline.canBeAdjusted = () => true;
     order.prices.taxDetails.total_amount_no_rounding = 15;
     expectDoneAdjustAmountAction(comp.getPaymentActionState(paymentline));
