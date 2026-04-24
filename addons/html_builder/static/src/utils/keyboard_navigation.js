odoo/odoo

commit 1f863651ed64b207683fc7295e1f7e5a38920dac

[IMP] html_builder, website: use arrow keys to navigate in tab panes
The tab panes of the AddSnippetDialog and the AddPageDialog, as well as
the snippets blocks in the sidebar, would benefit from a proper keyboard
navigation. Since [9ae02d8], the snippets and page layouts are tabable.
But when there are many snippets or layouts, an approach using the
arrows would be more practical and intuitive, as the dialogs and the
snippets categories all use a masonry layout.

The logic makes sure to handle both LTR and RTL languages.

[9ae02d8]: https://github.com/odoo/odoo/commit/9ae02d80894d4043e49d8e2cad068f8018e6f113

task-5384672

closes odoo/odoo#240095

Signed-off-by: Francois Georis (fge) <fge@odoo.com>

addons/html_builder/static/src/utils/keyboard_navigation.js
@@ -1,0 +1,157 @@
+import { onMounted, onPatched } from "@odoo/owl";
+import { browser } from "@web/core/browser/browser";
+import { getActiveHotkey } from "@web/core/hotkeys/hotkey_service";
+import { localization } from "@web/core/l10n/localization";
+
+/**
+ * Makes a 2D matrix of elements from left to right and from top to bottom.
+ *
+ * @param {HTMLElement} containerEl
+ * @param {string} selector - selector for descendants of containerEl
+ * @returns {Array<Array<HTMLElement>>} matrix
+ */
+function makeElementsPositionMatrix(containerEl, selector) {
+    const leftMap = new Map();
+    for (const el of containerEl.querySelectorAll(selector)) {
+        if (!leftMap.has(el.offsetLeft)) {
+            leftMap.set(el.offsetLeft, [el]);
+        } else {
+            leftMap.get(el.offsetLeft).push(el);
+        }
+    }
+    const matrix = [...leftMap.keys()]
+        .sort((a, b) => a - b)
+        .map((leftPos) => leftMap.get(leftPos).sort((a, b) => a.offsetTop - b.offsetTop));
+
+    return matrix;
+}
+
+/**
+ * Handles navigation keys (arrows, home, end, page up, page down) on a 2D
+ * layout.
+ *
+ * @param {KeyboardEvent} ev
+ * @param {Object} options
+ * @param {Array<Array<HTMLElement>>} options.matrix - 2D navigation matrix
+ * @param {HTMLElement} options.activeMatrixEl - currently active matrix sibling
+ * @param {string} [options.focusableChildSelector] - selector used if the
+ * actual focusable element is a child of `activeMatrixEl`
+ */
+function handleMatrixKeyNavigation(ev, { matrix, activeMatrixEl, focusableChildSelector }) {
+    const hotkey = getActiveHotkey(ev);
+
+    if (
+        [
+            "arrowup",
+            "arrowdown",
+            "arrowleft",
+            "arrowright",
+            "home",
+            "end",
+            "pagedown",
+            "pageup",
+        ].includes(hotkey)
+    ) {
+        ev.preventDefault(); // Do not scroll.
+        let nextActiveMatrixEl;
+        const elMatrixColIdx = matrix.findIndex((col) => col.includes(activeMatrixEl));
+        if (["home", "end"].includes(hotkey)) {
+            const isRTL = localization.direction === "rtl";
+            const firstCol = isRTL ? matrix.at(-1) : matrix[0];
+            const lastCol = isRTL ? matrix[0] : matrix.at(-1);
+            nextActiveMatrixEl = hotkey === "home" ? firstCol[0] : lastCol.at(-1);
+        } else if (["arrowup", "arrowdown"].includes(hotkey)) {
+            const elMatrixColumn = matrix[elMatrixColIdx];
+            const elIdx = elMatrixColumn.indexOf(activeMatrixEl);
+            if (hotkey === "arrowup") {
+                nextActiveMatrixEl = elMatrixColumn[Math.max(elIdx - 1, 0)];
+            } else if (hotkey === "arrowdown") {
+                nextActiveMatrixEl = elMatrixColumn[Math.min(elIdx + 1, elMatrixColumn.length - 1)];
+            }
+        } else if (["arrowleft", "arrowright"].includes(hotkey)) {
+            const rect = activeMatrixEl.getBoundingClientRect();
+            let nextCol;
+            if (hotkey === "arrowleft") {
+                if (elMatrixColIdx === 0) {
+                    return;
+                }
+                nextCol = matrix[elMatrixColIdx - 1];
+            } else if (hotkey === "arrowright") {
+                if (elMatrixColIdx === matrix.length - 1) {
+                    return;
+                }
+                nextCol = matrix[elMatrixColIdx + 1];
+            }
+            nextActiveMatrixEl = nextCol.findLast(
+                (el) => rect.y + rect.height / 2 >= el.getBoundingClientRect().y
+            );
+        } else if (["pageup", "pagedown"].includes(hotkey)) {
+            // The default behavior is weird in this context, but implementing
+            // a behavior that makes sense (e.g. focus the last visible element
+            // and scroll) is complex for little use. Accessibility patterns of
+            // similar usecases don't seem to take these keys into account.
+            return;
+        }
+        if (focusableChildSelector) {
+            nextActiveMatrixEl = nextActiveMatrixEl.querySelector(focusableChildSelector);
+        }
+        const reducedMotion = browser.matchMedia("(prefers-reduced-motion: reduce)").matches;
+        nextActiveMatrixEl.scrollIntoView({
+            block: "nearest",
+            behavior: reducedMotion ? "instant" : "smooth",
+        });
+        nextActiveMatrixEl.focus();
+    }
+}
+
+/**
+ * @param {() => HTMLElement[]} getContainerEls - common container ancestors
+ * (one per matrix group)
+ * @param {string} matrixSiblingSelector - selector to target the elements
+ * to use for the navigation logic (i.e. the elements whose siblings make up the
+ * masonry layout). The selector will be queried both up
+ * (`ev.currentTarget.closest`) and down (`containerEl.querySelectorAll`).
+ * @param {string} [focusableChildSelector] - selector used if the actual
+ * actual focusable element is a child of `matrixSiblingSelector`
+ * @returns {(ev: KeyboardEvent) => void} keydown event handler
+ */
+export function useMatrixKeyNavigation(
+    getContainerEls,
+    matrixSiblingSelector,
+    focusableChildSelector
+) {
+    const matrices = new WeakMap();
+
+    onMounted(() => {
+        for (const containerEl of getContainerEls()) {
+            const matrix = makeElementsPositionMatrix(containerEl, matrixSiblingSelector);
+            matrices.set(containerEl, matrix);
+        }
+    });
+
+    onPatched(() => {
+        for (const containerEl of getContainerEls()) {
+            if (
+                !matrices.has(containerEl) ||
+                !matrices.get(containerEl).flat().length !==
+                    containerEl.querySelectorAll(matrixSiblingSelector).length ||
+                !matrices.get(containerEl)[0][0].isConnected
+            ) {
+                const matrix = makeElementsPositionMatrix(containerEl, matrixSiblingSelector);
+                matrices.set(containerEl, matrix);
+            }
+        }
+    });
+
+    return (ev) => {
+        const currentContainerEl = getContainerEls().find((containerEl) =>
+            containerEl.contains(ev.currentTarget)
+        );
+        const activeMatrixEl = ev.currentTarget.closest(matrixSiblingSelector);
+        handleMatrixKeyNavigation(ev, {
+            matrix: matrices.get(currentContainerEl),
+            activeMatrixEl,
+            focusableChildSelector,
+        });
+    };
+}

addons/html_builder/__manifest__.py
@@ -21,6 +21,9 @@
         'web._assets_primary_variables': [
             'html_builder/static/src/**/*.variables.scss',
         ],
+        'web.assets_backend': [
+            'html_builder/static/src/utils/keyboard_navigation.js',
+        ],
         # this bundle is lazy loaded when the editor is ready
         'html_builder.assets': [
             ('include', 'web._assets_helpers'),

addons/html_builder/static/src/sidebar/block_tab.js
@@ -8,6 +8,7 @@ import { closest } from "@web/core/utils/ui";
 import { useDragAndDrop } from "@html_editor/utils/drag_and_drop";
 import { getCSSVariableValue } from "@html_editor/utils/formatting";
 import { useSnippets } from "@html_builder/snippets/snippet_service";
+import { useMatrixKeyNavigation } from "@html_builder/utils/keyboard_navigation";
 import { Snippet } from "./snippet";
 import { CustomInnerSnippet } from "./custom_inner_snippet";
 
@@ -36,8 +37,16 @@ export class BlockTab extends Component {
         this.popover = useService("popover");
         this.snippetModel = useSnippets(this.props.snippetsName);
         this.blockTabRef = useRef("block-tab");
+        this.groupSnippetsContainer = useRef("group-snippets-container");
+        this.innerSnippetsContainer = useRef("inner-snippets-container");
         // Needed to avoid race condition in tours.
         this.state = useState({ ongoingInsertion: false });
+
+        this.onSnippetKeydown = useMatrixKeyNavigation(
+            () => [this.groupSnippetsContainer.el, this.innerSnippetsContainer.el],
+            ".o_snippet",
+            ".o_snippet_thumbnail_area, .o_install_btn"
+        );
 
         onMounted(() => {
             this.makeSnippetDraggable();
@@ -443,7 +452,7 @@ export class BlockTab extends Component {
      * Opens the corresponding snippet group dialog after the installation of a
      * newly installed snippet module.
      *
-     * @param {string} newInstalledModule - The JSON object containing title of 
+     * @param {string} newInstalledModule - The JSON object containing title of
      * the snippet group to open.
      */
     async handlePostModuleInstall(newInstalledModule) {

addons/html_builder/static/src/sidebar/block_tab.xml
@@ -10,11 +10,12 @@
          role="tabpanel"
          aria-labelledby="blocks-tab">
         <div t-if="this.snippetModel.snippetGroups.length" class="o_snippets_container" id="snippet_groups">
-            <div class="o_snippets_container_body o_snippets_container_body_row_cols_2">
+            <div t-custom-ref="group-snippets-container" class="o_snippets_container_body o_snippets_container_body_row_cols_2">
                 <t t-foreach="this.snippetModel.snippetGroups" t-as="snippet" t-key="snippet.id">
                     <Snippet snippet="snippet"
                              snippetModel="this.snippetModel"
                              onClickHandler="() => this.onSnippetGroupClick(snippet)"
+                             onSnippetKeydown="this.onSnippetKeydown"
                              disabledTooltip="disabledGroupTooltip"/>
                 </t>
             </div>
@@ -37,11 +38,12 @@
         <div t-if="this.snippetModel.snippetInnerContents.length"
             class="o_snippets_container" id="snippet_content">
             <div class="o_snippets_container_header"><span>Inner Content</span></div>
-            <div class="o_snippets_container_body">
+            <div t-custom-ref="inner-snippets-container" class="o_snippets_container_body">
                 <t t-foreach="this.snippetModel.snippetInnerContents" t-as="snippet" t-key="snippet.id">
                     <Snippet snippet="snippet"
                              snippetModel="this.snippetModel"
                              onClickHandler.bind="this.showSnippetTooltip"
+                             onSnippetKeydown="this.onSnippetKeydown"
                              disabledTooltip="disabledBlockTooltip"/>
                 </t>
             </div>

addons/html_builder/static/src/sidebar/snippet.js
@@ -8,6 +8,7 @@ export class Snippet extends Component {
         snippetModel: { type: Object },
         snippet: { type: Object },
         onClickHandler: { type: Function },
+        onSnippetKeydown: { type: Function },
         disabledTooltip: { type: String },
     };
 

addons/html_builder/static/src/sidebar/snippet.xml
@@ -11,7 +11,7 @@
          t-on-mouseover="this.onInstallableHover"
          t-on-mouseout="this.onInstallableHover">
         <div class="o_snippet_thumbnail" t-att-data-snippet="this.snippet.name">
-            <button t-if="!this.snippet.isInstallable and !this.snippet.isDisabled" class="o_snippet_thumbnail_area" t-on-click="this.props.onClickHandler" title="Insert snippet"/>
+            <button t-if="!this.snippet.isInstallable and !this.snippet.isDisabled" class="o_snippet_thumbnail_area" t-on-click="this.props.onClickHandler" t-on-keydown="this.props.onSnippetKeydown" t-attf-aria-label="Insert {{this.snippet.title}} block"/>
             <Image t-if="this.snippet.isDisabled" src="'/html_builder/static/img/snippet_disabled.svg'" class="'o_snippet_undroppable'"/>
             <div class="o_snippet_thumbnail_img" t-attf-style="background-image: url({{this.snippet.thumbnailSrc}});"/>
             <span class="o_snippet_thumbnail_title" t-out="this.snippet.title"/>
@@ -19,6 +19,7 @@
                 t-if="this.snippet.isInstallable"
                 class="o_install_btn btn btn-success btn-lg position-absolute start-50 bottom-50 translate-middle-x visually-hidden-focusable z-1"
                 t-on-click="this.onClickInstall"
+                t-on-keydown="this.props.onSnippetKeydown"
                 >
                 Install
             </button>

addons/html_builder/static/src/snippets/add_snippet_dialog.js
@@ -187,6 +187,9 @@ export class AddSnippetDialog extends Component {
      */
     onIframeDocumentKeydown(ev) {
         const hotkey = getActiveHotkey(ev);
+        if (hotkey === "escape") {
+            this.env.dialogData.close({ dismiss: true });
+        }
         if (!["tab", "shift+tab"].includes(hotkey)) {
             return;
         }

addons/html_builder/static/src/snippets/add_snippet_dialog.scss
@@ -33,6 +33,12 @@
 
             min-width: 200px;
             max-width: 250px;
+
+            .list-group-item:focus-visible {
+                box-shadow: inset 0 0 0 2px $o-we-handles-accent-color;
+                border-color: $o-we-handles-accent-color;
+                outline: none;
+            }
         }
     }
 

commit 5dfbdb20b32a37ad06e0511d2910a587311d3e51

[FIX] html_builder: fix hoot tests fails
Fix Hoot fails introduced by [1f86365].

Fixes:
-:WebSuite.test_unit_desktop[@website/builder],
-:WebSuite.test_unit_desktop[@html_builder/background_shape_color/
Connections shape color updates when snippet is dropped next to it],
-:WebSuite.test_unit_desktop[@html_builder/block_tab],
-:WebSuite.test_unit_desktop[@html_builder/operation]

[1f86365]: https://github.com/odoo/odoo/commit/1f863651ed64b207683fc7295e1f7e5a38920dac

runbot-243304
runbot-243316

addons/html_builder/static/src/utils/keyboard_navigation.js
@@ -124,6 +124,9 @@ export function useMatrixKeyNavigation(
 
     onMounted(() => {
         for (const containerEl of getContainerEls()) {
+            if (!containerEl) {
+                continue;
+            }
             const matrix = makeElementsPositionMatrix(containerEl, matrixSiblingSelector);
             matrices.set(containerEl, matrix);
         }
@@ -131,6 +134,9 @@ export function useMatrixKeyNavigation(
 
     onPatched(() => {
         for (const containerEl of getContainerEls()) {
+            if (!containerEl) {
+                continue;
+            }
             if (
                 !matrices.has(containerEl) ||
                 !matrices.get(containerEl).flat().length !==
@@ -145,8 +151,11 @@ export function useMatrixKeyNavigation(
 
     return (ev) => {
         const currentContainerEl = getContainerEls().find((containerEl) =>
-            containerEl.contains(ev.currentTarget)
+            containerEl?.contains(ev.currentTarget)
         );
+        if (!currentContainerEl) {
+            return;
+        }
         const activeMatrixEl = ev.currentTarget.closest(matrixSiblingSelector);
         handleMatrixKeyNavigation(ev, {
             matrix: matrices.get(currentContainerEl),