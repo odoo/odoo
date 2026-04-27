f5a9be824a46ebbfe67b3da8f73752552a0f52e6
Maryam Kia <maki@odoo.com>
2026-04-27 11:15:41 +0000

    [FIX] mail: prevent attachment upload leak on record switch
    Currently, when uploading a bunch of attachments or a big one to the chatter, if
    you click on the pager (e.g. next) before the upload is complete, the
    attachments that have not yet been uploaded are uploaded to the next record.
    Due to the persistence of the Chatter component during record navigation and the
     fact that the `FileUploader` logic is tied to `state.thread`, an async callback
       that finishes after a record switch will attempt to update the currently
       active thread rather than the one that initiated the upload.
    
    With this change we tie uploader lifecycle to a specific record and ensure the
    completion callback only affects that record.
    
    task-5119290
    
    closes odoo/odoo#263144
    
    X-original-commit: 6437b6f67bf3e30aa0f87641f725233fb92757c8
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>
    Signed-off-by: Maryam Kia (maki) <maki@odoo.com>

--- addons/mail/static/src/chatter/web/chatter_patch.js
+++ addons/mail/static/src/chatter/web/chatter_patch.js
@@ -101,6 +101,8 @@ const chatterPatch = {
         /** @type {number|null} */
         this.loadingAttachmentTimeout = null;
         this.subjectInputRef = useRef("subjectInput");
+        /** @type {Map<string, Function>} */
+        this.uploadHandlers = new Map();
         useCustomDropzone(
             this.rootRef,
             MailAttachmentDropzone,
@@ -400,16 +402,34 @@ const chatterPatch = {
         this.load(thread, ["suggestedRecipients"]);
     },
 
-    async onUploaded(data) {
-        await this.attachmentUploader.uploadData(data);
-        if (this.props.hasParentReloadOnAttachmentsChanged) {
-            this.reloadParentView();
+    /**
+     * @param {string} data deprecated, passing thread is enough
+     * @param {import("models").Thread} thread
+     */
+    onUploaded(data, { thread } = {}) {
+        const threadLocalId = thread.localId;
+        if (!this.uploadHandlers.has(threadLocalId)) {
+            const self = this;
+            this.uploadHandlers.set(threadLocalId, async function handleUpload(data) {
+                try {
+                    await self.attachmentUploader.uploadData(data, { thread });
+                    if (!thread.eq(self.state.thread)) {
+                        return;
+                    }
+                    if (self.props.hasParentReloadOnAttachmentsChanged) {
+                        self.reloadParentView();
+                    }
+                    self.state.isAttachmentBoxOpened = true;
+                    if (self.rootRef.el) {
+                        self.rootRef.el.scrollTop = 0;
+                    }
+                    self.state.thread.scrollTop = "bottom";
+                } finally {
+                    self.uploadHandlers.delete(threadLocalId);
+                }
+            });
         }
-        this.state.isAttachmentBoxOpened = true;
-        if (this.rootRef.el) {
-            this.rootRef.el.scrollTop = 0;
-        }
-        this.state.thread.scrollTop = "bottom";
+        return this.uploadHandlers.get(threadLocalId);
     },
 
     async reloadParentView() {
