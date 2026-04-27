27f6854cd04fd35d4eeb2d75a1f0d81663dd7530
Tanguy Quéguineur <taqu@odoo.com>
2026-04-27 14:34:50 +0200

    [FIX] hr_holidays_attendance: count time off in hours for flexible
    **Steps to reproduce**
    - Install `hr_attendance_gantt` and `hr_holidays`
    - Have an employee using a flexible calendar (e.g. 8h/day, 40h/week)
    - Create a time off or this employee in custom hours
    - Go to the attendance gantt view for the day of the time off and notice
    that the progress bar denominator (expected hours) only takes the time
    off into account if it is near the middle of the day.
    
    **Cause**
    `_gantt_compute_max_work_hours_within_interval` calls
    `_attendance_intervals_batch`, which creates attendance intervals
    centered around mid-day for flexible employees, since their actual
    attendances are unknown.
    
    **Change**
    Add specific handling of time off taken in hours to substract the
    hours outside of the virtual attendances from the expected hours.
    
    opw-6102805

--- addons/hr_holidays_attendance/models/hr_attendance.py
+++ addons/hr_holidays_attendance/models/hr_attendance.py
@@ -3,6 +3,7 @@
 
 from odoo import models
 from odoo.osv.expression import AND
+from odoo.addons.resource.models.utils import sum_intervals
 
 
 class HrAttendance(models.Model):
@@ -12,3 +13,17 @@ class HrAttendance(models.Model):
         domain = super()._get_overtime_leave_domain()
         # resource_id = False => Public holidays
         return AND([domain, ['|', ('holiday_id.holiday_status_id.time_type', '=', 'leave'), ('resource_id', '=', False)]])
+
+    def _gantt_compute_max_work_hours_within_interval(self, employee, start, stop):
+        """ Override of `hr_attendance_gantt`. For flexible employees, virtual attendances are centered
+        at noon so hour-based time offs outside that window are not subtracted automatically. """
+        res = super()._gantt_compute_max_work_hours_within_interval(employee, start, stop)
+        employee_sudo = employee.sudo()
+        if not employee_sudo.is_flexible or employee_sudo.is_fully_flexible:
+            return res
+        # For flexible employees, virtual attendances are centered at noon so hour-based time offs outside
+        # that window are not subtracted automatically.
+        attendance_intervals = employee_sudo.resource_calendar_id._work_intervals_batch(start, stop, employee_sudo.resource_id, compute_leaves=False)[employee_sudo.resource_id.id]
+        leaves_intervals = employee_sudo.resource_calendar_id._leave_intervals_batch(start, stop, employee_sudo.resource_id, [('holiday_id.request_unit_hours', '=', True)])[employee_sudo.resource_id.id]
+        uncovered = leaves_intervals - attendance_intervals
+        return max(0.0, res - sum_intervals(uncovered))
