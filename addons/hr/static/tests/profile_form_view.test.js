2c454ed2a67104137bdc9d08f2feddcd2515273e
Quentin Colla (qucol) <qucol@odoo.com>
2026-04-27 13:48:15 +0200

    [FIX] web, hr: apply correct domain when browsing employee forms
    ## Issue
    When browsing employee's forms using the *Next* button (arrow) in the
    upper-right corner, the versions displayed in the statusbar are incorrect:
    some versions of an employee will be missing, and versions from other
    employees will be displayed.
    
    ## Steps to reproduce
    1. Install *Employees* (`hr`)
    2. Create an Employee E1:
        - Contract: January 1 to Indefinite
    3. Create an Employee E2:
        - Contract: January 10 to Indefinite
        - Add a version on January 20 using the `+` sign in the statusbar.
        You should have two versions, one for January 10 and one for January 20.
    4. Go back to the Employee E1's form and click the *Next* button (arrow)
    in the upper right corner to go to E2's form.
    5. **The versions displayed in the statusbar are January 1 and January 20,
    even though E2's versions are January 10 and January 20.**
    6. Go back to E1's form using the *Previous* button.
    7. **All three versions are displayed (January 1, 10 and 20)**
    
    ## Cause
    Since https://github.com/odoo/odoo/commit/a343b26e52d4c0def3505a836e587b9bd9ee09c5,
    the logic to define the domain for the versions statusbar was moved from the `setup`
    method of the `VersionsTimeline` to a `getDomain` method called by the `setup` method
    of the `StatusBarField` component.
    
    https://github.com/odoo/odoo/blob/d76ca3ed350ee7ab9bf252843b25d127319ee07a/addons/web/static/src/views/fields/statusbar/statusbar_field.js#L106
    
    When moving on to a new employee using the *Next* button, the `getDomain`
    method is called but it still uses the props from the first employee, not
    the one we're moving to. This means that the versions from the "current employee"
    will be displayed on the form of the "next employee".
    
    This issue is resolved by passing the most recent props to the `getDomain` call.
    
    opw-6146460

--- addons/hr/static/tests/profile_form_view.test.js
+++ addons/hr/static/tests/profile_form_view.test.js
@@ -39,3 +39,41 @@ test("editing the 'lang' field and saving it triggers a 'reload_context'", async
     await clickSave();
     expect.verifySteps(["reload_context"]);
 });
+
+test("employees should only have their versions in the versions_timeline", async function() {
+    const { env } = await makeMockServer();
+    const [version_1, version_2, version_3] = env["hr.version"].create([
+        { name: "Version 1", date_version: "2026-01-01" },
+        { name: "Version 2", date_version: "2026-01-10" },
+        { name: "Version 3", date_version: "2026-01-20" },
+    ])
+    const [first_employee, second_employee] = env["hr.employee"].create([
+        { version_ids: [version_1] },
+        { version_ids: [version_2, version_3] },
+    ])
+    await mountView({
+        type: "form",
+        resModel: "hr.employee",
+        arch: `
+            <form>
+                <field name="version_id" widget="versions_timeline"/>
+            </form>`,
+        domain: [],
+        resId: first_employee,
+        resIds: [first_employee, second_employee],
+    });
+    // Employee A
+    expect(".o_statusbar_status button:contains('Version 1')").toHaveCount(1);
+    expect(".o_statusbar_status button:contains('Version 2')").toHaveCount(0);
+    expect(".o_statusbar_status button:contains('Version 3')").toHaveCount(0);
+    await contains(".o_pager button.o_pager_next:enabled").click();
+    // Employee B
+    expect(".o_statusbar_status button:contains('Version 1')").toHaveCount(0);
+    expect(".o_statusbar_status button:contains('Version 2')").toHaveCount(1);
+    expect(".o_statusbar_status button:contains('Version 3')").toHaveCount(1);
+    await contains(".o_pager button.o_pager_previous:enabled").click();
+    // Employee A
+    expect(".o_statusbar_status button:contains('Version 1')").toHaveCount(1);
+    expect(".o_statusbar_status button:contains('Version 2')").toHaveCount(0);
+    expect(".o_statusbar_status button:contains('Version 3')").toHaveCount(0);
+})
