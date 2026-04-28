af22f5e5118697e31d82f8ea2a7c23ae2d159be1
sbbh-odoo <sbbh@odoo.com>
2026-04-28 15:19:02 +0530

    [FIX] html_editor, mail: remove select_all_overrides
    Description of the change:
    
    - After this commit [1], selectAll uses getDeepestEditablePosition and
      resolves the deepest editable node correctly; hence, select_all_overrides
      introduced in this commit [2] is no longer needed.
    
    [1]: b127ceaabb9463ffcab3468c071c4214c5cb9fad
    [2]: 9503cf4f1977cfb02a303724e4e245c33e3ed803
    
    task-6112484

--- addons/mail/static/src/core/common/plugin/mention_plugin.js
+++ addons/mail/static/src/core/common/plugin/mention_plugin.js
@@ -1,19 +1,10 @@
 import { Plugin } from "@html_editor/plugin";
-import { closestElement } from "@html_editor/utils/dom_traversal";
 import { generateChannelMentionElement } from "@mail/utils/common/format";
 
 export class MentionPlugin extends Plugin {
     static id = "mention";
     static dependencies = ["baseContainer", "selection", "history"];
     resources = {
-        is_node_editable_predicates: (node) => {
-            for (const { selector } of this.MENTION_SELECTORS) {
-                if (closestElement(node, selector)) {
-                    return true;
-                }
-            }
-        },
-        select_all_overrides: this.selectAll.bind(this),
         on_selectionchange_handlers: this.detectMentions.bind(this),
         selectors_for_feff_providers: () =>
             this.MENTION_SELECTORS.map(({ selector }) => selector).join(", "),
@@ -23,30 +14,6 @@ export class MentionPlugin extends Plugin {
         super.setup();
         /** @type {import("models").Store} */
         this.store = this.services["mail.store"];
-    }
-
-    /**
-     * Extend the selection to include whole mention elements at the borders
-     * so that it doesn't get stuck into the contenteditable=false
-     */
-    selectAll({ anchorNode, anchorOffset, focusNode, focusOffset }) {
-        const SELECTOR = this.MENTION_SELECTORS.map(({ selector }) => selector).join(", ");
-        if (closestElement(anchorNode, SELECTOR)) {
-            const startMention = closestElement(anchorNode, SELECTOR);
-            anchorNode = startMention.parentNode;
-            anchorOffset = Array.prototype.indexOf.call(anchorNode.childNodes, startMention);
-        }
-        if (closestElement(focusNode, SELECTOR)) {
-            const endMention = closestElement(focusNode, SELECTOR);
-            focusNode = endMention.parentNode;
-            focusOffset = Array.prototype.indexOf.call(focusNode.childNodes, endMention) + 1;
-        }
-        this.dependencies.selection.setSelection({
-            anchorNode,
-            anchorOffset,
-            focusNode,
-            focusOffset,
-        });
     }
 
     get MENTION_SELECTORS() {
