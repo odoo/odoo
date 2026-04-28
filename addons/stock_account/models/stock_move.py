c78349b11ed29bfc4164b0ce5f63c742854a2f15
MaximeNoirhomme <noma@odoo.com>
2026-04-28 08:33:03 +0000

    [FIX] stock_account: propagate the partner on the journal entry
    **Steps to reproduce**:
    - Activate analytic accounting on the settings
    - Create an Analytic distribution models for a partner
    - Create a product P with a cost and Inventory Valuation set to Perpetual
    - Create a location L with a Location Type set to Inventory Loss and a Loss Account
    - Create an internal transfer from Stock to location L for product P
    - Confirm it
    - Check the associated journal entry:
    -> The analytic distribution is not set of the move lines
    
    **Cause**:
    While validating the picking:
    https://github.com/odoo/odoo/blob/ae63688e869148ad62ef107bd8f35c4cdb33a190/addons/stock/models/stock_picking.py#L1426
    An account move is created without specifying `partner_id`:
    https://github.com/odoo/odoo/blob/ae63688e869148ad62ef107bd8f35c4cdb33a190/addons/stock_account/models/stock_move.py#L178
    https://github.com/odoo/odoo/blob/ae63688e869148ad62ef107bd8f35c4cdb33a190/addons/stock_account/models/stock_move.py#L200-L205
    This leads to the creation of account move lines, triggering `_inverse_analytic_distribution`:
    https://github.com/odoo/odoo/blob/ae63688e869148ad62ef107bd8f35c4cdb33a190/addons/account/models/account_move_line.py#L1416-L1417
    The method accesses `analytic_distribution` of the `move_line`:
    https://github.com/odoo/odoo/blob/ae63688e869148ad62ef107bd8f35c4cdb33a190/addons/account/models/account_move_line.py#L1410
    which triggers its associate compute method:
    https://github.com/odoo/odoo/blob/ae63688e869148ad62ef107bd8f35c4cdb33a190/addons/account/models/account_move_line.py#L1213
    
    To retrieve the right `analytic_distribution`:
    https://github.com/odoo/odoo/blob/ae63688e869148ad62ef107bd8f35c4cdb33a190/addons/account/models/account_move_line.py#L1224
    By defining this search domain:
    https://github.com/odoo/odoo/blob/ae63688e869148ad62ef107bd8f35c4cdb33a190/addons/analytic/models/analytic_distribution_model.py#L85
    
    if `partner_id` is not in the `vals`, it falls back to False:
    https://github.com/odoo/odoo/blob/ae63688e869148ad62ef107bd8f35c4cdb33a190/addons/analytic/models/analytic_distribution_model.py#L79
    As a result, the distribution linked to the `partner_id!` is not found, since the `partner_id`
    of the vals is determined from the `account.move.line`:
    https://github.com/odoo/odoo/blob/ae63688e869148ad62ef107bd8f35c4cdb33a190/addons/account/models/account_move_line.py#L1237
    which is False since it is not specified while creating the account move.
    
    opw-5918058
    
    closes odoo/odoo#262723
    
    X-original-commit: dad9adc7f2f5fdb56f5f3339571c9d24f9ab34ea
    Signed-off-by: Lancelot Semal (lase) <lase@odoo.com>
    Signed-off-by: Maxime Noirhomme (noma) <noma@odoo.com>

--- addons/stock_account/models/stock_move.py
+++ addons/stock_account/models/stock_move.py
@@ -192,6 +192,7 @@ class StockMove(models.Model):
 
         account_move = self.env['account.move'].sudo().create({
             'ref': joined_refs,
+            'partner_id': self._get_partner_id_for_valuation_lines(),
             'journal_id': self.company_id.account_stock_journal_id.id,
             'line_ids': [Command.create(aml_vals) for aml_vals in aml_vals_list],
             'date': self.env.context.get('force_period_date') or fields.Date.context_today(self),
@@ -199,6 +200,9 @@ class StockMove(models.Model):
         self.env['stock.move'].browse(move_to_link).account_move_id = account_move.id
         account_move._post()
         return account_move
+
+    def _get_partner_id_for_valuation_lines(self):
+        return (self.picking_id.partner_id and self.env['res.partner']._find_accounting_partner(self.picking_id.partner_id).id) or False
 
     def _create_analytic_move(self):
         for move in self:
