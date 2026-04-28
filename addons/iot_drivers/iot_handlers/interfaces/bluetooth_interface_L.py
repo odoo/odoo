42642c830480db0d58c04b35c682b95ea51bf93b
orma-odoo <orma@odoo.com>
2026-04-28 12:33:38 +0000

    [FIX] iot_drivers: restore automatic bluetooth connection
    According to the IoT Box documentation written in 10/2019, supported Bluetooth devices
    should connect automatically whenever they are in range of the IoT Box:
    https://github.com/odoo/documentation/commit/16f2f26f8ae6d9040195c854da337e0dcbbff955#diff-b5974750d9a7f12db80e4922399a7cacd26228dec979ca0fbb504faac386efc8R27
    
    This behavior had broken at some point, requiring manual pairing via
    bluetoothctl on the IoT Box to establish a connection.
    
    Root causes identified and fixed:
    - The interface was forcibly disconnecting all already-connected BLE
      devices at startup, kicking them mid-GATT handshake
    - No BlueZ pairing agent was registered, causing AuthenticationFailed
      during the pairing negotiation
    
    Automatic connection on proximity is now restored.
    
    opw-5473691
    
    closes odoo/odoo#261897
    
    X-original-commit: 2c5c0e10806d061926d9e83ddc2c20546378c9c4
    Signed-off-by: Max Whale (mawh) <mawh@odoo.com>
    Signed-off-by: Maxime Orban (orma) <orma@odoo.com>

--- addons/iot_drivers/iot_handlers/interfaces/bluetooth_interface_L.py
+++ addons/iot_drivers/iot_handlers/interfaces/bluetooth_interface_L.py
@@ -1,6 +1,7 @@
 from gatt import DeviceManager as Gatt_DeviceManager
 from gatt.errors import NotReady
 import dbus
+import dbus.service
 from gi.repository import GLib
 import logging
 from threading import Thread
@@ -11,11 +12,62 @@ _logger = logging.getLogger(__name__)
 
 bluetooth_devices = {}
 
+AGENT_PATH = "/odoo/iot/agent/bluetooth"
+
+
+class BtPairingAgent(dbus.service.Object):
+    def __init__(self, bus):
+        super().__init__(bus, AGENT_PATH)
+
+    @dbus.service.method("org.bluez.Agent1", in_signature="", out_signature="")
+    def Release(self): pass
+
+    @dbus.service.method("org.bluez.Agent1", in_signature="os", out_signature="")
+    def AuthorizeService(self, device, uuid): pass
+
+    @dbus.service.method("org.bluez.Agent1", in_signature="o", out_signature="s")
+    def RequestPinCode(self, device): return "000000"
+
+    @dbus.service.method("org.bluez.Agent1", in_signature="o", out_signature="u")
+    def RequestPasskey(self, device): return dbus.UInt32(0)
+
+    @dbus.service.method("org.bluez.Agent1", in_signature="ouq", out_signature="")
+    def DisplayPasskey(self, device, passkey, entered): pass
+
+    @dbus.service.method("org.bluez.Agent1", in_signature="os", out_signature="")
+    def DisplayPinCode(self, device, pincode): pass
+
+    @dbus.service.method("org.bluez.Agent1", in_signature="ou", out_signature="")
+    def RequestConfirmation(self, device, passkey): pass
+
+    @dbus.service.method("org.bluez.Agent1", in_signature="o", out_signature="")
+    def RequestAuthorization(self, device): pass
+
+    @dbus.service.method("org.bluez.Agent1", in_signature="", out_signature="")
+    def Cancel(self): pass
+
+
+def _register_pairing_agent():
+    try:
+        bus = dbus.SystemBus()
+        agent = BtPairingAgent(bus)
+        agent_manager = dbus.Interface(
+            bus.get_object("org.bluez", "/org/bluez"),
+            "org.bluez.AgentManager1"
+        )
+        agent_manager.RegisterAgent(AGENT_PATH, "NoInputNoOutput")
+        agent_manager.RequestDefaultAgent(AGENT_PATH)
+        _logger.info("BtPairingAgent: Registered at %s", AGENT_PATH)
+        return agent
+    except dbus.exceptions.DBusException:
+        _logger.exception("BtPairingAgent: Registration failed")
+
 
 class GattBtManager(Gatt_DeviceManager):
     def device_discovered(self, device):
         identifier = "bt_%s" % device.mac_address
         if identifier not in bluetooth_devices:
+            _logger.debug("New device discovered: %s alias=%s", identifier, device.alias())
             device.manager = self
             bluetooth_devices[identifier] = device
 
@@ -61,9 +113,14 @@ class BtManager(Thread):
         super().__init__(daemon=True)
 
     def run(self):
+        self._pairing_agent = _register_pairing_agent()
         dm = GattBtManager(adapter_name='hci0')
-        for device in [device_con for device_con in dm.devices() if device_con.is_connected()]:
-            device.disconnect()
+        for device in dm.devices():
+            if device.is_connected():
+                identifier = f"bt_{device.mac_address}"
+                _logger.debug("Already connected device found at startup: %s alias=%s", identifier, device.alias())
+                device.manager = dm
+                bluetooth_devices[identifier] = device
         try:
             dm.start_discovery()
             dm.run()
