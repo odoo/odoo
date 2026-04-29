d327caeeb275f9d3ac10c0f4d1951dbae26ca2fc
fja-odoo <fja@odoo.com>
2026-04-29 04:50:20 +0000

    [PERF] account: allow account condition override
    Currently to exclude accounts from the reconciliation we modify the
    domain by adding a second condition on the field account_id of the
    account_move_line table to exclude these accounts while including them
    in a first condition in the method we override.
    
    This is not very efficient as it prevents the use of indexes.
    This commit add a way to override the account condition to exclude the
    accounts directly.
    
    closes odoo/odoo#263448
    
    X-original-commit: 358ba7a628282540b1e669c650bab32784edf5ba
    Related: odoo/enterprise#116695
    Signed-off-by: Sven Führ (svfu) <svfu@odoo.com>
    Signed-off-by: fja-odoo <fja@odoo.com>

--- addons/account/models/account_bank_statement_line.py
+++ addons/account/models/account_bank_statement_line.py
@@ -502,12 +502,13 @@ class AccountBankStatementLine(models.Model):
             company=self.company_id,
         )
 
-    def _get_default_amls_matching_domain(self, allow_draft=False):
-        self.ensure_one()
-        all_reconcilable_account_ids = self.env['account.account'].sudo().search([
+    def _get_all_reconcilable_account_ids(self):
+        return self.env['account.account'].sudo().search([
             ("company_ids", "child_of", self.company_id.root_id.id),
             ('reconcile', '=', True),
         ]).ids
+
+    def _get_default_amls_matching_domain(self, allow_draft=False):
         state_domain = [('parent_state', '=', 'posted')]
         if allow_draft:
             # Set if bank recon will display draft invoices/bills that have a partner.
@@ -521,7 +522,7 @@ class AccountBankStatementLine(models.Model):
             # Reconciliation domain.
             ('reconciled', '=', False),
             # Domain to use the account_move_line__unreconciled_index
-            ('account_id', 'in', all_reconcilable_account_ids),
+            ('account_id', 'in', self._get_all_reconcilable_account_ids()),
             # Special domain for payments.
             '|',
             ('account_id.account_type', 'not in', ('asset_receivable', 'liability_payable')),
