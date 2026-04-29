fa640e71fae54951155003cc590af09a2a2c48ed
Rémi Rahir (rar) <rar@odoo.com>
2026-04-29 11:02:41 +0000

    [FIX] spreadsheet: avoid multiple server calls on list functions
    How to reproduce:
    
    - Create a spreadsheet with a CRM list and only set 2 cells content
      A1: =odoo.list(1, 1, "id")
      A2: =odoo.list.header(1,"zip")
    - save and reload the spreadsheet and look at the server calls
    
    ⮕ web_search_read called 2 times
    
    The problem is that the datasource methods early return if the datasource
    is already loading  without adding the field to the list to fetch.
    It was partially solved by explicitely adding the field to fetch in the
    *getter* `getListCellValueAndFormat` but not  on `getListHeaderValue`.
    
    This revision ensures that we always add the field to the list to fetch
    in the datasource directly, this responsibility should not be held by
    the plugin getters.
    
    Task-6175523
    
    closes odoo/odoo#265151
    
    X-original-commit: b4888c54bca830ab5695b2b7356aaec9fe1fe9eb
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- addons/spreadsheet/static/tests/lists/list_plugin.test.js
+++ addons/spreadsheet/static/tests/lists/list_plugin.test.js
@@ -967,6 +967,7 @@ test("fetch all and only required fields", async function () {
                     A1: '=ODOO.LIST(1, 1, "foo")', // in the definition
                     A2: '=ODOO.LIST(1, 1, "product_id")', // not in the definition
                     A3: '=ODOO.LIST(1, 1, "invalid_field")',
+                    A4: '=ODOO.LIST.HEADER(1, "bar")',
                 },
             },
         ],
@@ -989,6 +990,7 @@ test("fetch all and only required fields", async function () {
                 expect(args.kwargs.specification).toEqual({
                     id: {},
                     foo: {},
+                    bar: {},
                     product_id: {
                         fields: {
                             display_name: {},
@@ -998,6 +1000,7 @@ test("fetch all and only required fields", async function () {
             }
         },
     });
+    await animationFrame();
     expect.verifySteps(["data-fetched"]);
 });
 
