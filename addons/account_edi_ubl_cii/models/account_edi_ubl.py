a059a2d331bbf2984f2d6cae9cd399bd96635a1c
Jugurtha <jugac@odoo.com>
2026-04-29 15:46:36 +0000

    [IMP] account_edi_ubl_cii: Support receiving Peppol extra fields.
    Currently, Odoo allows sending invoices with additional Peppol fields, but didn't support the receiving.
    This limitation prevents users from receiving fully compliant invoices.
    
    After this commit, users will be able to receive these extra fields if they already created them using Studio.
    
    task-6033667
    
    X-original-commit: 7326c64a402cb8fb118df5470f85b5008d38b331

--- addons/account_edi_ubl_cii/models/account_edi_ubl.py
+++ addons/account_edi_ubl_cii/models/account_edi_ubl.py
@@ -15,6 +15,7 @@ from odoo.addons.account_edi_ubl_cii.models.account_edi_common import (
     UOM_TO_UNECE_CODE,
 )
 from odoo.addons.base.models.res_partner_bank import sanitize_account_number
+from odoo.addons.account_edi_ubl_cii.tools.ubl_20_optional_fields import PEPPOL_INVOICE_OPTIONAL_FIELDS, PEPPOL_INVOICE_OPTIONAL_LINE_FIELDS, PEPPOL_CREDIT_NOTE_OPTIONAL_FIELDS, PEPPOL_CREDIT_NOTE_OPTIONAL_LINE_FIELDS
 
 _logger = logging.getLogger(__name__)
 
@@ -2318,6 +2319,32 @@ class AccountEdiUBL(models.AbstractModel):
             if tax_values:
                 taxes_values.append(tax_values)
 
+    def _import_ubl_invoice_line_add_optional_fields(self, collected_values):
+        line_tree = collected_values['line_tree']
+        invoice = collected_values['invoice']
+
+        if not invoice.is_purchase_document():
+            return {}
+
+        line_fields = {}
+        if invoice.move_type == 'in_invoice':
+            line_fields = PEPPOL_INVOICE_OPTIONAL_LINE_FIELDS
+        elif invoice.move_type == 'in_refund':
+            line_fields = PEPPOL_CREDIT_NOTE_OPTIONAL_LINE_FIELDS
+
+        vals = {}
+        AccountMoveLine = self.env['account.move.line']
+        for field_name, config in line_fields.items():
+            node_value = self._find_value(
+                './/' + '/'.join(config.get('path')),
+                line_tree,
+            )
+            field = AccountMoveLine._fields.get(field_name)
+            if not field or field.type not in config['supported_types'] or not node_value:
+                continue
+            vals[field_name] = node_value
+        return vals
+
     def _import_ubl_invoice_add_invoice_line_values(self, collected_values):
         lines_collected_values = collected_values['lines_collected_values'] = []
         tree = collected_values['tree']
@@ -2468,6 +2495,11 @@ class AccountEdiUBL(models.AbstractModel):
             base_line_kwargs['_create_values']['deferred_end_date'] = deferred_end_date
         if vehicle_id := to_write.get('vehicle_id'):
             base_line_kwargs['_create_values']['vehicle_id'] = vehicle_id
+
+        base_line_kwargs['_create_values'] = {
+            **base_line_kwargs['_create_values'],
+            **self._import_ubl_invoice_line_add_optional_fields(collected_values),
+        }
         return base_line_kwargs
 
     def _import_ubl_invoice_get_allowance_charge_line_kwargs(self, collected_values):
@@ -2653,6 +2685,30 @@ class AccountEdiUBL(models.AbstractModel):
             for base_line in collected_values['base_lines']
             if (not base_line['currency_id'].is_zero(base_line['tax_details']['total_included_currency']) or base_line.get('discount'))
         ]
+
+    def _import_ubl_invoice_optional_fields(self, collected_values):
+        invoice = collected_values.get('invoice')
+        tree = collected_values.get('tree')
+
+        if not invoice.is_purchase_document():
+            return
+
+        invoice_fields = {}
+        if invoice.move_type == 'in_invoice':
+            invoice_fields = PEPPOL_INVOICE_OPTIONAL_FIELDS
+        elif invoice.move_type == 'in_refund':
+            invoice_fields = PEPPOL_CREDIT_NOTE_OPTIONAL_FIELDS
+
+        AccountMove = self.env['account.move']
+        for field_name, config in invoice_fields.items():
+            node_value = self._find_value(
+                './/' + '/'.join(config.get('path')),
+                tree,
+            )
+            field = AccountMove._fields.get(field_name)
+            if not field or field.type not in config['supported_types'] or not node_value:
+                continue
+            collected_values['to_write'][field_name] = node_value
 
     def _import_ubl_invoice_write_collected_values(self, collected_values):
         invoice = collected_values['invoice']
@@ -2918,6 +2974,9 @@ class AccountEdiUBL(models.AbstractModel):
         self._import_ubl_invoice_add_narration(collected_values)
         self._import_ubl_invoice_add_payment_reference(collected_values)
         self._import_ubl_invoice_add_delivery(collected_values)
+
+        # optional fields
+        self._import_ubl_invoice_optional_fields(collected_values)
 
         # invoice_incoterm_id
         self._import_ubl_invoice_add_incoterm_values(collected_values)
