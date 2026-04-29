cdb278a6dad2e0090ec16ba7276a4a8af5fc288b
sawer <sawer@odoo.com>
2026-04-29 11:29:02 +0200

    [FIX] l10n_pl_edi: authorize bills from foreign companies with KSeF
    When we import bills from KSeF, the vendor (Podmiot1) XML tag must
    include a Polish NIP number XML tag.
    The NIP is the base number composing a polish vat number,
    but without the prefix PL. This is true even if the vendor is from
    another country like Luxembourg: if they have a stable organization
    in Poland and sells in Poland - then they have to use a polish NIP
    to use the KSeF and issue their invoices.
    
    Two issues:
    
    We search the vendor by NIP as it was a vat number, but we add
    the vendor_country code as prefix instead of PL. I.e. we search for
    LU012345678 instead of PL012345678.
    
    When we don't find the vendor in the database, we create one using
    the NIP number coming straight from the tag, as it was a vat number.
    I.e. for a partner in Luxembourg, vat will become LU012345678
    instead of PL012345678
    
    ref: https://ksef.podatki.gov.pl/media/4u1bmhx4/information-sheet-on-the-fa-3-logical-structure.pdf
    
    opw-6148039
    
    closes odoo/odoo#261964
    
    Signed-off-by: Paolo Gatti (pgi) <pgi@odoo.com>

--- addons/l10n_pl_edi/tests/test_l10n_pl_edi.py
+++ addons/l10n_pl_edi/tests/test_l10n_pl_edi.py
@@ -558,7 +558,7 @@ class TestL10nPlEdi(AccountTestInvoicingCommon, CronMixinCase):
 
         created_move = self.env['account.move'].search([('l10n_pl_edi_number', '=', '7492091229-20260210-0700A043714A-5E')])
         self.assertTrue(created_move)
-        self.assertEqual(created_move.partner_id.vat, '7492091229')
+        self.assertEqual(created_move.partner_id.vat, 'PL7492091229')
         self.assertEqual(len(created_move), 1)
         self.assertRecordValues(
             created_move, [
@@ -685,3 +685,22 @@ class TestL10nPlEdi(AccountTestInvoicingCommon, CronMixinCase):
                 'tax_ids': self.env['account.chart.template'].ref('vz_kraj_5').ids,
             },
         ])
+
+    def test_import_invoice_from_foreign_country_retrieve_correct_partner(self):
+        partner = self.env["res.partner"].create({
+            'name': "LU Company",
+            'vat': "PL7492091229",
+            'country_id': self.env.ref('base.lu').id,
+        })
+        self._assert_import_invoice('invoice_from_foreign_country.xml', [{
+            'partner_id': partner.id,
+        }])
+
+    def test_import_invoice_from_foreign_country_partner_correctly_created(self):
+        path = 'l10n_pl_edi/tests/import_xmls/invoice_from_foreign_country.xml'
+        with tools.file_open(path, mode='rb') as fd:
+            content = fd.read()
+            invoice_data = self.env['account.move'].l10n_pl_edi_get_ksef_bill_vals_from_xml(content)
+            invoice = self.env['account.move'].create(invoice_data)
+        self.assertEqual(invoice.partner_id.name, "LU Company")
+        self.assertIn("7492091229", invoice.partner_id.vat)
