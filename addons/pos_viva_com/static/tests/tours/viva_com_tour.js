2b415f0bfa4b19946ca9dd01955e5ea638ac61f8
Max Whale <mawh@odoo.com>
2026-04-30 13:17:45 +0000

    [FIX] pos_viva_com: referenced refunds not working
    Referenced refunds in Viva.com require the session ID of the original
    payment to be sent, resulting in that payment being reversed.
    Unfortunately this functionality was broken when a forward-port PR
    (odoo/odoo#236004) mistakenly removed the `parentSessionId` field from
    the request.
    
    This commit restores the `parentSessionId` field, fixing the issue.
    
    X-original-commit: e573bec0e5a5fcea7390bf4688e9bbf7d785ef33

--- addons/pos_viva_com/static/tests/tours/viva_com_tour.js
+++ addons/pos_viva_com/static/tests/tours/viva_com_tour.js
@@ -1,8 +1,40 @@
+/* global posmodel */
+
+import * as FeedbackScreen from "@point_of_sale/../tests/pos/tours/utils/feedback_screen_util";
 import * as ProductScreen from "@point_of_sale/../tests/pos/tours/utils/product_screen_util";
 import * as PaymentScreen from "@point_of_sale/../tests/pos/tours/utils/payment_screen_util";
+import * as TicketScreen from "@point_of_sale/../tests/pos/tours/utils/ticket_screen_util";
 import * as Chrome from "@point_of_sale/../tests/pos/tours/utils/chrome_util";
 import * as Dialog from "@point_of_sale/../tests/generic_helpers/dialog_util";
 import { registry } from "@web/core/registry";
+
+const mockVivaWebhook = () => ({
+    content: "Waiting for Viva payment to be processed",
+    trigger: ".paymentline_status_title",
+    run: async function () {
+        const payment_terminal =
+            posmodel.getPendingPaymentLine("viva_com").payment_method_id.payment_interface;
+
+        // ==> pretend to be viva and send the notification to the POS
+        const resp = await fetch(
+            `/pos_viva_com/notification?company_id=${payment_terminal.pos.company.id}&token=viva_com_test`,
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    EventData: {
+                        TerminalId: "01234543210",
+                        MerchantTrns: `123/${odoo.pos_session_id}`,
+                    },
+                    EventTypeId: 1796,
+                }),
+            }
+        );
+        if (!resp.ok) {
+            throw new Error("Failed to notify Viva webhook");
+        }
+    },
+});
 
 registry.category("web_tour.tours").add("VivaComTour", {
     steps: () =>
@@ -13,6 +45,17 @@ registry.category("web_tour.tours").add("VivaComTour", {
             ProductScreen.clickPayButton(),
             PaymentScreen.isShown(),
             PaymentScreen.clickPaymentMethod("Viva"),
+            mockVivaWebhook(),
+            FeedbackScreen.isShown(),
+            FeedbackScreen.clickNextOrder(),
+            Chrome.clickOrders(),
+            TicketScreen.selectFilter("Paid"),
+            TicketScreen.selectOrder("0001"),
+            TicketScreen.confirmRefund(),
             PaymentScreen.isShown(),
+            PaymentScreen.clickPaymentMethod("Viva"),
+            PaymentScreen.clickRefundButton(),
+            mockVivaWebhook(),
+            FeedbackScreen.isShown(),
         ].flat(),
 });
