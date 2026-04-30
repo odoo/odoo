fe38892d82f4784b17f6389c39d4fb04f31eb19c
Brice bib Bartoletti <bib@odoo.com>
2026-04-30 18:29:25 +0200

    [MOV] odoo,auth_oauth: fragment_to_query_string
    The aim of this commit is to allow module unrelated to auth_oauth to
    import fragment_to_query_string.
    
    task-id: 6071808

--- odoo/http/requestlib.py
+++ odoo/http/requestlib.py
@@ -59,6 +59,28 @@ def borrow_request():
         _request_stack.push(req)
 
 
+def fragment_to_query_string(func):
+    @functools.wraps(func)
+    def wrapper(self, *a, **kw):
+        kw.pop('debug', False)
+        if not kw:
+            return Response("""<html><head><script>
+                var l = window.location;
+                var q = l.hash.substring(1);
+                var r = l.pathname + l.search;
+                if(q.length !== 0) {
+                    var s = l.search ? (l.search === '?' ? '' : '&') : '?';
+                    r = l.pathname + l.search + s + q;
+                }
+                if (r == l.pathname) {
+                    r = '/';
+                }
+                window.location = r;
+            </script></head><body></body></html>""")
+        return func(self, *a, **kw)
+    return wrapper
+
+
 def is_cors_preflight(request: Request, endpoint: Endpoint) -> bool:
     return (
         request.httprequest.method == 'OPTIONS'
