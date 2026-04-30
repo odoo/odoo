e99821ba550b747a384fbb82a695333b76271a5a
Jinjiu Liu <jili@odoo.com>
2026-04-30 17:22:11 +0200

    [FIX] html_editor: close table menu when one or more cells are selected
    Before this commit: the table menu is shown when one or more cells are
    selected, which is overlapping with the toolbar.
    
    After this commit: the table menu is only shown when there's no selected
    cell.
    
    task-5973036

--- addons/html_editor/static/src/main/table/table_ui_plugin.js
+++ addons/html_editor/static/src/main/table/table_ui_plugin.js
@@ -88,6 +88,7 @@ export class TableUIPlugin extends Plugin {
             }
         };
         this.addDomListener(this.document, "scroll", closeMenus, true);
+        this.addDomListener(this.document, "click", this.onClick);
         this.tableMenuMethods = Object.assign({}, ...this.getResource("table_menu_commands"));
         this.addDomListener(this.document, "touchend", this.onTouchEnd);
     }
@@ -147,8 +148,25 @@ export class TableUIPlugin extends Plugin {
         this.activeTd = false;
     }
 
+    onClick() {
+        const selectedTds = this.document.querySelectorAll(".o_selected_td");
+        // If there is a selected td, we hide the table menu as it manipulates
+        // the whole table and shouldn't overlap with the toolbar.
+        if (selectedTds.length !== 0) {
+            this.setActiveTd(null);
+            return;
+        }
+    }
+
     onMouseMove(ev) {
         const target = ev.target;
+        const selectedTds = this.document.querySelectorAll(".o_selected_td");
+        // If there is a selected td, we hide the table menu as it manipulates
+        // the whole table and shouldn't overlap with the toolbar.
+        if (selectedTds.length !== 0) {
+            this.setActiveTd(null);
+            return;
+        }
         if (this.isMenuOpened) {
             return;
         }
