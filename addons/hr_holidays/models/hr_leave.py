0da011145f8dc2f21be655d2c2e082f2bafc8716
josm-odoo <josm@odoo.com>
2026-05-01 14:21:48 +0000

    [FIX] hr_holidays: hr.leave.type.leave_validation_type = 'both' doesnt fallback to responsible_ids
    Issue:
    When ('both','By Employee's Approver and Time Off Officer') is selected on a new HR Leave Type  it does not fall back to the responsible_ids or “Notify HR”.
    
    Steps:
    1) Setup a neutralized outgoing mail server
    2) install hr_holidays
    3) make a new hr.leave.Type (Approval) with ('both','By Employee's Approver and Time Off Officer') and select a 'Notified Time Off Officer'(responsible_ids)
    4) select an emplyee with a reelated user and remove the coach, manager, and responsible 'Time Off'.
    5) save
    6) Sign in as the employee, make a time off request under the new Type
    7) No email
    
    Fix:
    Add a conditional with the lowest priority to fall back to responsible_ids
    
    opw-6101637
    
    closes odoo/odoo#263399
    
    X-original-commit: 1eb3304655a378fb12de24fce2b8db8955c05236
    Signed-off-by: Romain Carlier (romc) <romc@odoo.com>

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -1296,6 +1296,8 @@ Attempting to double-book your time off won't magically make your vacation 2x be
                 responsible = self.employee_id.leave_manager_id
             elif self.employee_id.parent_id.user_id:
                 responsible = self.employee_id.parent_id.user_id
+            elif self.holiday_status_id.responsible_ids:
+                responsible = self.holiday_status_id.responsible_ids
         elif self.validation_type == 'hr' or (self.validation_type == 'both' and self.state == 'validate1'):
             if self.holiday_status_id.responsible_ids:
                 responsible = self.holiday_status_id.responsible_ids
