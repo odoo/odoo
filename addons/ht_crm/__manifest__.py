9c247343dab6ce6cf023c45c04a58b2ec0f5312d
Gural <122676712+GuralSyncalypse@users.noreply.github.com>
2026-05-05 16:11:06 +0700

    Removed ht_crm_ui

--- addons/ht_crm/__manifest__.py
+++ addons/ht_crm/__manifest__.py
@@ -10,13 +10,12 @@
     'website': "https://www.htland.net.vn",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'CRM/Sales',
     'version': '1.9',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'web'],
 
     # always loaded
     'data': [
