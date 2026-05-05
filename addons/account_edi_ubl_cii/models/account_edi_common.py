99750e838a1e5cd94d8acf67253ec0d3d52fefea
Yassien Ghoniem <yagho@odoo.com>
2026-05-05 16:56:41 +0200

    [FIX] account_edi_ubl_cii: Fix a wrong tax exemption reason called
    A tax exemption reason code that does not pass the peppol validation was added
    in the xml generation in this task-id-5905176
    task-id-none

--- addons/account_edi_ubl_cii/models/account_edi_common.py
+++ addons/account_edi_ubl_cii/models/account_edi_common.py
@@ -196,7 +196,7 @@ class AccountEdiCommon(models.AbstractModel):
 
         cocontractant_note = self._get_belgian_cocontractant_note(invoice, customer)
         if cocontractant_note:
-            return create_dict(tax_category_code='AE', tax_exemption_reason_code='VATEX-EU-AE - Reverse charge', tax_exemption_reason=cocontractant_note)
+            return create_dict(tax_category_code='AE', tax_exemption_reason_code='VATEX-EU-AE', tax_exemption_reason=cocontractant_note)
 
         if supplier.country_id == customer.country_id:
             if not tax or tax.amount == 0:
