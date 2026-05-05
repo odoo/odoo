ee784afbbd372acb57a13c5404c77729f935ed52
nfl <nfl@odoo.com>
2026-05-05 12:58:31 +0000

    [FIX] base: _lookup_xmlids not flushing before executing SQL.
    Steps to Reproduce:
    
    - write on ir.model.data to modify noupdate.
    - _lookup_xmlids still returns the old noupdate value.
    
    Example:
    In [1]: imd = self.env['ir.model.data']
    
    In [2]: xml_id = self.env['ir.model.data'].search([], limit=1)
    
    In [3]: imd._lookup_xmlids([xml_id.complete_name], self.env[xml_id.model])
    Out[3]: [(18603, 'auth_signup', 'action_send_password_reset_instructions', 'ir.actions.server', 149, False, 149)]
    
    In [4]: xml_id.write({'noupdate': not xml_id.noupdate})
    Out[4]: True
    
    In [5]: imd._lookup_xmlids([xml_id.complete_name], self.env[xml_id.model])
    Out[5]: [(18603, 'auth_signup', 'action_send_password_reset_instructions', 'ir.actions.server', 149, False, 149)]
    
    In [6]: imd.flush_model()
    
    In [7]: imd._lookup_xmlids([xml_id.complete_name], self.env[xml_id.model])
    Out[7]: [(18603, 'auth_signup', 'action_send_password_reset_instructions', 'ir.actions.server', 149, True, 149)]
    
    Issue:
    
    - _lookup_xmlids is returning values from ir.model.data executing an SQL query w/o flushing.
    
    Fix:
    
    - Add flushing in _lookup_xmlids.
    
    closes odoo/odoo#262940
    
    X-original-commit: 5b85287ec4ea9f1b51e0f33402900777dfeeb725
    Signed-off-by: Chong Wang (cwg) <cwg@odoo.com>

--- odoo/addons/base/models/ir_model.py
+++ odoo/addons/base/models/ir_model.py
@@ -2344,6 +2344,7 @@ class IrModelData(models.Model):
 
         # query xml_ids by prefix
         result = []
+        self.flush_model()
         cr = self.env.cr
         for prefix, suffixes in bymodule.items():
             query = """
