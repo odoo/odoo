2316ba7ddf28cb37ab5278515ad093212453f765
Léo Gizard <legi@odoo.com>
2026-05-05 10:21:08 +0200

    [FIX] account{,edi_ubl_cii}: fixing ubl import with included taxes
    **PROBLEMS**
    1. On a company with taxes with price_include = True, we fail to retrieve a tax when importing a ubl.
    2. The price_unit adjustement for when importing price-included taxes doesn't account for quantity.
    
    **STEP TO REPRODUCE**
    1. Have a setup where the tax are only price_include.
    2. Import a ubl, the taxes will not be retrieved.
    3. Run odoo only with the fix for problem 1, and import the same ubl with some invoice quantity != 1
    4. Notice the price unit are messed up for lines with quantity != 1
    (odoo tries to correct the untaxed amount with a line, but this doesn't fixes the tax).
    
    opw-6159394
    
    closes odoo/odoo#262686
    
    Signed-off-by: Léo Gizard (legi) <legi@odoo.com>

--- addons/account_edi_ubl_cii/tests/test_ubl_import_bis3_invoice_be_retrieve_tax.py
+++ addons/account_edi_ubl_cii/tests/test_ubl_import_bis3_invoice_be_retrieve_tax.py
@@ -23,8 +23,8 @@ class TestUblImportBis3InvoiceBERetrieveTax(TestUblImportBis3InvoiceBE):
                     'tax_ids': [],
                 },
                 {
-                    'quantity': 1.0,
-                    'price_unit': 500.0,
+                    'quantity': 5.0,
+                    'price_unit': 100.0,
                     'tax_ids': [],
                 },
             ],
@@ -55,8 +55,8 @@ class TestUblImportBis3InvoiceBERetrieveTax(TestUblImportBis3InvoiceBE):
                     'tax_ids': tax_21.ids,
                 },
                 {
-                    'quantity': 1.0,
-                    'price_unit': 500.0,
+                    'quantity': 5.0,
+                    'price_unit': 100.0,
                     'tax_ids': tax_21.ids,
                 },
             ],
@@ -161,8 +161,8 @@ class TestUblImportBis3InvoiceBERetrieveTax(TestUblImportBis3InvoiceBE):
                     'tax_ids': tax_21_1.ids,
                 },
                 {
-                    'quantity': 1.0,
-                    'price_unit': 500.0,
+                    'quantity': 5.0,
+                    'price_unit': 100.0,
                     'tax_ids': tax_21_2.ids,
                 },
             ],
@@ -206,3 +206,39 @@ class TestUblImportBis3InvoiceBERetrieveTax(TestUblImportBis3InvoiceBE):
             journal=self.company_data['default_journal_sale'],
         )
         self.assertEqual(bill.invoice_line_ids.tax_ids, tax_21)
+
+    def test_partial_import_tax_included_invoice(self):
+        tax_21 = self.percent_tax(21.0, price_include_override='tax_included')
+
+        invoice = self._import_invoice_as_attachment_on(
+            test_name='test_partial_import_tax_manual_tax_amounts',
+            journal=self.company_data['default_journal_sale'],
+        )
+
+        self.assertRecordValues(
+            invoice.invoice_line_ids,
+            [
+                {
+                    'quantity': 1.0,
+                    'price_unit': 605.0,
+                    'discount': 0.0,
+                    'tax_ids': tax_21.ids,
+                },
+                {
+                    'quantity': 5.0,
+                    'price_unit': 121.0,
+                    'discount': 0.0,
+                    'tax_ids': tax_21.ids,
+                },
+            ],
+        )
+        self.assertRecordValues(
+            invoice,
+            [
+                {
+                    'amount_untaxed': 1000.0,
+                    'amount_tax': 210.01,
+                    'amount_total': 1210.01,
+                },
+            ],
+        )
