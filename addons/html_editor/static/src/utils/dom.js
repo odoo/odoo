222cb6ab7390c05d7e2fb69420357ff96d918ae0
dhba <dhba@odoo.com>
2026-05-05 18:09:42 +0530

    [FIX] html_editor: handle &nbsp; for syntax highlighting
    Step to reproduce:
    
    - Go to To-Do → Create New
    - Type text with multiple consecutive spaces in the same line
    - In the same line → insert a /code block
    
    Issue:
    Multiple spaces are converted into `&nbsp;` inside the code block.
    
    Cause:
    When the code block is processed for syntax highlighting, its `innerHTML` is
    used as the source text. During this process, `&nbsp;` is not handled as a
    result it remains as literal text, so syntax highlighting displays `&nbsp;`
    instead of a normal space.
    
    Solution:
    Convert &nbsp; into a normal space before the content is used for syntax
    highlighting.
    
    task-6184686
    
    closes odoo/odoo#263053
    
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/html_editor/static/src/utils/dom.js
+++ addons/html_editor/static/src/utils/dom.js
@@ -352,9 +352,11 @@ export function splitTextNode(textNode, offset, originalNodeSide = DIRECTIONS.RI
  * @param {import("@html_editor/core/selection_plugin").Cursors} [cursors]
  */
 export function removeInvisibleWhitespace(el, cursors) {
-    const [countLeadingWhitespace, countTrailingWhitespace] = [/^\s+/, /\s+$/].map(
-        (regex) => (node) => node?.textContent.match(regex)?.[0]?.length || 0
-    );
+    const whitespaceRegex = /[^\S\u00A0\uFEFF]/;
+    const [countLeadingWhitespace, countTrailingWhitespace] = [
+        new RegExp(`^${whitespaceRegex.source}+`),
+        new RegExp(`${whitespaceRegex.source}+$`),
+    ].map((regex) => (node) => node?.textContent.match(regex)?.[0]?.length || 0);
     const isInlineElement = (node) => node?.nodeType === Node.ELEMENT_NODE && !isBlock(node);
     const textChildren = descendants(el).filter((child) => child.nodeType === Node.TEXT_NODE);
     let removedTrailingSpaceBefore = false;
@@ -386,8 +388,8 @@ export function removeInvisibleWhitespace(el, cursors) {
                 leadingWhitespace,
                 child.textContent.length - trailingWhitespace || leadingWhitespace
             )
-            .replace(/^\s+/, " ")
-            .replace(/\s+$/, " ");
+            .replace(new RegExp(`^${whitespaceRegex.source}+`), " ")
+            .replace(new RegExp(`${whitespaceRegex.source}+$`), " ");
         if (!child.textContent) {
             child.remove();
         }
