MoLeap/odooo

[FIX] point_of_sale: compute description for product with attributes
Step to reproduce:
- install "purchase_stock" and "point_of_sale"
- Create a product attribute
-> Display Type: Radio Button
-> Variant Creation Mode: Never create variants
-> Add two attribute values, ensuring that one includes a 'is_custom' field.
- Create a product and assign this attribute to it.
- Enable the MTO (Make To Order) route.
- Add a vendor to the product.
- Create and confirm a POS order for this product. (add text for custom attr)
- Observe that a Purchase Order is generated automatically.

Observation:
- the PO does not have that text in description

Cause:
- After commit [1], `description_picking` became a computed field.
- Previously, its value (`product_description_variants`) was set in
  `_prepare_procurement_values`.
- That key is no longer used for this purpose, so the information
  is not propagated and the data is lost.

Fix:
- The description is computed for pos orders

[1] https://github.com/odoo/odoo/commit/6b2d3af64a076654e04494972acc4c42d7c54bd8

opw-5969378

addons/point_of_sale/models/stock_picking.py
@@ -347,3 +347,23 @@ class StockMove(models.Model):
                             if existing_lot:
                                 move._update_reserved_quantity(qty, move.location_id, lot_id=existing_lot)
                                 continue
+
+    @api.depends("product_id")
+    def _compute_description_picking(self):
+        super()._compute_description_picking()
+        seen = set()
+        for move in self:
+            if not (pos_order := move.group_id.pos_order_id) or move.description_picking_manual:
+                continue
+
+            product = move.product_id
+            line = pos_order.lines.filtered(
+                lambda l: l.product_id == product
+                and l.id not in seen
+                and any(av.attribute_id.create_variant == "no_variant" for av in l.attribute_value_ids)
+            )[:1]
+
+            if line and move.description_picking == product.display_name:
+                extra = line.full_product_name.replace(product.name, "", 1).strip()
+                move.description_picking = f"\n{extra}" if extra else ""
+                seen.add(line.id)

odoo/odoo

X-original-commit: e53dae2cf46e580720111f4c72b03dce8a7a898b

addons/point_of_sale/models/stock_picking.py
@@ -301,3 +301,23 @@ class StockMove(models.Model):
                             if existing_lot:
                                 move._update_reserved_quantity(qty, move.location_id, lot_id=existing_lot)
                                 continue
+
+    @api.depends("product_id")
+    def _compute_description_picking(self):
+        super()._compute_description_picking()
+        seen = set()
+        for move in self:
+            if not (pos_order := move.reference_ids.pos_order_ids) or move.description_picking_manual:
+                continue
+
+            product = move.product_id
+            line = pos_order.lines.filtered(
+                lambda l: l.product_id == product
+                and l.id not in seen
+                and any(av.attribute_id.create_variant == "no_variant" for av in l.attribute_value_ids)
+            )[:1]
+
+            if line and move.description_picking == product.display_name:
+                extra = line.full_product_name.replace(product.name, "", 1).strip()
+                move.description_picking = f"\n{extra}" if extra else ""
+                seen.add(line.id)