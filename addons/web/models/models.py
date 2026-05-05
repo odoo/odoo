odoo/odoo

commit 14d3893c763f6413581e7f36099cee0adb2caa83

[PERF] web: respect limit during onchange fetch
Problem:
During an `onchange` call, if a field is defined in the `fields_spec` with a `limit` attribute, the `fetch` method doesn't respect it, and will fetch all records satisfying the domain. In certain circumstances, this leads to slow requests.

Solution:
Enforce the `limit` when fetching if it is present.

Steps to reproduce:
- Have a product with many quant records
1. Open a picking for this product in Barcode
2. Change the lot/serial

The frontend will send an `onchange` request that includes `product_stock_quant_ids` in the `fields_spec` (with default `limit` 40). Odoo will fetch all quants for this product regardless of the limit, and the request will take a while to resolve.

Benchmark:
# of quants | Before | After
17193 | ~9s | ~400ms

opw-6041705

closes odoo/odoo#259983

Signed-off-by: Krzysztof Magusiak (krma) <krma@odoo.com>

addons/web/models/models.py
@@ -3,7 +3,6 @@ from typing import Dict, List
 
 import babel.dates
 import base64
-import copy
 import itertools
 import json
 import pytz
@@ -1219,7 +1218,9 @@ class RecordSnapshot(dict):
             if 'context' in self.fields_spec[field_name]:
                 lines = lines.with_context(**self.fields_spec[field_name]['context'])
             sub_fields_spec = self.fields_spec[field_name].get('fields') or {}
-            self[field_name] = {line.id: RecordSnapshot(line, sub_fields_spec) for line in lines}
+            limit = self.fields_spec[field_name].get('limit')
+            limited_lines = lines[:limit] if limit else lines
+            self[field_name] = {line.id: RecordSnapshot(line, sub_fields_spec) for line in limited_lines}
         else:
             self[field_name] = self.record[field_name]
 

commit 3515c5db0e3132a85c057ffb0a884c7d7a7d82b4

[REV] web: process all BoM components regardless of page size limit
Bug introduced in: https://github.com/odoo/odoo/commit/14d3893c763f6413581e7f36099cee0adb2caa83

Steps to reproduce the bug:
- Create a BoM with more than 40 components
- Create a manufacturing order with this BoM

Problem:
Only the first 40 components are taken into account and their moves are created; the remaining ones are not created.

opw-6186544

closes odoo/odoo#262698

Forward-port-of: odoo/odoo#262692
Signed-off-by: Adrien Widart (awt) <awt@odoo.com>
Signed-off-by: Djamel Touati (otd) <otd@odoo.com>

addons/web/models/models.py
@@ -2216,9 +2216,7 @@ class RecordSnapshot(dict):
             if 'context' in self.fields_spec[field_name]:
                 lines = lines.with_context(**self.fields_spec[field_name]['context'])
             sub_fields_spec = self.fields_spec[field_name].get('fields') or {}
-            limit = self.fields_spec[field_name].get('limit')
-            limited_lines = lines[:limit] if limit else lines
-            self[field_name] = {line.id: RecordSnapshot(line, sub_fields_spec) for line in limited_lines}
+            self[field_name] = {line.id: RecordSnapshot(line, sub_fields_spec) for line in lines}
         else:
             self[field_name] = self.record[field_name]
 