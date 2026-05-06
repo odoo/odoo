28fffcdf21b07086d377d26bff5664426f4ebcb2
syna-odoo <syna@odoo.com>
2026-05-06 17:12:19 +0530

    [FIX] website: restrict top level menu as mega menu
    Issue:
    -------
    After the fix:
    https://github.com/odoo/odoo/commit/f1557211d9e7f83761bb36e4800e4c2f62b234c5
    we can't create a child menu for a mega menu or a menu can't be a mega menu when
    there's an existing child menu except the case of top level menu i.e;
    (url: /default-main-menu) and that menu will have no parent_id obviously...
    
    Now, as per the above pr conditions the top level can be set as mega menu since
    it has no parent id. And in version 17.3 in the pr
    https://github.com/odoo/odoo/commit/47af533e9f5f721b63570d3b301951f3855384a1
    a 'Jobs' menu is being created and its parent_id refers to that top level menu
    which we have set as mega menu. And when the records gets validated during
    migration the database will get blocked.
    
    Solution:
    -----------
    Restrict the user by throwing the same user error, when checking/selecting
    the top level menu as mega menu since it has existing child menus.
    
    Step to reproduce:
    -----------------------
    1. Create a database in version 17.0 with 'website_hr_recruitment' installed.
    2. Go to website menus, set a top level menu(/default-main-menu) as mega menu.
    3. Migrate the database to version 18.0 or more.
    
    Traceback:
    ```
    File "/home/odoo/src/odoo/18.0/odoo/models.py", line 5297, in _create
        records._validate_fields(name for data in data_list for name in data['stored'])
      File "/home/odoo/src/odoo/18.0/odoo/models.py", line 1636, in _validate_fields
        check(self)
      File "/home/odoo/src/odoo/18.0/addons/website/models/website_menu.py", line 95, in _validate_parent_menu
        raise UserError(_("A mega menu cannot have a parent or child menu."))
    odoo.exceptions.UserError: A mega menu cannot have a parent or child menu.
    
     File "/home/odoo/src/odoo/18.0/odoo/tools/convert.py", line 603, in _tag_root
        raise ParseError('while parsing %s:%s, somewhere inside\n%s' % (
    odoo.tools.convert.ParseError: while parsing /home/odoo/src/odoo/18.0/addons/website_hr_recruitment/data/config_data.xml:13, somewhere inside
    <record id="website_menu_jobs" model="website.menu">
                <field name="name">Jobs</field>
                <field name="url">/jobs</field>
                <field name="parent_id" ref="website.main_menu"/>
                <field name="sequence">59</field>
            </record>
    ```
    
    OPW - 6094298

--- addons/website/models/website_menu.py
+++ addons/website/models/website_menu.py
@@ -77,7 +77,7 @@ class Menu(models.Model):
         - Menus with children cannot be added as a submenu under another menu.
         """
         for record in self:
-            parent_menu = record.parent_id.sudo() if record.parent_id else None
+            parent_menu = record.parent_id.sudo() if record.parent_id else self.env[self._name]
 
             # Check hierarchy level
             level = 0
@@ -88,14 +88,13 @@ class Menu(models.Model):
                 if level > 2:
                     raise UserError(_("Menus cannot have more than two levels of hierarchy."))
 
-            if parent_menu:
-                # Mega menu constraint
-                if parent_menu.is_mega_menu or (record.is_mega_menu and (parent_menu.parent_id or record.child_id)):
-                    raise UserError(_("A mega menu cannot have a parent or child menu."))
+            # Mega menu constraint
+            if parent_menu.is_mega_menu or (record.is_mega_menu and (parent_menu.parent_id or record.child_id)):
+                raise UserError(_("A mega menu cannot have a parent or child menu."))
 
-                # Submenu structure constraint
-                if record.child_id and (parent_menu.parent_id or record.child_id.child_id):
-                    raise UserError(_("Menus with child menus cannot be added as a submenu."))
+            # Submenu structure constraint
+            if record.child_id and (parent_menu.parent_id or record.child_id.child_id):
+                raise UserError(_("Menus with child menus cannot be added as a submenu."))
 
     @api.model_create_multi
     def create(self, vals_list):
