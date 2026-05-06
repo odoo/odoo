82cbdb3430c5bf05d0bd046fcda8e19341cb444d
Jean Schoenlaub <jesc@odoo.com>
2026-05-06 15:25:40 +0200

    wrong todo

--- addons/mail/static/tests/thread/message_highlight.test.js
+++ addons/mail/static/tests/thread/message_highlight.test.js
@@ -42,8 +42,7 @@ test("can highlight messages that are not yet loaded", async () => {
     await isInViewportOf(".o-mail-Message:contains(message 100)", ".o-mail-Thread");
 });
 
-test.tags("owl3");
-test.todo("can highlight message (slow ref registration)", async () => {
+test("can highlight message (slow ref registration)", async () => {
     disableAnimations();
     const pyEnv = await startServer();
     const channelId = pyEnv["discuss.channel"].create({ name: "general" });
