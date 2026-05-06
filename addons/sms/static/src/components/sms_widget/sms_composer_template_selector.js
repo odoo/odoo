a4248b3fd9efc2a32fa0cd7074720823c2ec6f8e
Dario Liotta (dalio) <dalio@odoo.com>
2026-05-06 15:32:23 +0200

    [IMP] sms: fix scheduling behaviour and improve UI alignment
    The SMS scheduling was previously non-functional due to the lack of
    proper data propagation between the composer wizard and the technical
    SMS records.
    
    - Added a _compute_scheduled_date() in the SmsComposer class to fix the
      broken scheduling flow in the SMS composer wizard.
    - Refactored the UI: moved the "Insert Field" (dynamic placeholder)
      button inside the text area container.
    - Added a unit test (TestSMSComposerSchedule) to verify the correct
      creation of outgoing SMS with a deferred date.
    
    task-6178288

---
+++ addons/sms/static/src/components/sms_widget/sms_composer_template_selector.js
@@ -0,0 +1,51 @@
+/** @odoo-module **/
+
+import { MailComposerTemplateSelector } from "@mail/core/web/mail_composer_template_selector";
+import { patch } from "@web/core/utils/patch";
+import { SelectCreateDialog } from "@web/views/view_dialogs/select_create_dialog";
+
+patch(MailComposerTemplateSelector.prototype, {
+    async fetchTemplates() {
+        if (this.props.record.resModel === 'sms.composer') {
+            const fields = ["display_name"];
+            const domain = [["model", "=", this.props.record.data.render_model]];
+            
+            const templates = await this.orm.searchRead("sms.template", domain, fields, { limit: this.limit });
+            this.state.templates = templates;
+        } else {
+            return super.fetchTemplates(...arguments);
+        }
+    },
+
+    onSelectTemplateSearchMoreBtnClick() {
+        if (this.props.record.resModel === 'sms.composer') {
+            this.env.services.dialog.add(SelectCreateDialog, {
+                resModel: "sms.template",
+                title: this.env._t("Select a Template"),
+                multiSelect: false,
+                noCreate: true,
+                domain: [["model", "=", this.props.record.data.render_model]],
+                onSelected: async templateIds => {
+                    await this.props.record.update({
+                        template_id: { id: templateIds[0] },
+                    });
+                },
+            });
+        } else {
+            return super.onSelectTemplateSearchMoreBtnClick(...arguments);
+        }
+    },
+
+    async onManageTemplateBtnClick() {
+        if (this.props.record.resModel === 'sms.composer') {
+            const action = await this.action.loadAction("sms.sms_template_action");
+            action.context = {
+                search_default_model: this.props.record.data.model,
+                default_model: this.props.record.data.model,
+            };
+            return this.action.doAction(action);
+        } else {
+            return super.onManageTemplateBtnClick(...arguments);
+        }
+    }
+});
