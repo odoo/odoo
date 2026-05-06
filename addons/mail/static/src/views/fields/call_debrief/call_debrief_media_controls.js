a155f0f70f2ae5633efeb5b7379e488b1f57b313
Brieuc-brd <brd@odoo.com>
2026-05-06 08:50:33 +0200

    [IMP] mail: fine-tune call debrief design
    This commit redesigns the call debrief field to make it more
    user-friendly and intuitive, while also improving its usability on
    mobile devices.
    
    task-6119464

--- addons/mail/static/src/views/fields/call_debrief/call_debrief_media_controls.js
+++ addons/mail/static/src/views/fields/call_debrief/call_debrief_media_controls.js
@@ -1,4 +1,7 @@
-import { Component, props, signal } from "@odoo/owl";
+import { Component, props } from "@odoo/owl";
+import { formatDuration } from "@mail/views/fields/call_debrief/call_debrief_utils";
+import { _t } from "@web/core/l10n/translation";
+import { isMobileOS } from "@web/core/browser/feature_detection";
 
 export class CallDebriefMediaControls extends Component {
     static template = "mail.CallDebriefMediaControls";
@@ -10,36 +13,67 @@ export class CallDebriefMediaControls extends Component {
         playbackRates: { type: Array },
         currentTime: { type: Number },
         totalDuration: { type: Number },
-        media: { type: Object, optional: true },
+        mediaUrl: { type: String, optional: true },
         onTogglePlay: { type: Function },
         onSeek: { type: Function },
         onSetPlaybackRate: { type: Function },
         onSetVolume: { type: Function },
         onToggleMute: { type: Function },
         feedback: { type: Object, optional: true },
+        hasVideo: { type: Boolean, optional: true },
+        isFullscreen: { type: Boolean, optional: true },
+        onToggleFullscreen: { type: Function, optional: true },
     };
 
     props = props();
 
     setup() {
-        this.isVolumeSliderVisible = signal(false);
+        this.isMobileOS = isMobileOS();
+    }
+
+    formatDuration(seconds) {
+        return formatDuration(seconds, this.props.totalDuration);
+    }
+
+    downloadMedia() {
+        window.open(this.props.mediaUrl + "?download=1", "_blank");
+    }
+
+    get formattedTotalDuration() {
+        return this.formatDuration(this.props.totalDuration);
     }
 
     get volumeIconClass() {
         if (this.props.isMuted || this.props.volume === 0) {
-            return "fa fa-volume-off";
+            return "fa-volume-off";
         }
         if (this.props.volume < 0.5) {
-            return "fa fa-volume-down";
+            return "fa-volume-down";
         }
-        return "fa fa-volume-up";
+        return "fa-volume-up";
     }
 
-    showVolumeSlider() {
-        this.isVolumeSliderVisible.set(true);
+    get playPauseTooltip() {
+        return !this.isMobileOS ? (this.props.isPlaying ? _t("Pause (K)") : _t("Play (K)")) : '';
     }
 
-    hideVolumeSlider() {
-        this.isVolumeSliderVisible.set(false);
+    get skipBackwardTooltip() {
+        return !this.isMobileOS ? 'Skip backward (J)' : '';
+    }
+
+    get skipForwardTooltip() {
+        return !this.isMobileOS ? 'Skip forward (L)' : '';
+    }
+
+    get muteTooltip() {
+        return !this.isMobileOS ? (this.props.isMuted ? _t("Unmute (M)") : _t("Mute (M)")) : '';
+    }
+
+    get fullscreenTooltip() {
+        return !this.isMobileOS ? (this.props.isFullscreen ? _t("Exit fullscreen (F)") : _t("Fullscreen (F)")) : '';
+    }
+
+    get downloadTooltip() {
+        return !this.isMobileOS ? _t("Download") : '';
     }
 }
