ee05ee7a4f48f5c60a03eae30e54e6d5420fee84
Merlin Guillaume <megu@odoo.com>
2026-05-06 09:38:55 +0000

    [FIX] website_sale_collect: partner is subscribed to in_store delivery
    Customers placing an order without logging in and with the "Pick up in
    store" delivery method are not notified when the delivery is confirmed
    
    Steps to reproduce:
    1. Install eCommerce and Sales
    2. Go to Settings > Website > Delivery and enable "Click & Collect"
    3. Go to Settings > Inventory > Shipping and enable "Email Confirmation"
    4. Go to Website > Configuration > Payment Providers and Install Demo
    5. Go to Website > Configuration > Delivery Methods and open "Pick up in
        store", set YourCompany as warehouse and publish it
    6. Go to Sales > Products, open product "Office Lamp", click on "Update
        Quantity" in the status bar and add 5 units
    7. Log out
    8. Go to the shop, add product "Office Lamp" to the cart and checkout
    9. Fill in the address form and continue checkout
    10. Select "Pick up in store" as delivery method and select a location
    11. Confirm the order and pay with Demo
    12. As user Mitchell Admin, go to Sales, remove the default filter and
        open the newly created sale order
    13. Open the related delivery with the smart button and validate it
    14. No delivery order confirmation was sent to the customer (check
        emails)
    
    Issue:
    Pickup addresses are always inactive, preventing the partner from
    receiving email confirmation
    
    Solution:
    Disable archiving of pickup addresses
    
    opw-6095396
    
    closes odoo/odoo#270288
    
    X-original-commit: fb74a371407ee19c6b1a3ab9f5a7b314978cb5cb
    Signed-off-by: Valeriya Chuprina (vchu) <vchu@odoo.com>
    Signed-off-by: Merlin Guillaume (megu) <megu@odoo.com>

--- addons/website_sale_collect/tests/test_sale_order.py
+++ addons/website_sale_collect/tests/test_sale_order.py
@@ -240,3 +240,28 @@ class TestSaleOrder(ClickAndCollectCommon):
         })
         so.set_pickup_location(pickup_location_data)
         self.assertEqual(so.fiscal_position_id, fp_company_2)
+
+    def test_partner_email_confirmation(self):
+        """Partner receives email confirmation for in_store delivery."""
+        self.company.stock_move_email_validation = True
+        wh_partner = self.warehouse.partner_id
+        new_so = self._create_in_store_delivery_order()
+        new_so.set_pickup_location(
+            json.dumps({
+                "id": self.warehouse.id,
+                "name": wh_partner.name,
+                "street": "New test street",
+                "zip_code": wh_partner.zip,
+                "city": "New test city",
+                "state": wh_partner.state_id.code,
+                "country_code": wh_partner.country_code,
+            })
+        )
+        new_so.action_confirm()
+        new_so.picking_ids.button_validate()
+        self.assertTrue(
+            any(
+                partner.email == self.partner.email
+                for partner in new_so.picking_ids.message_ids.notified_partner_ids
+            )
+        )
