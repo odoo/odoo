e853961a7be9837f693ac4e912891220e9a54831
Anh Thao Pham (pta) <pta@odoo.com>
2026-05-07 16:20:31 +0200

    [FIX] point_of_sale: only use AvaTax FP if activated in POS
    Steps to reproduce:
    - Install Accounting and Point of Sale
    
    - In Accounting settings, activate "AvaTax"
    - Configure the AvaTax fiscal position and activate "Detect
    Automatically" option
    - Make sure that the other fiscal positions don't have that
    option set or that they are ordered after the AvaTax one
    
    - Go to the settings of a point of Sale
    - Activate "Flexible Taxes" and configure "Default" and "Allowed"
    - Make sure that AvaTax fiscal position is not allowed
    - Do not activate "AvaTax PoS Integration"
    
    - Open a POS session
    - Select a customer with an address in the US and without fiscal
    position
    - Check the fiscal position
    
    Issue:
    The selected fiscal position is the AvaTax one even though AvaTax
    is not activated in the POS.
    
    Cause:
    We force the use of a fiscal position if it is configured on a
    customer.
    In this case, as no fiscal position is configured on the customer,
    we try to retrieve one that matches the condition and the AvaTax
    one is selected.
    
    Solution:
    When searching for the fiscal position of a customer, if AvaTax is
    not configured in the POS and if its fiscal positions are not
    allowed in POS, we ignore the fiscal positions using AvaTax.
    
    opw-6154089
    
    closes odoo/odoo#263342
    
    Related: odoo/enterprise#116626
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/point_of_sale/static/src/app/models/pos_order.js
+++ addons/point_of_sale/static/src/app/models/pos_order.js
@@ -900,6 +900,15 @@ export class PosOrder extends Base {
         return false;
     }
 
+    findFiscalPosition(fiscalPosition) {
+        if (fiscalPosition) {
+            return this.models["account.fiscal.position"].find(
+                (position) => position.id === fiscalPosition.id
+            );
+        }
+        return false;
+    }
+
     updatePricelistAndFiscalPosition(newPartner) {
         let newPartnerPricelist, newPartnerFiscalPosition;
         const defaultFiscalPosition = this.models["account.fiscal.position"].find(
@@ -907,11 +916,8 @@ export class PosOrder extends Base {
         );
 
         if (newPartner) {
-            newPartnerFiscalPosition = newPartner.fiscal_position_id
-                ? this.models["account.fiscal.position"].find(
-                      (position) => position.id === newPartner.fiscal_position_id?.id
-                  )
-                : defaultFiscalPosition;
+            newPartnerFiscalPosition =
+                this.findFiscalPosition(newPartner.fiscal_position_id) || defaultFiscalPosition;
             newPartnerPricelist =
                 this.config.available_pricelist_ids.find(
                     (pricelist) => pricelist.id === newPartner.property_product_pricelist?.id
