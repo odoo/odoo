565b181c4a7aa8042ac11d6cba370b771564e0a5
Dirk Douglas <dido@odoo.com>
2026-05-07 19:21:38 +0000

    [FIX] account: Allow branch users create journal currency transactions
    **Problem:**
    When a branch user with no access to the parent company tries to create a
    transaction for a parent company's journal with a foreign currency set, this
    will raise an access error.
    
    **Steps to Reproduce:**
    - Make a branch of "My Company (San Francisco)"
    - Set user "Marc Demo" to only have access to the branch
    - Add a new bank journal set to "EUR" currency
    - Switch to Marc Demo
    - Try to add a transaction in the new bank journal
    
    **Root Cause:**
    When a transaction is created, Odoo determines the amount in company currency
    by converting it from the foreign currency. The method to convert currency uses
    "with_company()" to use the company's rates, but the allowed companies of the
    branch user does not have access to the parent company, causing an access
    error.
    
    **Solution:**
    If the company is the parent company of the active company, get the currency
    `inverse_rate` using `sudo()`.
    
    opw-6186901
    
    X-original-commit: 824e40d1bfcd957ae1c7095fa69f0c1d392e5ff7

--- addons/account/tests/test_company_branch.py
+++ addons/account/tests/test_company_branch.py
@@ -312,3 +312,39 @@ class TestCompanyBranch(AccountTestInvoicingCommon):
            to negative value."""
         with self.assertRaises(ValidationError):
             self.root_company.fiscalyear_last_day = -1
+
+    def test_branch_user_bank_statement_foreign_currency(self):
+        # Create a user that only belongs to branch a
+        user = self.env['res.users'].create({
+            'name': "User A",
+            'login': "user_a@example.com",
+            'email': "user_a@example.com",
+            'company_id': self.branch_a.id,
+            'company_ids': [Command.set([self.branch_a.id])],
+            'group_ids': [Command.set([
+                self.env.ref('account.group_account_user').id,
+            ])],
+        })
+
+        journal = self.env['account.journal'].create({
+            'name': "Bank (EUR)",
+            'code': "EBNK",
+            'type': "bank",
+            'company_id': self.root_company.id,
+            'currency_id': self.other_currency.id,
+        })
+
+        statement_line = self.env['account.bank.statement.line'].with_user(user.id).create({
+            'date': '2019-01-01',
+            'journal_id': journal.id,
+            'payment_ref': 'line_1',
+            'partner_id': False,
+            'foreign_currency_id': False,
+            'amount': 25,
+            'company_id': self.branch_a.id
+        })
+
+        self.assertRecordValues(statement_line, [{
+            'amount_total_in_currency_signed': 25,
+            'amount_total_signed': 12.5,
+        }])
