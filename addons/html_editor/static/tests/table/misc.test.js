bc7f25ebbaecb7b79d8685713d28e1d26612c82e
Jinjiu Liu <jili@odoo.com>
2026-05-07 10:34:15 +0200

    [FIX] html_editor: show bg color when double click on empty cell Before this commit: the bg color is not updated when double clicking an empty cell.
    After this commit: we call setSelection with the current editable
    selection to trigger updateSelectedColor in the color plugin, when
    double clicking on an empty cell and selecting table cell(s) by keyboard.
    
    We adapted the test "select single cell containing text when pressing
    shift + arrow key", because the setSelection corrects the selection at
    the fake <br>
    
    task-5976046

--- addons/html_editor/static/tests/table/misc.test.js
+++ addons/html_editor/static/tests/table/misc.test.js
@@ -1,6 +1,12 @@
 import { describe, expect, test } from "@odoo/hoot";
 import { setupEditor } from "../_helpers/editor";
-import { click, queryAll, queryFirst } from "@odoo/hoot-dom";
+import {
+    click,
+    queryAll,
+    queryFirst,
+    waitFor,
+    manuallyDispatchProgrammaticEvent,
+} from "@odoo/hoot-dom";
 import { animationFrame, tick } from "@odoo/hoot-mock";
 import { getContent, setSelection } from "../_helpers/selection";
 import { execCommand } from "../_helpers/userCommands";
@@ -182,6 +188,92 @@ describe("selected cell color in toolbar", () => {
         expect(".fa-paint-brush").toHaveCount(1);
         expect(".fa-paint-brush").toHaveStyle({
             "border-bottom": "2px solid rgba(0, 0, 0, 0)",
+        });
+    });
+    test("empty cell's selected color should be shown in toolbar on double click", async () => {
+        const { el } = await setupEditor(`
+            <table class="table table-bordered o_table">
+                <tbody>
+                    <tr><td style="background-color: rgba(255, 0, 0, 0.6);"><div class="o-paragraph">[]<br></div></td><td><br></td><td><br></td></tr>
+                    <tr><td><br></td><td><br></td><td><br></td></tr>
+                </tbody>
+            </table>`);
+
+        const BORDER_SENSITIVITY = 5;
+        const firstTd = el.querySelector("td");
+        const offset = BORDER_SENSITIVITY + 1;
+
+        manuallyDispatchProgrammaticEvent(firstTd, "mousedown", {
+            detail: 2,
+            clientX: offset,
+            clientY: offset,
+        });
+        await animationFrame();
+
+        manuallyDispatchProgrammaticEvent(firstTd, "mouseup", {
+            detail: 2,
+            clientX: offset,
+            clientY: offset,
+        });
+        manuallyDispatchProgrammaticEvent(firstTd, "click", {
+            detail: 2,
+            clientX: offset,
+            clientY: offset,
+        });
+        // the selectionchange event is usually triggered by the browser after
+        // the click event, but since we are programmatically dispatching the
+        // click event, we also need to manually dispatch the selectionchange
+        // event to trigger the toolbar update
+        manuallyDispatchProgrammaticEvent(document, "selectionchange");
+        await animationFrame();
+
+        // set a timeout for the deplayed toolbar update
+        await waitFor(".o-we-toolbar", { timeout: 1500 });
+        await expandToolbar();
+        expect(".fa-paint-brush").toHaveCount(1);
+        expect(".fa-paint-brush").toHaveStyle({
+            "border-bottom": "2px solid rgba(255, 0, 0, 0.6)",
+        });
+    });
+
+    test("non-empty cell's selected color should be shown in toolbar on triple click", async () => {
+        const { el } = await setupEditor(`
+            <table class="table table-bordered o_table">
+                <tbody>
+                    <tr><td style="background-color: rgba(255, 0, 0, 0.6);"><div class="o-paragraph">pp[]pp</div></td><td><br></td><td><br></td></tr>
+                    <tr><td><br></td><td><br></td><td><br></td></tr>
+                </tbody>
+            </table>`);
+
+        const BORDER_SENSITIVITY = 5;
+        const firstTd = el.querySelector("td");
+        const offset = BORDER_SENSITIVITY + 1;
+
+        manuallyDispatchProgrammaticEvent(firstTd, "mousedown", {
+            detail: 3,
+            clientX: offset,
+            clientY: offset,
+        });
+        await animationFrame();
+
+        manuallyDispatchProgrammaticEvent(firstTd, "mouseup", {
+            detail: 3,
+            clientX: offset,
+            clientY: offset,
+        });
+        manuallyDispatchProgrammaticEvent(firstTd, "click", {
+            detail: 3,
+            clientX: offset,
+            clientY: offset,
+        });
+        await animationFrame();
+
+        // set a timeout for the deplayed toolbar update
+        await waitFor(".o-we-toolbar", { timeout: 1500 });
+        await expandToolbar();
+        expect(".fa-paint-brush").toHaveCount(1);
+        expect(".fa-paint-brush").toHaveStyle({
+            "border-bottom": "2px solid rgba(255, 0, 0, 0.6)",
         });
     });
 });
