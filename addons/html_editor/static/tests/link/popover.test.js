3eedb289d468a5f6cf16090c2661ea20515a06bf
Adnan Saiyed <adsa@odoo.com>
2026-05-07 12:19:48 +0000

    [FIX] html_editor: cancel file uploading on link discard
    Current behavior before commit:
    
    Steps to reproduce the issue:
    
    - Go to Todo, In the network tab switch to "Slow 4G" so that
      file upload can take few seconds to upload.
    - Upload a file using link popover.
    - While the file upload is in progress, hit the discard button
      of the link popover.
    - Notice that the upload continues in the background and when it
    completes successfully, link is inserted.
    
    Desired behavior after commit:
    
    Discarding the link popover during file upload should cancel the
    upload request and prevent inserting the link.
    
    task-6199113
    
    closes odoo/odoo#266952
    
    X-original-commit: 303f0ad3e9cefb5dd26327df6c19df016fe9253b
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Adnan Saiyed (adsa) <adsa@odoo.com>

--- addons/html_editor/static/tests/link/popover.test.js
+++ addons/html_editor/static/tests/link/popover.test.js
@@ -1,6 +1,7 @@
 import { describe, expect, test } from "@odoo/hoot";
 import {
     click,
+    delay,
     fill,
     press,
     queryFirst,
@@ -1701,6 +1702,69 @@ describe("upload file via link popover", () => {
         );
         const favIcon = await waitFor(".o_we_preview_favicon span.o_image");
         expect(favIcon).toHaveAttribute("data-mimetype", "text/plain");
+    });
+
+    test("should not insert attachment as link if popover is discarded during file upload", async () => {
+        const patchUpload = (editor) => {
+            const mockedUploadPromise = new Promise((resolve) => {
+                patchWithCleanup(editor.services.uploadLocalFiles, {
+                    async upload({ resId, resModel }, { setAbortCallback } = {}) {
+                        const attachments = [];
+                        await editor.services.upload.uploadFiles(
+                            [
+                                new File(["test"], "fake_file.txt", {
+                                    type: "text/plain",
+                                    size: 100,
+                                }),
+                            ],
+                            { resModel, resId },
+                            (attachment) => {
+                                attachments.push(attachment);
+                            },
+                            setAbortCallback
+                        );
+                        resolve();
+                        return attachments;
+                    },
+                });
+            });
+            return mockedUploadPromise;
+        };
+
+        onRpc("/html_editor/attachment/add_data", async (request) => {
+            const { params } = await request.json();
+            await delay(100);
+            return {
+                name: params.name,
+            };
+        });
+        const { editor, el } = await setupEditor("<p>[]<br></p>");
+        const mockedUpload = patchUpload(editor);
+        execCommand(editor, "openLinkTools");
+        await waitFor(".o-we-linkpopover");
+        let xhr;
+        const waitForRequest = new Promise((res) => {
+            patchWithCleanup(XMLHttpRequest.prototype, {
+                open() {
+                    xhr = this;
+                    super.open(...arguments);
+                    res();
+                },
+                abort() {
+                    xhr.dispatchEvent(new Event("abort"));
+                    super.abort();
+                },
+            });
+        });
+        await click("button i[class='fa fa-upload']");
+        await waitForRequest;
+        await expectElementCount(".o_notification_manager .o_notification", 1);
+        await click(".o_we_discard_link");
+        await expectElementCount(".o-we-linkpopover", 0);
+        await expectElementCount(".o_notification_manager .o_notification", 0);
+        await mockedUpload;
+        await tick();
+        expect(getContent(el)).toBe(`<p o-we-hint-text='Type "/" for commands' class="o-we-hint">[]<br></p>`);
     });
 });
 
