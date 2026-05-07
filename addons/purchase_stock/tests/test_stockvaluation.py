71148a58aa303aa944342b1f35719a406a46357e
plha-odoo <plha@odoo.com>
2026-05-07 12:23:14 +0000

    [FIX] purchase_stock: no tax recompute bill diff from cost
    Issue is really similar commit 21877c09863222a237fe99334787ac46935dcca4
    except here the compensations amls are created in
    _stock_account_prepare_anglo_saxon_in_lines_vals
    because of a difference between bill price and product cost
    
    **Steps to reproduce:**
    - create storable product with category standard auto
    - on the category, set an account in the field
    'price difference account'
    - set a cost of 10
    - set a purchase tax
    - confirm a PO for 1 @ 15 and validate receipt
    - create bill, set a date, save
    - on the Bill set the total tax at 100 (it's bellow
    'untaxed amount' on the bottom right of the bill and
    should be 2.25 before you change it, if the tax is 15%)
    - confirm the bill
    
    **Current behavior:**
    tax was reset to 2.25
    
    **Expected behavior:**
    It should stay 100 as it was manually set
    
    **Cause of the issue:**
    The total tax amount is computed based on the tax lines
    in Journal Items
    https://github.com/odoo/odoo/blob/2744396733bb3ad60813e9e093d67192c0d38b36/addons/account/models/account_move.py#L1171
    So the problem is actually that a recomputation of the
    balance of the tax account.move.line (the one with the
    account "tax paid" in journal items) is triggered when
    we confirm the Bill.
    
    That's because:
     When we confirm the bill,
    _stock_account_prepare_anglo_saxon_in_lines_vals() creates
    two amls :
    - one debiting 5 on the account set in the field 'price
    difference account'
    - one crediting 5 in the stock interim received account
    (This makes sense and is there to realign with the fact
    that, on the account move linked to the svl, the amount
    credited from stock interim received is rightfully 10
    because that's cost of the product and it's a standard
    price product)
    
    When we create those amls from,  the create method
    from account.move.lines calls super() inside a context
    manager calling _sync_dynamic_lines().
    https://github.com/odoo/odoo/blob/5583cbcebae00d8122dce5ce929b650929966a90/addons/account/models/account_move_line.py#L1628-L1635
    the yield of sync_dynamic_lines() is inside a context
    manager calling _sync_tax_lines.
    https://github.com/odoo/odoo/blob/5583cbcebae00d8122dce5ce929b650929966a90/addons/account/models/account_move.py#L3250
    Therefore, the first half of sync_tax_lines() (untill
    the yield) is ran before the call to super and the rest
    (from the yield) is ran after the call to super.
    Because we added two lines in the account.move,
    get_changed_lines will return those 2 new line and
    because there is a tax_ids on the new lines round_from_tax
    will be False.
    https://github.com/odoo/odoo/blob/5583cbcebae00d8122dce5ce929b650929966a90/addons/account/models/account_move.py#L3034-L3041
    Therefore we won't reach continue.
    https://github.com/odoo/odoo/blob/73d73c5c6606e0b34c754bfc4de035840951dd3b/addons/account/models/account_move.py#L3055-L3059
    And the tax line will be recomputed using
    _prepare_tax_line()
    https://github.com/odoo/odoo/blob/73d73c5c6606e0b34c754bfc4de035840951dd3b/addons/account/models/account_move.py#L3065
    
    Here is why there is a tax_ids on the new lines :
    The field is precompute so if we don't set a value for
    it, _compute_tax_ids will be ran to compute it.
    As the account move on which the lines are added
    is a bill, the tax_ids will the supplier_tax_id of the product.
    https://github.com/odoo/odoo/blob/73d73c5c6606e0b34c754bfc4de035840951dd3b/addons/account/models/account_move_line.py#L898-L901
    
    **fix**
    There is no need for a tax_ids on these lines as they
    are not meant to (and should'nt) impact the taxes.
    
    opw-6014710
    
    X-original-commit: c3f390e5bbd4f428e0e8d7d8fa264eae447b5037

--- addons/purchase_stock/tests/test_stockvaluation.py
+++ addons/purchase_stock/tests/test_stockvaluation.py
@@ -2123,4 +2123,124 @@ class TestStockValuationWithCOA(AccountTestInvoicingCommon):
         with Form(bill) as move_form:
             move_form.invoice_currency_rate = 4
         bill.action_post()
+<<<<<<< d7a9270b8f3b9c41f876117cf8292eb18637c244
         self.assertEqual(po.picking_ids.move_ids.value, 25)
+||||||| 4dec3c5e41c0f3023fcddb1acac01e27d0d1cb9f
+
+        # check bill total tax has not changed
+        self.assertEqual(bill.amount_tax, 500)
+
+    def test_svl_account_move_analytic_account_model_change_from_PO(self):
+        """ Tests whether, when an analytic account rule is set, and user changes manually the analytic account on
+        the po, it is the same that is mentioned in the account move created by the svl.
+        """
+        # Required for `analytic.group_analytic_accounting` to be visible in the view
+        self.env.user.group_ids += self.env.ref('analytic.group_analytic_accounting')
+        analytic_plan = self.env['account.analytic.plan'].create({'name': 'Plan Test'})
+        analytic_account_default = self.env['account.analytic.account'].create({'name': 'default', 'plan_id': analytic_plan.id})
+        analytic_account_manual = self.env['account.analytic.account'].create({'name': 'manual', 'plan_id': analytic_plan.id})
+        self.product1.standard_price = 10
+
+        self.env['account.analytic.distribution.model'].create({
+            'analytic_distribution': {analytic_account_default.id: 100},
+            'product_id': self.product1.id,
+        })
+        analytic_distribution_manual = {str(analytic_account_manual.id): 100}
+
+        po_form = Form(self.env['purchase.order'].with_context(tracking_disable=True))
+        po_form.partner_id = self.partner_a
+        with po_form.order_line.new() as po_line_form:
+            po_line_form.name = self.product1.name
+            po_line_form.product_id = self.product1
+            po_line_form.product_qty = 1.0
+            po_line_form.price_unit = 10
+            po_line_form.analytic_distribution = analytic_distribution_manual
+
+        purchase_order = po_form.save()
+        purchase_order.button_confirm()
+        purchase_order.picking_ids.button_validate()
+
+        amls = purchase_order.picking_ids.move_ids.stock_valuation_layer_ids.account_move_id.line_ids
+        self.assertEqual(amls[0].analytic_distribution, analytic_distribution_manual)
+        self.assertEqual(amls[1].analytic_distribution, analytic_distribution_manual)
+=======
+
+        # check bill total tax has not changed
+        self.assertEqual(bill.amount_tax, 500)
+
+    def test_svl_account_move_analytic_account_model_change_from_PO(self):
+        """ Tests whether, when an analytic account rule is set, and user changes manually the analytic account on
+        the po, it is the same that is mentioned in the account move created by the svl.
+        """
+        # Required for `analytic.group_analytic_accounting` to be visible in the view
+        self.env.user.group_ids += self.env.ref('analytic.group_analytic_accounting')
+        analytic_plan = self.env['account.analytic.plan'].create({'name': 'Plan Test'})
+        analytic_account_default = self.env['account.analytic.account'].create({'name': 'default', 'plan_id': analytic_plan.id})
+        analytic_account_manual = self.env['account.analytic.account'].create({'name': 'manual', 'plan_id': analytic_plan.id})
+        self.product1.standard_price = 10
+
+        self.env['account.analytic.distribution.model'].create({
+            'analytic_distribution': {analytic_account_default.id: 100},
+            'product_id': self.product1.id,
+        })
+        analytic_distribution_manual = {str(analytic_account_manual.id): 100}
+
+        po_form = Form(self.env['purchase.order'].with_context(tracking_disable=True))
+        po_form.partner_id = self.partner_a
+        with po_form.order_line.new() as po_line_form:
+            po_line_form.name = self.product1.name
+            po_line_form.product_id = self.product1
+            po_line_form.product_qty = 1.0
+            po_line_form.price_unit = 10
+            po_line_form.analytic_distribution = analytic_distribution_manual
+
+        purchase_order = po_form.save()
+        purchase_order.button_confirm()
+        purchase_order.picking_ids.button_validate()
+
+        amls = purchase_order.picking_ids.move_ids.stock_valuation_layer_ids.account_move_id.line_ids
+        self.assertEqual(amls[0].analytic_distribution, analytic_distribution_manual)
+        self.assertEqual(amls[1].analytic_distribution, analytic_distribution_manual)
+
+    def test_bill_price_diff_cost_no_reset_tax(self):
+        """ Check that confirming a Bill for a standard perpetual product with price different
+        than the product's cost (so in a situation where price difference compensation amls are created)
+        does not reset a manually set total tax on the Bill."""
+
+        self.product1.product_tmpl_id.categ_id.property_cost_method = 'standard'
+        self.product1.product_tmpl_id.categ_id.property_valuation = 'real_time'
+        self.product1.supplier_taxes_id = self.company.account_purchase_tax_id
+        self.product1.standard_price = 10
+
+        po = self.env['purchase.order'].create({
+            'partner_id': self.partner_id.id,
+            'order_line': [
+                Command.create({
+                    'name': self.product1.name,
+                    'product_id': self.product1.id,
+                    'product_qty': 1.0,
+                    'price_unit': 20.0,
+                }),
+            ],
+        })
+        po.button_confirm()
+        receipt = po.picking_ids
+        receipt.move_ids.move_line_ids.quantity = 1
+        receipt.button_validate()
+
+        action = po.action_create_invoice()
+        bill = self.env["account.move"].browse(action["res_id"])
+        bill.invoice_date = fields.Date.today()
+
+        bill.line_ids.filtered(lambda l: l.display_type == "tax").balance = 100
+        bill.action_post()
+
+        self.assertEqual(bill.amount_tax, 100)
+        self.assertRecordValues(bill.line_ids.sorted('balance'), [
+            {'account_id': self.company_data['default_account_payable'].id,        'credit': 120.0,   'debit': 0.0},
+            {'account_id': self.stock_input_account.id,                            'credit': 10.0,    'debit': 0.0},
+            {'account_id': self.company_data['default_account_expense'].id,        'credit': 0.0,     'debit': 10.0},
+            {'account_id': self.stock_input_account.id,                            'credit': 0.0,     'debit': 20.0},
+            {'account_id': self.company_data['default_account_tax_purchase'].id,   'credit': 0.0,     'debit': 100.0},
+        ])
+>>>>>>> 3cbe2b2837050cc84560a0ebb8bbbc1cf47f8bcd
