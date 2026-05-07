c101e33654033012f1870f3a74ee033f9f7dca85
Adnan Saiyed <adsa@odoo.com>
2026-05-07 12:19:48 +0000

    [FIX] html_editor: cancel file uploading on link discard
    Current behavior before commit:
    
    Steps to reproduce the issue:
    
    - Go to Todo, In the network tab switch to "Slow 4G" so that
      file upload can take few seconds to upload.
    - Upload a file using link popover.
    - While the file upload is in progress, hit the discard button
      of the link popover.
    - Notice that the upload continues in the background and when it
    completes successfully, link is inserted.
    
    Desired behavior after commit:
    
    Discarding the link popover during file upload should cancel the
    upload request and prevent inserting the link.
    
    task-6199113
    
    closes odoo/odoo#267611
    
    X-original-commit: 9eca68aa6818c47d668ac8a234601525efba9786
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Adnan Saiyed (adsa) <adsa@odoo.com>

--- addons/html_editor/static/src/main/link/link_popover.js
+++ addons/html_editor/static/src/main/link/link_popover.js
@@ -306,6 +306,12 @@ export class LinkPopover extends Component {
             // all btn-* classes anyway.
         ];
     }
+
+    discard() {
+        this.props.onDiscard();
+        this.cancelUpload?.();
+    }
+
     onChange() {
         // Apply changes to update the link preview.
         this.props.onChange(
@@ -716,7 +722,14 @@ export class LinkPopover extends Component {
     async uploadFile() {
         const { upload, getURL } = this.uploadService;
         const { resModel, resId } = this.props.recordInfo;
-        const [attachment] = await upload({ resModel, resId }, { accessToken: true });
+        const setAbortCallback = (abortFn) => {
+            this.cancelUpload = abortFn;
+        };
+        const [attachment] = await upload(
+            { resModel, resId },
+            { accessToken: true, setAbortCallback }
+        );
+        delete this.cancelUpload;
         if (!attachment) {
             // No file selected or upload failed
             return;
