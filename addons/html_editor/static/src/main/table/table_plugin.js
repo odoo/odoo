8d748ae62285f3155f3d9b5cf8c874d713fdfd87
Jinjiu Liu <jili@odoo.com>
2026-05-07 10:34:15 +0200

    [FIX] html_editor: show bg color when double click on empty cell Before this commit: the bg color is not updated when double clicking an empty cell.
    After this commit: we call setSelection with the current editable
    selection to trigger updateSelectedColor in the color plugin, when
    double clicking on an empty cell and selecting table cell(s) by keyboard.
    
    task-5976046

--- addons/html_editor/static/src/main/table/table_plugin.js
+++ addons/html_editor/static/src/main/table/table_plugin.js
@@ -1384,6 +1384,10 @@ export class TablePlugin extends Plugin {
             } else {
                 ev.preventDefault();
                 this.selectTableCells(this.dependencies.selection.getEditableSelection());
+                // For an empty cell with a div inside, we need to use setSelection to trigger the selectionchange event.
+                this.dependencies.selection.setSelection(
+                    this.dependencies.selection.getEditableSelection()
+                );
             }
             return;
         }
@@ -1586,6 +1590,13 @@ export class TablePlugin extends Plugin {
                     // which deselects the single cell. Hence, we need a label
                     // to keep it selected.
                     this._isFirefoxDoubleMousedown = true;
+                }
+                if (ev.detail === 2) {
+                    // Specifically for double click on empty cell, to trigger
+                    // selectionchange event and update the toolbar button states.
+                    this.dependencies.selection.setSelection(
+                        this.dependencies.selection.getEditableSelection()
+                    );
                 }
                 if (ev.detail === 3) {
                     // Doing a tripleclick on a text will change the selection.
