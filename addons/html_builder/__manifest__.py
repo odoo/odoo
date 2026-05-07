3852b6c6db077511c31014a86af46e19f082fd58
Robin Lejeune (role) <role@odoo.com>
2026-05-07 14:24:45 +0200

    [CLN] html_builder: bump to version 1.0
    The html_builder manifest still declared a version 0.1. We can bump it
    to version 1.0, which is the default in manifests when the line is not
    mentioned.
    
    closes odoo/odoo#263304
    
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/html_builder/__manifest__.py
+++ addons/html_builder/__manifest__.py
@@ -8,11 +8,6 @@
 
     'author': 'Odoo S.A.',
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'version': '0.1',
-
     # so stupid that we need to use the stupid defineMailModel helper, so we need
     # to depend on mail
     'depends': ['base', 'html_editor', 'mail'],
