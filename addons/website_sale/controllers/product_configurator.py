951475f04d2d33442e39027ae4cde2d991592228
krip-odoo <krip@odoo.com>
2026-05-08 12:48:13 +0000

    [FIX] {website_,}sale: make sure strikethrough is update properly
    Steps to produce:
    - Install website_sale module.
    - Enable ` Units of Measure & Packagings `and `Comparison Price` features from
      settings.
    - Create product > set sales price as 5 and Compare to Price as 12.
    - From the sales tab, under Upsell & Cross-Sell > set Packagings as pack of 6.
    - Go to the shop page on eCommerce, and add your product via the shop page (this
      should open the product configurator).
    - Change the UOM from the radio.
    
    Issue:
    - Changing the UOM doesn't change the strikethrough price.
    
    Root cause:
    - At [1], The `_get_strikethrough_price` method was not receiving the selected
      uom parameter, causing it to compute the compare_list_price based on the
      product's base uom instead of the user-selected uom.
    
    Solution:
    - Pass `uom` parameter from `_get_basic_product_information` to
      `_get_strikethrough_price`
    - Apply uom conversion to compare_list_price when the selected uom
      differs from the product's base uom.
    - Also fix pricelist base price calculation to use the selected uom.
    - Update the JS logic to refresh the strikethrough price when the uom
      changes.
    
    [1]: https://github.com/odoo/odoo/blob/bfcb22256226ae056e934e2f9e498e8cea4d2f63/addons/website_sale/controllers/product_configurator.py#L101-L154
    
    opw-6201754
    
    X-original-commit: 9a0bff7d33071aec17ad4ae0a8b8a97e916562d7

--- addons/website_sale/controllers/product_configurator.py
+++ addons/website_sale/controllers/product_configurator.py
@@ -139,7 +139,8 @@ class WebsiteSaleProductConfiguratorController(SaleProductConfiguratorController
             # is populated).
             strikethrough_price = self._get_strikethrough_price(
                 product_or_template.with_context(
-                    **product_or_template._get_product_price_context(combination)
+                    uom=kwargs.get('uom'),
+                    **product_or_template._get_product_price_context(combination),
                 ),
                 currency,
                 date,
@@ -178,6 +179,7 @@ class WebsiteSaleProductConfiguratorController(SaleProductConfiguratorController
         :rtype: float|None
         :return: The strikethrough price of the product, if there is one.
         """
+        uom = product_or_template.env.context.get("uom")
         pricelist_rule = request.env['product.pricelist.item'].browse(pricelist_rule_id)
 
         # First, try to use the base price as the strikethrough price.
@@ -187,7 +189,7 @@ class WebsiteSaleProductConfiguratorController(SaleProductConfiguratorController
                 pricelist_rule._compute_price_before_discount(
                     product=product_or_template,
                     quantity=1.0,
-                    uom=product_or_template.uom_id,
+                    uom=uom or product_or_template.uom_id,
                     date=date,
                     currency=currency,
                 ),
@@ -211,6 +213,10 @@ class WebsiteSaleProductConfiguratorController(SaleProductConfiguratorController
                 date=date,
                 round=False,
             )
+            if uom and uom != product_or_template.uom_id:
+                compare_list_price = product_or_template.uom_id._compute_price(
+                    compare_list_price, uom,
+                )
             # Only show `compare_list_price` if it's greater than the actual price.
             if currency.compare_amounts(compare_list_price, price) == 1:
                 return compare_list_price
