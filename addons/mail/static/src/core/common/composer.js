abe08b3b883f13854ca643e40c1b63637f27dc00
Atovange <gito@odoo.com>
2026-05-08 09:09:21 +0000

    [FIX] mail: autofocus composer after emoji in portal chatter
    Before this commit, after adding an emoji via the emoji picker in the
    portal chatter, the composer would not be focused.
    
    This is due to the `autofocus` prop of the composer being optional
    and not having a default value, leading to `NaN` when being
    incremented while `undefined`.
    
    This commit fixes the issue by giving it a default value of 0.
    
    task-6204911
    
    closes odoo/odoo#263688
    
    X-original-commit: e93798f9fcd28bf14e268794bc32e75eb036ade4
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>
    Signed-off-by: Giorgio Tocco (gito) <gito@odoo.com>

--- addons/mail/static/src/core/common/composer.js
+++ addons/mail/static/src/core/common/composer.js
@@ -101,6 +101,7 @@ export class Composer extends Component {
         Wysiwyg,
     };
     static defaultProps = {
+        autofocus: 0,
         mode: "normal",
         className: "",
         sidebar: true,
