4a8ecead73bd91255db1754e33d8a32d7399a26e
Arnold Moyaux <arm@odoo.com>
2026-05-08 14:03:55 +0200

    [FIX] stock_account: modifying valued location usage
    Usecase:
    - Set 2 internal locations: stock1 and stock2
    - Create a product AVCO with std price = 5$
    - Do an inventory of 5 units at 5$ (25$) in stock1
    - Do a receipt for 10 units at 10$ in stock2
    - Go to stock2 and change the type to a inventory loss
    - Do a last receipt for 10 units at 10$ in stock2
    
    Expected behvior:
    The product cost is 9$ (225$ / 25)
    
    Current behavior:
    The product cost is 11.25$ (225$ / 20)
    
    It happens because the value for the first move in (inventory)
    is counted but not the quantity. In run_average_batch when he replays
    the history, it uses `move.value` which is store but it uses
    `_get_valued_qty` that is computed on current location usage.
    
    In order to avoid this we prevent to update a location usage with some
    valuation inside.

--- addons/stock_account/models/stock_location.py
+++ addons/stock_account/models/stock_location.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import UserError
 from odoo.fields import Domain
 
 
@@ -14,6 +15,30 @@ class StockLocation(models.Model):
         help="Expense account used to re-qualify products removed from stock and sent to this location")
     is_valued_internal = fields.Boolean('Is valued inside the company', compute="_compute_is_valued", search="_search_is_valued")
     is_valued_external = fields.Boolean('Is valued outside the company', compute="_compute_is_valued")
+
+    @api.constrains('usage')
+    def _check_usage_no_valued_moves(self):
+        non_valued = self.filtered(lambda l: l.usage not in ('internal', 'transit'))
+        if not non_valued:
+            return
+        move_lines = self.env['stock.move.line'].search([
+            ('move_id.state', '=', 'done'),
+            ('move_id.value', '!=', 0),
+            '|',
+            ('location_id', 'in', non_valued.ids),
+            ('location_dest_id', 'in', non_valued.ids),
+        ])
+        if move_lines:
+            non_valued_ids = set(non_valued.ids)
+            offending_ids = (
+                set(move_lines.mapped('location_id.id'))
+                | set(move_lines.mapped('location_dest_id.id'))
+            ) & non_valued_ids
+            offending = self.browse(offending_ids)
+            raise UserError(self.env._(
+                "You cannot change the type of the following locations because they have stock moves with valuation:\n%(locations)s",
+                locations='\n'.join(offending.mapped('display_name')),
+            ))
 
     def _search_is_valued(self, operator, value):
         if operator not in ['=', '!=']:
