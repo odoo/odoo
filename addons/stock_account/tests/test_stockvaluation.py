62edb28e81cff68be6e37798c6d9318e8a6be4ef
plha-odoo <plha@odoo.com>
2026-05-08 11:36:44 +0000

    [FIX] stock_account: generate entry from branch with correct company
    **Steps to reproduce:**
    - create a new company A
    - in the branch tab, create a new branch A for this company
    - create a warehouse for the company A and a warehouse for
    the branch A
    - from the company A, in settings for the 'fiscal localization' set
    Package : Generic Chart of account, if not already set
    (to have account journals).
    
    From the branch A:
    - create a storable product with standard perpetual category
    - set a cost of 10
    - confirm a PO for 10 and validate delivery
    - navigate to 'inventory valuation'
    
    Make sure the branch A is the main company, but
    both branch A and company A are selected:
    - click on generate entry
    - click on the 'Other Info' tab
    
    **Current behavior:**
    The company of the account move is the parent company
    (Company A)
    
    **Expected behavior:**
    It should be the branch A.
    (As it is the case if only branch A is selected when clicking
    on "Generate entry")
    IAs a consequence, f you click on 'Inventory Valuation' on the
    top left to go back to the view, you will have an access error.
    
    **Cause of the issue:**
    When computing the company_id on the account move,
    move.journal_id.company_id will be the parent company because
    the journal_id of the branch is the one of the parent company
    (by default).
    So we will call _accessible_branches() on the parent company.
    https://github.com/odoo/odoo/blob/661ddbb7f12e32394a3c11b5a4cd2f38a9e156f5/addons/account/models/account_move.py#L878-L881
    Inside __accessible_branches(), 'accessible' will be based on
    self.env.companies
    https://github.com/odoo/odoo/blob/661ddbb7f12e32394a3c11b5a4cd2f38a9e156f5/odoo/addons/base/models/res_company.py#L430-L439
    (which is based on 'allowed_company_ids' in the context.
    https://github.com/odoo/odoo/blob/661ddbb7f12e32394a3c11b5a4cd2f38a9e156f5/odoo/orm/environments.py#L266)
    So the return value of __accessible_branches()
    will be a list with 2 ids, the one of the parent
    company and the one of the branch.
    And we will use the first element of this list,
    which will be the parent company_id, in _compute_company_id
    to set the company of the account move.
    
    **fix:**
    When fetching the data for the inventory valuation view,
    only the data from the main company selected matters,
    https://github.com/odoo/odoo/blob/dd84309df7fd39e9e97ed02d135d25b211085135/addons/stock_account/report/stock_valuation_report.py#L13
    
    Therefore, when creating the account move the company
    of the move should be the main company.
    We already did something very similar in this PR
    https://github.com/odoo/odoo/pull/262776 where we
    modified the context in action_close_stock_valuation()
    before calling _action_close_stock_valuation()
    https://github.com/odoo/odoo/blob/43f5ceadbc1f7df9898c327bf65bffdbe9860c1c/addons/stock_account/models/res_company.py#L56
    opw-6144294
    
    X-original-commit: 54ac68c90ad3a768292d749cf65475e101ff4e05

--- addons/stock_account/tests/test_stockvaluation.py
+++ addons/stock_account/tests/test_stockvaluation.py
@@ -3406,3 +3406,17 @@ class TestStockValuation(TestStockValuationCommon):
             # Check values 1 day after the moves
             self.assertEqual(self.product_avco.with_context(to_date=date_1).avg_cost, 10)
             self.assertEqual(self.product_avco.with_context(to_date=date_1).total_value, 10)
+
+    def test_generate_entry_branch_correct_account(self):
+        """ When generating entry on a branch and the main company is also selected, the account move is
+        linked to the branch
+        """
+        # Unbilled inventory move in branch
+        self.product_avco_auto.with_company(self.branch).categ_id.property_cost_method = 'average'
+        self.product_avco_auto.with_company(self.branch).categ_id.property_valuation = 'real_time'
+        self._make_in_move(self.product_avco_auto, 2, unit_cost=50, company=self.branch)
+
+        # generate entry on branch with main comp also selected
+        closing = self.branch.with_context(allowed_company_ids=[self.branch.id, self.company.id]).action_close_stock_valuation()
+        closing_lines = self.env['account.move'].browse(closing['res_id']).line_ids
+        self.assertEqual(closing_lines.move_id.company_id.id, self.branch.id)
