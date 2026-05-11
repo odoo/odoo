4b09a5aefe86309b0ca17b4a7c1ae28ac3cbced5
Pierre Pulinckx <pipu@odoo.com>
2026-05-11 11:38:46 +0200

    [FIX] *: remove undeterministicTour key
    Fix undeterministic tours by making some triggers more precise in
    a few steps.

--- addons/website/static/tests/tours/translate_menu_name.js
+++ addons/website/static/tests/tours/translate_menu_name.js
@@ -9,7 +9,6 @@ import { translationIsReady } from "@web/core/l10n/translation";
 registerWebsitePreviewTour(
     "translate_menu_name",
     {
-        undeterministicTour_doNotCopy: true, // Remove this key to make the tour failed. ( It removes delay between steps )
         edition: false,
     },
     () => [
