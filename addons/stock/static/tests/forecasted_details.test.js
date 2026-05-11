ddf5dea3cf34ea49eac4f761cdae882e2ecdd9a2
Michaël Mattiello <mcm@odoo.com>
2026-05-11 10:13:33 +0200

    todo test ForecastedDetails

--- addons/stock/static/tests/forecasted_details.test.js
+++ addons/stock/static/tests/forecasted_details.test.js
@@ -1,11 +1,13 @@
 import { expect, test } from "@odoo/hoot";
 import { ForecastedDetails } from "@stock/stock_forecasted/forecasted_details";
 
-test("forecast detail sameDocument receipt date", async () => {
+test.tags("owl3");
+test.todo("forecast detail sameDocument receipt date", async () => {
     const document_in = { id: 10, _name: "stock.picking", name: "PICK/001" };
     const line1 = { receipt_date: "2024-01-01", product: { id: 1 }, document_in };
     const line2 = { receipt_date: "2024-01-01", product: { id: 1 }, document_in };
     const doc1 = { lines: [line1, line2] };
+    // ForecastedDetails should not be created manually. Mount the component!
     const forecast1 = new ForecastedDetails({ docs: doc1 });
     forecast1.OnHandLinesPerProduct = {};
     forecast1.NotAvailableLinesPerProduct = {};
