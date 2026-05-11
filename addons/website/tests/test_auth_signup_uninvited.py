b37285b13aaabb85923a5063849ea3e167ed37a4
assk-odoo <assk@odoo.com>
2026-05-11 10:24:33 +0000

    [FIX] website: prevent signup/login for logged in users
    Steps to reproduce:
    1.Log in to the backend as an Admin (or any authenticated user).
    2.Navigate to Website -> Configuration -> System Pages and open the
    Signup page.
    3.Fill in the signup form and submit it.
    4.After successfully signing up, click the Logout button.
    5.Observe that a "405 Method Not Allowed" error is displayed.
    
    Before this commit:
    When an already logged-in user accessed the signup page through the
    System Pages menu and submitted the signup form, clicking the Logout
    button afterward resulted in a 405 Method Not Allowed error.
    
    After this commit:
    When an already logged-in user accesses the signup or login page,
    a warning message is displayed and the Sign Up or Log In button is
    disabled, preventing the form from being submitted.
    
    task-6023075
    
    closes odoo/odoo#271224
    
    X-original-commit: 84d4034373371cc18e0405c0efad68960ff2f991
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Sujal Kamleshbhai Asodariya (assk) <assk@odoo.com>

--- addons/website/tests/test_auth_signup_uninvited.py
+++ addons/website/tests/test_auth_signup_uninvited.py
@@ -1,6 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo.tests import common, tagged
+from odoo.tests import common, HttpCase, tagged
 
 
 @tagged('-at_install', 'post_install')
@@ -10,3 +10,11 @@ class TestAuthSignupUninvited(common.TransactionCase):
         self.env.ref('base.default_website').auth_signup_uninvited = 'b2c'
         config = self.env['res.config.settings'].create({})
         self.assertEqual(config.auth_signup_uninvited, 'b2c')
+
+
+@tagged("post_install", "-at_install")
+class TestAuthFormsWarning(HttpCase):
+
+    def test_auth_forms_warning(self):
+        self.start_tour(self.env["website"].get_client_action_url('/web/login'), 'auth_login_warning', login='admin')
+        self.start_tour(self.env["website"].get_client_action_url('/web/signup'), 'auth_signup_warning', login='admin')
