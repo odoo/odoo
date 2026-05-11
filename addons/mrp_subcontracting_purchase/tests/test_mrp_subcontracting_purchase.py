9df09e2ea6ae72078b5a030055ed7b16d0ffd60c
Zachary (zavan) <zavan@odoo.com>
2026-05-11 11:14:17 +0000

    [FIX] stock,*: properly compute forecast availability after PO resupply scrap
    * : mrp_subcontracting_purchase
    
    # How to reproduce
    - Enable Subcontracting in the settings
    - Create Product A with :
    	- Quantity : > 0
    	- Routes : Buy & Resupply Subcontractor on Order
    - Create Product B
    - Create BOM for that Product with
    	- BOM Type : Subcontracting
    	- Subcontractors : any
    	- Component : Product A
    - Create a PO for Product B
    - Confirm the PO Order
    - Use the Resupply smart button
    - Click on the gear icons > Scrap
    - Scrap Product A
    
    # The problem
    A traceback will appear.
    
    # Cause
    There are two main ways to get the picking type's code of a move. Either :
    
    - `product_code` which is a related field to `picking_id.picking_type_id.code` :
    https://github.com/odoo/odoo/blob/0442c66d26b0c23313f17c566b16e34e7b22c2b6/addons/stock/models/stock_move.py#L174
    
    - `picking_type_id.code` where `picking_type_id` is a computed field :
    https://github.com/odoo/odoo/blob/0442c66d26b0c23313f17c566b16e34e7b22c2b6/addons/stock/models/stock_move.py#L283-L287
    
    When we scrap the products, we call the `do_scrap()` function that creates a new
    scrap move :
    https://github.com/odoo/odoo/blob/0442c66d26b0c23313f17c566b16e34e7b22c2b6/addons/stock/models/stock_scrap.py#L158
    
    When we do so, the create move's `picking_code` wil be the code of the picking type
    of the current picking (The subcontractor resupply) :
    https://github.com/odoo/odoo/blob/0442c66d26b0c23313f17c566b16e34e7b22c2b6/addons/stock/models/stock_scrap.py#L151
    
    But `picking_type_id.code` will be different because there is a
    `default_picking_type_id` value set in the context by :
    https://github.com/odoo/odoo/blob/0442c66d26b0c23313f17c566b16e34e7b22c2b6/addons/purchase_stock/models/purchase_order.py#L223
    
    In our case, theses values end up not being the same. Later, when we compute the forecast
    information of the move, we prefetch virtual available keys and put the moves in a dict
    based on those keys.
    The computation of the virtual available key is based on the `picking_code` of the move :
    https://github.com/odoo/odoo/blob/0442c66d26b0c23313f17c566b16e34e7b22c2b6/addons/stock/models/stock_move.py#L488-L490
    https://github.com/odoo/odoo/blob/0442c66d26b0c23313f17c566b16e34e7b22c2b6/addons/stock/models/stock_move.py#L496-L499
    
    When later we try to fetch back the move, we compute the virtual available key based on
    `picking_type_id.code` :
    https://github.com/odoo/odoo/blob/0442c66d26b0c23313f17c566b16e34e7b22c2b6/addons/stock/models/stock_move.py#L529-L536
    
    But since `picking_code` and `picking_type_id.code` are different, the output
    `key_virtual_available` is also different. Essentially, we add the move in the
    dict with key A and then try to fetch it back using key B, which gives us a KeyError.
    
    opw-6145887
    
    closes odoo/odoo#272171
    
    X-original-commit: 8e035abe08c821057a645fd0f2b282ec234c03c9
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Zachary Vanvlasselaer (zavan) <zavan@odoo.com>

--- addons/mrp_subcontracting_purchase/tests/test_mrp_subcontracting_purchase.py
+++ addons/mrp_subcontracting_purchase/tests/test_mrp_subcontracting_purchase.py
@@ -1104,3 +1104,29 @@ class MrpSubcontractingPurchaseTest(TestAccountSubcontractingFlows):
         self.assertIn(replenish_wizard.route_id, buy_routes)
         manufacture_route = self.env['stock.rule'].search([('action', '=', 'manufacture'), ('company_id', '=', self.company.id)]).route_id
         self.assertNotIn(manufacture_route, replenish_wizard.allowed_route_ids)
+
+    def test_forecast_after_scrap_resupply(self):
+        """Tests that the computation of the forecast_availability of the resupply of a PO
+        does not raise after scraping"""
+        resupply_sub_on_order_route = self.env['stock.route'].search([('name', '=', 'Resupply Subcontractor on Order')])
+        self.comp1.route_ids += resupply_sub_on_order_route
+
+        po = self.env['purchase.order'].create({
+            'partner_id': self.bom.subcontractor_ids.id,
+            'order_line': [Command.create({
+                'product_id': self.finished.id,
+                'product_qty': 1,
+            })],
+        })
+        po.button_confirm()
+        resupply = po._get_subcontracting_resupplies()
+        self.env['stock.quant']._update_available_quantity(self.comp1, resupply.location_id, 1)
+        scrap = self.env['stock.scrap'].with_context(default_picking_type_id=po.picking_type_id.id).create({
+            'product_id': self.comp1.id,
+            'scrap_qty': 1,
+            'picking_id': resupply.id
+        })
+        scrap.action_validate()
+        self.assertEqual(scrap.state, 'done')
+        resupply.move_ids.invalidate_recordset(['forecast_availability'])
+        self.assertRecordValues(resupply.move_ids, [{'forecast_availability': -1.0}, {'forecast_availability': 0.0}])
