57c913e9ba598a2c0f09c13bb3d10ba9bc1b85e1
Pedram (PEBR) <pebr@odoo.com>
2026-05-11 13:21:17 +0000

    [FIX] pos_online_payment: prevent access error on payment method form
    Only admin users have read access to the `payment.provider` model.
    Opening the PoS payment method form as a non-admin would raise an
    access error because the `online_payment_provider_ids` many2many field
    tries to fetch `payment.provider` records on form load.
    
    Grant read-only access on `payment.provider` to `group_pos_manager`
    so POS admins can use the field. Restrict the field's group in the
    form view to `point_of_sale.group_pos_manager,base.group_system` so
    it is not rendered for users without either role.
    
    opw-6208656
    
    X-original-commit: e6f18bb9831ff86818aa8589f73fbb38f9d6917c

--- addons/pos_online_payment/__manifest__.py
+++ addons/pos_online_payment/__manifest__.py
@@ -6,6 +6,8 @@
     'name': 'Point of Sale online payment',
     'depends': ['point_of_sale', 'account_payment'],
     'data': [
+        'security/ir.model.access.csv',
+        'views/payment_provider_views.xml',
         'views/payment_transaction_views.xml',
         'views/pos_payment_views.xml',
         'views/pos_payment_method_views.xml',
