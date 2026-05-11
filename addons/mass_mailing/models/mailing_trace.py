b2e6eaa68f9bb72739ee082ecca091c851732946
Haroune Hassine (Hahas) <hahas@odoo.com>
2026-05-11 17:08:49 +0200

    [MOV] mass_mailing: move view stats method to mailing.trace/link.tracker
    The `_action_view_mailing_statistics_filtered` method serves two
    functionalities: displaying clicks stats, and displaying other KPIs'
    stats.
    
    The purpose of this commit is to split the logic of this method into
    two methods each with one responsibility, and move them to the
    `link.tracker.click` for the clicks' stats, and to the `mailing.trace`
    for the other KPIs' stats.
    
    This will eventually allow for the re-usability of those methods by
    other models.
    
    task-5358279
    
    Part-of: odoo/odoo#257098
    Related: odoo/enterprise#114690
    Related: odoo/upgrade#10095
    Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>

--- addons/mass_mailing/models/mailing_trace.py
+++ addons/mass_mailing/models/mailing_trace.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+from markupsafe import Markup
 
+from odoo import api, fields, models, _
+from odoo.fields import Domain
 
 class MailingTrace(models.Model):
     """ MailingTrace models the statistics collected about emails. Those statistics
@@ -189,3 +191,66 @@ class MailingTrace(models.Model):
         traces = self + (self.search(domain) if domain else self.env['mailing.trace'])
         traces.write({'trace_status': 'cancel'})
         return traces
+
+    def _action_view_mailing_statistics_filtered(self, domain: Domain, view_filter: str):
+        """Display the mailing statistics for the given domain and KPI
+
+        :param domain: the base domain
+        :param view_filter: the KPI to which the statistics are related. Supported filters
+        are: `reply`, `bounce`, `open`, `delivered`, and `sent`
+        Note: if `search_default_filter_name` is passed in the context, the filter is not
+        passed in the domain of the view."""
+        helper_header = None
+        helper_message = None
+        context = {
+            **self.env.context,
+            'create': False,
+            'graph_mode': 'bar',
+            'stacked': True,
+        }
+        if view_filter == 'reply':
+            action_name = _('Mailing Statistics')
+            if not self.env.context.get('search_default_filter_replied'):
+                domain &= Domain('trace_status', '=', 'reply')
+            context = {**context, 'search_default_group_reply_date': True}
+            helper_header = _("No Recipient replied to your mailing yet!")
+            helper_message = _("To track how many replies this mailing gets, make sure "
+                               "its reply-to address belongs to this database.")
+        elif view_filter == 'bounce':
+            action_name = _('Mailing Statistics')
+            if not self.env.context.get('search_default_filter_bounced'):
+                domain &= Domain('trace_status', '=', 'bounce')
+            helper_header = _("No Recipient address bounced yet!")
+            helper_message = _("Bounce happens when a mailing cannot be delivered (fake address, "
+                               "server issues, ...). Check each record to see what went wrong.")
+        elif view_filter == 'open':
+            action_name = _('Mailing Statistics')
+            if not self.env.context.get('search_default_filter_opened'):
+                domain &= Domain('trace_status', 'in', ('open', 'reply'))
+            context = {**context, 'search_default_group_open_date': True}
+            helper_header = _("No Recipient opened your mailing yet!")
+            helper_message = _("Come back once your mailing has been sent to track who opened your mailing.")
+        elif view_filter == 'delivered':
+            action_name = _('Mailing Statistics')
+            if not self.env.context.get('search_default_filter_delivered'):
+                domain &= Domain('trace_status', 'in', ('sent', 'open', 'reply'))
+            helper_header = _("No Recipient received your mailing yet!")
+            helper_message = _("Wait until your mailing has been sent to check how many recipients you managed to reach.")
+        elif view_filter == 'sent':
+            action_name = _('Mailing Statistics')
+            if not self.env.context.get('search_default_filter_sent'):
+                domain &= Domain('sent_datetime', '!=', False)
+
+        action = {
+            'name': action_name,
+            'type': 'ir.actions.act_window',
+            'view_mode': 'graph,list',
+            'res_model': 'mailing.trace',
+            'domain': domain,
+            'context': context,
+        }
+        if helper_header and helper_message:
+            action['help'] = Markup('<p class="o_view_nocontent_smiling_face">%s</p><p>%s</p>') % (
+                helper_header, helper_message,
+            )
+        return action
