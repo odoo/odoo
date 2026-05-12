5f2540780b2d5f891467a1ab6514f9da801647d3
thle-odoo <thle@odoo.com>
2026-05-12 18:10:22 +0200

    [FIX] auth_totp_mail_enforce: assert correct flow
    The `_get_totp_mail_code` method must be used during MFA.
    
    Task-6218044

--- addons/auth_totp_mail_enforce/models/res_users.py
+++ addons/auth_totp_mail_enforce/models/res_users.py
@@ -68,6 +68,10 @@ class Users(models.Model):
 
     def _get_totp_mail_code(self):
         self.ensure_one()
+        assert (
+            self.env.su
+            or (request and not request.session.uid and request.session.get('pre_uid') == self.id)  # nosemgrep: requests-in-models
+        )
 
         key = self._get_totp_mail_key()
 
