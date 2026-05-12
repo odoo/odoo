7ca5b18a21aca439df6a28d6bcf26d771e191e6f
Traina Ludo <ltra@odoo.com>
2026-05-12 09:55:04 +0200

    [FIX] pos_restaurant: enable auto course allocation for scanned products
    ..., point_of_sale
    
    ---
    
    When adding a product by clicking on its card with auto course allocation enabled,
    the product is correctly placed inside its course. However, when adding a product
    by scanning its barcode, the auto course allocation was not applied.
    
    This commit fixes the issue by extracting the auto course allocation logic into a
    separate function and calling it in both cases: clicking on the card and scanning
    the barcode.
    
    ---
    
    Task: https://www.odoo.com/odoo/project/1737/tasks/6197864

--- addons/pos_restaurant/static/src/app/screens/product_screen/product_screen.js
+++ addons/pos_restaurant/static/src/app/screens/product_screen/product_screen.js
@@ -78,40 +78,9 @@ patch(ProductScreen.prototype, {
         this.state.isValidBuffer = Boolean(res);
     },
     async addProductToOrder(product) {
-        const config = this.pos.config;
-        const order = this.pos.getOrder();
-
-        if (!config.module_pos_restaurant || !config.use_course_allocation) {
-            return await super.addProductToOrder(product);
-        }
-
-        const categories = product.pos_categ_ids
-            .map((c) => c.id)
-            .includes(this.pos.selectedCategory?.id)
-            ? [this.pos.selectedCategory]
-            : product.pos_categ_ids;
-        const courseCandidate = categories
-            .map((c) => c.course_id)
-            .filter(Boolean)
-            .sort((a, b) => a.sequence - b.sequence);
-
-        if (courseCandidate.length === 0) {
-            return await super.addProductToOrder(product);
-        }
-
-        let isNew = false;
-        let course = order.course_ids.find((c) => c.name === courseCandidate[0].name);
-        if (!course) {
-            isNew = true;
-            course = this.pos.addCourse({ backendCourse: courseCandidate[0] });
-        }
-
-        order.selectCourse(course);
+        const allocation = this.autoCourseAllocation(product);
         const result = await super.addProductToOrder(product);
-        if (!result && isNew) {
-            course.delete();
-        }
-
+        this.cleanAutoCourseAllocation(result, allocation);
         return result;
     },
 });
