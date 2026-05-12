odoo/odoo

commit e61403f5a3574a09f76d3ef63b0b8ed05a04515d

[FIX] product_margin: Use price_subtotal as base for sale_avg_price
Before the price_unit field was used to compute the sale_avg_price
this field either set with a amount inclusive tax or exclusive tax
This depends what account.tax is used on the account.move.line

When using the price_unit field the sale_avg_price could be wrong
because it average of inclusive tax and exclusive tax amounts

For example:
Invoice A with 19 % Tax exluded ->
Invoice A Line for Product A with a price_unit of 100
Invoice B with 19 % Tax included ->
Invoice A Line for Product A with a price_unit of 119

The amount for the sale_avg_price field is then
(100 + 119) / 2 = 109.5

But it should be 100
Basically the net amounts must be used
(100 + 100) / 2 = 100

Since there is the price_subtotal field on account.move.line
which represents the net amount after discount, this field can be used.

closes odoo/odoo#226994

X-original-commit: d6a8fa0650c8a186e0b81c7a4c1b11d8901cba55
Signed-off-by: Ali Alfie (alal) <alal@odoo.com>

addons/product_margin/tests/test_product_margin.py
@@ -10,29 +10,23 @@ from odoo.tests import tagged
 @tagged('post_install', '-at_install')
 class TestProductMargin(AccountTestInvoicingCommon):
 
-    def test_aggregates(self):
-        model = self.env['product.product']
-        field_names = [
-            'turnover', 'sale_avg_price', 'sale_num_invoiced', 'purchase_num_invoiced',
-            'sales_gap', 'purchase_gap', 'total_cost', 'sale_expected', 'normal_cost',
-            'total_margin', 'expected_margin', 'total_margin_rate', 'expected_margin_rate',
-        ]
-        self.assertEqual(
-            model.fields_get(field_names, ['aggregator']),
-            dict.fromkeys(field_names, {'aggregator': 'sum'}),
-            f"Fields {', '.join(map(repr, field_names))} must be flagged as aggregatable.",
-        )
-
-    def test_product_margin(self):
-        ''' In order to test the product_margin module '''
-
-        supplier = self.env['res.partner'].create({'name': 'Supplier'})
-        customer = self.env['res.partner'].create({'name': 'Customer'})
-        ipad = self.env['product.product'].create({
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.supplier = cls.env['res.partner'].create({'name': 'Supplier'})
+        cls.customer = cls.env['res.partner'].create({'name': 'Customer'})
+        cls.ipad = cls.env['product.product'].create({
             'name': 'Ipad',
             'standard_price': 500.0,
             'list_price': 750.0,
         })
+
+    def test_product_margin(self):
+        ''' In order to test the product_margin module '''
+
+        supplier = self.supplier
+        customer = self.customer
+        ipad = self.ipad
 
         invoices = self.env['account.move'].create([
             {
@@ -96,13 +90,9 @@ class TestProductMargin(AccountTestInvoicingCommon):
         Test that product margins are calculated correctly when move lines
         include negative quantities or prices.
         """
-        supplier = self.env['res.partner'].create({'name': 'Supplier'})
-        customer = self.env['res.partner'].create({'name': 'Customer'})
-        ipad = self.env['product.product'].create({
-            'name': 'Ipad',
-            'standard_price': 1000.0,
-            'list_price': 1000.0,
-        })
+        supplier = self.supplier
+        customer = self.customer
+        ipad = self.ipad
 
         customer_invoice = self.env['account.move'].create([{
                 'move_type': 'out_invoice',
@@ -149,3 +139,36 @@ class TestProductMargin(AccountTestInvoicingCommon):
         results = ipad._compute_product_margin_fields_values()
         self.assertEqual(results[ipad.id]['total_cost'], 250)
         self.assertEqual(results[ipad.id]['total_margin'], 750)
+
+    def test_sale_avg_price(self):
+        customer = self.customer
+        ipad = self.ipad
+        tax_exclude, tax_include = self.env['account.tax'].create([
+            {
+                'name': 'Tax Exclude',
+                'amount': 10.0,
+                'price_include_override': 'tax_excluded',
+            },
+            {
+                'name': 'Tax Include',
+                'amount': 10.0,
+                'price_include_override': 'tax_included',
+            },
+        ])
+        invoices = self.env['account.move'].create([
+            {
+                'move_type': 'out_invoice',
+                'partner_id': customer.id,
+                'invoice_line_ids': [(0, 0, {'product_id': ipad.id, 'quantity': 20.0, 'price_unit': 750.0, 'tax_ids': [(6, 0, tax_exclude.ids)]})],
+            },
+            {
+                'move_type': 'out_invoice',
+                'partner_id': customer.id,
+                'invoice_line_ids': [(0, 0, {'product_id': ipad.id, 'quantity': 10.0, 'price_unit': 110.0, 'tax_ids': [(6, 0, tax_include.ids)]})],
+            },
+        ])
+        invoices.invoice_date = invoices[0].date
+        invoices.action_post()
+        result = ipad._compute_product_margin_fields_values()
+        sale_avg_price = ((20.0 * 750.0) + (10.0 * 100)) / 30.0
+        self.assertEqual(result[ipad.id]['sale_avg_price'], sale_avg_price)

addons/product_margin/models/product_product.py
@@ -123,8 +123,8 @@ class ProductProduct(models.Model):
                 SELECT
                     l.product_id as product_id,
                     SUM(
-                        l.price_unit / (CASE COALESCE(cr.rate, 0) WHEN 0 THEN 1.0 ELSE cr.rate END) *
-                        l.quantity * (CASE WHEN i.move_type IN ('out_invoice', 'in_invoice') THEN 1 ELSE -1 END) * ((100 - l.discount) * 0.01)
+                        l.price_subtotal / (CASE COALESCE(cr.rate, 0) WHEN 0 THEN 1.0 ELSE cr.rate END) *
+                        (CASE WHEN i.move_type IN ('out_invoice', 'in_invoice') THEN 1 ELSE -1 END)
                     ) / NULLIF(SUM(l.quantity * (CASE WHEN i.move_type IN ('out_invoice', 'in_invoice') THEN 1 ELSE -1 END)), 0) AS avg_unit_price,
                     SUM(l.quantity * (CASE WHEN i.move_type IN ('out_invoice', 'in_invoice') THEN 1 ELSE -1 END)) AS num_qty,
                     SUM(CASE WHEN i.move_type = 'out_invoice' THEN -l.balance WHEN i.move_type = 'in_invoice' THEN l.balance ELSE -ABS(l.balance) END) AS total,

commit e5491c5c79aaa2b9f66dc4c411d0414451049e0a

[REV] product_margin: re-add test_aggregates
Commit e61403f5a3 accidentally removed test_aggregates while resolving
a forward-port conflict. Restoring the test to maintain coverage.

no task-id

addons/product_margin/tests/test_product_margin.py
@@ -21,6 +21,19 @@ class TestProductMargin(AccountTestInvoicingCommon):
             'list_price': 750.0,
         })
 
+    def test_aggregates(self):
+        model = self.env['product.product']
+        field_names = [
+            'turnover', 'sale_avg_price', 'sale_num_invoiced', 'purchase_num_invoiced',
+            'sales_gap', 'purchase_gap', 'total_cost', 'sale_expected', 'normal_cost',
+            'total_margin', 'expected_margin', 'total_margin_rate', 'expected_margin_rate',
+        ]
+        self.assertEqual(
+            model.fields_get(field_names, ['aggregator']),
+            {field: {'aggregator': 'sum'} for field in field_names},
+            f"Fields {', '.join(map(repr, field_names))} must be flagged as aggregatable.",
+        )
+
     def test_product_margin(self):
         ''' In order to test the product_margin module '''
 