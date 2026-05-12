93c8aee5f9963fdfdfdf9c78ed1316d1048d72a3
Walid (wasa) <wasa@odoo.com>
2026-05-12 14:15:27 +0000

    [FIX] html_editor: keep link popover title in sync with file name
    Problem:
    After updating a file name, the link popover still shows the original
    file name.
    
    Cause:
    The link popover always displays the attachment name instead of the
    current link content.
    
    Solution:
    Use the link content as the popover title so it reflects the updated
    file title.
    
    Steps to reproduce:
    - Go to To-Do → Create New.
    - Upload a file.
    - Change its title.
    - Observe that the title shown in the link popover still uses the
      original file name.
    
    task-6213840
    
    X-original-commit: 42acdc49bfe8dd4041942df9336cbf7555ce4cd8

--- addons/html_editor/static/src/main/link/link_popover.js
+++ addons/html_editor/static/src/main/link/link_popover.js
@@ -1,6 +1,14 @@
 import { session } from "@web/session";
 import { _t } from "@web/core/l10n/translation";
-import { Component, useState, useRef, useEffect, useExternalListener } from "@odoo/owl";
+import {
+    Component,
+    useState,
+    onMounted,
+    useRef,
+    useEffect,
+    useExternalListener,
+    onWillUnmount,
+} from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
 import { cleanZWChars, deduceURLfromText } from "./utils";
 import { useColorPicker } from "@web/core/color_picker/color_picker";
@@ -22,6 +30,15 @@ const formatColor = (color) => {
     }
     return color;
 };
+
+function useContentChange(el, callback) {
+    onMounted(() => {
+        el.addEventListener("keyup", callback);
+    });
+    onWillUnmount(() => {
+        el.removeEventListener("keyup", callback);
+    });
+}
 
 export class LinkPopover extends Component {
     static template = "html_editor.linkPopover";
@@ -259,6 +276,9 @@ export class LinkPopover extends Component {
             // Listen to pointerdown outside the iframe
             useExternalListener(document, "pointerdown", onPointerDown);
         }
+        useContentChange(this.props.linkElement, () => {
+            this.state.urlTitle = this.props.linkElement.textContent;
+        });
     }
 
     toggleAdvancedOptions() {
@@ -545,9 +565,9 @@ export class LinkPopover extends Component {
             return;
         }
         if (this.isAttachmentUrl()) {
-            const { name, mimetype } = await this.props.getAttachmentMetadata(this.state.url);
+            const { mimetype } = await this.props.getAttachmentMetadata(this.state.url);
             this.resetPreview();
-            this.state.urlTitle = name;
+            this.state.urlTitle = this.props.linkElement.textContent;
             this.state.previewIcon = { type: "mimetype", value: mimetype };
             return;
         }
