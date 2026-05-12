ffd55e7ed723cb04fe24d83d9c711afa004f799f
Alex Kühn <aku@odoo.com>
2026-05-12 18:04:13 +0200

    [REF] mail: reconcile record props to props.record in DiscussAvatar
    Before this commit, passing record to DiscussAvatar requires to
    pass the named prop. For example:
    
    ```xml
    <DiscussAvatar member="member"/>
    <DiscussAvatar channel="channel"/>
    <DiscussAvatar user="user"/>
    <DiscussAvatar t-if="channel or persona" channel="channel" persona="persona"/>
    ```
    
    This commit simplifies passing of props to simply `record`:
    
    ```xml
    <DiscussAvatar record="member"/>
    <DiscussAvatar record="channel"/>
    <DiscussAvatar record="user"/>
    <DiscussAvatar t-if="channel or persona" record="channel or persona"/>
    ```
    
    This makes it easier to see the mandatory prop and the optional props
    for customization, and also makes it easier to add newer records we
    support by just adapting the appropriate colored getters that maps
    `record` to the appropriate typed record.

--- addons/mail/static/src/core/common/discuss_avatar.js
+++ addons/mail/static/src/core/common/discuss_avatar.js
@@ -6,19 +6,33 @@ import { isBrowserSafari } from "@web/core/browser/feature_detection";
 
 let nextId = 0;
 
+/** @typedef {import("models").ChannelMember} ChannelMember */
+/** @typedef {import("models").MailGuest} MailGuest */
+/** @typedef {import("models").DiscussChannel} DiscussChannel */
+/** @typedef {import("models").ResPartner} ResPartner */
+/** @typedef {import("models").ResUsers} ResUsers */
+/** @typedef {import("models").Thread} Thread */
+/** @typedef {ChannelMember|DiscussChannel|MailGuest|ResPartner|ResUsers|Thread} DiscussAvatarRecord */
+
+/**
+ * @typedef {Object} Props
+ * @property {string} [className=""]
+ * @property {string} [iconExtraTransform]
+ * @property {string} [imgRoundedClass]
+ * @property {DiscussAvatarRecord} record
+ * @property {number} [size=32]
+ * @property {boolean} [typing=true]
+ * @extends {Component<Props, Env>}
+ */
 export class DiscussAvatar extends Component {
     static template = "mail.DiscussAvatar";
     static props = [
-        "channel?",
-        "member?",
-        "persona?",
-        "thread?",
+        "className?",
         "iconExtraTransform?",
+        "imgRoundedClass?",
+        "record",
         "size?",
         "typing?",
-        "className?",
-        "imgRoundedClass?",
-        "user?",
     ];
     static defaultProps = { className: "", size: 32, typing: true };
     static components = { ImStatus, ThreadIcon };
@@ -29,16 +43,52 @@ export class DiscussAvatar extends Component {
         this.uniqueId = `mail.DiscussAvatar.${nextId++}`;
     }
 
+    /** @returns {DiscussChannel|undefined} */
     get channel() {
-        return this.props.channel ?? this.props.thread?.channel;
+        const record = this.props.record;
+        if (record.Model.getName() === "discuss.channel") {
+            return record;
+        }
+        if (record.Model.getName() === "mail.thread") {
+            return record.channel;
+        }
+        return undefined;
     }
 
+    /** @returns {ChannelMember|undefined} */
+    get channelMember() {
+        const record = this.props.record;
+        if (record.Model.getName() === "discuss.channel.member") {
+            return record;
+        }
+        return undefined;
+    }
+
+    /** @returns {MailGuest|ResPartner|undefined} */
     get persona() {
-        return this.props.user?.partner_id || this.props.persona || this.props.member?.persona;
+        const record = this.props.record;
+        if (record.Model.getName() === "res.users") {
+            return record.partner_id;
+        }
+        if (["mail.guest", "res.partner"].includes(record.Model.getName())) {
+            return record;
+        }
+        if (record.Model.getName() === "discuss.channel.member") {
+            return record.persona;
+        }
+        return undefined;
     }
 
+    /** @returns {MailThread|undefined} */
     get thread() {
-        return this.props.thread ?? this.props.channel?.thread;
+        const record = this.props.record;
+        if (record.Model.getName() === "mail.thread") {
+            return record;
+        }
+        if (record.Model.getName() === "discuss.channel") {
+            return record.thread;
+        }
+        return undefined;
     }
 
     get isTyping() {
@@ -48,8 +98,8 @@ export class DiscussAvatar extends Component {
         if (this.channel) {
             return this.channel.hasOtherMembersTyping;
         }
-        if (this.props.member) {
-            return this.props.member.isTyping;
+        if (this.channelMember) {
+            return this.channelMember.isTyping;
         }
         return false;
     }
@@ -58,10 +108,18 @@ export class DiscussAvatar extends Component {
         if (this.channel) {
             return this.channel.showThreadIcon({ ignoreTyping: !this.props.typing });
         }
-        return this.props.member?.imStatusUI || this.persona?.imStatusUI;
+        return this.channelMember?.imStatusUI || this.persona?.imStatusUI;
     }
 
+    /** @returns {ResUsers|undefined} */
     get user() {
-        return this.props.user || this.persona?.main_user_id;
+        const record = this.props.record;
+        if (record.Model.getName() === "res.users") {
+            return record;
+        }
+        if (record.Model.getName() === "res.partner") {
+            return record.main_user_id;
+        }
+        return undefined;
     }
 }
