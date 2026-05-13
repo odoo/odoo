8a8a8fbf5541b46b2b51f420b230c813c171a16f
Michele Di Croce <dicroce.m@stesi.eu>
2026-05-13 10:35:58 +0000

    [FIX] l10n_it_edi: inline account.edi.common helpers for FatturaPA
    l10n_it_edi calls self.env['account.edi.common'] for partner and
    bank account import, but does not declare account_edi_ubl_cii as
    a dependency. The FatturaPA import crashes with KeyError when the
    module is absent.
    
    Copy _import_partner and _import_partner_bank from account.edi.common
    into AccountMove as _l10n_it_edi_import_partner and
    _l10n_it_edi_import_partner_bank. The helpers rely on
    res.partner._retrieve_partner and _run_vat_test from account, and
    res.partner.bank._find_or_create_bank_account from base, all
    already reachable through the existing depends chain.
    
    The two calls landed in PR #254505 (commit 63926d9d).
    
    closes odoo/odoo#264478
    
    X-original-commit: d8e4676ebbcacdb89d27f03f797cb5eabd2bd2ff
    Signed-off-by: Paolo Gatti (pgi) <pgi@odoo.com>

--- addons/l10n_it_edi/models/account_move.py
+++ addons/l10n_it_edi/models/account_move.py
@@ -1140,6 +1140,71 @@ class AccountMove(models.Model):
                 return partner
         return self.env['res.partner']
 
+    def _l10n_it_edi_import_partner(self, company_id, name, phone, email, vat, peppol_eas=False, peppol_endpoint=False, postal_address={}):
+        """Retrieve the partner, create one when no match is found.
+
+        Local copy of account.edi.common._import_partner so l10n_it_edi does not
+        need to depend on account_edi_ubl_cii for the FatturaPA import flow.
+        """
+        logs = []
+        if peppol_eas and peppol_endpoint:
+            domain = [('peppol_eas', '=', peppol_eas), ('peppol_endpoint', '=', peppol_endpoint)]
+        else:
+            domain = False
+        partner = self.env['res.partner'] \
+            .with_company(company_id) \
+            ._retrieve_partner(name=name, phone=phone, email=email, vat=vat, domain=domain)
+        country_code = postal_address.get('country_code')
+        country = self.env['res.country'].search([('code', '=', country_code.upper())]) if country_code else self.env['res.country']
+        state_code = postal_address.get('state_code')
+        state = self.env['res.country.state'].search(
+            [('country_id', '=', country.id), ('code', '=', state_code)],
+            limit=1,
+        ) if state_code and country else self.env['res.country.state']
+        if not partner and name and vat:
+            partner_vals = {'name': name, 'email': email, 'phone': phone, 'is_company': True}
+            if peppol_eas and peppol_endpoint:
+                partner_vals.update({'peppol_eas': peppol_eas, 'peppol_endpoint': peppol_endpoint})
+            partner = self.env['res.partner'].create(partner_vals)
+            if vat and self.env['res.partner']._run_vat_test(vat, country, partner.is_company):
+                partner.vat = vat
+            logs.append(_("Could not retrieve a partner corresponding to '%s'. A new partner was created.", name))
+        if not partner.country_id and not partner.street and not partner.street2 and not partner.city and not partner.zip and not partner.state_id:
+            partner.write({
+                'country_id': country.id,
+                'street': postal_address.get('street'),
+                'street2': postal_address.get('additional_street'),
+                'city': postal_address.get('city'),
+                'zip': postal_address.get('zip'),
+                'state_id': state.id,
+            })
+        return partner, logs
+
+    def _l10n_it_edi_import_partner_bank(self, invoice, bank_details):
+        """Search or create the partner bank account for the imported invoice.
+
+        Local copy of account.edi.common._import_partner_bank so l10n_it_edi does
+        not need to depend on account_edi_ubl_cii for the FatturaPA import flow.
+        """
+        if invoice.move_type in ('out_refund', 'in_invoice'):
+            partner = invoice.partner_id
+        elif invoice.move_type in ('out_invoice', 'in_refund'):
+            partner = invoice.company_id.partner_id
+        else:
+            return
+        banks = self.env['res.partner.bank']
+        for account_number in bank_details:
+            try:
+                banks += self.env['res.partner.bank']._find_or_create_bank_account(
+                    account_number=account_number,
+                    partner=partner,
+                    company=invoice.company_id,
+                )
+            except UserError as e:
+                invoice._message_log(body=_("The bank account couldn't be fetched: %s", str(e)))
+        if banks:
+            invoice.partner_bank_id = banks[0]
+
     def _l10n_it_edi_search_tax_for_import(self, company, percentage, extra_domain=None, l10n_it_exempt_reason=None):
         """ Returns the VAT, Withholding or Pension Fund tax that suits the conditions given
             and matches the percentage found in the XML for the company. """
@@ -1350,7 +1415,7 @@ class AccountMove(models.Model):
             # If not found by Codice Fiscale, search for VAT or create, just like all other EDIs do
             if not self.partner_id:
                 import_partner_args = {k: v for k, v in partner_info.items() if k not in ('codice_fiscale', 'is_company')}
-                self.partner_id, logs = self.env['account.edi.common']._import_partner(company_id=company, **import_partner_args)
+                self.partner_id, logs = self._l10n_it_edi_import_partner(company_id=company, **import_partner_args)
                 if logs:
                     self.partner_id.is_company = partner_info.get('is_company')
                     self.partner_id.l10n_it_codice_fiscale = partner_info.get('codice_fiscale', False)
@@ -1410,7 +1475,7 @@ class AccountMove(models.Model):
                     for payment_info in payments_info['info']:
                         # Search / Create the bank account if iban is present
                         if iban := payment_info.get('acc_number'):
-                            self.env['account.edi.common'].with_company(company)._import_partner_bank(self, [iban])
+                            self.with_company(company)._l10n_it_edi_import_partner_bank(self, [iban])
                         # Set payment data on the bill
                         self.payment_reference = self.payment_reference or payment_info.get('payment_code', False)
                         payment_due_dates.append(payment_info.get('invoice_date_due'))
