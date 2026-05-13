de4f11bde7dbb7199ddfd22686315b0f86cbfd8d
Mehdi Outagant (MOU) <mou@odoo.com>
2026-05-13 16:12:56 +0000

    [FIX] website: restore dynamic snippet row layout
    Before [1], the `s_dynamic_snippet_row` class was added to all dynamic
    snippets using the `website.s_dynamic_snippet.grid` template and
    defining `columnClasses` values.
    
    In [1], a fix was introduced to prevent adding this class on
    mono-record snippets, as it was breaking the layout when used inside
    small containers (`o_container_small`).
    
    However, the condition introduced by that fix is too broad and currently
    removes the class from all dynamic snippets.
    
    This commit fixes the condition so that `s_dynamic_snippet_row` is only
    excluded from mono-record snippets, restoring the intended layout for
    other dynamic snippets.
    
    [1]: https://github.com/odoo/odoo/commit/fe2279f760adc6a53ba2242961f3873a5d3215dd
    
    closes odoo/odoo#266804
    
    X-original-commit: 5abc0ae8afa6aad726e12833cd247ca9d59f24f2
    Signed-off-by: Colin Louis (loco) <loco@odoo.com>
    Signed-off-by: Outagant Mehdi (mou) <mou@odoo.com>

--- addons/website/static/src/snippets/s_dynamic_snippet/dynamic_snippet.js
+++ addons/website/static/src/snippets/s_dynamic_snippet/dynamic_snippet.js
@@ -153,6 +153,7 @@ export class DynamicSnippet extends Interaction {
             unique_id: this.uniqueId,
             extraClasses: dataset.extraClasses || "",
             columnClasses: dataset.columnClasses || "",
+            is_single_record: this.isSingleMode,
         };
     }
 
