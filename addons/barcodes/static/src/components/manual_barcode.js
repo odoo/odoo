c8e071cf0d7df5a8d58556d1a4740758a4ff924b
Lucas Gvasalia <lugva@odoo.com>
2026-05-13 11:22:56 +0200

    [IMP] barcodes: created a separate barcode view component
    Created a reusable barcode view component which handles
    BarcodeVideoScanner and BarcodeInput together. It is used by
    stock_barcode. Moveover, for clarity moved BarcodeInput to a separate
    component and made sure the imports have been adjusted. BarcodeInput
    accepts a separate onResult function from BarcodeVideoScanner,
    in case some additional operations like cleaning the barcode are needed.
    
    task-4170451

--- addons/barcodes/static/src/components/manual_barcode.js
+++ addons/barcodes/static/src/components/manual_barcode.js
@@ -1,39 +1,6 @@
 import { BarcodeDialog, barcodeDialogProps } from "@web/core/barcode/barcode_dialog";
-import { Component, onMounted, proxy, signal, t, useProps } from "@odoo/owl";
-import { getActiveHotkey } from "@web/core/hotkeys/hotkey_utils";
-import { _t } from "@web/core/l10n/translation";
-
-export class BarcodeInput extends Component {
-    static template = "barcodes.BarcodeInput";
-    props = useProps({
-        onSubmit: t.function(),
-        placeholder: t.string().optional(_t("Enter a barcode...")),
-    });
-    barcodeManual = signal.ref();
-
-    setup() {
-        this.state = proxy({
-            barcode: false,
-        });
-        // Autofocus processing was blocked because a document already has a focused element.
-        onMounted(() => {
-            this.barcodeManual()?.focus();
-        });
-    }
-
-    /**
-     * Called when press Enter after filling barcode input manually.
-     *
-     * @private
-     * @param {KeyboardEvent} ev
-     */
-    _onKeydown(ev) {
-        const hotkey = getActiveHotkey(ev);
-        if (hotkey === "enter" && this.state.barcode) {
-            this.props.onSubmit(this.state.barcode);
-        }
-    }
-}
+import { BarcodeInput } from "./barcode_input";
+import { useProps, t } from "@odoo/owl";
 
 export class ManualBarcodeScanner extends BarcodeDialog {
     static template = "barcodes.ManualBarcodeScanner";
