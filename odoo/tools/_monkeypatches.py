7e7132fd84d9ace1ac55399a1dc52d03246cae3f
Leloup Loïc (lole) <lole@odoo.com>
2026-05-13 11:03:09 +0200

    [FIX] * : failed Response.json() exception handling
    The requests library uses different libraries to parse json objects depending on
    whether or not the simplejson library is installed
    (https://github.com/psf/requests/blob/dc9dbdfb3434c6e58d48fd102f93e5342308817e/src/requests/compat.py#L74).
    This in turn causes our try/excepts to fail if the simplejson library is installed in the env
    we simply re-raise the json error in case the simplejson library is installed so our try/excepts flows
    are not broken by the existence of a random package
    
    opw-6150349
    
    closes odoo/odoo#264303
    
    Signed-off-by: Loïc Leloup (lole) <lole@odoo.com>

--- odoo/tools/_monkeypatches.py
+++ odoo/tools/_monkeypatches.py
@@ -1,6 +1,8 @@
 import ast
 import os
 import logging
+import requests
+import json
 from email._policybase import _PolicyBase
 from odoo import MIN_PY_VERSION
 from shutil import copyfileobj
@@ -175,3 +177,26 @@ def policy_add(self, other):
 orig_policy_clone = _PolicyBase.clone
 _PolicyBase.clone = policy_clone
 _PolicyBase.__add__ = policy_add
+
+orig_request_json = requests.Response.json
+
+# The requests library uses different libraries to parse json objects depending on
+# whether or not the simplejson library is installed
+# (https://github.com/psf/requests/blob/dc9dbdfb3434c6e58d48fd102f93e5342308817e/src/requests/compat.py#L74).
+# This in turn causes our try/excepts to fail if the simplejson library is installed in the env
+# we simply re-raise the json error in case the simplejson library is installed so our try/excepts flows
+# are not broken by the existence of a random package
+# This is only valid for python versions < 3.10 that install requests==2.25.  This issue
+# was fixed in requests==2.27
+
+try:
+    import simplejson
+except ImportError:
+    pass    # no need to change anything if simplejson isn't installed
+else:
+    def new_json(self, **kwargs):
+        try:
+            return orig_request_json(self, **kwargs)
+        except simplejson.JSONDecodeError as e:
+            raise json.JSONDecodeError(e.msg, e.doc, e.pos)
+    requests.Response.json = new_json
