1250e2196225aadea9a4e0a0252552318efc028f
sbbh-odoo <sbbh@odoo.com>
2026-05-13 06:36:57 +0000

    [FIX] html_editor: restore selection after movenode drop
    Description of the issue:
    - MoveNode restores the selection at the container end position
    - After moving a table, the cursor ends outside the table body
    
    After this commit:
    - Preserve the selection if it was inside the moved node
    - Otherwise place the cursor at the start of the moved node
    
    Steps to reproduce:
    - Create a table in the editor
    - Move the table using Movenode
    - Drop the table and check the cursor position
    - The cursor ends outside the moved table body
    - Select some text in a paragraph
    - Move that paragraph using Movenode
    - The selection is placed at the end of the moved node
    
    task-6215743
    
    X-original-commit: 063db1006bbe856d5e4e392a4cfe5dfb77ec6e48

--- addons/html_editor/static/src/main/movenode_plugin.js
+++ addons/html_editor/static/src/main/movenode_plugin.js
@@ -1,11 +1,12 @@
 import { useNativeDraggable } from "@html_editor/utils/drag_and_drop";
-import { childNodeIndex, endPos } from "@html_editor/utils/position";
+import { childNodeIndex } from "@html_editor/utils/position";
 import { xml } from "@odoo/owl";
 import { Plugin } from "../plugin";
 import { closestElement } from "../utils/dom_traversal";
 import { _t } from "@web/core/l10n/translation";
 import { escape } from "@web/core/utils/strings";
 import { baseContainerGlobalSelector } from "@html_editor/utils/base_container";
+import { getDeepestPosition } from "@html_editor/utils/dom_info";
 
 /** @typedef {import("plugins").CSSSelector} CSSSelector */
 
@@ -429,6 +430,7 @@ export class MoveNodePlugin extends Plugin {
         this.dropzoneHintContainer.replaceChildren();
 
         if (this._currentDropHintElementPosition) {
+            const cursors = this.dependencies.selection.preserveSelection();
             const [position, focusElelement] = this._currentDropHintElementPosition;
             this._currentDropHintElementPosition = undefined;
             const previousParent = movableElement.parentElement;
@@ -460,11 +462,21 @@ export class MoveNodePlugin extends Plugin {
                     previousParent.append(baseContainer);
                 }
             }
-            const selectionPosition = endPos(movableElement);
-            this.dependencies.selection.setSelection({
-                anchorNode: selectionPosition[0],
-                anchorOffset: selectionPosition[1],
-            });
+            // Preserve the selection if it was inside the moved element,
+            // otherwise place the caret at the start of the moved element.
+            const isSelectionInsideMovedNode =
+                movableElement.contains(cursors.anchor.node) &&
+                movableElement.contains(cursors.focus.node);
+            if (isSelectionInsideMovedNode) {
+                cursors.restore();
+            } else {
+                const selectionPosition = getDeepestPosition(movableElement, 0);
+                this.dependencies.selection.setSelection({
+                    anchorNode: selectionPosition[0],
+                    anchorOffset: selectionPosition[1],
+                });
+            }
+            this.dependencies.selection.focusEditable();
             this.dependencies.history.addStep();
         }
     }
