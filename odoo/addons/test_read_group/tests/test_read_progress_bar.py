odoo/odoo

commit b22124789a85eff9fcab07e0c46368dc71dc871d

[REF] core: Deprecated read_group
Rational
========

read_group() has an old API that has many inconsistencies with the new
_read_group() API introduced in https://github.com/odoo/odoo/pull/110737.
In addition, some developers continue to use the old read_group() API in
Python, which is not very efficient and mostly overkill. To provide a
smooth transition, we decided to deprecate read_group() and create a new
dedicated method, formatted_read_group(), which "fixes" some weird
behavior of read_group() and comes closer to the _read_group() API
with a formatted result. This new method is in the web module.

Change
======

The main logic remains the same, formatted_read_group() returns a
formatted version of _read_group() with a list of dictionaries as
return. Each dictionary corresponds to a group containing aggregate/groupby
values and other special values mainly needed for the web client.

The list of API changes between read_group() => formatted_read_group():
- Arguments formatted_read_group() follows the same order and naming
as _read_group() version: inverse position of 'groupby' with 'fields',
'fields' becomes 'aggregates', add 'having' parameter and rename 'orderby'
to 'order'.

- Remove the lazy parameter, which was confusing and no longer necessary.
formatted_read_group() always acts as lazy=false and doesn't return '__context'
key in groups anymore.

- Now, aggregates specifications should be always explicit (Field.aggregator
is now only used by the webclient). The dict naming key always follows this
explicit aggregate specification to avoid confusion. This is also true for
the special __count aggregate, it should be explicitly requested when needed
it and its name key group dict of it will always remain __count (in read_group()
the key name of __count was <many2one>_count when the lazy params were True).

- The key naming feature (return_name:aggregator(field) syntax) doesn't
exist anymore. It added extra complexity for no reason and some overrides

odoo/addons/test_read_group/tests/test_read_progress_bar.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from odoo.tests import common
+from odoo.tools import mute_logger
 
 
 @common.tagged('post_install', '-at_install')
@@ -27,7 +28,7 @@ class TestReadProgressBar(common.TransactionCase):
 
     def test_week_grouping(self):
         """The labels associated to each record in read_progress_bar should match
-        the ones from read_group, even in edge cases like en_US locale on sundays
+        the ones from formatted_read_group, even in edge cases like en_US locale on sundays
         """
         context = {"lang": "en_US"}
         groupby = "date:week"
@@ -41,8 +42,8 @@ class TestReadProgressBar(common.TransactionCase):
             }
         }
 
-        groups = self.Model.with_context(context).read_group(
-            [('name', "like", "testWeekGrouping%")], fields=['date', 'name'], groupby=[groupby])
+        groups = self.Model.with_context(context).formatted_read_group(
+            [('name', "like", "testWeekGrouping%")], groupby=[groupby])
         progressbars = self.Model.with_context(context).read_progress_bar(
             [('name', "like", "testWeekGrouping%")], group_by=groupby, progress_bar=progress_bar)
         self.assertEqual(len(groups), 2)
@@ -54,9 +55,8 @@ class TestReadProgressBar(common.TransactionCase):
             next(record_name for record_name, count in data.items() if count): group_name
             for group_name, data in progressbars.items()
         }
-
-        self.assertEqual(groups[0][groupby], pg_groups["testWeekGrouping_first"])
-        self.assertEqual(groups[1][groupby], pg_groups["testWeekGrouping_second"])
+        self.assertEqual(groups[0][groupby][0], pg_groups["testWeekGrouping_first"])
+        self.assertEqual(groups[1][groupby][0], pg_groups["testWeekGrouping_second"])
 
     def test_simple(self):
         model = self.env['ir.model'].create({
@@ -120,9 +120,9 @@ class TestReadProgressBar(common.TransactionCase):
         # check date aggregation and format
         result = self.env['x_progressbar'].read_progress_bar([], 'x_date:week', progress_bar)
         self.assertEqual(result, {
-            'W1 2019': {'foo': 3, 'bar': 1, 'baz': 1},
-            'W2 2019': {'foo': 3, 'bar': 2, 'baz': 2},
-            'W3 2019': {'foo': 0, 'bar': 0, 'baz': 3},
+            '2018-12-30': {'foo': 3, 'bar': 1, 'baz': 1},
+            '2019-01-06': {'foo': 3, 'bar': 2, 'baz': 2},
+            '2019-01-13': {'foo': 0, 'bar': 0, 'baz': 3},
         })
 
         # add a computed field on model
@@ -144,5 +144,5 @@ class TestReadProgressBar(common.TransactionCase):
             'colors': {'foo': 'success', 'bar': 'warning', 'baz': 'danger'},
         }
         # It is not possible to read_progress_bar with ungroupable fields
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError), mute_logger('odoo.domains'):
             self.env['x_progressbar'].read_progress_bar([], 'x_country_id', progress_bar)

odoo/addons/test_converter/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class Test_ConverterTest_Model(models.Model):
@@ -12,7 +12,7 @@ class Test_ConverterTest_Model(models.Model):
     integer = fields.Integer()
     float = fields.Float()
     numeric = fields.Float(digits=(16, 2))
-    many2one = fields.Many2one('test_converter.test_model.sub', group_expand='_gbf_m2o')
+    many2one = fields.Many2one('test_converter.test_model.sub')
     binary = fields.Binary(attachment=False)
     date = fields.Date()
     datetime = fields.Datetime()
@@ -25,14 +25,6 @@ class Test_ConverterTest_Model(models.Model):
               u"qu'il fait une escale technique à St Claude, on dit:")
     html = fields.Html()
     text = fields.Text()
-
-    # `base` module does not contains any model that implement the functionality
-    # `group_expand`; test this feature here...
-
-    @api.model
-    def _gbf_m2o(self, subs, domain):
-        sub_ids = subs.sudo()._search([], order=subs._order)
-        return subs.browse(sub_ids)
 
 
 class Test_ConverterTest_ModelSub(models.Model):

odoo/addons/test_read_group/models.py
@@ -70,6 +70,8 @@ class Test_Read_GroupOrder(models.Model):
     date = fields.Date()
     company_dependent_name = fields.Char(company_dependent=True)
     many2one_id = fields.Many2one('test_read_group.order')
+    name = fields.Char()
+    fold = fields.Boolean()
 
     @property
     def _order(self):
@@ -82,7 +84,8 @@ class Test_Read_GroupOrderLine(models.Model):
     _name = 'test_read_group.order.line'
     _description = 'Sales order line'
 
-    order_id = fields.Many2one('test_read_group.order', ondelete='cascade')
+    order_id = fields.Many2one('test_read_group.order')
+    order_expand_id = fields.Many2one('test_read_group.order', group_expand='_read_group_expand_full')
     value = fields.Integer()
     date = fields.Date(related='order_id.date')
 
@@ -148,7 +151,7 @@ class Test_Read_GroupRelated_Foo(models.Model):
     bar_name_sudo = fields.Char('bar_name_sudo', related='bar_id.name')
     bar_name = fields.Char('bar_name', related='bar_id.name', related_sudo=False)
 
-    bar_base_ids = fields.Many2many('bar_name', related='bar_id.base_ids')
+    bar_base_ids = fields.Many2many(related='bar_id.base_ids')
 
 
 class Test_Read_GroupRelated_Base(models.Model):

odoo/orm/models.py
@@ -558,7 +558,7 @@ class BaseModel(metaclass=MetaModel):
     """dependencies of models backed up by SQL views
     ``{model_name: field_names}``, where ``field_names`` is an iterable.
     This is only used to determine the changes to flush to database before
-    executing ``search()`` or ``read_group()``. It won't be used for cache
+    executing any search/read operations. It won't be used for cache
     invalidation or recomputing fields.
     """
 
@@ -2630,28 +2630,9 @@ class BaseModel(metaclass=MetaModel):
                 row['__domain'] &= Domain(fullname, '=', row[fullname])
 
     @api.model
-    def _read_group_get_annotated_groupby(self, groupby, lazy):
-        groupby = [groupby] if isinstance(groupby, str) else groupby
-        lazy_groupby = groupby[:1] if lazy else groupby
-
-        annotated_groupby = {}  # Key as the name in the result, value as the explicit groupby specification
-        for group_spec in lazy_groupby:
-            field_name, property_name, granularity = parse_read_group_spec(group_spec)
-            if field_name not in self._fields:
-                raise ValueError(f"Invalid field {field_name!r} on model {self._name!r}")
-            field = self._fields[field_name]
-            if property_name and field.type != 'properties':
-                raise ValueError(f"Property name {property_name!r} has to be used on a property field.")
-            if field.type in ('date', 'datetime'):
-                annotated_groupby[group_spec] = f"{field_name}:{granularity or 'month'}"
-            else:
-                annotated_groupby[group_spec] = group_spec
-        return annotated_groupby
-
-    @api.model
     @api.readonly
     def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
-        """Get the list of records in list view grouped by the given ``groupby`` fields.
+        """Deprecated - Get the list of records in list view grouped by the given ``groupby`` fields.
 
         :param list domain: :ref:`A search domain <reference/orm/domains>`. Use an empty
                      list to match all records.
@@ -2689,7 +2670,10 @@ class BaseModel(metaclass=MetaModel):
         :rtype: [{'field_name_1': value, ...}, ...]
         :raise AccessError: if user is not allowed to access requested information
         """
-
+        warnings.warn(
+            "Since 19.0, read_group is deprecated. Please use _read_group in the backend code or web_read_group for a complete formatted result",
+            DeprecationWarning,
+        )
         groupby = [groupby] if isinstance(groupby, str) else groupby
         lazy_groupby = groupby[:1] if lazy else groupby
 
@@ -2697,7 +2681,21 @@ class BaseModel(metaclass=MetaModel):
         # - Modify `groupby` default value 'month' into specific groupby specification
         # - Modify `fields` into aggregates specification of _read_group
         # - Modify the order to be compatible with the _read_group specification
-        annotated_groupby = self._read_group_get_annotated_groupby(groupby, lazy=lazy)
+        groupby = [groupby] if isinstance(groupby, str) else groupby
+        lazy_groupby = groupby[:1] if lazy else groupby
+
+        annotated_groupby = {}  # Key as the name in the result, value as the explicit groupby specification
+        for group_spec in lazy_groupby:
+            field_name, property_name, granularity = parse_read_group_spec(group_spec)
+            if field_name not in self._fields:
+                raise ValueError(f"Invalid field {field_name!r} on model {self._name!r}")
+            field = self._fields[field_name]
+            if property_name and field.type != 'properties':
+                raise ValueError(f"Property name {property_name!r} has to be used on a property field.")
+            if field.type in ('date', 'datetime'):
+                annotated_groupby[group_spec] = f"{field_name}:{granularity or 'month'}"
+            else:
+                annotated_groupby[group_spec] = group_spec
 
         annotated_aggregates = {  # Key as the name in the result, value as the explicit aggregate specification
             f"{lazy_groupby[0].split(':')[0]}_count" if lazy and len(lazy_groupby) == 1 else '__count': '__count',
@@ -6246,7 +6244,7 @@ class BaseModel(metaclass=MetaModel):
         recordsets are guaranteed to be part of the same prefetch-set.
 
         Provides a convenience method to partition existing recordsets without
-        the overhead of a :meth:`~.read_group`, but performs no aggregation.
+        the overhead of a :meth:`~._read_group`, but performs no aggregation.
 
         .. note:: unlike :func:`itertools.groupby`, does not care about input
                   ordering, however the tradeoff is that it can not be lazy

commit f68f1d9aa49e8ce89dd8e27c978afc3df5468bdb

[FIX] web: fix read_progress_bar key mismatch on date(time) group by
Steps to reproduce
1. Open any kanban view declaring a <progressbar> (e.g. Project > Tasks,
   or Field Service > My Tasks).
2. Group by a date or datetime field with a granularity (e.g.
   Creation Date: Year, planned_date_begin:day).
3. Have records spread across several groups with different state values
   (done, in progress, canceled, ...).
4. Observe the colored progress bar at the top of each column.

Issue
Every column's progress bar renders as a uniform grey block regardless
of the actual state distribution of its records. The effect is visible
only when grouping by a date/datetime field; grouping by stage or
assignees still works.

The kanban relies on two separate RPCs whose keys must match:
formatted_read_group (used by web_read_group) returns the columns the
client stores, and the client uses each column's formatted groupby
value as the lookup key; read_progress_bar returns a dict mapping that
same key to per-state counts.

For a datetime group by with a time granularity,
formatted_read_group goes through _web_read_group_groupby_formatter,
which localizes the naive timestamp in the user's timezone and then
converts it to UTC before strftime, see
https://github.com/odoo/odoo/blob/8d80ed39a2b6250bc5dfbe1f0fe4c84cc1b778ce/addons/web/models/models.py#L698-L726.
On the client, serializeDateTime also formats in UTC, see
https://github.com/odoo/odoo/blob/8d80ed39a2b6250bc5dfbe1f0fe4c84cc1b778ce/addons/web/static/src/core/l10n/dates.js#L455-L462,
so group.serverValue is the UTC-shifted string the server sent, e.g.
"2025-01-01 08:00:00" for a Pacific-tz user on a 2025-year group.

read_progress_bar called _read_group directly, which returns the raw
naive datetime out of date_trunc, see
https://github.com/odoo/odoo/blob/8d80ed39a2b6250bc5dfbe1f0fe4c84cc1b778ce/odoo/orm/models.py#L1764,
so its dict was keyed by str(datetime(2025, 1, 1, 0, 0)) =
"2025-01-01 00:00:00". The UTC-formatted key the client looks up in
_pbCounts[groupValue], see
https://github.com/odoo/odoo/blob/8d80ed39a2b6250bc5dfbe1f0fe4c84cc1b778ce/addons/web/static/src/views/kanban/progress_bar_hook.js#L69,
never matched, per-state counts stayed at zero, and the synthetic
"Other" bucket with color "200" (grey) absorbed the whole group.count,
see
https://github.com/odoo/odoo/blob/8d80ed39a2b6250bc5dfbe1f0fe4c84cc1b778ce/addons/web/static/src/views/kanban/progress_bar_hook.js#L88,
hence the uniform grey bar.

Non-date groupbys (many2one, selection, boolean) are unaffected because
their keys are scalar ids / selection codes that round-trip identically
on both sides. The "None" column also works because both sides emit the
literal string "False".

The regression was introduced by b22124789a85eff9fcab07e0c46368dc71dc871d when read_progress_bar was switched from

addons/web/models/models.py
@@ -1369,18 +1369,22 @@ class Base(models.AbstractModel):
             progress bar field values to the related number of records
         """
         def adapt(value):
-            if isinstance(value, BaseModel):
-                return value.id
+            if isinstance(value, tuple):
+                return value[0]
             return value
 
         result = defaultdict(lambda: dict.fromkeys(progress_bar['colors'], 0))
 
-        for main_group, field_value, count in self._read_group(
+        # formatted_read_group produces the same group_by keys the kanban
+        # client uses to look up progress bar counts, so the two sides match
+        # for every field type (m2o, selection, date granularities, ...).
+        for group in self.formatted_read_group(
             domain, [group_by, progress_bar['field']], ['__count'],
         ):
+            field_value = group[progress_bar['field']]
             if field_value in progress_bar['colors']:
-                group_by_value = str(adapt(main_group))
-                result[group_by_value][field_value] += count
+                group_by_value = str(adapt(group[group_by]))
+                result[group_by_value][field_value] += group['__count']
 
         return result
 

odoo/addons/test_read_group/tests/test_read_progress_bar.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo.fields import Command
 from odoo.tests import common
 from odoo.tools import mute_logger
 
@@ -146,3 +147,54 @@ class TestReadProgressBar(common.TransactionCase):
         # It is not possible to read_progress_bar with ungroupable fields
         with self.assertRaises(ValueError), mute_logger('odoo.domains'):
             self.env['x_progressbar'].read_progress_bar([], 'x_country_id', progress_bar)
+
+    def test_datetime_grouping_with_tz(self):
+        """Test that read_progress_bar keys match formatted_read_group keys
+        when grouping by a datetime field with a time granularity under a
+        non-UTC timezone. formatted_read_group converts the truncated group
+        boundary to UTC before returning it to the web client, so
+        read_progress_bar must go through the same formatter, otherwise the
+        kanban is unable to correlate the per-state counts with their
+        columns and every record falls into the "Other" gray bucket.
+        """
+        ir_model = self.env['ir.model'].create({
+            'model': 'x_progressbar_dt',
+            'name': 'progress_bar datetime',
+            'field_id': [
+                Command.create({
+                    'field_description': 'Planned',
+                    'name': 'x_planned',
+                    'ttype': 'datetime',
+                }),
+                Command.create({
+                    'field_description': 'State',
+                    'name': 'x_state',
+                    'ttype': 'selection',
+                    'selection': "[('foo', 'Foo'), ('bar', 'Bar')]",
+                }),
+            ],
+        })
+        model = self.env[ir_model.model].with_context(tz='Europe/Brussels')
+        model.create([
+            {'x_planned': '2025-01-01 12:00:00', 'x_state': 'foo'},
+            {'x_planned': '2025-01-01 18:00:00', 'x_state': 'bar'},
+            {'x_planned': '2025-06-15 12:00:00', 'x_state': 'foo'},
+        ])
+
+        groupby = 'x_planned:day'
+        progress_bar = {
+            'field': 'x_state',
+            'colors': {'foo': 'success', 'bar': 'warning'},
+        }
+        groups = model.formatted_read_group([], groupby=[groupby])
+        progressbars = model.read_progress_bar(
+            [], group_by=groupby, progress_bar=progress_bar,
+        )
+
+        expected = {group[groupby][0]: group for group in groups}
+        self.assertEqual(set(progressbars), set(expected))
+        day_1, day_2 = sorted(expected)
+        self.assertEqual(dict(progressbars), {
+            day_1: {'foo': 1, 'bar': 1},
+            day_2: {'foo': 1, 'bar': 0},
+        })