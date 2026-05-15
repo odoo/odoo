c1e2594646d53bdc7bb1a6d2e3deaedca30f86e8
Rons <adro@odoo.com>
2026-05-15 17:23:09 +0530

    [FIX] hr_holidays: compute leave hours when Time Off duration is set to hours
    Issue:
    'request_hour_from' and 'request_hour_to' should be computed using the default
     calendar attendance values, but instead they display 12:00 AM, meaning their
    values remain 0.0.
    
    Steps to Reproduce:
    
    - Open the Time Off app.
    - Configure a Time Off Type with the Duration Type (request_unit) set to Hours.
    - Set this Time Off Type as the default one while creating a new Time Off.
    - Create a new Time Off request.
    - Observe that 'request_hour_from' and 'request_hour_to' are not computed
    and display 12:00 AM (0.0).
    
    Root Cause:
    While fixing the issue related to preserving leave hours when changing Time Off
    Types, a regression was introduced.
    
    Related PR: https://github.com/odoo/odoo/pull/227235
    
    During the creation of a new Time Off with the duration type set to Hours
    by default, the field 'request_unit_hours' is already True. This prevents the
    computation of 'request_hour_from' and 'request_hour_to', resulting in
    both values remaining 0.0.
    
    Solution:
    Compute 'request_hour_from' and 'request_hour_to' when their values are
    still 0.0 (initial Time Off creation case), and skip recomputation only when
    switching between Time Off Types in order to preserve manually entered hours.
    
    closes odoo/odoo#264707
    
    Task-id: 6209992
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -257,11 +257,17 @@ class HrLeave(models.Model):
         env_company_calendar = self.env.company.resource_calendar_id
         for leave in self:
             calendar = leave.resource_calendar_id or env_company_calendar
-            if (not leave.request_unit_hours
-                    and leave.employee_id
-                    and leave.request_date_from
-                    and leave.request_date_to
-                    and calendar):
+            if (
+                leave.employee_id
+                and leave.request_date_from
+                and leave.request_date_to
+                and calendar
+                and not (
+                    leave.request_unit_hours
+                    and leave.request_hour_from
+                    and leave.request_hour_to
+                )
+            ):
                 hour_from, hour_to = leave._get_hour_from_to(leave.request_date_from, leave.request_date_to)
                 leave.request_hour_from = hour_from
                 leave.request_hour_to = hour_to
