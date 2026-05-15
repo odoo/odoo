5accf6f2e11506eb0a83c93bd4e9eb4ec000d515
SurryaT10 <sutha@odoo.com>
2026-05-15 10:45:40 +0000

    [FIX] hr_holidays: Remove approving multiple times in group allocation
    Steps to Reproduce:
    1. Go to Time Off > Management > Allocations
    2. Create new group allocation
    3. Invalid operation error
    
    Bug Cause:
    In this commit 1b6f3a1335302ca029ab62a25c7bcff0953b99be, a additional approval line was introduced which tried to approve allocations which were already approved.
    
    Solution:
    Remove this line and move the accrual filter right before the first action approve
    Unit Test for group allocation.
    
    closes odoo/odoo#266015
    
    Task: 6218151
    X-original-commit: 4decec05065e8b4e8ebafa815d90f3bc18575107
    Signed-off-by: Romain Carlier (romc) <romc@odoo.com>

--- addons/hr_holidays/tests/test_allocations.py
+++ addons/hr_holidays/tests/test_allocations.py
@@ -607,3 +607,38 @@ class TestAllocations(TestHrHolidaysCommon):
                 'date_to': date(2024, 6, 30),
             })
             allocation_2.action_approve()
+
+    @users('admin')
+    def test_action_generate_group_allocations(self):
+        self.env.user.write({
+            'company_ids': [(4, self.company.id)]
+        })
+        self.work_entry_type.write({
+            'allocation_validation_type': 'hr'
+        })
+
+        wizard_env = self.env['hr.leave.allocation.generate.multi.wizard'].with_company(self.company)
+        wizard_group = wizard_env.create({
+            'name': 'Company Wide Allocation',
+            'work_entry_type_id': self.work_entry_type.id,
+            'duration': 10,
+        })
+        wizard_group.action_generate_allocations()
+        allocations = self.env['hr.leave.allocation'].search([
+            ('work_entry_type_id', '=', self.work_entry_type.id),
+            ('name', '=', 'Company Wide Allocation')
+        ])
+
+        expected_employee_count = self.env['hr.employee'].search_count([
+            ('company_id', '=', self.company.id)
+        ])
+        self.assertTrue(
+            len(allocations) == expected_employee_count,
+            "Group allocation should generate records for all employees in the company."
+        )
+        self.assertIn(self.employee, allocations.mapped('employee_id'))
+        self.assertIn(self.employee_emp, allocations.mapped('employee_id'))
+        self.assertTrue(
+            all(state == 'validate' for state in allocations.mapped('state')),
+            "All group allocations should be automatically validated for an HR admin."
+        )
