6c0136368473737e9db35edd823616e85091c966
Eve Lin (evlin) <evlin@odoo.com>
2026-05-15 21:24:39 +0000

    [FIX] account: distinct fallback names for dynamic reports
    Problem:
    When adding additional dynamic reports to the “Invoice: Send by Email”
    template, reports without a configured `print_report_name`
    incorrectly use the invoice filename. This is an issue because
    multiple attachments can have the same exact filename.
    
    Example from related ticket: the user attached timesheets to their
    template and both the invoice PDF and timesheet attachment used the
    same filename.
    
    Expected:
    The additional report should use its own fallback filename
    (ex: `timesheets_INV_XXX.pdf`) or its configured
    `print_report_name`.
    
    Actual:
    The additional report uses the invoice filename instead.
    
    To fix this, reports without `print_report_name` now fallback to:
    `<report name>_<invoice name>.pdf` as done in v18.0
    
    Related Ticket: 6207518 and 6175376
    
    closes odoo/odoo#265585
    
    X-original-commit: 24c3a118fd40e4ca9a72b115372d8c1c5cf4db6e
    Signed-off-by: Sven Führ (svfu) <svfu@odoo.com>
    Signed-off-by: Eve Lin (evlin) <evlin@odoo.com>

--- addons/account/models/account_move_send.py
+++ addons/account/models/account_move_send.py
@@ -272,7 +272,10 @@ class AccountMoveSend(models.AbstractModel):
         extra_mail_templates = mail_template.report_template_ids - invoice_template
         attachments = []
         for extra_mail_template in extra_mail_templates:
-            filename = move._get_invoice_report_filename(report=extra_mail_template)
+            if extra_mail_template.print_report_name:
+                filename = move._get_invoice_report_filename(report=extra_mail_template)
+            else:
+                filename = f'{extra_mail_template.name.lower()}_{move.name}.pdf'
             attachments.append({
                 'id': f'placeholder_{extra_mail_template.name.lower()}_{filename}',
                 'name': filename,
