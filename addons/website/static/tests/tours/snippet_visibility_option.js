566fdbc2f44a5ff8062e4691cc1044ffe28459f9
Sébastien (blse) <blse@odoo.com>
2026-05-15 13:21:31 +0200

    [FIX] website: fix correctness of tour `snippet_visibility_option`
    The tour `snippet_visibility_option` was modified in commit
    752f16a0db3ed57714cc3953830a4b92abaee9fc to replace `run` steps with
    `trigger` steps and to remove the delay. The transformation contains
    a mistake about the double negation of `!!isSubentryDisplayed` which
    was turned into a simple negation. The tour passed because the selector
    matches before the effect of the previous steps have taken place, but
    this is not the desired test. Also, the checks around visibility were
    quite weak, the tour assumes that the popup is hidden or visible based
    on the eye in the sidebar, but those were not in sync because bootstrap
    ignores the hide/show when there is a transition showing/hiding the
    modal.
    
    This commit fixes the selector, changes the prose that was not always
    an accurate description of the steps, uses stricter selectors, checks
    the modal is actually hidden or not, and waits for the end of animations
    before trying to hide/show the popup.
    
    task-5149984

--- addons/website/static/tests/tours/snippet_visibility_option.js
+++ addons/website/static/tests/tours/snippet_visibility_option.js
@@ -5,6 +5,18 @@ import {
     registerWebsitePreviewTour,
 } from "@website/js/tours/tour_utils";
 
+function waitEndOfPopupAnimation({ hidden = false } = {}) {
+    return {
+        content: "Wait for end of animation showing popup (hiding/showing disabled during it)",
+        trigger: `:iframe #wrap .s_popup .modal${hidden ? ":not(:visible)" : ""}`,
+        run: async ({ anchor }) => {
+            await new Promise((resolve) =>
+                window.Modal.getOrCreateInstance(anchor)._queueCallback(resolve, anchor)
+            );
+        },
+    };
+}
+
 registerWebsitePreviewTour(
     "snippet_visibility_option",
     {
@@ -27,6 +39,10 @@ registerWebsitePreviewTour(
             run: "click",
         },
         {
+            content: "Check the column is hidden",
+            trigger: ":iframe #wrap .s_popup .o_cc1:not(:visible)",
+        },
+        {
             content: "Click on the banner within the popup snippet.",
             trigger: ":iframe #wrap .s_popup .s_banner",
             run: "click",
@@ -37,14 +53,28 @@ registerWebsitePreviewTour(
             run: "click",
         },
         {
-            content: "Click on the popup snippet to hide",
-            trigger: ".o_we_invisible_root_parent",
+            content: "Check the banner is hidden",
+            trigger: ":iframe #wrap .s_popup .s_banner:not(:visible)",
+        },
+        waitEndOfPopupAnimation(),
+        {
+            content: "Click on the popup entry to hide it",
+            trigger: ".o_we_invisible_root_parent > i.fa-eye",
             run: "click",
         },
         {
-            content: "Click on the popup snippet in the list of invisible elements.",
-            trigger: ".o_we_invisible_root_parent",
+            content: "Check the popup is hidden",
+            trigger: ":iframe #wrap .s_popup .modal:not(:visible)",
+        },
+        waitEndOfPopupAnimation({ hidden: true }),
+        {
+            content: "Click on the popup entry in the list of invisible elements.",
+            trigger: ".o_we_invisible_root_parent > i.fa-eye-slash",
             run: "click",
+        },
+        {
+            content: "Check the popup is visible",
+            trigger: ":iframe #wrap .s_popup .modal:visible",
         },
         {
             content: "Check that only the banner is marked as invisible",
@@ -57,55 +87,82 @@ registerWebsitePreviewTour(
         ...clickOnSave(),
         ...clickOnEditAndWaitEditMode(),
         {
-            content: "Check that only the popup entry is displayed...",
+            content: "Check the popup is hidden",
+            trigger: ":iframe #wrap .s_popup .modal:not(:visible)",
+        },
+        {
+            content: "Check that the popup entry is displayed and marked as invisible",
             trigger: ".o_we_invisible_root_parent > i.fa-eye-slash",
         },
         {
-            content: "...and that it is invisible",
+            content: "Check that the entry for the child of popup is not displayed",
             trigger: "body:not(:has(li .o_we_invisible_entry))",
         },
+        waitEndOfPopupAnimation({ hidden: true }),
         {
             content: "Click on the popup entry to show it.",
-            trigger: ".o_we_invisible_root_parent",
+            trigger: ".o_we_invisible_root_parent > i.fa-eye-slash",
             run: "click",
+        },
+        {
+            content: "Check the popup is visible",
+            trigger: ":iframe #wrap .s_popup .modal:visible",
         },
         {
             content: "Click on the banner entry to show it.",
-            trigger: "li > .o_we_invisible_entry",
+            trigger: "li > .o_we_invisible_entry > i.fa-eye-slash",
             run: "click",
         },
         {
-            trigger: ".o_we_invisible_root_parent i.fa-eye",
+            content: "Check that the popup entry is displayed and marked as visible",
+            trigger: ".o_we_invisible_root_parent > i.fa-eye",
         },
         {
-            trigger: "li .o_we_invisible_entry i.fa-eye",
+            content: "Check that the banner entry is displayed and marked as visible",
+            trigger: "li .o_we_invisible_entry > i.fa-eye",
         },
         {
-            trigger: "li li .o_we_invisible_entry i.fa-eye-slash",
+            content: "Check that the column entry is displayed and marked as invisible",
+            trigger: "li li .o_we_invisible_entry > i.fa-eye-slash",
         },
+        waitEndOfPopupAnimation(),
         {
             content: "Click on the popup entry to hide it.",
-            trigger: ".o_we_invisible_root_parent",
+            trigger: ".o_we_invisible_root_parent > i.fa-eye",
             run: "click",
         },
         {
-            trigger: "li .o_we_invisible_entry",
+            content: "Check the popup is hidden",
+            trigger: ":iframe #wrap .s_popup .modal:not(:visible)",
         },
         {
+            content: "Check that the popup entry is displayed and marked as invisible",
             trigger: ".o_we_invisible_root_parent > i.fa-eye-slash",
         },
         {
+            content: "Check that the entry for the child of popup is not displayed",
+            trigger: "body:not(:has(li .o_we_invisible_entry))",
+        },
+        waitEndOfPopupAnimation({ hidden: true }),
+        {
             content: "Click on the popup entry to show it.",
-            trigger: ".o_we_invisible_root_parent",
+            trigger: ".o_we_invisible_root_parent > i.fa-eye-slash",
             run: "click",
         },
         {
+            content: "Check the popup is visible",
+            trigger: ":iframe #wrap .s_popup .modal:visible",
+        },
+        {
+            content: "Check that the popup entry is displayed and marked as visible",
             trigger: ".o_we_invisible_root_parent i.fa-eye",
         },
         {
+            content: "Check that the banner entry is displayed and marked as visible",
             trigger: "li .o_we_invisible_entry i.fa-eye",
         },
         {
+            content: "Check that the column entry is displayed and marked as invisible",
             trigger: "li li .o_we_invisible_entry i.fa-eye-slash",
         },
     ]
