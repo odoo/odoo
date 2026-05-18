a3717568f0b039df680bf77178a4ea4fc00d1778
Merlin Guillaume <megu@odoo.com>
2026-05-18 13:45:45 +0000

    [FIX] sale: convert combo product extra price
    The total of a sale order containing a combo product that has an extra
    price is not correclty converted to the sale order's pricelist currency
    
    Steps to reproduce:
    1. Install Sales
    2. Go to Invoicing > Configuration > Accounting > Currencies and
        activate currency MXN
    3. Go to Sales > Products > Pricelists and create a new pricelist for
        currency MXN
    4. Go to Sales > Products and create a new combo product "test"
    5. Create a combo choice "combo" with options "Large Cabinet" and extra
        price 10000$
    6. Go to Sales and create a new quotation for customer Acme Corporation
        with product "test" (total is $10,001)
    7. Change the pricelist to MXN and update prices
    8. The total is ~MX$10,018 (it should be ~MX$186,682)
    
    Issue:
    The extra price of a combo product is not converted to the sale order's
    pricelist currency, so we end up adding the price of the product in the
    order's currency with the extra price not converted
    
    Solution:
    Convert the extra price of the combo product to the sale order's
    pricelist currency
    
    opw-6192935
    
    X-original-commit: e8ad754db572815092b1dee23d8f44c8d08c65eb

--- addons/sale/models/sale_order_line.py
+++ addons/sale/models/sale_order_line.py
@@ -872,13 +872,16 @@ class SaleOrderLine(models.Model):
             combo_prices[combo_line.product_template_id.sudo().combo_ids[-1]] += combo_price_delta
         # Add the extra price of this combo item, as well as the extra prices of any `no_variant`
         # attributes to the combo price.
-        return (
-            combo_prices[self.combo_item_id.combo_id]
-            + self.combo_item_id.extra_price
-            + self.product_id._get_no_variant_attributes_price_extra(
-                self.product_no_variant_attribute_value_ids
-            )
+        extra_price = self.combo_item_id.currency_id._convert(
+            from_amount=self.combo_item_id.extra_price
+                        + self.product_id._get_no_variant_attributes_price_extra(
+                            self.product_no_variant_attribute_value_ids
+                        ),
+            to_currency=self.currency_id,
+            company=self.company_id,
+            date=self.order_id.date_order,
         )
+        return (combo_prices[self.combo_item_id.combo_id] + extra_price)
 
     @api.depends("product_id", "product_uom_id", "product_uom_qty")
     def _compute_discount(self):
