f53e1ca2b51779042ccedae9a1f24b5f5415795f
Ramez Ibrahim <raibr@odoo.com>
2026-05-18 12:25:39 +0200

    [IMP] resource: choose company timezone from country
    Use the country's timezone when it’s unambiguous.
    If a country has multiple zones, prefer the current user's timezone.
    Otherwise fall back to UTC.
    Preserve any manually set company timezone.
    
    Task Id : 5972738
    
    closes odoo/odoo#251184
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/resource/models/res_users.py
+++ addons/resource/models/res_users.py
@@ -12,13 +12,3 @@ class ResUsers(models.Model):
     resource_calendar_id = fields.Many2one(
         'resource.calendar', 'Default Working Hours',
         related='resource_ids.calendar_id', readonly=False)
-
-    def write(self, vals):
-        rslt = super().write(vals)
-
-        # If the timezone of the admin user gets set on their first login, also update the timezone of the company
-        if (vals.get('tz') and len(self) == 1 and not self.env.user.login_date
-            and self.env.user == self.env.ref('base.user_admin', False) and self == self.env.user):
-            if self.company_id:
-                self.company_id.tz = vals['tz']
-        return rslt
