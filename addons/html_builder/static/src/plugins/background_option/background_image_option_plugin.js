5912e231faa1a9b128885bc0acbf0d952fd9c920
Benjamin Vray <bvr@odoo.com>
2026-05-18 15:22:15 +0000

    [FIX] html_builder: clear bg filter when removing image
    Steps to reproduce:
    - Edit a website page.
    - Select a section with a background image.
    - Set a gradient color filter on the background image.
    - Remove the background image.
    => The gradient color filter stays in the section DOM.
    
    After this commit, `removeBackgroundImage` directly removes the
    related `.o_we_bg_filter`.
    
    closes odoo/odoo#267417
    
    X-original-commit: 0d6bf49b31a9695332cead53e7d74c198ca51a9c
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Benjamin Vray (bvr) <bvr@odoo.com>

--- addons/html_builder/static/src/plugins/background_option/background_image_option_plugin.js
+++ addons/html_builder/static/src/plugins/background_option/background_image_option_plugin.js
@@ -70,7 +70,7 @@ export class BackgroundImageOptionPlugin extends Plugin {
         oldEditingEl.classList.remove("o_bg_img_opt_repeat");
         oldEditingEl.style.removeProperty("background-size");
         const filterColorAction = this.dependencies.builderActions.getAction("selectFilterColor");
-        const editingElement = this.getResource("get_target_element_providers")[0](oldEditingEl);
+        const editingElement = this.getTargetElement(oldEditingEl);
         const filter = filterColorAction.getValue({ editingElement });
         this.setImageBackground(oldEditingEl, "");
         if (filter) {
@@ -140,7 +140,7 @@ export class BackgroundImageOptionPlugin extends Plugin {
         if (backgroundURL) {
             el.classList.add("oe_img_bg", "o_bg_img_center", "o_bg_img_origin_border_box");
         } else {
-            const editingElement = this.getResource("get_target_element_providers")[0](el);
+            const editingElement = this.getTargetElement(el);
             this.dependencies.builderActions
                 .getAction("selectFilterColor")
                 .apply({ editingElement });
@@ -165,6 +165,7 @@ export class BackgroundImageOptionPlugin extends Plugin {
      * @param {Object} [context.params]
      */
     removeBackgroundImage({ editingElement, params }) {
+        this.getTargetElement(editingElement).querySelector(":scope > .o_we_bg_filter")?.remove();
         this.applyReplaceBackgroundImage({
             editingElement,
             loadResult: "",
@@ -175,6 +176,15 @@ export class BackgroundImageOptionPlugin extends Plugin {
         editingElement.classList.remove("o_bg_img_opt_repeat");
         editingElement.style.removeProperty("background-size");
         this.dispatchTo("on_bg_image_hide_handlers", editingElement);
+    }
+
+    getTargetElement(el) {
+        for (const fn of this.getResource("get_target_element_providers")) {
+            const targetEl = fn(el);
+            if (targetEl) {
+                return targetEl;
+            }
+        }
     }
 }
 
