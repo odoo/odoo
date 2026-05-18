a767f7c45d54ad452cca38ad4b10e8b7fe66cdd3
Ajit Singh <aksi@odoo.com>
2026-05-18 17:20:58 +0530

    [FIX] pos_hr: compute employees per company
    Step to reproduce:
    - install point_of_sale with demo
    - have two company and 1 pos in each company
    - install pos_hr
    - install pos_sale
    
    Observation:
    - we get a traceback
    ```
    File "/src/saas-19.2/odoo/orm/fields_misc.py", line 115, in __get__
        raise ValueError("Expected singleton: %s" % record) from None
    ValueError: Expected singleton: res.company(7, 8)
     ```
    - and pos_sale is not installed
    
    Cause:
    - when installing `pos_sale`  `_ensure_downpayment_product` is called.
    - this sets downpayment product on every `pos.config` record.
    - this triggers write call from pos_hr, which tries to identify users for pos
    - this uses `with_company(self.company)`   <-----  actual issue
    - here self has two config, each from different company, hence `with_company`
    raises singleton error
    
    why not in earlier verison:
    - before commit [1] we only set downpayment product on only pos_config_main
    - now we set it on every pos.config, so now self contain multiple records
    
    [1] https://github.com/odoo/odoo/commit/a3f9114434c7cab8757b0283698662792e6b7946
    
    Fix:
    - compute users per config
    
    opw-6217135

--- addons/pos_hr/models/pos_config.py
+++ addons/pos_hr/models/pos_config.py
@@ -19,32 +19,34 @@ class PosConfig(models.Model):
         help='Employees linked to users with the PoS Manager role are automatically added to this list')
 
     def write(self, vals):
+        sudo_fields = ('minimal_employee_ids', 'basic_employee_ids', 'advanced_employee_ids')
+        res = True
+
         if 'advanced_employee_ids' not in vals:
             vals['advanced_employee_ids'] = []
-        group_users = self.sudo()._get_group_pos_manager().with_company(self.company_id).user_ids.filtered(
-            lambda u: self.company_id in u.company_ids
-        )
-        allowed_employees = group_users.sudo().mapped('employee_id')
-        if not allowed_employees and group_users:
-            target_user = group_users.sudo().with_company(self.company_id).filtered(lambda user: not user.employee_id)[0]
-            target_user.action_create_employee()
-            allowed_employees = target_user.employee_id
+        pos_manager_group = self.sudo()._get_group_pos_manager()
+        for config in self:
+            config_vals = dict(vals)
+            group_users = pos_manager_group.with_company(config.company_id).user_ids.filtered(
+                lambda u: config.company_id in u.company_ids
+            )
+            allowed_employees = group_users.employee_id
+            if not allowed_employees and group_users:
+                target_user = group_users.with_company(config.company_id).filtered(lambda user: not user.employee_id)[0]
+                target_user.action_create_employee()
+                allowed_employees = target_user.employee_id
 
-        # Update the vals list
-        vals['advanced_employee_ids'] += [(4, emp.id) for emp in allowed_employees]
-
-        # write employees in sudo, because we have no access to these corecords
-        sudo_vals = {
-            field_name: vals.pop(field_name)
-            for field_name in ('minimal_employee_ids', 'basic_employee_ids', 'advanced_employee_ids')
-            if not self.env.su
-            if isinstance(vals.get(field_name), list)
-            if all(isinstance(cmd, (list, tuple)) for cmd in vals[field_name])
-        }
-
-        res = super().write(vals)
-        if sudo_vals:
-            super(PosConfig, self.sudo()).write(sudo_vals)
+            config_vals['advanced_employee_ids'] += [(4, emp.id) for emp in allowed_employees]
+            sudo_vals = {
+                field_name: config_vals.pop(field_name)
+                for field_name in sudo_fields
+                if not config.env.su
+                if isinstance(config_vals.get(field_name), list)
+                if all(isinstance(cmd, (list, tuple)) for cmd in config_vals[field_name])
+            }
+            res &= super().write(config_vals)
+            if sudo_vals:
+                super(PosConfig, config.sudo()).write(sudo_vals)
         return res
 
     @api.onchange('minimal_employee_ids')
