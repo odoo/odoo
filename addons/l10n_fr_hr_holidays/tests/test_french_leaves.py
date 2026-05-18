alkuneemi/Luxara

[FIX] l10n_fr_hr_holidays: correct leave duration for half-day time off types
Steps to reproduce:
-------------------------
1. Install l10n_fr_hr_holidays with demo data and switch to a French company.
2. Create a 40h working schedule and assign it to the company.
3. Create a 21h working schedule and assign it to a new employee.
   (Use full-day periods without morning/afternoon split for both schedules.)
4. Create a half-day time off type and assign it to the company's
paid time off type (Disable "Requires allocation" for simplification).
5. Create a full-day leave request using this time off type.

Observation:
----------------
The leave duration is incorrectly computed as a decimal value
(e.g., 0.875) instead of 1 day.

Cause:
---------
`request_unit_half` is now True for any leave of type "half_day", regardless
of the actual request span.
https://github.com/odoo/odoo/blob/14ccd1f54e21c07ac68593c8dec7682371f8663c/addons/hr_holidays/models/hr_leave.py#L467-L470
However, `_get_durations` in `l10n_fr_hr_holidays` was not updated accordingly.
https://github.com/odoo/odoo/blob/30b4edace6b0859cb1b1ba4f7f2ea80ba5398e3d/addons/l10n_fr_hr_holidays/models/hr_leave.py#L148-L150

This caused the override of `_get_durations` to always trigger the
standard half-day calculation logic, even for full-day or multi-day requests:
https://github.com/odoo/odoo/blob/30b4edace6b0859cb1b1ba4f7f2ea80ba5398e3d/addons/resource/models/resource_calendar.py#L638
`duration_days * interval_hours / duration_hours` -> `1 * 7 / 8 = 0.875`

Solution:
------------
Update the condition to strictly check if the request is within the same period
before applying the standard half-day logic.

opw-6172402

addons/l10n_fr_hr_holidays/tests/test_french_leaves.py
@@ -581,3 +581,59 @@ class TestFrenchLeaves(TransactionCase):
         self.assertEqual(leave.date_from.date(), date(2026, 1, 12))
         self.assertEqual(leave.date_to.date(), date(2026, 1, 12))
         self.assertEqual(leave.number_of_hours, 8.0, 'Duration should be 8 hours (one working day)')
+
+    def test_leave_type_half_day_different_working_hours_with_full_day_period(self):
+        """
+        Test that leave duration is correctly computed for half-day time off types
+        when the employee has a different working schedule than the company.
+
+        Setup:
+        - Company calendar: 40h/week (8h/day, Mon-Fri)
+        - Employee calendar: 21h/week (7h/day, Mon-Wed)
+
+        Scenarios:
+        1. Full-day request: Should return 1.0 day (not 0.875), even if hours per day differ.
+        2. Actual half-day request: Should remain 0.5 days using standard logic.
+        3. End-of-contract-week request: Should apply French legal extension, counting
+           from the last work day (Wed) until the company's next work day (Fri).
+        """
+        company_calendar = self.env['resource.calendar'].create({
+            'name': 'Flexible 40h/week',
+            'hours_per_day': 8,
+            'hours_per_week': 40.0,
+            'full_time_required_hours': 40.0,
+            'flexible_hours': True,
+        })
+        employee_calendar = self.env['resource.calendar'].create({
+            'name': 'Employee Calendar',
+            'attendance_ids': [
+                (0, 0, {'name': 'Monday', 'dayofweek': '0', 'hour_from': 8, 'hour_to': 15, 'day_period': 'full_day'}),
+                (0, 0, {'name': 'Tuesday', 'dayofweek': '1', 'hour_from': 8, 'hour_to': 15, 'day_period': 'full_day'}),
+                (0, 0, {'name': 'Wednesday', 'dayofweek': '2', 'hour_from': 8, 'hour_to': 15, 'day_period': 'full_day'}),
+            ],
+        })
+
+        self.company.resource_calendar_id = company_calendar
+        self.employee.resource_calendar_id = employee_calendar
+
+        test_leave = self.env['hr.leave'].create({
+            'name': 'Test leave',
+            'holiday_status_id': self.time_off_type.id,
+            'employee_id': self.employee.id,
+            'request_date_from': '2026-05-04',
+            'request_date_to': '2026-05-04',
+        })
+        self.assertEqual(test_leave.number_of_days, 1, 'The duration should be 1 day.')
+
+        test_leave.request_date_from_period = 'am'
+        test_leave.request_date_to_period = 'am'
+        self.assertEqual(test_leave.number_of_days, 0.5, 'The duration should be 0.5 day.')
+
+        last_day_leave = self.env['hr.leave'].create({
+            'name': 'Test last day leave',
+            'holiday_status_id': self.time_off_type.id,
+            'employee_id': self.employee.id,
+            'request_date_from': '2026-05-06',
+            'request_date_to': '2026-05-06',
+        })
+        self.assertEqual(last_day_leave.number_of_days, 3, 'The duration should be 3 day.')

odoo/odoo

addons/l10n_fr_hr_holidays/tests/test_french_leaves.py
@@ -581,3 +581,59 @@ class TestFrenchLeaves(TransactionCase):
         self.assertEqual(leave.date_from.date(), date(2026, 1, 12))
         self.assertEqual(leave.date_to.date(), date(2026, 1, 12))
         self.assertEqual(leave.number_of_hours, 8.0, 'Duration should be 8 hours (one working day)')
+
+    def test_leave_type_half_day_different_working_hours_with_full_day_period(self):
+        """
+        Test that leave duration is correctly computed for half-day time off types
+        when the employee has a different working schedule than the company.
+
+        Setup:
+        - Company calendar: 40h/week (8h/day, Mon-Fri)
+        - Employee calendar: 21h/week (7h/day, Mon-Wed)
+
+        Scenarios:
+        1. Full-day request: Should return 1.0 day (not 0.875), even if hours per day differ.
+        2. Actual half-day request: Should remain 0.5 days using standard logic.
+        3. End-of-contract-week request: Should apply French legal extension, counting
+           from the last work day (Wed) until the company's next work day (Fri).
+        """
+        company_calendar = self.env['resource.calendar'].create({
+            'name': 'Flexible 40h/week',
+            'hours_per_day': 8,
+            'hours_per_week': 40.0,
+            'full_time_required_hours': 40.0,
+            'flexible_hours': True,
+        })
+        employee_calendar = self.env['resource.calendar'].create({
+            'name': 'Employee Calendar',
+            'attendance_ids': [
+                (0, 0, {'name': 'Monday', 'dayofweek': '0', 'hour_from': 8, 'hour_to': 15, 'day_period': 'full_day'}),
+                (0, 0, {'name': 'Tuesday', 'dayofweek': '1', 'hour_from': 8, 'hour_to': 15, 'day_period': 'full_day'}),
+                (0, 0, {'name': 'Wednesday', 'dayofweek': '2', 'hour_from': 8, 'hour_to': 15, 'day_period': 'full_day'}),
+            ],
+        })
+
+        self.company.resource_calendar_id = company_calendar
+        self.employee.resource_calendar_id = employee_calendar
+
+        leave = self.env['hr.leave'].create({
+            'name': 'Test leave',
+            'holiday_status_id': self.time_off_type.id,
+            'employee_id': self.employee.id,
+            'request_date_from': '2026-05-04',
+            'request_date_to': '2026-05-04',
+        })
+        self.assertEqual(leave.number_of_days, 1, 'The duration should be 1 day.')
+
+        leave.request_date_from_period = 'am'
+        leave.request_date_to_period = 'am'
+        self.assertEqual(leave.number_of_days, 0.5, 'The duration should be 0.5 day.')
+
+        last_day_leave = self.env['hr.leave'].create({
+            'name': 'Test last day leave',
+            'holiday_status_id': self.time_off_type.id,
+            'employee_id': self.employee.id,
+            'request_date_from': '2026-05-06',
+            'request_date_to': '2026-05-06',
+        })
+        self.assertEqual(last_day_leave.number_of_days, 3, 'The duration should be 3 days.')