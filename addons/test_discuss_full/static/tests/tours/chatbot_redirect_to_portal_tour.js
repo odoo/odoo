04cf4c315252bd1feaa917b21308657b46425256
Didier (did) <did@odoo.com>
2026-05-18 10:31:13 +0200

    [IMP] im_livechat: open redirect_link in a new tab
    Before this PR, the redirect link was opened inside the current tab,
    interrupting the ongoing conversation.
    
    This PR makes it so it opens in a new tab.
    
    task-4805377
    
    closes odoo/odoo#264904
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/test_discuss_full/static/tests/tours/chatbot_redirect_to_portal_tour.js
+++ addons/test_discuss_full/static/tests/tours/chatbot_redirect_to_portal_tour.js
@@ -1,30 +1,40 @@
 import { registry } from "@web/core/registry";
+import { patch } from "@web/core/utils/patch";
+import { browser } from "@web/core/browser/browser";
+import { Chatbot } from "@im_livechat/core/common/chatbot_model";
 
 registry.category("web_tour.tours").add("chatbot_redirect_to_portal", {
-    steps: () => [
-        {
-            trigger: ".o-livechat-root:shadow .o-livechat-LivechatButton",
-            run: "click",
-        },
-        {
-            trigger: ".o-livechat-root:shadow .o-mail-ChatWindow-header:contains(Redirection Bot)",
-        },
-        {
-            trigger:
-                ".o-livechat-root:shadow .o-mail-Message:contains(Hello, were do you want to go?)",
-            run: "click",
-        },
-        {
-            trigger: ".o-livechat-root:shadow li button:contains(Go to the portal page)",
-            run: "click",
-            expectUnloadPage: true,
-        },
-        {
-            trigger: ".o-livechat-root:shadow .o-mail-Message:contains('Go to the portal page')",
-        },
-        { trigger: "#chatterRoot:shadow .o-mail-Chatter" },
-        {
-            trigger: ".o-livechat-root:shadow .o-mail-Message:last:contains('Tadam')",
-        },
-    ],
+    steps: () => {
+        patch(Chatbot.prototype, {
+            redirect: (url) => browser.open(url, "_self"),
+        });
+        return [
+            {
+                trigger: ".o-livechat-root:shadow .o-livechat-LivechatButton",
+                run: "click",
+            },
+            {
+                trigger:
+                    ".o-livechat-root:shadow .o-mail-ChatWindow-header:contains(Redirection Bot)",
+            },
+            {
+                trigger:
+                    ".o-livechat-root:shadow .o-mail-Message:contains(Hello, were do you want to go?)",
+                run: "click",
+            },
+            {
+                trigger: ".o-livechat-root:shadow li button:contains(Go to the portal page)",
+                run: "click",
+                expectUnloadPage: true,
+            },
+            {
+                trigger:
+                    ".o-livechat-root:shadow .o-mail-Message:contains('Go to the portal page')",
+            },
+            { trigger: "#chatterRoot:shadow .o-mail-Chatter" },
+            {
+                trigger: ".o-livechat-root:shadow .o-mail-Message:last:contains('Tadam')",
+            },
+        ];
+    },
 });
--- addons/website_livechat/static/tests/tours/website_livechat_chatbot_redirect.js
+++ addons/website_livechat/static/tests/tours/website_livechat_chatbot_redirect.js
@@ -1,60 +1,70 @@
 import { registry } from "@web/core/registry";
+import { patch } from "@web/core/utils/patch";
+import { browser } from "@web/core/browser/browser";
+import { Chatbot } from "@im_livechat/core/common/chatbot_model";
 
 registry.category("web_tour.tours").add("website_livechat.chatbot_redirect", {
-    steps: () => [
-        {
-            trigger: ".o-livechat-root:shadow .o-livechat-LivechatButton",
-            run: "click",
-        },
-        {
-            trigger:
-                ".o-livechat-root:shadow .o-mail-Message:contains(Hello, were do you want to go?)",
-            run: "click",
-        },
-        {
-            trigger: ".o-livechat-root:shadow button:contains(Go to the #chatbot-redirect anchor)",
-            run: "click",
-        },
-        {
-            trigger:
-                ".o-livechat-root:shadow .o-mail-Message:contains(Tadam, we are on the page you asked for!)",
-            run() {
-                const url = new URL(location.href);
-                if (url.pathname !== "/contactus" || url.hash !== "#chatbot-redirect") {
-                    throw new Error(
-                        "Chatbot should have redirected to the #chatbot-redirect anchor."
-                    );
-                }
+    steps: () => {
+        patch(Chatbot.prototype, {
+            redirect: (url) => browser.open(url, "_self"),
+        });
+        return [
+            {
+                trigger: ".o-livechat-root:shadow .o-livechat-LivechatButton",
+                run: "click",
             },
-        },
-        {
-            trigger: ".o-livechat-root:shadow button[title='Restart Conversation']",
-            run: "click",
-        },
-        {
-            trigger: ".o-livechat-root:shadow button:contains(Go to the /chatbot-redirect page)",
-            run: "click",
-            expectUnloadPage: true,
-        },
-        {
-            isActive: ["mobile"], //chatwindow is folded on mobile
-            trigger: ".o-livechat-root:shadow .o-mail-ChatBubble[name='Redirection Bot']",
-            run: "click",
-        },
-        {
-            trigger:
-                ".o-livechat-root:shadow .o-mail-Message:contains('Go to the /chatbot-redirect page')",
-        },
-        {
-            trigger: ".o-livechat-root:shadow .o-mail-Message:last:contains('Tadam')",
-            run() {
-                const url = new URL(location.href);
-                if (url.pathname !== "/chatbot-redirect") {
-                    throw new Error(
-                        "Chatbot should have redirected to the /chatbot-redirect page."
-                    );
-                }
+            {
+                trigger:
+                    ".o-livechat-root:shadow .o-mail-Message:contains(Hello, were do you want to go?)",
+                run: "click",
             },
-        },
-    ],
+            {
+                trigger:
+                    ".o-livechat-root:shadow button:contains(Go to the #chatbot-redirect anchor)",
+                run: "click",
+            },
+            {
+                trigger:
+                    ".o-livechat-root:shadow .o-mail-Message:contains(Tadam, we are on the page you asked for!)",
+                run() {
+                    const url = new URL(location.href);
+                    if (url.pathname !== "/contactus" || url.hash !== "#chatbot-redirect") {
+                        throw new Error(
+                            "Chatbot should have redirected to the #chatbot-redirect anchor."
+                        );
+                    }
+                },
+            },
+            {
+                trigger: ".o-livechat-root:shadow button[title='Restart Conversation']",
+                run: "click",
+            },
+            {
+                trigger:
+                    ".o-livechat-root:shadow button:contains(Go to the /chatbot-redirect page)",
+                run: "click",
+                expectUnloadPage: true,
+            },
+            {
+                isActive: ["mobile"], //chatwindow is folded on mobile
+                trigger: ".o-livechat-root:shadow .o-mail-ChatBubble[name='Redirection Bot']",
+                run: "click",
+            },
+            {
+                trigger:
+                    ".o-livechat-root:shadow .o-mail-Message:contains('Go to the /chatbot-redirect page')",
+            },
+            {
+                trigger: ".o-livechat-root:shadow .o-mail-Message:last:contains('Tadam')",
+                run() {
+                    const url = new URL(location.href);
+                    if (url.pathname !== "/chatbot-redirect") {
+                        throw new Error(
+                            "Chatbot should have redirected to the /chatbot-redirect page."
+                        );
+                    }
+                },
+            },
+        ];
+    },
 });
