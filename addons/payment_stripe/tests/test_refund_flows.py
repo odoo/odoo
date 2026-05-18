31b7edc96338d78b837e2606663e3a48d0b466fb
Krzysztof Magusiak (krma) <krma@odoo.com>
2026-05-18 08:54:55 +0000

    [FIX] orm: keep inactive records in many2many cache on write
    When writing on a many2many field, the cache is updated with the
    resulting new relation ids. The ids are computed with sudo to avoid
    interferrence with filtering using access rules. For active_test, we
    just compute the difference and use the flag to do it. When the flag is
    True, we miss inactive records for the `_update_cache` call.
    
    X-original-commit: ad1a34a021b09c348206be8e54f4ae84ace3a755

--- addons/payment_stripe/tests/test_refund_flows.py
+++ addons/payment_stripe/tests/test_refund_flows.py
@@ -53,6 +53,7 @@ class TestRefundFlows(StripeCommon, PaymentHttpCommon):
         "odoo.addons.payment_stripe.models.payment_transaction",
     )
     def test_void_webhook_notification_does_not_trigger_processing(self):
+        self.provider.payment_method_ids.active = False  # deactivate default payment methods
         self.provider.capture_manually = True
         tx = self._create_transaction("direct", state="authorized")
         url = self._build_url(StripeController._webhook_url)
