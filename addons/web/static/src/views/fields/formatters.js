e4be285c89c471dcf8ca7c10f4f1c92e97b89bce
Bastien PIERRE <ipb@odoo.com>
2026-05-19 12:51:22 +0200

    [FIX] web: list groups formatted according to field widget with options
    Before this commit, the values of groups in list view didn't get the options
    of the widget.
    
    Now, the groups extract the options of the column.
    The fact that the groups use the formatter of the widget now,
    show that there was an issue between the widget percentage and
    his formatter. The formatter, by default, show the trailing zero,
    but the widget, by default, doesn't. So, formatter has been fixed
    to be like the widget behavior.
    
    TASK-6226377

--- addons/web/static/src/views/fields/formatters.js
+++ addons/web/static/src/views/fields/formatters.js
@@ -443,7 +443,10 @@ export function formatPercentage(value, options = {}) {
     const formatted = formatFloatNumber(value * 100, options);
     return `${formatted}${options.noSymbol ? "" : "%"}`;
 }
-formatPercentage.extractOptions = formatFloat.extractOptions;
+formatPercentage.extractOptions = ({options}) => ({
+    ...formatFloat.extractOptions,
+    trailingZeros: !(options.hide_trailing_zeros ?? true),
+});
 
 /**
  * Returns a string representing the value of the python properties field
