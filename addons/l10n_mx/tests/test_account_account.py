ddc35e0065b28288e18876685c77559f7e75529a
assh-odoo <assh@odoo.com>
2026-05-19 09:03:41 +0000

    [FIX] l10n_mx: prevent error when creating an account
    Currently, an error occurs when a user creates an account record.
    
    Steps to Reproduce:
     - Install Accounting and l10n_mx module with demo data.
     - Switch to ZAPATERIA URTADO ÑERI company.
     - Go to Accounting > Configuration > Accounting > Chart of Accounts
       and create a record.
    
    TypeError: 'bool' object is not subscriptable
    
    After a [recent commit], account codes became optional and can be removed.
    As a result, when a user creates an account without a code, the code
    attempts to check whether the first letter of the account code is in the debit
    codes. Since the account code is False, it raises an error.
    
    This commit ensures that if the internal group of an account is asset or
    expense, the debit tag is assigned to the account. If the internal group
    is off balance, no tag is assigned. Otherwise, the credit tag is assigned
    to the account.
    
    [recent commit]: https://github.com/odoo/odoo/commit/c3313b336b9f1305c363097745926f2bdf61e277
    
    [1]: https://github.com/odoo/odoo/blob/a60643538479caf29aefeec89b8d356d8d5439c7/addons/l10n_mx/models/account_account.py#L17-L20
    
    sentry-7490691483
    
    X-original-commit: 95b681d846249f036f1337bb831678e682ee0fb4

---
+++ addons/l10n_mx/tests/test_account_account.py
@@ -0,0 +1,40 @@
+from odoo import Command
+from odoo.tests import tagged
+
+from odoo.addons.l10n_mx.tests.common import TestMxCommon
+
+
+@tagged('post_install_l10n', 'post_install', '-at_install')
+class TestAccountAccount(TestMxCommon):
+
+    def test_create_account_without_code(self):
+        """
+        Test that creating an account without a code assigns the correct tag
+        based on the account internal group.
+        """
+        debit_account, credit_account, off_balance_account = self.env['account.account'].create([
+            {
+                'name': "Debit Account",
+                'account_type': 'asset_receivable',
+                'company_ids': [Command.link(self.company_data['company'].id)],
+            },
+            {
+                'name': "Credit Account",
+                'account_type': 'liability_current',
+                'company_ids': [Command.link(self.company_data['company'].id)],
+            },
+            {
+                'name': "Off Balance Account",
+                'account_type': 'off_balance',
+                'company_ids': [Command.link(self.company_data['company'].id)],
+            },
+        ])
+
+        self.assertEqual(debit_account.internal_group, 'asset')
+        self.assertEqual(debit_account.tag_ids, self.env.ref('l10n_mx.tag_debit_balance_account'))
+
+        self.assertEqual(credit_account.internal_group, 'liability')
+        self.assertEqual(credit_account.tag_ids, self.env.ref('l10n_mx.tag_credit_balance_account'))
+
+        self.assertEqual(off_balance_account.internal_group, 'off')
+        self.assertFalse(off_balance_account.tag_ids)
