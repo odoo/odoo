4d649d09d5a9c00b18faea011691dd46f16dc9f0
djameltouati <otd@odoo.com>
2026-05-19 16:26:41 +0200

    [FIX] mrp: dont update by-products qty on MO validation
    Backport of:
    - https://github.com/odoo/odoo/commit/1dac720a74547e7120d14733c7abda81e959fb3c
    - https://github.com/odoo/odoo/commit/7e21cb9befae0e9ce1eabafc2d5366de5097547c
    
    Steps to reproduce:
    - Create a product P1 with the Follwing BoM:
        - 1 component C1 (10 units)
        - 1 by-product BP1 (10 units)
    
    - Create a Manufacturing Order for 1 unit of P1 and confirm it
    - Set quantity to 5 units for C1 and 5 units for BP1
    - Set Consumed = True for C1 and Produced = True for BP1
    - Validate the MO
    
    Problem:
    C1 is correctly validated with 5 units, but BP1 is reset to 10 units
    
    _onchange_quantity only checks raw_material_production_id to set
    `manual_consumption = True`, so by-products (which use production_id)
    never get `manual_consumption` set. As a result, `_set_qty_producing`
    does not skip the by-product move and resets its quantity to the
    product_uom_qty on validation.
    
    opw-6221482
    
    closes odoo/odoo#265276
    
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -1320,6 +1320,9 @@ class MrpProduction(models.Model):
             origin = '%s,%s' % (origin, self.name)
         return origin
 
+    def _mark_byproducts_as_produced(self):
+        self.move_byproduct_ids.picked = True
+
     def _set_qty_producing(self, pick_manual_consumption_moves=True):
         if self.product_id.tracking == 'serial':
             qty_producing_uom = self.product_uom_id._compute_quantity(self.qty_producing, self.product_id.uom_id, rounding_method='HALF-UP')
@@ -1334,8 +1337,9 @@ class MrpProduction(models.Model):
             self.move_raw_ids.filtered(lambda m: not is_waiting or m.product_id.tracking == 'none')
             | self.move_finished_ids.filtered(lambda m: m.product_id != self.product_id or m.product_id.tracking == 'serial')
         ):
+            is_byproduct = move in self.move_byproduct_ids  # Never update already produced by-product moves.
             # picked + manual means the user set the quantity manually
-            if move.manual_consumption and move.picked:
+            if move.picked and (is_byproduct or move.manual_consumption):
                 continue
 
             # sudo needed for portal users
@@ -1346,7 +1350,8 @@ class MrpProduction(models.Model):
             move._set_quantity_done(new_qty)
             if (not move.manual_consumption or pick_manual_consumption_moves) \
                     and move.quantity \
-                    and (move.product_id != self.product_id or not move.production_id or move.product_id.tracking != 'serial'):
+                    and not is_byproduct \
+                    and (move.raw_material_production_id or move.product_id.tracking != 'serial'):
                 move.picked = True
 
     def _should_postpone_date_finished(self, date_finished):
@@ -2233,7 +2238,7 @@ class MrpProduction(models.Model):
 
     def pre_button_mark_done(self):
         self._button_mark_done_sanity_checks()
-        productions_auto = set()
+        production_auto_ids = set()
         for production in self:
             if not float_is_zero(production.qty_producing, precision_rounding=production.product_uom_id.rounding):
                 production.move_raw_ids.filtered(
@@ -2241,12 +2246,15 @@ class MrpProduction(models.Model):
                 ).picked = True
                 continue
             if production._auto_production_checks():
-                productions_auto.add(production.id)
+                production_auto_ids.add(production.id)
             else:
                 return production.action_mass_produce()
 
-        for production in self.env['mrp.production'].browse(productions_auto):
+        productions_auto = self.env['mrp.production'].browse(production_auto_ids)
+        for production in productions_auto:
             production._set_quantities()
+        # Produce by-products also for not auto productions.
+        (self - productions_auto)._mark_byproducts_as_produced()
 
         consumption_issues = self._get_consumption_issues()
         if consumption_issues:
@@ -2831,6 +2839,7 @@ class MrpProduction(models.Model):
         else:
             self.qty_producing = self.product_qty - self.qty_produced
         self._set_qty_producing()
+        self._mark_byproducts_as_produced()
 
         for move in self.move_raw_ids:
             if move.state in ('done', 'cancel') or not move.product_uom_qty:
