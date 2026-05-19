alkuneemi/Luxara

[FIX] hr_holidays: fix duplicate leave deduction
Steps to reproduce:-
- Navigate to Time off Dashboard calendar view.
- Create a leave. First approve it then refuse it.
- Now on the same day create a leave and approve it.
- Now re-approve the previously refused leave from step 2.
- System will let user to create 2 leave of same types on same day!

Cause:-
In `_compute_dashboard_warning_message`, refused/cancelled leaves were
excluded from warning computation. When approving a refused request,
the warning message was not set, allowing the constraint check to pass
even when conflicting approved requests existed for the same period.

Fix:-
1. Refactored `_compute_dashboard_warning_message` to only compute
   warnings for active leaves (non-refused/cancelled) while still
   detecting conflicts with already approved requests
2. Updated `_check_date` constraint to skip validation for refused/
   cancelled leaves, but enforce it when state changes to validate
3. Added 'state' to constraint triggers to ensure validation runs
   when approving previously refused requests

task-6181717

addons/hr_holidays/tests/test_holidays_calendar.py
@@ -5,8 +5,11 @@ from datetime import date, timedelta
 from odoo.addons.base.tests.common import HttpCase
 from odoo.tests.common import tagged
 from odoo.tests.common import users
+from odoo.exceptions import ValidationError
+
 
 from odoo.addons.hr_holidays.tests.common import TestHrHolidaysCommon
+
 
 @tagged('post_install', '-at_install', 'holiday_calendar')
 class TestHolidaysCalendar(HttpCase, TestHrHolidaysCommon):
@@ -101,3 +104,38 @@ class TestHolidaysCalendar(HttpCase, TestHrHolidaysCommon):
         self.assertEqual(leave_half.meeting_id.allday, False)
         self.assertEqual(leave_half.meeting_id.start, leave_half.date_from)
         self.assertEqual(leave_half.meeting_id.stop, leave_half.date_to)
+
+    def test_overlapping_refused_time_off_approval(self):
+        """
+        Test that a refused time off request shows a warning message
+        when another approved request exists for the same period.
+        """
+        leave_type = self.env['hr.leave.type'].create({
+            'name': 'Test Leave Type',
+            'requires_allocation': False,
+            'request_unit': 'day',
+            'leave_validation_type': 'no_validation',
+            'allow_request_on_top': False,
+        })
+        test_date = date(2025, 4, 22)
+
+        # Now create leave requests
+        leave_request_a = self.env['hr.leave'].create({
+            'name': 'First Time Off Request',
+            'employee_id': self.employee_emp.id,
+            'holiday_status_id': leave_type.id,
+            'request_date_from': test_date,
+            'request_date_to': test_date,
+        })
+        leave_request_a.action_approve()
+        leave_request_a.action_refuse()
+        leave_request_b = self.env['hr.leave'].create({
+            'name': 'Second Time Off Request',
+            'employee_id': self.employee_emp.id,
+            'holiday_status_id': leave_type.id,
+            'request_date_from': test_date,
+            'request_date_to': test_date,
+        })
+        leave_request_b.action_approve()
+        with self.assertRaises(ValidationError):
+            leave_request_a.action_approve()

odoo/odoo

X-original-commit: 6f30429d375dcc17e9add79077fa3098c676fef3

addons/hr_holidays/tests/test_holidays_calendar.py
@@ -5,8 +5,11 @@ from datetime import date, timedelta
 from odoo.addons.base.tests.common import HttpCase
 from odoo.tests.common import tagged
 from odoo.tests.common import users
+from odoo.exceptions import ValidationError
+
 
 from odoo.addons.hr_holidays.tests.common import TestHrHolidaysCommon
+
 
 @tagged('post_install', '-at_install', 'holiday_calendar')
 class TestHolidaysCalendar(HttpCase, TestHrHolidaysCommon):
@@ -102,3 +105,39 @@ class TestHolidaysCalendar(HttpCase, TestHrHolidaysCommon):
         self.assertEqual(leave_half.meeting_id.allday, False)
         self.assertEqual(leave_half.meeting_id.start, leave_half.date_from)
         self.assertEqual(leave_half.meeting_id.stop, leave_half.date_to)
+
+    def test_overlapping_refused_time_off_approval(self):
+        """
+        Test that a refused time off request shows a warning message
+        when another approved request exists for the same period.
+        """
+        leave_type = self.env['hr.work.entry.type'].create({
+            'name': 'Test Leave Type',
+            'code': "TEST",
+            'requires_allocation': False,
+            'request_unit': 'day',
+            'leave_validation_type': 'no_validation',
+            'allow_request_on_top': False,
+        })
+        test_date = date(2025, 4, 22)
+
+        # Now create leave requests
+        leave_request_a = self.env['hr.leave'].create({
+            'name': 'First Time Off Request',
+            'employee_id': self.employee_emp.id,
+            'work_entry_type_id': leave_type.id,
+            'request_date_from': test_date,
+            'request_date_to': test_date,
+        })
+        leave_request_a.action_approve()
+        leave_request_a.action_refuse()
+        leave_request_b = self.env['hr.leave'].create({
+            'name': 'Second Time Off Request',
+            'employee_id': self.employee_emp.id,
+            'work_entry_type_id': leave_type.id,
+            'request_date_from': test_date,
+            'request_date_to': test_date,
+        })
+        leave_request_b.action_approve()
+        with self.assertRaises(ValidationError):
+            leave_request_a.action_approve()