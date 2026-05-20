df7893697fa5b3425a11b293d53189e20504a59a
Robin Lejeune (role) <role@odoo.com>
2026-05-20 17:31:33 +0200

    [IMP] html_builder: add hotkeys on add snippet dialog
    1. For power users, we add hotkeys on the mobile toggle (Alt + M) and
    the search input (Alt + S).
    
    2. Commit [1f86365] made sure the `escape` key would also close the
    dialog (which doesn't work by default because the iframe "swallows" the
    key strokes while the dialog lives on the top window). But this is
    easily handled by the hotkey service by registering the iframe directly,
    so we rather use the default method to do it.
    
    [1f86365]: https://github.com/odoo/odoo/commit/1f863651ed64b207683fc7295e1f7e5a38920dac
    
    task-6250075
    
    Part-of: odoo/odoo#265596
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/html_builder/static/src/snippets/add_snippet_dialog.js
+++ addons/html_builder/static/src/snippets/add_snippet_dialog.js
@@ -8,7 +8,7 @@ import { localization } from "@web/core/l10n/localization";
 import { _t } from "@web/core/l10n/translation";
 import { getFirstAndLastTabableElements } from "@web/core/ui/ui_service";
 import { cookie } from "@web/core/browser/cookie";
-import { useAutofocus, useChildRef } from "@web/core/utils/hooks";
+import { useAutofocus, useChildRef, useService } from "@web/core/utils/hooks";
 import { SnippetViewer } from "./snippet_viewer";
 
 /**
@@ -35,6 +35,7 @@ export class AddSnippetDialog extends Component {
 
     setup() {
         useAutofocus();
+        this.hotkeyService = useService("hotkey");
         this.iframeRef = useRef("iframe");
         this.modalRef = useChildRef();
         this.state = proxy({
@@ -83,6 +84,8 @@ export class AddSnippetDialog extends Component {
             iframeDocument.body.style.setProperty("direction", localization.direction);
             iframeDocument.body.tabIndex = "-1";
             iframeDocument.addEventListener("keydown", this.onIframeDocumentKeydown.bind(this));
+
+            this.hotkeyService.registerIframe(this.iframeRef.el);
 
             root = this.__owl__.app.createRoot(SnippetViewer, {
                 env: Object.create(this.env),
@@ -196,9 +199,6 @@ export class AddSnippetDialog extends Component {
      */
     onIframeDocumentKeydown(ev) {
         const hotkey = getActiveHotkey(ev);
-        if (hotkey === "escape") {
-            this.env.dialogData.close({ dismiss: true });
-        }
         if (!["tab", "shift+tab"].includes(hotkey)) {
             return;
         }
