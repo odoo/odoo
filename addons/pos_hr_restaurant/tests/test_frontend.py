61faf723cfde8554437e7bd1a551dd55b0a44cfc
Arthur Nanson <artn@odoo.com>
2026-05-20 15:08:04 +0000

    [FIX] pos_hr: display correct employee in tracked order's chatter
    **Steps to reproduce:**
    
    - Enable "Track orders edits" in the settings
    - Enable "Log in with Employees"
    - Go to the Restaurant, log in with employee A
    - Go to a table, order 3 Sushis
    - Go back to the floor plan and change to employee B
    - Go back to the table and change the qty of 3 Sushis to 2 Sushis
    - Go to the order in the backend and check the chatter
    - It will indicate that employee A did the change, but it was employee B
    
    **Why the fix:**
    
    We always used the cashier set on the order to
    determine who should be put in the chatter,
    regardless of who is actually connected at that
    point.
    
    We now use the session's current employee to
    write who did the change in the chatter.
    
    We do not change the order's employee, because
    it will be done once the order has been paid.
    
    We use the context to get who is actually
    using the session at the given time, as
    in the case of a session being used by
    multiple devices, the session.employee_id
    might be wrong.
    
    In the case where we are not logged in
    but pos_hr is installed, the employee_id
    might be the id of a res.user, and
    browsing it might return the wrong value.
    To avoid this, we check if the value exists
    as a hr.employee before assigning the name.
    
    The way we return the value has been changed
    because the linter wasn't happy about it.
    
    opw-6213504
    
    closes odoo/odoo#270768
    
    X-original-commit: fbecc04a42d0d27c7247ce7b7066c5fc54ad6fcc
    Signed-off-by: Manu Vaillant (manv) <manv@odoo.com>
    Signed-off-by: Arthur Nanson (artn) <artn@odoo.com>

--- addons/pos_hr_restaurant/tests/test_frontend.py
+++ addons/pos_hr_restaurant/tests/test_frontend.py
@@ -14,3 +14,21 @@ class TestUi(TestPosHrHttpCommon, TestFrontendCommon):
         self.main_pos_config.default_screen = "register"
         self.main_pos_config.with_user(self.pos_admin).open_ui()
         self.start_pos_tour("test_post_login_default_screen_is_register", login="pos_admin")
+
+    def test_employee_chatter_with_tracked_order(self):
+        """
+        Tests that when changing the session's employee mid session,
+        the message in the chatter created with the track order setting
+        will reflect this change and not keep the old employee
+        """
+        self.main_pos_config.order_edit_tracking = True
+        self.main_pos_config.open_ui()
+        self.start_tour(
+            "/pos/ui?config_id=%d" % self.main_pos_config.id,
+            "test_employee_chatter_with_tracked_order",
+            login="pos_admin",
+        )
+        order = self.env['pos.order'].search([
+            ('config_id', '=', self.main_pos_config.id)
+        ], order='id desc', limit=1)
+        self.assertNotIn("Mitchell Admin", order.message_ids[0].body)
