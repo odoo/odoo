340fae20470feb0b6b8e2b8860fb264b6fae659a
Jugurtha <jugac@odoo.com>
2026-05-21 11:38:58 +0200

    [FIX] account_edi_ubl_cii: Never import or export deffered dates.
    The current implementation of the Peppol XML export incorrectly populates
    the <cac:InvoicePeriod> nodes with internal deferred entry dates.
    
    These dates are intended for the vendor's revenue recognition process,
    and the customer has nothing to do with these dates.
    
    This commit ensures that:
    - deferred entries are never created when importing vendor bills.
    - <cac:InvoicePeriod> is no longer exported in invoices (for now).
    
    task-6014315
    
    closes odoo/odoo#265796
    
    Signed-off-by: Ricardo Gomes Rodrigues (rigr) <rigr@odoo.com>

--- addons/account_edi_ubl_cii/models/account_edi_xml_ubl_20.py
+++ addons/account_edi_ubl_cii/models/account_edi_xml_ubl_20.py
@@ -541,12 +541,6 @@ class AccountEdiXmlUBL20(models.AbstractModel):
             for vals in allowance_charge_vals_list
             if vals.get('from_fixed_tax')
         )
-        period_vals = {}
-        # deferred_start_date & deferred_end_date are enterprise-only fields
-        if line._fields.get('deferred_start_date') and (line.deferred_start_date or line.deferred_end_date):
-            period_vals.update({'start_date': line.deferred_start_date})
-            period_vals.update({'end_date': line.deferred_end_date})
-
         return self._add_invoice_extra_line_vals(line, {
             'currency': line.currency_id,
             'currency_dp': self._get_currency_decimal_places(line.currency_id),
@@ -558,7 +552,6 @@ class AccountEdiXmlUBL20(models.AbstractModel):
             'tax_total_vals': self._get_invoice_line_tax_totals_vals_list(line, taxes_vals),
             'item_vals': self._get_invoice_line_item_vals(line, taxes_vals),
             'price_vals': self._get_invoice_line_price_vals(line),
-            'invoice_period_vals_list': [period_vals] if period_vals else []
         })
 
     def _get_invoice_monetary_total_vals(self, invoice, taxes_vals, line_extension_amount, allowance_total_amount, charge_total_amount):
@@ -770,7 +763,6 @@ class AccountEdiXmlUBL20(models.AbstractModel):
                     'base_quantity_attrs': {'unitCode': 'C62'},
                     'product_price_dp': self.env['decimal.precision'].precision_get('Product Price'),
                 },
-                'invoice_period_vals_list': []
             })
             line_extension_amount += tax_vals['tax_amount_currency']
 
@@ -1091,15 +1083,6 @@ class AccountEdiXmlUBL20(models.AbstractModel):
             elif name_node is not None:
                 invoice_line.name = name_node.text  # Fallback on Name if Description is not found.
 
-            # Start and End date (enterprise fields)
-            if invoice_line._fields.get('deferred_start_date'):
-                start_date = tree.find('./{*}InvoicePeriod/{*}StartDate')
-                end_date = tree.find('./{*}InvoicePeriod/{*}EndDate')
-                if start_date is not None and end_date is not None:  # there is a constraint forcing none or the two to be set
-                    invoice_line.write({
-                        'deferred_start_date': start_date.text,
-                        'deferred_end_date': end_date.text,
-                    })
             xpath_dict = {
                 'basis_qty': [
                     './{*}Price/{*}BaseQuantity',
@@ -1147,15 +1130,6 @@ class AccountEdiXmlUBL20(models.AbstractModel):
         elif name_node is not None:
             invoice_line.name = name_node.text  # Fallback on Name if Description is not found.
 
-        # Start and End date (enterprise fields)
-        if invoice_line._fields.get('deferred_start_date'):
-            start_date = tree.find('./{*}InvoicePeriod/{*}StartDate')
-            end_date = tree.find('./{*}InvoicePeriod/{*}EndDate')
-            if start_date is not None and end_date is not None:  # there is a constraint forcing none or the two to be set
-                invoice_line.write({
-                    'deferred_start_date': start_date.text,
-                    'deferred_end_date': end_date.text,
-                })
         xpath_dict = {
             'basis_qty': [
                 './{*}Price/{*}BaseQuantity',
