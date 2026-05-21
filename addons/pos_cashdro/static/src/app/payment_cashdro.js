e02c23e6dde35bcf3987f7f9b7a6cface1854a16
Max Whale <mawh@odoo.com>
2026-05-21 10:48:12 +0000

    [FIX] pos_cashdro: fix delete payment line
    Before this commit, if you tried to cancel and delete a Cashdro payment
    line by clicking the x, the payment would be cancelled but the line
    would not be deleted, just left in the 'retry' state.
    
    After this commit, the payment line is deleted after being cancelled as
    expected.
    
    X-original-commit: 4106f69af3d6f5fa3668ff7eaa714aeaf1944e3b

--- addons/pos_cashdro/static/src/app/payment_cashdro.js
+++ addons/pos_cashdro/static/src/app/payment_cashdro.js
@@ -98,6 +98,10 @@ export class PaymentCashdro extends PaymentInterface {
         const netAmount = this.env.utils.roundCurrency(moneyIn - moneyOut);
         if (netAmount == 0) {
             // Payment was cancelled
+            if (this.cancellationResolver) {
+                this.cancellationResolver(true);
+                this.cancellationResolver = null;
+            }
             return false;
         }
         if (this.paymentLine.pos_order_id.remainingDue === 0 && this.paymentLine.amount > 0) {
@@ -144,8 +148,10 @@ export class PaymentCashdro extends PaymentInterface {
         await this.cashdroService.cancelPayment(this.operationId).catch((error) => {
             this.showError(_t("Cashdro cancellation failed: %s", error.message));
         });
-        // Don't resolve the cancellation, we want to wait
-        return new Promise(() => {});
+
+        const { promise, resolve } = Promise.withResolvers();
+        this.cancellationResolver = resolve;
+        return promise;
     }
 
     /**
