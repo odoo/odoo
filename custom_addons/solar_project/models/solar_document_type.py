f0f49ae05e5eeae4b8a154d0569ccd9b1ba07807
akoziar <koziar.artem@gmail.com>
2026-05-23 00:09:05 +0300

    [ADD] solar_project: add solar.document.type model with uniqueness constraint

---
+++ custom_addons/solar_project/models/solar_document_type.py
@@ -0,0 +1,26 @@
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class SolarDocumentType(models.Model):
+    _name = "solar.document.type"
+    _description = "Solar Document Type"
+    _order = "sequence, name"
+
+    name = fields.Char(required=True, translate=True)
+    code = fields.Char(required=True, index=True)
+    sequence = fields.Integer(default=10)
+    description = fields.Text(translate=True)
+    active = fields.Boolean(default=True)
+
+    _sql_constraints = [
+        ("code_unique", "UNIQUE(code)", "Document type code must be unique."),
+    ]
+
+    @api.constrains("code")
+    def _check_code_unique(self):
+        for rec in self:
+            if self.search_count([("code", "=", rec.code), ("id", "!=", rec.id)]) > 0:
+                raise ValidationError(
+                    f'Document type code "{rec.code}" already exists.'
+                )
--- custom_addons/solar_project/tests/test_solar_project.py
+++ custom_addons/solar_project/tests/test_solar_project.py
@@ -1,3 +1,4 @@
+from odoo.exceptions import ValidationError
 from odoo.tests import TransactionCase, tagged
 
 
@@ -10,3 +11,22 @@ class TestSolarProjectBase(TransactionCase):
 
     def test_project_created(self):
         self.assertEqual(self.project.name, "Test Solar Project")
+
+
+@tagged("solar_project", "post_install", "-at_install")
+class TestSolarDocumentType(TransactionCase):
+    def test_document_type_created(self):
+        doc_type = self.env["solar.document.type"].create(
+            {
+                "name": "Electricity Bill",
+                "code": "bill_electricity",
+            }
+        )
+        self.assertEqual(doc_type.code, "bill_electricity")
+
+    def test_document_type_code_unique(self):
+        self.env["solar.document.type"].create({"name": "T1", "code": "unique_code"})
+        with self.assertRaises(ValidationError):
+            self.env["solar.document.type"].create(
+                {"name": "T2", "code": "unique_code"}
+            )
