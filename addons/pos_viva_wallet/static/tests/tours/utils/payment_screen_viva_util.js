alkuneemi/Luxara

[FIX] pos_viva_wallet: send cashRegisterId for Viva when cashier name is missing
When validating a payment in POS Kiosk with Viva payment method
we get a Viva.com error
Viva’s card-terminal API validates the JSON body with Pydantic and
requires a non-empty ``cashRegisterId``.

Steps to reproduce:
-------------------
* Open POS in kiosk
* Make an order and pay with Viva
> Observation:
Viva returns a validation error: ``cashRegisterId`` is missing
or required in the request body (Pydantic ``missing`` on ``body.cashRegisterId``).

Why the fix:
------------
Compute ``cashRegisterId`` in the POS client as cashier name, then
``pos.config.name`` so the value is always a non-empty string sent
to ``viva_wallet_send_payment_request``.

opw-6091223

addons/pos_viva_wallet/static/tests/tours/utils/payment_screen_viva_util.js
@@ -1,5 +1,24 @@
 /** @odoo-module */
 
+export function simulateKioskNamelessCashier() {
+    return [
+        {
+            content: "Simulate kiosk: cashier has no usable name",
+            trigger: ".payment-screen",
+            run: function () {
+                const pos = window.posmodel;
+                pos._vivaWalletTestOrigGetCashier = pos.get_cashier.bind(pos);
+                pos.get_cashier = () => ({
+                    name: "",
+                    id: 1,
+                    _role: "cashier",
+                    raw: { role: "cashier" },
+                });
+            },
+        },
+    ];
+}
+
 export function send_payment_cancel() {
     return [
         {

odoo/odoo

X-original-commit: 2f9bf0ce58c6b99d8d96af16cce7764944249063

addons/pos_viva_wallet/static/tests/tours/utils/payment_screen_viva_util.js
@@ -1,3 +1,24 @@
+/** @odoo-module */
+
+export function simulateKioskNamelessCashier() {
+    return [
+        {
+            content: "Simulate kiosk: cashier has no usable name",
+            trigger: ".payment-screen",
+            run: function () {
+                const pos = window.posmodel;
+                pos._vivaWalletTestOrigGetCashier = pos.getCashier.bind(pos);
+                pos.getCashier = () => ({
+                    name: "",
+                    id: 1,
+                    _role: "cashier",
+                    raw: { role: "cashier" },
+                });
+            },
+        },
+    ];
+}
+
 export function send_payment_cancel() {
     return [
         {