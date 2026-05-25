odoo/odoo

commit 29d06d76889c8231190e563688fa698a551c3672

[FIX] pos_restaurant: allow table transfer to diff floor
In c57f2ba594d2e1d081e6521b06d63a7d2a260921 we refactored the table
transfer mechanism. This introduced a bug, namely the fact that table
transfer to a different floor no longer works. This is because with the
new logic, a click anywhere on the screen other than on a table would
cancel the transfer. This makes sense when clicking on the burger menu for ex,
but it removes the possibility of transferring an order to a different floor,
because that requires clicking on the floor selector button.

In this commit we fix the issue by ignoring clicks on the floor selector
in the transfer cancellation logic.

closes odoo/odoo#185690

Task: 4285748
Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

addons/pos_restaurant/static/src/app/control_buttons/control_buttons.js
@@ -51,22 +51,21 @@ patch(ControlButtons.prototype, {
         const orderUuid = this.pos.get_order().uuid;
         this.pos.get_order().setBooked(true);
         this.pos.showScreen("FloorScreen");
-        document.addEventListener(
-            "click",
-            async (ev) => {
-                if (this.pos.isOrderTransferMode) {
-                    this.pos.isOrderTransferMode = false;
-                    const tableElement = ev.target.closest(".table");
-                    if (!tableElement) {
-                        return;
-                    }
-                    const table = this.pos.getTableFromElement(tableElement);
-                    await this.pos.transferOrder(orderUuid, table);
-                    this.pos.setTableFromUi(table);
-                }
-            },
-            { once: true }
-        );
+        const onClickWhileTransfer = async (ev) => {
+            if (ev.target.closest(".button-floor")) {
+                return;
+            }
+            this.pos.isOrderTransferMode = false;
+            const tableElement = ev.target.closest(".table");
+            if (!tableElement) {
+                return;
+            }
+            const table = this.pos.getTableFromElement(tableElement);
+            await this.pos.transferOrder(orderUuid, table);
+            this.pos.setTableFromUi(table);
+            document.removeEventListener("click", onClickWhileTransfer);
+        };
+        document.addEventListener("click", onClickWhileTransfer);
     },
     async clickTakeAway() {
         const isTakeAway = !this.currentOrder.takeaway;

commit 4996b07bf42be5abd8576ba1bc2844de1d0afdfa

[FIX] pos_restaurant: detach transfer listener on misclick
Before patch: cashier hits Transfer / Merge, clicks somewhere that isn't a table, the "transfer ongoing" banner disappears, then any later click on a table silently transfers the order to it. Nasty on floorplans with many tables.

The document click listener was only removed on the success path.
Move removeEventListener so it fires as soon as transfer mode exits, regardless of whether the click hit a table.

Regression from https://github.com/odoo/odoo/commit/29d06d76889c.

addons/pos_restaurant/static/src/app/control_buttons/control_buttons.js
@@ -56,6 +56,7 @@ patch(ControlButtons.prototype, {
                 return;
             }
             this.pos.isOrderTransferMode = false;
+            document.removeEventListener("click", onClickWhileTransfer);
             const tableElement = ev.target.closest(".table");
             if (!tableElement) {
                 return;
@@ -63,7 +64,6 @@ patch(ControlButtons.prototype, {
             const table = this.pos.getTableFromElement(tableElement);
             await this.pos.transferOrder(orderUuid, table);
             this.pos.setTableFromUi(table);
-            document.removeEventListener("click", onClickWhileTransfer);
         };
         document.addEventListener("click", onClickWhileTransfer);
     },