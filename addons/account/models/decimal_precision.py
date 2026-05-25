b8db8a2024628096022b9f8853ad310a449f35c5
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2026-05-25 09:26:03 +0000

    [FIX] account_edi_ubl_cii: fix imported quantity decimal precision
    When importing an XML bill and linkin git to a purcahse order, the
    invoiced quantity may be computed incorrectly, due to a decimal
    precision mismatch.
    
    Steps to reproduce:
    - Import an XML bill having a line with quantity 1800.0
    - Link to a purchase order with the same line
    
    Issue: The invoiced quantity will be computed with 1 cent difference
    (1800.01)
    
    Analysis: Because the system forced a decimal precision of 13 for
    'Product Unit of Measure', quantity is imported as 1800.0000000000016.
    Later, when computing the invoiced quantity, the system round the
    quantity using 'UP' strategy, rounding the amount to 1800.01
    
    opw-6194824
    
    X-original-commit: 546b69c21bfc207784d1c609c9849ddd60e0ce53

--- addons/account/models/decimal_precision.py
+++ addons/account/models/decimal_precision.py
@@ -6,6 +6,9 @@ class DecimalPrecision(models.Model):
 
     def precision_get(self, application):
         stackmap = self.env.cr.cache.get('account_disable_recursion_stack', {})
-        if stackmap.get('ignore_discount_precision') and application in ('Discount', 'Product Unit'):
-            return 13
+        if stackmap.get('ignore_discount_precision'):
+            if application == 'Discount':
+                return 13
+            if application == 'Product Unit':
+                return 10
         return super().precision_get(application)
