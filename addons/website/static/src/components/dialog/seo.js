23dbd4e4e6c2a245af5a66921e64848f77b359e3
Rahil Ghanchi <rahg@odoo.com>
2026-05-26 11:17:44 +0530

    [IMP] website: improve keyword management in SEO dialog
    Before this commit:
    - Users lost access to the "Generate Keywords" button after adding
    custom keywords.
    - There was no option to remove all keywords at once.
    
    After this commit:
    - Users can add custom keywords without losing access to the
    "Generate Keywords" button.
    - Added a "Delete All" button to remove all keywords in a single
    action.
    - Users lose access to the "Generate Keywords" button only after adding
    more than 10 keywords, which is the maximum recommended number of
    keywords for SEO purposes.
    
    task-5952515

--- addons/website/static/src/components/dialog/seo.js
+++ addons/website/static/src/components/dialog/seo.js
@@ -181,7 +181,9 @@ const getSeo = async (self, onlyKeywords = false) => {
 
     const keywords = extractKeywords();
     if (keywords.length) {
-        self.seoContext.keywords = keywords;
+        self.seoContext.keywords.push(
+            ...keywords.filter((kw) => !self.seoContext.keywords.includes(kw))
+        );
     }
     if (!onlyKeywords) {
         self.seoContext.title = htmlToTextContentInline(self.seoContext.defaultTitle);
@@ -471,6 +473,10 @@ class MetaKeywords extends Component {
         getSeo(this, true);
     }
 
+    normalizeKeyword(keyword) {
+        return keyword.replaceAll(/,+\s*/g, " ").trim();
+    }
+
     onKeyup(ev) {
         // Add keyword on enter.
         if (ev.key === "Enter") {
@@ -484,13 +490,13 @@ class MetaKeywords extends Component {
         );
     }
 
-    get isFull() {
+    get hasTooManyKeywords() {
         return this.seoContext.keywords.length >= this.maxKeywords;
     }
 
     addKeyword(keyword) {
-        keyword = keyword.replaceAll(/,\s*/gi, " ").trim();
-        if (keyword && !this.isFull && !this.seoContext.keywords.includes(keyword)) {
+        keyword = this.normalizeKeyword(keyword);
+        if (keyword && !this.seoContext.keywords.includes(keyword)) {
             this.seoContext.keywords.push(keyword);
             this.state.keyword = "";
         }
@@ -498,6 +504,15 @@ class MetaKeywords extends Component {
 
     removeKeyword(keyword) {
         this.seoContext.keywords = this.seoContext.keywords.filter((kw) => kw !== keyword);
+    }
+
+    removeAllKeywords() {
+        this.seoContext.keywords = [];
+    }
+
+    get hasKeyword() {
+        const keyword = this.normalizeKeyword(this.state.keyword);
+        return this.seoContext.keywords.includes(keyword);
     }
 }
 
