alkuneemi/Luxara

[FIX] l10n_ar, l10n_latam_base: prevent child contacts from being detected as companies
Issue:
When creating a company with CUIT 30999003156,
any child contact added under it is incorrectly considered as a company

Cause:
In `l10n_ar`, `_compute_is_company()` relies on:
`l10n_ar_afip_code` and the prefix of `l10n_ar_vat`
However, these fields are propagated to child contacts
As a result, child contacts inherit the same values as the parent company
and are incorrectly computed with `is_company = True`

Note:
This same fix also fix the issue on `l10n_latam_base` and `l10n_co`

Steps to reproduce:
- Install `l10n_ar` and switch to an AR Company
- Create a Partner in Contacts (Name: Test Company, Country: Argentina, Identification Number: CUIT 30999003156)
- Add a Contact (Name: Test Contact)
- Go in Contacts and add a Filter for Name: Test

Before the fix:
The Contacts are: Test Company and Test Contact

After the fix:
The Contacts are: Test Company and Test Company, Test Contact

opw-6140921

addons/l10n_ar/models/res_partner.py
@@ -59,6 +59,7 @@ class ResPartner(models.Model):
             lambda p: not p._is_vat_void(p.vat)
                 and p.l10n_latam_identification_type_id.l10n_ar_afip_code
                 and p.country_code == 'AR'
+                and p.commercial_partner_id == p
         )
         for partner in l10n_ar_partners:
             afip_code = partner.l10n_latam_identification_type_id.l10n_ar_afip_code

odoo/odoo

X-original-commit: 01aad6e99aa4312e83e5b23ba8937f2ddce24d5c

addons/l10n_ar/models/res_partner.py
@@ -64,7 +64,10 @@ class ResPartner(models.Model):
             afip_code = partner.l10n_latam_identification_type_id.l10n_ar_afip_code
             prefix = (partner.l10n_ar_vat or '')[:2]
 
-            if afip_code == '80' and prefix in ('30', '33', '34', '51', '55'):  # CUIT
+            if (
+                afip_code == '80' and prefix in ('30', '33', '34', '51', '55')  # CUIT
+                and partner.commercial_partner_id == partner
+            ):
                 partner.is_company = True
             else:
                 partner.is_company = False  # CUIL or DNI or Unknown type → default to individual