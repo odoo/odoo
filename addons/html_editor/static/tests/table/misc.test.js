2dfec2f23be9642aebd21b5c001a7f392918636f
Jinjiu Liu <jili@odoo.com>
2026-05-26 09:41:17 +0000

    [FIX] html_editor: table cell bgcolor test to ensure the color updated
    Before this commit: the test `cell's selected color should be shown in
    toolbar (3)` could fail when the bd color indicator isn't updated before
    the checking
    
    After this commit: we create an util to wait for the selectionchange
    event is fired by the browser to make sure the toolbar is updated before
    verifying.
    
    runbot-937780
    
    X-original-commit: 2fa8932362c107d29cc2a4880da93f2ac346728b

--- addons/html_editor/static/tests/table/misc.test.js
+++ addons/html_editor/static/tests/table/misc.test.js
@@ -8,7 +8,7 @@ import {
     manuallyDispatchProgrammaticEvent,
 } from "@odoo/hoot-dom";
 import { animationFrame, tick } from "@odoo/hoot-mock";
-import { getContent, setSelection } from "../_helpers/selection";
+import { getContent, setSelection, waitForSelectionChange } from "../_helpers/selection";
 import { execCommand } from "../_helpers/userCommands";
 import { expandToolbar } from "../_helpers/toolbar";
 import { expectElementCount } from "../_helpers/ui_expectations";
@@ -184,6 +184,7 @@ describe("selected cell color in toolbar", () => {
             focusNode: nonStyledCellTwo,
             focusOffset: 1,
         });
+        await waitForSelectionChange();
         await animationFrame();
         expect(".fa-paint-brush").toHaveCount(1);
         expect(".fa-paint-brush").toHaveStyle({
