44121a3ca55e1618eb4dfce18de12f0e91e3a26a
sawer <sawer@odoo.com>
2026-05-27 12:03:11 +0000

    [FIX] account: use a more precise cache key tax during UBL import
    When we import a UBL file, we call the `_import_retrieve_tax`
    method to fetch taxes to indicate on lines.
    
    During the process, we use cache to avoid performing the search
    a second time if a new line is the same as a previous one.
    
    https://github.com/odoo/odoo/blame/d0424f2ffcf99ee59befe288150f1643b3fa0112/addons/account/models/account_tax.py#L4459-L4462
    
    The cache_key used is defined as follows: {line's invoice,
    line's name, line's partner}.
    This implies that if two lines from the same invoice share the
    same name and partner, the same tax will automatically be
    used even if different taxes were indicated in the file.
    
    This is not desirable as we should match what is indicated
    in the XML file imported.
    
    opw-6226166
    
    closes odoo/odoo#267497
    
    X-original-commit: f726393267a28cedd5febd2106de17ae3838f3ff
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- addons/account/models/account_tax.py
+++ addons/account/models/account_tax.py
@@ -5217,6 +5217,8 @@ class AccountTax(models.Model):
                         cache_key = repr(domain.optimize(self.env['account.tax']))
                     else:
                         cache_key = criteria.get('cache_key')
+                        if cache_key:
+                            cache_key = (cache_key, str(tax_domain))
 
                     # Look at the cache if the value has already been tested with this key.
                     if cache_key and cache_key in cache:
