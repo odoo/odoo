alkuneemi/Luxara

[IMP] hr_work_entry: introduce unified work entry export
We want to unify the way work entries are exported from Odoo. Currently,
we have different modules for each of the most important 3rd party
applications that accept work entry reports but here we want to drop
those and only create one work entry export format for Odoo.

The exports will be accessible from the employee views via the cog menu
or the action menu. In hr_presence we unintelligently override the
whole cog menu/action menu of the views to add the presence-related
buttons. This is a problem because depending on the installation order
of the modules we might lose the content of one or the other. To avoid
this, here we instead create a common override in hr and then add to it
in both hr_work_entry and hr_presence.

Task: 6196138

addons/hr_work_entry/__manifest__.py
@@ -13,12 +13,20 @@
         'security/ir.model.access.csv',
         'data/hr_work_entry_type_data.xml',
         'views/hr_work_entry_type_views.xml',
+        'views/hr_work_entry_export_views.xml',
+        'views/hr_employee_views.xml',
+        'views/res_company_views.xml',
         'views/resource_calendar_views.xml',
         'views/menuitems.xml',
     ],
     'demo': [
         'data/hr_work_entry_demo.xml',
     ],
+    'assets': {
+        'web.assets_backend': [
+            'hr_work_entry/static/src/**/*',
+        ],
+    },
     'author': 'Odoo S.A.',
     'license': 'LGPL-3',
 }

odoo/odoo

addons/hr_work_entry/__manifest__.py
@@ -12,13 +12,21 @@
         'security/hr_work_entry_security.xml',
         'security/ir.model.access.csv',
         'data/hr_work_entry_type_data.xml',
+        'wizard/hr_work_entry_export_views.xml',
         'views/hr_work_entry_type_views.xml',
+        'views/hr_employee_views.xml',
+        'views/res_company_views.xml',
         'views/resource_calendar_views.xml',
         'views/menuitems.xml',
     ],
     'demo': [
         'data/hr_work_entry_demo.xml',
     ],
+    'assets': {
+        'web.assets_backend': [
+            'hr_work_entry/static/src/**/*',
+        ],
+    },
     'author': 'Odoo S.A.',
     'license': 'LGPL-3',
 }