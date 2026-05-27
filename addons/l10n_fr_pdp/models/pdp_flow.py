e03c5e22d3f008ca742c5b4c1a267e3ec66f8791
Victor Decleire <videc@odoo.com>
2026-05-27 18:06:32 +0200

    [IMP] l10n_fr_pdp: authenticate before using pdp
    Before this commit, we could use pdp without any authentication proof,
    this commit will enfore the identity authentication before using pdp
    
    task-5395365
    
    Part-of: odoo/odoo#267150
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>

--- addons/l10n_fr_pdp/models/pdp_flow.py
+++ addons/l10n_fr_pdp/models/pdp_flow.py
@@ -4,7 +4,7 @@ from markupsafe import Markup
 
 from odoo import api, fields, models
 from odoo.exceptions import UserError, RedirectWarning
-from odoo.osv import expression
+from odoo.fields import Domain
 
 _logger = logging.getLogger(__name__)
 
@@ -523,7 +523,7 @@ class PdpFlow(models.Model):
         """Open list view of invalid invoices."""
         action = self._get_moves()._get_records_action(name=self.env._("Invalid Invoices"))
         domain = action.get('domain') or []
-        action['domain'] = expression.AND([domain, [('l10n_fr_pdp_has_error', '=', True)]])
+        action['domain'] = Domain.AND([domain, [('l10n_fr_pdp_has_error', '=', True)]])
         return action
 
     def action_open_send_wizard(self):
