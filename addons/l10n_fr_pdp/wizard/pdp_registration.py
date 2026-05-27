6d2f71c012c621bd692d0e588239c5020a7a0008
Victor Decleire <videc@odoo.com>
2026-05-27 18:06:32 +0200

    [IMP] l10n_fr_pdp: authenticate before using pdp
    Before this commit, we could use pdp without any authentication proof,
    this commit will enfore the identity authentication before using pdp
    
    task-5395365

--- addons/l10n_fr_pdp/wizard/pdp_registration.py
+++ addons/l10n_fr_pdp/wizard/pdp_registration.py
@@ -1,5 +1,9 @@
 from odoo import api, fields, models, modules
-from odoo.exceptions import UserError, ValidationError
+from odoo.exceptions import UserError, ValidationError, RedirectWarning
+from odoo.addons.iap.tools import iap_tools
+
+ENDPOINT = 'https://pdp.odoo.com'
+TEST_ENDPOINT = 'https://pdp.test.odoo.com'
 
 
 class PdpRegistration(models.TransientModel):
@@ -46,6 +50,24 @@ class PdpRegistration(models.TransientModel):
         string="Warnings",
         compute="_compute_warnings",
     )
+    siren_number = fields.Char(
+        compute='_compute_siren_number',
+        store=True,
+        readonly=False
+    )
+    pdp_authentication_uuid = fields.Char(
+        string="Authentication IAP UUID",
+    )
+    pdp_kyc_status = fields.Selection(
+        string="Authentication status",
+        related='company_id.pdp_kyc_status',
+        readonly=False,
+    )
+    auth_url_hash = fields.Char(
+        string="Company Authentication IAP UUID",
+        related="company_id.pdp_authentication_uuid",
+        readonly=False,
+    )
 
     # -------------------------------------------------------------------------
     # ONCHANGE METHODS
@@ -60,6 +82,11 @@ class PdpRegistration(models.TransientModel):
     # -------------------------------------------------------------------------
     # COMPUTE METHODS
     # -------------------------------------------------------------------------
+
+    @api.depends('company_id.siret')
+    def _compute_siren_number(self):
+        for wizard in self:
+            wizard.siren_number = wizard.company_id.siret[:9] if wizard.company_id.siret else ''
 
     @api.depends('company_id.account_edi_proxy_client_ids')
     def _compute_edi_user_id(self):
@@ -140,6 +167,97 @@ class PdpRegistration(models.TransientModel):
     # -------------------------------------------------------------------------
     # BUSINESS ACTIONS
     # -------------------------------------------------------------------------
+
+    def button_trigger_authentication(self):
+        self.ensure_one()
+        if not self.siren_number:
+            raise RedirectWarning(
+                message=self.env._("You need to have a valid siren number to authenticate."),
+                action=self.company_id._get_records_action(),
+                button_text=self.env._("Go to company"),
+            )
+        base_url = ENDPOINT if self.edi_mode == 'prod' else TEST_ENDPOINT
+        response = iap_tools.iap_jsonrpc(f'{base_url}/api/id_authentication/1/authentication', params={
+            'db_uuid': self.env['ir.config_parameter'].sudo().get_param('database.uuid'),
+            'vat': self.siren_number,
+            'auth_email': self.contact_email,
+            'company_name': self.company_id.name,
+            'localization': 'FR',
+        })
+        self.pdp_authentication_uuid = response.get('object_uuid')
+        self.auth_url_hash = response.get('url_hash')
+        self.pdp_kyc_status = 'processing'
+
+        return {
+            'type': 'ir.actions.act_url',
+            'url': f'{base_url}/api/id_authentication/1/authentication_portal/{self.auth_url_hash}',
+            'target': 'new',
+        }
+
+    def _display_status_notification(self):
+        self.ensure_one()
+        if self.pdp_kyc_status == 'success':
+            type_color = 'success'
+            message = self.env._("Identity verified.")
+            next_action = self._action_open_pdp_form()
+        elif self.pdp_kyc_status == 'fail':
+            type_color = 'danger'
+            message = self.env._("Authentication failed.")
+            next_action = {'type': 'ir.actions.act_window_close'}
+
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'message': message,
+                'type': type_color,
+                'sticky': True,
+                'next': next_action,
+            },
+        }
+
+    def button_refresh_authentication(self):
+        self.ensure_one()
+        base_url = ENDPOINT if self.edi_mode == 'prod' else TEST_ENDPOINT
+        response = iap_tools.iap_jsonrpc(f'{base_url}/api/signaturit_id_authentication/1/kyc_status', params={
+            'object_uuid': self.pdp_authentication_uuid,
+        })
+        kyc_status = response.get('kyc_status')
+        if kyc_status in ('success', 'fail'):
+            self.pdp_kyc_status = kyc_status
+            return self._display_status_notification()
+
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'message': self.env._("Status updated."),
+                'type': 'success',
+                'next': self._action_open_pdp_form(),
+            },
+        }
+
+    def button_open_authentication_link(self):
+        self.ensure_one()
+        base_url = ENDPOINT if self.edi_mode == 'prod' else TEST_ENDPOINT
+        return {
+            'type': 'ir.actions.act_url',
+            'url': f'{base_url}/api/id_authentication/1/authentication_portal/{self.auth_url_hash}',
+            'target': 'new',
+        }
+
+    def button_cancel_authentication(self):
+        self.ensure_one()
+        self.pdp_kyc_status = 'fail'
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'message': self.env._("Authentication cancelled"),
+                'type': 'danger',
+                'next': {'type': 'ir.actions.act_window_close'},
+            }
+        }
 
     def button_register_pdp_participant(self):
         self.ensure_one()
