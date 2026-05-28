alkuneemi/Luxara

[FIX] pos_viva_wallet: send cashRegisterId for Viva when cashier name is missing
When validating a payment in POS Kiosk with Viva payment method
we get a Viva.com error
Viva’s card-terminal API validates the JSON body with Pydantic and
requires a non-empty ``cashRegisterId``.

Steps to reproduce:
-------------------
* Open POS in kiosk
* Make an order and pay with Viva
> Observation:
Viva returns a validation error: ``cashRegisterId`` is missing
or required in the request body (Pydantic ``missing`` on ``body.cashRegisterId``).

Why the fix:
------------
Compute ``cashRegisterId`` in the POS client as cashier name, then
``pos.config.name`` so the value is always a non-empty string sent
to ``viva_wallet_send_payment_request``.

opw-6091223

addons/pos_viva_wallet/tests/test_frontend.py
@@ -39,3 +39,25 @@ class TestVivaWalletHttpCommon(TestPointOfSaleHttpCommon):
         with patch.object(PosPaymentMethod, '_call_viva_wallet', mocked_call_viva_wallet_check_post_data):
             self.main_pos_config.open_ui()
             self.start_tour("/pos/ui?config_id=%d" % self.main_pos_config.id, 'VivaWalletTour', login="accountman")
+
+    def test_vw_kiosk_request_data(self):
+        """Kiosk: ``cashRegisterId`` must fall back to ``pos.config.name``."""
+        config_name = self.main_pos_config.name
+
+        def mocked_call_viva_wallet_kiosk(self, endpoint, action, data=None):
+            if action == 'post' and data is not None:
+                cash_register_id = data.get('cashRegisterId')
+                if cash_register_id != config_name:
+                    raise AssertionError(
+                        "Without a cashier, Viva payload must use pos.config.name as cashRegisterId "
+                        f"(expected {config_name!r}, got {cash_register_id!r})",
+                    )
+            return {}
+
+        with patch.object(PosPaymentMethod, '_call_viva_wallet', mocked_call_viva_wallet_kiosk):
+            self.main_pos_config.open_ui()
+            self.start_tour(
+                "/pos/ui?config_id=%d" % self.main_pos_config.id,
+                'VivaWalletKioskTour',
+                login="accountman",
+            )

odoo/odoo

X-original-commit: 438149819185aea3d26dbad1fcb62d5fbac9832a

addons/pos_viva_com/tests/test_frontend.py
@@ -56,3 +56,25 @@ class TestVivaComHttpCommon(TestPointOfSaleHttpCommon):
         with patch.object(PosPaymentMethod, '_call_viva_com', mocked_call_viva_com_check_post_data):
             self.main_pos_config.open_ui()
             self.start_tour("/pos/ui?config_id=%d" % self.main_pos_config.id, 'VivaComTour', login="accountman")
+
+    def test_viva_com_kiosk_request_data(self):
+        """Kiosk: ``cashRegisterId`` must fall back to ``pos.config.name``."""
+        config_name = self.main_pos_config.name
+
+        def mocked_call_viva_com_kiosk(self, endpoint, action, data=None):
+            if action == 'post' and data is not None:
+                cash_register_id = data.get('cashRegisterId')
+                if cash_register_id != config_name:
+                    raise AssertionError(
+                        "Without a cashier, Viva payload must use pos.config.name as cashRegisterId "
+                        f"(expected {config_name!r}, got {cash_register_id!r})",
+                    )
+            return {}
+
+        with patch.object(PosPaymentMethod, '_call_viva_com', mocked_call_viva_com_kiosk):
+            self.main_pos_config.open_ui()
+            self.start_tour(
+                "/pos/ui?config_id=%d" % self.main_pos_config.id,
+                'VivaComKioskTour',
+                login="accountman",
+            )