3473f5920783526d30489fe787977e7008cd762a
Traina Ludo <ltra@odoo.com>
2026-05-29 10:02:38 +0000

    [FIX] point_of_sale, ...: restore preparation ticket styling
    ..., pos_restaurant, pos_self_order, pos_urban_piper
    
    ---
    
    During the refactor of the receipt printing system, some CSS classes were no longer loaded with preparation tickets.
    As a result, preparation tickets lost part of their original styling.
    
    To restore the expected rendering, ensure all required classes are properly loaded while keeping the loading minimal.
    
    Additionally, some receipt data were missing after the refactor and some UI elements could be improved.
    This commit restores the missing data and improves the overall UI.
    
    It also fixes an issue where customer notes were not printed on the receipt.
    
    Templates checked:
    
    * point_of_sale.pos_order_change_receipt
    * point_of_sale.pos_order_change_receipt_line
    
    ---
    
    Task: https://www.odoo.com/odoo/project/1737/tasks/6133403
    Refacto: https://github.com/odoo/odoo/pull/244395
    X-original-commit: 0952b2771d0fcc041757a5d6b7a4b21af3da01af

--- addons/pos_restaurant/static/tests/unit/services/pos_service.test.js
+++ addons/pos_restaurant/static/tests/unit/services/pos_service.test.js
@@ -516,10 +516,14 @@ describe("restaurant pos_store.js", () => {
         const generator = store.ticketPrinter.getGenerator({ models: store.models, order });
         const orderChange = generator.generatePreparationData(new Set([...pos_categories]), {});
         expect(orderChange[0].extra_data.order_label).toBe(partner.name);
+        expect(orderChange[0].extra_data.table_name).toBeEmpty();
+        expect(orderChange[0].extra_data.floor_name).toBeEmpty();
 
         const table = store.models["restaurant.table"].get(2);
         const tableOrder = await getFilledOrder(store, { table_id: table });
         tableOrder.setPartner(partner);
+        expect(tableOrder.floating_order_name).not.toBe(partner.name);
+
         const tableGenerator = store.ticketPrinter.getGenerator({
             models: store.models,
             order: tableOrder,
@@ -528,6 +532,8 @@ describe("restaurant pos_store.js", () => {
             new Set([...pos_categories]),
             {}
         );
-        expect(tableOrderChange[0].extra_data.order_label).toBe(`T ${table.table_number}`);
+        expect(tableOrderChange[0].extra_data.order_label).toBe(false);
+        expect(tableOrderChange[0].extra_data.table_name).toBe(table.table_number);
+        expect(tableOrderChange[0].extra_data.floor_name).toBe(table.floor_id.name);
     });
 });
