9f743274bc1f8e29ea3a9dd9d2061de8a7267acf
Stéphane Diez <snd@odoo.com>
2026-05-29 11:36:05 +0200

    [CLN] stock,mrp,repair: make traceability code clearer
    The code of this report hasn't been adapted since around Odoo 10. There
    are a lot of improvement to be made just on simplicity and readability.
    
    task 5265405
    
    Part-of: odoo/odoo#269357
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/mrp/models/stock_traceability.py
+++ addons/mrp/models/stock_traceability.py
@@ -7,29 +7,34 @@ class StockTraceabilityReport(models.TransientModel):
     @api.model
     def _get_reference(self, move_line):
         res_model, res_id, ref = super()._get_reference(move_line)
-        if move_line.move_id.production_id and move_line.move_id.location_dest_usage != 'inventory':
+        move = move_line.move_id
+        if move.production_id and move.location_dest_usage != 'inventory':
             res_model = 'mrp.production'
-            res_id = move_line.move_id.production_id.id
-            ref = move_line.move_id.production_id.name
-        if move_line.move_id.raw_material_production_id and move_line.move_id.location_dest_usage != 'inventory':
+            res_id = move.production_id.id
+            ref = move.production_id.name
+        elif move.raw_material_production_id and move.location_dest_usage != 'inventory':
             res_model = 'mrp.production'
-            res_id = move_line.move_id.raw_material_production_id.id
-            ref = move_line.move_id.raw_material_production_id.name
-        if move_line.move_id.unbuild_id:
+            res_id = move.raw_material_production_id.id
+            ref = move.raw_material_production_id.name
+        elif move.unbuild_id:
             res_model = 'mrp.unbuild'
-            res_id = move_line.move_id.unbuild_id.id
-            ref = move_line.move_id.unbuild_id.name
-        if move_line.move_id.consume_unbuild_id:
+            res_id = move.unbuild_id.id
+            ref = move.unbuild_id.name
+        elif move.consume_unbuild_id:
             res_model = 'mrp.unbuild'
-            res_id = move_line.move_id.consume_unbuild_id.id
-            ref = move_line.move_id.consume_unbuild_id.name
+            res_id = move.consume_unbuild_id.id
+            ref = move.consume_unbuild_id.name
         return res_model, res_id, ref
 
     @api.model
     def _get_linked_move_lines(self, move_line):
-        move_lines, is_used = super()._get_linked_move_lines(move_line)
-        if not move_lines:
-            move_lines = (move_line.move_id.consume_unbuild_id and move_line.produce_line_ids) or (move_line.move_id.production_id and move_line.consume_line_ids)
-        if not is_used:
-            is_used = (move_line.move_id.unbuild_id and move_line.consume_line_ids) or (move_line.move_id.raw_material_production_id and move_line.produce_line_ids)
-        return move_lines, is_used
+        """ Return all the move lines linked to move_line.
+        parent_lines are the move lines that happen first in the production chain. We go towards the raw components.
+        children_lines are the move lines that happen later in the production chain. We go towards the final products. """
+        parent_lines, children_lines = super()._get_linked_move_lines(move_line)
+        move = move_line.move_id
+        if not parent_lines:
+            parent_lines = (move.consume_unbuild_id and move_line.produce_line_ids) or (move.production_id and move_line.consume_line_ids)
+        if not children_lines:
+            children_lines = (move.unbuild_id and move_line.consume_line_ids) or (move.raw_material_production_id and move_line.produce_line_ids)
+        return parent_lines, children_lines
