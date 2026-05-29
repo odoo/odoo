b8aaed8f83f4e00facbeff608bbd3b5a9d213505
Mathieu Coutant <mcou@odoo.com>
2026-05-29 16:11:22 +0200

    [FIX] hr_attendance: fix missing overtime on the last day of multi-day attendances
    Steps to reproduce:
    ----------------------------------------
    - Create two rules in an overtime ruleset:
        - Non-working hours rule:
            - Timing
            - Outside of a specific schedule
            - Select a schedule working Monday to Friday
        - Weekend rule:
            - Timing
            - On any non-working day
    - Give this ruleset to an employee
    - Create an attendance for this employee
        - from 21pm on Friday
        - to 4am on Saturday, the next day
    - Check the overtime lines of the attendance
    - There is only one overtime line for the first rule
    
    Cause:
    ----------------------------------------
    In the overtime refactor 49952e57ab2e8af908112fa77acd22a5e26fa627 the method `_get_dates()` was introduced to get the dates which an attendance overlap.
    
    It uses `rrule()` to create a list of datetime:
    `list(rrule(DAILY, dtstart=localized_start, until=localized_end))`
    
    But `rrule` is returning a new date every 24 hours after the time given in `dtstart`.
    In our example only the datetime onat 21pm on Friday is returned.
    If it was ending after 21pm on Friday this time would also be returned.
    
    These dates are given as `min_check_in` and `max_check_out`.
    So later these dates are used to calculate the non working-days:
    
    https://github.com/odoo/odoo/blob/8d14665af5acf1bd391d05a5048dc701986e8b15/addons/hr_attendance/models/hr_attendance_overtime_rule.py#L441-L448
    
    Only Friday is returned, so the second rule is ignored.
    
    Solution:
    ----------------------------------------
    We use `date()` when calling `rrule()` so the hours are ignored.
    
    This has no impact as `min_check_in` and `max_check_out` are always used later with `datetime.combine(min_check_in, datetime.min.time())`.
    
    opw-6159674
    
    closes odoo/odoo#267202
    
    Signed-off-by: Einar Andersen (eian) <eian@odoo.com>

--- addons/hr_attendance/models/hr_attendance.py
+++ addons/hr_attendance/models/hr_attendance.py
@@ -676,7 +676,7 @@ class HrAttendance(models.Model):
         result = {}
         for attendance in self:
             localized_start, localized_end = attendance._get_localized_times()
-            result[attendance] = list(rrule(DAILY, dtstart=localized_start, until=localized_end))
+            result[attendance] = list(rrule(DAILY, dtstart=localized_start.date(), until=localized_end.date()))
         return result
 
     def _get_attendance_by_periods_by_employee(self):
