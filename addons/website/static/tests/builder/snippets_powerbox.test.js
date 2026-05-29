e6954efd6b73b29890972c10e23b899eacf40d02
Sébastien (blse) <blse@odoo.com>
2026-05-29 05:19:22 +0000

    [FIX] website: prevent unwrapping `s_blockquote` on insert with powerbox
    When the snippet `s_blockquote` was inserted with the powerbox or pasted
    from clipboard in an unbreakable element which does not allow blocks
    as children, the `<blockquote>` element itself was abandonned and its
    children were inserted instead. This lead to insertion of a broken
    snippet.
    
    This commit marks the `s_blockquote` snippet as "unsplittable" so that
    always stays in one piece when inserted.
    
    Steps to reproduce:
    - Open website builder
    - Put cursor in a link
    - Type `/blockquote` and press enter
    - Bug: the snippet's children are inserted, instead of snippet itself
    
    task-6259092
    
    X-original-commit: f2db32fabd4d6df57617f2252711d85cf0dcd077

--- addons/website/static/tests/builder/snippets_powerbox.test.js
+++ addons/website/static/tests/builder/snippets_powerbox.test.js
@@ -2,6 +2,7 @@ import { defineWebsiteModels, setupWebsiteBuilder } from "./website_helpers";
 import { animationFrame, describe, expect, queryAllTexts, queryOne, test } from "@odoo/hoot";
 import { setSelection } from "@html_editor/../tests/_helpers/selection";
 import { insertText } from "@html_editor/../tests/_helpers/user_actions";
+import { execCommand } from "@html_editor/../tests/_helpers/userCommands";
 
 defineWebsiteModels();
 
@@ -22,4 +23,12 @@ describe("alert snippet availability in powerbox", () => {
         await animationFrame();
         expect(queryAllTexts(".o-we-command-name")).not.toInclude("Alert");
     });
+});
+
+test("should not unwrap s_blockquote when inserting it in unbreakable inline element", async () => {
+    const { getEditor } = await setupWebsiteBuilder(`<span class="oe_unbreakable">ab</span>`);
+    setSelection({ anchorNode: queryOne(":iframe span"), anchorOffset: 0 });
+    execCommand(getEditor(), "s_blockquote");
+    expect(":iframe span .s_blockquote").toHaveCount(0);
+    expect(":iframe .s_blockquote").toHaveCount(1);
 });
