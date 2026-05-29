20fd0a8671b835ec3f8b75c22bff623cb261e938
Benjamin Vray <bvr@odoo.com>
2026-05-29 15:17:41 +0200

    [FIX] html_builder: show full truncated option titles
    Before this commit, long option container titles could be clipped by the
    sidebar, with no way to read the full title.
    
    After this commit, clipped option container titles use an `ellipsis`,
    and hovering them shows the full title in a `tooltip`.
    
    task-6251151
    
    X-original-commit: https://github.com/odoo/odoo/commit/bd517f8e765a498e177be6537cd3a861ff662d5f

--- addons/html_builder/static/src/sidebar/option_container.js
+++ addons/html_builder/static/src/sidebar/option_container.js
@@ -37,6 +37,7 @@ export class OptionsContainer extends BaseOptionComponent {
         useVisibilityObserver("content", useApplyVisibility("root"));
 
         this.rootRef = useRef("root");
+        this.titleRef = useRef("title");
         useListener(browser, "focusin", this.updateOverlayPreview.bind(this));
         useListener(browser, "pointermove", this.updateOverlayPreview.bind(this));
         useExternalListener(this.document, "pointermove", this.updateOverlayPreview);
@@ -85,6 +86,15 @@ export class OptionsContainer extends BaseOptionComponent {
             : "";
 
         return (title || getSnippetName(this.env.getEditingElement())) + titleExtraInfo;
+    }
+
+    updateTitleTooltip(ev) {
+        if (!ev.currentTarget.dataset.tooltip) {
+            const titleEl = this.titleRef.el;
+            if (titleEl.offsetWidth < titleEl.scrollWidth) {
+                ev.currentTarget.dataset.tooltip = this.title;
+            }
+        }
     }
 
     /** @param {PointerEvent | FocusEvent} ev */
