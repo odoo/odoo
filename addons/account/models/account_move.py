7e1f1ce7fb627cd11a21dfbe3c7c7284a74944ed
Damien Bouvy <dbo@odoo.com>
2026-06-01 08:48:06 +0200

    [FIX] account: do not use archived bank accounts on new credit notes
    When creating a credit note, it is possible for the compute method
    _compute_partner_bank_id to be called in a context where active_test
    is falsy, leading to moves that cannot be validated because it would
    raise with the following error message:
    > The recipient bank account linked to this invoice is archived.
    So you cannot confirm the invoice.
    
    The state of the 'active_test' ctx key cannot be known in advance in a
    compute and should not be assumed as True; according to the framework
    team:
    > In practice, a compute method cannot expect active_test to have
    > a particular value. It may be invoked with any context. There is no
    > "context purge" done by the ORM. The computation may be "prepared"
    > with a context (the one of modified()) and actually done with another
    > context (code accessing the field before some explicit flush). In
    > other words, if the compute method searches for a record that matches
    > some conditions, and if that record cannot be inactive, then this
    > condition must be explicit in the search domain (or in the context).
    
    opw-6229286
    
    closes odoo/odoo#267398
    
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -1058,9 +1058,10 @@ class AccountMove(models.Model):
                 move.partner_bank_id = payment_method.journal_id.bank_account_id
                 continue
 
-            move.partner_bank_id = move.bank_partner_id.bank_ids.filtered(
-                lambda bank: not bank.company_id or bank.company_id == move.company_id
-            ).sorted(key=_bank_selection_key)[:1]
+            move.partner_bank_id = move.bank_partner_id.bank_ids.filtered_domain([
+                *self.env['res.partner.bank']._check_company_domain(move.company_id),
+                ('active', '=', True),  # active_test could be False in the context
+            ]).sorted(key=_bank_selection_key)[:1]
 
     @api.depends('partner_id')
     def _compute_invoice_payment_term_id(self):
