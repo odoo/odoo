afa2b6b7b47d6420146ceb6dad897405aa92c682
Pierre Paridans <app@odoo.com>
2026-06-01 13:50:55 +0000

    [FIX] tools: fix RuntimeError on OrderedSet copy with Python 3.14
    In Python 3.14, iterating over weak references (like `transaction.envs`)
    can trigger a `RuntimeError: dictionary changed size during iteration`.
    This happens mostly because the Garbage Collector can remove a weakref
    while `OrderedSet.copy()` is rebuilding the set via `dict.fromkeys()`.
    
    Instead of re-initializing the set by iterating over its elements, we
    now directly use the dictionary's native `.copy()` method. This atomic
    operation prevents the GC from modifying the size of the underlying
    `_map` during the copy.
    
    closes odoo/odoo#268074
    
    X-original-commit: 7dc4b82c157ef1414e543f54f4b9642af0518dd7
    Signed-off-by: Krzysztof Magusiak (krma) <krma@odoo.com>

--- odoo/tools/misc.py
+++ odoo/tools/misc.py
@@ -1090,7 +1090,9 @@ class OrderedSet(MutableSet[T], typing.Generic[T]):
         return reduce(OrderedSet.__and__, others, self)
 
     def copy(self):
-        return self.__class__(self)
+        new_set = OrderedSet()
+        new_set._map = self._map.copy()  # Atomic dict copy
+        return new_set
 
 
 class LastOrderedSet(OrderedSet[T], typing.Generic[T]):
@@ -1098,6 +1100,11 @@ class LastOrderedSet(OrderedSet[T], typing.Generic[T]):
     def add(self, elem):
         self.discard(elem)
         super().add(elem)
+
+    def copy(self):
+        new_set = LastOrderedSet()
+        new_set._map = self._map.copy()  # Atomic dict copy
+        return new_set
 
 
 class Callbacks:
