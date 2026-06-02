1b131e64bb4b38270b437a6d7ceca96490988e28
defl <defl@odoo.com>
2026-06-02 13:10:20 +0000

    [FIX] attachment_indexation,base: fix file indexation after memory optimization
    **Steps to reproduce:**
    - Install Documents app
    - Upload a new pdf file
    - Try to search the file with the 'Indexed Content' filter
    - Search won't find the file even if the uploaded
        document contains the searched keyword/content
    
    **Issue:**
    After [1] (19.3+) the file upload process was reworked
    to avoid loading entire files into memory during
    attachment creation (removed `'raw': file.read()`).
    
    But this breaks the index content creation as it was
    using the `raw` field value during the create to
    trigger the `_index` function in `_get_datas_related_values`.
    
    Also, restoring the previous behavior for the indexation
    would undo the memory usage improvements that were made.
    
    **Fix:**
    Added the `_index` call in `_upload_file` after the
    attachment creation.
    
    Also optimize the default text index to avoid reducing
    too much the memory improvements (but for now the other
    mimetypes can still be impacted by the type-specific `_index_*`
    and the external libraries performances).
    
    [1] https://github.com/odoo/odoo/commit/6222dedaf89a595b6f499679c3f553aa081c46bd
    
    opw-6232999
    
    X-original-commit: 4bdffc4738007eb677cf25e47fd869070f3ab3f3

--- addons/attachment_indexation/tests/test_indexation.py
+++ addons/attachment_indexation/tests/test_indexation.py
@@ -1,6 +1,7 @@
 from unittest import skipIf
 
 from odoo.tests.common import TransactionCase, tagged
+from odoo.tools import file_open
 
 try:
     from pdfminer.pdfinterp import PDFResourceManager
@@ -16,3 +17,13 @@ class TestCaseIndexation(TransactionCase):
         pdf = self.file_read('attachment_indexation/tests/files/test_content.pdf')
         text = self.env['ir.attachment']._index(pdf, 'application/pdf')
         self.assertEqual(text, 'TestContent!!', 'the index content should be correct')
+
+    @skipIf(PDFResourceManager is None, "pdfminer not installed")
+    def test_file_upload_indexation(self):
+        test_file = 'attachment_indexation/tests/files/test_content.pdf'
+        with file_open(test_file, 'rb') as f:
+            attach = self.env['ir.attachment']._upload_file(
+                f,
+                {'name': 'test_content.pdf'},
+            )
+        self.assertEqual(attach.index_content, 'TestContent!!')
