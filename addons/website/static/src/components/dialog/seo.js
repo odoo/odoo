85df33808a95bc4d591eec298bd6add86eadc603
Rahil Ghanchi <rahg@odoo.com>
2026-06-02 14:34:33 +0530

    [IMP] website, website_*: add warning for unpublished & unindexed pages *= sale
    Specification:
    - If the page is unpublished, unindexed, or has restricted access,
      show a warning banner at the top of the SEO dialog.
    - Provide a button in the warning banner that allows users to open
      the page properties directly.
    - If a user attempts to open page properties from the SEO dialog
      while there are unsaved changes, show a confirmation dialog and
      ask whether they want to save and continue to page properties or
      return to the SEO dialog.
    
    task-5952515

--- addons/website/static/src/components/dialog/seo.js
+++ addons/website/static/src/components/dialog/seo.js
@@ -1,18 +1,20 @@
 import { useLayoutEffect } from "@web/owl2/utils";
 import { _t } from "@web/core/l10n/translation";
 import { deduceURLfromText } from "@html_editor/main/link/utils";
-import { pyToJsLocale, jsToPyLocale } from "@web/core/l10n/utils";
+import { pyToJsLocale, jsToPyLocale, formatList } from "@web/core/l10n/utils";
 import { htmlToTextContentInline } from "@mail/utils/common/format";
 import { rpc } from "@web/core/network/rpc";
 import { escapeRegExp } from "@web/core/utils/strings";
 import { useService, useAutofocus } from "@web/core/utils/hooks";
 import { isVisible } from "@web/core/utils/ui";
 import { CheckBox } from "@web/core/checkbox/checkbox";
+import { ConfirmationDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 import { MediaDialog } from "@html_editor/main/media/media_dialog/media_dialog";
 import { getMimetype } from "@html_editor/utils/image";
 import { WebsiteDialog } from "./dialog";
 import {
     Component,
+    computed,
     onMounted,
     onWillStart,
     onWillUnmount,
@@ -625,6 +627,10 @@ export class TitleDescription extends Component {
         defaultTitle: t.string(),
         previewDescription: t.string(),
         url: t.string(),
+        isPublished: t.boolean(),
+        isVisibilityPublic: t.boolean(),
+        save: t.function(),
+        isDirty: t.function(),
     });
     static components = {
         SEOPreview,
@@ -634,6 +640,8 @@ export class TitleDescription extends Component {
     setup() {
         this.seoContext = proxy(seoContext);
         this.website = useService("website");
+        this.dialogs = useService("dialog");
+        this.websiteCustomMenus = useService("website_custom_menus");
         useAutofocus({ ref: this.autofocusRef });
 
         this.state = proxy({
@@ -722,12 +730,50 @@ export class TitleDescription extends Component {
         );
     }
 
+    get visibilityWarning() {
+        const reasons = [];
+        if (!this.props.isPublished) {
+            reasons.push(_t("unpublished"));
+        }
+        if (!this.props.isVisibilityPublic) {
+            reasons.push(_t("restricted to some visitors"));
+        }
+        if (!this.props.isIndexed) {
+            reasons.push(_t("hidden from search engines"));
+        }
+        return reasons.length
+            ? _t("This page is %(reasons)s.", { reasons: formatList(reasons) })
+            : "";
+    }
+
     //--------------------------------------------------------------------------
     // Handlers
     //--------------------------------------------------------------------------
 
     autoFill() {
         getSeo(this);
+    }
+
+    openPagePropertiesDialog() {
+        const openPageProperties = () =>
+            this.websiteCustomMenus.open({
+                xmlid: "website.menu_page_properties",
+            });
+        if (!this.props.isDirty()) {
+            return openPageProperties();
+        }
+        const saveAndOpen = async () => {
+            await this.props.save(false);
+            await openPageProperties();
+        };
+        this.dialogs.add(ConfirmationDialog, {
+            title: _t("Unsaved changes"),
+            body: _t("You made changes to your page's SEO settings, do you want to save them?"),
+            confirmLabel: _t("Save and go to Page Properties"),
+            cancelLabel: _t("Go back"),
+            confirm: saveAndOpen,
+            cancel: () => {},
+        });
     }
 
     /**
@@ -821,6 +867,7 @@ export class SeoChecks extends Component {
     };
     props = useProps({
         isDefaultLang: t.boolean(),
+        initialSeoState: t.object(),
     });
 
     async setup() {
@@ -994,6 +1041,14 @@ export class SeoChecks extends Component {
             isImageLink: link.isImageLink,
             validLink: null,
         }));
+        Object.assign(this.props.initialSeoState, {
+            brokenLinks: seoContext.brokenLinks.map(({ oldLink, newLink, remove, broken }) => ({
+                oldLink,
+                newLink,
+                remove,
+                broken,
+            })),
+        });
     }
 }
 
@@ -1011,6 +1066,20 @@ export class OptimizeSEODialog extends Component {
         close: t.function(),
     });
 
+    savedContext = signal(false);
+    isDirty = computed(
+        () => {
+            this.savedContext();
+            const currentSeoState = JSON.stringify(this.getEditableSeoState());
+            return currentSeoState !== JSON.stringify(this.initialSeoState);
+        },
+        {
+            set: (value) => {
+                this.savedContext.set(value);
+            },
+        }
+    );
+
     setup() {
         this.website = useService("website");
         this.dialogs = useService("dialog");
@@ -1020,8 +1089,10 @@ export class OptimizeSEODialog extends Component {
         this.saveButton = _t("Save");
         this.size = "lg";
         this.contentClass = "oe_seo_configuration";
+        this.boundSave = this.save.bind(this);
 
         onWillStart(async () => {
+            this.initialSeoState = {};
             // Wait for the preview iframe because this dialog reads directly
             // from the iframe DOM.
             await this.waitForIframe();
@@ -1030,8 +1101,10 @@ export class OptimizeSEODialog extends Component {
             } = this.website.currentWebsite;
             this.object = seoObject || mainObject;
             this.data = await rpc("/website/get_seo_data", {
-                res_id: this.object.id,
-                res_model: this.object.model,
+                seo_id: this.object.id,
+                seo_model: this.object.model,
+                main_object_id: mainObject.id,
+                main_object_model: mainObject.model,
             });
 
             if (this.data.multi_lang) {
@@ -1050,6 +1123,10 @@ export class OptimizeSEODialog extends Component {
 
             // If website.page, hide the google preview & tell user his page is currently unindexed
             this.isIndexed = "website_indexed" in this.data ? this.data.website_indexed : true;
+            // If its publishable object, tell user his page is currently unpublished.
+            this.isPublished = this.data.website_is_published ?? true;
+            // If website.page, tell user his page is currently public or not.
+            this.isVisibilityPublic = this.data.website_page_visibility ?? true;
             this.seoNameHelp = _t(
                 "This value will be escaped to be compliant with all major browsers and used in url. Keep it empty to use the default name of the record."
             );
@@ -1091,6 +1168,30 @@ export class OptimizeSEODialog extends Component {
                 seoContext.keywords = [];
             }
         });
+        onMounted(() => {
+            Object.assign(this.initialSeoState, this.getEditableSeoState());
+        });
+    }
+
+    getEditableSeoState() {
+        return {
+            title: seoContext.title,
+            description: seoContext.description,
+            seoName: seoContext.seoName,
+            metaImage: seoContext.metaImage,
+            keywords: Array.from(seoContext.keywords),
+            altAttributes: seoContext.altAttributes.map(({ id, alt, decorative }) => ({
+                id,
+                alt,
+                decorative,
+            })),
+            brokenLinks: seoContext.brokenLinks.map(({ oldLink, newLink, remove, broken }) => ({
+                oldLink,
+                newLink,
+                remove,
+                broken,
+            })),
+        };
     }
 
     async waitForIframe() {
@@ -1139,7 +1240,7 @@ export class OptimizeSEODialog extends Component {
         return el && el.content;
     }
 
-    async save() {
+    async save(refresh = true) {
         const data = {};
         if (this.canEditTitle) {
             data.website_meta_title = seoContext.title;
@@ -1232,12 +1333,16 @@ export class OptimizeSEODialog extends Component {
         }
 
         await Promise.all(rpcCalls);
+        Object.assign(this.initialSeoState, this.getEditableSeoState());
+        this.isDirty.set(!this.savedContext());
 
-        this.website.goToWebsite({
-            path: this.url.replace(
-                this.previousSeoName || this.seoNameDefault,
-                seoContext.seoName || this.seoNameDefault
-            ),
-        });
+        if (refresh) {
+            this.website.goToWebsite({
+                path: this.url.replace(
+                    this.previousSeoName || this.seoNameDefault,
+                    seoContext.seoName || this.seoNameDefault
+                ),
+            });
+        }
     }
 }
