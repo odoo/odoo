205c0a96576048253519e2afd3d1ec1e28a8dff5
pkgu-odoo <pkgu@odoo.com>
2026-06-03 00:08:51 +0530

    [FIX] l10n_din5008_stock: always show informations in din 5008 for delivery
    Steps to reproduce:
    -------------------
    - Install `l10n_din5008` and `stock` module.
    - Configure the document layout as DIN 5008
    - Create and validate a delivery order
    - Print the delivery slip
    
    Issue:
    ------
    The delivery slip not displays the `informations` div
    (Order, Shipping Date, Total Weight, etc.), In the DIN 5008 Loyout
    
     Cause:
    ---------
    
    The DIN 5008 layout default hides the  `#informations` block:
    
    https://github.com/odoo/odoo/blob/674c748458e6e29715ad3763f267802d921dce55/addons/l10n_din5008/static/src/scss/report_din5008.scss#L73
    
    In this commit
    https://github.com/odoo/odoo/commit/d10174e49759a58058aed6c44026060a4d293cc3
      `#informations` id apply into delivery slip.
    
    https://github.com/odoo/odoo/blob/674c748458e6e29715ad3763f267802d921dce55/addons/stock/report/report_deliveryslip.xml#L49
    
    So as default behavior of DIN 5008 layout to hide `#informations` block
    but there was already this `class="report-wrapping-flexbox"`
    
    However, that class was added in this commit
    https://github.com/odoo/odoo/commit/28c1cc6025005cd0937afbc452a722235cc5a38d
    
    This prevents the `informations` block from being hidden because of the CSS
    `!important` property:
    
    https://github.com/odoo/odoo/blob/674c748458e6e29715ad3763f267802d921dce55/addons/web/static/src/webclient/actions/reports/bootstrap_review_report.scss#L59-L62
    
    So when switching to the DIN 5008 layout, it tries to hide the `informations`
    block, but the `!important` property prevents it from being hidden.
    
    Later, this commit:
    https://github.com/odoo/odoo/commit/8d588f8198d9057311304e596c009a0795ca6ec7
    
    changed the behavior. Instead of targeting the `#informations` block itself,
    it targets its children and hides all of them except `plan`.
    
    As a result, all other children of the information block become hidden.
    Since there is no `!important` rule on the children, they can be hidden
    successfully. The `informations` block then becomes empty so nothing is
    displayed.
    
    Solution:
    -----------
    
    - Since only the stock delivery slip uses the
    `report-wrapping-flexbox clearfix` class, which prevents the
    `informations` block from being hidden in the DIN 5008 layout, we add a
    condition to avoid hiding the children of `informations` when the report
    is rendered for a stock picking.
    
    - This preserves the original delivery slip behavior while keeping the DIN
    5008 hiding logic for other reports.
    
    opw-6250072

--- addons/l10n_din5008_stock/__manifest__.py
+++ addons/l10n_din5008_stock/__manifest__.py
@@ -12,6 +12,11 @@
         'report/din5008_stock_templates.xml',
         'report/din5008_stock_picking_layout.xml',
     ],
+    'assets': {
+        'web.report_assets_common': [
+            'l10n_din5008_stock/static/src/scss/report_din5008.scss',
+        ],
+    },
     'auto_install': True,
     'author': 'Odoo S.A.',
     'license': 'LGPL-3',
