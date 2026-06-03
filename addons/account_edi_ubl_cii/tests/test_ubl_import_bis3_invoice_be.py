065611fcff576fb46f78e03baecb1676b449b885
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2026-06-03 13:25:56 +0000

    [FIX] account,account_edi_ubl_cii: nested attachment import
    Fixup of 3fc85b6ed7936956abbaf8e8364bb2b288cbe289
    
    Issue 1
    - Import XML bill into documents app
    - Create vendor bill from the document
    
    Issue:
    Only the main attachment would be found in the created bill
    It occurs because in the early creation of attachments, they get
    linked to the wrong record (document instead of move)
    
    opw-6267888
    
    ---
    
    Issue 2
    
    - From the accounting app import XML bill containing two identically
      named documents
    Issue:
    Two bills were created
    It occurs because in case of identical file name the system would still
    create a separate bill
    
    opw-6231265
    
    X-original-commit: 004ba620147b8b94cbc99e144d4108c4e050ce44

--- addons/account_edi_ubl_cii/tests/test_ubl_import_bis3_invoice_be.py
+++ addons/account_edi_ubl_cii/tests/test_ubl_import_bis3_invoice_be.py
@@ -101,3 +101,69 @@ class TestUblImportBis3InvoiceBE(TestUblBis3Common, TestUblCiiBECommon):
                 },
             ],
         )
+<<<<<<< 97d06d335d57b3f3c24a2ca02616d6a0df1f1bb5
+||||||| c06be48ce7277a667719fd756e0a1f63e91cda27
+
+    def test_import_embedded_pdf(self):
+        """
+        Importing an xml with embedded pdf should correctly import the
+        pdf in the newly created bill
+        """
+        journal = self.company_data['default_journal_purchase']
+        xml_attachment = self._import_invoice_as_attachment(test_name='test_import_embedded_pdf')
+
+        # Import the document manually
+        bill = self._import_invoice_as_attachment_on(attachment=xml_attachment, journal=journal)
+        self.assertEqual(len(bill.attachment_ids), 1)
+
+        init_vals = {'move_type': 'in_invoice', 'journal_id': journal.id}
+        email_raw = self._get_raw_mail_message_str(attachments=xml_attachment, email_to=journal.alias_id.display_name)
+        # Import the document via mail alias
+        move_id = self.env['mail.thread'].message_process('account.move', email_raw, custom_values=init_vals)
+        bill = self.env['account.move'].browse(move_id)
+
+        self.assertEqual(len(bill.attachment_ids), 1)
+=======
+
+    def test_import_embedded_pdf(self):
+        """
+        Importing an xml with embedded pdf should correctly import the
+        pdf in the newly created bill
+        """
+        journal = self.company_data['default_journal_purchase']
+        xml_attachment = self._import_invoice_as_attachment(test_name='test_import_embedded_pdf')
+
+        # Import the document manually
+        created_moves = []
+        move_create = self.env.registry['account.move'].create
+
+        # patch used to retrieve all created documents
+        def patched_create(self, vals_list):
+            records = move_create(self, vals_list)
+            created_moves.extend(records.ids)
+            return records
+        self.patch(self.env.registry['account.move'], 'create', patched_create)
+
+        # Import the document manually
+        journal.create_document_from_attachment(xml_attachment.id)
+
+        self.assertEqual(len(created_moves), 1, "A single bill should be created")
+        bill = self.env['account.move'].browse(created_moves)
+        self.assertTrue(bill.message_main_attachment_id, "The Bill should have a main attachment")
+        self.assertEqual(bill.message_main_attachment_id.mimetype, "application/pdf", "The main attachment should be a pdf")
+        self.assertEqual(bill.message_main_attachment_id.res_id, bill.id, "The main attachment res_id should be the invoice id")
+        self.assertEqual(len(bill.message_ids.mapped('attachment_ids')), 4, "All nested attachments should be attached to a chatter message")
+
+        # Import the document via mail alias
+        init_vals = {'move_type': 'in_invoice', 'journal_id': journal.id}
+        email_raw = self._get_raw_mail_message_str(attachments=xml_attachment, email_to=journal.alias_id.display_name)
+        created_moves = []
+        move_id = self.env['mail.thread'].message_process('account.move', email_raw, custom_values=init_vals)
+        bill = self.env['account.move'].browse(move_id)
+
+        self.assertEqual(len(created_moves), 1, "A single bill should be created")
+        self.assertTrue(bill.message_main_attachment_id, "The Bill should have a main attachment")
+        self.assertEqual(bill.message_main_attachment_id.mimetype, "application/pdf", "The main attachment should be a pdf")
+        self.assertEqual(bill.message_main_attachment_id.res_id, bill.id, "The main attachment res_id should be the invoice id")
+        self.assertEqual(len(bill.message_ids.mapped('attachment_ids')), 4, "All nested attachments should be attached to a chatter message")
+>>>>>>> 1d79a636208663a54835882d1a68e3fea961fcef
