667220637df8af91a12168a4355a10f6448c76f4
Claire (clbr) <clbr@odoo.com>
2026-06-03 22:10:42 +0000

    [FIX] point_of_sale,*: mandatory fields missing on partner form
    *: l10n_{ar,co,in,pe,uy}_pos
    
    **Problem:**
    The POS "Edit/Create customer" Form was switched to a standalone,
    hardcoded form view (view_partner_form_pos_ui) that inherits nothing
    during [1], in the attempt to simplify the view when accessed from the
    PoS.
    
    Every field that localizations and other modules add to the partner
    form by inheriting base.view_partner_form therefore disappeared when
    accessed from PoS. Some of the fields are required, for example, to invoice.
    
    **Solution:**
    Keep the simplified view as the default, but route the view selection
    through an overridable hook that localization can tweak case by case.
    The override is applied to the affected POS bridges (see module list).
    
    Add a test to prevent future regression.
    
    [19.3]: also re-add the additional_identifiers that were removed.
    
    **Note:**
    Another possibility is to re-inherit for each localization the new
    standalone view, but this fix would need to update the module to work,
    while this one works with just a restart.
    There are still ongoing discussion with PoS team to see if we really
    want to go back to each localization needing to inherit backend views.
    
    [1]: https://github.com/odoo/odoo/pull/230721/changes#diff-66cd201e7e8cfff5218a9fa93efd72f0bd77659b87359f2ca8763702462aaf92R26
    
    opw-6244777 (many more)
    
    X-original-commit: cb6b92428264ee4d495e3946948a72139c8e90a7

--- addons/l10n_ar_pos/tests/test_ar_pos.py
+++ addons/l10n_ar_pos/tests/test_ar_pos.py
@@ -5,6 +5,7 @@ from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 
 @tagged('post_install', '-at_install', 'post_install_l10n')
 class TestGenericAR(TestGenericLocalization):
+    pos_partner_pos_form_fields = ['l10n_latam_identification_type_id', 'l10n_ar_afip_responsibility_type_id']
 
     @classmethod
     @AccountTestInvoicingCommon.setup_country('ar')
