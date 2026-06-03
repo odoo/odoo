fleetapps/odoo3

[IMP] html_editor,website_*: improve media dialog video
Unify and rework the multiple different ways we used to match external platform video in the editor and website.

TPVP : third party video providers (YouTube, Vimeo, Intagram, ...)

- Refactor the 'get_video_source_data' python util into a JavaScript equivalent to avoid a lot RPC calls.
- Remove the '/web_editor/video_url/data' python controller which became obsolete.
- Encapsulate supported TPVP regexes and settings into their own classes.
- Remove TPVP thumbnail url definitions from the 'get_video_thumbnail' html_editor util.
- Modify 'product_image.py' model to expect thumbnailUrl in record creation, instead of 'get_video_thumbnail' util calculating it based on video ID.
- Create a new way to parse url and determine if they are from a TPVP then get all the necessary data from `TPVPClass.getVideoUrlData()`.
- Refactor the `VideoSelector` component, used by the media dialog, to be simpler and use the new `getVideoUrlData` mechanism.

task-5318096

addons/html_editor/static/src/others/embedded_components/backend/video/video.js
@@ -4,23 +4,26 @@ import {
     StateChangeManager,
     useEmbeddedState,
 } from "@html_editor/others/embedded_component_utils";
-import { getVideoUrl } from "@html_editor/utils/url";
-import {
-    Component,
-    onMounted,
-    onWillDestroy,
-    onWillUnmount,
-} from "@odoo/owl";
+import { Component, onMounted, onWillDestroy, onWillUnmount } from "@odoo/owl";
 import { Dropdown } from "@web/core/dropdown/dropdown";
 import { useDropdownState } from "@web/core/dropdown/dropdown_hooks";
 import { DropdownItem } from "@web/core/dropdown/dropdown_item";
-import { ReadonlyEmbeddedVideoComponent } from "../../core/video/readonly_video";
+import { ReadonlyEmbeddedVideoComponent } from "@html_editor/others/embedded_components/core/video/readonly_video";
+import { PLATFORMS } from "@html_editor/main/media/media_dialog/video_selector";
 
 export class EmbeddedVideoComponent extends ReadonlyEmbeddedVideoComponent {
     static template = "html_editor.EmbeddedVideo";
     static props = {
-        platform: { type: String },
-        videoId: { type: String },
+        // The emebedded video can be initialized either with :
+        // the platform and videoId props
+        //  OR
+        // the src prop
+        platform: { type: String, optional: true },
+        videoId: { type: String, optional: true },
+        // 'src' should be 'embedUrl' to keep consistency,
+        // but we can't to ensure retro compatibility.
+        src: { type: String, optional: true },
+        baseUrl: { type: String, optional: true },
         params: { type: Object, optional: true },
         host: { type: HTMLElement },
         createOverlay: { type: Function, optional: true },
@@ -33,6 +36,23 @@ export class EmbeddedVideoComponent extends ReadonlyEmbeddedVideoComponent {
         super.setup();
         this.videoBlock = this.props.host;
         this.state = useEmbeddedState(this.videoBlock);
+
+        if (!this.state.platform || !this.state.videoId) {
+            if (!this.props.src) {
+                console.error("Video data missing to mount video emebeded Component");
+                return;
+            }
+            const videoData = this.getVideoDataFromSrc(this.props.src);
+            if (videoData) {
+                this.state.platform = videoData.platform;
+                this.state.videoId = videoData.videoId;
+                this.state.baseUrl = videoData.baseUrl;
+                this.state.params = videoData.options;
+            } else {
+                console.error("Provided src is not a valid url or supported platform");
+            }
+        }
+
         this.dropdown = useDropdownState();
 
         this.videoSettingsOverlay = this.props.createOverlay(VideoSettings, {
@@ -77,13 +97,9 @@ export class EmbeddedVideoComponent extends ReadonlyEmbeddedVideoComponent {
         });
     }
 
-    get url() {
-        const urlParams = Object.fromEntries(
-            Object.entries(this.state.params).filter(([key]) => key !== "isVertical")
-        );
-        return decodeURIComponent(
-            getVideoUrl(this.state.platform, this.state.videoId, urlParams).toString()
-        );
+    get embedUrl() {
+        const platFormClass = PLATFORMS[this.state.platform];
+        return platFormClass.getEmbedUrl(this.state.videoId, this.state.params);
     }
 
     /**
@@ -91,10 +107,12 @@ export class EmbeddedVideoComponent extends ReadonlyEmbeddedVideoComponent {
      * @param {Object} media
      */
     replaceVideo(media) {
-        this.state.videoId = media.videoId;
+        this.state.baseUrl = media.baseUrl;
+        this.state.embedUrl = media.embedUrl;
         this.state.platform = media.platform;
-        this.state.params = media.params;
-        const isVertical = media.params.isVertical;
+        this.state.videoId = media.videoId;
+        this.state.params = media.options;
+        const isVertical = !!media.options?.isVertical;
         if (isVertical) {
             this.videoBlock.dataset.isVertical = "true";
         } else {

addons/html_editor/static/tests/media/paste_video_url.test.js
@@ -1,0 +1,171 @@
+import { describe, expect, mockFetch, test } from "@odoo/hoot";
+import { press, waitFor, waitForNone } from "@odoo/hoot-dom";
+import { animationFrame } from "@odoo/hoot-mock";
+
+import { setupEditor } from "../_helpers/editor";
+import { insertText, pasteText, undo } from "../_helpers/user_actions";
+import { expectElementCount } from "../_helpers/ui_expectations";
+import { getContent } from "../_helpers/selection";
+import { cleanLinkArtifacts } from "../_helpers/format";
+import { PLATFORMS } from "@html_editor/main/media/media_dialog/video_selector";
+
+import {
+    EMBEDDED_COMPONENT_PLUGINS,
+    MAIN_PLUGINS,
+    NO_EMBEDDED_COMPONENTS_FALLBACK_PLUGINS,
+} from "@html_editor/plugin_sets";
+import { MAIN_EMBEDDINGS } from "@html_editor/others/embedded_components/embedding_sets";
+const NO_EMBEDDED_COMPONENTS_CONFIG = {
+    Plugins: [...MAIN_PLUGINS, ...NO_EMBEDDED_COMPONENTS_FALLBACK_PLUGINS],
+};
+const EMBEDDED_COMPONENTS_CONFIG = {
+    Plugins: [...MAIN_PLUGINS, ...EMBEDDED_COMPONENT_PLUGINS],
+    resources: { embedded_components: MAIN_EMBEDDINGS },
+};
+
+for (const [platform, platformClass] of Object.entries(PLATFORMS)) {
+    describe(`paste ${platform} url`, () => {
+        const videoUrl = platformClass.unitTestUrls.base;
+        test(`should transform a ${platform} URL`, async () => {
+            const { editor } = await setupEditor("<p>ab[]cd</p>", {
+                config: NO_EMBEDDED_COMPONENTS_CONFIG,
+            });
+            mockFetch(() => '{"data": "mockFetch api result data"}');
+            pasteText(editor, videoUrl);
+            await animationFrame();
+            await expectElementCount(".o-we-powerbox", 1);
+            // Validate powerbox on the default first choice
+            await press("Enter");
+            await animationFrame();
+
+            const iframeContainer = await waitFor(`div.media_iframe_video`);
+            const iframe = await waitFor(`div.media_iframe_video iframe`);
+            const iframeContainerData = iframeContainer?.dataset || {};
+
+            expect(iframeContainerData.platform).toBe(platform);
+            expect(iframeContainerData.baseUrl).toBe(videoUrl);
+
+            expect(iframeContainerData.videoId).not.toBeEmpty();
+            expect(videoUrl.includes(iframeContainerData.videoId)).toBe(true);
+            expect(iframeContainerData.embedUrl).not.toBeEmpty();
+            expect("undefined").not.toBe(iframeContainerData.embedUrl);
+            expect(iframeContainerData.embedUrl).toBe(iframe.src);
+        });
+        test(`should transform a ${platform} URL with embeded Components`, async () => {
+            const { editor } = await setupEditor("<p>ab[xx]cd</p>", {
+                config: EMBEDDED_COMPONENTS_CONFIG,
+            });
+            mockFetch(() => '{"data": "mockFetch api result data"}');
+            pasteText(editor, videoUrl);
+            await animationFrame();
+            await expectElementCount(".o-we-powerbox", 1);
+            // Force powerbox validation on the default first choice
+            await press("Enter");
+
+            const iframeContainer = await waitFor(`[data-embedded="video"]`);
+            const iframe = await waitFor(`[data-embedded="video"] iframe`);
+            const iframeContainerData = iframeContainer?.dataset || {};
+
+            const embededProps = JSON.parse(iframeContainerData.embeddedProps);
+            expect(embededProps.platform).toBe(platform);
+            expect(embededProps.baseUrl).toBe(videoUrl);
+
+            expect(iframe.dataset.baseUrl).not.toBeEmpty();
+            expect(iframe.dataset.baseUrl).toBe(embededProps.baseUrl);
+
+            // QWeb templates rendered during unit test store src in data-src
+            const iframeSrc = iframe.getAttribute("src") || iframe.dataset.src || "";
+            expect(iframeSrc).not.toBeEmpty();
+            expect(iframeSrc.includes("https")).toBe(true);
+        });
+    });
+}
+
+describe("generic paste video url behaviors", () => {
+    const videoUrl = "https://youtu.be/dQw4w9WgXcQ";
+    test(`should NOT transform a video URL in a existing link`, async () => {
+        const { el, editor, plugins } = await setupEditor(
+            '<p>a<a href="http://existing.com">b[]c</a>d</p>',
+            { config: EMBEDDED_COMPONENTS_CONFIG }
+        );
+        pasteText(editor, videoUrl);
+        // Ensure the powerbox is active
+        await expectElementCount(".o-we-powerbox", 0);
+        const powerbox = plugins.get("powerbox");
+        expect(powerbox.overlay.isOpen).not.toBe(true);
+        expect(cleanLinkArtifacts(getContent(el))).toBe(
+            `<p>a<a href="http://existing.com">b${videoUrl}[]c</a>d</p>`
+        );
+    });
+    test(`should paste a video URL as a link when selecting alternate powerbox option`, async () => {
+        const { el, editor } = await setupEditor("<p>[]</p>", {
+            config: NO_EMBEDDED_COMPONENTS_CONFIG,
+        });
+        pasteText(editor, videoUrl);
+        await animationFrame();
+        await expectElementCount(".o-we-powerbox", 1);
+        // Pick the second command (Paste as URL)
+        await press("ArrowDown");
+        await press("Enter");
+        expect(cleanLinkArtifacts(getContent(el))).toBe(
+            `<p><a href="${videoUrl}">${videoUrl}</a>[]</p>`
+        );
+    });
+    test("should not revert a history step when pasting a video URL as a link", async () => {
+        const { el, editor } = await setupEditor("<p>[]</p>", {
+            config: NO_EMBEDDED_COMPONENTS_CONFIG,
+        });
+        // paste text to have a history step recorded
+        pasteText(editor, "*should not disappear*");
+        pasteText(editor, videoUrl);
+        await animationFrame();
+        await expectElementCount(".o-we-powerbox", 1);
+        // Pick the second command (Paste as URL)
+        await press("ArrowDown");
+        await press("Enter");
+        expect(cleanLinkArtifacts(getContent(el))).toBe(
+            `<p>*should not disappear*<a href="${videoUrl}">${videoUrl}</a>[]</p>`
+        );
+    });
+    test("should restore selection after pasting video URL followed by UNDO (1)", async () => {
+        const { el, editor } = await setupEditor("<p>[abc]</p>", {
+            config: NO_EMBEDDED_COMPONENTS_CONFIG,
+        });
+        pasteText(editor, videoUrl);
+        await animationFrame();
+        await expectElementCount(".o-we-powerbox", 1);
+        // Force powerbox validation on the default first choice
+        await press("Enter");
+        // Undo
+        undo(editor);
+        expect(getContent(el)).toBe("<p>[abc]</p>");
+    });
+
+    test("should restore selection after pasting video URL followed by UNDO (2)", async () => {
+        const { el, editor } = await setupEditor("<p>[abc]</p>", {
+            config: NO_EMBEDDED_COMPONENTS_CONFIG,
+        });
+        pasteText(editor, videoUrl);
+        await animationFrame();
+        await expectElementCount(".o-we-powerbox", 1);
+        // Pick the second command (Paste as URL)
+        await press("ArrowDown");
+        await press("Enter");
+        // Undo
+        undo(editor);
+        expect(getContent(el)).toBe("<p>[abc]</p>");
+    });
+    test("should close powerbox after an undo", async () => {
+        const { el, editor } = await setupEditor("<p>a[]b</p>", {
+            config: NO_EMBEDDED_COMPONENTS_CONFIG,
+        });
+        await insertText(editor, "x");
+        expect(getContent(el)).toBe(`<p>ax[]b</p>`);
+        pasteText(editor, videoUrl);
+        await animationFrame();
+        await expectElementCount(".o-we-powerbox", 1);
+        await undo(editor);
+        await waitForNone(".o-we-powerbox");
+        expect(getContent(el)).toBe(`<p>ax[]b</p>`);
+    });
+});

odoo/odoo

addons/html_editor/static/src/others/embedded_components/backend/video/video.js
@@ -4,23 +4,26 @@ import {
     StateChangeManager,
     useEmbeddedState,
 } from "@html_editor/others/embedded_component_utils";
-import { getVideoUrl } from "@html_editor/utils/url";
-import {
-    Component,
-    onMounted,
-    onWillDestroy,
-    onWillUnmount,
-} from "@odoo/owl";
+import { Component, onMounted, onWillDestroy, onWillUnmount } from "@odoo/owl";
 import { Dropdown } from "@web/core/dropdown/dropdown";
 import { useDropdownState } from "@web/core/dropdown/dropdown_hooks";
 import { DropdownItem } from "@web/core/dropdown/dropdown_item";
-import { ReadonlyEmbeddedVideoComponent } from "../../core/video/readonly_video";
+import { ReadonlyEmbeddedVideoComponent } from "@html_editor/others/embedded_components/core/video/readonly_video";
+import { PLATFORMS } from "@html_editor/main/media/media_dialog/video_selector";
 
 export class EmbeddedVideoComponent extends ReadonlyEmbeddedVideoComponent {
     static template = "html_editor.EmbeddedVideo";
     static props = {
-        platform: { type: String },
-        videoId: { type: String },
+        // The embedded video can be initialized either with:
+        // the platform and videoId props
+        //  OR
+        // the src prop
+        platform: { type: String, optional: true },
+        videoId: { type: String, optional: true },
+        // 'src' should be 'embedUrl' to keep consistency,
+        // but we can't to ensure retro compatibility.
+        src: { type: String, optional: true },
+        baseUrl: { type: String, optional: true },
         params: { type: Object, optional: true },
         host: { type: HTMLElement },
         createOverlay: { type: Function, optional: true },
@@ -33,6 +36,23 @@ export class EmbeddedVideoComponent extends ReadonlyEmbeddedVideoComponent {
         super.setup();
         this.videoBlock = this.props.host;
         this.state = useEmbeddedState(this.videoBlock);
+
+        if (!this.state.platform || !this.state.videoId) {
+            if (!this.props.src) {
+                console.error("Video data missing to mount video embedded Component");
+                return;
+            }
+            const videoData = this.getVideoDataFromSrc(this.props.src);
+            if (videoData) {
+                this.state.platform = videoData.platform;
+                this.state.videoId = videoData.videoId;
+                this.state.baseUrl = videoData.baseUrl;
+                this.state.params = videoData.options;
+            } else {
+                console.error("Provided src is not a valid url or supported platform");
+            }
+        }
+
         this.dropdown = useDropdownState();
 
         this.videoSettingsOverlay = this.props.createOverlay(VideoSettings, {
@@ -77,13 +97,9 @@ export class EmbeddedVideoComponent extends ReadonlyEmbeddedVideoComponent {
         });
     }
 
-    get url() {
-        const urlParams = Object.fromEntries(
-            Object.entries(this.state.params).filter(([key]) => key !== "isVertical")
-        );
-        return decodeURIComponent(
-            getVideoUrl(this.state.platform, this.state.videoId, urlParams).toString()
-        );
+    get embedUrl() {
+        const platFormClass = PLATFORMS[this.state.platform];
+        return platFormClass.getEmbedUrl(this.state.videoId, this.state.params);
     }
 
     /**
@@ -91,10 +107,12 @@ export class EmbeddedVideoComponent extends ReadonlyEmbeddedVideoComponent {
      * @param {Object} media
      */
     replaceVideo(media) {
-        this.state.videoId = media.videoId;
+        this.state.baseUrl = media.baseUrl;
+        this.state.embedUrl = media.embedUrl;
         this.state.platform = media.platform;
-        this.state.params = media.params;
-        const isVertical = media.params.isVertical;
+        this.state.videoId = media.videoId;
+        this.state.params = media.options;
+        const isVertical = !!media.options?.isVertical;
         if (isVertical) {
             this.videoBlock.dataset.isVertical = "true";
         } else {

addons/html_editor/static/tests/media/paste_video_url.test.js
@@ -1,0 +1,158 @@
+import { describe, expect, mockFetch, test } from "@odoo/hoot";
+import { press, waitFor } from "@odoo/hoot-dom";
+import { animationFrame } from "@odoo/hoot-mock";
+
+import { setupEditor } from "../_helpers/editor";
+import { pasteText, undo } from "../_helpers/user_actions";
+import { expectElementCount } from "../_helpers/ui_expectations";
+import { getContent } from "../_helpers/selection";
+import { cleanLinkArtifacts } from "../_helpers/format";
+import { PLATFORMS } from "@html_editor/main/media/media_dialog/video_selector";
+
+import {
+    EMBEDDED_COMPONENT_PLUGINS,
+    MAIN_PLUGINS,
+    NO_EMBEDDED_COMPONENTS_FALLBACK_PLUGINS,
+} from "@html_editor/plugin_sets";
+import { MAIN_EMBEDDINGS } from "@html_editor/others/embedded_components/embedding_sets";
+const NO_EMBEDDED_COMPONENTS_CONFIG = {
+    Plugins: [...MAIN_PLUGINS, ...NO_EMBEDDED_COMPONENTS_FALLBACK_PLUGINS],
+};
+const EMBEDDED_COMPONENTS_CONFIG = {
+    Plugins: [...MAIN_PLUGINS, ...EMBEDDED_COMPONENT_PLUGINS],
+    resources: { embedded_components: MAIN_EMBEDDINGS },
+};
+
+for (const [platform, platformClass] of Object.entries(PLATFORMS)) {
+    describe(`paste ${platform} url`, () => {
+        const videoUrl = platformClass.exampleUrls.base;
+        test(`should transform a ${platform} URL`, async () => {
+            const { editor } = await setupEditor("<p>ab[]cd</p>", {
+                config: NO_EMBEDDED_COMPONENTS_CONFIG,
+            });
+            mockFetch(() => '{"data": "mockFetch api result data"}');
+            pasteText(editor, videoUrl);
+            await animationFrame();
+            await expectElementCount(".o-we-powerbox", 1);
+            // Validate powerbox on the default first choice
+            await press("Enter");
+            await animationFrame();
+
+            const iframeContainer = await waitFor(`div.media_iframe_video`);
+            const iframe = await waitFor(`div.media_iframe_video iframe`);
+            const iframeContainerData = iframeContainer?.dataset || {};
+
+            expect(iframeContainerData.platform).toBe(platform);
+            expect(iframeContainerData.baseUrl).toBe(videoUrl);
+
+            expect(iframeContainerData.videoId).not.toBeEmpty();
+            expect(videoUrl.includes(iframeContainerData.videoId)).toBe(true);
+            expect(iframeContainerData.embedUrl).not.toBeEmpty();
+            expect("undefined").not.toBe(iframeContainerData.embedUrl);
+            expect(iframeContainerData.embedUrl).toBe(iframe.src);
+        });
+        test(`should transform a ${platform} URL with embeded Components`, async () => {
+            const { editor } = await setupEditor("<p>ab[xx]cd</p>", {
+                config: EMBEDDED_COMPONENTS_CONFIG,
+            });
+            mockFetch(() => '{"data": "mockFetch api result data"}');
+            pasteText(editor, videoUrl);
+            await animationFrame();
+            await expectElementCount(".o-we-powerbox", 1);
+            // Force powerbox validation on the default first choice
+            await press("Enter");
+
+            const iframeContainer = await waitFor(`[data-embedded="video"]`);
+            const iframe = await waitFor(`[data-embedded="video"] iframe`);
+            const iframeContainerData = iframeContainer?.dataset || {};
+
+            const embededProps = JSON.parse(iframeContainerData.embeddedProps);
+            expect(embededProps.platform).toBe(platform);
+            expect(embededProps.baseUrl).toBe(videoUrl);
+
+            expect(iframe.dataset.baseUrl).not.toBeEmpty();
+            expect(iframe.dataset.baseUrl).toBe(embededProps.baseUrl);
+
+            // QWeb templates rendered during unit test store src in data-src
+            const iframeSrc = iframe.getAttribute("src") || iframe.dataset.src || "";
+            expect(iframeSrc).not.toBeEmpty();
+            expect(iframeSrc.includes("https")).toBe(true);
+        });
+    });
+}
+
+describe("generic paste video url behaviors", () => {
+    const videoUrl = "https://youtu.be/dQw4w9WgXcQ";
+    test(`should NOT transform a video URL in a existing link`, async () => {
+        const { el, editor, plugins } = await setupEditor(
+            '<p>a<a href="http://existing.com">b[]c</a>d</p>',
+            { config: EMBEDDED_COMPONENTS_CONFIG }
+        );
+        pasteText(editor, videoUrl);
+        // Ensure the powerbox is active
+        await expectElementCount(".o-we-powerbox", 0);
+        const powerbox = plugins.get("powerbox");
+        expect(powerbox.overlay.isOpen).not.toBe(true);
+        expect(cleanLinkArtifacts(getContent(el))).toBe(
+            `<p>a<a href="http://existing.com">b${videoUrl}[]c</a>d</p>`
+        );
+    });
+    test(`should paste a video URL as a link when selecting alternate powerbox option`, async () => {
+        const { el, editor } = await setupEditor("<p>[]</p>", {
+            config: NO_EMBEDDED_COMPONENTS_CONFIG,
+        });
+        pasteText(editor, videoUrl);
+        await animationFrame();
+        await expectElementCount(".o-we-powerbox", 1);
+        // Pick the second command (Paste as URL)
+        await press("ArrowDown");
+        await press("Enter");
+        expect(cleanLinkArtifacts(getContent(el))).toBe(
+            `<p><a href="${videoUrl}">${videoUrl}</a>[]</p>`
+        );
+    });
+    test("should not revert a history commit when pasting a video URL as a link", async () => {
+        const { el, editor } = await setupEditor("<p>[]</p>", {
+            config: NO_EMBEDDED_COMPONENTS_CONFIG,
+        });
+        // paste text to have a history commit recorded
+        pasteText(editor, "*should not disappear*");
+        pasteText(editor, videoUrl);
+        await animationFrame();
+        await expectElementCount(".o-we-powerbox", 1);
+        // Pick the second command (Paste as URL)
+        await press("ArrowDown");
+        await press("Enter");
+        expect(cleanLinkArtifacts(getContent(el))).toBe(
+            `<p>*should not disappear*<a href="${videoUrl}">${videoUrl}</a>[]</p>`
+        );
+    });
+    test("should restore selection after pasting video URL followed by UNDO (1)", async () => {
+        const { el, editor } = await setupEditor("<p>[abc]</p>", {
+            config: NO_EMBEDDED_COMPONENTS_CONFIG,
+        });
+        pasteText(editor, videoUrl);
+        await animationFrame();
+        await expectElementCount(".o-we-powerbox", 1);
+        // Force powerbox validation on the default first choice
+        await press("Enter");
+        // Undo
+        undo(editor);
+        expect(getContent(el)).toBe("<p>[abc]</p>");
+    });
+
+    test("should restore selection after pasting video URL followed by UNDO (2)", async () => {
+        const { el, editor } = await setupEditor("<p>[abc]</p>", {
+            config: NO_EMBEDDED_COMPONENTS_CONFIG,
+        });
+        pasteText(editor, videoUrl);
+        await animationFrame();
+        await expectElementCount(".o-we-powerbox", 1);
+        // Pick the second command (Paste as URL)
+        await press("ArrowDown");
+        await press("Enter");
+        // Undo
+        undo(editor);
+        expect(getContent(el)).toBe("<p>[abc]</p>");
+    });
+});