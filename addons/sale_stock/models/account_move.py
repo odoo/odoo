122c109a7a8c91720828f2cca419d3c389eac0c0
plha-odoo <plha@odoo.com>
2026-06-03 14:18:50 +0000

    [FIX] sale_stock: take analytic from SO for svl account move
    PR very similar to https://github.com/odoo/odoo/pull/263236 but here
    on the SO side instead of PO
    **Problem:**
    account move created by stock valuation layer
    does not take analytic account from SO
    
    **Steps to reproduce:**
    - make sure you have at least one analytic account
    - create a storable product with categ standard automated
    - set a positive cost and a positive on hand quantity
    - create a SO for 1 quantity
    - on the SO line of the product, in the analytic distribution
    column (might need to be unfiltered) set an analytic account
    - confirm SO and validate delivery
    - click on the valuation smart button and on the book widget
    of the stock valuation layer
    
    **Current behavior:**
    the account move lines have no analytic distribution
    
    **Expected behavior:**
    The account move lines should inherit the analytic account
    from the sale order line like it's the case for the invoice.
    
    For the analytic distribution of the Invoice, the selection is :
    1) take analytic distribution from SO if one
    2) if not, take from distribution model if there is one
    3) empty
    
    Currently for the account move lines of the svl the selection is:
    1) take from distribution model if there is one
    2) empty
    
    But we should use same selection as for the invoice
    
    **Cause of the issue:**
    When setting the analytic distribution we first
    try to use the one from PO/SO by calling
    _related_analytic_distribution()
    https://github.com/odoo/odoo/blob/4cc1e6884be673523f768d5ec471a1ffa19c5fb4/addons/account/models/account_move_line.py#L1157
    But since the account move lines have no sale_line_ids
    no analytic distribution will be returned
    https://github.com/odoo/odoo/blob/261b15953ca89657644f52d1cb9ecda6e3b686c5/addons/sale/models/account_move_line.py#L41-L46
    
    opw-6022695
    
    X-original-commit: 466c7774c2f64f010ce37d55b1f36b10e94dc1cf

--- addons/sale_stock/models/account_move.py
+++ addons/sale_stock/models/account_move.py
@@ -190,4 +190,99 @@ class AccountMoveLine(models.Model):
                 lambda l: l.display_type == 'cogs' and l.product_id == self.product_id and
                 l.product_uom_id == self.product_uom_id and l.price_unit >= 0
             )
+<<<<<<< f8741728294a5147c7d2427d9997738096386262
         return original_lines
+||||||| 97b5a41366ca5d9d203668726225bb2f2ce4fed5
+        if so_line:
+            is_line_reversing = False
+            if self.move_id.move_type == 'out_refund' and not move_is_downpayment:
+                is_line_reversing = True
+            qty_to_invoice = self.product_uom_id._compute_quantity(self.quantity, self.product_id.uom_id)
+            if self.move_id.move_type == 'out_refund' and move_is_downpayment:
+                qty_to_invoice = -qty_to_invoice
+            account_moves = so_line.invoice_lines.move_id.filtered(lambda m: m.state == 'posted' and bool(m.reversed_entry_id) == is_line_reversing)
+
+            posted_cogs = self.env['account.move.line'].search([
+                ('move_id', 'in', account_moves.ids),
+                ('display_type', '=', 'cogs'),
+                ('product_id', '=', self.product_id.id),
+                ('balance', '>', 0),
+            ])
+            posted_cogs = posted_cogs.filtered(lambda l: so_line in l.cogs_origin_id.sale_line_ids)
+            qty_invoiced = 0
+            product_uom = self.product_id.uom_id
+            for line in posted_cogs:
+                if float_compare(line.quantity, 0, precision_rounding=product_uom.rounding) and line.move_id.move_type == 'out_refund' and any(line.move_id.invoice_line_ids.sale_line_ids.mapped('is_downpayment')):
+                    qty_invoiced += line.product_uom_id._compute_quantity(abs(line.quantity), line.product_id.uom_id)
+                else:
+                    qty_invoiced += line.product_uom_id._compute_quantity(line.quantity, line.product_id.uom_id)
+            value_invoiced = sum(posted_cogs.mapped('balance'))
+            reversal_moves = self.env['account.move']._search([('reversed_entry_id', 'in', posted_cogs.move_id.ids)])
+            reversal_cogs = self.env['account.move.line'].search([
+                ('move_id', 'in', reversal_moves),
+                ('display_type', '=', 'cogs'),
+                ('product_id', '=', self.product_id.id),
+                ('balance', '>', 0)
+            ])
+            for line in reversal_cogs:
+                if float_compare(line.quantity, 0, precision_rounding=product_uom.rounding) and line.move_id.move_type == 'out_refund' and any(line.move_id.invoice_line_ids.sale_line_ids.mapped('is_downpayment')):
+                    qty_invoiced -= line.product_uom_id._compute_quantity(abs(line.quantity), line.product_id.uom_id)
+                else:
+                    qty_invoiced -= line.product_uom_id._compute_quantity(line.quantity, line.product_id.uom_id)
+            value_invoiced -= sum(reversal_cogs.mapped('balance'))
+
+            product = self.product_id.with_company(self.company_id).with_context(value_invoiced=value_invoiced)
+            average_price_unit = product._compute_average_price(qty_invoiced, qty_to_invoice, so_line.move_ids, is_returned=is_line_reversing)
+            price_unit = self.product_id.uom_id.with_company(self.company_id)._compute_price(average_price_unit, self.product_uom_id)
+        return price_unit
+=======
+        if so_line:
+            is_line_reversing = False
+            if self.move_id.move_type == 'out_refund' and not move_is_downpayment:
+                is_line_reversing = True
+            qty_to_invoice = self.product_uom_id._compute_quantity(self.quantity, self.product_id.uom_id)
+            if self.move_id.move_type == 'out_refund' and move_is_downpayment:
+                qty_to_invoice = -qty_to_invoice
+            account_moves = so_line.invoice_lines.move_id.filtered(lambda m: m.state == 'posted' and bool(m.reversed_entry_id) == is_line_reversing)
+
+            posted_cogs = self.env['account.move.line'].search([
+                ('move_id', 'in', account_moves.ids),
+                ('display_type', '=', 'cogs'),
+                ('product_id', '=', self.product_id.id),
+                ('balance', '>', 0),
+            ])
+            posted_cogs = posted_cogs.filtered(lambda l: so_line in l.cogs_origin_id.sale_line_ids)
+            qty_invoiced = 0
+            product_uom = self.product_id.uom_id
+            for line in posted_cogs:
+                if float_compare(line.quantity, 0, precision_rounding=product_uom.rounding) and line.move_id.move_type == 'out_refund' and any(line.move_id.invoice_line_ids.sale_line_ids.mapped('is_downpayment')):
+                    qty_invoiced += line.product_uom_id._compute_quantity(abs(line.quantity), line.product_id.uom_id)
+                else:
+                    qty_invoiced += line.product_uom_id._compute_quantity(line.quantity, line.product_id.uom_id)
+            value_invoiced = sum(posted_cogs.mapped('balance'))
+            reversal_moves = self.env['account.move']._search([('reversed_entry_id', 'in', posted_cogs.move_id.ids)])
+            reversal_cogs = self.env['account.move.line'].search([
+                ('move_id', 'in', reversal_moves),
+                ('display_type', '=', 'cogs'),
+                ('product_id', '=', self.product_id.id),
+                ('balance', '>', 0)
+            ])
+            for line in reversal_cogs:
+                if float_compare(line.quantity, 0, precision_rounding=product_uom.rounding) and line.move_id.move_type == 'out_refund' and any(line.move_id.invoice_line_ids.sale_line_ids.mapped('is_downpayment')):
+                    qty_invoiced -= line.product_uom_id._compute_quantity(abs(line.quantity), line.product_id.uom_id)
+                else:
+                    qty_invoiced -= line.product_uom_id._compute_quantity(line.quantity, line.product_id.uom_id)
+            value_invoiced -= sum(reversal_cogs.mapped('balance'))
+
+            product = self.product_id.with_company(self.company_id).with_context(value_invoiced=value_invoiced)
+            average_price_unit = product._compute_average_price(qty_invoiced, qty_to_invoice, so_line.move_ids, is_returned=is_line_reversing)
+            price_unit = self.product_id.uom_id.with_company(self.company_id)._compute_price(average_price_unit, self.product_uom_id)
+        return price_unit
+
+    def _related_analytic_distribution(self):
+        # EXTENDS 'account'
+        vals = super()._related_analytic_distribution()
+        if not self.sale_line_ids and not self.analytic_distribution and self.move_id.stock_move_id.sale_line_id:
+            vals |= self.move_id.stock_move_id.sale_line_id.analytic_distribution or {}
+        return vals
+>>>>>>> f925de6f7615b90fcdcd1a999134e835f3bd8b60
