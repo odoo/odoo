de151acc708b7b9be32eae4c32d0b92126c7e7fe
Daniel Duato Catalán <dadu@odoo.com>
2026-06-03 13:20:59 +0000

    [FIX] website: store filenames of binary fields uploaded through a form
    Since [1], studio binary fields uploaded through a form store their filename.
    
    Due to the condition of [1], this behaviour is restricted to manual fields, which limits the usage of those fields in standard and is particularly problematic when Saas modules that use this feature are migrated to Python.
    
    Given that a more appropriate condition has already been added in [2], it should no longer be necessary to restrict this feature to manual fields.
    
    This commit removes that restriction to allow standard binary fields to store their filename when uploaded through a form.
    
    [1] https://github.com/odoo/odoo/commit/0e2f3b144581c47d25a99cecdd7e058a3d55bcc3
    [2] https://github.com/odoo/odoo/commit/1bcab2f42eebf98127416e54f31cd6e351938b7f
    
    closes odoo/odoo#270723
    
    X-original-commit: 6feedc18b7873bfde62047ece1a5de882319bc21
    Signed-off-by: Krzysztof Magusiak (krma) <krma@odoo.com>

--- addons/website/controllers/form.py
+++ addons/website/controllers/form.py
@@ -183,7 +183,7 @@ class WebsiteForm(http.Controller):
                 if field_name in authorized_fields and authorized_fields[field_name]['type'] == 'binary':
                     data['record'][field_name] = BinaryBytes(field_value.read())
                     field_value.stream.seek(0)  # do not consume value forever
-                    if authorized_fields[field_name]['manual'] and field_name + "_filename" in dest_model:
+                    if field_name + "_filename" in dest_model:
                         data['record'][field_name + "_filename"] = field_value.filename
                 else:
                     field_value.field_name = field_name
