2d1b8e6453a7bb6d81da1cc23e175a6fa6a49a49
Jean Schoenlaub <jesc@odoo.com>
2026-06-03 10:55:46 +0200

    [REF] web: Replace useLayoutEffect
    Replaces useLayoutEffect with onMounted + onPatched
    
    closes odoo/odoo#267715
    
    Why: UseLayoutEffect is deprecated in OWL3
    Related: odoo/enterprise#119119
    Signed-off-by: Géry Debongnie <ged@odoo.com>

--- addons/web/static/src/views/graph/graph_renderer.js
+++ addons/web/static/src/views/graph/graph_renderer.js
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useRef } from "@web/owl2/utils";
+import { useRef } from "@web/owl2/utils";
 import { _t } from "@web/core/l10n/translation";
 import {
     getBorderWhite,
@@ -16,7 +16,7 @@ import { loadBundle } from "@web/core/assets";
 import { renderToMarkup } from "@web/core/utils/render";
 import { useService } from "@web/core/utils/hooks";
 
-import { Component, onWillUnmount, onWillStart, markup } from "@odoo/owl";
+import { Component, onWillUnmount, onWillStart, markup, onMounted, onPatched } from "@odoo/owl";
 import { Dropdown } from "@web/core/dropdown/dropdown";
 import { DropdownItem } from "@web/core/dropdown/dropdown_item";
 import { cookie } from "@web/core/browser/cookie";
@@ -136,7 +136,8 @@ export class GraphRenderer extends Component {
             await loadBundle("web.chartjs_lib");
         });
 
-        useLayoutEffect(() => this.renderChart());
+        onMounted(() => this.renderChart());
+        onPatched(() => this.renderChart());
         onWillUnmount(this.onWillUnmount);
     }
 
