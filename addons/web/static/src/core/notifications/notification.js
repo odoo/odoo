067ee98a93c0917da69ad1253cd2a0861d011f1e
Julien Coppin (jucop) <jucop@odoo.com>
2026-06-04 13:57:14 +0200

    [IMP] web: transform notificationTimer into useTimer
    Replace the startNotificationTimer() class method with a
    useTimer(duration) composable hook.
    
    The old implementation drove the progress bar by directly mutating
    style.width inside a raw requestAnimationFrame loop, relied on
    luxon.DateTime for timestamps, and had no cleanup path.

--- addons/web/static/src/core/notifications/notification.js
+++ addons/web/static/src/core/notifications/notification.js
@@ -1,5 +1,6 @@
-import { Component, onMounted, signal, applyDefaults, useProps } from "@odoo/owl";
+import { Component, useEffect, useProps, signal, applyDefaults } from "@odoo/owl";
 import { NotificationSchema } from "./notification_plugin";
+import { useTimer } from "@web/core/utils/timing";
 
 export class Notification extends Component {
     static template = "web.NotificationWowl";
@@ -8,44 +9,31 @@ export class Notification extends Component {
     autocloseProgress = signal.ref();
 
     setup() {
-        onMounted(() => this.startNotificationTimer());
+        if (!this.props.sticky && this.props.autocloseDelay > 0) {
+            this.timer = useTimer(this.props.autocloseDelay);
+
+            useEffect(() => {
+                if (this.timer.progress() >= 1) {
+                    this.close();
+                } else if (this.autocloseProgress()) {
+                    this.autocloseProgress().style.width = `${(1 - this.timer.progress()) * 100}%`;
+                }
+            });
+        }
     }
 
     freeze() {
-        this.startedTimestamp = false;
+        this.timer?.stop();
         if (this.autocloseProgress()) {
             this.autocloseProgress().style.width = 0;
         }
     }
 
     refresh() {
-        this.startNotificationTimer();
+        this.timer?.reset();
     }
 
     close() {
         this.props.close();
-    }
-
-    startNotificationTimer() {
-        if (this.props.sticky) {
-            return;
-        }
-        this.startedTimestamp = luxon.DateTime.now().ts;
-
-        const cb = () => {
-            if (this.startedTimestamp) {
-                const currentProgress =
-                    (luxon.DateTime.now().ts - this.startedTimestamp) / this.props.autocloseDelay;
-                if (currentProgress > 1) {
-                    this.close();
-                    return;
-                }
-                if (this.autocloseProgress()) {
-                    this.autocloseProgress().style.width = `${(1 - currentProgress) * 100}%`;
-                }
-                requestAnimationFrame(cb);
-            }
-        };
-        cb();
     }
 }
