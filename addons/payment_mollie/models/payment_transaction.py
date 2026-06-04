9d5de14fe156a3c4bdcf5a4ee93e104921c9ec01
Max Whale <mawh@odoo.com>
2026-06-04 08:46:49 +0200

    [FIX] payment_mollie: payment fails with incomplete billing address
    Steps to reproduce:
    1. Setup a Mollie online payment method.
    2. Make a payment with a customer that has an incomplete* billing
       address.
    
    Expected behaviour: The payment request is initiated.
    
    Actual behaviour: Mollie rejects the payment request.
    
    *: Mollie will either accept no billing address, or a full address (must
    include street, postal code, city and country). If only some of these
    fields are present, Mollie will reject the payment request.
    
    closes odoo/odoo#268188
    
    Signed-off-by: Max Whale (mawh) <mawh@odoo.com>

--- addons/payment_mollie/models/payment_transaction.py
+++ addons/payment_mollie/models/payment_transaction.py
@@ -99,15 +99,19 @@ class PaymentTransaction(models.Model):
         :rtype: dict
         """
         given_name, family_name = payment_utils.split_partner_name(self.partner_name)
-        return {
+        billing_address = {
             "givenName": given_name,
             "familyName": family_name,
-            "streetAndNumber": self.partner_address or "",
-            "postalCode": self.partner_zip or "",
-            "city": self.partner_city or "",
-            "country": self.partner_country_id.code or "",
             "email": self.partner_email or "",
         }
+        if all((self.partner_address, self.partner_zip, self.partner_city, self.partner_country_id)):
+            billing_address |= {
+                "streetAndNumber": self.partner_address,
+                "postalCode": self.partner_zip,
+                "city": self.partner_city,
+                "country": self.partner_country_id.code,
+            }
+        return billing_address
 
     @api.model
     def _extract_reference(self, provider_code, payment_data):
