77cda8dd500c7f24bd0ee98a0e6b7fadc6be5c31
plha-odoo <plha@odoo.com>
2026-06-04 08:35:51 +0000

    [FIX] l10n_lu: correct valuation account lux
    **Problem:**
    Valuation account for luxembourg is currently 60761 Merchandise
    which is incorrect because it's an expense account.
    We should rather use a current asset account like
    301 Inventories of raw materials
    
    **Steps to reproduce on a fresh db:**
    - create a new db with modules stock_account and accountant
    (without demo data)
    - On the 'fiscal localization setting' set the package
    as 'Luxembourg' and save
    - ativate the automatic accounting setting
    
    **Current Behavior:**
    The 'stock valuation account' appearing below
    the automatic accounting setting is :
    60761 Merchandise
    
    **Expected behaviour:**
    It should be 301 Inventories of raw materials
    
    closes odoo/odoo#270048
    
    X-original-commit: 24a71ac96e38d57da57fbad9bc886e95074bd5af
    Signed-off-by: Yolann Sabaux (yosa) <yosa@odoo.com>
    Signed-off-by: Pierre-Louis Hance (plha) <plha@odoo.com>

--- addons/l10n_lu/models/template_lu.py
+++ addons/l10n_lu/models/template_lu.py
@@ -11,7 +11,7 @@ class AccountChartTemplate(models.AbstractModel):
         return {
             'property_account_receivable_id': 'lu_2011_account_4011',
             'property_account_payable_id': 'lu_2011_account_44111',
-            'property_stock_valuation_account_id': 'lu_2020_account_60761',
+            'property_stock_valuation_account_id': 'lu_2011_account_301',
             'code_digits': '6',
         }
 
