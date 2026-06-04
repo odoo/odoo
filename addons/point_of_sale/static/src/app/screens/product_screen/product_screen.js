d703701462e5cf8c9c759560da62bf0c9e1d0f65
abhk-odoo <abhk@odoo.com>
2026-06-04 11:03:11 +0530

    [IMP] pos_{ ,self_order}: handle snoozed products in pos and self-order flows
    Before this commit:
    - Snoozed products could still be selected from the product screen and combo
    configurator without any warning.
    - Users could add snoozed products to the order by mistake.
    - In self-order, snoozed products were still selectable in combo items.
    - Snooze checking was only based on product template id. For product variants
      (`product.product`), the `product_tmpl_id` was not checked.
    
    After this commit:
    - Add a `canAddProductToCurrentOrder` method to show a warning before adding
    a snoozed product.
    - Apply this check in the product screen and combo configurator.
    - Improve snooze detection by supporting both `product.template`
    and `product.product` (via `product_tmpl_id`).
    - In self-order, If a product is snoozed, show it as 'Out of stock'.
    - If a product is not available in self-order, do not show it in the list.
    - Fix the radio input attribute in the snooze dialog.
    
    Task:6012412
    
    X-original-commit: a24ebacc3dd0f9befea48883b14c7d20a9d2bc4e

--- addons/point_of_sale/static/src/app/screens/product_screen/product_screen.js
+++ addons/point_of_sale/static/src/app/screens/product_screen/product_screen.js
@@ -339,6 +339,10 @@ export class ProductScreen extends Component {
         }
         this.sound.play("beep");
 
+        if (!(await this.pos.canAddProductToCurrentOrder(product.product_tmpl_id))) {
+            return;
+        }
+
         await this.pos.addLineToCurrentOrder(
             { product_id: product, product_tmpl_id: product.product_tmpl_id },
             { code },
@@ -392,6 +396,11 @@ export class ProductScreen extends Component {
             return;
         }
         this.sound.play("beep");
+
+        if (!(await this.pos.canAddProductToCurrentOrder(product.product_tmpl_id))) {
+            return;
+        }
+
         const vals = { product_id: product, product_tmpl_id: product.product_tmpl_id };
         if (
             qty &&
@@ -424,6 +433,9 @@ export class ProductScreen extends Component {
     }
 
     async addProductToOrder(product) {
+        if (!(await this.pos.canAddProductToCurrentOrder(product))) {
+            return;
+        }
         const options = {};
         if (this.searchWord && product.isConfigurable()) {
             const barcode = this.searchWord;
