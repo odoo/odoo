54c02338b0b973b73ac177a37cd1eb6db13cb1d0
Mahamadasif Ansari <maan@odoo.com>
2026-06-04 19:02:31 +0530

    [FIX] payment_demo: set Demo provider to Test when journal_id is set
    Currently an exception is generated when the scheduled action
    `Payment: Post-process transactions` tries to create a `account.payment`
    record without `journal_id` as the following step:
    
    - Create a new DB without demo data and install `website_sale` module
    - Delete `Bank` Journal from Invoicing
    - Install `Demo` payment provider
    - Go to the website shop and create an order with the demo payment provider
    - Error will occur in the log when `Payment: Post-process transactions`
      schedule is running
    
    The issue occurs because the `Demo` payment provider is automatically
    created from the demo XML with its state set to `Test`. The field
    `journal_id (Payment Journal)` is mandatory when the provider is in the `Test`
    or `Enabled` state. However, this field cannot be assigned because the
    associated `Bank Journal` has been deleted. As a result, an error is raised
    during `payment post-processing` when the system attempts to create an
    `Account Payment` record with a missing required field.
    
    This commit fixes the above issue by not automatically setting the
    `Demo` payment provider state as `Test` during creation; instead, it
    sets the state through the `post_init_hook`. It will make sure the
    `Demo` provider is set to `Test` state when `journal_id` is set.
    
    sentry-7526177028

--- addons/payment_demo/__init__.py
+++ addons/payment_demo/__init__.py
@@ -9,6 +9,10 @@ from odoo.addons.payment import setup_provider, reset_payment_provider
 def post_init_hook(env):
     setup_provider(env, 'demo')
 
+    demo_provider = env.ref('payment_demo.payment_provider_demo', raise_if_not_found=False)
+    if demo_provider and demo_provider.journal_id:
+        demo_provider.write({'state': 'test', 'is_published': True})
+
 
 def uninstall_hook(env):
     reset_payment_provider(env, 'demo')
