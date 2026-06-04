11138ef81916b388d02df8af2a4c0d50c5232045
Max Whale <mawh@odoo.com>
2026-06-04 06:46:49 +0000

    [FIX] payment_mollie: payment fails with incomplete billing address
    Steps to reproduce:
    1. Setup a Mollie online payment method.
    2. Make a payment with a customer that has an incomplete* billing
       address.
    
    Expected behaviour: The payment request is initiated.
    
    Actual behaviour: Mollie rejects the payment request.
    
    *: Mollie will either accept no billing address, or a full address (must
    include street, postal code, city and country). If only some of these
    fields are present, Mollie will reject the payment request.
    
    X-original-commit: 9d5de14fe156a3c4bdcf5a4ee93e104921c9ec01

--- addons/payment_mollie/tests/test_mollie.py
+++ addons/payment_mollie/tests/test_mollie.py
@@ -90,6 +90,19 @@ class MollieTest(MollieCommon, PaymentHttpCommon):
             },
         )
 
+    def test_incomplete_billing_address_not_sent(self):
+        self.default_partner.zip = ''
+        tx = self._create_transaction(flow='redirect')
+
+        payload = tx._mollie_prepare_payment_request_payload()
+        expected_billing_address = {
+            'givenName': 'Norbert',
+            'familyName': 'Buyer',
+            'email': 'norbert.buyer@example.com',
+        }
+
+        self.assertDictEqual(payload['billingAddress'], expected_billing_address)
+
     @mute_logger(
         "odoo.addons.payment_mollie.controllers.main",
         "odoo.addons.payment_mollie.models.payment_transaction",
