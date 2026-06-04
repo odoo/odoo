1887fda9fab43a25a9ab39aa36a729ad9e17e7b5
Loukas Wets (lowe) <lowe@odoo.com>
2026-06-04 13:45:01 +0000

    [FIX] pos_cashmatic: fix issues for the certification
    First issue:
    While paying if a user takes more than 15min the token is revoked.
    
    Second issue:
    When cancelling a payment where a user has already inserted money and
    there is an issue with giving back the money, no popup was shown on the
    PoS.
    
    Third issue:
    Fetch took too long when the device was not reacheable.
    
    Fourth issue:
    Cancel payment when Force Done
    
    X-original-commit: 65702b4446ea4b6069103b6bcda9d79df4aba310

---
+++ addons/pos_cashmatic/static/src/app/screens/payment_screen/payment_screen.js
@@ -0,0 +1,12 @@
+import { PaymentScreen } from "@point_of_sale/app/screens/payment_screen/payment_screen";
+import { patch } from "@web/core/utils/patch";
+
+patch(PaymentScreen.prototype, {
+    async sendForceDone(line) {
+        const paymentInterface = line.payment_method_id.payment_terminal;
+        if (paymentInterface && line.payment_method_id.payment_method_type === "cashmatic") {
+            paymentInterface.cashmaticService.cancelCurrentPayment();
+        }
+        return super.sendForceDone(...arguments);
+    },
+});
