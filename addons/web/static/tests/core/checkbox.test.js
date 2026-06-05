4691fd7a9bd6e56944e92d243c9dba4566fe9460
Jean Schoenlaub <jesc@odoo.com>
2026-06-05 11:03:32 +0200

    [REF] web: refactor indeterminate checkbox JS
    The indeterminate checkbox is a standard HTML API implemented in Odoo in
    several ways, currently not working out-of-the-box with enterprise.
    
    Problem 1: the original implementation via JS (eg. `el.indeterminate = true`
    is a workaround for https://github.com/odoo/owl/issues/713, since been fixed.
    
    Problem 2: the intended API (`t-att-indeterminate` / `<Checkbox
    indeterminate="...">`) doesn't work with enterprise installed because
    `web_enterprise/static/src/scss/bootstrap_overridden.scss` sets
    `$component-active-color: unset !default;` & Bootstrap's uses
    `$form-check-input-indeterminate-color: $component-active-color`
    therefore resolves to `unset`, making the dash invisible.
    This was fixed in studio only (odoo/enterprise#81372) but should apply globally.
    
    Fix:
    - Refactor the JS workaround to use `t-att-indeterminate`.
    - Fix `$form-check-input-indeterminate-color: $o-component-active-color !default`
    in `bootstrap_overriden` (similar to what was done with `input-checked`
    --> also make the border and bg match the input checked style
    - Finally keep the studio workaround with
    `background-image: url("/web_studio/static/src/img/ui/checkbox_indeterminate.svg");`
    is kept because studio uses a different color scheme

--- addons/web/static/tests/core/checkbox.test.js
+++ addons/web/static/tests/core/checkbox.test.js
@@ -1,5 +1,5 @@
 import { expect, test } from "@odoo/hoot";
-import { check, uncheck } from "@odoo/hoot-dom";
+import { check, queryFirst, uncheck } from "@odoo/hoot-dom";
 import { Component, xml, proxy } from "@odoo/owl";
 import { contains, defineParams, mountWithCleanup } from "@web/../tests/web_test_helpers";
 
@@ -145,4 +145,47 @@ test("checkbox with props indeterminate", async () => {
 
     expect(`.o-checkbox input`).toHaveCount(1);
     expect(`.o-checkbox input`).toBeChecked({ indeterminate: true });
+});
+
+test("indeterminate style attribute renders a visible dash", async () => {
+    class Parent extends Component {
+        static components = { CheckBox };
+        static props = {};
+        static template = xml`
+            <CheckBox indeterminate="true" className="'cb-indeterminate'" />
+            <CheckBox value="true" className="'cb-checked'" />
+            <input type="checkbox" t-att-indeterminate="true" class="form-check-input cb-raw-indeterminate"/>
+            <input type="checkbox" checked="1" class="form-check-input cb-raw-checked"/>
+        `;
+    }
+
+    await mountWithCleanup(Parent);
+
+    const indeterminate = queryFirst(".cb-indeterminate input");
+    const checked = queryFirst(".cb-checked input");
+    const rawIndeterminate = queryFirst(".cb-raw-indeterminate");
+    const rawChecked = queryFirst(".cb-raw-checked");
+
+    const getStrokeColor = (el) =>
+        getComputedStyle(el)
+            .getPropertyValue("--form-check-bg-image")
+            .match(/stroke='(%23[^']+)'/)?.[1];
+
+    // --- CheckBox component ---
+    expect(indeterminate).toBeChecked({ indeterminate: true });
+    expect(indeterminate.indeterminate).toBe(true);
+
+    expect(getComputedStyle(indeterminate).backgroundColor).toBe(
+        getComputedStyle(checked).backgroundColor
+    );
+    expect(getStrokeColor(indeterminate)).toBe(getStrokeColor(checked));
+
+    // --- Input element ---
+    expect(rawIndeterminate.indeterminate).toBe(true);
+    expect(rawIndeterminate.checked).toBe(false);
+    // Check again color of input match when indeterminate and checked
+    expect(getComputedStyle(rawIndeterminate).backgroundColor).toBe(
+        getComputedStyle(rawChecked).backgroundColor
+    );
+    expect(getStrokeColor(rawIndeterminate)).toBe(getStrokeColor(rawChecked));
 });
