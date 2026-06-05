b12c68b25c64c48a2c4dee5f9a1fb2c7348d1b86
Leila Kerroum Balaguer <leker@odoo.com>
2026-06-05 09:18:05 +0200

    [IMP] sale_management, tests: remove template group
    Following the previous commit, we decided that managing templates via a configuration setting was unnecessary. If users do not need the feature, they can simply choose not to use it.
    
    This commit, makes the quotation template option always available by removing the `sales_management.group_sale_order_template` group and all its references.
    
    task-5260606
    
    closes odoo/odoo#252541
    
    Related: odoo/enterprise#110832
    Related: odoo/upgrade#10415
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/auth_totp/tests/test_totp.py
+++ addons/auth_totp/tests/test_totp.py
@@ -120,11 +120,6 @@ class TestTOTP(TestTOTPMixin, HttpCase):
 
     def test_totp_administration(self):
         self.start_tour('/odoo', 'totp_tour_setup', login='test_user')
-        # If not enabled (like in demo data), landing on res.config will try
-        # to disable module_sale_quotation_builder and raise an issue
-        group_order_template = self.env.ref('sale_management.group_sale_order_template', raise_if_not_found=False)
-        if group_order_template:
-            self.env.ref('base.group_user').write({"implied_ids": [(4, group_order_template.id)]})
         self.start_tour('/odoo', 'totp_admin_disables', login='admin')
         self.start_tour('/', 'totp_login_disabled', login=None)
 
--- addons/test_website_modules/tests/test_configurator.py
+++ addons/test_website_modules/tests/test_configurator.py
@@ -9,9 +9,4 @@ from odoo.addons.website.tests.test_configurator import TestConfiguratorCommon
 class TestConfigurator(TestConfiguratorCommon):
 
     def test_01_configurator_flow(self):
-        # If not enabled (like in demo data), landing on res.config will try
-        # to disable module_sale_quotation_builder and raise an issue
-        group_order_template = self.env.ref('sale_management.group_sale_order_template', raise_if_not_found=False)
-        if group_order_template:
-            self.env.ref('base.group_user').write({"implied_ids": [(4, group_order_template.id)]})
         self.start_tour('/odoo/action-website.action_website_configuration', 'configurator_flow', login="admin")
