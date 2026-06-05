c8e615838c778422ffd5a2b5e94f8536d67b422d
rpka-odoo <rpka@odoo.com>
2026-06-05 06:01:45 +0000

    [FIX] pos_self_order: preserve self ordering table reference on empty table_id
    Steps to reproduce:
    - Enable QR Menu & Ordering in POS
    - Enable Service at Table
    - Create a self-order from a table QR
    - Validate/pay the order from the POS
    
    Cause:
    The write override unconditionally  `table_id` to
    `self_ordering_table_id`, even when `table_id` was falsy, clearing the
    original self-order table link.
    
    Fix:
    Only update `self_ordering_table_id` when `table_id` is explicitly set
    and truthy.
    
    Task-6272642
    
    closes odoo/odoo#273833
    
    X-original-commit: 8ebb29d30c4113b26b2c39ae4b4bf98a5a0a4cd6
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Rohit Pravinbhai Kalsariya (rpka) <rpka@odoo.com>

--- addons/pos_self_order/static/tests/pos/pos_self_order_tour.js
+++ addons/pos_self_order/static/tests/pos/pos_self_order_tour.js
@@ -1,4 +1,5 @@
 import * as ProductScreenPos from "@point_of_sale/../tests/pos/tours/utils/product_screen_util";
+import * as PaymentScreen from "@point_of_sale/../tests/pos/tours/utils/payment_screen_util";
 import * as ProductScreenResto from "@pos_restaurant/../tests/tours/utils/product_screen_util";
 const ProductScreen = { ...ProductScreenPos, ...ProductScreenResto };
 import * as TicketScreen from "@point_of_sale/../tests/pos/tours/utils/ticket_screen_util";
@@ -16,6 +17,8 @@ registry.category("web_tour.tours").add("test_pos_self_order_preparation_changes
             TicketScreen.loadSelectedOrder(),
             ProductScreen.isShown(),
             ProductScreen.orderlinesHaveNoChange(),
+            ProductScreen.clickPayButton(),
+            PaymentScreen.clickValidate(),
         ].flat(),
 });
 
