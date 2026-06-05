f361c84261324a50afca0cf61cfb0b7b7f767bcd
Sarah Bellefroid <sbel@odoo.com>
2026-06-05 11:37:02 +0000

    [FIX] point_of_sale: fix multicompany issue in test setup
    Making the test only depend on one class setup to avoid
    potential (already present) multicompany issues. In this case
    the env.user came from one setup class but was incompatible to
    use during the setup of the second class that was creating
    records for another company. By making the test only depend
    on one of the tests we'll avoid this issue.
    
    runbot-939375
    
    closes odoo/odoo#272458
    
    X-original-commit: a7f5275d43b278b57d0e262397a1808797a5d8bf
    Signed-off-by: Sarah Bellefroid (sbel) <sbel@odoo.com>

--- addons/point_of_sale/tests/test_digest.py
+++ addons/point_of_sale/tests/test_digest.py
@@ -1,18 +1,31 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo.addons.digest.tests.common import TestDigestCommon
 from odoo.addons.point_of_sale.tests.common import CommonPosTest
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 from odoo.tools import mute_logger
 from odoo.tests import tagged
 
 
 @tagged('post_install', '-at_install')
-class TestAccountDigest(TestDigestCommon, CommonPosTest):
+class TestAccountDigest(CommonPosTest):
 
     @classmethod
     @mute_logger('odoo.models.unlink')
     def setUpClass(cls):
         super().setUpClass()
+        context = {
+            'start_datetime': datetime.now() - relativedelta(days=1),
+            'end_datetime': datetime.now() + relativedelta(days=1),
+        }
+
+        cls.digest = cls.env['digest.digest'].with_context(context).create([{
+            'name': 'Digest 1',
+            'company_id': cls.env.company.id,
+            'kpi_mail_message_total': True,
+            'kpi_res_users_connected': True,
+            'periodicity': 'daily',
+        }])
 
     def test_kpi_invoiced_pos_orders_counted(self):
         order_data = {
@@ -31,4 +44,4 @@ class TestAccountDigest(TestDigestCommon, CommonPosTest):
         self.create_backend_pos_order({**order_data, 'order_data': {'to_invoice': True, 'partner_id': self.partner.id}})
         current_session.close_session_from_ui()
 
-        self.assertEqual(self.digest_1.kpi_pos_total_value, 20.0)
+        self.assertEqual(self.digest.kpi_pos_total_value, 20.0)
