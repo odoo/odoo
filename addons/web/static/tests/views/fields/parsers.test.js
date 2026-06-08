01d78788758cdedf9793a3fcc2a58a86febebdbb
Mathieu Coutant <mcou@odoo.com>
2026-06-08 15:00:40 +0000

    [FIX] web: remove test depending on chrome version
    Cause:
    ----------------------------------------
    Commit 121806b57816d0fbd48b5538d2a8beae35905d99 added a test verifying that the duration in German are correctly recognized.
    In the test, "2 Std. 30 Min. 45 Sek.", was supposed to be recognized as 2.5125 hours.
    But on some browsers version it fails as in the latest Chrome version.
    
    This is because we use `DurationFormat()` to get the localized units. This method is supported by all browsers and will use the browser's data to get the translations. This data comes from [Unicode CLDR](https://cldr.unicode.org/) which is updated regularly.
    
    We would need to change the test depending on the Chrome version, so we just delete it.
    It appears that for German hours the CLDR definitions have historically shifted between two distinct representations: "Std." and "h"
    
    As an example:
    - CLDR 47:
    
    https://github.com/unicode-org/cldr-json/blame/16f6b8578ba5fe98959034706f337674f816fc3f/cldr-json/cldr-units-full/main/de/units.json#L3526-L3527
    
    - CLDR 48:
    
    https://github.com/unicode-org/cldr-json/blame/4d06be52b51bb2f75688d0abe55c52a66afed790/cldr-json/cldr-units-full/main/de/units.json#L3916-L3917
    
    So the test fails in CLDR 48 but succeeds in CLDR 47.
    
    As the latest version of `ICU` (used by browsers) updates its dependency to CLDR 48 ([src](https://unicode-org.github.io/icu/download/78.html)) this explains why the latest Chrome version makes the test fail.
    
    runbot-939543
    
    X-original-commit: 3e6a7f1c81986265b260a9e7399bea4d38ec07d0

--- addons/web/static/tests/views/fields/parsers.test.js
+++ addons/web/static/tests/views/fields/parsers.test.js
@@ -80,10 +80,6 @@ test("parseFloatTime", () => {
     expect(parseFloatTime("٢س ٣٠د ٤٥ث")).toBe(2.5125);
 
     clearMemoizeCaches();
-    localization.locale = "de-DE";
-    expect(parseFloatTime("2 Std. 30 Min. 45 Sek.")).toBe(2.5125);
-
-    clearMemoizeCaches();
     localization.thousandsSep = ".";
     localization.decimalPoint = ",";
     expect(parseFloatTime("0,5")).toBe(0.5);
