98917eb248c93648e2bab99a281dedad797d7788
adch-odoo <adch@odoo.com>
2026-06-08 08:40:49 +0000

    [FIX] html_editor: paste plain text in static file box
    Purpose of this commit:
    
    - Formatting is not supported inside static file boxes because the toolbar is
    unavailable there. Pasted content is now inserted as plain text when the
    cursor is inside an `.o_file_box`.
    
    task-6283420
    
    X-original-commit: a5b568b18b6f85937dbfdc41c7898a2fc51a5929
    Part-of: odoo/odoo#275765
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Adnan Chaudhary (adch) <adch@odoo.com>

--- addons/html_editor/static/src/main/media/file_plugin.js
+++ addons/html_editor/static/src/main/media/file_plugin.js
@@ -12,7 +12,7 @@ import { DISABLED_NAMESPACE } from "../toolbar/toolbar_plugin";
 
 export class FilePlugin extends Plugin {
     static id = "file";
-    static dependencies = ["dom", "history", "selection"];
+    static dependencies = ["clipboard", "dom", "history", "selection"];
     static defaultConfig = {
         allowFile: true,
     };
@@ -69,6 +69,16 @@ export class FilePlugin extends Plugin {
         is_powerbox_available_predicates: (node) => {
             if (closestElement(node, ".o_file_box")) {
                 return false;
+            }
+        },
+
+        /** Overrides */
+        paste_overrides: (selection, clipboardData) => {
+            if (closestElement(selection.anchorNode, ".o_file_box")) {
+                this.dependencies.clipboard.pasteText(
+                    clipboardData.getData("text/plain")
+                );
+                return true;
             }
         },
     };
