7724a335e7eb198d2dea473d829e657f4971254e
adch-odoo <adch@odoo.com>
2026-06-08 14:10:49 +0530

    [FIX] html_editor: paste plain text in static file box
    Purpose of this commit:
    
    - Formatting is not supported inside static file boxes because the toolbar is
    unavailable there. Pasted content is now inserted as plain text when the
    cursor is inside an `.o_file_box`.
    
    task-6283420

--- addons/html_editor/static/tests/file.test.js
+++ addons/html_editor/static/tests/file.test.js
@@ -3,7 +3,15 @@ import { EmbeddedFilePlugin } from "@html_editor/others/embedded_components/plug
 import { EMBEDDED_COMPONENT_PLUGINS, MAIN_PLUGINS } from "@html_editor/plugin_sets";
 import { isZwnbsp } from "@html_editor/utils/dom_info";
 import { describe, expect, test } from "@odoo/hoot";
-import { animationFrame, click, press, queryAll, queryOne, waitFor } from "@odoo/hoot-dom";
+import {
+    animationFrame,
+    click,
+    manuallyDispatchProgrammaticEvent,
+    press,
+    queryAll,
+    queryOne,
+    waitFor,
+} from "@odoo/hoot-dom";
 import { onRpc, patchWithCleanup } from "@web/../tests/web_test_helpers";
 import { setupEditor } from "./_helpers/editor";
 import { getContent, setSelection } from "./_helpers/selection";
@@ -252,6 +260,33 @@ describe("file command", () => {
         await insertText(editor, "/");
         await animationFrame();
         expect(".o-we-powerbox").toHaveCount(0);
+    });
+
+    test.tags("desktop");
+    test("should paste only plain text inside a static file box", async () => {
+        const { editor } = await setupEditor("<p>[]<br></p>");
+        patchUpload(editor);
+        execCommand(editor, "uploadFile");
+        // Wait until the file name is rendered.
+        await waitFor('.o_file_box .o_file_name_container:contains("file.txt")');
+
+        // Enable editing on the file name.
+        const fileNameEl = queryOne(".o_file_box .o_file_name_container .o_link_readonly");
+        await click(fileNameEl);
+        await animationFrame();
+
+        editor.shared.selection.setCursorEnd(fileNameEl);
+        await animationFrame();
+
+        // Simulate a paste with both HTML and plain text data.
+        const clipboardData = new DataTransfer();
+        clipboardData.setData("text/html", "<h1><b>rich content</b></h1>");
+        clipboardData.setData("text/plain", "plain content");
+        await manuallyDispatchProgrammaticEvent(fileNameEl, "paste", { clipboardData });
+        await animationFrame();
+
+        // Only the plain text should be inserted; no HTML elements (e.g. <h1>, <b>).
+        expect(fileNameEl.innerHTML).toBe("file.txtplain content");
     });
 });
 
