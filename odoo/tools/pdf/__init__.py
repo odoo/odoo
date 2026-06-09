ef80f3dd33f42bc74658c59d228b97eca49d3395
Mohamed GadAlrab <myah@odoo.com>
2026-06-09 13:09:17 +0000

    [FIX] core, account_edi_ubl_cii: set correct AFRelationship on PDF
    Adapt `add_attachment` to allow setting the AFRelationship tag on the PDF
    filespec object, In compliance with Factur-X/ZUGFeRD specs that require
    the AFRelationship tag in the PDF filespec object to reflect
    the relationship between the embedded XML and the visual PDF content:
    - /Data: the visual PDF contains more invoicing data than the XML.
    - /Alternative: the XML and the PDF are two equivalent representations
    of the same invoice.
    
    Additionally, update the embedded XML filename from `zugferd-invoice.xml`
    to `factur-x.xml`. The former is marked as deprecated since ZUGFeRD 2.0
    
    Ref: sections 6.2.2, 6.3.1, 6.3.2 of the ZUGFeRD 2.4 specification:
    https://www.ferd-net.de/en/downloads/publications/details/zugferd-24-english
    
    opw-6252082
    
    X-original-commit: 0d0c9646267afd0c96f11f0a7a93462d7396d785

--- odoo/tools/pdf/__init__.py
+++ odoo/tools/pdf/__init__.py
@@ -386,19 +386,35 @@ class OdooPdfFileWriter(PdfFileWriter):
             adapted_subtype = ''
         return adapted_subtype
 
-    def add_attachment(self, name, data, subtype=None):
+    def add_attachment(self, name, data, subtype=None, afrelationship='/Data'):
         """ Add an attachment to the pdf. Supports adding multiple attachment, while respecting PDF/A rules.
 
         :param name: The name of the attachement
         :param data: The data of the attachement
         :param subtype: The mime-type of the attachement. This is required by PDF/A, but not essential otherwise.
+        :param afrelationship: The relationship between the embedded file and the PDF content. This is required by PDF/A.
         """
+        # NOTE: Currently AFRelationship can only be '/Alternative' as it is coupled to the hardcoded
+        # <fx:ConformanceLevel>EXTENDED</fx:ConformanceLevel> in the XMP metadata template
+        # (account_invoice_pdfa_3_facturx_metadata). If support for MINIMUM/BASIC-WL is ever added,
+        # both afrelationship and ConformanceLevel must change together.
+
+        # Valid AFRelationship values per PDF 2.0 spec (ISO 32000-2, section 7.11.3)
+        valid_afrelationships = {'/Source', '/Data', '/Alternative', '/Supplement', '/Unspecified', '/EncryptedPayload', '/FormData', '/Schema'}
+        if afrelationship not in valid_afrelationships:
+            _logger.warning(
+                "Invalid AFRelationship value '%s', falling back to '/Data'. "
+                "Valid values are: %s",
+                afrelationship, ', '.join(sorted(valid_afrelationships))
+            )
+            afrelationship = '/Data'
         adapted_subtype = self.format_subtype(subtype)
 
         attachment = self._create_attachment_object({
             'filename': name,
             'content': data,
             'subtype': adapted_subtype,
+            'afrelationship': afrelationship,
         })
         if self._root_object.get('/Names') and self._root_object['/Names'].get('/EmbeddedFiles'):
             names_array = self._root_object["/Names"]["/EmbeddedFiles"]["/Names"]
@@ -431,9 +447,9 @@ class OdooPdfFileWriter(PdfFileWriter):
             })
     addAttachment = add_attachment
 
-    def embed_odoo_attachment(self, attachment, subtype=None):
+    def embed_odoo_attachment(self, attachment, subtype=None, afrelationship='/Data'):
         assert attachment, "embed_odoo_attachment cannot be called without attachment."
-        self.add_attachment(attachment.name, attachment.raw, subtype=subtype or attachment.mimetype)
+        self.add_attachment(attachment.name, attachment.raw, subtype=subtype or attachment.mimetype, afrelationship=afrelationship)
 
     def clone_reader_document_root(self, reader):
         super().clone_reader_document_root(reader)
@@ -612,7 +628,7 @@ class OdooPdfFileWriter(PdfFileWriter):
         file_entry_object = self._add_object(file_entry)
         filename_object = create_string_object(attachment['filename'])
         filespec_object = DictionaryObject({
-            NameObject("/AFRelationship"): NameObject("/Data"),
+            NameObject("/AFRelationship"): NameObject(attachment.get('afrelationship', '/Data')),
             NameObject("/Type"): NameObject("/Filespec"),
             NameObject("/F"): filename_object,
             NameObject("/EF"):
