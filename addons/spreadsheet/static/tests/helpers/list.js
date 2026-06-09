842f88dfab2fd73713c655b2025413d12c102c01
Adrien Minne (adrm) <adrm@odoo.com>
2026-06-09 14:57:59 +0200

    [FIX] spreadsheet: add `skipWaitForDataLoaded` for list tests
    Added the parameter `skipWaitForDataLoaded` to `createSpreadsheetWithList`
    to test what happens when the list is not ready yet.
    
    closes odoo/odoo#269096
    
    Task: 6289944
    Related: odoo/enterprise#119876
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- addons/spreadsheet/static/tests/helpers/list.js
+++ addons/spreadsheet/static/tests/helpers/list.js
@@ -46,6 +46,7 @@ export function insertListInSpreadsheet(model, params) {
  * @param {function} [params.mockRPC]
  * @param {number} [params.linesNumber]
  * @param {[number, number]} [params.position]
+ * @param {object} [params.skipWaitForDataLoaded]
  * @param {string} [params.sheetId]
  * @param {{name: string, asc: boolean}[]} [params.orderBy]
  *
@@ -68,6 +69,8 @@ export async function createSpreadsheetWithList(params = {}) {
 
     const env = model.config.custom.env;
     env.model = model;
-    await waitForDataLoaded(model);
+    if (!params.skipWaitForDataLoaded) {
+        await waitForDataLoaded(model);
+    }
     return { model, env };
 }
