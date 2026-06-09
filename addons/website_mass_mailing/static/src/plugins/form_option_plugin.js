c1c652bb25eced4ab03f74bc72f40776443b2231
Florian Charlier <flch@odoo.com>
2026-06-09 16:53:31 +0200

    [FIX] website_mass_mailing: avoid private lists on newsletter signup
    The patch to avoid showing display name is unnecessary since
    807477e7, and the function was broken since the parent's signature
    change at c49b6d56.
    
    Note that we must keep the guards in `WebsiteNewsletterForm`'s
    `_handle_website_form` as existing snippets won't be impacted
    by this fix.
    
    Task-6110549

--- addons/website_mass_mailing/static/src/plugins/form_option_plugin.js
+++ addons/website_mass_mailing/static/src/plugins/form_option_plugin.js
@@ -1,15 +1,14 @@
+import { Domain } from "@web/core/domain";
 import { patch } from '@web/core/utils/patch';
 import { FormOptionPlugin } from "@website/builder/plugins/form/form_option_plugin";
 
 patch(FormOptionPlugin.prototype, {
     /**
-     * Adds fieldName as "name" so the "name" field gets fetched instead of the
-     * "display_name" field to avoid seeing `(number of contacts)` in the
-     * "Subscribe to Newsletter" form editor.
+     * Adds domain to avoid loading private mailing lists.
      */
-    async _fetchFieldRecords(field) {
+    async _fetchFieldRecords({ field }) {
         if (field.name === 'list_ids' && field.relation === 'mailing.list') {
-            field.fieldName = 'name';
+            field.domain = Domain.and([field.domain || [], [['is_public', '=', true]]]).toList();
         }
         return super._fetchFieldRecords(...arguments);
     }
