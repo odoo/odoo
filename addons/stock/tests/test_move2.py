28359b2331e883b921166524183433c2c43a5d91
Moamen(msho) <msho@odoo.com>
2026-06-09 16:20:45 +0200

    [FIX] stock: make pushed move location_id more accurate
    This commit fixes the moves (and their picking) `location_id` to be
    more accurate after applying push rules on the moves.
    
    Before this commit:
    The new move introduced from push rules (and its new picking) would have
    the `location_id = old_move.location_dest_id` which could be a parent
    of the location_dest_id of the old move_lines which is more accurate.
    
    For example, if a move has a `location_dest_id = 'stock'` and the user
    changes the move_lines of that move so that
    `move_lines.location_dest_id = 'stock/sublocation'`.
    When push rules are applied on the move_lines,
    `new_move.location_id = old_move.location_dest_id = 'stock'`. But, it
    should be `new_move.location_id = 'stock/sublocation'` as the
    `rule.location_src_id` in this case.
    
    After this commit:
    The new move now (and its picking) will have the `location_id` set as
    the `rule.location_src_id` or `old_move.location_dest_id` just in case
    the `old_move.location_dest_id` is a sublocation of
    `rule.location_src_id` which gives a more accurate source location for
    the move and the picking.
    
    Task-6226781
    
    closes odoo/odoo#269122
    
    Signed-off-by: Steve Van Essche <svs@odoo.com>

--- addons/stock/tests/test_move2.py
+++ addons/stock/tests/test_move2.py
@@ -3360,6 +3360,59 @@ class TestRoutes(TestStockCommon):
         self.assertEqual(ship.location_dest_id, final_location)
         self.assertEqual(ship.move_ids.location_dest_id, final_location)
 
+    def test_push_rule_moves_source_location(self):
+        """
+        Check that the source location on pushed moves is accurately set to be the
+        rule.location_src_id if it's a child of the old_move.location_dest_id
+        """
+        warehouse = self.warehouse_1
+        warehouse.reception_steps = 'two_steps'
+        input_subloc = self.env['stock.location'].create({
+            'name': 'Sublocation',
+            'location_id': warehouse.wh_input_stock_loc_id.id,
+        })
+        self.env['stock.rule'].create({
+            'name': 'Input/Sub -> Scrap',
+            'location_src_id': input_subloc.id,
+            'location_dest_id': self.scrap_location.id,
+            'route_id': warehouse.reception_route_id.id,
+            'company_id': self.env.company.id,
+            'picking_type_id': self.picking_type_in.id,
+            'action': 'push',
+            'auto': 'manual',
+        })
+
+        move = self.env['stock.move'].create({
+            'location_id': self.supplier_location.id,
+            'location_dest_id': warehouse.wh_input_stock_loc_id.id,
+            'picking_type_id': self.picking_type_in.id,
+            'product_id': self.product1.id,
+            'uom_id': self.uom_unit.id,
+            'product_uom_qty': 2.0,
+            'move_line_ids': [
+                Command.create({
+                    'location_dest_id': input_subloc.id,
+                    'quantity': 1.0,
+                }),
+                Command.create({
+                    'location_dest_id': warehouse.wh_input_stock_loc_id.id,
+                    'quantity': 1.0,
+                }),
+            ],
+        })
+        move.picked = True
+        move._action_done()
+
+        self.assertEqual(len(move.move_dest_ids), 2)
+        push_move_a, push_move_b = move.move_dest_ids
+        self.assertEqual(push_move_a.location_id, input_subloc)
+        self.assertEqual(push_move_b.location_id, warehouse.wh_input_stock_loc_id)
+        self.assertEqual(push_move_a.location_dest_id, self.scrap_location)
+        self.assertEqual(push_move_b.location_dest_id, self.stock_location)
+        self.assertNotEqual(push_move_a.picking_id, push_move_b.picking_id)
+        self.assertEqual(push_move_a.picking_id.location_id, input_subloc)
+        self.assertEqual(push_move_b.picking_id.location_id, warehouse.wh_input_stock_loc_id)
+
 
 class TestAutoAssign(TestStockCommon):
     _test_user_groups = (
