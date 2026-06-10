bec3628e93b951280477d7d91f9d15fa88e967bb
hossamelgendy1 <hoelg@odoo.com>
2026-06-10 15:27:30 +0000

    [PERF] stock: speedup backorder creation
    A PostgreSQL "memory exhausted" error was caused by generating a large number
    of OR'ed conditions, equal to the number of processed moves. Fixed by grouping moves by
    `location_dest_id` and generating one condition per location using an `in` clause, which
    is typically much smaller than generating one condition per move.
    
    OPW-6272667
    
    closes odoo/odoo#273555
    
    X-original-commit: 76c4efcd5538ce86bd165347181a05eee792bc4e
    Signed-off-by: Piryns Victor (pivi) <pivi@odoo.com>
    Signed-off-by: Hossam Elgendi (hoelg) <hoelg@odoo.com>

--- addons/stock/models/stock_move.py
+++ addons/stock/models/stock_move.py
@@ -2623,8 +2623,8 @@ Please change the quantity done or the rounding precision in your settings.""",
             return
 
         product_domains = Domain.OR(
-            [('product_id', '=', move.product_id.id), ('location_id', 'parent_of', move.location_dest_id.id)]
-            for move in self
+            [('product_id', 'in', moves.product_id.ids), ('location_id', 'parent_of', location_dest.id)]
+            for location_dest, moves in self.grouped('location_dest_id').items()
         )
         static_domain = [('state', 'in', ['confirmed', 'partially_available']),
                          ('procure_method', '=', 'make_to_stock'),
