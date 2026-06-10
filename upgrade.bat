b580402bac02e86d9d144d5ce7a126ed71bc08a7
Gural <122676712+GuralSyncalypse@users.noreply.github.com>
2026-06-10 23:14:52 +0700

    Record Rule updated

--- upgrade.bat
+++ upgrade.bat
@@ -1,2 +1,2 @@
 @echo off
-python odoo-bin -r odoo -w admin --addons-path=addons -d htland -u ht_tools --stop-after-init
+python odoo-bin -r odoo -w admin --addons-path=addons -d htland -u ht_crm --stop-after-init
