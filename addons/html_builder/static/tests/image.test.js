2bed8ba49b663ecc0df0d63580b0d1cdc7035d2c
felin-odoo <felin@odoo.com>
2026-06-10 16:00:59 -0400

    [FIX] html_builder: prevent crash on legacy image shapes
    When the Website Editor encounters an image shape that does not exist
    in the registry, it fatally crashes upon saving (`TypeError: Cannot
    read properties of undefined`), blocking the user from saving the page.
    
    While an upgrade script exists to remap these shapes
    ([commit f348be0](https://github.com/odoo/upgrade/commit/f348be0dcbc63c1f74f742b562509f81767564c0)),
    timeline gaps leave SaaS databases vulnerable. For example, if a client
    upgraded their database to 17.0 in Feb 2024, they bypassed the migration
    script merged in Dec 2024. This leaves the legacy shape permanently
    orphaned inside their modern views.
    
    This commit adds a `getImageShape` fallback. Instead of crashing,the
    editor now defaults to standard values and renders "None" in the UI,
    allowing the user to select a new shape and save their work.
    
    Steps to Reproduce:
    Since standard upgrades now run the latest scripts, we simulate the
    state of a database that upgraded during the timeline gap:
    1. Install Website.
    2. Go to Site -> HTML / CSS Editor.
    3. Add `data-shape="web_editor/basic/bsc_organic_2"` to any <img>.
    4. Click "Edit" to open the Website Builder.
    5. Click "Save" without making changes.
    6. Observe the fatal JS traceback.
    
    [opw-6286044](https://www.odoo.com/odoo/my-support-tasks/6286044?debug=assets)
    [opw-6291591](https://www.odoo.com/odoo/my-support-tasks/6291591?debug=assets)

--- addons/html_builder/static/tests/image.test.js
+++ addons/html_builder/static/tests/image.test.js
@@ -14,3 +14,36 @@ test("Size should not be displayed on CORS protected images", async () => {
     await waitSidebarUpdated();
     expect(".o-hb-image-size-info").toHaveCount(0);
 });
+
+test("Handle legacy image shapes from older versions", async () => {
+    const oldShapeId = "web_editor/basic/bsc_organic_2";
+    const testImageUrl = "/web/image/123/test_image.jpg";
+
+    onRpc("/web_editor/static/image_shapes/basic/bsc_organic_2.svg", () => "");
+    onRpc("/html_builder/static/image_shapes/basic/bsc_organic_2.svg", () => "");
+
+    onRpc(testImageUrl, () => dataURItoBlob(dummyBase64Img));
+
+    onRpc("/html_editor/get_image_info", () => ({
+        original: {
+            id: 123,
+            image_src: testImageUrl,
+            mimetype: "image/jpeg",
+        },
+    }));
+
+    const { waitSidebarUpdated } = await setupHTMLBuilder(`
+        <img class="img-fluid test-image"
+            src="${testImageUrl}"
+            data-original-id="123"
+            data-mimetype="image/jpeg"
+            data-shape="${oldShapeId}"
+            width="100" height="100"
+            style="display: block;">
+    `);
+
+    await contains(":iframe img.test-image").click();
+    await waitSidebarUpdated();
+
+    expect(":iframe img").toHaveCount(1);
+});
