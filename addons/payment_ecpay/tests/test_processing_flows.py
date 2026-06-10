346f377da4bb61f82da5e7a2c7725a8940d733b4
Brian Nguyen <ngbri@odoo.com>
2026-06-10 04:31:39 +0000

    [FIX] payment_ecpay: update languages for traditional and simplified chinese
    Current behavior:
    When Odoo locale is set in traditional Chinese (zh_TW),
    the ECPay payment portal shows up in simplified Chinese (zh_CN).
    
    Expected behavior:
    When Odoo locale is set in traditional Chinese (zh_TW),
    the ECPay payment portal should show up in traditional Chinese (zh_TW) also.
    
    Steps to reproduce:
    In Odoo 19.0,
    1. Install modules: payment_ecpay, website, ecommerce
    2. Change the company and website language to Traditional Chinese (zh_TW).
    Make sure the currency is in TWD (Taiwanese Dollar)
    3. Add ECPay as a payment method
    4. Add some test products and checkout with ECPay
    5. Observe the language on ECPay portal website, and payload in V5
       package in network tab
    
    Cause of the issue:
    Initially the locale mapping was wrong, mapping CHI to zh. There's no
    "zh" locale, must either be "zh_TW" or "zh_CN".
    So when the Odoo locale is set to traditional Chinese (zh_TW), it
    included Language=CHI in the payload and redirect to ECPAY,
    making ECPay think that we're trying to request the simplified Chinese site.
    Caused by commit: 76c81d6f67b54940879b1020068c7140c4f2199b
    
    Fix:
    Fixed the language mapping following ECPay developer docs,
    and what language code to be included in the payload sent to ECPay
    from current Odoo locale.
    
    opw-6270820
    
    ---------------------------------------------------------------
    
    closes odoo/odoo#272415
    
    X-original-commit: b0bbe808c4f681faa3ddf9cc44ce040ca8f1b2e5
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>
    Signed-off-by: Brian Nguyen (ngbri) <ngbri@odoo.com>

--- addons/payment_ecpay/tests/test_processing_flows.py
+++ addons/payment_ecpay/tests/test_processing_flows.py
@@ -19,6 +19,18 @@ class TestProcessingFlows(EcpayCommon, PaymentHttpCommon):
         signature = self.provider._ecpay_calculate_signature(self.payment_result_data)
         self.assertEqual(signature, self.webhook_payment_data_signature)
 
+    def test_compute_signature_without_language_returns_correct_signature(self):
+        self.env["res.lang"]._activate_lang("zh_TW")
+        tx = self._create_transaction(
+            "redirect", payment_method_id=self.env.ref("payment.payment_method_card").id
+        )
+        rendering_values = tx.with_context(lang="zh_TW")._get_specific_rendering_values(None)
+        signature_data = dict(rendering_values)
+        signature_data.pop("CheckMacValue", None)
+        signature_data.pop("api_url", None)
+        expected_mac = tx.provider_id._ecpay_calculate_signature(signature_data)
+        self.assertEqual(rendering_values["CheckMacValue"], expected_mac)
+
     @mute_logger("odoo.addons.payment_ecpay.controllers.main")
     def test_returning_from_payment_triggers_processing(self):
         """Test that receiving a valid redirect notification triggers the processing of the
