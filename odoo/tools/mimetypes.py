ca7e9769260ff0333f7334ae608f4fbf26baed1d
Pierre Paridans <app@odoo.com>
2026-06-11 11:06:49 +0000

    [FIX] core: workaround buggy libmagic 0.46 zip buffer detection
    Libmagic version 0.46 (currently available in Debian Trixie/Forky and
    Ubuntu Resolute) introduced a regression regarding ZIP file detection.
    While it correctly identifies a ZIP file when reading directly from a
    file path, it fails when reading the exact same content from a buffer,
    returning a generic 'application/octet-stream' instead.
    
    Because `guess_mimetype` primarily evaluates buffers, this upstream
    bug breaks MIME type detection for ZIP files (and related formats like
    docx, xlsx, etc.) in Odoo environments running this libmagic version.
    
    Since we cannot directly fix the library itself, this commit adapts
    Odoo's `guess_mimetype` to fallback to our custom implementation when
    libmagic returns the generic 'application/octet-stream' to workaround
    this library's bug.
    
    Upstream libmagic fixes:
    - https://github.com/file/file/commit/f1adef05b8a85be50d28965b1fd21fcceacf7a4e
    - https://github.com/file/file/commit/60b2032b96fc185b37fb0f2152e834efb2edad6e
    
    Upstream python-magic issue:
    - https://github.com/ahupp/python-magic/issues/354
    
    runbot-938197
    
    closes odoo/odoo#270121
    
    X-original-commit: 2d7ea95b00726d24187bd2e373a4a174c6edd88c
    Signed-off-by: Julien Castiaux (juc) <juc@odoo.com>
    Signed-off-by: Pierre Paridans (app) <app@odoo.com>

--- odoo/tools/mimetypes.py
+++ odoo/tools/mimetypes.py
@@ -205,6 +205,8 @@ if magic:
         if isinstance(bin_data, bytearray):
             bin_data = bytes(bin_data[:MIMETYPE_HEAD_SIZE])
         mimetype = _guesser(bin_data[:MIMETYPE_HEAD_SIZE])
+        if mimetype == 'application/octet-stream':
+            mimetype = _odoo_guess_mimetype(bin_data)
         # upgrade incorrect mimetype to official one, fixed upstream
         # https://github.com/file/file/commit/1a08bb5c235700ba623ffa6f3c95938fe295b262
         if mimetype == 'image/svg':
