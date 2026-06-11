3e05909231bc96bd83881032f039795c0b6fac92
MaximeNoirhomme <noma@odoo.com>
2026-06-11 13:00:59 +0000

    [FIX] approval_purchase, product: avoid access error with inaccessible vendors
    **Issue**
    Having supplier pricelists with at least one vendor inaccessible to the current user can trigger an access error
    when creating an RFQ approval request.
    
    **Steps to reproduce**
    - Have two companies A and B and two users u1 and u2
    - user u2 only have access to company A
    - With user u1:
    	- Create two vendors v1 and v2 without any company assigned
    	- Create vendor pricelists for a product for each vendor and assign the company A to the pricelist
    - Add the company B for the vendor v2
    - With user u2:
    	- Open approval application
    	- Try to create an approval for an RFQ for that product (the vendor v1 will be automatically selected)
    	- Save it
    -> An access error is thrown
    
    **Cause**
    Saving the approval request computes `has_no_seller`, which calls `_select_seller`:
    https://github.com/odoo/enterprise/blob/03c737685ff6dfc95a8bc72491646774fc426b1f/approvals_purchase/views/approval_product_line_views.xml#L9
    https://github.com/odoo/enterprise/blob/03c737685ff6dfc95a8bc72491646774fc426b1f/approvals_purchase/models/approval_product_line.py#L32
    https://github.com/odoo/enterprise/blob/03c737685ff6dfc95a8bc72491646774fc426b1f/approvals_purchase/models/approval_product_line.py#L62-L70
    Which filtered the right seller
    https://github.com/odoo/odoo/blob/c37e76850d3ff790b76493bd1003d80e170bd4bf/addons/product/models/product_product.py#L759
    By preparing the sellers:
    https://github.com/odoo/odoo/blob/c37e76850d3ff790b76493bd1003d80e170bd4bf/addons/product/models/product_product.py#L721
    https://github.com/odoo/odoo/blob/c37e76850d3ff790b76493bd1003d80e170bd4bf/addons/product/models/product_product.py#L712
    Please note that `self.seller_ids` contains both sellers (even v2)
    By filtering the suppliers:
    https://github.com/odoo/odoo/blob/c37e76850d3ff790b76493bd1003d80e170bd4bf/addons/product/models/product_supplierinfo.py#L104-L105
    But at that point, one of the supplier in `self`, can be accessed, thus an access error is thrown while
    trying to access its associated `partner_id`.
    
    opw-6203910
    
    X-original-commit: aed1619c34b1f65bd9a3d155fe76a82d404ef5e7

--- addons/product/models/product_supplierinfo.py
+++ addons/product/models/product_supplierinfo.py
@@ -120,4 +120,4 @@ class ProductSupplierinfo(models.Model):
         return res
 
     def _get_filtered_supplier(self, company_id, product_id, params=False):
-        return self.filtered(lambda s: (not s.company_id or s.company_id.id == company_id.id) and (s.partner_id.active and (not s.product_id or s.product_id == product_id)))
+        return self.filtered(lambda s: (not s.company_id or s.company_id.id == company_id.id) and (s.partner_id.sudo().active and (not s.product_id or s.product_id == product_id)))
