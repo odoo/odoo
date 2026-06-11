6979506cd4ee319d7af291d833cdd77fa6d118b6
Max Whale <mawh@odoo.com>
2026-06-11 11:27:19 +0200

    [IMP] point_of_sale: minor typescript fixes
    This commit fixes the following small typing issues:
    - The import path for the scale interface was wrong in `registry.d.ts`.
    - The `services.d.ts` file was missing a few services and included a
      service that no longer exists.
    - The `PosStore` object had no type information for its services/env due
      to an untyped `setup` method.

--- addons/point_of_sale/static/src/@types/registry.d.ts
+++ addons/point_of_sale/static/src/@types/registry.d.ts
@@ -1,6 +1,6 @@
 declare module "registries" {
     import { PaymentInterface } from "@point_of_sale/app/utils/payment/payment_interface";
-    import { ScaleInterface } from "@point_of_sale/app/services/scale_interface";
+    import { ScaleInterface } from "@point_of_sale/app/utils/scale/scale_interface";
 
     interface GlobalRegistryCategories {
         pos_payment_providers: typeof PaymentInterface;
