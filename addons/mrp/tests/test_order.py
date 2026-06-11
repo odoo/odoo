f61afffc934b26d21336152b2e0a6d020d8e0c16
Lancelot Semal <lase@odoo.com>
2026-06-11 13:59:38 +0000

    [FIX] mrp: update confirmed mo from bom line deletion/update
    Steps to reproduce:
    
    - Create a product with a bom and 2 operations
    - Create an MO for 1 unit of that product
    - Confirm the MO
    - On the bom, delete the second operation and modify the first operation
      on anything else than the company, name or workcenter
    - Go back to the MO, click the "Update Bom" button
    > The second operation is not unlinked and the first operation is not
      updated
    
    Cause of the issue:
    
    The `action_update_bom` updates the move raws and operations of the MO
    via the `_link_bom`:
    https://github.com/odoo/odoo/blob/f66614193cce18f5a3298d03ce7e5f29d54f07e9/addons/mrp/models/mrp_production.py#L1214-L1218
    For draft MO's all the work of these updates is done via the compute
    methods and by deleting all the records unrelevant to the new bom:
    https://github.com/odoo/odoo/blob/f66614193cce18f5a3298d03ce7e5f29d54f07e9/addons/mrp/models/mrp_production.py#L2603-L2626
    And, in that case all the workorders that are not linked to an operation
    of the bom are expected to be deleted. However, when the MO is not in
    draft, the update of operations is expected to be performed here:
    https://github.com/odoo/odoo/blob/f66614193cce18f5a3298d03ce7e5f29d54f07e9/addons/mrp/models/mrp_production.py#L2647-L2664
    However, since the operation of the bom has been deleted, the workorder
    that is expected to be deleted is not linked to any operation and hence
    does not satisfy the condition to be deleted:
    https://github.com/odoo/odoo/blob/f66614193cce18f5a3298d03ce7e5f29d54f07e9/addons/mrp/models/mrp_production.py#L2663-L2664
    Concerning the non update of operations, it happens because the MO's
    operation are only updated on the three fields: `company_id`,
    `workcenter_id`, `name`:
    https://github.com/odoo/odoo/blob/31df5033e31c193b4576ef37dfbc5fc683817bc5/addons/mrp/models/mrp_production.py#L2647-L2664
    https://github.com/odoo/odoo/blob/31df5033e31c193b4576ef37dfbc5fc683817bc5/addons/mrp/models/mrp_production.py#L2628-L2629
    However, many other changes can and are actually relevant.
    
    Note:
    
    Prior to commit 80e6ed658fb43584bc2fad673ca40d9af6cf0ab6 operations were
    archived on boms rather than deleted:
    https://github.com/odoo/odoo/blob/4a5270218fe6fd7d30edb6d684b3340dc7423bab/addons/mrp/views/mrp_routing_views.xml#L53-L55
    As such they would still be linked to an operation (but unrelated to the
    present values of the bom) and hence would fall into the condition of
    being unlinked from the MO.
    
    Since the bom operations are no longer archived there is no way to
    determine if an operation used to be linked to a bom and we therefore
    need to chose between deleting all operations unrelated to the present
    bom or to keep them all (when the MO has been confirmed).
    
    Note on teh forward port:
    
    Starting from saas-19.1, the fix conflict with commit https://github.com/odoo/odoo/commit/5c4cf2e0fcc3ed5bef77380bb65f356c56b5dac4
    That is, the `action_update_bom`, now replan's workorders:
    https://github.com/odoo/odoo/blob/3412253211daf13d50755fdce7edd9ed9c4400df/addons/mrp/models/mrp_production.py#L1207-L1217
    However, the `_link_bom` now unlinks the obsolete and re-creates the
    still valid workorders (possibly unplanned). In particular, both the
    computation of `is_planned` and of the new `duration_expected` might
    differ from the expected checked in the `action_update_bom`. Note that
    to ensure everything goes as planned, we need to unlink the obsolete
    workorders before creating the new workorders which would be incorrectly
    replanned. Other than that, the check on the `duration_expected` in the
    `action_update_bom` needs to be updated accordingly.
    
    opw-6285878
    opw-6261738
    
    X-original-commit: 21ac02258adafd63043ef8f8fd3c86c803f0cb1c

--- addons/mrp/tests/test_order.py
+++ addons/mrp/tests/test_order.py
@@ -3319,7 +3319,7 @@ class TestMrpOrder(TestMrpCommon, MailCase):
 
         # New planning: Gift Wrap Maching is at 8:00 (90 minutes),
         # then Cutting Machine at 9:30 (40 minutes), then Weld Machine at 10:10.
-        self.assertRecordValues(mo.workorder_ids, [
+        self.assertRecordValues(mo.workorder_ids.sorted('sequence'), [
             {'date_start': datetime(2025, 10, 1, 8, 0)},
             {'date_start': datetime(2025, 10, 1, 9, 30)},
             {'date_start': datetime(2025, 10, 1, 10, 10)},
@@ -5533,6 +5533,100 @@ class TestMrpOrder(TestMrpCommon, MailCase):
         test_bom.allow_operation_dependencies = False
         mo.button_plan()
         self.assertTrue(mo.is_planned)
+
+    def test_update_bom_removes_deleted_operation_workorder(self):
+        """
+        When an operation is deleted from the BOM and action_update_bom is called on a
+        confirmed MO, the workorder for the deleted operation must be removed from workorder_ids.
+        """
+        bom = self.env['mrp.bom'].create({
+            'product_tmpl_id': self.product.product_tmpl_id.id,
+            'product_qty': 1.0,
+            'operation_ids': [
+                Command.create({'name': 'Operation 1', 'workcenter_id': self.workcenter_1.id}),
+                Command.create({'name': 'Operation 2', 'workcenter_id': self.workcenter_2.id}),
+            ],
+        })
+        operation_to_update, operation_to_delete = bom.operation_ids
+        mo = self.env['mrp.production'].create({
+            'product_id': self.product.id,
+            'bom_id': bom.id,
+            'product_qty': 1.0,
+        })
+        mo.action_confirm()
+        self.assertEqual(mo.state, 'confirmed')
+        workorder_to_delete = mo.workorder_ids.filtered(lambda wo: wo.operation_id == operation_to_delete)
+        self.assertRecordValues(mo.workorder_ids - workorder_to_delete, [{'duration_expected': 90.0}])
+
+        operation_to_update.time_cycle_manual = 40.0
+        operation_to_delete.unlink()
+        mo.action_update_bom()
+        self.assertRecordValues(mo.workorder_ids, [{'operation_id': operation_to_update.id, 'duration_expected': 65.0}])
+
+    def test_update_bom_with_cancelled_workorder_of_deleted_operation(self):
+        """
+        Check that updating the BoM of a confirmed MO still works when an operation whose
+        work order is cancelled has been removed from the BoM.
+        """
+        bom = self.env['mrp.bom'].create({
+            'product_tmpl_id': self.product.product_tmpl_id.id,
+            'product_qty': 1.0,
+            'operation_ids': [
+                Command.create({'name': 'Operation 1', 'workcenter_id': self.workcenter_1.id}),
+                Command.create({'name': 'Operation 2', 'workcenter_id': self.workcenter_2.id}),
+            ],
+        })
+        cancelled_operation, remaining_operation = bom.operation_ids
+        mo = self.env['mrp.production'].create({
+            'product_id': self.product.id,
+            'bom_id': bom.id,
+            'product_qty': 1.0,
+        })
+        mo.action_confirm()
+        cancelled_workorder = mo.workorder_ids.filtered(lambda wo: wo.operation_id == cancelled_operation)
+        cancelled_workorder.action_cancel()
+        self.assertEqual(cancelled_workorder.state, 'cancel')
+        # the Update BoM button is only shown on a draft or confirmed MO
+        self.assertEqual(mo.state, 'confirmed')
+
+        cancelled_operation.unlink()
+        mo.action_update_bom()
+        self.assertRecordValues(mo.workorder_ids, [
+            {'operation_id': False, 'state': 'cancel'},
+            {'operation_id': remaining_operation.id, 'state': 'ready'},
+        ])
+
+    @freeze_time('2025-10-01 08:00')
+    def test_update_bom_keeps_workorder_planning(self):
+        """
+        Check that updating the BoM of a planned MO after a component change keeps its work
+        orders planned at the same time.
+        """
+        final_product, component = self.product, self.product_1
+        bom = self.env['mrp.bom'].create({
+            'product_tmpl_id': final_product.product_tmpl_id.id,
+            'product_qty': 1.0,
+            'bom_line_ids': [Command.create({'product_id': component.id, 'product_qty': 1})],
+            'operation_ids': [
+                Command.create({'name': 'Operation 1', 'workcenter_id': self.workcenter_1.id}),
+                Command.create({'name': 'Operation 2', 'workcenter_id': self.workcenter_2.id}),
+            ],
+        })
+        mo = self.env['mrp.production'].create({
+            'product_id': final_product.id,
+            'bom_id': bom.id,
+            'product_qty': 1.0,
+        })
+        mo.action_confirm()
+        mo.button_plan()
+        self.assertTrue(mo.is_planned)
+        planned_starts = mo.workorder_ids.sorted('sequence').mapped('date_start')
+
+        bom.write({'bom_line_ids': [Command.update(bom.bom_line_ids.id, {'product_qty': 2})]})
+        self.assertTrue(mo.is_outdated_bom)
+        mo.action_update_bom()
+        self.assertTrue(mo.is_planned)
+        self.assertEqual(mo.workorder_ids.sorted('sequence').mapped('date_start'), planned_starts)
 
 
 class TestMrpOrderPostInstall(TestMrpCommon):
