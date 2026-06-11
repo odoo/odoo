alkuneemi/Luxara

[FIX] mail : fix chatter suggestions position
# How to reproduce
- Go into any form view of a model with a chatter (e.g. Quotation)
- Add multiple long log notes. You need to be able to scroll enough to not see the last log note
- Click edit on the last log note
- Scroll down to the bottom

# The problem
An empty rectangle is displayed next to the log note in edit mode.

# Cause
The rectangle comes from the NavigableList Component, which is the list that displays suggestions when typing things like "@" or "#" :
https://github.com/odoo/odoo/blob/1fd44c3bb11a79d5b6aa72bf7de5a83e6c45be46/addons/mail/static/src/core/common/composer.xml#L137

This components uses the `usePostion()` hook, which purpose is to try to find the
most appropriate place to put the element. It will try different postions (e.g. on the left, below,
above, etc.) and will pick the most appropriate one. It will then adjust the element's style
to position it correctly. It is possible to ask for a preferred position using the options given
to the hook. This position will be prioritized over the others if it is suitable. In the case of
the NavigableList of the chatter, we give it either 'bottom-fit' or the 'top-fit' positions, wich means
it will prefer to be displayed above or below the message :
https://github.com/odoo/odoo/blob/dd84309df7fd39e9e97ed02d135d25b211085135/addons/mail/static/src/core/common/composer.js#L449-L459

But in our case, when we scroll back up, the position of the rectangle stays on the left, even
though the below space is available. That is because of this commit that introduced a
memorization of the last solution :
https://github.com/odoo/odoo/commit/b2b8d2dbb8396d86492a3089db9b1b1545c8f13b
https://github.com/odoo/odoo/blob/f2434aac74324a65ccd81aa18c7b0e8318e59fde/addons/web/static/src/core/position/position_hook.js#L59-L61

This means that when we scroll down, the bottom positions fails and so the left one
is defaulted to. Since the position is memorized, it stays on the left. T

addons/mail/static/src/core/common/navigable_list.js
@@ -20,6 +20,7 @@ export class NavigableList extends Component {
         position: { type: String, optional: true },
         closeOnSelect: { type: Boolean, optional: true },
         isLoading: { type: Boolean, optional: true },
+        forgetSolution: { type: Boolean, optional: true }
     };
     static defaultProps = {
         position: "bottom",
@@ -48,7 +49,7 @@ export class NavigableList extends Component {
             this.close();
         });
         // position and size
-        usePosition("root", () => this.props.anchorRef, { position: this.props.position });
+        usePosition("root", () => this.props.anchorRef, { position: this.props.position, forgetSolution: this.props.forgetSolution });
         useEffect(
             () => {
                 this.open();

odoo/odoo

X-original-commit: 7f54d5480a71ea2dd7ca10aebe0204a81671bd36

addons/mail/static/src/core/common/navigable_list.js
@@ -21,6 +21,7 @@ export class NavigableList extends Component {
         position: { type: String, optional: true },
         closeOnSelect: { type: Boolean, optional: true },
         isLoading: { type: Boolean, optional: true },
+        rememberPosition: { type: Boolean, optional: true }
     };
     static defaultProps = {
         position: "bottom",
@@ -49,7 +50,7 @@ export class NavigableList extends Component {
             this.close();
         });
         // position and size
-        usePosition("root", () => this.props.anchorRef, { position: this.props.position });
+        usePosition("root", () => this.props.anchorRef, { position: this.props.position, rememberPosition: this.props.rememberPosition });
         useLayoutEffect(
             () => {
                 this.open();