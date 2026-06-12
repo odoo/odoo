27ccd929260c53aace6e4495404136e5513be0fb
Chrysanthe (chgo) <chgo@odoo.com>
2026-06-12 10:40:29 +0200

    [IMP] website: introduce `s_about_bold` snippet
    This commit introduces the new `s_about_bold` snippet.
    
    task-6069352

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -228,6 +228,7 @@
         'views/snippets/s_banner_categories.xml',
         'views/snippets/s_animated_cover.xml',
         'views/snippets/s_projects_list.xml',
+        'views/snippets/s_about_bold.xml',
         'views/snippets/s_age_verification_popup.xml',
         'views/new_page_template_templates.xml',
         'views/website_views.xml',
