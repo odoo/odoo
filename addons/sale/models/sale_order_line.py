3ce97c812b5fca71cc051bbe5e67b9640b2b6d1c
krip-odoo <krip@odoo.com>
2026-06-12 13:19:11 +0000

    [FIX] sale,purchase: allow order confirmation with readonly product access
    Steps to produce:
    - Install the sales module.
    - Create a product and enable track inventory.
    - Log in as user with only view access rights in products
      and also have the sales access rights.
    - Create a sale order containing product and confirm it.
    
    Issue:
    -  An access error is raised during order confirmation.
    
    Root cause:
    - In [commit], to handle inventory tracking, the `qty_available` field
    was moved to `product.product`. Unlike before, this value is increased
    or decreased depending on the operation performed.
    
    As a consequence, creating or updating a sale order triggers a write
    to this `qty_available` field on the related product. This
    write happens under the current user's permissions, so users who
    only have read access to products (but can create/edit sale orders)
    hit an `AccessError`, since they lack write access on
    `product.product`.
    
    Solution:
    - Use `sudo()` when accessing the required product quantity information
      to ensure the operation can be completed without requiring additional product
      access rights.
    
    The same issue also occurs when confirming a purchase order.
    
    [commit]: https://github.com/odoo/odoo/commit/ca96992919b11105da44238c3e522f8eec4a740b
    
    opw-6290608
    
    closes odoo/odoo#276891
    
    X-original-commit: 093bcf83b8afd48e7e77fe1ecf4e549f3622d100
    Signed-off-by: Krishna Arvindkumar Patel (krip) <krip@odoo.com>

--- addons/sale/models/sale_order_line.py
+++ addons/sale/models/sale_order_line.py
@@ -1634,7 +1634,7 @@ class SaleOrderLine(models.Model):
                 qty_delivered = line.product_uom_id._compute_quantity(
                     line.qty_delivered, line.product_id.uom_id
                 )
-                line.product_id.with_context(
+                line.product_id.sudo().with_company(line.company_id).with_context(
                     skip_qty_available_update=True
                 ).qty_available -= qty_delivered
             if not line.display_type and line.state == "sale":
@@ -1702,7 +1702,7 @@ class SaleOrderLine(models.Model):
                 delta_qty_delivered = line.product_uom_id._compute_quantity(
                     delta_qty_delivered, line.product_id.uom_id
                 )
-                line.product_id.with_context(
+                line.product_id.sudo().with_company(line.company_id).with_context(
                     skip_qty_available_update=True
                 ).qty_available -= delta_qty_delivered
 
