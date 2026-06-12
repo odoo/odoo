a9acefb255780a3bf0d95145d09fdef7efa0439a
Sébastien Theys <seb@odoo.com>
2026-06-12 12:15:26 +0200

    [FIX] website_livechat: fix race condition in test_complete_chatbot_flow_ui
    Follow up of https://github.com/odoo/odoo/pull/267671
    
    Using `:last` is prone to race condition because the chatbot will post
    several messages in a row, so only the last message of the chain is
    guaranteed to actually be last. A better way to resolve duplicate steps
    is to check the index of each result.
    
    The opportunity is taken to clean the test further and convert :contains
    to :text for more robustness.
    
    runbot-939963
    
    closes odoo/odoo#269756
    
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- addons/website_livechat/static/tests/tours/website_livechat_chatbot_flow.js
+++ addons/website_livechat/static/tests/tours/website_livechat_chatbot_flow.js
@@ -3,8 +3,9 @@ import { registry } from "@web/core/registry";
 import { Chatbot } from "@im_livechat/core/common/chatbot_model";
 
 const trigger = `.o-livechat-root:shadow .o-mail-Composer-input`;
-const messagesContain = (text) =>
-    `.o-livechat-root:shadow .o-mail-Message:last:contains("${text}")`;
+const waitForMessage = (text, index = 0) => ({
+    trigger: `.o-livechat-root:shadow .o-mail-Message:has(:text("${text}")):eq(${index})`,
+});
 const postMessage = (text) => ({
     trigger,
     run: `edit ${text} && press Enter`,
@@ -28,67 +29,40 @@ registry.category("web_tour.tours").add("website_livechat_chatbot_flow_tour", {
             TYPING_DELAY: 0,
         });
         return [
-            {
-                // check second welcome message is posted
-                trigger: messagesContain("I help lost visitors find their way."),
-            },
-            {
-                trigger: messagesContain("How can I help you?"),
-            },
+            waitForMessage("Hello! I'm a bot!"),
+            waitForMessage("I help lost visitors find their way."),
+            waitForMessage("How can I help you?"),
             {
                 content: "Reactions should not be available before thread is persisted.",
                 trigger: `body:not(:has(.o-mail-Message-actions [title='Add a Reaction']))`,
             },
             {
-                trigger: '.o-livechat-root:shadow button:contains("I\'d like to buy the software")',
+                trigger: '.o-livechat-root:shadow button:text("I\'d like to buy the software")',
                 run: "click",
             },
             {
                 // check selected option is posted and reactions are available since
                 // the thread has been persisted in the process
                 trigger:
-                    ".o-livechat-root:shadow .o-mail-ChatWindow .o-mail-Message:has(.o-mail-Message-actions [title='Add a Reaction']):contains('I\\'d like to buy the software')",
+                    ".o-livechat-root:shadow .o-mail-ChatWindow .o-mail-Message:has(.o-mail-Message-actions [title='Add a Reaction']):text('I\\'d like to buy the software')",
             },
-            {
-                // check ask email step following selecting option A
-                trigger: messagesContain("Can you give us your email please?"),
-            },
+            waitForMessage("Can you give us your email please?"),
             postMessage("No, you won't get my email!"),
-            {
-                // check invalid email detected and the bot asks for a retry
-                trigger: messagesContain(
-                    "'No, you won't get my email!' does not look like a valid email. Can you please try again?"
-                ),
-            },
+            waitForMessage(
+                "'No, you won't get my email!' does not look like a valid email. Can you please try again?"
+            ),
             postMessage("okfine@fakeemail.com"),
-            {
-                // check that this time the email goes through and we proceed to next step
-                trigger: messagesContain("Your email is validated, thank you!"),
-            },
-            {
-                trigger: messagesContain("Can you give us your phone number please?"),
-            },
+            waitForMessage("Your email is validated, thank you!"),
+            waitForMessage("Can you give us your phone number please?"),
             postMessage("123456"),
-            {
-                trigger: messagesContain(
-                    "'123456' does not look like a valid phone number. Can you please try again?"
-                ),
-            },
+            waitForMessage(
+                "'123456' does not look like a valid phone number. Can you please try again?"
+            ),
             postMessage("+919876543210"),
-            {
-                trigger: messagesContain("Your phone number is validated. thank you!"),
-            },
-            {
-                // should ask for website now
-                trigger: messagesContain("Would you mind providing your website address?"),
-            },
+            waitForMessage("Your phone number is validated. thank you!"),
+            waitForMessage("Would you mind providing your website address?"),
             postMessage("https://www.fakeaddress.com"),
-            {
-                trigger: messagesContain(
-                    "Great, do you want to leave any feedback for us to improve?"
-                ),
-                // should ask for feedback now
-            },
+            waitForMessage("Great, do you want to leave any feedback for us to improve?"),
             postMessage("Yes, actually, I'm glad you asked!"),
             postMessage("I think it's outrageous that you ask for all my personal information!"),
             postMessage("I will be sure to take this to your manager!"),
@@ -122,73 +96,39 @@ registry.category("web_tour.tours").add("website_livechat_chatbot_flow_tour", {
                     await chatbotDelayProcessingDef.promise;
                 },
             },
-            {
-                // last step is displayed
-                trigger: messagesContain("Ok bye!"),
-            },
+            waitForMessage("Ok bye!"),
             {
                 trigger:
                     ".o-livechat-root:shadow .o-mail-ChatWindow-header [title='Restart Conversation']",
                 run: "click",
             },
+            waitForMessage("Restarting conversation..."),
+            waitForMessage("Hello! I'm a bot!", 1),
+            waitForMessage("I help lost visitors find their way.", 1),
+            waitForMessage("How can I help you?", 1),
             {
-                // check that conversation is properly restarting
-                trigger: messagesContain("Restarting conversation..."),
-            },
-            {
-                // check first welcome message is posted
-                trigger: messagesContain("Hello! I'm a bot!"),
-            },
-            {
-                // check second welcome message is posted
-                trigger: messagesContain("I help lost visitors find their way."),
-            },
-            {
-                // check question_selection message is posted
-                trigger: messagesContain("How can I help you?"),
-            },
-            {
-                trigger: '.o-livechat-root:shadow button:contains("Pricing Question")',
+                trigger: '.o-livechat-root:shadow button:text("Pricing Question")',
                 run: "click",
             },
-            {
-                // the path should now go towards 'Pricing Question (first part)'
-                trigger: messagesContain(
-                    "For any pricing question, feel free ton contact us at pricing@mycompany.com"
-                ),
-            },
-            {
-                // the path should now go towards 'Pricing Question (second part)'
-                trigger: messagesContain("We will reach back to you as soon as we can!"),
-            },
-            {
-                // should ask for website now
-                trigger: messagesContain("Would you mind providing your website address?"),
-            },
+            waitForMessage(
+                "For any pricing question, feel free ton contact us at pricing@mycompany.com"
+            ),
+            waitForMessage("We will reach back to you as soon as we can!"),
+            waitForMessage("Would you mind providing your website address?", 1),
             postMessage("no"),
-            {
-                trigger: messagesContain(
-                    "Great, do you want to leave any feedback for us to improve?"
-                ),
-            },
+            waitForMessage("Great, do you want to leave any feedback for us to improve?", 1),
             postMessage("no, nothing so say"),
-            {
-                trigger: messagesContain("Ok bye!"),
-                run: "click",
-            },
+            waitForMessage("Ok bye!", 1),
             {
                 trigger:
                     ".o-livechat-root:shadow .o-mail-ChatWindow-header [title='Restart Conversation']",
                 run: "click",
             },
             {
-                trigger:
-                    ".o-livechat-root:shadow button:contains(I want to speak with an operator)",
+                trigger: ".o-livechat-root:shadow button:text(I want to speak with an operator)",
                 run: "click",
             },
-            {
-                trigger: messagesContain("I will transfer you to a human."),
-            },
+            waitForMessage("I will transfer you to a human."),
             {
                 // Wait for the operator to be added: composer is only enabled at that point.
                 trigger: ".o-livechat-root:shadow .o-mail-Composer-input:enabled",
