alkuneemi/Luxara

[FIX] l10n_es_edi_facturae, l10n_es_edi_tbai: allow external references for corrective invoices/bills
To comply with Spanish EDI requirements (Facturae and TicketBAI),
corrective documents (RINV and RBILL) must strictly reference the
original invoice number and date. Currently, Odoo mandates a direct link
to an existing `account.move` record to populate these fields in the XML.

This creates a blocker for users who have migrated to Odoo and need to
issue credit notes against historical invoices or vendor bills that only
exist in legacy systems.

This commit introduces the flexibility to reference external documents by:
- Adding `original_invoice_credited` to store external invoice/bill names.
- Adding `l10n_es_tbai_original_invoice_date` for TicketBAI compliance
  when the source document is not present in the system.
- Allowing the Facturae reason code to be edited directly from the
  invoice/bill form, enabling corrections after the reversal wizard.

Technical choices:
- The `original_invoice_credited` field is automatically populated during
  reversal for internal moves to maintain UX, but remains manual for
  external references.
- Facturae templates now use this Char field instead of relying on the
  `name` of a linked record.
- Logic in `_reverse_moves` ensures that the invoicing period (start/end)
  is carried over from the original document to the refund.
- TicketBAI logic now validates that if no internal invoice is linked,
  both the external name and date are provided before posting.

task-6086457

addons/l10n_es_edi_facturae/tests/test_edi_xml.py
@@ -722,3 +722,78 @@ class TestEdiFacturaeXmls(AccountTestInvoicingCommon):
             with file_open("l10n_es_edi_facturae/tests/data/expected_out_invoice_round_2.xml", "rt") as f:
                 expected_xml = lxml.etree.fromstring(f.read().encode())
             self.assertXmlTreeEqual(lxml.etree.fromstring(generated_file), expected_xml)
+
+    def test_refund_invoice_corrective_invoice_number_prefers_linked_invoice_over_manual_original(self):
+        def get_invoice_numbers(xml_bytes):
+            tree = lxml.etree.fromstring(xml_bytes)
+            return (
+                tree.findtext('.//Invoice/InvoiceHeader/InvoiceNumber'),
+                tree.findtext('.//Invoice/InvoiceHeader/Corrective/InvoiceNumber'),
+            )
+
+        with freeze_time(self.frozen_today), \
+                patch(f"{self.certificate_module}.fields.Datetime.now", lambda x=None: self.frozen_today), \
+                self._mock_sha1():
+            invoice = self._create_invoice_es(
+                partner_id=self.partner_a.id,
+                move_type='out_invoice',
+                invoice_line_ids=[{'price_unit': 100.0, 'tax_ids': [self.tax.id]}],
+            )
+            invoice.action_post()
+
+            reversal_wizard = self.env['account.move.reversal'].create({
+                'move_ids': invoice.ids,
+                'journal_id': invoice.journal_id.id,
+                'date': self.frozen_today,
+                'company_id': self.company_data['company'].id,
+                'l10n_es_edi_facturae_reason_code': '01',
+            })
+            reversal_wizard.modify_moves()
+            refund = invoice.reversal_move_ids
+
+            xml_before, errors = refund._l10n_es_edi_facturae_render_facturae()
+            self.assertFalse(errors)
+            refund_number, corrective_number_before = get_invoice_numbers(xml_before)
+            self.assertEqual(corrective_number_before, invoice.name)
+
+            new_original = 'INV/MANUAL/00099'
+            refund.original_invoice_credited = new_original
+
+            xml_after, errors = refund._l10n_es_edi_facturae_render_facturae()
+            self.assertFalse(errors)
+            refund_number_after, corrective_number_after = get_invoice_numbers(xml_after)
+
+            self.assertEqual(corrective_number_after, invoice.name)
+            self.assertEqual(corrective_number_after, corrective_number_before)
+            self.assertNotEqual(corrective_number_after, new_original)
+            self.assertEqual(refund_number_after, refund_number)
+
+    def test_refund_invoice_manual_original_from_scratch_can_be_sent(self):
+        with freeze_time(self.frozen_today), \
+                patch(f"{self.certificate_module}.fields.Datetime.now", lambda x=None: self.frozen_today), \
+                self._mock_sha1():
+            refund = self._create_invoice_es(
+                partner_id=self.partner_a.id,
+                move_type='out_refund',
+                invoice_line_ids=[{'price_unit': 100.0, 'tax_ids': [self.tax.id]}],
+                original_invoice_credited='INV/MANUAL/00042',
+                l10n_es_edi_facturae_reason_code='01',
+                l10n_es_invoicing_period_start_date='2022-12-01',
+                l10n_es_invoicing_period_end_date='2022-12-31',
+            )
+            refund.action_post()
+
+            generated_file, errors = refund._l10n_es_edi_facturae_render_facturae()
+            self.assertFalse(errors)
+            self.assertTrue(generated_file)
+
+            tree = lxml.etree.fromstring(generated_file)
+            self.assertEqual(tree.findtext('.//Invoice/InvoiceHeader/Corrective/InvoiceNumber'), 'INV/MANUAL/00042')
+            self.assertEqual(tree.findtext('.//Invoice/InvoiceHeader/Corrective/ReasonCode'), '01')
+            self.assertEqual(tree.findtext('.//Invoice/InvoiceHeader/Corrective/TaxPeriod/StartDate'), '2022-12-01')
+            self.assertEqual(tree.findtext('.//Invoice/InvoiceHeader/Corrective/TaxPeriod/EndDate'), '2022-12-31')
+
+            wizard = self.create_send_and_print(refund)
+            result = wizard.action_send_and_print()
+            self.assertEqual(result['type'], 'ir.actions.act_window_close')
+            self.assertTrue(refund.l10n_es_edi_facturae_xml_id)

addons/l10n_es_edi_tbai/tests/test_move_reversal.py
@@ -4,6 +4,8 @@ from odoo.tests import tagged
 
 from .common import TestEsEdiTbaiCommonGipuzkoa
 
+from datetime import date
+
 
 @tagged('post_install', '-at_install', 'post_install_l10n')
 class TestSendAndPrintEdiGipuzkoa(TestEsEdiTbaiCommonGipuzkoa):
@@ -46,3 +48,37 @@ class TestSendAndPrintEdiGipuzkoa(TestEsEdiTbaiCommonGipuzkoa):
             credit_note.l10n_es_tbai_cancel()
 
         self.assertEqual(credit_note.l10n_es_tbai_state, 'cancelled')
+
+    def test_manual_refund_from_scratch_can_be_sent(self):
+        """Test that a manual refund (without linked original invoice) can be sent with TBAI."""
+        refund = self.env['account.move'].create({
+            'move_type': 'out_refund',
+            'invoice_date': date(2025, 1, 1),
+            'partner_id': self.partner_a.id,
+            'original_invoice_credited': 'INV/MANUAL/00042',
+            'l10n_es_tbai_refund_reason': 'R4',
+            'l10n_es_tbai_original_invoice_date': date(2024, 12, 1),
+            'invoice_line_ids': [(0, 0, {
+                'product_id': self.product_a.id,
+                'price_unit': 100.0,
+                'quantity': 1,
+                'tax_ids': [(6, 0, self._get_tax_by_xml_id('s_iva21b').ids)],
+            })],
+        })
+        refund.action_post()
+
+        # Verify refund fields are set
+        self.assertEqual(refund.original_invoice_credited, 'INV/MANUAL/00042')
+        self.assertEqual(refund.l10n_es_tbai_refund_reason, 'R4')
+        self.assertEqual(refund.l10n_es_tbai_original_invoice_date, date(2024, 12, 1))
+
+        # Send refund
+        refund_send_wizard = self._get_invoice_send_wizard(refund)
+        with patch(
+            'odoo.addons.l10n_es_edi_tbai.models.l10n_es_edi_tbai_document.requests.Session.request',
+            return_value=self.mock_response_post_invoice_success,
+        ):
+            refund_send_wizard.action_send_and_print()
+
+        self.assertEqual(refund.l10n_es_tbai_state, 'sent')
+        self.assertTrue(refund.l10n_es_tbai_post_file)

odoo/odoo

addons/l10n_es_edi_facturae/tests/test_edi_xml.py
@@ -722,3 +722,78 @@ class TestEdiFacturaeXmls(AccountTestInvoicingCommon):
             with file_open("l10n_es_edi_facturae/tests/data/expected_out_invoice_round_2.xml", "rt") as f:
                 expected_xml = lxml.etree.fromstring(f.read().encode())
             self.assertXmlTreeEqual(lxml.etree.fromstring(generated_file), expected_xml)
+
+    def test_refund_invoice_corrective_invoice_number_prefers_linked_invoice_over_manual_original(self):
+        def get_invoice_numbers(xml_bytes):
+            tree = lxml.etree.fromstring(xml_bytes)
+            return (
+                tree.findtext('.//Invoice/InvoiceHeader/InvoiceNumber'),
+                tree.findtext('.//Invoice/InvoiceHeader/Corrective/InvoiceNumber'),
+            )
+
+        with freeze_time(self.frozen_today), \
+                patch(f"{self.certificate_module}.fields.Datetime.now", lambda x=None: self.frozen_today), \
+                self._mock_sha1():
+            invoice = self._create_invoice_es(
+                partner_id=self.partner_a.id,
+                move_type='out_invoice',
+                invoice_line_ids=[{'price_unit': 100.0, 'tax_ids': [self.tax.id]}],
+            )
+            invoice.action_post()
+
+            reversal_wizard = self.env['account.move.reversal'].create({
+                'move_ids': invoice.ids,
+                'journal_id': invoice.journal_id.id,
+                'date': self.frozen_today,
+                'company_id': self.company_data['company'].id,
+                'l10n_es_edi_facturae_reason_code': '01',
+            })
+            reversal_wizard.modify_moves()
+            refund = invoice.reversal_move_ids
+
+            xml_before, errors = refund._l10n_es_edi_facturae_render_facturae()
+            self.assertFalse(errors)
+            refund_number, corrective_number_before = get_invoice_numbers(xml_before)
+            self.assertEqual(corrective_number_before, invoice.name)
+
+            new_original = 'INV/MANUAL/00099'
+            refund.l10n_es_original_invoice_credited = new_original
+
+            xml_after, errors = refund._l10n_es_edi_facturae_render_facturae()
+            self.assertFalse(errors)
+            refund_number_after, corrective_number_after = get_invoice_numbers(xml_after)
+
+            self.assertEqual(corrective_number_after, invoice.name)
+            self.assertEqual(corrective_number_after, corrective_number_before)
+            self.assertNotEqual(corrective_number_after, new_original)
+            self.assertEqual(refund_number_after, refund_number)
+
+    def test_refund_invoice_manual_original_from_scratch_can_be_sent(self):
+        with freeze_time(self.frozen_today), \
+                patch(f"{self.certificate_module}.fields.Datetime.now", lambda x=None: self.frozen_today), \
+                self._mock_sha1():
+            refund = self._create_invoice_es(
+                partner_id=self.partner_a.id,
+                move_type='out_refund',
+                invoice_line_ids=[{'price_unit': 100.0, 'tax_ids': [self.tax.id]}],
+                l10n_es_original_invoice_credited='INV/MANUAL/00042',
+                l10n_es_edi_facturae_reason_code='01',
+                l10n_es_invoicing_period_start_date='2022-12-01',
+                l10n_es_invoicing_period_end_date='2022-12-31',
+            )
+            refund.action_post()
+
+            generated_file, errors = refund._l10n_es_edi_facturae_render_facturae()
+            self.assertFalse(errors)
+            self.assertTrue(generated_file)
+
+            tree = lxml.etree.fromstring(generated_file)
+            self.assertEqual(tree.findtext('.//Invoice/InvoiceHeader/Corrective/InvoiceNumber'), 'INV/MANUAL/00042')
+            self.assertEqual(tree.findtext('.//Invoice/InvoiceHeader/Corrective/ReasonCode'), '01')
+            self.assertEqual(tree.findtext('.//Invoice/InvoiceHeader/Corrective/TaxPeriod/StartDate'), '2022-12-01')
+            self.assertEqual(tree.findtext('.//Invoice/InvoiceHeader/Corrective/TaxPeriod/EndDate'), '2022-12-31')
+
+            wizard = self.create_send_and_print(refund)
+            result = wizard.action_send_and_print()
+            self.assertEqual(result['type'], 'ir.actions.act_window_close')
+            self.assertTrue(refund.l10n_es_edi_facturae_xml_id)

addons/l10n_es_edi_tbai/tests/test_move_reversal.py
@@ -4,6 +4,8 @@ from odoo.tests import tagged
 
 from .common import TestEsEdiTbaiCommonGipuzkoa
 
+from datetime import date
+
 
 @tagged('post_install', '-at_install', 'post_install_l10n')
 class TestSendAndPrintEdiGipuzkoa(TestEsEdiTbaiCommonGipuzkoa):
@@ -26,6 +28,7 @@ class TestSendAndPrintEdiGipuzkoa(TestEsEdiTbaiCommonGipuzkoa):
                             })
         credit_note_id = move_reversal.refund_moves()['res_id']
         credit_note = self.env['account.move'].browse(credit_note_id)
+        credit_note.l10n_es_original_invoice_credited = invoice.name or "INV/2026/00001"
         credit_note.action_post()
 
         self.assertEqual(credit_note.l10n_es_tbai_refund_reason, 'R4')
@@ -46,3 +49,37 @@ class TestSendAndPrintEdiGipuzkoa(TestEsEdiTbaiCommonGipuzkoa):
             credit_note.l10n_es_tbai_cancel()
 
         self.assertEqual(credit_note.l10n_es_tbai_state, 'cancelled')
+
+    def test_manual_refund_from_scratch_can_be_sent(self):
+        """Test that a manual refund (without linked original invoice) can be sent with TBAI."""
+        refund = self.env['account.move'].create({
+            'move_type': 'out_refund',
+            'invoice_date': date(2025, 1, 1),
+            'partner_id': self.partner_a.id,
+            'l10n_es_original_invoice_credited': 'INV/MANUAL/00042',
+            'l10n_es_tbai_refund_reason': 'R4',
+            'l10n_es_tbai_original_invoice_date': date(2024, 12, 1),
+            'invoice_line_ids': [(0, 0, {
+                'product_id': self.product_a.id,
+                'price_unit': 100.0,
+                'quantity': 1,
+                'tax_ids': [(6, 0, self._get_tax_by_xml_id('s_iva21b').ids)],
+            })],
+        })
+        refund.action_post()
+
+        # Verify refund fields are set
+        self.assertEqual(refund.l10n_es_original_invoice_credited, 'INV/MANUAL/00042')
+        self.assertEqual(refund.l10n_es_tbai_refund_reason, 'R4')
+        self.assertEqual(refund.l10n_es_tbai_original_invoice_date, date(2024, 12, 1))
+
+        # Send refund
+        refund_send_wizard = self._get_invoice_send_wizard(refund)
+        with patch(
+            'odoo.addons.l10n_es_edi_tbai.models.l10n_es_edi_tbai_document.requests.Session.request',
+            return_value=self.mock_response_post_invoice_success,
+        ):
+            refund_send_wizard.action_send_and_print()
+
+        self.assertEqual(refund.l10n_es_tbai_state, 'sent')
+        self.assertTrue(refund.l10n_es_tbai_post_file)