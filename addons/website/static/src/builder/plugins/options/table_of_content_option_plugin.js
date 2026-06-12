alkuneemi/Luxara

[IMP] html_builder, website: allow anchor options via plugin resources
Previously, anchor creation relied on hardcoded selectors, which made it
difficult to adapt behavior for specific snippets like the Table of
Contents (TOC). As a result, users could not link to individual TOC
sections and were limited to linking the block as a whole.

This commit introduces resource-based configuration for anchor
visibility, allowing plugins to define allowed and excluded selectors.

With this, the TOC can expose anchors on individual sections, enabling
direct links to specific content, while other snippets can easily opt in
or out of anchor support without modifying core logic.

task-5438399

addons/website/static/src/builder/plugins/options/table_of_content_option_plugin.js
@@ -45,6 +45,9 @@ export class TableOfContentOptionPlugin extends Plugin {
         },
         is_unremovable_selectors: ".s_table_of_content_navbar_wrap, .s_table_of_content_main",
         content_not_editable_selectors: ".s_table_of_content_navbar",
+        anchor_excluded_selectors:
+            ".s_table_of_content, .s_table_of_content_navbar_wrap, .s_table_of_content_main",
+        anchor_force_allowed_selectors: ".s_table_of_content_main > *",
     };
 
     normalize(root) {

odoo/odoo

addons/website/static/src/builder/plugins/options/table_of_content_option_plugin.js
@@ -45,6 +45,9 @@ export class TableOfContentOptionPlugin extends Plugin {
         },
         is_unremovable_selectors: ".s_table_of_content_navbar_wrap, .s_table_of_content_main",
         content_not_editable_selectors: ".s_table_of_content_navbar",
+        anchor_allowed_selectors: ".s_table_of_content_main > *",
+        anchor_excluded_selectors:
+            ".s_table_of_content, .s_table_of_content_navbar_wrap, .s_table_of_content_main",
     };
 
     normalize(root) {