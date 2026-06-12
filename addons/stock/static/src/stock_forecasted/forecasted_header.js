a11d9b8fc6227c7d32fc11194211502732c5a7a0
Jean Schoenlaub <jesc@odoo.com>
2026-06-12 13:19:17 +0200

    [REF] web, stock: remove useTooltip hook
    Remove the useTooltip hook (tooltip_hook.js) which was redundant
    with the t-att-tooltip attribute and tooltip service.
    The hook was also using useLayoutEffect, which is deprecated in OWL3.
    
    NOTE:
    Also drop the unused tooltip service binding from ForecastedHeader
    that I found while contemplating refactoring the tooltip service as
    well (but which we will do in another PR)

--- addons/stock/static/src/stock_forecasted/forecasted_header.js
+++ addons/stock/static/src/stock_forecasted/forecasted_header.js
@@ -9,7 +9,6 @@ export class ForecastedHeader extends Component {
     setup(){
         this.orm = useService("orm");
         this.action = useService("action");
-        this.tooltip = useService("tooltip");
 
         this._formatFloat = (num) => formatFloat(num, { digits: [0, this.props.docs.precision] });
     }
