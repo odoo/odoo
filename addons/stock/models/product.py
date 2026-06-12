c9728a07b09c7b6c37cb7db13a4d6ee0bc83487b
Dirk Douglas <dido@odoo.com>
2026-06-12 16:34:15 +0000

    [PERF] stock: Search free_qty faster with stock.quant only
    **Problem:**
    Searching on free_qty can be slow when there are many stock.move and
    stock.quant. The search method uses _compute_quantities_dict() to compute the
    free_qty based on stock.move and stock.quant, which is expensive.
    
    **Solution:**
    A similar field, qty_available, has a faster search method based on stock.quant
    only. This method can be adapted to support fast searching on free_qty by also
    aggregating the reserved_qty and subtracting it from the qty_available.
    
    **Perf Table:**
    Record: product.template, with ~20k stock.picking and most products w/o moves.
    
    |Record count|Time before|Queries before|Time after|Queries after|
    |------------|-----------|--------------|----------|-------------|
    |10k         |2.14s      |214           |370ms     |45           |
    |50k         |6.06s      |644           |829ms     |50           |
    |100k        |11.42s     |1196          |1.05s     |46           |
    
    opw-6266096
    
    X-original-commit: 0e91a0611a15aed7d2b4ac1f95dfe3a7b6b18859

--- addons/stock/models/product.py
+++ addons/stock/models/product.py
@@ -491,6 +491,12 @@ class ProductProduct(models.Model):
         return self._search_product_quantity(operator, value, 'outgoing_qty')
 
     def _search_free_qty(self, operator, value):
+        if not ({'from_date', 'to_date'} & set(self.env.context.keys())):
+            product_ids = self._search_field_by_quants(
+                operator, value, self.env.context.get('lot_id'), self.env.context.get('owner_id'),
+                self.env.context.get('package_id'), field="free_qty"
+            )
+            return [('id', 'in', product_ids)]
         return self._search_product_quantity(operator, value, 'free_qty')
 
     def _search_product_quantity(self, operator, value, field):
@@ -500,6 +506,9 @@ class ProductProduct(models.Model):
         return [('id', 'in', ids)]
 
     def _search_qty_available_new(self, operator, value, lot_id=False, owner_id=False, package_id=False):
+        return self._search_field_by_quants(operator, value, lot_id, owner_id, package_id)
+
+    def _search_field_by_quants(self, operator, value, lot_id=False, owner_id=False, package_id=False, field="qty_available"):
         ''' Optimized method which doesn't search on stock.moves, only on stock.quants. '''
         op = PY_OPERATORS.get(operator)
         if not op:
@@ -517,17 +526,18 @@ class ProductProduct(models.Model):
             domain_quant.append(('owner_id', '=', owner_id))
         if package_id:
             domain_quant.append(('package_id', '=', package_id))
-        quants_groupby = self.env['stock.quant']._read_group(domain_quant, ['product_id'], ['quantity:sum'])
+        quants_groupby = self.env['stock.quant']._read_group(domain_quant, ['product_id'], ['quantity:sum', 'reserved_quantity:sum'])
 
         # check if we need include zero values in result
         include_zero = op(0.0, value)
 
         processed_product_ids = set()
-        for product, quantity_sum in quants_groupby:
+        for product, quantity_sum, reserved_sum in quants_groupby:
             product_id = product.id
+            field_sum = quantity_sum - reserved_sum if field == "free_qty" else quantity_sum
             if include_zero:
                 processed_product_ids.add(product_id)
-            if op(quantity_sum, value):
+            if op(field_sum, value):
                 product_ids.add(product_id)
 
         if include_zero:
