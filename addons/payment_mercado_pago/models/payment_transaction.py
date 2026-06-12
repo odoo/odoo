0873ef8a4106708213262d0792e7419004780903
Leila Kerroum Balaguer <leker@odoo.com>
2026-06-12 11:43:08 +0200

    [IMP] payment_mercado_pago: review ANKO 3

--- addons/payment_mercado_pago/models/payment_transaction.py
+++ addons/payment_mercado_pago/models/payment_transaction.py
@@ -33,14 +33,14 @@ class PaymentTransaction(models.Model):
         # Initiate the payment and retrieve the payment link data.
         idempotency_key = None
         if self.payment_method_code == "pix":
-            payload = self._mercado_pago_prepare_pix_payload()
             endpoint = "/v1/orders"
+            payload = self._mercado_pago_prepare_pix_payload()
             idempotency_key = payment_utils.generate_idempotency_key(
                 self, scope="payment_request_order"
             )
         else:
-            payload = self._mercado_pago_prepare_preference_request_payload()
             endpoint = "/checkout/preferences"
+            payload = self._mercado_pago_prepare_preference_request_payload()
         try:
             response_content = self._send_api_request(
                 "POST", endpoint, json=payload, idempotency_key=idempotency_key
@@ -55,6 +55,33 @@ class PaymentTransaction(models.Model):
             "api_url": api_url,
             "http_method": "get",
             "url_params": payment_utils.extract_url_params(api_url),
+        }
+
+    def _mercado_pago_prepare_pix_payload(self):
+        """Create the specific payload for Pix payment requests.
+
+        :return: The Pix specific request payload.
+        :rtype: dict
+        """
+        if not self.partner_email:
+            raise ValidationError(
+                _("An email is required to process PIX payments. Please update your profile.")
+            )
+
+        return {
+            "type": "online",
+            "total_amount": str(self.amount),
+            "external_reference": self.reference,
+            "processing_mode": "automatic",
+            "transactions": {
+                "payments": [
+                    {
+                        "amount": str(self.amount),
+                        "payment_method": {"id": "pix", "type": "bank_transfer"},
+                    }
+                ]
+            },
+            "payer": {"email": self.partner_email},
         }
 
     def _mercado_pago_prepare_preference_request_payload(self):
@@ -129,38 +156,16 @@ class PaymentTransaction(models.Model):
             "statement_descriptor": self.company_id.name,
         }
 
-    def _mercado_pago_prepare_pix_payload(self):
-        """Create the specific payload for Pix payment requests.
-
-        :return: The Pix specific request payload.
-        :rtype: dict
-        """
-        if not self.partner_email:
-            raise ValidationError(
-                _("An email is required to process PIX payments. Please update your profile.")
-            )
-
-        return {
-            "type": "online",
-            "total_amount": str(self.amount),
-            "external_reference": self.reference,
-            "processing_mode": "automatic",
-            "transactions": {
-                "payments": [
-                    {
-                        "amount": str(self.amount),
-                        "payment_method": {"id": "pix", "type": "bank_transfer"},
-                    }
-                ]
-            },
-            "payer": {"email": self.partner_email},
-        }
-
     def _mercado_pago_get_api_url(self, response_content):
         if self.payment_method_code == "pix":
-            return response_content["transactions"]["payments"][0]["payment_method"]["ticket_url"]
-
-        return response_content["init_point" if self.provider_id.is_live else "sandbox_init_point"]
+            if response_content["status"] == "action_required":
+                return response_content["transactions"]["payments"][0]["payment_method"][
+                    "ticket_url"
+                ]
+        else:
+            return response_content[
+                "init_point" if self.provider_id.is_live else "sandbox_init_point"
+            ]
 
     def _send_payment_request(self):
         """Override of `payment` to send a payment request to Mercado Pago.
