053ed473709aa0a1d7b410251e97909e2a57d3da
Jérémy Bazin (baje) <baje@odoo.com>
2026-06-12 13:10:06 +0000

    [FIX] l10n_fr_pdp_pos: avoid access restriction on POS entries
    Accounting users can access POS closing journal entries even when they
    do not have Point of Sale access rights.
    
    The PDP POS helper checked POS session/order links directly while
    computing e-reporting fields on account moves. This could raise an
    access error on `pos.session` for accounting users without POS rights.
    
    closes odoo/odoo#269961
    
    X-original-commit: 42bb359f552376ae088b84dde486a5b88c2beb2f
    Signed-off-by: de Wouters de Bouchout Jean-Benoît (jbw) <jbw@odoo.com>
    Signed-off-by: Jérémy Bazin (baje) <baje@odoo.com>

--- addons/l10n_fr_pdp_pos/models/account_move.py
+++ addons/l10n_fr_pdp_pos/models/account_move.py
@@ -5,7 +5,7 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     def _l10n_fr_pdp_reports_pos_is_transaction_entry(self):
-        return self.move_type == 'entry' and self.pos_session_ids and not self.reversed_pos_order_id
+        return self.move_type == 'entry' and self.sudo().pos_session_ids and not self.sudo().reversed_pos_order_id
 
     def _l10n_fr_pdp_is_sale(self):
         return super()._l10n_fr_pdp_is_sale() or self._l10n_fr_pdp_reports_pos_is_transaction_entry()
