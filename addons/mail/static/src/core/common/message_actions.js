088ba2824d7c76b0908950f6cc0f12f13d6ae511
Alex Kühn <aku@odoo.com>
2026-06-15 16:23:50 +0000

    [FIX] mail: no crash on message reaction on small screen + desktop
    Before this commit, when browser window is small while not on a
    mobile device, clicking on the message action "Add a reaction" would
    lead to the following crash:
    
    ```
    Cannot destructure property 'owner' of 'undefined' as it is undefined.
        at Proxy.onSelected
    ```
    
    This happens because cliking on this button on small screen would
    immediately trigger the complete showing of the emoji picker rather
    than just the quick menu. While this calls `action.onSelected()` and
    is expected to work [1], the problem is that this was passing the
    action definition rather than the action object as prop. As a result,
    `onSelected()` was using the definition and didn't pass the expected
    params that are destructed in the definition.
    
    This commit fixes the issue by passing the `action` object to
    `QuickReactionMenu` component, so that the `action.onSelected()` is
    properly passing the `action.params`.
    
    [1]: https://github.com/odoo/odoo/blob/19.0/addons/mail/static/src/core/common/quick_reaction_menu.js#L84
    
    X-original-commit: f0f155ebfe3cb0ff26bfb3001d9b9b5007a6e8e6

--- addons/mail/static/src/core/common/message_actions.js
+++ addons/mail/static/src/core/common/message_actions.js
@@ -31,9 +31,9 @@ export function registerMessageAction(id, definition) {
 
 registerMessageAction("reaction", {
     component: QuickReactionMenu,
-    componentProps: ({ message, owner }) => ({
+    componentProps: ({ action, message, owner }) => ({
+        action,
         message,
-        action: messageActionsRegistry.get("reaction"),
         messageActive: owner.isActive,
     }),
     componentCondition: ({ owner }) => !isMobileOS() && !owner.isMessageContextMenu,
