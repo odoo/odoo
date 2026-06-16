4dd7355126b684502a2e8af6c37fb6550e8b21c9
Sébastien (blse) <blse@odoo.com>
2026-06-16 11:00:46 +0000

    [IMP] html_builder: allow edition of date fields from website builder
    Since [website builder refactor], a date or datetime field that appears
    in the page is not editable from website builder.
    
    This commit re-introduces the ability to do so.
    
    [website builder refactor]: 9fe45e2b7ddbbfd0445ffe25a859e67a316d02b2
    
    task-6230582
    
    closes odoo/odoo#278171
    
    X-original-commit: 3ee740cbb9a74d18b0169dbd13e2e3d71c63c94f
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Sébastien Blondiau (blse) <blse@odoo.com>

--- addons/html_builder/static/src/plugins/date_time_field_plugin.js
+++ addons/html_builder/static/src/plugins/date_time_field_plugin.js
@@ -1,14 +1,51 @@
+import { BuilderAction } from "@html_builder/core/builder_action";
+import { BaseOptionComponent } from "@html_builder/core/base_option_component";
+import { useDomState } from "@html_builder/core/utils";
 import { Plugin } from "@html_editor/plugin";
+import { formatDate, formatDateTime, parseDateTime } from "@web/core/l10n/dates";
 import { registry } from "@web/core/registry";
+
+const { DateTime } = luxon;
+
+export class FieldDateTimeAction extends BuilderAction {
+    static id = "fieldDateTime";
+    getValue({ editingElement }) {
+        const dateTime = editingElement.dataset.modifiedDate
+            ? editingElement.textContent
+            : // If there is no date, oeOriginal is not set and
+            // oeOriginalWithFormat contains the format
+            editingElement.dataset.oeOriginal
+            ? editingElement.dataset.oeOriginalWithFormat
+            : "";
+        return dateTime && parseDateTime(dateTime).toUnixInteger().toString();
+    }
+    apply({ editingElement, value }) {
+        const format = { date: formatDate, datetime: formatDateTime }[
+            editingElement.dataset.oeType
+        ];
+        editingElement.dataset.modifiedDate = "true";
+        editingElement.textContent = format(DateTime.fromSeconds(parseInt(value)));
+    }
+}
+
+export class DateTimeFieldOption extends BaseOptionComponent {
+    static id = "date_time_field_option";
+    static template = "html_builder.DateTimeFieldOption";
+    setup() {
+        super.setup();
+        this.state = useDomState((el) => ({ fieldType: el.dataset.oeType }));
+    }
+}
+
+registry.category("builder-options").add(DateTimeFieldOption.id, DateTimeFieldOption);
 
 export class DateTimeFieldPlugin extends Plugin {
     static id = "dateTimeField";
     /** @type {import("plugins").BuilderResources} */
     resources = {
-        content_not_editable_selectors: [
-            "[data-oe-field][data-oe-type=date]",
-            "[data-oe-field][data-oe-type=datetime]",
-        ],
+        content_not_editable_selectors:
+            "[data-oe-field][data-oe-type=date], [data-oe-field][data-oe-type=datetime]",
+        builder_actions: { FieldDateTimeAction },
     };
 }
 registry.category("builder-plugins").add(DateTimeFieldPlugin.id, DateTimeFieldPlugin);
