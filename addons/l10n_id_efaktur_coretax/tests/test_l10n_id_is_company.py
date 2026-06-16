alkuneemi/Luxara

[IMP] l10n_id_efaktur_coretax: e-faktur and contact fields without customer PKP
- No customer PKP requirement for e-faktur download / invoice flag.
- Res.partner view: Coretax on Invoicing; document type/number in misc;
- Fields NIK,TKU removed.
- XML BuyerDocumentNumber when set (incl. TIN) + test.
- TKU in additional_identifiers; 1.1 pre-migrate from l10n_id_tku.
- Indonesia vat_label changed: NPWP/NIK.

task-6047036

addons/l10n_id_efaktur_coretax/tests/test_l10n_id_is_company.py
@@ -17,7 +17,7 @@ class L10nIDPartnerCompanyTest(AccountTestInvoicingCommon):
             'name': 'ID Parent Company',
             'country_id': indonesia.id,
             'vat': '1000000000014256',
-            'l10n_id_tku': '000000',
+            'additional_identifiers': {'TKU': '000000'},
         })
 
         # Parent Contact - Company (rule: starts with 0X)
@@ -25,7 +25,7 @@ class L10nIDPartnerCompanyTest(AccountTestInvoicingCommon):
             'name': 'ID Parent Company 0X',
             'country_id': indonesia.id,
             'vat': '0864099064063000',
-            'l10n_id_tku': '000000',
+            'additional_identifiers': {'TKU': '000000'},
         })
 
         # Parent Contact - Individual
@@ -34,7 +34,7 @@ class L10nIDPartnerCompanyTest(AccountTestInvoicingCommon):
             'name': 'ID Parent Individual',
             'country_id': indonesia.id,
             'vat': '3273061809970002',
-            'l10n_id_tku': '000000',
+            'additional_identifiers': {'TKU': '000000'},
             'parent_id': False,
         })
 
@@ -43,7 +43,7 @@ class L10nIDPartnerCompanyTest(AccountTestInvoicingCommon):
             'name': 'ID Branch',
             'country_id': indonesia.id,
             'parent_id': cls.partner_company.id,
-            'l10n_id_tku': '123456',
+            'additional_identifiers': {'TKU': '123456'},
         })
 
         # Child Contact - Individual

odoo/odoo

addons/l10n_id_efaktur_coretax/tests/test_l10n_id_is_company.py
@@ -17,7 +17,6 @@ class L10nIDPartnerCompanyTest(AccountTestInvoicingCommon):
             'name': 'ID Parent Company',
             'country_id': indonesia.id,
             'vat': '1000000000014256',
-            'l10n_id_tku': '000000',
         })
 
         # Parent Contact - Company (rule: starts with 0X)
@@ -25,7 +24,6 @@ class L10nIDPartnerCompanyTest(AccountTestInvoicingCommon):
             'name': 'ID Parent Company 0X',
             'country_id': indonesia.id,
             'vat': '0864099064063000',
-            'l10n_id_tku': '000000',
         })
 
         # Parent Contact - Individual
@@ -34,7 +32,7 @@ class L10nIDPartnerCompanyTest(AccountTestInvoicingCommon):
             'name': 'ID Parent Individual',
             'country_id': indonesia.id,
             'vat': '3273061809970002',
-            'l10n_id_tku': '000000',
+            'additional_identifiers': {'ID_TKU': '000000'},
             'parent_id': False,
         })
 
@@ -43,7 +41,7 @@ class L10nIDPartnerCompanyTest(AccountTestInvoicingCommon):
             'name': 'ID Branch',
             'country_id': indonesia.id,
             'parent_id': cls.partner_company.id,
-            'l10n_id_tku': '123456',
+            'additional_identifiers': {'ID_TKU': '123456'},
         })
 
         # Child Contact - Individual