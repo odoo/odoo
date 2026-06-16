fleetapps/odoo3

[IMP] calendar,*: show synchronized email address
*: {google,microsoft}_account, {google,microsoft,hr}_calendar

When hovering over the sync button while synchronized, show the currently linked
account/email. To fetch the email, additional claims had to be added to the
oauth requests for both Microsoft and Google, as the calendar ones did not
provide access to the user's email

Task-5346620

addons/google_calendar/static/tests/google_calendar.test.js
@@ -85,6 +85,7 @@ onRpc("/google_calendar/sync_data", () => ({ status: "no_new_event_from_google" 
 onRpc("check_synchronization_status", async () => ({ google_calendar: "sync_active" }));
 onRpc("get_attendee_detail", () => []);
 onRpc("get_default_duration", () => 3.25);
+onRpc("get_calendar_email", () => false);
 
 beforeEach(() => {
     mockDate("2016-12-12 08:00:00");

addons/hr_calendar/static/tests/calendar_homeworking.test.js
@@ -118,6 +118,7 @@ onRpc("get_state_selections", () => [
     ["needsAction", "Needs Action"],
 ]);
 onRpc("res.users", "read", () => [{ user: serverState.userId, employee_id: [{ employee_id: 1 }] }]);
+onRpc("res.users", "get_calendar_email", () => false);
 
 beforeEach(() => {
     mockDate("2020-12-10 15:00:00");

addons/microsoft_calendar/static/tests/microsoft_calendar.test.js
@@ -84,6 +84,7 @@ onRpc("/microsoft_calendar/sync_data", () => ({ status: "no_new_event_from_micro
 onRpc("check_synchronization_status", async () => ({ microsoft_calendar: "sync_active" }));
 onRpc("get_attendee_detail", () => []);
 onRpc("get_default_duration", () => 3.25);
+onRpc("get_calendar_email", () => false);
 
 beforeEach(() => {
     mockDate("2016-12-12 08:00:00");

odoo/odoo

addons/google_calendar/static/tests/google_calendar.test.js
@@ -85,6 +85,7 @@ onRpc("/google_calendar/sync_data", () => ({ status: "no_new_event_from_google" 
 onRpc("check_synchronization_status", async () => ({ google_calendar: "sync_active" }));
 onRpc("get_attendee_detail", () => []);
 onRpc("get_default_duration", () => 3.25);
+onRpc("get_calendar_sync_email", () => false);
 
 beforeEach(() => {
     mockDate("2016-12-12 08:00:00");

addons/hr_calendar/static/tests/calendar_homeworking.test.js
@@ -118,6 +118,7 @@ onRpc("get_state_selections", () => [
     ["needsAction", "Needs Action"],
 ]);
 onRpc("res.users", "read", () => [{ user: serverState.userId, employee_id: [{ employee_id: 1 }] }]);
+onRpc("res.users", "get_calendar_sync_email", () => false);
 
 beforeEach(() => {
     mockDate("2020-12-10 15:00:00");

addons/microsoft_calendar/static/tests/microsoft_calendar.test.js
@@ -84,6 +84,7 @@ onRpc("/microsoft_calendar/sync_data", () => ({ status: "no_new_event_from_micro
 onRpc("check_synchronization_status", async () => ({ microsoft_calendar: "sync_active" }));
 onRpc("get_attendee_detail", () => []);
 onRpc("get_default_duration", () => 3.25);
+onRpc("get_calendar_sync_email", () => false);
 
 beforeEach(() => {
     mockDate("2016-12-12 08:00:00");