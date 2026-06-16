8ed9de3201448ad7ac3a124e281f2e0cbcffe02c
Sébastien (blse) <blse@odoo.com>
2026-06-16 13:00:46 +0200

    [IMP] html_builder: allow edition of date fields from website builder
    Since [website builder refactor], a date or datetime field that appears
    in the page is not editable from website builder.
    
    This commit re-introduces the ability to do so.
    
    [website builder refactor]: 9fe45e2b7ddbbfd0445ffe25a859e67a316d02b2
    
    task-6230582

--- addons/html_builder/static/src/plugins/date_time_field_plugin.js
+++ addons/html_builder/static/src/plugins/date_time_field_plugin.js
@@ -1,5 +1,40 @@
+import { BuilderAction } from "@html_builder/core/builder_action";
+import { BaseOptionComponent, useDomState } from "@html_builder/core/utils";
 import { Plugin } from "@html_editor/plugin";
+import { formatDate, formatDateTime, parseDateTime } from "@web/core/l10n/dates";
 import { registry } from "@web/core/registry";
+
+const { DateTime } = luxon;
+
+export class FieldDateTimeAction extends BuilderAction {
+    static id = "fieldDateTime";
+    getValue({ editingElement }) {
+        const dateTime = editingElement.dataset.modifiedDate
+            ? editingElement.textContent
+            : editingElement.dataset.oeOriginalWithFormat;
+        return parseDateTime(dateTime).toUnixInteger().toString();
+    }
+    apply({ editingElement, value }) {
+        if (value) {
+            const format = { date: formatDate, datetime: formatDateTime }[
+                editingElement.dataset.oeType
+            ];
+            editingElement.dataset.modifiedDate = "true";
+            editingElement.textContent = format(DateTime.fromSeconds(parseInt(value)));
+        } else {
+            editingElement.textContent = editingElement.dataset.oeOriginalWithFormat;
+        }
+    }
+}
+
+export class DateTimeFieldOption extends BaseOptionComponent {
+    static template = "html_builder.DateTimeFieldOption";
+    static selector = "[data-oe-field][data-oe-type=date], [data-oe-field][data-oe-type=datetime]";
+    setup() {
+        super.setup();
+        this.state = useDomState((el) => ({ fieldType: el.dataset.oeType }));
+    }
+}
 
 export class DateTimeFieldPlugin extends Plugin {
     static id = "dateTimeField";
@@ -9,6 +44,8 @@ export class DateTimeFieldPlugin extends Plugin {
             "[data-oe-field][data-oe-type=date]",
             "[data-oe-field][data-oe-type=datetime]",
         ],
+        builder_options: DateTimeFieldOption,
+        builder_actions: { FieldDateTimeAction },
     };
 }
 registry.category("builder-plugins").add(DateTimeFieldPlugin.id, DateTimeFieldPlugin);
--- addons/html_builder/static/tests/date_field.test.js
+++ addons/html_builder/static/tests/date_field.test.js
@@ -1,5 +1,6 @@
 import { setupHTMLBuilder } from "@html_builder/../tests/helpers";
 import { expect, test, describe } from "@odoo/hoot";
+import { contains } from "@web/../tests/web_test_helpers";
 
 describe.current.tags("desktop");
 
@@ -10,4 +11,28 @@ test("should not allow edition of date and datetime fields", async () => {
         </time>`
     );
     expect(":iframe time").toHaveProperty("isContentEditable", false);
+});
+
+test("should allow changing datetime fields from the sidebar", async () => {
+    await setupHTMLBuilder(
+        `<time data-oe-model="blog.post" data-oe-id="3" data-oe-field="post_date" data-oe-type="datetime" data-oe-expression="blog_post.post_date" data-oe-original="2025-07-30 09:54:36" data-oe-original-with-format="07/30/2025 09:54:36" data-oe-original-tz="Europe/Brussels">
+            Jul 30, 2025
+        </time>`
+    );
+    await contains(":iframe time").click();
+    expect("[data-action-id=fieldDateTime] input").toHaveValue("07/30/2025 09:54:36");
+    await contains("[data-action-id=fieldDateTime] input").edit("09/22/2042 01:23:45");
+    expect(":iframe time").toHaveText("09/22/2042 01:23:45");
+});
+
+test("should allow changing date fields from the sidebar", async () => {
+    await setupHTMLBuilder(
+        `<time data-oe-model="blog.post" data-oe-id="3" data-oe-field="post_date" data-oe-type="date" data-oe-expression="blog_post.post_date" data-oe-original="2025-07-30" data-oe-original-with-format="07/30/2025">
+            Jul 30, 2025
+        </time>`
+    );
+    await contains(":iframe time").click();
+    expect("[data-action-id=fieldDateTime] input").toHaveValue("07/30/2025");
+    await contains("[data-action-id=fieldDateTime] input").edit("09/22/2042");
+    expect(":iframe time").toHaveText("09/22/2042");
 });
