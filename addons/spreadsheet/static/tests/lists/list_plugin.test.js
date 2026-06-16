704c2448c56f45043f041b18cf6b79854c19dd3c
Lucas Lefèvre (lul) <lul@odoo.com>
2026-06-16 10:12:19 +0000

    [FIX] spreadsheet: don't stringify x2many
    Steps to reproduce:
    - insert a sale.order list into a spreadsheet
    - change the locale to FR_fr
    - add the formula in A1:
    	=ODOO.LIST(1, 1, "invoice_ids.amount_total"
    - in A2:
    	=A1+1
    => error
    
    The value for the "amount_total" is stringified
    to something like "4.5" because of the `[].join(",")`
    but "4.5" is not interpreted as a number in the FR locale
    where the decimal separator is "," and it should be "4,5"
    
    With this commit, when there's a single record in the x2many,
    we fallback on the "normal" case and return the real value.
    
    When there are multiple values, we volontarily keep the current
    behavior. We can't "just format it" because we join with ","
    and it wouldn't work if "," is also the decimal separator.
    This is a known and accepted limitation, especially in stable
    
    task-6307092
    
    X-original-commit: 8d38066f9d67aeca1cf613fd20dcd725a1499595

--- addons/spreadsheet/static/tests/lists/list_plugin.test.js
+++ addons/spreadsheet/static/tests/lists/list_plugin.test.js
@@ -1423,6 +1423,56 @@ test("Chaining fields are fetched with the same web_search_read", async function
     expect.verifySteps(["web_search_read"]);
 });
 
+test("chained field in x2many with a single value", async function () {
+    Partner._fields.product_ids = fields.Many2many({
+        string: "Products",
+        relation: "product",
+        store: true,
+        searchable: true,
+    });
+    Partner._records = [
+        {
+            id: 1,
+            product_ids: [7],
+        },
+    ];
+    Product._records = [{ id: 7, pognon: 699.99, currency_id: 1 }];
+    const { model } = await createSpreadsheetWithList();
+    model.dispatch("UPDATE_LOCALE", { locale: TEST_LOCALES.fr_FR });
+    const listId = model.getters.getListIds()[0];
+    setCellContent(model, "A1", `=ODOO.LIST(${listId}, 1, "product_ids.pognon")`);
+    await animationFrame();
+    expect(getCellValue(model, "A1")).toBe(699.99);
+    expect(getEvaluatedCell(model, "A1").formattedValue).toBe("699,99€");
+});
+
+test("chained field in x2many with multiple values", async function () {
+    Partner._fields.product_ids = fields.Many2many({
+        string: "Products",
+        relation: "product",
+        store: true,
+        searchable: true,
+    });
+    Partner._records = [
+        {
+            id: 1,
+            product_ids: [7, 8],
+        },
+    ];
+    Product._records = [
+        { id: 7, pognon: 699.99, currency_id: 1 },
+        { id: 8, pognon: 499.99, currency_id: 1 },
+    ];
+    const { model } = await createSpreadsheetWithList();
+    model.dispatch("UPDATE_LOCALE", { locale: TEST_LOCALES.fr_FR });
+    const listId = model.getters.getListIds()[0];
+    setCellContent(model, "A1", `=ODOO.LIST(${listId}, 1, "product_ids.pognon")`);
+    await animationFrame();
+    // known limitation: the values are not localized.
+    expect(getCellValue(model, "A1")).toBe("699.99, 499.99");
+    expect(getEvaluatedCell(model, "A1").formattedValue).toBe("699.99, 499.99");
+});
+
 test("Chaining monetary fields includes the currency field", async function () {
     let initialLoad = true;
     const { model } = await createSpreadsheetWithList({
