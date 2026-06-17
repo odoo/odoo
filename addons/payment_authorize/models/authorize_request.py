odoo/odoo

commit c4556637e8eeef07ce6e3cc3b3b4cf28fa10e468

[FIX] payment_authorize: limit address length
Steps to reproduce:
- install payment_authorize module;
- complete a credit card payment using Authorize.net with more than 60 characters
    on any other field than first name, last name or company;
- confirm the payment.

Issue:
An error message appears.

Cause:
The Authorize.net API define the max length of information.
It is possible that some information exceeds the maximum length.
(https://apitest.authorize.net/xml/v1/schema/AnetApiSchema.xsd)

Solution:
Truncate information if the number of character is too large.

opw-6141441

X-original-commit: 5c293bc3214da98f4cdb48aaf39892214a3a02eb

addons/payment_authorize/models/authorize_request.py
@@ -174,11 +174,11 @@ class AuthorizeAPI:
                     'firstName': split_name[0][:50],
                     'lastName': split_name[1][:50],  # lastName is always required
                     'company': tx.partner_name[:50] if tx.partner_id.is_company else '',
-                    'address': tx.partner_address,
-                    'city': tx.partner_city,
-                    'state': tx.partner_state_id.name or '',
-                    'zip': tx.partner_zip,
-                    'country': tx.partner_country_id.name or '',
+                    'address': tx.partner_address[:60],
+                    'city': tx.partner_city[:40],
+                    'state': (tx.partner_state_id.name or '')[:40],
+                    'zip': tx.partner_zip[:20],
+                    'country': (tx.partner_country_id.name or '')[:60],
                 }
             }
 

commit 494c5c126a1ea1ed82eb944dd6c4a4b348caab0d

[FIX] payment_authorize: fix bill address computation
Fix bug introduced by commit https://github.com/odoo/odoo/commit/c4556637e8eeef07ce6e3cc3b3b4cf28fa10e468
that caused an error if an address field was not set, due to trying to
cut a False field. Now, unset fields are set to empty strings.

addons/payment_authorize/models/authorize_request.py
@@ -174,9 +174,9 @@ class AuthorizeAPI:
                     'lastName': split_name[1][:50],  # lastName is always required
                     'company': tx.partner_name[:50] if tx.partner_id.is_company else '',
                     'address': tx.partner_address[:60],
-                    'city': tx.partner_city[:40],
+                    'city': (tx.partner_city or '')[:40],
                     'state': (tx.partner_state_id.name or '')[:40],
-                    'zip': tx.partner_zip[:20],
+                    'zip': (tx.partner_zip or '')[:20],
                     'country': (tx.partner_country_id.name or '')[:60],
                 }
             }