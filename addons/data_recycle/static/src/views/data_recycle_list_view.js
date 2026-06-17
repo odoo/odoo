4a12c3c9e1148b46c256533787c566ef7dccc657
Jean Schoenlaub <jesc@odoo.com>
2026-06-17 15:03:41 +0200

    [REF] web: Do not merge data_recycle_list_view

--- addons/data_recycle/static/src/views/data_recycle_list_view.js
+++ addons/data_recycle/static/src/views/data_recycle_list_view.js
@@ -7,21 +7,21 @@ export class DataRecycleListController extends DataCleaningCommonListController 
     setup() {
         super.setup();
         useLayoutEffect(() => {
-            const selectedRecords = this.model.root.selection;
+            // const selectedRecords = this.model.root.selection;
 
-            // Check the active state of selected records
-            const allInactive = selectedRecords.every(r => r.data.active === false);
-            const allActive = selectedRecords.every(r => r.data.active === true);
+            // // Check the active state of selected records
+            // const allInactive = selectedRecords.every(r => r.data.active === false);
+            // const allActive = selectedRecords.every(r => r.data.active === true);
 
-            // Helper function to toggle the 'd-none' class
-            const toggleBtnVisibility = (selector, shouldShow) => {
-                const btn = document.querySelector(selector);
-                if (btn) btn.classList.toggle('d-none', !shouldShow);
-            };
+            // // Helper function to toggle the 'd-none' class
+            // const toggleBtnVisibility = (selector, shouldShow) => {
+            //     const btn = document.querySelector(selector);
+            //     if (btn) btn.classList.toggle('d-none', !shouldShow);
+            // };
 
-            toggleBtnVisibility('button[name="action_validate"]', allActive);
-            toggleBtnVisibility('button[name="action_discard"]', allActive);
-            toggleBtnVisibility('button[name="action_undiscard"]', allInactive);
+            // toggleBtnVisibility('button[name="action_validate"]', allActive);
+            // toggleBtnVisibility('button[name="action_discard"]', allActive);
+            // toggleBtnVisibility('button[name="action_undiscard"]', allInactive);
         });
     }
 };
