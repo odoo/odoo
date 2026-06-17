3eb10f037427e88a152c6b6eca7180c42a4e3945
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2026-06-17 11:35:29 +0000

    [FIX] l10n_es*: include pos entries in VAT record books
    Helpers for the enterprise PR
    
    opw-5862529
    
    closes odoo/odoo#277534
    
    X-original-commit: ab5cef4d3438889952a8dffdd4195903beedef77
    Related: odoo/enterprise#125011
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>
    Signed-off-by: Andrea Grazioso (agr) <agr@odoo.com>

--- addons/l10n_es_edi_tbai_pos/models/res_company.py
+++ addons/l10n_es_edi_tbai_pos/models/res_company.py
@@ -7,3 +7,7 @@ class ResCompany(models.Model):
     @api.model
     def _load_pos_data_fields(self, config_id):
         return super()._load_pos_data_fields(config_id) + ['l10n_es_tbai_is_enabled']
+
+    def _l10n_es_get_pos_edi_mode(self):
+        self.ensure_one()
+        return 'tbai' if self.l10n_es_tbai_is_enabled else super()._l10n_es_get_pos_edi_mode()
--- addons/l10n_es_edi_verifactu_pos/models/res_company.py
+++ addons/l10n_es_edi_verifactu_pos/models/res_company.py
@@ -7,3 +7,7 @@ class ResCompany(models.Model):
     @api.model
     def _load_pos_data_fields(self, config_id):
         return super()._load_pos_data_fields(config_id) + ['l10n_es_edi_verifactu_required']
+
+    def _l10n_es_get_pos_edi_mode(self):
+        self.ensure_one()
+        return 'verifactu' if self.l10n_es_edi_verifactu_required else super()._l10n_es_get_pos_edi_mode()
