odoo/odoo

commit 7c1d28c16697bdf76768e2174f025b06cbd4815a

[REV] website_sale: disable archiving of guest contacts
Archiving guest contacts upon SO validation breaks mail confirmations
for related pickings.

When a guest contact is archived, the ORM automatically filters it out
from any search
https://github.com/odoo/odoo/blob/e8a372b71dc91a3e3649e572380f59ee648c5bdc/odoo/orm/fields_relational.py#L673-L677
As a result, the partner is silently dropped from the `partner_ids`
Many2Many on the mail composer even though we do write it
https://github.com/odoo/odoo/blob/e8a372b71dc91a3e3649e572380f59ee648c5bdc/addons/mail/wizard/mail_compose_message.py#L538-L539
and the picking confirmation email is never sent.

A potential fix would have been to disable this filtering at the ORM
level but that would have impacted any flow that relies on archived
partners being excluded.

This reverts commit 3a20ff382d164f05d3d6b66e94318ed80aaa41cc.

This reverts commit 64d9ded9637286ef0cfd9e65ba7c60d4f48d6c16.

This reverts commit ef10f93b77263836815034e15bae6cddbd38c4f9.

opw-6232937

closes odoo/odoo#269570

X-original-commit: 5ee97ff5cc5c02bd6822bd3de834b01e6abec067
Related: odoo/enterprise#120269
Signed-off-by: Merlin Guillaume (megu) <megu@odoo.com>

addons/website_sale/models/__init__.py
@@ -8,7 +8,6 @@ from . import (
     ir_http,
     ir_module_module,
     payment_token,
-    payment_transaction,
     product_attribute,
     product_attribute_category,
     product_document,

addons/website_sale/models/payment_transaction.py
@@ -1,14 +1,0 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import models
-
-
-class PaymentTransaction(models.Model):
-    _inherit = 'payment.transaction'
-
-    def _send_invoice(self):
-        """Override of `sale` to archive guest contacts."""
-        super()._send_invoice()
-        self.sale_order_ids.filtered(
-            lambda so: so.state == "sale" and so.website_id
-        )._archive_partner_if_no_user()

addons/website_sale/models/sale_order.py
@@ -945,12 +945,7 @@ class SaleOrder(models.Model):
         self._recompute_taxes()
         self._recompute_prices()
 
-    def _validate_order(self):
-        super()._validate_order()
-        # After SO confirmation and email sending, archive customers without accounts
-        self.filtered('website_id')._archive_partner_if_no_user()
-
-    def _archive_partner_if_no_user(self):
+    def _archive_partner_if_no_user(self):  # TODO: remove in master
         """Archive SO customer if it has no linked users."""
         if (
             not self

addons/website_sale/tests/test_cart_payment.py
@@ -80,23 +80,3 @@ class WebsiteSaleCartPayment(PaymentHttpCommon, WebsiteSaleCommon):
                 salesperson,
                 "Salesperson should get assigned when sending payment confirmation mail",
             )
-
-    def test_payment_archives_customer_if_automatic_invoice(self):
-        # Async emails must be disabled otherwise invoice (and partner archiving) will be handled in
-        # a cron.
-        self.env["ir.config_parameter"].set_bool("sale.async_emails", False)
-        self.env["ir.config_parameter"].set_bool("sale.automatic_invoice", True)
-        self.tx._set_done()
-        self.tx._post_process()
-        self.assertFalse(self.cart.partner_id.active)
-
-    def test_payment_doesnt_archive_partner_if_manual_invoicing(self):
-        """Customers shouldn't be archived if automatic invoice is disabled, otherwise their address
-        won't be considered when sending the invoice later on."""
-        # Async emails must be disabled otherwise invoice (and partner archiving) will be handled in
-        # a cron.
-        self.env["ir.config_parameter"].set_bool("sale.async_emails", False)
-        self.env["ir.config_parameter"].set_bool("sale.automatic_invoice", False)
-        self.tx._set_done()
-        self.tx._post_process()
-        self.assertTrue(self.cart.partner_id.active)

addons/website_sale/tests/test_sale_order.py
@@ -63,54 +63,3 @@ class TestSaleOrder(WebsiteSaleCommon):
             self.cart.website_id,
             invoice.website_id,
         )
-
-    def test_unregistered_customer_archiving(self):
-        self.cart._archive_partner_if_no_user()
-        self.assertFalse(self.cart.partner_id.active)
-
-        # Customers with parent company record (if a parent_name is provided)
-        customer = self.env["res.partner"].create({"parent_name": "Company", "name": "Cuzco"})
-        billing = self.env["res.partner"].create({
-            "parent_id": customer.parent_id.id,
-            "type": "invoice",
-            "name": "Cuzco Billing",
-        })
-        cart = self._create_so(partner_id=customer.id)
-        self.assertEqual(cart.partner_invoice_id, billing)
-
-        cart._archive_partner_if_no_user()
-        self.assertFalse(customer.active)
-        self.assertFalse(billing.active)
-        self.assertFalse(customer.parent_id.active)
-
-        # Customers without a parent company.
-        customer = self.env["res.partner"].create({"name": "Simple"})
-        billing = self.env["res.partner"].create({
-            "parent_id": customer.id,
-            "type": "invoice",
-            "name": "Simple Billing",
-        })
-        cart = self._create_so(partner_id=customer.id)
-        self.assertEqual(cart.partner_invoice_id, billing)
-
-        cart._archive_partner_if_no_user()
-        self.assertFalse(customer.active)
-        self.assertFalse(billing.active)
-
-    def test_customer_archiving_if_has_user(self):
-        self._create_new_portal_user(login="whatever", partner_id=self.cart.partner_id.id)
-        self.cart._archive_partner_if_no_user()
-        self.assertTrue(
-            self.cart.partner_id.active, "Registered customers shouldn't be archived on payment"
-        )
-
-        # Case when SO is toward the parent company and one of its contact has a user
-        customer = self.env["res.partner"].create({"parent_name": "Company", "name": "Cuzco"})
-        self._create_new_portal_user(login=f"whatever-{customer.id}", partner_id=customer.id)
-        self.assertNotEqual(customer, customer.parent_id)
-
-        cart = self._create_so(partner_id=customer.parent_id.id)
-        cart._archive_partner_if_no_user()
-        self.assertTrue(
-            self.cart.partner_id.active, "Registered company shouldn't be archived if any contact has a user"
-        )

addons/website_sale_stock/tests/test_website_sale_stock_stock_notification.py
@@ -53,3 +53,15 @@ class TestStockNotificationProduct(WebsiteSaleStockCommon, HttpCase):
                 yield env(cr=cr)
         finally:
             env.invalidate_all()
+
+    def test_partner_email_confirmation(self):
+        """Partner receives email confirmation for a delivery if the setting is enabled."""
+        self.company.stock_move_email_validation = True
+        self.partner.email = 'test@example.com'
+        new_so = self._create_so()
+        new_so._validate_order()
+        new_so.picking_ids.button_validate()
+        self.assertTrue(
+            any(partner.email == self.partner.email
+            for partner in new_so.picking_ids.message_ids.notified_partner_ids
+        ))

commit 0aa71df650dfbb5e0785a3c258488bb173772f1c

[FIX] website_sale: fix model imports
The revert commit 7c1d28c16697bdf76768e2174f025b06cbd4815a accidentally
removed payment_transactions import from the website_sale models'
__init__.py, this puts it back.

addons/website_sale/models/__init__.py
@@ -10,6 +10,7 @@ from . import (
     ir_http,
     ir_module_module,
     payment_token,
+    payment_transaction,
     product_attribute,
     product_attribute_category,
     product_combo,