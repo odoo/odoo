5b6206789c0c40393c3f508be57a066823858c9e
gteboul <gute@odoo.com>
2026-06-18 11:49:41 +0200

    [FIX] account: ignore precision product unit
    When importing an xml where the quantity decimal precision is above 2, the price subtotal is wrong and does not correspond to the xml. (Find the xml on the ticket)
    
    This commit ignores the precision in precision get for product unit in those case
    f321338598a7a9c990a3cfd58f418ebc3b7421e6
    
    This commit backport this feature in 17.0
    
    opw-6276756

--- addons/account/models/decimal_precision.py
+++ addons/account/models/decimal_precision.py
@@ -5,6 +5,6 @@ class DecimalPrecision(models.Model):
 
     def precision_get(self, application):
         stackmap = self.env.cr.cache.get('account_disable_recursion_stack', {})
-        if application == 'Discount' and stackmap.get('ignore_discount_precision'):
+        if application in ('Discount', 'Product Unit of Measure') and stackmap.get('ignore_discount_precision'):
             return 13
         return super().precision_get(application)
