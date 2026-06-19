a294d4f1eef865274d57ad46b408fae99e436afa
Julien Coppin (jucop) <jucop@odoo.com>
2026-06-19 17:25:53 +0200

    [REF] bus: convert bus_parameters service to plugin

--- addons/im_livechat/static/src/embed/external/bus_parameters_service_patch.js
+++ addons/im_livechat/static/src/embed/external/bus_parameters_service_patch.js
@@ -1,13 +1,11 @@
-import { busParametersService } from "@bus/bus_parameters_service";
+import { BusParametersPlugin } from "@bus/bus_parameters_plugin";
 
 import { patch } from "@web/core/utils/patch";
 import { session } from "@web/session";
 
-patch(busParametersService, {
-    start() {
-        return {
-            ...super.start(...arguments),
-            serverURL: session.livechatData.serverUrl.replace(/\/+$/, ""),
-        };
+patch(BusParametersPlugin, {
+    setup() {
+        super.setup();
+        this.serverURL = session.livechatData.serverUrl.replace(/\/+$/, "");
     },
 });
