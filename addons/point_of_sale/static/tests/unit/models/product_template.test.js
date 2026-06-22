11aee432bbdccfd5a9179c99cd20c9c68fe0db5e
adgu-odoo <adgu@odoo.com>
2026-06-22 12:21:04 +0200

    [FIX] point_of_sale, pos_self_order: multi choice with 1 option
    Before this commit, when a product had a multi choice attribute with
    only one option, it was not possible to configure the product in the POS
    or in the self. This is a problem since multi choice are different from
    other attribute display type because their options are opttional.
    The user should thus be able to select if he wants the option or not
    so we should display the configurator even if there is only one option.

--- addons/point_of_sale/static/tests/unit/models/product_template.test.js
+++ addons/point_of_sale/static/tests/unit/models/product_template.test.js
@@ -13,3 +13,14 @@ test("product with single 'is_custom' attr is configurable", async () => {
     expect(ptv[0].is_custom).toBe(true);
     expect(!!product.isConfigurable()).toBe(true);
 });
+
+test("product with single 'multi' display_type attr with single choice is configurable", async () => {
+    const store = await setupPosEnv();
+    const product = store.models["product.template"].get(52);
+    const line = product.attribute_line_ids[0];
+    const ptv = line.product_template_value_ids;
+    expect(ptv.length).toBe(1);
+    expect(ptv[0].is_custom).toBe(false);
+    expect(line.attribute_id.display_type).toBe("multi");
+    expect(!!product.isConfigurable()).toBe(true);
+});
