31ec218c153d4a9f795ab5b07d28b0e9c8b76f13
Antoine Boonen <aboo@odoo.com>
2026-06-22 13:29:50 +0200

    [IMP] account: Don't always reverse CABA moves upon un-reconciliation
    Problem
    ---------
    Currenlty, once a CABA move is created through the bank reco widget, it
    is impossible to draft/unlink it, the move can only be reverted. This
    leads to noisy journal when users unreco - reco their CABA payment. Do
    this a few times and it gets impossible to audit.
    
    Objective
    ---------
    The objectives of this change are as followed:
    
    1. Allow for CABA moves to be unlinked once the payment is unreconciled.
    This is only allowed when the move is not locked behind HARD locks and
    tax lock.
    2. In the case the CABA move is locked behind mentioned locks, revert it
    as it currently work.
    3. Since the deletion/creation of moves can create holes in the CABA
    journal sequence (which is not really allowed in audits), it adds a Tax
    Return default check to warns the user in case of a hole sequence in the
    moves included in the Tax Return/End of the Year Statement.
    
    task-6226555

--- addons/account/tests/test_account_move_in_refund.py
+++ addons/account/tests/test_account_move_in_refund.py
@@ -1124,14 +1124,8 @@ class TestAccountMoveInRefundOnchanges(AccountTestInvoicingCommon):
         # unreconcile
         debit_aml = invoice.line_ids.filtered('debit')
         debit_aml.remove_move_reconcile()
-        # check caba move reverse is same as caba move with only debit/credit inverted
-        reversed_caba_move = self.env['account.move'].search([('reversed_entry_id', '=', caba_move.id)])
-        for value in expected_values:
-            value.update({
-                'debit': value['credit'],
-                'credit': value['debit'],
-            })
-        self.assertRecordValues(reversed_caba_move.line_ids, expected_values)
+        # unlocked caba moves should get deleted upon unreconciliation
+        self.assertFalse(caba_move.exists())
 
     def test_in_refund_with_down_payment_caba(self):
         tax_waiting_account = self.env['account.account'].create({
