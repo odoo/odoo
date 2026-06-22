3f618a431294ae18ba301f62e1655bcf14a1467b
Atovange <gito@odoo.com>
2026-06-22 07:04:54 +0000

    [FIX] portal: keep prettified message link on refresh in portal
    chatter
    
    Before this commit, a message link posted on a portal chatter would
    lose it's prettified link (introduced in [1]) upon page refresh.
    
    This happens because `prepareMessageBody` needs the linked message
    thread's `displayName` to create the prettified message link, which is not
    sent on `_portal_message_format`.
    
    This commit fixes the issue by sending the linked messages thread's name.
    
    [1] https://github.com/odoo/odoo/pull/221069
    
    task-6204819
    
    closes odoo/odoo#271427
    
    X-original-commit: fc99f578cf82595256977391c68829c505b66764
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>
    Signed-off-by: Giorgio Tocco (gito) <gito@odoo.com>

--- addons/portal/controllers/thread.py
+++ addons/portal/controllers/thread.py
@@ -133,6 +133,7 @@ class PortalWebClientController(WebclientController):
             return bool(has_access)
 
         res.attr("can_react", can_react)
+        res.attr("display_name")
         res.attr("hasReadAccess", lambda t: t.sudo(False).has_access("read"))
         res.one(
             "portal_partner",
