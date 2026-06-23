187c47544382dcbe9556caa644bdfcd3b0bb1c3a
Robin Engels (roen) <roen@odoo.com>
2026-06-23 15:15:05 +0000

    [FIX] point_of_sale,pos_loyalty: fix grouping key
    When computing taxes for the order, we group the base lines based on
    a grouping key. One of the components of this key is `is_refund`, and
    reward lines where considered as refund lines, which caused them to be
    grouped separately. This caused a discrepency between the tax amount
    computed in the frontend and the one computed in the backend.
    
    Steps to reproduce:
    -------------------
    * Create a 21% tax not included in price
    * Create a product with a price of 76.01 and the tax created above
    * Create a loyalty program with a 10% discount
    * Create a POS order with the product above and apply the loyalty
      program
    * Validate the order and generate the invoice
    > Observation: The invoice states that 0.01$ is still due
    
    Why the fix:
    ------------
    In `round_tax_details_tax_amounts`, we try to detect any delta with the
    expeced tax amount. To do that we group the base lines based on a
    grouping key. The grouping key `is_refund` was incorrectly set to `true`
    for reward lines, which caused them to not be grouped. The delta was
    then not detected and tax amounts were not adjusted. We now make sure
    not to consider reward lines as refund.
    
    opw-6052112
    
    closes odoo/odoo#275350
    
    X-original-commit: 4aea004091b064eb24a6b1c5d503b84ed06c9abf
    Signed-off-by: Stéphane Vanmeerhaeghe (stva) <stva@odoo.com>
    Signed-off-by: Robin Engels (roen) <roen@odoo.com>

--- addons/point_of_sale/models/pos_order_line.py
+++ addons/point_of_sale/models/pos_order_line.py
@@ -377,7 +377,7 @@ class PosOrderLine(models.Model):
             account = fiscal_position.map_account(account)
 
         is_refund_order = line.order_id.is_refund or line.order_id.amount_total < 0.0
-        is_refund_line = line.qty * line.price_unit < 0
+        is_refund_line = line.isRefund()
 
         lang = line.order_id.partner_id.lang or self.env.user.lang
         product_name = line.product_id.with_context(lang=lang).display_name
@@ -440,3 +440,6 @@ class PosOrderLine(models.Model):
         if not self.combo_line_ids:
             return get_total(self)
         return sum(get_total(cl) for cl in self.combo_line_ids)
+
+    def isRefund(self):
+        return self.qty * self.price_unit < 0
