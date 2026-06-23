8f70600dcd00d773da0cfe6b7e4486df13bdc854
Pedram (PEBR) <pebr@odoo.com>
2026-06-23 13:23:33 +0000

    [FIX] pos_online_payment: sync order when re-adding online payment
    When an online payment line is added, the order is synced to the server
    so the customer can pay it online. This sync was only performed when the
    order did not yet exist on the server (string id). As a result, once an
    order had already been synced, modifying it (e.g. increasing the amount)
    and adding the online payment line again did not push the new amount to
    the server.
    
    opw-6314690
    
    closes odoo/odoo#272572
    
    X-original-commit: f1026af24df42f0bd08d10259047488c022e9bc0
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Pedram Bi Ria (pebr) <pebr@odoo.com>

--- addons/pos_online_payment/static/src/overrides/pos_overrides/components/payment_screen/payment_screen.js
+++ addons/pos_online_payment/static/src/overrides/pos_overrides/components/payment_screen/payment_screen.js
@@ -9,7 +9,12 @@ import { serializeDateTime } from "@web/core/l10n/dates";
 
 patch(PaymentScreen.prototype, {
     async addNewPaymentLine(paymentMethod) {
-        if (paymentMethod.is_online_payment && typeof this.currentOrder.id === "string") {
+        // Sync the order to the server only for the first online payment line: syncing a
+        // draft order strips its online payment lines, wiping previously added ones.
+        const hasOnlinePaymentLine = this.paymentLines.some(
+            (line) => line.payment_method_id.is_online_payment
+        );
+        if (paymentMethod.is_online_payment && !hasOnlinePaymentLine) {
             this.currentOrder.date_order = serializeDateTime(luxon.DateTime.now());
             this.pos.addPendingOrder([this.currentOrder.id]);
             await this.pos.syncAllOrders();
