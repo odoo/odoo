40f20fce6f56bc839c299d46ae7d9336717093e6
rame-odoo <rame@odoo.com>
2026-06-23 17:28:16 +0530

    [IMP] hr_recruitment: add applicant smart button on employee form
    Purpose:
    - Improve navigation between Recruitment and Employees by allowing recruiters
      to easily access the applicant record from the employee form.
    
    This PR includes:
    - Added a new 'Applicant' smart button on the employee form for Recruitment
      Officers.
    - Displayed the linked applicant name on the smart button.
    - Redirected users directly to the linked applicant form view.
    - Displayed the smart button only when an applicant is linked to the employee.
    
    task-6313817

--- addons/hr_recruitment/models/hr_employee.py
+++ addons/hr_recruitment/models/hr_employee.py
@@ -7,6 +7,7 @@ class HrEmployee(models.Model):
     _inherit = "hr.employee"
 
     applicant_ids = fields.One2many('hr.applicant', 'employee_id', 'Applicants', groups="hr.group_hr_user")
+    applicant_name = fields.Char(compute="_compute_applicant_name", groups="hr.group_hr_user")
 
     def _get_partner_count_depends(self):
         return super()._get_partner_count_depends() + ['applicant_ids']
@@ -25,3 +26,22 @@ class HrEmployee(models.Model):
                     render_values={'applicant': employee_sudo.applicant_ids}
                 )
         return employees
+
+    @api.depends('applicant_ids.partner_name')
+    def _compute_applicant_name(self):
+        for employee in self:
+            if len(employee.applicant_ids) == 1:
+                employee.applicant_name = employee.applicant_ids.partner_name
+            else:
+                employee.applicant_name = employee.name
+
+    def action_open_applicant(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': self.env._('Applicant'),
+            'res_model': 'hr.applicant',
+            'view_mode': 'form',
+            'res_id': self.applicant_ids.id,
+            'target': 'current',
+        }
