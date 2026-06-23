bc694d3081ffc169d8161b0f1c6b28da8c5760a0
Robin Engels (roen) <roen@odoo.com>
2026-06-23 15:15:05 +0000

    [FIX] point_of_sale,pos_loyalty: fix grouping key
    When computing taxes for the order, we group the base lines based on
    a grouping key. One of the components of this key is `is_refund`, and
    reward lines where considered as refund lines, which caused them to be
    grouped separately. This caused a discrepency between the tax amount
    computed in the frontend and the one computed in the backend.
    
    Steps to reproduce:
    -------------------
    * Create a 21% tax not included in price
    * Create a product with a price of 76.01 and the tax created above
    * Create a loyalty program with a 10% discount
    * Create a POS order with the product above and apply the loyalty
      program
    * Validate the order and generate the invoice
    > Observation: The invoice states that 0.01$ is still due
    
    Why the fix:
    ------------
    In `round_tax_details_tax_amounts`, we try to detect any delta with the
    expeced tax amount. To do that we group the base lines based on a
    grouping key. The grouping key `is_refund` was incorrectly set to `true`
    for reward lines, which caused them to not be grouped. The delta was
    then not detected and tax amounts were not adjusted. We now make sure
    not to consider reward lines as refund.
    
    opw-6052112
    
    X-original-commit: 8e9ae84edf0c7edd7e0e72c8cc33f4099c2569f0

--- addons/pos_loyalty/static/src/app/models/pos_order_line.js
+++ addons/pos_loyalty/static/src/app/models/pos_order_line.js
@@ -94,4 +94,7 @@ patch(PosOrderline.prototype, {
             this._e_wallet_program_id === orderline._e_wallet_program_id
         );
     },
+    isRefund() {
+        return super.isRefund(...arguments) && !this.is_reward_line;
+    },
 });
