94b1c7ad861b555860f6aec60d6bf21056e9c2e5
Krzysztof Magusiak (krma) <krma@odoo.com>
2026-06-23 20:11:06 +0000

    [FIX] core: preload dbs in gevent server
    The code to set the registry size was moved to `preload_registries`.
    The gevent server does not preload registries and thus does not set the
    registries size. Instead of moving the code again, we can preload
    registries in the gevent server.
    
    closes odoo/odoo#271754
    
    X-original-commit: 00ece8e0dd2f3820affbe3b4b1ea4545feaf345a
    Signed-off-by: Raphael Collet <rco@odoo.com>
    Signed-off-by: Krzysztof Magusiak (krma) <krma@odoo.com>

--- odoo/service/server.py
+++ odoo/service/server.py
@@ -908,6 +908,8 @@ class GeventServer(CommonServer):
             super().stop()
 
     def run(self, preload, stop):
+        if rc := preload_registries(preload):
+            return rc
         self.start()
         self.stop()
         _logger.info('Gevent server stopped')
