c3a7c9e7bb0eb9bc9f2eb07396a2e4cd3a45e0f7
Mathieu Walravens <wama@odoo.com>
2026-06-23 09:07:30 +0200

    [FIX] base: continue download on filename error
    Before this commit:
    Downloading a file with a filename field which you do not have access
    returns with a 403.
    
    After this commit:
    The filename falls back to the default name to ensure smooth download.
    
    opw-6446124

--- odoo/addons/base/models/ir_binary.py
+++ odoo/addons/base/models/ir_binary.py
@@ -158,7 +158,9 @@ class IrBinary(models.AbstractModel):
             if filename:
                 stream.download_name = filename
             elif filename_field in record:
-                stream.download_name = record[filename_field]
+                field = record._fields[filename_field]
+                if 'name' in filename_field or record.sudo(False).has_field_access(field, 'read'):
+                    stream.download_name = record[filename_field]
             if not stream.download_name:
                 stream.download_name = f'{record._table}-{record.id}-{field_name}'
 
