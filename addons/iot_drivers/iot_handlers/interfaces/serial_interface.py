d8d076a09422048c4dfade957174b48716347c09
orma-odoo <orma@odoo.com>
2026-06-23 08:45:03 +0000

    [FIX] iot_drivers: fix ghost serial driver
    Quick disconnects/reconnects of serial devices crash the serial driver thread, leaving "ghost" processes that cause deadlocks. It's way faster than the main 3s discovery loop, leading to the interface not seeing the device left then came back.
    
    This fix adds a health check to SerialInterface.get_devices(): if a driver thread is dead, it is excluded from the discovery list. This triggers Odoo's native removal flow to cleanly shut down the stale connection, allowing the device to auto-recover on the next poll cycle.
    
    opw-6201161, opw-6122057
    
    closes odoo/odoo#271727
    
    X-original-commit: 0094a6a7eec670f935f4ffd8d4cc585965dc0c73
    Signed-off-by: Louis Travaux (lotr) <lotr@odoo.com>
    Signed-off-by: Maxime Orban (orma) <orma@odoo.com>

--- addons/iot_drivers/iot_handlers/interfaces/serial_interface.py
+++ addons/iot_drivers/iot_handlers/interfaces/serial_interface.py
@@ -4,16 +4,27 @@ from serial.tools.list_ports import comports
 
 from odoo.addons.iot_drivers.tools.system import IS_WINDOWS
 from odoo.addons.iot_drivers.interface import Interface
+from odoo.addons.iot_drivers.main import iot_devices
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class SerialInterface(Interface):
     allow_unsupported = True
 
     def get_devices(self):
+        to_remove = set()
+        for identifier, driver in iot_devices.items():
+            if driver.connection_type == self.connection_type and not driver.is_alive():
+                _logger.warning("Driver for %s is dead (Thread not alive). Forcing removal.", identifier)
+                to_remove.add(identifier)
+
         serial_devices = {
             port.device: {'identifier': port.device}
             for port in comports()
-            if IS_WINDOWS or port.device != '/dev/ttyAMA10'
+            if (IS_WINDOWS or port.device != '/dev/ttyAMA10') and port.device not in to_remove
             # RPI 5 uses ttyAMA10 as a console serial port for system messages: odoo interprets it as scale -> avoid it
         }
+
         return serial_devices
