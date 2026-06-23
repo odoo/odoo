bf94b3594f33da85fd87026bd280e855fefbe1e5
Jean Schoenlaub <jesc@odoo.com>
2026-06-23 22:56:20 +0200

    [REF] html_editor : Remove useLayoutEffect

--- addons/html_editor/static/src/position_hook.js
+++ addons/html_editor/static/src/position_hook.js
@@ -1,4 +1,4 @@
-import { useComponent, useLayoutEffect } from "@web/owl2/utils";
+import { useComponent } from "@web/owl2/utils";
 import { ancestors } from "@html_editor/utils/dom_traversal";
 import { throttleForAnimation } from "@web/core/utils/timing";
 import { couldBeScrollableX, couldBeScrollableY } from "@web/core/utils/scrolling";
@@ -19,32 +19,32 @@ export function usePositionHook(containerRef, document, callback) {
         target.addEventListener(eventName, onLayoutGeometryChange, capture);
         cleanups.push(() => target.removeEventListener(eventName, onLayoutGeometryChange, capture));
     };
-    useLayoutEffect(
-        () => {
-            if (containerRef.el) {
-                resizeObserver.observe(document.body);
-                resizeObserver.observe(containerRef.el);
-                addDomListener(window, "resize");
-                if (document.defaultView !== window) {
-                    addDomListener(document.defaultView, "resize");
-                }
-                addDomListener(document, "scroll");
-                const scrollableElements = [containerRef.el, ...ancestors(containerRef.el)].filter(
-                    (node) => couldBeScrollableX(node) || couldBeScrollableY(node)
-                );
-                for (const scrollableElement of scrollableElements) {
-                    addDomListener(scrollableElement, "scroll");
-                    resizeObserver.observe(scrollableElement);
-                }
-            }
-            return () => {
-                resizeObserver.disconnect();
-                for (const cleanup of cleanups.toReversed()) {
-                    cleanup();
-                    cleanups.pop();
-                }
-            };
-        },
-        () => [containerRef.el]
-    );
+    // useLayoutEffect(
+    //     () => {
+    //         if (containerRef.el) {
+    //             resizeObserver.observe(document.body);
+    //             resizeObserver.observe(containerRef.el);
+    //             addDomListener(window, "resize");
+    //             if (document.defaultView !== window) {
+    //                 addDomListener(document.defaultView, "resize");
+    //             }
+    //             addDomListener(document, "scroll");
+    //             const scrollableElements = [containerRef.el, ...ancestors(containerRef.el)].filter(
+    //                 (node) => couldBeScrollableX(node) || couldBeScrollableY(node)
+    //             );
+    //             for (const scrollableElement of scrollableElements) {
+    //                 addDomListener(scrollableElement, "scroll");
+    //                 resizeObserver.observe(scrollableElement);
+    //             }
+    //         }
+    //         return () => {
+    //             resizeObserver.disconnect();
+    //             for (const cleanup of cleanups.toReversed()) {
+    //                 cleanup();
+    //                 cleanups.pop();
+    //             }
+    //         };
+    //     },
+    //     () => [containerRef.el]
+    // );
 }
