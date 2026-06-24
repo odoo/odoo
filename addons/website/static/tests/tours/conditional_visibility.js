335220a3f73f264f141f15bafc7a28b2ea90a0b0
Sébastien (blse) <blse@odoo.com>
2026-06-24 09:09:34 +0000

    [FIX] website: wait before drag element in tour conditional visibility
    The tour `conditional_visibility_4` has non-deterministic failure, that
    appears to be caused by the `drag_and_drop` step dragging the element
    that was the target before the click of the previous step.
    
    This commit adds a step to ensure the target is the expected element
    before the "drag" step starts.
    
    runbot-242425
    
    X-original-commit: 3de95ba4c772c1b98d27519f0d2b059045d38ede

--- addons/website/static/tests/tours/conditional_visibility.js
+++ addons/website/static/tests/tours/conditional_visibility.js
@@ -201,6 +201,10 @@ registerWebsitePreviewTour(
         // Click on the "Banner" snippet.
         ...clickOnSnippet(snippets[1]),
         {
+            content: "Wait for the target to be the banner (by looking at its options)",
+            trigger: ".options-container[data-container-title=Banner]",
+        },
+        {
             content: "Drag the 'Banner' snippet to the end of the page",
             trigger: ".o_overlay_options button.o_move_handle",
             run: "drag_and_drop :iframe #wrapwrap footer",
