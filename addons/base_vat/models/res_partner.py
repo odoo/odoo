ad799c4e1281ba36581daffbfa24e2c43c2bba7b
pssh-odoo <pssh@odoo.com>
2026-06-24 17:44:30 +0530

    [IMP] base_vat,l10n_uz: adjust TIN and display VAT ID in invoices
    This commit adds `company_register` VAT Registry (VAT ID) to the invoice header
    for [legal reasons](https://lex.uz/ru/docs/4948595#5225819). Also, the condition
    for `TIN` has been updated to be 14 digits if the contact is a person and 9
    digits if it is a company.
    
    task-6205255

--- addons/base_vat/models/res_partner.py
+++ addons/base_vat/models/res_partner.py
@@ -81,7 +81,7 @@ _ref_vat = {
     'tr': _lt('11111111111 (NIN) or 2222222222 (VKN)'),
     'ua': _lt('12345678 or UA12345678 (EDRPOU), 1234567890 (RNOPP) or 123456789012 (IPN)'),
     'uy': _lt("Example: '219999830019' (format: 12 digits, all numbers, valid check digit)"),
-    'uz': _lt('XXXXXXXXX [9 digits]'),
+    'uz': _lt('123456789 (TIN) or 12345678901234 (PINFL)'),
     've': 'V-12345678-1, V123456781, V-12.345.678-1',
     'xi': 'XI123456782',
     'sa': _lt('310175397400003 [Fifteen digits, first and last digits should be "3"]'),
@@ -687,7 +687,11 @@ class ResPartner(models.Model):
         )
 
     def check_vat_uz(self, vat):
-        return len(vat) == 9 and vat.isdigit()
+        if not vat.isdigit():
+            return False
+        if self.is_company:
+            return len(vat) == 9
+        return len(vat) == 14
 
     def check_vat_ve(self, vat):
         # https://tin-check.com/en/venezuela/
