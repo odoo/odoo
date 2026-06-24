6eb150c6a886f505ffc2a8b24657b2221ce80298
guce <guce@odoo.com>
2026-06-24 14:34:57 +0200

    [IMP] mass_mailing, html_editor: preview target selector
    This commit alters the preview record selector pager present in
    the mass_mailing mobile preview view to allow users to manually
    select a recipient, instead of rotating through the available
    recipients.
    
    This commit also introduces an alternate-color version of the
    HTML editor phone_svg image, to be used in the dark theme preview.
    
    task-6321624

---
+++ addons/mass_mailing/static/src/components/mailing_preview_iframe/mass_mailing_preview_record_field.js
@@ -0,0 +1,5 @@
+import { MailPreviewRecordField } from "@mail/views/web/fields/mail_preview_record_field/mail_preview_record_field";
+
+export class MassMailingPreviewRecordField extends MailPreviewRecordField {
+    static template = "mass_mailing.MassMailingPreviewRecordField";
+}
