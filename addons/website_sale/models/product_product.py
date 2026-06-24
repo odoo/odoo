0cc611190dcbda5f161d84fe738af867103fc0af
Mahdi Alijani (malj) <malj@odoo.com>
2026-06-24 11:43:53 +0000

    [FIX] website_sale: fix rating chatter tb on website
    Issue:
    ---
    Adding a rating with a message on website is causing TB as the portal
    user doesn't have the access to send rating message.
    
    opw-6316142
    
    closes odoo/odoo#277258
    
    X-original-commit: b11bef8c724cc6c0c9d063d45be553caa4f92d24
    Signed-off-by: Mohammadmahdi Alijani (malj) <malj@odoo.com>

--- addons/website_sale/models/product_product.py
+++ addons/website_sale/models/product_product.py
@@ -114,3 +114,14 @@ class Product(models.Model):
             fiscal_position_sudo.map_tax(product_taxes),
             self,
         )
+
+    @api.model
+    def _get_mail_message_access(self, res_ids, operation, model_name=None):
+        if (
+            (not model_name or model_name == 'product.product')
+            and operation == 'create'
+            and not self.env.user._is_internal()
+            and not self.env['website'].is_view_active('website_sale.product_comment')
+        ):
+            return 'write'
+        return super()._get_mail_message_access(res_ids, operation, model_name=model_name)
