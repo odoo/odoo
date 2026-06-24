5b5c5ac283cf9147e5d68e91070359b3b49abe9f
Tyler Landivar (tylan) <tylan@odoo.com>
2026-06-24 21:05:34 +0000

    [FIX] event, website_event_track: fix incorrect ICS description frontend
    Steps to reproduce:
    
    - Make sure you have 2 or more languages installed
    - Install events and website app
    - For the website, apply the languages you have in settings
    - Make sure website default language is different from backend language
    - Create an event
    - Go to the event's website page through the smart button
    - Switch the language of the frontend to one that is different from backend
    - Translate (alter) the description for this language
    - Click on the outlook icon to download the ics
    
    Current Behavior:
    The ics will contain a description based on the backend's chosen language
    
    Expected Behavior:
    The ics will contain a description based on the website's chosen language.
    If there's no website, it will use the browser's language
    
    Clarification:
    The current behavior is confusing for users as they will expect the ics to match
    the description of the current webpage which is dependent on the frontend's
    selected language. This current behavior also contradicts the google calendar
    behavior which grabs the description based on the frontend's selected language.
    
    The endpoints for retrieving the ics file now utilize the website=True flag to
    take advantage of IrHttp._match which will automatically apply a standard
    and dynamic heuristic including the frontend language, if set. This will ensure
    matching website language when clicking the event's ics link on the website.
    
    opw-6235320
    
    X-original-commit: a30e378d6b8d8006bb9a995d8e5f9854a972d98d

--- addons/event/controllers/main.py
+++ addons/event/controllers/main.py
@@ -11,12 +11,8 @@ from odoo.tools import consteq, format_datetime
 
 class EventController(Controller):
 
-    @route(['''/event/<model("event.event"):event>/ics'''], type='http', auth="public")
+    @route(['''/event/<model("event.event"):event>/ics'''], type='http', auth="public", website=True, sitemap=False)
     def event_ics_file(self, event, **kwargs):
-        lang = request.env.context.get('lang', request.env.user.lang)
-        if request.env.user._is_public():
-            lang = request.cookies.get('frontend_lang')
-        event = event.with_context(lang=lang)
         slot_id = int(kwargs['slot_id']) if kwargs.get('slot_id') else False
         files = event._get_ics_file(slot=request.env['event.slot'].sudo().browse(slot_id))
         if not event.id in files:
