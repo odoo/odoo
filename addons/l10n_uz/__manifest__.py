890fb3654beba0bb12d64020786a608a673b58b0
pssh-odoo <pssh@odoo.com>
2026-06-24 12:14:30 +0000

    [IMP] base_vat, l10n_uz: adjust TIN and display VAT ID in invoices
    This commit adds `company_register` VAT Registry (VAT ID) to the invoice header
    for [legal reasons](https://lex.uz/ru/docs/4948595#5225819). Also, the condition
    for `TIN` has been updated to be 14 digits if the contact is a person and 9
    digits if it is a company.
    
    Upgrade PR: https://github.com/odoo/upgrade/pull/10679
    task-6205255
    
    X-original-commit: 0673caeb596a9b56c10b15a4957e023986c43336

--- addons/l10n_uz/__manifest__.py
+++ addons/l10n_uz/__manifest__.py
@@ -20,6 +20,7 @@ Uzbekistan accounting chart and localization.
         'data/account.account.tag.csv',
         'views/res_company_views.xml',
         'views/res_partner_views.xml',
+        'views/report_invoice.xml',
     ],
     'author': 'Odoo S.A.',
     'license': 'LGPL-3',
