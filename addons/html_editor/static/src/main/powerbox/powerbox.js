fa85b5a2c2e75c16adcc52faf285d09f5b9d3b4c
Jean Schoenlaub <jesc@odoo.com>
2026-06-24 21:37:37 +0200

    [REF] html_editor : Replace useLayoutEffect with OWL3 API

--- addons/html_editor/static/src/main/powerbox/powerbox.js
+++ addons/html_editor/static/src/main/powerbox/powerbox.js
@@ -1,5 +1,5 @@
 import { useExternalListener, useRef } from "@web/owl2/utils";
-import { Component, onPatched } from "@odoo/owl";
+import { Component, onPatched, useListener } from "@odoo/owl";
 
 /**
  * @todo @phoenix i think that most of the "control" code in this component
@@ -32,15 +32,14 @@ export class Powerbox extends Component {
         // If necessary attach the same listener on the document on which
         // the powerbox is mounted, serving the same purpose:
         // do not trigger re-renderings when we are scrolling the powerbox
-        // useLayoutEffect(
-        //     (ownDoc, propsDoc) => {
-        //         if (ownDoc && propsDoc && ownDoc !== propsDoc) {
-        //             ownDoc.addEventListener("mousemove", onMouseMove);
-        //             return () => ownDoc.removeEventListener("mousemove", onMouseMove);
-        //         }
-        //     },
-        //     () => [ref.el?.ownerDocument, this.props.document]
-        // );
+        useListener(
+            () => {
+                const ownDoc = ref.el?.ownerDocument;
+                return ownDoc && ownDoc !== this.props.document ? ownDoc : null;
+            },
+            "mousemove",
+            onMouseMove
+        );
     }
 
     get commands() {
