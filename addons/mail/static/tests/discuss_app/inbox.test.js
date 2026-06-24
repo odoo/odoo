alkuneemi/Luxara

[FIX] mail: cannot add reaction/reply on thread without write access
Before this commit it was possible to add a reaction or reply to a
message without write access on the thread. This leads to an access
error.

This commit remove the add reaction if the user doesn't have read access
on the thread and the replyTo action he doesn't have write access

The condition to show the reply button is also splitted between
`canReplyTo` and the `reply-to` message action.

This commit merge and clean both condition into `canReplyTo` and add a
new one on `thread.canPostMessage`.

Task-6071789

addons/mail/static/tests/discuss_app/inbox.test.js
@@ -166,9 +166,11 @@ test('"reply to" composer should send message if message replied to is not a not
 
 test("show subject of message in Inbox", async () => {
     const pyEnv = await startServer();
+    const fakeId = pyEnv["res.fake"].create({ name: "my-channel" });
     const messageId = pyEnv["mail.message"].create({
         body: "not empty",
-        model: "discuss.channel",
+        model: "res.fake",
+        res_id: fakeId,
         needaction: true,
         subject: "Salutations, voyageur",
     });
@@ -185,10 +187,12 @@ test("show subject of message in Inbox", async () => {
 
 test("show subject of message in history", async () => {
     const pyEnv = await startServer();
+    const fakeId = pyEnv["res.fake"].create({ name: "my-record" });
     const messageId = pyEnv["mail.message"].create({
         body: "not empty",
-        model: "discuss.channel",
+        model: "res.fake",
         subject: "Salutations, voyageur",
+        res_id: fakeId,
     });
     pyEnv["mail.notification"].create({
         is_read: true,
@@ -204,11 +208,11 @@ test("show subject of message in history", async () => {
 
 test("subject should not be shown when subject is the same as the thread name", async () => {
     const pyEnv = await startServer();
-    const channelId = pyEnv["discuss.channel"].create({ name: "Salutations, voyageur" });
+    const fakeId = pyEnv["res.fake"].create({ name: "Salutations, voyageur" });
     const messageId = pyEnv["mail.message"].create({
         body: "not empty",
-        model: "discuss.channel",
-        res_id: channelId,
+        model: "res.fake",
+        res_id: fakeId,
         needaction: true,
         subject: "Salutations, voyageur",
     });

odoo/odoo

addons/mail/static/tests/discuss_app/inbox.test.js
@@ -166,16 +166,19 @@ test('"reply to" composer should send message if message replied to is not a not
 
 test("show subject of message in Inbox", async () => {
     const pyEnv = await startServer();
+    const channelId = pyEnv["discuss.channel"].create({ name: "my-channel" });
     const [messageId1, messageId2] = pyEnv["mail.message"].create([
         {
             body: "not empty",
             model: "discuss.channel",
+            res_id: channelId,
             needaction: true,
             subject: "Salutations, voyageur",
         },
         {
             body: "",
             model: "discuss.channel",
+            res_id: channelId,
             needaction: true,
             subject: "Hello, wanderer",
         },
@@ -203,10 +206,12 @@ test("show subject of message in Inbox", async () => {
 
 test("show subject of message in history", async () => {
     const pyEnv = await startServer();
+    const fakeId = pyEnv["res.fake"].create({ name: "my-record" });
     const messageId = pyEnv["mail.message"].create({
         body: "not empty",
-        model: "discuss.channel",
+        model: "res.fake",
         subject: "Salutations, voyageur",
+        res_id: fakeId,
     });
     pyEnv["mail.notification"].create({
         is_read: true,
@@ -222,11 +227,11 @@ test("show subject of message in history", async () => {
 
 test("subject should not be shown when subject is the same as the thread name", async () => {
     const pyEnv = await startServer();
-    const channelId = pyEnv["discuss.channel"].create({ name: "Salutations, voyageur" });
+    const fakeId = pyEnv["res.fake"].create({ name: "Salutations, voyageur" });
     const messageId = pyEnv["mail.message"].create({
         body: "not empty",
-        model: "discuss.channel",
-        res_id: channelId,
+        model: "res.fake",
+        res_id: fakeId,
         needaction: true,
         subject: "Salutations, voyageur",
     });