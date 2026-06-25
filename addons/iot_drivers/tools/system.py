6c0f50bdcf9c77946f7b9648156d9f125ac52f1c
Louis Travaux <lotr@odoo.com>
2026-06-25 08:29:29 +0000

    [FIX] iot_drivers: add `http_interface` to conf
    v19 WIoT Boxes will be listening on localhost when updating
    to v19.1+, we need to ensure they use `http_interface = 0.0.0.0`
    after the update.
    
    X-original-commit: 11deecd32004680057bbe900e7bef169a573603a

--- addons/iot_drivers/tools/system.py
+++ addons/iot_drivers/tools/system.py
@@ -103,7 +103,7 @@ def get_version(detailed_version=False):
 
     version = IOT_CHAR + image_version
     if detailed_version:
-        # Note: on windows IoT, the `release.version` finish with the build date
+        # Note: on Windows IoT, the `release.version` finish with the build date
         version += f"-{odoo_release.version}"
         if IS_RPI:
             commit_hash = git("rev-parse", "--short", "HEAD")
@@ -112,11 +112,11 @@ def get_version(detailed_version=False):
     return version
 
 
-def update_conf(values, section='iot.box'):
+def update_conf(values: dict, section: str = "iot.box"):
     """Update odoo.conf with the given key and value.
 
-    :param dict values: key-value pairs to update the config with.
-    :param str section: The section to update the key-value pairs in (Default: iot.box).
+    :param values: key-value pairs to update the config with.
+    :param section: The section to update the key-value pairs in (Default: `iot.box`).
     """
     _logger.debug("Updating odoo.conf with values: %s", values)
     conf = get_conf()
@@ -132,12 +132,12 @@ def update_conf(values, section='iot.box'):
         conf.write(f)
 
 
-def get_conf(key=None, section='iot.box'):
+def get_conf(key: str | None = None, section: str = "iot.box"):
     """Get the value of the given key from odoo.conf, or the full config if no key is provided.
 
     :param key: The key to get the value of.
-    :param section: The section to get the key from (Default: iot.box).
-    :return: The value of the key provided or None if it doesn't exist, or full conf object if no key is provided.
+    :param section: The section to get the key from (Default: `iot.box`).
+    :return: The value of the key provided or `None` if it doesn't exist, or full conf object if no key is provided.
     """
     conf = configparser.RawConfigParser()
     conf.read(path_file("odoo.conf"))
@@ -152,7 +152,7 @@ def _get_identifier():
     elif IS_TEST:
         return 'test_identifier'
 
-    # On windows, get motherboard's uuid (serial number isn't reliable as it's not always present)
+    # On Windows, get motherboard's uuid (serial number isn't reliable as it's not always present)
     command = ['powershell', '-Command', "(Get-CimInstance Win32_ComputerSystemProduct).UUID"]
     p = subprocess.run(command, stdout=subprocess.PIPE, check=False)
     identifier = get_conf('generated_identifier')  # Fallback identifier if windows does not return mb UUID
