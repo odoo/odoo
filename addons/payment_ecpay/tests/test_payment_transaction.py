odoo/odoo

commit 89cb64bc2890c1caf66cd7846c978d45ef16eb11

[ADD] payment_ecpay: add ECPay payment provider
This change integrates a new payment provider: ECPay for payments in Taiwan.

Supported Payment methods of the provider:
- card: credit/debit card
- bank transfer
- wechat pay
- [NEW] Convenience Stores (eg. 7/11, OK Mart etc.)
- [NEW] Mobile Wallet (eg. iPASS Money, Jkopay etc.)
- [NEW] TWQR

Note:
- Tokenization and Refund is not implemented in this commit.

Task-5168802

closes odoo/odoo#235069

Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>

addons/payment_ecpay/const.py
@@ -1,0 +1,63 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from types import MappingProxyType
+
+# Routes for ECPay redirection and webhook.
+PAYMENT_RETURN_ROUTE = "/payment/ecpay/return"
+WEBHOOK_ROUTE = "/payment/ecpay/webhook"
+
+# The currency supported by ECPay, in ISO 4217 format.
+SUPPORTED_CURRENCY = "TWD"
+
+# The codes of the payment methods to activate when ECPay is activated.
+DEFAULT_PAYMENT_METHOD_CODES = (
+    # Primary payment methods.
+    "card",
+    "wechat_pay",
+    "cvs",
+    "bank_transfer",
+    "mobile_wallet",
+    "twqr",
+    # Brand payment methods.
+    "visa",
+    "mastercard",
+    "jcb",
+    "unionpay",
+    "ok_mart",
+    "hi_life",
+    "family_mart",
+    "ipass_money",
+)
+
+# Mapping of payment method codes to ECPay codes.
+PAYMENT_METHODS_MAPPING = MappingProxyType({
+    "card": ("Credit",),
+    "wechat_pay": ("WeiXin",),
+    "cvs": ("CVS", "BARCODE"),
+    "bank_transfer": ("ATM", "WebATM"),
+    "mobile_wallet": ("DigitalPayment",),
+    "twqr": ("TWQR",),
+    "none": (
+        "BNPL",
+        "ApplePay",
+    ),  # Unused but required to compute the ignored methods to send to ECPay
+})
+
+# Mapping of Odoo payment method codes to the values returned by ECPay notifications.
+# This map intentionally contains only methods where the returned value lets us pick a brand PM
+# in Odoo (e.g., convenience stores and specific wallets). Other methods are still supported via
+# PAYMENT_METHODS_MAPPING, but their response values are either generic (e.g., Credit_CreditCard)
+# or not mapped to brand PMs in Odoo (e.g., ATM/WebATM bank variants).
+PAYMENT_METHODS_RESPONSE_MAPPING = MappingProxyType({
+    "ok_mart": "CVS_OK",
+    "hi_life": "CVS_HILIFE",
+    "family_mart": "CVS_FAMILY",
+    "ipass_money": "DigitalPayment_IPASS",
+})
+
+# Mapping IETF language tags (e.g.: 'fr-BE') to ECPay language codes.
+# If a language tag is not listed, the country code prefix can serve as fallback.
+LANGUAGE_CODES_MAPPING = MappingProxyType({"en": "ENG", "ja_JP": "JPN", "ko": "KOR", "zh": "CHI"})
+
+# Mapping of transaction states to ECPay success codes.
+SUCCESS_CODE_MAPPING = MappingProxyType({"done": ("1", "2", "10100073")})

addons/payment_ecpay/models/payment_transaction.py
@@ -1,0 +1,138 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from zoneinfo import ZoneInfo
+
+from odoo import api, models
+from odoo.tools.urls import urljoin
+
+from odoo.addons.payment import utils as payment_utils
+from odoo.addons.payment_ecpay import const
+
+
+class PaymentTransaction(models.Model):
+    _inherit = "payment.transaction"
+
+    @api.model
+    def _compute_reference(self, provider_code, prefix=None, separator="-", **kwargs):
+        """Override of `payment` to ensure that ECPay requirements for references are satisfied.
+
+        ECPay requirements for references are as follows:
+        - References must be unique at provider level for a given merchant account.
+          This is satisfied by singularizing the prefix with the current datetime. If two
+          transactions are created simultaneously, `_compute_reference` ensures the uniqueness of
+          references by suffixing a sequence number.
+        - References must be at most 20 characters long.
+        - References must consist of a combination of letters and numbers in Chinese and English.
+        - Special characters (e.g., #, @, &) are not permitted.
+
+        :param str provider_code: The code of the provider handling the transaction.
+        :param str prefix: The custom prefix used to compute the full reference.
+        :param str separator: The custom separator used to separate the prefix from the suffix.
+        :return: The unique reference for the transaction.
+        :rtype: str
+        """
+        if provider_code != "ecpay":
+            return super()._compute_reference(
+                provider_code, prefix=prefix, separator=separator, **kwargs
+            )
+
+        prefix = payment_utils.singularize_reference_prefix(separator="", max_length=20)
+        return super()._compute_reference(provider_code, prefix=prefix, separator="", **kwargs)
+
+    def _get_specific_rendering_values(self, processing_values):
+        """Override of `payment` to return ECPay-specific rendering values.
+
+        Note: self.ensure_one() from `_get_processing_values`.
+
+        :param dict processing_values: The generic and specific processing values of the
+                                       transaction.
+        :return: The dict of provider-specific processing values.
+        :rtype: dict
+        """
+        if self.provider_code != "ecpay":
+            return super()._get_specific_rendering_values(processing_values)
+
+        base_url = self.provider_id.get_base_url()
+        all_payment_methods = {
+            item for sublist in const.PAYMENT_METHODS_MAPPING.values() for item in sublist
+        }
+        ignored_payment_methods = "#".join(
+            all_payment_methods.difference(const.PAYMENT_METHODS_MAPPING[self.payment_method_code])
+        )
+
+        rendering_values = {
+            "MerchantID": self.provider_id.ecpay_merchant_id,
+            "MerchantTradeNo": self.reference,
+            "MerchantTradeDate": (
+                self.create_date
+                .replace(tzinfo=ZoneInfo("UTC"))
+                .astimezone(ZoneInfo("Asia/Taipei"))
+                .strftime("%Y/%m/%d %H:%M:%S")
+            ),
+            "PaymentType": "aio",
+            "TotalAmount": int(self.amount),
+            "TradeDesc": "ECPay from Odoo",
+            "ItemName": self.reference,
+            "ReturnURL": urljoin(base_url, const.WEBHOOK_ROUTE),
+            "ChoosePayment": "ALL",
+            "EncryptType": "1",
+            "ClientBackURL": urljoin(base_url, const.PAYMENT_RETURN_ROUTE),
+            "OrderResultURL": urljoin(base_url, const.PAYMENT_RETURN_ROUTE),
+            "IgnorePayment": ignored_payment_methods,
+            "Language": payment_utils.get_language_code(
+                self.env.context.get("lang", "en_US"), const.LANGUAGE_CODES_MAPPING
+            ),
+        }
+        rendering_values.update({
+            "CheckMacValue": self.provider_id._ecpay_calculate_signature(rendering_values),
+            "api_url": self.provider_id._ecpay_get_api_url(),
+        })
+        return rendering_values
+
+    @api.model
+    def _extract_reference(self, provider_code, payment_data):
+        """Override of `payment` to extract the reference from the payment data."""
+        if provider_code != "ecpay":
+            return super()._extract_reference(provider_code, payment_data)
+
+        return payment_data.get("MerchantTradeNo")
+
+    def _extract_amount_data(self, payment_data):
+        """Override of `payment` to extract the amount and currency from the payment data."""
+        if self.provider_code != "ecpay":
+            return super()._extract_amount_data(payment_data)
+
+        amount = float(payment_data.get("TradeAmt"))
+        return {"amount": amount, "currency_code": self.currency_id.name}
+
+    def _apply_updates(self, payment_data):
+        """Override of `payment` to update the transaction based on the payment data."""
+        if self.provider_code != "ecpay":
+            return super()._apply_updates(payment_data)
+
+        # Update the provider's reference.
+        self.provider_reference = payment_data.get("TradeNo")
+
+        # Update the payment method.
+        payment_method_code = payment_data.get("PaymentType")
+        payment_method = self.env["payment.method"]._get_from_code(
+            payment_method_code, mapping=const.PAYMENT_METHODS_RESPONSE_MAPPING
+        )
+        self.payment_method_id = payment_method or self.payment_method_id
+
+        # Update the payment state.
+        return_code = payment_data.get("RtnCode")
+        if not return_code:
+            self._set_error(self.env._("Received data with missing return code."))
+        elif return_code in const.SUCCESS_CODE_MAPPING["done"]:
+            self._set_done()
+        else:
+            return_message = payment_data.get("RtnMsg")
+            self._set_error(
+                self.env._(
+                    "An error occurred (return code %(return_code)s; return message"
+                    " %(return_message)s).",
+                    return_code=return_code,
+                    return_message=return_message,
+                )
+            )

addons/payment_ecpay/tests/test_payment_transaction.py
@@ -1,0 +1,100 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from zoneinfo import ZoneInfo
+
+from odoo.tests import tagged
+from odoo.tools import urls
+
+from odoo.addons.payment_ecpay import const
+from odoo.addons.payment_ecpay.tests.common import EcpayCommon
+
+
+@tagged("post_install", "-at_install")
+class TestPaymentTransaction(EcpayCommon):
+    def test_reference_uses_only_alphanumeric_chars(self):
+        """The computed reference must be alphanumeric."""
+        reference = self.env["payment.transaction"]._compute_reference(provider_code="ecpay")
+        self.assertRegex(reference, r"^[a-zA-Z0-9]+$")
+
+    def test_reference_length_is_at_most_20_chars(self):
+        """The computed reference must be at most 20 characters long."""
+        reference = self.env["payment.transaction"]._compute_reference(provider_code="ecpay")
+        self.assertTrue(len(reference) <= 20)
+
+    def test_no_item_missing_from_rendering_values(self):
+        """Test that the rendered values are conform to the transaction fields."""
+        # Create a transaction with known values
+        localhost_url = "http://127.0.0.1:8069"
+        self.env["ir.config_parameter"].set_param("web.base.url", localhost_url)
+
+        tx = self._create_transaction(
+            "redirect", payment_method_id=self.env.ref("payment.payment_method_card").id
+        )
+        # The ignored payment methods are computed from the mapping
+        all_payment_methods = {
+            item for methods in const.PAYMENT_METHODS_MAPPING.values() for item in methods
+        }
+        ignored_payment_methods = "#".join(
+            all_payment_methods.difference(const.PAYMENT_METHODS_MAPPING[tx.payment_method_code])
+        )
+        expected_values = {
+            "MerchantID": self.provider.ecpay_merchant_id,
+            "MerchantTradeNo": tx.reference,
+            "MerchantTradeDate": (
+                tx.create_date
+                .replace(tzinfo=ZoneInfo("UTC"))
+                .astimezone(ZoneInfo("Asia/Taipei"))
+                .strftime("%Y/%m/%d %H:%M:%S")
+            ),
+            "PaymentType": "aio",
+            "TotalAmount": int(tx.amount),
+            "TradeDesc": "ECPay from Odoo",
+            "ItemName": tx.reference,
+            "ReturnURL": urls.urljoin(localhost_url, const.WEBHOOK_ROUTE),
+            "ChoosePayment": "ALL",
+            "EncryptType": "1",
+            "ClientBackURL": urls.urljoin(localhost_url, const.PAYMENT_RETURN_ROUTE),
+            "OrderResultURL": urls.urljoin(localhost_url, const.PAYMENT_RETURN_ROUTE),
+            "IgnorePayment": ignored_payment_methods,
+            "Language": "ENG",
+        }
+        # The CheckMacValue must be computed using the provider's method
+        expected_values["CheckMacValue"] = tx.provider_id._ecpay_calculate_signature(
+            expected_values
+        )
+        expected_values["api_url"] = "https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5"
+        self.assertEqual(tx._get_specific_rendering_values(None), expected_values)
+
+    def test_extract_reference_finds_reference(self):
+        """Test that the transaction reference is found in the payment data."""
+        tx = self._create_transaction("redirect")
+        reference = self.env["payment.transaction"]._extract_reference(
+            "ecpay", self.payment_result_data
+        )
+        self.assertEqual(tx.reference, reference)
+
+    def test_extract_amount_data_returns_amount_and_currency(self):
+        """Test that the amount and currency are returned from the payment data."""
+        tx = self._create_transaction("redirect")
+        amount_data = tx._extract_amount_data(self.payment_result_data)
+        self.assertDictEqual(
+            amount_data, {"amount": tx.amount, "currency_code": tx.currency_id.name}
+        )
+
+    def test_apply_updates_confirms_transaction(self):
+        """Test that the transaction state is set to 'done' on successful payment."""
+        tx = self._create_transaction("redirect")
+        tx._apply_updates(self.payment_result_data)
+        self.assertEqual(tx.state, "done")
+
+    def test_apply_updates_sets_provider_reference(self):
+        """Test that the provider reference is updated from the payment data."""
+        tx = self._create_transaction("redirect")
+        tx._apply_updates(self.payment_result_data)
+        self.assertEqual(tx.provider_reference, self.payment_result_data["TradeNo"])
+
+    def test_apply_updates_sets_payment_method(self):
+        """Test that the payment method is updated from the payment data."""
+        tx = self._create_transaction("redirect")
+        tx._apply_updates(self.payment_result_data)
+        self.assertEqual(tx.payment_method_id, self.env.ref("payment.payment_method_ipass_money"))

addons/payment_ecpay/tests/test_processing_flows.py
@@ -1,0 +1,84 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from unittest.mock import patch
+
+from werkzeug.exceptions import Forbidden
+
+from odoo.tests import tagged
+from odoo.tools import mute_logger
+
+from odoo.addons.payment.tests.http_common import PaymentHttpCommon
+from odoo.addons.payment_ecpay import const
+from odoo.addons.payment_ecpay.controllers.main import EcpayController
+from odoo.addons.payment_ecpay.tests.common import EcpayCommon
+
+
+@tagged("post_install", "-at_install")
+class TestProcessingFlows(EcpayCommon, PaymentHttpCommon):
+    def test_compute_signature_returns_correct_signature(self):
+        signature = self.provider._ecpay_calculate_signature(self.payment_result_data)
+        self.assertEqual(signature, self.webhook_payment_data_signature)
+
+    @mute_logger("odoo.addons.payment_ecpay.controllers.main")
+    def test_returning_from_payment_triggers_processing(self):
+        """Test that receiving a valid redirect notification triggers the processing of the
+        payment data."""
+        self._create_transaction("redirect")
+        url = self._build_url(const.PAYMENT_RETURN_ROUTE)
+        with (
+            patch("odoo.addons.payment_ecpay.controllers.main.EcpayController._verify_signature"),
+            patch(
+                "odoo.addons.payment.models.payment_transaction.PaymentTransaction._process"
+            ) as process_mock,
+        ):
+            self._make_http_post_request(url, data=self.payment_result_data)
+        self.assertEqual(process_mock.call_count, 1)
+
+    @mute_logger("odoo.addons.payment_ecpay.controllers.main")
+    def test_webhook_notification_triggers_processing(self):
+        """Test that receiving a valid webhook notification triggers the processing of the payment
+        data."""
+        self._create_transaction("redirect")
+        url = self._build_url(const.WEBHOOK_ROUTE)
+        with (
+            patch("odoo.addons.payment_ecpay.controllers.main.EcpayController._verify_signature"),
+            patch(
+                "odoo.addons.payment.models.payment_transaction.PaymentTransaction._process"
+            ) as process_mock,
+        ):
+            self._make_http_post_request(url, data=self.payment_result_data)
+        self.assertEqual(process_mock.call_count, 1)
+
+    @mute_logger("odoo.addons.payment_ecpay.controllers.main")
+    def test_webhook_triggers_signature_check(self):
+        """Test that receiving a webhook notification triggers a signature check."""
+        self._create_transaction("redirect")
+        url = self._build_url(const.WEBHOOK_ROUTE)
+        with (
+            patch(
+                "odoo.addons.payment_ecpay.controllers.main.EcpayController._verify_signature"
+            ) as signature_check_mock,
+            patch("odoo.addons.payment.models.payment_transaction.PaymentTransaction._process"),
+        ):
+            self._make_http_post_request(url, data=self.payment_result_data)
+        self.assertEqual(signature_check_mock.call_count, 1)
+
+    def test_accept_notification_with_valid_signature(self):
+        """Test the verification of a notification with a valid signature."""
+        tx = self._create_transaction("redirect")
+        payload = dict(self.payment_result_data, CheckMacValue=self.webhook_payment_data_signature)
+        self._assert_does_not_raise(Forbidden, EcpayController._verify_signature, payload, tx)
+
+    @mute_logger("odoo.addons.payment_ecpay.controllers.main")
+    def test_reject_notification_with_missing_signature(self):
+        """Test the verification of a notification with a missing signature."""
+        tx = self._create_transaction("redirect")
+        payload = dict(self.payment_result_data, CheckMacValue=None)
+        self.assertRaises(Forbidden, EcpayController._verify_signature, payload, tx)
+
+    @mute_logger("odoo.addons.payment_ecpay.controllers.main")
+    def test_reject_notification_with_invalid_signature(self):
+        """Test the verification of a notification with an invalid signature."""
+        tx = self._create_transaction("redirect")
+        payload = dict(self.payment_result_data, CheckMacValue="dummy")
+        self.assertRaises(Forbidden, EcpayController._verify_signature, payload, tx)

addons/payment/data/payment_method_data.xml
@@ -1020,6 +1020,19 @@
         <field name="supported_currency_ids" eval="[Command.set([ref('base.EGP')])]" />
     </record>
 
+    <!-- Generic payment method representing convenience store payments like 7/11, OKMart etc. -->
+    <record id="payment_method_cvs" model="payment.method">
+        <field name="name">Convenience Stores</field>
+        <field name="code">cvs</field>
+        <field name="sequence">1000</field>
+        <field name="active">False</field>
+        <field name="image" type="base64" file="payment/static/img/cvs.png"/>
+        <field name="support_tokenization">False</field>
+        <field name="support_express_checkout">False</field>
+        <field name="support_manual_capture">none</field>
+        <field name="support_refund">partial</field>
+    </record>
+
     <record id="payment_method_dana" model="payment.method">
         <field name="name">Dana</field>
         <field name="code">dana</field>
@@ -2052,6 +2065,19 @@
                          ref('base.NOK'),
                      ])]"
         />
+    </record>
+
+    <!-- Generic payment method representing mobile wallet -->
+    <record id="payment_method_mobile_wallet" model="payment.method">
+        <field name="name">Mobile Wallet</field>
+        <field name="code">mobile_wallet</field>
+        <field name="sequence">10</field>
+        <field name="active">False</field>
+        <field name="image" type="base64" file="payment/static/img/wallet.png"/>
+        <field name="support_tokenization">False</field>
+        <field name="support_express_checkout">False</field>
+        <field name="support_manual_capture">none</field>
+        <field name="support_refund">none</field>
     </record>
 
     <record id="payment_method_mobile_wallet_eg" model="payment.method">
@@ -3592,6 +3618,23 @@
         />
     </record>
 
+    <record id="payment_method_twqr" model="payment.method">
+        <field name="name">TWQR</field>
+        <field name="code">twqr</field>
+        <field name="sequence">1000</field>
+        <field name="active">False</field>
+        <field name="image" type="base64" file="payment/static/img/twqr.png"/>
+        <field name="support_tokenization">False</field>
+        <field name="support_express_checkout">False</field>
+        <field name="support_manual_capture">none</field>
+        <field name="support_refund">none</field>
+        <field name="supported_currency_ids"
+               eval="[Command.set([
+                         ref('base.TWD'),
+                     ])]"
+        />
+    </record>
+
     <record id="payment_method_uatp" model="payment.method">
         <field name="name">Universal Air Travel Plan</field>
         <field name="code">uatp</field>
@@ -4201,6 +4244,15 @@
         <field name="image" type="base64" file="payment/static/img/facilito.png"/>
     </record>
 
+    <record id="payment_method_family_mart" model="payment.method">
+        <field name="name">Family Mart</field>
+        <field name="code">family_mart</field>
+        <field name="primary_payment_method_id" eval="ref('payment.payment_method_cvs')"/>
+        <field name="sequence">1000</field>
+        <field name="active">False</field>
+        <field name="image" type="base64" file="payment/static/img/family_mart.png"/>
+    </record>
+
     <record id="payment_method_hipercard" model="payment.method">
         <field name="name">Hipercard</field>
         <field name="code">hipercard</field>
@@ -4210,6 +4262,16 @@
         <field name="image" type="base64" file="payment/static/img/hipercard.png"/>
     </record>
 
+    <record id="payment_method_hi_life" model="payment.method">
+        <field name="name">Hi-Life</field>
+        <field name="code">hi_life</field>
+        <field name="primary_payment_method_id" eval="ref('payment.payment_method_cvs')"/>
+        <field name="sequence">1000</field>
+        <field name="active">False</field>
+        <field name="image" type="base64" file="payment/static/img/hi_life.png"/>
+    </record>
+
+
     <record id="payment_method_ideal_brand" model="payment.method">
         <field name="name">iDEAL</field>
         <field name="code">ideal_brand</field>
@@ -4217,6 +4279,15 @@
         <field name="sequence">1000</field>
         <field name="active">False</field>
         <field name="image" type="base64" file="payment/static/img/ideal.png"/>
+    </record>
+
+    <record id="payment_method_ipass_money" model="payment.method">
+        <field name="name">iPASS Money</field>
+        <field name="code">ipass_money</field>
+        <field name="primary_payment_method_id" eval="ref('payment.payment_method_mobile_wallet')"/>
+        <field name="sequence">1000</field>
+        <field name="active">False</field>
+        <field name="image" type="base64" file="payment/static/img/ipass_money.png"/>
     </record>
 
     <record id="payment_method_jcb" model="payment.method">
@@ -4317,6 +4388,15 @@
         <field name="sequence">1000</field>
         <field name="active">False</field>
         <field name="image" type="base64" file="payment/static/img/card.png"/>
+    </record>
+
+    <record id="payment_method_ok_mart" model="payment.method">
+        <field name="name">OK Mart</field>
+        <field name="code">ok_mart</field>
+        <field name="primary_payment_method_id" eval="ref('payment.payment_method_cvs')"/>
+        <field name="sequence">1000</field>
+        <field name="active">False</field>
+        <field name="image" type="base64" file="payment/static/img/ok_mart.png"/>
     </record>
 
     <record id="payment_method_omannet" model="payment.method">

commit f2b118e03b8fdcc5af71b3e9f99a5fa08ccbd20c

[FIX] payment_ecpay: update languages for traditional and simplified chinese
Current behavior:
When Odoo locale is set in traditional Chinese (zh_TW),
the ECPay payment portal shows up in simplified Chinese (zh_CN).

Expected behavior:
When Odoo locale is set in traditional Chinese (zh_TW),
the ECPay payment portal should show up in traditional Chinese (zh_TW) also.

Steps to reproduce:
In Odoo 19.0,
1. Install modules: payment_ecpay, website, ecommerce
2. Change the company and website language to Traditional Chinese (zh_TW).
Make sure the currency is in TWD (Taiwanese Dollar)
3. Add ECPay as a payment method
4. Add some test products and checkout with ECPay
5. Observe the language on ECPay portal website, and payload in V5
   package in network tab

Cause of the issue:
Initially the locale mapping was wrong, mapping CHI to zh. There's no
"zh" locale, must either be "zh_TW" or "zh_CN".
So when the Odoo locale is set to traditional Chinese (zh_TW), it
included Language=CHI in the payload and redirect to ECPAY,
making ECPay think that we're trying to request the simplified Chinese site.
Caused by commit: 89cb64bc2890c1caf66cd7846c978d45ef16eb11

Fix:
Fixed the language mapping following ECPay developer docs,
and what language code to be included in the payload sent to ECPay
from current Odoo locale.

opw-6270820

---------------------------------------------------------------

closes odoo/odoo#269206

Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>

addons/payment_ecpay/const.py
@@ -57,7 +57,13 @@ PAYMENT_METHODS_RESPONSE_MAPPING = MappingProxyType({
 
 # Mapping IETF language tags (e.g.: 'fr-BE') to ECPay language codes.
 # If a language tag is not listed, the country code prefix can serve as fallback.
-LANGUAGE_CODES_MAPPING = MappingProxyType({"en": "ENG", "ja_JP": "JPN", "ko": "KOR", "zh": "CHI"})
+# zh_TW is intentionally omitted so that ECPay defaults to it when Language is missing.
+LANGUAGE_CODES_MAPPING = MappingProxyType({
+    "en": "ENG",
+    "ja_JP": "JPN",
+    "ko": "KOR",
+    "zh_CN": "CHI",
+})
 
 # Mapping of transaction states to ECPay success codes.
 SUCCESS_CODE_MAPPING = MappingProxyType({"done": ("1", "2", "10100073")})

addons/payment_ecpay/models/payment_transaction.py
@@ -79,10 +79,13 @@ class PaymentTransaction(models.Model):
             "ClientBackURL": urljoin(base_url, const.PAYMENT_RETURN_ROUTE),
             "OrderResultURL": urljoin(base_url, const.PAYMENT_RETURN_ROUTE),
             "IgnorePayment": ignored_payment_methods,
-            "Language": payment_utils.get_language_code(
-                self.env.context.get("lang", "en_US"), const.LANGUAGE_CODES_MAPPING
-            ),
         }
+        # Find the language code based on the user lang; ECPay defaults to zh_TW if omitted
+        language_code = payment_utils.get_language_code(
+            self.env.context.get("lang", "en_US"), const.LANGUAGE_CODES_MAPPING, fallback=None
+        )
+        if language_code:
+            rendering_values["Language"] = language_code
         rendering_values.update({
             "CheckMacValue": self.provider_id._ecpay_calculate_signature(rendering_values),
             "api_url": self.provider_id._ecpay_get_api_url(),

addons/payment_ecpay/tests/test_payment_transaction.py
@@ -65,6 +65,22 @@ class TestPaymentTransaction(EcpayCommon):
         expected_values["api_url"] = "https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5"
         self.assertEqual(tx._get_specific_rendering_values(None), expected_values)
 
+    def test_rendering_values_include_language_for_simplified_chinese(self):
+        self.env["res.lang"]._activate_lang("zh_CN")
+        tx = self._create_transaction(
+            "redirect", payment_method_id=self.env.ref("payment.payment_method_card").id
+        )
+        rendering_values = tx.with_context(lang="zh_CN")._get_specific_rendering_values(None)
+        self.assertEqual(rendering_values.get("Language"), "CHI")
+
+    def test_rendering_values_omit_language_for_traditional_chinese(self):
+        self.env["res.lang"]._activate_lang("zh_TW")
+        tx = self._create_transaction(
+            "redirect", payment_method_id=self.env.ref("payment.payment_method_card").id
+        )
+        rendering_values = tx.with_context(lang="zh_TW")._get_specific_rendering_values(None)
+        self.assertNotIn("Language", rendering_values)
+
     def test_extract_reference_finds_reference(self):
         """Test that the transaction reference is found in the payment data."""
         tx = self._create_transaction("redirect")

addons/payment_ecpay/tests/test_processing_flows.py
@@ -19,6 +19,18 @@ class TestProcessingFlows(EcpayCommon, PaymentHttpCommon):
         signature = self.provider._ecpay_calculate_signature(self.payment_result_data)
         self.assertEqual(signature, self.webhook_payment_data_signature)
 
+    def test_compute_signature_without_language_returns_correct_signature(self):
+        self.env["res.lang"]._activate_lang("zh_TW")
+        tx = self._create_transaction(
+            "redirect", payment_method_id=self.env.ref("payment.payment_method_card").id
+        )
+        rendering_values = tx.with_context(lang="zh_TW")._get_specific_rendering_values(None)
+        signature_data = dict(rendering_values)
+        signature_data.pop("CheckMacValue", None)
+        signature_data.pop("api_url", None)
+        expected_mac = tx.provider_id._ecpay_calculate_signature(signature_data)
+        self.assertEqual(rendering_values["CheckMacValue"], expected_mac)
+
     @mute_logger("odoo.addons.payment_ecpay.controllers.main")
     def test_returning_from_payment_triggers_processing(self):
         """Test that receiving a valid redirect notification triggers the processing of the