5f2964f9a858e89129987dd7543376ad40741410
3amo-magdy <ammg@odoo.com>
2026-06-26 20:45:09 +0200

    [IMP] rime rule list view domain_count widget to show a link with count to the employees that each rule applies to

---
+++ addons/hr_work_entry/static/src/components/domain_count_field/domain_count_field.js
@@ -0,0 +1,18 @@
+import { registry } from "@web/core/registry";
+import { DomainField, domainFieldProps } from "@web/views/fields/domain/domain_field";
+
+export class DomainCountField extends DomainField {
+    static template = "hr_work_entry.DomainCountField";
+}
+
+registry.category("fields").add("domain_count", {
+    component: DomainCountField,
+    supportedTypes: ["char", "text"],
+    isEmpty: () => false,
+    extractProps({ options }, dynamicInfo) {
+        return {
+            resModel: options.model,
+            context: dynamicInfo.context,
+        };
+    },
+});
