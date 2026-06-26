0b1cc82f3491a28fdee1156f36a527c6cdc8df24
sami odoo <sami@odoo.com>
2026-06-26 16:59:53 +0530

    [REF] sale_pdf_quote_builder: replace t-custom-model with t-model/t-model.proxy
    As part of the migration from `owl 2` to `owl 3`, this commit replaces
    uses of `t-custom-model` with `t-model` or `t-model.proxy`.

--- addons/sale_pdf_quote_builder/static/src/js/custom_content_kanban_like_widget/custom_field_card/custom_field_card.js
+++ addons/sale_pdf_quote_builder/static/src/js/custom_content_kanban_like_widget/custom_field_card/custom_field_card.js
@@ -1,18 +1,20 @@
 import { useRef } from "@web/owl2/utils";
-import { Component } from "@odoo/owl";
+import { Component, props, signal, t, useEffect } from "@odoo/owl";
 import { _t } from "@web/core/l10n/translation";
 import { useAutoresize } from "@web/core/utils/autoresize";
 
 export class CustomFieldCard extends Component {
     static template = "sale_pdf_quote_builder.customFieldCard";
-    static props = {
-        name: String,
-        value: String,
-        onChange: Function,
-        readonly: { type: Boolean, optional: true },
-    };
+    props = props({
+        name: t.string(),
+        value: t.string(),
+        onChange: t.function(),
+        readonly: t.boolean().optional(),
+    });
 
     setup() {
+        this.value = signal(this.props.value || "");
+        useEffect(() => this.value.set(this.props.value || ""));
         this.customFormFieldTextAreaRef = useRef('customFieldCardTextArea');
         this.placeholder = _t("Click to write content for the PDF quote...");
         useAutoresize(this.customFormFieldTextAreaRef);
