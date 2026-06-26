852844bcaeea1da651b38a695601e7bbb969eb43
Patricia Daloze (pda) <pda@odoo.com>
2026-06-26 10:13:13 +0200

    [FIX] payment_custom: exclude Pay on Invoice on invoices
    Paying an invoice with the pay later provider "Pay on Invoice" doesn't make sense.
    Removing it from the list of available providers in such documents.
    
    closes odoo/odoo#272376
    
    Signed-off-by: Patricia Daloze (pda) <pda@odoo.com>

--- addons/account_payment_custom/tests/__init__.py
+++ addons/account_payment_custom/tests/__init__.py
@@ -1,3 +1,3 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import common, test_account_bank_statement_line, test_payment_provider
+from . import common, test_account_bank_statement_line, test_payment_provider, test_portal
