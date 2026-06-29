odoo/odoo

commit 3223deb871ca4cb4ac0381e4321f2dbf79a60189

[FIX] mrp: consume expected quantity on mo backorder
Steps to reproduce:
- In the settings enable: Multi-Steps Routes
-  Set your warehouse to manufacture in 2 steps (pick then manufacture).
- Create a final product (FP) with a BOM in flexible consumption:
  - 2 x COMP (lot tracked)
- Put a lot for 6 units in of COMP in stock
- Create and confirm an MO for 5 units of FP
- Set the quantity producing on the MO to 1, requiring 2 of the 6
  available units of COMP
- Validate the MO and create a backorder for the remaining quantity.
> The consumed qty on the main MO is of 0 units rather than 2.

Cause of the issue:

Since the component is tracked, and since the pbm move was backordered,
the move quantity will not be automatically set when setting the
`qty_producing`:
https://github.com/odoo/odoo/blob/a1bcd917846493d08dd02b63e6110078ff5156a3/addons/mrp/models/mrp_production.py#L1405-L1411
And in particular, the move is not picked as it would if the product was
untracked or if the pbm move was not backordered:
https://github.com/odoo/odoo/blob/a1bcd917846493d08dd02b63e6110078ff5156a3/addons/mrp/models/mrp_production.py#L1421-L1427
And, since the move will not be picked at any other point in this flow,
the move will be unreserved during the `button_mark_done`:
https://github.com/odoo/odoo/blob/7c35e183d6cc33a6e5d20e5e97ffef79e03b49d4/addons/mrp/models/mrp_production.py#L2216
https://github.com/odoo/odoo/blob/7c35e183d6cc33a6e5d20e5e97ffef79e03b49d4/addons/mrp/models/mrp_production.py#L1895-L1896
https://github.com/odoo/odoo/blob/7c35e183d6cc33a6e5d20e5e97ffef79e03b49d4/addons/mrp/models/mrp_production.py#L1901

opw-6128575

closes odoo/odoo#269235

Signed-off-by: William Henrotin (whe) <whe@odoo.com>

addons/mrp/models/mrp_production.py
@@ -1402,11 +1402,8 @@ class MrpProduction(models.Model):
             if qty_producing_uom != qty_production_uom and not (qty_producing_uom == 0 and self._origin.qty_producing != self.qty_producing):
                 self.qty_producing = self.product_id.uom_id._compute_quantity(len(self.lot_producing_ids), self.product_uom_id, rounding_method='HALF-UP')
 
-        # waiting for a preproduction move before assignement
-        is_waiting = self.warehouse_id.manufacture_steps != 'mrp_one_step' and self.picking_ids.filtered(lambda p: p.picking_type_id == self.warehouse_id.pbm_type_id and p.state not in ('done', 'cancel'))
-
         for move in (
-            self.move_raw_ids.filtered(lambda m: not is_waiting or m.product_id.tracking == 'none')
+            self.move_raw_ids
             | self.move_finished_ids.filtered(lambda m: m.product_id != self.product_id or m.product_id.tracking == 'serial')
         ):
             is_byproduct = move in self.move_byproduct_ids
@@ -1419,6 +1416,13 @@ class MrpProduction(models.Model):
                 continue
 
             new_qty = move.product_uom.round((self.qty_producing - self.qty_produced) * move.unit_factor)
+            if move.has_tracking != 'none':
+                qty_waiting = 0
+                for move_orig in move.move_orig_ids:
+                    if move_orig.state not in ('draft', 'done', 'cancel'):
+                        qty_waiting += move_orig.product_uom._compute_quantity(move_orig.quantity, move.product_uom)
+                if not move.product_uom.is_zero(qty_waiting):
+                    new_qty = min(new_qty, move.product_uom_qty - qty_waiting)
             move._set_quantity_done(new_qty)
             if (not move.manual_consumption or pick_manual_consumption_moves) \
                     and move.quantity \

addons/mrp/tests/test_backorder.py
@@ -978,6 +978,59 @@ class TestMrpProductionBackorder(TestMrpCommon):
         mo.production_group_id.production_ids[-1].action_cancel()
         self.assertFalse(mo.picking_ids.filtered(lambda p: p.state == 'cancel' and p.product_id == self.product_6))
 
+    def test_onchange_lot_ids_for_lot_tracked_product_with_reservation(self):
+        """
+        Checks that the consumed quantity is correctly recorded on a manufacturing
+        order configured for 2-step manufacturing (pick before manufacture) when a
+        backorder is created. After partially transferring lot-tracked components to
+        the pre-production location and producing a fraction of the total quantity,
+        the done order should reflect the actual components consumed, not zero.
+        """
+        self.warehouse_1.manufacture_steps = 'pbm'
+        final_product, component = self.product, self.productA
+        self.productA.tracking = 'lot'
+        bom = self.env['mrp.bom'].create({
+            'product_id': final_product.id,
+            'product_tmpl_id': final_product.product_tmpl_id.id,
+            'product_uom_id': self.uom_unit.id,
+            'product_qty': 1.0,
+            'type': 'normal',
+            'consumption': 'flexible',
+            'bom_line_ids': [Command.create({'product_id': component.id, 'product_qty': 2.0})],
+        })
+        lot = self.env['stock.lot'].create({'name': 'LOT001', 'product_id': component.id})
+        self.env['stock.quant']._update_available_quantity(component, self.stock_location, 10.0, lot_id=lot)
+
+        mo = self.env['mrp.production'].create({
+            'product_id': final_product.id,
+            'bom_id': bom.id,
+            'product_qty': 5,
+            'warehouse_id': self.warehouse_1.id,
+        })
+        mo.action_confirm()
+
+        # Reserve and partially validate the PBM picking
+        pbm_picking = mo.picking_ids
+        pbm_picking.move_ids.quantity = 6.0
+        Form.from_action(self.env, pbm_picking.button_validate()).save().process()
+
+        # Produce 1 unit (consumes 2 components), then create a backorder
+        with Form(mo) as mo_form:
+            mo_form.qty_producing = 1
+        Form.from_action(self.env, mo.button_mark_done()).save().action_backorder()
+        self.assertEqual(mo.move_raw_ids.quantity, 2.0)
+        backorder = mo.production_group_id.production_ids - mo
+        # Produce 3 unit (should consume 6 components but only 4 are already available)
+        with Form(backorder) as backorder_form:
+            backorder_form.qty_producing = 3
+        self.assertEqual(backorder.move_raw_ids.quantity, 4.0)
+        backorder.picking_ids[-1].button_validate()
+        with Form(backorder) as backorder_form:
+            backorder_form.qty_producing = 4
+        self.assertEqual(backorder.move_raw_ids.quantity, 8.0)
+        mo.button_mark_done()
+        self.assertEqual(backorder.move_raw_ids.quantity, 8.0)
+
 
 class TestMrpWorkorderBackorder(TransactionCase):
     @classmethod

addons/mrp/tests/test_consume_component.py
@@ -386,7 +386,6 @@ class TestConsumeComponent(TestConsumeComponentCommon):
             {'quantity': 2.0, 'picked': False, 'lot_ids': lot_1.ids},
             {'quantity': 1.0, 'picked': False, 'lot_ids': lot_2.ids},
         ])
-        mo.move_raw_ids.picked = True
         mo.button_mark_done()
 
     def test_automatic_consume_new_added_component(self):

commit deed945596b6a8bf4567ebfd38432cb2d2c11c10

[FIX] mrp: consume expected quantity on mo backorder without reservation
Steps to reproduce:

- In the settings enable: Multi-Steps Routes
-  Set your warehouse to manufacture in 2 steps (pick then manufacture).
- Create a final product (FP) with a BOM in flexible consumption:
  - 1 x COMP (lot tracked)
- Create and confirm an MO for 1 units of FP
- Set the quantity producing on the MO to 1
> The consumed qty was updated to 1 unit
- Set a lot on the pre-production pikcing and validate
> The lot is not transfered to the MO which you are not able to
  validate since the registered component is lot less

Cause of the issue:

The issue is caused by
https://github.com/odoo/odoo/commit/3223deb871ca4cb4ac0381e4321f2dbf79a60189
as the `qty_waiting` is based on the reservation state of the move
origin of the move rather than its actual demand:
https://github.com/odoo/odoo/blob/00118002bd6eab2f4c34a32e993a9219fded06ac/addons/mrp/models/mrp_production.py#L1419-L1426
In particular, since the backorder of the pre-production picking was not
reserved (since nothing was available in stock), it was not taken into
account as it should have been.

Issue 2:

Steps to reproduce:

- In the settings Enable Multi-Steps Routes
- Unarchive MTO
- Create 3 products:
  - Final Product: Tracked by SN with a BOM: 1 X Super Component
  - Super Component: Tracked by SN, MTO with a BOM: 1 X Component
  - Basic Component: Put 10 units in stock
- Create and confirm an MO for 3 units of Final Product
> This should create an MO for 3 units of Super Component
- Go to the Child MO > Cogs wheel > Split in 3 MO's
- Click "Generate serial" on each Child MO and validate the first one
- On the MO for Final Product > Cogs wheel > Split in 3 MO's
- On the first MO, click "Generate Serial"
> Error: Reserving a negative quantity is not allowed.

Cause of the issue:

The `action_generate_serial` calls in turn the `set_qty_producing`:

addons/mrp/models/mrp_production.py
@@ -1418,12 +1418,19 @@ class MrpProduction(models.Model):
 
             new_qty = move.product_uom.round((self.qty_producing - self.qty_produced) * move.unit_factor)
             if move.has_tracking != 'none':
-                qty_waiting = 0
+                relevant_orig_ids = set()
+                qty_available = 0
                 for move_orig in move.move_orig_ids:
-                    if move_orig.state not in ('draft', 'done', 'cancel'):
-                        qty_waiting += move_orig.product_uom._compute_quantity(move_orig.quantity, move.product_uom)
-                if not move.product_uom.is_zero(qty_waiting):
-                    new_qty = min(new_qty, move.product_uom_qty - qty_waiting)
+                    if move_orig.state not in ('draft', 'cancel'):
+                        relevant_orig_ids.add(move_orig.id)
+                    if move_orig.state == 'done':
+                        qty_available += move_orig.product_uom._compute_quantity(move_orig.quantity, move.product_uom)
+                qty_taken = 0
+                for move_sibling in move.move_orig_ids.move_dest_ids - move:
+                    if move_sibling.state == 'done':
+                        qty_taken += move_sibling.product_uom._compute_quantity(move_sibling.quantity, move.product_uom)
+                if relevant_orig_ids and move.product_uom.compare(qty_available, qty_taken) >= 0:
+                    new_qty = min(new_qty, qty_available - qty_taken)
             move._set_quantity_done(new_qty)
             if (not move.manual_consumption or pick_manual_consumption_moves) \
                     and move.quantity \

addons/mrp/tests/test_backorder.py
@@ -998,9 +998,7 @@ class TestMrpProductionBackorder(TestMrpCommon):
             'consumption': 'flexible',
             'bom_line_ids': [Command.create({'product_id': component.id, 'product_qty': 2.0})],
         })
-        lot = self.env['stock.lot'].create({'name': 'LOT001', 'product_id': component.id})
-        self.env['stock.quant']._update_available_quantity(component, self.stock_location, 10.0, lot_id=lot)
-
+        lots = self.env['stock.lot'].create([{'name': f'LOT00{i + 1}', 'product_id': component.id} for i in range(2)])
         mo = self.env['mrp.production'].create({
             'product_id': final_product.id,
             'bom_id': bom.id,
@@ -1012,6 +1010,7 @@ class TestMrpProductionBackorder(TestMrpCommon):
         # Reserve and partially validate the PBM picking
         pbm_picking = mo.picking_ids
         pbm_picking.move_ids.quantity = 6.0
+        pbm_picking.move_ids.move_line_ids.lot_id = lots[0]
         Form.from_action(self.env, pbm_picking.button_validate()).save().process()
 
         # Produce 1 unit (consumes 2 components), then create a backorder
@@ -1024,7 +1023,10 @@ class TestMrpProductionBackorder(TestMrpCommon):
         with Form(backorder) as backorder_form:
             backorder_form.qty_producing = 3
         self.assertEqual(backorder.move_raw_ids.quantity, 4.0)
-        backorder.picking_ids[-1].button_validate()
+        pbm_picking_bo = backorder.picking_ids[-1]
+        pbm_picking_bo.move_ids.quantity = 4.0
+        pbm_picking_bo.move_ids.move_line_ids.lot_id = lots[1]
+        pbm_picking_bo.button_validate()
         with Form(backorder) as backorder_form:
             backorder_form.qty_producing = 4
         self.assertEqual(backorder.move_raw_ids.quantity, 8.0)

addons/mrp/tests/test_procurement.py
@@ -1453,3 +1453,68 @@ class TestProcurement(TestMrpCommon):
         self.assertRecordValues(target_moves, [
             {'quantity': 2.0}, {'quantity': 2.0}, {'quantity': 3.0}
         ])
+
+    def test_split_sn_mo_with_mto_component(self):
+        """
+        Check that generating a serial on a split MO with an MTO child MO does not raise a negative reservation error.
+        """
+        mto_route = self.warehouse_1.mto_pull_id.route_id
+        mto_route.action_unarchive()
+        final_product, super_component, basic_component = self.env['product.product'].create([
+            {'name': 'Final Product', 'is_storable': True, 'tracking': 'serial'},
+            {'name': 'Super Component', 'is_storable': True, 'tracking': 'serial', 'route_ids': [Command.set(mto_route.ids)]},
+            {'name': 'Basic Component', 'is_storable': True},
+        ])
+        self.env['mrp.bom'].create([
+            {
+                'product_tmpl_id': final_product.product_tmpl_id.id,
+                'product_qty': 1.0,
+                'bom_line_ids': [Command.create({'product_id': super_component.id, 'product_qty': 1})],
+            },
+            {
+                'product_tmpl_id': super_component.product_tmpl_id.id,
+                'product_qty': 1.0,
+                'bom_line_ids': [Command.create({'product_id': basic_component.id, 'product_qty': 1})],
+            },
+        ])
+        mo = self.env['mrp.production'].create({
+            'product_id': final_product.id,
+            'product_qty': 4.0,
+        })
+        mo.action_confirm()
+
+        # Split the child MO in 3 and validate the first one
+        child_mo = mo._get_children()
+        wizard = Form.from_action(self.env, child_mo.action_split())
+        wizard.max_batch_size = 1
+        wizard.save().action_split()
+        child_mos = child_mo.production_group_id.production_ids
+        self.assertEqual(len(child_mos), 4)
+        child_mos[-1].action_generate_serial()
+        child_mos[-1].button_mark_done()
+        self.assertEqual(child_mos[-1].state, 'done')
+
+        # Split the main MO in 3 and validate the first one
+        wizard = Form.from_action(self.env, mo.action_split())
+        wizard.max_batch_size = 1
+        wizard.save().action_split()
+        mos = mo.production_group_id.production_ids
+        self.assertEqual(len(mos), 4)
+        mos[0].action_generate_serial()
+        mos[0].button_mark_done()
+        self.assertEqual(mos[0].state, 'done')
+        self.assertEqual(mos.move_raw_ids.lot_ids, child_mos[-1].lot_producing_ids)
+
+        # Generate 3 serials but produce only 2 Super Component
+        child_mos[0].action_generate_serial()
+        child_mos[1].action_generate_serial()
+        child_mos[2].action_generate_serial()
+        child_mos[1:3].button_mark_done()
+        self.assertEqual(child_mos[1].state, 'done')
+        self.assertEqual(child_mos[2].state, 'done')
+        mos[1].action_generate_serial()
+        mos[1].button_mark_done()
+        self.assertEqual(mos[1].state, 'done')
+        self.assertEqual(mos[1].move_raw_ids.lot_ids, child_mos[1].lot_producing_ids)
+        self.assertEqual(mos[2].state, 'confirmed')
+        self.assertEqual(mos[2].move_raw_ids.lot_ids, child_mos[2].lot_producing_ids)