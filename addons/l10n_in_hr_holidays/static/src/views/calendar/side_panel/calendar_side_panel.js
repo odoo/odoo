038b1867e35439535b33cf13bede3010a174be96
Romain Estievenart <res@odoo.com>
2026-06-29 15:14:25 +0200

    [FIX] {,l10n_in_}hr_holidays: replace onWillUpdateProps with owl3 variant
    This commit is a follow-up of 197d0ca5, as part of the Owl 3 migration,
    replace onWillUpdateProps hook with the appropriate Owl 3 alternatives
    
    X-original-commit: 945b159ef60c6d5e1ecfb6e7e51f77528934a013

--- addons/l10n_in_hr_holidays/static/src/views/calendar/side_panel/calendar_side_panel.js
+++ addons/l10n_in_hr_holidays/static/src/views/calendar/side_panel/calendar_side_panel.js
@@ -1,29 +1,14 @@
-import { asyncComputed } from "@odoo/owl";
+import { computed } from "@odoo/owl";
 import { patch } from "@web/core/utils/patch";
 import { TimeOffCalendarSidePanel } from "@hr_holidays/views/calendar/calendar_side_panel/calendar_side_panel";
 
 patch(TimeOffCalendarSidePanel.prototype, {
     setup() {
         super.setup();
-        this.optionalHolidays = asyncComputed(() => this.getOptionalHolidays());
-    },
-
-    async getOptionalHolidays() {
-        const { rangeStart, rangeEnd } = this.props.model;
-        const specialDays = await this._specialDaysCache.read(rangeStart, rangeEnd);
-        specialDays["optionalHolidays"].forEach((optionalHoliday) => {
-            optionalHoliday.start = luxon.DateTime.fromISO(optionalHoliday.start);
-            optionalHoliday.end = luxon.DateTime.fromISO(optionalHoliday.end);
-        });
-        return specialDays["optionalHolidays"];
-    },
-
-    get leaveState() {
-        return {
-            bankHolidays: this.specialDays()?.bankHolidays || [],
-            mandatoryDays: this.specialDays()?.mandatoryDays || [],
-            holidays: this.holidays() || [],
-            optionalHolidays: this.optionalHolidays() || [],
-        };
+        this.optionalHolidays = computed(() =>
+          this._mapIsoToDatetimes(
+            this.specialDays().optionalHolidays || []
+          )
+        );
     }
-})
+});
