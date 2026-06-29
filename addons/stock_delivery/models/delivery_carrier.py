d267221451d4113d195555030e92dd2e29f6e575
Stef Ossé <stoss@odoo.com>
2026-06-29 11:22:29 +0200

    [IMP] stock_delivery,delivery_stock_picking_batch: generate one shipping label per batch
    Issue
    ----------------------------
    When confirming a batch picking, each individual picking will generate a shipping label.
    This shipping label has the weight of the whole picking in stead of the weight of the picking.
    
    Steps to reproduce
    ------------------
    1. create two sales orders for the same customer, with the same carrier
    2. create a batch picking for the two pickings
    3. put the batch picking in a box
    4. validate the batch picking
    
    You will see that there are labels for each picking in the batch.
    These labels have both the weight of the full batch in stead of their own product.
    
    Desired behaviour
    -----------------
    If we have a batch consisting of pickings for the same client, with the same source, address,
    carrier in the same package we want to send them together.
    
    See odoo/enterprise#122415
    Task-6296793

--- addons/stock_delivery/models/delivery_carrier.py
+++ addons/stock_delivery/models/delivery_carrier.py
@@ -2,7 +2,7 @@
 
 from odoo import _, fields, models
 
-from odoo.exceptions import UserError, ValidationError
+from odoo.exceptions import UserError
 from odoo.tools.float_utils import float_round
 from odoo.tools.misc import groupby
 
@@ -33,23 +33,45 @@ class DeliveryCarrier(models.Model):
     # -------------------------- #
 
     def send_shipping(self, pickings):
-        ''' Send the package to the service provider
+        """ Send the package to the service provider
 
         :param pickings: A recordset of pickings
-        :returns: A list of dictionaries (one per picking) containing of
-            the form::
+        :returns: A list of dictionaries (one per picking) containing of the form::
+                {
+                    'picking': {
+                        'exact_price': price,
+                        'tracking_number': number,
+                        'delivery_labels': attachment_ids,
+                        'return_labels': attachment_ids,
+                        'shipping_docs': attachment_ids,
+                        'error_messages': [string],
+                    }
+                }
+        :rtype: dict[dict] | None
+        """
+        pickings_by_carrier = pickings.grouped('carrier_id')
 
-                         { 'exact_price': price,
-                           'tracking_number': number }
-        :rtype: list[dict] | None
-        '''
-        # TODO missing labels per package
-        # TODO missing currency
-        # TODO missing success, error, warnings
-        self.ensure_one()
-        if hasattr(self, '%s_send_shipping' % self.delivery_type):
-            return getattr(self, '%s_send_shipping' % self.delivery_type)(pickings)
-        return None
+        results = {}
+        for carrier, picks in pickings_by_carrier.items():
+            if hasattr(carrier, '%s_send_shipping' % carrier.delivery_type):
+                if self._bundle_pickings(picks):
+                    pick = picks[0]
+                    pick_name = pick.name
+                    pick.name = pick.batch_id.name
+                    res = getattr(carrier, '%s_send_shipping' % carrier.delivery_type)(pick)
+                    results[picks] = res[pick]
+                    pick.name = pick_name
+                else:
+                    results |= getattr(carrier, '%s_send_shipping' % carrier.delivery_type)(picks)
+            else:
+                results |= {p: {
+                    'exact_price': 0.0,
+                    'delivery_labels': self.env['ir.attachment'],
+                    'return_labels': self.env['ir.attachment'],
+                    'delivery_docs': self.env['ir.attachment'],
+                    'error_messages': [_("%s_send_shipping method not found!", carrier.delivery_type)],
+                } for p in picks}
+        return results
 
     def get_return_label(self, pickings, tracking_number=None, origin_date=None):
         self.ensure_one()
@@ -246,10 +268,16 @@ class DeliveryCarrier(models.Model):
     # ------------------------------------------------ #
 
     def fixed_send_shipping(self, pickings):
-        res = []
+        res = {}
         for p in pickings:
-            res = res + [{'exact_price': p.carrier_id.fixed_price,
-                          'tracking_number': False}]
+            res[p] = {
+                'exact_price': p.carrier_id.fixed_price,
+                'tracking_number': False,
+                'delivery_labels': self.env['ir.attachment'],
+                'return_labels': self.env['ir.attachment'],
+                'delivery_docs': self.env['ir.attachment'],
+                'error_messages': [],
+            }
         return res
 
     def fixed_get_tracking_link(self, picking):
@@ -265,13 +293,20 @@ class DeliveryCarrier(models.Model):
     # ----------------------------------- #
 
     def base_on_rule_send_shipping(self, pickings):
-        res = []
+        res = {}
         for p in pickings:
             carrier = self._match_address(p.partner_id)
+            error_messages = []
             if not carrier:
-                raise ValidationError(_('There is no matching delivery rule.'))
-            res = res + [{'exact_price': p.carrier_id._get_price_available(p.sale_id) if p.sale_id else 0.0,  # TODO cleanme
-                          'tracking_number': False}]
+                error_messages.append(_('There is no matching delivery rule.'))
+            res[p] = {
+                'exact_price': p.carrier_id._get_price_available(p.sale_id) if p.sale_id else 0.0,  # TODO cleanme
+                'tracking_number': False,
+                'delivery_labels': self.env['ir.attachment'],
+                'return_labels': self.env['ir.attachment'],
+                'delivery_docs': self.env['ir.attachment'],
+                'error_messages': error_messages,
+            }
         return res
 
     def base_on_rule_get_tracking_link(self, picking):
@@ -281,3 +316,19 @@ class DeliveryCarrier(models.Model):
 
     def base_on_rule_cancel_shipment(self, pickings):
         raise NotImplementedError()
+
+    def _bundle_pickings(self, pickings):
+        bundle = True
+        if not pickings:
+            return False
+        ref = pickings[0]
+        # Check if each move line has a target package
+        bundle &= len(ref.batch_id.move_line_ids.filtered(lambda move_line: move_line.result_package_id)) == len(ref.batch_id.move_line_ids)
+        if not bundle:
+            return False
+        packages = set(ref.batch_id.move_line_ids.mapped(lambda move_line: move_line.result_package_id.name))
+        bundle &= len(packages) == 1
+        for picking in pickings[1:]:
+            bundle &= ref.location_id == picking.location_id
+            bundle &= ref.partner_id == picking.partner_id
+        return bundle
