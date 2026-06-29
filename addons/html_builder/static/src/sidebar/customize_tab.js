ee35650c6dc440ce450245aa474904c18feaf8ad
Saurabh <sauc@odoo.com>
2026-06-29 11:33:50 +0530

    [REF] html_builder: replace onWillUpdateProps with owl3 alternatives
    As part of the Owl 3 migration, replace onWillUpdateProps hook
    with the appropriate Owl 3 alternatives.
    
    closes odoo/odoo#273035
    
    Signed-off-by: Géry Debongnie <ged@odoo.com>

--- addons/html_builder/static/src/sidebar/customize_tab.js
+++ addons/html_builder/static/src/sidebar/customize_tab.js
@@ -1,4 +1,4 @@
-import { Component, onWillUpdateProps, props, proxy, t } from "@odoo/owl";
+import { Component, props, proxy, t } from "@odoo/owl";
 import { OptionsContainer } from "./option_container";
 import { useVisibilityObserver } from "../core/utils";
 import { CustomizeComponent } from "@html_builder/sidebar/customize_component";
@@ -19,16 +19,6 @@ export class CustomizeTab extends Component {
         );
         useVisibilityObserver("content", (hasContent) => {
             this.state.hasContent = hasContent;
-        });
-        onWillUpdateProps((nextProps) => {
-            if (
-                !this.state.hasContent &&
-                nextProps.currentOptionsContainers.length > 0 &&
-                nextProps.currentOptionsContainers !== this.props.currentOptionsContainers
-            ) {
-                // Force a reconsideration of `content`
-                this.state.hasContent = true;
-            }
         });
         this.overlayPreviewedEl = null;
     }
