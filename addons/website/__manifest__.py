2ac4261a99fe499c6e2fa9f9429bcb7e0b09cc25
Antoine (anso) <anso@odoo.com>
2026-06-30 09:56:43 +0200

    [IMP] website: improve `s_card_offset` and add `s_banner_glow`
    This commit highlights the blurred background and custom shadow options
    in multiple snippets:
    - `s_banner_glow` -> new snippet
    - `s_card_offset` -> improved snippet
    
    task-6065733
    
    closes odoo/odoo#273756
    
    Related: odoo/design-themes#1306
    Signed-off-by: Soukéina Bojabza (sobo) <sobo@odoo.com>

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -140,6 +140,7 @@
         'views/snippets/s_pricelist_boxed.xml',
         'views/snippets/s_adventure.xml',
         'views/snippets/s_image_title.xml',
+        'views/snippets/s_banner_glow.xml',
         'views/snippets/s_bento_grid_avatars.xml',
         'views/snippets/s_key_images.xml',
         'views/snippets/s_kickoff.xml',
