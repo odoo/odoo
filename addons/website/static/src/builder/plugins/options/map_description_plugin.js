4b7d9bee908842d241592eec4c9a0918e7af72b1
Sébastien (blse) <blse@odoo.com>
2026-06-30 15:03:30 +0200

    WIP use p for description

--- addons/website/static/src/builder/plugins/options/map_description_plugin.js
+++ addons/website/static/src/builder/plugins/options/map_description_plugin.js
@@ -4,13 +4,14 @@ import { registry } from "@web/core/registry";
 
 export class MapDescriptionPlugin extends Plugin {
     static id = "mapDescription";
+    static dependencies = ["dom"];
     /** @type {import("plugins").WebsiteResources} */
     resources = {
         builder_actions: {
             MapDescriptionAction,
         },
         content_not_editable_selectors: ".s_map, .s_google_map",
-        content_editable_selectors: ":is(.s_map, .s_google_map) .description_content",
+        content_editable_selectors: ":is(.s_map, .s_google_map) .description",
         // Prevent the overlay of `s_map` when clicking on the description
         // (which prevents future clicks in the description)
         has_overlay_options: {
@@ -19,32 +20,17 @@ export class MapDescriptionPlugin extends Plugin {
     };
 
     setup() {
-        // A click on the div containing the editable span may remove the focus
-        // from the span, thus this handler forces the focus back in
-        this.addDomListener(this.editable, "click", (ev) => {
-            if (ev.detail === 1 && ev.target.matches(":is(.s_map, .s_google_map) .description")) {
-                ev.target.querySelector(":scope > .description_content").focus();
-            }
-        });
-
         this.upgradeSnippets();
     }
 
     upgradeSnippets() {
-        // Ensure the description's content is wrapped in an inline div which
-        // is used to set the contenteditable=true
-        // This is for pages which already existed before the wrapper existed
+        // Ensure the description's tag is `p`
+        // This is for pages which already existed before, when it was a `div`
         this.document
-            .querySelectorAll(":is(.s_map, .s_google_map) .description")
-            .forEach((descriptionEl) => {
-                if (!descriptionEl.querySelector(".description_content")) {
-                    const descriptionContentEl = this.document.createElement("div");
-                    descriptionContentEl.classList.add("description_content", "d-inline");
-                    descriptionContentEl.replaceChildren(...descriptionEl.childNodes);
-                    descriptionEl.classList.add("oe_unremovable");
-                    descriptionEl.replaceChildren(descriptionContentEl);
-                }
-            });
+            .querySelectorAll(":is(.s_map, .s_google_map) div.description")
+            .forEach((oldEl) =>
+                this.dependencies.dom.setTagName(oldEl, "p").classList.add("oe_unremovable")
+            );
     }
 }
 
