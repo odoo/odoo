2be7d67528ad840b44e33a4d56a895aa18fb9722
Mahdi Alijani (malj) <malj@odoo.com>
2026-06-30 12:53:43 +0000

    [FIX] website_sale_stock: show correct stock when variant doesn't exist
    Issue:
    ---
    If a product template has dynamic attributes, some variants might not
    exist. For those variants, we are showing wrong stock in the website.
    
    To reproduce:
    1- Create a product with a dynamic attribute and two values.
    2- Publish the product and uncheck sell when out-of-stock and check
       show product when the qty is less than 5.
    3- Create a purchase order with qty = 4 for the first value, so a
       variant is created for it.
    4- Go to the website shop. Open the product. 4 available qty in stock
       is shown for the first variant which is correct.
    5- Select 2nd variant.
    
    As you see, still 4 available qty is shown which is wrong.
    
    As the out-of-stock sale is unchecked, an out-of-stock warning should
    be shown.
    
    Cause and Fix:
    ---
    This is due to `isMainProduct` being always False when `product_id` is
    not set which makes `free_qty` and `out_of_stock` not to be updated.
    
    Also in `get_combination_info_website`, `is_storable` value is set to
    variant `is_storable` field which will be False if product variant is
    not exist.
    
    opw-6237602
    
    closes odoo/odoo#276247
    
    X-original-commit: f2be3625e34399313ca767fe35d321f360ec31f4
    Signed-off-by: Valeriya Chuprina (vchu) <vchu@odoo.com>

--- addons/website_sale_stock/controllers/variant.py
+++ addons/website_sale_stock/controllers/variant.py
@@ -11,5 +11,5 @@ class WebsiteSaleStockVariantController(WebsiteSaleVariantController):
     def get_combination_info_website(self, *args, **kwargs):
         request.update_context(website_sale_stock_get_quantity=True)
         res = super().get_combination_info_website(*args, **kwargs)
-        res['is_storable'] = request.env['product.product'].browse(res['product_id']).is_storable
+        res['is_storable'] = request.env['product.template'].browse(res['product_template_id']).is_storable
         return res
