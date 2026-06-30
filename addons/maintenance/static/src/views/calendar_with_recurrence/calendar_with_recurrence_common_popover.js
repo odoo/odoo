60549b44e7c968ddabd35dea2fa6509af48f602f
Aaron Bohy <aab@odoo.com>
2026-06-30 15:53:56 +0200

    [FIXUP] extensions

--- addons/maintenance/static/src/views/calendar_with_recurrence/calendar_with_recurrence_common_popover.js
+++
@@ -1,3 +0,0 @@
-import { CalendarCommonPopover } from "@web/views/calendar/calendar_common/calendar_common_popover";
-
-export class CalendarWithRecurrenceCommonPopover extends CalendarCommonPopover {}
