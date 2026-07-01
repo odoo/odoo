34ccf675067f7410c75700728623aae67c22ac41
adch-odoo <adch@odoo.com>
2026-07-01 08:52:21 +0000

    [FIX] html_editor: flaky toolbar click test
    Purpose of this commit:
    
    - Fixes non-deterministic failures in the 'toolbar should not open
    between double and triple click' Hoot test.
    - Because browser-level selectionchange events are dispatched asynchronously
    in the event loop, asserting on the presence of `.o-we-toolbar` in the DOM
    leads to timing race conditions.
    
    Solution:
    
    - Resolves the flakiness by introducing a wrapper method
    `triggerDebouncedUpdateToolbar` in `ToolbarPlugin` and
    refactoring the test to track method call sequences instead
    of asserting on DOM elements. This verifies the scheduled
    debounced updates in a deterministic sequence.
    
    Task: https://runbot.odoo.com/odoo/error/243145
    X-original-commit: 3d420b388abb2b705eef9013ddbe1a3ef10dde8e

--- addons/html_editor/static/src/main/toolbar/toolbar_plugin.js
+++ addons/html_editor/static/src/main/toolbar/toolbar_plugin.js
@@ -217,7 +217,7 @@ export class ToolbarPlugin extends Plugin {
                 if (ev.detail >= 2) {
                     // Delayed open, waiting for a possible triple click.
                     this.onSelectionChangeActive = true;
-                    this.debouncedUpdateToolbar();
+                    this.triggerDebouncedUpdateToolbar();
                 } else {
                     // Fast open, just wait for a possible selection change due
                     // to mouseup.
@@ -247,7 +247,7 @@ export class ToolbarPlugin extends Plugin {
                 if (ev.key?.startsWith("Arrow")) {
                     this.pendingArrowKey = false;
                     this.onSelectionChangeActive = true;
-                    this.debouncedUpdateToolbar();
+                    this.triggerDebouncedUpdateToolbar();
                 }
             });
             if (isMacOS()) {
@@ -255,7 +255,7 @@ export class ToolbarPlugin extends Plugin {
                     if (this.pendingArrowKey && !this.isMouseDown) {
                         this.pendingArrowKey = false;
                         this.onSelectionChangeActive = true;
-                        this.debouncedUpdateToolbar();
+                        this.triggerDebouncedUpdateToolbar();
                     }
                 });
                 this.addDomListener(this.editable, "mousedown", () => (this.isMouseDown = true));
@@ -269,6 +269,13 @@ export class ToolbarPlugin extends Plugin {
         this.debouncedUpdateToolbar.cancel();
         this.overlay.close();
         super.destroy();
+    }
+
+    /**
+     * Schedules a debounced toolbar update.
+     */
+    triggerDebouncedUpdateToolbar() {
+        this.debouncedUpdateToolbar();
     }
 
     /**
