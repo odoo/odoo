40099794101c5f4e9f72fd6a2146ac2f4bae065a
Alay Patel <alap@odoo.com>
2026-07-01 11:30:49 +0000

    [FIX] website, *: update shape color when changing the color palette
    [*] = html_builder
    
    In the commit [1], an edge case was left uncovered where applied
    background/image shapes were not re-rendered after changing the theme
    color palette.
    
    Steps to reproduce:
    
    1. Go to the Website app and drop any snippet.
    2. Apply a background or image shape.
    3. If a background shape is applied, click the **Flip Shape** option.
    4. Go to the **Theme** tab and change the color palette.
    
    Issue:
    The shape color is not updated after changing the color palette.
    
    Reason:
    In the commit [1], an edge case was left uncovered where applied
    background/image shapes were not re-rendered after changing the theme
    color palette, so its color is not refreshed to match newly selected
    palette.
    
    [1]-https://github.com/odoo/odoo/commit/aec8918018b92cbb5cb3dd761824d4
    
    closes odoo/odoo#278243
    
    X-original-commit: bbafbbd8950ec7123ab652851ede5479484eee26
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Alay Patel (alap) <alap@odoo.com>

--- addons/html_builder/static/src/plugins/image/image_shape_option_plugin.js
+++ addons/html_builder/static/src/plugins/image/image_shape_option_plugin.js
@@ -111,7 +111,7 @@ export class ImageShapeOptionPlugin extends Plugin {
         on_snippet_dropped_handlers: ({ snippetEl }) => {
             this.addShapeColorAttribute(snippetEl);
         },
-        on_website_color_updated_handlers: this.syncShapeColorsWithTheme.bind(this),
+        on_website_color_updated_handlers: this.syncImageShapeColorsWithTheme.bind(this),
     };
     setup() {
         this.shapeSvgTextCache = {};
@@ -163,18 +163,22 @@ export class ImageShapeOptionPlugin extends Plugin {
      * Update the shape color (when a theme color is selected) whenever the
      * theme preset color changes.
      *
-     * @param {String} updatedColorVariable - Updated theme color variable value
-     * like 'o-color-*'.
+     * @param {String[]} updatedColorVariables - Updated theme color variables.
      */
-    async syncShapeColorsWithTheme(updatedColorVariable) {
-        if (!updatedColorVariable.startsWith("o-color-")) {
-            return;
+    async syncImageShapeColorsWithTheme(updatedColorVariables) {
+        for (const colorVar of updatedColorVariables) {
+            if (!colorVar.startsWith("o-color-")) {
+                continue;
+            }
+            const selector = `img[data-shape][data-shape-colors*="${colorVar};"], img[data-shape][data-shape-colors$="${colorVar}"]`;
+            await this.refreshImgShapes([...this.document.querySelectorAll(selector)]);
+            await this.config.snippetModel.updateContent(
+                "snippet_custom",
+                async (snippetContent) => {
+                    await this.refreshImgShapes([...snippetContent.querySelectorAll(selector)]);
+                }
+            );
         }
-        const selector = `img[data-shape][data-shape-colors*="${updatedColorVariable};"], img[data-shape][data-shape-colors$="${updatedColorVariable}"]`;
-        await this.refreshImgShapes([...this.document.querySelectorAll(selector)]);
-        await this.config.snippetModel.updateContent("snippet_custom", async (snippetContent) => {
-            await this.refreshImgShapes([...snippetContent.querySelectorAll(selector)]);
-        });
     }
     async refreshImgShapes(shapeEls) {
         // Promise.allSettled is used here to ensure that all shapes are
