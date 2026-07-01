6c660d1535caaad4cf452ee79a35f6cf2e3eb0ec
Ester Andreetto <esand@odoo.com>
2026-07-01 09:00:14 +0000

    [FIX] account: allow zip export of move types
    Steps to reproduce the issue:
    1. Download Accounting
    2. Go to one move type (ex. customer invoices, vendor bills, etc.)
    3. Select some records, click the wheel button and then export ZIP
    4. Error raised: Nothing to export
    
    Cause of the issue:
    Commit 438603ac forward-ported the zip export feature from v17 to v18,
    but failed to adapt the action_export_zip function and import the controller route.
    
    Reason to introduce the fix:
    It has been decided to completly remove the button EXPORT ZIP since
    there are already other buttons that download/export the PDFs in a zip
    file (ex. button Download PDF).
    
    opw-6313750
    
    X-original-commit: 29e228fd7f99a797c42ed70c74f7a5c98692f4bb

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -12,6 +12,7 @@ import logging
 from markupsafe import Markup
 import re
 import os
+import warnings
 from textwrap import shorten
 from urllib.parse import urlencode
 
@@ -6840,6 +6841,7 @@ class AccountMove(models.Model):
     # -------------------------------------------------------------------------
 
     def _get_moves_zip_export_docs(self):
+        warnings.warn("The '_get_moves_zip_export_docs' method is deprecated and has been removed in future versions.", DeprecationWarning)
         docs = set()
         for move in self.filtered(lambda m: m.state == 'posted' and m.is_sale_document()):
             try:
@@ -6852,6 +6854,7 @@ class AccountMove(models.Model):
         return docs, filename
 
     def action_export_zip(self):
+        warnings.warn("The 'action_export_zip' method is deprecated and has been removed in future versions.", DeprecationWarning)
         attachment_ids, filename = self._get_moves_zip_export_docs()
         if not attachment_ids:
             raise UserError(_('Nothing to export.'))
