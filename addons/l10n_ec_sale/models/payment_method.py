68e59402afda1a5af188166149c67af671c979b9
Valeriya (vchu) <vchu@odoo.com>
2026-07-01 11:50:13 +0200

    [FIX] l10n_ec_sale: set payment method SRI field on payment methods
    Since bcfeed4b24f5155c111c3866e779bb2f119b9da8 the field used a default
    function relying on self.code, which is always falsy on creation.
    Make this field computed to correctly set the value.
    
    closes odoo/odoo#273311
    
    Signed-off-by: Sven Führ (svfu) <svfu@odoo.com>

--- addons/l10n_ec_sale/models/payment_method.py
+++ addons/l10n_ec_sale/models/payment_method.py
@@ -1,19 +1,10 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class PaymentMethod(models.Model):
     _inherit = 'payment.method'
-
-    def _get_l10n_ec_sri_payment_id(self):
-        if self.code == "card":
-            return self.env.ref('l10n_ec.P19', raise_if_not_found=False)
-        elif self.code == "bank_transfer":
-            return self.env.ref('l10n_ec.P20', raise_if_not_found=False)
-        elif self.code == "bank_account":
-            return self.env.ref('l10n_ec.P21', raise_if_not_found=False)
-        return self.env['l10n_ec.sri.payment']
 
     def _get_fiscal_country_codes(self):
         return ','.join(self.env.companies.mapped('account_fiscal_country_id.code'))
@@ -21,7 +12,21 @@ class PaymentMethod(models.Model):
     l10n_ec_sri_payment_id = fields.Many2one(
         comodel_name="l10n_ec.sri.payment",
         string="SRI Payment Method",
-        default=_get_l10n_ec_sri_payment_id,
+        compute='_compute_l10n_ec_sri_payment_id',
+        store=True,
+        readonly=False,
     )
 
     fiscal_country_codes = fields.Char(store=False, default=_get_fiscal_country_codes)
+
+    @api.depends('code')
+    def _compute_l10n_ec_sri_payment_id(self):
+        for method in self:
+            if method.code == 'card':
+                method.l10n_ec_sri_payment_id = self.env.ref('l10n_ec.P19', raise_if_not_found=False)
+            elif method.code == 'bank_transfer':
+                method.l10n_ec_sri_payment_id = self.env.ref('l10n_ec.P20', raise_if_not_found=False)
+            elif method.code == 'bank_account':
+                method.l10n_ec_sri_payment_id = self.env.ref('l10n_ec.P21', raise_if_not_found=False)
+            else:
+                method.l10n_ec_sri_payment_id = False
