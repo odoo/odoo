8ee7007b71be4aef171bf720ffe75c3586b20a00
Alay Patel <alap@odoo.com>
2026-07-01 11:30:49 +0000

    [FIX] website, *: update shape color when changing the color palette
    [*] = html_builder
    
    Steps to reproduce:
    1. Go to the Website app and drop any snippet.
    2. Apply a background or image shape.
    3. If a background shape is applied, click the "Flip Shape" option.
    4. Go to the "Theme" tab and change the color palette.
    
    Issue:
    The shape color is not updated after changing the color palette.
    
    Reason:
    The background/image shapes were not re-rendered after changing the
    theme color palette, so its color was not refreshed to match newly
    selected palette.
    
    X-original-commit: bbafbbd8950ec7123ab652851ede5479484eee26
    
    task-5438314
    
    closes odoo/odoo#276135
    
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/website/static/src/builder/plugins/customize_website_plugin.js
+++ addons/website/static/src/builder/plugins/customize_website_plugin.js
@@ -38,7 +38,7 @@ import { loadImage } from "@html_editor/utils/image_processing";
  */
 
 /**
- * @typedef {((color: string) => void)[]} on_website_color_updated_handlers
+ * @typedef {((colors: string[]) => void)[]} on_website_color_updated_handlers
  */
 
 export const NO_IMAGE_SELECTION = Symbol.for("NoImageSelection");
@@ -1078,7 +1078,7 @@ export class CustomizeWebsiteColorAction extends BuilderAction {
         }
         setBuilderCSSVariables(getHtmlStyle(this.document));
         await Promise.allSettled(
-            this.getResource("on_website_color_updated_handlers").map((handler) => handler(color))
+            this.getResource("on_website_color_updated_handlers").map((handler) => handler([color]))
         );
     }
 }
