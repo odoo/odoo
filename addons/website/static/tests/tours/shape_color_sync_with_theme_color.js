da124a746c7788605d85af5b098fff99eb292767
Alay Patel <alap@odoo.com>
2026-07-01 11:30:49 +0000

    [FIX] website, *: update shape color when changing the color palette
    [*] = html_builder
    
    In the commit [1], an edge case was left uncovered where applied
    background/image shapes were not re-rendered after changing the theme
    color palette.
    
    Steps to reproduce:
    
    1. Go to the Website app and drop any snippet.
    2. Apply a background or image shape.
    3. If a background shape is applied, click the **Flip Shape** option.
    4. Go to the **Theme** tab and change the color palette.
    
    Issue:
    The shape color is not updated after changing the color palette.
    
    Reason:
    In the commit [1], an edge case was left uncovered where applied
    background/image shapes were not re-rendered after changing the theme
    color palette, so its color is not refreshed to match newly selected
    palette.
    
    [1]-https://github.com/odoo/odoo/commit/aec8918018b92cbb5cb3dd761824d4
    
    closes odoo/odoo#278325
    
    X-original-commit: bbafbbd8950ec7123ab652851ede5479484eee26
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Alay Patel (alap) <alap@odoo.com>

--- addons/website/static/tests/tours/shape_color_sync_with_theme_color.js
+++ addons/website/static/tests/tours/shape_color_sync_with_theme_color.js
@@ -12,15 +12,16 @@ import {
 } from "@website/js/tours/tour_utils";
 
 const TEST_COLOR_HEX = "1AEF74";
+const TEST_COLOR_HEX_2 = "7ED1ED";
 
-function verifyShapeColorsUpdated(trigger) {
+function verifyShapeColorsUpdated(trigger, expectedHex) {
     return {
         content: "Verify that the shape colors are updated",
         trigger,
         async run() {
             const backgroundImageUrl =
                 this.anchor.querySelector(".o_we_shape").style.backgroundImage;
-            if (!backgroundImageUrl.includes(TEST_COLOR_HEX)) {
+            if (!backgroundImageUrl.includes(expectedHex)) {
                 throw new Error(
                     "Updating the theme color should also update the background shape color."
                 );
@@ -28,7 +29,7 @@ function verifyShapeColorsUpdated(trigger) {
             await assertSvgColors(
                 this.anchor.querySelector("img[data-shape]"),
                 "Updating the theme color should update the image shape SVG color.",
-                [`#${TEST_COLOR_HEX}`]
+                [`#${expectedHex}`]
             );
         },
     };
@@ -60,6 +61,34 @@ registerWebsitePreviewTour(
         ...goToTheme(),
         clickOnElement("color option", "[data-label='Colors'] button"),
         clickOnElement(
+            "Click color palette",
+            "div[data-container-title='Colors'] .o-hb-select-wrapper svg"
+        ),
+        clickOnElement(
+            "Change color palette",
+            `.o-color-palette-dropdown [data-action-value="'default-light-1'"]`
+        ),
+        {
+            content: "Wait for no loading",
+            trigger: "body:not(:has(.o_we_ui_loading))",
+        },
+        verifyShapeColorsUpdated(":iframe .s_company_team", TEST_COLOR_HEX_2),
+        goBackToBlocks(),
+        clickOnElement(
+            "custom category block",
+            ".o_snippet[name='Custom'] .o_snippet_thumbnail_area"
+        ),
+        verifyShapeColorsUpdated(
+            ":iframe .o_snippets_preview_row .s_company_team",
+            TEST_COLOR_HEX_2
+        ),
+        {
+            content: "Press ESC to close the 'Insert snippet' dialog",
+            trigger: ":iframe",
+            run: "press Escape",
+        },
+        ...goToTheme(),
+        clickOnElement(
             "color picker of theme preset 1",
             ".hb-sliding-panel-content .o_we_color_preview"
         ),
@@ -77,7 +106,7 @@ registerWebsitePreviewTour(
             content: "Wait for no loading",
             trigger: "body:not(:has(.o_we_ui_loading)) :iframe body:not(:has(.o_loading_screen))",
         },
-        verifyShapeColorsUpdated(":iframe .s_company_team"),
+        verifyShapeColorsUpdated(":iframe .s_company_team", TEST_COLOR_HEX),
         clickOnElement("any image in the snippet", ":iframe .s_company_team img[data-shape]"),
         assertCssVariable(
             "background-color",
@@ -89,6 +118,6 @@ registerWebsitePreviewTour(
             "custom category block",
             ".o_snippet[name='Custom'] .o_snippet_thumbnail_area"
         ),
-        verifyShapeColorsUpdated(":iframe .o_snippets_preview_row .s_company_team"),
+        verifyShapeColorsUpdated(":iframe .o_snippets_preview_row .s_company_team", TEST_COLOR_HEX),
     ]
 );
