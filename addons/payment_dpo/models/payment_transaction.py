odoo/odoo

commit 97ec8a3e72d9d48a0a9620c53feea533421b9d67

[REF] payment(_*): use a generic redirect form for redirect payments
Prior to this commit, each redirect payment provider used its own
template to render the redirect form. This required as many different
view records as there are redirect payment providers, and the
transaction values were needlessly mapped to a Python dict first,
before being mapped again to the redirect form inputs.

This commit adds a generic redirect form template in `payment` for all
redirect providers to use.

closes odoo/odoo#250848

Related: odoo/upgrade#9572
Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>
Co-authored-by: Antoine Vandevenne (anv) <anv@odoo.com>

addons/payment_dpo/models/payment_transaction.py
@@ -29,9 +29,12 @@ class PaymentTransaction(models.Model):
             return super()._get_specific_rendering_values(processing_values)
 
         transaction_token = self._dpo_create_token()
-        api_url = f'https://secure.3gdirectpay.com/payv2.php?ID={transaction_token}'
-
-        return {'api_url': api_url}
+        return {
+            'api_url': 'https://secure.3gdirectpay.com/payv2.php',
+            'url_params': {
+                'ID': transaction_token,
+            },
+        }
 
     def _dpo_create_token(self):
         """ Create a transaction token and return the response data.

addons/payment_dpo/tests/test_payment_transaction.py
@@ -15,7 +15,10 @@ class TestPaymentTransaction(DPOCommon):
         tx = self._create_transaction(flow='redirect')
         transaction_token = "dummy_token"
         expected_values = {
-            'api_url': f'https://secure.3gdirectpay.com/payv2.php?ID={transaction_token}',
+            'api_url': 'https://secure.3gdirectpay.com/payv2.php',
+            'url_params': {
+                'ID': transaction_token,
+            },
         }
         with patch(
             'odoo.addons.payment_dpo.models.payment_transaction.PaymentTransaction'

addons/payment_aps/models/payment_transaction.py
@@ -52,7 +52,7 @@ class PaymentTransaction(models.Model):
         converted_amount = payment_utils.to_minor_currency_units(self.amount, self.currency_id)
         base_url = self.provider_id.get_base_url()
         payment_option = aps_utils.get_payment_option(self.payment_method_id.code)
-        rendering_values = {
+        url_params = {
             'command': 'PURCHASE',
             'access_code': self.provider_id.aps_access_code,
             'merchant_identifier': self.provider_id.aps_merchant_identifier,
@@ -64,14 +64,14 @@ class PaymentTransaction(models.Model):
             'return_url': urls.urljoin(base_url, APSController._return_url),
         }
         if payment_option:  # Not included if the payment method is 'card'.
-            rendering_values['payment_option'] = payment_option
-        rendering_values.update({
-            'signature': self.provider_id._aps_calculate_signature(
-                rendering_values, incoming=False
-            ),
+            url_params['payment_option'] = payment_option
+        url_params['signature'] = self.provider_id._aps_calculate_signature(
+            url_params, incoming=False
+        )
+        return {
             'api_url': self.provider_id._aps_get_api_url(),
-        })
-        return rendering_values
+            'url_params': url_params,
+        }
 
     @api.model
     def _extract_reference(self, provider_code, payment_data):

addons/payment_aps/tests/test_payment_transaction.py
@@ -26,18 +26,20 @@ class TestPaymentTransaction(APSCommon):
 
         converted_amount = payment_utils.to_minor_currency_units(self.amount, self.currency)
         expected_values = {
-            'command': 'PURCHASE',
-            'access_code': self.provider.aps_access_code,
-            'merchant_identifier': self.provider.aps_merchant_identifier,
-            'merchant_reference': tx.reference,
-            'payment_option': 'UNKNOWN',
-            'amount': str(converted_amount),
-            'currency': self.currency.name,
-            'language': tx.partner_lang[:2],
-            'customer_email': tx.partner_id.email_normalized,
-            'return_url': self._build_url(APSController._return_url),
-            'signature': 'c9b9f35a607606c045f8882e762a4a4a35572cf230fe1cd45fa18d7c8681aeb9',
             'api_url': self.provider._aps_get_api_url(),
+            'url_params': {
+                'command': 'PURCHASE',
+                'access_code': self.provider.aps_access_code,
+                'merchant_identifier': self.provider.aps_merchant_identifier,
+                'merchant_reference': tx.reference,
+                'payment_option': 'UNKNOWN',
+                'amount': str(converted_amount),
+                'currency': self.currency.name,
+                'language': tx.partner_lang[:2],
+                'customer_email': tx.partner_id.email_normalized,
+                'return_url': self._build_url(APSController._return_url),
+                'signature': 'c9b9f35a607606c045f8882e762a4a4a35572cf230fe1cd45fa18d7c8681aeb9'
+            },
         }
         self.assertEqual(tx._get_specific_rendering_values(None), expected_values)
 
@@ -54,9 +56,9 @@ class TestPaymentTransaction(APSCommon):
             'currency',
             'language',
             'customer_email',
-            'signature',
-            'payment_option',
             'return_url',
+            'payment_option',
+            'signature',
         ]
         processing_values = tx._get_processing_values()
         form_info = self._extract_values_from_html_form(processing_values['redirect_form_html'])

addons/payment_asiapay/models/payment_transaction.py
@@ -81,24 +81,27 @@ class PaymentTransaction(models.Model):
         # The lang is taken from the context rather than from the partner because it is not required
         # to be logged in to make a payment, and because the lang is not always set on the partner.
         lang = self.env.context.get('lang') or 'en_US'
-        rendering_values = {
-            'merchant_id': self.provider_id.asiapay_merchant_id,
+        return_url = urls.urljoin(base_url, AsiaPayController._return_url)
+        url_params = {
+            'merchantId': self.provider_id.asiapay_merchant_id,
             'amount': self.amount,
-            'reference': self.reference,
-            'currency_code': const.CURRENCY_MAPPING[self.provider_id.available_currency_ids[0].name],
-            'mps_mode': 'SCP',
-            'return_url': urls.urljoin(base_url, AsiaPayController._return_url),
-            'payment_type': 'N',
-            'language': get_language_code(lang),
-            'payment_method': const.PAYMENT_METHODS_MAPPING.get(self.payment_method_id.code, 'ALL'),
+            'orderRef': self.reference,
+            'currCode': const.CURRENCY_MAPPING[self.provider_id.available_currency_ids[0].name],
+            'mpsMode': 'SCP',
+            'successUrl': return_url,
+            'failUrl': return_url,
+            'cancelUrl':  return_url,
+            'payType': 'N',
+            'lang': get_language_code(lang),
+            'payMethod': const.PAYMENT_METHODS_MAPPING.get(self.payment_method_id.code, 'ALL'),
         }
-        rendering_values.update({
-            'secure_hash': self.provider_id._asiapay_calculate_signature(
-                rendering_values, incoming=False
-            ),
-            'api_url': self.provider_id._asiapay_get_api_url()
-        })
-        return rendering_values
+        url_params['secureHash'] = self.provider_id._asiapay_calculate_signature(
+            url_params, incoming=False
+        )
+        return {
+            'api_url': self.provider_id._asiapay_get_api_url(),
+            'url_params': url_params,
+        }
 
     @api.model
     def _extract_reference(self, provider_code, payment_data):

addons/payment_asiapay/tests/test_payment_transaction.py
@@ -67,20 +67,25 @@ class TestPaymentTransaction(AsiaPayCommon, PaymentHttpCommon):
             '._asiapay_calculate_signature', return_value='dummy_signature'
         ):
             rendering_values = tx._get_specific_rendering_values(None)
+            return_url = self._build_url('/payment/asiapay/return')
             self.assertDictEqual(
                 rendering_values,
                 {
-                    'amount': tx.amount,
                     'api_url': tx.provider_id._asiapay_get_api_url(),
-                    'currency_code': const.CURRENCY_MAPPING[tx.currency_id.name],
-                    'language': const.LANGUAGE_CODES_MAPPING['en'],
-                    'merchant_id': tx.provider_id.asiapay_merchant_id,
-                    'mps_mode': 'SCP',
-                    'payment_method': 'ALL',
-                    'payment_type': 'N',
-                    'reference': tx.reference,
-                    'return_url': self._build_url('/payment/asiapay/return'),
-                    'secure_hash': 'dummy_signature',
+                    'url_params': {
+                        'amount': tx.amount,
+                        'currCode': const.CURRENCY_MAPPING[tx.currency_id.name],
+                        'lang': const.LANGUAGE_CODES_MAPPING['en'],
+                        'merchantId': tx.provider_id.asiapay_merchant_id,
+                        'mpsMode': 'SCP',
+                        'payMethod': 'ALL',
+                        'payType': 'N',
+                        'orderRef': tx.reference,
+                        'successUrl': return_url,
+                        'failUrl': return_url,
+                        'cancelUrl': return_url,
+                        'secureHash': 'dummy_signature',
+                    },
                 }
             )
 

addons/payment_buckaroo/models/payment_transaction.py
@@ -27,8 +27,7 @@ class PaymentTransaction(models.Model):
             return super()._get_specific_rendering_values(processing_values)
 
         return_url = urls.urljoin(self.provider_id.get_base_url(), BuckarooController._return_url)
-        rendering_values = {
-            'api_url': self.provider_id._buckaroo_get_api_url(),
+        url_params = {
             'Brq_websitekey': self.provider_id.buckaroo_website_key,
             'Brq_amount': self.amount,
             'Brq_currency': self.currency_id.name,
@@ -40,11 +39,14 @@ class PaymentTransaction(models.Model):
             'Brq_returnreject': return_url,
         }
         if self.partner_lang:
-            rendering_values['Brq_culture'] = self.partner_lang.replace('_', '-')
-        rendering_values['Brq_signature'] = self.provider_id._buckaroo_generate_digital_sign(
-            rendering_values, incoming=False
+            url_params['Brq_culture'] = self.partner_lang.replace('_', '-')
+        url_params['Brq_signature'] = self.provider_id._buckaroo_generate_digital_sign(
+            url_params, incoming=False
         )
-        return rendering_values
+        return {
+            'api_url': self.provider_id._buckaroo_get_api_url(),
+            'url_params': url_params,
+        }
 
     @api.model
     def _extract_reference(self, provider_code, payment_data):

addons/payment_custom/models/payment_transaction.py
@@ -26,7 +26,9 @@ class PaymentTransaction(models.Model):
 
         return {
             'api_url': CustomController._process_url,
-            'reference': self.reference,
+            'url_params': {
+                'reference': self.reference,
+            },
         }
 
     def _get_communication(self):

commit 65e5f2be8a59f3230a603f9982472629bfa60dc0

[FIX] payment_dpo: redirect to the payment page with a GET request
When a customer paid through DPO, they landed on the DPO error page
("Not Valid - this transaction is no longer valid") instead of the
payment page, even though the transaction token was created correctly.

The customer is redirected to `https://secure.3gdirectpay.com/payv2.php`
with the transaction token passed as the `ID` parameter. `payv2.php`
answers with a 302 redirect to `payv3.php`. Per HTTP semantics, a 302
turns the request into a GET and drops the body, so when `ID` is sent in
the body of a POST it never reaches `payv3.php`: the customer arrives at
`payv3.php?` with no token and DPO rejects it as invalid.

This regressed with 97ec8a3e72d9d48a0a9620c53feea533421b9d67, which moved
redirect providers to the generic redirect form. Before that commit, the
token was part of the action URL itself (`payv2.php?ID=<token>`), so it
stayed in the query string across the redirect. The refactor moved `ID`
into `url_params` rendered as a hidden input, and since `http_method` was
left unset the generic form defaults to POST, putting `ID` in the body.

Set `http_method` to "get" so the token is serialized back into the
query string and survives the `payv2.php` -> `payv3.php` redirect,
restoring the pre-refactor behavior. This also matches DPO's documented
convention of passing the token in the URL query string.

opw-6312732

addons/payment_dpo/models/payment_transaction.py
@@ -30,6 +30,7 @@ class PaymentTransaction(models.Model):
         transaction_token = self._dpo_create_token()
         return {
             "api_url": "https://secure.3gdirectpay.com/payv2.php",
+            "http_method": "get",  # `payv2.php` redirects to `payv3.php`, dropping POSTed params.
             "url_params": {"ID": transaction_token},
         }
 

addons/payment_dpo/tests/test_payment_transaction.py
@@ -15,6 +15,7 @@ class TestPaymentTransaction(DPOCommon):
         transaction_token = "dummy_token"
         expected_values = {
             "api_url": "https://secure.3gdirectpay.com/payv2.php",
+            "http_method": "get",
             "url_params": {"ID": transaction_token},
         }
         with patch(