478e77ad67267aedc942bc217cd5a907605e2173
Saurabh <sauc@odoo.com>
2026-07-01 15:21:45 +0530

    [FIX] l10n_in_hr_holidays: adapt TimeOffCalendarSidePanel to Owl 3 changes
    The Owl 3 migration of `TimeOffCalendarSidePanel` replaced the
    `onWillUpdateProps` with `asyncComputed`.
    
    The `l10n_in_hr_holidays` patch was still relying on the previous
    implementation, making it incompatible with the migrated component.
    
    Adapt the patch to follow the new implementation by exposing
    optional holidays through asyncComputed and extending the leaveState
    getter accordingly.
    
    runbot-error-941221
    runbot-error-941222
    runbot-error-941223
    runbot-error-941224
    runbot-error-941225

--- addons/l10n_in_hr_holidays/static/src/views/calendar/side_panel/calendar_side_panel.js
+++ addons/l10n_in_hr_holidays/static/src/views/calendar/side_panel/calendar_side_panel.js
@@ -1,24 +1,29 @@
-import { proxy } from "@odoo/owl";
+import { asyncComputed } from "@odoo/owl";
 import { patch } from "@web/core/utils/patch";
 import { TimeOffCalendarSidePanel } from "@hr_holidays/views/calendar/calendar_side_panel/calendar_side_panel";
 
 patch(TimeOffCalendarSidePanel.prototype, {
     setup() {
         super.setup();
-        this.leaveState = proxy({
-            ...this.leaveState,
-            optionalHolidays: [],
-        });
+        this.optionalHolidays = asyncComputed(() => this.getOptionalHolidays());
     },
 
-    async updateSpecialDays() {
-        await super.updateSpecialDays();
+    async getOptionalHolidays() {
         const { rangeStart, rangeEnd } = this.props.model;
         const specialDays = await this._specialDaysCache.read(rangeStart, rangeEnd);
         specialDays["optionalHolidays"].forEach((optionalHoliday) => {
             optionalHoliday.start = luxon.DateTime.fromISO(optionalHoliday.start);
             optionalHoliday.end = luxon.DateTime.fromISO(optionalHoliday.end);
         });
-        this.leaveState.optionalHolidays = specialDays["optionalHolidays"];
+        return specialDays["optionalHolidays"];
+    },
+
+    get leaveState() {
+        return {
+            bankHolidays: this.specialDays()?.bankHolidays || [],
+            mandatoryDays: this.specialDays()?.mandatoryDays || [],
+            holidays: this.holidays() || [],
+            optionalHolidays: this.optionalHolidays() || [],
+        };
     }
 })
