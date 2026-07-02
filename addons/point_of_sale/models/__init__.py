899ba2d791f629febed0acfe1151943458c14ad7
Shubham Agarwal(shag) <shag@odoo.com>
2026-07-02 12:46:44 +0530

    [IMP] point_of_sale: apply removal strategy for tracked products with lots
    Before this commit:
    - The lot/serial selection popup was always shown when adding products tracked
    by lots.
    
    Following this commit:
    - Products configured with FIFO/LIFO removal strategies are automatically
    assigned the corresponding lot and added directly to the cart without opening
    the selection popup.
    - If no removal strategy is configured, the existing lot selection behavior is
    preserved.
    
    task-6226577
    
    closes odoo/odoo#273323
    
    X-original-commit: 7ba49980d9f78fad55e0e15ea965b49e33501eb5
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Shubham Agarwal (shag) <shag@odoo.com>

--- addons/point_of_sale/models/__init__.py
+++ addons/point_of_sale/models/__init__.py
@@ -22,6 +22,7 @@ from . import pos_snooze
 from . import product_pricelist
 from . import product_attribute
 from . import product_category
+from . import product_removal
 from . import product_product
 from . import product_template
 from . import uom
