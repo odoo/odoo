3d99325f35b468fddc2bbbac58636f9e95da156d
svs-odoo <svs@odoo.com>
2026-07-03 09:57:20 +0200

    [REM] stock, *: remove dead code
    *: mrp, mrp_subcontracting, purchase_stock, sale_stock
    
    - Remove `show_operation`, this field is not used anymore and a old
      '#TODO' comment was asking to remove it since years;
    - Remove `package_type_sequence_id` unused field;
    - Remove `_compute_goods_received_not_invoiced` and
      `_compute_goods_delivered_not_invoiced`, those methods weren't used
      anymore since [^1]. In addition, their files were deleted too (in
      fact, they weren't even imported so it was kind of they were already
      deleted.)
    
    [^1]: https://github.com/odoo/odoo/pull/247625/

--- addons/mrp/tests/test_stock_report.py
+++ addons/mrp/tests/test_stock_report.py
@@ -148,7 +148,6 @@ class TestMrpStockReports(TestReportsCommon):
         mo_1.picking_type_id.create_backorder = 'ask'
         mo_1.action_confirm()
         pick = mo_1.move_raw_ids.move_orig_ids.picking_id
-        pick.picking_type_id.show_operations = True  # Could be false without demo data, as the lot group is disabled
         pick_form = Form(pick)
         with Form(pick.move_ids, view='stock.view_stock_move_operations') as form:
             with form.move_line_ids.edit(0) as move_line:
