edeb4ef0e925f3774d0e8434d9ff349fcef6e056
David Monnom (moda) <moda@odoo.com>
2026-07-03 15:27:41 +0200

    [IMP] point_of_sale: avoid running useless mobile test
    Before this commit all tests were run on both desktop and mobile, even
    if the test was only relevant for one of the two platforms.
    
    Mobile test should only test the UI of the mobile version of the PoS
    to be able to check that all the features are working properly on mobile
    
    Desktop version is the main version of the test, which will test
    business logic and features.

--- addons/point_of_sale/tests/__init__.py
+++ addons/point_of_sale/tests/__init__.py
@@ -26,3 +26,4 @@ from . import test_res_config_settings
 from . import test_pos_product_variants
 from . import test_generic_localization
 from . import test_order_receipt
+from . import test_frontend_desktop_only
