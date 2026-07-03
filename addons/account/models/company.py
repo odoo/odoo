ce7ed9e284887a65e50c63f4d65ee4d488e67bf3
Hoang Tran <thhoang.tr@gmail.com>
2026-07-03 06:53:52 +0000

    [FIX] account: create batch payment sequence after init
    closes odoo/odoo#276313
    
    X-original-commit: bedb3481a89e18ee396924a84ffad72f6e6ac1d0
    Signed-off-by: Julien Alardot (jual) <jual@odoo.com>

--- addons/account/models/company.py
+++ addons/account/models/company.py
@@ -438,6 +438,25 @@ class ResCompany(models.Model):
         for company in self:
             onboardings.with_company(company)._search_or_create_progress()
 
+    def _get_batch_payment_sequence_values(self):
+        self.ensure_one()
+        return {
+            'name': _("Group Payments Number Sequence"),
+            'implementation': 'no_gap',
+            'padding': 5,
+            'use_date_range': True,
+            'company_id': self.id,
+            'prefix': 'GROUP/%(range_year)s/',
+        }
+
+    def _create_batch_payment_sequence(self):
+        for company in self:
+            if not company.batch_payment_sequence_id:
+                sequence = self.env['ir.sequence'].sudo().create(
+                    company._get_batch_payment_sequence_values()
+                )
+                company.batch_payment_sequence_id = sequence
+
     @api.model_create_multi
     def create(self, vals_list):
         companies = super().create(vals_list)
@@ -450,15 +469,7 @@ class ResCompany(models.Model):
                         install_demo=False,
                     )
                 self.env.cr.precommit.add(try_loading)
-            if not company.batch_payment_sequence_id:
-                company.batch_payment_sequence_id = self.env['ir.sequence'].sudo().create({
-                    'name': _("Group Payments Number Sequence"),
-                    'implementation': 'no_gap',
-                    'padding': 5,
-                    'use_date_range': True,
-                    'company_id': company.id,
-                    'prefix': 'GROUP/%(year)s/',
-                })
+            company._create_batch_payment_sequence()
         return companies
 
     def get_new_account_code(self, current_code, old_prefix, new_prefix):
