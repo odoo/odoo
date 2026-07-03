b5ca2e0f6e36bf1aa69f6f924c008af2f3e332d5
Harsh Modi <hamo@odoo.com>
2026-07-03 10:21:27 +0530

    [IMP] l10n_in_ewaybill: Show existing e-waybill reference
    Before this commit :
    Users had no visibility of existing e-waybills across Accounting document
    and Inventory document. Could lead to duplicate e-waybill creation.
    
    After this commit :
    Shows an alert when an e-waybill already exists.
    Works both ways on Accounting document
    and Inventory document
    
    task-6310632

--- addons/l10n_in_purchase_stock/models/stock_picking.py
+++ addons/l10n_in_purchase_stock/models/stock_picking.py
@@ -17,3 +17,10 @@ class StockPicking(models.Model):
         if purchase_order := self.purchase_id:
             purchase_order.fiscal_position_id
         return super()._l10n_in_get_fiscal_position()
+
+    def _l10n_in_related_account_moves(self):
+        if purchase_order := self.purchase_id:
+            # We filter out refunds (Credit Note) because, returns should be handled through
+            # Pickings and Ewaybill don't support Credit Note as valid document
+            return purchase_order.invoice_ids.filtered(lambda m: m.move_type == 'in_invoice')
+        return super()._l10n_in_related_account_moves()
