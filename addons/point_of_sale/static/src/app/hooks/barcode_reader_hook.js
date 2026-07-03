c950ff46ddff8194ef6f5c116c4238d63f9ed3a8
Jean Schoenlaub <jesc@odoo.com>
2026-07-03 14:50:55 +0200

    [REF] pos: replace useEffect with onWillDestroy in useBarcodeReader
    Replaced useLayoutEffect with onWillDestroy from @odoo/owl because
    useLayoutEffect is deprecated in OWL3.
    
    The useLayoutEffect refactored in this PR has test coverage
    — below are some tests that failed when the effect was commented out, and are now passing:
    
    TestUi/MobileTestUi.test_07_pos_barcodes_scan
    TestUi/MobileTestUi.test_09_pos_barcodes_scan_product_packaging
    TestUi/MobileTestUi.test_GS1_pos_barcodes_scan
    see commented-out runbot build: https://runbot.odoo.com/runbot/batch/2604795/build/115380428

--- addons/point_of_sale/static/src/app/hooks/barcode_reader_hook.js
+++ addons/point_of_sale/static/src/app/hooks/barcode_reader_hook.js
@@ -1,12 +1,9 @@
+import { onWillDestroy } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
-import { useLayoutEffect } from "@web/owl2/utils";
 
 export function useBarcodeReader(callbackMap, exclusive = false) {
     const barcodeReader = useService("barcode_reader");
     if (barcodeReader) {
-        useLayoutEffect(
-            () => barcodeReader.register(callbackMap, exclusive),
-            () => []
-        );
+        onWillDestroy(barcodeReader.register(callbackMap, exclusive));
     }
 }
