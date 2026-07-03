alkuneemi/Luxara

[FIX] point_of_sale, pos_mrp: adapt kit qty in pos picking
**Steps to reproduce:**

- Create a product A, tracked by lots
- Create a kit product, include a component A
- Change the UoM to 0.5
- Go to the PoS, order this kit product
- Also order the component A, with a quantity of 2
- Pay for it, ask for an invoice
- Go to the created picking
- The Demand column is correctly computed and is 0.5
- The Quantity column is wrong and is 2

**Why the fix:**

When getting the data from https://github.com/odoo/odoo/blob/e0d84c7fbb270d0d1f82572daefa96c2978d3785/addons/point_of_sale/models/stock_picking.py#L283
we always get the component's line, as the
move's product is the component, even if it
used to be the kit product's move. This is
because when exploding a kit's moves, it
gets the kit's component as a product instead
of keeping the kit product.

This was introducing a weird behavior because
we took the quantity from the component line,
and not from the kit line, meaning the kit
would always have the same quantity as the
component.

We now check if the move is actually a kit
product's move, and if it is we adapt the
qty to correct one by fetching the correct
line's qty, and adapting it with the
correct UoM.

Changing the line in itself would not work,
as the kit itself is not tracked by lots,
so we would not enter https://github.com/odoo/odoo/blob/e0d84c7fbb270d0d1f82572daefa96c2978d3785/addons/point_of_sale/models/stock_picking.py#L284
and the move line would not be correctly
created.

opw-6153000

addons/pos_mrp/tests/test_pos_mrp_flow.py
@@ -636,3 +636,71 @@ class TestPosMrp(TestPointOfSaleCommon):
             {'product_id': product_2.id, 'total_cost': 20},
             {'product_id': product_1.id, 'total_cost': 10},
         ])
+
+    def test_pos_picking_kit_qty(self):
+        """
+        Tests that when ordering a kit and its component, the move quantity in the generated
+        picking is the kit's quantity and not the component's.
+        """
+        kit, component = self.env['product.template'].create([
+            {
+                'name': 'Kit Product',
+                'available_in_pos': True,
+                'list_price': 10.0,
+                'type': 'product',
+            },
+            {
+                'name': 'Kit Component',
+                'available_in_pos': True,
+                'list_price': 5.0,
+                'tracking': 'lot',
+                'type': 'product',
+            }
+        ])
+        self.env['mrp.bom'].create({
+            'product_tmpl_id': kit.id,
+            'product_qty': 1.0,
+            'type': 'phantom',
+            'bom_line_ids': [Command.create({
+                'product_id': component.product_variant_id.id,
+                'product_qty': 0.5,
+            })],
+        })
+        self.pos_config.open_ui()
+        order = self.env['pos.order'].create({
+            'company_id': self.env.company.id,
+            'session_id': self.pos_config.current_session_id.id,
+            'partner_id': self.partner1.id,
+            'lines': [
+                Command.create({
+                    'product_id': kit.product_variant_id.id,
+                    'price_unit': 10.0,
+                    'qty': 1.0,
+                    'price_subtotal': 10.0,
+                    'price_subtotal_incl': 10.0,
+                }),
+                Command.create({
+                    'product_id': component.product_variant_id.id,
+                    'price_unit': 5.0,
+                    'qty': 2.0,
+                    'price_subtotal': 10.0,
+                    'price_subtotal_incl': 10.0,
+                    'pack_lot_ids': [Command.create({
+                        'lot_name': 'lot',
+                    })],
+                }),
+            ],
+            'amount_total': 20.0,
+            'amount_tax': 0.0,
+            'amount_paid': 20.0,
+            'amount_return': 0.0,
+        })
+        payment_context = {"active_ids": order.ids, "active_id": order.id}
+        order_payment = self.env['pos.make.payment'].with_context(**payment_context).create({
+            'amount': order.amount_total,
+            'payment_method_id': self.cash_payment_method.id,
+        })
+        order_payment.with_context(**payment_context).check()
+
+        self.assertEqual(order.picking_ids.move_ids[0].quantity, 2)
+        self.assertEqual(order.picking_ids.move_ids[1].quantity, 0.5)

odoo/odoo

X-original-commit: 86a3b31833ad5ac892f79870c7d73a0b590848a3

addons/pos_mrp/tests/test_pos_mrp_flow.py
@@ -763,3 +763,73 @@ class TestPosMrp(TestPointOfSaleCommon):
             {'product_id': product_2.id, 'total_cost': 20},
             {'product_id': product_1.id, 'total_cost': 10},
         ])
+
+    def test_pos_picking_kit_qty(self):
+        """
+        Tests that when ordering a kit and its component, the move quantity in the generated
+        picking is the kit's quantity and not the component's.
+        """
+        kit, component = self.env['product.template'].create([
+            {
+                'name': 'Kit Product',
+                'available_in_pos': True,
+                'list_price': 10.0,
+                'type': 'consu',
+                'is_storable': True,
+            },
+            {
+                'name': 'Kit Component',
+                'available_in_pos': True,
+                'list_price': 5.0,
+                'tracking': 'lot',
+                'type': 'consu',
+                'is_storable': True,
+            }
+        ])
+        self.env['mrp.bom'].create({
+            'product_tmpl_id': kit.id,
+            'product_qty': 1.0,
+            'type': 'phantom',
+            'bom_line_ids': [Command.create({
+                'product_id': component.product_variant_id.id,
+                'product_qty': 0.5,
+            })],
+        })
+        self.pos_config.open_ui()
+        order = self.env['pos.order'].create({
+            'company_id': self.env.company.id,
+            'session_id': self.pos_config.current_session_id.id,
+            'partner_id': self.partner1.id,
+            'lines': [
+                Command.create({
+                    'product_id': kit.product_variant_id.id,
+                    'price_unit': 10.0,
+                    'qty': 1.0,
+                    'price_subtotal': 10.0,
+                    'price_subtotal_incl': 10.0,
+                }),
+                Command.create({
+                    'product_id': component.product_variant_id.id,
+                    'price_unit': 5.0,
+                    'qty': 2.0,
+                    'price_subtotal': 10.0,
+                    'price_subtotal_incl': 10.0,
+                    'pack_lot_ids': [Command.create({
+                        'lot_name': 'lot',
+                    })],
+                }),
+            ],
+            'amount_total': 20.0,
+            'amount_tax': 0.0,
+            'amount_paid': 20.0,
+            'amount_return': 0.0,
+        })
+        payment_context = {"active_ids": order.ids, "active_id": order.id}
+        order_payment = self.env['pos.make.payment'].with_context(**payment_context).create({
+            'amount': order.amount_total,
+            'payment_method_id': self.cash_payment_method.id,
+        })
+        order_payment.with_context(**payment_context).check()
+
+        self.assertEqual(order.picking_ids.move_ids[0].quantity, 2)
+        self.assertEqual(order.picking_ids.move_ids[1].quantity, 0.5)