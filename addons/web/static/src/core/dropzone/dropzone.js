45b3762f28215afe8eb7d5a03e0430e792d9d29b
Jean Schoenlaub <jesc@odoo.com>
2026-07-04 09:41:06 +0200

    [REF] web : Remove useLayoutEffect

--- addons/web/static/src/core/dropzone/dropzone.js
+++ addons/web/static/src/core/dropzone/dropzone.js
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useRef } from "@web/owl2/utils";
+import { useRef } from "@web/owl2/utils";
 import { Component, proxy } from "@odoo/owl";
 
 export class Dropzone extends Component {
@@ -16,11 +16,11 @@ export class Dropzone extends Component {
         this.state = proxy({
             isDraggingInside: false,
         });
-        useLayoutEffect(() => {
-            const getEl =
-                typeof this.props.ref === "function" ? this.props.ref : () => this.props.ref.el;
-            const { top, left, width, height } = getEl().getBoundingClientRect();
-            this.root.el.style = `top:${top}px;left:${left}px;width:${width}px;height:${height}px;`;
-        });
+        // useLayoutEffect(() => {
+        //     const getEl =
+        //         typeof this.props.ref === "function" ? this.props.ref : () => this.props.ref.el;
+        //     const { top, left, width, height } = getEl().getBoundingClientRect();
+        //     this.root.el.style = `top:${top}px;left:${left}px;width:${width}px;height:${height}px;`;
+        // });
     }
 }
