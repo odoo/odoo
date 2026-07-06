3ce58052a11c54e529fa7b75d69563e3a4ec52ef
dhha-odoo <dhha@odoo.com>
2026-07-06 09:10:16 +0000

    [FIX] stock: prevent traceback when splitting a done picking
    Issue before this commit:
    =========================
    When splitting a done picking that contains at least one stock move
    whose done quantity is less than the demanded quantity (product_uom_qty),
    an expected singleton traceback occurs.
    
    Steps to Reproduce:
    =========================
    - Install the stock module with demo data.
    - Create a delivery picking for any product with a demand of 5.
    - Set the done quantity to 2.
    - Validate the picking without creating a backorder.
    - Try to split the validated/done picking.
    - An expected singleton traceback is raised.
    
    Cause of the issue:
    =========================
    Previously, attempting to split a done picking simply returned because
    there was nothing left to split.
    
    After this [PR](https://github.com/odoo/odoo/pull/224952), the split action
    calls message_post() to post a note on the
    original picking of the generated backorder. However, no backorder is created
    when splitting a done picking since there is no remaining quantity to split.
    As a result, message_post() is called on an empty recordset, leading to an
    expected singleton traceback.
    
    With This Commit:
    =========================
    Splitting a done picking has no functional purpose, as there is nothing left
    to split. In this case, simply return without performing any action.
    This preserves the previous behavior and prevents the traceback.
    
    X-original-commit: ffb7011b7b21010724ae637aa5f861576a398bce

--- addons/stock/models/stock_picking.py
+++ addons/stock/models/stock_picking.py
@@ -1615,6 +1615,8 @@ class StockPicking(models.Model):
         return True
 
     def action_split_transfer(self):
+        if self.state == 'done':
+            return
         if all(m.uom_id.is_zero(m.quantity) for m in self.move_ids):
             raise UserError(_("%s: Nothing to split. Fill the quantities you want in a new transfer in the done quantities", self.display_name))
         if all(m.uom_id.compare(m.quantity, m.product_uom_qty) == 0 for m in self.move_ids):
