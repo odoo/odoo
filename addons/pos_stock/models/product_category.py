c0f3eea029627e13ea195ebdd1c7b855bab2b294
Ajit Singh <aksi@odoo.com>
2026-07-06 11:13:00 +0530

    [FIX] point_of_sale,pos_stock: move field to correct module
    after commit [1] we are not able to open pos session, if we have installed only
    point_of_sale
    
    [1] https://github.com/odoo/odoo/commit/f20b2d22dbec2e6a5e682539489ab94d9c96fb21
    
    cause:
    - field `removal_strategy_id` comes from stock and from saas-19.3 point_of_sale
    is not dependent on stock.
    
    FIx:
    - correct module should be `pos_stock`
    
    opw-6364126

---
+++ addons/pos_stock/models/product_category.py
@@ -0,0 +1,12 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import api, models
+
+
+class ProductCategory(models.Model):
+    _inherit = 'product.category'
+
+    @api.model
+    def _load_pos_data_fields(self, config_id):
+        pos_data_fields = super()._load_pos_data_fields(config_id)
+        pos_data_fields.append('removal_strategy_id')
+        return pos_data_fields
