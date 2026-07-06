dff52643d4f344ef040fe135338fa1f889b88835
Paolo Gatti <pgi@odoo.com>
2026-07-06 13:19:55 +0000

    [FIX] l10n_it_edi{,ndd}: TD07 Simplified invoice fixes
    - Added the BolloVirtuale in the Simplified invoice template
    - Now it's possible to force the Simplified format on
      exported invoice when the `l10n_it_document_type` is set
      to a simplified one
    - Factored the Italian partner recognition (_l10n_it_edi_is_italian)
    - Added a check on the invoice, no simplified format
      for non-domestic / PA partners
    
    X-original-commit: aa6ef4b60aee1eb26f25f89a2212c152c2faf76a

--- addons/l10n_it_edi/tests/test_edi_export.py
+++ addons/l10n_it_edi/tests/test_edi_export.py
@@ -246,7 +246,7 @@ class TestItEdiExport(TestItEdi):
         invoice.action_post()
         self._assert_export_invoice(invoice, 'invoice_total_400_VAT_simplified.xml')
 
-    def test_invoice_more_400_simplified(self):
+    def test_invoice_more_400_is_not_simplified(self):
         invoice = self.env['account.move'].with_company(self.company).create({
             'move_type': 'out_invoice',
             'invoice_date': '2022-03-24',
@@ -260,9 +260,10 @@ class TestItEdiExport(TestItEdi):
                 }),
             ],
         })
+        self.assertFalse(invoice._l10n_it_edi_is_simplified())
         self.assertEqual(['l10n_it_edi_partner_address_missing'], list(invoice._l10n_it_edi_export_data_check().keys()))
 
-    def test_invoice_non_domestic_simplified(self):
+    def test_invoice_non_domestic_is_not_simplified(self):
         invoice = self.env['account.move'].with_company(self.company).create({
             'move_type': 'out_invoice',
             'invoice_date': '2022-03-24',
@@ -276,6 +277,7 @@ class TestItEdiExport(TestItEdi):
                 }),
             ],
         })
+        self.assertFalse(invoice._l10n_it_edi_is_simplified())
         self.assertEqual(['l10n_it_edi_partner_address_missing'], list(invoice._l10n_it_edi_export_data_check().keys()))
 
     def test_bill_refund_no_reconcile(self):
@@ -743,3 +745,43 @@ class TestItEdiExport(TestItEdi):
 
         uom_nodes = invoice_tree.xpath("//*[local-name()='DettaglioLinee']/*[local-name()='UnitaMisura']")
         self.assertEqual(uom_nodes[0].text, 'm2')
+
+    # Simplified tests ----------------------------------------------------
+    def _force_simplified(self, partner):
+        td07 = self.env['l10n_it.document.type'].search([('code', '=', 'TD07')], limit=1)
+        return self.env['account.move'].with_company(self.company).create({
+            'move_type': 'out_invoice',
+            'invoice_date': '2022-03-24',
+            'invoice_date_due': '2022-03-24',
+            'partner_id': partner.id,
+            'invoice_line_ids': [
+                Command.create({
+                    'name': 'cheap_line',
+                    'price_unit': 100.00,
+                    'tax_ids': [Command.set(self.default_tax.ids)],
+                }),
+            ],
+            'l10n_it_document_type': td07.id,
+        })
+
+    def _get_simplified_errors(self, moves):
+        return [
+            k
+            for k, v in moves._l10n_it_edi_is_simplified_checks().items()
+            if v.get('level') in ('warning', 'error')
+        ]
+
+    def test_invoice_non_domestic_force_simplified(self):
+        """ If the user forces a simplified document type (i.e. TD07) on a non-italian partner, an error is raised """
+        invoice = self._force_simplified(self.american_partner)
+        self.assertEqual(['l10n_it_edi_move_simplified_partner'], self._get_simplified_errors(invoice))
+
+    def test_invoice_domestic_force_simplified(self):
+        """ If the user forces a simplified document type (i.e. TD07) on an italian partner, it works """
+        invoice = self._force_simplified(self.italian_partner_a)
+        self.assertEqual([], self._get_simplified_errors(invoice))
+
+    def test_invoice_pa_force_simplified(self):
+        """ If the user forces a simplified document type (i.e. TD07) on an italian PA partner, errors are raised """
+        invoice = self._force_simplified(self.italian_partner_b)
+        self.assertEqual(['l10n_it_edi_move_simplified_partner'], self._get_simplified_errors(invoice))
