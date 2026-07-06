6ae6564f2ce3d554992f737f582b871654862ef6
romo <romo@odoo.com>
2026-07-06 16:16:48 +0200

    [IMP] website: introduce animated number snippet
    Introduce a new snippet that displays a number counting up/down from
    configured start and end values. It can be used to highlight statistics
    and metrics growth or decrease.
    
    Timing controls are shared with on-appearance animations so users do not
    have to configure the same delay and duration twice when combining both
    effects.
    
    The number is set as formattable but not editable as its text content
    will changed during the animation, but we still want the user to be
    able to customize the style.
    
    task-5358340

---
+++ addons/website/static/src/snippets/s_animated_number/animated_number.edit.js
@@ -0,0 +1,29 @@
+import { registry } from "@web/core/registry";
+import { AnimatedNumber } from "./animated_number";
+
+export const AnimatedNumberEdit = (I) =>
+    class extends I {
+        start() {
+            super.start();
+            this.addListener(this.el, "click", () => (this.forcedStop = true), { once: true });
+        }
+
+        setNumberValue(value) {
+            this.protectSyncAfterAsync(() => super.setNumberValue(value))();
+        }
+
+        getConfigurationSnapshot() {
+            const style = getComputedStyle(this.el);
+            return JSON.stringify({
+                startValue: this.el.dataset.startValue,
+                endValue: this.el.dataset.endValue,
+                animationDelay: style.animationDelay,
+                animationDuration: style.animationDuration,
+            });
+        }
+    };
+
+registry.category("public.interactions.edit").add("website.animated_number", {
+    Interaction: AnimatedNumber,
+    mixin: AnimatedNumberEdit,
+});
