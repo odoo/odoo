5263c0a6c80375e6993653f06786a4e38ae4a1d5
Mongy (abmn) <abmn@odoo.com>
2026-07-06 12:55:25 +0000

    [FIX] l10n_pl_edi: QR code generation in PDF
    Due to this (https://github.com/odoo/odoo/pull/244421/changes/546a4425884e61080a7a7e8d2b2a97c8a2e3f38e), PDF generation was broken.
    
    Removing the manual encoding of the data.
    
    runbot-941292
    
    closes odoo/odoo#274729
    
    X-original-commit: 7eb4491d72637f36e5dab21233f04d0e019dbc42
    Signed-off-by: Thomas Becquevort (thbe) <thbe@odoo.com>
    Signed-off-by: Abdulrahman Mongy (abmn) <abmn@odoo.com>

--- addons/l10n_pl_edi/models/account_move.py
+++ addons/l10n_pl_edi/models/account_move.py
@@ -12,7 +12,7 @@ from decimal import Decimal
 
 from odoo import Command, api, fields, models
 from odoo.exceptions import UserError
-from odoo.tools import float_compare, float_is_zero, float_repr, mute_logger, OrderedSet
+from odoo.tools import BinaryBytes, float_compare, float_is_zero, float_repr, mute_logger, OrderedSet
 from odoo.tools.business_data import split_vat
 
 from odoo.addons.l10n_pl_edi.tools.ksef_api_service import KsefApiService
@@ -304,21 +304,19 @@ class AccountMove(models.Model):
                 f"{base_link}"
                 f"{compact(self.company_id.vat)}/"
                 f"{self.invoice_date.strftime('%d-%m-%Y')}/"
-                f"{base64.urlsafe_b64encode(sha256(base64.b64decode(self.l10n_pl_edi_attachment_file)).digest()).decode()}"
+                f"{base64.urlsafe_b64encode(sha256(self.l10n_pl_edi_attachment_file).digest()).decode()}"
             )
         return ""
 
     def _l10n_pl_edi_generate_qr(self):
         self.ensure_one()
-        return base64.b64encode(
-            self.env['ir.actions.report'].barcode(
-                barcode_type='QR',
-                value=self._l10n_pl_edi_generate_qr_link(),
-                width=180,
-                height=180,
-                quiet=0,
-            )
-        )
+        return BinaryBytes(self.env['ir.actions.report'].barcode(
+            barcode_type='QR',
+            value=self._l10n_pl_edi_generate_qr_link(),
+            width=180,
+            height=180,
+            quiet=0,
+        ))
 
     def _l10n_pl_edi_get_status_mapping(self):
         """
