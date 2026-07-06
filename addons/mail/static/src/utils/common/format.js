6b5904c6e8c5e9ecb20bb10166a1de4a4e59e450
Didier (did) <did@odoo.com>
2026-07-06 09:39:51 +0000

    [FIX] mail: fix localhost url detection
    Before this commit, url like `http://localhost:8069` were ignored by the
    `urlRegexp` because it requires a Top-Level Domain. This commit makes
    the Top-Level Domain optional.
    
    closes odoo/odoo#274570
    
    X-original-commit: 3e2ee05076774ba4fd478b4024825bcbeb58c9bc
    Signed-off-by: Didier Debondt (did) <did@odoo.com>

--- addons/mail/static/src/utils/common/format.js
+++ addons/mail/static/src/utils/common/format.js
@@ -28,7 +28,7 @@ import { getOrigin } from "@web/core/utils/urls";
 import { setAttributes } from "@web/core/utils/xml";
 
 const urlRegexp =
-    /\b(?:https?:\/\/\d{1,3}(?:\.\d{1,3}){3}|(?:https?:\/\/|(?:www\.))[-a-z0-9@:%._+~#=\u00C0-\u024F\u1E00-\u1EFF]{1,256}\.[a-z]{2,13})\b(?:[-a-z0-9@:%_+~#?&[\]^|{}`\\'$//=\u00C0-\u024F\u1E00-\u1EFF]|[.]*[-a-z0-9@:%_+~#?&[\]^|{}`\\'$//=\u00C0-\u024F\u1E00-\u1EFF]|,(?!$| )|\.(?!$| |\.)|;(?!$| ))*/gi;
+    /\b(?:https?:\/\/\d{1,3}(?:\.\d{1,3}){3}|(?:https?:\/\/|(?:www\.))[-a-z0-9@:%._+~#=\u00C0-\u024F\u1E00-\u1EFF]{1,256}(?:\.{1})?(?:[a-z]{2,13}))\b(?:[-a-z0-9@:%_+~#?&[\]^|{}`\\'$//=\u00C0-\u024F\u1E00-\u1EFF]|[.]*[-a-z0-9@:%_+~#?&[\]^|{}`\\'$//=\u00C0-\u024F\u1E00-\u1EFF]|,(?!$| )|\.(?!$| |\.)|;(?!$| ))*/gi;
 const messageUrlRegExp = new RegExp(`^${escapeRegExp(getOrigin())}/mail/message/(\\d+)$`);
 const MENTION_CLASSNAMES = new Set([
     "o_mail_redirect",
