ab29b2d1cd5352f4315554dc61bf7cef2f497ff9
Bayron Alpízar Quesada <bayalpiizar777@gmail.com>
2026-07-06 22:27:47 -0600

    feat(l10n_cr_fe): mapeo de factura lee configuracion por empresa en vez de parametros globales

--- addons/l10n_cr_fe_crlibre/tests/test_account_move_mapping.py
+++ addons/l10n_cr_fe_crlibre/tests/test_account_move_mapping.py
@@ -6,32 +6,34 @@ class TestAccountMoveMapping(TransactionCase):
 
     def setUp(self):
         super().setUp()
+        self.env['l10n_cr.fe.config'].create({
+            'company_id': self.env.company.id,
+            'environment': 'stag',
+            'identification_type': '01',
+            'identification_number': '702320717',
+            'legal_name': 'Frutas Demo SA',
+            'economic_activity_code': '011101',
+            'province': '1', 'canton': '01', 'district': '08', 'neighborhood': '01',
+            'address_detail': 'Local de demostración',
+            'email': 'demo@frutasdemo.cr',
+        })
         self.partner = self.env['res.partner'].create({
             'name': 'Cliente Demo',
             'vat': '102340567',
         })
-        product = self.env['product.product'].create({'name': 'Producto demo'})
+        self.product = self.env['product.product'].create({
+            'name': 'Producto demo',
+        })
         self.invoice = self.env['account.move'].create({
             'move_type': 'out_invoice',
             'partner_id': self.partner.id,
             'invoice_line_ids': [(0, 0, {
-                'product_id': product.id,
+                'product_id': self.product.id,
                 'quantity': 1,
                 'price_unit': 1000.0,
                 'name': 'Producto demo',
             })],
         })
-
-    def test_build_detalles_has_required_fields(self):
-        detalles = self.invoice._l10n_cr_fe_build_detalles()
-        self.assertEqual(len(detalles), 1)
-        d = detalles[0]
-        for field in ('codigoCABYS', 'subTotal', 'impuestoAsumidoEmisorFabrica',
-                      'impuestoNeto', 'cantidad', 'unidadMedida', 'detalle',
-                      'precioUnitario', 'montoTotal', 'montoTotalLinea'):
-            self.assertIn(field, d)
-        self.assertEqual(d['cantidad'], 1.0)
-        self.assertEqual(d['precioUnitario'], 1000.0)
 
     def test_build_clave_params(self):
         params = self.invoice._l10n_cr_fe_build_clave_params()
@@ -41,14 +43,16 @@ class TestAccountMoveMapping(TransactionCase):
         self.assertEqual(len(params['codigoSeguridad']), 8)
         self.assertTrue(params['codigoSeguridad'].isdigit())
 
-    def test_build_genxml_params_serializes_detalles(self):
+    def test_build_genxml_params_uses_company_config(self):
         import json
-        detalles = self.invoice._l10n_cr_fe_build_detalles()
+        detalles = [{'codigoCABYS': '0111101000000', 'cantidad': 1, 'unidadMedida': 'Unid',
+                     'detalle': 'x', 'precioUnitario': 1000.0, 'montoTotal': 1000.0,
+                     'subTotal': 1000.0, 'baseImponible': 1000.0,
+                     'impuestoAsumidoEmisorFabrica': 0, 'impuestoNeto': 0.0,
+                     'montoTotalLinea': 1000.0}]
         params = self.invoice._l10n_cr_fe_build_genxml_params('5' * 50, '0' * 20, detalles)
-        self.assertEqual(params['clave'], '5' * 50)
-        self.assertEqual(params['consecutivo'], '0' * 20)
+        self.assertEqual(params['emisor_nombre'], 'Frutas Demo SA')
+        self.assertEqual(params['emisor_num_identif'], '702320717')
         self.assertEqual(params['receptor_num_identif'], '102340567')
-        # detalles y medios_pago van serializados como JSON string
         self.assertIsInstance(params['detalles'], str)
         self.assertEqual(json.loads(params['detalles'])[0]['codigoCABYS'], '0111101000000')
-        self.assertIsInstance(params['medios_pago'], str)
