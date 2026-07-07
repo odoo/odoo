d32c2ae10cd793259ec937b3bae4b4bc64dfaff8
Bayron Alpízar Quesada <bayalpiizar777@gmail.com>
2026-07-06 22:41:20 -0600

    feat(l10n_cr_fe): campo CABYS por producto

--- addons/l10n_cr_fe_crlibre/models/__init__.py
+++ addons/l10n_cr_fe_crlibre/models/__init__.py
@@ -1,3 +1,4 @@
 from . import crlibre_client
 from . import fe_config
+from . import product_template
 from . import account_move
--- addons/l10n_cr_fe_crlibre/tests/__init__.py
+++ addons/l10n_cr_fe_crlibre/tests/__init__.py
@@ -1,4 +1,5 @@
 from . import test_crlibre_client
 from . import test_fe_config
+from . import test_product_template
 from . import test_account_move_mapping
 from . import test_generate_action
