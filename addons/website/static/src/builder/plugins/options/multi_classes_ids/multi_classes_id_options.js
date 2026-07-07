fe7600330e3acd5f6cc635eb2ba7e712c55dccad
imranrafique <frontend.devs786@gmail.com>
2026-07-07 12:25:45 +0500

    [REVERT]

--- addons/website/static/src/builder/plugins/options/multi_classes_ids/multi_classes_id_options.js
+++
@@ -1,9 +0,0 @@
-import {BaseOptionComponent} from "@html_builder/core/utils";
-
-export class SnippetOptions extends BaseOptionComponent {
-    static template = "multi_classes_ids.SnippetOptions";
-
-    setup() {
-        super.setup();
-    }
-}
