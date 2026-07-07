b04d631f8f24e232bbdeb02ad57476be839390f8
David <dacat@odoo.com>
2026-07-07 08:33:54 +0000

    [FIX] base_vat: prevent double VIES vat call
    When we create a company with a EU VAT, we used to do 2 IAP call to
    verify the VAT number. One was on the create() and the other one on the
    write().
    
    For performance reason and because the vies check service may limit ip address,
    the verification was already disable when importing files
    (in both create and write).
    
    This commit remove the compute on the create one (and keep the one on
    write), so that it only do 1 IAP call to verify the VAT.
    
    Task-6139346
    
    X-original-commit: 4f71d35fe723adaf73c93cd7c933c000d4338cd9

--- addons/base_vat/tests/test_vat_numbers.py
+++ addons/base_vat/tests/test_vat_numbers.py
@@ -127,9 +127,9 @@ class TestStructure(TransactionCase):
         with patch('odoo.addons.base_vat.models.res_partner.ResPartner._check_vies_validity_iap', TestStructure._check_vies_validity_iap):
             partner = self.env["res.partner"].create({
                 'name': 'Dummy Partner',
-                'vat': 'BE0477472701',
                 'country_id': self.env.ref("base.be").id,
             })
+            partner.vat = 'BE0477472701'
             self.assertEqual(partner.vies_valid, True)
 
         with patch('odoo.addons.base_vat.models.res_partner.ResPartner._check_vies_validity_iap',
