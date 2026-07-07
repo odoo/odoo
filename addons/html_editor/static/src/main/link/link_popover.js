1f897813066c844dd04b7bd1d499a0982395a10d
Benoit Socias (bso) <bso@odoo.com>
2026-07-07 11:23:57 +0000

    [FIX] html_editor: avoid failing when pressing tab within link preview
    Since [1] elements within the link preview can be focused.
    However, when pressing tab an error is raised.
    
    This commit prevents this error from happening.
    
    [1]: https://github.com/odoo/odoo/commit/34db19f4a2b42d7e41205d7d793f5b3f408c19af
    
    task-6366337
    
    closes odoo/odoo#275715
    
    X-original-commit: 6355d9f93b7d153be6680f3f8312f86982a8de9f
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Benoit Socias (bso) <bso@odoo.com>

--- addons/html_editor/static/src/main/link/link_popover.js
+++ addons/html_editor/static/src/main/link/link_popover.js
@@ -242,6 +242,9 @@ export class LinkPopover extends Component {
     }
 
     onKeydown(ev) {
+        if (!this.editingWrapper?.el) {
+            return;
+        }
         if (ev.key === "Escape") {
             ev.preventDefault();
             ev.stopImmediatePropagation();
