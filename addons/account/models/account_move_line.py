419ae0fe8411b673944e56758c123b2229721b37
Waleed Elgamal <waelg@odoo.com>
2026-07-08 13:40:56 +0000

    [FIX] account: correct analytic distribution on discount allocation with mutltiple lines
    **Steps to reproduce:**
    1. Install Accounting and l10n_be and switch to the Belgian company
    2. In the settings, set the discount account (`708000`) on Customer Invoices under "Default Accounts" and enable analytic accounting
    3. Go to [Accounting -> Configuration -> Analytic Accounts] and create 4 new accounts with "Project" plan (i.e 1,2,3,4)
    4. Create a new invoice with two lines, each having 2 of the analytic accounts with 50% each.
    5. Set the price to 1000 and a 10% discount for each line then save.
    6. Edit the second line and set the discount to 20%.
    7. Open the Journal Items tab
    
    **Issue:**
     - When an invoice contains multiple lines with analytic distributions,
    changing the discount percentage on any line other than the first fails
    to correctly update the analytic distribution percentages on the
    corresponding discount journal items.
    - The analytic account distribution splits the percentage evenly across
    both accounts event if they are not split 50/50
    
    **Why this happens:**
    - This occurred because `_compute_discount_allocation_needed` iterated over
    `self` to populate target changes. When only one line was modified, `self`
    contains that line only which is correctly updated with the new analytic
    distribution. Later in the execution in `_sync_dynamic_line`, particularly in
    https://github.com/odoo/odoo/blob/5a14360705a55f4d91edf39c936d7a5d8573044b/addons/account/models/account_move.py#L2263-L2274
    The first line in `computed_needed` is what gets set in res, and subsequent
    lines only modify the field if it's monetary. So if the second invoice line
    is the one  updated, it will never override the `analytic_distribution` with
    the updated values, leaving stale values in that field.
    - The code iterated directly over `line.analytic_distribution`
    dictionary keys (the account IDs) rather than its `.items()`. This caused it to
    ignore the individual percentage value splits (e.g. 60/40), accumulating the
    un-weighted full discount amount to each account ID.
    https://github.com/odoo/odoo/blob/5a14360705a55f4d91edf39c936d7a5d8573044b/addons/account/models/account_move_line.py#L1044-L1052
    
    **Fix:**
    - Change the processing loop  inside `_compute_discount_allocation_needed` from
    `self` to `self.move_id.line_ids` to calculate the correct `analytic_distribution` across
    all records.
    - Applying the factored weight ratio
      (`amount * (percentage / 100.0)`) to `distribution_totals`
    
    opw-6362084
    
    closes odoo/odoo#280909
    
    X-original-commit: 1e54146494d584eaf71a675b90ea116c5d4d8d11
    Signed-off-by: Waleed Elgamal (waelg) <waelg@odoo.com>

--- addons/account/models/account_move_line.py
+++ addons/account/models/account_move_line.py
@@ -1019,14 +1019,15 @@ class AccountMoveLine(models.Model):
         distribution_totals = defaultdict(lambda: defaultdict(float))
         for line, discounted_amounts in line2discounted_amount.items():
             for account, _amount_currency, amount in discounted_amounts:
-                for analytic_account_id in line.analytic_distribution or {}:
+                for analytic_account_id, percentage in (line.analytic_distribution or {}).items():
+                    weighted_amount = amount * percentage / 100
                     distribution_totals[frozendict({
                         'move_id': line.move_id.id,
                         'account_id': account.id,
                         'currency_rate': line.currency_rate,
-                    })][analytic_account_id] += amount
+                    })][analytic_account_id] += weighted_amount
 
-        for line in self:
+        for line in self.move_id.line_ids:
             line.discount_allocation_dirty = True
             if line not in line2discounted_amount:
                 line.discount_allocation_needed = False
