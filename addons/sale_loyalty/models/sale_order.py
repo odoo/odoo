ce422c57864d1f4f4bd128b6a2d83d23bc0869c4
Kirra Hyde <kihy@odoo.com>
2026-07-08 15:08:08 -0700

    [FIX] sale_loyalty: send invoice for zero amount orders
    When a shopper pays for an order entirely with a reward (e.g. a discount
    code), no payment.transaction is ever created. So, the order is
    confirmed through `_validate_order` on sale orders instead of
    `_post_process` on payment transactions. With automatic invoicing
    enabled, `_validate_order` creates and posts an invoice for the zero
    amount order. But nothing sends the invoice to the customer. The sending
    logic lives in payment.transaction's _post_process/_send_invoice.
    
    This fix borrows the sending logic from the `_send_invoice` method on
    payment transactions. That way, `_validate_order` will also send
    invoices to customers.
    
    opw-6363334
    
    closes odoo/odoo#275190
    
    Signed-off-by: Louis Tinel (loti) <loti@odoo.com>

--- addons/sale_loyalty/models/sale_order.py
+++ addons/sale_loyalty/models/sale_order.py
@@ -1533,3 +1533,20 @@ class SaleOrder(models.Model):
             self._force_lines_to_invoice_policy_order()
             invoice = self._create_invoices(final=True)
             invoice.action_post()
+
+            if invoice._is_ready_to_be_sent():
+                invoice.is_move_sent = True  # Mark invoice as sent
+                send_context = {'allow_raising': False, 'allow_fallback_pdf': True}
+
+                default_template_param = (
+                    self.env['ir.config_parameter']
+                    .sudo()
+                    .get_param('sale.default_invoice_email_template', False)
+                )
+
+                if default_template_param:
+                    mail_template = self.env['mail.template'].sudo().browse(int(default_template_param))
+                    if mail_template.exists():
+                        send_context['mail_template'] = mail_template
+
+                self.env['account.move.send']._generate_and_send_invoices(invoice, **send_context)
