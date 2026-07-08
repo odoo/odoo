264fcb14c5f3fef67e26381123ced09001952ab5
Benoit Socias (bso) <bso@odoo.com>
2026-07-08 07:35:14 +0000

    [FIX] html_editor: hide mobile toolbar during image preview
    When displaying the full screen image preview lightbox on mobile, the
    toolbar remains displayed. Because of this, the toolbar of the lightbox
    cannot be accessed.
    
    This commit hides the toolbar when a lightbox is displayed.
    
    This commit also hides the keyboard on mobile when opening lightbox.
    
    task-6370220
    
    closes odoo/odoo#286267
    
    X-original-commit: b533aa384579062f3e7f5b4702c6cc916d9b5c00
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Benoit Socias (bso) <bso@odoo.com>

--- addons/html_editor/static/src/main/media/image_plugin.js
+++ addons/html_editor/static/src/main/media/image_plugin.js
@@ -274,6 +274,7 @@ export class ImagePlugin extends Plugin {
             downloadUrl: targetedImg.src,
         };
         this.document.getSelection().collapseToEnd();
+        this.document.activeElement.blur();
         this.fileViewer.open(fileModel);
     }
 
