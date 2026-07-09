963fdf60f15292479b55f1861ce46b69d4763678
Laetitia (ldau) <ldau@odoo.com>
2026-07-09 10:50:52 +0200

    [IMP] sale: redesign empty screen
    This commit introduces a new illustration for the Services & Material
    empty screen.
    
    task-6341698

--- addons/sale/__manifest__.py
+++ addons/sale/__manifest__.py
@@ -54,7 +54,8 @@ This module contains all the common features of Sales Management and eCommerce.
     "demo": ["data/product_demo.xml", "data/sale_demo.xml"],
     "assets": {
         "web.assets_backend": [
-            "sale/static/src/scss/sale_onboarding.scss",
+             "sale/static/src/scss/sale_onboarding.scss",
+            "sale/static/src/scss/services_and_material.scss",
             "sale/static/src/js/badge_extra_price/*",
             "sale/static/src/js/sale_action_helper/*",
             "sale/static/src/js/combo_configurator_dialog/*",
@@ -87,7 +88,10 @@ This module contains all the common features of Sales Management and eCommerce.
             "sale/static/src/xml/**/*",
             "sale/static/src/views/**/*",
         ],
-        "web.assets_web_dark": ["sale/static/src/js/dashboard/**/*.dark.scss"],
+        'web.assets_web_dark': [
+            "sale/static/src/js/dashboard/**/*.dark.scss",
+            "sale/static/src/scss/services_and_material.dark.scss",
+        ],
         "web.assets_frontend": [
             "sale/static/src/interactions/**/*",
             "sale/static/src/scss/sale_portal.scss",
