26e855b9ba283ee0fba65ca08d6654276a51c973
kcv-odoo <kcv@odoo.com>
2026-07-09 09:30:04 +0000

    [FIX] portal: make commercial fields editable
    Before this commit, there was no way to edit commercial fields after a user entered a company name in the
    address form. Setting a company name created a parent company, and editing commercial fields was blocked
    because the customer had a parent record.
    
    This commit allows commercial fields to be edited again for customer addresses whose parent company has
    only a single direct child.
    
    X-original-commit: 42f9b885f79e4fd22577c94517e0abe47fdd3af8

--- addons/portal/models/res_partner.py
+++ addons/portal/models/res_partner.py
@@ -22,12 +22,25 @@ class ResPartner(models.Model):
         self.ensure_one()
         return True
 
+    # TODO: Rename me in master
     def can_edit_vat(self):
-        """ `vat` is a commercial field, synced between the parent (commercial
-        entity) and the children. Only the commercial entity should be able to
-        edit it (as in backend)."""
+        """Return whether the commercial fields of the partner can be edited by the customer.
+
+        Commercial fields are synced between the parent (commercial entity) and the children. Only
+        the commercial entity should be able to edit it (as in backend).
+        """
         self.ensure_one()
-        return not self.parent_id
+        if not self.parent_id:
+            return True
+
+        contact_childs = self.commercial_partner_id.child_ids.filtered(
+            lambda child: child.type == "contact"
+        )
+        return (
+            len(contact_childs) == 1
+            and contact_childs == self
+            and not self.commercial_partner_id.user_ids
+        )
 
     def _can_be_edited_by_current_customer(self, **kwargs):
         """Return whether partner can be edited by current user."""
