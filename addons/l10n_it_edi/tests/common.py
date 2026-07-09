4fef2c5b69fac10594fac81b149d542ee4621b13
Ester Andreetto <esand@odoo.com>
2026-07-09 12:50:43 +0000

    [FIX] l10n_it_edi{_*}: import vat taxes and their pension fund taxes in bill
    Issue before the commit:
    During the import of Italian e-invoices, Pension Fund taxes (Cassa
    Previdenziale) linked to a 0% VAT rate with a specific exemption
    reason (Natura, e.g., N2.2) are ignored and not applied to the invoice
    lines.
    
    Steps to reproduce the issue:
    1. Download Accounting and l10n_it
    2. Go to vendor -> bills and import the bill in the ticket
    3. Check that taxes are not imported as expected
    
    Cause of the issue:
    The system incorrectly used the Natura to search for the Pension Fund
    tax itself. Fiscally, the Natura belongs to the related VAT, not the
    Pension Fund. This incorrect domain caused the tax search to fail. The
    Pension Fund tax should not have a Natura setted.
    
    Reason to introduce the fix:
    To correctly apply Pension Fund taxes to exempt invoice lines.
    
    opw-6357133
    
    closes odoo/odoo#277779
    
    X-original-commit: 4806c3d28aed0e9e41e0a0585c7af2f5d22e1a87
    Signed-off-by: Paolo Gatti (pgi) <pgi@odoo.com>
    Signed-off-by: Ester Andreetto (esand) <esand@odoo.com>

--- addons/l10n_it_edi/tests/common.py
+++ addons/l10n_it_edi/tests/common.py
@@ -129,6 +129,24 @@ class TestItEdi(AccountTestInvoicingCommon):
             'amount_type': 'percent',
         })
 
+        cls.vat_0_N1_purchase = cls.env['account.tax'].with_company(cls.company).create({
+            'name': "VAT 0% Natura N1",
+            'amount': 0.0,
+            'amount_type': 'percent',
+            'type_tax_use': 'purchase',
+            'l10n_it_exempt_reason': 'N1',
+            'l10n_it_law_reference': 'test',
+        })
+
+        cls.vat_0_N2_1_purchase = cls.env['account.tax'].with_company(cls.company).create({
+            'name': "VAT 0% Natura N2.1",
+            'amount': 0.0,
+            'amount_type': 'percent',
+            'type_tax_use': 'purchase',
+            'l10n_it_exempt_reason': 'N2.1',
+            'l10n_it_law_reference': 'test',
+        })
+
         cls.module = 'l10n_it_edi'
 
     def _assert_export_invoice(self, invoice, filename):
