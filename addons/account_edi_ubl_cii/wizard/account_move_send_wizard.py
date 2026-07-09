76ccd3fea599c15ac60fd01f0ff15edc03ed55b2
roto-odoo <roto@odoo.com>
2026-07-09 12:11:23 +0000

    [FIX] account_edi_ubl_cii: No unsupported attachments in non ubl
    In the send wizard, don't mark attachments as "unsupported" if
    the sending method is not ubl dependent
    
    Also fix a typo in "Unspported"
    
    no-task
    
    closes odoo/odoo#275564
    
    X-original-commit: 4d7002e98318c619181dba1696abf807ec3b49c3
    Signed-off-by: Laurent Smet (las) <las@odoo.com>
    Signed-off-by: Romain Toulmonde (roto) <roto@odoo.com>

--- addons/account_edi_ubl_cii/wizard/account_move_send_wizard.py
+++ addons/account_edi_ubl_cii/wizard/account_move_send_wizard.py
@@ -7,11 +7,15 @@ class AccountMoveSendWizard(models.TransientModel):
     @api.depends('invoice_edi_format', 'mail_attachments_widget')
     def _compute_attachments_not_supported(self):
         for wizard in self:
+            if not self.env['res.partner']._get_ubl_cii_formats_info().get(wizard.invoice_edi_format):
+                wizard.attachments_not_supported = {}
+                continue
+
             _attachments_to_embed, attachments_not_supported = wizard._get_ubl_available_attachments(
                 wizard.mail_attachments_widget,
                 wizard.invoice_edi_format
             )
             wizard.attachments_not_supported = {
-                attachment.id: _("Unspported file type via %s", wizard.invoice_edi_format)
+                attachment.id: _("Unsupported file type via %s", wizard.invoice_edi_format)
                 for attachment in attachments_not_supported
             }
