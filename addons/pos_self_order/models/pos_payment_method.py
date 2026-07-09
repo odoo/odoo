fleetapps/odoo3

[IMP] pos*: Allow payment at counter with other payments in self order
pos*: point_of_sale, pos_self_order

This commit allows the user to allow his customer to pay at the counter
even if they already have payment method set in the self order.

task-id: 5960666

addons/pos_self_order/models/pos_payment_method.py
@@ -16,6 +16,6 @@ class PosPaymentMethod(models.Model):
     @api.model
     def _load_pos_self_data_domain(self, data, config):
         if config.self_ordering_mode == 'kiosk':
-            return [('use_payment_terminal', '!=', False), ('id', 'in', config.payment_method_ids.ids)]
+            return [('id', 'in', config.payment_method_ids.ids), '|', ('is_cash_count', '=', True), ('use_payment_terminal', '!=', False)]
         else:
             return [('id', '=', False)]

odoo/odoo

Task-id: 5960666
X-original-commit: 6c0a46d9dfbd98b0ec15961dcce807806824fdba

addons/pos_self_order/models/pos_payment_method.py
@@ -15,5 +15,5 @@ class PosPaymentMethod(models.Model):
     @api.model
     def _load_pos_self_data_domain(self, data, config):
         if config.self_ordering_mode == 'kiosk':
-            return [('payment_provider', '!=', False), ('id', 'in', config.payment_method_ids.ids)]
+            return [('id', 'in', config.payment_method_ids.ids), '|', ('is_cash_count', '=', True), ('payment_provider', '!=', False)]
         return [('id', '=', False)]