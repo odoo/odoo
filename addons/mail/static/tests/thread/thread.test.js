1fe6f642818d085847d9573b9a60b52e14fe070a
Maryam Kia <maki@odoo.com>
2026-07-09 09:30:59 +0000

    [FIX] mail, test_mail_full: read `mountedAndLoaded` from `state`
    Since #198829, a `t-att-class` has been added to the `loadOlder` template that
    relies on `mountedAndLoaded` of thread state to control the visibility of the
    `Load More` button. But it doesn't read the value from the state properly. As a
    result, the button is always transparent. This change fixes this by reading the
    value from the state.
    
    X-original-commit: 014815aaf0de112c90ce2b1b2d34fe74f4680ed6

--- addons/mail/static/tests/thread/thread.test.js
+++ addons/mail/static/tests/thread/thread.test.js
@@ -19,7 +19,14 @@ import { mailDataHelpers } from "@mail/../tests/mock_server/mail_mock_server";
 import { Thread } from "@mail/core/common/thread";
 
 import { describe, expect, test } from "@odoo/hoot";
-import { advanceFrame, animationFrame, press, queryFirst, queryValue } from "@odoo/hoot-dom";
+import {
+    advanceFrame,
+    animationFrame,
+    press,
+    queryFirst,
+    queryOne,
+    queryValue,
+} from "@odoo/hoot-dom";
 import { mockDate, tick } from "@odoo/hoot-mock";
 import {
     Command,
@@ -144,7 +151,8 @@ test("load more messages from channel (auto-load on scroll)", async () => {
     });
     await start();
     await openDiscuss(channelId);
-    await contains("button:text('Load More')", { before: [".o-mail-Message", { count: 30 }] });
+    await contains("button:text(Load More)", { before: [".o-mail-Message", { count: 30 }] });
+    expect(getComputedStyle(queryOne("button:text(Load More)")).opacity).toBe("1");
     await contains(".o-mail-Thread", { scroll: "bottom" });
     await scroll(".o-mail-Thread", 0);
     await contains(".o-mail-Message", { count: 60 });
