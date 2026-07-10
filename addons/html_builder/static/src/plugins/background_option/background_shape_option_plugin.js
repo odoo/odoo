ebb62ba00507019250bf0504a875a3ad03b62cd9
Alay Patel <alap@odoo.com>
2026-07-10 17:02:28 +0530

    [FIX] html_builder, *: sync background shape color with color preset
    [*]=website
    
    Steps to reproduce:
    1. Go to the website and enter edit mode.
    2. Drop any snippet.
    3. Add a background shape.
    4. Set the background shape color to "o-color-1".
    5. Go to theme tab.
    6. Change the value of theme color 1 from theme preset.
    
    Issue:
    The background shape color is not updated when the theme color changes.
    
    Reason:
    The background shape color is embedded in the URL of the
    "background-image" style attribute. When the theme color value changes,
    this URL is not updated. Additionally, the URL uses color variables
    rather than resolved hexadecimal color values as parameters. As a
    result, even when an updation occurs, the URL itself remains unchanged,
    preventing the background shape color from being updated.
    
    task-5438314
    
    X-original-commit: cf0e4ef47838698dab67b5d9ebd35ffd7c5c823d
    Part-of: odoo/odoo#274079
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>

--- addons/html_builder/static/src/plugins/background_option/background_shape_option_plugin.js
+++ addons/html_builder/static/src/plugins/background_option/background_shape_option_plugin.js
@@ -69,6 +69,7 @@ export class BackgroundShapeOptionPlugin extends Plugin {
             }),
         on_snippet_dropped_handlers: ({ snippetEl }) => this.handleBgColorUpdated(snippetEl),
         on_cloned_handlers: ({ cloneEl }) => this.handleBgColorUpdated(cloneEl),
+        on_website_color_updated_handlers: this.syncShapeColorsWithTheme.bind(this),
     };
     static shared = [
         "getShapeStyleUrl",
@@ -205,6 +206,31 @@ export class BackgroundShapeOptionPlugin extends Plugin {
         return shapeInfo.subgroup === "connections";
     }
     /**
+     * Update the shape color (when a theme color is selected) whenever the
+     * theme preset color changes.
+     *
+     * @param {String} updatedColorVariable - Updated theme color variable
+     * like 'o-color-*'.
+     */
+    syncShapeColorsWithTheme(updatedColorVariable) {
+        if (!updatedColorVariable.startsWith("o-color-")) {
+            return;
+        }
+        const selector = `[data-oe-shape-data*='"${updatedColorVariable}"'] .o_we_shape[style*="background-image"]`;
+        this.refreshBgShapes([...this.document.querySelectorAll(selector)]);
+        this.config.snippetModel.updateContent("snippet_custom", (snippetContent) => {
+            this.refreshBgShapes([...snippetContent.querySelectorAll(selector)]);
+        });
+    }
+    refreshBgShapes(shapeEls) {
+        for (const shapeEl of shapeEls) {
+            shapeEl.style.setProperty(
+                "background-image",
+                `url("${this.getShapeSrc(this.getShapeData(shapeEl.parentElement))}")`
+            );
+        }
+    }
+    /**
      * Handles everything related to saving state before preview and restoring
      * it after a preview or locking in the changes when not in preview.
      *
@@ -246,19 +272,19 @@ export class BackgroundShapeOptionPlugin extends Plugin {
 
         shapeContainerEl.classList.toggle("o_we_animated", animated === "true");
 
-        // We need to check if the colors are the default ones because since we
-        // do not apply a shape by default anymore, "getDefaultColors" might
-        // return an empty object if its the first time a shape is added.
-        const defaultColors = getDefaultColors(editingElement);
-        const areCustomColors =
-            Boolean(colors) &&
-            !Object.entries(colors).every(
-                ([colorName, colorValue]) =>
-                    colorValue.toLowerCase() === defaultColors[colorName]?.toLowerCase()
-            );
+        let defaultColors;
+        const areDefaultColors = Object.entries(colors || {}).every(([colorName, colorValue]) => {
+            if (colorValue === `o-color-${colorName.slice(1)}`) {
+                return true;
+            }
+            defaultColors ??= getDefaultColors(editingElement);
+            return colorValue.toLowerCase() === defaultColors[colorName]?.toLowerCase();
+        });
 
         const shouldCustomize =
-            areCustomColors || flip.length > 0 || parseFloat(shapeAnimationSpeed) !== 0;
+            (Boolean(colors) && !areDefaultColors) ||
+            flip.length > 0 ||
+            parseFloat(shapeAnimationSpeed) !== 0;
 
         if (shouldCustomize) {
             // Apply custom image, flip, speed
@@ -320,7 +346,12 @@ export class BackgroundShapeOptionPlugin extends Plugin {
      */
     getImplicitColors(editingElement, shapeName, previousColors = {}) {
         const selectedBackgroundUrl = this.getShapeStyleUrl(shapeName);
-        const defaultColors = this.getShapeDefaultColors(selectedBackgroundUrl);
+        const defaultColors = Object.fromEntries(
+            Object.keys(this.getShapeDefaultColors(selectedBackgroundUrl)).map((key) => [
+                key,
+                `o-color-${key.slice(1)}`,
+            ])
+        );
         let colors = Object.assign(
             { ...previousColors },
             this.getComputedConnectionsColors(editingElement, shapeName)
@@ -376,6 +407,8 @@ export class BackgroundShapeOptionPlugin extends Plugin {
             return "";
         }
         const searchParams = Object.entries(colors).map(([colorName, colorValue]) => {
+            // To convert 'o-color-*' colorValue to respective hex code.
+            colorValue = normalizeColor(colorValue, getHtmlStyle(this.document));
             const encodedCol = encodeURIComponent(colorValue);
             return `${colorName}=${encodedCol}`;
         });
