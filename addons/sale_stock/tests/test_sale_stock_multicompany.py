bbc132ed80ad65beed992181b5a9df09aebdbf51
Robert Smith (rosm) <rosm@odoo.com>
2026-07-10 16:30:18 +0000

    [FIX] sale_stock: correct qty_delivered for interco routes
    Problem:
    When computing qty_delivered on a sale order line, intercompany 'delieveries' from the other company would count towards the delivery total.
    
    Solution:
    When retreiving the relevant moves for quantity calculation (_get_outgoing_incoming_moves), we will filter out any moves not belonging to the SOL's company. This will prevent moves from other companies into the interco location from being counted towards qty_delivered.
    
    Steps to Replicate (runbot v19):
    1. Create a route
        - Pull Comp B -> Interco, MTO, Comp B delivery
        - Pull Interco -> Comp A, MTS, Comp A receipt
        (You can review the test for more info about this route config)
    2. Attach a product to this new route
    3. Create a SO for the product and confirm it
    4. You should see 4 pickings, validate the chain
    5. The qty_delivered on the sale order is double the demand
    
    opw-6361559
    
    closes odoo/odoo#279793
    
    X-original-commit: ab8167f488437708c384b7d41f4303887b689960
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Robert Smith (rosm) <rosm@odoo.com>

--- addons/sale_stock/tests/test_sale_stock_multicompany.py
+++ addons/sale_stock/tests/test_sale_stock_multicompany.py
@@ -176,3 +176,105 @@ class TestSaleStockMultiCompany(TestSaleCommon, ValuationReconciliationTestCommo
             [(rec['product_name'], rec['lot_id']) for rec in invoice._get_invoiced_lot_values()],
             [(self.product_a.name, sn.id)]
         )
+
+    def test_intercompany_delivered_qty(self):
+        """
+        Test that when an inter-company route triggers moves across multiple companies,
+        the delivered quantity on the SO line isn't doubled by the outgoing moves of the other company.
+        Company A -> Sells to Customer
+        Company B -> Supplies Inter-company Transit -> Supplies Company A
+        """
+        company_a = self.env.company
+        company_b = self.company_data_2['company']
+
+        self.product_a.is_storable = True
+
+        interco_location = self.env.ref('stock.stock_location_inter_company')
+        customer_location = self.env.ref('stock.stock_location_customers')
+        vendor_location = self.env.ref('stock.stock_location_suppliers')
+
+        wh_a = self.warehouse_A
+        loc_a = wh_a.lot_stock_id
+
+        wh_b = self.warehouse_B
+        loc_b = wh_b.lot_stock_id
+
+        self.env['stock.quant']._update_available_quantity(self.product_a, loc_b, 10.0)
+
+        route = self.env['stock.route'].create({
+            'name': 'Pull from B to A via Interco',
+            'sale_selectable': True,
+            'company_id': False,
+            'rule_ids': [
+                Command.create({
+                    'name': 'Co B to Interco',
+                    'action': 'pull',
+                    'location_src_id': loc_b.id,
+                    'location_dest_id': interco_location.id,
+                    'picking_type_id': wh_b.out_type_id.id,
+                    'procure_method': 'make_to_order',
+                    'company_id': company_b.id,
+                    'route_id': False,
+                    'location_dest_from_rule': True,
+                }),
+                Command.create({
+                    'name': 'Interco to Co A',
+                    'action': 'pull',
+                    'location_src_id': interco_location.id,
+                    'location_dest_id': loc_a.id,
+                    'picking_type_id': wh_a.int_type_id.id,
+                    'procure_method': 'make_to_order',
+                    'company_id': company_a.id,
+                    'route_id': False,
+                }),
+            ]
+        })
+        route.rule_ids[0].route_id = route.id
+        route.rule_ids[1].route_id = route.id
+
+        self.product_a.route_ids = [Command.set([route.id])]
+
+        customer = self.partner_a
+
+        so = self.env['sale.order'].create({
+            'partner_id': customer.id,
+            'company_id': company_a.id,
+            'order_line': [Command.create({
+                'product_id': self.product_a.id,
+                'product_uom_qty': 5.0,
+            })]
+        })
+
+        so.action_confirm()
+
+        # At this point, we should have 4 moves generated:
+        # 1. Vendor -> Co B (Company B)
+        # 2. Co B -> Interco (Company B)
+        # 3. Interco -> Co A (Company A)
+        # 4. Co A -> Customer (Company A)
+        moves = so.order_line.move_ids | self.env['stock.move'].search([
+            ('product_id', '=', self.product_a.id),
+            ('origin', 'ilike', so.name)
+        ])
+
+        vendor_move = moves.filtered(lambda m: m.location_id == vendor_location and m.location_dest_id == loc_b)
+        vendor_move.quantity = 5.0
+        vendor_move.picked = True
+        vendor_move.picking_id.button_validate()
+
+        interco_1 = moves.filtered(lambda m: m.location_id == loc_b and m.location_dest_id == interco_location)
+        interco_1.quantity = 5.0
+        interco_1.picked = True
+        interco_1.picking_id.button_validate()
+
+        interco_2 = moves.filtered(lambda m: m.location_id == interco_location and m.location_dest_id == loc_a)
+        interco_2.quantity = 5.0
+        interco_2.picked = True
+        interco_2.picking_id.button_validate()
+
+        delivery = moves.filtered(lambda m: m.location_id == loc_a and m.location_dest_id == customer_location)
+        delivery.quantity = 5.0
+        delivery.picked = True
+        delivery.picking_id.button_validate()
+
+        self.assertEqual(so.order_line.qty_delivered, 5.0, "Intercompany transfers are being counted incorrectly")
