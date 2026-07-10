odoo/odoo

commit e30fb722c00805e7226d2ee9e3e587b3c2204840

[FIX] stock: correctly estimate available quantity in stock for mtso
When creating a procurment through mtso, if the product has a muti level bom with the same component at multiple levels, it will consider the free quantity multiple times.

Steps to reproduce:
-------------------
* Enable MTO and change supply method to: "Take From Stock, if unavailable, Trigger Another Rule"
* Create three products : final, semi, component
   - final: mtso, manufacture
   - semi: mtso, manufacture
   - component: mtso, buy, on hand quantity to 4
* Create a bom for final:
   - 10 components
   - 1 semi
* Create a bom for semi:
  - 10 components
* Create and confirm a MO for 1 "final"
-> Issue the purchase order is only for 12 components and not 16.

Observation:
-------------
When confirming our MO, it will create a manufacture procurement for the products.
The procurement will recursively create procurements and stock moves for each of its components.
https://github.com/odoo/odoo/blob/b0b8a102153eaa9231321524ec5140cc6d754502/addons/stock/models/stock_move.py#L1563-L1571
Since its a MTSO, it will first check the products that are available in stock (free_qty) and create the procurment for the missing quantity:
https://github.com/odoo/odoo/blob/b0b8a102153eaa9231321524ec5140cc6d754502/addons/stock/models/stock_move.py#L1646-L1647
https://github.com/odoo/odoo/blob/b0b8a102153eaa9231321524ec5140cc6d754502/addons/stock/models/stock_move.py#L1657-L1663
And each procurement, if it is of the manufacture type, will create corresponding procurements for their components.

Once all the procurements and stock moves have been created, the stock move will confirmed and assigned.

-> the issue arise because the free_qty will be updated when the stock moves are assigned,
which means that when a procurement is created it will always consider the same free_qty even if it has already been used by another procurement from another level.

opw-5514788

closes odoo/odoo#253958

addons/mrp/tests/test_procurement.py
@@ -353,6 +353,59 @@ class TestProcurement(TestMrpCommon):
         move_dest._action_assign()
         self.assertEqual(move_dest.quantity, 10.0)
 
+    def test_mtso_with_multi_lvl_bom(self):
+        """ Tests that a Manufacturing Order use the adequate quantity of components
+        using a MTSO resupply route with a multi-level BoM.
+        """
+        route_mto = self.warehouse_1.mto_pull_id.route_id
+        route_mto.active = True
+        route_mto.rule_ids.procure_method = "mts_else_mto"
+        products = self.product_4 | self.productB | self.productC
+        products.write({
+            'route_ids': [Command.link(route_mto.id)],
+        })
+        self.env['mrp.bom'].create([{
+            'product_id': self.productB.id,
+            'product_tmpl_id': self.productB.product_tmpl_id.id,
+            'product_qty': 1.0,
+            'bom_line_ids': [
+                Command.create({
+                    'product_id': self.product_4.id,
+                    'product_qty': 1,
+                }),
+            ],
+        },
+        {
+            'product_id': self.productC.id,
+            'product_tmpl_id': self.productC.product_tmpl_id.id,
+            'product_qty': 1.0,
+            'bom_line_ids': [
+                Command.create({
+                    'product_id': self.product_4.id,
+                    'product_qty': 5,
+                }),
+                Command.create({
+                    'product_id': self.productB.id,
+                    'product_qty': 5,
+                }),
+            ],
+        }])
+        self.env['stock.quant']._update_available_quantity(self.product_4, self.warehouse_1.lot_stock_id, 2)
+        mo = self.env['mrp.production'].create({
+            'product_id': self.productC.id,
+            'product_qty': 1,
+            'location_src_id': self.warehouse_1.lot_stock_id.id,
+        })
+        self.assertEqual(self.product_4.free_qty, 2)
+        mo.action_confirm()
+        child_mo_stick, child_mo_B = mo._get_children()
+        grand_child_mo_stick = child_mo_B._get_children()
+
+        self.assertEqual(self.product_4.free_qty, 0)
+        self.assertRecordValues(child_mo_B, [{'product_id': self.productB.id, 'product_uom_qty': 5}])
+        self.assertRecordValues(child_mo_stick, [{'product_id': self.product_4.id, 'product_uom_qty': 3}])
+        self.assertRecordValues(grand_child_mo_stick, [{'product_id': self.product_4.id, 'product_uom_qty': 5}])
+
     def test_mtso_with_empty_bom(self):
         """Test to ensure that a Manufacturing Order is created in 'draft' state
         via MTSO route when BoM has no components or operations.

addons/stock/models/stock_move.py
@@ -1548,6 +1548,7 @@ Please change the quantity done or the rounding precision in your settings.""",
         in another move of the same picking sharing its characteristics.
         """
         # Use OrderedSet of id (instead of recordset + |= ) for performance
+        consumed_from_stock_dict = self.env.context.get('consumed_from_stock_dict', defaultdict(float))
         move_create_proc, move_to_confirm, move_waiting = OrderedSet(), OrderedSet(), OrderedSet()
         to_assign = defaultdict(OrderedSet)
         for move in self:
@@ -1573,7 +1574,7 @@ Please change the quantity done or the rounding precision in your settings.""",
         # create procurements for make to order moves
         procurement_requests = []
         move_create_proc = self.browse(move_create_proc)
-        quantities = move_create_proc._prepare_procurement_qty()
+        quantities = move_create_proc.with_context(consumed_from_stock_dict=consumed_from_stock_dict)._prepare_procurement_qty()
         for move, quantity in zip(move_create_proc, quantities):
             values = move._prepare_procurement_values()
             origin = move._prepare_procurement_origin()
@@ -1581,7 +1582,7 @@ Please change the quantity done or the rounding precision in your settings.""",
                 move.product_id, quantity, move.product_uom,
                 move.location_id, move.rule_id and move.rule_id.name or "/",
                 origin, move.company_id, values))
-        self.env['stock.rule'].run(procurement_requests, raise_user_error=not self.env.context.get('from_orderpoint'))
+        self.env['stock.rule'].with_context(consumed_from_stock_dict=consumed_from_stock_dict).run(procurement_requests, raise_user_error=not self.env.context.get('from_orderpoint'))
 
         move_to_confirm, move_waiting = self.browse(move_to_confirm).filtered(lambda m: m.state != 'cancel'), self.browse(move_waiting).filtered(lambda m: m.state != 'cancel')
         move_to_confirm.write({'state': 'confirmed'})
@@ -1643,6 +1644,7 @@ Please change the quantity done or the rounding precision in your settings.""",
         return (self.reference_ids and self.reference_ids[0].name) or self.origin or self.picking_id.display_name
 
     def _prepare_procurement_qty(self):
+        consumed_from_stock_dict = self.env.context.get('consumed_from_stock_dict', defaultdict(float))
         quantities = []
         mtso_products_by_locations = defaultdict(list)
         mtso_moves = set()
@@ -1667,11 +1669,11 @@ Please change the quantity done or the rounding precision in your settings.""",
                 quantities.append(move.product_uom_qty)
                 continue
 
-            free_qty = max(forecasted_qties_by_loc[move.location_id][move.product_id.id], 0)
+            free_qty = max(forecasted_qties_by_loc[move.location_id][move.product_id.id] - consumed_from_stock_dict[move.location_id, move.product_id.id], 0)
             quantity = max(move.product_qty - free_qty, 0)
             product_uom_qty = move.product_id.uom_id._compute_quantity(quantity, move.product_uom, rounding_method='HALF-UP')
             quantities.append(product_uom_qty)
-            forecasted_qties_by_loc[move.location_id][move.product_id.id] -= min(move.product_qty, free_qty)
+            consumed_from_stock_dict[move.location_id, move.product_id.id] += min(move.product_qty, free_qty)
 
         return quantities
 

commit dd6ee071f949752d31497d9f975ba7fc41ebcd98

[FIX] mrp: correctly estimate available quantity in stock for mtso
Steps to reproduce:

- In the settings enable Multi-Steps route
- Unarchive the MTO route and set its production rule in MTSO
- Create 3 products: P1, P2 and COMP all using the MTO route
- Create 2 BOM's, one for P1 and one for P2: 1 X COMP
- Put 2 units of COMP in stock and add an empty bom (to trigger a
  child MO creation in case the MTSO route is triggered)
- Create and confirm a sale order for: 1 x P1 and 1 X P2
> An MO was generated for both product but P2 also generated a child
  MO for 1 unit of COMP instead of using the available unit

Cause of the issue:

The issue happens in the `_prepare_procurement_qty` which incorrectly
assess that 1 unit of COMP will be required. The issue has been
introduced by commit https://github.com/odoo/odoo/commit/e30fb722c00805e7226d2ee9e3e587b3c2204840
which introduced a dictionary to keep track of units of  products that
will be used by the confirmation process of other concurrent mtso moves:
https://github.com/odoo/odoo/blob/71f0715bd5e29e976a1e8bfa7c4fa6e04735ebd7/addons/stock/models/stock_move.py#L1683-L1689
https://github.com/odoo/odoo/blob/71f0715bd5e29e976a1e8bfa7c4fa6e04735ebd7/addons/stock/models/stock_move.py#L1712-L1715
https://github.com/odoo/odoo/blob/71f0715bd5e29e976a1e8bfa7c4fa6e04735ebd7/addons/stock/models/stock_move.py#L1810-L1814
While by design this propagates the information used by other mtso moves
in a common `_action_confirm` stack, the issue that we encounter is that
this quantity is only relevant to be substracted to the free_qty when
the unit is not yet reserved and hence already accounted negatively in
`free_qty`. However, in the present case, confirming the receipt of
P1 and P2 will confirm both moves simultaneously, triggering a common
`_run_manufacture` to generate both an MO for P1 and for P2. At this
point the dictionary `consumed_from_stock_dict` is shared in both MO's

addons/mrp/models/stock_rule.py
@@ -120,9 +120,7 @@ class StockRule(models.Model):
             productions_vals_list = new_productions_values_by_company[company_id]['values']
             # create the MO as SUPERUSER because the current user may not have the rights to do it (mto product launched by a sale for example)
             productions = self.env['mrp.production'].with_user(SUPERUSER_ID).sudo().with_company(company_id).create(productions_vals_list)
-            for mo in productions:
-                if self._should_auto_confirm_procurement_mo(mo):
-                    mo.action_confirm()
+            productions.filtered(self._should_auto_confirm_procurement_mo).action_confirm()
             productions._post_run_manufacture(new_productions_values_by_company[company_id]['procurements'])
         return True
 

addons/mrp/tests/test_procurement.py
@@ -431,6 +431,92 @@ class TestProcurement(TestMrpCommon):
         self.assertRecordValues(child_mo_stick, [{'product_id': self.product_4.id, 'product_uom_qty': 3}])
         self.assertRecordValues(grand_child_mo_stick, [{'product_id': self.product_4.id, 'product_uom_qty': 5}])
 
+    def test_concurrent_mtso_manufacture(self):
+        """
+        Tests that the appropriate amount of MO's are generated to satisfy MTSO manufactured
+        products using a common component.
+
+        Have 4 MTSO manufacture product productB, productC, productD, COMP:
+        1 x productB:
+            - 1 x ProductC:
+                - 1 x COMP
+            - 1 x ProductD:
+                - 1 x COMP
+        Put 3 units of COMP in stock and manufacture 2 X ProductB requiring 4 x COMP.
+        Check that a single MO is generated to fulfil 1 unit of COMP.
+        """
+        route_mto = self.warehouse_1.mto_pull_id.route_id
+        route_mto.active = True
+        route_mto.rule_ids.procure_method = "mts_else_mto"
+        mtso_products = self.productB | self.productC | self.productD | self.product_4
+        mtso_products.write({
+            'route_ids': [Command.link(route_mto.id)],
+        })
+        component = self.product_4
+        self.env['mrp.bom'].create([
+            {
+                'product_id': self.productB.id,
+                'product_tmpl_id': self.productB.product_tmpl_id.id,
+                'product_qty': 1.0,
+                'bom_line_ids': [
+                    Command.create({
+                        'product_id': self.productC.id,
+                        'product_qty': 1,
+                    }),
+                    Command.create({
+                        'product_id': self.productD.id,
+                        'product_qty': 1,
+                    }),
+                ],
+            },
+            {
+                'product_id': self.productC.id,
+                'product_tmpl_id': self.productC.product_tmpl_id.id,
+                'product_qty': 1.0,
+                'bom_line_ids': [
+                    Command.create({
+                        'product_id': component.id,
+                        'product_qty': 1,
+                    }),
+                ],
+            },
+            {
+                'product_id': self.productD.id,
+                'product_tmpl_id': self.productD.product_tmpl_id.id,
+                'product_qty': 1.0,
+                'bom_line_ids': [
+                    Command.create({
+                        'product_id': component.id,
+                        'product_qty': 1,
+                    }),
+                ],
+            },
+            {
+                'product_id': component.id,
+                'product_tmpl_id': component.product_tmpl_id.id,
+                'product_qty': 1.0,
+            },
+        ])
+        self.env['stock.quant']._update_available_quantity(component, self.warehouse_1.lot_stock_id, 3)
+        mo = self.env['mrp.production'].create({
+            'product_id': self.productB.id,
+            'product_qty': 2,
+            'location_src_id': self.warehouse_1.lot_stock_id.id,
+        })
+        self.assertEqual(component.free_qty, 3)
+        mo.action_confirm()
+
+        self.assertEqual(component.free_qty, 0)
+        child_mo_C, child_mo_D = mo._get_children()
+        self.assertRecordValues(child_mo_C | child_mo_D, [
+            {'product_id': self.productC.id, 'product_uom_qty': 2.0},
+            {'product_id': self.productD.id, 'product_uom_qty': 2.0},
+        ])
+        self.assertFalse(child_mo_C._get_children())
+        self.assertRecordValues(child_mo_D._get_children(), [
+            {'product_id': component.id, 'product_uom_qty': 1.0},
+        ])
+
     def test_mtso_with_empty_bom(self):
         """Test to ensure that a Manufacturing Order is created in 'draft' state
         via MTSO route when BoM has no components or operations.