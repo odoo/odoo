5e691075965f423a531a1642aedf9a6d5a97f469
Soukéina Bojabza <sobo@odoo.com>
2026-07-10 14:55:22 +0000

    [FIX] html_builder, website: prevent screen from jumping on scroll
    Steps to reproduce the issue easily:
    - Remove the section in the footer.
    - Drop enough snippets to have a scroll bar and to not see the footer
      when the scroll is at the top.
    - Drop the "Pricelist" snippet at the bottom of the page and click on a
      column.
    - Scroll up so the footer and the bottom of the snippet are not visible,
      and add a pricelist item with the "Add Product" option.
    - => The page scrolls to the new item, but a big white space appears at
      the bottom of the screen, as if we scrolled too far.
    
    The same issue happens with similar steps in the following cases:
    - When using any option using the `addItem` action.
    - When undoing/redoing a step that was done in an element not in the
      viewport (the screen will scroll to it and we will have the issue).
    - When showing an invisible element (the screen will scroll to it if not
      in the viewport)
    - Adding a grid item with the "Add Elements" option.
    - Adding a new card in the "Floating Cards" snippet.
    
    The common point to all these cases is that they all scroll to the added
    or shown element with the `scrollIntoView` built-in function, which
    scrolls everything, including the viewport. It also doesn't take into
    account the header that changes during the scroll, often ending with the
    element hidden by the header.
    
    This commit fixes these issues by using the builder `scrollTo` util,
    which takes the header into account and only scrolls what needs to be.
    This function should always be preferred when scrolling in the builder.
    
    task-6314322
    
    closes odoo/odoo#278897
    
    X-original-commit: 7d03dee4c595c0929370aa9779513021fa13b5ab
    Signed-off-by: Romaric Moyeuvre (romo) <romo@odoo.com>
    Signed-off-by: Soukéina Bojabza (sobo) <sobo@odoo.com>

--- addons/html_builder/static/src/core/builder_options_plugin.js
+++ addons/html_builder/static/src/core/builder_options_plugin.js
@@ -5,6 +5,7 @@ import { isRemovable } from "./remove_plugin";
 import { getElementsWithOption, isElementInViewport } from "@html_builder/utils/utils";
 import { OptionsContainer } from "@html_builder/sidebar/option_container";
 import { shouldEditableMediaBeEditable } from "@html_builder/utils/utils_css";
+import { scrollTo } from "@html_builder/utils/scrolling";
 import { _t } from "@web/core/l10n/translation";
 import { closestElement } from "@html_editor/utils/dom_traversal";
 
@@ -562,8 +563,7 @@ export class BuilderOptionsPlugin extends Plugin {
                 this.updateContainers(targetEl, { forceUpdate: true });
                 // Scroll to the target if not visible.
                 if (!isElementInViewport(targetEl)) {
-                    // Firefox mis-scrolls with block "center" on tall snippets; keep "start".
-                    targetEl.scrollIntoView({ behavior: "smooth", block: "start" });
+                    scrollTo(targetEl);
                 }
             } else {
                 this.deactivateContainers();
