59d3a9ad385695949eb02e512777125f59c48e7d
adak(ahmed) <adak@odoo.com>
2026-07-13 09:48:53 +0200

    [IMP] l10n_be_hr_payroll : allow employee to have multiple cars

--- addons/hr_fleet/models/hr_employee.py
+++ addons/hr_fleet/models/hr_employee.py
@@ -11,7 +11,7 @@ class HrEmployee(models.Model):
     employee_cars_count = fields.Integer(compute="_compute_employee_cars_count", string="Cars", groups="fleet.fleet_group_user")
     car_ids = fields.One2many(
         'fleet.vehicle', 'driver_employee_id', string='Vehicles (private)',
-        groups="fleet.fleet_group_manager,hr.group_hr_user",
+        groups="fleet.fleet_group_manager,hr.group_hr_user,hr_payroll.group_hr_payroll_user",
     )
     license_plate = fields.Char(compute="_compute_license_plate", search="_search_license_plate", groups="hr.group_hr_user")
 
