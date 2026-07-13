9611de9c68c0e2c72d04215db64b482c6b79e9e3
Ravij Parikh <snrav@odoo.com>
2026-07-13 14:37:17 +0530

    [FIX] payment_worldline: correct Cofidis payment mapping
    Update the Cofidis payment method code according to
    the latest Worldline documentation.
    
    opw-6368979

--- addons/payment_worldline/const.py
+++ addons/payment_worldline/const.py
@@ -19,7 +19,7 @@ PAYMENT_METHODS_MAPPING = {
     'bancontact': 3012,
     'bizum': 5001,
     'cartes_bancaires': 130,
-    'cofidis': 3012,
+    'cofidis': 5129,
     'diners': 132,
     'discover': 128,
     'eps': 5406,
@@ -44,6 +44,7 @@ PAYMENT_METHODS_MAPPING = {
 REDIRECT_PAYMENT_METHODS = {
     'alipay_plus',
     'bizum',
+    'cofidis',
     'eps',
     'floa_bank',
     'ideal',
