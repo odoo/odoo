odoo/odoo

commit f54d6918d8ef5aa143e63b7dc79e8c3e3d8583f8

[FIX] account: create batch payment sequence after init
closes odoo/odoo#273905

Signed-off-by: Julien Alardot (jual) <jual@odoo.com>

addons/account/models/company.py
@@ -402,6 +402,25 @@ class ResCompany(models.Model):
         for company in self:
             onboardings.with_company(company)._search_or_create_progress()
 
+    def _get_batch_payment_sequence_values(self):
+        self.ensure_one()
+        return {
+            'name': _("Batch Payment Number Sequence"),
+            'implementation': 'no_gap',
+            'padding': 5,
+            'use_date_range': True,
+            'company_id': self.id,
+            'prefix': 'BATCH/%(range_year)s/',
+        }
+
+    def _create_batch_payment_sequence(self):
+        for company in self:
+            if not company.batch_payment_sequence_id:
+                sequence = self.env['ir.sequence'].sudo().create(
+                    self._get_batch_payment_sequence_values()
+                )
+                company.batch_payment_sequence_id = sequence
+
     @api.model_create_multi
     def create(self, vals_list):
         companies = super().create(vals_list)
@@ -414,16 +433,7 @@ class ResCompany(models.Model):
                         install_demo=False,
                     )
                 self.env.cr.precommit.add(try_loading)
-            if not company.batch_payment_sequence_id:
-                sequence = self.env['ir.sequence'].sudo().create({
-                    'name': _("Batch Payment Number Sequence"),
-                    'implementation': 'no_gap',
-                    'padding': 5,
-                    'use_date_range': True,
-                    'company_id': company.id,
-                    'prefix': 'BATCH/%(range_year)s/',
-                })
-                company.batch_payment_sequence_id = sequence
+            company._create_batch_payment_sequence()
         return companies
 
     def get_new_account_code(self, current_code, old_prefix, new_prefix):

addons/account/__init__.py
@@ -10,8 +10,18 @@ def _set_fiscal_country(env):
     env['res.company'].search([]).compute_account_tax_fiscal_country()
 
 
+def _create_batch_payment_sequence(env):
+    """ Creates a Batch Payment Number Sequence for every company that does not
+    have one yet. This covers companies that existed before the ``account``
+    module was installed.
+    """
+    to_create_seqs = env['res.company'].search([('batch_payment_sequence_id', '=', False)])
+    to_create_seqs._create_batch_payment_sequence()
+
+
 def _account_post_init(env):
     _set_fiscal_country(env)
+    _create_batch_payment_sequence(env)
 
 # imported here to avoid dependency cycle issues
 # pylint: disable=wrong-import-position

commit 64a7f30762d9df99b596dbd38fa1fe2e84baa1f4

[FIX] account: company instead of self in loop
This commit https://github.com/odoo/odoo/commit/f54d6918d8ef5aa143e63b7dc79e8c3e3d8583f8 introduced a bug, calling _get_batch_payment_sequence_values on self instead of company triggering possible ensure_one exception if self is not one company.

addons/account/models/company.py
@@ -417,7 +417,7 @@ class ResCompany(models.Model):
         for company in self:
             if not company.batch_payment_sequence_id:
                 sequence = self.env['ir.sequence'].sudo().create(
-                    self._get_batch_payment_sequence_values()
+                    company._get_batch_payment_sequence_values()
                 )
                 company.batch_payment_sequence_id = sequence
 