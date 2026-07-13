f061087afb9f1e8210f1079560072921dab09a15
pajo <pajo@odoo.com>
2026-07-13 13:33:24 +0000

    [FIX] hr_expense: allow splitting approved expenses with attachments
    Steps to reproduce:
    ----------------------------------------
    1. Install the Expense module.
    2. Create an expense with a receipt/attachment and approve it.
    3. Try to split the approved expense using the "Split Expense" button.
    
    Observation:
    ----------------------------------------
    An Access Error is raised: "You can't add attachments to an expense once it has
    been approved."
    
    Issue:
    ----------------------------------------
    When splitting an approved expense, Odoo duplicates the original expense into
    multiple split parts. Since the original expense is already in the `approved`
    state, the newly created duplicate records also have `state == 'approved'`.
    During the split process, Odoo copies the attachments from the original expense
    to the newly created split records:
    https://github.com/odoo/odoo/blob/e696bc516c97bc7157d52c5dba0b42e7cb8bab48/addons/hr_expense/wizard/hr_expense_split_wizard.py#L59-L61
    
    Because `copied_expense.state` is 'approved', the create method of
    `ir.attachment` triggers an AccessError via the security validation
    https://github.com/odoo/odoo/blob/e696bc516c97bc7157d52c5dba0b42e7cb8bab48/addons/hr_expense/models/ir_attachment.py#L23-L27
    
    Solution:
    ----------------------------------------
    Bypass the attachment restriction if the creation is initiated from the split
    wizard. It only bypasses the validation during the split operation,
    preserving the security constraints for normal user uploads to approved expenses
    
    opw-6373579
    
    X-original-commit: e9353235cd446bf17edfa5d4b6a63e3b353799ab

--- addons/hr_expense/models/ir_attachment.py
+++ addons/hr_expense/models/ir_attachment.py
@@ -2,6 +2,13 @@ from collections.abc import Collection
 
 from odoo import api, models
 from odoo.exceptions import AccessError
+<<<<<<< ad45fb0c110890d0df633012483e428a402bb6da
+||||||| 01e3f58492fa045c82279acefc9e704ce090117c
+from odoo.tools.misc import clean_context
+=======
+from odoo.tools.misc import clean_context
+from odoo.addons.hr_expense.wizard.hr_expense_split_wizard import _COPY_ATTACHMENT_FROM_SPLIT_WIZARD
+>>>>>>> 713650737160372bdd5c4f9d41bfb659596c26bb
 
 
 class IrAttachment(models.Model):
@@ -17,6 +24,7 @@ class IrAttachment(models.Model):
         ):
             raise AccessError(self.env._("You can't delete attachments from an expense once it has been submitted."))
 
+<<<<<<< ad45fb0c110890d0df633012483e428a402bb6da
     def _inaccessible_comodel_records(self, model_and_ids: dict[str, Collection[int]], operation: str):
         expense_ids = list(filter(None, model_and_ids.get('hr.expense', [])))
         expenses = self.env['hr.expense'].browse(expense_ids)
@@ -30,6 +38,27 @@ class IrAttachment(models.Model):
             ).ids
             for expense_id in blocked_expense_ids:
                 yield 'hr.expense', expense_id
+||||||| 01e3f58492fa045c82279acefc9e704ce090117c
+    @api.model_create_multi
+    def create(self, vals_list):
+        expense_attachments = [vals for vals in vals_list if vals.get('res_model') == 'hr.expense' and vals.get('res_id')]
+        expenses = self.env['hr.expense'].browse([vals['res_id'] for vals in expense_attachments])
+        if not all(
+            (expense.state in {'draft', 'submitted'} and (expense.has_access('write') or expense.employee_id.user_id == self.env.user)) or self.env.su
+            for expense in expenses
+        ):
+            raise AccessError(self.env._("You can't add attachments to an expense once it has been approved."))
+=======
+    @api.model_create_multi
+    def create(self, vals_list):
+        expense_attachments = [vals for vals in vals_list if vals.get('res_model') == 'hr.expense' and vals.get('res_id')]
+        expenses = self.env['hr.expense'].browse([vals['res_id'] for vals in expense_attachments])
+        if not (self.env.context.get('from_split_wizard') is _COPY_ATTACHMENT_FROM_SPLIT_WIZARD or all(
+            (expense.state in {'draft', 'submitted'} and (expense.has_access('write') or expense.employee_id.user_id == self.env.user)) or self.env.su
+            for expense in expenses
+        )):
+            raise AccessError(self.env._("You can't add attachments to an expense once it has been approved."))
+>>>>>>> 713650737160372bdd5c4f9d41bfb659596c26bb
 
         model_and_ids = {
             **model_and_ids,
