odoo/odoo

commit e80750d20b7ddbd015451a885016151347f3975b

[FIX] hr_holidays: fix invalid time display for hourly time off
Issue:
When users selected time off based on hours, "InvalidDateTime" was shown
instead of the correct "From" and "To" time.
This happened after a recent
refactor:https://github.com/odoo/odoo/pull/240555/changes#diff-0beecbb730e1c7762f2a8d5f7038c02e123da10a85dec2e5d16a02748373f2fe
 of getFormatValue, which now returns
DateTime in a different format than before.

Fix:
Updated  the method and adapt the new refactored format.

Impact:
The correct DateTime values are now displayed instead of "InvalidDateTime".

Task-5940185

closes odoo/odoo#248966

Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

addons/hr_holidays/static/src/components/float_time_selection/float_time_selection.js
@@ -44,9 +44,21 @@ export class FloatTimeSelectionField extends FloatTimeField {
 
     get formattedValue() {
         const unitAmount = super.formattedValue;
-        return DateTime
-            .fromFormat(unitAmount, 'hh:mm', { numberingSystem: 'latn', zone: 'default'})
-            .toLocaleString({ hour: 'numeric', minute: 'numeric'});
+        let hours = 0;
+        let minutes = 0;
+
+        unitAmount.split(" ").forEach((data) => {
+            if (data.endsWith("h")) {
+                hours = parseInt(data);
+            } else if (data.endsWith("m")) {
+                minutes = parseInt(data);
+            }
+        });
+
+        return DateTime.fromObject(
+            { hour: hours, minute: minutes },
+            { numberingSystem: "latn", zone: "default" }
+        ).toFormat("h:mm a");
     }
 
     onCharHoursClick(ev) {

commit 8849c428df9f4f128d09bdb5ce69ea5523439e1d

[FIX] hr_holidays, web: handle duration formatting in all languages
## Issue
When using a language that does not use `h` for "hours" and/or `m` for
"minutes", times are not properly displayed in the Time Off app. For example,
changing the `request_hour_to` of a time off request in Dutch will always
show 12:00 AM, regardless of the time we set.

## Steps to reproduce
1. Install *Time Off* (`hr_holidays`)
2. Change the user language to Dutch
3. In Time Off, create a new time off request
    - Time Off Type: Any type with a "Custom Hours" duration type
4. **The times displayed are "12:00 a.m" for both the start and the end
of the time off. We can change the times but they will always display
12:00 a.m, even if the proper values are used in the backend.**

## Cause
The displayed time is formatted in the FloatTimeSelectionField component:

https://github.com/odoo/odoo/blob/dcaba2dd99d0553bb3df7b9b5f0b77fec2d53159/addons/hr_holidays/static/src/components/float_time_selection/float_time_selection.js#L45-L62

The getter attempts to parse the provided `unitAmount` (e.g. "13h 14m")
by looking for `h` and `m` to determine the hours and minutes. This works
fine in multiple languages such as French or English, but it does not work
for languages that use other letters for hours and/or minutes (such as Dutch,
which uses "u" for hours).

This issue was introduced by https://github.com/odoo/odoo/commit/e80750d20b7ddbd015451a885016151347f3975b,
which was correcting an issue where *"Invalid DateTime"* being displayed
instead of actual time values.

## Fix
The solution is to use the correct hour and minute identifiers depending
on the user language. This is done by the `durationUnitsRegex` function:

https://github.com/odoo/odoo/blob/dcaba2dd99d0553bb3df7b9b5f0b77fec2d53159/addons/web/static/src/core/l10n/time.js#L287-L307

addons/hr_holidays/static/src/components/float_time_selection/float_time_selection.js
@@ -4,6 +4,7 @@ import { usePopover } from "@web/core/popover/popover_hook";
 import { FloatTimeSelectionPopover } from "./float_time_selection_popover";
 
 import { FloatTimeField, floatTimeField } from "@web/views/fields/float_time/float_time_field";
+import { parseDuration } from "@web/views/fields/parsers";
 const { DateTime } = luxon;
 
 function floatToHoursMinutes(floatValue) {
@@ -44,19 +45,8 @@ export class FloatTimeSelectionField extends FloatTimeField {
 
     get formattedValue() {
         const unitAmount = super.formattedValue;
-        let hours = 0;
-        let minutes = 0;
-
-        unitAmount.split(" ").forEach((data) => {
-            if (data.endsWith("h")) {
-                hours = parseInt(data);
-            } else if (data.endsWith("m")) {
-                minutes = parseInt(data);
-            }
-        });
-
-        return DateTime.fromObject(
-            { hour: hours, minute: minutes },
+        const duration = parseDuration(unitAmount);
+        return DateTime.fromObject(duration,
             { numberingSystem: "latn", zone: "default" }
         ).toFormat("h:mm a");
     }

addons/web/static/src/views/fields/parsers.js
@@ -141,7 +141,7 @@ export function parseFloatTime(value, unit = "hours") {
  * @param {UnitOfTime} [unit="hours"]
  * @return {Duration}
  */
-function parseDuration(value, unit = "hours") {
+ export function parseDuration(value, unit = "hours") {
     let isNegative;
     const regexTimes = durationUnitsRegex();
     const duration = {

addons/hr_holidays/static/tests/time_off_dashboard.test.js
@@ -1,7 +1,10 @@
 import { describe, expect, test } from "@odoo/hoot";
 import { mockDate } from "@odoo/hoot-mock";
-import { defineModels, fields, getService, models, mountWebClient, onRpc } from "@web/../tests/web_test_helpers";
+import { user } from "@web/core/user";
+import { contains, defineModels, defineParams, fields, getService, models, mountView, mountWebClient, onRpc } from "@web/../tests/web_test_helpers";
+import { clickDate } from "@web/../tests/views/calendar/calendar_test_helpers";
 import { defineHrHolidaysModels } from "./hr_holidays_test_helpers";
+
 
 describe.current.tags("desktop");
 
@@ -29,6 +32,7 @@ class HrLeave extends models.Model {
 
     color = fields.Integer({ related: "work_entry_type_id.color" });
     date_from = fields.Datetime();
+    hour_from = fields.Float();
     date_to = fields.Datetime();
     department_id = fields.Many2one({ relation: "hr.department" });
     employee_id =  fields.Many2one({ relation: "hr.employee" });
@@ -106,3 +110,26 @@ test(`test basic rendering`, async () => {
     expect(`.o_calendar_filter:contains("Mar 17, 2025 : Test Mandatory Day")`).toHaveCount(1);
     expect(`.fc-day.hr_mandatory_day_5[data-date="2025-03-17"]`).toHaveCount(1);
 });
+
+test(`test time rendering in foreign languages`, async () => {
+    mockDate("2025-03-18 08:00:00");
+    defineParams({
+        lang: "nl",
+    });
+    HrLeave._views["form,hr_leave_view_form_dashboard_new_time_off"] = `
+        <form>
+            <field name="hour_from" widget="float_time_selection"/>
+        </form>
+    `;
+    await mountView({
+        type: "calendar",
+        resModel: "hr.leave",
+        context: user.context,
+    });
+    await clickDate("2025-03-18");
+    await contains(".o_field_widget[name='hour_from'] input").click();
+    await contains(".o_popover select:nth-of-type(1)").select("14");
+    await contains(".o_popover select:nth-of-type(2)").select("30");
+    await contains(".modal-content").click();  // Confirm selection
+    await expect("div[name='hour_from'] input").toHaveValue("2:30 p.m.");
+})