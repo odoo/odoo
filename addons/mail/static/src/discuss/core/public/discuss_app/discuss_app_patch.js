a05ea2c85f4a72860ada94fd8cd8152cfa25373a
Didier (did) <did@odoo.com>
2026-07-13 09:27:36 +0200

    [REF] mail: remove useEffectLayout from discuss_app_patch
    task-6373278

--- addons/mail/static/src/discuss/core/public/discuss_app/discuss_app_patch.js
+++ addons/mail/static/src/discuss/core/public/discuss_app/discuss_app_patch.js
@@ -1,19 +1,19 @@
-import { useLayoutEffect } from "@web/owl2/utils";
 import { Discuss } from "@mail/core/public_web/discuss_app/discuss_app";
 import { useService } from "@web/core/utils/hooks";
 import { patch } from "@web/core/utils/patch";
+import { useOnChange } from "@mail/utils/common/hooks";
 
 patch(Discuss.prototype, {
     setup() {
         super.setup();
         this.title = useService("title");
-        useLayoutEffect(
+        useOnChange(
+            () => [this.thread?.channel?.displayName || this.thread?.displayName],
             (threadName) => {
                 if (threadName) {
                     this.title.setParts({ action: threadName });
                 }
-            },
-            () => [this.thread?.channel?.displayName || this.thread?.displayName]
+            }
         );
     },
 });
