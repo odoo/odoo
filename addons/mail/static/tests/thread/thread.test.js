66ef8a359441dd44d079d99d636c13de804115cb
Alex Kühn <aku@odoo.com>
2026-07-13 10:23:50 +0200

    [FIX] mail: fix test from discuss unify menu
    The test "Update unread counter when receiving new message" was
    failing non-deterministically because there was 2 counters in the
    discuss sidebar: the counter of "Chats" category and the counter of
    the sidebar item representing the chat with unread messages.
    
    This test is failing since recent PR unifying the discuss menus [1].
    Before this change, the sidebar of discuss was showing the badge only
    on the item. The unify menu change now adds the badge on the category
    too.
    
    [1]: https://github.com/odoo/odoo/pull/269104

--- addons/mail/static/tests/thread/thread.test.js
+++ addons/mail/static/tests/thread/thread.test.js
@@ -879,7 +879,7 @@ test("Update unread counter when receiving new message", async () => {
     });
     await start();
     await openDiscuss(undefined);
-    await contains(".o-mail-DiscussApp-sidebar .o-discuss-badge:text('1')");
+    await contains(".o-mail-NotificationItem .o-discuss-badge:text('1')");
 
     await withUser(userId, () =>
         rpc("/mail/message/post", {
@@ -892,7 +892,7 @@ test("Update unread counter when receiving new message", async () => {
             thread_model: "discuss.channel",
         })
     );
-    await contains(".o-mail-DiscussApp-sidebar .o-discuss-badge:text('2')");
+    await contains(".o-mail-NotificationItem .o-discuss-badge:text('2')");
 });
 
 test("Show start message of conversation", async () => {
