1142668b89d89c8e82996689b9790a4f5dcca7c8
Michael Judd - Augur Ltd <michael.judd@auguraccountants.com>
2026-07-13 22:01:10 +0100

    [ADD] Please see https://github.com/odoo/enterprise/pull/124158 on enterprise branch

---
+++ doc/cla/corporate/augur-ltd.py
@@ -0,0 +1,7 @@
+United Kingdom, 13/07/2026
+Augur Ltd agrees to the terms of the Odoo Corporate Contributor License Agreement v1.0.
+
+I declare that I am authorized and able to make this agreement and sign this declaration.
+
+Signed,
+Michael Judd michael.judd@auguraccountants.com https://github.com/auguruk
