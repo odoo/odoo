f687c2865fba2ce87a31ff58d6693d6d82f2e4b4
lase <lase@odoo.com>
2026-07-14 14:50:13 +0000

    [FIX] stock, mrp_account: make kit qties computations company dependant
    Steps to reproduce:
    
    - Create 2 companies: c1, c2
    - Create 2 storable products: P, Comp
    - In c1 update the available qty of P to 10 units and its cost to 50
    - In c2 create a kit bom for P: 1 x Comp
    - With c1 > Accounting > Review > inventory > Inventory Valuation
    > The total value of Super product is 0 instead of 500
    
    Cause of the issue:
    
    The total value of the product will be set to 0 since the `qty_available`
    of the product is incorrectly computed to be 0:
    https://github.com/odoo/odoo/blob/cb7b3de6cea07464bcadd1325f52533d34ce09bc/addons/stock_account/models/product.py#L240-L243
    This hapens because the `_find_bom` used in the override of the
    `_compute_quantities_dict` in mrp does not consider take the contextual
    company into account:
    https://github.com/odoo/odoo/blob/cb7b3de6cea07464bcadd1325f52533d34ce09bc/addons/mrp/models/product.py#L271-L289
    and hence considers incorrectly that the product is a kit.
    
    Note:
    
    We make the same company dependency as in the `is_kits` computation:
    https://github.com/odoo/odoo/blob/cb7b3de6cea07464bcadd1325f52533d34ce09bc/addons/mrp/models/product.py#L41-L47
    
    opw-6361690
    
    X-original-commit: 8d704e0d1eb63856d1b0ec6517cb8919288158e0

--- addons/mrp/models/product.py
+++ addons/mrp/models/product.py
@@ -279,7 +279,7 @@ class ProductProduct(models.Model):
         This override is used to get the correct quantities of products
         with 'phantom' as BoM type.
         """
-        bom_kits = self.env['mrp.bom']._bom_find(self, bom_type='phantom')
+        bom_kits = self.env['mrp.bom']._bom_find(self, bom_type='phantom', company_id=self.env.company.id)
         kits = self.filtered(lambda p: bom_kits.get(p))
         regular_products = self - kits
         res = (
