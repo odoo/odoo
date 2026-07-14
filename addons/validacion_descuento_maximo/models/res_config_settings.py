2be5672be21884003c41e8c20007fbc71e749633
Aaron Quiñonez Delgado <aquinonezd@unsa.edu.pe>
2026-07-13 20:56:19 -0500

    feat/fix: resolucion de conflicto de interfaz en Ajustes y cierre funcional Closes #65

--- addons/validacion_descuento_maximo/models/res_config_settings.py
+++ addons/validacion_descuento_maximo/models/res_config_settings.py
@@ -1,11 +1,26 @@
 from odoo import fields, models
 
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+    discount_limit_percentage = fields.Float(
+        string='Límite de descuento global (%)',
+        default=15.0
+    )
+    limite_descuento_global = fields.Float(
+        string='Límite de descuento antiguo (%)',
+        related='discount_limit_percentage',
+        readonly=False
+    )
 
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
-
     discount_limit_percentage = fields.Float(
         string='Límite de descuento (%)',
+        related='company_id.discount_limit_percentage',
+        readonly=False
+    )
+    limite_descuento_global = fields.Float(
+        string='Límite de descuento antiguo Ajustes (%)',
         related='company_id.discount_limit_percentage',
         readonly=False
     )
