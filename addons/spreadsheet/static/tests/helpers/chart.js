68e8c1547fa64924b9fb9e6083795b5677c6e1eb
Rémi Rahir (rar) <rar@odoo.com>
2026-07-14 14:56:14 +0200

    [FIX] spreadsheet: Logging should ignore invalid datasources
    The getter `getLoadedDataSources` was filtering out datasources that are
    not 'ready' but they should actually filter out datasources that were
    already loaded (so ready) but invalid.
    
    closes odoo/odoo#276255
    
    Task: 6387729
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>

--- addons/spreadsheet/static/tests/helpers/chart.js
+++ addons/spreadsheet/static/tests/helpers/chart.js
@@ -51,7 +51,7 @@ export async function createSpreadsheetWithChart(params = {}) {
     return { model, env };
 }
 
-function getChartDefinition(type) {
+export function getChartDefinition(type) {
     return {
         metaData: {
             groupBy: ["foo", "bar"],
