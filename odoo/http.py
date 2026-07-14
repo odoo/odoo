31aad6c03cc8ffbe5a206524fb9b849b94dce785
Julien Castiaux <juc@odoo.com>
2026-07-14 08:47:07 +0200

    [FIX] core: jail /static

--- odoo/http.py
+++ odoo/http.py
@@ -2159,6 +2159,7 @@ class Application:
         """
 
         netloc, path = urlparse(url)[1:3]
+        path = os.path.normpath(os.path.normcase(path))
         try:
             path_netloc, module, static, resource = path.split('/', 3)
         except ValueError:
