b84dcfb8de60c0773920d5c37e6b88e06b8b8893
agbr-odoo <agbr@odoo.com>
2026-07-14 12:48:42 +0000

    [FIX] stock: allow printing nested traceability reports
    Currently, users encounter a traceback when attempting to print the traceability
    report for a manufacturing order.
    
    Steps to produce:
    - Install Manufacturing without demo data.
    - Enable Lots & Serial Numbers in Settings.
    - Create two products: Car and Engine.
    - Configure Engine to use Unique Serial Number tracking
      (Inventory →Traceability).
    - Set the on-hand quantity of Engine to 1 with serial number 0001.
    - Create a BoM for Car using Engine as a component.
    - Create and confirm a manufacturing order for Car.
    - Click `Consumed` for the component and click Produce All.
    - Navigate to Lots & Serial Numbers → Engine → 0001 → Traceability.
    - Click the arrow next to the manufacturing order reference, then click Print.
    
    Observed Behavior:
    An `Arbitrary Uncaught Python Exception` traceback is raised, resulting in a 404
    error.
    
    Root Cause:
    This issue occurs because, when the user clicks the arrow button in the
    traceability report, the template [1] invokes the `onClickUpDownStream`
    function.
    
    This function adds the URL `/stock/output_format/stock/active_id` to the context
    , as shown in [2]. Later, when the user clicks the Print button,
    `onClickPrint()` [3] constructs the print URL using the `controllerUrl` value by
     replacing the active model and active ID placeholders with values from the
    context.
    
    However, the URL stored in the context contains `/active_id` instead of
    `/:active_id`. As a result, the placeholder replacement does not occur, leaving
    the URL unchanged. This causes the print action to use an invalid URL,
    ultimately resulting in a 404 error.
    
    As shown in [4], `controllerUrl` is assigned directly from the context.
    
    Solution:
    Pass the correct URL in the context so that the active ID placeholder can be
    replaced correctly during the print action. This ensures that the generated
    print URL is valid, allowing the user to print the report without encountering
    any errors.
    
    [1]:
    https://github.com/odoo/odoo/blob/a398ade607940a281552f8cba2c1cf80bb0e77f6/addons/stock/static/src/client_actions/stock_traceability_report_backend.xml#L52-L56
    [2]:
    https://github.com/odoo/odoo/blob/a398ade607940a281552f8cba2c1cf80bb0e77f6/addons/stock/static/src/client_actions/stock_traceability_report_backend.js#L105-L118
    [3]:
    https://github.com/odoo/odoo/blob/a398ade607940a281552f8cba2c1cf80bb0e77f6/addons/stock/static/src/client_actions/stock_traceability_report_backend.js#L120-L131
    [4]:
    https://github.com/odoo/odoo/blob/a398ade607940a281552f8cba2c1cf80bb0e77f6/addons/stock/static/src/client_actions/stock_traceability_report_backend.js#L53-L55
    
    opw-6372834
    
    closes odoo/odoo#279800
    
    X-original-commit: 317f38f03c41021f6524f9aa156d046330926967
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Anshik Gopalbhai Brahmbhatt (agbr) <agbr@odoo.com>

--- addons/stock/static/tests/stock_traceability_report_backend_tests.js
+++ addons/stock/static/tests/stock_traceability_report_backend_tests.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { getFixture } from "@web/../tests/helpers/utils";
+import { click, getFixture, patchWithCleanup } from "@web/../tests/helpers/utils";
 import { createWebClient, doAction } from "@web/../tests/webclient/helpers";
 
 let serverData;
@@ -43,5 +43,56 @@ QUnit.module("Stock", {}, function (hooks) {
             target.querySelector(".o_stock_reports_page").textContent,
             "No operation made on this lot."
         );
+    });
+
+    QUnit.test("Traceability Report action contains correct url in context", async function (assert) {
+        assert.expect(1);
+        const webClient = await createWebClient({
+            serverData,
+            mockRPC: async function (route) {
+                if (route === "/web/dataset/call_kw/stock.traceability.report/get_main_lines") {
+                    return [
+                        {
+                            id: 42,
+                            model: "stock.move.line",
+                            model_id: 42,
+                            parent_id: false,
+                            usage: "out",
+                            is_used: true,
+                            lot_name: "0001",
+                            lot_id: 1,
+                            reference: "WH/MO/00001",
+                            res_id: 4,
+                            res_model: "mrp.production",
+                            columns: [
+                                "WH/MO/00001",
+                                "Engine",
+                                "06/08/2025 15:00:00",
+                                "0000000010001",
+                                "Stock",
+                                "Production",
+                                "1.00 Units",
+                            ],
+                            level: 1,
+                            unfoldable: false,
+                        },
+                    ];
+                }
+            },
+        });
+        patchWithCleanup(webClient.env.services.action, {
+            doAction(action) {
+                const res = super.doAction(...arguments);
+                if (action?.type === "ir.actions.client") {
+                    assert.strictEqual(
+                        "/stock/output_format/stock?active_id=:active_id&active_model=:active_model",
+                        action.context.url
+                    );
+                }
+                return res;
+            },
+        });
+        await doAction(webClient, 42);
+        await click(target.querySelector(".fa-level-up"));
     });
 });
