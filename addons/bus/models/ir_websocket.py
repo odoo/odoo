f7645c509e4dcffcf6229afe1dc6aed7fe84f943
tsm-odoo <tsm@odoo.com>
2026-07-14 10:35:48 +0200

    [MOV] bus: move dispatcher code out of bus model
    As the dispatching logic grows, having both the bus model
    and the dispatcher in a single file becomes cumbersome.
    
    This commit moves the dispatcher in its own file.
    
    part of task-5449503
    
    Part-of: odoo/odoo#278597
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/bus/models/ir_websocket.py
+++ addons/bus/models/ir_websocket.py
@@ -5,7 +5,8 @@ from odoo.http import request
 from odoo.http.session import check
 from odoo.tools.misc import OrderedSet
 
-from odoo.addons.bus.models.bus import channel_with_db, dispatch
+from odoo.addons.bus.bus_dispatcher import dispatch
+from odoo.addons.bus.models.bus import channel_with_db
 from odoo.addons.bus.websocket import wsrequest
 
 
--- addons/bus/tools/__init__.py
+++ addons/bus/tools/__init__.py
@@ -1,1 +1,2 @@
+from . import misc
 from . import orjson
