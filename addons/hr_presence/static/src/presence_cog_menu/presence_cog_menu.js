02409a025613a984606f4792cc7b12fa6ba0f3f5
Abdelrahman Gad (abgad) <abgad@odoo.com>
2026-07-14 13:39:08 +0000

    [FIX] hr_presence: remove redundant presence actions
    The actions menu is cluttered with redundant presence actions. To streamline
    the UI, the presence actions ("Set Present", "Set Absent", "Send SMS", and
    "Create a Time Off") have been removed from the root menu and are now kept
    exclusively within the "Presence Control" submenu.
    
    Steps to reproduce:
    1. Navigate to an Employee record.
    2. Open the Actions menu.
    3. Observe that "Set Present", "Set Absent", "Send SMS", and "Create a
       Time Off" appear in the root menu AND in the "Presence Control" submenu.
    
    Furthermore, invoking these actions from the Presence Control submenu did not
    pass the correct record context. Because of this, dialogs like
    "Create a Time Off" failed to automatically prefill with the currently
    selected employee's data, disrupting the workflow.
    
    1. Navigate to an Employee record.
    2. Open the Actions menu and hover over the "Presence Control" submenu.
    3. Click "Create a Time Off".
    4. Observe that the employee field in the dialog is blank instead of
       prefilling with the current employee.
    
    task-6384993
    
    closes odoo/odoo#279137
    
    X-original-commit: 23a9dffa34b0f20587255d1f43694f9d01a79eb2
    Signed-off-by: Gabriel de Paula Felix (gdpf) <gdpf@odoo.com>

--- addons/hr_presence/static/src/presence_cog_menu/presence_cog_menu.js
+++ addons/hr_presence/static/src/presence_cog_menu/presence_cog_menu.js
@@ -26,11 +26,24 @@ export class PresenceCogMenu extends Component {
 
     onItemSelected(item) {
         const options = {};
-        // Get active_id from record prop or resId prop (both available in form view)
-        const activeId = this.props.record?.id || this.props.resId;
-        if (activeId) {
-            options.additionalContext = { active_id: activeId };
+        let activeIds = [];
+
+        // Fetch the selected record IDs if there is a selection, otherwise fetch the currently focused record's ID.
+        if (this.env.model.root.selection && this.env.model.root.selection.length) {
+            activeIds = this.env.model.root.selection.map(r => r.resId);
+        } else if (this.env.model.root.resId) {
+            activeIds = [this.env.model.root.resId];
         }
+
+        // Inject the fetched ID(s) and model into the action's context so the backend knows which record(s) to process.
+        if (activeIds.length > 0) {
+            options.additionalContext = { 
+                active_id: activeIds[0],
+                active_ids: activeIds,
+                active_model: "hr.employee",
+            };
+        }
+
         this.actionService.doAction(item.id, options);
     }
 
