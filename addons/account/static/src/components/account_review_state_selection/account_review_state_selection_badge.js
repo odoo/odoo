dd358db399af1899155843e402e25706ad7774f3
Lucas Perais (lpe) <lpe@odoo.com>
2026-07-14 13:49:00 +0000

    [FIX] account: review state selection field: fix opening a new record
    Click Working Files menu, then open studio.
    Before this commit there was an error, because the accounting code
    tried to check access rights on an new record (no id)
    
    After this commit there is no crash.
    
    runbot-error-941248
    
    closes odoo/odoo#276623
    
    X-original-commit: ccacf1c60ad01c1a4a02be1fcee94ae6d2e5aabe
    Related: odoo/enterprise#124505
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>
    Signed-off-by: Lucas Perais (lpe) <lpe@odoo.com>

--- addons/account/static/src/components/account_review_state_selection/account_review_state_selection_badge.js
+++ addons/account/static/src/components/account_review_state_selection/account_review_state_selection_badge.js
@@ -57,7 +57,7 @@ export class AccountReviewStateSelectionBadge extends Component {
             editableOptions.push(false);
         }
 
-        if (await user.checkAccessRight(this.props.record.resModel, "write", this.props.record.resId)) {
+        if (this.props.record.resId && await user.checkAccessRight(this.props.record.resModel, "write", this.props.record.resId)) {
             for (let [key, value] of Object.entries(this.props.options)) {
                 if (
                     [true, undefined].includes(value.can_edit)
