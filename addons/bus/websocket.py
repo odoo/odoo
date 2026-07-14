4cf17ce3a3670e832333a0872368cbb975a5950e
tsm-odoo <tsm@odoo.com>
2026-07-14 15:56:58 +0200

    [MOV] bus: extract websocket protocol to its own file
    Move the frame opcode, close codes and connection state enums, along with
    the websocket exceptions out of `websocket.py` in order to prevent
    cyclic dependencies between the dispatcher and the websocket file.
    
    part of task-5449503
    
    Part-of: odoo/odoo#278597
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/bus/websocket.py
+++ addons/bus/websocket.py
@@ -44,6 +44,20 @@ from odoo.tools import config
 from .models.bus import dispatch, fetch_bus_notifications
 from .session_helpers import check_session, new_env
 from .tools import orjson
+from .websocket_protocol import (
+    CloseCode,
+    ConnectionClosed,
+    ConnectionState,
+    InvalidCloseCodeException,
+    InvalidDatabaseException,
+    InvalidStateException,
+    InvalidWebsocketRequest,
+    LifecycleEvent,
+    Opcode,
+    PayloadTooLargeException,
+    ProtocolError,
+    RateLimitExceededException,
+)
 
 _logger = logging.getLogger(__name__)
 _stopping = threading.Event()
@@ -98,61 +112,6 @@ class UpgradeRequired(HTTPException):
         return headers
 
 
-class WebsocketException(Exception):
-    """ Base class for all websockets exceptions """
-
-
-class ConnectionClosed(WebsocketException):
-    """
-    Raised when the other end closes the socket without performing
-    the closing handshake.
-    """
-
-
-class InvalidCloseCodeException(WebsocketException):
-    def __init__(self, code):
-        super().__init__(f"Invalid close code: {code}")
-
-
-class InvalidDatabaseException(WebsocketException):
-    """
-    When raised: the database probably does not exists anymore, the
-    database is corrupted or the database version doesn't match the
-    server version.
-    """
-
-
-class InvalidStateException(WebsocketException):
-    """
-    Raised when an operation is forbidden in the current state.
-    """
-
-
-class InvalidWebsocketRequest(WebsocketException):
-    """
-    Raised when a websocket request is invalid (format, wrong args).
-    """
-
-
-class PayloadTooLargeException(WebsocketException):
-    """
-    Raised when a websocket message is too large.
-    """
-
-
-class ProtocolError(WebsocketException):
-    """
-    Raised when a frame format doesn't match expectations.
-    """
-
-
-class RateLimitExceededException(Exception):
-    """
-    Raised when a client exceeds the number of request in a given
-    time.
-    """
-
-
 # Idea taken from the python cookbook:
 # https://github.com/dabeaz/python-cookbook/blob/6e46b7/src/12/polling_multiple_thread_queues/pqueue.py
 class PollablePriorityQueue(PriorityQueue):
@@ -179,52 +138,8 @@ class PollablePriorityQueue(PriorityQueue):
 
 
 # ------------------------------------------------------
-# WEBSOCKET LIFECYCLE
-# ------------------------------------------------------
-
-
-class LifecycleEvent(IntEnum):
-    OPEN = 0
-    CLOSE = 1
-
-
-# ------------------------------------------------------
 # WEBSOCKET
 # ------------------------------------------------------
-
-
-class Opcode(IntEnum):
-    CONTINUE = 0x00
-    TEXT = 0x01
-    BINARY = 0x02
-    CLOSE = 0x08
-    PING = 0x09
-    PONG = 0x0A
-
-
-class CloseCode(IntEnum):
-    CLEAN = 1000
-    GOING_AWAY = 1001
-    PROTOCOL_ERROR = 1002
-    INCORRECT_DATA = 1003
-    ABNORMAL_CLOSURE = 1006
-    INCONSISTENT_DATA = 1007
-    MESSAGE_VIOLATING_POLICY = 1008
-    MESSAGE_TOO_BIG = 1009
-    EXTENSION_NEGOTIATION_FAILED = 1010
-    SERVER_ERROR = 1011
-    RESTART = 1012
-    TRY_LATER = 1013
-    BAD_GATEWAY = 1014
-    SESSION_EXPIRED = 4001
-    KEEP_ALIVE_TIMEOUT = 4002
-    KILL_NOW = 4003
-
-
-class ConnectionState(IntEnum):
-    OPEN = 0
-    CLOSING = 1
-    CLOSED = 2
 
 
 # Used to maintain order of commands in the queue according to their priority
