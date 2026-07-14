86134c9c98b785072bbeb2e188f55766e5c1c4a1
Bayron Alpízar Quesada <bayalpiizar777@gmail.com>
2026-07-14 17:31:53 -0600

    fix(distribuidora_ventas): quitar restriccion de fecha de entrega lunes/miercoles/viernes

--- addons/distribuidora_ventas/tests/__init__.py
+++ addons/distribuidora_ventas/tests/__init__.py
@@ -1,4 +1,4 @@
 from . import test_partner_categories
-from . import test_sale_order_delivery_day
 from . import test_customer_pricelist
 from . import test_invoice_from_order_quantity
+from . import test_sale_order_accepts_any_delivery_date
