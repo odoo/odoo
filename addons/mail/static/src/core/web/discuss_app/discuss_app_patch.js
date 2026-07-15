9a194c88dbc5f2c35ffa54b2bcea9c00142a6475
Didier (did) <did@odoo.com>
2026-07-15 10:04:34 +0200

    [REF] mail: remove useLayoutEffect from discuss_app_patch
    task-6373278

--- addons/mail/static/src/core/web/discuss_app/discuss_app_patch.js
+++ addons/mail/static/src/core/web/discuss_app/discuss_app_patch.js
@@ -1,22 +1,21 @@
-import { useLayoutEffect } from "@web/owl2/utils";
-
 import { Discuss } from "@mail/core/public_web/discuss_app/discuss_app";
 
 import { patch } from "@web/core/utils/patch";
 import { ControlPanel } from "@web/search/control_panel/control_panel";
+import { useOnChange } from "@mail/utils/common/hooks";
 
 Object.assign(Discuss.components, { ControlPanel });
 
 patch(Discuss.prototype, {
     setup() {
         super.setup();
-        useLayoutEffect(
+        useOnChange(
+            () => [this.thread?.displayName],
             (threadName) => {
                 if (threadName) {
                     this.env.config?.setDisplayName(threadName);
                 }
-            },
-            () => [this.thread?.displayName]
+            }
         );
     },
 });
