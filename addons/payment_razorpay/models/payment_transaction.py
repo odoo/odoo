83b25d36a90cf26bedd22700b644182a75b5f3fe
Valentin Chevalier <vcr@odoo.com>
2026-07-15 17:45:07 +0200

    [FIX] payment_razorpay: discard invalid notification data opw-3097856

--- addons/payment_razorpay/models/payment_transaction.py
+++ addons/payment_razorpay/models/payment_transaction.py
@@ -3,6 +3,7 @@
 import logging
 import pprint
 
+from werkzeug.exceptions import Forbidden
 from werkzeug.urls import url_encode, url_join
 
 from odoo import _, models
@@ -272,6 +273,10 @@ class PaymentTransaction(models.Model):
                 "Response of '/payments' request for transaction with reference %s:\n%s",
                 self.reference, pprint.pformat(entity_data)
             )
+        if self.reference != entity_data["description"]:
+            _logger.warning("Received payment data with incorrect reference")
+            raise Forbidden()
+
         entity_id = entity_data.get('id')
         if not entity_id:
             raise ValidationError("Razorpay: " + _("Received data with missing entity id."))
