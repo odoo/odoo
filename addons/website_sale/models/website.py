ed199901349fc868cf31a8805c8725a23de79a0d
Mahdi Alijani (malj) <malj@odoo.com>
2026-07-15 08:59:36 +0000

    [FIX] website_sale: use delivery address to calculate default fp
    Issue:
    ---
    Fiscal position is wrongly set to `self.env.user.partner_id.country_id`
    instead of `partner_shipping` country, if `partner_shipping_id` is not
    changed in the checkout process.
    
    Steps:
    1- Create two auto detect fiscal positions: France, Germany
    2- Set portal user's partner address country to France.
    3- Using portal user, shop from website, and create a delivery address.
    4- Pay and confirm the order.
    5- Using the admin user, you check the SO's FP which is correctly set
       to Germany.
    6- Using portal user, again shop from website, and don't change
       address. Keep previous shipping address which is Germany.
    7- Confirm and pay the order.
    8- Using admin user, check the new SO's FP. It's set to France.
    
    Cause:
    ---
    `_compute_fiscal_position_id` in SO depends on `partner_shipping_id`.
    When the `partner_shipping_id` is not changed, the fiscal position
    value set in create will remain.
    This value is set in `Website._prepare_sale_order_values()`.
    The `fiscal_position_id` is set to self.fiscal_position_id, which is
    `_get_fiscal_position(self.env.user.partner_id)`.
    
    Fix:
    ---
    If the user has already a SO, we can use last SO's shipping address
    and invoice address to calculate FP in `_prepare_sale_order_values`.
    
    opw-6357638
    
    closes odoo/odoo#281039
    
    X-original-commit: e5e5a6f39cccb1188b0f6bde4416c17b4555c995
    Signed-off-by: Mohammadmahdi Alijani (malj) <malj@odoo.com>

--- addons/website_sale/models/website.py
+++ addons/website_sale/models/website.py
@@ -691,7 +691,7 @@ class Website(models.Model):
             'company_id': self.company_id.id,
             'partner_id': partner_sudo.id,
 
-            'fiscal_position_id': request.fiscal_position.id,
+            **(self.is_public_user() and {'fiscal_position_id': request.fiscal_position.id} or {}),
             'pricelist_id': request.pricelist.id,
 
             'team_id': self.salesteam_id.id,
