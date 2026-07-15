c608a22b03f2d4b25672e05523353eda737a26e2
Jugurtha <jugac@odoo.com>
2026-07-15 14:24:55 +0200

    [IMP] account_peppol_selfbilling: ensure unique sequence per partner for self-billing bills
    Self-billing bill sequences should be unique per partner, as implemented in v19+.
    This commit backports that behavior to 18.0.
    
    Part of :
    task-6103142
    
    closes odoo/odoo#276028
    
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>

--- addons/account_peppol_selfbilling/models/account_move.py
+++ addons/account_peppol_selfbilling/models/account_move.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import _, models, fields
 
 
 class AccountMove(models.Model):
@@ -31,3 +31,36 @@ class AccountMove(models.Model):
             and edi_builder._can_export_selfbilling()
             and self.journal_id.is_self_billing
         )
+
+    def _get_last_sequence_domain(self, relaxed=False):
+        # EXTENDS 'account'
+        where_string, param = super()._get_last_sequence_domain(relaxed)
+        if self.journal_id.type == 'purchase' and self.journal_id.is_self_billing:
+            if self.partner_id:
+                where_string += " AND commercial_partner_id = %(partner_id)s "
+                param['partner_id'] = self.partner_id.commercial_partner_id.id
+            else:
+                where_string += " AND false "
+
+        return where_string, param
+
+    def _get_starting_sequence(self):
+        # EXTENDS 'account'
+        self.ensure_one()
+
+        if not (self.journal_id.type == 'purchase' and self.journal_id.is_self_billing):
+            return super()._get_starting_sequence()
+
+        partner_identifier = str(self.partner_id.commercial_partner_id.id) if self.partner_id else _('[Partner id]')
+        move_date = self.date or self.invoice_date or fields.Date.context_today(self)
+        starting_sequence = "%s%s/%04d/%02d/0000" % (
+            self.journal_id.code,
+            partner_identifier.zfill(5),
+            move_date.year,
+            move_date.month,
+        )
+        if self.journal_id.refund_sequence and self.move_type in ('out_refund', 'in_refund'):
+            starting_sequence = "R" + starting_sequence
+        if self.journal_id.payment_sequence and self.payment_id:
+            starting_sequence = "P" + starting_sequence
+        return starting_sequence
