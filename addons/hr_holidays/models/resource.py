40c581b7e649b1be2ac7dbdc0b0ad6f37422bfc7
Yassin Abdelkarim <yabd@odoo.com>
2026-07-15 10:11:50 +0200

    [FIX] hr_holidays: fix company check on calendar associated leaves
    Following odoo/odoo#230736, the function now includes in its count the
    leaves that have no calendar, regardless of the company. This commit
    fixes this by grouping them by company as well, and including that
    count.

--- addons/hr_holidays/models/resource.py
+++ addons/hr_holidays/models/resource.py
@@ -176,14 +176,47 @@ class ResourceCalendar(models.Model):
 
     def _compute_associated_leaves_count(self):
         leaves_read_group = self.env['resource.calendar.leaves']._read_group(
-            [('resource_id', '=', False), '|', ('calendar_id', 'in', self.ids), ('calendar_id', '=', False)],
-            ['calendar_id'],
+            [('resource_id', '=', False),
+             '|',
+                ('calendar_id', 'in', self.ids),
+                '&',
+                    ('calendar_id', '=', False),
+                    ('company_id', 'in', self.company_id.ids + [False])],
+            ['calendar_id', 'company_id'],
             ['__count'],
         )
-        result = {calendar.id if calendar else 'global': count for calendar, count in leaves_read_group}
-        global_leave_count = result.get('global', 0)
+        calendar_leaves = {}
+        company_leaves = {}
+        for calendar, company, count in leaves_read_group:
+            if calendar:
+                calendar_leaves[calendar.id] = calendar_leaves.get(calendar.id, 0) + count
+            else:
+                company_leaves[company.id] = company_leaves.get(company.id, 0) + count
         for calendar in self:
-            calendar.associated_leaves_count = result.get(calendar.id, 0) + global_leave_count
+            calendar.associated_leaves_count = calendar_leaves.get(calendar.id, 0) + company_leaves.get(calendar.company_id.id, 0)
+
+    def action_open_public_holidays(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'resource.calendar.leaves',
+            'name': _('Public Holidays'),
+            'view_mode': 'list',
+            'view_id': self.env.ref('hr_holidays.resource_calendar_leaves_tree_inherit').id,
+            'domain': [
+                ('resource_id', '=', False),
+                '|',
+                    ('calendar_id', '=', self.id),
+                    '&',
+                        ('calendar_id', '=', False),
+                        ('company_id', 'in', self.company_id.ids + [False]),
+            ],
+            'context': {
+                'default_calendar_id': self.id,
+                'search_default_filter_date': True,
+            },
+            'search_view_id': self.env.ref('hr_holidays.resource_calendar_leaves_view_search_inherit').id,
+        }
 
 
 class ResourceResource(models.Model):
