a893dccc62b921d27530c211e12be4ed28963c03
Lucas Lefèvre (lul) <lul@odoo.com>
2026-07-16 15:58:29 +0200

    [REF] calendar: use named placeholders
    This commit prepares the ground the next commits.
    
    Remove the postgresql `now()` from the query because it cannot
    be mocked in tests. `self.env.cr.now()` is the exact same value, but can be
    patched.

--- addons/calendar/models/calendar_alarm_manager.py
+++ addons/calendar/models/calendar_alarm_manager.py
@@ -21,6 +21,11 @@ class CalendarAlarm_Manager(models.AbstractModel):
             self.env[model_name].flush_model()
 
         result = {}
+        params = {
+            'alarm_type': alarm_type,
+            'partner_ids': tuple(partners.ids),
+            'now': self.env.cr.now(),
+        }
         delta_request = SQL("""
             SELECT
                 rel.calendar_event_id,
@@ -29,9 +34,9 @@ class CalendarAlarm_Manager(models.AbstractModel):
             FROM
                 calendar_alarm_calendar_event_rel AS rel
             LEFT JOIN calendar_alarm AS alarm ON alarm.id = rel.calendar_alarm_id
-            WHERE alarm.alarm_type = %s
+            WHERE alarm.alarm_type = %(alarm_type)s
             GROUP BY rel.calendar_event_id
-        """, alarm_type)
+        """, **params)
         base_request = SQL("""
             SELECT
                 cal.id,
@@ -46,9 +51,9 @@ class CalendarAlarm_Manager(models.AbstractModel):
             INNER JOIN calcul_delta ON calcul_delta.calendar_event_id = cal.id
             INNER JOIN calendar_event_res_partner_rel AS part_rel
                 ON part_rel.calendar_event_id = cal.id
-                AND part_rel.res_partner_id IN %s
+                AND part_rel.res_partner_id IN %(partner_ids)s
             WHERE cal.active = True
-        """, tuple(partners.ids))
+        """, **params)
 
         # Upper bound on first_alarm of requested events
         # first alarm in the future + 3 minutes if there is one, now otherwise
@@ -56,17 +61,22 @@ class CalendarAlarm_Manager(models.AbstractModel):
             COALESCE((SELECT MIN(cal.start - interval '1' minute  * calcul_delta.max_delta)
             FROM calendar_event cal
             RIGHT JOIN calcul_delta ON calcul_delta.calendar_event_id = cal.id
-            WHERE cal.start - interval '1' minute  * calcul_delta.max_delta > now() at time zone 'utc'
-        ) + interval '3' minute, now() at time zone 'utc')""")
+            WHERE cal.start - interval '1' minute  * calcul_delta.max_delta > %(now)s
+        ) + interval '3' minute, %(now)s)""", **params)
 
         self.env.flush_all()
         self.env.cr.execute(SQL("""
-            WITH calcul_delta AS (%s)
+            WITH calcul_delta AS (%(delta_request)s)
             SELECT *
-                FROM ( %s ) AS ALL_EVENTS
-            WHERE ALL_EVENTS.first_alarm < %s
-                AND ALL_EVENTS.last_alarm > (%s at time zone 'utc')
-        """, delta_request, base_request, first_alarm_max_value, fields.Datetime.now()))
+                FROM ( %(base_request)s ) AS ALL_EVENTS
+            WHERE ALL_EVENTS.first_alarm < %(first_alarm_max_value)s
+                AND ALL_EVENTS.last_alarm > %(now)s
+        """,
+            delta_request=delta_request,
+            base_request=base_request,
+            first_alarm_max_value=first_alarm_max_value,
+            now=params['now'],
+        ))
 
         for event_id, first_alarm, last_alarm, first_meeting, last_meeting, min_duration, max_duration in self.env.cr.fetchall():
             result[event_id] = {
