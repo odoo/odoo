c6ccec955bba656f5a7b9bde73061024321da205
Sébastien (blse) <blse@odoo.com>
2026-07-16 12:57:11 +0200

    [REF] html_builder: simplify cleaning of snippet when saving a snippet
    This commit changes `saveSnippet` method on the snippet service to
    accept a single callback that does the whole preparation for save.
    
    This is possible because the method `cleanSnippetForSave`
    has no overrides (thanks to the previous commits, that uses
    `clean_for_save_processors` instead).
    
    task-6398653

--- addons/html_builder/static/src/core/save_snippet_plugin.js
+++ addons/html_builder/static/src/core/save_snippet_plugin.js
@@ -55,27 +55,6 @@ export class SaveSnippetPlugin extends Plugin {
         ];
     }
 
-    /**
-     * Execute the `on_will_save_handlers` on {@link snippetEl},
-     * then execute {@link callback}, and finally execute the
-     * `on_saved_handlers` on {@link snippetEl}.
-     * This is used, for example, to stop the interactions before cloning a
-     * snippet, and restarting them after cloning it.
-     *
-     * @param {HTMLElement} snippetEl
-     * @param {Function} callback
-     */
-    async wrapWithBeforeAfterSaveHandlers(snippetEl, callback) {
-        await Promise.all(this.trigger("on_will_save_handlers", snippetEl));
-        let node;
-        try {
-            node = callback();
-        } finally {
-            this.trigger("on_saved_handlers", snippetEl);
-        }
-        return node;
-    }
-
     async saveSnippet(el) {
         // When saving a parent handler, save the child snippet instead
         if (el.matches(BLOCKQUOTE_PARENT_HANDLERS)) {
@@ -84,18 +63,19 @@ export class SaveSnippetPlugin extends Plugin {
                 el = childBlockquote;
             }
         }
-        const cleanForSaveProcessors = [
-            ...this.getResource("clean_for_save_processors"),
-            (root) => {
-                escapeTextNodes(root);
-                return root;
-            },
-        ];
-        const savedName = await this.config.saveSnippet(
-            el,
-            cleanForSaveProcessors,
-            this.wrapWithBeforeAfterSaveHandlers.bind(this)
-        );
+        const savedName = await this.config.saveSnippet(el, async (el) => {
+            await Promise.all(this.trigger("on_will_save_handlers", el));
+            try {
+                const cloneEl = el.cloneNode(true);
+                const cleanedEl = this.processThrough("clean_for_save_processors", cloneEl, {
+                    saveSnippet: true,
+                });
+                escapeTextNodes(cleanedEl);
+                return cleanedEl;
+            } finally {
+                this.trigger("on_saved_handlers", el);
+            }
+        });
         this.dependencies.disableSnippets.disableUndroppableSnippets();
         if (savedName) {
             if (this.delegateTo("custom_snippets_notification_overrides", savedName)) {
