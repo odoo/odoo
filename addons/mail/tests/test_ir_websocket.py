6d89e5df55bee805d61c3c8b24f77599183fea20
tsm-odoo <tsm@odoo.com>
2026-07-16 16:15:34 +0200

    [FIX] mail: subscribe to category without token
    [1] added a bus channel for discuss categories. This can be done
    with or without any token. When the token is not passed,
    `verify_limited_field_access_token` is still called and crashes.
    
    When no token is provided, we should use category access rights
    instead and avoir verifying the token (which is `None`).
    
    [1]: https://github.com/odoo/odoo/pull/243131

--- addons/mail/tests/test_ir_websocket.py
+++ addons/mail/tests/test_ir_websocket.py
@@ -114,3 +114,19 @@ class TestIrWebsocket(WebsocketCase):
         self.assertEqual(notification["message"]["payload"]["im_status"], "away")
         self.assertEqual(notification["message"]["payload"]["presence_status"], "away")
         self.assertEqual(notification["message"]["payload"]["partner_id"], away_user.partner_id.id)
+
+    def test_subscribe_discuss_category_without_token(self):
+        new_test_user(self.env, login="bob_user", groups="base.group_user")
+        session = self.authenticate("bob_user", "bob_user")
+        category = self.env["discuss.category"].create({"name": "favorites"})
+        websocket = self.websocket_connect(cookie=f"session_id={session.sid};")
+        self.subscribe(
+            websocket,
+            [f"discuss.category_{category.id}"],
+            self.env["bus.bus"]._bus_last_id(),
+        )
+        category._bus_send("sanity_check", None)
+        self.trigger_notification_dispatching(category)
+        notifications = json.loads(websocket.recv())
+        self.assertEqual(len(notifications), 1)
+        self.assertEqual(notifications[0]["message"]["type"], "sanity_check")
