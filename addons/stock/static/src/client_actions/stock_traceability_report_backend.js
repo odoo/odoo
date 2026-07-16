af185952827204b9b9981d22a3dcbbd1a0ffdc3c
Stéphane Diez <snd@odoo.com>
2026-07-16 11:13:56 +0200

    [IMP] stock: traceability report - shift rows to right
    Shift rows to the right when expanding children lines.
    This keeps track of all the levels of children lines and uses
    the highest level to determine the number of pixels Odoo needs to
    shift subsequent rows.
    
    task 5265405
    
    Part-of: odoo/odoo#269357
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/stock/static/src/client_actions/stock_traceability_report_backend.js
+++ addons/stock/static/src/client_actions/stock_traceability_report_backend.js
@@ -58,6 +58,7 @@ export class TraceabilityReport extends Component {
         this.state = proxy({
             lines: this.props.state?.lines || [],
         });
+        this.levels = new Map();
         this.hasUnfoldableLines = this.state.lines.some((line) => line.unfoldable);
         this.isExpanded = false;
 
@@ -94,6 +95,7 @@ export class TraceabilityReport extends Component {
             );
             this.hasUnfoldableLines = this.state.lines.some((line) => line.unfoldable);
         }
+        this.addLevels(this.state.lines);
     }
 
     get hasFoldedLines() {
@@ -145,6 +147,7 @@ export class TraceabilityReport extends Component {
     }
 
     async onClickUnfold() {
+        this.levels.clear();
         const unfoldLines = (lines) => {
             for (const line of lines) {
                 if (line.unfoldable) {
@@ -161,10 +164,12 @@ export class TraceabilityReport extends Component {
             ).map(
                 (line) => { return processLine(line, line.lines, false); }
             );
+            this.addLevels(this.state.lines);
             this.isExpanded = true;
         } else {
             unfoldLines(this.state.lines);
         }
+        this.addLevels(this.state.lines);
     }
 
     onClickFold() {
@@ -177,6 +182,38 @@ export class TraceabilityReport extends Component {
             }
         };
         foldLines(this.state.lines);
+        this.removeLevels(this.state.lines);
+    }
+
+    addLevels(lines) {
+        for (const line of lines) {
+            if (line.line_type === 'child' || line.level === 1) {
+                this.levels.set(line.id, line.level);
+                if (!line.isFolded && line.lines.length) {
+                    this.addLevels(line.lines);
+                }
+            }
+        }
+    }
+
+    removeLevels(lines) {
+        for (const line of lines) {
+            if (line.level != 1) {
+                this.levels.delete(line.id);
+            }
+            if (line.lines.length) {
+                this.removeLevels(line.lines);
+            }
+        }
+    }
+
+    getLevel(line) {
+        const maxLevel = Math.max(...this.levels.values());
+        if (line.line_type === 'parent') {
+            return maxLevel + line.level;
+        } else {
+            return maxLevel - line.level;
+        }
     }
 
     async toggleLine(line, line_type=false) {
@@ -191,6 +228,7 @@ export class TraceabilityReport extends Component {
                 (line) => { return processLine(line); }
             );
         }
+        line.isFolded ? this.addLevels(line.lines) : this.removeLevels(line.lines);
         line.isFolded = !line.isFolded;
     }
 }
