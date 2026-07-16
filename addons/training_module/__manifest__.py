8f424ed2a2e06ca05077aaadb7c4bd87de435bb4
Shahedkhalaileh <khalaileh.shahed@gmail.com>
2026-07-16 22:10:21 +0300

    [ADD] training_module: add initial training record model

---
+++ addons/training_module/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Training Module',
+    'version': '1.0',
+    'category': 'Training',
+    'summary': 'Custom module for Odoo training exercises',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
