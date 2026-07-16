d98597d62bc8fedc710b2721c9f5634ef8d1c53b
Sébastien (blse) <blse@odoo.com>
2026-07-16 11:31:17 +0200

    [REF] website: re-use implementation of `escapeTextNodes`
    Following the website builder refactor, escaping text nodes in saved
    view was implemented twice.
    
    This commit removes one of the implementation, and use the other
    instead.
    
    task-6398653

--- addons/website/static/src/builder/plugins/translation/save_translation_plugin.js
+++ addons/website/static/src/builder/plugins/translation/save_translation_plugin.js
@@ -1,3 +1,4 @@
+import { escapeTextNodes } from "@html_builder/utils/escaping";
 import { Plugin } from "@html_editor/plugin";
 import { withSequence } from "@html_editor/utils/resource";
 import { rpc } from "@web/core/network/rpc";
@@ -52,9 +53,10 @@ export class SaveTranslationPlugin extends Plugin {
             const translations = {};
             translations[this.services.website.currentWebsite.metadata.lang] = Object.assign(
                 {},
-                ...els.map((el) => ({
-                    [el.dataset["oeTranslationSourceSha"]]: this.getEscapedElement(el).innerHTML,
-                }))
+                ...els.map((el) => {
+                    escapeTextNodes(el);
+                    return { [el.dataset["oeTranslationSourceSha"]]: el.innerHTML };
+                })
             );
             return rpc("/website/field/translation/update", {
                 model: els[0].dataset["oeModel"],
@@ -65,34 +67,6 @@ export class SaveTranslationPlugin extends Plugin {
         }
         await this.dependencies.websiteSavePlugin.saveView(els[0], false);
         return true;
-    }
-
-    getEscapedElement(el) {
-        const escapedEl = el.cloneNode(true);
-        const allElements = [escapedEl, ...escapedEl.querySelectorAll("*")];
-        const exclusion = [];
-        for (const element of allElements) {
-            if (
-                element.matches(
-                    "object,iframe,script,style,[data-oe-model]:not([data-oe-model='ir.ui.view'])"
-                )
-            ) {
-                exclusion.push(element);
-                exclusion.push(...element.querySelectorAll("*"));
-            }
-        }
-        const exclusionSet = new Set(exclusion);
-        const toEscapeEls = allElements.filter((el) => !exclusionSet.has(el));
-        for (const toEscapeEl of toEscapeEls) {
-            for (const child of Array.from(toEscapeEl.childNodes)) {
-                if (child.nodeType === 3) {
-                    const divEl = document.createElement("div");
-                    divEl.textContent = child.nodeValue;
-                    child.nodeValue = divEl.innerHTML;
-                }
-            }
-        }
-        return escapedEl;
     }
 }
 
