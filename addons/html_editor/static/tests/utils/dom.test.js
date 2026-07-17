54eec0ec0059ed276b7758a2557626a03d8bfe91
Jinjiu Liu <jili@odoo.com>
2026-07-17 07:38:51 +0000

    [FIX] html_editor: avoid element splitting with invisible first child
    Before this commit: In some cases, an element may have a invisible `/n`
    first child from the html. When inserting content to it, the insert
    function doesn't check if the first child is visible, and then split the
    element wrongly.
    
    After this commit: we now check if the previous sibling of the current
    node is invisible, if so, we don't split the parent element.
    
    task-6352841
    
    closes odoo/odoo#277313
    
    X-original-commit: 7f4c5d90b7223c2ba7c91538379a91d14d720a82
    Signed-off-by: Geelen Sébastien (sge) <sge@odoo.com>
    Signed-off-by: Jinjiu Liu (jili) <jili@odoo.com>

--- addons/html_editor/static/tests/utils/dom.test.js
+++ addons/html_editor/static/tests/utils/dom.test.js
@@ -388,7 +388,6 @@ describe("wrapInlinesInBlocks", () => {
                 <div>
                     <div contenteditable="false" style="display: inline;">inline</div><span class="a">span</span>[]
                 </div>
-                <div class="o-paragraph"><br></div>
                 <div>
                     text
                     <div contenteditable="false" style="display: inline;">inline</div>
