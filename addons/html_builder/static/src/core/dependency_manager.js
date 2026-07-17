7ccf3a31cec0350c34a884597e1b01abdddcf35e
Géry Debongnie <ged@odoo.com>
2026-07-17 13:50:55 +0000

    [IMP] web: update owl to v3.0.0-alpha.44
    Release notes:
    https://github.com/odoo/owl/releases/tag/v3.0.0-alpha.44

--- addons/html_builder/static/src/core/dependency_manager.js
+++ addons/html_builder/static/src/core/dependency_manager.js
@@ -17,6 +17,8 @@ export class DependencyManager extends EventBus {
         this.dependenciesMap = {};
         this.count = 0;
         this.dirty = false;
+        this.retired = new Set();
+        this.sweepScheduled = false;
         this.triggerDependencyUpdated = batched(() => {
             this.trigger("dependency-updated");
         });
@@ -37,6 +39,18 @@ export class DependencyManager extends EventBus {
      * @param {Boolean} ignored - should not add the dependency to the map
      */
     add(id, value, ignored = false) {
+        // A new registration supersedes entries with the same id that were
+        // retired by cancelled components: they were kept alive only until
+        // their replacement (this one) showed up (see retireByValue).
+        const superseded = this.dependencies.filter(
+            ([entryId, entryValue]) => entryId === id && this.retired.has(entryValue)
+        );
+        if (superseded.length) {
+            this.dependencies = this.dependencies.filter((entry) => !superseded.includes(entry));
+            for (const [, entryValue] of superseded) {
+                this.retired.delete(entryValue);
+            }
+        }
         // In case the dependency is added after a dependent try to get it
         // an event is scheduled to notify the dependent about it.
         if (!ignored || !(id in this.dependenciesMap)) {
@@ -60,6 +74,46 @@ export class DependencyManager extends EventBus {
      */
     removeByValue(value) {
         this.dependencies = this.dependencies.filter(([, v]) => v !== value);
+        this.retired.delete(value);
+        this.dirty = true;
+        this.triggerDependencyUpdated();
+    }
+    /**
+     * Removes the entry at the next animation frame rather than immediately,
+     * unless a new entry with the same id supersedes it first (see add()).
+     *
+     * This is used when the owning component is destroyed before being
+     * mounted, i.e. when its render was cancelled by a new render of an
+     * ancestor: that new render usually recreates a replacement component,
+     * but asynchronously (willStart, slots). Removing the entry right away
+     * would make dependents observe a transiently missing dependency and
+     * flip their state back and forth on each recreation, up to an infinite
+     * render loop. So keep serving the current entry in the meantime, and
+     * only drop it if no replacement showed up by the next animation frame
+     * (which is when cancelled components used to be destroyed).
+     *
+     * @param {BuilderOptionDependency} value
+     */
+    retireByValue(value) {
+        if (!this.dependencies.some(([, v]) => v === value)) {
+            return;
+        }
+        this.retired.add(value);
+        if (!this.sweepScheduled) {
+            this.sweepScheduled = true;
+            requestAnimationFrame(() => {
+                this.sweepScheduled = false;
+                this.sweepRetired();
+            });
+        }
+    }
+    sweepRetired() {
+        if (!this.retired.size) {
+            return;
+        }
+        const retired = this.retired;
+        this.retired = new Set();
+        this.dependencies = this.dependencies.filter(([, v]) => !retired.has(v));
         this.dirty = true;
         this.triggerDependencyUpdated();
     }
