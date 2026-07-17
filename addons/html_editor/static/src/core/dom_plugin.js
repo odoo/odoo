ad06dec46aec1711914f7db8b330e63bcaa4f4ae
Jinjiu Liu <jili@odoo.com>
2026-07-17 07:38:51 +0000

    [FIX] html_editor: avoid element splitting with invisible first child
    Before this commit: In some cases, an element may have a invisible `/n`
    first child from the html. When inserting content to it, the insert
    function doesn't check if the first child is visible, and then split the
    element wrongly.
    
    After this commit: we now check if the previous sibling of the current
    node is invisible, if so, we don't split the parent element.
    
    task-6352841
    
    X-original-commit: 7f4c5d90b7223c2ba7c91538379a91d14d720a82

--- addons/html_editor/static/src/core/dom_plugin.js
+++ addons/html_editor/static/src/core/dom_plugin.js
@@ -28,6 +28,7 @@ import {
     isEditorTab,
     isPhrasingContent,
     getDeepestEditablePosition,
+    isVisible,
 } from "../utils/dom_info";
 import {
     childNodes,
@@ -368,7 +369,10 @@ export class DomPlugin extends Plugin {
                     if (!insertBefore) {
                         offset += 1;
                     }
-                    if (offset) {
+                    if (
+                        (offset === 1 && !insertBefore) ||
+                        (offset && isVisible(currentNode?.previousSibling))
+                    ) {
                         const [left, right] = this.dependencies.split.splitElement(
                             currentNode.parentElement,
                             offset
