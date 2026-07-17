8d1661cc0249ca5913e75c4748ca0e149e70163c
Alex Kühn <aku@odoo.com>
2026-07-17 15:48:16 +0000

    [FIX] mail: no crash when loading chathub on logging in as diff user
    Before this commit, when logging in as Demo user with a direct chat,
    logging out + logging in with Mitchell Admin would crash with the
    following error:
    
    ```
    TypeError: can't access property "imStatusUI", this.channel.correspondent is undefined
    ```
    
    This happens because this attempts to restore the direct chat in chat
    window. Normally this shouldn't happen thanks to being unable to
    fetch inaccessible channels, however Mitchell Admin is the admin of
    DB thus can fetch the channel data. The crash comes from computation
    of `correspondent` that is `undefined` because it can't determine a
    correspondent from the Point of view of Mitchell Admin, and the
    template isn't defensive against missing a `correspondent` in a
    direct chat.
    
    One solution could have been to support opening of direct chat, but
    this implies fixing a lot of complex issues, such as:
    
    - Mitchell Admin can't use composer because this attempts to add a
    member but there's a contraint for direct chat to have at most 2
    members
    - This direct chat looks like a group chat without a name, we can
    guess this is a direct chat only because of broken avatar and the
    start message in message list stating this is a direct chat
    - Several other actions show weird behavior, like "Invite People"
    ignoring people in the chat
    
    This commit fixes the issue by ignoring the restore of chat in chat
    hub for direct chats that current user is not a member.
    
    X-original-commit: 7e12f0d371c80945766f3f7b83a8d8f4822f9617

--- addons/im_livechat/static/tests/mock_server/livechat_mock_server.js
+++ addons/im_livechat/static/tests/mock_server/livechat_mock_server.js
@@ -221,13 +221,35 @@ async function livechat_conversation_create_and_link_expertise(request) {
 
 patch(mailDataHelpers, {
     _process_request_for_all(store, name, params) {
-        const ResPartner = this.env["res.partner"];
+        const MailGuest = this.env["mail.guest"];
         const ResUsers = this.env["res.users"];
         super._process_request_for_all(...arguments);
         store.add_global_values({ livechat_available: true });
         if (name === "init_livechat") {
             if (this.env.user && !ResUsers._is_public(this.env.uid)) {
-                store.add(ResPartner.browse(this.env.user.partner_id), ["email"]);
+                store.add_global_values((res) =>
+                    res.one(
+                        "self_user",
+                        (res) =>
+                            res.one("partner_id", (res) => {
+                                res.from_method("_store_partner_fields");
+                                res.attr("email");
+                            }),
+                        { value: ResUsers.browse(this.env.user.id) }
+                    )
+                );
+            }
+            if (this.env.cookie.get("dgid")) {
+                store.add_global_values((res) =>
+                    res.one(
+                        "self_guest",
+                        (res) => {
+                            res.from_method("_store_avatar_fields");
+                            res.from_method("_store_im_status_fields");
+                        },
+                        { value: MailGuest.browse(this.env.cookie.get("dgid")) }
+                    )
+                );
             }
         }
     },
