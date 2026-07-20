3a44b1d98175a0c69facf6016f2500ec3e2dd8f9
Bayron Alpízar Quesada <bayalpiizar777@gmail.com>
2026-07-20 16:13:46 -0600

    feat(distribuidora_ventas): agregar acceso directo a lista de precios en menu de apps

--- addons/distribuidora_ventas/tests/__init__.py
+++ addons/distribuidora_ventas/tests/__init__.py
@@ -3,3 +3,4 @@ from . import test_customer_pricelist
 from . import test_invoice_from_order_quantity
 from . import test_sale_order_accepts_any_delivery_date
 from . import test_sale_order_form_hides_commitment_date
+from . import test_pricelist_menu
