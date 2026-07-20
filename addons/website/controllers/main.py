7f90e63ddb6070587643403e3adfaf37813cf55b
Zachary (zavan) <zavan@odoo.com>
2026-07-20 16:40:39 +0000

    [FIX] website: fix SEO image description change
    # How to reproduce
    - Go to a website page
    - Add atleast one image to the page
    - Go to Site > Optimize SEO
    - Add a description to an image
    - Save
    
    # The issue
    The description is not applied to the image. It is not present in the
    image's alt in the page and if we re-open the SEO modal, it is not there
    either.
    
    # Cause
    This [PR] changed the way the image's id are generated in
    `get_alt_image()`, but did not change the id generation in the check
    done in `update_alt_images()`, so this check always fails:
    https://github.com/odoo/odoo/blob/36eee85dbaed927ff76b2d44a44ba476e9588c72/addons/website/controllers/main.py#L894
    
    [PR]: https://github.com/odoo/odoo/commit/1f1980a1d9835567597ca239d404f47d4677dc47
    
    # Proposed solution
    Added an helper function for the id generation. This is not
    necessary but might prevent this from happening again, up to the
    reviewer to keep it or not
    
    opw-6389523
    
    X-original-commit: 0d9ea6dd2f976253c7288b2e11faf74d6cf53d49

--- addons/website/controllers/main.py
+++ addons/website/controllers/main.py
@@ -873,7 +873,7 @@ class Website(Home):
                         "updated": False,
                         "res_model": model['model'],
                         "res_id": model['id'],
-                        "id": f"{model['model']}-{model['id']}-{model['field']}-{index}",
+                        "id": self._get_image_id(model['model'], model['id'], model['field'], index),
                         "field": model.get('field'),
                     })
         return json.dumps(result)
@@ -890,7 +890,7 @@ class Website(Home):
             tree = html.fromstring(str(record[img['field']]))
             modified = False
             for index, element in enumerate(tree.xpath('//img')):
-                imgId = f"{img['res_model']}-{img['res_id']}-{index!s}"
+                imgId = self._get_image_id(img['res_model'], img['res_id'], img['field'], str(index))
                 if imgId == img['id']:
                     if (img['decorative']):
                         element.set('alt', '')
@@ -902,6 +902,10 @@ class Website(Home):
             if modified:
                 new_html_content = html.tostring(tree, encoding='unicode', method='html')
                 record.write({img['field']: new_html_content})
+
+    @staticmethod
+    def _get_image_id(model, model_id, field, index):
+        return f"{model}-{model_id}-{field}-{index}"
 
     @http.route(['/website/update_broken_links'], type='jsonrpc', auth="user", website=True)
     def update_broken_links(self, links):
