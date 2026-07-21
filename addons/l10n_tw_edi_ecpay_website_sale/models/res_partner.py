100efdb793c4497e097ed71e554ca2f43bb03b31
kcv-odoo <kcv@odoo.com>
2026-07-21 16:21:26 +0530

    [FIX] l10n_tw_edi_ecpay_website_sale: fix address updation issue
    Before this commit TW localisation was adding `l10n_tw_edi_require_paper_format`
    field value for all the pages but it only needed for checkout, also was
    updating order's `l10n_tw_edi_is_print` on each address update and allowing
    write on parent company from child address which might cause some issues.
    
    This commit improve the way TW handle address management creating/updating
    on customer's company and only update `l10n_tw_edi_is_print` when user
    create/update partner or partner's details on order instead when updating
    address details.

--- addons/l10n_tw_edi_ecpay_website_sale/models/res_partner.py
+++ addons/l10n_tw_edi_ecpay_website_sale/models/res_partner.py
@@ -9,6 +9,12 @@ class ResPartner(models.Model):
         help="If checked, the partner requires paper format for ECPay e-invoices.",
     )
 
+    def _get_frontend_writable_fields(self):
+        frontend_writable_fields = super()._get_frontend_writable_fields()
+        frontend_writable_fields.add("l10n_tw_edi_require_paper_format")
+
+        return frontend_writable_fields
+
     @api.model
     def _commercial_fields(self):
         return super()._commercial_fields() + ['l10n_tw_edi_require_paper_format']
--- addons/l10n_tw_edi_ecpay_website_sale/models/sale_order.py
+++ addons/l10n_tw_edi_ecpay_website_sale/models/sale_order.py
@@ -21,6 +21,12 @@ class SaleOrder(models.Model):
     l10n_tw_edi_carrier_number = fields.Char(string="Carrier Number")
     l10n_tw_edi_carrier_number_2 = fields.Char(string="Carrier Number 2")
 
+    # TODO master: Make it compute based on partner_id of the order
+    def _update_address(self, partner_id, fnames=None):
+        """Override of `website_sale` to update l10n_tw_edi_is_print of customer."""
+        super()._update_address(partner_id, fnames=fnames)
+        self.l10n_tw_edi_is_print = self.partner_id.commercial_partner_id.l10n_tw_edi_require_paper_format
+
     def _prepare_invoice(self):
         res = super()._prepare_invoice()
         if self.company_id.account_fiscal_country_id.code == 'TW' and self.company_id._is_ecpay_enabled():
