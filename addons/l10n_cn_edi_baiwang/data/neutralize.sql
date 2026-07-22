2b3bca6266a54d5fac52e31cc78797a79fd61c72
erle <erle@odoo.com>
2026-07-22 11:40:00 +0800

    [ADD] l10n_cn_edi_baiwang: add database neutralization script
    Add data/neutralize.sql to reset Baiwang subscription tokens and force
    demo/test modes when duplicating databases to staging environments.
    
    Clean up redundant field declaration in the move reversal view.

---
+++ addons/l10n_cn_edi_baiwang/data/neutralize.sql
@@ -0,0 +1,10 @@
+UPDATE account_edi_proxy_client_user
+   SET edi_mode = 'demo'
+ WHERE proxy_type = 'l10n_cn_edi_baiwang';
+
+UPDATE res_company
+   SET l10n_cn_edi_mode = 'test',
+       l10n_cn_baiwang_subscription_status = 'not_subscribed',
+       l10n_cn_baiwang_org_auth_code = NULL,
+       l10n_cn_baiwang_cached_token = NULL,
+       l10n_cn_baiwang_refresh_token = NULL;
