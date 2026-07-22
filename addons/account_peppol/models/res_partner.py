133f6a1a6f7588d45ea92bf639e1983b78094a9f
Maximilien <malb@odoo.com>
2026-07-22 14:48:00 +0200

    [IMP] Onboarding DONT LOOK AT THIS

--- addons/account_peppol/models/res_partner.py
+++ addons/account_peppol/models/res_partner.py
@@ -231,15 +231,11 @@ class ResPartner(models.Model):
         return res
 
     def _compute_peppol_endpoint(self):
-        # Don't recompute on partners corresponding to registered companies
-        partners_not_to_recompute = self._get_partners_to_skip_peppol_computation()
-        partners_to_recompute = self.browse([partner.id for partner in self if partner._origin not in partners_not_to_recompute])
+        partners_to_recompute = self.browse([partner.id for partner in self if partner._origin not in self])
         super(ResPartner, partners_to_recompute)._compute_peppol_endpoint()
 
     def _compute_peppol_eas(self):
-        # Don't recompute on partners corresponding to registered companies
-        partners_not_to_recompute = self._get_partners_to_skip_peppol_computation()
-        partners_to_recompute = self.browse([partner.id for partner in self if partner._origin not in partners_not_to_recompute])
+        partners_to_recompute = self.browse([partner.id for partner in self if partner._origin not in self])
         super(ResPartner, partners_to_recompute)._compute_peppol_eas()
 
     # -------------------------------------------------------------------------
@@ -300,11 +296,6 @@ class ResPartner(models.Model):
         frontend_writable_fields.update({'peppol_eas', 'peppol_endpoint'})
 
         return frontend_writable_fields
-
-    def _get_partners_to_skip_peppol_computation(self):
-        return self.env['res.company'].search([
-            ('account_peppol_proxy_state', 'in', self.env['account_edi_proxy_client.user']._get_can_send_domain()),
-        ]).mapped('partner_id')
 
     @api.model
     def _get_peppol_proxy_identification_info(self, peppol_eas, peppol_endpoint):
