1868fc152ffa6c342d4036ed35d21121a5ef39cb
usseif <yoahm@odoo.com>
2026-07-22 13:08:44 +0200

    [FIX] hr_holidays: Fix leave time selection traceback
    . Add safe execution to the element before use focus()
    
    task-6409715

--- addons/hr_holidays/static/src/components/float_time_selection/float_time_selection.js
+++ addons/hr_holidays/static/src/components/float_time_selection/float_time_selection.js
@@ -48,7 +48,7 @@ export class FloatTimeSelectionField extends FloatTimeField {
             onTimeChange: this.onTimeChange.bind(this),
         });
         setTimeout(() => {
-            this.inputFloatTimeRef.el.focus(); // focus on the input rather than the popover
+            this.inputFloatTimeRef.el?.focus(); // focus on the input rather than the popover
         }, 0);
     }
 
