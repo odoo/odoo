f9b794bed80f32df543644fdc337577c0d584939
dijo-odoo <dijo@odoo.com>
2026-07-22 06:05:29 +0000

    [IMP] l10n_it: improve invoice xml import
    with this commit :-
      - We remove the regex check on the imported file name.
      - The behavior before was checking filename to fill data to invoice.
      - Which should not constraint, so we can ignore the filename
    
    task-6152773
    
    closes odoo/odoo#284973
    
    X-original-commit: d1759c9e6147cc18b7f7f589e669e7afddb58ad6
    Signed-off-by: Paolo Gatti (pgi) <pgi@odoo.com>

--- addons/l10n_it_edi/models/ir_attachment.py
+++ addons/l10n_it_edi/models/ir_attachment.py
@@ -2,10 +2,10 @@
 
 from odoo import api, models
 from odoo.addons.l10n_it_edi.tools.remove_signature import remove_signature
+from odoo.tools.mimetypes import guess_mimetype
 
 from lxml import etree
 import logging
-import re
 
 _logger = logging.getLogger(__name__)
 
@@ -15,11 +15,8 @@ FATTURAPA_FILENAME_RE = "[A-Z]{2}[A-Za-z0-9]{2,28}_[A-Za-z0-9]{0,5}.((?i:xml.p7m
 class IrAttachment(models.Model):
     _inherit = 'ir.attachment'
 
-    def _decode_edi_l10n_it_edi(self, name, content):
-        """ Decodes a  into a list of one dictionary representing an attachment.
-            :returns:           A list with a dictionary.
-        """
-        def parse_xml(parser, name, content):
+    def _parse_xml_with_recovery(self, content, name=None):
+        def parse_xml(parser, content):
             try:
                 return etree.fromstring(content, parser)
             except (etree.ParseError, ValueError, TypeError) as e:
@@ -27,30 +24,54 @@ class IrAttachment(models.Model):
                 _logger.info("XML parsing of %s failed: %s", name, e)
 
         parser = etree.XMLParser(recover=True, resolve_entities=False)
-        if (xml_tree := parse_xml(parser, name, content)) is None:
-            # The file may have a Cades signature, trying to remove it
-            if (xml_tree := parse_xml(parser, name, remove_signature(content))) is None:
-                _logger.info("Italian EDI invoice file %s cannot be decoded.", name)
-                return []
+        xml_tree = parse_xml(parser, content)
+
+        if xml_tree is None:
+            cleaned = remove_signature(content)
+            if cleaned:
+                xml_tree = parse_xml(parser, cleaned)
+
+        return xml_tree
+
+    def _decode_edi_l10n_it_edi(self, name, content):
+        """ Tries to decode the content of the file being imported
+            into a list of one dictionary representing an attachment.
+            :returns:           A list with a dictionary.
+        """
+        xml_tree = self._parse_xml_with_recovery(content, name)
+
+        if xml_tree is None or not etree.QName(xml_tree).localname.startswith('FatturaElettronica'):
+            _logger.info("Italian EDI invoice file %s cannot be decoded.", name)
+            return []
 
         return [{
             'filename': name,
             'content': content,
             'attachment': self,
-            'xml_tree': xml_move_tree,
+            'xml_tree': xml_tree,
             'type': 'l10n_it_edi',
             'sort_weight': 11,
-        } for xml_move_tree in xml_tree.xpath('//FatturaElettronicaBody')]
+        }]
 
     def _is_l10n_it_edi_import_file(self):
-        is_xml = (
-            self.name.endswith('.xml')
-            or self.mimetype.endswith('/xml')
-            or 'text/plain' in self.mimetype
-            and self.raw
-            and self.raw.startswith(b'<?xml'))
-        is_p7m = self.mimetype == 'application/pkcs7-mime'
-        return (is_xml or is_p7m) and re.search(FATTURAPA_FILENAME_RE, self.name)
+        """ Determine whether the attachment contains a supported Italian EDI XML or P7M file."""
+
+        if self.raw and (
+            self.mimetype.endswith('/xml')
+            or 'application/pkcs7-mime' in self.mimetype
+            or (
+                'text/plain' in self.mimetype
+                and (
+                    self.name.lower().endswith(('.xml', '.p7m'))
+                    or guess_mimetype(self.raw or b'').endswith('/xml')
+                )
+            )
+        ):
+            xml_tree = self._parse_xml_with_recovery(self.raw, self.name)
+            if xml_tree is not None:
+                return etree.QName(xml_tree).localname.startswith('FatturaElettronica')
+
+        return False
 
     @api.model
     def _get_edi_supported_formats(self):
