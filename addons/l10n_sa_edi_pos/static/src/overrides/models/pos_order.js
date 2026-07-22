5e932b1779c642f7ed9d96b7fa992246d1d26d02
Hadi El Yakhni <hael@odoo.com>
2026-07-22 14:32:36 +0000

    [FIX] l10n_sa_pos, l10n_sa_edi_pos: bigger ZATCA QR code on receipts
    Steps to reproduce
    ------------------
    1. install l10n_sa_edi and l10n_sa_pos
    2. onboard the company for ZATCA and link a printer to the PoS
    3. make a PoS order with a customer and print the receipt
    
    -> the ZATCA QR code is too small to be scanned.
    
    Why it's happening
    ------------------
    The phase 2 QR code is big because it also contains the invoice hash,
    signature and public key. We render it at 200 px and show it at half the
    receipt width, which is too small to scan a QR with that much data.
    
    The fix
    -------
    Render it at 400 px and show it at 75% of the receipt width, so the
    modules are big enough to scan.
    
    opw-6399766
    
    closes odoo/odoo#278988
    
    X-original-commit: d4a05f7c12be1bc1b46560ac156eb87393a824ce
    Signed-off-by: Ali Alfie (alal) <alal@odoo.com>
    Signed-off-by: Hadi El Yakhni (hael) <hael@odoo.com>

--- addons/l10n_sa_edi_pos/static/src/overrides/models/pos_order.js
+++ addons/l10n_sa_edi_pos/static/src/overrides/models/pos_order.js
@@ -58,7 +58,7 @@ patch(PosOrder.prototype, {
 
     generateQrcode() {
         if (!this.notLegal && this.isSACompany()) {
-            return qrCodeSrc(this.l10n_sa_invoice_qr_code_str);
+            return qrCodeSrc(this.l10n_sa_invoice_qr_code_str, { size: 400 });
         }
         return false;
     },
