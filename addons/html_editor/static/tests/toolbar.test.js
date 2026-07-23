62b0c860853310a9f51621adfd7e0173acc81f58
adch-odoo <adch@odoo.com>
2026-07-23 09:39:23 +0000

    [FIX] html_editor: do not open toolbar between double and triple click
    Purpose of this commit:
    
    - On double click, opening the toolbar is delayed by 300ms to prevent
    flickering before a potential triple click.
    
    - However, mouseup was re-enabling selection tracking
    (onSelectionChangeActive = true) before the 300ms delay finished.
    Because browser selectionchange events are dispatched asynchronously
    after mouseup, they triggered updateToolbar() immediately,
    bypassing the 300ms delay.
    
    - This fix re-enables selection tracking only after the 300ms debounced update
    actually finishes.
    
    runbot-941543
    
    closes odoo/odoo#279770
    
    X-original-commit: a5be9ab0134eb008bab10222d30522ff8546db25
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Adnan Chaudhary (adch) <adch@odoo.com>

--- addons/html_editor/static/tests/toolbar.test.js
+++ addons/html_editor/static/tests/toolbar.test.js
@@ -1696,7 +1696,8 @@ describe("toolbar open and close on user interaction", () => {
             manuallyDispatchProgrammaticEvent(document, "selectionchange");
             await tick();
 
-            await waitFor(".o-we-toolbar");
+            // wait for the debounced toolbar update
+            await advanceTime(500);
             await expectElementCount(".o-we-toolbar", 1);
         });
 
