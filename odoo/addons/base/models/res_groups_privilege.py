fcb2a2929a262be617a870bd1bd9236e2149f8f0
Nicolas Lempereur <nle@odoo.com>
2026-07-23 13:15:59 +0200

    [FIX] base: res.groups.privilege placeholder translatable
    Issue: res.groups.privilege placeholder and description field are not
    translatable so they always appear in english (eg. you always see "No"
    as option for "Dashboard" privilege).
    
    Fix: make them translatable.
    
    opw-6399810
    
    closes odoo/odoo#278044
    
    Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>

--- odoo/addons/base/models/res_groups_privilege.py
+++ odoo/addons/base/models/res_groups_privilege.py
@@ -7,8 +7,9 @@ class ResGroupsPrivilege(models.Model):
     _order = 'sequence, name, id'
 
     name = fields.Char(string='Name', required=True, translate=True)
-    description = fields.Text(string='Description')
-    placeholder = fields.Char(string='No group label', default="No", help="Label displayed when the user does not belong of any group of that scope.")
+    description = fields.Text(string='Description', translate=True)
+    placeholder = fields.Char(string='No group label', default=lambda self: self.env._('No'), translate=True,
+        help="Label displayed when the user does not belong of any group of that scope.")
     sequence = fields.Integer(string='Sequence', default=100)
     category_id = fields.Many2one('ir.module.category', string='Category', index=True)
     group_ids = fields.One2many('res.groups', 'privilege_id', string='Groups')
