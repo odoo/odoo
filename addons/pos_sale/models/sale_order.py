0df9dac9f623a8b844c1bbd650a83642e9f9fa62
Majed Alhanash (malh) <malh@odoo.com>
2026-07-23 13:06:40 +0400

    [FIX] pos_sale: fix sale order invoicing after down payment refund
    The following commit resets qty_invoiced to zero on sale order lines
    paid by a POS order when that order is refunded.
    https://github.com/odoo/odoo/commit/ac39aa4f68dfc77011c39e468e3f60e0338a3c69
    
    However, it does not handle the sale order line created for a refunded
    POS down payment. That line keeps `qty_invoiced` = -1, which causes the
    refunded amount to be included again when settling or invoicing the
    sale order.
    
    Steps to reproduce:
    - Create a sale order.
    - Pay a down payment through the POS.
    - Refund the down payment order from the POS.
    - Settle the remaining amount from the POS or invoice the sale order
      from the backend.
    
    Result:
    - The generated invoice includes the sale order total plus the refunded
      down payment.
    - Sale order `amount_invoiced` will be the down payment amount.
    
    Fix:
    - Delete the refunded downpayment to match the sale flow.
    - Include refunded down payments in the amount_invoiced computation.
    
    opw-6378891
    
    closes odoo/odoo#278011
    
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- addons/pos_sale/models/sale_order.py
+++ addons/pos_sale/models/sale_order.py
@@ -72,7 +72,9 @@ class SaleOrder(models.Model):
             if order.invoice_status == 'invoiced':
                 continue
             # We need to account for the downpayment paid in POS with and without invoice
-            order_amount = sum(order.sudo().pos_order_line_ids.filtered(lambda pol: pol.order_id.state in ['paid', 'done', 'invoiced'] and pol.sale_order_line_id.is_downpayment).mapped('price_subtotal_incl'))
+            order_lines = order.sudo().pos_order_line_ids.filtered(lambda pol: pol.sale_order_line_id.is_downpayment)
+            pos_lines = order_lines | order_lines.refund_orderline_ids
+            order_amount = sum(pos_lines.filtered(lambda pol: pol.order_id.state in ['paid', 'done', 'invoiced']).mapped('price_subtotal_incl'))
             order.amount_invoiced += order_amount
 
 
