aebf1720c2c0ed537b6403c26460a5379acf09ce
Mark Orban (maorb) <maorb@odoo.com>
2026-07-23 15:27:20 +0000

    [FIX] website, *: disable optimize seo from link tracker page
    *: website_links
    
    Since [this commit][1] you're able to optimize the link tracker page
    using "optimize seo." This makes no sense as it contains no useful
    content for visitors to the website. Access to the action is now
    disabled when the current page is the link tracking page. The page
    properties and link tracker menu items have also been removed for
    similar reasons.
    
    [1]: https://github.com/odoo/odoo/commit/ac55f2bb113ecf7c774fe6e96d28e716184a97d1
    
    Task-6288891
    
    closes odoo/odoo#284215
    
    X-original-commit: f9d53aeaa9a5442587492549bc6f1c4314b4ddd0
    Signed-off-by: Colin Louis (loco) <loco@odoo.com>
    Signed-off-by: Mark Orban (maorb) <maorb@odoo.com>

--- addons/website_links/static/src/services/website_custom_menus.js
+++ addons/website_links/static/src/services/website_custom_menus.js
@@ -1,6 +1,8 @@
-import { registry } from '@web/core/registry';
+import { registry } from "@web/core/registry";
 
-registry.category('website_custom_menus').add('website_links.menu_link_tracker', {
+registry.category("website_custom_menus").add("website_links.menu_link_tracker", {
     openWidget: (services) => services.website.goToWebsite({ path: `/r?u=${encodeURIComponent(services.website.contentWindow.location.href)}` }),
-    isDisplayed: (env) => env.services.website.currentWebsite && env.services.website.contentWindow,
+    isDisplayed: (env) => env.services.website.currentWebsite && env.services.website.contentWindow && window.location.pathname !== "/r",
 });
+registry.category("website.should_display_seo").add("website_links.seo_test_fn", () => window.location.pathname !== "/r");
+registry.category("website.should_display_page_properties").add("website_links.page_properties_test_fn", () => window.location.pathname !== "/r");
