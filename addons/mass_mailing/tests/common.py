3594e54df40f30c3df97d30246e7f35423172596
Thibault Delavallée <tde@odoo.com>
2026-07-23 08:34:13 +0000

    [FIX] mass_mailing: fix mail.mail gc
    Mail GC currently deletes inner mail.message, while it should actually
    remove mail.mail records. Removing messages is managed at mail.mail
    unlink level using the 'is_notification' field. Indeed a mass_mailing
    email should remove the linked mail.message, while a canceled
    notification email should not remove the chatter message.
    
    Also do not crash comparing strings to integer, do not forget to
    cast the value to int.
    
    Followup of odoo/odoo#209494
    
    Task-6412451
    
    X-original-commit: 370edfe9858b6f64d1299080ef9006c8e0ba77ff

--- addons/mass_mailing/tests/common.py
+++ addons/mass_mailing/tests/common.py
@@ -85,6 +85,7 @@ class MassMailCase(MailCase, MockLinkTracker):
         """
         # map trace state to email state
         state_mapping = {
+            'outgoing': 'outgoing',
             'sent': 'sent',
             'open': 'sent',  # opened implies something has been sent
             'reply': 'sent',  # replied implies something has been sent
