dfabf8195e9a1c5c686f7031dbae59abae1b69b2
Khumam Alzagim <alkh@odoo.com>
2026-07-23 18:00:27 +0400

    [IMP] l10n_pk: general code cleanup
    Cleanup manifest and module code, no functional change.
    
    task-5951560

--- addons/l10n_pk/demo/account_demo.py
+++ addons/l10n_pk/demo/account_demo.py
@@ -1,8 +1,8 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from dateutil.relativedelta import relativedelta
 
-from odoo import fields, models, Command
+from odoo import Command, fields, models
+
 from odoo.addons.account.models.chart_template import template
 
 
--- addons/l10n_pk/migrations/1.1/end-migrate_update_taxes.py
+++ addons/l10n_pk/migrations/1.1/end-migrate_update_taxes.py
@@ -1,5 +1,4 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import api, SUPERUSER_ID
+from odoo import SUPERUSER_ID, api
 
 
 def migrate(cr, version):
