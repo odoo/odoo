268df22d1db0ba45bdea4aa02cb60965aeab5b98
Hesham Saleh <hsal@odoo.com>
2026-07-23 15:40:36 +0200

    [FIX] account: Fix Audit Trail
    Because of this PR https://github.com/odoo/odoo/pull/235719
    Audit Trail no longer functions as expected:
    1) Tracking messages no longer show in the Audit Trail
    2) Description is missing from Audit Trail
    
    This commit fixes these issues.
    
    task-6413110
    
    closes odoo/odoo#278099
    
    Signed-off-by: Quentin De Paoli (qdp) <qdp@odoo.com>

---
+++ addons/account/static/src/views/audit_trail/audit_trail_body_field.js
@@ -0,0 +1,19 @@
+import { Component } from "@odoo/owl";
+import { registry } from "@web/core/registry";
+import { standardFieldProps } from "@web/views/fields/standard_field_props";
+
+export class AuditTrailBodyField extends Component {
+    static template = "account.AuditTrailBodyField";
+    static props = {
+        ...standardFieldProps,
+    };
+
+    get isTracking() {
+        return this.props.record.data.message_type === "tracking";
+    }
+}
+
+registry.category("fields").add("audit_trail_body_field", {
+    component: AuditTrailBodyField,
+    fieldDependencies: [{ name: "message_type", type: "selection" }],
+});
