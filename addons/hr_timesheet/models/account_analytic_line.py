a2c208253d3db466b78b02ab9a86cf43ce58ffbf
Robin Engels (roen) <roen@odoo.com>
2026-07-24 10:29:59 +0000

    [FIX] hr_timesheet: sync project and task
    When modifying project_id on a timesheet through mass edit/rpc or
    anything that is not triggering `onChange`. The task_id would not be
    reset if it doesnt' belong to the new project set on the timesheet.
    
    Steps to reproduce:
    -------------------
    * Install studio for easier reproducing of the issue
    * Open the timesheet list view
    * Open studio and activate the mass edit on the view
    * Modify the project_id on multiple records
    > Observation: The task_id stays the same even if they do not belong to
      the new set project
    
    Why the fix:
    ------------
    Instead of relying only on the onChange we add an inverse to the
    project_id that will reset the task when needed.
    
    opw-6259149
    
    closes odoo/odoo#284451
    
    X-original-commit: 0eb94ef03ab70b595fed3a675670d1b0b3c8fb3e
    Related: odoo/enterprise#129166
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>
    Signed-off-by: Robin Engels (roen) <roen@odoo.com>

--- addons/hr_timesheet/models/account_analytic_line.py
+++ addons/hr_timesheet/models/account_analytic_line.py
@@ -63,7 +63,7 @@ class AccountAnalyticLine(models.Model):
     parent_task_id = fields.Many2one('project.task', related='task_id.parent_id', store=True, index='btree_not_null')
     project_id = fields.Many2one(
         'project.project', 'Project', domain=_domain_project_id, index=True,
-        compute='_compute_project_id', store=True, readonly=False)
+        compute='_compute_project_id', inverse='_inverse_project_id', store=True, readonly=False)
     user_id = fields.Many2one(compute='_compute_user_id', store=True, readonly=False)
     employee_id = fields.Many2one('hr.employee', "Employee", domain=_domain_employee_id, context={'active_test': False},
         index=True, help="Define an 'hourly cost' on the employee to track the cost of their time.")
@@ -122,6 +122,11 @@ class AccountAnalyticLine(models.Model):
             readonly_timesheets = self.filtered(lambda timesheet: timesheet._is_readonly())
             readonly_timesheets.readonly_timesheet = True
             (self - readonly_timesheets).readonly_timesheet = False
+
+    def _inverse_project_id(self):
+        for line in self:
+            if line.task_id.project_id != line.project_id:
+                line.sudo().task_id = False
 
     def _compute_encoding_uom_id(self):
         for analytic_line in self:
