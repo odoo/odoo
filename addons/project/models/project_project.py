771814418fa0f352583ea262197bce85240782d2
Mahdi Alijani (malj) <malj@odoo.com>
2026-07-27 13:44:31 +0000

    [FIX] project: prevent traceback on portal subscribe task
    Portal subscribe task can create tb because the task was archived
    before unsubscribing.
    
    opw-6397850
    
    X-original-commit: 495c1cd1cd3e0cbde0033a0e9ef2d3273e0f0ac6

--- addons/project/models/project_project.py
+++ addons/project/models/project_project.py
@@ -1230,9 +1230,9 @@ class ProjectProject(models.Model):
             elif project.privacy_visibility in ['invited_users', 'portal']:
                 portal_users = project.message_partner_ids.user_ids.filtered('share')
                 project.message_unsubscribe(partner_ids=portal_users.partner_id.ids)
-                project.tasks._unsubscribe_portal_users()
+                project.with_context(active_test=False).tasks._unsubscribe_portal_users()
                 # revoke access_token since the project and its tasks are no longer accessible for portal/public users
-                project.tasks.access_token = ''
+                project.with_context(active_test=False).tasks.access_token = ''
                 project.access_token = ''
 
     # ---------------------------------------------------
