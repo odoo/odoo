6e98e530fb37a2768fd64f9ab786595bdb3c9ef9
Anton Romanova (roan) <roan@odoo.com>
2026-07-27 13:30:14 +0000

    [IMP] l10n_fr_pdp: self-service migrations to odoo
    closes odoo/odoo#279902
    
    X-original-commit: ed8b673ac70e72a8e3ab9229dc29a23ab040ada5
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>
    Signed-off-by: Anton Romanova (roan) <roan@odoo.com>

--- addons/l10n_fr_pdp/models/account_edi_proxy_user.py
+++ addons/l10n_fr_pdp/models/account_edi_proxy_user.py
@@ -167,6 +167,7 @@ class AccountEdiProxyClientUser(models.Model):
                     'peppol_identifier': peppol_identifier,
                     'public_key': private_key_sudo._get_public_key_bytes(encoding='pem').decode(),
                     'auth_url_hash': company.pdp_authentication_uuid,
+                    'mobility_registration': True,
                 })
             except AccountEdiProxyError as e:
                 raise UserError(e.message)
