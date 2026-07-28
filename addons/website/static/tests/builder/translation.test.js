dzdzdzdz1010/OdooStandard

move some test for the save mecanism of website to website module

addons/website/static/tests/builder/translation.test.js
@@ -1,6 +1,11 @@
 import { EditWebsiteSystrayItem } from "@website/client_actions/website_preview/edit_website_systray_item";
 import { setContent, setSelection } from "@html_editor/../tests/_helpers/selection";
-import { insertText, pasteHtml, pasteText } from "@html_editor/../tests/_helpers/user_actions";
+import {
+    insertText,
+    pasteHtml,
+    pasteText,
+    undo,
+} from "@html_editor/../tests/_helpers/user_actions";
 import { beforeEach, delay, describe, expect, globals, press, test } from "@odoo/hoot";
 import {
     animationFrame,
@@ -507,3 +512,74 @@ function getTranslateEditable({
             </div>
         </main>`;
 }
+
+describe("replicated translations", () => {
+    test("should not add dirty marks on the ones receiving the replicated changes", async () => {
+        const { getEditor } = await setupSidebarBuilderForTranslation({
+            websiteContent: `
+                <div class="test-1">
+                    <span data-oe-model="ir.ui.view" data-oe-id="600" data-oe-field="arch_db" data-oe-translation-state="translated" data-oe-translation-source-sha="4242">Contactez-nous</span>
+                </div>
+                <div class="test-2">
+                    <span data-oe-model="ir.ui.view" data-oe-id="600" data-oe-field="arch_db" data-oe-translation-state="translated" data-oe-translation-source-sha="4242">Contactez-nous</span>
+                </div>
+                <div class="test-3">
+                    <span data-oe-model="ir.ui.view" data-oe-id="600" data-oe-field="arch_db" data-oe-translation-state="translated" data-oe-translation-source-sha="4242">Contactez-nous</span>
+                </div>
+            `,
+        });
+        const span1 = queryOne(":iframe .test-1 span");
+        const span2 = queryOne(":iframe .test-2 span");
+        const span3 = queryOne(":iframe .test-3 span");
+
+        const editor = getEditor();
+        span2.append(" ici");
+        editor.shared.history.addStep();
+        expect(span1).not.toHaveClass("o_dirty");
+        expect(span2).toHaveClass("o_dirty");
+        expect(span3).not.toHaveClass("o_dirty");
+        expect([span1, span2, span3]).toHaveText("Contactez-nous ici");
+
+        span1.append("!");
+        editor.shared.history.addStep();
+        expect(span1).toHaveClass("o_dirty");
+        expect(span2).toHaveClass("o_dirty");
+        expect(span3).not.toHaveClass("o_dirty");
+        expect([span1, span2, span3]).toHaveText("Contactez-nous ici!");
+
+        undo(editor);
+        expect(span1).not.toHaveClass("o_dirty");
+        expect(span2).toHaveClass("o_dirty");
+        expect(span3).not.toHaveClass("o_dirty");
+        expect([span1, span2, span3]).toHaveText("Contactez-nous ici");
+    });
+
+    test("changing several occurences at the same time should converge to the same value", async () => {
+        const { getEditor } = await setupSidebarBuilderForTranslation({
+            websiteContent: `
+                <div class="test-1">
+                    <span data-oe-model="ir.ui.view" data-oe-id="600" data-oe-field="arch_db" data-oe-translation-state="translated" data-oe-translation-source-sha="4242">Contactez-nous</span>
+                </div>
+                <div class="test-2">
+                    <span data-oe-model="ir.ui.view" data-oe-id="600" data-oe-field="arch_db" data-oe-translation-state="translated" data-oe-translation-source-sha="4242">Contactez-nous</span>
+                </div>
+                <div class="test-3">
+                    <span data-oe-model="ir.ui.view" data-oe-id="600" data-oe-field="arch_db" data-oe-translation-state="translated" data-oe-translation-source-sha="4242">Contactez-nous</span>
+                </div>
+            `,
+            translateMode: true,
+        });
+        const span1 = queryOne(":iframe .test-1 span");
+        const span2 = queryOne(":iframe .test-2 span");
+        const span3 = queryOne(":iframe .test-3 span");
+
+        span2.append(" ici");
+        span1.append("!");
+        const editor = getEditor();
+        editor.shared.history.addStep();
+        expect(span1).toHaveClass("o_dirty");
+        expect(span2).toHaveClass("o_dirty");
+        expect(span3).not.toHaveClass("o_dirty");
+        expect([span2, span3]).toHaveText(span1.textContent); // all the same text
+    });
+});

odoo/odoo

addons/website/static/tests/builder/translation.test.js
@@ -1,6 +1,11 @@
 import { EditWebsiteSystrayItem } from "@website/client_actions/website_preview/edit_website_systray_item";
 import { setContent, setSelection } from "@html_editor/../tests/_helpers/selection";
-import { insertText, pasteHtml, pasteText } from "@html_editor/../tests/_helpers/user_actions";
+import {
+    insertText,
+    pasteHtml,
+    pasteText,
+    undo,
+} from "@html_editor/../tests/_helpers/user_actions";
 import { beforeEach, delay, describe, expect, globals, press, test } from "@odoo/hoot";
 import { animationFrame, manuallyDispatchProgrammaticEvent, queryOne } from "@odoo/hoot-dom";
 import { contains, mockService, onRpc, patchWithCleanup } from "@web/../tests/web_test_helpers";
@@ -1020,5 +1025,76 @@ describe("translate images", () => {
         );
         expect(":iframe img.o_modified_image_to_save").toHaveAttribute("data-gl-filter", "inkwell");
         expect(":iframe img.o_modified_image_to_save").toHaveAttribute("data-resize-width", "1234");
+    });
+});
+
+describe("replicated translations", () => {
+    test("should not add dirty marks on the ones receiving the replicated changes", async () => {
+        const { getEditor } = await setupSidebarBuilderForTranslation({
+            websiteContent: `
+                <div class="test-1">
+                    <span data-oe-model="ir.ui.view" data-oe-id="600" data-oe-field="arch_db" data-oe-translation-state="translated" data-oe-translation-source-sha="4242">Contactez-nous</span>
+                </div>
+                <div class="test-2">
+                    <span data-oe-model="ir.ui.view" data-oe-id="600" data-oe-field="arch_db" data-oe-translation-state="translated" data-oe-translation-source-sha="4242">Contactez-nous</span>
+                </div>
+                <div class="test-3">
+                    <span data-oe-model="ir.ui.view" data-oe-id="600" data-oe-field="arch_db" data-oe-translation-state="translated" data-oe-translation-source-sha="4242">Contactez-nous</span>
+                </div>
+            `,
+        });
+        const span1 = queryOne(":iframe .test-1 span");
+        const span2 = queryOne(":iframe .test-2 span");
+        const span3 = queryOne(":iframe .test-3 span");
+
+        const editor = getEditor();
+        span2.append(" ici");
+        editor.shared.history.commit();
+        expect(span1).not.toHaveClass("o_dirty");
+        expect(span2).toHaveClass("o_dirty");
+        expect(span3).not.toHaveClass("o_dirty");
+        expect([span1, span2, span3]).toHaveText("Contactez-nous ici");
+
+        span1.append("!");
+        editor.shared.history.commit();
+        expect(span1).toHaveClass("o_dirty");
+        expect(span2).toHaveClass("o_dirty");
+        expect(span3).not.toHaveClass("o_dirty");
+        expect([span1, span2, span3]).toHaveText("Contactez-nous ici!");
+
+        undo(editor);
+        expect(span1).not.toHaveClass("o_dirty");
+        expect(span2).toHaveClass("o_dirty");
+        expect(span3).not.toHaveClass("o_dirty");
+        expect([span1, span2, span3]).toHaveText("Contactez-nous ici");
+    });
+
+    test("changing several occurences at the same time should converge to the same value", async () => {
+        const { getEditor } = await setupSidebarBuilderForTranslation({
+            websiteContent: `
+                <div class="test-1">
+                    <span data-oe-model="ir.ui.view" data-oe-id="600" data-oe-field="arch_db" data-oe-translation-state="translated" data-oe-translation-source-sha="4242">Contactez-nous</span>
+                </div>
+                <div class="test-2">
+                    <span data-oe-model="ir.ui.view" data-oe-id="600" data-oe-field="arch_db" data-oe-translation-state="translated" data-oe-translation-source-sha="4242">Contactez-nous</span>
+                </div>
+                <div class="test-3">
+                    <span data-oe-model="ir.ui.view" data-oe-id="600" data-oe-field="arch_db" data-oe-translation-state="translated" data-oe-translation-source-sha="4242">Contactez-nous</span>
+                </div>
+            `,
+            translateMode: true,
+        });
+        const span1 = queryOne(":iframe .test-1 span");
+        const span2 = queryOne(":iframe .test-2 span");
+        const span3 = queryOne(":iframe .test-3 span");
+
+        span2.append(" ici");
+        span1.append("!");
+        const editor = getEditor();
+        editor.shared.history.commit();
+        expect(span1).toHaveClass("o_dirty");
+        expect(span2).toHaveClass("o_dirty");
+        expect(span3).not.toHaveClass("o_dirty");
+        expect([span2, span3]).toHaveText(span1.textContent); // all the same text
     });
 });