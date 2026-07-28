111230ec879c86a7ffafa7b1375b316d0a3fe588
Benoit Socias (bso) <bso@odoo.com>
2026-07-28 10:01:25 +0000

    [FIX] html_editor: make sure pending mocked request does not complete
    The test that verifies the behavior of cancelling the link popover
    during an attachment upload relies on a hard-coded delay. Occasionally,
    when the test runs on an overloaded runbot infrastructure, the upload
    manages to complete before the discard happens.
    
    This commit fixes this by making sure the upload never completes within
    the test.
    
    runbot-940190
    runbot-944098
    
    closes odoo/odoo#279022
    
    X-original-commit: 7128932c4a0167fc3b2f9222ed8a4ea883906cf3
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Benoit Socias (bso) <bso@odoo.com>

--- addons/html_editor/static/tests/link/popover.test.js
+++ addons/html_editor/static/tests/link/popover.test.js
@@ -1,7 +1,6 @@
 import { describe, expect, test } from "@odoo/hoot";
 import {
     click,
-    delay,
     fill,
     press,
     queryFirst,
@@ -832,11 +831,8 @@ describe("popover for file uploads", () => {
         };
 
         onRpc("/html_editor/attachment/add_data", async (request) => {
-            const { params } = await request.json();
-            await delay(100);
-            return {
-                name: params.name,
-            };
+            await request.json();
+            await new Promise((res) => false); // We do not want this to complete.
         });
         const { editor, el } = await setupEditor("<p>[]<br></p>");
         const mockedUpload = patchUpload(editor);
