01a8ef6383cea8910a60663d5891275c3fda7a9d
pkgu-odoo <pkgu@odoo.com>
2026-07-28 12:51:42 +0000

    [FIX] mrp: restrict combo products from being used as BoM components
    Version:
    ---------
    - 19.0+
    
    Steps to reproduce:
    -------------------
    - Install `mrp` and `sale_management`
    - Create a product of type `combo`
    - Go to Manufacturing > Products > Bills of Materials
    - Create or edit a BoM
    - Add a component line
    - Select the combo product
    
    Issue:
    ------
    Combo products can be selected as BoM components.
    
    Since combo products represent configurable sales bundles rather than
    physical products, they are not meaningful manufacturing components.
    
    Before Commit:
    -------------------
    - It was possible to select a product of type 'combo'
    as a component in a Bill of Materials.
    
    After Commit:
    -----------------
    - Combo products are filtered out of the component picker and cannot
      be added as BoM components
    
    opw-6425024
    
    X-original-commit: f492da022530330a32e9320c738b4bc2dfccbace

--- addons/mrp/models/mrp_bom.py
+++ addons/mrp/models/mrp_bom.py
@@ -680,7 +680,7 @@ class MrpBomLine(models.Model):
     def _default_uom_id(self):
         return self.env['uom.uom'].search([], limit=1, order='id').id
 
-    product_id = fields.Many2one('product.product', 'Component', required=True, check_company=True, index=True)
+    product_id = fields.Many2one('product.product', 'Component', required=True, check_company=True, index=True, domain="[('type', 'in', ['consu', 'service'])]")
     product_tmpl_id = fields.Many2one('product.template', 'Product Template', related='product_id.product_tmpl_id', store=True, index=True)
     company_id = fields.Many2one(
         related='bom_id.company_id', store=True, index=True, readonly=True)
