8c38079339a425c51a1c5a0705d955a6ceaab0fa
Anh Thao Pham (pta) <pta@odoo.com>
2026-07-29 09:02:10 +0000

    [FIX] account: propagate tax amount in tax details of base line
    Steps to reproduce:
    - Create a tax that affects the base of the subsequent ones
    - Create an invoice with this tax and another one on the same line
    
    Issue:
    In "_aggregate_base_line_tax_details", the tax amount from the
    first tax should be included in the following values of the
    second tax:
    - raw_total_excluded
    - raw_total_excluded_currency
    - target_total_excluded
    - target_total_excluded_currency
    - total_excluded
    - total_excluded_currency
    
    But it is not.
    
    opw-6235909
    
    X-original-commit: 4ce8b40dc3ed7234f83225d65dcf41ccad6c4cb1

--- addons/account/static/src/helpers/account_tax.js
+++ addons/account/static/src/helpers/account_tax.js
@@ -472,6 +472,7 @@ export const accountTaxHelpers = {
                         batch: batching_results.batch_per_tax[tax_data.tax.id],
                         tax_amount: tax_data.tax_amount,
                         price_include: tax_data.price_include,
+                        original_price_include: tax_data.original_price_include,
                         base_amount: tax_data.base,
                         is_reverse_charge: tax_data.is_reverse_charge || false,
                     }
@@ -1523,6 +1524,7 @@ export const accountTaxHelpers = {
         const tax_details = base_line.tax_details;
         const taxes_data = tax_details.taxes_data;
         const manual_tax_amounts = base_line.manual_tax_amounts;
+        const tax_amount_to_propagate = {}
 
         // If there are no taxes, we pass an empty object to the grouping function.
         for (const tax_data of taxes_data.length !== 0 ? taxes_data : [null]) {
@@ -1549,9 +1551,13 @@ export const accountTaxHelpers = {
                     const excluded_target_field = `target_${excluded_rounded_field}`;
                     const excluded_manual_field = `manual_${excluded_rounded_field}`;
 
-                    const excluded_rounded_amount =
+                    let excluded_rounded_amount =
                         tax_details[excluded_rounded_field] + tax_details[excluded_delta_field];
-                    const excluded_raw_amount = tax_details[excluded_raw_field];
+                    let excluded_raw_amount = tax_details[excluded_raw_field];
+                    if (tax_data && tax_data.tax.id in tax_amount_to_propagate) {
+                        excluded_rounded_amount += tax_amount_to_propagate[tax_data.tax.id][excluded_rounded_field]
+                        excluded_raw_amount += tax_amount_to_propagate[tax_data.tax.id][excluded_raw_field]
+                    }
 
                     values[excluded_rounded_field] = excluded_rounded_amount;
                     values[excluded_raw_field] = excluded_raw_amount;
@@ -1603,6 +1609,22 @@ export const accountTaxHelpers = {
 
             // Tax amount.
             if (tax_data) {
+                // Compute the amount that should be propagated by tax
+                for (const tax of tax_data['taxes']) {
+                    const tax_id = tax.id
+                    if (tax_id in tax_amount_to_propagate) {
+                        tax_amount_to_propagate[tax_id]['raw_total_excluded'] += tax_data['raw_tax_amount']
+                        tax_amount_to_propagate[tax_id]['raw_total_excluded_currency'] += tax_data['raw_tax_amount_currency']
+                        tax_amount_to_propagate[tax_id]['total_excluded'] += tax_data['tax_amount']
+                        tax_amount_to_propagate[tax_id]['total_excluded_currency'] += tax_data['tax_amount_currency']
+                    } else {
+                        tax_amount_to_propagate[tax_id] = {}
+                        tax_amount_to_propagate[tax_id]['raw_total_excluded'] = tax_data['raw_tax_amount']
+                        tax_amount_to_propagate[tax_id]['raw_total_excluded_currency'] = tax_data['raw_tax_amount_currency']
+                        tax_amount_to_propagate[tax_id]['total_excluded'] = tax_data['tax_amount']
+                        tax_amount_to_propagate[tax_id]['total_excluded_currency'] = tax_data['tax_amount_currency']
+                    }
+                }
                 const reverse_charge_sign = tax_data.is_reverse_charge ? -1 : 1;
                 const values = values_per_grouping_key[grouping_key];
                 for (const suffix of ["_currency", ""]) {
