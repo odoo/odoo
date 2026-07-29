2d2447cdcbf532018ab5dbb1bb6c0979c4eb5614
Xavier-Do <xdo@odoo.com>
2026-07-29 07:54:02 +0000

    [FIX] logging: fix json formater and add tests
    The JsonFormatter has two bugs
    - the ignore list is not working as expected
    - in 18.0-18.4 the 'test' key is broken
    
    This commit add tests to ensure those behaviour works as expected
    
    While on it, also adds a `additional_record_keys` parameter to allow
    to specifically add keys to the default list, without having to override
    the whole list.
    
    closes odoo/odoo#279344
    
    X-original-commit: 1710344b1f55c29512b75de794f9e421b7ac51de
    Signed-off-by: Xavier Dollé (xdo) <xdo@odoo.com>

--- odoo/addons/base/tests/__init__.py
+++ odoo/addons/base/tests/__init__.py
@@ -34,6 +34,7 @@ from . import test_ir_sequence
 from . import test_ir_sequence_date_range
 from . import test_ir_embedded_actions
 from . import test_ir_default
+from . import test_logging
 from . import test_mail
 from . import test_menu
 from . import test_mimetypes
