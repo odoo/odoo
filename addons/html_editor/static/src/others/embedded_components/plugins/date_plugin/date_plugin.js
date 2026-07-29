alkuneemi/Luxara

[IMP] html_editor: introduce date command
This commit aims to add a new powerbox category `Date`
which will have multiple date commands:

- `/today` - Insert today'd date
- `/date` - Show a date picker to select any date
- `/hour` - Insert current time
- `/datetime` - Show a datetime picker to select any date with time

Clicking any inserted date will open a date picker to edit the date
or datetime.

task-6127844

addons/html_editor/static/src/others/embedded_components/plugins/date_plugin/date_plugin.js
@@ -1,0 +1,127 @@
+import { Plugin } from "@html_editor/plugin";
+import { parseHTML } from "@html_editor/utils/html";
+import { withSequence } from "@html_editor/utils/resource";
+import { DateTimePickerPopover } from "@web/core/datetime/datetime_picker_popover";
+import { _t } from "@web/core/l10n/translation";
+import { user } from "@web/core/user";
+import { renderToString } from "@web/core/utils/render";
+import { reactive } from "@web/owl2/utils";
+const { DateTime } = luxon;
+
+export class DatePlugin extends Plugin {
+    static id = "date";
+    static dependencies = ["history", "overlay", "dom", "embeddedComponents"];
+
+    resources = {
+        user_commands: [
+            {
+                id: "insertDateToday",
+                title: _t("Today"),
+                description: _t("Insert today's date"),
+                icon: "fa-calendar",
+                run: () => {
+                    this.insertDate(DateTime.now(), "date");
+                },
+            },
+            {
+                id: "insertDate",
+                title: _t("Date"),
+                description: _t("Insert any date"),
+                icon: "fa-calendar",
+                run: () => {
+                    this.openDateTimePicker("date");
+                },
+            },
+            {
+                id: "insertTime",
+                title: _t("Hour"),
+                description: _t("Insert the current time"),
+                icon: "fa-calendar",
+                run: () => {
+                    this.insertDate(DateTime.now(), "time");
+                },
+            },
+            {
+                id: "insertDateTime",
+                title: _t("Date and Time"),
+                description: _t("Insert any date and time"),
+                icon: "fa-calendar",
+                run: () => {
+                    this.openDateTimePicker("datetime");
+                },
+            },
+        ],
+
+        powerbox_categories: withSequence(30, { id: "date", name: _t("Date") }),
+
+        powerbox_items: [
+            {
+                commandId: "insertDateToday",
+                categoryId: "date",
+            },
+            {
+                commandId: "insertTime",
+                categoryId: "date",
+            },
+            {
+                commandId: "insertDate",
+                categoryId: "date",
+            },
+            {
+                commandId: "insertDateTime",
+                categoryId: "date",
+            },
+        ],
+    };
+
+    setup() {
+        this.overlay = this.dependencies.overlay.createOverlay(DateTimePickerPopover, {
+            className: "popover mw-100",
+        });
+    }
+
+    /**
+     * @param {"date" | "datetime"} [type="date"]
+     */
+    openDateTimePicker(type = "date") {
+        const pickerProps = reactive({
+            value: DateTime.now().setZone(user.tz || "local"), // Default date
+            type,
+            onSelect: (date) => {
+                if (type === "date") {
+                    this.insertDate(date, type);
+                    this.overlay.close();
+                } else {
+                    pickerProps.value = date;
+                }
+            },
+        });
+        this.overlay.open({
+            props: {
+                showResetButton: false,
+                pickerProps,
+                close: () => {
+                    this.insertDate(pickerProps.value, type);
+                    this.overlay.close();
+                },
+            },
+        });
+    }
+
+    /**
+     * @param {Object} date - Luxon DateTime instance.
+     * @param {"date" | "datetime" | "time"} type
+     */
+    insertDate(date, type) {
+        const dateUTC = date.toUTC().toISO();
+        const dateEl = parseHTML(
+            this.document,
+            renderToString("html_editor.EmbeddedDateBlueprint", {
+                embeddedProps: JSON.stringify({ date: dateUTC, type }),
+            })
+        );
+
+        this.dependencies.dom.insert(dateEl);
+        this.dependencies.history.addStep();
+    }
+}

odoo/odoo

addons/html_editor/static/src/others/embedded_components/plugins/date_plugin/date_plugin.js
@@ -1,0 +1,127 @@
+import { Plugin } from "@html_editor/plugin";
+import { parseHTML } from "@html_editor/utils/html";
+import { withSequence } from "@html_editor/utils/resource";
+import { proxy } from "@odoo/owl";
+import { DateTimePickerPopover } from "@web/core/datetime/datetime_picker_popover";
+import { _t } from "@web/core/l10n/translation";
+import { renderToString } from "@web/core/utils/render";
+const { DateTime } = luxon;
+
+export class DatePlugin extends Plugin {
+    static id = "date";
+    static dependencies = ["history", "overlay", "dom", "embeddedComponents"];
+
+    resources = {
+        user_commands: [
+            {
+                id: "insertDateToday",
+                title: _t("Today"),
+                description: _t("Insert today's date"),
+                icon: "calendar_today",
+                run: () => {
+                    this.insertDate(DateTime.now(), "date");
+                },
+            },
+            {
+                id: "insertDate",
+                title: _t("Date"),
+                description: _t("Insert any date"),
+                icon: "calendar_today",
+                run: () => {
+                    this.openDateTimePicker("date");
+                },
+            },
+            {
+                id: "insertTime",
+                title: _t("Hour"),
+                description: _t("Insert the current time"),
+                icon: "calendar_today",
+                run: () => {
+                    this.insertDate(DateTime.now(), "time");
+                },
+            },
+            {
+                id: "insertDateTime",
+                title: _t("Date and Time"),
+                description: _t("Insert any date and time"),
+                icon: "calendar_today",
+                run: () => {
+                    this.openDateTimePicker("datetime");
+                },
+            },
+        ],
+
+        powerbox_categories: withSequence(30, { id: "date", name: _t("Date") }),
+
+        powerbox_items: [
+            {
+                commandId: "insertDateToday",
+                categoryId: "date",
+            },
+            {
+                commandId: "insertTime",
+                categoryId: "date",
+            },
+            {
+                commandId: "insertDate",
+                categoryId: "date",
+            },
+            {
+                commandId: "insertDateTime",
+                categoryId: "date",
+            },
+        ],
+    };
+
+    setup() {
+        this.overlay = this.dependencies.overlay.createOverlay(DateTimePickerPopover, {
+            className: "popover mw-100",
+        });
+    }
+
+    /**
+     * @param {"date" | "datetime"} [type="date"]
+     */
+    openDateTimePicker(type = "date") {
+        const pickerProps = proxy({
+            value: DateTime.now(), // Default date
+            type,
+            rounding: 1,
+            onSelect: (date) => {
+                if (type === "date") {
+                    this.insertDate(date, type);
+                    this.overlay.close();
+                } else {
+                    pickerProps.value = date;
+                }
+            },
+        });
+        this.overlay.open({
+            props: {
+                showResetButton: false,
+                pickerProps,
+                close: () => {
+                    this.insertDate(pickerProps.value, type);
+                    this.overlay.close();
+                },
+            },
+        });
+    }
+
+    /**
+     * @param {Object} date - Luxon DateTime instance.
+     * @param {"date" | "datetime" | "time"} type
+     */
+    insertDate(date, type) {
+        const dateUTC = date.toUTC().toISO();
+        const dateEl = parseHTML(
+            this.document,
+            renderToString("html_editor.EmbeddedDateBlueprint", {
+                embeddedProps: JSON.stringify({ date: dateUTC, type }),
+            })
+        );
+
+        this.dependencies.dom.insert(dateEl);
+        this.dependencies.history.commit();
+    }
+}