be1fd7d071bbeab3379a981954b208979dc627d9
ELCO <elco@odoo.com>
2026-07-29 08:16:33 +0000

    [IMP] web: revert removing of default_group_by facet from all views
    This reverts commit 7413e08c166ef3123ac7ed6623db1fb366f44e3c (#265574)
    
    task-6425644
    
    closes odoo/odoo#284293
    
    X-original-commit: a34e55e1e666fc94bd0eedb38083198d27c94517
    Related: odoo/enterprise#129062
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/tests/views/list/list_view.test.js
+++ addons/web/static/tests/views/list/list_view.test.js
@@ -17768,8 +17768,8 @@ test(`list view with default_group_by`, async () => {
     if (isSmall()) {
         await contains(".o_control_panel_navigation > button").click();
     }
-    // The default_group_by should not create any facet
-    expect(`.o_searchview_facet`).toHaveCount(0);
+    expect(`.o_searchview_facet`).toHaveCount(1);
+    expect(`.o_searchview_facet`).toHaveText("Bar");
     expect.verifySteps(["web_read_group1"]);
 
     await selectGroup("m2m");
@@ -17780,12 +17780,13 @@ test(`list view with default_group_by`, async () => {
 
     await toggleMenuItem("M2m");
     expect(`.o_group_header`).toHaveCount(2);
-    expect(`.o_searchview_facet`).toHaveCount(0);
+    expect(`.o_searchview_facet`).toHaveCount(1);
+    expect(`.o_searchview_facet`).toHaveText("Bar");
     expect.verifySteps(["web_read_group3"]);
 
     await toggleMenuItem("My Filter");
-    expect(`.o_searchview_facet`).toHaveCount(1);
-    expect(queryAllTexts(`.o_searchview_facet`)).toEqual(["My Filter"]);
+    expect(`.o_searchview_facet`).toHaveCount(2);
+    expect(queryAllTexts(`.o_searchview_facet`)).toEqual(["Bar", "My Filter"]);
     expect.verifySteps(["web_read_group4"]);
 });
 
@@ -17821,8 +17822,8 @@ test(`list view with multi-fields default_group_by`, async () => {
     if (isSmall()) {
         await contains(".o_control_panel_navigation > button").click();
     }
-    // The default_group_by should not create any facet
-    expect(`.o_searchview_facet`).toHaveCount(0);
+    expect(`.o_searchview_facet`).toHaveCount(1);
+    expect(`.o_searchview_facet`).toHaveText("Foo\n>\nBar");
     expect.verifySteps(["web_read_group1"]);
     await contains(`.o_group_header`).click();
     expect(`.o_group_header`).toHaveCount(5);
