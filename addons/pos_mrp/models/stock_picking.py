2b97c865c4dacc68c7e3e7bd837daefc94cf3f25
Arthur Nanson <artn@odoo.com>
2026-07-29 16:01:43 +0200

    [FIX] pos_mrp: allow kit and component with ship later
    ## Steps to reproduce:
    
    - Enable ship later for the PoS
    - Create a product A, tracked by lots
    - Create a kit product, include a component A
    - Go to the PoS, order this kit product
    - Also order the component A
    - Tick the ship later option
    --> A traceback appears
    
    ## Why the fix:
    
    With the ship later option activated, we do
    not compute the lines_data in the same way
    we do when asking for an invoice.
    
    For the invoice, https://github.com/odoo/odoo/blob/7676723dbb47c09a71925831b13ced97eb855297/addons/point_of_sale/models/stock_picking.py#L37
    We take every stockable line that is positive,
    but with the ship later option, we only take
    the lines that are tracked, meaning in this
    exemple we do not take the Kit line in
    lines_data. https://github.com/odoo/odoo/blob/7676723dbb47c09a71925831b13ced97eb855297/addons/point_of_sale/models/pos_order.py#L1525-L1532
    
    The result of this is that we do not get the
    kit line in **_get_lot_line_qty**, explaining
    the traceback, as we are trying to access it.
    
    Instead of relying on **lines_data** that could
    change depending on the context, we now use
    the pos order to fetch the line(s) with our
    kit product.
    
    Adding the kit line to **lines_data** will
    most likely result in unwanted changes because
    lots of operations are done on each of it's items.
    
    opw-6431457

--- addons/pos_mrp/models/stock_picking.py
+++ addons/pos_mrp/models/stock_picking.py
@@ -7,5 +7,7 @@ class StockMove(models.Model):
     def _get_lot_line_qty(self, line, move, lines_data):
         qty = super()._get_lot_line_qty(line, move, lines_data)
         if move.bom_line_id:
-            qty = lines_data[move.bom_line_id.bom_id.product_tmpl_id.product_variant_id.id]['order_lines'].qty * move.bom_line_id.product_qty
+            kit_product_id = move.bom_line_id.bom_id.product_tmpl_id.product_variant_id.id
+            kit_lines = line.order_id.lines.filtered(lambda l: l.product_id.id == kit_product_id)
+            qty = sum(kit_lines.mapped('qty')) * move.bom_line_id.product_qty
         return qty
