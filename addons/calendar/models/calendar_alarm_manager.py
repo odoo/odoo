5197d7556848ddb058f7d8e3ad9dfef4bc6b36be
Lucas Lefèvre (lul) <lul@odoo.com>
2026-07-29 11:03:08 +0200

    [FIX] calendar: get alarms for event starting in 24h+
    Steps to reproduce:
    - create an event starting in 25h
    - setup an alarm due very soon, say in 15 minutes
    
    => the alarm is ignored because the event starts in more than
    24h, even though the alarm is due very soon.
    
    The code is currently comparing `future` (which is in 24h) to `past` (which
    is the event start).
    `future` should be compared to `notify_at`
    
    task-6396053
    
    X-original-commit: bd16c413b7ff103a96fcc2fc9dfbe06a2470819c

--- addons/calendar/models/calendar_alarm_manager.py
+++ addons/calendar/models/calendar_alarm_manager.py
@@ -77,22 +77,18 @@ class CalendarAlarm_Manager(models.AbstractModel):
         """
         result = []
         # TODO: remove event_maxdelta and if using it
-        past = one_date - timedelta(minutes=(missing * event_maxdelta))
         future = fields.Datetime.now() + timedelta(seconds=in_the_next_X_seconds)
-        if future <= past:
-            return result
         for alarm in event.alarm_ids:
             if alarm.alarm_type != alarm_type:
                 continue
             past = one_date - timedelta(minutes=(missing * alarm.duration_minutes))
-            if future <= past:
-                continue
-            if after and past <= fields.Datetime.from_string(after):
+            notify_at = one_date - timedelta(minutes=alarm.duration_minutes)
+            if future <= notify_at or (after and past <= fields.Datetime.from_string(after)):
                 continue
             result.append({
                 'alarm_id': alarm.id,
                 'event_id': event.id,
-                'notify_at': one_date - timedelta(minutes=alarm.duration_minutes),
+                'notify_at': notify_at,
             })
         return result
 
