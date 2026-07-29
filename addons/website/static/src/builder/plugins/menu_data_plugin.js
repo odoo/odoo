e416a56d55fe35e4bae610e84b0ea001675226cb
Louis (loco) <loco@odoo.com>
2026-07-29 14:09:35 +0200

    [IMP] website: only rewrite page url for internal links
    Steps to reproduce:
    - Create a new page and add it to the website menu.
    - Edit the menu item and change its URL to an external one (e.g.
    https://www.odoo.com).
    - Save.
    
    -> Clicking on the menu item does not open the external url.
    
    Because the menu is linked to a page, changing the menu url rewrites the
    page url. This is not the expected behavior if the new url is external.
    Note that this problem became visible thanks to the previous commits
    that made menu url not stored. Indeed, before those commits, changing
    the menu url still rewrote the page url but as the url was a stored
    field of the menu record, clicking on the menu led to the external url.
    
    task-3355343
    
    Part-of: odoo/odoo#254427
    Related: odoo/upgrade#10905
    Signed-off-by: Habib Ayob (ayh) <ayh@odoo.com>

--- addons/website/static/src/builder/plugins/menu_data_plugin.js
+++ addons/website/static/src/builder/plugins/menu_data_plugin.js
@@ -3,6 +3,7 @@ import { registry } from "@web/core/registry";
 import { NavbarLinkPopover } from "./navbar_link_popover/navbar_link_popover";
 import { MenuDialog, EditMenuDialog } from "@website/components/dialog/edit_menu";
 import { withSequence } from "@html_editor/utils/resource";
+import { deduceURLfromText } from "@html_editor/main/link/utils";
 
 /**
  * @typedef { Object } MenuDataShared
@@ -37,7 +38,7 @@ export class MenuDataPlugin extends Plugin {
                                 const data = {
                                     id: parseInt(menuEl.attributes["data-oe-id"].nodeValue),
                                     name,
-                                    url,
+                                    url: deduceURLfromText(url) || url,
                                 };
                                 const result = await this.services.orm.call(
                                     "website.menu",
@@ -55,7 +56,7 @@ export class MenuDataPlugin extends Plugin {
                 }),
             }),
         ],
-        is_link_editable_predicates: linkElement => {
+        is_link_editable_predicates: (linkElement) => {
             if (this.isMenuLink(linkElement)) {
                 return true;
             }
