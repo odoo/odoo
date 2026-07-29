3266540bbb97acb556453310a5e77935de7c7063
Mark Orban (maorb) <maorb@odoo.com>
2026-07-29 08:51:56 +0000

    [FIX] html_editor, *: fix button gradient loss after text color change
    *: html_builder
    
    Steps to Reproduce :
    1. Go to Website → Edit Mode
    2. Add a snippet with button
    3. Click on button and change its type to : " Custom"
    4. Apply the gradient type color in fill color option
    5. Apply any color in text color option
    6. You will notice that the gradient type color in fill color option is
    removed.
    
    Problem:
    Since [this commit][1] new button style options have been added to the
    sidebar. If one changes the style of a button to custom, changes the
    background to gradient, and tries to change the text color, the
    background gradient is removed.
    
    Cause:
    Whenever a gradient is added either to text or as a background, it is
    applied as a background image. In the case of a text gradient, an
    additional class, `text-gradient`, is applied for correct styling. Once
    a change to either color or background/fill is applied that is not a
    gradient color change, the background image of the element would be
    reset to nothing. This is the result of [this line][2] from a [previous
    commit][3]. In the case of a button, this meant changing the font
    color would reset the background. That is not the desired outcome.
    The flaw was only discovered once new options were added to change
    non-text/font elements' backgrounds to gradient.
    
    Solution:
    An additional check has been added to see if the element being edited
    was text with a gradient. If so, we remove the background image.
    Otherwise, it is kept so that the button case from above is resolved.
    
    [1]: https://github.com/odoo/odoo/commit/2bf1db001b195480b963b338583c170aa1c009a3
    [2]: https://github.com/odoo/odoo/blob/8e0845712f462ecfafd2176406dcbafc869a5564/addons/html_editor/static/src/main/font/color_plugin.js#L575
    [3]: https://github.com/odoo/odoo/commit/8e0845712f462ecfafd2176406dcbafc869a5564
    
    task-6247134
    
    closes odoo/odoo#285063
    
    X-original-commit: b6860a1db8a5487af634b04053ecaae90c2c5e51
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Mark Orban (maorb) <maorb@odoo.com>

--- addons/html_editor/static/src/main/font/color_plugin.js
+++ addons/html_editor/static/src/main/font/color_plugin.js
@@ -525,7 +525,6 @@ export class ColorPlugin extends Plugin {
             removePresetGradient(element);
         }
 
-        const hasGradientStyle = element.style.backgroundImage.includes("-gradient");
         if (mode === "backgroundColor") {
             if (!color) {
                 element.classList.remove("o_cc", ...COLOR_COMBINATION_CLASSES);
@@ -535,7 +534,7 @@ export class ColorPlugin extends Plugin {
             let newBackgroundImage = backgroundImagePartsToCss(parts);
             // we override the bg image if the new bg image is empty, but the previous one is a gradient.
             if (hasGradient && !newBackgroundImage) {
-                newBackgroundImage = "none";
+                newBackgroundImage = "";
             }
             element.style.backgroundImage = newBackgroundImage;
             element.style["background-color"] = "";
@@ -578,7 +577,7 @@ export class ColorPlugin extends Plugin {
             );
         } else {
             delete parts.gradient;
-            if (hasGradientStyle && !backgroundImagePartsToCss(parts)) {
+            if (oldClassName.includes("text-gradient") && !backgroundImagePartsToCss(parts)) {
                 element.style["background-image"] = "";
             }
             if (color.startsWith("text") || color.startsWith("bg-")) {
