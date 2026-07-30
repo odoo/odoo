8201bbe7aabb8e5cf10e5f51ba039446e08e1e8c
Hadi El Yakhni <hael@odoo.com>
2026-07-30 10:43:02 +0200

    [FIX] l10n_pe_pos: print the identification type on the receipt
    Steps to reproduce
    ------------------
    1. Install `l10n_pe_pos`.
    2. Create a contact with the identification type DNI and a number.
    3. Sell a product to this contact and print the receipt.
    -> the receipt shows "RUC" in front of the number, even though the
    number is a DNI and not a RUC.
    
    Why it's happening
    ------------------
    Since `aeaca097ae39` the number of the contact is printed with a label in
    front of it, before there was no label at all. This label is the
    `vat_label` of the country, so "RUC" for Peru. But in Peru each contact
    can have a different identifier, and the type is on the contact in
    `l10n_latam_identification_type_id`.
    
    The fix
    -------
    In `l10n_pe_pos` we set `partner_vat_label` with the name of the
    identification type of the contact. If the contact has no identification
    type we keep the number alone. We also load the identification type when
    the fiscal country is Peru as the PoS frontend now needs it to print the
    label.
    
    opw-6422619
    
    closes odoo/odoo#279409
    
    Signed-off-by: Hadi El Yakhni (hael) <hael@odoo.com>

--- addons/l10n_pe_pos/tests/test_pe_pos.py
+++ addons/l10n_pe_pos/tests/test_pe_pos.py
@@ -1,4 +1,5 @@
 from odoo.addons.account_edi.tests.common import AccountTestInvoicingCommon
+from odoo.addons.point_of_sale.tests.test_frontend import TestPointOfSaleHttpCommon
 from odoo.addons.point_of_sale.tests.test_generic_localization import TestGenericLocalization
 from odoo.tests import tagged
 
@@ -11,3 +12,31 @@ class TestGenericPE(TestGenericLocalization):
     @AccountTestInvoicingCommon.setup_country('pe')
     def setUpClass(cls):
         super().setUpClass()
+
+
+@tagged('post_install', '-at_install', 'post_install_l10n')
+class TestPePosReceipt(TestPointOfSaleHttpCommon):
+    @classmethod
+    @AccountTestInvoicingCommon.setup_country('pe')
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.partner = cls.env['res.partner'].create({
+            'name': 'Luigys Toro',
+            'country_id': cls.env.ref('base.pe').id,
+            'l10n_latam_identification_type_id': cls.env.ref('l10n_pe.it_DNI').id,
+            'vat': '70025425',
+        })
+
+    def test_receipt_partner_vat_label(self):
+        self.main_pos_config.open_ui()
+        order = self.env['pos.order'].create({
+            'session_id': self.main_pos_config.current_session_id.id,
+            'partner_id': self.partner.id,
+            'amount_total': 0,
+            'amount_paid': 0,
+            'amount_tax': 0,
+            'amount_return': 0,
+        })
+
+        extra_data = order.order_receipt_generate_data()['extra_data']
+        self.assertEqual(extra_data['partner_vat_label'], 'DNI')
