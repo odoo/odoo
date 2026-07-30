0ed6e6bf3ec03d545a25e024be07c3f3d5a481b0
Abo Taha <ahmoh@odoo.com>
2026-07-30 09:27:26 +0200

    [ADD] l10n_gr_edi_e_invoo: issue invoices through e-invoo
    Greek customer invoices were transmitted directly to myDATA, but this is
    not legally compliant as odoo is not AADE-certified yet.
    
    Add a bridge module that routes outgoing Greek invoices through the
    Greek EDI IAP service and e-invoo, an AADE-certified YPAHES provider.
    
    task-6395556

---
+++ addons/l10n_gr_edi_e_invoo/models/account_edi_proxy_user.py
@@ -0,0 +1,72 @@
+from stdnum.gr import vat as gr_vat
+
+from odoo import fields, models
+from odoo.exceptions import RedirectWarning
+from odoo.tools import index_exists
+
+
+L10N_GR_EDI_DEFAULT_IAP_ENDPOINT = 'https://l10n-gr-edi.api.odoo.com'
+L10N_GR_EDI_DEFAULT_IAP_TEST_ENDPOINT = 'https://l10n-gr-edi.test.odoo.com'
+L10N_GR_EDI_IAP_ENDPOINT_PARAM = 'l10n_gr_edi.iap_endpoint'
+L10N_GR_EDI_IAP_ROUTE_PREFIX = '/api/l10n_gr_edi/1'
+L10N_GR_EDI_PROXY_TYPE = 'l10n_gr_edi'
+
+
+class AccountEdiProxyClientUser(models.Model):
+    _inherit = 'account_edi_proxy_client.user'
+
+    proxy_type = fields.Selection(
+        selection_add=[(L10N_GR_EDI_PROXY_TYPE, 'Greek EDI')],
+        ondelete={L10N_GR_EDI_PROXY_TYPE: 'cascade'},
+    )
+
+    _sql_constraints = [
+        (
+            'unique_identification_l10n_gr_edi',
+            '',
+            'This EDI identification is already assigned to an active user.',
+        ),
+    ]
+
+    def _auto_init(self):
+        super()._auto_init()
+        if not index_exists(
+            self.env.cr,
+            'account_edi_proxy_client_user_unique_identification_l10n_gr_edi',
+        ):
+            self.env.cr.execute("""
+                CREATE UNIQUE INDEX account_edi_proxy_client_user_unique_identification_l10n_gr_edi
+                                 ON account_edi_proxy_client_user(edi_identification, edi_mode)
+                              WHERE (active IS True AND proxy_type = 'l10n_gr_edi')
+            """)
+
+    def _get_proxy_urls(self):
+        urls = super()._get_proxy_urls()
+        endpoint_override = self.env['ir.config_parameter'].sudo().get_param(L10N_GR_EDI_IAP_ENDPOINT_PARAM)
+        urls[L10N_GR_EDI_PROXY_TYPE] = {
+            'prod': L10N_GR_EDI_DEFAULT_IAP_ENDPOINT,
+            'test': (endpoint_override or L10N_GR_EDI_DEFAULT_IAP_TEST_ENDPOINT).rstrip('/'),
+            'demo': False,
+        }
+        return urls
+
+    def _get_proxy_identification(self, company, proxy_type):
+        if proxy_type == L10N_GR_EDI_PROXY_TYPE:
+            if not company.vat:
+                raise RedirectWarning(
+                    message=self.env._(
+                        'Please fill the VAT number of company "%(company_name)s" before sending electronic invoices.',
+                        company_name=company.display_name,
+                    ),
+                    action=company._get_records_action(),
+                    button_text=self.env._("Go to company"),
+                )
+            return gr_vat.compact(company.vat)
+        return super()._get_proxy_identification(company, proxy_type)
+
+    def _l10n_gr_edi_proxy_request(self, route, params):
+        self.ensure_one()
+        return self._make_request(
+            url=f"{self._get_server_url()}{L10N_GR_EDI_IAP_ROUTE_PREFIX}/{route}",
+            params={**params, 'lang': self.env.lang},
+        )
