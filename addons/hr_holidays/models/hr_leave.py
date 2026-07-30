7f5432ee47d10050e54e8d318228b2b737dc8e5b
Jurgen Gjini <jugj@odoo.com>
2026-07-30 11:34:32 +0000

    [FIX] hr_holidays: Wrong country id used in _compute_work_entry_type_id domain
    X-original-commit: d6eebfd4208f3c7ac547a762c14c3e11bbf23dd7

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -673,9 +673,21 @@ class HrLeave(models.Model):
     @api.depends('employee_id', 'request_date_from', 'request_date_to')
     def _compute_work_entry_type_id(self):
         for holiday in self:
+<<<<<<< 87e3bc10da283d18f87150660c5b357b4af8d973
             allowed_country_ids = [holiday.employee_id.company_id.country_id.id, False]
             local_work_entry_types = self.env['hr.work.entry.type'].with_context(default_date_from=holiday.request_date_from, default_date_to=holiday.request_date_to).search([('country_id', 'in', allowed_country_ids)])
             if holiday.work_entry_type_id and holiday.work_entry_type_id in local_work_entry_types:
+||||||| 4b5b01e4f72562ca858c357792678bc144bbc470
+            local_work_entry_types = self.env['hr.work.entry.type'].with_context(default_date_from=holiday.request_date_from, default_date_to=holiday.request_date_to).search([('country_id', 'in', [holiday.employee_id.country_id.id or holiday.employee_id.company_id.country_id.id] + [False])])
+            all_valid_work_entry_types = local_work_entry_types.with_context(default_date_from=holiday.request_date_from, default_date_to=holiday.request_date_to).filtered_domain([('has_valid_allocation', '=', True)])
+            no_allocation_required_work_entry_types = local_work_entry_types.with_context(default_date_from=holiday.request_date_from, default_date_to=holiday.request_date_to).filtered_domain([('requires_allocation', '=', False)])
+            if not holiday.work_entry_type_id.requires_allocation:
+=======
+            local_work_entry_types = self.env['hr.work.entry.type'].with_context(default_date_from=holiday.request_date_from, default_date_to=holiday.request_date_to).search([('country_id', 'in', [holiday.employee_id.company_id.country_id.id] + [False])])
+            all_valid_work_entry_types = local_work_entry_types.with_context(default_date_from=holiday.request_date_from, default_date_to=holiday.request_date_to).filtered_domain([('has_valid_allocation', '=', True)])
+            no_allocation_required_work_entry_types = local_work_entry_types.with_context(default_date_from=holiday.request_date_from, default_date_to=holiday.request_date_to).filtered_domain([('requires_allocation', '=', False)])
+            if not holiday.work_entry_type_id.requires_allocation:
+>>>>>>> 2e5c0fcc88ccde0d4f09b7133382aea7d6585201
                 continue
             all_valid_work_entry_types = local_work_entry_types.filtered_domain([('has_valid_allocation', '=', True)])
             no_alloc_types = local_work_entry_types.filtered_domain([('requires_allocation', '=', False)])
