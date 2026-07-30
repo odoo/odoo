86fb245ae89eec1ff6f78a9b0de09417f97896aa
Ruben Gomes <rugo@odoo.com>
2026-07-30 13:40:54 +0200

    [REF] test_tests: remove base models dependencies
    closes odoo/odoo#279381
    
    Signed-off-by: Krzysztof Magusiak (krma) <krma@odoo.com>

--- odoo/addons/test_tests/tests/test_cases.py
+++ odoo/addons/test_tests/tests/test_cases.py
@@ -20,23 +20,23 @@ class TestTransactionCase(TransactionCase):
 
     def test_00(self):
         """ Create a partner. """
-        partners = self.env['res.partner'].search([('name', '=', 'test_per_class_teardown_partner')])
+        partners = self.env['test_testing_utilities.c'].search([('name', '=', 'test_per_class_teardown_partner')])
         self.assertEqual(0, len(partners), "Test partner found.")
-        self.env['res.partner'].create({'name': 'test_per_class_teardown_partner'})
-        partners = self.env['res.partner'].search([('name', '=', 'test_per_class_teardown_partner')])
+        self.env['test_testing_utilities.c'].create({'name': 'test_per_class_teardown_partner'})
+        partners = self.env['test_testing_utilities.c'].search([('name', '=', 'test_per_class_teardown_partner')])
         self.assertEqual(1, len(partners), "Test partner not found.")
 
     def test_01(self):
         """ Don't find the created partner. """
-        partners = self.env['res.partner'].search([('name', '=', 'test_per_class_teardown_partner')])
+        partners = self.env['test_testing_utilities.c'].search([('name', '=', 'test_per_class_teardown_partner')])
         self.assertEqual(0, len(partners), "Test partner found.")
 
     def test_20a(self):
         """ Create a partner with a XML ID then resolve xml id with ref() and browse_ref() """
-        pid, _ = self.env['res.partner'].name_create('Mr Yellow')
+        pid, _ = self.env['test_testing_utilities.c'].name_create('Mr Yellow')
         self.env['ir.model.data'].create({'name': 'test_partner_yellow',
                                           'module': 'base',
-                                          'model': 'res.partner',
+                                          'model': 'test_testing_utilities.c',
                                           'res_id': pid})
         xid = 'base.test_partner_yellow'
         partner = self.env.ref(xid)
