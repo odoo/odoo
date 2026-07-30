2c1c9e3e0632e682e99736868b6f9dbc6b5b1c73
Jurgen Gjini <jugj@odoo.com>
2026-07-30 11:34:32 +0000

    [FIX] hr_holidays: Wrong country id used in _compute_work_entry_type_id domain
    closes odoo/odoo#279640
    
    X-original-commit: d6eebfd4208f3c7ac547a762c14c3e11bbf23dd7
    Signed-off-by: Pierre Masereel (pim) <pim@odoo.com>

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -516,7 +516,7 @@ class HrLeave(models.Model):
     @api.depends('employee_id', 'request_date_from', 'request_date_to')
     def _compute_work_entry_type_id(self):
         for holiday in self:
-            local_work_entry_types = self.env['hr.work.entry.type'].with_context(default_date_from=holiday.request_date_from, default_date_to=holiday.request_date_to).search([('country_id', 'in', [holiday.employee_id.country_id.id or holiday.employee_id.company_id.country_id.id] + [False])])
+            local_work_entry_types = self.env['hr.work.entry.type'].with_context(default_date_from=holiday.request_date_from, default_date_to=holiday.request_date_to).search([('country_id', 'in', [holiday.employee_id.company_id.country_id.id] + [False])])
             all_valid_work_entry_types = local_work_entry_types.with_context(default_date_from=holiday.request_date_from, default_date_to=holiday.request_date_to).filtered_domain([('has_valid_allocation', '=', True)])
             no_allocation_required_work_entry_types = local_work_entry_types.with_context(default_date_from=holiday.request_date_from, default_date_to=holiday.request_date_to).filtered_domain([('requires_allocation', '=', False)])
             if not holiday.work_entry_type_id.requires_allocation:
