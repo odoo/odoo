e6fe0dc87ac5e8cd7685cb3f6dd5c60de8b49c3a
paan-odoo <paan@odoo.com>
2026-07-30 11:24:11 +0530

    [FIX] point_of_sale: handle single-value attributes
    Before this commit:
    
    - When a product attribute had only one available value, it was not automatically
      selected for display types other than multi.
    
    After this commit:
    
    - Automatically select the attribute value when an attribute has a single
      available value and its display type is not multi, allowing the product to be
      added without any additional user interaction.
    
    Task-6327371
    
    closes odoo/odoo#272437
    
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- addons/point_of_sale/tests/test_frontend.py
+++ addons/point_of_sale/tests/test_frontend.py
@@ -2647,6 +2647,10 @@ class TestUi(TestPointOfSaleHttpCommon):
         self.main_pos_config.with_user(self.pos_user).open_ui()
         self.start_pos_tour('test_single_value_multi_attribute_configurator')
 
+    def test_single_attribute_value_products(self):
+        self.main_pos_config.open_ui()
+        self.start_pos_tour('test_single_attribute_value_products')
+
 
 # This class just runs the same tests as above but with mobile emulation
 class MobileTestUi(TestUi):
