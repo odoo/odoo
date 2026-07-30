2e04fc8e1dcd968cf590cb20ea20c469743ea4a0
adsh-odoo <adsh@odoo.com>
2026-07-30 16:33:56 +0530

    [REM] survey: remove zombie code
    Here https://github.com/odoo/odoo/commit/719e72f6f44f01c360ef44719404bf47754a8f2d we redesigned client facing survey view.
    And we removed those icons that are being used by this piece of code, and due to
    that, actually, this code does nothing. We noticed it via a traceback occurring
    in saas-19.5 during the recently material symbol icons conversion https://github.com/odoo/odoo/commit/c5a40a608280017ae9ea8f9e9e1c59f778d629ae
    which was accessing the class in the wrong manner.
    
    Instead of fixing the traceback in saas-19.5, this commit removes the code from
    the place where it becomes useless.
    
    Task-6421899
    
    closes odoo/odoo#279739
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- addons/survey/static/src/interactions/survey_form.js
+++ addons/survey/static/src/interactions/survey_form.js
@@ -1434,16 +1434,6 @@ export class SurveyForm extends Interaction {
                 const answerId = buttonEl.querySelector("input").value;
                 const isCorrect = correctAnswer.includes(parseInt(answerId));
                 buttonEl.classList.add(`bg-${isCorrect ? "success" : "danger"}`, "text-white");
-                // For the user incorrect answers, replace the empty check icon by a crossed check icon
-                if (!isCorrect && buttonEl.classList.contains("o_survey_selected")) {
-                    let fromIcon = "fa-check-circle";
-                    let toIcon = "fa-times-circle";
-                    if (questionType === "multiple_choice") {
-                        fromIcon = "fa-check-square";
-                        toIcon = "fa-times-rectangle"; // fa-times-square doesn't exist in fontawesome 4.7
-                    }
-                    buttonEl.querySelector(`i.${fromIcon}`)?.classList.replace(fromIcon, toIcon);
-                }
             }
         }
     }
