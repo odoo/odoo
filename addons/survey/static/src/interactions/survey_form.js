7ac3fa85d54f8f981e71f4479ebfe4cc7859db54
adsh-odoo <adsh@odoo.com>
2026-07-30 11:03:56 +0000

    [REM] survey: remove zombie code
    Here https://github.com/odoo/odoo/commit/719e72f6f44f01c360ef44719404bf47754a8f2d we redesigned client facing survey view.
    And we removed those icons that are being used by this piece of code, and due to
    that, actually, this code does nothing. We noticed it via a traceback occurring
    in saas-19.5 during the recently material symbol icons conversion https://github.com/odoo/odoo/commit/c5a40a608280017ae9ea8f9e9e1c59f778d629ae
    which was accessing the class in the wrong manner.
    
    Instead of fixing the traceback in saas-19.5, this commit removes the code from
    the place where it becomes useless.
    
    Task-6421899
    
    X-original-commit: 2e04fc8e1dcd968cf590cb20ea20c469743ea4a0

--- addons/survey/static/src/interactions/survey_form.js
+++ addons/survey/static/src/interactions/survey_form.js
@@ -1454,16 +1454,6 @@ export class SurveyForm extends Interaction {
                 const answerId = buttonEl.querySelector("input").value;
                 const isCorrect = correctAnswer.includes(parseInt(answerId));
                 buttonEl.classList.add(`bg-${isCorrect ? "success" : "danger"}`, "text-white");
-                // For the user incorrect answers, replace the empty check icon by a crossed check icon
-                if (!isCorrect && buttonEl.classList.contains("o_survey_selected")) {
-                    let fromIcon = "check_circle";
-                    let toIcon = "cancel";
-                    if (questionType === "multiple_choice") {
-                        fromIcon = "check_box";
-                        toIcon = "cancel";
-                    }
-                    buttonEl.querySelector(`i.[data-icon="${fromIcon}"]`)?.setAttribute("data-icon", toIcon);
-                }
             }
         }
     }
