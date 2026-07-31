5813f1217ad7725af4a721e4b4d6c9a1f45615f1
Quentin De Paoli <qdp@odoo.com>
2026-07-31 12:39:23 +0000

    [FIX] account: entries neutralization in automatic entry wizard (change_period)
    The previous code wasn't reconciling the destination and accrual moves, leaving potentially non-neutral journal items and their counterpart polluting the DB.
    
    task-6438783
    
    closes odoo/odoo#287524
    
    X-original-commit: 31d0b41012157c117d5709987f1d20c81d1a2c71
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/account/tests/test_account_move_out_invoice.py
+++ addons/account/tests/test_account_move_out_invoice.py
@@ -2980,7 +2980,7 @@ class TestAccountMoveOutInvoiceOnchanges(AccountTestInvoicingCommon):
                 'debit': 0.0,
                 'credit': 300.0,
                 'account_id': wizard.revenue_accrual_account.id,
-                'reconciled': False
+                'reconciled': True
             },
             {
                 'amount_currency': 120.0,
@@ -2994,7 +2994,7 @@ class TestAccountMoveOutInvoiceOnchanges(AccountTestInvoicingCommon):
                 'debit': 0.0,
                 'credit': 60.0,
                 'account_id': wizard.revenue_accrual_account.id,
-                'reconciled': False
+                'reconciled': True
             },
             {
                 'amount_currency': -600.0,
@@ -3008,7 +3008,7 @@ class TestAccountMoveOutInvoiceOnchanges(AccountTestInvoicingCommon):
                 'debit': 300.0,
                 'credit': 0.0,
                 'account_id': wizard.revenue_accrual_account.id,
-                'reconciled': False
+                'reconciled': True
             },
             {
                 'amount_currency': -120.0,
@@ -3022,7 +3022,7 @@ class TestAccountMoveOutInvoiceOnchanges(AccountTestInvoicingCommon):
                 'debit': 60.0,
                 'credit': 0.0,
                 'account_id': wizard.revenue_accrual_account.id,
-                'reconciled': False
+                'reconciled': True
             },
         ])
 
