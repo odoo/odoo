537363bb6ab0845637c0233dd3bf1442d3cff4dc
Frantisek Binovsky <frbin@odoo.com>
2026-07-31 11:53:02 +0200

    [FIX] {google,microsoft}_account: fix return_url construction
    ISSUE:
    If the `url_return` already contains some query parameters (eg. ?debug=), an
    extra ? will be appended to the query instead of using &
    
    FIX:
    deconstruct the query first and take the existing parameters into account
    
    Task-5485938

--- addons/microsoft_account/controllers/main.py
+++ addons/microsoft_account/controllers/main.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import json
-from urllib.parse import urlencode
+from urllib.parse import urlencode, urlsplit, urlunsplit, parse_qsl
 from werkzeug.exceptions import BadRequest
 
 from odoo import http
@@ -20,6 +20,13 @@ class MicrosoftAuth(http.Controller):
         if (not service or (kw.get('code') and not url_return)):
             raise BadRequest()
 
+        def _add_query_params(url, params):
+            """Merge params into url's query string, whether or not it already has one."""
+            parts = urlsplit(url)
+            query = dict(parse_qsl(parts.query, keep_blank_values=True))
+            query.update(params)
+            return urlunsplit(parts._replace(query=urlencode(query)))
+
         if kw.get('code'):
             base_url = request.httprequest.url_root.strip('/') or request.env.user.get_base_url()
             access_token, refresh_token, ttl = request.env['microsoft.service']._get_microsoft_tokens(
@@ -28,8 +35,8 @@ class MicrosoftAuth(http.Controller):
                 redirect_uri=f'{base_url}/microsoft_account/authentication'
             )
             request.env.user._set_microsoft_auth_tokens(access_token, refresh_token, ttl)
-            return request.redirect("%s?%s" % (url_return, urlencode({"auth_success": "True"})))
+            return request.redirect(_add_query_params(url_return, {"auth_success": "True"}))
         elif kw.get('error'):
-            return request.redirect("%s%s%s" % (url_return, "?error=", kw['error']))
+            return request.redirect(_add_query_params(url_return, {"error": kw['error']}))
         else:
-            return request.redirect("%s%s" % (url_return, "?error=Unknown_error"))
+            return request.redirect(_add_query_params(url_return, {"error": "Unknown_error"}))
