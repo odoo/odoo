499d1d3197dd0f7f8420863a7bc782e4707331fb
Kyrillos <kymag@odoo.com>
2026-08-03 16:57:32 +0200

    [FIX] l10n_fr_pdp: prevent SIRET from being overwritten during PDP registration
    The SIRET value on the contact form should not be overwritten when registering with the PDP.
    This fix from 18.0 to 18.2
    
    task-6442186

--- addons/l10n_fr_pdp/models/res_company.py
+++ addons/l10n_fr_pdp/models/res_company.py
@@ -98,16 +98,22 @@ class ResCompany(models.Model):
         for record in self:
             if not record.pdp_identifier:
                 continue
+            update = {
+                'peppol_eas': '0225',
+                'peppol_endpoint': record.pdp_identifier,  # Will be verified by `_check_peppol_fields` constraint
+            }
             match = PDP_identifier_re.match(record.pdp_identifier or '')
             siren = match and match.group(1)
             if not siren:
                 raise UserError(self.env._("The identifier %s is not valid. The expected format is: SIREN, SIREN_SIRET, SIREN_SIRET_CodeRoutage or SIREN_SuffixeAdressage", record.pdp_identifier))
-            siret = match.group(2)[1:] if match and match.group(2) else False  # Remove `_` at the start
-            record.partner_id.write({
-                'peppol_eas': '0225',
-                'peppol_endpoint': record.pdp_identifier,  # Will be verified by `_check_peppol_fields` constraint
-                'siret': siret or siren,
-            })
+            if not record.siret:
+                siret = match.group(2)[1:] if match and match.group(2) else False  # Remove `_` at the start
+                if siret:
+                    update['siret'] = siret
+                else:
+                    update['company_registry'] = siren
+
+            record.partner_id.write(update)
 
     @api.depends('l10n_fr_pdp_annuaire_start_date', 'account_peppol_proxy_state')
     def _compute_l10n_fr_pdp_registered(self):
