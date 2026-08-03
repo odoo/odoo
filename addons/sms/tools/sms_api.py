c9a7d2c4e5c61761e8699efe42191b0c321d0106
David <dacat@odoo.com>
2026-08-03 12:06:02 +0000

    [FIX] sms: remove unused IAP response that exposed token
    We use to have a chatter response for the IAP code "registrations_needed" that gives in plain text
    the sms account token. However this IAP code doesn't exist anymore.
    
    Task-6425300
    
    closes odoo/odoo#281318
    
    X-original-commit: 85abba8ff74c66a54fd44c58452736cbbf81d958
    Signed-off-by: Ricardo Gomes Rodrigues (rigr) <rigr@odoo.com>
    Signed-off-by: David Catoul (dacat) <dacat@odoo.com>

--- addons/sms/tools/sms_api.py
+++ addons/sms/tools/sms_api.py
@@ -98,7 +98,7 @@ class SmsApi(SmsApiBase):  # TODO RIGR in master: rename SmsApi to SmsApiIAP, an
                   uuid: UUID of the request,
                   state: ONE of: {
                       'success', 'processing', 'server_error', 'unregistered', 'insufficient_credit',
-                      'wrong_number_format', 'duplicate_message', 'country_not_supported', 'registration_needed', 'not_active_db',
+                      'wrong_number_format', 'duplicate_message', 'country_not_supported', 'not_active_db',
                   },
                   credit: Optional: Credits spent to send SMS (provided if the actual price is known)
               }, ...
@@ -118,12 +118,6 @@ class SmsApi(SmsApiBase):  # TODO RIGR in master: rename SmsApi to SmsApiIAP, an
         buy_credits_url = self.env['iap.account'].sudo().get_credits_url(service_name='sms')
         buy_credits = '<a href="{}" target="_blank">{}</a>'.format(buy_credits_url, _("Buy credits."))
 
-        sms_endpoint = self.env['ir.config_parameter'].sudo().get_str('sms.endpoint') or self.DEFAULT_ENDPOINT
-        sms_account_token = self.env['iap.account'].sudo().get('sms').sudo().account_token
-        register_now = f'<a href="{sms_endpoint}/1/account?account_token={sms_account_token}" target="_blank">%s</a>' % (
-            _('Register now.')
-        )
-
         error_dict = super()._get_sms_api_error_messages()
         error_dict.update({
             'unregistered': _("You don't have an eligible IAP account."),
@@ -132,7 +126,6 @@ class SmsApi(SmsApiBase):  # TODO RIGR in master: rename SmsApi to SmsApiIAP, an
             'duplicate_message': _("This SMS has been removed as the number was already used."),
             'country_not_supported': _("The destination country is not supported."),
             'incompatible_content': _("The content of the message violates rules applied by our providers."),
-            'registration_needed': ' '.join([_("Country-specific registration required."), register_now]),
         })
         return error_dict
 
