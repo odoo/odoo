9ad1fc33fd8786faf6fc35e03acec614d78d6f6f
Corentin Heinix (cohe) <cohe@odoo.com>
2026-08-03 10:51:10 +0200

    [FIX] l10n_fr_pdp: ensure notes are present for all FR e-invoicing
    Issue:
    When a company is not connected to the French Electronic Invoicing
    PDP proxy, the generated XML is missing required notes:
    `PMT`, `PMD` and `AAB`
    These notes are required by Factur-X rule `BR-FR-05/BT-22`
    and their absence causes validation errors on the FNFE validator
    
    Cause:
    `_l10n_fr_pdp_get_default_notes` only added the notes when the
    company was using a PDP proxy type
    Non-PDP users sending invoices via other means were excluded,
    which contradicts the French e-invoicing requirements
    
    Steps to reproduce:
    - Install `l10n_fr_pdp` and switch to the FR company
    - In Settings, ensure French Electronic Invoicing is not activated
    - Create and confirm an invoice (any line with tax)
    - Send the invoice and open the generated XML
    Before the fix, the `PMT`, `PMD` and `AAB` notes are missing
    Activating French Electronic Invoicing would include them
    
    opw-6392262
    
    closes odoo/odoo#279966
    
    Signed-off-by: Sven Führ (svfu) <svfu@odoo.com>

--- addons/l10n_fr_pdp/tests/test_xml_ubl_21_fr.py
+++ addons/l10n_fr_pdp/tests/test_xml_ubl_21_fr.py
@@ -16,6 +16,27 @@ class TestL10nFrPdpXml(TestPdpMessagesCommon):
         self._send_patched(invoice)
         self._assert_invoice_ubl_file(invoice, "ubl_21_fr_out_invoice")
 
+    def test_export_invoice_partner_fr_without_pdp(self):
+        """
+        A French Peppol proxy user must have the BR-FR-05 mandatory notes
+        (`PMT`, `PMD`, `AAB`) included in the `ubl_21_fr` exported XML.
+        The export is tested directly rather than via send, as the patched
+        send does not allow verifying the XML content in all versions.
+        """
+        self.env.company._reset_peppol_configuration()
+        # To be able to generate the same XML as in `test_export_invoice_partner_fr`
+        self.env.company.write({
+            'peppol_eas': '0225',
+            'peppol_endpoint': '968515759_96851575905899',
+        })
+
+        invoice = self._create_french_invoice()
+        invoice.action_post()
+
+        wizard = self.create_send_and_print(invoice, checkbox_ubl_cii_xml=True)
+        wizard.action_send_and_print()
+        self._assert_invoice_ubl_file(invoice, "ubl_21_fr_out_invoice")
+
     def test_export_credit_note_partner_fr(self):
         invoice = self._create_french_invoice()
 
