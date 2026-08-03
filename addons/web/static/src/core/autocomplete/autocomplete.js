3a8f8d8a4081492221341fbafeded55aed4752b1
Oussema Heni <ouelh@odoo.com>
2026-08-03 08:59:31 +0000

    [FIX] web: match autocomplete dropdown min-width to input
    Following odoo/odoo#269253, the dropdown width was set to the input
    width, which could truncate long items.
    
    This commit makes the input width the dropdown's minimum width. Longer
    items can still expand the dropdown to remain fully visible.
    
    Task-6431654
    
    X-original-commit: 70867d977b3807d5a80cdaec2ac2a2c055c87251

--- addons/web/static/src/core/autocomplete/autocomplete.js
+++ addons/web/static/src/core/autocomplete/autocomplete.js
@@ -133,7 +133,7 @@ export class AutoComplete extends Component {
             position: "bottom-start",
             onPositioned: (popperEl, solution) => {
                 if (["bottom", "top"].includes(solution.direction)) {
-                    popperEl.style.width = getComputedStyle(this.targetDropdown).width;
+                    popperEl.style.minWidth = getComputedStyle(this.targetDropdown).width;
                 }
             },
             ...this.props.menuPositionOptions,
