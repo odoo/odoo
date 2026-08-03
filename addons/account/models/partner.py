e2c359139ec6580b1cf4f613d12b164aacf2b8ec
Corentin Heinix (cohe) <cohe@odoo.com>
2026-08-03 11:11:03 +0000

    [FIX] account: treat '/' VAT as no VAT in fiscal position matching
    Issue:
    A partner with VAT set to '/' incorrectly matches a fiscal position
    with `VAT required`, instead of one without
    
    The '/' value is the placeholder suggested by the UI to indicate
    that the partner is known to have no VAT, but it was treated as
    a valid VAT by the fiscal position matching logic
    
    Cause:
    `_get_fpos_ranking_functions` uses `_get_vat_valid` to rank
    fiscal positions based on VAT presence
    `_get_vat_valid` returned `True` for any non-empty VAT value,
    including '/'
    The '/' case was not excluded, causing it to be treated
    as a valid VAT number
    
    Steps to reproduce:
    - Install `account`
    - Create two fiscal positions with auto-apply:
      -- Name: FP VAT, VAT required: True, sequence: 1
      -- Name: FP no VAT, VAT required: False, sequence: 2
    - Create a partner with VAT: '/'
    - Create an Invoice for that partner and check the Fiscal Position
    Before the fix, `FP VAT` is selected instead of `FP no VAT`
    
    opw-6204531
    
    closes odoo/odoo#280921
    
    X-original-commit: 3a5a5fb2a19ec6391408aa04e3b6251b795683b2
    Signed-off-by: Sven Führ (svfu) <svfu@odoo.com>
    Signed-off-by: Corentin Heinix (cohe) <cohe@odoo.com>

--- addons/account/models/partner.py
+++ addons/account/models/partner.py
@@ -867,7 +867,7 @@ class ResPartner(models.Model):
     def _get_vat_required_valid(self, company=None):
         """ Hook for determining VAT validity with more complex VAT requirements. (like VIES)"""
         self.ensure_one()
-        return bool(self.vat)
+        return bool(self.vat and self.vat != '/')
 
     # TODO accounting/JCO, seems strange that this address validation logic is only there for pos, and
     # not for standard address management on portal/ecommerce
