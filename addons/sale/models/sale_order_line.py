f3a940ce867694def0da645638e4ff137dc123e2
Tiffany Chang (tic) <tic@odoo.com>
2026-08-03 17:45:47 +0200

    [IMP] purchase, sale: avoid unnecessary chatter note
    Sometimes we don't want to use the default logged notes of certain
    changes in SOs/POs. See corresponding Enterprise PR for more details of
    one of the cases where we override it.
    
    Task-4816396

--- addons/sale/models/sale_order_line.py
+++ addons/sale/models/sale_order_line.py
@@ -1783,7 +1783,7 @@ class SaleOrderLine(models.Model):
             return lines
 
         for line in lines:
-            if not line.display_type and line.state == "sale":
+            if not line.display_type and line.state == "sale" and not self.env.context.get('skip_intercompany_sync'):
                 msg = self.env._("Extra line with %s", line.product_id.display_name or line.name)
                 line.order_id.message_post(body=msg)
 
@@ -1818,7 +1818,7 @@ class SaleOrderLine(models.Model):
         ):
             raise UserError(self.env._("You cannot modify the product of this order line."))
 
-        if "product_uom_qty" in values:
+        if "product_uom_qty" in values and not self.env.context.get('skip_intercompany_sync'):
             precision = self.env["decimal.precision"].precision_get("Product Unit")
             self.filtered(
                 lambda r: (
