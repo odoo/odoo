ac6702eff1dd6ae0d16a9e96dc9c1f3a2f78fb9a
Aaron Bohy <aab@odoo.com>
2026-08-04 13:33:34 +0000

    [FIX] web: fix barcode unit test failing randomly
    This is rather an attempt of fix, as we couldn't reproduce the
    error locally or in a multi build., so we have no guarantee that
    this strenghtens the test.
    
    The test sometimes fails as `.o_crop_icon` can't be found within
    200ms. Before that, we wait for the video element to be ready (we
    rely on a patch of the `isVideoReady` method of the component to
    know that the video is ready). Once it is, the isReady flag in the
    state is set to true and the CropOverlay component renders its
    `o_crop_icon` element.
    
    Our guess is that we may sometimes early return in `isVideoReady`,
    because the component has been destroyed (a new rendering might be
    on the way). To ensure that we don't take that as the ready signal
    in the test, we now only consider that we're ready if isVideoReady
    returned true (i.e. no early return).
    
    runbot error-241798
    
    X-original-commit: 661c46ba77607e3fb65a1c3522db030622f1f777

--- addons/web/static/tests/webclient/barcode/barcode_scanner.test.js
+++ addons/web/static/tests/webclient/barcode/barcode_scanner.test.js
@@ -62,7 +62,9 @@ test("Barcode scanner crop overlay", async () => {
     patchWithCleanup(BarcodeVideoScanner.prototype, {
         async isVideoReady() {
             const result = await super.isVideoReady(...arguments);
-            videoReady.resolve();
+            if (result) {
+                videoReady.resolve();
+            }
             return result;
         },
         onResize(overlayInfo) {
