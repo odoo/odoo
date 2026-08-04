7b6c3578dadec57f8aa1a546c561e9b8861855da
agbr-odoo <agbr@odoo.com>
2026-08-04 12:41:56 +0530

    [FIX] stock{,_dropshipping}: show correct delivery address on reports
    Currently, when the user changes the contact on a dropshipping picking, the
    report incorrectly displays the selected contact's address as the
    delivery address.
    
    Steps to replicate:
    - Install Sales and Inventory.
    - Enable dropshipping in settings.
    - Go to Vendors > Create a new Vendor 'T-shirt company'
    - Add a contact 'T-shirt seller' and save
    - Create a product 'T-shirt' with:
       - Vendor as 'T-shirt Company' price 10
       - Routes: Dropship
    - Create and confirm a sales order for a customer with the t-shirt
    - Confirm the purchase order
    - Open the dropship picking > Print > Delivery Address is for customer
      (expected)
    - Change contact from 'T-shirt company' to 'T-shirt seller' and print again
    
    Observed behavior:
    The delivery address is now changed to vendor instead of customer.
    
    Root cause:
    When the partner on the picking is changed, the system also updates the partner
    on the related stock moves ([1]). As a result, the report uses the updated
    partner from move_id, as shown in [2].
    
    Why the report initially showed the correct customer:
    Initially, the report showed the correct customer because the stock move was
    created with the destination address (customer address) as its partner ([3]).
    Additionally, the compute method excludes dropship moves ([4]), so the partner
    on those moves was not recomputed
    
    Solution:
    When the contact on the picking is changed, do not update the customer address
    on the related stock moves. In dropshipping, users may change the contact to a
    specific supplier's employee for reference purposes.
    
    Preserving the original customer address on the stock move ensures the report
    continues to display the correct delivery address and avoids showing the
    supplier contact's address instead.
    
    This can be done by removing the update of partner_id on the stock move
    when writing partner_id, which is already handled by the compute method [5] and
    method [6] for dropshipping moves.
    
    [1]:
    https://github.com/odoo/odoo/blob/f61a1c6727d34579c764dd6612457f05534f0c0f/addons/stock/models/stock_picking.py#L1161-L1162
    [2]:
    https://github.com/odoo/odoo/blob/f61a1c6727d34579c764dd6612457f05534f0c0f/addons/stock/report/report_stockpicking_operations.xml#L28-L35
    [3]:
    https://github.com/odoo/odoo/blob/f61a1c6727d34579c764dd6612457f05534f0c0f/addons/purchase_stock/models/purchase_order_line.py#L283-L300
    [4]:
    https://github.com/odoo/odoo/blob/f61a1c6727d34579c764dd6612457f05534f0c0f/addons/purchase_stock/models/stock_move.py#L39-L42
    [5]:
    https://github.com/odoo/odoo/blob/4291b65978287b737a2e094d3122c20ed8564852/addons/stock/models/stock_move.py#L386-L389
    [6]:
    https://github.com/odoo/odoo/blob/4291b65978287b737a2e094d3122c20ed8564852/addons/purchase_stock/models/stock_move.py#L39-L42
    
    opw-6405304

--- addons/stock/models/stock_picking.py
+++ addons/stock/models/stock_picking.py
@@ -1165,8 +1165,6 @@ class StockPicking(models.Model):
             after_vals['location_id'] = vals['location_id']
         if vals.get('location_dest_id'):
             after_vals['location_dest_id'] = vals['location_dest_id']
-        if 'partner_id' in vals:
-            after_vals['partner_id'] = vals['partner_id']
         if after_vals:
             self.move_ids.filtered(lambda move: move.location_dest_usage != 'inventory').write(after_vals)
         if vals.get('move_ids'):
