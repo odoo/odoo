941afe45baf84100e25949a0e0619010b3f93cbe
Walid (wasa) <wasa@odoo.com>
2026-08-04 13:41:17 +0000

    [FIX] html_editor: consider zero-width chars as non-whitespace neighbors
    Problem:
    `isVisibleTextNode` fails to check the space visibility in case it is
    preccedded with a `feff`.
    
    Cause:
    The final check uses `visibleCharRegex` on the preceding node, which
    excludes zero-width chars like `feff`. But `feff` is not whitespace,
    so it shouldn't make the adjacent space collapse either.
    
    Solution:
    Check for non-whitespace instead of visibility on the preceding node.
    
    task-6397398
    
    closes odoo/odoo#282138
    
    X-original-commit: 6a1cd7d166bf14336afd47693a4d270f750e9e40
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Walid Sahli (wasa) <wasa@odoo.com>

--- addons/html_editor/static/src/utils/dom_info.js
+++ addons/html_editor/static/src/utils/dom_info.js
@@ -266,7 +266,7 @@ export function isVisibleTextNode(testedNode) {
         return false;
     }
     // Preceding is whitespace or following is whitespace: invisible
-    return visibleCharRegex.test(preceding.textContent);
+    return !isWhitespace(preceding);
 }
 
 /**
