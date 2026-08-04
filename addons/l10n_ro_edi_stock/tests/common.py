b572f2ce65472fa27b0ee1de5893ba776518e374
Arbaaz-Khan-Tech <asurk@odoo.com>
2026-08-04 10:53:20 +0530

    [IMP] l10n_ro_edi_stock: support dropship operations
    Before this commit:
    Dropship operations were not supported by the Romanian eTransport
    integration. As a result, eTransport declarations could not be
    generated for dropshipping flows.
    
    After this commit:
    eTransport XML files can now be generated for international and
    domestic B2C dropship operations. Proper errors are also raised when
    attempting to generate declarations for B2B dropship operations.
    
    task-6391489

--- addons/l10n_ro_edi_stock/tests/common.py
+++ addons/l10n_ro_edi_stock/tests/common.py
@@ -1,3 +1,4 @@
+from odoo.tools import misc
 from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 from odoo.addons.stock_account.tests.test_anglo_saxon_valuation_reconciliation_common import ValuationReconciliationTestCommon
 
@@ -49,3 +50,33 @@ class TestL10nRoEdiStockCommon(ValuationReconciliationTestCommon):
             'product_tmpl_id': product_tmpl.id if product_tmpl else product.product_tmpl_id.id,
             'new_quantity': new_quantity,
         }).change_product_qty()
+
+    def _assert_picking_state(self, picking, state=False, amt_documents=0, enabled_fields=('enable', 'fields_readonly')):
+        self.assertEqual(picking.l10n_ro_edi_stock_state, state)
+        if amt_documents > 0:
+            self.assertTrue(picking.l10n_ro_edi_stock_document_ids)
+            self.assertEqual(len(picking.l10n_ro_edi_stock_document_ids), amt_documents)
+        else:
+            self.assertFalse(picking.l10n_ro_edi_stock_document_ids)
+
+        for suffix in ('enable', 'enable_send', 'enable_fetch', 'enable_amend', 'fields_readonly'):
+            field_value = picking[f"l10n_ro_edi_stock_{suffix}"]
+            self.assertEqual(field_value, suffix in enabled_fields)
+
+    def _assert_etransport_document(self, document, filename):
+        with misc.file_open(f'{self.test_module}/tests/test_files/{filename}.xml', 'rb') as file:
+            expected_document = file.read()
+
+        expected_tree = self.get_xml_tree_from_string(expected_document)
+
+        if 'intrastat_code_id' in self.env['product.product']._fields:
+            nsmap = expected_tree.nsmap
+            nsmap['etr'] = nsmap[None]
+            nsmap.pop(None)
+            for tag in expected_tree.xpath('//*/etr:bunuriTransportate', namespaces=nsmap):
+                tag.attrib['codTarifar'] = self.default_intrastat_code.code
+
+        self.assertXmlTreeEqual(
+            self.get_xml_tree_from_string(document.attachment_id.raw),
+            expected_tree,
+        )
