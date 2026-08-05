601c7dba49802ee03af14e6b9b9860c84a70e6a7
yoelf <yoelf@odoo.com>
2026-08-05 14:46:13 +0000

    [FIX] website_sale: align shop search domains
    Shop product lookup and facet computations used different searchable fields. In particular, facets searched raw website_description HTML, so CSS tokens could match a large part of a catalog even when the displayed result set was small.
    
    Use one shared field list for product lookup and facets, replace internal and raw website descriptions with the eCommerce description.
    
    opw-6391984
    
    X-original-commit: efdb9e30e38cae224f42190013d342accd686937

--- addons/website_sale/models/product_template.py
+++ addons/website_sale/models/product_template.py
@@ -1303,6 +1303,19 @@ class ProductTemplate(models.Model):
         ]
 
     @api.model
+    def _get_website_sale_search_fields(self, search_in_description=True):
+        search_fields = [
+            "name",
+            "variants_default_code",
+        ]
+        if search_in_description:
+            search_fields.append("description_ecommerce")
+        search_fields.extend(("attribute_line_ids.value_ids.name", "product_tag_ids.name"))
+        if search_in_description:
+            search_fields.append("description_sale")
+        return search_fields
+
+    @api.model
     def _search_get_detail(self, website, order, options):  # noqa: ARG002
         domains = [website.sale_product_domain()]
         category = options.get("category")
@@ -1332,15 +1345,7 @@ class ProductTemplate(models.Model):
             domains.append([("list_price", "<=", max_price)])
         if attribute_value_dict:
             domains.extend(self._get_attribute_value_domain(attribute_value_dict))
-        search_fields = [
-            "name",
-            "default_code",
-            "variants_default_code",
-            "description_ecommerce",
-            "attribute_line_ids.value_ids.name",
-            "product_tag_ids.name",
-            "description_sale",
-        ]
+        search_fields = self._get_website_sale_search_fields(options.get("displayDescription", True))
         fetch_fields = ["id", "name", "website_url", "description_ecommerce", "description_sale"]
         mapping = {
             "name": {"name": "name", "type": "text", "match": True},
