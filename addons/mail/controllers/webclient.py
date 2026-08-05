0d88d859ba5d0e71b8c8c3ebcf27d3c8253349f3
Maryam Kia <maki@odoo.com>
2026-08-05 12:36:07 +0000

    [FIX] mail: cleaning thread data
    This change cleans up the requested data from `/mail/thread/data` route,
    ensuring it aligns with what is actually needed depending on the user and thread.
    
    part of task-6452761
    
    closes odoo/odoo#284350
    
    X-original-commit: 814d3cd0e21e75408e5daaa8023e4d0b33ac08a0
    Signed-off-by: Maryam Kia (maki) <maki@odoo.com>

--- addons/mail/controllers/webclient.py
+++ addons/mail/controllers/webclient.py
@@ -94,10 +94,15 @@ class WebclientController(ThreadController):
                 thread = request.env[params["thread_model"]].browse(params["thread_id"])
                 store.add(thread, {"hasReadAccess": False, "hasWriteAccess": False}, as_thread=True)
             else:
+                request_list = params["request_list"]
+                if not request.env.user._is_internal() or not thread.sudo(False).with_context(
+                    allowed_company_ids=[]
+                ).has_access("read"):
+                    request_list = []
                 store.add(
                     thread,
                     "_store_thread_fields",
-                    fields_params={"request_list": params["request_list"], "chatter_fields": True},
+                    fields_params={"request_list": request_list, "chatter_fields": True},
                     as_thread=True,
                 )
         if name == "res.partner":
