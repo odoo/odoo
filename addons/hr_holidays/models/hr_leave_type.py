9f12eb477d23724a6c98bd9cffbed9a1627f8023
Ahmed Naem (Ahnae) <ahnae@odoo.com>
2026-08-05 09:30:16 +0200

    [IMP] hr_holidays: expose create_calendar_meeting on hr.leave.type
    The `create_calendar_meeting` field on `hr.leave.type` allows users to
    choose if leave requests created with a given time off type generate a
    corresponding entry in the Calendar app. However, this field was not
    displayed on the form view.
    
    This commit adds `create_calendar_meeting` to the `hr.leave.type` form view
    inside the configuration section, along with dedicated help text explaining
    its behavior.

--- addons/hr_holidays/models/hr_leave_type.py
+++ addons/hr_holidays/models/hr_leave_type.py
@@ -31,7 +31,11 @@ class HolidaysType(models.Model):
     name = fields.Char('Time Off Type', required=True, translate=True)
     sequence = fields.Integer(default=100,
         help='The type with the smallest sequence is the default value in time off request')
-    create_calendar_meeting = fields.Boolean(string="Display Time Off in Calendar", default=True)
+    create_calendar_meeting = fields.Boolean(
+        string="Display Time Off in Calendar",
+        default=True,
+        help="If this field is checked, every leave request of this type will have a corresponding entry in the calendar application. There will be no entry if this stays unchecked."
+    )
     color = fields.Integer(string='Color', help="The color selected here will be used in every screen with the time off type.")
     icon_id = fields.Many2one('ir.attachment', string='Cover Image', domain="[('res_model', '=', 'hr.leave.type'), ('res_field', '=', 'icon_id')]")
     active = fields.Boolean('Active', default=True,
