6fad786e592f4834e91f72332e9760dbee3e6ec3
Martin Doyen <mdoy@odoo.com>
2026-08-05 14:34:50 +0000

    [FIX] l10n_fr_pdp: fix Factur-X e-invoicing format import
    When importing new documents from Peppol into the database, we determine
    whether they are self-billed by checking a Type Code in the XML file.
    
    Factur-X is an hybrid format where the XML is embedded inside a PDF.
    
    Currently, we are not extracting the XML before searching for that Type
    Code, and it leads to an error that prevents the document from being
    imported correctly:
    
    - V17, V18: An empty invoice is created and linked with the attachment.
    - V19+: Only the attachment is created.
    
    Additionnaly, we only check for InvoiceTypeCode or CreditNoteTypeCode,
    but the CII XML format embedded inside the hybrid Factur-X format use
    TypeCode instead.
    
    This PR aims at fixing both these issues.
    
    Ticket: opw-6417682
    X-original-commit: 714c5240d8d6adcbcb0be4648263791a33bbfb8b

--- addons/account_peppol/models/account_edi_proxy_user.py
+++ addons/account_peppol/models/account_edi_proxy_user.py
@@ -322,6 +322,14 @@ class Account_Edi_Proxy_ClientUser(models.Model):
                         ),
                     )
 
+    def _get_type_code(self, files_data):
+        file_data = files_data[0]
+        # Fallback to avoid issues with large EmbeddedDocumentBinaryObject
+        if file_data['xml_tree'] is None:
+            file_data['raw'] = REMOVE_EMBEDDED_DOCUMENT_BINARY_OBJECT_RE.sub(b'', file_data['raw'])
+            file_data['xml_tree'] = self.env['account.move']._get_xml_tree(file_data)
+        return file_data['xml_tree'].findtext('.//{*}InvoiceTypeCode') or file_data['xml_tree'].findtext('.//{*}CreditNoteTypeCode')
+
     def _peppol_import_invoice(self, attachment, peppol_state, uuid, journal=None):
         """Save new documents in an accounting journal, when one is specified on the company.
 
@@ -333,21 +341,17 @@ class Account_Edi_Proxy_ClientUser(models.Model):
         """
         self.ensure_one()
 
-        file_data = self.env['account.move']._to_files_data(attachment)[0]
-
-        # Fallback to avoid issues with large EmbeddedDocumentBinaryObject
-        if file_data['xml_tree'] is None:
-            file_data['raw'] = REMOVE_EMBEDDED_DOCUMENT_BINARY_OBJECT_RE.sub(b'', file_data['raw'])
-            file_data['xml_tree'] = self.env['account.move']._get_xml_tree(file_data)
-
-        # Self-billed invoices are invoices which your customer creates on your behalf and sends you via Peppol.
-        # In this case, the invoice needs to be created as an out_invoice in a self-billing sale journal.
-        # If no self-billing sale journal was found, the invoice will be created in a regular sale journal.
-        # 329/527: Self-billing invoice; 261: Self-billing credit note
         is_self_billed = False
-        if file_data['xml_tree'].findtext('.//{*}InvoiceTypeCode') in ['389', '527'] or file_data['xml_tree'].findtext('.//{*}CreditNoteTypeCode') == '261':
-            is_self_billed = True
-
+        files_data = self.env['account.move']._to_files_data(attachment)
+        try:
+            # Self-billed invoices are invoices which your customer creates on your behalf and sends you via Peppol.
+            # In this case, the invoice needs to be created as an out_invoice in a self-billing sale journal.
+            # If no self-billing sale journal was found, the invoice will be created in a regular sale journal.
+            # 389/527: Self-billing invoice; 261: Self-billing credit note
+            type_code = self._get_type_code(files_data)
+            is_self_billed = type_code in ['389', '527', '261']
+        except Exception:
+            _logger.exception("The Peppol XML file is invalid or empty for attachment ID %s", attachment.id)
         if not is_self_billed:
             journal = journal or self.company_id.peppol_purchase_journal_id
             move_type = 'in_invoice'
@@ -384,7 +388,7 @@ class Account_Edi_Proxy_ClientUser(models.Model):
             move.is_in_extractable_state = False
 
         try:
-            move._extend_with_attachments([file_data], new=True)
+            move._extend_with_attachments(files_data, new=True)
             move._autopost_bill()
         except Exception:
             _logger.exception("Unexpected error occurred during the import of bill with id %s", move.id)
