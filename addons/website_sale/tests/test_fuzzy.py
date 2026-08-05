fb33e20005cf2a61c00d1641dfb04bd681125ac7
yoelf <yoelf@odoo.com>
2026-08-05 14:46:13 +0000

    [FIX] website_sale: align shop search domains
    Shop product lookup and facet computations used different searchable fields. In particular, facets searched raw website_description HTML, so CSS tokens could match a large part of a catalog even when the displayed result set was small.
    
    Use one shared field list for product lookup and facets, replace internal and raw website descriptions with the eCommerce description.
    
    opw-6391984
    
    closes odoo/odoo#285045
    
    X-original-commit: efdb9e30e38cae224f42190013d342accd686937
    Signed-off-by: Youssef El Fatihi (yoelf) <yoelf@odoo.com>

--- addons/website_sale/tests/test_fuzzy.py
+++ addons/website_sale/tests/test_fuzzy.py
@@ -1,9 +1,12 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from odoo.fields import Command
 from odoo.tests import tagged
 
-from odoo.addons.http_routing.tests.common import MockRequest
+from odoo.addons.http_routing.tests.common import MockRequest as HttpRoutingMockRequest
 from odoo.addons.product.tests.common import ProductVariantsCommon
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo.addons.website_sale.tests.common import MockRequest
 
 
 @tagged("-at_install", "post_install")
@@ -15,6 +18,74 @@ class TestFuzzy(ProductVariantsCommon):
     )
 
     _test_user_name = 'Test Product Manager'
+
+    def test_shop_and_search_bar_use_same_search_fields(self):
+        website = self.env.ref("base.default_website")
+        product_tmpl = self.env['product.template']
+        expected_fields = [
+            'name', 'variants_default_code',
+            'description_ecommerce', 'attribute_line_ids.value_ids.name',
+            'product_tag_ids.name', 'description_sale',
+        ]
+        options = {
+            'displayImage': False, 'displayDescription': True, 'displayExtraLink': False,
+            'displayDetail': False, 'display_currency': website.currency_id, 'allowFuzzy': False,
+        }
+
+        search_detail = product_tmpl._search_get_detail(website, 'name asc', options)
+        self.assertEqual(search_detail['search_fields'], expected_fields)
+
+        with MockRequest(self.env, website=website):
+            shop_domain = WebsiteSale()._get_shop_domain('unique_term', None, {})
+
+        searched_products = product_tmpl
+        for field in ('name', 'description_sale', 'description_ecommerce'):
+            product = product_tmpl.create({
+                'name': f'Product searched by {field}',
+                field: 'unique_term',
+                'is_published': True,
+            })
+            searched_products |= product
+
+        product_by_reference = product_tmpl.create({
+            'name': 'Product searched by reference',
+            'is_published': True,
+        })
+        product_by_reference.product_variant_id.default_code = 'unique_term'
+        searched_products |= product_by_reference
+
+        visible_tag = self.env['product.tag'].create({
+            'name': 'unique_term',
+            'visible_to_customers': True,
+        })
+        product_by_tag = product_tmpl.create({
+            'name': 'Product searched by tag',
+            'is_published': True,
+            'product_tag_ids': [Command.link(visible_tag.id)],
+        })
+        searched_products |= product_by_tag
+        self.assertEqual(searched_products.filtered_domain(shop_domain), searched_products)
+
+        for field in ('description', 'website_description'):
+            product = product_tmpl.create({
+                'name': f'Product not searched by {field}',
+                field: 'unique_term',
+                'is_published': True,
+            })
+            self.assertFalse(product.filtered_domain(shop_domain), f'Should not search in {field}')
+
+        hidden_tag = self.env['product.tag'].create({
+            'name': 'hidden_unique_term',
+            'visible_to_customers': False,
+        })
+        product_by_hidden_tag = product_tmpl.create({
+            'name': 'Product not searched by hidden tag',
+            'is_published': True,
+            'product_tag_ids': [Command.link(hidden_tag.id)],
+        })
+        with MockRequest(self.env, website=website):
+            hidden_tag_domain = WebsiteSale()._get_shop_domain('hidden_unique_term', None, {})
+        self.assertFalse(product_by_hidden_tag.filtered_domain(hidden_tag_domain))
 
     def test_variant_default_code(self):
         website = self.env.ref("base.default_website")
@@ -95,7 +166,7 @@ class TestFuzzy(ProductVariantsCommon):
         result_count, results, _ = website.with_context(website_id=website.id)._search_with_fuzzy('product_template', 'Some tag', 0, 5, 'name asc', options)
         self.assertEqual(result_count, 1)
         # Needed because `_get_additionnal_combination_info` uses request.pricelist & request.fiscal_position
-        with MockRequest(self.env, website=website) as request:
+        with HttpRoutingMockRequest(self.env, website=website) as request:
             request.pricelist = self.env['product.pricelist'].create({
                 'name': 'Some pricelist',
             })
