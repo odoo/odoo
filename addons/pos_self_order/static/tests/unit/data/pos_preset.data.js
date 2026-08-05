123e5d32fb6abd7805473b1f4b2cef29af2eb268
abhk-odoo <abhk@odoo.com>
2026-08-05 12:48:57 +0530

    [FIX] pos_self_order: auto select single available preset
    In this commit:
    - When only one preset remains available after filtering table
    identifier in self-order mode, automatically select it and skip
    the preset selection page.
    - This avoids showing a location selection page when there is
    no actual choice available to the customer.
    
    Task:6217791
    
    Enterprise PR : https://github.com/odoo/enterprise/pull/122979
    
    X-original-commit: 9ac0e2477f17b7f6710b38c49a001e959e5b3313

--- addons/pos_self_order/static/tests/unit/data/pos_preset.data.js
+++ addons/pos_self_order/static/tests/unit/data/pos_preset.data.js
@@ -3,7 +3,7 @@ import { PosPreset } from "@point_of_sale/../tests/unit/data/pos_preset.data";
 
 patch(PosPreset.prototype, {
     _load_pos_data_fields() {
-        return [...super._load_pos_data_fields(), "mail_template_id"];
+        return [...super._load_pos_data_fields(), "mail_template_id", "service_at"];
     },
 });
 
