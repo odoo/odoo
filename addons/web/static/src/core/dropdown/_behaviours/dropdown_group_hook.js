0f0635fecf8b663ffeaf11c5634d4e2f77ff2746
Michaël Mattiello <mcm@odoo.com>
2026-08-05 11:01:21 +0200

    [REF] web: remove dropdown_group from env
    This commit removes `DROPDOWN_GROUP` from env. It is replaced by a plugin.

--- addons/web/static/src/core/dropdown/_behaviours/dropdown_group_hook.js
+++ addons/web/static/src/core/dropdown/_behaviours/dropdown_group_hook.js
@@ -1,13 +1,69 @@
-import { useEffect } from "@odoo/owl";
-import { DROPDOWN_GROUP } from "@web/core/dropdown/dropdown_group";
-import { useEnv } from "@web/owl2/utils";
+import { onWillDestroy, Plugin, providePlugins, t, useConfig, usePlugin } from "@odoo/owl";
+import { services } from "@web/core/services";
+
+const GROUPS = new Map();
+
+function getGroup(id) {
+    if (!GROUPS.has(id)) {
+        GROUPS.set(id, {
+            group: new Set(),
+            count: 0,
+        });
+    }
+    GROUPS.get(id).count++;
+    return GROUPS.get(id).group;
+}
+
+function removeGroup(id) {
+    const groupData = GROUPS.get(id);
+    groupData.count--;
+    if (groupData.count <= 0) {
+        GROUPS.delete(id);
+    }
+}
+
+class DropdownGroupPlugin extends Plugin {
+    /** @private */
+    groupId = useConfig("groupId", t.string().optional());
+
+    /** @private */
+    items = new Set();
+    isInGroup = true;
+
+    get isOpen() {
+        return this.isInGroup && [...this.items].some((dropdown) => dropdown.isOpen);
+    }
+
+    setup() {
+        if (this.groupId) {
+            this.group = getGroup(this.groupId);
+            onWillDestroy(() => {
+                removeGroup(this.groupId);
+            });
+        }
+    }
+
+    add(state) {
+        this.items.add(state);
+    }
+
+    remove(state) {
+        this.items.delete(state);
+    }
+}
+
+class GlobalDropdownGroupPlugin extends Plugin {
+    static id = "DropdownGroupPlugin";
+
+    isInGroup = false;
+    isOpen = false;
+
+    add() {}
+    remove() {}
+}
+services.add(GlobalDropdownGroupPlugin);
 
 /**
- * @typedef {{
- *  isInGroup: boolean;
- *  readonly isOpen: boolean;
- * }} DropdownGroupState
- *
  * @typedef {(typeof import("../dropdown").dropdownProps)["state"]} DropdownState
  */
 
@@ -19,22 +75,12 @@ import { useEnv } from "@web/owl2/utils";
  * @param {DropdownState} state
  */
 export function useDropdownGroup(state) {
-    const env = useEnv();
-
-    /** @type {DropdownGroupState} */
-    const group = {
-        isInGroup: DROPDOWN_GROUP in env,
-        get isOpen() {
-            return this.isInGroup && [...env[DROPDOWN_GROUP]].some((dropdown) => dropdown.isOpen);
-        },
-    };
-
-    if (group.isInGroup) {
-        useEffect(() => {
-            env[DROPDOWN_GROUP].add(state);
-            return () => env[DROPDOWN_GROUP].delete(state);
-        });
-    }
-
+    const group = usePlugin(DropdownGroupPlugin);
+    group.add(state);
+    onWillDestroy(() => group.remove(state));
     return group;
+}
+
+export function provideDropdownGroup(groupId) {
+    providePlugins([DropdownGroupPlugin], { groupId });
 }
