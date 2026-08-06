LAIDOUDI33/LAID_ODOO

[IMP] mail: read more props as signals (2)
Continue reading props as signals (`this.x()`) across more mail
components via `propComputed`/`propSignal`: the chatter scheduled
message and follower list, polls, link previews, the chat bubble
and the small leaf components (country flag, date section,
priority, message link preview list).

A callback handed to a slotless list child is made `props.static`
and bound once in the caller: a fresh closure every render makes the
prop compare unequal and forces every item to re-render, whereas a
stable reference lets unchanged items skip it. Done for
`AttachmentList.unlinkAttachment` and
`ScheduledMessage.onScheduledMessageChanged`.

`ChatBubblePreview.chatWindow` is chained down as a `propSignal`: the
parent already holds the value as a signal and now passes the signal
itself, so the preview reacts to it directly.

Click handlers take the record they act on as a parameter, captured
at render right before the handler (a scoped `recordAtRender` local)
and passed as `(ev, { recordAtRender })`. A signal read inside a
handler resolves at call time, so if the value changed since the
render the user saw, the handler would act on a record different from
the one displayed; the captured record keeps the action tied to what
was clicked. A callback that runs later, such as a dialog
confirmation, receives the same captured record instead of re-reading
the signal.

A callback prop type shared between a child and the parents that pass
it is defined once as an exported `(store) => t.function(...)` factory
and reused rather than duplicated at each declaration.

addons/mail/static/src/core/web/activity.js
@@ -1,6 +1,7 @@
 import { useAttachmentUploader } from "@mail/core/common/attachment_uploader_hook";
 import { ActivityAssignPopover } from "@mail/core/web/activity_assign_popover";
 import { ActivityMailTemplate } from "@mail/core/web/activity_mail_template";
+import { onActivityChangedType } from "@mail/core/web/activity_types";
 import { ActivityMarkAsDone } from "@mail/core/web/activity_markasdone_popover";
 import { computeDelay, getMsToTomorrow } from "@mail/utils/common/dates";
 import { AvatarCard } from "@mail/core/web/avatar_card/avatar_card";
@@ -23,7 +24,10 @@ export class Activity extends Component {
         super.setup();
         this.store = useService("mail.store");
         this.activity = propComputed("activity", t.instanceOf(this.store["mail.activity"].Class));
-        this.onActivityChanged = props.static("onActivityChanged", t.function([]));
+        this.onActivityChanged = props.static(
+            "onActivityChanged",
+            onActivityChangedType(this.store)
+        );
         this.reloadParentView = props.static("reloadParentView", t.function([]));
         this.assignPopover = usePopover(ActivityAssignPopover, { position: "bottom" });
         this.markDonePopover = usePopover(ActivityMarkAsDone, { position: "right" });
@@ -32,12 +36,7 @@ export class Activity extends Component {
             this.updateDelayAtNight();
         });
         onWillUnmount(() => browser.clearTimeout(this.updateDelayMidnightTimeout));
-        this.thread = computed(() =>
-            this.store["mail.thread"].insert({
-                model: this.activity().res_model,
-                id: this.activity().res_id,
-            })
-        );
+        this.thread = computed(() => this.activity().thread);
         this.attachmentUploader = useAttachmentUploader(this.thread);
     }
 
@@ -94,40 +93,52 @@ export class Activity extends Component {
         });
     }
 
-    async onFileUploaded(data) {
-        const activity = this.activity();
-        const thread = this.thread();
+    /**
+     * @param {Object} data
+     * @param {{ activityAtRender: import("models").Activity }} param1
+     */
+    async onFileUploaded(data, { activityAtRender }) {
+        const thread = activityAtRender.thread;
         const { id: attachmentId } = await this.attachmentUploader.uploadData(data, {
-            activity,
+            activity: activityAtRender,
         });
-        await activity.markAsDone([attachmentId]);
-        this.onActivityChanged(thread);
-        await thread.fetchNewMessages();
+        await activityAtRender.markAsDone([attachmentId]);
+        this.onActivityChanged({ thread });
+        await thread?.fetchNewMessages();
     }
 
-    onClickAvatar(ev) {
-        if (!this.activity().user_id) {
+    /**
+     * @param {MouseEvent} ev
+     * @param {{ activityAtRender: import("models").Activity }} param1
+     */
+    onClickAvatar(ev, { activityAtRender }) {
+        if (!activityAtRender.user_id) {
             return;
         }
         const target = ev.currentTarget;
         if (!this.avatarCard.isOpen) {
             this.avatarCard.open(target, {
-                id: this.activity().user_id.id,
+                id: activityAtRender.user_id.id,
                 model: "res.users",
             });
         }
     }
 
-    async edit() {
-        const thread = this.thread();
-        await this.activity().edit();
-        this.onActivityChanged(thread);
+    /**
+     * @param {MouseEvent} ev
+     * @param {{ activityAtRender: import("models").Activity }} param1
+     */
+    async edit(ev, { activityAtRender }) {
+        const thread = activityAtRender.thread;
+        await activityAtRender.edit();
+        this.onActivityChanged({ thread });
     }
 
     /**
      * @param {MouseEvent} ev
+     * @param {{ activityAtRender: import("models").Activity }} param1
      */
-    onClick(ev) {
-        this.store.handleClickOnLink(ev, this.thread());
+    onClick(ev, { activityAtRender }) {
+        this.store.handleClickOnLink(ev, activityAtRender.thread);
     }
 }

addons/mail/static/src/core/web/activity_list_popover.js
@@ -1,8 +1,9 @@
 import { ActivityListPopoverItem } from "@mail/core/web/activity_list_popover_item";
-import { useOnChange } from "@mail/utils/common/hooks";
+import { onActivityChangedType } from "@mail/core/web/activity_types";
+import { propComputed, useOnChange } from "@mail/utils/common/hooks";
 import { compareDatetime } from "@mail/utils/common/misc";
 
-import { Component, props, types } from "@odoo/owl";
+import { Component, computed, props, types } from "@odoo/owl";
 
 import { useService } from "@web/core/utils/hooks";
 
@@ -12,19 +13,29 @@ export class ActivityListPopover extends Component {
 
     setup() {
         super.setup();
-        this.props = props({
-            activityIds: types.array(types.number()),
-            close: types.function([]),
-            defaultActivityTypeId: types.number().optional(),
-            onActivityChanged: types.function([]),
-            resId: types.number(),
-            /** Ids of record selection used to schedule activities in batch; it must include resId. */
-            resIds: types.array(types.number()).optional(),
-            resModel: types.string(),
-        });
         this.store = useService("mail.store");
+        this.activityIds = propComputed("activityIds", types.array(types.number()));
+        this.close = props.static("close", types.function([]));
+        this.defaultActivityTypeId = propComputed(
+            "defaultActivityTypeId",
+            types.number().optional()
+        );
+        this.onActivityChanged = props.static(
+            "onActivityChanged",
+            onActivityChangedType(this.store)
+        );
+        this.resId = propComputed("resId", types.number());
+        /** Ids of record selection used to schedule activities in batch; it must include resId. */
+        this.resIds = propComputed("resIds", types.array(types.number()).optional());
+        this.resModel = propComputed("resModel", types.string());
+        this.thread = computed(() =>
+            this.store["mail.thread"].insert({
+                model: this.resModel(),
+                id: this.resId(),
+            })
+        );
         useOnChange(
-            () => [this.props.activityIds],
+            () => [this.activityIds()],
             (activityIds) => this.store.fetchStoreData("mail.activity", { ids: activityIds })
         );
     }
@@ -33,19 +44,23 @@ export class ActivityListPopover extends Component {
         /** @type {import("models").Activity[]} */
         const allActivities = Object.values(this.store["mail.activity"].records);
         return allActivities
-            .filter((activity) => this.props.activityIds.includes(activity.id))
+            .filter((activity) => this.activityIds().includes(activity.id))
             .sort((a, b) => compareDatetime(a.date_deadline, b.date_deadline) || a.id - b.id);
     }
 
-    onClickAddActivityButton() {
+    /**
+     * @param {MouseEvent} ev
+     * @param {{ threadAtRender: import("models").Thread }} param1
+     */
+    onClickAddActivityButton(ev, { threadAtRender }) {
         this.store
             .scheduleActivity(
-                this.props.resModel,
-                this.props.resIds ? this.props.resIds : [this.props.resId],
-                this.props.defaultActivityTypeId
+                threadAtRender.model,
+                this.resIds() ? this.resIds() : [threadAtRender.id],
+                this.defaultActivityTypeId()
             )
-            .then(() => this.props.onActivityChanged());
-        this.props.close();
+            .then(() => this.onActivityChanged({ thread: threadAtRender }));
+        this.close();
     }
 
     get doneActivities() {

odoo/odoo

addons/mail/static/src/core/web/activity.js
@@ -1,6 +1,7 @@
 import { useAttachmentUploader } from "@mail/core/common/attachment_uploader_hook";
 import { ActivityAssignPopover } from "@mail/core/web/activity_assign_popover";
 import { ActivityMailTemplate } from "@mail/core/web/activity_mail_template";
+import { onActivityChangedType } from "@mail/core/web/activity_types";
 import { ActivityMarkAsDone } from "@mail/core/web/activity_markasdone_popover";
 import { computeDelay, getMsToTomorrow } from "@mail/utils/common/dates";
 import { AvatarCard } from "@mail/core/web/avatar_card/avatar_card";
@@ -23,7 +24,10 @@ export class Activity extends Component {
         super.setup();
         this.store = useService("mail.store");
         this.activity = propComputed("activity", t.instanceOf(this.store["mail.activity"]));
-        this.onActivityChanged = useProps.static("onActivityChanged", t.function([]));
+        this.onActivityChanged = useProps.static(
+            "onActivityChanged",
+            onActivityChangedType(this.store)
+        );
         this.reloadParentView = useProps.static("reloadParentView", t.function([]));
         this.assignPopover = usePopover(ActivityAssignPopover, { position: "bottom" });
         this.markDonePopover = usePopover(ActivityMarkAsDone, { position: "right" });
@@ -32,12 +36,7 @@ export class Activity extends Component {
             this.updateDelayAtNight();
         });
         onWillUnmount(() => browser.clearTimeout(this.updateDelayMidnightTimeout));
-        this.thread = computed(() =>
-            this.store["mail.thread"].insert({
-                model: this.activity().res_model,
-                id: this.activity().res_id,
-            })
-        );
+        this.thread = computed(() => this.activity().thread);
         this.attachmentUploader = useAttachmentUploader(this.thread);
     }
 
@@ -94,40 +93,52 @@ export class Activity extends Component {
         });
     }
 
-    async onFileUploaded(data) {
-        const activity = this.activity();
-        const thread = this.thread();
+    /**
+     * @param {Object} data
+     * @param {{ activityAtRender: import("models").Activity }} param1
+     */
+    async onFileUploaded(data, { activityAtRender }) {
+        const thread = activityAtRender.thread;
         const { id: attachmentId } = await this.attachmentUploader.uploadData(data, {
-            activity,
+            activity: activityAtRender,
         });
-        await activity.markAsDone([attachmentId]);
-        this.onActivityChanged(thread);
-        await thread.fetchNewMessages();
+        await activityAtRender.markAsDone([attachmentId]);
+        this.onActivityChanged({ thread });
+        await thread?.fetchNewMessages();
     }
 
-    onClickAvatar(ev) {
-        if (!this.activity().user_id) {
+    /**
+     * @param {MouseEvent} ev
+     * @param {{ activityAtRender: import("models").Activity }} param1
+     */
+    onClickAvatar(ev, { activityAtRender }) {
+        if (!activityAtRender.user_id) {
             return;
         }
         const target = ev.currentTarget;
         if (!this.avatarCard.isOpen) {
             this.avatarCard.open(target, {
-                id: this.activity().user_id.id,
+                id: activityAtRender.user_id.id,
                 model: "res.users",
             });
         }
     }
 
-    async edit() {
-        const thread = this.thread();
-        await this.activity().edit();
-        this.onActivityChanged(thread);
+    /**
+     * @param {MouseEvent} ev
+     * @param {{ activityAtRender: import("models").Activity }} param1
+     */
+    async edit(ev, { activityAtRender }) {
+        const thread = activityAtRender.thread;
+        await activityAtRender.edit();
+        this.onActivityChanged({ thread });
     }
 
     /**
      * @param {MouseEvent} ev
+     * @param {{ activityAtRender: import("models").Activity }} param1
      */
-    onClick(ev) {
-        this.store.handleClickOnLink(ev, this.thread());
+    onClick(ev, { activityAtRender }) {
+        this.store.handleClickOnLink(ev, activityAtRender.thread);
     }
 }

addons/mail/static/src/core/web/activity_list_popover.js
@@ -1,7 +1,9 @@
 import { ActivityListPopoverItem } from "@mail/core/web/activity_list_popover_item";
+import { onActivityChangedType } from "@mail/core/web/activity_types";
+import { propComputed } from "@mail/utils/common/hooks";
 import { compareDatetime } from "@mail/utils/common/misc";
 
-import { Component, signal, types, useOnChange, useProps } from "@odoo/owl";
+import { Component, computed, signal, types, useOnChange, useProps } from "@odoo/owl";
 
 import { useService } from "@web/core/utils/hooks";
 
@@ -13,19 +15,29 @@ export class ActivityListPopover extends Component {
 
     setup() {
         super.setup();
-        this.props = useProps({
-            activityIds: types.array(types.number()),
-            close: types.function([]),
-            defaultActivityTypeId: types.number().optional(),
-            onActivityChanged: types.function([]),
-            resId: types.number(),
-            /** Ids of record selection used to schedule activities in batch; it must include resId. */
-            resIds: types.array(types.number()).optional(),
-            resModel: types.string(),
-        });
         this.store = useService("mail.store");
+        this.activityIds = propComputed("activityIds", types.array(types.number()));
+        this.close = useProps.static("close", types.function([]));
+        this.defaultActivityTypeId = propComputed(
+            "defaultActivityTypeId",
+            types.number().optional()
+        );
+        this.onActivityChanged = useProps.static(
+            "onActivityChanged",
+            onActivityChangedType(this.store)
+        );
+        this.resId = propComputed("resId", types.number());
+        /** Ids of record selection used to schedule activities in batch; it must include resId. */
+        this.resIds = propComputed("resIds", types.array(types.number()).optional());
+        this.resModel = propComputed("resModel", types.string());
+        this.thread = computed(() =>
+            this.store["mail.thread"].insert({
+                model: this.resModel(),
+                id: this.resId(),
+            })
+        );
         useOnChange(
-            () => [this.props.activityIds],
+            () => [this.activityIds()],
             (activityIds) => this.store.fetchStoreData("mail.activity", { ids: activityIds })
         );
     }
@@ -34,19 +46,23 @@ export class ActivityListPopover extends Component {
         /** @type {import("models").Activity[]} */
         const allActivities = Object.values(this.store["mail.activity"].records);
         return allActivities
-            .filter((activity) => this.props.activityIds.includes(activity.id))
+            .filter((activity) => this.activityIds().includes(activity.id))
             .sort((a, b) => compareDatetime(a.date_deadline, b.date_deadline) || a.id - b.id);
     }
 
-    onClickAddActivityButton() {
+    /**
+     * @param {MouseEvent} ev
+     * @param {{ threadAtRender: import("models").Thread }} param1
+     */
+    onClickAddActivityButton(ev, { threadAtRender }) {
         this.store
             .scheduleActivity(
-                this.props.resModel,
-                this.props.resIds ? this.props.resIds : [this.props.resId],
-                this.props.defaultActivityTypeId
+                threadAtRender.model,
+                this.resIds() ? this.resIds() : [threadAtRender.id],
+                this.defaultActivityTypeId()
             )
-            .then(() => this.props.onActivityChanged());
-        this.props.close();
+            .then(() => this.onActivityChanged({ thread: threadAtRender }));
+        this.close();
     }
 
     get doneActivities() {