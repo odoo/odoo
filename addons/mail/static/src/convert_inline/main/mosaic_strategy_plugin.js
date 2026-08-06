0e27f622bf1d6b4b9e65fb722f6679dad22e9c40
abd-msyukyu-odoo <abd@odoo.com>
2026-08-06 16:26:18 +0200

    wip

--- addons/mail/static/src/convert_inline/main/mosaic_strategy_plugin.js
+++ addons/mail/static/src/convert_inline/main/mosaic_strategy_plugin.js
@@ -16,7 +16,18 @@ export class MosaicStrategyPlugin extends Plugin {
         ) {
             return defaultEmailNodeArguments;
         }
-        // investigate children of referenceNode per row
+        // request for mosaic cells providers => check ancestor snippet class
+        // s_comparisons -> card (columns) -> card-body | card-footer = cells
+        // approximation: align card-bodies and card-footers in a table of 2 rows
+        // that is a better approximation than a table of 2 columns 1 row.
+        //
+        // s_masonry_block -> data-name="Block" (cells) => position each block
+        // using the boundingclientrect
+        //
+        // neutralize isTableContainer and isHybridFluidContainer if a provider
+        // exists. Compute final table dimensions using desktop geometry
+        // compute final mobile dimensions using mobile geometry?
+
         return defaultEmailNodeArguments;
     }
     fillMosaicContainer() {}
