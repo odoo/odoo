568bf986122860cf0013b2ed925c19cee17c268b
Ruben Gomes <rugo@odoo.com>
2026-08-06 10:06:01 +0200

    [REF] structure

---
+++ odoo/addons/test_orm_new/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Test ORM',
+    'category': 'Hidden/Tests',
+    'description': """A module to test the ORM.""",
+    'depends': ['base'],
+    'data': [],
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
