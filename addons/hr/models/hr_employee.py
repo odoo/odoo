9087a162a03a2cf74c7f37b9f62240e7b977460b
romc-odoo <romc@odoo.com>
2026-08-06 11:44:40 +0000

    [FIX] hr: fix current_version_id compute method
    The compute method should not depend whether or not the employee is active or not.
    
    closes odoo/odoo#281044
    
    X-original-commit: f4510925cc73c00433c6dd8f76105aad498aeaa6
    Related: odoo/enterprise#127171
    Signed-off-by: Jurgen Gjini (jugj) <jugj@odoo.com>
    Signed-off-by: Romain Carlier (romc) <romc@odoo.com>

--- addons/hr/models/hr_employee.py
+++ addons/hr/models/hr_employee.py
@@ -717,7 +717,7 @@ class HrEmployee(models.Model):
             current_location = employee.exceptional_location_id or employee[dayfield]
             employee.work_location_type = current_location.location_type
 
-    @api.depends('version_ids.date_version', 'version_ids.active', 'active')
+    @api.depends('version_ids.date_version', 'version_ids.active')
     def _compute_current_version_id(self):
         today = fields.Date.context_today(self)
         for employee in self:
