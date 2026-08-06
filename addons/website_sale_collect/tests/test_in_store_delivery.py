4e665e3b5a5befd4e4d7d6c6c78e7c1dda42a3c8
Zachary (zavan) <zavan@odoo.com>
2026-08-06 16:01:13 +0200

    [FIX] website_sale_collect: limit pickup locations by company
    # How to reproduce
    - Have two company A & B
    - Install the Sales + Click & Collect modules
    - Create a Warehouse for each Company
    - Enable both company A & B
    - Go to the Delivery Methods & select "Pick up in store"
    - Add two Stores, one for each Company
    - Go to a website that is limited to Company A
    - Go to the page of a product with Click & Collect Available
    > Both Stores are available
    
    - Select the Store associated for Company B
    - Click on "Choose this location"
    
    # Issue
    We get an error notification saying :
    "Invalid Operation. Uh-oh! You’ve got some company inconsistencies here: ..."
    
    # Cause
    When displaying the available stores, we don't filter them using the
    website's company.
    
    When we select a store with a different company than the website, the
    write on `sale.order` will trigger a call to `_check_company`, that will
    detect an inconsistency between the SO's company and the warehouse's
    company
    
    # Proposed solution
    We limit the available locations to warehouses that have the same
    company as the cart's SO.
    
    opw-6417309

--- addons/website_sale_collect/tests/test_in_store_delivery.py
+++ addons/website_sale_collect/tests/test_in_store_delivery.py
@@ -4,6 +4,7 @@ from unittest.mock import patch
 from odoo.tests import tagged
 
 from odoo.addons.payment.tests.http_common import PaymentHttpCommon
+from odoo.addons.website_sale.tests.common import MockRequest
 from odoo.addons.website_sale_collect.controllers.delivery import InStoreDelivery
 from odoo.addons.website_sale_collect.tests.common import ClickAndCollectCommon
 
@@ -24,3 +25,22 @@ class TestInStoreDeliveryController(PaymentHttpCommon, ClickAndCollectCommon):
             self.make_jsonrpc_request(url, {'product_id': 1})
         count_so_after = self.env['sale.order'].search_count([])
         self.assertEqual(count_so_after, count_so_before)
+
+    def test_limit_pickup_locations_to_website_company(self):
+        """Make sure that the warehouses proposed as a pickup location for an order
+        have the same company as the order, to prevent company inconsistencies
+        """
+        other_company = self.env['res.company'].create({'name': 'Other Company'})
+        self.in_store_dm.warehouse_ids += self._create_warehouse(company_id=other_company.id)
+        self.assertEqual(len(self.in_store_dm.warehouse_ids), 2)
+
+        url = self._build_url('/website_sale/get_pickup_locations')
+        result = self.make_jsonrpc_request(url, {'product_id': 1})['pickup_locations']
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0]['id'], self.warehouse.id)
+
+        so = self._create_in_store_delivery_order()
+        with MockRequest(self.env, website=self.website, sale_order_id=so.id):
+            result = self.InStoreController.website_sale_get_pickup_locations(product_id=1)['pickup_locations']
+            self.assertEqual(len(result), 1)
+            self.assertEqual(result[0]['id'], self.warehouse.id)
