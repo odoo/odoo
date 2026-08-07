4dbd825be3698cd3ee43f30936757ae0a9d3d403
adip-odoo <adip@odoo.com>
2026-08-07 12:22:38 +0000

    [FIX] pos: Load product attribute data before demo data
    Steps to Reproduce:
    1. Install the PoS and Sales modules without demo data.
    2. Settings> Enable Variants.
    3. Sales > Products > Attributes > Delete "Brand" attribute.
    4. Create a Clothes Shop and open the register.
    5. Load the Sample products.
    
    Error:
    ParseError - while parsing /home/odoo/src/odoo/saas-19.4/addons/product/data/product_attribute_demo.xml:5, somewhere inside
    
    Cause:
    The product_attribute_demo.xml file references attributes that do not exist
    when the demo data is loaded, which raises an error.
    
    Before 19.4, the attributes were defined in the same file.
    After this commit https://github.com/odoo/odoo/commit/56942bcf34785e869c7648cf100c8818c5da0b6d,
    the attributes are defined separately in the product_attribute_data.xml file.
    
    Fix:
    This commit loads the data file before, ensure the referenced attributes are
    available when the demo file is processed.
    
    sentry-7640019804
    
    closes odoo/odoo#283240
    
    X-original-commit: 1a801005628a3953356c5844c69d721b9453e7ce
    Signed-off-by: Loukas Wets (lowe) <lowe@odoo.com>
    Signed-off-by: Aditi Patel (adip) <adip@odoo.com>

--- addons/pos_restaurant/models/pos_config.py
+++ addons/pos_restaurant/models/pos_config.py
@@ -142,7 +142,10 @@ class PosConfig(models.Model):
         self.ensure_one()
         convert.convert_file(self._env_with_clean_context(), 'pos_restaurant', 'data/scenarios/restaurant_category_data.xml', idref=None, mode='init', noupdate=True)
         if with_demo_data:
-            self._load_product_demo_data([('data/product_attribute_demo.xml', 'product.pa_sides')])
+            self._load_product_demo_data([
+                ("data/product_attribute_data.xml", "product.pa_sides"),
+                ("data/product_attribute_demo.xml", "product.pav_brand_adidas"),
+            ])
             convert.convert_file(self._env_with_clean_context(), 'pos_restaurant', 'data/scenarios/restaurant_demo_data.xml', idref=None, mode='init', noupdate=True)
             self.write({
                 'show_product_images': False,
