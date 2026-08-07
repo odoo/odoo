odoo/odoo

commit efc2788dfccd13ee6feb309430ff57e49664ff97

[IMP] payment: replace exceptions bubbling with state-based error reporting
This commit introduces a significant change in the paradigm of how the
payment engine handles processing errors.

The previous mechanism relied on exceptions bubbling to signal
processing errors to the callers (payment form, web client, cron). This
approach posed two problems: it was necessary to catch and handle any
exception raised by the payment engine, and transactions were not
persisted if an error was encountered and the cursor rolled back.

The new approach consists of catching exceptions as soon as possible to
prevent them from discarding database changes, setting the transaction
to the 'error' state, and saving the error message on the transaction.
It allows tracking transactions whose processing failed, and simplifies
the handling of payment errors for applications integrating with online
payments (e.g., Subscriptions).

A number of collateral changes are made to enable the state-based
reporting and streamline the integration of new payment providers:

- A new `_send_api_request` util method is introduced on both the
  `payment.provider` and `payment.transaction` models to handle all API
  requests for the providers and centralize the management of processing
  errors. A series of specific hook methods are introduced to allow
  providers to configure the requests: `_build_request_url`,
  `_build_request_headers`, `_build_request_auth`,
  `_parse_response_error`, and `_parse_response_content`.
- The processing-oriented methods (`*_from_notification_data`) are
  replaced by a new set of core and hook methods to centralize all the
  common logic involving error management. The overrides are kept to a
  bare minimum, consisting of only the provider-specific handling of
  transactions.
- Provider-specific overrides of core `payment` methods are no longer
  required to call the `super()` method. This is made possible by moving

addons/payment_authorize/models/payment_transaction.py
@@ -1,17 +1,16 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import logging
 import pprint
 
 from odoo import _, models
-from odoo.exceptions import UserError, ValidationError
 
 from odoo.addons.payment import utils as payment_utils
+from odoo.addons.payment.logging import get_payment_logger
 from odoo.addons.payment_authorize import const
 from odoo.addons.payment_authorize.models.authorize_request import AuthorizeAPI
 
 
-_logger = logging.getLogger(__name__)
+_logger = get_payment_logger(__name__)
 
 
 class PaymentTransaction(models.Model):
@@ -26,9 +25,8 @@ class PaymentTransaction(models.Model):
         :return: The dict of provider-specific processing values
         :rtype: dict
         """
-        res = super()._get_specific_processing_values(processing_values)
         if self.provider_code != 'authorize':
-            return res
+            return super()._get_specific_processing_values(processing_values)
 
         return {
             'access_token': payment_utils.generate_access_token(
@@ -53,58 +51,41 @@ class PaymentTransaction(models.Model):
             return authorize_API.auth_and_capture(self, opaque_data=opaque_data)
 
     def _send_payment_request(self):
-        """ Override of payment to send a payment request to Authorize.
-
-        Note: self.ensure_one()
-
-        :return: None
-        :raise: UserError if the transaction is not linked to a token
-        """
-        super()._send_payment_request()
+        """Override of `payment` to send a payment request to Authorize."""
         if self.provider_code != 'authorize':
-            return
-
-        if not self.token_id.authorize_profile:
-            raise UserError("Authorize.Net: " + _("The transaction is not linked to a token."))
+            return super()._send_payment_request()
 
         authorize_API = AuthorizeAPI(self.provider_id)
         if self.provider_id.capture_manually:
             res_content = authorize_API.authorize(self, token=self.token_id)
             _logger.info(
-                "authorize request response for transaction with reference %s:\n%s",
+                "authorize request response for transaction %s:\n%s",
                 self.reference, pprint.pformat(res_content)
             )
         else:
             res_content = authorize_API.auth_and_capture(self, token=self.token_id)
             _logger.info(
-                "auth_and_capture request response for transaction with reference %s:\n%s",
+                "auth_and_capture request response for transaction %s:\n%s",
                 self.reference, pprint.pformat(res_content)
             )
-        self._handle_notification_data('authorize', {'response': res_content})
+        self._process('authorize', {'response': res_content})
 
-    def _send_refund_request(self, amount_to_refund=None):
-        """ Override of payment to send a refund request to Authorize.
-
-        Note: self.ensure_one()
-
-        :param float amount_to_refund: The amount to refund
-        :return: The refund transaction created to process the refund request.
-        :rtype: recordset of `payment.transaction`
-        """
-        self.ensure_one()
-
+    def _send_refund_request(self):
+        """Override of `payment` to send a refund request to Authorize."""
         if self.provider_code != 'authorize':
-            return super()._send_refund_request(amount_to_refund=amount_to_refund)
+            return super()._send_refund_request()
 
         authorize_api = AuthorizeAPI(self.provider_id)
-        tx_details = authorize_api.get_transaction_details(self.provider_reference)
+        tx_details = authorize_api.get_transaction_details(
+            self.source_transaction_id.provider_reference
+        )
         if 'err_code' in tx_details:  # Could not retrieve the transaction details.
-            raise ValidationError("Authorize.Net: " + _(
+            self._set_error(_(
                 "Could not retrieve the transaction details. (error code: %(error_code)s; error_details: %(error_message)s)",
                 error_code=tx_details['err_code'], error_message=tx_details.get('err_msg'),
             ))
+            return
 
-        refund_tx = self.env['payment.transaction']
         tx_status = tx_details.get('transaction', {}).get('transactionStatus')
         if tx_status in const.TRANSACTION_STATUS_MAPPING['voided']:
             # The payment has been voided from Authorize.net side before we could refund it.
@@ -112,8 +93,7 @@ class PaymentTransaction(models.Model):
         elif tx_status in const.TRANSACTION_STATUS_MAPPING['refunded']:
             # The payment has been refunded from Authorize.net side before we could refund it. We
             # create a refund tx on Odoo to reflect the move of the funds.
-            refund_tx = super()._send_refund_request(amount_to_refund=amount_to_refund)
-            refund_tx._set_done()
+            self._set_done()
             # Immediately post-process the transaction as the post-processing will not be
             # triggered by a customer browsing the transaction from the portal.
             self.env.ref('payment.cron_post_process_payment_tx')._trigger()
@@ -121,114 +101,79 @@ class PaymentTransaction(models.Model):
             if tx_status in const.TRANSACTION_STATUS_MAPPING['authorized']:
                 # The payment has not been settled on Authorize.net yet. It must be voided rather
                 # than refunded. Since the funds have not moved yet, we don't create a refund tx.
-                res_content = authorize_api.void(self.provider_reference)
-                tx_to_process = self
+                res_content = authorize_api.void(self.source_transaction_id.provider_reference)
             else:
                 # The payment has been settled on Authorize.net side. We can refund it.
-                refund_tx = super()._send_refund_request(amount_to_refund=amount_to_refund)
-                rounded_amount = round(amount_to_refund, self.currency_id.decimal_places)
+                rounded_amount = round(self.amount, self.currency_id.decimal_places)
                 res_content = authorize_api.refund(
                     self.provider_reference, rounded_amount, tx_details
                 )
-                tx_to_process = refund_tx
             _logger.info(
-                "refund request response for transaction with reference %s:\n%s",
+                "refund request response for transaction %s:\n%s",
                 self.reference, pprint.pformat(res_content)
             )
-            data = {'reference': tx_to_process.reference, 'response': res_content}
-            tx_to_process._handle_notification_data('authorize', data)
+            data = {'reference': self.reference, 'response': res_content}
+            self._process('authorize', data)
         else:
-            raise ValidationError("Authorize.net: " + _(
-                "The transaction is not in a status to be refunded. (status: %(status)s, details: %(message)s)",
+            err_msg = _(
+                "The transaction is not in a status to be refunded."
+                " (status: %(status)s, details: %(message)s)",
                 status=tx_status, message=tx_details.get('messages', {}).get('message'),
-            ))
-        return refund_tx
+            )
+            _logger.warning(err_msg)
+            self._set_error(err_msg)
 
-    def _send_capture_request(self, amount_to_capture=None):
-        """ Override of `payment` to send a capture request to Authorize. """
-        child_capture_tx = super()._send_capture_request(amount_to_capture=amount_to_capture)
+    def _send_capture_request(self):
+        """Override of `payment` to send a capture request to Authorize."""
         if self.provider_code != 'authorize':
-            return child_capture_tx
+            return super()._send_capture_request()
 
         authorize_API = AuthorizeAPI(self.provider_id)
         rounded_amount = round(self.amount, self.currency_id.decimal_places)
-        res_content = authorize_API.capture(self.provider_reference, rounded_amount)
+        res_content = authorize_API.capture(
+            self.source_transaction_id.provider_reference, rounded_amount
+        )
         _logger.info(
-            "capture request response for transaction with reference %s:\n%s",
+            "capture request response for transaction %s:\n%s",
             self.reference, pprint.pformat(res_content)
         )
-        self._handle_notification_data('authorize', {'response': res_content})
+        self._process('authorize', {'response': res_content})
 
-        return child_capture_tx
-
-    def _send_void_request(self, amount_to_void=None):
-        """ Override of payment to send a void request to Authorize. """
-        child_void_tx = super()._send_void_request(amount_to_void=amount_to_void)
+    def _send_void_request(self):
+        """Override of `payment` to send a void request to Authorize."""
         if self.provider_code != 'authorize':
-            return child_void_tx
+            return super()._send_void_request()
 
         authorize_API = AuthorizeAPI(self.provider_id)
         res_content = authorize_API.void(self.provider_reference)
         _logger.info(
-            "void request response for transaction with reference %s:\n%s",
+            "void request response for transaction %s:\n%s",
             self.reference, pprint.pformat(res_content)
         )
-        self._handle_notification_data('authorize', {'response': res_content})
+        self._process('authorize', {'response': res_content})
 
-        return child_void_tx
-
-    def _get_tx_from_notification_data(self, provider_code, notification_data):
-        """ Find the transaction based on Authorize.net data.
-
-        :param str provider_code: The code of the provider that handled the transaction
-        :param dict notification_data: The notification data sent by the provider
-        :return: The transaction if found
-        :rtype: recordset of `payment.transaction`
-        """
-        tx = super()._get_tx_from_notification_data(provider_code, notification_data)
-        if provider_code != 'authorize' or len(tx) == 1:
-            return tx
-
-        reference = notification_data.get('reference')
-        tx = self.search([('reference', '=', reference), ('provider_code', '=', 'authorize')])
-        if not tx:
-            raise ValidationError(
-                "Authorize.Net: " + _("No transaction found matching reference %s.", reference)
-            )
-        return tx
-
-    def _compare_notification_data(self, notification_data):
-        """ Override of `payment` to compare the transaction based on Authorize data.
-
-        :param dict notification_data: The notification data sent by the provider.
-        :return: None
-        :raise ValidationError: If the transaction's amount and currency don't match the
-            notification data.
-        """
+    def _extract_amount_data(self, payment_data):
+        """Override of `payment` to extract the amount and currency from the payment data."""
         if self.provider_code != 'authorize':
-            return super()._compare_notification_data(notification_data)
+            return super()._extract_amount_data(payment_data)
 
         tx_details = AuthorizeAPI(self.provider_id).get_transaction_details(
-            notification_data.get('response', {}).get('x_trans_id')
+            payment_data.get('response', {}).get('x_trans_id')
         )
         amount = tx_details.get('transaction', {}).get('authAmount')
         # Authorize supports only one currency per account.
         currency_code = self.provider_id.available_currency_ids[0].name
-        self._validate_amount_and_currency(amount, currency_code)
+        return {
+            'amount': float(amount),
+            'currency_code': currency_code,
+        }
 
-    def _process_notification_data(self, notification_data):
-        """ Override of payment to process the transaction based on Authorize data.
-
-        Note: self.ensure_one()
-
-        :param dict notification_data: The notification data sent by the provider
-        :return: None
-        """
-        super()._process_notification_data(notification_data)
+    def _apply_updates(self, payment_data):
+        """Override of `payment` to update the transaction based on the payment data."""
         if self.provider_code != 'authorize':
-            return
+            return super()._apply_updates(payment_data)
 
-        response_content = notification_data.get('response')
+        response_content = payment_data.get('response')
 
         # Update the provider reference.
         self.provider_reference = response_content.get('x_trans_id')
@@ -246,14 +191,10 @@ class PaymentTransaction(models.Model):
             status_type = response_content.get('x_type').lower()
             if status_type in ('auth_capture', 'prior_auth_capture'):
                 self._set_done()
-                if self.tokenize and not self.token_id:
-                    self._authorize_tokenize()
             elif status_type == 'auth_only':
                 self._set_authorized()
-                if self.tokenize and not self.token_id:
-                    self._authorize_tokenize()
                 if self.operation == 'validation':
-                    self._send_void_request()  # In last step because it processes the response.
+                    self._void()  # In last step because it processes the response.
             elif status_type == 'void':
                 if self.operation == 'validation':  # Validation txs are authorized and then voided
                     self._set_done()  # If the refund went through, the validation tx is confirmed
@@ -271,57 +212,40 @@ class PaymentTransaction(models.Model):
         else:  # Error / Unknown code
             error_code = response_content.get('x_response_reason_text')
             _logger.info(
-                "received data with invalid status (%(status)s) and error code (%(err)s) for "
-                "transaction with reference %(ref)s",
+                "Received data with invalid status (%(status)s) and error code (%(err)s) for "
+                "transaction %(ref)s.",
                 {
                     'status': status_code,
                     'err': error_code,
                     'ref': self.reference,
                 },
             )
-            self._set_error(
-                "Authorize.Net: " + _(
-                    "Received data with status code \"%(status)s\" and error code \"%(error)s\"",
-                    status=status_code, error=error_code
-                )
-            )
+            self._set_error(_(
+                "Received data with status code \"%(status)s\" and error code \"%(error)s\".",
+                status=status_code, error=error_code
+            ))
 
-    def _authorize_tokenize(self):
-        """ Create a token for the current transaction.
+    def _extract_token_values(self, payment_data):
+        """Override of `payment` to extract the token values from the payment data."""
+        if self.provider_code != 'authorize':
+            return super()._extract_token_values(payment_data)
 
-        Note: self.ensure_one()
-
-        :return: None
-        """
-        self.ensure_one()
+        if self.token_id:
+            return {}
 
         authorize_API = AuthorizeAPI(self.provider_id)
         cust_profile = authorize_API.create_customer_profile(
             self.partner_id, self.provider_reference
         )
         _logger.info(
-            "create_customer_profile request response for transaction with reference %s:\n%s",
+            "create_customer_profile request response for transaction %s:\n%s",
             self.reference, pprint.pformat(cust_profile)
         )
-        if cust_profile:
-            token = self.env['payment.token'].create({
-                'provider_id': self.provider_id.id,
-                'payment_method_id': self.payment_method_id.id,
-                'payment_details': cust_profile.get('payment_details'),
-                'partner_id': self.partner_id.id,
-                'provider_ref': cust_profile.get('payment_profile_id'),
-                'authorize_profile': cust_profile.get('profile_id'),
-            })
-            self.write({
-                'token_id': token.id,
-                'tokenize': False,
-            })
-            _logger.info(
-                "created token with id %(token_id)s for partner with id %(partner_id)s from "
-                "transaction with reference %(ref)s",
-                {
-                    'token_id': token.id,
-                    'partner_id': self.partner_id.id,
-                    'ref': self.reference,
-                },
-            )
+        if not cust_profile or 'payment_profile_id' not in cust_profile:  # Failed to fetch data.
+            return {}
+
+        return {
+            'payment_details': cust_profile.get('payment_details'),
+            'provider_ref': cust_profile['payment_profile_id'],
+            'authorize_profile': cust_profile.get('profile_id'),
+        }

addons/payment_authorize/tests/test_authorize.py
@@ -53,10 +53,29 @@ class AuthorizeTest(AuthorizeCommon):
             'odoo.addons.payment_authorize.models.authorize_request.AuthorizeAPI'
             '.get_transaction_details', return_value={'transaction': {'authAmount': self.amount}},
         ):
-            source_tx._handle_notification_data('authorize', {
+            source_tx._process('authorize', {
                 'response': {
                     'x_response_code': '1',
                     'x_type': 'void',
                 },
             })
         self.assertEqual(source_tx.state, 'cancel')
+
+    @mute_logger('odoo.addons.payment_authorize.models.payment_transaction')
+    def test_extract_token_values_maps_fields_correctly(self):
+        tx = self._create_transaction('direct')
+        payment_data = {
+            'payment_details': '1234',
+            'payment_profile_id': '123456789',
+            'profile_id': '987654321',
+        }
+        with patch(
+            'odoo.addons.payment_authorize.models.authorize_request.AuthorizeAPI'
+            '.create_customer_profile', return_value=payment_data,
+        ):
+            token_values = tx._extract_token_values({})
+        self.assertDictEqual(token_values, {
+            'payment_details': '1234',
+            'provider_ref': '123456789',
+            'authorize_profile': '987654321',
+        })

addons/account_payment/models/payment_transaction.py
@@ -66,7 +66,7 @@ class PaymentTransaction(models.Model):
     #=== BUSINESS METHODS - PAYMENT FLOW ===#
 
     @api.model
-    def _compute_reference_prefix(self, provider_code, separator, **values):
+    def _compute_reference_prefix(self, separator, **values):
         """ Compute the reference prefix from the transaction values.
 
         If the `values` parameter has an entry with 'invoice_ids' as key and a list of (4, id, O) or
@@ -75,7 +75,6 @@ class PaymentTransaction(models.Model):
 
         Note: This method should be called in sudo mode to give access to documents (INV, SO, ...).
 
-        :param str provider_code: The code of the provider handling the transaction
         :param str separator: The custom separator used to separate data references
         :param dict values: The transaction values used to compute the reference prefix. It should
                             have the structure {'invoice_ids': [(X2M command), ...], ...}.
@@ -92,7 +91,7 @@ class PaymentTransaction(models.Model):
                 if name := values.get('name_next_installment'):
                     prefix = name
                 return prefix
-        return super()._compute_reference_prefix(provider_code, separator, **values)
+        return super()._compute_reference_prefix(separator, **values)
 
     #=== BUSINESS METHODS - POST-PROCESSING ===#
 
@@ -123,9 +122,8 @@ class PaymentTransaction(models.Model):
 
             if tx.payment_id:
                 message = _(
-                    "The payment related to the transaction with reference %(ref)s has been"
-                    " posted: %(link)s",
-                    ref=tx.reference,
+                    "The payment related to transaction %(ref)s has been posted: %(link)s",
+                    ref=tx._get_html_link(),
                     link=tx.payment_id._get_html_link(),
                 )
                 tx._log_message_on_linked_documents(message)

commit 334ac43beae0f64c45fe2239fd97dca0d35aecc3

[FIX] payment_authorize: tokenize authorized payment before voiding it
Issue:
---
Authorize payment tokenization doesn't work.

Steps:
1- Setup authorize payment provider.
2- Using portal page, add a new payment method for the user.

The created payment method is not saved.

Cause:
---
The issue was introduced in efc2788dfccd13ee6feb309430ff57e49664ff97.
Before that, we were calling `_tokenize` before voiding the tx.
In that PR, the `_tokenize` call was moved to `_process()`, after
`_apply_updates()`.
So now what happens is that we void the tx, then call `_tokenize()`.
Inside tokenize we try to create a customer profile, which fails
because the tx is already voided.

Fix:
---
We can fix it by calling `_tokenize()` once before voiding the tx.

The redundant tokenize call inside the general payment tx `_process`
is rendered ineffective by two safeguards:
1- There is a check for `tx.tokenize`, which neutralizes double
tokenization:

https://github.com/odoo/odoo/blob/fffd987cc98d1ea0cd04e24dda2ed8b64a219cdc/addons/payment/models/payment_transaction.py#L754-L755
https://github.com/odoo/odoo/blob/fffd987cc98d1ea0cd04e24dda2ed8b64a219cdc/addons/payment/models/payment_transaction.py#L893-L896

2- If `token_id` is already set, no token value is returned:

https://github.com/odoo/odoo/blob/fffd987cc98d1ea0cd04e24dda2ed8b64a219cdc/addons/payment_authorize/models/payment_transaction.py#L237-L243

opw-6426847

addons/payment_authorize/models/payment_transaction.py
@@ -196,6 +196,8 @@ class PaymentTransaction(models.Model):
                 self._set_done()
             elif status_type == 'auth_only':
                 self._set_authorized()
+                if self.tokenize:
+                    self._tokenize(payment_data)  # Tokenize before voiding
                 if self.operation == 'validation':
                     self._void()  # In last step because it processes the response.
             elif status_type == 'void':

addons/payment_authorize/tests/test_authorize.py
@@ -95,3 +95,41 @@ class AuthorizeTest(AuthorizeCommon):
             'provider_ref': '123456789',
             'authorize_profile': '987654321',
         })
+
+    def test_validation_tx_is_tokenized_before_being_voided(self):
+        """Test that the tokenization request is sent before the void request.
+        The customer profile can only be created from a transaction that has not been voided yet,
+        so the tokenization must happen before the validation transaction is voided.
+        """
+        tx = self._create_transaction("direct", operation="validation", tokenize=True)
+        call_order = []
+
+        def tokenize(*args, **kwargs):
+            call_order.append("tokenize")
+            tx.tokenize = False
+
+        def void(*args, **kwargs):
+            call_order.append("void")
+
+        with (
+            patch(
+                "odoo.addons.payment.models.payment_transaction.PaymentTransaction._tokenize",
+                side_effect=tokenize,
+            ),
+            patch(
+                "odoo.addons.payment.models.payment_transaction.PaymentTransaction._void",
+                side_effect=void,
+            ),
+        ):
+            tx._process(
+                "authorize",
+                {
+                    "response": {
+                        "x_response_code": "1",
+                        "x_type": "auth_only",
+                        "x_trans_id": "test_trans_id",
+                    }
+                },
+            )
+
+        self.assertEqual(call_order, ["tokenize", "void"])
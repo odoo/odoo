334ac43beae0f64c45fe2239fd97dca0d35aecc3
Mahdi Alijani (malj) <malj@odoo.com>
2026-08-06 16:50:21 +0200

    [FIX] payment_authorize: tokenize authorized payment before voiding it
    Issue:
    ---
    Authorize payment tokenization doesn't work.
    
    Steps:
    1- Setup authorize payment provider.
    2- Using portal page, add a new payment method for the user.
    
    The created payment method is not saved.
    
    Cause:
    ---
    The issue was introduced in efc2788dfccd13ee6feb309430ff57e49664ff97.
    Before that, we were calling `_tokenize` before voiding the tx.
    In that PR, the `_tokenize` call was moved to `_process()`, after
    `_apply_updates()`.
    So now what happens is that we void the tx, then call `_tokenize()`.
    Inside tokenize we try to create a customer profile, which fails
    because the tx is already voided.
    
    Fix:
    ---
    We can fix it by calling `_tokenize()` once before voiding the tx.
    
    The redundant tokenize call inside the general payment tx `_process`
    is rendered ineffective by two safeguards:
    1- There is a check for `tx.tokenize`, which neutralizes double
    tokenization:
    
    https://github.com/odoo/odoo/blob/fffd987cc98d1ea0cd04e24dda2ed8b64a219cdc/addons/payment/models/payment_transaction.py#L754-L755
    https://github.com/odoo/odoo/blob/fffd987cc98d1ea0cd04e24dda2ed8b64a219cdc/addons/payment/models/payment_transaction.py#L893-L896
    
    2- If `token_id` is already set, no token value is returned:
    
    https://github.com/odoo/odoo/blob/fffd987cc98d1ea0cd04e24dda2ed8b64a219cdc/addons/payment_authorize/models/payment_transaction.py#L237-L243
    
    opw-6426847

--- addons/payment_authorize/tests/test_authorize.py
+++ addons/payment_authorize/tests/test_authorize.py
@@ -95,3 +95,41 @@ class AuthorizeTest(AuthorizeCommon):
             'provider_ref': '123456789',
             'authorize_profile': '987654321',
         })
+
+    def test_validation_tx_is_tokenized_before_being_voided(self):
+        """Test that the tokenization request is sent before the void request.
+        The customer profile can only be created from a transaction that has not been voided yet,
+        so the tokenization must happen before the validation transaction is voided.
+        """
+        tx = self._create_transaction("direct", operation="validation", tokenize=True)
+        call_order = []
+
+        def tokenize(*args, **kwargs):
+            call_order.append("tokenize")
+            tx.tokenize = False
+
+        def void(*args, **kwargs):
+            call_order.append("void")
+
+        with (
+            patch(
+                "odoo.addons.payment.models.payment_transaction.PaymentTransaction._tokenize",
+                side_effect=tokenize,
+            ),
+            patch(
+                "odoo.addons.payment.models.payment_transaction.PaymentTransaction._void",
+                side_effect=void,
+            ),
+        ):
+            tx._process(
+                "authorize",
+                {
+                    "response": {
+                        "x_response_code": "1",
+                        "x_type": "auth_only",
+                        "x_trans_id": "test_trans_id",
+                    }
+                },
+            )
+
+        self.assertEqual(call_order, ["tokenize", "void"])
