06a5ee82c8d3356afdce16baf7c71357782a65c5
Lionel Piraux (lipi) <lipi@odoo.com>
2026-08-07 19:22:50 +0200

    [IMP] website_sale, l10n_*_website_sale: invert the default tax display
    *: website_sale_(loyalty, stock), l10n_(ar, br)_website_sale
    
    Websites default to displaying prices tax excluded, which is backwards:
    eCommerce shows prices tax included in most countries, Canada and the
    United States being the notable exceptions. The Argentinian and
    Brazilian localizations already had to opt back into tax included, while
    every other user starts from the wrong display and has to change the
    setting themselves.
    
    This commit makes tax included the default and pushes the tax excluded
    default down to the two countries that need it. The AR and BR overrides
    become redundant and are dropped, and two new bridge modules,
    l10n_ca_website_sale and l10n_us_website_sale, restore the tax excluded
    default for Canadian and American companies.
    
    task-4826932

--- addons/l10n_ar_website_sale/models/website.py
+++ addons/l10n_ar_website_sale/models/website.py
@@ -17,11 +17,3 @@ class Website(models.Model):
             website.l10n_ar_website_sale_show_both_prices = (
                 website.company_id.account_fiscal_country_id.code == 'AR'
             )
-
-    @api.depends('company_id.account_fiscal_country_id')
-    def _compute_show_line_subtotals_tax_selection(self):
-        # EXTENDS 'website_sale'
-        super()._compute_show_line_subtotals_tax_selection()
-        for website in self:
-            if website.company_id.account_fiscal_country_id.code == 'AR':
-                website.show_line_subtotals_tax_selection = 'tax_included'
--- addons/website_sale/models/website.py
+++ addons/website_sale/models/website.py
@@ -371,7 +371,12 @@ class Website(models.Model):
 
     @api.depends("company_id.account_fiscal_country_id")
     def _compute_show_line_subtotals_tax_selection(self):
-        self.show_line_subtotals_tax_selection = "tax_excluded"
+        for country_code, websites in self.grouped(
+            lambda website: website.company_id.account_fiscal_country_id.code
+        ).items():
+            websites.show_line_subtotals_tax_selection = (
+                "tax_excluded" if country_code in const.TAX_EXCLUDED_COUNTRIES else "tax_included"
+            )
 
     @api.depends("pricelist_id", "show_line_subtotals_tax_selection")
     def _compute_tax_display(self):
