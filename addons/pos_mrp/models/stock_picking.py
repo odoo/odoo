3a87ccc745c6675af7263278165d0bc405b4d6f2
visp-odoo <visp@odoo.com>
2026-08-07 12:29:01 +0000

    [FIX] pos_mrp: fix qty calculation for multiple order lines
    Steps to Reproduce :
    
    1. Create Product A and enable lot tracking for it.
    2. Create a Kit product and add Product A as one of its components.
    3. Go to PoS and create orders using:
      - Product A
      - The Kit product
      - A combination of Product A and the Kit product
    4. Create three different orders with these combinations.
    5. Try to close the PoS session.
    
    Issue :
    
    An error occurs when attempting to close the PoS session.
    
    The issue is in the pos_mrp module, specifically in the _get_lot_line_qty
    method. When accessing the following line:
    
    `lines_data[move.bom_line_id.bom_id.product_tmpl_id.product_variant_id.id]['order_lines'].qty`
    
    we expect order_lines to contain a single record. However, in this scenario,
    multiple order lines can be returned for the same product. As a result,
    accessing .qty directly on order_lines raises a singleton error.
    
    Solution :
    
    With this fix, we first retrieve the quantity from each order line and then
    sum the quantities together.
    
    This ensures that multiple matching order lines are handled correctly and
    prevents the singleton error when closing the PoS session.
    
    opw-6442765
    
    closes odoo/odoo#285884
    
    X-original-commit: 9adeeb7e42e65402e131194a0007aedcc9f8348a
    Signed-off-by: Manu Vaillant (manv) <manv@odoo.com>
    Signed-off-by: Vishal Padhiyar (visp) <visp@odoo.com>

--- addons/pos_mrp/models/stock_picking.py
+++ addons/pos_mrp/models/stock_picking.py
@@ -7,5 +7,6 @@ class StockMove(models.Model):
     def _get_lot_line_qty(self, line, move, lines_data):
         qty = super()._get_lot_line_qty(line, move, lines_data)
         if move.bom_line_id:
-            qty = lines_data[move.bom_line_id.bom_id.product_tmpl_id.product_variant_id.id]['order_lines'].qty * move.bom_line_id.product_qty
+            order_lines = lines_data[move.bom_line_id.bom_id.product_tmpl_id.product_variant_id.id]['order_lines']
+            qty = sum(order_lines.mapped("qty")) * move.bom_line_id.product_qty
         return qty
