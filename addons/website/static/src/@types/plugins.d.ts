311e1c22afd3ce521e950789dad858d0d4abaf40
Sébastien (blse) <blse@odoo.com>
2026-08-07 12:54:02 +0200

    [FIX] website: fix history of `value` property of input field elements
    The `value` property of the elements is not tracked by the history
    plugin, because `MutationObserver` does not produce mutations for that.
    
    This commit uses custom mutations when the `value` is changed, to
    restore the previous value on undo.
    
    Steps to reproduce:
    - Open website builder
    - Add a form
    - Set a "Default Value" on a text field
    - Press enter (to end preview)
    - Undo with the button
    - Bug: The value shown in the page did not revert with undo
    
    Similar bug in translate mode
    
    task-6229671
    
    X-original-commit: f40a1feeea0ff5017c6a0c2ab6f3199bfdf04c62
    Part-of: odoo/odoo#287325
    Signed-off-by: Romaric Moyeuvre (romo) <romo@odoo.com>
    Signed-off-by: Sébastien Blondiau (blse) <blse@odoo.com>

--- addons/website/static/src/@types/plugins.d.ts
+++ addons/website/static/src/@types/plugins.d.ts
@@ -34,6 +34,7 @@ declare module "plugins" {
     import { on_nodes_marked_translatable_handlers, TranslationShared } from "@website/builder/plugins/translation/translation_plugin";
     import { WebsiteSaveShared } from "@website/builder/plugins/website_save_plugin";
     import { force_background_translation_state_selectors } from "@website/builder/plugins/translation/repeat_translation_state_plugin";
+    import { ValueHistoryShared } from "@website/builder/plugins/value_history_plugin";
     import { WebsiteBridgeShared } from "@website/builder/plugins/website_bridge_plugin";
 
     interface SharedMethods {
@@ -61,6 +62,7 @@ declare module "plugins" {
         themeTab: ThemeTabShared;
         translateWebpageOption: TranslateWebpageOptionShared;
         translation: TranslationShared;
+        valueHistory: ValueHistoryShared;
         websiteBackgroundVideoPlugin: WebsiteBackgroundVideoShared;
         websiteFont: WebsiteFontShared;
         websiteFormOption: FormOptionShared;
