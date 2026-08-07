55d9dc4d4fadf28fbfca2b85744550c0a4e9e090
qsm-odoo <qsm@odoo.com>
2026-08-07 16:57:03 +0200

    [IMP] web, mail: merge activity/phone field code into 1 patchable point
    The parent commit basically adds a VoIP feature to all call activities:
    showing the phone number and being able to call it by clicking... this
    is something the phone field widget also does. This commit factorizes
    the code and makes it so VoIP can patch both points as an unique patch.
    
    This also allows web_enterprise to patch both at the same time to add
    the "Phone Install Dialog", suggesting VoIP installation to admins when
    available.
    
    task-6416875

--- addons/mail/static/tests/activity/activity.test.js
+++ addons/mail/static/tests/activity/activity.test.js
@@ -13,7 +13,8 @@ import {
 } from "@mail/../tests/mail_test_helpers";
 import { hover } from "@odoo/hoot-dom";
 import { advanceTime, mockDate } from "@odoo/hoot-mock";
-import { mockService, onRpc, serverState } from "@web/../tests/web_test_helpers";
+import { mockService, onRpc, patchWithCleanup, serverState } from "@web/../tests/web_test_helpers";
+import { browser } from "@web/core/browser/browser";
 import { deserializeDateTime, serializeDate, today } from "@web/core/l10n/dates";
 import { getOrigin } from "@web/core/utils/urls";
 
@@ -215,7 +216,12 @@ test("activity with a summary layout", async () => {
     await contains(".o-mail-Activity .o-mail-Activity-info span:text('test summary')");
 });
 
-test("call activity displays a phone link", async () => {
+test("call activity displays a phone link and opens the native dialer", async () => {
+    patchWithCleanup(browser, {
+        open(url) {
+            expect.step(url);
+        },
+    });
     const pyEnv = await startServer();
     const partnerId = pyEnv["res.partner"].create({});
     pyEnv["mail.activity"].create({
@@ -228,6 +234,8 @@ test("call activity displays a phone link", async () => {
 
     await contains(".o-mail-Activity-phoneNumber", { text: "+1 202 555 0182" });
     expect(".o-mail-Activity-phoneNumber > a").toHaveAttribute("href", "tel:+12025550182");
+    await click(".o-mail-Activity-phoneNumber > a");
+    expect.verifySteps(["tel:+12025550182"]);
 });
 
 test("activity without summary layout", async () => {
