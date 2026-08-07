7e22ca419442266799f0c4e945f2e6271f9d0eae
Stéphane Diez <snd@odoo.com>
2026-08-07 14:59:53 +0000

    [FIX] mrp_subcontracting_repair: leftover data
    An old refactor left some data around that are in conflict with
    other records for the same model.
    
    X-original-commit: 03435afa8ba536e5025c4f37273b9cc9aeb1fbb7

--- addons/mrp_subcontracting_repair/__manifest__.py
+++ addons/mrp_subcontracting_repair/__manifest__.py
@@ -9,10 +9,6 @@ Bridge module between MRP subcontracting and Repair
     'depends': [
         'mrp_subcontracting', 'repair'
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'security/mrp_subcontracting_repair_security.xml',
-    ],
     'auto_install': True,
     'author': 'Odoo S.A.',
     'license': 'LGPL-3',
