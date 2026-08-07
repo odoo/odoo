dc572c00c347ac05245b44a2edfb96a918b29fbb
Jinjiu Liu <jili@odoo.com>
2026-08-07 17:02:48 +0200

    [FIX] html_editor: avoid wrong deletion of Gboard for word suggestion
    Before this commit: on mobile, when typing using Gboard and select a
    word suggestion will only delete the last character and put the new
    word at the beginning of the word to be replaced. This is because Gboard
    extends the selection to the text to be corrected, then deletes it, and
    inserts the corrected text. This flow falls in our previous fix for MS
    Swiftkey's delete backward, and wrongly uses cached old selection
    instead of using extended new selection from Gboard.
    
    After this commit: We strict the Swiftkey fix further, and only execute
    it when the cursor is at the beginning of the p element.
    
    Related commit: https://github.com/odoo/odoo/commit/822fd4e8fec7e114e6748dd8c9b4969f423fb290
    
    task-6233756

--- addons/html_editor/static/tests/delete/backward.test.js
+++ addons/html_editor/static/tests/delete/backward.test.js
@@ -2352,5 +2352,28 @@ describe("Selection not collapsed", () => {
             await tick(); // Wait for the selection change to be handled
             expect(getContent(el)).toBe("<p>wonderful []</p>");
         });
+
+        test.tags("mobile");
+        test("should not break Gboard correction", async () => {
+            const dispatch = (type, eventInit) =>
+                manuallyDispatchProgrammaticEvent(editor.editable, type, eventInit);
+            const { editor, el } = await setupEditor("<p>Gxf[]</p>");
+            // Roughly as observed on Android Chrome with Gboard:
+            // - selection change
+            // - keydown event
+            // - input deleteContentBackward
+            // - keydown event
+            // - input insertText
+            const selection = editor.document.getSelection();
+            await dispatch("keydown", { key: "Unidentified" });
+            for (let i = 0; i < 2; i++) {
+                selection.modify("extend", "backward", "character");
+            }
+            await backspaceAndroid(editor);
+            await dispatch("keydown", { key: "Unidentified" });
+            await insertText(editor, "if ");
+            await tick(); // Wait for the selection change to be handled
+            expect(getContent(el)).toBe("<p>Gif []</p>");
+        });
     });
 });
