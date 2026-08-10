d448d1726158694f9ef8ab040641be555331d0f7
defl <defl@odoo.com>
2026-08-10 11:56:21 +0000

    [FIX] html_editor: fix line break in Safari
    **Steps to reproduce:**
    - Use a Mac with Safari
    - Install Knowledge app
    - Go to any article
    - Press Shift+Enter to try to enter a soft line break
    - Hard split is done instead
    
    **Issue:**
    Shift+Enter causes a `insertParagraph` event instead of
    `insertLineBreak` in Safari, which triggers the `SplitPlugin`
    instead of the `LineBreakPlugin`.
    
    **Fix:**
    Check if the browser is Safari and call `insertLineBreak`
    from the `SplitPlugin` (when needed) by listening to
    the "keydown" events.
    
    (note: I was not able to find any other key combination
    to properly trigger the `insertLineBreak` event in Safari)
    
    opw-6413507
    
    closes odoo/odoo#283936
    
    X-original-commit: 85dddb9cea016a5023518a5de2e13140a5dace60
    Signed-off-by: Florentin Delcourt (defl) <defl@odoo.com>

--- addons/html_editor/static/src/core/split_plugin.js
+++ addons/html_editor/static/src/core/split_plugin.js
@@ -12,6 +12,7 @@ import { prepareUpdate } from "../utils/dom_state";
 import { childNodes, closestElement, firstLeaf, lastLeaf } from "../utils/dom_traversal";
 import { DIRECTIONS, childNodeIndex, nodeSize } from "../utils/position";
 import { isProtected, isProtecting } from "@html_editor/utils/dom_info";
+import { isBrowserSafari } from "@web/core/browser/feature_detection";
 
 /**
  * @typedef { Object } SplitShared
@@ -72,6 +73,13 @@ export class SplitPlugin extends Plugin {
             (node) => node.nodeName === "SECTION",
         ],
     };
+
+    setup() {
+        super.setup();
+        if (isBrowserSafari()) {
+            this.addDomListener(this.editable, "keydown", this.onKeyDown);
+        }
+    }
 
     // --------------------------------------------------------------------------
     // commands
@@ -320,8 +328,19 @@ export class SplitPlugin extends Plugin {
     onBeforeInput(e) {
         if (e.inputType === "insertParagraph") {
             e.preventDefault();
+            // Safari reports Shift+Enter as "insertParagraph" instead of "insertLineBreak"
+            // Track it on keydown to handle it properly
+            if (this.forceLineBreak) {
+                this.forceLineBreak = false;
+                this.dependencies.lineBreak.insertLineBreak();
+                return;
+            }
             this.splitBlock();
             this.dependencies.history.addStep();
         }
+    }
+
+    onKeyDown(e) {
+        this.forceLineBreak = e.key === "Enter" && e.shiftKey;
     }
 }
