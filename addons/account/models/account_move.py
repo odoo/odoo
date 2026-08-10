5aed45bed75f4f017555d55cb71c9720fdc4517f
assh-odoo <assh@odoo.com>
2026-08-10 16:41:32 +0530

    [FIX] account: button_draft should return empty dict
    Currently, an error occurs in 19.0 when resetting a bank statement line
    to draft.
    
    Steps to Reproduce in 19.0:
    
     - Install the Accounting module.
     - Go to Accounting Dashboard and click the three-dot on bank journal.
     - Open Transactions.
     - Create a new statement line.
     - Select the statement line, click the gear action, and click Reset to Draft.
    
    AttributeError: 'bool' object has no attribute 'setdefault'
    
    After the [recent commit], button_draft() was changed to return True to
    prevent an XML-RPC error when returning None. In 19.0, when resetting
    a statement line to draft, the server action runs , the linked move is reset to
    draft, and button_draft() returns True [1]. When this result is passed to
    clean_action() [2], it raises an error.
    
    This commit ensures that button_draft() returns an empty action dictionary
    instead of True, avoiding the error in clean_action() while keeping the
    return value compatible with XML-RPC.
    
    [recent commit]: https://github.com/odoo/odoo/commit/712718d9df0fd5044ac57fdd9ec58e64bece36c0
    
    [1]: https://github.com/odoo/enterprise/blob/eae51e3ca155ca29e1de54f4bd223e7540b2aa7f/account_accountant/models/account_bank_statement.py#L112-L114
    
    [2]: https://github.com/odoo/odoo/blob/b3444e4bd421a30229b0dc7c8d5eb6c78b8b51ed/addons/web/controllers/utils.py#L24
    
    sentry-7354160052

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -5623,7 +5623,7 @@ class AccountMove(models.Model):
         self.sending_data = False
 
         self._detach_attachments()
-        return True
+        return {}
 
     def _get_fields_to_detach(self):
         """"
