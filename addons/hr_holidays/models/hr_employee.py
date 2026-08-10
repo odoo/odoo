906de114486ff2bdfd1386216dc3cda210ad634c
Eve Lin (evlin) <evlin@odoo.com>
2026-08-10 19:32:43 +0000

    [FIX] hr_holidays: show Time Off to employee approvers
    Issue: A user without any rights on Time Off should still be able to
    manage the requests of the users he's manager of. However, the
    computation for `show_leaves` only considers Time Off Officers/Admins
    and the employee themselves, while it should also consider the
    employee's Time Off approver.
    
    Fix: Include the employee's Time Off approver when computing
    `show_leaves`, allowing them to access the employee's Time Off smart
    button.
    
    Reproduction Steps:
    
    1. Set a user (e.g. Marc Demo) to "No" for Time Off.
    
    Note: If reproducing in v19, Administrator rights on Employees are
    also needed to access the private employee form view
    where the issue occurs.
    
    2. Set that user as another employee's Time Off approver.
    3. Log in as that user and open the employee's form view.
    4. Observe that the Time Off smart button is not visible.
    
    Related Tickets:
    opw-6445714
    
    closes odoo/odoo#290555
    
    X-original-commit: 8d05257d83f9128953f580a066db67c48fcdb96f
    Signed-off-by: Eve Lin (evlin) <evlin@odoo.com>
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- addons/hr_holidays/models/hr_employee.py
+++ addons/hr_holidays/models/hr_employee.py
@@ -238,7 +238,7 @@ class HrEmployee(models.Model):
     def _compute_show_leaves(self):
         show_leaves = self.env.user.has_group('hr_holidays.group_hr_holidays_user')
         for employee in self:
-            if show_leaves or employee.user_id == self.env.user:
+            if show_leaves or employee.user_id == self.env.user or employee.leave_manager_id == self.env.user:
                 employee.show_leaves = True
             else:
                 employee.show_leaves = False
