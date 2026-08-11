a068ffdb9c1788da1a6c0366d69ce890c36a3dd0
Max Whale <mawh@odoo.com>
2026-08-11 12:55:48 +0000

    [IMP] pos_mollie: add unit tests
    This commit adds both Python and JS unit tests for the Mollie POS
    payment method.
    
    X-original-commit: c6ec6ab38a7f03a431414d7e034a83d2588994dd

---
+++ addons/pos_mollie/static/tests/unit/data/pos_payment_method.data.js
@@ -0,0 +1,15 @@
+import { PosPaymentMethod } from "@point_of_sale/../tests/unit/data/pos_payment_method.data";
+
+PosPaymentMethod._records = [
+    ...PosPaymentMethod._records,
+    {
+        id: 4,
+        name: "Mollie",
+        type: "bank",
+        image: false,
+        sequence: 1,
+        payment_method_type: "terminal",
+        payment_provider: "mollie",
+        default_qr: false,
+    },
+];
