odoo/odoo

commit 76be1f20098d62a35b08a58ce9be1c380388ac01

[IMP] mrp(_workorder): Plan as Soon as Possible
Allow scheduling as soon possible by default
while still allowing to schedule based on date_start
(using the current mechanism)

task: 5259035

See odoo/enterprise#100451

Part-of: odoo/odoo#237316
Signed-off-by: William Henrotin (whe) <whe@odoo.com>

addons/mrp/models/mrp_production.py
@@ -149,7 +149,7 @@ class MrpProduction(models.Model):
     date_start = fields.Datetime(
         'Start', copy=False, default=_get_default_date_start,
         help="Date you plan to start production or date you actually started production.",
-        index=True, required=True)
+        index=True, required=True, tracking=True)
     date_finished = fields.Datetime(
         'End', copy=False, default=_get_default_date_finished,
         compute='_compute_date_finished', store=True,
@@ -1005,7 +1005,7 @@ class MrpProduction(models.Model):
                 if production.state in ['done', 'cancel']:
                     raise UserError(_('You cannot move a manufacturing order once it is cancelled or done.'))
                 if production.is_planned:
-                    production.button_unplan()
+                    production._unplan_workorders()
             if vals.get('date_start'):
                 production.move_raw_ids.write({'date': production.date_start, 'date_deadline': production.date_start})
             if vals.get('date_finished'):
@@ -1177,7 +1177,7 @@ class MrpProduction(models.Model):
                 if production.is_planned:
                     new_durations = production.workorder_ids.mapped('duration_expected')
                     if old_durations != new_durations:
-                        production.button_unplan()
+                        production._unplan_workorders()
                         production._plan_workorders()
         self.is_outdated_bom = False
 
@@ -1652,13 +1652,16 @@ class MrpProduction(models.Model):
             production.move_raw_ids._action_assign()
         return True
 
-    def button_plan(self):
+    def button_plan(self, as_soon_as_possible=True):
         """ Create work orders. And probably do stuff, like things. """
         orders_to_plan = self.filtered(lambda order: not order.is_planned)
         orders_to_confirm = orders_to_plan.filtered(lambda mo: mo.state == 'draft')
         orders_to_confirm.action_confirm()
         for order in orders_to_plan:
+            if as_soon_as_possible:
+                order.date_start = fields.Datetime.now()
             order._plan_workorders()
+            order.message_post(body=self.env._("The manufacturing order has been planned."), subtype_id=self.env.ref('mrp.mrp_mo_planned').id)
         return True
 
     def _plan_workorders(self, replan=False):
@@ -1691,6 +1694,20 @@ class MrpProduction(models.Model):
         })
 
     def button_unplan(self):
+        self._unplan_workorders()
+        for order in self:
+            previous_date_start = None
+            for message in order.message_ids:
+                if message.tracking_value_ids.field_id.mapped('name') == ['date_start']:
+                    previous_date_start = message.tracking_value_ids.old_value_datetime
+                    break
+                if message.subtype_id.id == self.env.ref('mrp.mrp_mo_planned').id:
+                    break
+            if previous_date_start:
+                order.date_start = previous_date_start
+            order.message_post(body=self.env._("The manufacturing order has been unplanned."))
+
+    def _unplan_workorders(self):
         if any(wo.state == 'done' for wo in self.workorder_ids):
             raise UserError(_("Some work orders are already done, so you cannot unplan this manufacturing order.\n\n"
                 "It’d be a shame to waste all that progress, right?"))
@@ -2540,16 +2557,6 @@ class MrpProduction(models.Model):
             'view_mode': 'form',
             'res_id': production.id,
         }
-
-    def action_plan_with_components_availability(self):
-        for production in self.filtered(lambda p: p.state in ('draft', 'confirmed')):
-            if production.state == 'draft':
-                production.action_confirm()
-            move_expected_date = production.move_raw_ids.filtered('forecast_expected_date').mapped('forecast_expected_date')
-            expected_date = max(move_expected_date, default=False)
-            if expected_date and production.components_availability_state != 'unavailable':
-                production.date_start = expected_date
-        self.filtered(lambda p: p.state == 'confirmed').button_plan()
 
     def _has_workorders(self):
         return self.workorder_ids

addons/mrp/tests/test_order.py
@@ -2344,7 +2344,7 @@ class TestMrpOrder(TestMrpCommon):
         mo.workorder_ids[1].duration_expected = 60
 
         mo.action_confirm()
-        mo.button_plan()
+        mo.button_plan(as_soon_as_possible=False)
 
         # Asia/Bangkok is UTC+7 and the start date is on Monday at 06:00 UTC (i.e., 13:00 UTC+7).
         # So, in Bangkok, the first workorder uses the entire Monday afternoon slot 13:00 - 17:00 UTC+7 (i.e., 06:00 - 10:00 UTC)
@@ -5379,6 +5379,34 @@ class TestMrpOrder(TestMrpCommon):
         self.assertNotEqual(mo1.workorder_ids.product_uom_id, mo2.workorder_ids.product_uom_id)
         self.assertEqual(mo1.product_qty, mo2.product_qty)
         self.assertEqual(wo1.qty_produced, wo2.qty_produced)
+
+    def test_plan_as_soon_as_possible(self):
+        now = datetime.now()
+        self.product_1.is_storable = True
+        # Receive component in 3 weeks on next monday
+        receipt_form = Form(self.env['stock.picking'])
+        receipt_form.partner_id = self.partner
+        receipt_form.picking_type_id = self.picking_type_in
+        receipt_form.scheduled_date = now + timedelta(days=21 + (7 - now.weekday()))
+        receipt = receipt_form.save()
+        with receipt_form.move_ids.new() as move_line:
+            move_line.product_id = self.product_1
+            move_line.product_uom_qty = 100
+        receipt = receipt_form.save()
+        receipt.action_confirm()
+        # Make production order
+        production_form = Form(self.env['mrp.production'])
+        production_form.bom_id = self.bom_4
+        production = production_form.save()
+        production.action_confirm()
+        # Check plan at date : on receipt's date or later
+        production.date_start = max(production.move_raw_ids.filtered('forecast_expected_date').mapped('forecast_expected_date'))
+        production.button_plan(as_soon_as_possible=False)
+        self.assertGreaterEqual(production.date_start, receipt.scheduled_date)
+        # Check plan as soon as possible : now or later
+        production.button_unplan()
+        production.button_plan()
+        self.assertGreaterEqual(production.date_start, now)
 
 
 @tagged('-at_install', 'post_install')

addons/mrp/data/mail_message_subtype_data.xml
@@ -36,4 +36,10 @@
         <field name="sequence" eval="105"/>
         <field name="description">MO Cancelled</field>
     </record>
+
+    <record id="mrp_mo_planned" model="mail.message.subtype">
+        <field name="name">MO Planned</field>
+        <field name="res_model">mrp.production</field>
+        <field name="default" eval="False"/>
+    </record>
 </odoo>

addons/mrp/views/mrp_production_views.xml
@@ -147,13 +147,13 @@
                 action = records.action_open_label_type()</field>
         </record>
 
-        <record id="action_plan_with_components_availability" model="ir.actions.server">
-            <field name="name">Plan based on Components Availability</field>
+        <record id="action_plan_at_date" model="ir.actions.server">
+            <field name="name">Plan at Date</field>
             <field name="model_id" ref="mrp.model_mrp_production"/>
             <field name="binding_model_id" ref="mrp.model_mrp_production"/>
             <field name="binding_view_types">list,kanban,form</field>
             <field name="state">code</field>
-            <field name="code">action = records.action_plan_with_components_availability()</field>
+            <field name="code">records.button_plan(as_soon_as_possible=False)</field>
         </record>
 
         <record id="action_production_order_merge" model="ir.actions.server">
@@ -195,7 +195,7 @@
                     <button name="button_mark_done" invisible="move_raw_ids or not show_produce_all" string="Produce All" type="object" class="oe_highlight" data-hotkey="g"
                             confirm="There are no components to consume. Are you still sure you want to continue?"/>
                     <button name="action_confirm" invisible="state != 'draft'" string="Confirm" type="object" class="oe_highlight" data-hotkey="q"/>
-                    <button name="button_plan" invisible="state not in ('confirmed', 'progress', 'to_close') or not workorder_ids or is_planned" type="object" string="Plan" class="oe_highlight" data-hotkey="z"/>
+                    <button name="button_plan" invisible="state not in ('confirmed', 'progress', 'to_close') or is_planned" type="object" string="Plan" class="oe_highlight" data-hotkey="z"/>
                     <button name="button_unplan" type="object" string="Unplan" invisible="not is_planned or state in ['cancel', 'done']" data-hotkey="z"/>
                     <button name="action_start"  type="object" string="Start" invisible="state != 'confirmed'"/>
                     <button name="action_assign" invisible="state in ('draft', 'done', 'cancel') or not reserve_visible" string="Check availability" type="object" data-hotkey="c"/>

addons/mrp/wizard/change_production_qty.py
@@ -96,7 +96,7 @@ class ChangeProductionQty(models.TransientModel):
             # replan production based on new workorder durations
             if factor != 1.0 and production.is_planned and (production.state == 'confirmed'
                 or (production.state == 'progress' and not any(wo.state in ['done', 'progress'] for wo in production.workorder_ids))):
-                production.button_unplan()
+                production._unplan_workorders()
                 production._plan_workorders()
         # run scheduler for moves forecasted to not have enough in stock
         self.mo_id.filtered(lambda mo: mo.state in ['confirmed', 'progress']).move_raw_ids._trigger_scheduler()

commit 4d63bdc7c0e4a44d5d9212769bca4de05dbc98ee

[FIX] mrp: allow non-admin manager to unplan a manufacturing order
Steps to reproduce the bug:
- As an administrator, create a user with the "Manufacturing / Administrator"
  right but without the "Administration / Settings" right (base.group_system)
- Log in as that user
- Create and confirm a manufacturing order
- Click "Plan" -> works fine
- Click "Unplan"

Problem:
Clicking "Unplan" raises:
"You do not have enough rights to access the field 'tracking_value_ids' on
Message (mail.message)."

This regression was introduced by commit:
https://github.com/odoo/odoo/commit/76be1f20098d62a35b08a58ce9be1c380388ac01
which made button_unplan() walk order.message_ids looking for the
tracking value of the previous date_start, so it can be restored:
https://github.com/odoo/odoo/blob/320d3422345a4bd792a07652490a9869d79ae716/addons/mrp/models/mrp_production.py#L1750-L1752

mail.message.tracking_value_ids is restricted with groups="base.group_system",
so reading it as the current (non-Settings-admin) user raises an AccessError.
button_plan() never hits this code path since it only calls message_post(),
which is why the same user can plan but not unplan.

Solution:
Read message_ids in sudo(), since this is only internal bookkeeping to
recover the previous date_start and not something that should require
Settings/Administration access.

opw-6455333

addons/mrp/models/mrp_production.py
@@ -1747,7 +1747,7 @@ class MrpProduction(models.Model):
         self._unplan_workorders()
         for order in self:
             previous_date_start = None
-            for message in order.message_ids:
+            for message in order.sudo().message_ids:
                 if message.tracking_value_ids.field_id.mapped('name') == ['date_start']:
                     previous_date_start = message.tracking_value_ids.old_value_datetime
                     break

addons/mrp/tests/test_order.py
@@ -5499,6 +5499,18 @@ class TestMrpOrder(TestMrpCommon):
         production.button_plan()
         self.assertGreaterEqual(production.date_start, now)
 
+    def test_unplan_access_rights_manager(self):
+        """ An MRP manager without Settings/Administration rights should be able to
+        unplan a manufacturing order they were able to plan. """
+        man_order = self.env['mrp.production'].with_user(self.user_mrp_manager).create({
+            'bom_id': self.bom_1.id,
+        })
+        man_order.action_confirm()
+        man_order.button_plan()
+        self.assertTrue(man_order.is_planned)
+        man_order.button_unplan()
+        self.assertFalse(man_order.is_planned)
+
     def test_update_component_qty_consumption(self):
         """Ensure that when updating the component's quantity to consume,
         the move is not marked as picked allowing the new quantity to be