0be0ec2f4e2bd26b7b1893ee816533dc62683498
fmvt-odoo <148151879+fmvt-odoo@users.noreply.github.com>
2026-08-11 12:23:14 -0600

    [IMP] l10n_mx_edi: Test

--- addons/account/models/account_account.py
+++ addons/account/models/account_account.py
@@ -30,6 +30,7 @@ class AccountAccount(models.Model):
     _parent_store = True
     _check_company_auto = True
     _check_company_domain = models.check_companies_domain_parent_of
+    .
 
     @api.constrains('account_type', 'reconcile')
     def _check_reconcile(self):
