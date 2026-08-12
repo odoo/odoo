a5590f30f9340c7dea4013815772aa55ed70b8e6
Sébastien (blse) <blse@odoo.com>
2026-08-12 11:13:07 +0200

    [FIX] website_sale: fix action `addToCartAction`
    The commit c5a40a608280017ae9ea8f9e9e1c59f778d629ae updated to icons to
    use `data-icon` attribute instead of `fa-*` classes.
    
    This commit adapts the `addToCartAction` to correctly update the icon
    (by changing the attribute instead of the class). And fixes a few bugs
    related to that action as well.
    
    Steps to reproduce:
    - Open website builder
    - Drop a "Add to cart button"
      - Select a "Product" with no variants (for example "Chair protection")
      - Change the "Action"
      - Bug: the action is not changed (but a class with no effects is added)
    -
      - Select text in it
      - Copy
      - Paste
      - Bug: there is a <button> in a <button>
    -
      - Move caret just before the icon
      - Type text
      - Bug: the text goes outside the button
    -
      - Select a "Product" with no variants (for example "Chair protection")
      - Delete the icon
      - Change the "Action"
      - Bug: crash
    -
      - Select a "Product" with no variants (for example "Chair protection")
      - Select a few letter
      - Set their style to bold
      - Change the "Action"
      - Bug: only part of the text is changed
    
    task-6466422
    
    closes odoo/odoo#281984
    
    Signed-off-by: Louis Tinel (loti) <loti@odoo.com>

--- addons/website_sale/static/src/website_builder/add_to_cart_option_plugin.js
+++ addons/website_sale/static/src/website_builder/add_to_cart_option_plugin.js
@@ -2,11 +2,13 @@ import { Plugin } from "@html_editor/plugin";
 import { registry } from "@web/core/registry";
 import { _t } from "@web/core/l10n/translation";
 import { BuilderAction } from "@html_builder/core/builder_action";
+import { childNodeIndex } from "@html_editor/utils/position";
 
 export class AddToCartOptionPlugin extends Plugin {
     static id = "addToCartOption";
     static dependencies = ["builderActions", "websiteBridge"];
     static shared = ["addToCartValues"];
+    /** @type {import("plugins").WebsiteResources} */
     resources = {
         so_content_addition_selectors: [".s_add_to_cart"],
         builder_actions: {
@@ -14,14 +16,25 @@ export class AddToCartOptionPlugin extends Plugin {
             VariantToCartAction,
             AddToCartActionAction,
         },
+        content_not_editable_selectors: ".s_add_to_cart:has(> button.s_add_to_cart_btn)",
+        content_editable_selectors: ".s_add_to_cart > button.s_add_to_cart_btn",
     };
     addToCartValues() {
         return {
-            addToCart: { action: "add_to_cart", icon: "add_shopping_cart", label: _t("Add to Cart"), label_website: this.dependencies.websiteBridge._t("Add to Cart") },
-            buyNow: { action: "buy_now", icon: "credit_card", label: _t("Buy Now"), label_website: this.dependencies.websiteBridge._t("Buy Now") },
+            addToCart: {
+                action: "add_to_cart",
+                icon: "add_shopping_cart",
+                label: _t("Add to Cart"),
+                content: this.dependencies.websiteBridge._t("Add to Cart"),
+            },
+            buyNow: {
+                action: "buy_now",
+                icon: "credit_card",
+                label: _t("Buy Now"),
+                content: this.dependencies.websiteBridge._t("Buy Now"),
+            },
         };
-    };
-
+    }
 }
 
 export class ProductToCartAction extends BuilderAction {
@@ -111,30 +124,29 @@ export class VariantToCartAction extends BuilderAction {
 }
 export class AddToCartActionAction extends BuilderAction {
     static id = "addToCartAction";
-    static dependencies = ["builderActions", "addToCartOption"];
-    apply({ editingElement, params: { action, icon, label_website: label } }) {
-        const classAction = this.dependencies.builderActions.getAction("classAction");
+    static dependencies = ["builderActions", "addToCartOption", "selection", "dom"];
+    apply({ editingElement, params: { action, icon, content: label } }) {
         editingElement.dataset.action = action;
         const buttonEl = editingElement.querySelector(".s_add_to_cart_btn");
         buttonEl.dataset.action = action;
         const iconEl = buttonEl.querySelector("i");
-        classAction.apply({
-            editingElement: iconEl,
-            params: { mainParam: icon },
-        });
-        buttonEl.lastChild.textContent = label;
-    }
-    clean({ editingElement, params: { icon } }) {
-        const classAction = this.dependencies.builderActions.getAction("classAction");
-
-        delete editingElement.dataset.action;
-        const buttonEl = editingElement.querySelector(".s_add_to_cart_btn");
-        delete buttonEl.dataset.action;
-        const iconEl = buttonEl.querySelector("i");
-        classAction.clean({
-            editingElement: iconEl,
-            params: { mainParam: icon },
-        });
+        if (iconEl) {
+            iconEl.dataset.icon = icon;
+            this.dependencies.selection.setSelection({
+                anchorNode: iconEl.parentElement,
+                anchorOffset: childNodeIndex(iconEl) + 1,
+                focusNode: buttonEl,
+                focusOffset: buttonEl.childNodes.length,
+            });
+        } else {
+            this.dependencies.selection.setSelection({
+                anchorNode: buttonEl,
+                anchorOffset: 0,
+                focusNode: buttonEl,
+                focusOffset: buttonEl.childNodes.length,
+            });
+        }
+        this.dependencies.dom.insert(label);
     }
     isApplied({ editingElement, params: { action } }) {
         return editingElement.dataset.action === action;
@@ -143,7 +155,6 @@ export class AddToCartActionAction extends BuilderAction {
     resetDefaultAction(editingElement) {
         const addToCartValues = this.dependencies.addToCartOption.addToCartValues();
         if (this.isApplied({ editingElement, params: addToCartValues.buyNow })) {
-            this.clean({ editingElement, params: addToCartValues.buyNow });
             this.apply({ editingElement, params: addToCartValues.addToCart });
         }
     }
