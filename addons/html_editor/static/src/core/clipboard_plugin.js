7e7131c113f9b012ad06e16a8ed9c8c8f3ced58c
Adnan Saiyed <adsa@odoo.com>
2026-08-12 13:17:36 +0000

    [FIX] html_editor: incorrect mail URLs on paste
    Current behavior before commit:
    
    Issue 1:
    
    Steps to reproduce the issue:
    
    - Create a mail link in editor.
    - Copy link via link popover.
    - Paste it anywhere in editor.
    - Click on it to open popover.
    
    Notice that newly created link is not a correct mail link.
    
    Issue 2:
    
    Steps to reproduce the issue:
    
    - Copy a mail URL from google docs or from some other
      website.
    - Paste the copied link in editor.
    
    Notice that URL is pasted as plain text instead of link. This
    happens because when a link is copied with HTML content,
    `handlePasteText` is called after `handlePasteHtml`, and
    it ends up being pasted as html content.
    
    Desired behavior after commit:
    
    This commit aims to ensure that mail URLs are being pasted
    correctly in editor.
    
    task-6462998
    
    closes odoo/odoo#289981
    
    X-original-commit: 2c1ce76770ea5d08f68027f5fe5fe7467db8f478
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Adnan Saiyed (adsa) <adsa@odoo.com>

--- addons/html_editor/static/src/core/clipboard_plugin.js
+++ addons/html_editor/static/src/core/clipboard_plugin.js
@@ -17,6 +17,7 @@ import {
 import { DIRECTIONS } from "../utils/position";
 import { isHtmlContentSupported } from "./selection_plugin";
 import { getRowIndex } from "@html_editor/utils/table";
+import { EMAIL_REGEX } from "@html_editor/main/link/utils";
 
 /**
  * @typedef { import("./selection_plugin").EditorSelection } EditorSelection
@@ -99,7 +100,10 @@ export const CLIPBOARD_WHITELISTS = {
     styledTags: ["SPAN", "B", "STRONG", "I", "S", "U", "FONT", "TD"],
 };
 
-const ONLY_LINK_REGEX = /^(https?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w-./?%&=]*)?$/i;
+const ONLY_LINK_REGEX = new RegExp(
+    `^(https?:\\/\\/)?([\\w-]+\\.)+[\\w-]+(\\/[\\w-./?%&=]*)?$|${EMAIL_REGEX.source}`,
+    "i"
+);
 
 /**
  * @typedef {Object} ClipboardShared
