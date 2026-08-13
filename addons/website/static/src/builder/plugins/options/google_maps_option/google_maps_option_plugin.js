1315220f618e5c1c9fc8260eb490bf1734c39326
Rushil Patel <rusp@odoo.com>
2026-08-13 07:48:18 +0000

    [IMP] website: replace deprecated Google Maps Autocomplete
    google.maps.places.Autocomplete is deprecated in the new Places API.
    The replacement (`AutocompleteSuggestion.fetchAutocompleteSuggestions`)
    does not rely on DOM events, making it incompatible with the previous
    event-listener based integration used in GPSPicker.
    
    A new Owl component (`PlacesAutoComplete`) is introduced to wrap the new
    API, built on top of the existing `AutoCompleteWithPages`.
    
    Autocomplete requests are now grouped using an
    `AutocompleteSessionToken`, ensuring they are associated with a single
    session. This groups multiple queries
    (e.g., "I", "In", "Ind", "Indi", "India") and the final selection into
    one session, avoiding per-request billing.
    
    task-4441041
    
    closes odoo/odoo#282343
    
    X-original-commit: f617363be6392ba6c52a5eb7d8563fd2250e86b5
    Signed-off-by: Robin Lejeune (role) <role@odoo.com>

--- addons/website/static/src/builder/plugins/options/google_maps_option/google_maps_option_plugin.js
+++ addons/website/static/src/builder/plugins/options/google_maps_option/google_maps_option_plugin.js
@@ -34,6 +34,7 @@ import { BuilderAction } from "@html_builder/core/builder_action";
  * @property { GoogleMapsOptionPlugin['commitPlace'] } commitPlace
  * @property { GoogleMapsOptionPlugin['getPlace'] } getPlace
  * @property { GoogleMapsOptionPlugin['getMapsAPI'] } getMapsAPI
+ * @property { GoogleMapsOptionPlugin['notifyGMapsError'] } notifyGMapsError
  */
 
 export class GoogleMapsOptionPlugin extends Plugin {
@@ -48,6 +49,7 @@ export class GoogleMapsOptionPlugin extends Plugin {
         "commitPlace",
         "getPlace",
         "getMapsAPI",
+        "notifyGMapsError",
     ];
     /** @type {import("plugins").WebsiteResources} */
     resources = {
@@ -151,7 +153,7 @@ export class GoogleMapsOptionPlugin extends Plugin {
         if (place?.geometry) {
             const location = place.geometry.location;
             /** @type {Coordinates} */
-            const coordinates = `(${location.lat()},${location.lng()})`;
+            const coordinates = `(${location.lat},${location.lng})`;
             this.gpsMapCache.set(coordinates, place);
             /** @type {{mapGps: Coordinates, pinAddress: string}} */
             const currentMapData = editingElement.dataset;
