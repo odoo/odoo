be79e33b8b9d171fd2004e1c8c44a97edce9a01a
Walid (wasa) <wasa@odoo.com>
2026-08-13 11:16:43 +0200

    [FIX] web_editor: avoid duplicate Outlook image clones in mailing conversion
    Problem:
    An `img-fluid` image ended up duplicated twice inside `[if mso]` comments
    instead of once when converting a mailing body to inline HTML.
    
    Cause:
    `classToStyle` resets the image's `width` attribute back to `100%` after the
    `img-fluid` fix already added its Outlook clone. This caused `enforceImagesResponsivity`
    to match the image again and duplicate it a second time.
    
    Solution:
    Mark images handled by the `img-fluid` fix with the `.mso-hide` class so they are
    excluded from `enforceImagesResponsivity`'s query (`:not(.mso-hide)`). Adding `.mso-hide`
    also ensures `_hideForOutlook` automatically processes the element at the end of `toInline`.
    
    Steps to reproduce:
    - Add an image in a new email template.
    - Set its width to "100%".
    - Save.
    - Observe the saved HTML has two MSO comments for one image.
    
    opw-6411348

--- addons/web_editor/static/src/js/backend/convert_inline.js
+++ addons/web_editor/static/src/js/backend/convert_inline.js
@@ -653,8 +653,10 @@ function enforceImagesResponsivity(editable) {
     // Remove the height attribute in card images so they can resize
     // responsively, but leave it for Outlook.
     for (const image of editable.querySelectorAll('img[width="100%"][height]')) {
-        image.before(_createMso(image.outerHTML));
-        image.classList.add('mso-hide');
+        if (!image.classList.contains("mso-hide")) {
+            image.before(_createMso(image.outerHTML));
+            image.classList.add('mso-hide');
+        }
         image.removeAttribute('height');
     }
 }
@@ -727,7 +729,7 @@ export async function toInline($editable, cssRules, $iframe) {
         clone.style.setProperty('width', width + 'px');
         clone.style.removeProperty('max-width');
         image.before(_createMso(clone.outerHTML));
-        _hideForOutlook(image);
+        image.classList.add("mso-hide");
     }
 
     classToStyle($editable, cssRules);
