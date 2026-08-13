aeb610f86c0cac6542e25b3061027d61062067da
Rushil Patel <rusp@odoo.com>
2026-08-13 07:48:18 +0000

    [IMP] website: replace deprecated Google Maps Autocomplete
    google.maps.places.Autocomplete is deprecated in the new Places API.
    The replacement (`AutocompleteSuggestion.fetchAutocompleteSuggestions`)
    does not rely on DOM events, making it incompatible with the previous
    event-listener based integration used in GPSPicker.
    
    A new Owl component (`PlacesAutoComplete`) is introduced to wrap the new
    API, built on top of the existing `AutoCompleteWithPages`.
    
    Autocomplete requests are now grouped using an
    `AutocompleteSessionToken`, ensuring they are associated with a single
    session. This groups multiple queries
    (e.g., "I", "In", "Ind", "Indi", "India") and the final selection into
    one session, avoiding per-request billing.
    
    task-4441041
    
    X-original-commit: f617363be6392ba6c52a5eb7d8563fd2250e86b5

--- addons/website/static/src/builder/plugins/options/google_maps_option/google_maps_option.js
+++ addons/website/static/src/builder/plugins/options/google_maps_option/google_maps_option.js
@@ -1,7 +1,9 @@
 import { useLayoutEffect, useRef } from "@web/owl2/utils";
-import { onMounted, onWillDestroy, proxy } from "@odoo/owl";
+import { onMounted, onWillDestroy, proxy, useApp } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { BaseOptionComponent } from "@html_builder/core/base_option_component";
+import { PlacesAutoComplete } from "@website/components/googleplaces_autocomplete/places_autocomplete";
+import wUtils from "@website/js/utils";
 
 /** @import { Coordinates, Place } from './google_maps_option_plugin.js' */
 
@@ -19,6 +21,7 @@ export class GoogleMapsOption extends BaseOptionComponent {
         /** @type {function(Element, Place):void} */
         this.commitPlace = this.dependencies.googleMapsOption.commitPlace;
 
+        this.app = useApp();
         this.inputRef = useRef("inputRef");
         /** @type {{ formattedAddress: string }} */
         this.state = proxy({
@@ -34,14 +37,7 @@ export class GoogleMapsOption extends BaseOptionComponent {
             this.initializeAutocomplete(this.inputRef.el);
         });
         onWillDestroy(() => {
-            if (this.autocompleteListener) {
-                this.getMapsAPI().event.removeListener(this.autocompleteListener);
-            }
-            // Without this, the Google library injects elements inside the
-            // DOM but does not remove them once the option is closed.
-            for (const container of document.body.querySelectorAll(".pac-container")) {
-                container.remove();
-            }
+            this.unmountAutocomplete?.();
         });
     }
 
@@ -51,37 +47,29 @@ export class GoogleMapsOption extends BaseOptionComponent {
      * @param {Element} inputEl
      */
     initializeAutocomplete(inputEl) {
-        if (!this.googleMapsAutocomplete && this.getMapsAPI()) {
-            const mapsAPI = this.getMapsAPI();
-            this.googleMapsAutocomplete = new mapsAPI.places.Autocomplete(inputEl, {
-                types: ["geocode"],
+        const editingElement = this.env.getEditingElement();
+        this.unmountAutocomplete = wUtils.mountAutocompleteComponent(this.app, PlacesAutoComplete, {
+            targetDropdown: inputEl,
+            maps: this.getMapsAPI(),
+            onPlaceSelected: this.onPlaceSelected.bind(this),
+            onError: () => this.dependencies.googleMapsOption.notifyGMapsError(editingElement),
+        });
+        if (!this.state.formattedAddress) {
+            /** @type {Coordinates} */
+            const coordinates = editingElement.dataset.mapGps;
+            this.getPlace(editingElement, coordinates).then((place) => {
+                if (place?.formatted_address) {
+                    this.state.formattedAddress = place.formatted_address;
+                }
             });
-            this.autocompleteListener = mapsAPI.event.addListener(
-                this.googleMapsAutocomplete,
-                "place_changed",
-                this.onPlaceChanged.bind(this)
-            );
-            if (!this.state.formattedAddress) {
-                const editingElement = this.env.getEditingElement();
-                /** @type {Coordinates} */
-                const coordinates = editingElement.dataset.mapGps;
-                this.getPlace(editingElement, coordinates).then((place) => {
-                    if (place?.formatted_address) {
-                        this.state.formattedAddress = place.formatted_address;
-                    }
-                });
-            }
         }
     }
 
     /**
-     * Retrieve the new place given by Google Places API's autocompletion
-     * whenever it sends a signal that the place changed, and send it to the
-     * plugin.
+     *
+     * @param {Place} place Place object from Google Maps API
      */
-    onPlaceChanged() {
-        /** @type {Place | undefined} */
-        const place = this.googleMapsAutocomplete.getPlace();
+    onPlaceSelected(place) {
         this.commitPlace(this.env.getEditingElement(), place);
         this.state.formattedAddress = place?.formatted_address || "";
     }
