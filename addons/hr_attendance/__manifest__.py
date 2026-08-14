114e56bc580571f32f609bda6bb1769546d52d9e
ahta <ahta@odoo.com>
2026-08-14 08:00:38 +0000

    [FIX] hr_attendance: remove presence status from kiosk bundle
    this commit removes the hr_attendance_presence_status.js file from the
     kiosk bundle, as it is not needed in the kiosk view and can cause
     unnecessary loading of resources.
    
    task-6468972
    
    closes odoo/odoo#283317
    
    X-original-commit: 55a8145bcd083c0960841ebbe48acaa20f0e0790
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>
    Signed-off-by: Ahmed Taha Abdelaziz (ahta) <ahta@odoo.com>

--- addons/hr_attendance/__manifest__.py
+++ addons/hr_attendance/__manifest__.py
@@ -76,6 +76,7 @@ actions(Check in/Check out) performed by them.
             # Public Kiosk app and its components
             "hr_attendance/static/src/public_kiosk/**/*",
             'hr_attendance/static/src/components/**/*',
+            ('remove', 'hr_attendance/static/src/components/hr_presence_status/hr_attendance_presence_status.js'),
 
             'hr_attendance/static/src/scss/kiosk/hr_attendance.scss',
             "web/static/src/views/fields/formatters.js",
