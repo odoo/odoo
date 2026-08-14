bdb97f7acd1ac782a8ce4dfea98b3430d4be5304
Dhrumit Parmar <dgpa@odoo.com>
2026-08-14 04:43:28 +0000

    [FIX] web: handle false write_date in many2one avatar
    Issue:
    The `Many2OneAvatarField` and `KanbanMany2OneAvatarField` templates were
    directly calling `value.write_date?.toMillis()`. Optional chaining does not
    handle the case where `write_date` is `false`, resulting in a `TypeError`
    because `toMillis()` is not available on a boolean value.
    
    Solution:
    Added a `uniqueId` getter in both `Many2OneAvatarField` and
     `KanbanMany2OneAvatarField` to safely handle a missing or false `write_date`.
     The getter calls `toMillis()` only when `write_date` is available and returns
    `undefined` otherwise. Both templates now use `uniqueId` for the avatar URL.
    
    opw-6464172
    
    closes odoo/odoo#284015
    
    X-original-commit: 354a84538156a9ff727036c000d554018c098b00
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/mail/static/src/views/web/fields/many2one_avatar_user_field/many2one_avatar_user_field.js
+++ addons/mail/static/src/views/web/fields/many2one_avatar_user_field/many2one_avatar_user_field.js
@@ -47,7 +47,7 @@ export class Many2OneAvatarUserField extends Component {
     }
 
     get uniqueId() {
-        return this.value?.write_date?.toMillis();
+        return this.value?.write_date ? this.value.write_date.toMillis() : undefined;
     }
 }
 
