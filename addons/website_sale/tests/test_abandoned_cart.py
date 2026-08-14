LAIDOUDI33/LAID_ODOO

[IMP] website: Using a testing user environment instead of super user

addons/website_sale/tests/test_abandoned_cart.py
@@ -21,7 +21,7 @@ class TestWebsiteSaleCartAbandonedCommon(TransactionCaseWithUserPortal):
                 email_got_sent = True
 
         with patch.object(MailTemplate, "send_mail", check_send_mail_called):
-            self.env["website"]._send_abandoned_cart_email()
+            self.env["website"].sudo()._send_abandoned_cart_email()
         return email_got_sent
 
 

addons/website_sale_stock/tests/test_website_sale_stock_abandoned_cart_email.py
@@ -14,13 +14,19 @@ from odoo.addons.website_sale_stock.tests.common import WebsiteSaleStockCommon
 class TestWebsiteSaleStockAbandonedCartEmail(
     TestWebsiteSaleCartAbandonedCommon, WebsiteSaleStockCommon
 ):
-    _test_user_groups = None  # FIXME list needed groups
+    _test_user_groups = (
+        'base.group_user',
+        'product.group_product_manager',
+        'sales_team.group_sale_manager',  # FIXME: use sales_team.group_sale_salesman
+    )
+
+    _test_user_name = 'Test Sales & Product Manager'
 
     def test_website_sale_stock_abandoned_cart_email(self):
         """Make sure the send_abandoned_cart_email method sends the correct emails."""
         website = self.env.ref('base.default_website')
-        website.send_abandoned_cart_email = True
-        website.write({
+        website.sudo().send_abandoned_cart_email = True
+        website.sudo().write({
             "send_abandoned_cart_email_activation_time": (
                 datetime.utcnow() - relativedelta(hours=website.cart_abandoned_delay)
             )

odoo/odoo

addons/website_sale/tests/test_abandoned_cart.py
@@ -27,7 +27,7 @@ class TestWebsiteSaleCartAbandonedCommon(TransactionCaseWithUserPortal):
                 self.env.registry["ir.cron"], "_commit_progress", return_value=float("inf")
             ),
         ):
-            self.env["website"]._cron_send_abandoned_cart_email()
+            self.env["website"].sudo()._cron_send_abandoned_cart_email()
         return email_got_sent
 
 

addons/website_sale_stock/tests/test_website_sale_stock_abandoned_cart_email.py
@@ -14,13 +14,19 @@ from odoo.addons.website_sale_stock.tests.common import WebsiteSaleStockCommon
 class TestWebsiteSaleStockAbandonedCartEmail(
     TestWebsiteSaleCartAbandonedCommon, WebsiteSaleStockCommon
 ):
-    _test_user_groups = None  # FIXME list needed groups
+    _test_user_groups = (
+        'base.group_user',
+        'product.group_product_manager',
+        'sales_team.group_sale_manager',  # FIXME: use sales_team.group_sale_salesman
+    )
+
+    _test_user_name = 'Test Sales & Product Manager'
 
     def test_website_sale_stock_abandoned_cart_email(self):
         """Make sure the send_abandoned_cart_email method sends the correct emails."""
         website = self.env.ref('base.default_website')
-        website.send_abandoned_cart_followup = True
-        website.write({
+        website.sudo().send_abandoned_cart_followup = True
+        website.sudo().write({
             "send_abandoned_cart_email_activation_time": (
                 datetime.utcnow() - relativedelta(hours=website.cart_abandoned_delay)
             )