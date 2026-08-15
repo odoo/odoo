c761ad6e2166ce150e42e63f270124b1522d22c3
Krzysztof Magusiak (krma) <krma@odoo.com>
2026-08-15 21:18:52 +0000

    [IMP] phone_validation: simply the search implementation
    Simplify the cases implemented by letting the framework to handle
    negative operators. During the rewrite use the Query object and SQL
    instead of f-strings.

--- addons/phone_validation/models/mail_thread_phone.py
+++ addons/phone_validation/models/mail_thread_phone.py
@@ -5,7 +5,7 @@ import re
 from odoo import api, fields, models, _
 from odoo.exceptions import AccessError, UserError
 from odoo.fields import Domain
-from odoo.tools.sql import create_index, make_identifier
+from odoo.tools.sql import SQL, create_index, make_identifier
 
 PHONE_REGEX_PATTERN = r'[\s\\./\(\)\-]'
 
@@ -90,77 +90,59 @@ class MailThreadPhone(models.AbstractModel):
                              where=f'{fname} IS NOT NULL')
 
     def _search_phone_mobile_search(self, operator, value):
-        if operator == 'not in':
-            return Domain.AND(self._search_phone_mobile_search('!=', v) for v in value)
         if operator == 'in':
             return Domain.OR(self._search_phone_mobile_search('=', v) for v in value)
+        if operator in Domain.NEGATIVE_OPERATORS:
+            return NotImplemented
         value = value.strip() if isinstance(value, str) else value
         phone_fields = self._phone_get_phone_mobile_search_fields()
         if not phone_fields:
             raise UserError(_('Missing definition of phone fields.'))
 
         # search if phone/mobile is set or not
-        if (value is True or not value) and operator in ('=', '!='):
-            if value:
-                # inverse the operator
-                operator = '=' if operator == '!=' else '!='
-            op = Domain.AND if operator == '=' else Domain.OR
-            return op(Domain(phone_field, operator, False) for phone_field in phone_fields)
+        if (value is True or not value) and operator == '=':
+            domain = Domain.AND(Domain(phone_field, '=', False) for phone_field in phone_fields)
+            return ~domain if value else domain
 
         if not value:
             return Domain.TRUE
         if self._phone_search_min_length and len(value) < self._phone_search_min_length:
             raise UserError(_('Please enter at least 3 characters when searching a Phone number.'))
 
-        sql_operator = {'=like': 'LIKE', '=ilike': 'ILIKE'}.get(operator, operator)
-
-        if value.startswith('+') or value.startswith('00'):
-            if operator in Domain.NEGATIVE_OPERATORS:
-                # searching on +32485112233 should also finds 0032485112233 (and vice versa)
-                # we therefore remove it from input value and search for both of them in db
-                where_str = ' AND '.join(
-                    f"""model.{phone_field} IS NULL OR (
-                            REGEXP_REPLACE(model.{phone_field}, %s, '', 'g') {sql_operator} %s OR
-                            REGEXP_REPLACE(model.{phone_field}, %s, '', 'g') {sql_operator} %s
-                    )"""
-                    for phone_field in phone_fields
-                )
-            else:
-                # searching on +32485112233 should also finds 0032485112233 (and vice versa)
-                # we therefore remove it from input value and search for both of them in db
-                where_str = ' OR '.join(
-                    f"""model.{phone_field} IS NOT NULL AND (
-                            REGEXP_REPLACE(model.{phone_field}, %s, '', 'g') {sql_operator} %s OR
-                            REGEXP_REPLACE(model.{phone_field}, %s, '', 'g') {sql_operator} %s
-                    )"""
-                    for phone_field in phone_fields
-                )
-            query = f"SELECT model.id FROM {self._table} model WHERE {where_str};"
-
-            term = re.sub(PHONE_REGEX_PATTERN, '', value[1 if value.startswith('+') else 2:])
-            if operator not in ('=', '!='):  # for like operators
-                term = f'{term}%'
-            self.env.cr.execute(
-                query, (PHONE_REGEX_PATTERN, '00' + term, PHONE_REGEX_PATTERN, '+' + term) * len(phone_fields)
-            )
+        if operator == '=':
+            sql_operator = SQL('=')
+        elif operator == 'like' or operator == '=like':
+            sql_operator = SQL('LIKE')
+        elif operator == 'ilike' or operator == '=ilike':
+            sql_operator = SQL('ILIKE')
         else:
-            if operator in Domain.NEGATIVE_OPERATORS:
-                where_str = ' AND '.join(
-                    f"(model.{phone_field} IS NULL OR REGEXP_REPLACE(model.{phone_field}, %s, '', 'g') {sql_operator} %s)"
-                    for phone_field in phone_fields
+            return NotImplemented  # unsupported operator
+
+        if value.startswith(('+', '00')):
+            # searching on +32485112233 should also find 0032485112233 (and vice versa)
+            # we therefore remove it from input value and search for both of them in db
+            term = re.sub(PHONE_REGEX_PATTERN, '', value[1 if value.startswith('+') else 2:])
+            if operator != '=':  # for all like operators
+                term = f'{term}%'
+            return Domain.custom(to_sql=lambda t: SQL("(%s)", SQL(' OR ').join(
+                SQL("""REGEXP_REPLACE(%s, %s, '', 'g') %s %s
+                        OR REGEXP_REPLACE(%s, %s, '', 'g') %s %s
+                    """,
+                    t[phone_field], PHONE_REGEX_PATTERN, sql_operator, '00' + term,
+                    t[phone_field], PHONE_REGEX_PATTERN, sql_operator, '+' + term,
                 )
-            else:
-                where_str = ' OR '.join(
-                    f"(model.{phone_field} IS NOT NULL AND REGEXP_REPLACE(model.{phone_field}, %s, '', 'g') {sql_operator} %s)"
-                    for phone_field in phone_fields
-                )
-            query = f"SELECT model.id FROM {self._table} model WHERE {where_str};"
+                for phone_field in phone_fields
+            )))
+        else:
             term = re.sub(PHONE_REGEX_PATTERN, '', value)
-            if operator not in ('=', '!='):  # for like operators
+            if operator != '=':  # for all like operators
                 term = f'%{term}%'
-            self.env.cr.execute(query, (PHONE_REGEX_PATTERN, term) * len(phone_fields))
-        res = self.env.cr.fetchall()
-        return Domain('id', 'in', [r[0] for r in res])
+            return Domain.custom(to_sql=lambda t: SQL("(%s)", SQL(' OR ').join(
+                SQL("REGEXP_REPLACE(%s, %s, '', 'g') %s %s",
+                    t[phone_field], PHONE_REGEX_PATTERN, sql_operator, term
+                )
+                for phone_field in phone_fields
+            )))
 
     @api.depends(lambda self: self._phone_get_sanitize_triggers())
     def _compute_phone_sanitized(self):
