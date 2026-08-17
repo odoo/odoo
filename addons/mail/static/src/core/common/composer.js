841339e328b589ad9acab0e779bd706528d663d1
Jeremy Lawson (jelaw) <jelaw@odoo.com>
2026-08-17 13:10:43 -0400

    [FIX] mail: Continue using composer no crash
    Reproduction steps:
     - Open a record that has a chatter where you can log notes
     - Start logging a note in the composer
     - Close the composer
     - Click log note again
     - See "Continue with Full Composer?" popup
     - Hit escape, up, or down
     - See traceback
    
    This shouldnt really do anything, so this fix makes it do nothing instead
    of crashing.
    
    opw-6476660
    
    closes odoo/odoo#282803
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/core/common/composer.js
+++ addons/mail/static/src/core/common/composer.js
@@ -149,7 +149,7 @@ export class Composer extends Component {
             closeOnClickAway: false,
             closeOnEscape: false,
             position: "top-end",
-            popoverClass: "dropdown-menu bg-view overflow-visible o-rounded-bubble mx-1",
+            popoverClass: "dropdown-menu o-dropdown--menu bg-view overflow-visible o-rounded-bubble mx-1",
         });
         this.fullComposerBus = new EventBus();
         this.selection = useSelection({
