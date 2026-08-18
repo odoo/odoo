alkuneemi/Luxara

[IMP] website_sale_{stock,collect}: Click and collect, out of stock improvements
**Purpose:**
Improve the way click and collect work with out of stock products

**Specification:**
- Do not hide Add to Cart button for rental products
- Set the out of stock message inside the out of stock badge when we don't allow to sell out of stock products
- Move the out of stock message below the Add to cart button
- If Click and Collect is enabled, show the out of stock message as the click and collect availability message

Task-6081690

addons/website_sale_stock/static/tests/tours/website_sale_stock_multilang.js
@@ -9,7 +9,7 @@ registry.category("web_tour.tours").add('website_sale_stock.product_warning_mult
             expectUnloadPage: true,
         }, {
             content: "Check out of stock message",
-            trigger: '#out_of_stock_message:contains("Hors-stock")',
+            trigger: '#out_of_stock_message_badge:contains("Hors-stock"), [name="click_and_collect_availability"]:contains("Hors-stock")',
         }, {
             content: "Check price",
             trigger: 'span:contains("123,45")',
@@ -24,7 +24,7 @@ registry.category("web_tour.tours").add('website_sale_stock.product_warning_mult
             expectUnloadPage: true,
         }, {
             content: "Check out of stock message",
-            trigger: '#out_of_stock_message:contains("Out of stock")',
+            trigger: '#out_of_stock_message_badge:contains("Out of stock"), [name="click_and_collect_availability"]:contains("Out of stock")',
         }, {
             content: "Check price",
             trigger: 'span:contains("123.45")',
@@ -39,7 +39,7 @@ registry.category("web_tour.tours").add('website_sale_stock.product_warning_mult
             expectUnloadPage: true,
         }, {
             content: "Check out of stock message",
-            trigger: '#out_of_stock_message:contains("Hors-stock")',
+            trigger: '#out_of_stock_message_badge:contains("Hors-stock"), [name="click_and_collect_availability"]:contains("Hors-stock")',
         }, {
             content: "Check price",
             trigger: 'span:contains("123,45")',

odoo/odoo

addons/website_sale_stock/static/tests/tours/website_sale_stock_multilang.js
@@ -9,7 +9,7 @@ registry.category("web_tour.tours").add('website_sale_stock.product_warning_mult
             expectUnloadPage: true,
         }, {
             content: "Check out of stock message",
-            trigger: '#out_of_stock_message:contains("Hors-stock")',
+            trigger: '#out_of_stock_message:contains("Hors-stock"), [name="click_and_collect_availability"]:contains("Hors-stock")',
         }, {
             content: "Check price",
             trigger: 'span:contains("123,45")',
@@ -24,7 +24,7 @@ registry.category("web_tour.tours").add('website_sale_stock.product_warning_mult
             expectUnloadPage: true,
         }, {
             content: "Check out of stock message",
-            trigger: '#out_of_stock_message:contains("Out of stock")',
+            trigger: '#out_of_stock_message:contains("Out of stock"), [name="click_and_collect_availability"]:contains("Out of stock")',
         }, {
             content: "Check price",
             trigger: 'span:contains("123.45")',
@@ -39,7 +39,7 @@ registry.category("web_tour.tours").add('website_sale_stock.product_warning_mult
             expectUnloadPage: true,
         }, {
             content: "Check out of stock message",
-            trigger: '#out_of_stock_message:contains("Hors-stock")',
+            trigger: '#out_of_stock_message:contains("Hors-stock"), [name="click_and_collect_availability"]:contains("Hors-stock")',
         }, {
             content: "Check price",
             trigger: 'span:contains("123,45")',