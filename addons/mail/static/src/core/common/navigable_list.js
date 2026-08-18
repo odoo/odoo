c74620feca8813f8a4e7e396c4a8d73c0a2060ae
Sébastien Theys <seb@odoo.com>
2026-08-18 09:06:08 +0000

    [FIX] mail: select the suggestion shown on screen
    Before this commit, clicking a composer suggestion could leave the
    composer with the typed search instead of the selected name, as in the
    test "Mention a partner with special character (e.g. apostrophe ')" on
    runbot:
    
    Failed to find 1 of ".o-mail-Composer-input" with value "..." (Timeout
    of 10 seconds). Found 0 instead.
    
    This happens because NavigableList looks up the clicked option by index
    in its current props, while the item clicked comes from the last render.
    Typing "@" lists the two members of the channel and typing "Pyn" drops
    one of them: owl assigns the filtered options one frame before it
    patches the list, so a click in between looks up index 1 in a list of
    one option, finds nothing and returns.
    
    This commit passes the rendered option to the click handler, keeping the
    index lookup as a fallback so that the signature stays the same on a
    stable version.
    
    https://runbot.odoo.com/odoo/error/946154
    
    closes odoo/odoo#282942
    
    X-original-commit: a96dacc66f556af7500d33b09e9fd865ea86cfc4
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/mail/static/src/core/common/navigable_list.js
+++ addons/mail/static/src/core/common/navigable_list.js
@@ -116,8 +116,14 @@ export class NavigableList extends Component {
         }
     }
 
+    /**
+     * @param {Event} ev
+     * @param {number} index
+     * @param {Object} [params]
+     * @param {Object} [params.option] defaults to `props.options[index]`
+     */
     selectOption(ev, index, params = {}) {
-        const option = this.props.options[index];
+        const { option = this.props.options[index], ...onSelectParams } = params;
         if (!option) {
             return;
         }
@@ -126,7 +132,7 @@ export class NavigableList extends Component {
             return;
         }
         this.props.onSelect(ev, option, {
-            ...params,
+            ...onSelectParams,
         });
         this.close();
     }
