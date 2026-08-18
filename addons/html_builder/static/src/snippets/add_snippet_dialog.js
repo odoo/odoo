LAIDOUDI33/LAID_ODOO

[REF] htm_builder, html_editor: remove onWillRender usage
As part of the Owl 3 migration, replace deprecated onWillRender hooks
with the appropriate Owl 3 alternatives.

addons/html_builder/static/src/snippets/add_snippet_dialog.js
@@ -1,5 +1,4 @@
-import { onWillRender } from "@web/owl2/utils";
-import { Component, onMounted, onWillUnmount, props, proxy, signal, t, useApp } from "@odoo/owl";
+import { Component, onMounted, onWillUnmount, props, proxy, signal, t, useApp, useEffect } from "@odoo/owl";
 import { loadBundle, loadCSS } from "@web/core/assets";
 import { isBrowserFirefox } from "@web/core/browser/feature_detection";
 import { Dialog } from "@web/core/dialog/dialog";
@@ -96,7 +95,7 @@ export class AddSnippetDialog extends Component {
             this.state.showIframe = true;
         });
 
-        onWillRender(() => {
+        useEffect(() => {
             if (!this.props.snippetModel.hasCustomGroup && this.state.groupSelected === "custom") {
                 this.state.groupSelected = this.props.snippetModel.snippetGroups[0].groupName;
             }

odoo/odoo

addons/html_builder/static/src/snippets/add_snippet_dialog.js
@@ -1,5 +1,4 @@
-import { onWillRender } from "@web/owl2/utils";
-import { Component, onMounted, onWillUnmount, useProps, proxy, signal, t, useApp } from "@odoo/owl";
+import { Component, onMounted, onWillUnmount, useProps, proxy, signal, t, useApp, useEffect } from "@odoo/owl";
 import { loadBundle, loadCSS } from "@web/core/assets";
 import { isBrowserFirefox } from "@web/core/browser/feature_detection";
 import { Dialog } from "@web/core/dialog/dialog";
@@ -96,7 +95,7 @@ export class AddSnippetDialog extends Component {
             this.state.showIframe = true;
         });
 
-        onWillRender(() => {
+        useEffect(() => {
             if (!this.props.snippetModel.hasCustomGroup && this.state.groupSelected === "custom") {
                 this.state.groupSelected = this.props.snippetModel.snippetGroups[0].groupName;
             }