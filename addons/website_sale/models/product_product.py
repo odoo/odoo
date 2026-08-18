4791d5eb85796e723e6b946717c42811416e6e38
Pilar Vargas <pilar.vargas@tecnativa.com>
2026-08-18 09:32:08 +0000

    [IMP] website_sale: add product SKU to structured data
    Include `default_code` as the Schema.org `sku` property in the product
    structured data when it is set.
    
    TT64220
    
    closes odoo/odoo#284342
    
    X-original-commit: 0fcf651b44640d72c870f49c980cbdbf18f5facb
    Signed-off-by: Valeriya Chuprina (vchu) <vchu@odoo.com>

--- addons/website_sale/models/product_product.py
+++ addons/website_sale/models/product_product.py
@@ -204,6 +204,8 @@ class ProductProduct(models.Model):
         }
         if self.website_meta_description or self.description_sale:
             markup_data['description'] = self.website_meta_description or self.description_sale
+        if self.default_code:
+            markup_data["sku"] = self.default_code
         if self.barcode:
             markup_data['gtin'] = self.barcode
         return markup_data
