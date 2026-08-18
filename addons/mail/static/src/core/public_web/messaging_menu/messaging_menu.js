LAIDOUDI33/LAID_ODOO

[IMP] mail: allow custom tabs view

addons/mail/static/src/core/public_web/messaging_menu/messaging_menu.js
@@ -79,6 +79,10 @@ export class MessagingMenu extends Component {
         return !this.messages().length;
     }
 
+    get visibleTabs() {
+        return this.store.messagingMenu.visibleTabs;
+    }
+
     get noSearchResultText() {
         return this.searchTerm() ? _t('No results for "%s".', this.searchTerm()) : "";
     }

odoo/odoo

addons/mail/static/src/core/public_web/messaging_menu/messaging_menu.js
@@ -82,6 +82,10 @@ export class MessagingMenu extends Component {
         return !this.messages().length && !this.showPushPermissionRequest;
     }
 
+    get visibleTabs() {
+        return this.store.messagingMenu.sortedVisibleTabs;
+    }
+
     get noSearchResultText() {
         return this.searchTerm() ? _t('No results for "%s".', this.searchTerm()) : "";
     }