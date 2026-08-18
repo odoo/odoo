81a7eaa83b5776b61294dc2b8c6e7b3a5152adca
Mohamed-moali <moali@odoo.com>
2026-08-18 14:12:30 +0000

    [IMP] hr_recruitment: adding domains for hr.job
    In order to maintain proper job listings and make sure all working schedules are valid, working schedule domain is now depeding on the job listing company.
    
    closes odoo/odoo#284487
    
    Task: 6408914
    X-original-commit: d028f9eb0a92796ea92780e6de43aa0d369401a7
    Related: odoo/enterprise#129199
    Signed-off-by: Abdelrahman Mahmoud (amah) <amah@odoo.com>

--- addons/hr/models/hr_job.py
+++ addons/hr/models/hr_job.py
@@ -42,8 +42,8 @@ class HrJob(models.Model):
             position. The Recruiter is automatically added to all meetings with the Applicant.",
     )
     department_id = fields.Many2one('hr.department', string='Department', check_company=True, tracking=True, index='btree_not_null')
-    company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company, tracking=True)
-    contract_type_id = fields.Many2one('hr.contract.type', string='Contract Type', tracking=True)
+    company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company, tracking=True, domain=lambda self: [('id', 'in', self.env.companies.ids)])
+    contract_type_id = fields.Many2one('hr.contract.type', string='Employment Type', tracking=True)
 
     _name_company_uniq = models.Constraint(
         'unique(name, company_id, department_id)',
