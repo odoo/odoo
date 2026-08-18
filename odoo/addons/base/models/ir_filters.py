47bf5e2e5e91cfbebd30ee83fc92441d551001d3
thle-odoo <thle@odoo.com>
2026-08-18 14:42:39 +0200

    [FIX] base: remove defaults for shared filters
    A shared filter's context must be cleaned, to prevent it from
    polluting the contexts of other users who use the filter.
    
    Task-6461702

--- odoo/addons/base/models/ir_filters.py
+++ odoo/addons/base/models/ir_filters.py
@@ -2,6 +2,7 @@
 import ast
 
 from odoo import api, fields, models
+from odoo.tools.misc import clean_context
 
 
 class IrFilters(models.Model):
@@ -39,6 +40,14 @@ class IrFilters(models.Model):
         "Invalid sort definition",
     )
 
+    def _sanitize_shared_context(self):
+        for shared_filter in self.filtered(lambda f: f.user_ids != self.env.user):
+            context = ast.literal_eval(shared_filter.context)
+            cleaned = clean_context(context)
+            defaults = context.keys() - cleaned.keys()
+            if defaults:
+                shared_filter.context = repr(cleaned)
+
     @api.model
     def _list_all_models(self):
         lang = self.env.lang or 'en_US'
@@ -57,9 +66,17 @@ class IrFilters(models.Model):
                 del vals['embedded_parent_res_id']
         return [dict(vals, name=self.env._("%s (copy)", ir_filter.name)) for ir_filter, vals in zip(self, vals_list)]
 
+    @api.model_create_multi
+    def create(self, vals_list):
+        filters = super().create(vals_list)
+        filters._sanitize_shared_context()
+        return filters
+
     def write(self, vals):
         new_filter = super().write(vals)
         self.check_access('write')
+        if 'context' in vals or 'user_ids' in vals:
+            self._sanitize_shared_context()
         return new_filter
 
     def _get_eval_domain(self):
