8fcdce7fa3cccb2383457eb5281eceb731032043
Louis Gobert <loug@odoo.com>
2026-08-18 10:16:00 +0000

    [FIX] account: fix out_receipt move type in analysis
    When enabling Sale/Purchase Receipts and trying to open the form view
    from Invoice Analysis, an error occurs.
    
    The issue is caused by the `move_type` used in `_where()`, which includes
    a type that is not defined in the `move_type` field selection.
    
    Steps to reproduce:
    - Enable Sale/Purchase Receipts.
    - Create a Sale/Purchase Receipt for partner A and confirm it
    - Go to Invoice Analysis and open the Pivot view
    - Click on a cell to open partner A's receipt, then try to open the form
    view
    - Error
    
    opw-6462153
    
    closes odoo/odoo#288216
    
    X-original-commit: a9cdfe0974a399fa5cfd411721932cb0dce630c7
    Signed-off-by: Louis Gobert (loug) <loug@odoo.com>
    Signed-off-by: Paolo Gatti (pgi) <pgi@odoo.com>

--- addons/account/report/account_invoice_report.py
+++ addons/account/report/account_invoice_report.py
@@ -29,6 +29,8 @@ class AccountInvoiceReport(models.Model):
         ('in_invoice', 'Vendor Bill'),
         ('out_refund', 'Customer Credit Note'),
         ('in_refund', 'Vendor Credit Note'),
+        ('out_receipt', 'Sales Receipt'),
+        ('in_receipt', 'Purchase Receipt'),
         ], readonly=True)
     state = fields.Selection([
         ('draft', 'Draft'),
@@ -150,8 +152,8 @@ class AccountInvoiceReport(models.Model):
     @api.model
     def _where(self) -> SQL:
         return SQL(
-            '''
-            WHERE move.move_type IN ('out_invoice', 'out_refund', 'in_invoice', 'in_refund', 'out_receipt', 'in_receipt')
+            f'''
+            WHERE move.move_type IN ({','.join(f"'{t[0]}'" for t in self.env['account.invoice.report']._fields['move_type'].selection)})
                 AND line.account_id IS NOT NULL
                 AND line.display_type = 'product'
             ''',
