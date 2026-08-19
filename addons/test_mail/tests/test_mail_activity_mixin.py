ca39c01925fab19d9e768d64dc5a97da42891a48
Pierre-Yves Dufays <pydu@odoo.com>
2026-08-19 15:52:55 +0200

    [IMP] {test_}mail: ensure activity type consistency by falling back to default type
    Although unrelated to this PR, the TODO regarding activity type model
    consistency has been resolved by always resetting to a default compatible
    type when the activity type is not consistent (not present or not compatible).
    
    We also remove the invalid activity type from the activity values if present,
    to prevent the incompatible type from being restored.
    
    Task-6255045
    
    closes odoo/odoo#277467
    
    Signed-off-by: Stéphane Debauche (std) <std@odoo.com>

--- addons/test_mail/tests/test_mail_activity_mixin.py
+++ addons/test_mail/tests/test_mail_activity_mixin.py
@@ -694,6 +694,31 @@ class TestActivityMixin(TestActivityCommon):
             ])
             self.assertFalse(record, "Should not find record if the only late activity is done")
 
+    @mute_logger('odoo.addons.mail.models.mail_activity_mixin')
+    def test_activity_schedule_create_vals_inconsistent_type_falls_back(self):
+        """An activity type restricted to another model must be replaced by the model default type."""
+        fallback_type = self.env['mail.activity.type'].create({
+            'name': 'Fallback Type',
+            'res_model': self.test_record._name,
+            'sequence': -10,  # ensure it is the default type for self.test_record
+        })
+        wrong_model_type = self.env['mail.activity.type'].create({
+            'name': 'Wrong Model Type',
+            'res_model': 'mail.test.simple',
+        })
+
+        create_vals = self.test_record._activity_schedule_create_vals(
+            activity_type_id=wrong_model_type.id,
+            note='<p>Plan note</p>',
+            summary='Plan summary',
+            user_id=self.user_employee.id,
+        )
+        self.assertEqual(len(create_vals), 1)
+        self.assertEqual(create_vals[0]['activity_type_id'], fallback_type.id)
+        self.assertEqual(create_vals[0]['note'], '<p>Plan note</p>')
+        self.assertEqual(create_vals[0]['summary'], 'Plan summary')
+        self.assertEqual(create_vals[0]['user_id'], self.user_employee.id)
+
     @users('employee')
     def test_record_unlink(self):
         test_record = self.test_record.with_user(self.env.user)
