5e9babde092690015fe9f0024457b2fd71b47731
josm-odoo <josm@odoo.com>
2026-08-19 16:16:43 -0400

    [FIX] mail: cron not being found for batch mass emails
    Issue:
    
    When the process_email_queue runs its not commiting the progress becuase self.env.get('ir_cron') returns nothing
    
    To Replicate:
    
    1) Create a new DB and install Contacts
    2) make a couple contacts and set an email for them
    3) I created a server action to create X amount of mail.mail records
    4) go into the ir_cron (Scheduled action) Labeled 'Mail: Email Queue Manager'
    5) Click 'Run Manually'
    6) Inside the logs you will see it stop abruptly after the first batch.
    7) Set a breakpoint at the line below and 'self.env.get('ir_cron')' returns nothing.
    
    the logging line that records the processed batch to put a breakpoint up to go up the stack [Link](https://github.com/odoo/odoo/blob/446ef9882f4c7f092da5a29cc639ce54121ecd8d/addons/mail/models/mail_mail.py#L739-L741)
    
    Fix: make the get call on the context which is where the cron_id is located.
    
    opw-6477492
    
    The original issue was fixed with PR [282892](https://github.com/odoo/odoo/pull/282892.
    Removed the main code changes but keeping the tests.

--- addons/mail/tests/test_mail_mail.py
+++ addons/mail/tests/test_mail_mail.py
@@ -1,10 +1,11 @@
 from odoo.tests import TransactionCase, tagged
+from odoo.addons.mail.tests.common import MockSmtplibCase
 from unittest import mock
 import smtplib
 
 
 @tagged('mail_mail')
-class MailCase(TransactionCase):
+class MailCase(TransactionCase, MockSmtplibCase):
 
     def test_mail_send_non_connected_smtp_session(self):
         """Check to avoid SMTPServerDisconnected error while trying to
@@ -29,3 +30,31 @@ class MailCase(TransactionCase):
         )
         # if we get here SMTPServerDisconnected was not raised
         self.assertEqual(mail.state, "outgoing")
+
+    def test_batch_send_through_cron(self):
+        """
+        Test the detection of the cron when processesing "Mail: Email Queue Manager"
+        'code' : 'model.process_email_queue()'. With the change from _notify_progress
+        to _commit_progress we need to properly detect the cron model for batch processing
+        """
+        post_data = [{
+            "body": "Hello Peter",
+            "email_add_signature": False,
+            "message_type": "comment",
+            "email_to": ["peter.parker@marvel.com"],
+
+        }] * 50
+        messages = self.env["mail.mail"].create(post_data)
+        self.assertEqual(len(messages.filtered_domain([["state", "=", "outgoing"]])), 50)
+
+        mail_cron = self.env.ref('mail.ir_cron_mail_scheduler_action')
+        mail_cron.write({"code": "model.process_email_queue(batch_size=5)"})  # changing the batch size from default 1000 to 5
+
+        # Establish a mock smtp connection and enter the test mode for registry
+        # for crons otherwise it wont commit
+        with self.mock_smtplib_connection(), self.enter_registry_test_mode():
+            mail_cron.method_direct_trigger()
+
+        messages = self.env["mail.mail"].browse(messages.ids)
+        self.assertEqual(len(messages.filtered_domain([["state", "=", "outgoing"]])), 0)
+        self.assertEqual(len(messages.filtered_domain([["state", "=", "sent"]])), 50)
