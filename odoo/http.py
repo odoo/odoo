0b68de638e19b810faa6b1d896ffda575784548a
Julien Castiaux <juc@odoo.com>
2026-08-19 22:22:52 +0000

    [FIX] core: --dev=werkzeug is long gone
    The feature was removed in odoo/odoo#115076
    
    X-original-commit: 4ff28d8b863f33135b8b55c28f18fe24a3de7e2d

--- odoo/http.py
+++ odoo/http.py
@@ -2585,11 +2585,6 @@ class Request:
     def _update_served_exception(self, exc):
         if isinstance(exc, HTTPException) and exc.code is None:
             return exc  # bubble up to _serve_db
-        if (
-            'werkzeug' in config['dev_mode']
-            and self.dispatcher.routing_type != JsonRPCDispatcher.routing_type
-        ):
-            return exc  # bubble up to werkzeug.debug.DebuggedApplication
         if not hasattr(exc, 'error_response'):
             if isinstance(exc, AccessDenied):
                 exc.suppress_traceback()
