0f1bbff363737c3f2005e1f72867059233570c8b
Benjamin Vray <bvr@odoo.com>
2026-08-19 10:35:34 +0200

    [IMP] website: simplify form field addition
    Steps to reproduce:
    - Drag and drop a "Form" snippet onto the page.
    - Select one of its fields.
    - Click the `+ New` button in the field header.
    
    Before this commit, the button opened a submenu to add a field or other
    content after the selected field.
    
    After this commit, the `+ Field` button directly adds a new field after
    the selected field, consistently with the form header button.
    
    task-6259086

--- addons/website/static/src/builder/plugins/form/form_option_add_field_button.js
+++ addons/website/static/src/builder/plugins/form/form_option_add_field_button.js
@@ -1,34 +1,6 @@
 import { useOperation } from "@html_builder/core/operation_plugin";
 import { BaseOptionComponent } from "@html_builder/core/base_option_component";
 import { useProps, t } from "@odoo/owl";
-import { Dropdown } from "@web/core/dropdown/dropdown";
-import { DropdownItem } from "@web/core/dropdown/dropdown_item";
-
-const FORM_INNER_SNIPPETS = [
-    {
-        name: "Title",
-        id: "s_inline_text",
-        build: (el) => {
-            const span = document.createElement("span");
-            span.textContent = "Add a Section Title";
-            span.className = "h2-fs";
-            el.style.textAlign = "center";
-            el.replaceChildren(span);
-            return el;
-        },
-    },
-    {
-        name: "Text",
-        id: "s_inline_text",
-        build: (el) => {
-            el.style.textAlign = "center";
-            el.textContent =
-                "Add or adjust these fields to collect the information relevant to your needs.";
-            return el;
-        },
-    },
-    { name: "Separator", id: "s_hr" },
-];
 
 export class FormOptionAddFieldButton extends BaseOptionComponent {
     static template = "website.s_website_form_form_option_add_field_button";
@@ -44,28 +16,6 @@ export class FormOptionAddFieldButton extends BaseOptionComponent {
     addField() {
         this.callOperation(() => {
             this.props.addField(this.env.getEditingElement());
-        });
-    }
-}
-
-export class FormOptionAddContentDropdown extends BaseOptionComponent {
-    static template = "website.s_website_form_form_option_add_snippet_dropdown";
-    static components = {
-        DropdownItem,
-        Dropdown,
-    };
-    props = useProps({
-        addField: t.function(),
-        tooltip: t.string(),
-    });
-
-    setup() {
-        this.callOperation = useOperation();
-        this.snippets = [{ name: "Field", id: "field" }, ...FORM_INNER_SNIPPETS];
-    }
-    addField(config) {
-        this.callOperation(() => {
-            this.props.addField(this.env.getEditingElement(), config);
         });
     }
 }
