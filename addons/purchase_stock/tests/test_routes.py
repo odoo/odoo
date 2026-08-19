bc46bb464f166d453b0b80d176b39d2bc9859937
modm-odoo <modm@odoo.com>
2026-08-19 10:31:55 +0000

    [FIX] purchase_stock: filter buy route in multi-company environment
    Steps to reproduce:
    1. Install the Inventory and Purchase module and enable Multi-Step Routes.
    2. Open the Buy route and set Company A as its company.
    3. Create a user who has access only to Company B.
    4. Log in as this user and navigate to Products.
    5. Click New to create a product.
    6. An AccessError is raised because the Buy route belongs to Company A, which is
    not accessible to the user.
    
    Issue:
    
    The buy route returned by `env.ref` is not necessarily applicable to the
    current company. Returning `buy_route.ids` directly can therefore cause
    issues when creating a product in a multi-company environment.
    
    When the user has access to multiple companies, the buy route belonging
    to another company can be selected during product creation. When the
    user does not have access to the company of the buy route, this can
    instead result in an AccessError.
    
    The previous implementation performed an ORM search on `stock.route`,
    which implicitly filtered the route according to the current environment.
    However, that query was removed as part of this pr
    https://github.com/odoo/odoo/pull/276554.
    
    Solution:
    
    Filter the route returned by env.ref against the companies available in
    the current environment before returning its ID.
    
    Use sudo() while filtering so that the route's company_id can be read
    even when the route belongs to a company inaccessible to the current
    user. The route is still explicitly checked against env.companies, so
    sudo() does not allow an inaccessible company's route to be returned.
    
    opw-6448497
    
    closes odoo/odoo#284179
    
    X-original-commit: e5ab4821f9cbcb6871b8e2399828ae626a1878cb
    Signed-off-by: Piryns Victor (pivi) <pivi@odoo.com>
    Signed-off-by: Marag Modh <modm@odoo.com>

--- addons/purchase_stock/tests/test_routes.py
+++ addons/purchase_stock/tests/test_routes.py
@@ -107,3 +107,45 @@ class TestRoutes(TransactionCase):
 
         forecast = product.with_context(location=sub_location.id).virtual_available
         self.assertEqual(forecast, 10.0, "forecasted quantity should increment to 10.0 units")
+
+    def test_product_create_with_inaccessible_buy_route(self):
+        """
+        Creating a product should not raise an AccessError when the buy route
+        belongs to a company the user cannot access.
+        """
+        company_a = self.env.company
+        company_b = self.env['res.company'].create({
+            'name': 'Company B',
+        })
+        buy_route = self.env.ref('purchase_stock.route_warehouse0_buy')
+        buy_route.rule_ids.unlink()
+        buy_route.company_id = company_a
+
+        # The user can only access Company B.
+        user = self.env['res.users'].create({
+            'name': 'Purchase User',
+            'login': 'purchase_user_test',
+            'company_id': company_b.id,
+            'company_ids': [(6, 0, [company_b.id])],
+            'groups_id': [(6, 0, [self.env.ref('purchase.group_purchase_manager').id])],
+        })
+
+        Product = (
+            self.env['product.template']
+            .with_user(user)
+            .with_company(company_b)
+        )
+
+        self.assertFalse(
+            Product._get_buy_route(),
+            'A route from an inaccessible company must not be used by default',
+        )
+        with Form(Product) as product_form:
+            product_form.name = 'Test Product'
+
+        buy_route.company_id = company_b
+        self.assertEqual(
+            Product._get_buy_route(),
+            buy_route.ids,
+            'A route from the active company must still be used by default',
+        )
