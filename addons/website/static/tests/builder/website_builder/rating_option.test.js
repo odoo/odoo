f72d17194e6edb7f630b00394a32512f9241477b
Julien Piron <jupir@odoo.com>
2026-08-20 15:10:23 +0200

    [FIX] html_builder, mass_mailing, website: update aria-label for rating
    aria-label was not used correctly on s_rating snippet. Moreover, it was
    not updated when the number of stars is updated.
    
    Now, aria-label is updated correctly.
    
    Task-6009931

--- addons/website/static/tests/builder/website_builder/rating_option.test.js
+++ addons/website/static/tests/builder/website_builder/rating_option.test.js
@@ -3,10 +3,29 @@ import {
     setupWebsiteBuilderWithSnippet,
 } from "@website/../tests/builder/website_helpers";
 import { expect, test } from "@odoo/hoot";
+import { contains } from "@web/../tests/web_test_helpers";
 
 defineWebsiteModels();
 
 test("rating snippet should not be user-selectable", async () => {
     await setupWebsiteBuilderWithSnippet("s_rating", { loadIframeBundles: true });
     expect(":iframe .s_rating").toHaveStyle({ "user-select": "none" });
+});
+
+test("changing score should update the aria-label", async () => {
+    await setupWebsiteBuilderWithSnippet("s_rating", { loadIframeBundles: true });
+
+    await contains(":iframe .s_rating").click();
+
+    await contains("[data-action-id='activeIconsNumber'] input").clear();
+    await contains("[data-action-id='activeIconsNumber'] input").fill("2");
+
+    expect(":iframe .s_rating_icons").toHaveAttribute("aria-label", "2 out of 5 stars");
+
+    await contains("[data-action-id='activeIconsNumber'] input").clear();
+    await contains("[data-action-id='activeIconsNumber'] input").fill("1");
+    await contains("[data-action-id='totalIconsNumber'] input").clear();
+    await contains("[data-action-id='totalIconsNumber'] input").fill("0");
+
+    expect(":iframe .s_rating_icons").toHaveAttribute("aria-label", "1 out of 10 stars");
 });
