30b91b18fa8409ac2f544ccbd58c0392dd438f5a
hatr-odoo <hatr@odoo.com>
2026-08-20 05:18:30 +0000

    [FIX] account,*: provide correct dependency to `is_used`
    Currently, `is_used` is computed using queries on `account.move.line`,
    `account.reconcile.model.line`, etc. As a result, it has no depends and is not
    automatically updated when records in either model are created, modified,
    or deleted.
    
    This commit reverse M2M fields for respective models and use it as
    dependency to `_compute_is_used`.
    
    It also adds a missing dependency of `is_used` to
    `_compute_repartition_lines_str`.
    
    X-original-commit: 3d7a04ca7288056878cfcd1ca55b85535c86b3c0

--- addons/account/tests/test_account_tax.py
+++ addons/account/tests/test_account_tax.py
@@ -373,7 +373,6 @@ class TestAccountTax(AccountTestInvoicingCommon, MailCase):
                 }),
             ],
         })
-        tax_invoice.invalidate_model(fnames=['is_used'])
         self.assertTrue(tax_invoice.is_used)
 
         # Account.reconcile is another of transaction
@@ -388,7 +387,6 @@ class TestAccountTax(AccountTestInvoicingCommon, MailCase):
                 'tax_ids': [Command.set(tax_reconciliation.ids)],
             })],
         })
-        tax_reconciliation.invalidate_model(fnames=['is_used'])
         self.assertTrue(tax_reconciliation.is_used)
 
     def test_tax_no_duplicate_in_repartition_line(self):
--- addons/point_of_sale/models/pos_order_line.py
+++ addons/point_of_sale/models/pos_order_line.py
@@ -41,7 +41,14 @@ class PosOrderLine(models.Model):
     is_total_cost_computed = fields.Boolean(help="Allows to know if the total cost has already been computed or not")
     discount = fields.Float(string='Discount (%)', digits=0)
     order_id = fields.Many2one('pos.order', string='Order Ref', ondelete='cascade', required=True, index=True)
-    tax_ids = fields.Many2many('account.tax', string='Taxes', readonly=True)
+    tax_ids = fields.Many2many(
+        comodel_name='account.tax',
+        relation='account_tax_pos_order_line_rel',
+        column1='pos_order_line_id',
+        column2='account_tax_id',
+        string='Taxes',
+        readonly=True,
+    )
     tax_ids_after_fiscal_position = fields.Many2many('account.tax', compute='_get_tax_ids_after_fiscal_position', string='Taxes to Apply')
     product_uom_id = fields.Many2one('uom.uom', string='Product Unit', related='product_id.uom_id')
     currency_id = fields.Many2one('res.currency', related='order_id.currency_id')
