5a795b4af3024cb7c8ef10bbc4d0b60e28b8e028
paan-odoo <paan@odoo.com>
2026-08-20 15:40:13 +0530

    [IMP] pos_{, restaurant}: allow reprinting the whole order
    Before this commit:
    
    - From the Product Screen, users could only reprint the last order change, while
      from the Ticket Screen, they could reprint all previous order changes one by
      one.
    
    After this commit:
    
    - Users can now reprint the entire order as an order change directly from both
      the Product Screen and the Ticket Screen.
    
    Task-6230594
    
    X-original-commit: b369bef588a09e631270a0b55970d4268f309728

--- addons/point_of_sale/static/src/app/screens/ticket_screen/ticket_screen.js
+++ addons/point_of_sale/static/src/app/screens/ticket_screen/ticket_screen.js
@@ -203,9 +203,11 @@ export class TicketScreen extends Component {
         }
     }
     async onClickReprintAll(order) {
-        const printingChanges = order.lastPrints;
-        if (printingChanges.length) {
-            await this.pos.printChanges(order, printingChanges, true);
+        order.uiState.isReprinting = true;
+        try {
+            await this.pos.sendOrderInPreparation(order);
+        } finally {
+            order.uiState.isReprinting = false;
         }
     }
     async onNextPage() {
