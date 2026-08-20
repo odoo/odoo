9c9c79d197cb4623d2bdd24aa860d6396b9a9233
Ahmed Naem (Ahnae) <ahnae@odoo.com>
2026-08-20 08:03:47 +0000

    [IMP] hr_holidays: expose create_calendar_meeting on hr.leave.type
    The `create_calendar_meeting` field on `hr.leave.type` allows users to
    choose if leave requests created with a given time off type generate a
    corresponding entry in the Calendar app. However, this field was not
    displayed on the form view.
    
    This commit adds `create_calendar_meeting` to the `hr.leave.type` form view
    inside the configuration section, along with dedicated help text explaining
    its behavior.
    
    Task: 6445794
    X-original-commit: 511f6602cfb4089fc6663fdb595d813d14d316de

--- addons/hr_holidays/models/hr_work_entry_type.py
+++ addons/hr_holidays/models/hr_work_entry_type.py
@@ -36,7 +36,12 @@ class HrWorkEntryType(models.Model):
     def _model_sorting_key(self, work_entry_type):
         return work_entry_type.sequence
 
-    create_calendar_meeting = fields.Boolean(string="Display Time Off in Calendar", default=True, tracking=True)
+    create_calendar_meeting = fields.Boolean(
+        string="Display Time Off in Calendar",
+        default=True,
+        tracking=True,
+        help="If this field is checked, every leave request of this type will have a corresponding entry in the calendar application. There will be no entry if this stays unchecked.",
+    )
     color = fields.Integer(string='Color', help="The color selected here will be used in every screen with the time type.")
     hide_on_dashboard = fields.Boolean(default=False, string="Hide On Dashboard", tracking=True, help="Non-visible allocations can still be selected when taking a leave, but will simply not be displayed on the leave dashboard.")
 
