eebd397822ff99c24d66d88565248dc6b72bda52
hatr-odoo <hatr@odoo.com>
2026-08-20 05:18:30 +0000

    [FIX] account,*: provide correct dependency to `is_used`
    Currently, `is_used` is computed using queries on `account.move.line`,
    `account.reconcile.model.line`, etc. As a result, it has no depends and is not
    automatically updated when records in either model are created, modified,
    or deleted.
    
    This commit reverse M2M fields for respective models and use it as
    dependency to `_compute_is_used`.
    
    It also adds a missing dependency of `is_used` to
    `_compute_repartition_lines_str`.
    
    closes odoo/odoo#283848
    
    X-original-commit: d3302d6e642ebc9a0474bb1443482fd73e1f886b
    Signed-off-by: William André (wan) <wan@odoo.com>

--- addons/account/tests/test_account_tax.py
+++ addons/account/tests/test_account_tax.py
@@ -223,7 +223,6 @@ class TestAccountTax(AccountTestInvoicingCommon):
                 }),
             ],
         })
-        tax_invoice.invalidate_model(fnames=['is_used'])
         self.assertTrue(tax_invoice.is_used)
 
         # Account.reconcile is another of transaction
@@ -240,7 +239,6 @@ class TestAccountTax(AccountTestInvoicingCommon):
                 'tax_ids': [Command.set(tax_reconciliation.ids)],
             })],
         })
-        tax_reconciliation.invalidate_model(fnames=['is_used'])
         self.assertTrue(tax_reconciliation.is_used)
 
     def test_tax_no_duplicate_in_repartition_line(self):
