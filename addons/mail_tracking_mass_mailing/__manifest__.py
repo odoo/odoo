a5c6e2a9c8eea0cd3a42d2b214ebe9ad7f762db3
Thibault Delavallée <tde@odoo.com>
2026-08-20 15:40:13 +0200

    [IMP] mail_tracking_mass_mailing: track source of message
    When 'mail_tracking' is installed, message post source should also
    track when messages come from mailings. This commit adds field and
    code to store source_mailing_id.
    
    Task-6368610 [mail] Track content source
    Part of Task-6260135 [mail] Ability to review outgoing content

---
+++ addons/mail_tracking_mass_mailing/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Mass Mailing Tracking',
+    'summary': 'Track source of messages by mass mailing',
+    'category': 'Productivity/Discuss',
+    'depends': [
+        'mail_tracking',
+        'mass_mailing',
+    ],
+    'data': [
+        'views/mail_message_views.xml',
+    ],
+    'author': 'Odoo S.A.',
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
