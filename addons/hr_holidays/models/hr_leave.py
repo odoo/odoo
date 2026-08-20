39b584371ef1596fa5d41d959ab9d6b0d2f7e18f
romc-odoo <romc@odoo.com>
2026-08-20 11:32:59 +0000

    [FIX] hr_holidays: do not recompute work entry type of validated leaves
    We should recompute the work entry type of only draft leaves.
    
    closes odoo/odoo#283736
    
    X-original-commit: e82c6f13b19864bc7fe31d50893252f838b1b54a
    Signed-off-by: Jurgen Gjini (jugj) <jugj@odoo.com>
    Signed-off-by: Romain Carlier (romc) <romc@odoo.com>

--- addons/hr_holidays/models/hr_leave.py
+++ addons/hr_holidays/models/hr_leave.py
@@ -425,7 +425,7 @@ class HrLeave(models.Model):
             ])
         ])
         versions = self.env['hr.version'].sudo().search(domain)
-        return versions.filtered(lambda v: v._is_overlapping_period(self.date_from.date(), self.date_to.date()))
+        return versions.filtered(lambda v: v._is_overlapping_period(self.request_date_from, self.request_date_to))
 
     @api.constrains('date_from', 'date_to')
     def _check_contracts(self):
@@ -515,7 +515,8 @@ class HrLeave(models.Model):
 
     @api.depends('employee_id', 'request_date_from', 'request_date_to')
     def _compute_work_entry_type_id(self):
-        for holiday in self:
+        draft_leaves = self.filtered(lambda l: l.state == 'confirm')
+        for holiday in draft_leaves:
             local_work_entry_types = self.env['hr.work.entry.type'].with_context(default_date_from=holiday.request_date_from, default_date_to=holiday.request_date_to).search([('country_id', 'in', [holiday.employee_id.company_id.country_id.id] + [False])])
             all_valid_work_entry_types = local_work_entry_types.with_context(default_date_from=holiday.request_date_from, default_date_to=holiday.request_date_to).filtered_domain([('has_valid_allocation', '=', True)])
             no_allocation_required_work_entry_types = local_work_entry_types.with_context(default_date_from=holiday.request_date_from, default_date_to=holiday.request_date_to).filtered_domain([('requires_allocation', '=', False)])
