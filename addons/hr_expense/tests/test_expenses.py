af221d1f2d4b3e5711c4aa5653ed95e8fc7979c7
hatr-odoo <hatr@odoo.com>
2026-08-20 05:18:30 +0000

    [FIX] account,*: provide correct dependency to `is_used`
    Currently, `is_used` is computed using queries on `account.move.line`,
    `account.reconcile.model.line`, etc. As a result, it has no depends and is not
    automatically updated when records in either model are created, modified,
    or deleted.
    
    This commit reverse M2M fields for respective models and use it as
    dependency to `_compute_is_used`.
    
    It also adds a missing dependency of `is_used` to
    `_compute_repartition_lines_str`.
    
    X-original-commit: d3302d6e642ebc9a0474bb1443482fd73e1f886b

--- addons/hr_expense/tests/test_expenses.py
+++ addons/hr_expense/tests/test_expenses.py
@@ -898,7 +898,6 @@ class TestExpenses(TestExpenseCommon):
         })
 
         self.create_expenses({'tax_ids': [Command.set(tax_expense.ids)]})
-        tax_expense.invalidate_model(fnames=['is_used'])
         self.assertTrue(tax_expense.is_used)
 
     def test_expense_by_company_with_caba_tax(self):
