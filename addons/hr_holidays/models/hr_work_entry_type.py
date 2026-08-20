035373268b58f664d9720da00746a29d39b055aa
Rana <raame@odoo.com>
2026-08-20 10:26:28 +0200

    [IMP] hr, hr_holidays, hr_work_entry: Payroll UX Imp
    UX Improvement for payroll
    
    Task:6456624
    
    closes odoo/odoo#283457
    
    Related: odoo/enterprise#128551
    Related: odoo/upgrade#11193
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/hr_holidays/models/hr_work_entry_type.py
+++ addons/hr_holidays/models/hr_work_entry_type.py
@@ -91,7 +91,7 @@ class HrWorkEntryType(models.Model):
         ('hour', 'Custom Hours')], default='day', string='Duration Type', required=True,
         tracking=True,
         help="""Define the minimum time off duration in which an employee can take when requesting a leave""")
-    unit_of_measure = fields.Selection([('hour', 'Hours'), ('day', 'Days')], default="hour", string="Unit of Measure", required=True,
+    unit_of_measure = fields.Selection([('hour', 'Hours'), ('day', 'Days')], default="hour", string="Unit", required=True,
                                        tracking=True,
                                        help="Define if the time type will be allocated/accrued in hours or days")
     unpaid = fields.Boolean('Is Unpaid', default=False, tracking=True)
@@ -390,9 +390,9 @@ been taken for this time off type. Changing it now would affect existing employe
                 is_popover = self.env.context.get("is_popover", False)
                 is_hour = record.unit_of_measure == "hour"
                 if is_popover and is_hour:
-                    name = self.env._("%(name)s (%(time)g/%(maximum)g hours)", name=record.name, time=remaining_time, maximum=maximum)
+                    name = self.env._("%(name)s (%(time)g hours left)", name=record.name, time=remaining_time)
                 elif is_popover:
-                    name = self.env._("%(name)s (%(time)g/%(maximum)g days)", name=record.name, time=remaining_time, maximum=maximum)
+                    name = self.env._("%(name)s (%(time)g days left)", name=record.name, time=remaining_time)
                 elif is_hour:
                     name = self.env._("%(name)s (%(time)g remaining out of %(maximum)g hours)", name=record.name, time=remaining_time, maximum=maximum)
                 else:
