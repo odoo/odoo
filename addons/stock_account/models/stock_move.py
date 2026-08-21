LAIDOUDI33/LAID_ODOO

[CLN] stock_account: remove _get_value_from_lots, rename to _run_avco and _get_fifo_value
Drop the deprecated _run_avco and _get_value_from_lots methods and call
_run_average_batch directly from stock.lot, then rename _run_average_batch
to _run_avco and _run_fifo to _get_fifo_value for clarity.

The _run_* methods replay a valuation globally, based on the cost_method.
So at the end, we will still have a _run_fifo (from _run_fifo_batch).
But we will also have 2 helper functions: _get_fifo_value and
_get_fifo_stack.

addons/stock_account/models/stock_move.py
@@ -344,7 +344,7 @@ class StockMove(models.Model):
 
             if move.product_id.cost_method == 'fifo':
                 valued_qty = move._get_valued_qty()
-                move.value = - move.product_id.with_context(fifo_qty_already_processed=fifo_qty_processed[move.product_id])._run_fifo(valued_qty)
+                move.value = - move.product_id.with_context(fifo_qty_already_processed=fifo_qty_processed[move.product_id])._get_fifo_price_unit(valued_qty)
                 fifo_qty_processed[move.product_id] += valued_qty
             else:
                 move.value = - move.product_id.standard_price * move._get_valued_qty()

odoo/odoo

addons/stock_account/models/stock_move.py
@@ -343,7 +343,7 @@ class StockMove(models.Model):
 
             if move.product_id.cost_method == 'fifo':
                 valued_qty = move._get_valued_qty()
-                move.value = - move.product_id.with_context(fifo_qty_already_processed=fifo_qty_processed[move.product_id])._run_fifo(valued_qty)
+                move.value = - move.product_id.with_context(fifo_qty_already_processed=fifo_qty_processed[move.product_id])._get_fifo_value(valued_qty)
                 fifo_qty_processed[move.product_id] += valued_qty
             else:
                 move.value = - move.product_id.standard_price * move._get_valued_qty()