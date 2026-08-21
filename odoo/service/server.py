96efb0ea1e6bb01d7af373b0413287bdd87da45e
Jeremy Lawson (jelaw) <jelaw@odoo.com>
2026-08-21 19:17:51 +0000

    [FIX] core: Ruff checks on while 1
    Ruff checks on runbot flagged `while 1:`
    
    Preferred syntax is to use `while True`
    
    https://docs.astral.sh/ruff/rules/while-one
    
    runbot-945983
    
    closes odoo/odoo#284655
    
    X-original-commit: bb827376ffe19dde73c388d0fda9469734056657
    Signed-off-by: Jeremy Lawson (jelaw) <jelaw@odoo.com>
    Signed-off-by: Krzysztof Magusiak (krma) <krma@odoo.com>

--- odoo/service/server.py
+++ odoo/service/server.py
@@ -1003,7 +1003,7 @@ class PreforkServer(CommonServer):
 
     def process_zombie(self):
         # reap dead workers
-        while 1:
+        while True:
             try:
                 wpid, status = os.waitpid(-1, os.WNOHANG)
                 if not wpid:
@@ -1121,7 +1121,7 @@ class PreforkServer(CommonServer):
         sql_db.close_all()
 
         _logger.debug("Multiprocess starting")
-        while 1:
+        while True:
             try:
                 #_logger.debug("Multiprocess beat (%s)",time.time())
                 self.process_signals()
