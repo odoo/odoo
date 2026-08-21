fa1049241d7309ac2e54e199ff7eb2f060838663
Soukéina Bojabza <sobo@odoo.com>
2026-08-21 20:48:14 +0200

    [FIX] website: remove remaining id when not a TOC title anymore
    Steps to reproduce:
    - Drop a "Table of Content" snippet.
    - Add a title in the content.
      => the title has an id, to allow the ToC to scroll to it.
    - Transform this title into a simple paragraph.
      => the paragraph still has the id, but is is useless since it is not
      considered by the ToC.
    
    This commit fixes this by correctly removing the ids when not needed
    anymore.
    
    task-3083656
    
    closes odoo/odoo#216148
    
    Signed-off-by: Soukéina Bojabza (sobo) <sobo@odoo.com>
    Co-authored-by: Chrysanthe (chgo) <chgo@odoo.com>

--- addons/website/static/src/builder/plugins/options/table_of_content_option_plugin.js
+++ addons/website/static/src/builder/plugins/options/table_of_content_option_plugin.js
@@ -133,6 +133,14 @@ export class TableOfContentOptionPlugin extends Plugin {
             return;
         }
 
+        // Remove stale TOC ids from non-heading elements (e.g. a heading
+        // converted back to a paragraph)
+        for (const el of tableOfContentMain.querySelectorAll("[id^='table_of_content_heading_']")) {
+            if (!el.matches(targetedElements)) {
+                el.removeAttribute("id");
+            }
+        }
+
         const headingIds = currentHeadingItems.map(({ el }) => getTocAndHeadingId(el).headingId);
         let maxHeadingIds = Math.max(0, ...headingIds);
 
