LAIDOUDI33/LAID_ODOO

[FIX] hr_holidays: Accrual Plan Frequency Daily error
Current behavior:
On a Monday–Friday working schedule, a Daily accrual plan that is based
on worked time grants accrued time on Saturday as well, even though Saturday
is not a working day. The employee accrues on 6 days per week instead of
5 (Sunday is correctly skipped. Only Saturday is wrong).

Expected behavior:
The employee accrues only on the 5 working days (Mon–Fri) → 5 grants per
week. Saturday and Sunday should add nothing.

Setup:
- Working schedule: Standard 40h/week, Monday–Friday, 08:00–17:00.
- All timezones set to Australia/Brisbane (UTC+10) and matching: employee,
working schedule, and user are all the same timezone.
- Accrual plan milestone: accrue 5 Hours, Daily, "At the end of the accrual
period", "Based on worked time = Yes".

Steps to reproduce:
- Create the working schedule and accrual plan above, with the calendar
timezone set to Australia/Brisbane.
- Assign the accrual allocation to an employee, Starting on a Monday.
- On the Time Off dashboard, use "Balance at the (date)" to project the
balance day by day across a weekend (Friday → Saturday → Sunday → Monday).

Cause of the issue:
Accrual period boundaries were built as naive UTC midnights
instead of local calendar midnights.

Fix:
Localize accrual period boundaries in the employee/resource
timezone before calling resource calendar APIs.

opw-6316062

addons/hr_holidays/tests/test_accrual_allocations.py
@@ -394,6 +394,67 @@ class TestAccrualAllocations(TestHrHolidaysCommon):
                 allocation._update_accrual()
                 self.assertEqual(allocation.number_of_days, 1, 'There should be only 1 day allocated.')
 
+    @freeze_time('2025-09-01')
+    def test_frequency_daily_worked_time_non_utc_timezone(self):
+        calendar = self.env['resource.calendar'].create({
+            'name': 'Brisbane 40 Hours',
+            'tz': 'Australia/Brisbane',
+            'hours_per_day': 8,
+            'attendance_ids': [
+                Command.create({
+                    'name': f'Weekday {weekday} Morning',
+                    'dayofweek': str(weekday),
+                    'hour_from': 8,
+                    'hour_to': 12,
+                    'day_period': 'morning',
+                })
+                for weekday in range(5)
+            ] + [
+                Command.create({
+                    'name': f'Weekday {weekday} Afternoon',
+                    'dayofweek': str(weekday),
+                    'hour_from': 13,
+                    'hour_to': 17,
+                    'day_period': 'afternoon',
+                })
+                for weekday in range(5)
+            ],
+        })
+        self.employee_emp.tz = 'Australia/Brisbane'
+        self.employee_emp.resource_calendar_id = calendar
+        self.user_hrmanager.tz = 'Australia/Brisbane'
+        accrual_plan = self.env['hr.leave.accrual.plan'].create({
+            'name': 'Daily Worked Time Accrual',
+            'is_based_on_worked_time': True,
+            'accrued_gain_time': 'end',
+            'can_be_carryover': True,
+            'carryover_date': 'allocation',
+            'level_ids': [Command.create({
+                'milestone_date': 'creation',
+                'added_value': 5,
+                'added_value_type': 'hour',
+                'frequency': 'daily',
+                'action_with_unused_accruals': 'all',
+            })],
+        })
+        allocation = self.env['hr.leave.allocation'].with_user(self.user_hrmanager).create({
+            'name': 'Brisbane Daily Accrual',
+            'accrual_plan_id': accrual_plan.id,
+            'employee_id': self.employee_emp.id,
+            'holiday_status_id': self.leave_type_hour.id,
+            'date_from': date(2025, 9, 1),
+            'number_of_days': 0,
+            'allocation_type': 'accrual',
+        })
+        allocation.action_approve()
+
+        balances = []
+        for day in range(2, 9):
+            allocation._process_accrual_plans(date(2025, 9, day))
+            balances.append(allocation.number_of_hours_display)
+
+        self.assertEqual(balances, [5, 10, 15, 20, 25, 25, 25])
+
     def test_frequency_weekly(self):
         with freeze_time("2017-12-05"):
             accrual_plan = self.env['hr.leave.accrual.plan'].with_context(tracking_disable=True).create({

odoo/odoo

X-original-commit: 0ce22d5fea5fdaf58c446ef130636cbb4dc6e574

addons/hr_holidays/tests/test_accrual_allocations.py
@@ -423,6 +423,61 @@ class TestAccrualAllocations(TestHrHolidaysCommon):
                 allocation._update_accrual()
                 self.assertEqual(allocation.number_of_days, 1, 'There should be only 1 day allocated.')
 
+    @freeze_time('2025-09-01')
+    def test_frequency_daily_worked_time_non_utc_timezone(self):
+        calendar = self.env['resource.calendar'].create({
+            'name': 'Brisbane 40 Hours',
+            'hours_per_day': 8,
+            'attendance_ids': [
+                Command.create({
+                    'dayofweek': str(weekday),
+                    'hour_from': 8,
+                    'hour_to': 12,
+                })
+                for weekday in range(5)
+            ] + [
+                Command.create({
+                    'dayofweek': str(weekday),
+                    'hour_from': 13,
+                    'hour_to': 17,
+                })
+                for weekday in range(5)
+            ],
+        })
+        self.employee_emp.tz = 'Australia/Brisbane'
+        self.employee_emp.resource_calendar_id = calendar
+        self.user_hrmanager.tz = 'Australia/Brisbane'
+        accrual_plan = self.env['hr.leave.accrual.plan'].create({
+            'name': 'Daily Worked Time Accrual',
+            'is_based_on_worked_time': True,
+            'accrued_gain_time': 'end',
+            'can_be_carryover': True,
+            'carryover_date': 'allocation',
+            'level_ids': [Command.create({
+                'milestone_date': 'creation',
+                'added_value': 5,
+                'added_value_type': 'hour',
+                'frequency': 'daily',
+                'action_with_unused_accruals': 'all',
+            })],
+        })
+        allocation = self.env['hr.leave.allocation'].with_user(self.user_hrmanager).create({
+            'name': 'Brisbane Daily Accrual',
+            'accrual_plan_id': accrual_plan.id,
+            'employee_id': self.employee_emp.id,
+            'work_entry_type_id': self.work_entry_type_hour.id,
+            'date_from': date(2025, 9, 1),
+            'number_of_days': 0,
+        })
+        allocation.action_approve()
+
+        balances = []
+        for day in range(2, 9):
+            allocation._process_accrual_plans(date(2025, 9, day))
+            balances.append(allocation.number_of_hours_display)
+
+        self.assertEqual(balances, [5, 10, 15, 20, 25, 25, 25])
+
     def test_frequency_weekly(self):
         with freeze_time("2017-12-05"):
             accrual_plan = self.env['hr.leave.accrual.plan'].create({