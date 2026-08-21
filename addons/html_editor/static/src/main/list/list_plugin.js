3706b769ceadff14a2a1bc68ad5d7f5201b9dbee
Walid (wasa) <wasa@odoo.com>
2026-08-21 10:06:42 +0200

    [FIX] html_editor: allow removing background color from list item
    Problem:
    When clearing format/color on a list item that has `background-color` on `li.style`,
    the background color was not removed from the list item.
    
    Cause:
    `ListPlugin`'s color handler only removed `color` style from `li` nodes when clearing colors,
    ignoring `backgroundColor`.
    
    Solution:
    In `ListPlugin`, check for and remove `background-color` style from `li` elements when clearing formatting/color.
    
    opw-6481665
    
    closes odoo/odoo#283158
    
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/html_editor/static/src/main/list/list_plugin.js
+++ addons/html_editor/static/src/main/list/list_plugin.js
@@ -1128,7 +1128,7 @@ export class ListPlugin extends Plugin {
         const listItems = new Set(
             targetedNodes.map((n) => closestElement(n, "li")).filter(Boolean)
         );
-        if (!listItems.size || mode !== "color" || isColorGradient(color)) {
+        if (!listItems.size || (mode !== "color" && color) || isColorGradient(color)) {
             return;
         }
         const cursors = this.dependencies.selection.preserveSelection();
@@ -1151,6 +1151,9 @@ export class ListPlugin extends Plugin {
 
                     if (node.style.color) {
                         removeStyle(node, "color");
+                    }
+                    if (node.style.backgroundColor) {
+                        removeStyle(node, "background-color");
                     }
                 }
 
