bd755ac9e9e46b8afdd5baf28447bf1cda8d1101
Brett Cutler <brcut@odoo.com>
2026-08-21 19:20:33 +0000

    [FIX] website_sale: test_website filter out inactive records
    Description of the issue/feature this PR addresses:
    
    Addresses an issue causing test failures by ensuring that [inactive products](https://github.com/odoo-dev/odoo/blob/dbc917ddc263a330ff70f5edec716ccafe88d7a6/addons/website_sale/tests/test_product_filters.py#L93-L99) are filtered out rather than leaking from the environment into the test execution.
    
    I have verified that this issue does not allow [inactive records to leak to customers](https://www.odoo.com/mail/message/1151343506).
    
    runbot-242426
    
    X-original-commit: 12a1b070ea287bb574bf8ea866e723ee152d10c8

--- addons/website_sale/tests/test_product_filters.py
+++ addons/website_sale/tests/test_product_filters.py
@@ -120,7 +120,7 @@ class TestWebsiteSaleProductFilters(WebsiteSaleCommon, TestProductAttributeValue
                     "filter_id": filter.id,
                     "template_key": "website_sale.dynamic_filter_template_product_product_products_item",  # noqa: E501
                     "limit": 16,
-                    "search_domain": [],
+                    "search_domain": [["active", "=", True]],
                     **kwargs,
                 }
             },
