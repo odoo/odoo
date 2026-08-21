LAIDOUDI33/LAID_ODOO

[FIX] base,certificate: pass cryptography objects to the SSL context
pyOpenSSL 24.3.0 deprecated passing its own X509/PKey objects to
Context.use_certificate()/use_privatekey(), and started accepting
cryptography objects instead. Odoo pins pyopenssl 24.1.0, but the distro
builds run the version shipped by the OS: since the test added by
f0fb287c6502 covers that path, they now add a warning in the logs.

Make the older versions accept the cryptography objects as well, so that
the callers can always pass those, whichever version is installed.

Reference: https://github.com/pyca/pyopenssl/commit/b0cb4b4

This fix is based on https://github.com/odoo/odoo/blob/a2b4f618328f3ce3f654fd2c1ee4410365706a7e/odoo/addons/base/models/ir_mail_server.py#L34-L46

Fix Runbot #944176

odoo/_monkeypatches/pyopenssl.py
@@ -1,0 +1,36 @@
+from importlib.metadata import version
+
+from cryptography.hazmat.primitives.serialization import Encoding, NoEncryption, PrivateFormat
+from OpenSSL.crypto import FILETYPE_PEM, PKey, X509, load_certificate, load_privatekey
+from OpenSSL.SSL import Context
+
+from odoo.tools import parse_version
+
+orig_use_certificate = Context.use_certificate
+orig_use_privatekey = Context.use_privatekey
+
+
+def patch_pyopenssl():
+    # pyOpenSSL 24.3.0 started accepting cryptography objects in
+    # Context.use_certificate and Context.use_privatekey, and
+    # deprecated passing its own X509 and PKey objects.
+
+    if parse_version(version("pyopenssl")) >= parse_version("24.3.0"):
+        return  # No need to patch, cryptography objects are supported.
+
+    def use_certificate(self, cert):
+        if not isinstance(cert, X509):
+            cert = load_certificate(FILETYPE_PEM, cert.public_bytes(Encoding.PEM))
+        orig_use_certificate(self, cert)
+
+    def use_privatekey(self, pkey):
+        if not isinstance(pkey, PKey):
+            pkey = load_privatekey(FILETYPE_PEM, pkey.private_bytes(
+                encoding=Encoding.PEM,
+                format=PrivateFormat.PKCS8,
+                encryption_algorithm=NoEncryption()
+            ))
+        orig_use_privatekey(self, pkey)
+
+    Context.use_certificate = use_certificate
+    Context.use_privatekey = use_privatekey

odoo/odoo

odoo/_monkeypatches/pyopenssl.py
@@ -1,0 +1,36 @@
+from importlib.metadata import version
+
+from cryptography.hazmat.primitives.serialization import Encoding, NoEncryption, PrivateFormat
+from OpenSSL.crypto import FILETYPE_ASN1, PKey, X509, load_certificate, load_privatekey
+from OpenSSL.SSL import Context
+
+from odoo.tools import parse_version
+
+orig_use_certificate = Context.use_certificate
+orig_use_privatekey = Context.use_privatekey
+
+
+def patch_pyopenssl():
+    # pyOpenSSL 24.3.0 started accepting cryptography objects in
+    # Context.use_certificate and Context.use_privatekey, and
+    # deprecated passing its own X509 and PKey objects.
+
+    if parse_version(version("pyopenssl")) >= parse_version("24.3.0"):
+        return  # No need to patch, cryptography objects are supported.
+
+    def use_certificate(self, cert):
+        if not isinstance(cert, X509):
+            cert = load_certificate(FILETYPE_ASN1, cert.public_bytes(Encoding.DER))
+        orig_use_certificate(self, cert)
+
+    def use_privatekey(self, pkey):
+        if not isinstance(pkey, PKey):
+            pkey = load_privatekey(FILETYPE_ASN1, pkey.private_bytes(
+                encoding=Encoding.DER,
+                format=PrivateFormat.PKCS8,
+                encryption_algorithm=NoEncryption()
+            ))
+        orig_use_privatekey(self, pkey)
+
+    Context.use_certificate = use_certificate
+    Context.use_privatekey = use_privatekey