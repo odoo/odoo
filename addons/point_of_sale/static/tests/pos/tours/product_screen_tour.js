e6ccc59729433102c9293e4e10eb2d2a19dbb573
manv-afk <manv@odoo.com>
2026-08-24 09:02:24 +0000

    [IMP] point_of_sale: stop awaiting receipt print after order validation
    - Stop awaiting the receipt print in the POS after order payment validation
    - Adapt tours to this behavior change
    
    Task-id: 6425204
    X-original-commit: adc4272e75587925c7ebb2208559ab1a45d5990c

--- addons/point_of_sale/static/tests/pos/tours/product_screen_tour.js
+++ addons/point_of_sale/static/tests/pos/tours/product_screen_tour.js
@@ -751,8 +751,6 @@ registry
                 PaymentScreen.clickPaymentMethod("Bank"),
                 PaymentScreen.clickValidate(),
                 FeedbackScreen.isShown(),
-                Dialog.confirm(),
-                FeedbackScreen.clickNextOrder(),
                 ProductScreen.isShown(),
             ].flat(),
     });
