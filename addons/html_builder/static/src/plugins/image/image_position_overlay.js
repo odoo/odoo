LAIDOUDI33/LAID_ODOO

[IMP] web, *: set passive on animation events
*: account, html_builder, html_editor, website, website_event_track,
website_sale, website_slides

The `passive` option on event listeners gives the information to the
browser that `event.preventDefault()` will not be called. It allows
browsers to handle events more efficiently (especially on mobile) and is
interesting for events that are repeated a lot in a very short time
span: scroll, resize, wheel, touchmove, etc.

While since 2025, [Google considers] that it is generally not necessary
for 1st-party scripts, it could still prove interesting for Odoo and all
these kind of events that we have. It is also a minor change for us.

[Google considers]: https://developer.chrome.com/docs/lighthouse/best-practices/uses-passive-event-listeners/

task-6348669

addons/html_builder/static/src/plugins/image/image_position_overlay.js
@@ -48,9 +48,13 @@ export class ImagePositionOverlay extends Component {
         useExternalListener(editableDocument, "pointerdown", this.discard.bind(this));
         useListener(document, "pointerdown", this.discard.bind(this));
 
-        useListener(window, "resize", this._dimensionOverlay.bind(this));
-        useExternalListener(this.iframeEl.contentWindow, "resize", this._dimensionOverlay);
-        useExternalListener(this.iframeEl.contentWindow, "scroll", this._dimensionOverlay);
+        useListener(window, "resize", this._dimensionOverlay, { passive: true });
+        useExternalListener(this.iframeEl.contentWindow, "resize", this._dimensionOverlay, {
+            passive: true,
+        });
+        useExternalListener(this.iframeEl.contentWindow, "scroll", this._dimensionOverlay, {
+            passive: true,
+        });
 
         onWillStart(async () => {
             const position = this.props

addons/html_editor/static/src/core/overlay.js
@@ -92,7 +92,7 @@ export class EditorOverlay extends Component {
             // rangeElement is a sibling of the editable, so position_hook's scroll
             // listener won't detect editable.contains(rangeElement) as true. Listen
             // directly so scrolling within the editable still triggers repositioning.
-            useExternalListener(editable, "scroll", () => position.unlock());
+            useExternalListener(editable, "scroll", () => position.unlock(), { passive: true });
         }
         const positionOptions = {
             position: "bottom-start",

odoo/odoo

addons/html_builder/static/src/plugins/image/image_position_overlay.js
@@ -47,9 +47,13 @@ export class ImagePositionOverlay extends Component {
         useListener(editableDocument, "pointerdown", this.discard.bind(this));
         useListener(document, "pointerdown", this.discard.bind(this));
 
-        useListener(window, "resize", this._dimensionOverlay.bind(this));
-        useListener(this.iframeEl.contentWindow, "resize", this._dimensionOverlay.bind(this));
-        useListener(this.iframeEl.contentWindow, "scroll", this._dimensionOverlay.bind(this));
+        useListener(window, "resize", this._dimensionOverlay, { passive: true });
+        useListener(this.iframeEl.contentWindow, "resize", this._dimensionOverlay, {
+            passive: true,
+        });
+        useListener(this.iframeEl.contentWindow, "scroll", this._dimensionOverlay, {
+            passive: true,
+        });
 
         onWillStart(async () => {
             const position = this.props

addons/html_editor/static/src/core/overlay.js
@@ -92,7 +92,7 @@ export class EditorOverlay extends Component {
             // rangeElement is a sibling of the editable, so position_hook's scroll
             // listener won't detect editable.contains(rangeElement) as true. Listen
             // directly so scrolling within the editable still triggers repositioning.
-            useListener(editable, "scroll", () => position.unlock());
+            useListener(editable, "scroll", () => position.unlock(), { passive: true });
         }
         const positionOptions = {
             position: "bottom-start",