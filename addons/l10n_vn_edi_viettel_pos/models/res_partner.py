4f117691ebba2bdaf11c024835c4c2f94c35d60e
assh-odoo <assh@odoo.com>
2026-08-25 11:02:24 +0530

    [FIX] l10n_vn_edi_viettel_pos: prevent error when opening the POS register
    Currently, an error occurs when user opening the POS register.
    
    Steps to Reproduce:
     - Install l10n_vn_edi_viettel_pos with demo data.
     - Switch to the VN company.
     - Open Point of Sale and create any POS configuration.
     - Go to Point of Sale > Orders > Customers.
     - Remove the "Customer Invoices" filter, search for "Walk-in Customer",
       and delete it.
     - Go back to the Point of Sale dashboard and click "Open Register" on the
       POS configuration.
    
    AttributeError: 'NoneType' object has no attribute 'id'
    
    This error occurs when the user opens the POS config register. It loads
    the default partner in the POS session from [1] and prepares the POS
    data domain [2] if the default customer exists. However, if the default
    customer does not exist, it attempts to access the `id` of `None`,
    which raises the error.
    
    This commit ensure the Walk-in Customer partner cannot be deleted as it
    is required by the Vietnam point of sale.
    
    [1]: https://github.com/odoo/odoo/blob/dd75e71513e5fb19139de8cfd2712dd4e5af81d3/addons/l10n_vn_edi_viettel_pos/models/pos_session.py#L15-L16
    
    [2]: https://github.com/odoo/odoo/blob/dd75e71513e5fb19139de8cfd2712dd4e5af81d3/addons/l10n_vn_edi_viettel_pos/models/res_partner.py#L14-L16

--- addons/l10n_vn_edi_viettel_pos/models/res_partner.py
+++ addons/l10n_vn_edi_viettel_pos/models/res_partner.py
@@ -1,11 +1,25 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import models, api
+from odoo.exceptions import UserError
 from odoo.fields import Domain
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
+
+    @api.ondelete(at_uninstall=False)
+    def _vn_unlink_except_master_data(self):
+        walk_in_customer = self.env.ref(
+            'l10n_vn_edi_viettel_pos.partner_walk_in_customer', raise_if_not_found=False
+        )
+        if walk_in_customer and walk_in_customer in self:
+            raise UserError(
+                self.env._(
+                    "Deleting the partner %s is not allowed because it is required by the Vietnam point of sale.",
+                    walk_in_customer.display_name,
+                )
+            )
 
     @api.model
     def _load_pos_data_domain(self, data, config):
