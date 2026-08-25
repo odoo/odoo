7a1c88e125c0617123c07c952ba639b578ebf74d
Quentin Colla (qucol) <qucol@odoo.com>
2026-08-25 15:40:32 +0200

    [FIX] sale_timesheet: allow period invoicing after manual over-invoicing
    ## Issue
    When a use rmanually increases the invoiced quantity on a timesheet invoice,
    subsequent timesheets logged in future periods can no longer be invoiced.
    The system assumes that the most recent logged hours are already covered
    by the previously over-invoiced amount, blocking the billing of the most
    recent timesheet entries.
    
    ## Steps to reproduce
    1. Install *Sales Timesheet* (`sale_timesheet`)
    2. Create a Product P
        - Product Type: Service
        - Invoicing Policy: Based on Timesheets
        - Create on Order: (Project &) Task
    3. Create an SO:
        - Customer: Any
        - Product: P (any quantity)
        - Confirm
    4. Record hours on the task created:
        - 06/01/2026 (June 1st): 1 hour
        - 07/01/2026 (July 1st): 1 hour
    5. Create the invoice for the June timesheet entry (by setting a timesheets
    period when creating the invoice), then **change the quantity to any value
    strictly greater than 2** and confirm.
    6. Create the invoice for July
    7. **An "Invalid Operation" error appears, stating that there's nothing
    to invoice, even though the timesheet entry from July was never invoiced.**
    
    ## Cause
    Since https://github.com/odoo/odoo/commit/3b86ac3d180993239b63fd305ee0ba1f17f7eb20,
    the computation of the quantity to invoice changed to include the difference
    between the quantity delivered and the quantity already invoiced. In the
    flow described by the steps to reproduce above, the quantity to invoice
    is larger than the quantity delivered, making the `qty_to_invoice` equal
    to `0.0`.
    
    https://github.com/odoo/odoo/blob/626d31fa0191bfe7ca8e1fcf177357eeeb60f2c7/addons/sale_timesheet/models/sale_order.py#L331-L334
    
    The reason for the fix above being the partial refunding of timesheet-related
    invoices, we can keep that solution when working with refunded invoices,
    and keep the previous behavior for other cases.
    
    This solution solves the issue in the steps to reproduce above, but was
    also manually tested on the issues from https://github.com/odoo/odoo/commit/3b86ac3d180993239b63fd305ee0ba1f17f7eb20
    and https://github.com/odoo/odoo/commit/64cf4afabd0c5ef040cf87fc6f3125fe0bb81bbb.
    
    opw-6485674
    
    closes odoo/odoo#284470
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- addons/sale_timesheet/models/sale_order.py
+++ addons/sale_timesheet/models/sale_order.py
@@ -310,7 +310,8 @@ class SaleOrderLine(models.Model):
         """
         lines_by_timesheet = self.filtered(lambda sol: sol.product_id and sol.product_id._is_delivered_timesheet())
         domain = lines_by_timesheet._timesheet_compute_delivered_quantity_domain()
-        refund_account_moves = self.order_id.invoice_ids.filtered(lambda am: am.state == 'posted' and am.move_type == 'out_refund').reversed_entry_id
+        credit_notes = self.order_id.invoice_ids.filtered(lambda am: am.state == 'posted' and am.move_type == 'out_refund' and am.reversed_entry_id)
+        refund_account_moves = credit_notes.reversed_entry_id
         timesheet_domain = [
             '|',
             ('timesheet_invoice_id', '=', False),
@@ -326,12 +327,17 @@ class SaleOrderLine(models.Model):
         mapping = lines_by_timesheet.sudo()._get_delivered_quantity_by_analytic(domain)
 
         for line in lines_by_timesheet:
-            # A period only selects which delivered hours are candidates; qty_delivered
-            # - qty_invoiced remains the authoritative quantity still due.
-            qty_to_invoice = max(0.0, min(
-                mapping.get(line.id, 0.0),
-                line.qty_delivered - line.qty_invoiced,
-            ))
+            if line.invoice_lines.move_id & credit_notes:
+                # `qty_delivered - qty_invoiced` to prevent over-billing
+                # when credit notes complicate the invoiced state
+                qty_to_invoice = max(0.0, min(
+                    mapping.get(line.id, 0.0),
+                    line.qty_delivered - line.qty_invoiced,
+                ))
+            else:
+                # No related credit note: invoice exactly what is
+                # calculated for this period
+                qty_to_invoice = max(0.0, mapping.get(line.id, 0.0))
             if qty_to_invoice:
                 line.qty_to_invoice = qty_to_invoice
             else:
