5ad065b0a289ebf5df984f8f760999f6f6b84ea3
Aurelien van Delft (avd) <avd@odoo.com>
2026-08-25 14:46:01 +0000

    [REV] tools: revert current version of gif resize
    Revert commit d9fae40571c4f10c17fe00efc087cb25b30b85ab as it's slow on
    odoo.com and the call to `frame.copy()` is raising a MemoryError when
    loading a KanbanView with multiple gifs.
    
    closes odoo/odoo#284686
    
    X-original-commit: aae5fdef2f8c0f3a811e8ffb8779b6a89b9372ee
    Signed-off-by: Aurélien van Delft (avd) <avd@odoo.com>

--- odoo/tools/image.py
+++ odoo/tools/image.py
@@ -5,7 +5,7 @@ import binascii
 import io
 from typing import Tuple, Union
 
-from PIL import Image, ImageOps, ImageSequence
+from PIL import Image, ImageOps
 # We can preload Ico too because it is considered safe
 from PIL import IcoImagePlugin
 try:
@@ -77,7 +77,6 @@ class ImageProcess:
         """
         self.source = source or False
         self.operationsCount = 0
-        self.animated_frames = []
 
         if not source or source[:1] == b'<':
             # don't process empty source or SVG
@@ -106,18 +105,6 @@ class ImageProcess:
             w, h = self.image.size
             if verify_resolution and w * h > IMAGE_MAX_RESOLUTION:
                 raise UserError(_lt("Too large image (above %sMpx), reduce the image size.", str(IMAGE_MAX_RESOLUTION / 1e6)))
-
-    def _ensure_gif_frames_extracted(self):
-        """Extract raw frame copies into memory once.
-
-        This detaches frames from the read-only GifImageFile stream so
-        self.image becomes a normal PIL Image whose size updates natively.
-        """
-        if self.original_format == 'GIF' and not self.animated_frames:
-            all_frames = [frame.copy() for frame in ImageSequence.Iterator(self.image)]
-            if all_frames:
-                self.image = all_frames[0]
-                self.animated_frames = all_frames[1:]
 
     def image_quality(self, quality=0, output_format=''):
         """Return the image resulting of all the image processing
@@ -169,7 +156,6 @@ class ImageProcess:
         if output_format == 'GIF':
             opt['optimize'] = True
             opt['save_all'] = True
-            opt['append_images'] = self.animated_frames
         if output_image.mode not in ["1", "L", "P", "RGB", "RGBA"] or (output_format == 'JPEG' and output_image.mode == 'RGBA'):
             output_image = output_image.convert("RGB")
 
@@ -192,9 +178,8 @@ class ImageProcess:
         If `max_width` or `max_height` is falsy, it will be computed from the
         other to keep the current ratio. If both are falsy, no resize is done.
 
-        Upscaling (expanding) is intentionally not supported for animated GIFs
-        as forcing color palette interpolation across sequential frames causes
-        the resulting file size to skyrocket.
+        It is currently not supported for GIF because we do not handle all the
+        frames properly.
 
         :param int max_width: max width
         :param int max_height: max height
@@ -202,16 +187,10 @@ class ImageProcess:
         :return: self to allow chaining
         :rtype: ImageProcess
         """
-        if self.image and (max_width or max_height):
+        if self.image and self.original_format != 'GIF' and (max_width or max_height):
             w, h = self.image.size
             asked_width = max_width or (w * max_height) // h
             asked_height = max_height or (h * max_width) // w
-            if self.original_format == 'GIF':
-                if asked_width < w or asked_height < h:
-                    self._ensure_gif_frames_extracted()
-                    [f.thumbnail((asked_width, asked_height), Resampling.LANCZOS) for f in [self.image] + self.animated_frames]
-                    self.operationsCount += 1
-                return self
             if expand and (asked_width > w or asked_height > h):
                 self.image = self.image.resize((asked_width, asked_height))
                 self.operationsCount += 1
@@ -250,7 +229,7 @@ class ImageProcess:
         :return: self to allow chaining
         :rtype: ImageProcess
         """
-        if self.image and max_width and max_height:
+        if self.image and self.original_format != 'GIF' and max_width and max_height:
             w, h = self.image.size
             # We want to keep as much of the image as possible -> at least one
             # of the 2 crop dimensions always has to be the same value as the
@@ -275,16 +254,9 @@ class ImageProcess:
             h_offset = int((h - new_h) * center_y)
 
             if new_w != w or new_h != h:
-                crop_box = (x_offset, h_offset, x_offset + new_w, h_offset + new_h)
-                if self.original_format == 'GIF':
-                    self._ensure_gif_frames_extracted()
-                    self.image = self.image.crop(crop_box)
-                    self.animated_frames = [frame.crop(crop_box) for frame in self.animated_frames]
+                self.image = self.image.crop((x_offset, h_offset, x_offset + new_w, h_offset + new_h))
+                if self.image.width != w or self.image.height != h:
                     self.operationsCount += 1
-                else:
-                    self.image = self.image.crop(crop_box)
-                    if self.image.width != w or self.image.height != h:
-                        self.operationsCount += 1
 
         return self.resize(max_width, max_height)
 
@@ -297,7 +269,7 @@ class ImageProcess:
         """
         if color is None:
             color = (randrange(32, 224, 24), randrange(32, 224, 24), randrange(32, 224, 24))
-        if self.image and self.original_format != 'GIF':
+        if self.image:
             original = self.image
             self.image = Image.new('RGB', original.size)
             self.image.paste(color, box=(0, 0) + original.size)
@@ -312,8 +284,7 @@ class ImageProcess:
         :return: self to allow chaining
         :rtype: ImageProcess
         """
-        self._ensure_gif_frames_extracted()
-        if self.image and not self.animated_frames:
+        if self.image:
             img_width, img_height = self.image.size
             self.image = self.image.resize((img_width - 2 * padding, img_height - 2 * padding))
             self.image = ImageOps.expand(self.image, border=padding)
