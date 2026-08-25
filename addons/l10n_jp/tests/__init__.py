c68113c9fc379e1ca23dd9faf8051f1c05a7446c
erle <erle@odoo.com>
2026-08-25 09:13:37 +0800

    [IMP] l10n_jp: JIS document layout
    Japanese documents address recipients with an honorific (様 for
    individuals, 御中 for companies) and label tax groups 対象額,
    conventions the generic external layouts don't follow.
    
    task-3272592

---
+++ addons/l10n_jp/tests/__init__.py
@@ -0,0 +1,7 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import test_jp_account_report
+from . import test_jp_address_format
+from . import test_jp_purchase_report
+from . import test_jp_sale_report
+from . import test_jp_stock_report
