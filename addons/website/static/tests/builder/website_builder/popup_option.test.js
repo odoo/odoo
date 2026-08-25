89139c69de71e53aeae359b99a2346337cfb67f6
Sébastien (blse) <blse@odoo.com>
2026-08-25 07:32:28 +0000

    [FIX] website: fix non-deterministic test that hides a popup
    The test `undoing something on a target outside s_popup closes it` had
    a few fails in CI: the `fa-eye-slash` was not set as expected. This
    commit adds a `waitSidebarUpdated` call just before to ensure owl has no
    pending rendering when checking the eye.
    
    The fix is similar to aaf0f54d1feda60becb0bfbad578b366715c0172 which is
    about a similar failure in another test.
    
    runbot-938967
    
    closes odoo/odoo#284615
    
    X-original-commit: bc8c1294734c3fc428e1f0020426a41e4763d7ec
    Signed-off-by: Francois Georis (fge) <fge@odoo.com>
    Signed-off-by: Sébastien Blondiau (blse) <blse@odoo.com>

--- addons/website/static/tests/builder/website_builder/popup_option.test.js
+++ addons/website/static/tests/builder/website_builder/popup_option.test.js
@@ -226,6 +226,7 @@ describe("Popup options: popup in page before edit", () => {
         await expectToTriggerEvent(":iframe .s_popup .modal", "hidden.bs.modal", () =>
             undo(builder.getEditor())
         );
+        await builder.waitSidebarUpdated();
         expect(".o_we_invisible_entry .oi").toHaveAttribute("data-icon", "visibility_off");
     });
 
