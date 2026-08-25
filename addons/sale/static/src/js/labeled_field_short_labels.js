7380e195e035b967bba76ea7370f8ef285f911aa
Cezar Bulancea (cebul) <cebul@odoo.com>
2026-08-25 08:38:42 +0200

    [FIX] sale,sale_margin: improve stacked fields in so
    The addition of stacked fields made that the
    width of some columns in the so to be too big.
    That's why now, instead of the full label, we
    use abbreviations as labels in stacked fields.
    We managed to bring the columns to their
    initial size by deleting the width options.
    
    The section lines contain now just the
    description and amount.
    
    closes odoo/odoo#284175
    
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

---
+++ addons/sale/static/src/js/labeled_field_short_labels.js
@@ -0,0 +1,34 @@
+import { _t } from "@web/core/l10n/translation";
+
+/**
+ * Abbreviated labels displayed in front of the fields that are stacked inside a
+ * single list view cell, by field name.
+ *
+ * The mapping can only be built once the translations are loaded, hence the
+ * function: at module load time, `_t` returns a lazy string that cannot be
+ * evaluated yet.
+ *
+ * @return {Object} The abbreviated labels, by field name.
+ */
+function getShortLabels() {
+    return {
+        margin: _t("M"),
+        margin_percent: _t("M%"),
+        price_total: _t("TI"),
+        price_subtotal: _t("TE"),
+        qty_delivered_percent: _t("D%"),
+    };
+}
+
+/**
+ * Get the abbreviated label of the provided field.
+ *
+ * The full label is kept everywhere else (e.g. in the optional columns
+ * dropdown), only the stacked field itself is abbreviated.
+ *
+ * @param {String} name The field name.
+ * @return {String|undefined} The abbreviated label, if the field has one.
+ */
+export function getShortLabel(name) {
+    return getShortLabels()[name];
+}
