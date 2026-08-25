5675ee23868e58a1cb0a252cb855646fea99bd84
Nisarg (nipl) <nipl@odoo.com>
2026-08-25 14:57:50 +0000

    [FIX] sale: prevent traceback when opening configurator in debug mode
    This commit prevents a traceback when opening the product configurator in debug
    mode. Prop validation only happens in debug mode, which exposed an issue with
    products that allow entering custom attribute values (e.g. Acoustic Bloc
    Screen).
    
    When a custom value is left empty, it is read as `false` when custom attributes
    are retrieved from the frontend. As a result, the `custom_value` key in the
    `customPtavs` prop passed to the product configurator contains a boolean,
    whereas the prop expects a string.
    
    This commit ensures that an empty string is passed instead of `false` when
    opening the configurator for a line with an empty custom attribute value.
    
    X-original-commit: f27f93603b7b72a85f14fdd003a3203b05c84c58

--- addons/sale/static/src/js/sale_product_mixin.js
+++ addons/sale/static/src/js/sale_product_mixin.js
@@ -371,7 +371,7 @@ export const saleProductMixin = () => ({
         return customPtavs.map(customPtav => ({
             id: customPtav.custom_product_template_attribute_value_id &&
                 customPtav.custom_product_template_attribute_value_id.id,
-            value: customPtav.custom_value,
+            value: customPtav.custom_value || "",
         }));
     },
 });
