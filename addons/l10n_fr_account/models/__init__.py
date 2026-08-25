793d11e30845b76ac579671dfc766ed94a17ff98
Maximilien <malb@odoo.com>
2026-08-25 11:16:25 +0200

    [ADD] l10n_{gf,gp,mq,re,yt}: new localization
    Like Monaco, we need to add new localization package for
    the drom countries
    
    task-6321652

--- addons/l10n_fr_account/models/__init__.py
+++ addons/l10n_fr_account/models/__init__.py
@@ -1,9 +1,14 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from . import account_move
+from . import base_document_layout
+from . import res_company
+from . import res_partner
 from . import template_fr
 from . import template_fr_asso
 from . import template_fr_comp
+from . import template_gf
+from . import template_gp
 from . import template_mc
-from . import res_partner
-from . import res_company
-from . import base_document_layout
+from . import template_mq
+from . import template_re
+from . import template_yt
---
+++ addons/l10n_fr_account/models/template_gf.py
@@ -0,0 +1,18 @@
+from odoo import models
+from odoo.addons.account.models.chart_template import template
+
+
+class AccountChartTemplate(models.AbstractModel):
+    _inherit = 'account.chart.template'
+
+    @template('gf')
+    def _get_gf_template_data(self):
+        return {
+            'code_digits': '6',
+            'parent': 'fr_comp',
+        }
+
+    def _deref_account_tags(self, template_code, tax_data):
+        if template_code == 'gf':
+            template_code = 'fr_comp'
+        return super()._deref_account_tags(template_code, tax_data)
