LAIDOUDI33/LAID_ODOO

[FIX] html_editor: remove format from li containing heading
Current behavior before commit:

- Create a list with h1, have some text inside.
- Select whole heading and apply underline style.
- Click on remove format button.

You will notice that underline style is not removed from list.

Desired behavior after commit:

Now, format is removed correctly from the list.

task-6348753

addons/html_editor/static/src/main/list/list_plugin.js
@@ -214,6 +214,12 @@ export class ListPlugin extends Plugin {
                 }
             }
         },
+        is_node_in_boundary_predicates: (n, rootNode) => {
+            const listAncestor = closestElement(n, "ul, ol");
+            if (listAncestor && rootNode.contains(listAncestor)) {
+                return false;
+            }
+        },
 
         /** Providers */
         color_target_providers: (node) => {
@@ -1209,8 +1215,8 @@ export class ListPlugin extends Plugin {
         cursors.restore();
     }
 
-    postFormatAppliedOnList(node, formatName, applyStyle) {
-        if (formatName !== "fontSize") {
+    postFormatAppliedOnList(node, formatSpec, applyStyle) {
+        if (formatSpec.id !== "fontSize") {
             return;
         }
         const listsSet = new Set();
@@ -1218,7 +1224,9 @@ export class ListPlugin extends Plugin {
             const sublists = childNodes(node).filter(isListElement);
             for (const list of sublists) {
                 if (applyStyle) {
-                    list.classList.add("o_default_font_size");
+                    formatSpec.addNeutralStyle(list);
+                } else {
+                    formatSpec.removeStyle(list);
                 }
             }
             listsSet.add(node.parentElement);

odoo/odoo

addons/html_editor/static/src/main/list/list_plugin.js
@@ -214,6 +214,12 @@ export class ListPlugin extends Plugin {
                 }
             }
         },
+        is_node_in_same_block_segment_predicates: (node, blockNode) => {
+            const listAncestor = closestElement(node, "ul, ol");
+            if (listAncestor && blockNode.contains(listAncestor)) {
+                return false;
+            }
+        },
 
         /** Providers */
         color_target_providers: (node) => {
@@ -1224,8 +1230,8 @@ export class ListPlugin extends Plugin {
         cursors.restore();
     }
 
-    postFormatAppliedOnList(node, formatName, applyStyle) {
-        if (formatName !== "fontSize") {
+    postFormatAppliedOnList(node, formatSpec, applyStyle) {
+        if (formatSpec.id !== "fontSize") {
             return;
         }
         const listsSet = new Set();
@@ -1233,7 +1239,9 @@ export class ListPlugin extends Plugin {
             const sublists = childNodes(node).filter(isListElement);
             for (const list of sublists) {
                 if (applyStyle) {
-                    list.classList.add("o_default_font_size");
+                    formatSpec.addNeutralStyle(list);
+                } else {
+                    formatSpec.removeStyle(list);
                 }
             }
             listsSet.add(node.parentElement);