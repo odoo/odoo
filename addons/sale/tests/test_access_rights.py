LAIDOUDI33/LAID_ODOO

[FIX] account, sale: prevent access error when accessing invoice with cash_rounding
Behavior:
When a sales user without accounting access rights tries to view an invoice created from a sale order where
a cash rounding is applied, an AccessError is raised.

This occurs because loading the invoice view triggers `_compute_tax_totals()`,
which then passes the invoice's `cash_rounding_id` to `_get_tax_totals_summary()`. Which ends up failing when trying to access
fields on the `cash_rounding` record  due to missing accounting rights, even though the user is allowed to view
the parent invoice.

This is fixed by ensuring reading fields on `cash_rounding` during tax total computation bypasses the access check using `sudo()`,
as the user already has legitimate access to the invoice itself.

Steps to reproduce:
- As an admin, enable cash rounding then create one.
- Create an invoice and set the Cash Rounding Method
- In debug mode, go to 'Set Default Values' in the debug dropdown and set Cash Rounding Method = your rounding
for all users
- Go to users, and ensures that Demo has no accounting rights but has sales user rights
- As Demo, create a sale order, confirm it, then create the related invoice.
- When trying to acces said invoice, you should get an Access Error

opw-6379781

addons/sale/tests/test_access_rights.py
@@ -2,7 +2,7 @@
 
 from odoo import Command
 from odoo.exceptions import AccessError, UserError
-from odoo.tests import tagged
+from odoo.tests import tagged, Form
 from odoo.tools import mute_logger
 
 from odoo.addons.mail.tests.common import MailCommon
@@ -155,3 +155,32 @@ class TestAccessRights(SaleCommon, MailCommon):
         # Employee can't delete the SO
         with self.assertRaises(AccessError):
             so_as_internal_user.unlink()
+
+    def test_access_invoice_from_sale_order(self):
+        """ Test access rights on invoices created from sale orders when the current user has no
+        accounting access rights but is the default salesperson related to the created invoice.
+
+        Cash rounding can trigger access checks on accounting records; this test ensures
+        the salesperson retains read/write access via the default user_id relation.
+        """
+        # Enable cash rounding in the settings, create one and set it as default for invoices
+        self.env.user.group_ids += self.env.ref('account.group_cash_rounding')
+        rounding = self.env['account.cash.rounding'].create({
+            'name': 'rounding',
+        })
+        self.env['ir.default'].set('account.move', 'invoice_cash_rounding_id', rounding.id)
+        # Create a sale order as a salesperson that has no accounting access rights
+        sale_order = self.env['sale.order'].with_user(self.sale_user).create({
+                'partner_id': self.partner.id,
+                'order_line': [
+                Command.create({
+                    'product_id': self.product.id,
+                }),
+            ],
+        })
+        # Salesperson confirms the SO and creates the related invoice
+        sale_order.action_confirm()
+        invoice = sale_order._create_invoices().with_user(self.sale_user)
+        # Ensure the salesperson can access the invoice even when a cash rounding is set
+        with Form(invoice) as form:
+            self.assertEqual(form.user_id, self.sale_user)

odoo/odoo

X-original-commit: d35e1cacb79d0ce8c9637e4a7afe6e314f5b7896

addons/sale/tests/test_access_rights.py
@@ -2,7 +2,7 @@
 
 from odoo import Command
 from odoo.exceptions import AccessError, UserError
-from odoo.tests import tagged
+from odoo.tests import Form, tagged
 from odoo.tools import mute_logger
 
 from odoo.addons.mail.tests.common import MailCommon
@@ -169,3 +169,34 @@ class TestAccessRights(SaleCommon, MailCommon):
         )
         sale_order.action_confirm()
         self.assertTrue(sale_order.state == "sale")
+
+    def test_access_invoice_from_sale_order(self):
+        """ Test access rights on invoices created from sale orders when the current user has no
+        accounting access rights but is the default salesperson related to the created invoice.
+
+        Cash rounding can trigger access checks on accounting records; this test ensures
+        the salesperson retains read/write access via the default user_id relation.
+        """
+        # Enable cash rounding in the settings, create one and set it as default for invoices
+        self.env.user.group_ids += self.env.ref('account.group_cash_rounding')
+        rounding = self.env['account.cash.rounding'].create({
+            'name': 'rounding',
+        })
+        self.env['ir.default'].set('account.move', 'invoice_cash_rounding_id', rounding.id)
+        # Create a sale order as a salesperson that has no accounting access rights
+        sale_order = self.env['sale.order'].with_user(self.sale_user).create({
+            'partner_id': self.partner.id,
+            'order_line': [
+                Command.create({
+                    'product_id': self.product.id,
+                }),
+            ],
+        })
+        # Salesperson confirms the SO and creates the related invoice
+        sale_order.action_confirm()
+        invoice = sale_order._create_invoices().with_user(self.sale_user)
+        # Ensure cache is clear so that the user needs to access fields
+        rounding.invalidate_recordset(["strategy"])
+        # Ensure the salesperson can access the invoice even when a cash rounding is set
+        with Form(invoice) as form:
+            self.assertEqual(form.user_id, self.sale_user)