e9f7c5f2a384ff537eb884616e6cd38955bd1669
Julien Piron <jupir@odoo.com>
2026-08-27 11:09:47 +0200

    [FIX] website: streamline new tab link interaction
    Screen readers generally do not automatically announce that a link
    opens in a new tab when a user navigates to it, creating a potential
    barrier for non-visual users who may become disoriented if the current
    page is replaced without warning.
    
    Commit 2135b3c1a89c3ba0887191f36b5d73d08682f4b6 already fixed it, but
    this new approach follows more closely the Interaction framework.

--- addons/website/static/src/interactions/a11y_new_tab_link.js
+++ addons/website/static/src/interactions/a11y_new_tab_link.js
@@ -4,37 +4,29 @@ import { _t } from "@web/core/l10n/translation";
 
 export class A11yNewTabLink extends Interaction {
     static selector = "a[target='_blank']";
+    dynamicContent = {
+        _root: {
+            "t-att-aria-label": () => {
+                if (this.el.hasAttribute("aria-label")) {
+                    return `${this.originalAriaLabel} ${this.openInNewTab}`;
+                }
+            },
+        },
+    };
+
+    setup() {
+        this.originalAriaLabel = this.el.getAttribute("aria-label");
+        this.openInNewTab = _t("(Open in new tab)");
+    }
 
     start() {
-        if (this.el.hasAttribute("aria-label")) {
-            this.addIndicatorInAriaLabel();
-        } else {
-            this.addVisuallyHiddenIndicator();
+        if (!this.el.hasAttribute("aria-label")) {
+            const newTabIndicatorEl = document.createElement("span");
+            newTabIndicatorEl.textContent = ` ${this.openInNewTab}`;
+            newTabIndicatorEl.classList.add("visually-hidden");
+
+            this.insert(newTabIndicatorEl);
         }
-    }
-
-    destroy() {
-        const ariaLabel = this.el.getAttribute("aria-label");
-        const indicator = ` ${_t("(Open in new tab)")}`;
-        if (ariaLabel && ariaLabel.endsWith(indicator)) {
-            this.el.setAttribute(
-                "aria-label",
-                ariaLabel.slice(0, ariaLabel.length - indicator.length)
-            );
-        }
-    }
-
-    addVisuallyHiddenIndicator() {
-        const newTabIndicatorEl = document.createElement("span");
-        newTabIndicatorEl.textContent = ` ${_t("(Open in new tab)")}`;
-        newTabIndicatorEl.classList.add("visually-hidden");
-
-        this.insert(newTabIndicatorEl);
-    }
-
-    addIndicatorInAriaLabel() {
-        const ariaLabelContent = this.el.getAttribute("aria-label");
-        this.el.setAttribute("aria-label", `${ariaLabelContent} ${_t("(Open in new tab)")}`);
     }
 }
 
