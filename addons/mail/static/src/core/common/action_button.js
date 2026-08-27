a314a115b9a0d015344904ce8346edaa0ba3d320
Didier (did) <did@odoo.com>
2026-08-27 13:25:59 +0200

    [REF] mail: remove useless props and class
    Since the components are splited some props and class are not useful

--- addons/mail/static/src/core/common/action_button.js
+++ addons/mail/static/src/core/common/action_button.js
@@ -10,9 +10,7 @@ export class ActionButton extends Component {
         "style?",
         "isInlineCircleButton",
         "inMeetingViewCallButtonsFullscreen",
-        "dropdown?",
         "fw?",
-        "inline?",
         "isFirstInGroup?",
         "isLastInGroup?",
         "hasBtnBg?",
@@ -23,7 +21,6 @@ export class ActionButton extends Component {
     setup() {
         super.setup();
         this.store = useService("mail.store");
-        this.ui = useService("ui");
     }
 
     get action() {
@@ -47,7 +44,6 @@ export class ActionButton extends Component {
             active: action.isActive,
             "o-odooControlPanelSwitchStyle": this.props.odooControlPanelSwitchStyle,
             "o-hasBtnBg": this.hasBtnBg,
-            "o-inline": this.props.inline,
             "bg-secondary":
                 action.isActive &&
                 !action.tags.includes("PRIMARY") &&
@@ -60,38 +56,26 @@ export class ActionButton extends Component {
             "btn-primary": action.tags.includes("PRIMARY"),
             "btn-danger": action.tags.includes("DANGER"),
             "btn-success": action.tags.includes("SUCCESS"),
-            "d-flex align-items-center": this.props.inline && isInlineCircleButton,
-            "text-start": this.props.dropdown && !this.ui.isSmall,
-            "border-0": this.props.inline && !this.hasBtnBg && action.icon,
-            "border-2": this.props.inline && !this.hasBtnBg && !action.icon,
-            "rounded-circle": this.props.inline && isInlineCircleButton,
-            "rounded-start-3":
-                this.props.inline && !isInlineCircleButton && this.props.isFirstInGroup,
-            "rounded-end-3": this.props.inline && !isInlineCircleButton && this.props.isLastInGroup,
-            "o-mx-0_5": this.props.inline && !this.hasBtnBg && !action.icon,
+            "d-flex align-items-center": isInlineCircleButton,
+            "border-0": !this.hasBtnBg && action.icon,
+            "border-2": !this.hasBtnBg && !action.icon,
+            "rounded-circle": isInlineCircleButton,
+            "rounded-start-3": !isInlineCircleButton && this.props.isFirstInGroup,
+            "rounded-end-3": !isInlineCircleButton && this.props.isLastInGroup,
+            "o-mx-0_5": !this.hasBtnBg && !action.icon,
             "px-1 py-2": this.props.inMeetingViewCallButtonsFullscreen,
             "o-px-1_5 py-1":
-                this.props.inline &&
-                ((this.hasBtnBg && !isInlineCircleButton && !this.env.inMeetingView) ||
-                    (!this.hasBtnBg && this.env.inComposer)),
+                (this.hasBtnBg && !isInlineCircleButton && !this.env.inMeetingView) ||
+                (!this.hasBtnBg && this.env.inComposer),
             "o-p-1_5":
-                this.props.inline &&
                 !this.env.inMeetingView &&
                 this.hasBtnBg &&
                 isInlineCircleButton &&
                 !this.env.inChatWindow,
-            "o-px-0_5":
-                this.props.inline && !this.env.inMeetingView && !this.hasBtnBg && !action.icon,
-            "p-1":
-                this.props.inline && this.hasBtnBg && isInlineCircleButton && this.env.inChatWindow,
-            "o-p-0_5":
-                this.props.inline &&
-                !this.env.inMeetingView &&
-                !this.hasBtnBg &&
-                !this.env.inComposer,
-            "o-px-0_5 py-0": this.props.inline && this.env.inMessage,
-            "px-3 py-2": this.props.dropdown && this.ui.isSmall,
-            "px-2 py-1": this.props.dropdown && !this.ui.isSmall,
+            "o-px-0_5": !this.env.inMeetingView && !this.hasBtnBg && !action.icon,
+            "p-1": this.hasBtnBg && isInlineCircleButton && this.env.inChatWindow,
+            "o-p-0_5": !this.env.inMeetingView && !this.hasBtnBg && !this.env.inComposer,
+            "o-px-0_5 py-0": this.env.inMessage,
             "o-text-white o-simulateDarkTheme": this.store.shouldSimulateDarkTheme(this),
             "bg-transparent": this.store.shouldSimulateDarkTheme(this) && !this.hasBtnBg,
             "o-inDiscussCall":
--- addons/mail/static/src/core/common/action_dropdown.js
+++ addons/mail/static/src/core/common/action_dropdown.js
@@ -11,9 +11,7 @@ export class ActionDropdown extends Component {
         "attrs",
         "isInlineCircleButton",
         "inMeetingViewCallButtonsFullscreen",
-        "dropdown?",
         "fw?",
-        "inline?",
         "isFirstInGroup?",
         "isLastInGroup?",
         "hasBtnBg?",
@@ -41,14 +39,12 @@ export class ActionDropdown extends Component {
 
     get btnClass() {
         const action = this.action;
-        const isInlineCircleButton = this.props.isInlineCircleButton;
         return attClassObjectToString({
             "o-first": this.props.isFirstInGroup,
             "o-last": this.props.isLastInGroup,
             active: action.isActive,
             "o-odooControlPanelSwitchStyle": this.props.odooControlPanelSwitchStyle,
             "o-hasBtnBg": this.hasBtnBg,
-            "o-inline": this.props.inline,
             "bg-secondary":
                 action.isActive &&
                 !action.tags.includes("PRIMARY") &&
@@ -61,38 +57,10 @@ export class ActionDropdown extends Component {
             "btn-primary": action.tags.includes("PRIMARY"),
             "btn-danger": action.tags.includes("DANGER"),
             "btn-success": action.tags.includes("SUCCESS"),
-            "d-flex align-items-center": this.props.inline && isInlineCircleButton,
-            "text-start": this.props.dropdown && !this.ui.isSmall,
-            "border-0": this.props.inline && !this.hasBtnBg && action.icon,
-            "border-2": this.props.inline && !this.hasBtnBg && !action.icon,
-            "rounded-circle": this.props.inline && isInlineCircleButton,
-            "rounded-start-3":
-                this.props.inline && !isInlineCircleButton && this.props.isFirstInGroup,
-            "rounded-end-3": this.props.inline && !isInlineCircleButton && this.props.isLastInGroup,
-            "o-mx-0_5": this.props.inline && !this.hasBtnBg && !action.icon,
+            "text-start": !this.ui.isSmall,
             "px-1 py-2": this.props.inMeetingViewCallButtonsFullscreen,
-            "o-px-1_5 py-1":
-                this.props.inline &&
-                ((this.hasBtnBg && !isInlineCircleButton && !this.env.inMeetingView) ||
-                    (!this.hasBtnBg && this.env.inComposer)),
-            "o-p-1_5":
-                this.props.inline &&
-                !this.env.inMeetingView &&
-                this.hasBtnBg &&
-                isInlineCircleButton &&
-                !this.env.inChatWindow,
-            "o-px-0_5":
-                this.props.inline && !this.env.inMeetingView && !this.hasBtnBg && !action.icon,
-            "p-1":
-                this.props.inline && this.hasBtnBg && isInlineCircleButton && this.env.inChatWindow,
-            "o-p-0_5":
-                this.props.inline &&
-                !this.env.inMeetingView &&
-                !this.hasBtnBg &&
-                !this.env.inComposer,
-            "o-px-0_5 py-0": this.props.inline && this.env.inMessage,
-            "px-3 py-2": this.props.dropdown && this.ui.isSmall,
-            "px-2 py-1": this.props.dropdown && !this.ui.isSmall,
+            "px-3 py-2": this.ui.isSmall,
+            "px-2 py-1": !this.ui.isSmall,
             "o-text-white o-simulateDarkTheme": this.store.shouldSimulateDarkTheme(this),
             "bg-transparent": this.store.shouldSimulateDarkTheme(this) && !this.hasBtnBg,
             "o-inDiscussCall":
