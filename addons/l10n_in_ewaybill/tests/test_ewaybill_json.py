2a8be40cd88e1916cd2f24d6b27963681cb05ecd
Arbaaz-Khan-Tech <asurk@odoo.com>
2026-08-27 13:18:17 +0000

    [FIX] l10n_in_ewaybill_stock,l10n_in_ewaybill: grant stock_user ewaybill access
    Before this commit
    Inventory users without Accounting permissions could get an Access Error when viewing or creating an E-Waybill because they could not access the required document types.
    
    After this commit
    Inventory users can now view and create E-Waybills without an Access Error.
    
    task-6515093
    
    X-original-commit: 0b50eae0c3682b2a2bfc5d467a2be3abb8cf025b

--- addons/l10n_in_ewaybill/tests/test_ewaybill_json.py
+++ addons/l10n_in_ewaybill/tests/test_ewaybill_json.py
@@ -457,3 +457,12 @@ class TestEwaybillJson(L10nInTestInvoicingCommon):
         })
         expected_msg = _('- Transporter %s does not have a valid GST Number', self.partner_b.name)
         self.assertEqual(ewaybill_invoice_2._check_transporter(), [expected_msg])
+
+    def test_ewaybill_access_accounting_user(self):
+        self.env.user.group_ids = self.env.ref('account.group_account_invoice')
+        ewaybill = self.env['l10n.in.ewaybill'].create({
+            'account_move_id': self.invoice.id,
+            'mode': False,
+            'type_id': self.env.ref('l10n_in_ewaybill.type_tax_invoice_sub_type_supply').id,
+        })
+        self.assertTrue(ewaybill)
