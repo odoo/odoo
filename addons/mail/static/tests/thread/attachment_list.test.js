74a1af1ddd808323722e9ce51becc9721ce00532
defl <defl@odoo.com>
2026-08-27 12:39:54 +0000

    [FIX] mail: prevent duplicate attachment deletion requests
    **Steps to reproduce:**
    - Open Discuss app
    - Open any conversation
    - Setup the browser with throttled connection (e.g. Slow 4G)
    - Select multiple files to attach but don't send the message
    - While some are loading, double click the delete button of
    one of the loaded attachment preview
    - Traceback will appear: `404 Not Found`
    
    **Issue:**
    On click the delete button triggers a deletion request on `/mail/attachment/delete`.
    
    Repeatedly clicking the delete button before the first request was completed
    (and the `AttachmentList` get updated) but after the attachment was deleted
    will cause a `404 Not Found` due to the `raise NotFound()` which was added
    by [1] in `mail_attachment_delete`.
    
    ```py
    attachment = request.env["ir.attachment"].browse(int(attachment_id)).exists()
    if not attachment or not attachment._has_attachments_ownership([access_token]):
        request.env.user._bus_send("ir.attachment/delete", {"id": attachment_id})
        raise NotFound()
    ```
    
    **Fix:**
    Prevent concurrent deletion requests for the same attachment by keeping
    track of attachments currently being deleted.
    
    [1] https://github.com/odoo/odoo/commit/50f45dd436b97027cb1461410efeee7057dad4e8#
    
    opw-6499125
    
    X-original-commit: 28adc3cade898483ea790961aab9409ba5b8a271

--- addons/mail/static/tests/thread/attachment_list.test.js
+++ addons/mail/static/tests/thread/attachment_list.test.js
@@ -2,6 +2,7 @@ import {
     click,
     contains,
     defineMailModels,
+    inputFiles,
     onRpcBefore,
     openDiscuss,
     openFormView,
@@ -137,6 +138,35 @@ test("clicking on the delete attachment button multiple times should do the rpc 
     await click(".modal-footer .btn-primary");
     await click(".modal-footer .btn-primary");
     await click(".modal-footer .btn-primary");
+    await contains(".o-mail-Attachment-unlink", { count: 0 });
+    await expect.waitForSteps(["attachment_unlink"]); // The unlink method must be called once
+});
+
+test("clicking on the delete attachment button multiple times in composer should do the rpc only once", async () => {
+    const pyEnv = await startServer();
+    const channelId = pyEnv["discuss.channel"].create({
+        channel_type: "channel",
+        name: "channel1",
+    });
+    const text = new File(["hello, world"], "text.txt", { type: "text/plain" });
+    let resolveDelete;
+    const deletePromise = new Promise((resolve) => {
+        resolveDelete = resolve;
+    });
+    onRpcBefore("/mail/attachment/delete", async () => {
+        expect.step("attachment_unlink");
+        await deletePromise;
+    });
+    await start();
+    await openDiscuss(channelId);
+    await inputFiles(".o-mail-Composer .o_input_file", [text]);
+    await contains(
+        ".o-mail-Composer-footer .o-mail-AttachmentList .o-mail-AttachmentContainer:not(.o-isUploading):contains(text.txt)"
+    );
+    await click(".o-mail-Attachment-unlink");
+    await click(".o-mail-Attachment-unlink");
+    resolveDelete();
+    // Let the pending deletion settle, so any extra rpc has been registered.
     await contains(".o-mail-Attachment-unlink", { count: 0 });
     await expect.waitForSteps(["attachment_unlink"]); // The unlink method must be called once
 });
