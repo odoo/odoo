d046187418d1f7057059dcd8975b177f0efdc084
Aurélien Bertrand <auber@odoo.com>
2026-08-27 17:51:27 +0200

    [IMP] *project*: clean search view
    In this commit, we refactor and clean the search view of Project.
    
    task-6516390

--- addons/project_hr_skills/__manifest__.py
+++ addons/project_hr_skills/__manifest__.py
@@ -11,6 +11,7 @@
     'auto_install': True,
     'data': [
         'views/project_task_views.xml',
+        'report/project_report_views.xml',
     ],
     'author': 'Odoo S.A.',
     'license': 'LGPL-3',
