319ab7e6795252c08db9a2bfa5c16c7739fe6ce7
Robin Lejeune (role) <role@odoo.com>
2026-08-28 09:00:37 +0000

    [FIX] website_event_track: fix agenda scroll stuttering
    On Firefox and Safari, horizontal scrolling on an agenda with many items
    stuttered.
    This was likely caused by triggering too many scroll events, without
    throttling them.
    
    Steps to reproduce:
    - On Firefox or Safari, run with demo data
    - Go to the "Design Fair Los Angeles" agenda (click on the event > Talks
    > Agenda. Alternatively, enter the URL directly: `/event/ID/agenda`,
    with the right event ID)
    - Resize the page (or open the dev tools) so that there is a horizontal
    scrollbar.
    - Scroll horizontally with a trackpad or with shift + mouse wheel.
    => The agenda stutters/shakes: as you go to the right, it sometimes
    slightly go back left, and vice-versa.
    You might have to scroll from left to right and right to left a few
    times to see the issue.
    
    X-original-commit: 69f9ca25ce2be7a591454b10e7a27eefc84a943e

--- addons/website_event_track/static/src/interactions/website_event_track.js
+++ addons/website_event_track/static/src/interactions/website_event_track.js
@@ -6,18 +6,18 @@ export class WebsiteEventTrack extends Interaction {
 
     dynamicContent = {
         _window: {
-            "t-on-scroll": () => this.updateAgendaScroll(),
-            "t-on-resize": () => this.updateAgendaScroll(),
+            "t-on-scroll": () => this.throttledUpdateAgendaScroll(),
+            "t-on-resize": () => this.throttledUpdateAgendaScroll(),
         },
         "#event_track_search": { "t-on-input.prevent.withTarget": (ev, currentTargetEl) => {
             this.searchText = currentTargetEl.value.toLowerCase();
         }},
-        ".o_we_online_agenda": { "t-on-scroll.withTarget": this.onAgendaScroll },
+        ".o_we_online_agenda": { "t-on-scroll.withTarget": this.throttled(this.onAgendaScroll) },
         ".event_track": { "t-att-class": (el) => ({ "invisible": !el.textContent.toLowerCase().includes(this.searchText) }) },
         "#search_summary": { "t-att-class": () => ({ "invisible": !this.searchText }) },
         "#search_number": { "t-out": () => this.tracks.filter(element => !element.classList.contains('invisible')).length },
         ".o_we_agenda_horizontal_scroller_container": {
-            "t-on-scroll": this.alignAgendaScroll,
+            "t-on-scroll": () => this.throttledAlignScroll(this.visibleAgenda),
             "t-att-class": () => ({
                 "d-none": !(this.visibleAgenda && this.visibleAgenda.classList.contains("o_we_online_agenda_has_scroll")),
             }),
@@ -44,15 +44,30 @@ export class WebsiteEventTrack extends Interaction {
             this.checkAgendasOverflow(this.agendas);
         }
 
-        if (this.agendaScroller) {
-            this.updateAgendaScroll = this.debounced(this.updateAgendaScroll, 50);
-            this.updateAgendaScroll();
-        }
+        this.throttledAlignScroll = this.throttled(this.alignScroll);
+        this.throttledUpdateAgendaScroll = this.throttled(this.updateAgendaScroll);
+        this.throttledUpdateAgendaScroll();
     }
 
+    /**
+     * @deprecated
+     * Kept for stable policy, to remove in master (20.0).
+     */
     alignAgendaScroll() {
         if (this.visibleAgenda && this.agendaScroller) {
             this.visibleAgenda.scrollLeft = this.agendaScroller.scrollLeft;
+        }
+    }
+
+    /**
+     * @param {HTMLElement} toAlignEl
+     */
+    alignScroll(toAlignEl) {
+        if (this.visibleAgenda && this.agendaScroller) {
+            const alignedEl = toAlignEl === this.visibleAgenda
+                ? this.agendaScroller
+                : this.visibleAgenda;
+            toAlignEl.scrollLeft = alignedEl.scrollLeft;
         }
     }
 
@@ -86,11 +101,7 @@ export class WebsiteEventTrack extends Interaction {
             return (containerOffset.top < windowOffset.bottom) &&
                 !(containerOffset.bottom < windowOffset.bottom);
         });
-        if (this.visibleAgenda) {
-            requestAnimationFrame(() => {
-                this.agendaScroller.scrollLeft = this.visibleAgenda.scrollLeft;
-            });
-        }
+        this.throttledAlignScroll(this.agendaScroller);
     }
 
     /**
@@ -116,16 +127,12 @@ export class WebsiteEventTrack extends Interaction {
 
         currentTargetEl.classList.add("o_we_online_agenda_is_scrolling");
         currentTargetEl.classList.toggle("o_we_online_agenda_has_content_hidden", gap > Math.ceil(currentTargetEl.scrollLeft));
+        this.throttledAlignScroll(this.agendaScroller);
 
-        requestAnimationFrame(() => {
-            setTimeout(() => {
-                currentTargetEl.classList.remove("o_we_online_agenda_is_scrolling");
-            }, 200);
-        });
-
-        if (this.agendaScroller && this.visibleAgenda) {
-            this.agendaScroller.scrollLeft = this.visibleAgenda.scrollLeft;
-        }
+        clearTimeout(this.removeScrollingClassTimeout);
+        this.removeScrollingClassTimeout = this.waitForTimeout(() => {
+            currentTargetEl.classList.remove("o_we_online_agenda_is_scrolling");
+        }, 200);
     }
 
     computeScrollerWidth() {
