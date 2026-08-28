ed635bc00d02ab3b5fe75fa62390f4fbb3c879d7
Robin Engels (roen) <roen@odoo.com>
2026-08-28 17:54:34 +0200

    [FIX] point_of_sale: remove useless sanitizeData
    This fix is no longer required(https://github.com/odoo/odoo/pull/202752)
    as this one (https://github.com/odoo/odoo/pull/257043) is cleaner and
    less aggressive (it will only delete stale reward lines).
    
    This is the relevant part of the new fix to delete the previous one:
    https://github.com/odoo-dev/odoo/blob/f03f5ba011cf24193df6f558c7d7ac6d66574bc3/addons/pos_loyalty/static/src/overrides/models/pos_store.js#L100-L104
    
    opw-6447092

--- addons/point_of_sale/static/src/app/models/data_service.js
+++ addons/point_of_sale/static/src/app/models/data_service.js
@@ -302,19 +302,7 @@ export class PosData extends Reactive {
             "pos.payment": payments,
         });
         this.loadedIndexedDBProducts = dbData ? dbData["product.product"] : [];
-        this.sanitizeData();
         this.network.loading = false;
-    }
-
-    sanitizeData() {
-        const order_to_delete = this.models["pos.order"].filter((order) =>
-            order.lines.some((line) => line.is_reward_line && !line.coupon_id)
-        );
-        for (const order of order_to_delete) {
-            for (let i = order.lines.length - 1; i >= 0; i--) {
-                order.lines[i].delete();
-            }
-        }
     }
 
     async execute({
