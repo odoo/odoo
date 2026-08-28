ff32079b01fbc6ca3d71c44145911ce3e4a872ec
plha-odoo <plha@odoo.com>
2026-08-28 13:06:35 +0000

    [FIX] stock_*,mrp_subcontracting_dropshipping: dropship no impact avco
    stock_*:
    stock_account, stock_dropshipping, stock_landed_costs
    
    **Problem:**
    dropship moves impact the average cost
    of products which can lead to negative balance
    in stock valuation account
    
    **Steps to reproduce:**
    On a new db with no demo data and stock_dropshipping,
    sale_management and accountant module installed
    (bug also reproducible in runbot with same steps,
    but it's easier to see the negative impact
    on accounting on a new db) :
    1) enable dropshipping
    2) create a storable product with average perpetual
    category
    3) in the purchase tab set a vendor with a price of 10
    4) in the inventory tab select the dropship route
    5) create PO for 1 unit @ 5, validate receipt and confirm bill
    6) confirm a SO for 1 unit of the product
    7) confirm linked PO and validate dropship move
    8) confirm invoice and vendor bill
    -> see how the standard price is now 7.5
    
    9) remove dropship route from the inventory tab of
    the product
    10) confirm a SO for 1 unit of the product
    11) validate delivery and confirm invoice
    12) open 'inventory valuation' view
    
    **Current behavior:**
    the initial balance of stock valuation is -2.5
    
    **Expected behavior:**
    it should be 0 (there shouldn't be a negative
    initial balance if all invoices and bills are confirmed)
    
    **Cause of the issue:**
    The issue happens after step 8)
    The problem is that the dropship has an impact on the
    average price of the product but not on the accounting.
    
    Before the dropship we have 1 unit in stock @ 5
    and the stock valuation account has a balance of 5
    (from the bill), so all is good.
    
    The dropship then changes the standard price to 7.5.
    That's because currently, in _run_average_batch()
    the dropship move first impacts average cost like
    an incoming move with a value of 10 (at this point we
    have 1 move @ 10 and 1 @ 5 so average cost is 7.5)
    https://github.com/odoo/odoo/blob/bcaa8b6b00fa1f16513d680c5187fd424bc7cf8c/addons/stock_account/models/product.py#L493-L500
    https://github.com/odoo/odoo/blob/bcaa8b6b00fa1f16513d680c5187fd424bc7cf8c/addons/stock_account/models/product.py#L505-L510
    
    and then it impacts the value as a regular outgoing move
    (meaning it leaves the inventory at the average cost of
    7.5) and does not impact the average cost (which is the
    basic behaviour of outgoing moves)
    https://github.com/odoo/odoo/blob/bcaa8b6b00fa1f16513d680c5187fd424bc7cf8c/addons/stock_account/models/product.py#L515-L517
    https://github.com/odoo/odoo/blob/60b5b99d569dbe183ef9c980217ccfe9bf243756/addons/stock_account/models/product.py#L522
    
    Therefore after step 8), the standard price is 7.5 and
    we have a unit in stock so, in the inventory valuation
    view the ending stock is 7.5$.
    But the dropship did not impact the stock valuation
    account so the initial balance is still 5$ and we have
    lines with credits and debits of 2.5$ in the the stock
    variation section.
    
    After steps 9 to 12, both the initial balance and ending stock
    decrease by 7.5 (which is expected), leading to a negative
    initial balance in stock valuation.
    
    **fix:**
    We don't take into account the stock move
    from dropships in the avco computation
    
    **tests:**
    The fix requires modifications in a few tests:
    - test_dropship_bill_standard_price_update
    checks that  the bill of a dropship move impacts
    the standard price, so we delete this test
    - test_lot_normal_3, the asserts on the total_value
    still make sense but not those on standard_price
    - test_dropship_kit_bom_updates_component_standard_price
    test_average_cost_dropship_in_negative_quantity,
    test_out_move_validate_as_stock_user:
    standard price should not be impacted by dropship
    
    Task 6515358
    
    X-original-commit: 673d2f029eda3842979efca850c65358f919677a

--- addons/stock_account/models/stock_move.py
+++ addons/stock_account/models/stock_move.py
@@ -388,7 +388,7 @@ class StockMove(models.Model):
 
         for move in self:
             move = move.with_company(move.company_id)
-            if move.is_dropship or move.is_in:
+            if move.is_in:
                 products_to_recompute.add(move.product_id.id)
                 if move.product_id.lot_valuated:
                     if any(not ml.lot_id for ml in move.move_line_ids):
@@ -396,7 +396,6 @@ class StockMove(models.Model):
                             "A lot/serial number is required for product '%s' as it has lot valuation enabled.",
                             move.product_id.display_name))
                     lots_to_recompute.update(move.move_line_ids.lot_id.ids)
-            if move.is_in or move.is_dropship:
                 move.value = move.sudo()._get_value()
                 continue
             # Outgoing moves
