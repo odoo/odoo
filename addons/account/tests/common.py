22163801d7082c09c1be7c92e3ac6bc1bda5bedb
Saif (segn) <segn@odoo.com>
2026-08-28 15:46:47 +0200

    [IMP] account,l10n_*: Move closing accounts to return type
    Move the tax closing accounts from `account.tax.group` to
    `account.return.type` to make their configuration easier to find and avoid
    duplicating the same accounts across tax groups.
    
    Remove the accounts set in `account.tax.group-*.csv` files
    
    task-6237033

--- addons/account/tests/common.py
+++ addons/account/tests/common.py
@@ -356,8 +356,16 @@ class AccountTestInvoicingCommon(ProductCommon):
                     *account_company_domain,
                     ('account_type', '=', 'liability_payable')
                 ], limit=1),
-            'default_tax_account_receivable': company.account_purchase_tax_id.tax_group_id.tax_receivable_account_id,
-            'default_tax_account_payable': company.account_sale_tax_id.tax_group_id.tax_payable_account_id,
+            'default_tax_account_receivable': AccountAccount.search([
+                    *account_company_domain,
+                    ('account_type', '=', 'asset_receivable'),
+                    ('non_trade', '=', True),
+                ], limit=1),
+            'default_tax_account_payable': AccountAccount.search([
+                    *account_company_domain,
+                    ('account_type', '=', 'liability_payable'),
+                    ('non_trade', '=', True),
+                ], limit=1),
             'default_account_assets': AccountAccount.search([
                     *account_company_domain,
                     ('account_type', '=', 'asset_fixed')
