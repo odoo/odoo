156c4c38d945a49fdf3e8e7364eb2e2439883c06
Chong Wang (cwg) <cwg@odoo.com>
2026-08-30 20:17:31 +0000

    [FIX] test_translation_mode: fix patch
    Fix patch for test_translation_mode
    Patch tools in a compatible way which will work for other tenants.
    Patch tools in a compatible way which will work after uninstallation.
    python code translation is not patchable by design.
    
    closes odoo/odoo#286058
    
    X-original-commit: f28aa8e7aa0e2c6fb97c841711800410a1da3b72
    Signed-off-by: Chong Wang (cwg) <cwg@odoo.com>

--- addons/test_translation_mode/models/ir_http.py
+++ addons/test_translation_mode/models/ir_http.py
@@ -1,4 +1,7 @@
-from odoo import models
+from odoo import api, models
+from odoo.tools.misc import frozendict
+
+from odoo.addons.test_translation_mode.tools.translate import contextualize_entry
 
 
 class IrHttp(models.AbstractModel):
@@ -6,3 +9,23 @@ class IrHttp(models.AbstractModel):
 
     def _get_debug_modes(self):
         return super()._get_debug_modes() | {'translate'}
+
+    @api.model
+    def _get_translations_for_webclient(self, modules, lang):
+        translations_per_module, lang_params = super()._get_translations_for_webclient(modules, lang)
+        for module, data in translations_per_module.items():
+            messages = data.get('messages') or ()
+            translations_per_module[module] = frozendict({
+                'messages': tuple(
+                    frozendict({
+                        'id': msg['id'],
+                        'string': contextualize_entry({
+                            'module': module,
+                            'src': msg['id'],
+                            'value': msg['string'],
+                        })['value'],
+                    })
+                    for msg in messages
+                ),
+            })
+        return translations_per_module, lang_params
