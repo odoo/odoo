LAIDOUDI33/LAID_ODOO

[IMP] point-of-sale, pos_*: revamp burger menu buttons In this commit we've switched the burger menu buttons from a dropdown to a dialog.
task-6253998

addons/point_of_sale/static/tests/pos/tours/utils/chrome_util.js
@@ -19,7 +19,7 @@ export function clickMenuOption(name, options) {
         waitForMenuButtons(),
         clickMenuButton(),
         waitForMenuOptionsToOpen(),
-        clickMenuDropdownOption(name, options),
+        clickMenuDialogOption(name, options),
     ];
 }
 export function waitForMenuButtons() {
@@ -31,15 +31,22 @@ export function waitForMenuButtons() {
 export function waitForMenuOptionsToOpen() {
     return {
         content: `Wait for the menu options to be available`,
-        trigger: `span.dropdown-item`,
+        trigger: `.o_pos_burger_menu_buttons > button.btn`,
     };
 }
-export function clickMenuDropdownOption(name, { expectUnloadPage = false } = {}) {
+export function clickMenuDialogOption(name, { expectUnloadPage = false } = {}) {
     return {
         content: `click on something in the burger menu`,
-        trigger: `span.dropdown-item:contains(${name})`,
+        trigger: `.o_pos_burger_menu_buttons > button.btn:contains(${name})`,
         run: "click",
         expectUnloadPage,
+    };
+}
+export function closeBurgerMenu() {
+    return {
+        content: "close the burger menu dialog",
+        trigger: `.modal:not(.o_inactive_modal) .modal-header button[aria-label="Close"]`,
+        run: "click",
     };
 }
 export function existMenuOption(name) {
@@ -47,18 +54,20 @@ export function existMenuOption(name) {
         clickMenuButton(),
         {
             content: `check that ${name} exists in the burger menu`,
-            trigger: `span.dropdown-item:contains(${name})`,
+            trigger: `.o_pos_burger_menu_buttons > button.btn:contains(${name})`,
         },
-        clickMenuButton(),
+        closeBurgerMenu(),
     ];
 }
 export function notExistMenuOption(name) {
     return [
         clickMenuButton(),
+        waitForMenuOptionsToOpen(),
         {
             content: `check that ${name} doesn't exist in the burger menu`,
-            trigger: negate(`span.dropdown-item:contains(${name})`),
+            trigger: negate(`.o_pos_burger_menu_buttons > button.btn:contains(${name})`),
         },
+        closeBurgerMenu(),
     ];
 }
 export function isCashMoveButtonHidden() {
@@ -335,7 +344,7 @@ export function clickOnScanButton() {
 export function ClickOnCustomerDisplayButton() {
     return {
         content: "Click on the customer display button inside the burger menu",
-        trigger: "span i[data-icon='desktop_windows']",
+        trigger: "button i[data-icon='desktop_windows']",
         run: "click",
     };
 }
@@ -444,4 +453,11 @@ export function closePrintingWarning() {
             timeout: 15000,
         },
     ];
+}
+
+export function closeRegisterPopupIsShown() {
+    return {
+        content: "Check that the close register popup is shown",
+        trigger: ".modal .close-pos-popup",
+    };
 }

odoo/odoo

addons/point_of_sale/static/tests/pos/tours/utils/chrome_util.js
@@ -19,7 +19,7 @@ export function clickMenuOption(name, options) {
         waitForMenuButtons(),
         clickMenuButton(),
         waitForMenuOptionsToOpen(),
-        clickMenuDropdownOption(name, options),
+        clickMenuDialogOption(name, options),
     ];
 }
 export function waitForMenuButtons() {
@@ -31,15 +31,22 @@ export function waitForMenuButtons() {
 export function waitForMenuOptionsToOpen() {
     return {
         content: `Wait for the menu options to be available`,
-        trigger: `span.dropdown-item`,
+        trigger: `.o_pos_burger_menu_buttons > button.btn`,
     };
 }
-export function clickMenuDropdownOption(name, { expectUnloadPage = false } = {}) {
+export function clickMenuDialogOption(name, { expectUnloadPage = false } = {}) {
     return {
         content: `click on something in the burger menu`,
-        trigger: `span.dropdown-item:contains(${name})`,
+        trigger: `.o_pos_burger_menu_buttons > button.btn:contains(${name})`,
         run: "click",
         expectUnloadPage,
+    };
+}
+export function closeBurgerMenu() {
+    return {
+        content: "close the burger menu dialog",
+        trigger: `.modal:not(.o_inactive_modal) .modal-body button[aria-label="Close"]`,
+        run: "click",
     };
 }
 export function existMenuOption(name) {
@@ -47,18 +54,20 @@ export function existMenuOption(name) {
         clickMenuButton(),
         {
             content: `check that ${name} exists in the burger menu`,
-            trigger: `span.dropdown-item:contains(${name})`,
+            trigger: `.o_pos_burger_menu_buttons > button.btn:contains(${name})`,
         },
-        clickMenuButton(),
+        closeBurgerMenu(),
     ];
 }
 export function notExistMenuOption(name) {
     return [
         clickMenuButton(),
+        waitForMenuOptionsToOpen(),
         {
             content: `check that ${name} doesn't exist in the burger menu`,
-            trigger: negate(`span.dropdown-item:contains(${name})`),
+            trigger: negate(`.o_pos_burger_menu_buttons > button.btn:contains(${name})`),
         },
+        closeBurgerMenu(),
     ];
 }
 export function isCashMoveButtonHidden() {
@@ -335,7 +344,7 @@ export function clickOnScanButton() {
 export function ClickOnCustomerDisplayButton() {
     return {
         content: "Click on the customer display button inside the burger menu",
-        trigger: "span i[data-icon='desktop_windows']",
+        trigger: "button i[data-icon='desktop_windows']",
         run: "click",
     };
 }
@@ -444,4 +453,11 @@ export function closePrintingWarning() {
             timeout: 15000,
         },
     ];
+}
+
+export function closeRegisterPopupIsShown() {
+    return {
+        content: "Check that the close register popup is shown",
+        trigger: ".modal .close-pos-popup",
+    };
 }