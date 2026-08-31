f6039c3a68977edc2d31267937b07874210a7fe5
bepro@odoo <bepro@odoo.com>
2026-08-31 11:55:04 +0000

    [FIX] l10n_tr_nilvera_edispatch: Add dependency on stock_account
    View 'l10n_tr_nilvera_edispatch.view_picking_form_inherit_l10n_tr_nilvera_edispatch'
    fails to install in single-module test skip auto_install because it
    depends on field stock.picking:country_code. That field is provided by
    module 'stock_account' which is not in the dependency path of the module.
    In normal install the module 'stock_account' is present through auto_install
    when both 'account' and 'stock' are installed.
    
    Adding the direct dependency on 'stock_account' is not a problem because
    the view crashes without it. 'stock_account' is available through the
    chain below.
    
    [l10n_tr_nilvera_edispatch] ──[depends]──> [stock] ──⚡[AUTOLOAD]──> [stock_account]
    [l10n_tr_nilvera_edispatch] ──[depends]──> [l10n_tr_nilvera] ──[depends]──> [l10n_tr] ──[depends]──> [account] ──⚡[AUTOLOAD]──> [stock_account]
    
    X-original-commit: 219bab18b90ff977d69d3e0f46e47a498bc588da

--- addons/l10n_tr_nilvera_edispatch/__manifest__.py
+++ addons/l10n_tr_nilvera_edispatch/__manifest__.py
@@ -2,7 +2,13 @@
     'name': "Türkiye - e-Irsaliye (e-Dispatch)",
     'description': "Allows the users to create the UBL 1.2.1 e-Dispatch file",
     'countries': ['tr'],
+<<<<<<< b5327368234993d3829f6ea899deb20c4635be27
     'depends': ['l10n_tr_nilvera_einvoice', 'stock_account'],
+||||||| dcaf4ea12ed1f41ddb1c8e1ef03a2d024b3d3436
+    'depends': ['l10n_tr_nilvera', 'stock'],
+=======
+    'depends': ['l10n_tr_nilvera', 'stock', 'stock_account'],
+>>>>>>> fb19d7be30b78f5209423350418c940f0a6c3c2d
     'license': "LGPL-3",
     'category': 'Accounting/Localizations',
     'data': [
