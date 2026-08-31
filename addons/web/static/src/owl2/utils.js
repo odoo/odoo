e6b188b9e0d1a4a0d41cde2024b9ff677c8aaaa5
Julien Mougenot <jum@odoo.com>
2026-08-31 17:11:24 +0200

    [REF] *: remove useComponent
    This commit removes the deprecated 'useComponent' hook from the codebase.
    
    Its definition has been left in the Owl 2 compatibility module to allow
    remaining uses in o-spreadsheet.
    
    closes odoo/odoo#285708
    
    Signed-off-by: Michaël Mattiello (mcm) <mcm@odoo.com>

--- addons/web/static/src/owl2/utils.js
+++ addons/web/static/src/owl2/utils.js
@@ -11,7 +11,6 @@ export function render(component, deep = false) {
 }
 
 export const onWillRender = owl.onWillRender;
-export const useComponent = owl.useComponent;
 export const useLayoutEffect = owl.useLayoutEffect;
 export const useEnv = owl.useEnv;
 export const useSubEnv = owl.useSubEnv;
