0f3daf23af69088940b4e9f8b36efd9f5c4c45ce
Laetitia (ldau) <ldau@odoo.com>
2026-08-31 16:14:51 +0200

    [IMP] web,*: fixes dark mode
    This commit adjusts certain elements of dark mode:
    - It updates the dark color variables that were "hardcoded"
    - The gradient has been reintroduced more subtly; however, it was removed
    in certain cases where it was unnecessary or problematic.
    - Certain colors have been corrected to ensure better compatibility with
    dark mode.
    
    It also changes the background color assignment for time-off cards, which should actually use the color defined in the configurations.
    
    *: account, api_doc, hr_holidays, html_builder, html_editor, mail, mass_mailing,
    sale, spreadsheet, stock
    
    task-6522638

--- addons/account/static/src/views/account_journal_dashboard_graph/account_journal_dashboard_graph_field.js
+++ addons/account/static/src/views/account_journal_dashboard_graph/account_journal_dashboard_graph_field.js
@@ -7,7 +7,7 @@ import {
 } from "@web/views/fields/journal_dashboard_graph/journal_dashboard_graph_field";
 
 const colorScheme = cookie.get("color_scheme");
-const GRAPH_GRID_COLOR = getCustomColor(colorScheme, "#d8dadd", "#3C3E4B");
+const GRAPH_GRID_COLOR = getCustomColor(colorScheme, "#d8dadd", "#3A3B41");
 const GRAPH_LABEL_COLOR = getCustomColor(colorScheme, "#6c757d", "#ADB5BD");
 
 export class AccountJournalDashboardGraphField extends JournalDashboardGraphField {
