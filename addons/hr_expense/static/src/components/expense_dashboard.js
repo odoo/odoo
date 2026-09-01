1cdeb048d549bda57330f67ccf81bfa0d8c86254
Tom Hermann (toher) <toher@odoo.com>
2026-09-01 15:53:33 +0200

    [IMP] hr_expense: Disable expense dashboard banner on-click
    Disable the click on the banner to make it read-only. Disallowing all impact from the banner to the filters
    
    Task-6507226

--- addons/hr_expense/static/src/components/expense_dashboard.js
+++ addons/hr_expense/static/src/components/expense_dashboard.js
@@ -1,7 +1,6 @@
 import { useService } from '@web/core/utils/hooks';
 import { formatMonetary } from "@web/views/fields/formatters";
 import { Component, onWillStart, useState, onWillUpdateProps } from "@odoo/owl";
-import { Domain } from "@web/core/domain";
 
 export class ExpenseDashboard extends Component {
     static template = "hr_expense.ExpenseDashboard";
@@ -39,36 +38,5 @@ export class ExpenseDashboard extends Component {
         );
 
         this.state.expenses = expense_states;
-    }
-
-    async applyFilter(filterName) {
-        const searchModel = this.env.searchModel;
-
-        // Search for active filters implying an expense state
-        const stateActiveFilters = searchModel.query.filter(item => {
-            const filters = searchModel.searchItems[item.searchItemId];
-
-            if (!filters || !filters.domain) {
-                return false;
-            }
-            try {
-                const domain = new Domain(filters.domain).toList();
-                return domain.some(tuple => tuple[0] === "state");
-            } catch {
-                return false;
-            }
-        });
-
-        // Deactivate the filters implying an expense state
-        if (stateActiveFilters) {
-            stateActiveFilters.forEach(item => {
-                searchModel.toggleSearchItem(item.searchItemId);
-            });
-        }
-        // Toggle the filter corresponding to the current state
-        const searchItem = Object.values(searchModel.searchItems).find(
-            (searchItem) => searchItem.name === filterName
-        );
-        searchModel.toggleSearchItem(searchItem.id);
     }
 }
