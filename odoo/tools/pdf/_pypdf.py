9b3d0a5a8a3a919a2e10ed2224e3aa2052437939
Pierre Paridans <app@odoo.com>
2026-09-01 15:56:31 +0200

    [FIX] tools: export pypdf's PageObject
    Prior to this commit, `PageObject` was not re-exported by
    `odoo.tools.pdf`, forcing tests to patch internal module paths like
    `PyPDF2._page.PageObject`.
    
    This resulted in test failures depending on the installed PDF library
    version:
    - `pypdf` (>= 3.0.0): `PyPDF2` submodules no longer exist.
    - `PyPDF2` 1.x: `PageObject` resides in `PyPDF2.pdf` rather than
      `PyPDF2._page`.
    
    To resolve this, this commit re-exports `PageObject` through
    `odoo.tools.pdf` across all backend wrappers.
    
    runbot-946795
    
    closes odoo/odoo#285978
    
    Related: odoo/enterprise#130017
    Signed-off-by: Krzysztof Magusiak (krma) <krma@odoo.com>

--- odoo/tools/pdf/_pypdf.py
+++ odoo/tools/pdf/_pypdf.py
@@ -1,11 +1,11 @@
 from typing import Dict, Any
 
-import pypdf
-from pypdf import errors, filters, generic, PdfReader as _Reader, PdfWriter as _Writer
+from pypdf import errors, filters, generic, PageObject, PdfReader as _Reader, PdfWriter as _Writer
 from pypdf.generic import create_string_object
 from pypdf import __version__  # noqa: F401
 
 __all__ = [
+    "PageObject",
     "PdfReader",
     "PdfWriter",
     "create_string_object",
@@ -24,10 +24,10 @@ def _set_crop_box(self, value):
     self.cropbox = value
 
 
-pypdf.PageObject.mergePage = lambda self, page2: self.merge_page(page2)
-pypdf.PageObject.compressContentStreams = lambda self: self.compress_content_streams()
-pypdf.PageObject.mediaBox = property(lambda self: self.mediabox, _set_media_box)
-pypdf.PageObject.cropBox = property(lambda self: self.cropbox, _set_crop_box)
+PageObject.mergePage = lambda self, page2: self.merge_page(page2)
+PageObject.compressContentStreams = lambda self: self.compress_content_streams()
+PageObject.mediaBox = property(lambda self: self.mediabox, _set_media_box)
+PageObject.cropBox = property(lambda self: self.cropbox, _set_crop_box)
 # use lambdas (rather than copying) to allow overrides of the base method
 generic.PdfObject.getObject = lambda self: self.get_object()
 generic.StreamObject.getData = lambda self: self.get_data()
