9d48936894cbf44222f490e813ab9108ec43ebba
ripil-odoo <ripil@odoo.com>
2026-09-01 15:23:05 +0530

    [IMP] account: template-level version tracking for COA updates
    Before this commit:
    - When coa was updated, there was no way for the user to know
      about the update
    
    After this commit:
    - Introduce the `version` key in chart template mapping.
    - Store `coa_version` and `skipped_coa_version` on `res_company`
      to check on company level.
    - Banner appears on the accounting dashboard when there is a
      mismatch in the stored or skipped version and the template
      version with an option to reload or dismiss (skip) it.
    - Also, created test cases to check banner logic.
    
    task-6236419

--- addons/account/models/account_journal_dashboard.py
+++ addons/account/models/account_journal_dashboard.py
@@ -11,6 +11,7 @@ from odoo.fields import Command, Domain
 from odoo.release import version
 from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DF, SQL, date_utils
 from odoo.tools.misc import formatLang, format_date as odoo_format_date, get_lang
+from odoo.tools.parse_version import parse_version
 
 
 def group_by_journal(vals_list):
@@ -644,34 +645,37 @@ class AccountJournal(models.Model):
         self.env['account.move.line'].flush_model()
         self.env['account.payment'].flush_model()
         dashboard_data = {}  # container that will be filled by functions below
-
-        company_banner = {}
+        company_data = {}
+        chart_template_data = self.env['account.chart.template']._get_chart_template_mapping()
         for company in self.mapped('company_id'):
             template_code = company.chart_template
+            template_data = chart_template_data.get(template_code, {})
             show_banner = False
             coa_name = ''
-            if template_code:
-                template_info = self.env['account.chart.template']._get_chart_template_mapping().get(template_code, {})
-                module = self.env['ir.module.module'].search([
-                    ('name', '=', template_info.get('module')),
-                    ('state', '=', 'installed'),
-                ], limit=1)
-                # due to Odoo's legacy naming, latest_version contains the module version installed in the db
-                if module and company.coa_version != module.latest_version:
+            if template_code and self.env.is_admin():
+                template_version = template_data.get('version')
+                if (
+                    template_version
+                    and company.coa_version
+                    and parse_version(company.coa_version) < parse_version(template_version)
+                    and (
+                        not company.skipped_coa_version
+                        or parse_version(template_version) > parse_version(company.skipped_coa_version)
+                    )
+                ):
                     show_banner = True
-                    coa_name = template_info.get('name', '')
-            company_banner[company.id] = {
+                    coa_name = template_data.get('name')
+            company_data[company.id] = {
                 'show_banner': show_banner,
                 'coa_name': coa_name,
             }
-
         for journal in self:
             dashboard_data[journal.id] = {
                 'currency_id': journal.currency_id.id or journal.company_id.sudo().currency_id.id,
                 'show_company': len(self.env.companies) > 1 or journal.company_id.id != self.env.company.id,
                 'company_name': journal.company_id.sudo().name,
-                'show_coa_banner': company_banner[journal.company_id.id]['show_banner'],
-                'coa_name': company_banner[journal.company_id.id]['coa_name'],
+                'show_coa_banner': company_data[journal.company_id.id]['show_banner'],
+                'coa_name': company_data[journal.company_id.id]['coa_name'],
             }
         self._fill_bank_cash_dashboard_data(dashboard_data)
         self._fill_sale_purchase_dashboard_data(dashboard_data)
@@ -1458,8 +1462,19 @@ class AccountJournal(models.Model):
 
     def action_reload_coa(self):
         self.ensure_one()
-        company = self.company_id
-        template_code = company.chart_template
-        if not template_code:
+        if not self.company_id.chart_template:
             return
-        self.env['account.chart.template'].try_loading(template_code, company=company)
+        return self.env['account.chart.template'].try_loading(
+            template_code=self.company_id.chart_template,
+            company=self.company_id,
+        )
+
+    def action_skip_coa_reload(self):
+        self.ensure_one()
+        chart_template_code = self.company_id.chart_template
+        if not chart_template_code:
+            return
+        chart_template_mapping = self.env['account.chart.template']._get_chart_template_mapping()
+        template_version = chart_template_mapping.get(chart_template_code, {}).get('version')
+        if template_version:
+            self.company_id.skipped_coa_version = template_version
--- addons/account/models/company.py
+++ addons/account/models/company.py
@@ -287,7 +287,10 @@ class ResCompany(models.Model):
         help="Default on whether the sales price used on the product and invoices with this Company includes its taxes."
     )
     company_vat_placeholder = fields.Char(compute='_compute_company_vat_placeholder')
-
+    coa_version = fields.Char(
+        string="COA Version",
+        help="Version of the chart of accounts template that was last loaded for this company.",
+    )
     income_account_id = fields.Many2one(
         comodel_name='account.account',
         string="Income Account",
@@ -322,6 +325,10 @@ class ResCompany(models.Model):
         domain=ACCOUNT_DOMAIN,
         help="During perpetual valuation, this account will hold the price difference between the standard price and the bill price.",
     )
+    skipped_coa_version = fields.Char(
+        string="Skipped COA Version",
+        help="Version of the chart of accounts template update that was skipped for this company.",
+    )
 
     # Cash Rounding default accounts
     cash_rounding_profit_account_id = fields.Many2one(
@@ -337,8 +344,6 @@ class ResCompany(models.Model):
     has_ledger = fields.Boolean(
         compute='_compute_has_ledger',
     )
-
-    coa_version = fields.Char(string="COA Version")
 
     def get_next_batch_payment_communication(self):
         '''
