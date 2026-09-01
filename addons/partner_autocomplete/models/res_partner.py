f4ab0dfe248f288d7818502578df690fb2c929e4
Jérémy Bazin (baje) <baje@odoo.com>
2026-09-01 14:09:33 +0200

    [IMP] partner_autocomplete: add INPI data to French autocomplete
    INPI provides official and frequently updated information about French companies. Using this data in partner autocomplete gives users more complete information when creating French partners and companies.
    
    French companies can also be found by name, SIREN, or SIRET while keeping the existing provider as a fallback.
    
    Task-6348880

--- addons/partner_autocomplete/models/res_partner.py
+++ addons/partner_autocomplete/models/res_partner.py
@@ -84,6 +84,16 @@ class ResPartner(models.Model):
 
     @api.model
     def autocomplete_by_field(self, fieldName, query, query_country_id, timeout=15):
+        # SIREN and SIRET do not carry a country prefix, so always search them in France.
+        if (
+            self.env['ir.module.module']._get('l10n_fr').state == 'installed'
+            and (
+                fieldName in ('FR_SIREN', 'FR_SIRET')
+                or (fieldName == 'name' and query.isdigit() and len(query) in (9, 14))
+            )
+        ):
+            return self.autocomplete_by_vat(query, self.env.ref('base.fr').id)
+
         match fieldName:
             case "name":
                 return self.autocomplete_by_name(query, query_country_id)
@@ -93,6 +103,13 @@ class ResPartner(models.Model):
                 return self.autocomplete_by_vat(query, query_country_id)
 
     @api.model
+    def _get_supported_enrichment_types(self):
+        enrichment_types = ['duns']
+        if self.env['ir.module.module']._get('l10n_fr').state == 'installed':
+            enrichment_types.append('vat')
+        return enrichment_types
+
+    @api.model
     def autocomplete_by_name(self, query, query_country_id, timeout=15):
         if query_country_id is False:  # If it's 0, we purposely do not want to filter on the country
             query_country_id = self.env.company.country_id.id
@@ -100,6 +117,7 @@ class ResPartner(models.Model):
         response, _ = self.env['iap.autocomplete.api']._request_partner_autocomplete('search_by_name', {
             'query': query,
             'query_country_code': query_country_code,
+            'supported_enrichment_types': self._get_supported_enrichment_types(),
         }, timeout=timeout)
         if response and not response.get("error"):
             results = []
@@ -116,6 +134,7 @@ class ResPartner(models.Model):
         response, _ = self.env['iap.autocomplete.api']._request_partner_autocomplete('search_by_vat', {
             'query': vat,
             'query_country_code': query_country_code,
+            'supported_enrichment_types': self._get_supported_enrichment_types(),
         }, timeout=timeout)
         if response and not response.get("error"):
             results = []
@@ -201,6 +220,13 @@ class ResPartner(models.Model):
     def enrich_by_duns(self, duns, timeout=15):
         response, error = self.env['iap.autocomplete.api']._request_partner_autocomplete('enrich_by_duns', {
             'duns': duns,
+        }, timeout=timeout)
+        return self._process_enriched_response(response, error)
+
+    @api.model
+    def enrich_by_vat(self, vat, timeout=15):
+        response, error = self.env['iap.autocomplete.api']._request_partner_autocomplete('enrich_by_vat', {
+            'vat': vat,
         }, timeout=timeout)
         return self._process_enriched_response(response, error)
 
