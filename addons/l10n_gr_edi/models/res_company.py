34cec2f9948ea800c6baa4e124da7587d0def921
Abo Taha <ahmoh@odoo.com>
2026-09-01 10:16:52 +0200

    [REF] l10n_gr_edi{_e_invoo}: merge e-invoo bridge module
    The e-invoo integration is now the standard invoice issuance flow for
    Greek EDI, the bridge module was just added to preserve the stable
    policy but in master no need for it.
    
    Remove the bridge module and adapt the new flow in the main l10n_gr_edi
    module.
    
    Related: https://github.com/odoo/odoo/pull/281739

--- addons/l10n_gr_edi/models/res_company.py
+++ addons/l10n_gr_edi/models/res_company.py
@@ -8,6 +8,8 @@ from requests import RequestException
 from odoo import api, fields, models, Command
 from odoo.addons.l10n_gr_edi.models.preferred_classification import INVOICE_TYPES_HAVE_EXPENSE, VAT_CATEGORY_TO_RATE
 
+from .account_edi_proxy_user import L10N_GR_EDI_PROXY_TYPE
+
 NS_MYDATA = {"ns": "http://www.aade.gr/myDATA/invoice/v1.0"}
 
 _logger = logging.getLogger(__name__)
@@ -24,6 +26,22 @@ class ResCompany(models.Model):
         default=True,
         help="Enable test environments with credentials obtained from https://mydata-dev-register.azurewebsites.net/",
     )
+
+    def _l10n_gr_edi_get_or_create_proxy_user(self):
+        self.ensure_one()
+        edi_mode = 'test' if self.l10n_gr_edi_test_env else 'prod'
+
+        domain = [
+            ('company_id', '=', self.id),
+            ('proxy_type', '=', L10N_GR_EDI_PROXY_TYPE),
+            ('edi_mode', '=', edi_mode),
+        ]
+        proxy_user_sudo = self.env['account_edi_proxy_client.user'].sudo()
+        if proxy_user := proxy_user_sudo.search(domain, limit=1):
+            return proxy_user
+
+        self.lock_for_update()
+        return proxy_user_sudo._register_proxy_user(self, L10N_GR_EDI_PROXY_TYPE, edi_mode)
 
     @api.model
     def _cron_l10n_gr_edi_fetch_invoices(self):
