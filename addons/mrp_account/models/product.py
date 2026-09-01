bdf766766fac31049da60f0f09d7920af23ae9b1
Lucas Gvasalia <lugva@odoo.com>
2026-09-01 16:19:25 +0200

    [IMP] mrp_subcontracting_purchase,*: include BoM components in MO cost
    * - purchase_mrp
      - mrp_account
    
    In the MO overview, if one component is subcontracted, the MO cost is
    equal to the RFQ/PO price. However, to ensure accuracy, it should also
    include the BoM components.
    
    This commit ensures that for subcontracted products price is calculated
    correctly, by also taking into account the quantity of the product.
    
    task-6516013
    
    closes odoo/odoo#285980
    
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- addons/mrp_account/models/product.py
+++ addons/mrp_account/models/product.py
@@ -60,7 +60,10 @@ class ProductProduct(models.Model):
                 if price:
                     self.standard_price = price
 
-    def _compute_bom_price(self, bom, boms_to_recompute=False, byproduct_bom=False):
+    def _get_bom_components_price(self, bom, boms_to_recompute=False, byproduct_bom=False):
+        """
+        Calculates only the price of the BoM components
+        """
         self.ensure_one()
         if not bom:
             return 0
@@ -97,6 +100,9 @@ class ProductProduct(models.Model):
                 total *= float_round(1 - byproduct_cost_share / 100, precision_rounding=0.0001)
             return bom.uom_id._compute_price(total / bom.product_qty, self.uom_id)
         return 0.0
+
+    def _compute_bom_price(self, bom, boms_to_recompute=False, byproduct_bom=False):
+        return self._get_bom_components_price(bom, boms_to_recompute=boms_to_recompute, byproduct_bom=byproduct_bom)
 
     def _compute_value(self):
         """
