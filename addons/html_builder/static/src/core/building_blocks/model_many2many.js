LAIDOUDI33/LAID_ODOO

[REF] upgrade_code,web,*: convert field useService to plugin
*: html_builder,html_editor,spreadsheet,spreadsheet_dashboard

`fieldService` has been converted to an OWL3 Plugin, so existing
useService("field") call sites need to be rewritten.
This commit is the result of the `owl3-migration->useservice` migration.

addons/html_builder/static/src/core/building_blocks/model_many2many.js
@@ -1,6 +1,6 @@
-import { Component, onWillStart, onWillUpdateProps, props, proxy, status, t } from "@odoo/owl";
+import { FieldPlugin } from "@web/core/field_plugin";
+import { Component, onWillStart, onWillUpdateProps, plugin, props, proxy, status, t } from "@odoo/owl";
 import { uniqueId } from "@web/core/utils/functions";
-import { useService } from "@web/core/utils/hooks";
 import { useDomState } from "@html_builder/core/utils";
 import { useCachedModel } from "@html_builder/core/cached_model_utils";
 import { BuilderComponent } from "./builder_component";
@@ -24,7 +24,7 @@ export class ModelMany2Many extends Component {
     static components = { BuilderComponent, BasicMany2Many };
 
     setup() {
-        this.fields = useService("field");
+        this.fields = plugin(FieldPlugin);
         this.cachedModel = useCachedModel();
         this.state = proxy({
             searchModel: undefined,

addons/web/static/src/core/domain_selector/domain_selector.js
@@ -1,4 +1,5 @@
-import { Component, onWillStart, onWillUpdateProps, props, t } from "@odoo/owl";
+import { FieldPlugin } from "@web/core/field_plugin";
+import { Component, onWillStart, onWillUpdateProps, plugin, props, t } from "@odoo/owl";
 import { CheckBox } from "@web/core/checkbox/checkbox";
 import { Domain } from "@web/core/domain";
 import { getDomainDisplayedOperators } from "@web/core/domain_selector/domain_selector_operator_editor";
@@ -36,7 +37,7 @@ export class DomainSelector extends Component {
     props = props(domainSelectorProps);
 
     setup() {
-        this.fieldService = useService("field");
+        this.fieldService = plugin(FieldPlugin);
         this.treeProcessor = useService("tree_processor");
 
         this.tree = null;

addons/web/static/src/core/model_field_selector/model_field_selector.js
@@ -1,7 +1,7 @@
-import { Component, onWillStart, onWillUpdateProps, props, proxy, t } from "@odoo/owl";
+import { FieldPlugin } from "@web/core/field_plugin";
+import { Component, onWillStart, onWillUpdateProps, plugin, props, proxy, t } from "@odoo/owl";
 import { usePopover } from "@web/core/popover/popover_hook";
 import { KeepLast } from "@web/core/utils/concurrency";
-import { useService } from "@web/core/utils/hooks";
 import { hasTouch } from "@web/core/browser/feature_detection";
 import { ModelFieldSelectorPopover } from "./model_field_selector_popover";
 
@@ -26,7 +26,7 @@ export class ModelFieldSelector extends Component {
     });
 
     setup() {
-        this.fieldService = useService("field");
+        this.fieldService = plugin(FieldPlugin);
         this.popover = usePopover(this.constructor.components.Popover, {
             popoverClass: "o_popover_field_selector",
             onClose: async () => {

odoo/odoo

addons/html_builder/static/src/core/building_blocks/model_many2many.js
@@ -4,9 +4,9 @@ import {
     getAllActionsAndOperations,
     useDomState,
 } from "@html_builder/core/utils";
-import { Component, onWillStart, proxy, status, t, useEffect, useProps } from "@odoo/owl";
+import { Component, onWillStart, plugin, proxy, status, t, useEffect, useProps } from "@odoo/owl";
+import { FieldPlugin } from "@web/core/field_plugin";
 import { uniqueId } from "@web/core/utils/functions";
-import { useService } from "@web/core/utils/hooks";
 import { BasicMany2Many } from "./basic_many2many";
 import { BuilderComponent } from "./builder_component";
 
@@ -27,7 +27,7 @@ export class ModelMany2Many extends Component {
     });
 
     setup() {
-        this.fields = useService("field");
+        this.fields = plugin(FieldPlugin);
         this.cachedModel = useCachedModel();
         this.state = proxy({
             searchModel: undefined,

addons/web/static/src/core/domain_selector/domain_selector.js
@@ -1,7 +1,8 @@
-import { Component, onWillStart, onWillUpdateProps, t, useProps } from "@odoo/owl";
+import { Component, onWillStart, onWillUpdateProps, plugin, t, useProps } from "@odoo/owl";
 import { CheckBox } from "@web/core/checkbox/checkbox";
 import { Domain } from "@web/core/domain";
 import { getDomainDisplayedOperators } from "@web/core/domain_selector/domain_selector_operator_editor";
+import { FieldPlugin } from "@web/core/field_plugin";
 import { _t } from "@web/core/l10n/translation";
 import { ModelFieldSelector } from "@web/core/model_field_selector/model_field_selector";
 import {
@@ -36,7 +37,7 @@ export class DomainSelector extends Component {
     props = useProps(domainSelectorProps);
 
     setup() {
-        this.fieldService = useService("field");
+        this.fieldService = plugin(FieldPlugin);
         this.treeProcessor = useService("tree_processor");
 
         this.tree = null;

addons/web/static/src/core/model_field_selector/model_field_selector.js
@@ -1,7 +1,7 @@
-import { Component, onWillStart, onWillUpdateProps, proxy, t, useProps } from "@odoo/owl";
+import { Component, onWillStart, onWillUpdateProps, plugin, proxy, t, useProps } from "@odoo/owl";
 import { usePopover } from "@web/core/popover/popover_hook";
+import { FieldPlugin } from "@web/core/field_plugin";
 import { KeepLast } from "@web/core/utils/concurrency";
-import { useService } from "@web/core/utils/hooks";
 import { hasTouch } from "@web/core/browser/feature_detection";
 import { ModelFieldSelectorPopover } from "./model_field_selector_popover";
 
@@ -26,7 +26,7 @@ export class ModelFieldSelector extends Component {
     });
 
     setup() {
-        this.fieldService = useService("field");
+        this.fieldService = plugin(FieldPlugin);
         this.popover = usePopover(this.constructor.components.Popover, {
             popoverClass: "o_popover_field_selector",
             onClose: async () => {