d1da18ba8d5e6502c85e44e940115d810d02ea69
Chong Wang (cwg) <cwg@odoo.com>
2026-09-02 09:44:39 +0200

    [IMP] orm: non-blocking connection pool for gevent
    make gevent connection pool non-blocking to avoid
    'The Connection Pool Is Full' error when too many
    coroutines are borrowing connections in gevent
    server.

--- addons/bus/websocket.py
+++ addons/bus/websocket.py
@@ -13,7 +13,7 @@ import threading
 import time
 from collections import defaultdict, deque
 from collections.abc import Buffer
-from contextlib import ExitStack, contextmanager, suppress
+from contextlib import suppress
 from enum import IntEnum
 from itertools import count
 from queue import PriorityQueue
@@ -49,34 +49,8 @@ _logger = logging.getLogger(__name__)
 _stopping = threading.Event()
 
 
-MAX_TRY_ON_POOL_ERROR = 10
-DELAY_ON_POOL_ERROR = 0.15
-JITTER_ON_POOL_ERROR = 0.3
-
-
-@contextmanager
 def acquire_cursor(db):
-    """ Try to acquire a cursor up to `MAX_TRY_ON_POOL_ERROR` """
-    delay = DELAY_ON_POOL_ERROR
-    try:
-        for _ in range(MAX_TRY_ON_POOL_ERROR):
-            # Yield before trying to acquire the cursor to let other
-            # greenlets release their cursor.
-            time.sleep(0)
-            with ExitStack() as stack:
-                cr = None
-                with suppress(PoolError):
-                    cr = stack.enter_context(db_connect(db).cursor())
-                if cr is not None:
-                    yield cr
-                    return
-            time.sleep(delay + random.uniform(0, JITTER_ON_POOL_ERROR))
-            delay *= 1.5
-        raise PoolError('Failed to acquire cursor after %s retries' % MAX_TRY_ON_POOL_ERROR)
-    finally:
-        # Yield after releasing the cursor to let waiting greenlets
-        # immediately pick up the freed connection.
-        time.sleep(0)
+    return db_connect(db).cursor()
 
 # ------------------------------------------------------
 # EXCEPTIONS
