a3cd9d8f82ccb5f244405be444a392058b748b20
Alessandro Lupo <alup@odoo.com>
2026-09-02 20:52:59 +0200

    fixup! [MOV] website, *: move `location_selector` to `website`

--- addons/website/static/src/components/location_selector/location/location.js
+++ addons/website/static/src/components/location_selector/location/location.js
@@ -6,13 +6,14 @@ export class Location extends Component {
     static components = { LocationSchedule };
     static template = "website.locationSelector.location";
     props = useProps({
+        additionalData: t.object().optional(),
         id: t.string(),
         number: t.number(),
         name: t.string(),
         street: t.string(),
         city: t.string(),
         zipCode: t.string(),
-        openingHours: t.record(t.array(t.string()).optional()),
+        openingHours: t.record(t.array(t.string()).optional()).optional({}),
         additionalData: t.object().optional(),
         isSelected: t.boolean(),
         setSelectedLocation: t.function(),
