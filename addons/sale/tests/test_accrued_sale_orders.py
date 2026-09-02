odoo/odoo

commit 81f25bc57b8433a65bf33950c64dc7582240a229

[IMP] *: rework accrual entries for PO/SO
*: account, pos_sale, purchase, purchase_mrp, purchase_stock, sale,
sale_mrp, sale_project, sale_stock, sale_timesheet, stock_account

This commit modifies `sale.order.line` and `purchase.order.line` models
to work with the new accrual reports.

List of changes made:

Split compute methods
=====================
The `purchase.order.line` has two fields, `qty_received` and
`qty_invoiced`, and we would like to be able to know what was those
fields value in the past at a given date. The `sale.order.line` has also
two fields in the same situation, `qty_delivered` and `qty_invoiced`.

Currently, those fields are computed and stored, thus we need a
computed non-stored version of those fields to be able to re-compute
their value depending of the context (so, we can pass the accrual date
into the context.) The non-stored version of those fields have the same
name with the suffix "at_date" (eg.: `qty_invoiced_at_date`.)

To achieve that, we move all the compute code outside of the compute
method to have a method we can call from both fields' compute.
For example, we have now a method named `_prepare_qty_delivered` which
returns the `qty_delivered` value for the SO lines, and both
`_compute_qty_delivered` and `_compute_qty_delivered_at_date` use it.

"at_date" fields
================
Those new computed stored fields use the `accrual_entry_date` context
key to compute their value at this date.
This key was already there in the code and already used by the accrual
wizard but before, it was always an actual date object, now it can be a
string (when pass in the context from the client side) and thus needs to
be parsed.

A field `amount_to_invoice_at_date` was also added, both for PO line and
SO line, and this field is simply the difference between the
delivered/received quantity and the invoiced quantity times the price
unit. It is used by the accrual views (see enterprise PR) and the
accrual wizard.

addons/sale/models/sale_order_line.py
@@ -147,6 +147,7 @@ class SaleOrderLine(models.Model):
     linked_line_ids = fields.One2many(
         string="Linked Order Lines", comodel_name='sale.order.line', inverse_name='linked_line_id',
     )
+    categ_id = fields.Many2one(related='product_id.categ_id')
     # Uniquely identifies this sale order line before the record is saved in the DB, i.e. before the
     # record has an `id`.
     virtual_id = fields.Char()
@@ -287,6 +288,18 @@ class SaleOrderLine(models.Model):
         compute='_compute_amount_to_invoice',
         compute_sudo=True,  # ensure same access as `untaxed_amount_to_invoice`
     )
+    amount_to_invoice_at_date = fields.Float(string='Amount', compute='_compute_amount_to_invoice_at_date')
+
+    # Same than `qty_delivered` and `qty_invoiced` but non-stored and depending of the context.
+    qty_delivered_at_date = fields.Float(
+        string="Delivered",
+        compute='_compute_qty_delivered_at_date',
+        digits='Product Unit')
+    qty_invoiced_at_date = fields.Float(
+        string="Invoiced",
+        compute='_compute_qty_invoiced_at_date',
+        digits='Product Unit')
+
     # Technical field holding custom data for the taxes computation engine.
     extra_tax_data = fields.Json()
 
@@ -876,11 +889,31 @@ class SaleOrderLine(models.Model):
             take their concerned so lines, compute and set the `qty_delivered` field, and call super with the remaining
             records.
         """
+        delivered_qties = self._prepare_qty_delivered()
+        for so_line in self:
+            if not so_line.qty_delivered or so_line in delivered_qties:
+                so_line.qty_delivered = delivered_qties[so_line]
+
+    @api.depends('qty_delivered')
+    @api.depends_context('accrual_entry_date')
+    def _compute_qty_delivered_at_date(self):
+        if not self._date_in_the_past():
+            # Avoid useless compute if we don't look in the past.
+            for line in self:
+                line.qty_delivered_at_date = line.qty_delivered
+            return
+        delivered_qties = self._prepare_qty_delivered()
+        for line in self:
+            line.qty_delivered_at_date = delivered_qties[line]
+
+    def _prepare_qty_delivered(self):
         # compute for analytic lines
+        delivered_qties = defaultdict(float)
         lines_by_analytic = self.filtered(lambda sol: sol.qty_delivered_method == 'analytic')
         mapping = lines_by_analytic._get_delivered_quantity_by_analytic([('amount', '<=', 0.0)])
         for so_line in lines_by_analytic:
-            so_line.qty_delivered = mapping.get(so_line.id or so_line._origin.id, 0.0)
+            delivered_qties[so_line] = mapping.get(so_line.id or so_line._origin.id, 0.0)
+        return delivered_qties
 
     def _get_downpayment_state(self):
         self.ensure_one()
@@ -897,8 +930,8 @@ class SaleOrderLine(models.Model):
         return ''
 
     def _get_delivered_quantity_by_analytic(self, additional_domain):
-        """ Compute and write the delivered quantity of current SO lines, based on their related
-            analytic lines.
+        """ Compute and return the delivered quantity of current SO lines,
+            based on their related analytic lines.
             :param additional_domain: domain to restrict AAL to include in computation (required since timesheet is an AAL with a project ...)
         """
         result = defaultdict(float)
@@ -937,15 +970,34 @@ class SaleOrderLine(models.Model):
         a refund made would automatically decrease the invoiced quantity, then there is a risk of reinvoicing
         it automatically, which may not be wanted at all. That's why the refund has to be created from the SO
         """
+        invoiced_quantities = self._prepare_qty_invoiced()
         for line in self:
-            qty_invoiced = 0.0
+            if not line.qty_invoiced or line in invoiced_quantities:
+                line.qty_invoiced = invoiced_quantities[line]
+
+    @api.depends('qty_invoiced')
+    @api.depends_context('accrual_entry_date')
+    def _compute_qty_invoiced_at_date(self):
+        if not self._date_in_the_past():
+            # Avoid useless compute if we don't look in the past.
+            for line in self:
+                line.qty_invoiced_at_date = line.qty_invoiced
+            return
+        invoiced_quantities = self._prepare_qty_invoiced()
+        for line in self:
+            line.qty_invoiced_at_date = invoiced_quantities[line]
+
+    def _prepare_qty_invoiced(self):
+        invoiced_qties = defaultdict(float)
+        for line in self:
             for invoice_line in line._get_invoice_lines():
                 if invoice_line.move_id.state != 'cancel' or invoice_line.move_id.payment_state == 'invoicing_legacy':
+                    invoice_qty = invoice_line.product_uom_id._compute_quantity(invoice_line.quantity, line.product_uom_id)
                     if invoice_line.move_id.move_type == 'out_invoice':
-                        qty_invoiced += invoice_line.product_uom_id._compute_quantity(invoice_line.quantity, line.product_uom_id)
+                        invoiced_qties[line] += invoice_qty
                     elif invoice_line.move_id.move_type == 'out_refund':
-                        qty_invoiced -= invoice_line.product_uom_id._compute_quantity(invoice_line.quantity, line.product_uom_id)
-            line.qty_invoiced = qty_invoiced
+                        invoiced_qties[line] -= invoice_qty
+        return invoiced_qties
 
     @api.depends('invoice_lines.move_id.state', 'invoice_lines.quantity')
     def _compute_qty_invoiced_posted(self):
@@ -967,8 +1019,9 @@ class SaleOrderLine(models.Model):
     def _get_invoice_lines(self):
         self.ensure_one()
         if self.env.context.get('accrual_entry_date'):
+            accrual_date = fields.Date.from_string(self.env.context['accrual_entry_date'])
             return self.invoice_lines.filtered(
-                lambda l: l.move_id.invoice_date and l.move_id.invoice_date <= self.env.context['accrual_entry_date']
+                lambda l: l.move_id.invoice_date and l.move_id.invoice_date <= accrual_date
             )
         else:
             return self.invoice_lines
@@ -1139,6 +1192,12 @@ class SaleOrderLine(models.Model):
                 line.amount_to_invoice = unit_price_total * qty_to_invoice
             else:
                 line.amount_to_invoice = 0.0
+
+    @api.depends('price_unit', 'qty_invoiced_at_date', 'qty_delivered_at_date')
+    @api.depends_context('accrual_entry_date')
+    def _compute_amount_to_invoice_at_date(self):
+        for line in self:
+            line.amount_to_invoice_at_date = (line.qty_delivered_at_date - line.qty_invoiced_at_date) * line.price_unit
 
     @api.depends('order_id.partner_id', 'product_id')
     def _compute_analytic_distribution(self):
@@ -1647,6 +1706,13 @@ class SaleOrderLine(models.Model):
                 round=False,
             )
         return amount
+
+    @api.model
+    def _date_in_the_past(self):
+        if not 'accrual_entry_date' in self.env.context:
+            return False
+        accrual_date = fields.Date.from_string(self.env.context['accrual_entry_date'])
+        return accrual_date < fields.Date.today()
 
     def has_valued_move_ids(self):
         return self.move_ids

addons/sale/tests/test_accrued_sale_orders.py
@@ -1,5 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from odoo import fields
 from odoo.exceptions import UserError
 from odoo.fields import Command
 from odoo.tests import freeze_time, tagged
@@ -73,16 +74,23 @@ class TestAccruedSaleOrders(TestSaleCommon):
             'active_ids': cls.sale_order.ids,
         }).create({
             'account_id': cls.account_expense.id,
+            'date': fields.Date.today(),
         })
 
     def test_accrued_order(self):
+        # self.wizard = self.wizard.with_context(accrual_entry_date=fields.Date.today())
+        self.wizard.date = fields.Date.today()
         # nothing to invoice : no entries to be created
         with self.assertRaises(UserError):
             self.wizard.create_entries()
 
         # 5 qty of each product invoiceable
         self.sale_order.order_line.qty_delivered = 5
-        self.assertRecordValues(self.env['account.move'].search(self.wizard.create_entries()['domain']).line_ids, [
+        # Call accrual wizard at today date because calling in the past will
+        # re-compute delivred and invoiced quantities for this date and thus
+        # generate nothing since there was no delivered quantity at this time.
+        account_move = self.env['account.move'].search(self.wizard.create_entries()['domain'])
+        self.assertRecordValues(account_move.line_ids, [
             # reverse move lines
             {'account_id': self.account_revenue.id, 'debit': 5000, 'credit': 0},
             {'account_id': self.alt_inc_account.id, 'debit': 1000, 'credit': 0},
@@ -95,10 +103,8 @@ class TestAccruedSaleOrders(TestSaleCommon):
 
         # delivered products invoiced, nothing to invoice left
         invoices = self.sale_order._create_invoices()
-        invoices.invoice_date = self.wizard.date
         invoices.action_post()
         with self.assertRaises(UserError):
-            self.env['account.move.line']._invalidate_cache()
             self.wizard.create_entries()
         self.assertTrue(self.wizard.display_amount)
 

addons/sale_mrp/models/sale_order_line.py
@@ -30,11 +30,11 @@ class SaleOrderLine(models.Model):
                 if components and components != [line.product_id.id]:
                     line.display_qty_widget = True
 
-    def _compute_qty_delivered(self):
-        super(SaleOrderLine, self)._compute_qty_delivered()
+    def _prepare_qty_delivered(self):
+        delivered_qties = super()._prepare_qty_delivered()
         for order_line in self:
             if order_line.qty_delivered_method == 'stock_move':
-                boms = order_line.move_ids.filtered(lambda m: m.state != 'cancel').mapped('bom_line_id.bom_id')
+                boms = order_line.move_ids.filtered(lambda m: m.state != 'cancel').bom_line_id.bom_id
                 dropship = any(m._is_dropshipped() for m in order_line.move_ids)
                 # We fetch the BoMs of type kits linked to the order_line,
                 # the we keep only the one related to the finished produst.
@@ -60,9 +60,9 @@ class SaleOrderLine(models.Model):
                                                  sum(sub_m.product_uom._compute_quantity(sub_m.quantity, m.product_uom) for sub_m in m.returned_move_ids if sub_m.state == 'done'),
                                                  precision_rounding=m.product_uom.rounding) > 0)
                                for m in moves) or not moves:
-                            order_line.qty_delivered = 0
+                            delivered_qties[order_line] = 0
                         else:
-                            order_line.qty_delivered = order_line.product_uom_qty
+                            delivered_qties[order_line] = order_line.product_uom_qty
                         continue
                     moves = order_line.move_ids.filtered(lambda m: m.state == 'done' and m.location_dest_usage != 'inventory')
                     filters = {
@@ -75,7 +75,7 @@ class SaleOrderLine(models.Model):
                     }
                     order_qty = order_line.product_uom_id._compute_quantity(order_line.product_uom_qty, relevant_bom.product_uom_id)
                     qty_delivered = moves._compute_kit_quantities(order_line.product_id, order_qty, relevant_bom, filters)
-                    order_line.qty_delivered += relevant_bom.product_uom_id._compute_quantity(qty_delivered, order_line.product_uom_id)
+                    delivered_qties[order_line] += relevant_bom.product_uom_id._compute_quantity(qty_delivered, order_line.product_uom_id)
 
                 # If no relevant BOM is found, fall back on the all-or-nothing policy. This happens
                 # when the product sold is made only of kits. In this case, the BOM of the stock moves
@@ -83,9 +83,10 @@ class SaleOrderLine(models.Model):
                 elif boms:
                     # if the move is ingoing, the product **sold** has delivered qty 0
                     if all(m.state == 'done' and m.location_dest_id.usage == 'customer' for m in order_line.move_ids):
-                        order_line.qty_delivered = order_line.product_uom_qty
+                        delivered_qties[order_line] = order_line.product_uom_qty
                     else:
-                        order_line.qty_delivered = 0.0
+                        delivered_qties[order_line] = 0.0
+        return delivered_qties
 
     def compute_uom_qty(self, new_qty, stock_move, rounding=True):
         #check if stock move concerns a kit

addons/sale_project/models/sale_order_line.py
@@ -78,13 +78,16 @@ class SaleOrderLine(models.Model):
         milestones_lines.qty_delivered_method = 'milestones'
         super(SaleOrderLine, self - milestones_lines)._compute_qty_delivered_method()
 
-    @api.depends('qty_delivered_method', 'product_uom_qty', 'reached_milestones_ids.quantity_percentage')
+    @api.depends('product_uom_qty', 'reached_milestones_ids.quantity_percentage')
     def _compute_qty_delivered(self):
+        super()._compute_qty_delivered()
+
+    def _prepare_qty_delivered(self):
         lines_by_milestones = self.filtered(lambda sol: sol.qty_delivered_method == 'milestones')
-        super(SaleOrderLine, self - lines_by_milestones)._compute_qty_delivered()
+        delivered_qties = super(SaleOrderLine, self - lines_by_milestones)._prepare_qty_delivered()
 
         if not lines_by_milestones:
-            return
+            return delivered_qties
 
         project_milestone_read_group = self.env['project.milestone']._read_group(
             [('sale_line_id', 'in', lines_by_milestones.ids), ('is_reached', '=', True)],
@@ -94,7 +97,8 @@ class SaleOrderLine(models.Model):
         reached_milestones_per_sol = {sale_line.id: percentage_sum for sale_line, percentage_sum in project_milestone_read_group}
         for line in lines_by_milestones:
             sol_id = line.id or line._origin.id
-            line.qty_delivered = reached_milestones_per_sol.get(sol_id, 0.0) * line.product_uom_qty
+            delivered_qties[line] = reached_milestones_per_sol.get(sol_id, 0.0) * line.product_uom_qty
+        return delivered_qties
 
     @api.depends('order_id.partner_id', 'product_id', 'order_id.project_id')
     def _compute_analytic_distribution(self):

addons/sale_stock/models/sale_order_line.py
@@ -196,6 +196,8 @@ class SaleOrderLine(models.Model):
     def _compute_qty_delivered(self):
         super(SaleOrderLine, self)._compute_qty_delivered()
 
+    def _prepare_qty_delivered(self):
+        delivered_qties = super()._prepare_qty_delivered()
         for line in self:  # TODO: maybe one day, this should be done in SQL for performance sake
             if line.qty_delivered_method == 'stock_move':
                 qty = 0.0
@@ -208,7 +210,8 @@ class SaleOrderLine(models.Model):
                     if move.state != 'done':
                         continue
                     qty -= move.product_uom._compute_quantity(move.quantity, line.product_uom_id, rounding_method='HALF-UP')
-                line.qty_delivered = qty
+                delivered_qties[line] = qty
+        return delivered_qties
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -311,7 +314,8 @@ class SaleOrderLine(models.Model):
                     triggering_rule_ids.append(move.rule_id.id)
                     seen_wh_ids.add(move.warehouse_id.id)
         if self.env.context.get('accrual_entry_date'):
-            moves = moves.filtered(lambda r: fields.Date.context_today(r, r.date) <= self.env.context['accrual_entry_date'])
+            accrual_date = fields.Date.from_string(self.env.context['accrual_entry_date'])
+            moves = moves.filtered(lambda r: fields.Date.context_today(r, r.date) <= accrual_date)
 
         for move in moves:
             if not move._is_dropshipped_returned() and (

addons/sale_timesheet/models/sale_order_line.py
@@ -70,11 +70,15 @@ class SaleOrderLine(models.Model):
     @api.depends('analytic_line_ids.project_id', 'project_id.pricing_type')
     def _compute_qty_delivered(self):
         super()._compute_qty_delivered()
+
+    def _prepare_qty_delivered(self):
+        delivered_qties = super()._prepare_qty_delivered()
         lines_by_timesheet = self.filtered(lambda sol: sol.qty_delivered_method == 'timesheet')
         domain = lines_by_timesheet._timesheet_compute_delivered_quantity_domain()
         mapping = lines_by_timesheet.sudo()._get_delivered_quantity_by_analytic(domain)
         for line in lines_by_timesheet:
-            line.qty_delivered = mapping.get(line.id or line._origin.id, 0.0)
+            delivered_qties[line] = mapping.get(line.id or line._origin.id, 0.0)
+        return delivered_qties
 
     def _timesheet_compute_delivered_quantity_domain(self):
         """ Hook for validated timesheet in addionnal module """

commit 94a3b172c0218814f3803c5bcc6e77703f7ebc92

[FIX] sale: keep order-policy services out of accrued revenue
Steps to reproduce
---
1. Set a service product's Invoicing Policy to "Ordered quantities".
2. Confirm a sale order for 10 units of that product.
3. Create and post a regular invoice for the ordered quantity.
4. Open the Accrued Revenue Entry wizard on that order at today's date.

Expected: nothing to accrue, the order is fully invoiced and its delivery is not tracked.
Actual: the wizard reports a negative amount and the line stays permanently in the "Invoiced Not Delivered" accrual report.

Issue
---
`_compute_amount_to_invoice_at_date` derives the amount from `qty_delivered_at_date - qty_invoiced_at_date`.

https://github.com/odoo/odoo/blob/d26fa077fdaa6baf942caf53d5eedc69e010415d/addons/sale/models/sale_order_line.py#L1234-L1237

For a line invoiced on ordered quantities whose delivery is never tracked (`qty_delivered_method == 'manual'`, i.e. a service), `qty_delivered_at_date` stays 0 while `qty_invoiced_at_date`
climbs to the ordered quantity. The difference is a permanent negative that no delivery can ever offset, so the line is reported as accruable forever even though it is fully invoiced and,
by policy, has nothing left to reconcile against delivery. This regressed in 81f25bc, which reworked the accrual computation to key solely on delivered quantity and dropped the
ordered-quantity case. When the line is invoiced on ordered quantities and delivery is untracked, anchoring the reference on `product_uom_qty` instead of `qty_delivered_at_date` makes
invoicing the order close the accrual, while delivery-tracked lines (goods via stock moves, timesheets, analytic) keep using the delivered quantity unchanged.

opw-6108887

addons/sale/models/sale_order_line.py
@@ -1229,11 +1229,17 @@ class SaleOrderLine(models.Model):
             price_unit *= self.product_id.uom_id.factor / self.product_uom_id.factor
         return price_unit
 
-    @api.depends('price_unit', 'discount', 'qty_invoiced_at_date', 'qty_delivered_at_date')
+    @api.depends('price_unit', 'discount', 'qty_invoiced_at_date', 'qty_delivered_at_date', 'product_uom_qty', 'qty_delivered_method')
     @api.depends_context('accrual_entry_date')
     def _compute_amount_to_invoice_at_date(self):
         for line in self:
-            qty_to_invoice = line.product_uom_id._compute_quantity(line.qty_delivered_at_date - line.qty_invoiced_at_date, line.product_id.uom_id)
+            if line.product_id.invoice_policy == "order" and line.qty_delivered_method == "manual":
+                qty_ref = line.product_uom_qty
+            else:
+                qty_ref = line.qty_delivered_at_date
+            qty_to_invoice = line.product_uom_id._compute_quantity(
+                qty_ref - line.qty_invoiced_at_date, line.product_id.uom_id,
+            )
             line.amount_to_invoice_at_date = qty_to_invoice * line._get_gross_price_unit()
 
     @api.depends('order_id.partner_id', 'product_id')

addons/sale/tests/test_accrued_sale_orders.py
@@ -200,3 +200,30 @@ class TestAccruedSaleOrders(TestSaleCommon):
             {'debit': 0.0, 'credit': 90.0},
             {'debit': 90.0, 'credit': 0.0},
         ])
+
+    def test_accrued_order_excludes_order_policy_line(self):
+        """A fully invoiced order billed on ordered quantities has nothing left
+        to accrue, so the Accrued Revenue Entry generates no move."""
+        self.product_a.invoice_policy = "order"
+        sale_order = self.env["sale.order"].with_context(tracking_disable=True).create({
+            "partner_id": self.partner_a.id,
+            "order_line": [
+                Command.create({"product_id": self.product_a.id, "product_uom_qty": 10.0}),
+            ],
+        })
+        sale_order.action_confirm()
+        sale_order._create_invoices()
+        sale_order.invoice_ids.action_post()
+        self.assertRecordValues(sale_order.order_line, [
+            {"qty_invoiced": 10.0, "qty_delivered": 0.0},
+        ])
+
+        wizard = self.env["account.accrued.orders.wizard"].with_context(
+            active_model="sale.order",
+            active_ids=sale_order.ids,
+        ).create({
+            "account_id": self.account_expense.id,
+            "date": fields.Date.today(),
+        })
+        with self.assertRaises(UserError):
+            wizard.create_entries()