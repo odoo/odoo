e87032d312518b0a12962e05bdcbbeb871b2983f
assk-odoo <assk@odoo.com>
2026-09-02 14:36:29 +0530

    [IMP] website,*: improve accessibility and tabindex sequence
    *: website_blog, website_event, website_event_exhibitor, website_sale
    website_event_track, website_hr_recruitment, website_sale_slides
    
    This commit adds:
    1.Add missing alt attributes to meaningful and decorative images.
    2.Add aria-label attributes to the search bar and scroll-down button.
    3.Define the carousel control order for screen reader users:
    - Previous slide button
    - Next slide button
    - Pause button
    - Slide indicator
    
    Task-6391348

--- addons/website/static/tests/interactions/snippets/gallery_slider.test.js
+++ addons/website/static/tests/interactions/snippets/gallery_slider.test.js
@@ -28,13 +28,17 @@ function getDefaultGallery(bsRide = false, bsInterval = 0, showPopup = false) {
                 <div class="o_container_small overflow-hidden">
                     <div id="slideshow_sample" class="carousel carousel-dark slide" data-bs-ride="${bsRide}" data-bs-interval="${bsInterval}">
                         <div class="o_carousel_controllers">
-                            <button class="o_carousel_pause o_snippet_not_selectable o_not_editable btn z-2" t-attf-aria-controls="slideshow_sample" title="Pause slides">
-                                <span class="o_carousel_pause_icon oi oi-filled" data-icon="pause" aria-hidden="true"/>
-                                <span class="visually-hidden">Pause slides</span>
-                            </button>
                             <button class="carousel-control-prev o_not_editable" contenteditable="false" data-bs-target="#slideshow_sample" data-bs-slide="prev" aria-label="Previous" title="Previous">
                                 <span class="carousel-control-prev-icon" aria-hidden="true"/>
                                 <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next o_not_editable" contenteditable="false" data-bs-target="#slideshow_sample" data-bs-slide="next" aria-label="Next" title="Next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"/>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                            <button class="o_carousel_pause o_snippet_not_selectable o_not_editable btn z-2" t-attf-aria-controls="slideshow_sample" title="Pause slides">
+                                <span class="o_carousel_pause_icon oi oi-filled" data-icon="pause" aria-hidden="true"/>
+                                <span class="visually-hidden">Pause slides</span>
                             </button>
                             <div class="carousel-indicators">
                                 <button type="button" data-bs-target="#slideshow_sample" data-bs-slide-to="0" class="active" aria-selected="true">
@@ -50,10 +54,6 @@ function getDefaultGallery(bsRide = false, bsInterval = 0, showPopup = false) {
                                     <img class="object-fit-cover w-100 h-100" aria-hidden="true" src="/web/image/website.landscape_md_1"/>
                                 </button>
                             </div>
-                            <button class="carousel-control-next o_not_editable" contenteditable="false" data-bs-target="#slideshow_sample" data-bs-slide="next" aria-label="Next" title="Next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"/>
-                                <span class="visually-hidden">Next</span>
-                            </button>
                         </div>
                         <div class="carousel-inner">
                             <div class="carousel-item active">
