44ca031f816bc7c70e99bfd4ca79cd0c3e294c18
ThanhDodeurOdoo <tso@odoo.com>
2026-09-02 19:02:23 +0200

    [FIX] discuss: use derived key for sfu
    derive channel-specific key from db secret and the channel id.

--- addons/mail/tests/discuss/test_rtc.py
+++ addons/mail/tests/discuss/test_rtc.py
@@ -4,12 +4,19 @@ from dateutil.relativedelta import relativedelta
 
 from odoo import fields
 from odoo.addons.mail.tests.common import MailCommon
+from odoo.addons.mail.tools import discuss
 from odoo.tests.common import tagged, users
 from odoo.tools import mute_logger
 
 
 @tagged("RTC", "post_install", "-at_install")
 class TestChannelRTC(MailCommon):
+
+    @users("employee")
+    def test_00_get_derived_sfu_key(self):
+        channel_key = discuss.get_derived_sfu_key(self.env, 42)
+        self.assertEqual(channel_key, discuss.get_derived_sfu_key(self.env, 42))
+        self.assertNotEqual(channel_key, discuss.get_derived_sfu_key(self.env, 43))
 
     @users('employee')
     @mute_logger('odoo.models.unlink')
