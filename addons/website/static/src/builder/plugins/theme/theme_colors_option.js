MoLeap/odooo

[IMP] website, html_builder: add theme colors palette preview
Steps to reproduce:
- Open the website builder.
- Go to Theme > Colors.
- Change theme colors.
- Try to evaluate the impact on UI elements.

Before this commit, it was hard to understand how theme colors affected
typical components while editing.

After this commit, users can open a dedicated colors preview dialog, and
theme CSS updates are reloaded in both the page iframe and the preview
iframe.

task-5960098

addons/website/static/src/builder/plugins/theme/theme_colors_option.js
@@ -1,25 +1,33 @@
-import { onMounted } from "@odoo/owl";
+import { onMounted, onWillUnmount } from "@odoo/owl";
 import { BaseOptionComponent } from "@html_builder/core/base_option_component";
 import { useDomState } from "@html_builder/core/utils";
 import { getCSSVariableValue } from "@html_editor/utils/formatting";
 import { _t } from "@web/core/l10n/translation";
+import { useBus } from "@web/core/utils/hooks";
+import { useState } from "@web/owl2/utils";
 
 export class ThemeColorsOption extends BaseOptionComponent {
     static template = "website.ThemeColorsOption";
     static dependencies = ["themeTab"];
     setup() {
         super.setup();
+        this.isMobileBeforeThemeColorsPreview = null;
         this.palettes = this.getPalettes();
         this.colorPresetToShow = this.env.colorPresetToShow;
         this.grays = this.dependencies.themeTab.getGrays();
         this.state = useDomState(() => ({
             presets: this.getPresets(),
         }));
+        this.websiteContext = useState(this.services.website.context);
+        useBus(this.services.website.bus, "CLOSE-THEME-COLORS-PREVIEW", () =>
+            this.closeThemeColorsPreview()
+        );
         onMounted(() => {
             this.iframeDocument = document.querySelector("iframe").contentWindow.document;
             this.state.presets = this.getPresets();
             this.colorPresetToShow = null;
         });
+        onWillUnmount(() => this.closeThemeColorsPreview());
     }
 
     getPalettes() {
@@ -83,5 +91,32 @@ export class ThemeColorsOption extends BaseOptionComponent {
             );
         }
         return getCSSVariableValue(color, this.iframeStyle);
+    }
+
+    get showThemeColorsPreview() {
+        return this.websiteContext.showThemeColorsPreview;
+    }
+
+    openThemeColorsPreview() {
+        const { context } = this.services.website;
+        if (context.showThemeColorsPreview) {
+            this.closeThemeColorsPreview();
+            return;
+        }
+        this.isMobileBeforeThemeColorsPreview = context.isMobile;
+        context.showThemeColorsPreview = true;
+        context.isMobile = false;
+    }
+
+    closeThemeColorsPreview() {
+        const { context } = this.services.website;
+        if (!context.showThemeColorsPreview) {
+            return;
+        }
+        context.showThemeColorsPreview = false;
+        if (this.isMobileBeforeThemeColorsPreview !== null) {
+            context.isMobile = this.isMobileBeforeThemeColorsPreview;
+            this.isMobileBeforeThemeColorsPreview = null;
+        }
     }
 }

odoo/odoo

addons/website/static/src/builder/plugins/theme/theme_colors_option.js
@@ -1,25 +1,32 @@
-import { onMounted } from "@odoo/owl";
+import { onMounted, onWillUnmount, proxy } from "@odoo/owl";
 import { BaseOptionComponent } from "@html_builder/core/base_option_component";
 import { useDomState } from "@html_builder/core/utils";
 import { getCSSVariableValue } from "@html_editor/utils/formatting";
 import { _t } from "@web/core/l10n/translation";
+import { useBus } from "@web/core/utils/hooks";
 
 export class ThemeColorsOption extends BaseOptionComponent {
     static template = "website.ThemeColorsOption";
     static dependencies = ["themeTab"];
     setup() {
         super.setup();
+        this.isMobileBeforeThemeColorsPreview = null;
         this.palettes = this.getPalettes();
         this.colorPresetToShow = this.env.colorPresetToShow;
         this.grays = this.dependencies.themeTab.getGrays();
         this.state = useDomState(() => ({
             presets: this.getPresets(),
         }));
+        this.websiteContext = proxy(this.services.website.context);
+        useBus(this.services.website.bus, "CLOSE-THEME-COLORS-PREVIEW", () =>
+            this.closeThemeColorsPreview()
+        );
         onMounted(() => {
             this.iframeDocument = document.querySelector("iframe").contentWindow.document;
             this.state.presets = this.getPresets();
             this.colorPresetToShow = null;
         });
+        onWillUnmount(() => this.closeThemeColorsPreview());
     }
 
     getPalettes() {
@@ -85,5 +92,32 @@ export class ThemeColorsOption extends BaseOptionComponent {
             );
         }
         return getCSSVariableValue(color, this.iframeStyle);
+    }
+
+    get showThemeColorsPreview() {
+        return this.websiteContext.showThemeColorsPreview;
+    }
+
+    openThemeColorsPreview() {
+        const { context } = this.services.website;
+        if (context.showThemeColorsPreview) {
+            this.closeThemeColorsPreview();
+            return;
+        }
+        this.isMobileBeforeThemeColorsPreview = context.isMobile;
+        context.showThemeColorsPreview = true;
+        context.isMobile = false;
+    }
+
+    closeThemeColorsPreview() {
+        const { context } = this.services.website;
+        if (!context.showThemeColorsPreview) {
+            return;
+        }
+        context.showThemeColorsPreview = false;
+        if (this.isMobileBeforeThemeColorsPreview !== null) {
+            context.isMobile = this.isMobileBeforeThemeColorsPreview;
+            this.isMobileBeforeThemeColorsPreview = null;
+        }
     }
 }