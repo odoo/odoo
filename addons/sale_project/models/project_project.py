LAIDOUDI33/LAID_ODOO

[MOV] *,sale_(margin,management): merge sale_margin module and related modules
       * = [pos_sale,pos_sale_margin,sale_expense,sale_expense_margin,sale_project,
           sale_project_margin,sale_stock_margin,sale_timesheet,
           sale_timesheet_margin]

Sales Margin was a separate, opt-in module: users had to enable a
dedicated `Margins` setting before margin/cost fields ever appeared
on quotations, sales orders, or related reports. In practice this
is a basic, low-overhead feature that most sales flows benefit from having
available by default.

This merges sale_margin into sale_management so that margin
tracking is a native part of Sales, with no separate module and
no settings toggle to turn on first. Any app that integrates with
Sales (POS, Expenses, Project, Stock, Timesheets) gets its margin
support merged into its own bridge with Sales the same way, instead
of depending on a standalone *_margin module.

Bridge modules that existed purely to connect sale_margin to
another app are merged into that app's primary Sales bridge module, since there
is no longer a separate margin module to bridge to:
  - pos_sale_margin       -> pos_sale
  - sale_expense_margin   -> sale_expense
  - sale_project_margin   -> sale_project
  - sale_timesheet_margin -> sale_timesheet

Per review, everything is folded into the existing target files
instead of new *_margin-suffixed ones: sale_order_margin.py and
sale_order_line_margin.py into sale_order.py/sale_order_line.py,
sale_margin_views.xml merged into sale_order_form_quote/sale_order_views.xml
(including the form view, previously kept as a separate inheriting
record), the report views file moved to report/, demo data merged
and renamed, and every bridge module's margin test class folded
directly into its existing test class rather than kept as a
separate class. sale_stock_margin is renamed to sale_management_stock
(sale_stock doesn't depend on sale_management, so it stays a
separate bridge module, just without the stale nam

addons/sale_project/models/project_project.py
@@ -50,6 +50,8 @@ class ProjectProject(models.Model):
     real_cost = fields.Monetary(compute='_compute_real_cost', export_string_translation=False)
     real_cost_ratio = fields.Float(compute='_compute_real_cost', export_string_translation=False)
     sale_warning_text = fields.Text('Project Warning', compute='_compute_sale_warning_text', help='Warning for the partner as set by the user.')
+    estimated_cost = fields.Monetary(compute='_compute_estimated_cost', export_string_translation=False)
+    estimated_cost_ratio = fields.Float(compute='_compute_estimated_cost', export_string_translation=False)
 
     @api.model
     def default_get(self, fields):
@@ -186,6 +188,13 @@ class ProjectProject(models.Model):
             project.real_cost = abs(cost_per_account.get(project.account_id, 0))
             total = project.real_cost + revenue_per_account.get(project.account_id, 0)
             project.real_cost_ratio = 100 * (project.real_cost / total) if total else 0
+
+    def _compute_estimated_cost(self):
+        for project in self:
+            all_sale_orders_lines = project._fetch_sale_order_items({'project.task': [('is_closed', '=', False)]})
+            total_sold = sum(all_sale_orders_lines.mapped('price_subtotal'))
+            project.estimated_cost = sum(all_sale_orders_lines.mapped(lambda line: line.purchase_price * line.product_uom_qty))
+            project.estimated_cost_ratio = 100 * (project.estimated_cost / total_sold) if total_sold else 0.0
 
     def _fetch_linked_products(self, project_ids, limit=None):
         return self.env["product.template"].search([
@@ -551,4 +560,23 @@ class ProjectProject(models.Model):
                 'pivot_column_groupby': ['date:month'],
             } if embedded_action_context else {}),
         }
+        return action
+
+    def action_estimated_margin(self):
+        self.ensure_one()
+        embedded_action_context = self.env.context.get('from_embedded_action', False)
+        action = self.env['ir.actions.act_window']._for_xml_id('sale_management.action_order_report_projected_margins')
+        all_sale_orders_lines = self._fetch_sale_order_items({'project.task': [('is_closed', '=', False)]})
+        action['display_name'] = self.env._("%(name)s's Forecast Margins", name=self.name)
+        action["domain"] = [("id", "in", all_sale_orders_lines.ids)]
+        action_context = {
+            **ast.literal_eval(action.get('context', '{}')),
+            'from_embedded_action': embedded_action_context,
+            'search_default_Customer': 0,
+        }
+        if embedded_action_context:
+            action_context.update({
+                'search_default_filter_order_date': 0,
+            })
+        action['context'] = action_context
         return action

odoo/odoo

addons/sale_project/models/project_project.py
@@ -50,6 +50,8 @@ class ProjectProject(models.Model):
     real_cost = fields.Monetary(compute='_compute_real_cost', export_string_translation=False)
     real_cost_ratio = fields.Float(compute='_compute_real_cost', export_string_translation=False)
     sale_warning_text = fields.Text('Project Warning', compute='_compute_sale_warning_text', help='Warning for the partner as set by the user.')
+    estimated_cost = fields.Monetary(compute='_compute_estimated_cost', export_string_translation=False)
+    estimated_cost_ratio = fields.Float(compute='_compute_estimated_cost', export_string_translation=False)
 
     @api.model
     def default_get(self, fields):
@@ -184,6 +186,13 @@ class ProjectProject(models.Model):
             project.real_cost = abs(cost_per_account.get(project.account_id, 0))
             total = project.real_cost + revenue_per_account.get(project.account_id, 0)
             project.real_cost_ratio = 100 * (project.real_cost / total) if total else 0
+
+    def _compute_estimated_cost(self):
+        for project in self:
+            all_sale_orders_lines = project._fetch_sale_order_items({'project.task': [('is_closed', '=', False)]})
+            total_sold = sum(all_sale_orders_lines.mapped('price_subtotal'))
+            project.estimated_cost = sum(all_sale_orders_lines.mapped(lambda line: line.purchase_price * line.product_uom_qty))
+            project.estimated_cost_ratio = 100 * (project.estimated_cost / total_sold) if total_sold else 0.0
 
     def _fetch_linked_products(self, project_ids, limit=None):
         return self.env["product.template"].search([
@@ -553,4 +562,23 @@ class ProjectProject(models.Model):
                 'pivot_column_groupby': ['date:month'],
             } if embedded_action_context else {}),
         }
+        return action
+
+    def action_estimated_margin(self):
+        self.ensure_one()
+        embedded_action_context = self.env.context.get('from_embedded_action', False)
+        action = self.env['ir.actions.act_window']._for_xml_id('sale_project.action_order_report_projected_margins')
+        all_sale_orders_lines = self._fetch_sale_order_items({'project.task': [('is_closed', '=', False)]})
+        action['display_name'] = self.env._("%(name)s's Forecast Margins", name=self.name)
+        action["domain"] = [("id", "in", all_sale_orders_lines.ids)]
+        action_context = {
+            **ast.literal_eval(action.get('context', '{}')),
+            'from_embedded_action': embedded_action_context,
+            'search_default_Customer': 0,
+        }
+        if embedded_action_context:
+            action_context.update({
+                'search_default_filter_order_date': 0,
+            })
+        action['context'] = action_context
         return action