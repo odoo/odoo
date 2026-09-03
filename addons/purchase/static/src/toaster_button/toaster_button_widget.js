6508e08913a5b76cb4404449d30a8d6fbd1fc9df
bhna-odoo <bhna@odoo.com>
2026-09-03 18:36:20 +0530

    [FIX] purchase: disable send reminder button while orm call is pending
    Currently, an error occurs when user clicks on the send reminder button multiple
    times.
    
    Steps to replicate:
    - Install purchase with demo, setup an outgoing mail server.
    - Open Purchase > Create a new purchase order, add a vendor and save.
    - Check `Ask confirmation` and click the `i` button 6-7 times.
    
    Error:
    ```
    psycopg2.errors.SerializationFailure: could not serialize access due to concurrent update
    ```
    
    Cause:
    - As the button was clicked multiple times, multiple ORM calls were triggered
      simultaneously. This caused multiple requests to access the same database row
      concurrently, resulting in the issue.
    
    Solution:
    - The button now remains disabled until the ORM call is completed, preventing
      multiple requests from being triggered simultaneously.
    
    sentry-6375128449

--- addons/purchase/static/src/toaster_button/toaster_button_widget.js
+++ addons/purchase/static/src/toaster_button/toaster_button_widget.js
@@ -1,6 +1,6 @@
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 import { standardWidgetProps } from "@web/views/widgets/standard_widget_props";
 
 class ButtonWithNotification extends Component {
@@ -13,14 +13,19 @@ class ButtonWithNotification extends Component {
     setup() {
         this.orm = useService("orm");
         this.notification = useService("notification");
+        this.state = useState({
+            isDisabled: false,
+        });
     }
 
     async onClick() {
+        this.state.isDisabled = true;
         const result = await this.orm.call(this.props.record.resModel, this.props.method, [
             this.props.record.resId,
         ]);
         const message = result.toast_message;
         this.notification.add(message, { type: "success" });
+        this.state.isDisabled = false;
     }
 }
 
