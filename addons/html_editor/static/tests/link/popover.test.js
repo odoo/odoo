c455310a7e2c86734a9d41045ac725c4fa2087be
Walid (wasa) <wasa@odoo.com>
2026-09-03 13:30:35 +0000

    [FIX] html_editor: do not move caret when clicking editable link child
    Problem:
    Clicking inside a navigation link in website builder causes the caret to jump to the start of the link element.
    
    Cause:
    `LinkPlugin` unconditionally reset the selection to the start of non-editable link elements, ignoring whether the anchor node was inside an editable child element.
    
    Solution:
    Do not reset selection if the anchor node is inside a `contenteditable` element.
    
    Steps to reproduce:
    - Open website builder.
    - Click inside a navbar link to place the caret.
    => Caret no longer jumps to the start of the link.
    
    opw-6535386
    
    closes odoo/odoo#287301
    
    X-original-commit: 157874aad3aebef5bc9268de6e17530641107e31
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>
    Signed-off-by: Walid Sahli (wasa) <wasa@odoo.com>

--- addons/html_editor/static/tests/link/popover.test.js
+++ addons/html_editor/static/tests/link/popover.test.js
@@ -2128,7 +2128,8 @@ test("Should should show link popover without edit", async () => {
 test("Should open link popover in read only mode when link is not editable", async () => {
     onRpc("/html_editor/link_preview_internal", () => ({}));
     onRpc("/link", () => ({}));
-    await setupEditor('<p><a contenteditable="false" href="/link">link</a></p>');
+    const { el } = await setupEditor('<p><a contenteditable="false" href="/link">link</a></p>');
+    setSelection({ anchorNode: el.querySelector("a"), anchorOffset: 1 });
     await click(queryOne(`a[contenteditable="false"]`));
     await waitFor(".o-we-linkpopover");
     expect(".o_we_edit_link").toHaveCount(0);
@@ -2153,4 +2154,18 @@ test("should hide title replace icon on popover for a link with image", async ()
     );
     await expectElementCount(".o-we-linkpopover", 1);
     expect(".o-we-linkpopover .o_we_replace_title_btn").toHaveCount(0);
+});
+
+test("Should change selection when clicking inside a contenteditable under non editable link", async () => {
+    onRpc("/html_editor/link_preview_internal", () => ({}));
+    onRpc("/link", () => ({}));
+    const { el } = await setupEditor(
+        '<p><a contenteditable="false" href="/link"><span contenteditable="true">abc</span></a></p>'
+    );
+    setSelection({ anchorNode: el.querySelector("span"), anchorOffset: 1 });
+    await click(queryOne(`a[contenteditable="false"]`));
+    await waitFor(".o-we-linkpopover");
+    expect(getContent(el)).toBe(
+        '<p><a contenteditable="false" href="/link"><span contenteditable="true">abc[]</span></a></p>'
+    );
 });
