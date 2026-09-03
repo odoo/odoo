12cc68c9bee4b77cc5e0c10217e46667e211efac
abd-msyukyu-odoo <abd@odoo.com>
2026-09-03 07:50:49 +0200

    wip

--- addons/mail/static/src/convert_inline/main/mso_strategy_plugin.js
+++ addons/mail/static/src/convert_inline/main/mso_strategy_plugin.js
@@ -6,8 +6,15 @@ export class MsoStrategyPlugin extends Plugin {
     static dependencies = [];
 
     // TODO EGGMAIL: replace background images with VML output
-    // border in MSO can not be bigger than 8px
     // handle image borders (as they are now allowed (radius for normal images and width for icons))
+    // TODO LIST:
+    // div border
+    // border-radius not supported for MSO
+    // border <= 8px for MSO
+    // background-image
+    // hybrid-fluid strategy => inline-block => wrapped in table for MSO
+    // buttons
+    // badges (all native inline-block elements)
 }
 
 registry.category("mail-html-conversion-main-plugins").add(MsoStrategyPlugin.id, MsoStrategyPlugin);
