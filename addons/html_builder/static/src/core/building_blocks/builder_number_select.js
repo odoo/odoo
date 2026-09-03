ee3ae540d92a8e7f6dee758b815e952fcd622d4d
Mehdi Outagant (MOU) <mou@odoo.com>
2026-09-03 19:20:08 +0200

    [IMP] website, *: add a BuilderNumberSelect for Border Radius options
    *: html_builder.
    
    This commit introduces a `BuilderNumberSelect` component that combines
    number input and select functionalities, making it easier to support
    Bootstrap’s built-in "rounded" utility classes alongside custom
    `border-radius` values.
    
    The corresponding Bootstrap `border-radius` variables can be configured
    from the Theme tab.
    
    task-3641683

---
+++ addons/html_builder/static/src/core/building_blocks/builder_number_select.js
@@ -0,0 +1,32 @@
+import { BuilderNumberInput } from "@html_builder/core/building_blocks/builder_number_input";
+import { Component, useProps, t } from "@odoo/owl";
+import { Dropdown } from "@web/core/dropdown/dropdown";
+import { BuilderComponent } from "./builder_component";
+import { WithIgnoreItem, builderSelectProps, useBuilderSelect } from "./builder_select";
+
+export class BuilderNumberSelect extends Component {
+    static components = {
+        Dropdown,
+        BuilderComponent,
+        WithIgnoreItem,
+        BuilderNumberInput,
+    };
+    static template = "html_builder.BuilderNumberSelect";
+
+    props = useProps({
+        ...builderSelectProps,
+        default: t.or([t.number(), t.literal(null)]).optional(0),
+        unit: t.string().optional(),
+        saveUnit: t.string().optional(),
+        step: t.number().optional(),
+        min: t.number().optional(),
+        max: t.number().optional(),
+        composable: t.boolean().optional(false),
+        applyWithUnit: t.boolean().optional(true),
+    });
+
+    setup() {
+        Object.assign(this, useBuilderSelect(this.props));
+        this.showNumberInput = true;
+    }
+}
