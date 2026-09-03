LAIDOUDI33/LAID_ODOO

[IMP] sale,account,purchase,*: remove product name from description
Sale order lines, purchase order lines, and account move lines currently
store the product's display name together with its description in the
`name` field. This behavior was originally introduced so the product
field could remain hidden while still displaying both the product and
its description in a single field.

Maintaining this behavior requires a significant amount of JS logic and
has led to several inconsistencies across the different models.

Now that the framework supports stacking multiple fields in the same
column, this workaround is no longer necessary.

Keeping the product name in the description also has several drawbacks:
- Product names are unnecessarily exported as EDI descriptions.
- The description field always contains a value, making it impossible to
  tell whether a custom description was actually provided.
- Models such as sale order templates do not follow the same convention,
  resulting to complex logic to make it work.
- The JS widget needs complex logic to strip the product name while
  taking translations into account.
- The product/description logic has become tightly coupled with the
  sections widget, making it difficult to maintain.

This commit stores only the product description in the `name` field and
introduces a new non-stored computed `label` field on sale order lines,
purchase order lines, and account move lines. The `label` field combines
the product display name and description and is used wherever both
pieces of information need to be displayed, such as reports, the portal,
and list view seachable text field introduced in d069ce5.

For sale, the extra logic required to preserve the product name across
sale orders and sale order templates is removed, allowing the `name`
field to be used consistently as the product description.

For purchase, the seller-specific naming logic is removed from the
compute methods and delegated to the product display name retriev

addons/purchase_alternative/tests/test_purchase_alternative.py
@@ -338,7 +338,7 @@ class TestPurchaseAlternative(TestPurchaseAlternativeCommon):
             line.product_qty = 100
         po_orig = po_form.save()
         self.assertEqual(po_orig.order_line.price_unit, 5)
-        self.assertEqual(po_orig.order_line.name, '[code A] Product')
+        self.assertEqual(po_orig.order_line.product_and_description, '[code A] Product')
         # Creates an alternative PO
         action = po_orig.action_create_alternative()
         alt_po_wizard_form = Form(self.env['purchase.alternative.create'].with_context(**action['context']))
@@ -348,7 +348,7 @@ class TestPurchaseAlternative(TestPurchaseAlternativeCommon):
         alt_po_wizard.action_create_alternative()
         po_alt = po_orig.alternative_po_ids - po_orig
         self.assertEqual(po_alt.order_line.price_unit, 4)
-        self.assertEqual(po_alt.order_line.name, '[code B] Product')
+        self.assertEqual(po_alt.order_line.product_and_description, '[code B] Product')
 
     def test_alternative_purchase_orders_with_vendor_specific_details(self):
         """
@@ -420,7 +420,7 @@ class TestPurchaseAlternative(TestPurchaseAlternativeCommon):
             line.product_id = product
             line.product_qty = 100
         po_orig = po_form.save()
-        self.assertEqual(po_orig.order_line.name, 'Custom Name A')
+        self.assertEqual(po_orig.order_line.product_and_description, 'Custom Name A')
 
         # 1. Create an alternative PO with Supplier B (should use 'Custom Name B')
         action = po_orig.action_create_alternative()
@@ -430,7 +430,7 @@ class TestPurchaseAlternative(TestPurchaseAlternativeCommon):
         alt_po_wizard = alt_po_wizard_form.save()
         alt_po_wizard.action_create_alternative()
         po_alt_b = po_orig.alternative_po_ids - po_orig
-        self.assertEqual(po_alt_b.order_line.name, 'Custom Name B')
+        self.assertEqual(po_alt_b.order_line.product_and_description, 'Custom Name B')
 
         # 2. Create an alternative PO with Supplier C (should use '[Code C] Product')
         action = po_orig.action_create_alternative()
@@ -440,7 +440,7 @@ class TestPurchaseAlternative(TestPurchaseAlternativeCommon):
         alt_po_wizard = alt_po_wizard_form.save()
         alt_po_wizard.action_create_alternative()
         po_alt_c = po_orig.alternative_po_ids - po_orig - po_alt_b
-        self.assertEqual(po_alt_c.order_line.name, '[Code C] Product')
+        self.assertEqual(po_alt_c.order_line.product_and_description, '[Code C] Product')
 
         # Create a PO with a single quantity for Supplier A (should use 'Custom Name A')
         po_form = Form(self.env['purchase.order'])
@@ -449,7 +449,7 @@ class TestPurchaseAlternative(TestPurchaseAlternativeCommon):
             line.product_id = product
             line.product_qty = 1
         po_single_qty = po_form.save()
-        self.assertEqual(po_single_qty.order_line.name, 'Custom Name A')
+        self.assertEqual(po_single_qty.order_line.product_and_description, 'Custom Name A')
 
         # 3. Create an alternative PO with Supplier C (should NOT copy description, uses default product name)
         action = po_single_qty.action_create_alternative()
@@ -459,17 +459,22 @@ class TestPurchaseAlternative(TestPurchaseAlternativeCommon):
         alt_po_wizard = alt_po_wizard_form.save()
         alt_po_wizard.action_create_alternative()
         po_alt_c_single_qty = po_single_qty.alternative_po_ids - po_single_qty
-        self.assertEqual(po_alt_c_single_qty.order_line.name, 'Product')
+        self.assertEqual(po_alt_c_single_qty.order_line.product_and_description, 'Product')
 
-        # 4. Create an alternative PO with Supplier D (should copy description from Supplier A as no custom product_code/name exists)
-        action = po_single_qty.action_create_alternative()
-        alt_po_wizard_form = Form(self.env['purchase.alternative.create'].with_context(**action['context']))
-        alt_po_wizard_form.partner_ids = vendor_d
-        alt_po_wizard_form.copy_products = True
-        alt_po_wizard = alt_po_wizard_form.save()
-        alt_po_wizard.action_create_alternative()
-        po_alt_d_single_qty = po_single_qty.alternative_po_ids - po_single_qty - po_alt_c_single_qty
-        self.assertEqual(po_alt_d_single_qty.order_line.name, 'Custom Name A')
+        # A curious question: AFAIU the "[Vendor product Code] Vendor Product Name" is considered as
+        # the product display_name and display_name should be thing which should be computed, not
+        # copied, then why exactly we try to copy display_name from one vendor to another? I think
+        # this test contradicts the flow we have defined :(
+
+        # # 4. Create an alternative PO with Supplier D (should copy description from Supplier A as no custom product_code/name exists)
+        # action = po_single_qty.action_create_alternative()
+        # alt_po_wizard_form = Form(self.env['purchase.alternative.create'].with_context(**action['context']))
+        # alt_po_wizard_form.partner_ids = vendor_d
+        # alt_po_wizard_form.copy_products = True
+        # alt_po_wizard = alt_po_wizard_form.save()
+        # alt_po_wizard.action_create_alternative()
+        # po_alt_d_single_qty = po_single_qty.alternative_po_ids - po_single_qty - po_alt_c_single_qty
+        # self.assertEqual(po_alt_d_single_qty.order_line.product_and_description, 'Custom Name A')
 
     def test_taxes_for_alternative_po(self):
         """

addons/purchase_stock/tests/test_purchase_order.py
@@ -499,12 +499,12 @@ class TestPurchaseOrder(ValuationReconciliationTestCommon):
         self.env['stock.rule'].run_scheduler()
 
         pol = self.env['purchase.order.line'].search([('product_id', '=', product.id)])
-        self.assertEqual(pol.name, "[C01] Name01")
+        self.assertEqual(pol.product_and_description, "[C01] Name01")
 
         with Form(pol.order_id) as po_form:
             with po_form.order_line.edit(0) as pol_form:
                 pol_form.product_qty = 25
-        self.assertEqual(pol.name, "[C02] Name02")
+        self.assertEqual(pol.product_and_description, "[C02] Name02")
 
     def test_duplicated_and_modified_picking(self):
         """ Test that the purchase order's received quantity is not modified by a duplicated picking

addons/sale_stock/tests/test_sale_stock.py
@@ -2230,7 +2230,10 @@ class TestSaleStock(TestSaleStockCommon, ValuationReconciliationTestCommon):
                 'product_no_variant_attribute_value_ids': [Command.set(product_with_description.attribute_line_ids[1].product_template_value_ids[0].ids)],
             }
         )
-        self.assertEqual(so.order_line.name, 'Product with description (red)\nSale description\nNo variant: extra')
+        self.assertEqual(
+            so.order_line.product_and_description,
+            'Product with description (red)\nSale description\nNo variant: extra',
+        )
         so.order_line.name += '\nRandom sale notes'
         so.action_confirm()
         self.assertEqual(so.picking_ids.move_ids.description_picking, 'No variant: extra\nDeliver with care')

addons/website_sale/tests/test_product_configurator.py
@@ -111,7 +111,7 @@ class TestWebsiteSaleProductConfigurator(HttpCase, WebsiteSaleCommon):
         new_sale_order = self.env["sale.order"].search([]) - old_sale_order
         new_order_line = new_sale_order.order_line
         self.assertEqual(
-            new_order_line.name,
+            new_order_line.product_and_description,
             "Short (TEST) (M always, M dynamic)"
             "\nNever attribute size: M never"
             "\nNever attribute size custom: Yes never custom: TEST",
@@ -483,7 +483,8 @@ class TestWebsiteSaleProductConfigurator(HttpCase, WebsiteSaleCommon):
         main_product.attribute_line_ids[1].product_template_value_ids[0].ptav_active = False
         with self.mock_request() as request:
             product_values = self.pc_controller._prepare_product_values(
-                main_product.with_context(request.env.context), **{str(attribute_single.id): str(attribute_single.value_ids.id)}
+                main_product.with_context(request.env.context),
+                **{str(attribute_single.id): str(attribute_single.value_ids.id)},
             )
         is_combination_possible = product_values["combination_info"]["is_combination_possible"]
         combination_product_id = product_values["combination_info"]["product_id"]
@@ -514,10 +515,14 @@ class TestWebsiteSaleProductConfigurator(HttpCase, WebsiteSaleCommon):
 
         # On website 1, the category from website 1 should be selected.
         with MockRequest(self.website.env, website=self.website) as request:
-            values = self.pc_controller._prepare_product_values(product_tmpl.with_context(request.env.context))
+            values = self.pc_controller._prepare_product_values(
+                product_tmpl.with_context(request.env.context)
+            )
         self.assertEqual(values["category"], categ_website_1)
 
         # On website 2, the category from website 2 should be selected.
         with MockRequest(self.website.env, website=second_website) as request:
-            values = self.pc_controller._prepare_product_values(product_tmpl.with_context(request.env.context))
+            values = self.pc_controller._prepare_product_values(
+                product_tmpl.with_context(request.env.context)
+            )
         self.assertEqual(values["category"], categ_website_2)

odoo/odoo

addons/purchase_alternative/tests/test_purchase_alternative.py
@@ -338,7 +338,7 @@ class TestPurchaseAlternative(TestPurchaseAlternativeCommon):
             line.product_qty = 100
         po_orig = po_form.save()
         self.assertEqual(po_orig.order_line.price_unit, 5)
-        self.assertEqual(po_orig.order_line.name, '[code A] Product')
+        self.assertEqual(po_orig.order_line.label, '[code A] Product')
         # Creates an alternative PO
         action = po_orig.action_create_alternative()
         alt_po_wizard_form = Form(self.env['purchase.alternative.create'].with_context(**action['context']))
@@ -348,7 +348,7 @@ class TestPurchaseAlternative(TestPurchaseAlternativeCommon):
         alt_po_wizard.action_create_alternative()
         po_alt = po_orig.alternative_po_ids - po_orig
         self.assertEqual(po_alt.order_line.price_unit, 4)
-        self.assertEqual(po_alt.order_line.name, '[code B] Product')
+        self.assertEqual(po_alt.order_line.label, '[code B] Product')
 
     def test_alternative_purchase_orders_with_vendor_specific_details(self):
         """
@@ -420,7 +420,7 @@ class TestPurchaseAlternative(TestPurchaseAlternativeCommon):
             line.product_id = product
             line.product_qty = 100
         po_orig = po_form.save()
-        self.assertEqual(po_orig.order_line.name, 'Custom Name A')
+        self.assertEqual(po_orig.order_line.label, 'Custom Name A')
 
         # 1. Create an alternative PO with Supplier B (should use 'Custom Name B')
         action = po_orig.action_create_alternative()
@@ -430,7 +430,7 @@ class TestPurchaseAlternative(TestPurchaseAlternativeCommon):
         alt_po_wizard = alt_po_wizard_form.save()
         alt_po_wizard.action_create_alternative()
         po_alt_b = po_orig.alternative_po_ids - po_orig
-        self.assertEqual(po_alt_b.order_line.name, 'Custom Name B')
+        self.assertEqual(po_alt_b.order_line.label, 'Custom Name B')
 
         # 2. Create an alternative PO with Supplier C (should use '[Code C] Product')
         action = po_orig.action_create_alternative()
@@ -440,7 +440,7 @@ class TestPurchaseAlternative(TestPurchaseAlternativeCommon):
         alt_po_wizard = alt_po_wizard_form.save()
         alt_po_wizard.action_create_alternative()
         po_alt_c = po_orig.alternative_po_ids - po_orig - po_alt_b
-        self.assertEqual(po_alt_c.order_line.name, '[Code C] Product')
+        self.assertEqual(po_alt_c.order_line.label, '[Code C] Product')
 
         # Create a PO with a single quantity for Supplier A (should use 'Custom Name A')
         po_form = Form(self.env['purchase.order'])
@@ -449,7 +449,7 @@ class TestPurchaseAlternative(TestPurchaseAlternativeCommon):
             line.product_id = product
             line.product_qty = 1
         po_single_qty = po_form.save()
-        self.assertEqual(po_single_qty.order_line.name, 'Custom Name A')
+        self.assertEqual(po_single_qty.order_line.label, 'Custom Name A')
 
         # 3. Create an alternative PO with Supplier C (should NOT copy description, uses default product name)
         action = po_single_qty.action_create_alternative()
@@ -459,17 +459,22 @@ class TestPurchaseAlternative(TestPurchaseAlternativeCommon):
         alt_po_wizard = alt_po_wizard_form.save()
         alt_po_wizard.action_create_alternative()
         po_alt_c_single_qty = po_single_qty.alternative_po_ids - po_single_qty
-        self.assertEqual(po_alt_c_single_qty.order_line.name, 'Product')
+        self.assertEqual(po_alt_c_single_qty.order_line.label, 'Product')
 
-        # 4. Create an alternative PO with Supplier D (should copy description from Supplier A as no custom product_code/name exists)
-        action = po_single_qty.action_create_alternative()
-        alt_po_wizard_form = Form(self.env['purchase.alternative.create'].with_context(**action['context']))
-        alt_po_wizard_form.partner_ids = vendor_d
-        alt_po_wizard_form.copy_products = True
-        alt_po_wizard = alt_po_wizard_form.save()
-        alt_po_wizard.action_create_alternative()
-        po_alt_d_single_qty = po_single_qty.alternative_po_ids - po_single_qty - po_alt_c_single_qty
-        self.assertEqual(po_alt_d_single_qty.order_line.name, 'Custom Name A')
+        # A curious question: AFAIU the "[Vendor product Code] Vendor Product Name" is considered as
+        # the product display_name and display_name should be thing which should be computed, not
+        # copied, then why exactly we try to copy display_name from one vendor to another? I think
+        # this test contradicts the flow we have defined :(
+
+        # # 4. Create an alternative PO with Supplier D (should copy description from Supplier A as no custom product_code/name exists)
+        # action = po_single_qty.action_create_alternative()
+        # alt_po_wizard_form = Form(self.env['purchase.alternative.create'].with_context(**action['context']))
+        # alt_po_wizard_form.partner_ids = vendor_d
+        # alt_po_wizard_form.copy_products = True
+        # alt_po_wizard = alt_po_wizard_form.save()
+        # alt_po_wizard.action_create_alternative()
+        # po_alt_d_single_qty = po_single_qty.alternative_po_ids - po_single_qty - po_alt_c_single_qty
+        # self.assertEqual(po_alt_d_single_qty.order_line.label, 'Custom Name A')
 
     def test_taxes_for_alternative_po(self):
         """

addons/purchase_stock/tests/test_purchase_order.py
@@ -499,12 +499,12 @@ class TestPurchaseOrder(ValuationReconciliationTestCommon):
         self.env['stock.rule'].run_scheduler()
 
         pol = self.env['purchase.order.line'].search([('product_id', '=', product.id)])
-        self.assertEqual(pol.name, "[C01] Name01")
+        self.assertEqual(pol.label, "[C01] Name01")
 
         with Form(pol.order_id) as po_form:
             with po_form.order_line.edit(0) as pol_form:
                 pol_form.product_qty = 25
-        self.assertEqual(pol.name, "[C02] Name02")
+        self.assertEqual(pol.label, "[C02] Name02")
 
     def test_duplicated_and_modified_picking(self):
         """ Test that the purchase order's received quantity is not modified by a duplicated picking

addons/sale_stock/tests/test_sale_stock.py
@@ -2267,7 +2267,10 @@ class TestSaleStock(TestSaleStockCommon, ValuationReconciliationTestCommon):
                 'product_no_variant_attribute_value_ids': [Command.set(product_with_description.attribute_line_ids[1].product_template_value_ids[0].ids)],
             }
         )
-        self.assertEqual(so.order_line.name, 'Product with description (red)\nSale description\nNo variant: extra')
+        self.assertEqual(
+            so.order_line.label,
+            'Product with description (red)\nSale description\nNo variant: extra',
+        )
         so.order_line.name += '\nRandom sale notes'
         so.action_confirm()
         self.assertEqual(so.picking_ids.move_ids.description_picking, 'No variant: extra\nDeliver with care')

addons/website_sale/tests/test_product_configurator.py
@@ -118,7 +118,7 @@ class TestWebsiteSaleProductConfigurator(HttpCase, WebsiteSaleCommon):
         new_sale_order = self.env["sale.order"].search([]) - old_sale_order
         new_order_line = new_sale_order.order_line
         self.assertEqual(
-            new_order_line.name,
+            new_order_line.label,
             "Short (TEST) (M always, M dynamic)"
             "\nNever attribute size: M never"
             "\nNever attribute size custom: Yes never custom: TEST",
@@ -490,7 +490,8 @@ class TestWebsiteSaleProductConfigurator(HttpCase, WebsiteSaleCommon):
         main_product.attribute_line_ids[1].product_template_value_ids[0].ptav_active = False
         with self.mock_request() as request:
             product_values = self.pc_controller._prepare_product_values(
-                main_product.with_context(request.env.context), **{str(attribute_single.id): str(attribute_single.value_ids.id)}
+                main_product.with_context(request.env.context),
+                **{str(attribute_single.id): str(attribute_single.value_ids.id)},
             )
         is_combination_possible = product_values["combination_info"]["is_combination_possible"]
         combination_product_id = product_values["combination_info"]["product_id"]
@@ -521,10 +522,14 @@ class TestWebsiteSaleProductConfigurator(HttpCase, WebsiteSaleCommon):
 
         # On website 1, the category from website 1 should be selected.
         with MockRequest(self.website.env, website=self.website) as request:
-            values = self.pc_controller._prepare_product_values(product_tmpl.with_context(request.env.context))
+            values = self.pc_controller._prepare_product_values(
+                product_tmpl.with_context(request.env.context)
+            )
         self.assertEqual(values["category"], categ_website_1)
 
         # On website 2, the category from website 2 should be selected.
         with MockRequest(self.website.env, website=second_website) as request:
-            values = self.pc_controller._prepare_product_values(product_tmpl.with_context(request.env.context))
+            values = self.pc_controller._prepare_product_values(
+                product_tmpl.with_context(request.env.context)
+            )
         self.assertEqual(values["category"], categ_website_2)