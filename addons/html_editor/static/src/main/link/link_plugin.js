a8ad38e5b02f0e96fa5fe093495c34b8e2219989
Walid (wasa) <wasa@odoo.com>
2026-09-03 13:30:35 +0000

    [FIX] html_editor: do not move caret when clicking editable link child
    Problem:
    Clicking inside a navigation link in website builder causes the caret to jump to the start of the link element.
    
    Cause:
    `LinkPlugin` unconditionally reset the selection to the start of non-editable link elements, ignoring whether the anchor node was inside an editable child element.
    
    Solution:
    Do not reset selection if the anchor node is inside a `contenteditable` element.
    
    Steps to reproduce:
    - Open website builder.
    - Click inside a navbar link to place the caret.
    => Caret no longer jumps to the start of the link.
    
    opw-6535386
    
    X-original-commit: 9ee4f13f3eeb52c14ae5c78f80433fdabd34d882

--- addons/html_editor/static/src/main/link/link_plugin.js
+++ addons/html_editor/static/src/main/link/link_plugin.js
@@ -7,6 +7,7 @@ import { LinkPopover } from "./link_popover";
 import { DIRECTIONS, leftPos, nodeSize, rightPos } from "@html_editor/utils/position";
 import { EMAIL_REGEX, URL_REGEX, cleanZWChars, deduceURLfromText } from "./utils";
 import {
+    isContentEditable,
     isElement,
     isPhrasingContent,
     isProtected,
@@ -332,9 +333,13 @@ export class LinkPlugin extends Plugin {
         this.addDomListener(this.editable, "click", (ev) => {
             const linkEl = ev.target.closest("a");
             if (linkEl) {
+                const selection = this.dependencies.selection.getEditableSelection();
+                const clickedInsideNonEditableLink =
+                    !linkEl.isContentEditable &&
+                    !isContentEditable(closestElement(selection.anchorNode));
                 if (ev.ctrlKey || ev.metaKey) {
                     window.open(linkEl.href, "_blank");
-                } else if (!linkEl.isContentEditable) {
+                } else if (clickedInsideNonEditableLink) {
                     this.dependencies.selection.setSelection({
                         anchorNode: linkEl,
                         anchorOffset: 0,
