LAIDOUDI33/LAID_ODOO

[IMP] sale,account,purchase,*: remove product name from description
Sale order lines, purchase order lines, and account move lines currently
store the product's display name together with its description in the
`name` field. This behavior was originally introduced so the product
field could remain hidden while still displaying both the product and
its description in a single field.

Maintaining this behavior requires a significant amount of JS logic and
has led to several inconsistencies across the different models.

Now that the framework supports stacking multiple fields in the same
column, this workaround is no longer necessary.

Keeping the product name in the description also has several drawbacks:
- Product names are unnecessarily exported as EDI descriptions.
- The description field always contains a value, making it impossible to
  tell whether a custom description was actually provided.
- Models such as sale order templates do not follow the same convention,
  resulting to complex logic to make it work.
- The JS widget needs complex logic to strip the product name while
  taking translations into account.
- The product/description logic has become tightly coupled with the
  sections widget, making it difficult to maintain.

This commit stores only the product description in the `name` field and
introduces a new non-stored computed `label` field on sale order lines,
purchase order lines, and account move lines. The `label` field combines
the product display name and description and is used wherever both
pieces of information need to be displayed, such as reports, the portal,
and list view seachable text field introduced in d069ce5.

For sale, the extra logic required to preserve the product name across
sale orders and sale order templates is removed, allowing the `name`
field to be used consistently as the product description.

For purchase, the seller-specific naming logic is removed from the
compute methods and delegated to the product display name retriev

addons/account/models/account_move_line.py
@@ -120,7 +120,11 @@ class AccountMoveLine(models.Model):
         compute='_compute_name', store=True, readonly=False, precompute=True,
         tracking=True,
     )
-    translated_product_name = fields.Text(compute='_compute_translated_product_name')
+    product_and_description = fields.Text(
+        string="Product & Description",
+        compute="_compute_product_and_description",
+        inverse="_set_description",
+    )
     debit = fields.Monetary(
         string='Debit',
         compute='_compute_debit_credit', inverse='_inverse_debit', store=True, precompute=True,
@@ -646,14 +650,10 @@ class AccountMoveLine(models.Model):
             if not product:
                 return False
 
-            if line.journal_id.type == 'sale':
-                values.append(product.display_name)
-                if product.description_sale:
-                    values.append(product.description_sale)
-            elif line.journal_id.type == 'purchase':
-                values.append(product.display_name)
-                if product.description_purchase:
-                    values.append(product.description_purchase)
+            if line.journal_id.type == 'sale' and product.description_sale:
+                values.append(product.description_sale)
+            elif line.journal_id.type == 'purchase' and product.description_purchase:
+                values.append(product.description_purchase)
             return '\n'.join(values) if values else False
 
         term_by_move = (self.move_id.line_ids | self).filtered(lambda l: l.display_type == 'payment_term').sorted(lambda l: l.date_maturity or date.max).grouped('move_id')
@@ -682,12 +682,24 @@ class AccountMoveLine(models.Model):
             if not line.name or line._origin.name == get_name(line._origin) or line.product_id != line._origin.product_id:
                 line.name = get_name(line)
 
-    @api.depends('product_id')
-    def _compute_translated_product_name(self):
+    @api.depends("product_id", "name")
+    def _compute_product_and_description(self):
         for line in self:
-            line.translated_product_name = line.product_id.with_context(
-                lang=line.partner_id.lang,
-            ).display_name
+            if line.product_id and line.name:
+                line.product_and_description = line.product_id.display_name + "\n" + line.name
+            elif line.product_id and not line.name:
+                line.product_and_description = line.product_id.display_name
+            else:
+                line.product_and_description = line.name
+
+    def _set_description(self):
+        for line in self:
+            if line.product_id:
+                line.name = line.product_and_description.removeprefix(
+                    line.product_id.display_name
+                ).removeprefix("\n")
+            else:
+                line.name = line.product_and_description
 
     def _compute_account_id(self):
         term_lines = self.filtered(lambda line: line.display_type == 'payment_term')
@@ -3805,9 +3817,6 @@ class AccountMoveLine(models.Model):
         }
         return res
 
-    def _get_journal_items_full_name(self, name, display_name):
-        return name if not display_name or display_name in name else f"{display_name}\n{name}"
-
     def _check_edi_line_tax_required(self):
         return self.product_id.type != 'combo'
 
@@ -3848,7 +3857,7 @@ class AccountMoveLine(models.Model):
         section_subtotal = sum(l.price_subtotal for l in children_lines)
         section_total = sum(l.price_total for l in children_lines)
         result = [{
-            'name': self.name,
+            'product_and_description': self.product_and_description,
             'product': False,
             'taxes': [tax.tax_label for tax in children_lines.tax_ids if tax.tax_label] if not self.collapse_prices else [],
             'price_subtotal': section_subtotal,
@@ -3864,7 +3873,7 @@ class AccountMoveLine(models.Model):
 
         for line in direct_children_lines:
             result.append({
-                'name': line.name,
+                'product_and_description': line.product_and_description,
                 'product': line.product_id,
                 'taxes': [tax.tax_label for tax in line.tax_ids if tax.tax_label],
                 'price_subtotal': line.price_subtotal,
@@ -3887,7 +3896,7 @@ class AccountMoveLine(models.Model):
                     continue
                 if subsection_line.collapse_composition:
                     result.append({
-                        'name': subsection_line.name,
+                        'product_and_description': subsection_line.product_and_description,
                         'product': False,
                         'taxes': tax_labels,
                         'price_subtotal': subtotal,
@@ -3901,7 +3910,7 @@ class AccountMoveLine(models.Model):
                 else:
                     for line in subsection_line | lines_for_tax_group:
                         result.append({
-                            'name': line.name,
+                            'product_and_description': line.product_and_description,
                             'product': line.product_id,
                             'taxes': tax_labels if (line == subsection_line and not self.collapse_prices) or (line != subsection_line and self.collapse_prices) else [],
                             'price_subtotal': subtotal if line == subsection_line else line.price_subtotal,
@@ -3913,7 +3922,7 @@ class AccountMoveLine(models.Model):
                             'discount': line.discount,
                         })
         return result or [{
-            'name': self.name,
+            'product_and_description': self.product_and_description,
             'taxes': [],
             'price_subtotal': 0.0,
             'price_total': 0.0,

odoo/odoo

addons/account/models/account_move_line.py
@@ -116,11 +116,11 @@ class AccountMoveLine(models.Model):
     # TODO: move the search method on the `account_id` field when it's possible to add a search on a stored field
     search_account_id = fields.Many2one('account.account', search='_search_account_id', store=False)
     name = fields.Text(
-        string='Label',
+        string='Description',
         compute='_compute_name', store=True, readonly=False, precompute=True,
         tracking=True,
     )
-    translated_product_name = fields.Text(compute='_compute_translated_product_name')
+    label = fields.Text(string="Label", compute="_compute_label", inverse="_inverse_label")
     debit = fields.Monetary(
         string='Debit',
         compute='_compute_debit_credit', inverse='_inverse_debit', store=True, precompute=True,
@@ -640,23 +640,17 @@ class AccountMoveLine(models.Model):
     def _compute_name(self):
         def get_name(line):
             values = []
-            if line.move_id.partner_id.lang:
-                product = line.product_id.with_context(lang=line.move_id.partner_id.lang)
-            elif line.partner_id.lang:
-                product = line.product_id.with_context(lang=line.partner_id.lang)
+            if lang := line.move_id._get_lang():
+                product = line.product_id.with_context(lang=lang)
             else:
                 product = line.product_id
             if not product:
                 return False
 
-            if line.journal_id.type == 'sale':
-                values.append(product.display_name)
-                if product.description_sale:
-                    values.append(product.description_sale)
-            elif line.journal_id.type == 'purchase':
-                values.append(product.display_name)
-                if product.description_purchase:
-                    values.append(product.description_purchase)
+            if line.journal_id.type == 'sale' and product.description_sale:
+                values.append(product.description_sale)
+            elif line.journal_id.type == 'purchase' and product.description_purchase:
+                values.append(product.description_purchase)
             return '\n'.join(values) if values else False
 
         term_by_move = (self.move_id.line_ids | self).filtered(lambda l: l.display_type == 'payment_term').sorted(lambda l: l.date_maturity or date.max).grouped('move_id')
@@ -685,12 +679,30 @@ class AccountMoveLine(models.Model):
             if not line.name or line._origin.name == get_name(line._origin) or line.product_id != line._origin.product_id:
                 line.name = get_name(line)
 
-    @api.depends('product_id')
-    def _compute_translated_product_name(self):
+    @api.depends_context("display_default_code")
+    @api.depends("product_id", "name")
+    def _compute_label(self):
         for line in self:
-            line.translated_product_name = line.product_id.with_context(
-                lang=line.partner_id.lang,
-            ).display_name
+            lang_line = line.with_context(
+                lang=line.move_id._get_lang(),
+                display_default_code=self.env.context.get("display_default_code", True),
+            )
+            if lang_line.product_id and lang_line.name:
+                line.label = lang_line.product_id.display_name + "\n" + lang_line.name
+            elif lang_line.product_id and not lang_line.name:
+                line.label = lang_line.product_id.display_name
+            else:
+                line.label = lang_line.name
+
+    def _inverse_label(self):
+        for line in self:
+            if line.product_id and line.label:
+                lang_line = line.with_context(lang=line.move_id._get_lang())
+                line.name = lang_line.label.removeprefix(
+                    lang_line.product_id.display_name
+                ).removeprefix("\n")
+            else:
+                line.name = line.label
 
     def _compute_account_id(self):
         term_lines = self.filtered(lambda line: line.display_type == 'payment_term')
@@ -3809,9 +3821,6 @@ class AccountMoveLine(models.Model):
         }
         return res
 
-    def _get_journal_items_full_name(self, name, display_name):
-        return name if not display_name or display_name in name else f"{display_name}\n{name}"
-
     def _check_edi_line_tax_required(self):
         return self.product_id.type != 'combo'
 
@@ -3852,7 +3861,7 @@ class AccountMoveLine(models.Model):
         section_subtotal = sum(l.price_subtotal for l in children_lines)
         section_total = sum(l.price_total for l in children_lines)
         result = [{
-            'name': self.name,
+            'label': self.label,
             'product': False,
             'taxes': [tax.tax_label for tax in children_lines.tax_ids if tax.tax_label] if not self.collapse_prices else [],
             'price_subtotal': section_subtotal,
@@ -3868,7 +3877,7 @@ class AccountMoveLine(models.Model):
 
         for line in direct_children_lines:
             result.append({
-                'name': line.name,
+                'label': line.label,
                 'product': line.product_id,
                 'taxes': [tax.tax_label for tax in line.tax_ids if tax.tax_label],
                 'price_subtotal': line.price_subtotal,
@@ -3891,7 +3900,7 @@ class AccountMoveLine(models.Model):
                     continue
                 if subsection_line.collapse_composition:
                     result.append({
-                        'name': subsection_line.name,
+                        'label': subsection_line.label,
                         'product': False,
                         'taxes': tax_labels,
                         'price_subtotal': subtotal,
@@ -3905,7 +3914,7 @@ class AccountMoveLine(models.Model):
                 else:
                     for line in subsection_line | lines_for_tax_group:
                         result.append({
-                            'name': line.name,
+                            'label': line.label,
                             'product': line.product_id,
                             'taxes': tax_labels if (line == subsection_line and not self.collapse_prices) or (line != subsection_line and self.collapse_prices) else [],
                             'price_subtotal': subtotal if line == subsection_line else line.price_subtotal,
@@ -3917,7 +3926,7 @@ class AccountMoveLine(models.Model):
                             'discount': line.discount,
                         })
         return result or [{
-            'name': self.name,
+            'label': self.label,
             'taxes': [],
             'price_subtotal': 0.0,
             'price_total': 0.0,