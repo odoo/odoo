90c4d062953da8b593a7bb28652b8f866470ab54
Merlin Guillaume <megu@odoo.com>
2026-09-04 17:15:48 +0200

    [FIX] sale_management: save during sortDrop
    When we save a sale order just after reordering its sale order lines,
    there's a chance that an error is thrown
    
    Steps to reproduce:
    1. Install Sales
    2. Go to Sales and open any quotation that has multiple sale order lines
    3. Reorder a sale order line and immediately save (there's more chance
        to reproduce if you have a lot of sol and if you use the shortcut
        `ALT + S`)
    4. An error is thrown
    
    Issue:
    It is possible that the save takes place during the execution of
    super.sortDrop. This reassigns the id of all the records in
    this.props.list.records
    Therefore, calling `_handleQuantityAdjustment` with the recordMap
    computed before the save uses an id that has disappeard from
    this.props.list.records so we cannot find it at
    https://github.com/odoo/odoo/blob/4973903252865a6a7a2da235bc3a01675dbbff4d/addons/sale_management/static/src/fields/sale_order_line_field/sale_order_line_field.js#L263
    which eventually throws an error
    
    Solution:
    Suspend any save mechanism when sortDrop starts and resume it when
    sortDrop has finished
    
    opw-6483206

--- addons/sale_management/static/src/fields/sale_order_line_field/sale_order_line_field.js
+++ addons/sale_management/static/src/fields/sale_order_line_field/sale_order_line_field.js
@@ -2,6 +2,7 @@ import { getSectionRecords } from '@account/components/section_and_note_fields_b
 import { SaleOrderLineListRenderer } from '@sale/js/sale_order_line_field/sale_order_line_field';
 import { makeContext } from '@web/core/context';
 import { x2ManyCommands } from '@web/core/orm_service';
+import { useBus } from "@web/core/utils/hooks";
 import { patch } from '@web/core/utils/patch';
 
 patch(SaleOrderLineListRenderer.prototype, {
@@ -9,6 +10,11 @@ patch(SaleOrderLineListRenderer.prototype, {
     setup() {
         super.setup();
         this.copyFields.push('is_optional');
+        this.sortDropProm = Promise.resolve();
+        // Ensure save waits for any pending sortDrop operation to complete.
+        useBus(this.props.list.model.bus, "NEED_LOCAL_CHANGES", ({ detail }) => {
+            detail.proms.push(this.sortDropProm);
+        });
     },
 
     /**
@@ -174,14 +180,22 @@ patch(SaleOrderLineListRenderer.prototype, {
      */
     async sortDrop(dataRowId, dataGroupId, { element, previous }) {
         const record = this.props.list.records.find(r => r.id === dataRowId);
-        // Prevent the record from being abandoned when leaveEditMode or sortDrop is called
-        record.dirty = true;
-        await this.props.list.leaveEditMode();
-        const recordMap = this._getRecordsToRecompute(record, previous ? previous.dataset.id : null);
 
-        await super.sortDrop(dataRowId, dataGroupId, { element, previous });
-
-        await this._handleQuantityAdjustment(recordMap);
+        // Keep the promise on the component so save() can wait for it to resolve.
+        // Otherwise, the record could be saved before the quantity adjustment is applied,
+        // resulting in inconsistent record values.
+        this.sortDropProm = (async () => {
+            // Prevent the record from being abandoned when leaveEditMode or sortDrop is called
+            record.dirty = true;
+            await this.props.list.leaveEditMode();
+            const recordMap = this._getRecordsToRecompute(
+                record,
+                previous ? previous.dataset.id : null
+            );
+            await super.sortDrop(dataRowId, dataGroupId, { element, previous });
+            await this._handleQuantityAdjustment(recordMap);
+        })();
+        await this.sortDropProm;
     },
 
     /**
