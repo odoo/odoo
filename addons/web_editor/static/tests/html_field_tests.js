LAIDOUDI33/LAID_ODOO

[FIX] web_editor: save content typed on a virtual keyboard
On a virtual keyboard the editor handles typing as a composition and only commits its history step on a later interaction. The html field marks itself dirty from that step, so right after typing the field is not dirty. On save the form calls commitChanges, but it only writes the value when the field is dirty, or on an urgent or inline-style save. None of those hold for a plain sanitized html field, so the typed content is not written to the record.

Finalize the pending composition in commitChanges by calling the editor _compositionStep() before that check. commitChanges is the single point every save path goes through, so committing the pending step there marks the field dirty and the content is saved. It does nothing on a physical keyboard, where the composition flag is cleared after each keystroke.

Steps to reproduce:
1. Open the Sales app and a quotation.
2. In the chatter, click Activities then Schedule Activity.
3. On a phone, or a browser with a virtual keyboard, fill Summary, tap the note field and type a note.
4. Tap Schedule while the note field still has focus.
5. Open the created activity.

=> the note is empty

Ticket [link](https://www.odoo.com/odoo/project.task/6331719)
opw-6331719

addons/web_editor/static/tests/html_field_tests.js
@@ -1185,6 +1185,47 @@ QUnit.module("WebEditor.HtmlField", ({ beforeEach }) => {
 
     });
 
+    QUnit.test("content typed on a virtual keyboard is saved", async (assert) => {
+        serverData.models.partner.records.push({
+            id: 1,
+            txt: "<p><br></p>",
+        });
+        let htmlField;
+        const wysiwygPromise = makeDeferred();
+        patchWithCleanup(HtmlField.prototype, {
+            async startWysiwyg() {
+                await super.startWysiwyg(...arguments);
+                htmlField = this;
+                wysiwygPromise.resolve();
+            }
+        });
+        await makeView({
+            type: "form",
+            resId: 1,
+            resModel: "partner",
+            serverData,
+            arch: `
+                <form>
+                    <field name="txt" widget="html"/>
+                </form>`,
+        });
+        await wysiwygPromise;
+        const editor = htmlField.wysiwyg.odooEditor;
+        editor.testMode = true;
+
+        const paragraph = editor.editable.querySelector("p");
+        paragraph.replaceChildren(document.createTextNode("mobile note"));
+        setSelection(paragraph.firstChild, "mobile note".length);
+        editor.keyboardType = "VIRTUAL";
+        editor._onInput({ inputType: "insertText", data: "e", isTrusted: false });
+        editor.observerFlush();
+        assert.strictEqual(htmlField._isDirty(), false, "the pending composition leaves the field not dirty");
+
+        await htmlField.commitChanges({ shouldInline: true });
+        assert.strictEqual(htmlField.props.record.data.txt.toString(), "<p>mobile note</p>",
+            "the note typed on a virtual keyboard is saved");
+    });
+
     QUnit.module("Image transform");
 
     QUnit.test("Image transform should reset after click twice", async (assert) => {

odoo/odoo

X-original-commit: 8ee301c3f78c7acebbd2accd9031c7323250f966

addons/web_editor/static/tests/html_field_tests.js
@@ -1116,6 +1116,46 @@ QUnit.module("WebEditor.HtmlField", ({ beforeEach }) => {
 
     });
 
+    QUnit.test("content typed on a virtual keyboard is saved", async (assert) => {
+        serverData.models.partner.records.push({
+            id: 1,
+            txt: "<p><br></p>",
+        });
+        let htmlField;
+        const wysiwygPromise = makeDeferred();
+        patchWithCleanup(HtmlField.prototype, {
+            async startWysiwyg() {
+                await super.startWysiwyg(...arguments);
+                htmlField = this;
+                wysiwygPromise.resolve();
+            }
+        });
+        await makeView({
+            type: "form",
+            resId: 1,
+            resModel: "partner",
+            serverData,
+            arch: `
+                <form>
+                    <field name="txt" widget="html_legacy"/>
+                </form>`,
+        });
+        await wysiwygPromise;
+        const editor = htmlField.wysiwyg.odooEditor;
+        editor.testMode = true;
+
+        const paragraph = editor.editable.querySelector("p");
+        paragraph.replaceChildren(document.createTextNode("mobile note"));
+        setSelection(paragraph.firstChild, "mobile note".length);
+        editor.keyboardType = "VIRTUAL";
+        editor._onInput({ inputType: "insertText", data: "e", isTrusted: false });
+        editor.observerFlush();
+
+        await htmlField.commitChanges({ shouldInline: true });
+        assert.strictEqual(htmlField.props.record.data.txt.toString(), "<p>mobile note</p>",
+            "the note typed on a virtual keyboard is saved");
+    });
+
     QUnit.module("Image transform");
 
     QUnit.test("Image transform should reset after click twice", async (assert) => {