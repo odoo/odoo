7761c4eec05070500b6f6d5dbf8d6627b8ed411c
Ester Andreetto <esand@odoo.com>
2026-09-04 10:39:21 +0200

    [FIX] l10n_ar: add error message while changing ARCA Responsibility Type
    Issue before this commit:
    While trying to change the ARCA Responsibility Type field for an
    argentinian company from the settings you receive an error if there
    already exists some entries while if you try to do it from the contacts
    app you suceed even tho it's not correct.
    
    Steps to reproduce the issue:
    1) Download Contacts and l10n_ar
    2) Switch to (AR)Exento company (or any other AR company)
    3) Be sure that there is at least one account.move.line present
    4) Go to Settings > Manage companies > (AR)Exento
    5) Try to change ARCA Responsibility Type field to something else and
    save. You will receive the error: Invalid Operation — You cannot change
    the ARCA responsibility of this company because it already has
    accounting entries.
    6) Go to contacts and search (AR)Exento and try to change the field
    again. You can succeed here
    
    Cause of the issue:
    https://github.com/odoo/odoo/blob/7281a0c5f19a0ac94a5455b35e51601a4a5b9927/addons/l10n_ar/models/res_company.py#L38-L44
    The control is only on the model res.company but actually, the field:
    https://github.com/odoo/odoo/blob/7281a0c5f19a0ac94a5455b35e51601a4a5b9927/addons/l10n_ar/models/res_company.py#L15-L16
    is related to the one in the model res.partner:
    https://github.com/odoo/odoo/blob/7281a0c5f19a0ac94a5455b35e51601a4a5b9927/addons/l10n_ar/models/res_partner.py#L25-L28
    
    Reason to introduce the fix:
    The goal is to align the behaviours in both models.
    
    opw-6519256

--- addons/l10n_ar/tests/test_partner.py
+++ addons/l10n_ar/tests/test_partner.py
@@ -1,4 +1,4 @@
-from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError, ValidationError
 from odoo.tests import tagged
 from . import common
 
@@ -9,3 +9,57 @@ class TestResPartner(common.TestArCommon):
     def test_l10n_ar_cuit_number(self):
         with self.assertRaisesRegex(ValidationError, 'Invalid length for "CUIT"'):
             self.partner_ri.vat = "BE0477472701"
+
+    def test_prevent_arca_responsibility_change(self):
+        """ Test that changing the ARCA responsibility is blocked for a company
+        if accounting entries already exist, but allowed for standard partners."""
+
+        invoice = self._create_invoice_ar(
+            partner_id=self.res_partner_adhoc,
+            company_id=self.company_ri,
+        )
+        self._post(invoice)
+        self.assertTrue(self.company_ri._existing_accounting())
+
+        new_responsibility = self.env.ref("l10n_ar.res_IVAE")
+
+        # Try to change responsibility type for the company partner, should raise error
+        with self.assertRaisesRegex(UserError, 'Could not change the ARCA Responsibility'):
+            self.partner_ri.l10n_ar_afip_responsibility_type_id = new_responsibility
+
+        # Try to change responsibility type via the company itself, should raise error
+        with self.assertRaisesRegex(UserError, 'Could not change the ARCA Responsibility'):
+            self.company_ri.l10n_ar_afip_responsibility_type_id = new_responsibility
+
+        # Try to change responsibility type for a normal partner, should succeed
+        self.res_partner_adhoc.l10n_ar_afip_responsibility_type_id = new_responsibility
+        self.assertEqual(self.res_partner_adhoc.l10n_ar_afip_responsibility_type_id, new_responsibility)
+
+    def test_arca_responsibility_change_branch_without_entries(self):
+        """ Test that a branch company with no accounting entries can change its ARCA
+        responsibility type even if the parent company already has accounting entries."""
+
+        branch = self.company_ri.create({
+            'name': 'Branch RI',
+            'parent_id': self.company_ri.id,
+            'country_id': self.company_ri.country_id.id,
+        })
+
+        invoice = self._create_invoice_ar(
+            partner_id=self.res_partner_adhoc,
+            company_id=self.company_ri,
+        )
+        self._post(invoice)
+
+        # Parent has accounting entries, branch does not
+        self.assertTrue(self.company_ri._existing_accounting())
+        self.assertFalse(branch._existing_accounting())
+
+        # Branch can change its ARCA responsibility type
+        new_responsibility = self.env.ref("l10n_ar.res_IVAE")
+        branch.partner_id.l10n_ar_afip_responsibility_type_id = new_responsibility
+        self.assertEqual(branch.l10n_ar_afip_responsibility_type_id, new_responsibility)
+
+        # Parent cannot change its ARCA responsibility type
+        with self.assertRaisesRegex(UserError, 'Could not change the ARCA Responsibility'):
+            self.partner_ri.l10n_ar_afip_responsibility_type_id = new_responsibility
