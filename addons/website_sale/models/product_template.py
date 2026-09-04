LAIDOUDI33/LAID_ODOO

[IMP] website_sale(_stock), sale: Out of stock improvements
**Purpose:**
Out of stock was not completely managed in eCommerce.

The out of stock ribbon was not reflecting correctly the status of the partially out of stock products.
The notification and wishlists buttons were inconsistents with the rest of the features.

**Specification:**
Shop page:
- Make the out of stock ribbon appear only if all variants are out of stock.
- If a product if completely out of stock, hide the add to cart button.
- If a variant is still available, select the first available variant in
the configurator or when clicking on the tile to open the product page.

Product page:
- Make the notification button replace the Add to cart button.
- Rationalise the wishlist button of out of stock products.
- When clicking notify me, show form to enter email if not logged in, subscribe directly if logged in

Task-6026756

addons/website_sale/models/product_template.py
@@ -994,11 +994,18 @@ class ProductTemplate(models.Model):
                 "has_stock_notification": has_stock_notification,
                 "stock_notification_email": stock_notification_email,
                 "is_in_wishlist": product_sudo._is_in_wishlist(),
+                "is_sold_out": (
+                    not product_sudo.allow_out_of_stock_order and (free_qty - cart_quantity) < 1
+                ),
             })
             if self.env["res.groups"]._is_feature_enabled("uom.group_uom"):
                 combination_info["uom_name"] = uom.name
         else:
-            combination_info.update({"free_qty": 0, "cart_qty": 0})
+            combination_info.update({
+                "free_qty": 0,
+                "cart_qty": 0,
+                "is_sold_out": not product_or_template.allow_out_of_stock_order,
+            })
 
         return combination_info
 
@@ -1558,8 +1565,7 @@ class ProductTemplate(models.Model):
         :returns: the ribbon to display, if there is one.
         :rtype: `product.ribbon` recordset
         """
-        variant = variant or self.product_variant_id
-        ribbon = variant.sudo().variant_ribbon_id or self.sudo().website_ribbon_id
+        ribbon = (variant and variant.sudo().variant_ribbon_id) or self.sudo().website_ribbon_id
         if not ribbon:
             # The None check ensures that we do not recompute the ribbons when no ribbons were
             # previously found.
@@ -1568,6 +1574,11 @@ class ProductTemplate(models.Model):
                 auto_assign_ribbons = self.env["product.ribbon"].search_fetch([
                     ("assign", "!=", "manual")
                 ])
+
+            if auto_assign_ribbons:
+                variant = variant or self._get_variant_for_combination(
+                    self._get_first_possible_combination()
+                )
             for rb in auto_assign_ribbons:
                 if rb._is_applicable_for(variant, price_vals):
                     return rb
@@ -1661,20 +1672,45 @@ class ProductTemplate(models.Model):
             return self._get_available_uoms()[:1] or self.uom_id
         return super()._get_main_uom()
 
+    def _get_first_possible_combination(self, necessary_values=None):
+        """Override of `product` to return the first combination that has stock."""
+        if self.env.context.get("website_id"):
+            combinations = self._get_possible_combinations(necessary_values)
+            return next(
+                filter(self._is_combination_in_stock, combinations),
+                super()._get_first_possible_combination(necessary_values),
+            )
+        return super()._get_first_possible_combination(necessary_values)
+
+    def _is_combination_in_stock(self, combination):
+        """Check that the given combination is not sold out.
+
+        :param recordset combination: the product combination to check
+        :returns whether there is stock for the combination
+        :rtype: boolean
+        """
+        try:
+            variant = self._get_variant_for_combination(combination)
+            return variant and not variant._is_sold_out()
+        except ValueError:
+            return False
+
     def _is_sold_out(self):
         """Return whether the product is sold out (no available quantity).
 
         If a product inventory is not tracked, or if it's allowed to be sold regardless
         of availabilities, the product is never considered sold out.
 
-        Note: only checks the availability of the first variant of the template.
+        Note: checks the availability of all variants of the template.
 
         :return: whether the product can still be sold
         :rtype: bool
         """
         if not self.is_storable or self.allow_out_of_stock_order:
             return False
-        return not self.product_variant_id or self.product_variant_id._is_sold_out()
+        return not self.product_variant_ids or all(
+            variant._is_sold_out() for variant in self.product_variant_ids
+        )
 
     @api.model
     def _get_additional_configurator_data(

addons/website_sale/static/src/interactions/product_page.js
@@ -8,7 +8,7 @@ import { memoize, uniqueId } from '@web/core/utils/functions';
 import { KeepLast } from '@web/core/utils/concurrency';
 import { setElementContent, createElementWithContent } from '@web/core/utils/html';
 import { insertThousandsSep, formatFloat } from '@web/core/utils/numbers';
-import { renderToElement, renderToFragment } from '@web/core/utils/render';
+import { renderToFragment } from '@web/core/utils/render';
 import { isEmail } from '@web/core/utils/strings';
 import { throttleForAnimation } from '@web/core/utils/timing';
 import { htmlEscape, markup } from '@odoo/owl';
@@ -789,9 +789,7 @@ export class ProductPage extends Interaction {
 
         const addQtyInput = parent.querySelector('input[name="add_qty"]');
         const qty = parseFloat(addQtyInput?.value) || 1;
-        const ctaWrapper = parent.querySelector('#o_wsale_cta_wrapper');
-        ctaWrapper.classList.replace('d-none', 'd-flex');
-        ctaWrapper.classList.remove('out_of_stock');
+        this._toggleOutOfStockButtons(parent, combination, true)
 
         if (!combination.allow_out_of_stock_order) {
             const unavailableQty = await this.waitFor(this._getUnavailableQty(combination));
@@ -806,8 +804,7 @@ export class ProductPage extends Interaction {
                 }
             }
             if (combination.free_qty < 1 && !combination.prevent_sale) {
-                ctaWrapper.classList.replace('d-flex', 'd-none');
-                ctaWrapper.classList.add('out_of_stock');
+                this._toggleOutOfStockButtons(parent, combination, false)
             }
         }
 
@@ -819,8 +816,7 @@ export class ProductPage extends Interaction {
                 }
             }
             if (combination.max_combo_quantity < 1 && !combination.prevent_sale) {
-                ctaWrapper.classList.replace('d-flex', 'd-none');
-                ctaWrapper.classList.add('out_of_stock');
+                this._toggleOutOfStockButtons(parent, combination, false)
             }
         }
 
@@ -834,9 +830,9 @@ export class ProductPage extends Interaction {
             }
         }
 
-        this.el.querySelectorAll(
-            '.availability_message_' + combination.product_template
-        ).forEach(el => el.remove());
+        document.querySelector('.oe_website_sale')
+            .querySelectorAll('#product_stock_availability')
+            .forEach(el => el.remove());
         if (combination.out_of_stock_message) {
             const outOfStockMessage = document.createElement('div');
             setElementContent(outOfStockMessage, combination.out_of_stock_message);
@@ -845,17 +841,22 @@ export class ProductPage extends Interaction {
         this.el.querySelector('div.availability_messages').append(renderToFragment(
             'website_sale.product_availability', combination
         ));
-        if (this.el.querySelector('.o_add_wishlist_dyn')) {
-            const messageEl = this.el.querySelector('div.availability_messages');
-            if (messageEl && !this.el.querySelector('#stock_wishlist_message')) {
-                this.services['public.interactions'].stopInteractions(messageEl);
-                messageEl.append(
-                    renderToElement('website_sale.product_availability_wishlist', combination)
-                    || ''
-                );
-                this.services['public.interactions'].startInteractions(messageEl);
-            }
-        }
+    }
+
+    _toggleOutOfStockButtons(parent, combination, in_stock) {
+        const addToCart = parent.querySelector('#add_to_cart_wrap');
+        const ctaWrapper = parent.querySelector('#o_wsale_cta_wrapper');
+        const outOfStockWrapper = ctaWrapper.querySelector('#out_of_stock_buttons_wrapper');
+        const notifyMeButton = ctaWrapper.querySelector('#product_stock_notification_message');
+        const subscribedButton = ctaWrapper.querySelector('#stock_notification_success_message');
+
+        addToCart.classList.toggle('d-inline-flex', in_stock);
+        addToCart.classList.toggle('d-none', !in_stock);
+        ctaWrapper.classList.toggle('out_of_stock', !in_stock);
+        outOfStockWrapper?.classList.toggle('d-flex', !in_stock);
+        outOfStockWrapper?.classList.toggle('d-none', in_stock);
+        notifyMeButton?.classList.toggle('d-none', in_stock || combination.has_stock_notification);
+        subscribedButton?.classList.toggle('d-none', in_stock || !combination.has_stock_notification);
     }
 
     async _getUnavailableQty(combination) {
@@ -926,8 +927,14 @@ export class ProductPage extends Interaction {
         const partnerEmail = document.querySelector('#wsale_user_email').value;
         const emailInputEl = document.querySelector('#stock_notification_input');
 
-        emailInputEl.value = partnerEmail;
-        this._handleClickStockNotificationMessage(ev);
+        if (partnerEmail) {
+            emailInputEl.value = partnerEmail;
+            this.onClickSubmitProductStockNotificationForm(ev);
+        } else {
+            const notifyMeButton = document.querySelector('#product_stock_notification_message')
+            notifyMeButton.disabled=true
+            this._handleClickStockNotificationMessage(ev);
+        }
     }
 
     onClickSubmitProductStockNotificationForm(ev) {
@@ -936,14 +943,17 @@ export class ProductPage extends Interaction {
     }
 
     _handleClickStockNotificationMessage(ev) {
-        ev.currentTarget.classList.add('d-none');
-        ev.currentTarget.parentElement.querySelector('#stock_notification_form').classList.remove('d-none');
+        const ctaSection = ev.currentTarget.closest("#o_wsale_product_cta_section");
+        ctaSection
+            .querySelector("#stock_notification_div_wrapper")
+            .classList.replace("d-none", "d-flex");
     }
 
     async _handleClickSubmitStockNotificationForm(ev, productId) {
-        const stockNotificationEl = ev.currentTarget.closest('#stock_notification_div');
-        const formEl = stockNotificationEl.querySelector('#stock_notification_form');
-        const email = stockNotificationEl.querySelector('#stock_notification_input').value.trim();
+        const ctaSection = ev.currentTarget.closest("#o_wsale_product_cta_section");
+        const stockNotificationEl = ctaSection.querySelector("#stock_notification_div_wrapper");
+        const notifyEl = ctaSection.querySelector("#product_stock_notification_message");
+        const email = stockNotificationEl.querySelector("#stock_notification_input").value.trim();
 
         if (!isEmail(email)) {
             return this._displayErrorMessage(_t('Invalid email'), stockNotificationEl);
@@ -960,9 +970,10 @@ export class ProductPage extends Interaction {
             }
             throw error;
         }
-        const message = stockNotificationEl.querySelector('#stock_notification_success_message');
-        message.classList.remove('d-none');
-        formEl.classList.add('d-none');
+        const message = ctaSection.querySelector("#stock_notification_success_message");
+        stockNotificationEl.classList.replace("d-flex", "d-none");
+        message.classList.remove("d-none");
+        notifyEl.classList.add("d-none");
     }
 
     _displayErrorMessage(message, stockNotificationEl) {

addons/website_sale/static/src/js/product_configurator_dialog/product_configurator_dialog.js
@@ -32,7 +32,16 @@ patch(ProductConfiguratorDialog.prototype, {
         useSubEnv({
             isFrontend: this.props.isFrontend,
             isMainProductConfigurable: this.props.options?.isMainProductConfigurable ?? true,
+            isQuantityAllowed: this._isQuantityAllowed.bind(this),
         });
+    },
+
+    async _setQuantity(productTmplId, quantity) {
+        const product = this._findProduct(productTmplId);
+        if (!this._isQuantityAllowed(product, quantity)) {
+            quantity = product.free_qty;
+        }
+        return super._setQuantity(productTmplId, quantity)
     },
 
     /**
@@ -41,7 +50,7 @@ patch(ProductConfiguratorDialog.prototype, {
      * @return {Boolean} - Whether all selected products can be sold.
      */
     canBeSold() {
-        return this.state.products.every(p => p.can_be_sold);
+        return this.state.products.every(p => p.can_be_sold && this._isQuantityAllowed(p, p.quantity));
     },
 
     /**
@@ -69,6 +78,19 @@ patch(ProductConfiguratorDialog.prototype, {
             return _t("Total: %s", this.getFormattedTotal());
         }
         return super.totalMessage(...arguments);
+    },
+
+    /**
+     * Check whether the provided product quantity can be added to the cart.
+     *
+     * @param {Object} product - The provided product.
+     * @param {Number} quantity - The new quantity of the product.
+     * @return {Boolean} - Whether the provided product quantity can be added to the cart.
+     */
+    _isQuantityAllowed(product, quantity) {
+        // `product` may be a props object, which always exposes every declared key (including the
+        // optional `free_qty`), so the presence of the key can't be used to detect stock tracking.
+        return product.free_qty === undefined || product.free_qty >= quantity;
     },
 
 });

odoo/odoo

addons/website_sale/models/product_template.py
@@ -807,7 +807,7 @@ class ProductTemplate(models.Model):
         uom = self.env["uom.uom"].browse(uom_id) or self._get_main_uom()
 
         if not product_id and not combination and not only_template:
-            combination = self._get_first_possible_combination()
+            combination = self._get_first_available_combination()
 
         if only_template:
             product = self.env["product.product"]
@@ -1043,11 +1043,18 @@ class ProductTemplate(models.Model):
                 "has_stock_notification": has_stock_notification,
                 "stock_notification_email": stock_notification_email,
                 "is_in_wishlist": product_sudo._is_in_wishlist(),
+                "is_sold_out": (
+                    not product_sudo.allow_out_of_stock_order and (free_qty - cart_quantity) < 1
+                ),
             })
             if self.env["res.groups"]._is_feature_enabled("uom.group_uom"):
                 combination_info["uom_name"] = uom.name
         else:
-            combination_info.update({"free_qty": 0, "cart_qty": 0})
+            combination_info.update({
+                "free_qty": 0,
+                "cart_qty": 0,
+                "is_sold_out": not product_or_template.allow_out_of_stock_order,
+            })
 
         return combination_info
 
@@ -1681,8 +1688,7 @@ class ProductTemplate(models.Model):
         :returns: the ribbon to display, if there is one.
         :rtype: `product.ribbon` recordset
         """
-        variant = variant or self.product_variant_id
-        ribbon = variant.sudo().variant_ribbon_id or self.sudo().website_ribbon_id
+        ribbon = (variant and variant.sudo().variant_ribbon_id) or self.sudo().website_ribbon_id
         if not ribbon:
             # The None check ensures that we do not recompute the ribbons when no ribbons were
             # previously found.
@@ -1691,6 +1697,11 @@ class ProductTemplate(models.Model):
                 auto_assign_ribbons = self.env["product.ribbon"].search_fetch([
                     ("assign", "!=", "manual")
                 ])
+
+            if auto_assign_ribbons:
+                variant = variant or self._get_variant_for_combination(
+                    self._get_first_available_combination()
+                )
             for rb in auto_assign_ribbons:
                 if rb._is_applicable_for(variant, price_vals):
                     return rb
@@ -1784,20 +1795,45 @@ class ProductTemplate(models.Model):
             return self._get_available_uoms()[:1] or self.uom_id
         return super()._get_main_uom()
 
+    def _get_first_available_combination(self, necessary_values=None):
+        """Override of `product` to return the first combination that has stock."""
+        if self.env.context.get("website_id"):
+            combinations = self._get_possible_combinations(necessary_values)
+            return next(
+                filter(self._is_combination_in_stock, combinations),
+                super()._get_first_available_combination(necessary_values),
+            )
+        return super()._get_first_available_combination(necessary_values)
+
+    def _is_combination_in_stock(self, combination):
+        """Check that the given combination is not sold out.
+
+        :param recordset combination: the product combination to check
+        :returns whether there is stock for the combination
+        :rtype: boolean
+        """
+        try:
+            variant = self._get_variant_for_combination(combination)
+            return variant and not variant._is_sold_out()
+        except ValueError:
+            return False
+
     def _is_sold_out(self):
         """Return whether the product is sold out (no available quantity).
 
         If a product inventory is not tracked, or if it's allowed to be sold regardless
         of availabilities, the product is never considered sold out.
 
-        Note: only checks the availability of the first variant of the template.
+        Note: checks the availability of all variants of the template.
 
         :return: whether the product can still be sold
         :rtype: bool
         """
         if not self.is_storable or self.allow_out_of_stock_order:
             return False
-        return not self.product_variant_id or self.product_variant_id._is_sold_out()
+        return not self.product_variant_ids or all(
+            variant._is_sold_out() for variant in self.product_variant_ids
+        )
 
     @api.model
     def _get_additional_configurator_data(

addons/website_sale/static/src/interactions/product_page.js
@@ -8,7 +8,7 @@ import { memoize, uniqueId } from '@web/core/utils/functions';
 import { KeepLast } from '@web/core/utils/concurrency';
 import { setElementContent, createElementWithContent } from '@web/core/utils/html';
 import { insertThousandsSep, formatFloat } from '@web/core/utils/numbers';
-import { renderToElement, renderToFragment } from '@web/core/utils/render';
+import { renderToFragment } from '@web/core/utils/render';
 import { isEmail } from '@web/core/utils/strings';
 import { throttleForAnimation } from '@web/core/utils/timing';
 import { htmlEscape, markup, usePlugin } from '@odoo/owl';
@@ -38,10 +38,10 @@ export class ProductPage extends Interaction {
             "t-on-mouseleave": this.onMouseLeavePackagingButton,
             "t-on-click": this.onMouseLeavePackagingButton,
         },
-        "#product_stock_notification_message": {
+        "#stock_notification_button": {
             "t-on-click": this.onClickProductStockNotificationMessage.bind(this),
         },
-        "#product_stock_notification_form_submit_button": {
+        "#stock_notification_submit_button": {
             "t-on-click": this.onClickSubmitProductStockNotificationForm.bind(this),
         },
         "button[name='add_to_cart']": {
@@ -746,6 +746,8 @@ export class ProductPage extends Interaction {
 
         const preventSale = combination.prevent_sale;
         const hidePrice = combination.hide_price;
+        const notifyMeButton = parent.querySelector('#stock_notification_button')
+        const stockNotificationEl = parent.querySelector("#stock_notification_wrapper");
         productPrice?.classList.toggle('d-inline-block', !hidePrice);
         productPrice?.classList.toggle('d-none', hidePrice);
         boxedPriceWrapper?.classList.toggle('d-flex', !hidePrice);
@@ -756,6 +758,7 @@ export class ProductPage extends Interaction {
         addToCart?.classList.toggle('d-none', preventSale);
         contactUsButton?.classList?.toggle('d-none', !preventSale);
         contactUsButton?.classList?.toggle('d-flex', preventSale);
+        stockNotificationEl?.classList?.replace("d-flex", "d-none")
 
         if (contactUsButton) {
             const link = contactUsButton.querySelector('a');
@@ -764,6 +767,10 @@ export class ProductPage extends Interaction {
                 linkUrl.searchParams.set('subject', combination.display_name);
                 link.href = linkUrl.toString();
             }
+        }
+
+        if (notifyMeButton) {
+            notifyMeButton.disabled=false
         }
 
         const price = parent.querySelector('.oe_price')?.querySelector('.oe_currency_value');
@@ -839,9 +846,9 @@ export class ProductPage extends Interaction {
 
         const addQtyInput = parent.querySelector('input[name="add_qty"]');
         const qty = parseFloat(addQtyInput?.value) || 1;
-        const ctaWrapper = parent.querySelector('#o_wsale_cta_wrapper');
-        ctaWrapper.classList.replace('d-none', 'd-flex');
-        ctaWrapper.classList.remove('out_of_stock');
+        if (this._showOutOfStock(combination)) {
+            this._toggleOutOfStockButtons(parent, combination, true)
+        }
 
         if (!combination.allow_out_of_stock_order) {
             const unavailableQty = await this.waitFor(this._getUnavailableQty(combination));
@@ -859,8 +866,7 @@ export class ProductPage extends Interaction {
                 }
             }
             if (combination.free_qty < 1 && this._showOutOfStock(combination)) {
-                ctaWrapper.classList.replace('d-flex', 'd-none');
-                ctaWrapper.classList.add('out_of_stock');
+                this._toggleOutOfStockButtons(parent, combination, false)
             }
         }
 
@@ -872,8 +878,7 @@ export class ProductPage extends Interaction {
                 }
             }
             if (combination.max_combo_quantity < 1 && this._showOutOfStock(combination)) {
-                ctaWrapper.classList.replace('d-flex', 'd-none');
-                ctaWrapper.classList.add('out_of_stock');
+                this._toggleOutOfStockButtons(parent, combination, false)
             }
         }
 
@@ -898,19 +903,20 @@ export class ProductPage extends Interaction {
         this.el.querySelector('div.availability_messages').append(renderToFragment(
             'website_sale.product_availability', combination
         ));
-        if (!this._showOutOfStock(combination)) {
-            this.el.querySelector('#stock_notification_div')?.classList.add('d-none')
-        } else if (this.el.querySelector('.o_add_wishlist_dyn')) {
-            const messageEl = this.el.querySelector('div.availability_messages');
-            if (messageEl && !this.el.querySelector('#stock_wishlist_message')) {
-                this.services['public.interactions'].stopInteractions(messageEl);
-                messageEl.append(
-                    renderToElement('website_sale.product_availability_wishlist', combination)
-                    || ''
-                );
-                this.services['public.interactions'].startInteractions(messageEl);
-            }
-        }
+    }
+
+    _toggleOutOfStockButtons(parent, combination, in_stock) {
+        const addToCartWrap = parent.querySelector('#add_to_cart_wrap');
+        const outOfStockWrapper = parent.querySelector('#out_of_stock_buttons_wrapper');
+        const notifyMeButton = parent.querySelector('#stock_notification_button');
+        const subscribedButton = parent.querySelector('#stock_notification_subscribed_button');
+
+        addToCartWrap.classList.toggle('d-inline-flex', in_stock);
+        addToCartWrap.classList.toggle('d-none', !in_stock);
+        outOfStockWrapper?.classList.toggle('d-flex', !in_stock);
+        outOfStockWrapper?.classList.toggle('d-none', in_stock);
+        notifyMeButton?.classList.toggle('d-none', combination.has_stock_notification);
+        subscribedButton?.classList.toggle('d-none', !combination.has_stock_notification);
     }
 
     async _getUnavailableQty(combination) {
@@ -991,8 +997,14 @@ export class ProductPage extends Interaction {
         const partnerEmail = document.querySelector('#wsale_user_email').value;
         const emailInputEl = document.querySelector('#stock_notification_input');
 
-        emailInputEl.value = partnerEmail;
-        this._handleClickStockNotificationMessage(ev);
+        if (partnerEmail) {
+            emailInputEl.value = partnerEmail;
+            this.onClickSubmitProductStockNotificationForm(ev);
+        } else {
+            const notifyMeButton = document.querySelector('#stock_notification_button')
+            notifyMeButton.disabled=true
+            this._handleClickStockNotificationMessage(ev);
+        }
     }
 
     onClickSubmitProductStockNotificationForm(ev) {
@@ -1001,14 +1013,17 @@ export class ProductPage extends Interaction {
     }
 
     _handleClickStockNotificationMessage(ev) {
-        ev.currentTarget.classList.add('d-none');
-        ev.currentTarget.parentElement.querySelector('#stock_notification_form').classList.remove('d-none');
+        const ctaSection = ev.currentTarget.closest("#o_wsale_product_cta_section");
+        ctaSection
+            .querySelector("#stock_notification_wrapper")
+            .classList.replace("d-none", "d-flex");
     }
 
     async _handleClickSubmitStockNotificationForm(ev, productId) {
-        const stockNotificationEl = ev.currentTarget.closest('#stock_notification_div');
-        const formEl = stockNotificationEl.querySelector('#stock_notification_form');
-        const email = stockNotificationEl.querySelector('#stock_notification_input').value.trim();
+        const ctaSection = ev.currentTarget.closest("#o_wsale_product_cta_section");
+        const stockNotificationEl = ctaSection.querySelector("#stock_notification_wrapper");
+        const notifyEl = ctaSection.querySelector("#stock_notification_button");
+        const email = stockNotificationEl.querySelector("#stock_notification_input").value.trim();
 
         if (!isEmail(email)) {
             return this._displayErrorMessage(_t('Invalid email'), stockNotificationEl);
@@ -1025,9 +1040,10 @@ export class ProductPage extends Interaction {
             }
             throw error;
         }
-        const message = stockNotificationEl.querySelector('#stock_notification_success_message');
-        message.classList.remove('d-none');
-        formEl.classList.add('d-none');
+        const message = ctaSection.querySelector("#stock_notification_subscribed_button");
+        stockNotificationEl.classList.replace("d-flex", "d-none");
+        message.classList.remove("d-none");
+        notifyEl.classList.add("d-none");
     }
 
     _displayErrorMessage(message, stockNotificationEl) {

addons/website_sale/static/src/js/product_configurator_dialog/product_configurator_dialog.js
@@ -32,7 +32,19 @@ patch(ProductConfiguratorDialog.prototype, {
         useSubEnv({
             isFrontend: this.props.isFrontend,
             isMainProductConfigurable: this.props.options?.isMainProductConfigurable ?? true,
+            isQuantityAllowed: this._isQuantityInStock.bind(this),
         });
+    },
+
+    /**
+     * Override of `sale` to limit the quantity to the stock availability
+     */
+    async _setQuantity(productTmplId, quantity) {
+        const product = this._findProduct(productTmplId);
+        if (!this._isQuantityInStock(product, quantity)) {
+            quantity = product.free_qty;
+        }
+        return super._setQuantity(productTmplId, quantity)
     },
 
     /**
@@ -41,7 +53,7 @@ patch(ProductConfiguratorDialog.prototype, {
      * @return {Boolean} - Whether all selected products can be sold.
      */
     canBeSold() {
-        return this.state.products.every(p => p.can_be_sold);
+        return this.state.products.every(p => p.can_be_sold && this._isQuantityInStock(p, p.quantity));
     },
 
     /**
@@ -69,6 +81,19 @@ patch(ProductConfiguratorDialog.prototype, {
             return _t("Total: %s", this.getFormattedTotal());
         }
         return super.totalMessage(...arguments);
+    },
+
+    /**
+     * Check whether the provided product quantity can be added to the cart.
+     *
+     * @param {Object} product - The provided product.
+     * @param {Number} quantity - The new quantity of the product.
+     * @return {Boolean} - Whether the provided product quantity can be added to the cart.
+     */
+    _isQuantityInStock(product, quantity) {
+        // `product` may be a props object, which always exposes every declared key (including the
+        // optional `free_qty`), so the presence of the key can't be used to detect stock tracking.
+        return product.free_qty === undefined || product.free_qty >= quantity;
     },
 
 });