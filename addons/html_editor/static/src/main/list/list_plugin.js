f104074108f241b6801537add12aff6b10e5101d
Walid (wasa) <wasa@odoo.com>
2026-09-07 12:46:35 +0000

    [FIX] html_editor: adjust list padding when converting block to list
    Problem:
    Creating a list on a header block with large font size content causes the list marker/bullet to overflow to the left.
    
    Cause:
    `ListPlugin.blockToList()` wrapped block elements into a list without invoking `this.adjustListPadding(list)`, leaving the list padding unadjusted for larger font sizes.
    
    Solution:
    Call `this.adjustListPadding(list)` in `blockToList` so that proper inline padding is set based on the list item content font size.
    
    Steps to reproduce:
    - Create a header block (e.g. Header 4).
    - Change the font size of the header content to be bigger.
    - Apply a list on the content.
    => Observe that the list marker overflows to the left.
    
    opw-6542903
    
    X-original-commit: bce5c19eca5678e7f174d3c3785eb0712e36ccb4

--- addons/html_editor/static/src/main/list/list_plugin.js
+++ addons/html_editor/static/src/main/list/list_plugin.js
@@ -402,6 +402,7 @@ export class ListPlugin extends Plugin {
                 list.setAttribute("dir", element.getAttribute("dir"));
             }
         }
+        this.adjustListPadding(list);
         cursors.restore();
         return list;
     }
