LAIDOUDI33/LAID_ODOO

[FIX] l10n_account_withholding_tax: added fallback value for currency
Currently, an error occurs when user tries to pay on a vendor bill and removes
the currency.

Steps to replicate:
- Install `l10n_account_withholding_tax`and activate multiple currencies.
- Open Invoicing > Vendors > Bills and create a new bill and add a vendor
  and bill date.
- Add a product and tax `2% WTH`.
- From the Cog menu > Click Pay > Remove the Currency.

Error:
```
  File '/home/odoo/src/odoo/saas-19.4/addons/l10n_account_withholding_tax/models/account_withholding_line.py', line 208, in _compute_original_amounts
    line.original_base_amount = line_curr.round(base_amount * rate)
  File '/home/odoo/src/odoo/saas-19.4/odoo/addons/base/models/res_currency.py', line 264, in round
    self.ensure_one()
  File '/home/odoo/src/odoo/saas-19.4/odoo/orm/models.py', line 5342, in ensure_one
    raise ValueError('Expected singleton: %s' % self)
ValueError: Expected singleton: res.currency()
```

Cause:
- As the user removed currency, the `comodel_currency_id`is received as false.
- Later when we call `round()` on the empty res.currency recordset causes this
  error to occur.

Solution:
- Added the company currency as a fallback value when `currency_id` is removed
  by user, since `currency_id` is a required field user will need to select a
  currency when saving.

sentry-7616890592

addons/l10n_account_withholding_tax/tests/test_account_withholding_flows.py
@@ -1159,3 +1159,21 @@ class TestL10nAccountWithholdingTaxesFlows(TestTaxCommon, AnalyticCommon):
             {"balance": 1000.0,     "tax_tag_ids": base_tag_minus.ids},
             {"balance": -1000.0,    "tax_tag_ids": []},
         ])
+
+    def test_payment_register_with_empty_currency(self):
+        withholding_tax = self.percent_tax(
+            -1,
+            is_withholding_tax_on_payment=True,
+            withholding_sequence_id=self.withholding_sequence.id
+        )
+
+        invoice = self._create_invoice_one_line(tax_ids=withholding_tax, price_unit=1000.0)
+
+        payment_register = self.env['account.payment.register']\
+            .with_context(active_model='account.move', active_ids=invoice.ids)\
+            .create({})
+
+        payment_register_form = Form(payment_register)
+        payment_register_form.currency_id = self.env['res.currency']
+
+        self.assertEqual(payment_register.withholding_line_ids.original_base_amount, 1000.0)

odoo/odoo

X-original-commit: f7655b1feb72b17cfb2c427d42cf5b66d336f502

addons/l10n_account_withholding_tax/tests/test_account_withholding_flows.py
@@ -1340,3 +1340,21 @@ class TestL10nAccountWithholdingTaxesFlows(TestTaxCommon, AnalyticCommon):
             'withholding_residual_amount_currency': 0.00,
             'withholding_net_residual_amount_currency': 0.00,
         }])
+
+    def test_payment_register_with_empty_currency(self):
+        withholding_tax = self.percent_tax(
+            -1,
+            is_withholding_tax=True,
+            withholding_sequence_id=self.withholding_sequence.id
+        )
+
+        invoice = self._create_invoice_one_line(tax_ids=withholding_tax, price_unit=1000.0)
+
+        payment_register = self.env['account.payment.register']\
+            .with_context(active_model='account.move', active_ids=invoice.ids)\
+            .create({})
+
+        payment_register_form = Form(payment_register)
+        payment_register_form.currency_id = self.env['res.currency']
+
+        self.assertEqual(payment_register.withholding_line_ids.original_base_amount, 1000.0)