5c2ff3e026aa77bcae5f65274b55da27562b7374
Shubham Agarwal <shag@odoo.com>
2026-09-07 17:30:20 +0530

    [IMP] point_of_sale: set default preset for floating order
    Before this commit:
    - Default preset was not being set when `Select preset` dialog was closed
    without choosing any preset.
    
    Following this commit:
    - Default preset will be set.
    
    task-6538754

--- addons/point_of_sale/static/src/app/services/pos_store.js
+++ addons/point_of_sale/static/src/app/services/pos_store.js
@@ -2306,6 +2306,7 @@ export class PosStore extends WithLazyGetterTrap {
         }
     }
     async selectPreset(preset = false, order = this.getOrder(), presetSelection = false) {
+        const defaultPreset = this.config.default_preset_id;
         if (!preset || presetSelection) {
             const selectionList = this.config.available_preset_ids.map((preset) => ({
                 id: preset.id,
@@ -2362,6 +2363,8 @@ export class PosStore extends WithLazyGetterTrap {
             } else if (!preset.use_timing) {
                 order.preset_time = false;
             }
+        } else if (defaultPreset) {
+            order.setPreset(defaultPreset);
         }
     }
     orderUsageUTCtoLocal(data) {
