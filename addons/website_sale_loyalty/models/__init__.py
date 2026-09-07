5082f2663021e23a4580fb308eab70edf12d6ad3
Zachary (zavan) <zavan@odoo.com>
2026-09-07 11:00:03 +0000

    [FIX] website_sale_loyalty: disable on site payment if gift card/eWallet
    We don't want to support gift cards and eWallets in certain conditions
    
    opw-6483282
    
    X-original-commit: feb8122ad062f3ba4e15426f95e89168584d4332

--- addons/website_sale_loyalty/models/__init__.py
+++ addons/website_sale_loyalty/models/__init__.py
@@ -4,6 +4,11 @@
 from . import loyalty_card
 from . import loyalty_program
 from . import loyalty_rule
+<<<<<<< 3dfed87e09ccbfca8252fc353f8d62cb45f06c90
 from . import product_product
+||||||| ebea31d05ee7cf8f627210366255b170d7dde4fe
+=======
+from . import payment_provider
+>>>>>>> fa3872ca8bac8065f64a678ba52f90163044ac6b
 from . import sale_order_line
 from . import sale_order
