LAIDOUDI33/LAID_ODOO

[IMP] sale_stock_product_expiry: add expiration date to invoice
There was no option to show the expiration dates of SN/LN tracked product on the invoice.
This information could be useful for clients to ensure they receive products that are of good quality.
This option will also improve the traceability of the product and result in increased transparency.

Task-5259448

addons/stock_account_product_expiry/models/res_config_settings.py
@@ -1,0 +1,8 @@
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    group_exp_date_on_invoice = fields.Boolean("Display Expiration Dates on Invoices",
+        implied_group='stock_account_product_expiry.group_exp_date_on_invoice')

odoo/odoo

addons/sale_stock_product_expiry/models/res_config_settings.py
@@ -1,0 +1,8 @@
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    group_exp_date_on_invoice = fields.Boolean("Display Expiration Dates on Invoices",
+        implied_group='sale_stock_product_expiry.group_exp_date_on_invoice')