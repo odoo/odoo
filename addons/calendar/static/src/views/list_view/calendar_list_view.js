2da3e30428fc71a87505f007d453480ad746a144
mekav-odoo <mekav@odoo.com>
2026-09-08 15:26:53 +0530

    [FIX] calendar: display attendee filters in list view
    When switching from the calendar view to the list view, records are
    filtered by the attendees selected in the calendar side panel, but the
    attendee filter is not visible in the search bar.
    
    Display the selected attendees as a search facet when switching to the
    list view, while keeping the list records filtered by the same attendees.
    
    Task-6424956

--- addons/calendar/static/src/views/list_view/calendar_list_view.js
+++ addons/calendar/static/src/views/list_view/calendar_list_view.js
@@ -1,4 +1,5 @@
 import { listView } from "@web/views/list/list_view";
+import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
 import { user } from "@web/core/user";
 import { CaledarListController } from "./calendar_list_controller";
@@ -16,14 +17,36 @@ export class CalendarListModel extends listView.Model {
         const filters = params?.context?.calendar_filters;
         const emptyDomain = Array.isArray(params?.domain) && params.domain.length == 0;
         if (filters && emptyDomain) {
-            const selectedPartnerIds = await this.orm.call(
+            const selectedPartners = await this.orm.call(
                 "res.users",
-                "get_selected_calendars_partner_ids",
+                "get_selected_calendars_partners",
                 [[user.userId], filters["user"]]
             );
+            const selectedPartnerIds = selectedPartners.map((p) => p.id);
             // Filter attendees to be shown if 'everybody' filter isn't active.
-            if (!filters["all"]) {
-                params.domain.push(["partner_ids", "in", selectedPartnerIds]);
+            if (!filters["all"] && selectedPartnerIds.length) {
+                const searchModel = this.env.searchModel;
+                const key = selectedPartnerIds.join(",");
+
+                if (searchModel._calendarAttendeeFilterKey !== key) {
+                    searchModel._calendarAttendeeFilterKey = key;
+                    params.domain.push(["partner_ids", "in", selectedPartnerIds]);
+                    const searchItem = Object.values(searchModel.searchItems).find(
+                        (item) => item.fieldName === "partner_ids"
+                    );
+
+                    if (searchItem) {
+                        searchModel.addAutoCompletionValues(searchItem.id, {
+                            label: selectedPartners
+                                .map((p) => p.display_name)
+                                .join(` ${_t("or")} `),
+                            value: selectedPartnerIds,
+                            operator: "in",
+                        });
+                    }
+                }
+            } else {
+                this.env.searchModel._calendarAttendeeFilterKey = null;
             }
         }
         return super.load(params);
@@ -44,4 +67,4 @@ function _mockGetCalendarPartnerIds(params) {
 registry.category("views").add("calendar_list_view", CalendarListView);
 registry
     .category("sample_server")
-    .add("get_selected_calendars_partner_ids", _mockGetCalendarPartnerIds);
+    .add("get_selected_calendars_partners", _mockGetCalendarPartnerIds);
