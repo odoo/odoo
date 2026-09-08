a714a9ed893ea66455190b29374509962d9dfbd9
JemXiaoLong <escuderojem@outlook.com>
2026-09-08 16:07:24 -0600

    [REF] spreadsheet: collapse OdooPivotLoader's own load state machine
    OdooPivotLoader hand-rolled the same load/error/validity state machine as
    LoadableDataSource (identical branch structure for load(), assertIsValid(),
    isModelValid()/isValid()) instead of extending it, even though it already
    imported getFields/LOADING_ERROR/ModelNotFoundError from that same file.
    The duplication meant a fix to one implementation had no way to reach
    the other.
    
    Solution:
    - Make OdooPivotLoader extend LoadableDataSource, overriding _load() to
      call the injected load function, keeping only its two extra methods
      (getFields, getModelLabel) and hasEverBeenLoaded()
    
    Task ID: 31853

--- addons/spreadsheet/static/src/pivot/odoo_pivot_loader.js
+++ addons/spreadsheet/static/src/pivot/odoo_pivot_loader.js
@@ -1,99 +1,33 @@
 /** @odoo-module native */
 //@ts-check
 
-import { CellErrorType, EvaluationError } from "@odoo/o-spreadsheet";
-import {
-    getFields,
-    LOADING_ERROR,
-    ModelNotFoundError,
-} from "@spreadsheet/data_sources/data_source";
-import { RPCError } from "@web/core/network";
-import { _t } from "@web/core/translation";
-import { KeepLast } from "@web/core/utils/concurrency";
+import { getFields, LoadableDataSource } from "@spreadsheet/data_sources/data_source";
 
 /**
  * @typedef {import("@spreadsheet").OdooFields} OdooFields
  * @typedef {import("@spreadsheet/data_sources/odoo_data_provider").OdooDataProvider} OdooDataProvider
  */
 
-export class OdooPivotLoader {
+/**
+ * Loads pivot data via an injected load function, reusing LoadableDataSource's
+ * load/error/validity state machine instead of re-implementing it.
+ */
+export class OdooPivotLoader extends LoadableDataSource {
     /**
      * @param {OdooDataProvider} odooDataProvider
      * @param {Function} load Function to fetch data
      */
     constructor(odooDataProvider, load) {
-        /** @private @type {OdooDataProvider} */
-        this.odooDataProvider = odooDataProvider;
+        super({ odooDataProvider });
         /** @protected */
         this.loadFn = load;
-
-        /**
-         * Last time that this dataSource has been updated
-         */
-        this.lastUpdate = undefined;
-
-        /** @protected */
-        this.concurrency = new KeepLast();
-        /** @protected */
-        this.loadPromise = undefined;
-        /** @protected */
-        this.isFullyLoaded = false;
-        /** @protected */
-        this._isValid = true;
-        /** @protected */
-        this.loadError = undefined;
-        /** @protected */
-        this._isModelValid = true;
     }
 
     /**
-     * Load data in the model
-     * @param {object} [options] options for fetching data
-     * @param {boolean} [options.reload=false] Force the reload of the data
-     *
-     * @returns {Promise} Resolved when data are fetched.
+     * @protected
      */
-    async load(options) {
-        if (options && options.reload) {
-            this.odooDataProvider.cancelPromise(this.loadPromise);
-            this.loadPromise = undefined;
-        }
-        if (!this.loadPromise) {
-            this.isFullyLoaded = false;
-            this._isValid = true;
-            this.loadError = undefined;
-            this.loadPromise = this.concurrency
-                .add(this.loadFn())
-                .catch((e) => {
-                    this._isValid = false;
-                    if (e instanceof ModelNotFoundError) {
-                        this._isModelValid = false;
-                        this.loadError = Object.assign(
-                            new EvaluationError(
-                                _t(`The model "%(model)s" does not exist.`, {
-                                    model: e.message,
-                                }),
-                            ),
-                            {
-                                cause: e,
-                            },
-                        );
-                        return;
-                    }
-                    this.loadError = Object.assign(
-                        new EvaluationError(
-                            e instanceof RPCError ? e.data.message : e.message,
-                        ),
-                        { cause: e },
-                    );
-                })
-                .finally(() => {
-                    this.lastUpdate = Date.now();
-                    this.isFullyLoaded = true;
-                });
-            await this.odooDataProvider.notifyWhenPromiseResolves(this.loadPromise);
-        }
-        return this.loadPromise;
+    async _load() {
+        return this.loadFn();
     }
 
     /**
@@ -114,34 +48,7 @@ export class OdooPivotLoader {
         return result[0]?.display_name || "";
     }
 
-    isModelValid() {
-        return this.isFullyLoaded && this._isModelValid;
-    }
-
-    isValid() {
-        return this.isFullyLoaded && this._isValid;
-    }
-
     hasEverBeenLoaded() {
-        return this.loadPromise !== undefined;
-    }
-
-    assertIsValid({ throwOnError } = { throwOnError: true }) {
-        if (!this.isFullyLoaded) {
-            this.load();
-            if (throwOnError) {
-                throw LOADING_ERROR;
-            }
-            return LOADING_ERROR;
-        }
-        if (!this.isValid()) {
-            if (throwOnError) {
-                throw this.loadError;
-            }
-            return {
-                value: CellErrorType.GenericError,
-                message: this.loadError.message,
-            };
-        }
+        return this._loadPromise !== undefined;
     }
 }
