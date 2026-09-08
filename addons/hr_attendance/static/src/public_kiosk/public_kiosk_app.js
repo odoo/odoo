LAIDOUDI33/LAID_ODOO

[REF] barcode, *: Refactor barcodeService into BarcodePlugin
Services need to be converted to Plugin as part of the Owl3 migration.
This commit converts the barcodeService into a new BarcodePlugin.

addons/hr_attendance/static/src/public_kiosk/public_kiosk_app.js
@@ -5,7 +5,7 @@ import { KioskBarcodeScanner } from "@hr_attendance/components/kiosk_barcode/kio
 import { KioskManualSelection } from "@hr_attendance/components/manual_selection/manual_selection";
 import { NewEmployeeDialog } from "@hr_attendance/components/new_employee_dialog/new_employee_dialog";
 import { KioskPinCode } from "@hr_attendance/components/pin_code/pin_code";
-import { Component, proxy, whenReady } from "@odoo/owl";
+import { Component, proxy, plugin, whenReady } from "@odoo/owl";
 import { browser } from "@web/core/browser/browser";
 import { isIosApp } from "@web/core/browser/feature_detection";
 import { _t } from "@web/core/l10n/translation";
@@ -16,6 +16,7 @@ import { url } from "@web/core/utils/urls";
 import { mountComponent } from "@web/env";
 import { session } from "@web/session";
 import { DocumentationLink } from "@web/views/widgets/documentation_link/documentation_link";
+import { BarcodePlugin } from "@barcodes/barcode_plugin";
 
 class kioskAttendanceApp extends Component {
     static template = "hr_attendance.public_kiosk_app";
@@ -43,7 +44,7 @@ class kioskAttendanceApp extends Component {
 
     setup() {
         this.dialogService = useService("dialog");
-        this.barcode = useService("barcode");
+        this.barcode = plugin(BarcodePlugin);
         this.notification = useService("notification");
         this.ui = useService("ui");
         this.companyImageUrl = url("/web/binary/company_logo", {

odoo/odoo

addons/hr_attendance/static/src/public_kiosk/public_kiosk_app.js
@@ -6,7 +6,7 @@ import { KioskBarcodeScanner } from "@hr_attendance/components/kiosk_barcode/kio
 import { KioskManualSelection } from "@hr_attendance/components/manual_selection/manual_selection";
 import { NewEmployeeDialog } from "@hr_attendance/components/new_employee_dialog/new_employee_dialog";
 import { KioskPinCode } from "@hr_attendance/components/pin_code/pin_code";
-import { Component, proxy, whenReady } from "@odoo/owl";
+import { Component, proxy, usePlugin, whenReady } from "@odoo/owl";
 import { browser } from "@web/core/browser/browser";
 import { _t } from "@web/core/l10n/translation";
 import { MainComponentsContainer } from "@web/core/main_components_container";
@@ -16,6 +16,7 @@ import { url } from "@web/core/utils/urls";
 import { mountComponent } from "@web/env";
 import { session } from "@web/session";
 import { DocumentationLink } from "@web/views/widgets/documentation_link/documentation_link";
+import { BarcodePlugin } from "@barcodes/barcode_plugin";
 
 class kioskAttendanceApp extends Component {
     static template = "hr_attendance.public_kiosk_app";
@@ -43,7 +44,7 @@ class kioskAttendanceApp extends Component {
 
     setup() {
         this.dialogService = useService("dialog");
-        this.barcode = useService("barcode");
+        this.barcode = usePlugin(BarcodePlugin);
         this.notification = useService("notification");
         this.ui = useService("ui");
         this.companyImageUrl = url("/web/binary/company_logo", {