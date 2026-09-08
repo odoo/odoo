7df00a76747c0c2e080bbf7626547e6a1f00df22
Ester Andreetto <esand@odoo.com>
2026-09-08 15:17:44 +0200

    [FIX] account_edi_ubl_cii: import PaymentReference from CII XML
    Issue before this commit:
    When importing a CII XML invoice containing a PaymentReference, the
    value is not transferred to the generated vendor bill in Odoo.
    
    Steps to reproduce the issue:
    1. Download Accounting
    2. Try to import the invoice in the ticket
    3. See that in the tab other info the payment reference is not imported
    
    Cause of the issue:
    During a previous refactoring (ffbdf29a816d0ff4136488d26b55b9707fc37fc6),
    the helper function responsible for extracting the payment reference
    during the import process was omitted.
    
    Reason to introduce the fix:
    Add the missing extraction logic to ensure the payment reference is
    correctly retrieved from the XML and assigned to the Odoo invoice.
    
    opw-6530627

--- addons/account_edi_ubl_cii/tests/test_cii_facturx_fr_decode_invoice_line.py
+++ addons/account_edi_ubl_cii/tests/test_cii_facturx_fr_decode_invoice_line.py
@@ -218,7 +218,10 @@ class TestCiiImportFacturXFRInvoiceLine(CiiImportFacturXFR):
 
     def test_partial_import_invoice_discount_on_price_zero(self):
         imported_invoice = self._import_invoice_as_attachment_on(test_name='test_partial_import_invoice_discount_on_price_zero')
-        self.assertRecordValues(imported_invoice, [{'amount_total': 1.73}])
+        self.assertRecordValues(imported_invoice, [{
+            'amount_total': 1.73,
+            'payment_reference': 'INV/2026/00001',
+            }])
         self.assertRecordValues(imported_invoice.invoice_line_ids, [{
             'name': self.product_a.name,
             'price_subtotal': 1.5,
