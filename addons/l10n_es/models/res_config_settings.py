7390dff7a242f918f5fe6fa3d2c22542c7a9d246
jlll <jlll@odoo.com>
2026-09-08 15:36:39 +0200

    [IMP] l10n_es: Merge edi settings and canary accounting plan

--- addons/l10n_es/models/res_config_settings.py
+++ addons/l10n_es/models/res_config_settings.py
@@ -8,8 +8,10 @@ class ResConfigSettings(models.TransientModel):
         related='company_id.l10n_es_simplified_invoice_limit',
         readonly=False,
     )
-
     l10n_es_special_vat_regime = fields.Selection(
         related='company_id.l10n_es_special_vat_regime',
         readonly=False,
     )
+    module_l10n_es_edi_verifactu = fields.Boolean('Veri*Factu')
+    module_l10n_es_edi_sii = fields.Boolean('SII')
+    module_l10n_es_edi_tbai = fields.Boolean('TicketBai')
