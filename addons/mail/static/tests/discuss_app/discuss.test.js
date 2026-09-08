20ded28a2c57498bbe0073ed4dbae0b0dfab2ff7
Haja Ram <ramh@odoo.com>
2026-09-08 12:00:36 +0530

    [FIX] mail: prevent interactions in message preview
    Prior to this commit, clicking links or mentions in the message preview could
    trigger their actions instead of opening the related record. This could
    happen when clicking the preview from the messaging menu, while links and
    mentions in the chat bubble preview could also be clicked directly.
    
    This commit prevents clicking links and mentions from triggering their actions
    in message previews in both the messaging menu and chat bubbles. The links and
    mentions keep their visual appearance, but clicking the preview now only opens
    the related record in messaging menu and does nothing in bubble preview.
    
    Task-6569382

--- addons/mail/static/tests/discuss_app/discuss.test.js
+++ addons/mail/static/tests/discuss_app/discuss.test.js
@@ -2785,3 +2785,32 @@ test("Cannot call read-only channels", async () => {
         count: 6,
     });
 });
+
+test("Prevent link interactions in message preview", async () => {
+    const pyEnv = await startServer();
+    const partnerId = pyEnv["res.partner"].create({ name: "Marc" });
+    const channelId = pyEnv["discuss.channel"].create({
+        channel_member_ids: [
+            Command.create({ partner_id: serverState.partnerId }),
+            Command.create({ partner_id: partnerId }),
+        ],
+        channel_type: "chat",
+    });
+    const messageId = pyEnv["mail.message"].create({
+        body: "<a href='https://www.odoo.com'>https://www.odoo.com</a>",
+        model: "discuss.channel",
+        res_id: channelId,
+        message_type: "comment",
+    });
+    const [memberId] = pyEnv["discuss.channel.member"].search([
+        ["channel_id", "=", channelId],
+        ["partner_id", "=", partnerId],
+    ]);
+    pyEnv["discuss.channel.member"].write([memberId], { seen_message_id: messageId });
+    await start();
+    await openDiscuss();
+    await contains("h4:text('No conversation selected.')");
+    await click(".o-mail-NotificationItem-text a");
+    expect(".o-mail-NotificationItem-text").toHaveStyle({ pointerEvents: "none" });
+    await contains(".o-mail-Message:has(:text('https://www.odoo.com'))");
+});
