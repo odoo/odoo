02eb998bec20dc217abad7874cd7236ed63d7078
agbr-odoo <agbr@odoo.com>
2026-09-08 12:24:38 +0530

    [FIX] html_editor: properly remove characters at link edges
    Problem:
    When selection at the start of link and we try to remove the character
    before the link nothing happens.
    
    Cause:
    `deleteCharUnmergeable` was wrongly proceccing the link edges deletion.
    
    Solution:
    We should for `deleteCharUnmergeable` only consider unmergeable blocks.
    
    Steps to reproduce:
    - Add a link between two characters.
    - Delete the content of the link.
    - When the link is empty, press Backspace or Delete.
    - Observe that the selection moves improperly instead of staying at the
    link position.
    
    opw-6478839

--- addons/html_builder/static/tests/monetary_field.test.js
+++ addons/html_builder/static/tests/monetary_field.test.js
@@ -1,6 +1,13 @@
 import { setupHTMLBuilder } from "@html_builder/../tests/helpers";
+import { BuilderContentEditablePlugin } from "@html_builder/core/builder_content_editable_plugin";
+import { MonetaryFieldPlugin } from "@html_builder/plugins/monetary_field_plugin";
+import { MAIN_PLUGINS } from "@html_editor/plugin_sets";
 import { expect, test, describe } from "@odoo/hoot";
 import { click, queryOne } from "@odoo/hoot-dom";
+import { testEditor } from "@html_editor/../tests/_helpers/editor";
+import { unformat } from "@html_editor/../tests/_helpers/format";
+import { deleteBackward } from "@html_editor/../tests/_helpers/user_actions";
+import { defineMailModels } from "@mail/../tests/mail_test_helpers";
 
 describe.current.tags("desktop");
 
@@ -27,4 +34,39 @@ test("clicking on the monetary field should select the amount", async () => {
             queryOne(":iframe span.oe_currency_value")
         )
     ).toBe(true, { message: "value of monetary field is selected" });
+});
+
+test("should make a span inside a monetary field be unremovable", async () => {
+    defineMailModels();
+    await testEditor({
+        contentBefore: unformat(`
+            <p>
+                <span data-oe-type="monetary" class="o_editable">
+                    $&nbsp;
+                    <span class="oe_currency_value" data-oe-zws-empty-inline="">[]\u200b</span>
+                </span>
+            </p>
+        `),
+        contentBeforeEdit: unformat(`
+                <p>
+                    <span data-oe-type="monetary" class="o_editable" contenteditable="true">
+                        $&nbsp;
+                        <span class="oe_currency_value" data-oe-zws-empty-inline="">[]\u200b</span>
+                    </span>
+                </p>
+            `),
+        stepFunction: deleteBackward,
+        contentAfter: unformat(`
+                <p>
+                    <span data-oe-type="monetary" class="o_editable">
+                        $
+                        <span class="oe_currency_value" data-oe-zws-empty-inline="">[]</span>
+                    </span>
+                </p>
+            `),
+        config: {
+            Plugins: [...MAIN_PLUGINS, MonetaryFieldPlugin, BuilderContentEditablePlugin],
+            cleanEmptyStructuralContainers: false,
+        },
+    });
 });
