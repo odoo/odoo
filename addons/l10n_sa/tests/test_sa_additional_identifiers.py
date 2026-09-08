11378fa74cabf06b674af7bd77a7d32044486139
adip-odoo <adip@odoo.com>
2026-09-08 16:31:11 +0530

    [FIX] l10n_sa: fix error when computing SA TIN without VAT
    Steps to Reproduce:
    - Install the `l10n_sa` and `contacts` modules.
    - Create a new contact and set the country to Saudi Arabia.
    - Click the "+" sign next to the TIN field.
    - Select "Tax Identification Number".
    
    Error:
    TypeError: 'NoneType' object is not subscriptable
    
    Cause:
    The onchange method tries to extract the SA TIN from the VAT even when the VAT
    is not set. This causes an error when slicing the None value.
    
    Fix:
    Skip populating the SA TIN when the VAT is not set.
    
    sentry-7716910106
    
    closes odoo/odoo#287217
    
    Signed-off-by: Seif Gneedy (segn) <segn@odoo.com>

--- addons/l10n_sa/tests/test_sa_additional_identifiers.py
+++ addons/l10n_sa/tests/test_sa_additional_identifiers.py
@@ -1,5 +1,5 @@
 from odoo.addons.account.tests.common import AccountTestInvoicingCommon
-from odoo.tests import tagged
+from odoo.tests import Form, tagged
 from odoo.exceptions import ValidationError
 
 
@@ -69,3 +69,8 @@ class TestL10nSaAdditionalIdentifiers(AccountTestInvoicingCommon):
         })
 
         self.assertNotIn('OTHER', self.partner_a.available_additional_identifiers_metadata)
+
+    def test_populate_sa_tin_without_vat(self):
+        partner = Form(self.partner_sa)
+        partner.additional_identifiers = {'SA_TIN': ''}
+        self.assertEqual(partner.additional_identifiers.get('SA_TIN'), '')
