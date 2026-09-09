15edff0bd9328cdf47316552001f46b32c4a4cff
Julien Mougenot <jum@odoo.com>
2026-09-09 17:29:31 +0200

    [REF] purchase*: convert static props to useProps
    Replaced deprecated uses of `static props` with calls to `useProps()`,
    providing actual props validation for Owl components.
    
    Whenever possible, also removed "empty" props definition, being either:
    - `static props = ["*"]` with no use of `this.props`;
    - `static props = {}`.

--- addons/purchase/static/src/product_catalog/purchase_order_line/purchase_order_line.js
+++ addons/purchase/static/src/product_catalog/purchase_order_line/purchase_order_line.js
@@ -1,9 +1,9 @@
+import { t, useProps } from "@odoo/owl";
 import { ProductCatalogOrderLine } from "@product/product_catalog/order_line/order_line";
 
 export class ProductCatalogPurchaseOrderLine extends ProductCatalogOrderLine {
-    static props = {
-        ...ProductCatalogPurchaseOrderLine.props,
-        min_qty: { type: Number, optional: true },
-        packaging: { type: Object, optional: true },
-    };
+    purchaseOrderLineProps = useProps({
+        min_qty: t.number().optional(),
+        packaging: t.object().optional(),
+    });
 }
