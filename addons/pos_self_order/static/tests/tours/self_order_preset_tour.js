727cb3760807283e0edfa3d877fb1d6e5f63f121
dhruv <djip@odoo.com>
2026-09-09 06:34:05 +0000

    [FIX] point_of_sale: fix slot visibility
    Before this commit:
    * Slots that reached their maximum capacity for a given time frame were hidden
      from the `pos_self_order` slot selection dialog.
    
    After this commit:
    * Slots remain visible but are disabled when they reach their maximum capacity.
    
    task-6340956
    
    X-original-commit: 312ab0525e9dd2082d749232450e0d586783d4dd

--- addons/pos_self_order/static/tests/tours/self_order_preset_tour.js
+++ addons/pos_self_order/static/tests/tours/self_order_preset_tour.js
@@ -3,9 +3,15 @@ import * as Utils from "@pos_self_order/../tests/tours/utils/common";
 import * as CartPage from "@pos_self_order/../tests/tours/utils/cart_page_util";
 import * as LandingPage from "@pos_self_order/../tests/tours/utils/landing_page_util";
 import * as ProductPage from "@pos_self_order/../tests/tours/utils/product_page_util";
+<<<<<<< 165b08303b1c4444c0085e9742b5bb699852a707
 import * as Chrome from "@point_of_sale/../tests/pos/tours/utils/chrome_util";
 const { DateTime } = luxon;
 import { today } from "@web/core/l10n/dates";
+||||||| 157874aad3aebef5bc9268de6e17530641107e31
+import * as Chrome from "@point_of_sale/../tests/pos/tours/utils/chrome_util";
+import { today } from "@web/core/l10n/dates";
+=======
+>>>>>>> 4695f4eeff770fce02ca0ccf7754db0d9955b7af
 
 registry.category("web_tour.tours").add("self_order_preset_dine_in_tour", {
     steps: () => [
@@ -83,29 +89,6 @@ registry.category("web_tour.tours").add("self_order_preset_slot_tour", {
         Utils.checkConfirmationString(true),
         Utils.clickBtn("Ok"),
     ],
-});
-
-registry.category("web_tour.tours").add("test_slot_limit_orders", {
-    steps: () =>
-        [
-            Chrome.freezeDateTime(today().ts),
-            Utils.checkIsNoBtn("My Order"),
-            Utils.clickBtn("Order Now"),
-            LandingPage.selectLocation("Takeaway"),
-            ProductPage.clickProduct("Free"),
-            Utils.clickBtn("Checkout"),
-            Utils.clickBtn("Order"),
-            CartPage.selectSpecificValueInInput(".slot-select", "18:00"),
-            CartPage.fillInput("Name", "Dr Dre"),
-            Utils.clickBtn("Continue"),
-            Utils.clickBtn("Ok"),
-            Utils.clickBtn("Order Now"),
-            LandingPage.selectLocation("Takeaway"),
-            ProductPage.clickProduct("Free"),
-            Utils.clickBtn("Checkout"),
-            Utils.clickBtn("Order"),
-            CartPage.checkSlotUnavailable("18:00"),
-        ].flat(),
 });
 
 registry.category("web_tour.tours").add("test_preset_takeaway_email_tour", {
