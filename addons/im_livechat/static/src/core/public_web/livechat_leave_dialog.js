695d53a34ce2abc387673d6861e7a2ddaaf71d1e
Julien Mougenot <jum@odoo.com>
2026-09-09 17:22:58 +0200

    [REF] im_livechat: convert static props to useProps
    Replaced deprecated uses of `static props` with calls to `useProps()`,
    providing actual props validation for Owl components.
    
    Whenever possible, also removed "empty" props definition, being either:
    - `static props = ["*"]` with no use of `this.props`;
    - `static props = {}`.

--- addons/im_livechat/static/src/core/public_web/livechat_leave_dialog.js
+++ addons/im_livechat/static/src/core/public_web/livechat_leave_dialog.js
@@ -1,16 +1,16 @@
 import { discussComponentRegistry } from "@mail/core/common/discuss_component_registry";
-import { Component } from "@odoo/owl";
+import { Component, t, useProps } from "@odoo/owl";
 
 import { Dialog } from "@web/core/dialog/dialog";
 import { _t } from "@web/core/l10n/translation";
 
 export class LivechatLeaveDialog extends Component {
     static components = { Dialog };
-    static props = {
-        close: Function,
-        channel: Object,
-        onConfirm: Function,
-    };
+    props = useProps({
+        close: t.function(),
+        channel: t.object(),
+        onConfirm: t.function(),
+    });
     static template = "im_livechat.LivechatLeaveDialog";
 
     get messageComponent() {
