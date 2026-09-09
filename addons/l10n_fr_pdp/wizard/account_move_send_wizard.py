563c64a62152175ba33ced16f651c9780cf59c71
Yassien Ghoniem <yagho@odoo.com>
2026-09-09 13:06:59 +0000

    [IMP] l10n_fr_pdp: recompute French EAS and identifier
    for existing french partners that dont have their EAS set to the FRCTC,
    a check will be made to see if they are on the annuaire using the IAP
    annuaire lines, if one line exists, the identifier is set to that, if
    more than one line exists, there is no way for us to know which one
    belongs to that partner, so we prompt the user to go to the partner's
    settings and choose the correct identifier
    task-id-6327357
    
    X-original-commit: 73dce9e5f11ded51e3dc93a3d6293191958b3944

--- addons/l10n_fr_pdp/wizard/account_move_send_wizard.py
+++ addons/l10n_fr_pdp/wizard/account_move_send_wizard.py
@@ -1,4 +1,4 @@
-from odoo import models
+from odoo import api, models
 
 
 class AccountMoveSendWizard(models.TransientModel):
@@ -32,3 +32,72 @@ class AccountMoveSendWizard(models.TransientModel):
         if reason:
             return f" ({reason})"
         return ""
+
+    # -------------------------------------------------------------------------
+    # COMPUTES
+    # -------------------------------------------------------------------------
+
+    def _compute_sending_method_checkboxes(self):
+        # EXTENDS 'account'
+
+        multiple_lines_records = self.env['account.move.send.wizard']
+
+        for wizard in self:
+            move = wizard.move_id
+            partner = move.partner_id.commercial_partner_id
+
+            if (
+                not move or move.company_id._get_peppol_proxy_type() != 'pdp'
+                or (partner.peppol_eas == '0225' and partner.peppol_endpoint)
+                or not (siren := partner._l10n_fr_pdp_get_siren())
+            ):
+                continue
+
+            lookup_result = self.env['res.partner']._fetch_active_annuaire_lines(siren)
+            if line_count := lookup_result.get('count', 0):
+                if line_count == 1:
+                    partner.write({
+                        'peppol_eas': '0225',
+                        'peppol_endpoint': lookup_result.get('identifiers')[0],
+                    })
+                elif line_count > 1:
+                    multiple_lines_records |= wizard
+
+        super()._compute_sending_method_checkboxes()
+
+        # For a given SIREN, if more than one line exists on the annuaire, warn the user to go the partner's settings and choose
+        # the correct identifier as there is no way to know which line belongs to the partner, and disable the checkbox to send via peppol
+        for wizard in multiple_lines_records:
+            checkboxes = wizard.sending_method_checkboxes
+            if 'peppol' in checkboxes:
+                checkboxes['peppol'].update({
+                    'checked': False,
+                    'readonly': True,
+                    'disabled': True,
+                    'l10n_fr_pdp_ambiguous': True,
+                })
+                wizard.sending_method_checkboxes = checkboxes
+
+    @api.depends('sending_method_checkboxes')
+    def _compute_alerts(self):
+        # EXTENDS 'account'
+        super()._compute_alerts()
+
+        for wizard in self:
+            peppol_box = (wizard.sending_method_checkboxes or {}).get('peppol', {})
+            partner = wizard.move_id.partner_id.commercial_partner_id
+
+            if peppol_box.get('l10n_fr_pdp_ambiguous'):
+                new_alerts = wizard.alerts if wizard.alerts else {}
+
+                new_alerts['l10n_fr_pdp_ambiguous_annuaire'] = {
+                    'level': 'warning',
+                    'message': self.env._(
+                        "Multiple active registrations were found in the French directory for %(partner)s. "
+                        "Please select the correct e-invoicing identifier before sending.",
+                        partner=partner.display_name
+                    ),
+                    'action_text': self.env._("Open Partner Settings"),
+                    'action': partner._get_records_action(),
+                }
+                wizard.alerts = new_alerts
