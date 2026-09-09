96ab00e597d90c67133d583586038f9d8ede77e8
Julien Mougenot <jum@odoo.com>
2026-09-09 17:39:25 +0200

    [REF] loyalty: convert static props to useProps
    Replaced deprecated uses of `static props` with calls to `useProps()`,
    providing actual props validation for Owl components.
    
    Whenever possible, also removed "empty" props definition, being either:
    - `static props = ["*"]` with no use of `this.props`;
    - `static props = {}`.
    
    Part-of: odoo/odoo#287603
    Signed-off-by: Géry Debongnie <ged@odoo.com>

--- addons/loyalty/static/src/js/portal/loyalty_card_dialog/loyalty_card_dialog.js
+++ addons/loyalty/static/src/js/portal/loyalty_card_dialog/loyalty_card_dialog.js
@@ -1,10 +1,29 @@
-import { Component } from '@odoo/owl';
+import { Component, t, useProps } from '@odoo/owl';
 import { Dialog } from '@web/core/dialog/dialog';
 
 export class PortalLoyaltyCardDialog extends Component {
     static components = { Dialog };
     static template = 'loyalty.portal_loyalty_card_dialog';
-    static props = ['*'];
+    props = useProps({
+        close: t.function(),
+        img_path: t.string(),
+        program: t.object({
+            program_name: t.string(),
+        }),
+        card: t.object({
+            points_display: t.any(),
+            expiration_date: t.any().optional(),
+            id: t.any(),
+        }),
+        history_lines: t.array(
+            t.object({
+                order_id: t.any().optional(),
+                order_portal_url: t.any().optional(),
+                description: t.any(),
+                points: t.any(),
+            })
+        ),
+    });
 
     setup() {
         this.csrf_token = odoo.csrf_token;
