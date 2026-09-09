357a09b57e91395b63e4321610a0b208fe76d3b9
Laetitia (ldau) <ldau@odoo.com>
2026-09-09 13:50:01 +0200

    [IMP] payment, product: adapt card's elements
    This commit adjusts the image border-radius in the payment section to
    match the card's, and standardizes the vertical and horizontal padding
    for "simple" cards containing both text and an image.
    
    task-6519210

--- addons/product/__manifest__.py
+++ addons/product/__manifest__.py
@@ -70,6 +70,7 @@ Print product labels with barcode.
             'product/static/src/product_catalog/**/*.js',
             'product/static/src/product_catalog/**/*.xml',
             'product/static/src/product_catalog/**/*.scss',
+            'product/static/src/scss/product_template_views.scss',
         ],
         'web.report_assets_common': [
             'product/static/src/scss/report_label_sheet.scss',
