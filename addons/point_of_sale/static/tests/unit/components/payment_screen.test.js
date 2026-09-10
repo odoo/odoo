14982f710914d8e6315a3d3646fe386b5e538a29
Robin Engels (roen) <roen@odoo.com>
2026-09-10 16:10:14 +0200

    [FIX] point_of_sale: force invoice caba pay_later
    When selling a product with a cash basis tax and using a pay later
    payment method. The accounting entry in the session would already have
    a Tax Grid set even if the order was not paid already. But the tax grid
    should only be set when the order is paid.
    
    Steps to reproduce:
    -------------------
    * Install PoS and the French accounting localization
    * Create a product that uses the 20% S tax
    * Open a PoS session and add the product to the order
    * Go to the payment screen and select the customer account method
    * Validate the order and close the session
    * Go to the session journal items
    > Observation: The journal items already has a tax grid even if the
      order is not yet really paid.
    
    Why the fix:
    ------------
    To fix this issue, we need to force the invoice so that separated
    journal items are created for this specific order. This way, the tax
    grid and caba entries are only created when the invoice is actually
    paid.
    
    opw-6451898

--- addons/point_of_sale/static/tests/unit/components/payment_screen.test.js
+++ addons/point_of_sale/static/tests/unit/components/payment_screen.test.js
@@ -4,6 +4,7 @@ import { setupPosEnv, getFilledOrder, expectFormattedPrice } from "../utils";
 import { definePosModels } from "../data/generate_model_definitions";
 import { queryOne } from "@odoo/hoot-dom";
 import { PaymentScreen } from "@point_of_sale/app/screens/payment_screen/payment_screen";
+import OrderPaymentValidation from "@point_of_sale/app/utils/order_payment_validation";
 
 definePosModels();
 
@@ -61,4 +62,33 @@ test("Do not print stock report if not configured", async () => {
     await comp.addNewPaymentLine(firstPm);
     await comp.validateOrder();
     expect(order.picking_type_id.has_stock_reports_to_print).toBeEmpty();
+});
+
+test("Caba pay later always invoice", async () => {
+    const store = await setupPosEnv();
+    const order = await getFilledOrder(store);
+    const existingPartner = store.models["res.partner"].get(3);
+    order.partner_id = existingPartner;
+    const customerAccount = store.models["pos.payment.method"].get(3);
+    const tax = store.models["account.tax"].get(1);
+    tax.update({ tax_exigibility: "on_payment" });
+    order.config.iface_tax_included = "total";
+    const comp = await mountWithCleanup(PaymentScreen, {
+        props: { orderUuid: order.uuid },
+    });
+    expect(order.isToInvoice()).toBe(false);
+    await comp.addNewPaymentLine(customerAccount);
+    expect(order.isToInvoice()).toBe(true);
+
+    const validation = new OrderPaymentValidation({ pos: store, orderUuid: order.uuid });
+    comp.toggleIsToInvoice();
+    expect(order.isToInvoice()).toBe(false);
+    const res = await validation.isOrderValid();
+    expect(res).toBe(false);
+
+    const validation2 = new OrderPaymentValidation({ pos: store, orderUuid: order.uuid });
+    comp.toggleIsToInvoice();
+    expect(order.isToInvoice()).toBe(true);
+    const res2 = await validation2.isOrderValid();
+    expect(res2).toBe(true);
 });
