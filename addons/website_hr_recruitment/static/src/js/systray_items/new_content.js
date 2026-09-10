1792bd7c633871aa740db4b3526d8f8eb2095cf1
fdardenne <dafl@odoo.com>
2026-09-10 17:39:30 +0200

    [FIX] website_hr_recruitment: warn on company mismatch before creating job
    - With Mitchell admin only select "My belgian company"
    - Go to website on My Website (My Company website-
    - New -> Job Offer
    - Modify -> Save
    - Access right crash
    
    Current Behaviour
    It created a record regardless of the user active company.
    If that company wasn't among the user's currently active companies,
    the immediate read-back (redirect into edit mode) hit the multi-company
    record rule and threw a generic Access Error.
    
    Now:
    Check the mismatch upfront and show a clear, actionable message
    instead of letting the record get created and fail on read.
    
    closes odoo/odoo#287692
    
    Related: odoo/enterprise#131147
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- addons/website_hr_recruitment/static/src/js/systray_items/new_content.js
+++ addons/website_hr_recruitment/static/src/js/systray_items/new_content.js
@@ -4,6 +4,9 @@ import {
 } from "@website/client_actions/website_preview/new_content_systray_item";
 import { rpc } from "@web/core/network/rpc";
 import { patch } from "@web/core/utils/patch";
+import { user } from "@web/core/user";
+import { _t } from "@web/core/l10n/translation";
+import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 
 patch(NewContentSystrayItem.prototype, {
     setup() {
@@ -15,7 +18,25 @@ patch(NewContentSystrayItem.prototype, {
         newJobElement.model = 'hr.job';
     },
 
+    canCreateJobOnCurrentWebsite() {
+        const websiteCompanyId = this.website.currentWebsite.company_id;
+        if (!websiteCompanyId || user.activeCompanies.some((c) => c.id === websiteCompanyId)) {
+            return true;
+        }
+        const websiteCompany = user.allowedCompanies.find((c) => c.id === websiteCompanyId);
+        this.dialogs.add(AlertDialog, {
+            title: _t("Company Mismatch"),
+            body: websiteCompany
+                ? _t("This website belongs to %(company)s, which isn't one of your active companies. Switch to it first, then try again.", { company: websiteCompany.name })
+                : _t("This website belongs to a company you don't have access to. Ask your administrator for access before creating a Job Position here."),
+        });
+        return false;
+    },
+
     async createNewJob() {
+        if (!this.canCreateJobOnCurrentWebsite()) {
+            return;
+        }
         const url = await rpc('/jobs/add');
         this.website.goToWebsite({ path: url, edition: true });
     },
