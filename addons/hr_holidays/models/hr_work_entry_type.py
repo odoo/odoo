c3b006b44775bdd215b29cfc0774ad8e9f40a2a7
Quentin Colla (qucol) <qucol@odoo.com>
2026-09-10 17:08:45 +0200

    [FIX] hr_holidays: rename time off (allocation) requests options
    ## Issue
    Currently, the different options for the approval of a time off (allocation)
    request are the following:
    1. None needed
    2. By Time Off Officer
    3. By Employee's Approver
    4. By Employee's Approver and Time Off Officer
    
    On the Employee's view form, in the Settings tab, the approvers are named:
    1. HR Responsible
    2. Time Off
    
    The *By Time Off Officer* option does not notify the *Time Off* approver,
    but the *HR Responsible*, which is not clear for the users. It would be
    clearer to use a similar naming convention for both the Approvers on the
    Employee's form and the options for the approval of the time off (allocation)
    requests.
    
    ## Steps to reproduce
    1. Install *Time Off* (`hr_holidays`)
    2. For an Employee E, set an HR Responsible and a (different) Time Off
    approvers
    3. Create a Time Off request for the Employee E
        - Ensure that the selected Time Off Type has its approval method set
        to "By Time Off Officer"
    4. **The user notified off the time off request is the HR Responsible,
    which is unexpected, considering that we expect an approval from the
    "Time Off Officer".**
    
    ## Justification
    In the `_get_responsible_for_approval` method, when using the "By Time
    Off Officer" option (`'hr'`), it is clearly the `hr_responsible_id`
    (HR Responsible) who is selected as "responsible" for the approval.
    
    https://github.com/odoo/odoo/blob/dbed91769c48768f58b6a66bff0ed6d90394de38/addons/hr_holidays/models/hr_leave.py#L1557-L1559
    
    The exact same logic is applied for the allocation requests [here](https://github.com/odoo/odoo/blob/dbed91769c48768f58b6a66bff0ed6d90394de38/addons/hr_holidays/models/hr_leave_allocation.py#L1070-L1072).
    
    ## Targeted version
    We are targeting version 19.2, as it is the version from which the
    `responsible_ids` field (which was clearly specifying who had to be
    notified for the approval of a time off request) was removed
    (see https://github.com/odoo/odoo/commit/d37cf89a6ff134988b4a7c001a97973cd95a2ea8).
    
    If deemed interesting, the same modification can be backported to previous
    versions too.
    
    opw-6535827
    
    closes odoo/odoo#287686
    
    Signed-off-by: Mélanie Peyrat (mepe) <mepe@odoo.com>

--- addons/hr_holidays/models/hr_work_entry_type.py
+++ addons/hr_holidays/models/hr_work_entry_type.py
@@ -67,18 +67,18 @@ class HrWorkEntryType(models.Model):
     country_code = fields.Char(related='country_id.code', depends=['country_id'], readonly=True)
     leave_validation_type = fields.Selection([
         ('no_validation', 'None needed'),
-        ('hr', 'By Time Off Officer'),
-        ('manager', "By Employee's Approver"),
-        ('both', "By Employee's Approver and Time Off Officer")], default='hr', string='Time Off Validation')
+        ('hr', 'By HR Responsible'),
+        ('manager', "By Time Off Approver"),
+        ('both', "By HR Responsible and Time Off Approver")], default='hr', string='Time Off Validation')
     requires_allocation = fields.Boolean(default=True, required=True, string='Requires Allocation')
     employee_requests = fields.Boolean(default=False, required=True, string="Allow Employee Requests",
         help="""Extra Days Requests Allowed: User can request an allocation for himself.\n
         Not Allowed: User cannot request an allocation.""")
     allocation_validation_type = fields.Selection([
         ('no_validation', 'None needed'),
-        ('hr', 'By Time Off Officer'),
-        ('manager', "By Employee's Approver"),
-        ('both', "By Employee's Approver and Time Off Officer")], default='hr', string='Approval',
+        ('hr', 'By HR Responsible'),
+        ('manager', "By Time Off Approver"),
+        ('both', "By HR Responsible and Time Off Approver")], default='hr', string='Approval',
         help="""Select the level of approval needed in case of request by employee
             #     - No validation needed: The employee's request is automatically approved.
             #     - Approved by Time Off Officer: The employee's request need to be manually approved
