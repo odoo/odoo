83f322ce3241373384a21c99b969cf15759081ef
Aaron Bohy <aab@odoo.com>
2026-09-10 08:28:54 +0000

    [FIX] web: don't autosave form while editing a x2many list row
    The form view autosaves on 'visibilitychange' (e.g. when the user
    switches tab/app) to avoid losing unsaved changes. On mobile, opening
    the native file picker for a binary field also fires 'visibilitychange',
    which triggered this autosave.
    
    When that binary field was part of an editable x2many list, the
    autosave forced the row out of edition before the file could be
    selected, silently discarding the edition in progress.
    
    Skip the autosave when a x2many field of the root record currently has
    a row in edition.
    
    Can be reproduced in eLearning > course > content > Additional
    Resources, on mobile devices (must force "desktop mode" in the
    browser), and on tablets.
    
    opw~6517735

--- addons/web/static/src/views/form/form_controller.js
+++ addons/web/static/src/views/form/form_controller.js
@@ -481,8 +481,17 @@ export class FormController extends Component {
     }
 
     beforeVisibilityChange() {
-        if (document.visibilityState === "hidden" && this.formInDialog === 0) {
-            return this.model.root.save();
+        const root = this.model.root;
+        const isEditingX2Many = Object.keys(root.activeFields).some((fieldName) => {
+            const field = root.fields[fieldName];
+            return (
+                ["one2many", "many2many"].includes(field.type) &&
+                !field.relatedPropertyField &&
+                root.data[fieldName].editedRecord
+            );
+        });
+        if (document.visibilityState === "hidden" && this.formInDialog === 0 && !isEditingX2Many) {
+            return root.save();
         }
     }
 
