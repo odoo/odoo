a7d9dedcc02c5c9535a6b006c7a391962db83aa0
khaj-odoo <khaj@odoo.com>
2026-09-10 19:26:33 +0530

    [IMP] sale(_margin): auto-install margins
    Margins were gated behind a settings toggle even though sale_margin only
    depends on sale_management, which every quotation flow already requires.
    Mark sale_margin auto_install so it always installs alongside
    sale_management, and dropping the "Margins" setting.
    
    task-6255022
    
    closes odoo/odoo#287669
    
    Related: odoo/documentation#20022
    Related: odoo/upgrade#11293
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- addons/sale_margin/__manifest__.py
+++ addons/sale_margin/__manifest__.py
@@ -17,6 +17,7 @@ Price and Cost Price.
         "views/sale_order_template_views.xml",
         "report/sale_report_views.xml",
     ],
+    "auto_install": True,
     "author": "Odoo S.A.",
     "license": "LGPL-3",
 }
