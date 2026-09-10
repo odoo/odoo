8035ca1c9527e3f893e0efc7056aa3a8515a0044
Alex Kühn <aku@odoo.com>
2026-09-10 11:41:39 +0200

    [FIX] mail: fix discuss border in dark theme
    Before this commit, Discuss borders in dark theme were very light.
    
    This happens because Discuss used border-secondary as this provided
    the consistent look between white and dark theme, which the default
    border didn't: the default border was too distracting in dark theme,
    making the UI look bloated.
    
    With frost, the default border now look consistent between white and
    dark themes. A recent PR changed border-secondary to be much more
    visible in dark theme [1], which goes against the intended look in
    Discuss.
    
    This commit fixes the issue by removing most border-secondary in
    Discuss UI, relying on default border that gives the intended look.
    
    The recent PR [1] also impacted:
    
    - the livechat "disconnect" banner bg color, which made it barely
    readable in dark theme. This is fixed in this commit by using
    `.alert` like the unread message banner.
    
    - the poll secondary color, in both white and dark theme. This commit
    reduce the opacity so this made secondary compared to o-action and
    primary colors as intended.
    
    [1]: https://github.com/odoo/enterprise/pull/129792

--- addons/mail/static/src/core/common/store_service.js
+++ addons/mail/static/src/core/common/store_service.js
@@ -193,7 +193,7 @@ export class Store extends BaseStore {
     discussDropdownMenuClass(ctx) {
         const simulateDarkTheme = this.shouldSimulateDarkTheme(ctx);
         return attClassObjectToString({
-            "o-discuss-dropdownMenu d-flex flex-column border-secondary px-1": true,
+            "o-discuss-dropdownMenu d-flex flex-column px-1": true,
             "o-simulateDarkTheme": simulateDarkTheme,
         });
     }
@@ -508,10 +508,7 @@ export class Store extends BaseStore {
                 // Ignore invalid URLs
                 return false;
             }
-            if (
-                location.host === url.host &&
-                location.pathname.startsWith("/odoo")
-            ) {
+            if (location.host === url.host && location.pathname.startsWith("/odoo")) {
                 this.ChatWindow.get({ channel: thread.channel })?.fold();
             }
         }
