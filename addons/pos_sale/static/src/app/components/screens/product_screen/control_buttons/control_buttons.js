eeb776af3a7d15cd1a8f45422cdaa74c7551d7e7
Traina Ludo <ltra@odoo.com>
2026-09-10 17:28:47 +0200

    [FIX] pos_sale: allow removing the quotation partner filter
    When a customer was selected before opening the quotations/orders view,
    a default partner filter was applied.
    
    However, the partner was also added directly to the domain. Therefore,
    removing the partner filter from the search bar had no effect, as the
    domain continued to restrict the records to that partner.
    
    We now only use the default search filter so that users can remove it
    and display all available quotations/orders.
    
    ---
    
    Task: https://www.odoo.com/odoo/project/1737/tasks/6537521

--- addons/pos_sale/static/src/app/components/screens/product_screen/control_buttons/control_buttons.js
+++ addons/pos_sale/static/src/app/components/screens/product_screen/control_buttons/control_buttons.js
@@ -15,12 +15,7 @@ patch(ControlButtons.prototype, {
             ["currency_id", "=", this.pos.currency.id],
             ["amount_unpaid", ">", 0],
         ];
-        if (this.pos.getOrder()?.getPartner()) {
-            domain = [
-                ...domain,
-                ["partner_id", "any", [["id", "child_of", [this.pos.getOrder().getPartner().id]]]],
-            ];
-        }
+
         const saleOrderIds = this.pos
             .getOrder()
             ?.lines?.map((l) => l.sale_order_origin_id?.id)
