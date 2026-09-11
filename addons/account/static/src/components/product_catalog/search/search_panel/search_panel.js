5f549e6c90fc9d11afcdfc1515a06f7d3e8b298f
Carmine <csan@odoo.com>
2026-09-11 15:01:00 +0200

    [IMP] account, product: add mobile drawer for product catalog search panel
    The product catalog search panel used the generic responsive
    `web.SearchPanel.Small` layout on mobile, which is cramped and hard to
    use on a small screen.
    
    ProductCatalogSearchPanel now renders a dedicated mobile drawer
    (product.SearchPanel.MobileDrawer) instead of the default responsive
    layout. account.ProductCatalogSearchPanel now extends the new base
    class instead of the generic web SearchPanel.
    
    task-5270010

--- addons/account/static/src/components/product_catalog/search/search_panel/search_panel.js
+++ addons/account/static/src/components/product_catalog/search/search_panel/search_panel.js
@@ -1,15 +1,15 @@
-import { onWillStart, proxy, signal } from "@odoo/owl";
+import { proxy, signal } from "@odoo/owl";
 import { formatCurrency } from "@web/core/currency";
 import { getActiveHotkey } from "@web/core/hotkeys/hotkey_utils";
 import { useBus, useService } from "@web/core/utils/hooks";
 import { useNestedSortable } from "@web/core/utils/nested_sortable";
 import { useSubEnv } from "@web/owl2/utils";
-import { SearchPanel } from "@web/search/search_panel/search_panel";
+import { ProductCatalogSearchPanel } from "@product/product_catalog/product_catalog_search_panel";
 import { SectionRow } from "../section_row/section_row";
 
-export class AccountProductCatalogSearchPanel extends SearchPanel {
+export class AccountProductCatalogSearchPanel extends ProductCatalogSearchPanel {
     static template = "account.ProductCatalogSearchPanel";
-    static components = { ...SearchPanel.components, SectionRow };
+    static components = { ...ProductCatalogSearchPanel.components, SectionRow };
 
     sectionTreeRef = signal.ref();
 
@@ -54,24 +54,11 @@ export class AccountProductCatalogSearchPanel extends SearchPanel {
             this.updateSectionSubtotal(detail.sectionId, detail.subtotalDelta);
         });
 
-        onWillStart(async () => {
-            const { name, sections, amountUntaxed } = await this.orm.call(
-                this.orderModel,
-                "get_catalog_section_data",
-                [this.orderId],
-                { child_field: this.childField }
-            );
-
-            this.orderName = name;
-            if (this.showPrices) {
-                this.state.totalUntaxedAmount = amountUntaxed;
-            }
-
-            this._setSectionsState(sections);
-            if (this.state.sections.length) {
-                this.setSelectedSection(this.state.sections[0].id);
-            }
-        });
+        this.orderName = this.env.searchModel.catalogOrderDetails.name;
+        if (this.showPrices) {
+            this.state.totalUntaxedAmount = this.env.searchModel.catalogOrderDetails.amount_untaxed;
+        }
+        this._setSectionsState(this.env.searchModel.catalogSections);
 
         useNestedSortable({
             ref: this.sectionTreeRef,
@@ -401,6 +388,12 @@ export class AccountProductCatalogSearchPanel extends SearchPanel {
             } else {
                 rootSections.push(node);
             }
+        }
+
+        const selectedSection = sectionsById.get(this.selectedSectionId);
+        if (selectedSection?.parent_id) {
+            // Expand the parent so the initially selected subsection is visible.
+            sectionsById.get(selectedSection.parent_id).isOpen = true;
         }
 
         this.state.sections = rootSections;
