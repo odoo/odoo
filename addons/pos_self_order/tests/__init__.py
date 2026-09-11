b8de7abfb2758711612bf020cd11e21d58f2d559
manv-afk <manv@odoo.com>
2026-09-11 09:20:04 +0200

    [IMP] wip dup order_label

--- addons/pos_self_order/tests/__init__.py
+++ addons/pos_self_order/tests/__init__.py
@@ -16,3 +16,4 @@ from . import test_self_order_payment_method
 from . import test_takeaway_preset_mail
 from . import test_self_order_prices
 from . import test_self_order_data_loading
+from . import test_order_receipt
