d58ff507e55489d538ba590869fcb34d5443adfb
Sébastien Theys <seb@odoo.com>
2026-09-11 15:24:00 +0000

    [FIX] web: cancel the pending search on autocomplete select
    Before this commit, the autocomplete dropdown of a many2one could open
    again right after a suggestion was picked, and stay open. The step of
    mail_template_dynamic_placeholder_tour waiting for it to close then
    times out:
    
        FAILED: [8/36] Tour mail_template_dynamic_placeholder_tour
        Step Wait for the drop down to disappear
        (trigger: div[name="model_id"] .o-autocomplete:not(:has(.ui-autocomplete)))
        TIMEOUT step failed to complete within 10000 ms.
    
    This happens because the search filling the dropdown runs 250ms after
    the last keystroke, so a suggestion of the previous search can be
    picked while the next search is still scheduled. Picking a suggestion
    only closes the dropdown. The scheduled search then runs, opens the
    dropdown on the selected value, and nothing closes it after that.
    
    This commit fixes the issue by discarding the scheduled search when a
    suggestion is picked.
    
    https://runbot.odoo.com/odoo/error/233574
    https://runbot.odoo.com/odoo/error/237744
    https://runbot.odoo.com/odoo/error/944454
    https://runbot.odoo.com/odoo/error/945517
    https://runbot.odoo.com/odoo/error/947141
    
    closes odoo/odoo#290413
    
    X-original-commit: dc286719ce7e199bebf994702351e37809ca662c
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- addons/web/static/src/core/autocomplete/autocomplete.js
+++ addons/web/static/src/core/autocomplete/autocomplete.js
@@ -185,6 +185,15 @@ export class AutoComplete extends Component {
         this.mouseSelectionActive = false;
     }
 
+    discardPendingInput() {
+        this.debouncedProcessInput.cancel();
+        this.pendingPromise?.resolve();
+        if (this.loadingPromise === this.pendingPromise) {
+            this.loadingPromise = null;
+        }
+        this.pendingPromise = null;
+    }
+
     cancel() {
         if (this.inputRef.el.value.length) {
             if (this.props.autoSelect) {
@@ -263,6 +272,7 @@ export class AutoComplete extends Component {
 
     selectOption(option) {
         this.inEdition = false;
+        this.discardPendingInput();
         if (option.unselectable) {
             return;
         }
