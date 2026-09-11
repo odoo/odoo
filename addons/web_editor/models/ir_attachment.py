52d2fbbc90d9cc351658e03bc19178985408d31c
Mahdi Alijani (malj) <malj@odoo.com>
2026-09-11 10:18:57 +0200

    [FIX] web_editor, website: fix attachment model mismatch on image copy
    When an attachment tied to one record was copied while editing
    content linked to a different one, the duplicate could keep
    referencing the original model instead of the new target, leaving
    the two out of sync until the next save.
    
    opw-6560233
    
    closes odoo/odoo#287784
    
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/web_editor/models/ir_attachment.py
+++ addons/web_editor/models/ir_attachment.py
@@ -84,3 +84,6 @@ class IrAttachment(models.Model):
         - Non admin user uploading an unsplash image (bypass binary/url check)
         """
         return False
+
+    def _get_mimetype_exempt_groups(self):
+        return []
