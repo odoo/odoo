7b0473c8d13add35acfdc92b3dc96ee59189aded
Youssef Bashandy <yoba@odoo.com>
2026-09-14 12:33:02 +0000

    [FIX] hr : Refactor work_contact_id computation
    Ensure correct access to avoid unexpected behavior
    
    opw-6560194
    
    X-original-commit: 2df25c68396510abdb85f9b94ae0ba73f8cb340d

--- addons/hr/tests/common.py
+++ addons/hr/tests/common.py
@@ -11,4 +11,4 @@ class TestHrCommon(common.TransactionCase):
     def setUpClass(cls):
         super(TestHrCommon, cls).setUpClass()
 
-        cls.res_users_hr_officer = mail_new_test_user(cls.env, login='hro', groups='base.group_user,hr.group_hr_user', name='HR Officer', email='hro@example.com')
+        cls.res_users_hr_officer = mail_new_test_user(cls.env, login='hro', groups='base.group_user,hr.group_hr_user,base.group_partner_manager', name='HR Officer', email='hro@example.com')
