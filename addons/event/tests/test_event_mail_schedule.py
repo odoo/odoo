741a5e1e218f6c2282e9c4f576da7c6c911b4412
Thibault Delavallée <tde@odoo.com>
2026-09-14 09:28:49 +0200

    [FIX] various: update query counters
    Just update some counters, just to assert value for (upcoming) 20.0.

--- addons/event/tests/test_event_mail_schedule.py
+++ addons/event/tests/test_event_mail_schedule.py
@@ -737,7 +737,7 @@ class TestMailSchedule(EventMailCommon):
         # consider having hanging registrations, still not processed (e.g. adding
         # a new scheduler after)
         self.env.invalidate_all()
-        # event 19 - runbot 21
+        # event ?? - runbot 22
         # - event_crm: +4 (w demo) / +1 (no demo)
         # nightly: 31 :shrug:
         with self.assertQueryCount(31), self.mock_datetime_and_now(reference_now), \
@@ -762,7 +762,7 @@ class TestMailSchedule(EventMailCommon):
             }),
         ]})
         self.env.invalidate_all()
-        # event 49 - runbot 52, +3 with nightly demo (still to check)
+        # event ?? - runbot 50, +3 with nightly demo (still to check)
         # - event_crm: +2 (no demo)
         # nightly: 64 :shrugs
         with self.assertQueryCount(64), \
