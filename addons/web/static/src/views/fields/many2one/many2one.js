7532f95526364d519b7424ca59a252e0103c4908
Mahiv Ram <maram@odoo.com>
2026-09-14 11:02:33 +0000

    [FIX] web: don't open many2one dropdown on extra lines click
    Clicking on the informative extra text of a many2one field would
    focus the field and open the dropdown, making it inconvenient for
    the user to simply select or copy the text.
    
    This commit prevents the many2one dropdown from opening when the
    user clicks on the extra lines.
    
    Task-6538439
    
    closes odoo/odoo#289172
    
    X-original-commit: 99ea6ad9bd7960da01fb2fe12810ec5e186f4b08
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/src/views/fields/many2one/many2one.js
+++ addons/web/static/src/views/fields/many2one/many2one.js
@@ -216,12 +216,6 @@ export class Many2One extends Component {
         return `/odoo/${relation}/${this.props.value.id}`;
     }
 
-    onExtraLinesClick() {
-        this.rootRef()
-            ?.querySelector(this.props.readonly ? ".o_form_uri" : "input")
-            .click();
-    }
-
     async openBarcodeScanner() {
         const barcode = await BarcodeScanner.scanBarcode(this.env);
         if (barcode) {
