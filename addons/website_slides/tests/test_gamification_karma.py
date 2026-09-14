20472f81372f2987d012f23929f3aa0543847d39
Thibault Delavallée <tde@odoo.com>
2026-09-14 09:28:49 +0200

    [FIX] various: update query counters
    Just update some counters, just to assert value for (upcoming) 20.0.

--- addons/website_slides/tests/test_gamification_karma.py
+++ addons/website_slides/tests/test_gamification_karma.py
@@ -130,7 +130,7 @@ class TestKarmaGain(common.SlidesCase):
         self.assertEqual(len(channel_partners), 4)
 
         # Set courses as completed and update karma
-        with self.assertQueryCount(66):  # com 55
+        with self.assertQueryCount(63):  # com ??
             channel_partners._post_completion_update_hook()
 
         computed_karma = self.channel.karma_gen_channel_finish + self.channel_2.karma_gen_channel_finish
