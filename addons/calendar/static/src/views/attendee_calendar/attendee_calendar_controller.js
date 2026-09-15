alkuneemi/Luxara

[REF] calendar: rename calendar_form -> calendar_event_form
Since the calendar model is being introduced, the 'calendar_form' and related
files have misleading titles, since they deal with calendar events and not
calendars themselves

Task-5485938

addons/calendar/static/src/views/attendee_calendar/attendee_calendar_controller.js
@@ -4,13 +4,14 @@ import { CalendarController } from "@web/views/calendar/calendar_controller";
 import { user } from "@web/core/user";
 import { useService } from "@web/core/utils/hooks";
 import { onWillStart } from "@odoo/owl";
-import { CalendarQuickCreate } from "@calendar/views/calendar_form/calendar_quick_create";
+import { CalendarEventQuickCreate } from "@calendar/views/calendar_form/calendar_event_quick_create";
+
 export class AttendeeCalendarController extends CalendarController {
     static template = "calendar.AttendeeCalendarController";
     static components = {
         ...AttendeeCalendarController.components,
         CalendarSidePanel: AttendeeCalendarSidePanel,
-        QuickCreateFormView: CalendarQuickCreate,
+        QuickCreateFormView: CalendarEventQuickCreate,
     };
 
     setup() {

addons/calendar/static/src/views/calendar_form/calendar_event_quick_create.js
@@ -1,7 +1,7 @@
 import { registry } from "@web/core/registry";
 import { FormViewDialog } from "@web/views/view_dialogs/form_view_dialog";
-import { CalendarFormView } from "./calendar_form_view";
-import { CalendarFormController } from "./calendar_form_controller";
+import { CalendarEventFormView } from "./calendar_event_form_view";
+import { CalendarEventFormController } from "./calendar_event_form_controller";
 import { serializeDate, serializeDateTime } from "@web/core/l10n/dates";
 
 export const QUICK_CREATE_CALENDAR_EVENT_FIELDS = {
@@ -37,7 +37,7 @@ function getDefaultValuesFromRecord(data) {
     return context;
 }
 
-export class CalendarQuickCreateFormController extends CalendarFormController {
+export class CalendarEventQuickCreateFormController extends CalendarEventFormController {
 
     goToFullEvent() {
         const context = getDefaultValuesFromRecord(this.model.root.data);
@@ -73,18 +73,18 @@ export class CalendarQuickCreateFormController extends CalendarFormController {
     }
 }
 
-registry.category("views").add("calendar_quick_create_form_view", {
-    ...CalendarFormView,
-    Controller: CalendarQuickCreateFormController,
+registry.category("views").add("calendar_event_quick_create_form_view", {
+    ...CalendarEventFormView,
+    Controller: CalendarEventQuickCreateFormController,
 });
 
-export class CalendarQuickCreate extends FormViewDialog {
+export class CalendarEventQuickCreate extends FormViewDialog {
 
     setup() {
         super.setup();
         Object.assign(this.viewProps, {
             ...this.viewProps,
-            buttonDialogTemplate: "calendar.CalendarQuickCreateButtons",
+            buttonDialogTemplate: "calendar.CalendarEventQuickCreateButtons",
         });
     }
 }

odoo/odoo

addons/calendar/static/src/views/attendee_calendar/attendee_calendar_controller.js
@@ -6,13 +6,14 @@ import { useArchiveOrUnlinkCalendarEvent } from "@calendar/views/hooks";
 import { user } from "@web/core/user";
 import { useService } from "@web/core/utils/hooks";
 import { onWillStart } from "@odoo/owl";
-import { CalendarQuickCreate } from "@calendar/views/calendar_form/calendar_quick_create";
+import { CalendarEventQuickCreateFormViewDialog } from "@calendar/views/calendar_form/calendar_event_quick_create";
+
 export class AttendeeCalendarController extends CalendarController {
     static template = "calendar.AttendeeCalendarController";
     static components = {
         ...AttendeeCalendarController.components,
         CalendarSidePanel: AttendeeCalendarSidePanel,
-        QuickCreateFormView: CalendarQuickCreate,
+        QuickCreateFormView: CalendarEventQuickCreateFormViewDialog,
     };
 
     setup() {

addons/calendar/static/src/views/calendar_form/calendar_event_quick_create.js
@@ -1,7 +1,7 @@
 import { registry } from "@web/core/registry";
 import { FormViewDialog } from "@web/views/view_dialogs/form_view_dialog";
-import { CalendarFormView } from "./calendar_form_view";
-import { CalendarFormController } from "./calendar_form_controller";
+import { CalendarEventFormView } from "./calendar_event_form_view";
+import { CalendarEventFormController } from "./calendar_event_form_controller";
 import { serializeDate, serializeDateTime } from "@web/core/l10n/dates";
 
 export const QUICK_CREATE_CALENDAR_EVENT_FIELDS = {
@@ -39,7 +39,7 @@ function getDefaultValuesFromRecord(data) {
     return context;
 }
 
-export class CalendarQuickCreateFormController extends CalendarFormController {
+export class CalendarEventQuickCreateFormController extends CalendarEventFormController {
 
     getFullEventContext() {
         const context = getDefaultValuesFromRecord(this.model.root.data);
@@ -82,18 +82,18 @@ export class CalendarQuickCreateFormController extends CalendarFormController {
     }
 }
 
-registry.category("views").add("calendar_quick_create_form_view", {
-    ...CalendarFormView,
-    Controller: CalendarQuickCreateFormController,
+registry.category("views").add("calendar_event_quick_create_form_view", {
+    ...CalendarEventFormView,
+    Controller: CalendarEventQuickCreateFormController,
 });
 
-export class CalendarQuickCreate extends FormViewDialog {
+export class CalendarEventQuickCreateFormViewDialog extends FormViewDialog {
 
     setup() {
         super.setup();
         Object.assign(this.viewProps, {
             ...this.viewProps,
-            buttonDialogTemplate: "calendar.CalendarQuickCreateButtons",
+            buttonDialogTemplate: "calendar.CalendarEventQuickCreateButtons",
         });
     }
 }