LAIDOUDI33/LAID_ODOO

[MOV] *,sale_(margin,management): merge sale_margin module and related modules
       * = [pos_sale,pos_sale_margin,sale_expense,sale_expense_margin,sale_project,
           sale_project_margin,sale_stock_margin,sale_timesheet,
           sale_timesheet_margin]

Sales Margin was a separate, opt-in module: users had to enable a
dedicated `Margins` setting before margin/cost fields ever appeared
on quotations, sales orders, or related reports. In practice this
is a basic, low-overhead feature that most sales flows benefit from having
available by default.

This merges sale_margin into sale_management so that margin
tracking is a native part of Sales, with no separate module and
no settings toggle to turn on first. Any app that integrates with
Sales (POS, Expenses, Project, Stock, Timesheets) gets its margin
support merged into its own bridge with Sales the same way, instead
of depending on a standalone *_margin module.

Bridge modules that existed purely to connect sale_margin to
another app are merged into that app's primary Sales bridge module, since there
is no longer a separate margin module to bridge to:
  - pos_sale_margin       -> pos_sale
  - sale_expense_margin   -> sale_expense
  - sale_project_margin   -> sale_project
  - sale_timesheet_margin -> sale_timesheet

Per review, everything is folded into the existing target files
instead of new *_margin-suffixed ones: sale_order_margin.py and
sale_order_line_margin.py into sale_order.py/sale_order_line.py,
sale_margin_views.xml merged into sale_order_form_quote/sale_order_views.xml
(including the form view, previously kept as a separate inheriting
record), the report views file moved to report/, demo data merged
and renamed, and every bridge module's margin test class folded
directly into its existing test class rather than kept as a
separate class. sale_stock_margin is renamed to sale_management_stock
(sale_stock doesn't depend on sale_management, so it stays a
separate bridge module, just without the stale nam

addons/sale_expense/tests/test_sale_expense.py
@@ -248,3 +248,77 @@ class TestSaleExpense(TestExpenseCommon, TestSaleCommon):
         self.assertTrue(invoice.attachment_ids.filtered(
             lambda attachment: attachment.name == 'manual_sales_price_receipt.txt'
         ))
+
+    def test_expense_reinvoice_purchase_price(self):
+        # re-invoiceable products
+        product_with_cost = self.product_a
+        product_with_cost.write({'standard_price': 1000, 'reinvoice_policy': 'sales_price'})
+        product_with_no_cost = self.product_c
+        product_with_no_cost.write({'reinvoice_policy': 'sales_price'})
+
+        # create SO line and confirm SO (with only one line)
+        sale_order = self.env['sale.order'].with_context(
+            mail_create_nolog=True,
+        ).sudo().create({
+            'partner_id': self.partner_a.id,
+            'partner_invoice_id': self.partner_a.id,
+            'partner_shipping_id': self.partner_a.id,
+            'order_line': [Command.create({
+                'name': product_with_cost.name,
+                'product_id': product_with_cost.id,
+                'product_uom_qty': 2.0,
+            })],
+        })
+
+        sale_order.action_confirm()
+
+        expense = self.create_expenses([
+            {
+                # expense with zero cost product, with 15% tax
+                'name': 'expense_1',
+                'date': '2020-10-07',
+                'product_id': product_with_no_cost.id,
+                'total_amount_currency': 100,
+                'tax_ids': [Command.set(self.company_data['default_tax_purchase'].ids)],
+                'sale_order_id': sale_order.id,
+            },
+            {
+                # expense with zero cost product, with no tax
+                'name': 'expense_2',
+                'date': '2020-10-07',
+                'product_id': product_with_no_cost.id,
+                'total_amount_currency': 100,
+                'tax_ids': False,
+                'sale_order_id': sale_order.id
+            },
+            {
+                # expense with product with cost (1000), with 15% tax
+                'name': 'expense_3',
+                'date': '2020-10-07',
+                'product_id': product_with_cost.id,
+                'quantity': 3,
+                'tax_ids': [Command.set(self.company_data['default_tax_purchase'].ids)],
+                'sale_order_id': sale_order.id
+            },
+            {
+                # expense with product with cost (1000), with no tax
+                'name': 'expense_4',
+                'date': '2020-10-07',
+                'product_id': product_with_cost.id,
+                'quantity': 5,
+                'tax_ids': False,
+                'sale_order_id': sale_order.id
+            },
+        ]).sorted('name')
+
+        expense.action_submit()
+        expense._do_approve()  # Skip duplicate wizard
+        self.post_expenses_with_wizard(expense)
+
+        self.assertAlmostEqual(sale_order.order_line[0].purchase_price, 1000.0)
+        self.assertFalse(sale_order.order_line[0].is_expense)
+
+        # Expense Lines
+        for line, expected_purchase_price in zip(sale_order.order_line[1:], [86.96, 100.0, 869.5666667, 1000.0]):
+            self.assertAlmostEqual(line.purchase_price, expected_purchase_price)
+            self.assertTrue(line.is_expense)

odoo/odoo

addons/sale_expense/tests/test_sale_expense.py
@@ -246,3 +246,77 @@ class TestSaleExpense(TestExpenseCommon, TestSaleCommon):
             {'name': new_attachments[1].name, 'checksum': new_attachments[1].checksum},
         ])
         self.assertFalse((attachments + new_attachments) & linked_attachments)
+
+    def test_expense_reinvoice_purchase_price(self):
+        # re-invoiceable products
+        product_with_cost = self.product_a
+        product_with_cost.write({'standard_price': 1000, 'reinvoice_policy': 'sales_price'})
+        product_with_no_cost = self.product_c
+        product_with_no_cost.write({'reinvoice_policy': 'sales_price'})
+
+        # create SO line and confirm SO (with only one line)
+        sale_order = self.env['sale.order'].with_context(
+            mail_create_nolog=True,
+        ).sudo().create({
+            'partner_id': self.partner_a.id,
+            'partner_invoice_id': self.partner_a.id,
+            'partner_shipping_id': self.partner_a.id,
+            'order_line': [Command.create({
+                'name': product_with_cost.name,
+                'product_id': product_with_cost.id,
+                'product_uom_qty': 2.0,
+            })],
+        })
+
+        sale_order.action_confirm()
+
+        expense = self.create_expenses([
+            {
+                # expense with zero cost product, with 15% tax
+                'name': 'expense_1',
+                'date': '2020-10-07',
+                'product_id': product_with_no_cost.id,
+                'total_amount_currency': 100,
+                'tax_ids': [Command.set(self.company_data['default_tax_purchase'].ids)],
+                'sale_order_id': sale_order.id,
+            },
+            {
+                # expense with zero cost product, with no tax
+                'name': 'expense_2',
+                'date': '2020-10-07',
+                'product_id': product_with_no_cost.id,
+                'total_amount_currency': 100,
+                'tax_ids': False,
+                'sale_order_id': sale_order.id
+            },
+            {
+                # expense with product with cost (1000), with 15% tax
+                'name': 'expense_3',
+                'date': '2020-10-07',
+                'product_id': product_with_cost.id,
+                'quantity': 3,
+                'tax_ids': [Command.set(self.company_data['default_tax_purchase'].ids)],
+                'sale_order_id': sale_order.id
+            },
+            {
+                # expense with product with cost (1000), with no tax
+                'name': 'expense_4',
+                'date': '2020-10-07',
+                'product_id': product_with_cost.id,
+                'quantity': 5,
+                'tax_ids': False,
+                'sale_order_id': sale_order.id
+            },
+        ]).sorted('name')
+
+        expense.action_submit()
+        expense._do_approve()  # Skip duplicate wizard
+        self.post_expenses(expense)
+
+        self.assertAlmostEqual(sale_order.order_line[0].purchase_price, 1000.0)
+        self.assertFalse(sale_order.order_line[0].is_expense)
+
+        # Expense Lines
+        for line, expected_purchase_price in zip(sale_order.order_line[1:], [86.96, 100.0, 869.5666667, 1000.0]):
+            self.assertAlmostEqual(line.purchase_price, expected_purchase_price)
+            self.assertTrue(line.is_expense)