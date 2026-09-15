9739b3b9677815906aa039a942b30167e281e23b
Lulu Grimalkin (lugr) <lugr@odoo.com>
2026-09-15 09:47:02 +0200

    [FIX] mrp: assign the MO's date to all of its finished moves
    Followup to a2ad5ba1, which describes this bug and fixes it
    incompletely, only covering zero-quantity/unconsumed moves instead of
    the finished ones (which get overwritten in `_post_inventory()`). This
    fix writes the set date value to all finished moves.
    
    This issue manifests as a heisenbug with the test
    `test_component_addition_to_finished_mo` nondeterministically breaking,
    as the fix and the test focused on the newly added component moves and
    didn't take the initial move into account.
    
    Task ID: 6226710
    
    closes odoo/odoo#288213
    
    Signed-off-by: Stéphane Diez (snd) <snd@odoo.com>

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -2428,11 +2428,12 @@ class MrpProduction(models.Model):
         # case the user edits the MO later on and sets some consumed quantity on those, we do not
         # want the move lines to be canceled.
         now = fields.Datetime.now()
-        (productions_not_to_backorder.move_raw_ids | productions_not_to_backorder.move_finished_ids).filtered(lambda x: x.state not in ('done', 'cancel')).write({
-            'date': now,
+        finished_moves = productions_not_to_backorder.move_raw_ids | productions_not_to_backorder.move_finished_ids
+        finished_moves.filtered(lambda x: x.state not in ('done', 'cancel')).write({
             'state': 'done',
             'product_uom_qty': 0.0,
         })
+        finished_moves.filtered(lambda x: x.state == 'done').date = now
         for production in self:
             production.write({
                 'date_finished': now,
