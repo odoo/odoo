898b534239f93c4d89ac04640464564c9e66a473
paan-odoo <paan@odoo.com>
2026-09-15 19:05:31 +0530

    [FIX] pos_*: secure payment status controller
    pos_*: point_of_sale, pos_razorpay, pos_self_order_razorpay
    
    Before this commit:
    
    - The public controller `/pos-self-order/razorpay-fetch-payment-status/` did not
      validate the Razorpay payment reference against the corresponding PoS order.
    - An attacker with a known `p2pRequestId` from a previously paid order could use
      it to validate a different self-order.
    - The controller could also return order data when called with an empty `payment_data`,
      a random order ID, and a valid configuration access token.
    
    After this commit:
    
    - Validate the Razorpay response `externalRefNumber` against the `referenceId`
      of the payment linked to the corresponding order, preventing a `p2pRequestId`
      from one order from being used to validate another order.
    - Require an `order_token` in `payment_data` and validate it against the order's
      `access_token`. This adds an additional validation layer and prevents unauthorized
      access to order data through the public controller.
    - Also changed the referenceId sent from the PoS and kiosk to the following format:
      `{order.config_id.id}/{order.uuid}/{self.id}/{order.currency_id.name}/{amount_before_decimal}`.
    
    Task-6527316

--- addons/pos_self_order_razorpay/models/pos_payment_method.py
+++ addons/pos_self_order_razorpay/models/pos_payment_method.py
@@ -1,4 +1,3 @@
-import uuid
 from odoo import models, api
 from odoo.osv import expression
 
@@ -9,10 +8,9 @@ class PosPaymentMethod(models.Model):
     def _payment_request_from_kiosk(self, order):
         if self.use_payment_terminal != 'razorpay':
             return super()._payment_request_from_kiosk(order)
-        reference_prefix = order.config_id.name.replace(' ', '')
         data = {
             'amount': order.amount_total,
-            'referenceId': f'{reference_prefix}/Order/{order.id}/{uuid.uuid4().hex}',
+            'referenceId': f'{order.config_id.id}/{order.uuid}/{self.id}/{order.currency_id.name}/{order.amount_total}',
         }
         return self.razorpay_make_payment_request(data)
 
