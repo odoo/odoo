a2708151f67da8c5dfe3b3623af728a7516e1a07
Jérémy Bazin (baje) <baje@odoo.com>
2026-09-15 09:40:45 +0200

    [IMP] l10n_fr: compute siren from vat
    Some French partners only have a VAT number, while electronic
    invoicing primarily identifies them through their SIREN.
    
    Derive the SIREN from the VAT so the 0225 EAS and PDP identifiers
    can be filled without requiring duplicate partner data.
    
    Task-ID : 6560466

--- addons/l10n_fr/models/res_partner.py
+++ addons/l10n_fr/models/res_partner.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import re
+
 from odoo import fields, models
+
+
+VAT_SIREN_RE = re.compile(r'FR[A-Z0-9]{2}([0-9]{9})', re.IGNORECASE)
 
 
 class ResPartner(models.Model):
@@ -17,3 +22,9 @@ class ResPartner(models.Model):
     def _peppol_eas_endpoint_depends(self):
         # extends account_edi_ubl_cii
         return super()._peppol_eas_endpoint_depends() + ['siret']
+
+    def _l10n_fr_get_siren_from_vat(self):
+        self.ensure_one()
+        vat = (self.vat or '').replace(' ', '')
+        match = VAT_SIREN_RE.fullmatch(vat)
+        return match.group(1) if match else False
