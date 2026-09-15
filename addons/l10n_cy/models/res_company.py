979e029d8da80e5ff3433acc87d157ce4003fbc3
Josse Colpaert <jco@odoo.com>
2026-09-15 18:03:41 +0200

    [IMP] l10n_*: forgot to add files

---
+++ addons/l10n_cy/models/res_company.py
@@ -0,0 +1,17 @@
+from odoo import models
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    def _get_default_vat_disabled_tax(self):
+        self.ensure_one()
+        if self.account_fiscal_country_id.code != 'CY':
+            return super()._get_default_vat_disabled_tax()
+        return self._get_or_create_chart_template_tax('VAT_S_NOT_SUBJECT_CY')
+
+    def _get_default_vat_disabled_purchase_tax(self):
+        self.ensure_one()
+        if self.account_fiscal_country_id.code != 'CY':
+            return super()._get_default_vat_disabled_purchase_tax()
+        return self._get_or_create_chart_template_tax('VAT_P_DISABLED_ND_CY')
---
+++ addons/l10n_lt/models/res_company.py
@@ -0,0 +1,17 @@
+from odoo import models
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    def _get_default_vat_disabled_tax(self):
+        self.ensure_one()
+        if self.account_fiscal_country_id.code != 'LT':
+            return super()._get_default_vat_disabled_tax()
+        return self._get_or_create_chart_template_tax('account_tax_template_sales_not_subject')
+
+    def _get_default_vat_disabled_purchase_tax(self):
+        self.ensure_one()
+        if self.account_fiscal_country_id.code != 'LT':
+            return super()._get_default_vat_disabled_purchase_tax()
+        return self._get_or_create_chart_template_tax('account_tax_template_purchase_not_deductible_21')
