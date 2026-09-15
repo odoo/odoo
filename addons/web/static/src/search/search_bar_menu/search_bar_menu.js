f5281cb3ad4626fa7882a3567cd5a6e182e71267
Aaron Bohy <aab@odoo.com>
2026-09-15 12:01:20 +0000

    [IMP] web: support date ranges in search view filters
    A date filter (`<filter date="date_start"/>`) filters on a single field: the
    periods it offers, as well as the relative ones introduced in [1], match the
    records that *start* in the searched period.
    
    For models describing a range rather than a single date, it is useful to be
    able to search for records that are ongoing during a given period.
    
    A date filter can now name the field ending that span:
    
        <filter string="Period" name="period" date="date_start" end_date="date_stop"/>
    
    Its periods and its relative options then match the records overlapping the
    searched period:
    
        ['&', ('date_start', '<=', period_end),
         '|', ('date_stop', '>=', period_start),
         '&', ('date_stop', '=', False), ('date_start', '>=', period_start)]
    
    A record with no end date ends when it starts. Each bound is serialized with
    the type of the field it is compared to, so that a date field and a datetime
    field can be paired.
    
    Task-6546268
    
    [1] https://github.com/odoo/odoo/pull/267980
    
    closes odoo/odoo#289619
    
    Related: odoo/enterprise#132421
    Related: odoo/documentation#20147
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web/static/src/search/search_bar_menu/search_bar_menu.js
+++ addons/web/static/src/search/search_bar_menu/search_bar_menu.js
@@ -18,6 +18,7 @@ import { _t } from "@web/core/l10n/translation";
 import { render } from "@web/owl2/utils";
 import { condition } from "@web/core/tree_editor/condition_tree";
 import { domainFromTree } from "@web/core/tree_editor/domain_from_tree";
+import { constructDateRange } from "@web/search/utils/dates";
 
 const favoriteMenuRegistry = registry.category("favoriteMenu");
 
@@ -98,8 +99,26 @@ export class SearchBarMenu extends Component {
     }
 
     /** Opens a domain editor dialog for the given item, default to "is in" "today" option */
-    onAddCustomDateFilterClick({ fieldName, fieldType }) {
-        const domain = domainFromTree(condition(fieldName, "in range", [fieldType, "today"]));
+    onAddCustomDateFilterClick(item) {
+        const { fieldName, fieldType, endFieldName, endFieldType } = item;
+        let domain;
+        if (endFieldName) {
+            // The value editors of the dialog cannot read smart dates back
+            // ("today +1d"), so the range is prefilled with the bounds of the
+            // current day instead of with the relative "today" option.
+            const range = constructDateRange({
+                referenceMoment: this.env.searchModel.referenceMoment,
+                fieldName,
+                fieldType,
+                endFieldName,
+                endFieldType,
+                granularity: "day",
+                setParam: {},
+            });
+            domain = range.domain.toString();
+        } else {
+            domain = domainFromTree(condition(fieldName, "in range", [fieldType, "today"]));
+        }
         this.env.searchModel.spawnCustomFilterDialog({ domain });
     }
 
