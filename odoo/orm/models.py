7bd1b173ebeec369e513ef66bbbb987a21a7e21b
Miquel Raïch <miquel.raich@forgeflow.com>
2026-09-16 15:04:03 +0000

    [FIX] base: fix broken format in export xid assertion
    X-original-commit: 2e42a6c3a97671f971a8a56a233fa6a6980ce9d4

--- odoo/orm/models.py
+++ odoo/orm/models.py
@@ -870,7 +870,7 @@ class BaseModel(metaclass=MetaModel):
                 for record, xid in self.env[model].browse(ids).__ensure_xml_id():
                     for i, j in xidmap.pop((record._name, record.id)):
                         lines[i][j] = xid
-            assert not xidmap, "failed to export xids for %s" % ', '.join('{}:{}' % it for it in xidmap.items())
+            assert not xidmap, "failed to export xids for %s" % ', '.join('%s:%s' % it for it in xidmap.items())
 
         if _is_toplevel_call:
             self.env.cr.cache.pop('export_properties_cache', None)
