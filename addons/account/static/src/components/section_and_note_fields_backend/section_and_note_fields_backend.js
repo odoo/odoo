2328fcd318d46274c3d7e639a3d7ffbac3dc51f3
Corentin Deruyck <coder@odoo.com>
2026-09-16 08:18:44 +0000

    [FIX] account: copy product when duplicating a section
    Duplicating a section on a confirmed SO with stock installed left the
    product empty on the copied line: product_id/product_template_id
    become readonly once a stock move exists, and copyFields didn't
    force-copy them.
    
    closes odoo/odoo#289303
    
    X-original-commit: edc8ab2f7bdde6fc0354263e2aee92a6016093a6
    Signed-off-by: Corentin Deruyck (coder) <coder@odoo.com>

--- addons/account/static/src/components/section_and_note_fields_backend/section_and_note_fields_backend.js
+++ addons/account/static/src/components/section_and_note_fields_backend/section_and_note_fields_backend.js
@@ -89,8 +89,14 @@ export class SectionAndNoteListRenderer extends ListRenderer {
         super.setup();
         this.productAndDescriptionColumn = "product_and_description";
         this.priceColumns = [...this.props.aggregatedFields, "price_unit"];
-        // invisible fields to force copy when duplicating a section
-        this.copyFields = ["display_type", "collapse_composition", "collapse_prices"];
+        // invisible/readonly fields to force copy when duplicating a section
+        this.copyFields = [
+            "display_type",
+            "collapse_composition",
+            "collapse_prices",
+            "product_id",
+            "product_template_id",
+        ];
         onPatched(() => {
             this.focusToName(this.editedRecord());
         });
