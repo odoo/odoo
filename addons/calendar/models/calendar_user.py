67f157681d92520f76c7a77f1941e3e5fcb1669d
Frantisek Binovsky <frbin@odoo.com>
2026-09-16 16:40:20 +0200

    [IMP] calendar: Show other calendar members
    Users should be able to see other calendar members, even if they don't own the
    calendar. It is useful to know who you are sharing your events with.
    
    Task-6574702
    
    closes odoo/odoo#289107
    
    Signed-off-by: Noé Antoine (nan) <nan@odoo.com>

--- addons/calendar/models/calendar_user.py
+++ addons/calendar/models/calendar_user.py
@@ -15,6 +15,8 @@ class CalendarUser(models.Model):
     _single_primary_calendar_per_user = models.UniqueIndex('(user_id) WHERE is_primary')
 
     calendar_id = fields.Many2one('calendar.calendar', string='Calendar', ondelete='cascade', index='btree', required=True, readonly=True)
+    calendar_share_user_ids = fields.Many2many(related='calendar_id.share_user_ids', readonly=True)
+    calendar_owner_id = fields.Many2one(related='calendar_id.owner_id')
     user_id = fields.Many2one('res.users', string='User', required=True, readonly=True)
     is_primary = fields.Boolean('Primary', readonly=True)
     name = fields.Char('Label')
