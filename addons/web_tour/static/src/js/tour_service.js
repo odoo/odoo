45127d7750693f30f84d0d91cc429bc862eeb267
Pierre Pulinckx <pipu@odoo.com>
2026-09-16 08:53:59 +0200

    [FIX] web_tour: allow testing a recorded tour from the tours list
    Clicking "Testing" on a custom/recorded tour called startTour with
    mode "auto" and fromDB true, but getTour() only loaded from the
    database when mode was "manual". For a tour that only exists in the
    web_tour.tour record (not in the client-side registry), this left
    `tour` undefined and startTour() silently did nothing.
    
    Load from the database whenever fromDB is set, regardless of mode.
    
    Introduced by 837d2de58dd8.
    
    closes odoo/odoo#288474
    
    Task-id: 6575435
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- addons/web_tour/static/src/js/tour_service.js
+++ addons/web_tour/static/src/js/tour_service.js
@@ -158,7 +158,7 @@ export class TourService {
      */
     async getTour(name, options) {
         let tour = tourRegistry.get(name, null);
-        if (options.mode === "manual" && options.fromDB) {
+        if (options.fromDB) {
             tour = await this.orm.call("web_tour.tour", "get_tour_json_by_name", [name]);
             if (!tour) {
                 throw new Error(`Tour '${name}' is not found in the database.`);
