b259c06219951d260a3115b644de9f124ef9a68f
rhe-odoo <rhe@odoo.com>
2026-09-16 12:58:58 +0200

    [FIX] stock_fleet: enable batch group on installation
    If stock_fleet is installed directly without enabling batch transfers beforehand,
    stock.group_stock_picking_batch is not enabled for internal users.
    When opening the settings, the onchange on group_stock_picking_batch
    unchecks module_stock_fleet, and saving triggers the uninstallation
    wizard of stock_fleet.
    
    This commit fixes the issue by enabling stock.group_stock_picking_batch
    for base.group_user in the post_init_hook of stock_fleet.

--- addons/stock_fleet/__init__.py
+++ addons/stock_fleet/__init__.py
@@ -1,8 +1,13 @@
 from . import models
 from . import report
 
+from odoo import Command
+
 
 def _enable_dispatch_management(env):
+    group_stock_picking_batch = env.ref('stock.group_stock_picking_batch')
+    env.ref('base.group_user').write({'implied_ids': [Command.link(group_stock_picking_batch.id)]})
+
     for delivery_steps, warehouses in env['stock.warehouse']._read_group([], ['delivery_steps'], ['id:recordset']):
         if delivery_steps == 'pick_pack_ship':
             warehouses.pack_type_id.dispatch_management = True
