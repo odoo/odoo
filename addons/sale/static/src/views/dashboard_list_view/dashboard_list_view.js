044878f2081263f7bbef8580bc674eb4c091d119
Corentin Deruyck <coder@odoo.com>
2026-09-16 09:53:25 +0000

    [IMP] sale: dynamically reload the sale dashboard
    In the Sales app, when selecting multiple SOs in list view and performing
    actions on them, the dashboard was not dynamically updated. This commit fixes
    this by sending an event on the bus.
    
    closes odoo/odoo#289614
    
    X-original-commit: 1023f02b0c69ace382bd3e6e6c5f5f105ac09c33
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>
    Signed-off-by: Corentin Deruyck (coder) <coder@odoo.com>

--- addons/sale/static/src/views/dashboard_list_view/dashboard_list_view.js
+++ addons/sale/static/src/views/dashboard_list_view/dashboard_list_view.js
@@ -1,6 +1,7 @@
 import { registry } from '@web/core/registry';
 import { SaleListRenderer } from '../sale_onboarding_list/sale_onboarding_list_renderer';
 import { saleFileUploadListView } from '../sale_file_upload_list/sale_file_upload_list_view';
+import { SaleFileUploadListController } from '../sale_file_upload_list/sale_file_upload_list_controller';
 import { Dashboard } from '../../js/dashboard/dashboard';
 
 export class DashboardListRenderer extends SaleListRenderer {
@@ -11,7 +12,23 @@ export class DashboardListRenderer extends SaleListRenderer {
     };
 }
 
+export class DashboardListController extends SaleFileUploadListController {
+    get actionMenuProps() {
+        const actionMenuProps = super.actionMenuProps;
+        return {
+            ...actionMenuProps,
+            onActionExecuted: ({ noReload } = {}) => {
+                actionMenuProps.onActionExecuted({ noReload });
+                if (!noReload) {
+                    this.env.bus.trigger("reload");
+                }
+            },
+        };
+    }
+}
+
 export const dashboardListView = Object.assign(Object.create(saleFileUploadListView), {
+    Controller: DashboardListController,
     Renderer: DashboardListRenderer,
 });
 
