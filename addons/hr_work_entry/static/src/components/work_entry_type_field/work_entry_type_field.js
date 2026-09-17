c59be592e0dad9720766fb3bca7656b9e67ea018
aski97 <vibel@odoo.com>
2026-09-17 10:36:45 +0200

    [REF] hr_work_entry: consolidate work entry type field widgets and templates.
    Unify the work entry type rendering logic to eliminate duplicate files and code:
    - Remove redundant `work_entry_type_badge_field.js` and `work_entry_type_badge_field.xml`.
    - Extend the `hr_work_entry.WorkEntryType` QWeb template to accept an optional `label` parameter, enabling it to render either a standalone color pill or a pill with an adjacent label.
    - Move `WorkEntryTypeBadgeField` into `work_entry_type_field.js` to keep all related field registrations in a single component directory.
    - Update `hr_work_entry.WorkEntryTypeBadgeField` and `hr_work_entry.Many2OneWorkEntryTypeField` templates to call the unified sub-template with their respective data.
    
    Task:6578031

--- addons/hr_work_entry/static/src/components/work_entry_type_field/work_entry_type_field.js
+++ addons/hr_work_entry/static/src/components/work_entry_type_field/work_entry_type_field.js
@@ -1,11 +1,29 @@
 import { Component, useProps } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { standardFieldProps } from "@web/views/fields/standard_field_props";
 import {
     buildM2OFieldDescription,
     many2OneFieldProps,
 } from "@web/views/fields/many2one/many2one_field";
 import { computeM2OProps, Many2One } from "@web/views/fields/many2one/many2one";
 
+//List View Widget
+export class WorkEntryTypeBadgeField extends Component {
+    static template = "hr_work_entry.WorkEntryTypeBadgeField";
+    props = useProps(standardFieldProps);
+}
+
+export const workEntryTypeBadgeField = {
+    component: WorkEntryTypeBadgeField,
+    fieldDependencies: [
+        { name: "color", type: "integer" },
+        { name: "display_code", type: "char" },
+        { name: "name", type: "char" },
+    ],
+};
+registry.category("fields").add("work_entry_type_badge", workEntryTypeBadgeField);
+
+//Form View M2O Widget
 export class WorkEntryTypeMany2One extends Many2One {
     /**
      * @override
