db9d5664b7a950b0f56a6fee576df2ccf6b7d81f
Alex Kühn <aku@odoo.com>
2026-09-17 18:24:47 +0200

    [FIX] mail: DiscussAvatar mask matches icon shape
    Before this commit, the mask of im status of discuss avatar was
    always a circle. This is ok for the most used im status "online",
    "away", "busy" and "offline" where icons are rounded. We have a few
    exceptions like:
    - odoobot's heart
    - user on leave with plane icon
    - user in office / homeworking with bulding / house icon
    
    This commit fixes the mask to take the shape of icon into account,
    so this looks like whatever the icon of im status.
    
    closes odoo/odoo#288972
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- addons/mail/static/src/core/common/discuss_avatar.js
+++ addons/mail/static/src/core/common/discuss_avatar.js
@@ -1,7 +1,7 @@
-import { ImStatus } from "@mail/core/common/im_status";
+import { getImStatusData, ImStatus } from "@mail/core/common/im_status";
 import { ThreadIcon } from "@mail/core/common/thread_icon";
 
-import { Component, t, useProps } from "@odoo/owl";
+import { Component, Portal, t, useProps } from "@odoo/owl";
 
 import { isBrowserSafari } from "@web/core/browser/feature_detection";
 import { useService } from "@web/core/utils/hooks";
@@ -10,7 +10,7 @@ let nextId = 0;
 
 export class DiscussAvatar extends Component {
     static template = "mail.DiscussAvatar";
-    static components = { ImStatus, ThreadIcon };
+    static components = { ImStatus, Portal, ThreadIcon };
 
     setup() {
         super.setup();
@@ -104,6 +104,18 @@ export class DiscussAvatar extends Component {
 
     get showIconMask() {
         return this.showIcon;
+    }
+
+    get imStatusData() {
+        if (this.isTyping || (this.thread && !this.channel?.showImStatus)) {
+            return undefined;
+        }
+        const member = this.channelMember || this.channel?.correspondent;
+        const persona = member?.persona || this.persona;
+        if (!persona) {
+            return undefined;
+        }
+        return getImStatusData({ member, persona, user: this.user || persona.main_user_id });
     }
 
     /** @returns {ResUsers|undefined} */
