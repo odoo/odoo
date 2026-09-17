dc056d3c61f3b0d89161804a27321b1c7f8d28f8
William Henrotin <whe@odoo.com>
2026-09-17 13:51:11 +0200

    [IMP] stock: recall email based on move line
    The commit 0c2d63f9cd8a0 introduced a way to send recall email based on
    problematic lot/serial number. The email was sent from the stock picking
    object. This prevent the body message to access the product name as well
    as lot data (name, expiry date, ...).
    This commit give a message template and base it on the stock move line
    object.
    
    closes odoo/odoo#288882
    
    Task: 6585450
    Signed-off-by: Quentin Wolfs (quwo) <quwo@odoo.com>

--- addons/stock/models/stock_move_line.py
+++ addons/stock/models/stock_move_line.py
@@ -713,21 +713,24 @@ class StockMoveLine(models.Model):
             affected_pickings._check_entire_pack()
 
     def action_send_recall_email(self):
-        partners = self.picking_partner_id
-        if partners:
-            return {
-                'name': _('Send Email'),
-                'type': 'ir.actions.act_window',
-                'res_model': 'mail.compose.message',
-                'view_mode': 'form',
-                'target': 'new',
-                'context': {
-                    'default_composition_mode': 'mass_mail' if len(partners) > 1 else 'comment',
-                    'default_partner_ids': partners.ids,
-                    'default_model': 'stock.picking',
-                    'default_res_ids': self.picking_id.ids,
-                }
-            }
+        move_lines = self.filtered('picking_partner_id')
+        if not move_lines:
+            return {}
+        template = self.env.ref('stock.mail_template_data_stock_move_line_recall', raise_if_not_found=False)
+        return {
+            'name': _('Send Email'),
+            'type': 'ir.actions.act_window',
+            'res_model': 'mail.compose.message',
+            'view_mode': 'form',
+            'target': 'new',
+            'context': {
+                'default_composition_mode': 'mass_mail',
+                'default_model': 'stock.move.line',
+                'default_res_ids': move_lines.ids,
+                'default_template_id': template.id if template else False,
+                'default_use_template': bool(template),
+            },
+        }
 
     def _synchronize_quant(self, quantity, location, action="available", in_date=False, **quants_value):
         """ quantity should be express in product's UoM"""
