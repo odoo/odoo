42364c1a110daf90388c2dfa6bd1ba96f53586f0
Luis Marin <marin.guadarrama@gmail.com>
2026-09-17 13:30:44 -0600

    [REF] resource: code reordering

--- addons/resource/models/mixin_resource_scheduling.py
+++ addons/resource/models/mixin_resource_scheduling.py
@@ -23,56 +23,6 @@ class MixinResourceScheduling(models.AbstractModel):
         search="_search_schedule_overlap_count",
     )
 
-    def _get_fields_reservation_date(self):
-        return (None, None)
-
-    def _is_scheduling_dated(self):
-        start_field, end_field = self._get_fields_reservation_date()
-        return bool(start_field and end_field and self[start_field] and self[end_field])
-
-    def _prepare_reservation_vals_list(self):
-        self.check_singleton()
-        return []
-
-    def _get_fields_sync_trigger(self):
-        triggers = set()
-        start_field, end_field = self._get_fields_reservation_date()
-        if start_field:
-            triggers.add(start_field)
-        if end_field:
-            triggers.add(end_field)
-        return triggers
-
-    def _sync_reservations(self):
-        start_field, end_field = self._get_fields_reservation_date()
-        if not start_field or not end_field or not self:
-            return
-        reservation_model = self.env["resource.reservation"]
-        existing_all = (
-            reservation_model.sudo()
-            .with_context(active_test=False)
-            .search(
-                [
-                    ("res_model", "=", self._name),
-                    ("res_id", "in", self.ids),
-                ]
-            )
-        )
-        existing_by_record = existing_all.grouped("res_id")
-        no_reservations = existing_all.browse()
-        for record in self:
-            reservation_model._sync_projection(
-                record,
-                record._prepare_reservation_vals_list(),
-                existing=existing_by_record.get(record.id, no_reservations),
-            )
-        self.invalidate_recordset(["reservation_ids", "schedule_overlap_count"])
-
-    def _active_for_sync(self):
-        if "active" in self._fields:
-            return self.filtered("active")
-        return self
-
     @api.model_create_multi
     def create(self, vals_list):
         records = super().create(vals_list)
@@ -115,13 +65,6 @@ class MixinResourceScheduling(models.AbstractModel):
         ).unlink()
         return result
 
-    def _get_fields_overlap_depends(self):
-        return [
-            "reservation_ids.schedule_overlap_count",
-            "reservation_ids.active",
-            *sorted(self._get_fields_sync_trigger()),
-        ]
-
     @api.depends(lambda self: self._get_fields_overlap_depends())
     def _compute_schedule_overlap_count(self):
         stored = self.filtered(lambda record: isinstance(record.id, int))
@@ -131,6 +74,39 @@ class MixinResourceScheduling(models.AbstractModel):
             )
         for record in self - stored:
             record.schedule_overlap_count = len(record._get_schedule_conflicts())
+
+    @api.model
+    def _search_schedule_overlap_count(self, operator, value):
+        if not isinstance(value, int) or value != 0 or operator not in ("=", "!="):
+            return NotImplemented
+        conflicted = (
+            self.env["resource.reservation"]
+            .sudo()
+            .search(
+                [("res_model", "=", self._name), ("schedule_overlap_count", ">", 0)]
+            )
+        )
+        record_ids = list({reservation.res_id for reservation in conflicted})
+        return [("id", "not in" if operator == "=" else "in", record_ids)]
+
+    def _get_fields_reservation_date(self):
+        return (None, None)
+
+    def _get_fields_sync_trigger(self):
+        triggers = set()
+        start_field, end_field = self._get_fields_reservation_date()
+        if start_field:
+            triggers.add(start_field)
+        if end_field:
+            triggers.add(end_field)
+        return triggers
+
+    def _get_fields_overlap_depends(self):
+        return [
+            "reservation_ids.schedule_overlap_count",
+            "reservation_ids.active",
+            *sorted(self._get_fields_sync_trigger()),
+        ]
 
     def _get_schedule_conflicts(self):
         self.check_singleton()
@@ -161,16 +137,40 @@ class MixinResourceScheduling(models.AbstractModel):
             )
         return result
 
-    @api.model
-    def _search_schedule_overlap_count(self, operator, value):
-        if not isinstance(value, int) or value != 0 or operator not in ("=", "!="):
-            return NotImplemented
-        conflicted = (
-            self.env["resource.reservation"]
-            .sudo()
+    def _prepare_reservation_vals_list(self):
+        self.check_singleton()
+        return []
+
+    def _sync_reservations(self):
+        start_field, end_field = self._get_fields_reservation_date()
+        if not start_field or not end_field or not self:
+            return
+        reservation_model = self.env["resource.reservation"]
+        existing_all = (
+            reservation_model.sudo()
+            .with_context(active_test=False)
             .search(
-                [("res_model", "=", self._name), ("schedule_overlap_count", ">", 0)]
+                [
+                    ("res_model", "=", self._name),
+                    ("res_id", "in", self.ids),
+                ]
             )
         )
-        record_ids = list({reservation.res_id for reservation in conflicted})
-        return [("id", "not in" if operator == "=" else "in", record_ids)]
+        existing_by_record = existing_all.grouped("res_id")
+        no_reservations = existing_all.browse()
+        for record in self:
+            reservation_model._sync_projection(
+                record,
+                record._prepare_reservation_vals_list(),
+                existing=existing_by_record.get(record.id, no_reservations),
+            )
+        self.invalidate_recordset(["reservation_ids", "schedule_overlap_count"])
+
+    def _active_for_sync(self):
+        if "active" in self._fields:
+            return self.filtered("active")
+        return self
+
+    def _is_scheduling_dated(self):
+        start_field, end_field = self._get_fields_reservation_date()
+        return bool(start_field and end_field and self[start_field] and self[end_field])
