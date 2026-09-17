LAIDOUDI33/LAID_ODOO

[FIX] sale_stock: prevent merge delivery of orders sharing a reassigned transfer
Steps to reproduce:
-------------------
- Install `stock` and `sale_management`
- Enable **Storage Locations**
- Create two quotations (Quote 1 and Quote 2) for a storable product
- Create an internal transfer in draft
- In the **Other Info** tab, set its Sales Order to Quote 1
- Confirm the internal transfer
- Change the transfer's Sales Order to Quote 2
- Confirm Quote 2
- Confirm Quote 1

Issue:
------
Instead of creating its own delivery order, Quote 1 reuses Quote 2's
existing delivery. Both sales orders end up sharing the same delivery
order, and no delivery is created for Quote 1.

Cause:
------
Since 19.0, delivery grouping is based on `stock.reference` records
rather than procurement groups.

A sale order is linked to a `stock.reference` through the many2many
relation `stock_reference_sale_rel`, exposed as
`sale.order.stock_reference_ids` and `stock.reference.sale_ids`. These
are the two ends of the same relation, so writing one changes the other.

When `sale_id` is assigned on a picking, `StockPicking._set_sale_id()`
creates or updates the corresponding `stock.reference` and links it to
that picking. So the internal transfer picking itself carries the
reference.

During the initial assignment (sale_id = Quote 1), the picking has no
reference yet, so a new `stock.reference` R is created with
sale_ids = [Quote 1] and attached to the transfer.

When the picking is later reassigned to Quote 2, the existing logic only
adds the new sale order to the reference:
```py
    self.reference_ids.sale_ids = [Command.link(self.sale_id.id)]
```
The previous sale order is never removed, leaving the same reference R
linked to both quotations (R.sale_ids = {Quote 1, Quote 2}), and by
symmetry both Quote1.stock_reference_ids and Quote2.stock_reference_ids
now contain R.

When each quotation is confirmed, its procurement inherits
`stock_reference_ids` from the sale order:
```txt
    sale.order.act

addons/sale_stock/tests/test_sale_stock.py
@@ -2763,3 +2763,30 @@ class TestSaleStock(TestSaleStockCommon, ValuationReconciliationTestCommon):
         self.assertRecordValues(sale_order.order_line, [
             {'product_id': self.new_product.id, 'product_uom_qty': 0, 'qty_delivered': 3}
         ])
+
+    def test_change_linked_sale_order_on_internal_transfer(self):
+        """Check that changing the sale order linked to an internal transfer does
+        not change the link of the sale order with respect to other pickings.
+        """
+        warehouse = self.company_data['default_warehouse']
+        sale_orders = self._get_new_sale_order(product=self.new_product) | self._get_new_sale_order(product=self.new_product)
+        internal_transfer = self.env['stock.picking'].create({
+            'picking_type_id': warehouse.int_type_id.id,
+            'location_id': warehouse.lot_stock_id.id,
+            'location_dest_id': warehouse.lot_stock_id.id,
+            'move_ids': [Command.create({
+                'product_id': self.new_product.id,
+                'product_uom_qty': 1,
+                'location_id': warehouse.lot_stock_id.id,
+                'location_dest_id': warehouse.lot_stock_id.id,
+            })],
+        })
+        internal_transfer.sale_id = sale_orders[0]
+        internal_transfer.action_confirm()
+        internal_transfer.sale_id = sale_orders[1]
+        sale_orders.action_confirm()
+        self.assertRecordValues((sale_orders.picking_ids | internal_transfer).sorted('id'), [
+            {'picking_type_code': 'internal', 'sale_id': sale_orders[1].id},
+            {'picking_type_code': 'outgoing', 'sale_id': sale_orders[0].id},
+            {'picking_type_code': 'outgoing', 'sale_id': sale_orders[1].id},
+        ])

odoo/odoo

addons/sale_stock/tests/test_sale_stock.py
@@ -2864,3 +2864,30 @@ class TestSaleStock(TestSaleStockCommon, ValuationReconciliationTestCommon):
         self.assertRecordValues(sale_order.order_line, [
             {'product_id': self.new_product.id, 'product_uom_qty': 0, 'qty_delivered': 3}
         ])
+
+    def test_change_linked_sale_order_on_internal_transfer(self):
+        """Test that changing the sale order linked to an internal transfer does
+        not change the link of the sale order with respect to other pickings.
+        """
+        warehouse = self.company_data['default_warehouse']
+        sale_orders = self._get_new_sale_order(product=self.new_product) | self._get_new_sale_order(product=self.new_product)
+        internal_transfer = self.env['stock.picking'].create({
+            'picking_type_id': warehouse.int_type_id.id,
+            'location_id': warehouse.lot_stock_id.id,
+            'location_dest_id': warehouse.lot_stock_id.id,
+            'move_ids': [Command.create({
+                'product_id': self.new_product.id,
+                'product_uom_qty': 1,
+                'location_id': warehouse.lot_stock_id.id,
+                'location_dest_id': warehouse.lot_stock_id.id,
+            })],
+        })
+        internal_transfer.sale_id = sale_orders[0]
+        internal_transfer.action_confirm()
+        internal_transfer.sale_id = sale_orders[1]
+        sale_orders.action_confirm()
+        self.assertRecordValues((sale_orders.picking_ids | internal_transfer).sorted('id'), [
+            {'picking_type_code': 'internal', 'sale_id': sale_orders[1].id},
+            {'picking_type_code': 'outgoing', 'sale_id': sale_orders[0].id},
+            {'picking_type_code': 'outgoing', 'sale_id': sale_orders[1].id},
+        ])