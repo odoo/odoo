7f6df86e3f1d1be819ef4f84f9000922b5bdd948
Paolo Gatti (pgi) <pgi@odoo.com>
2026-09-17 14:31:53 +0200

    [FIX] account_peppol: disable move send on already sent invoices
    If the invoice is already sent through PEPPOL, the checkbox
    for sending the invoice should then be readonly and unchecked.
    Currently is correctly unchecked but not readonly.
    We fix it by copying what French localization (`l10n_fr_pdp`) does already,
    giving a reason for disabling.
    
    closes odoo/odoo#288898
    
    Signed-off-by: Sven Führ (svfu) <svfu@odoo.com>

--- addons/account_peppol/wizard/account_move_send_wizard.py
+++ addons/account_peppol/wizard/account_move_send_wizard.py
@@ -14,6 +14,8 @@ class AccountMoveSendWizard(models.TransientModel):
 
     def _get_peppol_checkbox_addendum_disable_reason(self):
         self.ensure_one()
+        if self.move_id.peppol_is_sent:
+            return self.env._(" (Previously sent)")
         peppol_partner = self.move_id.partner_id.commercial_partner_id.with_company(self.company_id)
         if peppol_partner.peppol_verification_state == 'not_valid':
             return self.env._(' (Customer not on Peppol)')
