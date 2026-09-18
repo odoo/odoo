24f4a156093d0cb6878e80931243625fa49071f4
bepro@odoo <bepro@odoo.com>
2026-09-18 11:26:33 +0000

    [FIX] account: Add sort tiebreaker to Fiscal Position
    The algorithm to find the right fiscal position works on top of
    deterministically ordered fiscal position records. This invariant is
    not enforced by the functional code.
    
    Importing data without explicit sequence number results in multiple
    records ending up with the same value in this column. Sorting by column
    'sequence' is not good enough to have repetitive results in this case.
    The fiscal position algorithm can return a different record due to
    database implementation details.
    
    Adding field "id" to the default ordering ensures the order of fiscal
    position records remains deterministic. This is because insertion order
    is preserved and locked into order-preserving id-values when importing
    data.
    
    runbot-945738
    
    closes odoo/odoo#289754
    
    X-original-commit: 10b648651e393ba9d4c6f4d733d5b4e1a250edae
    Related: odoo/enterprise#132490
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>
    Signed-off-by: Bert Proesmans (bepro) <bepro@odoo.com>

--- addons/account/models/partner.py
+++ addons/account/models/partner.py
@@ -30,7 +30,7 @@ _logger = logging.getLogger(__name__)
 class AccountFiscalPosition(models.Model):
     _name = 'account.fiscal.position'
     _description = 'Fiscal Position'
-    _order = 'sequence'
+    _order = 'sequence, id'
     _check_company_auto = True
     _check_company_domain = models.check_company_domain_parent_of
 
