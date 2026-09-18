54713f03776775038fed72269fa3e728efb632be
Michaël Mattiello <mcm@odoo.com>
2026-09-18 10:37:06 +0200

    [FIX] *: declare missing props
    * html_editor,l10n_es_edi_tbai_pos,l10n_es_edi_verifactu_pos,
      l10n_sa_pos,mail,mass_mailing,portal_rating,web
    
    Before this commit, some components/patches read props from `this.props` without
    declaring it. It used to work because the compatibility layer still defined it. Now that
    the compatibility layer stopped to define it, these components/patches started to crash.
    
    closes odoo/odoo#289550
    
    Related: odoo/enterprise#132379
    Signed-off-by: Michaël Mattiello (mcm) <mcm@odoo.com>

--- addons/portal_rating/static/src/chatter/portal/portal_chatter_patch.js
+++ addons/portal_rating/static/src/chatter/portal/portal_chatter_patch.js
@@ -2,12 +2,16 @@ import { PortalChatter } from "@portal/chatter/portal/portal_chatter";
 import { PortalRatingPlugin } from "@portal_rating/chatter/portal/portal_rating_plugin";
 
 import { patch } from "@web/core/utils/patch";
-import { providePlugins, usePlugin } from "@odoo/owl";
+import { providePlugins, t, usePlugin, useProps } from "@odoo/owl";
 
 patch(PortalChatter.prototype, {
     setup() {
         super.setup(...arguments);
         providePlugins([PortalRatingPlugin]);
-        usePlugin(PortalRatingPlugin).reviewChatter.set(this.props.reviewChatter ?? false);
+
+        this.portalChatterProps = useProps({
+            reviewChatter: t.any().optional(),
+        });
+        usePlugin(PortalRatingPlugin).reviewChatter.set(this.portalChatterProps.reviewChatter ?? false);
     },
 });
