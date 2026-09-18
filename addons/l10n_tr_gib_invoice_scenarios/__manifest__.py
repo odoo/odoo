LAIDOUDI33/LAID_ODOO

[ADD] l10n_tr_gib_invoice_scenarios: commercial and return invoices
This commit is a backport of [1] and [2], which were merged in master,
delivered as a new optional module for 19.0.

Adds the Commercial (TICARIFATURA) invoice scenario and the "Return"
(IADE) and "Withholding Return" (TEVKIFATIADE) invoice types to the
Nilvera e-invoicing flow:

- Credit notes can be sent to Nilvera as return invoices with a
  reference to the original invoice.
- Commercial invoices/bills track the counterpart's response: accept or
  reject commercial bills from Odoo and synchronize responses from
  Nilvera.

[1]: https://github.com/odoo/odoo/pull/245173
[2]: https://github.com/odoo/odoo/pull/246950

task-6365900

addons/l10n_tr_gib_invoice_scenarios/__manifest__.py
@@ -1,0 +1,25 @@
+{
+    'name': 'Türkiye - Nilvera E-Invoice Commercial and Return',
+    'version': '1.0',
+    'category': 'Accounting/Accounting',
+    'description': """
+This module extends the Nilvera integration with additional invoice scenarios and types required for Turkish e-Invoicing compliance.
+
+Features include:
+
+- Support for invoice scenario: Commercial
+- Support for invoice types: Return and Withholding Return
+- Sending credit notes to Nilvera as return invoices linked to the original invoice
+- Ability to accept or reject commercial bills and synchronize customer responses.
+    """,
+    'depends': ['l10n_tr_nilvera_einvoice_extended'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/cron.xml',
+        'views/account_move_views.xml',
+        'wizards/l10n_tr_nilvera_einvoice_ticarifatura_response_wizard_views.xml',
+    ],
+    'installable': True,
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}

addons/l10n_tr_gib_invoice_scenarios/tests/test_xml_ubl_tr.py
@@ -1,0 +1,73 @@
+from freezegun import freeze_time
+
+from odoo import Command
+from odoo.tests import tagged
+from odoo.tools import file_open
+
+from odoo.addons.l10n_tr_nilvera_einvoice_extended.tests.test_xml_ubl_tr import TestUBLTR
+
+
+@tagged("post_install_l10n", "post_install", "-at_install")
+class TestUBLTRGibInvoiceScenarios(TestUBLTR):
+    def _generate_invoice(self, partner_id, tax=None, **kwargs):
+        invoice_tax = (tax and tax.ids) or self.tax_20.ids
+        invoice = self.env["account.move"].create(
+            {
+                "move_type": "out_invoice",
+                "company_id": self.company_data["company"].id,
+                "partner_id": partner_id.id,
+                "name": "EIN/2025/1",
+                "invoice_date": "2025-03-03",
+                "narration": "3 products",
+                "invoice_line_ids": [
+                    Command.create(
+                        {
+                            "product_id": self.product_a.id,
+                            "price_unit": 50.00,
+                            "quantity": 3,
+                            "discount": 12,
+                            "tax_ids": [Command.set(invoice_tax)],
+                        }
+                    ),
+                ],
+                **kwargs,
+            }
+        )
+        invoice.action_post()
+        return invoice
+
+    def _generate_invoice_xml(self, partner_id, tax=None, include_invoice=False, **kwargs):
+        invoice = self._generate_invoice(partner_id, tax, **kwargs)
+        if include_invoice:
+            return self.env["account.edi.xml.ubl.tr"]._export_invoice(invoice)[0], invoice
+        return self.env["account.edi.xml.ubl.tr"]._export_invoice(invoice)[0]
+
+    def _generate_return_invoice_xml(self, partner_id, tax=None, **kwargs):
+        invoice = self._generate_invoice(partner_id, tax, **kwargs)
+        credit_note = invoice._reverse_moves()
+        credit_note.action_post()
+        return self.env["account.edi.xml.ubl.tr"]._export_invoice(credit_note)[0]
+
+    def test_xml_invoice_basic_return_einvoice(self):
+        with freeze_time("2025-03-05"):
+            generated_xml = self._generate_return_invoice_xml(self.einvoice_partner)
+
+        with file_open(
+            "l10n_tr_gib_invoice_scenarios/tests/expected_xmls/invoice_basic_return_einvoice.xml", "rb"
+        ) as expected_xml_file:
+            expected_xml = expected_xml_file.read()
+
+        self.assertXmlTreeEqual(self.get_xml_tree_from_string(generated_xml), self.get_xml_tree_from_string(expected_xml))
+
+    def test_xml_invoice_basic_withholding_return_einvoice(self):
+        with freeze_time("2025-03-05"):
+            generated_xml = self._generate_return_invoice_xml(
+                self.einvoice_partner, self.tax_20_withholding, l10n_tr_gib_invoice_type="TEVKIFAT"
+            )
+
+        with file_open(
+            "l10n_tr_gib_invoice_scenarios/tests/expected_xmls/invoice_return_basic_withholding_einvoice.xml", "rb"
+        ) as expected_xml_file:
+            expected_xml = expected_xml_file.read()
+
+        self.assertXmlTreeEqual(self.get_xml_tree_from_string(generated_xml), self.get_xml_tree_from_string(expected_xml))

odoo/odoo

X-original-commit: 1ef36e9a7a51c0617507e792482094d90833b6bb

addons/l10n_tr_gib_invoice_scenarios/__manifest__.py
@@ -1,0 +1,23 @@
+{
+    'name': 'Türkiye - Nilvera E-Invoice Commercial and Return',
+    'category': 'Accounting/Accounting',
+    'description': """
+This module extends the Nilvera integration with additional invoice scenarios and types required for Turkish e-Invoicing compliance.
+
+Features include:
+
+- Support for invoice scenario: Commercial
+- Support for invoice types: Return and Withholding Return
+- Sending credit notes to Nilvera as return invoices linked to the original invoice
+- Ability to accept or reject commercial bills and synchronize customer responses.
+    """,
+    'depends': ['l10n_tr_nilvera_einvoice'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/cron.xml',
+        'views/account_move_views.xml',
+        'wizards/l10n_tr_nilvera_einvoice_ticarifatura_response_wizard_views.xml',
+    ],
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}

addons/l10n_tr_gib_invoice_scenarios/tests/test_xml_ubl_tr.py
@@ -1,0 +1,73 @@
+from freezegun import freeze_time
+
+from odoo import Command
+from odoo.tests import tagged
+from odoo.tools import file_open
+
+from odoo.addons.l10n_tr_nilvera_einvoice.tests.test_xml_ubl_tr import TestUBLTR
+
+
+@tagged("post_install_l10n", "post_install", "-at_install")
+class TestUBLTRGibInvoiceScenarios(TestUBLTR):
+    def _generate_invoice(self, partner_id, tax=None, **kwargs):
+        invoice_tax = (tax and tax.ids) or self.tax_20.ids
+        invoice = self.env["account.move"].create(
+            {
+                "move_type": "out_invoice",
+                "company_id": self.company_data["company"].id,
+                "partner_id": partner_id.id,
+                "name": "EIN/2025/1",
+                "invoice_date": "2025-03-03",
+                "narration": "3 products",
+                "invoice_line_ids": [
+                    Command.create(
+                        {
+                            "product_id": self.product_a.id,
+                            "price_unit": 50.00,
+                            "quantity": 3,
+                            "discount": 12,
+                            "tax_ids": [Command.set(invoice_tax)],
+                        }
+                    ),
+                ],
+                **kwargs,
+            }
+        )
+        invoice.action_post()
+        return invoice
+
+    def _generate_invoice_xml(self, partner_id, tax=None, include_invoice=False, **kwargs):
+        invoice = self._generate_invoice(partner_id, tax, **kwargs)
+        if include_invoice:
+            return self.env["account.edi.xml.ubl.tr"]._export_invoice(invoice)[0], invoice
+        return self.env["account.edi.xml.ubl.tr"]._export_invoice(invoice)[0]
+
+    def _generate_return_invoice_xml(self, partner_id, tax=None, **kwargs):
+        invoice = self._generate_invoice(partner_id, tax, **kwargs)
+        credit_note = invoice._reverse_moves()
+        credit_note.action_post()
+        return self.env["account.edi.xml.ubl.tr"]._export_invoice(credit_note)[0]
+
+    def test_xml_invoice_basic_return_einvoice(self):
+        with freeze_time("2025-03-05"):
+            generated_xml = self._generate_return_invoice_xml(self.einvoice_partner)
+
+        with file_open(
+            "l10n_tr_gib_invoice_scenarios/tests/expected_xmls/invoice_basic_return_einvoice.xml", "rb"
+        ) as expected_xml_file:
+            expected_xml = expected_xml_file.read()
+
+        self.assertXmlTreeEqual(self.get_xml_tree_from_string(generated_xml), self.get_xml_tree_from_string(expected_xml))
+
+    def test_xml_invoice_basic_withholding_return_einvoice(self):
+        with freeze_time("2025-03-05"):
+            generated_xml = self._generate_return_invoice_xml(
+                self.einvoice_partner, self.tax_20_withholding, l10n_tr_gib_invoice_type="TEVKIFAT"
+            )
+
+        with file_open(
+            "l10n_tr_gib_invoice_scenarios/tests/expected_xmls/invoice_return_basic_withholding_einvoice.xml", "rb"
+        ) as expected_xml_file:
+            expected_xml = expected_xml_file.read()
+
+        self.assertXmlTreeEqual(self.get_xml_tree_from_string(generated_xml), self.get_xml_tree_from_string(expected_xml))