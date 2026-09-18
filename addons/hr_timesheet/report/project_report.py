56a13f419cdcca28c336a50174b339ef2bd22a8a
Vincent Larcin <vila@odoo.com>
2026-09-18 08:54:30 +0000

    Cherry pick of 93cee95526742476953d64aa0cef9d49cfdea463 failed
    stdout:
    Auto-merging addons/hr_timesheet/report/project_report.py
    CONFLICT (content): Merge conflict in addons/hr_timesheet/report/project_report.py
    Auto-merging addons/hr_timesheet/tests/__init__.py
    
    stderr:

--- addons/hr_timesheet/report/project_report.py
+++ addons/hr_timesheet/report/project_report.py
@@ -14,10 +14,22 @@ class ReportProjectTaskUser(models.Model):
     overtime = fields.Float(readonly=True, groups="hr_timesheet.group_hr_timesheet_user")
 
     def _select(self):
+<<<<<<< bccb550d5a5a1dfd81e6c234a7fa9a7794fc5702
         return super()._select() + """,
                 CASE WHEN COALESCE(t.allocated_hours, 0) = 0 THEN NULL ELSE t.effective_hours * 100 / t.allocated_hours END as progress,
                 NULLIF(t.effective_hours, 0) as effective_hours,
                 CASE WHEN COALESCE(t.allocated_hours, 0) = 0 THEN NULL ELSE t.allocated_hours - t.effective_hours END as remaining_hours,
+||||||| 2c1ce76770ea5d08f68027f5fe5fe7467db8f478
+        return super()._select() +  """,
+                CASE WHEN COALESCE(t.allocated_hours, 0) = 0 THEN 0.0 ELSE t.effective_hours * 100 / t.allocated_hours END as progress,
+                t.effective_hours,
+                CASE WHEN COALESCE(t.allocated_hours, 0) = 0 THEN 0.0 ELSE t.allocated_hours - t.effective_hours END as remaining_hours,
+=======
+        return super()._select() +  """,
+                CASE WHEN COALESCE(t.allocated_hours, 0) = 0 THEN 0.0 ELSE t.progress * 100 END as progress,
+                t.effective_hours,
+                CASE WHEN COALESCE(t.allocated_hours, 0) = 0 THEN 0.0 ELSE t.remaining_hours END as remaining_hours,
+>>>>>>> 3d883e0ebd4edc7a2494f1dfb2c1b59d3c8564d6
                 CASE WHEN t.allocated_hours > 0 THEN t.remaining_hours / t.allocated_hours ELSE 0 END as remaining_hours_percentage,
                 NULLIF(t.allocated_hours, 0) as allocated_hours,
                 NULLIF(t.overtime, 0) as overtime
@@ -27,5 +39,15 @@ class ReportProjectTaskUser(models.Model):
         return super()._group_by() + """,
                 t.effective_hours,
                 t.allocated_hours,
+<<<<<<< bccb550d5a5a1dfd81e6c234a7fa9a7794fc5702
                 t.overtime
+||||||| 2c1ce76770ea5d08f68027f5fe5fe7467db8f478
+                t.overtime,
+                t.total_hours_spent
+=======
+                t.overtime,
+                t.progress,
+                t.remaining_hours,
+                t.total_hours_spent
+>>>>>>> 3d883e0ebd4edc7a2494f1dfb2c1b59d3c8564d6
         """
--- addons/hr_timesheet/tests/__init__.py
+++ addons/hr_timesheet/tests/__init__.py
@@ -9,3 +9,4 @@ from . import test_project_template
 from . import test_employee_delete_wizard
 from . import test_timesheet_import_template
 from . import test_project_sharing
+from . import test_task_analysis
