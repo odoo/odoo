c16825ddc2505be785374588355051825d79a9f2
AlienAtSystem <gerald.malsch@manatec.de>
2026-09-21 08:58:56 +0000

    [FIX] account_edi_ubl_cii: Factur-X compliance for invoice addresses
    X-original-commit: c4c7821a8f5cd9633cdb343f258c264514c9e412

--- addons/account_edi_ubl_cii/models/account_edi_cii.py
+++ addons/account_edi_ubl_cii/models/account_edi_cii.py
@@ -546,6 +546,7 @@ class AccountEdiCii(models.AbstractModel):
     def _cii_get_buyer_trade_party_node(self, vals):
         customer = vals['customer']
         commercial_partner = customer.commercial_partner_id
+        name = customer.display_name if customer.name else commercial_partner.display_name
         scheme_id = None
         legal_organization_val = commercial_partner.company_registry
         if siret := commercial_partner._get_additional_identifier('FR_SIRET'):
@@ -553,11 +554,11 @@ class AccountEdiCii(models.AbstractModel):
             legal_organization_val = siret[:9]
         return self._cii_get_partner_trade_party_node(vals, {
             'gln': False,
-            'name': customer.name,
+            'name': name,
             'partner_specified_legal_organization': legal_organization_val,
             'partner_specified_legal_organization_scheme': scheme_id,
             'contact_values': {
-                'name': customer.name,
+                'name': name,
                 'phone': customer.phone,
                 'email': customer.email,
             },
