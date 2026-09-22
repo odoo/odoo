9dfd75c58fdcd60caee553fa95e6ca2b7e36d871
Pierre Pulinckx <pipu@odoo.com>
2026-09-22 10:12:17 +0200

    [FIX] web_tour: allow testing a recorded tour from the tours list
    Clicking "Testing" on a custom/recorded tour called startTour with
    mode "auto" and fromDB true, but getTour() only loaded from the
    database when mode was "manual". For a tour that only exists in the
    web_tour.tour record (not in the client-side registry), this left
    `tour` undefined and startTour() silently did nothing.
    
    Load from the database whenever fromDB is set, regardless of mode.
    
    Introduced by 837d2de58dd8.
    
    closes odoo/odoo#289208
    
    Task-id: 6575435
    X-original-commit: 773ed450797c3300869f9f4a2afaac70a7dec04f
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>
    Signed-off-by: Pierre Pulinckx (pipu) <pipu@odoo.com>

--- addons/web_tour/static/tests/tour_automatic.test.js
+++ addons/web_tour/static/tests/tour_automatic.test.js
@@ -7,6 +7,7 @@ import {
     getService,
     makeMockEnv,
     mountWithCleanup,
+    onRpc,
     patchWithCleanup,
 } from "@web/../tests/web_test_helpers";
 import { browser } from "@web/core/browser/browser";
@@ -139,6 +140,27 @@ test("Step Tour validity", async () => {
   }
 ]`,
     ]);
+});
+
+test("testing a custom tour that only exists in the database", async () => {
+    onRpc("web_tour.tour", "get_tour_json_by_name", () => ({
+        name: "custom_tour",
+        steps: [{ trigger: ".button0", run: "click" }],
+    }));
+
+    class Root extends Component {
+        static components = {};
+        static template = xml/*html*/ `<button class="button0" t-on-click="this.onClick">Button 0</button>`;
+        static props = ["*"];
+        onClick() {
+            expect.step("clicked");
+        }
+    }
+
+    await mountWithCleanup(Root);
+    await odoo.startTour("custom_tour", { mode: "auto", fromDB: true });
+    await waitForMacro();
+    expect.verifySteps(["clicked"]);
 });
 
 test("a tour with invalid step trigger", async () => {
