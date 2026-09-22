64cd29568c0b9c567ab33f33a1adfcb76e15f3ca
David Monjoie <dmo@odoo.com>
2026-09-22 22:03:55 +0200

    [FIX] html_editor: copy attributes when converting DIV into P
    The PR [1] forgot to copy the attributes from the DIV into the new P.
    
    It is not possible to use `setTagName` here because the function is
    in DomPlugin which depends on BaseContainerPlugin.
    
    See [2] for more information.
    
    [1]: https://github.com/odoo/odoo/pull/289891
    [2]: https://github.com/odoo/odoo/pull/289891#discussion_r4075860275
    
    closes odoo/odoo#290085
    
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/html_editor/static/tests/paste.test.js
+++ addons/html_editor/static/tests/paste.test.js
@@ -22,6 +22,7 @@ import { createBaseContainer } from "@html_editor/utils/base_container";
 import { expectElementCount } from "./_helpers/ui_expectations";
 import { nodeSize } from "@html_editor/utils/position";
 import { iconClasses } from "@html_editor/utils/dom_info";
+import { Plugin } from "@html_editor/plugin";
 
 function isInline(node) {
     return ["I", "B", "U", "S", "EM", "STRONG", "IMG", "BR", "A", "FONT"].includes(node);
@@ -3769,14 +3770,25 @@ describe("Odoo editor own html", () => {
     });
 
     test("should convert unsupported base containuers", async () => {
+        class TestSystemPlugin extends Plugin {
+            static id = "x";
+            resources = {
+                system_classes: ["x"],
+                system_attributes: ["data-x"],
+            };
+        }
         await testEditor({
             contentBefore: "<p>a[]</p><p>b</p>",
             stepFunction: async (editor) => {
-                pasteOdooEditorHtml(editor, `<div>c</div><div>d</div><div>e</div>`);
+                pasteOdooEditorHtml(
+                    editor,
+                    `<div>c</div><div class="x y" data-x="system">d</div><div style="color: red">e</div>`
+                );
             },
-            contentAfter: "<p>ac</p><p>d</p><p>e[]</p><p>b</p>",
+            contentAfter: `<p>ac</p><p class="y">d</p><p style="color: red">e[]</p><p>b</p>`,
             config: {
                 baseContainers: ["P"],
+                includePlugins: [TestSystemPlugin],
             },
         });
     });
