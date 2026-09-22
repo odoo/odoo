d9bc8a8c4d6f49d509beb26562a33eb7726d9b5b
Julien Mougenot <jum@odoo.com>
2026-09-22 11:22:03 +0200

    [FIX] web,*: fix uses of 'Generic' field type
    The `Generic` field constructor in mock models is meant to be used to
    simulate a custom field type defined on the database. As such, it should
    always require a (string) 'type' key to define that custom type.
    
    Before this commit, the `Generic` constructor was misused, as most use
    cases didn't provide a 'type', which neither the mock server nor the
    receiver components are meant to handle properly.
    
    The probable reason for these misuses could be that field validation was
    done too early (i.e. when defining the field on the model class), which
    means that there was no way to add a partial field definition meant to
    extend a server field that would be loaded later on.
    
    This commit does 3 things:
    
    - replace misues of `Generic` by regular field types;
    - enforce the presence of a 'type' key when declaring actual generic
    types;
    - perform the field validation (required field properties) *after* the
    server fields have been loaded, so that server fields can be augmented
    from local **partial** field definitions.

--- addons/web/static/tests/_framework/mock_server/mock_model.js
+++ addons/web/static/tests/_framework/mock_server/mock_model.js
@@ -20,8 +20,13 @@ import {
     safeSplit,
 } from "./mock_server_utils";
 
-const { DEFAULT_FIELD_VALUES, DEFAULT_RELATIONAL_FIELD_VALUES, S_FIELD, copyFields, isComputed } =
-    fields;
+const {
+    DEFAULT_FIELD_VALUES,
+    DEFAULT_RELATIONAL_FIELD_VALUES,
+    S_FIELD_REQUIRED_KEYS,
+    copyFields,
+    isComputed,
+} = fields;
 
 /**
  * @typedef {import("fields").INumerical["aggregator"]} Aggregator
@@ -312,7 +317,8 @@ function getModelDefinition(previous, constructor) {
 
     // Fields declared as JS class fields (do not override explicit fields)
     for (const [fieldName, fieldDef] of Object.entries(model)) {
-        if (!fieldDef?.[S_FIELD]) {
+        if (!fieldDef?.[S_FIELD_REQUIRED_KEYS]) {
+            // Not a field
             continue;
         }
         model._fields[fieldName] ||= validateFieldDefinition(fieldName, fieldDef);
@@ -1237,12 +1243,11 @@ function updateComodelRelationalFields(model, record, originalRecord) {
  * @param {FieldDefinition} fieldDef
  */
 function validateFieldDefinition(fieldName, fieldDef) {
-    if (fieldDef[S_FIELD] && fieldDef.name) {
+    if (S_FIELD_REQUIRED_KEYS in fieldDef && fieldDef.name) {
         throw new MockServerError(
             `Cannot set the name of field "${fieldName}" from its definition: got "${fieldDef.name}"`
         );
     }
-    delete fieldDef[S_FIELD];
     return fieldDef;
 }
 
