c02a5b694e423033bce7d7657772ab352c099917
Jigar Vaghela <jva@odoo.com>
2026-09-22 20:14:57 +0530

    [FIX] l10n_in_ewaybill: global discount
    Before this commit-
    We didn't consider, global discount for ewaybill
    After this commit-
    We consider the global discount for ewaybill
    opw-6592878
    
    closes odoo/odoo#290217
    
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>

--- addons/l10n_in_ewaybill/models/l10n_in_ewaybill.py
+++ addons/l10n_in_ewaybill/models/l10n_in_ewaybill.py
@@ -407,6 +407,7 @@ class L10nInEwaybill(models.Model):
         for line in invoice_lines:
             if (
                 line.display_type == 'product'
+                and line.l10n_in_hsn_code
                 and not AccountMove._l10n_in_is_service_hsn(line.l10n_in_hsn_code)
                 and (hsn_error_message := line._l10n_in_check_invalid_hsn_code())
             ):
@@ -700,7 +701,13 @@ class L10nInEwaybill(models.Model):
         tax_details = self.account_move_id._l10n_in_prepare_tax_details()
         tax_details_by_code = self.env['account.move']._get_l10n_in_tax_details_by_line_code(tax_details.get("tax_details", {}))
         invoice_line_tax_details = tax_details.get("tax_details_per_record")
+        filtered_invoice_line_tax_details = {
+            line: line_tax_details
+            for line, line_tax_details in invoice_line_tax_details.items()
+            if line.l10n_in_hsn_code and line.tax_ids
+        }
         sign = self.account_move_id.is_inbound() and -1 or 1
+        other_base_values = sum(line.balance for line in invoice_line_tax_details if not (line.l10n_in_hsn_code and line.tax_ids)) * sign
         rounding_amount = sum(line.balance for line in self.account_move_id.line_ids if line.display_type == 'rounding') * sign
         total_invoice_value = tax_details.get("base_amount", 0.00) + tax_details.get("tax_amount", 0.00) + rounding_amount
         if self.account_move_id.l10n_in_gst_treatment == 'overseas' and self.partner_ship_to_id.country_id.code != 'IN':
@@ -712,15 +719,15 @@ class L10nInEwaybill(models.Model):
             )
             total_invoice_value -= adjusting_rc_amount
         return {
-            "itemList": list(starmap(self._get_l10n_in_ewaybill_line_details, invoice_line_tax_details.items())),
+            "itemList": list(starmap(self._get_l10n_in_ewaybill_line_details, filtered_invoice_line_tax_details.items())),
             "totalValue": round_value(tax_details.get("base_amount", 0.00)),
             **{
                 f'{tax_type}Value': round_value(tax_details_by_code.get(f'{tax_type}_amount', 0.00))
                 for tax_type in ['cgst', 'sgst', 'igst', 'cess']
             },
             "cessNonAdvolValue": round_value(tax_details_by_code.get("cess_non_advol_amount", 0.00)),
-            "otherValue": round_value(tax_details_by_code.get("other_amount", 0.00) + rounding_amount),
-            "totInvValue": round_value(total_invoice_value),
+            "otherValue": round_value(tax_details_by_code.get("other_amount", 0.00) + rounding_amount + other_base_values),
+            "totInvValue": round_value(total_invoice_value + other_base_values),
         }
 
     def _ewaybill_generate_direct_json(self):
