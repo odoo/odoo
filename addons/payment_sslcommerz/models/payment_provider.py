LAIDOUDI33/LAID_ODOO

[ADD] payment_sslcommerz: integration of a new payment provider
Add new payment provider SSLCOMMERZ.

See README.md for more details.

task-6185518

addons/payment_sslcommerz/models/payment_provider.py
@@ -1,0 +1,69 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+from odoo.exceptions import ValidationError
+from odoo.tools import urls
+
+from odoo.addons.payment_sslcommerz import const
+
+
+class PaymentProvider(models.Model):
+    _inherit = "payment.provider"
+
+    code = fields.Selection(
+        selection_add=[("sslcommerz", "SSLCOMMERZ")], ondelete={"sslcommerz": "set default"}
+    )
+    sslcommerz_store_id = fields.Char(
+        string="SSLCOMMERZ Store ID", required_if_provider="sslcommerz", copy=False
+    )
+    sslcommerz_store_passwd = fields.Char(
+        string="SSLCOMMERZ Store Password",
+        required_if_provider="sslcommerz",
+        copy=False,
+        groups="base.group_system",
+    )
+
+    # === COMPUTE METHODS === #
+
+    def _get_supported_currencies(self):
+        """Override of `payment` to return the supported currencies."""
+        supported_currencies = super()._get_supported_currencies()
+        if self.code == "sslcommerz":
+            supported_currencies = supported_currencies.filtered(
+                lambda c: c.name == const.SUPPORTED_CURRENCY
+            )
+        return supported_currencies
+
+    # === CRUD METHODS === #
+
+    def _get_default_payment_method_codes(self):
+        """Override of `payment` to return the default payment method codes."""
+        self.ensure_one()
+        if self.code != "sslcommerz":
+            return super()._get_default_payment_method_codes()
+        return const.DEFAULT_PAYMENT_METHOD_CODES
+
+    # === REQUEST HELPERS === #
+
+    def _build_request_url(self, endpoint, **kwargs):
+        """Override of `payment` to build the request URL."""
+        if self.code != "sslcommerz":
+            return super()._build_request_url(endpoint, **kwargs)
+
+        if self.is_live:
+            api_url = const.PAYMENT_API_LIVE_URL
+        else:
+            api_url = const.PAYMENT_API_TEST_URL
+        return urls.urljoin(api_url, endpoint)
+
+    def _parse_response_content(self, response, **kwargs):
+        """Override of `payment` to parse the response content."""
+        if self.code != "sslcommerz":
+            return super()._parse_response_content(response, **kwargs)
+
+        response_content = response.json()
+
+        if failed_reason := response_content.get("failedreason"):
+            raise ValidationError(failed_reason)
+
+        return response_content

addons/payment_sslcommerz/tests/common.py
@@ -1,0 +1,28 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo.addons.payment.tests.common import PaymentCommon
+
+
+class SSLCommerzCommon(PaymentCommon):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.sslcommerz = cls._prepare_provider(
+            "sslcommerz",
+            update_values={
+                "sslcommerz_store_id": "test_store",
+                "sslcommerz_store_passwd": "test_password",
+            },
+        )
+        cls.provider = cls.sslcommerz
+        cls.currency_bdt = cls._enable_currency("BDT")
+        cls.currency = cls.currency_bdt
+
+        cls.payment_data = {
+            "tran_id": cls.reference,
+            "status": "VALID",
+            "val_id": "260101012345",
+            "bank_tran_id": "230101012345",
+            "currency_amount": cls.amount,
+            "currency_type": cls.currency.name,
+        }

addons/payment_sslcommerz/tests/test_payment_transaction.py
@@ -1,0 +1,92 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo.tests import tagged
+from odoo.tools import urls
+
+from odoo.addons.payment_sslcommerz import const
+from odoo.addons.payment_sslcommerz.tests.common import SSLCommerzCommon
+
+
+@tagged("post_install", "-at_install")
+class TestPaymentTransaction(SSLCommerzCommon):
+    def test_no_item_missing_from_prepared_payload(self):
+        """Test that no expected item is missing from the payload sent to SSLCOMMERZ."""
+        payment_method = self.env.ref("payment_sslcommerz.payment_method_card")
+        tx = self._create_transaction("redirect", payment_method_id=payment_method.id)
+        base_url = tx.provider_id.get_base_url()
+        return_url = urls.urljoin(base_url, const.PAYMENT_RETURN_ROUTE)
+        expected_payload = {
+            "store_id": tx.provider_id.sslcommerz_store_id,
+            "store_passwd": tx.provider_id.sslcommerz_store_passwd,
+            "total_amount": tx.amount,
+            "currency": tx.currency_id.name,
+            "tran_id": tx.reference,
+            "success_url": return_url,
+            "fail_url": return_url,
+            "cancel_url": return_url,
+            "ipn_url": urls.urljoin(base_url, const.IPN_ROUTE),
+            "cus_name": tx.partner_name,
+            "cus_email": tx.partner_email,
+            "product_name": "Online Payment",
+            "product_category": "general",
+            "product_profile": "non-physical-goods",
+            "multi_card_name": const.PAYMENT_METHODS_MAPPING[tx.payment_method_code],
+        }
+        self.assertDictEqual(tx._sslcommerz_prepare_session_payload(), expected_payload)
+
+    def test_extract_reference_finds_reference(self):
+        """Test that the transaction reference is found in the payment data."""
+        tx = self._create_transaction("redirect")
+        reference = self.env["payment.transaction"]._extract_reference(
+            "sslcommerz", self.payment_data
+        )
+        self.assertEqual(tx.reference, reference)
+
+    def test_extract_amount_data_returns_amount_and_currency(self):
+        """Test that the amount and currency are returned from the payment data."""
+        tx = self._create_transaction("redirect")
+        amount_data = tx._extract_amount_data(self.payment_data)
+        self.assertDictEqual(
+            amount_data, {"amount": tx.amount, "currency_code": tx.currency_id.name}
+        )
+
+    def test_apply_updates_sets_provider_reference(self):
+        """Test that the provider reference is set when processing the payment data."""
+        tx = self._create_transaction("redirect")
+        tx.with_context(payment_safe_write=True)._apply_updates(self.payment_data)
+        self.assertEqual(tx.provider_reference, self.payment_data["bank_tran_id"])
+
+    def test_apply_updates_sets_mastercard_payment_method(self):
+        """Test that the card payment method brand is updated from the payment data."""
+        self.payment_data.update({"card_brand": "MASTER"})
+        tx = self._create_transaction("redirect")
+        tx.with_context(payment_safe_write=True)._apply_updates(self.payment_data)
+        self.assertEqual(tx.payment_method_id.code, "mastercard")
+
+    def test_apply_updates_sets_mobile_banking_payment_method(self):
+        """Test that the mobile banking payment method is updated from the payment data."""
+        self.payment_data.update({"card_brand": "MOBILEBANKING"})
+        tx = self._create_transaction("redirect")
+        tx.with_context(payment_safe_write=True)._apply_updates(self.payment_data)
+        self.assertEqual(tx.payment_method_id.code, "mobile_banking")
+
+    def test_apply_updates_confirms_transaction(self):
+        """Test that the transaction state is set to 'done' when the payment data indicate a
+        successful payment."""
+        tx = self._create_transaction("redirect")
+        tx.with_context(payment_safe_write=True)._apply_updates(self.payment_data)
+        self.assertEqual(tx.state, "done")
+
+    def test_apply_updates_cancels_transaction(self):
+        """Test that the transaction state is set to 'cancel' when the payment data indicate a
+        canceled payment."""
+        tx = self._create_transaction("redirect")
+        tx.with_context(payment_safe_write=True)._apply_updates({"status": "CANCELLED"})
+        self.assertEqual(tx.state, "cancel")
+
+    def test_apply_updates_errors_transaction(self):
+        """Test that the transaction state is set to 'error' when the payment data indicate a
+        failed payment."""
+        tx = self._create_transaction("redirect")
+        tx.with_context(payment_safe_write=True)._apply_updates({"status": "FAILED"})
+        self.assertEqual(tx.state, "error")

odoo/odoo

addons/payment_sslcommerz/models/payment_provider.py
@@ -1,0 +1,69 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+from odoo.exceptions import ValidationError
+from odoo.tools import urls
+
+from odoo.addons.payment_sslcommerz import const
+
+
+class PaymentProvider(models.Model):
+    _inherit = "payment.provider"
+
+    code = fields.Selection(
+        selection_add=[("sslcommerz", "SSLCOMMERZ")], ondelete={"sslcommerz": "set default"}
+    )
+    sslcommerz_store_id = fields.Char(
+        string="SSLCOMMERZ Store ID", required_if_provider="sslcommerz", copy=False
+    )
+    sslcommerz_store_password = fields.Char(
+        string="SSLCOMMERZ Store Password",
+        required_if_provider="sslcommerz",
+        copy=False,
+        groups="base.group_system",
+    )
+
+    # === COMPUTE METHODS === #
+
+    def _get_supported_currencies(self):
+        """Override of `payment` to return the supported currencies."""
+        supported_currencies = super()._get_supported_currencies()
+        if self.code == "sslcommerz":
+            supported_currencies = supported_currencies.filtered(
+                lambda c: c.name in const.SUPPORTED_CURRENCIES
+            )
+        return supported_currencies
+
+    # === CRUD METHODS === #
+
+    def _get_default_payment_method_codes(self):
+        """Override of `payment` to return the default payment method codes."""
+        self.ensure_one()
+        if self.code != "sslcommerz":
+            return super()._get_default_payment_method_codes()
+        return const.DEFAULT_PAYMENT_METHOD_CODES
+
+    # === REQUEST HELPERS === #
+
+    def _build_request_url(self, endpoint, **kwargs):
+        """Override of `payment` to build the request URL."""
+        if self.code != "sslcommerz":
+            return super()._build_request_url(endpoint, **kwargs)
+
+        if self.is_live:
+            api_url = const.PAYMENT_API_LIVE_URL
+        else:
+            api_url = const.PAYMENT_API_TEST_URL
+        return urls.urljoin(api_url, endpoint)
+
+    def _parse_response_content(self, response, **kwargs):
+        """Override of `payment` to parse the response content."""
+        if self.code != "sslcommerz":
+            return super()._parse_response_content(response, **kwargs)
+
+        response_content = response.json()
+
+        if failed_reason := response_content.get("failedreason"):
+            raise ValidationError(failed_reason)
+
+        return response_content

addons/payment_sslcommerz/tests/common.py
@@ -1,0 +1,28 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo.addons.payment.tests.common import PaymentCommon
+
+
+class SSLCommerzCommon(PaymentCommon):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.sslcommerz = cls._prepare_provider(
+            "sslcommerz",
+            update_values={
+                "sslcommerz_store_id": "test_store",
+                "sslcommerz_store_password": "test_password",
+            },
+        )
+        cls.provider = cls.sslcommerz
+        cls.currency_bdt = cls._enable_currency("BDT")
+        cls.currency = cls.currency_bdt
+
+        cls.payment_data = {
+            "tran_id": cls.reference,
+            "status": "VALID",
+            "val_id": "260101012345",
+            "bank_tran_id": "230101012345",
+            "currency_amount": cls.amount,
+            "currency_type": cls.currency.name,
+        }

addons/payment_sslcommerz/tests/test_payment_transaction.py
@@ -1,0 +1,92 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo.tests import tagged
+from odoo.tools import urls
+
+from odoo.addons.payment_sslcommerz import const
+from odoo.addons.payment_sslcommerz.tests.common import SSLCommerzCommon
+
+
+@tagged("post_install", "-at_install")
+class TestPaymentTransaction(SSLCommerzCommon):
+    def test_no_item_missing_from_prepared_payload(self):
+        """Test that no expected item is missing from the payload sent to SSLCOMMERZ."""
+        payment_method = self.env.ref("payment_sslcommerz.payment_method_card")
+        tx = self._create_transaction("redirect", payment_method_id=payment_method.id)
+        base_url = tx.provider_id.get_base_url()
+        return_url = urls.urljoin(base_url, const.PAYMENT_RETURN_ROUTE)
+        expected_payload = {
+            "store_id": tx.provider_id.sslcommerz_store_id,
+            "store_passwd": tx.provider_id.sslcommerz_store_password,
+            "total_amount": tx.amount,
+            "currency": tx.currency_id.name,
+            "tran_id": tx.reference,
+            "success_url": return_url,
+            "fail_url": return_url,
+            "cancel_url": return_url,
+            "ipn_url": urls.urljoin(base_url, const.IPN_ROUTE),
+            "cus_name": tx.partner_name,
+            "cus_email": tx.partner_email,
+            "product_name": "Online Payment",
+            "product_category": "general",
+            "product_profile": "non-physical-goods",
+            "multi_card_name": const.PAYMENT_METHODS_MAPPING[tx.payment_method_code],
+        }
+        self.assertDictEqual(tx._sslcommerz_prepare_session_payload(), expected_payload)
+
+    def test_extract_reference_finds_reference(self):
+        """Test that the transaction reference is found in the payment data."""
+        tx = self._create_transaction("redirect")
+        reference = self.env["payment.transaction"]._extract_reference(
+            "sslcommerz", self.payment_data
+        )
+        self.assertEqual(tx.reference, reference)
+
+    def test_extract_amount_data_returns_amount_and_currency(self):
+        """Test that the amount and currency are returned from the payment data."""
+        tx = self._create_transaction("redirect")
+        amount_data = tx._extract_amount_data(self.payment_data)
+        self.assertDictEqual(
+            amount_data, {"amount": tx.amount, "currency_code": tx.currency_id.name}
+        )
+
+    def test_apply_updates_sets_provider_reference(self):
+        """Test that the provider reference is set when processing the payment data."""
+        tx = self._create_transaction("redirect")
+        tx.with_context(payment_safe_write=True)._apply_updates(self.payment_data)
+        self.assertEqual(tx.provider_reference, self.payment_data["bank_tran_id"])
+
+    def test_apply_updates_sets_mastercard_payment_method(self):
+        """Test that the card payment method brand is updated from the payment data."""
+        self.payment_data.update({"card_brand": "Mastercard"})
+        tx = self._create_transaction("redirect")
+        tx.with_context(payment_safe_write=True)._apply_updates(self.payment_data)
+        self.assertEqual(tx.payment_method_id.code, "mastercard")
+
+    def test_apply_updates_sets_bkash_payment_method(self):
+        """Test that the bKash payment method is updated from the payment data."""
+        self.payment_data.update({"card_brand": "MOBILEBANKING"})
+        tx = self._create_transaction("redirect")
+        tx.with_context(payment_safe_write=True)._apply_updates(self.payment_data)
+        self.assertEqual(tx.payment_method_id.code, "bkash")
+
+    def test_apply_updates_confirms_transaction(self):
+        """Test that the transaction state is set to 'done' when the payment data indicate a
+        successful payment."""
+        tx = self._create_transaction("redirect")
+        tx.with_context(payment_safe_write=True)._apply_updates(self.payment_data)
+        self.assertEqual(tx.state, "done")
+
+    def test_apply_updates_cancels_transaction(self):
+        """Test that the transaction state is set to 'cancel' when the payment data indicate a
+        canceled payment."""
+        tx = self._create_transaction("redirect")
+        tx.with_context(payment_safe_write=True)._apply_updates({"status": "CANCELLED"})
+        self.assertEqual(tx.state, "cancel")
+
+    def test_apply_updates_errors_transaction(self):
+        """Test that the transaction state is set to 'error' when the payment data indicate a
+        failed payment."""
+        tx = self._create_transaction("redirect")
+        tx.with_context(payment_safe_write=True)._apply_updates({"status": "FAILED"})
+        self.assertEqual(tx.state, "error")