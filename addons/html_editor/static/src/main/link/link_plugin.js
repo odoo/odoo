22e47c12bee544bd13fefb7838fa76d94baf6ea8
Sébastien Geelen <sge@odoo.com>
2026-09-22 14:35:59 +0200

    [FIX] html_editor: allow click inside link with format
    Since #284197 a pointerDown event inside a formated link was not setting the selection in the correct position.
    
    We fixed the issue by getting the clicked element closest link to ensure we are effectively outside a link.
    
    task-6592373
    
    closes odoo/odoo#289945
    
    closes odoo/odoo#290137
    
    Signed-off-by: David Monjoie (dmo) <dmo@odoo.com>

--- addons/html_editor/static/src/main/link/link_plugin.js
+++ addons/html_editor/static/src/main/link/link_plugin.js
@@ -279,7 +279,7 @@ export class LinkPlugin extends Plugin {
                 caretPosition.offset = range?.startOffset;
             }
             const link = caretPosition?.offsetNode && closestElement(caretPosition.offsetNode, "A");
-            if (clickedEl.nodeName === "A" && isZwnbsp(caretPosition.offsetNode)) {
+            if (clickedEl.closest("a") && isZwnbsp(caretPosition.offsetNode)) {
                 // This handles the case of clicking at the start of the button
                 const isFirstFeff = !caretPosition.offsetNode.previousSibling;
                 if (isFirstFeff && caretPosition.offset === 0) {
@@ -289,7 +289,7 @@ export class LinkPlugin extends Plugin {
                         anchorOffset: 1,
                     });
                 }
-            } else if (clickedEl.nodeName !== "A" && link) {
+            } else if (!clickedEl.closest("a") && link) {
                 // This handles the case of clicking outside the link that is
                 // at the start/end of paragraph
                 ev.preventDefault();
