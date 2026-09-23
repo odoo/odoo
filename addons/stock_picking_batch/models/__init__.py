660ccb694cbddfd70f04278bf5a2f87fb07fcd0f
Luis Marin <marin.guadarrama@gmail.com>
2026-09-23 13:00:40 -0600

    [REF] stock_picking_type: split py file, one class per file

--- addons/stock_picking_batch/models/__init__.py
+++ addons/stock_picking_batch/models/__init__.py
@@ -1,5 +1,6 @@
 from . import stock_move
 from . import stock_move_line
+from . import stock_picking_type
 from . import stock_picking
 from . import stock_picking_batch
 from . import stock_warehouse
