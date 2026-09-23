119274a9eb543735ac408848e623cf982e11cfdd
Ravij Parikh <snrav@odoo.com>
2026-09-23 19:40:15 +0530

    [IMP] remove size selector

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -26,73 +26,6 @@ from odoo.addons.website_sale.models.website import PRICELIST_SELECTED_SESSION_C
 _lt = LazyTranslate(__name__)
 
 
-class TableCompute:
-    def __init__(self):
-        self.table = {}
-
-    def _check_place(self, posx, posy, sizex, sizey, ppr):
-        res = True
-        for y in range(sizey):
-            for x in range(sizex):
-                if posx + x >= ppr:
-                    res = False
-                    break
-                row = self.table.setdefault(posy + y, {})
-                if row.setdefault(posx + x) is not None:
-                    res = False
-                    break
-            for x in range(ppr):
-                self.table[posy + y].setdefault(x, None)
-        return res
-
-    def process(self, products, ppg=20, ppr=4):
-        # Compute products positions on the grid
-        minpos = 0
-        maxy = 0
-        x = 0
-        for index, p in enumerate(products):
-            x = min(max(p.website_size_x, 1), ppr)
-            y = min(max(p.website_size_y, 1), ppr)
-            if index >= ppg:
-                x = y = 1
-
-            pos = minpos
-            while not self._check_place(pos % ppr, pos // ppr, x, y, ppr):
-                pos += 1
-            # if 21st products (index 20) and the last line is full (ppr products in it), break
-            # (pos + 1.0) / ppr is the line where the product would be inserted
-            # maxy is the number of existing lines
-            # + 1.0 is because pos begins at 0, thus pos 20 is actually the 21st block
-            # and to force python to not round the division operation
-            if index >= ppg and ((pos + 1.0) // ppr) > maxy:
-                break
-
-            if x == 1 and y == 1:  # simple heuristic for CPU optimization
-                minpos = pos // ppr
-
-            for y2 in range(y):
-                for x2 in range(x):
-                    self.table[(pos // ppr) + y2][(pos % ppr) + x2] = False
-            self.table[pos // ppr][pos % ppr] = {
-                "product": p,
-                "x": x,
-                "y": y,
-                "ribbon": p.sudo().website_ribbon_id,
-            }
-            if index <= ppg:
-                maxy = max(maxy, y + (pos // ppr))
-
-        # Format table according to HTML needs
-        rows = sorted(self.table.items())
-        rows = [r[1] for r in rows]
-        for col in range(len(rows)):
-            cols = sorted(rows[col].items())
-            x += len(cols)
-            rows[col] = [r[1] for r in cols if r[1]]
-
-        return rows
-
-
 def _get_parent_category_route(depth, param_name="_"):
     """Recursively build the parent category part of the route."""
     if depth < 1:
@@ -668,7 +601,6 @@ class WebsiteSale(payment_portal.PaymentPortal):
             "product_variants": product_variants,
             "search_product": search_product,
             "search_count": product_count,  # common for all searchbox
-            "bins": TableCompute().process(products, ppg, ppr),
             "ppg": ppg,
             "ppr": ppr,
             "gap": gap,
@@ -1940,8 +1872,6 @@ class WebsiteSale(payment_portal.PaymentPortal):
                 product.set_sequence_up()
             elif sequence == "down":
                 product.set_sequence_down()
-        if {"x", "y"} <= options.keys():
-            product.write({"website_size_x": options["x"], "website_size_y": options["y"]})
         if {"tag_field", "tag_ids"} <= options.keys():
             product.write({options["tag_field"]: options["tag_ids"] or False})
 
