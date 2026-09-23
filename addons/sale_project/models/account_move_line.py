d24e9ed9d9c4a6452b85ed3a25bb709594bf9923
Orrin Zhong <orzh@odoo.com>
2026-09-22 17:39:48 -0700

    [FIX] sale_project: prevent project analytic distributions on payment journal items
    Issue: When creating a payment for a vendor bill that was opened from a project, the payment's journal items can incorrectly receive the project's analytic distribution
    
    Steps to reproduce:
    1. Install sale_project and account_accountant
    2. Activate analytic accounting in the settings
    3. Add an outstanding account to the bank journal's manual outgoing payment method
    4. Create a new billable project
    5. From the project, open the Vendor Bills view and create a new bill from there. Notice that it correctly uses the project's analytic distribution
    6. Confirm the bill
    7. Register a payment for the bill
    8. Open the payment's journal entry and notice that it incorrectly has the project's analytic distribution
    
    Cause:
    When a vendor bill is opened from a project, project_id is added to the context so _compute_analytic_distribution applies the project's analytic distribution to the bill's lines. This is correct. However when creating a payment, the same context is used when creating the payment’s journal entry, causing the project’s analytic distribution to show up on the payment’s journal items as well
    
    Solution:
    In _compute_analytic_distribution, filter out payment journal items by adding the filter “and not line.move_id.origin_payment_id”. This ensures that the journal item is not a part of a payment journal entry. We check origin_payment_id because payment_ids may not be fully populated when _compute_analytic_distribution is run
    
    opw-6429622

--- addons/sale_project/models/account_move_line.py
+++ addons/sale_project/models/account_move_line.py
@@ -19,7 +19,7 @@ class AccountMoveLine(models.Model):
             project = self.env['project.project'].browse(project_id)
             # Payement lines could receive this context if the payment was created after opening the bill from a project
             lines = self.filtered(lambda line: line.account_type not in ['asset_receivable', 'liability_payable']
-                                  and not line.move_id.payment_ids)
+                                  and not line.move_id.payment_ids and not line.move_id.origin_payment_id)
             lines.analytic_distribution = project._get_analytic_distribution()
 
     def _get_so_mapping_domain(self):
