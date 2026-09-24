6f765d9c62b1826e34f639ef7e86fa648609ee8c
Ajit Singh <aksi@odoo.com>
2026-09-24 15:27:09 +0530

    [FIX] point_of_sale: fix product exclusion issue
    Step to reproduce:
    - Create 2 attributes with 2 values each A1V1 A1V2 and A2V1 A2V2 (both should be pills or color )
    - Create a product with these attributes and set the attribute exclusion so that
    only 2 valid combinations are possible.
    (ex: a1v1 excludes a2v2 and a1v2 excludes a2v1)
    - Open PoS and try to add the product to the cart.
    - The configurator popup will appear.
    
    Observation:
    - You will not be able to change the selection because the
    other combinations are not correct.
    
    Cause:
    - The issue was already fixed [1] but issue was reintroduced in [2]
    
    Fix:
    - Remove the code, which was causing the issue, we shouldn't disable an option
    and allow customer to change the combiantion
    
    opw-6585342

--- addons/point_of_sale/static/tests/unit/components/product_configurator_popup.test.js
+++ addons/point_of_sale/static/tests/unit/components/product_configurator_popup.test.js
@@ -1,5 +1,5 @@
 import { test, expect } from "@odoo/hoot";
-import { mountWithCleanup } from "@web/../tests/web_test_helpers";
+import { contains, mountWithCleanup } from "@web/../tests/web_test_helpers";
 import { setupPosEnv } from "../utils";
 import { ProductConfiguratorPopup } from "@point_of_sale/app/components/popups/product_configurator_popup/product_configurator_popup";
 import { definePosModels } from "../data/generate_model_definitions";
@@ -78,4 +78,69 @@ test("Extra price is shown when a percentage pricelist rule prices the product",
     // A percentage rule discounts the price the extra is part of, so it still counts.
     await mountConfigurator({ compute_price: "percentage", percent_price: 50 });
     expect(".price_extra").toHaveCount(1);
+});
+
+const setupExcludedValuesConfigurator = async (displayType) => {
+    const store = await setupPosEnv();
+    const productTemplate = store.models["product.template"].get(51);
+    const ptal = store.models["product.template.attribute.line"].get(5);
+
+    store.models["product.attribute"].get(10).display_type = displayType;
+    store.models["product.attribute"].get(7).display_type = displayType;
+
+    productTemplate.update({
+        attribute_line_ids: [productTemplate.attribute_line_ids[0], ptal],
+    });
+
+    const [choco, vanilla, s, m] = store.models["product.template.attribute.value"]
+        .getAll()
+        .filter((c) => [5, 6, 8, 9].includes(c.id));
+    choco.update({ excluded_value_ids: [m] });
+    vanilla.update({ excluded_value_ids: [s] });
+    store.processProductAttributes();
+
+    const popup = await mountWithCleanup(ProductConfiguratorPopup, {
+        props: {
+            productTemplate: productTemplate,
+            getPayload: () => {},
+            close: () => {},
+        },
+    });
+    return { dialog: popup, productTemplate: productTemplate };
+};
+
+test("pills: excluded value remains selectable and resolves to a valid combination", async () => {
+    await setupExcludedValuesConfigurator("pills");
+    expect("label[name='Chocolate']").toHaveClass("active");
+    expect("label[name='S']").toHaveClass("active");
+    expect(".btn:contains('Add')").not.toHaveClass("disabled");
+
+    await contains("label[name='Vanilla']").click();
+    expect("label[name='Vanilla']").toHaveClass("active");
+    expect(".btn:contains('Add')").toHaveClass("disabled");
+    await contains("label[name='M']").click();
+    expect("label[name='M']").toHaveClass("active");
+    expect(".btn:contains('Add')").not.toHaveClass("disabled");
+
+    await contains(".modal-dialog .btn:contains('Chocolate')").click();
+    expect("label[name='Chocolate']").toHaveClass("active");
+    expect(".btn:contains('Add')").toHaveClass("disabled");
+});
+
+test("color: excluded value remains selectable and resolves to a valid combination", async () => {
+    await setupExcludedValuesConfigurator("color");
+    expect("label[title='Chocolate']").toHaveClass("active");
+    expect("label[title='S']").toHaveClass("active");
+    expect(".btn:contains('Add')").not.toHaveClass("disabled");
+
+    await contains("label[title='Vanilla']").click();
+    expect("label[title='Vanilla']").toHaveClass("active");
+    expect(".btn:contains('Add')").toHaveClass("disabled");
+    await contains("label[title='M']").click();
+    expect("label[title='M']").toHaveClass("active");
+    expect(".btn:contains('Add')").not.toHaveClass("disabled");
+
+    await contains("label[title='Chocolate']").click();
+    expect("label[title='Chocolate']").toHaveClass("active");
+    expect(".btn:contains('Add')").toHaveClass("disabled");
 });
