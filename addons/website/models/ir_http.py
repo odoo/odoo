MoLeap/odooo

[IMP] website: add draft preview mode
This commit adds a draft mode for website. All changes you make in this
mode will be saved as a draft only, and won't be visible to your users
until you publish the changes.

We believe this option might be advanced for the end-user, so by
default, this mode isn't enabled. To enable it, you must toggle the
setting in the settings.

Right now, there's 2 options for publishing/deleting the draft:
- doing it for the whole website
- doing it for the page + shared components (header, styles, etc)

There's still some things that aren't working well with this option:
- Translation, you can't edit the draft content
- Search (it will search only published things)
- Widget values (e.g. product price)

task-5440958

addons/website/models/ir_http.py
@@ -271,10 +271,20 @@ class IrHttp(models.AbstractModel):
         else:
             allowed_company_ids = user.company_id.ids
 
+        extra_context = {}
+        # Allow to preview draft content by passing `?draft_preview=1` in the URL.
+        if (
+            request.httprequest.args.get('draft_preview')
+            and request.env['res.config.settings'].is_draft_preview_enabled()
+            and user.has_group('website.group_website_designer')
+        ):
+            extra_context['draft_preview'] = True
+
         request.update_context(
             allowed_company_ids=allowed_company_ids,
             website_id=website.id,
             **cls._get_editor_context(),
+            **extra_context,
         )
 
         request.website = website.with_context(request.env.context)

odoo/odoo

addons/website/models/ir_http.py
@@ -264,6 +264,14 @@ class IrHttp(models.AbstractModel):
         else:
             context['allowed_company_ids'] = user.company_id.ids
 
+        # Allow to preview draft content by passing `?draft_preview=1` in the URL.
+        if (
+            request.httprequest.args.get('draft_preview')
+            and request.env['res.config.settings'].is_draft_preview_enabled()
+            and user.has_group('website.group_website_designer')
+        ):
+            context['draft_preview'] = True
+
         request.update_context(**context)
 
     @classmethod